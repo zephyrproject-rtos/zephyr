baa9659d53fd4cf95bc4320905c5f4fd6e3ee700
Todor Minchev <todor.minchev@intel.com>
2015-03-30 19:13:20 +0100

    vxmicro-env.bash: more robust setting of VXMICRO_BASE
    VXMICRO_BASE is incorrectly set if vxmicro-env.bash is
    sourced from outside the repo root directory
    
    Change-Id: I9aa71bb2f994e5be3e5a0d64086223c54ce2b1a2
    Signed-off-by: Todor Minchev <todor.minchev@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- vxmicro-env.bash
+++ vxmicro-env.bash
@@ -9,7 +9,7 @@ fi
 # run (if it exists) by this script.
 
 # identify OS source tree root directory
-export VXMICRO_BASE=$(pwd)
+export VXMICRO_BASE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # prepend VxMicro build system tools to PATH, if not already present
 vxmicro_linux_bin=${VXMICRO_BASE}/host/x86-linux2/bin
