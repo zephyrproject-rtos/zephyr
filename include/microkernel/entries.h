68637b0fdc1716e24159f04426ba6caf42a2ec89
Peter Mitsis <peter.mitsis@windriver.com>
2015-04-15 16:10:45 -0400

    Remove K_memcpyrpl()
    The K_memcpyrpl() routine is not applicable to a single node system.
    
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    Change-Id: Ia778a6fc43e390a6c0478d641c206d2423a93c27

--- include/microkernel/entries.h
+++ include/microkernel/entries.h
@@ -108,8 +108,6 @@ extern void K_rawdata(struct k_args *DataPacket);
 extern void K_mvdsndack(struct k_args *SndDAck);
 extern void K_mvdrcvack(struct k_args *RcvDAck);
 
-extern void K_memcpyrpl(struct k_args *Stopper);
-
 extern void K_ChSendReq(struct k_args *Writer);
 extern void K_ChSendTmo(struct k_args *Writer);
 extern void K_ChSendRpl(struct k_args *Writer);
