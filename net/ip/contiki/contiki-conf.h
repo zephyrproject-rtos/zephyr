1cf34680cb05c9c76f6bd142bac91ce7e051fc4c
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-04-22 09:58:33 +0300

    net: Using correct defines to configure the net stack
    Change-Id: Ia2f5f8e496d11c73125aa3e74c0f9ad48141c712
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- net/ip/contiki/contiki-conf.h
+++ net/ip/contiki/contiki-conf.h
@@ -13,4 +13,28 @@
 typedef uint32_t clock_time_t;
 typedef unsigned int uip_stats_t;
 
+#define CLOCK_CONF_SECOND 100
+
+/* No TCP yet */
+#define UIP_CONF_TCP 0
+
+/* We do not want to be a router */
+#define UIP_CONF_ROUTER 0
+
+/* No Rime */
+#define NETSTACK_CONF_WITH_RIME 0
+
+/* No reassembly yet */
+#define SICSLOWPAN_CONF_FRAG 0
+
+#define NETSTACK_CONF_NETWORK	uip_driver
+#define NETSTACK_CONF_FRAMER	framer_802154
+#define NETSTACK_CONF_LLSEC	nullsec_driver
+#define NETSTACK_CONF_MAC	csma_driver
+#define NETSTACK_CONF_RDC	sicslowmac_driver
+
+#ifndef NETSTACK_CONF_RADIO
+/* #error "No radio configured, cannot continue!" */
+#endif
+
 #endif /* __CONTIKI_CONF_H__ */
