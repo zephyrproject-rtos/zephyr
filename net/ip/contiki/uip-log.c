7d7d4e41615bc1ab6397e96c3fb73301799522dc
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-04-28 16:23:43 +0300

    net: Enable logging in uIP stack
    Allow the uIP stack to print information about its internal
    status. By default the logging is disabled.
    
    Change-Id: I55a03051d3f541b5f6ea950fdd6406814f937197
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

---
+++ net/ip/contiki/uip-log.c
@@ -0,0 +1,61 @@
+/* uip-log.c - Logging support */
+
+/*
+ * Copyright (c) 2015 Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1) Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2) Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3) Neither the name of Intel Corporation nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <misc/printk.h>
+
+#if defined(CONFIG_STDOUT_CONSOLE)
+#include <stdio.h>
+#define PRINT           printf
+#else
+#include <misc/printk.h>
+#define PRINT           printk
+#endif
+
+#include "ip/uipopt.h"
+#include "sys/log.h"
+
+#if UIP_LOGGING
+void uip_log(char *message)
+{
+	PRINT(message);
+	PRINT("\n");
+}
+#endif /* UIP_LOGGING */
+
+#if LOG_CONF_ENABLED
+void log_message(const char *part1, const char *part2)
+{
+	PRINT(part1);
+	PRINT(part2);
+	PRINT("\n");
+}
+#endif /* LOG_CONF_ENABLED */
