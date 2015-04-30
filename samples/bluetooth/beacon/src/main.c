8fc12c160030d3d362d0596fb302a0982e7c2d49
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-04-30 17:08:16 +0300

    Bluetooth: Add LE beacon sample app
    Adds a sample which initializes Bluetooth and enables non-connectable
    advertising (i.e. starts acting in the LE broadcaster role).
    
    Change-Id: Ic554b1354464b357b44215af1c0b613f123aeb5d
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

---
+++ samples/bluetooth/beacon/src/main.c
@@ -0,0 +1,86 @@
+/* main.c - Application main entry point */
+
+/*
+ * Copyright (c) 2015 Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1) Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2) Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3) Neither the name of Intel Corporation nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdint.h>
+#include <stddef.h>
+#include <string_s.h>
+
+#include "bluetooth/bluetooth.h"
+#include "bluetooth/hci.h"
+
+const struct bt_eir ad[] = {
+	{
+		.len = 3,
+		.type = BT_EIR_UUID16_ALL,
+		.data = { 0xd8, 0xfe },
+	},
+	{
+		.len = 9,
+		.type = BT_EIR_SVC_DATA16,
+		.data = { 0xd8, 0xfe, 0x00, 0x20, 0x02, '0', '1',
+			  0x08 },
+	},
+	{ }
+};
+const struct bt_eir sd[] = {
+	{
+		.len = 16,
+		.type = BT_EIR_NAME_COMPLETE,
+		.data = "Test beacon",
+	},
+	{ }
+};
+
+#ifdef CONFIG_MICROKERNEL
+void mainloop(void)
+#else
+void main(void)
+#endif
+{
+	int err;
+
+	err = bt_init();
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	err = bt_start_advertising(BT_LE_ADV_SCAN_IND, ad, sd);
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Beacon started\n");
+}
