9c27fbac74b033b8a911da36c1ae1a3b49d6dd39
Allan Stephens <allan.stephens@windriver.com>
2015-05-06 14:18:06 -0400

    Relocate idle-related microkernel code
    Consolidates all architecture-independent microkernel code dealing
    with advanced power management into the main microkernel idling file,
    so that it can be more easily maintained.
    
    Change-Id: I140575508e32fe25d5210b8611ad1ffdf55b7529
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>

--- kernel/microkernel/idle.c
+++ kernel/microkernel/idle.c
@@ -211,7 +211,98 @@ void workload_time_slice_set(int32_t t)
 #endif
 }
 
+unsigned char _sys_power_save_flag = 1;
+
 #if defined(CONFIG_ADVANCED_POWER_MANAGEMENT)
+
+#include <cputype.h>
+#include <nanokernel.h>
+#include <nanokernel/cpu.h>
+#include <microkernel/k_types.h>
+#ifdef CONFIG_ADVANCED_IDLE
+#include <advidle.h>
+#endif
+#if defined(CONFIG_TICKLESS_IDLE)
+#include <drivers/system_timer.h>
+#endif
+
+extern void nano_cpu_idle(void);
+extern void nano_cpu_set_idle(int32_t ticks);
+
+#if defined(CONFIG_TICKLESS_IDLE)
+/*
+ * Idle time must be this value or higher for timer to go into tickless idle
+ * state.
+ */
+int32_t _sys_idle_threshold_ticks =
+	CONFIG_TICKLESS_IDLE_THRESH;
+#endif /* CONFIG_TICKLESS_IDLE */
+
+/*******************************************************************************
+*
+* _sys_power_save_idle - power management policy when kernel is idle
+*
+* This routine implements the power management policy based on the time
+* until the timer expires, in system ticks.
+* Routine is invoked from the idle task with interrupts disabled
+*
+* RETURNS: N/A
+*
+* \NOMANUAL
+*/
+
+void _sys_power_save_idle(int32_t ticks)
+{
+#if defined(CONFIG_TICKLESS_IDLE)
+	if ((ticks == -1) || ticks >= _sys_idle_threshold_ticks) {
+		/* Put the system timer into idle state until the next timer
+		 * event */
+		_timer_idle_enter(ticks);
+	}
+#endif /* CONFIG_TICKLESS_IDLE */
+#ifdef CONFIG_ADVANCED_IDLE
+	/*
+	 * Call the advanced sleep function, which checks if the system should
+	 * enter a deep sleep state. If so, the function will return a non-zero
+	 * value when the system resumes here after the deep sleep ends.
+	 * If the time to sleep is too short to go to advanced sleep mode, the
+	 * function returns zero immediately and we do normal idle processing.
+	 */
+
+	if (_AdvIdleFunc(ticks) == 0) {
+		nano_cpu_set_idle(ticks);
+		nano_cpu_idle();
+	}
+#else
+	nano_cpu_set_idle(ticks);
+	nano_cpu_idle();
+#endif /* CONFIG_ADVANCED_IDLE */
+}
+
+/*******************************************************************************
+*
+* _sys_power_save_idle_exit - power management policy when kernel leaves idle state
+*
+* This routine implements the power management policy when kernel leaves idle
+* state. Routine can be modified to wake up other devices.
+* The routine is invoked from interrupt context, with interrupts disabled.
+*
+* RETURNS: N/A
+*
+* \NOMANUAL
+*/
+
+void _sys_power_save_idle_exit(int32_t ticks)
+{
+#ifdef CONFIG_TICKLESS_IDLE
+	if ((ticks == -1) || ticks >= _sys_idle_threshold_ticks) {
+		_timer_idle_exit();
+	}
+#else
+	ARG_UNUSED(ticks);
+#endif /* CONFIG_TICKLESS_IDLE */
+}
+
 /*******************************************************************************
 *
 * _get_next_timer_expiry - obtain number of ticks until next timer expires
