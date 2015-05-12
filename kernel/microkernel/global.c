a139256afcb0654825a6a156c4ad4999f02ce044
Allan Stephens <allan.stephens@windriver.com>
2015-05-12 14:19:30 -0400

    Enhance microkernel command stack size bounds checking
    Ensures that the microkernel's command stack has at least one element
    at project configuration time, rather than at project build time.
    
    Change-Id: I74f10bcb0d0275854801b50d5160fc85fb911250
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>

--- kernel/microkernel/global.c
+++ kernel/microkernel/global.c
@@ -52,9 +52,6 @@ struct nano_stack __noinit _k_command_stack;
 int _k_debug_halt = 0;
 #endif
 
-#if CONFIG_COMMAND_STACK_SIZE <= 0
-#error COMMAND_STACK_SIZE must be greater than zero
-#endif
 uint32_t __noinit _k_server_command_stack_storage[CONFIG_COMMAND_STACK_SIZE];
 
 extern void K_swapper(int i1, int i2);
