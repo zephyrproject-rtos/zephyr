76103ecc2b858b705b0e74fc575c341251c49fd2
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-06-14 13:44:34 -0400

    test_sema: test timeout APIs
    Test the nano_xxx_sem_wait_timeout() APIs.
    
    First, the task waits with a timeout and times out. Then it wait with a
    timeout, but gets the semaphore in time.
    
    Then, multiple timeout tests are done for the fibers, to test the
    ordering of queueing/dequeueing when timeout occurs, first on one
    semaphore, then on multiple semaphores.
    
    Finally, multiple fibers pend on one semaphore, and they all get the
    semaphore in time, except the last one: this tests that the timeout is
    recomputed correctly when timeouts are aborted.
    
    Change-Id: I73c174e6f15f38377a14573cb7f531e5e6ca4c07
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>

--- samples/nanokernel/test/test_sema/README.txt
+++ samples/nanokernel/test/test_sema/README.txt
@@ -54,6 +54,32 @@ multiple-waiter fiber 0 acquired semaphore, sending reply
 multiple-waiter fiber 1 acquired semaphore, sending reply
 multiple-waiter fiber 2 acquired semaphore, sending reply
 Task took multi-waiter reply semaphore 3 times, as expected.
+test nano_task_sem_take_wait_timeout with timeout > 0
+nano_task_sem_take_wait_timeout timed out as expected
+nano_task_sem_take_wait_timeout got sem in time, as expected
+testing timeouts of 5 fibers on same sem
+ got fiber (q order: 2, t/o: 10, sem: 200001c8) as expected
+ got fiber (q order: 3, t/o: 15, sem: 200001c8) as expected
+ got fiber (q order: 0, t/o: 20, sem: 200001c8) as expected
+ got fiber (q order: 4, t/o: 25, sem: 200001c8) as expected
+ got fiber (q order: 1, t/o: 30, sem: 200001c8) as expected
+testing timeouts of 9 fibers on different sems
+ got fiber (q order: 0, t/o: 10, sem: 200001d4) as expected
+ got fiber (q order: 5, t/o: 15, sem: 200001c8) as expected
+ got fiber (q order: 7, t/o: 20, sem: 200001c8) as expected
+ got fiber (q order: 1, t/o: 25, sem: 200001c8) as expected
+ got fiber (q order: 8, t/o: 30, sem: 200001d4) as expected
+ got fiber (q order: 2, t/o: 35, sem: 200001c8) as expected
+ got fiber (q order: 6, t/o: 40, sem: 200001c8) as expected
+ got fiber (q order: 4, t/o: 45, sem: 200001d4) as expected
+ got fiber (q order: 3, t/o: 50, sem: 200001d4) as expected
+testing 5 fibers timing out, but obtaining the sem in time
+(except the last one, which times out)
+ got fiber (q order: 0, t/o: 20, sem: 200001c8) as expected
+ got fiber (q order: 1, t/o: 30, sem: 200001c8) as expected
+ got fiber (q order: 2, t/o: 10, sem: 200001c8) as expected
+ got fiber (q order: 3, t/o: 15, sem: 200001c8) as expected
+ got fiber (q order: 4, t/o: 25, sem: 200001c8) as expected
 ===================================================================
 PASS - main.
 ===================================================================
--- samples/nanokernel/test/test_sema/src/sema.c
+++ samples/nanokernel/test/test_sema/src/sema.c
@@ -1,7 +1,7 @@
 /* sema.c - test nanokernel semaphore APIs */
 
 /*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ * Copyright (c) 2012-2015 Wind River Systems, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -32,13 +32,14 @@
 
 /*
 DESCRIPTION
-This module tests three basic scenarios with the usage of the following semaphore
+This module tests four basic scenarios with the usage of the following semaphore
 routines:
 
    nano_sem_init
    nano_fiber_sem_give, nano_fiber_sem_take, nano_fiber_sem_take_wait
    nano_task_sem_give, nano_task_sem_take, nano_task_sem_take_wait
    nano_isr_sem_give, nano_isr_sem_take
+   nano_fiber_sem_take_wait_timeout, nano_task_sem_take_wait_timeout
 
 Scenario #1:
    A task, fiber or ISR does not wait for the semaphore when taking it.
@@ -48,10 +49,14 @@ Scenario #2:
 
 Scenario #3:
    Multiple fibers pend on the same semaphore.
+
+Scenario #4:
+   Timeout scenarios with multiple semaphores and fibers.
 */
 
 #include <tc_util.h>
 #include <arch/cpu.h>
+#include <misc/util.h>
 
 /* test uses 2 software IRQs */
 #define NUM_SW_IRQS 2
@@ -514,6 +519,421 @@ static int test_multiple_waiters(void)
 	return TC_PASS;
 }
 
+/* timeout tests
+ *
+ * Test the nano_xxx_sem_wait_timeout() APIs.
+ *
+ * First, the task waits with a timeout and times out. Then it wait with a
+ * timeout, but gets the semaphore in time.
+ *
+ * Then, multiple timeout tests are done for the fibers, to test the ordering
+ * of queueing/dequeueing when timeout occurs, first on one semaphore, then on
+ * multiple semaphores.
+ *
+ * Finally, multiple fibers pend on one semaphore, and they all get the
+ * semaphore in time, except the last one: this tests that the timeout is
+ * recomputed correctly when timeouts are aborted.
+ */
+
+#include <tc_nano_timeout_common.h>
+
+static struct nano_sem sem_timeout[2];
+struct nano_fifo timeout_order_fifo;
+
+struct reply_packet {
+	void *link_in_fifo;
+	int reply;
+};
+
+struct timeout_order_data {
+	void *link_in_fifo;
+	struct nano_sem *sem;
+	int32_t timeout;
+	int timeout_order;
+	int q_order;
+};
+
+struct timeout_order_data timeout_order_data[] = {
+	{0, &sem_timeout[0], TIMEOUT(2), 2, 0},
+	{0, &sem_timeout[0], TIMEOUT(4), 4, 1},
+	{0, &sem_timeout[0], TIMEOUT(0), 0, 2},
+	{0, &sem_timeout[0], TIMEOUT(1), 1, 3},
+	{0, &sem_timeout[0], TIMEOUT(3), 3, 4},
+};
+
+struct timeout_order_data timeout_order_data_mult_sem[] = {
+	{0, &sem_timeout[1], TIMEOUT(0), 0, 0},
+	{0, &sem_timeout[0], TIMEOUT(3), 3, 1},
+	{0, &sem_timeout[0], TIMEOUT(5), 5, 2},
+	{0, &sem_timeout[1], TIMEOUT(8), 8, 3},
+	{0, &sem_timeout[1], TIMEOUT(7), 7, 4},
+	{0, &sem_timeout[0], TIMEOUT(1), 1, 5},
+	{0, &sem_timeout[0], TIMEOUT(6), 6, 6},
+	{0, &sem_timeout[0], TIMEOUT(2), 2, 7},
+	{0, &sem_timeout[1], TIMEOUT(4), 4, 8},
+};
+
+#define TIMEOUT_ORDER_NUM_FIBERS ARRAY_SIZE(timeout_order_data_mult_sem)
+static char __stack timeout_stacks[TIMEOUT_ORDER_NUM_FIBERS][FIBER_STACKSIZE];
+
+/* a fiber sleeps then gives a semaphore */
+static void test_fiber_give_timeout(int sem, int timeout)
+{
+	fiber_sleep((int32_t)timeout);
+	nano_fiber_sem_give((struct nano_sem *)sem);
+}
+
+/* a fiber pends on a semaphore then times out */
+static void test_fiber_pend_and_timeout(int data, int unused)
+{
+	struct timeout_order_data *the_data = (void *)data;
+	int32_t orig_ticks = nano_tick_get();
+	int rv;
+
+	ARG_UNUSED(unused);
+
+	rv = nano_fiber_sem_take_wait_timeout(the_data->sem, the_data->timeout);
+	if (rv) {
+		TC_ERROR(" *** timeout of %d did not time out.\n",
+					the_data->timeout);
+		return;
+	}
+	if (!is_timeout_in_range(orig_ticks, the_data->timeout)) {
+		return;
+	}
+
+	nano_fiber_fifo_put(&timeout_order_fifo, the_data);
+}
+
+/* the task spins several fibers that pend and timeout on sempahores */
+static int test_multiple_fibers_pending(struct timeout_order_data *test_data,
+										int test_data_size)
+{
+	int ii;
+
+	for (ii = 0; ii < test_data_size; ii++) {
+		task_fiber_start(timeout_stacks[ii], FIBER_STACKSIZE,
+							test_fiber_pend_and_timeout,
+							(int)&test_data[ii], 0,
+							FIBER_PRIORITY, 0);
+	}
+
+	for (ii = 0; ii < test_data_size; ii++) {
+		struct timeout_order_data *data =
+			nano_task_fifo_get_wait(&timeout_order_fifo);
+
+		if (data->timeout_order == ii) {
+			TC_PRINT(" got fiber (q order: %d, t/o: %d, sem: %p) as expected\n",
+						data->q_order, data->timeout, data->sem);
+		} else {
+			TC_ERROR(" *** fiber %d woke up, expected %d\n",
+						data->timeout_order, ii);
+			return TC_FAIL;
+		}
+	}
+
+	return TC_PASS;
+}
+
+/* a fiber pends on a semaphore with a timeout and gets the semaphore in time */
+static void test_fiber_pend_and_get_sem(int data, int unused)
+{
+	struct timeout_order_data *the_data = (void *)data;
+	int rv;
+
+	ARG_UNUSED(unused);
+
+	rv = nano_fiber_sem_take_wait_timeout(the_data->sem, the_data->timeout);
+	if (!rv) {
+		TC_PRINT(" *** fiber (q order: %d, t/o: %d, sem: %p) timed out!\n",
+						the_data->q_order, the_data->timeout, the_data->sem);
+		return;
+	}
+
+	nano_fiber_fifo_put(&timeout_order_fifo, the_data);
+}
+
+/* the task spins fibers that get the sem in time, except the last one */
+static int test_multiple_fibers_get_sem(struct timeout_order_data *test_data,
+										int test_data_size)
+{
+	struct timeout_order_data *data;
+	int ii;
+
+	for (ii = 0; ii < test_data_size-1; ii++) {
+		task_fiber_start(timeout_stacks[ii], FIBER_STACKSIZE,
+							test_fiber_pend_and_get_sem,
+							(int)&test_data[ii], 0,
+							FIBER_PRIORITY, 0);
+	}
+	task_fiber_start(timeout_stacks[ii], FIBER_STACKSIZE,
+						test_fiber_pend_and_timeout,
+						(int)&test_data[ii], 0,
+						FIBER_PRIORITY, 0);
+
+	for (ii = 0; ii < test_data_size-1; ii++) {
+		nano_task_sem_give(test_data[ii].sem);
+
+		data = nano_task_fifo_get_wait(&timeout_order_fifo);
+
+		if (data->q_order == ii) {
+			TC_PRINT(" got fiber (q order: %d, t/o: %d, sem: %p) as expected\n",
+						data->q_order, data->timeout, data->sem);
+		} else {
+			TC_ERROR(" *** fiber %d woke up, expected %d\n",
+						data->q_order, ii);
+			return TC_FAIL;
+		}
+	}
+
+	data = nano_task_fifo_get_wait(&timeout_order_fifo);
+	if (data->q_order == ii) {
+		TC_PRINT(" got fiber (q order: %d, t/o: %d, sem: %p) as expected\n",
+					data->q_order, data->timeout, data->sem);
+	} else {
+		TC_ERROR(" *** fiber %d woke up, expected %d\n",
+					data->timeout_order, ii);
+		return TC_FAIL;
+	}
+
+	return TC_PASS;
+}
+
+static void test_fiber_ticks_special_values(int packet, int special_value)
+{
+	struct reply_packet *reply_packet = (void *)packet;
+
+	reply_packet->reply =
+		nano_fiber_sem_take_wait_timeout(&sem_timeout[0], special_value);
+	nano_fiber_fifo_put(&timeout_order_fifo, reply_packet);
+}
+
+/* the timeout test entry point */
+static int test_timeout(void)
+{
+	int64_t orig_ticks;
+	int32_t timeout;
+	int rv;
+	int test_data_size;
+	struct reply_packet reply_packet;
+
+	nano_sem_init(&sem_timeout[0]);
+	nano_sem_init(&sem_timeout[1]);
+	nano_fifo_init(&timeout_order_fifo);
+
+	/* test nano_task_sem_take_wait_timeout() with timeout */
+	timeout = 10;
+	orig_ticks = nano_tick_get();
+	rv = nano_task_sem_take_wait_timeout(&sem_timeout[0], timeout);
+	if (rv) {
+		TC_ERROR(" *** timeout of %d did not time out.\n", timeout);
+		return TC_FAIL;
+	}
+	if ((nano_tick_get() - orig_ticks) < timeout) {
+		TC_ERROR(" *** task did not wait long enough on timeout of %d.\n",
+					timeout);
+		return TC_FAIL;
+	}
+
+	/* test nano_task_sem_take_wait_timeout with timeout of 0 */
+
+	rv = nano_task_sem_take_wait_timeout(&sem_timeout[0], 0);
+	if (rv) {
+		TC_ERROR(" *** timeout of 0 did not time out.\n");
+		return TC_FAIL;
+	}
+
+	/* test nano_task_sem_take_wait_timeout with timeout > 0 */
+
+	TC_PRINT("test nano_task_sem_take_wait_timeout with timeout > 0\n");
+
+	timeout = 3;
+	orig_ticks = nano_tick_get();
+
+	rv = nano_task_sem_take_wait_timeout(&sem_timeout[0], timeout);
+
+	if (rv) {
+		TC_ERROR(" *** timeout of %d did not time out.\n",
+				timeout);
+		return TC_FAIL;
+	}
+
+	if (!is_timeout_in_range(orig_ticks, timeout)) {
+		return TC_FAIL;
+	}
+
+	TC_PRINT("nano_task_sem_take_wait_timeout timed out as expected\n");
+
+	/*
+	 * test nano_task_sem_take_wait_timeout with a timeout and fiber that gives
+	 * the semaphore on time
+	 */
+
+	timeout = 5;
+	orig_ticks = nano_tick_get();
+
+	task_fiber_start(timeout_stacks[0], FIBER_STACKSIZE,
+						test_fiber_give_timeout, (int)&sem_timeout[0],
+						timeout,
+						FIBER_PRIORITY, 0);
+
+	rv = nano_task_sem_take_wait_timeout(&sem_timeout[0], (int)(timeout + 5));
+	if (!rv) {
+		TC_ERROR(" *** timed out even if semaphore was given in time.\n");
+		return TC_FAIL;
+	}
+
+	if (!is_timeout_in_range(orig_ticks, timeout)) {
+		return TC_FAIL;
+	}
+
+	TC_PRINT("nano_task_sem_take_wait_timeout got sem in time, as expected\n");
+
+	/*
+	 * test nano_task_sem_take_wait_timeout with TICKS_NONE and the
+	 * semaphore unavailable.
+	 */
+
+	if (nano_task_sem_take_wait_timeout(&sem_timeout[0], TICKS_NONE)) {
+		TC_ERROR("task with TICKS_NONE got sem, but shouldn't have\n");
+		return TC_FAIL;
+	}
+
+	TC_PRINT("task with TICKS_NONE did not get sem, as expected\n");
+
+	/*
+	 * test nano_task_sem_take_wait_timeout with TICKS_NONE and the
+	 * semaphore available.
+	 */
+
+	nano_task_sem_give(&sem_timeout[0]);
+	if (!nano_task_sem_take_wait_timeout(&sem_timeout[0], TICKS_NONE)) {
+		TC_ERROR("task with TICKS_NONE did not get available sem\n");
+		return TC_FAIL;
+	}
+
+	TC_PRINT("task with TICKS_NONE got available sem, as expected\n");
+
+	/*
+	 * test nano_task_sem_take_wait_timeout with TICKS_UNLIMITED and the
+	 * semaphore available.
+	 */
+
+	TC_PRINT("Trying to take available sem with TICKS_UNLIMITED:\n"
+			 " will hang the test if it fails.\n");
+
+	nano_task_sem_give(&sem_timeout[0]);
+	if (!nano_task_sem_take_wait_timeout(&sem_timeout[0], TICKS_UNLIMITED)) {
+		TC_ERROR(" *** This will never be hit!!! .\n");
+		return TC_FAIL;
+	}
+
+	TC_PRINT("task with TICKS_UNLIMITED got available sem, as expected\n");
+
+	/* test fiber with timeout of TICKS_NONE not getting empty semaphore */
+
+	task_fiber_start(timeout_stacks[0], FIBER_STACKSIZE,
+						test_fiber_ticks_special_values,
+						(int)&reply_packet, TICKS_NONE, FIBER_PRIORITY, 0);
+
+	if (!nano_task_fifo_get(&timeout_order_fifo)) {
+		TC_ERROR(" *** fiber should have run and filled the fifo.\n");
+		return TC_FAIL;
+	}
+
+	if (reply_packet.reply != 0) {
+		TC_ERROR(" *** fiber should not have obtained the semaphore.\n");
+		return TC_FAIL;
+	}
+
+	TC_PRINT("fiber with TICKS_NONE did not get sem, as expected\n");
+
+	/* test fiber with timeout of TICKS_NONE getting full semaphore */
+
+	nano_task_sem_give(&sem_timeout[0]);
+
+	task_fiber_start(timeout_stacks[0], FIBER_STACKSIZE,
+						test_fiber_ticks_special_values,
+						(int)&reply_packet, TICKS_NONE, FIBER_PRIORITY, 0);
+
+	if (!nano_task_fifo_get(&timeout_order_fifo)) {
+		TC_ERROR(" *** fiber should have run and filled the fifo.\n");
+		return TC_FAIL;
+	}
+
+	if (reply_packet.reply != 1) {
+		TC_ERROR(" *** fiber should have obtained the semaphore.\n");
+		return TC_FAIL;
+	}
+
+	TC_PRINT("fiber with TICKS_NONE got available sem, as expected\n");
+
+	/* test fiber with timeout of TICKS_UNLIMITED getting full semaphore */
+
+	nano_task_sem_give(&sem_timeout[0]);
+
+	task_fiber_start(timeout_stacks[0], FIBER_STACKSIZE,
+						test_fiber_ticks_special_values,
+						(int)&reply_packet, TICKS_UNLIMITED, FIBER_PRIORITY, 0);
+
+	if (!nano_task_fifo_get(&timeout_order_fifo)) {
+		TC_ERROR(" *** fiber should have run and filled the fifo.\n");
+		return TC_FAIL;
+	}
+
+	if (reply_packet.reply != 1) {
+		TC_ERROR(" *** fiber should have obtained the semaphore.\n");
+		return TC_FAIL;
+	}
+
+	TC_PRINT("fiber with TICKS_UNLIMITED got available sem, as expected\n");
+
+	/* test multiple fibers pending on the same sem with different timeouts */
+
+	test_data_size = ARRAY_SIZE(timeout_order_data);
+
+	TC_PRINT("testing timeouts of %d fibers on same sem\n", test_data_size);
+
+	rv = test_multiple_fibers_pending(timeout_order_data, test_data_size);
+	if (rv != TC_PASS) {
+		TC_ERROR(" *** fibers did not time out in the right order\n");
+		return TC_FAIL;
+	}
+
+	/* test multiple fibers pending on different sems with different timeouts */
+
+	test_data_size = ARRAY_SIZE(timeout_order_data_mult_sem);
+
+	TC_PRINT("testing timeouts of %d fibers on different sems\n",
+				test_data_size);
+
+	rv = test_multiple_fibers_pending(timeout_order_data_mult_sem,
+										test_data_size);
+	if (rv != TC_PASS) {
+		TC_ERROR(" *** fibers did not time out in the right order\n");
+		return TC_FAIL;
+	}
+
+	/*
+	 * test multiple fibers pending on same sem with different timeouts, but
+	 * getting the semaphore in time, except the last one.
+	 */
+
+	test_data_size = ARRAY_SIZE(timeout_order_data);
+
+	TC_PRINT("testing %d fibers timing out, but obtaining the sem in time\n"
+				"(except the last one, which times out)\n",
+				test_data_size);
+
+	rv = test_multiple_fibers_get_sem(timeout_order_data, test_data_size);
+	if (rv != TC_PASS) {
+		TC_ERROR(" *** fibers did not get the sem in the right order\n");
+		return TC_FAIL;
+	}
+
+	return TC_PASS;
+}
+
 /*******************************************************************************
 *
 * main - entry point to semaphore tests
@@ -557,6 +977,11 @@ void main(void)
 	}
 
 	rv = test_multiple_waiters();
+	if (rv != TC_PASS) {
+		goto doneTests;
+	}
+
+	rv = test_timeout();
 	if (rv != TC_PASS) {
 		goto doneTests;
 	}
