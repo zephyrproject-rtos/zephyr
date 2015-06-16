4ccef33723b40652d8cb81eed037848c9383005b
Mariusz Skamra <mariusz.skamra@tieto.com>
2015-06-16 16:18:57 +0200

    Bluetooth: Add interactive connection and disconnection commands
    Adds above commands to the shell app.
    Connection is made based on given address and type eg:
    btshell> connect 00:00:00:00:00:00 public
    
    In callback we get local connection id which is in fact,
    the number of array entry in which the pointer to the corresponding
    bt_conn has been written.
    
    To disconnect, just provide the id eg:
    btshell> disconnect 0
    
    Change-Id: I24f7ec2de7f84c53ab847ded28a1821753c2a60d
    Signed-off-by: Mariusz Skamra <mariusz.skamra@tieto.com>

--- samples/bluetooth/shell/src/main.c
+++ samples/bluetooth/shell/src/main.c
@@ -30,14 +30,50 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <errno.h>
 #include <stdint.h>
 #include <stddef.h>
+#include <string.h>
 #include <misc/printk.h>
 
 #include <console/uart_console.h>
 #include <bluetooth/bluetooth.h>
 
 #include "btshell.h"
+
+static struct bt_conn *conns[CONFIG_BLUETOOTH_MAX_CONN];
+
+static void connected(struct bt_conn *conn)
+{
+	int i;
+
+	for (i = 0; i < CONFIG_BLUETOOTH_MAX_CONN + 1; i++) {
+		if (conns[i] == NULL) {
+			conns[i] = bt_conn_get(conn);;
+			printk("Connected %p, id %d\n", conn, i);
+			return;
+		}
+	}
+}
+
+static void disconnected(struct bt_conn *conn)
+{
+	int i;
+
+	for (i = 0; i < CONFIG_BLUETOOTH_MAX_CONN + 1; i++) {
+		if (conns[i] == conn) {
+			bt_conn_put(conn);
+			conns[i] = NULL;
+			printk("Disconnected %p\n", conn);
+			return;
+		}
+	}
+}
+
+static struct bt_conn_cb conn_callbacks = {
+		.connected = connected,
+		.disconnected = disconnected,
+};
 
 static int cmd_init(int argc, const char *line)
 {
@@ -53,6 +89,85 @@ static int cmd_init(int argc, const char *line)
 	return 0;
 }
 
+static int str2bt_addr_le(const char *str, bt_addr_le_t *addr)
+{
+	int i, j;
+
+	if (strlen(str) != 17) {
+		return -EINVAL;
+	}
+
+	for (i = 5, j = 1; *str != '\0'; str++, j++) {
+		if (!(j % 3) && (*str != ':')) {
+			return -EINVAL;
+		} else if (*str == ':') {
+			i--;
+			continue;
+		}
+
+		addr->val[i] = addr->val[i] << 4;
+
+		if (*str >= '0' && *str <= '9') {
+			addr->val[i] |= *str - '0';
+		} else if (*str >= 'a' && *str <= 'f') {
+			addr->val[i] |= *str - 'a' + 10;
+		} else if (*str >= 'A' && *str <= 'F') {
+			addr->val[i] |= *str - 'A' + 10;
+		} else {
+			return -EINVAL;
+		}
+	}
+
+	if (strcmp(++str, "public") == 0) {
+		addr->type = BT_ADDR_LE_PUBLIC;
+	} else if (strcmp(str, "random") == 0) {
+		addr->type = BT_ADDR_LE_RANDOM;
+	} else {
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int cmd_connect_le(int argc, const char *line)
+{
+	int err;
+	bt_addr_le_t addr;
+
+	err = str2bt_addr_le(line += strlen(line) + 1, &addr);
+	if (err) {
+		printk("Invalid peer address (err %d)\n", err);
+		return err;
+	}
+
+	err = bt_connect_le(&addr);
+	if (err) {
+		printk("Connection failed (err %d)\n", err);
+		return err;
+	}
+
+	return 0;
+}
+
+static int cmd_disconnect(int argc, const char *line)
+{
+	int err;
+	uint8_t conn_id = *(line += strlen(line) + 1) - '0';
+
+	if (conn_id > CONFIG_BLUETOOTH_MAX_CONN - 1 || conns[conn_id] == NULL) {
+		printk("Invalid conn id %d\n", conn_id);
+	}
+
+	err = bt_disconnect(conns[conn_id], BT_HCI_ERR_REMOTE_USER_TERM_CONN);
+
+	if (err) {
+		printk("Disconnection failed (err %d)\n", err);
+		return err;
+	}
+
+	return 0;
+}
+
 #ifdef CONFIG_MICROKERNEL
 void mainloop(void)
 #else
@@ -60,6 +175,9 @@ void main(void)
 #endif
 {
 	shell_init("btshell> ");
+	bt_conn_cb_register(&conn_callbacks);
 
 	shell_cmd_register("init", cmd_init);
+	shell_cmd_register("connect", cmd_connect_le);
+	shell_cmd_register("disconnect", cmd_disconnect);
 }
