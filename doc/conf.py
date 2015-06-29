128675085c1c362a821748d8de2e0275268c36d7
Anas Nashif <anas.nashif@intel.com>
2015-06-29 15:33:28 -0400

    doc: add custom zephyr theme
    Add a new theme inherited from sphinxdocs to allow zephyr
    specific customisations and improvements.
    
    Initially, it adds css style for guilabel and makes that
    appear bold in html output.
    
    Change-Id: I2c80b3be4d06ab73907d2f117ea84c847c0ddf7f
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- doc/conf.py
+++ doc/conf.py
@@ -114,7 +114,7 @@ rst_epilog = """
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'alabaster'
-html_theme = 'sphinxdoc'
+html_theme = 'zephyr'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -122,11 +122,11 @@ html_theme = 'sphinxdoc'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['./themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "Zephyr Project Documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
