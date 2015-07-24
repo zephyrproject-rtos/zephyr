f581894abfb671ec44dbbec2f3fd0596a9d86538
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-07-24 13:17:33 +0300

    Bluetooth: Make central sample connect to devices supporting HRS
    This makes central sample to parse the Advertising Data and in case the
    device supports HeartRate Service stop scanning and connect to it, if the
    device disconnects then start scanning again.
    
    Change-Id: I49962eadb5c82c7a910185212063cd576cadf62e
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- samples/bluetooth/central/src/main.c
+++ samples/bluetooth/central/src/main.c
@@ -38,9 +38,13 @@
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
 #include <bluetooth/conn.h>
+#include <bluetooth/uuid.h>
+#include <misc/byteorder.h>
 
 #define SLEEPTIME  5000
 #define SLEEPTICKS (SLEEPTIME * sys_clock_ticks_per_sec / 1000)
+
+static struct bt_conn *default_conn;
 
 static void connected(struct bt_conn *conn)
 {
@@ -51,13 +55,103 @@ static void connected(struct bt_conn *conn)
 	printk("Connected: %s\n", addr);
 }
 
+static bool eir_found(const struct bt_eir *eir, void *user_data)
+{
+	bt_addr_le_t *addr = user_data;
+	uint16_t u16;
+	int i;
+
+	printk("[AD]: %u len %u\n", eir->type, eir->len);
+
+	switch (eir->type) {
+	case BT_EIR_UUID16_SOME:
+	case BT_EIR_UUID16_ALL:
+		if ((eir->len - sizeof(eir->type)) % sizeof(u16) != 0) {
+			printk("AD malformed\n");
+			return true;
+		}
+
+		for (i = 0; i < eir->len; i += sizeof(u16)) {
+			memcpy(&u16, &eir->data[i], sizeof(u16));
+			if (sys_le16_to_cpu(u16) == BT_UUID_HRS) {
+				int err = bt_stop_scanning();
+
+				if (err) {
+					printk("Stopping scanning failed"
+						" (err %d)\n", err);
+				}
+
+				default_conn = bt_conn_create_le(addr);
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+static void ad_parse(const uint8_t *data, uint8_t len,
+		     bool (*func)(const struct bt_eir *eir, void *user_data),
+		     void *user_data)
+{
+	const void *p;
+
+	for (p = data; len > 0;) {
+		const struct bt_eir *eir = p;
+
+		/* Check for early termination */
+		if (eir->len == 0) {
+			return;
+		}
+
+		if ((eir->len + sizeof(eir->len) > len) ||
+		    (len < sizeof(eir->len) + sizeof(eir->type))) {
+			printk("AD malformed\n");
+			return;
+		}
+
+		if (!func(eir, user_data)) {
+			return;
+		}
+
+		p += sizeof(eir->len) + eir->len;
+		len -= sizeof(eir->len) + eir->len;
+	}
+}
+
+static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
+			 const uint8_t *ad, uint8_t len)
+{
+	char dev[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(addr, dev, sizeof(dev));
+	printk("[DEVICE]: %s, AD evt type %u, AD data len %u, RSSI %i\n",
+	       dev, type, len, rssi);
+
+	/* TODO: Move this to a place it can be shared */
+	ad_parse(ad, len, eir_found, (void *)addr);
+}
+
 static void disconnected(struct bt_conn *conn)
 {
 	char addr[BT_ADDR_LE_STR_LEN];
+	int err;
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
 	printk("Disconnected: %s\n", addr);
+
+	if (default_conn != conn) {
+		return;
+	}
+
+	bt_conn_put(default_conn);
+	default_conn = NULL;
+
+	err = bt_start_scanning(BT_SCAN_FILTER_DUP_DISABLE, device_found);
+	if (err) {
+		printk("Scanning failed to start (err %d)\n", err);
+	}
 }
 
 static struct bt_conn_cb conn_callbacks = {
@@ -83,7 +177,7 @@ void main(void)
 
 	bt_conn_cb_register(&conn_callbacks);
 
-	err = bt_start_scanning(BT_SCAN_FILTER_DUP_ENABLE, NULL);
+	err = bt_start_scanning(BT_SCAN_FILTER_DUP_ENABLE, device_found);
 
 	if (err) {
 		printk("Scanning failed to start (err %d)\n", err);
@@ -91,14 +185,9 @@ void main(void)
 	}
 
 	printk("Scanning successfully started\n");
-	task_sleep(SLEEPTICKS);
 
-	err = bt_stop_scanning();
-
-	if (err) {
-		printk("Stopping scanning failed (err %d)\n", err);
-		return;
+	while (1) {
+		task_sleep(SLEEPTICKS);
 	}
-	printk("Scanning successfully stopped\n");
 }
 
