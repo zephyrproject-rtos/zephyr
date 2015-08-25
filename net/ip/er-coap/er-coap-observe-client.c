036eda7b9f913dadd205a412fdc2a6245292bc2e
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-08-25 16:44:57 +0300

    net: coap: Use uIP debug macros
    Use the debug macros from uip-debug.h so that they are
    in one place.
    
    Change-Id: I342d727d4c6af9a655729db6245668968bee4b07
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- net/ip/er-coap/er-coap-observe-client.c
+++ net/ip/er-coap/er-coap-observe-client.c
@@ -46,28 +46,8 @@
 /* Compile this code only if client-side support for CoAP Observe is required */
 #if COAP_OBSERVE_CLIENT
 
-#define DEBUG 1
-#if DEBUG
-#define PRINTF(...) printf(__VA_ARGS__)
-#define PRINT6ADDR(addr) PRINTF("[%02x%02x:%02x%02x:%02x%02x:%02x%02x:" \
-                                "%02x%02x:%02x%02x:%02x%02x:%02x%02x]", \
-                                ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], \
-                                ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], \
-                                ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], \
-                                ((uint8_t *)addr)[6], ((uint8_t *)addr)[7], \
-                                ((uint8_t *)addr)[8], ((uint8_t *)addr)[9], \
-                                ((uint8_t *)addr)[10], ((uint8_t *)addr)[11], \
-                                ((uint8_t *)addr)[12], ((uint8_t *)addr)[13], \
-                                ((uint8_t *)addr)[14], ((uint8_t *)addr)[15])
-#define PRINTLLADDR(lladdr) PRINTF("[%02x:%02x:%02x:%02x:%02x:%02x]", \
-                                   (lladdr)->addr[0], (lladdr)->addr[1], \
-                                   (lladdr)->addr[2], (lladdr)->addr[3], \
-                                   (lladdr)->addr[4], (lladdr)->addr[5])
-#else
-#define PRINTF(...)
-#define PRINT6ADDR(addr)
-#define PRINTLLADDR(addr)
-#endif
+#define DEBUG DEBUG_NONE
+#include "net/ip/uip-debug.h"
 
 MEMB(obs_subjects_memb, coap_observee_t, COAP_MAX_OBSERVEES);
 LIST(obs_subjects_list);
--- net/ip/er-coap/er-coap-separate.c
+++ net/ip/er-coap/er-coap-separate.c
@@ -42,17 +42,8 @@
 #include "er-coap-separate.h"
 #include "er-coap-transactions.h"
 
-#define DEBUG 0
-#if DEBUG
-#include <stdio.h>
-#define PRINTF(...) printf(__VA_ARGS__)
-#define PRINT6ADDR(addr) PRINTF("[%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x]", ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], ((uint8_t *)addr)[6], ((uint8_t *)addr)[7], ((uint8_t *)addr)[8], ((uint8_t *)addr)[9], ((uint8_t *)addr)[10], ((uint8_t *)addr)[11], ((uint8_t *)addr)[12], ((uint8_t *)addr)[13], ((uint8_t *)addr)[14], ((uint8_t *)addr)[15])
-#define PRINTLLADDR(lladdr) PRINTF("[%02x:%02x:%02x:%02x:%02x:%02x]", (lladdr)->addr[0], (lladdr)->addr[1], (lladdr)->addr[2], (lladdr)->addr[3], (lladdr)->addr[4], (lladdr)->addr[5])
-#else
-#define PRINTF(...)
-#define PRINT6ADDR(addr)
-#define PRINTLLADDR(addr)
-#endif
+#define DEBUG DEBUG_NONE
+#include "net/ip/uip-debug.h"
 
 /*---------------------------------------------------------------------------*/
 /*- Separate Response API ---------------------------------------------------*/
