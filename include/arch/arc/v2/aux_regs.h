6525b656cd91111e1de6f7d4cb7daaab3c5e85d2
Juan Manuel Cruz <juan.m.cruz.alcaraz@linux.intel.com>
2015-08-26 13:43:59 -0500

    arc registers: fixes the macros to read arc registers
    This commit fixes small errors on the asm implementation of arc
    register reading and the built-in functions call.
    
    Change-Id: Iea2df715eaefb25095770971ac03d441311abf16
    Signed-off-by: Juan Manuel Cruz <juan.m.cruz.alcaraz@linux.intel.com>

--- include/arch/arc/v2/aux_regs.h
+++ include/arch/arc/v2/aux_regs.h
@@ -94,15 +94,16 @@ Definitions for auxiliary registers.
 #ifndef _ASMLANGUAGE
 #if defined(__GNUC__)
 
-#define _arc_v2_aux_reg_read(reg) __builtin_arc_lr(reg)
-#define _arc_v2_aux_reg_write(reg, val) __builtin_arc_sr((unsigned int)val, reg)
+#include <stdint.h>
+#define _arc_v2_aux_reg_read(reg) __builtin_arc_lr((volatile uint32_t)reg)
+#define _arc_v2_aux_reg_write(reg, val) __builtin_arc_sr((unsigned int)val, (volatile uint32_t)reg)
 
 #else /* ! __GNUC__ */
 
 #define _arc_v2_aux_reg_read(reg)                                \
 	({                                               \
 		unsigned int __ret;                      \
-		__asm__ __volatile__("       lr 0, [%1]" \
+		__asm__ __volatile__("       lr %0, [%1]" \
 				     : "=r"(__ret)       \
 				     : "i"(reg));        \
 		__ret;                                   \
