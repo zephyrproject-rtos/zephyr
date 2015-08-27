69871aaecc1d1d95f4144f0f1db728d4b0f8410d
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2015-08-27 16:58:28 +0300

    uart: Configure ioapic for UART interrupts
    Follow similar configuration for other platforms.
    
    Change-Id: I1478d10799cbb375c715bed4d30d1bcf223d282f
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- arch/x86/platforms/ia32_pci/system.c
+++ arch/x86/platforms/ia32_pci/system.c
@@ -88,6 +88,19 @@ static inline void hpet_irq_set(void)
 	} while ((0))
 #endif
 
+#ifdef CONFIG_CONSOLE_HANDLER
+static inline void console_irq_set(void)
+{
+	_ioapic_irq_set(CONFIG_UART_CONSOLE_IRQ,
+			CONFIG_UART_CONSOLE_IRQ + INT_VEC_IRQ0,
+			UART_IOAPIC_FLAGS);
+}
+#else
+#define console_irq_set()	\
+	do { /* nothing */	\
+	} while ((0))
+#endif
+
 /**
  *
  * @brief Perform basic hardware initialization
@@ -106,6 +119,7 @@ static int ia32_pci_init(struct device *arg)
 	loapic_init();       /* NOP if not needed */
 	ioapic_init();       /* NOP if not needed */
 	hpet_irq_set();      /* NOP if not needed */
+	console_irq_set();   /* NOP if not needed */
 
 #if defined(CONFIG_PCI_DEBUG) && defined(CONFIG_PCI_ENUMERATION)
 	/* Rescan PCI and display the list of PCI attached devices */
