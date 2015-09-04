cfa51f89f1e351fb4eedf79ad6708a54fc5ba10d
Szymon Janc <ext.szymon.janc@tieto.com>
2015-09-04 19:39:03 +0200

    Bluetooth: Strip command extra spaces in shell app
    This makes shell a bit more convenient to use.
    
    Change-Id: I50490eb3cf9434723737a71fb382316876ba6291
    Signed-off-by: Szymon Janc <ext.szymon.janc@tieto.com>

--- samples/bluetooth/shell/src/btshell.c
+++ samples/bluetooth/shell/src/btshell.c
@@ -75,10 +75,27 @@ static size_t line2argv(char *str, char *argv[], size_t size)
 		return 0;
 	}
 
+	while (*str && *str == ' ') {
+		str++;
+	}
+
+	if (!*str) {
+		return 0;
+	}
+
 	argv[argc++] = str;
 
 	while ((str = strchr(str, ' '))) {
 		*str++ = '\0';
+
+		while (*str && *str == ' ') {
+			str++;
+		}
+
+		if (!*str) {
+			break;
+		}
+
 		argv[argc++] = str;
 
 		if (argc == size) {
