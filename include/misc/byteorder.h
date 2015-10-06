935eedeedf55d05f10b53bc21acdc7fc6db4c1ee
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2015-10-06 13:26:34 +0300

    byteorder: Add all remaining helpers for both little and big endian
    - making bswap_16 and bswap_32 for both
    - adding sys_be16_to_cpu, sys_cpu_to_be16, sys_be32_to_cpu and
      sys_cpu_to_be32
    
    Change-Id: I3aacc91590ee584d83c512d78a9287380969d9aa
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- include/misc/byteorder.h
+++ include/misc/byteorder.h
@@ -30,19 +30,29 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#define bswap_16(x) ((uint16_t) ((((x) >> 8) & 0xff) | (((x) & 0xff) << 8)))
+
+#define bswap_32(x) ((uint32_t) ((((x) >> 24) & 0xff) | (((x) >> 8) & 0xff00) \
+				| (((x) & 0xff00) << 8) | (((x) & 0xff) << 24)))
+
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #define sys_le16_to_cpu(val) (val)
 #define sys_cpu_to_le16(val) (val)
+#define sys_be16_to_cpu(val) bswap_16(val)
+#define sys_cpu_to_be16(val) bswap_16(val)
 #define sys_le32_to_cpu(val) (val)
 #define sys_cpu_to_le32(val) (val)
+#define sys_be32_to_cpu(val) bswap_32(val)
+#define sys_cpu_to_be32(val) bswap_32(val)
 #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-#define bswap_16(x) ((uint16_t) ((((x) >> 8) & 0xff) | (((x) & 0xff) << 8)))
 #define sys_le16_to_cpu(val) bswap_16(val)
 #define sys_cpu_to_le16(val) bswap_16(val)
-#define bswap_32(x) ((uint32_t) ((((x) >> 24) & 0xff) | (((x) >> 8) & 0xff00) \
-				| (((x) & 0xff00) << 8) | (((x) & 0xff) << 24)))
+#define sys_be16_to_cpu(val) (val)
+#define sys_cpu_to_be16(val) (val)
 #define sys_le32_to_cpu(val) bswap_32(val)
 #define sys_cpu_to_le32(val) bswap_32(val)
+#define sys_be32_to_cpu(val) (val)
+#define sys_cpu_to_be32(val) (val)
 #else
 #error "Unknown byte order"
 #endif
