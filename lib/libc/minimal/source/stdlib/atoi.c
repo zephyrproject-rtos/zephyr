7cfc70371146fd2efa31c92f78cafed0d50340d9
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-10-14 13:27:07 -0700

    checkpatch: error - trailing statements
    Change-Id: I461134536da0288261b90f605a904ab3ea466340
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>

--- lib/libc/minimal/source/stdlib/atoi.c
+++ lib/libc/minimal/source/stdlib/atoi.c
@@ -29,13 +29,18 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 int atoi(const char *s)
 {
 	int n=0, neg=0;
-	while (isspace(*s)) s++;
+	while (isspace(*s)) {
+		s++;
+	}
 	switch (*s) {
-		case '-': neg=1;
-		case '+': s++;
+		case '-':
+			neg=1;
+		case '+':
+			s++;
 	}
 	/* Compute n as a negative number to avoid overflow on INT_MIN */
-	while (isdigit(*s))
+	while (isdigit(*s)) {
 		n = 10*n - (*s++ - '0');
+	}
 	return neg ? n : -n;
 }
