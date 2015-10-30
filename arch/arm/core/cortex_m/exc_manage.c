bd94d9825540a67555691ae7fc7d0741ea127050
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-10-30 14:29:49 -0400

    arm: add connecting exceptions at runtime
    Add sys_exc_connect() (and its x86-compatible alias nanoCpuExcConnect())
    that allows connecting an exception handler at runtime.
    
    The current implementation is a bit of a bastard, to avoid disturbing
    the current implementation of the exception handlers. Instead of hooking
    _exc_wrapper() in all vectors and adapting the exception handlers, the
    current exception handlers are still hooked directly in the vectors.
    When an exception is hooked at runtime, _exc_wrapper() gets installed in
    the vector and the real handler gets inserted in _sw_exc_table; this
    means that the scheme only works with non-XIP kernels.
    
    This should be enhanced so that _exc_wrapper() is hooked in all vectors,
    and that current exception handlers (for faults mostly) are reworked to
    be inserted in the _sw_exc_table and wrapped in _exc_wrapper().
    
    Change-Id: Icaa14f4835b57873d2905b7fbcbb94eeb3b247d1
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>

---
+++ arch/arm/core/cortex_m/exc_manage.c
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2015 Wind River Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file dynamic exception management
+ */
+
+/* can only be used with non-XIP kernels, since they don't have their vector
+ * table in the FLASH
+ */
+#if !defined(CONFIG_XIP)
+
+#include <nanokernel.h>
+#include <arch/cpu.h>
+#include <misc/__assert.h>
+#include <toolchain.h>
+#include <sections.h>
+#include "vector_table.h"
+
+static inline int exc_can_be_connected(int num)
+{
+	static const uint16_t connectable_exceptions = (
+		(1 << _EXC_MPU_FAULT) |
+		(1 << _EXC_BUS_FAULT) |
+		(1 << _EXC_USAGE_FAULT) |
+		(1 << _EXC_DEBUG) |
+		0
+	);
+
+	return !!(connectable_exceptions & (1 << num));
+}
+
+/*
+ * Can be initialized with garbage, it doesn't matter until the wrapper is
+ * inserted in the vector table, at which point the relevant entry will contain
+ * the pointer to the handler.
+ */
+sys_exc_handler_t *_sw_exc_table[_NUM_EXC] __noinit;
+
+extern void _exc_wrapper(void);
+void sys_exc_connect(unsigned int num, sys_exc_handler_t *handler, void *unused)
+{
+	__ASSERT(exc_can_be_connected(num), "not a connectable exception");
+
+	_sw_exc_table[num] = handler;
+
+	/*
+	 * The compiler sets thumb bit (bit0) of the value of the _vector_table
+	 * symbol, probably because it is in the .text section: to get the correct
+	 * offset in the table, mask bit0.
+	 */
+	((void **)(((uint32_t)_vector_table) & 0xfffffffe))[num] = _exc_wrapper;
+}
+
+FUNC_ALIAS(sys_exc_connect, nanoCpuExcConnect, void);
+
+#include <misc/printk.h>
+void sys_exc_esf_dump(NANO_ESF *esf)
+{
+	printk("r0/a1:  %x  ", esf->a1);
+	printk("r1/a2:  %x  ", esf->a2);
+	printk("r2/a3:  %x\n", esf->a3);
+	printk("r3/a4:  %x  ", esf->a4);
+	printk("r12/ip: %x  ", esf->ip);
+	printk("r14/lr: %x\n", esf->lr);
+	printk("r15/pc: %x  ", esf->pc);
+	printk("xpsr:   %x\n", esf->xpsr);
+}
+
+#endif /* CONFIG_XIP */
