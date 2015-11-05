c5bb958db4665e9ec724a226bb0f13991a5d4e93
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-11-05 18:26:01 -0500

    arm: add software reboot for QEMU
    This simulates a reboot by jumping back to the address stored in the
    reset vector in vector table found at address 0. It is supported from
    interrupt/exception level, which means that sys_arch_reboot() in this
    case finds out if it is called from thread mode or handler mode, and in
    the latter case, it unwinds the nested exception stack as needed.
    
    Change-Id: Ib67f850f8411f1ee8fc592a5f31f2f70d0af14a4
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>

--- arch/arm/core/cortex_m/scb.c
+++ arch/arm/core/cortex_m/scb.c
@@ -30,19 +30,37 @@
 
 #define SCB_AIRCR_VECTKEY_EN_W 0x05FA
 
-/**
- *
- * @brief Reset the system
- *
- * This routine resets the processor.
- *
- * @return N/A
+#if defined(CONFIG_SOC_TI_LM3S6965_QEMU)
+/*
+ * QEMU is missing the support for rebooting through the SYSRESETREQ mechanism.
+ * Just jump back to __reset() of the image in flash, which address can
+ * _always_ be found in the vector table reset slot located at address 0x4.
  */
-void sys_arch_reboot(int type)
+
+static void software_reboot(void)
+{
+	extern void _do_software_reboot(void);
+	extern void _force_exit_one_nested_irq(void);
+	/*
+	 * force enable interrupts locked via PRIMASK if somehow disabled: the
+	 * boot code does not enable them
+	 */
+	__asm__ volatile("cpsie i" :::);
+
+	if (_ScbIsInThreadMode()) {
+		_do_software_reboot();
+	} else {
+		__asm__ volatile(
+			"ldr r0,  =_force_exit_one_nested_irq\n\t"
+			"bx r0\n\t"
+			:::);
+	}
+}
+#define DO_REBOOT() software_reboot()
+#else
+static void reboot_through_sysresetreq(void)
 {
 	union __aircr reg;
-
-	ARG_UNUSED(type);
 
 	reg.val = __scs.scb.aircr.val;
 	reg.bit.vectkey = SCB_AIRCR_VECTKEY_EN_W;
@@ -53,6 +71,23 @@ void sys_arch_reboot(int type)
 	for (;;) {
 		;
 	}
+}
+#define DO_REBOOT() reboot_through_sysresetreq()
+#endif
+
+/**
+ *
+ * @brief Reset the system
+ *
+ * This routine resets the processor.
+ *
+ * @return N/A
+ */
+
+void sys_arch_reboot(int type)
+{
+	ARG_UNUSED(type);
+	DO_REBOOT();
 }
 
 /**
