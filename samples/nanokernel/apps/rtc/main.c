a039c486cb1dd2ab757275ed3af8f3a7ce2bfa87
Anas Nashif <anas.nashif@intel.com>
2015-11-30 13:17:54 -0500

    samples: add a sample application using RTC
    This sample creates an alarm and repeats the alarm on every
    interrupt.
    
    Change-Id: I97583e3c5af826c51536f8d244c016e2c628e88a
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

---
+++ samples/nanokernel/apps/rtc/main.c
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2015 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <zephyr.h>
+
+#include <device.h>
+#include <rtc.h>
+#include <misc/printk.h>
+
+
+#define ALARM (RTC_ALARM_MINUTE / 6)
+#define RTC_DRIVER "rtc"
+struct device *rtc_dev;
+
+void test_rtc_interrupt_fn(void)
+{
+	uint32_t now = rtc_read(rtc_dev);
+
+	printk("Alarm\n");
+	rtc_set_alarm(rtc_dev, now + ALARM);
+}
+
+void main(void)
+{
+	struct rtc_config config;
+
+
+	printk("Test RTC driver\n");
+	rtc_dev = device_get_binding(RTC_DRIVER);
+
+	config.init_val = 0;
+	config.alarm_enable = 1;
+	config.alarm_val = ALARM;
+	config.cb_fn = test_rtc_interrupt_fn;
+
+	rtc_enable(rtc_dev);
+
+	rtc_set_config(rtc_dev, &config);
+
+	while (1) {
+		/* do nothing */
+	}
+}
