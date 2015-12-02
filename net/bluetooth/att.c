6e28748e3e78b4ddcc4ae66e262ecf5581a5b445
Mariusz Skamra <mariusz.skamra@tieto.com>
2015-12-02 15:04:01 +0100

    Bluetooth: GATT: Minor fix in warning message
    This patch fixes warning that ATT MTU exceeded. An opcode size should
    be taken into account.
    
    Change-Id: Ia69c9514a59767542cca0ef36fc77d896d20db28
    Signed-off-by: Mariusz Skamra <mariusz.skamra@tieto.com>

--- net/bluetooth/att.c
+++ net/bluetooth/att.c
@@ -1596,7 +1596,7 @@ struct net_buf *bt_att_create_pdu(struct bt_conn *conn, uint8_t op, size_t len)
 
 	if (len + sizeof(op) > att->chan.tx.mtu) {
 		BT_WARN("ATT MTU exceeded, max %u, wanted %u",
-			att->chan.tx.mtu, len);
+			att->chan.tx.mtu, len + sizeof(op));
 		return NULL;
 	}
 
