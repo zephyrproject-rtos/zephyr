e5c41f07202224c3f445c1e54714ec901f5db9fd
Anas Nashif <anas.nashif@intel.com>
2015-12-03 11:49:28 -0500

    watchdog: add sample application
    This application creates a watchdog with 4.194s timout and reloads
    every time it kicks (and does not force a reboot).
    
    Change-Id: Ib128e2f7f91d05081d1bfb8cc24258b69bd3cb6e
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

---
+++ samples/nanokernel/apps/watchdog/src/main.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2015 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <zephyr.h>
+
+#include <device.h>
+#include <watchdog.h>
+#include "board.h"
+#include <misc/printk.h>
+
+
+uint32_t wdt_fired;
+
+#define WDT_DRIVER "wdt_dw"
+static struct device *wdt_dev;
+
+/* WDT Requires a callback, there is no interrupt enable / disable. */
+void wdt_example_cb(void)
+{
+	wdt_fired++;
+	printk("watchdog fired\n");
+	wdt_reload(wdt_dev);
+}
+
+void main(void)
+{
+	struct wdt_config wr_cfg;
+	struct wdt_config cfg;
+
+	printk("Start watchdog test\n");
+	wr_cfg.timeout = WDT_2_27_CYCLES;
+	wr_cfg.mode = WDT_MODE_INTERRUPT_RESET;
+	wr_cfg.interrupt_fn = wdt_example_cb;
+
+	wdt_fired = 0;
+	wdt_dev = device_get_binding(WDT_DRIVER);
+
+	wdt_enable(wdt_dev);
+	wdt_set_config(wdt_dev, &wr_cfg);
+
+	wdt_get_config(wdt_dev, &cfg);
+	printk("timeout: %d\n", cfg.timeout);
+	printk("mode: %d\n", cfg.mode);
+}
+
