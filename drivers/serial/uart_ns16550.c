e2e78baf68ffb8c3b39183b93f543ae41b89cc07
Fabien Chereau <fabien.chereau@intel.com>
2016-01-18 14:16:33 +0100

    ns16550: implement the irq_tx_empty function
    Change-Id: I5b043354aefe8d54c2f8e06e06204e3de8e715b5
    Signed-off-by: Fabien Chereau <fabien.chereau@intel.com>

--- drivers/serial/uart_ns16550.c
+++ drivers/serial/uart_ns16550.c
@@ -472,6 +472,18 @@ static int uart_ns16550_irq_tx_ready(struct device *dev)
 }
 
 /**
+ * @brief Check if nothing remains to be transmitted
+ *
+ * @param dev UART device struct (of type struct uart_device_config)
+ *
+ * @return 1 if nothing remains to be transmitted, 0 otherwise
+ */
+static int uart_ns16550_irq_tx_empty(struct device *dev)
+{
+	return (INBYTE(LSR(dev)) & (LSR_TEMT | LSR_THRE)) == (LSR_TEMT | LSR_THRE);
+}
+
+/**
  * @brief Enable RX interrupt in IER
  *
  * @param dev UART device struct (of type struct uart_device_config)
@@ -644,6 +656,7 @@ static struct uart_driver_api uart_ns16550_driver_api = {
 	.irq_tx_enable = uart_ns16550_irq_tx_enable,
 	.irq_tx_disable = uart_ns16550_irq_tx_disable,
 	.irq_tx_ready = uart_ns16550_irq_tx_ready,
+	.irq_tx_empty = uart_ns16550_irq_tx_empty,
 	.irq_rx_enable = uart_ns16550_irq_rx_enable,
 	.irq_rx_disable = uart_ns16550_irq_rx_disable,
 	.irq_rx_ready = uart_ns16550_irq_rx_ready,
