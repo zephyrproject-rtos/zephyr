efc8ed379f8fa8778d63d7f1faf8f6de38e8ba79
Andrew Boie <andrew.p.boie@intel.com>
2016-01-20 11:42:12 -0800

    quark_se: increase IPM buffer size and make it configurable
    The original value of 128 was selected more or less randomly, and
    isn't sufficiently large enough for QA needs.
    
    Change-Id: I8a9fcc86d6b5fa7dc5ba05896c62f7dd608f9ed2
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- arch/x86/soc/quark_se/soc_config.c
+++ arch/x86/soc/quark_se/soc_config.c
@@ -49,9 +49,8 @@ SYS_DEFINE_DEVICE(quark_se_ipm, NULL, PRIMARY,
 QUARK_SE_IPM_DEFINE(quark_se_ipm4, 4, QUARK_SE_IPM_INBOUND);
 
 #define QUARK_SE_IPM_CONSOLE_LINE_BUF_SIZE	80
-#define QUARK_SE_IPM_CONSOLE_RING_BUF_SIZE32	128
 
-static uint32_t ipm_console_ring_buf_data[QUARK_SE_IPM_CONSOLE_RING_BUF_SIZE32];
+static uint32_t ipm_console_ring_buf_data[CONFIG_QUARK_SE_IPM_CONSOLE_RING_BUF_SIZE32];
 static char __stack ipm_console_fiber_stack[IPM_CONSOLE_STACK_SIZE];
 static char ipm_console_line_buf[QUARK_SE_IPM_CONSOLE_LINE_BUF_SIZE];
 
@@ -59,7 +58,7 @@ struct ipm_console_receiver_config_info quark_se_ipm_receiver_config = {
 	.bind_to = "quark_se_ipm4",
 	.fiber_stack = ipm_console_fiber_stack,
 	.ring_buf_data = ipm_console_ring_buf_data,
-	.rb_size32 = QUARK_SE_IPM_CONSOLE_RING_BUF_SIZE32,
+	.rb_size32 = CONFIG_QUARK_SE_IPM_CONSOLE_RING_BUF_SIZE32,
 	.line_buf = ipm_console_line_buf,
 	.lb_size = QUARK_SE_IPM_CONSOLE_LINE_BUF_SIZE,
 	.flags = IPM_CONSOLE_PRINTK
