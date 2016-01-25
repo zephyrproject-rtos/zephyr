48bdd70a07eec423d405df65cbf5a86de74ca6c6
Vlad Dogaru <vlad.dogaru@intel.com>
2016-01-25 18:21:51 +0200

    Add infrastructure for sensor drivers
    Origin: Original
    Change-Id: I846b1601f1bdf15ad068b27baefc4b23ffab81e3
    Signed-off-by: Vlad Dogaru <vlad.dogaru@intel.com>

---
+++ drivers/sensor/sensor.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <nanokernel.h>
+#include <sensor.h>
+#include <init.h>
+
+static char __stack sensor_fiber_stack[CONFIG_SENSOR_DELAYED_WORK_STACK_SIZE];
+static struct nano_fifo sensor_fifo;
+
+struct nano_fifo *sensor_get_work_fifo(void)
+{
+	return &sensor_fifo;
+}
+
+static void sensor_fiber_main(int arg1, int arg2)
+{
+	ARG_UNUSED(arg1);
+	ARG_UNUSED(arg2);
+
+	while (1) {
+		struct sensor_work *work;
+
+		work = nano_fiber_fifo_get(&sensor_fifo, TICKS_UNLIMITED);
+
+		work->handler(work->arg);
+	}
+}
+
+static int sensor_init(struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	nano_fifo_init(&sensor_fifo);
+
+	fiber_fiber_start(sensor_fiber_stack,
+			  CONFIG_SENSOR_DELAYED_WORK_STACK_SIZE,
+			  sensor_fiber_main, 0, 0,
+			  CONFIG_SENSOR_DELAYED_WORK_PRIORITY, 0);
+
+	return DEV_OK;
+}
+
+SYS_INIT(sensor_init, PRIMARY, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
