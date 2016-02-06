3010de9850fb749d3ffc807048d666c2aad69827
Rodrigo Caballero <rodrigo.caballero.abraham@intel.com>
2016-02-06 16:18:22 -0600

    doc: Edit the IPM driver APIs
    Edit public API details for clarity, grammar, punctuation found
    on file ipm.h
    
    Made consistent use of @retval to document return values.
    
    Used imperative verbs for all @brief descriptions.
    
    Change-Id: Ie5516fcb18648a99c58bcfef7888286dead7cff4
    Signed-off-by: Rodrigo Caballero <rodrigo.caballero.abraham@intel.com>

--- include/ipm.h
+++ include/ipm.h
@@ -1,4 +1,8 @@
-/* ipm.h - Generic low-level inter-processor mailbox communication API */
+/**
+ * @file
+ *
+ * @brief Generic low-level inter-processor mailbox communication API.
+ */
 
 /*
  * Copyright (c) 2015 Intel Corporation
@@ -34,16 +38,16 @@ extern "C" {
 #endif
 
 /**
- * Callback API for incoming IPM messages
+ * @brief Callback API for incoming IPM messages
  *
- * These callbacks are executed in interrupt context, and only interrupt-
- * safe APIS may be used. Registration of callbacks is done via
- * ipm_register_callback()
+ * These callbacks execute in interrupt context. Therefore, use only
+ * interrupt-safe APIS. Registration of callbacks is done via
+ * @a ipm_register_callback
  *
- * @param context Arbitrary context pointer provided at registration time
- * @param id Message type identifier
+ * @param context Arbitrary context pointer provided at registration time.
+ * @param id Message type identifier.
  * @param data Message data pointer. The correct amount of data to read out
- * needs to be inferred by the message id/upper level protocol
+ * must be inferred using the message id/upper level protocol.
  */
 typedef void (*ipm_callback_t)(void *context, uint32_t id, volatile void *data);
 
@@ -64,34 +68,35 @@ struct ipm_driver_api {
 };
 
 /**
- * Try to send a message over the IPM device
+ * @brief Try to send a message over the IPM device.
  *
- * @param ipmdev Driver instance
- * @param wait If nonzero, busy-wait for remote to consume the message. The
- *	       message is considered consumed once the remote interrupt handler
- *	       finishes. If there is deferred processing on the remote side,
- *	       or you would like to queue outgoing messages and wait on an
- *	       event/semaphore, you can implement that in a high-level driver
- * @param id Message identifier. Values are constrained by
- *	     ipm_max_data_size_get() since many platforms only allow for a
- *	     subset of bits in a 32-bit register to store the ID.
- * @param data Pointer to data to send in the message
- * @param size size of the data pointed to
+ * A message is considered consumed once the remote interrupt handler
+ * finishes. If there is deferred processing on the remote side,
+ * or if outgoing messages must be queued and wait on an
+ * event/semaphore, a high-level driver can implement that.
  *
  * There are constraints on how much data can be sent or the maximum value
- * of id, use the ipm_max_data_size_get() and ipm_max_id_val_get() functions
- * to determine these.
+ * of id. Use the @a ipm_max_data_size_get and @a ipm_max_id_val_get routines
+ * to determine them.
  *
- * The size parameter is only used on the sending side to know how much data
- * to put in the message registers, it is not passed along to the receiving
- * side. How much data to read back should be dictated by the upper-level
- * protocol.
+ * The @a size parameter is used only on the sending side to determine
+ * the amount of data to put in the message registers. It is not passed along
+ * to the receiving side. The upper-level protocol dictates the amount of
+ * data read back.
  *
- * @return -EBUSY if the remote hasn't yet read the last data sent
- *	   -EMSGSIZE If the supplied data size is unsupported by the driver
- *	   -EINVAL   Bad parameter, such as a too-large id value, or
- *		     the device isn't an outbound IPM channel
- *	   0 Success
+ * @param ipmdev Driver instance
+ * @param wait Time to busy-wait for remote to consume the message.
+ * @param id Message identifier. Values are constrained by
+ *        @a ipm_max_data_size_get since many platforms only allow for a
+ *        subset of bits in a 32-bit register to store the ID.
+ * @param data Pointer to the data sent in the message.
+ * @param size Size of the data.
+ *
+ * @retval EBUSY    If the remote hasn't yet read the last data sent.
+ * @retval EMSGSIZE If the supplied data size is unsupported by the driver.
+ * @retval EINVAL   If there was a bad parameter, such as: too-large id value.
+ *                  or the device isn't an outbound IPM channel.
+ * @retval 0        On success.
  */
 static inline int ipm_send(struct device *ipmdev, int wait, uint32_t id,
 			   void *data, int size)
@@ -103,10 +108,10 @@ static inline int ipm_send(struct device *ipmdev, int wait, uint32_t id,
 }
 
 /**
- * Register a callback function for incoming messages
+ * @brief Register a callback function for incoming messages.
  *
- * @param ipmdev Driver instance
- * @param cb Callback function to execute on incoming message interrupts
+ * @param ipmdev Driver instance pointer.
+ * @param cb Callback function to execute on incoming message interrupts.
  * @param context Application-specific context pointer which will be passed
  *        to the callback function when executed.
  */
@@ -120,14 +125,14 @@ static inline void ipm_register_callback(struct device *ipmdev,
 }
 
 /**
- * Return the maxmimum number of bytes that can be sent in an outbound message
+ * @brief Return the maximum number of bytes possible in an outbound message.
  *
- * IPM implementations vary in how much data can be sent in a single message
- * since the data payload is typically stored in registers.
+ * IPM implementations vary on the amount of data that can be sent in a
+ * single message since the data payload is typically stored in registers.
  *
- * @param ipmdev Driver instance pointer
+ * @param ipmdev Driver instance pointer.
  *
- * @return Max size of a message in bytes
+ * @return Maximum possible size of a message in bytes.
  */
 static inline int ipm_max_data_size_get(struct device *ipmdev)
 {
@@ -139,14 +144,14 @@ static inline int ipm_max_data_size_get(struct device *ipmdev)
 
 
 /**
- * Return the maximum id value that can be sent in an outbound message
+ * @brief Return the maximum id value possible in an outbound message.
  *
- * Many IPM implementations store the id in a register which may have some bits
- * reserved for other use
+ * Many IPM implementations store the message's ID in a register with
+ * some bits reserved for other uses.
  *
- * @param ipmdev Driver instance pointer
+ * @param ipmdev Driver instance pointer.
  *
- * @return Maximum value of a message id
+ * @return Maximum possible value of a message ID.
  */
 static inline uint32_t ipm_max_id_val_get(struct device *ipmdev)
 {
@@ -157,12 +162,13 @@ static inline uint32_t ipm_max_id_val_get(struct device *ipmdev)
 }
 
 /**
- * For inbound channels, enable interrupts/callbacks
+ * @brief Enable interrupts and callbacks for inbound channels.
  *
- * @param ipmdev Driver instance pointer
- * @param enable 0=disable nonzero=enable
+ * @param ipmdev Driver instance pointer.
+ * @param enable Set to 0 to disable and to nonzero to enable.
  *
- * @return 0 on success, -EINVAL if this isn't an inbound channel
+ * @retval 0      On success.
+ * @retval EINVAL If it isn't an inbound channel.
  */
 static inline int ipm_set_enabled(struct device *ipmdev, int enable)
 {
