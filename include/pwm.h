a7738f49c4a6c3793ec58b29360d43b982862f95
Rodrigo Caballero <rodrigo.caballero.abraham@intel.com>
2016-02-07 12:56:47 -0600

    doc: Edit the PWM driver APIs
    Edit public API details for clarity, grammar, punctuation found
    on file pwm.h
    
    Made consistent use of @retval to document return values.
    
    Used imperative verbs for all @brief descriptions.
    
    Change-Id: I5a0f6290361e499ceb911473338358128f85e25f
    Signed-off-by: Rodrigo Caballero <rodrigo.caballero.abraham@intel.com>

--- include/pwm.h
+++ include/pwm.h
@@ -40,7 +40,6 @@ extern "C" {
 #include <stddef.h>
 #include <device.h>
 
-/* driver API definition */
 typedef int (*pwm_config_t)(struct device *dev, int access_op,
 			    uint32_t pwm, int flags);
 typedef int (*pwm_set_values_t)(struct device *dev, int access_op,
@@ -50,6 +49,7 @@ typedef int (*pwm_set_duty_cycle_t)(struct device *dev, int access_op,
 typedef int (*pwm_suspend_dev_t)(struct device *dev);
 typedef int (*pwm_resume_dev_t)(struct device *dev);
 
+/** @brief PWM driver API definition. */
 struct pwm_driver_api {
 	pwm_config_t config;
 	pwm_set_values_t set_values;
@@ -61,11 +61,12 @@ struct pwm_driver_api {
 /**
  * @brief Configure a single PWM output.
  *
- * @param dev Pointer to device structure for driver instance
- * @param pwm PWM output
- * @param flags PWM configuration flags
+ * @param dev Pointer to the device structure for the driver instance.
+ * @param pwm PWM output.
+ * @param flags PWM configuration flags.
  *
- * @return DEV_OK if successful, otherwise failed.
+ * @retval DEV_OK If successful,
+ * @retval failed Otherwise.
  */
 static inline int pwm_pin_configure(struct device *dev, uint8_t pwm,
 				    int flags)
@@ -79,12 +80,13 @@ static inline int pwm_pin_configure(struct device *dev, uint8_t pwm,
 /**
  * @brief Set the ON/OFF values for a single PWM output.
  *
- * @param dev Pointer to device structure for driver instance
- * @param pwm PWM output
- * @param on ON value to set the PWM to
- * @param off OFF value to set the PWM to
+ * @param dev Pointer to the device structure for the driver instance.
+ * @param pwm PWM output.
+ * @param on ON value set to the PWM.
+ * @param off OFF value set to the PWM.
  *
- * @return DEV_OK if successful, otherwise failed.
+ * @retval DEV_OK If successful.
+ * @retval failed Otherwise.
  */
 static inline int pwm_pin_set_values(struct device *dev, uint32_t pwm,
 				     uint32_t on, uint32_t off)
@@ -98,13 +100,15 @@ static inline int pwm_pin_set_values(struct device *dev, uint32_t pwm,
 /**
  * @brief Set the duty cycle of a single PWM output.
  *
- * This overrides any ON/OFF values being set before.
+ * This routine overrides any ON/OFF values set before.
  *
- * @param dev Pointer to device structure for driver instance.
- * @param pwm PWM output
- * @param duty Duty cycle to set the PWM to (in %, e.g. 50 => 50%)
+ * @param dev Pointer to the device structure for the driver instance.
+ * @param pwm PWM output.
+ * @param duty Duty cycle to set to the PWM in %, for example,
+ *        50 sets to 50%.
  *
- * @return DEV_OK if successful, otherwise failed.
+ * @retval DEV_OK If successful.
+ * @retval failed Otherwise.
  */
 static inline int pwm_pin_set_duty_cycle(struct device *dev, uint32_t pwm,
 					 uint8_t duty)
@@ -116,12 +120,13 @@ static inline int pwm_pin_set_duty_cycle(struct device *dev, uint32_t pwm,
 }
 
 /**
- * @brief Configure all PWM outputs.
+ * @brief Configure all the PWM outputs.
  *
- * @param dev Pointer to device structure for driver instance
- * @param flags PWM configuration flags
+ * @param dev Pointer to the device structure for the driver instance.
+ * @param flags PWM configuration flags.
  *
- * @return DEV_OK if successful, otherwise failed.
+ * @retval DEV_OK If successful.
+ * @retval failed Otherwise.
  */
 static inline int pwm_all_configure(struct device *dev, int flags)
 {
@@ -134,11 +139,12 @@ static inline int pwm_all_configure(struct device *dev, int flags)
 /**
  * @brief Set the ON/OFF values for all PWM outputs.
  *
- * @param dev Pointer to device structure for driver instance
- * @param on ON value to set the PWM to
- * @param off OFF value to set the PWM to
+ * @param dev Pointer to the device structure for the driver instance.
+ * @param on ON value set to the PWM.
+ * @param off OFF value set to the PWM.
  *
- * @return DEV_OK if successful, otherwise failed.
+ * @retval DEV_OK If successful.
+ * @retval failed Otherwise.
  */
 static inline int pwm_all_set_values(struct device *dev,
 				     uint32_t on, uint32_t off)
@@ -154,10 +160,12 @@ static inline int pwm_all_set_values(struct device *dev,
  *
  * This overrides any ON/OFF values being set before.
  *
- * @param dev Pointer to device structure for driver instance.
- * @param duty Duty cycle to set the PWM to (in %, e.g. 50 => 50%)
+ * @param dev Pointer to the device structure for the driver instance.
+ * @param duty Duty cycle to set to the PWM in %, for example,
+ *        50 sets to 50%.
  *
- * @return DEV_OK if successful, otherwise failed.
+ * @retval DEV_OK If successful.
+ * @retval failed Otherwise.
  */
 static inline int pwm_all_set_duty_cycle(struct device *dev, uint8_t duty)
 {
@@ -168,11 +176,13 @@ static inline int pwm_all_set_duty_cycle(struct device *dev, uint8_t duty)
 }
 
 /**
- * @brief Save the state of the device and go to low power state
+ * @brief Save the state of the device and makes it go to the low power
+ * state.
  *
- * @param dev Pointer to device structure for driver instance.
+ * @param dev Pointer to the device structure for the driver instance.
  *
- * @return DEV_OK if successful, otherwise failed.
+ * @retval DEV_OK If successful.
+ * @retval failed Otherwise.
  */
 static inline int pwm_suspend(struct device *dev)
 {
@@ -183,11 +193,12 @@ static inline int pwm_suspend(struct device *dev)
 }
 
 /**
- * @brief Restore state stored during suspend and resume operation.
+ * @brief Restore state stored during suspend and resumes operation.
  *
- * @param dev Pointer to device structure for driver instance.
+ * @param dev Pointer to the device structure for the driver instance.
  *
- * @return DEV_OK if successful, otherwise failed.
+ * @retval DEV_OK If successful.
+ * @retval failed Otherwise.
  */
 static inline int pwm_resume(struct device *dev)
 {
