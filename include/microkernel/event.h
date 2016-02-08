c70c851dedb5c3bade175304bb7092a96a7c8d33
Rodrigo Caballero <rodrigo.caballero.abraham@intel.com>
2016-02-08 09:28:39 -0600

    doc: API doc edits for microkernel event.h
    Review and edit public API details for clarity, consistency,
    and punctuation.
    
    Made consistent use of @retval to document return values.
    
    Used imperative verbs for all @brief descriptions.
    
    Change-Id: I7f1ec25ceddacedcc7a9c63e7efd0621e80ad0de
    Signed-off-by: Rodrigo Caballero <rodrigo.caballero.abraham@intel.com>

--- include/microkernel/event.h
+++ include/microkernel/event.h
@@ -35,14 +35,14 @@ extern "C" {
 
 #include <microkernel/base_api.h>
 
-/* well-known events */
+/** Well-known events. */
 extern const kevent_t TICK_EVENT;
 
 /**
  *
- * @brief Signal an event from an ISR
+ * @brief Signal an event from an ISR.
  *
- * This routine does NOT validate the specified event number.
+ * This routine does @em not validate the specified event number.
  *
  * @param event Event to signal.
  *
@@ -52,9 +52,9 @@ extern void isr_event_send(kevent_t event);
 
 /**
  *
- * @brief Signal an event from a fiber
+ * @brief Signal an event from a fiber.
  *
- * This routine does NOT validate the specified event number.
+ * This routine does @em not validate the specified event number.
  *
  * @param event Event to signal.
  *
@@ -64,55 +64,58 @@ extern void fiber_event_send(kevent_t event);
 
 /**
  *
- * @brief Set event handler request
+ * @brief Set event handler request.
  *
- * This routine specifies the event handler that runs (in the context of the
- * microkernel server fiber) when the associated event is signaled. Specifying
+ * This routine specifies the event handler that runs in the context of the
+ * microkernel server fiber when the associated event is signaled. Specifying
  * a non-NULL handler installs a new handler, while specifying a NULL event
  * handler removes the existing event handler.
  *
- * A new event handler cannot be installed if one already exists for that event;
- * the old handler must be removed first. However, it is permitted to replace
- * the NULL event handler with itself.
+ * A new event handler cannot be installed if one already exists for that event.
+ * The old handler must be removed first. However, the NULL event handler can be
+ * replaced with itself.
  *
- * @param event Event upon which to reigster.
+ * @param event Event upon which to register.
  * @param handler Function pointer to handler.
  *
- * @return RC_FAIL if an event handler exists or the event number is invalid,
- *          else RC_OK
+ * @retval RC_FAIL If an event handler exists or the event number is invalid.
+ * @retval RC_OK Otherwise.
  */
 extern int task_event_handler_set(kevent_t event, kevent_handler_t handler);
 
 /**
  *
- * @brief Signal an event request
+ * @brief Signal an event request.
  *
  * This routine signals the specified event from a task. If an event handler
- * is installed for that event, it will run; if no event handler is installed,
+ * is installed for that event, it will run. If no event handler is installed,
  * any task waiting on the event is released.
  *
  * @param event Event to signal.
  *
- * @return RC_FAIL if event number is invalid, else RC_OK
+ * @retval RC_FAIL If the event number is invalid.
+ * @retval RC_OK Otherwise.
  */
 extern int task_event_send(kevent_t event);
 
 /**
  *
- * @brief Test for event request with timeout
+ * @brief Test for an event request with timeout.
  *
  * This routine tests an event to see if it has been signaled.
  *
- * @param event Event for which to test.
- * @param timeout Affects the action taken should the event not yet be
- * signaled. If TICKS_NONE, then return immediately. If TICKS_UNLIMITED, then
- * wait as long as necessary. Otherwise wait up to the specified number of
- * ticks before timing out.
+ * @param event Event to test.
+ * @param timeout Determines the action to take when the event has not yet
+ *        been signaled.
+ *        For TICKS_NONE, return immediately.
+ *        For TICKS_UNLIMITED, wait as long as necessary.
+ *        Otherwise, wait up to the specified number of ticks before
+ *        timing out.
  *
  * @retval RC_OK Successfully received signaled event
  * @retval RC_TIME Timed out while waiting for signaled event
  * @retval RC_FAIL Failed to immediately receive signaled event when
- * @a timeout = TICKS_NONE
+ *                 timeout = TICKS_NONE
  */
 extern int task_event_recv(kevent_t event, int32_t timeout);
 
