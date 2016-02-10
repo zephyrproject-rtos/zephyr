8e6fc98203b316eafb8f9b0fc7604c792d7643c3
L.S. Cook <leonax.cook@intel.com>
2016-02-10 08:53:07 -0800

    doc: Edit API documentation on microkernel task_irq.h
    Reviewed for clarity and consistency in punctuation and style among
    all the microkernel APIs.
    
    Change-Id: I61548a906b4e89f2fbed214dbf7cda9fadac217d
    Signed-off-by: L.S. Cook <leonax.cook@intel.com>

--- include/microkernel/task_irq.h
+++ include/microkernel/task_irq.h
@@ -37,18 +37,18 @@ extern "C" {
 
 /**
  *
- * @brief Register a task IRQ object
+ * @brief Register a task IRQ object.
  *
  * This routine connects a task IRQ object to a system interrupt based
  * upon the specified IRQ and priority values.
  *
- * IRQ allocation is done via the microkernel server fiber so that simultaneous
- * allocation requests are single-threaded.
+ * IRQ allocation is done via the microkernel server fiber, making simultaneous
+ * allocation requests single-threaded.
  *
- * @param irq_obj IRQ object identifier
- * @param irq Request IRQ
- * @param priority Requested interrupt priority
- * @param flags IRQ flags
+ * @param irq_obj IRQ object identifier.
+ * @param irq Request IRQ.
+ * @param priority Requested interrupt priority.
+ * @param flags IRQ flags.
  *
  * @return assigned interrupt vector if successful, INVALID_VECTOR if not
  */
@@ -57,30 +57,33 @@ extern uint32_t task_irq_alloc(kirq_t irq_obj, uint32_t irq, uint32_t priority,
 
 /**
  *
- * @brief Re-enable a task IRQ object's interrupt
+ * @brief Re-enable a task IRQ object's interrupt.
  *
  * This re-enables the interrupt for a task IRQ object.
- * @param irq_obj IRQ object identifier
+ *
+ * @param irq_obj IRQ object identifier.
+ *
  * @return N/A
  */
 extern void task_irq_ack(kirq_t irq_obj);
 
 /**
- * @brief Wait for task IRQ to signal an interrupt
+ * @brief Wait for task IRQ to signal an interrupt.
  *
  * This routine waits up to @a timeout ticks for the IRQ object @a irq_obj
  * to signal an interrupt.
  *
- * @param irq_obj IRQ object identifier
- * @param timeout Affects the action taken should the task IRQ not yet be
- * signaled. If TICKS_NONE, the return immediately. If TICKS_UNLIMITED, then
- * wait as long as necessary. Otherwise wait up to the specified number of
- * ticks before timing out.
+ * @param irq_obj IRQ object identifier.
+ * @param timeout Determines the action when task IRQ is not yet signaled.
+ *  For TICKS_NONE, return immediately.
+ *  For TICKS_UNLIMITED, wait as long as necessary.
+ *  Otherwise, wait up to the specified number of ticks before timing out.
  *
- * @retval RC_OK Successfully detected signaled interrupt
- * @retval RC_TIME Timed out while waiting for interrupt
+ * @retval RC_OK Successfully detected signaled interrupt.
+ * @retval RC_TIME Timed out while waiting for interrupt.
  * @retval RC_FAIL Failed to immediately detect signaled interrupt when
- * @a timeout = TICKS_NONE
+ * @a timeout = TICKS_NONE.
+ * @sa TICKS_NONE, TICKS_UNLIMITED
  */
 extern int task_irq_wait(kirq_t irq_obj, int32_t timeout);
 
