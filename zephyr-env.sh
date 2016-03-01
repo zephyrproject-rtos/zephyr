fb61bd4d5d28fc46c2e8347f95fc0b6fb65dad81
Benjamin Walsh <benjamin.walsh@windriver.com>
2016-03-01 10:41:00 -0500

    add missing license to zephyr-env.sh
    Change-Id: I0a43e634534ee1cb43c6ad2dc966d9ee20e0b9e5
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>

--- zephyr-env.sh
+++ zephyr-env.sh
@@ -1,3 +1,18 @@
+#
+# Copyright (c) 2015 Wind River Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 if [ "X$(basename -z -- "$0")" "==" "Xzephyr-env.sh" ]; then
     echo "Source this file (do NOT execute it!) to set the Zephyr Kernel environment."
