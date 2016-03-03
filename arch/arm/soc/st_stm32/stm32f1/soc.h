1c78f3d39b54b025208287f55903d94d4d85ee82
Maciek Borzecki <maciek.borzecki@gmail.com>
2016-03-03 15:32:21 +0100

    st_stm32/stm32f1: introduce STM32F1x SoC family
    The patch introduces a new family of SoCs based on STMicroelectronics'
    STM32 MCU line. The patch introduces a basic arch/arm/soc layout along
    with configuration of 2 MCUs from STM32F1 series: STM32F103VE and
    STM32F103RB.
    
    The patch assumes that other MCUs from STM32 family will be included
    under arch/arm/soc/st_stm32 tree, to achieve the following layout:
    
    arch/
      arm/
        soc/
          st_stm32/
            stm32f0/
            stm32f1/
            stm32f2/
            ...
            stm32l0/
    
    Most of the configuration within a single MCU family (ex. STM32F1) is
    shared, however individual MCUs differ with respect to SRAM size, flash
    size or the number of available peripherals. The patch assumes that per
    MCU line Kconfig.soc.family file should introduce basic setup for given
    series. This can be further tuned by per MCU files, with
    Kconfig.soc.stm32f103rb and Kconfig.soc.stm32f103ve as examples.
    
    Each family defines a configuration option, ex. CONFIG_STM32F10X, while
    individual MCUs define a corresponding per MCU config options,
    ex. CONFIG_STM32F103VE.
    
    From the menuconfig perspective, the user is presented with a family
    selection under General Platform Configuration/SoC Selection.  A
    specific MCU model can be selected by accessing General Platform
    Configuration/STM32F1x MCU Selection, with the default entry being
    selected by the board configuration.
    
    Change-Id: I22e4defd4a08ed1b2e2cad0e214b34f565e08831
    Origin: Original
    Signed-off-by: Maciej Borzecki <maciek.borzecki@gmail.com>

---
+++ arch/arm/soc/st_stm32/stm32f1/soc.h
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2016 Open-RnD Sp. z o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file SoC configuration macros for the STM32F103 family processors.
+ *
+ * Based on reference manual:
+ *   STM32F101xx, STM32F102xx, STM32F103xx, STM32F105xx and STM32F107xx
+ *   advanced ARM ® -based 32-bit MCUs
+ *
+ * Chapter 3.3: Memory Map
+ */
+
+
+#ifndef _STM32F1_SOC_H_
+#define _STM32F1_SOC_H_
+
+/* peripherals start address */
+#define PERIPH_BASE           0x40000000
+
+/* use naming consistent with STMF10x Peripherals Library */
+#define APB1PERIPH_BASE        PERIPH_BASE
+#define APB2PERIPH_BASE       (PERIPH_BASE + 0x10000)
+#define AHBPERIPH_BASE        (PERIPH_BASE + 0x20000)
+
+/* UART */
+#define USART1_ADDR           (APB2PERIPH_BASE + 0x3800)
+#define USART2_ADDR           (APB1PERIPH_BASE + 0x4400)
+#define USART3_ADDR           (APB1PERIPH_BASE + 0x4800)
+
+/* Reset and Clock Control */
+#define RCC_BASE              (AHBPERIPH_BASE + 0x1000)
+
+#define GPIO_REG_SIZE         0x400
+#define GPIOA_BASE            (APB2PERIPH_BASE + 0x0800)
+#define GPIOB_BASE            (APB2PERIPH_BASE + 0x0C00)
+#define GPIOC_BASE            (APB2PERIPH_BASE + 0x1000)
+#define GPIOD_BASE            (APB2PERIPH_BASE + 0x1400)
+#define GPIOE_BASE            (APB2PERIPH_BASE + 0x1800)
+#define GPIOF_BASE            (APB2PERIPH_BASE + 0x1C00)
+#define GPIOG_BASE            (APB2PERIPH_BASE + 0x2000)
+/* base address for where GPIO registers start */
+#define GPIO_PORTS_BASE       (GPIOA_BASE)
+
+#ifndef _ASMLANGUAGE
+
+#include <device.h>
+#include <misc/util.h>
+#include <drivers/rand32.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _STM32F1_SOC_H_ */
