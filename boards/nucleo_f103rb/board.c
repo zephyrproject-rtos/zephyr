4ee4a6abad7e2372fbd9f39e61e4fe814b0acf35
Maciek Borzecki <maciek.borzecki@gmail.com>
2016-03-05 21:49:40 +0100

    boards/nucleo_f103rb: add new board
    Add configuration for Nucleo-64 F103RB board. By default, the UART
    console is forwarded to USART2, available on STLink V2-1 USB
    connector. All GPIO ports available on the connecot headers are
    enabled.
    
    Change-Id: I266170d1288ef27f668410c5737c46cdf716e137
    Origin: Original
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

---
+++ boards/nucleo_f103rb/board.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2016 Open-RnD Sp. z o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <pinmux/pinmux_stm32.h>
+
+/* pin assignments for NUCLEO-F103RB board */
+static const struct pin_config pinconf[] = {
+#ifdef CONFIG_UART_STM32_PORT_0
+	{STM32_PIN_PA9,  STM32F1_PINMUX_FUNC_PA9_USART1_TX},
+	{STM32_PIN_PA10, STM32F1_PINMUX_FUNC_PA10_USART1_RX},
+#endif	/* CONFIG_UART_STM32_PORT_0 */
+#ifdef CONFIG_UART_STM32_PORT_1
+	{STM32_PIN_PA2, STM32F1_PINMUX_FUNC_PA2_USART2_TX},
+	{STM32_PIN_PA3, STM32F1_PINMUX_FUNC_PA3_USART2_RX},
+#endif	/* CONFIG_UART_STM32_PORT_1 */
+#ifdef CONFIG_UART_STM32_PORT_2
+	{STM32_PIN_PB10, STM32F1_PINMUX_FUNC_PB10_USART3_TX},
+	{STM32_PIN_PB11, STM32F1_PINMUX_FUNC_PB11_USART3_RX},
+#endif	/* CONFIG_UART_STM32_PORT_2 */
+};
+
+const struct pin_config *stm32_board_get_pinconf(size_t *pins)
+{
+	*pins = ARRAY_SIZE(pinconf);
+
+	return pinconf;
+}
