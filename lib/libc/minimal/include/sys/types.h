6692e982988ebd83742a0cb4ba09380290420719
Johan Hedberg <johan.hedberg@intel.com>
2016-03-09 21:42:01 +0200

    libc: Move ssize_t definition to sys/types.h
    There's no need to have a separate file for this, and we'll be adding
    other types like off_t here soon as well.
    
    Change-Id: I40629a5a0fba7af44828eaeead294e4e55844bb0
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- lib/libc/minimal/include/sys/types.h
+++ lib/libc/minimal/include/sys/types.h
@@ -17,6 +17,19 @@
 #ifndef __INC_sys_types_h__
 #define __INC_sys_types_h__
 
-#include <bits/ssize_t.h>
+#if !defined(__ssize_t_defined)
+#define __ssize_t_defined
+
+#ifdef __i386
+typedef long int ssize_t;
+#elif defined(__ARM_ARCH)
+typedef int ssize_t;
+#elif defined(__arc__)
+typedef int ssize_t;
+#else
+#error "The minimal libc library does not recognize the architecture!\n"
+#endif
+
+#endif
 
 #endif /* __INC_sys_types_h__ */
