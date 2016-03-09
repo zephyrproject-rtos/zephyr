ed4f794ac8d93292942a34745073cfe7c0d85159
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-03-10 00:11:28 +0200

    net: apps: Change echo-server to use common testing header file
    Move network testing setup from echo-server to common net_testing.h
    file which makes the application much simpler.
    
    Change-Id: I6f68826916c870ad7db597d30f20245bada70091
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- samples/net/echo_server/src/echo-server.c
+++ samples/net/echo_server/src/echo-server.c
@@ -31,89 +31,29 @@
 #include <net/net_core.h>
 #include <net/net_socket.h>
 
-#include <contiki/ipv6/uip-ds6-route.h>
-#include <contiki/ip/uip.h>
-#include <contiki/ipv6/uip-ds6.h>
-
 #include <bluetooth/bluetooth.h>
 #include <ipsp/src/ipss.h>
 
-#if !defined(CONFIG_ETHERNET) && !defined(CONFIG_BLUETOOTH)
-/* The peer is the client in our case. Just invent a mac
- * address for it because lower parts of the stack cannot set it
- * in this test as we do not have any radios.
- */
-static uint8_t peer_mac[] = { 0x15, 0x0a, 0xbe, 0xef, 0xf0, 0x0d };
-
-/* This is my mac address
- */
-static uint8_t my_mac[] = { 0x0a, 0xbe, 0xef, 0x15, 0xf0, 0x0d };
+#if defined(CONFIG_NET_TESTING)
+#include <net_testing.h>
 #endif
 
 #if defined(CONFIG_NETWORKING_WITH_IPV6)
-#if 0
-/* The 2001:db8::/32 is the private address space for documentation RFC 3849 */
-#define MY_IPADDR { { { 0x20, 0x01, 0x0d, 0xb8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x1 } } }
-#define PEER_IPADDR { { { 0x20, 0x01, 0x0d, 0xb8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x2 } } }
-#else
-#define PEER_IPADDR { { { 0xfe, 0x80, 0, 0, 0, 0, 0, 0, 0, 0x17, 0x0a, 0xbe, 0xef, 0xf0, 0x0d, 0 } } }
-#define MY_IPADDR { { { 0xfe, 0x80, 0, 0, 0, 0, 0, 0, 0, 0x08, 0xbe, 0xef, 0x15, 0xf0, 0x0d, 0 } } }
-#endif
-
 /* admin-local, dynamically allocated multicast address */
 #define MCAST_IPADDR { { { 0xff, 0x84, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x2 } } }
-
-static const struct in6_addr in6addr_peer = PEER_IPADDR;
-static struct in6_addr in6addr_my = MY_IPADDR;
 #else
-/* The 192.0.2.0/24 is the private address space for documentation RFC 5737 */
-#define MY_IPADDR { { { 192, 0, 2, 2 } } }
-
 /* Organization-local 239.192.0.0/14 */
 #define MCAST_IPADDR { { { 239, 192, 0, 2 } } }
 #endif
+
 #define MY_PORT 4242
 
 static inline void init_app(void)
 {
 	PRINT("%s: run echo server\n", __func__);
 
-#if !defined(CONFIG_BLUETOOTH)
-#if !defined(CONFIG_ETHERNET)
-	net_set_mac(my_mac, sizeof(my_mac));
-#endif
-
-#if defined(CONFIG_NETWORKING_WITH_IPV4)
-	{
-		uip_ipaddr_t addr;
-		uip_ipaddr(&addr, 192,0,2,2);
-		uip_sethostaddr(&addr);
-	}
-#endif
-
-#if defined(CONFIG_NETWORKING_IPV6_NO_ND)
-	{
-		uip_ipaddr_t *addr;
-
-		/* Set the routes and neighbor cache only if we do not have
-		 * neighbor discovery enabled. This setting should only be
-		 * used if running in qemu and using slip (tun device).
-		 */
-		const uip_lladdr_t *lladdr = (const uip_lladdr_t *)&peer_mac;
-
-		addr = (uip_ipaddr_t *)&in6addr_peer;
-		uip_ds6_defrt_add(addr, 0);
-
-		/* We cannot send to peer unless it is in neighbor
-		 * cache. Neighbor cache should be populated automatically
-		 * but do it here so that test works from first packet.
-		 */
-		uip_ds6_nbr_add(addr, lladdr, 0, NBR_REACHABLE);
-
-		addr = (uip_ipaddr_t *)&in6addr_my;
-		uip_ds6_addr_add(addr, 0, ADDR_MANUAL);
-	}
-#endif
+#if defined(CONFIG_NET_TESTING)
+	net_testing_setup();
 #endif
 }
 
@@ -142,12 +82,8 @@ static inline struct net_buf *prepare_reply(const char *name,
 	 */
 	reverse(ip_buf_appdata(buf), ip_buf_appdatalen(buf));
 
-#if !defined(CONFIG_ETHERNET) && !defined(CONFIG_BLUETOOTH)
-	/* Set the mac address of the peer in net_buf because
-	 * there is no radio layer involved in this test app.
-	 * Normally there is no need to do this.
-	 */
-	memcpy(&ip_buf_ll_src(buf), &peer_mac, sizeof(ip_buf_ll_src(buf)));
+#if defined(CONFIG_NET_TESTING)
+	net_testing_set_reply_address(buf);
 #endif
 
 	return buf;
@@ -195,6 +131,7 @@ static inline bool get_context(struct net_context **recv,
 	static struct net_addr my_addr;
 
 #if defined(CONFIG_NETWORKING_WITH_IPV6)
+	static const struct in6_addr in6addr_my = IN6ADDR_ANY_INIT;
 	static const struct in6_addr in6addr_any = IN6ADDR_ANY_INIT;
 	static const struct in6_addr in6addr_mcast = MCAST_IPADDR;
 
