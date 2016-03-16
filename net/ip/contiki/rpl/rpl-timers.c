9641a0fed09702d8bbfc86c506e7e4e4cda8cd1d
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-03-16 17:11:32 +0200

    net: rpl: Enable timer debugging via Kconfig
    User can activate RPL timer debugging by setting
    CONFIG_NETWORK_IP_STACK_DEBUG_RPL_TIMERS config option.
    
    Change-Id: I66079640e4135a0f85620cc4eca04612eee1a890
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- net/ip/contiki/rpl/rpl-timers.c
+++ net/ip/contiki/rpl/rpl-timers.c
@@ -47,7 +47,9 @@
 #include "lib/random.h"
 #include "sys/ctimer.h"
 
-#define DEBUG DEBUG_NONE
+#ifdef CONFIG_NETWORK_IP_STACK_DEBUG_RPL_TIMERS
+#define DEBUG 1
+#endif
 #include "contiki/ip/uip-debug.h"
 
 /*---------------------------------------------------------------------------*/
