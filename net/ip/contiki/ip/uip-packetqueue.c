0588b3ceedab7fbf1aa70507db04eb3876f0b0d6
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2016-03-17 16:00:44 +0200

    net: contiki: Enable uip packet queue debugging via Kconfig
    User can activate packet queue debugging by setting
    CONFIG_NETWORK_IP_STACK_DEBUG_PACKET_QUEUE config option.
    
    Change-Id: I70ea0b48b3c120e085a8b342981fc7a9028d45b1
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>

--- net/ip/contiki/ip/uip-packetqueue.c
+++ net/ip/contiki/ip/uip-packetqueue.c
@@ -9,7 +9,10 @@
 #define MAX_NUM_QUEUED_PACKETS 2
 MEMB(packets_memb, struct uip_packetqueue_packet, MAX_NUM_QUEUED_PACKETS);
 
-#define DEBUG DEBUG_NONE
+#ifdef CONFIG_NETWORK_IP_STACK_DEBUG_PACKET_QUEUE
+#define DEBUG 1
+#endif
+
 #include "contiki/ip/uip-debug.h"
 
 /*---------------------------------------------------------------------------*/
