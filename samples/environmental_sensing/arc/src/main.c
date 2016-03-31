9773fdaadef7f05accdd6859a7412e3b545fac2d
Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
2016-03-31 18:56:07 +0300

    samples: add environmental sensing sample for Arduino 101
    The ARC application collects temperature, humidity and pressure data from
    a set of sensors connected to the Arduino 101 and sends it to the x86 core
    through IPM. The collected data is also displayed on a Grove LCD.
    
    The x86 application exposes the received sensor data as a simple Bluetooth
    Environmental Sensing Service.
    
    This version of the sample does not offer notifications when the sensor data
    values change.
    
    Change-Id: Iee456d5d2455c7ccb7c5923ef3c94ecb20b5cecb
    Signed-off-by: Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>

---
+++ samples/environmental_sensing/arc/src/main.c
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <device.h>
+#include <ipm.h>
+#include <ipm/ipm_quark_se.h>
+#include <misc/printk.h>
+#include <misc/util.h>
+#include <sensor.h>
+#include <zephyr.h>
+
+#ifdef CONFIG_GROVE_LCD_RGB
+#include <display/grove_lcd.h>
+#include <stdio.h>
+#include <string.h>
+#endif
+
+QUARK_SE_IPM_DEFINE(ess_ipm, 0, QUARK_SE_IPM_OUTBOUND);
+
+struct channel_info {
+	int chan;
+	char *dev_name;
+};
+
+/* change device names if you want to use different sensors */
+static struct channel_info info[] = {
+	{SENSOR_CHAN_TEMP, "HDC1008"},
+	{SENSOR_CHAN_HUMIDITY, "HDC1008"},
+	{SENSOR_CHAN_PRESS, "BMP280"},
+};
+
+void main(void)
+{
+	struct device *ipm, *dev[ARRAY_SIZE(info)];
+	struct sensor_value val[ARRAY_SIZE(info)];
+	unsigned int i;
+	int rc;
+
+	ipm = device_get_binding("ess_ipm");
+	if (ipm == NULL) {
+		printk("Failed to get ESS IPM device\n");
+		return;
+	}
+
+
+	for (i = 0; i < ARRAY_SIZE(info); i++) {
+		dev[i] = device_get_binding(info[i].dev_name);
+		if (dev[i] == NULL) {
+			printk("Failed to get \"%s\" device\n", info[i].dev_name);
+			return;
+		}
+	}
+
+#ifdef CONFIG_GROVE_LCD_RGB
+	struct device *glcd;
+
+	glcd = device_get_binding(GROVE_LCD_NAME);
+	if (glcd == NULL) {
+		printk("Failed to get Grove LCD\n");
+		return;
+	}
+
+	/* configure LCD */
+	glcd_function_set(glcd, GLCD_FS_ROWS_2 | GLCD_FS_DOT_SIZE_LITTLE |
+			  GLCD_FS_8BIT_MODE);
+	glcd_display_state_set(glcd, GLCD_DS_DISPLAY_ON);
+#endif
+
+	while (1) {
+		/* fetch sensor samples */
+		for (i = 0; i < ARRAY_SIZE(info); i++) {
+			rc = sensor_sample_fetch(dev[i]);
+			if (rc) {
+				printk("Failed to fetch sample for device %s (%d)\n",
+				       info[i].dev_name, rc);
+			}
+		}
+
+		/* send sensor data to x86 core via IPM */
+		for (i = 0; i < ARRAY_SIZE(info); i++) {
+			rc = sensor_channel_get(dev[i], info[i].chan, &val[i]);
+			if (rc) {
+				printk("Failed to get data for device %s (%d)\n",
+				       info[i].dev_name, rc);
+				continue;
+			}
+
+			rc = ipm_send(ipm, 1, info[i].chan, &val[i], sizeof(val[i]));
+			if (rc) {
+				printk("Failed to send data for device %s (%d)\n",
+				       info[i].dev_name, rc);
+			}
+		}
+
+#ifdef CONFIG_GROVE_LCD_RGB
+		char row[16];
+
+		/* clear LCD */
+		memset(row, ' ', sizeof(row));
+		glcd_cursor_pos_set(glcd, 0, 0);
+		glcd_print(glcd, row, sizeof(row));
+		glcd_cursor_pos_set(glcd, 0, 1);
+		glcd_print(glcd, row, sizeof(row));
+
+		/* display temperature on LCD */
+		glcd_cursor_pos_set(glcd, 0, 0);
+		sprintf(row, "T:%d.%d%cC", val[0].val1,
+			val[0].val2/100000, 223 /* degree symbol */);
+		glcd_print(glcd, row, strlen(row));
+
+		/* display himidity on LCD */
+		glcd_cursor_pos_set(glcd, 17 - strlen(row), 0);
+		sprintf(row, "RH:%d%c", val[1].val1/1000,
+			37 /* percent symbol */);
+		glcd_print(glcd, row, strlen(row));
+
+		/* display pressure on LCD */
+		glcd_cursor_pos_set(glcd, 0, 1);
+		sprintf(row, "P:%d.%02dkPa", val[2].val1,
+		       val[2].val2 / 10000);
+		glcd_print(glcd, row, strlen(row));
+#endif
+
+		task_sleep(sys_clock_ticks_per_sec);
+	}
+}
---
+++ samples/environmental_sensing/x86/src/main.c
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/uuid.h>
+#include <bluetooth/gatt.h>
+#include <device.h>
+#include <ipm.h>
+#include <ipm/ipm_quark_se.h>
+#include <misc/byteorder.h>
+#include <misc/printk.h>
+#include <sensor.h>
+#include <zephyr.h>
+
+#define DEVICE_NAME			"Environmental Sensor"
+#define DEVICE_NAME_LEN			(sizeof(DEVICE_NAME) - 1)
+#define TEMPERATURE_CUD			"Temperature"
+#define HUMIDITY_CUD			"Humidity"
+#define PRESSURE_CUD			"Pressure"
+
+QUARK_SE_IPM_DEFINE(ess_ipm, 0, QUARK_SE_IPM_INBOUND);
+
+static int16_t temp_value;
+static uint16_t humidity_value;
+static uint32_t pressure_value;
+
+static ssize_t read_u16(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+			void *buf, uint16_t len, uint16_t offset)
+{
+	const uint16_t *u16 = attr->user_data;
+	uint16_t value = sys_cpu_to_le16(*u16);
+
+	return bt_gatt_attr_read(conn, attr, buf, len, offset, &value,
+				 sizeof(value));
+}
+
+static ssize_t read_u32(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+			void *buf, uint16_t len, uint16_t offset)
+{
+	const uint32_t *u32 = attr->user_data;
+	uint16_t value = sys_cpu_to_le32(*u32);
+
+	return bt_gatt_attr_read(conn, attr, buf, len, offset, &value,
+				 sizeof(value));
+}
+
+static struct bt_gatt_attr attrs[] = {
+	BT_GATT_PRIMARY_SERVICE(BT_UUID_ESS),
+
+	BT_GATT_CHARACTERISTIC(BT_UUID_TEMPERATURE, BT_GATT_CHRC_READ),
+	BT_GATT_DESCRIPTOR(BT_UUID_TEMPERATURE, BT_GATT_PERM_READ,
+			   read_u16, NULL, &temp_value),
+	BT_GATT_CUD(TEMPERATURE_CUD, BT_GATT_PERM_READ),
+
+	BT_GATT_CHARACTERISTIC(BT_UUID_HUMIDITY, BT_GATT_CHRC_READ),
+	BT_GATT_DESCRIPTOR(BT_UUID_HUMIDITY, BT_GATT_PERM_READ,
+			   read_u16, NULL, &humidity_value),
+	BT_GATT_CUD(HUMIDITY_CUD, BT_GATT_PERM_READ),
+
+	BT_GATT_CHARACTERISTIC(BT_UUID_PRESSURE, BT_GATT_CHRC_READ),
+	BT_GATT_DESCRIPTOR(BT_UUID_PRESSURE, BT_GATT_PERM_READ,
+			   read_u32, NULL, &pressure_value),
+	BT_GATT_CUD(PRESSURE_CUD, BT_GATT_PERM_READ),
+};
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+};
+
+static struct bt_data sd[] = {
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static void bt_ready(int err)
+{
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	bt_gatt_register(attrs, ARRAY_SIZE(attrs));
+
+	err = bt_le_adv_start(BT_LE_ADV(BT_LE_ADV_IND), ad, ARRAY_SIZE(ad),
+			      sd, ARRAY_SIZE(sd));
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+}
+
+static void sensor_ipm_callback(void *context, uint32_t id, volatile void *data)
+{
+	volatile struct sensor_value *val = data;
+
+	switch (id) {
+	case SENSOR_CHAN_TEMP:
+		/* resolution of 0.01 degrees Celsius */
+		temp_value = val->val1 * 100 + val->val2 / 10000;
+		break;
+	case SENSOR_CHAN_HUMIDITY:
+		/* resolution of 0.01 percent */
+		humidity_value = val->val1 / 10;
+		break;
+	case SENSOR_CHAN_PRESS:
+		/* resolution of 0.1 Pa */
+		pressure_value = val->val1 * 10000 + val->val2 / 100;
+		break;
+	default:
+		break;
+	}
+}
+
+void main(void)
+{
+	struct device *ipm;
+	int rc;
+
+	rc = bt_enable(bt_ready);
+	if (rc) {
+		printk("Bluetooth init failed (err %d)\n", rc);
+		return;
+	}
+
+	ipm = device_get_binding("ess_ipm");
+	ipm_register_callback(ipm, sensor_ipm_callback, NULL);
+	ipm_set_enabled(ipm, 1);
+
+	task_sleep(TICKS_UNLIMITED);
+}
