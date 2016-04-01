f0f91d4a7bb3d99943d5b67dcc642eea60f1fd28
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2016-04-01 10:38:50 +0300

    drivers/nble: Refactor Nordic BLE chip enable functions
    Move NBLE pin handling to the driver from gap.c.
    
    Change-Id: I4b7fd408c623971d19da12784c656c5c605852a5
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- drivers/nble/gap.c
+++ drivers/nble/gap.c
@@ -18,7 +18,6 @@
 
 #include <nanokernel.h>
 #include <device.h>
-#include <gpio.h>
 
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/conn.h>
@@ -37,10 +36,6 @@ static uint8_t compatibe_firmware[4] = { '0', '3', '1', '7' };
 #undef BT_DBG
 #define BT_DBG(fmt, ...)
 #endif
-
-#define NBLE_SWDIO_PIN	6
-#define NBLE_RESET_PIN	NBLE_SWDIO_PIN
-#define NBLE_BTWAKE_PIN 5
 
 static bt_ready_cb_t bt_ready_cb;
 static bt_le_scan_cb_t *scan_dev_found_cb;
@@ -95,79 +90,13 @@ void on_nble_get_version_rsp(const struct nble_version_response *rsp)
 
 int bt_enable(bt_ready_cb_t cb)
 {
-	struct device *gpio;
 	int ret;
 
 	BT_DBG("");
 
-	gpio = device_get_binding(CONFIG_GPIO_DW_0_NAME);
-	if (!gpio) {
-		BT_ERR("Cannot find %s", CONFIG_GPIO_DW_0_NAME);
-		return -ENODEV;
-	}
-
-	ret = gpio_pin_configure(gpio, NBLE_RESET_PIN, GPIO_DIR_OUT);
-	if (ret) {
-		BT_ERR("Error configuring pin %d", NBLE_RESET_PIN);
-		return -ENODEV;
-	}
-
-	/* Reset hold time is 0.2us (normal) or 100us (SWD debug) */
-	ret = gpio_pin_write(gpio, NBLE_RESET_PIN, 0);
-	if (ret) {
-		BT_ERR("Error pin write %d", NBLE_RESET_PIN);
-		return -EINVAL;
-	}
-
-	ret = gpio_pin_configure(gpio, NBLE_BTWAKE_PIN, GPIO_DIR_OUT);
-	if (ret) {
-		BT_ERR("Error configuring pin %d", NBLE_BTWAKE_PIN);
-		return -ENODEV;
-	}
-
-	ret = gpio_pin_write(gpio, NBLE_BTWAKE_PIN, 1);
-	if (ret) {
-		BT_ERR("Error pin write %d", NBLE_BTWAKE_PIN);
-		return -EINVAL;
-	}
-
-	/**
-	 * NBLE reset is achieved by asserting low the SWDIO pin.
-	 * However, the BLE Core chip can be in SWD debug mode,
-	 * and NRF_POWER->RESET = 0 due to, other constraints: therefore,
-	 * this reset might not work everytime, especially after
-	 * flashing or debugging.
-	 */
-
-	/* sleep 1ms depending on context */
-	switch (sys_execution_context_type_get()) {
-	case NANO_CTX_FIBER:
-		fiber_sleep(MSEC(1));
-		break;
-	case NANO_CTX_TASK:
-		task_sleep(MSEC(1));
-		break;
-	default:
-		BT_ERR("ISR context is not supported");
-		return -EINVAL;
-	}
-
 	ret = nble_open();
 	if (ret) {
 		return ret;
-	}
-
-	ret = gpio_pin_write(gpio, NBLE_RESET_PIN, 1);
-	if (ret) {
-		BT_ERR("Error pin write %d", NBLE_RESET_PIN);
-		return -EINVAL;
-	}
-
-	/* Set back GPIO to input to avoid interfering with external debugger */
-	ret = gpio_pin_configure(gpio, NBLE_RESET_PIN, GPIO_DIR_IN);
-	if (ret) {
-		BT_ERR("Error configuring pin %d", NBLE_RESET_PIN);
-		return -ENODEV;
 	}
 
 	bt_ready_cb = cb;
