16d0b8b0a3a21f8cb3dd4057182e9926cca5930a
Daniel Leung <daniel.leung@intel.com>
2016-04-01 09:35:15 -0700

    tests: enable pinmux build tests
    Enable building pinmux drivers to catch build breakage.
    
    Origin: Original
    Change-Id: I86ec02423bf23ee6aca41b9413e240e984313fcf
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

---
+++ tests/drivers/pinmux/README.txt
@@ -0,0 +1,3 @@
+This is simply to build pinmux drivers to catch build breakage.
+
+The source code comes from samples/hello_world/nanokernel.
---
+++ tests/drivers/pinmux/src/main.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <zephyr.h>
+
+#if defined(CONFIG_STDOUT_CONSOLE)
+#include <stdio.h>
+#define PRINT           printf
+#else
+#include <misc/printk.h>
+#define PRINT           printk
+#endif
+
+
+/*
+ * @file
+ * @brief Hello World demo
+ * Nanokernel version of hello world demo
+ */
+
+
+void main(void)
+{
+	PRINT("Hello World!\n");
+}
+
