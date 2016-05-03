2d5645e57a73928e03feea84e29ec27dfb352b91
Andrew Boie <andrew.p.boie@intel.com>
2016-05-03 11:47:01 -0700

    nios2: add minimal crt0.S
    At the moment this just jumps into prep_c, with comments left
    on other things that need to be done. Having this here ensures that
    the early boot code isn't discarded by gc-sections.
    
    vector_table.c removed, it isn't the right approach for this CPU.
    Proper method for initializing reset and exception vectors still
    being investigated.
    
    Change-Id: Id7965c671f1a55c42ecfb65119497405a646bec4
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- include/arch/nios2/asm.h
+++ include/arch/nios2/asm.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Intel Corporation
+ * Copyright (c) 2015 Intel Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,31 +14,13 @@
  * limitations under the License.
  */
 
-#include <nano_private.h>
+#ifndef _NIOS2_ASM_H
+#define _NIOS2_ASM_H
 
-void __reset_handler(void)
-{
-	/* stub */
-}
-
-void __exception_handler(void)
-{
-	/* stub */
-}
+#include <toolchain.h>
+#include <sections.h>
 
 
-struct vector_table {
-	uint32_t reset;
-	uint32_t exception;
-};
 
-/* FIXME not using CONFIG_RESET_VECTOR or CONFIG_EXCEPTION_VECTOR like we
- * should
- */
-struct vector_table _vector_table _GENERIC_SECTION(.exc_vector_table) = {
-	(uint32_t)__reset_handler,
-	(uint32_t)__exception_handler
-};
-
-extern struct vector_table __start _ALIAS_OF(_vector_table);
+#endif
 
