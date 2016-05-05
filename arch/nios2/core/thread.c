e91f7101c6b07f98b3d39643e055561b1cea8847
Andrew Boie <andrew.p.boie@intel.com>
2016-05-05 10:30:14 -0700

    nios2: update _new_thread prototype to latest changes
    Now matches kernel/nanokernel/include/nano_internal.h.
    
    Change-Id: I4dbbf50aa05c55de42100a6896fe0fa3b26955ec
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- arch/nios2/core/thread.c
+++ arch/nios2/core/thread.c
@@ -19,32 +19,11 @@
 
 tNANO _nanokernel = {0};
 
-/**
- *
- * @brief Create a new kernel execution context
- *
- * This function initializes a thread control structure (TCS) for a
- * new kernel execution context. A fake stack frame is created as if
- * the context had been "swapped out" via _Swap()
- *
- * @param stack_memory pointer to the context stack area
- * @param stack_size size of contexts stack area
- * @param thread_func new contexts entry function
- * @param parameter1 first entry function parameter
- * @param parameter2 second entry function parameter
- * @param parameter3 third entry function parameter
- * @param priority Priority of the new context
- * @param options Additional options for the context
- *
- * @return none
- *
- * \NOMANUAL
- */
-
 void _new_thread(char *stack_memory, unsigned stack_size,
-		 _thread_entry_t thread_func, void *parameter1,
-		 void *parameter2, void *parameter3, int priority,
-		 unsigned options)
+		 void *uk_task_ptr, _thread_entry_t thread_func,
+		 _thread_arg_t arg1, _thread_arg_t arg2,
+		 _thread_arg_t arg3,
+		 int prio, unsigned options)
 {
 	/* STUB */
 }
