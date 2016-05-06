49dec0dad5d5810a92dd4588cac8bef68d396638
Chuck Jordan <cjordan@synopsys.com>
2016-05-06 12:56:17 -0700

    arc: clarify in comment that ARC must have timer0
    The timer implementation for ARC currently requires timer0 to be present.
    I've added a comment that this is an assumption and to encourage developers
    to build the ARC CPU with Timer0, when it is to be used with Zephyr.
    There is also an optional provision for a Timer1. In future, this
    code could be conditional to use either timer.
    
    Change-Id: I4eb3aec59ba4e85f8b70d5531b21bdaab00b93bb
    Signed-off-by: Chuck Jordan <cjordan@synopsys.com>

--- drivers/timer/arcv2_timer0.c
+++ drivers/timer/arcv2_timer0.c
@@ -63,12 +63,14 @@
 #include <misc/__assert.h>
 
 /*
- * A board support package's board.h header must provide definitions for the
- * following constants:
+ * The file(s) arch/arc/soc/<soc>/soc.h must provide a definition for the
+ * following constant:
  *
  *    CONFIG_ARCV2_TIMER0_CLOCK_FREQ
  *
- * This is the sysTick input clock frequency.
+ * This is the ARC CPU input clock frequency.
+ * note: This implementation assumes Timer0 is present. Be sure
+ * to build the ARC CPU with Timer0.
  */
 
 #include <board.h>
