4ad41d8d7fc9c22eb51c40ae614738be92262580
Chuck Jordan <cjordan@synopsys.com>
2016-05-09 14:26:28 -0700

    arc: early init should invalidate d-cache and set vector table
    Some ARC targets can have a data-cache. Although there is no special
    instruction to clear exceptions during early init, it is necessary to
    invalidate the d-cache BEFORE any data is fetched. The ARC on arduino 101
    doesn't have d-cache, and will thus skip this d-cache invalidate.
    
    Also, it is important to set the vector table base register to point to
    the interrupt vector table EARLY, so that if an exception is encountered,
    the correct vector table is found. Set this base only if it is found to be
    different from the one compiled in to the code.
    These initialization steps assure that proper exception handling
    is in place during early init.
    
    Change-Id: Ie8b5928e5813e104680a6d6510c85d32dc8ed8f3
    Signed-off-by: Chuck Jordan <cjordan@synopsys.com>

--- arch/arc/core/prep_c.c
+++ arch/arc/core/prep_c.c
@@ -29,6 +29,7 @@
 #include <stdint.h>
 #include <toolchain.h>
 #include <linker-defs.h>
+#include <arch/arc/v2/aux_regs.h>
 
 /**
  *
@@ -75,6 +76,58 @@ static void dataCopy(void)
 }
 #endif
 
+
+/**
+ *
+ * @brief Invalidate the data cache if present
+ *
+ * For those ARC CPUs that have a data cache present,
+ * invalidate the data cache.
+ *
+ * @return N/A
+ */
+
+static void invalidate_dcache(void)
+{
+	unsigned int val;
+
+	val = _arc_v2_aux_reg_read(_ARC_V2_D_CACHE_BUILD);
+	val &= 0xff; /* version field */
+	if (val == 0) {
+		return; /* skip if d-cache is not present */
+	}
+	_arc_v2_aux_reg_write(_ARC_V2_DC_IVDC, 1);
+}
+
+
+/**
+ *
+ * @brief Adjust the vector table base
+ *
+ * Set the vector table base if the value found in the
+ * _ARC_V2_IRQ_VECT_BASE auxiliary register is different from the
+ * _VectorTable known by software. It is important to do this very early
+ * so that exception vectors can be handled.
+ *
+ * @return N/A
+ */
+
+static void adjust_vector_table_base(void)
+{
+	extern struct vector_table _VectorTable;
+	unsigned int vbr;
+	/* if the compiled-in vector table is different
+	 * from the base address known by the ARC CPU,
+	 * set the vector base to the compiled-in address.
+	 */
+	vbr = _arc_v2_aux_reg_read(_ARC_V2_IRQ_VECT_BASE);
+	vbr &= 0xfffffc00;
+	if (vbr != (unsigned int)&_VectorTable) {
+		_arc_v2_aux_reg_write(_ARC_V2_IRQ_VECT_BASE,
+					(unsigned int)&_VectorTable);
+	}
+}
+
 extern FUNC_NORETURN void _Cstart(void);
 /**
  *
@@ -87,6 +140,8 @@ extern FUNC_NORETURN void _Cstart(void);
 
 void _PrepC(void)
 {
+	adjust_vector_table_base();
+	invalidate_dcache();
 	bssZero();
 	dataCopy();
 	_Cstart();
