9fed4ce48a09d8c19b47e1ec90c33fd10bcecd4f
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2016-05-26 17:38:31 +0300

    Bluetooth: Add missing flags to beacon sample
    The flag BT_LE_AD_NO_BREDR shall be set which makes BT_DATA_FLAGS
    mandatory.
    
    Change-Id: Id933a4d73bc9fdcdc53c197041bb488d45ba149a
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- samples/bluetooth/beacon/src/main.c
+++ samples/bluetooth/beacon/src/main.c
@@ -33,6 +33,7 @@
  * https://github.com/google/eddystone/tree/master/eddystone-url
  */
 static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
 	BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0xaa, 0xfe),
 	BT_DATA_BYTES(BT_DATA_SVC_DATA16,
 		      0xaa, 0xfe, /* Eddystone UUID */
