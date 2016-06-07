3b9302158cd3ea40b10759f031c723c61ba38fc3
Andrew Boie <andrew.p.boie@intel.com>
2016-06-07 13:11:45 -0700

    nios2: set up common linker script for XIP and non-XIP
    We will require 6 variables to be defined by SOC-specific
    linker script; these values in turn can be pulled from
    defines in layout.h.
    
    To help position code correctly we define two new ELF sections
    for this arch, 'reset' and 'exceptions'.
    
    Change-Id: Idffbd53895945b7d0ec0aac281e5bf7c85b4b2c2
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- arch/nios2/soc/nios2e-zephyr/include/layout.h
+++ arch/nios2/soc/nios2e-zephyr/include/layout.h
@@ -14,10 +14,13 @@
  * limitations under the License.
  */
 
-/**
- * @brief Linker script for the Nios II/e CPU with timer and 16550 UART
- */
+#include <system.h>
 
-#include <layout.h>
+#define _RESET_VECTOR		ALT_CPU_RESET_ADDR
+#define	_EXC_VECTOR		ALT_CPU_EXCEPTION_ADDR
 
-#include <arch/nios2/linker.ld>
+#define _ROM_ADDR		ONCHIP_FLASH_0_DATA_BASE
+#define _ROM_SIZE		ONCHIP_FLASH_0_DATA_SPAN
+
+#define _RAM_ADDR		ONCHIP_MEMORY2_0_BASE
+#define _RAM_SIZE		ONCHIP_MEMORY2_0_SPAN
