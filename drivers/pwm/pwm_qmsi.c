9852ee55732972a4c5fa03e2243e6defba7186be
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2016-06-08 14:30:58 -0700

    pwm: Implement set period and duty cycle API
    This allow the PWM QMSI shim driver to implement the set duty cycle
    API, and correct the behavior of Set Value
    
    Jira: ZEP-69 ZEP-156 ZEP-158
    
    Change-Id: I47744958fed889116fbb5024343ea00f76ed7706
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>

--- drivers/pwm/pwm_qmsi.c
+++ drivers/pwm/pwm_qmsi.c
@@ -24,6 +24,31 @@
 #include "qm_pwm.h"
 #include "clk.h"
 
+#define HW_CLOCK_CYCLES_PER_USEC  (CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC / \
+				   USEC_PER_SEC)
+
+/* pwm uses 32 bits counter to control low and high period */
+#define MAX_LOW_PERIOD_IN_HW_CLOCK_CYCLES (((uint64_t)1) << 32)
+#define MAX_HIGH_PERIOD_IN_HW_CLOCK_CYCLES (((uint64_t)1) << 32)
+
+#define MAX_PERIOD_IN_HW_CLOCK_CYCLES (MAX_LOW_PERIOD_IN_HW_CLOCK_CYCLES + \
+				       MAX_HIGH_PERIOD_IN_HW_CLOCK_CYCLES)
+
+/* in micro seconds. */
+#define MAX_PERIOD (MAX_PERIOD_IN_HW_CLOCK_CYCLES / HW_CLOCK_CYCLES_PER_USEC)
+
+/**
+ * in micro seconds. To be able to get 1% granularity, MIN_PERIOD should
+ * have at least 100 HW clock cycles.
+ */
+#define MIN_PERIOD ((100 + (HW_CLOCK_CYCLES_PER_USEC - 1)) / \
+		    HW_CLOCK_CYCLES_PER_USEC)
+
+/* in micro seconds */
+#define DEFAULT_PERIOD 2000
+
+static uint32_t  pwm_channel_period[CONFIG_PWM_QMSI_NUM_PORTS];
+
 static int pwm_qmsi_configure(struct device *dev, int access_op,
 				 uint32_t pwm, int flags)
 {
@@ -42,9 +67,17 @@ static int __set_one_port(qm_pwm_t id, uint32_t pwm, uint32_t on, uint32_t off)
 	/* Disable timer to prevent any output */
 	qm_pwm_stop(id, pwm);
 
-	if ((off == 0) || (on == 0)) {
+	if (on == 0) {
 		/* stop PWM if so specified */
 		return 0;
+	}
+
+	/**
+	 * off period must be more than zero. Otherwise, the PWM pin will be
+	 * turned off. Let's use the minimum value which is 1 for this case.
+	 */
+	if (off == 0) {
+		off = 1;
 	}
 
 	/* PWM mode, user-defined count mode, timer disabled */
@@ -91,6 +124,11 @@ static int pwm_qmsi_set_values(struct device *dev, int access_op,
 {
 	int i;
 
+	if (off == 0) {
+		on--;
+		off = 1;
+	}
+
 	switch (access_op) {
 	case PWM_ACCESS_BY_PIN:
 		/* make sure the PWM port exists */
@@ -112,19 +150,85 @@ static int pwm_qmsi_set_values(struct device *dev, int access_op,
 
 }
 
-static int pwm_qmsi_set_duty_cycle(struct device *dev, int access_op,
-				 uint32_t pwm, uint8_t duty)
+static int pwm_qmsi_set_period(struct device *dev, int access_op,
+				 uint32_t pwm, uint32_t period)
 {
-	/* The IP block does not natively support duty cycle settings.
-	 * So need to use set_values().
-	 */
+	uint32_t *channel_period = dev->config->config_info;
 
-	ARG_UNUSED(dev);
-	ARG_UNUSED(access_op);
-	ARG_UNUSED(pwm);
-	ARG_UNUSED(duty);
+	if (channel_period == NULL) {
+		return -EIO;
+	}
 
-	return -ENOTSUP;
+	if (period < MIN_PERIOD || period > MAX_PERIOD) {
+		return -ENOTSUP;
+	}
+
+	switch (access_op) {
+	case PWM_ACCESS_BY_PIN:
+		/* make sure the PWM port exists */
+		if (pwm >= CONFIG_PWM_QMSI_NUM_PORTS) {
+			return -EIO;
+		}
+		channel_period[pwm] = period * HW_CLOCK_CYCLES_PER_USEC;
+		break;
+	case PWM_ACCESS_ALL:
+		for (int i = 0; i < CONFIG_PWM_QMSI_NUM_PORTS; i++) {
+			channel_period[i] = period *
+					    HW_CLOCK_CYCLES_PER_USEC;
+		}
+		break;
+	default:
+		return -ENOTSUP;
+	}
+
+	return 0;
+}
+
+static int pwm_qmsi_set_duty_cycle(struct device *dev, int access_op,
+				   uint32_t pwm, uint8_t duty)
+{
+	uint32_t *channel_period = dev->config->config_info;
+	uint32_t on, off;
+
+	if (channel_period == NULL) {
+		return -EIO;
+	}
+
+	if (duty > 100) {
+		return -ENOTSUP;
+	}
+
+	switch (access_op) {
+	case PWM_ACCESS_BY_PIN:
+		/* make sure the PWM port exists */
+		if (pwm >= CONFIG_PWM_QMSI_NUM_PORTS) {
+			return -EIO;
+		}
+		on = (channel_period[pwm] * duty) / 100;
+		off = channel_period[pwm] - on;
+		if (off == 0) {
+			on--;
+			off = 1;
+		}
+		return __set_one_port(QM_PWM_0, pwm, on, off);
+	case PWM_ACCESS_ALL:
+		for (int i = 0; i < CONFIG_PWM_QMSI_NUM_PORTS; i++) {
+			on = (channel_period[i] * duty) / 100;
+			off = channel_period[i] - on;
+			if (off == 0) {
+				on--;
+				off = 1;
+			}
+			if (__set_one_port(QM_PWM_0, i, on, off) != 0) {
+				return -EIO;
+			}
+		}
+		break;
+	default:
+		return -ENOTSUP;
+	}
+
+	return 0;
 }
 
 /*
@@ -160,6 +264,7 @@ static int pwm_qmsi_resume(struct device *dev)
 static struct pwm_driver_api pwm_qmsi_drv_api_funcs = {
 	.config = pwm_qmsi_configure,
 	.set_values = pwm_qmsi_set_values,
+	.set_period = pwm_qmsi_set_period,
 	.set_duty_cycle = pwm_qmsi_set_duty_cycle,
 	.suspend = pwm_qmsi_suspend,
 	.resume = pwm_qmsi_resume,
@@ -167,12 +272,18 @@ static struct pwm_driver_api pwm_qmsi_drv_api_funcs = {
 
 static int pwm_qmsi_init(struct device *dev)
 {
+	uint32_t *channel_period = dev->config->config_info;
+
+	for (int i = 0; i < CONFIG_PWM_QMSI_NUM_PORTS; i++) {
+		channel_period[i] = DEFAULT_PERIOD *
+				    HW_CLOCK_CYCLES_PER_USEC;
+	}
+
 	clk_periph_enable(CLK_PERIPH_PWM_REGISTER | CLK_PERIPH_CLK);
 	return 0;
 }
 
-DEVICE_AND_API_INIT(pwm_qmsi_0, CONFIG_PWM_QMSI_DEV_NAME, pwm_qmsi_init,
-		    NULL, NULL,
-		    SECONDARY, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
+DEVICE_AND_API_INIT(pwm_qmsi_0, CONFIG_PWM_QMSI_DEV_NAME, pwm_qmsi_init, NULL,
+		    pwm_channel_period, SECONDARY,
+		    CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
 		    (void *)&pwm_qmsi_drv_api_funcs);
-
