00e77da3569ef86d381192fa5a7198a5fb5dd470
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-14 09:56:50 +0300

    net: Generic connection handler for UDP and TCP
    The connection handling will provide an API where caller can
    register a callback which is called when desired UDP or TCP
    network packet is received.
    
    Change-Id: I9a80e8b30e92c4f2e9238ff58c24ac4648aad8ea
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

---
+++ net/yaip/connection.c
@@ -0,0 +1,745 @@
+/** @file
+ * @brief Generic connection related functions
+ */
+
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#if defined(CONFIG_NETWORK_IP_STACK_DEBUG_CONN)
+#define SYS_LOG_DOMAIN "net/conn"
+#define NET_DEBUG 1
+#endif
+
+#include <errno.h>
+#include <misc/util.h>
+
+#include <net/net_core.h>
+#include <net/nbuf.h>
+#include <net/net_stats.h>
+
+#include "net_private.h"
+#include "icmpv6.h"
+#include "icmpv4.h"
+#include "connection.h"
+
+/** Is this connection used or not */
+#define NET_CONN_IN_USE BIT(0)
+
+/** Remote address set */
+#define NET_CONN_REMOTE_ADDR_SET BIT(1)
+
+/** Local address set */
+#define NET_CONN_LOCAL_ADDR_SET BIT(2)
+
+/** Rank bits */
+#define NET_RANK_LOCAL_PORT         BIT(0)
+#define NET_RANK_REMOTE_PORT        BIT(1)
+#define NET_RANK_LOCAL_UNSPEC_ADDR  BIT(2)
+#define NET_RANK_REMOTE_UNSPEC_ADDR BIT(3)
+#define NET_RANK_LOCAL_SPEC_ADDR    BIT(4)
+#define NET_RANK_REMOTE_SPEC_ADDR   BIT(5)
+
+static struct net_conn conns[CONFIG_NET_MAX_CONN];
+
+/* This is only used for getting source and destination ports. Because
+ * both TCP and UDP header have these in the same location, we can check
+ * them both using the UDP struct.
+ */
+#define NET_CONN_BUF(buf) ((struct net_udp_hdr *)(net_nbuf_udp_data(buf)))
+
+#if NET_DEBUG
+static inline const char *proto2str(enum ip_protocol proto)
+{
+	switch (proto) {
+	case IPPROTO_ICMP:
+		return "ICMPv4";
+	case IPPROTO_TCP:
+		return "TCP";
+	case IPPROTO_UDP:
+		return "UDP";
+	case IPPROTO_ICMPV6:
+		return "ICMPv6";
+	default:
+		break;
+	}
+
+	return "<unknown>";
+}
+#endif /* NET_DEBUG */
+
+#if defined(CONFIG_NET_CONN_CACHE)
+
+/* Cache the connection so that we do not have to go
+ * through the full list of connections when receiving
+ * a network packet. The cache contains an index to
+ * corresponding entry in conns array.
+ *
+ * Hash value is constructed like this:
+ *
+ *   bit      description
+ *   0  - 3   bits from remote port
+ *   4  - 7   bits from local port
+ *   8  - 18  bits from remote address
+ *   19 - 29  bits from local address
+ *   30       family
+ *   31       protocol
+ */
+struct conn_hash {
+	uint32_t value;
+	int32_t idx;
+};
+
+struct conn_hash_neg {
+	uint32_t value;
+};
+
+/** Connection cache */
+static struct conn_hash conn_cache[CONFIG_NET_MAX_CONN];
+
+/** Negative cache, we definitely do not have a connection
+ * to these hosts.
+ */
+static struct conn_hash_neg conn_cache_neg[CONFIG_NET_MAX_CONN];
+
+#define TAKE_BIT(val, bit, max, used)				\
+	(((val & BIT(bit)) >> bit) << (max - used))
+
+static inline uint8_t ports_to_hash(uint16_t remote_port,
+				    uint16_t local_port)
+{
+	/* Note that we do not convert port value to network byte order */
+	return (remote_port & BIT(0)) |
+		((remote_port & BIT(4)) >> 3) |
+		((remote_port & BIT(8)) >> 6) |
+		((remote_port & BIT(15)) >> 12) |
+		(((local_port & BIT(0)) |
+		  ((local_port & BIT(4)) >> 3) |
+		  ((local_port & BIT(8)) >> 6) |
+		  ((local_port & BIT(15)) >> 12)) << 4);
+}
+
+static inline uint16_t ipv6_to_hash(struct in6_addr *addr)
+{
+	/* There is 11 bits available for IPv6 address */
+	/* Use more bits from the lower part of address space */
+	return
+		/* Take 3 bits from higher values */
+		TAKE_BIT(addr->s6_addr32[0], 31, 11, 1) |
+		TAKE_BIT(addr->s6_addr32[0], 15, 11, 2) |
+		TAKE_BIT(addr->s6_addr32[0], 7, 11, 3) |
+
+		/* Take 2 bits from higher middle values */
+		TAKE_BIT(addr->s6_addr32[1], 31, 11, 4) |
+		TAKE_BIT(addr->s6_addr32[1], 15, 11, 5) |
+
+		/* Take 2 bits from lower middle values */
+		TAKE_BIT(addr->s6_addr32[2], 31, 11, 6) |
+		TAKE_BIT(addr->s6_addr32[2], 15, 11, 7) |
+
+		/* Take 4 bits from lower values */
+		TAKE_BIT(addr->s6_addr32[3], 31, 11, 8) |
+		TAKE_BIT(addr->s6_addr32[3], 15, 11, 9) |
+		TAKE_BIT(addr->s6_addr32[3], 7, 11, 10) |
+		TAKE_BIT(addr->s6_addr32[3], 0, 11, 11);
+}
+
+static inline uint16_t ipv4_to_hash(struct in_addr *addr)
+{
+	/* There is 11 bits available for IPv4 address */
+	/* Use more bits from the lower part of address space */
+	return
+		TAKE_BIT(addr->s_addr[0], 31, 11, 1) |
+		TAKE_BIT(addr->s_addr[0], 27, 11, 2) |
+		TAKE_BIT(addr->s_addr[0], 21, 11, 3) |
+		TAKE_BIT(addr->s_addr[0], 17, 11, 4) |
+		TAKE_BIT(addr->s_addr[0], 14, 11, 5) |
+		TAKE_BIT(addr->s_addr[0], 11, 11, 6) |
+		TAKE_BIT(addr->s_addr[0], 8, 11, 7) |
+		TAKE_BIT(addr->s_addr[0], 5, 11, 8) |
+		TAKE_BIT(addr->s_addr[0], 3, 11, 9) |
+		TAKE_BIT(addr->s_addr[0], 2, 11, 10) |
+		TAKE_BIT(addr->s_addr[0], 0, 11, 11);
+}
+
+/* Return either the first free position in the cache (idx < 0) or
+ * the existing cached position (idx >= 0)
+ */
+static int32_t check_hash(enum ip_protocol proto,
+			  sa_family_t family,
+			  void *remote_addr,
+			  void *local_addr,
+			  uint16_t remote_port,
+			  uint16_t local_port,
+			  uint32_t *cache_value)
+{
+	int i, free_pos = -1;
+	uint32_t value = 0;
+
+	value = ports_to_hash(remote_port, local_port);
+
+#if defined(CONFIG_NET_UDP)
+	if (proto == IPPROTO_UDP) {
+		value |= BIT(31);
+	}
+#endif
+
+#if defined(CONFIG_NET_TCP)
+	if (proto == IPPROTO_TCP) {
+		value &= ~BIT(31);
+	}
+#endif
+
+#if defined(CONFIG_NET_IPV6)
+	if (family == AF_INET6) {
+		value |= BIT(30);
+		value |= ipv6_to_hash((struct in6_addr *)remote_addr) << 8;
+		value |= ipv6_to_hash((struct in6_addr *)local_addr) << 19;
+	}
+#endif
+
+#if defined(CONFIG_NET_IPV4)
+	if (family == AF_INET) {
+		value &= ~BIT(30);
+		value |= ipv4_to_hash((struct in_addr *)remote_addr) << 8;
+		value |= ipv4_to_hash((struct in_addr *)local_addr) << 19;
+	}
+#endif
+
+	for (i = 0; i < CONFIG_NET_MAX_CONN; i++) {
+		if (conn_cache[i].value == value) {
+			return i;
+		}
+
+		if (conn_cache[i].idx < 0 && free_pos < 0) {
+			free_pos = i;
+		}
+	}
+
+	if (free_pos >= 0) {
+		conn_cache[free_pos].value = value;
+		return free_pos;
+	}
+
+	*cache_value = value;
+
+	return -ENOENT;
+}
+
+static inline int32_t get_conn(enum ip_protocol proto,
+			       sa_family_t family,
+			       struct net_buf *buf,
+			       uint32_t *cache_value)
+{
+#if defined(CONFIG_NET_IPV4)
+	if (family == AF_INET) {
+		return check_hash(proto, family,
+				  &NET_IPV4_BUF(buf)->src,
+				  &NET_IPV4_BUF(buf)->dst,
+				  NET_UDP_BUF(buf)->src_port,
+				  NET_UDP_BUF(buf)->dst_port,
+				  cache_value);
+	}
+#endif
+
+#if defined(CONFIG_NET_IPV6)
+	if (family == AF_INET6) {
+		return check_hash(proto, family,
+				  &NET_IPV6_BUF(buf)->src,
+				  &NET_IPV6_BUF(buf)->dst,
+				  NET_UDP_BUF(buf)->src_port,
+				  NET_UDP_BUF(buf)->dst_port,
+				  cache_value);
+	}
+#endif
+
+	return -1;
+}
+
+static inline void cache_add_neg(uint32_t cache_value)
+{
+	int i;
+
+	for (i = 0; i < CONFIG_NET_MAX_CONN && cache_value > 0; i++) {
+		if (conn_cache_neg[i].value) {
+			continue;
+		}
+
+		NET_DBG("Add to neg cache value 0x%x", cache_value);
+
+		conn_cache_neg[i].value = cache_value;
+		break;
+	}
+}
+
+static inline bool cache_check_neg(uint32_t cache_value)
+{
+	int i;
+
+	for (i = 0; i < CONFIG_NET_MAX_CONN && cache_value > 0; i++) {
+		if (conn_cache_neg[i].value == cache_value) {
+			NET_DBG("Cache neg [%d] value 0x%x found",
+				i, cache_value);
+			return true;
+		}
+	}
+
+	return false;
+}
+
+static void cache_clear(void)
+{
+	int i;
+
+	for (i = 0; i < CONFIG_NET_MAX_CONN; i++) {
+		conn_cache[i].idx = -1;
+		conn_cache_neg[i].value = 0;
+	}
+}
+
+static inline enum net_verdict cache_check(enum ip_protocol proto,
+					   struct net_buf *buf,
+					   uint32_t *cache_value,
+					   int32_t *pos)
+{
+	*pos = get_conn(proto, net_nbuf_family(buf), buf, cache_value);
+	if (*pos >= 0) {
+		if (conn_cache[*pos].idx >= 0) {
+			/* Connection is in the cache */
+			struct net_conn *conn;
+
+			conn = &conns[conn_cache[*pos].idx];
+
+			NET_DBG("Cache %s listener for buf %p src port %u "
+				"dst port %u family %d cache[%d] 0x%x",
+				proto2str(proto), buf,
+				ntohs(NET_CONN_BUF(buf)->src_port),
+				ntohs(NET_CONN_BUF(buf)->dst_port),
+				net_nbuf_family(buf), *pos,
+				conn_cache[*pos].value);
+
+			return conn->cb(buf, conn->user_data);
+		}
+	} else if (*cache_value > 0) {
+		if (cache_check_neg(*cache_value)) {
+			NET_DBG("Drop by cache");
+			return NET_DROP;
+		}
+	}
+
+	return NET_CONTINUE;
+}
+#else
+#define cache_clear(...)
+#define cache_add_neg(...)
+#define cache_check(...) NET_CONTINUE
+#endif /* CONFIG_NET_CONN_CACHE */
+
+int net_conn_unregister(void *handle)
+{
+	struct net_conn *conn = (struct net_conn *)handle;
+
+	if (conn < &conns[0] || conn > &conns[CONFIG_NET_MAX_CONN]) {
+		return -EINVAL;
+	}
+
+	if (!conn->flags & NET_CONN_IN_USE) {
+		return -ENOENT;
+	}
+
+	NET_DBG("[%d] connection handler %p removed",
+		(conn - conns) / sizeof(*conn), conn);
+
+	conn->flags = 0;
+
+	return 0;
+}
+
+#if NET_DEBUG
+static inline
+void prepare_register_debug_print(char *dst, char *src,
+				  const struct net_addr *remote_addr,
+				  const struct net_addr *local_addr)
+{
+	if (remote_addr && remote_addr->family == AF_INET6) {
+#if defined(CONFIG_NET_IPV6)
+		snprintf(dst, sizeof(dst),
+			 net_sprint_ipv6_addr(&remote_addr->in6_addr));
+#else
+		snprintf(dst, sizeof(dst), "?");
+#endif
+
+	} else if (remote_addr && remote_addr->family == AF_INET) {
+#if defined(CONFIG_NET_IPV4)
+		snprintf(dst, sizeof(dst),
+			 net_sprint_ipv4_addr(&remote_addr->in_addr));
+#else
+		snprintf(dst, sizeof(dst), "?");
+#endif
+
+	} else {
+		snprintf(dst, sizeof(dst), "-");
+	}
+
+	if (local_addr && local_addr->family == AF_INET6) {
+#if defined(CONFIG_NET_IPV6)
+		snprintf(src, sizeof(src),
+			 net_sprint_ipv6_addr(&local_addr->in6_addr));
+#else
+		snprintf(src, sizeof(src), "?");
+#endif
+
+	} else if (local_addr && local_addr->family == AF_INET) {
+#if defined(CONFIG_NET_IPV4)
+		snprintf(src, sizeof(src),
+			 net_sprint_ipv4_addr(&local_addr->in_addr));
+#else
+		snprintf(src, sizeof(src), "?");
+#endif
+
+	} else {
+		snprintf(src, sizeof(src), "-");
+	}
+}
+#endif /* NET_DEBUG */
+
+int net_conn_register(enum ip_protocol proto,
+		      const struct net_addr *remote_addr,
+		      const struct net_addr *local_addr,
+		      uint16_t remote_port,
+		      uint16_t local_port,
+		      net_conn_cb_t cb,
+		      void *user_data,
+		      void **handle)
+{
+	int i;
+	uint8_t rank = 0;
+
+	for (i = 0; i < CONFIG_NET_MAX_CONN; i++) {
+		if (conns[i].flags & NET_CONN_IN_USE) {
+			continue;
+		}
+
+		if (remote_addr) {
+			if (remote_addr->family != AF_INET &&
+			    remote_addr->family != AF_INET6) {
+				NET_DBG("Remote address family not set.");
+				return -EINVAL;
+			}
+
+			conns[i].flags |= NET_CONN_REMOTE_ADDR_SET;
+
+			memcpy(&conns[i].remote_addr, remote_addr,
+			       sizeof(struct net_addr));
+
+#if defined(CONFIG_NET_IPV6)
+			if (remote_addr->family == AF_INET6) {
+				if (net_is_ipv6_addr_unspecified(
+					    &remote_addr->in6_addr)) {
+					rank |= NET_RANK_REMOTE_UNSPEC_ADDR;
+				} else {
+					rank |= NET_RANK_REMOTE_SPEC_ADDR;
+				}
+			}
+#endif
+
+#if defined(CONFIG_NET_IPV4)
+			if (remote_addr->family == AF_INET) {
+				if (!remote_addr->in_addr.s_addr[0]) {
+					rank |= NET_RANK_REMOTE_UNSPEC_ADDR;
+				} else {
+					rank |= NET_RANK_REMOTE_SPEC_ADDR;
+				}
+			}
+#endif
+		}
+
+		if (local_addr) {
+			if (local_addr->family != AF_INET &&
+			    local_addr->family != AF_INET6) {
+				NET_DBG("Local address family not set.");
+				return -EINVAL;
+			}
+
+			conns[i].flags |= NET_CONN_LOCAL_ADDR_SET;
+
+			memcpy(&conns[i].local_addr, local_addr,
+			       sizeof(struct net_addr));
+
+#if defined(CONFIG_NET_IPV6)
+			if (local_addr->family == AF_INET6) {
+				if (net_is_ipv6_addr_unspecified(
+					    &local_addr->in6_addr)) {
+					rank |= NET_RANK_LOCAL_UNSPEC_ADDR;
+				} else {
+					rank |= NET_RANK_LOCAL_SPEC_ADDR;
+				}
+			}
+#endif
+
+#if defined(CONFIG_NET_IPV4)
+			if (local_addr->family == AF_INET) {
+				if (!local_addr->in_addr.s_addr[0]) {
+					rank |= NET_RANK_LOCAL_UNSPEC_ADDR;
+				} else {
+					rank |= NET_RANK_LOCAL_SPEC_ADDR;
+				}
+			}
+#endif
+		}
+
+		if (remote_addr && local_addr) {
+			if (remote_addr->family != local_addr->family) {
+				NET_DBG("Address families different.");
+				return -EINVAL;
+			}
+		}
+
+		if (remote_port) {
+			rank |= NET_RANK_REMOTE_PORT;
+		}
+
+		if (local_port) {
+			rank |= NET_RANK_LOCAL_PORT;
+		}
+
+		conns[i].flags |= NET_CONN_IN_USE;
+		conns[i].remote_port = remote_port;
+		conns[i].local_port = local_port;
+		conns[i].cb = cb;
+		conns[i].user_data = user_data;
+		conns[i].rank = rank;
+		conns[i].proto = proto;
+
+		/* Cache needs to be cleared if new entries are added. */
+		cache_clear();
+
+#if NET_DEBUG
+		do {
+			char dst[sizeof("xxxx:xxxx:xxxx:xxxx:xxxx:xxxx")];
+			char src[sizeof("xxxx:xxxx:xxxx:xxxx:xxxx:xxxx")];
+
+			prepare_register_debug_print(dst, src,
+						     remote_addr,
+						     local_addr);
+
+			NET_DBG("[%d/%u/0x%02x] remote %p/%s/%u local %p/%s/%u "
+				"cb %p ud %p", i, proto, rank,
+				remote_addr, dst, remote_port,
+				local_addr, src, local_port,
+				cb, user_data);
+		} while (0);
+#endif /* NET_DEBUG */
+
+		if (handle) {
+			*handle = &conns[i];
+		}
+
+		return 0;
+	}
+
+	return -ENOENT;
+}
+
+static bool check_addr(struct net_buf *buf, struct net_addr *addr,
+		       bool is_remote)
+{
+	if (addr->family != net_nbuf_family(buf)) {
+		return false;
+	}
+
+#if defined(CONFIG_NET_IPV6)
+	if (net_nbuf_family(buf) == AF_INET6 && addr->family == AF_INET6) {
+		struct in6_addr *addr6;
+
+		if (is_remote) {
+			addr6 = &NET_IPV6_BUF(buf)->src;
+		} else {
+			addr6 = &NET_IPV6_BUF(buf)->dst;
+		}
+
+		if (!net_is_ipv6_addr_unspecified(&addr->in6_addr)) {
+			if (!net_ipv6_addr_cmp(&addr->in6_addr, addr6)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+#endif /* CONFIG_NET_IPV6 */
+
+#if defined(CONFIG_NET_IPV4)
+	if (net_nbuf_family(buf) == AF_INET && addr->family == AF_INET) {
+		struct in_addr *addr4;
+
+		if (is_remote) {
+			addr4 = &NET_IPV4_BUF(buf)->src;
+		} else {
+			addr4 = &NET_IPV4_BUF(buf)->dst;
+		}
+
+		if (addr->in_addr.s_addr[0]) {
+			if (!net_ipv4_addr_cmp(&addr->in_addr, addr4)) {
+				return false;
+			}
+		}
+	}
+#endif /* CONFIG_NET_IPV4 */
+
+	return true;
+}
+
+static inline void send_icmp_error(struct net_buf *buf)
+{
+	if (net_nbuf_family(buf) == AF_INET6) {
+#if defined(CONFIG_NET_IPV6)
+		net_icmpv6_send_error(buf, NET_ICMPV6_DST_UNREACH,
+				      NET_ICMPV6_DST_UNREACH_NO_PORT);
+#endif /* CONFIG_NET_IPV6 */
+
+	} else {
+
+#if defined(CONFIG_NET_IPV4)
+		net_icmpv4_send_error(buf, NET_ICMPV4_DST_UNREACH,
+				      NET_ICMPV4_DST_UNREACH_NO_PORT);
+#endif /* CONFIG_NET_IPV4 */
+	}
+}
+
+enum net_verdict net_conn_input(enum ip_protocol proto, struct net_buf *buf)
+{
+	int i, best_match = -1;
+	int16_t best_rank = -1;
+
+#if defined(CONFIG_NET_CONN_CACHE)
+	enum net_verdict verdict;
+	uint32_t cache_value = 0;
+	int32_t pos;
+
+	verdict = cache_check(proto, buf, &cache_value, &pos);
+	if (verdict != NET_CONTINUE) {
+		return verdict;
+	}
+#endif
+
+	NET_DBG("Check %s listener for buf %p src port %u dst port %u "
+		"family %d", proto2str(proto), buf,
+		ntohs(NET_CONN_BUF(buf)->src_port),
+		ntohs(NET_CONN_BUF(buf)->dst_port),
+		net_nbuf_family(buf));
+
+	for (i = 0; i < CONFIG_NET_MAX_CONN; i++) {
+		if (!(conns[i].flags & NET_CONN_IN_USE)) {
+			continue;
+		}
+
+		if (conns[i].proto != proto) {
+			continue;
+		}
+
+		if (conns[i].remote_port) {
+			if (conns[i].remote_port !=
+			    ntohs(NET_CONN_BUF(buf)->src_port)) {
+				continue;
+			}
+		}
+
+		if (conns[i].local_port) {
+			if (conns[i].local_port !=
+			    ntohs(NET_CONN_BUF(buf)->dst_port)) {
+				continue;
+			}
+		}
+
+		if (conns[i].flags & NET_CONN_REMOTE_ADDR_SET) {
+			if (!check_addr(buf, &conns[i].remote_addr, true)) {
+				continue;
+			}
+		}
+
+		if (conns[i].flags & NET_CONN_LOCAL_ADDR_SET) {
+			if (!check_addr(buf, &conns[i].local_addr, false)) {
+				continue;
+			}
+		}
+
+		if (best_rank < conns[i].rank) {
+			best_rank = conns[i].rank;
+			best_match = i;
+		}
+	}
+
+	if (best_match >= 0) {
+#if defined(CONFIG_NET_CONN_CACHE)
+		NET_DBG("[%d] match found cb %p ud %p rank 0x%02x cache 0x%x",
+			best_match,
+			conns[best_match].cb,
+			conns[best_match].user_data,
+			conns[best_match].rank,
+			pos < 0 ? 0 : conn_cache[pos].value);
+
+		if (pos >= 0) {
+			conn_cache[pos].idx = best_match;
+		}
+#else
+		NET_DBG("[%d] match found cb %p ud %p rank 0x%02x",
+			best_match,
+			conns[best_match].cb,
+			conns[best_match].user_data,
+			conns[best_match].rank);
+#endif /* CONFIG_NET_CONN_CACHE */
+
+		if (conns[best_match].cb(buf,
+			     conns[best_match].user_data) == NET_DROP) {
+			goto drop;
+		}
+
+#if defined(CONFIG_NET_UDP)
+		if (proto == IPPROTO_UDP) {
+			NET_STATS(++net_stats.udp.recv);
+		}
+#endif /* CONFIG_NET_UDP */
+
+		return NET_OK;
+	}
+
+	NET_DBG("No match found.");
+
+	cache_add_neg(cache_value);
+
+	send_icmp_error(buf);
+
+drop:
+#if defined(CONFIG_NET_UDP)
+	if (proto == IPPROTO_UDP) {
+		NET_STATS(++net_stats.udp.drop);
+	}
+#endif /* CONFIG_NET_UDP */
+
+	return NET_DROP;
+}
+
+void net_conn_init(void)
+{
+#if defined(CONFIG_NET_CONN_CACHE)
+	do {
+		int i;
+
+		for (i = 0; i < CONFIG_NET_MAX_CONN; i++) {
+			conn_cache[i].idx = -1;
+		}
+	} while (0);
+#endif /* CONFIG_NET_CONN_CACHE */
+}
---
+++ net/yaip/connection.h
@@ -0,0 +1,149 @@
+/** @file
+ @brief Generic connection handling.
+
+ This is not to be included by the application.
+ */
+
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __CONNECTION_H
+#define __CONNECTION_H
+
+#include <stdint.h>
+
+#include <misc/util.h>
+
+#include <net/net_core.h>
+#include <net/net_ip.h>
+#include <net/nbuf.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Function that is called by connection subsystem when UDP/TCP
+ * packet is received and which matches local and remote IP address
+ * and port.
+ */
+typedef enum net_verdict (*net_conn_cb_t)(struct net_buf *buf,
+					  void *user_data);
+
+/**
+ * @brief Information about a connection in the system.
+ *
+ * Stores the connection information.
+ *
+ */
+struct net_conn {
+	/** Remote IP address */
+	struct net_addr remote_addr;
+
+	/** Local IP address */
+	struct net_addr local_addr;
+
+	/** Remote port */
+	uint16_t remote_port;
+
+	/** Local port */
+	uint16_t local_port;
+
+	/** Callback to be called when matching UDP packet is received */
+	net_conn_cb_t cb;
+
+	/** Possible user to pass to the callback */
+	void *user_data;
+
+	/** Connection protocol */
+	uint8_t proto;
+
+	/** Flags for the connection */
+	uint8_t flags;
+
+	/** Rank of this connection. Higher rank means more specific
+	 * connection.
+	 * Value is constructed like this:
+	 *   bit 0  local port, bit set if specific value
+	 *   bit 1  remote port, bit set if specific value
+	 *   bit 2  local address, bit set if unspecified address
+	 *   bit 3  remote address, bit set if unspecified address
+	 *   bit 4  local address, bit set if specific address
+	 *   bit 5  remote address, bit set if specific address
+	 */
+	uint8_t rank;
+};
+
+/**
+ * @brief Register a callback to be called when UDP/TCP packet
+ * is received corresponding to received packet.
+ *
+ * @param proto Protocol for the connection (UDP or TCP)
+ * @param remote_addr Remote address of the connection end point.
+ * @param local_addr Local address of the connection end point.
+ * @param remote_port Remote port of the connection end point.
+ * @param local_port Local port of the connection end point.
+ * @param cb Callback to be called
+ * @param user_data User data supplied by caller.
+ * @param handle UDP handle that can be used when unregistering
+ *
+ * @return Return 0 if the registration succeed, <0 otherwise.
+ */
+int net_conn_register(enum ip_protocol proto,
+		      const struct net_addr *remote_addr,
+		      const struct net_addr *local_addr,
+		      uint16_t remote_port,
+		      uint16_t local_port,
+		      net_conn_cb_t cb,
+		      void *user_data,
+		      void **handle);
+
+/**
+ * @brief Unregister connection handler.
+ *
+ * @param handle Handle from registering.
+ *
+ * @return Return 0 if the unregistration succeed, <0 otherwise.
+ */
+int net_conn_unregister(void *handle);
+
+/**
+ * @brief Called by net_core.c when a network packet is received.
+ *
+ * @param buf Network buffer holding received data
+ *
+ * @return NET_OK if the packet was consumed, NET_DROP if
+ * the packet parsing failed and the caller should handle
+ * the received packet. If corresponding IP protocol support is
+ * disabled, the function will always return NET_DROP.
+ */
+#if defined(CONFIG_NET_UDP) || defined(CONFIG_NET_TCP)
+enum net_verdict net_conn_input(enum ip_protocol proto, struct net_buf *buf);
+#else
+static inline enum net_verdict net_conn_input(enum ip_protocol proto,
+					      struct net_buf *buf)
+{
+	return NET_DROP;
+}
+#endif /* CONFIG_NET_UDP || CONFIG_NET_TCP */
+
+void net_conn_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __CONNECTION_H */
