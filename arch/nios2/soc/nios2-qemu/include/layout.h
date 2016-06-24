543aa77392c6f4b7ed32cfcbabbb73d370cc862b
Andrew Boie <andrew.p.boie@intel.com>
2016-06-24 11:48:26 -0700

    nios2-qemu: fix CONFIG_XIP
    It's all RAM, but we pretend the range 0x410000 - 0x420000
    is the "ROM" region, and stuff gets copied into RAM starting
    at 0x400000.
    
    Change-Id: Idf6bd603e2552593f588cf6130ee4da946bcf5a3
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- arch/nios2/soc/nios2-qemu/include/layout.h
+++ arch/nios2/soc/nios2-qemu/include/layout.h
@@ -16,11 +16,15 @@
 
 #include <system.h>
 
-#define _RESET_VECTOR		ONCHIP_MEMORY2_0_BASE
+/* To simulate XIP on QEMU, we split RAM into two chunks, with the
+ * higher-addressed chunk considered "ROM"
+ */
+
+#define _RESET_VECTOR		ONCHIP_MEMORY2_0_BASE + 0x10000
 #define	_EXC_VECTOR		ALT_CPU_EXCEPTION_ADDR
 
-#define _ROM_ADDR		ONCHIP_FLASH_0_DATA_BASE
-#define _ROM_SIZE		ONCHIP_FLASH_0_DATA_SPAN
+#define _ROM_ADDR		ONCHIP_MEMORY2_0_BASE + 0x10000
+#define _ROM_SIZE		0x10000
 
 #define _RAM_ADDR		ONCHIP_MEMORY2_0_BASE
-#define _RAM_SIZE		ONCHIP_MEMORY2_0_SPAN
+#define _RAM_SIZE		0x10000
