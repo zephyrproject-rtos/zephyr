84edd8f2f1dd96decce14a377598e9c30cbe276a
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-28 15:04:30 +0300

    net: apps: Fix echo-server to use the new user API
    Echo-server no longer reverses the data as doing it
    when we have a fragment chain is quite challenging.
    
    Change-Id: I298669f3ddba67000672320e7b3b14f72e9f12d4
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- samples/net/echo_server/src/echo-server-yaip.c
+++ samples/net/echo_server/src/echo-server-yaip.c
@@ -1,7 +1,7 @@
 /* echo.c - Networking echo server */
 
 /*
- * Copyright (c) 2015 Intel Corporation.
+ * Copyright (c) 2016 Intel Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,18 +16,18 @@
  * limitations under the License.
  */
 
-#if defined(CONFIG_STDOUT_CONSOLE)
-#include <stdio.h>
-#define PRINT           printf
-#else
-#include <misc/printk.h>
-#define PRINT           printk
+#if 1
+#define SYS_LOG_DOMAIN "echo-server"
+#define SYS_LOG_LEVEL SYS_LOG_LEVEL_DEBUG
+#define NET_DEBUG 1
 #endif
 
 #include <zephyr.h>
 #include <sections.h>
 #include <errno.h>
+#include <stdio.h>
 
+#include <net/nbuf.h>
 #include <net/net_if.h>
 #include <net/net_core.h>
 #include <net/net_context.h>
@@ -39,13 +39,14 @@
 
 #if defined(CONFIG_NET_TESTING)
 #include <net_testing.h>
-#else
 #endif /* CONFIG_NET_TESTING */
 
 #if defined(CONFIG_NET_IPV6)
 /* admin-local, dynamically allocated multicast address */
 #define MCAST_IP6ADDR { { { 0xff, 0x84, 0, 0, 0, 0, 0, 0, \
 			    0, 0, 0, 0, 0, 0, 0, 0x2 } } }
+
+struct in6_addr in6addr_mcast = MCAST_IP6ADDR;
 
 /* Define my IP address where to expect messages */
 #if !defined(CONFIG_NET_TESTING)
@@ -55,11 +56,10 @@
 #endif
 
 #if defined(CONFIG_NET_TESTING)
-static const struct in6_addr in6addr_my = IN6ADDR_ANY_INIT;
+static struct in6_addr in6addr_my = IN6ADDR_ANY_INIT;
 #else
-static const struct in6_addr in6addr_my = MY_IP6ADDR;
+static struct in6_addr in6addr_my = MY_IP6ADDR;
 #endif
-
 #endif /* IPv6 */
 
 #if defined(CONFIG_NET_IPV4)
@@ -68,144 +68,179 @@ static const struct in6_addr in6addr_my = MY_IP6ADDR;
 
 /* The 192.0.2.0/24 is the private address space for documentation RFC 5737 */
 #define MY_IP4ADDR { { { 192, 0, 2, 1 } } }
+
+static struct in_addr in4addr_my = MY_IP4ADDR;
 #endif /* IPv4 */
 
 #define MY_PORT 4242
 
-struct in6_addr addr6 = { { { 0x20, 0x01, 0x0d, 0xb8, 0, 0, 0, 0,
-			      0, 0, 0, 0, 0, 0, 0, 0x1 } } };
-static struct in_addr in4addr_my = MY_IP4ADDR;
+#if defined(CONFIG_NANOKERNEL)
+#define STACKSIZE 2000
+char __noinit __stack fiberStack[STACKSIZE];
+#endif
+
+/* How many tics to wait for a network packet */
+#if 1
+#define WAIT_TICKS (sys_clock_ticks_per_sec)
+#else
+#define WAIT_TICKS TICKS_UNLIMITED
+#endif
+
+static struct nano_sem quit_lock;
+
+static inline void quit(void)
+{
+	nano_sem_give(&quit_lock);
+}
 
 static inline void init_app(void)
 {
-	PRINT("%s: run echo server\n", __func__);
+	NET_INFO("%s: run echo server", __func__);
+
+	nano_sem_init(&quit_lock);
 
 #if defined(CONFIG_NET_TESTING)
 	net_testing_setup();
 #endif
 
 #if defined(CONFIG_NET_IPV6)
-	net_if_ipv6_addr_add(net_if_get_default(), &addr6, NET_ADDR_MANUAL, 0);
+	net_if_ipv6_addr_add(net_if_get_default(), &in6addr_my,
+			     NET_ADDR_MANUAL, 0);
+
+	net_if_ipv6_maddr_add(net_if_get_default(), &in6addr_mcast);
 #endif
 
 #if defined(CONFIG_NET_IPV4)
-	net_if_ipv4_addr_add(net_if_get_default(), &in4addr_my, NET_ADDR_MANUAL, 0);
+	net_if_ipv4_addr_add(net_if_get_default(), &in4addr_my,
+			     NET_ADDR_MANUAL, 0);
 #endif
 }
-
-
-/* How many tics to wait for a network packet */
-#if 1
-#define WAIT_TIME 0
-#define WAIT_TICKS (WAIT_TIME * sys_clock_ticks_per_sec / 10)
-#else
-#define WAIT_TICKS TICKS_UNLIMITED
-#endif
 
 static inline bool get_context(struct net_context **udp_recv4,
 			       struct net_context **udp_recv6,
 			       struct net_context **tcp_recv4,
 			       struct net_context **tcp_recv6,
-			       struct net_context **mcast_recv4,
 			       struct net_context **mcast_recv6)
 {
+	int ret;
+
 #if defined(CONFIG_NET_IPV6)
-	static struct net_addr mcast_addr6;
-	static struct net_addr any_addr6;
-	static struct net_addr my_addr6;
-	static const struct in6_addr in6addr_any = IN6ADDR_ANY_INIT;
-	static const struct in6_addr in6addr_mcast = MCAST_IP6ADDR;
+	struct sockaddr_in6 mcast_addr6 = { 0 };
+	struct sockaddr_in6 my_addr6 = { 0 };
 #endif
 
 #if defined(CONFIG_NET_IPV4)
-	static struct net_addr mcast_addr4;
-	static struct net_addr any_addr4;
-	static struct net_addr my_addr4;
-	static const struct in_addr in4addr_any = { { { 0 } } };
-	static struct in_addr in4addr_mcast = MCAST_IP4ADDR;
+	struct sockaddr_in my_addr4 = { 0 };
 #endif
 
 #if defined(CONFIG_NET_IPV6)
-	mcast_addr6.in6_addr = in6addr_mcast;
-	mcast_addr6.family = AF_INET6;
+	net_ipaddr_copy(&mcast_addr6.sin6_addr, &in6addr_mcast);
+	mcast_addr6.sin6_family = AF_INET6;
 
-	any_addr6.in6_addr = in6addr_any;
-	any_addr6.family = AF_INET6;
-
-	my_addr6.in6_addr = in6addr_my;
-	my_addr6.family = AF_INET6;
+	net_ipaddr_copy(&my_addr6.sin6_addr, &in6addr_my);
+	my_addr6.sin6_family = AF_INET6;
+	my_addr6.sin6_port = htons(MY_PORT);
 #endif
 
 #if defined(CONFIG_NET_IPV4)
-	mcast_addr4.in_addr = in4addr_mcast;
-	mcast_addr4.family = AF_INET;
-
-	any_addr4.in_addr = in4addr_any;
-	any_addr4.family = AF_INET;
-
-	my_addr4.in_addr = in4addr_my;
-	my_addr4.family = AF_INET;
+	net_ipaddr_copy(&my_addr4.sin_addr, &in4addr_my);
+	my_addr4.sin_family = AF_INET;
+	my_addr4.sin_port = htons(MY_PORT);
 #endif
 
 #if defined(CONFIG_NET_IPV6) && defined(CONFIG_NET_UDP)
-	*udp_recv6 = net_context_get(IPPROTO_UDP,
-				     &any_addr6, 0,
-				     &my_addr6, MY_PORT);
-	if (!*udp_recv6) {
-		PRINT("%s: Cannot get network context\n", __func__);
-		return NULL;
-	}
-
-	*mcast_recv6 = net_context_get(IPPROTO_UDP,
-				       &any_addr6, 0,
-				       &mcast_addr6, MY_PORT);
-	if (!*mcast_recv6) {
-		PRINT("%s: Cannot get receiving mcast network context\n",
-		      __func__);
+	ret = net_context_get(AF_INET6, SOCK_DGRAM, IPPROTO_UDP, udp_recv6);
+	if (ret < 0) {
+		NET_ERR("%s: Cannot get network context for IPv6 UDP (%d)",
+		      __func__, ret);
 		return false;
 	}
 
+	ret = net_context_bind(*udp_recv6, (struct sockaddr *)&my_addr6);
+	if (ret < 0) {
+		NET_ERR("%: Cannot bind IPv6 UDP port %d (%d)",
+		      ntohs(my_addr6.sin6_port), ret);
+		return false;
+	}
+
+	ret = net_context_get(AF_INET6, SOCK_DGRAM, IPPROTO_UDP, mcast_recv6);
+	if (ret < 0) {
+		NET_ERR("%s: Cannot get receiving IPv6 mcast "
+		      "network context (%d)", __func__, ret);
+		return false;
+	}
+
+	ret = net_context_bind(*mcast_recv6, (struct sockaddr *)&mcast_addr6);
+	if (ret < 0) {
+		NET_ERR("%: Cannot bind IPv6 mcast (%d)", ret);
+		return false;
+	}
 #endif
 
 #if defined(CONFIG_NET_IPV4) && defined(CONFIG_NET_UDP)
-	*udp_recv4 = net_context_get(IPPROTO_UDP,
-				    &any_addr4, 0,
-				    &my_addr4, MY_PORT);
-	if (!*udp_recv4) {
-		PRINT("%s: Cannot get network context\n", __func__);
-		return NULL;
+	ret = net_context_get(AF_INET, SOCK_DGRAM, IPPROTO_UDP, udp_recv4);
+	if (ret < 0) {
+		NET_ERR("%s: Cannot get network context for IPv4 UDP (%d)",
+		      __func__, ret);
+		return false;
 	}
 
-	*mcast_recv4 = net_context_get(IPPROTO_UDP,
-				       &any_addr4, 0,
-				       &mcast_addr4, MY_PORT);
-	if (!*mcast_recv4) {
-		PRINT("%s: Cannot get receiving mcast network context\n",
-		      __func__);
+	ret = net_context_bind(*udp_recv4, (struct sockaddr *)&my_addr4);
+	if (ret < 0) {
+		NET_ERR("%: Cannot bind IPv4 UDP port %d (%d)",
+		      ntohs(my_addr4.sin_port), ret);
 		return false;
 	}
 #endif
 
 #if defined(CONFIG_NET_IPV6) && defined(CONFIG_NET_TCP)
 	if (tcp_recv6) {
-		*tcp_recv6 = net_context_get(IPPROTO_TCP,
-					     &any_addr6, 0,
-					     &my_addr6, MY_PORT);
-		if (!*tcp_recv6) {
-			PRINT("%s: Cannot get network context\n", __func__);
-			return NULL;
+		ret = net_context_get(AF_INET6, SOCK_STREAM, IPPROTO_TCP,
+				      tcp_recv6);
+		if (ret < 0) {
+			NET_ERR("%s: Cannot get network context "
+			      "for IPv6 TCP (%d)", __func__, ret);
+			return false;
+		}
+
+		ret = net_context_bind(*tcp_recv6,
+				       (struct sockaddr *)&my_addr6);
+		if (ret < 0) {
+			NET_ERR("%: Cannot bind IPv6 TCP port %d (%d)",
+			      ntohs(my_addr6.sin_port), ret);
+			return false;
+		}
+
+		ret = net_context_listen(*tcp_recv6, 0);
+		if (ret < 0) {
+			NET_ERR("%s: Cannot listen IPv6 TCP (%d)", ret);
+			return false;
 		}
 	}
 #endif
 
 #if defined(CONFIG_NET_IPV4) && defined(CONFIG_NET_TCP)
 	if (tcp_recv4) {
-		*tcp_recv4 = net_context_get(IPPROTO_TCP,
-					     &any_addr4, 0,
-					     &my_addr4, MY_PORT);
-		if (!*tcp_recv4) {
-			PRINT("%s: Cannot get network context\n", __func__);
-			return NULL;
+		ret = net_context_get(AF_INET, SOCK_STREAM, IPPROTO_TCP,
+				      tcp_recv4);
+		if (ret < 0) {
+			NET_ERR("%s: Cannot get network context for IPv4 TCP",
+			      __func__);
+			return false;
+		}
+
+		ret = net_context_bind(*tcp_recv4,
+				       (struct sockaddr *)&my_addr4);
+		if (ret < 0) {
+			NET_ERR("%: Cannot bind IPv4 TCP port %d",
+			      ntohs(my_addr4.sin_port));
+			return false;
+		}
+
+		ret = net_context_listen(*tcp_recv6, 0);
+		if (ret < 0) {
+			NET_ERR("%s: Cannot listen IPv6 TCP");
+			return false;
 		}
 	}
 #endif
@@ -213,28 +248,189 @@ static inline bool get_context(struct net_context **udp_recv4,
 	return true;
 }
 
-#if defined(CONFIG_NANOKERNEL)
-#define STACKSIZE 2000
-char __noinit __stack fiberStack[STACKSIZE];
-#endif
+static struct net_buf *udp_recv(const char *name,
+				struct net_context *context,
+				struct net_buf *buf)
+{
+	struct net_buf *reply_buf, *frag, *tmp;
+	int header_len, recv_len, reply_len;
+
+	NET_INFO("%s received %d bytes", name,
+	      net_nbuf_appdatalen(buf));
+
+	reply_buf = net_nbuf_get_tx(context);
+
+	NET_ASSERT(reply_buf);
+
+	recv_len = net_buf_frags_len(buf->frags);
+
+	tmp = buf->frags;
+
+	/* First fragment will contain IP header so move the data
+	 * down in order to get rid of it.
+	 */
+	header_len = net_nbuf_appdata(buf) - tmp->data;
+
+	NET_ASSERT(header_len < CONFIG_NET_NBUF_DATA_SIZE);
+
+	net_buf_pull(tmp, header_len);
+
+	while (tmp) {
+		frag = net_nbuf_get_data(context);
+
+		memcpy(net_buf_add(frag, tmp->len), tmp->data, tmp->len);
+
+		net_buf_frag_add(reply_buf, frag);
+
+		net_buf_frag_del(buf, tmp);
+
+		net_nbuf_unref(tmp);
+
+		tmp = buf->frags;
+	}
+
+	reply_len = net_buf_frags_len(reply_buf->frags);
+
+	NET_ASSERT_INFO(recv_len != reply_len,
+			"Received %d bytes, sending %d bytes",
+			recv_len, reply_len);
+
+	return reply_buf;
+}
+
+#if defined(CONFIG_NET_UDP)
+#define MAX_DBG_PRINT 64
+
+static inline void udp_sent(struct net_context *context,
+			    int status,
+			    void *token,
+			    void *user_data)
+{
+	if (!status) {
+		NET_INFO("Sent %d bytes", POINTER_TO_UINT(token));
+	}
+}
+
+static inline void set_dst_addr(sa_family_t family,
+				struct net_buf *buf,
+				struct sockaddr *dst_addr)
+{
+#if defined(CONFIG_NET_IPV6)
+	if (family == AF_INET6) {
+		net_ipaddr_copy(&net_sin6(dst_addr)->sin6_addr,
+				&NET_IPV6_BUF(buf)->src);
+		net_sin6(dst_addr)->sin6_family = AF_INET6;
+		net_sin6(dst_addr)->sin6_port = NET_UDP_BUF(buf)->src_port;
+	}
+#endif /* CONFIG_NET_IPV6) */
+
+#if defined(CONFIG_NET_IPV4)
+	if (family == AF_INET) {
+		net_ipaddr_copy(&net_sin(dst_addr)->sin_addr,
+				&NET_IPV4_BUF(buf)->src);
+		net_sin(dst_addr)->sin_family = AF_INET;
+		net_sin(dst_addr)->sin_port = NET_UDP_BUF(buf)->src_port;
+	}
+#endif /* CONFIG_NET_IPV6) */
+}
+
+static void udp_received(struct net_context *context,
+			 struct net_buf *buf,
+			 int status,
+			 void *user_data)
+{
+	struct net_buf *reply_buf;
+	struct sockaddr dst_addr;
+	sa_family_t family = net_nbuf_family(buf);
+	static char dbg[MAX_DBG_PRINT + 1];
+	int ret;
+
+	snprintf(dbg, MAX_DBG_PRINT, "UDP IPv%c",
+		 family == AF_INET6 ? '6' : '4');
+
+	set_dst_addr(family, buf, &dst_addr);
+
+	reply_buf = udp_recv(dbg, context, buf);
+
+	net_nbuf_unref(buf);
+
+	ret = net_context_sendto(reply_buf, &dst_addr, udp_sent, 0,
+				 UINT_TO_POINTER(net_buf_frags_len(reply_buf)),
+				 user_data);
+	if (ret < 0) {
+		NET_ERR("Cannot send data to peer (%d)", ret);
+		net_nbuf_unref(reply_buf);
+
+		quit();
+	}
+}
+
+static void setup_udp_recv(struct net_context *udp_recv4,
+			   struct net_context *udp_recv6)
+{
+	int ret;
+
+#if defined(CONFIG_NET_IPV6)
+	ret = net_context_recv(udp_recv6, udp_received, 0, NULL);
+	if (ret < 0) {
+		NET_ERR("%s: Cannot receive IPv6 UDP packets");
+	}
+#endif /* CONFIG_NET_IPV6 */
+
+#if defined(CONFIG_NET_IPV4)
+	ret = net_context_recv(udp_recv4, udp_received, 0, NULL);
+	if (ret < 0) {
+		NET_ERR("%s: Cannot receive IPv4 UDP packets");
+	}
+#endif /* CONFIG_NET_IPV4 */
+}
+#endif /* CONFIG_NET_UDP */
 
 void receive(void)
 {
-	static struct net_context *udp_recv4, *udp_recv6,
-		*tcp_recv4, *tcp_recv6;
-	static struct net_context *mcast_recv4, *mcast_recv6;
+	struct net_context *udp_recv4 = { 0 };
+	struct net_context *udp_recv6 = { 0 };
+	struct net_context *tcp_recv4 = { 0 };
+	struct net_context *tcp_recv6 = { 0 };
+	struct net_context *mcast_recv6 = { 0 };
 
 	if (!get_context(&udp_recv4, &udp_recv6,
 			 &tcp_recv4, &tcp_recv6,
-			 &mcast_recv4, &mcast_recv6)) {
-		PRINT("%s: Cannot get network contexts\n", __func__);
+			 &mcast_recv6)) {
+		NET_ERR("%s: Cannot get network contexts", __func__);
 		return;
 	}
 
-	while (1) {
-		/* FIXME - Implementation is missing */
-		fiber_sleep(100);
-	}
+	NET_INFO("Starting to wait");
+
+#if defined(CONFIG_NET_TCP)
+	setup_tcp_accept(tcp_recv4, tcp_recv6);
+#endif
+
+#if defined(CONFIG_NET_UDP)
+	setup_udp_recv(udp_recv4, udp_recv6);
+#endif
+
+	nano_sem_take(&quit_lock, TICKS_UNLIMITED);
+
+	NET_INFO("Stopping...");
+
+#if defined(CONFIG_NET_IPV6) && defined(CONFIG_NET_UDP)
+	net_context_put(udp_recv6);
+	net_context_put(mcast_recv6);
+#endif
+
+#if defined(CONFIG_NET_IPV4) && defined(CONFIG_NET_UDP)
+	net_context_put(udp_recv4);
+#endif
+
+#if defined(CONFIG_NET_IPV6) && defined(CONFIG_NET_TCP)
+	net_context_put(tcp_recv6);
+#endif
+
+#if defined(CONFIG_NET_IPV4) && defined(CONFIG_NET_TCP)
+	net_context_put(tcp_recv4);
+#endif
 }
 
 void main(void)
@@ -243,7 +439,7 @@ void main(void)
 
 #if defined(CONFIG_NETWORKING_WITH_BT)
 	if (bt_enable(NULL)) {
-		PRINT("Bluetooth init failed\n");
+		NET_ERR("Bluetooth init failed");
 		return;
 	}
 	ipss_init();
