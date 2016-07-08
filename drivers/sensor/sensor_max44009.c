a3488d13e06842ff350f50df0d4f69eda5231c24
Baohong Liu <baohong.liu@intel.com>
2016-07-07 18:33:29 -0700

    sensor: add driver for MAX44009 light sensor
    Add device driver for MAX44009 light sensor
    
    Datasheet:
    https://datasheets.maximintegrated.com/en/ds/MAX44009.pdf
    
    Jira: ZEP-357
    
    Origin: Original
    Change-Id: I6b5c8e0afc36c22de0a6432d01cd5b7eb2e12f1b
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>

---
+++ drivers/sensor/sensor_max44009.c
@@ -0,0 +1,200 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <device.h>
+#include <i2c.h>
+#include <sensor.h>
+#include <misc/__assert.h>
+
+#include "sensor_max44009.h"
+
+static int max44009_reg_read(struct max44009_data *drv_data, uint8_t reg,
+			     uint8_t *val, bool send_stop)
+{
+	struct i2c_msg msgs[2] = {
+		{
+			.buf = &reg,
+			.len = 1,
+			.flags = I2C_MSG_WRITE,
+		},
+		{
+			.buf = val,
+			.len = 1,
+			.flags = I2C_MSG_READ,
+		},
+	};
+
+	if (send_stop) {
+		msgs[1].flags |= I2C_MSG_STOP;
+	}
+
+	if (i2c_transfer(drv_data->i2c, msgs, 2, MAX44009_I2C_ADDRESS) != 0) {
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static int max44009_reg_write(struct max44009_data *drv_data, uint8_t reg,
+			      uint8_t val)
+{
+	uint8_t tx_buf[2] = {reg, val};
+
+	return i2c_write(drv_data->i2c, tx_buf, sizeof(tx_buf),
+			 MAX44009_I2C_ADDRESS);
+}
+
+static int max44009_reg_update(struct max44009_data *drv_data, uint8_t reg,
+			       uint8_t mask, uint8_t val)
+{
+	uint8_t old_val = 0;
+	uint8_t new_val = 0;
+
+	if (max44009_reg_read(drv_data, reg, &old_val, true) != 0) {
+		return -EIO;
+	}
+
+	new_val = old_val & ~mask;
+	new_val |= val & mask;
+
+	return max44009_reg_write(drv_data, reg, new_val);
+}
+
+static int max44009_attr_set(struct device *dev, enum sensor_channel chan,
+			     enum sensor_attribute attr,
+			     const struct sensor_value *val)
+{
+	struct max44009_data *drv_data = dev->driver_data;
+	uint8_t value;
+	uint32_t cr;
+
+	if (chan != SENSOR_CHAN_LIGHT) {
+		return -ENOTSUP;
+	}
+
+	switch (attr) {
+	case SENSOR_ATTR_SAMPLING_FREQUENCY:
+		if (val->type != SENSOR_VALUE_TYPE_INT_PLUS_MICRO) {
+			return -ENOTSUP;
+		}
+
+		/* convert rate to mHz */
+		cr = val->val1 * 1000 + val->val2 / 1000;
+
+		/* the sensor supports 1.25Hz or continuous conversion */
+		switch (cr) {
+		case 1250:
+			value = 0;
+			break;
+		default:
+			value = MAX44009_CONTINUOUS_SAMPLING;
+		}
+
+		if (max44009_reg_update(drv_data, MAX44009_REG_CONFIG,
+					MAX44009_SAMPLING_CONTROL_BIT,
+					value) != 0) {
+			SYS_LOG_DBG("Failed to set attribute!");
+			return -EIO;
+		}
+
+		return 0;
+
+	default:
+		return -ENOTSUP;
+	}
+
+	return 0;
+}
+
+static int max44009_sample_fetch(struct device *dev, enum sensor_channel chan)
+{
+	struct max44009_data *drv_data = dev->driver_data;
+	uint8_t val_h, val_l;
+
+	__ASSERT(chan == SENSOR_CHAN_ALL || chan == SENSOR_CHAN_LIGHT);
+
+	drv_data->sample = 0;
+
+	if (max44009_reg_read(drv_data, MAX44009_REG_LUX_HIGH_BYTE, &val_h,
+			      false) != 0) {
+		return -EIO;
+	}
+
+	if (max44009_reg_read(drv_data, MAX44009_REG_LUX_LOW_BYTE, &val_l,
+			      true) != 0) {
+		return -EIO;
+	}
+
+	drv_data->sample = ((uint16_t)val_h) << 8;
+	drv_data->sample += val_l;
+
+	return 0;
+}
+
+static int max44009_channel_get(struct device *dev, enum sensor_channel chan,
+				struct sensor_value *val)
+{
+	struct max44009_data *drv_data = dev->driver_data;
+	uint32_t uval;
+
+	if (chan != SENSOR_CHAN_LIGHT) {
+		return -ENOTSUP;
+	}
+
+	/**
+	 * sample consists of 4 bits of exponent and 8 bits of mantissa
+	 * bits 15 to 12 are exponent bits
+	 * bits 11 to 8 and 3 to 0 are the mantissa bits
+	 */
+	uval = drv_data->sample;
+	uval = (uval & MAX44009_MANTISSA_LOW_NIBBLE_MASK) +
+	       ((uval & MAX44009_MANTISSA_HIGH_NIBBLE_MASK) >> 4);
+	uval = uval << (drv_data->sample >> MAX44009_SAMPLE_EXPONENT_SHIFT);
+
+	val->type = SENSOR_VALUE_TYPE_DOUBLE;
+	/* lux is the integer of sample output multiplied by 0.045. */
+	val->dval = ((double)uval) * 0.045;
+
+	return 0;
+}
+
+static struct sensor_driver_api max44009_driver_api = {
+	.attr_set = max44009_attr_set,
+	.sample_fetch = max44009_sample_fetch,
+	.channel_get = max44009_channel_get,
+};
+
+int max44009_init(struct device *dev)
+{
+	struct max44009_data *drv_data = dev->driver_data;
+
+	drv_data->i2c = device_get_binding(CONFIG_MAX44009_I2C_DEV_NAME);
+	if (drv_data->i2c == NULL) {
+		SYS_LOG_DBG("Failed to get pointer to %s device!",
+			    CONFIG_MAX44009_I2C_MASTER_DEV_NAME);
+		return -EINVAL;
+	}
+
+	dev->driver_api = &max44009_driver_api;
+
+	return 0;
+}
+
+static struct max44009_data max44009_drv_data;
+
+DEVICE_INIT(max44009, CONFIG_MAX44009_DRV_NAME, max44009_init,
+	    &max44009_drv_data, NULL, SECONDARY,
+	    CONFIG_MAX44009_INIT_PRIORITY);
---
+++ drivers/sensor/sensor_max44009.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _SENSOR_MAX44009
+#define _SENSOR_MAX44009
+
+#include <misc/util.h>
+
+#define SYS_LOG_DOMAIN "MAX44009"
+#define SYS_LOG_LEVEL CONFIG_MAX44009_SYS_LOG_LEVEL
+#include <misc/sys_log.h>
+
+#if CONFIG_MAX44009_I2C_ADDR_0
+	#define MAX44009_I2C_ADDRESS	0x4a
+#elif CONFIG_MAX44009_I2C_ADDR_1
+	#define MAX44009_I2C_ADDRESS	0x4b
+#endif
+
+#define MAX44009_SAMPLING_CONTROL_BIT	BIT(7)
+#define MAX44009_CONTINUOUS_SAMPLING	BIT(7)
+#define MAX44009_SAMPLE_EXPONENT_SHIFT	12
+#define MAX44009_MANTISSA_HIGH_NIBBLE_MASK	0xf00
+#define MAX44009_MANTISSA_LOW_NIBBLE_MASK	0xf
+
+#define MAX44009_REG_CONFIG		0x02
+#define MAX44009_REG_LUX_HIGH_BYTE	0x03
+#define MAX44009_REG_LUX_LOW_BYTE	0x04
+
+struct max44009_data {
+	struct device *i2c;
+	uint16_t sample;
+};
+
+#endif /* _SENSOR_MAX44009_ */
