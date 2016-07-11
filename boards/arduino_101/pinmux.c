03c41c80a9966bb93dd2bec9dc10c58fd95b1ce6
Flavio Santes <flavio.santes@intel.com>
2016-07-11 15:44:02 -0500

    drivers/pinmux: Change spaces by tabs in the pinmap table
    Change spaces by tabulators in the Arduino 101 pinmap table.
    
    Add comment for spi master 1 pin connection.
    Jira: ZEP-518
    
    Change-Id: Iba2e807b315c3d1b70eceddd1c6709f5a5866cba
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>

--- boards/arduino_101/pinmux.c
+++ boards/arduino_101/pinmux.c
@@ -32,76 +32,76 @@
  * A as the hardware brings up all devices by default in mode A.
  */
 
-/* pin, ball, mode A, mode B, mode C */
-/* 0  F02, gpio_0, ain_0, spi_s_cs */			/* IO10 */
-/* 1  G04, gpio_1, ain_1, spi_s_miso */			/* IO12 */
-/* 2  H05, gpio_2, ain_2, spi_s_sck */			/* IO13 */
-/* 3  J06, gpio_3, ain_3, spi_s_mosi */			/* IO11 */
-/* 4  K06, gpio_4, ain_4, NA */
-/* 5  L06, gpio_5, ain_5, NA */
-/* 6  H04, gpio_6, ain_6, NA */
-/* 7  G03, gpio_7, ain_7, NA */
-/* 8  L05, gpio_ss_0, ain_8, uart1_cts */
-/* 9  M05, gpio_ss_1, ain_9, uart1_rts */		/* AD5 */
-/* 10 K05, gpio_ss_2, ain_10 */				/* AD0 */
-/* 11 G01, gpio_ss_3, ain_11 */				/* AD1 */
-/* 12 J04, gpio_ss_4, ain_12 */				/* AD2 */
-/* 13 G02, gpio_ss_5, ain_13 */				/* AD3 */
-/* 14 F01, gpio_ss_6, ain_14 */				/* AD4 */
-/* 15 J05, gpio_ss_7, ain_15 */
-/* 16 L04, gpio_ss_8, ain_16, uart1_txd */		/* IO1 */
-/* 17 M04, gpio_ss_9, ain_17, uart1_rxd */		/* IO0 */
-/* 18 K04, uart0_rx, ain_18, NA */
-/* 19 B02, uart0_tx, gpio_31, NA */
-/* 20 C01, i2c0_scl, NA, NA */
-/* 21 C02, i2c0_sda, NA, NA */
-/* 22 D01, i2c1_scl, NA, NA */
-/* 23 D02, i2c1_sda, NA, NA */
-/* 24 E01, i2c0_ss_sda, NA, NA */
-/* 25 E02, i2c0_ss_scl, NA, NA */
-/* 26 B03, i2c1_ss_sda, NA, NA */
-/* 27 A03, i2c1_ss_scl, NA, NA */
-/* 28 C03, spi0_ss_miso, NA, NA */
-/* 29 E03, spi0_ss_mosi, NA, NA */
-/* 30 D03, spi0_ss_sck, NA, NA */
-/* 31 D04, spi0_ss_cs0, NA, NA */
-/* 32 C04, spi0_ss_cs1, NA, NA */
-/* 33 B04, spi0_ss_cs2, gpio_29, NA */
-/* 34 A04, spi0_ss_cs3, gpio_30, NA */
-/* 35 B05, spi1_ss_miso, NA, NA */
-/* 36 C05, spi1_ss_mosi, NA, NA */
-/* 37 D05, spi1_ss_sck, NA, NA */
-/* 38 E05, spi1_ss_cs0, NA, NA */
-/* 39 E04, spi1_ss_cs1, NA, NA */
-/* 40 A06, spi1_ss_cs2, uart0_cts, NA */
-/* 41 B06, spi1_ss_cs3, uart0_rts, NA */
-/* 42 C06, gpio_8, spi1_m_sck, NA */
-/* 43 D06, gpio_9, spi1_m_miso, NA */
-/* 44 E06, gpio_10, spi1_m_mosi, NA */
-/* 45 D07, gpio_11, spi1_m_cs0, NA */
-/* 46 C07, gpio_12, spi1_m_cs1, NA */
-/* 47 B07, gpio_13, spi1_m_cs2, NA */
-/* 48 A07, gpio_14, spi1_m_cs3, NA */
-/* 49 B08, gpio_15, i2s_rxd, NA */			/* IO5 */
-/* 50 A08, gpio_16, i2s_rscki, NA */			/* IO8 */
-/* 51 B09, gpio_17, i2s_rws, NA */			/* IO3 */
-/* 52 A09, gpio_18, i2s_tsck, NA */			/* IO2 */
-/* 53 C09, gpio_19, i2s_twsi, NA */			/* IO4 */
-/* 54 D09, gpio_20, i2s_txd, NA */			/* IO7 */
-/* 55 D08, gpio_21, spi0_m_sck, NA */
-/* 56 E07, gpio_22, spi0_m_miso, NA */
-/* 57 E09, gpio_23, spi0_m_mosi, NA */
-/* 58 E08, gpio_24, spi0_m_cs0, NA */
-/* 59 A10, gpio_25, spi0_m_cs1, NA */
-/* 60 B10, gpio_26, spi0_m_cs2, NA */
-/* 61 C10, gpio_27, spi0_m_cs3, NA */
-/* 62 D10, gpio_28, NA, NA */
-/* 63 E10, gpio_ss_10, pwm_0, NA */			/* IO3 */
-/* 64 D11, gpio_ss_11, pwm_1, NA */			/* IO5 */
-/* 65 C11, gpio_ss_12, pwm_2, NA */			/* IO6 */
-/* 66 B11, gpio_ss_13, pwm_3, NA */			/* IO9 */
-/* 67 D12, gpio_ss_14, clkout_32khz, NA */
-/* 68 C12, gpio_ss_15, clkout_16mhz, NA */
+/* pin,	ball,	mode A,		mode B,		mode C		*/
+/* 0	F02,	gpio_0,		ain_0,		spi_s_cs	*/   /* IO10 */
+/* 1	G04,	gpio_1,		ain_1,		spi_s_miso	*/   /* IO12 */
+/* 2	H05,	gpio_2,		ain_2,		spi_s_sck	*/   /* IO13 */
+/* 3	J06,	gpio_3,		ain_3,		spi_s_mosi	*/   /* IO11 */
+/* 4	K06,	gpio_4,		ain_4,		NA		*/
+/* 5	L06,	gpio_5,		ain_5,		NA		*/
+/* 6	H04,	gpio_6,		ain_6,		NA		*/
+/* 7	G03,	gpio_7,		ain_7,		NA		*/
+/* 8	L05,	gpio_ss_0,	ain_8,		uart1_cts	*/
+/* 9	M05,	gpio_ss_1,	ain_9,		uart1_rts	*/    /* AD5 */
+/* 10	K05,	gpio_ss_2,	ain_10				*/    /* AD0 */
+/* 11	G01,	gpio_ss_3,	ain_11				*/    /* AD1 */
+/* 12	J04,	gpio_ss_4,	ain_12				*/    /* AD2 */
+/* 13	G02,	gpio_ss_5,	ain_13				*/    /* AD3 */
+/* 14	F01,	gpio_ss_6,	ain_14				*/    /* AD4 */
+/* 15	J05,	gpio_ss_7,	ain_15				*/
+/* 16	L04,	gpio_ss_8,	ain_16,		uart1_txd	*/    /* IO1 */
+/* 17	M04,	gpio_ss_9,	ain_17,		uart1_rxd	*/    /* IO0 */
+/* 18	K04,	uart0_rx,	ain_18,		NA		*/
+/* 19	B02,	uart0_tx,	gpio_31,	NA		*/
+/* 20	C01,	i2c0_scl,	NA,		NA		*/
+/* 21	C02,	i2c0_sda,	NA,		NA		*/
+/* 22	D01,	i2c1_scl,	NA,		NA		*/
+/* 23	D02,	i2c1_sda,	NA,		NA		*/
+/* 24	E01,	i2c0_ss_sda,	NA,		NA		*/
+/* 25	E02,	i2c0_ss_scl,	NA,		NA		*/
+/* 26	B03,	i2c1_ss_sda,	NA,		NA		*/
+/* 27	A03,	i2c1_ss_scl,	NA,		NA		*/
+/* 28	C03,	spi0_ss_miso,	NA,		NA		*/
+/* 29	E03,	spi0_ss_mosi,	NA,		NA		*/
+/* 30	D03,	spi0_ss_sck,	NA,		NA		*/
+/* 31	D04,	spi0_ss_cs0,	NA,		NA		*/
+/* 32	C04,	spi0_ss_cs1,	NA,		NA		*/
+/* 33	B04,	spi0_ss_cs2,	gpio_29,	NA		*/
+/* 34	A04,	spi0_ss_cs3,	gpio_30,	NA		*/
+/* 35	B05,	spi1_ss_miso,	NA,		NA		*/
+/* 36	C05,	spi1_ss_mosi,	NA,		NA		*/
+/* 37	D05,	spi1_ss_sck,	NA,		NA		*/
+/* 38	E05,	spi1_ss_cs0,	NA,		NA		*/
+/* 39	E04,	spi1_ss_cs1,	NA,		NA		*/
+/* 40	A06,	spi1_ss_cs2,	uart0_cts,	NA		*/
+/* 41	B06,	spi1_ss_cs3,	uart0_rts,	NA		*/
+/* 42	C06,	gpio_8,		spi1_m_sck,	NA		*/   /* IO13 */
+/* 43	D06,	gpio_9,		spi1_m_miso,	NA		*/   /* IO12 */
+/* 44	E06,	gpio_10,	spi1_m_mosi,	NA		*/   /* IO11 */
+/* 45	D07,	gpio_11,	spi1_m_cs0,	NA		*/
+/* 46	C07,	gpio_12,	spi1_m_cs1,	NA		*/
+/* 47	B07,	gpio_13,	spi1_m_cs2,	NA		*/
+/* 48	A07,	gpio_14,	spi1_m_cs3,	NA		*/
+/* 49	B08,	gpio_15,	i2s_rxd,	NA		*/    /* IO5 */
+/* 50	A08,	gpio_16,	i2s_rscki,	NA		*/    /* IO8 */
+/* 51	B09,	gpio_17,	i2s_rws,	NA		*/    /* IO3 */
+/* 52	A09,	gpio_18,	i2s_tsck,	NA		*/    /* IO2 */
+/* 53	C09,	gpio_19,	i2s_twsi,	NA		*/    /* IO4 */
+/* 54	D09,	gpio_20,	i2s_txd,	NA		*/    /* IO7 */
+/* 55	D08,	gpio_21,	spi0_m_sck,	NA		*/
+/* 56	E07,	gpio_22,	spi0_m_miso,	NA		*/
+/* 57	E09,	gpio_23,	spi0_m_mosi,	NA		*/
+/* 58	E08,	gpio_24,	spi0_m_cs0,	NA		*/
+/* 59	A10,	gpio_25,	spi0_m_cs1,	NA		*/
+/* 60	B10,	gpio_26,	spi0_m_cs2,	NA		*/
+/* 61	C10,	gpio_27,	spi0_m_cs3,	NA		*/
+/* 62	D10,	gpio_28,	NA,		NA		*/
+/* 63	E10,	gpio_ss_10,	pwm_0,		NA		*/    /* IO3 */
+/* 64	D11,	gpio_ss_11,	pwm_1,		NA		*/    /* IO5 */
+/* 65	C11,	gpio_ss_12,	pwm_2,		NA		*/    /* IO6 */
+/* 66	B11,	gpio_ss_13,	pwm_3,		NA		*/    /* IO9 */
+/* 67	D12,	gpio_ss_14,	clkout_32khz,	NA		*/
+/* 68	C12,	gpio_ss_15,	clkout_16mhz,	NA		*/
 
 /* Note:
  * 1. I2C pins on the shield are connected to i2c0_ss_sda and i2c_0_ss_scl,
