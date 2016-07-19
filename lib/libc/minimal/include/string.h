6d5981bf28c9ba35c7a83cb3ec15987b1fde25ff
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2016-07-19 11:54:43 -0700

    lib: Adding the strstr() function
    The strstr function finds the first occurrence of a substring
    in another string, null terminated strings are not compared, this
    function is added for compability for porting other libraries (like
    mbedtls)
    
    Jira: ZEP-327
    
    Origin: http://www.leidinger.net/freebsd/dox/libkern/html/d3/d29/
    strstr_8c_source.html
    
    Change-Id: I52aac218ce0bd86373ec60f5afc49a92c85f6319
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>

--- lib/libc/minimal/include/string.h
+++ lib/libc/minimal/include/string.h
@@ -35,6 +35,7 @@ extern int    strcmp(const char *s1, const char *s2);
 extern int    strncmp(const char *s1, const char *s2, size_t n);
 extern char *strcat(char *_Restrict dest, const char *_Restrict src);
 extern char  *strncat(char *_Restrict d, const char *_Restrict s, size_t n);
+extern char *strstr(const char *s, const char *find);
 
 extern int    memcmp(const void *m1, const void *m2, size_t n);
 extern void  *memmove(void *d, const void *s, size_t n);
