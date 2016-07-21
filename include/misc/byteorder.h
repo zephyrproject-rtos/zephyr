55209323a10606b32d3e51800a26a97a3bae2988
Johan Hedberg <johan.hedberg@intel.com>
2016-07-21 08:18:14 +0300

    misc/byteorder.h: Add documentation for existing APIs
    Add doxygen documentation for existing documentation in byteorder.h.
    
    Change-Id: I0de668e140eeb011807c31d47212f452ccd81627
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- include/misc/byteorder.h
+++ include/misc/byteorder.h
@@ -1,7 +1,9 @@
-/* byteorder.h - Byte order helpers */
+/** @file
+ *  @brief Byte order helpers.
+ */
 
 /*
- * Copyright (c) 2015, Intel Corporation.
+ * Copyright (c) 2015-2016, Intel Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,10 +18,90 @@
  * limitations under the License.
  */
 
+#include <stdint.h>
+
+/** @def bswap_16
+ *  @brief Change the endianness of a 16-bit integer.
+ *
+ *  @param x 16-bit integer.
+ *
+ *  @return 16-bit integer in swapped endianness.
+ */
 #define bswap_16(x) ((uint16_t) ((((x) >> 8) & 0xff) | (((x) & 0xff) << 8)))
 
+/** @def bswap_32
+ *  @brief Change the endianness of a 32-bit integer.
+ *
+ *  @param x 32-bit integer.
+ *
+ *  @return 32-bit integer in swapped endianness.
+ */
 #define bswap_32(x) ((uint32_t) ((((x) >> 24) & 0xff) | (((x) >> 8) & 0xff00) \
 				| (((x) & 0xff00) << 8) | (((x) & 0xff) << 24)))
+
+/** @def sys_le16_to_cpu
+ *  @brief Convert 16-bit integer from little-endian to host endianness.
+ *
+ *  @param val 16-bit integer in little-endian format.
+ *
+ *  @return 16-bit integer in host endianness.
+ */
+
+/** @def sys_cpu_to_le16
+ *  @brief Convert 16-bit integer from host endianness to little-endian.
+ *
+ *  @param val 16-bit integer in host endianness.
+ *
+ *  @return 16-bit integer in little-endian format.
+ */
+
+/** @def sys_be16_to_cpu
+ *  @brief Convert 16-bit integer from big-endian to host endianness.
+ *
+ *  @param val 16-bit integer in big-endian format.
+ *
+ *  @return 16-bit integer in host endianness.
+ */
+
+/** @def sys_cpu_to_be16
+ *  @brief Convert 16-bit integer from host endianness to big-endian.
+ *
+ *  @param val 16-bit integer in host endianness.
+ *
+ *  @return 16-bit integer in big-endian format.
+ */
+
+/** @def sys_le32_to_cpu
+ *  @brief Convert 32-bit integer from little-endian to host endianness.
+ *
+ *  @param val 32-bit integer in little-endian format.
+ *
+ *  @return 32-bit integer in host endianness.
+ */
+
+/** @def sys_cpu_to_le32
+ *  @brief Convert 32-bit integer from host endianness to little-endian.
+ *
+ *  @param val 32-bit integer in host endianness.
+ *
+ *  @return 32-bit integer in little-endian format.
+ */
+
+/** @def sys_be32_to_cpu
+ *  @brief Convert 32-bit integer from big-endian to host endianness.
+ *
+ *  @param val 32-bit integer in big-endian format.
+ *
+ *  @return 32-bit integer in host endianness.
+ */
+
+/** @def sys_cpu_to_be32
+ *  @brief Convert 32-bit integer from host endianness to big-endian.
+ *
+ *  @param val 32-bit integer in host endianness.
+ *
+ *  @return 32-bit integer in big-endian format.
+ */
 
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #define sys_le16_to_cpu(val) (val)
