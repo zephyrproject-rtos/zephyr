5bc458a0fae521522273302bbb2082d73895e2c9
Andy Ross <andrew.j.ross@intel.com>
2016-08-16 09:47:16 -0700

    util.h: Add IS_ENABLED() macro for expression-legal ifdef-checking
    This trick was pioneered in Linux as the config_enabled() macro.  The
    madness has the effect of taking a macro value that may be defined to
    "1" (e.g. CONFIG_MYFEATURE), or may not be defined at all and turning
    it into a literal expression that can be used at "runtime".  That is,
    it works similarly to "defined(CONFIG_MYFEATURE)" does except that it
    is an expansion that can exist in a standard expression and be seen by
    the compiler and optimizer.  Thus much #ifdef usage can be replaced
    with cleaner expressions like:
    
        if (IS_ENABLED(CONFIG_MYFEATURE))
                myfeature_enable();
    
    Change-Id: I40657d2aa3f802429ac33675a1fe245a5da86615
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- include/misc/util.h
+++ include/misc/util.h
@@ -130,6 +130,58 @@ static inline int64_t arithmetic_shift_right(int64_t value, uint8_t shift)
 
 #define BIT_MASK(n) (BIT(n) - 1)
 
+/**
+ * @brief Check for macro definition in compiler-visible expressions
+ *
+ * This trick was pioneered in Linux as the config_enabled() macro.
+ * The madness has the effect of taking a macro value that may be
+ * defined to "1" (e.g. CONFIG_MYFEATURE), or may not be defined at
+ * all and turning it into a literal expression that can be used at
+ * "runtime".  That is, it works similarly to
+ * "defined(CONFIG_MYFEATURE)" does except that it is an expansion
+ * that can exist in a standard expression and be seen by the compiler
+ * and optimizer.  Thus much ifdef usage can be replaced with cleaner
+ * expressions like:
+ *
+ *     if (IS_ENABLED(CONFIG_MYFEATURE))
+ *             myfeature_enable();
+ *
+ * INTERNAL
+ * First pass just to expand any existing macros, we need the macro
+ * value to be e.g. a literal "1" at expansion time in the next macro,
+ * not "(1)", etc...  Standard recursive expansion does not work.
+ */
+#define IS_ENABLED(config_macro) _IS_ENABLED1(config_macro)
+
+/* Now stick on a "_XXXX" prefix, it will now be "_XXXX1" if config_macro
+ * is "1", or just "_XXXX" if it's undefined.
+ *   ENABLED:   _IS_ENABLED2(_XXXX1)
+ *   DISABLED   _IS_ENABLED2(_XXXX)
+ */
+#define _IS_ENABLED1(config_macro) _IS_ENABLED2(_XXXX##config_macro)
+
+/* Here's the core trick, we map "_XXXX1" to "_YYYY," (i.e. a string
+ * with a trailing comma), so it has the effect of making this a
+ * two-argument tuple to the preprocessor only in the case where the
+ * value is defined to "1"
+ *   ENABLED:    _YYYY,    <--- note comma!
+ *   DISABLED:   _XXXX
+ */
+#define _XXXX1 _YYYY,
+
+/* Then we append an extra argument to fool the gcc preprocessor into
+ * accepting it as a varargs macro.
+ *                         arg1   arg2  arg3
+ *   ENABLED:   _IS_ENABLED3(_YYYY,    1,    0)
+ *   DISABLED   _IS_ENABLED3(_XXXX 1,  0)
+ */
+#define _IS_ENABLED2(one_or_two_args) _IS_ENABLED3(one_or_two_args 1, 0)
+
+/* And our second argument is thus now cooked to be 1 in the case
+ * where the value is defined to 1, and 0 if not:
+ */
+#define _IS_ENABLED3(ignore_this, val, ...) val
+
 #ifdef __cplusplus
 }
 #endif
