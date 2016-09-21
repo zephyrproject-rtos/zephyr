3fa32becd5c0f3b9785a10fa1303cfcd0a80f65d
Kumar Gala <kumar.gala@linaro.org>
2016-09-21 12:17:07 -0500

    arm: merge Cortex-M3/M4 memory map into master Cortex-M memory map
    Merge the Cortex-M3/M4 memory map bits into the master memory map in
    prep for it being shared with Cortex-M7 support and Cortex-M0 support
    going forward.
    
    Change-Id: I211fc2a2d7d49082b51463f06e6e71cca75d886f
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- include/arch/arm/cortex_m/memory_map.h
+++ include/arch/arm/cortex_m/memory_map.h
@@ -58,7 +58,29 @@
 /* 0xe0000000 -> 0xffffffff: varies by processor (see below) */
 
 #if defined(CONFIG_CPU_CORTEX_M3_M4)
-#include <arch/arm/cortex_m/memory_map-m3-m4.h>
+/* 0xe0000000 -> 0xe00fffff: private peripheral bus */
+
+/* 0xe0000000 -> 0xe003ffff: internal [256KB] */
+#define _PPB_INT_BASE_ADDR (_EDEV_END_ADDR + 1)
+#define _PPB_INT_ITM _PPB_INT_BASE_ADDR
+#define _PPB_INT_DWT (_PPB_INT_ITM + KB(4))
+#define _PPB_INT_FPB (_PPB_INT_DWT + KB(4))
+#define _PPB_INT_RSVD_1 (_PPB_INT_FPB + KB(4))
+#define _PPB_INT_SCS (_PPB_INT_RSVD_1 + KB(44))
+#define _PPB_INT_RSVD_2 (_PPB_INT_SCS + KB(4))
+#define _PPB_INT_END_ADDR (_PPB_INT_RSVD_2 + KB(196) - 1)
+
+/* 0xe0040000 -> 0xe00fffff: external [768K] */
+#define _PPB_EXT_BASE_ADDR (_PPB_INT_END_ADDR + 1)
+#define _PPB_EXT_TPIU _PPB_EXT_BASE_ADDR
+#define _PPB_EXT_ETM (_PPB_EXT_TPIU + KB(4))
+#define _PPB_EXT_PPB (_PPB_EXT_ETM + KB(4))
+#define _PPB_EXT_ROM_TABLE (_PPB_EXT_PPB + KB(756))
+#define _PPB_EXT_END_ADDR (_PPB_EXT_ROM_TABLE + KB(4) - 1)
+
+/* 0xe0100000 -> 0xffffffff: vendor-specific [0.5GB-1MB or 511MB]  */
+#define _VENDOR_BASE_ADDR (_PPB_EXT_END_ADDR + 1)
+#define _VENDOR_END_ADDR 0xffffffff
 #else
 #error Unknown CPU
 #endif
