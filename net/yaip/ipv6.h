434f1ed85a48aaedbe8655760304a84c2f3736ac
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2016-09-23 10:52:31 +0300

    net: Remove multiple declarations of net_ipv6_start_rs
    Change-Id: Id3da5c9cfebbddd78a2d16220fe32e04b1cd3053
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>

--- net/yaip/ipv6.h
+++ net/yaip/ipv6.h
@@ -47,7 +47,6 @@ int net_ipv6_send_ns(struct net_if *iface, struct net_buf *pending,
 		     struct in6_addr *src, struct in6_addr *dst,
 		     struct in6_addr *tgt, bool is_my_address);
 
-int net_ipv6_start_rs(struct net_if *iface);
 int net_ipv6_send_rs(struct net_if *iface);
 int net_ipv6_start_rs(struct net_if *iface);
 
