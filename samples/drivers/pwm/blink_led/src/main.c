e1db5f20192f19c228cb3035deb916e7b3ec1e90
Baohong Liu <baohong.liu@intel.com>
2016-09-23 16:31:32 -0700

    samples: pwm: use new APIs and rename the app
    Replace existing APIs which will be deprecated by
    the new API in this app and rename the app.
    
    Following the recent pwm RFC, all existing APIs will
    be deprecated. Two new APIs will be added. The changes
    for the new APIs and the required driver function have
    been posted.
    
    Since the two existing APIs (pwm_pin_set_period and
    pwm_pin_set_values) used by this app will be deprecated,
    this change is to replace them with the new API
    pwm_pin_set_usec().
    
    To demonstrate the usage of the new PWM APIs, more apps
    will be added. All the apps will be located in their own
    sub directories. The existing app is actually one to
    blink LED. So, it is moved the "blink_led" sub directory.
    
    Jira: ZEP-745 ZEP-1029
    
    Change-Id: Icc3fbf6b6c25db9590c63bc08f40ec62c87c0f16
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>

---
+++ samples/drivers/pwm/blink_led/src/main.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file Sample app to demonstrate PWM.
+ *
+ * This app uses PWM[0].
+ */
+
+#include <zephyr.h>
+#include <misc/printk.h>
+#include <device.h>
+#include <pwm.h>
+
+/* in micro second */
+#define MIN_PERIOD	2000
+
+/* in micro second */
+#define MAX_PERIOD	2000000
+
+void main(void)
+{
+	struct device *pwm_dev;
+	uint32_t period = MAX_PERIOD;
+	uint8_t dir = 0;
+
+	printk("PWM demo app-blink LED\n");
+
+	pwm_dev = device_get_binding("PWM_0");
+	if (!pwm_dev) {
+		printk("Cannot find PWM_0!\n");
+		return;
+	}
+
+	while (1) {
+		if (pwm_pin_set_usec(pwm_dev, 0, period, period / 2)) {
+			printk("pwm pin set fails\n");
+			return;
+		}
+
+		if (dir) {
+			period *= 2;
+
+			if (period > MAX_PERIOD) {
+				dir = 0;
+				period = MAX_PERIOD;
+			}
+		} else {
+			period /= 2;
+
+			if (period < MIN_PERIOD) {
+				dir = 1;
+				period = MIN_PERIOD;
+			}
+		}
+
+		task_sleep(SECONDS(4));
+	}
+}
