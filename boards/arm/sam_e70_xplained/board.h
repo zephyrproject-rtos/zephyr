5e916f1c8ae8dddd3a551a2a5d1ae6b443a4db16
Piotr Mienkowski <Piotr.Mienkowski@schmid-telecom.ch>
2016-09-27 21:10:50 +0200

    boards: Add Atmel SAM E70 Xplained board support
    Origin: Original
    
    Jira: ZEP-979
    Change-Id: I1af64364b5b3b4aadbee225ee8f89615fa292863
    Signed-off-by: Piotr Mienkowski <Piotr.Mienkowski@schmid-telecom.ch>

---
+++ boards/arm/sam_e70_xplained/board.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2016 Piotr Mienkowski
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _INC_BOARD_H_
+#define _INC_BOARD_H_
+
+#ifndef _ASMLANGUAGE
+#include <soc.h>
+#endif
+
+#endif /* _INC_BOARD_H_ */
