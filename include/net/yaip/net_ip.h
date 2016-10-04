41fa0c02df983f1e84c6dd329c31489f764abaad
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-10-04 10:04:36 +0300

    net: Add func that checks if address is global multicast one
    Change-Id: If40e8833f8b08c0b066650a2c680771ad0cdc252
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- include/net/yaip/net_ip.h
+++ include/net/yaip/net_ip.h
@@ -524,6 +524,19 @@ static inline bool net_is_ipv6_addr_solicited_node(const struct in6_addr *addr)
 }
 
 /**
+ * @brief Check if the IPv6 address is a global multicast address (FFxE::/16).
+ *
+ * @param addr IPv6 address.
+ *
+ * @return True if the address is global multicast address, false otherwise.
+*/
+static inline bool net_is_ipv6_addr_mcast_global(const struct in6_addr *addr)
+{
+	return addr->s6_addr[0] == 0xff &&
+		(addr->s6_addr[1] & 0x0e) == 0x0e;
+}
+
+/**
  *  @brief Create solicited node IPv6 multicast address
  *  FF02:0:0:0:0:1:FFXX:XXXX defined in RFC 3513
  *
