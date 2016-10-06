61bb244ab1a9abd8930158c3ffd341ea831903b9
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-10-06 11:33:31 -0300

    tests/net/nbuf: Fix wrong size to linearize() call
    linearize() expects the amount of room available in the buffer.
    
    Change-Id: I1062912f76951758177b7dd6588e7e0ae280dc03
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>

--- tests/net/nbuf/src/main.c
+++ tests/net/nbuf/src/main.c
@@ -228,7 +228,7 @@ static int test_fragment_copy(void)
 
 	printk("Total copy data len %zd\n", orig_len);
 
-	linearize(buf, buf_orig, sizeof(orig_len));
+	linearize(buf, buf_orig, orig_len);
 
 	/* Then copy a fragment list to a new fragment list */
 	new_frag = net_nbuf_copy_all(buf->frags, sizeof(struct ipv6_hdr) +
