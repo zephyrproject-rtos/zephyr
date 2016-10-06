98e1e0674cb40bffb341f621f4670bdc6ed8ba61
Allan Stephens <allan.stephens@windriver.com>
2016-10-06 10:19:38 -0500

    unified/test: Fix issues affecting stack canary applications
    The nanokernel version of this application now uses its own source
    file to eliminate its reliance on the build system setting the
    CONFIG_MICROKERNEL and CONFIG_NANOKERNEL options correctly
    (which the unified kernel build system doesn't do).
    
    Change-Id: Ife27f8172b2be33b95136ccdfa29522c8a6fba0b
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>

--- tests/kernel/test_stackprot/nanokernel/src/stackprot.c
+++ tests/kernel/test_stackprot/nanokernel/src/stackprot.c
@@ -21,14 +21,19 @@
 DESCRIPTION
   This is the test program to test stack protection using canary.
 
-  The main task starts a second task, which generates a stack check failure.
-  By design, the second task will not complete its execution and
+  The main task starts a fiber, which generates a stack check failure.
+  By design, the fiber will not complete its execution and
   will not set tcRC to TC_FAIL.
  */
 
 #include <tc_util.h>
 
 #include <zephyr.h>
+
+#include <arch/cpu.h>
+
+#define STACKSIZE               1024
+char __stack fiberStack[STACKSIZE];
 
 static int count = 0;
 static int tcRC = TC_PASS;
@@ -83,19 +88,19 @@ void check_input(const char *name, const char *input)
 
 /**
  *
- * This task passes a long string to check_input function.  It terminates due
+ * This fiber passes a long string to check_input function.  It terminates due
  * to stack overflow and reports "Stack Check Fail" when stack protection
  * feature is enabled.  Hence it will not execute the printLoop function
  * and will not set tcRC to TC_FAIL.
  *
  * @return N/A
  */
-void AlternateTask(void)
+void fiber1(void)
 {
 	TC_PRINT("Starts %s\n", __func__);
 	check_input(__func__, "Input string is too long and stack overflowed!\n");
 	/*
-	 * Expect this task to terminate due to stack check fail and will not
+	 * Expect this fiber to terminate due to stack check fail and will not
 	 * execute pass here.
 	 */
 	printLoop(__func__);
@@ -106,19 +111,20 @@ void AlternateTask(void)
 /**
  *
  * This is the entry point to the test stack protection feature.
- * It starts the task that tests stack protection, then prints out
+ * It starts the fiber that tests stack protection, then prints out
  * a few messages before terminating.
  *
  * @return N/A
  */
 
-void RegressionTask(void)
+void main(void)
 {
 	TC_START("Test Stack Protection Canary\n");
 	TC_PRINT("Starts %s\n", __func__);
 
-	/* Start task */
-	task_start(ALTERNATETASK);       /* refer to prj.mdef file */
+	/* Start fiber */
+	task_fiber_start(&fiberStack[0], STACKSIZE,
+			 (nano_fiber_entry_t) fiber1, 0, 0, 7, 0);
 
 	if (tcRC == TC_FAIL) {
 		goto errorExit;
