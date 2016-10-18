0273ddff248c5db206080753415924d999a4240c
Baohong Liu <baohong.liu@intel.com>
2016-10-18 14:39:53 -0700

    samples: pwm: use new API and add fade led app
    Add a new pwm app (fade led) using the new API.
    
    Following the recent pwm RFC, all existing APIs will
    be deprecated. Two new APIs will be added. The changes
    for the new APIs and the required driver function have
    been posted.
    
    To demonstrate the usage of the new PWM APIs, this app
    to fade led is added. It will use the new API pwm_pin_
    set_usec().
    
    This app will be located in a new sub directory.
    
    Jira: ZEP-745 ZEP-1029
    
    Change-Id: I8d929a21f7de88307c47b52f98f131e00b2fdbe7
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>

---
+++ samples/drivers/pwm/fade_led/src/main.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file Sample app to demonstrate PWM.
+ *
+ * This app uses PWM[0].
+ */
+
+#include <zephyr.h>
+#include <misc/printk.h>
+#include <device.h>
+#include <pwm.h>
+
+/*
+ * 50 is flicker fusion threshold. Modulated light will be perceived
+ * as steady by our eyes when blinking rate is at least 50.
+ */
+#define PERIOD (USEC_PER_SEC / 50)
+
+/* in micro second */
+#define FADESTEP	2000
+
+void main(void)
+{
+	struct device *pwm_dev;
+	uint32_t pulse_width = 0;
+	uint8_t dir = 0;
+
+	printk("PWM demo app-fade LED\n");
+
+	pwm_dev = device_get_binding("PWM_0");
+	if (!pwm_dev) {
+		printk("Cannot find PWM_0!\n");
+		return;
+	}
+
+	while (1) {
+		if (pwm_pin_set_usec(pwm_dev, 0, PERIOD, pulse_width)) {
+			printk("pwm pin set fails\n");
+			return;
+		}
+
+		if (dir) {
+			if (pulse_width < FADESTEP) {
+				dir = 0;
+				pulse_width = 0;
+			} else {
+				pulse_width -= FADESTEP;
+			}
+		} else {
+			pulse_width += FADESTEP;
+
+			if (pulse_width >= PERIOD) {
+				dir = 1;
+				pulse_width = PERIOD;
+			}
+		}
+
+		task_sleep(SECONDS(1));
+	}
+}
