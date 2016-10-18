22a10c3b6c6fea195becb27df6448eba25a980fa
Baohong Liu <baohong.liu@intel.com>
2016-10-18 15:49:02 -0700

    samples: pwm: use new API and add motor control app
    Add a new pwm app (motor control) using the new API.
    
    Following the recent pwm RFC, all existing APIs will
    be deprecated. Two new APIs will be added. The changes
    for the new APIs and the required driver function have
    been posted.
    
    To demonstrate the usage of the new PWM APIs, this app
    to drive a servo motor is added. It will use the new
    API pwm_pin_set_usec().
    
    This app will be located in a new sub directory.
    
    Jira: ZEP-745 ZEP-1029
    
    Origin: Original
    
    Change-Id: I65857848e64d0478c4e12d1317b2089cdd1ecf5c
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>

---
+++ samples/drivers/pwm/servo_motor/src/main.c
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file Sample app to demonstrate PWM.
+ *
+ * This app uses PWM[0].
+ */
+
+#include <zephyr.h>
+#include <misc/printk.h>
+#include <device.h>
+#include <pwm.h>
+
+/*
+ * Unlike pulse width, period is not a critical parameter for
+ * motor control. 20ms is commonly used.
+ */
+#define PERIOD (USEC_PER_SEC / 50)
+
+/* all in micro second */
+#define STEPSIZE 100
+#define MINPULSEWIDTH 700
+#define MAXPULSEWIDTH 2300
+
+void main(void)
+{
+	struct device *pwm_dev;
+	uint32_t pulse_width = MINPULSEWIDTH;
+	uint8_t dir = 0;
+
+	printk("PWM demo app-servo control\n");
+
+	pwm_dev = device_get_binding("PWM_0");
+	if (!pwm_dev) {
+		printk("Cannot find PWM_0!\n");
+		return;
+	}
+
+	while (1) {
+		if (pwm_pin_set_usec(pwm_dev, 0, PERIOD, pulse_width)) {
+			printk("pwm pin set fails\n");
+			return;
+		}
+
+		if (dir) {
+			if (pulse_width <= MINPULSEWIDTH) {
+				dir = 0;
+				pulse_width = MINPULSEWIDTH;
+			} else {
+				pulse_width -= STEPSIZE;
+			}
+		} else {
+			pulse_width += STEPSIZE;
+
+			if (pulse_width >= MAXPULSEWIDTH) {
+				dir = 1;
+				pulse_width = MAXPULSEWIDTH;
+			}
+		}
+
+		task_sleep(SECONDS(1));
+	}
+}
