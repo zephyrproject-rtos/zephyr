4be15a986c36b97be2fc0a747a31f21699057e55
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-10-24 08:30:25 +0100

    sensor/bma280: Make driver_api structure const.
    Change-Id: Ia7601a99ca888b582227a0acea42ebd4018e5c44
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>

--- drivers/sensor/bma280/bma280.c
+++ drivers/sensor/bma280/bma280.c
@@ -111,7 +111,7 @@ static int bma280_channel_get(struct device *dev,
 	return 0;
 }
 
-static struct sensor_driver_api bma280_driver_api = {
+static const struct sensor_driver_api bma280_driver_api = {
 #if CONFIG_BMA280_TRIGGER
 	.attr_set = bma280_attr_set,
 	.trigger_set = bma280_trigger_set,
