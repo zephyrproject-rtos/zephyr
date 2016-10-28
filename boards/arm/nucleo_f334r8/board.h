5f2fa051a65e5d5b73922c3d38972ea34f21334b
Adam Podogrocki <adam.podogrocki@rndity.com>
2016-10-28 12:02:20 +0200

    boards: add initial support for Nucleo-64 with Soc STM32F334
    Change-Id: I0b9992e26353cea3b0415aa57ab3b983d84cef05
    Signed-off-by: Adam Podogrocki <adam.podogrocki@rndity.com>

---
+++ boards/arm/nucleo_f334r8/board.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2016 RnDity Sp. z o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+/* USER push button */
+#define USER_PB_GPIO_PORT	"GPIOC"
+#define USER_PB_GPIO_PIN	13
+
+/* LD2 green LED */
+#define LD2_GPIO_PORT		"GPIOA"
+#define LD2_GPIO_PIN		5
+
+/* Create aliases to make the basic samples work */
+#define SW0_GPIO_NAME		USER_PB_GPIO_PORT
+#define SW0_GPIO_PIN		USER_PB_GPIO_PIN
+#define LED0_GPIO_PORT		LD2_GPIO_PORT
+#define LED0_GPIO_PIN		LD2_GPIO_PIN
+
+#endif /* __INC_BOARD_H */
