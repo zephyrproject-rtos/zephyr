aabd839213995b3aba4c3b322929f5a39dd6ca37
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-10-31 14:32:02 +0200

    net: ieee802154: MAC address needs to be stored in big endian format
    The upper IP stack uses the MAC address to construct the IPv6
    address in SLAAC. Because of this the MAC needs to be stored
    in big-endian format so that it can be used directly as is.
    
    Change-Id: Ib31da56307017b4284031328772d4cb51d84fa23
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- net/yaip/l2/ieee802154/ieee802154.c
+++ net/yaip/l2/ieee802154/ieee802154.c
@@ -313,12 +313,15 @@ void ieee802154_init(struct net_if *iface)
 #ifdef CONFIG_NET_L2_IEEE802154_ORFD
 	struct ieee802154_context *ctx = net_if_l2_data(iface);
 	const uint8_t *mac = iface->link_addr.addr;
+	uint8_t long_addr[8];
 	uint16_t short_addr;
+
+	sys_memcpy_swap(long_addr, mac, 8);
 
 	short_addr = (mac[0] << 8) + mac[1];
 
 	radio->set_short_addr(iface->dev, short_addr);
-	radio->set_ieee_addr(iface->dev, mac);
+	radio->set_ieee_addr(iface->dev, long_addr);
 
 	ctx->pan_id = CONFIG_NET_L2_IEEE802154_ORFD_PAN_ID;
 	ctx->channel = CONFIG_NET_L2_IEEE802154_ORFD_CHANNEL;
