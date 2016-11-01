c4e512651995d31a8d492c4e42b09187a83ecf90
Vincenzo Frascino <vincenzo.frascino@linaro.org>
2016-11-01 12:15:44 +0000

    soc_beetle: Add initial PM configuration
    This patch adds the boot time Power Management configuration for Beetle
    on Zephyr. In particular it defines the states of the peripherals during
    sleep and deep sleep and the allowed wakeup sources.
    
    Jira: ZEP-1300
    Change-Id: Iad9c0f851771ea60d94bbe5420b7b3ee0743b77e
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@linaro.org>

--- arch/arm/soc/arm/beetle/soc.h
+++ arch/arm/soc/arm/beetle/soc.h
@@ -69,8 +69,20 @@
 #define _BEETLE_SYSCON_BASE     (_BEETLE_AHB_BASE + 0xF000)
 
 /* Beetle SoC APB peripherals */
+#define _BEETLE_TIMER0_BASE	(_BEETLE_APB_BASE + 0x0000)
+#define _BEETLE_TIMER1_BASE	(_BEETLE_APB_BASE + 0x1000)
+#define _BEETLE_DTIMER_BASE	(_BEETLE_APB_BASE + 0x2000)
+#define _BEETLE_FCACHE_BASE	(_BEETLE_APB_BASE + 0x3000)
 #define _BEETLE_UART0_BASE	(_BEETLE_APB_BASE + 0x4000)
 #define _BEETLE_UART1_BASE	(_BEETLE_APB_BASE + 0x5000)
+#define _BEETLE_RTC_BASE	(_BEETLE_APB_BASE + 0x6000)
+#define _BEETLE_I2C0_BASE	(_BEETLE_APB_BASE + 0x7000)
+#define _BEETLE_WDOG_BASE	(_BEETLE_APB_BASE + 0x8000)
+#define _BEETLE_QSPI_BASE	(_BEETLE_APB_BASE + 0xB000)
+#define _BEETLE_SPI0_BASE	(_BEETLE_APB_BASE + 0xC000)
+#define _BEETLE_SPI1_BASE	(_BEETLE_APB_BASE + 0xD000)
+#define _BEETLE_I2C1_BASE	(_BEETLE_APB_BASE + 0xE000)
+#define _BEETLE_TRNG_BASE	(_BEETLE_APB_BASE + 0xF000)
 
 #ifndef _ASMLANGUAGE
 
@@ -78,7 +90,9 @@
 #include <misc/util.h>
 
 #include "soc_pins.h"
+#include "soc_power.h"
 #include "soc_registers.h"
+#include "soc_pll.h"
 
 /* System Control Register (SYSCON) */
 #define __BEETLE_SYSCON ((volatile struct syscon *)_BEETLE_SYSCON_BASE)
@@ -89,9 +103,19 @@
 #define CMSDK_AHB_GPIO2 _BEETLE_GPIO2_BASE
 #define CMSDK_AHB_GPIO3 _BEETLE_GPIO3_BASE
 
+/* CMSDK APB Timers */
+#define CMSDK_APB_TIMER0 _BEETLE_TIMER0_BASE
+#define CMSDK_APB_TIMER1 _BEETLE_TIMER1_BASE
+
+/* CMSDK APB Dual Timer */
+#define CMSDK_APB_DTIMER _BEETLE_DTIMER_BASE
+
 /* CMSDK APB Universal Asynchronous Receiver-Transmitter (UART) */
 #define CMSDK_APB_UART0 _BEETLE_UART0_BASE
 #define CMSDK_APB_UART1 _BEETLE_UART1_BASE
+
+/* CMSDK APB Watchdog */
+#define CMSDK_APB_WDOG _BEETLE_WDOG_BASE
 
 #endif /* !_ASMLANGUAGE */
 
