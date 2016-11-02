bf169afd469c768cdc3dd35e3d8e621aad3b1fd1
Anas Nashif <anas.nashif@intel.com>
2016-11-02 18:00:28 -0400

    sensors: sx9500: convert to and build with unified kernel
    Change-Id: Ic3e8d0c59eda97759f1d56127f42120b0c0a6def
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- samples/sensor/sx9500/src/main.c
+++ samples/sensor/sx9500/src/main.c
@@ -54,7 +54,7 @@ void do_main(struct device *dev)
 	setup_trigger(dev);
 
 	while (1) {
-		task_sleep(sys_clock_ticks_per_sec);
+		k_sleep(1000);
 	}
 }
 
@@ -75,7 +75,7 @@ static void do_main(struct device *dev)
 		ret = sensor_channel_get(dev, SENSOR_CHAN_PROX, &prox_value);
 		printk("prox is %d\n", prox_value.val1);
 
-		task_sleep(SECONDS(1));
+		k_sleep(1000);
 	}
 }
 
