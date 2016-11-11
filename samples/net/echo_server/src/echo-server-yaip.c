26ea431c5fe165be5225d88a1fbe1861aa4a731c
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-11-11 12:03:16 +0200

    samples: net: echo apps: Do not set preferred IPv6 address
    Previous commits support auto configured IPv6 addresses so no
    need to manually tweak the address settings.
    
    Change-Id: Ie641212981a04229721c84990a54876977d9cfc4
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- samples/net/echo_server/src/echo-server-yaip.c
+++ samples/net/echo_server/src/echo-server-yaip.c
@@ -103,12 +103,6 @@ static inline void init_app(void)
 
 		ifaddr = net_if_ipv6_addr_add(net_if_get_default(),
 					      &in6addr_my, NET_ADDR_MANUAL, 0);
-
-		/*
-		 * Make the address preferred so that it gets selected
-		 * properly.
-		 */
-		ifaddr->addr_state = NET_ADDR_PREFERRED;
 	} while (0);
 
 	net_if_ipv6_maddr_add(net_if_get_default(), &in6addr_mcast);
