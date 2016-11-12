5db9bc88953f45332988b6f61064e4a6e2235da4
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-11-12 19:53:38 +0200

    net: ipv4: Add utils to create IPv4 network packet
    Create utility functions to create IPv4 packet if the
    network context is not known.
    
    Change-Id: I982830d09f3b91e23b75e6b63a5d97bfea0e8d4e
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- net/yaip/ipv4.c
+++ net/yaip/ipv4.c
@@ -31,13 +31,16 @@
 #include "net_private.h"
 #include "ipv4.h"
 
-struct net_buf *net_ipv4_create(struct net_context *context,
-				struct net_buf *buf,
-				const struct in_addr *addr)
+struct net_buf *net_ipv4_create_raw(struct net_buf *buf,
+				    uint16_t reserve,
+				    const struct in_addr *src,
+				    const struct in_addr *dst,
+				    struct net_if *iface,
+				    uint8_t next_header)
 {
 	struct net_buf *header;
 
-	header = net_nbuf_get_data(context);
+	header = net_nbuf_get_reserve_data(reserve);
 
 	net_buf_frag_insert(buf, header);
 
@@ -45,32 +48,47 @@ struct net_buf *net_ipv4_create(struct net_context *context,
 	NET_IPV4_BUF(buf)->tos = 0x00;
 	NET_IPV4_BUF(buf)->proto = 0;
 
-	NET_IPV4_BUF(buf)->ttl =
-		net_if_ipv4_get_ttl(net_context_get_iface(context));
+	NET_IPV4_BUF(buf)->ttl = net_if_ipv4_get_ttl(iface);
 	NET_IPV4_BUF(buf)->offset[0] = NET_IPV4_BUF(buf)->offset[1] = 0;
 	NET_IPV4_BUF(buf)->id[0] = NET_IPV4_BUF(buf)->id[1] = 0;
 
-	net_ipaddr_copy(&NET_IPV4_BUF(buf)->dst, addr);
+	net_ipaddr_copy(&NET_IPV4_BUF(buf)->dst, dst);
+	net_ipaddr_copy(&NET_IPV4_BUF(buf)->src, src);
 
-	NET_ASSERT(((struct sockaddr_in_ptr *)&context->local)->sin_addr);
-	net_ipaddr_copy(&NET_IPV4_BUF(buf)->src,
-		((struct sockaddr_in_ptr *)&context->local)->sin_addr);
-
-#if defined(CONFIG_NET_UDP)
-	if (net_context_get_ip_proto(context) == IPPROTO_UDP) {
-		NET_IPV4_BUF(buf)->proto = IPPROTO_UDP;
-	}
-#endif /* CONFIG_NET_UDP */
+	NET_IPV4_BUF(buf)->proto = next_header;
 
 	net_nbuf_set_ip_hdr_len(buf, sizeof(struct net_ipv4_hdr));
+	net_nbuf_set_family(buf, AF_INET);
 
 	net_buf_add(header, sizeof(struct net_ipv4_hdr));
 
 	return buf;
 }
 
-struct net_buf *net_ipv4_finalize(struct net_context *context,
-				  struct net_buf *buf)
+struct net_buf *net_ipv4_create(struct net_context *context,
+				struct net_buf *buf,
+				const struct in_addr *addr)
+{
+	const struct in_addr *src;
+
+	NET_ASSERT(((struct sockaddr_in_ptr *)&context->local)->sin_addr);
+
+	src = ((struct sockaddr_in_ptr *)&context->local)->sin_addr;
+
+	if (net_is_ipv4_addr_unspecified(src) || net_is_ipv4_addr_mcast(src)) {
+		src = &net_nbuf_iface(buf)->ipv4.unicast[0].address.in_addr;
+	}
+
+	return net_ipv4_create_raw(buf,
+				   net_nbuf_ll_reserve(buf),
+				   src,
+				   addr,
+				   net_context_get_iface(context),
+				   net_context_get_ip_proto(context));
+}
+
+struct net_buf *net_ipv4_finalize_raw(struct net_buf *buf,
+				      uint8_t next_header)
 {
 	/* Set the length of the IPv4 header */
 	size_t total_len;
@@ -86,19 +104,26 @@ struct net_buf *net_ipv4_finalize(struct net_context *context,
 	NET_IPV4_BUF(buf)->chksum = ~net_calc_chksum_ipv4(buf);
 
 #if defined(CONFIG_NET_UDP)
-	if (net_context_get_ip_proto(context) == IPPROTO_UDP) {
+	if (next_header == IPPROTO_UDP) {
 		NET_UDP_BUF(buf)->chksum = 0;
 		NET_UDP_BUF(buf)->chksum = ~net_calc_chksum_udp(buf);
 	}
 #endif
 #if defined(CONFIG_NET_TCP)
-	if (net_context_get_ip_proto(context) == IPPROTO_TCP) {
+	if (next_header == IPPROTO_TCP) {
 		NET_TCP_BUF(buf)->chksum = 0;
 		NET_TCP_BUF(buf)->chksum = ~net_calc_chksum_tcp(buf);
 	}
 #endif
 
 	return buf;
+}
+
+struct net_buf *net_ipv4_finalize(struct net_context *context,
+				  struct net_buf *buf)
+{
+	return net_ipv4_finalize_raw(buf,
+				     net_context_get_ip_proto(context));
 }
 
 const struct in_addr *net_ipv4_unspecified_address(void)
--- net/yaip/ipv4.h
+++ net/yaip/ipv4.h
@@ -35,6 +35,25 @@
 /**
  * @brief Create IPv4 packet in provided net_buf.
  *
+ * @param buf Network buffer
+ * @param reserve Link layer reserve
+ * @param src Source IPv4 address
+ * @param dst Destination IPv4 address
+ * @param iface Network interface
+ * @param next_header Protocol type of the next header after IPv4 header.
+ *
+ * @return Return network buffer that contains the IPv4 packet.
+ */
+struct net_buf *net_ipv4_create_raw(struct net_buf *buf,
+				    uint16_t reserve,
+				    const struct in_addr *src,
+				    const struct in_addr *dst,
+				    struct net_if *iface,
+				    uint8_t next_header);
+
+/**
+ * @brief Create IPv4 packet in provided net_buf.
+ *
  * @param context Network context for a connection
  * @param buf Network buffer
  * @param dst_addr Destination IPv4 address
@@ -44,6 +63,20 @@
 struct net_buf *net_ipv4_create(struct net_context *context,
 				struct net_buf *buf,
 				const struct in_addr *addr);
+
+/**
+ * @brief Finalize IPv4 packet. It should be called right before
+ * sending the packet and after all the data has been added into
+ * the packet. This function will set the length of the
+ * packet and calculate the higher protocol checksum if needed.
+ *
+ * @param buf Network buffer
+ * @param next_header Protocol type of the next header after IPv4 header.
+ *
+ * @return Return network buffer that contains the IPv4 packet.
+ */
+struct net_buf *net_ipv4_finalize_raw(struct net_buf *buf,
+				      uint8_t next_header);
 
 /**
  * @brief Finalize IPv4 packet. It should be called right before
