b6b35f61134f714d8317c1f5d6493a4ddab22cd8
Vincenzo Frascino <vincenzo.frascino@linaro.org>
2016-11-18 16:08:47 +0000

    clock_control: Cleanup stm32f4x driver
    This patch addresses the following issues:
    * Aligns the Kconfig code style with Zephyr projects requirements.
    * Removes redundant "depends on" from Kconfig.
    * Adds static to the containing file scope declarations.
    
    Change-Id: I14651826724c014cc71d62ab6cab03c668c578c0
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@linaro.org>

--- drivers/clock_control/stm32f4x_clock.c
+++ drivers/clock_control/stm32f4x_clock.c
@@ -104,7 +104,7 @@ struct regval_map {
 	int reg;
 };
 
-int map_reg_val(const struct regval_map *map, size_t cnt, int val)
+static int map_reg_val(const struct regval_map *map, size_t cnt, int val)
 {
 	for (int i = 0; i < cnt; i++) {
 		if (map[i].val == val) {
@@ -180,7 +180,7 @@ static int __pllp_div(int div)
 }
 #endif	/* CONFIG_CLOCK_STM32F4X_SYSCLK_SRC_PLL */
 
-uint32_t __get_ahb_clock(uint32_t sysclk)
+static uint32_t __get_ahb_clock(uint32_t sysclk)
 {
 	/* AHB clock is generated based on SYSCLK */
 	uint32_t sysclk_div = CONFIG_CLOCK_STM32F4X_AHB_PRESCALER;
@@ -192,7 +192,7 @@ uint32_t __get_ahb_clock(uint32_t sysclk)
 	return sysclk / sysclk_div;
 }
 
-uint32_t __get_apb_clock(uint32_t ahb_clock, uint32_t prescaler)
+static uint32_t __get_apb_clock(uint32_t ahb_clock, uint32_t prescaler)
 {
 	if (prescaler == 0) {
 		prescaler = 1;
@@ -237,7 +237,7 @@ static struct clock_control_driver_api stm32f4x_clock_control_api = {
 	.get_rate = stm32f4x_clock_control_get_subsys_rate,
 };
 
-int stm32f4x_clock_control_init(struct device *dev)
+static int stm32f4x_clock_control_init(struct device *dev)
 {
 	struct stm32f4x_rcc_data *data = dev->driver_data;
 	volatile struct stm32f4x_rcc *rcc =
