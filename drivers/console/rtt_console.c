5f1c6ee2c8057ed704ba4d4e81c8315f48a9f9b9
Carles Cufi <carles.cufi@nordicsemi.no>
2016-11-22 15:33:46 +0100

    console: Add a Segger RTT console driver
    The Real Time Terminal functionality of the Segger J-Link
    debug probe and software suite allows applications to write
    to a certain RAM area that is read in real-time by the
    debugger. This is specially useful when a UART is not
    available to provide a console.
    This commit adds a console driver that outputs over an RTT
    "connection".
    
    Change-Id: Ifce8020060e9f519a61f3f1fbe9f56f98da2ef7f
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

---
+++ drivers/console/rtt_console.c
@@ -0,0 +1,51 @@
+/* rtt_console.c - Console messages to a RAM buffer that is then read by
+ * the Segger J-Link debugger
+ */
+
+/*
+ * Copyright (c) 2016 Nordic Semiconductor ASA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include <kernel.h>
+#include <misc/printk.h>
+#include <device.h>
+#include <init.h>
+#include <rtt/SEGGER_RTT.h>
+
+extern void __printk_hook_install(int (*fn)(int));
+extern void __stdout_hook_install(int (*fn)(int));
+
+static int rtt_console_out(int character)
+{
+	char c = (char)character;
+
+	SEGGER_RTT_WriteNoLock(0, &c, 1);
+	return character;
+}
+
+static int rtt_console_init(struct device *d)
+{
+	ARG_UNUSED(d);
+
+	SEGGER_RTT_Init();
+
+	__printk_hook_install(rtt_console_out);
+	__stdout_hook_install(rtt_console_out);
+
+	return 0;
+}
+
+SYS_INIT(rtt_console_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
