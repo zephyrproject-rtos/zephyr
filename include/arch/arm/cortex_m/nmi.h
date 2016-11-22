4d8c0218204f6000d1e9fcc2f35affda024bb22b
Vincenzo Frascino <vincenzo.frascino@linaro.org>
2016-11-22 16:38:46 +0000

    arm: Fix CONFIG_RUNTIME_NMI behavior
    Zephyr kernel is unable to compile when CONFIG_RUNTIME_NMI is enabled in
    defconfig on ARM's architectures.
    
    This patch addresses the following issues:
    * In nmi.c _DefaultHandler() is referencing a function
    (_ScbSystemReset()) not defined in Zephyr. This has now been replaced
    with sys_arch_reboot.
    * nmi.h is included in ASM files and due to the usage of "extern" the
    compilation ends with an error. Added the directive _ASMLANGUAGE to
    prevent the problem.
    
    Jira: ZEP-1319
    Change-Id: I7623ca97523cde04e4c6db40dc332d93ca801928
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@linaro.org>

--- include/arch/arm/cortex_m/nmi.h
+++ include/arch/arm/cortex_m/nmi.h
@@ -23,11 +23,13 @@
 #ifndef __CORTEX_M_NMI_H
 #define __CORTEX_M_NMI_H
 
+#ifndef _ASMLANGUAGE
 #ifdef CONFIG_RUNTIME_NMI
 extern void _NmiInit(void);
 #define NMI_INIT() _NmiInit()
 #else
 #define NMI_INIT()
+#endif
 #endif
 
 #endif /* __CORTEX_M_NMI_H */
