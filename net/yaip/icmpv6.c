30285fd5502fd4c3a43e6d6712927955373ae61d
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-11-24 15:25:50 +0200

    net: ICMPv6 Echo-Reply message was incorrectly setup
    We tried to use the received Echo-Request buffer to send
    the Echo-Reply. This did not work that well as then the
    source link layer address was set to be the same as
    destination address by the lower level L2 driver.
    
    Jira: ZEP-1350
    
    Change-Id: I0e7c06866d5b863f091e1d5c2a79dfcc2f23df20
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- net/yaip/icmpv6.c
+++ net/yaip/icmpv6.c
@@ -41,73 +41,16 @@ void net_icmpv6_register_handler(struct net_icmpv6_handler *handler)
 	sys_slist_prepend(&handlers, &handler->node);
 }
 
-static enum net_verdict handle_echo_request(struct net_buf *buf)
-{
-	/* Note that we send the same data buffers back and just swap
-	 * the addresses etc.
-	 */
-#if NET_DEBUG > 0
-	char out[NET_IPV6_ADDR_LEN];
-
-	snprintf(out, sizeof(out),
-		 net_sprint_ipv6_addr(&NET_IPV6_BUF(buf)->dst));
-	NET_DBG("Received Echo Request from %s to %s",
-		net_sprint_ipv6_addr(&NET_IPV6_BUF(buf)->src), out);
-#endif /* NET_DEBUG > 0 */
-
-	if (net_is_ipv6_addr_mcast(&NET_IPV6_BUF(buf)->dst)) {
-		net_ipaddr_copy(&NET_IPV6_BUF(buf)->dst,
-				&NET_IPV6_BUF(buf)->src);
-
-		net_ipaddr_copy(&NET_IPV6_BUF(buf)->src,
-				net_if_ipv6_select_src_addr(net_nbuf_iface(buf),
-						&NET_IPV6_BUF(buf)->dst));
-	} else {
-		struct in6_addr addr;
-
-		net_ipaddr_copy(&addr, &NET_IPV6_BUF(buf)->src);
-		net_ipaddr_copy(&NET_IPV6_BUF(buf)->src,
-				&NET_IPV6_BUF(buf)->dst);
-		net_ipaddr_copy(&NET_IPV6_BUF(buf)->dst, &addr);
-	}
-
-	NET_IPV6_BUF(buf)->hop_limit =
-		net_if_ipv6_get_hop_limit(net_nbuf_iface(buf));
-
-	NET_ICMP_BUF(buf)->type = NET_ICMPV6_ECHO_REPLY;
-	NET_ICMP_BUF(buf)->code = 0;
-	NET_ICMP_BUF(buf)->chksum = 0;
-	NET_ICMP_BUF(buf)->chksum = ~net_calc_chksum_icmpv6(buf);
-
-	net_nbuf_ll_swap(buf);
-
-#if NET_DEBUG > 0
-	snprintf(out, sizeof(out),
-		 net_sprint_ipv6_addr(&NET_IPV6_BUF(buf)->dst));
-	NET_DBG("Sending Echo Reply from %s to %s",
-		net_sprint_ipv6_addr(&NET_IPV6_BUF(buf)->src), out);
-#endif /* NET_DEBUG > 0 */
-
-	if (net_send_data(buf) < 0) {
-		NET_STATS(++net_stats.icmp.drop);
-		return NET_DROP;
-	}
-
-	NET_STATS(++net_stats.icmp.sent);
-
-	return NET_OK;
-}
-
-static inline void setup_ipv6_header(struct net_buf *buf, uint8_t extra_len,
+static inline void setup_ipv6_header(struct net_buf *buf, uint16_t extra_len,
 				     uint8_t hop_limit, uint8_t icmp_type,
 				     uint8_t icmp_code)
 {
 	NET_IPV6_BUF(buf)->vtc = 0x60;
 	NET_IPV6_BUF(buf)->tcflow = 0;
 	NET_IPV6_BUF(buf)->flow = 0;
-	NET_IPV6_BUF(buf)->len[0] = 0;
-	NET_IPV6_BUF(buf)->len[1] = NET_ICMPH_LEN + extra_len +
-		NET_ICMPV6_UNUSED_LEN;
+
+	sys_put_be16(NET_ICMPH_LEN + extra_len + NET_ICMPV6_UNUSED_LEN,
+		     NET_IPV6_BUF(buf)->len);
 
 	NET_IPV6_BUF(buf)->nexthdr = IPPROTO_ICMPV6;
 	NET_IPV6_BUF(buf)->hop_limit = hop_limit;
@@ -121,6 +64,111 @@ static inline void setup_ipv6_header(struct net_buf *buf, uint8_t extra_len,
 	 */
 	memset(net_nbuf_icmp_data(buf) + sizeof(struct net_icmp_hdr), 0,
 	       NET_ICMPV6_UNUSED_LEN);
+}
+
+static enum net_verdict handle_echo_request(struct net_buf *orig)
+{
+	struct net_buf *buf, *frag;
+	struct in6_addr *src, *dst;
+	struct net_if *iface;
+	uint16_t payload_len;
+	uint32_t id, seq;
+	uint8_t *ptr;
+
+#if NET_DEBUG > 0
+	do {
+		char out[NET_IPV6_ADDR_LEN];
+
+		snprintf(out, sizeof(out),
+			 net_sprint_ipv6_addr(&NET_IPV6_BUF(orig)->dst));
+		NET_DBG("Received Echo Request from %s to %s",
+			net_sprint_ipv6_addr(&NET_IPV6_BUF(orig)->src), out);
+	} while (0);
+#endif /* NET_DEBUG > 0 */
+
+	iface = net_nbuf_iface(orig);
+
+	buf = net_nbuf_get_reserve_tx(0);
+
+	/* We need to remember the original location of source and destination
+	 * addresses as the net_nbuf_copy() will mangle the original buffer.
+	 */
+	src = &NET_IPV6_BUF(orig)->src;
+	dst = &NET_IPV6_BUF(orig)->dst;
+
+	/* The seq and id fields from original request needs to be copied
+	 * to echo reply.
+	 */
+	ptr = (uint8_t *)NET_ICMP_BUF(orig) + sizeof(struct net_icmp_hdr);
+	id = sys_get_be32(ptr);
+	seq = sys_get_be32(ptr + sizeof(uint32_t));
+
+	payload_len = sys_get_be16(NET_IPV6_BUF(orig)->len) -
+		sizeof(NET_ICMPH_LEN) - NET_ICMPV6_UNUSED_LEN;
+
+	frag = net_nbuf_copy_all(orig->frags, 0);
+	if (!frag) {
+		goto drop;
+	}
+
+	net_buf_frag_add(buf, frag);
+	net_nbuf_set_family(buf, AF_INET6);
+	net_nbuf_set_iface(buf, iface);
+	net_nbuf_set_ll_reserve(buf, net_buf_headroom(frag));
+	net_nbuf_set_ext_len(buf, 0);
+
+	setup_ipv6_header(buf, payload_len, net_if_ipv6_get_hop_limit(iface),
+			  NET_ICMPV6_ECHO_REPLY, 0);
+
+	if (net_is_ipv6_addr_mcast(&NET_IPV6_BUF(buf)->dst)) {
+		net_ipaddr_copy(&NET_IPV6_BUF(buf)->dst, src);
+
+		net_ipaddr_copy(&NET_IPV6_BUF(buf)->src,
+				net_if_ipv6_select_src_addr(iface, dst));
+	} else {
+		struct in6_addr addr;
+
+		net_ipaddr_copy(&addr, src);
+		net_ipaddr_copy(&NET_IPV6_BUF(buf)->src, dst);
+		net_ipaddr_copy(&NET_IPV6_BUF(buf)->dst, &addr);
+	}
+
+	net_nbuf_ll_src(buf)->addr = net_nbuf_ll_dst(orig)->addr;
+	net_nbuf_ll_src(buf)->len = net_nbuf_ll_dst(orig)->len;
+	net_nbuf_ll_dst(buf)->addr = net_nbuf_ll_src(orig)->addr;
+	net_nbuf_ll_dst(buf)->len = net_nbuf_ll_src(orig)->len;
+
+	ptr = (uint8_t *)NET_ICMP_BUF(buf) + sizeof(struct net_icmp_hdr);
+	sys_put_be32(id, ptr);
+	sys_put_be32(seq, ptr + sizeof(uint32_t));
+
+	NET_ICMP_BUF(buf)->chksum = 0;
+	NET_ICMP_BUF(buf)->chksum = ~net_calc_chksum_icmpv6(buf);
+
+#if NET_DEBUG > 0
+	do {
+		char out[NET_IPV6_ADDR_LEN];
+
+		snprintf(out, sizeof(out),
+			 net_sprint_ipv6_addr(&NET_IPV6_BUF(buf)->dst));
+		NET_DBG("Sending Echo Reply from %s to %s",
+			net_sprint_ipv6_addr(&NET_IPV6_BUF(buf)->src), out);
+	} while (0);
+#endif /* NET_DEBUG > 0 */
+
+	if (net_send_data(buf) < 0) {
+		goto drop;
+	}
+
+	NET_STATS(++net_stats.icmp.sent);
+
+	return NET_DROP;
+
+drop:
+	net_nbuf_unref(buf);
+
+	NET_STATS(++net_stats.icmp.drop);
+	return NET_DROP;
 }
 
 int net_icmpv6_send_error(struct net_buf *orig, uint8_t type, uint8_t code,
