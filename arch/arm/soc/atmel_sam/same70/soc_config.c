f604480438bb238c0bbd03bf9c23b1425ae51dc6
Piotr Mienkowski <piotr.mienkowski@gmail.com>
2016-12-02 03:23:20 +0100

    arch: Add support for Atmel SAM E70 SoC configuration at boot
    Added support for early SoC configuration which does not belong
    conceptually to system initialization. ERASE, JTAG TDI pins are
    currently configured here.
    
    Tested on Atmel SMART SAM E70 Xplained board
    
    Origin: Original
    
    Jira: ZEP-978
    Change-Id: Id51adddb4c7cce22907af214461d75db83df129a
    Signed-off-by: Piotr Mienkowski <piotr.mienkowski@gmail.com>

---
+++ arch/arm/soc/atmel_sam/same70/soc_config.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2016 Piotr Mienkowski
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/** @file
+ * @brief System module to support early Atmel SAM E70 MCU configuration
+ */
+
+#include <device.h>
+#include <init.h>
+#include <soc.h>
+#include <arch/cpu.h>
+
+/**
+ * @brief Perform SoC configuration at boot.
+ *
+ * This should be run early during the boot process but after basic hardware
+ * initialization is done.
+ *
+ * @return 0
+ */
+static int atmel_same70_config(struct device *dev)
+{
+#ifdef SOC_ATMEL_SAME70_DISABLE_ERASE_PIN
+	/* Disable ERASE function on PB12 pin, this is controlled by Bus Matrix */
+	MATRIX->CCFG_SYSIO |= CCFG_SYSIO_SYSIO12;
+#endif
+
+	/* In Cortex-M based SoCs JTAG interface can be used to perform
+	 * IEEE1149.1 JTAG Boundary scan only. It can not be used as a debug
+	 * interface therefore there is no harm done by disabling the JTAG TDI
+	 * pin by default.
+	 */
+	/* Disable TDI function on PB4 pin, this is controlled by Bus Matrix */
+	MATRIX->CCFG_SYSIO |= CCFG_SYSIO_SYSIO4;
+
+	return 0;
+}
+
+SYS_INIT(atmel_same70_config, PRE_KERNEL_1, 1);
