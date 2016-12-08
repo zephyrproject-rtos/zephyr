08d97e4fe9f7536aba636b489d87cfc0bf0e36ec
Carles Cufi <carles.cufi@nordicsemi.no>
2016-12-08 16:31:05 +0100

    boards: nrf52_pca10040: Add button and LED definitions
    For the basic samples to work, button and LED definitions are required
    in board.h. These map the buttons and LEDs present on the board to
    the corresponding GPIOs they are wired into.
    
    Change-Id: I2785441c286b95fd77e636e9bf3d68bc9f5a1acc
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- boards/arm/nrf52_pca10040/board.h
+++ boards/arm/nrf52_pca10040/board.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Nordic Semiconductor ASA
+ * Copyright (c) 2016 Nordic Semiconductor ASA.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,5 +18,37 @@
 #define __INC_BOARD_H
 
 #include <soc.h>
+
+/* Push button switch 0 */
+#define SW0_GPIO_PIN	13
+#define SW0_GPIO_NAME	CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+/* Push button switch 1 */
+#define SW1_GPIO_PIN	14
+#define SW1_GPIO_NAME	CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+/* Push button switch 2 */
+#define SW2_GPIO_PIN	15
+#define SW2_GPIO_NAME	CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+/* Push button switch 3 */
+#define SW3_GPIO_PIN	16
+#define SW3_GPIO_NAME	CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+/* Onboard GREEN LED 0 */
+#define LED0_GPIO_PIN   17
+#define LED0_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+/* Onboard GREEN LED 1 */
+#define LED1_GPIO_PIN   18
+#define LED1_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+/* Onboard GREEN LED 2 */
+#define LED2_GPIO_PIN   19
+#define LED2_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+/* Onboard GREEN LED 3 */
+#define LED3_GPIO_PIN   20
+#define LED3_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
 
 #endif /* __INC_BOARD_H */
