c4fa607499e598a7ed06655f02d96e688d8e7bca
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2016-12-09 18:19:15 +0100

    Bluetooth: Fix format specifier in UUID DBG helpers
    Replace precision formatter (.) in printk with padding flag like %04x
    since for now precision is not handled properly in printk family.
    
    Change-Id: Ib63198e407ef584c5650d6452518b1767047630f
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>

--- subsys/bluetooth/host/uuid.c
+++ subsys/bluetooth/host/uuid.c
@@ -101,10 +101,10 @@ void bt_uuid_to_str(const struct bt_uuid *uuid, char *str, size_t len)
 
 	switch (uuid->type) {
 	case BT_UUID_TYPE_16:
-		snprintk(str, len, "%.4x", BT_UUID_16(uuid)->val);
+		snprintk(str, len, "%04x", BT_UUID_16(uuid)->val);
 		break;
 	case BT_UUID_TYPE_32:
-		snprintk(str, len, "%.4x", BT_UUID_32(uuid)->val);
+		snprintk(str, len, "%04x", BT_UUID_32(uuid)->val);
 		break;
 	case BT_UUID_TYPE_128:
 		memcpy(&tmp0, &BT_UUID_128(uuid)->val[0], sizeof(tmp0));
@@ -114,7 +114,7 @@ void bt_uuid_to_str(const struct bt_uuid *uuid, char *str, size_t len)
 		memcpy(&tmp4, &BT_UUID_128(uuid)->val[10], sizeof(tmp4));
 		memcpy(&tmp5, &BT_UUID_128(uuid)->val[12], sizeof(tmp5));
 
-		snprintk(str, len, "%.8x-%.4x-%.4x-%.4x-%.8x%.4x",
+		snprintk(str, len, "%08x-%04x-%04x-%04x-%.8x%04x",
 			 tmp5, tmp4, tmp3, tmp2, tmp1, tmp0);
 		break;
 	default:
