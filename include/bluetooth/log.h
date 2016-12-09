fb37a5c330444330c0bddb0b1cd30c71f6d83e2d
Johan Hedberg <johan.hedberg@intel.com>
2016-12-09 14:02:28 +0200

    Bluetooth: Fix stack overhead amount when debug is enabled
    After some more careful measurements, the worst measured overhead is
    288 bytes. Adjust BT_STACK_DEBUG_EXTRA correspondingly to 300 (to give
    a bit of playroom still on top of this).
    
    Change-Id: Icdf477b05b40917027314b180c2b69c8c6c759b3
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- include/bluetooth/log.h
+++ include/bluetooth/log.h
@@ -48,7 +48,7 @@ void bt_log(int prio, const char *fmt, ...);
 #define BT_INFO(fmt, ...) bt_log(BT_LOG_INFO, fmt, ##__VA_ARGS__)
 
 /* Enabling debug increases stack size requirement */
-#define BT_STACK_DEBUG_EXTRA	160
+#define BT_STACK_DEBUG_EXTRA	300
 
 #elif defined(CONFIG_BLUETOOTH_DEBUG_LOG)
 
@@ -63,7 +63,7 @@ void bt_log(int prio, const char *fmt, ...);
 #define BT_INFO(fmt, ...) SYS_LOG_INF(fmt, ##__VA_ARGS__)
 
 /* Enabling debug increases stack size requirement considerably */
-#define BT_STACK_DEBUG_EXTRA	160
+#define BT_STACK_DEBUG_EXTRA	300
 
 #else
 
