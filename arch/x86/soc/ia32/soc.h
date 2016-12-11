59c8f32f0f8c22c41d276d00c350902ae903161a
Flavio Santes <flavio.santes@intel.com>
2016-12-11 00:20:19 -0600

    x86/soc: Add ARG_UNUSED macro to avoid compiler warnings
    The ARG_UNUSED macro is added to avoid compiler warnings.
    
    Change-Id: If0242548849ee5b258bb3fce9fd727b377411343
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>

--- arch/x86/soc/ia32/soc.h
+++ arch/x86/soc/ia32/soc.h
@@ -76,6 +76,8 @@
 
 static inline int pci_pin2irq(int bus, int dev, int pin)
 {
+	ARG_UNUSED(bus);
+
 	if ((pin < PCI_INTA) || (pin > PCI_INTD))
 		return -1;
 	return 10 + (((pin + dev - 1) >> 1) & 1);
