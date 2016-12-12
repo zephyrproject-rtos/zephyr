bd40ca87413aec0b0556e460b5d3feb999fed497
Maureen Helm <maureen.helm@nxp.com>
2016-12-12 11:15:27 -0600

    frdm_k64f: hexiwear_k64: Add defaults for the mcux gpio driver
    Jira: ZEP-1394
    Change-Id: I1371e5a1a65dd85a8dd93b2c5d113d7520d5656f
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- boards/arm/frdm_k64f/board.h
+++ boards/arm/frdm_k64f/board.h
@@ -19,6 +19,8 @@
 
 #include <soc.h>
 
+#ifdef CONFIG_GPIO_K64
+
 /* Push button switch 2 */
 #define SW2_GPIO_NAME	CONFIG_GPIO_K64_C_DEV_NAME
 #define SW2_GPIO_PIN	6
@@ -38,6 +40,32 @@
 /* Blue LED */
 #define BLUE_GPIO_NAME	CONFIG_GPIO_K64_B_DEV_NAME
 #define BLUE_GPIO_PIN	21
+
+#endif /* CONFIG_GPIO_K64 */
+
+#ifdef CONFIG_GPIO_MCUX
+
+/* Push button switch 2 */
+#define SW2_GPIO_NAME	CONFIG_GPIO_MCUX_PORTC_NAME
+#define SW2_GPIO_PIN	6
+
+/* Push button switch 3 */
+#define SW3_GPIO_NAME	CONFIG_GPIO_MCUX_PORTA_NAME
+#define SW3_GPIO_PIN	4
+
+/* Red LED */
+#define RED_GPIO_NAME	CONFIG_GPIO_MCUX_PORTB_NAME
+#define RED_GPIO_PIN	22
+
+/* Green LED */
+#define GREEN_GPIO_NAME	CONFIG_GPIO_MCUX_PORTE_NAME
+#define GREEN_GPIO_PIN	26
+
+/* Blue LED */
+#define BLUE_GPIO_NAME	CONFIG_GPIO_MCUX_PORTB_NAME
+#define BLUE_GPIO_PIN	21
+
+#endif /* CONFIG_GPIO_MCUX */
 
 /* LED0. There is no physical LED on the board with this name, so create an
  * alias to the green LED to make the basic blinky sample work.
