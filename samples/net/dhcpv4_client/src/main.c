0637511e68c0edcf213fc4b8e5810e57d16d6b9e
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-12-12 15:41:31 +0000

    dhcpv4: Report address acquisition.
    Use the network management interface to catch and report IPv4 address
    acquisition in the application.
    
    Change-Id: Idd1bcaac90fffbd617600a7713206599e547826f
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>

--- samples/net/dhcpv4_client/src/main.c
+++ samples/net/dhcpv4_client/src/main.c
@@ -31,12 +31,25 @@
 #include <net/net_if.h>
 #include <net/net_core.h>
 #include <net/net_context.h>
+#include <net/net_mgmt.h>
 
 static struct k_sem quit_lock;
 
 static inline void quit(void)
 {
 	k_sem_give(&quit_lock);
+}
+
+static struct net_mgmt_event_callback mgmt_cb;
+
+static void handler(struct net_mgmt_event_callback *cb,
+		    uint32_t mgmt_event,
+		    struct net_if *iface)
+{
+	if (mgmt_event == NET_EVENT_IPV4_ADDR_ADD) {
+		NET_INFO("NET_EVENT_IPV4_ADDR_ADD");
+		quit();
+	}
 }
 
 static inline void init_app(void)
@@ -50,6 +63,10 @@ static inline void init_app(void)
 	net_dhcpv4_start(iface);
 
 	k_sem_init(&quit_lock, 0, UINT_MAX);
+
+	net_mgmt_init_event_callback(&mgmt_cb, handler,
+				     NET_EVENT_IPV4_ADDR_ADD);
+	net_mgmt_add_event_callback(&mgmt_cb);
 }
 
 void main_fiber(void)
@@ -61,8 +78,6 @@ void main_fiber(void)
 void main(void)
 {
 	NET_INFO("In main");
-
-	init_app();
 
 	main_fiber();
 }
