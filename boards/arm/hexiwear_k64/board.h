d73ee7e37dbccbf3b576767691169102ace88a2b
Maureen Helm <maureen.helm@nxp.com>
2016-12-13 08:34:45 -0600

    frdm_k64f: hexiwear_k64: Fix basic blinky sample
    The basic blinky sample expects LED0_GPIO_PORT and LED0_GPIO_PIN macros
    to be defined by board.h. The frdm_k64f and hexiwear_k64 boards don't
    have a physical LED with this name, so create an alias to the green LED
    to make the sample work.
    
    Change-Id: I3426c571bb2e3165dbd9f4372d863b474c35fd5c
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- boards/arm/hexiwear_k64/board.h
+++ boards/arm/hexiwear_k64/board.h
@@ -31,4 +31,10 @@
 #define BLUE_GPIO_NAME		CONFIG_GPIO_K64_C_DEV_NAME
 #define BLUE_GPIO_PIN		9
 
+/* LED0. There is no physical LED on the board with this name, so create an
+ * alias to the green LED to make the basic blinky sample work.
+ */
+#define LED0_GPIO_PORT	GREEN_GPIO_NAME
+#define LED0_GPIO_PIN	GREEN_GPIO_PIN
+
 #endif /* __INC_BOARD_H */
