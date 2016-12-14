8b146d48d71f62d7e0008f48edbdf5482aba8c15
Baohong Liu <baohong.liu@intel.com>
2016-12-14 11:57:55 -0800

    samples: bmi160: replace printf with printk
    Change-Id: If1d05242d271ea4f8e4b60de8530984341f43d43
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>

--- samples/sensor/bmi160/src/bmi160.c
+++ samples/sensor/bmi160/src/bmi160.c
@@ -18,7 +18,7 @@
 #include <stdio.h>
 #include <device.h>
 #include <sensor.h>
-#include <misc/util.h>
+#include <misc/printk.h>
 
 #define MAX_TEST_TIME	15000
 #define SLEEPTIME	300
@@ -171,7 +171,7 @@ static void print_gyro_data(struct device *bmi160)
 	char buf_x[18], buf_y[18], buf_z[18];
 
 	if (sensor_channel_get(bmi160, SENSOR_CHAN_GYRO_ANY, val) < 0) {
-		printf("Cannot read bmi160 gyro channels.\n");
+		printk("Cannot read bmi160 gyro channels.\n");
 		return;
 	}
 
@@ -179,7 +179,7 @@ static void print_gyro_data(struct device *bmi160)
 	sensor_value_snprintf(buf_y, sizeof(buf_y), &val[1]);
 	sensor_value_snprintf(buf_z, sizeof(buf_z), &val[2]);
 
-	printf("Gyro (rad/s): X=%s, Y=%s, Z=%s\n", buf_x, buf_y, buf_z);
+	printk("Gyro (rad/s): X=%s, Y=%s, Z=%s\n", buf_x, buf_y, buf_z);
 }
 #endif
 
@@ -191,7 +191,7 @@ static void print_accel_data(struct device *bmi160)
 
 	if (sensor_channel_get(bmi160,
 			       SENSOR_CHAN_ACCEL_ANY, val) < 0) {
-		printf("Cannot read bmi160 accel channels.\n");
+		printk("Cannot read bmi160 accel channels.\n");
 		return;
 	}
 
@@ -199,7 +199,7 @@ static void print_accel_data(struct device *bmi160)
 	sensor_value_snprintf(buf_y, sizeof(buf_y), &val[1]);
 	sensor_value_snprintf(buf_z, sizeof(buf_z), &val[2]);
 
-	printf("Acc (m/s^2): X=%s, Y=%s, Z=%s\n", buf_x, buf_y, buf_z);
+	printk("Acc (m/s^2): X=%s, Y=%s, Z=%s\n", buf_x, buf_y, buf_z);
 }
 #endif
 
@@ -209,13 +209,13 @@ static void print_temp_data(struct device *bmi160)
 	char buf[18];
 
 	if (sensor_channel_get(bmi160, SENSOR_CHAN_TEMP, &val) < 0) {
-		printf("Temperature channel read error.\n");
+		printk("Temperature channel read error.\n");
 		return;
 	}
 
 	sensor_value_snprintf(buf, sizeof(buf), &val);
 
-	printf("Temperature (Celsius): %s\n", buf);
+	printk("Temperature (Celsius): %s\n", buf);
 }
 
 static void test_polling_mode(struct device *bmi160)
@@ -231,7 +231,7 @@ static void test_polling_mode(struct device *bmi160)
 
 	if (sensor_attr_set(bmi160, SENSOR_CHAN_ACCEL_ANY,
 			    SENSOR_ATTR_SAMPLING_FREQUENCY, &attr) < 0) {
-		printf("Cannot set sampling frequency for accelerometer.\n");
+		printk("Cannot set sampling frequency for accelerometer.\n");
 		return;
 	}
 #endif
@@ -244,7 +244,7 @@ static void test_polling_mode(struct device *bmi160)
 
 	if (sensor_attr_set(bmi160, SENSOR_CHAN_GYRO_ANY,
 			    SENSOR_ATTR_SAMPLING_FREQUENCY, &attr) < 0) {
-		printf("Cannot set sampling frequency for gyroscope.\n");
+		printk("Cannot set sampling frequency for gyroscope.\n");
 		return;
 	}
 #endif
@@ -252,7 +252,7 @@ static void test_polling_mode(struct device *bmi160)
 	/* poll the data and print it */
 	do {
 		if (sensor_sample_fetch(bmi160) < 0) {
-			printf("Sample update error.\n");
+			printk("Sample update error.\n");
 			return;
 		}
 
@@ -281,7 +281,7 @@ static void trigger_hdlr(struct device *bmi160,
 	}
 
 	if (sensor_sample_fetch(bmi160) < 0) {
-		printf("Sample update error.\n");
+		printk("Sample update error.\n");
 		return;
 	}
 
@@ -317,7 +317,7 @@ static void test_anymotion_trigger(struct device *bmi160)
 	attr.val2 = 980665;
 	if (sensor_attr_set(bmi160, SENSOR_CHAN_ACCEL_ANY,
 			    SENSOR_ATTR_SLOPE_TH, &attr) < 0) {
-		printf("Cannot set anymotion slope threshold.\n");
+		printk("Cannot set anymotion slope threshold.\n");
 		return;
 	}
 
@@ -332,7 +332,7 @@ static void test_anymotion_trigger(struct device *bmi160)
 	attr.val2 = 0;
 	if (sensor_attr_set(bmi160, SENSOR_CHAN_ACCEL_ANY,
 			    SENSOR_ATTR_SLOPE_DUR, &attr) < 0) {
-		printf("Cannot set anymotion slope duration.\n");
+		printk("Cannot set anymotion slope duration.\n");
 		return;
 	}
 
@@ -341,11 +341,11 @@ static void test_anymotion_trigger(struct device *bmi160)
 	trig.chan = SENSOR_CHAN_ACCEL_ANY;
 
 	if (sensor_trigger_set(bmi160, &trig, trigger_hdlr) < 0) {
-		printf("Cannot enable anymotion trigger.\n");
+		printk("Cannot enable anymotion trigger.\n");
 		return;
 	}
 
-	printf("Anymotion test: shake the device to get anymotion events.\n");
+	printk("Anymotion test: shake the device to get anymotion events.\n");
 	do {
 		/* wait a while */
 		k_sleep(SLEEPTIME);
@@ -353,10 +353,10 @@ static void test_anymotion_trigger(struct device *bmi160)
 
 	} while (remaining_test_time > 0);
 
-	printf("Anymotion test: finished, removing anymotion trigger...\n");
+	printk("Anymotion test: finished, removing anymotion trigger...\n");
 
 	if (sensor_trigger_set(bmi160, &trig, NULL) < 0) {
-		printf("Cannot remove anymotion trigger.\n");
+		printk("Cannot remove anymotion trigger.\n");
 		return;
 	}
 }
@@ -371,11 +371,11 @@ static void test_data_ready_trigger(struct device *bmi160)
 	trig.chan = SENSOR_CHAN_ACCEL_ANY;
 
 	if (sensor_trigger_set(bmi160, &trig, trigger_hdlr) < 0) {
-		printf("Cannot enable data ready trigger.\n");
+		printk("Cannot enable data ready trigger.\n");
 		return;
 	}
 
-	printf("Data ready test:\n");
+	printk("Data ready test:\n");
 	do {
 		/* wait a while */
 		k_sleep(SLEEPTIME);
@@ -383,10 +383,10 @@ static void test_data_ready_trigger(struct device *bmi160)
 
 	} while (remaining_test_time > 0);
 
-	printf("Data ready test: finished, removing data ready trigger...\n");
+	printk("Data ready test: finished, removing data ready trigger...\n");
 
 	if (sensor_trigger_set(bmi160, &trig, NULL) < 0) {
-		printf("Cannot remove data ready trigger.\n");
+		printk("Cannot remove data ready trigger.\n");
 		return;
 	}
 }
@@ -403,7 +403,7 @@ static void test_trigger_mode(struct device *bmi160)
 
 	if (sensor_attr_set(bmi160, SENSOR_CHAN_ACCEL_ANY,
 			    SENSOR_ATTR_SAMPLING_FREQUENCY, &attr) < 0) {
-		printf("Cannot set sampling frequency for accelerometer.\n");
+		printk("Cannot set sampling frequency for accelerometer.\n");
 		return;
 	}
 #endif
@@ -416,7 +416,7 @@ static void test_trigger_mode(struct device *bmi160)
 
 	if (sensor_attr_set(bmi160, SENSOR_CHAN_GYRO_ANY,
 			    SENSOR_ATTR_SAMPLING_FREQUENCY, &attr) < 0) {
-		printf("Cannot set sampling frequency for gyroscope.\n");
+		printk("Cannot set sampling frequency for gyroscope.\n");
 		return;
 	}
 #endif
@@ -437,10 +437,10 @@ void main(void)
 	struct sensor_value attr;
 #endif
 
-	printf("IMU: Binding...\n");
+	printk("IMU: Binding...\n");
 	bmi160 = device_get_binding("bmi160");
 	if (!bmi160) {
-		printf("Gyro: Device not found.\n");
+		printk("Gyro: Device not found.\n");
 		return;
 	}
 
@@ -453,7 +453,7 @@ void main(void)
 
 	if (sensor_attr_set(bmi160, SENSOR_CHAN_ACCEL_ANY,
 			    SENSOR_ATTR_FULL_SCALE, &attr) < 0) {
-		printf("Cannot set accelerometer range.\n");
+		printk("Cannot set accelerometer range.\n");
 		return;
 	}
 #endif
@@ -467,7 +467,7 @@ void main(void)
 
 	if (sensor_attr_set(bmi160, SENSOR_CHAN_GYRO_ANY,
 			    SENSOR_ATTR_FULL_SCALE, &attr) < 0) {
-		printf("Cannot set gyro range.\n");
+		printk("Cannot set gyro range.\n");
 		return;
 	}
 #endif
@@ -475,7 +475,7 @@ void main(void)
 #ifdef PERFORM_MANUAL_CALIBRATION
 	/* manually adjust accelerometer and gyro offsets */
 	if (manual_calibration(bmi160) < 0) {
-		printf("Manual calibration failed.\n");
+		printk("Manual calibration failed.\n");
 		return;
 	}
 #endif
@@ -483,18 +483,18 @@ void main(void)
 #ifdef PERFORM_AUTO_CALIBRATION
 	/* auto calibrate accelerometer and gyro */
 	if (auto_calibration(bmi160) < 0) {
-		printf("HW calibration failed.\n");
+		printk("HW calibration failed.\n");
 		return;
 	}
 #endif
 
-	printf("Testing the polling mode.\n");
+	printk("Testing the polling mode.\n");
 	test_polling_mode(bmi160);
-	printf("Testing the polling mode finished.\n");
+	printk("Testing the polling mode finished.\n");
 
 #ifdef CONFIG_BMI160_TRIGGER
-	printf("Testing the trigger mode.\n");
+	printk("Testing the trigger mode.\n");
 	test_trigger_mode(bmi160);
-	printf("Testing the trigger mode finished.\n");
+	printk("Testing the trigger mode finished.\n");
 #endif
 }
