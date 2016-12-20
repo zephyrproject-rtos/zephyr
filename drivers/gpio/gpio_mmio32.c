e7391e3068428ecc616e7f0748b0810082c258ed
Jon Medhurst <tixy@linaro.org>
2016-12-20 16:55:41 +0000

    gpio: Add gpio_mmio32 driver to access basic 32-bit i/o registers
    It is envisaged that this will be used by SoC or board code to make
    available fixed purpose memory-mapped i/o registers to the rest of the
    system which normally expects to use GPIO devices, e.g. for driving chip
    select lines, LEDs or reading button states.
    
    As such, the driver code doesn't provide a kconfig based configuration
    mechanism, instead SoC/board code can hard-wire the devices it wants
    with something simple like:
    
    GPIO_MMIO32_INIT(misc_reg1, "MISC1", 0x12345678, 0xffffffffu)
    
    Then, for example, if bit N of the register at 0x12345678 is wired up as
    an SPI device chip select line, the SPI driver could be configured to
    use pin N of the "MISC1" GPIO driver and not need any other board
    specific code.
    
    Change-Id: Ib02fcbab73fcf9637e25834db060fb3108626f47
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

---
+++ drivers/gpio/gpio_mmio32.c
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2016 Linaro Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Driver to provide the GPIO API for a simple 32-bit i/o register
+ *
+ * This is a driver for accessing a simple, fixed purpose, 32-bit
+ * memory-mapped i/o register using the same APIs as GPIO drivers. This is
+ * useful when an SoC or board has registers that aren't part of a GPIO IP
+ * block and these registers are used to control things that Zephyr normaly
+ * expects to be specified using a GPIO pin, e.g. for driving an LED, or
+ * chip-select line for an SPI device.
+ *
+ * The implementation expects that all bits of the hardware register are both
+ * readable and writeable, and that for any bits that act as outputs, the value
+ * read will have the value that was last written to it. This requirement
+ * stems from the use of a read-modify-write method for all changes.
+ *
+ * It is possible to specify a restricted mask of bits that are valid for
+ * access, and whenever the register is written, the value of bits outside this
+ * mask will be preserved, even when the whole port is written to using
+ * gpio_port_write.
+ */
+
+#include <gpio/gpio_mmio32.h>
+#include <errno.h>
+
+static int gpio_mmio32_config(struct device *dev, int access_op,
+					uint32_t pin, int flags)
+{
+	struct gpio_mmio32_context *context = dev->driver_data;
+	const struct gpio_mmio32_config *config = context->config;
+
+	if (access_op != GPIO_ACCESS_BY_PIN) {
+		return -ENOTSUP;
+	}
+
+	if ((config->mask & (1 << pin)) == 0) {
+		return -EINVAL; /* Pin not in our validity mask */
+	}
+
+	if (flags & ~(GPIO_DIR_MASK | GPIO_POL_MASK)) {
+		/* We ignore direction and fake polarity, rest is unsupported */
+		return -ENOTSUP;
+	}
+
+	if ((flags & GPIO_POL_MASK) == GPIO_POL_INV) {
+		context->invert |= (1 << pin);
+	} else {
+		context->invert &= ~(1 << pin);
+	}
+
+	return 0;
+}
+
+static int gpio_mmio32_write(struct device *dev, int access_op,
+					uint32_t pin, uint32_t value)
+{
+	struct gpio_mmio32_context *context = dev->driver_data;
+	const struct gpio_mmio32_config *config = context->config;
+	volatile uint32_t *reg = config->reg;
+	uint32_t mask = config->mask;
+	uint32_t invert = context->invert;
+	unsigned int key;
+
+	if (access_op == GPIO_ACCESS_BY_PIN) {
+		mask &= 1 << pin;
+		if (!mask) {
+			return -EINVAL; /* Pin not in our validity mask */
+		}
+		value = value ? mask : 0;
+	}
+
+	value = (value ^ invert) & mask;
+
+	/* Update pin state atomically */
+	key = irq_lock();
+	*reg = (*reg & ~mask) | value;
+	irq_unlock(key);
+
+	return 0;
+}
+
+static int gpio_mmio32_read(struct device *dev, int access_op,
+					uint32_t pin, uint32_t *value)
+{
+	struct gpio_mmio32_context *context = dev->driver_data;
+	const struct gpio_mmio32_config *config = context->config;
+	uint32_t bits;
+
+	bits = (*config->reg ^ context->invert) & config->mask;
+	if (access_op == GPIO_ACCESS_BY_PIN) {
+		*value = (bits >> pin) & 1;
+		if ((config->mask & (1 << pin)) == 0) {
+			return -EINVAL; /* Pin not in our validity mask */
+		}
+	} else {
+		*value = bits;
+	}
+
+	return 0;
+}
+
+static const struct gpio_driver_api gpio_mmio32_api = {
+	.config = gpio_mmio32_config,
+	.write = gpio_mmio32_write,
+	.read = gpio_mmio32_read,
+};
+
+int gpio_mmio32_init(struct device *dev)
+{
+	struct gpio_mmio32_context *context = dev->driver_data;
+	const struct gpio_mmio32_config *config = dev->config->config_info;
+
+	context->config = config;
+	dev->driver_api = &gpio_mmio32_api;
+
+	return 0;
+}
---
+++ include/drivers/gpio/gpio_mmio32.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2016 Linaro Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _GPIO_MMIO32_H_
+#define _GPIO_MMIO32_H_
+
+#include <device.h>
+#include <gpio.h>
+#include <stdint.h>
+
+struct gpio_mmio32_config {
+	volatile uint32_t *reg;
+	uint32_t mask;
+};
+
+struct gpio_mmio32_context {
+	const struct gpio_mmio32_config *config;
+	uint32_t invert; /* Mask of 'reg' bits that should be inverted */
+};
+
+int gpio_mmio32_init(struct device *dev);
+
+#ifdef CONFIG_GPIO_MMIO32
+
+/**
+ * Create a device object for accessing a simple 32-bit i/o register using the
+ * same APIs as GPIO drivers.
+ *
+ * @param _dev_name Device name.
+ * @param _drv_name The name this instance of the driver exposes to the system.
+ * @param _address  The address of the 32-bit i/o register the device will
+ *		    provide access to.
+ * @param _mask     Mask of bits in the register that it is valid to access.
+ *		    E.g. 0xffffffffu to allow access to all of them.
+ *
+ * @see DEVICE_INIT
+ */
+#define GPIO_MMIO32_INIT(_dev_name, _drv_name, _address, _mask)		\
+									\
+static struct gpio_mmio32_context _dev_name##_dev_data;			\
+									\
+static const struct gpio_mmio32_config _dev_name##_dev_cfg = {		\
+	.reg	= (volatile uint32_t *)_address,			\
+	.mask	= _mask,						\
+};									\
+									\
+DEVICE_INIT(_dev_name, _drv_name,					\
+	&gpio_mmio32_init,						\
+	&_dev_name##_dev_data,						\
+	&_dev_name##_dev_cfg,						\
+	PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEVICE)
+
+#else /* CONFIG_GPIO_MMIO32 */
+
+/* Null definition for when support not configured into kernel */
+#define GPIO_MMIO32_INIT(_dev_name, _drv_name, _address, _mask)
+
+#endif
+
+
+#endif /* _GPIO_MMIO32_H_ */
