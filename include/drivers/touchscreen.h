7f3f374e0df667e02e5ef9d995a241ab8aa00133
Jon Medhurst <tixy@linaro.org>
2016-12-22 14:22:52 +0000

    touchscreen: Add library for translating coordinates
    A touchscreen returns X,Y values that bare no direct relationship to
    the coordinates used by a graphical display. This provides functions
    for translating between the two coordinate systems.
    
    Change-Id: I8789eca0ffcc1e3812076b373e8208ade980b3c9
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- include/drivers/touchscreen.h
+++ include/drivers/touchscreen.h
@@ -87,4 +87,63 @@ static inline void touchscreen_set_callback(struct device *dev,
 	api->set_callback(dev, callback);
 }
 
+
+#ifdef CONFIG_TOUCHSCREEN_HELPERS
+
+/** X,Y coordinates for a point on the touchscreen or display. */
+struct touchscreen_point {
+	u16_t x;
+	u16_t y;
+};
+
+/**
+ * @brief Touchscreen coordinate translation parameters
+ *
+ * These are initialised by calling touchscreen_set_calibration().
+ * Members should be considered private and not accessed directly.
+ */
+struct touchscreen_xlat {
+	s64_t	A;
+	s64_t	B;
+	s64_t	C;
+	s64_t	D;
+	s64_t	E;
+	s64_t	F;
+};
+
+/**
+ * @brief Initialise touchscreen coordinate translation parameters.
+ *
+ * This requires that three different and valid display/touchscreen coordinate
+ * pairs are know.
+ *
+ * @param xlat		Translation parameters to be initialised.
+ * @param display	Three display coordinates that correspond to the given
+ *			touchscreen coordinates.
+ * @param touchscreen	Three touchscreen coordinates that correspond to the
+ *			given display coordinates.
+ */
+void touchscreen_set_calibration(struct touchscreen_xlat *xlat,
+				 const struct touchscreen_point display[3],
+				 const struct touchscreen_point touchscreen[3]);
+
+/**
+ * @brief Translate touchscreen coordinates into screen coordinates.
+ *
+ * Convert a touchscreen X,Y sample into the corresponding display X,Y
+ * coordinates by using the previously initialised translation parameters.
+ *
+ * @param xlat		Translation parameters.
+ * @param dislpay_x	Display X coordinate
+ * @param dislpay_x	Display Y coordinate
+ * @param touch_x	Touchscreen X sample
+ * @param touch_y	Touchscreen Y sample
+ */
+void touchscreen_translate(struct touchscreen_xlat *xlat,
+			   int *display_x, int *display_y,
+			   int touch_x, int touch_y);
+
+#endif /* CONFIG_TOUCHSCREEN_HELPERS */
+
+
 #endif /* _TOUCHSCREEN_H_ */
