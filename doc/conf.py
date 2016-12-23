55cc5b6358c0d4b868eb3a873082dc234355987a
Anas Nashif <anas.nashif@intel.com>
2016-12-23 12:27:57 -0500

    doc: use ReadTheDocs.org theme if available
    Instead of including the rtd theme in zephyr use an installed instance,
    if nothing is installed, use the default zephyr theme.
    
    Change-Id: Ife4bd878e3f879cf59ecf2bc5d186a531a3bf1b6
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- doc/conf.py
+++ doc/conf.py
@@ -112,18 +112,14 @@ rst_epilog = """
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#html_theme = 'alabaster'
-html_theme = 'zephyr'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
-
-# Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['./themes']
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    html_theme = 'zephyr'
+    html_theme_path = ['./themes']
+else:
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 if tags.has('release'):
     docs_title = 'Docs / %s' %(version)
