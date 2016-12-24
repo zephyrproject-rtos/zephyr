36c5167b72339feb89936ccee7dbee001cbd7122
Anas Nashif <anas.nashif@intel.com>
2016-12-24 07:10:20 -0500

    shell: add tasks command to kernel module
    Change-Id: I67c497c04e3c92a7f55cc5d95d7d3cfea414b4d8
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- subsys/shell/modules/kernel_service.c
+++ subsys/shell/modules/kernel_service.c
@@ -17,6 +17,7 @@
 #include <misc/printk.h>
 #include <shell/shell.h>
 #include <init.h>
+#include <debug/object_tracing.h>
 
 #define SHELL_KERNEL "kernel"
 
@@ -54,11 +55,36 @@ static int shell_cmd_cycles(int argc, char *argv[])
 	return 0;
 }
 
+#if defined(CONFIG_OBJECT_TRACING) && defined(CONFIG_THREAD_MONITOR)
+static int shell_cmd_tasks(int argc, char *argv[])
+{
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+	struct k_thread *thread_list = NULL;
+
+	printk("tasks:\n");
+
+	thread_list   = (struct k_thread *)SYS_THREAD_MONITOR_HEAD;
+	while (thread_list != NULL) {
+		printk("%s%p:   flags: 0x%x priority: %d\n",
+		       (thread_list == k_current_get()) ? "*" : " ",
+		       thread_list,
+		       thread_list->base.execution_flags,
+		       k_thread_priority_get(thread_list));
+		thread_list = (struct k_thread *)SYS_THREAD_MONITOR_NEXT(thread_list);
+	}
+	return 0;
+}
+#endif
+
 
 struct shell_cmd kernel_commands[] = {
 	{ "version", shell_cmd_version, "show kernel version" },
 	{ "uptime", shell_cmd_uptime, "show system uptime in milliseconds" },
 	{ "cycles", shell_cmd_cycles, "show system hardware cycles" },
+#if defined(CONFIG_OBJECT_TRACING) && defined(CONFIG_THREAD_MONITOR)
+	{ "tasks", shell_cmd_tasks, "show running tasks" },
+#endif
 	{ NULL, NULL, NULL }
 };
 
