d5b5af07beb6b3a2e417106ca53b091a99ff5d84
Arun Jagadish <arun.jagadish@intel.com>
2016-12-26 14:45:29 +0530

    Bluetooth: AVDTP: Added params to AVDTP Request structure
    Added pointer to store the callback function
    from the application in the Request structure.
    
    Added userdata param, to be used to fill AVDTP resp.
    
    Change-Id: I8f3289545fdbbd91e4ed7f9983f4f4331d9b59a6
    Signed-off-by: Arun Jagadish <arun.jagadish@intel.com>

--- subsys/bluetooth/host/avdtp_internal.h
+++ subsys/bluetooth/host/avdtp_internal.h
@@ -123,17 +123,11 @@ struct bt_avdtp_ind_cb {
 	 */
 };
 
-struct bt_pending_req {
-	uint8_t signal_id;
-	uint8_t transaction_id;
-	struct k_delayed_work timeout_work;
-};
-
 /** @brief Global AVDTP session structure. */
 struct bt_avdtp {
 	struct bt_l2cap_br_chan br_chan;
 	struct bt_avdtp_stream *streams; /* List of AV streams */
-	struct bt_pending_req req;
+	struct bt_avdtp_req *req;
 };
 
 struct bt_avdtp_event_cb {
