7d6c139082ea9e0fe19c08c9a2d061072c4a4342
Adam Podogrocki <adam.podogrocki@rndity.com>
2017-01-06 17:31:45 +0100

    pinmux/stm32: default pin assignment for NUCLEO-F334R8 board
    Change-Id: Ib70471ceb2e4444d5826a5a17cbfc42161df78d2
    Signed-off-by: Adam Podogrocki <adam.podogrocki@rndity.com>

---
+++ drivers/pinmux/stm32/pinmux_board_nucleo_f334r8.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2017 RnDity Sp. z o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <kernel.h>
+#include <device.h>
+#include <init.h>
+#include <pinmux.h>
+#include <sys_io.h>
+#include "pinmux/pinmux.h"
+
+#include "pinmux_stm32.h"
+
+/* pin assignments for NUCLEO-F334RB board */
+static const struct pin_config pinconf[] = {
+#ifdef CONFIG_UART_STM32_PORT_1
+	{STM32_PIN_PA9,  STM32F3_PINMUX_FUNC_PA9_USART1_TX},
+	{STM32_PIN_PA10, STM32F3_PINMUX_FUNC_PA10_USART1_RX},
+#endif	/* CONFIG_UART_STM32_PORT_1 */
+#ifdef CONFIG_UART_STM32_PORT_2
+	{STM32_PIN_PA2, STM32F3_PINMUX_FUNC_PA2_USART2_TX},
+	{STM32_PIN_PA3, STM32F3_PINMUX_FUNC_PA3_USART2_RX},
+#endif	/* CONFIG_UART_STM32_PORT_2 */
+#ifdef CONFIG_UART_STM32_PORT_3
+	{STM32_PIN_PB10, STM32F3_PINMUX_FUNC_PB10_USART3_TX},
+	{STM32_PIN_PB11, STM32F3_PINMUX_FUNC_PB11_USART3_RX},
+#endif	/* CONFIG_UART_STM32_PORT_3 */
+#ifdef CONFIG_PWM_STM32_1
+	{STM32_PIN_PA8, STM32F3_PINMUX_FUNC_PA8_PWM1_CH1},
+#endif /* CONFIG_PWM_STM32_1 */
+};
+
+static int pinmux_stm32_init(struct device *port)
+{
+	ARG_UNUSED(port);
+
+	stm32_setup_pins(pinconf, ARRAY_SIZE(pinconf));
+
+	return 0;
+}
+
+SYS_INIT(pinmux_stm32_init, PRE_KERNEL_1,
+	 CONFIG_PINMUX_STM32_DEVICE_INITIALIZATION_PRIORITY);
