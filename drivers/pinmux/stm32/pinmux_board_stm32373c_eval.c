a0dcfcce9778abb6d2beb58d31e2a966c0bdd31b
Adam Podogrocki <adam.podogrocki@rndity.com>
2017-01-06 17:35:24 +0100

    pinmux/stm32: default pin assignment for STM32373C-EVAL board
    Change-Id: Id4b04749e1d707d12aa78cabd8d103701b6d55b6
    Signed-off-by: Adam Podogrocki <adam.podogrocki@rndity.com>

---
+++ drivers/pinmux/stm32/pinmux_board_stm32373c_eval.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2017 RnDity Sp. z o.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <nanokernel.h>
+#include <device.h>
+#include <init.h>
+#include <pinmux.h>
+#include <sys_io.h>
+#include "pinmux/pinmux.h"
+
+#include "pinmux_stm32.h"
+
+/* pin assignments for STM32373C-EVAL board */
+static const struct pin_config pinconf[] = {
+#ifdef CONFIG_UART_STM32_PORT_2
+	{STM32_PIN_PD5, STM32F3_PINMUX_FUNC_PD5_USART2_TX},
+	{STM32_PIN_PD6, STM32F3_PINMUX_FUNC_PD6_USART2_RX},
+#endif	/* CONFIG_UART_STM32_PORT_2 */
+};
+
+static int pinmux_stm32_init(struct device *port)
+{
+	ARG_UNUSED(port);
+
+	stm32_setup_pins(pinconf, ARRAY_SIZE(pinconf));
+
+	return 0;
+}
+
+SYS_INIT(pinmux_stm32_init, PRE_KERNEL_1,
+	 CONFIG_PINMUX_STM32_DEVICE_INITIALIZATION_PRIORITY);
