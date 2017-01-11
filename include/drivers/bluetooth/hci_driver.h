fb47e2f121a62bf95a66041a18448299edfff0de
Johan Hedberg <johan.hedberg@intel.com>
2017-01-11 16:15:55 +0200

    Bluetooth: Add missing documentation to HCI driver APIs
    Properly document the various APIs exposed by hci_driver.h.
    
    Change-Id: Ic8daba4956e4c5d2cc6597556b55ab5221495ad7
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- include/drivers/bluetooth/hci_driver.h
+++ include/drivers/bluetooth/hci_driver.h
@@ -35,17 +35,20 @@
 extern "C" {
 #endif
 
-/** Helper for the HCI driver to know which events are ok to be passed
-  * through the RX thread and which must be given to bt_recv() from another
-  * context. If this function returns true it's safe to pass the event
-  * through the RX thread, however if it returns false then this risks
-  * a deadlock.
-  *
-  * @param evt HCI event code.
-  *
-  * @return true if the event can be processed in the RX thread, false
-  *         if it cannot.
-  */
+/**
+ * @brief Check if an HCI event is high priority or not.
+ *
+ * Helper for the HCI driver to know which events are ok to be passed
+ * through the RX thread and which must be given to bt_recv_prio() from
+ * another context (e.g. ISR). If this function returns true it's safe
+ * to pass the event through the RX thread, however if it returns false
+ * then this risks a deadlock.
+ *
+ * @param evt HCI event code.
+ *
+ * @return true if the event can be processed in the RX thread, false
+ *         if it cannot.
+ */
 static inline bool bt_hci_evt_is_prio(uint8_t evt)
 {
 	switch (evt) {
@@ -60,12 +63,41 @@ static inline bool bt_hci_evt_is_prio(uint8_t evt)
 	}
 }
 
-/* Receive data from the controller/HCI driver */
+/**
+ * @brief Receive data from the controller/HCI driver.
+ *
+ * This is the main function through which the HCI driver provides the
+ * host with data from the controller. The buffer needs to have its type
+ * set with the help of bt_buf_set_type() before calling this API. This API
+ * should not be used for so-called high priority HCI events, which should
+ * instead be delivered to the host stack through bt_recv_prio().
+ *
+ * @param buf Network buffer containing data from the controller.
+ *
+ * @return 0 on success or negative error number on failure.
+ */
 int bt_recv(struct net_buf *buf);
 
-/* Receive priority event from the controller/HCI driver */
+/**
+ * @brief Receive high priority data from the controller/HCI driver.
+ *
+ * This is the same as bt_recv(), except that it should be used for
+ * so-called high priority HCI events. There's a separate
+ * bt_hci_evt_is_prio() helper that can be used to identify which events
+ * are high priority.
+ *
+ * As with bt_recv(), the buffer needs to have its type set with the help of
+ * bt_buf_set_type() before calling this API. The only exception is so called
+ * high priority HCI events which should be delivered to the host stack through
+ * bt_recv_prio() instead.
+ *
+ * @param buf Network buffer containing data from the controller.
+ *
+ * @return 0 on success or negative error number on failure.
+ */
 int bt_recv_prio(struct net_buf *buf);
 
+/** Possible values for the 'bus' member of the bt_hci_driver struct */
 enum bt_hci_driver_bus {
 	BT_HCI_DRIVER_BUS_VIRTUAL       = 0,
 	BT_HCI_DRIVER_BUS_USB           = 1,
@@ -78,24 +110,57 @@ enum bt_hci_driver_bus {
 	BT_HCI_DRIVER_BUS_I2C           = 8,
 };
 
+/**
+ * @brief Abstraction which represents the HCI transport to the controller.
+ *
+ * This struct is used to represent the HCI transport to the Bluetooth
+ * controller.
+ */
 struct bt_hci_driver {
-	/* Name of the driver */
+	/** Name of the driver */
 	const char *name;
 
-	/* Bus of the transport (BT_HCI_DRIVER_BUS_*) */
+	/** Bus of the transport (BT_HCI_DRIVER_BUS_*) */
 	enum bt_hci_driver_bus bus;
 
-	/* Open the HCI transport. If the driver uses its own RX thread,
-	 * i.e. CONFIG_BLUETOOTH_RECV_IS_RX_THREAD is set, then this
+	/**
+	 * @brief Open the HCI transport.
+	 *
+	 * Opens the HCI transport for operation. This function must not
+	 * return until the transport is ready for operation, meaning it
+	 * is safe to start calling the send() handler.
+	 *
+	 * If the driver uses its own RX thread, i.e.
+	 * CONFIG_BLUETOOTH_RECV_IS_RX_THREAD is set, then this
 	 * function is expected to start that thread.
+	 *
+	 * @return 0 on success or negative error number on failure.
 	 */
 	int (*open)(void);
 
-	/* Send HCI buffer to controller */
+	/**
+	 * @brief Send HCI buffer to controller.
+	 *
+	 * Send an HCI command or ACL data to the controller. The exact
+	 * type of the data can be checked with the help of bt_buf_get_type().
+	 *
+	 * @param buf Buffer containing data to be sent to the controller.
+	 *
+	 * @return 0 on success or negative error number on failure.
+	 */
 	int (*send)(struct net_buf *buf);
 };
 
-/* Register a new HCI driver to the Bluetooth stack */
+/**
+ * @brief Register a new HCI driver to the Bluetooth stack.
+ *
+ * This needs to be called before any application code runs. The bt_enable()
+ * API will fail if there is no driver registered.
+ *
+ * @param drv A bt_hci_driver struct representing the driver.
+ *
+ * @return 0 on success or negative error number on failure.
+ */
 int bt_hci_driver_register(struct bt_hci_driver *drv);
 
 #ifdef __cplusplus
