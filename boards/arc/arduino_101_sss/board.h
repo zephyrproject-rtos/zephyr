78adda30c980710204a5ae30af2084296e883976
Anas Nashif <anas.nashif@intel.com>
2017-01-21 09:48:48 -0500

    boards: arduino 101: set user LED values correctly
    Change-Id: Ie6b2c4c4c0ac6af5bc5a7d628599313ab971f56b
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- boards/arc/arduino_101_sss/board.h
+++ boards/arc/arduino_101_sss/board.h
@@ -9,7 +9,7 @@
 
 #include <soc.h>
 
-#define LED0_GPIO_PORT  CONFIG_GPIO_QMSI_1_NAME
-#define LED0_GPIO_PIN   5
+#define LED0_GPIO_PORT  CONFIG_GPIO_QMSI_0_NAME
+#define LED0_GPIO_PIN   8
 
 #endif /* __INC_BOARD_H */
