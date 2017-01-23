2618fcfb3c0e8d4287282711e86900547eb875d9
Michael Scott <michael.scott@linaro.org>
2017-01-23 15:25:59 -0800

    samples: net: irc_bot: add Linaro copyright
    Change-Id: Ic16902e6ce28765ffd8d9cb2f1699a33af19d1ae
    Signed-off-by: Michael Scott <michael.scott@linaro.org>

--- samples/net/irc_bot/src/irc-bot.c
+++ samples/net/irc_bot/src/irc-bot.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2017 Intel Corporation.
+ * Copyright (c) 2017 Linaro Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
