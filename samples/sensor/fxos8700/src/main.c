1938f5dadba48d9111798ca601b3b52b034acbc4
Maureen Helm <maureen.helm@nxp.com>
2017-01-25 15:36:51 -0600

    samples: fxos8700: Decimate sensor data before printing to the console
    At the default sample rate of 400 Hz, there is not enough bandwidth on
    the UART at 115200 baud to print every sample. Fix by decimating the
    sensor data by 4 before printing to the console.
    
    This issue was discovered after fixing ZEP-1507.
    
    Change-Id: I451ec9873ca93fe417d5d5a2d6d13b5e921c7611
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- samples/sensor/fxos8700/src/main.c
+++ samples/sensor/fxos8700/src/main.c
@@ -8,34 +8,40 @@
 #include <sensor.h>
 #include <stdio.h>
 
+#define DECIMATION_FACTOR	4
+
+K_SEM_DEFINE(sem, 0, 1);	/* starts off "not available" */
+
 static void trigger_handler(struct device *dev, struct sensor_trigger *trigger)
 {
-	struct sensor_value accel[3];
-	struct sensor_value magn[3];
-
+	static int decimator;
 	ARG_UNUSED(trigger);
 
+	/* Always fetch the sample to clear the data ready interrupt in the
+	 * sensor.
+	 */
 	if (sensor_sample_fetch(dev)) {
 		printf("sensor_sample_fetch failed\n");
 		return;
 	}
 
-	sensor_channel_get(dev, SENSOR_CHAN_ACCEL_ANY, accel);
-	sensor_channel_get(dev, SENSOR_CHAN_MAGN_ANY, magn);
+	/* Decimate the sensor data before printing to the console. There is
+	 * not enough bandwidth on the UART at 115200 baud to print every
+	 * sample.
+	 */
+	if (++decimator < DECIMATION_FACTOR) {
+		return;
+	}
 
-	/* Print accel x,y,z and mag x,y,z data */
-	printf("AX=%10.6f AY=%10.6f AZ=%10.6f "
-	       "MX=%10.6f MY=%10.6f MZ=%10.6f\n",
-	       sensor_value_to_double(&accel[0]),
-	       sensor_value_to_double(&accel[1]),
-	       sensor_value_to_double(&accel[2]),
-	       sensor_value_to_double(&magn[0]),
-	       sensor_value_to_double(&magn[1]),
-	       sensor_value_to_double(&magn[2]));
+	decimator = 0;
+
+	k_sem_give(&sem);
 }
 
 void main(void)
 {
+	struct sensor_value accel[3];
+	struct sensor_value magn[3];
 	struct device *dev = device_get_binding(CONFIG_FXOS8700_NAME);
 
 	if (dev == NULL) {
@@ -51,5 +57,22 @@ void main(void)
 	if (sensor_trigger_set(dev, &trig, trigger_handler)) {
 		printf("Could not set trigger\n");
 		return;
+	}
+
+	while (1) {
+		k_sem_take(&sem, K_FOREVER);
+
+		sensor_channel_get(dev, SENSOR_CHAN_ACCEL_ANY, accel);
+		sensor_channel_get(dev, SENSOR_CHAN_MAGN_ANY, magn);
+
+		/* Print accel x,y,z and mag x,y,z data */
+		printf("AX=%10.6f AY=%10.6f AZ=%10.6f "
+		       "MX=%10.6f MY=%10.6f MZ=%10.6f\n",
+		       sensor_value_to_double(&accel[0]),
+		       sensor_value_to_double(&accel[1]),
+		       sensor_value_to_double(&accel[2]),
+		       sensor_value_to_double(&magn[0]),
+		       sensor_value_to_double(&magn[1]),
+		       sensor_value_to_double(&magn[2]));
 	}
 }
