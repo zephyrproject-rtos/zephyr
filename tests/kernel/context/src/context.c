daf28fa339fc4a5733180001884cd2243523291e
Mazen NEIFER <mazen@nestwave.com>
2017-01-26 16:38:55 +0100

    Xtensa port: Fixed tests/kernel/context to compile with Xtensa internal timer.
    Change-Id: I4293adf23b7d46851747e3bcdd41b4a09e8fff05
    Signed-off-by: Mazen NEIFER <mazen@nestwave.com>

--- tests/kernel/context/src/context.c
+++ tests/kernel/context/src/context.c
@@ -51,6 +51,9 @@
 /* MVIC case */
 #define TICK_IRQ CONFIG_MVIC_TIMER_IRQ
 #endif
+#elif defined(CONFIG_XTENSA)
+#include <xtensa_timer.h>
+#define TICK_IRQ XT_TIMER_INTNUM
 #elif defined(CONFIG_ALTERA_AVALON_TIMER)
 #define TICK_IRQ TIMER_0_IRQ
 #elif defined(CONFIG_ARCV2_TIMER)
