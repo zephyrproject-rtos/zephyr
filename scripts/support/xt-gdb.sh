eb50958266d3626e79e801fb54b2c3ff3078213c
Andrew Boie <andrew.p.boie@intel.com>
2017-02-06 13:46:52 -0800

    xt-sim: add support for 'make debug'
    Change-Id: I60b64c6b8c83e1e1c49d8cb2c5f857faee88a7a0
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

---
+++ scripts/support/xt-gdb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+XT_GDB=$XTENSA_TOOLS_PATH/bin/xt-gdb
+ELF_NAME=${O}/${KERNEL_ELF_NAME}
+
+set -e
+
+do_debug() {
+    ${XT_GDB} ${ELF_NAME}
+}
+
+CMD="$1"
+shift
+
+case "${CMD}" in
+  debug)
+    do_debug "$@"
+    ;;
+  *)
+    echo "${CMD} not supported"
+    exit 1
+    ;;
+esac
