c99c686b2cbe9bbe87c44e131fae122c9478b951
Andrew Boie <andrew.p.boie@intel.com>
2017-02-09 14:37:32 -0800

    nios2: use gen_isr_tables mechanism
    Change-Id: If1ffcedf86a015789b42e7aec45dae3cc58f74fa
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- include/arch/nios2/arch.h
+++ include/arch/nios2/arch.h
@@ -74,13 +74,11 @@ typedef unsigned int vaddr_t;
  */
 #define _ARCH_IRQ_CONNECT(irq_p, priority_p, isr_p, isr_param_p, flags_p) \
 ({ \
-	enum { IRQ = irq_p }; \
-	static struct _isr_table_entry _CONCAT(_isr_irq, irq_p) \
-		__attribute__ ((used))  \
-		__attribute__ ((section(STRINGIFY(_CONCAT(.gnu.linkonce.isr_irq, irq_p))))) = \
-			{isr_param_p, isr_p}; \
+	_ISR_DECLARE(irq_p, 0, isr_p, isr_param_p); \
 	irq_p; \
 })
+
+extern void _irq_spurious(void *unused);
 
 static ALWAYS_INLINE unsigned int _arch_irq_lock(void)
 {
