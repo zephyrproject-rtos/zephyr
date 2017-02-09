ff0828faf38ece428aa36c3d4bd3ad7526255970
Mazen NEIFER <mazen@nestwave.com>
2017-02-09 23:05:30 +0100

    Xtensa port: Fixed compilation errors caused by last rebase on master.
    Change-Id: If2896bcce26652ded2059a11620370e34f98d1e4
    Signed-off-by: Mazen NEIFER <mazen@nestwave.com>

--- drivers/timer/xtensa_sys_timer.c
+++ drivers/timer/xtensa_sys_timer.c
@@ -94,7 +94,7 @@ void _timer_idle_enter(int32_t ticks)
 	uint32_t F; /* Idle timer fire time */
 	uint32_t f; /* Last programmed timer fire time */
 
-	if ((ticks == TICKS_UNLIMITED) || (ticks > max_system_ticks)) {
+	if ((ticks == K_FOREVER) || (ticks > max_system_ticks)) {
 		/*
 		 * The number of cycles until the timer must fire next might
 		 * not fit in the 32-bit counter register. To work around this,
