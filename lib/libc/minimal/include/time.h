f054d3e897db4aa92a0f4ee78afeba715961d0b5
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2017-02-13 17:39:38 -0800

    libc/include: Adding time.h
    Dummy time.h to fulfill the compilations requirements of certain
    libraries i.e. mbedTLS
    
    Change-Id: I07e66dbf07337b935dabe9eecdf1be3850bbf394
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>

---
+++ lib/libc/minimal/include/time.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+/* Dummy time.h to fulfill the requirements of certain libraries
+ * i.e. mbedTLS
+ */
