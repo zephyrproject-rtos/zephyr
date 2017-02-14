3abc1c5f2379ae4d853c2eae410c3a767181025e
Sharron LIU <sharron.liu@intel.com>
2017-02-14 14:39:35 +0800

    tests: kernel: added testapp profiling_api
    tests/kernel/profiling/profiling_api:
    added testapp to cover k_call_stack_analyze.
    
    Jira: ZEP-1242
    
    Change-Id: I65de70b2c4e5a294763d625b2a45c990f99f9f90
    Signed-off-by: Sharron LIU <sharron.liu@intel.com>

---
+++ tests/kernel/profiling/profiling_api/src/main.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ztest.h>
+
+extern void test_call_stacks_analyze_main(void);
+extern void test_call_stacks_analyze_idle(void);
+extern void test_call_stacks_analyze_workq(void);
+
+/*test case main entry*/
+void test_main(void *p1, void *p2, void *p3)
+{
+	ztest_test_suite(test_profiling_api,
+		ztest_unit_test(test_call_stacks_analyze_main),
+		ztest_unit_test(test_call_stacks_analyze_idle),
+		ztest_unit_test(test_call_stacks_analyze_workq));
+	ztest_run_test_suite(test_profiling_api);
+}
