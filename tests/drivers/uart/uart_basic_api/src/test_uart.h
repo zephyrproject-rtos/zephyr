34b53f181f30d9e81b5e426b07abc8f97d6d9a7b
Qiu Peiyang <peiyangx.qiu@intel.com>
2017-02-14 13:36:43 +0800

    tests: add zephyr uart driver api test case
    This commit verifies the following uart driver apis:
    	uart_irq_callback_set()
    	uart_irq_rx_enable()
    	uart_irq_rx_disable()
    	uart_irq_rx_ready()
    	uart_irq_tx_enable()
    	uart_irq_tx_disable()
    	uart_irq_tx_ready()
    	uart_fifo_fill()
    	uart_fifo_read()
    	uart_irq_update()
    	uart_poll_in()
    	uart_poll_out()
    
    Change-Id: I9be9341aee4357f86a2bc49f19733fb84273e89c
    Signed-off-by: Qiu Peiyang <peiyangx.qiu@intel.com>

---
+++ tests/drivers/uart/uart_basic_api/src/test_uart.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief UART cases header file
+ *
+ * Header file for UART cases
+ */
+
+#ifndef __TEST_UART_H__
+#define __TEST_UART_H__
+
+#include <uart.h>
+#include <ztest.h>
+
+#define UART_DEVICE_NAME CONFIG_UART_CONSOLE_ON_DEV_NAME
+
+void test_uart_poll_out(void);
+void test_uart_fifo_fill(void);
+void test_uart_fifo_read(void);
+void test_uart_poll_in(void);
+
+#endif /* __TEST_UART_H__ */
