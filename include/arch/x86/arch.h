86ad95135820522cbe67b9dcd04195fff61ca4cc
Andrew Boie <andrew.p.boie@intel.com>
2017-02-21 09:47:14 -0800

    x86: fix operand modifiers in IRQ_CONNECT()
    It's not entirely clear where the '%P' came from, but the proper,
    documented operand modifier for inserting an immediate value without
    punctuation is '%c'.
    
    Change-Id: I17346a6c70183ce79e91faa2fbe61e7761dfd513
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- include/arch/x86/arch.h
+++ include/arch/x86/arch.h
@@ -179,15 +179,15 @@ typedef struct s_isrList {
 ({ \
 	__asm__ __volatile__(							\
 		".pushsection .intList\n\t" \
-		".long %P[isr]_irq%P[irq]_stub\n\t"	/* ISR_LIST.fnc */ \
-		".long %P[irq]\n\t"		/* ISR_LIST.irq */ \
-		".long %P[priority]\n\t"	/* ISR_LIST.priority */ \
-		".long %P[vector]\n\t"		/* ISR_LIST.vec */ \
+		".long %c[isr]_irq%c[irq]_stub\n\t"	/* ISR_LIST.fnc */ \
+		".long %c[irq]\n\t"		/* ISR_LIST.irq */ \
+		".long %c[priority]\n\t"	/* ISR_LIST.priority */ \
+		".long %c[vector]\n\t"		/* ISR_LIST.vec */ \
 		".long 0\n\t"			/* ISR_LIST.dpl */ \
 		".popsection\n\t" \
 		".pushsection .text.irqstubs\n\t" \
-		".global %P[isr]_irq%P[irq]_stub\n\t" \
-		"%P[isr]_irq%P[irq]_stub:\n\t" \
+		".global %c[isr]_irq%c[irq]_stub\n\t" \
+		"%c[isr]_irq%c[irq]_stub:\n\t" \
 		"pushl %[isr_param]\n\t" \
 		"pushl %[isr]\n\t" \
 		"jmp _interrupt_enter\n\t" \
