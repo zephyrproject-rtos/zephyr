50cb5a6bd337a7be8c220726a879e669f1e3b367
Bogdan Davidoaia <bogdan.davidoaia@linaro.org>
2017-02-27 16:45:08 +0200

    random: mcux: rename random_mcux to random_mcux_rnga
    MCUX contains more than one type of random number generator,
    so refrect this in config and file names.
    
    Change-Id: Iba4482a1ae41f35d471686f8b159c113147c4df8
    Signed-off-by: Bogdan Davidoaia <bogdan.davidoaia@linaro.org>

--- drivers/random/random_mcux_rnga.c
+++ drivers/random/random_mcux_rnga.c
@@ -11,7 +11,7 @@
 
 #include "fsl_rnga.h"
 
-static uint8_t random_mcux_get_uint8(void)
+static uint8_t random_mcux_rnga_get_uint8(void)
 {
 	uint32_t random;
 	uint8_t output = 0;
@@ -36,32 +36,32 @@ static uint8_t random_mcux_get_uint8(void)
 	return output;
 }
 
-static int random_mcux_get_entropy(struct device *dev, uint8_t *buffer,
-				   uint16_t length)
+static int random_mcux_rnga_get_entropy(struct device *dev, uint8_t *buffer,
+					uint16_t length)
 {
 	int i;
 
 	ARG_UNUSED(dev);
 
 	for (i = 0; i < length; i++) {
-		buffer[i] = random_mcux_get_uint8();
+		buffer[i] = random_mcux_rnga_get_uint8();
 	}
 
 	return 0;
 }
 
-static const struct random_driver_api random_mcux_api_funcs = {
-	.get_entropy = random_mcux_get_entropy
+static const struct random_driver_api random_mcux_rnga_api_funcs = {
+	.get_entropy = random_mcux_rnga_get_entropy
 };
 
-static int random_mcux_init(struct device *);
+static int random_mcux_rnga_init(struct device *);
 
-DEVICE_AND_API_INIT(random_mcux, CONFIG_RANDOM_NAME,
-		    random_mcux_init, NULL, NULL,
+DEVICE_AND_API_INIT(random_mcux_rnga, CONFIG_RANDOM_NAME,
+		    random_mcux_rnga_init, NULL, NULL,
 		    PRE_KERNEL_2, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
-		    &random_mcux_api_funcs);
+		    &random_mcux_rnga_api_funcs);
 
-static int random_mcux_init(struct device *dev)
+static int random_mcux_rnga_init(struct device *dev)
 {
 	uint32_t seed = k_cycle_get_32();
 
@@ -85,8 +85,8 @@ uint32_t sys_rand32_get(void)
 	uint32_t output;
 	int r;
 
-	r = random_mcux_get_entropy(DEVICE_GET(random_mcux),
-				    (uint8_t *) &output, sizeof(output));
+	r = random_mcux_rnga_get_entropy(DEVICE_GET(random_mcux_rnga),
+					 (uint8_t *) &output, sizeof(output));
 	__ASSERT_NO_MSG(!r);
 
 	return output;
