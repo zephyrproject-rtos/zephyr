54739069937954d759ebc02bed32d08457c90a15
Bogdan Davidoaia <bogdan.davidoaia@linaro.org>
2017-02-28 13:24:43 +0200

    random: add MCUX TRNG driver
    Provide a random driver wrapped around the MCUX TRNG driver.
    
    Change-Id: Icbd7ab587aa18ecbd7eae52290aaa5d8ee504cf2
    Origin: Original
    Signed-off-by: Bogdan Davidoaia <bogdan.davidoaia@linaro.org>

---
+++ drivers/random/random_mcux_trng.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2017 Linaro Limited.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <device.h>
+#include <random.h>
+#include <drivers/rand32.h>
+#include <init.h>
+
+#include "fsl_trng.h"
+
+static int random_mcux_trng_get_entropy(struct device *dev, uint8_t *buffer,
+					uint16_t length)
+{
+	status_t status;
+
+	ARG_UNUSED(dev);
+
+	status = TRNG_GetRandomData(TRNG0, buffer, length);
+	__ASSERT_NO_MSG(!status);
+
+	return 0;
+}
+
+static const struct random_driver_api random_mcux_trng_api_funcs = {
+	.get_entropy = random_mcux_trng_get_entropy
+};
+
+static int random_mcux_trng_init(struct device *);
+
+DEVICE_AND_API_INIT(random_mcux_trng, CONFIG_RANDOM_NAME,
+		    random_mcux_trng_init, NULL, NULL,
+		    PRE_KERNEL_2, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
+		    &random_mcux_trng_api_funcs);
+
+static int random_mcux_trng_init(struct device *dev)
+{
+	trng_config_t conf;
+	status_t status;
+
+	ARG_UNUSED(dev);
+
+	status = TRNG_GetDefaultConfig(&conf);
+	__ASSERT_NO_MSG(!status);
+
+	status = TRNG_Init(TRNG0, &conf);
+	__ASSERT_NO_MSG(!status);
+
+	return 0;
+}
+
+uint32_t sys_rand32_get(void)
+{
+	uint32_t output;
+	int rc;
+
+	rc = random_mcux_trng_get_entropy(DEVICE_GET(random_mcux_trng),
+					  (uint8_t *) &output, sizeof(output));
+	__ASSERT_NO_MSG(!rc);
+
+	return output;
+}
