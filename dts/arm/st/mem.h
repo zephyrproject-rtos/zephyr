d899ce16b0834571e2d531e87d8c6fc6c9bb32f2
Erwan Gouriou <erwan.gouriou@linaro.org>
2017-03-06 11:11:19 +0100

    dts: provide stm32 soc dtsi files for stm32 base boards
    This commit provides dtsi files for available stm32 base boards.
    For now only uart nodes and IRQ number are provided in order to
    enable delivery of coherent material.
    
    It also clears additional content from stm32f103xb.dtsi
    
    Change-Id: I62d932c7f22b56e95bcd9566ce39e14a393dd640
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- dts/arm/st/mem.h
+++ dts/arm/st/mem.h
@@ -4,10 +4,26 @@
 #if defined(CONFIG_SOC_STM32F103XB)
 #define DT_FLASH_SIZE		0x20000
 #define DT_SRAM_SIZE		0x5000
+#elif defined(CONFIG_SOC_STM32F103XE)
+#define DT_FLASH_SIZE		0x80000
+#define DT_SRAM_SIZE		0x10000
+#elif defined(CONFIG_SOC_STM32F107XC)
+#define DT_FLASH_SIZE		0x40000
+#define DT_SRAM_SIZE		0x10000
+#elif defined(CONFIG_SOC_STM32F334X8)
+#define DT_FLASH_SIZE		0x10000
+#define DT_SRAM_SIZE		0x3000
+#elif defined(CONFIG_SOC_STM32F373XC)
+#define DT_FLASH_SIZE		0x40000
+#define DT_SRAM_SIZE		0x8000
+#elif defined(CONFIG_SOC_STM32F401XE) || defined(CONFIG_SOC_STM32F411XE)
+#define DT_FLASH_SIZE		0x80000
+#define DT_SRAM_SIZE		0x18000
 #elif defined(CONFIG_SOC_STM32L476XX)
 #define DT_FLASH_SIZE		0x100000
 #define DT_SRAM_SIZE		0x18000
 #else
+#error "Flash and RAM sizes not defined for this chip"
 #endif
 
 #endif /* __DT_BINDING_ST_MEM_H */
