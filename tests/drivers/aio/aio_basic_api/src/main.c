b0962e3bac67698f1f18df2b6a22a7783e61d030
Qiu Peiyang <peiyangx.qiu@intel.com>
2017-03-15 09:46:50 +0800

    tests: aio: port AIO cases to run on more platforms
    AIO should be supported on more platforms. Adapt
    this case to make it run on more platforms.
    Also keep reference voltage for comprator as internal.
    
    Signed-off-by: Qiu Peiyang <peiyangx.qiu@intel.com>

--- tests/drivers/aio/aio_basic_api/src/main.c
+++ tests/drivers/aio/aio_basic_api/src/main.c
@@ -12,18 +12,28 @@
  *
  * Setup: loop PIN_OUT with PIN_IN on target board
  *
- * quark_se_c1000_devboard
+ * quark_se_c1000_devboard - x86
  * ------------------------
- * 1. PIN_OUT is GPIO_17
- * 2. PIN_IN is AIN_15
+ * 1. PIN_OUT is GPIO_15
+ * 2. PIN_IN is AIN_10
+ *
+ * quark_se_c1000_ss_evboard - arc
+ * ------------------------
+ * 1. PIN_OUT is GPIO_SS_3
+ * 2. PIN_IN is AIN_10
  */
 
-#include "test_aio.h"
+#include <ztest.h>
+
+extern void test_aio_callback_rise(void);
+extern void test_aio_callback_fall(void);
+extern void test_aio_callback_rise_disable(void);
 
 void test_main(void)
 {
 	ztest_test_suite(aio_basic_api_test,
 			 ztest_unit_test(test_aio_callback_rise),
+			 ztest_unit_test(test_aio_callback_fall),
 			 ztest_unit_test(test_aio_callback_rise_disable));
 	ztest_run_test_suite(aio_basic_api_test);
 }
--- tests/drivers/aio/aio_basic_api/src/test_callback.c
+++ tests/drivers/aio/aio_basic_api/src/test_callback.c
@@ -4,22 +4,98 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "test_aio.h"
+#include <ztest.h>
+#include <gpio.h>
+#include <pinmux.h>
+#include <aio_comparator.h>
 
-static int cb_cnt;
+#define AIO_CMP_DEV_NAME	CONFIG_AIO_COMPARATOR_0_NAME
+#define PINMUX_NAME		CONFIG_PINMUX_NAME
+
+#ifdef CONFIG_ARC
+#define GPIO_DEV_NAME		CONFIG_GPIO_QMSI_SS_0_NAME
+#else
+#define GPIO_DEV_NAME		CONFIG_GPIO_QMSI_0_NAME
+#endif
+
+#if defined(CONFIG_BOARD_QUARK_SE_C1000_DEVBOARD)
+#define PIN_OUT 15	/* GPIO15 */
+#define PIN_IN 10	/* GPIO_SS_AIN_10 */
+#elif defined(CONFIG_BOARD_QUARK_SE_C1000_DEVBOARD_SS)
+#define PIN_OUT 3	/* GPIO_SS_3 */
+#define PIN_IN 10	/* GPIO_SS_AIN_10 */
+#elif defined(CONFIG_BOARD_QUARK_D2000_CRB)
+#define PIN_OUT 8	/* GPIO_8 */
+#define PIN_IN 10	/* AIN_10 */
+#elif defined(CONFIG_BOARD_ARDUINO_101)
+#define PIN_OUT 16	/* GPIO_16 */
+#define PIN_IN 10	/* AIN_10 */
+#elif defined(CONFIG_BOARD_ARDUINO_101_SSS)
+#define PIN_OUT 3	/* GPIO_SS_3 */
+#define PIN_IN 10	/* AIN_10 */
+#endif
+
+volatile int cb_cnt;
+static struct device *aio_dev;
+static struct device *gpio_dev;
 
 static void callback(void *param)
 {
+	int polarity = *(int *)param;
+
 	cb_cnt++;
+	TC_PRINT("*** callback triggered %s\n",
+		(polarity == AIO_CMP_POL_RISE) ? "rising" : "falling"
+	);
+
+	if (polarity == AIO_CMP_POL_FALL) {
+		gpio_pin_write(gpio_dev, PIN_OUT, 1);
+	} else {
+		gpio_pin_write(gpio_dev, PIN_OUT, 0);
+	}
+
+	if (aio_cmp_get_pending_int(aio_dev)) {
+		TC_PRINT("Catch aio_cmp pending interrupt\n");
+	} else {
+		TC_PRINT("Fail to catch aio_cmp pending interrupt\n");
+	}
+
+	aio_cmp_disable(aio_dev, PIN_IN);
 }
 
-static void set_aio_callback(int polarity, int disable)
+static int set_aio_callback(int polarity, int disable)
 {
-	struct device *aio_dev = device_get_binding(AIO_CMP_DEV_NAME);
+	struct device *pinmux = device_get_binding(PINMUX_NAME);
 
-	struct device *gpio_dev = device_get_binding(GPIO_DEV_NAME);
+	if (!pinmux) {
+		TC_PRINT("Cannot get PINMUX\n");
+		return TC_FAIL;
+	}
 
-	gpio_pin_configure(gpio_dev, PIN_OUT, GPIO_DIR_OUT);
+	aio_dev = device_get_binding(AIO_CMP_DEV_NAME);
+	if (!aio_dev) {
+		TC_PRINT("AIO Device binding failed\n");
+		return TC_FAIL;
+	}
+
+	gpio_dev = device_get_binding(GPIO_DEV_NAME);
+	if (!gpio_dev) {
+		TC_PRINT("GPIO Device binding failed\n");
+		return TC_FAIL;
+	}
+
+	if (gpio_pin_configure(gpio_dev, PIN_OUT, GPIO_DIR_OUT)) {
+		TC_PRINT("Fail to configure GPIO Pin %d\n", PIN_OUT);
+		return TC_FAIL;
+	}
+
+	if (pinmux_pin_set(pinmux, PIN_IN, PINMUX_FUNC_B)) {
+		TC_PRINT("Fail to set pin func, %u\n", PIN_IN);
+		return TC_FAIL;
+	}
+
+	gpio_pin_write(gpio_dev, PIN_OUT,
+			(polarity == AIO_CMP_POL_RISE) ? 0 : 1);
 
 	/* config AIN callback */
 	zassert_true(aio_cmp_configure(aio_dev, PIN_IN,
@@ -27,25 +103,34 @@ static void set_aio_callback(int polarity, int disable)
 				      callback, (void *)aio_dev) == 0,
 		    "ERROR registering callback");
 	if (disable == 1) {
-		aio_cmp_disable(aio_dev, PIN_IN);
+		if (aio_cmp_disable(aio_dev, PIN_IN)) {
+			TC_PRINT("Fail to disable callback\n");
+			return TC_FAIL;
+		}
 	}
 
-	/* update the AIN input to trigger state */
-	gpio_pin_write(gpio_dev, PIN_OUT,
-		       (polarity == AIO_CMP_POL_RISE) ? 0 : 1);
 	k_sleep(100);
 	cb_cnt = 0;
+	k_sleep(100);
+
 	gpio_pin_write(gpio_dev, PIN_OUT,
-		       (polarity == AIO_CMP_POL_RISE) ? 1 : 0);
+		(polarity == AIO_CMP_POL_RISE) ? 1 : 0);
 
 	k_sleep(1000);
 	TC_PRINT("... cb_cnt = %d\n", cb_cnt);
+
+	return TC_PASS;
 }
 
-/* export test cases */
 void test_aio_callback_rise(void)
 {
 	set_aio_callback(AIO_CMP_POL_RISE, 0);
+	zassert_true(cb_cnt == 1, "callback is not invoked correctly");
+}
+
+void test_aio_callback_fall(void)
+{
+	set_aio_callback(AIO_CMP_POL_FALL, 0);
 	zassert_true(cb_cnt == 1, "callback is not invoked correctly");
 }
 
