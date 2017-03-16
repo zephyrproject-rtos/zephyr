042624fb62c15a7b36b1f9116f23936f23ef2c81
Johan Hedberg <johan.hedberg@intel.com>
2017-03-16 10:50:04 +0200

    Bluetooth: Move crypto APIs to dedicated header file
    This avoids overpopulating the high-level bluetooth.h
    
    Change-Id: Icab8500be92003aa45d837ff111a8d93689865cc
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- include/bluetooth/bluetooth.h
+++ include/bluetooth/bluetooth.h
@@ -23,6 +23,7 @@
 #include <misc/util.h>
 #include <net/buf.h>
 #include <bluetooth/hci.h>
+#include <bluetooth/crypto.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -456,34 +457,6 @@ int bt_br_set_discoverable(bool enable);
  *  already set. Zero if done successfully.
  */
 int bt_br_set_connectable(bool enable);
-
-/** @brief Generate random data.
- *
- *  A random number generation helper which utilizes the Bluetooth
- *  controller's own RNG.
- *
- *  @param buf Buffer to insert the random data
- *  @param len Length of random data to generate
- *
- *  @return Zero on success or error code otherwise, positive in case
- *  of protocol error or negative (POSIX) in case of stack internal error
- */
-int bt_rand(void *buf, size_t len);
-
-/** @brief AES encrypt data.
- *
- *  An AES encrypt helper is used to request the Bluetooth controller's own
- *  hardware to encrypt the plaintext using the key and returns the encrypted
- *  data.
- *
- *  @param key 128 bit LS byte first key for the encryption of the plaintext
- *  @param plaintext 128 bit LS byte first plaintext data block to be encrypted
- *  @param enc_data 128 bit LS byte first encrypted data block
- *
- *  @return Zero on success or error code otherwise.
- */
-int bt_encrypt(const uint8_t key[16], const uint8_t plaintext[16],
-	       uint8_t enc_data[16]);
 
 /**
  * @}
