0ab8874fdeeb486cf4adff4c6ee3c0f9e4a51c9a
Maureen Helm <maureen.helm@nxp.com>
2017-03-30 16:10:55 -0500

    samples: Add max30101 sample application
    Adds a max30101 sample application that polls sensor data and prints it
    to the console.
    
    Jira: ZEP-720
    Change-Id: I50f28eaf9ea2ff5bfbb9fb6922c4006d5c02e739
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

---
+++ samples/sensor/max30101/src/main.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2017, NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <sensor.h>
+#include <stdio.h>
+
+void main(void)
+{
+	struct sensor_value green;
+	struct device *dev = device_get_binding(CONFIG_MAX30101_NAME);
+
+	if (dev == NULL) {
+		printf("Could not get max30101 device\n");
+		return;
+	}
+
+	while (1) {
+		sensor_sample_fetch(dev);
+		sensor_channel_get(dev, SENSOR_CHAN_GREEN, &green);
+
+		/* Print green LED data*/
+		printf("GREEN=%d\n", green.val1);
+
+		k_sleep(20);
+	}
+}
