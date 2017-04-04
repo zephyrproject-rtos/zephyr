77d0211f0c22499219bd02fdae59aed53104201c
Johan Hedberg <johan.hedberg@intel.com>
2017-04-04 21:55:27 +0300

    boards: bbc_microbit: Remove bogus references to green LEDs
    The LEDs on BBC micro:bit boards are red and not green. Since
    mentioning the color doesn't really provide any value to the GPIO pin
    definitions, just remove it.
    
    Change-Id: I7f53e0d8a0733cda911a624d0b53c750eacfa685
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- boards/arm/bbc_microbit/board.h
+++ boards/arm/bbc_microbit/board.h
@@ -17,51 +17,51 @@
 #define SW1_GPIO_PIN	26
 #define SW1_GPIO_NAME	CONFIG_GPIO_NRF5_P0_DEV_NAME
 
-/* Onboard GREEN LED Row 1 */
+/* Onboard LED Row 1 */
 #define LED_ROW1_GPIO_PIN   13
 #define LED_ROW1_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
 
-/* Onboard GREEN LED Row 2 */
+/* Onboard LED Row 2 */
 #define LED_ROW2_GPIO_PIN   14
 #define LED_ROW2_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
 
-/* Onboard GREEN LED Row 3 */
+/* Onboard LED Row 3 */
 #define LED_ROW3_GPIO_PIN   15
 #define LED_ROW3_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
 
-/* Onboard GREEN LED Column 1 */
+/* Onboard LED Column 1 */
 #define LED_COL1_GPIO_PIN   4
 #define LED_COL1_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
 
-/* Onboard GREEN LED Column 2 */
+/* Onboard LED Column 2 */
 #define LED_COL2_GPIO_PIN   5
 #define LED_COL2_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
 
-/* Onboard GREEN LED Column 3 */
+/* Onboard LED Column 3 */
 #define LED_COL3_GPIO_PIN   6
 #define LED_COL3_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
 
-/* Onboard GREEN LED Column 4 */
+/* Onboard LED Column 4 */
 #define LED_COL4_GPIO_PIN   7
 #define LED_COL4_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
 
-/* Onboard GREEN LED Column 5 */
+/* Onboard LED Column 5 */
 #define LED_COL5_GPIO_PIN   8
 #define LED_COL5_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
 
-/* Onboard GREEN LED Column 6 */
+/* Onboard LED Column 6 */
 #define LED_COL6_GPIO_PIN   9
 #define LED_COL6_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
 
-/* Onboard GREEN LED Column 7 */
+/* Onboard LED Column 7 */
 #define LED_COL7_GPIO_PIN   10
 #define LED_COL7_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
 
-/* Onboard GREEN LED Column 8 */
+/* Onboard LED Column 8 */
 #define LED_COL8_GPIO_PIN   11
 #define LED_COL8_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
 
-/* Onboard GREEN LED Column 9 */
+/* Onboard LED Column 9 */
 #define LED_COL9_GPIO_PIN   12
 #define LED_COL9_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
 
