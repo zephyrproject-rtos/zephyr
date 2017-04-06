8dd59f3d49b1bf17884a20fb35b923b7f99c8f46
Maureen Helm <maureen.helm@nxp.com>
2017-04-06 12:39:17 -0500

    hexiwear_kw40z: Add hexiwear_kw40z board
    Adds initial support and documentation for the kw40z on the hexiwear
    board.
    
    Jira: ZEP-1391
    Change-Id: Idb58bfb3c2951b1f737a8c547860bde4ef4d9a3e
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

---
+++ boards/arm/hexiwear_kw40z/pinmux.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2017, NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+#include <pinmux.h>
+#include <fsl_port.h>
+
+static int hexiwear_kw40z_pinmux_init(struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+#ifdef CONFIG_PINMUX_MCUX_PORTC
+	struct device *portc =
+		device_get_binding(CONFIG_PINMUX_MCUX_PORTC_NAME);
+#endif
+
+#ifdef CONFIG_UART_MCUX_LPUART_0
+	/* UART0 RX, TX */
+	pinmux_pin_set(portc,  6, PORT_PCR_MUX(kPORT_MuxAlt4));
+	pinmux_pin_set(portc,  7, PORT_PCR_MUX(kPORT_MuxAlt4));
+#endif
+
+#if CONFIG_I2C_1
+	/* I2C1 SCL, SDA */
+	pinmux_pin_set(portc,  6, PORT_PCR_MUX(kPORT_MuxAlt3)
+					| PORT_PCR_PS_MASK);
+	pinmux_pin_set(portc,  6, PORT_PCR_MUX(kPORT_MuxAlt3)
+					| PORT_PCR_PS_MASK);
+#endif
+
+	return 0;
+}
+
+SYS_INIT(hexiwear_kw40z_pinmux_init, PRE_KERNEL_1, CONFIG_PINMUX_INIT_PRIORITY);
