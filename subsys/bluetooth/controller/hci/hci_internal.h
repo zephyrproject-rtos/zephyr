cd923e4f834b656a7aeb6e7e57e285f3ee5d085d
Carles Cufi <carles.cufi@nordicsemi.no>
2017-04-07 09:53:02 +0200

    Bluetooth: controller: Controller to Host flow control
    The Bluetooth Specification allows for optional Controller to Host flow
    control based on the same credit-based mechanism as the Host to
    Controller one. This is particularly useful in 2-chip solutions where
    the Host and the Controller are connected via a physical link (UART, SPI
    or similar) where the Host is sometimes required to ask the Controller
    to throttle its data traffic while still making sure that relevant
    events get through the line.
    
    This implementation is based on a simple queue of pending events and
    data that is populated whenever the Controller detects that the Host is
    out of buffers and then emptied whenever the Host notifies the
    Controller that is ready to receive data again. Events relevant to the
    connections are also queued to preserve the order of arrival.
    
    At this point the Controller ignores the connection handle sent by the
    Host and treats all connections equally, and it also queues events even
    for connections that have no data pending in the queue. Both this items
    can be improved if necessity arises.
    
    Note that Number of Completed Packets will still flow freely from the
    Controller to the Host regardless of the pending ACL data packets, which
    might lead to inconsistencies in the sequential order of certain
    operations that include bi-directional data transfer.
    
    Jira: ZEP-1735
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- subsys/bluetooth/controller/hci/hci_internal.h
+++ subsys/bluetooth/controller/hci/hci_internal.h
@@ -8,10 +8,27 @@
 #ifndef _HCI_CONTROLLER_H_
 #define _HCI_CONTROLLER_H_
 
+#if defined(CONFIG_BLUETOOTH_CONTROLLER_TO_HOST_FC)
+extern s32_t    hci_hbuf_total;
+extern u32_t    hci_hbuf_sent;
+extern u32_t    hci_hbuf_acked;
+extern atomic_t hci_state_mask;
+
+#define HCI_STATE_BIT_RESET 0
+#endif
+
+#define HCI_CLASS_EVT_REQUIRED    0
+#define HCI_CLASS_EVT_DISCARDABLE 1
+#define HCI_CLASS_EVT_CONNECTION  2
+#define HCI_CLASS_ACL_DATA        3
+
+void hci_init(struct k_poll_signal *signal_host_buf);
 struct net_buf *hci_cmd_handle(struct net_buf *cmd);
-int hci_acl_handle(struct net_buf *acl);
 void hci_evt_encode(struct radio_pdu_node_rx *node_rx, struct net_buf *buf);
+s8_t hci_get_class(struct radio_pdu_node_rx *node_rx);
+#if defined(CONFIG_BLUETOOTH_CONN)
+int hci_acl_handle(struct net_buf *acl);
 void hci_acl_encode(struct radio_pdu_node_rx *node_rx, struct net_buf *buf);
 void hci_num_cmplt_encode(struct net_buf *buf, u16_t handle, u8_t num);
-bool hci_evt_is_discardable(struct radio_pdu_node_rx *node_rx);
+#endif
 #endif /* _HCI_CONTROLLER_H_ */
