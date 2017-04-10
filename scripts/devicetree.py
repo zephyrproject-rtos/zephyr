ed3d7c1de5ea04b8f8e04a10d2b556a4576d782a
Anas Nashif <anas.nashif@intel.com>
2017-04-10 08:53:23 -0400

    build: make sure we are calling python3 in all scripts
    Jira: ZEP-1759
    Change-Id: I0fc7329f806ca0049c555d3402dd145e92e54028
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/devicetree.py
+++ scripts/devicetree.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #
 # Copyright (c) 2017 Intel Corporation
 #
--- scripts/qemu-machine-hack.py
+++ scripts/qemu-machine-hack.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 
 # For some baffling reason IAMCU sets the instruction set architecture
