eea12f6f147fd88aed93f742b2363efd6dcd53d1
Sharron LIU <sharron.liu@intel.com>
2017-04-14 18:55:02 +0800

    tests: kernel: add tickless test
    Added test cases to verify tickless idle concepts defined in
    https://www.zephyrproject.org/doc/subsystems/power_management.html#tickless-idle
    
    Test points:
    verify system clock recovery after exiting tickless idle;
    verify slicing scheduler behaves as expected
    
    Jira: ZEP-339
    
    Change-Id: Ic0a86d725e9692aa217375cedc7396372a026a88
    Signed-off-by: Sharron LIU <sharron.liu@intel.com>

---
+++ tests/kernel/tickless/tickless_concept/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ztest.h>
+extern void test_tickless_sysclock(void);
+extern void test_tickless_slice(void);
+
+/*test case main entry*/
+void test_main(void *p1, void *p2, void *p3)
+{
+	ztest_test_suite(test_tickless_concept,
+		ztest_unit_test(test_tickless_sysclock),
+		ztest_unit_test(test_tickless_slice));
+	ztest_run_test_suite(test_tickless_concept);
+}
