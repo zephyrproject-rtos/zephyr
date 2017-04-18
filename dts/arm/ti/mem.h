de20a6558adf3dcb903fe2856265e6abdb4bcf4f
Gil Pitney <gil.pitney@linaro.org>
2017-04-18 14:18:50 -0700

    cc3200: Make use of mem.h file in soc dtsi file.
    This in preparation for adding a cc3220sf DTS file, which
    has a different address for sram0 than the cc3200.
    
    Also moved baud-rate out of soc dtsi file to board dts.
    
    Jira: JEP-1958
    
    Change-Id: I641452c0a8a6d1ad4424e132d6ef2de71d8545b4
    Signed-off-by: Gil Pitney <gil.pitney@linaro.org>

---
+++ dts/arm/ti/mem.h
@@ -0,0 +1,14 @@
+#ifndef __DT_BINDING_TI_MEM_H
+#define __DT_BINDING_TI_MEM_H
+
+#define __SIZE_K(x) (x * 1024)
+
+#if defined(CONFIG_SOC_CC3200)
+#define DT_SFLASH_SIZE		__SIZE_K(1024)
+#define DT_SRAM_SIZE		__SIZE_K(240)
+#define DT_SRAM_START		0x20004000
+#else
+#error "Flash and RAM sizes not defined for this chip"
+#endif
+
+#endif /* __DT_BINDING_TI_MEM_H */
