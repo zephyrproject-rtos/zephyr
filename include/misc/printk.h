4a157f3e48358b6f3a7507bba16470bb85bdfb23
Marti Bolivar <marti.bolivar@linaro.org>
2017-04-21 17:33:58 -0400

    printk: add missing __printf_like attributes
    GCC supports __attribute__((format (printf ...))) even when the
    variadic arguments are not present. In this case, the attribute
    argument specifying the start of the variadic arguments should be
    zero.
    
    Use this in printk.h to add __printf_like where it's missing.
    
    Change-Id: I7868439d5791e391aeb07356af9819524e68c771
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>

--- include/misc/printk.h
+++ include/misc/printk.h
@@ -45,9 +45,11 @@ extern "C" {
 extern __printf_like(1, 2) int printk(const char *fmt, ...);
 extern __printf_like(3, 4) int snprintk(char *str, size_t size,
 					const char *fmt, ...);
-extern int vsnprintk(char *str, size_t size, const char *fmt, va_list ap);
+extern __printf_like(3, 0) int vsnprintk(char *str, size_t size,
+					  const char *fmt, va_list ap);
 
-void _vprintk(int (*out)(int, void *), void *ctx, const char *fmt, va_list ap);
+extern __printf_like(3, 0) void _vprintk(int (*out)(int, void *), void *ctx,
+					 const char *fmt, va_list ap);
 #else
 static inline __printf_like(1, 2) int printk(const char *fmt, ...)
 {
@@ -64,8 +66,8 @@ static inline __printf_like(3, 4) int snprintk(char *str, size_t size,
 	return 0;
 }
 
-static inline int vsnprintk(char *str, size_t size, const char *fmt,
-			    va_list ap)
+static inline __printf_like(3, 0) int vsnprintk(char *str, size_t size,
+						const char *fmt, va_list ap)
 {
 	ARG_UNUSED(str);
 	ARG_UNUSED(size);
