3f5070767227989493cdbd8eaacddcbe451dfe58
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-04-25 17:54:31 +0300

    kernel: queue, fifo: Add cancel_wait operation.
    Currently, a queue/fifo getter chooses how long to wait for an
    element. But there are scenarios when putter would know better,
    there should be a way to expire getter's timeout to make it run
    again. k_queue_cancel_wait() and k_fifo_cancel_wait() functions
    do just that. They cause corresponding *_get() functions to return
    with NULL value, as if timeout expired on getter's side (even
    K_FOREVER).
    
    This can be used to signal out of band conditions from putter to
    getter, e.g. end of processing, error, configuration change, etc.
    A specific event would be communicated to getter by other means
    (e.g. using existing shared context structures).
    
    Without this call, achieving the same effect would require e.g.
    calling k_fifo_put() with a pointer to a special sentinal memory
    structure - such structure would need to be allocated somewhere
    and somehow, and getter would need to recognize it from a normal
    data item. Having cancel_wait() functions offers an elegant
    alternative. From this perspective, these calls can be seen as
    an equivalent to e.g. k_fifo_put(fifo, NULL), except that such
    call won't work in practice.
    
    Change-Id: I47b7f690dc325a80943082bcf5345c41649e7024
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- tests/kernel/fifo/test_fifo_api/src/main.c
+++ tests/kernel/fifo/test_fifo_api/src/main.c
@@ -17,6 +17,7 @@ extern void test_fifo_thread2isr(void);
 extern void test_fifo_isr2thread(void);
 extern void test_fifo_get_fail(void);
 extern void test_fifo_loop(void);
+extern void test_fifo_cancel_wait(void);
 extern void test_fifo_is_empty_thread(void);
 extern void test_fifo_is_empty_isr(void);
 
@@ -29,6 +30,7 @@ void test_main(void *p1, void *p2, void *p3)
 		ztest_unit_test(test_fifo_isr2thread),
 		ztest_unit_test(test_fifo_get_fail),
 		ztest_unit_test(test_fifo_loop),
+		ztest_unit_test(test_fifo_cancel_wait),
 		ztest_unit_test(test_fifo_is_empty_thread),
 		ztest_unit_test(test_fifo_is_empty_isr));
 	ztest_run_test_suite(test_fifo_api);
