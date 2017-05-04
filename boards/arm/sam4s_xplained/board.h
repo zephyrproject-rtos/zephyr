106bf1c09a18fff94368a2a3501086478f5f4266
fallrisk <jwatson5@gmail.com>
2017-05-04 16:35:04 -0700

    arm: board: Add support for board Atmel SAM4S Xplained.
    This commit provides support for Atmel SAM4S Xplained board
    
    Signed-off-by: Justin Watson <jwatson5@gmail.com>

---
+++ boards/arm/sam4s_xplained/board.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Justin Watson 2017
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+#endif /* __INC_BOARD_H */
