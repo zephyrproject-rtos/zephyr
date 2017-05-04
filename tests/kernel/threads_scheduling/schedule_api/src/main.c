ab6d4c1a424d2a6b58111faa5b56e90b2a9086e5
Sharron LIU <sharron.liu@intel.com>
2017-05-04 10:11:04 +0800

    tests: kernel: added tests for timeslice reset
    Added test cases to verify timeslice reset among thread context
    switching.
    
    Jira: ZEP-948
    
    Signed-off-by: Sharron LIU <sharron.liu@intel.com>

--- tests/kernel/threads_scheduling/schedule_api/src/main.c
+++ tests/kernel/threads_scheduling/schedule_api/src/main.c
@@ -28,7 +28,8 @@ void test_main(void *p1, void *p2, void *p3)
 		ztest_unit_test(test_time_slicing_disable_preemptible),
 		ztest_unit_test(test_lock_preemptible),
 		ztest_unit_test(test_unlock_preemptible),
-		ztest_unit_test(test_sched_is_preempt_thread)
+		ztest_unit_test(test_sched_is_preempt_thread),
+		ztest_unit_test(test_slice_reset)
 		);
 	ztest_run_test_suite(test_threads_scheduling);
 }
