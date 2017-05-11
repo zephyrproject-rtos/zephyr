e42dc4860e1cd163de96039c499986c40867fc23
Fabien Parent <fparent@baylibre.com>
2017-05-11 11:47:03 +0200

    board: stm32: add support for STM32F723E-DISCO board
    Add support for STM32F723E DISCOVERY.
    
    Signed-off-by: Fabien Parent <fparent@baylibre.com>

---
+++ drivers/pinmux/stm32/pinmux_stm32f7.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016 Linaro Limited.
+ * Copyright (c) 2017 BayLibre, SAS
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _STM32F7_PINMUX_H_
+#define _STM32F7_PINMUX_H_
+
+/**
+ * @file Header for STM32F7 pin multiplexing helper
+ */
+
+#define STM32F7_PINMUX_FUNC_PC6_USART6_TX	STM32_PINMUX_FUNC_ALT_8
+#define STM32F7_PINMUX_FUNC_PC7_USART6_RX	STM32_PINMUX_FUNC_ALT_8
+
+#endif /* _STM32F7_PINMUX_H_ */
