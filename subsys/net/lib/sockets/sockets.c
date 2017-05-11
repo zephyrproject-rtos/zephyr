4d35a4d81962f22ee82d6e2bd65b9e8ded26cf0f
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-05-11 13:47:27 +0300

    net: sockets: Bootstrap Sockets API implementation
    This adds Kconfig and build infrastructure and implements
    zsock_socket() and zsock_close() functions.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

---
+++ subsys/net/lib/sockets/sockets.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2017 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#if defined(CONFIG_NET_DEBUG_SOCKETS)
+#define SYS_LOG_DOMAIN "net/sock"
+#define NET_LOG_ENABLED 1
+#endif
+
+#include <net/net_context.h>
+#include <net/net_pkt.h>
+#include <net/socket.h>
+
+#define SET_ERRNO(x) \
+	{ int _err = x; if (_err < 0) { errno = -_err; return -1; } }
+
+int zsock_socket(int family, int type, int proto)
+{
+	struct net_context *ctx;
+
+	SET_ERRNO(net_context_get(family, type, proto, &ctx));
+	/* recv_q and accept_q are in union */
+	k_fifo_init(&ctx->recv_q);
+
+	/* TODO: Ensure non-negative */
+	return POINTER_TO_INT(ctx);
+}
+
+int zsock_close(int sock)
+{
+	struct net_context *ctx = INT_TO_POINTER(sock);
+
+	SET_ERRNO(net_context_put(ctx));
+	return 0;
+}
