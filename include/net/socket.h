386c5bacd12ce5bd665df766ea1ffb7f4028dd8b
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-05-12 16:52:34 +0300

    net: sockets: Add configurable option to provide raw POSIX API names
    With CONFIG_NET_SOCKETS_POSIX_NAMES=y, "raw" POSIX names like
    socket(), recv(), close() will be exposed (using macro defines).
    The close() is the biggest culprit here, because in POSIX it
    applies to any file descriptor, but in this implementation -
    only to sockets.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- include/net/socket.h
+++ include/net/socket.h
@@ -7,12 +7,20 @@
 #ifndef __NET_SOCKET_H
 #define __NET_SOCKET_H
 
+#include <zephyr/types.h>
+#include <net/net_ip.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 int zsock_socket(int family, int type, int proto);
 int zsock_close(int sock);
+
+#if defined(CONFIG_NET_SOCKETS_POSIX_NAMES)
+#define socket zsock_socket
+#define close zsock_close
+#endif
 
 #ifdef __cplusplus
 }
