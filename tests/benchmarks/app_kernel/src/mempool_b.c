dfb5a260b21895ea7d884e6adf0f3eecda58c086
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2017-05-23 10:57:29 +0530

    tests: benchmark: app_kernel: Return values from kernel APIs are read.
    Static code analysis reported some kernel APIs were used without
    reading the return value. Since the benchmark doesn't need error
    conditions, a simple read of the values followed by a ARG_UNUSED
    is used to handle static code analysis errors.
    
    JIRA: ZEP-2134
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>

--- tests/benchmarks/app_kernel/src/mempool_b.c
+++ tests/benchmarks/app_kernel/src/mempool_b.c
@@ -20,17 +20,24 @@ void mempool_test(void)
 {
 	u32_t et; /* elapsed time */
 	int i;
+	s32_t return_value = 0;
 	struct k_mem_block block;
 
 	PRINT_STRING(dashline, output_file);
 	et = BENCH_START();
 	for (i = 0; i < NR_OF_POOL_RUNS; i++) {
-		k_mem_pool_alloc(&DEMOPOOL, &block, 16, K_FOREVER);
+		return_value |= k_mem_pool_alloc(&DEMOPOOL,
+						&block,
+						16,
+						K_FOREVER);
 		k_mem_pool_free(&block);
 	}
 	et = TIME_STAMP_DELTA_GET(et);
 	check_result();
 
+	if (return_value != 0) {
+		k_panic();
+	}
 	PRINT_F(output_file, FORMAT,
 		"average alloc and dealloc memory pool block",
 		SYS_CLOCK_HW_CYCLES_TO_NS_AVG(et, (2 * NR_OF_POOL_RUNS)));
