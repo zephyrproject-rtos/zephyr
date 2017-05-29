f2e8a21d7f863e9bb53343cc378c261d29fd513c
Erwan Gouriou <erwan.gouriou@linaro.org>
2017-05-29 15:35:26 +0200

    stm32f1xx: remove references to native driver
    Following introduction of stm32cube LL based clock control driver,
    remove references to former native driver.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- arch/arm/soc/st_stm32/stm32f1/soc_config.c
+++ arch/arm/soc/st_stm32/stm32f1/soc_config.c
@@ -87,20 +87,3 @@ int stm32_get_pin_config(int pin, int func)
 	}
 	return -EINVAL;
 }
-
-clock_control_subsys_t stm32_get_port_clock(int port)
-{
-	const clock_control_subsys_t ports_to_clock[STM32_PORTS_MAX] = {
-		UINT_TO_POINTER(STM32F10X_CLOCK_SUBSYS_IOPA),
-		UINT_TO_POINTER(STM32F10X_CLOCK_SUBSYS_IOPB),
-		UINT_TO_POINTER(STM32F10X_CLOCK_SUBSYS_IOPC),
-		UINT_TO_POINTER(STM32F10X_CLOCK_SUBSYS_IOPD),
-		UINT_TO_POINTER(STM32F10X_CLOCK_SUBSYS_IOPE),
-	};
-
-	if (port > STM32_PORTE) {
-		return NULL;
-	}
-
-	return ports_to_clock[port];
-}
