6abccb6d401346dff0cff134ef9a807d859f658c
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2017-05-29 12:54:56 +0300

    Bluetooth: Shell: Split GATT command to its own file
    This makes it simpler to organize the GATT command implementation.
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

---
+++ subsys/bluetooth/shell/bt.h
@@ -0,0 +1,18 @@
+/** @file
+ @brief Bluetooth shell functions
+
+ This is not to be included by the application.
+ */
+
+/*
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __BT_H
+#define __bt_H
+
+extern struct bt_conn *default_conn;
+
+#endif /* __BT_H */
