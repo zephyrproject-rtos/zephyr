7567962f3a61acc57e1f1fb088ba1956fdd98ce2
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-06-08 17:14:08 +0300

    net: sockets: Implement bind(), connect(), listen(), accept()
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- include/net/socket.h
+++ include/net/socket.h
@@ -16,10 +16,18 @@ extern "C" {
 
 int zsock_socket(int family, int type, int proto);
 int zsock_close(int sock);
+int zsock_bind(int sock, const struct sockaddr *addr, socklen_t addrlen);
+int zsock_connect(int sock, const struct sockaddr *addr, socklen_t addrlen);
+int zsock_listen(int sock, int backlog);
+int zsock_accept(int sock, struct sockaddr *addr, socklen_t *addrlen);
 
 #if defined(CONFIG_NET_SOCKETS_POSIX_NAMES)
 #define socket zsock_socket
 #define close zsock_close
+#define bind zsock_bind
+#define connect zsock_connect
+#define listen zsock_listen
+#define accept zsock_accept
 #endif
 
 #ifdef __cplusplus
