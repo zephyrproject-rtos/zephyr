41e88ea40c4b170717f5c6bb86b2d799dd6b5fe6
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-06-10 21:53:03 +0300

    net: sockets: Implement send()
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- include/net/socket.h
+++ include/net/socket.h
@@ -7,6 +7,7 @@
 #ifndef __NET_SOCKET_H
 #define __NET_SOCKET_H
 
+#include <sys/types.h>
 #include <zephyr/types.h>
 #include <net/net_ip.h>
 
@@ -20,6 +21,7 @@ int zsock_bind(int sock, const struct sockaddr *addr, socklen_t addrlen);
 int zsock_connect(int sock, const struct sockaddr *addr, socklen_t addrlen);
 int zsock_listen(int sock, int backlog);
 int zsock_accept(int sock, struct sockaddr *addr, socklen_t *addrlen);
+ssize_t zsock_send(int sock, const void *buf, size_t len, int flags);
 
 #if defined(CONFIG_NET_SOCKETS_POSIX_NAMES)
 #define socket zsock_socket
@@ -28,6 +30,7 @@ int zsock_accept(int sock, struct sockaddr *addr, socklen_t *addrlen);
 #define connect zsock_connect
 #define listen zsock_listen
 #define accept zsock_accept
+#define send zsock_send
 
 #define inet_ntop net_addr_ntop
 #define inet_pton net_addr_pton
