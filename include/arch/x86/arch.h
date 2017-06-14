a9fbff857de0b7f88a240a79055203538677882d
Andrew Boie <andrew.p.boie@intel.com>
2017-06-14 16:10:20 -0700

    x86: place application data in its own sections
    Implements CONFIG_APPLICATION_MEMORY for x86. Working in
    XIP and non-XIP configurations.
    
    This patch does *not* implement any alignment constraints
    imposed by the x86 MMU, such enabling will be done later.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- include/arch/x86/arch.h
+++ include/arch/x86/arch.h
@@ -491,6 +491,27 @@ extern FUNC_NORETURN void _NanoFatalErrorHandler(unsigned int reason,
 extern FUNC_NORETURN void _SysFatalErrorHandler(unsigned int reason,
 						const NANO_ESF * pEsf);
 
+
+/* All thread stacks, regardless of whether owned by application or kernel,
+ * go in the .stacks input section, which will end up in the kernel's
+ * noinit.
+ */
+
+#define _ARCH_THREAD_STACK_DEFINE(sym, size) \
+	char _GENERIC_SECTION(.stacks) __aligned(STACK_ALIGN) sym[size]
+
+#define _ARCH_THREAD_STACK_ARRAY_DEFINE(sym, nmemb, size) \
+	char _GENERIC_SECTION(.stacks) __aligned(STACK_ALIGN) sym[nmemb][size]
+
+#define _ARCH_THREAD_STACK_MEMBER(sym, size) \
+	char __aligned(STACK_ALIGN) sym[size]
+
+#define _ARCH_THREAD_STACK_SIZEOF(sym) \
+	sizeof(sym)
+
+#define _ARCH_THREAD_STACK_BUFFER(sym) \
+	sym
+
 #if CONFIG_X86_KERNEL_OOPS
 #define _ARCH_EXCEPT(reason_p) do { \
 	__asm__ volatile( \
