50980fdd83b8e388b16c64fad93f248680c88502
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2017-06-14 15:15:49 +0530

    tests: benchmark: boot_time: Reading time stamps made arch agnostic
    1. Changed _tsc_read() to k_cycles_get_32(). Thus reading the
    time stamp will be agnostic of the architecutre used.
    2. Changed the variable names from *_tsc to *_time_stamp.
    
    JIRA: ZEP-1426
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>

--- tests/benchmarks/boot_time/src/main.c
+++ tests/benchmarks/boot_time/src/main.c
@@ -20,22 +20,22 @@
 #include <tc_util.h>
 
 /* externs */
-extern u64_t __start_tsc;    /* timestamp when kernel begins executing */
-extern u64_t __main_tsc;     /* timestamp when main() begins executing */
-extern u64_t __idle_tsc;     /* timestamp when CPU went idle */
+extern u64_t __start_time_stamp;    /* timestamp when kernel begins executing */
+extern u64_t __main_time_stamp;     /* timestamp when main() begins executing */
+extern u64_t __idle_time_stamp;     /* timestamp when CPU went idle */
 
 void main(void)
 {
-	u64_t task_tsc;      /* timestamp at beginning of first task  */
+	u64_t task_time_stamp;      /* timestamp at beginning of first task  */
 	u64_t _start_us;     /* being of __start timestamp in us	 */
 	u64_t main_us;       /* begin of main timestamp in us	 */
 	u64_t task_us;       /* begin of task timestamp in us	 */
-	u64_t s_main_tsc;    /* __start->main timestamp		 */
-	u64_t s_task_tsc;    /*__start->task timestamp		 */
+	u64_t s_main_time_stamp;    /* __start->main timestamp		 */
+	u64_t s_task_time_stamp;    /*__start->task timestamp		 */
 	u64_t idle_us;       /* begin of idle timestamp in us	 */
-	u64_t s_idle_tsc;    /*__start->idle timestamp		 */
+	u64_t s_idle_time_stamp;    /*__start->idle timestamp		 */
 
-	task_tsc = _tsc_read();
+	task_time_stamp = (u64_t)k_cycle_get_32();
 
 	/*
 	 * Go to sleep for 1 tick in order to timestamp when idle thread halts.
@@ -44,13 +44,13 @@ void main(void)
 
 	int freq = CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC / 1000000;
 
-	_start_us  =  __start_tsc / freq;
-	s_main_tsc =  __main_tsc - __start_tsc;
-	main_us    =  s_main_tsc / freq;
-	s_task_tsc =  task_tsc   - __start_tsc;
-	task_us    =  s_task_tsc / freq;
-	s_idle_tsc =  __idle_tsc - __start_tsc;
-	idle_us    =  s_idle_tsc / freq;
+	_start_us  =  __start_time_stamp / freq;
+	s_main_time_stamp =  __main_time_stamp - __start_time_stamp;
+	main_us    =  s_main_time_stamp / freq;
+	s_task_time_stamp =  task_time_stamp   - __start_time_stamp;
+	task_us    =  s_task_time_stamp / freq;
+	s_idle_time_stamp =  __idle_time_stamp - __start_time_stamp;
+	idle_us    =  s_idle_time_stamp / freq;
 
 	/* Indicate start for sanity test suite */
 	TC_START("Boot Time Measurement");
@@ -59,16 +59,16 @@ void main(void)
 	TC_PRINT("Boot Result: Clock Frequency: %d MHz\n",
 		 freq);
 	TC_PRINT("__start       : %d cycles, %d us\n",
-		 (u32_t)(__start_tsc & 0xFFFFFFFFULL),
+		 (u32_t)(__start_time_stamp & 0xFFFFFFFFULL),
 		 (u32_t) (_start_us  & 0xFFFFFFFFULL));
 	TC_PRINT("_start->main(): %d cycles, %d us\n",
-		 (u32_t)(s_main_tsc & 0xFFFFFFFFULL),
+		 (u32_t)(s_main_time_stamp & 0xFFFFFFFFULL),
 		 (u32_t)  (main_us  & 0xFFFFFFFFULL));
 	TC_PRINT("_start->task  : %d cycles, %d us\n",
-		 (u32_t)(s_task_tsc & 0xFFFFFFFFULL),
+		 (u32_t)(s_task_time_stamp & 0xFFFFFFFFULL),
 		 (u32_t)  (task_us  & 0xFFFFFFFFULL));
 	TC_PRINT("_start->idle  : %d cycles, %d us\n",
-		 (u32_t)(s_idle_tsc & 0xFFFFFFFFULL),
+		 (u32_t)(s_idle_time_stamp & 0xFFFFFFFFULL),
 		 (u32_t)  (idle_us  & 0xFFFFFFFFULL));
 
 	TC_PRINT("Boot Time Measurement finished\n");
