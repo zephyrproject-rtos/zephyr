7a1f06b37497a2e73fe0c227db604c3e8b694fc7
Huaqi Fang <huaqi.fang@synopsys.com>
2017-06-15 04:32:01 -0400

    arc: em7d: Add support for em starterkit 2.2 EM7D
    Since em starterkit has different firmware versions(2.2 and 2.3),
    but the EM7D of 2.3 has new secureshield feature, which is not supported
    in Zephyr, but EM7D of 2.2 is a normal EM core, which can be supported,
    so we add support for 2.2 EM7D.
    
    Signed-off-by: Huaqi Fang <huaqi.fang@synopsys.com>

--- arch/arc/soc/em7d/soc.h
+++ arch/arc/soc/em7d/soc.h
@@ -26,9 +26,18 @@
 
 #define IRQ_TIMER0				16
 #define IRQ_TIMER1				17
+
+#if defined(CONFIG_BOARD_EM_STARTERKIT_R23) && defined(CONFIG_SOC_EM7D)
 #define IRQ_SEC_TIMER0				20
+#endif
+
+#if defined(CONFIG_BOARD_EM_STARTERKIT_R23) && defined(CONFIG_SOC_EM7D)
 #define IRQ_CORE_DMA_COMPLETE			22
 #define IRQ_CORE_DMA_ERROR			23
+#else
+#define IRQ_CORE_DMA_COMPLETE			20
+#define IRQ_CORE_DMA_ERROR			21
+#endif
 
 #ifndef _ASMLANGUAGE
 
@@ -51,34 +60,28 @@
 
 /* I2C_0 is on Pmod2 connector */
 #define I2C_DW_0_BASE_ADDR			0xF0004000
-#define I2C_DW_0_IRQ				25
 
 
 /* I2C_1 is on Pmod4 connector */
 #define I2C_DW_1_BASE_ADDR			0xF0005000
-#define I2C_DW_1_IRQ				26
 
 #define I2C_DW_IRQ_FLAGS			0
 
 /* GPIO */
 #define GPIO_DW_0_BASE_ADDR			0xF0002000 /* GPIO 0 : PORTA */
-#define GPIO_DW_0_IRQ				24
 #define GPIO_DW_0_BITS				32
 #define GPIO_DW_PORT_0_INT_MASK			0 /* n/a */
 #define GPIO_DW_0_IRQ_FLAGS			0 /* Defaults */
 
 #define GPIO_DW_1_BASE_ADDR			0xF000200C /* GPIO 1 : PORTB */
-#define GPIO_DW_1_IRQ				0          /* can't interrupt */
 #define GPIO_DW_1_BITS				9          /* 9 LEDs on board */
 #define GPIO_DW_PORT_1_INT_MASK			0 /* n/a */
 
 #define GPIO_DW_2_BASE_ADDR			0xF0002018 /* GPIO 2 : PORTC */
-#define GPIO_DW_2_IRQ				0          /* can't interrupt */
 #define GPIO_DW_2_BITS				32
 #define GPIO_DW_PORT_2_INT_MASK			0 /* n/a */
 
 #define GPIO_DW_3_BASE_ADDR			0xF0002024 /* GPIO 3 : PORTD */
-#define GPIO_DW_3_IRQ				0          /* can't interrupt */
 #define GPIO_DW_3_BITS				12
 #define GPIO_DW_PORT_3_INT_MASK			0 /* n/a */
 
@@ -94,8 +97,6 @@
 #define SPI_DW_PORT_0_REGS			0xF0006000
 #define SPI_DW_PORT_1_REGS			0xF0007000
 
-#define SPI_DW_PORT_0_IRQ			27
-#define SPI_DW_PORT_1_IRQ			28
 
 #define SPI_DW_IRQ_FLAGS			0
 
@@ -117,18 +118,44 @@
      UART2 vector 31 0xF000A000
  */
 #define UART_NS16550_PORT_0_BASE_ADDR			0xF0008000
-#define UART_NS16550_PORT_0_IRQ				29
 #define UART_NS16550_PORT_0_CLK_FREQ			SYSCLK_DEFAULT_IOSC_HZ
 
 #define UART_NS16550_PORT_1_BASE_ADDR			0xF0009000
-#define UART_NS16550_PORT_1_IRQ				30
 #define UART_NS16550_PORT_1_CLK_FREQ			SYSCLK_DEFAULT_IOSC_HZ
 
 #define UART_NS16550_PORT_2_BASE_ADDR			0xF000A000
-#define UART_NS16550_PORT_2_IRQ				31
 #define UART_NS16550_PORT_2_CLK_FREQ			SYSCLK_DEFAULT_IOSC_HZ
 
 #define UART_IRQ_FLAGS					0 /* Default */
+
+/**
+ * Peripheral Interrupt Connection Configurations
+ */
+
+#ifdef CONFIG_BOARD_EM_STARTERKIT_R23
+#define GPIO_DW_0_IRQ				24
+#define I2C_DW_0_IRQ				25
+#define I2C_DW_1_IRQ				26
+#define SPI_DW_PORT_0_IRQ			27
+#define SPI_DW_PORT_1_IRQ			28
+#define UART_NS16550_PORT_0_IRQ			29
+#define UART_NS16550_PORT_1_IRQ			30
+#define UART_NS16550_PORT_2_IRQ			31
+#else
+#define GPIO_DW_0_IRQ				22
+#define I2C_DW_0_IRQ				23
+#define I2C_DW_1_IRQ				24
+#define SPI_DW_PORT_0_IRQ			25
+#define SPI_DW_PORT_1_IRQ			26
+#define UART_NS16550_PORT_0_IRQ			27
+#define UART_NS16550_PORT_1_IRQ			28
+#define UART_NS16550_PORT_2_IRQ			29
+#endif
+
+#define GPIO_DW_1_IRQ				0          /* can't interrupt */
+#define GPIO_DW_2_IRQ				0          /* can't interrupt */
+#define GPIO_DW_3_IRQ				0          /* can't interrupt */
+
 #endif /* !_ASMLANGUAGE */
 
 #endif /* _BOARD__H_ */
