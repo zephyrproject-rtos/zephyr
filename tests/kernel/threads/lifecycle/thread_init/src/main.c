61bfe80098f80ca3535c62289ab7ec31b02f767e
Anas Nashif <anas.nashif@intel.com>
2017-06-16 22:12:03 -0400

    tests: thread_init: fix style
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/kernel/threads/lifecycle/thread_init/src/main.c
+++ tests/kernel/threads/lifecycle/thread_init/src/main.c
@@ -16,9 +16,9 @@ extern void test_kinit_coop_thread(void);
 void test_main(void *p1, void *p2, void *p3)
 {
 	ztest_test_suite(test_thread_init,
-		ztest_unit_test(test_kdefine_preempt_thread),
-		ztest_unit_test(test_kdefine_coop_thread),
-		ztest_unit_test(test_kinit_preempt_thread),
-		ztest_unit_test(test_kinit_coop_thread));
+			 ztest_unit_test(test_kdefine_preempt_thread),
+			 ztest_unit_test(test_kdefine_coop_thread),
+			 ztest_unit_test(test_kinit_preempt_thread),
+			 ztest_unit_test(test_kinit_coop_thread));
 	ztest_run_test_suite(test_thread_init);
 }
