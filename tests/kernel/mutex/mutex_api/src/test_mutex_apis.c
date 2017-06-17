3ad53365d6b513f110ea572a5f3f017fe998b4e8
Anas Nashif <anas.nashif@intel.com>
2017-06-16 22:04:51 -0400

    tests: mutex_api: fix code style
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/kernel/mutex/mutex_api/src/test_mutex_apis.c
+++ tests/kernel/mutex/mutex_api/src/test_mutex_apis.c
@@ -32,7 +32,7 @@ static struct k_thread tdata;
 static void tThread_entry_lock_forever(void *p1, void *p2, void *p3)
 {
 	zassert_false(k_mutex_lock((struct k_mutex *)p1, K_FOREVER) == 0,
-		     "access locked resource from spawn thread");
+		      "access locked resource from spawn thread");
 	/* should not hit here */
 }
 
@@ -96,13 +96,13 @@ static void tmutex_test_lock_unlock(struct k_mutex *pmutex)
 {
 	k_mutex_init(pmutex);
 	zassert_true(k_mutex_lock(pmutex, K_FOREVER) == 0,
-		    "fail to lock K_FOREVER");
+		     "fail to lock K_FOREVER");
 	k_mutex_unlock(pmutex);
 	zassert_true(k_mutex_lock(pmutex, K_NO_WAIT) == 0,
-		    "fail to lock K_NO_WAIT");
+		     "fail to lock K_NO_WAIT");
 	k_mutex_unlock(pmutex);
 	zassert_true(k_mutex_lock(pmutex, TIMEOUT) == 0,
-		    "fail to lock TIMEOUT");
+		     "fail to lock TIMEOUT");
 	k_mutex_unlock(pmutex);
 }
 
