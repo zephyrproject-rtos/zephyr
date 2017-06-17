cff71db53b2d3f9d04dc5cec7aa6f0b64b8f34ef
Anas Nashif <anas.nashif@intel.com>
2017-06-16 22:07:22 -0400

    tests: queue: fix code style
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/kernel/queue/src/main.c
+++ tests/kernel/queue/src/main.c
@@ -22,10 +22,10 @@ extern void test_queue_loop(void);
 void test_main(void *p1, void *p2, void *p3)
 {
 	ztest_test_suite(test_queue_api,
-		ztest_unit_test(test_queue_thread2thread),
-		ztest_unit_test(test_queue_thread2isr),
-		ztest_unit_test(test_queue_isr2thread),
-		ztest_unit_test(test_queue_get_fail),
-		ztest_unit_test(test_queue_loop));
+			 ztest_unit_test(test_queue_thread2thread),
+			 ztest_unit_test(test_queue_thread2isr),
+			 ztest_unit_test(test_queue_isr2thread),
+			 ztest_unit_test(test_queue_get_fail),
+			 ztest_unit_test(test_queue_loop));
 	ztest_run_test_suite(test_queue_api);
 }
