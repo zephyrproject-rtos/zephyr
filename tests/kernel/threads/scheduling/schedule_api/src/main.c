00b55663f18e07d90c8b00d6b2a8bed6e65bf85a
Anas Nashif <anas.nashif@intel.com>
2017-06-16 22:12:46 -0400

    tests: schedule_api: fix style
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/kernel/threads/scheduling/schedule_api/src/main.c
+++ tests/kernel/threads/scheduling/schedule_api/src/main.c
@@ -19,17 +19,17 @@
 void test_main(void *p1, void *p2, void *p3)
 {
 	ztest_test_suite(test_threads_scheduling,
-		ztest_unit_test(test_priority_cooperative),
-		ztest_unit_test(test_priority_preemptible),
-		ztest_unit_test(test_yield_cooperative),
-		ztest_unit_test(test_sleep_cooperative),
-		ztest_unit_test(test_sleep_wakeup_preemptible),
-		ztest_unit_test(test_time_slicing_preemptible),
-		ztest_unit_test(test_time_slicing_disable_preemptible),
-		ztest_unit_test(test_lock_preemptible),
-		ztest_unit_test(test_unlock_preemptible),
-		ztest_unit_test(test_sched_is_preempt_thread),
-		ztest_unit_test(test_slice_reset)
-		);
+			 ztest_unit_test(test_priority_cooperative),
+			 ztest_unit_test(test_priority_preemptible),
+			 ztest_unit_test(test_yield_cooperative),
+			 ztest_unit_test(test_sleep_cooperative),
+			 ztest_unit_test(test_sleep_wakeup_preemptible),
+			 ztest_unit_test(test_time_slicing_preemptible),
+			 ztest_unit_test(test_time_slicing_disable_preemptible),
+			 ztest_unit_test(test_lock_preemptible),
+			 ztest_unit_test(test_unlock_preemptible),
+			 ztest_unit_test(test_sched_is_preempt_thread),
+			 ztest_unit_test(test_slice_reset)
+			 );
 	ztest_run_test_suite(test_threads_scheduling);
 }
