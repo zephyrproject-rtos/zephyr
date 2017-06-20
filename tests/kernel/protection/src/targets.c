ce8a47ab72f47ee3733fc474ffd23cf0a3c94cb5
Stephen Smalley <sds@tycho.nsa.gov>
2017-06-20 08:54:24 -0400

    tests: Add a self-protection test suite
    Add a self-protection test suite with a set of tests
    to check whether one can overwrite read-only data
    and text, and whether one can execute from data,
    stack, or heap buffers.  These tests are modeled after
    a subset of the lkdtm tests in the Linux kernel.
    
    These tests have twice caught bugs in the Zephyr NXP MPU
    driver, once during initial testing/review of the code
    (in its earliest forms on gerrit, reported to the original
    author there) and most recently the regression introduced
    by commit bacbea6e21275bf5 ("arm: nxp: mpu: Rework handling
    of region descriptor 0"), which was fixed by
    commit a8aa9d4f3dbbe8 ("arm: nxp: mpu: Fix region descriptor
    0 attributes") after being reported.
    
    This is intended to be a testsuite of self-protection features
    rather than just a test of MPU functionality.  It is envisioned
    that these tests will be expanded to cover a wider range of
    protection features beyond just memory protection, and the
    current tests are independent of any particular enforcement
    mechanism (e.g. MPU, MMU, or other).
    
    The tests are intended to be cross-platform, and have been
    built and run on both x86- and ARM-based boards.  The tests
    currently fail on x86-based boards, but this is an accurate
    reflection of current protections and should change as MMU
    support arrives.
    
    The tests leverage the ztest framework, making them suitable
    for incorporation into automated regression testing for Zephyr.
    
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>

---
+++ tests/kernel/protection/src/targets.c
@@ -0,0 +1,14 @@
+#include <zephyr.h>
+#include <misc/printk.h>
+
+#include "targets.h"
+
+const u32_t rodata_var = RODATA_VALUE;
+
+u8_t data_buf[BUF_SIZE] __aligned(sizeof(int));
+
+int overwrite_target(int i)
+{
+	printk("text not modified\n");
+	return (i - 1);
+}
