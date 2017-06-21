27ef01aa2d4c6817857b385b014069e47a414906
Carles Cufi <carles.cufi@nordicsemi.no>
2017-06-21 12:30:01 +0200

    Bluetooth: controller: Add whitelist population with privacy
    When using privacy, an additional cache of the actual privacy peers is
    required to avoid additional processing in the ISR (since some of the
    peers in the whitelist will be disabled by the corresponding privacy
    settings). Add the cache and populate the actual whitelist just before
    advertising, scanning or initiating a connection.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ll_filter.h
+++ subsys/bluetooth/controller/ll_sw/ll_filter.h
@@ -13,11 +13,13 @@ struct ll_filter {
 };
 
 void ll_filter_reset(bool init);
+void ll_filters_adv_update(u8_t adv_fp);
+void ll_filters_scan_update(u8_t scan_fp);
 
 struct ll_filter *ctrl_filter_get(void);
 
 bool ctrl_rl_enabled(void);
 void ll_rl_rpa_update(bool timeout);
 
-int ll_rl_idx_find(u8_t id_addr_type, u8_t *id_addr);
+int ll_rl_find(u8_t id_addr_type, u8_t *id_addr);
 void ll_rl_pdu_adv_update(int idx, struct pdu_adv *pdu);
--- subsys/bluetooth/controller/ll_sw/ll_scan.c
+++ subsys/bluetooth/controller/ll_sw/ll_scan.c
@@ -13,6 +13,7 @@
 #include "pdu.h"
 #include "ctrl.h"
 #include "ll.h"
+#include "ll_filter.h"
 
 static struct {
 	u16_t interval;
@@ -25,7 +26,7 @@ static struct {
 #endif /* !CONFIG_BLUETOOTH_CONTROLLER_ADV_EXT */
 
 	u8_t  tx_addr:1;
-	u8_t  filter_policy:1;
+	u8_t  filter_policy:2;
 } ll_scan;
 
 u32_t ll_scan_params_set(u8_t type, u16_t interval, u16_t window,
@@ -67,6 +68,9 @@ u32_t ll_scan_enable(u8_t enable)
 		return 0;
 	}
 
+#if defined(CONFIG_BLUETOOTH_CONTROLLER_PRIVACY)
+	ll_filters_scan_update(ll_scan.filter_policy);
+#endif
 	status = radio_scan_enable(ll_scan.type, ll_scan.tx_addr,
 				   ll_addr_get(ll_scan.tx_addr, NULL),
 				   ll_scan.interval, ll_scan.window,
