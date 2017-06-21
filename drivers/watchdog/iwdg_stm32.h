6d4da19602dbcf68b99347bf6983712be79e397d
Adam Podogrocki <adam.apodogrocki@rndity.com>
2017-06-21 10:36:18 +0200

    iwdg: make independent watchdog driver compliant with STM32Cube LL API
    Appropriate inclusion done for F1/F3/F4/L4 families in SoC tree.
    
    JIRA: ZEP-2347
    
    Signed-off-by: Adam Podogrocki <adam.podogrocki@rndity.com>

--- drivers/watchdog/iwdg_stm32.h
+++ drivers/watchdog/iwdg_stm32.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016 Open-RnD Sp. z o.o.
+ * Copyright (c) 2017 RnDity Sp. z o.o.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -12,65 +13,23 @@
 /**
  * @brief Driver for Independent Watchdog (IWDG) for STM32 MCUs
  *
- * Based on reference manual:
- *   STM32F101xx, STM32F102xx, STM32F103xx, STM32F105xx and STM32F107xx
- *   advanced ARM(r)-based 32-bit MCUs
+ * The driver targets all STM32 SoCs. For details please refer to
+ * an appropriate reference manual and look for chapter called:
  *
- * Chapter 19: Independent watchdog (IWDG)
+ *   Independent watchdog (IWDG)
  *
  */
 
-
-/* counter reload trigger */
-#define STM32_IWDG_KR_RELOAD 0xaaaa
-/* magic value for unlocking write access to PR and RLR */
-#define STM32_IWDG_KR_UNLOCK 0x5555
-/* watchdog start */
-#define STM32_IWDG_KR_START  0xcccc
-
-/* 19.4.1 IWDG_KR */
-union __iwdg_kr {
-	u32_t val;
-	struct {
-		u16_t key;
-		u16_t rsvd;
-	} bit;
+/* driver data */
+struct iwdg_stm32_data {
+	/* IWDG peripheral instance. */
+	IWDG_TypeDef *Instance;
 };
 
-/* 19.4.2 IWDG_PR */
-union __iwdg_pr {
-	u32_t val;
-	struct {
-		u32_t pr :3 __packed;
-		u32_t rsvd__3_31 :29 __packed;
-	} bit;
-};
+#define IWDG_STM32_DATA(dev)					\
+	((struct iwdg_stm32_data * const)(dev)->driver_data)
 
-/* 19.4.3 IWDG_RLR */
-union __iwdg_rlr {
-	u32_t val;
-	struct {
-		u32_t rl :12 __packed;
-		u32_t rsvd__12_31 :20 __packed;
-	} bit;
-};
-
-/* 19.4.4 IWDG_SR */
-union __iwdg_sr {
-	u32_t val;
-	struct {
-		u32_t pvu :1 __packed;
-		u32_t rvu :1 __packed;
-		u32_t rsvd__2_31 :30 __packed;
-	} bit;
-};
-
-/* 19.4.5 IWDG register map */
-struct iwdg_stm32 {
-	union __iwdg_kr kr;
-	union __iwdg_pr pr;
-	union __iwdg_rlr rlr;
-	union __iwdg_sr sr;
-};
+#define IWDG_STM32_STRUCT(dev)					\
+	((IWDG_TypeDef *)(IWDG_STM32_DATA(dev))->Instance)
 
 #endif	/* _STM32_IWDG_H_ */
