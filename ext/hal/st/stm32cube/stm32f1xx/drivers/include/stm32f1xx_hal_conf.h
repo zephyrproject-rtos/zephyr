f2d190f8d6a5bee22fecf3e860a63fe7750e456c
Jorge Ramirez-Ortiz <jorge.ramirez-ortiz@linaro.org>
2017-06-23 12:11:34 +0200

    ext: hal: stm32f1x and stm32f4x: disable i2c HAL
    Enabling the HAL and the LL flags duplicate definitions when
    compiling in driver.
    
    Since the i2c HAL is not needed, disable it.
    This is a HAL issue that needs to be addressed (not zephyr)
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez-ortiz@linaro.org>

--- ext/hal/st/stm32cube/stm32f1xx/drivers/include/stm32f1xx_hal_conf.h
+++ ext/hal/st/stm32cube/stm32f1xx/drivers/include/stm32f1xx_hal_conf.h
@@ -51,6 +51,12 @@
 /* ########################## Module Selection ############################## */
 /**
   * @brief This is the list of modules to be used in the HAL driver
+  *
+  *     Note:
+  *     	HAL_I2C_MODULE_ENABLED had to be disabled due to the HAL
+  *     	redefining I2C_SPEED_STANDARD and I2C_SPEED_FAST when the
+  *     	I2C LL driver was enabled.
+  *
   */
 #define HAL_MODULE_ENABLED
 #define HAL_ADC_MODULE_ENABLED
@@ -64,7 +70,6 @@
 #define HAL_FLASH_MODULE_ENABLED
 #define HAL_GPIO_MODULE_ENABLED
 #define HAL_HCD_MODULE_ENABLED
-#define HAL_I2C_MODULE_ENABLED
 #define HAL_I2S_MODULE_ENABLED
 #define HAL_IRDA_MODULE_ENABLED
 #define HAL_IWDG_MODULE_ENABLED
@@ -84,6 +89,23 @@
 #define HAL_USART_MODULE_ENABLED
 #define HAL_WWDG_MODULE_ENABLED
 #define HAL_MMC_MODULE_ENABLED
+
+/**
+ * HAL_I2C_MODULE_ENABLED: [disabled]
+ * ----------------------------------
+ *
+ * When enabling the STM32 I2C LL driver this would otherwise trigger:
+ *
+ *  include/i2c.h:34:0: warning: "I2C_SPEED_STANDARD" redefined
+ *  stm32f1xx_hal_i2c.h:578:0: this is the location of the previous definition
+ *
+ *  include/i2c.h:37:0: warning: "I2C_SPEED_FAST" redefined
+ *  stm32f1xx_hal_i2c.h:579:0:  this is the location of the previous definition
+ *
+ */
+#if 0
+#define HAL_I2C_MODULE_ENABLED
+#endif
 
 /* ########################## Oscillator Values adaptation ####################*/
 /**
