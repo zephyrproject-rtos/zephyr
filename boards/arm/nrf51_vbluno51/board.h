2c0f99f65c1b984df8f54d1f46340f4c420df7c2
Nam Do <robotden@gmail.com>
2017-06-29 11:02:40 +0700

    boards: arm: Add support for the VBLUno51 board
        + VBLUno51 board
            nRF51822
            Bluetooth Low Energy
            DAPLink interface
            UNO pinout
            4 power
    
        + Wiki: https://vngiotlab.github.io/vbluno/
    
        + The following samples were tested:
            hello_world
            basic/button
            basic/blinky
            bluetooth/peripheral_hr
            bluetooth/beacon
    
    Signed-off-by: Nam Do <robotden@gmail.com>

---
+++ boards/arm/nrf51_vbluno51/board.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017 VNG IoT Lab Limited.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+/* Button*/
+#define BUT_GPIO_PIN	15
+#define BUT_GPIO_NAME	CONFIG_GPIO_NRF5_P0_DEV_NAME
+#define SW0_GPIO_PIN	BUT_GPIO_PIN
+#define SW0_GPIO_NAME	BUT_GPIO_NAME
+
+/* Onboard GREEN LED 0 */
+#define LED0_GPIO_PIN   7
+#define LED0_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+#define LED_GPIO_PIN    LED0_GPIO_PIN
+#define LED_GPIO_PORT   LED0_GPIO_PORT
+
+#endif /* __INC_BOARD_H */
