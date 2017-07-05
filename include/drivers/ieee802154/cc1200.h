8ded57d3a2de381d64c81d880f392c38f9218d77
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-07-05 16:56:49 +0200

    drivers/ieee802154: Add support for Sub-GHz TI CC1200 chip
    CC1200 is a sub-ghz chip supporting 6 ISM & SRD bands: 169, 433, 470,
    868, 915 and 920 MHz, with features dedicated to IEEE 802.15.4(g).
    
    Current driver enables CC1200 against actual IEEE 802.15.4 Soft-MAC. 'g'
    version support in the Soft-MAC will follow later.
    
    The chip itself is closer to a bare metal radio modem than to a usual
    15.4 chip: up to the user to provide the right RF settings for the
    carrier band. Such settings can be generaten through TI's SmartRF tool.
    
    Hopefully, for channel selection, this driver will be clever enough to
    compute the proper register change without any special input from the
    user. This will work for all the bands supported by the chip.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

---
+++ include/drivers/ieee802154/cc1200.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __CC1200_H__
+#define __CC1200_H__
+
+#include <device.h>
+
+/* RF settings
+ *
+ * First 42 entries are for the 42 first registers from
+ * address 0x04 to 0x2D included.
+ * Next, the last 58 entries are for the 58 registers from
+ * extended address 0x00 to 0x39 included
+ *
+ * If CONFIG_IEEE802154_CC1200_RF_PRESET is not used, one will need
+ * no provide 'cc1200_rf_settings' with proper settings. These can
+ * be generated through TI's SmartRF application.
+ *
+ */
+struct cc1200_rf_registers_set {
+	u32_t chan_center_freq0;
+	u16_t channel_limit;
+	/* to fit in u16_t, spacing is a multiple of 100 Hz,
+	 * 12.5KHz for instance will be 125.
+	 */
+	u16_t channel_spacing;
+	u8_t registers[100];
+};
+
+#ifndef CONFIG_IEEE802154_CC1200_RF_PRESET
+extern const struct cc1200_rf_registers_set cc1200_rf_settings;
+#endif
+
+/* Note for EMK & EM adapter booster pack users:
+ * SPI pins are easy, RESET as well, but when it comes to GPIO:
+ * CHIP -> EM adapter
+ * GPIO0 -> GPIOA
+ * GPIO1 -> reserved (it's SPI MISO)
+ * GPIO2 -> GPIOB
+ * GPIO3 -> GPIO3
+ */
+
+enum cc1200_gpio_index {
+	CC1200_GPIO_IDX_GPIO0,
+	CC1200_GPIO_IDX_MAX,
+};
+
+struct cc1200_gpio_configuration {
+	struct device *dev;
+	u32_t pin;
+};
+
+struct cc1200_gpio_configuration *cc1200_configure_gpios(void);
+
+#endif /* __CC1200_H__ */
