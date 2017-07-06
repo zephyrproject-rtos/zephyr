139d759b2a5f894692ed250dd00c76a41a1af677
Li Liu <li.liu@linaro.org>
2017-07-06 16:34:22 +0800

    watchdog: Add WDT driver for rda5981a (ARM Cortex-M4 Wireless MCU)
    Signed-off-by: Li Liu <li.liu@linaro.org>

---
+++ drivers/watchdog/wdt_rda5981a.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2017 RDA
+ * Copyright (c) 2016-2017 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+#ifndef _RDA_WDT_H_
+#define _RDA_WDT_H_
+
+/** WDT HAL macro
+ */
+#define WDT_EN_BIT	9
+#define WDT_CLR_BIT	10
+#define WDT_TMRCNT_OFST	11
+#define WDT_TMRCNT_WIDTH	4
+
+#define WDT_TIMER	2
+
+
+#endif	/* _RDA_WDT_H_ */
