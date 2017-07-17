3d498dd10ccb6cd6715cbf26315d1c21b9cc50ce
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2017-07-17 15:11:03 -0700

    build: workaround build bug in tests
    When an app uses a construct such as:
    
    obj-y = main.o ../../../../samples/bluetooth/gatt/hrs.o
    
    in its makefile, it causes said object module to be built in the
    source tree, not in the object tree.
    
    When building massively parallel, this usually resuls on the files
    getting corrupted, leading to bugs such as:
    
    https://jira.zephyrproject.org/browse/ZEP-2316
    https://jira.zephyrproject.org/browse/ZEP-2317
    
    src/../../../../samples/bluetooth/gatt/.gap.o.cmd:3: warning: NUL character seen; rest of line ignored
    src/../../../../samples/bluetooth/gatt/.gap.o.cmd:4: warning: NUL character seen; rest of line ignored
    src/../../../../samples/bluetooth/gatt/.gap.o.cmd:5: *** missing separator.  Stop.
    
    as multiple build are trying to touch the same file in the source tree
    and of course, race and causes a build bug.
    
    We have known about this issue for a long time, but it requires
    modifications in the build system that there is no time to tackle.
    
    A suggested workaround is to include the source files into a local .c
    file, so this is what this patch does, to remove the random noise.
    
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>

---
+++ samples/bluetooth/peripheral_hr/src/hrs.c
@@ -0,0 +1,2 @@
+/* Workaround build system bug that will put objects in source dir */
+#include "../../gatt/hrs.c"
---
+++ tests/crypto/ecc_dh/src/test_ecc_utils.c
@@ -0,0 +1,2 @@
+/* Workaround build system bug that will put objects in source dir */
+#include "../../ecc_dsa/src/test_ecc_utils.c"
