c42b2ca0c8de8db34310b2f2f1b2fccf12a16c90
Gil Pitney <gil.pitney@linaro.org>
2017-07-17 17:56:22 -0700

    cc32xx: watchdog fixes and added debugger stall option
    This patch fixes basic watchdog timer support.
    
    True watchdog reset support is requires processing the
    WDT_MODE configuration argument to wdt_set_config().
    
    Signed-off-by: Gil Pitney <gil.pitney@linaro.org>

--- drivers/watchdog/wdt_cc32xx.c
+++ drivers/watchdog/wdt_cc32xx.c
@@ -74,9 +74,10 @@ static void wdt_cc32xx_get_config(struct device *dev, struct wdt_config *config)
 static void wdt_cc32xx_reload(struct device *dev)
 {
 	ARG_UNUSED(dev);
+	MAP_WatchdogUnlock(WDT_BASE);
 	MAP_WatchdogReloadSet(WDT_BASE, CONFIG_WDT_CC32XX_RELOAD_TIME *
 			      CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC);
-	MAP_WatchdogIntClear(WDT_BASE);
+	MAP_WatchdogLock(WDT_BASE);
 }
 
 static const struct wdt_driver_api wdt_cc32xx_api = {
@@ -94,22 +95,29 @@ static int wdt_cc32xx_init(struct device *dev)
 	/* store ref for callback */
 	wdog_r = dev;
 
-	MAP_PRCMPeripheralClkEnable(PRCM_WDT, PRCM_RUN_MODE_CLK);
-
-	/* Check to see if the registers are locked, and if so, unlock them. */
-	if (MAP_WatchdogLockState(WDT_BASE) == true) {
-		MAP_WatchdogUnlock(WDT_BASE);
+	MAP_PRCMPeripheralClkEnable(PRCM_WDT,
+				    PRCM_RUN_MODE_CLK | PRCM_SLP_MODE_CLK);
+	/* spin until status returns TRUE */
+	while(!MAP_PRCMPeripheralStatusGet(PRCM_WDT)) {
 	}
 
+	MAP_WatchdogUnlock(WDT_BASE);
 	MAP_WatchdogReloadSet(WDT_BASE, CONFIG_WDT_CC32XX_RELOAD_TIME *
 			      CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC);
 	MAP_WatchdogIntClear(WDT_BASE);
+
+	/* Set debug stall mode */
+#ifdef CONFIG_WDT_CC32XX_DEBUGGER_STALL_MODE
+	MAP_WatchdogStallEnable(WDT_BASE);
+#endif
 
 	IRQ_CONNECT(EXCEPTION_WDT0, CONFIG_WDT_CC32XX_IRQ_PRI,
 		    wdt_cc32xx_isr, DEVICE_GET(wdt_cc32xx), 0);
 
 	MAP_IntPendClear(INT_WDT);
 	irq_enable(EXCEPTION_WDT0);
+
+	MAP_WatchdogLock(WDT_BASE);
 
 	return 0;
 }
