99692a38dd0cb1458c2222c29991fe3bb44ad29c
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-07-18 11:53:06 +0300

    include: kernel: Fix use of K_POLL_MODE_INFORM_ONLY in docstring
    K_POLL_MODE_INFORM_ONLY was renamed to K_POLL_MODE_NOTIFY_ONLY, but
    stale use was in a docstring.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- include/kernel.h
+++ include/kernel.h
@@ -3646,7 +3646,7 @@ struct k_poll_event {
  *             values. Only values that apply to the same object being polled
  *             can be used together. Choosing K_POLL_TYPE_IGNORE disables the
  *             event.
- * @param mode Future. Use K_POLL_MODE_INFORM_ONLY.
+ * @param mode Future. Use K_POLL_MODE_NOTIFY_ONLY.
  * @param obj Kernel object or poll signal.
  *
  * @return N/A
