dbba0b6891bc984ea3fd090170cc80f222acffdf
Robert Chou <robert.ch.chou@acer.com>
2017-07-25 16:54:25 +0800

    net: lwm2m: add firmware push support
    1. Add handling block1 option in handle_request(). The basic idea is
       to declare structure block_context at compiled time and use "token"
       as a key to pick up the on-going block cotext. It should be able to
       support multiple blockwise transfer concurrently
    2. Use write callback implemented in lwm2m_obj_firmware to deal w/ the
       update state transition and than call the callback registered by the
       application
    3. move default_block_size to lwm2m_engine.c to share between
       lwm2m_engine and lwm2m_obj_firmware_pull
    
    Signed-off-by: Robert Chou <robert.ch.chou@acer.com>
    [michael.scott@linaro.org: rebased on LwM2M net_app changes.]
    Signed-off-by: Michael Scott <michael.scott@linaro.org>

--- subsys/net/lib/lwm2m/lwm2m_engine.h
+++ subsys/net/lib/lwm2m/lwm2m_engine.h
@@ -103,6 +103,8 @@ void lwm2m_udp_receive(struct lwm2m_ctx *client_ctx, struct net_pkt *pkt,
 		       bool handle_separate_response,
 		       udp_request_handler_cb_t udp_request_handler);
 
+enum zoap_block_size lwm2m_default_block_size(void);
+
 #if defined(CONFIG_LWM2M_FIRMWARE_UPDATE_OBJ_SUPPORT)
 u8_t lwm2m_firmware_get_update_state(void);
 void lwm2m_firmware_set_update_state(u8_t state);
