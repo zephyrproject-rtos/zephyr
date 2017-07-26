a87073471fd76e69bd104083e26cc3db22bb4f99
Andrew Boie <andrew.p.boie@intel.com>
2017-07-25 19:05:51 -0700

    timing_info: fix how stacks are referenced
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- tests/benchmarks/timing_info/src/msg_passing_bench.c
+++ tests/benchmarks/timing_info/src/msg_passing_bench.c
@@ -71,8 +71,8 @@ k_tid_t thread_mbox_async_put_receive_tid;
 
 /* To time thread creation*/
 #define STACK_SIZE 500
-extern char my_stack_area[];
-extern char my_stack_area_0[];
+extern K_THREAD_STACK_DEFINE(my_stack_area, STACK_SIZE);
+extern K_THREAD_STACK_DEFINE(my_stack_area_0, STACK_SIZE);
 extern struct k_thread my_thread;
 extern struct k_thread my_thread_0;
 
--- tests/benchmarks/timing_info/src/yield_bench.c
+++ tests/benchmarks/timing_info/src/yield_bench.c
@@ -14,8 +14,8 @@ K_SEM_DEFINE(yield_sem, 0, 1);
 
 /* To time thread creation*/
 #define STACK_SIZE 500
-extern char my_stack_area[];
-extern char my_stack_area_0[];
+extern K_THREAD_STACK_DEFINE(my_stack_area, STACK_SIZE);
+extern K_THREAD_STACK_DEFINE(my_stack_area_0, STACK_SIZE);
 extern struct k_thread my_thread;
 extern struct k_thread my_thread_0;
 
