56559a7d0c2d3b091600803661a63cd06bad0016
Andrew Boie <andrew.p.boie@intel.com>
2017-08-01 15:22:06 -0700

    x86: enable MMU for application memory
    Configuring the RAM/ROM regions will be the same for all
    x86 targets as this is done with linker symbols.
    
    Peripheral configuration left at the SOC level.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- arch/x86/soc/ia32/soc.c
+++ arch/x86/soc/ia32/soc.c
@@ -21,16 +21,7 @@
 #include <linker/linker-defs.h>
 
 #ifdef CONFIG_X86_MMU
-/* Mark text and rodata as read-only, for XIP rest of flash unmapped */
-MMU_BOOT_REGION((u32_t)&_image_rom_start, (u32_t)&_image_rom_size,
-		MMU_ENTRY_READ);
-
-/* From _image_ram_start to the end of all RAM, read/write */
-MMU_BOOT_REGION((u32_t)&_image_ram_start, (u32_t)&_image_ram_all,
-		MMU_ENTRY_WRITE);
-
 MMU_BOOT_REGION(CONFIG_LOAPIC_BASE_ADDRESS, KB(4), MMU_ENTRY_WRITE);
 MMU_BOOT_REGION(CONFIG_IOAPIC_BASE_ADDRESS, MB(1), MMU_ENTRY_WRITE);
 MMU_BOOT_REGION(CONFIG_HPET_TIMER_BASE_ADDRESS, KB(4), MMU_ENTRY_WRITE);
-
 #endif /* CONFIG_X86_MMU*/
