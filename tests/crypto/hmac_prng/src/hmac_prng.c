26bacab162a5e5b171c5300290105fe8e58b7574
Niranjhana N <niranjhana.n@intel.com>
2017-08-02 13:32:18 +0530

    tests: hmac_prng: convert to ztest
    - replaced test points with ztest APIs wherever possible
    - replaced CONFIG_MAIN_STACK_SIZE with CONFIG_ZTEST_STACKSIZE
      in the config file
    - split the main file into two files:
        - main.c has ztest entry and runs separate functions for ztest
        - hmac_prng.c has the original routines
    
    JIRA: ZEP-2449
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>

--- tests/crypto/hmac_prng/src/hmac_prng.c
+++ tests/crypto/hmac_prng/src/hmac_prng.c
@@ -46,12 +46,13 @@
 #include <stdio.h>
 #include <tc_util.h>
 #include <drivers/system_timer.h>
+#include <ztest.h>
 
 /*
  * Main task to test AES
  */
 
-void main(void)
+void test_hmac_prng(void)
 {
 	/* Fake personalization and additional_input
 	 * (replace by appropriate values)
@@ -60,7 +61,6 @@ void main(void)
 	u8_t additional_input[] = "additional input";
 	u8_t personalization[] = "HOSTNAME";
 	u32_t size = (1 << 15);
-	u32_t result = TC_PASS;
 	struct tc_hmac_prng_struct h;
 	u8_t random[size];
 	u8_t seed[128];
@@ -75,34 +75,23 @@ void main(void)
 	}
 
 	TC_PRINT("HMAC-PRNG test#1 (init):\n");
-	if (tc_hmac_prng_init(&h, personalization,
-			      sizeof(personalization)) == 0) {
-		TC_ERROR("HMAC-PRNG initialization failed.\n");
-		result = TC_FAIL;
-		goto exitTest;
-	}
-	TC_END_RESULT(result);
+
+	/**TESTPOINT: Check HMAC-PRNG init*/
+	zassert_true(tc_hmac_prng_init(&h, personalization,
+		sizeof(personalization)), "HMAC-PRNG initialization failed.");
 
 	TC_PRINT("HMAC-PRNG test#1 (reseed):\n");
-	if (tc_hmac_prng_reseed(&h, seed, sizeof(seed), additional_input,
-				sizeof(additional_input)) == 0) {
-		TC_ERROR("HMAC-PRNG reseed failed.\n");
-		result = TC_FAIL;
-		goto exitTest;
-	}
-	TC_END_RESULT(result);
+
+	/**TESTPOINT: Check HMAC-PRNG reseed*/
+	zassert_true(tc_hmac_prng_reseed(&h, seed, sizeof(seed),
+				additional_input, sizeof(additional_input)),
+			"HMAC-PRNG reseed failed.");
 
 	TC_PRINT("HMAC-PRNG test#1 (generate):\n");
-	if (tc_hmac_prng_generate(random, size, &h) < 1) {
-		TC_ERROR("HMAC-PRNG generate failed.\n");
-		result = TC_FAIL;
-		goto exitTest;
-	}
-	TC_END_RESULT(result);
+
+	/**TESTPOINT: Check HMAC-PRNG generate*/
+	zassert_false(tc_hmac_prng_generate(random, size, &h) < 1,
+			"HMAC-PRNG generate failed.");
 
 	TC_PRINT("All HMAC tests succeeded!\n");
-
-exitTest:
-	TC_END_RESULT(result);
-	TC_END_REPORT(result);
 }
