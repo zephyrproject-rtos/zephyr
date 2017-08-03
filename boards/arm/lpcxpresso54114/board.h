633f6badbd4de0cd9e56be1251a12f68adc76c84
Shiksha Patel <shiksha.patel@nxp.com>
2017-08-03 08:35:35 -0500

    lpc: Add lpcxpresso54114 board based on its architecture.
    Add default drivers , pinmux configurations and other
    
    board level configuration parameters in respective Kconfigs.
    
    Add board dts and fixup file for lpcxpresso54114 in /dts/arm/.
    
    Modify Makefile in /dts/arm/ to include lpcxpresso54114.
    
    Add lpcxpresso54114.png and lpcxpresso54114.rst in doc in
    
    boards.
    
    Signed-off-by: Shiksha Patel <shiksha.patel@nxp.com>

---
+++ boards/arm/lpcxpresso54114/board.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2017, NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+/* Red LED */
+#define RED_GPIO_NAME	CONFIG_GPIO_LPC_MCUX_PORT0_NAME
+#define RED_GPIO_PIN    29
+
+/* Green LED */
+#define GREEN_GPIO_NAME CONFIG_GPIO_LPC_MCUX_PORT1_NAME
+#define GREEN_GPIO_PIN  10
+
+/* Blue LED */
+#define BLUE_GPIO_NAME  CONFIG_GPIO_MCUX_PORT1_NAME
+#define BLUE_GPIO_PIN   9
+
+/* LED0. There is no physical LED on the board with this name, so create an
+ * alias to the green LED to make the basic blinky sample work.
+ */
+#define LED0_GPIO_PORT  GREEN_GPIO_NAME
+#define LED0_GPIO_PIN   GREEN_GPIO_PIN
+
+#endif /* __INC_BOARD_H */
