50f112cdc1933782ea7393d6d25ea8b04a4a6a9b
Niranjhana N <niranjhana.n@intel.com>
2017-08-03 11:43:56 +0530

    tests: xip: convert to ztest
    - replaced a test point with ztest API
    - separated the main file into two:
        - main.c, which has ztest entry
        - xip.c, which has the original routine
    
    JIRA: ZEP-2382
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>

--- tests/kernel/xip/src/xip.c
+++ tests/kernel/xip/src/xip.c
@@ -1,5 +1,3 @@
-/* test_xip.c - test XIP */
-
 /*
  * Copyright (c) 2014 Wind River Systems, Inc.
  *
@@ -7,17 +5,17 @@
  */
 
 /*
-DESCRIPTION
-This module tests that XIP performs as expected. If the first
-task is even activated that is a good indication that XIP is
-working. However, the test does do some some testing on
-global variables for completeness sake.
+ * DESCRIPTION
+ * This module tests that XIP performs as expected. If the first
+ * task is even activated that is a good indication that XIP is
+ * working. However, the test does do some some testing on
+ * global variables for completeness sake.
  */
 
 #include <zephyr.h>
 #include <tc_util.h>
 #include "test.h"
-
+#include <ztest.h>
 
 /**
  *
@@ -26,9 +24,8 @@ global variables for completeness sake.
  * @return N/A
  */
 
-void main(void)
+void test_xip(void)
 {
-	int  tcRC = TC_PASS;
 	int  i;
 
 	TC_START("kernel_xip");
@@ -41,14 +38,8 @@ void main(void)
 
 	/* Array should be filled with monotomically incrementing values */
 	for (i = 0; i < XIP_TEST_ARRAY_SZ; i++) {
-		if (xip_array[i] != (i+1)) {
-			TC_PRINT("xip_array[%d] != %d\n", i, i+1);
-			tcRC = TC_FAIL;
-			goto exitRtn;
-		}
-	}
 
-exitRtn:
-	TC_END_RESULT(tcRC);
-	TC_END_REPORT(tcRC);
+		/**TESTPOINT: Check if the array value is correct*/
+		zassert_equal(xip_array[i], (i+1), "Array value is incorrect");
+	}
 }
