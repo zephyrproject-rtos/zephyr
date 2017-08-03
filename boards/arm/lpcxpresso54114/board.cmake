effe0bd728192e3093fed13e0270caa78711251d
Shiksha Patel <shiksha.patel@nxp.com>
2017-08-03 08:35:35 -0500

    lpc: Add lpcxpresso54114 board based on its architecture
    Add default drivers, pinmux configurations, and other board level
    configuration parameters in respective Kconfigs.
    
    Add board dts, fixup, and docs.
    
    Samples tested include:
    - hello_world
    - sychronization
    - basic/blinky
    - basic/threads
    
    Signed-off-by: Shiksha Patel <shiksha.patel@nxp.com>
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

---
+++ boards/arm/lpcxpresso54114/board.cmake
@@ -0,0 +1,15 @@
+#
+# Copyright (c) 2017, NXP
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set_ifndef(LPCLINK_FW jlink)
+
+if(LPCLINK_FW STREQUAL jlink)
+  set_ifndef(BOARD_DEBUG_RUNNER jlink)
+endif()
+
+board_runner_args(jlink "--device=LPC54114J256_M4")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
