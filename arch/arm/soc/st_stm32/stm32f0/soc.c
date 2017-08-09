9b366d843f993db2d061582b0ed34572313b5dca
Maciej Debski <maciej.debski@rndity.com>
2017-08-09 11:20:33 +0200

    arch: arm: soc: provide support for stm32f0.
    Fixes #3923
    
    Signed-off-by: Maciej Debski <maciej.debski@rndity.com>
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>

---
+++ arch/arm/soc/st_stm32/stm32f0/soc.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2017 RnDity Sp. z o.o.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief System/hardware module for STM32F0 processor
+ */
+
+#include <kernel.h>
+#include <device.h>
+#include <init.h>
+#include <soc.h>
+#include <arch/cpu.h>
+#include <cortex_m/exc.h>
+
+/**
+ * @brief This function configures the source of stm32cube time base.
+ *        Cube HAL expects a 1ms tick which matches with k_uptime_get_32.
+ *        Tick interrupt priority is not used
+ * @return HAL status
+ */
+uint32_t HAL_GetTick(void)
+{
+	return k_uptime_get_32();
+}
+
+/**
+ * @brief Perform basic hardware initialization at boot.
+ *
+ * This needs to be run from the very beginning.
+ * So the init priority has to be 0 (zero).
+ *
+ * @return 0
+ */
+static int stm32f0_init(struct device *arg)
+{
+	u32_t key;
+
+	ARG_UNUSED(arg);
+
+	key = irq_lock();
+
+	_ClearFaults();
+
+	/* Install default handler that simply resets the CPU
+	 * if configured in the kernel, NOP otherwise
+	 */
+	NMI_INIT();
+
+	irq_unlock(key);
+
+	/* Update CMSIS SystemCoreClock variable (HCLK) */
+	/* At reset, System core clock is set to 4MHz */
+	SystemCoreClock = 4000000;
+
+	return 0;
+}
+
+SYS_INIT(stm32f0_init, PRE_KERNEL_1, 0);
