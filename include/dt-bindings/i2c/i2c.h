670fc3230d2067af3c49da5e3071b0f53f919b37
Kumar Gala <kumar.gala@linaro.org>
2017-08-10 11:22:24 -0500

    i2c: Cleanup dts clock-frequency property usage
    Clarify that the clock-frequency is the bitrate at boot and introduce
    defines that .dts files can use to set the clock-frequency.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

---
+++ include/dt-bindings/i2c/i2c.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __DT_BINDINGS_I2C_I2C_H
+#define __DT_BINDINGS_I2C_I2C_H
+
+#define I2C_BITRATE_STANDARD	100000	/* 100 Kbit/s */
+#define I2C_BITRATE_FAST	400000	/* 400 Kbit/s */
+#define I2C_BITRATE_FAST_PLUS	1000000 /* 1 Mbit/s */
+#define I2C_BITRATE_HIGH	3400000	/* 3.4 Mbit/s */
+#define I2C_BITRATE_ULTRA	5000000 /* 5 Mbit/s */
+
+#endif /* __DT_BINDINGS_I2C_I2C_H */
