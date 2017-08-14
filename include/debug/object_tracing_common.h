773773a63823a821f83470b58e8aacfce525cb8f
Andrew Boie <andrew.p.boie@intel.com>
2017-08-14 15:24:16 -0700

    object_tracing: fix definition
    This is a header file, the definition for _trace_list_sys_ring_buf
    needs to be 'extern' otherwise multiple instances of this variable
    could be instantiated, leading to linker errors.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- include/debug/object_tracing_common.h
+++ include/debug/object_tracing_common.h
@@ -99,7 +99,7 @@
 
 struct ring_buf;
 
-struct ring_buf   *_trace_list_sys_ring_buf;
+extern struct ring_buf   *_trace_list_sys_ring_buf;
 
 #endif  /*CONFIG_OBJECT_TRACING*/
 #endif  /*_OBJECT_TRACING_COMMON_H_*/
