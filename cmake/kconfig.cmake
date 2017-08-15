74f9a10d0e0a4b2568c39645d5e7b70845531562
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-08-15 17:20:23 +0200

    cmake: CONF_FILE: Allow user to override CONF_FILE
    Also, only default to prj.conf if the file exists.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- cmake/kconfig.cmake
+++ cmake/kconfig.cmake
@@ -5,20 +5,36 @@ file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/kconfig/include/generated)
 file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/kconfig/include/config)
 
 set(BOARD_DEFCONFIG ${PROJECT_SOURCE_DIR}/boards/${ARCH}/${BOARD}/${BOARD}_defconfig)
-set(CONF_FILE       ${APPLICATION_SOURCE_DIR}/prj.conf)
 set(DOTCONFIG       ${PROJECT_BINARY_DIR}/.config)
+
+if(CONF_FILE)
+  # CONF_FILE has been specified on the cmake CLI and has precedence
+elseif(DEFINED ENV{CONF_FILE})
+  set(CONF_FILE $ENV{CONF_FILE})
+elseif(EXISTS   ${APPLICATION_SOURCE_DIR}/prj.conf)
+  set(CONF_FILE ${APPLICATION_SOURCE_DIR}/prj.conf)
+endif()
+
+set(CONF_FILE ${CONF_FILE} CACHE STRING "If desired, you can build the application using\
+the configuration settings specified in an alternate .conf file using this parameter. \
+These settings will override the settings in the application’s .config file or its default .conf file. ")
 
 set(ENV{srctree}            ${PROJECT_SOURCE_DIR})
 set(ENV{KERNELVERSION}      ${PROJECT_VERSION})
 set(ENV{KCONFIG_CONFIG}     ${DOTCONFIG})
 set(ENV{KCONFIG_AUTOHEADER} ${AUTOCONF_H})
 
-# Create new .config if the file does not exists, or the user has edited one of the configuration files.
-if(NOT EXISTS ${DOTCONFIG}
-   OR ${BOARD_DEFCONFIG} IS_NEWER_THAN ${DOTCONFIG}
-   OR ${CONF_FILE}       IS_NEWER_THAN ${DOTCONFIG}
-  )
-
+# Create new .config if the file does not exists, or the user has
+# edited one of the configuration files.
+set(CREATE_NEW_DOTCONFIG "")
+if(NOT EXISTS ${DOTCONFIG} OR ${BOARD_DEFCONFIG} IS_NEWER_THAN ${DOTCONFIG})
+  set(CREATE_NEW_DOTCONFIG 1)
+elseif(${CONF_FILE})
+  if(${CONF_FILE} IS_NEWER_THAN ${DOTCONFIG})
+    set(CREATE_NEW_DOTCONFIG 1)
+  endif()
+endif()
+if(CREATE_NEW_DOTCONFIG)
   execute_process(
     COMMAND ${PYTHON_EXECUTABLE} ${PROJECT_SOURCE_DIR}/scripts/kconfig/merge_config.py -m -q
       -O ${PROJECT_BINARY_DIR}
