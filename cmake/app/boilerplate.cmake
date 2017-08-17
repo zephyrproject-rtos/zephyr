7c7595cd16bef0e8ba2a88d5b6d6ba71ab5354bf
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-08-17 18:49:15 +0200

    cmake: Restructure relationship between app and zephyr
    Trying to port hci_uart exposed several problems with how applications
    and zephyr build scripts interact.
    
    The application is now a normal CMake library instead of an OBJECT
    library. This allows the user to link an application against a Zephyr
    CMake library.
    
    The CMake API that allows you to globally modify the compiler flags
    etc. has been deprecated in favour of explicitly attributing
    compilation options to targets. The zephyr library took over all of
    the global options.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- cmake/app/boilerplate.cmake
+++ cmake/app/boilerplate.cmake
@@ -1,8 +1,9 @@
+
 # This file must be included into the toplevel CMakeLists.txt file of
 # Zephyr applications, e.g. zephyr/samples/hello_world/CMakeLists.txt
 # must start with the line:
 #
-# include(ENV${ZEPHYR_BASE}/cmake/app/boilerplate.cmake)
+# include($ENV{ZEPHYR_BASE}/cmake/app/boilerplate.cmake)
 #
 # It exists to reduce boilerplate code that Zephyr expects to be in
 # application CMakeLists.txt code.
@@ -10,9 +11,12 @@
 # Omitting it is permitted, but doing so incurs a maintenance cost as
 # the application must manage upstream changes to this file.
 
-cmake_minimum_required(VERSION 3.7)
+add_library(app STATIC "")
+target_link_libraries(app zephyr)
 
-add_subdirectory($ENV{ZEPHYR_BASE})
+set(__build_dir ${CMAKE_CURRENT_BINARY_DIR}/zephyr)
+add_subdirectory($ENV{ZEPHYR_BASE} ${__build_dir})
 
-# TODO: What is this for?
-add_subdirectory(${CMAKE_CURRENT_BINARY_DIR}/zephyr)
+# To reduce boilerplate in application CMakeLists.txt code we choose
+# to omit cmake_minimum_version()
+cmake_policy(SET CMP0000 OLD)
