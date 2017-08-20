1153d21f4113d9f8e20d0483c4601fa06323c7b5
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-08-20 18:27:38 +0200

    cmake: Initial x86 support
    Still needs a lot of testing and changes but blinky now builds on
    arduino_101 at least.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- cmake/app/boilerplate.cmake
+++ cmake/app/boilerplate.cmake
@@ -1,4 +1,3 @@
-
 # This file must be included into the toplevel CMakeLists.txt file of
 # Zephyr applications, e.g. zephyr/samples/hello_world/CMakeLists.txt
 # must start with the line:
@@ -11,12 +10,41 @@
 # Omitting it is permitted, but doing so incurs a maintenance cost as
 # the application must manage upstream changes to this file.
 
-add_library(app STATIC "")
-target_link_libraries(app zephyr)
+# app is a CMake library containing all the application code and is
+# modified by the entry point ${APPLICATION_SOURCE_DIR}/CMakeLists.txt
+# that was specified when cmake was called.
+
+# To reduce boilerplate in application CMakeLists.txt code we choose
+# to omit cmake_minimum_version()
+cmake_minimum_required(VERSION 3.7)
+cmake_policy(SET CMP0000 OLD)
+cmake_policy(SET CMP0002 NEW)
+
+include($ENV{ZEPHYR_BASE}/cmake/extensions.cmake)
+
+zephyr_library_named(app)
+target_link_libraries(app zephyr) # TODO: Needed?
 
 set(__build_dir ${CMAKE_CURRENT_BINARY_DIR}/zephyr)
 add_subdirectory($ENV{ZEPHYR_BASE} ${__build_dir})
 
-# To reduce boilerplate in application CMakeLists.txt code we choose
-# to omit cmake_minimum_version()
-cmake_policy(SET CMP0000 OLD)
+define_property(GLOBAL PROPERTY ZEPHYR_LIBS
+    BRIEF_DOCS "Global list of all Zephyr CMake libs that should be linked in"
+    FULL_DOCS "Global list of all Zephyr CMake libs that should be linked in. zephyr_library() appends libs to this list.")
+set_property(GLOBAL PROPERTY ZEPHYR_LIBS "")
+
+define_property(GLOBAL PROPERTY GENERATED_KERNEL_OBJECT_FILES
+  BRIEF_DOCS "Object files that are generated after Zephyr has been linked once."
+  FULL_DOCS "\
+Object files that are generated after Zephyr has been linked once.\
+May include mmu tables, etc."
+  )
+set_property(GLOBAL PROPERTY GENERATED_KERNEL_OBJECT_FILES "")
+
+define_property(GLOBAL PROPERTY GENERATED_KERNEL_SOURCE_FILES
+  BRIEF_DOCS "Source files that are generated after Zephyr has been linked once."
+  FULL_DOCS "\
+Object files that are generated after Zephyr has been linked once.\
+May include isr_tables.c etc."
+  )
+set_property(GLOBAL PROPERTY GENERATED_KERNEL_SOURCE_FILES "")
