ab574075290c789fc1563f154853ab03e1e2db34
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-08-22 15:42:55 +0200

    cmake: Support dropping the flag -DCMAKE_TOOLCHAIN_FILE=[..]
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- cmake/toolchain-zephyr.cmake
+++ cmake/toolchain-zephyr.cmake
@@ -57,7 +57,13 @@ elseif("${ARCH}" STREQUAL "riscv32")
 
 endif()
 
-set(CMAKE_C_COMPILER ${CROSS_COMPILE}gcc)
+set(CMAKE_C_COMPILER   ${CROSS_COMPILE}gcc     CACHE INTERNAL " " FORCE)
+set(CMAKE_CXX_COMPILER ${CROSS_COMPILE}g++     CACHE INTERNAL " " FORCE)
+set(CMAKE_OBJCOPY      ${CROSS_COMPILE}objcopy CACHE INTERNAL " " FORCE)
+set(CMAKE_OBJDUMP      ${CROSS_COMPILE}objdump CACHE INTERNAL " " FORCE)
+#set(CMAKE_LINKER      ${CROSS_COMPILE}ld      CACHE INTERNAL " " FORCE) # Not in use yet
+set(CMAKE_AR           ${CROSS_COMPILE}ar      CACHE INTERNAL " " FORCE)
+set(CMAKE_RANLILB      ${CROSS_COMPILE}ranlib  CACHE INTERNAL " " FORCE)
 
 execute_process(
   COMMAND ${CMAKE_C_COMPILER} --print-file-name=include
@@ -73,23 +79,18 @@ execute_process(
 string(REGEX REPLACE "\n" "" _OUTPUT ${_OUTPUT})
 list(APPEND NOSTDINC ${_OUTPUT})
 
-if(TOOLCHAIN_DO_PASS2 AND TARGET zephyr)
-  get_property(ZEPHYR_C_FLAGS TARGET zephyr PROPERTY COMPILE_OPTIONS)
-  list(FILTER ZEPHYR_C_FLAGS EXCLUDE REGEX "\\$<.*>") # remove generator expressions
+set(SYSROOT_DIR ${ZEPHYR_SDK_INSTALL_DIR}/sysroots/${SYSROOT_TARGET})
 
-  execute_process(
-    COMMAND ${CMAKE_C_COMPILER} ${ZEPHYR_C_FLAGS} --sysroot ${ZEPHYR_SDK_INSTALL_DIR}/sysroots/${SYSROOT_TARGET} --print-libgcc-file-name
-    OUTPUT_VARIABLE LIBGCC_DIR
-    OUTPUT_STRIP_TRAILING_WHITESPACE
+execute_process(
+  COMMAND ${CMAKE_C_COMPILER} --sysroot ${SYSROOT_DIR} --print-libgcc-file-name
+  OUTPUT_VARIABLE LIBGCC_DIR
+  OUTPUT_STRIP_TRAILING_WHITESPACE
   )
 
-  get_filename_component(LIBGCC_DIR ${LIBGCC_DIR} DIRECTORY)
+get_filename_component(LIBGCC_DIR ${LIBGCC_DIR} DIRECTORY)
 
-  set(LIB_INCLUDE_DIR -L${LIBGCC_DIR})
-  set(TOOLCHAIN_LIBS gcc)
-else()
-  set(TOOLCHAIN_DO_PASS2 ON)
-endif()
+set(LIB_INCLUDE_DIR -L${LIBGCC_DIR})
+set(TOOLCHAIN_LIBS gcc)
 
 set(DTC ${TOOLCHAIN_HOME}/usr/bin/dtc)
 
@@ -99,3 +100,11 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 set(BUILD_SHARED_LIBS OFF)
+
+# For CMake to be able to test if a compiler flag is supported by the
+# toolchain we need to give CMake the necessary flags to compile and
+# link a dummy C file.
+#
+# CMake checks compiler flags with check_c_compiler_flag() (Which we
+# wrap with target_cc_option() in extentions.cmake)
+set(CMAKE_REQUIRED_FLAGS "-nostartfiles -nostdlib --sysroot=${SYSROOT_DIR}")
