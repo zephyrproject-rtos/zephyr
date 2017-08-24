090a6d4f4e02baec2890ddf729325fa799c2da07
Kumar Gala <kumar.gala@linaro.org>
2017-08-24 11:22:57 -0500

    libc: minimal: Add typedefs for fast types
    Some of the HALs in ext require fast types to be defined.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- lib/libc/minimal/include/stdint.h
+++ lib/libc/minimal/include/stdint.h
@@ -42,10 +42,22 @@ typedef signed short        int16_t;
 typedef signed int          int32_t;
 typedef signed long long    int64_t;
 
+/* Assume int to be the fastest type for all types except 64bit ones */
+
+typedef signed int          int_fast8_t;
+typedef signed int          int_fast16_t;
+typedef signed int          int_fast32_t;
+typedef signed long long    int_fast64_t;
+
 typedef unsigned char       uint8_t;
 typedef unsigned short      uint16_t;
 typedef unsigned int        uint32_t;
 typedef unsigned long long  uint64_t;
+
+typedef unsigned int        uint_fast8_t;
+typedef unsigned int        uint_fast16_t;
+typedef unsigned int        uint_fast32_t;
+typedef unsigned long long  uint_fast64_t;
 
 typedef int                 intptr_t;
 typedef unsigned int        uintptr_t;
