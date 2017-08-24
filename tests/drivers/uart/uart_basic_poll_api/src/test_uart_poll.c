00d14627d8cd5d49e205f607cefeb9fb46350cb9
Nirmala Devi <nirmala.devix.m@intel.com>
2017-08-24 23:46:46 +0530

    drivers: uart: Add testcase for uart in polling mode
    Basic testcase to test uart in poll in and poll out mode.
    
    Signed-off-by: Nirmala Devi <nirmala.devix.m@intel.com>

---
+++ tests/drivers/uart/uart_basic_poll_api/src/test_uart_poll.c
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
+ * @addtogroup t_uart_basic
+ * @{
+ * @defgroup t_uart_poll test_uart_poll
+ * @brief TestPurpose: verify UART works well in poll mode
+ * @details
+ * - Test Steps
+ *    - Poll Output:
+ *      -# Output the prepared data using uart_poll_out(), and compare
+ *         the output characters with the original characters.
+ *    - Poll Input:
+ *      -# Wait for data from UART console using uart_poll_in(). User sends
+ *         data to UART console using echo "qwer" > /dev/ttyUSB* and waits
+ *         for uart_poll_in() exit.
+ * - Expected Results
+ *   -# When test UART poll out, the return value from uart_poll_out()
+ *      will be equal to the character sent out.
+ *   -# When test UART poll in, the app will wait for input from UART
+ *      console and exit after receiving '\n'.
+ * @}
+ */
+
+#include <uart.h>
+#include <ztest.h>
+
+#define UART_DEVICE_NAME CONFIG_UART_CONSOLE_ON_DEV_NAME
+static struct device *uart_dev;
+
+static const char *poll_data = "This is a POLL test.\r\n";
+
+static int test_poll_in(void)
+{
+	unsigned char recv_char;
+
+	uart_dev = device_get_binding(UART_DEVICE_NAME);
+	if (!uart_dev) {
+		TC_PRINT("Cannot get UART device\n");
+		return TC_FAIL;
+	}
+
+	TC_PRINT("Please send characters to serial console\n");
+
+	/* Verify uart_poll_in() */
+	while (1) {
+		while (uart_poll_in(uart_dev, &recv_char) < 0)
+			;
+
+		TC_PRINT("%c", recv_char);
+
+		if ((recv_char == '\n') || (recv_char == '\r'))
+			break;
+	}
+
+	return TC_PASS;
+}
+
+static int test_poll_out(void)
+{
+	int i;
+	unsigned char sent_char;
+
+	uart_dev = device_get_binding(UART_DEVICE_NAME);
+	if (!uart_dev) {
+		TC_PRINT("Cannot get UART device\n");
+		return TC_FAIL;
+	}
+
+	/* Verify uart_poll_out() */
+	for (i = 0; i < strlen(poll_data); i++)
+		sent_char = uart_poll_out(uart_dev, poll_data[i]);
+		/* TODO: Check on return value */
+
+
+	return TC_PASS;
+}
+
+void test_uart_poll_out(void)
+{
+	zassert_true(test_poll_out() == TC_PASS, NULL);
+}
+
+void test_uart_poll_in(void)
+{
+	zassert_true(test_poll_in() == TC_PASS, NULL);
+}
+
+void test_main(void)
+{
+	ztest_test_suite(uart_basic_test,
+			ztest_unit_test(test_uart_poll_in),
+			ztest_unit_test(test_uart_poll_out));
+	ztest_run_test_suite(uart_basic_test);
+}
