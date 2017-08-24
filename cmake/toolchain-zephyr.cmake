590922ade2415f0331edfa8bb8ddee040c2eea9e
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-08-24 17:26:02 +0200

    cmake: Remove the need for the QEMU_${ARCH} variable
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- cmake/toolchain-zephyr.cmake
+++ cmake/toolchain-zephyr.cmake
@@ -93,8 +93,13 @@ set(LIB_INCLUDE_DIR -L${LIBGCC_DIR})
 set(TOOLCHAIN_LIBS gcc)
 
 set(DTC       ${TOOLCHAIN_HOME}/usr/bin/dtc)
-set(QEMU      ${TOOLCHAIN_HOME}/usr/bin/${QEMU_${ARCH}})
 set(QEMU_BIOS ${TOOLCHAIN_HOME}/usr/share/qemu)
+
+if("${ARCH}" STREQUAL "x86")
+  set(QEMU ${TOOLCHAIN_HOME}/usr/bin/qemu-system-i386)
+else()
+  set(QEMU ${TOOLCHAIN_HOME}/usr/bin/qemu-system-${ARCH})
+endif()
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
