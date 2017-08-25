f4ee5f8e86f5747ef03eb4e065e2e6673974079c
Maureen Helm <maureen.helm@nxp.com>
2017-08-25 17:13:39 -0500

    dts: Add Kinetis SIM clock bindings
    Adds device tree bindings for the Kinetis System Integration Module
    (SIM), and defines peripheral source clocks (e.g., system clock or bus
    clock) and clock gates for all Kinetis SoCs.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

---
+++ include/dt-bindings/clock/kinetis_sim.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2017, NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __KINETIS_SIM_H
+#define __KINETIS_SIM_H
+
+#define KINETIS_SIM_CORESYS_CLK		0
+#define KINETIS_SIM_PLATFORM_CLK	1
+#define KINETIS_SIM_BUS_CLK		2
+
+#endif /* __KINETIS_SIM_H */
