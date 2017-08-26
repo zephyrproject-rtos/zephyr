5cde8cc39e33dac65f62c167c8b0c21305b6fb99
Anas Nashif <anas.nashif@intel.com>
2017-08-26 07:24:59 -0400

    tests: fp_sharing: include local headers
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/kernel/fp_sharing/src/pi.c
+++ tests/kernel/fp_sharing/src/pi.c
@@ -1,5 +1,3 @@
-/* pi.c - pi computation portion of FPU sharing test */
-
 /*
  * Copyright (c) 2011-2014 Wind River Systems, Inc.
  *
@@ -7,7 +5,9 @@
  */
 
 /*
- * DESCRIPTION
+ * @file
+ * pi computation portion of FPU sharing test
+ *
  * This module is used for the FPU sharing test, and supplements the basic
  * load/store test by incorporating two additional threads that utilize the
  * floating point unit.
@@ -33,7 +33,7 @@
 #include <stdio.h>
 #include <tc_util.h>
 
-#include <float_context.h>
+#include "float_context.h"
 
 /*
  * PI_NUM_ITERATIONS: This macro is defined in the project's Makefile and
