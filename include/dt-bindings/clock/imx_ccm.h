c51470b91438b4864204ae56a117a9b86fcefdf1
Maureen Helm <maureen.helm@nxp.com>
2017-08-28 14:09:55 -0500

    clock_control: Introduce mcux ccm driver
    Adds a new clock control driver for i.MX SoCs that have the clock
    control module (CCM).
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

---
+++ include/dt-bindings/clock/imx_ccm.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2017, NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __IMX_CCM_H
+#define __IMX_CCM_H
+
+#define IMX_CCM_CORESYS_CLK		0
+#define IMX_CCM_PLATFORM_CLK		1
+#define IMX_CCM_BUS_CLK			2
+#define IMX_CCM_LPUART_CLK		3
+#define IMX_CCM_LPI2C_CLK		4
+
+#endif /* __IMX_CCM_H */
