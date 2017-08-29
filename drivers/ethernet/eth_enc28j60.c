zephyrproject-rtos/zephyr

commit db11fcd1747c5c34a26e984f1483eaee1ee16fc1

net/net_pkt: Fully separate struct net_pkt from struct net_buf
- net_pkt becomes a stand-alone structure with network packet meta
  information.
- network packet data is still managed through net_buf, mostly named
  'frag'.
- net_pkt memory management is done through k_mem_slab
- function got introduced or relevantly renamed to target eithe net_pkt
  or net_buf fragments.
- net_buf's sent_list ends up in net_pkt now, and thus helps to save
  memory when TCP is enabled.

Change-Id: Ibd5c17df4f75891dec79db723a4c9fc704eb843d
Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

drivers/ethernet/eth_enc28j60.c
@@ -424,7 +424,7 @@ static int eth_enc28j60_init(struct device *dev)
 	return 0;
 }
 
-static int eth_enc28j60_tx(struct device *dev, struct net_buf *buf,
+static int eth_enc28j60_tx(struct device *dev, struct net_pkt *pkt,
 			   uint16_t len)
 {
 	struct eth_enc28j60_runtime *context = dev->driver_data;
@@ -461,13 +461,13 @@ static int eth_enc28j60_tx(struct device *dev, struct net_buf *buf,
 	per_packet_control = ENC28J60_PPCTL_BYTE;
 	eth_enc28j60_write_mem(dev, &per_packet_control, 1);
 
-	for (frag = buf->frags; frag; frag = frag->frags) {
+	for (frag = pkt->frags; frag; frag = frag->frags) {
 		uint8_t *data_ptr;
 		uint16_t data_len;
 
 		if (first_frag) {
-			data_ptr = net_pkt_ll(buf);
-			data_len = net_pkt_ll_reserve(buf) + frag->len;
+			data_ptr = net_pkt_ll(pkt);
+			data_len = net_pkt_ll_reserve(pkt) + frag->len;
 			first_frag = false;
 		} else {
 			data_ptr = frag->data;
@@ -522,10 +522,9 @@ static int eth_enc28j60_rx(struct device *dev)
 	k_sem_take(&context->tx_rx_sem, K_FOREVER);
 
 	do {
-		struct net_buf *last_frag;
 		struct net_buf *pkt_buf = NULL;
 		uint16_t frm_len = 0;
-		struct net_buf *buf;
+		struct net_pkt *pkt;
 		uint16_t next_packet;
 		uint8_t np[2];
 
@@ -552,13 +551,11 @@ static int eth_enc28j60_rx(struct device *dev)
 		lengthfr = frm_len;
 
 		/* Get the frame from the buffer */
-		buf = net_pkt_get_reserve_rx(0, config->timeout);
-		if (!buf) {
+		pkt = net_pkt_get_reserve_rx(0, config->timeout);
+		if (!pkt) {
 			SYS_LOG_ERR("Could not allocate rx buffer");
 			goto done;
 		}
-
-		last_frag = buf;
 
 		do {
 			size_t frag_len;
@@ -566,18 +563,17 @@ static int eth_enc28j60_rx(struct device *dev)
 			size_t spi_frame_len;
 
 			/* Reserve a data frag to receive the frame */
-			pkt_buf = net_pkt_get_frag(buf, config->timeout);
+			pkt_buf = net_pkt_get_frag(pkt, config->timeout);
 			if (!pkt_buf) {
 				SYS_LOG_ERR("Could not allocate data buffer");
-				net_buf_unref(buf);
+				net_pkt_unref(pkt);
 
 				goto done;
 			}
 
-			net_buf_frag_insert(last_frag, pkt_buf);
-			data_ptr = pkt_buf->data;
+			net_pkt_frag_insert(pkt, pkt_buf);
 
-			last_frag = pkt_buf;
+			data_ptr = pkt_buf->data;
 
 			/* Review the space available for the new frag */
 			frag_len = net_buf_tailroom(pkt_buf);
@@ -608,7 +604,7 @@ static int eth_enc28j60_rx(struct device *dev)
 
 		/* Feed buffer frame to IP stack */
 		SYS_LOG_DBG("Received packet of length %u", lengthfr);
-		net_recv_data(context->iface, buf);
+		net_recv_data(context->iface, pkt);
 done:
 		/* Free buffer memory and decrement rx counter */
 		eth_enc28j60_set_bank(dev, ENC28J60_REG_ERXRDPTL);
@@ -654,16 +650,16 @@ static void enc28j60_thread_main(void *arg1, void *unused1, void *unused2)
 	}
 }
 
-static int eth_net_tx(struct net_if *iface, struct net_buf *buf)
+static int eth_net_tx(struct net_if *iface, struct net_pkt *pkt)
 {
-	uint16_t len = net_pkt_ll_reserve(buf) + net_buf_frags_len(buf);
+	uint16_t len = net_pkt_ll_reserve(pkt) + net_pkt_get_len(pkt);
 	int ret;
 
-	SYS_LOG_DBG("buf %p (len %u)", buf, len);
+	SYS_LOG_DBG("pkt %p (len %u)", pkt, len);
 
-	ret = eth_enc28j60_tx(iface->dev, buf, len);
+	ret = eth_enc28j60_tx(iface->dev, pkt, len);
 	if (ret == 0) {
-		net_pkt_unref(buf);
+		net_pkt_unref(pkt);
 	}
 
 	return ret;

commit 85da44df818092fb032d50517e66b8f9d6a13a8f

drivers/ethernet: Fix a packet reception regression in ENC28J60 driver
Commit-id db11fcd1747c5c34a26e984f1483eaee1ee16fc1 broke the packet
reception logic in this driver.

Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

drivers/ethernet/eth_enc28j60.c
@@ -522,6 +522,7 @@ static int eth_enc28j60_rx(struct device *dev)
 
 	do {
 		struct net_buf *pkt_buf = NULL;
+		struct net_buf *last_buf = NULL;
 		u16_t frm_len = 0;
 		struct net_pkt *pkt;
 		u16_t next_packet;
@@ -570,7 +571,13 @@ static int eth_enc28j60_rx(struct device *dev)
 				goto done;
 			}
 
-			net_pkt_frag_insert(pkt, pkt_buf);
+			if (!last_buf) {
+				net_pkt_frag_insert(pkt, pkt_buf);
+			} else {
+				net_buf_frag_insert(last_buf, pkt_buf);
+			}
+
+			last_buf = pkt_buf;
 
 			data_ptr = pkt_buf->data;
 