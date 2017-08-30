2f5b63e0453b91d6a781fcd5bad374dcee50184b
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2017-08-30 14:45:44 +0530

    intel_s1000: uart: configure UART for intel_s1000
    intel_s1000 uses DesignWare IP for UART. National Semiconductor
    16550 (UART) component specification is followed in this IP.
    
    Change-Id: Ied7df1dc178d55b6dbe71d729d6383ba07274ea4
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

---
+++ arch/xtensa/soc/intel_s1000/soc.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017 Intel Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_SOC_H
+#define __INC_SOC_H
+
+/*
+ * UART - UART0
+ */
+#define UART_NS16550_PORT_0_BASE_ADDR                   0x00080800
+#define UART_NS16550_PORT_0_CLK_FREQ                    38400000
+
+#endif /* __INC_SOC_H */
