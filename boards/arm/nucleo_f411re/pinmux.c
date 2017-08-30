31745c69db78cf04a31a7356920b0bb673c6632f
Kumar Gala <kumar.gala@linaro.org>
2017-08-30 08:35:38 -0500

    pinmux: Move stm32 board pinmux files into board dirs
    Move all STM32 based board pinmux files into the board dirs so we are
    consistent across all the STM32 platforms/boards.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- boards/arm/nucleo_f411re/pinmux.c
+++ boards/arm/nucleo_f411re/pinmux.c
@@ -9,20 +9,19 @@
 #include <init.h>
 #include <pinmux.h>
 #include <sys_io.h>
-#include "pinmux/pinmux.h"
 
-#include "pinmux_stm32.h"
+#include <pinmux/stm32/pinmux_stm32.h>
 
-/* pin assignments for STM32F469I-DISCO board */
+/* pin assignments for NUCLEO-F411RE board */
 static const struct pin_config pinconf[] = {
-#ifdef CONFIG_UART_STM32_PORT_3
-	{STM32_PIN_PB10, STM32F4_PINMUX_FUNC_PB10_USART3_TX},
-	{STM32_PIN_PB11, STM32F4_PINMUX_FUNC_PB11_USART3_RX},
-#endif	/* CONFIG_UART_STM32_PORT_3 */
-#ifdef CONFIG_UART_STM32_PORT_6
-	{STM32_PIN_PG14, STM32F4_PINMUX_FUNC_PG14_USART6_TX},
-	{STM32_PIN_PG9, STM32F4_PINMUX_FUNC_PG9_USART6_RX},
-#endif	/* CONFIG_UART_STM32_PORT_6 */
+#ifdef CONFIG_UART_STM32_PORT_1
+	{STM32_PIN_PB6, STM32F4_PINMUX_FUNC_PB6_USART1_TX},
+	{STM32_PIN_PB7, STM32F4_PINMUX_FUNC_PB7_USART1_RX},
+#endif	/* CONFIG_UART_STM32_PORT_1 */
+#ifdef CONFIG_UART_STM32_PORT_2
+	{STM32_PIN_PA2, STM32F4_PINMUX_FUNC_PA2_USART2_TX},
+	{STM32_PIN_PA3, STM32F4_PINMUX_FUNC_PA3_USART2_RX},
+#endif	/* CONFIG_UART_STM32_PORT_2 */
 };
 
 static int pinmux_stm32_init(struct device *port)
--- boards/arm/olimexino_stm32/pinmux.c
+++ boards/arm/olimexino_stm32/pinmux.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, embedjournal.com
+ * Copyright (c) 2016 Open-RnD Sp. z o.o.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,16 +9,15 @@
 #include <init.h>
 #include <pinmux.h>
 #include <sys_io.h>
-#include "pinmux/pinmux.h"
 
-#include "pinmux_stm32.h"
+#include <pinmux/stm32/pinmux_stm32.h>
 
-/* pin assignments for STM32_MIN_DEV board */
+/* pin assignments for NUCLEO-F103RB board */
 static const struct pin_config pinconf[] = {
 #ifdef CONFIG_UART_STM32_PORT_1
 	{STM32_PIN_PA9,  STM32F1_PINMUX_FUNC_PA9_USART1_TX},
 	{STM32_PIN_PA10, STM32F1_PINMUX_FUNC_PA10_USART1_RX},
-#endif	/* CONFIG_UART_STM32_PORT_1 */
+#endif	/* CONFIG_UART_STM32_PORT_2 */
 #ifdef CONFIG_UART_STM32_PORT_2
 	{STM32_PIN_PA2, STM32F1_PINMUX_FUNC_PA2_USART2_TX},
 	{STM32_PIN_PA3, STM32F1_PINMUX_FUNC_PA3_USART2_RX},
@@ -27,6 +26,10 @@ static const struct pin_config pinconf[] = {
 	{STM32_PIN_PB10, STM32F1_PINMUX_FUNC_PB10_USART3_TX},
 	{STM32_PIN_PB11, STM32F1_PINMUX_FUNC_PB11_USART3_RX},
 #endif	/* CONFIG_UART_STM32_PORT_3 */
+#ifdef CONFIG_I2C_2
+	{STM32_PIN_PB10, STM32F1_PINMUX_FUNC_PB10_I2C2_SCL},
+	{STM32_PIN_PB11, STM32F1_PINMUX_FUNC_PB11_I2C2_SDA},
+#endif /* CONFIG_I2C_2 */
 };
 
 static int pinmux_stm32_init(struct device *port)
