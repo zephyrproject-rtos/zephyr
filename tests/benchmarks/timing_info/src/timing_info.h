91063a33a41dd864fb5614361370854d785d2a2d
Youvedeep Singh <youvedeep.singh@intel.com>
2017-08-31 20:22:18 +0530

    tests: benchmark: timing_info: Add support for nrf series processors
    nrf SOC uses nrf rtc timer (not sys tick), which is 32kHz,
    whereas CPU runs at higher speed (nrf52 runs at 64MHz).
    So 32Khz is too slow to measure critical kernel parameters.
    
    This patch does :-
    1. Add support for nrf SOC for timing_info benchmarking.
    2. Uses SOC timer to measure kernel parameters.
    
    Jira: ZEP-2314
    
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>

--- tests/benchmarks/timing_info/src/timing_info.h
+++ tests/benchmarks/timing_info/src/timing_info.h
@@ -27,10 +27,65 @@ extern u64_t __end_intr_time;
 extern u64_t __start_tick_time;
 extern u64_t __end_tick_time;
 
+/* NRF RTC TIMER runs ar very slow rate (32KHz), So in order to measure
+ * Kernel stats dedicated timer is used to measure kernel stats
+ */
+#if defined(CONFIG_NRF_RTC_TIMER)
+#define NANOSECS_PER_SEC 1000000000
+#define CYCLES_PER_SEC (16000000/(1 << NRF_TIMER2->PRESCALER))
+/* To get current count of timer, first 1 need to be written into
+ * Capture Register and Current Count will be copied into corresponding
+ * current count register.
+ */
+#define GET_CURRENT_TIME()  ((NRF_TIMER2->TASKS_CAPTURE[0] = 1) ?	\
+							NRF_TIMER2->CC[0] : 0)
+#define CYCLES_TO_NS(x) ((x) * (NANOSECS_PER_SEC/CYCLES_PER_SEC))
+#define PRINT_STATS(x, y, z)   PRINT_F(x, (y*((SystemCoreClock)/	\
+							CYCLES_PER_SEC)), z)
 
+/* Configure Timer parameters */
+static inline void benchmark_timer_init(void)
+{
+	NRF_TIMER2->TASKS_CLEAR = 1;	/* Clear Timer */
+	NRF_TIMER2->MODE = 0;		/* Timer Mode */
+	NRF_TIMER2->PRESCALER = 0;	/* 16M Hz */
+	NRF_TIMER2->BITMODE = 2;	/* 24 - bit */
+}
+
+/* Stop the timer */
+static inline void benchmark_timer_stop(void)
+{
+	NRF_TIMER2->TASKS_STOP = 1;	/* Stop Timer */
+}
+
+/*Start the timer */
+static inline void benchmark_timer_start(void)
+{
+	NRF_TIMER2->TASKS_START = 1;	/* Start Timer */
+}
+
+/* Get Core Frequency in MHz */
+static inline u32_t get_core_freq_MHz(void)
+{
+	return SystemCoreClock/1000000;
+}
+
+#else
 #define GET_CURRENT_TIME()  OS_GET_TIME()
 #define CYCLES_TO_NS(x) SYS_CLOCK_HW_CYCLES_TO_NS(x)
-#define PRINT_STATS(x, y, z)	PRINT_F(x, y, z)
+/* Dummy functions for timer */
+static inline void benchmark_timer_init(void)  {       }
+static inline void benchmark_timer_stop(void)  {       }
+static inline void benchmark_timer_start(void) {       }
+
+/* Get Core Frequency in MHz */
+static inline u32_t get_core_freq_MHz(void)
+{
+	return  (CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC/1000000);
+}
+
+#define PRINT_STATS(x, y, z)   PRINT_F(x, y, z)
+#endif /* CONFIG_NRF_RTC_TIMER */
 
 /* Function prototypes */
 void system_thread_bench(void);
@@ -99,7 +154,11 @@ void msg_passing_bench(void);
  * NOTE: Needed only when reading value from end of swap operation
  */
 #if CONFIG_ARM
+#if defined(CONFIG_SOC_FAMILY_NRF5)
+#define SUBTRACT_CLOCK_CYCLES(val) (val)
+#else
 #define SUBTRACT_CLOCK_CYCLES(val) (SysTick->LOAD - (u32_t) val)
+#endif /* CONFIG_SOC_FAMILY_NRF5 */
 #else
 #define SUBTRACT_CLOCK_CYCLES(val) (val)
 #endif /* CONFIG_ARM */
