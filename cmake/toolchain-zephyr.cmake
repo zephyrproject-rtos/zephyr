edd8f0be866caa4be406f8536c577fb3895b1b6a
Anas Nashif <anas.nashif@intel.com>
2017-09-02 07:23:21 -0400

    cmake: check for SDK version
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- cmake/toolchain-zephyr.cmake
+++ cmake/toolchain-zephyr.cmake
@@ -9,9 +9,18 @@ endif()
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR ${ARCH})
 
-#TODO: SDK version check
+set(REQUIRED_SDK_VER 0.9.1)
 set(TOOLCHAIN_VENDOR zephyr)
 set(TOOLCHAIN_ARCH x86_64)
+
+file(READ ${ZEPHYR_SDK_INSTALL_DIR}/sdk_version SDK_VERSION)
+if(${REQUIRED_SDK_VER} VERSION_GREATER ${SDK_VERSION})
+  message(FATAL_ERROR "The SDK version you are using is old, please update your SDK.
+You need at least SDK version ${REQUIRED_SDK_VER}.
+The new version of the SDK can be download from:
+https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${REQUIRED_SDK_VER}/zephyr-sdk-${REQUIRED_SDK_VER}-setup.run
+")
+endif()
 
 if(MINGW)
   set(TOOLCHAIN_HOME ${ZEPHYR_SDK_INSTALL_DIR}/sysroots/i686-pokysdk-mingw32)
@@ -148,8 +157,10 @@ set(LIB_INCLUDE_DIR -L${LIBGCC_DIR})
 set(TOOLCHAIN_LIBS gcc)
 
 set(DTC       		${TOOLCHAIN_HOME}/usr/bin/dtc)
-set(KCONFIG_CONF       	${TOOLCHAIN_HOME}/usr/bin/conf)
-set(KCONFIG_MCONF      	${TOOLCHAIN_HOME}/usr/bin/mconf)
+if(${SDK_VERSION} VERSION_GREATER "0.9.1")
+  set(KCONFIG_CONF       	${TOOLCHAIN_HOME}/usr/bin/conf)
+  set(KCONFIG_MCONF      	${TOOLCHAIN_HOME}/usr/bin/mconf)
+endif()
 set(QEMU_BIOS 		${TOOLCHAIN_HOME}/usr/share/qemu)
 
 if("${ARCH}" STREQUAL "x86")
