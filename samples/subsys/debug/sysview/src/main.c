1b15fb90f089b7a88da9ac00c40e022a9338571f
Anas Nashif <anas.nashif@intel.com>
2017-09-12 18:33:31 -0400

    cmake: support debug with systemview sample
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- samples/subsys/debug/sysview/src/main.c
+++ samples/subsys/debug/sysview/src/main.c
@@ -10,8 +10,8 @@
 #include <misc/util.h>
 #include <zephyr.h>
 
-#include "SEGGER_SYSVIEW.h"
-#include "SEGGER_RTT.h"
+#include "systemview/SEGGER_SYSVIEW.h"
+#include "rtt/SEGGER_RTT.h"
 
 static u8_t printer_stack[1024];
 static u8_t calc_stack[1024];
