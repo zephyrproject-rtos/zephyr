6dcf5551d9b1de3addaa5fed95a50b725febe2d6
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-09-13 22:05:33 +0300

    net: dns: Do not resolve IPv6 address if IPv6 is disabled
    If IPv6 is disabled, then it is useless to try to resolve
    IPv6 address because "struct sockaddr" does not have enough
    space to store IPv6 address.
    
    Fixes #1487
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- tests/net/lib/dns_resolve/src/main.c
+++ tests/net/lib/dns_resolve/src/main.c
@@ -374,9 +374,11 @@ static void dns_query_ipv4_server_count(void)
 			continue;
 		}
 
-		if (ctx->servers[i].dns_server.sa_family == AF_INET) {
-			count++;
+		if (ctx->servers[i].dns_server.sa_family == AF_INET6) {
+			continue;
 		}
+
+		count++;
 
 		if (net_sin(&ctx->servers[i].dns_server)->sin_port ==
 		    ntohs(53)) {
@@ -385,7 +387,7 @@ static void dns_query_ipv4_server_count(void)
 	}
 
 	zassert_equal(count, 2, "Invalid number of IPv4 servers");
-	zassert_equal(port, 2, "Invalid number of IPv4 servers with port 53");
+	zassert_equal(port, 1, "Invalid number of IPv4 servers with port 53");
 }
 
 static void dns_query_ipv6_server_count(void)
@@ -402,9 +404,11 @@ static void dns_query_ipv6_server_count(void)
 			continue;
 		}
 
-		if (ctx->servers[i].dns_server.sa_family == AF_INET6) {
-			count++;
+		if (ctx->servers[i].dns_server.sa_family == AF_INET) {
+			continue;
 		}
+
+		count++;
 
 		if (net_sin6(&ctx->servers[i].dns_server)->sin6_port ==
 		    ntohs(53)) {
@@ -412,8 +416,13 @@ static void dns_query_ipv6_server_count(void)
 		}
 	}
 
+#if defined(CONFIG_NET_IPV6)
 	zassert_equal(count, 2, "Invalid number of IPv6 servers");
-	zassert_equal(port, 2, "Invalid number of IPv6 servers with port 53");
+	zassert_equal(port, 1, "Invalid number of IPv6 servers with port 53");
+#else
+	zassert_equal(count, 0, "Invalid number of IPv6 servers");
+	zassert_equal(port, 0, "Invalid number of IPv6 servers with port 53");
+#endif
 }
 
 static void dns_query_too_many(void)
@@ -671,10 +680,12 @@ void dns_result_numeric_cb(enum dns_resolve_status status,
 	}
 
 	if (info && info->ai_family == AF_INET6) {
+#if defined(CONFIG_NET_IPV6)
 		if (net_ipv6_addr_cmp(&net_sin6(&info->ai_addr)->sin6_addr,
 				      &my_addr3) != true) {
 			zassert_true(false, "IPv6 address does not match");
 		}
+#endif
 	}
 
 	k_sem_give(&wait_data2);
