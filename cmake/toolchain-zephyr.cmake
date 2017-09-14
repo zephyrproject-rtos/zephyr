53d158f2997b2170e41e38555a842b04cbfd2287
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-09-14 12:45:07 +0200

    cmake: toolchain-zephyr.cmake: Removed duplicate code
    Signed-off-by: Sebastian Boe <sebastian.boe@nordicsemi.no>

--- cmake/toolchain-zephyr.cmake
+++ cmake/toolchain-zephyr.cmake
@@ -1,32 +1,7 @@
-if(NOT ZEPHYR_SDK_INSTALL_DIR)
-  set(ZEPHYR_SDK_INSTALL_DIR $ENV{ZEPHYR_SDK_INSTALL_DIR})
-endif()
-set(ZEPHYR_SDK_INSTALL_DIR ${ZEPHYR_SDK_INSTALL_DIR} CACHE PATH "Zephyr SDK install directory")
-if(NOT ZEPHYR_SDK_INSTALL_DIR)
-  message(FATAL_ERROR "ZEPHYR_SDK_INSTALL_DIR is not set")
-endif()
+# NB: Depends on host-tools.cmake having been executed already
 
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR ${ARCH})
-
-set(REQUIRED_SDK_VER 0.9.1)
-set(TOOLCHAIN_VENDOR zephyr)
-set(TOOLCHAIN_ARCH x86_64)
-
-file(READ ${ZEPHYR_SDK_INSTALL_DIR}/sdk_version SDK_VERSION)
-if(${REQUIRED_SDK_VER} VERSION_GREATER ${SDK_VERSION})
-  message(FATAL_ERROR "The SDK version you are using is old, please update your SDK.
-You need at least SDK version ${REQUIRED_SDK_VER}.
-The new version of the SDK can be download from:
-https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${REQUIRED_SDK_VER}/zephyr-sdk-${REQUIRED_SDK_VER}-setup.run
-")
-endif()
-
-if(MINGW)
-  set(TOOLCHAIN_HOME ${ZEPHYR_SDK_INSTALL_DIR}/sysroots/i686-pokysdk-mingw32)
-else()
-  set(TOOLCHAIN_HOME ${ZEPHYR_SDK_INSTALL_DIR}/sysroots/${TOOLCHAIN_ARCH}-pokysdk-linux)
-endif()
 
 # TODO fetch from environment
 if("${ARCH}" STREQUAL "arm")
