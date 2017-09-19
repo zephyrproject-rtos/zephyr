be6e31afe2500e9d4ea0f01f0b239cef21f1088d
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-09-19 16:39:05 +0200

    cmake: Ensure SDK tools have precedence over tools on PATH
    Signed-off-by: Sebastian Boe <sebastian.boe@nordicsemi.no>

--- cmake/host-tools.cmake
+++ cmake/host-tools.cmake
@@ -1,5 +1,9 @@
 include($ENV{ZEPHYR_BASE}/cmake/host-tools-${ZEPHYR_GCC_VARIANT}.cmake OPTIONAL)
 
+if(PREBUILT_HOST_TOOLS)
+  list(APPEND CMAKE_PROGRAM_PATH ${PREBUILT_HOST_TOOLS}/kconfig)
+endif()
+
 # Search for the must-have program dtc on PATH and in
 # TOOLCHAIN_HOME. Usually DTC will be provided by an SDK, but for
 # SDK-less projects like gccarmemb, it is up to the user to install
@@ -7,8 +11,6 @@ include($ENV{ZEPHYR_BASE}/cmake/host-tools-${ZEPHYR_GCC_VARIANT}.cmake OPTIONAL)
 find_program(
   DTC
   dtc
-  PATHS ${TOOLCHAIN_HOME} ENV PATH
-  PATH_SUFFIXES "usr/bin"
   )
 if(${DTC} STREQUAL DTC-NOTFOUND)
   message(FATAL_ERROR "Unable to find dtc")
@@ -17,19 +19,15 @@ endif()
 find_program(
   KCONFIG_CONF
   conf
-  PATHS ${TOOLCHAIN_HOME} ${PREBUILT_HOST_TOOLS}/kconfig ENV PATH
-  PATH_SUFFIXES "usr/bin"
   )
 if(${KCONFIG_CONF} STREQUAL KCONFIG_CONF-NOTFOUND)
   message(FATAL_ERROR "Unable to find the Kconfig program 'conf'")
 endif()
 
-# conf is an optional dependency
+# mconf is an optional dependency
 find_program(
   KCONFIG_MCONF
   mconf
-  PATHS ${TOOLCHAIN_HOME} ${PREBUILT_HOST_TOOLS}/kconfig ENV PATH
-  PATH_SUFFIXES "usr/bin"
   )
 
 # qemu is an optional dependency
@@ -41,8 +39,7 @@ endif()
 find_program(
   QEMU
   qemu-system-${QEMU_binary_suffix}
-  PATHS ${TOOLCHAIN_HOME} ENV PATH
-  PATH_SUFFIXES "usr/bin"
   )
+
 # TODO: Should we instead find one qemu binary for each ARCH?
 # TODO: This will probably need to be re-organized when there exists more than one SDK.
