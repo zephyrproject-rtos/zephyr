8b25e1a48408a52ca7a3c1d1a3cf17b9f37bead6
Savinay Dharmappa <savinay.dharmappa@intel.com>
2017-09-21 23:53:50 +0530

    dts: quark_se_c1000_ss_devboard: Add dts support
    Signed-off-by: Savinay Dharmappa <savinay.dharmappa@intel.com>

--- boards/arc/quark_se_c1000_ss_devboard/board.cmake
+++ boards/arc/quark_se_c1000_ss_devboard/board.cmake
@@ -1,6 +1,7 @@
 include($ENV{ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 
 set(OPENOCD_PRE_CMD "targets 1")
+set(OPENOCD_LOAD_CMD "load_image     ${PROJECT_BINARY_DIR}/${KERNEL_BIN_NAME} ${CONFIG_FLASH_BASE_ADDRESS}")
 
 set_property(GLOBAL APPEND PROPERTY FLASH_SCRIPT_ENV_VARS
   OPENOCD_PRE_CMD
