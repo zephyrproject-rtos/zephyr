e3adbf845c6baabae67e96cc17892f5e2bcf3ac2
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-09-21 23:30:32 +0300

    net: Add hostname setting to Kconfig
    User can configure hostname of the device in Kconfig. This can
    be used by mDNS responder to answer <hostname>.local queries.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

---
+++ include/net/hostname.h
@@ -0,0 +1,82 @@
+/** @file
+ * @brief Hostname configuration definitions
+ */
+
+/*
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __NET_HOSTNAME_H
+#define __NET_HOSTNAME_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Network hostname configuration library
+ * @defgroup net_hostname Network Hostname Library
+ * @{
+ */
+
+#if defined(CONFIG_NET_HOSTNAME_ENABLE)
+/**
+ * @brief Get the device hostname
+ *
+ * @details Return pointer to device hostname.
+ *
+ * @return Pointer to hostname or NULL if not set.
+ */
+const char *net_hostname_get(void);
+
+/**
+ * @brief Initialize and set the device hostname.
+ *
+ */
+void net_hostname_init(void);
+
+#else
+static inline const char *net_hostname_get(void)
+{
+	return NULL;
+}
+
+static inline void net_hostname_init(void)
+{
+}
+#endif /* CONFIG_NET_HOSTNAME_ENABLE */
+
+#if defined(CONFIG_NET_HOSTNAME_UNIQUE)
+/**
+ * @brief Set the device hostname postfix
+ *
+ * @details Set the device hostname to some value. This is only used if
+ * CONFIG_NET_HOSTNAME_UNIQUE is set.
+ *
+ * @param hostname_postfix Usually link address. The function will convert this
+ * to a string.
+ * @param postfix_len Length of the hostname_postfix array.
+ *
+ * @return 0 if ok, <0 if error
+ */
+int net_hostname_set_postfix(const u8_t *hostname_postfix,
+			      int postfix_len);
+#else
+static inline int net_hostname_set_postfix(const u8_t *hostname_postfix,
+					   int postfix_len)
+{
+	return -EMSGSIZE;
+}
+#endif /* CONFIG_NET_HOSTNAME_UNIQUE */
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __NET_HOSTNAME_H */
---
+++ subsys/net/hostname.c
@@ -0,0 +1,72 @@
+/** @file
+ * @brief Hostname configuration
+ */
+
+/*
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#if defined(CONFIG_NET_DEBUG_HOSTNAME)
+#define SYS_LOG_DOMAIN "net/hostname"
+#define NET_LOG_ENABLED 1
+#endif
+
+#include <zephyr.h>
+
+#include <net/net_core.h>
+
+#if defined(CONFIG_NET_HOSTNAME_UNIQUE)
+/* Allocate extra space to append MAC address to hostname */
+#define EXTRA_SPACE (8 * 2)
+#else
+#define EXTRA_SPACE 0
+#endif /* CONFIG_NET_HOSTNAME_UNIQUE */
+
+static char hostname[sizeof(CONFIG_NET_HOSTNAME) - 1 + EXTRA_SPACE];
+
+const char *net_hostname_get(void)
+{
+	return hostname;
+}
+
+#if defined(CONFIG_NET_HOSTNAME_UNIQUE)
+int net_hostname_set_postfix(const u8_t *hostname_postfix,
+			     int postfix_len)
+{
+	static bool postfix_set;
+	int pos = 0;
+	int i;
+
+	if (postfix_set) {
+		return -EALREADY;
+	}
+
+	NET_ASSERT(postfix_len > 0);
+
+	/* Note that we convert the postfix to hex (2 chars / byte) */
+	if ((postfix_len * 2) >
+	    ((sizeof(hostname) - 1) - (sizeof(CONFIG_NET_HOSTNAME) - 1))) {
+		return -EMSGSIZE;
+	}
+
+	for (i = 0; i < postfix_len; i++, pos += 2) {
+		snprintk(&hostname[sizeof(CONFIG_NET_HOSTNAME) - 1 + pos],
+			 2 + 1, "%02x", hostname_postfix[i]);
+	}
+
+	NET_DBG("New hostname %s", hostname);
+
+	postfix_set = true;
+
+	return 0;
+}
+#endif /* CONFIG_NET_HOSTNAME_UNIQUE */
+
+void net_hostname_init(void)
+{
+	memcpy(hostname, CONFIG_NET_HOSTNAME, sizeof(CONFIG_NET_HOSTNAME) - 1);
+
+	NET_DBG("Hostname set to %s", hostname);
+}
