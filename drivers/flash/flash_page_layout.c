e02e4d64dc8336f37d6d0cc3a90a9bef752fdcdf
Marti Bolivar <marti.bolivar@linaro.org>
2017-09-21 12:47:09 -0400

    drivers: flash: add flash_page_foreach()
    This patch adds a routine which can be used to iterate over all flash
    pages on the device.
    
    This can be also done by using flash_get_page_info_by_idx(), but that
    would add an unnecessary loop over the layout array for each page.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>

--- drivers/flash/flash_page_layout.c
+++ drivers/flash/flash_page_layout.c
@@ -80,3 +80,31 @@ size_t flash_get_page_count(struct device *dev)
 
 	return count;
 }
+
+void flash_page_foreach(struct device *dev, flash_page_cb cb, void *data)
+{
+	const struct flash_driver_api *api = dev->driver_api;
+	const struct flash_pages_layout *layout;
+	struct flash_pages_info page_info;
+	size_t block, num_blocks, page = 0, i;
+	off_t off = 0;
+
+	api->page_layout(dev, &layout, &num_blocks);
+
+	for (block = 0; block < num_blocks; block++) {
+		const struct flash_pages_layout *l = &layout[block];
+		page_info.size = l->pages_size;
+
+		for (i = 0; i < l->pages_count; i++) {
+			page_info.start_offset = off;
+			page_info.index = page;
+
+			if (!cb(&page_info, data)) {
+				return;
+			}
+
+			off += page_info.size;
+			page++;
+		}
+	}
+}
