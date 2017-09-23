13ca6fe2842fe204ec525bce02287e74c587a48e
Andrew Boie <andrew.p.boie@intel.com>
2017-09-23 12:05:49 -0700

    syscalls: reorganize headers
    - syscall.h now contains those APIs needed to support invoking calls
      from user code. Some stuff moved out of main kernel.h.
    - syscall_handler.h now contains directives useful for implementing
      system call handler functions. This header is not pulled in by
      kernel.h and is intended to be used by C files implementing kernel
      system calls and driver subsystem APIs.
    - syscall_list.h now contains the #defines for system call IDs. This
      list is expected to grow quite large so it is put in its own header.
      This is now an enumerated type instead of defines to make things
      easier as we introduce system calls over the new few months. In the
      fullness of time when we desire to have a fixed userspace/kernel ABI,
      this can always be converted to defines.
    
    Some new code added:
    
    - _SYSCALL_MEMORY() macro added to check memory regions passed up from
      userspace in handler functions
    - _syscall_invoke{7...10}() inline functions declare for invoking system
      calls with more than 6 arguments. 10 was chosen as the limit as that
      corresponds to the largest arg list we currently have
      which is for k_thread_create()
    
    Other changes
    
    - auto-generated K_SYSCALL_DECLARE* macros documented
    - _k_syscall_table in userspace.c is not a placeholder. There's no
      strong need to generate it and doing so would require the introduction
      of a third build phase.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

---
+++ include/syscall_list.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2017, Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache 2.0
+ */
+
+
+#ifndef _ZEPHYR_SYSCALL_LIST_H_
+#define _ZEPHYR_SYSCALL_LIST_H_
+
+#ifndef _ASMLANGUAGE
+
+enum {
+	K_SYSCALL_BAD,
+
+	K_SYSCALL_LIMIT /* Always last */
+};
+
+#endif /* _ASMLANGUAGE */
+
+#endif /* _ZEPHYR_SYSCALL_LIST_H_ */
---
+++ kernel/include/syscall_handler.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2017, Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache 2.0
+ */
+
+
+#ifndef _ZEPHYR_SYSCALL_HANDLER_H_
+#define _ZEPHYR_SYSCALL_HANDLER_H_
+
+#ifdef CONFIG_USERSPACE
+
+#ifndef _ASMLANGUAGE
+#include <kernel.h>
+#include <misc/printk.h>
+#include <nano_internal.h>
+
+extern const _k_syscall_handler_t _k_syscall_table[K_SYSCALL_LIMIT];
+
+/**
+ * @brief Runtime expression check for system call arguments
+ *
+ * Used in handler functions to perform various runtime checks on arguments,
+ * and generate a kernel oops if anything is not expected
+ *
+ * @param expr Boolean expression to verify, a false result will trigger an
+ *             oops
+ * @param ssf Syscall stack frame argument passed to the handler function
+ */
+#define _SYSCALL_VERIFY(expr, ssf) \
+	do { \
+		if (!(expr)) { \
+			printk("FATAL: syscall failed check: " #expr "\n"); \
+			_arch_syscall_oops(ssf); \
+		} \
+	} while (0)
+
+/**
+ * @brief Runtime check that a user thread has proper access to a memory area
+ *
+ * Checks that the particular memory area is readable or writable by the
+ * currently running thread if the CPU was in user mode, and generates a kernel
+ * oops if it wasn't. Prevents userspace from getting the kernel to read or
+ * modify memory the thread does not have access to, or passing in garbage
+ * pointers that would crash/pagefault the kernel if accessed.
+ *
+ * @param ptr Memory area to examine
+ * @param size Size of the memory area
+ * @param write If the thread should be able to write to this memory, not just
+ *		read it
+ * @param ssf Syscall stack frame argument passed to the handler function
+ */
+#define _SYSCALL_MEMORY(ptr, size, write, ssf) \
+	_SYSCALL_VERIFY(!_arch_buffer_validate(ptr, size, write), ssf)
+
+/**
+ * @brief Runtime check that a pointer is a kernel object of expected type
+ *
+ * Passes along arguments to _k_object_validate() and triggers a kernel oops
+ * if the object wasn't valid or had incorrect permissions.
+ *
+ * @param ptr Untrusted kernel object pointer
+ * @param type Expected kernel object type
+ * @param init Whether this is an init function handler
+ * @param ssf Syscall stack frame argument passed to the handler function
+ */
+#define _SYSCALL_IS_OBJ(ptr, type, init, ssf) \
+	_SYSCALL_VERIFY(!_k_object_validate((void *)ptr, type, init), ssf)
+
+/* Convenience macros for handler implementations */
+#define _SYSCALL_ARG0	ARG_UNUSED(arg1); ARG_UNUSED(arg2); ARG_UNUSED(arg3); \
+			ARG_UNUSED(arg4); ARH_UNUSED(arg5); ARG_UNUSED(arg6)
+
+#define _SYSCALL_ARG1	ARG_UNUSED(arg2); ARG_UNUSED(arg3); ARG_UNUSED(arg4); \
+			ARG_UNUSED(arg5); ARG_UNUSED(arg6)
+
+#define _SYSCALL_ARG2	ARG_UNUSED(arg3); ARG_UNUSED(arg4); ARG_UNUSED(arg5); \
+			ARG_UNUSED(arg6)
+
+#define _SYSCALL_ARG3	ARG_UNUSED(arg4); ARG_UNUSED(arg5); ARG_UNUSED(arg6)
+
+
+#define _SYSCALL_ARG4	ARG_UNUSED(arg5); ARG_UNUSED(arg6)
+
+#define _SYSCALL_ARG5	ARG_UNUSED(arg6)
+#endif /* _ASMLANGUAGE */
+
+#endif /* CONFIG_USERSPACE */
+
+#endif /* _ZEPHYR_SYSCALL_H_ */
