0889c693a8f439e4e9b5577f3c1f53b97cde0c24
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-09-25 16:47:15 +0200

    cmake: Ported FLASH_SCRIPT for all boards
    Signed-off-by: Sebastian Boe <sebastian.boe@nordicsemi.no>

---
+++ boards/arm/frdm_kw41z/board.cmake
@@ -0,0 +1,13 @@
+set_ifndef(OPENSDA_FW jlink)
+
+if(OPENSDA_FW STREQUAL jlink)
+  set_ifndef(DEBUG_SCRIPT jlink.sh)
+elseif(OPENSDA_FW STREQUAL daplink)
+  set_ifndef(DEBUG_SCRIPT pyocd.sh)
+  set_ifndef(FLASH_SCRIPT pyocd.sh)
+endif()
+
+set_property(GLOBAL APPEND PROPERTY FLASH_SCRIPT_ENV_VARS
+  JLINK_DEVICE=MKW41Z512xxx4
+  PYOCD_TARGET=kw41z4
+  )
