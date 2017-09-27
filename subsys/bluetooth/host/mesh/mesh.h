a78b466c1e1f7ded15432681dedade8551a564a9
Johan Hedberg <johan.hedberg@intel.com>
2017-09-27 09:51:31 +0300

    Bluetooth: Mesh: Drop invalid destination addresses
    This is required to pass certain PTS tests.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- subsys/bluetooth/host/mesh/mesh.h
+++ subsys/bluetooth/host/mesh/mesh.h
@@ -12,6 +12,7 @@
 #define BT_MESH_ADDR_IS_UNICAST(addr) ((addr) && (addr) < 0x8000)
 #define BT_MESH_ADDR_IS_GROUP(addr) ((addr) >= 0xc000 && (addr) <= 0xff00)
 #define BT_MESH_ADDR_IS_VIRTUAL(addr) ((addr) >= 0x8000 && (addr) < 0xc000)
+#define BT_MESH_ADDR_IS_RFU(addr) ((addr) >= 0xff00 && (addr) <= 0xfffb)
 
 struct bt_mesh_net;
 
