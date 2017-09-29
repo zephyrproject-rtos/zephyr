8dd04742a04842175d6996a4877939c45b24148a
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-09-29 09:40:15 +0200

    ieee802154: Make packet display enablement via a Kconfig option
    Such option should be use carefully. Printing out in/out packets is
    extremely verbose.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- subsys/net/ip/l2/ieee802154/ieee802154.c
+++ subsys/net/ip/l2/ieee802154/ieee802154.c
@@ -31,7 +31,7 @@
 #include "ieee802154_security.h"
 #include "ieee802154_utils.h"
 
-#if 0
+#ifdef CONFIG_NET_DEBUG_L2_IEEE802154_DISPLAY_PACKET
 
 #include "net_private.h"
 
@@ -42,7 +42,7 @@ static inline void pkt_hexdump(struct net_pkt *pkt, bool full)
 
 #else
 #define pkt_hexdump(...)
-#endif
+#endif /* CONFIG_NET_DEBUG_L2_IEEE802154_DISPLAY_PACKET */
 
 #ifdef CONFIG_NET_L2_IEEE802154_ACK_REPLY
 static inline void ieee802154_acknowledge(struct net_if *iface,
