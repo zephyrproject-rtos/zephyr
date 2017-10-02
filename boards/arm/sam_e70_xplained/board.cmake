ea21f9ef69d013b660d1f7eeb345b6b7a88535ac
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-10-02 17:28:31 +0200

    cmake: Fixed references to openocd.board.cmake
    Signed-off-by: Sebastian Boe <sebastian.boe@nordicsemi.no>

--- boards/arm/sam_e70_xplained/board.cmake
+++ boards/arm/sam_e70_xplained/board.cmake
@@ -1,4 +1,4 @@
-include(../../common/openocd.board.cmake)
+include($ENV{ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 
 set_property(GLOBAL APPEND PROPERTY FLASH_SCRIPT_ENV_VARS
   OPENOCD_POST_CMD="atsamv gpnvm set 1"
