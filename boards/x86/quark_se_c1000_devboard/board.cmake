591d4cc85fec32da7033cb871a7854ec613646c1
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-10-02 17:28:31 +0200

    cmake: Fixed references to openocd.board.cmake
    Signed-off-by: Sebastian Boe <sebastian.boe@nordicsemi.no>

--- boards/x86/quark_se_c1000_devboard/board.cmake
+++ boards/x86/quark_se_c1000_devboard/board.cmake
@@ -1,4 +1,4 @@
-include(../../common/openocd.board.cmake)
+include($ENV{ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 
 set_property(GLOBAL APPEND PROPERTY FLASH_SCRIPT_ENV_VARS
   OPENOCD_PRE_CMD="targets 1"
