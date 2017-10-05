fac35eef9390ab8d5b4ea5aa95c72831f98b8a62
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-10-05 10:51:00 +0200

    samples/net: Getting rid of common ieee802154 settings altogether.
    Only ieee802154 should have been using it, but it ended in various
    samples that did not require it anymore once they've been using
    net_app. Unlike former samples settings, net_app settings are tied to
    net_app, so let's just forget about all of it and silently use net_app.
    If something goes wrong in setting net options, it will be a unique
    place.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- samples/net/ieee802154/qemu/src/ieee802154_qemu_test.c
+++ samples/net/ieee802154/qemu/src/ieee802154_qemu_test.c
@@ -4,61 +4,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#if 1
-#define SYS_LOG_DOMAIN "Setup"
-#define NET_SYS_LOG_LEVEL SYS_LOG_LEVEL_DEBUG
-#define NET_LOG_ENABLED 1
-#endif
-
 #include <zephyr.h>
-#include <errno.h>
-
-#include <net/net_if.h>
-#include <net/net_core.h>
-
-#define MCAST_IP6ADDR "ff84::2"
-
-static void setup_device(void)
-{
-	char hr_addr[NET_IPV6_ADDR_LEN];
-	struct net_if *iface;
-	struct in6_addr addr;
-	struct device *dev;
-
-	dev = device_get_binding(CONFIG_IEEE802154_UPIPE_DRV_NAME);
-	if (!dev) {
-		NET_INFO("Cannot get UPIPE device\n");
-		return;
-	}
-
-	iface = net_if_lookup_by_dev(dev);
-	if (!iface) {
-		NET_INFO("Cannot get UPIPE network interface\n");
-		return;
-	}
-
-	if (net_addr_pton(AF_INET6, CONFIG_NET_APP_MY_IPV6_ADDR, &addr)) {
-		NET_ERR("Invalid address: %s", CONFIG_NET_APP_MY_IPV6_ADDR);
-		return;
-	}
-
-	net_if_ipv6_addr_add(iface, &addr, NET_ADDR_MANUAL, 0);
-
-	NET_INFO("IPv6 address: %s",
-		 net_addr_ntop(AF_INET6, &addr, hr_addr, NET_IPV6_ADDR_LEN));
-
-	if (net_addr_pton(AF_INET6, MCAST_IP6ADDR, &addr)) {
-		NET_ERR("Invalid address: %s", MCAST_IP6ADDR);
-		return;
-	}
-
-	NET_INFO("802.15.4 device up and running\n");
-}
-
 
 void main(void)
 {
-	setup_device();
-
 	return;
 }
