100a6079b2ae10f122727f445499212d967fc90d
Andrew Boie <andrew.p.boie@intel.com>
2017-10-07 14:41:38 -0700

    tests: semaphore: run some threads in user mode
    main.c and test_sema_contexts.c merged
    
    userspace tag added to testcase.yaml
    
    stack-allocated semaphore in test_sema_thread2thread now just uses
    the global semaphore with the same name.
    
    ISR tests run in supervisor mode.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- tests/kernel/semaphore/sema_api/src/test_sema_contexts.c
+++ tests/kernel/semaphore/sema_api/src/test_sema_contexts.c
@@ -25,9 +25,9 @@
 #define SEM_LIMIT 2
 /**TESTPOINT: init via K_SEM_DEFINE*/
 K_SEM_DEFINE(ksema, SEM_INITIAL, SEM_LIMIT);
-static struct k_sem sema;
+__kernel struct k_sem sema;
 static K_THREAD_STACK_DEFINE(tstack, STACK_SIZE);
-static struct k_thread tdata;
+__kernel struct k_thread tdata;
 
 /*entry of contexts*/
 static void tisr_entry(void *p)
@@ -45,7 +45,8 @@ static void tsema_thread_thread(struct k_sem *psem)
 	/**TESTPOINT: thread-thread sync via sema*/
 	k_tid_t tid = k_thread_create(&tdata, tstack, STACK_SIZE,
 				      thread_entry, psem, NULL, NULL,
-				      K_PRIO_PREEMPT(0), 0, 0);
+				      K_PRIO_PREEMPT(0),
+				      K_USER | K_INHERIT_PERMS, 0);
 
 	zassert_false(k_sem_take(psem, K_FOREVER), NULL);
 	/*clean the spawn thread avoid side effect in next TC*/
@@ -62,10 +63,9 @@ static void tsema_thread_isr(struct k_sem *psem)
 /*test cases*/
 void test_sema_thread2thread(void)
 {
-	struct k_sem sema;
-
 	/**TESTPOINT: test k_sem_init sema*/
 	k_sem_init(&sema, SEM_INITIAL, SEM_LIMIT);
+
 	tsema_thread_thread(&sema);
 
 	/**TESTPOINT: test K_SEM_DEFINE sema*/
@@ -113,4 +113,18 @@ void test_sema_count_get(void)
 	/**TESTPOINT: sem give above limit*/
 	k_sem_give(&sema);
 	zassert_equal(k_sem_count_get(&sema), SEM_LIMIT, NULL);
+}
+
+/*test case main entry*/
+void test_main(void)
+{
+	k_thread_access_grant(k_current_get(), &ksema, &tdata, &sema, &tstack,
+			      NULL);
+
+	ztest_test_suite(test_sema_api,
+			 ztest_user_unit_test(test_sema_thread2thread),
+			 ztest_unit_test(test_sema_thread2isr),
+			 ztest_user_unit_test(test_sema_reset),
+			 ztest_user_unit_test(test_sema_count_get));
+	ztest_run_test_suite(test_sema_api);
 }
