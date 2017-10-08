c725e64b648c48d6d26e8a2d5a7813a880705d1b
Andrew Boie <andrew.p.boie@intel.com>
2017-10-08 12:52:44 -0700

    tests: lifecycle_api: run in user mode
    Some unnecessary k_thread_abort() removed.
    
    userspace tag added to testcase.yaml.
    
    Suspend/resume, spawn_forever, and spawn_priority tests remain in
    supervisor mode due to the priority requests they make.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- tests/kernel/threads/lifecycle/lifecycle_api/src/main.c
+++ tests/kernel/threads/lifecycle/lifecycle_api/src/main.c
@@ -25,21 +25,27 @@ extern void test_threads_cancel_started(void);
 extern void test_threads_abort_self(void);
 extern void test_threads_abort_others(void);
 
+__kernel struct k_thread tdata;
+#define STACK_SIZE (256 + CONFIG_TEST_EXTRA_STACKSIZE)
+K_THREAD_STACK_DEFINE(tstack, STACK_SIZE);
+
 /*test case main entry*/
 void test_main(void)
 {
+	k_thread_access_grant(k_current_get(), &tdata, tstack, NULL);
+
 	ztest_test_suite(test_threads_lifecycle,
-			 ztest_unit_test(test_threads_spawn_params),
+			 ztest_user_unit_test(test_threads_spawn_params),
 			 ztest_unit_test(test_threads_spawn_priority),
-			 ztest_unit_test(test_threads_spawn_delay),
+			 ztest_user_unit_test(test_threads_spawn_delay),
 			 ztest_unit_test(test_threads_spawn_forever),
 			 ztest_unit_test(test_threads_suspend_resume_cooperative),
 			 ztest_unit_test(test_threads_suspend_resume_preemptible),
-			 ztest_unit_test(test_threads_cancel_undelayed),
-			 ztest_unit_test(test_threads_cancel_delayed),
-			 ztest_unit_test(test_threads_cancel_started),
-			 ztest_unit_test(test_threads_abort_self),
-			 ztest_unit_test(test_threads_abort_others)
+			 ztest_user_unit_test(test_threads_cancel_undelayed),
+			 ztest_user_unit_test(test_threads_cancel_delayed),
+			 ztest_user_unit_test(test_threads_cancel_started),
+			 ztest_user_unit_test(test_threads_abort_self),
+			 ztest_user_unit_test(test_threads_abort_others)
 			 );
 	ztest_run_test_suite(test_threads_lifecycle);
 }
