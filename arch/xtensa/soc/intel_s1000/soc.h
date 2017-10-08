63d50e158e3d5fa1da721263de46ff0e7cea0cd6
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2017-10-08 08:44:24 +0530

    intel_s1000: gpio: enable GPIO handling
    Enable GPIO handling for intel_s1000. It uses a DesignWare IP.
    
    Change-Id: I522534935e4ef3a56d93aca669f6de961d927481
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- arch/xtensa/soc/intel_s1000/soc.h
+++ arch/xtensa/soc/intel_s1000/soc.h
@@ -6,10 +6,15 @@
 #ifndef __INC_SOC_H
 #define __INC_SOC_H
 
-/*
- * UART - UART0
- */
-#define UART_NS16550_PORT_0_BASE_ADDR                   0x00080800
-#define UART_NS16550_PORT_0_CLK_FREQ                    38400000
+/* GPIO */
+#define GPIO_DW_0_BASE_ADDR			0x00080C00
+#define GPIO_DW_0_BITS				32
+#define GPIO_DW_PORT_0_INT_MASK			0
+#define GPIO_DW_0_IRQ_FLAGS			0
+#define GPIO_DW_0_IRQ				20
+
+/* UART - UART0 */
+#define UART_NS16550_PORT_0_BASE_ADDR		0x00080800
+#define UART_NS16550_PORT_0_CLK_FREQ		38400000
 
 #endif /* __INC_SOC_H */
