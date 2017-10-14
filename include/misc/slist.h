5fa972d52dff4f71fda7c933fba448dc91510d62
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-10-14 21:33:01 +0300

    misc: slist: Rework sys_slist_get_not_empty
    Magic spell to address #4022.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- include/misc/slist.h
+++ include/misc/slist.h
@@ -358,10 +358,10 @@ static inline sys_snode_t *sys_slist_get_not_empty(sys_slist_t *list)
 {
 	sys_snode_t *node = list->head;
 
-	list->head = node->next;
 	if (list->tail == node) {
-		list->tail = list->head;
+		list->tail = NULL;
 	}
+	list->head = node->next;
 
 	return node;
 }
