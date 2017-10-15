17404abed37355d3f9d05216de9a3a3b1a480d8c
Harry Jiang <explora26@gmail.com>
2017-10-15 17:11:37 +0800

    samples: environmental_sensing: fix the data type for read_u32()
    Signed-off-by: Harry Jiang <explora26@gmail.com>

--- samples/boards/arduino_101/environmental_sensing/ap/src/main.c
+++ samples/boards/arduino_101/environmental_sensing/ap/src/main.c
@@ -42,7 +42,7 @@ static ssize_t read_u32(struct bt_conn *conn, const struct bt_gatt_attr *attr,
 			void *buf, u16_t len, u16_t offset)
 {
 	const u32_t *u32 = attr->user_data;
-	u16_t value = sys_cpu_to_le32(*u32);
+	u32_t value = sys_cpu_to_le32(*u32);
 
 	return bt_gatt_attr_read(conn, attr, buf, len, offset, &value,
 				 sizeof(value));
