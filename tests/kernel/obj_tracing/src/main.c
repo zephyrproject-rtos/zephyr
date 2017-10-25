72a5f06cebeae11a691c3e092960fa4ce5dcf29d
Punit Vara <punit.vara@intel.com>
2017-10-25 16:03:22 +0530

    tests: obj_tracing: Convert legacy test to ztest
    Use ztest API in legacy test to support ztest
    framework and also update README according to new
    output.
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>

--- tests/kernel/obj_tracing/src/main.c
+++ tests/kernel/obj_tracing/src/main.c
@@ -7,6 +7,7 @@
  */
 
 #include <zephyr.h>
+#include <ztest.h>
 #include "phil.h"
 
 #define STSIZE 1024
@@ -20,7 +21,8 @@ K_THREAD_STACK_DEFINE(mon_stack, STSIZE);
 static struct k_thread mon_data;
 struct k_sem forks[N_PHILOSOPHERS];
 
-int main(void)
+K_SEM_DEFINE(f3, -5, 1);
+void test_tracing(void)
 {
 	int i;
 
@@ -39,6 +41,11 @@ int main(void)
 	k_thread_create(&mon_data, mon_stack, STSIZE,
 			(k_thread_entry_t)object_monitor, NULL, NULL, NULL,
 			K_PRIO_COOP(7), 0, K_NO_WAIT);
+}
 
-	return 0;
+void test_main(void)
+{
+	ztest_test_suite(test_obj_tracing,
+			ztest_unit_test(test_tracing));
+	ztest_run_test_suite(test_obj_tracing);
 }
