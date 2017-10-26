b6c70fd331e9b2687445f9a7e39602622c08c6a3
Sahaj Sarup <sahaj.sarup@linaro.org>
2017-10-26 23:35:38 +0530

    samples: Basic Thread Demo
    A basic demo to showcase multi-threading using K_THREAD_DEFINE
    
    Signed-off-by: Sahaj Sarup <sahaj.sarup@linaro.org>

---
+++ samples/basic/threads/README.rst
@@ -0,0 +1,28 @@
+.. _96b_carbon_multi_thread_blinky:
+
+Basic Thread Example
+####################
+
+Overview
+********
+
+This example demonstrates spawning of multiple threads using K_THREAD_DEFINE.
+
+The example works by spawning  three threads. The first two threads control a
+separate LED. Both of these LEDs (USR1 and USR2) have their individual loop
+control and timing logic defined by separate functions.
+
+The third thread, uart_out(), sends out messages on the UART using printk().
+
+- blink1() controls the USR1 LED that has a 100ms sleep cycle
+- blink2() controls the USR2 LED that has a 1000ms sleep cycle
+
+Each thread is then defined at compile time using K_THREAD_DEFINE.
+
+Building
+********
+
+.. code-block:: console
+
+    $ cd samples/basic/threads
+    $ make
---
+++ samples/basic/threads/src/main.c
@@ -0,0 +1,70 @@
+#include <zephyr.h>
+#include <device.h>
+#include <gpio.h>
+#include <misc/printk.h>
+#include <board.h>
+
+/* size of stack area used by each thread */
+#define STACKSIZE 1024
+
+/* scheduling priority used by each thread */
+#define PRIORITY 7
+
+/* Change this if you have an LED connected to a custom port */
+#define PORT0	LED0_GPIO_PORT
+#define PORT1	LED1_GPIO_PORT
+
+/* Change this if you have an LED connected to a custom pin */
+#define LED0	LED0_GPIO_PIN
+#define LED1	LED1_GPIO_PIN
+
+
+void blink1(void)
+{
+	int cnt = 0;
+	struct device *gpioa;
+
+	gpioa = device_get_binding(PORT0);
+	gpio_pin_configure(gpioa, LED0, GPIO_DIR_OUT);
+	while (1) {
+		gpio_pin_write(gpioa, LED0, (cnt + 1) % 2);
+		k_sleep(100);
+		cnt++;
+	}
+}
+
+void blink2(void)
+{
+	int cnt = 0;
+	struct device *gpiod;
+
+	gpiod = device_get_binding(PORT1);
+	gpio_pin_configure(gpiod, LED1, GPIO_DIR_OUT);
+	while (1) {
+		gpio_pin_write(gpiod, LED1, cnt % 2);
+		k_sleep(1000);
+		cnt++;
+	}
+}
+
+void uart_out(void)
+{
+	int cnt = 1;
+
+	while (1) {
+		printk("Toggle USR1 LED: Counter = %d\n", cnt);
+		if (cnt >= 10) {
+			printk("Toggle USR2 LED: Counter = %d\n", cnt);
+			cnt = 0;
+		}
+		k_sleep(100);
+		cnt++;
+	}
+}
+
+K_THREAD_DEFINE(blink1_id, STACKSIZE, blink1, NULL, NULL, NULL,
+		PRIORITY, 0, K_NO_WAIT);
+K_THREAD_DEFINE(blink2_id, STACKSIZE, blink2, NULL, NULL, NULL,
+		PRIORITY, 0, K_NO_WAIT);
+K_THREAD_DEFINE(uart_out_id, STACKSIZE, uart_out, NULL, NULL, NULL,
+		PRIORITY, 0, K_NO_WAIT);
