226aa3e578ee61d2f8c0da57c54e0dad14ae112a
Marti Bolivar <marti.bolivar@linaro.org>
2017-10-26 11:05:10 -0400

    scripts: runner: add qemu.py, a stand-in for QEMU
    This, like the shell script, is just a placeholder.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>

---
+++ scripts/support/runner/qemu.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2017 Linaro Limited.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+'''Runner stub for QEMU.'''
+
+from .core import ZephyrBinaryRunner
+
+
+class QemuBinaryRunner(ZephyrBinaryRunner):
+    '''Place-holder for QEMU runner customizations.'''
+
+    def __init__(self, debug=False):
+        super(QemuBinaryRunner, self).__init__(debug=debug)
+
+    def replaces_shell_script(shell_script, command):
+        return shell_script == 'qemu.sh'
+
+    def create_from_env(command, debug):
+        '''Create runner. No environment dependencies.'''
+        return QemuBinaryRunner()
+
+    def run(self, command, **kwargs):
+        if command == 'debugserver':
+            print('Detached GDB server')
