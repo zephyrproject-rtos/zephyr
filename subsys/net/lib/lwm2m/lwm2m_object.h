f5d1a62f1b7219590b2c8515fefeeec1bc2f0bf1
Michael Scott <michael.scott@linaro.org>
2017-10-26 23:32:47 -0700

    net: lwm2m: introduce getter/setter for OPAQUE data
    Each content formatter should have a way of handling opaque data.
    
    For instance TLV data will individually be able to specify a length
    but plain text will take up the rest of the packet.
    
    Signed-off-by: Michael Scott <michael.scott@linaro.org>

--- subsys/net/lib/lwm2m/lwm2m_object.h
+++ subsys/net/lib/lwm2m/lwm2m_object.h
@@ -229,6 +229,9 @@ struct lwm2m_input_context {
 
 	/* length of incoming coap/lwm2m payload */
 	u16_t payload_len;
+
+	/* length of incoming opaque */
+	u16_t opaque_len;
 };
 
 /* LWM2M format writer for the various formats supported */
@@ -265,6 +268,9 @@ struct lwm2m_writer {
 	size_t (*put_bool)(struct lwm2m_output_context *out,
 			   struct lwm2m_obj_path *path,
 			   bool value);
+	size_t (*put_opaque)(struct lwm2m_output_context *out,
+			     struct lwm2m_obj_path *path,
+			     char *buf, size_t buflen);
 };
 
 struct lwm2m_reader {
@@ -280,6 +286,8 @@ struct lwm2m_reader {
 				 float64_value_t *value);
 	size_t (*get_bool)(struct lwm2m_input_context *in,
 			   bool *value);
+	size_t (*get_opaque)(struct lwm2m_input_context *in,
+			     u8_t *buf, size_t buflen, bool *last_block);
 };
 
 /* LWM2M engine context */
@@ -388,6 +396,17 @@ static inline size_t engine_put_bool(struct lwm2m_output_context *out,
 	return out->writer->put_bool(out, path, value);
 }
 
+static inline size_t engine_put_opaque(struct lwm2m_output_context *out,
+				       struct lwm2m_obj_path *path,
+				       char *buf, size_t buflen)
+{
+	if (out->writer->put_opaque) {
+		return out->writer->put_opaque(out, path, buf, buflen);
+	}
+
+	return 0;
+}
+
 static inline size_t engine_get_s32(struct lwm2m_input_context *in,
 				    s32_t *value)
 {
@@ -422,6 +441,17 @@ static inline size_t engine_get_bool(struct lwm2m_input_context *in,
 				     bool *value)
 {
 	return in->reader->get_bool(in, value);
+}
+
+static inline size_t engine_get_opaque(struct lwm2m_input_context *in,
+				       u8_t *buf, size_t buflen,
+				       bool *last_block)
+{
+	if (in->reader->get_opaque) {
+		return in->reader->get_opaque(in, buf, buflen, last_block);
+	}
+
+	return 0;
 }
 
 #endif /* LWM2M_OBJECT_H_ */
