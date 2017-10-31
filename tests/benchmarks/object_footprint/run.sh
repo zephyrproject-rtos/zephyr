a562d24214614b535d0e557dc09424608aa7e515
Anas Nashif <anas.nashif@intel.com>
2017-10-31 09:32:07 -0400

    tests: use cmake to build object benchmarks
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/benchmarks/object_footprint/run.sh
+++ tests/benchmarks/object_footprint/run.sh
@@ -2,10 +2,13 @@
 rm results.txt
 rm -rf results
 for conf in `ls -v *.conf`; do
-	echo $conf
-	outdir=results/$(basename $conf .conf)
-	app=$(basename $conf .conf)
-	make BOARD=arduino_101 CONF_FILE=$conf O=${outdir}
-	${ZEPHYR_BASE}/scripts/sanitycheck -z ${outdir}/${app}.elf >> results.txt
+    echo $conf
+    app=$(basename $conf .conf)
+    build_dir=results/$(basename $conf .conf)
+
+    cmake -DBOARD=arduino_101 -DCONF_FILE=$conf -H. -B${build_dir}
+    cmake --build ${build_dir}
+
+    ${ZEPHYR_BASE}/scripts/sanitycheck -z ${build_dir}/zephyr/${app}.elf >> results.txt
 done
 
