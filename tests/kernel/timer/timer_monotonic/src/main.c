03a38ee6aaceec472cff1cae317bf41dee9a6b4e
Shilpashree L C <shilpashree.lc@intel.com>
2017-11-03 15:38:35 +0530

    tests: timer: convert test case to use ztest
    This patch convert kernel related testcase by replacing
    test points with ztest APIs wherever possible
    
    Signed-off-by: Shilpashree L C <shilpashree.lc@intel.com>

--- tests/kernel/timer/timer_monotonic/src/main.c
+++ tests/kernel/timer/timer_monotonic/src/main.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <ztest.h>
 #include <zephyr.h>
 #include <inttypes.h>
 #include <tc_util.h>
@@ -25,23 +26,19 @@ int test_frequency(void)
 	       sys_clock_hw_cycles_per_sec, pct);
 
 	/* Heuristic: if we're more than 10% off, throw an error */
-	if (pct < 90 || pct > 110) {
-		TC_PRINT("Clock calibration is way off!\n");
-		return -1;
-	}
-
+	zassert_false((pct < 90 || pct > 110), "Clock calibration is way off!\n");
 	return 0;
 }
 
 
-void main(void)
+void test_timer(void)
 {
 	u32_t t_last, t_now, i, errors;
 	s32_t diff;
-	int rv = TC_PASS;
 
 	errors = 0;
 
+//	k_thread_priority_set(k_current_get(), 0);
 	TC_PRINT("sys_clock_us_per_tick = %d\n", sys_clock_us_per_tick);
 	TC_PRINT("sys_clock_hw_cycles_per_tick = %d\n",
 		 sys_clock_hw_cycles_per_tick);
@@ -54,28 +51,23 @@ void main(void)
 
 	for (i = 0; i < 1000000; i++) {
 		t_now = k_cycle_get_32();
-
 		if (t_now < t_last) {
-			diff = t_now - t_last;
-			TC_PRINT("diff = %" PRId32 " (t_last = %" PRIu32
-				 " : t_now = %" PRIu32 "); i = %u\n",
-				 diff, t_last, t_now, i);
-			errors++;
-		}
+                	diff = t_now - t_last;
+                        TC_PRINT("diff = %" PRId32 " (t_last = %" PRIu32
+                                 " : t_now = %" PRIu32 "); i = %u\n",
+                                 diff, t_last, t_now, i);
+                        errors++;
+                }
 		t_last = t_now;
 	}
-
-	if (errors) {
-		TC_PRINT("errors = %d\n", errors);
-		rv = TC_FAIL;
-	} else {
-		TC_PRINT("Cycle results appear to be monotonic\n");
-	}
-
-	if (test_frequency()) {
-		rv = TC_FAIL;
-	}
-
-	TC_END_REPORT(rv);
+	zassert_false(errors, "errors = %d\n", errors);
+	zassert_false(test_frequency(), "test frequency failure");
 }
 
+
+void test_main(void *p1, void *p2, void *p3)
+{
+	ztest_test_suite(test_timer_fn,
+		ztest_unit_test(test_timer));
+	ztest_run_test_suite(test_timer_fn);
+}
