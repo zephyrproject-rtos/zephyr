a2c4996966f8ebf376a0342d3ca22d1e4354fc5e
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2017-11-06 09:41:31 +0100

    ext: lib: Add base64 library
    This library enables support for RFC 1521 base64 encoding/decoding.
    The library is needed for support mcuboot serial recovery interface.
    It is extracted from mbedtls because it could be required as standalone
    module while mbedtls is excluded from build.
    
    Origin: mbed TLS
    License Apache-2.0
    URL: https://tls.mbed.org/download/start/mbedtls-2.6.0-apache.tgz
    Maintained-by: External
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

---
+++ ext/lib/base64/include/mbedtls/config.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2017 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CONFIG_BASE64_H
+#define CONFIG_BASE64_H
+
+/* This is configuration needed for standalone base64 module imported form
+   mbedtls. */
+#define MBEDTLS_BASE64_C
+
+#endif /* CONFIG_BASE64_H */
