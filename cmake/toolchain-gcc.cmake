6b68c5e87a0855d62e5e9224ec5ef440601d447d
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-11-06 17:03:27 +0100

    cmake: Fix system path when using MSYS
    Signed-off-by: Sebastian Boe <sebastian.boe@nordicsemi.no>

--- cmake/toolchain-gcc.cmake
+++ cmake/toolchain-gcc.cmake
@@ -33,6 +33,18 @@ foreach(file_name include include-fixed)
     OUTPUT_VARIABLE _OUTPUT
     )
   string(REGEX REPLACE "\n" "" _OUTPUT ${_OUTPUT})
+
+  if(MSYS)
+    # TODO: Remove this when
+    # https://github.com/zephyrproject-rtos/zephyr/issues/4687 is
+    # resolved
+    execute_process(
+      COMMAND cygpath -u ${_OUTPUT}
+      OUTPUT_VARIABLE _OUTPUT
+      )
+    string(REGEX REPLACE "\n" "" _OUTPUT ${_OUTPUT})
+  endif()
+
   list(APPEND NOSTDINC ${_OUTPUT})
 endforeach()
 
