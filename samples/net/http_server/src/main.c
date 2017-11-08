baba2735f6ec6123a716c89139a07b13143e0f9a
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-11-08 21:54:47 +0200

    samples: net: http: Do not overrun url array
    It is possible to access past end of url buffer by one byte.
    
    Coverity-CID: 178790
    Fixes #4784
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- samples/net/http_server/src/main.c
+++ samples/net/http_server/src/main.c
@@ -245,7 +245,7 @@ static void http_connected(struct http_ctx *ctx,
 			   void *user_data)
 {
 	char url[32];
-	int len = min(sizeof(url), ctx->http.url_len);
+	int len = min(sizeof(url) - 1, ctx->http.url_len);
 
 	memcpy(url, ctx->http.url, len);
 	url[len] = '\0';
