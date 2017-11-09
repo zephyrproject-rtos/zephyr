31bdfc014e786d94608b4405694520f41c5066bf
Andrew Boie <andrew.p.boie@intel.com>
2017-11-08 16:38:03 -0800

    userspace: add support for dynamic kernel objects
    A red-black tree is maintained containing the metadata for all
    dynamically created kernel objects, which are allocated out of the
    system heap.
    
    Currently, k_object_alloc() and k_object_free() are supervisor-only.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- kernel/userspace.c
+++ kernel/userspace.c
@@ -8,11 +8,15 @@
 #include <kernel.h>
 #include <string.h>
 #include <misc/printk.h>
+#include <misc/rb.h>
 #include <kernel_structs.h>
 #include <sys_io.h>
 #include <ksched.h>
 #include <syscall.h>
 #include <syscall_handler.h>
+#include <device.h>
+#include <init.h>
+#include <logging/sys_log.h>
 
 #define MAX_THREAD_BITS		(CONFIG_MAX_THREAD_BYTES * 8)
 
@@ -93,6 +97,185 @@ struct perm_ctx {
 	int child_id;
 	struct k_thread *parent;
 };
+
+#ifdef CONFIG_DYNAMIC_OBJECTS
+struct dyn_obj {
+	struct _k_object kobj;
+	struct rbnode node; /* must be immediately before data member */
+	u8_t data[]; /* The object itself */
+};
+
+struct visit_ctx {
+	_wordlist_cb_func_t func;
+	void *original_context;
+};
+
+extern struct _k_object *_k_object_gperf_find(void *obj);
+extern void _k_object_gperf_wordlist_foreach(_wordlist_cb_func_t func,
+					     void *context);
+
+static int node_lessthan(struct rbnode *a, struct rbnode *b);
+
+static struct rbtree obj_rb_tree = {
+	.lessthan_fn = node_lessthan
+};
+
+/* TODO: incorporate auto-gen with Leandro's patch */
+static size_t obj_size_get(enum k_objects otype)
+{
+	switch (otype) {
+	case K_OBJ_ALERT:
+		return sizeof(struct k_alert);
+	case K_OBJ_MSGQ:
+		return sizeof(struct k_msgq);
+	case K_OBJ_MUTEX:
+		return sizeof(struct k_mutex);
+	case K_OBJ_PIPE:
+		return sizeof(struct k_pipe);
+	case K_OBJ_SEM:
+		return sizeof(struct k_sem);
+	case K_OBJ_STACK:
+		return sizeof(struct k_stack);
+	case K_OBJ_THREAD:
+		return sizeof(struct k_thread);
+	case K_OBJ_TIMER:
+		return sizeof(struct k_timer);
+	default:
+		return sizeof(struct device);
+	}
+}
+
+static int node_lessthan(struct rbnode *a, struct rbnode *b)
+{
+	return a < b;
+}
+
+static inline struct dyn_obj *node_to_dyn_obj(struct rbnode *node)
+{
+	return CONTAINER_OF(node, struct dyn_obj, node);
+}
+
+static struct dyn_obj *dyn_object_find(void *obj)
+{
+	struct rbnode *node;
+	struct dyn_obj *ret;
+	int key;
+
+	/* For any dynamically allocated kernel object, the object
+	 * pointer is just a member of the conatining struct dyn_obj,
+	 * so just a little arithmetic is necessary to locate the
+	 * corresponding struct rbnode
+	 */
+	node = (struct rbnode *)((char *)obj - sizeof(struct rbnode));
+
+	key = irq_lock();
+	if (rb_contains(&obj_rb_tree, node)) {
+		ret = node_to_dyn_obj(node);
+	} else {
+		ret = NULL;
+	}
+	irq_unlock(key);
+
+	return ret;
+}
+
+void *k_object_alloc(enum k_objects otype)
+{
+	struct dyn_obj *dyn_obj;
+	int key;
+
+	/* Stacks are not supported, we don't yet have mem pool APIs
+	 * to request memory that is aligned
+	 */
+	__ASSERT(otype > K_OBJ_ANY && otype < K_OBJ_LAST &&
+		 otype != K_OBJ__THREAD_STACK_ELEMENT,
+		 "bad object type requested");
+
+	dyn_obj = k_malloc(sizeof(*dyn_obj) + obj_size_get(otype));
+	if (!dyn_obj) {
+		SYS_LOG_WRN("could not allocate kernel object");
+		return NULL;
+	}
+
+	dyn_obj->kobj.name = (char *)&dyn_obj->data;
+	dyn_obj->kobj.type = otype;
+	dyn_obj->kobj.flags = 0;
+	memset(dyn_obj->kobj.perms, 0, CONFIG_MAX_THREAD_BYTES);
+
+	/* The allocating thread implicitly gets permission on kernel objects
+	 * that it allocates
+	 */
+	_thread_perms_set(&dyn_obj->kobj, _current);
+
+	key = irq_lock();
+	rb_insert(&obj_rb_tree, &dyn_obj->node);
+	irq_unlock(key);
+
+	return dyn_obj->kobj.name;
+}
+
+void k_object_free(void *obj)
+{
+	struct dyn_obj *dyn_obj;
+	int key;
+
+	/* This function is intentionally not exposed to user mode.
+	 * There's currently no robust way to track that an object isn't
+	 * being used by some other thread
+	 */
+
+	key = irq_lock();
+	dyn_obj = dyn_object_find(obj);
+	if (dyn_obj) {
+		rb_remove(&obj_rb_tree, &dyn_obj->node);
+	}
+	irq_unlock(key);
+
+	if (dyn_obj) {
+		k_free(dyn_obj);
+	}
+}
+
+struct _k_object *_k_object_find(void *obj)
+{
+	struct _k_object *ret;
+
+	ret = _k_object_gperf_find(obj);
+
+	if (!ret) {
+		struct dyn_obj *dyn_obj;
+
+		dyn_obj = dyn_object_find(obj);
+		if (dyn_obj) {
+			ret = &dyn_obj->kobj;
+		}
+	}
+
+	return ret;
+}
+
+static void visit_fn(struct rbnode *node, void *context)
+{
+	struct visit_ctx *vctx = context;
+
+	vctx->func(&node_to_dyn_obj(node)->kobj, vctx->original_context);
+}
+
+void _k_object_wordlist_foreach(_wordlist_cb_func_t func, void *context)
+{
+	struct visit_ctx vctx;
+	int key;
+
+	_k_object_gperf_wordlist_foreach(func, context);
+
+	vctx.func = func;
+	vctx.original_context = context;
+
+	key = irq_lock();
+	rb_walk(&obj_rb_tree, visit_fn, &vctx);
+	irq_unlock(key);
+}
+#endif /* CONFIG_DYNAMIC_OBJECTS */
 
 static int thread_index_get(struct k_thread *t)
 {
