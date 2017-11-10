f0346bfac04704a8d9ca4bdcea718b66d11de01c
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-11-10 11:37:07 +0100

    flash: Change type of FLASH_SCRIPT_ENV_VARS
    The old way of constructing FLASH_SCRIPT_ENV_VARS was corrupting the
    values that were passed to the flasher. This new method is the
    standard way of creating a dictionary/hashmap in CMake and does not
    suffer from the same problem.
    
    This fixes
    https://github.com/zephyrproject-rtos/zephyr/issues/4844#event-1334599401
    
    Signed-off-by: Sebastian Boe <sebastian.boe@nordicsemi.no>

--- boards/arm/sam_e70_xplained/board.cmake
+++ boards/arm/sam_e70_xplained/board.cmake
@@ -1,5 +1,7 @@
 include($ENV{ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 
+set(OPENOCD_POST_CMD "atsamv gpnvm set 1")
+
 set_property(GLOBAL APPEND PROPERTY FLASH_SCRIPT_ENV_VARS
-  OPENOCD_POST_CMD="atsamv gpnvm set 1"
+  OPENOCD_POST_CMD
   )
