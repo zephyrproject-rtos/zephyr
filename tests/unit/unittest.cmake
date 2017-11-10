6257fb5d5695959e07fb11aba894e9e5c6650eaf
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-11-10 13:27:53 +0100

    unittest: Support EXTRA_*_FLAGS
    Signed-off-by: Sebastian Boe <sebastian.boe@nordicsemi.no>

--- tests/unit/unittest.cmake
+++ tests/unit/unittest.cmake
@@ -2,9 +2,17 @@ cmake_minimum_required(VERSION 3.8.2)
 cmake_policy(SET CMP0000 OLD)
 cmake_policy(SET CMP0002 NEW)
 
+enable_language(C CXX ASM)
+
 # Parameters:
 #   SOURCES: list of source files, default main.c
 #   INCLUDE: list of additional include paths relative to ZEPHYR_BASE
+
+separate_arguments(  EXTRA_CFLAGS_AS_LIST UNIX_COMMAND ${EXTRA_CFLAGS})
+separate_arguments(  EXTRA_AFLAGS_AS_LIST UNIX_COMMAND ${EXTRA_AFLAGS})
+separate_arguments(EXTRA_CPPFLAGS_AS_LIST UNIX_COMMAND ${EXTRA_CPPFLAGS})
+separate_arguments(EXTRA_CXXFLAGS_AS_LIST UNIX_COMMAND ${EXTRA_CXXFLAGS})
+separate_arguments(EXTRA_LD_FLAGS_AS_LIST UNIX_COMMAND ${EXTRA_LD_FLAGS})
 
 if(NOT SOURCES)
   set(SOURCES main.c)
@@ -19,7 +27,17 @@ list(APPEND INCLUDE
 
 add_executable(testbinary ${SOURCES})
 
-target_compile_options(testbinary PRIVATE -Wall)
+target_compile_options(testbinary PRIVATE
+  -Wall
+  ${EXTRA_CPPFLAGS_AS_LIST}
+  ${EXTRA_CFLAGS_AS_LIST}
+  $<$<COMPILE_LANGUAGE:CXX>:${EXTRA_CXXFLAGS_AS_LIST}>
+  $<$<COMPILE_LANGUAGE:ASM>:${EXTRA_AFLAGS_AS_LIST}>
+  )
+
+target_link_libraries(testbinary PRIVATE
+  ${EXTRA_LDFLAGS_AS_LIST}
+  )
 
 if(COVERAGE)
   target_compile_options(testbinary PRIVATE
