457699cd3c4697a033b54715bf1ec8930f860255
Maureen Helm <maureen.helm@nxp.com>
2017-11-11 08:54:34 -0600

    arm: Enable the ARM MPU on the NXP i.MX RT SoC and board
    Unlike other NXP SoCs currently in Zephyr, the mimxrt1052 has the ARM
    MPU rather than the NXP MPU. Start out by enabling it with a simple set
    of memory regions for "flash" (ITCM), "ram" (DTCM), and the peripheral
    buses. More regions will need to be added when we implement support for
    external memories.
    
    Tested with:
    - samples/mpu/mpu_stack_guard_test
    - tests/kernel/mem_protect/protection
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

---
+++ arch/arm/soc/nxp_imx/rt/arm_mpu_mem_cfg.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2017, NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef _IMX_RT_MPU_MEM_CFG_H_
+#define _IMX_RT_MPU_MEM_CFG_H_
+
+#include <soc.h>
+#include <arch/arm/cortex_m/mpu/arm_mpu.h>
+
+/* Flash Region Definitions */
+#if CONFIG_FLASH_SIZE == 64
+#define REGION_FLASH_SIZE REGION_64K
+#elif CONFIG_FLASH_SIZE == 128
+#define REGION_FLASH_SIZE REGION_128K
+#elif CONFIG_FLASH_SIZE == 256
+#define REGION_FLASH_SIZE REGION_256K
+#elif CONFIG_FLASH_SIZE == 512
+#define REGION_FLASH_SIZE REGION_512K
+#elif CONFIG_FLASH_SIZE == 1024
+#define REGION_FLASH_SIZE REGION_1M
+#elif CONFIG_FLASH_SIZE == 2048
+#define REGION_FLASH_SIZE REGION_2M
+#else
+#error "Unsupported configuration"
+#endif
+
+/* SRAM Region Definitions */
+#if CONFIG_SRAM_SIZE == 32
+#define REGION_SRAM_0_SIZE REGION_32K
+#elif CONFIG_SRAM_SIZE == 64
+#define REGION_SRAM_0_SIZE REGION_64K
+#elif CONFIG_SRAM_SIZE == 128
+#define REGION_SRAM_0_SIZE REGION_128K
+#elif CONFIG_SRAM_SIZE == 256
+#define REGION_SRAM_0_SIZE REGION_256K
+#else
+#error "Unsupported configuration"
+#endif
+
+#endif /* _IMX_RT_MPU_MEM_CFG_H_ */
