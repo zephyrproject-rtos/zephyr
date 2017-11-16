07007526139081f75877a9ac8eb6a99529ca5b9d
Anas Nashif <anas.nashif@intel.com>
2017-11-16 04:34:08 -0500

    cmake: parse variables from board yaml file
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- cmake/meta.cmake
+++ cmake/meta.cmake
@@ -1,10 +1,10 @@
 
-
-
-
 execute_process(
 	COMMAND
 	${PROJECT_SOURCE_DIR}/scripts/board_yaml2h.py
 	-f ${BOARD_DIR}/${BOARD}.yaml
-	-o ${PROJECT_BINARY_DIR}/include/generated/board_meta.h
+	-H ${PROJECT_BINARY_DIR}/include/generated/board_meta.h
+	-c ${PROJECT_BINARY_DIR}/include/generated/board_meta.conf
 	)
+
+import_kconfig(${PROJECT_BINARY_DIR}/include/generated/board_meta.conf)
--- scripts/board_yaml2h.py
+++ scripts/board_yaml2h.py
@@ -19,10 +19,16 @@ def parse_args():
             help="Board file to be parsed"
             )
     parser.add_argument(
-            '-o',
-            '--out-file',
+            '-H',
+            '--header-file',
             default=None,
-            help="name of output file"
+            help="name of output header file"
+            )
+    parser.add_argument(
+            '-c',
+            '--conf-file',
+            default=None,
+            help="name of output conf file"
             )
     return parser.parse_args()
 
@@ -32,8 +38,8 @@ def main():
                      "scripts", "sanity_chk", "sanitycheck-platform-schema.yaml"))
     board = scl.yaml_load_verify(args.file, schema)
 
-    if args.out_file:
-        outfile = open(args.out_file, "w")
+    if args.header_file:
+        outfile = open(args.header_file, "w")
         output =  "/* This file is auto-generated */"
         output += "\n\n\n"
         output += "#define META_BOARD_NAME \"%s\"\n" %(board['name'])
@@ -43,6 +49,24 @@ def main():
             output += "\n/* Supported Features */\n"
             for s in board['supported']:
                 output += "#define META_BOARD_SUPPORTED_%s 1\n" %(s.replace(":","_").upper())
+
+        outfile.write(output)
+        outfile.close()
+    if args.conf_file:
+        outfile = open(args.conf_file, "w")
+        output =  "# This file is auto-generated"
+        output += "\n\n\n"
+        output += "CONFIG_META_BOARD_NAME=\"%s\"\n" %(board['name'])
+        output += "CONFIG_META_BOARD_IDENTIFIER=\"%s\"\n" %(board['identifier'])
+
+        if 'supported' in board:
+            output += "\n# Supported Features\n"
+            for s in board['supported']:
+                output += "CONFIG_META_BOARD_SUPPORTED_%s=y\n" %(s.replace(":","_").upper())
+        if 'debug_interface' in board:
+            output += "\n# Debug Interface\n"
+            for s in board['debug_interface']:
+                output += "CONFIG_META_DEBUG_INTERFACE_%s=y\n" %(s.replace(":","_").upper())
 
         outfile.write(output)
         outfile.close()
