0c30c3805bdac68502c8426c047dc7bd6d3e12ed
Anas Nashif <anas.nashif@intel.com>
2017-11-17 19:02:11 -0500

    doc: add a new template variable
    Signify if the documentation is for a release or if it is the
    development version from master.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- doc/conf.py
+++ doc/conf.py
@@ -158,8 +158,10 @@ if tags.has('daily') or tags.has('release'):
 
 
 if tags.has('release'):
+    is_release = True
     docs_title = 'Docs / %s' %(version)
 else:
+    is_release = False
     docs_title = 'Docs'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
@@ -346,6 +348,7 @@ breathe_default_project = "Zephyr"
 html_context = {
     'show_license': html_show_license,
     'docs_title': docs_title,
+    'is_release': is_release,
 }
 
 extlinks = {'jira': ('https://jira.zephyrproject.org/browse/%s', '')}
