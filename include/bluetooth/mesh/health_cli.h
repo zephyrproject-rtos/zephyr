ce10be3a2ae0665d9fc610d9ff4201cd8e9f5339
Johan Hedberg <johan.hedberg@intel.com>
2017-11-22 13:45:09 +0200

    Bluetooth: Mesh: Add support for Health Fault Test messages
    Add support for sending Test and Test Unacknowledged messages.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- include/bluetooth/mesh/health_cli.h
+++ include/bluetooth/mesh/health_cli.h
@@ -46,6 +46,10 @@ int bt_mesh_health_fault_clear(u16_t net_idx, u16_t addr, u16_t app_idx,
 			       u16_t cid, u8_t *test_id, u8_t *faults,
 			       size_t *fault_count);
 
+int bt_mesh_health_fault_test(u16_t net_idx, u16_t addr, u16_t app_idx,
+			      u16_t cid, u8_t test_id, u8_t *faults,
+			      size_t *fault_count);
+
 s32_t bt_mesh_health_cli_timeout_get(void);
 void bt_mesh_health_cli_timeout_set(s32_t timeout);
 
