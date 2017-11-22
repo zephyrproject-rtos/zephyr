c880f7b2e294d95ec2e2e32bf73fb3f3b7bf91ef
Johan Hedberg <johan.hedberg@intel.com>
2017-11-22 18:16:42 +0200

    Bluetooth: Mesh: Add support for Health Period messages
    Add support for sending Health Period messages, as well as commands to
    use these new APIs from the shell.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- include/bluetooth/mesh/health_cli.h
+++ include/bluetooth/mesh/health_cli.h
@@ -50,6 +50,12 @@ int bt_mesh_health_fault_test(u16_t net_idx, u16_t addr, u16_t app_idx,
 			      u16_t cid, u8_t test_id, u8_t *faults,
 			      size_t *fault_count);
 
+int bt_mesh_health_period_get(u16_t net_idx, u16_t addr, u16_t app_idx,
+			      u8_t *divisor);
+
+int bt_mesh_health_period_set(u16_t net_idx, u16_t addr, u16_t app_idx,
+			      u8_t divisor, u8_t *updated_divisor);
+
 s32_t bt_mesh_health_cli_timeout_get(void);
 void bt_mesh_health_cli_timeout_set(s32_t timeout);
 
