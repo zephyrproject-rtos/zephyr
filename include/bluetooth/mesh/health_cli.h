b26231d770f458728ea0d5b6718d3f6f9afeb384
Johan Hedberg <johan.hedberg@intel.com>
2017-11-22 09:29:55 +0200

    Bluetooth: Mesh: Add support for sending Health Fault Get message
    Add the needed Health Client API for sending Health Fault Get, and add
    a command to the shell to utilize it.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- include/bluetooth/mesh/health_cli.h
+++ include/bluetooth/mesh/health_cli.h
@@ -34,6 +34,10 @@ extern const struct bt_mesh_model_op bt_mesh_health_cli_op[];
 
 int bt_mesh_health_cli_set(struct bt_mesh_model *model);
 
+int bt_mesh_health_fault_get(u16_t net_idx, u16_t addr, u16_t app_idx,
+			     u16_t cid, u8_t *test_id, u8_t *faults,
+			     size_t *fault_count);
+
 s32_t bt_mesh_health_cli_timeout_get(void);
 void bt_mesh_health_cli_timeout_set(s32_t timeout);
 
