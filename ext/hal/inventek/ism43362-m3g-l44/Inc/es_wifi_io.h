42e4c0d14979e27241518b087a335d37c2e4902b
Vincent Veron <vincent.veron@st.com>
2017-11-23 15:57:11 +0100

    drivers: wifi : add Inventek customization
    This is the customization needed by ext/hal/inventek/ism43362-m3g-l44
    to run on Zephyr.
    It mainly concern spi and gpio.
    
    Signed-off-by: Vincent Veron <vincent.veron@st.com>

--- ext/hal/inventek/ism43362-m3g-l44/Inc/es_wifi_io.h
+++ ext/hal/inventek/ism43362-m3g-l44/Inc/es_wifi_io.h
@@ -45,40 +45,19 @@
 /* Exported constants --------------------------------------------------------*/
 
 /* Exported macro ------------------------------------------------------------*/
-#define WIFI_RESET_MODULE()                do{\
-                                            HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, GPIO_PIN_RESET );\
-                                            HAL_Delay(10);\
-                                            HAL_GPIO_WritePin( GPIOE, GPIO_PIN_8, GPIO_PIN_SET );\
-                                            HAL_Delay(500);\
-                                             }while(0);
 
-
-#define WIFI_ENABLE_NSS()                  do{ \
-                                             HAL_GPIO_WritePin( GPIOE, GPIO_PIN_0, GPIO_PIN_RESET );\
-                                             HAL_Delay(10);\
-                                             }while(0);
-
-#define WIFI_DISABLE_NSS()                 do{ \
-                                             HAL_GPIO_WritePin( GPIOE, GPIO_PIN_0, GPIO_PIN_SET );\
-                                             HAL_Delay(10);\
-                                             }while(0);
-
-#define WIFI_IS_CMDDATA_READY()            (HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_1) == GPIO_PIN_SET)
-
-    
-
-/* Exported functions ------------------------------------------------------- */ 
+/* Exported functions ------------------------------------------------------- */
 void    SPI_WIFI_MspInit(SPI_HandleTypeDef* hspi);
 int8_t  SPI_WIFI_DeInit(void);
 int8_t  SPI_WIFI_Init(void);
 int16_t SPI_WIFI_ReceiveData(uint8_t *pData, uint16_t len, uint32_t timeout);
 int16_t SPI_WIFI_SendData( uint8_t *pData, uint16_t len, uint32_t timeout);
 void    SPI_WIFI_Delay(uint32_t Delay);
-    
+
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* __WIFI_IO__ */
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/    
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
