686a8ef8a4649575e62e6ac8e0c4fd2d0372e770
Alberto Escolar Piedras <alpi@oticon.com>
2017-11-24 13:28:16 +0100

    native: added bitfield operations to arch
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>

--- include/arch/posix/asm_inline_gcc.h
+++ include/arch/posix/asm_inline_gcc.h
@@ -28,6 +28,7 @@ extern "C" {
 
 #include <toolchain/common.h>
 #include <zephyr/types.h>
+#include <sys_io.h>
 
 /**
  *
@@ -68,6 +69,124 @@ static ALWAYS_INLINE unsigned int find_lsb_set(u32_t op)
 	return __builtin_ffs(op);
 }
 
+
+static ALWAYS_INLINE u8_t sys_read8(mem_addr_t addr)
+{
+	return *(volatile u8_t *)addr;
+}
+
+static ALWAYS_INLINE void sys_write8(u8_t data, mem_addr_t addr)
+{
+	*(volatile u8_t *)addr = data;
+}
+
+static ALWAYS_INLINE u16_t sys_read16(mem_addr_t addr)
+{
+	return *(volatile u16_t *)addr;
+}
+
+static ALWAYS_INLINE void sys_write16(u16_t data, mem_addr_t addr)
+{
+	*(volatile u16_t *)addr = data;
+}
+
+static ALWAYS_INLINE u32_t sys_read32(mem_addr_t addr)
+{
+	return *(volatile u32_t *)addr;
+}
+
+static ALWAYS_INLINE void sys_write32(u32_t data, mem_addr_t addr)
+{
+	*(volatile u32_t *)addr = data;
+}
+
+/* Memory bit manipulation functions */
+
+static ALWAYS_INLINE void sys_set_bit(mem_addr_t addr, unsigned int bit)
+{
+	u32_t temp = *(volatile u32_t *)addr;
+
+	*(volatile u32_t *)addr = temp | (1 << bit);
+}
+
+static ALWAYS_INLINE void sys_clear_bit(mem_addr_t addr, unsigned int bit)
+{
+	u32_t temp = *(volatile u32_t *)addr;
+
+	*(volatile u32_t *)addr = temp & ~(1 << bit);
+}
+
+static ALWAYS_INLINE int sys_test_bit(mem_addr_t addr, unsigned int bit)
+{
+	u32_t temp = *(volatile u32_t *)addr;
+
+	return temp & (1 << bit);
+}
+
+static ALWAYS_INLINE
+	void sys_bitfield_set_bit(mem_addr_t addr, unsigned int bit)
+{
+	/* Doing memory offsets in terms of 32-bit values to prevent
+	 * alignment issues
+	 */
+	sys_set_bit(addr + ((bit >> 5) << 2), bit & 0x1F);
+}
+
+static ALWAYS_INLINE
+	void sys_bitfield_clear_bit(mem_addr_t addr, unsigned int bit)
+{
+	sys_clear_bit(addr + ((bit >> 5) << 2), bit & 0x1F);
+}
+
+static ALWAYS_INLINE
+	int sys_bitfield_test_bit(mem_addr_t addr, unsigned int bit)
+{
+	return sys_test_bit(addr + ((bit >> 5) << 2), bit & 0x1F);
+}
+
+static ALWAYS_INLINE
+	int sys_test_and_set_bit(mem_addr_t addr, unsigned int bit)
+{
+	int ret;
+
+	ret = sys_test_bit(addr, bit);
+	sys_set_bit(addr, bit);
+
+	return ret;
+}
+
+static ALWAYS_INLINE
+	int sys_test_and_clear_bit(mem_addr_t addr, unsigned int bit)
+{
+	int ret;
+
+	ret = sys_test_bit(addr, bit);
+	sys_clear_bit(addr, bit);
+
+	return ret;
+}
+
+static ALWAYS_INLINE
+	int sys_bitfield_test_and_set_bit(mem_addr_t addr, unsigned int bit)
+{
+	int ret;
+
+	ret = sys_bitfield_test_bit(addr, bit);
+	sys_bitfield_set_bit(addr, bit);
+
+	return ret;
+}
+
+static ALWAYS_INLINE
+	int sys_bitfield_test_and_clear_bit(mem_addr_t addr, unsigned int bit)
+{
+	int ret;
+
+	ret = sys_bitfield_test_bit(addr, bit);
+	sys_bitfield_clear_bit(addr, bit);
+
+	return ret;
+}
 
 #endif /* _ASMLANGUAGE */
 
