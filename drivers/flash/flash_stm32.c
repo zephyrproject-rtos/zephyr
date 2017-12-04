109a6ea09127b03716ce187443b2e6a9264496b7
Neil Armstrong <narmstrong@baylibre.com>
2017-12-04 14:09:59 +0100

    flash: stm32: add support for STM32F0 flash controller
    Add the STM32F0 Embedded Flash support in the flash_stm32 driver
    interface.
    The STM32F0 has a particularity or use the HSI as clock source
    for the flash controller interface, so this clock must be
    enabled directly in the case HSE or another clock is used by
    the system clock.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>

--- drivers/flash/flash_stm32.c
+++ drivers/flash/flash_stm32.c
@@ -34,16 +34,25 @@ static inline void flash_stm32_sem_give(struct device *dev)
 static int flash_stm32_check_status(struct device *dev)
 {
 	u32_t const error =
-		FLASH_FLAG_WRPERR |
+#if defined(FLASH_FLAG_PGAERR)
 		FLASH_FLAG_PGAERR |
+#endif
 #if defined(FLASH_FLAG_RDERR)
 		FLASH_FLAG_RDERR  |
 #endif
 #if defined(FLASH_FLAG_PGPERR)
 		FLASH_FLAG_PGPERR |
 #endif
+#if defined(FLASH_FLAG_PGSERR)
 		FLASH_FLAG_PGSERR |
-		FLASH_FLAG_OPERR;
+#endif
+#if defined(FLASH_FLAG_OPERR)
+		FLASH_FLAG_OPERR |
+#endif
+#if defined(FLASH_FLAG_PGERR)
+		FLASH_FLAG_PGERR |
+#endif
+		FLASH_FLAG_WRPERR;
 
 	if (FLASH_STM32_REGS(dev)->sr & error) {
 		return -EIO;
@@ -73,6 +82,7 @@ int flash_stm32_wait_flash_idle(struct device *dev)
 	return 0;
 }
 
+#if !defined(CONFIG_SOC_SERIES_STM32F0X)
 static void flash_stm32_flush_caches(struct device *dev)
 {
 #if defined(CONFIG_SOC_SERIES_STM32F4X)
@@ -95,6 +105,7 @@ static void flash_stm32_flush_caches(struct device *dev)
 		regs->acr.val |= FLASH_ACR_DCEN;
 	}
 }
+#endif
 
 static int flash_stm32_read(struct device *dev, off_t offset, void *data,
 			    size_t len)
@@ -128,7 +139,9 @@ static int flash_stm32_erase(struct device *dev, off_t offset, size_t len)
 
 	rc = flash_stm32_block_erase_loop(dev, offset, len);
 
+#if !defined(CONFIG_SOC_SERIES_STM32F0X)
 	flash_stm32_flush_caches(dev);
+#endif
 
 	flash_stm32_sem_give(dev);
 
@@ -161,6 +174,8 @@ static int flash_stm32_write_protection(struct device *dev, bool enable)
 {
 #if defined(CONFIG_SOC_SERIES_STM32F4X)
 	struct stm32f4x_flash *regs = FLASH_STM32_REGS(dev);
+#elif defined(CONFIG_SOC_SERIES_STM32F0X)
+	struct stm32f0x_flash *regs = FLASH_STM32_REGS(dev);
 #elif defined(CONFIG_SOC_SERIES_STM32L4X)
 	struct stm32l4x_flash *regs = FLASH_STM32_REGS(dev);
 #endif
@@ -188,7 +203,11 @@ static int flash_stm32_write_protection(struct device *dev, bool enable)
 }
 
 static struct flash_stm32_priv flash_data = {
-#if defined(CONFIG_SOC_SERIES_STM32F4X)
+#if defined(CONFIG_SOC_SERIES_STM32F0X)
+	.regs = (struct stm32f0x_flash *) FLASH_R_BASE,
+	.pclken = { .bus = STM32_CLOCK_BUS_AHB1,
+		    .enr = LL_AHB1_GRP1_PERIPH_FLASH },
+#elif defined(CONFIG_SOC_SERIES_STM32F4X)
 	.regs = (struct stm32f4x_flash *) FLASH_R_BASE,
 #elif defined(CONFIG_SOC_SERIES_STM32L4X)
 	.regs = (struct stm32l4x_flash *) FLASH_R_BASE,
@@ -205,7 +224,9 @@ static const struct flash_driver_api flash_stm32_api = {
 #ifdef CONFIG_FLASH_PAGE_LAYOUT
 	.page_layout = flash_stm32_page_layout,
 #endif
-#if defined(CONFIG_SOC_SERIES_STM32F4X)
+#if defined(CONFIG_SOC_SERIES_STM32F0X)
+	.write_block_size = FLASH_WRITE_BLOCK_SIZE,
+#elif defined(CONFIG_SOC_SERIES_STM32F4X)
 	.write_block_size = 1,
 #elif defined(CONFIG_SOC_SERIES_STM32L4X)
 	.write_block_size = 8,
@@ -215,8 +236,20 @@ static const struct flash_driver_api flash_stm32_api = {
 static int stm32_flash_init(struct device *dev)
 {
 	struct flash_stm32_priv *p = FLASH_STM32_PRIV(dev);
-#if defined(CONFIG_SOC_SERIES_STM32L4X)
+#if defined(CONFIG_SOC_SERIES_STM32L4X) || \
+	defined(CONFIG_SOC_SERIES_STM32F0X)
 	struct device *clk = device_get_binding(STM32_CLOCK_CONTROL_NAME);
+
+	/*
+	 * On STM32F0, Flash interface clock source is always HSI,
+	 * so statically enable HSI here.
+	 */
+#if defined(CONFIG_SOC_SERIES_STM32F0X)
+	LL_RCC_HSI_Enable();
+
+	while (!LL_RCC_HSI_IsReady()) {
+	}
+#endif
 
 	/* enable clock */
 	clock_control_on(clk, (clock_control_subsys_t *)&p->pclken);
