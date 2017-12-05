8f4b4db71ffd26b8b3d08475cb8575a3d5667c1d
Michael Scott <michael@opensourcefoundries.com>
2017-12-04 21:52:16 -0800

    net: lwm2m: fix possible dereference in RD client
    While looping through possible lwm2m_ctx matches, we're referencing
    remote before checking that the context itself is valid.
    
    Also, reduce indentation issues.
    
    Signed-off-by: Michael Scott <michael@opensourcefoundries.com>

--- subsys/net/lib/lwm2m/lwm2m_rd_client.c
+++ subsys/net/lib/lwm2m/lwm2m_rd_client.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2017 Linaro Limited
+ * Copyright (c) 2017 Open Source Foundries Limited.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -163,34 +164,36 @@ static int find_clients_index(const struct sockaddr *addr)
 	struct sockaddr *remote;
 
 	for (i = 0; i < client_count; i++) {
+		if (!clients[i].ctx) {
+			continue;
+		}
+
 		remote = &clients[i].ctx->net_app_ctx.default_ctx->remote;
-		if (clients[i].ctx) {
-			if (remote->sa_family != addr->sa_family) {
-				continue;
-			}
+		if (remote->sa_family != addr->sa_family) {
+			continue;
+		}
 
 #if defined(CONFIG_NET_IPV6)
-			if (remote->sa_family == AF_INET6 &&
-			    net_ipv6_addr_cmp(&net_sin6(remote)->sin6_addr,
-					      &net_sin6(addr)->sin6_addr) &&
-			    net_sin6(remote)->sin6_port ==
-					net_sin6(addr)->sin6_port) {
-				index = i;
-				break;
-			}
+		if (remote->sa_family == AF_INET6 &&
+		    net_ipv6_addr_cmp(&net_sin6(remote)->sin6_addr,
+				      &net_sin6(addr)->sin6_addr) &&
+		    net_sin6(remote)->sin6_port ==
+				net_sin6(addr)->sin6_port) {
+			index = i;
+			break;
+		}
 #endif
 
 #if defined(CONFIG_NET_IPV4)
-			if (remote->sa_family == AF_INET &&
-			    net_ipv4_addr_cmp(&net_sin(remote)->sin_addr,
-					      &net_sin(addr)->sin_addr) &&
-			    net_sin(remote)->sin_port ==
-					net_sin(addr)->sin_port) {
-				index = i;
-				break;
-			}
-#endif
+		if (remote->sa_family == AF_INET &&
+		    net_ipv4_addr_cmp(&net_sin(remote)->sin_addr,
+				      &net_sin(addr)->sin_addr) &&
+		    net_sin(remote)->sin_port ==
+				net_sin(addr)->sin_port) {
+			index = i;
+			break;
 		}
+#endif
 	}
 
 	return index;
