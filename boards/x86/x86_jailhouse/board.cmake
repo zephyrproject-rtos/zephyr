666e8d6ea985cebaf1e92a4dc6325c049b2ed094
Gustavo Lima Chaves <gustavo.lima.chaves@intel.com>
2017-12-06 15:27:50 -0800

    cmake: jailhouse: fix arguments passed to QEMU on make run target
    "-serial mon:stdio" was already injected elsewhere, so remove
    duplication. That argument, when coming from the common infrastructure,
    also comes as one of the last ones. Because of that, the former "-serial
    vc" was taking precedence and we were never seeing output on stdio.
    Removing the virtual console altogether, for now, fixes that.
    
    Finally, since cmake came in, we have to fix the path for the fsdev
    argument to QEMU as well.
    
    Signed-off-by: Gustavo Lima Chaves <gustavo.lima.chaves@intel.com>

--- boards/x86/x86_jailhouse/board.cmake
+++ boards/x86/x86_jailhouse/board.cmake
@@ -15,12 +15,11 @@ set(QEMU_FLAGS_${ARCH}
   -cpu ${QEMU_CPU_TYPE_${ARCH}}
   -drive file=${JAILHOUSE_QEMU_IMG_FILE},format=qcow2,id=disk,if=none
   -device ide-hd,drive=disk
-  -serial mon:stdio
-  -serial vc -netdev user,id=net
+  -netdev user,id=net
   -device e1000e,addr=2.0,netdev=net
   -device intel-hda,addr=1b.0
   -device hda-duplex
-  -fsdev local,path=./,security_model=passthrough,id=vfs
+  -fsdev local,path=./zephyr,security_model=passthrough,id=vfs
   -device virtio-9p-pci,addr=1f.7,mount_tag=host,fsdev=vfs
   )
 
