f7ec62eb8525b5d3f5351df183c1f550d77d4e80
Andy Gross <andy.gross@linaro.org>
2017-12-08 12:26:38 -0600

    arm: mpu: Enable userspace support for NXP and ARM
    This patch set implements the APIs and changed required to support
    the user mode thread support.
    
    Signed-off-by: Andy Gross <andy.gross@linaro.org>

--- include/arch/arm/cortex_m/mpu/arm_core_mpu_dev.h
+++ include/arch/arm/cortex_m/mpu/arm_core_mpu_dev.h
@@ -30,9 +30,11 @@ extern "C" {
  * be managed inside the MPU driver and not escalated.
  */
 /* Thread Stack Region Intent Type */
+#define THREAD_STACK_USER_REGION 0x0	/* fake region for user mode stack */
 #define THREAD_STACK_REGION 0x1
-#define THREAD_STACK_GUARD_REGION 0x2
-#define THREAD_DOMAIN_PARTITION_REGION 0x3
+#define THREAD_APP_DATA_REGION 0x2
+#define THREAD_STACK_GUARD_REGION 0x3
+#define THREAD_DOMAIN_PARTITION_REGION 0x4
 
 #if defined(CONFIG_ARM_CORE_MPU)
 struct k_mem_domain;
