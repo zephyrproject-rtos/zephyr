aac804ec48274fb3c99b9187dbfa74e953b1e30c
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2017-12-12 20:09:23 -0500

    tests: boards: add SysID test app for altera_max10 board
    Add test application to demonstrate the usage of Nios-II
    System ID soft IP.
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>

---
+++ tests/boards/altera_max10/sysid/src/sysid.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <ztest.h>
+
+#include "altera_avalon_sysid.h"
+
+void testing_sysid(void)
+{
+	s32_t sysid, status = TC_FAIL;
+
+	sysid = alt_avalon_sysid_test();
+	if (!sysid) {
+		status = TC_PASS;
+		TC_PRINT("[SysID] hardware and software appear to be in sync\n");
+	} else if (sysid > 0) {
+		TC_PRINT("[SysID] software appears to be older than hardware\n");
+	} else {
+		TC_PRINT("[SysID] hardware appears to be older than software\n");
+	}
+
+	zassert_equal(status, TC_PASS, "SysID test failed");
+}
+
+void test_main(void)
+{
+	ztest_test_suite(nios2_sysid_test_suite,
+			ztest_unit_test(testing_sysid));
+	ztest_run_test_suite(nios2_sysid_test_suite);
+}
