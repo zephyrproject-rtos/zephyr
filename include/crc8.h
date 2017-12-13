a1b4902314d992bfa412f98f023d4e1cc0723116
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2017-12-13 14:57:38 +0100

    lib: crc: Add crc8-ccitt implementation
    This patch adds crc8-ccitt calculation routine.
    
    CRC8 CCITT is required for Flash Circular Buffer
    module (originally mynewt module).
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

---
+++ include/crc8.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017 Nordic Semiconductor ASA
+ * Copyright (c) 2015 Runtime Inc
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __CRC8_H_
+#define __CRC8_H_
+
+#include <zephyr/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Initial value expected to be used at the beginning of the crc8_ccitt
+ * computation.
+ */
+#define CRC8_CCITT_INITIAL_VALUE 0xFF
+
+/**
+ * @brief Compute CCITT variant of CRC 8
+ *
+ * Normal CCITT variant of CRC 8 is using 0x07.
+ *
+ * @param initial_value Initial value for the CRC computation
+ * @param buf Input bytes for the computation
+ * @param len Length of the input in bytes
+ *
+ * @return The computed CRC8 value
+ */
+u8_t crc8_ccitt(u8_t initial_value, void *buf, int len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
