be61a66e14bbba0ee6428e02cdd2434741b37095
Anas Nashif <anas.nashif@intel.com>
2017-12-15 16:33:10 -0500

    boards: remove panther board
    This board was never released and is similar in many ways to the
    quark_se_c1000_devboard.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- boards/x86/panther/board.cmake
+++
@@ -1,4 +0,0 @@
-set(PRE_LOAD targets 1)
-board_runner_args(openocd "--cmd-pre-load=\"${PRE_LOAD}\"")
-set(OPENOCD_ADDRESS ${CONFIG_PHYS_LOAD_ADDR})
-include($ENV{ZEPHYR_BASE}/boards/common/openocd.board.cmake)
