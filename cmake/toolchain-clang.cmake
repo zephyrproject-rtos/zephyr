f0f71b641e1e337178711034b5112c3560a39d22
Anas Nashif <anas.nashif@intel.com>
2017-12-29 23:43:03 -0500

    clang: wip
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- cmake/toolchain-clang.cmake
+++ cmake/toolchain-clang.cmake
@@ -1,24 +1,26 @@
 
-#set(triple arm-none-eabi)
-set(triple i686-pc-none-elf)
+set(CLANG_ROOT $ENV{CLANG_ROOT_DIR})
+
+set(triple arm-none-eabi)
+#set(triple i686-pc-none-elf)
 #set(triple i386-pc-elfiamcu)
 
-set(CMAKE_C_COMPILER /opt/llvm4.0.1/bin/clang)
-set(CMAKE_C_COMPILER_TARGET ${triple})
-#set(CMAKE_ASM_COMPILER llvm-as)
+set(CMAKE_C_COMPILER          ${CLANG_ROOT}/bin/clang)
+set(CMAKE_C_COMPILER_TARGET   ${triple})
+#set(CMAKE_ASM_COMPILER       ${CLANG_ROOT}/bin/llvm-as)
 set(CMAKE_ASM_COMPILER_TARGET ${triple})
-set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_CXX_COMPILER        ${CLANG_ROOT}/bin/clang++)
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
-set(CMAKE_AR           "/opt/llvm4.0.1/bin/llvm-ar"  CACHE INTERNAL " " FORCE)
-set(CMAKE_LINKER       "/opt/llvm4.0.1/bin/llvm-link" CACHE INTERNAL " " FORCE)
-SET(CMAKE_NM      "/opt/llvm4.0.1/bin/llvm-nm"       CACHE INTERNAL " " FORCE)
-SET(CMAKE_OBJDUMP "/opt/llvm4.0.1/bin/llvm-objdump" CACHE INTERNAL " " FORCE)
-SET(CMAKE_RANLIB  "/opt/llvm4.0.1/bin/llvm-ranlib" CACHE INTERNAL " " FORCE)
-set(CMAKE_OBJCOPY      objcopy CACHE INTERNAL " " FORCE)
-set(CMAKE_READELF      readelf CACHE INTERNAL " " FORCE)
+set(CMAKE_AR                  "${CLANG_ROOT}/bin/llvm-ar"  CACHE INTERNAL " " FORCE)
+set(CMAKE_LINKER              "${CLANG_ROOT}/bin/llvm-link" CACHE INTERNAL " " FORCE)
+SET(CMAKE_NM                  "${CLANG_ROOT}/bin/llvm-nm"       CACHE INTERNAL " " FORCE)
+SET(CMAKE_OBJDUMP             "${CLANG_ROOT}/bin/llvm-objdump" CACHE INTERNAL " " FORCE)
+SET(CMAKE_RANLIB              "${CLANG_ROOT}/bin/llvm-ranlib" CACHE INTERNAL " " FORCE)
+set(CMAKE_OBJCOPY             objcopy CACHE INTERNAL " " FORCE)
+set(CMAKE_READELF             readelf CACHE INTERNAL " " FORCE)
 
 
-#set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nosys.specs")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nosys.specs")
 
 
 set(NOSTDINC "")
@@ -30,22 +32,12 @@ foreach(file_name include include-fixed)
     )
   string(REGEX REPLACE "\n" "" _OUTPUT ${_OUTPUT})
 
-  if(MSYS)
-    # TODO: Remove this when
-    # https://github.com/zephyrproject-rtos/zephyr/issues/4687 is
-    # resolved
-    execute_process(
-      COMMAND cygpath -u ${_OUTPUT}
-      OUTPUT_VARIABLE _OUTPUT
-      )
-    string(REGEX REPLACE "\n" "" _OUTPUT ${_OUTPUT})
-  endif()
-
   list(APPEND NOSTDINC ${_OUTPUT})
 endforeach()
 
 foreach(isystem_include_dir ${NOSTDINC})
   list(APPEND isystem_include_flags -isystem ${isystem_include_dir})
 endforeach()
+
 set(CMAKE_REQUIRED_FLAGS -nostartfiles -nostdlib ${isystem_include_flags} -Wl,--unresolved-symbols=ignore-in-object-files)
 string(REPLACE ";" " " CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
