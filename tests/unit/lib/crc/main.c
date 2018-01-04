1d59bf121265bec86fb317588dd5c889d1159eba
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2018-01-04 12:01:59 +0100

    tests: unit: lib: crc: Add test for crc8-ccitt implementation
    CRC test suite was extended by crc8-ccitt test.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- tests/unit/lib/crc/main.c
+++ tests/unit/lib/crc/main.c
@@ -7,6 +7,7 @@
 #include <ztest.h>
 
 #include <lib/crc/crc16_sw.c>
+#include <lib/crc/crc8_sw.c>
 
 void test_crc16(void)
 {
@@ -19,8 +20,24 @@ void test_crc16(void)
 	zassert(crc16_ccitt(test2, sizeof(test2)) == 0xe5cc, "pass", "fail");
 }
 
+void test_crc8_ccitt(void)
+{
+	u8_t test0[] = { 0 };
+	u8_t test1[] = { 'A' };
+	u8_t test2[] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+	zassert(crc8_ccitt(CRC8_CCITT_INITIAL_VALUE, test0,
+			   sizeof(test0)) == 0xF3, "pass", "fail");
+	zassert(crc8_ccitt(CRC8_CCITT_INITIAL_VALUE, test1,
+			   sizeof(test1)) == 0x33, "pass", "fail");
+	zassert(crc8_ccitt(CRC8_CCITT_INITIAL_VALUE, test2,
+			   sizeof(test2)) == 0xFB, "pass", "fail");
+}
+
 void test_main(void)
 {
-	ztest_test_suite(test_crc16, ztest_unit_test(test_crc16));
-	ztest_run_test_suite(test_crc16);
+	ztest_test_suite(test_crc,
+			 ztest_unit_test(test_crc16),
+			 ztest_unit_test(test_crc8_ccitt));
+	ztest_run_test_suite(test_crc);
 }
