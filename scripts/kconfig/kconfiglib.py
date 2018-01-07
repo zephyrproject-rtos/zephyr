e1b422951e44ef9f3807231d4c9a45e0a96c4742
Carles Cufi <carles.cufi@nordicsemi.no>
2018-01-07 15:57:05 +0100

    scripts: kconfig: Prefer later defaults
    This commit mirrors c6390d559f068dece53efcdadff64a40357857d7 in
    Kconfiglib instead of Kconfig.
    
    Origin: https://github.com/carlescufi/Kconfiglib/tree/zephyr
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- scripts/kconfig/kconfiglib.py
+++ scripts/kconfig/kconfiglib.py
@@ -2438,7 +2438,7 @@ class Symbol(object):
 
             else:
                 # No user value or invalid user value. Look at defaults.
-
+                found = False
                 for val_expr, cond in self.defaults:
                     if expr_value(cond):
                         self._write_to_conf = True
@@ -2449,9 +2449,9 @@ class Symbol(object):
                             val_num = int(val, base)
                         else:
                             val_num = 0  # strtoll() on empty string
-
-                        break
-                else:
+                        found = True
+                        #break
+                if not found:
                     val_num = 0  # strtoll() on empty string
 
                 # This clamping procedure runs even if there's no default
@@ -2479,7 +2479,7 @@ class Symbol(object):
                     if expr_value(cond):
                         self._write_to_conf = True
                         val = val_expr.str_value
-                        break
+                        #break
 
         # Corresponds to SYMBOL_AUTO in the C implementation
         if self.env_var is not None:
@@ -2522,7 +2522,7 @@ class Symbol(object):
                     if cond_val:
                         val = min(expr_value(default), cond_val)
                         self._write_to_conf = True
-                        break
+                        #break
 
                 # Weak reverse dependencies are only considered if our
                 # direct dependencies are met
