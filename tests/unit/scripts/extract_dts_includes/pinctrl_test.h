9384a4665e376222ef8dac5376332ee630b2e6e9
Bobby Noelte <b0661n0e17e@gmail.com>
2018-01-13 10:13:49 +0100

    test: scripts: extract_dts_includes.py pinctrl directive
    Test correct generation of defines for the pinctrl directive
    in device trees.
    
    Signed-off-by: Bobby Noelte <b0661n0e17e@gmail.com>

---
+++ tests/unit/scripts/extract_dts_includes/pinctrl_test.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2017 Bobby Noelte
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* Device tree defines for the TEST PINCTRL device driver.
+ */
+
+#ifndef _PINCTRL_TEST_H
+#define _PINCTRL_TEST_H
+
+#define PINCTRL_TEST_DEVICE0_NAME "PINCTRL_TEST_DEVICE0"
+#define PINCTRL_TEST_DEVICE1_NAME "PINCTRL_TEST_DEVICE1"
+
+#define GPIO_PORT_PIN0 0x00000001U
+
+#endif /* _PINCTRL_TEST_H */
