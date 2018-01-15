bdfaf7f9941785c5262a075b006a2444d9128839
Kamil Sroka <kamil.sroka@nordicsemi.no>
2018-01-15 11:38:21 +0100

    include: net: net_l2: Add OpenThread L2
    Add OpenThread L2 to L2 list.
    
    Signed-off-by: Kamil Sroka <kamil.sroka@nordicsemi.no>

---
+++ include/net/openthread.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __NET_OPENTHREAD_H_
+#define __NET_OPENTHREAD_H_
+
+#include <kernel.h>
+
+#include <net/net_if.h>
+
+#include <openthread/openthread.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct pkt_list_elem {
+	struct net_pkt *pkt;
+};
+
+struct openthread_context {
+	otInstance *instance;
+	struct net_if *iface;
+	u16_t pkt_list_in_idx;
+	u16_t pkt_list_out_idx;
+	u8_t pkt_list_full;
+	struct pkt_list_elem pkt_list[CONFIG_OPENTHREAD_PKT_LIST_SIZE];
+};
+
+#define OPENTHREAD_L2_CTX_TYPE struct openthread_context
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __NET_OPENTHREAD_H_ */
