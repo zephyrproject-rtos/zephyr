0de443600b1adb5552e07de6484f3a7480d93138
Anas Nashif <anas.nashif@intel.com>
2018-01-15 13:58:50 -0500

    samples: shell: add standalone version command
    Combine modules with standalone commands.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- samples/subsys/shell/shell_module/src/main.c
+++ samples/subsys/shell/shell_module/src/main.c
@@ -7,6 +7,7 @@
 #include <zephyr.h>
 #include <misc/printk.h>
 #include <shell/shell.h>
+#include <version.h>
 
 static int shell_cmd_ping(int argc, char *argv[])
 {
@@ -28,6 +29,19 @@ static int shell_cmd_params(int argc, char *argv[])
 	}
 	return 0;
 }
+
+#define SHELL_CMD_VERSION "version"
+static int shell_cmd_version(int argc, char *argv[])
+{
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	printk("Zephyr version %s\n", KERNEL_VERSION_STRING);
+	return 0;
+}
+
+SHELL_REGISTER_COMMAND(SHELL_CMD_VERSION, shell_cmd_version,
+		       "Show kernel version");
 
 #define MY_SHELL_MODULE "sample_module"
 
