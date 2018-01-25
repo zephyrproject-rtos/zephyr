d41fe91e654f50996db6978ba1ad108e1f302177
Maureen Helm <maureen.helm@nxp.com>
2018-01-25 14:09:36 -0600

    boards: arm: Configure jlink flash runner on nxp boards
    Now that the jlink runner supports the flash command, set
    BOARD_FLASH_RUNNER when the OpenSDA or LPCLink debug adapter is
    programmed with jlink firmware.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- boards/arm/lpcxpresso54114/board.cmake
+++ boards/arm/lpcxpresso54114/board.cmake
@@ -8,6 +8,7 @@ set_ifndef(LPCLINK_FW jlink)
 
 if(LPCLINK_FW STREQUAL jlink)
   set_ifndef(BOARD_DEBUG_RUNNER jlink)
+  set_ifndef(BOARD_FLASH_RUNNER jlink)
 endif()
 
 board_runner_args(jlink "--device=LPC54114J256_M4")
