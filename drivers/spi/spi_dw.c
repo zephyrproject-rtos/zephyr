8b87d3024c7b76cbce7c494ffb43781e8c65d1c3
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-01-29 22:12:50 +0100

    api/spi: Add a dedicated Kconfig option for asynchronous mode enablement
    Instead of using CONFIG_POLL, which is not directly related to SPI and
    is a kernel option, let's have SPI_ASYNC instead. When enabled, it will
    select POLL automatically.
    
    Fixes #5839
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- drivers/spi/spi_dw.c
+++ drivers/spi/spi_dw.c
@@ -321,7 +321,7 @@ static int spi_dw_transceive(struct device *dev,
 	return transceive(dev, config, tx_bufs, rx_bufs, false, NULL);
 }
 
-#ifdef CONFIG_POLL
+#ifdef CONFIG_SPI_ASYNC
 static int spi_dw_transceive_async(struct device *dev,
 				   const struct spi_config *config,
 				   const struct spi_buf_set *tx_bufs,
@@ -332,7 +332,7 @@ static int spi_dw_transceive_async(struct device *dev,
 
 	return transceive(dev, config, tx_bufs, rx_bufs, true, async);
 }
-#endif /* CONFIG_POLL */
+#endif /* CONFIG_SPI_ASYNC */
 
 static int spi_dw_release(struct device *dev, const struct spi_config *config)
 {
@@ -382,9 +382,9 @@ out:
 
 static const struct spi_driver_api dw_spi_api = {
 	.transceive = spi_dw_transceive,
-#ifdef CONFIG_POLL
+#ifdef CONFIG_SPI_ASYNC
 	.transceive_async = spi_dw_transceive_async,
-#endif
+#endif /* CONFIG_SPI_ASYNC */
 	.release = spi_dw_release,
 };
 
