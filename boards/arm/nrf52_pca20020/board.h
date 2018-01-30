439a63daacec35af8349928baf0bc94a5ca61a12
Aapo Vienamo <aapo.vienamo@iki.fi>
2018-01-30 23:03:57 +0200

    boards: Add support for nRF52 Thingy:52 (PCA20020)
    A new board configuration is added for Thingy:52, also known as
    PCA20020. This includes support for some of the on-board sensors, and
    other peripherals such as the on-board GPIO expander.
    
    The board power rail sequencing is also implemented, as the rails need
    to be powered up in a correct order and before certain device driver
    init calls are made.
    
    Signed-off-by: Aapo Vienamo <aapo.vienamo@iki.fi>

---
+++ boards/arm/nrf52_pca20020/board.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018 Aapo Vienamo
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+/* Push button switch 0 */
+#define SW0_GPIO_PIN 11
+#define SW0_GPIO_NAME CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+#define LIGHTWELL_PORT CONFIG_GPIO_SX1509B_DEV_NAME
+#define LIGHTWELL_R_PIN	7
+#define LIGHTWELL_G_PIN	5
+#define LIGHTWELL_B_PIN	6
+
+#define LED0_GPIO_PORT  LIGHTWELL_PORT
+#define LED0_GPIO_PIN	LIGHTWELL_G_PIN
+
+#endif /* __INC_BOARD_H */
