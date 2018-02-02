deeb74651bae376b837bb83c6c3181568e86785f
Endre Karlson <endre.karlson@gmail.com>
2018-02-02 17:09:27 +0100

    arm: stm32l0: Add support for stm32l0 soc
    Add necessary kconfig and minimal device tree support for
    the stm32l072xz variants.
    
    Signed-off-by: Endre Karlson <endre.karlson@gmail.com>

---
+++ arch/arm/soc/st_stm32/stm32l0/soc.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2018 Endre Karlson <endre.karlson@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief System/hardware module for STM32L0 processor
+ */
+
+#include <kernel.h>
+#include <device.h>
+#include <init.h>
+#include <soc.h>
+#include <arch/cpu.h>
+#include <cortex_m/exc.h>
+#include <linker/linker-defs.h>
+#include <string.h>
+
+
+/**
+ * @brief This function configures the source of stm32cube time base.
+ *        Cube HAL expects a 1ms tick which matches with k_uptime_get_32.
+ *        Tick interrupt priority is not used
+ * @return HAL status
+ */
+uint32_t HAL_GetTick(void)
+{
+	return k_uptime_get_32();
+}
+
+/**
+ * @brief Perform basic hardware initialization at boot.
+ *
+ * This needs to be run from the very beginning.
+ * So the init priority has to be 0 (zero).
+ *
+ * @return 0
+ */
+static int stm32l0_init(struct device *arg)
+{
+	u32_t key;
+
+	ARG_UNUSED(arg);
+
+	key = irq_lock();
+
+	_ClearFaults();
+
+	/* Install default handler that simply resets the CPU
+	 * if configured in the kernel, NOP otherwise
+	 */
+	NMI_INIT();
+
+	irq_unlock(key);
+
+	/* Update CMSIS SystemCoreClock variable (HCLK) */
+	/* At reset, system core clock is set to 2.1 MHz from MSI */
+	SystemCoreClock = 2097152;
+
+	return 0;
+}
+
+SYS_INIT(stm32l0_init, PRE_KERNEL_1, 0);
