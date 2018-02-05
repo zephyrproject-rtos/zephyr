c33517427164120bc82c2c5f1d467cdb2defc998
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2018-02-05 13:04:55 +0200

    sensors: Redefine SENSOR_CHAN_DISTANCE to be in meters
    Most of sensor channels defined by Zephyr use the main, unscaled
    SI unit. We also have SENSOR_CHAN_ALTITUDE which is defined in
    meters. So, it only makes sense to define distance in meters too
    
    The only driver supporting SENSOR_CHAN_DISTANCE as of now is
    vl53l0x.c, which was updated accordingly. Also, update doc links
    in the driver based on the review comment.
    
    Fixes: #5693
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- include/sensor.h
+++ include/sensor.h
@@ -119,7 +119,7 @@ enum sensor_channel {
 	SENSOR_CHAN_PM_2_5,
 	/** 10 micro-meters Particulate Matter, in ug/m^3 */
 	SENSOR_CHAN_PM_10,
-	/** Distance. From sensor to target, in millimeters */
+	/** Distance. From sensor to target, in meters */
 	SENSOR_CHAN_DISTANCE,
 	/** All channels. */
 	SENSOR_CHAN_ALL,
