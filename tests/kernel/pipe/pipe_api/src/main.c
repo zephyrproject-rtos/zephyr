b6672da26f595f2e350427eb77d760f805246239
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-02-05 16:41:57 +0530

    tests: pipe: pipe_api: Converted pipe test cases to run in usermode.
    Converted few test cases to run in usermode.
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>

--- tests/kernel/pipe/pipe_api/src/main.c
+++ tests/kernel/pipe/pipe_api/src/main.c
@@ -19,13 +19,22 @@ extern void test_pipe_block_put(void);
 extern void test_pipe_block_put_sema(void);
 extern void test_pipe_get_put(void);
 
+/* k objects */
+extern struct k_pipe pipe, kpipe, put_get_pipe;
+extern struct k_sem end_sema;
+extern struct k_stack tstack;
+extern struct k_thread tdata;
 /*test case main entry*/
 void test_main(void)
 {
+	k_thread_access_grant(k_current_get(),
+			      &kpipe, &pipe, &end_sema, &tdata, &tstack,
+			      &put_get_pipe, NULL);
+
 	ztest_test_suite(test_pipe_api,
-			 ztest_unit_test(test_pipe_thread2thread),
-			 ztest_unit_test(test_pipe_put_fail),
-			 ztest_unit_test(test_pipe_get_fail),
+			 ztest_user_unit_test(test_pipe_thread2thread),
+			 ztest_user_unit_test(test_pipe_put_fail),
+			 ztest_user_unit_test(test_pipe_get_fail),
 			 ztest_unit_test(test_pipe_block_put),
 			 ztest_unit_test(test_pipe_block_put_sema),
 			 ztest_unit_test(test_pipe_get_put));
--- tests/kernel/pipe/pipe_api/src/test_pipe_fail.c
+++ tests/kernel/pipe/pipe_api/src/test_pipe_fail.c
@@ -22,38 +22,40 @@
 
 static unsigned char __aligned(4) data[] = "abcd1234";
 
+__kernel struct k_pipe put_get_pipe;
+
 /*test cases*/
 void test_pipe_put_fail(void *p1, void *p2, void *p3)
 {
-	struct k_pipe pipe;
+
 	size_t wt_byte = 0;
 
-	k_pipe_init(&pipe, data, PIPE_LEN);
-	zassert_false(k_pipe_put(&pipe, data, PIPE_LEN, &wt_byte,
+	k_pipe_init(&put_get_pipe, data, PIPE_LEN);
+	zassert_false(k_pipe_put(&put_get_pipe, data, PIPE_LEN, &wt_byte,
 				 1, K_FOREVER), NULL);
 	/**TESTPOINT: pipe put returns -EIO*/
-	zassert_equal(k_pipe_put(&pipe, data, PIPE_LEN, &wt_byte,
+	zassert_equal(k_pipe_put(&put_get_pipe, data, PIPE_LEN, &wt_byte,
 				 1, K_NO_WAIT), -EIO, NULL);
 	zassert_false(wt_byte, NULL);
 	/**TESTPOINT: pipe put returns -EAGAIN*/
-	zassert_equal(k_pipe_put(&pipe, data, PIPE_LEN, &wt_byte,
+	zassert_equal(k_pipe_put(&put_get_pipe, data, PIPE_LEN, &wt_byte,
 				 1, TIMEOUT), -EAGAIN, NULL);
 	zassert_true(wt_byte < 1, NULL);
 }
 
 void test_pipe_get_fail(void *p1, void *p2, void *p3)
 {
-	struct k_pipe pipe;
+
 	unsigned char rx_data[PIPE_LEN];
 	size_t rd_byte = 0;
 
-	k_pipe_init(&pipe, data, PIPE_LEN);
+	k_pipe_init(&put_get_pipe, data, PIPE_LEN);
 	/**TESTPOINT: pipe put returns -EIO*/
-	zassert_equal(k_pipe_get(&pipe, rx_data, PIPE_LEN, &rd_byte, 1,
+	zassert_equal(k_pipe_get(&put_get_pipe, rx_data, PIPE_LEN, &rd_byte, 1,
 				 K_NO_WAIT), -EIO, NULL);
 	zassert_false(rd_byte, NULL);
 	/**TESTPOINT: pipe put returns -EAGAIN*/
-	zassert_equal(k_pipe_get(&pipe, rx_data, PIPE_LEN, &rd_byte, 1,
+	zassert_equal(k_pipe_get(&put_get_pipe, rx_data, PIPE_LEN, &rd_byte, 1,
 				 TIMEOUT), -EAGAIN, NULL);
 	zassert_true(rd_byte < 1, NULL);
 }
