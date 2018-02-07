4b18cabd11d9520e39819058dc0197eaae8d1102
Endre Karlson <endre.karlson@gmail.com>
2018-02-07 23:31:59 +0100

    boards: arm: Add Dragino LSN50 LoRA node
    Add dts, kconfig and other necessary code for LSN50 board from Dragino
    to work with Zephyr.
    
    Signed-off-by: Endre Karlson <endre.karlson@gmail.com>

---
+++ boards/arm/dragino_lsn50/board.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2018 Endre Karlson <endre.karlson@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+#endif /* __INC_BOARD_H */
