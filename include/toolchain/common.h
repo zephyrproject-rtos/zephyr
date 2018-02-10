ce743932678d8ddc0ec93fb379954a6e7d1f98fa
Anas Nashif <anas.nashif@intel.com>
2018-02-10 08:32:08 -0600

    toolchain: gcc/common: add header guard
    Added guards to the toolchain/gcc.h and toolchain/common.h
    
    Those files should never be included directly, but a guard is useful
    regardless.
    
    Fixes #5130
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- include/toolchain/common.h
+++ include/toolchain/common.h
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#ifndef TOOLCHAIN_COMMON_H
+#define TOOLCHAIN_COMMON_H
 /**
  * @file
  * @brief Common toolchain abstraction
@@ -131,3 +133,5 @@
 /* build assertion with message -- common implementation swallows message. */
 #define BUILD_ASSERT_MSG(EXPR, MSG) BUILD_ASSERT(EXPR)
 #endif
+
+#endif /* TOOLCHAIN_COMMON_H */
