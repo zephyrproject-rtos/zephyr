4565ca86dab63e6700afb425e183060e24b109ef
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-02-11 18:30:56 +0200

    samples: net: syslog: Remote syslog service example
    The sample application will send syslog messages to remote
    syslog server.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

---
+++ samples/net/syslog_net/src/main.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2018 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_MODULE_NAME net_syslog
+#define NET_LOG_LEVEL LOG_LEVEL_DBG
+
+#include <zephyr.h>
+
+#include <net/net_core.h>
+#include <net/net_pkt.h>
+
+#define SLEEP_BETWEEN_PRINTS K_SECONDS(3)
+
+void main(void)
+{
+	int count = K_SECONDS(60) / SLEEP_BETWEEN_PRINTS;
+
+	/* Allow some setup time before starting to send data */
+	k_sleep(SLEEP_BETWEEN_PRINTS);
+
+	NET_DBG("Starting");
+
+	do {
+		NET_ERR("Error message");
+		NET_WARN("Warning message");
+		NET_INFO("Info message");
+		NET_DBG("Debug message");
+
+		k_sleep(SLEEP_BETWEEN_PRINTS);
+
+	} while (count--);
+
+	NET_DBG("Stopping");
+}
