891e15a69b31aa6759cd5fc9e4a6dc780d5845ee
Alberto Escolar Piedras <alpi@oticon.com>
2018-02-11 16:30:49 +0100

    native: added more proper command line parameters parsing
    Added a basic command line parameter parsing framework
    
    Added the following options by now:
    --stop-at=<time>: Auto-stop after <time> seconds
    --seed=<seed>   : random seed for entropy device
    --testargs      : any argument that follows is ignored in top level
                      and made available thru
                      native_get_test_cmd_line_args()
    
    All command line parameters are still avaliable by calling
    native_get_cmd_line_args(), but now you can also call
    native_get_test_cmd_line_args() to get whatever was set after
    --testargs
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>

--- boards/posix/native_posix/cmdline.c
+++ boards/posix/native_posix/cmdline.c
@@ -4,27 +4,113 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdbool.h>
+#include "cmdline_common.h"
+#include "zephyr/types.h"
+#include "hw_models_top.h"
+#include "cmdline.h"
+#include "toolchain.h"
 
-static int s_argc;
-static char **s_argv;
+static int s_argc, test_argc;
+static char **s_argv, **test_argv;
+
+static struct args_t args;
+
+static void cmd_stop_at_found(char *argv, int offset)
+{
+	ARG_UNUSED(offset);
+	if (args.stop_at < 0) {
+		posix_print_error_and_exit("Error: stop-at must be positive "
+					   "(%s)\n", argv);
+	}
+	hwm_set_end_of_time(args.stop_at*1e6);
+}
+
+#if defined(CONFIG_ENTROPY_NATIVE_POSIX)
+extern void entropy_native_posix_set_seed(unsigned int seed_i);
+static void cmd_seed_found(char *argv, int offset)
+{
+	ARG_UNUSED(argv);
+	ARG_UNUSED(offset);
+	entropy_native_posix_set_seed(args.seed);
+}
+#endif
 
 /**
- * Store the command line arguments for later use.
- * If this board already handles any, we do so here
+ * Handle possible command line arguments.
+ *
+ * We also store them for later use by possible test applications
  */
 void native_handle_cmd_line(int argc, char *argv[])
 {
+	int i;
+
+	struct args_struct_t args_struct[] = {
+		/*
+		 * Fields:
+		 * manual, mandatory, switch,
+		 * option_name, var_name ,type,
+		 * destination, callback,
+		 * description
+		 */
+		{false, false, false,
+		  "stop_at", "time", 'd',
+		(void *)&args.stop_at, cmd_stop_at_found,
+		"In simulated seconds, when to stop automatically"},
+
+#if defined(CONFIG_ENTROPY_NATIVE_POSIX)
+		{false, false, false,
+		  "seed", "r_seed", 'u',
+		(void *)&args.seed, cmd_seed_found,
+		"Seed for the entropy device"},
+#endif
+
+		{true, false, false,
+		 "testargs", "arg", 'l',
+		(void *)NULL, NULL,
+		"Any argument that follows will be ignored by the top level, "
+		"and made available for possible tests"},
+
+		ARG_TABLE_ENDMARKER
+	};
+
 	s_argv = argv;
 	s_argc = argc;
+
+	cmd_args_set_defaults(args_struct);
+
+	for (i = 1; i < argc; i++) {
+
+		if ((cmd_is_option(argv[i], "testargs", 0))) {
+			test_argc = argc - i - 1;
+			test_argv = &argv[i+1];
+			break;
+		}
+
+		if (!cmd_parse_one_arg(argv[i], args_struct)) {
+			cmd_print_switches_help(args_struct);
+			posix_print_error_and_exit("Incorrect option '%s'\n",
+						   argv[i]);
+		}
+	}
 }
 
-
 /**
- * The application/test can use this function to inspect the command line
+ * The application/test can use this function to inspect all the command line
  * arguments
  */
 void native_get_cmd_line_args(int *argc, char ***argv)
 {
 	*argc = s_argc;
 	*argv = s_argv;
+}
+
+/**
+ * The application/test can use this function to inspect the command line
+ * arguments received after --testargs
+ */
+void native_get_test_cmd_line_args(int *argc, char ***argv)
+{
+	*argc = test_argc;
+	*argv = test_argv;
 }
