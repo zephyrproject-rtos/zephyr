c97d9c6c277f680779a4de918a1d3f991039604a
Loic Poulain <loic.poulain@linaro.org>
2018-02-12 15:34:23 +0100

    usb: netusb: consider media connected on interface selection event
    The interface and its enpoints are only enabled once interface is
    selected by the host.
    
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

--- subsys/usb/class/netusb/netusb.c
+++ subsys/usb/class/netusb/netusb.c
@@ -29,7 +29,7 @@
 
 static struct __netusb {
 	struct net_if *iface;
-	bool configured;
+	bool enabled;
 	struct netusb_function *func;
 } netusb;
 
@@ -39,8 +39,8 @@ static int netusb_send(struct net_if *iface, struct net_pkt *pkt)
 
 	SYS_LOG_DBG("Send pkt, len %u", net_pkt_get_len(pkt));
 
-	if (!netusb.configured) {
-		SYS_LOG_ERR("Unconfigured device, conf %u", netusb.configured);
+	if (!netusb.enabled) {
+		SYS_LOG_ERR("interface disabled");
 		return -ENODEV;
 	}
 
@@ -81,25 +81,22 @@ static int netusb_disconnect_media(void)
 	return netusb.func->connect_media(false);
 }
 
-static void netusb_status_configured(u8_t *conf)
+static void netusb_status_interface(u8_t *iface)
 {
-	SYS_LOG_INF("Enable configuration number %u", *conf);
+	SYS_LOG_INF("Enable interface %u", *iface);
 
-	switch (*conf) {
-	case 1:
-		netusb.configured = true;
+#if defined(CONFIG_USB_COMPOSITE_DEVICE)
+	if (*iface == NETUSB_IFACE_IDX) {
+#else
+	if (*iface == 1) {
+#endif
+		netusb.enabled = true;
 		net_if_up(netusb.iface);
 		netusb_connect_media();
-		break;
-	case 0:
-		netusb.configured = false;
+	} else {
+		netusb.enabled = false;
 		netusb_disconnect_media();
 		net_if_down(netusb.iface);
-		break;
-	default:
-		SYS_LOG_ERR("Wrong configuration chosen: %u", *conf);
-		netusb.configured = false;
-		break;
 	}
 }
 
@@ -118,7 +115,6 @@ static void netusb_status_cb(enum usb_dc_status_code status, u8_t *param)
 		break;
 	case USB_DC_CONFIGURED:
 		SYS_LOG_DBG("USB device configured");
-		netusb_status_configured(param);
 		break;
 	case USB_DC_DISCONNECTED:
 		SYS_LOG_DBG("USB device disconnected");
@@ -128,6 +124,10 @@ static void netusb_status_cb(enum usb_dc_status_code status, u8_t *param)
 		break;
 	case USB_DC_RESUME:
 		SYS_LOG_DBG("USB device resumed");
+		break;
+	case USB_DC_INTERFACE:
+		SYS_LOG_DBG("USB interface selected");
+		netusb_status_interface(param);
 		break;
 	case USB_DC_UNKNOWN:
 	default:
@@ -139,12 +139,12 @@ static void netusb_status_cb(enum usb_dc_status_code status, u8_t *param)
 static int netusb_class_handler(struct usb_setup_packet *setup,
 				s32_t *len, u8_t **data)
 {
-	SYS_LOG_DBG("len %d req_type 0x%x req 0x%x conf %u",
+	SYS_LOG_DBG("len %d req_type 0x%x req 0x%x enabled %u",
 		    *len, setup->bmRequestType, setup->bRequest,
-		    netusb.configured);
+		    netusb.enabled);
 
-	if (!netusb.configured) {
-		SYS_LOG_ERR("Unconfigured device, conf %u", netusb.configured);
+	if (!netusb.enabled) {
+		SYS_LOG_ERR("interface disabled");
 		return -ENODEV;
 	}
 
