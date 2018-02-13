955bf22d5887a69876ff75be7fcb33af35fb148c
vikrant8051 <5979717+vikrant8051@users.noreply.github.com>
2018-02-13 09:59:46 +0530

    Update main.c

--- samples/subsys/fs/nffs/src/main.c
+++ samples/subsys/fs/nffs/src/main.c
@@ -12,9 +12,7 @@
 #include <gpio.h>
 #include <board.h>
 
-int lock=0;
-
-struct device *button_device[1];
+struct device *button_device;
 
 static struct k_work button_work;
 
@@ -26,25 +24,30 @@ static void button_pressed(struct device *dev, struct gpio_callback *cb, uint32_
 void press(struct k_work *work)
 {
 	char buf[20];
-	int err=0;
+	int err;
 
 	fs_file_t fp;
+	
+	struct fs_dirent entry;
 
-	err = fs_open(&fp,"/0.txt");
-	if (err != 0) {
-		printk("error %d while opening for read\n\r", err);
-	}
+	if(fs_stat("/0.txt",&entry) == 0)
+	{
+		err = fs_open(&fp,"/0.txt");
+		if (err < 0) {
+			printk("error %d while opening for read\n\r", err);
+		}
 
-	err = fs_read(&fp, buf, sizeof(buf));
-	if (err < 0) {
-		printk("error %d while reading\n\r", err);
-	}
+		err = fs_read(&fp, buf, sizeof(buf));
+		if (err < 0) {
+			printk("error %d while reading\n\r", err);
+		}
 
-	printk("Data read from file is --> %s \n", buf);
+		printk("Data read from file is --> %s \n", buf);
 
-	err = fs_close(&fp);
-	if (err != 0) {
-		printk("error %d while closing after read\n\r", err);
+		err = fs_close(&fp);
+		if (err < 0) {
+			printk("error %d while closing after read\n\r", err);
+		}
 	}
 }
 
@@ -64,7 +67,7 @@ static int test(int idx)
 
 	/* Open and write */
 	err = fs_open(&fp, name);
-	if (err != 0) {
+	if (err < 0) {
 		printk("%s: error %d while opening for write\n", name, err);
 		return err;
 	}
@@ -88,62 +91,51 @@ static int test(int idx)
 		return err;
 	}
 
-	struct fs_dirent entry;
+	
+	err = fs_open(&fp, name);
+	if (err != 0) {
+		printk("%s: error %d while opening for read\n", name, err);
+		return err;
+	}
 
-	if(fs_stat(name,&entry)==0)
-	{
-		err = fs_open(&fp, name);
-		if (err != 0) {
-			printk("%s: error %d while opening for read\n", name, err);
-			return err;
-		}
+	err = fs_read(&fp, buf, sizeof(buf));
+	if (err < 0) {
+		printk("%s: error %d while reading\n", name, err);
+		return err;
+	}
 
-		err = fs_read(&fp, buf, sizeof(buf));
-		if (err < 0) {
-			printk("%s: error %d while reading\n", name, err);
-			return err;
-		}
+	if (err != sizeof(msg)) {
+		printk("%s: tried to read %d bytes, only got %d\n", name, sizeof(msg), err);
+		return err;
+	}
 
-		if (err != sizeof(msg)) {
-			printk("%s: tried to read %d bytes, only got %d\n", name, sizeof(msg), err);
-			return err;
-		}
-
-		printk("%s: wrote then read \"%s\"\n", name, buf);
-
-		/* Close the file */
-		err = fs_close(&fp);
-		if (err != 0) {
-			printk("%s: error %d while closing after read\n", name, err);
-			return err;
-		}
- 	}
-
-	lock=1;
-
+	/* Close the file */
+	err = fs_close(&fp);
+	if (err != 0) {
+		printk("%s: error %d while closing after read\n", name, err);
+		return err;
+	}
+	
 	return 0;
 }
 
 void main(void)
 {
 
-	static struct gpio_callback button_cb[1];
+	static struct gpio_callback button_cb;
 	
-	k_work_init(&button_work, press);
-
-	button_device[0] = device_get_binding(SW0_GPIO_NAME);
-	gpio_pin_configure(button_device[0], SW0_GPIO_PIN, (GPIO_DIR_IN | GPIO_INT | GPIO_INT_EDGE | GPIO_PUD_PULL_UP | GPIO_INT_DEBOUNCE | GPIO_INT_DOUBLE_EDGE));
-	gpio_init_callback(&button_cb[0], button_pressed, BIT(SW0_GPIO_PIN));
-	gpio_add_callback(button_device[0], &button_cb[0]);
-	gpio_pin_enable_callback(button_device[0], SW0_GPIO_PIN);
-
 	if (device_get_binding(CONFIG_FS_NFFS_FLASH_DEV_NAME) == NULL) 
 	{
 		printk("warning: flash device %s not found\n",	CONFIG_FS_NFFS_FLASH_DEV_NAME);
 	}
+	
+	k_work_init(&button_work, press);
 
-	if(lock==0)
-	{
-		test(0);	
-	}	
+	button_device = device_get_binding(SW0_GPIO_NAME);
+	gpio_pin_configure(button_device, SW0_GPIO_PIN, (GPIO_DIR_IN | GPIO_INT | GPIO_INT_EDGE | GPIO_PUD_PULL_UP | GPIO_INT_DEBOUNCE | GPIO_INT_DOUBLE_EDGE));
+	gpio_init_callback(&button_cb, button_pressed, BIT(SW0_GPIO_PIN));
+	gpio_add_callback(button_device, &button_cb);
+	gpio_pin_enable_callback(button_device, SW0_GPIO_PIN);
+
+	test(0);	
 }
