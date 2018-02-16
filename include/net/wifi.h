c2744d545d036410e083866117f950540aea3fd1
Gil Pitney <gil.pitney@linaro.org>
2018-02-15 17:58:33 -0800

    net: wifi: Add a wifi.h to hold WiFi definitions
    Placeholder to hold generic WiFi definitions.
    
    Signed-off-by: Gil Pitney <gil.pitney@linaro.org>

---
+++ include/net/wifi.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2018 Texas Instruments, Incorporated
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief General WiFi Definitions
+ */
+
+#ifndef __WIFI_H__
+#define __WIFI_H__
+
+#define WIFI_SSID_MAX_LEN 32
+#define WIFI_PSK_MAX_LEN 64
+
+#define WIFI_CHANNEL_MAX 14
+#define WIFI_CHANNEL_ANY 255
+
+#endif /* __WIFI_H__ */
+
