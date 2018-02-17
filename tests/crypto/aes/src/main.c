69829122eea30ec87df1418965ed3f53c82c16df
Anas Nashif <anas.nashif@intel.com>
2018-02-17 09:03:58 -0600

    tests: crypto: aes: use meaningful test names
    instead of using numbers, change name to what is actually being testing
    to improve reporting.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/crypto/aes/src/main.c
+++ tests/crypto/aes/src/main.c
@@ -5,18 +5,18 @@
  */
 
 #include <ztest.h>
-extern void test_1(void);
-extern void test_2(void);
-extern void test_3(void);
-extern void test_4(void);
+extern void test_key_chain(void);
+extern void test_vectors(void);
+extern void test_fixed_key_variable_text(void);
+extern void test_variable_key_fixed_text(void);
 
 /**test case main entry*/
 void test_main(void)
 {
 	ztest_test_suite(test_aes_fn,
-		ztest_unit_test(test_1),
-		ztest_unit_test(test_2),
-		ztest_unit_test(test_3),
-		ztest_unit_test(test_4));
+		ztest_unit_test(test_key_chain),
+		ztest_unit_test(test_vectors),
+		ztest_unit_test(test_fixed_key_variable_text),
+		ztest_unit_test(test_variable_key_fixed_text));
 	ztest_run_test_suite(test_aes_fn);
 }
