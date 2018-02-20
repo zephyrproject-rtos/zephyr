813b41beec749ad0cf70a7f57844f5d82c178a30
Carles Cufi <carles.cufi@nordicsemi.no>
2018-02-20 21:24:07 +0100

    drivers: entropy: Port the LL rand driver to entropy
    The rand driver present in the BLE Link Layer is able to provide
    entropy for both threads and ISRs, and so it is more suited to be used
    as a generic nRF5x entropy driver instead of the current one. This will
    allow applications and the Link Layer to use the same driver without
    duplicating it.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

---
+++ include/drivers/entropy/nrf5_entropy.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _NRF5_ENTROPY_H_
+#define _NRF5_ENTROPY_H_
+
+/**
+ * @brief Fills a buffer with entropy in a non-blocking manner.
+ * 	  Callable from ISRs.
+ *
+ * @param dev Pointer to the device structure.
+ * @param buf Buffer to fill with entropy.
+ * @param len Buffer length.
+ * @retval number of bytes filled with entropy.
+ */
+u8_t entropy_get_entropy_isr(struct device *dev, u8_t *buf, u8_t len);
+
+#endif /* _NRF5_ENTROPY_H_ */
