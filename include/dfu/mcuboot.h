f31245d3659c4c4e33be83516b38c47bc75c7bc6
Christopher Collins <ccollins@apache.org>
2018-02-21 12:45:26 -0800

    subsys: dfu: boot: Remove unused ver query code.
    Duplicate code to query was mistakenly added in commit
    2ad7ccdb2de8c1ccf3ba94a2eb05b3a679f0cad5.  This code is redundant; the
    existing `boot_read_bank_header()` function can read the version from
    both image banks.
    
    Signed-off-by: Christopher Collins <ccollins@apache.org>

--- include/dfu/mcuboot.h
+++ include/dfu/mcuboot.h
@@ -30,13 +30,6 @@
 
 #define BOOT_IMG_VER_STRLEN_MAX 25  /* 255.255.65535.4294967295\0 */
 
-struct image_version {
-	u8_t iv_major;
-	u8_t iv_minor;
-	u16_t iv_revision;
-	u32_t iv_build_num;
-};
-
 /**
  * @brief MCUboot image header representation for image version
  *
@@ -145,8 +138,6 @@ bool boot_is_img_confirmed(void);
  * @return 0 on success, negative errno code on fail.
  */
 int boot_write_img_confirmed(void);
-
-int boot_current_image_version(struct image_version *out_ver);
 
 /**
  * @brief Determines the action, if any, that mcuboot will take on the next
