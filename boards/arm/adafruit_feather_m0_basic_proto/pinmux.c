79d331fcc54a8dae71a4e43446e61e60f49326b2
Henrik Brix Andersen <henrik@brixandersen.dk>
2018-02-21 23:14:59 +0100

    boards: Add support for the Adafruit Feather M0 Basic Proto.
    The Feather M0 Basic Proto is an Atmel SAMD21 based development board
    with USB, UART, SPI, LED and onboard lithium polymer battery charger.
    
    Use of the Adafruit stock photo (adafruit_feather_m0_basic_proto.jpg)
    for the board documentation was approved by Adafruit Industries.
    
    Signed-off-by: Henrik Brix Andersen <henrik@brixandersen.dk>

---
+++ boards/arm/adafruit_feather_m0_basic_proto/pinmux.c
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2018 Henrik Brix Andersen <henrik@brixandersen.dk>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+#include <pinmux.h>
+
+static int board_pinmux_init(struct device *dev)
+{
+	struct device *muxa = device_get_binding(CONFIG_PINMUX_SAM0_A_LABEL);
+#if CONFIG_SPI_SAM0_SERCOM4_BASE_ADDRESS
+	struct device *muxb = device_get_binding(CONFIG_PINMUX_SAM0_B_LABEL);
+#endif
+	ARG_UNUSED(dev);
+
+#if CONFIG_UART_SAM0_SERCOM0_BASE_ADDRESS
+	/* SERCOM0 on RX=PA11/pad 3, TX=PA10/pad 2 */
+	pinmux_pin_set(muxa, 11, PINMUX_FUNC_C);
+	pinmux_pin_set(muxa, 10, PINMUX_FUNC_C);
+#endif
+
+#if CONFIG_UART_SAM0_SERCOM1_BASE_ADDRESS
+#error Pin mapping is not configured
+#endif
+#if CONFIG_UART_SAM0_SERCOM2_BASE_ADDRESS
+#error Pin mapping is not configured
+#endif
+#if CONFIG_UART_SAM0_SERCOM3_BASE_ADDRESS
+#error Pin mapping is not configured
+#endif
+#if CONFIG_UART_SAM0_SERCOM4_BASE_ADDRESS
+#error Pin mapping is not configured
+#endif
+#if CONFIG_UART_SAM0_SERCOM5_BASE_ADDRESS
+#error Pin mapping is not configured
+#endif
+
+#if CONFIG_SPI_SAM0_SERCOM4_BASE_ADDRESS
+	/* SPI SERCOM4 on MISO=PA12/pad 0, MOSI=PB10/pad 2, SCK=PB11/pad 3 */
+	pinmux_pin_set(muxa, 12, PINMUX_FUNC_D);
+	pinmux_pin_set(muxb, 10, PINMUX_FUNC_D);
+	pinmux_pin_set(muxb, 11, PINMUX_FUNC_D);
+#endif
+
+#if CONFIG_SPI_SAM0_SERCOM0_BASE_ADDRESS
+#error Pin mapping is not configured
+#endif
+#if CONFIG_SPI_SAM0_SERCOM1_BASE_ADDRESS
+#error Pin mapping is not configured
+#endif
+#if CONFIG_SPI_SAM0_SERCOM2_BASE_ADDRESS
+#error Pin mapping is not configured
+#endif
+#if CONFIG_SPI_SAM0_SERCOM3_BASE_ADDRESS
+#error Pin mapping is not configured
+#endif
+#if CONFIG_SPI_SAM0_SERCOM5_BASE_ADDRESS
+#error Pin mapping is not configured
+#endif
+
+	return 0;
+}
+
+SYS_INIT(board_pinmux_init, PRE_KERNEL_1, CONFIG_PINMUX_INIT_PRIORITY);
