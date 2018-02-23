e20abd55fa382ab3f511d184023186598061eb03
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-02-23 12:25:06 +0530

    tests: mbox: mbox_api: Added new test cases.
    Added new test cases to increase the impact of the test cases.
    The following are the brief description of the testcases:
    1. Incorrect receiver address.
    2. Incorrect transmit address.
    3. Mbox_get timeout occurs when not able to retrive a msg.
    4. Discard a msg by passing an invalid pool id.
    5. Msg id mismatch between the receiver and transmitter.
    6. Copy tx buffer to a new block in the pool.
    7. Copy a big tx buffer to a block in the pool but no block
       can fit it.
    8. Dispose a msg as soon as it is read.
    9. Free tx pool when we read the msg.
    10. A Asynchronous put unblocks a waiting get.
    11. A Asynchronous put sends a wrong address to a waiting get.
    12. Multiple Asynchronous put populates the msg_q and mbox_get
        retrives it.
    13. Multiple waiting mbox_get and each put unblocks it one at a time.
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>

--- tests/kernel/mbox/mbox_api/src/main.c
+++ tests/kernel/mbox/mbox_api/src/main.c
@@ -13,6 +13,19 @@ extern void test_mbox_async_put_get_buffer(void);
 extern void test_mbox_async_put_get_block(void);
 extern void test_mbox_target_source_thread_buffer(void);
 extern void test_mbox_target_source_thread_block(void);
+extern void test_mbox_incorrect_receiver_tid(void);
+extern void test_mbox_incorrect_transmit_tid(void);
+extern void test_mbox_timed_out_mbox_get(void);
+extern void test_mbox_block_get_invalid_pool(void);
+extern void test_mbox_msg_tid_mismatch(void);
+extern void test_mbox_block_get_buff_to_pool(void);
+extern void test_mbox_block_get_buff_to_smaller_pool(void);
+extern void test_mbox_dispose_size_0_msg(void);
+extern void test_mbox_clean_up_tx_pool(void);
+extern void test_mbox_async_put_to_waiting_get(void);
+extern void test_mbox_get_waiting_put_incorrect_tid(void);
+extern void test_mbox_async_multiple_put(void);
+extern void test_mbox_multiple_waiting_get(void);
 
 /*test case main entry*/
 void test_main(void)
@@ -25,6 +38,21 @@ void test_main(void)
 			 ztest_unit_test(test_mbox_async_put_get_buffer),
 			 ztest_unit_test(test_mbox_async_put_get_block),
 			 ztest_unit_test(test_mbox_target_source_thread_buffer),
-			 ztest_unit_test(test_mbox_target_source_thread_block));
+			 ztest_unit_test(test_mbox_target_source_thread_block),
+			 ztest_unit_test(test_mbox_incorrect_receiver_tid),
+			 ztest_unit_test(test_mbox_incorrect_transmit_tid),
+			 ztest_unit_test(test_mbox_timed_out_mbox_get),
+			 ztest_unit_test(test_mbox_block_get_invalid_pool),
+			 ztest_unit_test(test_mbox_msg_tid_mismatch),
+			 ztest_unit_test(test_mbox_block_get_buff_to_pool),
+			 ztest_unit_test(
+				test_mbox_block_get_buff_to_smaller_pool),
+			 ztest_unit_test(test_mbox_dispose_size_0_msg),
+			 ztest_unit_test(test_mbox_clean_up_tx_pool),
+			 ztest_unit_test(test_mbox_async_put_to_waiting_get),
+			 ztest_unit_test(
+				test_mbox_get_waiting_put_incorrect_tid),
+			 ztest_unit_test(test_mbox_async_multiple_put),
+			 ztest_unit_test(test_mbox_multiple_waiting_get));
 	ztest_run_test_suite(test_mbox_api);
 }
