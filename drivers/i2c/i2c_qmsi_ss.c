74d18355053af423d3a5d11e8b132a440813fccb
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-03-02 13:14:33 +0100

    drivers/i2c: Make QMSI SS driver IRQs set via CONFIG_ options
    Now that dts i2 qmsi ss nodes generate the right options, let's use
    them. Apply the relevant fixup on the targeted SoC.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- drivers/i2c/i2c_qmsi_ss.c
+++ drivers/i2c/i2c_qmsi_ss.c
@@ -148,19 +148,19 @@ static void i2c_qmsi_ss_config_irq_0(void)
 	sys_write32(mask, SCSS_REGISTER_BASE + I2C_SS_0_STOP_MASK);
 
 	/* Connect the IRQs to ISR */
-	IRQ_CONNECT(I2C_SS_0_ERR_VECTOR, 1, qm_ss_i2c_0_error_isr,
-		    DEVICE_GET(i2c_ss_0), 0);
-	IRQ_CONNECT(I2C_SS_0_RX_VECTOR, 1, qm_ss_i2c_0_rx_avail_isr,
-		    DEVICE_GET(i2c_ss_0), 0);
-	IRQ_CONNECT(I2C_SS_0_TX_VECTOR, 1, qm_ss_i2c_0_tx_req_isr,
-		    DEVICE_GET(i2c_ss_0), 0);
-	IRQ_CONNECT(I2C_SS_0_STOP_VECTOR, 1, qm_ss_i2c_0_stop_det_isr,
-		    DEVICE_GET(i2c_ss_0), 0);
+	IRQ_CONNECT(CONFIG_I2C_SS_0_ERR_IRQ, CONFIG_I2C_SS_0_ERR_IRQ_PRI,
+		    qm_ss_i2c_0_error_isr, DEVICE_GET(i2c_ss_0), 0);
+	IRQ_CONNECT(CONFIG_I2C_SS_0_RX_IRQ, CONFIG_I2C_SS_0_RX_IRQ_PRI,
+		    qm_ss_i2c_0_rx_avail_isr, DEVICE_GET(i2c_ss_0), 0);
+	IRQ_CONNECT(CONFIG_I2C_SS_0_TX_IRQ, CONFIG_I2C_SS_0_TX_IRQ_PRI,
+		    qm_ss_i2c_0_tx_req_isr, DEVICE_GET(i2c_ss_0), 0);
+	IRQ_CONNECT(CONFIG_I2C_SS_0_STOP_IRQ, CONFIG_I2C_SS_0_STOP_IRQ_PRI,
+		    qm_ss_i2c_0_stop_det_isr, DEVICE_GET(i2c_ss_0), 0);
 
-	irq_enable(I2C_SS_0_ERR_VECTOR);
-	irq_enable(I2C_SS_0_RX_VECTOR);
-	irq_enable(I2C_SS_0_TX_VECTOR);
-	irq_enable(I2C_SS_0_STOP_VECTOR);
+	irq_enable(CONFIG_I2C_SS_0_ERR_IRQ);
+	irq_enable(CONFIG_I2C_SS_0_RX_IRQ);
+	irq_enable(CONFIG_I2C_SS_0_TX_IRQ);
+	irq_enable(CONFIG_I2C_SS_0_STOP_IRQ);
 }
 #endif /* CONFIG_I2C_SS_0 */
 
@@ -205,19 +205,19 @@ static void i2c_qmsi_ss_config_irq_1(void)
 	sys_write32(mask, SCSS_REGISTER_BASE + I2C_SS_1_STOP_MASK);
 
 	/* Connect the IRQs to ISR */
-	IRQ_CONNECT(I2C_SS_1_ERR_VECTOR, 1, qm_ss_i2c_1_error_isr,
-		    DEVICE_GET(i2c_ss_1), 0);
-	IRQ_CONNECT(I2C_SS_1_RX_VECTOR, 1, qm_ss_i2c_1_rx_avail_isr,
-		    DEVICE_GET(i2c_ss_1), 0);
-	IRQ_CONNECT(I2C_SS_1_TX_VECTOR, 1, qm_ss_i2c_1_tx_req_isr,
-		    DEVICE_GET(i2c_ss_1), 0);
-	IRQ_CONNECT(I2C_SS_1_STOP_VECTOR, 1, qm_ss_i2c_1_stop_det_isr,
-		    DEVICE_GET(i2c_ss_1), 0);
+	IRQ_CONNECT(CONFIG_I2C_SS_1_ERR_IRQ, CONFIG_I2C_SS_1_ERR_IRQ_PRI,
+		    qm_ss_i2c_1_error_isr, DEVICE_GET(i2c_ss_1), 0);
+	IRQ_CONNECT(CONFIG_I2C_SS_1_RX_IRQ, CONFIG_I2C_SS_1_RX_IRQ_PRI,
+		    qm_ss_i2c_1_rx_avail_isr, DEVICE_GET(i2c_ss_1), 0);
+	IRQ_CONNECT(CONFIG_I2C_SS_1_TX_IRQ, CONFIG_I2C_SS_1_TX_IRQ_PRI,
+		    qm_ss_i2c_1_tx_req_isr, DEVICE_GET(i2c_ss_1), 0);
+	IRQ_CONNECT(CONFIG_I2C_SS_1_STOP_IRQ, CONFIG_I2C_SS_1_STOP_IRQ_PRI,
+		    qm_ss_i2c_1_stop_det_isr, DEVICE_GET(i2c_ss_1), 0);
 
-	irq_enable(I2C_SS_1_ERR_VECTOR);
-	irq_enable(I2C_SS_1_RX_VECTOR);
-	irq_enable(I2C_SS_1_TX_VECTOR);
-	irq_enable(I2C_SS_1_STOP_VECTOR);
+	irq_enable(CONFIG_I2C_SS_1_ERR_IRQ);
+	irq_enable(CONFIG_I2C_SS_1_RX_IRQ);
+	irq_enable(CONFIG_I2C_SS_1_TX_IRQ);
+	irq_enable(CONFIG_I2C_SS_1_STOP_IRQ);
 }
 #endif /* CONFIG_I2C_SS_1 */
 
