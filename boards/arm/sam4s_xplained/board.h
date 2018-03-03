1f1fa32c6b7f9df5a2da47b8d669ce70da0577d6
Justin Watson <jwatson5@gmail.com>
2018-03-02 16:14:16 -0800

    drivers: GPIO Added GPIO for SAM family.
    Added GPIO driver for SAM family of SoCs.
    
    Signed-off-by: Justin Watson <jwatson5@gmail.com>

--- boards/arm/sam4s_xplained/board.h
+++ boards/arm/sam4s_xplained/board.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Justin Watson 2017
+ * Copyright (c) Justin Watson 2018
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -7,5 +7,23 @@
 #define __INC_BOARD_H
 
 #include <soc.h>
+
+#define BOARD_YELLOW_LED1_GPIO_PORT  CONFIG_GPIO_SAM_PORTC_LABEL
+#define BOARD_YELLOW_LED1_GPIO_PIN   10
+
+#define BOARD_YELLOW_LED2_GPIO_PORT  CONFIG_GPIO_SAM_PORTC_LABEL
+#define BOARD_YELLOW_LED2_GPIO_PIN   17
+
+#define BOARD_USER_PB2_GPIO_PORT     CONFIG_GPIO_SAM_PORTA_LABEL
+#define BOARD_USER_PB2_GPIO_PIN      5
+
+/* Aliases to make the basic samples work. */
+#define LED0_GPIO_PORT		BOARD_YELLOW_LED1_GPIO_PORT
+#define LED0_GPIO_PIN		BOARD_YELLOW_LED1_GPIO_PIN
+#define LED1_GPIO_PORT		BOARD_YELLOW_LED2_GPIO_PORT
+#define LED1_GPIO_PIN		BOARD_YELLOW_LED2_GPIO_PIN
+#define SW0_GPIO_NAME		BOARD_USER_PB2_GPIO_PORT
+#define SW0_GPIO_PIN		BOARD_USER_PB2_GPIO_PIN
+#define SW0_GPIO_PIN_PUD	GPIO_PUD_PULL_UP
 
 #endif /* __INC_BOARD_H */
