93e049a67ab0aa1cb71164bde0eb2c06797b7721
Yannis Damigos <giannis.damigos@gmail.com>
2018-03-03 16:56:52 +0200

    boards/arm/stm32f3_disco: Enable LSM303DLHC MEMS sensors
    Enable LSM303DLHC, ST MEMS system-in-package featuring
    a 3D digital linear acceleration sensor and a 3D digital
    magnetic sensor, on stm32f3_disco board.
    
    Signed-off-by: Yannis Damigos <giannis.damigos@gmail.com>

---
+++ boards/arm/stm32f3_disco/dts_fixup.h
@@ -0,0 +1,13 @@
+/* This file is a temporary workaround for mapping of the generated information
+ * to the current driver definitions.  This will be removed when the drivers
+ * are modified to handle the generated information, or the mapping of
+ * generated data matches the driver definitions.
+ */
+
+#define CONFIG_LSM303DLHC_ACCEL_NAME			ST_STM32_I2C_V2_40005400_ST_LSM303DLHC_ACCEL_19_LABEL
+#define CONFIG_LSM303DLHC_ACCEL_I2C_ADDR		ST_STM32_I2C_V2_40005400_ST_LSM303DLHC_ACCEL_19_BASE_ADDRESS
+#define CONFIG_LSM303DLHC_ACCEL_I2C_MASTER_DEV		ST_STM32_I2C_V2_40005400_ST_LSM303DLHC_ACCEL_19_BUS_NAME
+
+#define CONFIG_LSM303DLHC_MAGN_NAME			ST_STM32_I2C_V2_40005400_ST_LSM303DLHC_MAGN_1E_LABEL
+#define CONFIG_LSM303DLHC_MAGN_I2C_ADDR			ST_STM32_I2C_V2_40005400_ST_LSM303DLHC_MAGN_1E_BASE_ADDRESS
+#define CONFIG_LSM303DLHC_MAGN_I2C_MASTER_DEV		ST_STM32_I2C_V2_40005400_ST_LSM303DLHC_MAGN_1E_BUS_NAME
