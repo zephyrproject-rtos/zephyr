61ef30d10eca817ca76cb388a1bc5e98ad4a30e0
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-03-05 14:38:50 +0100

    drivers/uart: Use dts to set uart options for ns16550 driver
    Fix the ns16550 uart driver and relevant SoCs accordingly.
    All generic settings are now DTS based.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- drivers/serial/uart_ns16550.c
+++ drivers/serial/uart_ns16550.c
@@ -20,6 +20,7 @@
  *  UART_REG_ADDR_INTERVAL
  */
 
+#include <soc.h>
 #include <errno.h>
 
 #include <kernel.h>
@@ -749,7 +750,7 @@ static void irq_config_func_0(struct device *port);
 #endif
 
 static const struct uart_ns16550_device_config uart_ns16550_dev_cfg_0 = {
-	.sys_clk_freq = UART_NS16550_PORT_0_CLK_FREQ,
+	.sys_clk_freq = CONFIG_UART_NS16550_PORT_0_CLK_FREQ,
 
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
 	.irq_config_func = irq_config_func_0,
@@ -767,7 +768,7 @@ static struct uart_ns16550_dev_data_t uart_ns16550_dev_data_0 = {
 	.pci_dev.bar = UART_NS16550_PORT_0_PCI_BAR,
 #endif /* CONFIG_UART_NS16550_PORT_0_PCI */
 
-	.port = UART_NS16550_PORT_0_BASE_ADDR,
+	.port = CONFIG_UART_NS16550_PORT_0_BASE_ADDR,
 	.baud_rate = CONFIG_UART_NS16550_PORT_0_BAUD_RATE,
 	.options = CONFIG_UART_NS16550_PORT_0_OPTIONS,
 
@@ -786,11 +787,11 @@ static void irq_config_func_0(struct device *dev)
 {
 	ARG_UNUSED(dev);
 
-	IRQ_CONNECT(UART_NS16550_PORT_0_IRQ,
+	IRQ_CONNECT(CONFIG_UART_NS16550_PORT_0_IRQ,
 		    CONFIG_UART_NS16550_PORT_0_IRQ_PRI,
 		    uart_ns16550_isr, DEVICE_GET(uart_ns16550_0),
-		    UART_IRQ_FLAGS);
-	irq_enable(UART_NS16550_PORT_0_IRQ);
+		    CONFIG_UART_NS16550_PORT_0_IRQ_FLAGS);
+	irq_enable(CONFIG_UART_NS16550_PORT_0_IRQ);
 }
 #endif
 
@@ -803,7 +804,7 @@ static void irq_config_func_1(struct device *port);
 #endif
 
 static const struct uart_ns16550_device_config uart_ns16550_dev_cfg_1 = {
-	.sys_clk_freq = UART_NS16550_PORT_1_CLK_FREQ,
+	.sys_clk_freq = CONFIG_UART_NS16550_PORT_1_CLK_FREQ,
 
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
 	.irq_config_func = irq_config_func_1,
@@ -821,7 +822,7 @@ static struct uart_ns16550_dev_data_t uart_ns16550_dev_data_1 = {
 	.pci_dev.bar = UART_NS16550_PORT_1_PCI_BAR,
 #endif /* CONFIG_UART_NS16550_PORT_1_PCI */
 
-	.port = UART_NS16550_PORT_1_BASE_ADDR,
+	.port = CONFIG_UART_NS16550_PORT_1_BASE_ADDR,
 	.baud_rate = CONFIG_UART_NS16550_PORT_1_BAUD_RATE,
 	.options = CONFIG_UART_NS16550_PORT_1_OPTIONS,
 
@@ -840,11 +841,11 @@ static void irq_config_func_1(struct device *dev)
 {
 	ARG_UNUSED(dev);
 
-	IRQ_CONNECT(UART_NS16550_PORT_1_IRQ,
+	IRQ_CONNECT(CONFIG_UART_NS16550_PORT_1_IRQ,
 		    CONFIG_UART_NS16550_PORT_1_IRQ_PRI,
 		    uart_ns16550_isr, DEVICE_GET(uart_ns16550_1),
-		    UART_IRQ_FLAGS);
-	irq_enable(UART_NS16550_PORT_1_IRQ);
+		    CONFIG_UART_NS16550_PORT_1_IRQ_FLAGS);
+	irq_enable(CONFIG_UART_NS16550_PORT_1_IRQ);
 }
 #endif
 
