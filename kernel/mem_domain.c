zephyrproject-rtos/zephyr

commit 08de658eb90031339c16fe97588802bea935af55

kernel: mem_domain: Use u8_t for number of partitions in struct
During system initialization, the global static variable (to
mem_domain.c) is initialized with the number of maximum partitions per
domain.  This variable is of u8_t type.

Assertions throughout the code will check ranges and test for overflow
by relying on implicit type conversion.

Use an u8_t instead of u32_t to avoid doubts.  Also, reorder the
k_mem_partition struct to remove the alignment hole created by reducing
sizeof(num_partitions).

Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>

kernel/mem_domain.c
@@ -68,8 +68,8 @@ static inline bool sane_partition_domain(const struct k_mem_domain *domain,
 #define sane_partition_domain(...) (true)
 #endif
 
-void k_mem_domain_init(struct k_mem_domain *domain, u32_t num_parts,
-		struct k_mem_partition *parts[])
+void k_mem_domain_init(struct k_mem_domain *domain, u8_t num_parts,
+		       struct k_mem_partition *parts[])
 {
 	unsigned int key;
 

include/kernel.h
@@ -4260,14 +4260,14 @@ struct k_mem_partition {
 
 /* memory domian */
 struct k_mem_domain {
-	/* number of partitions in the domain */
-	u32_t num_partitions;
 #ifdef CONFIG_USERSPACE
 	/* partitions in the domain */
 	struct k_mem_partition partitions[CONFIG_MAX_DOMAIN_PARTITIONS];
 #endif	/* CONFIG_USERSPACE */
 	/* domain q */
 	sys_dlist_t mem_domain_q;
+	/* number of partitions in the domain */
+	u8_t num_partitions;
 };
 
 
@@ -4282,7 +4282,7 @@ struct k_mem_domain {
  *              if num_parts is zero.
  */
 
-extern void k_mem_domain_init(struct k_mem_domain *domain, u32_t num_parts,
+extern void k_mem_domain_init(struct k_mem_domain *domain, u8_t num_parts,
 			      struct k_mem_partition *parts[]);
 /**
  * @brief Destroy a memory domain.

commit 7b6f371e2ae861fbf774388494dbd3c31a67f7c1

kernel: mem_domain: Fix compile issues
Commit 08de658eb ("kernel: mem_domain: Check for overlapping regions
when considering W^X") introduced some compile issues on various
platforms.

The k_mem_partition_attr_t member is attr not attrs.  Also, fix an issue
where sane_partition_domain neesd a pointer to a parition.

Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

kernel/mem_domain.c
@@ -22,8 +22,8 @@ static bool sane_partition(const struct k_mem_partition *part,
 	u32_t i;
 
 	end = part->start + part->size;
-	exec = K_MEM_PARTITION_IS_EXECUTABLE(part->attrs);
-	write = K_MEM_PARTITION_IS_WRITABLE(part->attrs);
+	exec = K_MEM_PARTITION_IS_EXECUTABLE(part->attr);
+	write = K_MEM_PARTITION_IS_WRITABLE(part->attr);
 
 	if (exec && write) {
 		__ASSERT(0, "partition is writable and executable <start %x>",
@@ -41,8 +41,8 @@ static bool sane_partition(const struct k_mem_partition *part,
 			continue;
 		}
 
-		cur_write = K_MEM_PARTITION_IS_WRITABLE(parts[i].attrs);
-		cur_exec = K_MEM_PARTITION_IS_EXECUTABLE(parts[i].attrs);
+		cur_write = K_MEM_PARTITION_IS_WRITABLE(parts[i].attr);
+		cur_exec = K_MEM_PARTITION_IS_EXECUTABLE(parts[i].attr);
 
 		if ((cur_write && exec) || (cur_exec && write)) {
 			__ASSERT(0, "overlapping partitions are "
@@ -96,7 +96,7 @@ void k_mem_domain_init(struct k_mem_domain *domain, u8_t num_parts,
 #if defined(CONFIG_EXECUTE_XOR_WRITE)
 		for (i = 0; i < num_parts; i++) {
 			__ASSERT(sane_partition_domain(domain,
-						       domain->partitions[i]),
+						       &domain->partitions[i]),
 				 "");
 		}
 #endif