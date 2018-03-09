f02300973b89de9350dd38c58aed88e03f526a2e
Kumar Gala <kumar.gala@linaro.org>
2018-03-09 17:26:33 -0600

    arm: mps_an521: Add support for the AN521 SoC
    The AN521 is a Soft Macro Model implementation of the SSE-200 subsystem
    with SIE-200 and CMSDK components targeting the MPS2+ board.  The
    SSE-200 subsystem implements two Cortex-M33 cores.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- arch/arm/soc/arm/mps2/soc.h
+++ arch/arm/soc/arm/mps2/soc.h
@@ -7,6 +7,12 @@
 #ifndef _SOC_H_
 #define _SOC_H_
 
+#if defined(CONFIG_SOC_MPS2_AN521)
+#define __SAUREGION_PRESENT       1U        /* SAU regions present */
+#define __FPU_PRESENT             CONFIG_CPU_HAS_FPU
+#define __DSP_PRESENT             1U        /* no DSP extension present */
+#endif
+
 #include <soc_devices.h>
 
 #endif /* _SOC_H_ */
