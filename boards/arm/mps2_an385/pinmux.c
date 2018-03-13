570be1b6251b2629d364f20a37b58654d8cec366
Kumar Gala <kumar.gala@linaro.org>
2018-03-13 12:00:21 -0500

    boards: mps2_an385: cleanup pinmux comments
    Several of the pinmux comments where wrong.  Update them to match the
    docs.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- boards/arm/mps2_an385/pinmux.c
+++ boards/arm/mps2_an385/pinmux.c
@@ -125,32 +125,32 @@ static void arm_mps2_pinmux_defaults(void)
 
 	gpio_0 = (1<<0)   /* Shield 0 UART 3 RXD */
 	       | (1<<4)   /* Shield 0 UART 3 TXD */
-	       | (1<<5)   /* Shield 0 I2C 3 SDL SBCON2 */
-	       | (1<<15)  /* Shield 0 I2C 3 SDA SBCON2 */
-	       | (1<<11)  /* Shield 0 SPI 3 nCS */
-	       | (1<<12)  /* Shield 0 SPI 3 MOSI */
-	       | (1<<13)  /* Shield 0 SPI 3 MISO */
-	       | (1<<14); /* Shield 0 SPI 3 SCK */
+	       | (1<<5)   /* Shield 0 I2C SCL SBCON2 */
+	       | (1<<15)  /* Shield 0 I2C SDA SBCON2 */
+	       | (1<<11)  /* Shield 0 SPI 3 SCK */
+	       | (1<<12)  /* Shield 0 SPI 3 SS */
+	       | (1<<13)  /* Shield 0 SPI 3 MOSI */
+	       | (1<<14); /* Shield 0 SPI 3 MISO */
 
 	CMSDK_AHB_GPIO0_DEV->altfuncset = gpio_0;
 
 	gpio_1 = (1<<10) /* Shield 1 UART 4 RXD */
 	       | (1<<14) /* Shield 1 UART 4 TXD */
-	       | (1<<15) /* Shield 1 I2C 4 SCL */
-	       | (1<<0)  /* ADC SPI_1 nCS */
-	       | (1<<1)  /* ADC SPI_1 MOSI */
-	       | (1<<2)  /* ADC SPI_1 MISO */
-	       | (1<<3)  /* ADC SPI_1 SCK */
+	       | (1<<15) /* Shield 1 I2C SCL SBCON3 */
+	       | (1<<0)  /* ADC SPI 2 SS */
+	       | (1<<1)  /* ADC SPI 2 MISO */
+	       | (1<<2)  /* ADC SPI 2 MOSI */
+	       | (1<<3)  /* ADC SPI 2 SCK */
 	       | (1<<5)  /* USER BUTTON 0 */
 	       | (1<<6); /* USER BUTTON 1 */
 
 	CMSDK_AHB_GPIO1_DEV->altfuncset = gpio_1;
 
-	gpio_2 = (1<<9)   /* Shield 1 I2C 4 SDA */
-	       | (1<<6)   /* Shield 1 I2C 4 SDA */
-	       | (1<<7)   /* Shield 1 I2C 4 SDA */
-	       | (1<<8)   /* Shield 1 I2C 4 SDA */
-	       | (1<<12); /* Shield 1 I2C 4 SDA */
+	gpio_2 = (1<<9)   /* Shield 1 I2C SDA SBCON3 */
+	       | (1<<6)   /* Shield 1 SPI 4 SS */
+	       | (1<<7)   /* Shield 1 SPI 4 MOSI */
+	       | (1<<8)   /* Shield 1 SPI 4 MISO */
+	       | (1<<12); /* Shield 1 SPI 4 SCK */
 
 	CMSDK_AHB_GPIO2_DEV->altfuncset = gpio_2;
 }
