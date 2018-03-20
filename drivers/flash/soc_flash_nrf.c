zephyrproject-rtos/zephyr

commit f49150cab686ae327043d5a8e3e904284aa4f341

arch: arm: nrf: Rename nrf5 SoC Family to nrf
Upcoming Nordic ICs that share many of the peripherals and architecture
with the currently supported nRF5x ones are no longer part of the nRF5
family. In order to accomodate that, rename the SoC family from nrf5 to
nrf, so that it can contain all of the members of the wider Nordic
family.

Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

drivers/flash/Kconfig
@@ -140,7 +140,7 @@ config SOC_FLASH_QMSI_API_REENTRANCY
 
 config SOC_FLASH_NRF5
 	bool "Nordic Semiconductor nRF5X flash driver"
-	depends on FLASH && SOC_FAMILY_NRF5
+	depends on FLASH && SOC_FAMILY_NRF
 	default n
 	select FLASH_HAS_PAGE_LAYOUT
 	select FLASH_HAS_DRIVER_ENABLED

subsys/bluetooth/controller/ll_sw/ll.c
@@ -25,9 +25,9 @@
 #include "hal/radio.h"
 #include "hal/debug.h"
 
-#if defined(CONFIG_SOC_FAMILY_NRF5)
+#if defined(CONFIG_SOC_FAMILY_NRF)
 #include "hal/nrf5/ticker.h"
-#endif /* CONFIG_SOC_FAMILY_NRF5 */
+#endif /* CONFIG_SOC_FAMILY_NRF */
 
 #include "util/util.h"
 #include "util/mem.h"

arch/arm/core/cortex_m/mpu/Kconfig
@@ -24,7 +24,7 @@ config MPU_STACK_GUARD
 config ARM_MPU
 	bool "ARM MPU Support"
 	depends on CPU_HAS_MPU
-	depends on SOC_FAMILY_ARM || SOC_FAMILY_STM32 || SOC_FAMILY_NRF5 || SOC_FAMILY_IMX
+	depends on SOC_FAMILY_ARM || SOC_FAMILY_STM32 || SOC_FAMILY_NRF || SOC_FAMILY_IMX
 	select ARM_CORE_MPU
 	select ARCH_HAS_EXECUTABLE_PAGE_BIT
 	select MPU_REQUIRES_POWER_OF_TWO_ALIGNMENT

arch/arm/soc/nordic_nrf/Kconfig
@@ -1,0 +1,19 @@
+# Kconfig - Nordic Semiconductor nRFx MCU line
+#
+# Copyright (c) 2018 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+config SOC_FAMILY_NRF
+	bool
+	# omit prompt to signify a "hidden" option
+	default n
+
+if SOC_FAMILY_NRF
+config SOC_FAMILY
+	string
+	default nordic_nrf
+endif
+
+source "arch/arm/soc/nordic_nrf/*/Kconfig.soc"

arch/arm/soc/nordic_nrf5/Kconfig
@@ -1,19 +1,0 @@
-# Kconfig - Nordic Semiconductor nRF5 MCU line
-#
-# Copyright (c) 2016 Nordic Semiconductor ASA
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-config SOC_FAMILY_NRF5
-	bool
-	# omit prompt to signify a "hidden" option
-	default n
-
-if SOC_FAMILY_NRF5
-config SOC_FAMILY
-	string
-	default nordic_nrf5
-endif
-
-source "arch/arm/soc/nordic_nrf5/*/Kconfig.soc"

commit d1f19e90070ad208c766efdcda7fa4e74c6acd42

drivers: flash: nrf: Rename nrf5 to nrf
With upcoming ICs that are not in the nRF5x family, rename the flash
driver and all its dependencies from nrf5 to nrf.

Should also fix the issue introduced by f49150cab686ae3 which broke the
assignment of the flash device due to a partial rename.

Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

drivers/flash/CMakeLists.txt
@@ -1,6 +1,6 @@
 zephyr_sources_ifdef(CONFIG_SPI_FLASH_W25QXXDV spi_flash_w25qxxdv.c)
 zephyr_sources_ifdef(CONFIG_SOC_FLASH_QMSI soc_flash_qmsi.c)
-zephyr_sources_ifdef(CONFIG_SOC_FLASH_NRF5 soc_flash_nrf5.c)
+zephyr_sources_ifdef(CONFIG_SOC_FLASH_NRF soc_flash_nrf.c)
 zephyr_sources_ifdef(CONFIG_SOC_FLASH_MCUX soc_flash_mcux.c)
 zephyr_sources_ifdef(CONFIG_FLASH_PAGE_LAYOUT flash_page_layout.c)
 zephyr_sources_ifdef(CONFIG_USERSPACE flash_handlers.c)
@@ -33,4 +33,4 @@ zephyr_sources_ifdef(CONFIG_SOC_FLASH_STM32
 	)
 endif()
 
-zephyr_include_directories_ifdef(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC ${ZEPHYR_BASE}/subsys/bluetooth)
+zephyr_include_directories_ifdef(CONFIG_SOC_FLASH_NRF_RADIO_SYNC ${ZEPHYR_BASE}/subsys/bluetooth)

drivers/flash/Kconfig
@@ -138,18 +138,18 @@ config SOC_FLASH_QMSI_API_REENTRANCY
 	help
 	 Enable support for QMSI flash driver API reentrancy.
 
-config SOC_FLASH_NRF5
-	bool "Nordic Semiconductor nRF5X flash driver"
+config SOC_FLASH_NRF
+	bool "Nordic Semiconductor nRF flash driver"
 	depends on FLASH && SOC_FAMILY_NRF
 	default n
 	select FLASH_HAS_PAGE_LAYOUT
 	select FLASH_HAS_DRIVER_ENABLED
 	help
-	  Enables Nordic Semiconductor nRF5X flash driver.
+	  Enables Nordic Semiconductor nRF flash driver.
 
-config SOC_FLASH_NRF5_RADIO_SYNC
-	bool "Nordic nRF5x flash driver synchronized with radio"
-	depends on SOC_FLASH_NRF5 && BT_CTLR
+config SOC_FLASH_NRF_RADIO_SYNC
+	bool "Nordic nRFx flash driver synchronized with radio"
+	depends on SOC_FLASH_NRF && BT_CTLR
 	default y
 	help
 	  Enable synchronization between flash memory driver and radio.

drivers/flash/soc_flash_nrf.c
@@ -15,7 +15,7 @@
 #include <flash.h>
 #include <string.h>
 
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 #include <misc/__assert.h>
 #include <bluetooth/hci.h>
 #include "controller/hal/nrf5/ticker.h"
@@ -27,7 +27,7 @@
 #define FLASH_RADIO_ABORT_DELAY_US 500
 #define FLASH_TIMEOUT_MS ((FLASH_PAGE_ERASE_MAX_TIME_US)\
 			* (FLASH_PAGE_MAX_CNT) / 1000)
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 
 #define FLASH_OP_DONE    (0) /* 0 for compliance with the driver API. */
 #define FLASH_OP_ONGOING (-1)
@@ -35,21 +35,21 @@
 struct erase_context {
 	u32_t addr; /* Address off the 1st page to erase */
 	u32_t size; /* Size off area to erase [B] */
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 	u8_t enable_time_limit; /* execution limited to timeslot */
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 }; /*< Context type for f. @ref erase_op */
 
 struct write_context {
 	u32_t data_addr;
 	u32_t flash_addr; /* Address off the 1st page to erase */
 	u32_t len;        /* Size off data to write [B] */
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 	u8_t  enable_time_limit; /* execution limited to timeslot*/
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 }; /*< Context type for f. @ref write_op */
 
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 typedef int (*flash_op_handler_t) (void *context);
 
 struct flash_op_desc {
@@ -66,7 +66,7 @@ static int write_in_timeslice(off_t addr, const void *data, size_t len);
 
 static int erase_op(void *context); /* instance of flash_op_handler_t */
 static int erase_in_timeslice(u32_t addr, u32_t size);
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 
 /* semaphore for locking flash resources (tickers) */
 static struct k_sem sem_lock;
@@ -96,7 +96,7 @@ static void nvmc_wait_ready(void)
 	}
 }
 
-static int flash_nrf5_read(struct device *dev, off_t addr,
+static int flash_nrf_read(struct device *dev, off_t addr,
 			    void *data, size_t len)
 {
 	if (!is_addr_valid(addr, len)) {
@@ -112,7 +112,7 @@ static int flash_nrf5_read(struct device *dev, off_t addr,
 	return 0;
 }
 
-static int flash_nrf5_write(struct device *dev, off_t addr,
+static int flash_nrf_write(struct device *dev, off_t addr,
 			     const void *data, size_t len)
 {
 	int ret;
@@ -127,11 +127,11 @@ static int flash_nrf5_write(struct device *dev, off_t addr,
 
 	k_sem_take(&sem_lock, K_FOREVER);
 
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 	if (ticker_is_initialized(0)) {
 		ret = write_in_timeslice(addr, data, len);
 	} else
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 	{
 		ret = write(addr, data, len);
 	}
@@ -141,7 +141,7 @@ static int flash_nrf5_write(struct device *dev, off_t addr,
 	return ret;
 }
 
-static int flash_nrf5_erase(struct device *dev, off_t addr, size_t size)
+static int flash_nrf_erase(struct device *dev, off_t addr, size_t size)
 {
 	u32_t pg_size = NRF_FICR->CODEPAGESIZE;
 	u32_t n_pages = size / pg_size;
@@ -162,11 +162,11 @@ static int flash_nrf5_erase(struct device *dev, off_t addr, size_t size)
 
 	k_sem_take(&sem_lock, K_FOREVER);
 
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 	if (ticker_is_initialized(0)) {
 		ret = erase_in_timeslice(addr, size);
 	} else
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 	{
 		ret = erase(addr, size);
 	}
@@ -176,7 +176,7 @@ static int flash_nrf5_erase(struct device *dev, off_t addr, size_t size)
 	return ret;
 }
 
-static int flash_nrf5_write_protection(struct device *dev, bool enable)
+static int flash_nrf_write_protection(struct device *dev, bool enable)
 {
 	k_sem_take(&sem_lock, K_FOREVER);
 
@@ -195,7 +195,7 @@ static int flash_nrf5_write_protection(struct device *dev, bool enable)
 #if defined(CONFIG_FLASH_PAGE_LAYOUT)
 static struct flash_pages_layout dev_layout;
 
-static void flash_nrf5_pages_layout(struct device *dev,
+static void flash_nrf_pages_layout(struct device *dev,
 				     const struct flash_pages_layout **layout,
 				     size_t *layout_size)
 {
@@ -204,26 +204,26 @@ static void flash_nrf5_pages_layout(struct device *dev,
 }
 #endif /* CONFIG_FLASH_PAGE_LAYOUT */
 
-static const struct flash_driver_api flash_nrf5_api = {
-	.read = flash_nrf5_read,
-	.write = flash_nrf5_write,
-	.erase = flash_nrf5_erase,
-	.write_protection = flash_nrf5_write_protection,
+static const struct flash_driver_api flash_nrf_api = {
+	.read = flash_nrf_read,
+	.write = flash_nrf_write,
+	.erase = flash_nrf_erase,
+	.write_protection = flash_nrf_write_protection,
 #if defined(CONFIG_FLASH_PAGE_LAYOUT)
-	.page_layout = flash_nrf5_pages_layout,
+	.page_layout = flash_nrf_pages_layout,
 #endif
 	.write_block_size = 1,
 };
 
-static int nrf5_flash_init(struct device *dev)
+static int nrf_flash_init(struct device *dev)
 {
-	dev->driver_api = &flash_nrf5_api;
+	dev->driver_api = &flash_nrf_api;
 
 	k_sem_init(&sem_lock, 1, 1);
 
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 	k_sem_init(&sem_sync, 0, 1);
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 
 #if defined(CONFIG_FLASH_PAGE_LAYOUT)
 	dev_layout.pages_count = NRF_FICR->CODESIZE;
@@ -233,10 +233,10 @@ static int nrf5_flash_init(struct device *dev)
 	return 0;
 }
 
-DEVICE_INIT(nrf5_flash, FLASH_DEV_NAME, nrf5_flash_init,
+DEVICE_INIT(nrf_flash, FLASH_DEV_NAME, nrf_flash_init,
 	     NULL, NULL, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
 
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 
 static void time_slot_callback_work(u32_t ticks_at_expire, u32_t remainder,
 				    u16_t lazy, void *context)
@@ -385,7 +385,7 @@ static int write_in_timeslice(off_t addr, const void *data, size_t len)
 	return  work_in_time_slice(&flash_op_desc);
 }
 
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 
 static int erase_op(void *context)
 {
@@ -393,7 +393,7 @@ static int erase_op(void *context)
 	u32_t pg_size = NRF_FICR->CODEPAGESIZE;
 	struct erase_context *e_ctx = context;
 
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 	u32_t ticks_begin = 0;
 	u32_t ticks_diff;
 	u32_t i = 0;
@@ -401,7 +401,7 @@ static int erase_op(void *context)
 	if (e_ctx->enable_time_limit) {
 		ticks_begin = ticker_ticks_now_get();
 	}
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 
 	/* Erase uses a specific configuration register */
 	NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Een << NVMC_CONFIG_WEN_Pos;
@@ -414,7 +414,7 @@ static int erase_op(void *context)
 		e_ctx->size -= pg_size;
 		e_ctx->addr += pg_size;
 
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 		i++;
 
 		if (e_ctx->enable_time_limit) {
@@ -426,7 +426,7 @@ static int erase_op(void *context)
 				break;
 			}
 		}
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 
 	} while (e_ctx->size > 0);
 
@@ -450,7 +450,7 @@ static int write_op(void *context)
 	u32_t tmp_word;
 	u32_t count;
 
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 	u32_t ticks_begin = 0;
 	u32_t ticks_diff;
 	u32_t i = 1;
@@ -458,7 +458,7 @@ static int write_op(void *context)
 	if (w_ctx->enable_time_limit) {
 		ticks_begin = ticker_ticks_now_get();
 	}
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 
 	/* Start with a word-aligned address and handle the offset */
 	addr_word = (u32_t)w_ctx->flash_addr & ~0x3;
@@ -480,7 +480,7 @@ static int write_op(void *context)
 
 		shift_write_context(count, w_ctx);
 
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 		if (w_ctx->enable_time_limit) {
 			ticks_diff =
 				ticker_ticks_diff_get(ticker_ticks_now_get(),
@@ -491,7 +491,7 @@ static int write_op(void *context)
 				return FLASH_OP_ONGOING;
 			}
 		}
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 	}
 
 	/* Write all the 4-byte aligned data */
@@ -502,7 +502,7 @@ static int write_op(void *context)
 
 		shift_write_context(sizeof(u32_t), w_ctx);
 
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 		i++;
 
 		if (w_ctx->enable_time_limit) {
@@ -515,7 +515,7 @@ static int write_op(void *context)
 				return FLASH_OP_ONGOING;
 			}
 		}
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 	}
 
 	/* Write remaining data */
@@ -538,9 +538,9 @@ static int erase(u32_t addr, u32_t size)
 	struct erase_context context = {
 		.addr = addr,
 		.size = size,
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 		.enable_time_limit = 0 /* disable time limit */
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 	};
 
 	return	erase_op(&context);
@@ -552,9 +552,9 @@ static int write(off_t addr, const void *data, size_t len)
 		.data_addr = (u32_t) data,
 		.flash_addr = addr,
 		.len = len,
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 		.enable_time_limit = 0 /* disable time limit */
-#endif /* CONFIG_SOC_FLASH_NRF5_RADIO_SYNC */
+#endif /* CONFIG_SOC_FLASH_NRF_RADIO_SYNC */
 	};
 
 	return write_op(&context);

dts/arm/nordic/nrf51822.dtsi
@@ -21,11 +21,11 @@
 			#address-cells = <1>;
 			#size-cells = <1>;
 
-			label="NRF5_FLASH_DRV_NAME";
+			label="NRF_FLASH_DRV_NAME";
 
 			flash0: flash@0 {
 				compatible = "soc-nv-flash";
-				label = "NRF5_FLASH";
+				label = "NRF_FLASH";
 				reg = <0x00000000 DT_FLASH_SIZE>;
 				write-block-size = <4>;
 			};

dts/arm/nordic/nrf52832.dtsi
@@ -21,11 +21,11 @@
 			#address-cells = <1>;
 			#size-cells = <1>;
 
-			label="NRF5_FLASH_DRV_NAME";
+			label="NRF_FLASH_DRV_NAME";
 
 			flash0: flash@0 {
 				compatible = "soc-nv-flash";
-				label = "NRF5_FLASH";
+				label = "NRF_FLASH";
 				reg = <0x00000000 DT_FLASH_SIZE>;
 				write-block-size = <4>;
 			};

dts/arm/nordic/nrf52840.dtsi
@@ -21,11 +21,11 @@
 			#address-cells = <1>;
 			#size-cells = <1>;
 
-			label="NRF5_FLASH_DRV_NAME";
+			label="NRF_FLASH_DRV_NAME";
 
 			flash0: flash@0 {
 				compatible = "soc-nv-flash";
-				label = "NRF5_FLASH";
+				label = "NRF_FLASH";
 				reg = <0x00000000 DT_FLASH_SIZE>;
 				write-block-size = <4>;
 			};

samples/bluetooth/mesh_demo/prj_bbc_microbit.conf
@@ -5,7 +5,6 @@ CONFIG_SYSTEM_WORKQUEUE_STACK_SIZE=2048
 CONFIG_SYS_CLOCK_TICKS_PER_SEC=250
 CONFIG_GPIO=y
 CONFIG_FLASH=y
-CONFIG_SOC_FLASH_NRF5=y
 CONFIG_MICROBIT_DISPLAY=y
 CONFIG_PWM=y
 CONFIG_PWM_NRF5_SW=y

samples/drivers/soc_flash_nrf/prj.conf
@@ -1,5 +1,4 @@
 CONFIG_STDOUT_CONSOLE=y
 CONFIG_FLASH=y
-CONFIG_SOC_FLASH_NRF5=y
 CONFIG_FLASH_PAGE_LAYOUT=y
 CONFIG_MPU_ALLOW_FLASH_WRITE=y

samples/net/echo_client/prj_nrf52840_ot.conf
@@ -31,7 +31,6 @@ CONFIG_PRINTK=y
 CONFIG_UART_INTERRUPT_DRIVEN=y
 
 CONFIG_FLASH=y
-CONFIG_SOC_FLASH_NRF5=y
 CONFIG_FLASH_PAGE_LAYOUT=y
 CONFIG_MPU_ALLOW_FLASH_WRITE=y
 

samples/net/echo_server/prj_nrf52840_ot.conf
@@ -31,7 +31,6 @@ CONFIG_PRINTK=y
 CONFIG_UART_INTERRUPT_DRIVEN=y
 
 CONFIG_FLASH=y
-CONFIG_SOC_FLASH_NRF5=y
 CONFIG_FLASH_PAGE_LAYOUT=y
 CONFIG_MPU_ALLOW_FLASH_WRITE=y
 

samples/subsys/mgmt/mcumgr/smp_svr/prj.conf
@@ -42,4 +42,4 @@ CONFIG_MCUMGR_CMD_STAT_MGMT=y
 ### nRF5 specific settings
 
 # Specify the location of the NFFS file system.
-CONFIG_FS_NFFS_FLASH_DEV_NAME="NRF5_FLASH_DRV_NAME"
+CONFIG_FS_NFFS_FLASH_DEV_NAME="NRF_FLASH_DRV_NAME"

samples/subsys/mgmt/mcumgr/smp_svr/prj.conf.tiny
@@ -46,4 +46,4 @@ CONFIG_MCUMGR_CMD_STAT_MGMT=y
 ### nRF5 specific settings
 
 # Specify the location of the NFFS file system.
-#CONFIG_FS_NFFS_FLASH_DEV_NAME="NRF5_FLASH"
+#CONFIG_FS_NFFS_FLASH_DEV_NAME="NRF_FLASH"

subsys/bluetooth/controller/ll_sw/ll.c
@@ -44,7 +44,7 @@
 
 /* Global singletons */
 
-#if defined(CONFIG_SOC_FLASH_NRF5_RADIO_SYNC)
+#if defined(CONFIG_SOC_FLASH_NRF_RADIO_SYNC)
 #define FLASH_TICKER_NODES        1 /* No. of tickers reserved for flashing */
 #define FLASH_TICKER_USER_APP_OPS 1 /* No. of additional ticker operations */
 #else

subsys/bluetooth/shell/CMakeLists.txt
@@ -12,6 +12,6 @@ zephyr_library_sources_ifdef(
   ticker.c
   )
 zephyr_library_sources_ifdef(
-  CONFIG_SOC_FLASH_NRF5
+  CONFIG_SOC_FLASH_NRF
   flash.c
   )

tests/bluetooth/init/prj_controller.conf
@@ -11,7 +11,6 @@ CONFIG_BT_L2CAP_DYNAMIC_CHANNEL=y
 CONFIG_BT_GATT_CLIENT=y
 CONFIG_BT_BREDR=n
 CONFIG_FLASH=y
-CONFIG_SOC_FLASH_NRF5=y
-CONFIG_SOC_FLASH_NRF5_RADIO_SYNC=y
+CONFIG_SOC_FLASH_NRF_RADIO_SYNC=y
 CONFIG_ZTEST=y
 

tests/bluetooth/init/prj_controller_4_0.conf
@@ -37,7 +37,6 @@ CONFIG_BT_L2CAP_DYNAMIC_CHANNEL=y
 CONFIG_BT_GATT_CLIENT=y
 CONFIG_BT_BREDR=n
 CONFIG_FLASH=y
-CONFIG_SOC_FLASH_NRF5=y
-CONFIG_SOC_FLASH_NRF5_RADIO_SYNC=y
+CONFIG_SOC_FLASH_NRF_RADIO_SYNC=y
 CONFIG_ZTEST=y
 

tests/bluetooth/init/prj_controller_dbg.conf
@@ -55,7 +55,6 @@ CONFIG_BT_DEBUG_GATT=y
 CONFIG_BT_BREDR=n
 CONFIG_DEBUG=y
 CONFIG_FLASH=y
-CONFIG_SOC_FLASH_NRF5=y
-CONFIG_SOC_FLASH_NRF5_RADIO_SYNC=n
+CONFIG_SOC_FLASH_NRF_RADIO_SYNC=n
 CONFIG_ZTEST=y
 

tests/bluetooth/init/prj_controller_tiny.conf
@@ -39,7 +39,6 @@ CONFIG_BT_L2CAP_DYNAMIC_CHANNEL=y
 CONFIG_BT_GATT_CLIENT=y
 CONFIG_BT_BREDR=n
 CONFIG_FLASH=y
-CONFIG_SOC_FLASH_NRF5=y
-CONFIG_SOC_FLASH_NRF5_RADIO_SYNC=y
+CONFIG_SOC_FLASH_NRF_RADIO_SYNC=y
 CONFIG_ZTEST=y
 

tests/subsys/dfu/img_util/prj.conf
@@ -1,7 +1,6 @@
 CONFIG_ZTEST=y
 CONFIG_STDOUT_CONSOLE=y
 CONFIG_FLASH=y
-CONFIG_SOC_FLASH_NRF5=y
 CONFIG_IMG_MANAGER=y
 CONFIG_MCUBOOT_IMG_MANAGER=y
 CONFIG_IMG_BLOCK_BUF_SIZE=512

tests/subsys/dfu/mcuboot/prj.conf
@@ -1,7 +1,6 @@
 CONFIG_ZTEST=y
 CONFIG_STDOUT_CONSOLE=y
 CONFIG_FLASH=y
-CONFIG_SOC_FLASH_NRF5=y
 CONFIG_IMG_MANAGER=y
 CONFIG_MCUBOOT_IMG_MANAGER=y
 CONFIG_IMG_BLOCK_BUF_SIZE=512

tests/subsys/fs/fcb/prj.conf
@@ -1,7 +1,6 @@
 CONFIG_ZTEST=y
 CONFIG_STDOUT_CONSOLE=y
 CONFIG_FLASH=y
-CONFIG_SOC_FLASH_NRF5=y
 CONFIG_FLASH_PAGE_LAYOUT=y
 CONFIG_FLASH_MAP=y
 CONFIG_ARM_CORE_MPU=n

tests/subsys/fs/nffs_fs_api/nrf5x.conf
@@ -4,13 +4,12 @@ CONFIG_MAIN_STACK_SIZE=1024
 CONFIG_HEAP_MEM_POOL_SIZE=1024
 
 CONFIG_FLASH=y
-CONFIG_SOC_FLASH_NRF5=y
-CONFIG_SOC_FLASH_NRF5_RADIO_SYNC=y
+CONFIG_SOC_FLASH_NRF_RADIO_SYNC=y
 CONFIG_MPU_ALLOW_FLASH_WRITE=y
 
 CONFIG_FILE_SYSTEM=y
 CONFIG_FILE_SYSTEM_NFFS=y
-CONFIG_FS_NFFS_FLASH_DEV_NAME="NRF5_FLASH_DRV_NAME"
+CONFIG_FS_NFFS_FLASH_DEV_NAME="NRF_FLASH_DRV_NAME"
 CONFIG_FS_NFFS_NUM_FILES=4
 CONFIG_FS_NFFS_NUM_DIRS=4
 CONFIG_FS_NFFS_NUM_INODES=1024

tests/subsys/storage/flash_map/prj.conf
@@ -1,7 +1,6 @@
 CONFIG_ZTEST=y
 CONFIG_STDOUT_CONSOLE=y
 CONFIG_FLASH=y
-CONFIG_SOC_FLASH_NRF5=y
 CONFIG_FLASH_PAGE_LAYOUT=y
 CONFIG_FLASH_MAP=y
 CONFIG_ARM_CORE_MPU=n