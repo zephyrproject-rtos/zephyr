889e190699fa4ee320e7bfdfb625177ca7ce9eee
Juan Manuel Torres Palma <j.m.torrespalma@gmail.com>
2018-03-23 11:36:59 +0900

    tests: posix: semaphore: test for named semaphores
    New test to check that newly implemented functions
    sem_open(), sem_close() and sem_unlink() work
    according to the POSIX standard.
    
    Signed-off-by: Juan Manuel Torres Palma <j.m.torrespalma@gmail.com>

--- tests/posix/semaphore/src/sem.c
+++ tests/posix/semaphore/src/sem.c
@@ -54,8 +54,132 @@ static void test_sema(void)
 	zassert_false(sem_destroy(&sema), "sema is not destroyed\n");
 }
 
+/*
+ * Test for named semaphores
+ */
+
+#define N_THRD (3)
+#define SSZ (64)
+
+const char sem_name[] = "/shared_sem";
+int shared_counter = 0;
+
+/*
+ * Flags to track the status of child threads
+ */
+sem_t *ssem_value[N_THRD];
+unsigned int status_flag_open[N_THRD];
+unsigned int status_flag_count[N_THRD];
+unsigned int status_flag_close[N_THRD];
+unsigned int unlinked = 0;
+
+/*
+ * Simple test:
+ * - all child threads receive the same semaphore
+ * - can be used for shared variable access
+ * - can be destroyed properly
+ */
+void *child_code(void *vid)
+{
+	unsigned id = (unsigned) vid;
+	sem_t *ssem;
+	int rv;
+
+	/* Check open */
+	ssem = sem_open(sem_name, O_CREAT, 0666, 1);
+	ssem_value[id] = ssem;
+	status_flag_open[id] = 1;
+
+	/* Check correct locking and counter */
+	rv = sem_wait(ssem);
+	shared_counter++;
+	rv = sem_post(ssem);
+	status_flag_count[id] = 1;
+
+	/* Check close */
+	rv = sem_close(ssem);
+	status_flag_close[id] = 1;
+
+	/* Thread 0 takes care of unlinking */
+	if (id == 0) {
+		rv = sem_unlink(sem_name);
+		unlinked = 1;
+	}
+
+	return NULL;
+}
+
+K_THREAD_DEFINE(t0, SSZ, child_code, (void *)0, NULL, NULL, K_USER,
+		0, K_FOREVER);
+K_THREAD_DEFINE(t1, SSZ, child_code, (void *)1, NULL, NULL, K_USER,
+		0, K_FOREVER);
+K_THREAD_DEFINE(t2, SSZ, child_code, (void *)2, NULL, NULL, K_USER,
+		0, K_FOREVER);
+
+/*
+ * Checks if a status array is all set to 1.
+ */
+static inline bool check_barrier(unsigned int status[N_THRD])
+{
+	for (int i = 0; i < N_THRD; ++i) {
+		if (status[i] == 0)
+			return false;
+	}
+
+	return true;
+}
+
+/*
+ * Checks if the semaphore contains the same value for all threads.
+ */
+static inline bool check_sem(sem_t *sarr[N_THRD])
+{
+	for (int i = 1; i < N_THRD; ++i) {
+		if (sarr[0] != sarr[i])
+			return false;
+	}
+
+	return true;
+}
+
+
+/*
+ * This function behaves as a checker, and taking care all threads pass all
+ * checks.
+ * TODO: add timeout.
+ */
+static void test_named_semaphores(void)
+{
+	/* Launch threads */
+	k_thread_start(t0);
+	k_thread_start(t1);
+	k_thread_start(t2);
+
+	/* Wait for all to open and check they received the same object */
+	while (!check_barrier(status_flag_open)) {
+		k_sleep(100);
+	}
+	zassert_false(!check_sem(ssem_value),
+		      "Wrong in shared semaphore open");
+
+	/* Check that there is no race condition in variable rw op */
+	while (!check_barrier(status_flag_count)) {
+		k_sleep(100);
+	}
+	zassert_equal(shared_counter, N_THRD, "Shared variable access failed");
+
+	while (!check_barrier(status_flag_close)) {
+		k_sleep(100);
+	}
+
+	while (!unlinked) {
+		k_sleep(100);
+	}
+}
+
 void test_main(void)
 {
-	ztest_test_suite(test_sem, ztest_unit_test(test_sema));
+	ztest_test_suite(test_sem, ztest_unit_test(test_sema),
+			 ztest_unit_test(test_named_semaphores));
 	ztest_run_test_suite(test_sem);
 }
