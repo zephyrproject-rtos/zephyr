20cd4b551b938e83d8de250ebf810455a2a8a35f
Carles Cufi <carles.cufi@nordicsemi.no>
2018-03-23 13:15:21 +0100

    lib: base64: Add statement of changes
    As per the Apache v2 License, state changes made to the original code in
    the modified version of the files.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- lib/base64/base64.c
+++ lib/base64/base64.c
@@ -1,23 +1,25 @@
 /*
- *	RFC 1521 base64 encoding/decoding
+ * RFC 1521 base64 encoding/decoding
  *
- *	Copyright (C) 2018, Nordic Semiconductor ASA
- *	Copyright (C) 2006-2015, ARM Limited, All Rights Reserved
- *	SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2018, Nordic Semiconductor ASA
+ * Copyright (C) 2006-2015, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
  *
- *	Licensed under the Apache License, Version 2.0 (the "License"); you may
- *	not use this file except in compliance with the License.
- *	You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *	http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *	Unless required by applicable law or agreed to in writing, software
- *	distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- *	WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *	See the License for the specific language governing permissions and
- *	limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *	This file is part of mbed TLS (https://tls.mbed.org)
+ * Adapted for Zephyr by Carles Cufi (carles.cufi@nordicsemi.no)
+ *  - Removed mbedtls_ prefixes
+ *  - Reworked coding style
  */
 
 #include <stdint.h>
