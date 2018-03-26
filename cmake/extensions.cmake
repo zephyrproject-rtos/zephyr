64badd97cd683ddfb7d72d3abdf89a979e91912a
Marti Bolivar <marti@opensourcefoundries.com>
2018-03-26 15:28:30 -0400

    cmake: flash: save runner configuration to CMake cache
    Persist all the important information needed by the runner package to
    the CMake cache. This serves as a parseable record for various tools
    which need to understand how to run the binary.
    
    In particular, it will be used by the west tool, which will be
    introduced in subsequent patches.
    
    Signed-off-by: Marti Bolivar <marti@opensourcefoundries.com>

--- cmake/extensions.cmake
+++ cmake/extensions.cmake
@@ -570,7 +570,8 @@ endfunction()
 #   board_finalize_runner_args(runner)
 #
 # This ensures the build system captures all arguments added in any
-# board_runner_args() calls.
+# board_runner_args() calls, and otherwise finishes registering a
+# runner for use.
 #
 # Extended usage:
 #   board_runner_args(runner "--some-arg=default-value")
@@ -598,6 +599,9 @@ function(board_finalize_runner_args runner)
     # last, so they take precedence.
     ${explicit}
     )
+
+  # Add the finalized runner to the global property list.
+  set_property(GLOBAL APPEND PROPERTY ZEPHYR_RUNNERS ${runner})
 endfunction()
 
 # 1.5. Misc.
