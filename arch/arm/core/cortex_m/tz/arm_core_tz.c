179b8d15f10054bac2739156d8c1c5c66f65e055
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-03-26 11:15:36 +0200

    arch: arm: API to configure fault target state
    This commit defines and implements an internal ARMv8-M TrustZone
    API to allow the user to configure the target state of fault
    exceptions that are not banked between security states.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- arch/arm/core/cortex_m/tz/arm_core_tz.c
+++ arch/arm/core/cortex_m/tz/arm_core_tz.c
@@ -6,6 +6,7 @@
 
 #include <arch/arm/cortex_m/cmsis.h>
 #include <cortex_m/tz.h>
+#include <cortex_m/exc.h>
 
 static void configure_nonsecure_vtor_offset(u32_t vtor_ns)
 {
@@ -61,6 +62,19 @@ void tz_nonsecure_state_setup(const tz_nonsecure_setup_conf_t *p_ns_conf)
 	 */
 	configure_nonsecure_control(p_ns_conf->control_ns.spsel,
 		p_ns_conf->control_ns.npriv);
+}
+
+void tz_nbanked_exception_target_state_set(int secure_state)
+{
+	u32_t aircr_payload = SCB->AIRCR & (~(SCB_AIRCR_VECTKEY_Msk));
+	if (secure_state) {
+		aircr_payload &= ~(SCB_AIRCR_BFHFNMINS_Msk);
+	} else {
+		aircr_payload |= SCB_AIRCR_BFHFNMINS_Msk;
+	}
+	SCB->AIRCR = ((AIRCR_VECT_KEY_PERMIT_WRITE << SCB_AIRCR_VECTKEY_Pos)
+			& SCB_AIRCR_VECTKEY_Msk)
+		| aircr_payload;
 }
 
 void tz_sau_configure(int enable, int allns)
