1c65c2092d58165b76c3ccb297ffc8419b0b3903
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2018-03-26 09:48:57 +0200

    samples: Add sample that demonstrates a custom board definition
    Add a sample that demonstrates a custom board definition. This proves
    that BOARD_ROOT works and can be a useful reference when creating a
    custom board definition.
    
    Instead of spending time making up a board, the nrf52840_pca10056
    board has been copied as-is. And the hello world sample has been used
    as the basis for the application.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

---
+++ samples/application_development/out_of_tree_board/boards/arm/nrf52840_pca10056/board.cmake
@@ -0,0 +1,4 @@
+board_runner_args(nrfjprog "--nrf-family=NRF52")
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
---
+++ samples/application_development/out_of_tree_board/boards/arm/nrf52840_pca10056/board.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2016 Nordic Semiconductor ASA.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+/* Push button switch 0 */
+#define SW0_GPIO_PIN	11
+#define SW0_GPIO_NAME	CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+/* Push button switch 1 */
+#define SW1_GPIO_PIN	12
+#define SW1_GPIO_NAME	CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+/* Push button switch 2 */
+#define SW2_GPIO_PIN	24
+#define SW2_GPIO_NAME	CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+/* Push button switch 3 */
+#define SW3_GPIO_PIN	25
+#define SW3_GPIO_NAME	CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+/* Onboard GREEN LED 0 */
+#define LED0_GPIO_PIN   13
+#define LED0_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+/* Onboard GREEN LED 1 */
+#define LED1_GPIO_PIN   14
+#define LED1_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+/* Onboard GREEN LED 2 */
+#define LED2_GPIO_PIN   15
+#define LED2_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+/* Onboard GREEN LED 3 */
+#define LED3_GPIO_PIN   16
+#define LED3_GPIO_PORT  CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+#endif /* __INC_BOARD_H */
