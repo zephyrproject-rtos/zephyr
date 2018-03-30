97b83ffc2c9b1cdde01bdea8412fc5f8a921410b
Stig Bjørlykke <stig.bjorlykke@nordicsemi.no>
2018-03-30 21:25:37 +0200

    net: sockets: Check for errors from dns_get_addr_info
    When dns_get_addr_info() returns an error it does not call the
    resolve callback, and thus the semaphore will not be given.
    
    This fix will avoid a deadlock situation for various errors.
    
    Added some small tests for getaddrinfo().
    
    Signed-off-by: Stig Bjørlykke <stig.bjorlykke@nordicsemi.no>

---
+++ tests/net/socket/getaddrinfo/src/main.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <ztest_assert.h>
+
+#include <net/socket.h>
+
+void test_getaddrinfo_ok(void)
+{
+	struct addrinfo *res = NULL;
+	int ret;
+
+	ret = getaddrinfo("www.zephyrproject.org", NULL, NULL, &res);
+
+	/* Without a local dnsmasq server this request will be canceled. */
+	zassert_equal(ret, DNS_EAI_CANCELED, "Invalid result");
+
+	/* With a local dnsmasq server this request shall return 0. */
+	/* zassert_equal(ret, 0, "Invalid result"); */
+}
+
+void test_getaddrinfo_no_host(void)
+{
+	struct addrinfo *res = NULL;
+	int ret;
+
+	ret = getaddrinfo(NULL, NULL, NULL, &res);
+
+	zassert_equal(ret, DNS_EAI_SYSTEM, "Invalid result");
+	zassert_equal(errno, EINVAL, "Invalid errno");
+	zassert_is_null(res, "ai_addr is not NULL");
+}
+
+void test_main(void)
+{
+	ztest_test_suite(socket_getaddrinfo,
+			 ztest_unit_test(test_getaddrinfo_ok),
+			 ztest_unit_test(test_getaddrinfo_no_host));
+
+	ztest_run_test_suite(socket_getaddrinfo);
+}
