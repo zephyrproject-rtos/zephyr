9d3e3c5f0004891abbef67fd168e95fd6668ea58
Anas Nashif <anas.nashif@intel.com>
2018-03-31 08:25:54 -0500

    tests: xip: cleanup test
    Consolidate source files and cleanup testcase.yaml
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/kernel/xip/src/main.c
+++ tests/kernel/xip/src/main.c
@@ -4,8 +4,37 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/*
+ * @file
+ *
+ * This module tests that XIP performs as expected. If the first
+ * task is even activated that is a good indication that XIP is
+ * working. However, the test does do some some testing on
+ * global variables for completeness sake.
+ */
+
+#include <zephyr.h>
 #include <ztest.h>
-extern void test_globals(void);
+#include "test.h"
+
+/**
+ *
+ * @brief Regression test's entry point
+ *
+ * @return N/A
+ */
+
+void test_globals(void)
+{
+	int  i;
+
+	/* Array should be filled with monotomically incrementing values */
+	for (i = 0; i < XIP_TEST_ARRAY_SZ; i++) {
+
+		/**TESTPOINT: Check if the array value is correct*/
+		zassert_equal(xip_array[i], (i+1), "Array value is incorrect");
+	}
+}
 
 /**test case main entry*/
 void test_main(void)
