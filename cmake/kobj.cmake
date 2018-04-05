8020c9814d23e620c8036e3677b4e91fdd7130c9
Leandro Pereira <leandro.pereira@intel.com>
2018-04-05 13:59:33 -0700

    scripts: gen_kobject_list: Generate enums and case statements
    Adding a new kernel object type or driver subsystem requires changes
    in various different places.  This patch makes it easier to create
    those devices by generating as much as possible in compile time.
    
    No behavior change.
    
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>

---
+++ cmake/kobj.cmake
@@ -0,0 +1,27 @@
+function(gen_kobj gen_dir_out)
+  if (PROJECT_BINARY_DIR)
+    set(gen_dir ${PROJECT_BINARY_DIR}/include/generated)
+  else ()
+    set(gen_dir ${CMAKE_BINARY_DIR}/include/generated)
+  endif ()
+
+  set(KOBJ_TYPES ${gen_dir}/kobj-types-enum.h)
+  set(KOBJ_OTYPE ${gen_dir}/otype-to-str.h)
+
+  file(MAKE_DIRECTORY ${gen_dir})
+
+  add_custom_command(
+    OUTPUT ${KOBJ_TYPES} ${KOBJ_OTYPE}
+    COMMAND
+    ${PYTHON_EXECUTABLE}
+    $ENV{ZEPHYR_BASE}/scripts/gen_kobject_list.py
+    --kobj-types-output ${KOBJ_TYPES}
+    --kobj-otype-output ${KOBJ_OTYPE}
+    $<$<BOOL:${CMAKE_VERBOSE_MAKEFILE}>:--verbose>
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    )
+  add_custom_target(kobj_types_h_target DEPENDS ${KOBJ_TYPES} ${KOBJ_OTYPE})
+
+  set(${gen_dir_out} ${gen_dir} PARENT_SCOPE)
+
+endfunction ()
--- kernel/userspace.c
+++ kernel/userspace.c
@@ -28,61 +28,10 @@ const char *otype_to_str(enum k_objects otype)
 	 */
 #ifdef CONFIG_PRINTK
 	switch (otype) {
-	/* Core kernel objects */
-	case K_OBJ_ALERT:
-		return "k_alert";
-	case K_OBJ_MSGQ:
-		return "k_msgq";
-	case K_OBJ_MUTEX:
-		return "k_mutex";
-	case K_OBJ_PIPE:
-		return "k_pipe";
-	case K_OBJ_SEM:
-		return "k_sem";
-	case K_OBJ_STACK:
-		return "k_stack";
-	case K_OBJ_THREAD:
-		return "k_thread";
-	case K_OBJ_TIMER:
-		return "k_timer";
-	case K_OBJ__THREAD_STACK_ELEMENT:
-		return "k_thread_stack_t";
-
-	/* Driver subsystems */
-	case K_OBJ_DRIVER_ADC:
-		return "adc driver";
-	case K_OBJ_DRIVER_AIO_CMP:
-		return "aio comparator driver";
-	case K_OBJ_DRIVER_COUNTER:
-		return "counter driver";
-	case K_OBJ_DRIVER_CRYPTO:
-		return "crypto driver";
-	case K_OBJ_DRIVER_DMA:
-		return "dma driver";
-	case K_OBJ_DRIVER_FLASH:
-		return "flash driver";
-	case K_OBJ_DRIVER_GPIO:
-		return "gpio driver";
-	case K_OBJ_DRIVER_I2C:
-		return "i2c driver";
-	case K_OBJ_DRIVER_I2S:
-		return "i2s driver";
-	case K_OBJ_DRIVER_IPM:
-		return "ipm driver";
-	case K_OBJ_DRIVER_PINMUX:
-		return "pinmux driver";
-	case K_OBJ_DRIVER_PWM:
-		return "pwm driver";
-	case K_OBJ_DRIVER_ENTROPY:
-		return "entropy driver";
-	case K_OBJ_DRIVER_RTC:
-		return "realtime clock driver";
-	case K_OBJ_DRIVER_SENSOR:
-		return "sensor driver";
-	case K_OBJ_DRIVER_SPI:
-		return "spi driver";
-	case K_OBJ_DRIVER_UART:
-		return "uart driver";
+	/* otype-to-str.h is generated automatically during build by
+	 * gen_kobject_list.py
+	 */
+#include <otype-to-str.h>
 	default:
 		return "?";
 	}
