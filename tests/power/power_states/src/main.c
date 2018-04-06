b234fb060fd69a81922f155aef9088e107b6b65b
Anas Nashif <anas.nashif@intel.com>
2018-04-06 07:46:13 -0400

    tests: qmsi: remove soc watch sample
    Remove this feature specific to QMSI and available through samples only
    to allow for migration to tracing hooks.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/power/power_states/src/main.c
+++ tests/power/power_states/src/main.c
@@ -16,7 +16,6 @@
 #include <power.h>
 #include <soc_power.h>
 #include <string.h>
-#include "soc_watch_logger.h"
 
 static enum power_states states_list[] = {
 	SYS_POWER_STATE_CPU_LPS,
@@ -394,11 +393,6 @@ void main(void)
 #endif
 
 	build_suspend_device_list();
-
-#ifdef CONFIG_SOC_WATCH
-	/* Start the event monitoring thread */
-	soc_watch_logger_thread_start();
-#endif
 
 	/* All our application does is putting the task to sleep so the kernel
 	 * triggers the suspend operation.
