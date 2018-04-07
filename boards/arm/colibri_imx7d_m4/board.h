85e8eaa9a65145c9011e0b55a670760f35e12ea8
Diego Sueiro <diego.sueiro@gmail.com>
2018-04-07 07:14:31 +0100

    boards/arm: add support for colibri_imx7d_m4 board
    The Colibri iMX7D Computer on Module with Colibri Evaluation Board
    configuration supports the following hardware features on the Cortex M4
    Core:
    
    +-----------+------------+-------------------------------------+
    | Interface | Controller | Driver/Component                    |
    +===========+============+=====================================+
    | NVIC      | on-chip    | nested vector interrupt controller  |
    +-----------+------------+-------------------------------------+
    | SYSTICK   | on-chip    | systick                             |
    +-----------+------------+-------------------------------------+
    | UART      | on-chip    | serial port-polling;                |
    |           |            | serial port-interrupt               |
    +-----------+------------+-------------------------------------+
    
    The default configuration can be found in the defconfig file:
    boards/arm/colibri_imx7d_m4/colibri_imx7d_m4_defconfig
    
    Other hardware features are not currently supported by the port.
    
    Connections and IOs:
    
    The Colibri iMX7D Computer on Module with Colibri Evaluation Board
    was tested with the following pinmux controller configuration.
    
    +---------------+-----------------+---------------------------+
    | Board Name    | SoC Name        | Usage                     |
    +===============+=================+===========================+
    | UART_B RXD    | UART2_TXD       | UART Console              |
    +---------------+-----------------+---------------------------+
    | UART_B TXD    | UART2_RXD       | UART Console              |
    +---------------+-----------------+---------------------------+
    
    Signed-off-by: Diego Sueiro <diego.sueiro@gmail.com>

---
+++ boards/arm/colibri_imx7d_m4/board.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2018, Diego Sueiro <diego.sueiro@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+#endif /* __INC_BOARD_H */
