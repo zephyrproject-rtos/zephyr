0fe789ff2ea89b8d9b847b9d6bceae84d3beb144
Andrew Boie <andrew.p.boie@intel.com>
2018-04-12 18:35:56 -0700

    kernel: add k_msgq_alloc_init()
    User mode can't be trusted to provide a memory buffer to
    k_msgq_init(). Introduce k_msgq_alloc_init() which allocates
    the buffer out of the calling thread's resource pool and expose
    that as a system call instead.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- tests/kernel/msgq/msgq_api/src/test_msgq_attrs.c
+++ tests/kernel/msgq/msgq_api/src/test_msgq_attrs.c
@@ -10,38 +10,56 @@ static char __aligned(4) tbuffer[MSG_SIZE * MSGQ_LEN];
 static u32_t send_buf[MSGQ_LEN] = { MSG0, MSG1 };
 static u32_t rec_buf[MSGQ_LEN] = { MSG0, MSG1 };
 
+static void attrs_get(struct k_msgq *q)
+{
+	int ret;
+	struct k_msgq_attrs attrs;
+
+	k_msgq_get_attrs(q, &attrs);
+	zassert_equal(attrs.used_msgs, 0, NULL);
+
+	/*fill the queue to full*/
+	for (int i = 0; i < MSGQ_LEN; i++) {
+		ret = k_msgq_put(q, (void *)&send_buf[i], K_NO_WAIT);
+		zassert_equal(ret, 0, NULL);
+	}
+
+	k_msgq_get_attrs(q, &attrs);
+	zassert_equal(attrs.used_msgs, MSGQ_LEN, NULL);
+
+	for (int i = 0; i < MSGQ_LEN; i++) {
+		ret = k_msgq_get(q, (void *)&rec_buf[i], K_NO_WAIT);
+		zassert_equal(ret, 0, NULL);
+	}
+
+	k_msgq_get_attrs(q, &attrs);
+	zassert_equal(attrs.used_msgs, 0, NULL);
+}
+
 /**
  * @brief Verify zephyr msgq get attributes API.
  * @addtogroup kernel_message_queue
  * @{
  */
+
 void test_msgq_attrs_get(void)
 {
-	int ret;
-	struct k_msgq_attrs attrs;
-
 	k_msgq_init(&msgq, tbuffer, MSG_SIZE, MSGQ_LEN);
-
-	k_msgq_get_attrs(&msgq, &attrs);
-	zassert_equal(attrs.used_msgs, 0, NULL);
-
-	/*fill the queue to full*/
-	for (int i = 0; i < MSGQ_LEN; i++) {
-		ret = k_msgq_put(&msgq, (void *)&send_buf[i], K_NO_WAIT);
-		zassert_equal(ret, 0, NULL);
-	}
-
-	k_msgq_get_attrs(&msgq, &attrs);
-	zassert_equal(attrs.used_msgs, MSGQ_LEN, NULL);
-
-	for (int i = 0; i < MSGQ_LEN; i++) {
-		ret = k_msgq_get(&msgq, (void *)&rec_buf[i], K_NO_WAIT);
-		zassert_equal(ret, 0, NULL);
-	}
-
-	k_msgq_get_attrs(&msgq, &attrs);
-	zassert_equal(attrs.used_msgs, 0, NULL);
+	attrs_get(&msgq);
 }
+
+#ifdef CONFIG_USERSPACE
+void test_msgq_user_attrs_get(void)
+{
+	struct k_msgq *q;
+
+	q = k_object_alloc(K_OBJ_MSGQ);
+	zassert_not_null(q, "couldn't alloc message queue");
+	zassert_false(k_msgq_alloc_init(q, MSG_SIZE, MSGQ_LEN), NULL);
+	attrs_get(q);
+}
+#endif
+
 /**
  * @}
  */
