zephyrproject-rtos/zephyr

commit 7a9c4cbd9d56144235dde08b9ca1e84dde963e62

drivers: spi: Align nrfx_spi shim with the nrfx_spim one
Similar parts of code are aligned in the two shims, so that they
can be easily compared and updated if required.

Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

drivers/spi/spi_nrfx_spi.c
@@ -91,8 +91,8 @@ static int configure(struct device *dev,
 		return 0;
 	}
 
-	if (spi_cfg->operation & SPI_OP_MODE_SLAVE) {
-		SYS_LOG_ERR("Slave mode is not supported for %s",
+	if (SPI_OP_MODE_GET(spi_cfg->operation) != SPI_OP_MODE_MASTER) {
+		SYS_LOG_ERR("Slave mode is not supported on %s",
 			    dev->config->name);
 		return -EINVAL;
 	}
@@ -139,16 +139,16 @@ static void transfer_next_chunk(struct device *dev)
 	size_t chunk_len = spi_context_longest_current_buf(ctx);
 
 	if (chunk_len > 0) {
-		const nrfx_spi_xfer_desc_t xfer_desc = {
-			.p_tx_buffer = ctx->tx_buf,
-			.tx_length = spi_context_tx_buf_on(ctx) ? chunk_len : 0,
-			.p_rx_buffer = ctx->rx_buf,
-			.rx_length = spi_context_rx_buf_on(ctx) ? chunk_len : 0,
-		};
-		dev_data->chunk_len = chunk_len;
-		nrfx_err_t result = nrfx_spi_xfer(&get_dev_config(dev)->spi,
-						  &xfer_desc, 0);
+		nrfx_spim_xfer_desc_t xfer;
+		nrfx_err_t result;
 
+		dev_data->chunk_len = chunk_len;
+
+		xfer.p_tx_buffer = ctx->tx_buf;
+		xfer.tx_length   = spi_context_tx_buf_on(ctx) ? chunk_len : 0;
+		xfer.p_rx_buffer = ctx->rx_buf;
+		xfer.rx_length   = spi_context_rx_buf_on(ctx) ? chunk_len : 0;
+		result = nrfx_spi_xfer(&get_dev_config(dev)->spi, &xfer, 0);
 		if (result == NRFX_SUCCESS) {
 			return;
 		}

commit ba4746c42edd14fb17fb39a4bc6b597dd7d6c4e3

drivers: spi: Correct a typo in spi_nrfx_spi.c
Introduced with the commit 7a9c4cbd9d56144235dde08b9ca1e84dde963e62,
by copy-pasting from "spi_nrfx_spim.c". Shame on me...

Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

drivers/spi/spi_nrfx_spi.c
@@ -139,7 +139,7 @@ static void transfer_next_chunk(struct device *dev)
 	size_t chunk_len = spi_context_longest_current_buf(ctx);
 
 	if (chunk_len > 0) {
-		nrfx_spim_xfer_desc_t xfer;
+		nrfx_spi_xfer_desc_t xfer;
 		nrfx_err_t result;
 
 		dev_data->chunk_len = chunk_len;