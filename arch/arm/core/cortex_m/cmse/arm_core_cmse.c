e0696735728db3c963c6f79b5eaaf14265a154c9
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-04-16 18:44:39 +0200

    arch: arm: implement cmse address range check (secure)
    This commit contributes the implementation of the CMSE
    address range permission checks based on the ARMv8-M
    address range check intrinsics exlusicely for Secure state.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- arch/arm/core/cortex_m/cmse/arm_core_cmse.c
+++ arch/arm/core/cortex_m/cmse/arm_core_cmse.c
@@ -131,9 +131,43 @@ int arm_cmse_addr_nonsecure_read_ok(u32_t addr, int force_npriv)
 	return arm_cmse_addr_nonsecure_read_write_ok(addr, force_npriv, 0);
 }
 
-int arm_cmse_addr_nonsecure_read_write_ok(u32_t addr, int force_npriv)
+int arm_cmse_addr_nonsecure_readwrite_ok(u32_t addr, int force_npriv)
 {
 	return arm_cmse_addr_nonsecure_read_write_ok(addr, force_npriv, 1);
+}
+
+static int arm_cmse_addr_range_nonsecure_read_write_ok(u32_t addr, u32_t size,
+	int force_npriv, int rw)
+{
+	int flags = CMSE_NONSECURE;
+
+	if (force_npriv) {
+		flags |= CMSE_MPU_UNPRIV;
+	}
+	if (rw) {
+		flags |= CMSE_MPU_READWRITE;
+	} else {
+		flags |= CMSE_MPU_READ;
+	}
+	if (cmse_check_address_range((void *)addr, size, flags) != NULL) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+int arm_cmse_addr_range_nonsecure_read_ok(u32_t addr, u32_t size,
+	int force_npriv)
+{
+	return arm_cmse_addr_range_nonsecure_read_write_ok(addr, size,
+		force_npriv, 0);
+}
+
+int arm_cmse_addr_range_nonsecure_readwrite_ok(u32_t addr, u32_t size,
+	int force_npriv)
+{
+	return arm_cmse_addr_range_nonsecure_read_write_ok(addr, size,
+		force_npriv, 1);
 }
 
 #endif /* CONFIG_ARM_SECURE_FIRMWARE */
