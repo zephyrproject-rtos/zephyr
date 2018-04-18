f82e7dce53987df809d6450789e7ce55c88789f9
Stanislav Poboril <stanislav.poboril@nxp.com>
2018-04-18 17:53:39 +0200

    arch: Add imx6sx m4 soc support
    The i.MX 6SoloX SoC is a hybrid multi-core processor composed by one
    Cortex A9 core and one Cortex M4 core.
    
    Zephyr was ported to run on the M4 core. In a later release, it will
    also communicate with the A9 core (running Linux) via RPMsg.
    
    The low level drivers come from NXP FreeRTOS BSP and are located at
    ext/hal/nxp/imx. More details can be found at ext/hal/nxp/imx/README
    
    The A9 core is responsible to load the M4 binary application into the
    RAM, put the M4 in reset, set the M4 Program Counter and Stack Pointer,
    and get the M4 out of reset.
    The A9 can perform these steps at bootloader level after the Linux
    system has booted.
    
    Signed-off-by: Stanislav Poboril <stanislav.poboril@nxp.com>

---
+++ arch/arm/soc/nxp_imx/mcimx6x_m4/soc.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2018, NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _ASMLANGUAGE
+
+#include "rdc.h"
+#include "rdc_defs_imx6sx.h"
+#include "ccm_imx6sx.h"
+#include "clock_freq.h"
+
+#define RDC_DOMAIN_PERM_NONE  (0x0)
+#define RDC_DOMAIN_PERM_W     (0x1)
+#define RDC_DOMAIN_PERM_R     (0x2)
+#define RDC_DOMAIN_PERM_RW    (RDC_DOMAIN_PERM_W|RDC_DOMAIN_PERM_R)
+
+#define RDC_DOMAIN_PERM(domain, perm) (perm << (domain * 2))
+
+#endif /* !_ASMLANGUAGE */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SOC__H_ */
