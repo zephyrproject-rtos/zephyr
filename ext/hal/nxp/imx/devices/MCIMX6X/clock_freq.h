133ce37e76bc57472232558dc588973619888585
Stanislav Poboril <stanislav.poboril@nxp.com>
2018-04-18 17:11:21 +0200

    ext/hal/nxp/imx: Import the nxp imx6 freertos bsp
    This code component is used to add Zephyr support on iMX6SX
    processors, exclusively on Cortex M4 core, and to speed up the
    development process it was decided to have it based on NXP FreeRTOS BSP
    implementation.
    
    The i.MX FreeRTOS BSP is split into separate downloadable packages,
    based on SoC. The packages share most of the peripheral driver files
    and here they are combined together.
    
    The source code was imported from the following folders:
    FreeRTOS_BSP_1.0.1_iMX6SX/platform/drivers
    FreeRTOS_BSP_1.0.1_iMX6SX/platform/devices
    
    This source code depends on headers and sources from zephyr:
        ext/hal/cmsis
    
    Origin: i.MX 6SoloX FreeRTOS BSP 1.0.1 for Cortex-M4 Peripheral Driver
    License: BSD 3-Clause
    URL: https://www.nxp.com/webapp/Download?colCode=FreeRTOS_MX6SX_1.0.1_LINUX&appType=license
    commit: no commit hash
    Purpose: The peripheral driver wraps the H/W for i.MX6SX M4 core
    Maintained-by: External
    
    Signed-off-by: Stanislav Poboril <stanislav.poboril@nxp.com>

---
+++ ext/hal/nxp/imx/devices/MCIMX6X/clock_freq.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of Freescale Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __CLOCK_FREQ_H__
+#define __CLOCK_FREQ_H__
+
+#include "device_imx.h"
+
+/*!
+ * @addtogroup clock_freq_helper
+ * @{
+ */
+
+/*******************************************************************************
+ * API
+ ******************************************************************************/
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+/*!
+ * @brief Get clock frequency applys to the EPIT module
+ *
+ * @param base EPIT base pointer.
+ * @return clock frequency (in HZ) applys to the EPIT module
+ */
+uint32_t get_epit_clock_freq(EPIT_Type* base);
+
+/*!
+ * @brief Get clock frequency applys to the I2C module
+ *
+ * @param base I2C base pointer.
+ * @return clock frequency (in HZ) applys to the I2C module
+ */
+uint32_t get_i2c_clock_freq(I2C_Type* base);
+
+/*!
+ * @brief Get clock frequency applys to the ECSPI module
+ *
+ * @param base ECSPI base pointer.
+ * @return clock frequency (in HZ) applys to the ECSPI module
+ */
+uint32_t get_ecspi_clock_freq(ECSPI_Type* base);
+
+/*!
+ * @brief Get clock frequency applys to the UART module
+ *
+ * @param base UART base pointer.
+ * @return clock frequency (in HZ) applys to the UART module
+ */
+uint32_t get_uart_clock_freq(UART_Type* base);
+
+#if defined(__cplusplus)
+}
+#endif
+
+/*! @}*/
+
+#endif /* __CLOCK_FREQ_H__ */
+/*******************************************************************************
+ * EOF
+ ******************************************************************************/
