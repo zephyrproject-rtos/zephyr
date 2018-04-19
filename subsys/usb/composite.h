7e067414cd4b4fde955c52704992f64972a15d5f
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2018-04-19 14:40:27 +0300

    usb: Remove unneeded header include
    The should be included from sources
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- subsys/usb/composite.h
+++ subsys/usb/composite.h
@@ -9,8 +9,6 @@
 #ifndef __USB_COMPOSITE__
 #define __USB_COMPOSITE__
 
-#include <usb/usb_device.h>
-
 int composite_add_function(struct usb_cfg_data *cfg_data, u8_t if_num);
 
 #endif /* __USB_COMPOSITE__ */
