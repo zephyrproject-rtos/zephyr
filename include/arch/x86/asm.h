6e9243e0ca9aaa1b2f872b71b18f4a8a7ce58ed3
Leandro Pereira <leandro.pereira@intel.com>
2018-04-20 09:51:14 -0700

    arch: x86: Use retpolines in core assembly routines
    In order to mitigate Spectre variant 2 (branch target injection), use
    retpolines for indirect jumps and calls.
    
    The newly-added hidden CONFIG_X86_NO_SPECTRE flag, which is disabled
    by default, must be set by a x86 SoC if its CPU performs speculative
    execution.  Most targets supported by Zephyr do not, so this is
    set to "y" by default.
    
    A new setting, CONFIG_RETPOLINE, has been added to the "Security
    Options" sections, and that will be enabled by default if
    CONFIG_X86_NO_SPECTRE is disabled.
    
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>

--- include/arch/x86/asm.h
+++ include/arch/x86/asm.h
@@ -25,4 +25,55 @@
 #define SP_ARG7         28
 #define SP_ARG8         32
 
+#if defined(_ASMLANGUAGE)
+
+#if defined(CONFIG_RETPOLINE)
+/*
+ * For a description of how retpolines are constructed for both indirect
+ * jumps and indirect calls, please refer to this documentation:
+ * https://support.google.com/faqs/answer/7625886
+ *
+ * Since these macros are used in a few places in arch/x86/core assembly
+ * routines, with different reg parameters, it's not possible to use
+ * the "out of line" construction technique to share a trampoline.
+ */
+
+#define INDIRECT_JMP_IMPL(reg, id) \
+		call .set_up_target ## id; \
+	.speculative_trap ## id: \
+		pause; \
+		jmp .speculative_trap ## id; \
+	.set_up_target ## id: \
+		mov reg, (%esp); \
+		ret
+
+#define INDIRECT_CALL_IMPL(reg, id) \
+		call .set_up_return ## id; \
+	.inner_indirect_branch ## id: \
+		call .set_up_target ## id; \
+	.speculative_trap ## id: \
+		pause; \
+		jmp .speculative_trap ## id; \
+	.set_up_target ## id: \
+		mov reg, (%esp); \
+		ret; \
+	.set_up_return ## id: \
+		call .inner_indirect_branch ## id
+
+
+#define INDIRECT_CALL_IMPL1(reg, id)	INDIRECT_CALL_IMPL(reg, id)
+#define INDIRECT_JMP_IMPL1(reg, id)	INDIRECT_JMP_IMPL(reg, id)
+
+#define INDIRECT_CALL(reg)	INDIRECT_CALL_IMPL1(reg, __COUNTER__)
+#define INDIRECT_JMP(reg)	INDIRECT_JMP_IMPL1(reg, __COUNTER__)
+
+#else
+
+#define INDIRECT_CALL(reg)	call *reg
+#define INDIRECT_JMP(reg)	jmp *reg
+
+#endif /* CONFIG_RETPOLINE */
+
+#endif /* _ASMLANGUAGE */
+
 #endif /* __INCsysX86Asmh */
