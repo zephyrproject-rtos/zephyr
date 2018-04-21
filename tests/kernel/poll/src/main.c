1609f251eedf9322dc4ee7f661a27fcfe1c9b7f9
Anas Nashif <anas.nashif@intel.com>
2018-04-21 16:52:43 -0500

    tests: kernel: style, tag, and category fixes
    Fix coding style, test tags and use categories.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/kernel/poll/src/main.c
+++ tests/kernel/poll/src/main.c
@@ -19,10 +19,9 @@ extern void test_poll_multi(void);
 /*test case main entry*/
 void test_main(void)
 {
-	ztest_test_suite(poll_api
-			 , ztest_unit_test(test_poll_no_wait)
-			 , ztest_unit_test(test_poll_wait)
-			 , ztest_unit_test(test_poll_multi)
-			 );
+	ztest_test_suite(poll_api,
+			ztest_unit_test(test_poll_no_wait),
+			ztest_unit_test(test_poll_wait),
+			ztest_unit_test(test_poll_multi));
 	ztest_run_test_suite(poll_api);
 }
