297ac3765f6c8ab7ee3cf7524c7064b6c8e30fdf
Christian Tavares <christian.tavares@ossystems.com.br>
2018-04-23 11:50:01 -0300

    lib: updatehub: Add UpdateHub.io support
    UpdateHub is an enterprise-grade solution which makes simple to
    remotely update all your embedded devices in the field. It
    handles all aspects related to sending Firmware Over-the-Air(FOTA)
    updates with maximum security and efficiency, while you focus in
    adding value to your product.
    
    Signed-off-by: Christian Tavares <christian.tavares@ossystems.com.br>
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

---
+++ lib/updatehub/updatehub_firmware.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 O.S.Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __UPDATEHUB_FIRMWARE_H__
+#define __UPDATEHUB_FIRMWARE_H__
+
+#include <flash.h>
+#include <dfu/mcuboot.h>
+#include <dfu/flash_img.h>
+
+bool updatehub_get_firmware_version(char *version, int version_len);
+
+#endif /* __UPDATEHUB_FIRMWARE_H__ */
