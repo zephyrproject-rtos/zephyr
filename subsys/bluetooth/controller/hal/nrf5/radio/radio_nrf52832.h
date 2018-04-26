5f146e491ecf07ef325828f9229f9c4edbdc8452
Alberto Escolar Piedras <alpi@oticon.com>
2018-04-26 15:08:56 +0200

    Bluetooth: controller: Use SOC series macro instead of the board macro
    In the BLE controller radio HAL:
    To avoid confusion, use SOC series macro instead of board macro.
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>

--- subsys/bluetooth/controller/hal/nrf5/radio/radio_nrf52832.h
+++ subsys/bluetooth/controller/hal/nrf5/radio/radio_nrf52832.h
@@ -205,16 +205,16 @@
 
 static inline void hal_radio_reset(void)
 {
-#if !defined(CONFIG_BOARD_NRFXX_NWTSIM)
+#if !defined(CONFIG_SOC_SERIES_NWTSIM_NRFXX)
 	/* Anomalies 102, 106 and 107 */
 	*(volatile u32_t *)0x40001774 = ((*(volatile u32_t *)0x40001774) &
 					 0xfffffffe) | 0x01000000;
-#endif /* !CONFIG_BOARD_NRFXX_NWTSIM */
+#endif /* !CONFIG_SOC_SERIES_NWTSIM_NRFXX */
 }
 
 static inline void hal_radio_ram_prio_setup(void)
 {
-#if !defined(CONFIG_BOARD_NRFXX_NWTSIM)
+#if !defined(CONFIG_SOC_SERIES_NWTSIM_NRFXX)
 	struct {
 		u32_t volatile reserved_0[0x5a0 >> 2];
 		u32_t volatile bridge_type;
@@ -251,7 +251,7 @@ static inline void hal_radio_ram_prio_setup(void)
 	NRF_AMLI->RAMPRI.I2S     = 0xFFFFFFFFUL;
 	NRF_AMLI->RAMPRI.PDM     = 0xFFFFFFFFUL;
 	NRF_AMLI->RAMPRI.PWM     = 0xFFFFFFFFUL;
-#endif /* !CONFIG_BOARD_NRFXX_NWTSIM */
+#endif /* !CONFIG_SOC_SERIES_NWTSIM_NRFXX */
 }
 
 static inline u32_t hal_radio_phy_mode_get(u8_t phy, u8_t flags)
