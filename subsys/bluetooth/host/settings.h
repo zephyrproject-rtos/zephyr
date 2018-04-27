cb19a51f9d7ded5de2c6bb308b912c609857492e
Johan Hedberg <johan.hedberg@intel.com>
2018-04-27 13:24:02 +0300

    Bluetooth: settings: Add support for per-submodule handlers
    Add a new linker section for a list of submodule settings handlers,
    and iterate the list from the various settings callbacks.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- subsys/bluetooth/host/settings.h
+++ subsys/bluetooth/host/settings.h
@@ -4,4 +4,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+struct bt_settings_handler {
+	const char *name;
+	int (*set)(int argc, char **argv, char *val);
+	int (*commit)(void);
+	int (*export)(int (*func)(const char *name, char *val));
+};
+
+#define BT_SETTINGS_DEFINE(_name, _set, _commit, _export)               \
+	const struct bt_settings_handler _name __aligned(4)             \
+			__in_section(_bt_settings, static, _name) = {   \
+				.name = STRINGIFY(_name),               \
+				.set = _set,                            \
+				.commit = _commit,                      \
+				.export = _export,                      \
+			}
+
 int bt_settings_init(void);
