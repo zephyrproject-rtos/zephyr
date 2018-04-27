3e8f4bbe43bd802b74756cee51303dd5bd945a30
Andrew Boie <andrew.p.boie@intel.com>
2018-04-27 13:21:22 -0700

    k_queue: allow user mode access via allocators
    User mode may now use queue objects. Instead of embedding the kernel's
    linked list information directly in the data item, a container struct
    is allocated from the caller's resource pool which is then added to
    the queue. The new sflist type is now used to store a flag indicating
    whether a data item needs to be freed when removed from the queue.
    
    FIFO/LIFOs are derived from k_queues and have had allocator functions
    added.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- tests/kernel/queue/src/main.c
+++ tests/kernel/queue/src/main.c
@@ -12,17 +12,26 @@
  */
 
 #include <ztest.h>
-extern void test_queue_thread2thread(void);
-extern void test_queue_thread2isr(void);
-extern void test_queue_isr2thread(void);
-extern void test_queue_get_2threads(void);
-extern void test_queue_get_fail(void);
-extern void test_queue_loop(void);
+#include "test_queue.h"
+
+#ifndef CONFIG_USERSPACE
+static void test_queue_supv_to_user(void)
+{
+	ztest_test_skip();
+}
+
+static void test_auto_free(void)
+{
+	ztest_test_skip();
+}
+#endif
 
 /*test case main entry*/
 void test_main(void)
 {
 	ztest_test_suite(queue_api,
+			 ztest_unit_test(test_queue_supv_to_user),
+			 ztest_unit_test(test_auto_free),
 			 ztest_unit_test(test_queue_thread2thread),
 			 ztest_unit_test(test_queue_thread2isr),
 			 ztest_unit_test(test_queue_isr2thread),
