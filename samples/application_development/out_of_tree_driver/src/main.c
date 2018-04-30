55cff3eb67713e83a12ca8d6ced74fc44de68d72
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2018-04-30 16:00:19 +0200

    samples: Add sample 'application_development/out_of_tree_driver'
    Add a sample that demonstrates (and tests) that custom drivers can be
    maintained outside of Zephyr.
    
    The sample is fairly minimal with few dependencies and should
    therefore be very portable. It also includes a sample.yaml that should
    ensure that it does not regress.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

---
+++ samples/application_development/out_of_tree_driver/src/main.c
@@ -0,0 +1,20 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "hello_world_driver.h"
+#include <stdio.h>
+#include <zephyr.h>
+
+void main(void)
+{
+	printk("Hello World from the app!\n");
+
+	struct device *dev = device_get_binding("CUSTOM_DRIVER");
+
+	__ASSERT(dev, "Failed to get device binding");
+
+	printk("device is %p, name is %s\n", dev, dev->config->name);
+
+	hello_world_print(dev);
+}
