011da8c1b0e1d1dfe168c6b0e54a90d47530642e
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2018-04-30 16:00:19 +0200

    samples: Add sample 'application_development/out_of_tree_driver'
    Add a sample that demonstrates (and tests) that custom drivers can be
    maintained outside of Zephyr.
    
    The sample is fairly minimal with few dependencies and should
    therefore be very portable. It also includes a sample.yaml that should
    ensure that it does not regress.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

---
+++ samples/application_development/out_of_tree_driver/hello_world_module/zephyr/hello_world_driver.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "hello_world_driver.h"
+#include <zephyr/types.h>
+
+/**
+ * This is a minimal example of an out-of-tree driver
+ * implementation. See the header file of the same name for details.
+ */
+
+static struct hello_world_dev_data {
+	u32_t foo;
+} data;
+
+static int init(struct device *dev)
+{
+	data.foo = 5;
+
+	return 0;
+}
+
+static void print_impl(struct device *dev)
+{
+	printk("Hello World from the kernel: %d\n", data.foo);
+
+	__ASSERT(data.foo == 5, "Device was not initialized!");
+}
+
+DEVICE_AND_API_INIT(hello_world, "CUSTOM_DRIVER",
+		    init, &data, NULL,
+		    PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
+		    &((struct hello_world_driver_api){ .print = print_impl }));
