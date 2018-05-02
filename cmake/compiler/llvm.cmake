72edc4e15ff61410555073bcad626014461598b3
Anas Nashif <anas.nashif@intel.com>
2018-05-02 00:09:31 -0500

    clang/llvm: add initial configuration file for clang
    Add an LLVM backend and a clang toolchain variant to support building
    with llvm coming with popular Linux distributions.
    
    This has been tested with X86 boards:
    - quark_d2000_crb
    - quark_se_c1000_devboard/Arduino 101
    
    Use:
    
    export ZEPHYR_TOOLCHAIN_VARIANT=clang
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

---
+++ cmake/compiler/llvm.cmake
@@ -0,0 +1,22 @@
+# Configuration for host installed llvm
+#
+
+set(NOSTDINC "")
+
+foreach(file_name include include-fixed)
+  execute_process(
+    COMMAND ${CMAKE_C_COMPILER} --print-file-name=${file_name}
+    OUTPUT_VARIABLE _OUTPUT
+    )
+  string(REGEX REPLACE "\n" "" _OUTPUT ${_OUTPUT})
+
+  list(APPEND NOSTDINC ${_OUTPUT})
+endforeach()
+
+foreach(isystem_include_dir ${NOSTDINC})
+  list(APPEND isystem_include_flags -isystem ${isystem_include_dir})
+endforeach()
+
+set(CMAKE_REQUIRED_FLAGS -nostartfiles -nostdlib ${isystem_include_flags} -Wl,--unresolved-symbols=ignore-in-object-files)
+string(REPLACE ";" " " CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
+
