75d59c92ba05defe2abdb4d5adb1a4a1ec9763d2
Anas Nashif <anas.nashif@intel.com>
2018-05-05 14:08:01 -0500

    tests: rename main test to main.c
    For many tests, avoid splitting into files and put eveything in main.c.
    For many of the tests, use main.c as the test source file to keep things
    consistent.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/kernel/systhreads/src/main.c
+++ tests/kernel/systhreads/src/main.c
@@ -13,9 +13,36 @@
  */
 
 #include <ztest.h>
-#include "test_systhreads.h"
+static int main_prio;
 
-/* test case main entry */
+/* power hook functions if support */
+int _sys_soc_suspend(s32_t ticks)
+{
+	/** TESTPOINT: check idle thread priority */
+	zassert_true(k_thread_priority_get(k_current_get()) ==
+			K_IDLE_PRIO, NULL);
+	return 0;
+}
+
+/* test cases */
+void test_systhreads_setup(void)
+{
+	main_prio = k_thread_priority_get(k_current_get());
+}
+
+void test_systhreads_main(void)
+{
+	zassert_true(main_prio == CONFIG_MAIN_THREAD_PRIORITY, NULL);
+}
+
+void test_systhreads_idle(void)
+{
+	k_sleep(100);
+	/** TESTPOINT: check working thread priority should */
+	zassert_true(k_thread_priority_get(k_current_get()) <
+			K_IDLE_PRIO, NULL);
+}
+
 void test_main(void)
 {
 	test_systhreads_setup();
