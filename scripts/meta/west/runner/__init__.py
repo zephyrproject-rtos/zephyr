5a94e21f20b38437fdf719a19da14ef6e38eb2f9
Marti Bolivar <marti@opensourcefoundries.com>
2018-05-09 15:46:45 -0400

    scripts: west: runner: add get_runner_cls()
    Add a utility method for getting a runner class given its name.  This
    will be used in an upcoming patch which adds a command for printing
    runner information.
    
    Signed-off-by: Marti Bolivar <marti@opensourcefoundries.com>

--- scripts/meta/west/runner/__init__.py
+++ scripts/meta/west/runner/__init__.py
@@ -23,4 +23,11 @@ from . import qemu
 from . import xtensa
 from . import intel_s1000
 
-__all__ = ['ZephyrBinaryRunner']
+def get_runner_cls(runner):
+    '''Get a runner's class object, given its name.'''
+    for cls in ZephyrBinaryRunner.get_runners():
+        if cls.name() == runner:
+            return cls
+    raise ValueError('unknown runner "{}"'.format(runner))
+
+__all__ = ['ZephyrBinaryRunner', 'get_runner_cls']
