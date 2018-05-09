c3d5a0da62201bec26860f701b74ca322e8fd09f
Johan Hedberg <johan.hedberg@intel.com>
2018-05-09 21:34:52 +0300

    Bluetooth: Mesh: Support for storing model bindings persistently
    Add support for storing the bound App Keys for each model
    persistently. The bindings are stored under the settings key
    bt/mesh/s/<mod id>/bind for SIG models and bt/mesh/v/<mod id>/bind for
    vendor models.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- subsys/bluetooth/host/mesh/settings.h
+++ subsys/bluetooth/host/mesh/settings.h
@@ -10,6 +10,7 @@ void bt_mesh_store_seq(void);
 void bt_mesh_store_rpl(struct bt_mesh_rpl *rpl);
 void bt_mesh_store_subnet(struct bt_mesh_subnet *sub);
 void bt_mesh_store_app_key(struct bt_mesh_app_key *key);
+void bt_mesh_store_mod_bind(struct bt_mesh_model *mod);
 
 void bt_mesh_clear_net(void);
 void bt_mesh_clear_subnet(struct bt_mesh_subnet *sub);
