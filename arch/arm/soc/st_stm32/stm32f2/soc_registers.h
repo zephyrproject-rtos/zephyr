f4ce39446456efeb9891213b769d92d01ac46e5b
qianfan Zhao <qianfanguijin@163.com>
2018-05-11 20:13:15 +0800

    drivers: stm32-gpio: Provide GPIO driver for stm32f2
    Add necessary GPIO defines for the stm32f2 series soc
    
    Signed-off-by: qianfan Zhao <qianfanguijin@163.com>

---
+++ arch/arm/soc/st_stm32/stm32f2/soc_registers.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018 qianfan Zhao <qianfanguijin@163.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _STM32F2X_SOC_REGISTERS_H_
+#define _STM32F2X_SOC_REGISTERS_H_
+
+/* include register mapping headers */
+#include "gpio_registers.h"
+
+#endif /* _STM32F2X_SOC_REGISTERS_H_ */
