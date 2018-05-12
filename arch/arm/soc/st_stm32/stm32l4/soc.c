71114ea56cc1f0cb67df2888fc8b23b239506ab6
Yannis Damigos <giannis.damigos@gmail.com>
2018-05-12 18:50:44 +0300

    arch/soc/st_stm32: Move STM32Cube HAL core funtions
    STM32Cube HAL core initialization functions HAL_GetTick()
    and HAL_Delay() are implemented in all STM32 series. This
    patch moves these functions in file stm32cube_hal.c under
    "common" folder to reduce code duplication.
    
    Signed-off-by: Yannis Damigos <giannis.damigos@gmail.com>

--- arch/arm/soc/st_stm32/stm32l4/soc.c
+++ arch/arm/soc/st_stm32/stm32l4/soc.c
@@ -10,35 +10,10 @@
  * @brief System/hardware module for STM32L4 processor
  */
 
-#include <kernel.h>
 #include <device.h>
 #include <init.h>
-#include <soc.h>
 #include <arch/cpu.h>
 #include <cortex_m/exc.h>
-
-
-/**
- * @brief This function provides minimum delay (in milliseconds) based
- *	  on variable incremented.
- * @param Delay: specifies the delay time length, in milliseconds.
- * @return None
- */
-void HAL_Delay(__IO uint32_t Delay)
-{
-	k_sleep(Delay);
-}
-
-/**
- * @brief This function configures the source of stm32cube time base.
- *        Cube HAL expects a 1ms tick which matches with k_uptime_get_32.
- *        Tick interrupt priority is not used
- * @return HAL status
- */
-uint32_t HAL_GetTick(void)
-{
-	return k_uptime_get_32();
-}
 
 /**
  * @brief Perform basic hardware initialization at boot.
