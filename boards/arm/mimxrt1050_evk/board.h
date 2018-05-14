094e6659be4308ac7e8cfd74f6b82f3d3ccb77e0
Maureen Helm <maureen.helm@nxp.com>
2018-05-14 13:52:45 -0500

    mimxrt1050_evk: Move led and button definitions to dts
    Moves the led and button definitions for the mimxrt1050_evk board from
    board.h to dts.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- boards/arm/mimxrt1050_evk/board.h
+++ boards/arm/mimxrt1050_evk/board.h
@@ -9,10 +9,4 @@
 
 #include <soc.h>
 
-#define LED0_GPIO_PORT	CONFIG_MCUX_IGPIO_1_NAME
-#define LED0_GPIO_PIN	9
-
-#define SW0_GPIO_NAME	CONFIG_MCUX_IGPIO_5_NAME
-#define SW0_GPIO_PIN	0
-
 #endif /* __INC_BOARD_H */
