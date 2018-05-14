0987b99c279e8b693e972b412288b0039f7a6a67
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2018-05-14 14:54:48 +0530

    tests: posix: Add test for Posix File System API's
    Added a test to verify Posix File System API's.
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>

---
+++ tests/posix/fs/src/test_fs.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2018 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ztest.h>
+
+#define FATFS_MNTP	"/RAM:"
+#define TEST_FILE	FATFS_MNTP"/testfile.txt"
+#define TEST_DIR	FATFS_MNTP"/testdir"
+#define TEST_DIR_FILE	FATFS_MNTP"/testdir/testfile.txt"
+
+extern const char test_str[];
+
+void test_fs_mount(void);
+void test_fs_open(void);
+void test_fs_write(void);
+void test_fs_read(void);
+void test_fs_close(void);
+void test_fs_unlink(void);
+void test_fs_mkdir(void);
+void test_fs_readdir(void);
+void test_fs_rmdir(void);
+
