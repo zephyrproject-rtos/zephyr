ac3653ef9864807678b581f89a361bd28898e419
qianfan Zhao <qianfanguijin@163.com>
2018-05-19 16:34:32 +0800

    clock_control: stm32: add supports for MCO output
    config MCO1 and MCO2(if hardware supports) based on
    Kconfig when startup.
    
    Signed-off-by: qianfan Zhao <qianfanguijin@163.com>

--- drivers/clock_control/stm32_ll_clock.c
+++ drivers/clock_control/stm32_ll_clock.c
@@ -25,6 +25,12 @@
 #define apb2_prescaler(v) _apb2_prescaler(v)
 #endif /* CONFIG_SOC_SERIES_STM32F0X  */
 
+#define _mco1_prescaler(v) LL_RCC_MCO1_DIV_ ## v
+#define mco1_prescaler(v) _mco1_prescaler(v)
+
+#define _mco2_prescaler(v) LL_RCC_MCO2_DIV_ ## v
+#define mco2_prescaler(v) _mco2_prescaler(v)
+
 /**
  * @brief fill in AHB/APB buses configuration structure
  */
@@ -206,6 +212,23 @@ static void stm32_clock_switch_to_hsi(u32_t ahb_prescaler)
 	}
 }
 
+/*
+ * MCO configure doesn't active requested clock source,
+ * so please make sure the clock source was enabled.
+ */
+static inline void stm32_clock_control_mco_init(void)
+{
+#ifndef CONFIG_CLOCK_STM32_MCO1_SRC_NOCLOCK
+	LL_RCC_ConfigMCO(MCO1_SOURCE,
+			 mco1_prescaler(CONFIG_CLOCK_STM32_MCO1_DIV));
+#endif /* CONFIG_CLOCK_STM32_MCO1_SRC_NOCLOCK */
+
+#ifndef CONFIG_CLOCK_STM32_MCO2_SRC_NOCLOCK
+	LL_RCC_ConfigMCO(MCO2_SOURCE,
+			 mco2_prescaler(CONFIG_CLOCK_STM32_MCO2_DIV));
+#endif /* CONFIG_CLOCK_STM32_MCO2_SRC_NOCLOCK */
+}
+
 static int stm32_clock_control_init(struct device *dev)
 {
 	LL_UTILS_ClkInitTypeDef s_ClkInitStruct;
@@ -384,6 +407,9 @@ static int stm32_clock_control_init(struct device *dev)
 	LL_RCC_PLL_Disable();
 
 #endif /* CONFIG_CLOCK_STM32_SYSCLK_SRC_... */
+
+	/* configure MCO1/MCO2 based on Kconfig */
+	stm32_clock_control_mco_init();
 
 	return 0;
 }
