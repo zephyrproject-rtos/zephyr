a97dce162f2cae669103363b19449f43e03be386
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2018-05-22 15:19:47 -0700

    tests: unfold nffs_fs_api
    The introduction of `sanitycheck --list-tests` fails to properly parse
    the subcases in tests/subsys/fs/nffs_api/ beacause four of them are
    all packed with #ifdefs in a single src/main.c.
    
    So this breaks that testcase in four:
    
    - creating a common folder with the code itself
    - moving the common code in */src/main.c to common/test_nffs.h and
      adding ../common/*.c to the sources
    - thinning each testcase.yaml to have only the needed testcase
      definition
    - adding zephyr_include_directories(../common) to the cmakefiles
    
    so we now produce with --list-tests
    
        - filesystem.nffs.basic.append
        - filesystem.nffs.basic.corrupt_block
        - filesystem.nffs.basic.corrupt_scratch
        - filesystem.nffs.basic.fs_mount
        - filesystem.nffs.basic.gc
        - filesystem.nffs.basic.gc_on_oom
        - filesystem.nffs.basic.incomplete_block
        - filesystem.nffs.basic.large_write
        - filesystem.nffs.basic.long_filename
        - filesystem.nffs.basic.lost_found
        - filesystem.nffs.basic.many_children
        - filesystem.nffs.basic.mkdir
        - filesystem.nffs.basic.open
        - filesystem.nffs.basic.overwrite_many
        - filesystem.nffs.basic.overwrite_one
        - filesystem.nffs.basic.overwrite_three
        - filesystem.nffs.basic.overwrite_two
        - filesystem.nffs.basic.read
        - filesystem.nffs.basic.readdir
        - filesystem.nffs.basic.rename
        - filesystem.nffs.basic.split_file
        - filesystem.nffs.basic.unlink
        - filesystem.nffs.basic.wear_level
        - filesystem.nffs.cache.cache_large_file
        - filesystem.nffs.cache.fs_mount
        - filesystem.nffs.large.fs_mount
        - filesystem.nffs.large.large_system
        - filesystem.nffs.large.large_unlink
        - filesystem.nffs.performance.fs_mount
        - filesystem.nffs.performance.performance
       30 total.
    
    vs before, that we did:
    
        - filesystem.nffs.basic.append
        - filesystem.nffs.basic.corrupt_block
        - filesystem.nffs.basic.corrupt_scratch
        - filesystem.nffs.basic.fs_mount
        - filesystem.nffs.basic.gc
        - filesystem.nffs.basic.gc_on_oom
        - filesystem.nffs.basic.incomplete_block
        - filesystem.nffs.basic.large_write
        - filesystem.nffs.basic.long_filename
        - filesystem.nffs.basic.lost_found
        - filesystem.nffs.basic.many_children
        - filesystem.nffs.basic.mkdir
        - filesystem.nffs.basic.open
        - filesystem.nffs.basic.overwrite_many
        - filesystem.nffs.basic.overwrite_one
        - filesystem.nffs.basic.overwrite_three
        - filesystem.nffs.basic.overwrite_two
        - filesystem.nffs.basic.read
        - filesystem.nffs.basic.readdir
        - filesystem.nffs.basic.rename
        - filesystem.nffs.basic.split_file
        - filesystem.nffs.basic.unlink
        - filesystem.nffs.basic.wear_level
        - filesystem.nffs.cache.append
        - filesystem.nffs.cache.corrupt_block
        - filesystem.nffs.cache.corrupt_scratch
        - filesystem.nffs.cache.fs_mount
        ...
        - filesystem.nffs.cache.split_file
        - filesystem.nffs.cache.unlink
        - filesystem.nffs.cache.wear_level
        - filesystem.nffs.large.append
        - filesystem.nffs.large.corrupt_block
        - filesystem.nffs.large.corrupt_scratch
        ...
        - filesystem.nffs.large.split_file
        - filesystem.nffs.large.unlink
        - filesystem.nffs.large.wear_level
        - filesystem.nffs.performance.append
        - filesystem.nffs.performance.corrupt_block
        ...
        - filesystem.nffs.performance.unlink
        - filesystem.nffs.performance.wear_level
       92 total.
    
    (so it was repeating ALL the subcases for eatch main testcase)
    
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>

--- tests/subsys/fs/nffs_fs_api/basic/src/main.c
+++ tests/subsys/fs/nffs_fs_api/basic/src/main.c
@@ -7,56 +7,8 @@
 #include <nffs/nffs.h>
 #include "test_nffs.h"
 
-#if CONFIG_BOARD_QEMU_X86
-static struct nffs_area_desc nffs_selftest_area_descs[] = {
-	{ 0x00000000, 16 * 1024 },
-	{ 0x00004000, 16 * 1024 },
-	{ 0x00008000, 16 * 1024 },
-	{ 0x0000c000, 16 * 1024 },
-	{ 0x00010000, 64 * 1024 },
-	{ 0x00020000, 128 * 1024 },
-	{ 0x00040000, 128 * 1024 },
-	{ 0x00060000, 128 * 1024 },
-	{ 0x00080000, 128 * 1024 },
-	{ 0x000a0000, 128 * 1024 },
-	{ 0x000c0000, 128 * 1024 },
-	{ 0x000e0000, 128 * 1024 },
-	{ 0, 0 },
-};
-#else
-static struct nffs_area_desc nffs_selftest_area_descs[] = {
-	{ 0x00020000, 2 * 4096 },
-	{ 0x00022000, 2 * 4096 },
-	{ 0x00024000, 2 * 4096 },
-	{ 0x00026000, 2 * 4096 },
-	{ 0x00028000, 2 * 4096 },
-	{ 0x0002a000, 2 * 4096 },
-	{ 0x0002c000, 2 * 4096 },
-	{ 0x00030000, 2 * 4096 },
-	{ 0x00032000, 2 * 4096 },
-	{ 0x00034000, 2 * 4096 },
-	{ 0x00036000, 2 * 4096 },
-	{ 0x00038000, 2 * 4096 },
-	{ 0, 0 },
-};
-#endif
-
-static struct nffs_area_desc *save_area_descs;
-
-static void test_setup(void)
-{
-	save_area_descs = nffs_current_area_descs;
-	nffs_current_area_descs = nffs_selftest_area_descs;
-}
-
-static void test_teardown(void)
-{
-	nffs_current_area_descs = save_area_descs;
-}
-
 void test_main(void)
 {
-#ifdef TEST_basic
 	ztest_test_suite(nffs_fs_basic_test,
 		ztest_unit_test_setup_teardown(test_fs_mount,
 					       test_setup, test_teardown),
@@ -105,34 +57,4 @@ void test_main(void)
 		ztest_unit_test_setup_teardown(test_gc_on_oom,
 					       test_setup, test_teardown));
 	ztest_run_test_suite(nffs_fs_basic_test);
-#endif
-
-#ifdef TEST_large
-	ztest_test_suite(nffs_fs_large_test,
-		ztest_unit_test_setup_teardown(test_fs_mount,
-					       test_setup, test_teardown),
-		ztest_unit_test_setup_teardown(test_large_unlink,
-					       test_setup, test_teardown),
-		ztest_unit_test_setup_teardown(test_large_system,
-					       test_setup, test_teardown));
-	ztest_run_test_suite(nffs_fs_large_test);
-#endif
-
-#ifdef TEST_cache
-	ztest_test_suite(nffs_fs_cache_test,
-		ztest_unit_test_setup_teardown(test_fs_mount,
-					       test_setup, test_teardown),
-		ztest_unit_test_setup_teardown(test_cache_large_file,
-					       test_setup, test_teardown));
-	ztest_run_test_suite(nffs_fs_cache_test);
-#endif
-
-#ifdef TEST_performance
-	ztest_test_suite(nffs_fs_performace_test,
-		ztest_unit_test_setup_teardown(test_fs_mount,
-					       test_setup, test_teardown),
-		ztest_unit_test_setup_teardown(test_performance,
-					       test_setup, test_teardown));
-	ztest_run_test_suite(nffs_fs_performace_test);
-#endif
 }
---
+++ tests/subsys/fs/nffs_fs_api/large/src/main.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2017 Codecoup
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <nffs/nffs.h>
+#include "test_nffs.h"
+
+void test_main(void)
+{
+	ztest_test_suite(nffs_fs_large_test,
+		ztest_unit_test_setup_teardown(test_fs_mount,
+					       test_setup, test_teardown),
+		ztest_unit_test_setup_teardown(test_large_unlink,
+					       test_setup, test_teardown),
+		ztest_unit_test_setup_teardown(test_large_system,
+					       test_setup, test_teardown));
+	ztest_run_test_suite(nffs_fs_large_test);
+}
