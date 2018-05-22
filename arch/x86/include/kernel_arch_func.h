d4221f9a7176d952faf1c638e484ea743598e122
Leandro Pereira <leandro.pereira@intel.com>
2018-05-22 12:11:44 -0700

    arch: x86: Rename MSR-handling functions to conform to convention
    Rename _MsrRead() and _MsrWrite() to _x86_msr_read() and
    _x86_msr_write() respectively.
    
    Given that these functions are essentially implemented in assembly.
    make them static inline.  They can be inlined by the compiler quite
    well, most of the time incurring in space savings due to better
    handling of the cobbled registers.
    
    Also simplifies the inline assembly, using constraints instead of
    moving registers ourselves.  Should shave off a few bytes from code
    using these functions.
    
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>

--- arch/x86/include/kernel_arch_func.h
+++ arch/x86/include/kernel_arch_func.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016 Wind River Systems, Inc.
+ * Copyright (c) 2018 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -66,20 +67,54 @@ _set_thread_return_value(struct k_thread *thread, unsigned int value)
 
 extern void k_cpu_atomic_idle(unsigned int imask);
 
-extern void _MsrWrite(unsigned int msr, u64_t msrData);
-extern u64_t _MsrRead(unsigned int msr);
+/**
+ * @brief Write to a model specific register (MSR)
+ *
+ * This function is used to write to an MSR.
+ *
+ * The definitions of the so-called  "Architectural MSRs" are contained
+ * in kernel_structs.h and have the format: IA32_XXX_MSR
+ *
+ * @return N/A
+ */
+static inline void _x86_msr_write(unsigned int msr, u64_t data)
+{
+	u32_t high = data >> 32;
+	u32_t low = data & 0xFFFFFFFF;
+
+	__asm__ volatile ("wrmsr" : : "c"(msr), "a"(low), "d"(high));
+}
+
+/**
+ * @brief Read from a model specific register (MSR)
+ *
+ * This function is used to read from an MSR.
+ *
+ * The definitions of the so-called  "Architectural MSRs" are contained
+ * in kernel_structs.h and have the format: IA32_XXX_MSR
+ *
+ * @return N/A
+ */
+static inline u64_t _x86_msr_read(unsigned int msr)
+{
+	u64_t ret;
+
+	__asm__ volatile("rdmsr" : "=A" (ret) : "c" (msr));
+
+	return ret;
+}
 
 #ifdef CONFIG_JAILHOUSE_X2APIC
 #define MSR_X2APIC_BASE 0x00000800
 
 static inline u32_t read_x2apic(unsigned int reg)
 {
-	return _MsrRead(MSR_X2APIC_BASE + reg);
+	return _x86_msr_read(MSR_X2APIC_BASE + reg);
 }
 
 static inline void write_x2apic(unsigned int reg, u32_t val)
 {
-	_MsrWrite(MSR_X2APIC_BASE + reg, val);
+	_x86_msr_write(MSR_X2APIC_BASE + reg, val);
 }
 #endif
 
