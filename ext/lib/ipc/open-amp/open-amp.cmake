6a86b02642058401590a1a2c699a7f0db0146258
Kumar Gala <kumar.gala@linaro.org>
2018-05-23 09:55:55 -0500

    ext: open-amp: move open-amp down one dir
    Move open-amp to be in ext/lib/ipc/open-amp/open-amp.  This allows us to
    be Zephyr specific files and config like README (for import),
    CMakeLists.txt file, Kconfig, etc in ext/lib/ipc/open-amp/ that don't
    conflict with any files that might have the same name in
    ext/lib/ipc/open-amp/open-amp.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- ext/lib/ipc/open-amp/open-amp.cmake
+++ ext/lib/ipc/open-amp/open-amp.cmake
@@ -2,7 +2,7 @@ include(ExternalProject)
 
 ExternalProject_Add(
   open-amp
-  SOURCE_DIR $ENV{ZEPHYR_BASE}/ext/lib/ipc/open-amp/
+  SOURCE_DIR $ENV{ZEPHYR_BASE}/ext/lib/ipc/open-amp/open-amp/
   DEPENDS metal
   INSTALL_COMMAND ""      # This particular build system has no install command
   CMAKE_ARGS -DWITH_ZEPHYR=ON -DWITH_PROXY=OFF -DBOARD=${BOARD} -DLIBMETAL_INCLUDE_DIR=${ZEPHYR_BINARY_DIR}/ext/hal/libmetal/libmetal/lib/include -DLIBMETAL_LIB=${ZEPHYR_BINARY_DIR}/ext/hal/libmetal/libmetal/lib
