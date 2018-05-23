86b53643350f4883d5ed70219db37d14c92489b6
Andy Ross <andrew.j.ross@intel.com>
2018-05-23 09:26:39 -0700

    tests/kernel: Add preemption priority test
    This test exaustively tests preemption points between threads of all
    priority classes (cooperative, preemptible, and metairq), done both
    from a synchronous reschedule (via k_sem_give() and from interrupt
    context (via irq_offload()), and with and without the sched_lock()
    held.  It then detects the next thread that runs and validates vs. the
    documented priority rules.
    
    Note that there is a whitelisted case on ARM, where irq_offload()
    seems not to be working like a true interrupt (it always returns to
    the interrupted context and doesn't seem to hit the normal exception
    return path which can context switch).  And native_posix is excluded
    because of failures and the fact that it's actually never possible to
    truly preempt a thread there (they run to completion inside _Swap()
    et. al. and then block themselves).  Both of these should be fixable
    in the future but don't (seem to) directly relate to this test.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

---
+++ tests/kernel/sched/preempt/src/main.c
@@ -0,0 +1,277 @@
+/*
+ * Copyright (c) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr.h>
+#include <ztest.h>
+#include <irq_offload.h>
+#include <kernel_structs.h> /* for _THREAD_PENDING */
+
+/* Explicit preemption test.  Works by creating a set of threads in
+ * each priority class (cooperative, preemptive, metairq) which all go
+ * to sleep.  Then one is woken up (from a low priority manager
+ * thread) and arranges to wake up one other thread and validate that
+ * the next thread to be run is correct according to the documented
+ * rules.
+ *
+ * The wakeup test is repeated for all four combinations of threads
+ * either holding or not holding the scheduler lock, and by a
+ * synchronous wake vs. a wake in a (offloaded) interrupt.
+ */
+
+#if defined(CONFIG_SMP) && CONFIG_MP_NUM_CPUS > 1
+#error Preemption test requires single-CPU operation
+#endif
+
+#if CONFIG_NUM_METAIRQ_PRIORITIES < 1
+#error Need one metairq priority
+#endif
+
+#if CONFIG_NUM_COOP_PRIORITIES < 2
+#error Need two cooperative priorities
+#endif
+
+#if CONFIG_NUM_PREEMPT_PRIORITIES < 2
+#error Need two preemptible priorities
+#endif
+
+/* Two threads at each priority (to test the case of waking up a
+ * thread of equal priority).  But only one metairq, as it isn't
+ * technically legal to have more than one at the smae priority.
+ */
+const enum { METAIRQ, COOP, PREEMPTIBLE } worker_priorities[] = {
+	METAIRQ,
+	COOP, COOP,
+	PREEMPTIBLE, PREEMPTIBLE,
+};
+
+#define NUM_THREADS ARRAY_SIZE(worker_priorities)
+
+#define STACK_SIZE (384 + CONFIG_TEST_EXTRA_STACKSIZE)
+
+k_tid_t last_thread;
+
+struct k_thread manager_thread;
+
+K_THREAD_STACK_DEFINE(manager_stack, STACK_SIZE);
+
+struct k_thread worker_threads[NUM_THREADS];
+
+K_THREAD_STACK_ARRAY_DEFINE(worker_stacks, NUM_THREADS, STACK_SIZE);
+
+struct k_thread manager_thread;
+
+struct k_sem worker_sems[NUM_THREADS];
+
+/* Command to worker: who to wake up */
+int target;
+
+/* Command to worker: use a sched_lock()? */
+int do_lock;
+
+/* Command to worker: use irq_offload() to indirect the wakeup? */
+int do_irq;
+
+K_SEM_DEFINE(main_sem, 0, 1);
+
+void wakeup_src_thread(int id)
+{
+	/* irq_offload() on ARM appears not to do what we want.  It
+	 * doesn't appear to go through the normal exception return
+	 * path and always returns back into the calling context, so
+	 * it can't be used to fake preemption.
+	 */
+	if (do_irq && IS_ENABLED(CONFIG_ARM)) {
+		return;
+	}
+
+	/* A little bit of white-box inspection: check that all the
+	 * worker threads are pending.
+	 */
+	for (int i = 0; i < NUM_THREADS; i++) {
+		k_tid_t th = &worker_threads[i];
+
+		zassert_true(th->base.thread_state & _THREAD_PENDING,
+			     "worker thread %d not pending?", i);
+	}
+
+	last_thread = NULL;
+	k_sem_give(&worker_sems[id]);
+
+	/* We are lowest priority, SOMEONE must have run */
+	zassert_true(!!last_thread, "");
+}
+
+void manager(void *p1, void *p2, void *p3)
+{
+	zassert_true(k_current_get() == &manager_thread, "");
+
+	for (int src = 0; src < NUM_THREADS; src++) {
+		for (target = 0; target < NUM_THREADS; target++) {
+
+			if (src == target) {
+				continue;
+			}
+
+			for (do_lock = 0; do_lock < 2; do_lock++) {
+				for (do_irq = 0; do_irq < 2; do_irq++) {
+					wakeup_src_thread(src);
+				}
+			}
+		}
+	}
+
+	k_sem_give(&main_sem);
+}
+
+void irq_waker(void *p)
+{
+	ARG_UNUSED(p);
+	k_sem_give(&worker_sems[target]);
+}
+
+#define PRI(n) (worker_priorities[n])
+
+void validate_wakeup(int src, int target, k_tid_t last_thread)
+{
+	int preempted = &worker_threads[target] == last_thread;
+	int src_wins = PRI(src) < PRI(target);
+	int target_wins = PRI(target) < PRI(src);
+	int tie = PRI(src) == PRI(target);
+
+	if (preempted) {
+		zassert_true(target_wins, "preemption must raise priority");
+	}
+
+	if (PRI(target) == METAIRQ) {
+		zassert_true(preempted,
+			     "metairq threads must always preempt");
+	} else {
+		zassert_false(do_lock && preempted,
+			      "threads holding scheduler lock must not be preempted");
+
+		zassert_false(preempted && src_wins,
+			      "lower priority threads must never preempt");
+
+		if (!do_lock) {
+			zassert_false(!preempted && target_wins,
+				      "higher priority thread should have preempted");
+
+			/* The scheudler implements a 'first added to
+			 * queue' policy for threads within a single
+			 * priority, so the last thread woken up (the
+			 * target) must never run before the source
+			 * thread.
+			 *
+			 * NOTE: I checked, and Zephyr doesn't
+			 * actually document this behavior, though a
+			 * few other tests rely on it IIRC.  IMHO
+			 * there are good arguments for either this
+			 * policy OR the opposite ("run newly woken
+			 * threads first"), and long term we may want
+			 * to revisit this particular check and maybe
+			 * make the poilicy configurable.
+			 */
+			zassert_false(preempted && tie,
+				      "tied priority should not preempt");
+		}
+	}
+}
+
+void worker(void *p1, void *p2, void *p3)
+{
+	int id = (int)p1;
+	k_tid_t curr = &worker_threads[id], prev;
+
+	ARG_UNUSED(p2);
+	ARG_UNUSED(p3);
+
+	zassert_true(id >= 0 && id < NUM_THREADS, "");
+	zassert_true(curr == k_current_get(), "");
+
+	while (1) {
+		/* Wait for the manager or another test thread to wake
+		 * us up
+		 */
+		k_sem_take(&worker_sems[id], K_FOREVER);
+
+		last_thread = curr;
+
+		/* If we're the wakeup target, setting last_thread is
+		 * all we do
+		 */
+		if (id == target) {
+			continue;
+		}
+
+		if (do_lock) {
+			k_sched_lock();
+		}
+
+		if (do_irq) {
+			/* Do the sem_give() in a IRQ to validate that
+			 * ISR return does the right thing
+			 */
+			irq_offload(irq_waker, NULL);
+			prev = last_thread;
+		} else {
+			/* Do the sem_give() directly to validate that
+			 * the synchronous scheduling does the right
+			 * thing
+			 */
+			k_sem_give(&worker_sems[target]);
+			prev = last_thread;
+		}
+
+		if (do_lock) {
+			k_sched_unlock();
+		}
+
+		validate_wakeup(id, target, prev);
+	}
+}
+
+void test_preempt(void)
+{
+	int priority;
+
+	for (int i = 0; i < NUM_THREADS; i++) {
+		k_sem_init(&worker_sems[i], 0, 1);
+
+		if (worker_priorities[i] == METAIRQ) {
+			priority = K_HIGHEST_THREAD_PRIO;
+		} else if (worker_priorities[i] == COOP) {
+			priority = K_HIGHEST_THREAD_PRIO
+				+ CONFIG_NUM_METAIRQ_PRIORITIES;
+
+			zassert_true(priority < K_PRIO_PREEMPT(0), "");
+		} else {
+			priority = K_LOWEST_APPLICATION_THREAD_PRIO - 1;
+
+			zassert_true(priority >= K_PRIO_PREEMPT(0), "");
+		}
+
+		k_thread_create(&worker_threads[i],
+				worker_stacks[i], STACK_SIZE,
+				worker, (void *)i, NULL, NULL,
+				priority, 0, 0);
+	}
+
+	k_thread_create(&manager_thread, manager_stack, STACK_SIZE,
+			manager, NULL, NULL, NULL,
+			K_LOWEST_APPLICATION_THREAD_PRIO, 0, 0);
+
+	/* We don't control the priority of this thread so can't make
+	 * it part of the test.  Just get out of the way until the
+	 * test is done
+	 */
+	k_sem_take(&main_sem, K_FOREVER);
+}
+
+void test_main(void)
+{
+	ztest_test_suite(suite_preempt,
+			 ztest_unit_test(test_preempt));
+	ztest_run_test_suite(suite_preempt);
+}
