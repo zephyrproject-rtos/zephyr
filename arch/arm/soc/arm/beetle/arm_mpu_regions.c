6f6acb4667753a837e11f0d54d250b1d8821b482
Andrew Boie <andrew.p.boie@intel.com>
2018-05-24 13:51:53 -0700

    arm_mpu: reduce boot MPU regions for various soc
    The PPB, Peripheral, and any other I/O regions have been
    removed, the Shareable options are only important with
    unlocked dual cores. Just use the background mapping,
    which is RWX for supervisor and no access for user mode.
    
    The flash region needs to be kept to indicate read-only
    policy. The RAM regions need to be kept to disable execution.
    
    Fixes #6896
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- arch/arm/soc/arm/beetle/arm_mpu_regions.c
+++ arch/arm/soc/arm/beetle/arm_mpu_regions.c
@@ -16,22 +16,6 @@ static struct arm_mpu_region mpu_regions[] = {
 	MPU_REGION_ENTRY("RAM_0",
 			 CONFIG_SRAM_BASE_ADDRESS,
 			 REGION_RAM_ATTR(REGION_128K)),
-	/* Region 2 */
-	MPU_REGION_ENTRY("APB_0",
-			 _BEETLE_APB_BASE,
-			 REGION_IO_ATTR(REGION_64K)),
-	/* Region 3 */
-	MPU_REGION_ENTRY("AHB_0",
-			 _BEETLE_AHB_BASE,
-			 REGION_IO_ATTR(REGION_64K)),
-	/* Region 4 */
-	MPU_REGION_ENTRY("BITBAND_0",
-			 _BEETLE_BITBAND_BASE,
-			 REGION_IO_ATTR(REGION_32M)),
-	/* Region 5 */
-	MPU_REGION_ENTRY("PPB_0",
-			 _BEETLE_PPB_BASE,
-			 REGION_PPB_ATTR(REGION_1M)),
 };
 
 struct arm_mpu_config mpu_config = {
