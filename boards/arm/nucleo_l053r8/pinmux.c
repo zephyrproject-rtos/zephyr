ed0b6b7255fe9575c67e5a622a42f5690904a6ba
Anthony Kreft <anthony.kreft@gmail.com>
2018-05-24 07:39:19 -0500

    boards: arm: add support for NUCLEO-L053R8
    Support for the NUCLEO-L053R8 with STM32L053R8 SoC.
    
    Signed-off-by: Anthony Kreft <anthony.kreft@gmail.com>

---
+++ boards/arm/nucleo_l053r8/pinmux.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2018 Anthony Kreft <anthony.kreft@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <kernel.h>
+#include <device.h>
+#include <init.h>
+#include <pinmux.h>
+#include <sys_io.h>
+
+#include <pinmux/stm32/pinmux_stm32.h>
+
+/* pin assignments for NUCLEO-L053R8 board */
+static const struct pin_config pinconf[] = {
+#ifdef CONFIG_UART_STM32_PORT_1
+	{STM32_PIN_PB6, STM32L0_PINMUX_FUNC_PB6_USART1_TX},
+	{STM32_PIN_PB7, STM32L0_PINMUX_FUNC_PB7_USART1_RX},
+#endif	/* CONFIG_UART_STM32_PORT_1 */
+#ifdef CONFIG_UART_STM32_PORT_2
+	{STM32_PIN_PA2, STM32L0_PINMUX_FUNC_PA2_USART2_TX},
+	{STM32_PIN_PA3, STM32L0_PINMUX_FUNC_PA3_USART2_RX},
+#endif	/* CONFIG_UART_STM32_PORT_2 */
+#ifdef CONFIG_I2C_1
+	{STM32_PIN_PB8, STM32L0_PINMUX_FUNC_PB8_I2C1_SCL},
+	{STM32_PIN_PB9, STM32L0_PINMUX_FUNC_PB9_I2C1_SDA},
+#endif /* CONFIG_I2C_1 */
+#ifdef CONFIG_SPI_1
+	{STM32_PIN_PA5, STM32L0_PINMUX_FUNC_PA5_SPI1_SCK},
+	{STM32_PIN_PA6, STM32L0_PINMUX_FUNC_PA6_SPI1_MISO},
+	{STM32_PIN_PA7, STM32L0_PINMUX_FUNC_PA7_SPI1_MOSI},
+#endif /* CONFIG_SPI_1 */
+};
+
+static int pinmux_stm32_init(struct device *port)
+{
+	ARG_UNUSED(port);
+
+	stm32_setup_pins(pinconf, ARRAY_SIZE(pinconf));
+
+	return 0;
+}
+
+SYS_INIT(pinmux_stm32_init, PRE_KERNEL_1,
+	 CONFIG_PINMUX_STM32_DEVICE_INITIALIZATION_PRIORITY);
