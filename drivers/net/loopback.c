13309cb97e06875af6088a3cd078deab9afe8f9d
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-05-24 12:39:41 +0300

    net: Too long timeout for k_sleep
    Convert couple of MSEC() calls to K_MSEC() as the timeouts
    when using MSEC() are just too long.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- drivers/net/loopback.c
+++ drivers/net/loopback.c
@@ -69,7 +69,7 @@ static int loopback_send(struct net_if *iface, struct net_pkt *pkt)
 	 * must be dropped. This is very much needed for TCP packets where
 	 * the packet is reference counted in various stages of sending.
 	 */
-	cloned = net_pkt_clone(pkt, MSEC(100));
+	cloned = net_pkt_clone(pkt, K_MSEC(100));
 	if (!cloned) {
 		res = -ENOMEM;
 		goto out;
