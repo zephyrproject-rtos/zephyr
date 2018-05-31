7c26df73a62d8ac11acb1208dba2e780f6dafe41
Marti Bolivar <marti@opensourcefoundries.com>
2018-05-31 13:24:06 -0400

    boards: intel_s1000: move runner config out of boards/common
    The file boards/common/intel_s1000_crb.board.cmake is not common at
    all; it is specific to this board. Move its contents into the board's
    board.cmake.
    
    Signed-off-by: Marti Bolivar <marti@opensourcefoundries.com>

--- boards/xtensa/intel_s1000_crb/board.cmake
+++ boards/xtensa/intel_s1000_crb/board.cmake
@@ -1,1 +1,9 @@
-include(${ZEPHYR_BASE}/boards/common/intel_s1000_crb.board.cmake)
+set(BOARD_FLASH_RUNNER intel_s1000)
+set(BOARD_DEBUG_RUNNER intel_s1000)
+
+board_finalize_runner_args(intel_s1000
+  "--xt-ocd-dir=/opt/Tensilica/xocd-12.0.4/xt-ocd"
+  "--ocd-topology=topology_dsp0_flyswatter2.xml"
+  "--ocd-jtag-instr=dsp0_gdb.txt"
+  "--gdb-flash-file=load_elf.txt"
+)
