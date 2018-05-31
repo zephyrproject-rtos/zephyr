d7ff247c4cb981c88de164d45be77656889c066b
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2018-05-31 16:26:58 +0300

    usb: bos: Add USB BOS descriptors API
    Add API for USB BOS Descriptors.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- include/usb/usb_common.h
+++ include/usb/usb_common.h
@@ -198,25 +198,4 @@ struct usb_ep_descriptor {
 	u8_t bInterval;
 } __packed;
 
-/** Binary Device Object Store (BOS) */
-
-#define USB_BOS_CAPABILITY_PLATFORM	0x05
-
-/** BOS Capability Descriptor */
-struct usb_bos_platform_descriptor {
-	u8_t bLength;
-	u8_t bDescriptorType;
-	u8_t bDevCapabilityType;
-	u8_t bReserved;
-	u8_t PlatformCapabilityUUID[16];
-} __packed;
-
-/** BOS Descriptor */
-struct usb_bos_descriptor {
-	u8_t bLength;
-	u8_t bDescriptorType;
-	u16_t wTotalLength;
-	u8_t bNumDeviceCaps;
-} __packed;
-
 #endif /* USB_COMMON_H_ */
