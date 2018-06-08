bc8dd3e836b79c23857f7e0424d136ba0b6bdc2d
vikrant8051 <vikrant8051@gmail.com>
2018-06-08 18:13:31 +0530

    Bluetooth: Mesh: Generic OnOff & Level Models demo
    Minor updates to remove coding style errors !!
    
    Signed-off-by: Vikrant More <vikrant8051@gmail.com>

--- samples/boards/nrf52/mesh/onoff_level_app/src/mesh/publisher.c
+++ samples/boards/nrf52/mesh/onoff_level_app/src/mesh/publisher.c
@@ -6,18 +6,23 @@ void publish(struct k_work *work)
 	int err = 0;
 
 	if (CHECKB(NRF_P0->IN, 11) == 0) {
+
 		bt_mesh_model_msg_init(root_models[3].pub->msg,
 				       BT_MESH_MODEL_OP_2(0x82, 0x03));
 		net_buf_simple_add_u8(root_models[3].pub->msg, 0x01);
 		net_buf_simple_add_u8(root_models[3].pub->msg, tid_onoff++);
 		err = bt_mesh_model_publish(&root_models[3]);
+
 	} else if (CHECKB(NRF_P0->IN, 12) == 0) {
+
 		bt_mesh_model_msg_init(root_models[3].pub->msg,
 				       BT_MESH_MODEL_OP_2(0x82, 0x03));
 		net_buf_simple_add_u8(root_models[3].pub->msg, 0x00);
 		net_buf_simple_add_u8(root_models[3].pub->msg, tid_onoff++);
 		err = bt_mesh_model_publish(&root_models[3]);
+
 	} else if (CHECKB(NRF_P0->IN, 24) == 0) {
+
 		bt_mesh_model_msg_init(root_models[5].pub->msg,
 				       BT_MESH_MODEL_OP_2(0x82, 0x07));
 		net_buf_simple_add_le16(root_models[5].pub->msg, LEVEL_25);
@@ -33,6 +38,7 @@ void publish(struct k_work *work)
 #endif
 
 	} else if (CHECKB(NRF_P0->IN, 25) == 0) {
+
 		bt_mesh_model_msg_init(root_models[5].pub->msg,
 				       BT_MESH_MODEL_OP_2(0x82, 0x07));
 		net_buf_simple_add_le16(root_models[5].pub->msg, LEVEL_100);
