7bbd3a79ae04f75d084a29afedf5cd921bc4746b
Andy Ross <andrew.j.ross@intel.com>
2018-06-13 10:14:44 -0700

    tests/kernel: Add a test for CONFIG_MULTITHREADING=n
    This is a feature Ben added so you could use Zephyr's arch layer to
    bootstrap things like bootloaders without sucking in the whole kernel.
    And it's worked until now.
    
    But we never had a test for it, and I just broke it with the scheduler
    rework.  Add a trivial test just to make sure this continues to link
    and run.  Longer term it would be nice to have some kind of size
    metric here to guarantee that the feature stays lean and doesn't pull
    in needless code.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

---
+++ tests/kernel/threads/no-multithreading/src/main.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr.h>
+#include <ztest.h>
+
+/* The only point to CONFIG_MULTITHREADING=n is to use Zephyr's
+ * multiplatform toolchain, linkage and boostrap integration to arrive
+ * here so the user can run C code unimpeded.  In general, we don't
+ * promise that *any* Zephyr APIs are going to work properly, so don't
+ * try to test any.  That means we can't even use the ztest suite
+ * framework (which spawns threads internally).
+ */
+void test_main(void)
+{
+	TC_PRINT("It works\n");
+	TC_END_REPORT(TC_PASS);
+}
