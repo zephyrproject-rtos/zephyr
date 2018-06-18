d9a56d5ab3289e2b831c99fec45ad12e15496adf
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2018-06-18 10:35:43 +0200

    arch: arm: nrf: Use SystemInit() from MDK in SoC initialization
    Replace code that handles erratas and performs other SoC-specific
    initialization, that was actually copied from SystemInit() provided
    in MDK for particular SoCs, with a call to SystemInit().
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- arch/arm/soc/nordic_nrf/nrf51/soc.c
+++ arch/arm/soc/nordic_nrf/nrf51/soc.c
@@ -25,14 +25,7 @@ extern void _NmiInit(void);
 #define NMI_INIT()
 #endif
 
-#include "nrf.h"
-
-#define __SYSTEM_CLOCK (16000000UL)
-
-static bool ftpan_26(void);
-static bool ftpan_59(void);
-
-uint32_t SystemCoreClock __used = __SYSTEM_CLOCK;
+#include <system_nrf51.h>
 
 static int nordicsemi_nrf51_init(struct device *arg)
 {
@@ -40,37 +33,9 @@ static int nordicsemi_nrf51_init(struct device *arg)
 
 	ARG_UNUSED(arg);
 
-	/* Note:
-	 * Magic numbers below are obtained by reading the registers
-	 * when the SoC was running the SAM-BA bootloader
-	 * (with reserved bits set to 0).
-	 */
-
 	key = irq_lock();
 
-	/* Prepare the peripherals for use as indicated by the PAN 26 "System:
-	 * Manual setup is required to enable the use of peripherals" found at
-	 * Product Anomaly document for your device found at
-	 * https://www.nordicsemi.com/. The side effect of executing these
-	 * instructions in the devices that do not need it is that the new
-	 * peripherals in the second generation devices (LPCOMP for example)
-	 * will not be available.
-	 */
-	if (ftpan_26()) {
-		*(volatile u32_t *)0x40000504 = 0xC007FFDF;
-		*(volatile u32_t *)0x40006C18 = 0x00008000;
-	}
-
-	/* Disable PROTENSET registers under debug, as indicated by PAN 59
-	 * "MPU: Reset value of DISABLEINDEBUG register is incorrect" found
-	 * at Product Anomaly document for your device found at
-	 * https://www.nordicsemi.com/.
-	 */
-	if (ftpan_59()) {
-		NRF_MPU->DISABLEINDEBUG =
-			MPU_DISABLEINDEBUG_DISABLEINDEBUG_Disabled <<
-			MPU_DISABLEINDEBUG_DISABLEINDEBUG_Pos;
-	}
+	SystemInit();
 
 	/* Install default handler that simply resets the CPU
 	 * if configured in the kernel, NOP otherwise
@@ -80,40 +45,6 @@ static int nordicsemi_nrf51_init(struct device *arg)
 	irq_unlock(key);
 
 	return 0;
-}
-
-static bool ftpan_26(void)
-{
-	if ((((*(u32_t *)0xF0000FE0) & 0x000000FF) == 0x1) &&
-	    (((*(u32_t *)0xF0000FE4) & 0x0000000F) == 0x0)) {
-		if ((((*(u32_t *)0xF0000FE8) & 0x000000F0) == 0x00) &&
-		    (((*(u32_t *)0xF0000FEC) & 0x000000F0) == 0x0)) {
-			return true;
-		}
-		if ((((*(u32_t *)0xF0000FE8) & 0x000000F0) == 0x10) &&
-		    (((*(u32_t *)0xF0000FEC) & 0x000000F0) == 0x0)) {
-			return true;
-		}
-		if ((((*(u32_t *)0xF0000FE8) & 0x000000F0) == 0x30) &&
-		    (((*(u32_t *)0xF0000FEC) & 0x000000F0) == 0x0)) {
-			return true;
-		}
-	}
-
-	return false;
-}
-
-static bool ftpan_59(void)
-{
-	if ((((*(u32_t *)0xF0000FE0) & 0x000000FF) == 0x1) &&
-	    (((*(u32_t *)0xF0000FE4) & 0x0000000F) == 0x0)) {
-		if ((((*(u32_t *)0xF0000FE8) & 0x000000F0) == 0x40) &&
-		    (((*(u32_t *)0xF0000FEC) & 0x000000F0) == 0x0)) {
-			return true;
-		}
-	}
-
-	return false;
 }
 
 SYS_INIT(nordicsemi_nrf51_init, PRE_KERNEL_1, 0);
