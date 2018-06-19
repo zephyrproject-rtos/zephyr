0ab496b5bea22dda30b6d8ab1598479c14a4bfac
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-06-19 09:35:35 +0530

    tests/cmsis_rtos_v1: Introduce tests to make use of Thread APIs
    Some test programs to illustrate the usage of thread APIs.
    
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>

---
+++ tests/cmsis_rtos_v1/src/main.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ztest.h>
+#include <kernel.h>
+#include <cmsis_os.h>
+
+extern void test_thread_prio(void);
+extern void test_thread_apis(void);
+
+void test_main(void)
+{
+	ztest_test_suite(test_cmsis_apis,
+			ztest_unit_test(test_thread_apis),
+			ztest_unit_test(test_thread_prio));
+	ztest_run_test_suite(test_cmsis_apis);
+}
