75d23d59162fedcdd2f5372838656ccbfcc7382b
Tomasz Gorochowik <tgorochowik@antmicro.com>
2018-06-19 10:01:16 +0200

    drivers: eth: gmac: Add support for multiple hardware queues
    This commit adds support for multiple hardware TX and RX queues.
    The number of the queues to use can be configured through defconfig.
    
    Packets are sent and received through different hardware queues
    depending on their priority.
    
    Signed-off-by: Tomasz Gorochowik <tgorochowik@antmicro.com>

--- drivers/ethernet/eth_sam_gmac_priv.h
+++ drivers/ethernet/eth_sam_gmac_priv.h
@@ -25,12 +25,30 @@
 #endif
 /** Total number of queues supported by GMAC hardware module */
 #define GMAC_QUEUE_NO                     3
+/** Number of priority queues used */
+#define GMAC_PRIORITY_QUEUE_NO            (CONFIG_ETH_SAM_GMAC_QUEUES - 1)
+
 /** RX descriptors count for main queue */
 #define MAIN_QUEUE_RX_DESC_COUNT CONFIG_ETH_SAM_GMAC_BUF_RX_COUNT
 /** TX descriptors count for main queue */
 #define MAIN_QUEUE_TX_DESC_COUNT (CONFIG_NET_BUF_TX_COUNT + 1)
+
 /** RX/TX descriptors count for priority queues */
-#define PRIORITY_QUEUE_DESC_COUNT         1
+#if GMAC_PRIORITY_QUEUE_NO == 2
+#define PRIORITY_QUEUE2_RX_DESC_COUNT         MAIN_QUEUE_RX_DESC_COUNT
+#define PRIORITY_QUEUE2_TX_DESC_COUNT         MAIN_QUEUE_TX_DESC_COUNT
+#else
+#define PRIORITY_QUEUE2_RX_DESC_COUNT         1
+#define PRIORITY_QUEUE2_TX_DESC_COUNT         1
+#endif
+
+#if GMAC_PRIORITY_QUEUE_NO >= 1
+#define PRIORITY_QUEUE1_RX_DESC_COUNT         MAIN_QUEUE_RX_DESC_COUNT
+#define PRIORITY_QUEUE1_TX_DESC_COUNT         MAIN_QUEUE_TX_DESC_COUNT
+#else
+#define PRIORITY_QUEUE1_RX_DESC_COUNT         1
+#define PRIORITY_QUEUE1_TX_DESC_COUNT         1
+#endif
 
 /*
  * Receive buffer descriptor bit field definitions
@@ -109,6 +127,14 @@
 		(GMAC_IER_RCOMP | GMAC_INT_RX_ERR_BITS | \
 		 GMAC_IER_TCOMP | GMAC_INT_TX_ERR_BITS | GMAC_IER_HRESP)
 
+#define GMAC_INTPQ_RX_ERR_BITS \
+		(GMAC_IERPQ_RXUBR | GMAC_IERPQ_ROVR)
+#define GMAC_INTPQ_TX_ERR_BITS \
+		(GMAC_IERPQ_RLEX | GMAC_IERPQ_TFC)
+#define GMAC_INTPQ_EN_FLAGS \
+		(GMAC_IERPQ_RCOMP | GMAC_INTPQ_RX_ERR_BITS | \
+		 GMAC_IERPQ_TCOMP | GMAC_INTPQ_TX_ERR_BITS | GMAC_IERPQ_HRESP)
+
 /** List of GMAC queues */
 enum queue_idx {
 	GMAC_QUE_0,  /** Main queue */
@@ -143,6 +169,7 @@ struct gmac_queue {
 	struct gmac_desc_list rx_desc_list;
 	struct gmac_desc_list tx_desc_list;
 	struct k_sem tx_desc_sem;
+	struct k_delayed_work tx_timeout_work;
 
 	struct ring_buf rx_frag_list;
 	struct ring_buf tx_frames;
