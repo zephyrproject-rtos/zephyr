816100a6329f0a63b95db1f0adff4d94aefced03
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-06-19 11:18:46 +0200

    arch: arm: use CMSIS bitset flags for MPU attribute definitions
    This commit modifies the interal macro defitions for MPU region
    attributes in arm_mpu.h to use bitset flags directly from CMSIS.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- include/arch/arm/cortex_m/mpu/arm_mpu.h
+++ include/arch/arm/cortex_m/mpu/arm_mpu.h
@@ -12,38 +12,44 @@
 /* ARM MPU RASR Register */
 
 /* Privileged No Access, Unprivileged No Access */
-#define NO_ACCESS	(0x0 << 24)
+#define NO_ACCESS	((0x0 << MPU_RASR_AP_Pos) & MPU_RASR_AP_Msk)
 /* Privileged No Access, Unprivileged No Access */
-#define P_NA_U_NA	(0x0 << 24)
+#define P_NA_U_NA	((0x0 << MPU_RASR_AP_Pos) & MPU_RASR_AP_Msk)
 /* Privileged Read Write, Unprivileged No Access */
-#define P_RW_U_NA	(0x1 << 24)
+#define P_RW_U_NA	((0x1 << MPU_RASR_AP_Pos) & MPU_RASR_AP_Msk)
 /* Privileged Read Write, Unprivileged Read Only */
-#define P_RW_U_RO	(0x2 << 24)
+#define P_RW_U_RO	((0x2 << MPU_RASR_AP_Pos) & MPU_RASR_AP_Msk)
 /* Privileged Read Write, Unprivileged Read Write */
-#define P_RW_U_RW	(0x3 << 24)
+#define P_RW_U_RW	((0x3 << MPU_RASR_AP_Pos) & MPU_RASR_AP_Msk)
 /* Privileged Read Write, Unprivileged Read Write */
-#define FULL_ACCESS	(0x3 << 24)
+#define FULL_ACCESS	((0x3 << MPU_RASR_AP_Pos) & MPU_RASR_AP_Msk)
 /* Privileged Read Only, Unprivileged No Access */
-#define P_RO_U_NA	(0x5 << 24)
+#define P_RO_U_NA	((0x5 << MPU_RASR_AP_Pos) & MPU_RASR_AP_Msk)
 /* Privileged Read Only, Unprivileged Read Only */
-#define P_RO_U_RO	(0x6 << 24)
+#define P_RO_U_RO	((0x6 << MPU_RASR_AP_Pos) & MPU_RASR_AP_Msk)
 /* Privileged Read Only, Unprivileged Read Only */
-#define RO		(0x7 << 24)
+#define RO		((0x7 << MPU_RASR_AP_Pos) & MPU_RASR_AP_Msk)
 
-#define STRONGLY_ORDERED_SHAREABLE			(1 << 18)
-#define DEVICE_SHAREABLE				((1 << 16) | (1 << 18))
-#define NORMAL_OUTER_INNER_WRITE_THROUGH_SHAREABLE	((1 << 17) | (1 << 18))
-#define NORMAL_OUTER_INNER_WRITE_THROUGH_NON_SHAREABLE	(1 << 17)
+/* The following definitions are for internal use in this file. */
+#define STRONGLY_ORDERED_SHAREABLE      MPU_RASR_S_Msk
+#define DEVICE_SHAREABLE                (MPU_RASR_B_Msk | MPU_RASR_S_Msk)
+#define NORMAL_OUTER_INNER_WRITE_THROUGH_SHAREABLE \
+		(MPU_RASR_C_Msk | MPU_RASR_S_Msk)
+#define NORMAL_OUTER_INNER_WRITE_THROUGH_NON_SHAREABLE	MPU_RASR_C_Msk
 #define NORMAL_OUTER_INNER_WRITE_BACK_SHAREABLE	\
-				((1 << 17) | (1 << 16) | (1 << 18))
-#define NORMAL_OUTER_INNER_WRITE_BACK_NON_SHAREABLE	((1 << 17) | (1 << 16))
-#define NORMAL_OUTER_INNER_NON_CACHEABLE_SHAREABLE	((1 << 19) | (1 << 18))
-#define NORMAL_OUTER_INNER_NON_CACHEABLE_NON_SHAREABLE	(1 << 19)
+		(MPU_RASR_C_Msk | MPU_RASR_B_Msk | MPU_RASR_S_Msk)
+#define NORMAL_OUTER_INNER_WRITE_BACK_NON_SHAREABLE \
+		(MPU_RASR_C_Msk | MPU_RASR_B_Msk)
+#define NORMAL_OUTER_INNER_NON_CACHEABLE_SHAREABLE \
+		((1 << MPU_RASR_TEX_Pos) | MPU_RASR_S_Msk)
+#define NORMAL_OUTER_INNER_NON_CACHEABLE_NON_SHAREABLE \
+		(1 << MPU_RASR_TEX_Pos)
 #define NORMAL_OUTER_INNER_WRITE_BACK_WRITE_READ_ALLOCATE_SHAREABLE \
-				((1 << 19) | (1 << 17) | (1 << 16) | (1 << 18))
+	((1 << MPU_RASR_TEX_Pos) |\
+	 MPU_RASR_C_Msk | MPU_RASR_B_Msk | MPU_RASR_S_Msk)
 #define NORMAL_OUTER_INNER_WRITE_BACK_WRITE_READ_ALLOCATE_NONSHAREABLE \
-				((1 << 19) | (1 << 17) | (1 << 18))
-#define DEVICE_NON_SHAREABLE				(2 << 19)
+	((1 << MPU_RASR_TEX_Pos) | MPU_RASR_C_Msk | MPU_RASR_S_Msk)
+#define DEVICE_NON_SHAREABLE            (2 << MPU_RASR_TEX_Pos)
 
 /* Some helper defines for common regions */
 #define REGION_USER_RAM_ATTR(size) \
@@ -63,43 +69,43 @@
 #define REGION_PPB_ATTR(size) (STRONGLY_ORDERED_SHAREABLE | size | P_RW_U_NA)
 #define REGION_IO_ATTR(size) (DEVICE_NON_SHAREABLE | size | P_RW_U_NA)
 
-#define SUB_REGION_0_DISABLED	(0x01 << 8)
-#define SUB_REGION_1_DISABLED	(0x02 << 8)
-#define SUB_REGION_2_DISABLED	(0x04 << 8)
-#define SUB_REGION_3_DISABLED	(0x08 << 8)
-#define SUB_REGION_4_DISABLED	(0x10 << 8)
-#define SUB_REGION_5_DISABLED	(0x20 << 8)
-#define SUB_REGION_6_DISABLED	(0x40 << 8)
-#define SUB_REGION_7_DISABLED	(0x80 << 8)
+#define SUB_REGION_0_DISABLED	((0x01 << MPU_RASR_SRD_Pos) & MPU_RASR_SRD_Msk)
+#define SUB_REGION_1_DISABLED	((0x02 << MPU_RASR_SRD_Pos) & MPU_RASR_SRD_Msk)
+#define SUB_REGION_2_DISABLED	((0x04 << MPU_RASR_SRD_Pos) & MPU_RASR_SRD_Msk)
+#define SUB_REGION_3_DISABLED	((0x08 << MPU_RASR_SRD_Pos) & MPU_RASR_SRD_Msk)
+#define SUB_REGION_4_DISABLED	((0x10 << MPU_RASR_SRD_Pos) & MPU_RASR_SRD_Msk)
+#define SUB_REGION_5_DISABLED	((0x20 << MPU_RASR_SRD_Pos) & MPU_RASR_SRD_Msk)
+#define SUB_REGION_6_DISABLED	((0x40 << MPU_RASR_SRD_Pos) & MPU_RASR_SRD_Msk)
+#define SUB_REGION_7_DISABLED	((0x80 << MPU_RASR_SRD_Pos) & MPU_RASR_SRD_Msk)
 
-#define REGION_32B      (0x04 << 1)
-#define REGION_64B      (0x05 << 1)
-#define REGION_128B     (0x06 << 1)
-#define REGION_256B     (0x07 << 1)
-#define REGION_512B     (0x08 << 1)
-#define REGION_1K       (0x09 << 1)
-#define REGION_2K       (0x0A << 1)
-#define REGION_4K       (0x0B << 1)
-#define REGION_8K       (0x0C << 1)
-#define REGION_16K      (0x0D << 1)
-#define REGION_32K      (0x0E << 1)
-#define REGION_64K      (0x0F << 1)
-#define REGION_128K     (0x10 << 1)
-#define REGION_256K     (0x11 << 1)
-#define REGION_512K     (0x12 << 1)
-#define REGION_1M       (0x13 << 1)
-#define REGION_2M       (0x14 << 1)
-#define REGION_4M       (0x15 << 1)
-#define REGION_8M       (0x16 << 1)
-#define REGION_16M      (0x17 << 1)
-#define REGION_32M      (0x18 << 1)
-#define REGION_64M      (0x19 << 1)
-#define REGION_128M     (0x1A << 1)
-#define REGION_256M     (0x1B << 1)
-#define REGION_512M     (0x1C << 1)
-#define REGION_1G       (0x1D << 1)
-#define REGION_2G       (0x1E << 1)
-#define REGION_4G       (0x1F << 1)
+#define REGION_32B      ((0x04 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_64B      ((0x05 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_128B     ((0x06 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_256B     ((0x07 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_512B     ((0x08 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_1K       ((0x09 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_2K       ((0x0A << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_4K       ((0x0B << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_8K       ((0x0C << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_16K      ((0x0D << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_32K      ((0x0E << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_64K      ((0x0F << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_128K     ((0x10 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_256K     ((0x11 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_512K     ((0x12 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_1M       ((0x13 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_2M       ((0x14 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_4M       ((0x15 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_8M       ((0x16 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_16M      ((0x17 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_32M      ((0x18 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_64M      ((0x19 << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_128M     ((0x1A << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_256M     ((0x1B << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_512M     ((0x1C << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_1G       ((0x1D << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_2G       ((0x1E << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
+#define REGION_4G       ((0x1F << MPU_RASR_SIZE_Pos) & MPU_RASR_SIZE_Msk)
 
 /* Region definition data structure */
 struct arm_mpu_region {
