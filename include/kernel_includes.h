92b8a41f206e5aec50eda784d4479ff533d64f29
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-06-20 17:30:48 +0200

    include: create kernel_includes.h header to hold kernel includes
    This commit creates a new header file (kernel_include.h) that
    contains all header files to be included by kernel_init.h.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

---
+++ include/kernel_includes.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ *
+ * @brief Header files included by kernel.h.
+ */
+
+#ifndef _KERNEL_INCLUDES__H
+#define _KERNEL_INCLUDES__H
+
+#include <stddef.h>
+#include <zephyr/types.h>
+#include <limits.h>
+#include <toolchain.h>
+#include <linker/sections.h>
+#include <atomic.h>
+#include <errno.h>
+#include <misc/__assert.h>
+#include <sched_priq.h>
+#include <misc/dlist.h>
+#include <misc/slist.h>
+#include <misc/sflist.h>
+#include <misc/util.h>
+#include <misc/mempool_base.h>
+#include <kernel_version.h>
+#include <random/rand32.h>
+#include <kernel_arch_thread.h>
+#include <syscall.h>
+#include <misc/printk.h>
+#include <arch/cpu.h>
+#include <misc/rb.h>
+
+#endif /* _KERNEL_INCLUDES__H */
