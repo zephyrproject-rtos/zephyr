b6e41e715cdd28daca10a0ad69718ba285ca9398
Wayne Ren <wei.ren@synopsys.com>
2018-07-07 23:21:17 +0800

    boards: arc: add virtual board based on nsim
    two configs are contained in this board
    
    * nsim_em: em with arc MPUv2
    * nsim_sem: em with arc SecureShield and MPUv3
    
    Signed-off-by: Wayne Ren <wei.ren@synopsys.com>

---
+++ boards/arc/nsim_em/board.cmake
@@ -0,0 +1,10 @@
+set(BOARD_FLASH_RUNNER arc-nsim)
+set(BOARD_DEBUG_RUNNER arc-nsim)
+
+if(${CONFIG_SOC_NSIM_EM})
+board_runner_args(arc-nsim "--props=nsim.props")
+elseif(${CONFIG_SOC_NSIM_SEM})
+board_runner_args(arc-nsim "--props=nsim_sem.props")
+endif()
+
+board_finalize_runner_args(arc-nsim)
