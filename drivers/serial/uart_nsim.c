8a528a797f8f22673c506b83ced88be43256fb81
Wayne Ren <wei.ren@synopsys.com>
2018-07-07 23:25:25 +0800

    drivers: serial: add virtual uart driver for nsim
    In nsim, 'nsim_mem-dev=uart0,base=0xf0000000,irq=24'
    is used to enable a virtual uart.
    
    This uart is only used to output now, so only poll output
    is implemented.
    
    If required, input and interrupt driven are possible.
    
    Signed-off-by: Wayne Ren <wei.ren@synopsys.com>

--- drivers/serial/uart_nsim.c
+++ drivers/serial/uart_nsim.c
@@ -1,4 +1,4 @@
-/*
+/* Copyright (c) 2018, Synopsys, Inc.
  * Copyright (c) 2014-2015, Wind River Systems, Inc.
  *
  * SPDX-License-Identifier: Apache-2.0
@@ -17,16 +17,52 @@
 #include <init.h>
 #include <uart.h>
 
-#define NSIM_UART_DATA 0
-#define NSIM_UART_STATUS 1
+/*
+ * for nsimdrv, "nsim_mem-dev=uart0,base=0xf0000000,irq=24" is
+ * used to simulate a uart.
+ *
+ * UART Register set (this is not a Standards Compliant IP)
+ * Also each reg is Word aligned, but only 8 bits wide
+ */
+#define R_ID0	0
+#define R_ID1	4
+#define R_ID2	8
+#define R_ID3	12
+#define R_DATA	16
+#define R_STS	20
+#define R_BAUDL	24
+#define R_BAUDH	28
+
+/* Bits for UART Status Reg (R/W) */
+#define RXIENB  0x04	/* Receive Interrupt Enable */
+#define TXIENB  0x40	/* Transmit Interrupt Enable */
+
+#define RXEMPTY 0x20	/* Receive FIFO Empty: No char receivede */
+#define TXEMPTY 0x80	/* Transmit FIFO Empty, thus char can be written into */
+
+#define RXFULL  0x08	/* Receive FIFO full */
+#define RXFULL1 0x10	/* Receive FIFO has space for 1 char (tot space=4) */
+
+#define RXFERR  0x01	/* Frame Error: Stop Bit not detected */
+#define RXOERR  0x02	/* OverFlow Err: Char recv but RXFULL still set */
+
 
 #define DEV_CFG(dev) \
 	((const struct uart_device_config * const)(dev)->config->config_info)
 
-#define DATA_REG(dev) (DEV_CFG(dev)->regs + NSIM_UART_DATA)
-#define STATUS_REG(dev) (DEV_CFG(dev)->regs + NSIM_UART_STATUS)
 
-#define TXEMPTY 0x80 /* Transmit FIFO empty and next character can be sent */
+#define UART_REG_SET(u, r, v) ((*(u8_t *)(u + r)) = v)
+#define UART_REG_GET(u, r)    (*(u8_t *)(u + r))
+
+#define UART_REG_OR(u, r, v)  UART_REG_SET(u, r, UART_REG_GET(u, r) | (v))
+#define UART_REG_CLR(u, r, v) UART_REG_SET(u, r, UART_REG_GET(u, r) & ~(v))
+
+#define UART_SET_DATA(uart, val)   UART_REG_SET(uart, R_DATA, val)
+#define UART_GET_DATA(uart)        UART_REG_GET(uart, R_DATA)
+
+#define UART_CLR_STATUS(uart, val) UART_REG_CLR(uart, R_STS, val)
+#define UART_GET_STATUS(uart)      UART_REG_GET(uart, R_STS)
+
 
 static const struct uart_driver_api uart_nsim_driver_api;
 
@@ -54,11 +90,14 @@ static int uart_nsim_init(struct device *dev)
  */
 unsigned char uart_nsim_poll_out(struct device *dev, unsigned char c)
 {
-
+	u32_t regs = DEV_CFG(dev)->regs;
 	/* wait for transmitter to ready to accept a character */
-	while ((_arc_v2_aux_reg_read(STATUS_REG(dev)) & TXEMPTY) == 0)
+
+	while (!(UART_GET_STATUS(regs) & TXEMPTY))
 		;
-	_arc_v2_aux_reg_write(DATA_REG(dev), c);
+
+	UART_SET_DATA(regs, c);
+
 	return c;
 }
 
