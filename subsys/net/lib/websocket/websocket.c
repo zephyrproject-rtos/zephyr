1165f0d400668688d5b2e5f4438099fb38e499a1
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-07-10 15:51:59 +0300

    net: lib: Convert websocket library to use log level
    Use network log level in Websocket library.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- subsys/net/lib/websocket/websocket.c
+++ subsys/net/lib/websocket/websocket.c
@@ -4,11 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#if defined(CONFIG_NET_DEBUG_WEBSOCKET)
-#define SYS_LOG_DOMAIN "ws"
-#define NET_SYS_LOG_LEVEL SYS_LOG_LEVEL_DEBUG
-#define NET_LOG_ENABLED 1
-#endif
+#define LOG_MODULE_NAME net_websocket
+#define NET_LOG_LEVEL CONFIG_WEBSOCKET_LOG_LEVEL
 
 #include <zephyr.h>
 #include <string.h>
