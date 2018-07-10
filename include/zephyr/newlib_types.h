a5457e3025a2f0193b5652192edf6a03f824f9fd
Kumar Gala <kumar.gala@linaro.org>
2018-07-10 14:04:10 -0500

    newlib: Make newlib c types match with Zephyr
    Set defines to make newlib's view of base types to match what we have in
    Zephyr.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

---
+++ include/zephyr/newlib_types.h
@@ -0,0 +1,42 @@
+
+#undef  __INT8_TYPE__
+#define __INT8_TYPE__ signed char
+#undef  __INT16_TYPE__
+#define __INT16_TYPE__ signed short
+#undef  __INT32_TYPE__
+#define __INT32_TYPE__ signed int
+#undef  __INT64_TYPE__
+#define __INT64_TYPE__ signed long long
+
+#undef  __UINT8_TYPE__
+#define __UINT8_TYPE__ unsigned char
+#undef  __UINT16_TYPE__
+#define __UINT16_TYPE__ unsigned short
+#undef  __UINT32_TYPE__
+#define __UINT32_TYPE__ unsigned int
+#undef  __UINT64_TYPE__
+#define __UINT64_TYPE__ unsigned long long
+
+#undef  __INT_LEAST8_TYPE__
+#define __INT_LEAST8_TYPE__ signed char
+#undef  __INT_LEAST16_TYPE__
+#define __INT_LEAST16_TYPE__ signed short
+#undef  __INT_LEAST32_TYPE__
+#define __INT_LEAST32_TYPE__ signed int
+#undef  __INT_LEAST64_TYPE__
+#define __INT_LEAST64_TYPE__ signed long long
+
+#undef  __UINT_LEAST8_TYPE__
+#define __UINT_LEAST8_TYPE__ unsigned char
+#undef  __UINT_LEAST16_TYPE__
+#define __UINT_LEAST16_TYPE__ unsigned short
+#undef  __UINT_LEAST32_TYPE__
+#define __UINT_LEAST32_TYPE__ unsigned int
+#undef  __UINT_LEAST64_TYPE__
+#define __UINT_LEAST64_TYPE__ unsigned long long
+
+#undef  __INTPTR_TYPE__
+#define __INTPTR_TYPE__ int
+
+#undef  __UINTPTR_TYPE__
+#define __UINTPTR_TYPE__ unsigned int
