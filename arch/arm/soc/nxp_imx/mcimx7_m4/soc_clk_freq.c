cff307bfe62b62d42b25bfcbcbb6559ebad2bc9e
Diego Sueiro <diego.sueiro@gmail.com>
2018-07-17 06:42:30 +0100

    arch: Add i.MX7 PWM get clock frequency function
    This patch adds the get_pwm_clock_freq function to be used by the
    i.MX PWM driver.
    
    Signed-off-by: Diego Sueiro <diego.sueiro@gmail.com>

---
+++ arch/arm/soc/nxp_imx/mcimx7_m4/soc_clk_freq.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2018, Diego Sueiro
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ccm_imx7d.h>
+#include <ccm_analog_imx7d.h>
+#include "soc_clk_freq.h"
+
+#ifdef CONFIG_PWM_IMX
+u32_t get_pwm_clock_freq(PWM_Type *base)
+{
+	u32_t root;
+	u32_t hz;
+	u32_t pre, post;
+
+	switch ((u32_t)base) {
+	case PWM1_BASE:
+		root = CCM_GetRootMux(CCM, ccmRootPwm1);
+		CCM_GetRootDivider(CCM, ccmRootPwm1, &pre, &post);
+		break;
+	case PWM2_BASE:
+		root = CCM_GetRootMux(CCM, ccmRootPwm2);
+		CCM_GetRootDivider(CCM, ccmRootPwm2, &pre, &post);
+		break;
+	case PWM3_BASE:
+		root = CCM_GetRootMux(CCM, ccmRootPwm3);
+		CCM_GetRootDivider(CCM, ccmRootPwm3, &pre, &post);
+		break;
+	case PWM4_BASE:
+		root = CCM_GetRootMux(CCM, ccmRootPwm4);
+		CCM_GetRootDivider(CCM, ccmRootPwm4, &pre, &post);
+		break;
+	default:
+		return 0;
+	}
+
+	switch (root) {
+	case ccmRootmuxPwmOsc24m:
+		hz = 24000000;
+		break;
+	case ccmRootmuxPwmSysPllDiv4:
+		hz = CCM_ANALOG_GetSysPllFreq(CCM_ANALOG) >> 2;
+		break;
+	default:
+		return 0;
+	}
+
+	return hz / (pre + 1) / (post + 1);
+}
+#endif /* CONFIG_PWM_IMX */
---
+++ arch/arm/soc/nxp_imx/mcimx7_m4/soc_clk_freq.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2018, Diego Sueiro
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __SOC_CLOCK_FREQ_H__
+#define __SOC_CLOCK_FREQ_H__
+
+#include "device_imx.h"
+#include <zephyr/types.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#ifdef CONFIG_PWM_IMX
+/*!
+ * @brief Get clock frequency applies to the PWM module
+ *
+ * @param base PWM base pointer.
+ * @return clock frequency (in HZ) applies to the PWM module
+ */
+u32_t get_pwm_clock_freq(PWM_Type *base);
+#endif /* CONFIG_PWM_IMX */
+
+#if defined(__cplusplus)
+}
+#endif
+
+/*! @brief Root control names for root clock setting. */
+enum _ccm_root_control_extra {
+	ccmRootPwm1   = (u32_t)(&CCM_TARGET_ROOT106),
+	ccmRootPwm2   = (u32_t)(&CCM_TARGET_ROOT107),
+	ccmRootPwm3   = (u32_t)(&CCM_TARGET_ROOT108),
+	ccmRootPwm4   = (u32_t)(&CCM_TARGET_ROOT109),
+};
+
+/*! @brief Clock source enumeration for PWM peripheral. */
+enum _ccm_rootmux_pwm {
+	ccmRootmuxPwmOsc24m	  = 0U,
+	ccmRootmuxPwmEnetPllDiv10 = 1U,
+	ccmRootmuxPwmSysPllDiv4   = 2U,
+	ccmRootmuxPwmEnetPllDiv25 = 3U,
+	ccmRootmuxPwmAudioPll	  = 4U,
+	ccmRootmuxPwmExtClk2	  = 5U,
+	ccmRootmuxPwmRef1m	  = 6U,
+	ccmRootmuxPwmVideoPll	  = 7U,
+};
+
+/*! @brief CCM CCGR gate control. */
+enum _ccm_ccgr_gate_extra {
+	ccmCcgrGatePwm1      = (u32_t)(&CCM_CCGR132),
+	ccmCcgrGatePwm2      = (u32_t)(&CCM_CCGR133),
+	ccmCcgrGatePwm3      = (u32_t)(&CCM_CCGR134),
+	ccmCcgrGatePwm4      = (u32_t)(&CCM_CCGR135),
+};
+#endif /* __SOC_CLOCK_FREQ_H__ */
