ea12a6f8a7db3b2e456031031ab6e637f3108544
Sebastien Bourdelin <sebastien.bourdelin@savoirfairelinux.com>
2018-07-23 13:40:28 -0400

    drivers: flash: remove w25qxxdv driver and use spi_nor driver instead
    This commit replaces the use of the specific w25qxxdv SPI NOR flash
    driver to use the generic SPI NOR flash driver instead.
    The w25qxxdv driver has also been removed because it is now redundant
    with the generic SPI NOR flash driver.
    
    Signed-off-by: Sebastien Bourdelin <sebastien.bourdelin@savoirfairelinux.com>

--- samples/drivers/spi_flash/src/main.c
+++ samples/drivers/spi_flash/src/main.c
@@ -20,23 +20,17 @@ void main(void)
 	struct device *flash_dev;
 	u8_t buf[TEST_DATA_LEN];
 
-	printf("\nW25QXXDV SPI flash testing\n");
+	printf("\nSPI NOR flash testing\n");
 	printf("==========================\n");
 
-	flash_dev = device_get_binding(CONFIG_SPI_FLASH_W25QXXDV_DRV_NAME);
+	flash_dev = device_get_binding(CONFIG_SPI_NOR_DRV_NAME);
 
 	if (!flash_dev) {
 		printf("SPI flash driver was not found!\n");
 		return;
 	}
 
-	/* Write protection needs to be disabled in w25qxxdv flash before
-	 * each write or erase. This is because the flash component turns
-	 * on write protection automatically after completion of write and
-	 * erase operations.
-	 */
 	printf("\nTest 1: Flash erase\n");
-	flash_write_protection_set(flash_dev, false);
 	if (flash_erase(flash_dev,
 			FLASH_TEST_REGION_OFFSET,
 			FLASH_SECTOR_SIZE) != 0) {
@@ -46,8 +40,6 @@ void main(void)
 	}
 
 	printf("\nTest 2: Flash write\n");
-	flash_write_protection_set(flash_dev, false);
-
 	buf[0] = TEST_DATA_BYTE_0;
 	buf[1] = TEST_DATA_BYTE_1;
 	printf("   Attempted to write %x %x\n", buf[0], buf[1]);
