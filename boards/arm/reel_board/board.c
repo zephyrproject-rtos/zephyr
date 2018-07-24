646bae8e52bb5cc47591adfd67bd4beb157ba8b2
Johann Fischer <j.fischer@phytec.de>
2018-07-24 14:09:17 +0200

    boards: add basis support for the reel board
    Add basic support for the reel board
    
    Signed-off-by: Johann Fischer <j.fischer@phytec.de>

---
+++ boards/arm/reel_board/board.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018 Phytec Messtechnik GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+#include <board.h>
+#include <soc.h>
+
+static int board_reel_board_init(struct device *dev)
+{
+	ARG_UNUSED(dev);
+	volatile NRF_GPIO_Type *gpio = NRF_P1;
+
+	/*
+	 * Workaround to enable peripheral voltage.
+	 */
+	gpio->PIN_CNF[PERIPH_PON_PIN] =
+		(GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos) |
+		(GPIO_PIN_CNF_DIR_Output << GPIO_PIN_CNF_DIR_Pos);
+
+	gpio->OUTSET = BIT(PERIPH_PON_PIN);
+
+	return 0;
+}
+
+SYS_INIT(board_reel_board_init, PRE_KERNEL_2,
+	 CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
