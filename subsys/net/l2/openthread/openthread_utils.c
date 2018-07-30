b76b0e3d10e7922755853a34898ce30b4292847f
Robert Lubos <robert.lubos@nordicsemi.no>
2018-07-30 10:55:20 +0200

    net: openthread: Don't regsiter mcast addresses in Zephyr multiple times
    OpenThread L2 could've called multicast address registration multiple
    times for specific address, which resulted in having multiple entries
    containing the same multicast IPv6 address in Zephyr.
    Checking if address was already registered prevents that.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/net/l2/openthread/openthread_utils.c
+++ subsys/net/l2/openthread/openthread_utils.c
@@ -169,6 +169,12 @@ void add_ipv6_maddr_to_zephyr(struct openthread_context *context)
 
 	for (maddress = otIp6GetMulticastAddresses(context->instance);
 	     maddress; maddress = maddress->mNext) {
+		if (net_if_ipv6_maddr_lookup(
+				(struct in6_addr *)(&maddress->mAddress),
+				&context->iface) != NULL) {
+			continue;
+		}
+
 #if CONFIG_OPENTHREAD_L2_LOG_LEVEL == SYS_LOG_LEVEL_DEBUG
 		char buf[NET_IPV6_ADDR_LEN];
 
