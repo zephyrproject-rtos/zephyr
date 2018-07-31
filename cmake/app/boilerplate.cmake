66b524a62a590f07e3f75a1881cea24caf42bcdb
Anas Nashif <anas.nashif@intel.com>
2018-07-31 12:18:28 -0500

    soc: support out of dir soc code
    Just like boards, support also SoCs out of the tree.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- cmake/app/boilerplate.cmake
+++ cmake/app/boilerplate.cmake
@@ -195,6 +195,9 @@ set(CACHED_BOARD ${BOARD} CACHE STRING "Selected board")
 if(NOT BOARD_ROOT)
   set(BOARD_ROOT ${ZEPHYR_BASE})
 endif()
+if(NOT SOC_ROOT)
+  set(SOC_ROOT ${ZEPHYR_BASE})
+endif()
 
 find_path(BOARD_DIR NAMES "${BOARD}_defconfig" PATHS ${BOARD_ROOT}/boards/*/* NO_DEFAULT_PATH)
 
