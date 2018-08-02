7433f75e1a87a194208b1659430a8e124dee9cb2
Maureen Helm <maureen.helm@nxp.com>
2018-08-02 12:41:49 -0500

    mimxrt1050_evk: Update jlink device name
    A bug in the jlink application made it impossible to use OpenSDA
    firmware with the mimxrt1052 device name, so we used the generic
    Cortex-M7 device name as a workaround. This bug was fixed in jlink
    V6.32h (2018-07-05), so now we can use the more specific device name.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- boards/arm/mimxrt1050_evk/board.cmake
+++ boards/arm/mimxrt1050_evk/board.cmake
@@ -4,5 +4,5 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-board_runner_args(jlink "--device=Cortex-M7")
+board_runner_args(jlink "--device=MCIMXRT1052")
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
