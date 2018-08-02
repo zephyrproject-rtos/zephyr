6044f5f2de5a6a23e6b94acb1ec9d85970e4341b
Spoorthi K <spoorthi.k@intel.com>
2018-08-02 22:01:23 +0530

    tests: obj_tracing: Enhance object tracing test
    Enhance object tracing test to improve code coverage
    of kernel object initialization.
    
    Signed-off-by: Spoorthi K <spoorthi.k@intel.com>

--- tests/kernel/obj_tracing/src/trace_obj.c
+++ tests/kernel/obj_tracing/src/trace_obj.c
@@ -46,7 +46,24 @@ K_MBOX_DEFINE(kmbox);
 K_PIPE_DEFINE(kpipe, 256, 4);
 K_QUEUE_DEFINE(kqueue);
 
-struct k_mutex mutex;
+static struct k_timer timer;
+static struct k_mem_slab mslab;
+static struct k_sem sema;
+static struct k_mutex mutex;
+static struct k_alert alert;
+static struct k_stack stack;
+static struct k_msgq msgq;
+static struct k_mbox mbox;
+static struct k_pipe pipe;
+static struct k_queue queue;
+
+#define BLOCK_SIZE 4
+#define NUM_BLOCKS 4
+
+static char slab[BLOCK_SIZE * NUM_BLOCKS];
+static u32_t sdata[BLOCK_SIZE * NUM_BLOCKS];
+static char buffer[BLOCK_SIZE * NUM_BLOCKS];
+static char data[] = "test";
 
 static void get_obj_count(int obj_type)
 {
@@ -55,138 +72,177 @@ static void get_obj_count(int obj_type)
 
 	switch (obj_type) {
 	case TIMER:
+		k_timer_init(&timer, expiry_dummy_fn, stop_dummy_fn);
+
 		obj_list = SYS_TRACING_HEAD(struct k_timer, k_timer);
 		while (obj_list != NULL) {
 			/* TESTPOINT: Check if the object created
 			 * is added to the list
 			 */
-			if (obj_list == &ktimer) {
-				obj_found = 1;
-				TC_PRINT("Found timer object\n");
-				break;
+			if (obj_list == &ktimer || obj_list == &timer) {
+				obj_found++;
+				if (obj_found == 2) {
+					TC_PRINT("Found timer objects\n");
+					break;
+				}
 			}
 			obj_list = SYS_TRACING_NEXT(struct k_timer, k_timer,
 						    obj_list);
 		}
-		zassert_true(obj_found, "Didn't find timer object");
+		zassert_equal(obj_found, 2,  "Didn't find timer objects");
 		break;
 	case MEM_SLAB:
+		k_mem_slab_init(&mslab, slab, BLOCK_SIZE, NUM_BLOCKS);
+
 		obj_list = SYS_TRACING_HEAD(struct k_mem_slab, k_mem_slab);
 		while (obj_list != NULL) {
-			if (obj_list == &kmslab) {
-				obj_found = 1;
-				TC_PRINT("Found memory slab object\n");
-				break;
+			if (obj_list == &kmslab || obj_list == &mslab) {
+				obj_found++;
+				if (obj_found == 2) {
+					TC_PRINT("Found memory slab objects\n");
+					break;
+				}
 			}
 			obj_list = SYS_TRACING_NEXT(struct k_mem_slab,
-					k_mem_slab, obj_list);
+						    k_mem_slab, obj_list);
 		}
-		zassert_true(obj_found, "Didn't find mem_slab object");
+		zassert_equal(obj_found, 2, "Didn't find mem_slab objects");
 		break;
 	case SEM:
+		k_sem_init(&sema, 0, 1);
+
 		obj_list = SYS_TRACING_HEAD(struct k_sem, k_sem);
 		while (obj_list != NULL) {
-			if (obj_list == &ksema) {
-				obj_found = 1;
-				TC_PRINT("Found semaphore object\n");
-				break;
+			if (obj_list == &ksema || obj_list == &sema) {
+				obj_found++;
+				if (obj_found == 2) {
+					TC_PRINT("Found semaphore objects\n");
+					break;
+				}
 			}
 			obj_list = SYS_TRACING_NEXT(struct k_sem, k_sem,
 						    obj_list);
 		}
-		zassert_true(obj_found, "Didn't find semaphore object");
+		zassert_equal(obj_found, 2, "Didn't find semaphore objects");
 		break;
 	case MUTEX:
 		k_mutex_init(&mutex);
+
 		obj_list = SYS_TRACING_HEAD(struct k_mutex, k_mutex);
 		while (obj_list != NULL) {
 			if (obj_list == &kmutex || obj_list == &mutex) {
-				obj_found = 1;
-				TC_PRINT("Found mutex object\n");
-				break;
+				obj_found++;
+				if (obj_found == 2) {
+					TC_PRINT("Found mutex objects\n");
+					break;
+				}
 			}
 			obj_list = SYS_TRACING_NEXT(struct k_mutex, k_mutex,
 						    obj_list);
 		}
-		zassert_true(obj_found, "Didn't find mutex object");
+		zassert_equal(obj_found, 2, "Didn't find mutex objects");
 		break;
 	case ALERT:
+		k_alert_init(&alert, K_ALERT_IGNORE, 1);
+
 		obj_list = SYS_TRACING_HEAD(struct k_alert, k_alert);
 		while (obj_list != NULL) {
-			if (obj_list == &kalert) {
-				obj_found = 1;
-				TC_PRINT("Found alert object\n");
-				break;
+			if (obj_list == &kalert || obj_list == &alert) {
+				obj_found++;
+				if (obj_found == 2) {
+					TC_PRINT("Found alert objects\n");
+					break;
+				}
 			}
 			obj_list = SYS_TRACING_NEXT(struct k_alert, k_alert,
 						    obj_list);
 		}
-		zassert_true(obj_found, "Didn't find alert object");
+		zassert_equal(obj_found, 2, "Didn't find alert objects");
 		break;
 	case STACK:
+		k_stack_init(&stack, sdata, NUM_BLOCKS);
+
 		obj_list = SYS_TRACING_HEAD(struct k_stack, k_stack);
 		while (obj_list != NULL) {
-			if (obj_list == &kstack) {
-				obj_found = 1;
-				TC_PRINT("Found stack object\n");
-				break;
+			if (obj_list == &kstack || obj_list == &stack) {
+				obj_found++;
+				if (obj_found == 2) {
+					TC_PRINT("Found stack objects\n");
+					break;
+				}
 			}
 			obj_list = SYS_TRACING_NEXT(struct k_stack, k_stack,
 						    obj_list);
 		}
-		zassert_true(obj_found, "Didn't find stack object");
+		zassert_equal(obj_found, 2, "Didn't find stack objects");
 		break;
 	case MSGQ:
+		k_msgq_init(&msgq, buffer, BLOCK_SIZE, NUM_BLOCKS);
+
 		obj_list = SYS_TRACING_HEAD(struct k_msgq, k_msgq);
 		while (obj_list != NULL) {
-			if (obj_list == &kmsgq) {
-				obj_found = 1;
-				TC_PRINT("Found message queue object\n");
-				break;
+			if (obj_list == &kmsgq || obj_list == &msgq) {
+				obj_found++;
+				if (obj_found == 2) {
+					TC_PRINT("Found message queue objects\n");
+					break;
+				}
 			}
 			obj_list = SYS_TRACING_NEXT(struct k_msgq, k_msgq,
 						    obj_list);
 		}
-		zassert_true(obj_found, "Didn't find msgq object");
+		zassert_equal(obj_found, 2, "Didn't find msgq objects");
 		break;
 	case MBOX:
+		k_mbox_init(&mbox);
+
 		obj_list = SYS_TRACING_HEAD(struct k_mbox, k_mbox);
 		while (obj_list != NULL) {
-			if (obj_list == &kmbox) {
-				obj_found = 1;
-				TC_PRINT("Found mail box object\n");
-				break;
+			if (obj_list == &kmbox || obj_list == &mbox) {
+				obj_found++;
+				if (obj_found == 2) {
+					TC_PRINT("Found mail box objects\n");
+					break;
+				}
 			}
 			obj_list = SYS_TRACING_NEXT(struct k_mbox, k_mbox,
 						    obj_list);
 		}
-		zassert_true(obj_found, "Didn't find mbox object");
+		zassert_equal(obj_found, 2, "Didn't find mbox objects");
 		break;
 	case PIPE:
+		k_pipe_init(&pipe, data, 8);
+
 		obj_list = SYS_TRACING_HEAD(struct k_pipe, k_pipe);
 		while (obj_list != NULL) {
-			if (obj_list == &kpipe) {
-				obj_found = 1;
-				TC_PRINT("Found pipe object\n");
-				break;
+			if (obj_list == &kpipe || obj_list == &pipe) {
+				obj_found++;
+				if (obj_found == 2) {
+					TC_PRINT("Found pipe objects\n");
+					break;
+				}
 			}
 			obj_list = SYS_TRACING_NEXT(struct k_pipe, k_pipe,
 						    obj_list);
 		}
-		zassert_true(obj_found, "Didn't find pipe object");
+		zassert_equal(obj_found, 2, "Didn't find pipe objects");
 		break;
 	case QUEUE:
+		k_queue_init(&queue);
+
 		obj_list = SYS_TRACING_HEAD(struct k_queue, k_queue);
 		while (obj_list != NULL) {
-			if (obj_list == &kqueue) {
-				obj_found = 1;
-				TC_PRINT("Found queue object\n");
-				break;
+			if (obj_list == &kqueue || obj_list == &queue) {
+				obj_found++;
+				if (obj_found == 2) {
+					TC_PRINT("Found queue objects\n");
+					break;
+				}
 			}
 			obj_list = SYS_TRACING_NEXT(struct k_queue, k_queue,
 						    obj_list);
 		}
-		zassert_true(obj_found, "Didn't find queue object\n");
+		zassert_equal(obj_found, 2, "Didn't find queue objects\n");
 		break;
 	default:
 		zassert_unreachable("Undefined kernel object");
