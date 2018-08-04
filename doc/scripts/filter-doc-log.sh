727f92c5d5da0059bf6ffd049a1814181c6a1a6b
David B. Kinder <david.b.kinder@intel.com>
2018-08-04 11:53:30 -0700

    doc: update 1.9 release with RTD theme
    We've replaced the "zephyr-docs" theme with a simpler read-the-docs
    theme starting with the 1.13 release. This PR retrofits the theme
    changes back onto this 1.9 release and ignores attempts to use the
    zephyr-docs theme controlled by the make DOC_TAGS option.
    
    Also needed to backport changes to the warning filtering system
    implemented in later releases, fixes to how the config options were
    generated (genrest.py).
    
    Signed-off-by: David B. Kinder <david.b.kinder@intel.com>

---
+++ doc/scripts/filter-doc-log.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# run the filter-known-issues.py script to remove "expected" warning
+# messages from the output of the document build process and write
+# the filtered output to stdout
+#
+# Only argument is the name of the log file saved by the build.
+
+KI_SCRIPT=${ZEPHYR_BASE}/scripts/filter-known-issues.py
+CONFIG_DIR=${ZEPHYR_BASE}/.known-issues/doc
+
+LOG_FILE=$1
+
+red='\E[31m'
+green='\e[32m'
+
+if [ -z "${LOG_FILE}" ]; then
+        echo "Error in $0: missing input parameter <logfile>"
+        exit 1
+fi
+
+if [ -s "${LOG_FILE}" ]; then
+   $KI_SCRIPT --config-dir ${CONFIG_DIR} ${LOG_FILE} > doc.warnings 2>&1
+   if [ -s doc.warnings ]; then
+	   echo
+	   echo -e "${red}New errors/warnings found, please fix them:"
+	   echo -e "=============================================="
+	   tput sgr0
+	   echo
+	   cat doc.warnings
+	   echo
+   else
+	   echo -e "${green}No new errors/warnings."
+	   tput sgr0
+   fi
+
+else
+   echo "Error in $0: logfile \"${LOG_FILE}\" not found."
+   exit 1
+fi
