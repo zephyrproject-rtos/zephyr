1bcf1cf4756afebfdbba0f55a3d050052b90cc96
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-08-06 15:22:15 +0300

    samples: net: promiscuous: Convert to new logging system
    Use new logging system with support for network log level.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- samples/net/promiscuous_mode/src/main.c
+++ samples/net/promiscuous_mode/src/main.c
@@ -4,10 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define LOG_MODULE_NAME net_promisc
-#define SYS_LOG_DOMAIN "promisc"
-#define NET_SYS_LOG_LEVEL SYS_LOG_LEVEL_DEBUG
-#define NET_LOG_ENABLED 1
+#define LOG_MODULE_NAME net_promisc_sample
+#define NET_LOG_LEVEL LOG_LEVEL_DBG
 
 #include <zephyr.h>
 #include <errno.h>
