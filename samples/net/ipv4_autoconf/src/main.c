31a6d93c419337d3f26ff5f0bc8ddc8d52c50bf5
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-08-06 14:50:50 +0300

    samples: net: ipv4_autoconf: Convert to new logging system
    Use new logging system with support for network log level.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- samples/net/ipv4_autoconf/src/main.c
+++ samples/net/ipv4_autoconf/src/main.c
@@ -7,11 +7,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#if 1
-#define SYS_LOG_DOMAIN "ipv4ll"
-#define NET_SYS_LOG_LEVEL SYS_LOG_LEVEL_DEBUG
-#define NET_LOG_ENABLED 1
-#endif
+#define LOG_MODULE_NAME net_ipv4_autoconf_sample
+#define NET_LOG_LEVEL LOG_LEVEL_DBG
 
 #include <zephyr.h>
 #include <linker/sections.h>
