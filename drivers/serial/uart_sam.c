zephyrproject-rtos/zephyr

commit 57286afdd60bc0522aa09848f4862e930c75eb7d

drivers: uart: Allow to pass arbitrary user data to irq callback
Zephyr UART drivers offer very low-level functionality. Oftentimes,
it would be useful to provide higher-level wrappers around UART
device which would offer additional functionality. However, UART
driver irq callback routine receives just a pointer to (low-level)
UART device, and it's not possible to get to a wrapper structure
(without introducing expensive external mapping structures). This
is an indirect reason why the current UARt wrappers - uart_pipe,
console - are instantiated statically just for one underlying UART
device and cannot be reused for multiple devices.

Solve this by allowing to pass an arbitrary user data to irq
callback, set by new uart_irq_callback_user_data_set() function.
Existing uart_irq_callback_set() keeps setting a callback which
will receive pointer to the device.

While public API maintains compatibility, drivers themselves need
to be updated to support arbitrary user data storage/passing (as
legacy uart_irq_callback_set() functionality is now implemented in
terms of it).

Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

drivers/serial/uart_sam.c
@@ -80,7 +80,8 @@ struct uart_sam_dev_data {
 	u32_t baud_rate;
 
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
-	uart_irq_callback_t irq_cb;	/* Interrupt Callback */
+	uart_irq_callback_user_data_t irq_cb;	/* Interrupt Callback */
+	void *irq_cb_data;	/* Interrupt Callback Arg */
 #endif /* CONFIG_UART_INTERRUPT_DRIVEN */
 };
 
@@ -321,11 +322,13 @@ static int uart_sam_irq_update(struct device *dev)
 }
 
 static void uart_sam_irq_callback_set(struct device *dev,
-				      uart_irq_callback_t cb)
+				      uart_irq_callback_user_data_t cb,
+				      void *data)
 {
 	struct uart_sam_dev_data *const dev_data = DEV_DATA(dev);
 
 	dev_data->irq_cb = cb;
+	dev_data->irq_cb_data = cb_data;
 }
 
 static void uart_sam_isr(void *arg)
@@ -334,7 +337,7 @@ static void uart_sam_isr(void *arg)
 	struct uart_sam_dev_data *const dev_data = DEV_DATA(dev);
 
 	if (dev_data->irq_cb) {
-		dev_data->irq_cb(dev);
+		dev_data->irq_cb(dev_data->irq_cb_data);
 	}
 }
 

drivers/serial/uart_cc32xx.c
@@ -17,7 +17,8 @@
 
 struct uart_cc32xx_dev_data_t {
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
-	uart_irq_callback_t cb; /**< Callback function pointer */
+	uart_irq_callback_user_data_t cb; /**< Callback function pointer */
+	void *cb_data; /**< Callback function arg */
 #endif /* CONFIG_UART_INTERRUPT_DRIVEN */
 };
 
@@ -243,11 +244,13 @@ static int uart_cc32xx_irq_update(struct device *dev)
 }
 
 static void uart_cc32xx_irq_callback_set(struct device *dev,
-					 uart_irq_callback_t cb)
+					 uart_irq_callback_user_data_t cb,
+					 void *cb_data)
 {
 	struct uart_cc32xx_dev_data_t * const dev_data = DEV_DATA(dev);
 
 	dev_data->cb = cb;
+	dev_data->cb_data = cb_data;
 }
 
 /**
@@ -272,7 +275,7 @@ static void uart_cc32xx_isr(void *arg)
 						    1);
 
 	if (dev_data->cb) {
-		dev_data->cb(dev);
+		dev_data->cb(dev_data->cb_data);
 	}
 	/*
 	 * Clear interrupts only after cb called, as Zephyr UART clients expect

drivers/serial/uart_cmsdk_apb.c
@@ -63,7 +63,8 @@ struct uart_cmsdk_apb {
 struct uart_cmsdk_apb_dev_data {
 	u32_t baud_rate;	/* Baud rate */
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
-	uart_irq_callback_t irq_cb;
+	uart_irq_callback_user_data_t irq_cb;
+	void *irq_cb_data;
 #endif
 	/* UART Clock control in Active State */
 	const struct arm_clock_control_t uart_cc_as;
@@ -392,9 +393,11 @@ static int uart_cmsdk_apb_irq_update(struct device *dev)
  * @return N/A
  */
 static void uart_cmsdk_apb_irq_callback_set(struct device *dev,
-					    uart_irq_callback_t cb)
+					    uart_irq_callback_user_data_t cb,
+					    void *cb_data)
 {
 	DEV_DATA(dev)->irq_cb = cb;
+	DEV_DATA(dev)->irq_cb_data = cb_data;
 }
 
 /**
@@ -417,7 +420,7 @@ void uart_cmsdk_apb_isr(void *arg)
 
 	/* Verify if the callback has been registered */
 	if (data->irq_cb) {
-		data->irq_cb(dev);
+		data->irq_cb(data->irq_cb_data);
 	}
 }
 

drivers/serial/uart_fe310.c
@@ -58,7 +58,8 @@ struct uart_fe310_device_config {
 
 struct uart_fe310_data {
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
-	uart_irq_callback_t callback;
+	uart_irq_callback_user_data_t callback;
+	void *cb_data;
 #endif
 };
 
@@ -309,11 +310,13 @@ static int uart_fe310_irq_update(struct device *dev)
  * @return N/A
  */
 static void uart_fe310_irq_callback_set(struct device *dev,
-					uart_irq_callback_t cb)
+					uart_irq_callback_user_data_t cb,
+					void *cb_data)
 {
 	struct uart_fe310_data *data = DEV_DATA(dev);
 
 	data->callback = cb;
+	data->cb_data = cb_data;
 }
 
 static void uart_fe310_irq_handler(void *arg)
@@ -322,7 +325,7 @@ static void uart_fe310_irq_handler(void *arg)
 	struct uart_fe310_data *data = DEV_DATA(dev);
 
 	if (data->callback)
-		data->callback(dev);
+		data->callback(data->cb_data);
 }
 
 #endif /* CONFIG_UART_INTERRUPT_DRIVEN */

drivers/serial/uart_gecko.c
@@ -25,7 +25,8 @@ struct uart_gecko_config {
 
 struct uart_gecko_data {
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
-	uart_irq_callback_t callback;
+	uart_irq_callback_user_data_t callback;
+	void *cb_data;
 #endif
 };
 
@@ -203,11 +204,13 @@ static int uart_gecko_irq_update(struct device *dev)
 }
 
 static void uart_gecko_irq_callback_set(struct device *dev,
-				       uart_irq_callback_t cb)
+				       uart_irq_callback_user_data_t cb,
+				       void *cb_data)
 {
 	struct uart_gecko_data *data = dev->driver_data;
 
 	data->callback = cb;
+	data->cb_data = cb_data;
 }
 
 static void uart_gecko_isr(void *arg)
@@ -216,7 +219,7 @@ static void uart_gecko_isr(void *arg)
 	struct uart_gecko_data *data = dev->driver_data;
 
 	if (data->callback) {
-		data->callback(dev);
+		data->callback(data->cb_data);
 	}
 }
 #endif /* CONFIG_UART_INTERRUPT_DRIVEN */

drivers/serial/uart_imx.c
@@ -36,7 +36,8 @@ struct imx_uart_config {
 
 struct imx_uart_data {
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
-	uart_irq_callback_t callback;
+	uart_irq_callback_user_data_t callback;
+	void *cb_data;
 #endif
 };
 
@@ -228,11 +229,13 @@ static int uart_imx_irq_update(struct device *dev)
 }
 
 static void uart_imx_irq_callback_set(struct device *dev,
-		uart_irq_callback_t cb)
+		uart_irq_callback_user_data_t cb,
+		void *cb_data)
 {
 	struct imx_uart_data *data = dev->driver_data;
 
 	data->callback = cb;
+	data->cb_data = cb_data;
 }
 
 /**

drivers/serial/uart_mcux.c
@@ -23,7 +23,8 @@ struct uart_mcux_config {
 
 struct uart_mcux_data {
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
-	uart_irq_callback_t callback;
+	uart_irq_callback_user_data_t callback;
+	void *cb_data;
 #endif
 };
 
@@ -206,11 +207,13 @@ static int uart_mcux_irq_update(struct device *dev)
 }
 
 static void uart_mcux_irq_callback_set(struct device *dev,
-				       uart_irq_callback_t cb)
+				       uart_irq_callback_user_data_t cb,
+				       void *cb_data)
 {
 	struct uart_mcux_data *data = dev->driver_data;
 
 	data->callback = cb;
+	data->cb_data = cb_data;
 }
 
 static void uart_mcux_isr(void *arg)
@@ -219,7 +222,7 @@ static void uart_mcux_isr(void *arg)
 	struct uart_mcux_data *data = dev->driver_data;
 
 	if (data->callback) {
-		data->callback(dev);
+		data->callback(data->cb_data);
 	}
 }
 #endif /* CONFIG_UART_INTERRUPT_DRIVEN */

drivers/serial/uart_mcux_lpsci.c
@@ -23,7 +23,8 @@ struct mcux_lpsci_config {
 
 struct mcux_lpsci_data {
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
-	uart_irq_callback_t callback;
+	uart_irq_callback_user_data_t callback;
+	void *cb_data;
 #endif
 };
 
@@ -210,11 +211,13 @@ static int mcux_lpsci_irq_update(struct device *dev)
 }
 
 static void mcux_lpsci_irq_callback_set(struct device *dev,
-				       uart_irq_callback_t cb)
+				       uart_irq_callback_user_data_t cb,
+				       void *cb_data)
 {
 	struct mcux_lpsci_data *data = dev->driver_data;
 
 	data->callback = cb;
+	data->cb_data = cb_data;
 }
 
 static void mcux_lpsci_isr(void *arg)
@@ -223,7 +226,7 @@ static void mcux_lpsci_isr(void *arg)
 	struct mcux_lpsci_data *data = dev->driver_data;
 
 	if (data->callback) {
-		data->callback(dev);
+		data->callback(data->cb_data);
 	}
 }
 #endif /* CONFIG_UART_INTERRUPT_DRIVEN */

commit cf38bc60a829599b047e59c4b1edd443734fdc80

serial: sam: Fix compile error
commit 57286afdd60bc0522aa09848f4862e930c75eb7d introduced a compile
error in the uart_sam driver.  The function uart_sam_irq_callback_set
should have be updated to have the callback data variable called
cb_data instead of data.

Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

drivers/serial/uart_sam.c
@@ -323,7 +323,7 @@ static int uart_sam_irq_update(struct device *dev)
 
 static void uart_sam_irq_callback_set(struct device *dev,
 				      uart_irq_callback_user_data_t cb,
-				      void *data)
+				      void *cb_data)
 {
 	struct uart_sam_dev_data *const dev_data = DEV_DATA(dev);
 