cd3e5b561dbf741313b72863489899a364968088
Daniel Leung <daniel.leung@intel.com>
2018-08-08 09:14:53 -0700

    tests/kernel: kernel.queue.poll: fails if MAX_THREAD_BYTES > 2
    The kernel.queue.poll test fails if CONFIG_MAX_THREAD_BYTES is larger
    than 2, complaining about no memory for semaphore object. Turns out
    the memory pool is not large enough. So make it a bit larger.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- tests/kernel/queue/src/test_queue_user.c
+++ tests/kernel/queue/src/test_queue_user.c
@@ -15,7 +15,7 @@ static K_THREAD_STACK_DEFINE(child_stack, STACK_SIZE);
 static __kernel struct k_thread child_thread;
 static struct qdata qdata[LIST_LEN * 2];
 
-K_MEM_POOL_DEFINE(test_pool, 16, 64, 4, 4);
+K_MEM_POOL_DEFINE(test_pool, 16, 96, 4, 4);
 
 /**
  * @brief Tests for queue
