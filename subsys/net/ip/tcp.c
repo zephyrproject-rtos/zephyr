zephyrproject-rtos/zephyr

commit c6407659f3e66ec2aa5c25c3bfb975020e110766

net: tcp: Add the frag back to caller allocated net_pkt
The original fragment chain of incoming packet will be lost and leaked
in case of early error, add frag back to packet and
let the caller do unref.

Fixes #4323

Signed-off-by: june li <junelizh@foxmail.com>

subsys/net/ip/tcp.c
@@ -391,6 +391,8 @@ static struct net_pkt *prepare_segment(struct net_tcp *tcp,
 
 		if (pkt_allocated) {
 			net_pkt_unref(pkt);
+		} else {
+			pkt->frags = tail;
 		}
 
 		return NULL;
@@ -400,6 +402,8 @@ static struct net_pkt *prepare_segment(struct net_tcp *tcp,
 	if (!header) {
 		if (pkt_allocated) {
 			net_pkt_unref(pkt);
+		} else {
+			pkt->frags = tail;
 		}
 
 		return NULL;

commit 60fc163928050b7cb1404d204e05abd502d2196a

net: tcp: Fix net_buf leak in case of low available net_buf count
This net_buf leak happends when we are low on available net_buf
count. During TCP segment preparation we do allocate IP header
successfully, but we fail to allocate TCP header. In such case
pkt->frags is not NULL anymore (it contains IP header), but we
override it during TCP header allocation error path. This results
in net_buf containing IP header to never be deallocated, because
it does not belong to any net_pkt anymore.

Use net_pkt_frag_add() function to add tail for future net_pkt
deallocation, instead of assigning tail to pkt->frags pointer.

Fixes: c6407659f3e6 ("net: tcp: Add the frag back to caller allocated
  net_pkt")
Signed-off-by: Marcin Niestroj <m.niestroj@grinn-global.com>

subsys/net/ip/tcp.c
@@ -450,7 +450,7 @@ static int prepare_segment(struct net_tcp *tcp,
 		if (pkt_allocated) {
 			net_pkt_unref(pkt);
 		} else {
-			pkt->frags = tail;
+			net_pkt_frag_add(pkt, tail);
 		}
 
 		return -ENOMEM;