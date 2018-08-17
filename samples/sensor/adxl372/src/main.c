21e5dffdeb8156f5ab73e0f0177a66cef1fa3db0
Michael Hennerich <michael.hennerich@analog.com>
2018-08-17 09:31:42 +0200

    samples: sensor: adxl372: Add ADXL372 sample application
    This sample application produces slightly different outputs based on
    the chosen driver configuration mode. In Measuring Mode with trigger
    support, the acceleration on all three axis is printed in m/s^2 at
    the sampling rate (ODR). In polled Measuring Mode the instantaneous
    acceleration is polled every 2 seconds. In most high-g applications,
    a single (3-axis) acceleration sample at the peak of an impact event
    contains sufficient information about the event, and the full
    acceleration history is not required.
    In this Max Peak Detect Mode the device returns only the over
    threshold peak acceleration between two consecutive sample fetches or
    trigger events. Instead of printing the acceleration on all three axis,
    the sample application calculates the vector magnitude
    (root sum squared) and displays the result in g rather than in m/s^2,
    together with an bar graph.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>

---
+++ samples/sensor/adxl372/src/main.c
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2018 Analog Devices Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <sensor.h>
+#include <stdio.h>
+
+#define pow2(x) ((x) * (x))
+
+static double sqrt(double value)
+{
+	int i;
+	double sqrt = value / 3;
+
+	if (value <= 0) {
+		return 0;
+	}
+
+	for (i = 0; i < 6; i++) {
+		sqrt = (sqrt + value / sqrt) / 2;
+	}
+
+	return sqrt;
+}
+
+K_SEM_DEFINE(sem, 0, 1);
+
+static void trigger_handler(struct device *dev, struct sensor_trigger *trigger)
+{
+	ARG_UNUSED(trigger);
+
+	if (sensor_sample_fetch(dev)) {
+		printf("sensor_sample_fetch failed\n");
+		return;
+	}
+
+	k_sem_give(&sem);
+}
+
+void main(void)
+{
+	struct sensor_value accel[3];
+	double mag;
+	int i;
+	char meter[200];
+
+	struct device *dev = device_get_binding(CONFIG_ADXL372_DEV_NAME);
+
+	if (dev == NULL) {
+		printf("Could not get %s device\n", CONFIG_ADXL372_DEV_NAME);
+		return;
+	}
+
+	struct sensor_trigger trig = {
+		.type = SENSOR_TRIG_DATA_READY,
+		.chan = SENSOR_CHAN_ACCEL_XYZ,
+	};
+
+	if (IS_ENABLED(CONFIG_ADXL372_PEAK_DETECT_MODE)) {
+		trig.type = SENSOR_TRIG_THRESHOLD;
+	}
+
+	if (IS_ENABLED(CONFIG_ADXL372_TRIGGER)) {
+		if (sensor_trigger_set(dev, &trig, trigger_handler)) {
+			printf("Could not set trigger\n");
+			return;
+		}
+	}
+
+	while (1) {
+		if (IS_ENABLED(CONFIG_ADXL372_TRIGGER)) {
+			if (IS_ENABLED(CONFIG_ADXL372_PEAK_DETECT_MODE)) {
+				printf("Waiting for a threshold event\n");
+			}
+			k_sem_take(&sem, K_FOREVER);
+		} else {
+			if (sensor_sample_fetch(dev)) {
+				printf("sensor_sample_fetch failed\n");
+			}
+		}
+
+		sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, accel);
+
+		if (IS_ENABLED(CONFIG_ADXL372_PEAK_DETECT_MODE)) {
+			mag = sqrt(pow2(sensor_ms2_to_g(&accel[0])) +
+				pow2(sensor_ms2_to_g(&accel[1])) +
+				pow2(sensor_ms2_to_g(&accel[2])));
+
+			for (i = 0; i <= mag && i < (sizeof(meter) - 1); i++) {
+				meter[i] = '#';
+			}
+
+			meter[i] = '\0';
+
+			printf("%6.2f g: %s\n", mag, meter);
+		} else {
+			printf("AX=%10.2f AY=%10.2f AZ=%10.2f (m/s^2)\n",
+				sensor_value_to_double(&accel[0]),
+				sensor_value_to_double(&accel[1]),
+				sensor_value_to_double(&accel[2]));
+		}
+
+		if (!IS_ENABLED(CONFIG_ADXL372_TRIGGER)) {
+			k_sleep(2000);
+		}
+	}
+}
