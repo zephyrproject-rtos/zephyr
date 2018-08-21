50e05a544eb82ba476c4a06d6b4712a74a419978
Emanuele Di Santo <emdi@nordicsemi.no>
2018-08-21 15:46:51 +0200

    boards: arm: nrf52832_pca10040: use dts for LED and button config
    The LEDs, buttons and necessary aliases are now configured via dts.
    This commit breaks the sample under samples/boards/nrf52/power_mgr.
    
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>

--- boards/arm/nrf52_pca10040/board.h
+++ boards/arm/nrf52_pca10040/board.h
@@ -9,40 +9,4 @@
 
 #include <soc.h>
 
-/* Push button switch 0 */
-#define SW0_GPIO_PIN     13
-#define SW0_GPIO_NAME    CONFIG_GPIO_P0_DEV_NAME
-#define SW0_GPIO_PIN_PUD GPIO_PUD_PULL_UP
-
-/* Push button switch 1 */
-#define SW1_GPIO_PIN     14
-#define SW1_GPIO_NAME    CONFIG_GPIO_P0_DEV_NAME
-#define SW1_GPIO_PIN_PUD GPIO_PUD_PULL_UP
-
-/* Push button switch 2 */
-#define SW2_GPIO_PIN     15
-#define SW2_GPIO_NAME    CONFIG_GPIO_P0_DEV_NAME
-#define SW2_GPIO_PIN_PUD GPIO_PUD_PULL_UP
-
-/* Push button switch 3 */
-#define SW3_GPIO_PIN     16
-#define SW3_GPIO_NAME    CONFIG_GPIO_P0_DEV_NAME
-#define SW3_GPIO_PIN_PUD GPIO_PUD_PULL_UP
-
-/* Onboard GREEN LED 0 */
-#define LED0_GPIO_PIN  17
-#define LED0_GPIO_PORT CONFIG_GPIO_P0_DEV_NAME
-
-/* Onboard GREEN LED 1 */
-#define LED1_GPIO_PIN  18
-#define LED1_GPIO_PORT CONFIG_GPIO_P0_DEV_NAME
-
-/* Onboard GREEN LED 2 */
-#define LED2_GPIO_PIN  19
-#define LED2_GPIO_PORT CONFIG_GPIO_P0_DEV_NAME
-
-/* Onboard GREEN LED 3 */
-#define LED3_GPIO_PIN  20
-#define LED3_GPIO_PORT CONFIG_GPIO_P0_DEV_NAME
-
 #endif /* __INC_BOARD_H */
