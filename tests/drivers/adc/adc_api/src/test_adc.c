c640996536e8ff94f2c676de232d4c2dd36ab00c
Punit Vara <punit.vara@intel.com>
2018-08-24 10:25:19 +0530

    tests: adc_api: Add ARC related parameters for ADC
    Add ARC related parameters and modified test case to work
    this test case on arc sensor subsystem.
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>

--- tests/drivers/adc/adc_api/src/test_adc.c
+++ tests/drivers/adc/adc_api/src/test_adc.c
@@ -104,6 +104,16 @@
 #define ADC_1ST_CHANNEL_ID	0
 #define ADC_1ST_CHANNEL_INPUT	0
 
+#elif defined(CONFIG_BOARD_QUARK_SE_C1000_DEVBOARD_SS) || \
+	defined(CONFIG_BOARD_ARDUINO_101_SSS)
+#define ADC_DEVICE_NAME		CONFIG_ADC_0_NAME
+#define ADC_RESOLUTION		10
+#define ADC_GAIN		ADC_GAIN_1
+#define ADC_REFERENCE		ADC_REF_INTERNAL
+#define ADC_ACQUISITION_TIME	ADC_ACQ_TIME_DEFAULT
+#define ADC_1ST_CHANNEL_ID	10
+#define ADC_2ND_CHANNEL_ID	11
+
 #else
 #error "Unsupported board."
 #endif
@@ -180,7 +190,19 @@ static void check_samples(int expected_count)
 static int test_task_one_channel(void)
 {
 	int ret;
+
+#if defined(CONFIG_BOARD_QUARK_SE_C1000_DEVBOARD_SS) || \
+	defined(CONFIG_BOARD_ARDUINO_101_SSS)
+	const struct adc_sequence_options options = {
+		.interval_us     = 10,
+		.extra_samplings = 0,
+	};
+#endif
 	const struct adc_sequence sequence = {
+#if defined(CONFIG_BOARD_QUARK_SE_C1000_DEVBOARD_SS) || \
+	defined(CONFIG_BOARD_ARDUINO_101_SSS)
+		.options     = &options,
+#endif
 		.channels    = BIT(ADC_1ST_CHANNEL_ID),
 		.buffer      = m_sample_buffer,
 		.buffer_size = sizeof(m_sample_buffer),
@@ -212,7 +234,19 @@ void test_adc_sample_one_channel(void)
 static int test_task_two_channels(void)
 {
 	int ret;
+
+#if defined(CONFIG_BOARD_QUARK_SE_C1000_DEVBOARD_SS) || \
+	defined(CONFIG_BOARD_ARDUINO_101_SSS)
+	const struct adc_sequence_options options = {
+		.interval_us     = 30,
+		.extra_samplings = 0,
+	};
+#endif
 	const struct adc_sequence sequence = {
+#if defined(CONFIG_BOARD_QUARK_SE_C1000_DEVBOARD_SS) || \
+	defined(CONFIG_BOARD_ARDUINO_101_SSS)
+		.options     = &options,
+#endif
 		.channels    = BIT(ADC_1ST_CHANNEL_ID) |
 			       BIT(ADC_2ND_CHANNEL_ID),
 		.buffer      = m_sample_buffer,
@@ -234,6 +268,7 @@ static int test_task_two_channels(void)
 	return TC_PASS;
 }
 #endif /* defined(ADC_2ND_CHANNEL_ID) */
+
 void test_adc_sample_two_channels(void)
 {
 #if defined(ADC_2ND_CHANNEL_ID)
@@ -253,6 +288,7 @@ static int test_task_asynchronous_call(void)
 	int ret;
 	const struct adc_sequence_options options = {
 		.extra_samplings = 4,
+		.interval_us = 10,
 	};
 	const struct adc_sequence sequence = {
 		.options     = &options,
@@ -392,6 +428,7 @@ static int test_task_repeated_samplings(void)
 		 * Hence, the third sampling will not take place.
 		 */
 		.extra_samplings = 2,
+		.interval_us = 15,
 	};
 	const struct adc_sequence sequence = {
 		.options     = &options,
