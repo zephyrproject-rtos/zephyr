5e467bf9a479b66b718929289019768c34d1227e
Maureen Helm <maureen.helm@nxp.com>
2018-08-25 14:53:56 -0500

    samples: nvs: Use flash erase block size from dts
    The nvs sample assumed a 1 KB flash erase block size, which caused the
    sample to fail on frdm_k64f because its erase block size is 4 KB. Get
    the erase block size from dts instead.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- samples/subsys/nvs/src/main.c
+++ samples/subsys/nvs/src/main.c
@@ -45,7 +45,7 @@
 #include <string.h>
 #include <nvs/nvs.h>
 
-#define NVS_SECTOR_SIZE 1024 /* Multiple of FLASH_PAGE_SIZE */
+#define NVS_SECTOR_SIZE FLASH_ERASE_BLOCK_SIZE /* Multiple of FLASH_PAGE_SIZE */
 #define NVS_SECTOR_COUNT 3 /* At least 2 sectors */
 #define NVS_STORAGE_OFFSET FLASH_AREA_STORAGE_OFFSET /* Start address of the
 						      * filesystem in flash
