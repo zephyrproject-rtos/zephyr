511edf0175790656dfdeefd7ab0f0a80ced4816b
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-08-28 14:57:43 +0530

    Revert "sam_e70: enable instruction and data caches on sam_e70"
    This reverts commit c0907762f3f15116732f434937fc3a5737c3d40b.
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>

--- arch/arm/soc/atmel_sam/same70/soc.c
+++ arch/arm/soc/atmel_sam/same70/soc.c
@@ -227,11 +227,6 @@ static int atmel_same70_init(struct device *arg)
 
 	key = irq_lock();
 
-	SCB_EnableICache();
-	if (!(SCB->CCR & SCB_CCR_DC_Msk)) {
-		SCB_EnableDCache();
-	}
-
 	/* Clear all faults */
 	_ClearFaults();
 
