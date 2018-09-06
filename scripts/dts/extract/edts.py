05b4673d57346cb16a623c9a312917d95f19302f
Bobby Noelte <b0661n0e17e@gmail.com>
2018-09-06 10:48:43 +0200

    scripts: extract_dts_includes.py: create EDTS database for codegen
    Create extended DTS database to be used by codegen and others.
    
    Signed-off-by: Bobby Noelte <b0661n0e17e@gmail.com>

---
+++ scripts/dts/extract/edts.py
@@ -0,0 +1,44 @@
+#
+# Copyright (c) 2018 Bobby Noelte
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from .globals import *
+import edtsdatabase
+
+edts = edtsdatabase.EDTSDatabase()
+
+##
+# @brief Get EDTS device id associated to node address.
+#
+# @return ETDS device id
+def edts_device_id(node_address):
+	return node_address
+
+##
+# @brief Insert device property into EDTS
+#
+def edts_insert_device_property(node_address, property_path, property_value):
+    device_id = edts_device_id(node_address)
+    edts.insert_device_property(device_id, property_path, property_value)
+
+##
+# @brief Insert device parent-device property into EDTS
+#
+def edts_insert_device_parent_device_property(node_address):
+    # Check for a parent device this device is subordinated
+    parent_device_id = None
+    parent_node_address = ''
+    for comp in node_address.split('/')[1:-1]:
+        parent_node_address += '/' + comp
+        compatibles = reduced[parent_node_address]['props'] \
+                .get('compatible', None)
+        if compatibles:
+            # there is a parent device,
+            # only use the ones that have a minimum control on the child
+            if 'simple-bus' not in compatibles:
+                parent_device_id = edts_device_id(parent_node_address)
+    if parent_device_id:
+        edts_insert_device_property(edts_device_id(node_address),
+                    'parent-device', parent_device_id)
