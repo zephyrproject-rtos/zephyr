0a795ce40a0ea2cd31e0baf3c66bd588e81e868e
Robert Lubos <robert.lubos@nordicsemi.no>
2018-09-06 10:28:55 +0200

    net: mqtt: Add TLS socket transport
    Add TLS transport to socket MQTT implementation.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/net/lib/mqtt_sock/mqtt_transport.c
+++ subsys/net/lib/mqtt_sock/mqtt_transport.c
@@ -19,6 +19,16 @@ extern int mqtt_client_tcp_read(struct mqtt_client *client, u8_t *data,
 				u32_t buflen);
 extern int mqtt_client_tcp_disconnect(struct mqtt_client *client);
 
+#if defined(CONFIG_MQTT_LIB_TLS)
+/* Transport handler functions for TLS socket transport. */
+extern int mqtt_client_tls_connect(struct mqtt_client *client);
+extern int mqtt_client_tls_write(struct mqtt_client *client, const u8_t *data,
+				 u32_t datalen);
+extern int mqtt_client_tls_read(struct mqtt_client *client, u8_t *data,
+				u32_t buflen);
+extern int mqtt_client_tls_disconnect(struct mqtt_client *client);
+#endif /* CONFIG_MQTT_LIB_TLS */
+
 /**@brief Function pointer array for TCP/TLS transport handlers. */
 const struct transport_procedure transport_fn[MQTT_TRANSPORT_NUM] = {
 	{
@@ -26,7 +36,15 @@ const struct transport_procedure transport_fn[MQTT_TRANSPORT_NUM] = {
 		mqtt_client_tcp_write,
 		mqtt_client_tcp_read,
 		mqtt_client_tcp_disconnect,
+	},
+#if defined(CONFIG_MQTT_LIB_TLS)
+	{
+		mqtt_client_tls_connect,
+		mqtt_client_tls_write,
+		mqtt_client_tls_read,
+		mqtt_client_tls_disconnect,
 	}
+#endif /* CONFIG_MQTT_LIB_TLS */
 };
 
 int mqtt_transport_connect(struct mqtt_client *client)
