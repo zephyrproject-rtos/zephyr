215e57e808afc00d9bd7f369d103633ae918b6dd
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2018-09-13 12:52:32 +0300

    logger: Add native backend for native_posix
    Add backend to be used in native_posix arch.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

---
+++ include/logging/log_backend_native_posix.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef LOG_BACKEND_NATIVE_POSIX_H
+#define LOG_BACKEND_NATIVE_POSIX_H
+
+#include <logging/log_backend.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const struct log_backend_api log_backend_native_posix_api;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LOG_BACKEND_NATIVE_POSIX_H */
