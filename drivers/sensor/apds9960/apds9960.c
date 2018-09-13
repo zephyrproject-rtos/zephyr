bb3b0b2199b945c8c6b103808b7fd0fb70389f32
Johann Fischer <j.fischer@phytec.de>
2018-09-13 16:00:33 +0200

    driver: apds9960: reorganize ALS and proximity setups
    Refactor and reorganize ALS and proximity setup functions.
    Cleanup and reorder register, fix register fields.
    
    Signed-off-by: Johann Fischer <j.fischer@phytec.de>

--- drivers/sensor/apds9960/apds9960.c
+++ drivers/sensor/apds9960/apds9960.c
@@ -77,323 +77,188 @@ static int apds9960_channel_get(struct device *dev,
 	return 0;
 }
 
-static int apds9960_setproxint_lowthresh(struct device *dev, u8_t threshold)
-{
-	struct apds9960_data *data = dev->driver_data;
-	int temp = 0;
-
-	temp = i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_PILT_REG, threshold);
-	if (temp < 0) {
-		SYS_LOG_ERR(" Failed");
-		return temp;
-	}
-
-	return 0;
-}
-
-static int apds9960_setproxint_highthresh(struct device *dev, u8_t threshold)
-{
-	struct apds9960_data *data = dev->driver_data;
-	int temp = 0;
-
-	temp = i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_PIHT_REG, threshold);
-	if (temp < 0) {
-		SYS_LOG_ERR(" Failed");
-		return temp;
-	}
-
-	return 0;
-}
-
-static int apds9960_setlightint_lowthresh(struct device *dev, u16_t threshold)
-{
-	struct apds9960_data *data = dev->driver_data;
-	int temp = 0;
-	u8_t val_low;
-	u8_t val_high;
-
-	/* Break 16-bit threshold into 2 8-bit values */
-	val_low = threshold & 0x00FF;
-	val_high = (threshold & 0xFF00) >> 8;
-
-	temp = i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_INT_AILTL_REG, val_low);
-	if (temp < 0) {
-		SYS_LOG_ERR(" Failed");
-		return temp;
-	}
-
-	temp = i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_INT_AILTH_REG, val_high);
-	if (temp < 0) {
-		SYS_LOG_ERR(" Failed");
-		return temp;
-	}
-
-	return 0;
-}
-
-static int apds9960_setlightint_highthresh(struct device *dev, u16_t threshold)
-{
-	struct apds9960_data *data = dev->driver_data;
-	int temp = 0;
-	u8_t val_low;
-	u8_t val_high;
-
-	/* Break 16-bit threshold into 2 8-bit values */
-	val_low = threshold & 0x00FF;
-	val_high = (threshold & 0xFF00) >> 8;
-
-	temp = i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_INT_AIHTL_REG, val_low);
-	if (temp < 0) {
-		SYS_LOG_ERR(" Failed");
-		return temp;
-	}
-
-	temp = i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_INT_AIHTH_REG, val_high);
-	if (temp < 0) {
-		SYS_LOG_ERR(" Failed");
-		return temp;
-	}
-
-	return 0;
-}
-
 static int apds9960_proxy_setup(struct device *dev, int gain)
 {
 	struct apds9960_data *data = dev->driver_data;
-	int temp = 0;
-	u8_t mask = APDS9960_ENABLE_PROXY | APDS9960_ENABLE_PIEN;
-	u8_t val = APDS9960_PROXY_ON;
 
-	/* Power ON */
-	temp = i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_ENABLE_REG, BIT(0), APDS9960_POWER_ON);
-	if (temp < 0) {
-		SYS_LOG_ERR("Power on bit not set.");
-		return temp;
+	if (i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
+			       APDS9960_POFFSET_UR_REG,
+			       APDS9960_DEFAULT_POFFSET_UR)) {
+		SYS_LOG_ERR("Default offset UR not set ");
+		return -EIO;
 	}
 
-	/* ADC value */
-	temp = i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_ATIME_REG, APDS9960_ATIME_WRTIE, APDS9960_ADC_VALUE);
-	if (temp < 0) {
-		SYS_LOG_ERR("ADC bits are not written");
-		return temp;
+	if (i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
+			       APDS9960_POFFSET_DL_REG,
+			       APDS9960_DEFAULT_POFFSET_DL)) {
+		SYS_LOG_ERR("Default offset DL not set ");
+		return -EIO;
 	}
 
-	/* proxy Gain */
-	temp = i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_CONTROL_REG, APDS9960_CONTROL_PGAIN,
-		(gain & APDS9960_PGAIN_8X));
-
-	if (temp < 0) {
-		SYS_LOG_ERR("proxy Gain is not set");
-		return temp;
+	if (i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
+			       APDS9960_PPULSE_REG,
+			       APDS9960_DEFAULT_PROX_PPULSE)) {
+		SYS_LOG_ERR("Default pulse count not set ");
+		return -EIO;
 	}
 
-	/* Enable proxy */
-	temp = i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_ENABLE_REG, mask, val);
-	if (temp < 0) {
-		SYS_LOG_ERR("Proxy is not enabled");
-		return temp;
+	if (i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
+				APDS9960_CONTROL_REG,
+				APDS9960_CONTROL_LDRIVE,
+				APDS9960_DEFAULT_LDRIVE)) {
+		SYS_LOG_ERR("LED Drive Strength not set");
+		return -EIO;
 	}
 
-	return temp;
+	if (i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
+				APDS9960_CONTROL_REG, APDS9960_CONTROL_PGAIN,
+				(gain & APDS9960_PGAIN_8X))) {
+		SYS_LOG_ERR("Gain is not set");
+		return -EIO;
+	}
+
+	if (i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
+			       APDS9960_PILT_REG, APDS9960_DEFAULT_PILT)) {
+		SYS_LOG_ERR("Low threshold not set");
+		return -EIO;
+	}
+
+	if (i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
+			       APDS9960_PIHT_REG, APDS9960_DEFAULT_PIHT)) {
+		SYS_LOG_ERR("High threshold not set");
+		return -EIO;
+	}
+
+	if (i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
+				APDS9960_ENABLE_REG, APDS9960_ENABLE_PEN,
+				APDS9960_ENABLE_PEN)) {
+		SYS_LOG_ERR("Proximity mode is not enabled");
+		return -EIO;
+	}
+
+	return 0;
 }
 
 static int apds9960_ambient_setup(struct device *dev, int gain)
 {
 	struct apds9960_data *data = dev->driver_data;
-	int temp = 0;
+	u16_t th;
 
-	/* Power ON */
-	temp = i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_ENABLE_REG, BIT(0), APDS9960_POWER_ON);
-	if (temp < 0) {
-		SYS_LOG_ERR("Power on bit not set.");
+	/* ADC value */
+	if (i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
+			       APDS9960_ATIME_REG, APDS9960_DEFAULT_ATIME)) {
+		SYS_LOG_ERR("Default integration time not set for ADC");
 		return -EIO;
 	}
 
-	/* ADC value */
-	temp = i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_ATIME_REG, APDS9960_ATIME_WRTIE, APDS9960_ADC_VALUE);
-	if (temp < 0) {
-		SYS_LOG_ERR("ADC bits are not written");
-		return temp;
-	}
-
-	/* ALE Gain */
-	temp = i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_CONTROL_REG, APDS9960_CONTROL_AGAIN,
-		(gain & APDS9960_AGAIN_64X));
-	if (temp < 0) {
+	/* ALS Gain */
+	if (i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
+				APDS9960_CONTROL_REG,
+				APDS9960_CONTROL_AGAIN,
+				(gain & APDS9960_AGAIN_64X))) {
 		SYS_LOG_ERR("Ambient Gain is not set");
-		return temp;
+		return -EIO;
 	}
 
-	temp = i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_ENABLE_REG, BIT(4), 0x00);
-	if (temp < 0) {
-		return temp;
+	th = sys_cpu_to_le16(APDS9960_DEFAULT_AILT);
+	if (i2c_burst_write(data->i2c, APDS9960_I2C_ADDRESS,
+			    APDS9960_INT_AILTL_REG,
+			    (u8_t *)&th, sizeof(th))) {
+		SYS_LOG_ERR("ALS low threshold not set");
+		return -EIO;
 	}
 
-	/* Enable ALE */
-	temp = i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_ENABLE_REG, APDS9960_ENABLE_ALE, APDS9960_RGB_ON);
-	if (temp < 0) {
-		SYS_LOG_ERR("Proxy is not enabled");
-		return temp;
+	th = sys_cpu_to_le16(APDS9960_DEFAULT_AIHT);
+	if (i2c_burst_write(data->i2c, APDS9960_I2C_ADDRESS,
+			    APDS9960_INT_AIHTL_REG,
+			    (u8_t *)&th, sizeof(th))) {
+		SYS_LOG_ERR("ALS low threshold not set");
+		return -EIO;
+	}
+
+	/* Enable ALS */
+	if (i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
+				APDS9960_ENABLE_REG, APDS9960_ENABLE_AEN,
+				APDS9960_ENABLE_AEN)) {
+		SYS_LOG_ERR("ALS is not enabled");
+		return -EIO;
 	}
 
 	return 0;
 }
 
-static int apds9960_sensor_setup(struct device *dev, int gain)
+static int apds9960_sensor_setup(struct device *dev)
 {
 	struct apds9960_data *data = dev->driver_data;
-	int temp = 0;
 	u8_t chip_id;
 
-	temp = i2c_reg_read_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_ID_REG, &chip_id);
-	if (temp < 0) {
-		SYS_LOG_ERR("failed reading chip id");
-		return temp;
+	if (i2c_reg_read_byte(data->i2c, APDS9960_I2C_ADDRESS,
+			      APDS9960_ID_REG, &chip_id)) {
+		SYS_LOG_ERR("Failed reading chip id");
+		return -EIO;
 	}
 
 	if (!((chip_id == APDS9960_ID_1) || (chip_id == APDS9960_ID_2))) {
-		SYS_LOG_ERR("invalid chip id 0x%x", chip_id);
+		SYS_LOG_ERR("Invalid chip id 0x%x", chip_id);
 		return -EIO;
 	}
 
-	temp = i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_ENABLE_REG, APDS9960_ALL_BITS,
-		APDS9960_MODE_OFF);
-	if (temp < 0) {
-		SYS_LOG_ERR("ENABLE registered is not cleared");
-		return temp;
-	}
-
-	temp = i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_ATIME_REG, APDS9960_DEFAULT_ATIME);
-	if (temp < 0) {
-		SYS_LOG_ERR("Default integration time not set for ADC");
-		return temp;
-	}
-
-	temp = i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_WTIME_REG, APDS9960_DEFAULT_WTIME);
-	if (temp < 0) {
-		SYS_LOG_ERR("Default wait time not set ");
-		return temp;
-	}
-
-	temp = i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_PPULSE_REG, APDS9960_DEFAULT_PROX_PPULSE);
-	if (temp < 0) {
-		SYS_LOG_ERR("Default proximity ppulse not set ");
-		return temp;
-	}
-
-	temp = i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_POFFSET_UR_REG, APDS9960_DEFAULT_POFFSET_UR);
-	if (temp < 0) {
-		SYS_LOG_ERR("Default poffset UR not set ");
-		return temp;
-	}
-
-	temp = i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_POFFSET_DL_REG, APDS9960_DEFAULT_POFFSET_DL);
-	if (temp < 0) {
-		SYS_LOG_ERR("Default poffset DL not set ");
-		return temp;
-	}
-
-	temp = i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_CONFIG1_REG, APDS9960_DEFAULT_CONFIG1);
-	if (temp < 0) {
-		SYS_LOG_ERR("Default config1 not set ");
-		return temp;
-	}
-
-	temp = i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_CONTROL_REG, APDS9960_CONTROL_LDRIVE,
-		APDS9960_DEFAULT_LDRIVE);
-	if (temp < 0) {
-		SYS_LOG_ERR("LEDdrive not set");
+	/* Disable all functions and interrupts */
+	if (i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
+			       APDS9960_ENABLE_REG, 0)) {
+		SYS_LOG_ERR("ENABLE register is not cleared");
 		return -EIO;
 	}
 
-	temp = i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_CONTROL_REG, APDS9960_CONTROL_PGAIN,
-		(gain & APDS9960_DEFAULT_PGAIN));
-	if (temp < 0) {
-		SYS_LOG_ERR("proxy Gain is not set");
-		return temp;
+	/* Disable gesture interrupt */
+	if (i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
+			       APDS9960_GCONFIG4_REG, 0)) {
+		SYS_LOG_ERR("GCONFIG4 register is not cleared");
+		return -EIO;
 	}
 
-	temp = i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_CONTROL_REG, APDS9960_CONTROL_AGAIN,
-		(gain & APDS9960_DEFAULT_AGAIN));
-	if (temp < 0) {
-		SYS_LOG_ERR("Ambient Gain is not set");
-		return temp;
+	if (i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
+			       APDS9960_WTIME_REG, APDS9960_DEFAULT_WTIME)) {
+		SYS_LOG_ERR("Default wait time not set");
+		return -EIO;
 	}
 
-	temp = apds9960_setproxint_lowthresh(dev, APDS9960_DEFAULT_PILT);
-	if (temp < 0) {
-		SYS_LOG_ERR("prox low threshold not set");
-		return temp;
+	if (i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
+			       APDS9960_CONFIG1_REG,
+			       APDS9960_DEFAULT_CONFIG1)) {
+		SYS_LOG_ERR("Default WLONG not set");
+		return -EIO;
 	}
 
-	temp = apds9960_setproxint_highthresh(dev, APDS9960_DEFAULT_PIHT);
-	if (temp < 0) {
-		SYS_LOG_ERR("prox high threshold not set");
-		return temp;
+	if (i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
+			       APDS9960_CONFIG2_REG,
+			       APDS9960_DEFAULT_CONFIG2)) {
+		SYS_LOG_ERR("Configuration Register Two not set");
+		return -EIO;
 	}
 
-	temp = apds9960_setlightint_lowthresh(dev, APDS9960_DEFAULT_AILT);
-	if (temp < 0) {
-		SYS_LOG_ERR("light low threshold not set");
-		return temp;
+	if (i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
+			       APDS9960_PERS_REG,
+			       APDS9960_DEFAULT_PERS)) {
+		SYS_LOG_ERR("Interrupt persistence not set");
+		return -EIO;
 	}
 
-	temp = apds9960_setlightint_highthresh(dev, APDS9960_DEFAULT_AIHT);
-	if (temp < 0) {
-		SYS_LOG_ERR("light high threshold not set");
-		return temp;
-	}
-
-	temp = i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_PERS_REG, APDS9960_DEFAULT_PERS);
-	if (temp < 0) {
-		SYS_LOG_ERR("ALS interrupt persistence not set ");
-		return temp;
-	}
-
-	temp = i2c_reg_write_byte(data->i2c, APDS9960_I2C_ADDRESS,
-		APDS9960_CONFIG2_REG, APDS9960_DEFAULT_CONFIG2);
-	if (temp < 0) {
-		SYS_LOG_ERR("clear diode saturation interrupt is not enabled");
-		return temp;
-	}
-
-	temp = apds9960_proxy_setup(dev, gain);
-	if (temp < 0) {
+	if (apds9960_proxy_setup(dev, APDS9960_DEFAULT_PGAIN)) {
 		SYS_LOG_ERR("Failed to setup proximity functionality");
-		return temp;
+		return -EIO;
 	}
 
-	temp = apds9960_ambient_setup(dev, gain);
-	if (temp < 0) {
+	if (apds9960_ambient_setup(dev, APDS9960_DEFAULT_AGAIN)) {
 		SYS_LOG_ERR("Failed to setup ambient light functionality");
-		return temp;
+		return -EIO;
+	}
+
+	if (i2c_reg_update_byte(data->i2c, APDS9960_I2C_ADDRESS,
+				APDS9960_ENABLE_REG,
+				APDS9960_ENABLE_PON, APDS9960_ENABLE_PON)) {
+		SYS_LOG_ERR("Power on bit not set");
+		return -EIO;
 	}
 
 	return 0;
@@ -407,7 +272,6 @@ static const struct sensor_driver_api apds9960_driver_api = {
 static int apds9960_init(struct device *dev)
 {
 	struct apds9960_data *data = dev->driver_data;
-	int als_gain = 0;
 
 	data->i2c = device_get_binding(CONFIG_APDS9960_I2C_DEV_NAME);
 
@@ -420,7 +284,7 @@ static int apds9960_init(struct device *dev)
 	(void)memset(data->sample_crgb, 0, sizeof(data->sample_crgb));
 	data->pdata = 0;
 
-	apds9960_sensor_setup(dev, als_gain);
+	apds9960_sensor_setup(dev);
 
 	return 0;
 }
