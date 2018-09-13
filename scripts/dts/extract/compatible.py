a885e04b5c3ce9162d6e19f9fe846b2782917ca0
Erwan Gouriou <erwan.gouriou@linaro.org>
2018-09-13 17:28:07 +0200

    scripts: extract_dts_includes.py: extract compatible to EDTS database
    Get compatible info available by EDTS.
    This way the compatible info of all activated devices can be used in
    drivers.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- scripts/dts/extract/compatible.py
+++ scripts/dts/extract/compatible.py
@@ -5,6 +5,7 @@
 #
 
 from extract.globals import *
+from extract.edts import *
 from extract.directive import DTDirective
 
 ##
@@ -13,11 +14,25 @@ from extract.directive import DTDirective
 # Handles:
 # - compatible
 #
+# Generates in EDTS:
+# - compatible/<index> : compatible
 class DTCompatible(DTDirective):
 
     def __init__(self):
         pass
 
+    ##
+    # @brief Populate EDTS compatible information
+    #
+    # @param node_address Address of node owning the
+    #                     compatible definition.
+    #
+    def populate_edts(self, node_address, compatible):
+        # generate EDTS
+        device_id = edts_device_id(node_address)
+        for i, comp in enumerate(compatible):
+            edts_insert_device_property(device_id, 'compatible/{}'.format(i),
+					comp)
     ##
     # @brief Extract compatible
     #
