ba5476a2a07cd21e2a5b7c1802388648349d942d
Alberto Escolar Piedras <alpi@oticon.com>
2018-09-14 09:15:04 +0200

    native_posix: Add control of traces color
    For native_posix:
    Added command line options to control if traces should have
    or not colors.
    +
    Detect if we are connected to a tty or not, and if we are
    set the defaults for that option appropriately
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>

--- arch/posix/include/posix_trace.h
+++ arch/posix/include/posix_trace.h
@@ -13,6 +13,19 @@ extern "C" {
 void posix_print_error_and_exit(const char *format, ...);
 void posix_print_warning(const char *format, ...);
 void posix_print_trace(const char *format, ...);
+/**
+ * Return 1 if traces to <output> will go to a tty.
+ * When printing to a terminal we may use ASCII escapes for color or other
+ * niceties.
+ * But when redirecting to files, or piping to other commands, those should be
+ * disabled by default.
+ *
+ * Where the <output> should be set to 0 to query about posix_print_trace output
+ * (typically STDOUT)
+ * and 1 to query about the warning and error output (posix_print_error/warning)
+ * outputs (typically STDERR)
+ */
+int posix_trace_over_tty(int output);
 
 #ifdef __cplusplus
 }
