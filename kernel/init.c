a632efc93f1bf38ed2678bf3c0eec4804792e158
Anas Nashif <anas.nashif@intel.com>
2018-09-17 06:58:09 -0500

    kernel: move to new logger
    Use the new logger framework for kernel.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- kernel/init.c
+++ kernel/init.c
@@ -35,6 +35,9 @@
 #include <stdbool.h>
 
 #define IDLE_THREAD_NAME	"idle"
+#define LOG_LEVEL CONFIG_KERNEL_LOG_LEVEL
+#include <logging/log.h>
+LOG_MODULE_REGISTER(kernel);
 
 /* boot banner items */
 #if defined(CONFIG_BOOT_DELAY) && CONFIG_BOOT_DELAY > 0
