22abc97dd72a75b81c32b6ce65671af99528aad3
Erwan Gouriou <erwan.gouriou@linaro.org>
2018-09-18 21:36:06 +0200

    scripts: edts: Fill-in 'controllers' views
    Fill-in controllers hierarchy in edts database.
    Aim is to provide a per controller complete hierarchized view
    of the various controller types.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- scripts/dts/extract/edts.py
+++ scripts/dts/extract/edts.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
+import pprint
 from .globals import *
 import edtsdatabase
 
@@ -14,7 +14,22 @@ edts = edtsdatabase.EDTSDatabase()
 #
 # @return ETDS device id
 def edts_device_id(node_address):
-	return node_address
+    return node_address
+
+
+def edts_get_controller_type(controller_address):
+    controller_type = ''
+    controller_props = reduced[controller_address]['props']
+    for prop in controller_props:
+        if 'controller' in prop:
+            controller_type = prop
+            break
+        if 'parent' in prop:
+            controller_type = prop
+            break
+    if controller_type == '':
+        raise Exception("Try to instanciate {} has a controller".format(controller_address))
+    return controller_type
 
 def edts_insert_chosen(chosen, node_address):
     edts.insert_chosen(chosen, edts_device_id(node_address))
@@ -26,8 +41,21 @@ def edts_insert_device_property(node_address, property_path, property_value):
     device_id = edts_device_id(node_address)
     edts.insert_device_property(device_id, property_path, property_value)
 
+##
+# @brief Insert device type into EDTS
+#
 def edts_insert_device_type(compatible, device_type):
     edts.insert_device_type(compatible, device_type)
+
+##
+# @brief Insert device controller into EDTS
+#
+def edts_insert_device_controller(controller_address, node_address, line):
+    device_id = edts_device_id(node_address)
+    controller_id = edts_device_id(controller_address)
+    controller_type = edts_get_controller_type(controller_address)
+    edts.insert_device_controller(controller_type, controller_id,
+                                  device_id, line)
 
 ##
 # @brief Insert device parent-device property into EDTS
