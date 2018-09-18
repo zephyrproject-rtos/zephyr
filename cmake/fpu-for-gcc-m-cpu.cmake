7e89cb75f1c19e874f4ba85b92eeac9f1e6b506f
Aurelien Jarno <aurelien@aurel32.net>
2018-09-18 23:20:20 +0200

    arch/Kconfig: add a hidden option to enable double precision FPU
    This patch adds a hidden option to enable support for double-precision
    FPU. It is selected by the architecture, CPU or SoC that has such an FPU.
    This option is then used in fpu-for-gcc-m-cpu.cmake to select the
    correct FPU.
    
    Double precision FPU is enabled on x86 CPUs and SoCs, and on ARM on i.MX RT,
    SAME70 SoCs and on STM32F746XG and STM32F769XI. This effectively disables
    the double precision on STM32F723XE.
    
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- cmake/fpu-for-gcc-m-cpu.cmake
+++ cmake/fpu-for-gcc-m-cpu.cmake
@@ -1,5 +1,9 @@
 # Defines a mapping from GCC_M_CPU to FPU
 
 set(FPU_FOR_cortex-m4      fpv4-sp-d16)
+if(CONFIG_FP_DOUBLE_PRECISION)
 set(FPU_FOR_cortex-m7      fpv5-d16)
+else()
+set(FPU_FOR_cortex-m7      fpv5-sp-d16)
+endif()
 set(FPU_FOR_cortex-m33     fpv5-sp-d16)
