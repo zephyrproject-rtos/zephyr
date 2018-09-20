0ea26b10f3c3819ac6336cc0703999355b07ea90
Szymon Janc <szymon.janc@codecoup.pl>
2018-09-20 20:13:23 +0200

    Bluetooth: Controller: Add support for setting public address
    This allows to provide public address for controller without using
    VS HCI command from host. Useful for controller only builds or
    combined builds that are not using VS HCI commands.
    
    Signed-off-by: Szymon Janc <szymon.janc@codecoup.pl>

---
+++ include/bluetooth/controller.h
@@ -0,0 +1,39 @@
+/** @file
+ *  @brief Bluetooth subsystem controller APIs.
+ */
+
+/*
+ * Copyright (c) 2018 Codecoup
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_BLUETOOTH_CONTROLLER_H_
+#define ZEPHYR_INCLUDE_BLUETOOTH_CONTROLLER_H_
+
+/**
+ * @brief Bluetooth Controller
+ * @defgroup bt_ctrl Bluetooth Controller
+ * @ingroup bluetooth
+ * @{
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @brief Set public address for controller
+ *
+ *  Should be called before bt_enable().
+ *
+ *  @param addr Public address
+ */
+void bt_ctlr_set_public_addr(const u8_t *addr);
+
+#ifdef __cplusplus
+}
+#endif
+/**
+ * @}
+ */
+
+#endif /* ZEPHYR_INCLUDE_BLUETOOTH_CONTROLLER_H_ */
