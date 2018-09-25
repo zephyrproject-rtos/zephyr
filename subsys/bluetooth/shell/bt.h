b07ae1139ed5fb36114a3ad6e3f7b784850e700c
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2018-09-25 14:24:25 +0300

    Bluetooth: shell: Split BR/EDR commands
    This splits BR/EDR command under 'bt' to 'br' to reduce the command
    list and at same time cleaning the mix between different bearers.
    
    In addition to that also remove "br-" prefix for the commands:
    
    br - Bluetooth BR/EDR shell commands
    Options:
      -h, --help  :Show command help.
    Subcommands:
      auth-pincode       :<pincode>
      connect            :<address>
      discovery          :<value: on, off> [length: 1-48] [mode: limited]
      iscan              :<value: on, off>
      l2cap-register     :<psm>
      oob                :
      pscan              :value: on, off
      sdp-find           :<HFPAG>
      rfcomm-register    :<channel>
      rfcomm-connect     :<channel>
      rfcomm-send        :<number of packets>
      rfcomm-disconnect  :[none]
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- subsys/bluetooth/shell/bt.h
+++ subsys/bluetooth/shell/bt.h
@@ -22,4 +22,7 @@ extern const struct shell *ctx_shell;
 
 extern struct bt_conn *default_conn;
 
+int str2bt_addr(const char *str, bt_addr_t *addr);
+void conn_addr_str(struct bt_conn *conn, char *addr, size_t len);
+
 #endif /* __BT_H */
