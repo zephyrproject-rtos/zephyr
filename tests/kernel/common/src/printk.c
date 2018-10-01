db71f8a08123e5e04426978d7662733564862a9a
Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>
2018-10-01 13:59:39 +0200

    test: kernel: common: _vprintk test update
    Added test case to verify if _vprintk parses '*'
    correctly.
    
    Signed-off-by: Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>

--- tests/kernel/common/src/printk.c
+++ tests/kernel/common/src/printk.c
@@ -26,6 +26,7 @@ char *expected = "22 113 10000 32768 40000 22\n"
 		 "42 42 0042 00000042\n"
 		 "255     42    abcdef  0x0000002a      42\n"
 		 "-1 4294967295 ffffffff\n"
+		 "      test_test      \n"
 ;
 
 
@@ -89,6 +90,7 @@ void test_printk(void)
 	printk("%u %02u %04u %08u\n", 42, 42, 42, 42);
 	printk("%-8u%-6d%-4x%-2p%8d\n", 0xFF, 42, 0xABCDEF, (char *)42, 42);
 	printk("%lld %llu %llx\n", -1LL, -1ULL, -1ULL);
+	printk("%*s_%-*s\n", 10, "test", 10, "test");
 
 	ram_console[pos] = '\0';
 	zassert_true((strcmp(ram_console, expected) == 0), "printk failed");
@@ -121,6 +123,9 @@ void test_printk(void)
 	count += snprintk(ram_console + count, sizeof(ram_console) - count,
 			  "%lld %llu %llx\n",
 			  -1LL, -1ULL, -1ULL);
+	count += snprintk(ram_console + count, sizeof(ram_console) - count,
+			  "%*s_%-*s\n",
+			  10, "test", 10, "test");
 	ram_console[count] = '\0';
 	zassert_true((strcmp(ram_console, expected) == 0), "snprintk failed");
 }
