7859a38dccdce8910eef3c51184e2fb431806842
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2018-10-02 16:18:38 +0300

    Bluetooth: Shell: Make error message for unknown parameter consistent
    Also simplify it by having the 'unknown parameter:' as part of the
    message format.
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- subsys/bluetooth/shell/rfcomm.c
+++ subsys/bluetooth/shell/rfcomm.c
@@ -259,7 +259,8 @@ static int cmd_rfcomm(const struct shell *shell, size_t argc, char **argv)
 		return 0;
 	}
 
-	error(shell, "%s:%s%s", argv[0], "unknown parameter: ", argv[1]);
+	error(shell, "%s unknown parameter: %s", argv[0], argv[1]);
+
 	return -ENOEXEC;
 }
 
