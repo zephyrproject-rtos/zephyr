9d0846d72c841359ea1cda623c3c3550810f5fce
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-10-03 13:42:35 +0300

    samples: net: rpl_border_router: Convert to use new shell
    Use new shell instead of the legacy one.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- samples/net/rpl_border_router/src/shell.c
+++ samples/net/rpl_border_router/src/shell.c
@@ -8,7 +8,8 @@
 #define NET_LOG_LEVEL LOG_LEVEL_DBG
 
 #include <zephyr.h>
-#include <shell/legacy_shell.h>
+#include <shell/shell.h>
+#include <shell/shell_uart.h>
 #include <stdio.h>
 
 #include "../../../subsys/net/ip/rpl.h"
@@ -17,32 +18,44 @@
 
 #define BR_SHELL_MODULE "br"
 
-int br_repair(int argc, char *argv[])
+static int cmd_br_repair(const struct shell *shell,
+			 size_t argc, char *argv[])
 {
 	ARG_UNUSED(argc);
 	ARG_UNUSED(argv);
 
-	NET_INFO("Starting global repair...");
+	if (shell_help_requested(shell)) {
+		shell_help_print(shell, NULL, 0);
+		return -ENOEXEC;
+	}
+
+	shell_fprintf(shell, SHELL_INFO, "Starting global repair...\n");
 
 	net_rpl_repair_root(CONFIG_NET_RPL_DEFAULT_INSTANCE);
 
 	return 0;
 }
 
-int coap_send(int argc, char *argv[])
+static int cmd_coap_send(const struct shell *shell,
+			 size_t argc, char *argv[])
 {
 	struct in6_addr peer_addr;
 	enum coap_request_type type;
 	int r;
 
+	if (shell_help_requested(shell)) {
+		shell_help_print(shell, NULL, 0);
+		return -ENOEXEC;
+	}
+
 	if (argc != 3 || !argv[1] || !argv[2]) {
-		NET_INFO("Invalid arguments");
-		return -EINVAL;
+		shell_fprintf(shell, SHELL_ERROR, "Invalid arguments.\n");
+		return -ENOEXEC;
 	}
 
 	r = net_addr_pton(AF_INET6, argv[2], &peer_addr);
 	if (r < 0) {
-		return -EINVAL;
+		return -ENOEXEC;
 	}
 
 	if (strcmp(argv[1], "toggle") == 0) {
@@ -50,8 +63,8 @@ int coap_send(int argc, char *argv[])
 	} else if (strcmp(argv[1], "rpl_obs") == 0) {
 		type = COAP_REQ_RPL_OBS;
 	} else {
-		NET_INFO("Invalid arguments");
-		return -EINVAL;
+		shell_fprintf(shell, SHELL_ERROR, "Invalid arguments.\n");
+		return -ENOEXEC;
 	}
 
 	coap_send_request(&peer_addr, type, NULL, NULL);
@@ -59,15 +72,21 @@ int coap_send(int argc, char *argv[])
 	return 0;
 }
 
-static struct shell_cmd br_commands[] = {
-	/* Keep the commands in alphabetical order */
-	{ "coap", coap_send, "\n\tSend CoAP commands to Node\n"
-			"toggle <host>\n\tToggle the LED on Node\n"
-			"rpl_obs <host>\n\tSet RPL observer on Node\n"
-	},
-	{ "repair", br_repair,
-		"\n\tGlobal repair RPL network" },
-	{ NULL, NULL, NULL }
+SHELL_CREATE_STATIC_SUBCMD_SET(br_commands)
+{
+	SHELL_CMD(coap, NULL,
+		  "Send CoAP commands to Node\n"
+		  "toggle <host>   Toggle the LED on Node\n"
+		  "rpl_obs <host>  Set RPL observer on Node",
+		  cmd_coap_send),
+	SHELL_CMD(repair, NULL,
+		  "Global repair RPL network",
+		  cmd_br_repair),
+	SHELL_SUBCMD_SET_END
 };
 
-SHELL_REGISTER(BR_SHELL_MODULE, br_commands);
+SHELL_CMD_REGISTER(br, &br_commands, "Border router commands", NULL);
+
+void br_shell_init(void)
+{
+}
