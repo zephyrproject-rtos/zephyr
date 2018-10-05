70a2e74f970016ecfa3a05b24a0626a19a3d1404
Gil Pitney <gil.pitney@linaro.org>
2018-10-05 14:37:37 -0700

    net: sockets: Get socket offload to build after net logging overhaul.
    Also, defines one LOG_MODULE_NAME for the simplelink WiFi driver, and
    uses the same name for all files in this driver (module).
    
    Signed-off-by: Gil Pitney <gil.pitney@linaro.org>

--- subsys/net/lib/sockets/socket_offload.c
+++ subsys/net/lib/sockets/socket_offload.c
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#define LOG_MODULE_NAME net_socket_offload
+#define NET_LOG_LEVEL CONFIG_NET_SOCKETS_LOG_LEVEL
+
 #include <net/socket_offload.h>
 
 /* Only one provider may register socket operations upon boot. */
