b57c3dcad3c5568dbae8ab3d605afd284588e983
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2018-10-09 16:03:29 +0200

    DeviceTree: Convert 'generated_dts_board.h' into a source file
    extract_dts_includes.py has been generating DT output and then
    concatenating it with fixup header files to create
    'generated_dts_board.h'.
    
    In this patch we instead introduce a source file named
    'generated_dts_board.h' and have it \#include the appropriate DT
    output and fixup files.
    
    This results in a simpler system because users can now read
    'generated_dts_board.h' as C source code to see how fixup files and
    generated DT output relate to each other. Whereas before they would
    have to either read documentation or python code to gain the same
    understanding.
    
    Also, it reduces the scope and complexity of one of our most bloated
    python scripts, extract_dts_includes.py.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- cmake/dts.cmake
+++ cmake/dts.cmake
@@ -1,6 +1,6 @@
 # Zephyr code can configure itself based on a KConfig'uration with the
 # header file autoconf.h. There exists an analogous file
-# generated_dts_board.h that allows configuration based on information
+# generated_dts_board_unfixed.h that allows configuration based on information
 # encoded in DTS.
 #
 # Here we call on dtc, the gcc preprocessor, and
@@ -8,7 +8,7 @@
 # CMake configure-time.
 #
 # See ~/zephyr/doc/dts
-set(GENERATED_DTS_BOARD_H    ${PROJECT_BINARY_DIR}/include/generated/generated_dts_board.h)
+set(GENERATED_DTS_BOARD_UNFIXED_H    ${PROJECT_BINARY_DIR}/include/generated/generated_dts_board_unfixed.h)
 set(GENERATED_DTS_BOARD_CONF ${PROJECT_BINARY_DIR}/include/generated/generated_dts_board.conf)
 set_ifndef(DTS_SOURCE ${BOARD_DIR}/${BOARD}.dts)
 set_ifndef(DTS_COMMON_OVERLAYS ${ZEPHYR_BASE}/dts/common/common.dts)
@@ -48,7 +48,7 @@ if(CONFIG_HAS_DTS)
   endforeach()
 
   # TODO: Cut down on CMake configuration time by avoiding
-  # regeneration of generated_dts_board.h on every configure. How
+  # regeneration of generated_dts_board_unfixed.h on every configure. How
   # challenging is this? What are the dts dependencies? We run the
   # preprocessor, and it seems to be including all kinds of
   # directories with who-knows how many header files.
@@ -95,46 +95,6 @@ if(CONFIG_HAS_DTS)
     message(FATAL_ERROR "command failed with return code: ${ret}")
   endif()
 
-  # Error-out when the deprecated naming convention is found (until
-  # after 1.14.0 has been released)
-  foreach(path
-    ${BOARD_DIR}/dts.fixup
-    ${PROJECT_SOURCE_DIR}/soc/${ARCH}/${SOC_PATH}/dts.fixup
-      ${APPLICATION_SOURCE_DIR}/dts.fixup
-    )
-    if(EXISTS ${path})
-      message(FATAL_ERROR
-      "A deprecated filename has been detected. Porting is required."
-      "The file '${path}' exists, but it should be named dts_fixup.h instead."
-      "See https://github.com/zephyrproject-rtos/zephyr/pull/10352 for more details"
-      )
-    endif()
-  endforeach()
-
-  # Run extract_dts_includes.py for the header file
-  # generated_dts_board.h
-  set_ifndef(DTS_BOARD_FIXUP_FILE ${BOARD_DIR}/dts_fixup.h)
-  set_ifndef(DTS_SOC_FIXUP_FILE   ${SOC_DIR}/${ARCH}/${SOC_PATH}/dts_fixup.h)
-
-  list(APPEND dts_fixups
-    ${DTS_BOARD_FIXUP_FILE}
-    ${DTS_SOC_FIXUP_FILE}
-    ${APPLICATION_SOURCE_DIR}/dts_fixup.h
-    ${shield_dts_fixups}
-    )
-
-  foreach(fixup ${dts_fixups})
-    if(EXISTS ${fixup})
-      list(APPEND existing_dts_fixups ${fixup})
-    endif()
-  endforeach()
-
-  if("${existing_dts_fixups}" STREQUAL "")
-    unset(DTS_FIXUPS_WITH_FLAG)
-  else()
-    set(DTS_FIXUPS_WITH_FLAG --fixup ${existing_dts_fixups})
-  endif()
-
   if(NOT EXISTS ${DTS_APP_BINDINGS})
     set(DTS_APP_BINDINGS)
   endif()
@@ -142,9 +102,8 @@ if(CONFIG_HAS_DTS)
   set(CMD_EXTRACT_DTS_INCLUDES ${PYTHON_EXECUTABLE} ${ZEPHYR_BASE}/scripts/dts/extract_dts_includes.py
     --dts ${BOARD}.dts_compiled
     --yaml ${ZEPHYR_BASE}/dts/bindings ${DTS_APP_BINDINGS}
-    ${DTS_FIXUPS_WITH_FLAG}
     --keyvalue ${GENERATED_DTS_BOARD_CONF}
-    --include ${GENERATED_DTS_BOARD_H}
+    --include ${GENERATED_DTS_BOARD_UNFIXED_H}
     --old-alias-names
     )
 
@@ -163,5 +122,5 @@ if(CONFIG_HAS_DTS)
   import_kconfig(DT_     ${GENERATED_DTS_BOARD_CONF})
 
 else()
-  file(WRITE ${GENERATED_DTS_BOARD_H} "/* WARNING. THIS FILE IS AUTO-GENERATED. DO NOT MODIFY! */")
+  file(WRITE ${GENERATED_DTS_BOARD_UNFIXED_H} "/* WARNING. THIS FILE IS AUTO-GENERATED. DO NOT MODIFY! */")
 endif()
