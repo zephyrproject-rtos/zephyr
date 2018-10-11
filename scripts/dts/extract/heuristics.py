60520355649af7bc0a0ab7f1c8624a3d93b27571
Erwan Gouriou <erwan.gouriou@linaro.org>
2018-10-11 13:54:07 +0200

    scripts: extract_dts_includes.py: extract heuristics to EDTS database
    Extend directive handling to use heuristics to fill the EDTS database.
    
    heuristics was called when treating 'compatible' property
    on the assumption it is mandatory.
    Actually some nodes (children) don't have 'compatible', so
    move heuristics out or properties parsing and process them
    just before node properties treatment.
    
    Signed-off-by: Bobby Noelte <b0661n0e17e@gmail.com>
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

---
+++ scripts/dts/extract/heuristics.py
@@ -0,0 +1,75 @@
+#
+# Copyright (c) 2018 Bobby Noelte
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract.globals import *
+from extract.edts import *
+
+##
+# @brief Generate device tree information based on heuristics.
+#
+# Generates in EDTS:
+# - bus/master : device id of bus master for a bus device
+# - parent-device : device id of parent device
+class DTHeuristics(object):
+
+    def __init__(self):
+        pass
+
+    ##
+    # @brief Generate device tree information based on heuristics.
+    #
+    # Device tree properties may have to be deduced by heuristics
+    # as the property definitions are not always consistent across
+    # different node types.
+    #
+    # @param node_address Address of node owning the
+    #                     compatible definition.
+    # @param yaml YAML definition for the owning node.
+    #
+    def extract(self, node_address, yaml):
+
+        # Process compatible related work
+        try:
+            compatible = reduced[node_address]['props']['compatible']
+        except KeyError:
+            # No compat skip next part
+            return
+
+        if not isinstance(compatible, list):
+            compatible = [compatible]
+
+        # Check for <device>-device that is connected to a bus
+        for compat in compatible:
+            if compat not in yaml:
+                continue
+
+            if 'parent' not in yaml[compat]:
+                continue
+
+            bus_master_device_type = yaml[compat]['parent']['bus']
+
+            # get parent
+            parent_node_address = ''
+            for comp in node_address.split('/')[1:-1]:
+                parent_node_address += '/' + comp
+
+            # get parent yaml
+            parent_yaml = yaml[reduced[parent_node_address] \
+                                          ['props']['compatible']]
+
+            if bus_master_device_type != parent_yaml['child']['bus']:
+                continue
+
+            # generate EDTS
+            edts_insert_device_property(node_address, 'bus/master',
+                parent_node_address)
+
+        # Check for a parent device this device is subordinated
+        edts_insert_device_parent_device_property(node_address)
+
+##
+# @brief Management information for heuristics.
+heuristics = DTHeuristics()
