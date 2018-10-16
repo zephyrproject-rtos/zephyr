51eab9ff86463464a6b1cdc614730edd7abcee00
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2018-10-16 16:14:02 +0200

    cmake: Fix 'run' command of native_posix
    The 'run' target was missing it's dependency on the executable and
    thereby behaving incorrectly.
    
    This fixes #10639 .
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- cmake/emu/native.cmake
+++ cmake/emu/native.cmake
@@ -1,6 +1,7 @@
 add_custom_target(run
   COMMAND
   ${APPLICATION_BINARY_DIR}/zephyr/${KERNEL_EXE_NAME}
+  DEPENDS ${logical_target_for_zephyr_elf}
   WORKING_DIRECTORY ${APPLICATION_BINARY_DIR}
   USES_TERMINAL
   )
