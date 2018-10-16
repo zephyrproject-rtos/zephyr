d6fb3c74fbfb37517bab0bc77a7a1a7e0462fb21
Christian Taedcke <hacking@taedcke.com>
2018-10-16 21:51:00 +0200

    drivers/rtc: Enable RTC driver for STM32F7 series
    Enable RTC driver for STM32F7 series.
    
    Signed-off-by: Christian Taedcke <hacking@taedcke.com>

--- soc/arm/st_stm32/stm32f7/soc.h
+++ soc/arm/st_stm32/stm32f7/soc.h
@@ -50,6 +50,12 @@
 #include <stm32f7xx_ll_rng.h>
 #endif
 
+#ifdef CONFIG_RTC_STM32
+#include <stm32f7xx_ll_rtc.h>
+#include <stm32f7xx_ll_exti.h>
+#include <stm32f7xx_ll_pwr.h>
+#endif
+
 #endif /* !_ASMLANGUAGE */
 
 #endif /* _STM32F7_SOC_H_ */
