0c7a28c4cc3493fdfcf52ff809fa5b3d20bf5429
Diego Sueiro <diego.sueiro@gmail.com>
2018-10-16 17:51:01 +0100

    drivers: serial: Rework Silabs Gecko UART Driver
    Introduces the location property and adds the ability to use values
    generated by the device tree configuration.
    
    Signed-off-by: Diego Sueiro <diego.sueiro@gmail.com>
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- soc/arm/silabs_exx32/efr32fg1p/soc_pinmap.h
+++ soc/arm/silabs_exx32/efr32fg1p/soc_pinmap.h
@@ -18,13 +18,13 @@
 
 #ifdef CONFIG_SOC_PART_NUMBER_EFR32FG1P133F256GM48
 #ifdef CONFIG_USART_GECKO_0
-#if (CONFIG_USART_GECKO_0_GPIO_LOC == 0)
+#if (CONFIG_USART_GECKO_0_LOCATION == 0)
 #define PIN_USART0_TXD {gpioPortA, 0, gpioModePushPull, 1}
 #define PIN_USART0_RXD {gpioPortA, 1, gpioModeInput, 1}
-#elif (CONFIG_USART_GECKO_0_GPIO_LOC == 1)
+#elif (CONFIG_USART_GECKO_0_LOCATION == 1)
 #define PIN_USART0_TXD {gpioPortA, 1, gpioModePushPull, 1}
 #define PIN_USART0_RXD {gpioPortA, 2, gpioModeInput, 1}
-#elif (CONFIG_USART_GECKO_0_GPIO_LOC == 2)
+#elif (CONFIG_USART_GECKO_0_LOCATION == 2)
 #define PIN_USART0_TXD {gpioPortA, 2, gpioModePushPull, 1}
 #define PIN_USART0_RXD {gpioPortA, 3, gpioModeInput, 1}
 #else
@@ -33,15 +33,15 @@
 #endif /* CONFIG_USART_GECKO_0 */
 
 #ifdef CONFIG_USART_GECKO_1
-#if (CONFIG_USART_GECKO_1_GPIO_LOC == 0)
+#if (CONFIG_USART_GECKO_1_LOCATION == 0)
 #error ("Serial Driver for Gecko MCUs not implemented for this location index")
-#elif (CONFIG_USART_GECKO_1_GPIO_LOC == 1)
+#elif (CONFIG_USART_GECKO_1_LOCATION == 1)
 #define PIN_USART1_TXD {gpioPortF, 10, gpioModePushPull, 1}
 #define PIN_USART1_RXD {gpioPortF, 11, gpioModeInput, 1}
-#elif (CONFIG_USART_GECKO_1_GPIO_LOC == 2)
+#elif (CONFIG_USART_GECKO_1_LOCATION == 2)
 #define PIN_USART1_TXD {gpioPortB, 9, gpioModePushPull, 1}
 #define PIN_USART1_RXD {gpioPortB, 10, gpioModeInput, 1}
-#elif (CONFIG_USART_GECKO_1_GPIO_LOC == 3)
+#elif (CONFIG_USART_GECKO_1_LOCATION == 3)
 #define PIN_USART1_TXD {gpioPortE, 2, gpioModePushPull, 1}
 #define PIN_USART1_RXD {gpioPortE, 3, gpioModeInput, 1}
 #else
