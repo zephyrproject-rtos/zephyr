841679981f69a8f155a2ae94847f0863aa56a7fc
Kumar Gala <kumar.gala@linaro.org>
2018-10-17 18:55:37 -0500

    test

--- scripts/dts/tests/compat/run.sh
+++ scripts/dts/tests/compat/run.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
 ../../extract_dts_includes.py -d ./edts.dts -y ../bindings/ -e ./compat.edts -i /dev/null -k /dev/null
+../../extract_dts_includes.py -d ./compat.dts -y ../bindings/ -e ./test.edts -i /dev/null -k /dev/null
