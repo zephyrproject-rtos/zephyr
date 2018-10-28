4c1c349ab8ad21b2c4b12aa8e49fa47dd2067841
Maureen Helm <maureen.helm@nxp.com>
2018-10-28 16:25:39 -0500

    boards: lpcxpresso54114: Combine m4/m0 boards into one directory
    Combines the lpcxpresso54114_m4 and lpcxpresso54114_m0 boards into one
    directory containing both board configurations. This eliminates some
    duplicate code and documentation, and better represents the actual
    hardware.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- boards/arm/lpcxpresso54114/board.cmake
+++ boards/arm/lpcxpresso54114/board.cmake
@@ -11,6 +11,10 @@ if(LPCLINK_FW STREQUAL jlink)
   set_ifndef(BOARD_FLASH_RUNNER jlink)
 endif()
 
+if(CONFIG_BOARD_LPCXPRESSO54114_M4)
 board_runner_args(jlink "--device=LPC54114J256_M4")
+elseif(CONFIG_BOARD_LPCXPRESSO54114_M0)
+board_runner_args(jlink "--device=LPC54114J256_M0")
+endif()
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
