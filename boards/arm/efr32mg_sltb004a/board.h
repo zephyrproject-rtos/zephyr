554001696561fc6f0690db4f01c13b9d18716869
Diego Sueiro <diego.sueiro@gmail.com>
2018-10-30 20:56:31 +0000

    boards: Add Silabs EFR32MG-SLTB004A (Thunderboard Sense 2) support
    The EFR32™ Mighty Gecko Starter Kit EFR32MG-SLTB004A (a.k.a
    Thunderboard Sense 2) contains a MCU from the EFR32MG12P family built
    on ARM® Cortex®-M4F processor with low power capabilities.
    
    There is an On-Board J-Link Debugger that presents a virtual COM port
    for general purpose application serial data transfer with this
    interface and a Mass Storage for firmware flashing.
    
    Signed-off-by: Diego Sueiro <diego.sueiro@gmail.com>

---
+++ boards/arm/efr32mg_sltb004a/board.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018 Christian Taedcke, Diego Sueiro
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _ASMLANGUAGE
+
+#include <soc.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __INC_BOARD_H */
