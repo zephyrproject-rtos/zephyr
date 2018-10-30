83eea9796c3004a0c228ad3f0d2b70d74dfd0795
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-10-30 12:52:01 +0530

    tests/cmsis_rtos_v2: Introduce tests to make use of Mutex APIs
    Some test programs to illustrate the usage of Mutex APIs.
    
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>

--- tests/cmsis_rtos_v2/src/main.c
+++ tests/cmsis_rtos_v2/src/main.c
@@ -11,13 +11,17 @@
 extern void test_thread_apis(void);
 extern void test_thread_prio(void);
 extern void test_timer(void);
+extern void test_mutex(void);
+extern void test_mutex_lock_timeout(void);
 
 void test_main(void)
 {
 	ztest_test_suite(test_cmsis_v2_apis,
 			ztest_unit_test(test_thread_apis),
 			ztest_unit_test(test_thread_prio),
-			ztest_unit_test(test_timer));
+			ztest_unit_test(test_timer),
+			ztest_unit_test(test_mutex),
+			ztest_unit_test(test_mutex_lock_timeout));
 
 	ztest_run_test_suite(test_cmsis_v2_apis);
 }
