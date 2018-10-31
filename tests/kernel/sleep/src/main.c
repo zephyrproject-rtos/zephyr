574fc953e2601e75b80f408882a24fa1d7f884f2
Alberto Escolar Piedras <alpi@oticon.com>
2018-10-31 10:47:20 +0100

    tests: sleep: Bugfix for POSIX arch in TICKLESS
    In the POSIX architecture, with the inf_clock "SOC", time does
    not pass while the CPU is running. Tests that require time to pass
    while busy waiting should call k_busy_wait() or in some other way
    set the CPU to idle. This test was setting the CPU to idle while
    waiting for the next time slice. This is ok if the system tick
    (timer) is active and awaking the CPU every system tick period.
    But when configured in tickless mode that is not the case, and the
    CPU was set to sleep for an indefinite amount of time.
    This commit fixes it by using k_busy_wait(a few microseconds) inside
    that busy wait loop instead.
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>

--- tests/kernel/sleep/src/main.c
+++ tests/kernel/sleep/src/main.c
@@ -73,7 +73,7 @@ static void align_to_tick_boundary(void)
 	while (k_uptime_get_32() == tick) {
 		/* Busy wait to align to tick boundary */
 #if defined(CONFIG_ARCH_POSIX)
-		posix_halt_cpu();
+		k_busy_wait(50);
 #endif
 	}
 
