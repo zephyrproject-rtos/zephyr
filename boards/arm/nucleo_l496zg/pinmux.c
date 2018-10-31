3a002da53e3fe1be0287cfb041dc59de35557c46
Ioannis Konstantelias <ikonstadel@gmail.com>
2018-10-31 20:17:31 +0200

    boards: arm: Add initial support for NUCLEO-L496ZG
    This commit adds the simplest configuration for this Nucleo-144 board
    stripped down to the parts that are currently tested.
    
    Signed-off-by: Ioannis Konstantelias <ikonstadel@gmail.com>

---
+++ boards/arm/nucleo_l496zg/pinmux.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018 Centaur Analytics, Inc
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <kernel.h>
+#include <device.h>
+#include <init.h>
+#include <pinmux.h>
+#include <sys_io.h>
+
+#include <pinmux/stm32/pinmux_stm32.h>
+
+/* pin assignments for NUCLEO-L496ZG board */
+static const struct pin_config pinconf[] = {
+};
+
+static int pinmux_stm32_init(struct device *port)
+{
+	ARG_UNUSED(port);
+
+	stm32_setup_pins(pinconf, ARRAY_SIZE(pinconf));
+
+	return 0;
+}
+
+SYS_INIT(pinmux_stm32_init, PRE_KERNEL_1,
+	 CONFIG_PINMUX_STM32_DEVICE_INITIALIZATION_PRIORITY);
