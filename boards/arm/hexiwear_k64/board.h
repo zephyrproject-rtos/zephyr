c019d548367d85cbadb11845939865d90b4463ee
Kumar Gala <kumar.gala@linaro.org>
2018-11-01 15:54:26 -0500

    boards: hexiwear_k64: Remove PWM defines
    Now that the samples use DT generated defines we can remove the
    hexiwear_k64 specific ones in board.h
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- boards/arm/hexiwear_k64/board.h
+++ boards/arm/hexiwear_k64/board.h
@@ -9,17 +9,4 @@
 
 #include <soc.h>
 
-#ifdef CONFIG_PWM_3
-
-#define RED_PWM_NAME		CONFIG_FTM_3_NAME
-#define RED_PWM_CHANNEL		4
-
-#define GREEN_PWM_NAME		CONFIG_FTM_3_NAME
-#define GREEN_PWM_CHANNEL	0
-
-#define BLUE_PWM_NAME		CONFIG_FTM_3_NAME
-#define BLUE_PWM_CHANNEL	5
-
-#endif /* CONFIG_PWM_3 */
-
 #endif /* __INC_BOARD_H */
