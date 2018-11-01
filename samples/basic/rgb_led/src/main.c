b7ee51546626eb6cf68a372ab7084d9cb8e9c263
Kumar Gala <kumar.gala@linaro.org>
2018-11-01 15:52:28 -0500

    samples: baisc: Update to use DT generated pwm defines
    We update 'blink_led', 'fade_led' and 'rgb_led' to support dt generated
    defines for the PWM info needed for each.  Since hexiwear_k64 supports
    generating the info, moved the support for hexiwear_k64 to just use the
    dt generated defines.
    
    For 'blink_led' and 'fade_led' we use an alias in dt called 'pwm-led0'.
    For 'rgb_led' we use aliases 'red-pwm-led', 'green-pwm-led' and
    'blue-pwm-led'
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- samples/basic/rgb_led/src/main.c
+++ samples/basic/rgb_led/src/main.c
@@ -22,14 +22,21 @@
 #define PWM_CH1		1
 #define PWM_DEV2	CONFIG_PWM_QMSI_DEV_NAME
 #define PWM_CH2		2
-#elif defined(CONFIG_BOARD_HEXIWEAR_K64)
-#include <board.h>
-#define PWM_DEV0	RED_PWM_NAME
-#define PWM_CH0		RED_PWM_CHANNEL
-#define PWM_DEV1	GREEN_PWM_NAME
-#define PWM_CH1		GREEN_PWM_CHANNEL
-#define PWM_DEV2	BLUE_PWM_NAME
-#define PWM_CH2		BLUE_PWM_CHANNEL
+#elif defined(RED_PWM_LED_PWM_CONTROLLER) && \
+      defined(RED_PWM_LED_PWM_CHANNEL) && \
+      defined(GREEN_PWM_LED_PWM_CONTROLLER) && \
+      defined(GREEN_PWM_LED_PWM_CHANNEL) && \
+      defined(BLUE_PWM_LED_PWM_CONTROLLER) && \
+      defined(BLUE_PWM_LED_PWM_CHANNEL)
+/* Get the defines from dt (based on aliases 'red-pwm-led', 'green-pwm-led' &
+ * 'blue-pwm-led')
+ */
+#define PWM_DEV0	RED_PWM_LED_PWM_CONTROLLER
+#define PWM_CH0		RED_PWM_LED_PWM_CHANNEL
+#define PWM_DEV1	GREEN_PWM_LED_PWM_CONTROLLER
+#define PWM_CH1		GREEN_PWM_LED_PWM_CHANNEL
+#define PWM_DEV2	BLUE_PWM_LED_PWM_CONTROLLER
+#define PWM_CH2		BLUE_PWM_LED_PWM_CHANNEL
 #else
 #error "Choose supported board or add new board for the application"
 #endif
