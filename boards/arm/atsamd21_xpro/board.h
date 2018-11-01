a373c77b8d58cdc75ba9c65081e2c969ff418d6e
Kumar Gala <kumar.gala@linaro.org>
2018-11-01 05:15:59 -0500

    drivers: spi_sam0: move sercom pad info to dts
    Move the setting of the SERCOM PAD for dipo/dopo for the spi driver
    into device tree out of board.h
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- boards/arm/atsamd21_xpro/board.h
+++ boards/arm/atsamd21_xpro/board.h
@@ -9,8 +9,4 @@
 
 #include <soc.h>
 
-/* SPI SERCOM5 on MISO=PB16/pad 0, MOSI=PB22/pad 2, SCK=PB23/pad 3 */
-#define CONFIG_SPI_SAM0_SERCOM5_PADS \
-	(SERCOM_SPI_CTRLA_DIPO(0) | SERCOM_SPI_CTRLA_DOPO(2))
-
 #endif /* __INC_BOARD_H */
