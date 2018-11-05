22963b60c2b5f85a7d15d3c20856f6fcee829d13
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-11-05 11:26:56 +0100

    drivers/spi: Remove unused legacy options
    These have been osbolete for some time already.
    
    Fixes #11064
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- boards/x86/arduino_101/pinmux.c
+++ boards/x86/arduino_101/pinmux.c
@@ -116,7 +116,7 @@ static void _pinmux_defaults(u32_t base)
 	u32_t mux_config[PINMUX_MAX_REGISTERS] = { 0, 0, 0, 0, 0 };
 	int i = 0;
 
-#if !defined(CONFIG_SPI_1) && !defined(CONFIG_SPI_CS_GPIO)
+#if !defined(CONFIG_SPI_1)
 	PIN_CONFIG(mux_config,  0,  PINMUX_FUNC_B);
 #endif
 	PIN_CONFIG(mux_config,  1,  PINMUX_FUNC_B);
@@ -136,9 +136,7 @@ static void _pinmux_defaults(u32_t base)
 	PIN_CONFIG(mux_config, 42,  PINMUX_FUNC_B);
 	PIN_CONFIG(mux_config, 43,  PINMUX_FUNC_B);
 	PIN_CONFIG(mux_config, 44,  PINMUX_FUNC_B);
-#ifndef CONFIG_SPI_CS_GPIO
 	PIN_CONFIG(mux_config, 45,  PINMUX_FUNC_B);
-#endif
 #endif
 	PIN_CONFIG(mux_config, 55,  PINMUX_FUNC_B);
 	PIN_CONFIG(mux_config, 56,  PINMUX_FUNC_B);
