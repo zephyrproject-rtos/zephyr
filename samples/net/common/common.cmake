331b494fe178d9eeb9168f9017139306bd990ecf
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-11-05 21:08:31 +0100

    samples/net: Removing support of cc2520 on frdm_k64f
    There is an mcr20a shield board for frdm_k64f which is easier to use
    than wiring a cc2520 by hand. So let's remove this.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- samples/net/common/common.cmake
+++ samples/net/common/common.cmake
@@ -1,21 +1,5 @@
 # Common routines used in net samples
 
-if(CONFIG_BOARD_FRDM_K64F)
-  if(CONFIG_IEEE802154_CC2520)
-    target_include_directories(
-      app
-      PRIVATE
-      $ENV{ZEPHYR_BASE}/drivers/
-      $ENV{ZEPHYR_BASE}/include/drivers/
-      )
-    target_sources(
-      app
-      PRIVATE
-      $ENV{ZEPHYR_BASE}/samples/net/common/cc2520_frdm_k64f.c
-      )
-  endif()
-endif()
-
 if(CONFIG_NET_TESTING)
   target_include_directories(
     app
