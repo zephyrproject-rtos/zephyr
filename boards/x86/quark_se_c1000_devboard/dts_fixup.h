e8ce1ee968e878a82520fa893284c9447a7f0a0d
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-11-05 20:15:07 +0100

    boards/x86: Use dts for configuring the cc2520 device
    Only the SPI bus for now.
    Make cc2520's Kconfig aware of DTS on these settings.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

---
+++ boards/x86/quark_se_c1000_devboard/dts_fixup.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * This file is a temporary workaround for mapping of the generated information
+ * to the current driver definitions.  This will be removed when the drivers
+ * are modified to handle the generated information, or the mapping of
+ * generated data matches the driver definitions.
+ */
+
+#define CONFIG_IEEE802154_CC2520_SPI_DRV_NAME		DT_SNPS_DESIGNWARE_SPI_B0001400_TI_CC2520_0_BUS_NAME
+#define CONFIG_IEEE802154_CC2520_SPI_SLAVE		DT_SNPS_DESIGNWARE_SPI_B0001400_TI_CC2520_0_BASE_ADDRESS
+#define CONFIG_IEEE802154_CC2520_SPI_FREQ		DT_SNPS_DESIGNWARE_SPI_B0001400_TI_CC2520_0_SPI_MAX_FREQUENCY
+#define CONFIG_IEEE802154_CC2520_GPIO_SPI_CS_DRV_NAME	DT_SNPS_DESIGNWARE_SPI_B0001400_CS_GPIOS_CONTROLLER
+#define CONFIG_IEEE802154_CC2520_GPIO_SPI_CS_PIN	DT_SNPS_DESIGNWARE_SPI_B0001400_CS_GPIOS_PIN
