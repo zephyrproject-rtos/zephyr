ad55b096f16948de4542cbea7926f75c6c43f873
Endre Karlson <endre.karlson@gmail.com>
2018-11-09 09:08:12 +0100

    boards: efr32mg_sltb004a: Fix device name typos
    Chose the wrong device name here.
    
    Signed-off-by: Endre Karlson <endre.karlson@gmail.com>

--- boards/arm/efr32mg_sltb004a/board.cmake
+++ boards/arm/efr32mg_sltb004a/board.cmake
@@ -2,5 +2,5 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-board_runner_args(jlink "--device=EFM32MG12BxxxF1024")
+board_runner_args(jlink "--device=EFR32MG12PxxxF1024")
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
