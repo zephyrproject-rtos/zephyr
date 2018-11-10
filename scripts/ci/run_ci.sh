043b0a6a283286e39abc147ca81863976bbf44ef
Anas Nashif <anas.nashif@intel.com>
2018-11-10 15:03:36 -0500

    ci: build docs on last node
    Distribute the load and build docs correctly on the last node.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/ci/run_ci.sh
+++ scripts/ci/run_ci.sh
@@ -24,7 +24,6 @@ SANITYCHECK_OPTIONS_RETRY_2="${SANITYCHECK_OPTIONS} --only-failed --outdir=out-3
 SANITYCHECK="${ZEPHYR_BASE}/scripts/sanitycheck"
 MATRIX_BUILDS=1
 MATRIX=1
-DOC_MATRIX=${MATRIX_BUILDS}
 
 while getopts ":pm:b:r:M:" opt; do
   case $opt in
@@ -54,6 +53,7 @@ while getopts ":pm:b:r:M:" opt; do
   esac
 done
 
+DOC_MATRIX=${MATRIX_BUILDS}
 if [ -z "$BRANCH" ]; then
 	echo "No base branch given"
 	exit
