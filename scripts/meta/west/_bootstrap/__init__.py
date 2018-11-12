a2cab0d9dc3c4cde50c151848792059771c686dc
Carles Cufi <carles.cufi@nordicsemi.no>
2018-11-12 15:12:59 +0100

    scripts: remove west from scripts/
    west will now be installed via pip in order for the bootstrapper to be
    decoupled from the west runners.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- scripts/meta/west/_bootstrap/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2018 Open Source Foundries Limited.
-#
-# SPDX-License-Identifier: Apache-2.0
-
-# Empty file.
--- zephyr-env.cmd
+++ zephyr-env.cmd
@@ -4,14 +4,3 @@ set ZEPHYR_BASE=%~dp0
 if exist "%userprofile%\zephyrrc.cmd" (
 	call "%userprofile%\zephyrrc.cmd"
 )
-
-rem Zephyr meta-tool (west) launcher alias, which keeps monorepo
-rem Zephyr installations' 'make flash' etc. working.  See
-rem https://www.python.org/dev/peps/pep-0486/ for details on the
-rem virtualenv-related pieces. (We need to implement this manually
-rem because Zephyr's minimum supported Python version is 3.4.)
-if defined VIRTUAL_ENV (
-	doskey west=python %ZEPHYR_BASE%\scripts\west $*
-) else (
-	doskey west=py -3 %ZEPHYR_BASE%\scripts\west $*
-)
