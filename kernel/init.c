91c5b84cd5aa58bef2025815956ee02022a08ac8
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-11-13 16:57:45 +0530

    kernel: init.c: Added required hooks for the relocation
    This patch splits the text section into 2 parts. The first section
    will have some info regarding vector tables and debug info. The
    second section will have the complete text section.
    This is needed to force the required functions and data variables
    the correct locations.
    This is due to the behavior of the linker. The linker will only link
    once and hence this text section had to be split to make room
    for the generated linker script.
    
    Added a new Kconfig CODE_DATA_RELOCATION which when enabled will
    invoke the script, which does the required relocation.
    
    Added hooks inside init.c for bss zeroing and data copy operations.
    Needed when we have to copy data from ROM to required memory type.
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>

--- kernel/init.c
+++ kernel/init.c
@@ -147,6 +147,11 @@ void _bss_zero(void)
 	(void)memset(&__ccm_bss_start, 0,
 		     ((u32_t) &__ccm_bss_end - (u32_t) &__ccm_bss_start));
 #endif
+#ifdef CONFIG_CODE_DATA_RELOCATION
+	extern void bss_zeroing_relocation(void);
+
+	bss_zeroing_relocation();
+#endif	/* CONFIG_CODE_DATA_RELOCATION */
 #ifdef CONFIG_APPLICATION_MEMORY
 	(void)memset(&__app_bss_start, 0,
 		     ((u32_t) &__app_bss_end - (u32_t) &__app_bss_start));
@@ -171,6 +176,11 @@ void _data_copy(void)
 	(void)memcpy(&__ccm_data_start, &__ccm_data_rom_start,
 		 ((u32_t) &__ccm_data_end - (u32_t) &__ccm_data_start));
 #endif
+#ifdef CONFIG_CODE_DATA_RELOCATION
+	extern void data_copy_xip_relocation(void);
+
+	data_copy_xip_relocation();
+#endif	/* CONFIG_CODE_DATA_RELOCATION */
 #ifdef CONFIG_APP_SHARED_MEM
 	(void)memcpy(&_app_smem_start, &_app_smem_rom_start,
 		 ((u32_t) &_app_smem_end - (u32_t) &_app_smem_start));
