128fef2c095924827d0a6ac4472c0a3179a2c151
Kumar Gala <kumar.gala@linaro.org>
2018-11-13 17:00:57 -0600

    dts: Fix builds of arc EMSK & NSIM SoCs
    With the DT_ prefix change we missed adding prefixes in dts_fixup.h
    on snps_emsk & snps_nsim for ARC_ICCM_0_BASE_ADDRESS and
    ARC_ICCM_0_SIZE.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- soc/arc/snps_nsim/dts_fixup.h
+++ soc/arc/snps_nsim/dts_fixup.h
@@ -4,7 +4,7 @@
 #define DT_DCCM_BASE_ADDRESS       DT_ARC_DCCM_80000000_BASE_ADDRESS
 #define DT_DCCM_SIZE               (DT_ARC_DCCM_80000000_SIZE >> 10)
 
-#define DT_ICCM_BASE_ADDRESS       ARC_ICCM_0_BASE_ADDRESS
-#define DT_ICCM_SIZE               (ARC_ICCM_0_SIZE >> 10)
+#define DT_ICCM_BASE_ADDRESS       DT_ARC_ICCM_0_BASE_ADDRESS
+#define DT_ICCM_SIZE               (DT_ARC_ICCM_0_SIZE >> 10)
 
 /* End of SoC Level DTS fixup file */
