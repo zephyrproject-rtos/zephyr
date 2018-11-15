87915cd36f9027cb6348684aa3289a127ac2f823
Kumar Gala <kumar.gala@linaro.org>
2018-11-15 11:51:50 -0600

    dts: stop generating CONFIG_FLASH_LOAD_{OFFSET,SIZE}
    As a step to completing removing use of Kconfig defines in dts files we
    need to change how we configure/set CONFIG_FLASH_LOAD_{OFFSET,SIZE}.
    
    Previously we would set them based on how the chosen property
    'zephyr,code-partition' was set to.  If 'zephyr,code-partition' wasn't
    set we would default the values to 0.  We had some generic overlay logic
    which would define 'zephyr,code-partition' based on
    CONFIG_BOOTLOADER_MCUBOOT.
    
    Going forward if the DTS has 'zephyr,code-partition' set we will
    generate DT_CODE_PARTITION_OFFSET & DT_CODE_PARTITION_SIZE defines.  We
    will leave it to Kconfig to set CONFIG_FLASH_LOAD_OFFSET &
    CONFIG_FLASH_LOAD_SIZE.
    
    We introduce a python script that allows Kconfig to extract data from
    the DTS and thus we can utilize DT_CODE_PARTITION_OFFSET and
    DT_CODE_PARTITION_SIZE values to set defaults for
    CONFIG_FLASH_LOAD_OFFSET & CONFIG_FLASH_LOAD_SIZE.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

---
+++ scripts/kconfig/kconfigfunctions.py
@@ -0,0 +1,81 @@
+#
+# Copyright (c) 2018-2019 Linaro
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+
+# Types we support
+# 'string', 'int', 'hex', 'bool'
+
+bin_dir = os.environ.get('PROJECT_BINARY_DIR')
+conf_file = os.path.join(bin_dir, 'include', 'generated',
+                         'generated_dts_board.conf') if bin_dir else None
+
+dt_defines = {}
+if conf_file and os.path.isfile(conf_file):
+    with open(conf_file, 'r', encoding='utf-8') as fd:
+        for line in fd:
+            if '=' in line:
+                define, val = line.split('=')
+                dt_defines[define] = val.strip()
+
+def _dt_units_to_scale(unit):
+    if not unit:
+        return 0
+    if unit in {'k', 'K'}:
+        return 10
+    if unit in {'m', 'M'}:
+        return 20
+    if unit in {'g', 'G'}:
+        return 30
+
+def dt_int_val(kconf, _, name, unit=None):
+    """
+    This function looks up 'name' in the DTS generated "conf" style database
+    and if its found it will return the value as an decimal integer.  The
+    function will divide the value based on 'unit':
+        None        No division
+        'k' or 'K'  divide by 1024 (1 << 10)
+        'm' or 'M'  divide by 1,048,576 (1 << 20)
+        'g' or 'G'  divide by 1,073,741,824 (1 << 30)
+    """
+    if name not in dt_defines:
+        return "0"
+
+    d = dt_defines[name]
+    if d.startswith(('0x', '0X')):
+        d = int(d, 16)
+    else:
+        d = int(d)
+    d >>= _dt_units_to_scale(unit)
+
+    return str(d)
+
+def dt_hex_val(kconf, _, name, unit=None):
+    """
+    This function looks up 'name' in the DTS generated "conf" style database
+    and if its found it will return the value as an hex integer.  The
+    function will divide the value based on 'unit':
+        None        No division
+        'k' or 'K'  divide by 1024 (1 << 10)
+        'm' or 'M'  divide by 1,048,576 (1 << 20)
+        'g' or 'G'  divide by 1,073,741,824 (1 << 30)
+    """
+    if name not in dt_defines:
+        return "0x0"
+
+    d = dt_defines[name]
+    if d.startswith(('0x', '0X')):
+        d = int(d, 16)
+    else:
+        d = int(d)
+    d >>= _dt_units_to_scale(unit)
+
+    return hex(d)
+
+
+functions = {
+        "dt_int_val": (dt_int_val, 1, 2),
+        "dt_hex_val": (dt_hex_val, 1, 2),
+}
