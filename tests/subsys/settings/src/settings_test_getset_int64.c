6ace20e189171b246821899ad908df52008ddd03
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2018-11-16 16:47:34 +0100

    tests: subsys: settings: common tests rework
    Introduction of stream-style encoding required tests reworks.
    Test for checking especially byte-string encoding is not required
    anymore as any value is kept as byte-string.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- tests/subsys/settings/src/settings_test_getset_int64.c
+++ tests/subsys/settings/src/settings_test_getset_int64.c
@@ -10,39 +10,43 @@
 void test_config_getset_int64(void)
 {
 	char name[80];
-	char tmp[64], *str;
+	char tmp[64];
 	int rc;
-	s64_t cmp;
+	s64_t new_val64;
 
+	new_val64 = 0x8012345678901234;
 	strcpy(name, "myfoo/mybar64");
-	rc = settings_set_value(name, "-9218247941279444428");
+	rc = settings_set_value(name, &new_val64, sizeof(s64_t));
 	zassert_true(rc == 0, "can't set value");
 	zassert_true(test_set_called == 1, "the SET handler wasn't called");
-	cmp = 0x8012345678901234;
-	zassert_true(memcmp(&val64, &cmp, sizeof(val64)) == 0,
+	zassert_equal(val64, 0x8012345678901234,
 		     "SET handler: was called with wrong parameters");
 	ctest_clear_call_state();
 
 	strcpy(name, "myfoo/mybar64");
-	str = settings_get_value(name, tmp, sizeof(tmp));
-	zassert_not_null(str, "the key value should been available");
+	rc = settings_get_value(name, tmp, sizeof(tmp));
+	zassert_equal(rc, sizeof(s64_t), "the key value should been available");
 	zassert_true(test_get_called == 1, "the GET handler wasn't called");
-	zassert_true(!strcmp("-9218247941279444428", tmp),
-			     "unexpected value fetched %s\n", tmp);
+	memcpy(&new_val64, tmp, sizeof(s64_t));
+	zassert_equal(new_val64, 0x8012345678901234,
+		      "unexpected value fetched %d", tmp);
 	ctest_clear_call_state();
 
+	new_val64 = 1;
 	strcpy(name, "myfoo/mybar64");
-	rc = settings_set_value(name, "1");
+	rc = settings_set_value(name, &new_val64, sizeof(s64_t));
 	zassert_true(rc == 0, "can't set value");
 	zassert_true(test_set_called == 1, "the SET handler wasn't called");
-	zassert_true(val64 == 1,
+	zassert_equal(val64, 1,
 		     "SET handler: was called with wrong parameters");
 	ctest_clear_call_state();
 
 	strcpy(name, "myfoo/mybar64");
-	str = settings_get_value(name, tmp, sizeof(tmp));
-	zassert_not_null(str, "the key value should been available");
+	rc = settings_get_value(name, tmp, sizeof(tmp));
+	zassert_equal(rc, sizeof(s64_t), "the key value should been available");
 	zassert_true(test_get_called == 1, "the GET handler wasn't called");
-	zassert_true(!strcmp("1", tmp), "unexpected value fetched");
+	memcpy(&new_val64, tmp, sizeof(s64_t));
+	zassert_equal(new_val64, 1,
+		      "unexpected value fetched %d", tmp);
 	ctest_clear_call_state();
 }
