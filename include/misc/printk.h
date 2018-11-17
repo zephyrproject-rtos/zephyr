423c76af6dae92181e91b0e616d1338c9fefef0f
Flavio Ceolin <flavio.ceolin@intel.com>
2018-11-16 19:06:59 -0800

    misra: Fixes for MISRA-C rule 8.2
    In C90 was introduced function prototype, that allows argument types
    to be checked against parameter types, though it is not necessary
    specify names for the parameters. MISRA-C requires names for function
    prototype parameters, it claims that names can provide useful
    information regarding the function interface.
    
    MISRA-C rule 8.2
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- include/misc/printk.h
+++ include/misc/printk.h
@@ -49,7 +49,7 @@ extern __printf_like(3, 4) int snprintk(char *str, size_t size,
 extern __printf_like(3, 0) int vsnprintk(char *str, size_t size,
 					  const char *fmt, va_list ap);
 
-extern __printf_like(3, 0) void _vprintk(int (*out)(int, void *), void *ctx,
+extern __printf_like(3, 0) void _vprintk(int (*out)(int f, void *c), void *ctx,
 					 const char *fmt, va_list ap);
 #else
 static inline __printf_like(1, 2) void printk(const char *fmt, ...)
