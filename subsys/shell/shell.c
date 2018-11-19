bc4cb76df58c515c117fea23d04ed1de9a037d54
Flavio Ceolin <flavio.ceolin@intel.com>
2018-11-19 14:49:20 -0800

    shell: Fix return error in shell_execute_cmd
    ENOEXEC should be used for executable file format error.
    While is undertandable return it when the command is wrong, this
    function is returning it two cases where other errors are more
    descriptive. When parameter is NULL, is better return EINVAL.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- subsys/shell/shell.c
+++ subsys/shell/shell.c
@@ -1437,7 +1437,7 @@ int shell_execute_cmd(const struct shell *shell, const char *cmd)
 	}
 
 	if (cmd_len > (CONFIG_SHELL_CMD_BUFF_SIZE - 1)) {
-		return -ENOEXEC;
+		return -ENOMEM;
 	}
 
 	if (shell == NULL) {
