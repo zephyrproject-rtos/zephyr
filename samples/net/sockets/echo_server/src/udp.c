1d4223ef3a790b6201135bfaaea317c58c07267d
Robert Lubos <robert.lubos@nordicsemi.no>
2018-11-19 12:19:55 +0100

    samples: sockets: Set correct log module name in echo_server
    echo_server used incorrect net_echo_client_* names for logger.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- samples/net/sockets/echo_server/src/udp.c
+++ samples/net/sockets/echo_server/src/udp.c
@@ -7,7 +7,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define LOG_MODULE_NAME net_echo_client_udp
+#define LOG_MODULE_NAME net_echo_server_udp
 #define NET_LOG_LEVEL LOG_LEVEL_DBG
 
 #include <zephyr.h>
