8ba5aff8c05795cc5d0e6a93a16265176279e766
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2018-11-19 14:50:42 +0100

    kconfig: Drop support for specifying CROSS_COMPILE through Kconfig
    When we move DT infront of Kconfig we are going to need access to a C
    toolchain before Kconfig is evaluated. This means it will not be
    possible to specify the toolchain used through Kconfig.
    
    To deal with this we ...
    
    Drop support for specifying CROSS_COMPILE through Kconfig. Still
    available is the ability to specify CROSS_COMPILE through the
    environment or through a CMake variable.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- cmake/toolchain/cross-compile.cmake
+++ cmake/toolchain/cross-compile.cmake
@@ -10,27 +10,12 @@
 # prefix. This interface is consisent with the other non-"Zephyr SDK"
 # toolchains.
 #
-# It can be set from three sources, the KConfig option
-# CONFIG_CROSS_COMPILE, the CMake var CROSS_COMPILE, or the env var
-# CROSS_COMPILE.
+# It can be set from either the environment or from a CMake variable
+# of the same name.
 #
 # The env var has the lowest precedence.
-#
-# It is not clear what should have the highest precedence of a Kconfig
-# option and a CMake var so an error is triggered if they are both
-# defined, but are defined with different values.
 
-if(CONFIG_CROSS_COMPILE AND CROSS_COMPILE)
-  if(NOT (${CONFIG_CROSS_COMPILE} STREQUAL ${CROSS_COMPILE}))
-    message(FATAL_ERROR "The Kconfig and CMake var's must match when defined simultaneously:
-CONFIG_CROSS_COMPILE: ${CONFIG_CROSS_COMPILE}
-CROSS_COMPILE: ${CROSS_COMPILE}")
-  endif()
-endif()
-
-if(CONFIG_CROSS_COMPILE)
-  set(CROSS_COMPILE ${CONFIG_CROSS_COMPILE})
-elseif(DEFINED ENV{CROSS_COMPILE})
+if((NOT (DEFINED CROSS_COMPILE)) AND (DEFINED ENV{CROSS_COMPILE}))
   set(CROSS_COMPILE $ENV{CROSS_COMPILE})
 endif()
 
