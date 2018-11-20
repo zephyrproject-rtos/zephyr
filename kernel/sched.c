566ebf6a72500085336d2e47e2bd9d8352e4e00a
Andy Ross <andrew.j.ross@intel.com>
2018-11-20 08:26:34 -0800

    kernel/timeout: Fix race with clock timeout setting
    The call to z_clock_set_timeout() was being made outside the timeout
    lock, which can race against other contexts setting sooner-expiring
    timeouts.
    
    Also add a long comment to one spot (timeslicing) where this call is
    made outside the timeout spinlock (inside the scheduler lock) and why
    this is OK.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- kernel/sched.c
+++ kernel/sched.c
@@ -206,6 +206,17 @@ static struct k_thread *next_up(void)
 static int slice_time;
 static int slice_max_prio;
 
+/* Subtle note on locking here: in theory we're subject to a race
+ * where _get_next_timeout_expiry() returns a value that changes
+ * between the return and the z_clock_set_timeout() of the adjusted
+ * time: another context can add a new timeout that should expire
+ * sooner, and we'll miss it.  But that's OK, because (1) we're inside
+ * the scheduler lock, so cannot be interrupted on uniprocessor
+ * systems, and (2) it's an interrupt time for our local CPU -- in
+ * SMP, it's safe to miss a timeout as long as another CPU (i.e. the
+ * one we're racing against) is available to wake up at the
+ * appropriate time.
+ */
 static void reset_time_slice(void)
 {
 	int to = _get_next_timeout_expiry();
