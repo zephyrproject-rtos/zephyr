329adf0ee09e439b400fab7fe102bc16f12c1c50
Endre Karlson <endre.karlson@gmail.com>
2018-11-21 22:19:01 +0100

    boards: particle_xenon: Add support Xenon
    This patch adds support for the Particle Xenon dev board that
    has a nrf52840 mcu.
    
    Signed-off-by: Endre Karlson <endre.karlson@gmail.com>

---
+++ boards/arm/particle_xenon/board.cmake
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2018, Endre Karlson
+#
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
+board_runner_args(nrfjprog "--nrf-family=NRF52")
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
