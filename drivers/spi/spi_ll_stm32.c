121d68f379002d4338c79b9a4125fff96c8de7b1
Erwan Gouriou <erwan.gouriou@linaro.org>
2018-11-21 13:54:21 +0100

    drivers/spi: stm32: Use device tree for device clock configuration
    Use DT generated symbols to configure clock bus/bits of STM32
    SPI devices.
    
    Fixes #10447
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- drivers/spi/spi_ll_stm32.c
+++ drivers/spi/spi_ll_stm32.c
@@ -486,13 +486,8 @@ static void spi_stm32_irq_config_func_1(struct device *port);
 static const struct spi_stm32_config spi_stm32_cfg_1 = {
 	.spi = (SPI_TypeDef *) DT_SPI_1_BASE_ADDRESS,
 	.pclken = {
-#ifdef CONFIG_SOC_SERIES_STM32F0X
-		.enr = LL_APB1_GRP2_PERIPH_SPI1,
-		.bus = STM32_CLOCK_BUS_APB1_2
-#else
-		.enr = LL_APB2_GRP1_PERIPH_SPI1,
-		.bus = STM32_CLOCK_BUS_APB2
-#endif
+		.enr = DT_SPI_1_CLOCK_BITS,
+		.bus = DT_SPI_1_CLOCK_BUS
 	},
 #ifdef CONFIG_SPI_STM32_INTERRUPT
 	.irq_config = spi_stm32_irq_config_func_1,
@@ -529,8 +524,8 @@ static void spi_stm32_irq_config_func_2(struct device *port);
 static const struct spi_stm32_config spi_stm32_cfg_2 = {
 	.spi = (SPI_TypeDef *) DT_SPI_2_BASE_ADDRESS,
 	.pclken = {
-		.enr = LL_APB1_GRP1_PERIPH_SPI2,
-		.bus = STM32_CLOCK_BUS_APB1
+		.enr = DT_SPI_2_CLOCK_BITS,
+		.bus = DT_SPI_2_CLOCK_BUS
 	},
 #ifdef CONFIG_SPI_STM32_INTERRUPT
 	.irq_config = spi_stm32_irq_config_func_2,
@@ -567,8 +562,8 @@ static void spi_stm32_irq_config_func_3(struct device *port);
 static const  struct spi_stm32_config spi_stm32_cfg_3 = {
 	.spi = (SPI_TypeDef *) DT_SPI_3_BASE_ADDRESS,
 	.pclken = {
-		.enr = LL_APB1_GRP1_PERIPH_SPI3,
-		.bus = STM32_CLOCK_BUS_APB1
+		.enr = DT_SPI_3_CLOCK_BITS,
+		.bus = DT_SPI_3_CLOCK_BUS
 	},
 #ifdef CONFIG_SPI_STM32_INTERRUPT
 	.irq_config = spi_stm32_irq_config_func_3,
