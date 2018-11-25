3bff9b1b50ada02454d30141a22c05726b4268d3
Himanshu Jha <himanshujha199640@gmail.com>
2018-11-25 17:19:42 +0530

    drivers: flash_gecko: remove unnecessary variable
    Remove an unnecessary local variable to store the
    return value, instead return directly thereby saving
    few bits of memory.
    
    Found using Coccinelle.
    
    Signed-off-by: Himanshu Jha <himanshujha199640@gmail.com>

--- drivers/flash/flash_gecko.c
+++ drivers/flash/flash_gecko.c
@@ -109,7 +109,6 @@ static int flash_gecko_erase(struct device *dev, off_t offset, size_t size)
 static int flash_gecko_write_protection(struct device *dev, bool enable)
 {
 	struct flash_gecko_data *const dev_data = DEV_DATA(dev);
-	int ret = 0;
 
 	k_sem_take(&dev_data->mutex, K_FOREVER);
 
@@ -123,7 +122,7 @@ static int flash_gecko_write_protection(struct device *dev, bool enable)
 
 	k_sem_give(&dev_data->mutex);
 
-	return ret;
+	return 0;
 }
 
 /* Note:
