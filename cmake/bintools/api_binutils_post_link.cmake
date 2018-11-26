974f8adc9add254b30a7ad40b111f61efb414bcd
Mark Ruvald Pedersen <mped@oticon.com>
2018-11-26 22:39:38 +0100

    cmake: adapt 'support for images without gaps' for abstracted toolchains
    This is the rebased version of:
    
      c086b935aa kconfig: cmake: Support for images without gaps.
    
    Signed-off-by: Mark Ruvald Pedersen <mped@oticon.com>

--- cmake/bintools/api_binutils_post_link.cmake
+++ cmake/bintools/api_binutils_post_link.cmake
@@ -1,21 +1,26 @@
 macro(toolchain_bintools_post_link)
 
+  if(NOT CONFIG_BUILD_NO_GAP_FILL)
+    # Use ';' as separator to get proper space in resulting command.
+    set(GAP_FILL "--gap-fill;0xff")
+  endif()
+
   list_append_ifdef(
     CONFIG_BUILD_OUTPUT_HEX
     post_link_commands
-    COMMAND ${CMAKE_OBJCOPY} -S -Oihex --gap-fill 0xFF  -R .comment -R COMMON -R .eh_frame  ${KERNEL_ELF_NAME}    ${KERNEL_HEX_NAME}
+    COMMAND ${CMAKE_OBJCOPY} -S -Oihex ${GAP_FILL}  -R .comment -R COMMON -R .eh_frame  ${KERNEL_ELF_NAME}    ${KERNEL_HEX_NAME}
   )
 
   list_append_ifdef(
     CONFIG_BUILD_OUTPUT_BIN
     post_link_commands
-    COMMAND ${CMAKE_OBJCOPY} -S -Obinary --gap-fill 0xFF -R .comment -R COMMON -R .eh_frame  ${KERNEL_ELF_NAME}    ${KERNEL_BIN_NAME}
+    COMMAND ${CMAKE_OBJCOPY} -S -Obinary ${GAP_FILL} -R .comment -R COMMON -R .eh_frame  ${KERNEL_ELF_NAME}    ${KERNEL_BIN_NAME}
   )
 
   list_append_ifdef(
     CONFIG_BUILD_OUTPUT_S19
     post_link_commands
-    COMMAND ${CMAKE_OBJCOPY} --gap-fill 0xFF --srec-len 1 --output-target=srec ${KERNEL_ELF_NAME} ${KERNEL_S19_NAME}
+    COMMAND ${CMAKE_OBJCOPY} ${GAP_FILL} --srec-len 1 --output-target=srec ${KERNEL_ELF_NAME} ${KERNEL_S19_NAME}
   )
 
   list_append_ifdef(
