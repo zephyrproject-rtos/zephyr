229b96b349dbd180bf29aa35ef8fca169db1e525
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-11-26 20:50:23 +0530

    samples: code_relocation: An example for code relocation feature.
    This sample provides an example for using the code relocation
    feature. This example will place text,data,bss from 3 files to
    various parts in the SRAM. For this a custom linker file is used
    which is derived from include/arch/arm/cortex_m/scripts/linker.ld.
    
    Signed-off-by: Varun Sharma <varun.sharma@intel.com>
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>

---
+++ samples/application_development/code_relocation/src/test_file2.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <misc/printk.h>
+
+void function_in_sram(s32_t value)
+{
+	printk("Address of function_in_sram %p\n", &function_in_sram);
+	printk("Hello World! %s\n", CONFIG_BOARD);
+}
