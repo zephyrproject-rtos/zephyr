d7974a312846af4d056c37136835ceee2a52ef41
Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>
2018-11-27 12:01:04 +0100

    shell: remove console dependencies
    Removed Console dependencies from shell uart backend.
    Generated define: CONFIG_UART_SHELL_ON_DEV_NAME for each board.
    
    Fixes #10191
    
    Signed-off-by: Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>

--- scripts/dts/extract/globals.py
+++ scripts/dts/extract/globals.py
@@ -25,6 +25,7 @@ regs_config = {
 
 name_config = {
     'zephyr,console'     : 'CONFIG_UART_CONSOLE_ON_DEV_NAME',
+    'zephyr,shell-uart'  : 'CONFIG_UART_SHELL_ON_DEV_NAME',
     'zephyr,bt-uart'     : 'CONFIG_BT_UART_ON_DEV_NAME',
     'zephyr,uart-pipe'   : 'CONFIG_UART_PIPE_ON_DEV_NAME',
     'zephyr,bt-mon-uart' : 'CONFIG_BT_MONITOR_ON_DEV_NAME',
