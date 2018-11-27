f62f755e0471de2bd4beeebb1dd757f1a4b8b278
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-11-27 12:00:31 +0100

    net/ipv4: Add new API to create/finalize IPv4 headers.
    This API is meant to work with pre-allocated net_pkt.
    It assumes net_pkt's buffer cursor is at the right position where to
    create the IPv4 header. Once done, the cursor will be placed right
    after the newly created IPv4 header.
    
    Finalizing assumes the same.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- subsys/net/ip/ipv4.h
+++ subsys/net/ip/ipv4.h
@@ -52,4 +52,31 @@ struct net_pkt *net_ipv4_create(struct net_pkt *pkt,
  */
 void net_ipv4_finalize(struct net_pkt *pkt, u8_t next_header_proto);
 
+
+/**
+ * @brief Create IPv4 packet in provided net_pkt.
+ *
+ * @param pkt Network packet
+ * @param src Source IPv4 address
+ * @param dst Destination IPv4 address
+ *
+ * @return 0 on success, negative errno otherwise.
+ */
+int net_ipv4_create_new(struct net_pkt *pkt,
+			const struct in_addr *src,
+			const struct in_addr *dst);
+
+/**
+ * @brief Finalize IPv4 packet. It should be called right before
+ * sending the packet and after all the data has been added into
+ * the packet. This function will set the length of the
+ * packet and calculate the higher protocol checksum if needed.
+ *
+ * @param pkt Network packet
+ * @param next_header_proto Protocol type of the next header after IPv4 header.
+ *
+ * @return 0 on success, negative errno otherwise.
+ */
+int net_ipv4_finalize_new(struct net_pkt *pkt, u8_t next_header_proto);
+
 #endif /* __IPV4_H */
