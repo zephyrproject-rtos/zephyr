e9364d52fea305bad3a87c4864c223858be1bf36
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-11-28 16:23:51 +0200

    net: Discard received extra data at the end of the IP message
    If we receive extra data at the end of the IP message, then
    discard that data and accept the packet.
    
    Fixes #11649
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- subsys/net/ip/ipv4.c
+++ subsys/net/ip/ipv4.c
@@ -117,9 +117,11 @@ enum net_verdict net_ipv4_process_pkt(struct net_pkt *pkt)
 	int pkt_len = ntohs(hdr->len);
 	enum net_verdict verdict = NET_DROP;
 
-	if (real_len != pkt_len) {
+	if (real_len < pkt_len) {
 		NET_DBG("IPv4 packet size %d pkt len %d", pkt_len, real_len);
 		goto drop;
+	} else if (real_len > pkt_len) {
+		net_pkt_pull(pkt, pkt_len, real_len - pkt_len);
 	}
 
 	if (net_ipv4_is_addr_mcast(&hdr->src)) {
