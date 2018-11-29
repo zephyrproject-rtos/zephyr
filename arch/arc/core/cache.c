d8dc9f3759f5fd46403a9da57789e2ff03e4917e
Flavio Ceolin <flavio.ceolin@intel.com>
2018-11-29 13:16:34 -0800

    arch: Making body of selection statement a compound statement
    MISRA-C rule 15.6
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- arch/arc/core/cache.c
+++ arch/arc/core/cache.c
@@ -58,9 +58,9 @@ static int dcache_available(void)
 
 static void dcache_dc_ctrl(u32_t dcache_en_mask)
 {
-	if (!dcache_available())
-		return;
-	_arc_v2_aux_reg_write(_ARC_V2_DC_CTRL, dcache_en_mask);
+	if (dcache_available()) {
+		_arc_v2_aux_reg_write(_ARC_V2_DC_CTRL, dcache_en_mask);
+	}
 }
 
 static void dcache_enable(void)
@@ -107,8 +107,9 @@ static void dcache_flush_mlines(u32_t start_addr, u32_t size)
 		/* wait for flush completion */
 		do {
 			if ((_arc_v2_aux_reg_read(_ARC_V2_DC_CTRL) &
-				DC_CTRL_FLUSH_STATUS) == 0)
+			     DC_CTRL_FLUSH_STATUS) == 0) {
 				break;
+			}
 		} while (1);
 		start_addr += DCACHE_LINE_SIZE;
 	} while (start_addr <= end_addr);
