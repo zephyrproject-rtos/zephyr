eff86a3cddbe7d25490e24cfef7d3cbcf6140c56
Erwan Gouriou <erwan.gouriou@linaro.org>
2018-11-29 11:08:41 +0100

    drivers/counter: stm32: Provide STM32 RTC driver using counter API
    Provides a STM32 RTC driver using new counter API.
    Driver does not support wrap related functions (set, get, ..)
    
    Fixes #11373
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- soc/arm/st_stm32/stm32f3/soc.h
+++ soc/arm/st_stm32/stm32f3/soc.h
@@ -56,7 +56,7 @@
 #include <stm32f3xx_ll_iwdg.h>
 #endif
 
-#ifdef CONFIG_RTC_STM32
+#if defined(CONFIG_RTC_STM32) || defined(CONFIG_COUNTER_RTC_STM32)
 #include <stm32f3xx_ll_rtc.h>
 #include <stm32f3xx_ll_exti.h>
 #include <stm32f3xx_ll_pwr.h>
