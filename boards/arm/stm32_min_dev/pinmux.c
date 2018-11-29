d5eb52cdf72f25f4aed31a86f57bd7813fa6a4cd
Stephen Yi <stephen.jin.yee@gmail.com>
2018-11-29 21:54:44 +0800

    boards stm32_min_dev: enable usb support
    Added usb device related configs to dts and pinmux.
    STM32F103C8 has on-chip fullspeed usb interface and the board connected
    ralated pins to a mini usb port and xephyr supports it.
    Sample cdc_acm and hid work out-of-box.
    
    Signed-off-by: Stephen Yi <stephen.jin.yee@gmail.com>

--- boards/arm/stm32_min_dev/pinmux.c
+++ boards/arm/stm32_min_dev/pinmux.c
@@ -33,6 +33,10 @@ static const struct pin_config pinconf[] = {
 #ifdef CONFIG_PWM_STM32_1
 	{STM32_PIN_PA8, STM32F1_PINMUX_FUNC_PA8_PWM1_CH1},
 #endif /* CONFIG_PWM_STM32_1 */
+#ifdef CONFIG_USB_DC_STM32
+	{STM32_PIN_PA11, STM32F1_PINMUX_FUNC_PA11_USB_DM},
+	{STM32_PIN_PA12, STM32F1_PINMUX_FUNC_PA12_USB_DP},
+#endif	/* CONFIG_USB_DC_STM32 */
 };
 
 static int pinmux_stm32_init(struct device *port)
