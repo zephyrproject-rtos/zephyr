6243a578aba49d1f365da14d43a85f473830cd2f
Nathaniel Graff <nathaniel.graff@sifive.com>
2018-11-30 15:17:43 -0800

    drivers/plic: Update PLIC driver with DTS symbols
    Use the DTS-generated symbols for accessing MMIO registers in the PLIC.
    
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- drivers/interrupt_controller/plic.c
+++ drivers/interrupt_controller/plic.c
@@ -42,7 +42,7 @@ void riscv_plic_irq_enable(u32_t irq)
 	u32_t key;
 	u32_t plic_irq = irq - RISCV_MAX_GENERIC_IRQ;
 	volatile u32_t *en =
-		(volatile u32_t *)PLIC_IRQ_EN_BASE_ADDR;
+		(volatile u32_t *)DT_PLIC_IRQ_EN_BASE_ADDR;
 
 	key = irq_lock();
 	en += (plic_irq >> 5);
@@ -68,7 +68,7 @@ void riscv_plic_irq_disable(u32_t irq)
 	u32_t key;
 	u32_t plic_irq = irq - RISCV_MAX_GENERIC_IRQ;
 	volatile u32_t *en =
-		(volatile u32_t *)PLIC_IRQ_EN_BASE_ADDR;
+		(volatile u32_t *)DT_PLIC_IRQ_EN_BASE_ADDR;
 
 	key = irq_lock();
 	en += (plic_irq >> 5);
@@ -88,7 +88,7 @@ void riscv_plic_irq_disable(u32_t irq)
 int riscv_plic_irq_is_enabled(u32_t irq)
 {
 	volatile u32_t *en =
-		(volatile u32_t *)PLIC_IRQ_EN_BASE_ADDR;
+		(volatile u32_t *)DT_PLIC_IRQ_EN_BASE_ADDR;
 	u32_t plic_irq = irq - RISCV_MAX_GENERIC_IRQ;
 
 	en += (plic_irq >> 5);
@@ -109,14 +109,14 @@ int riscv_plic_irq_is_enabled(u32_t irq)
 void riscv_plic_set_priority(u32_t irq, u32_t priority)
 {
 	volatile u32_t *prio =
-		(volatile u32_t *)PLIC_PRIO_BASE_ADDR;
+		(volatile u32_t *)DT_PLIC_PRIO_BASE_ADDR;
 
 	/* Can set priority only for PLIC-specific interrupt line */
 	if (irq <= RISCV_MAX_GENERIC_IRQ)
 		return;
 
-	if (priority > PLIC_MAX_PRIORITY)
-		priority = PLIC_MAX_PRIORITY;
+	if (priority > DT_PLIC_MAX_PRIORITY)
+		priority = DT_PLIC_MAX_PRIORITY;
 
 	prio += (irq - RISCV_MAX_GENERIC_IRQ);
 	*prio = priority;
@@ -140,7 +140,7 @@ int riscv_plic_get_irq(void)
 static void plic_irq_handler(void *arg)
 {
 	volatile struct plic_regs_t *regs =
-		(volatile struct plic_regs_t *)PLIC_REG_BASE_ADDR;
+		(volatile struct plic_regs_t *)DT_PLIC_REG_BASE_ADDR;
 
 	u32_t irq;
 	struct _isr_table_entry *ite;
@@ -186,11 +186,11 @@ static int plic_init(struct device *dev)
 	ARG_UNUSED(dev);
 
 	volatile u32_t *en =
-		(volatile u32_t *)PLIC_IRQ_EN_BASE_ADDR;
+		(volatile u32_t *)DT_PLIC_IRQ_EN_BASE_ADDR;
 	volatile u32_t *prio =
-		(volatile u32_t *)PLIC_PRIO_BASE_ADDR;
+		(volatile u32_t *)DT_PLIC_PRIO_BASE_ADDR;
 	volatile struct plic_regs_t *regs =
-		(volatile struct plic_regs_t *)PLIC_REG_BASE_ADDR;
+		(volatile struct plic_regs_t *)DT_PLIC_REG_BASE_ADDR;
 	int i;
 
 	/* Ensure that all interrupts are disabled initially */
