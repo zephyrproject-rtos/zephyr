348802a938162dcb8cfc48909852ccb4ffa51b04
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-12-04 16:20:25 +0200

    tests: net: tcp: Fix interface initialization
    The network interface mac address was not initialized properly
    which caused assert in net_if.c:2881 to fail.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- tests/net/tcp/src/main.c
+++ tests/net/tcp/src/main.c
@@ -93,6 +93,34 @@ int net_tcp_dev_init(struct device *dev)
 
 static void net_tcp_iface_init(struct net_if *iface)
 {
+	static u8_t mac_addr_1[6];
+	static u8_t mac_addr_2[6];
+
+	if (mac_addr_1[0] == 0) {
+		/* 00-00-5E-00-53-xx Documentation RFC 7042 */
+		mac_addr_1[0] = 0x00;
+		mac_addr_1[1] = 0x00;
+		mac_addr_1[2] = 0x5E;
+		mac_addr_1[3] = 0x00;
+		mac_addr_1[4] = 0x53;
+		mac_addr_1[5] = 0x01;
+
+		net_if_set_link_addr(iface, mac_addr_1, 6,
+				     NET_LINK_ETHERNET);
+	}
+
+	if (mac_addr_2[0] == 0) {
+		mac_addr_2[0] = 0x00;
+		mac_addr_2[1] = 0x00;
+		mac_addr_2[2] = 0x5E;
+		mac_addr_2[3] = 0x00;
+		mac_addr_2[4] = 0x53;
+		mac_addr_2[5] = 0x02;
+
+		net_if_set_link_addr(iface, mac_addr_2, 6,
+				     NET_LINK_ETHERNET);
+	}
+
 	return;
 }
 
