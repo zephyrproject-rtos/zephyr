74340549378698b1a3de18d0ab1351f115e959de
Erwan Gouriou <erwan.gouriou@linaro.org>
2018-12-05 09:25:39 +0100

    drivers/gpio: stm32: return error when pin line already in use
    GPIO pin interrupts share common EXTI resources.
    Return an error when attempt to configure a line already in use.
    
    Fixes #10611
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- drivers/interrupt_controller/exti_stm32.h
+++ drivers/interrupt_controller/exti_stm32.h
@@ -68,7 +68,8 @@ typedef void (*stm32_exti_callback_t) (int line, void *user);
  * @param cb   user callback
  * @param arg  user arg
  */
-void stm32_exti_set_callback(int line, stm32_exti_callback_t cb, void *data);
+int stm32_exti_set_callback(int line, int port, stm32_exti_callback_t cb,
+				void *data);
 
 /**
  * @brief unset EXTI interrupt callback
