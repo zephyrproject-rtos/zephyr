fa4c07120cd2d6b70f9c7a814b2a91be8e52baf0
Johann Fischer <j.fischer@phytec.de>
2018-12-07 16:16:37 +0100

    boards: usb_kw24d512: add support for DAPLink based adapters
    Add support for using DAPLink based adapters through pyOCD.
    
    Signed-off-by: Johann Fischer <j.fischer@phytec.de>

--- boards/arm/usb_kw24d512/board.cmake
+++ boards/arm/usb_kw24d512/board.cmake
@@ -1,3 +1,5 @@
 board_runner_args(jlink "--device=MKW24D512xxx5" "--speed=4000")
+board_runner_args(pyocd "--target=kw24d5")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
