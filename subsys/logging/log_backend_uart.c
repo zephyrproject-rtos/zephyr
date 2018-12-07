bde6a6bf42d8c9d1b6392b6ddb38083a9ab64478
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2018-12-07 10:35:02 +0100

    logging: Add handling of dropped messages to UART backend
    Extend UART backend to handle dropped messages.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

--- subsys/logging/log_backend_uart.c
+++ subsys/logging/log_backend_uart.c
@@ -62,10 +62,18 @@ static void panic(struct log_backend const *const backend)
 {
 }
 
+static void dropped(const struct log_backend *const backend, u32_t cnt)
+{
+	ARG_UNUSED(backend);
+
+	log_output_dropped_process(&log_output, cnt);
+}
+
 const struct log_backend_api log_backend_uart_api = {
 	.put = put,
 	.panic = panic,
 	.init = log_backend_uart_init,
+	.dropped = dropped,
 };
 
 LOG_BACKEND_DEFINE(log_backend_uart, log_backend_uart_api, true);
