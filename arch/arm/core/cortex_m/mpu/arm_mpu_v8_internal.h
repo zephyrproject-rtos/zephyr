2f6904f9cb07f2e57cce7967dea206895005ed52
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-12-09 17:53:04 +0100

    arch: arm: mpu: group together USERSPACE-related implementation
    This commit moves all internal implementation, relevant for
    User mode, in a single place in the arm_mpu_v7_internal.h,
    arm_mpu_v8_internal, and in arm_mpu.c. Additionally, the
    commit cleans up internal function _get_region_attr() that
    is not used any more.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- arch/arm/core/cortex_m/mpu/arm_mpu_v8_internal.h
+++ arch/arm/core/cortex_m/mpu/arm_mpu_v8_internal.h
@@ -222,6 +222,29 @@ static inline int _is_enabled_region(u32_t index)
 	return (MPU->RLAR & MPU_RLAR_EN_Msk) ? 1 : 0;
 }
 
+/**
+ * This internal function validates whether a given memory buffer
+ * is user accessible or not.
+ */
+static inline int _mpu_buffer_validate(void *addr, size_t size, int write)
+{
+	u32_t _addr = (u32_t)addr;
+	u32_t _size = (u32_t)size;
+
+	if (write) {
+		if (arm_cmse_addr_range_readwrite_ok(_addr, _size, 1)) {
+			return 0;
+		}
+	} else {
+		if (arm_cmse_addr_range_read_ok(_addr, _size, 1)) {
+			return 0;
+		}
+	}
+
+	return -EPERM;
+}
+
+
 #endif /* CONFIG_USERSPACE */
 
 static int _region_allocate_and_init(const u8_t index,
@@ -468,92 +491,5 @@ static int _mpu_configure_dynamic_mpu_regions(const struct k_mem_partition
 
 	return mpu_reg_index;
 }
-
-#if defined(CONFIG_USERSPACE) || defined(CONFIG_MPU_STACK_GUARD) || \
-	defined(CONFIG_APPLICATION_MEMORY)
-
-/**
- * This internal function allocates default RAM cache-ability, share-ability,
- * and execution allowance attributes along with the requested access
- * permissions and size.
- */
-static inline void _get_mpu_ram_region_attr(arm_mpu_region_attr_t *p_attr,
-	u32_t ap, u32_t base, u32_t size)
-{
-	p_attr->rbar = ((1UL << MPU_RBAR_XN_Pos) & MPU_RBAR_XN_Msk)
-		| ((ap << MPU_RBAR_AP_Pos) & MPU_RBAR_AP_Msk);
-	p_attr->mair_idx = MPU_MAIR_INDEX_SRAM;
-	p_attr->r_limit = REGION_LIMIT_ADDR(base, size);
-}
-
-/**
- * This internal function is utilized by the MPU driver to combine a given
- * MPU RAM attribute configuration and region size and fill-in a structure with
- * the correct parameter set.
- */
-static inline void _get_ram_region_attr_by_conf(arm_mpu_region_attr_t *p_attr,
-	k_mem_partition_attr_t *attr, u32_t base, u32_t size)
-{
-	p_attr->rbar = attr->rbar & (MPU_RBAR_XN_Msk | MPU_RBAR_AP_Msk);
-	p_attr->mair_idx = attr->mair_idx;
-	p_attr->r_limit = REGION_LIMIT_ADDR(base, size);
-}
-
-/**
- * This internal function checks if region is enabled or not.
- *
- * Note:
- *   The caller must provide a valid region number.
- */
-static inline int _is_enabled_region(u32_t r_index)
-{
-	MPU->RNR = r_index;
-
-	return MPU->RLAR & MPU_RLAR_EN_Msk;
-}
-
-/**
- * This internal function checks if the given buffer is in the region.
- *
- * Note:
- *   The caller must provide a valid region number.
- */
-static inline int _is_in_region(u32_t r_index, u32_t start, u32_t size)
-{
-	u32_t region_start_addr = arm_cmse_mpu_region_get(start);
-	u32_t region_end_addr = arm_cmse_mpu_region_get(start + size - 1);
-
-	/* MPU regions are contiguous so return true if both start and end address
-	 * are in the same region and this region is indexed by r_index.
-	 */
-	if ((region_start_addr == r_index) && (region_end_addr == r_index)) {
-		return 1;
-	}
-
-	return 0;
-}
-
-/**
- * This internal function validates whether a given memory buffer
- * is user accessible or not.
- */
-static inline int _mpu_buffer_validate(void *addr, size_t size, int write)
-{
-	u32_t _addr = (u32_t)addr;
-	u32_t _size = (u32_t)size;
-
-	if (write) {
-		if (arm_cmse_addr_range_readwrite_ok(_addr, _size, 1)) {
-			return 0;
-		}
-	} else {
-		if (arm_cmse_addr_range_read_ok(_addr, _size, 1)) {
-			return 0;
-		}
-	}
-
-	return -EPERM;
-}
-#endif /* USERSPACE || MPU_STACK_GUARD || APPLICATION_MEMORY */
 
 #endif	/* ZEPHYR_ARCH_ARM_CORE_CORTEX_M_MPU_ARM_MPU_V8_INTERNAL_H_ */
