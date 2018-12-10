e36762753f866b6be58c662ae7fa50868258db5e
Kumar Gala <kumar.gala@linaro.org>
2018-12-10 08:10:56 -0600

    drivers: wifi: eswifi: Fix build issue due to -fno-common
    The introduction of -fno-common caused a link error in eswifi driver due
    to lack of missing extern on eswifi_bus_ops_spi.  Adding the extern
    resolves the link error.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- drivers/wifi/eswifi/eswifi.h
+++ drivers/wifi/eswifi/eswifi.h
@@ -97,7 +97,7 @@ static inline void eswifi_unlock(struct eswifi_dev *eswifi)
 	k_mutex_unlock(&eswifi->mutex);
 }
 
-struct eswifi_bus_ops eswifi_bus_ops_spi;
+extern struct eswifi_bus_ops eswifi_bus_ops_spi;
 int eswifi_offload_init(struct eswifi_dev *eswifi);
 struct eswifi_dev *eswifi_by_iface_idx(u8_t iface);
 bool eswifi_is_buf_at_ok(char *str);
