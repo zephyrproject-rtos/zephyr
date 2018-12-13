c338dd9b94a468810fd86238a7b5405e222b57b9
Erwan Gouriou <erwan.gouriou@linaro.org>
2018-12-13 15:53:33 +0100

    samples/drivers: gpio: LED0 and SW0 might not share GPIO controller
    In gpio driver sample, same GPIO controller was used for out pin
    (LED0) and in pin (SW0), which is actually a particular case.
    
    This change implements the general case, where button an led might
    not have the same gpio controller.
    Tested on nucleo_f429zi and frdm_k64f.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- samples/drivers/gpio/src/main.c
+++ samples/drivers/gpio/src/main.c
@@ -21,8 +21,9 @@
 #include <misc/util.h>
 
 #if defined(SW0_GPIO_CONTROLLER) && defined(LED0_GPIO_CONTROLLER)
-#define GPIO_DRV_NAME LED0_GPIO_CONTROLLER
+#define GPIO_OUT_DRV_NAME LED0_GPIO_CONTROLLER
 #define GPIO_OUT_PIN  LED0_GPIO_PIN
+#define GPIO_IN_DRV_NAME SW0_GPIO_CONTROLLER
 #define GPIO_INT_PIN  SW0_GPIO_PIN
 #else
 #error Change the pins based on your configuration. This sample \
@@ -39,30 +40,36 @@ static struct gpio_callback gpio_cb;
 
 void main(void)
 {
-	struct device *gpio_dev;
+	struct device *gpio_out_dev, *gpio_in_dev;
 	int ret;
 	int toggle = 1;
 
-	gpio_dev = device_get_binding(GPIO_DRV_NAME);
-	if (!gpio_dev) {
-		printk("Cannot find %s!\n", GPIO_DRV_NAME);
+	gpio_out_dev = device_get_binding(GPIO_OUT_DRV_NAME);
+	if (!gpio_out_dev) {
+		printk("Cannot find %s!\n", GPIO_OUT_DRV_NAME);
+		return;
+	}
+
+	gpio_in_dev = device_get_binding(GPIO_IN_DRV_NAME);
+	if (!gpio_in_dev) {
+		printk("Cannot find %s!\n", GPIO_IN_DRV_NAME);
 		return;
 	}
 
 	/* Setup GPIO output */
-	ret = gpio_pin_configure(gpio_dev, GPIO_OUT_PIN, (GPIO_DIR_OUT));
+	ret = gpio_pin_configure(gpio_out_dev, GPIO_OUT_PIN, (GPIO_DIR_OUT));
 	if (ret) {
 		printk("Error configuring pin %d!\n", GPIO_OUT_PIN);
 	}
 
 	/* Setup GPIO input, and triggers on rising edge. */
 #ifdef CONFIG_SOC_CC2650
-	ret = gpio_pin_configure(gpio_dev, GPIO_INT_PIN,
+	ret = gpio_pin_configure(gpio_in_dev, GPIO_INT_PIN,
 				 (GPIO_DIR_IN | GPIO_INT |
 				  GPIO_INT_EDGE | GPIO_INT_ACTIVE_HIGH |
 				  GPIO_INT_DEBOUNCE | GPIO_PUD_PULL_UP));
 #else
-	ret = gpio_pin_configure(gpio_dev, GPIO_INT_PIN,
+	ret = gpio_pin_configure(gpio_in_dev, GPIO_INT_PIN,
 				 (GPIO_DIR_IN | GPIO_INT |
 				  GPIO_INT_EDGE | GPIO_INT_ACTIVE_HIGH |
 				  GPIO_INT_DEBOUNCE));
@@ -73,12 +80,12 @@ void main(void)
 
 	gpio_init_callback(&gpio_cb, gpio_callback, BIT(GPIO_INT_PIN));
 
-	ret = gpio_add_callback(gpio_dev, &gpio_cb);
+	ret = gpio_add_callback(gpio_in_dev, &gpio_cb);
 	if (ret) {
 		printk("Cannot setup callback!\n");
 	}
 
-	ret = gpio_pin_enable_callback(gpio_dev, GPIO_INT_PIN);
+	ret = gpio_pin_enable_callback(gpio_in_dev, GPIO_INT_PIN);
 	if (ret) {
 		printk("Error enabling callback!\n");
 	}
@@ -86,7 +93,7 @@ void main(void)
 	while (1) {
 		printk("Toggling pin %d\n", GPIO_OUT_PIN);
 
-		ret = gpio_pin_write(gpio_dev, GPIO_OUT_PIN, toggle);
+		ret = gpio_pin_write(gpio_out_dev, GPIO_OUT_PIN, toggle);
 		if (ret) {
 			printk("Error set pin %d!\n", GPIO_OUT_PIN);
 		}
