821ea1bec9736d8de7c3fd135e8d5102eb4ff9d0
Christoph Schramm <schramm@makaio.com>
2018-12-17 13:48:41 +0100

    soc: nrf52840: add configuration option for REG0 output voltage
    Enables setting REG0 output voltage via configuration
    
    Signed-off-by: Christoph Schramm <schramm@makaio.com>

--- soc/arm/nordic_nrf/nrf52/soc.c
+++ soc/arm/nordic_nrf/nrf52/soc.c
@@ -38,9 +38,28 @@ extern void _NmiInit(void);
 #include <nrf.h>
 #include <hal/nrf_power.h>
 
+/**
+ * @brief REG0 voltage definitions for nRF52840
+ */
+#if defined(CONFIG_NRF52840_REG0_1V8)
+#define NRF52840_REG0_VOUT	UICR_REGOUT0_VOUT_1V8
+#elif defined(CONFIG_NRF52840_REG0_2V1)
+#define NRF52840_REG0_VOUT	UICR_REGOUT0_VOUT_2V1
+#elif defined(CONFIG_NRF52840_REG0_2V4)
+#define NRF52840_REG0_VOUT	UICR_REGOUT0_VOUT_2V4
+#elif defined(CONFIG_NRF52840_REG0_2V7)
+#define NRF52840_REG0_VOUT	UICR_REGOUT0_VOUT_2V7
+#elif defined(CONFIG_NRF52840_REG0_3V0)
+#define NRF52840_REG0_VOUT	UICR_REGOUT0_VOUT_3V0
+#elif defined(CONFIG_NRF52840_REG0_3V3)
+#define NRF52840_REG0_VOUT	UICR_REGOUT0_VOUT_3V3
+#endif
+
 #define LOG_LEVEL CONFIG_SOC_LOG_LEVEL
 #include <logging/log.h>
 LOG_MODULE_REGISTER(soc);
+
+static void nordicsemi_nrf52840_reg0_voltage_set(void);
 
 static int nordicsemi_nrf52_init(struct device *arg)
 {
@@ -55,6 +74,10 @@ static int nordicsemi_nrf52_init(struct device *arg)
 #ifdef CONFIG_NRF_ENABLE_ICACHE
 	/* Enable the instruction cache */
 	NRF_NVMC->ICACHECNF = NVMC_ICACHECNF_CACHEEN_Msk;
+#endif
+
+#if defined(NRF_POWER_REG0_VOLT)
+	nordicsemi_nrf52840_reg0_voltage_set();
 #endif
 
 #if defined(CONFIG_SOC_DCDC_NRF52X)
@@ -76,6 +99,39 @@ static int nordicsemi_nrf52_init(struct device *arg)
 void z_arch_busy_wait(u32_t time_us)
 {
 	nrfx_coredep_delay_us(time_us);
+}
+
+/**
+ * @brief Sets REG0 output voltage for nRF52840
+ *
+ * @note  REG0 output voltage will only be set for reseted boards
+ * 		  as function checks if current voltage is DEFAULT
+ * 		  (factory default)
+ *
+ */
+static void nordicsemi_nrf52840_reg0_voltage_set(void)
+{
+	if ((nrf_power_mainregstatus_get() == NRF_POWER_MAINREGSTATUS_HIGH) &&
+		((NRF_UICR->REGOUT0 & UICR_REGOUT0_VOUT_Msk) ==
+		 (UICR_REGOUT0_VOUT_DEFAULT << UICR_REGOUT0_VOUT_Pos))) {
+
+		NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
+		while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
+			;
+		}
+
+		NRF_UICR->REGOUT0 =
+				(NRF_UICR->REGOUT0 & ~((uint32_t)UICR_REGOUT0_VOUT_Msk)) |
+				(NRF52840_REG0_VOUT << UICR_REGOUT0_VOUT_Pos);
+
+		NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
+		while (NRF_NVMC->READY == NVMC_READY_READY_Busy) {
+			;
+		}
+
+		/* a reset is required for changes to take effect */
+		NVIC_SystemReset();
+	}
 }
 
 SYS_INIT(nordicsemi_nrf52_init, PRE_KERNEL_1, 0);
