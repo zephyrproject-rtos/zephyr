2b628776f84f56db0732d8071e64176601de3b84
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-12-20 15:42:07 +0530

    app_memdomain: Optimize the app_shared_mem feature.
    The app shared memory feature was built to read linker variables
    and then update the size of various regions at runtime.
    Due to the optimizations of the C language and the linker these
    variables were not reflecting the correct values. Therefore the
    size of the bss and the data regions if not correctly configured
    would cause a kernel oops.
    
    The python script gen_app_partitions.py will create sections and
    linker variables to place the application shared memory partitions.
    These are later read by the macros defined in app_memdomain.h for
    initialization purpose when APP_SHARED_MEM is enabled.
    
    This patch will read these linker defined variables and configure
    the required structures. This enabled in the removal of the API
    app_calc_size() from app_memdomain.c
    This new feature will reduce the compiled code and will decrease
    the boot time.
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>

--- subsys/app_memory/app_memdomain.c
+++ subsys/app_memory/app_memdomain.c
@@ -13,7 +13,7 @@ sys_dlist_t app_mem_list = SYS_DLIST_STATIC_INIT(&app_mem_list);
  * The following zeroizes each "bss" part of each subsection
  * as per the entries in the list.
  */
-void app_bss_zero(void)
+static inline void app_bss_zero(void)
 {
 	sys_dnode_t *node, *next_node;
 
@@ -22,67 +22,6 @@ void app_bss_zero(void)
 		struct app_region *region =
 			CONTAINER_OF(node, struct app_region, lnode);
 		(void)memset(region->bmem_start, 0, region->bmem_size);
-	}
-}
-
-/*
- * The following calculates the size of each subsection and adds
- * the computed sizes to the region structures. These calculations
- * are needed both for zeroizing "bss" parts of the partitions and
- * for the creation of the k_mem_partition.
- */
-void app_calc_size(void)
-{
-	sys_dnode_t *node, *next_node;
-
-	SYS_DLIST_FOR_EACH_NODE_SAFE(&app_mem_list, node, next_node)
-	{
-#ifndef CONFIG_MPU_REQUIRES_POWER_OF_TWO_ALIGNMENT
-		if (sys_dlist_is_tail(&app_mem_list, node)) {
-			struct app_region *region =
-				CONTAINER_OF(node, struct app_region, lnode);
-			region->bmem_size =
-				_app_smem_end -
-				(char *)region->bmem_start;
-			region->dmem_size =
-				(char *)region->bmem_start -
-				(char *)region->dmem_start;
-			region->smem_size =
-				region->bmem_size + region->dmem_size;
-			region->partition[0].size =
-				region->dmem_size + region->bmem_size;
-		} else {
-			struct app_region *region =
-				CONTAINER_OF(node, struct app_region, lnode);
-			struct app_region *nRegion =
-				CONTAINER_OF(next_node, struct app_region,
-					lnode);
-			region->bmem_size =
-				(char *)nRegion->dmem_start -
-				(char *)region->bmem_start;
-			region->dmem_size =
-				(char *)region->bmem_start -
-				(char *)region->dmem_start;
-			region->smem_size =
-				region->bmem_size + region->dmem_size;
-			region->partition[0].size =
-				region->dmem_size + region->bmem_size;
-		}
-
-#else
-		/* For power of 2 MPUs linker provides support to help us
-		 * calculate the region sizes.
-		 */
-		struct app_region *region =
-				CONTAINER_OF(node, struct app_region, lnode);
-
-		region->dmem_size = (char *)region->bmem_start -
-			(char *)region->dmem_start;
-		region->bmem_size = (char *)region->smem_size -
-			(char *)region->dmem_size;
-
-		region->partition[0].size = (s32_t)region->smem_size;
-#endif	/* CONFIG_MPU_REQUIRES_POWER_OF_TWO_ALIGNMENT */
 	}
 }
 
@@ -102,7 +41,6 @@ void appmem_init_app_memory(void)
 	arc_core_mpu_disable();
 #endif
 
-	app_calc_size();
 	app_bss_zero();
 
 #ifdef CONFIG_ARC
