585bc0996c18bd06e458e7d7884c080b79f93d42
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-12-28 14:32:33 +0530

    tests: net: socket: getaddrinfo: Updated for APP_SHARED_MEM.
    This test case uses libc from user threads. In order to support
    libc, a partition was created and initialized. This is also
    supported by the gen_app_partitions.py script. The script will
    push all the required data/bss to the correct location inside the
    partition. Thus supporting libc from user threads.
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>

--- tests/net/socket/getaddrinfo/src/main.c
+++ tests/net/socket/getaddrinfo/src/main.c
@@ -12,6 +12,9 @@ LOG_MODULE_REGISTER(net_test, CONFIG_NET_SOCKETS_LOG_LEVEL);
 
 #include <net/socket.h>
 
+APPMEM_PARTITION_OBJECT_DEFINE(libc)
+APPMEM_PARTITION_HEADER_DEFINE(libc)
+
 void test_getaddrinfo_ok(void)
 {
 	struct addrinfo *res = NULL;
@@ -44,6 +47,12 @@ void test_getaddrinfo_no_host(void)
 
 void test_main(void)
 {
+#ifdef CONFIG_APP_SHARED_MEM
+	APPMEM_INIT_PARTITIONS(libc);
+	appmem_init_app_memory();
+	appmem_add_part_ztest_dom0(libc);
+#endif	/* CONFIG_APP_SHARED_MEM */
+
 	k_thread_system_pool_assign(k_current_get());
 
 	ztest_test_suite(socket_getaddrinfo,
