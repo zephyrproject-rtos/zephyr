df7d4381a7ee29367b632023d56a9f8421fcc962
Spoorthi K <spoorthi.k@intel.com>
2019-01-01 21:22:46 +0530

    tests: cmsis_rtos_v2: Enhance mempool tests
    Add negative tests for mempool APIs supported
    in CMSIS RTOS V2 layer.
    
    Signed-off-by: Spoorthi K <spoorthi.k@intel.com>

--- tests/cmsis_rtos_v2/src/mempool.c
+++ tests/cmsis_rtos_v2/src/mempool.c
@@ -8,8 +8,8 @@
 #include <kernel.h>
 #include <cmsis_os2.h>
 
-#define MAX_BLOCKS	10
-#define TIMEOUT_TICKS	10
+#define MAX_BLOCKS      10
+#define TIMEOUT_TICKS   10
 
 struct mem_block {
 	int member1;
@@ -35,55 +35,76 @@ void test_mempool(void)
 {
 	int i;
 	osMemoryPoolId_t mp_id;
+	osMemoryPoolId_t dummy_id = NULL;
 	const char *name;
 	struct mem_block *addr_list[MAX_BLOCKS + 1];
 	osStatus_t status;
+
+	/* Create memory pool with invalid block size */
+	mp_id = osMemoryPoolNew(MAX_BLOCKS + 1, sizeof(struct mem_block),
+				&mp_attrs);
+	zassert_true(mp_id == NULL, "osMemoryPoolNew worked unexpectedly!");
 
 	mp_id = osMemoryPoolNew(MAX_BLOCKS, sizeof(struct mem_block),
 				&mp_attrs);
 	zassert_true(mp_id != NULL, "mempool creation failed");
 
+	zassert_true(osMemoryPoolGetName(dummy_id) == NULL,
+		     "Something's wrong with osMemoryPoolGetName!");
+
 	name = osMemoryPoolGetName(mp_id);
 	zassert_true(strcmp(mp_attrs.name, name) == 0,
-			"Error getting mempool name");
+		     "Error getting mempool name");
+
+	zassert_equal(osMemoryPoolGetCapacity(dummy_id), 0,
+		      "Something's wrong with osMemoryPoolGetCapacity!");
 
 	zassert_equal(osMemoryPoolGetCapacity(mp_id), MAX_BLOCKS,
-			"Something's wrong with osMemoryPoolGetCapacity!");
+		      "Something's wrong with osMemoryPoolGetCapacity!");
+
+	zassert_equal(osMemoryPoolGetBlockSize(dummy_id), 0,
+		      "Something's wrong with osMemoryPoolGetBlockSize!");
 
 	zassert_equal(osMemoryPoolGetBlockSize(mp_id),
-			sizeof(struct mem_block),
-			"Something's wrong with osMemoryPoolGetBlockSize!");
+		      sizeof(struct mem_block),
+		      "Something's wrong with osMemoryPoolGetBlockSize!");
 
 	/* The memory pool should be completely available at this point */
 	zassert_equal(osMemoryPoolGetCount(mp_id), 0,
-			"Something's wrong with osMemoryPoolGetCount!");
+		      "Something's wrong with osMemoryPoolGetCount!");
 	zassert_equal(osMemoryPoolGetSpace(mp_id), MAX_BLOCKS,
-			"Something's wrong with osMemoryPoolGetSpace!");
+		      "Something's wrong with osMemoryPoolGetSpace!");
 
 	for (i = 0; i < MAX_BLOCKS; i++) {
 		addr_list[i] = (struct mem_block *)osMemoryPoolAlloc(mp_id,
-								osWaitForever);
+								     osWaitForever);
 		zassert_true(addr_list[i] != NULL, "mempool allocation failed");
 	}
 
 	/* The memory pool should be completely in use at this point */
 	zassert_equal(osMemoryPoolGetCount(mp_id), MAX_BLOCKS,
-			"Something's wrong with osMemoryPoolGetCount!");
+		      "Something's wrong with osMemoryPoolGetCount!");
 	zassert_equal(osMemoryPoolGetSpace(mp_id), 0,
-			"Something's wrong with osMemoryPoolGetSpace!");
+		      "Something's wrong with osMemoryPoolGetSpace!");
 
 	/* All blocks in mempool are allocated, any more allocation
 	 * without free should fail
 	 */
 	addr_list[i] = (struct mem_block *)osMemoryPoolAlloc(mp_id,
-								TIMEOUT_TICKS);
+							     TIMEOUT_TICKS);
 	zassert_true(addr_list[i] == NULL, "allocation happened."
-			" Something's wrong!");
+		     " Something's wrong!");
+
+	zassert_equal(osMemoryPoolFree(dummy_id, addr_list[0]),
+		      osErrorParameter, "mempool free worked unexpectedly!");
 
 	for (i = 0; i < MAX_BLOCKS; i++) {
 		status = osMemoryPoolFree(mp_id, addr_list[i]);
 		zassert_true(status == osOK, "mempool free failed");
 	}
+
+	zassert_equal(osMemoryPoolDelete(dummy_id), osErrorParameter,
+		      "mempool delete worked unexpectedly!");
 
 	status = osMemoryPoolDelete(mp_id);
 	zassert_true(status == osOK, "mempool delete failure");
