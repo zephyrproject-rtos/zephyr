0f375034da3bcfd3619f54392b0c442243c28090
Spoorthi K <spoorthi.k@intel.com>
2019-01-01 21:18:40 +0530

    tests: cmsis_rtos_v2: Add tests for kernel management APIs
    Add negative scenarios, API tests and improve
    test coverage for kernel management APIs supported
    in CMSIS RTOS V2 portability layer.
    
    Signed-off-by: Spoorthi K <spoorthi.k@intel.com>

--- tests/cmsis_rtos_v2/src/main.c
+++ tests/cmsis_rtos_v2/src/main.c
@@ -8,6 +8,8 @@
 #include <kernel.h>
 #include <cmsis_os2.h>
 
+extern void test_kernel_apis(void);
+extern void test_delay(void);
 extern void test_thread_apis(void);
 extern void test_thread_prio(void);
 extern void test_timer(void);
@@ -26,20 +28,22 @@ extern void test_thread_flags_isr(void);
 void test_main(void)
 {
 	ztest_test_suite(test_cmsis_v2_apis,
-			ztest_unit_test(test_thread_apis),
-			ztest_unit_test(test_thread_prio),
-			ztest_unit_test(test_timer),
-			ztest_unit_test(test_mutex),
-			ztest_unit_test(test_mutex_lock_timeout),
-			ztest_unit_test(test_semaphore),
-			ztest_unit_test(test_mempool),
-			ztest_unit_test(test_messageq),
-			ztest_unit_test(test_event_flags_no_wait_timeout),
-			ztest_unit_test(test_event_flags_signalled),
-			ztest_unit_test(test_event_flags_isr),
-			ztest_unit_test(test_thread_flags_no_wait_timeout),
-			ztest_unit_test(test_thread_flags_signalled),
-			ztest_unit_test(test_thread_flags_isr));
+			 ztest_unit_test(test_kernel_apis),
+			 ztest_unit_test(test_delay),
+			 ztest_unit_test(test_thread_apis),
+			 ztest_unit_test(test_thread_prio),
+			 ztest_unit_test(test_timer),
+			 ztest_unit_test(test_mutex),
+			 ztest_unit_test(test_mutex_lock_timeout),
+			 ztest_unit_test(test_semaphore),
+			 ztest_unit_test(test_mempool),
+			 ztest_unit_test(test_messageq),
+			 ztest_unit_test(test_event_flags_no_wait_timeout),
+			 ztest_unit_test(test_event_flags_signalled),
+			 ztest_unit_test(test_event_flags_isr),
+			 ztest_unit_test(test_thread_flags_no_wait_timeout),
+			 ztest_unit_test(test_thread_flags_signalled),
+			 ztest_unit_test(test_thread_flags_isr));
 
 	ztest_run_test_suite(test_cmsis_v2_apis);
 }
