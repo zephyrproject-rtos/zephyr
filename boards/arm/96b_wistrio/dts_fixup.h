2738d60fee7cd667c16b49b7c93404aba0543b00
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2019-01-05 00:46:51 +0530

    boards: arm: Add 96Boards WisTrio LoRa Tracker board
    Add board support for 96Boards WisTrio LoRa Tracker board from
    RAK Wireless. This board is one of the 96Boards IoT Edition platform.
    
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

---
+++ boards/arm/96b_wistrio/dts_fixup.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2019 Linaro Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * This file is a temporary workaround for mapping of the generated information
+ * to the current driver definitions.  This will be removed when the drivers
+ * are modified to handle the generated information, or the mapping of
+ * generated data matches the driver definitions.
+ */
