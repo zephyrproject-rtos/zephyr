9f84889779395665ca56e2b47592f23dafada4c7
Piotr Mienkowski <piotr.mienkowski@gmail.com>
2019-01-04 20:14:52 +0100

    soc: same70: Add SWO logger support
    This commit configures ETM/TPIU clock upon startup in case SWO logger
    backend is enabled on Atmel SAM E70 SoC.
    
    Signed-off-by: Piotr Mienkowski <piotr.mienkowski@gmail.com>

--- soc/arm/atmel_sam/same70/soc_config.c
+++ soc/arm/atmel_sam/same70/soc_config.c
@@ -35,6 +35,22 @@ static int atmel_same70_config(struct device *dev)
 	/* Disable TDI function on PB4 pin, this is controlled by Bus Matrix */
 	MATRIX->CCFG_SYSIO |= CCFG_SYSIO_SYSIO4;
 
+#ifdef CONFIG_LOG_BACKEND_SWO
+	/* Disable PCK3 clock used by ETM module */
+	PMC->PMC_SCDR = PMC_SCDR_PCK3;
+	while ((PMC->PMC_SCSR) & PMC_SCSR_PCK3) {
+		;
+	}
+	/* Select PLLA clock as PCK3 clock */
+	PMC->PMC_PCK[3] = PMC_MCKR_CSS_PLLA_CLK;
+	/* Enable PCK3 clock */
+	PMC->PMC_SCER = PMC_SCER_PCK3;
+	/* Wait for PCK3 setup to complete */
+	while (!((PMC->PMC_SR) & PMC_SR_PCKRDY3)) {
+		;
+	}
+#endif
+
 	return 0;
 }
 
