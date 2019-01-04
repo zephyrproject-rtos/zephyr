5d835fa1f4a65fcb844fd6514e3bba54ae8acdc6
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2019-01-05 00:46:51 +0530

    boards: arm: Add 96Boards WisTrio LoRa Tracker board
    Add board support for 96Boards WisTrio LoRa Tracker board from
    RAK Wireless. This board is one of the 96Boards IoT Edition platform.
    
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

---
+++ boards/arm/96b_wistrio/pinmux.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019 Linaro Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <kernel.h>
+#include <device.h>
+#include <init.h>
+#include <pinmux.h>
+#include <sys_io.h>
+
+#include <pinmux/stm32/pinmux_stm32.h>
+
+static const struct pin_config pinconf[] = {
+#ifdef CONFIG_UART_1
+	{STM32_PIN_PA9,  STM32L1X_PINMUX_FUNC_PA9_USART1_TX},
+	{STM32_PIN_PA10, STM32L1X_PINMUX_FUNC_PA10_USART1_RX},
+#endif	/* CONFIG_UART_1 */
+#ifdef CONFIG_UART_2
+	{STM32_PIN_PA2, STM32L1X_PINMUX_FUNC_PA2_USART2_TX},
+	{STM32_PIN_PA15, STM32L1X_PINMUX_FUNC_PA15_USART2_RX},
+#endif	/* CONFIG_UART_2 */
+#ifdef CONFIG_I2C_1
+	{STM32_PIN_PB8, STM32L1X_PINMUX_FUNC_PB8_I2C1_SCL},
+	{STM32_PIN_PB9, STM32L1X_PINMUX_FUNC_PB9_I2C1_SDA},
+#endif /* CONFIG_I2C_1 */
+};
+
+static int pinmux_stm32_init(struct device *port)
+{
+	ARG_UNUSED(port);
+
+	stm32_setup_pins(pinconf, ARRAY_SIZE(pinconf));
+
+	return 0;
+}
+
+SYS_INIT(pinmux_stm32_init, PRE_KERNEL_1,
+	 CONFIG_PINMUX_STM32_DEVICE_INITIALIZATION_PRIORITY);
