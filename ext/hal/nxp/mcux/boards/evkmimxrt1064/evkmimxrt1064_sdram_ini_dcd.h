7c537a7c2d9dfc387ed327615446357839adbc7e
Ryan QIAN <jianghao.qian@nxp.com>
2019-01-07 07:50:00 +0800

    ext: hal: nxp: mcux: Add device files for RT1064
    Add mcux 2.4.0 drivers and device header files for mimxrt1064.
    
    Origins: NXP MCUxpresso SDK 2.4.0
    URL: mcuxpresso.nxp.com
    Maintained-by: External
    
    Signed-off-by: Ryan QIAN <jianghao.qian@nxp.com>

---
+++ ext/hal/nxp/mcux/boards/evkmimxrt1064/evkmimxrt1064_sdram_ini_dcd.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2018 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __EVKMIMXRT1064_DCD_SDRAM_INIT__
+#define __EVKMIMXRT1064_DCD_SDRAM_INIT__
+
+#include <stdint.h>
+
+/*************************************
+ *  DCD Data
+ *************************************/
+#define DCD_TAG_HEADER (0xD2)
+#define DCD_TAG_HEADER_SHIFT (24)
+#define DCD_VERSION (0x40)
+#define DCD_ARRAY_SIZE 1
+
+#endif /* __EVKMIMXRT1064_DCD_SDRAM_INIT__ */
