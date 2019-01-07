841f72f81e361703167e8c362f9f4ffec2997535
Alexander Wachter <alexander.wachter@student.tugraz.at>
2019-01-07 17:18:35 +0100

    drivers: hwinfo: add driver support for Nordic NRF device ID
    Add driver support for Nordic NRF ID device.
    This device has an ID of 8 bytes.
    
    Signed-off-by: Alexander Wachter <alexander.wachter@student.tugraz.at>

---
+++ drivers/hwinfo/hwinfo_nrf.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018 Alexander Wachter
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <soc.h>
+#include <hwinfo.h>
+#include <string.h>
+
+struct nrf_uid {
+	u32_t id[2];
+};
+
+ssize_t _impl_hwinfo_get_device_id(u8_t *buffer, size_t length)
+{
+	struct nrf_uid dev_id;
+
+	dev_id.id[0] = NRF_FICR->DEVICEID[0];
+	dev_id.id[1] = NRF_FICR->DEVICEID[1];
+
+	if (length > sizeof(dev_id.id)) {
+		length = sizeof(dev_id.id);
+	}
+
+	memcpy(buffer, dev_id.id, length);
+
+	return length;
+}
