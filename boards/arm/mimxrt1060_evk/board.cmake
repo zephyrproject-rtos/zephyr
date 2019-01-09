9554ae92659dd08b29096852f5247551165185c0
Maureen Helm <maureen.helm@nxp.com>
2019-01-09 12:49:33 -0600

    boards: Fix jlink device name on rt1020 and rt1060 boards
    The mimxrt1020_evk and mimxrt1060_evk boards had invalid jlink device
    names. Debugging via 'ninja debug' now works on these boards.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- boards/arm/mimxrt1060_evk/board.cmake
+++ boards/arm/mimxrt1060_evk/board.cmake
@@ -4,5 +4,5 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-board_runner_args(jlink "--device=MCIMXRT1062")
+board_runner_args(jlink "--device=MIMXRT1062xxx6A")
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
