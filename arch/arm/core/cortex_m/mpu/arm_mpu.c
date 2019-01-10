970e357700f485e2d0d43b3ac8815a81d70e4495
Andy Gross <andy.gross@linaro.org>
2019-01-10 15:38:18 -0600

    arch: arm: mpu: arm_mpu: Fix compiler error on stack_obj
    This patch adds a required cast for the size calculation utilizing
    pointer arithmetic.  The stack_obj needs a cast to u32_t.  This was
    caught using a newer compiler.
    
    Signed-off-by: Andy Gross <andy.gross@linaro.org>

--- arch/arm/core/cortex_m/mpu/arm_mpu.c
+++ arch/arm/core/cortex_m/mpu/arm_mpu.c
@@ -207,7 +207,7 @@ void arm_core_mpu_configure_user_context(struct k_thread *thread)
 	 * it is essential to include this size increase, to avoid
 	 * MPU faults.
 	 */
-	size += thread->stack_info.start - thread->stack_obj;
+	size += thread->stack_info.start - base;
 #endif
 
 	arm_core_mpu_configure(THREAD_STACK_REGION, base, size);
