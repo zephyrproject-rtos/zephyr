c64579a35998d2685d2e3bb81f9ce9c79e2d1b2c
Kumar Gala <kumar.gala@linaro.org>
2019-01-11 16:40:26 -0600

    kconfigfunctions: Add function to determine if a device exists in dts
    Added dt_dev_exist_by_path() function so we can determine if a specific
    device exists or not.
    
    For example:
    
      config PINMUX_MCUX_PORTB
    	default y if UART_MCUX_0
    
    Becomes:
    
      config PINMUX_MCUX_PORTB
    	default y if "$(dt_dev_exist_by_path,/soc/uart@4006a000)"
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- scripts/kconfig/kconfigfunctions.py
+++ scripts/kconfig/kconfigfunctions.py
@@ -6,10 +6,20 @@
 import sys
 import os
 import re
+sys.path.append(os.environ['srctree'])
+from scripts.dts.devicetree import parse_file
+from scripts.dts.extract.globals import get_reduced, reduced
 
 # Types we support
 # 'string', 'int', 'hex', 'bool'
 conf_file = os.path.join(os.environ['PROJECT_BINARY_DIR'], "include/generated/generated_dts_board.conf")
+dts_file_name = os.environ['BOARD'] + ".dts_compiled"
+dts_file = os.path.join(os.environ['PROJECT_BINARY_DIR'], dts_file_name)
+
+if os.path.isfile(dts_file):
+    with open(dts_file, 'r') as fd:
+        dts = parse_file(fd)
+        get_reduced(dts['/'], '/')
 
 regex = '='
 defines = {}
@@ -59,9 +69,15 @@ def dt_get_hex_val(kconf, name, arg, unit=None):
         d = 0x0
     return str(d)
 
+def dt_dev_exist_by_path(kconf, name, dts_path):
+    if dts_path in reduced.keys():
+        return "y"
+
+    return "n"
 
 functions = {
         "test_function": (dt_get_str_val, 1, 1),
         "dt_get_int_val": (dt_get_int_val, 1, 2),
         "dt_get_hex_val": (dt_get_hex_val, 1, 2),
+        "dt_dev_exist_by_path": (dt_dev_exist_by_path, 1, 1),
 }
