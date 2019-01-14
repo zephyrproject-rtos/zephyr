29fb3461104517de309befdc7f7bbee8f4b2f1db
Jan Van Winkel <jan.van_winkel@dxplore.eu>
2019-01-14 23:56:41 +0100

    samples: fs: Added  FS shell sample
    Added file system shell sample
    
    Signed-off-by: Jan Van Winkel <jan.van_winkel@dxplore.eu>

---
+++ samples/subsys/shell/fs/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019 Jan Van Winkel <jan.van_winkel@dxplore.eu>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <logging/log.h>
+LOG_MODULE_REGISTER(app);
+
+void main(void)
+{
+}
