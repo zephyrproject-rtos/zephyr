a148e11e2ad0590219678405cc281cb435500a4f
Piotr Mienkowski <piotr.mienkowski@gmail.com>
2019-01-14 16:39:40 +0100

    drivers: uart_gecko: use DT_<COMPAT>_<INSTANCE>_<PROP> defines
    Use the new DT_<COMPAT>_<INSTANCE>_<PROP> defines to instantiate
    devices. This commit adds also ability to define individual pin
    locations on SoC series that support the feature. Definitions of GPIO
    pins assigned to a given location have been moved from soc_pinmap.h file
    to board DTS file.
    
    Signed-off-by: Piotr Mienkowski <piotr.mienkowski@gmail.com>

--- soc/arm/silabs_exx32/efr32fg1p/soc_pinmap.h
+++ soc/arm/silabs_exx32/efr32fg1p/soc_pinmap.h
@@ -31,36 +31,4 @@
 #endif
 #endif /* CONFIG_GPIO_GECKO */
 
-#ifdef CONFIG_USART_GECKO_0
-#if (DT_SILABS_GECKO_USART_USART_0_LOCATION == 0)
-#define PIN_USART0_TXD {gpioPortA, 0, gpioModePushPull, 1}
-#define PIN_USART0_RXD {gpioPortA, 1, gpioModeInput, 1}
-#elif (DT_SILABS_GECKO_USART_USART_0_LOCATION == 1)
-#define PIN_USART0_TXD {gpioPortA, 1, gpioModePushPull, 1}
-#define PIN_USART0_RXD {gpioPortA, 2, gpioModeInput, 1}
-#elif (DT_SILABS_GECKO_USART_USART_0_LOCATION == 2)
-#define PIN_USART0_TXD {gpioPortA, 2, gpioModePushPull, 1}
-#define PIN_USART0_RXD {gpioPortA, 3, gpioModeInput, 1}
-#else
-#error ("Serial Driver for Gecko MCUs not implemented for this location index")
-#endif
-#endif /* CONFIG_USART_GECKO_0 */
-
-#ifdef CONFIG_USART_GECKO_1
-#if (DT_SILABS_GECKO_USART_USART_1_LOCATION == 0)
-#error ("Serial Driver for Gecko MCUs not implemented for this location index")
-#elif (DT_SILABS_GECKO_USART_USART_1_LOCATION == 1)
-#define PIN_USART1_TXD {gpioPortF, 10, gpioModePushPull, 1}
-#define PIN_USART1_RXD {gpioPortF, 11, gpioModeInput, 1}
-#elif (DT_SILABS_GECKO_USART_USART_1_LOCATION == 2)
-#define PIN_USART1_TXD {gpioPortB, 9, gpioModePushPull, 1}
-#define PIN_USART1_RXD {gpioPortB, 10, gpioModeInput, 1}
-#elif (DT_SILABS_GECKO_USART_USART_1_LOCATION == 3)
-#define PIN_USART1_TXD {gpioPortE, 2, gpioModePushPull, 1}
-#define PIN_USART1_RXD {gpioPortE, 3, gpioModeInput, 1}
-#else
-#error ("Serial Driver for Gecko MCUs not implemented for this location index")
-#endif
-#endif /* CONFIG_USART_GECKO_1 */
-
 #endif /* _SILABS_EFR32FG1P_SOC_PINMAP_H_ */
