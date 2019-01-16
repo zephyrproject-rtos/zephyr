395a92b36bfe38b0fb2c24bc6bd4d7b87aa41fce
Maureen Helm <maureen.helm@nxp.com>
2019-01-16 15:15:58 -0600

    clock_control: Add support for lpi2c clock in mcux ccm driver
    Adds support for getting the lpi2c clock frequency in the mcux ccm
    driver.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- drivers/clock_control/clock_control_mcux_ccm.c
+++ drivers/clock_control/clock_control_mcux_ccm.c
@@ -39,6 +39,20 @@ static int mcux_ccm_get_subsys_rate(struct device *dev,
 	u32_t clock_name = (u32_t) sub_system;
 
 	switch (clock_name) {
+
+#ifdef CONFIG_I2C_MCUX_LPI2C
+	case IMX_CCM_LPI2C_CLK:
+		if (CLOCK_GetMux(kCLOCK_Lpi2cMux) == 0) {
+			*rate = CLOCK_GetPllFreq(kCLOCK_PllUsb1) / 8
+				/ (CLOCK_GetDiv(kCLOCK_Lpi2cDiv) + 1);
+		} else {
+			*rate = CLOCK_GetOscFreq()
+				/ (CLOCK_GetDiv(kCLOCK_Lpi2cDiv) + 1);
+		}
+
+		break;
+#endif
+
 	case IMX_CCM_LPSPI_CLK:
 	{
 		u32_t lpspi_mux = CLOCK_GetMux(kCLOCK_LpspiMux);
