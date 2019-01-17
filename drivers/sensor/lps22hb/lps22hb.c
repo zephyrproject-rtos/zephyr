7b9bf08dceb6135d132d0a2f6d3280b94d101693
Kumar Gala <kumar.gala@linaro.org>
2019-01-17 13:43:03 -0600

    sensor: lps22hb: Convert to new DT_<COMPAT>_<INSTANCE> defines
    Convert lps22hb sensor driver to use new defines so we can remove the
    dts_fixup.h code for it.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- drivers/sensor/lps22hb/lps22hb.c
+++ drivers/sensor/lps22hb/lps22hb.c
@@ -152,12 +152,12 @@ static int lps22hb_init(struct device *dev)
 }
 
 static const struct lps22hb_config lps22hb_config = {
-	.i2c_master_dev_name = DT_LPS22HB_I2C_MASTER_DEV_NAME,
-	.i2c_slave_addr = DT_LPS22HB_I2C_ADDR,
+	.i2c_master_dev_name = DT_ST_LPS22HB_PRESS_0_BUS_NAME,
+	.i2c_slave_addr = DT_ST_LPS22HB_PRESS_0_BASE_ADDRESS,
 };
 
 static struct lps22hb_data lps22hb_data;
 
-DEVICE_AND_API_INIT(lps22hb, DT_LPS22HB_DEV_NAME, lps22hb_init,
+DEVICE_AND_API_INIT(lps22hb, DT_ST_LPS22HB_PRESS_0_LABEL, lps22hb_init,
 		    &lps22hb_data, &lps22hb_config, POST_KERNEL,
 		    CONFIG_SENSOR_INIT_PRIORITY, &lps22hb_api_funcs);
