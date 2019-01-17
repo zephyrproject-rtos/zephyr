c3d7bb1511ebb1792b1f1fcdbe38a874862742b4
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2019-01-17 10:26:08 +0100

    boards: arm: nrf: board definition for nrf9160_pca10090
    This commit contributes the Kconfig and DTS files for the
    nrf9160_pca10090 board definition. The nrf9160_pca10090 board
    shall be used to build Zephyr applications, running in Secure
    mode, on nRF9160_PCA10090 Dev Kit.
    
    Additionally, the commit introduces a default Secure and
    Non-Secure flash partition configuration for the
    nRF9160_PCA10090 board, allowing to use Zephyr for building
    both the Secure and Non-Secure firmware images.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

---
+++ boards/arm/nrf9160_pca10090/board.cmake
@@ -0,0 +1,4 @@
+board_runner_args(nrfjprog "--nrf-family=NRF91")
+board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
