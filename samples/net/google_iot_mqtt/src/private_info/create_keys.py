df4b2bdcc67a23d117eb59fef94429b879136971
Andy Gross <andy.gross@linaro.org>
2019-01-17 23:27:27 -0600

    samples: net: Add Google IOT Cloud full stack application
    This application provides a full stack implementation for
    communicating with the Google IOT Cloud.  It leverages the
    a JWT library to create JSON tokens which are then signed using
    a RSA or ECDSA private key.  The signed token is used to
    authenticate with the Google IOT cloud servers.  Once that is
    accomplished, the application can subscribe, or publish data at
    will.
    
    Included in the application is a python script that can be used to
    create RSA/ECDSA certificates and keys.  The user will load the
    certificate for the appropriate device on the Google IOT Cloud.  The
    private key is created by the script and stored in a C structure format
    that is then consumed by the application.  It is important to note that
    any keys generated from the scripts SHOULD NOT BE STORED publicly.
    
    Please refer to the README for more information.
    
    Signed-off-by: David Brown <david.brown@linaro.org>
    Signed-off-by: Andy Gross <andy.gross@linaro.org>

---
+++ samples/net/google_iot_mqtt/src/private_info/create_keys.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2018 Linaro
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import sys
+import os
+import re
+import subprocess
+import argparse
+
+def parse_args():
+    global args
+
+    parser = argparse.ArgumentParser(description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument(
+        "-d", "--device", required=True,
+        help="Name of device")
+
+    parser.add_argument(
+        "-e", "--ecdsa", required=False,
+        action='store_true',
+        help="Use elliptic curve")
+
+    parser.add_argument(
+        "-r", "--rsa", required=False,
+        action='store_true',
+        help="Use RSA")
+
+    args = parser.parse_args()
+
+split_string = lambda x, n: [x[i:i+n] for i in range(0, len(x), n)]
+
+def main():
+    parse_args()
+
+    fd = open("key.c", "w")
+
+    pem_file = args.device + "-private.pem"
+    cert_file = args.device + "-cert.pem"
+    der_file = args.device + "-private.der"
+
+    if args.ecdsa:
+        print("Generating ecdsa private key")
+        try:
+            subprocess.call(["openssl", "ecparam", "-noout", "-name", "prime256v1", "-genkey",
+                "-out", pem_file])
+
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+    else:
+        print("Generating rsa private key")
+        try:
+            subprocess.call(["openssl", "genrsa", "-out", pem_file, "2048"])
+
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+
+    print("Generating certificate")
+
+    try:
+        subprocess.check_call(["openssl", "req", "-new", "-x509", "-key", pem_file, "-out",
+            cert_file, "-days", "1000000", "-subj", "/CN=" + args.device])
+
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+
+
+    if args.ecdsa:
+        print("Parsing ECDSA private key")
+        o = subprocess.Popen(["openssl", "asn1parse", "-in", pem_file,
+            "-offset", "5", "-length", "34"], stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
+
+        if o.returncode == 0:
+            sys.exit("failed to parse ECDSA private key")
+
+        tmp = o.communicate()[0].decode('ascii').split(":")[3].lower().strip()
+
+        if o.returncode:
+            sys.exit("failed to parse ECDSA private key")
+
+        tmp = split_string(tmp, 2)
+        output = ["0x" + s for s in tmp]
+        output = ', '.join(output)
+
+    else:
+        print("Parsing RSA private key and generating DER output")
+        try:
+            subprocess.call(["openssl", "pkcs8", "-nocrypt", "-topk8",
+                "-inform", "pem", "-outform", "der", "-in", pem_file, "-out",
+                der_file])
+
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+
+        der_fd = open(der_file, "r");
+        o = subprocess.Popen(["xxd", "-i"], stdin=der_fd, stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
+
+        output = o.communicate()[0].decode('ascii')
+
+        if o.returncode:
+            sys.exit("failed to parse RSA private key")
+
+    print("Generating key.c")
+    fd.write("unsigned char zepfull_private_der[] = {\n")
+    key_len = len(output.split(','))
+    fd.write(output)
+    fd.write("};\n\n")
+
+    fd.write("unsigned int zepfull_private_der_len = " + str(key_len) + ";\n");
+
+if __name__ == "__main__":
+    main()
