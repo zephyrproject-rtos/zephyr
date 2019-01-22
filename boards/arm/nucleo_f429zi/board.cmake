75e7bd4e66181f1a66e6405448e46dfffb73bd38
Dave Marples <dave@marples.net>
2019-01-22 20:37:39 +0000

    nucleo_f429zi: Add JLink support for flash, as per nucleo_f070rb
    Signed-off-by: Dave Marples <dave@marples.net>
    
    Added ability to use JLink in addition to OpenOCD for flashing.

--- boards/arm/nucleo_f429zi/board.cmake
+++ boards/arm/nucleo_f429zi/board.cmake
@@ -1,1 +1,8 @@
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+set_ifndef(STLINK_FW stlink)
+
+if(STLINK_FW STREQUAL jlink)
+  board_runner_args(jlink "--device=stm32f429zi" "--speed=4000")
+  include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+elseif(STLINK_FW STREQUAL stlink)
+  include($ENV{ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+endif()
