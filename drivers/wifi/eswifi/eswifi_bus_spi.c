47825f51277e9c5028b95e9fa2495cfd880ab987
Kumar Gala <kumar.gala@linaro.org>
2019-01-23 04:25:56 -0600

    wifi: eswifi: Use generated SPI CS define
    Move to using the generated DT_INVENTEK_ESWIFI_ESWIFI0_CS_GPIO_pin and
    DT_INVENTEK_ESWIFI_ESWIFI0_CS_GPIO_CONTROLLER defines and drop the
    DT_ESWIFI0_CS_GPIOS_PIN & DT_INVENTEK_ESWIFI_ESWIFI0_CS_GPIO_PIN from
    dts_fixup.hl
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- drivers/wifi/eswifi/eswifi_bus_spi.c
+++ drivers/wifi/eswifi/eswifi_bus_spi.c
@@ -235,8 +235,9 @@ int eswifi_spi_init(struct eswifi_dev *eswifi)
 				  SPI_WORD_SET(16) | SPI_LINES_SINGLE |
 				  SPI_HOLD_ON_CS | SPI_LOCK_ON);
 	spi->spi_cfg.slave = DT_INVENTEK_ESWIFI_ESWIFI0_BASE_ADDRESS;
-	spi->spi_cs.gpio_dev = device_get_binding(DT_ESWIFI0_CS_GPIOS_CONTROLLER);
-	spi->spi_cs.gpio_pin = DT_ESWIFI0_CS_GPIOS_PIN;
+	spi->spi_cs.gpio_dev =
+		device_get_binding(DT_INVENTEK_ESWIFI_ESWIFI0_CS_GPIO_CONTROLLER);
+	spi->spi_cs.gpio_pin = DT_INVENTEK_ESWIFI_ESWIFI0_CS_GPIO_PIN;
 	spi->spi_cs.delay = 1000;
 	spi->spi_cfg.cs = &spi->spi_cs;
 
