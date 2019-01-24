61cb8b6c4f961a778175d5861b9456efe7556c91
Kumar Gala <kumar.gala@linaro.org>
2019-01-24 16:14:03 -0600

    drivers: wdog: cmsdk: Convert to new DT_<COMPAT>_<INSTANCE> defines
    Convert cmsdk driver to use new defines so we can remove the
    dts_fixup.h code for it.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- drivers/watchdog/wdog_cmsdk_apb.c
+++ drivers/watchdog/wdog_cmsdk_apb.c
@@ -57,7 +57,7 @@ struct wdog_cmsdk_apb {
 #define CMSDK_APB_WDOG_LOCK_VALUE (0x2BDDF662)
 
 #define WDOG_STRUCT \
-	((volatile struct wdog_cmsdk_apb *)(DT_CMSDK_APB_WDOG))
+	((volatile struct wdog_cmsdk_apb *)(DT_ARM_CMSDK_WATCHDOG_0_BASE_ADDRESS))
 
 /* Keep reference of the device to pass it to the callback */
 struct device *wdog_r;
