cf57312953aaf521b6628478b2983d1b6727e922
Peter A. Bigot <pab@pabigot.com>
2019-01-24 11:53:01 -0600

    samples: sensors: hts221: support triggered readings
    Allow trigger-initiated sampling at the support 7 and 12.5 Hz rates.
    
    Signed-off-by: Peter A. Bigot <pab@pabigot.com>

--- samples/sensor/hts221/src/main.c
+++ samples/sensor/hts221/src/main.c
@@ -10,9 +10,44 @@
 #include <stdio.h>
 #include <misc/util.h>
 
+static void process_sample(struct device *dev)
+{
+	static unsigned int obs;
+	struct sensor_value temp, hum;
+	if (sensor_sample_fetch(dev) < 0) {
+		printf("Sensor sample update error\n");
+		return;
+	}
+
+	if (sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp) < 0) {
+		printf("Cannot read HTS221 temperature channel\n");
+		return;
+	}
+
+	if (sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &hum) < 0) {
+		printf("Cannot read HTS221 humidity channel\n");
+		return;
+	}
+
+	++obs;
+	printf("Observation:%u\n", obs);
+
+	/* display temperature */
+	printf("Temperature:%.1f C\n", sensor_value_to_double(&temp));
+
+	/* display humidity */
+	printf("Relative Humidity:%.1f%%\n",
+	       sensor_value_to_double(&hum));
+}
+
+static void hts221_handler(struct device *dev,
+			   struct sensor_trigger *trig)
+{
+	process_sample(dev);
+}
+
 void main(void)
 {
-	struct sensor_value temp, hum;
 	struct device *dev = device_get_binding("HTS221");
 
 	if (dev == NULL) {
@@ -20,29 +55,20 @@ void main(void)
 		return;
 	}
 
-	while (1) {
-		if (sensor_sample_fetch(dev) < 0) {
-			printf("Sensor sample update error\n");
+	if (IS_ENABLED(CONFIG_HTS221_TRIGGER)) {
+		struct sensor_trigger trig = {
+			.type = SENSOR_TRIG_DATA_READY,
+			.chan = SENSOR_CHAN_ALL,
+		};
+		if (sensor_trigger_set(dev, &trig, hts221_handler) < 0) {
+			printf("Cannot configure trigger\n");
 			return;
-		}
+		};
+	}
 
-		if (sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp) < 0) {
-			printf("Cannot read HTS221 temperature channel\n");
-			return;
-		}
-
-		if (sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &hum) < 0) {
-			printf("Cannot read HTS221 humidity channel\n");
-			return;
-		}
-
-		/* display temperature */
-		printf("Temperature:%.1f C\n", sensor_value_to_double(&temp));
-
-		/* display humidity */
-		printf("Relative Humidity:%.1f%%\n",
-		       sensor_value_to_double(&hum));
-
+	while (!IS_ENABLED(CONFIG_HTS221_TRIGGER)) {
+		process_sample(dev);
 		k_sleep(2000);
 	}
+	k_sleep(K_FOREVER);
 }
