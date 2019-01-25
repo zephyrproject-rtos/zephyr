63841df1849e446c8f19fcf7053b3793441ef05c
Marti Bolivar <marti@foundries.io>
2019-01-24 21:17:46 -0700

    scripts: run_ci.sh: add pytest-based testing for west commands
    Incorporate these into run_ci.sh the same way that btsim results were
    done. This adds a dependency on pytest.
    
    Signed-off-by: Marti Bolivar <marti@foundries.io>

---
+++ scripts/west_commands/tests/test_imports.py
@@ -0,0 +1,15 @@
+from runners.core import ZephyrBinaryRunner
+
+
+def test_runner_imports():
+    # Ensure that all runner modules are imported and returned by
+    # get_runners().
+    #
+    # This is just a basic sanity check against errors introduced by
+    # tree-wide refactorings for runners that don't have their own
+    # test suites.
+    runner_names = set(r.name() for r in ZephyrBinaryRunner.get_runners())
+    expected = set(('arc-nsim', 'bossac', 'dfu-util', 'em-starterkit', 'esp32',
+                    'jlink', 'nios2', 'nrfjprog', 'openocd', 'pyocd',
+                    'qemu', 'xtensa', 'intel_s1000', 'blackmagicprobe'))
+    assert runner_names == expected
