704514386adb1fabb919026b57600c2091011150
Kumar Gala <kumar.gala@linaro.org>
2019-01-29 13:47:38 -0600

    sensor: adt7420: Convert to new DT_<COMPAT>_<INSTANCE> defines
    Convert adt7420 sensor driver to use new defines so we can remove the
    dts_fixup.h code for it.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- samples/sensor/adt7420/dts_fixup.h
+++
@@ -1,9 +0,0 @@
-#if defined(CONFIG_HAS_DTS_I2C)
-
-#ifndef DT_ADT7420_NAME
-#define DT_ADT7420_NAME ""
-#define DT_ADT7420_I2C_ADDR 0
-#define DT_ADT7420_I2C_MASTER_DEV_NAME ""
-#endif
-
-#endif /* CONFIG_HAS_DTS_I2C */
