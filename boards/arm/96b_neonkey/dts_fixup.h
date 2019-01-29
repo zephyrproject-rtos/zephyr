75d84ffd7a0c4f751fdb47419fc48d801e8c7ad8
Kumar Gala <kumar.gala@linaro.org>
2019-01-29 14:40:55 -0600

    led: lp3943: Remove dts_fixup.h
    Since the lp3943 led driver still supports both DTS and non-DTS
    configuration, we update the code to use DT_<COMPAT>_<INSTANCE>
    defines for the CONFIG_HAS_DTS_I2C case.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- boards/arm/96b_neonkey/dts_fixup.h
+++
@@ -1,9 +0,0 @@
-/* This file is a temporary workaround for mapping of the generated information
- * to the current driver definitions.  This will be removed when the drivers
- * are modified to handle the generated information, or the mapping of
- * generated data matches the driver definitions.
- */
-
-#define CONFIG_LP3943_DEV_NAME			DT_ST_STM32_I2C_V1_40005C00_TI_LP3943_60_LABEL
-#define CONFIG_LP3943_I2C_ADDRESS		DT_ST_STM32_I2C_V1_40005C00_TI_LP3943_60_BASE_ADDRESS
-#define CONFIG_LP3943_I2C_MASTER_DEV_NAME	DT_ST_STM32_I2C_V1_40005C00_TI_LP3943_60_BUS_NAME
