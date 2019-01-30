5450734728746da24031343165c074b8909a731f
Mark Ruvald Pedersen <mped@oticon.com>
2019-01-30 21:48:25 +0100

    cmake: Toolchain abstraction: Assembly
    Introduce toolchain_cc_asm macro to capture toolchain specific flags
    related to assembly.
    
    -D_ASMLANGUAGE is kept common for all, assuming -D as define flag is
    supported by all compilers (which is almost the case).
    
    No functional change expected.
    
    Clang's flags are compatible with gcc, and are thus inherited.
    
    This is motivated by the wish to abstract Zephyr's usage of toolchains,
    permitting easier porting to other (commercial) toolchains.
    
    Signed-off-by: Mark Ruvald Pedersen <mped@oticon.com>

---
+++ cmake/compiler/gcc/target_asm.cmake
@@ -0,0 +1,6 @@
+# See root CMakeLists.txt for description and expectations of this macro
+
+macro(toolchain_cc_asm_base_flags dest_var_name)
+  # Specify assembly as the source language for the preprocessor to expect
+  set_ifndef(${dest_var_name} "-xassembler-with-cpp")
+endmacro()
