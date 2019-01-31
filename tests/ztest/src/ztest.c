a4c28f70bf856d35c295af78aa6de9c6bb293c15
Andrew Boie <andrew.p.boie@intel.com>
2019-01-31 15:15:13 -0800

    ztest: setup a default app memory domain
    If CONFIG_APP_SHARED_MEM is enabled, ztest will set up a
    default memory domain with a single partition on startup.
    Test case globals may be added to the partition via
    ZTEST_BMEM/ZTEST_DMEM macros, add their own partitions,
    or leave the domain and join another one.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- tests/ztest/src/ztest.c
+++ tests/ztest/src/ztest.c
@@ -11,16 +11,16 @@
 __kernel static struct k_thread ztest_thread;
 #endif
 
-/* APPDMEMP0 and APPBMEMP0 are used for the application shared memory test  */
+/* ZTEST_DMEM and ZTEST_BMEM are used for the application shared memory test  */
 
-APPDMEMP0 enum {
+ZTEST_DMEM enum {
 	TEST_PHASE_SETUP,
 	TEST_PHASE_TEST,
 	TEST_PHASE_TEARDOWN,
 	TEST_PHASE_FRAMEWORK
 } phase = TEST_PHASE_FRAMEWORK;
 
-APPBMEMP0 static int test_status;
+static ZTEST_BMEM int test_status;
 
 static int cleanup_test(struct unit_test *test)
 {
@@ -152,8 +152,7 @@ out:
 
 K_THREAD_STACK_DEFINE(ztest_thread_stack, CONFIG_ZTEST_STACKSIZE +
 		      CONFIG_TEST_EXTRA_STACKSIZE);
-/* APPBMEMP0 is used for the application shared memory test    */
-APPBMEMP0 static int test_result;
+static ZTEST_BMEM int test_result;
 
 __kernel static struct k_sem test_end_signal;
 
@@ -279,6 +278,11 @@ void end_report(void)
 	}
 }
 
+#ifdef CONFIG_APP_SHARED_MEM
+struct k_mem_domain ztest_mem_domain;
+K_APPMEM_PARTITION_DEFINE(ztest_mem_partition);
+#endif
+
 #ifndef KERNEL
 int main(void)
 {
@@ -291,6 +295,19 @@ int main(void)
 #else
 void main(void)
 {
+#ifdef CONFIG_APP_SHARED_MEM
+	struct k_mem_partition *parts[] = {
+		&ztest_mem_partition
+	};
+
+	/* Ztests just have one memory domain with one partition.
+	 * Any variables that user code may reference need to go in them,
+	 * using the ZTEST_DMEM and ZTEST_BMEM macros.
+	 */
+	k_mem_domain_init(&ztest_mem_domain, ARRAY_SIZE(parts), parts);
+	k_mem_domain_add_thread(&ztest_mem_domain, k_current_get());
+#endif /* CONFIG_APP_SHARED_MEM */
+
 	_init_mock();
 	test_main();
 	end_report();
