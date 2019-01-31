2d9c53d82f1620bbbb780f05f1ceca84c16b2c57
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2019-01-31 11:59:54 +0200

    net: core: Handle packets when packet sockets are enabled
    If CONFIG_NET_SOCKETS_PACKET is enabled, then feed the packet
    to net_packet_socket_input() for processing. It will search
    for the net_contexts and if proper handler is found, pass
    the packet to connection handler.
    
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>

---
+++ subsys/net/ip/packet_socket.c
@@ -0,0 +1,42 @@
+/** @file
+ * @brief Packet Sockets related functions
+ */
+
+/*
+ * Copyright (c) 2019 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <logging/log.h>
+LOG_MODULE_REGISTER(net_sockets_raw, CONFIG_NET_SOCKETS_LOG_LEVEL);
+
+#include <errno.h>
+#include <net/net_pkt.h>
+#include <net/net_context.h>
+#include <net/ethernet.h>
+
+#include "connection.h"
+#include "packet_socket.h"
+
+enum net_verdict net_packet_socket_input(struct net_pkt *pkt)
+{
+	/* Currently we are skipping L2 layer verification and not
+	 * removing L2 header from packet.
+	 * TODO :
+	 * 1) Pass it through L2 layer, so that L2 will verify
+	 * that packet is intended to us or not and sets src and dst lladdr.
+	 * And L2 should not pull off L2 header when combination of socket
+	 * like this AF_PACKET, SOCK_RAW and ETH_P_ALL proto.
+	 * 2) Socket combination of AF_INET, SOCK_RAW, IPPROTO_RAW
+	 * packet has to go through L2 and L2 verfies it's header and removes
+	 * header. Only packet with L3 header will be given to socket.
+	 * 3) If user opens raw and non raw socket together, based on raw
+	 * socket combination packet has to be feed to raw socket and only
+	 * data part to be feed to non raw socket.
+	 */
+
+	net_pkt_set_family(pkt, AF_PACKET);
+
+	return net_conn_input(ETH_P_ALL, pkt);
+}
