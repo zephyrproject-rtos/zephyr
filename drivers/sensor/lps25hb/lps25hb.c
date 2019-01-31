37e77f942a5280beafc8d986ab42d7083a17c146
Armando Visconti <armando.visconti@st.com>
2019-01-31 11:14:19 +0100

    driver/sensor: lps25hb: align driver to auto-generated dts macros
    Use auto-generated device tree macros in LPS25HB driver to avoid
    usage of dts.fixup code for it.
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

--- drivers/sensor/lps25hb/lps25hb.c
+++ drivers/sensor/lps25hb/lps25hb.c
@@ -180,12 +180,12 @@ static int lps25hb_init(struct device *dev)
 }
 
 static const struct lps25hb_config lps25hb_config = {
-	.i2c_master_dev_name = DT_LPS25HB_I2C_MASTER_DEV_NAME,
-	.i2c_slave_addr = DT_LPS25HB_I2C_ADDR,
+	.i2c_master_dev_name = DT_ST_LPS25HB_PRESS_0_BUS_NAME,
+	.i2c_slave_addr = DT_ST_LPS25HB_PRESS_0_BASE_ADDRESS,
 };
 
 static struct lps25hb_data lps25hb_data;
 
-DEVICE_AND_API_INIT(lps25hb, DT_LPS25HB_DEV_NAME, lps25hb_init,
+DEVICE_AND_API_INIT(lps25hb, DT_ST_LPS25HB_PRESS_0_LABEL, lps25hb_init,
 		    &lps25hb_data, &lps25hb_config, POST_KERNEL,
 		    CONFIG_SENSOR_INIT_PRIORITY, &lps25hb_api_funcs);
