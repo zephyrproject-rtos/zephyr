06f51757608dbb9c71dd55597b44704c4f617376
Kumar Gala <kumar.gala@linaro.org>
2019-01-31 05:00:57 -0600

    boards: stm32: Cleanup/Fix USB Kconfig option
    The disco_l475_iot1 board should be using CONFIG_USB_DC_STM32 as the
    Kconfig sybmol.
    
    Also fix a minor typo in a comment in nucleo_f207zg with regards to
    CONFIG_USB_DC_STM32
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- boards/arm/nucleo_f207zg/pinmux.c
+++ boards/arm/nucleo_f207zg/pinmux.c
@@ -38,7 +38,7 @@ static const struct pin_config pinconf[] = {
 #ifdef CONFIG_USB_DC_STM32
 	{STM32_PIN_PA11, STM32F2_PINMUX_FUNC_PA11_OTG_FS_DM},
 	{STM32_PIN_PA12, STM32F2_PINMUX_FUNC_PA12_OTG_FS_DP},
-#endif	/* CONFIG_USB_DC_STM */
+#endif	/* CONFIG_USB_DC_STM32 */
 };
 
 static int pinmux_stm32_init(struct device *port)
