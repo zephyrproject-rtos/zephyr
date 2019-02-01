92ac6d8fc66153155bd8d1ceea94a55f8f565d8d
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2019-02-01 18:21:51 +0530

    soc: arm: st_stm32: Add STM32L1 SoC series
    Add STM32L1 SoC series support with STM32L15XXB as the target
    SoC.
    
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

---
+++ soc/arm/st_stm32/stm32l1/soc_registers.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2019 Linaro Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _STM32L1X_SOC_REGISTERS_H_
+#define _STM32L1X_SOC_REGISTERS_H_
+
+/* include register mapping headers */
+
+#endif /* _STM32L1X_SOC_REGISTERS_H_ */
