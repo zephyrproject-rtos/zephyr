b93df74fa260bef281e46d3d5c1afc39e409ef96
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2019-02-01 21:22:07 +0100

    net/context: Let's use the new net_ipv4_finalize
    And removing the legacy and unused one.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- subsys/net/ip/ipv4.h
+++ subsys/net/ip/ipv4.h
@@ -41,19 +41,6 @@ struct net_pkt *net_ipv4_create(struct net_pkt *pkt,
 				u8_t next_header_proto);
 
 /**
- * @brief Finalize IPv4 packet. It should be called right before
- * sending the packet and after all the data has been added into
- * the packet. This function will set the length of the
- * packet and calculate the higher protocol checksum if needed.
- *
- * @param pkt Network packet
- * @param next_header_proto Protocol type of the next header after IPv4 header.
- *
- */
-void net_ipv4_finalize(struct net_pkt *pkt, u8_t next_header_proto);
-
-
-/**
  * @brief Create IPv4 packet in provided net_pkt.
  *
  * @param pkt Network packet
