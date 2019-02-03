c4acdc6b0b3a23c34cdcf76c0bc1c2f54493ce9d
Kumar Gala <kumar.gala@linaro.org>
2019-02-03 09:45:49 -0600

    disk: sdhc: Convert to new DT_<COMPAT>_<INSTANCE> defines
    Convert disk_access_sdhc  driver to use new defines so we can remove
    the dts_fixup.h code for it.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- boards/arm/olimexino_stm32/dts_fixup.h
+++
@@ -1,9 +0,0 @@
-/* This file is a temporary workaround for mapping of the generated information
- * to the current driver definitions.  This will be removed when the drivers
- * are modified to handle the generated information, or the mapping of
- * generated data matches the driver definitions.
- */
-#define DT_DISK_SDHC0_BUS_ADDRESS			DT_ST_STM32_SPI_40003800_ZEPHYR_MMC_SPI_SLOT_0_BASE_ADDRESS
-#define DT_DISK_SDHC0_BUS_NAME				DT_ST_STM32_SPI_40003800_ZEPHYR_MMC_SPI_SLOT_0_BUS_NAME
-#define DT_DISK_SDHC0_CS_GPIOS_CONTROLLER		DT_ST_STM32_SPI_40003800_CS_GPIOS_CONTROLLER
-#define DT_DISK_SDHC0_CS_GPIOS_PIN			DT_ST_STM32_SPI_40003800_CS_GPIOS_PIN
