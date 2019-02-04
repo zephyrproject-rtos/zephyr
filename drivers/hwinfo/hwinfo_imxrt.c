c90ae9c16aa51ff254693bb63c47505757204daa
Alexander Wachter <alexander.wachter@student.tugraz.at>
2019-02-04 19:53:59 +0100

    drivers: hwinfo: add driver support for NXP i.mx RT device ID
    Add driver support for NXP i.mx RT ID device.
    This device has an ID of 8 bytes.
    
    Signed-off-by: Alexander Wachter <alexander.wachter@student.tugraz.at>

---
+++ drivers/hwinfo/hwinfo_imxrt.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2019 Alexander Wachter
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <soc.h>
+#include <hwinfo.h>
+#include <string.h>
+
+struct imxrt_uid {
+	u32_t id[2];
+};
+
+ssize_t _impl_hwinfo_get_device_id(u8_t *buffer, size_t length)
+{
+	struct imxrt_uid dev_id;
+
+	dev_id.id[0] = OCOTP->CFG1;
+	dev_id.id[1] = OCOTP->CFG2;
+
+	if (length > sizeof(dev_id.id)) {
+		length = sizeof(dev_id.id);
+	}
+
+	memcpy(buffer, dev_id.id, length);
+
+	return length;
+}
