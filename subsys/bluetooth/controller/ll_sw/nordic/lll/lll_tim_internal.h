61033dc93bb14488760fde0bdbd6ff99af204bab
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2019-02-04 22:26:01 +0530

    Bluetooth: controller: Move PKT_US from LLL to ULL header file
    Move PKT_US definition from LLL header file to ULL header so
    as to share it across vendors.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_tim_internal.h
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_tim_internal.h
@@ -6,18 +6,6 @@
 
 #define TIFS_US 150
 
-/* Macro to return PDU time */
-#if defined(CONFIG_BT_CTLR_PHY_CODED)
-#define PKT_US(octets, phy) \
-	(((phy) & BIT(2)) ? \
-	 (80 + 256 + 16 + 24 + ((((2 + (octets) + 4) * 8) + 24 + 3) * 8)) : \
-	 (((octets) + 14) * 8 / BIT(((phy) & 0x03) >> 1)))
-#else /* !CONFIG_BT_CTLR_PHY_CODED */
-#define PKT_US(octets, phy) \
-	(((octets) + 14) * 8 / BIT(((phy) & 0x03) >> 1))
-#endif /* !CONFIG_BT_CTLR_PHY_CODED */
-
-
 static inline u32_t addr_us_get(u8_t phy)
 {
 	switch (phy) {
