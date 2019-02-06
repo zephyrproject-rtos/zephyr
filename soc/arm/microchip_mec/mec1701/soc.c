eba3b00f2cdd9355795c5fcff43c0964ea51b092
Francisco Munoz <francisco.munoz.ruiz@intel.com>
2019-02-06 17:16:55 -0500

    arch: arm: Add Microchip MEC1701 SoC
    Initial support for Microchip MEC1701 series is incorporated to the
    tree. Additional support for UART is also included. This SOC supports
    two operation modes for interrupts (Direct and Aggregated). For this
    commit, the interrupts are configured in direct mode.
    
    Signed-off-by: Francisco Munoz <francisco.munoz.ruiz@intel.com>

---
+++ soc/arm/microchip_mec/mec1701/soc.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <device.h>
+#include <init.h>
+#include <soc.h>
+#include <kernel.h>
+
+
+static int soc_init(struct device *dev)
+{
+	__IO uint32_t *girc_enable_set;
+
+	__enable_irq();
+
+	/* Enable clocks for Interrupts and CPU */
+	PCR_INST->CLK_REQ_1_b.INT_CLK_REQ = 1;
+	PCR_INST->CLK_REQ_1_b.PROCESSOR_CLK_REQ = 1;
+
+	/* Route all interrupts from EC to NVIC */
+	EC_REG_BANK_INST->INTERRUPT_CONTROL = 0x1;
+	for (girc_enable_set = (uint32_t *)&INTS_INST->GIRQ08_EN_SET;
+	     girc_enable_set <= &INTS_INST->GIRQ15_EN_SET;
+	     girc_enable_set += 5) {
+	/* This probably will require tunning, but drawing 8.2 also
+	illustrates how to diasable spurious interrupts */
+		*girc_enable_set = 0xFFFFFFFF;
+	}
+
+	return 0;
+}
+
+SYS_INIT(soc_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
