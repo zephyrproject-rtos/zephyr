db2436765245e331e2b2644ea51d86650bfbac97
Carlos Stuart <carlosstuart1970@gmail.com>
2019-02-06 08:19:01 +0000

    tests: cmsis_rtos_v2: Dynamic memory pool tests
    Implemented a second set of memory pool tests that function on a
    dynamically allocated memory pool. Abstracted some of the common
    functionality to a common function.
    
    Signed-off-by: Carlos Stuart <carlosstuart1970@gmail.com>

--- tests/cmsis_rtos_v2/src/main.c
+++ tests/cmsis_rtos_v2/src/main.c
@@ -17,6 +17,7 @@ extern void test_mutex(void);
 extern void test_mutex_lock_timeout(void);
 extern void test_semaphore(void);
 extern void test_mempool(void);
+extern void test_mempool_dynamic(void);
 extern void test_messageq(void);
 extern void test_event_flags_no_wait_timeout(void);
 extern void test_event_flags_signalled(void);
@@ -37,6 +38,7 @@ void test_main(void)
 			 ztest_unit_test(test_mutex_lock_timeout),
 			 ztest_unit_test(test_semaphore),
 			 ztest_unit_test(test_mempool),
+			 ztest_unit_test(test_mempool_dynamic),
 			 ztest_unit_test(test_messageq),
 			 ztest_unit_test(test_event_flags_no_wait_timeout),
 			 ztest_unit_test(test_event_flags_signalled),
