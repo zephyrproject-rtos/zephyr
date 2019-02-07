4fd593068ee4c2a801696a16a48f3f7b1899bec3
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2019-02-07 21:55:08 +0300

    sub-sys: disk: ram: Make RAM disk size be configurable
    Hardcoded 96KB starts to overload RAM regions and fail CI tests.
    Quick test shows that 80KB ramdisk is ok, (passes tests/posix/fs).
    And of course, targets with wealth of RAM may want to use bigger
    ramdisks.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- subsys/disk/disk_access_ram.c
+++ subsys/disk/disk_access_ram.c
@@ -20,10 +20,10 @@
  */
 #include "fat12_ramdisk.h"
 #else
-/* A 96KB RAM Disk, which meets ELM FAT fs's minimum block requirement. Fit for
- * qemu testing (as it may exceed target's RAM limits).
+/* RAM Disk of configurable size. Fit for qemu testing (as it may exceed
+ * target's RAM limits).
  */
-#define RAMDISK_VOLUME_SIZE (192 * RAMDISK_SECTOR_SIZE)
+#define RAMDISK_VOLUME_SIZE (CONFIG_DISK_RAM_VOLUME_SIZE * 1024)
 static u8_t ramdisk_buf[RAMDISK_VOLUME_SIZE];
 #endif
 
