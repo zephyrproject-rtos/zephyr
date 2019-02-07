89bf1578d9624a146e9c41fb3f2bb75ac2ed4a66
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2019-02-07 14:44:15 +0200

    net: sockets: Add a way to register a socket family handler
    Allow automatic handling of registered socket families.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- subsys/net/lib/sockets/sockets.c
+++ subsys/net/lib/sockets/sockets.c
@@ -20,6 +20,9 @@ LOG_MODULE_REGISTER(net_sock, CONFIG_NET_SOCKETS_LOG_LEVEL);
 
 #include "sockets_internal.h"
 
+extern struct net_socket_register __net_socket_register_start[];
+extern struct net_socket_register __net_socket_register_end[];
+
 #define SET_ERRNO(x) \
 	{ int _err = x; if (_err < 0) { errno = -_err; return -1; } }
 
@@ -129,6 +132,25 @@ int zsock_socket_internal(int family, int type, int proto)
 
 int z_impl_zsock_socket(int family, int type, int proto)
 {
+	struct net_socket_register *sock_family;
+
+	for (sock_family = __net_socket_register_start;
+	     sock_family != __net_socket_register_end;
+	     sock_family++) {
+		if (sock_family->family != family &&
+		    sock_family->family != AF_UNSPEC) {
+			continue;
+		}
+
+		NET_ASSERT(sock_family->is_supported);
+
+		if (!sock_family->is_supported(family, type, proto)) {
+			continue;
+		}
+
+		return sock_family->handler(family, type, proto);
+	}
+
 #if defined(CONFIG_NET_SOCKETS_SOCKOPT_TLS)
 	if (((proto >= IPPROTO_TLS_1_0) && (proto <= IPPROTO_TLS_1_2)) ||
 	    (proto >= IPPROTO_DTLS_1_0 && proto <= IPPROTO_DTLS_1_2)) {
