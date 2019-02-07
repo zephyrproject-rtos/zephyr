0ceab99865aa1162438b6e98f1241d1bdb401104
Andrei Gansari <andrei.gansari@nxp.com>
2019-02-07 17:54:55 +0200

    samples: net: removed unused common cmake
    CONFIG_NET_TESTING never used, removed in this commit.
    
    Signed-off-by: Andrei Gansari <andrei.gansari@nxp.com>

--- samples/net/common/common.cmake
+++ samples/net/common/common.cmake
@@ -1,14 +1,1 @@
 # Common routines used in net samples
-
-if(CONFIG_NET_TESTING)
-  target_include_directories(
-    app
-    PRIVATE
-    $ENV{ZEPHYR_BASE}/samples/net/common/
-    )
-  target_compile_definitions(
-    app
-    PRIVATE
-    NET_TESTING_SERVER=1
-    )
-endif()
