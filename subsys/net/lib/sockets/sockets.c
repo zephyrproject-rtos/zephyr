b3cd478a5f0dd37165f70cef7fd437f7e005b513
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2019-02-07 15:34:46 +0200

    net: sockets: packet: Register handler for AF_PACKET
    Use the automatic registration of AF_PACKET type sockets.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- subsys/net/lib/sockets/sockets.c
+++ subsys/net/lib/sockets/sockets.c
@@ -151,12 +151,6 @@ int z_impl_zsock_socket(int family, int type, int proto)
 		return sock_family->handler(family, type, proto);
 	}
 
-#if defined(CONFIG_NET_SOCKETS_PACKET)
-	if (family == AF_PACKET) {
-		return zpacket_socket(family, type, proto);
-	}
-#endif
-
 #if defined(CONFIG_NET_SOCKETS_CAN)
 	if (family == AF_CAN) {
 		return zcan_socket(family, type, proto);
