c71ce193c45479804a0528e517e4ab6fe7add0c0
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2019-02-08 10:13:54 +0100

    net/pkt: Rename net_pkt_pull_new to net_pkt_pull
    Now that legacy net_pkt_pull function has been removed, the new
    function can be renamed accordingly.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- subsys/net/ip/net_pkt.c
+++ subsys/net/ip/net_pkt.c
@@ -2621,7 +2621,7 @@ int net_pkt_update_length(struct net_pkt *pkt, size_t length)
 	return !length ? 0 : -EINVAL;
 }
 
-int net_pkt_pull_new(struct net_pkt *pkt, size_t length)
+int net_pkt_pull(struct net_pkt *pkt, size_t length)
 {
 	struct net_pkt_cursor *c_op = &pkt->cursor;
 	struct net_pkt_cursor backup;
@@ -2651,7 +2651,7 @@ int net_pkt_pull_new(struct net_pkt *pkt, size_t length)
 
 		/* For now, empty buffer are not freed, and there is no
 		 * compaction done either.
-		 * net_pkt_pull_new() is currently used only in very specific
+		 * net_pkt_pull() is currently used only in very specific
 		 * places where such memory optimization would not make
 		 * that much sense. Let's see in future if it's worth do to it.
 		 */
