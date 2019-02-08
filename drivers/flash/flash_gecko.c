c962dc8b561ae4b4ab393274136fd77015bdb186
Kumar Gala <kumar.gala@linaro.org>
2019-02-08 12:34:48 -0600

    flash: flash_gecko: Convert FLASH_WRITE_BLOCK_SIZE to DT_
    Convert generic FLASH_WRITE_BLOCK_SIZE to driver specific generated
    define DT_SOC_NV_FLASH_0_WRITE_BLOCK_SIZE.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- drivers/flash/flash_gecko.c
+++ drivers/flash/flash_gecko.c
@@ -180,10 +180,7 @@ static const struct flash_driver_api flash_gecko_driver_api = {
 	.write = flash_gecko_write,
 	.erase = flash_gecko_erase,
 	.write_protection = flash_gecko_write_protection,
-	/* FLASH_WRITE_BLOCK_SIZE is extracted from device tree as flash node
-	 * property 'write-block-size'.
-	 */
-	.write_block_size = FLASH_WRITE_BLOCK_SIZE,
+	.write_block_size = DT_SOC_NV_FLASH_0_WRITE_BLOCK_SIZE,
 };
 
 static struct flash_gecko_data flash_gecko_0_data;
