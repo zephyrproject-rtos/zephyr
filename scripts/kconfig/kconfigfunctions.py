zephyrproject-rtos/zephyr

commit f2ef52f12240cd891aa471e5984fd22a3a1f5e93

kconfig: kconfigfunctions: update dt_str_val
Clarify the docs for dt_str_val that if the name isn't found we return
and empty string.  Also cleanup the code slightly as we don't need to
escape the double quote.

Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

scripts/kconfig/kconfigfunctions.py
@@ -79,12 +79,13 @@ def dt_hex_val(kconf, _, name, unit=None):
 def dt_str_val(kconf, _, name):
     """
     This function looks up 'name' in the DTS generated "conf" style database
-    and if its found it will return the value as string.
+    and if its found it will return the value as string.  If its not found we
+    return an empty string.
     """
     if doc_mode or name not in dt_defines:
         return ""
 
-    return dt_defines[name].strip('\"')
+    return dt_defines[name].strip('"')
 
 functions = {
         "dt_int_val": (dt_int_val, 1, 2),

commit d6f3be56d66d06d621e49ddd9beaa16161034d47

doc: fix misspelling in kconfig doc
Fix typos introduced by PR #13186

Signed-off-by: David B. Kinder <david.b.kinder@intel.com>

scripts/kconfig/kconfigfunctions.py
@@ -35,7 +35,7 @@ def _dt_units_to_scale(unit):
 def dt_int_val(kconf, _, name, unit=None):
     """
     This function looks up 'name' in the DTS generated "conf" style database
-    and if its found it will return the value as an decimal integer.  The
+    and if it's found it will return the value as an decimal integer.  The
     function will divide the value based on 'unit':
         None        No division
         'k' or 'K'  divide by 1024 (1 << 10)
@@ -57,7 +57,7 @@ def dt_int_val(kconf, _, name, unit=None):
 def dt_hex_val(kconf, _, name, unit=None):
     """
     This function looks up 'name' in the DTS generated "conf" style database
-    and if its found it will return the value as an hex integer.  The
+    and if it's found it will return the value as an hex integer.  The
     function will divide the value based on 'unit':
         None        No division
         'k' or 'K'  divide by 1024 (1 << 10)
@@ -79,7 +79,7 @@ def dt_hex_val(kconf, _, name, unit=None):
 def dt_str_val(kconf, _, name):
     """
     This function looks up 'name' in the DTS generated "conf" style database
-    and if its found it will return the value as string.  If its not found we
+    and if it's found it will return the value as string.  If it's not found we
     return an empty string.
     """
     if doc_mode or name not in dt_defines: