989eeaf96b246df7222c86ee516b45a81c4d61c2
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2019-02-08 14:13:42 +0200

    net: canbus: Set link address type correctly
    The link address type should be CANBUS of course.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- subsys/net/l2/canbus/canbus.c
+++ subsys/net/l2/canbus/canbus.c
@@ -18,10 +18,10 @@ static inline enum net_verdict canbus_recv(struct net_if *iface,
 {
 	net_pkt_lladdr_src(pkt)->addr = NULL;
 	net_pkt_lladdr_src(pkt)->len = 0;
-	net_pkt_lladdr_src(pkt)->type = NET_LINK_DUMMY;
+	net_pkt_lladdr_src(pkt)->type = NET_LINK_CANBUS;
 	net_pkt_lladdr_dst(pkt)->addr = NULL;
 	net_pkt_lladdr_dst(pkt)->len = 0;
-	net_pkt_lladdr_dst(pkt)->type = NET_LINK_DUMMY;
+	net_pkt_lladdr_dst(pkt)->type = NET_LINK_CANBUS;
 
 	net_pkt_set_family(pkt, AF_CAN);
 
