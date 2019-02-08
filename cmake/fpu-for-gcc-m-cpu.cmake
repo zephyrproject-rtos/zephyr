6698b27d2b00072cbc63ed9dab7075409f1ef702
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2019-02-08 14:58:26 +0100

    soc: arm: Kconfig option to indicate double-precision FP
    This commit introduces a Kconfig option for ARM SoCs, to signify
    that the SoC implements a double-precision floating point unit.
    The option is utilized in cmake/fpu-for-gcc-m-cpu to select the
    appropriate GCC compile option flag for FPU.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- cmake/fpu-for-gcc-m-cpu.cmake
+++ cmake/fpu-for-gcc-m-cpu.cmake
@@ -1,5 +1,11 @@
 # Defines a mapping from GCC_M_CPU to FPU
 
-set(FPU_FOR_cortex-m4      fpv4-sp-d16)
-set(FPU_FOR_cortex-m7      fpv5-d16)
-set(FPU_FOR_cortex-m33     fpv5-sp-d16)
+if(CONFIG_CPU_HAS_FPU_DOUBLE_PRECISION)
+  set(PRECISION_TOKEN)
+else()
+  set(PRECISION_TOKEN sp-)
+endif()
+
+set(FPU_FOR_cortex-m4      fpv4-${PRECISION_TOKEN}d16)
+set(FPU_FOR_cortex-m7      fpv5-${PRECISION_TOKEN}d16)
+set(FPU_FOR_cortex-m33     fpv5-${PRECISION_TOKEN}d16)
