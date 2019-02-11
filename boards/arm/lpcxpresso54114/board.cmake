4de99994f0131e041ae9c71eb9de9fe659fdb5d2
Maureen Helm <maureen.helm@nxp.com>
2019-02-11 14:09:11 -0600

    scripts: west: Add reset-after-load argument to jlink runner
    Adds a new argument to the jlink runner to reset the device after
    loading code to flash. This fixes a problem with the lpcxpresso54114
    board where it was necessary to manually reset the board to get new code
    to start running after the 'ninja flash' command. This new argument is
    optional and false by default because there are some cases were we must
    not reset after load, such as when we load the application into ITCM on
    imx rt devices.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- boards/arm/lpcxpresso54114/board.cmake
+++ boards/arm/lpcxpresso54114/board.cmake
@@ -12,9 +12,9 @@ if(LPCLINK_FW STREQUAL jlink)
 endif()
 
 if(CONFIG_BOARD_LPCXPRESSO54114_M4)
-board_runner_args(jlink "--device=LPC54114J256_M4")
+board_runner_args(jlink "--device=LPC54114J256_M4" "--reset-after-load")
 elseif(CONFIG_BOARD_LPCXPRESSO54114_M0)
-board_runner_args(jlink "--device=LPC54114J256_M0")
+board_runner_args(jlink "--device=LPC54114J256_M0" "--reset-after-load")
 endif()
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
