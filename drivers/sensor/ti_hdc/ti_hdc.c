4a38cae2af5dc89aafe2ce19540e8822f2c52d23
Nikos Oikonomou <nikoikonomou92@gmail.com>
2019-02-12 11:50:47 +0200

    sensors: renamed hdc1008 driver into ti_hdc
    Hdc1008 driver is renamed into ti_hdc to prepare it to support all
    available Texas Instruments HDC sensors (e.g. hdc1080, hdc2080).
    
    Signed-off-by: Nikos Oikonomou <nikoikonomou92@gmail.com>

--- drivers/sensor/ti_hdc/ti_hdc.c
+++ drivers/sensor/ti_hdc/ti_hdc.c
@@ -13,43 +13,43 @@
 #include <misc/__assert.h>
 #include <logging/log.h>
 
-#include "hdc1008.h"
+#include "ti_hdc.h"
 
 #define LOG_LEVEL CONFIG_SENSOR_LOG_LEVEL
-LOG_MODULE_REGISTER(HDC1008);
+LOG_MODULE_REGISTER(TI_HDC);
 
-static void hdc1008_gpio_callback(struct device *dev,
+static void ti_hdc_gpio_callback(struct device *dev,
 				  struct gpio_callback *cb, u32_t pins)
 {
-	struct hdc1008_data *drv_data =
-		CONTAINER_OF(cb, struct hdc1008_data, gpio_cb);
+	struct ti_hdc_data *drv_data =
+		CONTAINER_OF(cb, struct ti_hdc_data, gpio_cb);
 
 	ARG_UNUSED(pins);
 
-	gpio_pin_disable_callback(dev, DT_TI_HDC1008_0_DRDY_GPIOS_PIN);
+	gpio_pin_disable_callback(dev, DT_TI_HDC_0_DRDY_GPIOS_PIN);
 	k_sem_give(&drv_data->data_sem);
 }
 
-static int hdc1008_sample_fetch(struct device *dev, enum sensor_channel chan)
+static int ti_hdc_sample_fetch(struct device *dev, enum sensor_channel chan)
 {
-	struct hdc1008_data *drv_data = dev->driver_data;
+	struct ti_hdc_data *drv_data = dev->driver_data;
 	u8_t buf[4];
 
 	__ASSERT_NO_MSG(chan == SENSOR_CHAN_ALL);
 
 	gpio_pin_enable_callback(drv_data->gpio,
-				 DT_TI_HDC1008_0_DRDY_GPIOS_PIN);
+				 DT_TI_HDC_0_DRDY_GPIOS_PIN);
 
-	buf[0] = HDC1008_REG_TEMP;
+	buf[0] = TI_HDC_REG_TEMP;
 	if (i2c_write(drv_data->i2c, buf, 1,
-		      DT_TI_HDC1008_0_BASE_ADDRESS) < 0) {
+		      DT_TI_HDC_0_BASE_ADDRESS) < 0) {
 		LOG_DBG("Failed to write address pointer");
 		return -EIO;
 	}
 
 	k_sem_take(&drv_data->data_sem, K_FOREVER);
 
-	if (i2c_read(drv_data->i2c, buf, 4, DT_TI_HDC1008_0_BASE_ADDRESS) < 0) {
+	if (i2c_read(drv_data->i2c, buf, 4, DT_TI_HDC_0_BASE_ADDRESS) < 0) {
 		LOG_DBG("Failed to read sample data");
 		return -EIO;
 	}
@@ -61,11 +61,11 @@ static int hdc1008_sample_fetch(struct device *dev, enum sensor_channel chan)
 }
 
 
-static int hdc1008_channel_get(struct device *dev,
+static int ti_hdc_channel_get(struct device *dev,
 			       enum sensor_channel chan,
 			       struct sensor_value *val)
 {
-	struct hdc1008_data *drv_data = dev->driver_data;
+	struct ti_hdc_data *drv_data = dev->driver_data;
 	u64_t tmp;
 
 	/*
@@ -92,9 +92,9 @@ static int hdc1008_channel_get(struct device *dev,
 	return 0;
 }
 
-static const struct sensor_driver_api hdc1008_driver_api = {
-	.sample_fetch = hdc1008_sample_fetch,
-	.channel_get = hdc1008_channel_get,
+static const struct sensor_driver_api ti_hdc_driver_api = {
+	.sample_fetch = ti_hdc_sample_fetch,
+	.channel_get = ti_hdc_channel_get,
 };
 
 static u16_t read16(struct device *dev, u8_t a, u8_t d)
@@ -106,25 +106,25 @@ static u16_t read16(struct device *dev, u8_t a, u8_t d)
 	return (buf[0] << 8 | buf[1]);
 }
 
-static int hdc1008_init(struct device *dev)
+static int ti_hdc_init(struct device *dev)
 {
-	struct hdc1008_data *drv_data = dev->driver_data;
+	struct ti_hdc_data *drv_data = dev->driver_data;
 
-	drv_data->i2c = device_get_binding(DT_TI_HDC1008_0_BUS_NAME);
+	drv_data->i2c = device_get_binding(DT_TI_HDC_0_BUS_NAME);
 
 	if (drv_data->i2c == NULL) {
 		LOG_DBG("Failed to get pointer to %s device!",
-			DT_TI_HDC1008_0_BUS_NAME);
+			DT_TI_HDC_0_BUS_NAME);
 		return -EINVAL;
 	}
 
-	if (read16(drv_data->i2c, DT_TI_HDC1008_0_BASE_ADDRESS,
-		   HDC1008_REG_MANUFID) != HDC1008_MANUFID) {
+	if (read16(drv_data->i2c, DT_TI_HDC_0_BASE_ADDRESS,
+		   TI_HDC_REG_MANUFID) != TI_HDC_MANUFID) {
 		LOG_ERR("Failed to get correct manufacturer ID");
 		return -EINVAL;
 	}
-	if (read16(drv_data->i2c, DT_TI_HDC1008_0_BASE_ADDRESS,
-		   HDC1008_REG_DEVICEID) != HDC1008_DEVICEID) {
+	if (read16(drv_data->i2c, DT_TI_HDC_0_BASE_ADDRESS,
+		   TI_HDC_REG_DEVICEID) != TI_HDC_DEVICEID) {
 		LOG_ERR("Failed to get correct device ID");
 		return -EINVAL;
 	}
@@ -133,23 +133,23 @@ static int hdc1008_init(struct device *dev)
 
 	/* setup data ready gpio interrupt */
 	drv_data->gpio = device_get_binding(
-				DT_TI_HDC1008_0_DRDY_GPIOS_CONTROLLER);
+				DT_TI_HDC_0_DRDY_GPIOS_CONTROLLER);
 	if (drv_data->gpio == NULL) {
 		LOG_DBG("Failed to get pointer to %s device",
-			 DT_TI_HDC1008_0_DRDY_GPIOS_CONTROLLER);
+			 DT_TI_HDC_0_DRDY_GPIOS_CONTROLLER);
 		return -EINVAL;
 	}
 
-	gpio_pin_configure(drv_data->gpio, DT_TI_HDC1008_0_DRDY_GPIOS_PIN,
+	gpio_pin_configure(drv_data->gpio, DT_TI_HDC_0_DRDY_GPIOS_PIN,
 			   GPIO_DIR_IN | GPIO_INT | GPIO_INT_EDGE |
-#if defined(DT_TI_HDC1008_0_DRDY_GPIOS_FLAGS)
-			   DT_TI_HDC1008_0_DRDY_GPIOS_FLAGS |
+#if defined(DT_TI_HDC_0_DRDY_GPIOS_FLAGS)
+			   DT_TI_HDC_0_DRDY_GPIOS_FLAGS |
 #endif
 			   GPIO_INT_ACTIVE_LOW | GPIO_INT_DEBOUNCE);
 
 	gpio_init_callback(&drv_data->gpio_cb,
-			   hdc1008_gpio_callback,
-			   BIT(DT_TI_HDC1008_0_DRDY_GPIOS_PIN));
+			   ti_hdc_gpio_callback,
+			   BIT(DT_TI_HDC_0_DRDY_GPIOS_PIN));
 
 	if (gpio_add_callback(drv_data->gpio, &drv_data->gpio_cb) < 0) {
 		LOG_DBG("Failed to set GPIO callback");
@@ -159,8 +159,8 @@ static int hdc1008_init(struct device *dev)
 	return 0;
 }
 
-static struct hdc1008_data hdc1008_data;
+static struct ti_hdc_data ti_hdc_data;
 
-DEVICE_AND_API_INIT(hdc1008, DT_TI_HDC1008_0_LABEL, hdc1008_init, &hdc1008_data,
+DEVICE_AND_API_INIT(ti_hdc, DT_TI_HDC_0_LABEL, ti_hdc_init, &ti_hdc_data,
 		    NULL, POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY,
-		    &hdc1008_driver_api);
+		    &ti_hdc_driver_api);
