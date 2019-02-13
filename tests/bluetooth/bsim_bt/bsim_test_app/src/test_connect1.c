dc91515b5c55a18ab354e1f8a9a1a67e73d141a9
Wolfgang Puffitsch <wopu@oticon.com>
2019-02-13 10:31:47 +0100

    tests: bsim_bt: Extend connection test case.
    Change connection parameters and channel map after connection is
    established. Test encrypted connection also with split stack. Tighten
    pass/fail criteria.
    
    Signed-off-by: Wolfgang Puffitsch <wopu@oticon.com>

--- tests/bluetooth/bsim_bt/bsim_test_app/src/test_connect1.c
+++ tests/bluetooth/bsim_bt/bsim_test_app/src/test_connect1.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2015-2016 Intel Corporation
- * Copyright (c) 2017-2018 Oticon A/S
+ * Copyright (c) 2017-2019 Oticon A/S
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -30,21 +30,47 @@ static struct bt_uuid_16 uuid = BT_UUID_INIT_16(0);
 static struct bt_gatt_discover_params discover_params;
 static struct bt_gatt_subscribe_params subscribe_params;
 
+#define UPDATE_PARAM_INTERVAL_MIN 25
+#define UPDATE_PARAM_INTERVAL_MAX 45
+#define UPDATE_PARAM_LATENCY      1
+#define UPDATE_PARAM_TIMEOUT      250
+
+static struct bt_le_conn_param update_params = {
+	.interval_min = UPDATE_PARAM_INTERVAL_MIN,
+	.interval_max = UPDATE_PARAM_INTERVAL_MAX,
+	.latency = UPDATE_PARAM_LATENCY,
+	.timeout = UPDATE_PARAM_TIMEOUT,
+};
+
 static bool encrypt_link;
+
 /*
  * Basic connection test:
- *   We expect to find a connectable peripheral (a Zephyr's
- *   samples/bluetooth/peripheral) to which we will connect.
+ *   We expect to find a connectable peripheral to which we will
+ *   connect.
  *
- *   After connecting we expect to receive 2 notifications.
+ *   After connecting, we update connection parameters and channel
+ *   map, and expect to receive 2 notifications.
  *   If we do, the test case passes.
  *   If we do not in 5 seconds, the testcase is considered failed
  *
  *   The thread code is mostly a copy of the central_hr sample device
  */
 
-#define WAIT_TIME 5 /*seconds*/
+#define WAIT_TIME 6 /*seconds*/
 extern enum bst_result_t bst_result;
+
+#define FAIL(...)					\
+	do {						\
+		bst_result = Failed;			\
+		bs_trace_error_time_line(__VA_ARGS__);	\
+	} while (0)
+
+#define PASS(...)					\
+	do {						\
+		bst_result = Passed;			\
+		bs_trace_info_time(1, __VA_ARGS__);	\
+	} while (0)
 
 static void test_con1_init(void)
 {
@@ -64,9 +90,10 @@ static void test_con1_tick(bs_time_t HW_device_time)
 	 * If in WAIT_TIME seconds the testcase did not already pass
 	 * (and finish) we consider it failed
 	 */
-	bst_result = Failed;
-	bs_trace_error_line("test: connect1 failed (no notification received "
-			    "after %i seconds)\n", WAIT_TIME);
+	if (bst_result != Passed) {
+		FAIL("test_connect1 failed (not passed after %i seconds)\n",
+		     WAIT_TIME);
+	}
 }
 
 static u8_t notify_func(struct bt_conn *conn,
@@ -83,8 +110,19 @@ static u8_t notify_func(struct bt_conn *conn,
 	printk("[NOTIFICATION] data %p length %u\n", data, length);
 
 	if (notify_count++ >= 1) { /* We consider it passed */
-		bst_result = Passed;
-		bs_trace_exit_time("Testcase passed\n");
+		int err;
+
+		/* Disconnect before actually passing */
+		err = bt_conn_disconnect(default_conn, BT_HCI_ERR_SUCCESS);
+		if (err) {
+			FAIL("Disconnection failed (err %d)\n", err);
+			return BT_GATT_ITER_STOP;
+		}
+
+		if (bst_result != Failed) {
+			PASS("Testcase passed\n");
+		}
+		bs_trace_silent_exit(0);
 	}
 
 	return BT_GATT_ITER_CONTINUE;
@@ -112,7 +150,7 @@ static u8_t discover_func(struct bt_conn *conn,
 
 		err = bt_gatt_discover(conn, &discover_params);
 		if (err) {
-			printk("Discover failed (err %d)\n", err);
+			FAIL("Discover failed (err %d)\n", err);
 		}
 	} else if (!bt_uuid_cmp(discover_params.uuid,
 			BT_UUID_HRS_MEASUREMENT)) {
@@ -124,7 +162,7 @@ static u8_t discover_func(struct bt_conn *conn,
 
 		err = bt_gatt_discover(conn, &discover_params);
 		if (err) {
-			printk("Discover failed (err %d)\n", err);
+			FAIL("Discover failed (err %d)\n", err);
 		}
 	} else {
 		subscribe_params.notify = notify_func;
@@ -133,7 +171,7 @@ static u8_t discover_func(struct bt_conn *conn,
 
 		err = bt_gatt_subscribe(conn, &subscribe_params);
 		if (err && err != -EALREADY) {
-			printk("Subscribe failed (err %d)\n", err);
+			FAIL("Subscribe failed (err %d)\n", err);
 		} else {
 			printk("[SUBSCRIBED]\n");
 		}
@@ -144,6 +182,28 @@ static u8_t discover_func(struct bt_conn *conn,
 	return BT_GATT_ITER_STOP;
 }
 
+static void update_conn(struct bt_conn *conn, bool bonded)
+{
+	int err;
+
+	if (encrypt_link != bonded) {
+		FAIL("Unexpected bonding status\n");
+		return;
+	}
+
+	printk("Updating connection (bonded: %d)\n", bonded);
+
+	err = bt_conn_le_param_update(conn, &update_params);
+	if (err) {
+		FAIL("Parameter update failed (err %d)\n", err);
+		return;
+	}
+}
+
+static struct bt_conn_auth_cb auth_cb_success = {
+	.pairing_complete = update_conn,
+};
+
 static void connected(struct bt_conn *conn, u8_t conn_err)
 {
 	char addr[BT_ADDR_LE_STR_LEN];
@@ -152,7 +212,7 @@ static void connected(struct bt_conn *conn, u8_t conn_err)
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
 	if (conn_err) {
-		printk("Failed to connect to %s (%u)\n", addr, conn_err);
+		FAIL("Failed to connect to %s (%u)\n", addr, conn_err);
 		return;
 	}
 
@@ -164,12 +224,40 @@ static void connected(struct bt_conn *conn, u8_t conn_err)
 
 	if (encrypt_link) {
 		k_sleep(500);
-		bt_conn_auth_cb_register(NULL);
+		bt_conn_auth_cb_register(&auth_cb_success);
 		err = bt_conn_security(conn, BT_SECURITY_MEDIUM);
 		if (err) {
-			printk("bt_conn_security failed (err %d)\n", err);
+			FAIL("bt_conn_security failed (err %d)\n", err);
 			return;
 		}
+	} else {
+		update_conn(conn, false);
+	}
+}
+
+static void params_updated(struct bt_conn *conn, u16_t interval,
+			   u16_t latency, u16_t timeout)
+{
+	u8_t chm[5] = { 0x11, 0x22, 0x33, 0x44, 0x00 };
+	int err;
+
+	if (interval != UPDATE_PARAM_INTERVAL_MAX ||
+	    latency != UPDATE_PARAM_LATENCY ||
+	    timeout != UPDATE_PARAM_TIMEOUT) {
+		FAIL("Unexpected connection parameters "
+		     "(interval: %d, latency: %d, timeout: %d)\n",
+		     interval, latency, timeout);
+		return;
+	}
+
+	printk("Connection parameters updated "
+	       "(interval: %d, latency: %d, timeout: %d)\n",
+	       interval, latency, timeout);
+
+	err = bt_le_set_chan_map(chm);
+	if (err) {
+		FAIL("Channel map update failed (err %d)\n", err);
+		return;
 	}
 
 	memcpy(&uuid, BT_UUID_HRS, sizeof(uuid));
@@ -179,9 +267,9 @@ static void connected(struct bt_conn *conn, u8_t conn_err)
 	discover_params.end_handle = 0xffff;
 	discover_params.type = BT_GATT_DISCOVER_PRIMARY;
 
-	err = bt_gatt_discover(default_conn, &discover_params);
+	err = bt_gatt_discover(conn, &discover_params);
 	if (err) {
-		printk("Discover failed(err %d)\n", err);
+		FAIL("Discover failed(err %d)\n", err);
 		return;
 	}
 }
@@ -197,7 +285,7 @@ static bool eir_found(struct bt_data *data, void *user_data)
 	case BT_DATA_UUID16_SOME:
 	case BT_DATA_UUID16_ALL:
 		if (data->data_len % sizeof(u16_t) != 0) {
-			printk("AD malformed\n");
+			FAIL("AD malformed\n");
 			return true;
 		}
 
@@ -214,7 +302,7 @@ static bool eir_found(struct bt_data *data, void *user_data)
 
 			err = bt_le_scan_stop();
 			if (err) {
-				printk("Stop LE scan failed (err %d)\n", err);
+				FAIL("Stop LE scan failed (err %d)\n", err);
 				continue;
 			}
 
@@ -261,13 +349,14 @@ static void disconnected(struct bt_conn *conn, u8_t reason)
 	/* This demo doesn't require active scan */
 	err = bt_le_scan_start(BT_LE_SCAN_PASSIVE, device_found);
 	if (err) {
-		printk("Scanning failed to start (err %d)\n", err);
+		FAIL("Scanning failed to start (err %d)\n", err);
 	}
 }
 
 static struct bt_conn_cb conn_callbacks = {
 	.connected = connected,
 	.disconnected = disconnected,
+	.le_param_updated = params_updated,
 };
 
 static void test_con1_main(void)
@@ -277,7 +366,7 @@ static void test_con1_main(void)
 	err = bt_enable(NULL);
 
 	if (err) {
-		printk("Bluetooth init failed (err %d)\n", err);
+		FAIL("Bluetooth init failed (err %d)\n", err);
 		return;
 	}
 
@@ -288,7 +377,7 @@ static void test_con1_main(void)
 	err = bt_le_scan_start(BT_LE_SCAN_ACTIVE, device_found);
 
 	if (err) {
-		printk("Scanning failed to start (err %d)\n", err);
+		FAIL("Scanning failed to start (err %d)\n", err);
 		return;
 	}
 
@@ -298,18 +387,17 @@ static void test_con1_main(void)
 static const struct bst_test_instance test_connect[] = {
 	{
 		.test_id = "central",
-		.test_descr = "Basic connection test. It expects that another "
-			      "device running a sample/bluetooth/peripheral_hr "
-			      "can be found. The test will pass if it can "
-			      "connect to it, and receive a notification in "
-			      "less than 5 seconds",
+		.test_descr = "Basic connection test. It expects that a "
+			      "peripheral device can be found. The test will "
+			      "pass if it can connect to it, and receive a "
+			      "notification in less than 5 seconds.",
 		.test_post_init_f = test_con1_init,
 		.test_tick_f = test_con1_tick,
 		.test_main_f = test_con1_main
 	},
 	{
 		.test_id = "central_encrypted",
-		.test_descr = "Same as connect but with an encrypted link",
+		.test_descr = "Same as central but with an encrypted link",
 		.test_post_init_f = test_con_encrypted_init,
 		.test_tick_f = test_con1_tick,
 		.test_main_f = test_con1_main
