8b65a90e288ace2f61631ec17001184b05855ada
Vincent Wan <vincent.wan@linaro.org>
2019-02-13 16:55:28 -0800

    ext: simplelink: Add updated CC3220 board files
    This commit adds updated board files to configure the peripheral
    drivers on the CC3220.
    
    Signed-off-by: Vincent Wan <vincent.wan@linaro.org>

---
+++ ext/hal/ti/simplelink/source/ti/drivers/net/wifi/porting/CC3220SF_LAUNCHXL.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2017, Texas Instruments Incorporated
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef _CC3220SF_LAUNCHXL_H_
+#define _CC3220SF_LAUNCHXL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void CC3220SF_LAUNCHXL_init();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _CC3220SF_LAUNCHXL_H_ */
