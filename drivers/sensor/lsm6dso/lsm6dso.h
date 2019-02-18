72ae24d8d59793bc327024b88e6a8baa09b154d6
Armando Visconti <armando.visconti@st.com>
2019-02-18 15:17:54 +0100

    driver/sensor: lsm6dso: Add support
    Add support to STM LSM6DSO 6-axis IMU sensor driver.
    This driver supports communication with device though both
    I2C and SPI bus and both polling and drdy trigger mode.
    
    This driver supports also the sensorhub mode with the possibility
    to connect a maximum of two external devices, typically a
    magnetometer and an environmental sensor, currently selected among
    following devices: lis2mdl magnetometer, lps22hh or lps22hb
    pressure and temperature sensors, HTS221 humidity sensor.
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

---
+++ drivers/sensor/lsm6dso/lsm6dso.h
@@ -0,0 +1,203 @@
+/* ST Microelectronics LSM6DSO 6-axis IMU sensor driver
+ *
+ * Copyright (c) 2019 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Datasheet:
+ * https://www.st.com/resource/en/datasheet/lsm6dso.pdf
+ */
+
+#ifndef ZEPHYR_DRIVERS_SENSOR_LSM6DSO_LSM6DSO_H_
+#define ZEPHYR_DRIVERS_SENSOR_LSM6DSO_LSM6DSO_H_
+
+#include <sensor.h>
+#include <zephyr/types.h>
+#include <gpio.h>
+#include <drivers/spi.h>
+#include <sys/util.h>
+#include "lsm6dso_reg.h"
+
+#define LSM6DSO_EN_BIT					0x01
+#define LSM6DSO_DIS_BIT					0x00
+
+/* Accel sensor sensitivity grain is 61 ug/LSB */
+#define GAIN_UNIT_XL				(61LL)
+
+/* Gyro sensor sensitivity grain is 4.375 udps/LSB */
+#define GAIN_UNIT_G				(4375LL)
+
+#define SENSOR_PI_DOUBLE			(SENSOR_PI / 1000000.0)
+#define SENSOR_DEG2RAD_DOUBLE			(SENSOR_PI_DOUBLE / 180)
+#define SENSOR_G_DOUBLE				(SENSOR_G / 1000000.0)
+
+#if CONFIG_LSM6DSO_ACCEL_FS == 0
+	#define LSM6DSO_ACCEL_FS_RUNTIME 1
+	#define LSM6DSO_DEFAULT_ACCEL_FULLSCALE		0
+	#define LSM6DSO_DEFAULT_ACCEL_SENSITIVITY	GAIN_UNIT_XL
+#elif CONFIG_LSM6DSO_ACCEL_FS == 2
+	#define LSM6DSO_DEFAULT_ACCEL_FULLSCALE		0
+	#define LSM6DSO_DEFAULT_ACCEL_SENSITIVITY	GAIN_UNIT_XL
+#elif CONFIG_LSM6DSO_ACCEL_FS == 4
+	#define LSM6DSO_DEFAULT_ACCEL_FULLSCALE		2
+	#define LSM6DSO_DEFAULT_ACCEL_SENSITIVITY	(2.0 * GAIN_UNIT_XL)
+#elif CONFIG_LSM6DSO_ACCEL_FS == 8
+	#define LSM6DSO_DEFAULT_ACCEL_FULLSCALE		3
+	#define LSM6DSO_DEFAULT_ACCEL_SENSITIVITY	(4.0 * GAIN_UNIT_XL)
+#elif CONFIG_LSM6DSO_ACCEL_FS == 16
+	#define LSM6DSO_DEFAULT_ACCEL_FULLSCALE		1
+	#define LSM6DSO_DEFAULT_ACCEL_SENSITIVITY	(8.0 * GAIN_UNIT_XL)
+#endif
+
+#if (CONFIG_LSM6DSO_ACCEL_ODR == 0)
+#define LSM6DSO_ACCEL_ODR_RUNTIME 1
+#endif
+
+#define GYRO_FULLSCALE_125 4
+
+#if CONFIG_LSM6DSO_GYRO_FS == 0
+	#define LSM6DSO_GYRO_FS_RUNTIME 1
+	#define LSM6DSO_DEFAULT_GYRO_FULLSCALE		4
+	#define LSM6DSO_DEFAULT_GYRO_SENSITIVITY	GAIN_UNIT_G
+#elif CONFIG_LSM6DSO_GYRO_FS == 125
+	#define LSM6DSO_DEFAULT_GYRO_FULLSCALE		4
+	#define LSM6DSO_DEFAULT_GYRO_SENSITIVITY	GAIN_UNIT_G
+#elif CONFIG_LSM6DSO_GYRO_FS == 250
+	#define LSM6DSO_DEFAULT_GYRO_FULLSCALE		0
+	#define LSM6DSO_DEFAULT_GYRO_SENSITIVITY	(2.0 * GAIN_UNIT_G)
+#elif CONFIG_LSM6DSO_GYRO_FS == 500
+	#define LSM6DSO_DEFAULT_GYRO_FULLSCALE		1
+	#define LSM6DSO_DEFAULT_GYRO_SENSITIVITY	(4.0 * GAIN_UNIT_G)
+#elif CONFIG_LSM6DSO_GYRO_FS == 1000
+	#define LSM6DSO_DEFAULT_GYRO_FULLSCALE		2
+	#define LSM6DSO_DEFAULT_GYRO_SENSITIVITY	(8.0 * GAIN_UNIT_G)
+#elif CONFIG_LSM6DSO_GYRO_FS == 2000
+	#define LSM6DSO_DEFAULT_GYRO_FULLSCALE		3
+	#define LSM6DSO_DEFAULT_GYRO_SENSITIVITY	(16.0 * GAIN_UNIT_G)
+#endif
+
+
+#if (CONFIG_LSM6DSO_GYRO_ODR == 0)
+#define LSM6DSO_GYRO_ODR_RUNTIME 1
+#endif
+
+struct lsm6dso_config {
+	char *bus_name;
+	int (*bus_init)(struct device *dev);
+#ifdef CONFIG_LSM6DSO_TRIGGER
+	const char *int_gpio_port;
+	u8_t int_gpio_pin;
+	u8_t int_pin;
+#endif /* CONFIG_LSM6DSO_TRIGGER */
+#ifdef DT_ST_LSM6DSO_BUS_I2C
+	u16_t i2c_slv_addr;
+#elif DT_ST_LSM6DSO_BUS_SPI
+	struct spi_config spi_conf;
+#if defined(DT_INST_0_ST_LSM6DSO_CS_GPIO_CONTROLLER)
+	const char *gpio_cs_port;
+	u8_t cs_gpio;
+#endif /* DT_INST_0_ST_LSM6DSO_CS_GPIO_CONTROLLER */
+#endif /* DT_ST_LSM6DSO_BUS_I2C */
+};
+
+union samples {
+	u8_t raw[6];
+	struct {
+		s16_t axis[3];
+	};
+} __aligned(2);
+
+/* sensor data forward declaration (member definition is below) */
+struct lsm6dso_data;
+
+struct lsm6dso_tf {
+	int (*read_data)(struct lsm6dso_data *data, u8_t reg_addr,
+			 u8_t *value, u8_t len);
+	int (*write_data)(struct lsm6dso_data *data, u8_t reg_addr,
+			  u8_t *value, u8_t len);
+	int (*read_reg)(struct lsm6dso_data *data, u8_t reg_addr,
+			u8_t *value);
+	int (*write_reg)(struct lsm6dso_data *data, u8_t reg_addr,
+			u8_t value);
+	int (*update_reg)(struct lsm6dso_data *data, u8_t reg_addr,
+			  u8_t mask, u8_t value);
+};
+
+#define LSM6DSO_SHUB_MAX_NUM_SLVS			2
+
+struct lsm6dso_data {
+	struct device *bus;
+	s16_t acc[3];
+	u32_t acc_gain;
+	s16_t gyro[3];
+	u32_t gyro_gain;
+#if defined(CONFIG_LSM6DSO_ENABLE_TEMP)
+	int temp_sample;
+#endif
+#if defined(CONFIG_LSM6DSO_SENSORHUB)
+	u8_t ext_data[2][6];
+	u16_t magn_gain;
+
+	struct hts221_data {
+		s16_t x0;
+		s16_t x1;
+		s16_t y0;
+		s16_t y1;
+	} hts221;
+#endif /* CONFIG_LSM6DSO_SENSORHUB */
+
+	lsm6dso_ctx_t *ctx;
+
+	#ifdef DT_ST_LSM6DSO_BUS_I2C
+	lsm6dso_ctx_t ctx_i2c;
+	#elif DT_ST_LSM6DSO_BUS_SPI
+	lsm6dso_ctx_t ctx_spi;
+	#endif
+
+	u16_t accel_freq;
+	u8_t accel_fs;
+	u16_t gyro_freq;
+	u8_t gyro_fs;
+
+#ifdef CONFIG_LSM6DSO_TRIGGER
+	struct device *gpio;
+	struct gpio_callback gpio_cb;
+	sensor_trigger_handler_t handler_drdy_acc;
+	sensor_trigger_handler_t handler_drdy_gyr;
+	sensor_trigger_handler_t handler_drdy_temp;
+
+#if defined(CONFIG_LSM6DSO_TRIGGER_OWN_THREAD)
+	K_THREAD_STACK_MEMBER(thread_stack, CONFIG_LSM6DSO_THREAD_STACK_SIZE);
+	struct k_thread thread;
+	struct k_sem gpio_sem;
+#elif defined(CONFIG_LSM6DSO_TRIGGER_GLOBAL_THREAD)
+	struct k_work work;
+	struct device *dev;
+#endif
+#endif /* CONFIG_LSM6DSO_TRIGGER */
+
+#if defined(DT_INST_0_ST_LSM6DSO_CS_GPIO_CONTROLLER)
+	struct spi_cs_control cs_ctrl;
+#endif
+};
+
+int lsm6dso_spi_init(struct device *dev);
+int lsm6dso_i2c_init(struct device *dev);
+#if defined(CONFIG_LSM6DSO_SENSORHUB)
+int lsm6dso_shub_init(struct device *dev);
+int lsm6dso_shub_fetch_external_devs(struct device *dev);
+int lsm6dso_shub_get_idx(enum sensor_channel type);
+int lsm6dso_shub_config(struct device *dev, enum sensor_channel chan,
+			enum sensor_attribute attr,
+			const struct sensor_value *val);
+#endif /* CONFIG_LSM6DSO_SENSORHUB */
+
+#ifdef CONFIG_LSM6DSO_TRIGGER
+int lsm6dso_trigger_set(struct device *dev,
+			const struct sensor_trigger *trig,
+			sensor_trigger_handler_t handler);
+
+int lsm6dso_init_interrupt(struct device *dev);
+#endif
+
+#endif /* ZEPHYR_DRIVERS_SENSOR_LSM6DSO_LSM6DSO_H_ */
---
+++ drivers/sensor/lsm6dso/lsm6dso_trigger.c
@@ -0,0 +1,300 @@
+/* ST Microelectronics LSM6DSO 6-axis IMU sensor driver
+ *
+ * Copyright (c) 2019 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Datasheet:
+ * https://www.st.com/resource/en/datasheet/lsm6dso.pdf
+ */
+
+#include <kernel.h>
+#include <sensor.h>
+#include <gpio.h>
+#include <logging/log.h>
+
+#include "lsm6dso.h"
+
+#define LOG_LEVEL CONFIG_SENSOR_LOG_LEVEL
+LOG_MODULE_DECLARE(LSM6DSO);
+
+#if defined(CONFIG_LSM6DSO_ENABLE_TEMP)
+/**
+ * lsm6dso_enable_t_int - TEMP enable selected int pin to generate interrupt
+ */
+static int lsm6dso_enable_t_int(struct device *dev, int enable)
+{
+	const struct lsm6dso_config *cfg = dev->config->config_info;
+	struct lsm6dso_data *lsm6dso = dev->driver_data;
+	lsm6dso_pin_int2_route_t int2_route;
+
+	if (enable) {
+		axis1bit16_t buf;
+
+		/* dummy read: re-trigger interrupt */
+		lsm6dso_temperature_raw_get(lsm6dso->ctx, buf.u8bit);
+	}
+
+	/* set interrupt (TEMP DRDY interrupt is only on INT2) */
+	if (cfg->int_pin == 1)
+		return -EIO;
+
+	lsm6dso_read_reg(lsm6dso->ctx, LSM6DSO_INT2_CTRL,
+			 (uint8_t *)&int2_route.int2_ctrl, 1);
+	int2_route.int2_ctrl.int2_drdy_temp = enable;
+	return lsm6dso_write_reg(lsm6dso->ctx, LSM6DSO_INT2_CTRL,
+				 (uint8_t *)&int2_route.int2_ctrl, 1);
+}
+#endif
+
+/**
+ * lsm6dso_enable_xl_int - XL enable selected int pin to generate interrupt
+ */
+static int lsm6dso_enable_xl_int(struct device *dev, int enable)
+{
+	const struct lsm6dso_config *cfg = dev->config->config_info;
+	struct lsm6dso_data *lsm6dso = dev->driver_data;
+
+	if (enable) {
+		axis3bit16_t buf;
+
+		/* dummy read: re-trigger interrupt */
+		lsm6dso_acceleration_raw_get(lsm6dso->ctx, buf.u8bit);
+	}
+
+	/* set interrupt */
+	if (cfg->int_pin == 1) {
+		lsm6dso_pin_int1_route_t int1_route;
+
+		lsm6dso_read_reg(lsm6dso->ctx, LSM6DSO_INT1_CTRL,
+				 (uint8_t *)&int1_route.int1_ctrl, 1);
+
+		int1_route.int1_ctrl.int1_drdy_xl = enable;
+		return lsm6dso_write_reg(lsm6dso->ctx, LSM6DSO_INT1_CTRL,
+					 (uint8_t *)&int1_route.int1_ctrl, 1);
+	} else {
+		lsm6dso_pin_int2_route_t int2_route;
+
+		lsm6dso_read_reg(lsm6dso->ctx, LSM6DSO_INT2_CTRL,
+				 (uint8_t *)&int2_route.int2_ctrl, 1);
+		int2_route.int2_ctrl.int2_drdy_xl = enable;
+		return lsm6dso_write_reg(lsm6dso->ctx, LSM6DSO_INT2_CTRL,
+					 (uint8_t *)&int2_route.int2_ctrl, 1);
+	}
+}
+
+/**
+ * lsm6dso_enable_g_int - Gyro enable selected int pin to generate interrupt
+ */
+static int lsm6dso_enable_g_int(struct device *dev, int enable)
+{
+	const struct lsm6dso_config *cfg = dev->config->config_info;
+	struct lsm6dso_data *lsm6dso = dev->driver_data;
+
+	if (enable) {
+		axis3bit16_t buf;
+
+		/* dummy read: re-trigger interrupt */
+		lsm6dso_angular_rate_raw_get(lsm6dso->ctx, buf.u8bit);
+	}
+
+	/* set interrupt */
+	if (cfg->int_pin == 1) {
+		lsm6dso_pin_int1_route_t int1_route;
+
+		lsm6dso_read_reg(lsm6dso->ctx, LSM6DSO_INT1_CTRL,
+				 (uint8_t *)&int1_route.int1_ctrl, 1);
+		int1_route.int1_ctrl.int1_drdy_g = enable;
+		return lsm6dso_write_reg(lsm6dso->ctx, LSM6DSO_INT1_CTRL,
+					 (uint8_t *)&int1_route.int1_ctrl, 1);
+	} else {
+		lsm6dso_pin_int2_route_t int2_route;
+
+		lsm6dso_read_reg(lsm6dso->ctx, LSM6DSO_INT2_CTRL,
+				 (uint8_t *)&int2_route.int2_ctrl, 1);
+		int2_route.int2_ctrl.int2_drdy_g = enable;
+		return lsm6dso_write_reg(lsm6dso->ctx, LSM6DSO_INT2_CTRL,
+					 (uint8_t *)&int2_route.int2_ctrl, 1);
+	}
+}
+
+/**
+ * lsm6dso_trigger_set - link external trigger to event data ready
+ */
+int lsm6dso_trigger_set(struct device *dev,
+			  const struct sensor_trigger *trig,
+			  sensor_trigger_handler_t handler)
+{
+	struct lsm6dso_data *lsm6dso = dev->driver_data;
+
+	if (trig->chan == SENSOR_CHAN_ACCEL_XYZ) {
+		lsm6dso->handler_drdy_acc = handler;
+		if (handler) {
+			return lsm6dso_enable_xl_int(dev, LSM6DSO_EN_BIT);
+		} else {
+			return lsm6dso_enable_xl_int(dev, LSM6DSO_DIS_BIT);
+		}
+	} else if (trig->chan == SENSOR_CHAN_GYRO_XYZ) {
+		lsm6dso->handler_drdy_gyr = handler;
+		if (handler) {
+			return lsm6dso_enable_g_int(dev, LSM6DSO_EN_BIT);
+		} else {
+			return lsm6dso_enable_g_int(dev, LSM6DSO_DIS_BIT);
+		}
+	}
+#if defined(CONFIG_LSM6DSO_ENABLE_TEMP)
+	else if (trig->chan == SENSOR_CHAN_DIE_TEMP) {
+		lsm6dso->handler_drdy_temp = handler;
+		if (handler) {
+			return lsm6dso_enable_t_int(dev, LSM6DSO_EN_BIT);
+		} else {
+			return lsm6dso_enable_t_int(dev, LSM6DSO_DIS_BIT);
+		}
+	}
+#endif
+
+	return -ENOTSUP;
+}
+
+/**
+ * lsm6dso_handle_interrupt - handle the drdy event
+ * read data and call handler if registered any
+ */
+static void lsm6dso_handle_interrupt(void *arg)
+{
+	struct device *dev = arg;
+	struct lsm6dso_data *lsm6dso = dev->driver_data;
+	struct sensor_trigger drdy_trigger = {
+		.type = SENSOR_TRIG_DATA_READY,
+	};
+	const struct lsm6dso_config *cfg = dev->config->config_info;
+	lsm6dso_status_reg_t status;
+
+	while (1) {
+		if (lsm6dso_status_reg_get(lsm6dso->ctx, &status) < 0) {
+			LOG_DBG("failed reading status reg");
+			return;
+		}
+
+		if ((status.xlda == 0) && (status.gda == 0)
+#if defined(CONFIG_LSM6DSO_ENABLE_TEMP)
+					&& (status.tda == 0)
+#endif
+					) {
+			break;
+		}
+
+		if ((status.xlda) && (lsm6dso->handler_drdy_acc != NULL)) {
+			lsm6dso->handler_drdy_acc(dev, &drdy_trigger);
+		}
+
+		if ((status.gda) && (lsm6dso->handler_drdy_gyr != NULL)) {
+			lsm6dso->handler_drdy_gyr(dev, &drdy_trigger);
+		}
+
+#if defined(CONFIG_LSM6DSO_ENABLE_TEMP)
+		if ((status.tda) && (lsm6dso->handler_drdy_temp != NULL)) {
+			lsm6dso->handler_drdy_temp(dev, &drdy_trigger);
+		}
+#endif
+	}
+
+	gpio_pin_enable_callback(lsm6dso->gpio, cfg->int_gpio_pin);
+}
+
+static void lsm6dso_gpio_callback(struct device *dev,
+				    struct gpio_callback *cb, u32_t pins)
+{
+	struct lsm6dso_data *lsm6dso =
+		CONTAINER_OF(cb, struct lsm6dso_data, gpio_cb);
+	const struct lsm6dso_config *cfg = dev->config->config_info;
+
+	ARG_UNUSED(pins);
+
+	gpio_pin_disable_callback(dev, cfg->int_gpio_pin);
+
+#if defined(CONFIG_LSM6DSO_TRIGGER_OWN_THREAD)
+	k_sem_give(&lsm6dso->gpio_sem);
+#elif defined(CONFIG_LSM6DSO_TRIGGER_GLOBAL_THREAD)
+	k_work_submit(&lsm6dso->work);
+#endif /* CONFIG_LSM6DSO_TRIGGER_OWN_THREAD */
+}
+
+#ifdef CONFIG_LSM6DSO_TRIGGER_OWN_THREAD
+static void lsm6dso_thread(int dev_ptr, int unused)
+{
+	struct device *dev = INT_TO_POINTER(dev_ptr);
+	struct lsm6dso_data *lsm6dso = dev->driver_data;
+
+	ARG_UNUSED(unused);
+
+	while (1) {
+		k_sem_take(&lsm6dso->gpio_sem, K_FOREVER);
+		lsm6dso_handle_interrupt(dev);
+	}
+}
+#endif /* CONFIG_LSM6DSO_TRIGGER_OWN_THREAD */
+
+#ifdef CONFIG_LSM6DSO_TRIGGER_GLOBAL_THREAD
+static void lsm6dso_work_cb(struct k_work *work)
+{
+	struct lsm6dso_data *lsm6dso =
+		CONTAINER_OF(work, struct lsm6dso_data, work);
+
+	lsm6dso_handle_interrupt(lsm6dso->dev);
+}
+#endif /* CONFIG_LSM6DSO_TRIGGER_GLOBAL_THREAD */
+
+int lsm6dso_init_interrupt(struct device *dev)
+{
+	struct lsm6dso_data *lsm6dso = dev->driver_data;
+	const struct lsm6dso_config *cfg = dev->config->config_info;
+	int ret;
+
+	/* setup data ready gpio interrupt (INT1 or INT2) */
+	lsm6dso->gpio = device_get_binding(cfg->int_gpio_port);
+	if (lsm6dso->gpio == NULL) {
+		LOG_DBG("Cannot get pointer to %s device",
+			    cfg->int_gpio_port);
+		return -EINVAL;
+	}
+
+#if defined(CONFIG_LSM6DSO_TRIGGER_OWN_THREAD)
+	k_sem_init(&lsm6dso->gpio_sem, 0, UINT_MAX);
+
+	k_thread_create(&lsm6dso->thread, lsm6dso->thread_stack,
+			CONFIG_LSM6DSO_THREAD_STACK_SIZE,
+			(k_thread_entry_t)lsm6dso_thread, dev,
+			0, NULL, K_PRIO_COOP(CONFIG_LSM6DSO_THREAD_PRIORITY),
+			0, 0);
+#elif defined(CONFIG_LSM6DSO_TRIGGER_GLOBAL_THREAD)
+	lsm6dso->work.handler = lsm6dso_work_cb;
+	lsm6dso->dev = dev;
+#endif /* CONFIG_LSM6DSO_TRIGGER_OWN_THREAD */
+
+	ret = gpio_pin_configure(lsm6dso->gpio, cfg->int_gpio_pin,
+				 GPIO_DIR_IN | GPIO_INT | GPIO_INT_EDGE |
+				 GPIO_INT_ACTIVE_HIGH | GPIO_INT_DEBOUNCE);
+	if (ret < 0) {
+		LOG_DBG("Could not configure gpio");
+		return ret;
+	}
+
+	gpio_init_callback(&lsm6dso->gpio_cb,
+			   lsm6dso_gpio_callback,
+			   BIT(cfg->int_gpio_pin));
+
+	if (gpio_add_callback(lsm6dso->gpio, &lsm6dso->gpio_cb) < 0) {
+		LOG_DBG("Could not set gpio callback");
+		return -EIO;
+	}
+
+	/* enable interrupt on int1/int2 in pulse mode */
+	if (lsm6dso_int_notification_set(lsm6dso->ctx,
+					 LSM6DSO_ALL_INT_PULSED) < 0) {
+		LOG_DBG("Could not set pulse mode");
+		return -EIO;
+	}
+
+	return gpio_pin_enable_callback(lsm6dso->gpio, cfg->int_gpio_pin);
+}
