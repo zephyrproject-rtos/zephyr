246a15c4db055edf6effb192442b7e3ac8e5932f
Ulf Magnusson <Ulf.Magnusson@nordicsemi.no>
2019-02-21 17:11:41 +0100

    scripts/dts: Never ignore properties in generate_node_defines()
    If a binding specifies 'generate: define' for 'interrupt-names' (like
    some bindings do), then that ought to generate #defines for it, but the
    scripts/dts code currently hardcodes 'interrupt-names' to be ignored
    (along with some other properties).
    
    Maybe the 'generate: define' in those bindings is a mistake, but the
    code still ought to respect it. That also gets rid of some mystery code.
    
    Signed-off-by: Ulf Magnusson <Ulf.Magnusson@nordicsemi.no>

--- scripts/dts/extract_dts_includes.py
+++ scripts/dts/extract_dts_includes.py
@@ -123,10 +123,6 @@ def generate_node_defines(node_path):
         # patterns for things like reg, interrupts, etc that we don't need
         # any special case handling at a node level
         for prop in reduced[node_path]['props']:
-            if prop in {'interrupt-names', 'reg-names', 'phandle',
-                        'linux,phandle'}:
-                continue
-
             if re.fullmatch(yaml_prop, prop):
                 match = True
                 generate_prop_defines(node_path, prop)
