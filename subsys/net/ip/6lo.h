0bb8a1336003add0003441a22bafdaad1edec1ff
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2019-02-21 19:38:11 +0100

    net/6lo: Add a way to get the difference of hdr size after uncompression
    Since we don't want to redo the complex calculation, let's reuse the
    uncompress functions, adding a dry_run possibility to those.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- subsys/net/ip/6lo.h
+++ subsys/net/ip/6lo.h
@@ -78,4 +78,18 @@ void net_6lo_set_context(struct net_if *iface,
 			 struct net_icmpv6_nd_opt_6co *context);
 #endif
 
+/**
+ *  @brief Return the header size difference after uncompression
+ *
+ * @details This will do a dry-run on uncompressing the headers.
+ *          The point is only to calculate the difference.
+ *
+ * @param Pointer to network packet
+ *
+ * @return header difference or INT_MAX in case of error.
+ */
+#if defined(CONFIG_NET_6LO)
+int net_6lo_uncompress_hdr_diff(struct net_pkt *pkt);
+#endif
+
 #endif /* __NET_6LO_H */
