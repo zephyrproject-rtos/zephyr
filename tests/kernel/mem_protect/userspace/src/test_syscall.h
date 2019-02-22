c612522a9647216b6749b2afb3a4da4b30d8421e
Andrew Boie <andrew.p.boie@intel.com>
2019-02-21 16:00:29 -0800

    tests: userspace: check stack buffer access
    The stack information stored in the thread->stack_info
    fields need to represent the actual writable area for
    its associated thread. Perform various tests to ensure
    that the various reported and specified values are in
    agreement.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

---
+++ tests/kernel/mem_protect/userspace/src/test_syscall.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef USERSPACE_TEST_SYSCALL_H
+#define USERSPACE_TEST_SYSCALL_H
+
+__syscall void stack_info_get(u32_t *start_addr, u32_t *size);
+__syscall int check_perms(void *addr, size_t size, int write);
+
+#include <syscalls/test_syscall.h>
+
+#endif
