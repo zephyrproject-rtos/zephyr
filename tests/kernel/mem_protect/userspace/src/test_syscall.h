e610d2befa7db5d330e266cbd8a84d292118daa4
Andrew Boie <andrew.p.boie@intel.com>
2019-02-21 16:00:29 -0800

    tests: userspace: check stack buffer access
    The stack information stored in the thread->stack_info
    fields need to represent the actual writable area for
    its associated thread.
    
    Check that the starting location agrees with
    K_THREAD_STACK_BUFFER() on the stack object, and that
    a user thread can read/write all the memory locations
    within it.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

---
+++ tests/kernel/mem_protect/userspace/src/test_syscall.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2019 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef USERSPACE_TEST_SYSCALL_H
+#define USERSPACE_TEST_SYSCALL_H
+
+__syscall void stack_info_get(u32_t *start_addr, u32_t *size);
+__syscall int check_perms(void *addr, size_t size, int write);
+__syscall void *get_tls_pointer(void);
+
+#include <syscalls/test_syscall.h>
+
+#endif
