95f4556bc1231bae3bf0c6766ffd495fceb96058
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2019-02-22 09:11:15 +0100

    arch: arm: fix return reason in Secure/Non-Secure FW builds
    This commit fixes a bug that was always propagating the
    default error reason (_NANO_ERR_HW_EXCEPTION) to the
    system fatal error handler for (strictly) Secure and
    for Non-Secure FW builds on ARMv8-M.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- arch/arm/core/fault.c
+++ arch/arm/core/fault.c
@@ -670,7 +670,7 @@ static void _SecureStackDump(const NANO_ESF *secure_esf)
  */
 void _Fault(NANO_ESF *esf, u32_t exc_return)
 {
-	u32_t reason;
+	u32_t reason = _NANO_ERR_HW_EXCEPTION;
 	int fault = SCB->ICSR & SCB_ICSR_VECTACTIVE_Msk;
 
 	LOG_PANIC();
@@ -753,7 +753,6 @@ void _Fault(NANO_ESF *esf, u32_t exc_return)
 #if defined(CONFIG_ARM_SECURE_FIRMWARE) || \
 	defined(CONFIG_ARM_NONSECURE_FIRMWARE)
 _exit_fatal:
-	reason = _NANO_ERR_HW_EXCEPTION;
 #endif
 	_NanoFatalErrorHandler(reason, esf);
 }
