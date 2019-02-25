8b557ad723e2bf2caac5090eb70ee7644a9ca491
Erwan Gouriou <erwan.gouriou@linaro.org>
2019-02-25 14:54:45 +0100

    dts/arm/st: Add serial nodes on stm32wb
    Add USART1 and LPUART1 nodes on stm32wb series.
    Only these 2 ones are available for now on this series.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- soc/arm/st_stm32/stm32wb/soc.h
+++ soc/arm/st_stm32/stm32wb/soc.h
@@ -31,6 +31,11 @@
 #include <stm32wbxx_ll_gpio.h>
 #endif
 
+#ifdef CONFIG_SERIAL_HAS_DRIVER
+#include <stm32wbxx_ll_usart.h>
+#include <stm32wbxx_ll_lpuart.h>
+#endif
+
 #ifdef CONFIG_CLOCK_CONTROL_STM32_CUBE
 #include <stm32wbxx_ll_utils.h>
 #include <stm32wbxx_ll_bus.h>
