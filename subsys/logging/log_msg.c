e64cd5c74d3ccbb1b660e374b70a8b13e917ea96
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2019-02-26 14:33:47 +0100

    logging: Fix silent logs dropping when LOG_MODE_OVERFLOW disabled
    There was no notification about dropped logs When logger operated
    in the mode where message is dropped when logger has no space to
    store the message. Notification was printed only if logger operated
    in the mode which overwrites oldest log.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

--- subsys/logging/log_msg.c
+++ subsys/logging/log_msg.c
@@ -74,10 +74,13 @@ union log_msg_chunk *log_msg_no_space_handle(void)
 	if (IS_ENABLED(CONFIG_LOG_MODE_OVERFLOW)) {
 		do {
 			more = log_process(true);
+			log_dropped();
 			err = k_mem_slab_alloc(&log_msg_pool,
 					       (void **)&msg,
 					       K_NO_WAIT);
 		} while ((err != 0) && more);
+	} else {
+		log_dropped();
 	}
 	return msg;
 
