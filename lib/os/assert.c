728360c2d70a22781f98976484364dd31274bb62
Tim Sørensen (TIMS) <tims@oticon.com>
2019-02-27 16:26:50 +0100

    misc: assert_post_action: Added __weak keyword.
    Added __weak keyword to to support overriding assert_post_action().
    This allows system designers to change/augment the assert behaviour,
    i.e. add logging to persistant storage of program counter, line
    number etc, and/or change reboot behaviour.
    
    Signed-off-by: Tim Sørensen (TIMS) <tims@oticon.com>

--- lib/os/assert.c
+++ lib/os/assert.c
@@ -7,7 +7,25 @@
 #include <misc/__assert.h>
 #include <zephyr.h>
 
-void assert_post_action(void)
+
+/**
+ *
+ * @brief Assert Action Handler
+ *
+ * This routine implements the action to be taken when an assertion fails.
+ *
+ * System designers may wish to substitute this implementation to take other
+ * actions, such as logging program counter, line number, debug information
+ * to a persistent repository and/or rebooting the system.
+ *
+ * @param N/A
+ *
+ * @return N/A
+ */
+__weak void assert_post_action(const char *file, unsigned int line)
 {
-	k_panic();
+  ARG_UNUSED(file);
+  ARG_UNUSED(line);
+
+  k_panic();
 }
