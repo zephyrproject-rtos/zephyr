d4391186a648ae3f5741417c638f2350e7b18093
Andrew Boie <andrew.p.boie@intel.com>
2019-02-27 14:41:45 -0800

    mbedtls: provide user mode access
    The mbedtls library has some globals which results in faults
    when user mode tries to access them.
    
    Instantiate a memory partition for mbedtls's globals.
    The linker will place all globals found by building this
    library into this partition.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- tests/crypto/mbedtls/src/main.c
+++ tests/crypto/mbedtls/src/main.c
@@ -5,12 +5,17 @@
  */
 
 #include <ztest.h>
+#include <app_memory/partitions.h>
+
 extern void test_mbedtls(void);
 
 /**test case main entry*/
 void test_main(void)
 {
+#ifdef CONFIG_USERSPACE
+	k_mem_domain_add_partition(&ztest_mem_domain, &k_mbedtls_partition);
+#endif
 	ztest_test_suite(test_mbedtls_fn,
-		ztest_unit_test(test_mbedtls));
+		ztest_user_unit_test(test_mbedtls));
 	ztest_run_test_suite(test_mbedtls_fn);
 }
