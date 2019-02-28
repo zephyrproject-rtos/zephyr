18680e55e507984e34c63d26d5bfdd006675d8e2
Alexander Wachter <alexander.wachter@student.tugraz.at>
2019-02-28 10:57:23 +0100

    drivers: can: Fix userspace handler parameter types
    Change the parameter types from can_msg to zcan_frame and from
    can_filter to zcan_filter.
    
    Signed-off-by: Alexander Wachter <alexander.wachter@student.tugraz.at>

--- drivers/can/can_handlers.c
+++ drivers/can/can_handlers.c
@@ -19,12 +19,13 @@ Z_SYSCALL_HANDLER(can_send, dev, msg, timeout, callback_isr) {
 
 	Z_OOPS(Z_SYSCALL_DRIVER_CAN(dev, send));
 
-	Z_OOPS(Z_SYSCALL_MEMORY_READ((struct can_msg *)msg,
-				      sizeof(struct can_msg)));
-	Z_OOPS(Z_SYSCALL_MEMORY_READ(((struct can_msg *)msg)->data,
-				     sizeof((struct can_msg *)msg)->data));
+	Z_OOPS(Z_SYSCALL_MEMORY_READ((const struct zcan_frame *)msg,
+				      sizeof(struct zcan_frame)));
+	Z_OOPS(Z_SYSCALL_MEMORY_READ(((struct zcan_frame *)msg)->data,
+				     sizeof((struct zcan_frame *)msg)->data));
 
-	return _impl_can_send((struct device *)dev, (struct can_msg *)msg,
+	return _impl_can_send((struct device *)dev,
+			      (const struct zcan_frame *)msg,
 			      (s32_t)timeout, (can_tx_callback_t) callback_isr);
 }
 
@@ -32,8 +33,8 @@ Z_SYSCALL_HANDLER(can_attach_msgq, dev, msgq, filter) {
 
 	Z_OOPS(Z_SYSCALL_DRIVER_CAN(dev, attach_msgq));
 
-	Z_OOPS(Z_SYSCALL_MEMORY_READ((struct can_filter *)filter,
-				     sizeof(struct can_filter)));
+	Z_OOPS(Z_SYSCALL_MEMORY_READ((struct zcan_filter *)filter,
+				     sizeof(struct zcan_filter)));
 	Z_OOPS(Z_SYSCALL_MEMORY_WRITE((struct k_msgq *)msgq,
 				      sizeof(struct k_msgq)));
 	Z_OOPS(Z_SYSCALL_MEMORY_WRITE(((struct k_msgq *)msgq)->buffer_start,
@@ -42,19 +43,19 @@ Z_SYSCALL_HANDLER(can_attach_msgq, dev, msgq, filter) {
 
 	return _impl_can_attach_msgq((struct device *)dev,
 				     (struct k_msgq *)msgq,
-				     (const struct can_filter *) filter);
+				     (const struct zcan_filter *) filter);
 }
 
 Z_SYSCALL_HANDLER(can_attach_isr, dev, isr, filter) {
 
 	Z_OOPS(Z_SYSCALL_DRIVER_CAN(dev, attach_isr));
 
-	Z_OOPS(Z_SYSCALL_MEMORY_READ((struct can_filter *)filter,
-				     sizeof(struct can_filter)));
+	Z_OOPS(Z_SYSCALL_MEMORY_READ((struct zcan_filter *)filter,
+				     sizeof(struct zcan_filter)));
 
 	return _impl_can_attach_isr((struct device *)dev,
 				    (can_rx_callback_t)isr,
-				    (const struct can_filter *) filter);
+				    (const struct zcan_filter *) filter);
 }
 
 Z_SYSCALL_HANDLER(can_detach, dev, filter_id) {
