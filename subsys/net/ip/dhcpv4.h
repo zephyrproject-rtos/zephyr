ecfe1fc7e388739c7cfef54572e68b7f5ca790ff
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2019-02-28 15:43:41 +0200

    net: dhcpv4: Introduce config option for max delay
    As per RFC2131 4.1.1 requires we wait a random period
    between 1 and 10 seconds before sending the initial
    discover. But tests can not wait that longer. So this
    option helps test to configure the value to minimum.
    
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>

--- subsys/net/ip/dhcpv4.h
+++ subsys/net/ip/dhcpv4.h
@@ -110,12 +110,13 @@ enum dhcpv4_msg_type {
  */
 #define DHCPV4_INITIAL_RETRY_TIMEOUT 4
 
-/* Initial minimum and maximum delay in INIT state before sending the
- * initial DISCOVER message.
+/* Initial minimum delay in INIT state before sending the
+ * initial DISCOVER message. MAx value is defined with
+ * CONFIG_NET_DHCPV4_INITIAL_DELAY_MAX. Default max value
+ * should be 10.
  * RFC2131 4.1.1
  */
 #define DHCPV4_INITIAL_DELAY_MIN 1
-#define DHCPV4_INITIAL_DELAY_MAX 10
 
 int net_dhcpv4_init(void);
 
