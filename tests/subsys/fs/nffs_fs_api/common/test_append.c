b92f104b66fb963f87412ed1d5201db7fa44fa6e
Flavio Ceolin <flavio.ceolin@intel.com>
2019-02-28 16:54:15 -0800

    tests: fs: Add missing return check
    Checking the return of fs_stat to ensure that there is not hidden error.
    Problem spotted by coverity.
    
    CID 190949
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- tests/subsys/fs/nffs_fs_api/common/test_append.c
+++ tests/subsys/fs/nffs_fs_api/common/test_append.c
@@ -95,7 +95,8 @@ void test_append(void)
 
 	/*** Repeated appends to a large file. */
 	for (i = 0; i < 1000; i++) {
-		fs_stat(NFFS_MNTP"/mydir/gaga.txt", &info);
+		rc = fs_stat(NFFS_MNTP"/mydir/gaga.txt", &info);
+		zassert_equal(rc, 0, "fs_stat returned error");
 		zassert_equal(info.size, i, "file lengths not matching");
 
 		c = '0' + i % 10;
