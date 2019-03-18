96ccb913e45f3a5bef9c08b078ae7a48277f3444
Daniel Leung <daniel.leung@intel.com>
2019-03-18 10:26:31 -0700

    tests: kernel/smp: fixed missing 'z_' renaming
    There was a missing 'z_' renaming to
    z_is_thread_prevented_from_running which would have caused
    sanitycheck to fail but it is not being built at the moment.
    Fix this first.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- tests/kernel/smp/src/main.c
+++ tests/kernel/smp/src/main.c
@@ -372,7 +372,7 @@ static void wakeup_on_start_thread(int tnum)
 	for (i = 0; i < tnum; i++) {
 		while (thread_started[i] == 0) {
 		}
-		while (!_is_thread_prevented_from_running(tinfo[i].tid)) {
+		while (!z_is_thread_prevented_from_running(tinfo[i].tid)) {
 		}
 	}
 
