3cc45d512d53453d320f27a44a09d474f15efc6e
Henrik Brix Andersen <henrik@brixandersen.dk>
2019-03-24 20:24:31 +0100

    samples: drivers: ht16k33: add sample application for the HT16K33
    Add a sample application for showcasing the functionality of the
    Holtek HT16K33 LED driver with keyscan functionality.
    
    Signed-off-by: Henrik Brix Andersen <henrik@brixandersen.dk>

---
+++ samples/drivers/ht16k33/src/main.c
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2019 Henrik Brix Andersen <henrik@brixandersen.dk>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <device.h>
+#include <led.h>
+#include <gpio.h>
+#include <zephyr.h>
+
+#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <logging/log.h>
+LOG_MODULE_REGISTER(main);
+
+#define LED_DEV_NAME DT_HOLTEK_HT16K33_0_LABEL
+#define KS0_DEV_NAME DT_HOLTEK_HT16K33_KEYSCAN_0_LABEL
+#define KS1_DEV_NAME DT_HOLTEK_HT16K33_KEYSCAN_1_LABEL
+#define KS2_DEV_NAME DT_HOLTEK_HT16K33_KEYSCAN_2_LABEL
+
+#define KEYSCAN_DEVICES 3
+
+struct device *led_dev;
+struct device *ks_dev[KEYSCAN_DEVICES];
+static struct gpio_callback ks_cb[KEYSCAN_DEVICES];
+
+static void keyscan_callback(struct device *gpiob,
+			     struct gpio_callback *cb, u32_t pins)
+{
+	LOG_INF("%s: 0x%08x", gpiob->config->name, pins);
+}
+
+void main(void)
+{
+	int err;
+	int i;
+
+	/* LED device binding */
+	led_dev = device_get_binding(LED_DEV_NAME);
+	if (!led_dev) {
+		LOG_ERR("LED device %s not found", LED_DEV_NAME);
+		return;
+	}
+
+	/* Keyscan device bindings */
+	ks_dev[0] = device_get_binding(KS0_DEV_NAME);
+	ks_dev[1] = device_get_binding(KS1_DEV_NAME);
+	ks_dev[2] = device_get_binding(KS2_DEV_NAME);
+	for (i = 0; i < ARRAY_SIZE(ks_dev); i++) {
+		if (!ks_dev[i]) {
+			LOG_ERR("KS%d device not found", i);
+			return;
+		}
+
+		gpio_init_callback(&ks_cb[i], &keyscan_callback,
+				   GENMASK(12, 0));
+
+		err = gpio_add_callback(ks_dev[i], &ks_cb[i]);
+		if (err) {
+			LOG_ERR("Failed to add KS%d GPIO callback (err %d)", i,
+				err);
+			return;
+		}
+	}
+
+	while (1) {
+		LOG_INF("Iterating through all LEDs, turning them on "
+			"one-by-one");
+		for (i = 0; i < 128; i++) {
+			led_on(led_dev, i);
+			k_sleep(100);
+		}
+
+		for (i = 500; i <= 2000; i *= 2) {
+			LOG_INF("Blinking LEDs with a period of %d ms", i);
+			led_blink(led_dev, 0, i / 2, i / 2);
+			k_sleep(10 * i);
+		}
+		led_blink(led_dev, 0, 0, 0);
+
+		for (i = 100; i >= 0; i -= 10) {
+			LOG_INF("Setting LED brightness to %d%%", i);
+			led_set_brightness(led_dev, 0, i);
+			k_sleep(1000);
+		}
+
+		LOG_INF("Turning all LEDs off and restoring 100%% brightness");
+		for (i = 0; i < 128; i++) {
+			led_off(led_dev, i);
+		}
+		led_set_brightness(led_dev, 0, 100);
+	}
+}
