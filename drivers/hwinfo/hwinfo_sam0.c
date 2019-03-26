37f181245e10d3337a2ffe9dbd4f1bca4bfa3aac
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2019-03-26 14:17:48 +0100

    drivers: hwinfo: add driver support for Atmel SAM0 device ID
    Add driver support for Atmel SAM0 device ID, which is 16-bytes long.
    The device ID can simply be read from memory at a known location, but
    the location is only described in the data sheet, not in ASF.
    
    For SAMD2x it's 0x0080A00C, 0x0080A040, 0x0080A044 & 0x0080A048.
    For SAMD5x it's 0x008061FC, 0x00806010, 0x00806014 & 0x00806018.
    
    This adds a new property to the device tree to define the device ID
    registers for this SoC family.
    
    Signed-off-by: Benjamin Valentin <benjamin.valentin@ml-pa.com>

---
+++ drivers/hwinfo/hwinfo_sam0.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2019 ML!PA Consulting GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <soc.h>
+#include <hwinfo.h>
+#include <string.h>
+
+struct sam0_uid {
+	u32_t id[4];
+};
+
+ssize_t z_impl_hwinfo_get_device_id(u8_t *buffer, size_t length)
+{
+	struct sam0_uid dev_id;
+
+	dev_id.id[0] = *(const u32_t *) DT_ATMEL_SAM0_ID_0_BASE_ADDRESS_0;
+	dev_id.id[1] = *(const u32_t *) DT_ATMEL_SAM0_ID_0_BASE_ADDRESS_1;
+	dev_id.id[2] = *(const u32_t *) DT_ATMEL_SAM0_ID_0_BASE_ADDRESS_2;
+	dev_id.id[3] = *(const u32_t *) DT_ATMEL_SAM0_ID_0_BASE_ADDRESS_3;
+
+	if (length > sizeof(dev_id.id)) {
+		length = sizeof(dev_id.id);
+	}
+
+	memcpy(buffer, dev_id.id, length);
+
+	return length;
+}
