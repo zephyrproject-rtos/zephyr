6b3e24c009c7d75f5872a32aaf4cec8a74b7fc7f
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2019-03-27 09:40:42 +0200

    usb: usb_dc_kinetis: Fix endpoint index check
    Fix check for maximum endpoint index.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- drivers/usb/device/usb_dc_kinetis.c
+++ drivers/usb/device/usb_dc_kinetis.c
@@ -281,7 +281,7 @@ int usb_dc_ep_check_cap(const struct usb_dc_ep_cfg_data * const cfg)
 		return -1;
 	}
 
-	if (ep_idx > NUM_OF_EP_MAX) {
+	if (ep_idx > (NUM_OF_EP_MAX - 1)) {
 		LOG_ERR("endpoint index/address out of range");
 		return -1;
 	}
