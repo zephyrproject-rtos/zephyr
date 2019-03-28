7be80cb207ed5c30d9987d936157a469ee38ed88
Kumar Gala <kumar.gala@linaro.org>
2019-03-28 09:31:33 -0500

    cmake: Make git describe handle non-annotated tags
    If we tag via github a pre-release we don't get an annotated tag.  So
    the BUILD_VERSION isn't reported correctly.  We can fix this via adding
    --tags to 'git describe'.
    
    Fixes #14985
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- cmake/git.cmake
+++ cmake/git.cmake
@@ -4,7 +4,7 @@
 find_package(Git QUIET)
 if(GIT_FOUND)
   execute_process(
-    COMMAND ${GIT_EXECUTABLE} describe --abbrev=12
+    COMMAND ${GIT_EXECUTABLE} describe --abbrev=12 --tags
     WORKING_DIRECTORY                ${ZEPHYR_BASE}
     OUTPUT_VARIABLE                  BUILD_VERSION
     OUTPUT_STRIP_TRAILING_WHITESPACE
