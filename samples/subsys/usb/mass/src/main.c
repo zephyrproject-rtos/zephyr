323756a2b954c8e59fe2f3445b14a26e059d0c8b
Marcin Szymczyk <Marcin.Szymczyk@nordicsemi.no>
2019-03-28 12:13:16 +0100

    usb: samples: mass: add configuration and fs for FLASH
    Adds required Kconfig options for flash driver.
    Adds FatFS filesystem for flash disk.
    Fixes #14459.
    
    Note: As USB MSC does not support multiple disks (see #14937),
    only one (flash by default, modify CONFIG_MASS_STORAGE_DISK_NAME
    to "RAM" for RAM disk) will appear to host.
    
    Signed-off-by: Marcin Szymczyk <Marcin.Szymczyk@nordicsemi.no>

--- samples/subsys/usb/mass/src/main.c
+++ samples/subsys/usb/mass/src/main.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016 Intel Corporation.
+ * Copyright (c) 2019 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -10,11 +11,33 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(main);
 
+#if CONFIG_DISK_ACCESS_FLASH && CONFIG_FAT_FILESYSTEM_ELM
+#include <fs.h>
+#include <ff.h>
+
+static FATFS fat_fs;
+#define FATFS_MNTP	"/NAND:"
+
+static struct fs_mount_t fatfs_mnt = {
+	.type = FS_FATFS,
+	.mnt_point = FATFS_MNTP,
+	.fs_data = &fat_fs,
+};
+#endif
+
 void main(void)
 {
 	/* Nothing to be done other than the selecting appropriate build
 	 * config options. Everything is driven from the USB host side.
 	 */
 	LOG_INF("The device is put in USB mass storage mode.\n");
+
+#if CONFIG_DISK_ACCESS_FLASH && CONFIG_FAT_FILESYSTEM_ELM
+	int res = fs_mount(&fatfs_mnt);
+
+	if (res < 0) {
+		LOG_ERR("Mount failed.");
+	}
+#endif
 }
 
