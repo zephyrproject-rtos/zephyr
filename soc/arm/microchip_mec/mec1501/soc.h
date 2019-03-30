02c82518fe99a37819f09c47b3b0576287c12660
Scott Worley <scott.worley@microchip.com>
2019-03-30 15:18:49 -0400

    arch : arm : Add Microchip MEC1501 SoC
    Intial support for Microchip MEC1501 series is incorporated to the
    tree. Additional support for UART is also included. This SoC supports
    two operational modes for interrupts (Direct and Aggregated). For this
    commit, the direct capable interrupts are configured in direct mode.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

---
+++ soc/arm/microchip_mec/mec1501/soc.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __MEC_SOC_H
+#define __MEC_SOC_H
+
+#define SYSCLK_DEFAULT_IOSC_HZ MHZ(48)
+
+#ifndef _ASMLANGUAGE
+
+#include "MEC1501hsz.h"
+#include "regaccess.h"
+
+
+#endif
+
+#endif
