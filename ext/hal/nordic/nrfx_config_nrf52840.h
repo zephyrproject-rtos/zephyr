560b458a21bbf898c7402c8561e5c3aa9193333a
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2019-04-01 17:26:05 +0200

    drives/flash: use nrfx for nrf
    Introduce nrfx_nvmc driver into nordic flash driver implementation.
    Thanks to that nrf9160 SoC becomes supported by the driver.
    
    nrfx helps dealing with differences with interface to the NVMC
    in secure and non-secure execution modes.
    
    This patch adds NRFX_NVMC Kconfig entry for enabling nrfx_nvmc and
    select it along with nordic flash driver.
    
    Disabled UICR operation on nRF9160 for non-secure build
    as UICR is not available in this mode.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- ext/hal/nordic/nrfx_config_nrf52840.h
+++ ext/hal/nordic/nrfx_config_nrf52840.h
@@ -743,8 +743,8 @@
 
 // <e> NRFX_NVMC_ENABLED - nrfx_nvmc - NVMC peripheral driver
 //==========================================================
-#ifndef NRFX_NVMC_ENABLED
-#define NRFX_NVMC_ENABLED 0
+#ifdef CONFIG_NRFX_NVMC
+#define NRFX_NVMC_ENABLED 1
 #endif
 
 // </e>
