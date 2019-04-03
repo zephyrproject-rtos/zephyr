d422d566336ae978fab6fe71c26af5b836a2d636
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2019-04-03 14:57:22 +0200

    samples/drivers/soc_flash_nrf: enable nrf9160_pca10090 targets
    The patch provides flash offsets appropriate for running
    this sample on nrf9160_pac10090 targets.
    As that sample is small one, it is done with assumption that image_1
    flash partition is empty.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- samples/drivers/soc_flash_nrf/src/main.c
+++ samples/drivers/soc_flash_nrf/src/main.c
@@ -10,8 +10,13 @@
 #include <device.h>
 #include <stdio.h>
 
-/* Offset between pages */
-#define FLASH_TEST_OFFSET 0x40000
+
+#ifdef CONFIG_TRUSTED_EXECUTION_NONSECURE
+#define FLASH_TEST_OFFSET DT_FLASH_AREA_IMAGE_1_NONSECURE_OFFSET
+#else
+#define FLASH_TEST_OFFSET DT_FLASH_AREA_IMAGE_1_OFFSET
+#endif
+
 #define FLASH_PAGE_SIZE   4096
 #define TEST_DATA_WORD_0  0x1122
 #define TEST_DATA_WORD_1  0xaabb
