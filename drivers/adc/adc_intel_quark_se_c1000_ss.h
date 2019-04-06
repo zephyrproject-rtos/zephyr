14bf8be8b8b201a6da182fb4410a4fdabae0f2a8
Anas Nashif <anas.nashif@intel.com>
2019-04-06 08:44:00 -0400

    drivers: adc: fix license
    This is a leftover license header from when we used a third party
    driver that had since been replaced.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- drivers/adc/adc_intel_quark_se_c1000_ss.h
+++ drivers/adc/adc_intel_quark_se_c1000_ss.h
@@ -1,32 +1,8 @@
 /**
- * Synopsys DesignWare Sensor and Control IP Subsystem IO Software Driver and
- * documentation (hereinafter, "Software") is an Unsupported proprietary work
- * of Synopsys, Inc. unless otherwise expressly agreed to in writing between
- * Synopsys and you.
+ * Copyright (c) 2015-2019 Intel Corporation
  *
- * The Software IS NOT an item of Licensed Software or Licensed Product under
- * any End User Software License Agreement or Agreement for Licensed Product
- * with Synopsys or any supplement thereto. You are permitted to use and
- * redistribute this Software in source and binary forms, with or without
- * modification, provided that redistributions of source code must retain this
- * notice. You may not view, use, disclose, copy or distribute this file or
- * any information contained herein except pursuant to this license grant from
- * Synopsys. If you do not agree with this notice, including the disclaimer
- * below, then you are not authorized to use the Software.
- *
- * THIS SOFTWARE IS BEING DISTRIBUTED BY SYNOPSYS SOLELY ON AN "AS IS" BASIS
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE HEREBY DISCLAIMED. IN NO EVENT SHALL SYNOPSYS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
- * DAMAGE.
+ * SPDX-License-Identifier: Apache-2.0
  */
-
 /**
  * @file
  *
