97ca7095625c2cd25aa53c4c5740e439e5c25fdb
Anas Nashif <anas.nashif@intel.com>
2019-04-07 09:00:19 -0400

    net: telnet: remove obsolete code
    Remove obsolote telnet console code. We now have a proper shell backend
    for telnet.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- subsys/console/line_fifo.c
+++ subsys/console/line_fifo.c
@@ -15,9 +15,6 @@
 #ifdef CONFIG_UART_CONSOLE
 #include <drivers/console/uart_console.h>
 #endif
-#ifdef CONFIG_TELNET_CONSOLE
-#include <drivers/console/telnet_console.h>
-#endif
 #ifdef CONFIG_NATIVE_POSIX_CONSOLE
 #include <drivers/console/native_posix_console.h>
 #endif
@@ -29,9 +26,6 @@ void console_register_line_input(struct k_fifo *avail_queue,
 	/* Register serial console handler */
 #ifdef CONFIG_UART_CONSOLE
 	uart_register_input(avail_queue, out_queue, completion);
-#endif
-#ifdef CONFIG_TELNET_CONSOLE
-	telnet_register_input(avail_queue, out_queue, completion);
 #endif
 #ifdef CONFIG_NATIVE_POSIX_STDIN_CONSOLE
 	native_stdin_register_input(avail_queue, out_queue, completion);
