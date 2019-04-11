15411650917f5d324fbb31f6f6deec5524c2ba12
Piotr Zięcik <piotr.ziecik@nordicsemi.no>
2019-04-11 14:28:52 +0200

    soc: k6x: Get CPU clock frequency from DTS
    The SoC initialization code used system clock frequency
    as a CPU clock frequency. This commit corrects that by
    obtaining the needed value from DTS.
    
    Signed-off-by: Piotr Zięcik <piotr.ziecik@nordicsemi.no>

--- soc/arm/nxp_kinetis/k6x/soc.c
+++ soc/arm/nxp_kinetis/k6x/soc.c
@@ -138,7 +138,7 @@ static ALWAYS_INLINE void clkInit(void)
 #endif
 #if CONFIG_USB_KINETIS
 	CLOCK_EnableUsbfs0Clock(kCLOCK_UsbSrcPll0,
-				CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC);
+				DT_ARM_CORTEX_M4F_0_CLOCK_FREQUENCY);
 #endif
 }
 
