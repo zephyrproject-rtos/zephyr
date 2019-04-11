be4c6ddd9cb8c77d2eaf7293499d9bc2e55cc92d
Peter A. Bigot <pab@pabigot.com>
2019-04-11 14:46:45 -0500

    boards: arm: particle_*: enable PCB antenna on startup
    All three boards use a Skyworks SPDT switch to control whether the
    antenna is connected to a PCB antenna or an external u.FL connector.
    None of them power up in a state that properly enables an antenna.
    Add startup code to configure for the PCB antenna.
    
    Fixes: https://github.com/zephyrproject-rtos/zephyr/issues/14123
    
    Signed-off-by: Peter A. Bigot <pab@pabigot.com>
    [mike@foundries.io: adjusted code to use Zephyr GPIO APIs.  boron is
    now also based on DTS.]
    Signed-off-by: Michael Scott <mike@foundries.io>

--- boards/arm/particle_boron/board.c
+++ boards/arm/particle_boron/board.c
@@ -1,17 +1,43 @@
 /*
  * Copyright (c) 2019 Foundries.io
+ * Copyright (c) 2019 Peter Bigot Consulting, LLC
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <init.h>
-#include "board.h"
 #include <gpio.h>
+#include "board.h"
 
-static int particle_boron_init(struct device *dev)
+static inline void external_antenna(bool on)
 {
+	struct device *ant_sel_gpio_dev;
 
+	/*
+	 * On power-up the SKY13351 is left uncontrolled, so neither
+	 * PCB nor external antenna is selected.  Select the PCB
+	 * antenna.
+	 */
+	ant_sel_gpio_dev = device_get_binding(ANT_SEL_GPIO_NAME);
+	if (!ant_sel_gpio_dev) {
+		return;
+	}
+
+	gpio_pin_configure(ant_sel_gpio_dev, ANT_SEL_GPIO_PIN,
+			   GPIO_DIR_OUT | ANT_SEL_GPIO_FLAGS);
+
+	if (on) {
+		gpio_pin_write(ant_sel_gpio_dev, ANT_SEL_GPIO_PIN, 1);
+	} else {
+		gpio_pin_write(ant_sel_gpio_dev, ANT_SEL_GPIO_PIN, 0);
+	}
+}
+
+static int board_particle_boron_init(struct device *dev)
+{
 	ARG_UNUSED(dev);
+
+	external_antenna(false);
 
 #if defined(CONFIG_MODEM_UBLOX_SARA_R4)
 	struct device *gpio_dev;
@@ -32,4 +58,5 @@ static int particle_boron_init(struct device *dev)
 }
 
 /* needs to be done after GPIO driver init */
-SYS_INIT(particle_boron_init, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+SYS_INIT(board_particle_boron_init, POST_KERNEL,
+	 CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
