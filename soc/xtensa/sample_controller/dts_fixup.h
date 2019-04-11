f2d84f08ff7d8905c5b8f2d79d29bb7f0123e0af
Piotr Zięcik <piotr.ziecik@nordicsemi.no>
2019-04-11 14:28:52 +0200

    arch: xtensa: Get CPU clock frequency from DTS
    The SoC initialization code used system clock frequency
    as a CPU clock frequency. This commit corrects that by
    obtaining the needed value from DTS.
    
    Signed-off-by: Piotr Zięcik <piotr.ziecik@nordicsemi.no>

---
+++ soc/xtensa/sample_controller/dts_fixup.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* SoC level DTS fixup file */
+
+#define DT_CPU_CLOCK_FREQUENCY DT_SAMPLE_CONTROLLER_0_CLOCK_FREQUENCY
+
+/* End of SoC Level DTS fixup file */
