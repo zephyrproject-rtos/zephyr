4522f427a92f44241642ed13d4e3fcff8b7687e9
Yaël Boutreux <yael.boutreux@st.com>
2019-04-12 15:25:41 +0200

    pinmux: Add stm32mp157c_dk2 board support
    Implementation of pinmux for the stm32mp157c_dk2 board.
    Some UART pin mux definition has been added (mainly for
    UART console and UART/SPI Arduino shield support).
    This can be completed with pin mux for other stm32mp157c
    UART.
    
    Signed-off-by: Yaël Boutreux <yael.boutreux@st.com>
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>
    Signed-off-by: Alexandre Torgue <alexandre.torgue@st.com>

--- drivers/pinmux/stm32/pinmux_stm32.h
+++ drivers/pinmux/stm32/pinmux_stm32.h
@@ -117,6 +117,8 @@ void stm32_setup_pins(const struct pin_config *pinconf,
 #include "pinmux_stm32l1x.h"
 #elif CONFIG_SOC_SERIES_STM32L4X
 #include "pinmux_stm32l4x.h"
+#elif CONFIG_SOC_SERIES_STM32MP1X
+#include "pinmux_stm32mp1x.h"
 #elif CONFIG_SOC_SERIES_STM32WBX
 #include "pinmux_stm32wbx.h"
 #endif
