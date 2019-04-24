e90d18474699ba96bc21f4666433caeb12612461
Andrei Gansari <andrei.gansari@nxp.com>
2019-04-24 16:52:51 +0300

    soc: added LPC5500 family
    SoC implementation for LPC5500 family.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    Signed-off-by: Andrei Gansari <andrei.gansari@nxp.com>

---
+++ soc/arm/nxp_lpc/lpc55xxx/soc.c
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2017, NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief System/hardware module for nxp_lpc55s69 platform
+ *
+ * This module provides routines to initialize and support board-level
+ * hardware for the nxp_lpc55s69 platform.
+ */
+
+#include <kernel.h>
+#include <device.h>
+#include <init.h>
+#include <soc.h>
+#include <uart.h>
+#include <linker/sections.h>
+#include <arch/cpu.h>
+#include <cortex_m/exc.h>
+#include <fsl_power.h>
+#include <fsl_clock.h>
+#include <fsl_common.h>
+#include <fsl_device_registers.h>
+
+/**
+ *
+ * @brief Initialize the system clock
+ *
+ * @return N/A
+ *
+ */
+
+static ALWAYS_INLINE void clkInit(void)
+{
+#ifdef CONFIG_SOC_LPC55S69_CPU0
+    /*!< Set up the clock sources */
+    /*!< Configure FRO192M */
+	/*!< Ensure FRO is on  */
+	POWER_DisablePD(kPDRUNCFG_PD_FRO192M);
+	/*!< Set up FRO to the 12 MHz, just for sure */
+	CLOCK_SetupFROClocking(12000000U);
+	/*!< Switch to FRO 12MHz first to ensure we can change the clock */
+	CLOCK_AttachClk(kFRO12M_to_MAIN_CLK);
+
+	/* Enable FRO HF(96MHz) output */
+	CLOCK_SetupFROClocking(96000000U);
+
+	/*!< Set FLASH wait states for core */
+	CLOCK_SetFLASHAccessCyclesForFreq(96000000U);
+
+    /*!< Set up dividers */
+	CLOCK_SetClkDiv(kCLOCK_DivAhbClk, 1U, false);
+
+    /*!< Set up clock selectors - Attach clocks to the peripheries */
+	CLOCK_AttachClk(kFRO_HF_to_MAIN_CLK);
+
+	/* Enables the clock for the I/O controller.: Enable Clock. */
+    CLOCK_EnableClock(kCLOCK_Iocon);
+#endif /* CONFIG_SOC_LPC55S69_CPU0 */
+}
+
+/**
+ *
+ * @brief Perform basic hardware initialization
+ *
+ * Initialize the interrupt controller device drivers.
+ * Also initialize the timer device driver, if required.
+ *
+ * @return 0
+ */
+
+static int nxp_lpc55s69_init(struct device *arg)
+{
+	ARG_UNUSED(arg);
+
+	/* old interrupt lock level */
+	unsigned int oldLevel;
+
+	/* disable interrupts */
+	oldLevel = irq_lock();
+
+	z_clearfaults();
+
+	/* Initialize FRO/system clock to 48 MHz */
+	clkInit();
+
+	/*
+	 * install default handler that simply resets the CPU if configured in
+	 * the kernel, NOP otherwise
+	 */
+	NMI_INIT();
+
+	/* restore interrupt state */
+	irq_unlock(oldLevel);
+
+	return 0;
+}
+
+SYS_INIT(nxp_lpc55s69_init, PRE_KERNEL_1, 0);
---
+++ soc/arm/nxp_lpc/lpc55xxx/soc.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2017, NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Board configuration macros for the nxp_lpc55s69 platform
+ *
+ * This header file is used to specify and describe board-level aspects for the
+ * 'nxp_lpc55s69' platform.
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _ASMLANGUAGE
+#include <device.h>
+#include <misc/util.h>
+#include <fsl_common.h>
+#endif /* !_ASMLANGUAGE */
+
+#define IOCON_PIO_DIGITAL_EN 0x0100u  /*!<@brief Enables digital function */
+#define IOCON_PIO_FUNC0 0x00u         /*!<@brief Selects pin function 0 */
+#define IOCON_PIO_FUNC1 0x01u         /*!<@brief Selects pin function 1 */
+#define IOCON_PIO_FUNC10 0x0Au        /*!<@brief Selects pin function 10 */
+#define IOCON_PIO_INV_DI 0x00u        /*!<@brief Input function not inverted */
+#define IOCON_PIO_MODE_INACT 0x00u    /*!<@brief No addition pin function */
+#define IOCON_PIO_OPENDRAIN_DI 0x00u  /*!<@brief Open drain is disabled */
+#define IOCON_PIO_SLEW_STANDARD 0x00u /*!<@brief Standard slew rate mode */
+#define IOCON_PIO_MODE_PULLDOWN 0x10u /*!<@brief Selects pull-down function */
+#define IOCON_PIO_MODE_PULLUP 0x20u   /*!<@brief Selects pull-up function */
+#define IOCON_PIO_INPFILT_OFF 0x1000u  /*!<@brief Input filter disabled */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SOC__H_ */
