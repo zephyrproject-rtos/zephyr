a60c7845a113d1a8e50f8d75306165347ec52bcc
Mark Ruvald Pedersen <mped@oticon.com>
2019-04-25 15:45:39 +0200

    cmake: fix whitespace
    No functional change expected.
    
    Signed-off-by: Mark Ruvald Pedersen <mped@oticon.com>

--- cmake/target_toolchain.cmake
+++ cmake/target_toolchain.cmake
@@ -50,12 +50,9 @@ include(${ZEPHYR_BASE}/cmake/compiler/${COMPILER}/target.cmake OPTIONAL)
 #
 # What we are looking for, is a signature definition that is defined
 # like this:
-
-# Toolchains with the same signature will always support the same set
-# of flags.
-
+#  * Toolchains with the same signature will always support the same set
+#    of flags.
 # It is not clear how this signature should be constructed. The
 # strategy chosen is to md5sum the CC binary.
-
 file(MD5 ${CMAKE_C_COMPILER} CMAKE_C_COMPILER_MD5_SUM)
 set(TOOLCHAIN_SIGNATURE ${CMAKE_C_COMPILER_MD5_SUM})
