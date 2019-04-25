9915b4ec4e2fa1f1ba5c5c586008ba7059400cbb
Benoit Leforestier <benoit.leforestier@gmail.com>
2019-04-25 14:46:55 +0200

    C++: Fix compilation error "invalid conversion"
    When some header are included into C++ source file, this kind of
    compilations errors are generated:
    error: invalid conversion from 'void*'
    	to 'u32_t*' {aka 'unsigned int*'} [-fpermissive]
    
    Signed-off-by: Benoit Leforestier <benoit.leforestier@gmail.com>

--- include/clock_control.h
+++ include/clock_control.h
@@ -51,7 +51,8 @@ struct clock_control_driver_api {
 static inline int clock_control_on(struct device *dev,
 				   clock_control_subsys_t sys)
 {
-	const struct clock_control_driver_api *api = dev->driver_api;
+	const struct clock_control_driver_api *api =
+		(const struct clock_control_driver_api *)dev->driver_api;
 
 	return api->on(dev, sys);
 }
@@ -65,7 +66,8 @@ static inline int clock_control_on(struct device *dev,
 static inline int clock_control_off(struct device *dev,
 				    clock_control_subsys_t sys)
 {
-	const struct clock_control_driver_api *api = dev->driver_api;
+	const struct clock_control_driver_api *api =
+		(const struct clock_control_driver_api *)dev->driver_api;
 
 	return api->off(dev, sys);
 }
@@ -81,7 +83,8 @@ static inline int clock_control_get_rate(struct device *dev,
 					 clock_control_subsys_t sys,
 					 u32_t *rate)
 {
-	const struct clock_control_driver_api *api = dev->driver_api;
+	const struct clock_control_driver_api *api =
+		(const struct clock_control_driver_api *)dev->driver_api;
 
 	__ASSERT(api->get_rate != NULL, "%s not implemented for device %s",
 		__func__, dev->config->name);
