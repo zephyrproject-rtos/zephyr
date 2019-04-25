4080818300f3b1c1cdad561e1ccc794ba3036340
Mark Ruvald Pedersen <mped@oticon.com>
2019-04-25 15:54:29 +0200

    cmake: Toolchain abstraction: move LINKERFLAGPREFIX under GNU ld
    * LINKERFLAGPREFIX's value is GNU ld specific, and
    * LINKERFLAGPREFIX is not a convention always honored by all linkers.
    
    Thus we shall not set it from the common root CMakeList.txt.
    So we move to linker/ld/target.cmake.
    
    No functional change expected.
    
    This is motivated by the wish to abstract Zephyr's usage of toolchains,
    permitting non-intrusive porting to other (commercial) toolchains.
    
    Signed-off-by: Mark Ruvald Pedersen <mped@oticon.com>

--- cmake/linker/ld/target.cmake
+++ cmake/linker/ld/target.cmake
@@ -2,5 +2,7 @@
 
 find_program(CMAKE_LINKER     ${CROSS_COMPILE}ld      PATH ${TOOLCHAIN_HOME} NO_DEFAULT_PATH)
 
+set_ifndef(LINKERFLAGPREFIX -Wl)
+
 # Load toolchain_ld-family macros
 include(${ZEPHYR_BASE}/cmake/linker/${LINKER}/target_base.cmake)
