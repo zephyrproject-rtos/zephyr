b39ce44ffaed0935bb17e8c5797854ab1acd7ffa
Mark Ruvald Pedersen <mped@oticon.com>
2019-05-03 11:02:56 +0200

    cmake: Toolchain abstraction: Introduce toolchain_ld_configure_files
    No functional change expected.
    
    This is motivated by the wish to abstract Zephyr's usage of toolchains,
    permitting non-intrusive porting to other (commercial) toolchains.
    
    Signed-off-by: Mark Ruvald Pedersen <mped@oticon.com>

---
+++ cmake/linker/ld/target_configure.cmake
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# See root CMakeLists.txt for description and expectations of these macros
+
+macro(toolchain_ld_configure_files)
+  configure_file(
+       $ENV{ZEPHYR_BASE}/include/arch/common/app_data_alignment.ld
+       ${PROJECT_BINARY_DIR}/include/generated/app_data_alignment.ld)
+
+  configure_file(
+       $ENV{ZEPHYR_BASE}/include/linker/app_smem.ld
+       ${PROJECT_BINARY_DIR}/include/generated/app_smem.ld)
+
+  configure_file(
+       $ENV{ZEPHYR_BASE}/include/linker/app_smem_aligned.ld
+       ${PROJECT_BINARY_DIR}/include/generated/app_smem_aligned.ld)
+
+  configure_file(
+       $ENV{ZEPHYR_BASE}/include/linker/app_smem_unaligned.ld
+       ${PROJECT_BINARY_DIR}/include/generated/app_smem_unaligned.ld)
+endmacro()
