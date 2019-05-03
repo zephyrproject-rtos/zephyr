39598424d22cc357cfb37365901c9a5cf733436b
Sigvart Hovland <sigvart.hovland@nordicsemi.no>
2019-05-03 14:21:52 +0200

    [nrf noup] tree-wide: support NCS Partition Manager (PM) definitions
    Partition Manager (PM) is a component of the nRF Connect SDK (NCS)
    which uses yaml files to resolve flash partition placement with a
    holistic view of the entire device, including each firmware image
    present on the flash device, and various subsystems, such as settings
    and NFFS.
    
    When this NCS extension is used, various source files which would use
    partition information from devicetree in "vanilla" zephyr instead use
    defines generated by PM instead.
    
    This commit removes support for HEX_FILES_TO_MERGE, as it conflicts
    with PM.
    
    The settings subsystem pm.yml defines a partition 'settings_storage'.
    The nffs subsystem pm.yml defines 'nffs_storage'.
    
    Leverage label translation to avoid patching partition names.
    
    Refer to the NCS documentation page for this feature for more details.
    
    This is a long-running out of tree patch which has been worked on by
    several people. The following sign-offs are in alphabetical order by
    first name.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>
    Signed-off-by: Ole Sæther <ole.saether@nordicsemi.no>
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>
    Signed-off-by: Sigvart Hovland <sigvart.hovland@nordicsemi.no>
    Signed-off-by: Thomas Stenersen <thomas.stenersen@nordicsemi.no>
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
    Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
    Signed-off-by: Tomasz Moń <tomasz.mon@nordicsemi.no>
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>
    (cherry picked from commit a7fb26837763ee7c21fb666fe2faa8c7503a17f1)
    (cherry picked from commit ea83f6ccc7786349ddcf267af19a1b63e0fe950f)

--- arch/arm/core/mpu/arm_mpu_regions.c
+++ arch/arm/core/mpu/arm_mpu_regions.c
@@ -8,6 +8,9 @@
 #include <zephyr/arch/arm/mpu/arm_mpu.h>
 
 #include <zephyr/arch/arm/cortex_m/arm_mpu_mem_cfg.h>
+#if USE_PARTITION_MANAGER
+#include <pm_config.h>
+#endif
 
 static const struct arm_mpu_region mpu_regions[] = {
 	/* Region 0 */
@@ -21,6 +24,14 @@ static const struct arm_mpu_region mpu_regions[] = {
 #endif
 	/* Region 1 */
 	MPU_REGION_ENTRY("SRAM_0",
+#if USE_PARTITION_MANAGER
+			 PM_SRAM_ADDRESS,
+#if defined(CONFIG_ARMV8_M_BASELINE) || defined(CONFIG_ARMV8_M_MAINLINE)
+			 REGION_RAM_ATTR(PM_SRAM_ADDRESS, PM_SRAM_SIZE)),
+#else
+			 REGION_RAM_ATTR(REGION_SRAM_SIZE)),
+#endif
+#else
 			 CONFIG_SRAM_BASE_ADDRESS,
 #if defined(CONFIG_ARMV8_M_BASELINE) || defined(CONFIG_ARMV8_M_MAINLINE)
 			 REGION_RAM_ATTR(CONFIG_SRAM_BASE_ADDRESS, \
@@ -28,6 +39,8 @@ static const struct arm_mpu_region mpu_regions[] = {
 #else
 			 REGION_RAM_ATTR(REGION_SRAM_SIZE)),
 #endif
+
+#endif /* USE_PARTITION_MANAGER */
 };
 
 const struct arm_mpu_config mpu_config = {
