a88a3ff04be2003930a15c12f3a6ccf501de58dc
Henrik Brix Andersen <hebad@vestas.com>
2019-05-05 00:01:56 +0200

    soc: arm: ke1xf: add NXP Kinetis KE1xF SoC series support
    Add initial support for the NXP Kinetis KE1xF SoC series (MKE14F16,
    MKE16F16, and MKE18F16).
    
    Signed-off-by: Henrik Brix Andersen <hebad@vestas.com>

---
+++ soc/arm/nxp_kinetis/ke1xf/soc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2019 Vestas Wind Systems A/S
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#include <misc/util.h>
+
+#endif /* _SOC__H_ */
