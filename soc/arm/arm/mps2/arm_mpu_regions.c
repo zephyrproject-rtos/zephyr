502094b096206245a07031521d7252244c8b0d42
Kumar Gala <kumar.gala@linaro.org>
2019-05-06 16:56:38 +0800

    arm: mps_an521: Add support for the AN521 on MPS2+
    The AN521 is a Soft Macro Model implementation of the SSE-200 subsystem
    with SIE-200 and CMSDK components targeting the MPS2+ board. The
    SSE-200 subsystem implements two Cortex-M33 cores.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    Signed-off-by: Karl Zhang <karl.zhang@linaro.org>

--- soc/arm/arm/mps2/arm_mpu_regions.c
+++ soc/arm/arm/mps2/arm_mpu_regions.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Linaro Limited.
+ * Copyright (c) 2017-2019 Linaro Limited.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,15 +9,24 @@
 #include <soc.h>
 #include <arch/arm/cortex_m/mpu/arm_mpu.h>
 
+#if defined(CONFIG_ARMV8_M_BASELINE) || \
+    defined(CONFIG_ARMV8_M_MAINLINE)
+#define MPS2_FLASH_ATTR REGION_FLASH_ATTR(CONFIG_FLASH_BASE_ADDRESS, CONFIG_FLASH_SIZE*1024)
+#define MPS2_RAM_ATTR REGION_RAM_ATTR(CONFIG_SRAM_BASE_ADDRESS, CONFIG_SRAM_SIZE*1024)
+#else
+#define MPS2_FLASH_ATTR REGION_FLASH_ATTR(REGION_4M)
+#define MPS2_RAM_ATTR REGION_RAM_ATTR(REGION_2M)
+#endif
+
 static const struct arm_mpu_region mpu_regions[] = {
 	/* Region 0 */
 	MPU_REGION_ENTRY("FLASH_0",
 			 CONFIG_FLASH_BASE_ADDRESS,
-			 REGION_FLASH_ATTR(REGION_4M)),
+			 MPS2_FLASH_ATTR),
 
 	MPU_REGION_ENTRY("SRAM_0",
 			 CONFIG_SRAM_BASE_ADDRESS,
-			 REGION_RAM_ATTR(REGION_2M))
+			 MPS2_RAM_ATTR)
 };
 
 const struct arm_mpu_config mpu_config = {
