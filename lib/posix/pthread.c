1f45ab67b0cd001937fd6a812404639a9414bb34
Wentong Wu <wentong.wu@intel.com>
2019-05-11 19:43:01 +0800

    lib/posix: correct the meaning of CONFIG_MAX_PTHREAD_COUNT
    Current code implement CONFIG_MAX_PTHREAD_COUNT as the maximum number
    of POSIX threads that can ever be created, rather than the maximum
    number of active POSIX threads. Use pthread_state of struct posix_thread
    to track the state of posix thread in posix_thread_pool so that we can
    reuse the unused posix thread.
    
    Fixes #15516.
    
    Signed-off-by: Wentong Wu <wentong.wu@intel.com>

--- lib/posix/pthread.c
+++ lib/posix/pthread.c
@@ -35,7 +35,7 @@ static const pthread_attr_t init_pthread_attrs = {
 };
 
 static struct posix_thread posix_thread_pool[CONFIG_MAX_PTHREAD_COUNT];
-static u32_t pthread_num;
+PTHREAD_MUTEX_DEFINE(pthread_pool_lock);
 
 static bool is_posix_prio_valid(u32_t priority, int policy)
 {
@@ -132,6 +132,7 @@ int pthread_create(pthread_t *newthread, const pthread_attr_t *attr,
 		   void *(*threadroutine)(void *), void *arg)
 {
 	s32_t prio;
+	u32_t pthread_num;
 	pthread_condattr_t cond_attr;
 	struct posix_thread *thread;
 
@@ -144,6 +145,17 @@ int pthread_create(pthread_t *newthread, const pthread_attr_t *attr,
 	    || (attr->stack == NULL) || (attr->stacksize == 0)) {
 		return EINVAL;
 	}
+
+	pthread_mutex_lock(&pthread_pool_lock);
+	for (pthread_num = 0;
+	    pthread_num < CONFIG_MAX_PTHREAD_COUNT; pthread_num++) {
+		thread = &posix_thread_pool[pthread_num];
+		if (thread->state == PTHREAD_TERMINATED) {
+			thread->state = PTHREAD_JOINABLE;
+			break;
+		}
+	}
+	pthread_mutex_unlock(&pthread_pool_lock);
 
 	if (pthread_num >= CONFIG_MAX_PTHREAD_COUNT) {
 		return EAGAIN;
@@ -166,7 +178,6 @@ int pthread_create(pthread_t *newthread, const pthread_attr_t *attr,
 
 	pthread_cond_init(&thread->state_cond, &cond_attr);
 	sys_slist_init(&thread->key_list);
-	pthread_num++;
 
 	*newthread = (pthread_t) k_thread_create(&thread->thread, attr->stack,
 						 attr->stacksize,
