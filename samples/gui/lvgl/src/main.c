84de52e721fe7a293eb412c0949937e813b17b35
Maureen Helm <maureen.helm@nxp.com>
2019-05-13 15:15:18 -0500

    samples: gui: Hack the LittlevGL sample to use touch input
    Enables the touch sensor input and adds a keyboard object to the
    LittlevGL sample application.
    
    This is a hack, do not merge!
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- samples/gui/lvgl/src/main.c
+++ samples/gui/lvgl/src/main.c
@@ -37,6 +37,32 @@ void main(void)
 	count_label = lv_label_create(lv_scr_act(), NULL);
 	lv_obj_align(count_label, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
 
+	/*Create styles for the keyboard*/
+	static lv_style_t rel_style, pr_style;
+
+	lv_style_copy(&rel_style, &lv_style_btn_rel);
+	rel_style.body.radius = 0;
+
+	lv_style_copy(&pr_style, &lv_style_btn_pr);
+	pr_style.body.radius = 0;
+
+	/*Create a keyboard and apply the styles*/
+	lv_obj_t *kb = lv_kb_create(lv_scr_act(), NULL);
+
+	lv_kb_set_cursor_manage(kb, true);
+	lv_kb_set_style(kb, LV_KB_STYLE_BG, &lv_style_transp_tight);
+	lv_kb_set_style(kb, LV_KB_STYLE_BTN_REL, &rel_style);
+	lv_kb_set_style(kb, LV_KB_STYLE_BTN_PR, &pr_style);
+
+	/*Create a text area. The keyboard will write here*/
+	lv_obj_t *ta = lv_ta_create(lv_scr_act(), NULL);
+
+	lv_obj_align(ta, NULL, LV_ALIGN_IN_TOP_MID, 0, 10);
+	lv_ta_set_text(ta, "");
+
+	/*Assign the text area to the keyboard*/
+	lv_kb_set_ta(kb, ta);
+
 	display_blanking_off(display_dev);
 
 	while (1) {
