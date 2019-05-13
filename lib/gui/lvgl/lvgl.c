6aa1d7b5aa46d81cbb876bd90a7886652c946dec
Maureen Helm <maureen.helm@nxp.com>
2019-05-13 14:49:38 -0500

    lib: gui: Add support for touch input to LittlevGL integration
    Adds support for LittlevGL input devices using the new zio interface.
    This should ultimately be independent of the specific touch controller,
    however it is currently tied to the ft5336 due to the lack of an
    abstracted touch sensor data type.
    
    We also have not implemented gpio interrupts in the ft5336 driver and
    therefore rely on manual triggering, making the touch input appear a bit
    laggy in the gui.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- lib/gui/lvgl/lvgl.c
+++ lib/gui/lvgl/lvgl.c
@@ -6,6 +6,9 @@
 
 #include <init.h>
 #include <zephyr.h>
+#include <zio/zio_dev.h>
+#include <zio/zio_buf.h>
+#include <drivers/zio/ft5336.h>
 #include <lvgl.h>
 #include <lv_core/lv_refr.h>
 #include "lvgl_color.h"
@@ -46,6 +49,72 @@ static void lvgl_log(lv_log_level_t level, const char *file, uint32_t line,
 }
 #endif
 
+#if CONFIG_LVGL_INPUT_POINTER
+static struct device *lvgl_input_pointer_dev;
+static lv_indev_data_t indev_data;
+static ZIO_BUF_DEFINE(inputbuf);
+
+static struct k_poll_event events[1] = {
+	K_POLL_EVENT_STATIC_INITIALIZER(K_POLL_TYPE_SEM_AVAILABLE,
+			K_POLL_MODE_NOTIFY_ONLY, &inputbuf.sem, 0),
+};
+
+static bool lvgl_input_pointer_read(lv_indev_data_t *data)
+{
+	struct ft5336_datum datum;
+
+	if (zio_buf_get_length(&inputbuf) == 0) {
+		zio_dev_trigger(lvgl_input_pointer_dev);
+	}
+
+	if ((k_poll(events, 1, K_MSEC(1)) == 0) &&
+	    (zio_buf_pull(&inputbuf, &datum) == 1) &&
+	    (datum.id == 0)) {
+		indev_data.point.x = datum.y;
+		indev_data.point.y = datum.x;
+		indev_data.state =
+			((datum.event == FT5336_DOWN) ||
+			 (datum.event == FT5336_CONTACT)) ?
+			LV_INDEV_STATE_PR :
+			LV_INDEV_STATE_REL;
+
+		LOG_DBG("point state %d at (%d,%d)",
+			indev_data.state,
+			indev_data.point.x,
+			indev_data.point.y);
+	}
+
+	*data = indev_data;
+
+	return zio_buf_get_length(&inputbuf) > 0;
+}
+
+static int lvgl_input_pointer_init(void)
+{
+	lv_indev_drv_t indev_drv;
+
+	lvgl_input_pointer_dev =
+		device_get_binding(CONFIG_LVGL_INPUT_POINTER_DEV_NAME);
+
+	if (lvgl_input_pointer_dev == NULL) {
+		LOG_ERR("Input device not found.");
+		return -ENODEV;
+	}
+
+	zio_buf_attach(&inputbuf, lvgl_input_pointer_dev);
+
+	lv_indev_drv_init(&indev_drv);
+	indev_drv.type = LV_INDEV_TYPE_POINTER;
+	indev_drv.read = lvgl_input_pointer_read;
+
+	if (lv_indev_drv_register(&indev_drv) == NULL) {
+		LOG_ERR("Failed to register input device.");
+		return -EPERM;
+	}
+
+	return 0;
+}
+#endif /* CONFIG_LVGL_INPUT_POINTER */
 
 static int lvgl_init(struct device *dev)
 {
@@ -82,6 +151,9 @@ static int lvgl_init(struct device *dev)
 		LOG_ERR("Failed to register display device.");
 		return -EPERM;
 	}
+#if CONFIG_LVGL_INPUT_POINTER
+	lvgl_input_pointer_init();
+#endif
 
 	return 0;
 }
