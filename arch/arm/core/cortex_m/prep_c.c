ed682b95756e88e608b85a3b795fb8601aaf3f76
Wentong Wu <wentong.wu@intel.com>
2019-05-16 23:40:08 +0800

    arch: arm: switching stack pointer with assembly code
    With -O0 optimizion, gcc compiler doesn't inline "static inline"
    marked function. So when function call return from function
    set_and_switch_to_psp which is to switch sp from MSP to PSP, the
    ending "mov sp, r7" instruction will overwrite the just updated
    sp value(PSP) with the beginning stack pointer(should be MSP)
    stored in r7 register, so the switch doesn't happen. And it causes
    unpredictable problems in the initialization process, the backward
    analysis for this problem can be found on Github issue #15794.
    
    Fixes: #15794.
    
    Signed-off-by: Wentong Wu <wentong.wu@intel.com>

--- arch/arm/core/cortex_m/prep_c.c
+++ arch/arm/core/cortex_m/prep_c.c
@@ -22,7 +22,6 @@
 #include <linker/linker-defs.h>
 #include <kernel_internal.h>
 #include <arch/arm/cortex_m/cmsis.h>
-#include <cortex_m/stack.h>
 
 #if defined(__GNUC__)
 /*
@@ -36,44 +35,6 @@
 #endif
 
 #include <string.h>
-
-static inline void switch_sp_to_psp(void)
-{
-	__set_CONTROL(__get_CONTROL() | CONTROL_SPSEL_Msk);
-	/*
-	 * When changing the stack pointer, software must use an ISB instruction
-	 * immediately after the MSR instruction. This ensures that instructions
-	 * after the ISB instruction execute using the new stack pointer.
-	 */
-	__ISB();
-}
-
-static inline void set_and_switch_to_psp(void)
-{
-	u32_t process_sp;
-
-	process_sp = (u32_t)&_interrupt_stack + CONFIG_ISR_STACK_SIZE;
-	__set_PSP(process_sp);
-	switch_sp_to_psp();
-}
-
-void lock_interrupts(void)
-{
-#if defined(CONFIG_ARMV6_M_ARMV8_M_BASELINE)
-	__disable_irq();
-#elif defined(CONFIG_ARMV7_M_ARMV8_M_MAINLINE)
-	__set_BASEPRI(_EXC_IRQ_DEFAULT_PRIO);
-#else
-#error Unknown ARM architecture
-#endif /* CONFIG_ARMV6_M_ARMV8_M_BASELINE */
-}
-
-#ifdef CONFIG_INIT_STACKS
-static inline void init_stacks(void)
-{
-	memset(&_interrupt_stack, 0xAA, CONFIG_ISR_STACK_SIZE);
-}
-#endif
 
 #ifdef CONFIG_CPU_CORTEX_M_HAS_VTOR
 
@@ -191,14 +152,6 @@ extern void z_IntLibInit(void);
 #endif
 void _PrepC(void)
 {
-#ifdef CONFIG_INIT_STACKS
-	init_stacks();
-#endif
-	/*
-	 * Set PSP and use it to boot without using MSP, so that it
-	 * gets set to _interrupt_stack during initialization.
-	 */
-	set_and_switch_to_psp();
 	relocate_vector_table();
 	enable_floating_point();
 	z_bss_zero();
