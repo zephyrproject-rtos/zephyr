3912193d2ba9fda44694eca2672d711935111bc9
Kumar Gala <kumar.gala@linaro.org>
2019-05-15 22:20:14 -0500

    new_extract: start of register define extraction
    Starts of walking registers an creating define output.  Still a lot of
    work needed here
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- scripts/dts/new_extract.py
+++ scripts/dts/new_extract.py
@@ -6,6 +6,13 @@ import sys
 
 import edtlib
 
+def str_to_label(s):
+    # Change ,-@/ to _ and uppercase
+    return s.replace('-', '_') \
+            .replace(',', '_') \
+            .replace('@', '_') \
+            .replace('/', '_') \
+            .upper()
 
 def main():
     # Copied from extract_dts_includes.py
@@ -24,9 +31,29 @@ def main():
 
     edt = edtlib.EDT(args.dts, args.yaml[0])
 
+    import pprint
+    dt_output = {}
+    dt_alias = {}
+    for dev in edt.devices.values():
+        postfix = False
+        if len(dev.regs) > 1:
+            postfix = True
+        for r in dev.regs:
+            pre = "DT_{}".format(str_to_label(dev.compat))
+            body = "{:X}".format(r.addr)
+            post = "BASE_ADDRESS"
+            if postfix:
+                post = "{}_{}".format(post, dev.regs.index(r))
+            tag = (pre, body, post)
+            dt_output[tag] = r.addr
+            dt_alias[(pre, dev.instance, post)] = tag
+
     with open(args.keyvalue + "-new", "w") as f:
-        for dev in edt.devices.values():
-            print("{}=1".format(dev.name), file=f)
+        for (pre, body, post) in dt_output:
+            print("#define %s_%s_%s\t0x%x" % (pre, body, post, dt_output[(pre, body, post)]))
+        for (pre, body, post) in dt_alias:
+            (alias_pre, alias_body, alias_post) = dt_alias[(pre, body, post)]
+            print("#define %s_%s_%s\t%s_%s_%s" % (pre, body, post, alias_pre, alias_body, alias_post))
 
     with open(args.include + "-new", "w") as f:
         for dev in edt.devices.values():
