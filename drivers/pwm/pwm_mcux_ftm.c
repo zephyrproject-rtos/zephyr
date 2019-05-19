84c74993d4b3477b8e041ab13608379ae26093cd
Henrik Brix Andersen <hebad@vestas.com>
2019-05-19 23:42:28 +0200

    drivers: pwm: mcux_ftm: use device tree for obtaining clock frequency
    Use clock specified in the device tree for obtaining the source clock
    frequency for the pwm_mcux_ftm driver instead of relying on having an
    NXP Kinetis MCG clock available in all SoCs supporting FlexTimer (FTM)
    modules.
    
    Signed-off-by: Henrik Brix Andersen <hebad@vestas.com>

--- drivers/pwm/pwm_mcux_ftm.c
+++ drivers/pwm/pwm_mcux_ftm.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <drivers/clock_control.h>
 #include <errno.h>
 #include <drivers/pwm.h>
 #include <soc.h>
@@ -18,7 +19,8 @@ LOG_MODULE_REGISTER(pwm_mcux_ftm);
 
 struct mcux_ftm_config {
 	FTM_Type *base;
-	clock_name_t clock_source;
+	char *clock_name;
+	clock_control_subsys_t clock_subsys;
 	ftm_clock_source_t ftm_clock_source;
 	ftm_clock_prescale_t prescale;
 	u8_t channel_count;
@@ -26,6 +28,7 @@ struct mcux_ftm_config {
 };
 
 struct mcux_ftm_data {
+	u32_t clock_freq;
 	u32_t period_cycles;
 	ftm_chnl_pwm_signal_param_t channel[MAX_CHANNELS];
 };
@@ -55,7 +58,6 @@ static int mcux_ftm_pin_set(struct device *dev, u32_t pwm,
 		    pulse_cycles, period_cycles, duty_cycle);
 
 	if (period_cycles != data->period_cycles) {
-		u32_t clock_freq;
 		u32_t pwm_freq;
 		status_t status;
 
@@ -66,10 +68,11 @@ static int mcux_ftm_pin_set(struct device *dev, u32_t pwm,
 
 		data->period_cycles = period_cycles;
 
-		clock_freq = CLOCK_GetFreq(config->clock_source);
-		pwm_freq = (clock_freq >> config->prescale) / period_cycles;
+		pwm_freq = (data->clock_freq >> config->prescale) /
+			   period_cycles;
 
-		LOG_DBG("pwm_freq=%d, clock_freq=%d", pwm_freq, clock_freq);
+		LOG_DBG("pwm_freq=%d, clock_freq=%d", pwm_freq,
+			data->clock_freq);
 
 		if (pwm_freq == 0U) {
 			LOG_ERR("Could not set up pwm_freq=%d", pwm_freq);
@@ -80,7 +83,7 @@ static int mcux_ftm_pin_set(struct device *dev, u32_t pwm,
 
 		status = FTM_SetupPwm(config->base, data->channel,
 				      config->channel_count, config->mode,
-				      pwm_freq, clock_freq);
+				      pwm_freq, data->clock_freq);
 
 		if (status != kStatus_Success) {
 			LOG_ERR("Could not set up pwm");
@@ -102,8 +105,9 @@ static int mcux_ftm_get_cycles_per_sec(struct device *dev, u32_t pwm,
 				       u64_t *cycles)
 {
 	const struct mcux_ftm_config *config = dev->config->config_info;
+	struct mcux_ftm_data *data = dev->driver_data;
 
-	*cycles = CLOCK_GetFreq(config->clock_source) >> config->prescale;
+	*cycles = data->clock_freq >> config->prescale;
 
 	return 0;
 }
@@ -113,11 +117,24 @@ static int mcux_ftm_init(struct device *dev)
 	const struct mcux_ftm_config *config = dev->config->config_info;
 	struct mcux_ftm_data *data = dev->driver_data;
 	ftm_chnl_pwm_signal_param_t *channel = data->channel;
+	struct device *clock_dev;
 	ftm_config_t ftm_config;
 	int i;
 
 	if (config->channel_count > ARRAY_SIZE(data->channel)) {
 		LOG_ERR("Invalid channel count");
+		return -EINVAL;
+	}
+
+	clock_dev = device_get_binding(config->clock_name);
+	if (clock_dev == NULL) {
+		LOG_ERR("Could not get clock device");
+		return -EINVAL;
+	}
+
+	if (clock_control_get_rate(clock_dev, config->clock_subsys,
+				   &data->clock_freq)) {
+		LOG_ERR("Could not get clock frequency");
 		return -EINVAL;
 	}
 
@@ -145,7 +162,9 @@ static const struct pwm_driver_api mcux_ftm_driver_api = {
 #ifdef CONFIG_PWM_0
 static const struct mcux_ftm_config mcux_ftm_config_0 = {
 	.base = (FTM_Type *)DT_FTM_0_BASE_ADDRESS,
-	.clock_source = kCLOCK_McgFixedFreqClk,
+	.clock_name = DT_FTM_0_CLOCK_NAME,
+	.clock_subsys =
+		(clock_control_subsys_t) DT_FTM_0_CLOCK_SUBSYS,
 	.ftm_clock_source = kFTM_FixedClock,
 	.prescale = kFTM_Prescale_Divide_16,
 	.channel_count = FSL_FEATURE_FTM_CHANNEL_COUNTn(FTM0),
@@ -163,7 +182,9 @@ DEVICE_AND_API_INIT(mcux_ftm_0, DT_FTM_0_NAME, &mcux_ftm_init,
 #ifdef CONFIG_PWM_1
 static const struct mcux_ftm_config mcux_ftm_config_1 = {
 	.base = (FTM_Type *)DT_FTM_1_BASE_ADDRESS,
-	.clock_source = kCLOCK_McgFixedFreqClk,
+	.clock_name = DT_FTM_1_CLOCK_NAME,
+	.clock_subsys =
+		(clock_control_subsys_t) DT_FTM_1_CLOCK_SUBSYS,
 	.ftm_clock_source = kFTM_FixedClock,
 	.prescale = kFTM_Prescale_Divide_16,
 	.channel_count = FSL_FEATURE_FTM_CHANNEL_COUNTn(FTM1),
@@ -181,7 +202,9 @@ DEVICE_AND_API_INIT(mcux_ftm_1, DT_FTM_1_NAME, &mcux_ftm_init,
 #ifdef CONFIG_PWM_2
 static const struct mcux_ftm_config mcux_ftm_config_2 = {
 	.base = (FTM_Type *)DT_FTM_2_BASE_ADDRESS,
-	.clock_source = kCLOCK_McgFixedFreqClk,
+	.clock_name = DT_FTM_2_CLOCK_NAME,
+	.clock_subsys =
+		(clock_control_subsys_t) DT_FTM_2_CLOCK_SUBSYS,
 	.ftm_clock_source = kFTM_FixedClock,
 	.prescale = kFTM_Prescale_Divide_16,
 	.channel_count = FSL_FEATURE_FTM_CHANNEL_COUNTn(FTM2),
@@ -199,7 +222,9 @@ DEVICE_AND_API_INIT(mcux_ftm_2, DT_FTM_2_NAME, &mcux_ftm_init,
 #ifdef CONFIG_PWM_3
 static const struct mcux_ftm_config mcux_ftm_config_3 = {
 	.base = (FTM_Type *)DT_FTM_3_BASE_ADDRESS,
-	.clock_source = kCLOCK_McgFixedFreqClk,
+	.clock_name = DT_FTM_3_CLOCK_NAME,
+	.clock_subsys =
+		(clock_control_subsys_t) DT_FTM_3_CLOCK_SUBSYS,
 	.ftm_clock_source = kFTM_FixedClock,
 	.prescale = kFTM_Prescale_Divide_16,
 	.channel_count = FSL_FEATURE_FTM_CHANNEL_COUNTn(FTM3),
