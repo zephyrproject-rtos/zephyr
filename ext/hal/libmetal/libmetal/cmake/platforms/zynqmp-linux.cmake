fe0a50f8120371dcf31b7de3be566af78f095b05
Anas Nashif <anas.nashif@intel.com>
2019-05-22 16:58:54 -0700

    ext: move libmetal to an external module
    Move libmetal to be an external module and add it manifest.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- ext/hal/libmetal/libmetal/cmake/platforms/zynqmp-linux.cmake
+++
@@ -1,5 +0,0 @@
-set (CMAKE_SYSTEM_PROCESSOR "aarch64"            CACHE STRING "")
-set (CROSS_PREFIX           "aarch64-linux-gnu-" CACHE STRING "")
-include (cross-linux-gcc)
-
-# vim: expandtab:ts=2:sw=2:smartindent
