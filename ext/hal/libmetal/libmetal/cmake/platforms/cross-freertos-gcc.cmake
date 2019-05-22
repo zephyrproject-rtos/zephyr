61479686680d4782ba718ebcd980280de5ce1ba1
Anas Nashif <anas.nashif@intel.com>
2019-05-22 16:58:54 -0700

    ext: move libmetal to an external module
    Move libmetal to be an external module and add it manifest.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- ext/hal/libmetal/libmetal/cmake/platforms/cross-freertos-gcc.cmake
+++
@@ -1,6 +0,0 @@
-# cmake 3.3.2 does not know CMAKE_SYSTEM_NAME=FreeRTOS, we set it to Generic
-include (cross-generic-gcc)
-string (TOLOWER "FreeRTOS"                PROJECT_SYSTEM)
-string (TOUPPER "FreeRTOS"                PROJECT_SYSTEM_UPPER)
-
-# vim: expandtab:ts=2:sw=2:smartindent
--- ext/hal/libmetal/libmetal/cmake/platforms/zynq7-linux.cmake
+++
@@ -1,5 +0,0 @@
-set (CMAKE_SYSTEM_PROCESSOR "arm"                       CACHE STRING "")
-set (CROSS_PREFIX           "arm-xilinx-linux-gnueabi-" CACHE STRING "")
-include (cross-linux-gcc)
-
-# vim: expandtab:ts=2:sw=2:smartindent
