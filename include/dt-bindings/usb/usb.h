2f8c8047bf71c4a725f9aea4663e503ed0b1e682
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2019-05-22 16:44:50 +0300

    usb: dt-bindings: Change enum to macros
    Use macros since we need to use them in the preprocessor conditionals.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- include/dt-bindings/usb/usb.h
+++ include/dt-bindings/usb/usb.h
@@ -6,17 +6,13 @@
 #ifndef ZEPHYR_INCLUDE_DT_BINDINGS_USB_USB_H_
 #define ZEPHYR_INCLUDE_DT_BINDINGS_USB_USB_H_
 
-/* Ideally we'd generate this enum to match what's coming out of the YAML,
- * however, we dont have a good way to know how to name such an enum from
- * the generation point of view, so for now we just hand code the enum.  This
- * enum is expected to match the order in the yaml (dts/bindings/usb/usb.yaml)
+/* Use macros since we need to use them in the preprocessor conditionals.
+ * These macros are expected to match the order in the yaml
+ * (dts/bindings/usb/usb.yaml)
  */
-
-enum dt_usb_maximum_speed {
-	DT_USB_MAXIMUM_SPEED_LOW_SPEED,
-	DT_USB_MAXIMUM_SPEED_FULL_SPEED,
-	DT_USB_MAXIMUM_SPEED_HIGH_SPEED,
-	DT_USB_MAXIMUM_SPEED_SUPER_SPEED,
-};
+#define DT_USB_MAXIMUM_SPEED_LOW_SPEED		0
+#define DT_USB_MAXIMUM_SPEED_FULL_SPEED		1
+#define DT_USB_MAXIMUM_SPEED_HIGH_SPEED		2
+#define DT_USB_MAXIMUM_SPEED_SUPER_SPEED	3
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_USB_USB_H_ */
