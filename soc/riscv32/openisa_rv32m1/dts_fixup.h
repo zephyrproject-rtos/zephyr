6f6551154593e74a2f76188b303716d7432d48fe
Lyle Zhu <lyle.zhu@nxp.com>
2019-05-22 23:07:50 +0800

    driver: flash: add flash driver for the RV32M1 SOC
    Add driver and device tree for the flash controller for the RV32M1 SOC
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

---
+++ soc/riscv32/openisa_rv32m1/dts_fixup.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2019 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* SoC level DTS fixup file */
+
+#if defined(CONFIG_SOC_OPENISA_RV32M1_RISCV32)
+
+#define DT_FLASH_DEV_BASE_ADDRESS DT_OPENISA_RV32M1_FTFE_40023000_BASE_ADDRESS
+#define DT_FLASH_DEV_NAME         DT_OPENISA_RV32M1_FTFE_40023000_LABEL
+
+#endif /* CONFIG_SOC_OPENISA_RV32M1_RISCV32 */
+
+/* End of SoC Level DTS fixup file */
