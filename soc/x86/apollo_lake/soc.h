546cf75a402f4e42f94f05934cf854963b6c6835
Charles E. Youse <charles.youse@intel.com>
2019-05-23 14:15:46 -0700

    soc/x86/apollo_lake: remove legacy PCI support
    The Apollo Lake SoC no longer uses the legacy PCI driver, so remove
    references and supporting code.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

--- soc/x86/apollo_lake/soc.h
+++ soc/x86/apollo_lake/soc.h
@@ -27,24 +27,4 @@
 #include "soc_gpio.h"
 #endif
 
-#ifdef CONFIG_PCI
-
-/*
- * PCI definitions
- */
-#define PCI_BUS_NUMBERS                         1
-
-#define PCI_CTRL_ADDR_REG                       0xCF8
-#define PCI_CTRL_DATA_REG                       0xCFC
-
-/**
- * @brief Convert PCI interrupt PIN to IRQ
- *
- * BIOS should have assigned vectors linearly.
- * If not, override this in board configuration.
- */
-#define pci_pin2irq(bus, dev, pin)              (pin)
-
-#endif /* CONFIG_PCI */
-
 #endif /* __SOC_H_ */
