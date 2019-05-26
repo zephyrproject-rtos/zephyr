a1a3a4fced8d724edb3ad6c137913ad8e298585e
Charles E. Youse <charles.youse@intel.com>
2019-05-25 20:01:24 -0700

    arch/x86: add support for Multiboot boot information structure
    When booting using GRUB, some useful information about the environment
    is given to us via a boot information structure. We've not made any
    use of this information so far, but the x86 framebuffer driver will.
    
    A skeletal definition of the structure is given, and provisions are
    made to preserve its contents at boot if the configuration requires it.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

---
+++ arch/x86/core/multiboot.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <kernel.h>
+#include <arch/x86/multiboot.h>
+
+#ifdef CONFIG_X86_MULTIBOOT_INFO
+
+struct x86_multiboot_info x86_multiboot_info;
+
+#endif /* CONFIG_X86_MULTIBOOT_INFO */
