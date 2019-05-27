2eb2dc903a34f15d83595ba1f2db9ee84fb5decf
Nicolas Pitre <npitre@baylibre.com>
2019-05-27 19:26:46 -0400

    string.c: make it 64-bit compatible
    Casting a pointer to an int produces warnings with 64-bit targets.
    Furthermore, an int is not always the optimal memory element that
    can be copied in that case.
    
    Let's use uintptr_t to cast pointers to integers for alignment
    determination purposes, and mem_word_t to denote the optimal memory
    "word" that can be copied on the platform.
    
    The mem_word_t definition is equivalent to uintptr_t by default.
    However, some 32-bit targets such as ARM platforms with the LDRD/STRD
    instructions could benefit from word_t being an uint64_t.
    
    Signed-off-by: Nicolas Pitre <npitre@baylibre.com>

--- lib/libc/minimal/include/sys/types.h
+++ lib/libc/minimal/include/sys/types.h
@@ -8,6 +8,8 @@
 #ifndef ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_SYS_TYPES_H_
 #define ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_SYS_TYPES_H_
 
+#include <stdint.h>
+
 typedef unsigned int mode_t;
 
 #if !defined(__ssize_t_defined)
@@ -42,5 +44,19 @@ typedef int off_t;
 
 typedef long long time_t;
 typedef long suseconds_t;
+
+#if !defined(__mem_word_t_defined)
+#define __mem_word_t_defined
+
+/*
+ * The mem_word_t should match the optimal memory access word width
+ * on the target platform. Here we defaults it to uintptr_t.
+ */
+
+typedef uintptr_t mem_word_t;
+
+#define Z_MEM_WORD_T_WIDTH __INTPTR_WIDTH__
+
+#endif
 
 #endif /* ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_SYS_TYPES_H_ */
