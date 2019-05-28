23f7603f5b6ed12cf6df1a6435ef9f5053b4c186
Andrei Gansari <andrei.gansari@nxp.com>
2019-05-28 16:24:10 +0300

    boards: lpcxpresso55s69 pinmux macros refactor
    Board is refactored to use DTS generated value, not use
    magic numbers.
    
    Signed-off-by: Andrei Gansari <andrei.gansari@nxp.com>

--- boards/arm/lpcxpresso55s69/pinmux.c
+++ boards/arm/lpcxpresso55s69/pinmux.c
@@ -48,8 +48,8 @@ static int lpcxpresso_55s69_pinmux_init(struct device *dev)
 
 #endif
 
-#ifdef CONFIG_GPIO_MCUX_LPC_PORT0
-	const u32_t port0_pin5_config = (
+#ifdef DT_GPIO_KEYS_SW0_GPIO_CONTROLLER
+	const u32_t sw0_config = (
 			IOCON_PIO_FUNC0 |
 			IOCON_PIO_MODE_PULLUP |
 			IOCON_PIO_INV_DI |
@@ -58,12 +58,11 @@ static int lpcxpresso_55s69_pinmux_init(struct device *dev)
 			IOCON_PIO_SLEW_STANDARD |
 			IOCON_PIO_OPENDRAIN_DI
 			);
-
-	pinmux_pin_set(port0, 5, port0_pin5_config);
+	pinmux_pin_set(port0, DT_ALIAS_SW0_GPIOS_PIN, sw0_config);
 #endif
 
-#ifdef CONFIG_GPIO_MCUX_LPC_PORT0
-	const u32_t port1_pin18_config = (
+#ifdef DT_GPIO_KEYS_SW1_GPIO_CONTROLLER
+	const u32_t sw1_config = (
 			IOCON_PIO_FUNC0 |
 			IOCON_PIO_MODE_PULLUP |
 			IOCON_PIO_INV_DI |
@@ -72,10 +71,11 @@ static int lpcxpresso_55s69_pinmux_init(struct device *dev)
 			IOCON_PIO_SLEW_STANDARD |
 			IOCON_PIO_OPENDRAIN_DI
 			);
+	pinmux_pin_set(port1, DT_ALIAS_SW0_GPIOS_PIN, sw1_config);
+#endif
 
-	pinmux_pin_set(port1, 18, port1_pin18_config);
-
-	const u32_t port1_pin9_config = (
+#ifdef DT_GPIO_KEYS_SW2_GPIO_CONTROLLER
+	const u32_t sw2_config = (
 			IOCON_PIO_FUNC0 |
 			IOCON_PIO_MODE_PULLUP |
 			IOCON_PIO_INV_DI |
@@ -84,8 +84,7 @@ static int lpcxpresso_55s69_pinmux_init(struct device *dev)
 			IOCON_PIO_SLEW_STANDARD |
 			IOCON_PIO_OPENDRAIN_DI
 			);
-
-	pinmux_pin_set(port1, 9, port1_pin9_config);
+	pinmux_pin_set(port1, DT_ALIAS_SW0_GPIOS_PIN, sw2_config);
 #endif
 
 	return 0;
