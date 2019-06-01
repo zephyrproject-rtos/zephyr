430d9eddf4f213417a2d7b213959f86b1a293b56
Marti Bolivar <marti.bolivar@nordicsemi.no>
2019-05-31 18:00:47 -0600

    boards: nrf52_sparkfun: fix board.cmake
    This can't possibly have worked since the initial merge of the board.
    
    It looks like it was originally written as a port from a KBuild
    Makefile fragment but never tested, and has only been touched by
    tree-wide changes since then. Try to fix it.
    
    I don't have this hardware, but it should work the same way as
    96b_nitrogen if it truly supports pyocd.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@nordicsemi.no>

--- boards/arm/nrf52_sparkfun/board.cmake
+++ boards/arm/nrf52_sparkfun/board.cmake
@@ -1,10 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_set_flasher_ifnset(pyocd.sh)
-board_set_debugger_ifnset(pyocd.sh)
-
-set(PYOCD_TARGET nrf52)
-
-set_property(GLOBAL APPEND PROPERTY FLASH_SCRIPT_ENV_VARS
-  PYOCD_TARGET
-  )
+board_runner_args(pyocd "--target=nrf52")
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
