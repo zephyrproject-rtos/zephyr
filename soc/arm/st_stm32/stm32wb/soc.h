fad2624f32c40a5dfeedb9cfa88c1fcf0b586356
Roger N'Guessan <roger.nguessan@st.com>
2019-06-04 12:13:04 +0200

    drivers: i2c: enable I2C on STM32WB
    Enable I2C on STM32WB
    
    Signed-off-by: Roger N'Guessan <roger.nguessan@st.com>

--- soc/arm/st_stm32/stm32wb/soc.h
+++ soc/arm/st_stm32/stm32wb/soc.h
@@ -43,6 +43,10 @@
 #include <stm32wbxx_ll_system.h>
 #endif /* CONFIG_CLOCK_CONTROL_STM32_CUBE */
 
+#ifdef CONFIG_I2C
+#include <stm32wbxx_ll_i2c.h>
+#endif
+
 #endif /* !_ASMLANGUAGE */
 
 #endif /* _STM32WBX_SOC_H_ */
