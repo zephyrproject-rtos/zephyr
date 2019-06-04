912e117e9e5301e696812213aace4a9279700ac5
Asger Munk Nielsen <asmk@oticon.com>
2019-06-04 09:04:54 +0200

    Bluetooth: Controller: Test for user extensions
    With pull request #16208 and #16404 hooks for calling user defined
    code was implemented. These user extensions are gated by a number of
    Kconfigs. The test in this pull request tests that code compiles when
    these Kconfigs are enabled.
    
    Signed-off-by: Asger Munk Nielsen <asmk@oticon.com>

---
+++ tests/bluetooth/ctrl_user_ext/src/main.c
@@ -0,0 +1,27 @@
+/* main.c - Application main entry point */
+
+/*
+ * Copyright (c) 2015-2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <misc/printk.h>
+#include <ztest.h>
+
+#include <bluetooth/bluetooth.h>
+
+void test_ctrl_user_ext(void)
+{
+	zassert_false(bt_enable(NULL), "Bluetooth ctrl_user_ext failed");
+}
+
+/*test case main entry*/
+void test_main(void)
+{
+	ztest_test_suite(test_bluetooth,
+			ztest_unit_test(test_ctrl_user_ext));
+	ztest_run_test_suite(test_bluetooth);
+}
