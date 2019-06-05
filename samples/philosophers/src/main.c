eaf8863bb181b50b6c00f6c74b5948f4cafb5bce
Andrew Boie <andrew.p.boie@intel.com>
2019-06-05 14:20:26 -0700

    samples: philosophers: pause before main() exit
    On some systems the sample was failing validation because
    the coverage data would be emitted before the sample had
    a chance to print anything else.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- samples/philosophers/src/main.c
+++ samples/philosophers/src/main.c
@@ -253,4 +253,11 @@ void main(void)
 
 	init_objects();
 	start_threads();
+
+#ifdef CONFIG_COVERAGE
+	/* Wait a few seconds before main() exit, giving the sample the
+	 * opportunity to dump some output before coverage data gets emitted
+	 */
+	k_sleep(5000);
+#endif
 }
