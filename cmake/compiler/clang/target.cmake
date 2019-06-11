f190f0e9ce4ee029766b118fe4dd184bd6ef9add
Danny Oerndrup <daor@demant.com>
2019-06-11 13:55:53 +0200

    cmake: Toolchain abstraction: Introducing macro toolchain_cc_imacros
    The macro is intended to abstract the -imacros compiler option for
    inclusion of the autoconf.h header file. The abstraction allows for a
    given toolchain to decide how the inclusion of the header file is to
    be done.
    
    The intent here is to abstract Zephyr's dependence on toolchains,
    thus allowing for easier porting to other, perhaps commercial,
    toolchains and/or usecases.
    
    No functional change expected.
    
    Signed-off-by: Danny Oerndrup <daor@demant.com>

--- cmake/compiler/clang/target.cmake
+++ cmake/compiler/clang/target.cmake
@@ -80,6 +80,7 @@ include(${ZEPHYR_BASE}/cmake/compiler/gcc/target_cpp.cmake)
 include(${ZEPHYR_BASE}/cmake/compiler/gcc/target_asm.cmake)
 include(${ZEPHYR_BASE}/cmake/compiler/gcc/target_baremetal.cmake)
 include(${ZEPHYR_BASE}/cmake/compiler/${COMPILER}/target_warnings.cmake)
+include(${ZEPHYR_BASE}/cmake/compiler/gcc/target_imacros.cmake)
 
 macro(toolchain_cc_security_fortify)
   # No op, clang doesn't understand fortify at all
---
+++ cmake/compiler/gcc/target_imacros.cmake
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# See root CMakeLists.txt for description and expectations of these macros
+
+macro(toolchain_cc_imacros header_file)
+
+  zephyr_compile_options(-imacros ${header_file})
+
+endmacro()
