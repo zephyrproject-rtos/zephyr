6712db44b9430edcbf10a98c0c61455fe43630cd
Andrew Boie <andrew.p.boie@intel.com>
2019-06-11 17:49:03 -0700

    tests: queue: test k_queue_alloc_*pend()
    These were never getting called anywhere from user mode,
    except for k_queue_alloc_append(), but only by virtue of
    some workqueue tests.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- tests/kernel/queue/src/main.c
+++ tests/kernel/queue/src/main.c
@@ -17,13 +17,28 @@ static void test_auto_free(void)
 {
 	ztest_test_skip();
 }
+
+static void test_queue_alloc_prepend_user(void)
+{
+	ztest_test_skip();
+}
+
+static void test_queue_alloc_append_user(void)
+{
+	ztest_test_skip();
+}
 #endif
+K_MEM_POOL_DEFINE(test_pool, 16, 96, 4, 4);
 
 /*test case main entry*/
 void test_main(void)
 {
+	k_thread_resource_pool_assign(k_current_get(), &test_pool);
+
 	ztest_test_suite(queue_api,
 			 ztest_unit_test(test_queue_supv_to_user),
+			 ztest_user_unit_test(test_queue_alloc_prepend_user),
+			 ztest_user_unit_test(test_queue_alloc_append_user),
 			 ztest_unit_test(test_auto_free),
 			 ztest_unit_test(test_queue_thread2thread),
 			 ztest_unit_test(test_queue_thread2isr),
