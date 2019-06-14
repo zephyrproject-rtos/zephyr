f15c12d482a9aa1c95daf6ad15c631e78b6a88ad
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2019-06-14 09:08:11 +0200

    arch: arm: expose start/end limits of the thread priv stack section
    We introduce linker symbols to hold the start and end address of
    the memory area holding the thread privilege stack buffers,
    applicable when building with support for User Mode.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- include/linker/linker-defs.h
+++ include/linker/linker-defs.h
@@ -252,6 +252,17 @@ extern char _ramfunc_ram_size[];
 extern char _ramfunc_rom_start[];
 #endif /* CONFIG_ARCH_HAS_RAMFUNC_SUPPORT */
 
+/* Memory owned by the kernel. Memory region for thread privilege stack buffers,
+ * currently only applicable on ARM Cortex-M architecture when building with
+ * support for User Mode.
+ *
+ * All thread privilege stack buffers will be placed into this section.
+ */
+#ifdef CONFIG_USERSPACE
+extern char z_priv_stacks_ram_start[];
+extern char z_priv_stacks_ram_end[];
+#endif /* CONFIG_USERSPACE */
+
 #endif /* ! _ASMLANGUAGE */
 
 #endif /* ZEPHYR_INCLUDE_LINKER_LINKER_DEFS_H_ */
