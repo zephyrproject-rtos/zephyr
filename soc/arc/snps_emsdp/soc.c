81e5a2511b39e61175a5769ff00038422341a0d8
Watson Zeng <zhiwei@synopsys.com>
2019-06-20 15:04:33 +0800

    boards: arc: emsdp: add basic emsdp board support
    * add basic emsdp board support
    
    Signed-off-by: Watson Zeng <zhiwei@synopsys.com>

---
+++ soc/arc/snps_emsdp/soc.c
@@ -0,0 +1,12 @@
+
+/*
+ * Copyright (c) 2019 Synopsys, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * This module provides routines to initialize and support soc-level hardware
+ * for the IoT Development Kit board.
+ *
+ */
