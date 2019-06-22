1f51d7f27e23ad03c86f6ac5ee4ade8a4daa1d09
Peter A. Bigot <pab@pabigot.com>
2019-06-22 16:43:48 -0500

    libc: minimal: provide types in time.h
    Provide POSIX-compatible definitions for a subset of the time types that
    must be provided by this file, in anticipation of supporting civil time
    in Zephyr.
    
    Signed-off-by: Peter A. Bigot <pab@pabigot.com>

--- lib/libc/minimal/include/time.h
+++ lib/libc/minimal/include/time.h
@@ -4,7 +4,41 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#ifndef ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_TIME_H_
+#define ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_TIME_H_
+
+#include <stdint.h>
 
 /* Dummy time.h to fulfill the requirements of certain libraries
- * i.e. mbedTLS
+ * i.e. mbedTLS and Zephyr civil time APIs
  */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct tm {
+	int tm_sec;
+	int tm_min;
+	int tm_hour;
+	int tm_mday;
+	int tm_mon;
+	int tm_year;
+	int tm_wday;
+	int tm_yday;
+	int tm_isdst;
+};
+
+typedef int64_t time_t;
+typedef int32_t suseconds_t;
+
+struct timespec {
+	time_t tv_sec;
+	long tv_nsec;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_STDIO_H_ */
