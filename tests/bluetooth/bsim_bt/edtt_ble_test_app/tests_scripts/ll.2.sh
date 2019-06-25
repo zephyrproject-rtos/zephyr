d7ba3210b1d6ad2bae1f4278ee14bc14b4913547
Alberto Escolar Piedras <alpi@oticon.com>
2019-06-25 15:48:00 +0200

    tests: bsim_bt: Add EDTT based BT stack tests
    Add some scripts to automatically run a set of BT conformance tests.
    Each script has an associated file which selects which subset of tests
    are run by that script.
    The LL scripts are divided in 2 subsets so as to allow parallelizing
    a bit the run (the LL tests take the longest).
    Except these, all other sets are just divided by category.
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>

---
+++ tests/bluetooth/bsim_bt/edtt_ble_test_app/tests_scripts/ll.2.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Copyright 2019 Oticon A/S
+# SPDX-License-Identifier: Apache-2.0
+
+# LL regression tests based on the EDTTool (part 2)
+
+CWD=`pwd`"/"`dirname $0`
+
+export SIMULATION_ID="edtt_ll_set2"
+export TEST_FILE=${CWD}"/ll.set2.test_list"
+export TEST_MODULE="ll_verification"
+
+${CWD}/_controller_tests_inner.sh
