52b3d2b671d29eaac2b0f2ffe4fbaa5d3c9699ca
Anas Nashif <anas.nashif@intel.com>
2019-06-25 12:08:50 -0400

    cleanup: include/: move tty.h to console/tty.h
    move tty.h to console/tty.h and
    create a shim for backward-compatibility.
    
    No functional changes to the headers.
    A warning in the shim can be controlled with CONFIG_COMPAT_INCLUDES.
    
    Related to #16539
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- subsys/console/tty.c
+++ subsys/console/tty.c
@@ -7,7 +7,7 @@
 #include <zephyr.h>
 #include <uart.h>
 #include <misc/printk.h>
-#include <tty.h>
+#include <console/tty.h>
 
 static int tty_irq_input_hook(struct tty_serial *tty, u8_t c);
 static int tty_putchar(struct tty_serial *tty, u8_t c);
