310bdb3216b6a90fea87c79d3101e1ba1a356cfc
Charles E. Youse <charles.youse@intel.com>
2019-06-26 10:28:41 -0700

    cmake: move compiler-specific flags to compiler/gcc
    The top-level CMakeLists.txt sets compiler flags that are specific
    to GCC (or compilers that are broadly compatible with GCC's flags).
    These are moved to compiler/gcc/target_base.cmake so they don't
    contaminate other toolchains.
    
    There is a project underway to abstract the toolchain, and this is not
    intended to collide with that work. This solution is not intended to
    be the final form for the treatment of this specific set of flags.
    This patch is stop-gap measure to allow work on non-GCC toolchains
    to proceed while awaiting the final abstractions from those engaged
    in that work.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

--- cmake/compiler/gcc/target_base.cmake
+++ cmake/compiler/gcc/target_base.cmake
@@ -18,3 +18,52 @@ macro(toolchain_cc_cstd_flag dest_var_name c_std)
   set_ifndef(${dest_var_name} "-std=${c_std}")
 
 endmacro()
+
+#
+# The two macros are dumping grounds for toolchain-specific options
+# that were previously in the top-level CMakeLists.txt. This is merely
+# a stopgap until the toolchain abstraction work underway (by others)
+# is complete. (There are two of these to preserve the original order.)
+#
+
+macro(toolchain_cc_others_1)
+  zephyr_cc_option(-fno-asynchronous-unwind-tables)
+  zephyr_cc_option(-fno-pie)
+  zephyr_cc_option(-fno-pic)
+  zephyr_cc_option(-fno-strict-overflow)
+
+  if(CONFIG_OVERRIDE_FRAME_POINTER_DEFAULT)
+    if(CONFIG_OMIT_FRAME_POINTER)
+      zephyr_cc_option(-fomit-frame-pointer)
+    else()
+      zephyr_cc_option(-fno-omit-frame-pointer)
+    endif()
+  endif()
+endmacro()
+
+macro(toolchain_cc_others_2)
+  if(NOT CMAKE_C_COMPILER_ID STREQUAL "Clang")
+    zephyr_cc_option(-fno-reorder-functions)
+
+    if(NOT ${ZEPHYR_TOOLCHAIN_VARIANT} STREQUAL "xcc")
+      zephyr_cc_option(-fno-defer-pop)
+    endif()
+  endif()
+
+  zephyr_cc_option_ifdef(CONFIG_STACK_USAGE -fstack-usage)
+
+  # Strip the ${ZEPHYR_BASE} prefix from the __FILE__ macro used in __ASSERT*,
+  # in the .noinit."/home/joe/zephyr/fu/bar.c" section names and in any
+  # application code. This saves some memory, stops leaking user locations
+  # in binaries, makes failure logs more deterministic and most importantly
+  # makes builds more deterministic.
+
+  # If both match then the last one wins. This matters for tests/ and
+  # samples/ inside *both* CMAKE_SOURCE_DIR and ZEPHYR_BASE: for them
+  # let's strip the shortest prefix.
+
+  zephyr_cc_option(-fmacro-prefix-map=${CMAKE_SOURCE_DIR}=CMAKE_SOURCE_DIR)
+  zephyr_cc_option(-fmacro-prefix-map=${ZEPHYR_BASE}=ZEPHYR_BASE)
+
+  # TODO: -fmacro-prefix-map=modules/etc. "build/zephyr_modules.txt" might help.
+endmacro()
