0bed1e9ab74555e286a065855629fa2bb589bd43
Kumar Gala <kumar.gala@linaro.org>
2019-06-26 16:00:58 -0500

    boards: cc3220sf_launchxl: Use SDK OpenOCD
    The zephyr SDK 0.10.1 works well for the CC3220 so lets use it.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- boards/arm/cc3220sf_launchxl/board.cmake
+++ boards/arm/cc3220sf_launchxl/board.cmake
@@ -1,8 +1,3 @@
 # SPDX-License-Identifier: Apache-2.0
 
-# Use the TI OpenOCD (by default in /usr/local/openocd)
-# See the Zephyr project CC3220SF_LAUNCHXL documentation on
-# flashing prerequisites.
-set(OPENOCD "/usr/local/bin/openocd" CACHE FILEPATH "" FORCE)
-set(OPENOCD_DEFAULT_PATH ${OPENOCD}/scripts)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
