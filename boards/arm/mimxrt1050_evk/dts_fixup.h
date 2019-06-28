99b83c74d8f84d74286bea24b0f36f5134b6ed7e
Antoine <antoine@riedonetworks.com>
2019-06-28 12:24:47 +0200

    drivers: mimxrt1050 flash driver: Made driver working on tip again.
    Signed-off-by: Antoine <antoine@riedonetworks.com>

--- boards/arm/mimxrt1050_evk/dts_fixup.h
+++ boards/arm/mimxrt1050_evk/dts_fixup.h
@@ -1,4 +1,4 @@
-#define DT_FLASH_DEV_NAME	DT_NXP_IMX_FLEXSPI_402A8000_LABEL
+#define DT_FLASH_DEV_NAME	DT_NXP_IMX_FLEXSPI_402A8000_CYPRESS_S26KL_0_LABEL
 
 // Temporary fix-up until this goes to dts file
 
