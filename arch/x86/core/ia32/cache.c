4394a4abea2e36cc070d34d6100d44e6cee5e8c2
Charles E. Youse <charles.youse@intel.com>
2019-06-29 12:43:18 -0700

    arch/x86: eliminate cache_private.h
    This file merely declares external functions referenced only
    by ia32/cache.c, so the declarations are inlined instead.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

--- arch/x86/core/ia32/cache.c
+++ arch/x86/core/ia32/cache.c
@@ -15,8 +15,16 @@
 #include <sys/util.h>
 #include <toolchain.h>
 #include <cache.h>
-#include <cache_private.h>
 #include <stdbool.h>
+#include <cache.h>
+
+/*
+ * these functions are defined in cache_s.S
+ */
+
+extern int z_is_clflush_available(void);
+extern void z_cache_flush_wbinvd(vaddr_t addr, size_t len);
+extern size_t z_cache_line_size_get(void);
 
 #if defined(CONFIG_CLFLUSH_INSTRUCTION_SUPPORTED) || \
 	defined(CONFIG_CLFLUSH_DETECT)
