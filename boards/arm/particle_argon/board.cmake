f39f234d07bc7e333c1191457eae6dc6e69bb9eb
Henrik Brix Andersen <henrik@brixandersen.dk>
2019-06-30 23:41:01 +0200

    boards: match specified jlink swd speed when using pyocd runner
    Match the speed speficied for all boards using the jlink runner when
    using the pyocd runner on the same board.
    
    Signed-off-by: Henrik Brix Andersen <henrik@brixandersen.dk>

--- boards/arm/particle_argon/board.cmake
+++ boards/arm/particle_argon/board.cmake
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(pyocd "--target=nrf52840")
+board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
 board_runner_args(nrfjprog "--nrf-family=NRF52")
 board_runner_args(jlink "--device=nrf52" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
