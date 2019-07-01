ef35fbcf42bc626e006176243cbc4fbfb8c4eb6a
Erwan Gouriou <erwan.gouriou@linaro.org>
2019-07-01 14:05:55 +0200

    soc/arm: Provide basic support for STM32H7 series
    Enable basic support to STM32H7, in single core configuration (M7).
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

---
+++ soc/arm/st_stm32/stm32h7/dts_fixup.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* SoC level DTS fixup file */
+
+#define DT_NUM_IRQ_PRIO_BITS		DT_ARM_V7M_NVIC_E000E100_ARM_NUM_IRQ_PRIORITY_BITS
+
+#define DT_NUM_MPU_REGIONS		DT_ARM_ARMV7M_MPU_E000ED90_ARM_NUM_MPU_REGIONS
+
+/* End of SoC Level DTS fixup file */
---
+++ soc/arm/st_stm32/stm32h7/soc.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _STM32F7_SOC_H_
+#define _STM32F7_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <stm32h7xx.h>
+
+/* ARM CMSIS definitions must be included before kernel_includes.h.
+ * Therefore, it is essential to include kernel_includes.h after including
+ * core SOC-specific headers.
+ */
+#include <kernel_includes.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _STM32F7_SOC_H7_ */
