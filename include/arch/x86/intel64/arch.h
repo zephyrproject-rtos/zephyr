b12a09fe8e74fa6a47e7017b7630ebee4f25922e
Charles E. Youse <charles.youse@intel.com>
2019-07-04 11:27:20 -0700

    arch/x86: refactor arch.h
    Some of the elements of arch.h can be shared between subarches,
    so put them in a common file and factor out the rest. Placeholder
    left for the Intel64 definitions to be added later.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

---
+++ include/arch/x86/intel64/arch.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2019 Intel Corp.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_ARCH_X86_INTEL64_ARCH_H_
+#define ZEPHYR_INCLUDE_ARCH_X86_INTEL64_ARCH_H_
+
+#endif /* ZEPHYR_INCLUDE_ARCH_X86_INTEL64_ARCH_H_ */
