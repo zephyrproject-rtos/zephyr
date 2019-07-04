ec59a1fc73a11aa1b8ebe2471cd649e6668070ee
Charles E. Youse <charles.youse@intel.com>
2019-07-04 15:39:55 -0700

    arch/x86: share implementations of sys_io primitives
    The sys_inXX() and sys_read/writeXX() I/O primitives can be shared
    between subarches of x86 with minor modifications, so move them.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

--- include/arch/x86/arch.h
+++ include/arch/x86/arch.h
@@ -10,6 +10,125 @@
 #include <stdbool.h>
 #include <irq.h>
 
+#if !defined(_ASMLANGUAGE)
+
+#include <sys/sys_io.h>
+#include <zephyr/types.h>
+#include <stddef.h>
+
+static ALWAYS_INLINE void sys_out8(u8_t data, io_port_t port)
+{
+	__asm__ volatile("outb	%b0, %w1;\n\t"
+			 :
+			 : "a"(data), "Nd"(port));
+}
+
+static ALWAYS_INLINE u8_t sys_in8(io_port_t port)
+{
+	u8_t ret;
+
+	__asm__ volatile("inb	%w1, %b0;\n\t"
+			 : "=a"(ret)
+			 : "Nd"(port));
+	return ret;
+}
+
+static ALWAYS_INLINE void sys_out16(u16_t data, io_port_t port)
+{
+	__asm__ volatile("outw	%w0, %w1;\n\t"
+			 :
+			 : "a"(data), "Nd"(port));
+}
+
+static ALWAYS_INLINE u16_t sys_in16(io_port_t port)
+{
+	u16_t ret;
+
+	__asm__ volatile("inw	%w1, %w0;\n\t"
+			 : "=a"(ret)
+			 : "Nd"(port));
+	return ret;
+}
+
+static ALWAYS_INLINE void sys_out32(u32_t data, io_port_t port)
+{
+	__asm__ volatile("outl	%0, %w1;\n\t"
+			 :
+			 : "a"(data), "Nd"(port));
+}
+
+static ALWAYS_INLINE u32_t sys_in32(io_port_t port)
+{
+	u32_t ret;
+
+	__asm__ volatile("inl	%w1, %0;\n\t"
+			 : "=a"(ret)
+			 : "Nd"(port));
+	return ret;
+}
+
+static ALWAYS_INLINE void sys_write8(u8_t data, mm_reg_t addr)
+{
+	__asm__ volatile("movb	%0, %1;\n\t"
+			 :
+			 : "q"(data), "m" (*(volatile u8_t *)(uintptr_t) addr)
+			 : "memory");
+}
+
+static ALWAYS_INLINE u8_t sys_read8(mm_reg_t addr)
+{
+	u8_t ret;
+
+	__asm__ volatile("movb	%1, %0;\n\t"
+			 : "=q"(ret)
+			 : "m" (*(volatile u8_t *)(uintptr_t) addr)
+			 : "memory");
+
+	return ret;
+}
+
+static ALWAYS_INLINE void sys_write16(u16_t data, mm_reg_t addr)
+{
+	__asm__ volatile("movw	%0, %1;\n\t"
+			 :
+			 : "r"(data), "m" (*(volatile u16_t *)(uintptr_t) addr)
+			 : "memory");
+}
+
+static ALWAYS_INLINE u16_t sys_read16(mm_reg_t addr)
+{
+	u16_t ret;
+
+	__asm__ volatile("movw	%1, %0;\n\t"
+			 : "=r"(ret)
+			 : "m" (*(volatile u16_t *)(uintptr_t) addr)
+			 : "memory");
+
+	return ret;
+}
+
+static ALWAYS_INLINE void sys_write32(u32_t data, mm_reg_t addr)
+{
+	__asm__ volatile("movl	%0, %1;\n\t"
+			 :
+			 : "r"(data), "m" (*(volatile u32_t *)(uintptr_t) addr)
+			 : "memory");
+}
+
+static ALWAYS_INLINE u32_t sys_read32(mm_reg_t addr)
+{
+	u32_t ret;
+
+	__asm__ volatile("movl	%1, %0;\n\t"
+			 : "=r"(ret)
+			 : "m" (*(volatile u32_t *)(uintptr_t) addr)
+			 : "memory");
+
+	return ret;
+}
+
+#endif /* _ASMLANGUAGE */
+
 #ifdef CONFIG_X86_LONGMODE
 #include <arch/x86/intel64/arch.h>
 #else
