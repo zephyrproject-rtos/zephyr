b5ead453b289be2c431a2aeab7926ca74774a5f9
Charles E. Youse <charles.youse@intel.com>
2019-07-04 11:32:10 -0700

    arch/x86: add register definitions to msr.h
    Add definitions for the EFER MSR.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

--- include/arch/x86/msr.h
+++ include/arch/x86/msr.h
@@ -10,17 +10,20 @@
  * Model specific registers (MSR).  Access with z_x86_msr_read/write().
  */
 
-#define X86_SPEC_CTRL_MSR		0x0048
+#define X86_SPEC_CTRL_MSR		0x00000048
 #define X86_SPEC_CTRL_MSR_IBRS		BIT(0)
 #define X86_SPEC_CTRL_MSR_SSBD		BIT(2)
 
-#define X86_APIC_BASE_MSR		0x001b
+#define X86_APIC_BASE_MSR		0x0000001b
 #define X86_APIC_BASE_MSR_X2APIC	BIT(10)
 
-#define X86_MTRR_DEF_TYPE_MSR		0x02ff
+#define X86_MTRR_DEF_TYPE_MSR		0x000002ff
 #define X86_MTRR_DEF_TYPE_MSR_ENABLE	BIT(11)
 
-#define X86_X2APIC_BASE_MSR		0x0800	/* 0x0800-0x0BFF -> x2APIC */
+#define X86_X2APIC_BASE_MSR		0x00000800 /* .. thru 0x00000BFF */
+
+#define X86_EFER_MSR			0xC0000080
+#define X86_EFER_MSR_LME		BIT(8)
 
 #ifndef _ASMLANGUAGE
 #ifdef __cplusplus
