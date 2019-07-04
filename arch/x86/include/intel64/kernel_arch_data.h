4f10b28bfc9879cc1f9549a32a78521721ff663a
Charles E. Youse <charles.youse@intel.com>
2019-07-04 12:10:49 -0700

    arch/x86: refactor kernel_arch_data.h
    Some definitions may be shared between subarchitectures, so refactor
    accordingly. The definitions are also modified to separate bits. A
    placeholder is created for the Intel64 definitions.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

---
+++ arch/x86/include/intel64/kernel_arch_data.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2019 Intel Corp.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_ARCH_X86_INCLUDE_INTEL64_KERNEL_ARCH_DATA_H_
+#define ZEPHYR_ARCH_X86_INCLUDE_INTEL64_KERNEL_ARCH_DATA_H_
+
+#endif /* ZEPHYR_ARCH_X86_INCLUDE_INTEL64_KERNEL_ARCH_DATA_H_ */
