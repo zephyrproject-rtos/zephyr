9b00f7821ac7f0d79252f47d53e8f26a2fffa62f
Charles E. Youse <charles.youse@intel.com>
2019-07-05 12:02:49 -0700

    arch/x86: rework z_arch_irq_lock() and z_arch_irq_unlock()
    Simplified these two inline functions somewhat, and refactored them:
    unlocking can be shared between subarches, but locking cannot.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

--- include/arch/x86/intel64/arch.h
+++ include/arch/x86/intel64/arch.h
@@ -3,12 +3,34 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#ifndef ZEPHYR_INCLUDE_ARCH_X86_INTEL64_ARCH_H_
+#define ZEPHYR_INCLUDE_ARCH_X86_INTEL64_ARCH_H_
+
+#include <kernel_arch_thread.h>
+
+#ifndef _ASMLANGUAGE
+
+static ALWAYS_INLINE unsigned int z_arch_irq_lock(void)
+{
+	unsigned long key;
+
+	__asm__ volatile (
+		"pushfq\n\t"
+		"cli\n\t"
+		"popq %0\n\t"
+		: "=g" (key)
+		:
+		: "memory"
+		);
+
+	return (unsigned int) key;
+}
+
+#endif /* _ASMLANGUAGE */
+
 /*
  * dummies for now
  */
-
-#ifndef ZEPHYR_INCLUDE_ARCH_X86_INTEL64_ARCH_H_
-#define ZEPHYR_INCLUDE_ARCH_X86_INTEL64_ARCH_H_
 
 #define Z_ARCH_THREAD_STACK_DEFINE(sym, size) \
 	struct _k_thread_stack_element sym[size]
@@ -18,9 +40,6 @@
 
 #define Z_IRQ_TO_INTERRUPT_VECTOR(irq)	(0)
 #define Z_ARCH_IRQ_CONNECT(irq_p, priority_p, isr_p, isr_param_p, flags_p)
-
-#define z_arch_irq_lock()		(0)
-#define z_arch_irq_unlock(k)
 
 #define z_x86_msr_read(a)		(0)
 
