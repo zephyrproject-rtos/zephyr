ffbbec7a89ca3e1bea2d646ae92d3c36fd0739c8
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2019-07-08 17:49:44 +0530

    Bluetooth: controller: Fix DLE during data transmission
    Fix bug in Data Length Update procedure that caused the
    connection to drop due to the implementation sending bigger
    PDU before the peer has acknowledged the receipt of Length
    Response PDU.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ctrl_internal.h
+++ subsys/bluetooth/controller/ll_sw/ctrl_internal.h
@@ -241,12 +241,14 @@ struct connection {
 	struct {
 		u8_t  req;
 		u8_t  ack;
-		u8_t  state:2;
-#define LLCP_LENGTH_STATE_REQ        0
-#define LLCP_LENGTH_STATE_ACK_WAIT   1
-#define LLCP_LENGTH_STATE_RSP_WAIT   2
-#define LLCP_LENGTH_STATE_RESIZE     3
-		u8_t  pause_tx:1;
+		u8_t  state:3;
+#define LLCP_LENGTH_STATE_REQ                 0
+#define LLCP_LENGTH_STATE_REQ_ACK_WAIT        1
+#define LLCP_LENGTH_STATE_RSP_WAIT            2
+#define LLCP_LENGTH_STATE_RSP_ACK_WAIT        3
+#define LLCP_LENGTH_STATE_RESIZE              4
+#define LLCP_LENGTH_STATE_RESIZE_RSP          5
+#define LLCP_LENGTH_STATE_RESIZE_RSP_ACK_WAIT 6
 		u16_t rx_octets;
 		u16_t tx_octets;
 #if defined(CONFIG_BT_CTLR_PHY)
