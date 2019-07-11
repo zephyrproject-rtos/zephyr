66f0ebe79d58dbfa8db4694acf7a40808856370e
Peter A. Bigot <pab@pabigot.com>
2019-07-11 10:31:58 -0500

    libc: minimal: provide types in time.h
    Provide definitions for a subset of the standard time types that must be
    provided by this file, in anticipation of supporting civil time in
    Zephyr.
    
    Signed-off-by: Peter A. Bigot <pab@pabigot.com>

--- lib/libc/minimal/include/time.h
+++ lib/libc/minimal/include/time.h
@@ -1,10 +1,45 @@
 /*
  * Copyright (c) 2017 Intel Corporation
+ * Copyright (c) 2019 Peter Bigot Consulting, LLC
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#ifndef ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_TIME_H_
+#define ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_TIME_H_
 
-/* Dummy time.h to fulfill the requirements of certain libraries
- * i.e. mbedTLS
+#include <stdint.h>
+
+/* Minimal time.h to fulfill the requirements of certain libraries
+ * like mbedTLS and to support time APIs.
  */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct tm {
+	int tm_sec;
+	int tm_min;
+	int tm_hour;
+	int tm_mday;
+	int tm_mon;
+	int tm_year;
+	int tm_wday;
+	int tm_yday;
+	int tm_isdst;
+};
+
+typedef int64_t time_t;
+typedef int32_t suseconds_t;
+
+struct timespec {
+	time_t tv_sec;
+	long tv_nsec;
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_STDIO_H_ */
