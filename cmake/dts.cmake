bc87cd9bf03d83c8657957209e8e906248331671
Robert Lubos <robert.lubos@nordicsemi.no>
2019-07-11 19:17:57 -0600

    cmake: dts fixups
    Trivial dts.cmake cleanups.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- cmake/dts.cmake
+++ cmake/dts.cmake
@@ -32,6 +32,9 @@ list(APPEND
   ${BOARD_DIR}
   ${ZEPHYR_BASE}
   )
+list(REMOVE_DUPLICATES
+  DTS_ROOT
+  )
 
 list(REMOVE_DUPLICATES DTS_ROOT)
 
@@ -73,6 +76,7 @@ if(SUPPORTS_DTS)
     math(EXPR i "${i}+1")
   endforeach()
 
+  unset(DTS_ROOT_SYSTEM_INCLUDE_DIRS)
   foreach(dts_root ${DTS_ROOT})
     foreach(dts_root_path
         include
@@ -90,6 +94,7 @@ if(SUPPORTS_DTS)
     endforeach()
   endforeach()
 
+  unset(DTS_ROOT_BINDINGS)
   foreach(dts_root ${DTS_ROOT})
     set(full_path ${dts_root}/dts/bindings)
     if(EXISTS ${full_path})
