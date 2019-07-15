f1dfedec19d0483f4a59ed6a5101383d735112ff
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2019-07-15 10:49:35 +0200

    boards: arm: mps2_an521: adding support for qemu
    This commit adds support for QEMU on board
    mps2_an521 (ARM Cortex-M33).
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

---
+++ boards/arm/mps2_an521/board.cmake
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: Apache-2.0
+
+set(EMU_PLATFORM qemu)
+
+set(QEMU_CPU_TYPE_${ARCH} cortex-m33)
+set(QEMU_FLAGS_${ARCH}
+  -cpu ${QEMU_CPU_TYPE_${ARCH}}
+  -machine mps2-an521
+  -nographic
+  -m 16
+  -vga none
+  )
+
+board_set_debugger_ifnset(qemu)
