fa47457fedc91b021b510292ee14a83ffa168322
Charles E. Youse <charles.youse@intel.com>
2019-07-22 15:49:25 -0700

    boards/x86: add Intel64 (64-bit long-mode) QEMU target board
    Add qemu_x86_long board (with CONFIG_X86_LONGMODE=y) for testing.
    
    This requires adding support to soc/ia32 for long mode (trivial),
    and adding a quick 64- to 32-bit ELF conversion before invoking
    QEMU, which apparently doesn't like 64-bit kernel files.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

--- cmake/emu/qemu.cmake
+++ cmake/emu/qemu.cmake
@@ -224,6 +224,17 @@ if(CONFIG_X86_IAMCU)
     )
 endif()
 
+if(CONFIG_X86_LONGMODE)
+  # QEMU doesn't like 64-bit ELF files. Since we don't use any >4GB
+  # addresses, converting it to 32-bit is safe enough for emulation.
+  list(APPEND PRE_QEMU_COMMANDS
+    COMMAND
+    ${CMAKE_OBJCOPY}
+    -O elf32-i386
+    $<TARGET_FILE:${logical_target_for_zephyr_elf}>
+    )
+endif()
+
 if(CONFIG_X86_64)
   set(QEMU_KERNEL_FILE "${CMAKE_BINARY_DIR}/zephyr-qemu.elf")
 endif()
