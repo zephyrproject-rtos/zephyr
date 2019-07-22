7c144654a7b6a3bbf19a78554700d3abd026bcc0
Bradley Bolen <bbolen@lexmark.com>
2019-07-22 10:23:47 -0400

    soc: arm: xilinx_zynqmp: Add qemu based SoC
    This commit adds support for the Zynq UltraScale+ MPSoC as a qemu based
    platform for Cortex-R based testing.  This SoC only supports an
    interrupt controller and serial port for limited testing.
    
    Signed-off-by: Bradley Bolen <bbolen@lexmark.com>

---
+++ soc/arm/xilinx_zynqmp/soc.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2019 Lexmark International, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef _BOARD__H_
+#define _BOARD__H_
+
+#include <misc/util.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _ASMLANGUAGE
+
+#include <device.h>
+#include <misc/util.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _BOARD__H_ */
