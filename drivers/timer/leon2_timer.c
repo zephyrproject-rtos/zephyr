d02c91668df68024b41c37cba7c92b92210c19d4
Yasushi SHOJI <y-shoji@ispace-inc.com>
2019-07-24 14:07:40 +0900

    arch: sparc: clock: Use prescaler
    LEON2 timer counter is only 24 bits.  The bit length along with system
    clock of 50 MHz is not sufficient to be used for 500 ms busy wait
    which we do in tests/kernel/common/.
    
    Use prescaler to make 4 times longer.  This yields 1340 ms or 1.34 sec
    duration and 80 ns resolution, instead of the original 335 ms duration
    and 20 ns resolution.
    
    Signed-off-by: Yasushi SHOJI <y-shoji@ispace-inc.com>

--- drivers/timer/leon2_timer.c
+++ drivers/timer/leon2_timer.c
@@ -5,8 +5,18 @@
  */
 
 /*
- * GPTimer has two timers, timer 1 and timer 2.  We use timer 1 as the
+ * LEON2 has two timers, timer 1 and timer 2.  We use timer 1 as the
  * system tick timer and timer 2 as a HPET.
+ *
+ * LEON2 Timer unit only has 24 bit counters, which means, in case of
+ * 50 MHz system clock, the timer resolution is 20 ns and rollover
+ * time is about 335 ms.  335 ms is a bit too short; the boot delay
+ * value for tests/kernel/common/ is 500 ms.  We use 12.5 MHz as the
+ * default value of CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC along with a
+ * prescaler value of 4.  The actual value to write to the register
+ * file is 3 because the timer unit reloads the value when
+ * underflows).  This configuration yields 1340 ms or 1.34 sec
+ * duration and 80 ns resolution.
  */
 
 #include <device.h>
@@ -20,13 +30,11 @@
 #define TIMER_RL (BIT(1))
 #define TIMER_LD (BIT(2))
 
-/* NOTE: The datasheet for AT697 specifies that it has full 32 bits
- * for both counter register and reload register, but some
- * implementation of QEMU SPARC doesn't handle all 32 bits but masks
- * counter and reload register with 0x00ffffff, which is, in case of
- * 50 MHz system clock, the timer resolution is 20 ns and rollover
- * time is about 335 ms.  */
+#define SYSTEM_CLOCK_IN_HZ 50000000 /* 50 MHz */
+
 #define TIMER_RELOAD_VALUE (0x00ffffff)
+#define TIMER_PRESCALER_VALUE                                                  \
+	((SYSTEM_CLOCK_IN_HZ / CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC) - 1)
 
 uint32_t z_timer_cycle_get_32(void)
 {
@@ -50,10 +58,8 @@ static void timer_handler(int irq, void *arg)
 
 int z_clock_driver_init(struct device *device)
 {
-	/* We don't use prescaler for now.  Timers count down at the
-	 * speed of the system clock */
-	sys_write32(0, LEON2_PSCL_RLD);
-	sys_write32(0, LEON2_PSCL_CTR);
+	/* Set prescaler */
+	sys_write32(TIMER_PRESCALER_VALUE, LEON2_PSCL_RLD);
 
 	if (IS_ENABLED(CONFIG_SYS_CLOCK_EXISTS)) {
 		int ticks;
@@ -61,7 +67,7 @@ int z_clock_driver_init(struct device *device)
 		IRQ_CONNECT(TIMER1_IRQ, 0, timer_handler, NULL, 0);
 		irq_enable(TIMER1_IRQ);
 
-		/* GPTimer triggers at underflow (-1) */
+		/* LEON2 Timer triggers when it underflows (-1) */
 		ticks = k_ticks_to_cyc_floor32(1) - 1;
 		sys_write32(ticks, LEON2_TIMER1_RLD);
 		sys_write32(TIMER_EN | TIMER_RL | TIMER_LD, LEON2_TIMER1_CTL);
