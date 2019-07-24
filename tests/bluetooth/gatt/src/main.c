77dd3220899d4eda9f5f834fbb81bcfc7e0f45eb
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2019-07-24 17:41:20 +0300

    Bluetooth: tests: Use loopback connection to test GATT client
    This makes use of the loopback connection test bt_gatt APIs.
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

--- tests/bluetooth/gatt/src/main.c
+++ tests/bluetooth/gatt/src/main.c
@@ -14,6 +14,26 @@
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/gatt.h>
 
+/* Semaphore to test when exchange MTU completes  */
+static K_SEM_DEFINE(exch_sem, 0, 1);
+
+/* Semaphore to test when discover completes  */
+static K_SEM_DEFINE(disc_sem, 0, 1);
+
+/* Semaphore to test when read completes  */
+static K_SEM_DEFINE(read_sem, 0, 1);
+
+/* Semaphore to test when write completes  */
+static K_SEM_DEFINE(write_sem, 0, 1);
+
+/* Semaphore to test when a notification is received */
+static K_SEM_DEFINE(nfy_sem, 0, 1);
+
+/* Semaphore to test when a confirmation is received */
+static K_SEM_DEFINE(ind_sem, 0, 1);
+
+static struct bt_conn *conn;
+
 /* Custom Service Variables */
 static struct bt_uuid_128 test_uuid = BT_UUID_INIT_128(
 	0xf0, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12,
@@ -32,12 +52,24 @@ static const struct bt_uuid_128 test1_nfy_uuid = BT_UUID_INIT_128(
 	0xf5, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12,
 	0x78, 0x56, 0x34, 0x12, 0x78, 0x56, 0x34, 0x12);
 
-static struct bt_gatt_ccc_cfg test1_ccc_cfg[BT_GATT_CCC_MAX] = {};
+static const struct bt_uuid_128 test1_ind_uuid = BT_UUID_INIT_128(
+	0xf6, 0xde, 0xbc, 0x9a, 0x78, 0x56, 0x34, 0x12,
+	0x78, 0x56, 0x34, 0x12, 0x78, 0x56, 0x34, 0x12);
+
+static struct bt_gatt_ccc_cfg test1_nfy_cfg[BT_GATT_CCC_MAX] = {};
 static u8_t nfy_enabled;
 
-static void test1_ccc_cfg_changed(const struct bt_gatt_attr *attr, u16_t value)
+static void test1_nfy_cfg_changed(const struct bt_gatt_attr *attr, u16_t value)
 {
 	nfy_enabled = (value == BT_GATT_CCC_NOTIFY) ? 1 : 0;
+}
+
+static struct bt_gatt_ccc_cfg test1_ind_cfg[BT_GATT_CCC_MAX] = {};
+static u8_t ind_enabled;
+
+static void test1_ind_cfg_changed(const struct bt_gatt_attr *attr, u16_t value)
+{
+	ind_enabled = (value == BT_GATT_CCC_INDICATE) ? 1 : 0;
 }
 
 static ssize_t read_test(struct bt_conn *conn, const struct bt_gatt_attr *attr,
@@ -69,9 +101,9 @@ static struct bt_gatt_attr test_attrs[] = {
 	BT_GATT_PRIMARY_SERVICE(&test_uuid),
 
 	BT_GATT_CHARACTERISTIC(&test_chrc_uuid.uuid,
-			       BT_GATT_CHRC_READ | BT_GATT_CHRC_WRITE,
-			       BT_GATT_PERM_READ_AUTHEN |
-			       BT_GATT_PERM_WRITE_AUTHEN,
+			       BT_GATT_CHRC_READ | BT_GATT_CHRC_WRITE |
+			       BT_GATT_CHRC_WRITE_WITHOUT_RESP,
+			       BT_GATT_PERM_READ | BT_GATT_PERM_WRITE,
 			       read_test, write_test, test_value),
 };
 
@@ -84,7 +116,12 @@ static struct bt_gatt_attr test1_attrs[] = {
 	BT_GATT_CHARACTERISTIC(&test1_nfy_uuid.uuid,
 			       BT_GATT_CHRC_NOTIFY, BT_GATT_PERM_NONE,
 			       NULL, NULL, &nfy_enabled),
-	BT_GATT_CCC(test1_ccc_cfg, test1_ccc_cfg_changed),
+	BT_GATT_CCC(test1_nfy_cfg, test1_nfy_cfg_changed),
+
+	BT_GATT_CHARACTERISTIC(&test1_ind_uuid.uuid,
+			       BT_GATT_CHRC_INDICATE, BT_GATT_PERM_NONE,
+			       NULL, NULL, &ind_enabled),
+	BT_GATT_CCC(test1_ind_cfg, test1_ind_cfg_changed),
 };
 
 static struct bt_gatt_service test1_svc = BT_GATT_SERVICE(test1_attrs);
@@ -167,7 +204,7 @@ void test_gatt_foreach(void)
 
 	/* Iterate attributes */
 	bt_gatt_foreach_attr(test_attrs[0].handle, 0xffff, count_attr, &num);
-	zassert_equal(num, 7, "Number of attributes don't match");
+	zassert_equal(num, 10, "Number of attributes don't match");
 
 	/* Iterate 1 attribute */
 	num = 0;
@@ -200,7 +237,7 @@ void test_gatt_foreach(void)
 	num = 0;
 	bt_gatt_foreach_attr_type(test_attrs[0].handle, 0xffff,
 				  BT_UUID_GATT_CHRC, NULL, 0, count_attr, &num);
-	zassert_equal(num, 2, "Number of attributes don't match");
+	zassert_equal(num, 3, "Number of attributes don't match");
 
 	/* Find 1 characteristic */
 	attr = NULL;
@@ -265,6 +302,348 @@ void test_gatt_write(void)
 			  "Attribute write value don't match");
 }
 
+static void gatt_exchange(struct bt_conn *conn, u8_t err,
+			  struct bt_gatt_exchange_params *parms)
+{
+	zassert_equal(err, 0, "Failed to exchange MTU");
+
+	k_sem_give(&exch_sem);
+}
+
+void test_gatt_exchange_mtu(void)
+{
+	struct bt_gatt_exchange_params params = {
+		.func = gatt_exchange,
+	};
+
+	conn = bt_conn_create_le(BT_ADDR_LE_NONE, BT_LE_CONN_PARAM_DEFAULT);
+
+	zassert_not_null(conn, "Test loopback connection failed");
+
+	/* Attempt to exchange MTU */
+	zassert_false(bt_gatt_exchange_mtu(conn, &params) < 0,
+		     "Failed to exchange MTU");
+
+	zassert_false(k_sem_take(&exch_sem, K_MSEC(100)),
+		      "Exchange func not called within timeout");
+}
+
+static u16_t attr_count;
+
+static u8_t gatt_discover(struct bt_conn *conn,	const struct bt_gatt_attr *attr,
+			  struct bt_gatt_discover_params *params)
+{
+	if (attr) {
+		if (params->type == BT_GATT_DISCOVER_DESCRIPTOR) {
+			zassert_true(bt_uuid_cmp(attr->uuid, BT_UUID_GATT_CHRC),
+				     "Discovered a characteristic with "
+				     "descriptor discovery");
+		}
+		attr_count++;
+		return BT_GATT_ITER_CONTINUE;
+	}
+
+	k_sem_give(&disc_sem);
+
+	return BT_GATT_ITER_STOP;
+}
+
+void test_gatt_discover(void)
+{
+	struct bt_gatt_discover_params params = {
+		.func = gatt_discover,
+	};
+
+	/* Start with test_attr handle */
+	params.start_handle = test_attrs[0].handle;
+	params.end_handle = 0xffff;
+	params.type = BT_GATT_DISCOVER_PRIMARY;
+
+	/* Attempt to discover primary services */
+	zassert_false(bt_gatt_discover(conn, &params) < 0,
+		     "Failed to discover primary services");
+
+	zassert_false(k_sem_take(&disc_sem, K_MSEC(100)),
+		      "Discover func not called within timeout");
+
+	zassert_equal(attr_count, 2,
+		      "Unexpected number of primary services found");
+
+	params.type = BT_GATT_DISCOVER_SECONDARY;
+
+	/* Attempt to discover secondary services */
+	zassert_false(bt_gatt_discover(conn, &params) < 0,
+		     "Failed to discover secondary services");
+
+	zassert_false(k_sem_take(&disc_sem, K_MSEC(100)),
+		      "Discover func not called within timeout");
+
+	zassert_equal(attr_count, 2,
+		      "Unexpected number of secondary services found");
+
+	params.type = BT_GATT_DISCOVER_INCLUDE;
+
+	/* Attempt to discover secondary services */
+	zassert_false(bt_gatt_discover(conn, &params) < 0,
+		     "Failed to discover included services");
+
+	zassert_false(k_sem_take(&disc_sem, K_MSEC(100)),
+		      "Discover func not called within timeout");
+
+	zassert_equal(attr_count, 2,
+		      "Unexpected number of included services found");
+
+	params.type = BT_GATT_DISCOVER_CHARACTERISTIC;
+
+	/* Attempt to discover characteristics */
+	zassert_false(bt_gatt_discover(conn, &params) < 0,
+		     "Failed to discover characteristics");
+
+	zassert_false(k_sem_take(&disc_sem, K_MSEC(100)),
+		      "Discover func not called within timeout");
+
+	zassert_equal(attr_count, 5,
+		      "Unexpected number of characteristics found");
+
+	params.type = BT_GATT_DISCOVER_DESCRIPTOR;
+
+	/* Attempt to discover descriptors */
+	zassert_false(bt_gatt_discover(conn, &params) < 0,
+		     "Failed to discover descriptor");
+
+	zassert_false(k_sem_take(&disc_sem, K_MSEC(100)),
+		      "Discover func not called within timeout");
+
+	zassert_equal(attr_count, 10,
+		      "Unexpected number of descriptors found");
+
+	params.type = BT_GATT_DISCOVER_ATTRIBUTE;
+
+	/* Attempt to discover attributes */
+	zassert_false(bt_gatt_discover(conn, &params) < 0,
+		     "Failed to discover attributes");
+
+	zassert_false(k_sem_take(&disc_sem, K_MSEC(100)),
+		      "Discover func not called within timeout");
+
+	zassert_equal(attr_count, 20,
+		      "Unexpected number of attributes found");
+
+	params.uuid = &test_chrc_uuid.uuid;
+
+	/* Attempt to discover attributes by UUID */
+	zassert_false(bt_gatt_discover(conn, &params) < 0,
+		     "Failed to discover attributes");
+
+	zassert_false(k_sem_take(&disc_sem, K_MSEC(100)),
+		      "Discover func not called within timeout");
+
+	zassert_equal(attr_count, 21,
+		      "Unexpected number of attributes found");
+}
+
+static u8_t gatt_read(struct bt_conn *conn, u8_t err,
+		      struct bt_gatt_read_params *params,
+		      const void *data, u16_t length)
+{
+	if (!data) {
+		k_sem_give(&read_sem);
+		return BT_GATT_ITER_STOP;
+	}
+
+	switch (params->handle_count) {
+	case 0:
+	case 1:
+		zassert_equal(err, 0, "Attribute read return an error");
+		zassert_equal(length, strlen(test_value),
+			      "Attribute read length don't match");
+		zassert_mem_equal(data, test_value, length,
+				  "Attribute value don't match");
+		break;
+	}
+
+	return BT_GATT_ITER_CONTINUE;
+}
+
+void test_gatt_client_read(void)
+{
+	struct bt_gatt_read_params params = {
+		.func = gatt_read,
+	};
+
+	params.handle_count = 1;
+	params.single.handle = test_attrs[2].handle;
+
+	/* Attempt to read attribute */
+	zassert_false(bt_gatt_read(conn, &params) < 0,
+		      "Failed to read attribute");
+
+	zassert_false(k_sem_take(&read_sem, K_MSEC(100)),
+		      "Read func not called within timeout");
+
+	params.handle_count = 0;
+	params.by_uuid.start_handle = test_attrs[2].handle;
+	params.by_uuid.end_handle = 0xffff;
+	params.by_uuid.uuid = &test_chrc_uuid.uuid;
+
+	/* Attempt to read attribute by UUID */
+	zassert_false(bt_gatt_read(conn, &params) < 0,
+		      "Failed to read attribute");
+
+	zassert_false(k_sem_take(&read_sem, K_MSEC(100)),
+		      "Read func not called within timeout");
+}
+
+static void gatt_write(struct bt_conn *conn, u8_t err,
+		       struct bt_gatt_write_params *params)
+{
+	zassert_equal(err, 0, "Attribute write return an error");
+	k_sem_give(&write_sem);
+}
+
+void test_gatt_client_write(void)
+{
+	struct bt_gatt_write_params params = {
+		.func = gatt_write,
+	};
+	char *value = "Test";
+
+	params.handle = test_attrs[2].handle;
+	params.data = value;
+	params.length = strlen(value);
+
+	/* Attempt to write attribute */
+	zassert_false(bt_gatt_write(conn, &params) < 0,
+		      "Failed to write attribute");
+
+	zassert_false(k_sem_take(&write_sem, K_MSEC(100)),
+		      "Write func not called within timeout");
+
+	zassert_equal(strlen(value), strlen(test_value),
+		      "Attribute value length don't match");
+	zassert_mem_equal(value, test_value, strlen(value),
+			  "Attribute value don't match");
+
+	value = "    ";
+
+	/* Attempt to write attribute without response */
+	zassert_false(bt_gatt_write_without_response(conn,
+						     test_attrs[2].handle,
+						     value, strlen(value),
+						     false) < 0,
+		      "Failed to write attribute");
+
+	k_sleep(K_MSEC(100));
+
+	zassert_equal(strlen(value), strlen(test_value),
+		      "Attribute value length don't match");
+	zassert_mem_equal(value, test_value, strlen(value),
+			  "Attribute value don't match");
+}
+
+static u8_t gatt_notify(struct bt_conn *conn,
+			struct bt_gatt_subscribe_params *params,
+			const void *data, u16_t length)
+{
+	zassert_not_null(data, "Attribute unsubscribed");
+	zassert_mem_equal(data, test_value, strlen(test_value),
+			  "Notification data don't match");
+	k_sem_give(&nfy_sem);
+
+	return BT_GATT_ITER_CONTINUE;
+}
+
+static struct bt_gatt_subscribe_params nfy_params = {
+	.notify = gatt_notify,
+};
+
+static struct bt_gatt_subscribe_params ind_params = {
+	.notify = gatt_notify,
+};
+
+void test_gatt_subscribe(void)
+{
+	nfy_params.ccc_handle = test1_attrs[3].handle,
+	nfy_params.value_handle = test1_attrs[2].handle,
+	nfy_params.value = BT_GATT_CCC_NOTIFY;
+
+	/* Attempt to subscribe attribute for notifications */
+	zassert_false(bt_gatt_subscribe(conn, &nfy_params) < 0,
+		      "Failed to subscribe attribute");
+
+	k_sleep(K_MSEC(100));
+
+	zassert_true(nfy_enabled, "Notification not enabled");
+
+	ind_params.ccc_handle = test1_attrs[6].handle,
+	ind_params.value_handle = test1_attrs[5].handle,
+	ind_params.value = BT_GATT_CCC_INDICATE;
+
+	/* Attempt to subscribe attribute for indications */
+	zassert_false(bt_gatt_subscribe(conn, &ind_params) < 0,
+		      "Failed to subscribe attribute");
+
+	k_sleep(K_MSEC(100));
+
+	zassert_true(ind_enabled, "Indication not enabled");
+}
+
+void test_gatt_notify(void)
+{
+	/* Attempt to notify value */
+	zassert_false(bt_gatt_notify(NULL, &test1_attrs[2], test_value,
+				     strlen(test_value)) < 0,
+		      "Failed to notify value");
+
+	zassert_false(k_sem_take(&nfy_sem, K_MSEC(100)),
+		      "Notify func not called within timeout");
+
+	/* Attempt to notify value with connection */
+	zassert_false(bt_gatt_notify(NULL, &test1_attrs[2], test_value,
+				     strlen(test_value)) < 0,
+		      "Failed to notify value");
+
+	zassert_false(k_sem_take(&nfy_sem, K_MSEC(100)),
+		      "Notify func not called within timeout");
+}
+
+static void gatt_indicate(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+			  u8_t err)
+{
+	zassert_equal(err, 0, "Indicate value return an error");
+	k_sem_give(&ind_sem);
+}
+
+void test_gatt_indicate(void)
+{
+	struct bt_gatt_indicate_params params = {
+		.attr = &test1_attrs[5],
+		.func = gatt_indicate,
+		.data = test_value,
+		.len = strlen(test_value),
+	};
+
+	/* Attempt to indicate value */
+	zassert_false(bt_gatt_indicate(NULL, &params) < 0,
+		      "Failed to indicate value");
+
+	zassert_false(k_sem_take(&nfy_sem, K_MSEC(100)),
+		      "Notify func not called within timeout");
+
+	zassert_false(k_sem_take(&ind_sem, K_MSEC(100)),
+		      "Indicate func not called within timeout");
+
+	/* Attempt to indicate value with connection */
+	zassert_false(bt_gatt_indicate(NULL, &params) < 0,
+		      "Failed to indicate value");
+
+	zassert_false(k_sem_take(&nfy_sem, K_MSEC(100)),
+		      "Notify func not called within timeout");
+
+	zassert_false(k_sem_take(&ind_sem, K_MSEC(100)),
+		      "Indicate func not called within timeout");
+}
+
 /*test case main entry*/
 void test_main(void)
 {
@@ -273,6 +652,13 @@ void test_main(void)
 			 ztest_unit_test(test_gatt_unregister),
 			 ztest_unit_test(test_gatt_foreach),
 			 ztest_unit_test(test_gatt_read),
-			 ztest_unit_test(test_gatt_write));
+			 ztest_unit_test(test_gatt_write),
+			 ztest_unit_test(test_gatt_exchange_mtu),
+			 ztest_unit_test(test_gatt_discover),
+			 ztest_unit_test(test_gatt_client_read),
+			 ztest_unit_test(test_gatt_client_write),
+			 ztest_unit_test(test_gatt_subscribe),
+			 ztest_unit_test(test_gatt_notify),
+			 ztest_unit_test(test_gatt_indicate));
 	ztest_run_test_suite(test_gatt);
 }
