94b38daae68f5357ce39c88712d83068652cb2ea
Manivannan Sadhasivam <mani@kernel.org>
2019-07-27 12:08:00 +0530

    soc: arm: nxp_imx: Add i.MX8M Mini SoC support
    Add SoC support for the NXP i.MX8M Mini series MIMX8MM6 SoC. This SoC
    has a quad Cortex-A53 cluster and a single core Cortex-M4 core. Zephyr
    support is added to the Cortex-M4 core for running at 800MHz.
    
    More information about the SoC can be found here: https://www.nxp.com/products/processors-and-microcontrollers/arm-processors/i.mx-applications-processors/i.mx-8-processors/i.mx-8m-mini-arm-cortex-a53-cortex-m4-audio-voice-video:i.MX8MMINI
    
    Signed-off-by: Manivannan Sadhasivam <mani@kernel.org>

---
+++ soc/arm/nxp_imx/mimx8mm6_m4/soc.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2020, Manivannan Sadhasivam <mani@kernel.org>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _ASMLANGUAGE
+
+#include <fsl_device_registers.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SOC__H_ */
