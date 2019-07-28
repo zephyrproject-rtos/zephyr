2ab98557b7256eeb21f329ea8da06932088666d6
Andy Ross <andrew.j.ross@intel.com>
2019-07-28 06:34:02 -0700

    tests/kernel/interrupt: Correct timing usage
    This tests hard codes a 100 Hz tick rate, but it sets a k_timer for a
    DURATION value of 5ms, which cannot be done with a precision, then
    checks it against an unrelated k_busy_wait() time of exactly 10ms,
    which is right at the boundary of what is possible.  Playing with
    timer stuff can push this over the edge easily.
    
    Correct DURATION to reflect "one tick", and spin for that value, plus
    exactly one tick of slop before testing to see whether the timer ISR
    ran.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- tests/kernel/interrupt/src/nested_irq.c
+++ tests/kernel/interrupt/src/nested_irq.c
@@ -5,7 +5,7 @@
  */
 #include "interrupt.h"
 
-#define DURATION 5
+#define DURATION 10
 struct k_timer timer;
 
 /* This tests uses two IRQ lines, selected within the range of IRQ lines
@@ -85,7 +85,7 @@ void isr0(void *param)
 	 */
 	k_busy_wait(MS_TO_US(1000));
 #else
-	k_busy_wait(MS_TO_US(10));
+	k_busy_wait(DURATION * 1000 + (int)k_ticks_to_us_ceil64(1));
 #endif
 	printk("%s execution completed !!\n", __func__);
 	zassert_equal(new_val, old_val, "Nested interrupt is not working\n");
