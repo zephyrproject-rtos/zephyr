7e7bda29291ec75823f0251b86cb71fa5350c5a6
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2019-08-01 20:47:31 +0200

    soc: arm: nrf53: add nRF5340 CPUAPP SoC definition
    Nordic nRF5340 CPUAPP (Application MCU) SoC definition
    (Secure and Non-Secure domains).
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

---
+++ soc/arm/nordic_nrf/nrf53/soc.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file SoC configuration macros for the
+ *       Nordic Semiconductor nRF53 family processors.
+ */
+
+#ifndef _NORDICSEMI_NRF53_SOC_H_
+#define _NORDICSEMI_NRF53_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <nrfx.h>
+
+/* Add include for DTS generated information */
+#include <generated_dts_board.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#define FLASH_PAGE_ERASE_MAX_TIME_US  89700UL
+#define FLASH_PAGE_MAX_CNT  256UL
+
+#endif /* _NORDICSEMI_NRF53_SOC_H_ */
