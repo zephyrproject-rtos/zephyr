182be5dc5d7e989268da8aafb080ec7fdaf3e188
Robert Winkler <rwinkler@internships.antmicro.com>
2019-08-02 10:38:56 +0200

    drivers: gpio: Add LiteX GPIO driver
    This commits adds GPIO driver for LiteX SoC builder.
    
    Due to the fact that GPIO in LiteX is unidirectional and can be
    configured with different pins amount per port, additional entries
    were added to the dts file.
    
    Signed-off-by: Robert Winkler <rwinkler@internships.antmicro.com>
    Signed-off-by: Mateusz Holenko <mholenko@antmicro.com>
    Signed-off-by: Piotr Zierhoffer <pzierhoffer@antmicro.com>

--- soc/riscv/litex-vexriscv/soc.h
+++ soc/riscv/litex-vexriscv/soc.h
@@ -54,6 +54,38 @@ static inline void litex_write32(unsigned int value, unsigned long addr)
 	sys_write8(value >> 8, addr + 0x8);
 	sys_write8(value, addr + 0xC);
 }
+
+/* `reg_size` is assumed to be a multiple of 4 */
+static inline void litex_write(volatile u32_t *reg, u32_t reg_size, u32_t val)
+{
+	u32_t shifted_data;
+	volatile u32_t *reg_addr;
+	u32_t subregs = reg_size / 4;
+
+	for (int i = 0; i < subregs; ++i) {
+		shifted_data = val >> ((subregs - i - 1) * 8);
+		reg_addr = ((volatile u32_t *) reg) + i;
+		*(reg_addr) = shifted_data;
+	}
+}
+
+/* `reg_size` is assumed to be a multiple of 4 */
+static inline u32_t litex_read(volatile u32_t *reg, u32_t reg_size)
+{
+	u32_t shifted_data;
+	volatile u32_t *reg_addr;
+	u32_t result = 0;
+	u32_t subregs = reg_size / 4;
+
+	for (int i = 0; i < subregs; ++i) {
+		reg_addr = ((volatile u32_t *) reg) + i;
+		shifted_data = *(reg_addr);
+		result |= (shifted_data << ((subregs - i - 1) * 8));
+	}
+
+	return result;
+}
+
 #endif /* _ASMLANGUAGE */
 
 #endif /* __RISCV32_LITEX_VEXRISCV_SOC_H_ */
