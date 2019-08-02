6228a18d649fde934047eab9c0c8b198be48c8eb
Kumar Gala <kumar.gala@linaro.org>
2019-08-02 10:55:23 -0500

    cmake: Support SDK versions more broadly
    The SDK version is of the form X.Y.Z.  Change the cmake scripts to be
    based on X.Y of the version.  This allows us to easily support newer
    toolchains without having to explicitly add cmake files for the version
    as well as removes duplication between those files.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- cmake/toolchain/zephyr/generic.cmake
+++ cmake/toolchain/zephyr/generic.cmake
@@ -4,4 +4,4 @@ if (NOT ZEPHYR_SDK_INSTALL_DIR)
   message(FATAL_ERROR "ZEPHYR_SDK_INSTALL_DIR must be set")
 endif()
 
-include(${ZEPHYR_BASE}/cmake/toolchain/zephyr/${SDK_VERSION}/generic.cmake)
+include(${ZEPHYR_BASE}/cmake/toolchain/zephyr/${SDK_MAJOR_MINOR}/generic.cmake)
