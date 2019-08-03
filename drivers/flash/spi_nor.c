7501968027501df489146443e98be4f7c86a411b
Peter A. Bigot <pab@pabigot.com>
2019-08-03 13:18:59 -0500

    drivers/spi_nor: de-pessimize reads
    SPI NOR devices require that writes be performed within only one page at
    a time.  There is no such limitation on reads.  Remove the code that
    forced reads to be performed in 256-byte chunks.
    
    Signed-off-by: Peter A. Bigot <pab@pabigot.com>

--- drivers/flash/spi_nor.c
+++ drivers/flash/spi_nor.c
@@ -158,7 +158,6 @@ static int spi_nor_read(struct device *dev, off_t addr, void *dest,
 	struct spi_nor_data *const driver_data = dev->driver_data;
 	const struct spi_nor_config *params = dev->config->config_info;
 	int ret;
-	int to_read;
 
 	/* should be between 0 and flash size */
 	if ((addr < 0) || ((addr + size) > params->size)) {
@@ -169,26 +168,10 @@ static int spi_nor_read(struct device *dev, off_t addr, void *dest,
 
 	spi_nor_wait_until_ready(dev);
 
-	while (size) {
-		to_read = size;
-		if (size > SPI_NOR_PAGE_SIZE) {
-			to_read = SPI_NOR_PAGE_SIZE;
-		}
-
-		ret = spi_nor_cmd_addr_read(dev, SPI_NOR_CMD_READ, addr,
-					    dest, to_read);
-		if (ret != 0) {
-			SYNC_UNLOCK();
-			return ret;
-		}
-
-		size -= to_read;
-		addr += to_read;
-		dest = (u8_t *)dest + to_read;
-	}
+	ret = spi_nor_cmd_addr_read(dev, SPI_NOR_CMD_READ, addr, dest, size);
 
 	SYNC_UNLOCK();
-	return 0;
+	return ret;
 }
 
 static int spi_nor_write(struct device *dev, off_t addr, const void *src,
