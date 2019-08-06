abea4f279e3f140e7eed0b16da513da2ad56db55
Andrew Boie <andrew.p.boie@intel.com>
2019-08-06 12:10:49 -0700

    kernel: add z_fatal_halt() to interface
    Intended to be called from application-level implementations
    of k_sys_fatal_error_handler().
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- include/fatal.h
+++ include/fatal.h
@@ -32,6 +32,16 @@ enum k_fatal_error_reason {
 };
 
 /**
+ * @brief Halt the system on a fatal error
+ *
+ * Invokes architecture-specific code to power off or halt the system in
+ * a low power state. Lacking that, lock interupts and sit in an idle loop.
+ *
+ * @param reason Fatal exception reason code
+ */
+FUNC_NORETURN void k_fatal_halt(unsigned int reason);
+
+/**
  * @brief Fatal error policy handler
  *
  * This function is not invoked by application code, but is declared as a
