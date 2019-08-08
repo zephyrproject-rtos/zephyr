fa430cf1392fa51aa78fe6b49ae2cb8c800d3d57
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2019-08-08 17:31:06 +0300

    up_squared_adsp: tools: Add python logtool
    Add simple python logtool to extract logs from memory ring buffer, at
    the moment hardcoded.

---
+++ boards/xtensa/up_squared_adsp/tools/logtool.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+#FILE = "/dev/shm/qemu-bridge-hp-sram-mem"
+FILE = "/sys/kernel/debug/sof/etrace"
+#OFFSET = 0x8000
+OFFSET = 0x0
+MAGIC = 0x55aa
+SLOT_NUM = 32
+SLOT_LEN = 256
+
+def read_id(f):
+    buf = f.read(2)
+    return int.from_bytes(buf, byteorder='little')
+
+def read_magic(f):
+    buf = f.read(2)
+    return int.from_bytes(buf, byteorder='little')
+
+def read_log_slot(f):
+    magic = read_magic(f)
+
+    if magic == MAGIC:
+        id = read_id(f)
+        slot = f.read(SLOT_LEN - 4)
+        logstr = slot.decode(errors='replace').split('\r', 1)[0]
+        print("id %d %s" % (id, logstr))
+
+# Open a file
+f = open(FILE, "rb")
+
+for x in range(0, SLOT_NUM):
+    f.seek(OFFSET + x * SLOT_LEN)
+    read_log_slot(f)
