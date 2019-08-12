a957b0314a9d938c6c212d8874399abc4bdbbf9e
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2019-08-12 13:52:13 +0200

    cmake: Don't use -nostdinc with CXX
    It is observed that the C++ standard library headers can not be found
    when -nostdinc is used. To fix this we omit -nostdinc when building
    for C++.
    
    To reproduce the issue use the latest zephyr master and add
    
     #include <vector>
    
    to the list of includes in samples/cpp_synchronization.
    
    Without this fix the sample will not build for for gnuarmemb or the
    Zephyr SDK.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- cmake/compiler/gcc/target_baremetal.cmake
+++ cmake/compiler/gcc/target_baremetal.cmake
@@ -6,6 +6,7 @@ macro(toolchain_cc_nostdinc)
 
   if (NOT CONFIG_NEWLIB_LIBC AND
     NOT COMPILER STREQUAL "xcc" AND
+    NOT CONFIG_CPLUSPLUS AND
     NOT CONFIG_NATIVE_APPLICATION)
     zephyr_compile_options( -nostdinc)
     zephyr_system_include_directories(${NOSTDINC})
