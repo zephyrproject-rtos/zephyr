9fcadee5a4294d7b839c8670b13c8067c6c21d4b
Daniel Leung <daniel.leung@intel.com>
2019-08-14 23:03:53 -0700

    samples: hello_world: revert to original hello_world
    Should use samples/audio/sof instead of hello_world. So revert
    hello_world back to original version.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -7,30 +7,7 @@
 #include <zephyr.h>
 #include <sys/printk.h>
 
-#include <stdint.h>
-
-#include <xtensa/config/core.h>
-#include <xtensa/hal.h>
-
-#define LOG_LEVEL LOG_LEVEL_DBG
-#include <logging/log.h>
-LOG_MODULE_REGISTER(main);
-
-/* Platform defined trace code */
-#define platform_trace_point(__x) \
-        mailbox_sw_reg_write(SRAM_REG_FW_TRACEP, (__x))
-
 void main(void)
 {
-	int i = 0;
-
-	mailbox_sw_reg_write(SRAM_REG_ROM_STATUS, 0xabbac0fe);
-#if 0
-	platform_trace_point(0xabbac0ffe);
-#endif
-
-	do {
-		LOG_DBG("iteration %d", i++);
-	} while (true);
-
+	printk("Hello World! %s\n", CONFIG_BOARD);
 }
