392dabdf162df0627638944c6eb3b6f86c19aa48
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2019-08-18 15:30:54 +0200

    tests: arch: arm ramfunc: fix ASSERT expressions
    Fix ASSERT expression with volatile test variables
    in arch/arm/arm_ramfunc test suite. Fix, also, an
    logical expression in an if-statement in the test.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- tests/arch/arm/arm_ramfunc/src/arm_ramfunc.c
+++ tests/arch/arm/arm_ramfunc/src/arm_ramfunc.c
@@ -17,7 +17,10 @@ __ramfunc static void arm_ram_function(void)
 
 void test_arm_ramfunc(void)
 {
-	zassert_true(test_flag == 0, "Test flag not initialized to zero");
+	int init_flag, post_flag;
+
+	init_flag = test_flag;
+	zassert_true(init_flag == 0, "Test flag not initialized to zero");
 
 	/* Verify that the .ramfunc section is not empty, it is located
 	 * inside SRAM, and that arm_ram_function(.) is located inside
@@ -46,7 +49,8 @@ void test_arm_ramfunc(void)
 	arm_ram_function();
 
 	/* Verify that the function is executed successfully. */
-	zassert_true(test_flag = 1,
+	post_flag = test_flag;
+	zassert_true(post_flag == 1,
 		"arm_ram_function() execution failed.");
 }
 /**
