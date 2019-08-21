b5908fd89d4631c9eed777e42f55367081babf81
Andy Ross <andrew.j.ross@intel.com>
2019-08-21 12:03:46 -0700

    tests/kernel/interrupt: Add a simple dynamic interrupt test
    Platforms which use the GEN_SW_ISR mechanism for interrupt handling
    can make use of a really simple whitebox trick for verifying that it
    worked (i.e. that the pointer and argument get placed in the table
    correctly).
    
    Easy and simple way to get some coverage for dynamic IRQs, which is
    currently entirely missing.  Long term we'll want to replace this with
    a test that uses the API directly and chooses an arch-specific vector
    to set, and triggers it using arch-specific code, but that's quite a
    bit more effort and for now we need to land patches to
    z_irq_connect_dynamic() which show test coverage.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- tests/kernel/interrupt/src/main.c
+++ tests/kernel/interrupt/src/main.c
@@ -9,9 +9,60 @@
 extern void test_nested_isr(void);
 extern void test_prevent_interruption(void);
 
+extern void z_irq_spurious(void *unused);
+
+/* Simple whitebox test of dynamic interrupt handlers as implemented
+ * by the GEN_SW_ISR feature.
+ */
+#if (defined(CONFIG_DYNAMIC_INTERRUPTS)		\
+     && defined(CONFIG_GEN_SW_ISR_TABLE))
+#define DYNTEST 1
+#endif
+
+#ifdef DYNTEST
+static void dyn_isr(void *arg)
+{
+	ARG_UNUSED(arg);
+}
+
+extern struct _isr_table_entry __sw_isr_table _sw_isr_table[];
+
+static void do_isr_dynamic(void)
+{
+	int i;
+	void *argval;
+
+	for (i = CONFIG_GEN_IRQ_START_VECTOR; i < CONFIG_NUM_IRQS; i++) {
+		if (_sw_isr_table[i].isr == z_irq_spurious) {
+			break;
+		}
+	}
+
+	zassert_true(_sw_isr_table[i].isr == z_irq_spurious,
+		     "could not find slot for dynamic isr");
+
+	argval = &i;
+	z_arch_irq_connect_dynamic(i, 0, dyn_isr, argval, 0);
+
+	zassert_true(_sw_isr_table[i].isr == dyn_isr &&
+		     _sw_isr_table[i].arg == argval,
+		     "dynamic isr did not install successfully");
+}
+#endif /* DYNTEST */
+
+void test_isr_dynamic(void)
+{
+#ifdef DYNTEST
+	do_isr_dynamic();
+#else
+	ztest_test_skip();
+#endif
+}
+
 void test_main(void)
 {
 	ztest_test_suite(interrupt_feature,
+			ztest_unit_test(test_isr_dynamic),
 			ztest_unit_test(test_nested_isr),
 			ztest_unit_test(test_prevent_interruption)
 			);
