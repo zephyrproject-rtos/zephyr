0599cf43b76003013185db992ed7c954f62d680e
Charles E. Youse <charles.youse@intel.com>
2019-08-22 21:08:29 -0700

    drivers/gpio/gpio_dw: remove support for legacy PCI interface
    Only Quark SoCs used this IP block hanging off a PCI bus, and the PCI
    support is written for the deprecated legacy PCI subsystem, so remove.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

--- drivers/gpio/gpio_dw.h
+++ drivers/gpio/gpio_dw.h
@@ -11,11 +11,6 @@
 #include <drivers/gpio.h>
 #include "gpio_dw_registers.h"
 
-#ifdef CONFIG_PCI
-#include <pci/pci.h>
-#include <pci/pci_mgr.h>
-#endif /* CONFIG_PCI */
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -38,10 +33,6 @@ struct gpio_dw_config {
 
 struct gpio_dw_runtime {
 	u32_t base_addr;
-#ifdef CONFIG_PCI
-	struct pci_dev_info  pci_dev;
-#endif /* CONFIG_PCI */
-
 #ifdef CONFIG_GPIO_DW_CLOCK_GATE
 	struct device *clock;
 #endif
