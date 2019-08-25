1aa696bc2be26b1a756676d7f4af11c8b5d85ecd
Saravanan Sekar <saravanan@linumiz.com>
2019-08-25 21:56:11 +0200

    wifi: eswifi: Implement TCP/UDP socket offload with TLS
    Newly implement socket based communication on eswifi mainly to
    achive TLS. Tested with Inventek ISM43362-M3G-L44.
    
    Signed-off-by: Saravanan Sekar <saravanan@linumiz.com>

--- drivers/wifi/eswifi/eswifi_core.c
+++ drivers/wifi/eswifi/eswifi_core.c
@@ -401,6 +401,10 @@ static void eswifi_iface_init(struct net_if *iface)
 	eswifi_unlock(eswifi);
 
 	eswifi_offload_init(eswifi);
+#if defined(CONFIG_NET_SOCKETS_OFFLOAD)
+	eswifi_socket_offload_init(eswifi);
+#endif
+
 }
 
 static int eswifi_mgmt_scan(struct device *dev, scan_result_cb_t cb)
