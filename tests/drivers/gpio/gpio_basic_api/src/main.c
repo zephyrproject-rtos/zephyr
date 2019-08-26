de6f105b404d707f7452ad14b050778186e9a9eb
Peter Bigot <peter.bigot@nordicsemi.no>
2019-08-26 18:00:51 -0500

    tests/drivers/gpio: replace legacy API in callback manage tests
    Switch to gpio_pin_interrupt_configure() and the new interrupt flags.
    Use logical level pin set operations.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- tests/drivers/gpio/gpio_basic_api/src/main.c
+++ tests/drivers/gpio/gpio_basic_api/src/main.c
@@ -42,12 +42,12 @@ void test_main(void)
 	ztest_test_suite(gpio_basic_test,
 			 ztest_unit_test(test_gpio_port),
 			 ztest_unit_test(test_gpio_pin_read_write),
-			 ztest_unit_test(test_gpio_callback_edge_high),
-			 ztest_unit_test(test_gpio_callback_edge_low),
-			 ztest_unit_test(test_gpio_callback_level_high),
 			 ztest_unit_test(test_gpio_callback_add_remove),
 			 ztest_unit_test(test_gpio_callback_self_remove),
 			 ztest_unit_test(test_gpio_callback_enable_disable),
+			 ztest_unit_test(test_gpio_callback_edge_high),
+			 ztest_unit_test(test_gpio_callback_edge_low),
+			 ztest_unit_test(test_gpio_callback_level_high),
 			 ztest_unit_test(test_gpio_callback_level_low));
 	ztest_run_test_suite(gpio_basic_test);
 }
