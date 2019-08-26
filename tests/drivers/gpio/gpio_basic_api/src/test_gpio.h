7082637adf9439f8758ec3876a4e3ba7b6c7c292
Peter Bigot <peter.bigot@nordicsemi.no>
2019-08-26 18:19:21 -0500

    tests/drivers/gpio: replace legacy API in callback tests
    Switch to gpio_pin_interrupt_configure() and the new interrupt flags.
    Use logical level pin set operations.  Test all standard interrupt
    configurations including double edge.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- tests/drivers/gpio/gpio_basic_api/src/test_gpio.h
+++ tests/drivers/gpio/gpio_basic_api/src/test_gpio.h
@@ -50,13 +50,10 @@ struct drv_data {
 };
 
 void test_gpio_pin_read_write(void);
-void test_gpio_callback_edge_high(void);
-void test_gpio_callback_edge_low(void);
-void test_gpio_callback_level_high(void);
-void test_gpio_callback_level_low(void);
 void test_gpio_callback_add_remove(void);
 void test_gpio_callback_self_remove(void);
 void test_gpio_callback_enable_disable(void);
+void test_gpio_callback_variants(void);
 
 void test_gpio_port(void);
 
