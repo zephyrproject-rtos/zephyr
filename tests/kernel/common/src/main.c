0dcc637e7d6146e0f3d00c679948d75e25a55ce9
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2019-08-27 18:20:15 +0300

    tests: kernel: Add unit test for sys_put_be64()
    Make sure sys_put_be64() works as expected.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- tests/kernel/common/src/main.c
+++ tests/kernel/common/src/main.c
@@ -13,6 +13,7 @@
 extern void test_byteorder_memcpy_swap(void);
 extern void test_byteorder_mem_swap(void);
 extern void test_sys_get_be64(void);
+extern void test_sys_put_be64(void);
 extern void test_atomic(void);
 extern void test_intmath(void);
 extern void test_printk(void);
@@ -97,6 +98,7 @@ void test_main(void)
 			 ztest_unit_test(test_byteorder_memcpy_swap),
 			 ztest_unit_test(test_byteorder_mem_swap),
 			 ztest_unit_test(test_sys_get_be64),
+			 ztest_unit_test(test_sys_put_be64),
 			 ztest_user_unit_test(test_atomic),
 			 ztest_unit_test(test_bitfield),
 			 ztest_unit_test(test_printk),
