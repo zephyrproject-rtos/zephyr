8bb4201ce7c3fcd376d22838b3216ec4fd0a4adf
Joakim Andersson <joakim.andersson@nordicsemi.no>
2019-09-03 11:01:00 +0200

    Bluetooth: Shell: Fix shell build dependencies
    Fix shell build dependencies, subsys/bluetooth/controller is only added
    for CONFIG_BT_LL_SW_SPLIT or CONFIG_BT_LL_SW_LEGACY.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- subsys/bluetooth/shell/bt.c
+++ subsys/bluetooth/shell/bt.c
@@ -1667,6 +1667,7 @@ SHELL_STATIC_SUBCMD_SET_CREATE(bt_cmds,
 #if defined(CONFIG_BT_HCI_MESH_EXT)
 	SHELL_CMD(mesh_adv, NULL, "<on, off>", cmd_mesh_adv),
 #endif /* CONFIG_BT_HCI_MESH_EXT */
+#if defined(CONFIG_BT_LL_SW_LEGACY) || defined(CONFIG_BT_LL_SW_SPLIT)
 #if defined(CONFIG_BT_CTLR_ADV_EXT)
 #if defined(CONFIG_BT_BROADCASTER)
 	SHELL_CMD_ARG(advx, NULL, "<on off> [coded] [anon] [txp]", cmd_advx,
@@ -1687,6 +1688,7 @@ SHELL_STATIC_SUBCMD_SET_CREATE(bt_cmds,
 		      4, 0),
 	SHELL_CMD_ARG(test_end, NULL, HELP_NONE, cmd_test_end, 1, 0),
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
+#endif /* defined(CONFIG_BT_LL_SW_LEGACY) || defined(CONFIG_BT_LL_SW_SPLIT) */
 #if defined(CONFIG_BT_LL_SW_SPLIT)
 	SHELL_CMD(ull_reset, NULL, HELP_NONE, cmd_ull_reset),
 #endif /* CONFIG_BT_LL_SW_SPLIT */
