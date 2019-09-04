2c52cfdf565258b720f3f6eb1f91c5976a309d40
Richard Osterloh <richard.osterloh@gmail.com>
2019-09-04 09:47:06 +0100

    drivers: counter: Add STM32G4X counter support
    Add counter driver support for STM32G4X SoC series.
    
    Signed-off-by: Richard Osterloh <richard.osterloh@gmail.com>

--- soc/arm/st_stm32/stm32g4/soc.h
+++ soc/arm/st_stm32/stm32g4/soc.h
@@ -60,6 +60,12 @@
 #include <stm32g4xx_ll_adc.h>
 #endif
 
+#if defined(CONFIG_COUNTER_RTC_STM32)
+#include <stm32g4xx_ll_rtc.h>
+#include <stm32g4xx_ll_exti.h>
+#include <stm32g4xx_ll_pwr.h>
+#endif
+
 #endif /* !_ASMLANGUAGE */
 
 #endif /* _STM32G4_SOC_H_ */
