8c13954fa260794844b1e03cd8c06b01516ff570
Oliver Stäbler <oliver.staebler@bytesatwork.ch>
2019-09-05 08:18:25 +0200

    soc: arm: Add support for kinetis k2x
    This adds support for Kinetis K22 family, specifically for the
    MK22FX512.
    
    This is based on [1] and the PR [2] respectively.
    
    [1] https://github.com/sknsean/zephyr/commits/k22
    [2] https://github.com/zephyrproject-rtos/zephyr/pull/11852
    
    Signed-off-by: Oliver Stäbler <oliver.staebler@bytesatwork.ch>

---
+++ soc/arm/nxp_kinetis/k2x/soc.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019 bytes at work AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#include <sys/util.h>
+
+/* address bases */
+#define PERIPH_ADDR_BASE_WDOG 0x40052000 /* Watchdog Timer module */
+
+#endif /* _SOC__H_ */
