4f79e31566447428d997f0987cf2f1cf1c7798c0
Manivannan Sadhasivam <mani@kernel.org>
2019-09-07 14:42:02 +0530

    soc: arm: st_stm32: stm32l1: Add RTC/Counter support
    Add RTC/Counter support for STM32L1 SoCs.
    
    Signed-off-by: Manivannan Sadhasivam <mani@kernel.org>

--- soc/arm/st_stm32/stm32l1/soc.h
+++ soc/arm/st_stm32/stm32l1/soc.h
@@ -51,6 +51,12 @@
 #include <stm32l1xx_ll_i2c.h>
 #endif
 
+#if defined(CONFIG_COUNTER_RTC_STM32)
+#include <stm32l1xx_ll_rtc.h>
+#include <stm32l1xx_ll_exti.h>
+#include <stm32l1xx_ll_pwr.h>
+#endif
+
 #ifdef CONFIG_SPI_STM32
 #include <stm32l1xx_ll_spi.h>
 #endif
