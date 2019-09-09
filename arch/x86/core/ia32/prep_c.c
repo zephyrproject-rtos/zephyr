73922324295c300601cc4821893da078597758d0
Charles E. Youse <charles.youse@intel.com>
2019-09-09 14:05:41 -0400

    arch/x86: multiboot: migrate multiboot initialization to early C
    Originally, the multiboot info struct was copied in the early assembly
    language code. This code is moved to a C function in multiboot.c for
    two reasons:
    
    1. It's about to get more complicated, as we want the ability to use
       a multiboot-provided memory map if available, and
    2. this will faciliate its sharing between 32- and 64-bit subarches.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

--- arch/x86/core/ia32/prep_c.c
+++ arch/x86/core/ia32/prep_c.c
@@ -6,8 +6,9 @@
 
 #include <kernel_structs.h>
 #include <arch/x86/acpi.h>
+#include <arch/x86/multiboot.h>
 
-FUNC_NORETURN void z_x86_prep_c(void)
+FUNC_NORETURN void z_x86_prep_c(struct x86_multiboot_info *info)
 {
 	_kernel.nested = 0;
 	_kernel.irq_stack = Z_THREAD_STACK_BUFFER(_interrupt_stack) +
@@ -16,12 +17,21 @@ FUNC_NORETURN void z_x86_prep_c(void)
 #ifdef CONFIG_X86_VERY_EARLY_CONSOLE
 	z_x86_early_serial_init();
 #endif
+
+#ifdef CONFIG_X86_MULTIBOOT_INFO
+	z_x86_multiboot_init(info);
+#else
+	ARG_UNUSED(info);
+#endif
+
 #ifdef CONFIG_X86_ACPI
 	z_x86_acpi_init();
 #endif
+
 #ifdef CONFIG_X86_MMU
 	z_x86_paging_init();
 #endif
+
 #if CONFIG_X86_STACK_PROTECTION
 	z_x86_mmu_set_flags(&z_x86_kernel_pdpt, _interrupt_stack, MMU_PAGE_SIZE,
 			    MMU_ENTRY_READ, MMU_PTE_RW_MASK, true);
