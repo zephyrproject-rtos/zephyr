c5516179ccb6dc7233b6ee85e41177bbedd42b21
Daniel Leung <daniel.leung@intel.com>
2019-09-09 10:16:02 -0700

    boards/mec1501: Configure pinmux relevantly for PWM
    Each and every instance has a specific pin.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- boards/arm/mec15xxevb_assy6853/pinmux.c
+++ boards/arm/mec15xxevb_assy6853/pinmux.c
@@ -112,6 +112,54 @@ static int board_pinmux_init(struct device *dev)
 	pinmux_pin_set(portd, MCHP_GPIO_155, MCHP_GPIO_CTRL_MUX_F2 |
 		       MCHP_GPIO_CTRL_BUFT_OPENDRAIN);
 #endif
+
+#ifdef CONFIG_PWM_XEC
+#if defined(DT_INST_0_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM0, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(portb, MCHP_GPIO_053, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+
+#if defined(DT_INST_1_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM1, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(portb, MCHP_GPIO_054, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+
+#if defined(DT_INST_2_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM2, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(portb, MCHP_GPIO_055, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+
+#if defined(DT_INST_3_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM3, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(portb, MCHP_GPIO_056, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+
+#if defined(DT_INST_4_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM4, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(porta, MCHP_GPIO_011, MCHP_GPIO_CTRL_MUX_F2);
+#endif
+
+#if defined(DT_INST_5_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM5, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(porta, MCHP_GPIO_002, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+
+#if defined(DT_INST_6_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM6, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(porta, MCHP_GPIO_014, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+
+#if defined(DT_INST_7_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM7, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(porta, MCHP_GPIO_015, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+
+#if defined(DT_INST_8_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM8, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(porta, MCHP_GPIO_035, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+#endif /* CONFIG_PWM_XEC  */
+
 	return 0;
 }
 
