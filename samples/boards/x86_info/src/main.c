a414eb99cc21cbc4c9209cdd2ea5cef984f259ae
Charles E. Youse <charles.youse@intel.com>
2019-09-09 17:17:06 -0400

    samples/boards/x86_info: add platform information tool
    This sample demonstrates basic use of the x86 multiboot and ACPI
    systems, and also provides some useful information about the board
    it's booted on: data handed over by the multiboot loader (which is
    either QEMU or GRUB at this point), basic APIC CPU topology, and
    timer driver frequency (computed empirically).
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

---
+++ samples/boards/x86_info/src/main.c
@@ -0,0 +1,28 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2019 Intel Corp.
+ */
+
+#include <zephyr.h>
+#include "x86_info.h"
+
+__weak void multiboot(void)
+{
+	printk("MULTIBOOT: Not supported in this build.\n\n");
+}
+
+__weak void acpi(void)
+{
+	printk("ACPI: Not supported in this build.\n\n");
+}
+
+void main(void)
+{
+	printk("\n\nx86_info: the Zephyr x86 platform information tool\n\n");
+
+	multiboot();
+	acpi();
+	timer();
+
+	printk("x86_info: complete\n");
+}
