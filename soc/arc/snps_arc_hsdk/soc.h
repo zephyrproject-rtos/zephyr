9eb379f2bf100dcec866584871f638e25cdafb3b
Watson Zeng <zhiwei@synopsys.com>
2019-09-10 11:18:25 +0800

    arc: hsdk: add pinmux driver support and doc enhancement
    * add pinmux driver. hsdk board has arduino, mikrobus and
      pmod interfaces, which can be confiured for different function,
      such as: gpio, spi, uart, iic.
    * add introduction for arduino, mikrobus and pmod interfaces.
    
    Signed-off-by: Watson Zeng <zhiwei@synopsys.com>

--- soc/arc/snps_arc_hsdk/soc.h
+++ soc/arc/snps_arc_hsdk/soc.h
@@ -22,7 +22,9 @@
 #define IRQ_TIMER1				17
 #define IRQ_ICI					19
 
-#define BASE_ADDR_SYSCONFIG		0xF000A000
+#define BASE_ADDR_SYSCONFIG		0xF0000000
+
+#define CREG_GPIO_MUX_BASE_ADDR		(BASE_ADDR_SYSCONFIG + 0x1484)
 
 #ifndef _ASMLANGUAGE
 
@@ -36,6 +38,30 @@
 #define DT_UART_NS16550_PORT_0_IRQ_FLAGS	0 /* Default */
 #define DT_UART_NS16550_PORT_1_IRQ_FLAGS	0 /* Default */
 #define DT_UART_NS16550_PORT_2_IRQ_FLAGS	0 /* Default */
+
+/* PINMUX IO Hardware Functions */
+#define HSDK_PINMUX_FUNS		8
+
+#define HSDK_PINMUX_FUN0		0
+#define HSDK_PINMUX_FUN1		1
+#define HSDK_PINMUX_FUN2		2
+#define HSDK_PINMUX_FUN3		3
+#define HSDK_PINMUX_FUN4		4
+#define HSDK_PINMUX_FUN5		5
+#define HSDK_PINMUX_FUN6		6
+#define HSDK_PINMUX_FUN7		7
+
+/* PINMUX MAX SELS */
+#define HSDK_PINMUX_SELS		8
+
+#define HSDK_PINMUX_SEL0		0
+#define HSDK_PINMUX_SEL1		1
+#define HSDK_PINMUX_SEL2		2
+#define HSDK_PINMUX_SEL3		3
+#define HSDK_PINMUX_SEL4		4
+#define HSDK_PINMUX_SEL5		5
+#define HSDK_PINMUX_SEL6		6
+#define HSDK_PINMUX_SEL7		7
 
 
 #endif /* !_ASMLANGUAGE */
