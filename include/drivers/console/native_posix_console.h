1d999ffc9a2c47f1711326754351378439caf414
Kumar Gala <kumar.gala@linaro.org>
2019-09-10 08:51:05 -0500

    console: Remove deprecated function console_register_line_input
    console_register_line_input has been deprecated for at least 2 releases
    so we can now remove it.  Remove native_stdin_register_input that is
    associated with console_register_line_input.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- include/drivers/console/native_posix_console.h
+++ include/drivers/console/native_posix_console.h
@@ -15,11 +15,6 @@ extern "C" {
 
 void posix_flush_stdout(void);
 
-#if defined(CONFIG_NATIVE_POSIX_STDIN_CONSOLE)
-void native_stdin_register_input(struct k_fifo *avail, struct k_fifo *lines,
-			 u8_t (*completion)(char *str, u8_t len));
-#endif
-
 #ifdef __cplusplus
 }
 #endif
