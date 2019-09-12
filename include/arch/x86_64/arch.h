03bf8ab55d4e1ef82c3da13a355ad1f09f9855ec
Charles E. Youse <charles.youse@intel.com>
2019-09-12 09:53:07 -0400

    arch/x86_64: add HPET definitions to arch.h
    x86_64 doesn't use devicetree, so put some hand-coded definitions
    into arch.h to allow it to use the DT-enabled HPET driver.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

--- include/arch/x86_64/arch.h
+++ include/arch/x86_64/arch.h
@@ -12,5 +12,9 @@
 
 #define STACK_ALIGN 8
 
+#define DT_INST_0_INTEL_HPET_BASE_ADDRESS	0xFED00000U
+#define DT_INST_0_INTEL_HPET_IRQ_0		2
+#define DT_INST_0_INTEL_HPET_IRQ_0_PRIORITY	4
+
 typedef struct z_arch_esf_t z_arch_esf_t;
 #endif /* _X86_64_ARCH_H */
