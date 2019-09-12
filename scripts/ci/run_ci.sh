020f87eae8618e481e236e3b57ce3a8fc3bbc66e
Kumar Gala <kumar.gala@linaro.org>
2019-09-11 20:18:22 -0500

    ci: run_ci: Move functions
    Move functions up to make the code a bit more readable.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- scripts/ci/run_ci.sh
+++ scripts/ci/run_ci.sh
@@ -34,92 +34,6 @@ WEST_COMMANDS_RESULTS_FILE="./pytest_out/west_commands.xml"
 MATRIX_BUILDS=1
 MATRIX=1
 
-while getopts ":p:m:b:r:M:cfslR:" opt; do
-	case $opt in
-		c)
-			echo "Execute CI" >&2
-			MAIN_CI=1
-			;;
-		l)
-			echo "Executing script locally" >&2
-			LOCAL_RUN=1
-			MAIN_CI=1
-			;;
-		s)
-			echo "Success" >&2
-			SUCCESS=1
-			;;
-		f)
-			echo "Failure" >&2
-			FAILURE=1
-			;;
-		p)
-			echo "Testing a Pull Request: $OPTARG." >&2
-			PULL_REQUEST_NR=$OPTARG
-			;;
-		m)
-			echo "Running on Matrix $OPTARG" >&2
-			MATRIX=$OPTARG
-			;;
-		M)
-			echo "Running a matrix of $OPTARG slaves" >&2
-			MATRIX_BUILDS=$OPTARG
-			;;
-		b)
-			echo "Base Branch: $OPTARG" >&2
-			BRANCH=$OPTARG
-			;;
-		r)
-			echo "Remote: $OPTARG" >&2
-			REMOTE=$OPTARG
-			;;
-		R)
-			echo "Range: $OPTARG" >&2
-			RANGE=$OPTARG
-			;;
-		\?)
-			echo "Invalid option: -$OPTARG" >&2
-			;;
-	esac
-done
-
-if [ -n "$MAIN_CI" ]; then
-
-	# West handling
-	GIT_DIR=$(basename $PWD)
-        pushd ..
-	if [ ! -d .west ]; then
-		west init -l ${GIT_DIR}
-		west update
-	fi
-        popd
-
-	if [ -z "$BRANCH" ]; then
-		echo "No base branch given"
-		exit 1
-	else
-		COMMIT_RANGE=$REMOTE/${BRANCH}..HEAD
-		echo "Commit range:" ${COMMIT_RANGE}
-	fi
-	if [ -n "$RANGE" ]; then
-		COMMIT_RANGE=$RANGE
-	fi
-	source zephyr-env.sh
-	SANITYCHECK="${ZEPHYR_BASE}/scripts/sanitycheck"
-
-	# Possibly the only record of what exact version is being tested:
-	short_git_log='git log -n 5 --oneline --decorate --abbrev=12 '
-
-	if [ -n "$PULL_REQUEST_NR" ]; then
-		$short_git_log $REMOTE/${BRANCH}
-		# Now let's pray this script is being run from a
-		# different location
-# https://stackoverflow.com/questions/3398258/edit-shell-script-while-its-running
-		git rebase $REMOTE/${BRANCH};
-	fi
-	$short_git_log
-fi
-
 function handle_coverage() {
 	# this is for shippable coverage reports
 	echo "Calling gcovr"
@@ -250,6 +164,93 @@ function get_tests_to_run() {
 	fi
 	rm -f modified_tests.args modified_boards.args;
 }
+
+
+while getopts ":p:m:b:r:M:cfslR:" opt; do
+	case $opt in
+		c)
+			echo "Execute CI" >&2
+			MAIN_CI=1
+			;;
+		l)
+			echo "Executing script locally" >&2
+			LOCAL_RUN=1
+			MAIN_CI=1
+			;;
+		s)
+			echo "Success" >&2
+			SUCCESS=1
+			;;
+		f)
+			echo "Failure" >&2
+			FAILURE=1
+			;;
+		p)
+			echo "Testing a Pull Request: $OPTARG." >&2
+			PULL_REQUEST_NR=$OPTARG
+			;;
+		m)
+			echo "Running on Matrix $OPTARG" >&2
+			MATRIX=$OPTARG
+			;;
+		M)
+			echo "Running a matrix of $OPTARG slaves" >&2
+			MATRIX_BUILDS=$OPTARG
+			;;
+		b)
+			echo "Base Branch: $OPTARG" >&2
+			BRANCH=$OPTARG
+			;;
+		r)
+			echo "Remote: $OPTARG" >&2
+			REMOTE=$OPTARG
+			;;
+		R)
+			echo "Range: $OPTARG" >&2
+			RANGE=$OPTARG
+			;;
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			;;
+	esac
+done
+
+if [ -n "$MAIN_CI" ]; then
+
+	# West handling
+	GIT_DIR=$(basename $PWD)
+        pushd ..
+	if [ ! -d .west ]; then
+		west init -l ${GIT_DIR}
+		west update
+	fi
+        popd
+
+	if [ -z "$BRANCH" ]; then
+		echo "No base branch given"
+		exit 1
+	else
+		COMMIT_RANGE=$REMOTE/${BRANCH}..HEAD
+		echo "Commit range:" ${COMMIT_RANGE}
+	fi
+	if [ -n "$RANGE" ]; then
+		COMMIT_RANGE=$RANGE
+	fi
+	source zephyr-env.sh
+	SANITYCHECK="${ZEPHYR_BASE}/scripts/sanitycheck"
+
+	# Possibly the only record of what exact version is being tested:
+	short_git_log='git log -n 5 --oneline --decorate --abbrev=12 '
+
+	if [ -n "$PULL_REQUEST_NR" ]; then
+		$short_git_log $REMOTE/${BRANCH}
+		# Now let's pray this script is being run from a
+		# different location
+# https://stackoverflow.com/questions/3398258/edit-shell-script-while-its-running
+		git rebase $REMOTE/${BRANCH};
+	fi
+	$short_git_log
+fi
 
 if [ -n "$MAIN_CI" ]; then
 
