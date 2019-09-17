82d776dcf09b12be0f6a03152b5b7f07e169f25e
Peter Bigot <peter.bigot@nordicsemi.no>
2019-09-17 08:42:29 -0500

    gpio: put back deprecation comments
    The comments identifying replacement API were inadvertently removed
    along with the flag that triggers deprecation warnings.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- include/drivers/gpio.h
+++ include/drivers/gpio.h
@@ -1055,6 +1055,8 @@ static inline int gpio_pin_toggle(struct device *port, unsigned int pin)
  * @param pin Pin number where the data is written.
  * @param value Value set on the pin.
  * @return 0 if successful, negative errno code on failure.
+ *
+ * @deprecated Replace with gpio_pin_set_raw() or gpio_pin_set().
  */
 static inline int gpio_pin_write(struct device *port, u32_t pin,
 				 u32_t value)
@@ -1071,6 +1073,8 @@ static inline int gpio_pin_write(struct device *port, u32_t pin,
  * @param pin Pin number where data is read.
  * @param value Integer pointer to receive the data values from the pin.
  * @return 0 if successful, negative errno code on failure.
+ *
+ * @deprecated Replace with gpio_pin_get_raw() or gpio_pin_get().
  */
 static inline int gpio_pin_read(struct device *port, u32_t pin,
 				u32_t *value)
@@ -1158,6 +1162,9 @@ static inline int gpio_remove_callback(struct device *port,
  * Note: Depending on the driver implementation, this function will enable
  *       the pin to trigger an interruption. So as a semantic detail, if no
  *       callback is registered, of course none will be called.
+ *
+ * @deprecated Replace with ``gpio_pin_interrupt_configure()`` with
+ * ``GPIO_INT_ENABLE`` along with other interrupt configuration flags.
  */
 static inline int gpio_pin_enable_callback(struct device *port, u32_t pin)
 {
@@ -1169,6 +1176,9 @@ static inline int gpio_pin_enable_callback(struct device *port, u32_t pin)
  * @param port Pointer to the device structure for the driver instance.
  * @param pin Pin number where the callback function is disabled.
  * @return 0 if successful, negative errno code on failure.
+ *
+ * @deprecated Replace with ``gpio_pin_interrupt_configure()`` with
+ * ``GPIO_INT_DISABLE``.
  */
 static inline int gpio_pin_disable_callback(struct device *port, u32_t pin)
 {
