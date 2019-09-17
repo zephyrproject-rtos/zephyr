30795143c4e2ab67d9951c3d5c50dcca8561bbf5
Kumar Gala <kumar.gala@linaro.org>
2019-09-17 14:48:48 -0500

    boards: v2m_musca: Add pyocd support
    Enable pyocd support on musca-a1 board.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

---
+++ boards/arm/v2m_musca/board.cmake
@@ -0,0 +1,8 @@
+#SPDX-License-Identifier: Apache-2.0
+
+board_set_debugger_ifnset(pyocd)
+board_set_flasher_ifnset(pyocd)
+
+board_runner_args(pyocd "--target=musca_a1")
+
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
