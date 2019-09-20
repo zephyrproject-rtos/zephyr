1ffb515ae6e715341e2963ae85df2a4b29af8daf
Marcio Montenegro <mtuxpe@gmail.com>
2019-09-20 10:01:18 -0300

    Samples: Bluetooth: Add ST BLE Demo sample
    This sample demonstrates BLE peripheral for ST BLE Sensor.
    You can test button notification and LED service using
    ST BLE Sensor Android application
    
    Signed-off-by: Marcio Montenegro <mtuxpe@gmail.com>

---
+++ samples/bluetooth/st_ble_sensor/src/button_svc.h
@@ -0,0 +1,19 @@
+/** @file
+ *  @brief Button Service
+ */
+
+/*
+ * Copyright (c) 2019 Marcio Montenegro <mtuxpe@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int button_init(void);
+
+#ifdef __cplusplus
+}
+#endif
