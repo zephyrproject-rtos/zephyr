627e1cc359a62b3468e2b4cd3cb399d75f09ac7d
Ryan QIAN <jianghao.qian@nxp.com>
2019-09-24 09:00:39 +0800

    boards: arm: add board support for mimxrt1010_evk
    Add board support files for mimxrt1010_evk, the development board for
    i.MXRT1010 (CM7) SoC.
    
    - Add pinmux, dts and doc.
    - Tested samples: hello_world, philosophers, synchronization,
    basic/blinky, basic/button.
    
    Signed-off-by: Ryan QIAN <jianghao.qian@nxp.com>

---
+++ boards/arm/mimxrt1010_evk/board.cmake
@@ -0,0 +1,14 @@
+#
+# Copyright (c) 2019, NXP
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+set_ifndef(OPENSDA_FW jlink)
+
+if(OPENSDA_FW STREQUAL jlink)
+  board_set_debugger_ifnset(jlink)
+  board_set_flasher_ifnset(jlink)
+endif()
+
+board_runner_args(jlink "--device=MIMXRT1011")
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
