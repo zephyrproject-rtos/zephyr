b063456015c0cc0c059663b031fbf179a6ee5f7f
George Stefan <george.stefan@nxp.com>
2019-09-25 08:12:14 +0300

    bluetooth: controller: openisa/RV32M1: BLE Link Layer ULL/LLL split
    This commit takes the Nordic LLL and adapts it for RV32M1 SoCs, using
    the blocks that are specific to this SoC: the GenFSK & LPTMR IP
    blocks.
    
    Signed-off-by: George Stefan <george.stefan@nxp.com>
    Signed-off-by: Radu Alexe <radu.alexe@nxp.com>
    Signed-off-by: Ionut Ursescu <ionut.ursescu@nxp.com>

---
+++ subsys/bluetooth/controller/ll_sw/openisa/lll/lll_slave.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2018-2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int lll_slave_init(void);
+int lll_slave_reset(void);
+void lll_slave_prepare(void *param);
