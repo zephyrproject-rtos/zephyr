24d9ad24946fdf67e5f480d462cbf0f962615ce6
George Stefan <george.stefan@nxp.com>
2019-09-25 08:11:40 +0300

    bluetooth: controller: openisa/RV32M1: add HAL for LPTMR & GenFSK
    This commit adds the HAL layer needed for the BLE controller
    LL on RV32M1 SoCs on OpenISA boards. Specifically, the controller
    makes use of the the GenFSK and LPTMR IP blocks.
    
    Signed-off-by: George Stefan <george.stefan@nxp.com>
    Signed-off-by: Radu Alexe <radu.alexe@nxp.com>
    Signed-off-by: Ionut Ursescu <ionut.ursescu@nxp.com>

---
+++ subsys/bluetooth/controller/ll_sw/openisa/hal/RV32M1/debug.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016-2017 Nordic Semiconductor ASA
+ * Copyright (c) 2016 Vinayak Kariappa Chettimada
+ * Copyright 2019 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* CONFIG_BT_CTLR_DEBUG_PINS */
+#define DEBUG_INIT()
+#define DEBUG_CPU_SLEEP(flag)
+#define DEBUG_TICKER_ISR(flag)
+#define DEBUG_TICKER_TASK(flag)
+#define DEBUG_TICKER_JOB(flag)
+#define DEBUG_RADIO_ISR(flag)
+#define DEBUG_RADIO_HCTO(flag)
+#define DEBUG_RADIO_XTAL(flag)
+#define DEBUG_RADIO_ACTIVE(flag)
+#define DEBUG_RADIO_CLOSE(flag)
+#define DEBUG_RADIO_PREPARE_A(flag)
+#define DEBUG_RADIO_START_A(flag)
+#define DEBUG_RADIO_CLOSE_A(flag)
+#define DEBUG_RADIO_PREPARE_S(flag)
+#define DEBUG_RADIO_START_S(flag)
+#define DEBUG_RADIO_CLOSE_S(flag)
+#define DEBUG_RADIO_PREPARE_O(flag)
+#define DEBUG_RADIO_START_O(flag)
+#define DEBUG_RADIO_CLOSE_O(flag)
+#define DEBUG_RADIO_PREPARE_M(flag)
+#define DEBUG_RADIO_START_M(flag)
+#define DEBUG_RADIO_CLOSE_M(flag)
---
+++ subsys/bluetooth/controller/ll_sw/openisa/hal/RV32M1/mayfly.c
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2016-2019 Nordic Semiconductor ASA
+ * Copyright (c) 2016 Vinayak Kariappa Chettimada
+ * Copyright 2019 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/types.h>
+#include <soc.h>
+
+#include "hal/RV32M1/swi.h"
+
+#include "util/memq.h"
+#include "util/mayfly.h"
+
+#define LOG_MODULE_NAME bt_ctlr_rv32m1_mayfly
+#include "common/log.h"
+#include "hal/debug.h"
+
+#if defined(CONFIG_BT_LL_SW_SPLIT)
+#include "ll_sw/lll.h"
+#define MAYFLY_CALL_ID_LLL    TICKER_USER_ID_LLL
+#define MAYFLY_CALL_ID_WORKER TICKER_USER_ID_ULL_HIGH
+#define MAYFLY_CALL_ID_JOB    TICKER_USER_ID_ULL_LOW
+#else
+#error Unknown LL variant.
+#endif
+
+void mayfly_enable_cb(u8_t caller_id, u8_t callee_id, u8_t enable)
+{
+	(void)caller_id;
+
+	LL_ASSERT(callee_id == MAYFLY_CALL_ID_JOB);
+
+	if (enable) {
+		irq_enable(HAL_SWI_JOB_IRQ);
+	} else {
+		irq_disable(HAL_SWI_JOB_IRQ);
+	}
+}
+
+u32_t mayfly_is_enabled(u8_t caller_id, u8_t callee_id)
+{
+	(void)caller_id;
+
+	switch (callee_id) {
+#if defined(CONFIG_BT_LL_SW_SPLIT)
+	case MAYFLY_CALL_ID_LLL:
+		return irq_is_enabled(HAL_SWI_RADIO_IRQ);
+#endif /* CONFIG_BT_LL_SW_SPLIT */
+
+	case MAYFLY_CALL_ID_WORKER:
+		return irq_is_enabled(HAL_SWI_WORKER_IRQ);
+
+	case MAYFLY_CALL_ID_JOB:
+		return irq_is_enabled(HAL_SWI_JOB_IRQ);
+
+	default:
+		LL_ASSERT(0);
+		break;
+	}
+
+	return 0;
+}
+
+u32_t mayfly_prio_is_equal(u8_t caller_id, u8_t callee_id)
+{
+	return (caller_id == callee_id) ||
+#if defined(CONFIG_BT_LL_SW_SPLIT)
+#if (CONFIG_BT_CTLR_LLL_PRIO == CONFIG_BT_CTLR_ULL_HIGH_PRIO)
+	       ((caller_id == MAYFLY_CALL_ID_LLL) &&
+		(callee_id == MAYFLY_CALL_ID_WORKER)) ||
+	       ((caller_id == MAYFLY_CALL_ID_WORKER) &&
+		(callee_id == MAYFLY_CALL_ID_LLL)) ||
+#endif
+#if (CONFIG_BT_CTLR_LLL_PRIO == CONFIG_BT_CTLR_ULL_LOW_PRIO)
+	       ((caller_id == MAYFLY_CALL_ID_LLL) &&
+		(callee_id == MAYFLY_CALL_ID_JOB)) ||
+	       ((caller_id == MAYFLY_CALL_ID_JOB) &&
+		(callee_id == MAYFLY_CALL_ID_LLL)) ||
+#endif
+#if (CONFIG_BT_CTLR_ULL_HIGH_PRIO == CONFIG_BT_CTLR_ULL_LOW_PRIO)
+	       ((caller_id == MAYFLY_CALL_ID_WORKER) &&
+		(callee_id == MAYFLY_CALL_ID_JOB)) ||
+	       ((caller_id == MAYFLY_CALL_ID_JOB) &&
+		(callee_id == MAYFLY_CALL_ID_WORKER)) ||
+#endif
+#endif
+	       0;
+}
+
+void mayfly_pend(u8_t caller_id, u8_t callee_id)
+{
+	(void)caller_id;
+
+	switch (callee_id) {
+#if defined(CONFIG_BT_LL_SW_SPLIT)
+	case MAYFLY_CALL_ID_LLL:
+		hal_swi_lll_pend();
+		break;
+#endif /* CONFIG_BT_LL_SW_SPLIT */
+
+	case MAYFLY_CALL_ID_WORKER:
+		hal_swi_worker_pend();
+		break;
+
+	case MAYFLY_CALL_ID_JOB:
+		hal_swi_job_pend();
+		break;
+
+	default:
+		LL_ASSERT(0);
+		break;
+	}
+}
