752c6371ed093932500cb7faed2ffbe74b8c960a
Nicolas Pitre <npitre@baylibre.com>
2019-09-26 01:59:35 -0400

    sys_heap: remove need for last_chunk()
    We already have chunk #0 containing our struct z_heap and marked as
    used. We can add a partial chunk at the very end that is also marked
    as used. By doing so there is no longer a need for checking heap
    boundaries at run time when merging/splitting chunks meaning fewer
    conditionals in the code's hot path.
    
    Signed-off-by: Nicolas Pitre <npitre@baylibre.com>

--- include/mempool_heap.h
+++ include/mempool_heap.h
@@ -36,17 +36,17 @@ struct k_mem_pool {
  * objects defined, and include extra so there's enough metadata space
  * available for the maximum number of minimum-sized objects to be
  * stored: 8 bytes for each desired chunk header, and a 24 word block
- * to reserve room for a "typical" set of bucket list heads (this size
- * was picked more to conform with existing test expectations than any
- * rigorous theory -- we have tests that rely on being able to
- * allocate the blocks promised and ones that make assumptions about
+ * to reserve room for a "typical" set of bucket list heads and one heap
+ * footer(this size was picked more to conform with existing test
+ * expectations than any rigorous theory -- we have tests that rely on being
+ * able to allocate the blocks promised and ones that make assumptions about
  * when memory will run out).
  */
 #define Z_MEM_POOL_DEFINE(name, minsz, maxsz, nmax, align)		\
 		K_HEAP_DEFINE(poolheap_##name,				\
 			      ((maxsz) * (nmax))			\
 			      + 8 * ((maxsz) * (nmax) / (minsz))	\
-			      + 24 * sizeof(void *));			\
+			      + 25 * sizeof(void *));			\
 		struct k_mem_pool name = {				\
 			.heap = &poolheap_##name			\
 		}
