200056df2fbbd548df6bec1c5589d06c4ff68270
Charles E. Youse <charles.youse@intel.com>
2019-09-27 14:53:40 -0400

    arch/x86: rename CONFIG_X86_MULTIBOOT and related to CONFIG_MULTIBOOT
    Simple naming change, since MULTIBOOT is clear enough by itself and
    "namespacing" it to X86 is unnecessary and/or inappropriate.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

--- samples/boards/x86_info/src/multiboot.c
+++ samples/boards/x86_info/src/multiboot.c
@@ -8,7 +8,7 @@
 
 void multiboot(void)
 {
-#ifndef CONFIG_X86_MULTIBOOT_INFO
+#ifndef CONFIG_MULTIBOOT_INFO
 	printk("MULTIBOOT: info struct NOT preserved.\n\n");
 #else
 	/*
@@ -17,36 +17,36 @@ void multiboot(void)
 	 * (highly unlikely) we DID get a valid struct, but it was empty.
 	 */
 
-	if (!x86_multiboot_info.flags) {
+	if (!multiboot_info.flags) {
 		printk("MULTIBOOT: info struct UNAVAILABLE or EMPTY.\n\n");
 	} else {
 		printk("MULTIBOOT: boot info structure available.\n");
-		printk("\tFlags = 0x%08x\n", x86_multiboot_info.flags);
+		printk("\tFlags = 0x%08x\n", multiboot_info.flags);
 	}
 
-	if (x86_multiboot_info.flags & X86_MULTIBOOT_INFO_FLAGS_MEM) {
+	if (multiboot_info.flags & MULTIBOOT_INFO_FLAGS_MEM) {
 		printk("\tBasic memory map: lower = %dK, upper = %dK.\n",
-			x86_multiboot_info.mem_lower,
-			x86_multiboot_info.mem_upper);
+			multiboot_info.mem_lower,
+			multiboot_info.mem_upper);
 	} else {
 		printk("\tNo basic memory map available.\n");
 	}
 
-	if (x86_multiboot_info.flags & X86_MULTIBOOT_INFO_FLAGS_MMAP) {
+	if (multiboot_info.flags & MULTIBOOT_INFO_FLAGS_MMAP) {
 		printk("\tExtended memory map was at 0x%08x (%d bytes).\n",
-			x86_multiboot_info.mmap_addr,
-			x86_multiboot_info.mmap_length);
+			multiboot_info.mmap_addr,
+			multiboot_info.mmap_length);
 	} else {
 		printk("\tNo extended memory map available.\n");
 	}
 
-	if (x86_multiboot_info.flags & X86_MULTIBOOT_INFO_FLAGS_FB) {
+	if (multiboot_info.flags & MULTIBOOT_INFO_FLAGS_FB) {
 		printk("\tFramebuffer %dbpp %dX%d (pitch %d) @ %08x.\n",
-			x86_multiboot_info.fb_bpp,
-			x86_multiboot_info.fb_width,
-			x86_multiboot_info.fb_height,
-			x86_multiboot_info.fb_pitch,
-			x86_multiboot_info.fb_addr_lo);
+			multiboot_info.fb_bpp,
+			multiboot_info.fb_width,
+			multiboot_info.fb_height,
+			multiboot_info.fb_pitch,
+			multiboot_info.fb_addr_lo);
 	} else {
 		printk("\tFramebuffer data not present.\n");
 	}
