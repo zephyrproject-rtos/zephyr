66510db98ca6ffb217d268c6e36df83e2ea84133
Charles E. Youse <charles.youse@intel.com>
2019-09-30 13:28:36 -0400

    arch/x86: (Intel64) add scheduler IPI support
    Add z_arch_sched_ipi() and such to enable scheduler IPIs when SMP.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

--- arch/x86/core/intel64/irq.c
+++ arch/x86/core/intel64/irq.c
@@ -4,6 +4,7 @@
  */
 
 #include <kernel.h>
+#include <ksched.h>
 #include <arch/cpu.h>
 #include <kernel_arch_data.h>
 #include <drivers/interrupt_controller/sysapic.h>
@@ -103,3 +104,18 @@ void z_arch_irq_offload(irq_offload_routine_t routine, void *parameter)
 }
 
 #endif /* CONFIG_IRQ_OFFLOAD */
+
+#if defined(CONFIG_SMP)
+
+void z_x86_ipi_setup(void)
+{
+	/*
+	 * z_sched_ipi() doesn't have the same signature as a typical ISR, so
+	 * we fudge it with a cast. the argument is ignored, no harm done.
+	 */
+
+	x86_irq_funcs[CONFIG_SCHED_IPI_VECTOR - IV_IRQS] =
+		(void *) z_sched_ipi;
+}
+
+#endif
