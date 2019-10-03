4a66f20811d81ce5eb1d1b20e69af2c382a4b1d6
Andy Ross <andrew.j.ross@intel.com>
2019-10-03 10:03:58 -0700

    tests/kernel/mp: Update the MP test to run on emulated platforms
    This was a very early test and got bitrotten inside a esp32-only
    whitelist.  Make it run generically.
    
    SMP must be forced off by the test (it's commonly a platform default).
    
    Add a build-time failure when the configuration is single-CPU, for
    clarity.
    
    Filter the test likewise so it runs on all supported systems.
    
    Also, the key argument to the CPU startup function is vestigial and
    the test was being too strict by requiring it to be non-zero.
    
    Finally, the qemu command line needs to predicate the "-smp" argument
    on CONFIG_MP_NUM_CPUS and not just CONFIG_SMP so we have an extra CPU
    to test against.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- cmake/emu/qemu.cmake
+++ cmake/emu/qemu.cmake
@@ -252,7 +252,10 @@ if(NOT QEMU_PIPE)
   set(QEMU_PIPE_COMMENT "\nTo exit from QEMU enter: 'CTRL+a, x'\n")
 endif()
 
-if(CONFIG_SMP)
+# Don't just test CONFIG_SMP, there is at least one test of the lower
+# level multiprocessor API that wants an auxiliary CPU but doesn't
+# want SMP using it.
+if(NOT CONFIG_MP_NUM_CPUS MATCHES "1")
   list(APPEND QEMU_SMP_FLAGS -smp cpus=${CONFIG_MP_NUM_CPUS})
 endif()
 
