23a866b828df953a28f4fad5bb5d9ccec5a97f74
Jan Van Winkel <jan.van_winkel@dxplore.eu>
2019-10-04 00:54:00 +0200

    cmake: toolchain abstraction for undefined behaviour sanitizer
    Added toolchain abstraction for undefined behaviour sanitizer
    
    Signed-off-by: Jan Van Winkel <jan.van_winkel@dxplore.eu>

--- cmake/compiler/clang/target_sanitizers.cmake
+++ cmake/compiler/clang/target_sanitizers.cmake
@@ -6,3 +6,10 @@ zephyr_compile_options(-fsanitize=address)
 zephyr_ld_options(-fsanitize=address)
 
 endmacro()
+
+macro(toolchain_cc_ubsan)
+
+zephyr_compile_options(-fsanitize=undefined)
+zephyr_ld_options(-fsanitize=undefined)
+
+endmacro()
