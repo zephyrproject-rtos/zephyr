3a98d71fdf110579db7a9d1f3af291afe498f6b4
Jose Alberto Meza <jose.a.meza.arellano@intel.com>
2019-10-02 18:38:35 -0700

    board: arm: mec15modular_assy6885: Enable RTOS, PS2, PWM and ADC drivers
    Enable RTOS, PS2, PWM and ADC for modular MEC15xx
    Add extra step to build flashable image.
    
    Signed-off-by: Jose Alberto Meza <jose.a.meza.arellano@intel.com>

--- boards/arm/mec1501modular_assy6885/pinmux.c
+++ boards/arm/mec1501modular_assy6885/pinmux.c
@@ -67,6 +67,24 @@ static int board_pinmux_init(struct device *dev)
 	pinmux_pin_set(portd, MCHP_GPIO_171, MCHP_GPIO_CTRL_MUX_F1);
 #endif
 
+#ifdef CONFIG_ADC_XEC
+	/* Disable sleep for ADC block */
+	mchp_pcr_periph_slp_ctrl(PCR_ADC, MCHP_PCR_SLEEP_DIS);
+
+	/* ADC pin muxes, ADC00 - ADC07 */
+	pinmux_pin_set(porte, MCHP_GPIO_200, MCHP_GPIO_CTRL_MUX_F1);
+	pinmux_pin_set(porte, MCHP_GPIO_201, MCHP_GPIO_CTRL_MUX_F1);
+	pinmux_pin_set(porte, MCHP_GPIO_202, MCHP_GPIO_CTRL_MUX_F1);
+	pinmux_pin_set(porte, MCHP_GPIO_203, MCHP_GPIO_CTRL_MUX_F1);
+	pinmux_pin_set(porte, MCHP_GPIO_204, MCHP_GPIO_CTRL_MUX_F1);
+	pinmux_pin_set(porte, MCHP_GPIO_205, MCHP_GPIO_CTRL_MUX_F1);
+	pinmux_pin_set(porte, MCHP_GPIO_206, MCHP_GPIO_CTRL_MUX_F1);
+	pinmux_pin_set(porte, MCHP_GPIO_207, MCHP_GPIO_CTRL_MUX_F1);
+
+	/* VREF2_ADC */
+	pinmux_pin_set(portb, MCHP_GPIO_067, MCHP_GPIO_CTRL_MUX_F1);
+#endif /* CONFIG_ADC_XEC */
+
 #ifdef CONFIG_I2C_XEC_0
 	/* Set muxing, for I2C0 - SMB00 */
 	pinmux_pin_set(porta, MCHP_GPIO_003, MCHP_GPIO_CTRL_MUX_F1);
@@ -101,6 +119,72 @@ static int board_pinmux_init(struct device *dev)
 	pinmux_pin_set(portb, MCHP_GPIO_072, MCHP_GPIO_CTRL_MUX_F1);
 	pinmux_pin_set(portb, MCHP_GPIO_073, MCHP_GPIO_CTRL_MUX_F1);
 #endif
+
+#ifdef CONFIG_PS2_XEC_0
+	/* Set muxing for PS20B*/
+	mchp_pcr_periph_slp_ctrl(PCR_PS2_0, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(porta, MCHP_GPIO_007, MCHP_GPIO_CTRL_MUX_F2 |
+		       MCHP_GPIO_CTRL_BUFT_OPENDRAIN);
+	pinmux_pin_set(porta, MCHP_GPIO_010, MCHP_GPIO_CTRL_MUX_F2 |
+		       MCHP_GPIO_CTRL_BUFT_OPENDRAIN);
+#endif
+
+#ifdef CONFIG_PS2_XEC_1
+	/* Set muxing for PS21B*/
+	mchp_pcr_periph_slp_ctrl(PCR_PS2_1, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(portd, MCHP_GPIO_154, MCHP_GPIO_CTRL_MUX_F2 |
+		       MCHP_GPIO_CTRL_BUFT_OPENDRAIN);
+	pinmux_pin_set(portd, MCHP_GPIO_155, MCHP_GPIO_CTRL_MUX_F2 |
+		       MCHP_GPIO_CTRL_BUFT_OPENDRAIN);
+#endif
+
+#ifdef CONFIG_PWM_XEC
+#if defined(DT_INST_0_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM0, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(portb, MCHP_GPIO_053, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+
+#if defined(DT_INST_1_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM1, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(portb, MCHP_GPIO_054, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+
+#if defined(DT_INST_2_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM2, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(portb, MCHP_GPIO_055, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+
+#if defined(DT_INST_3_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM3, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(portb, MCHP_GPIO_056, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+
+#if defined(DT_INST_4_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM4, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(porta, MCHP_GPIO_011, MCHP_GPIO_CTRL_MUX_F2);
+#endif
+
+#if defined(DT_INST_5_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM5, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(porta, MCHP_GPIO_002, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+
+#if defined(DT_INST_6_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM6, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(porta, MCHP_GPIO_014, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+
+#if defined(DT_INST_7_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM7, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(porta, MCHP_GPIO_015, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+
+#if defined(DT_INST_8_MICROCHIP_XEC_PWM)
+	mchp_pcr_periph_slp_ctrl(PCR_PWM8, MCHP_PCR_SLEEP_DIS);
+	pinmux_pin_set(porta, MCHP_GPIO_035, MCHP_GPIO_CTRL_MUX_F1);
+#endif
+#endif /* CONFIG_PWM_XEC  */
+
 	return 0;
 }
 
