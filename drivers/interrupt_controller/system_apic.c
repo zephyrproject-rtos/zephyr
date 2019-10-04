1687c92e4d866b53b6717406a5f1a7e8c57eb65f
Charles E. Youse <charles.youse@intel.com>
2019-10-04 09:18:59 -0400

    interrupt_controller/system_apic.c: do not assume IRQS wired to APIC
    In some situations IRQs may be raised on systems with a local APIC
    that are not actually wired to any interrupt controller (e.g., MSI).
    The APIC controller code assumed that what wasn't an I/O APIC IRQ
    must be a local APIC IRQ, but this is not correct. Fix assumption.
    
    Signed-off-by: Charles E. Youse <charles.youse@intel.com>

--- drivers/interrupt_controller/system_apic.c
+++ drivers/interrupt_controller/system_apic.c
@@ -24,24 +24,15 @@
 #define IS_IOAPIC_IRQ(irq)  0
 #endif
 
-#define HARDWARE_IRQ_LIMIT ((LOAPIC_IRQ_BASE + LOAPIC_IRQ_COUNT) - 1)
+#define NR_APIC_IRQ (LOAPIC_IRQ_BASE + LOAPIC_IRQ_COUNT)
 
 /**
  *
  * @brief Program interrupt controller
  *
  * This routine programs the interrupt controller with the given vector
- * based on the given IRQ parameter.
- *
- * Drivers call this routine instead of IRQ_CONNECT() when interrupts are
- * configured statically.
- *
- * The Galileo board virtualizes IRQs as follows:
- *
- * - The first CONFIG_IOAPIC_NUM_RTES IRQs are provided by the IOAPIC so the
- *     IOAPIC is programmed for these IRQs
- * - The remaining IRQs are provided by the LOAPIC and hence the LOAPIC is
- *     programmed.
+ * based on the given IRQ parameter. If the IRQ is not "wired" (that is,
+ * not connected to an APIC of any kind), this function is a no-op.
  *
  * @param vector the vector number
  * @param irq the virtualized IRQ
@@ -51,12 +42,12 @@
 void __irq_controller_irq_config(unsigned int vector, unsigned int irq,
 				 u32_t flags)
 {
-	__ASSERT(irq <= HARDWARE_IRQ_LIMIT, "invalid irq line");
-
-	if (IS_IOAPIC_IRQ(irq)) {
-		z_ioapic_irq_set(irq, vector, flags);
-	} else {
-		z_loapic_int_vec_set(irq - LOAPIC_IRQ_BASE, vector);
+	if (irq < NR_APIC_IRQ) {
+		if (IS_IOAPIC_IRQ(irq)) {
+			z_ioapic_irq_set(irq, vector, flags);
+		} else {
+			z_loapic_int_vec_set(irq - LOAPIC_IRQ_BASE, vector);
+		}
 	}
 }
 
@@ -70,39 +61,29 @@ void __irq_controller_irq_config(unsigned int vector, unsigned int irq,
  *   extern void  irq_enable  (unsigned int irq);
  *   extern void  irq_disable (unsigned int irq);
  *
- * The irq_enable() routine is provided by the interrupt controller driver due
- * to the IRQ virtualization that is performed by this platform.  See the
- * comments in _interrupt_vector_allocate() for more information regarding IRQ
- * virtualization.
+ * These routines are no-ops if 'irq' is not wired to an APIC.
  *
  * @return N/A
  */
 void z_arch_irq_enable(unsigned int irq)
 {
-	if (IS_IOAPIC_IRQ(irq)) {
-		z_ioapic_irq_enable(irq);
-	} else {
-		z_loapic_irq_enable(irq - LOAPIC_IRQ_BASE);
+	if (irq < NR_APIC_IRQ) {
+		if (IS_IOAPIC_IRQ(irq)) {
+			z_ioapic_irq_enable(irq);
+		} else {
+			z_loapic_irq_enable(irq - LOAPIC_IRQ_BASE);
+		}
 	}
 }
 
-/**
- *
- * @brief Disable an individual interrupt (IRQ)
- *
- * The irq_disable() routine is provided by the interrupt controller driver due
- * to the IRQ virtualization that is performed by this platform.  See the
- * comments in _interrupt_vector_allocate() for more information regarding IRQ
- * virtualization.
- *
- * @return N/A
- */
 void z_arch_irq_disable(unsigned int irq)
 {
-	if (IS_IOAPIC_IRQ(irq)) {
-		z_ioapic_irq_disable(irq);
-	} else {
-		z_loapic_irq_disable(irq - LOAPIC_IRQ_BASE);
+	if (irq < NR_APIC_IRQ) {
+		if (IS_IOAPIC_IRQ(irq)) {
+			z_ioapic_irq_disable(irq);
+		} else {
+			z_loapic_irq_disable(irq - LOAPIC_IRQ_BASE);
+		}
 	}
 }
 
