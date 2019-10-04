52846df85aa98d9e31736858cf2c131bc7b9bddb
Kumar Gala <kumar.gala@linaro.org>
2019-10-04 14:36:39 -0500

    cmake: toolchain: Add initial Zephyr 0.11 SDK support
    copy the cmake files from 0.10 to 0.11 to support SDK version 0.11.  The
    SDK 0.11 will have some changes related to xtensa and is based on
    gcc-9.2.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

---
+++ cmake/toolchain/zephyr/0.11/target.cmake
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: Apache-2.0
+
+set(CROSS_COMPILE_TARGET_arm         arm-zephyr-eabi)
+set(CROSS_COMPILE_TARGET_nios2     nios2-zephyr-elf)
+set(CROSS_COMPILE_TARGET_riscv   riscv64-zephyr-elf)
+set(CROSS_COMPILE_TARGET_mips     mipsel-zephyr-elf)
+set(CROSS_COMPILE_TARGET_xtensa   xtensa-zephyr-elf)
+set(CROSS_COMPILE_TARGET_arc         arc-zephyr-elf)
+set(CROSS_COMPILE_TARGET_x86_64   x86_64-zephyr-elf)
+
+if(CONFIG_X86_LONGMODE)
+  set(CROSS_COMPILE_TARGET_x86 x86_64-zephyr-elf)
+else()
+  set(CROSS_COMPILE_TARGET_x86 i586-zephyr-elf)
+endif()
+
+set(CROSS_COMPILE_TARGET ${CROSS_COMPILE_TARGET_${ARCH}})
+set(SYSROOT_TARGET       ${CROSS_COMPILE_TARGET})
+
+set(CROSS_COMPILE ${TOOLCHAIN_HOME}/${CROSS_COMPILE_TARGET}/bin/${CROSS_COMPILE_TARGET}-)
+
+if("${ARCH}" STREQUAL "xtensa")
+  set(SYSROOT_DIR ${TOOLCHAIN_HOME}/${SYSROOT_TARGET})
+  set(TOOLCHAIN_INCLUDES
+    ${SYSROOT_DIR}/include/arch/include
+    ${SYSROOT_DIR}/include
+    )
+
+  LIST(APPEND TOOLCHAIN_LIBS hal)
+  LIST(APPEND LIB_INCLUDE_DIR -L${SYSROOT_DIR}/lib)
+elseif("${ARCH}" STREQUAL "x86_64")
+  list(APPEND TOOLCHAIN_C_FLAGS -mx32)
+  list(APPEND TOOLCHAIN_LD_FLAGS -mx32)
+endif()
+set(SYSROOT_DIR   ${TOOLCHAIN_HOME}/${SYSROOT_TARGET}/${SYSROOT_TARGET})
