2dba954abdf974433fa5d209846fe89dcaad9c88
Daniel Leung <daniel.leung@intel.com>
2019-10-08 01:09:11 -0700

    cmake: toolchain/xtools: update to align with latest sdk-ng
    The target.cmake for xtools needs to be updated for changes in
    sdk-ng repo. This is simply a copy from the target.cmake for
    0.10 SDK.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- cmake/toolchain/xtools/target.cmake
+++ cmake/toolchain/xtools/target.cmake
@@ -2,17 +2,21 @@
 
 set(CROSS_COMPILE_TARGET_arm         arm-zephyr-eabi)
 set(CROSS_COMPILE_TARGET_nios2     nios2-zephyr-elf)
-set(CROSS_COMPILE_TARGET_riscv   riscv32-zephyr-elf)
+set(CROSS_COMPILE_TARGET_riscv   riscv64-zephyr-elf)
 set(CROSS_COMPILE_TARGET_mips     mipsel-zephyr-elf)
 set(CROSS_COMPILE_TARGET_xtensa   xtensa-zephyr-elf)
 set(CROSS_COMPILE_TARGET_arc         arc-zephyr-elf)
-set(CROSS_COMPILE_TARGET_x86        i586-zephyr-elf)
+
+if(CONFIG_X86_64)
+  set(CROSS_COMPILE_TARGET_x86 x86_64-zephyr-elf)
+else()
+  set(CROSS_COMPILE_TARGET_x86 i586-zephyr-elf)
+endif()
 
 set(CROSS_COMPILE_TARGET ${CROSS_COMPILE_TARGET_${ARCH}})
 set(SYSROOT_TARGET       ${CROSS_COMPILE_TARGET})
 
 set(CROSS_COMPILE ${TOOLCHAIN_HOME}/${CROSS_COMPILE_TARGET}/bin/${CROSS_COMPILE_TARGET}-)
-set(SYSROOT_DIR   ${TOOLCHAIN_HOME}/${SYSROOT_TARGET}/${SYSROOT_TARGET})
 
 if("${ARCH}" STREQUAL "xtensa")
   set(SYSROOT_DIR ${TOOLCHAIN_HOME}/${SYSROOT_TARGET})
@@ -24,3 +28,4 @@ if("${ARCH}" STREQUAL "xtensa")
   LIST(APPEND TOOLCHAIN_LIBS hal)
   LIST(APPEND LIB_INCLUDE_DIR -L${SYSROOT_DIR}/lib)
 endif()
+set(SYSROOT_DIR   ${TOOLCHAIN_HOME}/${SYSROOT_TARGET}/${SYSROOT_TARGET})
