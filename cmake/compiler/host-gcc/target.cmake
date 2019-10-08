904dbad7f2aabae97657f070fb76ffecfea2cf78
Daniel Leung <daniel.leung@intel.com>
2019-10-08 01:14:02 -0700

    cmake: need to specify m32/mx32/m64 for x86_64 toolchain
    Since x86_64-zephyr-elf is a multi-lib toolchain, m32/mx32/m64
    need to be specified for the compiler to return the correct
    library path when queried (e.g. --print-libgcc-file-name).
    This affects the compile check done by CMake. Without these
    flags, the compiler returns incorrect toolchain path (e.g.
    requiring 64-bit libraries but returning 32-bit library path).
    This also affects compiler flag checks for "-lstdc++". Incorrect
    library path results in error when checking for "-lstdc++", and
    this flag will not be used for the build. This results in
    undefined references when compiling C++ code.
    
    This creates target_x86.cmake to add the necessary flags for
    CMake to use. The target_x86_64.cmake is also created to
    mirror the same change.
    
    Also removing the -m32 flags for host-gcc since we are not
    building x86 targets with the host-gcc compiler.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- cmake/compiler/host-gcc/target.cmake
+++ cmake/compiler/host-gcc/target.cmake
@@ -4,18 +4,6 @@
 
 find_program(CMAKE_C_COMPILER gcc)
 
-# -march={pentium,lakemont,...} do not automagically imply -m32, so
-# adding it here.
-
-# Maybe the -m32/-miamcu FLAGS should all be next to -march= in the
-# longer term?
-if (CONFIG_X86)
-  string(PREPEND CMAKE_ASM_FLAGS             "-m32 ")
-  string(PREPEND CMAKE_C_FLAGS               "-m32 ")
-  string(PREPEND CMAKE_CXX_FLAGS             "-m32 ")
-  string(PREPEND CMAKE_SHARED_LINKER_FLAGS   "-m32 ") # unused?
-endif()
-
 if(CONFIG_CPLUSPLUS)
   set(cplusplus_compiler g++)
 else()
