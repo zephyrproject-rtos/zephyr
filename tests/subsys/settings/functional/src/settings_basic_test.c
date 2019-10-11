7cd1a1bd7b07369531b39181bb108b0cf43f11fc
Kamil Piszczek <Kamil.Piszczek@nordicsemi.no>
2019-10-11 15:06:30 +0200

    tests: settings: functional: nvs: adding posix targets
    Added native_posix and native_posix_64 targets to the functional tests
    of NVS.
    
    Signed-off-by: Kamil Piszczek <Kamil.Piszczek@nordicsemi.no>

--- tests/subsys/settings/functional/src/settings_basic_test.c
+++ tests/subsys/settings/functional/src/settings_basic_test.c
@@ -30,7 +30,7 @@ LOG_MODULE_REGISTER(settings_basic_test);
  */
 static void test_clear_settings(void)
 {
-#if IS_ENABLED(CONFIG_SETTINGS_FCB)
+#if IS_ENABLED(CONFIG_SETTINGS_FCB) || IS_ENABLED(CONFIG_SETTINGS_NVS)
 	const struct flash_area *fap;
 	int rc = flash_area_open(DT_FLASH_AREA_STORAGE_ID, &fap);
 
