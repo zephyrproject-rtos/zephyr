ff86aefd5982f083502ecca833ada4be94d83566
Peter Bigot <peter.bigot@nordicsemi.no>
2019-10-16 12:11:18 -0500

    samples: sensor: lps22hb: add sample
    Copy and rename from hts221.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

---
+++ samples/sensor/lps22hb/src/main.c
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2017 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <device.h>
+#include <drivers/sensor.h>
+#include <stdio.h>
+#include <sys/util.h>
+
+static void process_sample(struct device *dev)
+{
+	static unsigned int obs;
+	struct sensor_value pressure, temp;
+
+	if (sensor_sample_fetch(dev) < 0) {
+		printf("Sensor sample update error\n");
+		return;
+	}
+
+	if (sensor_channel_get(dev, SENSOR_CHAN_PRESS, &pressure) < 0) {
+		printf("Cannot read LPS22HB pressure channel\n");
+		return;
+	}
+
+	if (sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp) < 0) {
+		printf("Cannot read LPS22HB temperature channel\n");
+		return;
+	}
+
+	++obs;
+	printf("Observation:%u\n", obs);
+
+	/* display pressure */
+	printf("Pressure:%.1f kPa\n", sensor_value_to_double(&pressure));
+
+	/* display temperature */
+	printf("Temperature:%.1f C\n", sensor_value_to_double(&temp));
+
+}
+
+static void lps22hb_handler(struct device *dev,
+			   struct sensor_trigger *trig)
+{
+	process_sample(dev);
+}
+
+void main(void)
+{
+	struct device *dev = device_get_binding("LPS22HB");
+
+	if (dev == NULL) {
+		printf("Could not get LPS22HB device\n");
+		return;
+	}
+
+	if (IS_ENABLED(CONFIG_LPS22HB_TRIGGER)) {
+		struct sensor_trigger trig = {
+			.type = SENSOR_TRIG_DATA_READY,
+			.chan = SENSOR_CHAN_ALL,
+		};
+		if (sensor_trigger_set(dev, &trig, lps22hb_handler) < 0) {
+			printf("Cannot configure trigger\n");
+			return;
+		};
+	}
+
+	while (!IS_ENABLED(CONFIG_LPS22HB_TRIGGER)) {
+		process_sample(dev);
+		k_sleep(K_MSEC(2000));
+	}
+	k_sleep(K_FOREVER);
+}
