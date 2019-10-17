f6f33dfb8f533ecc1889e3cd4e9d300fe98eeb59
Asger Munk Nielsen <asmk@oticon.com>
2019-10-17 08:43:59 +0200

    Bluetooth: controller: SW deferred privacy build test
    Build test of feature
    
    Signed-off-by: Asger Munk Nielsen <asmk@oticon.com>

---
+++ tests/bluetooth/ctrl_sw_privacy/src/main.c
@@ -0,0 +1,27 @@
+/* main.c - Application main entry point */
+
+/*
+ * Copyright (c) 2015-2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <sys/printk.h>
+#include <ztest.h>
+
+#include <bluetooth/bluetooth.h>
+
+void test_ctrl_sw_privacy(void)
+{
+	zassert_false(bt_enable(NULL), "%s failed", __func__);
+}
+
+/* test case main entry */
+void test_main(void)
+{
+	ztest_test_suite(test_bluetooth,
+			ztest_unit_test(test_ctrl_sw_privacy));
+	ztest_run_test_suite(test_bluetooth);
+}
