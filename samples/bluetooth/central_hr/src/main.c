e9bc466214851903db3b26989d8d49b8e936a0a0
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2019-10-17 17:42:23 +0530

    samples: bluetooth: No transmits after 3 supervision timeouts
    The central_hr samples stops sending PDUs after it has got
    3 times disconnected from peripheral with reason supervision
    timeout.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- samples/bluetooth/central_hr/src/main.c
+++ samples/bluetooth/central_hr/src/main.c
@@ -25,6 +25,58 @@ static struct bt_uuid_16 uuid = BT_UUID_INIT_16(0);
 static struct bt_gatt_discover_params discover_params;
 static struct bt_gatt_subscribe_params subscribe_params;
 
+static int write_cmd(struct bt_conn *conn);
+
+static void write_cmd_cb(struct bt_conn *conn, void *user_data)
+{
+	//write_cmd(conn);
+	//write_cmd(conn);
+}
+
+static int write_cmd(struct bt_conn *conn)
+{
+	u8_t data[244] = {0, };
+	int err;
+
+	err = bt_gatt_write_without_response_cb(conn, 0x0001, data,
+						sizeof(data), false,
+						write_cmd_cb, NULL);
+	if (err) {
+		printk("Write cmd failed (%d).\n", err);
+	}
+
+	return err;
+}
+
+static void mtu_exchange_cb(struct bt_conn *conn, u8_t err,
+			    struct bt_gatt_exchange_params *params)
+{
+	printk("MTU exchange %s (%u)\n", err == 0U ? "successful" : "failed",
+	       bt_gatt_get_mtu(conn));
+
+	//write_cmd(conn);
+}
+
+static struct bt_gatt_exchange_params mtu_exchange_params;
+
+static int mtu_exchange(struct bt_conn *conn)
+{
+	int err;
+
+	printk("MTU: %u\n", bt_gatt_get_mtu(conn));
+
+	mtu_exchange_params.func = mtu_exchange_cb;
+
+	err = bt_gatt_exchange_mtu(conn, &mtu_exchange_params);
+	if (err) {
+		printk("MTU exchange failed (err %d)", err);
+	} else {
+		printk("Exchange pending...");
+	}
+
+	return err;
+}
+
 static u8_t notify_func(struct bt_conn *conn,
 			   struct bt_gatt_subscribe_params *params,
 			   const void *data, u16_t length)
@@ -94,36 +146,6 @@ static u8_t discover_func(struct bt_conn *conn,
 	return BT_GATT_ITER_STOP;
 }
 
-static void connected(struct bt_conn *conn, u8_t conn_err)
-{
-	char addr[BT_ADDR_LE_STR_LEN];
-	int err;
-
-	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-	if (conn_err) {
-		printk("Failed to connect to %s (%u)\n", addr, conn_err);
-		return;
-	}
-
-	printk("Connected: %s\n", addr);
-
-	if (conn == default_conn) {
-		memcpy(&uuid, BT_UUID_HRS, sizeof(uuid));
-		discover_params.uuid = &uuid.uuid;
-		discover_params.func = discover_func;
-		discover_params.start_handle = 0x0001;
-		discover_params.end_handle = 0xffff;
-		discover_params.type = BT_GATT_DISCOVER_PRIMARY;
-
-		err = bt_gatt_discover(default_conn, &discover_params);
-		if (err) {
-			printk("Discover failed(err %d)\n", err);
-			return;
-		}
-	}
-}
-
 static bool eir_found(struct bt_data *data, void *user_data)
 {
 	bt_addr_le_t *addr = user_data;
@@ -171,6 +193,7 @@ static void device_found(const bt_addr_le_t *addr, s8_t rssi, u8_t type,
 	char dev[BT_ADDR_LE_STR_LEN];
 
 	bt_addr_le_to_str(addr, dev, sizeof(dev));
+
 	printk("[DEVICE]: %s, AD evt type %u, AD data len %u, RSSI %i\n",
 	       dev, type, ad->len, rssi);
 
@@ -180,6 +203,42 @@ static void device_found(const bt_addr_le_t *addr, s8_t rssi, u8_t type,
 	}
 }
 
+static struct bt_conn *g_conn;
+
+static void connected(struct bt_conn *conn, u8_t conn_err)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+	int err;
+
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	if (conn_err) {
+		printk("Failed to connect to %s (%u)\n", addr, conn_err);
+		return;
+	}
+
+	printk("Connected: %s\n", addr);
+
+	g_conn = conn;
+
+	mtu_exchange(conn);
+
+	#if 0
+	memcpy(&uuid, BT_UUID_HRS, sizeof(uuid));
+	discover_params.uuid = &uuid.uuid;
+	discover_params.func = discover_func;
+	discover_params.start_handle = 0x0001;
+	discover_params.end_handle = 0xffff;
+	discover_params.type = BT_GATT_DISCOVER_PRIMARY;
+
+	err = bt_gatt_discover(conn, &discover_params);
+	if (err) {
+		printk("Discover failed (err %d)\n", err);
+	}
+	#endif
+}
+
 static void disconnected(struct bt_conn *conn, u8_t reason)
 {
 	char addr[BT_ADDR_LE_STR_LEN];
@@ -187,13 +246,11 @@ static void disconnected(struct bt_conn *conn, u8_t reason)
 
 	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
-	printk("Disconnected: %s (reason 0x%02x)\n", addr, reason);
+	printk("Disconnected: %s (reason %u)\n", addr, reason);
 
-	if (default_conn != conn) {
-		return;
-	}
+	g_conn = NULL;
 
-	bt_conn_unref(default_conn);
+	bt_conn_unref(conn);
 	default_conn = NULL;
 
 	/* This demo doesn't require active scan */
@@ -211,13 +268,12 @@ static struct bt_conn_cb conn_callbacks = {
 void main(void)
 {
 	int err;
-	err = bt_enable(NULL);
 
+	err = bt_enable(NULL);
 	if (err) {
 		printk("Bluetooth init failed (err %d)\n", err);
 		return;
 	}
-
 	printk("Bluetooth initialized\n");
 
 	bt_conn_cb_register(&conn_callbacks);
@@ -230,4 +286,12 @@ void main(void)
 	}
 
 	printk("Scanning successfully started\n");
+
+	while (1) {
+		//k_sleep(MSEC_PER_SEC);
+		k_sleep(K_MSEC(1));
+		if (g_conn) {
+			write_cmd(g_conn);
+		}
+	}
 }
