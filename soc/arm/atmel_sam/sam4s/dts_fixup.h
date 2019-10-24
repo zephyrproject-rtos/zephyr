f3663292bccd6b9ebc9763d86ed2083e1acfdc1f
Gerson Fernando Budke <nandojve@gmail.com>
2019-10-24 20:12:52 -0300

    soc: arm: sam4s: Add spi driver support
    Add device tree fixups and pinmap to enable spi driver.
    
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

--- soc/arm/atmel_sam/sam4s/dts_fixup.h
+++ soc/arm/atmel_sam/sam4s/dts_fixup.h
@@ -1,4 +1,8 @@
-/* SPDX-License-Identifier: Apache-2.0 */
+/*
+ * Copyright (c) 2019 Gerson Fernando Budke
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 /* This file is a temporary workaround for mapping of the generated information
  * to the current driver definitions.  This will be removed when the drivers
@@ -38,6 +42,12 @@
 #define DT_I2C_1_IRQ		DT_ATMEL_SAM_I2C_TWI_4001C000_IRQ_0
 #define DT_I2C_1_IRQ_PRI		DT_ATMEL_SAM_I2C_TWI_4001C000_IRQ_0_PRIORITY
 #define DT_I2C_1_PERIPHERAL_ID	DT_ATMEL_SAM_I2C_TWI_4001C000_PERIPHERAL_ID
+
+#define DT_SPI_0_BASE_ADDRESS       DT_ATMEL_SAM_SPI_40008000_BASE_ADDRESS
+#define DT_SPI_0_NAME               DT_ATMEL_SAM_SPI_40008000_LABEL
+#define DT_SPI_0_IRQ                DT_ATMEL_SAM_SPI_40008000_IRQ_0
+#define DT_SPI_0_IRQ_PRI            DT_ATMEL_SAM_SPI_40008000_IRQ_0_PRIORITY
+#define DT_SPI_0_PERIPHERAL_ID      DT_ATMEL_SAM_SPI_40008000_PERIPHERAL_ID
 
 #define DT_UART_SAM_PORT_0_NAME	DT_ATMEL_SAM_UART_400E0600_LABEL
 #define DT_UART_SAM_PORT_0_BAUD_RATE	DT_ATMEL_SAM_UART_400E0600_CURRENT_SPEED
