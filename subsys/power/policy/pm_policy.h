de931d501eab68235c7c3a5b7e93c439bcfa2367
Wentong Wu <wentong.wu@intel.com>
2019-10-26 03:51:25 +0800

    Style: remove extern on the function declarations in the header
    Remove extern on the function declarations in the header file to
    keep consistent with others.
    
    Signed-off-by: Wentong Wu <wentong.wu@intel.com>

--- subsys/power/policy/pm_policy.h
+++ subsys/power/policy/pm_policy.h
@@ -16,27 +16,27 @@ extern "C" {
 /**
  * @brief Function to create device PM list
  */
-extern  void sys_pm_create_device_list(void);
+void sys_pm_create_device_list(void);
 
 /**
  * @brief Function to suspend the devices in PM device list
  */
-extern int sys_pm_suspend_devices(void);
+int sys_pm_suspend_devices(void);
 
 /**
  * @brief Function to force suspend the devices in PM device list
  */
-extern int sys_pm_force_suspend_devices(void);
+int sys_pm_force_suspend_devices(void);
 
 /**
  * @brief Function to resume the devices in PM device list
  */
-extern void sys_pm_resume_devices(void);
+void sys_pm_resume_devices(void);
 
 /**
  * @brief Function to get the next PM state based on the ticks
  */
-extern enum power_states sys_pm_policy_next_state(s32_t ticks);
+enum power_states sys_pm_policy_next_state(s32_t ticks);
 
 #ifdef __cplusplus
 }
