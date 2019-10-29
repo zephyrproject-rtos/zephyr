7308fdc3c7018985eafa1d717b44bc76fdc9b55b
Daniel Leung <daniel.leung@intel.com>
2019-10-29 12:58:07 -0700

    cmake: add --divide to GNU assembler options for x86
    GNU Assembler, by default on non-Linux targets, treats slashes as
    start of comments on i386.
    (https://sourceware.org/binutils/docs-2.33.1/as/i386_002dChars.html#i386_002dChars)
    In order to use division, `--divide` needs to be passed to
    the assembler.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- cmake/compiler/gcc/target_x86.cmake
+++ cmake/compiler/gcc/target_x86.cmake
@@ -9,3 +9,10 @@ else()
   string(PREPEND CMAKE_C_FLAGS   "-m32 ")
   string(PREPEND CMAKE_CXX_FLAGS "-m32 ")
 endif()
+
+# GNU Assembler, by default on non-Linux targets, treats slashes as
+# start of comments on i386.
+# (https://sourceware.org/binutils/docs-2.33.1/as/i386_002dChars.html#i386_002dChars)
+# In order to use division, `--divide` needs to be passed to
+# the assembler.
+list(APPEND TOOLCHAIN_C_FLAGS -Wa,--divide)
