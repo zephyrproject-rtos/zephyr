e362e7838ad317306f71dd85b90be63541cdeadb
Kumar Gala <kumar.gala@linaro.org>
2019-10-31 00:02:07 -0500

    samples: i2c_scanner: show example of device_get_dt_binding
    Hack up device_get_dt_binding, its really device_get_dt_binding_by_inst.
    Need to figure out how to handle the extern of __device_ZDID.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- samples/drivers/i2c_scanner/src/main.c
+++ samples/drivers/i2c_scanner/src/main.c
@@ -9,12 +9,18 @@
 #include <sys/printk.h>
 #include <device.h>
 #include <drivers/i2c.h>
+#include <generated_dts_board.h>
 
 #ifdef ARDUINO_I2C_LABEL
 #define I2C_DEV ARDUINO_I2C_LABEL
 #else
 #define I2C_DEV "I2C_0"
 #endif
+
+#define device_get_dt_binding_zdid(zdid) _CONCAT(&__device_, zdid)
+
+#define device_get_dt_binding(name, inst) \
+device_get_dt_binding_zdid(DT_ZDID_INST_##inst##_##name) \
 
 /**
  * @file This app scans I2C bus for any devices present
@@ -26,7 +32,7 @@ void main(void)
 
 	printk("Starting i2c scanner...\n");
 
-	i2c_dev = device_get_binding(I2C_DEV);
+	i2c_dev = device_get_dt_binding(SILABS_GECKO_I2C, 0);
 	if (!i2c_dev) {
 		printk("I2C: Device driver not found.\n");
 		return;
