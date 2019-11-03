c6a8ca39350bf5dcb001da91647b62ae4960e461
Gerson Fernando Budke <nandojve@gmail.com>
2019-11-02 23:09:04 -0300

    boards: arm: Add sam4e_xpro board
    Add sam4e_xpro board files. This enable use of Atmel sam4e SoC.
    
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

---
+++ boards/arm/sam4e_xpro/pinmux.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2019 Gerson Fernando Budke
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+
+static int sam4e_xpro_init(struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	return 0;
+}
+
+SYS_INIT(sam4e_xpro_init, PRE_KERNEL_1, 0);
