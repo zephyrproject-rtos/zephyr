4e8f6f0fe5bacfd64b377aed2b753547d359285a
Erwan Gouriou <erwan.gouriou@linaro.org>
2019-11-05 15:50:53 +0100

    sensors: lis3mdl: Fix warning when TIGGER_OWN_THREAD is enabled
    No need to cast dev in  k_thread_create.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- drivers/sensor/lis3mdl/lis3mdl_trigger.c
+++ drivers/sensor/lis3mdl/lis3mdl_trigger.c
@@ -140,7 +140,7 @@ int lis3mdl_init_interrupt(struct device *dev)
 
 	k_thread_create(&drv_data->thread, drv_data->thread_stack,
 			CONFIG_LIS3MDL_THREAD_STACK_SIZE,
-			(k_thread_entry_t)lis3mdl_thread, POINTER_TO_INT(dev),
+			(k_thread_entry_t)lis3mdl_thread, dev,
 			0, NULL, K_PRIO_COOP(CONFIG_LIS3MDL_THREAD_PRIORITY),
 			0, K_NO_WAIT);
 #elif defined(CONFIG_LIS3MDL_TRIGGER_GLOBAL_THREAD)
