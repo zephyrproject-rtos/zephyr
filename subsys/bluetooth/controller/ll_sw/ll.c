83e005b318dd0c312c93e5fac1cd9916bc6c8167
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2019-11-07 11:39:45 +0530

    Bluetooth: controller: legacy: Add missing BT_DEBUG_ENABLED define
    Add missing BT_DEBUG_ENABLED definitions.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ll.c
+++ subsys/bluetooth/controller/ll_sw/ll.c
@@ -15,7 +15,7 @@
 #include <bluetooth/hci.h>
 
 #define BT_DBG_ENABLED IS_ENABLED(CONFIG_BT_DEBUG_HCI_DRIVER)
-#define LOG_MODULE_NAME bt_ctlr_llsw_ll
+#define LOG_MODULE_NAME bt_ctlr_ll
 #include "common/log.h"
 
 #include "hal/cpu.h"
