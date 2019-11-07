7db06bfa5670d66eb2c46995d8e74e3246887f4f
Erwan Gouriou <erwan.gouriou@linaro.org>
2019-11-07 14:42:21 +0100

    dts: stm32: Introduce linux like pinctrl dt bindings
    Introduce Linux like dts bindings for stm32 devices.
    Difference is that pinctrl subnodes describe a single pin
    configuration for a dedicated function (eg: usart1_tx_pa9) while
    linux proposes directly pins combinations aiming at configuring an IP.
    Reason for this choice is to be able to provide a complete description
    of possible configurations in a finite number of subnodes.
    This will prevent need to modify these files each time a user wants
    to use a new pins combination for his own board.
    Other advantage is that pin configuration will appear more clearly
    in board dts files.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- include/dt-bindings/pinctrl/stm32-pinctrl-common.h
+++ include/dt-bindings/pinctrl/stm32-pinctrl-common.h
@@ -7,6 +7,31 @@
 #ifndef ZEPHYR_STM32_PINCTRL_COMMON_H_
 #define ZEPHYR_STM32_PINCTRL_COMMON_H_
 
+/* Extracted from Linux: include/dt-bindings/pinctrl/stm32-pinfunc.h */
+/*  define PIN modes */
+#define GPIO	0x0
+#define AF0	0x1
+#define AF1	0x2
+#define AF2	0x3
+#define AF3	0x4
+#define AF4	0x5
+#define AF5	0x6
+#define AF6	0x7
+#define AF7	0x8
+#define AF8	0x9
+#define AF9	0xa
+#define AF10	0xb
+#define AF11	0xc
+#define AF12	0xd
+#define AF13	0xe
+#define AF14	0xf
+#define AF15	0x10
+#define ANALOG	0x11
+
+/* define Pins number*/
+#define PIN_NO(port, line)	(((port) - 'A') * 0x10 + (line))
+
+#define STM32_PINMUX(port, line, mode) (((PIN_NO(port, line)) << 8) | (mode))
 
 /**
  * @brief numerical IDs for IO ports
