a61290e1a337dfdd4baceb63e6dac8eb96693629
Carlo Caione <ccaione@baylibre.com>
2019-11-10 17:43:26 +0000

    arch: arm64: Add support for qemu_cortex_a53 board
    This patch introduces support for the qemu_cortex_a53 board emulated
    using QEMU (virt platform) adding SoC, board and DTS files.
    
    | ./scripts/sanitycheck -p qemu_cortex_a53
    |
    | Total complete:  190/ 190  100%  skipped:   40, failed:    0
    | 150 of 150 tests passed (100.00%), 0 failed,
    |     40 skipped with 0 warnings in 580.93 seconds
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

---
+++ boards/arm/qemu_cortex_a53/board.cmake
@@ -0,0 +1,14 @@
+# Copyright (c) 2019 Carlo Caione <ccaione@baylibre.com>
+# SPDX-License-Identifier: Apache-2.0
+
+set(EMU_PLATFORM qemu)
+set(QEMU_ARCH aarch64)
+
+set(QEMU_CPU_TYPE_${ARCH} cortex-a53)
+set(QEMU_FLAGS_${ARCH}
+  -cpu ${QEMU_CPU_TYPE_${ARCH}}
+  -nographic
+  -machine virt
+  )
+
+board_set_debugger_ifnset(qemu)
