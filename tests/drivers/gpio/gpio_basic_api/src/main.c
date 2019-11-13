ae7a59eae4b877b1990c576b307f185c3a369484
Peter Bigot <peter.bigot@nordicsemi.no>
2019-11-13 11:01:37 -0600

    test/drivers/gpio: add tests for new API
    Test that the new port API functions all behave as expected, including
    physical vs logical level for input and output as well as masked and
    set-based output operations.  Also tests the new pin API functions.
    
    For running on real hardware this test now uses a local test-specific
    devicetree binding.  For build-only tests any platform with a GPIO
    alias should be tested.
    
    The new code increases flash requirements so add a filter to exclude
    platforms that won't link.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- tests/drivers/gpio/gpio_basic_api/src/main.c
+++ tests/drivers/gpio/gpio_basic_api/src/main.c
@@ -7,9 +7,40 @@
 
 #include "test_gpio.h"
 
+/* Grotesque hack for pinmux boards */
+#ifdef CONFIG_BOARD_FRDM_K64F
+#include <drivers/pinmux.h>
+#include <fsl_port.h>
+#endif
+
+static void board_setup(void)
+{
+#ifdef DT_INST_0_TEST_GPIO_BASIC_API
+	/* PIN_IN and PIN_OUT must be on same controller. */
+	if (strcmp(DT_INST_0_TEST_GPIO_BASIC_API_OUT_GPIOS_CONTROLLER,
+		   DT_INST_0_TEST_GPIO_BASIC_API_IN_GPIOS_CONTROLLER) != 0) {
+		printk("FATAL: output controller %s != input controller %s\n",
+		       DT_INST_0_TEST_GPIO_BASIC_API_OUT_GPIOS_CONTROLLER,
+		       DT_INST_0_TEST_GPIO_BASIC_API_IN_GPIOS_CONTROLLER);
+		k_panic();
+	}
+#endif
+
+#ifdef CONFIG_BOARD_FRDM_K64F
+	/* TODO figure out how to get this from "GPIO_2" */
+	const char *pmx_name = "portc";
+	struct device *pmx = device_get_binding(pmx_name);
+
+	pinmux_pin_set(pmx, PIN_OUT, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(pmx, PIN_IN, PORT_PCR_MUX(kPORT_MuxAsGpio));
+#endif
+}
+
 void test_main(void)
 {
+	board_setup();
 	ztest_test_suite(gpio_basic_test,
+			 ztest_unit_test(test_gpio_port),
 			 ztest_unit_test(test_gpio_pin_read_write),
 			 ztest_unit_test(test_gpio_callback_edge_high),
 			 ztest_unit_test(test_gpio_callback_edge_low),
