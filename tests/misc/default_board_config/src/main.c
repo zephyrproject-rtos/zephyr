5f5d027b9228a4d45eeed68dacbdc5e69580c93b
Erwan Gouriou <erwan.gouriou@linaro.org>
2019-11-19 17:38:42 +0100

    tests: Add test to check default board configuration compliance
    Aim of the test is to enforce compliance to default board
    configuration guidelines on in tree boards (out of tree boards
    are filtered out).
    Test is build_only and error is generated if any driver is enabled
    aside from basic gpio, pinmux, clock_control, serial or any subsystem
    other than console.
    
    Fixes #19116
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

---
+++ tests/misc/default_board_config/src/main.c
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2019 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+
+#ifdef CONFIG_IEEE802154
+#error IEEE802154 should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_ADC
+#error ADC should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_AUDIO
+#error AUDIO should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_BT
+#error BT should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_CAN
+#error CAN should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_COUNTER
+#error COUNTER should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_DMA
+#error DMA should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_DISK_ACCESS
+#error DISK_ACCESS should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_CRYPTO
+#error CRYPTO should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_CRYPTO
+#error CRYPTO should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_DISPLAY
+#error  should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_DMA
+#error DMA should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_EEPROM
+#error EEPROM should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_ESPI
+#error ESPI should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_FLASH
+#error FLASH should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_I2C
+#error I2C should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_I2S
+#error I2S should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_IPM
+#error IPM should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_KSCAN
+#error KSCAN should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_LED
+#error LED should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_MODEM
+#error MODEM should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_NEURAL_NET_ACCEL
+#error NEURAL_NET_ACCEL should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_NET
+#error NET should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_PCIE
+#error PCIE should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_PS2
+#error PS2 should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_PWN
+#error PWM should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_USB
+#error USB should not be part of default board configuration
+#endif
+
+#ifdef CONFIG_VIDEO
+#error VIDEO should not be part of default board configuration
+#endif
+
+
+
+void main(void)
+{
+}
