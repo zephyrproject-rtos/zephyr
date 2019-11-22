3ed3ea0c829243329734395ed874973b1bb77742
Ulf Magnusson <Ulf.Magnusson@nordicsemi.no>
2019-11-22 19:34:11 +0100

    ci: run_ci: Use lowercase for internal vars
    This makes it possible to tell at a glance which variables are internal
    to the script and which ones are parameters to it, which is very
    helpful.
    
    This convention is pretty common. See e.g. Google's shell style guide at
    https://google.github.io/styleguide/shell.xml#Naming_Conventions, and
    https://github.com/icy/bash-coding-style#naming-and-styles. It's older
    than those though.
    
    Signed-off-by: Ulf Magnusson <Ulf.Magnusson@nordicsemi.no>

--- scripts/ci/run_ci.sh
+++ scripts/ci/run_ci.sh
@@ -20,17 +20,17 @@
 
 set -xe
 
-SANITYCHECK_OPTIONS=" --inline-logs -N --timestamps"
+sanitycheck_options=" --inline-logs -N --timestamps"
 export BSIM_OUT_PATH="${BSIM_OUT_PATH:-/opt/bsim/}"
 if [ ! -d "${BSIM_OUT_PATH}" ]; then
         unset BSIM_OUT_PATH
 fi
 export BSIM_COMPONENTS_PATH="${BSIM_OUT_PATH}/components/"
-BSIM_BT_TEST_RESULTS_FILE="./bsim_bt_out/bsim_results.xml"
-WEST_COMMANDS_RESULTS_FILE="./pytest_out/west_commands.xml"
+bsim_bt_test_results_file="./bsim_bt_out/bsim_results.xml"
+west_commands_results_file="./pytest_out/west_commands.xml"
 
-MATRIX_BUILDS=1
-MATRIX=1
+matrix_builds=1
+matrix=1
 
 function handle_coverage() {
 	# this is for shippable coverage reports
@@ -40,7 +40,7 @@ function handle_coverage() {
 
 	# Upload to codecov.io only on merged builds or if CODECOV_IO variable
 	# is set.
-	if [ -n "${CODECOV_IO}" -o -z "${PULL_REQUEST_NR}" ]; then
+	if [ -n "${CODECOV_IO}" -o -z "${pull_request_nr}" ]; then
 		# Capture data
 		echo "Running lcov --capture ..."
 		lcov --capture \
@@ -97,17 +97,17 @@ function on_complete() {
 		cp ./sanity-out/sanitycheck.xml shippable/testresults/;
 	fi;
 
-	if [ -e ${BSIM_BT_TEST_RESULTS_FILE} ]; then
-		echo "Copy ${BSIM_BT_TEST_RESULTS_FILE}"
-		cp ${BSIM_BT_TEST_RESULTS_FILE} shippable/testresults/;
+	if [ -e ${bsim_bt_test_results_file} ]; then
+		echo "Copy ${bsim_bt_test_results_file}"
+		cp ${bsim_bt_test_results_file} shippable/testresults/;
 	fi;
 
-	if [ -e ${WEST_COMMANDS_RESULTS_FILE} ]; then
-		echo "Copy ${WEST_COMMANDS_RESULTS_FILE}"
-		cp ${WEST_COMMANDS_RESULTS_FILE} shippable/testresults;
+	if [ -e ${west_commands_results_file} ]; then
+		echo "Copy ${west_commands_results_file}"
+		cp ${west_commands_results_file} shippable/testresults;
 	fi;
 
-	if [ "$MATRIX" = "1" ]; then
+	if [ "$matrix" = "1" ]; then
 		echo "Skip handling coverage data..."
 		#handle_coverage
 	else
@@ -117,7 +117,7 @@ function on_complete() {
 
 
 function build_btsim() {
-	NRF_HW_MODELS_VERSION=`cat boards/posix/nrf52_bsim/hw_models_version`
+	nrf_hw_models_version=`cat boards/posix/nrf52_bsim/hw_models_version`
 	pushd . ;
 	cd ${BSIM_COMPONENTS_PATH} ;
 	if [ -d ext_NRF52_hw_models ]; then
@@ -130,7 +130,7 @@ function build_btsim() {
 			exit 1;
 		)
 	else
-		git clone -b ${NRF_HW_MODELS_VERSION} \
+		git clone -b ${nrf_hw_models_version} \
 		https://github.com/BabbleSim/ext_NRF52_hw_models.git
 	fi
 	cd ${BSIM_OUT_PATH}
@@ -140,20 +140,20 @@ function build_btsim() {
 
 function run_bsim_bt_tests() {
 	WORK_DIR=${ZEPHYR_BASE}/bsim_bt_out tests/bluetooth/bsim_bt/compile.sh
-	RESULTS_FILE=${ZEPHYR_BASE}/${BSIM_BT_TEST_RESULTS_FILE} \
+	RESULTS_FILE=${ZEPHYR_BASE}/${bsim_bt_test_results_file} \
 	SEARCH_PATH=tests/bluetooth/bsim_bt/bsim_test_app/tests_scripts \
 	tests/bluetooth/bsim_bt/run_parallel.sh
 }
 
 function get_tests_to_run() {
-	./scripts/ci/get_modified_tests.py --commits ${COMMIT_RANGE} > modified_tests.args;
-	./scripts/ci/get_modified_boards.py --commits ${COMMIT_RANGE} > modified_boards.args;
+	./scripts/ci/get_modified_tests.py --commits ${commit_range} > modified_tests.args;
+	./scripts/ci/get_modified_boards.py --commits ${commit_range} > modified_boards.args;
 
 	if [ -s modified_boards.args ]; then
-		${SANITYCHECK} ${SANITYCHECK_OPTIONS} +modified_boards.args --save-tests test_file_1.txt || exit 1;
+		${sanitycheck} ${sanitycheck_options} +modified_boards.args --save-tests test_file_1.txt || exit 1;
 	fi
 	if [ -s modified_tests.args ]; then
-		${SANITYCHECK} ${SANITYCHECK_OPTIONS} +modified_tests.args --save-tests test_file_2.txt || exit 1;
+		${sanitycheck} ${sanitycheck_options} +modified_tests.args --save-tests test_file_2.txt || exit 1;
 	fi
 	rm -f modified_tests.args modified_boards.args;
 }
@@ -161,10 +161,10 @@ function get_tests_to_run() {
 
 function west_setup() {
 	# West handling
-	GIT_DIR=$(basename $PWD)
+	git_dir=$(basename $PWD)
 	pushd ..
 	if [ ! -d .west ]; then
-		west init -l ${GIT_DIR}
+		west init -l ${git_dir}
 		west update
 	fi
 	popd
@@ -175,44 +175,44 @@ while getopts ":p:m:b:r:M:cfslR:" opt; do
 	case $opt in
 		c)
 			echo "Execute CI" >&2
-			MAIN_CI=1
+			main_ci=1
 			;;
 		l)
 			echo "Executing script locally" >&2
-			LOCAL_RUN=1
-			MAIN_CI=1
+			local_run=1
+			main_ci=1
 			;;
 		s)
 			echo "Success" >&2
-			SUCCESS=1
+			success=1
 			;;
 		f)
 			echo "Failure" >&2
-			FAILURE=1
+			failure=1
 			;;
 		p)
 			echo "Testing a Pull Request: $OPTARG." >&2
-			PULL_REQUEST_NR=$OPTARG
+			pull_request_nr=$OPTARG
 			;;
 		m)
 			echo "Running on Matrix $OPTARG" >&2
-			MATRIX=$OPTARG
+			matrix=$OPTARG
 			;;
 		M)
 			echo "Running a matrix of $OPTARG slaves" >&2
-			MATRIX_BUILDS=$OPTARG
+			matrix_builds=$OPTARG
 			;;
 		b)
 			echo "Base Branch: $OPTARG" >&2
-			BRANCH=$OPTARG
+			branch=$OPTARG
 			;;
 		r)
 			echo "Remote: $OPTARG" >&2
-			REMOTE=$OPTARG
+			remote=$OPTARG
 			;;
 		R)
 			echo "Range: $OPTARG" >&2
-			RANGE=$OPTARG
+			range=$OPTARG
 			;;
 		\?)
 			echo "Invalid option: -$OPTARG" >&2
@@ -220,32 +220,32 @@ while getopts ":p:m:b:r:M:cfslR:" opt; do
 	esac
 done
 
-if [ -n "$MAIN_CI" ]; then
+if [ -n "$main_ci" ]; then
 
 	west_setup
 
-	if [ -z "$BRANCH" ]; then
+	if [ -z "$branch" ]; then
 		echo "No base branch given"
 		exit 1
 	else
-		COMMIT_RANGE=$REMOTE/${BRANCH}..HEAD
-		echo "Commit range:" ${COMMIT_RANGE}
+		commit_range=$remote/${branch}..HEAD
+		echo "Commit range:" ${commit_range}
 	fi
-	if [ -n "$RANGE" ]; then
-		COMMIT_RANGE=$RANGE
+	if [ -n "$range" ]; then
+		commit_range=$range
 	fi
 	source zephyr-env.sh
-	SANITYCHECK="${ZEPHYR_BASE}/scripts/sanitycheck"
+	sanitycheck="${ZEPHYR_BASE}/scripts/sanitycheck"
 
 	# Possibly the only record of what exact version is being tested:
 	short_git_log='git log -n 5 --oneline --decorate --abbrev=12 '
 
-	if [ -n "$PULL_REQUEST_NR" ]; then
-		$short_git_log $REMOTE/${BRANCH}
+	if [ -n "$pull_request_nr" ]; then
+		$short_git_log $remote/${branch}
 		# Now let's pray this script is being run from a
 		# different location
 # https://stackoverflow.com/questions/3398258/edit-shell-script-while-its-running
-		git rebase $REMOTE/${BRANCH};
+		git rebase $remote/${branch};
 	fi
 	$short_git_log
 
@@ -255,22 +255,22 @@ if [ -n "$MAIN_CI" ]; then
 		build_btsim
 
 		# Run BLE tests in simulator on the 1st CI instance:
-		if [ "$MATRIX" = "1" ]; then
+		if [ "$matrix" = "1" ]; then
 			run_bsim_bt_tests
 		fi
 	else
 		echo "Skipping BT simulator tests"
 	fi
 
-	if [ "$MATRIX" = "1" ]; then
+	if [ "$matrix" = "1" ]; then
 		# Run pytest-based testing for Python in matrix
 		# builder 1.  For now, this is just done for the west
 		# extension commands, but additional directories which
 		# run pytest could go here too.
-		PYTEST=$(type -p pytest-3 || echo "pytest")
-		mkdir -p $(dirname ${WEST_COMMANDS_RESULTS_FILE})
-		PYTHONPATH=./scripts/west_commands "${PYTEST}" \
-			  --junitxml=${WEST_COMMANDS_RESULTS_FILE} \
+		pytest=$(type -p pytest-3 || echo "pytest")
+		mkdir -p $(dirname ${west_commands_results_file})
+		PYTHONPATH=./scripts/west_commands "${pytest}" \
+			  --junitxml=${west_commands_results_file} \
 			  ./scripts/west_commands/tests
 	else
 		echo "Skipping west command tests"
@@ -281,24 +281,24 @@ if [ -n "$MAIN_CI" ]; then
 	touch test_file_1.txt test_file_2.txt
 
 	# In a pull-request see if we have changed any tests or board definitions
-	if [ -n "${PULL_REQUEST_NR}" -o -n "${LOCAL_RUN}"  ]; then
+	if [ -n "${pull_request_nr}" -o -n "${local_run}"  ]; then
 		get_tests_to_run
 	fi
 
 	# Save list of tests to be run
-	${SANITYCHECK} ${SANITYCHECK_OPTIONS} --save-tests test_file_3.txt || exit 1
+	${sanitycheck} ${sanitycheck_options} --save-tests test_file_3.txt || exit 1
 	cat test_file_1.txt test_file_2.txt test_file_3.txt > test_file.txt
 
 	# Run a subset of tests based on matrix size
-	${SANITYCHECK} ${SANITYCHECK_OPTIONS} --load-tests test_file.txt \
-		--subset ${MATRIX}/${MATRIX_BUILDS} --retry-failed 3
+	${sanitycheck} ${sanitycheck_options} --load-tests test_file.txt \
+		--subset ${matrix}/${matrix_builds} --retry-failed 3
 
 	# cleanup
 	rm -f test_file*
 
-elif [ -n "$FAILURE" ]; then
+elif [ -n "$failure" ]; then
 	on_complete failure
-elif [ -n "$SUCCESS" ]; then
+elif [ -n "$success" ]; then
 	on_complete
 else
 	echo "Nothing to do"
