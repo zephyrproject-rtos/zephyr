3b066d4cf50a5941cf232e7183fd6bdab6261834
Andrew Boie <andrew.p.boie@intel.com>
2019-11-21 22:11:39 -0800

    tests: userspace: fix 64-bit issues
    In addition to not assuming all pointers fit in a u32_t,
    logic is added to find the privilege mode stack on x86_64
    and several error messages now contain more information.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- tests/kernel/mem_protect/userspace/src/test_syscall.h
+++ tests/kernel/mem_protect/userspace/src/test_syscall.h
@@ -7,7 +7,7 @@
 #ifndef USERSPACE_TEST_SYSCALL_H
 #define USERSPACE_TEST_SYSCALL_H
 
-__syscall void stack_info_get(u32_t *start_addr, u32_t *size);
+__syscall void stack_info_get(char **start_addr, size_t *size);
 __syscall int check_perms(void *addr, size_t size, int write);
 __syscall void missing_syscall(void);
 
