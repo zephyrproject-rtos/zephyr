102d402fc586ebe98bc798782c4cce46c027e1bc
Vikrant More <vikrant8051@gmail.com>
2019-11-29 01:04:50 +0530

    samples: mesh: nrf52: added some more macros support
    Added some more macros support.
    
    Signed-off-by: Vikrant More <vikrant8051@gmail.com>

--- samples/boards/nrf/mesh/onoff_level_lighting_vnd_app/src/mesh/publisher.c
+++ samples/boards/nrf/mesh/onoff_level_lighting_vnd_app/src/mesh/publisher.c
@@ -102,7 +102,7 @@ void publish(struct k_work *work)
 		err = bt_mesh_model_publish(&root_models[5]);
 #elif defined(LIGHT_LIGHTNESS_TT)
 		bt_mesh_model_msg_init(root_models[13].pub->msg,
-				       BT_MESH_MODEL_OP_2(0x82, 0x4D));
+				       BT_MESH_MODEL_LIGHT_LIGHTNESS_SET_UNACK);
 		net_buf_simple_add_le16(root_models[13].pub->msg, LEVEL_U25);
 		net_buf_simple_add_u8(root_models[13].pub->msg, tid++);
 		net_buf_simple_add_u8(root_models[13].pub->msg, 0x45);
@@ -110,7 +110,7 @@ void publish(struct k_work *work)
 		err = bt_mesh_model_publish(&root_models[13]);
 #elif defined(LIGHT_CTL)
 		bt_mesh_model_msg_init(root_models[16].pub->msg,
-				       BT_MESH_MODEL_OP_2(0x82, 0x5F));
+				       BT_MESH_MODEL_LIGHT_CTL_SET_UNACK);
 		/* Lightness */
 		net_buf_simple_add_le16(root_models[16].pub->msg, LEVEL_U25);
 		/* Temperature (value should be from 0x0320 to 0x4E20 */
@@ -122,7 +122,7 @@ void publish(struct k_work *work)
 		err = bt_mesh_model_publish(&root_models[16]);
 #elif defined(LIGHT_CTL_TT)
 		bt_mesh_model_msg_init(root_models[16].pub->msg,
-				       BT_MESH_MODEL_OP_2(0x82, 0x5F));
+				       BT_MESH_MODEL_LIGHT_CTL_SET_UNACK);
 		/* Lightness */
 		net_buf_simple_add_le16(root_models[16].pub->msg, LEVEL_U25);
 		/* Temperature (value should be from 0x0320 to 0x4E20 */
@@ -136,7 +136,7 @@ void publish(struct k_work *work)
 		err = bt_mesh_model_publish(&root_models[16]);
 #elif defined(LIGHT_CTL_TEMP)
 		bt_mesh_model_msg_init(root_models[16].pub->msg,
-				       BT_MESH_MODEL_OP_2(0x82, 0x65));
+				       BT_MESH_MODEL_LIGHT_CTL_TEMP_SET_UNACK);
 		/* Temperature (value should be from 0x0320 to 0x4E20 */
 		/* This is as per 6.1.3.1 in Mesh Model Specification */
 		net_buf_simple_add_le16(root_models[16].pub->msg, 0x0320);
@@ -169,7 +169,7 @@ void publish(struct k_work *work)
 		err = bt_mesh_model_publish(&root_models[5]);
 #elif defined(LIGHT_LIGHTNESS_TT)
 		bt_mesh_model_msg_init(root_models[13].pub->msg,
-				       BT_MESH_MODEL_OP_2(0x82, 0x4D));
+				       BT_MESH_MODEL_LIGHT_LIGHTNESS_SET_UNACK);
 		net_buf_simple_add_le16(root_models[13].pub->msg, LEVEL_U100);
 		net_buf_simple_add_u8(root_models[13].pub->msg, tid++);
 		net_buf_simple_add_u8(root_models[13].pub->msg, 0x45);
@@ -177,7 +177,7 @@ void publish(struct k_work *work)
 		err = bt_mesh_model_publish(&root_models[13]);
 #elif defined(LIGHT_CTL)
 		bt_mesh_model_msg_init(root_models[16].pub->msg,
-				       BT_MESH_MODEL_OP_2(0x82, 0x5F));
+				       BT_MESH_MODEL_LIGHT_CTL_SET_UNACK);
 		/* Lightness */
 		net_buf_simple_add_le16(root_models[16].pub->msg, LEVEL_U100);
 		/* Temperature (value should be from 0x0320 to 0x4E20 */
@@ -189,7 +189,7 @@ void publish(struct k_work *work)
 		err = bt_mesh_model_publish(&root_models[16]);
 #elif defined(LIGHT_CTL_TT)
 		bt_mesh_model_msg_init(root_models[16].pub->msg,
-				       BT_MESH_MODEL_OP_2(0x82, 0x5F));
+				       BT_MESH_MODEL_LIGHT_CTL_SET_UNACK);
 		/* Lightness */
 		net_buf_simple_add_le16(root_models[16].pub->msg, LEVEL_U100);
 		/* Temperature (value should be from 0x0320 to 0x4E20 */
@@ -203,7 +203,7 @@ void publish(struct k_work *work)
 		err = bt_mesh_model_publish(&root_models[16]);
 #elif defined(LIGHT_CTL_TEMP)
 		bt_mesh_model_msg_init(root_models[16].pub->msg,
-				       BT_MESH_MODEL_OP_2(0x82, 0x65));
+				       BT_MESH_MODEL_LIGHT_CTL_TEMP_SET_UNACK);
 		/* Temperature (value should be from 0x0320 to 0x4E20 */
 		/* This is as per 6.1.3.1 in Mesh Model Specification */
 		net_buf_simple_add_le16(root_models[16].pub->msg, 0x4E20);
