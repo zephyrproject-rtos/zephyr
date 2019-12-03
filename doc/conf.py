e86ec977fb92b4260cb6ef3e673e1eb9d72d78c3
Flavio Ceolin <flavio.ceolin@intel.com>
2019-12-03 13:49:54 -0800

    syscalls: Remove references to __syscall_inline
    There is no code to handle __syscall_inline so it is better removing
    it from doxygen and checkpatch.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- doc/conf.py
+++ doc/conf.py
@@ -516,7 +516,7 @@ breathe_default_project = "Zephyr"
 # Error when parsing function declaration and more.  This is a list
 # of strings that the parser additionally should accept as
 # attributes.
-cpp_id_attributes = ['__syscall', '__syscall_inline', '__deprecated',
+cpp_id_attributes = ['__syscall', '__deprecated',
     '__may_alias', '__used', '__unused', '__weak',
     '__DEPRECATED_MACRO', 'FUNC_NORETURN']
 
