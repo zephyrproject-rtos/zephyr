b943f71e1921b4b12affc977a3697a674da81139
Peter Bigot <peter.bigot@nordicsemi.no>
2019-12-11 11:09:41 -0600

    drivers: gpio_sam0: add support for pin validation
    Add the common config structure as a prefix of the driver-specific
    config structure and use the devicetree GPIO pin counts to initialize
    it.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- drivers/gpio/gpio_sam0.c
+++ drivers/gpio/gpio_sam0.c
@@ -18,6 +18,8 @@
 #endif
 
 struct gpio_sam0_config {
+	/* gpio_driver_config needs to be first */
+	struct gpio_driver_config common;
 	PortGroup *regs;
 #ifdef CONFIG_SAM0_EIC
 	u8_t id;
@@ -327,6 +329,9 @@ static int gpio_sam0_init(struct device *dev) { return 0; }
 #if DT_ATMEL_SAM0_GPIO_PORT_A_BASE_ADDRESS
 
 static const struct gpio_sam0_config gpio_sam0_config_0 = {
+	.common = {
+		.port_pin_mask = GPIO_PORT_PIN_MASK_FROM_NGPIOS(DT_INST_0_ATMEL_SAM0_GPIO_NGPIOS),
+	},
 	.regs = (PortGroup *)DT_ATMEL_SAM0_GPIO_PORT_A_BASE_ADDRESS,
 #ifdef CONFIG_SAM0_EIC
 	.id = 0,
@@ -345,6 +350,9 @@ DEVICE_AND_API_INIT(gpio_sam0_0, DT_ATMEL_SAM0_GPIO_PORT_A_LABEL,
 #if DT_ATMEL_SAM0_GPIO_PORT_B_BASE_ADDRESS
 
 static const struct gpio_sam0_config gpio_sam0_config_1 = {
+	.common = {
+		.port_pin_mask = GPIO_PORT_PIN_MASK_FROM_NGPIOS(DT_INST_1_ATMEL_SAM0_GPIO_NGPIOS),
+	},
 	.regs = (PortGroup *)DT_ATMEL_SAM0_GPIO_PORT_B_BASE_ADDRESS,
 #ifdef CONFIG_SAM0_EIC
 	.id = 1,
@@ -363,6 +371,9 @@ DEVICE_AND_API_INIT(gpio_sam0_1, DT_ATMEL_SAM0_GPIO_PORT_B_LABEL,
 #if DT_ATMEL_SAM0_GPIO_PORT_C_BASE_ADDRESS
 
 static const struct gpio_sam0_config gpio_sam0_config_2 = {
+	.common = {
+		.port_pin_mask = GPIO_PORT_PIN_MASK_FROM_NGPIOS(DT_INST_2_ATMEL_SAM0_GPIO_NGPIOS),
+	},
 	.regs = (PortGroup *)DT_ATMEL_SAM0_GPIO_PORT_C_BASE_ADDRESS,
 #ifdef CONFIG_SAM0_EIC
 	.id = 2,
@@ -381,6 +392,9 @@ DEVICE_AND_API_INIT(gpio_sam0_2, DT_ATMEL_SAM0_GPIO_PORT_C_LABEL,
 #if DT_ATMEL_SAM0_GPIO_PORT_D_BASE_ADDRESS
 
 static const struct gpio_sam0_config gpio_sam0_config_3 = {
+	.common = {
+		.port_pin_mask = GPIO_PORT_PIN_MASK_FROM_NGPIOS(DT_INST_3_ATMEL_SAM0_GPIO_NGPIOS),
+	},
 	.regs = (PortGroup *)DT_ATMEL_SAM0_GPIO_PORT_D_BASE_ADDRESS,
 #ifdef CONFIG_SAM0_EIC
 	.id = 3,
