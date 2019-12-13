66bdb76e7c20753c34747a4cc2d0d17f2084971c
Alberto Escolar Piedras <alpi@oticon.com>
2019-12-13 10:24:59 +0100

    POSIX arch: Fix C++ main() linkage issue
    To be able to define main() in C++ code we need to have its
    prototype defined somewhere visibly. Otherwise name mangling
    will prevent the linker from finding it.
    Zephyr assumes a void main(void) prototype and therefore
    this will be the prototype after renaming:
    void zephyr_app_main(void);
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>

--- arch/posix/include/posix_cheats.h
+++ arch/posix/include/posix_cheats.h
@@ -44,6 +44,20 @@
 #define main(...) zephyr_app_main(__VA_ARGS__)
 #endif
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+/* To be able to define main() in C++ code we need to have its prototype
+ * defined somewhere visibly. Otherwise name mangling will prevent the linker
+ * from finding it. Zephyr assumes a void main(void) prototype and therefore
+ * this will be the prototype after renaming:
+ */
+void zephyr_app_main(void);
+
+#ifdef __cplusplus
+}
+#endif
+
 #ifdef CONFIG_POSIX_API
 
 /*
