a3cba5453be2c7fd0983ff1b196ec906ff05b65c
Stephanos Ioannidis <root@stephanos.io>
2019-12-16 19:38:38 +0900

    tests: lib: cmsis_dsp: Add 'statistics' test application
    This commit adds the test application for the CMSIS-DSP 'statistics'
    functions.
    
    This test application is loosely based on the C++ test suite included
    in the upstream CMSIS-DSP distribution.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

---
+++ tests/lib/cmsis_dsp/statistics/src/main.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2019 Stephanos Ioannidis <root@stephanos.io>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ztest.h>
+#include <zephyr.h>
+
+extern void test_statistics_q7(void);
+extern void test_statistics_q15(void);
+extern void test_statistics_q31(void);
+extern void test_statistics_f32(void);
+
+void test_main(void)
+{
+    test_statistics_q7();
+    test_statistics_q15();
+    test_statistics_q31();
+    test_statistics_f32();
+}
