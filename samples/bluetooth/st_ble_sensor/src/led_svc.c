5a17046a90c829528f4eb63253628edc4791cb21
Marcio Montenegro <mtuxpe@gmail.com>
2019-12-16 16:52:42 -0300

    Samples: Bluetooth: st_ble_demo: Update to new GPIO API
    Convert to the new API pin and interrupt API.
    
    Signed-off-by: Marcio Montenegro <mtuxpe@gmail.com>

--- samples/bluetooth/st_ble_sensor/src/led_svc.c
+++ samples/bluetooth/st_ble_sensor/src/led_svc.c
@@ -26,24 +26,35 @@ LOG_MODULE_REGISTER(led_svc);
 struct device *led_dev;
 bool led_state;
 
-void led_on_off(u32_t led_state)
+void led_on_off(bool led_state)
 {
+	int led_param = (led_state == true) ? 1 : 0;
 	if (led_dev) {
-		gpio_pin_write(led_dev, LED, led_state);
+		gpio_pin_set(led_dev, LED, led_param);
 	}
 }
 
 int led_init(void)
 {
+	int ret;
+
 	led_dev = device_get_binding(LED_PORT);
 	if (!led_dev) {
 		return (-EOPNOTSUPP);
 	}
 
 	/* Set LED pin as output */
-	gpio_pin_configure(led_dev, LED, GPIO_DIR_OUT);
+
+	ret = gpio_pin_configure(led_dev, LED,
+				 GPIO_OUTPUT_ACTIVE
+				 | DT_ALIAS_LED0_GPIOS_FLAGS);
+	if (ret < 0) {
+		LOG_ERR("Error %d: failed to configure pin %d '%s'\n",
+		ret, LED, DT_ALIAS_LED0_LABEL);
+		return ret;
+	}
+
 	led_state = false;
-	gpio_pin_write(led_dev, LED, 0);
-	led_on_off(0);
+	led_on_off(led_state);
 	return 0;
 }
