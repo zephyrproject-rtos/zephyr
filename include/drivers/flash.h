8d41784d2bd7bacd96078fe820a03114eb03ceae
Peter A. Bigot <pab@pabigot.com>
2019-12-16 04:56:17 -0600

    include: respect line limit in api pointer initialization
    The automated process used to remove implicit casts resulted in code
    that exceeded the documented line length limits.  Break the assignment
    into two lines where this happened.
    
    Signed-off-by: Peter A. Bigot <pab@pabigot.com>

--- include/drivers/flash.h
+++ include/drivers/flash.h
@@ -101,7 +101,8 @@ __syscall int flash_read(struct device *dev, off_t offset, void *data,
 static inline int z_impl_flash_read(struct device *dev, off_t offset, void *data,
 			     size_t len)
 {
-	const struct flash_driver_api *api = (const struct flash_driver_api *)dev->driver_api;
+	const struct flash_driver_api *api =
+		(const struct flash_driver_api *)dev->driver_api;
 
 	return api->read(dev, offset, data, len);
 }
@@ -125,7 +126,8 @@ __syscall int flash_write(struct device *dev, off_t offset, const void *data,
 static inline int z_impl_flash_write(struct device *dev, off_t offset,
 				    const void *data, size_t len)
 {
-	const struct flash_driver_api *api = (const struct flash_driver_api *)dev->driver_api;
+	const struct flash_driver_api *api =
+		(const struct flash_driver_api *)dev->driver_api;
 
 	return api->write(dev, offset, data, len);
 }
@@ -156,7 +158,8 @@ __syscall int flash_erase(struct device *dev, off_t offset, size_t size);
 static inline int z_impl_flash_erase(struct device *dev, off_t offset,
 				    size_t size)
 {
-	const struct flash_driver_api *api = (const struct flash_driver_api *)dev->driver_api;
+	const struct flash_driver_api *api =
+		(const struct flash_driver_api *)dev->driver_api;
 
 	return api->erase(dev, offset, size);
 }
@@ -199,7 +202,8 @@ __syscall int flash_write_protection_set(struct device *dev, bool enable);
 static inline int z_impl_flash_write_protection_set(struct device *dev,
 						   bool enable)
 {
-	const struct flash_driver_api *api = (const struct flash_driver_api *)dev->driver_api;
+	const struct flash_driver_api *api =
+		(const struct flash_driver_api *)dev->driver_api;
 
 	return api->write_protection(dev, enable);
 }
@@ -286,7 +290,8 @@ __syscall size_t flash_get_write_block_size(struct device *dev);
 
 static inline size_t z_impl_flash_get_write_block_size(struct device *dev)
 {
-	const struct flash_driver_api *api = (const struct flash_driver_api *)dev->driver_api;
+	const struct flash_driver_api *api =
+		(const struct flash_driver_api *)dev->driver_api;
 
 	return api->write_block_size;
 }
