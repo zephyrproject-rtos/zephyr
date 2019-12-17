6fab978ae3c44cf037333fd7a2b8942a088acb1f
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2019-12-17 15:30:09 +0100

    drivers/interrupt_controller: Normalize DT aliases consitently for DW
    DT_<domain>_DW_<num>_<option> as for SPI, GPIO, DMA etc...
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- soc/xtensa/intel_s1000/soc.c
+++ soc/xtensa/intel_s1000/soc.c
@@ -54,7 +54,7 @@ void z_soc_irq_enable(u32_t irq)
 
 	switch (CAVS_IRQ_NUMBER(irq)) {
 	case DW_ICTL_IRQ_CAVS_OFFSET:
-		dev_ictl = device_get_binding(CONFIG_DW_ICTL_NAME);
+		dev_ictl = device_get_binding(DT_INTC_DW_0_NAME);
 		break;
 	default:
 		/* The source of the interrupt is in CAVS interrupt logic */
@@ -112,7 +112,7 @@ void z_soc_irq_disable(u32_t irq)
 
 	switch (CAVS_IRQ_NUMBER(irq)) {
 	case DW_ICTL_IRQ_CAVS_OFFSET:
-		dev_ictl = device_get_binding(CONFIG_DW_ICTL_NAME);
+		dev_ictl = device_get_binding(DT_INTC_DW_0_NAME);
 		break;
 	default:
 		/* The source of the interrupt is in CAVS interrupt logic */
@@ -179,7 +179,7 @@ int z_soc_irq_is_enabled(unsigned int irq)
 
 	switch (CAVS_IRQ_NUMBER(irq)) {
 	case DW_ICTL_IRQ_CAVS_OFFSET:
-		dev_ictl = device_get_binding(CONFIG_DW_ICTL_NAME);
+		dev_ictl = device_get_binding(DT_INTC_DW_0_NAME);
 		break;
 	default:
 		/* The source of the interrupt is in CAVS interrupt logic */
