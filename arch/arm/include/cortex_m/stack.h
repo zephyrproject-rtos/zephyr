d048faacf234061f6fb70e4926446268a68442bd
Carlo Caione <ccaione@baylibre.com>
2019-12-17 17:13:47 +0000

    aarch32: Add header shims to support old file locations
    Out-of-tree code can still be using the old file locations. Introduce
    header shims to include the headers from the new correct location and
    print a warning message.
    
    Add also a new Kconfig symbol to suppress such warning.
    
    The shim will go away after two releases, so make sure to adapt your
    application for the new locations.
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

---
+++ arch/arm/include/cortex_m/stack.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2019 Carlo Caione <ccaione@baylibre.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_ARCH_ARM_INCLUDE_CORTEX_M_STACK_H_
+#define ZEPHYR_ARCH_ARM_INCLUDE_CORTEX_M_STACK_H_
+
+#ifndef CONFIG_COMPAT_INCLUDES
+#warning "This header file has moved, include <aarch32/cortex_m/stack.h> instead."
+#endif
+
+#include <aarch32/cortex_m/stack.h>
+
+#endif /* ZEPHYR_ARCH_ARM_INCLUDE_CORTEX_M_STACK_H_ */
