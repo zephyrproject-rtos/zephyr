d1436e64f8e6c42f9affe53698936389b225577a
Ulf Magnusson <Ulf.Magnusson@nordicsemi.no>
2019-12-18 07:05:09 +0100

    Fix up direct references to *_CLOCK_BITS
    Skipped because they reference dts_fixup.h stuff:
    
     - drivers/adc/adc_stm32.c
     - drivers/can/can_stm32.c
     - drivers/counter/counter_ll_stm32_rtc.c
     - drivers/gpio/gpio_stm32.c
     - drivers/i2c/i2c_ll_stm32.c
     - drivers/i2s/i2s_ll_stm32.c
     - drivers/pwm/pwm_stm32.c
     - drivers/serial/uart_stm32.c
     - drivers/spi/spi_ll_stm32.c
     - drivers/usb/device/usb_dc_stm32.c
     - drivers/watchdog/wdt_wwdg_stm32.c

--- drivers/ipm/ipm_stm32_ipcc.c
+++ drivers/ipm/ipm_stm32_ipcc.c
@@ -287,7 +287,7 @@ static const struct stm32_ipcc_mailbox_config stm32_ipcc_mailbox_0_config = {
 	.irq_config_func = stm32_ipcc_mailbox_config_func,
 	.ipcc = (IPCC_TypeDef *)DT_INST_0_ST_STM32_IPCC_MAILBOX_BASE_ADDRESS,
 	.pclken = { .bus = DT_INST_0_ST_STM32_IPCC_MAILBOX_CLOCK_BUS,
-		    .enr = DT_INST_0_ST_STM32_IPCC_MAILBOX_CLOCK_BITS
+		    .enr = DT_INST_0_ST_STM32_IPCC_MAILBOX_CLOCKS_BITS
 	},
 
 };
