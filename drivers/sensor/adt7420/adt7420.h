50d1bb16e34856bb5a3c24b84faf3d423c59d50f
Peter Bigot <peter.bigot@nordicsemi.no>
2019-12-21 10:15:32 -0600

    sensor: adt7420: update for new GPIO API
    Update sample overlays.  Add GPIO flags to configuration state.
    Refactor to split out setup/handle/process phases.  Switch to new API
    replacing callback dis/enable with interrupt dis/enable.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- drivers/sensor/adt7420/adt7420.h
+++ drivers/sensor/adt7420/adt7420.h
@@ -68,13 +68,14 @@ struct adt7420_data {
 	sensor_trigger_handler_t th_handler;
 	struct sensor_trigger th_trigger;
 
+	struct device *dev;
+
 #if defined(CONFIG_ADT7420_TRIGGER_OWN_THREAD)
 	K_THREAD_STACK_MEMBER(thread_stack, CONFIG_ADT7420_THREAD_STACK_SIZE);
 	struct k_sem gpio_sem;
 	struct k_thread thread;
 #elif defined(CONFIG_ADT7420_TRIGGER_GLOBAL_THREAD)
 	struct k_work work;
-	struct device *dev;
 #endif
 #endif /* CONFIG_ADT7420_TRIGGER */
 
@@ -84,8 +85,9 @@ struct adt7420_dev_config {
 	const char *i2c_port;
 	u16_t i2c_addr;
 #ifdef CONFIG_ADT7420_TRIGGER
-	const char *gpio_port;
-	u8_t int_gpio;
+	gpio_pin_t int_pin;
+	gpio_flags_t int_flags;
+	const char *int_name;
 #endif
 };
 
