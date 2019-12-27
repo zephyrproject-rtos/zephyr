07388e39fecedad008100a710e4d90e16a7f61aa
Joakim Andersson <joakim.andersson@nordicsemi.no>
2019-12-27 14:58:40 +0100

    Bluetooth: mesh: Use 24-bit functions
    Use 24-bit functions for byteorder and net_buf in order to make the
    byteorder used more readable.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- subsys/bluetooth/mesh/cfg_srv.c
+++ subsys/bluetooth/mesh/cfg_srv.c
@@ -2785,9 +2785,7 @@ static void lpn_timeout_get(struct bt_mesh_model *model,
 	timeout = k_delayed_work_remaining_get(&frnd->timer) / 100;
 
 send_rsp:
-	net_buf_simple_add_u8(&msg, timeout);
-	net_buf_simple_add_u8(&msg, timeout >> 8);
-	net_buf_simple_add_u8(&msg, timeout >> 16);
+	net_buf_simple_add_le24(&msg, timeout);
 
 	if (bt_mesh_model_send(model, ctx, &msg, NULL, NULL)) {
 		BT_ERR("Unable to send LPN PollTimeout Status");
