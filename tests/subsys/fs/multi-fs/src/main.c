9c0fb144ee50da72096e26e63d77760da811978d
Dominik Ermel <dominik.ermel@nordicsemi.no>
2020-01-16 12:28:37 +0000

    tests/fs: NFFS has been replaced with Littlefs in multifs tests
    With removal of NFFS from Zaphyr it would be no longer possible to
    test multifs with NFFS and LittleFS will be now used instead.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- tests/subsys/fs/multi-fs/src/main.c
+++ tests/subsys/fs/multi-fs/src/main.c
@@ -4,65 +4,34 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <ztest.h>
+#include "test_common.h"
 #include "test_fat.h"
-#include "test_nffs.h"
+#include "test_littlefs.h"
 #include "test_fs_shell.h"
-
-static struct nffs_area_desc nffs_selftest_area_descs[] = {
-	{ 0x00000000, 16 * 1024 },
-	{ 0x00004000, 16 * 1024 },
-	{ 0x00008000, 16 * 1024 },
-	{ 0x0000c000, 16 * 1024 },
-	{ 0x00010000, 64 * 1024 },
-	{ 0x00020000, 128 * 1024 },
-	{ 0x00040000, 128 * 1024 },
-	{ 0x00060000, 128 * 1024 },
-	{ 0x00080000, 128 * 1024 },
-	{ 0x000a0000, 128 * 1024 },
-	{ 0x000c0000, 128 * 1024 },
-	{ 0x000e0000, 128 * 1024 },
-	{ 0, 0 },
-};
-
-static struct nffs_area_desc *save_area_descs;
-
-static void test_setup(void)
-{
-	save_area_descs = nffs_current_area_descs;
-	nffs_current_area_descs = nffs_selftest_area_descs;
-}
-
-static void test_teardown(void)
-{
-	nffs_current_area_descs = save_area_descs;
-}
 
 void test_main(void)
 {
 	ztest_test_suite(multifs_fs_test,
+			 ztest_unit_test(clear_flash),
 			 ztest_unit_test(test_fat_mount),
-			 ztest_unit_test_setup_teardown(test_nffs_mount,
-							test_setup, test_teardown),
+			 ztest_unit_test(test_littlefs_mount),
 			 ztest_unit_test(test_fat_mkdir),
-			 ztest_unit_test_setup_teardown(test_nffs_mkdir,
-							test_setup, test_teardown),
+			 ztest_unit_test(test_littlefs_mkdir),
 			 ztest_unit_test(test_fat_readdir),
+			 ztest_unit_test(test_littlefs_readdir),
 			 ztest_unit_test(test_fat_rmdir),
-			 ztest_unit_test_setup_teardown(test_nffs_readdir,
-							test_setup, test_teardown),
+			 ztest_unit_test(test_littlefs_rmdir),
 			 ztest_unit_test(test_fat_open),
-			 ztest_unit_test_setup_teardown(test_nffs_open,
-							test_setup, test_teardown),
+			 ztest_unit_test(test_littlefs_open),
 			 ztest_unit_test(test_fat_write),
-			 ztest_unit_test_setup_teardown(test_nffs_write,
-							test_setup, test_teardown),
+			 ztest_unit_test(test_littlefs_write),
 			 ztest_unit_test(test_fat_read),
+			 ztest_unit_test(test_littlefs_read),
 			 ztest_unit_test(test_fat_close),
-			 ztest_unit_test_setup_teardown(test_nffs_read,
-							test_setup, test_teardown),
+			 ztest_unit_test(test_littlefs_close),
 			 ztest_unit_test(test_fat_unlink),
-			 ztest_unit_test_setup_teardown(test_nffs_unlink,
-							test_setup, test_teardown),
+			 ztest_unit_test(test_littlefs_unlink),
 			 ztest_unit_test(test_fs_help));
 	ztest_run_test_suite(multifs_fs_test);
 }
--- tests/subsys/fs/multi-fs/src/test_littlefs_dir.c
+++ tests/subsys/fs/multi-fs/src/test_littlefs_dir.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Intel Corporation.
+ * Copyright (c) 2020 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -7,21 +7,21 @@
 #include <stdio.h>
 #include <fs/fs.h>
 #include "test_common.h"
-#include "test_fat.h"
-#include "test_fat_priv.h"
+#include "test_littlefs.h"
+#include "test_littlefs_priv.h"
 
 
-void test_fat_mkdir(void)
+void test_littlefs_mkdir(void)
 {
 	zassert_true(test_mkdir(TEST_DIR_PATH, TEST_FILE) == TC_PASS, NULL);
 }
 
-void test_fat_readdir(void)
+void test_littlefs_readdir(void)
 {
 	zassert_true(test_lsdir(TEST_DIR_PATH) == TC_PASS, NULL);
 }
 
-void test_fat_rmdir(void)
+void test_littlefs_rmdir(void)
 {
 	zassert_true(test_rmdir(TEST_DIR_PATH) == TC_PASS, NULL);
 }
