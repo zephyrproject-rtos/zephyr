zephyrproject-rtos/zephyr

commit e4768ad7e760a7dcd50d8ff7eb5aa9e482356f9b

scripts: runners: nrfjprog: restrict when we guess snr
Right now, the nrfjprog runner will prompt the user for which board to
use if there are multiple possibilities and the --snr command line was
not given to specify one ahead of time.

Tweak this so it only happens if standard input is connected to a
terminal. This should avoid stalling the process on board farms when
this runner is used in automation.

Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

scripts/west_commands/runners/nrfjprog.py
@@ -60,26 +60,34 @@ class NrfJprogBinaryRunner(ZephyrBinaryRunner):
 
     def ensure_snr(self):
         if not self.snr:
-            self.snr = self.get_board_snr_from_user()
+            self.snr = self.get_board_snr()
 
-    def get_board_snr_from_user(self):
+    def get_board_snr(self):
+        # Use nrfjprog --ids to discover connected boards.
+        #
+        # If there's exactly one board connected, it's safe to assume
+        # the user wants that one. Otherwise, bail unless there are
+        # multiple boards and we are connected to a terminal, in which
+        # case use print() and input() to ask what the user wants.
+
         snrs = self.check_output(['nrfjprog', '--ids'])
         snrs = snrs.decode(sys.getdefaultencoding()).strip().splitlines()
-
         if not snrs:
             raise RuntimeError('"nrfjprog --ids" did not find a board; '
                                'is the board connected?')
-
-        if len(snrs) == 1:
+        elif len(snrs) == 1:
             board_snr = snrs[0]
             if board_snr == '0':
                 raise RuntimeError('"nrfjprog --ids" returned 0; '
                                    'is a debugger already connected?')
             return board_snr
+        elif not sys.stdin.isatty():
+            raise RuntimeError(
+                f'refusing to guess which of {len(snrs)} '
+                'connected boards to use. (Interactive prompts '
+                'disabled since standard input is not a terminal.) '
+                'Please specify a serial number on the command line.')
 
-        # Use of print() here is advised. We don't want to lose
-        # this information in a separate log -- this is
-        # interactive and requires a terminal.
         print('There are multiple boards connected.')
         for i, snr in enumerate(snrs, 1):
             print('{}. {}'.format(i, snr))
@@ -103,10 +111,7 @@ class NrfJprogBinaryRunner(ZephyrBinaryRunner):
         self.ensure_snr()
 
         commands = []
-        if self.snr is None:
-            raise ValueError("self.snr must not be None")
-        else:
-            board_snr = self.snr.lstrip("0")
+        board_snr = self.snr.lstrip("0")
 
         if not os.path.isfile(self.hex_):
             raise ValueError('Cannot flash; hex file ({}) does not exist. '.

scripts/west_commands/tests/test_nrfjprog.py
@@ -241,7 +241,7 @@ def id_fn(test_case):
 
 @pytest.mark.parametrize('test_case', TEST_CASES, ids=id_fn)
 @patch('runners.core.ZephyrBinaryRunner.require', side_effect=require_patch)
-@patch('runners.nrfjprog.NrfJprogBinaryRunner.get_board_snr_from_user',
+@patch('runners.nrfjprog.NrfJprogBinaryRunner.get_board_snr',
        side_effect=get_board_snr_patch)
 @patch('runners.nrfjprog.NrfJprogBinaryRunner.check_call')
 def test_nrfjprog_init(cc, get_snr, req, test_case, runner_config):
@@ -262,7 +262,7 @@ def test_nrfjprog_init(cc, get_snr, req, test_case, runner_config):
 
 @pytest.mark.parametrize('test_case', TEST_CASES, ids=id_fn)
 @patch('runners.core.ZephyrBinaryRunner.require', side_effect=require_patch)
-@patch('runners.nrfjprog.NrfJprogBinaryRunner.get_board_snr_from_user',
+@patch('runners.nrfjprog.NrfJprogBinaryRunner.get_board_snr',
        side_effect=get_board_snr_patch)
 @patch('runners.nrfjprog.NrfJprogBinaryRunner.check_call')
 def test_nrfjprog_create(cc, get_snr, req, test_case, runner_config):

commit b2c2a19a8b1b213970a00fe705b49349d6e8ac63

scripts: runners: fix invalid syntax
Fix "SyntaxError: invalid syntax" introduced as regression in
commit e4768ad7e760 ("scripts: runners: nrfjprog: restrict when
we guess snr") as show below.

File "zephyr/scripts/west_commands/runners/nrfjprog.py", line 86
    f'refusing to guess which of {len(snrs)} '

Signed-off-by: Lianwei Wang <lianwei.wang@gm.com>

scripts/west_commands/runners/nrfjprog.py
@@ -83,7 +83,7 @@ class NrfJprogBinaryRunner(ZephyrBinaryRunner):
             return board_snr
         elif not sys.stdin.isatty():
             raise RuntimeError(
-                f'refusing to guess which of {len(snrs)} '
+                'refusing to guess which of {len(snrs)} '
                 'connected boards to use. (Interactive prompts '
                 'disabled since standard input is not a terminal.) '
                 'Please specify a serial number on the command line.')