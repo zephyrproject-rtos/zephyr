11eb3b6c4f9417c9b920cbc3c89abedb1ef36394
Ulf Magnusson <Ulf.Magnusson@nordicsemi.no>
2020-01-25 01:04:06 +0100

    canbus: canopen: Fix broken CANOPEN_STORAGE_HANDLER_ERASES_EEPROM ref.
    Missing CONFIG_ prefix in #ifdef, leading to some dead code.
    
    Found by scripts/kconfig/lint.py, which flagged it as unused.
    
    Piggyback removal of a redundant 'default n'.
    
    Signed-off-by: Ulf Magnusson <Ulf.Magnusson@nordicsemi.no>

--- subsys/canbus/canopen/canopen_storage.c
+++ subsys/canbus/canopen/canopen_storage.c
@@ -100,7 +100,7 @@ static CO_SDO_abortCode_t canopen_odf_1011(CO_ODF_arg_t *odf_arg)
 		LOG_DBG("deleted object dictionary ROM entries");
 	}
 
-#ifdef CANOPEN_STORAGE_HANDLER_ERASES_EEPROM
+#ifdef CONFIG_CANOPEN_STORAGE_HANDLER_ERASES_EEPROM
 	err = canopen_storage_erase(CANOPEN_STORAGE_EEPROM);
 	if (err == -ENOENT) {
 		LOG_DBG("no object dictionary EEPROM entries to delete");
