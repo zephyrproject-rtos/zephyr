e34ebc6e59c60c10f35f92787da6186cdd23e891
Piotr Mienkowski <piotr.mienkowski@gmail.com>
2020-01-26 22:37:50 +0100

    soc: silabs_exx32: Add support for SiLabs EFR32BG13P SoC
    This commit adds support for Silicon Labs EFR32BG13P (Blue Gecko) SoC.
    
    Signed-off-by: Piotr Mienkowski <piotr.mienkowski@gmail.com>

---
+++ soc/arm/silabs_exx32/efr32bg13p/soc.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2020 Piotr Mienkowski
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Register access macros for the EFR32BG13P SoC
+ *
+ */
+
+#ifndef EFR32BG13P_SOC_H_
+#define EFR32BG13P_SOC_H_
+
+#include <sys/util.h>
+
+#ifndef _ASMLANGUAGE
+
+#include <em_common.h>
+
+#include "soc_pinmap.h"
+#include "../common/soc_gpio.h"
+
+/* Add include for DTS generated information */
+#include <devicetree.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* EFR32BG13P_SOC_H_ */
