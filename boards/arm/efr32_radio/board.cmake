298fa9725f1171a9b54fab3852d642b64c1c11b9
Piotr Mienkowski <piotr.mienkowski@gmail.com>
2020-01-27 03:28:37 +0100

    boards: arm: Add support for SiLabs EFR32BG13 BRD4104A board
    This commit adds support for Silicon Labs BRD4104A (a.k.a. SLWRB4104A)
    Blue Gecko and BRD4250B (a.k.a. SLWRB4250B) Flex Gecko Radio Boards.
    
    Signed-off-by: Piotr Mienkowski <piotr.mienkowski@gmail.com>

---
+++ boards/arm/efr32_radio/board.cmake
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(openocd)
+
+if(CONFIG_BOARD_EFR32_RADIO_BRD4104A)
+board_runner_args(jlink "--device=EFR32BG13PxxxF512")
+elseif(CONFIG_BOARD_EFR32_RADIO_BRD4250B)
+board_runner_args(jlink "--device=EFR32FG1PxxxF256")
+endif()
+
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
