9d256b75ec89aa0f78a65ea6fc9cc7c8960d8105
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2020-01-27 14:55:18 +0100

    dtc: Support opting-out of installing dtc
    dtc is only used for static analysis (producing warnings) of the
    DeviceTree sources. This means that valid Zephyr firmware can sanely
    be built without it.
    
    For some users, for instance Windows users that are not permitted to
    use Chocolatey, installing dtc is problematic and installing it is not
    worth the DT warnings that it provides.
    
    To make using Zephyr easier for these users we make using DTC
    recommended and opt-out, instead of mandatory.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- cmake/host-tools.cmake
+++ cmake/host-tools.cmake
@@ -47,7 +47,7 @@ else()
   endif()
 endif()
 
-# Search for the must-have program dtc on PATH and in
+# dtc is an optional dependency. Search for it on PATH and in
 # TOOLCHAIN_HOME. Usually DTC will be provided by an SDK, but for
 # SDK-less projects like gnuarmemb, it is up to the user to install
 # dtc.
@@ -55,10 +55,8 @@ find_program(
   DTC
   dtc
   )
-if(${DTC} STREQUAL DTC-NOTFOUND)
-  message(FATAL_ERROR "Unable to find dtc")
-endif()
 
+if(DTC)
 # Parse the 'dtc --version' and make sure it is at least MIN_DTC_VERSION
 set(MIN_DTC_VERSION 1.4.6)
 execute_process(
@@ -75,6 +73,7 @@ if(${CMAKE_MATCH_1} VERSION_LESS ${MIN_DTC_VERSION})
     for how to use the SDK's dtc alongside a custom toolchain."
   )
 endif()
+endif(DTC)
 
 find_program(
   GPERF
