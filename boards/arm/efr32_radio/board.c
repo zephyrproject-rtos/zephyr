6dc7b40da993594ec293771c698f18efcf940b00
Piotr Mienkowski <piotr.mienkowski@gmail.com>
2020-01-27 03:28:37 +0100

    boards: arm: Add support for SiLabs EFR32BG13 BRD4104A board
    This commit adds support for Silicon Labs BRD4104A (a.k.a. SLWRB4104A)
    Blue Gecko and BRD4250B (a.k.a. SLWRB4250B) Flex Gecko Radio Boards.
    
    Signed-off-by: Piotr Mienkowski <piotr.mienkowski@gmail.com>

---
+++ boards/arm/efr32_radio/board.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2018 Christian Taedcke
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+#include <drivers/gpio.h>
+#include <sys/printk.h>
+
+/* This pin is used to enable the serial port using the board controller */
+#define VCOM_ENABLE_GPIO_NAME  DT_GPIO_GECKO_PORTA_NAME
+#define VCOM_ENABLE_GPIO_PIN   5
+
+static int efr32_radio_init(struct device *dev)
+{
+	struct device *vce_dev; /* Virtual COM Port Enable GPIO Device */
+
+	ARG_UNUSED(dev);
+
+	/* Enable the board controller to be able to use the serial port */
+	vce_dev = device_get_binding(VCOM_ENABLE_GPIO_NAME);
+	if (!vce_dev) {
+		printk("Virtual COM Port Enable device was not found!\n");
+		return -ENODEV;
+	}
+
+	gpio_pin_configure(vce_dev, VCOM_ENABLE_GPIO_PIN, GPIO_OUTPUT_HIGH);
+
+	return 0;
+}
+
+/* needs to be done after GPIO driver init */
+SYS_INIT(efr32_radio_init, PRE_KERNEL_1, CONFIG_BOARD_INIT_PRIORITY);
