69af9f9dd3874db45881016e2ea1e1c99669f2a6
Peter Bigot <peter.bigot@nordicsemi.no>
2020-01-28 10:27:28 -0600

    SQUASH: tests: gpio_basic_api: remove test for deprecated functions
    To be merged with the commit that deprecates; currently separated to
    support manual verification that the deprecation substitutions work.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- tests/drivers/gpio/gpio_basic_api/src/main.c
+++ tests/drivers/gpio/gpio_basic_api/src/main.c
@@ -123,7 +123,6 @@ void test_main(void)
 	board_setup();
 	ztest_test_suite(gpio_basic_test,
 			 ztest_unit_test(test_gpio_port),
-			 ztest_unit_test(test_gpio_pin_read_write),
 			 ztest_unit_test(test_gpio_callback_add_remove),
 			 ztest_unit_test(test_gpio_callback_self_remove),
 			 ztest_unit_test(test_gpio_callback_enable_disable),
