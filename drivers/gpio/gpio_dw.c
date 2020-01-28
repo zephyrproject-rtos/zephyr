zephyrproject-rtos/zephyr

commit c490219d1efb8712fccbc00600507992c4a331be

power_mgmt: Update sample and drivers according to new pm device API
Update the power sample and drivers with the new device driver power
management API using the existing logic

Jira: ZEP-954
Change-Id: Idd94232e458767635973e94e9fc673c01612c1e2
Signed-off-by: Amir Kaplan <amir.kaplan@intel.com>

drivers/gpio/gpio_dw.c
@@ -336,19 +336,57 @@ static inline int gpio_dw_disable_callback(struct device *port, int access_op,
 }
 
 #ifdef CONFIG_DEVICE_POWER_MANAGEMENT
-static inline int gpio_dw_suspend_port(struct device *port, int pm_policy)
+static void gpio_dw_set_power_state(struct device *port, uint32_t power_state)
+{
+	struct gpio_dw_runtime *context = port->driver_data;
+
+	context->device_power_state = power_state;
+}
+
+static uint32_t gpio_dw_get_power_state(struct device *port)
+{
+	struct gpio_dw_runtime *context = port->driver_data;
+
+	return context->device_power_state;
+}
+
+static inline int gpio_dw_suspend_port(struct device *port)
 {
 	_gpio_dw_clock_off(port);
+	gpio_dw_set_power_state(port, DEVICE_PM_SUSPEND_STATE);
 
 	return 0;
 }
 
-static inline int gpio_dw_resume_port(struct device *port, int pm_policy)
+static inline int gpio_dw_resume_from_suspend_port(struct device *port)
 {
 	_gpio_dw_clock_on(port);
-
+	gpio_dw_set_power_state(port, DEVICE_PM_ACTIVE_STATE);
 	return 0;
 }
+
+/*
+* Implements the driver control management functionality
+* the *context may include IN data or/and OUT data
+*/
+static int gpio_dw_device_ctrl(struct device *port, uint32_t ctrl_command,
+							void *context)
+{
+	if (ctrl_command == DEVICE_PM_SET_POWER_STATE) {
+		if (*((uint32_t *)context) == DEVICE_PM_SUSPEND_STATE) {
+			return gpio_dw_suspend_port(port);
+		} else if (*((uint32_t *)context) == DEVICE_PM_ACTIVE_STATE) {
+			return gpio_dw_resume_from_suspend_port(port);
+		}
+	} else if (ctrl_command == DEVICE_PM_GET_POWER_STATE) {
+		*((uint32_t *)context) = gpio_dw_get_power_state(port);
+		return 0;
+	}
+	return 0;
+}
+
+#else
+#define gpio_dw_set_power_state(...)
 #endif
 
 #if defined(CONFIG_SOC_QUARK_SE_C1000) || defined(CONFIG_SOC_QUARK_D2000)
@@ -456,6 +494,8 @@ int gpio_dw_initialize(struct device *port)
 		config->config_func(port);
 	}
 
+	gpio_dw_set_power_state(port, DEVICE_PM_ACTIVE_STATE);
+
 	return 0;
 }
 
@@ -491,13 +531,9 @@ struct gpio_dw_config gpio_config_0 = {
 struct gpio_dw_runtime gpio_0_runtime;
 
 #ifdef CONFIG_DEVICE_POWER_MANAGEMENT
-struct device_pm_ops gpio_dev_pm_ops = {
-		.suspend = gpio_dw_suspend_port,
-		.resume = gpio_dw_resume_port
-};
 
-DEVICE_AND_API_INIT_PM(gpio_dw_0, CONFIG_GPIO_DW_0_NAME, gpio_dw_initialize,
-		       &gpio_dev_pm_ops, &gpio_0_runtime, &gpio_config_0,
+DEVICE_DEFINE(gpio_dw_0, CONFIG_GPIO_DW_0_NAME, gpio_dw_initialize,
+		       gpio_dw_device_ctrl, &gpio_0_runtime, &gpio_config_0,
 		       SECONDARY, CONFIG_GPIO_DW_INIT_PRIORITY,
 		       &api_funcs);
 #else
@@ -563,8 +599,8 @@ struct gpio_dw_config gpio_dw_config_1 = {
 struct gpio_dw_runtime gpio_1_runtime;
 
 #ifdef CONFIG_DEVICE_POWER_MANAGEMENT
-DEVICE_AND_API_INIT_PM(gpio_dw_1, CONFIG_GPIO_DW_1_NAME, gpio_dw_initialize,
-		       &gpio_dev_pm_ops, &gpio_1_runtime, &gpio_dw_config_1,
+DEVICE_DEFINE(gpio_dw_1, CONFIG_GPIO_DW_1_NAME, gpio_dw_initialize,
+		       gpio_dw_device_ctrl, &gpio_1_runtime, &gpio_dw_config_1,
 		       SECONDARY, CONFIG_GPIO_DW_INIT_PRIORITY,
 		       &api_funcs);
 #else
@@ -629,8 +665,8 @@ struct gpio_dw_config gpio_dw_config_2 = {
 struct gpio_dw_runtime gpio_2_runtime;
 
 #ifdef CONFIG_DEVICE_POWER_MANAGEMENT
-DEVICE_AND_API_INIT_PM(gpio_dw_2, CONFIG_GPIO_DW_2_NAME, gpio_dw_initialize,
-		       &gpio_dev_pm_ops, &gpio_2_runtime, &gpio_dw_config_2,
+DEVICE_DEFINE(gpio_dw_2, CONFIG_GPIO_DW_2_NAME, gpio_dw_initialize,
+		       gpio_dw_device_ctrl, &gpio_2_runtime, &gpio_dw_config_2,
 		       SECONDARY, CONFIG_GPIO_DW_INIT_PRIORITY,
 		       &api_funcs);
 #else
@@ -695,8 +731,8 @@ struct gpio_dw_config gpio_dw_config_3 = {
 struct gpio_dw_runtime gpio_3_runtime;
 
 #ifdef CONFIG_DEVICE_POWER_MANAGEMENT
-DEVICE_AND_API_INIT_PM(gpio_dw_3, CONFIG_GPIO_DW_3_NAME, gpio_dw_initialize,
-		       &gpio_dev_pm_ops, &gpio_3_runtime, &gpio_dw_config_3,
+DEVICE_DEFINE(gpio_dw_3, CONFIG_GPIO_DW_3_NAME, gpio_dw_initialize,
+		       gpio_dw_device_ctrl, &gpio_3_runtime, &gpio_dw_config_3,
 		       SECONDARY, CONFIG_GPIO_DW_INIT_PRIORITY,
 		       &api_funcs);
 #else

drivers/counter/counter_qmsi_aonpt.c
@@ -31,6 +31,9 @@ static counter_callback_t user_cb;
 
 struct aon_data {
 	struct nano_sem sem;
+#ifdef CONFIG_DEVICE_POWER_MANAGEMENT
+	uint32_t device_power_state;
+#endif
 };
 
 #ifdef CONFIG_AON_API_REENTRANCY
@@ -161,6 +164,63 @@ static struct counter_driver_api aon_timer_qmsi_api = {
 	.set_alarm = aon_timer_qmsi_set_alarm,
 };
 
+#ifdef CONFIG_DEVICE_POWER_MANAGEMENT
+static uint32_t int_aonpt_mask_save;
+
+static void aonpt_qmsi_set_power_state(struct device *dev, uint32_t power_state)
+{
+	struct aon_data *context = dev->driver_data;
+
+	context->device_power_state = power_state;
+}
+
+static uint32_t aonpt_qmsi_get_power_state(struct device *dev)
+{
+	struct aon_data *context = dev->driver_data;
+
+	return context->device_power_state;
+}
+
+static int aonpt_suspend_device(struct device *dev)
+{
+	int_aonpt_mask_save = QM_SCSS_INT->int_aon_timer_mask;
+
+	aonpt_qmsi_set_power_state(dev, DEVICE_PM_SUSPEND_STATE);
+	return 0;
+}
+
+static int aonpt_resume_device_from_suspend(struct device *dev)
+{
+	QM_SCSS_INT->int_aon_timer_mask = int_aonpt_mask_save;
+
+	aonpt_qmsi_set_power_state(dev, DEVICE_PM_ACTIVE_STATE);
+	return 0;
+}
+
+/*
+* Implements the driver control management functionality
+* the *context may include IN data or/and OUT data
+*/
+static int aonpt_qmsi_device_ctrl(struct device *dev, uint32_t ctrl_command,
+				  void *context)
+{
+	if (ctrl_command == DEVICE_PM_SET_POWER_STATE) {
+		if (*((uint32_t *)context) == DEVICE_PM_SUSPEND_STATE) {
+			return aonpt_suspend_device(dev);
+		} else if (*((uint32_t *)context) == DEVICE_PM_ACTIVE_STATE) {
+			return aonpt_resume_device_from_suspend(dev);
+		}
+	} else if (ctrl_command == DEVICE_PM_GET_POWER_STATE) {
+		*((uint32_t *)context) = aonpt_qmsi_get_power_state(dev);
+		return 0;
+	}
+
+	return 0;
+}
+#else
+#define aonpt_qmsi_set_power_state(...)
+#endif
+
 static int aon_timer_init(struct device *dev)
 {
 	dev->driver_api = &aon_timer_qmsi_api;
@@ -176,37 +236,16 @@ static int aon_timer_init(struct device *dev)
 
 	aon_reentrancy_init(dev);
 
-	return 0;
-}
-
-#ifdef CONFIG_DEVICE_POWER_MANAGEMENT
-static uint32_t int_aonpt_mask_save;
-
-static int aonpt_suspend_device(struct device *dev, int pm_policy)
-{
-	if (pm_policy == SYS_PM_DEEP_SLEEP) {
-		int_aonpt_mask_save = QM_SCSS_INT->int_aon_timer_mask;
-	}
+	aonpt_qmsi_set_power_state(dev, DEVICE_PM_ACTIVE_STATE);
 
 	return 0;
 }
 
-static int aonpt_resume_device(struct device *dev, int pm_policy)
-{
-	if (pm_policy == SYS_PM_DEEP_SLEEP) {
-		QM_SCSS_INT->int_aon_timer_mask = int_aonpt_mask_save;
-	}
 
-	return 0;
-}
-#endif
-
-DEFINE_DEVICE_PM_OPS(aon_timer, aonpt_suspend_device, aonpt_resume_device);
-
-DEVICE_AND_API_INIT_PM(aon_timer, CONFIG_AON_TIMER_QMSI_DEV_NAME,
-		    aon_timer_init, DEVICE_PM_OPS_GET(aon_timer), AONPT_CONTEXT,
-		    NULL, SECONDARY, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
-		    (void *)&aon_timer_qmsi_api);
+DEVICE_DEFINE(aon_timer, CONFIG_AON_TIMER_QMSI_DEV_NAME, aon_timer_init,
+	      aonpt_qmsi_device_ctrl, AONPT_CONTEXT, NULL, SECONDARY,
+	      CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
+	      (void *)&aon_timer_qmsi_api);
 
 static void aonpt_int_callback(void *user_data)
 {

drivers/gpio/gpio_dw.h
@@ -54,6 +54,9 @@ struct gpio_dw_runtime {
 	struct device *clock;
 #endif
 	sys_slist_t callbacks;
+#ifdef CONFIG_DEVICE_POWER_MANAGEMENT
+	uint32_t device_power_state;
+#endif
 };
 
 #ifdef __cplusplus

drivers/gpio/gpio_qmsi.c
@@ -40,6 +40,9 @@ struct gpio_qmsi_runtime {
 #ifdef CONFIG_GPIO_QMSI_API_REENTRANCY
 	struct nano_sem sem;
 #endif /* CONFIG_GPIO_QMSI_API_REENTRANCY */
+#ifdef CONFIG_DEVICE_POWER_MANAGEMENT
+	uint32_t device_power_state;
+#endif
 };
 
 #ifdef CONFIG_GPIO_QMSI_API_REENTRANCY
@@ -80,6 +83,25 @@ static void gpio_critical_region_end(struct device *dev)
 
 int gpio_qmsi_init(struct device *dev);
 
+#ifdef CONFIG_DEVICE_POWER_MANAGEMENT
+static void gpio_qmsi_set_power_state(struct device *dev, uint32_t power_state)
+{
+	struct gpio_qmsi_runtime *context = dev->driver_data;
+
+	context->device_power_state = power_state;
+}
+
+static uint32_t gpio_qmsi_get_power_state(struct device *dev)
+{
+	struct gpio_qmsi_runtime *context = dev->driver_data;
+
+	return context->device_power_state;
+}
+#else
+#define gpio_qmsi_set_power_state(...)
+#endif
+
+
 #ifdef CONFIG_GPIO_QMSI_0
 static struct gpio_qmsi_config gpio_0_config = {
 	.gpio = QM_GPIO_0,
@@ -92,48 +114,68 @@ static struct gpio_qmsi_runtime gpio_0_runtime;
 QM_RW uint32_t save_reg[10];
 static uint32_t int_gpio_mask_save;
 
-static int gpio_suspend_device(struct device *dev, int pm_policy)
+static int gpio_suspend_device(struct device *dev)
 {
-	if (pm_policy == SYS_PM_DEEP_SLEEP) {
-		int_gpio_mask_save = REG_VAL(&QM_SCSS_INT->int_gpio_mask);
-		save_reg[0] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_swporta_dr);
-		save_reg[1] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_swporta_ddr);
-		save_reg[2] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_swporta_ctl);
-		save_reg[3] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_inten);
-		save_reg[4] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_intmask);
-		save_reg[5] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_inttype_level);
-		save_reg[6] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_int_polarity);
-		save_reg[7] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_debounce);
-		save_reg[8] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_ls_sync);
-		save_reg[9] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_int_bothedge);
-	}
+	int_gpio_mask_save = REG_VAL(&QM_SCSS_INT->int_gpio_mask);
+	save_reg[0] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_swporta_dr);
+	save_reg[1] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_swporta_ddr);
+	save_reg[2] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_swporta_ctl);
+	save_reg[3] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_inten);
+	save_reg[4] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_intmask);
+	save_reg[5] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_inttype_level);
+	save_reg[6] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_int_polarity);
+	save_reg[7] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_debounce);
+	save_reg[8] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_ls_sync);
+	save_reg[9] = REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_int_bothedge);
+
+	gpio_qmsi_set_power_state(dev, DEVICE_PM_SUSPEND_STATE);
+
 	return 0;
 }
 
-static int gpio_resume_device(struct device *dev, int pm_policy)
+static int gpio_resume_device_from_suspend(struct device *dev)
 {
-	if (pm_policy == SYS_PM_DEEP_SLEEP) {
-		REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_swporta_dr) = save_reg[0];
-		REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_swporta_ddr) = save_reg[1];
-		REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_swporta_ctl) = save_reg[2];
-		REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_inten) = save_reg[3];
-		REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_intmask) = save_reg[4];
-		REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_inttype_level) = save_reg[5];
-		REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_int_polarity) = save_reg[6];
-		REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_debounce) = save_reg[7];
-		REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_ls_sync) = save_reg[8];
-		REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_int_bothedge) = save_reg[9];
-		REG_VAL(&QM_SCSS_INT->int_gpio_mask) = int_gpio_mask_save;
+	REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_swporta_dr) = save_reg[0];
+	REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_swporta_ddr) = save_reg[1];
+	REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_swporta_ctl) = save_reg[2];
+	REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_inten) = save_reg[3];
+	REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_intmask) = save_reg[4];
+	REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_inttype_level) = save_reg[5];
+	REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_int_polarity) = save_reg[6];
+	REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_debounce) = save_reg[7];
+	REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_ls_sync) = save_reg[8];
+	REG_VAL(&QM_GPIO[QM_GPIO_0]->gpio_int_bothedge) = save_reg[9];
+	REG_VAL(&QM_SCSS_INT->int_gpio_mask) = int_gpio_mask_save;
+
+	gpio_qmsi_set_power_state(dev, DEVICE_PM_ACTIVE_STATE);
+
+	return 0;
+}
+
+/*
+* Implements the driver control management functionality
+* the *context may include IN data or/and OUT data
+*/
+static int gpio_qmsi_device_ctrl(struct device *port, uint32_t ctrl_command,
+				 void *context)
+{
+	if (ctrl_command == DEVICE_PM_SET_POWER_STATE) {
+		if (*((uint32_t *)context) == DEVICE_PM_SUSPEND_STATE) {
+			return gpio_suspend_device(port);
+		} else if (*((uint32_t *)context) == DEVICE_PM_ACTIVE_STATE) {
+			return gpio_resume_device_from_suspend(port);
+		}
+	} else if (ctrl_command == DEVICE_PM_GET_POWER_STATE) {
+		*((uint32_t *)context) = gpio_qmsi_get_power_state(port);
+		return 0;
 	}
 	return 0;
 }
 #endif
 
-DEFINE_DEVICE_PM_OPS(gpio, gpio_suspend_device, gpio_resume_device);
-
-DEVICE_INIT_PM(gpio_0, CONFIG_GPIO_QMSI_0_NAME, &gpio_qmsi_init,
-		DEVICE_PM_OPS_GET(gpio), &gpio_0_runtime, &gpio_0_config,
-		SECONDARY, CONFIG_GPIO_QMSI_INIT_PRIORITY);
+DEVICE_DEFINE(gpio_0, CONFIG_GPIO_QMSI_0_NAME, &gpio_qmsi_init,
+	      gpio_qmsi_device_ctrl, &gpio_0_runtime, &gpio_0_config,
+	      SECONDARY, CONFIG_GPIO_QMSI_INIT_PRIORITY, NULL);
 
 #endif /* CONFIG_GPIO_QMSI_0 */
 
@@ -149,30 +191,44 @@ static struct gpio_qmsi_runtime gpio_aon_runtime;
 #ifdef CONFIG_DEVICE_POWER_MANAGEMENT
 static uint32_t int_gpio_aon_mask_save;
 
-static int gpio_aon_suspend_device(struct device *dev, int pm_policy)
+static int gpio_aon_suspend_device(struct device *dev)
 {
-	if (pm_policy == SYS_PM_DEEP_SLEEP) {
-		int_gpio_aon_mask_save =
-				REG_VAL(&QM_SCSS_INT->int_aon_gpio_mask);
-	}
+	int_gpio_aon_mask_save = REG_VAL(&QM_SCSS_INT->int_aon_gpio_mask);
+	gpio_qmsi_set_power_state(dev, DEVICE_PM_SUSPEND_STATE);
 	return 0;
 }
 
-static int gpio_aon_resume_device(struct device *dev, int pm_policy)
+static int gpio_aon_resume_device_from_suspend(struct device *dev)
 {
-	if (pm_policy == SYS_PM_DEEP_SLEEP) {
-		REG_VAL(&QM_SCSS_INT->int_aon_gpio_mask) =
-				int_gpio_aon_mask_save;
+	REG_VAL(&QM_SCSS_INT->int_aon_gpio_mask) = int_gpio_aon_mask_save;
+	gpio_qmsi_set_power_state(dev, DEVICE_PM_ACTIVE_STATE);
+	return 0;
+}
+
+/*
+* Implements the driver control management functionality
+* the *context may include IN data or/and OUT data
+*/
+static int gpio_aon_device_ctrl(struct device *port, uint32_t ctrl_command,
+				void *context)
+{
+	if (ctrl_command == DEVICE_PM_SET_POWER_STATE) {
+		if (*((uint32_t *)context) == DEVICE_PM_SUSPEND_STATE) {
+			return gpio_aon_suspend_device(port);
+		} else if (*((uint32_t *)context) == DEVICE_PM_ACTIVE_STATE) {
+			return gpio_aon_resume_device_from_suspend(port);
+		}
+	} else if (ctrl_command == DEVICE_PM_GET_POWER_STATE) {
+		*((uint32_t *)context) = gpio_qmsi_get_power_state(port);
+		return 0;
 	}
 	return 0;
 }
 #endif
 
-DEFINE_DEVICE_PM_OPS(gpio_aon, gpio_aon_suspend_device, gpio_aon_resume_device);
-
-DEVICE_INIT_PM(gpio_aon, CONFIG_GPIO_QMSI_AON_NAME, &gpio_qmsi_init,
-	       DEVICE_PM_OPS_GET(gpio_aon), &gpio_aon_runtime,
-	       &gpio_aon_config, SECONDARY, CONFIG_GPIO_QMSI_INIT_PRIORITY);
+DEVICE_DEFINE(gpio_aon, CONFIG_GPIO_QMSI_AON_NAME, &gpio_qmsi_init,
+	      gpio_aon_device_ctrl, &gpio_aon_runtime, &gpio_aon_config,
+	      SECONDARY, CONFIG_GPIO_QMSI_INIT_PRIORITY, NULL);
 
 #endif /* CONFIG_GPIO_QMSI_AON */
 
@@ -421,6 +477,8 @@ int gpio_qmsi_init(struct device *port)
 	default:
 		return -EIO;
 	}
+
+	gpio_qmsi_set_power_state(port, DEVICE_PM_ACTIVE_STATE);
 
 	port->driver_api = &api_funcs;
 	return 0;

drivers/i2c/i2c_qmsi.c
@@ -66,94 +66,125 @@ struct i2c_qmsi_driver_data {
 	struct nano_sem sem;
 #ifdef CONFIG_DEVICE_POWER_MANAGEMENT
 	struct i2c_context_t ctx_save;
+	uint32_t device_power_state;
 #endif
 };
 
 #ifdef CONFIG_DEVICE_POWER_MANAGEMENT
-static int i2c_suspend_device(struct device *dev, int pm_policy)
+
+
+static void i2c_qmsi_set_power_state(struct device *dev, uint32_t power_state)
+{
+	struct i2c_qmsi_driver_data *drv_data = dev->driver_data;
+
+	drv_data->device_power_state = power_state;
+}
+
+static uint32_t i2c_qmsi_get_power_state(struct device *dev)
+{
+	struct i2c_qmsi_driver_data *drv_data = dev->driver_data;
+
+	return drv_data->device_power_state;
+}
+
+static int i2c_suspend_device(struct device *dev)
 {
 	if (device_busy_check(dev)) {
 		return -EBUSY;
 	}
 
-	if (pm_policy == SYS_PM_DEEP_SLEEP) {
-		qm_i2c_t instance = GET_CONTROLLER_INSTANCE(dev);
-		qm_i2c_reg_t *const regs = QM_I2C[instance];
-		struct i2c_qmsi_driver_data *drv_data = dev->driver_data;
-		struct i2c_context_t *const ctx_save = &drv_data->ctx_save;
+	qm_i2c_t instance = GET_CONTROLLER_INSTANCE(dev);
+	qm_i2c_reg_t *const regs = QM_I2C[instance];
+	struct i2c_qmsi_driver_data *drv_data = dev->driver_data;
+	struct i2c_context_t *const ctx_save = &drv_data->ctx_save;
 
-		ctx_save->ic_con = regs->ic_con;
-		ctx_save->ic_fs_spklen = regs->ic_fs_spklen;
-		ctx_save->ic_hs_spklen = regs->ic_hs_spklen;
-		ctx_save->ic_ss_scl_lcnt = regs->ic_ss_scl_lcnt;
-		ctx_save->ic_ss_scl_hcnt = regs->ic_ss_scl_hcnt;
-		ctx_save->ic_fs_scl_lcnt = regs->ic_fs_scl_lcnt;
-		ctx_save->ic_fs_scl_hcnt = regs->ic_fs_scl_hcnt;
-		ctx_save->ic_hs_scl_lcnt = regs->ic_hs_scl_lcnt;
-		ctx_save->ic_hs_scl_hcnt = regs->ic_hs_scl_hcnt;
-		ctx_save->ic_intr_mask = regs->ic_intr_mask;
-		ctx_save->ic_sda_hold = regs->ic_sda_hold;
-		ctx_save->ic_sda_setup = regs->ic_sda_setup;
-		ctx_save->ic_ack_general_call = regs->ic_ack_general_call;
-		ctx_save->ic_sar = regs->ic_sar;
-		ctx_save->ic_dma_cr = regs->ic_dma_cr;
-		ctx_save->ic_dma_tdlr = regs->ic_dma_tdlr;
-		ctx_save->ic_dma_rdlr = regs->ic_dma_rdlr;
+	ctx_save->ic_con = regs->ic_con;
+	ctx_save->ic_fs_spklen = regs->ic_fs_spklen;
+	ctx_save->ic_hs_spklen = regs->ic_hs_spklen;
+	ctx_save->ic_ss_scl_lcnt = regs->ic_ss_scl_lcnt;
+	ctx_save->ic_ss_scl_hcnt = regs->ic_ss_scl_hcnt;
+	ctx_save->ic_fs_scl_lcnt = regs->ic_fs_scl_lcnt;
+	ctx_save->ic_fs_scl_hcnt = regs->ic_fs_scl_hcnt;
+	ctx_save->ic_hs_scl_lcnt = regs->ic_hs_scl_lcnt;
+	ctx_save->ic_hs_scl_hcnt = regs->ic_hs_scl_hcnt;
+	ctx_save->ic_intr_mask = regs->ic_intr_mask;
+	ctx_save->ic_sda_hold = regs->ic_sda_hold;
+	ctx_save->ic_sda_setup = regs->ic_sda_setup;
+	ctx_save->ic_ack_general_call = regs->ic_ack_general_call;
+	ctx_save->ic_sar = regs->ic_sar;
+	ctx_save->ic_dma_cr = regs->ic_dma_cr;
+	ctx_save->ic_dma_tdlr = regs->ic_dma_tdlr;
+	ctx_save->ic_dma_rdlr = regs->ic_dma_rdlr;
 
-		if (instance == QM_I2C_0) {
-			ctx_save->int_i2c_mst_mask =
-				QM_SCSS_INT->int_i2c_mst_0_mask;
-		} else {
-			ctx_save->int_i2c_mst_mask =
-				QM_SCSS_INT->int_i2c_mst_1_mask;
-		}
-
+	if (instance == QM_I2C_0) {
+		ctx_save->int_i2c_mst_mask = QM_SCSS_INT->int_i2c_mst_0_mask;
+	} else {
+		ctx_save->int_i2c_mst_mask = QM_SCSS_INT->int_i2c_mst_1_mask;
 	}
 
+	i2c_qmsi_set_power_state(dev, DEVICE_PM_SUSPEND_STATE);
 	return 0;
 }
 
-static int i2c_resume_device(struct device *dev, int pm_policy)
+static int i2c_resume_device_from_suspend(struct device *dev)
 {
-	if (pm_policy == SYS_PM_DEEP_SLEEP) {
-		struct i2c_qmsi_config_info *config = dev->config->config_info;
-		qm_i2c_reg_t *const regs = QM_I2C[config->instance];
-		struct i2c_qmsi_driver_data *drv_data = dev->driver_data;
-		struct i2c_context_t *const ctx_save = &drv_data->ctx_save;
+	struct i2c_qmsi_config_info *config = dev->config->config_info;
+	qm_i2c_reg_t *const regs = QM_I2C[config->instance];
+	struct i2c_qmsi_driver_data *drv_data = dev->driver_data;
+	struct i2c_context_t *const ctx_save = &drv_data->ctx_save;
 
-		regs->ic_con = ctx_save->ic_con;
-		regs->ic_fs_spklen = ctx_save->ic_fs_spklen;
-		regs->ic_hs_spklen = ctx_save->ic_hs_spklen;
-		regs->ic_ss_scl_lcnt = ctx_save->ic_ss_scl_lcnt;
-		regs->ic_ss_scl_hcnt = ctx_save->ic_ss_scl_hcnt;
-		regs->ic_fs_scl_lcnt = ctx_save->ic_fs_scl_lcnt;
-		regs->ic_fs_scl_hcnt = ctx_save->ic_fs_scl_hcnt;
-		regs->ic_hs_scl_lcnt = ctx_save->ic_hs_scl_lcnt;
-		regs->ic_hs_scl_hcnt = ctx_save->ic_hs_scl_hcnt;
-		regs->ic_intr_mask = ctx_save->ic_intr_mask;
-		regs->ic_sda_hold = ctx_save->ic_sda_hold;
-		regs->ic_sda_setup = ctx_save->ic_sda_setup;
-		regs->ic_ack_general_call = ctx_save->ic_ack_general_call;
-		regs->ic_sar = ctx_save->ic_sar;
-		regs->ic_dma_cr = ctx_save->ic_dma_cr;
-		regs->ic_dma_tdlr = ctx_save->ic_dma_tdlr;
-		regs->ic_dma_rdlr = ctx_save->ic_dma_rdlr;
+	regs->ic_con = ctx_save->ic_con;
+	regs->ic_fs_spklen = ctx_save->ic_fs_spklen;
+	regs->ic_hs_spklen = ctx_save->ic_hs_spklen;
+	regs->ic_ss_scl_lcnt = ctx_save->ic_ss_scl_lcnt;
+	regs->ic_ss_scl_hcnt = ctx_save->ic_ss_scl_hcnt;
+	regs->ic_fs_scl_lcnt = ctx_save->ic_fs_scl_lcnt;
+	regs->ic_fs_scl_hcnt = ctx_save->ic_fs_scl_hcnt;
+	regs->ic_hs_scl_lcnt = ctx_save->ic_hs_scl_lcnt;
+	regs->ic_hs_scl_hcnt = ctx_save->ic_hs_scl_hcnt;
+	regs->ic_intr_mask = ctx_save->ic_intr_mask;
+	regs->ic_sda_hold = ctx_save->ic_sda_hold;
+	regs->ic_sda_setup = ctx_save->ic_sda_setup;
+	regs->ic_ack_general_call = ctx_save->ic_ack_general_call;
+	regs->ic_sar = ctx_save->ic_sar;
+	regs->ic_dma_cr = ctx_save->ic_dma_cr;
+	regs->ic_dma_tdlr = ctx_save->ic_dma_tdlr;
+	regs->ic_dma_rdlr = ctx_save->ic_dma_rdlr;
 
-		if (config->instance == QM_I2C_0) {
-			QM_SCSS_INT->int_i2c_mst_0_mask =
-				ctx_save->int_i2c_mst_mask;
-		} else {
-			QM_SCSS_INT->int_i2c_mst_1_mask =
-				ctx_save->int_i2c_mst_mask;
+	if (config->instance == QM_I2C_0) {
+		QM_SCSS_INT->int_i2c_mst_0_mask = ctx_save->int_i2c_mst_mask;
+	} else {
+		QM_SCSS_INT->int_i2c_mst_1_mask = ctx_save->int_i2c_mst_mask;
+	}
+
+	i2c_qmsi_set_power_state(dev, DEVICE_PM_ACTIVE_STATE);
+
+	return 0;
+}
+
+/*
+* Implements the driver control management functionality
+* the *context may include IN data or/and OUT data
+*/
+static int i2c_device_ctrl(struct device *dev, uint32_t ctrl_command,
+			   void *context)
+{
+	if (ctrl_command == DEVICE_PM_SET_POWER_STATE) {
+		if (*((uint32_t *)context) == DEVICE_PM_SUSPEND_STATE) {
+			return i2c_suspend_device(dev);
+		} else if (*((uint32_t *)context) == DEVICE_PM_ACTIVE_STATE) {
+			return i2c_resume_device_from_suspend(dev);
 		}
-
+	} else if (ctrl_command == DEVICE_PM_GET_POWER_STATE) {
+		*((uint32_t *)context) = i2c_qmsi_get_power_state(dev);
+		return 0;
 	}
 
 	return 0;
 }
+#else
+#define i2c_qmsi_set_power_state(...)
 #endif /* CONFIG_DEVICE_POWER_MANAGEMENT */
-
-DEFINE_DEVICE_PM_OPS(i2c, i2c_suspend_device, i2c_resume_device);
 
 #ifdef CONFIG_I2C_0
 
@@ -165,9 +196,9 @@ static struct i2c_qmsi_config_info config_info_0 = {
 	.clock_gate = CLK_PERIPH_I2C_M0_REGISTER | CLK_PERIPH_CLK,
 };
 
-DEVICE_INIT_PM(i2c_0, CONFIG_I2C_0_NAME, &i2c_qmsi_init,
-	       DEVICE_PM_OPS_GET(i2c), &driver_data_0, &config_info_0,
-	       SECONDARY, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
+DEVICE_DEFINE(i2c_0, CONFIG_I2C_0_NAME, &i2c_qmsi_init,
+	      i2c_device_ctrl, &driver_data_0, &config_info_0, SECONDARY,
+	      CONFIG_KERNEL_INIT_PRIORITY_DEVICE, NULL);
 
 #endif /* CONFIG_I2C_0 */
 
@@ -181,9 +212,9 @@ static struct i2c_qmsi_config_info config_info_1 = {
 	.clock_gate = CLK_PERIPH_I2C_M1_REGISTER | CLK_PERIPH_CLK,
 };
 
-DEVICE_INIT_PM(i2c_1, CONFIG_I2C_1_NAME, &i2c_qmsi_init,
-	       DEVICE_PM_OPS_GET(i2c), &driver_data_1, &config_info_1,
-	       SECONDARY, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
+DEVICE_DEFINE(i2c_1, CONFIG_I2C_1_NAME, &i2c_qmsi_init,
+	      i2c_device_ctrl, &driver_data_1, &config_info_1, SECONDARY,
+	      CONFIG_KERNEL_INIT_PRIORITY_DEVICE, NULL);
 
 #endif /* CONFIG_I2C_1 */
 
@@ -346,5 +377,8 @@ static int i2c_qmsi_init(struct device *dev)
 	}
 
 	dev->driver_api = &api;
+
+	i2c_qmsi_set_power_state(dev, DEVICE_PM_ACTIVE_STATE);
+
 	return 0;
 }

commit 13d43e3da3138b167ffff95feb2616394e826e0c

drivers/gpio: Fix indentation in DW driver
Commit ID c490219d1ef introduced wrong indentation there.

Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

drivers/gpio/gpio_dw.c
@@ -452,9 +452,9 @@ static struct gpio_dw_runtime gpio_0_runtime = {
 #ifdef CONFIG_DEVICE_POWER_MANAGEMENT
 
 DEVICE_DEFINE(gpio_dw_0, CONFIG_GPIO_DW_0_NAME, gpio_dw_initialize,
-		       gpio_dw_device_ctrl, &gpio_0_runtime, &gpio_config_0,
-		       POST_KERNEL, CONFIG_GPIO_DW_INIT_PRIORITY,
-		       &api_funcs);
+	      gpio_dw_device_ctrl, &gpio_0_runtime, &gpio_config_0,
+	      POST_KERNEL, CONFIG_GPIO_DW_INIT_PRIORITY,
+	      &api_funcs);
 #else
 DEVICE_AND_API_INIT(gpio_dw_0, CONFIG_GPIO_DW_0_NAME, gpio_dw_initialize,
 		    &gpio_0_runtime, &gpio_config_0,
@@ -511,9 +511,9 @@ static struct gpio_dw_runtime gpio_1_runtime = {
 
 #ifdef CONFIG_DEVICE_POWER_MANAGEMENT
 DEVICE_DEFINE(gpio_dw_1, CONFIG_GPIO_DW_1_NAME, gpio_dw_initialize,
-		       gpio_dw_device_ctrl, &gpio_1_runtime, &gpio_dw_config_1,
-		       POST_KERNEL, CONFIG_GPIO_DW_INIT_PRIORITY,
-		       &api_funcs);
+	      gpio_dw_device_ctrl, &gpio_1_runtime, &gpio_dw_config_1,
+	      POST_KERNEL, CONFIG_GPIO_DW_INIT_PRIORITY,
+	      &api_funcs);
 #else
 DEVICE_AND_API_INIT(gpio_dw_1, CONFIG_GPIO_DW_1_NAME, gpio_dw_initialize,
 		    &gpio_1_runtime, &gpio_dw_config_1,
@@ -569,9 +569,9 @@ static struct gpio_dw_runtime gpio_2_runtime = {
 
 #ifdef CONFIG_DEVICE_POWER_MANAGEMENT
 DEVICE_DEFINE(gpio_dw_2, CONFIG_GPIO_DW_2_NAME, gpio_dw_initialize,
-		       gpio_dw_device_ctrl, &gpio_2_runtime, &gpio_dw_config_2,
-		       POST_KERNEL, CONFIG_GPIO_DW_INIT_PRIORITY,
-		       &api_funcs);
+	      gpio_dw_device_ctrl, &gpio_2_runtime, &gpio_dw_config_2,
+	      POST_KERNEL, CONFIG_GPIO_DW_INIT_PRIORITY,
+	      &api_funcs);
 #else
 DEVICE_AND_API_INIT(gpio_dw_2, CONFIG_GPIO_DW_2_NAME, gpio_dw_initialize,
 		    &gpio_2_runtime, &gpio_dw_config_2,
@@ -627,9 +627,9 @@ static struct gpio_dw_runtime gpio_3_runtime = {
 
 #ifdef CONFIG_DEVICE_POWER_MANAGEMENT
 DEVICE_DEFINE(gpio_dw_3, CONFIG_GPIO_DW_3_NAME, gpio_dw_initialize,
-		       gpio_dw_device_ctrl, &gpio_3_runtime, &gpio_dw_config_3,
-		       POST_KERNEL, CONFIG_GPIO_DW_INIT_PRIORITY,
-		       &api_funcs);
+	      gpio_dw_device_ctrl, &gpio_3_runtime, &gpio_dw_config_3,
+	      POST_KERNEL, CONFIG_GPIO_DW_INIT_PRIORITY,
+	      &api_funcs);
 #else
 DEVICE_AND_API_INIT(gpio_dw_3, CONFIG_GPIO_DW_3_NAME, gpio_dw_initialize,
 		    &gpio_3_runtime, &gpio_dw_config_3,