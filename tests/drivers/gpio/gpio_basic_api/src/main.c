03146ec8ebd22ed265584cd748c974966f8f1799
Peter Bigot <peter.bigot@nordicsemi.no>
2020-01-28 10:27:28 -0600

    tests: gpio_basic_api: update for deprecation
    Since the callback enable and disable functions are deprecated, but
    cannot be re-implemented in terms of other API, add back an old-style
    interrupt configuration test that uses the deprecated flags and
    functions in the way existing code would do.  The test module
    internally disables the deprecation warnings.
    
    Remove the test for the deprecated read/write functions, incorporating
    a basic check into the module that tests deprecated callback functions.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- tests/drivers/gpio/gpio_basic_api/src/main.c
+++ tests/drivers/gpio/gpio_basic_api/src/main.c
@@ -123,10 +123,10 @@ void test_main(void)
 	board_setup();
 	ztest_test_suite(gpio_basic_test,
 			 ztest_unit_test(test_gpio_port),
-			 ztest_unit_test(test_gpio_pin_read_write),
 			 ztest_unit_test(test_gpio_callback_add_remove),
 			 ztest_unit_test(test_gpio_callback_self_remove),
 			 ztest_unit_test(test_gpio_callback_enable_disable),
-			 ztest_unit_test(test_gpio_callback_variants));
+			 ztest_unit_test(test_gpio_callback_variants),
+			 ztest_unit_test(test_gpio_deprecated));
 	ztest_run_test_suite(gpio_basic_test);
 }
