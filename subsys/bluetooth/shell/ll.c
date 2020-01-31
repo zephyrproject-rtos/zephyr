dca5dc71a61850c6eca9b97b0698499524b34b38
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2020-01-31 12:06:02 +0530

    Bluetooth: controller: Move Adv Ext interface to ll interface
    Move Advertising Extensions interface to LL interface header
    file.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/shell/ll.c
+++ subsys/bluetooth/shell/ll.c
@@ -119,7 +119,6 @@ int cmd_test_end(const struct shell *shell, size_t  argc, char *argv[])
 #endif /* CONFIG_BT_CTLR_DTM */
 
 #if defined(CONFIG_BT_CTLR_ADV_EXT)
-#include "../controller/ll_sw/ull_adv_aux.h"
 #include "../controller/ll_sw/lll.h"
 
 #if defined(CONFIG_BT_BROADCASTER)
