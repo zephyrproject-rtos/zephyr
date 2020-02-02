e62518eec363d88c5185d0ee211ba6865da56936
Kumar Gala <kumar.gala@linaro.org>
2020-02-02 10:06:37 -0600

    xtensa fix - wip
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- lib/libc/newlib/libc-hooks.c
+++ lib/libc/newlib/libc-hooks.c
@@ -270,3 +270,96 @@ __weak int *__errno(void)
 {
 	return z_errno();
 }
+
+#if CONFIG_XTENSA
+#ifndef CONFIG_POSIX_API
+_ssize_t _read_r(struct _reent *r, int fd, void *buf, size_t nbytes)
+{
+	ARG_UNUSED(r);
+
+	return _read(fd, (char *)buf, nbytes);
+}
+
+_ssize_t _write_r(struct _reent *r, int fd, const void *buf, size_t nbytes)
+{
+	ARG_UNUSED(r);
+
+	return _write(fd, buf, nbytes);
+}
+
+int _open_r(struct _reent *r, const char *name, int flags, int mode)
+{
+	ARG_UNUSED(r);
+	ARG_UNUSED(flags);
+
+	return _open(name, mode);
+}
+
+int _close_r(struct _reent *r, int file)
+{
+	ARG_UNUSED(r);
+
+	return _close(file);
+}
+
+_off_t _lseek_r(struct _reent *r, int file, _off_t ptr, int dir)
+{
+	ARG_UNUSED(r);
+
+	return _lseek(file, ptr, dir);
+}
+#endif
+
+int _isatty_r(struct _reent *r, int file)
+{
+	ARG_UNUSED(r);
+
+	return _isatty(file);
+}
+
+int _kill_r(struct _reent *r, int i, int j)
+{
+	ARG_UNUSED(r);
+
+	return _kill(i, j);
+}
+
+int _getpid_r(struct _reent *r)
+{
+	ARG_UNUSED(r);
+
+	return _getpid();
+}
+
+int _fstat_r(struct _reent *r, int file, struct stat *st)
+{
+	ARG_UNUSED(r);
+
+	return _fstat(file, st);
+}
+
+void _exit_r(struct _reent *r, int status)
+{
+	ARG_UNUSED(r);
+
+	_exit(status);
+}
+
+void *_sbrk_r(struct _reent *r, int count)
+{
+	ARG_UNUSED(r);
+
+	return _sbrk(count);
+}
+
+#if 0
+int _gettimeofday_r(struct _reent *r, struct timeval *__tp, void *__tzp)
+{
+	ARG_UNUSED(r);
+	ARG_UNUSED(__tp);
+	ARG_UNUSED(__tzp);
+
+	return -1;
+}
+#endif /* CONFIG_XTENSA */
+#endif /* CONFIG_XTENSA */
