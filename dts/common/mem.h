dacf211d36295cf68f7ff5de6570b26d94159b3b
Kumar Gala <kumar.gala@linaro.org>
2020-02-05 12:21:27 -0600

    dts: introduce macro's to help with changing unit-addr
    Since there are times that we include a base .dtsi file and change the
    reg address we tend to end up with warnings that the reg address and
    unit-address don't match.  To handle this introduce a simple DT_ADDR(x)
    macro that will prepend '0x' to a 'unit-address' style address.
    
      #define DCCM_ADDR 80000000 /* in unit-address format */
    
      dccm0: dccm@DCCM_ADDR {
         reg = <DT_ADDR(DCCM_ADDR) 0x1>;
         ...
      };
    
    This allows the dts file to override the value of DCCM_ADDR and than to
    have the unit-address of the node and the reg address match.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- dts/common/mem.h
+++ dts/common/mem.h
@@ -10,4 +10,9 @@
 #define DT_SIZE_K(x) ((x) * 1024)
 #define DT_SIZE_M(x) ((x) * 1024 * 1024)
 
-#endif /* __DT_BINDING_TI_MEM_H */
+/* concatenate the values of the arguments into one */
+#define _DT_DO_CONCAT(x, y) x ## y
+
+#define DT_ADDR(a) _DT_DO_CONCAT(0x, a)
+
+#endif /* __DT_MEM_H */
