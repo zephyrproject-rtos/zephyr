2534ab11c0b3acf3abe3aabac08384d6bc35f3ec
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2020-02-05 14:59:49 -0800

    USB: Add driver and PID for Bluetooth H4
    This adds USB_PID_BLE_HCI_H4_SAMPLE along with it driver which uses H4
    over bulk endpoints.
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

---
+++ subsys/usb/class/bt_h4.c
@@ -0,0 +1,315 @@
+/*
+ * Wireless / Bluetooth USB class
+ *
+ * Copyright (c) 2020 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+#include <sys/byteorder.h>
+
+#include <usb/usb_device.h>
+#include <usb/usb_common.h>
+#include <usb_descriptor.h>
+
+#include <net/buf.h>
+
+#include <bluetooth/buf.h>
+#include <bluetooth/hci_raw.h>
+#include <bluetooth/l2cap.h>
+
+#define LOG_LEVEL CONFIG_USB_DEVICE_LOG_LEVEL
+#include <logging/log.h>
+LOG_MODULE_REGISTER(usb_bt_h4);
+
+static K_FIFO_DEFINE(rx_queue);
+static K_FIFO_DEFINE(tx_queue);
+
+/* ACL data TX buffers */
+#if defined(CONFIG_BT_CTLR_TX_BUFFERS)
+#define BUF_COUNT (CONFIG_BT_HCI_CMD_COUNT + CONFIG_BT_CTLR_TX_BUFFERS)
+#else
+#define BUF_COUNT (CONFIG_BT_HCI_CMD_COUNT + 4)
+#endif
+
+#if defined(CONFIG_BT_CTLR_TX_BUFFER_SIZE)
+#define BT_L2CAP_MTU (CONFIG_BT_CTLR_TX_BUFFER_SIZE - BT_L2CAP_HDR_SIZE)
+#else
+#define BT_L2CAP_MTU 64
+#endif
+
+/* Data size needed for CMD and ACL buffers */
+#define BUF_SIZE MAX(BT_BUF_RX_SIZE, BT_L2CAP_BUF_SIZE(BT_L2CAP_MTU))
+NET_BUF_POOL_DEFINE(rx_pool, BUF_COUNT, BUF_SIZE, sizeof(u8_t), NULL);
+
+#define BT_H4_OUT_EP_ADDR               0x01
+#define BT_H4_IN_EP_ADDR                0x81
+
+#define BT_H4_OUT_EP_IDX                0
+#define BT_H4_IN_EP_IDX                 1
+
+#define BT_H4_BULK_EP_MPS               MIN(BUF_SIZE, USB_MAX_FS_BULK_MPS)
+
+/* HCI RX/TX threads */
+static K_THREAD_STACK_DEFINE(rx_thread_stack, 512);
+static struct k_thread rx_thread_data;
+static K_THREAD_STACK_DEFINE(tx_thread_stack, 512);
+static struct k_thread tx_thread_data;
+
+struct usb_bt_h4_config {
+	struct usb_if_descriptor if0;
+	struct usb_ep_descriptor if0_out_ep;
+	struct usb_ep_descriptor if0_in_ep;
+} __packed;
+
+USBD_CLASS_DESCR_DEFINE(primary, 0) struct usb_bt_h4_config bt_h4_cfg = {
+	/* Interface descriptor 0 */
+	.if0 = {
+		.bLength = sizeof(struct usb_if_descriptor),
+		.bDescriptorType = USB_INTERFACE_DESC,
+		.bInterfaceNumber = 0,
+		.bAlternateSetting = 0,
+		.bNumEndpoints = 2,
+		.bInterfaceClass = CUSTOM_CLASS, /* TBD */
+		.bInterfaceSubClass = 0,
+		.bInterfaceProtocol = 0,
+		.iInterface = 0,
+	},
+
+	/* Data Endpoint OUT */
+	.if0_out_ep = {
+		.bLength = sizeof(struct usb_ep_descriptor),
+		.bDescriptorType = USB_ENDPOINT_DESC,
+		.bEndpointAddress = BT_H4_OUT_EP_ADDR,
+		.bmAttributes = USB_DC_EP_BULK,
+		.wMaxPacketSize = sys_cpu_to_le16(BT_H4_BULK_EP_MPS),
+		.bInterval = 0x01,
+	},
+
+	/* Data Endpoint IN */
+	.if0_in_ep = {
+		.bLength = sizeof(struct usb_ep_descriptor),
+		.bDescriptorType = USB_ENDPOINT_DESC,
+		.bEndpointAddress = BT_H4_IN_EP_ADDR,
+		.bmAttributes = USB_DC_EP_BULK,
+		.wMaxPacketSize = sys_cpu_to_le16(BT_H4_BULK_EP_MPS),
+		.bInterval = 0x01,
+	},
+};
+
+#define H4_CMD 0x01
+#define H4_ACL 0x02
+#define H4_SCO 0x03
+#define H4_EVT 0x04
+
+static struct usb_ep_cfg_data bt_h4_ep_data[] = {
+	{
+		.ep_cb = usb_transfer_ep_callback,
+		.ep_addr = BT_H4_OUT_EP_ADDR,
+	},
+	{
+		.ep_cb = usb_transfer_ep_callback,
+		.ep_addr = BT_H4_IN_EP_ADDR,
+	},
+};
+
+static void bt_h4_read(u8_t ep, int size, void *priv)
+{
+	struct net_buf *buf = priv;
+
+	if (size > 0) {
+		buf->len += size;
+		net_buf_put(&rx_queue, buf);
+		buf = NULL;
+	}
+
+	if (buf) {
+		net_buf_unref(buf);
+	}
+
+	buf = net_buf_alloc(&rx_pool, K_FOREVER);
+
+	net_buf_reserve(buf, BT_BUF_RESERVE);
+
+	/* Start a new read transfer */
+	usb_transfer(bt_h4_ep_data[BT_H4_OUT_EP_IDX].ep_addr, buf->data,
+		     BUF_SIZE, USB_TRANS_READ, bt_h4_read, buf);
+}
+
+static void usb_h4_send(struct net_buf *buf)
+{
+	LOG_DBG("buf %p type %u len %u", buf, bt_buf_get_type(buf), buf->len);
+
+	switch (bt_buf_get_type(buf)) {
+	case BT_BUF_ACL_IN:
+		net_buf_push_u8(buf, H4_ACL);
+		break;
+	case BT_BUF_EVT:
+		net_buf_push_u8(buf, H4_EVT);
+		break;
+	default:
+		LOG_ERR("Unknown type %u", bt_buf_get_type(buf));
+		net_buf_unref(buf);
+		return;
+	}
+
+	usb_transfer_sync(bt_h4_ep_data[BT_H4_IN_EP_IDX].ep_addr,
+			  buf->data, buf->len, USB_TRANS_WRITE);
+
+	net_buf_unref(buf);
+}
+
+static void hci_tx_thread(void)
+{
+	LOG_DBG("Start USB Bluetooth thread");
+
+	while (true) {
+		struct net_buf *buf;
+
+		buf = net_buf_get(&tx_queue, K_FOREVER);
+		usb_h4_send(buf);
+	}
+}
+
+static void usb_h4_recv(struct net_buf *buf)
+{
+	u8_t type;
+
+	type = net_buf_pull_u8(buf);
+
+	switch (type) {
+	case H4_CMD:
+		bt_buf_set_type(buf, BT_BUF_CMD);
+		break;
+	case H4_ACL:
+		bt_buf_set_type(buf, BT_BUF_ACL_OUT);
+		break;
+	default:
+		LOG_ERR("Unknown H4 type %u", type);
+		return;
+	}
+
+	LOG_DBG("buf %p type %u len %u", buf, bt_buf_get_type(buf), buf->len);
+
+	if (bt_send(buf)) {
+		LOG_ERR("Error sending to driver");
+		net_buf_unref(buf);
+	}
+}
+
+static void hci_rx_thread(void)
+{
+	while (true) {
+		struct net_buf *buf;
+
+		buf = net_buf_get(&rx_queue, K_FOREVER);
+		usb_h4_recv(buf);
+	}
+}
+
+static void bt_h4_status_cb(struct usb_cfg_data *cfg,
+			    enum usb_dc_status_code status, const u8_t *param)
+{
+	ARG_UNUSED(cfg);
+
+	/* Check the USB status and do needed action if required */
+	switch (status) {
+	case USB_DC_CONFIGURED:
+		LOG_DBG("USB device configured");
+		/* Start reading */
+		bt_h4_read(bt_h4_ep_data[BT_H4_OUT_EP_IDX].ep_addr, 0, NULL);
+		break;
+	case USB_DC_DISCONNECTED:
+		LOG_DBG("USB device disconnected");
+		/* Cancel any transfer */
+		usb_cancel_transfer(bt_h4_ep_data[BT_H4_IN_EP_IDX].ep_addr);
+		usb_cancel_transfer(bt_h4_ep_data[BT_H4_OUT_EP_IDX].ep_addr);
+		break;
+	case USB_DC_CONNECTED:
+	case USB_DC_ERROR:
+	case USB_DC_RESET:
+	case USB_DC_SUSPEND:
+	case USB_DC_RESUME:
+	case USB_DC_SOF:
+	case USB_DC_UNKNOWN:
+	default:
+		LOG_DBG("USB unhandled status: %u", status);
+		break;
+	}
+}
+
+static int bt_h4_vendor_handler(struct usb_setup_packet *setup,
+				s32_t *len, u8_t **data)
+{
+	LOG_DBG("Class request: bRequest 0x%x bmRequestType 0x%x len %d",
+		setup->bRequest, setup->bmRequestType, *len);
+
+	if (REQTYPE_GET_RECIP(setup->bmRequestType) != REQTYPE_RECIP_DEVICE) {
+		return -ENOTSUP;
+	}
+
+	if (REQTYPE_GET_DIR(setup->bmRequestType) == REQTYPE_DIR_TO_DEVICE &&
+	    setup->bRequest == 0x5b) {
+		LOG_DBG("Host-to-Device, data %p", *data);
+		return 0;
+	}
+
+	if ((REQTYPE_GET_DIR(setup->bmRequestType) == REQTYPE_DIR_TO_HOST) &&
+	    (setup->bRequest == 0x5c)) {
+		LOG_DBG("Device-to-Host, wLength %d, data %p",
+			setup->wLength, *data);
+		return 0;
+	}
+
+	return -ENOTSUP;
+}
+
+static void bt_h4_interface_config(struct usb_desc_header *head,
+				   u8_t bInterfaceNumber)
+{
+	ARG_UNUSED(head);
+
+	bt_h4_cfg.if0.bInterfaceNumber = bInterfaceNumber;
+}
+
+USBD_CFG_DATA_DEFINE(primary, hci_h4) struct usb_cfg_data bt_h4_config = {
+	.usb_device_description = NULL,
+	.interface_config = bt_h4_interface_config,
+	.interface_descriptor = &bt_h4_cfg.if0,
+	.cb_usb_status = bt_h4_status_cb,
+	.interface = {
+		.class_handler = NULL,
+		.custom_handler = NULL,
+		.vendor_handler = bt_h4_vendor_handler,
+	},
+	.num_endpoints = ARRAY_SIZE(bt_h4_ep_data),
+	.endpoint = bt_h4_ep_data,
+};
+
+static int bt_h4_init(struct device *dev)
+{
+	int ret;
+
+	LOG_DBG("Initialization");
+
+	ret = bt_enable_raw(&tx_queue);
+	if (ret) {
+		LOG_ERR("Failed to open Bluetooth raw channel: %d", ret);
+		return ret;
+	}
+
+	k_thread_create(&rx_thread_data, rx_thread_stack,
+			K_THREAD_STACK_SIZEOF(rx_thread_stack),
+			(k_thread_entry_t)hci_rx_thread, NULL, NULL, NULL,
+			K_PRIO_COOP(8), 0, K_NO_WAIT);
+
+	k_thread_create(&tx_thread_data, tx_thread_stack,
+			K_THREAD_STACK_SIZEOF(tx_thread_stack),
+			(k_thread_entry_t)hci_tx_thread, NULL, NULL, NULL,
+			K_PRIO_COOP(8), 0, K_NO_WAIT);
+
+	return 0;
+}
+
+SYS_INIT(bt_h4_init, APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
