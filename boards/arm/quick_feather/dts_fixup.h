737477e0f9a5d64dec7d013b70f73af22d7d96f9
Jan Kowalewski <jkowalewski@antmicro.com>
2020-02-10 17:48:57 +0100

    boards: quick_feather: enable NOR SPI Flash
    Configure and enable NOR SPI Flash for EOS S3 Quick Feather board.
    
    Signed-off-by: Jan Kowalewski <jkowalewski@antmicro.com>

---
+++ boards/arm/quick_feather/dts_fixup.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2020 Antmicro <www.antmicro.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifdef CONFIG_FLASH
+#define DT_FLASH_DEV_NAME	DT_INST_0_JEDEC_SPI_NOR_LABEL
+#endif /* CONFIG_FLASH */
