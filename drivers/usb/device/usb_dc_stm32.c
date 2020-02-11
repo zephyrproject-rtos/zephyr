aa30450505854779eb6f49ddf4e388ac2bf4779c
Stefan Jaritz <stefan@kokoontech.com>
2020-02-11 18:03:07 +0000

    usb: stm32: removed C++ style comments
    removed C++ comments.
    
    Signed-off-by: Stefan Jaritz <stefan@kokoontech.com>

--- drivers/usb/device/usb_dc_stm32.c
+++ drivers/usb/device/usb_dc_stm32.c
@@ -846,12 +846,9 @@ int usb_dc_ep_mps(const u8_t ep)
 
 int usb_dc_detach(void)
 {
-	// stop usb dev
 	HAL_PCD_Stop(&usb_dc_stm32_state.pcd);
-	// take care that the endpoints are closed (usb_dc_ep_disable)
 	HAL_PCD_EP_Close(&usb_dc_stm32_state.pcd, EP0_IN);
 	HAL_PCD_EP_Close(&usb_dc_stm32_state.pcd, EP0_OUT);
-	// now we deinit
 	HAL_PCD_DeInit(&usb_dc_stm32_state.pcd);
 	return 0;
 }
