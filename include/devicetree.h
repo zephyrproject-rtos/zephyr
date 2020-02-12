e8739ef0ac0bf33532341b626d732fe43c4ebd11
Kumar Gala <kumar.gala@linaro.org>
2020-02-12 13:42:26 -0600

    devicetree: Add helper macro for use with phandle defines
    If we have a phandle & base address define like:
    
      #define DT_INST_0_NXP_KINETIS_GPIO_PORT \
                  DT_NXP_KINETIS_GPIO_400FF000_PORT
      #define DT_NXP_KINETIS_GPIO_400FF000_PORT_BASE_ADDR 0xdeadbeef
    
    And we want to get the 'BASE_ADDR' property value based on the
    phandle, we introduce a helper macro DT_PHANDLE_GET_PROP for this
    purpose.
    
    The macro can be used like:
      x = DT_PHANDLE_GET_PROP(DT_INST_0_NXP_KINETIS_GPIO_PORT, BASE_ADDR);
    
    Will get:
      x = 0xdeadbeef;
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- include/devicetree.h
+++ include/devicetree.h
@@ -8,6 +8,12 @@
 #ifndef DEVICETREE_H
 #define DEVICETREE_H
 
+/* concatenate the values of the arguments into one */
+#define _DT_PHANDLE_GET_PROP(phandle, prop) phandle ## _ ## prop
+
+/* Given a phandle define and a property name get back the actual value */
+#define DT_PHANDLE_GET_PROP(phandle, prop) _DT_PHANDLE_GET_PROP(phandle, prop)
+
 #include <devicetree_unfixed.h>
 #include <devicetree_fixups.h>
 
