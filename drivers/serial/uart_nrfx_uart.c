668d401293a86709abb63cca6467b075193caafd
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2020-02-17 09:09:22 +0100

    modules: hal: nordic: Define NRFX_ASSERT as __ASSERT_NO_MSG
    Update hal_nordic's revision, so that NRFX_ASSERT uses __ASSERT_NO_MSG
    directly, not through the assert macro that comes from from libc,
    as the definition of the latter might be different when some specific
    libc version is used, and this could generate troubles.
    
    Replace also uses of assert() with __ASSERT_NO_MSG() in nrfx driver
    shims that use this macro without including the corresponding header
    file (i.e. that implicitly rely on assert.h being included from
    nrfx_glue.h, which is no longer the case).
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- drivers/serial/uart_nrfx_uart.c
+++ drivers/serial/uart_nrfx_uart.c
@@ -1026,9 +1026,9 @@ static void uart_nrfx_set_power_state(struct device *dev, u32_t new_state)
 		nrf_uart_enable(uart0_addr);
 		nrf_uart_task_trigger(uart0_addr, NRF_UART_TASK_STARTRX);
 	} else {
-		assert(new_state == DEVICE_PM_LOW_POWER_STATE ||
-		       new_state == DEVICE_PM_SUSPEND_STATE ||
-		       new_state == DEVICE_PM_OFF_STATE);
+		__ASSERT_NO_MSG(new_state == DEVICE_PM_LOW_POWER_STATE ||
+				new_state == DEVICE_PM_SUSPEND_STATE ||
+				new_state == DEVICE_PM_OFF_STATE);
 		nrf_uart_disable(uart0_addr);
 		uart_nrfx_pins_enable(dev, false);
 	}
@@ -1047,7 +1047,7 @@ static int uart_nrfx_pm_control(struct device *dev, u32_t ctrl_command,
 			current_state = new_state;
 		}
 	} else {
-		assert(ctrl_command == DEVICE_PM_GET_POWER_STATE);
+		__ASSERT_NO_MSG(ctrl_command == DEVICE_PM_GET_POWER_STATE);
 		*((u32_t *)context) = current_state;
 	}
 
