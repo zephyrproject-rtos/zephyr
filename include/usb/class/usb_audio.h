98c9cebfac2d825e28335578726edd13ea0973a6
Emil Obalski <emil.obalski@nordicsemi.no>
2020-02-18 12:17:04 +0100

    usb: Add Audio class related files
    This commit adds files related to USB audio device class.
    
    Signed-off-by: Emil Obalski <emil.obalski@nordicsemi.no>

---
+++ include/usb/class/usb_audio.h
@@ -0,0 +1,20 @@
+/*
+ * USB audio class core header
+ *
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief USB Audio Device Class public header
+ *
+ * Header follows Device Class Definition for Audio Class
+ * Version 1.0 document (audio10.pdf).
+ */
+
+#ifndef ZEPHYR_INCLUDE_USB_CLASS_AUDIO_H_
+#define ZEPHYR_INCLUDE_USB_CLASS_AUDIO_H_
+
+#endif /* ZEPHYR_INCLUDE_USB_CLASS_AUDIO_H_ */
