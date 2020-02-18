126ddcb5b2f12e70add2b9cc17afbaa9b8addf32
Emil Obalski <emil.obalski@nordicsemi.no>
2020-02-18 12:17:04 +0100

    usb: Add Audio class related files
    This commit adds files related to USB audio device class.
    
    Signed-off-by: Emil Obalski <emil.obalski@nordicsemi.no>

---
+++ subsys/usb/class/audio/audio.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Audio device class driver
+ *
+ * Driver for USB Audio device class driver
+ */
