5309ef6c990864afce1c09912cb3e00260341f2d
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2020-02-20 00:00:25 +0900

    boards: soc: riscv: add QEMU RV32 virt board
    This patch add definitions and settings for QEMU RV32 virt machine.
    These are minimal supports only for CPU, Memory, CLINT and UART.
    
    QEMU has supported multiple machine types. It seems virt machine
    is most suitable for developing and testing SMP mode on rv32/rv64
    both architecture using QEMU.
    
     | Support       | sifive_u | spike | virt  |
     +---------------+----------+-------+-------+
     | SMP           | yes      | yes   | yes   |
     | RV32/64       | 64       | 32/64 | 32/64 |
     | UART input    | yes      | no    | yes   |
     | Real machine? | yes      | no    | no    |
    
    Signed-off-by: Katsuhiro Suzuki <katsuhiro@katsuster.net>

---
+++ soc/riscv/riscv-privilege/rv32-virt/soc.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 Katsuhiro Suzuki
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file SoC configuration macros for the QEMU virt series processor
+ */
+
+#ifndef __QEMU_RV32_VIRT_SOC_H_
+#define __QEMU_RV32_VIRT_SOC_H_
+
+#include <soc_common.h>
+#include <devicetree.h>
+
+/* Timer configuration */
+#define RISCV_MTIMECMP_BASE          0x02004000
+#define RISCV_MTIME_BASE             0x0200bff8
+
+#endif /* __QEMU_RV32_VIRT_SOC_H_ */
