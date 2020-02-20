04f00525dc40b5b2e9cda3c82a054b9d8a68ba0d
Kumar Gala <kumar.gala@linaro.org>
2020-02-20 12:10:11 -0600

    devicetree: add helper macro to get property from an identifier
    If we have a define for a PHANDLE this ends up having a DT identifier to
    another define.  We can use the DT_GET_PROP() macro to go from the
    PHANDLE define to the actualy node/property that the phandle points to.
    
    For example:
    
      DT_GET_PROP(DT_INST_0_NXP_KINETIS_GPIO_PORT, LABEL)
    
    Will get the "LABEL" property define for the node referend in the
    DT_INST_0_NXP_KINETIS_GPIO_PORT define.
    
    We can also use this with other identifier macros in the future.  For
    example:
    
      DT_GET_PROP(DT_NODELABEL_GPIOA, LABEL)
    
    This will resolve to the actual "label" property value for the node
    referenced by the node label 'gpioa'.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- include/devicetree.h
+++ include/devicetree.h
@@ -8,6 +8,12 @@
 #ifndef DEVICETREE_H
 #define DEVICETREE_H
 
+/* concatenate the values of the arguments into one */
+#define _DT_GET_PROP(ident, prop) DT_ ## ident ## _ ## prop
+
+/* Given an identifier and a property name get back the actual value */
+#define DT_GET_PROP(ident, prop) _DT_GET_PROP(ident, prop)
+
 #include <devicetree_unfixed.h>
 #include <devicetree_fixups.h>
 
