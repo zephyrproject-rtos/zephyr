dd0c8a784ce7cd7a03e3989f192e79818917dca3
Thomas Ebert Hansen <thoh@oticon.com>
2020-02-26 17:13:47 +0100

    Bluetooth: controller: Add memory release
    Tx/Ntf buffers needs to be given back to the LLCP system when
    done processing.
    
    Signed-off-by: Thomas Ebert Hansen <thoh@oticon.com>

--- subsys/bluetooth/controller/ll_sw/ull_llcp.h
+++ subsys/bluetooth/controller/ll_sw/ull_llcp.h
@@ -57,6 +57,16 @@ void ull_cp_conn_init(struct ull_cp_conn *conn);
 void ull_cp_state_set(struct ull_cp_conn *conn, u8_t state);
 
 /**
+ *
+ */
+void ull_cp_release_tx(struct node_tx *tx);
+
+/**
+ *
+ */
+void ull_cp_release_ntf(struct node_rx_pdu *ntf);
+
+/**
  * @breif Run pending LL Control Procedures.
  */
 void ull_cp_run(struct ull_cp_conn *conn);
