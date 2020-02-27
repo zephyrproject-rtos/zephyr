4c8ee4241ec7d81f5abbea98956723b4bc347525
Daniel Leung <daniel.leung@intel.com>
2020-02-26 18:11:44 -0800

    ipm: add driver for the CAVS DSP Intra-DSP Communication (IDC)
    This adds a rather primitive driver for use with the Intra-DSP
    Communication (IDC) on the DSP on certain Intel SoCs. The IDC
    generates interrupts from one core to another by writing to
    certain registers. This is also being utilized as
    the scheduler IPI since it can interrupt other cores.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

---
+++ drivers/ipm/ipm_cavs_idc_priv.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_IPM_IPM_CAVS_IDC_PRIV_H_
+#define ZEPHYR_DRIVERS_IPM_IPM_CAVS_IDC_PRIV_H_
+
+#define IDC_REG_SIZE		DT_INST_0_INTEL_CAVS_IDC_SIZE
+#define IDC_REG_BASE(x)		\
+	(DT_INST_0_INTEL_CAVS_IDC_BASE_ADDRESS + x * IDC_REG_SIZE)
+
+#define IDC_CPU_OFFSET		0x10
+
+#define REG_IDCTFC(x)		(0x0 + x * IDC_CPU_OFFSET)
+#define REG_IDCTEFC(x)		(0x4 + x * IDC_CPU_OFFSET)
+#define REG_IDCITC(x)		(0x8 + x * IDC_CPU_OFFSET)
+#define REG_IDCIETC(x)		(0xc + x * IDC_CPU_OFFSET)
+#define REG_IDCCTL		0x50
+
+#define REG_IDCTFC_BUSY		(1 << 31)
+#define REG_IDCTFC_MSG_MASK	0x7FFFFFFF
+
+#define REG_IDCTEFC_MSG_MASK	0x3FFFFFFF
+
+#define REG_IDCITC_BUSY		(1 << 31)
+#define REG_IDCITC_MSG_MASK	0x7FFFFFFF
+
+#define REG_IDCIETC_DONE	(1 << 30)
+#define REG_IDCIETC_MSG_MASK	0x3FFFFFFF
+
+#define REG_IDCCTL_IDCIDIE(x)	(0x100 << (x))
+#define REG_IDCCTL_IDCTBIE(x)	(0x1 << (x))
+
+static inline u32_t idc_read(u32_t reg, u32_t core_id)
+{
+	return sys_read32(IDC_REG_BASE(core_id) + reg);
+}
+
+static inline void idc_write(u32_t reg, u32_t core_id, u32_t val)
+{
+	sys_write32(val, IDC_REG_BASE(core_id) + reg);
+}
+
+#endif /* ZEPHYR_DRIVERS_IPM_IPM_CAVS_IDC_PRIV_H_ */
