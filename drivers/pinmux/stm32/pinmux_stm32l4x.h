84817f4906bc2747ea756b327d3cc852baf36d9b
Kwon Tae-young <tykwon@m2i.co.kr>
2020-02-27 16:33:44 +0900

    drivers: pinmux: stm32l4: I2C_1, CAN pinmuxes added
    Add pin configuration for I2C_1 and CAN.
    
    Signed-off-by: Kwon Tae-young <tykwon@m2i.co.kr>

--- drivers/pinmux/stm32/pinmux_stm32l4x.h
+++ drivers/pinmux/stm32/pinmux_stm32l4x.h
@@ -74,6 +74,11 @@
 	(STM32_PINMUX_ALT_FUNC_4 | STM32_OPENDRAIN_PULLUP)
 #define STM32L4X_PINMUX_FUNC_PA9_USART1_TX                                     \
 	(STM32_PINMUX_ALT_FUNC_7 | STM32_PUSHPULL_PULLUP)
+#if defined(CONFIG_SOC_STM32L432XX) || defined(CONFIG_SOC_STM32L433XX) ||      \
+	defined(CONFIG_SOC_STM32L452XX)
+#define STM32L4X_PINMUX_FUNC_PA9_I2C1_SCL                                      \
+	(STM32_PINMUX_ALT_FUNC_4 | STM32_OPENDRAIN_PULLUP)
+#endif
 
 #define STM32L4X_PINMUX_FUNC_PA10_I2C1_SDA                                     \
 	(STM32_PINMUX_ALT_FUNC_4 | STM32_OPENDRAIN_PULLUP)
@@ -81,6 +86,11 @@
 	(STM32_PINMUX_ALT_FUNC_7 | STM32_PUPDR_NO_PULL)
 #define STM32L4X_PINMUX_FUNC_PA10_OTG_FS_ID                                    \
 	(STM32_PINMUX_ALT_FUNC_10 | STM32_PUSHPULL_PULLUP)
+#if defined(CONFIG_SOC_STM32L432XX) || defined(CONFIG_SOC_STM32L433XX) ||      \
+	defined(CONFIG_SOC_STM32L452XX)
+#define STM32L4X_PINMUX_FUNC_PA10_I2C1_SDA                                     \
+	(STM32_PINMUX_ALT_FUNC_4 | STM32_OPENDRAIN_PULLUP)
+#endif
 
 #define STM32L4X_PINMUX_FUNC_PA11_USART1_CTS                                   \
 	(STM32_PINMUX_ALT_FUNC_7 | STM32_OPENDRAIN_PULLUP)
@@ -157,9 +167,17 @@
 
 #define STM32L4X_PINMUX_FUNC_PB8_I2C1_SCL                                      \
 	(STM32_PINMUX_ALT_FUNC_4 | STM32_OPENDRAIN_PULLUP)
+#ifndef CONFIG_SOC_STM32L432XX
+#define STM32L4X_PINMUX_FUNC_PB8_CAN_RX                                        \
+	(STM32_PINMUX_ALT_FUNC_9 | STM32_PUSHPULL_NOPULL)
+#endif
 
 #define STM32L4X_PINMUX_FUNC_PB9_I2C1_SDA                                      \
 	(STM32_PINMUX_ALT_FUNC_4 | STM32_OPENDRAIN_PULLUP)
+#ifndef CONFIG_SOC_STM32L432XX
+#define STM32L4X_PINMUX_FUNC_PB9_CAN_TX                                        \
+	(STM32_PINMUX_ALT_FUNC_9 | STM32_PUSHPULL_NOPULL)
+#endif
 
 #define STM32L4X_PINMUX_FUNC_PB10_SPI2_SCK                                     \
 	(STM32_PINMUX_ALT_FUNC_5 | STM32_PUPDR_PULL_DOWN | \
@@ -255,6 +273,16 @@
 	 STM32_OSPEEDR_VERY_HIGH_SPEED)
 
 /* Port D */
+#ifndef CONFIG_SOC_STM32L432XX
+#define STM32L4X_PINMUX_FUNC_PD0_CAN_RX                                        \
+	(STM32_PINMUX_ALT_FUNC_9 | STM32_PUSHPULL_NOPULL)
+#endif
+
+#ifndef CONFIG_SOC_STM32L432XX
+#define STM32L4X_PINMUX_FUNC_PD1_CAN_TX                                        \
+	(STM32_PINMUX_ALT_FUNC_9 | STM32_PUSHPULL_NOPULL)
+#endif
+
 #define STM32L4X_PINMUX_FUNC_PD2_USART3_RTS                                    \
 	(STM32_PINMUX_ALT_FUNC_7 | STM32_OPENDRAIN_PULLUP)
 
