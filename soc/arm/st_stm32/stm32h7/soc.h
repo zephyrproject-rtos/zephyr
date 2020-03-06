b8145dea847a169784c7b541bd570675585fe456
Gerard Marull-Paretas <gerard@teslabs.com>
2020-03-06 05:41:11 -0800

    drivers: counter: stm32: enable support for H7 series
    Enable counter driver support for H7 series. Tested with H743ZI MCU
    using samples/drivers/counter/alarm.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- soc/arm/st_stm32/stm32h7/soc.h
+++ soc/arm/st_stm32/stm32h7/soc.h
@@ -64,6 +64,12 @@
 #include <stm32h7xx_ll_utils.h>
 #endif
 
+#ifdef CONFIG_COUNTER_RTC_STM32
+#include <stm32h7xx_ll_rtc.h>
+#include <stm32h7xx_ll_exti.h>
+#include <stm32h7xx_ll_pwr.h>
+#endif /* CONFIG_COUNTER_RTC_STM32 */
+
 #endif /* !_ASMLANGUAGE */
 
 #endif /* _STM32F7_SOC_H7_ */
