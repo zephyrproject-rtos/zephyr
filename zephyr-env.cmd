439925acb3aef450ae1555d087fabcb88a80dfa2
Bilal Wasim <bilal_wasim@mentor.com>
2020-03-13 23:18:29 +0500

    Zephyr: Reverting bad change
    Signed-off-by: Bilal Wasim <bilalwasim676@gmail.com>

--- zephyr-env.cmd
+++ zephyr-env.cmd
@@ -4,6 +4,3 @@ set ZEPHYR_BASE=%~dp0
 if exist "%userprofile%\zephyrrc.cmd" (
 	call "%userprofile%\zephyrrc.cmd"
 )
-
-set ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb
-set GNUARMEMB_TOOLCHAIN_PATH=D:\OSS_Work\zephyr-rtos\tools\
