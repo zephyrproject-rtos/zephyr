10bf1361b0271ead593cc7054dc4b1f1ee26e3cc
Bilal Wasim <bilal_wasim@mentor.com>
2020-03-13 22:09:14 +0500

    # This is a combination of 2 commits. # This is the 1st commit message:
    boards/arm/nrf9160_innblue22: Adding support for nRF9160 InnBlue22 Board.
    
    Adding support for nRF9160 based InnBlue board V2.2. Supports both
    Secure and Non-Secure configurations along with various sensors
    (bq27421 / lis2dh12 / hts221) and devices(i2c / spi / adc / pwm).
    
    Signed-off-by: Bilal Wasim <bilalwasim676@gmail.com>
    
    # The commit message #2 will be skipped:
    
    # boards/arm/nrf9160_innblue22: Coding Standards compliance
    #
    # Went through the Zephyr Coding standards and ran scripts to
    # ensure that all files are compliant with the Zephyr standards.
    #
    # Signed-off-by: Bilal Wasim <bilalwasim676@gmail.com>

---
+++ boards/arm/nrf9160_innblue22/innblue22_board_init.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2020 InnBlue
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+#include <drivers/gpio.h>
+
+#define VDD_5V0_PWR_CTRL_GPIO_PIN  21   /* ENABLE_5V0_BOOST  --> speed sensor */
+
+static int pwr_ctrl_init(struct device *dev)
+{
+	struct device *gpio;
+	int    err = -ENODEV;
+
+	/* Get handle of the GPIO device. */
+	gpio = device_get_binding(DT_GPIO_P0_DEV_NAME);
+
+	/* Valid handle? */
+	if (gpio != NULL) {
+
+		/* Configure this pin as output. */
+		err = gpio_pin_configure(gpio, VDD_5V0_PWR_CTRL_GPIO_PIN,
+							GPIO_OUTPUT_ACTIVE);
+		if (err == 0) {
+
+			/* Write "1" to this pin. */
+			err = gpio_pin_set(gpio, VDD_5V0_PWR_CTRL_GPIO_PIN, 1);
+		}
+
+		/* Wait for the rail to come up and stabilize. */
+		k_sleep(1);
+	}
+
+	/* Operation status? */
+	return (err);
+}
+
+DEVICE_INIT(vdd_5v0_pwr_ctrl_init, "", pwr_ctrl_init, NULL, NULL,
+			POST_KERNEL, 70);
