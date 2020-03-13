abccb64a76a88e6cbedbadea6bb8b1d9fe6ca7cc
Bilal Wasim <bilal_wasim@mentor.com>
2020-03-13 23:10:34 +0500

    boards/arm/nrf9160_innblue22: Coding Standards compliance
    Went through the Zephyr Coding standards and ran scripts to
    ensure that all files are compliant with the Zephyr standards.
    
    Signed-off-by: Bilal Wasim <bilalwasim676@gmail.com>

--- boards/arm/nrf9160_innblue22/innblue22_board_init.c
+++ boards/arm/nrf9160_innblue22/innblue22_board_init.c
@@ -11,29 +11,31 @@
 
 static int pwr_ctrl_init(struct device *dev)
 {
-    struct device *gpio;
-    int    err = -ENODEV;
+	struct device *gpio;
+	int    err = -ENODEV;
 
-    /* Get handle of the GPIO device. */
-    gpio = device_get_binding(DT_GPIO_P0_DEV_NAME);
+	/* Get handle of the GPIO device. */
+	gpio = device_get_binding(DT_GPIO_P0_DEV_NAME);
 
-    /* Valid handle? */
-    if (gpio != NULL) {
+	/* Valid handle? */
+	if (gpio != NULL) {
 
-        /* Configure this pin as output. */
-        if ((err = gpio_pin_configure(gpio, VDD_5V0_PWR_CTRL_GPIO_PIN, GPIO_OUTPUT_ACTIVE)) == 0) {
+		/* Configure this pin as output. */
+		err = gpio_pin_configure(gpio, VDD_5V0_PWR_CTRL_GPIO_PIN,
+							GPIO_OUTPUT_ACTIVE);
+		if (err == 0) {
 
-            /* Write "1" to this pin. */
-        	err = gpio_pin_set(gpio, VDD_5V0_PWR_CTRL_GPIO_PIN, 1);
-        }
+			/* Write "1" to this pin. */
+			err = gpio_pin_set(gpio, VDD_5V0_PWR_CTRL_GPIO_PIN, 1);
+		}
 
-        /* Wait for the rail to come up and stabilize. */
-        k_sleep(1);
-    }
+		/* Wait for the rail to come up and stabilize. */
+		k_sleep(1);
+	}
 
-    /* Operation status? */
-    return (err);
+	/* Operation status? */
+	return (err);
 }
 
 DEVICE_INIT(vdd_5v0_pwr_ctrl_init, "", pwr_ctrl_init, NULL, NULL,
-            POST_KERNEL, 70);
+			POST_KERNEL, 70);
