7d53737e3a3a3d6e6c38e701a4e9828a6b555fdb
Takumi Ando <takumi@t15.red>
2020-03-18 08:52:28 +0900

    drivers: adc: Add STM32L1X ADC support
    Add ADC driver support for STM32L1X SoC series.
    
    Signed-off-by: Takumi Ando <takumi@t15.red>

--- soc/arm/st_stm32/stm32l1/soc.h
+++ soc/arm/st_stm32/stm32l1/soc.h
@@ -51,6 +51,10 @@
 #include <stm32l1xx_ll_i2c.h>
 #endif
 
+#ifdef CONFIG_ADC_STM32
+#include <stm32l1xx_ll_adc.h>
+#endif
+
 #if defined(CONFIG_COUNTER_RTC_STM32)
 #include <stm32l1xx_ll_rtc.h>
 #include <stm32l1xx_ll_exti.h>
