acb2799850674e53ddc61d7166ae446527759af4
Piotr Mienkowski <piotr.mienkowski@gmail.com>
2020-03-19 07:55:34 +0100

    dts: stm32: Introduce linux like pinctrl DT bindings
    Introduce Linux like dts bindings for stm32 devices.
    
    Signed-off-by: Piotr Mienkowski <piotr.mienkowski@gmail.com>

---
+++ include/dt-bindings/pinctrl/stm32l4-pins.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2017 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef INCLUDE_DT_BINDINGS_PINCTRL_STM32L4_PINCTRL_H_
+#define INCLUDE_DT_BINDINGS_PINCTRL_STM32L4_PINCTRL_H_
+
+#include "stm32-pinctrl.h"
+
+#define PIN_PA3_UART2_RX            STM32_PINMUX('A', 3, AF7)
+#define PIN_PA2_UART2_TX            STM32_PINMUX('A', 2, AF7)
+
+
+#endif /* INCLUDE_DT_BINDINGS_PINCTRL_STM32L4_PINCTRL_H_ */
