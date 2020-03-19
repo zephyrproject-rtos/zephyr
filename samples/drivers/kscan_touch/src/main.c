80967f6cf730da9737292f4fb4cf6ab31bc45a12
Mark Olsson <mark@markolsson.se>
2020-03-19 21:55:38 +0100

    samples: drivers: kscan: Add sample for touch panel drivers
    Simple sample to test touch panel drivers.
    Outputs a message to the console on every touch event.
    
    Signed-off-by: Mark Olsson <mark@markolsson.se>

---
+++ samples/drivers/kscan_touch/src/main.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2020 Mark Olsson <mark@markolsson.se>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <sys/printk.h>
+#include <drivers/gpio.h>
+#include <soc.h>
+#include <drivers/kscan.h>
+
+#define LOG_LEVEL LOG_LEVEL_DBG
+#include <logging/log.h>
+
+LOG_MODULE_REGISTER(main);
+
+struct device *kscan_dev;
+
+#define TOUCH_CONTROLLER_LABEL DT_LABEL(DT_ALIAS(kscan0))
+
+static void k_callback(struct device *dev, u32_t row, u32_t col, bool pressed)
+{
+	ARG_UNUSED(dev);
+	if (pressed) {
+		printk("row = %u col = %u\n", row, col);
+	}
+}
+
+void main(void)
+{
+	printk("Kscan touch panel sample application\n");
+
+	kscan_dev = device_get_binding(TOUCH_CONTROLLER_LABEL);
+	if (!kscan_dev) {
+		LOG_ERR("kscan device %s not found", TOUCH_CONTROLLER_LABEL);
+		return;
+	}
+
+	kscan_config(kscan_dev, k_callback);
+	kscan_enable_callback(kscan_dev);
+}
