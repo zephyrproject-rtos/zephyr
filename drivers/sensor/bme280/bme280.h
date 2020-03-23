0bcdcd2261b3c74a9608645d5465a8af77ea87c3
Christian Hirsch <christian.hirsch@tuwien.ac.at>
2020-03-23 11:51:57 +0100

    drivers: sensor: BME280: add forced sampling mode
    add the forced sampling mode, to take just a single
    measurement when fatching a sample
    
    Signed-off-by: Christian Hirsch <christian.hirsch@tuwien.ac.at>

--- drivers/sensor/bme280/bme280.h
+++ drivers/sensor/bme280/bme280.h
@@ -19,13 +19,21 @@
 #define BME280_REG_CONFIG               0xF5
 #define BME280_REG_CTRL_MEAS            0xF4
 #define BME280_REG_CTRL_HUM             0xF2
+#define BME280_REG_STATUS               0xF3
 
 #define BMP280_CHIP_ID_SAMPLE_1         0x56
 #define BMP280_CHIP_ID_SAMPLE_2         0x57
 #define BMP280_CHIP_ID_MP               0x58
 #define BME280_CHIP_ID                  0x60
+#define BME280_MODE_FORCED              0x01
 #define BME280_MODE_NORMAL              0x03
 #define BME280_SPI_3W_DISABLE           0x00
+
+#if defined CONFIG_BME280_MODE_NORMAL
+#define BME280_MODE BME280_MODE_NORMAL
+#elif defined CONFIG_BME280_MODE_FORCED
+#define BME280_MODE BME280_MODE_FORCED
+#endif
 
 #if defined CONFIG_BME280_TEMP_OVER_1X
 #define BME280_TEMP_OVER                (1 << 5)
@@ -95,7 +103,7 @@
 
 #define BME280_CTRL_MEAS_VAL            (BME280_PRESS_OVER | \
 					 BME280_TEMP_OVER |  \
-					 BME280_MODE_NORMAL)
+					 BME280_MODE)
 #define BME280_CONFIG_VAL               (BME280_STANDBY | \
 					 BME280_FILTER |  \
 					 BME280_SPI_3W_DISABLE)
