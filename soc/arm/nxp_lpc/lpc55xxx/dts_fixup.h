6143a628dd90266c5bd1ee5bc60cd246207b408a
Kumar Gala <kumar.gala@linaro.org>
2020-03-24 16:43:12 -0500

    soc: nxp_lpc: Convert to new DT_INST macros
    Convert older DT_INST_ macro use the new include/devicetree.h
    DT_INST macro APIs.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- soc/arm/nxp_lpc/lpc55xxx/dts_fixup.h
+++ soc/arm/nxp_lpc/lpc55xxx/dts_fixup.h
@@ -7,8 +7,8 @@
 /* SoC level DTS fixup file */
 
 #define DT_NUM_IRQ_PRIO_BITS DT_ARM_V8M_NVIC_E000E100_ARM_NUM_IRQ_PRIORITY_BITS
-#define DT_NUM_MPU_REGIONS   DT_INST_0_ARM_ARMV8M_MPU_ARM_NUM_MPU_REGIONS
+#define DT_NUM_MPU_REGIONS   DT_PROP(DT_INST(0, arm_armv8m_mpu), arm_num_mpu_regions)
 
-#define DT_FLASH_DEV_NAME    DT_INST_0_NXP_LPC_IAP_LABEL
+#define DT_FLASH_DEV_NAME    DT_LABEL(DT_INST(0, nxp_lpc_iap))
 
 /* End of SoC Level DTS fixup file */
