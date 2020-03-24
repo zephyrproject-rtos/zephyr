a92d61a316ddc3079ac7dac9404af965d81f4e5d
Kumar Gala <kumar.gala@linaro.org>
2020-03-24 16:45:26 -0500

    soc: litex: Convert to new DT_INST macros
    Convert older DT_INST_ macro use the new include/devicetree.h
    DT_INST macro APIs.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- soc/riscv/litex-vexriscv/soc.h
+++ soc/riscv/litex-vexriscv/soc.h
@@ -11,8 +11,8 @@
 #include <devicetree.h>
 
 /* lib-c hooks required RAM defined variables */
-#define RISCV_RAM_BASE              DT_INST_0_MMIO_SRAM_BASE_ADDRESS
-#define RISCV_RAM_SIZE              DT_INST_0_MMIO_SRAM_SIZE
+#define RISCV_RAM_BASE              DT_REG_ADDR(DT_INST(0, mmio_sram))
+#define RISCV_RAM_SIZE              DT_REG_SIZE(DT_INST(0, mmio_sram))
 
 #ifndef _ASMLANGUAGE
 /* CSR access helpers */
