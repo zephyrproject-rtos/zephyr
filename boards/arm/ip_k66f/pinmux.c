6cc1e1569da417d88b399d8594a07ed77b89303d
Lukasz Majewski <lukma@denx.de>
2020-03-25 15:18:25 +0100

    ip_k66f: pinmux: eth: Add ETH support
    Modify pinmux to properly configure ETH pins.
    
    Signed-off-by: Lukasz Majewski <lukma@denx.de>

--- boards/arm/ip_k66f/pinmux.c
+++ boards/arm/ip_k66f/pinmux.c
@@ -21,6 +21,17 @@ static int ip_k66f_pinmux_init(struct device *dev)
 	pinmux_pin_set(porta, 8, PORT_PCR_MUX(kPORT_MuxAsGpio));
 	pinmux_pin_set(porta, 10, PORT_PCR_MUX(kPORT_MuxAsGpio));
 
+#if DT_HAS_NODE(DT_NODELABEL(enet))
+	pinmux_pin_set(porta, 12, PORT_PCR_MUX(kPORT_MuxAlt4));/* RMII_RXD1 */
+	pinmux_pin_set(porta, 13, PORT_PCR_MUX(kPORT_MuxAlt4));/* RMII_RXD0 */
+	pinmux_pin_set(porta, 14, PORT_PCR_MUX(kPORT_MuxAlt4));/* RMII_CRS_DV */
+	pinmux_pin_set(porta, 15, PORT_PCR_MUX(kPORT_MuxAlt4));/* RMII_RX_EN */
+	pinmux_pin_set(porta, 16, PORT_PCR_MUX(kPORT_MuxAlt4));/* RMII_TXD0 */
+	pinmux_pin_set(porta, 17, PORT_PCR_MUX(kPORT_MuxAlt4));/* RMII_TXD1 */
+	pinmux_pin_set(porta, 24, PORT_PCR_MUX(kPORT_MuxAsGpio));/* !ETH_RST */
+	pinmux_pin_set(porta, 25, PORT_PCR_MUX(kPORT_MuxAsGpio));/* !ETH_PME */
+	pinmux_pin_set(porta, 26, PORT_PCR_MUX(kPORT_MuxAsGpio));/* !ETH_INT */
+#endif
 	return 0;
 }
 
