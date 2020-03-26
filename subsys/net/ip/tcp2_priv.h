711a31ed7393128692d0073a859667a677b77a06
Oleg Zhurakivskyy <oleg.zhurakivskyy@intel.com>
2020-03-26 11:24:43 +0200

    net: tcp2: Add a TIME_WAIT timer and state
    In order to support TIME_WAIT state during the TCP connection
    termination, add a TIME_WAIT timer and the corresponding state.
    
    Signed-off-by: Oleg Zhurakivskyy <oleg.zhurakivskyy@intel.com>

--- subsys/net/ip/tcp2_priv.h
+++ subsys/net/ip/tcp2_priv.h
@@ -135,6 +135,7 @@ struct tcp { /* TCP connection */
 	sys_slist_t send_queue;
 	bool in_retransmission;
 	size_t send_retries;
+	struct k_delayed_work timewait_timer;
 	struct net_if *iface;
 	net_tcp_accept_cb_t accept_cb;
 	atomic_t ref_count;
