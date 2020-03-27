32d29309bd8a6ff538f2a42b46c0361710e7c26a
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2020-03-27 12:28:55 +0200

    boards: up_squared_adsp: Change default flasher
    Change default flasher to misc-flasher, allowing to pass
    custom flash command.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- boards/xtensa/up_squared_adsp/board.cmake
+++ boards/xtensa/up_squared_adsp/board.cmake
@@ -1,11 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_set_flasher_ifnset(intel_s1000)
-board_set_debugger_ifnset(intel_s1000)
-
-board_finalize_runner_args(intel_s1000
-  "--xt-ocd-dir=/opt/tensilica/xocd-12.0.4/xt-ocd"
-  "--ocd-topology=topology_dsp0_flyswatter2.xml"
-  "--ocd-jtag-instr=dsp0_gdb.txt"
-  "--gdb-flash-file=load_elf.txt"
-)
+board_set_flasher_ifnset(misc-flasher)
+board_finalize_runner_args(misc-flasher)
