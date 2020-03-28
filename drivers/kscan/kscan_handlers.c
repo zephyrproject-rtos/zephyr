e5ebd62288cebee72600bcc5658d2704336456f0
Mark Olsson <mark@markolsson.se>
2020-03-28 18:01:12 +0100

    tests: drivers: fixes for failing tests
    Tests were failing as they were looking for an alias which didn't exist
    Also, because some code in kscan_handlers.c wasn't compilable
    
    Signed-off-by: Mark Olsson <mark@markolsson.se>

--- drivers/kscan/kscan_handlers.c
+++ drivers/kscan/kscan_handlers.c
@@ -11,13 +11,13 @@ static inline int z_vrfy_kscan_config(struct device *dev,
 				      kscan_callback_t callback_isr)
 {
 	Z_OOPS(Z_SYSCALL_DRIVER_KSCAN(dev, config));
-	Z_OOPS(Z_SYSCALL_VERIFY_MSG(callback == 0,
+	Z_OOPS(Z_SYSCALL_VERIFY_MSG(callback_isr == 0,
 				    "callback cannot be set from user mode"));
-	return z_impl_kscan_config((struct device *)dev, callback);
+	return z_impl_kscan_config((struct device *)dev, callback_isr);
 }
 #include <syscalls/kscan_config_mrsh.c>
 
-static inline int z_vrfy_kscan_disable_callback(struct device *dev);
+static inline int z_vrfy_kscan_disable_callback(struct device *dev)
 {
 	Z_OOPS(Z_SYSCALL_DRIVER_KSCAN(dev, disable_callback));
 
@@ -25,7 +25,7 @@ static inline int z_vrfy_kscan_disable_callback(struct device *dev);
 }
 #include <syscalls/kscan_disable_callback_mrsh.c>
 
-static int z_vrfy_kscan_enable_callback(struct device *dev);
+static int z_vrfy_kscan_enable_callback(struct device *dev)
 {
 	Z_OOPS(Z_SYSCALL_DRIVER_KSCAN(dev, enable_callback));
 
