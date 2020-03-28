65d8cda2f9865c5e6b9351e5059632f383ac1815
Gerard Marull-Paretas <gerard@teslabs.com>
2020-03-28 19:58:23 +0100

    soc: stm32: include H7 HAL headers based on usage
    As of today STM32 soc.h file includes HAL header files based on driver
    configuration instead of HAL usage. The proposed changes do the
    opposite, that is:
    
    - Driver selects which HAL components it needs (USE_STM32_LL_XXX or
      USE_STM32_HAL_XX)
    - soc.h file includes HAL headers based on selected configuration
    
    Advantages:
    
    - It is more clear what a driver needs/uses by looking at Kconfig
    - If an application uses HAL directly (e.g. because it has a custom
      driver) it just needs to select USE_STM32_XXX and the right headers
      will be included.
    
    The changes have been only applied to H7 series for demonstration
    purposes.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- soc/arm/st_stm32/stm32h7/soc.h
+++ soc/arm/st_stm32/stm32h7/soc.h
@@ -1,79 +1,138 @@
 /*
  * Copyright (c) 2019 Linaro Limited
+ * Copyright (c) 2020 Teslabs Engineering S.L.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef _STM32F7_SOC_H_
-#define _STM32F7_SOC_H_
+#ifndef _STM32H7_SOC_H_
+#define _STM32H7_SOC_H_
 
 #include <sys/util.h>
 
 #ifndef _ASMLANGUAGE
 
 #include <autoconf.h>
-#include <stm32h7xx.h>
-
-/* Add include for DTS generated information */
 #include <devicetree.h>
 
 #ifdef CONFIG_STM32H7_DUAL_CORE
-
 #define LL_HSEM_ID_0   (0U) /* HW semaphore 0 */
 #define LL_HSEM_MASK_0 (1 << LL_HSEM_ID_0)
 #define LL_HSEM_ID_1   (1U) /* HW semaphore 1 */
 #define LL_HSEM_MASK_1 (1 << LL_HSEM_ID_1)
-
-#include <stm32h7xx_ll_hsem.h>
-
-#ifdef CONFIG_CPU_CORTEX_M4
-
-#include <stm32h7xx_ll_bus.h>
-#include <stm32h7xx_ll_pwr.h>
-#include <stm32h7xx_ll_cortex.h>
-
-#endif /* CONFIG_CPU_CORTEX_M4 */
-
 #endif /* CONFIG_STM32H7_DUAL_CORE */
 
-#ifdef CONFIG_CLOCK_CONTROL_STM32_CUBE
+#include <stm32h7xx.h>
+
+#ifdef CONFIG_USE_STM32_LL_ADC
+#include <stm32h7xx_ll_adc.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_BDMA
+#include <stm32h7xx_ll_bdma.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_BUS
 #include <stm32h7xx_ll_bus.h>
-#include <stm32h7xx_ll_rcc.h>
-#include <stm32h7xx_ll_pwr.h>
-#include <stm32h7xx_ll_system.h>
-#endif /* CONFIG_CLOCK_CONTROL_STM32_CUBE */
-
-#ifdef CONFIG_EXTI_STM32
+#endif
+#ifdef CONFIG_USE_STM32_LL_COMP
+#include <stm32h7xx_ll_comp.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_CORTEX
+#include <stm32h7xx_ll_cortex.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_CRC
+#include <stm32h7xx_ll_crc.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_CRS
+#include <stm32h7xx_ll_crs.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_DAC
+#include <stm32h7xx_ll_dac.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_DELAYBLOCK
+#include <stm32h7xx_ll_delayblock.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_DMA
+#include <stm32h7xx_ll_dma.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_DMA2D
+#include <stm32h7xx_ll_dma2d.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_DMAMUX
+#include <stm32h7xx_ll_dmamux.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_EXTI
 #include <stm32h7xx_ll_exti.h>
-#endif /* CONFIG_EXTI_STM32 */
-
-#ifdef CONFIG_GPIO_STM32
+#endif
+#ifdef CONFIG_USE_STM32_LL_FMC
+#include <stm32h7xx_ll_fmc.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_GPIO
 #include <stm32h7xx_ll_gpio.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_HRTIM
+#include <stm32h7xx_ll_hrtim.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_HSEM
+#include <stm32h7xx_ll_hsem.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_I2C
+#include <stm32h7xx_ll_i2c.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_IWDG
+#include <stm32h7xx_ll_iwdg.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_LPTIM
+#include <stm32h7xx_ll_lptim.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_LPUART
+#include <stm32h7xx_ll_lpuart.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_MDMA
+#include <stm32h7xx_ll_mdma.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_OPAMP
+#include <stm32h7xx_ll_opamp.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_PWR
+#include <stm32h7xx_ll_pwr.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_RCC
+#include <stm32h7xx_ll_rcc.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_RNG
+#include <stm32h7xx_ll_rng.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_RTC
+#include <stm32h7xx_ll_rtc.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_SDMMC
+#include <stm32h7xx_ll_sdmmc.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_SPI
+#include <stm32h7xx_ll_spi.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_SWPMI
+#include <stm32h7xx_ll_swpmi.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_SYSTEM
 #include <stm32h7xx_ll_system.h>
-#endif /* CONFIG_GPIO_STM32 */
-
-#ifdef CONFIG_WWDG_STM32
+#endif
+#ifdef CONFIG_USE_STM32_LL_TIM
+#include <stm32h7xx_ll_tim.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_USART
+#include <stm32h7xx_ll_usart.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_USB
+#include <stm32h7xx_ll_usb.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_UTILS
+#include <stm32h7xx_ll_utils.h>
+#endif
+#ifdef CONFIG_USE_STM32_LL_WWDG
 #include <stm32h7xx_ll_wwdg.h>
 #endif
 
-#ifdef CONFIG_SERIAL_HAS_DRIVER
-#include <stm32h7xx_ll_usart.h>
-#endif /* CONFIG_SERIAL_HAS_DRIVER */
-
-#ifdef CONFIG_HWINFO_STM32
-#include <stm32h7xx_ll_utils.h>
-#endif
-
-#ifdef CONFIG_COUNTER_RTC_STM32
-#include <stm32h7xx_ll_rtc.h>
-#include <stm32h7xx_ll_exti.h>
-#include <stm32h7xx_ll_pwr.h>
-#endif /* CONFIG_COUNTER_RTC_STM32 */
-
-#ifdef CONFIG_I2C_STM32
-#include <stm32h7xx_ll_i2c.h>
-#endif /* CONFIG_I2C_STM32 */
-
 #endif /* !_ASMLANGUAGE */
 
-#endif /* _STM32F7_SOC_H7_ */
+#endif /* _STM32H7_SOC_H_ */
