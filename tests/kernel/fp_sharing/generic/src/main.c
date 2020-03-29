02fdfccc1ea15f190291a65d737ea2620ae5c45f
Stephanos Ioannidis <root@stephanos.io>
2020-03-29 22:46:32 +0900

    tests: kernel: fp_sharing: Use ztest
    This commit converts the `fp_sharing` tests to use the ztest framework.
    
    In addition, this commit also introduces a behavioural change to run
    the `pi` unit test separately from the `load_store` unit test, in order
    to allow more manageable and diagnosable test execution.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- tests/kernel/fp_sharing/generic/src/main.c
+++ tests/kernel/fp_sharing/generic/src/main.c
@@ -1,10 +1,13 @@
 /*
  * Copyright (c) 2011-2014 Wind River Systems, Inc.
+ * Copyright (c) 2020 Stephanos Ioannidis <root@stephanos.io>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr.h>
+#include <ztest.h>
+
+#include "test_common.h"
 
 #ifndef CONFIG_FLOAT
 #error Rebuild with the FLOAT config option enabled
@@ -14,19 +17,26 @@
 #error Rebuild with the FP_SHARING config option enabled
 #endif
 
-#if defined(CONFIG_X86)
-#ifndef CONFIG_SSE
+#if defined(CONFIG_X86) && !defined(CONFIG_SSE)
 #error Rebuild with the SSE config option enabled
 #endif
-#endif /* CONFIG_X86 */
 
-void main(void)
+extern void test_load_store(void);
+extern void test_pi(void);
+
+void test_main(void)
 {
-	/* This very old test didn't have a main() function, and would
-	 * dump gcov data immediately. Sleep forever, we'll invoke
-	 * gcov manually later when the test completes.
+	/*
+	 * Enable round robin scheduling to allow both the low priority pi
+	 * computation and load/store tasks to execute. The high priority pi
+	 * computation and load/store tasks will preempt the low priority tasks
+	 * periodically.
 	 */
-	while (true) {
-		k_sleep(K_MSEC(1000));
-	}
+	k_sched_time_slice_set(10, THREAD_LOW_PRIORITY);
+
+	/* Run the testsuite */
+	ztest_test_suite(fp_sharing,
+			 ztest_unit_test(test_load_store),
+			 ztest_unit_test(test_pi));
+	ztest_run_test_suite(fp_sharing);
 }
