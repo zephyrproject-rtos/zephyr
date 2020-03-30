7e10e6dd3d21741a80f87b625419a1942eec6dfa
Kumar Gala <kumar.gala@linaro.org>
2020-03-30 13:33:19 -0500

    samples: ipc: Convert to new DT_INST macros
    Convert older DT_INST_ macro use the new include/devicetree.h
    DT_INST macro APIs.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- samples/subsys/ipc/ipm_mcux/src/main_master.c
+++ samples/subsys/ipc/ipm_mcux/src/main_master.c
@@ -33,7 +33,7 @@ void main(void)
 	printk("Hello World from MASTER! %s\n", CONFIG_ARCH);
 
 	/* Get IPM device handle */
-	ipm = device_get_binding(DT_INST_0_NXP_LPC_MAILBOX_LABEL);
+	ipm = device_get_binding(DT_LABEL(DT_INST(0, nxp_lpc_mailbox)));
 	if (!ipm) {
 		printk("Could not get IPM device handle!\n");
 		while (1) {
