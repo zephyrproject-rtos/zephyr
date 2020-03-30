3350e27cf843564bd8750cfccb4a88c91156def6
Kumar Gala <kumar.gala@linaro.org>
2020-03-30 17:00:54 -0500

    drivers: audio: mpxxdtyy: Conver to use DT_ANY_INST_ON_BUS macro
    Convert driver to use new DT_ANY_INST_ON_BUS(i2s) away from
    DT_ST_MPXXDTYY_BUS_I2S.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- drivers/audio/mpxxdtyy-i2s.c
+++ drivers/audio/mpxxdtyy-i2s.c
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#define DT_DRV_COMPAT st_mpxxdtyy
+
 #include "mpxxdtyy.h"
 #include <drivers/i2s.h>
 
@@ -11,7 +13,7 @@
 #include <logging/log.h>
 LOG_MODULE_DECLARE(mpxxdtyy);
 
-#ifdef DT_ST_MPXXDTYY_BUS_I2S
+#if DT_ANY_INST_ON_BUS(i2s)
 
 #define NUM_RX_BLOCKS			4
 #define PDM_BLOCK_MAX_SIZE_BYTES	512
@@ -138,4 +140,4 @@ int mpxxdtyy_i2s_configure(struct device *dev, struct dmic_cfg *cfg)
 	data->state = DMIC_STATE_CONFIGURED;
 	return 0;
 }
-#endif /* DT_ST_MPXXDTYY_BUS_I2S */
+#endif /* DT_ANY_INST_ON_BUS(i2s) */
