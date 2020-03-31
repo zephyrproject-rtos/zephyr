c22652123f9f474844b2cd548203f2f876fec057
Stephanos Ioannidis <root@stephanos.io>
2020-03-31 10:06:32 +0900

    cmake: gcc: target_arm: Force FP hard ABI for DCLS
    When Dual-redundant Core Lock-step (DCLS) topology is used, the VFP
    registers across the two redundant cores must be manually initialised
    and synchronised, and this requires the `-mfloat-abi=hard` option to
    be specified.
    
    This commit forces the use of FP "hard" ABI on the VFP-equipped cores
    that are configured in DCLS topology.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- cmake/compiler/gcc/target_arm.cmake
+++ cmake/compiler/gcc/target_arm.cmake
@@ -14,12 +14,19 @@ if(CONFIG_FPU)
   list(APPEND TOOLCHAIN_C_FLAGS   -mfpu=${GCC_M_FPU})
   list(APPEND TOOLCHAIN_LD_FLAGS  -mfpu=${GCC_M_FPU})
 
-  if    (CONFIG_FP_SOFTABI)
-    list(APPEND TOOLCHAIN_C_FLAGS   -mfloat-abi=softfp)
-    list(APPEND TOOLCHAIN_LD_FLAGS  -mfloat-abi=softfp)
-  elseif(CONFIG_FP_HARDABI)
+  if(CONFIG_CPU_HAS_DCLS AND NOT CONFIG_FP_HARDABI)
+    # If the processor is equipped with VFP and configured in DCLS topology,
+    # the FP "hard" ABI must be used in order to facilitate the FP register
+    # initialisation and synchronisation.
+    set(FORCE_FP_HARDABI TRUE)
+  endif()
+
+  if    (CONFIG_FP_HARDABI OR FORCE_FP_HARDABI)
     list(APPEND TOOLCHAIN_C_FLAGS   -mfloat-abi=hard)
     list(APPEND TOOLCHAIN_LD_FLAGS  -mfloat-abi=hard)
+  elseif(CONFIG_FP_SOFTABI)
+    list(APPEND TOOLCHAIN_C_FLAGS   -mfloat-abi=softfp)
+    list(APPEND TOOLCHAIN_LD_FLAGS  -mfloat-abi=softfp)
   endif()
 endif()
 
