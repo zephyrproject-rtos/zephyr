e89a1b2eb18ae2b48b69ebee3e53e8607bf67335
Johan Hedberg <johan.hedberg@intel.com>
2020-04-01 15:59:41 +0300

    Bluetooth: samples: Fix timeout passed to k_sleep()
    Use the appropriate K_SECONDS() macro to pass a timeout to k_sleep().
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- samples/bluetooth/peripheral_hr/src/main.c
+++ samples/bluetooth/peripheral_hr/src/main.c
@@ -127,7 +127,7 @@ void main(void)
 	 * of starting delayed work so we do it here
 	 */
 	while (1) {
-		k_sleep(MSEC_PER_SEC);
+		k_sleep(K_SECONDS(1));
 
 		/* Heartrate measurements simulation */
 		hrs_notify();
