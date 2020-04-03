88bf06bb01b3048bbb62d288d96e6a4ebd87ca62
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2020-04-03 16:58:27 +0200

    boards: arm: nrf5340_dk_nrf5340: rename target to nrf5340pdk_nrf5340
    We rename the nRF53 Dev Kit board target (nrf5340_dk_nrf5340)
    to nrf5340pdk_nrf5340. We update all associated references
    in the supportive documentation and all nRF5340-related
    cofigurations and overlay files in the samples and tests
    in the tree.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- boards/arm/nrf5340pdk_nrf5340/board.cmake
+++ boards/arm/nrf5340pdk_nrf5340/board.cmake
@@ -1,12 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
-
-if(CONFIG_BOARD_NRF5340_DK_NRF5340_CPUAPP OR CONFIG_BOARD_NRF5340_DK_NRF5340_CPUAPPNS)
+if(CONFIG_BOARD_NRF5340PDK_NRF5340_CPUAPP OR CONFIG_BOARD_NRF5340PDK_NRF5340_CPUAPPNS)
 board_runner_args(nrfjprog "--nrf-family=NRF53" "--tool-opt=--coprocessor CP_APPLICATION")
 board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
 endif()
 
-if(CONFIG_BOARD_NRF5340_DK_NRF5340_CPUNET)
+if(CONFIG_BOARD_NRF5340PDK_NRF5340_CPUNET)
 board_runner_args(nrfjprog "--nrf-family=NRF53" "--tool-opt=--coprocessor CP_NETWORK")
 endif()
 
