2d6b58dce8d7afacc02e9e2d02c8e57928e06de1
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2020-04-03 14:56:47 +0300

    console: ipm_console: Add message queue to console
    Add message queue to console in CONFIG_IPM_CONSOLE_IMMEDIATE is not
    selected.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- drivers/console/ipm_console.c
+++ drivers/console/ipm_console.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Intel Corporation
+ * Copyright (c) 2020 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -12,12 +12,62 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(ipm_console, CONFIG_IPM_LOG_LEVEL);
 
-static struct device *ipm_console_device;
+#if defined(CONFIG_IPM_CONSOLE_IMMEDIATE)
+struct device *ipm_dev;
+#else
+/*
+ * Stack for the message queue handling thread
+ */
+static K_THREAD_STACK_DEFINE(thread_stack, 1024);
+static struct k_thread thread_data;
+
+K_MSGQ_DEFINE(msg_queue, sizeof(char), CONFIG_IPM_CONSOLE_QUEUE_SIZE, 4);
+
+static void msgq_thread(void *arg1, void *arg2, void *arg3)
+{
+	struct device *ipm_dev = arg1;
+
+	ARG_UNUSED(arg2);
+	ARG_UNUSED(arg3);
+
+	LOG_DBG("started thread");
+
+	while (true) {
+		int character = 0;
+		int ret;
+
+		/* Get character from message queue */
+		k_msgq_get(&msg_queue, &character, K_FOREVER);
+
+		/* Blocking write over IPM */
+		ret = ipm_send(ipm_dev, 1, character, NULL, 0);
+		if (ret) {
+			LOG_ERR("Error sending character %c over IPM, ret %d",
+				character, ret);
+		}
+
+		k_yield();
+	}
+}
+#endif /* ! CONFIG_IPM_CONSOLE_IMMEDIATE */
 
 static int console_out(int c)
 {
-	/* Send character with busy wait */
-	ipm_send(ipm_console_device, 1, c & ~BIT(31), NULL, 0);
+	int ret;
+
+#if defined(CONFIG_IPM_CONSOLE_IMMEDIATE)
+	ret = ipm_send(ipm_dev, 1, c & ~BIT(31), NULL, 0);
+	if (ret) {
+		LOG_ERR("Error sending character %c over IPM, ret %d", c, ret);
+	}
+#else
+	char character = c;
+
+	ret = k_msgq_put(&msg_queue, &character, K_NO_WAIT);
+	if (ret) {
+		LOG_ERR("Error queueing character %c, ret %d", character, ret);
+	}
+#endif
 
 	return c;
 }
@@ -47,15 +97,33 @@ static void ipm_console_hook_install(void)
 
 static int ipm_console_init(struct device *dev)
 {
+	struct device *ipm;
+
 	ARG_UNUSED(dev);
 
-	LOG_DBG("");
+	LOG_DBG("IPM console initialization");
 
-	ipm_console_device = device_get_binding(CONFIG_IPM_CONSOLE_ON_DEV_NAME);
-	if (!ipm_console_device) {
+	ipm = device_get_binding(CONFIG_IPM_CONSOLE_ON_DEV_NAME);
+	if (!ipm) {
 		LOG_ERR("Cannot get %s", CONFIG_IPM_CONSOLE_ON_DEV_NAME);
 		return -ENODEV;
 	}
+
+	if (ipm_max_id_val_get(ipm) < 0xFF) {
+		LOG_ERR("IPM driver does not support 8 bit ID values");
+		return -ENOTSUP;
+	}
+
+#if defined(CONFIG_IPM_CONSOLE_IMMEDIATE)
+	ipm_dev = ipm;
+#else
+	k_thread_create(&thread_data, thread_stack,
+			K_THREAD_STACK_SIZEOF(thread_stack),
+			msgq_thread, ipm, NULL, NULL,
+			CONFIG_MAIN_THREAD_PRIORITY - 1,
+			0, K_NO_WAIT);
+	k_thread_name_set(&thread_data, "ipm_console_thread");
+#endif /* CONFIG_IPM_CONSOLE_IMMEDIATE */
 
 	ipm_console_hook_install();
 
@@ -63,10 +131,4 @@ static int ipm_console_init(struct device *dev)
 }
 
 /* Need to be initialized after IPM */
-SYS_INIT(ipm_console_init,
-#if defined(CONFIG_EARLY_CONSOLE)
-	 PRE_KERNEL_2,
-#else
-	 POST_KERNEL,
-#endif
-	 CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+SYS_INIT(ipm_console_init, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
