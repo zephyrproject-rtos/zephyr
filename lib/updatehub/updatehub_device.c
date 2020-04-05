b26942901b48d595e0e99b576499faf00c28cb64
Gerson Fernando Budke <gerson.budke@ossystems.com.br>
2020-04-04 21:10:30 -0300

    lib: updatehub: Refact to use bin2hex
    Use bin2hex instead inline conversion.
    
    Signed-off-by: Gerson Fernando Budke <gerson.budke@ossystems.com.br>

--- lib/updatehub/updatehub_device.c
+++ lib/updatehub/updatehub_device.c
@@ -7,23 +7,16 @@
 
 bool updatehub_get_device_identity(char *id, int id_max_len)
 {
-	int i, id_len = 0;
-	char buf[3];
-	u8_t hwinfo_id[id_max_len];
+	u8_t hwinfo_id[DEVICE_ID_BIN_MAX_SIZE];
 	size_t length;
 
-	length = hwinfo_get_device_id(hwinfo_id, sizeof(hwinfo_id) - 1);
+	length = hwinfo_get_device_id(hwinfo_id, DEVICE_ID_BIN_MAX_SIZE);
 	if (length <= 0) {
 		return false;
 	}
 
 	memset(id, 0, id_max_len);
+	length = bin2hex(hwinfo_id, length, id, id_max_len - 1);
 
-	for (i = 0; i < length; i++) {
-		snprintk(buf, sizeof(buf), "%02x", hwinfo_id[i]);
-		id_len = strlen(id);
-		strncat(id, buf, id_max_len - id_len);
-	}
-
-	return true;
+	return length > 0;
 }
