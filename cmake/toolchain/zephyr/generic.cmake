aa2f1d4ede1259251f77af9d3c385fa481e2fe4e
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2020-04-06 23:15:39 +0200

    cmake: Zephyr sdk package handling
    This commit introduces Zephyr SDK CMake config package.
    
    This removes the need for setting ZEPHYR_SDK_INSTALL_DIR and
    ZEPHYR_TOOLCHAIN_VARIANT when using Zephyr SDK in Linux.
    It also allows to introduces never SDKs without breaking Zephyr.
    
    For example, with this PR, the current Zephyr SDK is 0.11.1 but when
    releasing 0.12 then the current Zephyr will no longer built.
    This PR moves the Zephyr SDK CMake related code to the SDK and thus
    allowing to use newer SDKs, as long as they are backwards compatible.
    
    It also allows multiple SDK installations, and will automatically select
    the version closet to the required version.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/toolchain/zephyr/generic.cmake
+++ cmake/toolchain/zephyr/generic.cmake
@@ -1,11 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-if (NOT ZEPHYR_SDK_INSTALL_DIR)
-  message(FATAL_ERROR "ZEPHYR_SDK_INSTALL_DIR must be set")
-endif()
+include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/generic.cmake)
 
-include(${CMAKE_CURRENT_LIST_DIR}/${SDK_MAJOR_MINOR}/generic.cmake)
-
-set(TOOLCHAIN_KCONFIG_DIR ${CMAKE_CURRENT_LIST_DIR}/${SDK_MAJOR_MINOR})
-
-message(STATUS "Found toolchain: zephyr (${ZEPHYR_SDK_INSTALL_DIR})")
+set(TOOLCHAIN_KCONFIG_DIR ${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr)
