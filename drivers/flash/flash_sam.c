bfba5c824daef06c7661817b178812d029497eae
Kumar Gala <kumar.gala@linaro.org>
2020-04-08 09:39:36 -0500

    drivers: flash: Use DT_INST_LABEL instead of DT_FLASH_DEV_NAME
    Replace use of DT_FLASH_DEV_NAME with DT_INST_LABEL in drivers as we
    want to phase out DT_FLASH_DEV_NAME.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- drivers/flash/flash_sam.c
+++ drivers/flash/flash_sam.c
@@ -352,7 +352,7 @@ static const struct flash_sam_dev_cfg flash_sam_cfg = {
 
 static struct flash_sam_dev_data flash_sam_data;
 
-DEVICE_AND_API_INIT(flash_sam, DT_FLASH_DEV_NAME,
+DEVICE_AND_API_INIT(flash_sam, DT_INST_LABEL(0),
 		    flash_sam_init, &flash_sam_data, &flash_sam_cfg,
 		    POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
 		    &flash_sam_api);
