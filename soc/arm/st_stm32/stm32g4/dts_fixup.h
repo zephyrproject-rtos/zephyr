563990c9ce1c29fe709f2662a028b9a67dad8c80
Paolo Teti <paolo.teti@gmail.com>
2020-04-10 18:29:02 +0200

    dts: stm32: G4: Add adc0 and adc1 nodes
    Add adc0 and adc1 nodes for STM32G4X SoC series.
    
    Signed-off-by: Paolo Teti <paolo.teti@gmail.com>

--- soc/arm/st_stm32/stm32g4/dts_fixup.h
+++ soc/arm/st_stm32/stm32g4/dts_fixup.h
@@ -8,6 +8,9 @@
 
 #define DT_NUM_IRQ_PRIO_BITS			DT_ARM_V7M_NVIC_E000E100_ARM_NUM_IRQ_PRIORITY_BITS
 
+#define DT_ADC_0_NAME			DT_LABEL(DT_INST(0, st_stm32_adc))
+#define DT_ADC_1_NAME			DT_LABEL(DT_INST(1, st_stm32_adc))
+
 #define DT_FLASH_DEV_NAME			DT_LABEL(DT_INST(0, st_stm32g4_flash_controller))
 
 #define DT_RTC_0_NAME				DT_LABEL(DT_INST(0, st_stm32_rtc))
