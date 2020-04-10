c9a3fca1e460e98af834f4a09912349ec6075e5b
Alexandre Mergnat <amergnat@baylibre.com>
2020-04-10 06:03:14 +0200

    samples: userspace: hello_world
    This sample print a hello world message through a user thread.
    
    Signed-off-by: Alexandre Mergnat <amergnat@baylibre.com>

---
+++ samples/userspace/hello_world_user/src/main.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020 BayLibre, SAS
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <stdio.h>
+#define USER_STACKSIZE	2048
+
+struct k_thread user_thread;
+K_THREAD_STACK_DEFINE(user_stack, USER_STACKSIZE);
+
+static void user_function(void *p1, void *p2, void *p3)
+{
+	printf("Hello World from UserSpace! %s\n", CONFIG_BOARD);
+}
+
+
+void main(void)
+{
+	k_thread_create(&user_thread, user_stack, USER_STACKSIZE,
+			user_function, NULL, NULL, NULL,
+			-1, K_USER, K_MSEC(0));
+}
