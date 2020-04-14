c523e3606a4f5dabade55def90eee3b891f48d12
Christoph Reiter <christoph.reiter@infineon.com>
2020-04-14 16:46:15 +0200

    drivers: i2c: nrfx: Fix log level is ignored
    Fixes the problem that the log level set in Kconfig is ignored.
    
    Signed-off-by: Christoph Reiter <christoph.reiter@infineon.com>

--- drivers/i2c/i2c_nrfx_twim.c
+++ drivers/i2c/i2c_nrfx_twim.c
@@ -9,10 +9,8 @@
 #include <dt-bindings/i2c/i2c.h>
 #include <nrfx_twim.h>
 
-#define LOG_DOMAIN "i2c_nrfx_twim"
-#define LOG_LEVEL CONFIG_I2C_LOG_LEVEL
 #include <logging/log.h>
-LOG_MODULE_REGISTER(i2c_nrfx_twim);
+LOG_MODULE_REGISTER(i2c_nrfx_twim, CONFIG_I2C_LOG_LEVEL);
 
 struct i2c_nrfx_twim_data {
 	struct k_sem transfer_sync;
