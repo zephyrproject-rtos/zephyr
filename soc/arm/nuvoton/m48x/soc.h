4a5a165e0ceafbf6d1e367ffc33eec42c2aaa4c4
Saravanan Sekar <saravanan@linumiz.com>
2020-04-15 10:04:50 +0200

    soc: arm: add support for nuvoton numicro m48x series
    Add initial support for nuvoton numicro m48x SoC series, basic
    init and uart functionality are covered with gpio and clock
    directly relies on HAL.
    
    Signed-off-by: Saravanan Sekar <saravanan@linumiz.com>

---
+++ soc/arm/nuvoton/m48x/soc.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 Linumiz
+ * Author: Saravanan Sekar <saravanan@linumiz.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_ARM_NUVOTON_M48X_SOC_H_
+#define ZEPHYR_SOC_ARM_NUVOTON_M48X_SOC_H_
+
+#include <sys/util.h>
+#include <devicetree.h>
+#include <NuMicro.h>
+
+#endif /* ZEPHYR_SOC_ARM_NUVOTON_M48X_SOC_H_*/
