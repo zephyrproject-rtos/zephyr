713b534ad93536d61073b0b56bb36a5ca760bb39
Marek Porwisz <marek.porwisz@nordicsemi.no>
2020-04-16 15:50:32 +0200

    tests: subsys: openthread: OpenThread radio interface tests
    Currently there are no test for openthread on zephyr.
    Created tests for OpenThread platform radio interface.
    
    Signed-off-by: Marek Porwisz <marek.porwisz@nordicsemi.no>

---
+++ tests/subsys/openthread/radio_stub.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* needed here so the static device_get_binding does not get renamed */
+#include <device.h>
+
+/* OpenThread not enabled here */
+#define CONFIG_OPENTHREAD_L2_LOG_LEVEL LOG_LEVEL_DBG
+
+#ifndef CONFIG_NET_CONFIG_IEEE802154_DEV_NAME
+	#define CONFIG_NET_CONFIG_IEEE802154_DEV_NAME ""
+#endif /* CONFIG_NET_CONFIG_IEEE802154_DEV_NAME */
+
+#define CONFIG_OPENTHREAD_THREAD_PRIORITY 5
+#define OT_WORKER_PRIORITY K_PRIO_COOP(CONFIG_OPENTHREAD_THREAD_PRIORITY)
+#define CONFIG_NET_L2_OPENTHREAD 1
+
+/* needed for stubbing device driver */
+struct device *device_get_binding_stub(const char *name);
+#define device_get_binding device_get_binding_stub
+
+/* file itself */
+#include "radio.c"
