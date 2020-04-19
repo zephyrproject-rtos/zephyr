88c0545ea422eb7545cc98ac13eae07eb5661961
Peter A. Bigot <pab@pabigot.com>
2020-04-18 22:58:08 -0500

    samples: usb: mass: add support for littlefs file systems
    This allows mass storage exposure of a littlefs file system,
    specifically one on the SPI NOR flash of the nrf52840dk_nrf52840.  In
    combination with littlefs-fuse this allows a host system to examine
    and change the local storage of a Zephyr application.
    
    Note that it is critical that all parameters of the file system match
    between what Zephyr is using and what littlefs-fuse is using.
    Inconsistencies can produce confusing results where each system sees
    different content.  The README has been updated with a detailed
    example.
    
    Signed-off-by: Peter A. Bigot <pab@pabigot.com>

--- samples/subsys/usb/mass/src/main.c
+++ samples/subsys/usb/mass/src/main.c
@@ -10,21 +10,39 @@
 #include <zephyr.h>
 #include <logging/log.h>
 #include <usb/usb_device.h>
+#include <stdio.h>
+
 LOG_MODULE_REGISTER(main);
 
-#if CONFIG_DISK_ACCESS_FLASH && CONFIG_FAT_FILESYSTEM_ELM
+#if CONFIG_DISK_ACCESS_FLASH
+#if CONFIG_FAT_FILESYSTEM_ELM
 #include <fs/fs.h>
 #include <ff.h>
 
 static FATFS fat_fs;
-#define FATFS_MNTP	"/NAND:"
+#define FATFS_MNTP      "/NAND:"
 
-static struct fs_mount_t fatfs_mnt = {
+static struct fs_mount_t fs_mnt = {
 	.type = FS_FATFS,
 	.mnt_point = FATFS_MNTP,
 	.fs_data = &fat_fs,
 };
-#endif
+#elif CONFIG_FILE_SYSTEM_LITTLEFS
+#include <fs/fs.h>
+#include <fs/littlefs.h>
+#include <storage/flash_map.h>
+
+FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(storage);
+static struct fs_mount_t fs_mnt = {
+	.type = FS_LITTLEFS,
+	.fs_data = &storage,
+	.storage_dev = (void *)DT_FLASH_AREA_STORAGE_ID,
+	.mnt_point = "/lfs",
+};
+#else
+#error No recognized file system
+#endif  /* file system */
+#endif  /* CONFIG_DISK_ACCESS_FLASH */
 
 void main(void)
 {
@@ -38,11 +56,71 @@ void main(void)
 
 	LOG_INF("The device is put in USB mass storage mode.\n");
 
-#if CONFIG_DISK_ACCESS_FLASH && CONFIG_FAT_FILESYSTEM_ELM
-	int res = fs_mount(&fatfs_mnt);
+#if CONFIG_DISK_ACCESS_FLASH
+	struct fs_mount_t *mp = &fs_mnt;
+	unsigned int id = (uintptr_t)mp->storage_dev;
+	int rc;
+	const struct flash_area *pfa;
 
-	if (res < 0) {
-		LOG_ERR("Mount failed.");
+	rc = flash_area_open(id, &pfa);
+	printk("Area %u at 0x%x on %s for %u bytes\n",
+	       id, (unsigned int)pfa->fa_off, pfa->fa_dev_name,
+	       (unsigned int)pfa->fa_size);
+
+	if (IS_ENABLED(CONFIG_APP_WIPE_STORAGE)) {
+		printk("Erasing flash area ... ");
+		rc = flash_area_erase(pfa, 0, pfa->fa_size);
+		printk("%d\n", rc);
 	}
+
+	rc = fs_mount(&fs_mnt);
+
+	/* Allow log messages to flush to avoid interleaved output */
+	k_sleep(K_MSEC(50));
+
+	printk("Mount %s: %d\n", fs_mnt.mnt_point, rc);
+
+	if (rc >= 0) {
+		struct fs_statvfs sbuf;
+
+		rc = fs_statvfs(mp->mnt_point, &sbuf);
+		if (rc < 0) {
+			printk("FAIL: statvfs: %d\n", rc);
+			goto out;
+		}
+
+		printk("%s: bsize = %lu ; frsize = %lu ;"
+		       " blocks = %lu ; bfree = %lu\n",
+		       mp->mnt_point,
+		       sbuf.f_bsize, sbuf.f_frsize,
+		       sbuf.f_blocks, sbuf.f_bfree);
+
+		struct fs_dir_t dir = { 0 };
+
+		rc = fs_opendir(&dir, mp->mnt_point);
+		printk("%s opendir: %d\n", mp->mnt_point, rc);
+
+		while (rc >= 0) {
+			struct fs_dirent ent = { 0 };
+
+			rc = fs_readdir(&dir, &ent);
+			if (rc < 0) {
+				break;
+			}
+			if (ent.name[0] == 0) {
+				printk("End of files\n");
+				break;
+			}
+			printk("  %c %u %s\n",
+			       (ent.type == FS_DIR_ENTRY_FILE) ? 'F' : 'D',
+			       ent.size,
+			       ent.name);
+		}
+
+		(void)fs_closedir(&dir);
+	}
+
+out:
+	flash_area_close(pfa);
 #endif
 }
