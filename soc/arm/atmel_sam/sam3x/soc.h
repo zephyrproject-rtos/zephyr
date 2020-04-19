d8325a8c792934fe81163a4a1607b6fff960069a
Gerson Fernando Budke <nandojve@gmail.com>
2020-04-19 12:03:22 -0300

    soc: arm: atmel_sam: Clock clean-up
    Small clock clean-up. This moves all devicetree entries on SoC to
    atmel_sam_dt.h.
    
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

--- soc/arm/atmel_sam/sam3x/soc.h
+++ soc/arm/atmel_sam/sam3x/soc.h
@@ -35,18 +35,13 @@
 #error Library does not support the specified device.
 #endif
 
-/* Add include for DTS generated information */
-#include <devicetree.h>
-
-#define ID_UART0   ID_UART
-#define UART0      UART
-
 #include "../common/soc_pmc.h"
 #include "../common/soc_gpio.h"
 #include "../common/atmel_sam_dt.h"
 
 /** Processor Clock (HCLK) Frequency */
-#define SOC_ATMEL_SAM_HCLK_FREQ_HZ DT_ARM_CORTEX_M3_0_CLOCK_FREQUENCY
+#define SOC_ATMEL_SAM_HCLK_FREQ_HZ ATMEL_SAM_DT_CPU_CLK_FREQ_HZ
+
 /** Master Clock (MCK) Frequency */
 #define SOC_ATMEL_SAM_MCK_FREQ_HZ SOC_ATMEL_SAM_HCLK_FREQ_HZ
 
