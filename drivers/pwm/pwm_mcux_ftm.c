9cc040aec011eb89611e10027b2caa7673980374
Henrik Brix Andersen <hebad@vestas.com>
2020-04-19 22:36:45 +0200

    dts: introduce shared binding for the NXP FlexTimer
    Do not assume in the SoC level device trees that NXP Kinetis FlexTimer
    nodes will always be configured as PWM. Instead, configure FlexTimer
    nodes for PWM at the board level for NXP Kinetis boards.
    
    Signed-off-by: Henrik Brix Andersen <hebad@vestas.com>

--- drivers/pwm/pwm_mcux_ftm.c
+++ drivers/pwm/pwm_mcux_ftm.c
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define DT_DRV_COMPAT nxp_kinetis_ftm
+#define DT_DRV_COMPAT nxp_kinetis_ftm_pwm
 
 #include <drivers/clock_control.h>
 #include <errno.h>
