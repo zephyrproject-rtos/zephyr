c2ba41136e20bf352e02b9a26692ab4d0bc926dd
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2020-04-20 08:50:37 -0400

    boards: up_squared_adsp: update terminal tool
    Update terminal tool to use line buffer read instead of one charcter
    over IPM.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- boards/xtensa/up_squared_adsp/tools/mbterm.py
+++ boards/xtensa/up_squared_adsp/tools/mbterm.py
@@ -1,15 +1,43 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2019 Intel Corporation
+# Copyright (c) 2020 Intel Corporation
 #
 # SPDX-License-Identifier: Apache-2.0
 
 from time import sleep
+from mmap import mmap
+from ctypes import c_uint8
 
 from lib.device import Device
 import lib.registers as regs
 
-def mailbox_poll(dev):
+MBOX = 0x91281000
+LENGTH = 0x1000
+
+with open("/dev/mem", "r+b") as f:
+    mem_map = mmap(f.fileno(), LENGTH, offset=MBOX)
+    mem = (c_uint8 * LENGTH).from_buffer(mem_map)
+
+def mailbox_poll_mem(dev):
+    while True:
+        if dev.dsp_hipct.value & regs.ADSP_IPC_HIPCT_BUSY:
+
+            # Use only id for passing character
+            line_len = dev.dsp_hipct.value & regs.ADSP_IPC_HIPCT_MSG
+
+            # Mask out internal bits
+            line_len &= 0x10FFFF
+
+            if line_len:
+                print(bytes(mem[:line_len]).decode())
+
+            # Clear interrupt
+            dev.dsp_hipct.value |= regs.ADSP_IPC_HIPCT_BUSY
+        else:
+            sleep(0.005)
+
+# Character passed in mailbox ID field
+def mailbox_poll_char(dev):
     while True:
         if dev.dsp_hipct.value & regs.ADSP_IPC_HIPCT_BUSY:
 
@@ -35,4 +63,4 @@ if __name__ == "__main__":
     device = Device()
     device.open_device()
 
-    mailbox_poll(device)
+    mailbox_poll_mem(device)
