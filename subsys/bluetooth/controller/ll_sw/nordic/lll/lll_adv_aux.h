e0bd57901fbcb8c4fde3ff5dc9b5e77fd150b4df
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2020-04-21 11:39:09 +0530

    Bluetooth: controller: split: Use uint8_t for advertising handles
    Refactor to use uint8_t for advertising handles.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_aux.h
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_aux.h
@@ -11,4 +11,4 @@ void lll_adv_aux_offset_fill(uint32_t ticks_offset, uint32_t start_us,
 			     struct pdu_adv *pdu);
 
 
-extern uint16_t ull_adv_aux_lll_handle_get(struct lll_adv_aux *lll);
+extern uint8_t ull_adv_aux_lll_handle_get(struct lll_adv_aux *lll);
