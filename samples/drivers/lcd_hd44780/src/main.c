2e6af610fe3b16fc86a366318e440b543bd08fa3
Kumar Gala <kumar.gala@linaro.org>
2020-04-21 17:58:19 -0500

    drivers: gpio: gpio_sam: Convert to DT_INST
    Convert driver to use new DT_INST macros throughout.  We update one
    sample app to use a nodelabel reference.  We also remove defines in
    dts_fixup.h as they are no longer needed.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- samples/drivers/lcd_hd44780/src/main.c
+++ samples/drivers/lcd_hd44780/src/main.c
@@ -72,7 +72,7 @@
 
 
 #if defined(CONFIG_SOC_PART_NUMBER_SAM3X8E)
-#define GPIO_DRV_NAME DT_GPIO_SAM_PORTC_LABEL
+#define GPIO_DRV_NAME DT_LABEL(DT_NODELABEL(pioc))
 #else
 #error "Unsupported GPIO driver"
 #endif
