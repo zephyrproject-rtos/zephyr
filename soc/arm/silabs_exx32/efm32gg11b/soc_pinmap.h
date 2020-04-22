0e862107d347e8eb9199e26429c743a869112c74
Kumar Gala <kumar.gala@linaro.org>
2020-04-22 05:18:45 -0500

    soc: arm: silabs_exx32: DT_GPIO_GECKO_SWO_LOCATION rework
    Change DT_GPIO_GECKO_SWO_LOCATION to use new DT_INST and DT_PROP macros
    and remove defining it in dts_fixup.h.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- soc/arm/silabs_exx32/efm32gg11b/soc_pinmap.h
+++ soc/arm/silabs_exx32/efm32gg11b/soc_pinmap.h
@@ -18,17 +18,22 @@
 #include <soc.h>
 #include <em_gpio.h>
 
+#define GPIO_NODE DT_INST(0, silabs_gecko_gpio)
+#if DT_NODE_HAS_PROP(GPIO_NODE, location_swo)
+#define SWO_LOCATION DT_PROP(GPIO_NODE, location_swo)
+#endif
+
 #ifdef CONFIG_GPIO_GECKO
 /* Serial Wire Output (SWO) */
-#if (DT_GPIO_GECKO_SWO_LOCATION == 0)
+#if (SWO_LOCATION == 0)
 #define PIN_SWO {gpioPortF, 2, gpioModePushPull, 1}
-#elif (DT_GPIO_GECKO_SWO_LOCATION == 1)
+#elif (SWO_LOCATION == 1)
 #define PIN_SWO {gpioPortC, 15, gpioModePushPull, 1}
-#elif (DT_GPIO_GECKO_SWO_LOCATION == 2)
+#elif (SWO_LOCATION == 2)
 #define PIN_SWO {gpioPortD, 1, gpioModePushPull, 1}
-#elif (DT_GPIO_GECKO_SWO_LOCATION == 3)
+#elif (SWO_LOCATION == 3)
 #define PIN_SWO {gpioPortD, 2, gpioModePushPull, 1}
-#elif (DT_GPIO_GECKO_SWO_LOCATION >= 4)
+#elif (SWO_LOCATION >= 4)
 #error ("Invalid SWO pin location")
 #endif
 #endif /* CONFIG_GPIO_GECKO */
