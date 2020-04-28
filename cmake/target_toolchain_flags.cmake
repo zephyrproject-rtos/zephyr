ffaa2d34d1c28d55e7ca9a26679fc737b1ee7fb5
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2020-04-28 08:33:37 +0200

    cmake: toolchain: Toolchain abstraction templates
    As part of toolchain abstraction three template files to facilitate
    additional toolchain support development has been introduced.
    
    Those covers:
    - bintools
    - compiler flags
    - linker flags
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/target_toolchain_flags.cmake
+++ cmake/target_toolchain_flags.cmake
@@ -4,6 +4,13 @@ add_custom_target(compiler)
 add_custom_target(compiler-cpp)
 add_custom_target(linker)
 
+# Loading of templates are strictly not needed as they does not set any
+# properties.
+# They purely provides an overview as well as a starting point for supporting
+# a new toolchain.
+include(${CMAKE_CURRENT_LIST_DIR}/compiler/compiler_flags_template.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/linker/linker_flags_template.cmake)
+
 # Configure the toolchain flags based on what toolchain technology is used
 # (gcc, host-gcc etc.)
 include(${TOOLCHAIN_ROOT}/cmake/compiler/${COMPILER}/compiler_flags.cmake OPTIONAL)
