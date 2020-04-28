9e2372325d91a5218a18f60df4e2fc22adb2cedd
Kumar Gala <kumar.gala@linaro.org>
2020-04-28 10:43:59 -0500

    soc: arc: Remove unused defines
    We always have CONFIG_HAS_DTS set so remove block of defines associated
    with ifndef CONFIG_HAS_DTS.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- soc/arc/snps_emsk/soc.h
+++ soc/arc/snps_emsk/soc.h
@@ -45,46 +45,6 @@
 #define INT_ENABLE_ARC				~(0x00000001 << 8)
 #define INT_ENABLE_ARC_BIT_POS			(8)
 
-#ifndef CONFIG_HAS_DTS
-/* I2C */
-/* I2C_0 is on Pmod2 connector */
-#define DT_I2C_0_BASE_ADDR			0xF0004000
-#define DT_I2C_0_IRQ_FLAGS			0
-
-/* I2C_1 is on Pmod4 connector */
-#define DT_I2C_1_BASE_ADDR			0xF0005000
-#define DT_I2C_1_IRQ_FLAGS			0
-
-/*
- * SPI Chip Select Assignments on EM Starter Kit
- *
- *  CS0   Pmod6 - pin 1 - J6
- *  CS1   Pmod5 - pin 1 - J5 & Pmod 6 - pin 7 - J6
- *  CS2   Pmod6 - pin 8 - J6
- *  CS3   SDCard (onboard)
- *  CS4   Internal SPI Slave - loopback
- *  CS5   SPI-Flash (onboard)
- */
-
-/**
- * Peripheral Interrupt Connection Configurations
- */
-#ifdef CONFIG_BOARD_EM_STARTERKIT_R23
-#define DT_GPIO_DW_0_IRQ				24
-#define DT_I2C_0_IRQ			25
-#define DT_I2C_1_IRQ			26
-#else /* CONFIG_BOARD_EM_STARTERKIT_R23 */
-#define DT_GPIO_DW_0_IRQ				22
-#define DT_I2C_0_IRQ			23
-#define DT_I2C_1_IRQ			24
-#endif /* !CONFIG_BOARD_EM_STARTERKIT_R23 */
-
-#define DT_GPIO_DW_1_IRQ				0	/* can't interrupt */
-#define DT_GPIO_DW_2_IRQ				0	/* can't interrupt */
-#define DT_GPIO_DW_3_IRQ				0	/* can't interrupt */
-
-#endif /* CONFIG_HAS_DTS */
-
 #endif /* !_ASMLANGUAGE */
 
 #endif /* _SOC__H_ */
