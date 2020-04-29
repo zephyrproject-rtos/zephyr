d4989b4d533f9c149cd1548f55f26fece5695b7d
Kumar Gala <kumar.gala@linaro.org>
2020-04-29 15:57:09 -0500

    soc: atmel: Cleanup dts_fixup.h
    Remove dts_fixup.h files that are needed anymore, remove defines that
    are used, and replace defines with new DT macros.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- soc/arm/atmel_sam0/samd20/dts_fixup.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: Apache-2.0 */
-
-/* SoC level DTS fixup file */
-
-#define DT_FLASH_DEV_NAME			DT_ATMEL_SAM0_NVMCTRL_41004000_LABEL
-
-/* End of SoC Level DTS fixup file */
