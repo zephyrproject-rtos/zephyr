e360598fd98f08da14c05dfcffd4eb3fcd31e11b
Erwan Gouriou <erwan.gouriou@linaro.org>
2020-04-29 14:26:21 +0200

    soc: stm32: adc: Remove use of DT_FOO_LABEL defines
    Complete STM32 conversion to new DT macros by removing
    remaining occurences of DT_ST_STM32_ADC_FOO_LABEL.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- soc/arm/st_stm32/stm32wb/dts_fixup.h
+++ soc/arm/st_stm32/stm32wb/dts_fixup.h
@@ -6,8 +6,8 @@
 
 /* SoC level DTS fixup file */
 
-#define DT_RTC_0_NAME				DT_LABEL(DT_INST(0, st_stm32_rtc))
+#define DT_RTC_0_NAME			DT_LABEL(DT_INST(0, st_stm32_rtc))
 
-#define DT_ADC_1_NAME			DT_ST_STM32_ADC_50040000_LABEL
+#define DT_ADC_0_NAME			DT_LABEL(DT_INST(0, st_stm32_adc))
 
 /* End of SoC Level DTS fixup file */
