771bf09183fa4ae93963956dbd9c89d9399a93b6
Wayne Ren <wei.ren@synopsys.com>
2020-04-29 16:00:06 +0800

    boards: enable mdb runner for arc boards
    * current supported boards:
       * emsk, iotdk, nsim, emsdp, hsdk.
    * for the unsupported future boards, pls take a
      reference of supported boards' board.cmake.
    * mdb runner is required and the default runner for SMP
      case, e.g., HSDK and nsim_hs_smp.
    * other ARC boards can also choose to use mdb by
      setting runner as mdb, e.g. west flash --runner mdb.
    * with mdb runner, user can make a debug through mdb gui
    * with arc_nsim or opencod runner (default runner), user
      can make a debug through gdb cmdline.
    
    Signed-off-by: Wayne Ren <wei.ren@synopsys.com>

--- boards/arc/em_starterkit/board.cmake
+++ boards/arc/em_starterkit/board.cmake
@@ -1,4 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(openocd "--use-elf")
+board_runner_args(mdb "--jtag=digilent")
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/mdb.board.cmake)
