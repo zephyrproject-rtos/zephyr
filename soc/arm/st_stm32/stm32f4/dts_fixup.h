63e17c107c532ac287e0d29d5c9b2ccba7cdfad4
Erwan Gouriou <erwan.gouriou@linaro.org>
2020-04-29 16:42:07 +0200

    soc: stm32: Cleanup DT_FLASH_DEV_NAME
    No more needed
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- soc/arm/st_stm32/stm32f4/dts_fixup.h
+++ soc/arm/st_stm32/stm32f4/dts_fixup.h
@@ -6,6 +6,4 @@
 
 #define DT_ADC_1_NAME			DT_ST_STM32_ADC_40012000_LABEL
 
-#define DT_FLASH_DEV_NAME		DT_LABEL(DT_INST(0, st_stm32f4_flash_controller))
-
 /* End of SoC Level DTS fixup file */
--- soc/arm/st_stm32/stm32l4/dts_fixup.h
+++ soc/arm/st_stm32/stm32l4/dts_fixup.h
@@ -4,8 +4,6 @@
 
 #define DT_RTC_0_NAME			DT_LABEL(DT_INST(0, st_stm32_rtc))
 
-#define DT_FLASH_DEV_NAME		DT_LABEL(DT_INST(0, st_stm32l4_flash_controller))
-
 #define DT_ADC_1_NAME			DT_ST_STM32_ADC_50040000_LABEL
 
 /* End of SoC Level DTS fixup file */
