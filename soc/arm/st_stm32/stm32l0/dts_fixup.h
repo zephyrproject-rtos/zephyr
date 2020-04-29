0d59f63c0a622d86b73b04bd40789d3f6441c9e8
Erwan Gouriou <erwan.gouriou@linaro.org>
2020-04-29 16:45:04 +0200

    soc: stm32: clean up DT_RTC symbols
    Replaced by the DT_INST version
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- soc/arm/st_stm32/stm32l0/dts_fixup.h
+++ soc/arm/st_stm32/stm32l0/dts_fixup.h
@@ -4,6 +4,6 @@
 
 #define DT_ADC_1_NAME			DT_ST_STM32_ADC_40012400_LABEL
 
-#define DT_RTC_0_NAME                   DT_ST_STM32_RTC_40002800_LABEL
+#define DT_RTC_0_NAME                   DT_LABEL(DT_INST(0, st_stm32_rtc))
 
 /* End of SoC Level DTS fixup file */
