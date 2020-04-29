3dd2f610b00282c7c549c2981f532f1bc3a80b2b
Kumar Gala <kumar.gala@linaro.org>
2020-04-29 10:11:28 -0500

    x86: Cleanup linker scripts to use new DTS macros
    Rework x86 linker scripts to use DT_REG_ADDR/DT_REG_SIZE on
    DT_CHOSEN(zephyr_sram) and DT_CHOSEN(zephyr_flash).  As part of this
    we remove the dts_fixup.h.  Using DT_REG_SIZE means we don't have to
    adjust the sizes by 1024.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- soc/x86/atom/dts_fixup.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: Apache-2.0 */
-
-/* SoC level DTS fixup file */
-
-#define DT_PHYS_RAM_ADDR			CONFIG_SRAM_BASE_ADDRESS
-
-#define DT_RAM_SIZE				CONFIG_SRAM_SIZE
-
-/* End of SoC Level DTS fixup file */
