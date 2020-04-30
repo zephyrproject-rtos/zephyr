7c9e7c8fd752281edcf0944d7c4b6eb4b5079683
Kumar Gala <kumar.gala@linaro.org>
2020-04-30 14:09:27 -0500

    dts: Remove unused or dead dts_fixup.h
    Remove dts_fixup.h files that are not used (empty) or the defines aren't
    used anymore and thus can be removed.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- boards/posix/native_posix/dts_fixup.h
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2019 Jan Van Winkel (jan.van_winkel@dxplore.eu)
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#define DT_UART_0_DEV_NAME  DT_ZEPHYR_NATIVE_POSIX_UART_UART_LABEL
--- boards/x86/qemu_x86/dts_fixup.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: Apache-2.0 */
-
-/* Board level DTS fixup file */
-
-#define DT_FLASH_ERASE_BLOCK_SIZE	DT_SOC_NV_FLASH_0_ERASE_BLOCK_SIZE
-#define DT_FLASH_WRITE_BLOCK_SIZE	DT_SOC_NV_FLASH_0_WRITE_BLOCK_SIZE
-
-/* End of Board Level DTS fixup file */
