d461befc2b9e875790d7ba9a00953396ff4a962f
Johann Fischer <j.fischer@phytec.de>
2020-05-04 01:15:28 +0200

    boards: decawave_dwm1001_dev: add jlink runner support
    Add jlink runner support.
    
    Signed-off-by: Johann Fischer <j.fischer@phytec.de>

--- boards/arm/decawave_dwm1001_dev/board.cmake
+++ boards/arm/decawave_dwm1001_dev/board.cmake
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
 board_runner_args(pyocd "--target=nrf52")
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
