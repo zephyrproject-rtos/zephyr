7f87a768304f89917a95ee0120b9953f9d3843d3
Stephanos Ioannidis <root@stephanos.io>
2020-05-05 08:10:46 +0900

    boards: arm: atsamd20_xpro: Add I2C device
    This commit configures the SERCOM2 peripheral to I2C mode for the Atmel
    SAM D20 Xplained Pro board.
    
    Note that the SERCOM2 on PA08/PA09 is the I2C peripheral/pin designated
    by the board user guide.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- boards/arm/atsamd20_xpro/pinmux.c
+++ boards/arm/atsamd20_xpro/pinmux.c
@@ -59,6 +59,27 @@ static int board_pinmux_init(struct device *dev)
 #warning Pin mapping may not be configured
 #endif
 
+#if ATMEL_SAM0_DT_SERCOM_CHECK(0, atmel_sam0_i2c)
+#warning Pin mapping may not be configured
+#endif
+#if ATMEL_SAM0_DT_SERCOM_CHECK(1, atmel_sam0_i2c)
+#warning Pin mapping may not be configured
+#endif
+#if ATMEL_SAM0_DT_SERCOM_CHECK(2, atmel_sam0_i2c)
+	/* SERCOM2 on SDA=PA08, SCL=PA09 */
+	pinmux_pin_set(muxa, 8, PINMUX_FUNC_D);
+	pinmux_pin_set(muxa, 9, PINMUX_FUNC_D);
+#endif
+#if ATMEL_SAM0_DT_SERCOM_CHECK(3, atmel_sam0_i2c)
+#warning Pin mapping may not be configured
+#endif
+#if ATMEL_SAM0_DT_SERCOM_CHECK(4, atmel_sam0_i2c)
+#warning Pin mapping may not be configured
+#endif
+#if ATMEL_SAM0_DT_SERCOM_CHECK(5, atmel_sam0_i2c)
+#warning Pin mapping may not be configured
+#endif
+
 	return 0;
 }
 
