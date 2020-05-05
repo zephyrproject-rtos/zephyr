463bce59e4d89ef57566b9761bdd9105bf1fc2d9
Kumar Gala <kumar.gala@linaro.org>
2020-05-05 05:14:15 -0500

    dts: Remove dts_fixup.h
    Remove all remaining dts_fixup.h as all defines in the files are no
    longer utilized.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- soc/arm/nxp_kinetis/ke1xf/dts_fixup.h
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2019 Vestas Wind Systems A/S
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/* SoC level DTS fixup file */
-#define DT_RTC_0_NAME	DT_LABEL(DT_INST(0, nxp_kinetis_rtc))
