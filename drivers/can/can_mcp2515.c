aec38f3fdab53193375b6646924f07336e70c76b
Kumar Gala <kumar.gala@linaro.org>
2020-05-05 09:37:56 -0500

    drivers: can: mcp2515: Convert from Kconfig to DT_NODELABEL
    As we phase out per instance Kconfig symbols convert to utilize
    DT_DRV_INST to initialize CAN instances.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- drivers/can/can_mcp2515.c
+++ drivers/can/can_mcp2515.c
@@ -826,7 +826,7 @@ static int mcp2515_init(struct device *dev)
 	return ret;
 }
 
-#ifdef CONFIG_CAN_1
+#if DT_HAS_DRV_INST(0)
 
 static K_THREAD_STACK_DEFINE(mcp2515_int_thread_stack,
 			     CONFIG_CAN_MCP2515_INT_THREAD_STACK_SIZE);
@@ -864,4 +864,4 @@ DEVICE_AND_API_INIT(can_mcp2515_1, DT_INST_LABEL(0), &mcp2515_init,
 		    &mcp2515_data_1, &mcp2515_config_1, POST_KERNEL,
 		    CONFIG_CAN_MCP2515_INIT_PRIORITY, &can_api_funcs);
 
-#endif /* CONFIG_CAN_1 */
+#endif /* DT_HAS_DRV_INST(0) */
