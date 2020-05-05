670d32587168ea6fbbccd6a45b31be99b3ad39f8
Kumar Gala <kumar.gala@linaro.org>
2020-05-05 05:14:15 -0500

    dts: Remove dts_fixup.h
    Remove all remaining dts_fixup.h as all defines in the files are no
    longer utilized.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- soc/arm/nxp_kinetis/k8x/dts_fixup.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2019 SEAL AG
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/* SoC level DTS fixup file */
-
-#define DT_RTC_0_NAME	DT_LABEL(DT_INST(0, nxp_kinetis_rtc))
--- soc/arm/st_stm32/stm32l0/dts_fixup.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: Apache-2.0 */
-
-/* SoC level DTS fixup file */
-
-#define DT_RTC_0_NAME                   DT_LABEL(DT_INST(0, st_stm32_rtc))
-
-/* End of SoC Level DTS fixup file */
