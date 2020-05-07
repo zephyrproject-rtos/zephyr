b2069057f1292ed4a2643e635b56c1f885ba6947
Ilya Tagunov <tagunil@gmail.com>
2020-05-08 01:52:44 +0300

    boards: nucleo_l152re: Enable DAC support
    Enable DAC support for Nucleo-L152RE board.
    
    Signed-off-by: Ilya Tagunov <tagunil@gmail.com>

--- boards/arm/nucleo_l152re/pinmux.c
+++ boards/arm/nucleo_l152re/pinmux.c
@@ -24,6 +24,9 @@ static const struct pin_config pinconf[] = {
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(adc1), okay) && CONFIG_ADC
 	{STM32_PIN_PA0, STM32L1X_PINMUX_FUNC_PA0_ADC1_IN0},
 #endif
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(dac1), okay) && CONFIG_DAC
+	{STM32_PIN_PA4, STM32L1X_PINMUX_FUNC_PA4_DAC_OUT1},
+#endif
 };
 
 static int pinmux_stm32_init(struct device *port)
