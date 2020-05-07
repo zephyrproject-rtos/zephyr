b899bbf9b948d0fecda3a05e4c388682386feefb
Ilya Tagunov <tagunil@gmail.com>
2020-05-08 01:45:07 +0300

    soc: stm32l1: Enable DAC support
    Enable STM32 DAC driver for STM32L1 series.
    
    Signed-off-by: Ilya Tagunov <tagunil@gmail.com>

--- soc/arm/st_stm32/stm32l1/soc.h
+++ soc/arm/st_stm32/stm32l1/soc.h
@@ -55,6 +55,10 @@
 #include <stm32l1xx_ll_adc.h>
 #endif
 
+#ifdef CONFIG_DAC_STM32
+#include <stm32l1xx_ll_dac.h>
+#endif
+
 #if defined(CONFIG_COUNTER_RTC_STM32)
 #include <stm32l1xx_ll_rtc.h>
 #include <stm32l1xx_ll_exti.h>
