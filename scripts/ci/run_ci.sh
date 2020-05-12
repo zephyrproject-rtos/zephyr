7a93442309181fafa4af79b4d6bf8ddc706b7eb0
Anas Nashif <anas.nashif@intel.com>
2020-05-11 22:00:53 -0400

    ci: no --subset needed on --test-only
    --test-only does not need --subset ...
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/ci/run_ci.sh
+++ scripts/ci/run_ci.sh
@@ -268,12 +268,11 @@ if [ -n "$main_ci" ]; then
 	tail -n +2 test_file_1.txt > test_file_1_in.txt
 	cat test_file_3.txt test_file_2_in.txt test_file_1_in.txt > test_file.txt
 
+	# Run a subset of tests based on matrix size
 	${sanitycheck} ${sanitycheck_options} --build-only --load-tests test_file.txt \
 		--subset ${matrix}/${matrix_builds}
 
-	# Run a subset of tests based on matrix size
-	${sanitycheck} ${sanitycheck_options} --test-only \
-		--subset ${matrix}/${matrix_builds} --retry-failed 3
+	${sanitycheck} ${sanitycheck_options} --test-only --retry-failed 3
 
 	# Run module tests on matrix #1
 	if [ "$matrix" = "1" ]; then
