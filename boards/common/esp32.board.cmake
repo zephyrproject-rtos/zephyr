74dbf9c33fb0c0508bdeaf02c5c044b8782f141a
Daniel Leung <daniel.leung@intel.com>
2020-05-12 23:48:44 -0700

    boards: esp32: only warn when ESP_IDF_PATH is not set
    This changes the assertion of ESP_IDF_PATH to simply a warning.
    The esp-idf tools are required only for flashing and are not
    needed for current CI jobs which only builds. This change allows
    CI to start building samples and tests for esp32.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- boards/common/esp32.board.cmake
+++ boards/common/esp32.board.cmake
@@ -1,7 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_set_flasher_ifnset(esp32)
-
 if(NOT DEFINED ESP_IDF_PATH)
   if(DEFINED ENV{ESP_IDF_PATH})
     message(WARNING "Setting ESP_IDF_PATH in the environment is deprecated. Use cmake -DESP_IDF_PATH=... instead.")
@@ -9,6 +7,9 @@ if(NOT DEFINED ESP_IDF_PATH)
   endif()
 endif()
 
-assert(ESP_IDF_PATH "ESP_IDF_PATH is not set")
-
-board_finalize_runner_args(esp32 "--esp-idf-path=${ESP_IDF_PATH}")
+if(DEFINED ESP_IDF_PATH)
+  board_set_flasher_ifnset(esp32)
+  board_finalize_runner_args(esp32 "--esp-idf-path=${ESP_IDF_PATH}")
+else()
+  message(WARNING "ESP_IDF_PATH is not set. Image flashing is not supported.")
+endif()
