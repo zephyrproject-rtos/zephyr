0990109f4506bd3f0bdc2b5e49f69c26a9c63b30
Erwan Gouriou <erwan.gouriou@linaro.org>
2020-05-13 08:43:26 +0200

    boards: stm32: pinmux: Restore Kconfig control on pinmux (dac)
    In order to avoid pin configuration conflicts between peripherals,
    add CONFIG_DAC flag to for each dac pinmux configuration.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- boards/arm/nucleo_l073rz/pinmux.c
+++ boards/arm/nucleo_l073rz/pinmux.c
@@ -34,7 +34,7 @@ static const struct pin_config pinconf[] = {
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(adc1), okay) && CONFIG_ADC
 	{STM32_PIN_PA0, STM32L0_PINMUX_FUNC_PA0_ADC_IN0},
 #endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(dac1), okay)
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(dac1), okay) && CONFIG_DAC
 	{STM32_PIN_PA4, STM32L0_PINMUX_FUNC_PA4_DAC_OUT1},
 #endif /* dac1 */
 };
