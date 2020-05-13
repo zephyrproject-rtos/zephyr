54a85d66e42e0fff740e122184d029da2cd7e713
Erwan Gouriou <erwan.gouriou@linaro.org>
2020-05-13 08:44:30 +0200

    boards: stm32: pinmux: Restore Kconfig control on pinmux (can)
    In order to avoid pin configuration conflicts between peripherals,
    add CONFIG_CAN flag to for each can pinmux configuration.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- boards/arm/olimexino_stm32/pinmux.c
+++ boards/arm/olimexino_stm32/pinmux.c
@@ -58,7 +58,7 @@ static const struct pin_config pinconf[] = {
 	{STM32_PIN_PA11, STM32F1_PINMUX_FUNC_PA11_USB_DM},
 	{STM32_PIN_PA12, STM32F1_PINMUX_FUNC_PA12_USB_DP},
 #endif /* CONFIG_USB_DC_STM32 */
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(can1), okay)
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(can1), okay) && CONFIG_CAN
 	{STM32_PIN_PB8, STM32F1_PINMUX_FUNC_PB8_CAN_RX},
 	{STM32_PIN_PB9, STM32F1_PINMUX_FUNC_PB9_CAN_TX},
 #endif
@@ -69,7 +69,7 @@ static int pinmux_stm32_init(struct device *port)
 	ARG_UNUSED(port);
 
 	stm32_setup_pins(pinconf, ARRAY_SIZE(pinconf));
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(can1), okay)
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(can1), okay) && CONFIG_CAN
 	/* Set pin-mux so that CAN1 is on PB8 and PB9 */
 	AFIO->MAPR |= AFIO_MAPR_CAN_REMAP_REMAP2;
 #endif
