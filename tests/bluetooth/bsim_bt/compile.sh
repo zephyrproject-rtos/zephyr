372a2e2fd3341f98be8e0f8ed1676ff69aeed5f8
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2020-05-14 11:48:17 +0530

    tests: Bluetooth: bsim_test: Include advx testing in CI
    Update compile.sh and added test_script, so that
    bsim_test_advx bsim_test application is built and run in
    CI.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- tests/bluetooth/bsim_bt/compile.sh
+++ tests/bluetooth/bsim_bt/compile.sh
@@ -55,5 +55,6 @@ app=tests/bluetooth/bsim_bt/bsim_test_app conf_file=prj_split.conf \
 	compile
 app=tests/bluetooth/bsim_bt/bsim_test_app conf_file=prj_split_privacy.conf \
   compile
+app=tests/bluetooth/bsim_bt/bsim_test_advx compile
 app=tests/bluetooth/bsim_bt/edtt_ble_test_app/hci_test_app compile
 app=tests/bluetooth/bsim_bt/edtt_ble_test_app/gatt_test_app compile
