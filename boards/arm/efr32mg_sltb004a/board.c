2872af1a935d6174657d940b614cceb92355cd54
Christian Taedcke <christian.taedcke@lemonbeat.com>
2020-05-15 17:44:20 +0200

    boards: efr32mg_sltb004a: Add support for sensor ccs811
    The on-board sensor has a enable pin that must be pulled
    high in order to power the sensor.
    Since no i2c sensor connected to ENV_I2C is supported by zephyr yet,
    changed the pins of i2c1 to be connected to CCS811_I2C pins.
    
    Signed-off-by: Christian Taedcke <christian.taedcke@lemonbeat.com>

---
+++ boards/arm/efr32mg_sltb004a/board.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020 Christian Taedcke
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+#include "board.h"
+#include <drivers/gpio.h>
+#include <sys/printk.h>
+
+static int efr32mg_sltb004a_init(struct device *dev)
+{
+	struct device *cur_dev;
+
+	ARG_UNUSED(dev);
+
+#ifdef CONFIG_CCS811
+	/* Enable the CCS811 power */
+	cur_dev = device_get_binding(CCS811_PWR_ENABLE_GPIO_NAME);
+	if (!cur_dev) {
+		printk("CCS811 power gpio port was not found!\n");
+		return -ENODEV;
+	}
+
+	gpio_pin_configure(cur_dev, CCS811_PWR_ENABLE_GPIO_PIN, GPIO_OUTPUT);
+	gpio_pin_set(cur_dev, CCS811_PWR_ENABLE_GPIO_PIN, 1);
+
+#endif /* CONFIG_CCS811 */
+
+	return 0;
+}
+
+/* needs to be done after GPIO driver init */
+SYS_INIT(efr32mg_sltb004a_init, PRE_KERNEL_1, CONFIG_BOARD_INIT_PRIORITY);
