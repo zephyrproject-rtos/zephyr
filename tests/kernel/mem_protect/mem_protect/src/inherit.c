2893aa0bc11e273bae39e6e7d9d39ec908cc072c
Maksim Masalski <maksim.masalski@intel.com>
2020-05-15 19:39:22 +0300

    tests: mem protection  new test inherit resource pool
    To improve Zephyr tests, I think that it will necessary to have test,
    that verifies a child thread inherits resource pool assignment
    of their parent.
    
    Signed-off-by: Maksim Masalski <maksim.masalski@intel.com>

--- tests/kernel/mem_protect/mem_protect/src/inherit.c
+++ tests/kernel/mem_protect/mem_protect/src/inherit.c
@@ -1,10 +1,11 @@
 /*
- * Copyright (c) 2017 Intel Corporation
+ * Copyright (c) 2020 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include "mem_protect.h"
+#include <syscall_handler.h>
 
 /* function prototypes */
 static inline void dummy_start(struct k_timer *timer)
@@ -18,11 +19,15 @@ static inline void dummy_end(struct k_timer *timer)
 
 /* Kernel objects */
 K_THREAD_STACK_DEFINE(test_1_stack, INHERIT_STACK_SIZE);
+K_THREAD_STACK_DEFINE(parent_thr_stack, STACK_SIZE);
+K_THREAD_STACK_DEFINE(child_thr_stack, STACK_SIZE);
+K_MEM_POOL_DEFINE(res_pool, BLK_SIZE_MIN, BLK_SIZE_MAX, BLK_NUM_MAX, BLK_ALIGN);
 K_SEM_DEFINE(inherit_sem, SEMAPHORE_INIT_COUNT, SEMAPHORE_MAX_COUNT);
+K_SEM_DEFINE(sync_sem, SEM_INIT_VAL, SEM_MAX_VAL);
 K_MUTEX_DEFINE(inherit_mutex);
 K_TIMER_DEFINE(inherit_timer, dummy_start, dummy_end);
 K_MSGQ_DEFINE(inherit_msgq, MSG_Q_SIZE, MSG_Q_MAX_NUM_MSGS, MSG_Q_ALIGN);
-struct k_thread test_1_tid;
+struct k_thread test_1_tid, parent_thr, child_thr;
 
 uint8_t MEM_DOMAIN_ALIGNMENT inherit_buf[MEM_REGION_ALLOC]; /* for mem domain */
 
@@ -99,4 +104,64 @@ void test_permission_inheritance(void *p1, void *p2, void *p3)
 			0, K_INHERIT_PERMS, K_NO_WAIT);
 
 	k_thread_join(&test_1_tid, K_FOREVER);
+}
+
+struct k_mem_pool *z_impl_ret_resource_pool_ptr(void)
+{
+	return _current->resource_pool;
+}
+
+static inline struct k_mem_pool *z_vrfy_ret_resource_pool_ptr(void)
+{
+	return z_impl_ret_resource_pool_ptr();
+}
+#include <syscalls/ret_resource_pool_ptr_mrsh.c>
+struct k_mem_pool *child_res_pool_ptr;
+struct k_mem_pool *parent_res_pool_ptr;
+
+void child_handler(void *p1, void *p2, void *p3)
+{
+	child_res_pool_ptr = ret_resource_pool_ptr();
+	k_sem_give(&sync_sem);
+}
+
+void parent_handler(void *p1, void *p2, void *p3)
+{
+	parent_res_pool_ptr = ret_resource_pool_ptr();
+	k_thread_create(&child_thr, child_thr_stack,
+			K_THREAD_STACK_SIZEOF(child_thr_stack),
+			child_handler,
+			NULL, NULL, NULL,
+			PRIORITY, 0, K_NO_WAIT);
+}
+
+/**
+ * @brief Test child thread inherits parent's thread resource pool
+ *
+ * @details - Create a resource pool res_pool for the parent thread.
+ * - Then special system call ret_resource_pool_ptr() returns pointer
+ *   to the resource pool of the current thread.
+ * - Call it in the parent_handler() and in the child_handler()
+ * - Then in the main test function test_inherit_resource_pool()
+ *   compare returned addresses
+ * - If the addresses are the same, it means that child thread inherited
+ *   resource pool of the parent's thread -test passed.
+ *
+ * @ingroup kernel_memprotect_tests
+ *
+ * @see k_thread_resource_pool_assign()
+ */
+void test_inherit_resource_pool(void *p1, void *p2, void *p3)
+{
+	k_sem_reset(&sync_sem);
+	k_thread_create(&parent_thr, parent_thr_stack,
+			K_THREAD_STACK_SIZEOF(parent_thr_stack),
+			parent_handler,
+			NULL, NULL, NULL,
+			PRIORITY, 0, K_NO_WAIT);
+	k_thread_resource_pool_assign(&parent_thr, &res_pool);
+	k_sem_take(&sync_sem, K_FOREVER);
+	zassert_true(parent_res_pool_ptr == child_res_pool_ptr,
+		     "Resource pool of the parent thread not inherited,"
+		     " by child thread");
 }
