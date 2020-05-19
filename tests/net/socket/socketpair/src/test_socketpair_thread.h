849b5180a3e50ef23d261d7da5e75da8ac27dbf2
Christopher Friedt <chrisfriedt@gmail.com>
2020-05-19 16:40:36 -0400

    tests: socket: socketpair: fix userspace thread permissions
    Kernel objects were being directly accessed without previously
    calling k_thread_access_grant().
    
    This change allows each test that requires an asynchronous
    event to send it to a common work queue with correct
    permissions.
    
    Signed-off-by: Christopher Friedt <chrisfriedt@gmail.com>

---
+++ tests/net/socket/socketpair/src/test_socketpair_thread.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020 Friedt Professional Engineering Services, Inc
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef TEST_SOCKETPAIR_THREAD_H_
+#define TEST_SOCKETPAIR_THREAD_H_
+
+#include <kernel.h>
+
+extern struct k_work_q test_socketpair_work_q;
+
+#endif /* TEST_SOCKETPAIR_THREAD_H_ */
