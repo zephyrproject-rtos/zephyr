1cd0ae814a0e4d91b32e30d345c98c52118804e8
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2020-05-19 12:47:29 +0300

    lib: os: fdtable: Add syscalls for POSIX read/write/close operations
    Fdtable is a kernel-level structure, so functions accessing it, like
    read(), write(), close(), should be syscalls. This conversion however
    emphasizes a difference between "syscall" and "C standard library
    function". C stdlib functions should be represented by just a
    standard linkage symbol, and can be used even without prototype.
    Syscall is however a special code sequence, which may require special
    attributes and thus a prototype for usage. To deal with these
    distinction, we define a convention that "sys_name" is a syscall,
    while "name" is a normal C function (implemented as a wrapper around
    syscall). (And we also need to define "_name" to interface with Newlib
    (actually, just "_write".))
    
    Fixes: #25407
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>

--- include/posix/unistd.h
+++ include/posix/unistd.h
@@ -22,7 +22,15 @@ extern "C" {
 #endif
 
 #ifdef CONFIG_POSIX_API
-/* File related operations */
+/* File related operations. Convention: "sys_name" is a syscall (needs
+ * prototype in this file for usage). "name" is a normal userspace
+ * function (implemented as a wrapper for syscall), usable even
+ * without prototype, per classical C handling. This distinction
+ * is however implemented on demand, based on the actual usecases seen.
+ */
+__syscall int sys_close(int file);
+__syscall ssize_t sys_write(int file, const void *buffer, size_t count);
+__syscall ssize_t sys_read(int file, void *buffer, size_t count);
 extern int close(int file);
 extern ssize_t write(int file, const void *buffer, size_t count);
 extern ssize_t read(int file, void *buffer, size_t count);
@@ -49,5 +57,9 @@ int usleep(useconds_t useconds);
 #ifdef __cplusplus
 }
 #endif
+
+#ifndef CONFIG_ARCH_POSIX
+#include <syscalls/unistd.h>
+#endif /* CONFIG_ARCH_POSIX */
 
 #endif	/* ZEPHYR_INCLUDE_POSIX_UNISTD_H_ */
