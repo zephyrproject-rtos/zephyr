1405dfd04de6fae9a48cca277ee5e8c92cf069ec
Jennifer Williams <jennifer.m.williams@intel.com>
2020-05-19 11:52:27 -0700

    drivers: uart: fix use of port / config info from const struct
    This driver was assigning to a field that was stored in a const
    structure. The issue was (mostly) resolved in af43e14 by moving
    the field to a RAM structure and in #25462 by preserving the
    original const qualifier. Moving the field was not implemented
    for PCIE=n and introduced an inline function for the helper
    macros that have otherwise been consistent.
    
    This commit further addresses the issue by moving the port field for
    PCIE=y and PCIE=n. It lets the get_port() workaround be refactored
    back to DEV_DATA() for consistency in the helper macros. The PCIE
    probe is moved to the init() function to avoid probing PCIE every
    time UART is being configured.
    
    Tested with qemu_x86 and qemu_x86_64 tests/drivers/uart_basic_api
    Tested with previous reproducable issue:
    west build -b up_squared tests/arch/x86/info/
    
    Fixes #25260 (partial)
    
    Signed-off-by: Jennifer Williams <jennifer.m.williams@intel.com>

--- drivers/serial/uart_ns16550.c
+++ drivers/serial/uart_ns16550.c
@@ -207,21 +207,30 @@ BUILD_ASSERT(IS_ENABLED(CONFIG_PCIE), "NS16550(s) in DT need CONFIG_PCIE");
 #define DEV_DATA(dev) \
 	((struct uart_ns16550_dev_data_t *)(dev)->driver_data)
 
-#define THR(dev) (get_port(dev) + REG_THR * UART_REG_ADDR_INTERVAL)
-#define RDR(dev) (get_port(dev) + REG_RDR * UART_REG_ADDR_INTERVAL)
+#define THR(dev) \
+	(DEV_DATA(dev)->devconf.port + REG_THR * UART_REG_ADDR_INTERVAL)
+#define RDR(dev) \
+	(DEV_DATA(dev)->devconf.port + REG_RDR * UART_REG_ADDR_INTERVAL)
 #define BRDL(dev) \
-	(get_port(dev) + REG_BRDL * UART_REG_ADDR_INTERVAL)
+	(DEV_DATA(dev)->devconf.port + REG_BRDL * UART_REG_ADDR_INTERVAL)
 #define BRDH(dev) \
-	(get_port(dev) + REG_BRDH * UART_REG_ADDR_INTERVAL)
-#define IER(dev) (get_port(dev) + REG_IER * UART_REG_ADDR_INTERVAL)
-#define IIR(dev) (get_port(dev) + REG_IIR * UART_REG_ADDR_INTERVAL)
-#define FCR(dev) (get_port(dev) + REG_FCR * UART_REG_ADDR_INTERVAL)
-#define LCR(dev) (get_port(dev) + REG_LCR * UART_REG_ADDR_INTERVAL)
-#define MDC(dev) (get_port(dev) + REG_MDC * UART_REG_ADDR_INTERVAL)
-#define LSR(dev) (get_port(dev) + REG_LSR * UART_REG_ADDR_INTERVAL)
-#define MSR(dev) (get_port(dev) + REG_MSR * UART_REG_ADDR_INTERVAL)
-#define DLF(dev) (get_port(dev) + REG_DLF)
-#define PCP(dev) (get_port(dev) + REG_PCP)
+	(DEV_DATA(dev)->devconf.port + REG_BRDH * UART_REG_ADDR_INTERVAL)
+#define IER(dev) \
+	(DEV_DATA(dev)->devconf.port + REG_IER * UART_REG_ADDR_INTERVAL)
+#define IIR(dev) \
+	(DEV_DATA(dev)->devconf.port + REG_IIR * UART_REG_ADDR_INTERVAL)
+#define FCR(dev) \
+	(DEV_DATA(dev)->devconf.port + REG_FCR * UART_REG_ADDR_INTERVAL)
+#define LCR(dev) \
+	(DEV_DATA(dev)->devconf.port + REG_LCR * UART_REG_ADDR_INTERVAL)
+#define MDC(dev) \
+	(DEV_DATA(dev)->devconf.port + REG_MDC * UART_REG_ADDR_INTERVAL)
+#define LSR(dev) (\
+	DEV_DATA(dev)->devconf.port + REG_LSR * UART_REG_ADDR_INTERVAL)
+#define MSR(dev) \
+	(DEV_DATA(dev)->devconf.port + REG_MSR * UART_REG_ADDR_INTERVAL)
+#define DLF(dev) (DEV_DATA(dev)->devconf.port + REG_DLF)
+#define PCP(dev) (DEV_DATA(dev)->devconf.port + REG_PCP)
 
 #define IIRC(dev) (DEV_DATA(dev)->iir_cache)
 
@@ -271,9 +280,7 @@ struct uart_ns16550_device_config {
 
 /** Device data structure */
 struct uart_ns16550_dev_data_t {
-#ifdef UART_NS16550_PCIE_ENABLED
 	struct uart_device_config devconf;
-#endif
 	struct uart_config uart_config;
 	struct k_spinlock lock;
 
@@ -289,17 +296,6 @@ struct uart_ns16550_dev_data_t {
 };
 
 static const struct uart_driver_api uart_ns16550_driver_api;
-
-static inline u32_t get_port(struct device *dev)
-{
-#ifdef UART_NS16550_PCIE_ENABLED
-	if (DEV_CFG(dev)->pcie) {
-		return DEV_DATA(dev)->devconf.port;
-	}
-#endif /* UART_NS16550_PCIE_ENABLED */
-
-	return DEV_CFG(dev)->devconf.port;
-}
 
 static void set_baud_rate(struct device *dev, u32_t baud_rate)
 {
@@ -344,18 +340,6 @@ static int uart_ns16550_configure(struct device *dev,
 
 	ARG_UNUSED(dev_data);
 	ARG_UNUSED(dev_cfg);
-
-#ifdef UART_NS16550_PCIE_ENABLED
-	if (dev_cfg->pcie) {
-		if (!pcie_probe(dev_cfg->pcie_bdf, dev_cfg->pcie_id)) {
-			ret = -EINVAL;
-			goto out;
-		}
-
-		dev_data->devconf.port = pcie_get_mbar(dev_cfg->pcie_bdf, 0);
-		pcie_set_cmd(dev_cfg->pcie_bdf, PCIE_CONF_CMDSTAT_MEM, true);
-	}
-#endif
 
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
 	dev_data->iir_cache = 0U;
@@ -484,7 +468,31 @@ static int uart_ns16550_config_get(struct device *dev, struct uart_config *cfg)
  */
 static int uart_ns16550_init(struct device *dev)
 {
+	struct uart_ns16550_dev_data_t * const dev_data = DEV_DATA(dev);
+	const struct uart_ns16550_device_config * const dev_cfg = DEV_CFG(dev);
+
 	int ret;
+
+	ARG_UNUSED(dev_data);
+	ARG_UNUSED(dev_cfg);
+
+#ifdef UART_NS16550_PCIE_ENABLED
+	if (dev_cfg->pcie) {
+		if (!pcie_probe(dev_cfg->pcie_bdf, dev_cfg->pcie_id)) {
+			ret = -EINVAL;
+		}
+
+		dev_data->devconf.port = pcie_get_mbar(dev_cfg->pcie_bdf, 0);
+		pcie_set_cmd(dev_cfg->pcie_bdf, PCIE_CONF_CMDSTAT_MEM, true);
+	} else
+#endif
+	{
+		/*
+ 		* Set port to DEV_DATA, but not overwrite for PCIE enabled
+ 		* set by pcie_get_mbar() via _configure() above
+ 		*/
+		DEV_DATA(dev)->devconf.port = DEV_CFG(dev)->devconf.port;
+	}
 
 	ret = uart_ns16550_configure(dev, &DEV_DATA(dev)->uart_config);
 	if (ret != 0) {
