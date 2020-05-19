636d6cd9cdb6648374bf906953d1a9b96f74aa7d
Flavio Ceolin <flavio.ceolin@intel.com>
2020-05-18 20:35:04 -0700

    samples: application_development: Fix for userspace
    This samples was defining a syscall but the whole machinery to this
    work was missing.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- samples/application_development/out_of_tree_driver/hello_world_module/zephyr/hello_world_driver.c
+++ samples/application_development/out_of_tree_driver/hello_world_module/zephyr/hello_world_driver.c
@@ -6,6 +6,7 @@
 
 #include "hello_world_driver.h"
 #include <zephyr/types.h>
+#include <syscall_handler.h>
 
 /**
  * This is a minimal example of an out-of-tree driver
@@ -29,6 +30,17 @@ static void print_impl(struct device *dev)
 
 	__ASSERT(data.foo == 5, "Device was not initialized!");
 }
+
+#ifdef CONFIG_USERSPACE
+static inline void z_vrfy_hello_world_print(struct device *dev)
+{
+	Z_OOPS(Z_SYSCALL_DRIVER_HELLO_WORLD(dev, print));
+
+	z_impl_hello_world_print(dev);
+}
+#include <syscalls/hello_world_print_mrsh.c>
+#endif /* CONFIG_USERSPACE */
+
 
 DEVICE_AND_API_INIT(hello_world, "CUSTOM_DRIVER",
 		    init, &data, NULL,
