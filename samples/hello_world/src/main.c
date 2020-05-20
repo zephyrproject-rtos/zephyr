84781a49cec9d7f3a1fded779d340f5de77e02ac
Gerson Fernando Budke <gerson.budke@ossystems.com.br>
2020-05-19 22:08:51 -0300

    samples: hello_world: Add MCUBoot confirm image
    Add boot_write_img_confirmed method to enforce image is valid.
    
    Signed-off-by: Gerson Fernando Budke <gerson.budke@ossystems.com.br>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -7,7 +7,20 @@
 #include <zephyr.h>
 #include <sys/printk.h>
 
+#include <dfu/mcuboot.h>
+
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	int ret = -1;
+
+	printk("Hello World! 2 %s\n", CONFIG_BOARD);
+
+	/* The image of application needed be confirmed */
+	printk("Confirming the boot image\n");
+	ret = boot_write_img_confirmed();
+	if (ret < 0) {
+		printk("Error to confirm the image\n");
+	} else {
+		printk("Image is OK\n");
+	}
 }
