24c1be3896a697fff279cefc147cf6a0867c2da7
Peter Bigot <peter.bigot@nordicsemi.no>
2020-05-20 11:11:53 -0500

    samples: nrf: system_off: demonstrate use of device pm to reduce power
    Currently system power management does not automatically control
    device power, and this example demonstrates high current draw due to
    the UART being enabled.  Enable device power management and
    demonstrate how to manually turn off greedy devices.
    
    Also remove the 60 s delay that confirms we successfully turned off
    the residency policy; that's not directly relevant to the purpose of
    the sample.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- samples/boards/nrf/system_off/src/main.c
+++ samples/boards/nrf/system_off/src/main.c
@@ -6,14 +6,20 @@
 
 #include <stdio.h>
 #include <zephyr.h>
+#include <device.h>
 #include <power/power.h>
 #include <hal/nrf_gpio.h>
+
+#define CONSOLE_LABEL DT_LABEL(DT_CHOSEN(zephyr_console))
 
 #define BUSY_WAIT_S 2U
 #define SLEEP_S 2U
 
 void main(void)
 {
+	int rc;
+	struct device *cons = device_get_binding(CONSOLE_LABEL);
+
 	printk("\n%s system off demo\n", CONFIG_BOARD);
 
 	/* Configure to generate PORT event (wakeup) on button 1 press. */
@@ -23,25 +29,31 @@ void main(void)
 			       NRF_GPIO_PIN_SENSE_LOW);
 
 	/* Prevent deep sleep (system off) from being entered on long
-	 * timeouts.
+	 * timeouts due to the default residency policy.
 	 */
 	sys_pm_ctrl_disable_state(SYS_POWER_STATE_DEEP_SLEEP_1);
 
 	printk("Busy-wait %u s\n", BUSY_WAIT_S);
 	k_busy_wait(BUSY_WAIT_S * USEC_PER_SEC);
 
+	printk("Busy-wait %u s with UART off\n", BUSY_WAIT_S);
+	rc = device_set_power_state(cons, DEVICE_PM_LOW_POWER_STATE, NULL, NULL);
+	k_busy_wait(BUSY_WAIT_S * USEC_PER_SEC);
+	rc = device_set_power_state(cons, DEVICE_PM_ACTIVE_STATE, NULL, NULL);
+
 	printk("Sleep %u s\n", SLEEP_S);
 	k_sleep(K_SECONDS(SLEEP_S));
 
-	printk("Sleep %u ms (deep sleep minimum)\n",
-	       CONFIG_SYS_PM_MIN_RESIDENCY_DEEP_SLEEP_1);
-	k_sleep(K_MSEC(CONFIG_SYS_PM_MIN_RESIDENCY_DEEP_SLEEP_1));
+	printk("Sleep %u s with UART off\n", SLEEP_S);
+	rc = device_set_power_state(cons, DEVICE_PM_LOW_POWER_STATE, NULL, NULL);
+	k_sleep(K_SECONDS(SLEEP_S));
+	rc = device_set_power_state(cons, DEVICE_PM_ACTIVE_STATE, NULL, NULL);
 
 	printk("Entering system off; press BUTTON1 to restart\n");
 
 	/* Above we disabled entry to deep sleep based on duration of
 	 * controlled delay.  Here we need to override that, then
-	 * force a sleep so that the deep sleep takes effect.
+	 * force entry to deep sleep on any delay.
 	 */
 	sys_pm_force_power_state(SYS_POWER_STATE_DEEP_SLEEP_1);
 	k_sleep(K_MSEC(1));
