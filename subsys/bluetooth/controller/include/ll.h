f2db274bc573959ec6364e57808b89a275d7987a
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2020-05-20 10:18:39 +0530

    Bluetooth: controller: Fix Periodic Adv Data Set parameter list
    Fix the parameter list of Periodic Advertising Data Set
    interface.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/include/ll.h
+++ subsys/bluetooth/controller/include/ll.h
@@ -35,8 +35,8 @@ uint8_t ll_adv_aux_set_remove(uint8_t handle);
 uint8_t ll_adv_aux_set_clear(void);
 uint8_t ll_adv_sync_param_set(uint8_t handle, uint16_t interval,
 			      uint16_t flags);
-uint8_t ll_adv_sync_ad_data_set(uint8_t handle, uint8_t op, uint8_t frag_pref,
-				uint8_t len, uint8_t const *const data);
+uint8_t ll_adv_sync_ad_data_set(uint8_t handle, uint8_t op, uint8_t len,
+				uint8_t const *const data);
 uint8_t ll_adv_sync_enable(uint8_t handle, uint8_t enable);
 #else /* !CONFIG_BT_CTLR_ADV_EXT */
 uint8_t ll_adv_params_set(uint16_t interval, uint8_t adv_type,
