6b857aaf31a17a620c83e162fe893556bff20f39
Flavio Ceolin <flavio.ceolin@intel.com>
2020-05-21 16:55:28 -0700

    debug: x86: Add gdbstub for X86
    It implements gdb remote protocol to talk with a host gdb during the
    debug session. The implementation is divided in three layers:
    
    1 - The top layer that is responsible for the gdb remote protocol.
    2 - An architecture specific layer responsible to write/read registers,
        set breakpoints, handle exceptions, ...
    3 - A transport layer to be used to communicate with the host
    
    The communication with GDB in the host is synchronous and the systems
    stops execution waiting for instructions and return its execution after
    a "continue" or "step" command. The protocol has an exception that is
    when the host sends a packet to cause an interruption, usually triggered
    by a Ctrl-C. This implementation ignores this instruction though.
    
    This initial work supports only X86 using uart as backend.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- arch/x86/core/ia32.cmake
+++ arch/x86/core/ia32.cmake
@@ -22,6 +22,7 @@ zephyr_library_sources(
 zephyr_library_sources_ifdef(CONFIG_IRQ_OFFLOAD		ia32/irq_offload.c)
 zephyr_library_sources_ifdef(CONFIG_X86_USERSPACE	ia32/userspace.S)
 zephyr_library_sources_ifdef(CONFIG_LAZY_FPU_SHARING	ia32/float.c)
+zephyr_library_sources_ifdef(CONFIG_GDBSTUB		ia32/gdbstub.c)
 
 zephyr_library_sources_ifdef(CONFIG_DEBUG_COREDUMP	ia32/coredump.c)
 
