8b285f3d5e702eeb056dfbf64db14cc9db7f1705
Tahir Akram <mtahirbutt@hotmail.com>
2020-05-21 17:05:07 -0700

    lib: adding support for modem
    Signed-off-by: Tahir Akram <mtahirbutt@hotmail.com>
    
    modem

--- samples/net/updatehub/src/main.c
+++ samples/net/updatehub/src/main.c
@@ -15,29 +15,6 @@
 #include "c_certificates.h"
 #endif
 
-
-#include <device.h>
-#include <drivers/gpio.h>
-
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
-
-#ifndef DT_ALIAS_LED0_GPIOS_FLAGS
-#define FLAGS 0
-#else
-#define FLAGS DT_ALIAS_LED0_GPIOS_FLAGS
-#endif
-
-/* Make sure the board's devicetree declares led0 in its /aliases. */
-#ifdef DT_ALIAS_LED0_GPIOS_CONTROLLER
-#define LED0	DT_ALIAS_LED0_GPIOS_CONTROLLER
-#define PIN	DT_ALIAS_LED0_GPIOS_PIN
-#else
-#error "Unsupported board: led0 devicetree alias is not defined"
-#define LED0	""
-#define PIN	0
-#endif
-
 LOG_MODULE_REGISTER(main);
 
 void main(void)
@@ -45,7 +22,6 @@ void main(void)
 	int ret = -1;
 
 	LOG_INF("UpdateHub sample app started");
-
 
 #if defined(CONFIG_UPDATEHUB_DTLS)
 	if (tls_credential_add(CA_CERTIFICATE_TAG,
@@ -62,7 +38,7 @@ void main(void)
 			       sizeof(private_key)) < 0) {
 		LOG_ERR("Failed to register private key");
 		return;
-	} 
+	}
 #endif
 
 	/* The image of application needed be confirmed */
@@ -75,7 +51,7 @@ void main(void)
 #if defined(CONFIG_UPDATEHUB_POLLING)
 	LOG_INF("Starting UpdateHub polling mode");
 	updatehub_autohandler();
-#endif 
+#endif
 
 #if defined(CONFIG_UPDATEHUB_MANUAL)
 	LOG_INF("Starting UpdateHub manual mode");
@@ -103,29 +79,6 @@ void main(void)
 	default:
 		LOG_ERR("Invalid response");
 		break;
-	} 
+	}
 #endif
-
-
-/*
-	struct device *dev;
-	bool led_is_on = true;
-
-	dev = device_get_binding(LED0);
-	if (dev == NULL) {
-		LOG_INF("No LED device found");
-		return;
-	}
-	LOG_INF("LED device found");
-	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
-	if (ret < 0) {
-		return;
-	}
-
-	while (1) {
-		gpio_pin_set(dev, PIN, (int)led_is_on);
-		led_is_on = !led_is_on;
-		k_sleep(SLEEP_TIME_MS);
-	}*/
 }
-
