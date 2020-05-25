d233f0c2ddd4906515e102657f8abb2382a0d85d
Rubin Gerritsen <rubin.gerritsen@nordicsemi.no>
2020-05-25 20:51:02 +0200

    nrf52_bsim: Add wrappers for some more CMSIS APIs
    This commit adds some more wrappers for commonly used CMSIS functions.
    
    Signed-off-by: Rubin Gerritsen <rubin.gerritsen@nordicsemi.no>

--- boards/posix/nrf52_bsim/irq_handler.c
+++ boards/posix/nrf52_bsim/irq_handler.c
@@ -13,6 +13,7 @@
 #include "kswap.h"
 #include "irq_ctrl.h"
 #include "posix_core.h"
+#include "posix_board_if.h"
 #include "board_soc.h"
 #include "sw_isr_table.h"
 #include "soc.h"
@@ -365,27 +366,61 @@ void posix_irq_offload(void (*routine)(void *), void *parameter)
 }
 #endif
 
-/**
- * Replacement for ARMs NVIC_SetPendingIRQ()
- *
- * Sets the interrupt IRQn as pending
- * Note:
- * This will interrupt immediately if the interrupt
- * is not masked and irqs are not locked, and this interrupt is higher
- * priority than a possibly currently running interrupt
+/*
+ *  Replacement for ARMs NVIC functions()
  */
 void NVIC_SetPendingIRQ(IRQn_Type IRQn)
 {
 	hw_irq_ctrl_raise_im_from_sw(IRQn);
 }
 
-/**
- *  Replacement for ARMs NVIC_ClearPendingIRQ()
- *  Clear pending interrupt IRQn
- */
 void NVIC_ClearPendingIRQ(IRQn_Type IRQn)
 {
 	hw_irq_ctrl_clear_irq(IRQn);
+}
+
+void NVIC_DisableIRQ(IRQn_Type IRQn)
+{
+	hw_irq_ctrl_disable_irq(IRQn);
+}
+
+void NVIC_EnableIRQ(IRQn_Type IRQn)
+{
+	hw_irq_ctrl_enable_irq(IRQn);
+}
+
+void NVIC_SetPriority(IRQn_Type IRQn, uint32_t priority)
+{
+	hw_irq_ctrl_prio_set(IRQn, priority);
+}
+
+uint32_t NVIC_GetPriority(IRQn_Type IRQn)
+{
+	return hw_irq_ctrl_get_prio(IRQn);
+}
+
+void NVIC_SystemReset(void)
+{
+	posix_print_warning("%s called. Exiting\n", __func__);
+	posix_exit(1);
+}
+
+/*
+ * Replacements for some other CMSIS functions
+ */
+void __enable_irq(void)
+{
+	hw_irq_ctrl_change_lock(true);
+}
+
+void __disable_irq(void)
+{
+	hw_irq_ctrl_change_lock(false);
+}
+
+uint32_t __get_PRIMASK(void)
+{
+	return hw_irq_ctrl_get_current_lock();
 }
 
 /*
@@ -402,6 +437,11 @@ void __WFE(void)
 		CPU_will_be_awaken_from_WFE = false;
 	}
 	CPU_event_set_flag = false;
+}
+
+void __WFI(void)
+{
+	__WFE();
 }
 
 void __SEV(void)
