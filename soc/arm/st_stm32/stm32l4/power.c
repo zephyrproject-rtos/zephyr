635b9632ef5744a0ea61c54508f5f8f14a525074
Francois Ramu <francois.ramu@st.com>
2020-05-26 13:53:06 +0200

    soc: arm: stm32 enable low power mode based on lptim
    Initialze the Low Power Timer to wakeUp from sleep mode.
    On timer expiration, after the given sleep time,
    the lptim irq is triggered and wakes Up the kernel.
    This is valid for sleep mode (stm32 low power 'stop').
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- soc/arm/st_stm32/stm32l4/power.c
+++ soc/arm/st_stm32/stm32l4/power.c
@@ -7,14 +7,37 @@
 #include <power/power.h>
 #include <soc.h>
 #include <init.h>
+#include <spinlock.h>
 
 #include <stm32l4xx_ll_bus.h>
 #include <stm32l4xx_ll_cortex.h>
 #include <stm32l4xx_ll_pwr.h>
 #include <stm32l4xx_ll_rcc.h>
+#include <stm32l4xx_ll_lptim.h>
+#include <stm32l4xx_ll_system.h>
 
 #include <logging/log.h>
 LOG_MODULE_DECLARE(soc, CONFIG_SOC_LOG_LEVEL);
+
+#ifdef CONFIG_SYS_PM_POLICY_RESIDENCY_STM32
+static struct k_spinlock lock;
+
+static void lptim_irq_handler(struct device *unused)
+{
+	ARG_UNUSED(unused);
+
+	if ((LL_LPTIM_IsActiveFlag_ARRM(LPTIM1) != 0)
+		&& LL_LPTIM_IsEnabledIT_ARRM(LPTIM1) != 0) {
+		/* when irq occurs, counter is already set to 0 */
+		k_spinlock_key_t key = k_spin_lock(&lock);
+
+		/* do not change ARR yet, z_clock_announce will do */
+		LL_LPTIM_ClearFLAG_ARRM(LPTIM1);
+
+		k_spin_unlock(&lock, key);
+	}
+}
+#endif /* CONFIG_SYS_PM_POLICY_RESIDENCY_STM32 */
 
 /* Invoke Low Power/System Off specific Tasks */
 void sys_set_power_state(enum power_states state)
@@ -23,7 +46,6 @@ void sys_set_power_state(enum power_states state)
 #ifdef CONFIG_SYS_POWER_SLEEP_STATES
 #ifdef CONFIG_HAS_SYS_POWER_STATE_SLEEP_1
 	case SYS_POWER_STATE_SLEEP_1:
-
 		/* this corresponds to the STOP0 mode: */
 #ifdef CONFIG_DEBUG
 		/* Enable the Debug Module during STOP mode */
@@ -112,6 +134,87 @@ void _sys_pm_power_state_exit_post_ops(enum power_states state)
 	irq_unlock(0);
 }
 
+#ifdef CONFIG_SYS_PM_POLICY_RESIDENCY_STM32
+/* Initialize STM32 LPTIM */
+static void stm32_lptim_init(void)
+{
+	/* enable LPTIM clock source */
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_LPTIM1);
+	LL_APB1_GRP1_ReleaseReset(LL_APB1_GRP1_PERIPH_LPTIM1);
+
+#ifdef CONFIG_STM32_LPTIM_CLOCK_LSI
+	/* enable LSI clock */
+#ifdef CONFIG_SOC_SERIES_STM32WBX
+	LL_RCC_LSI1_Enable();
+	while (!LL_RCC_LSI1_IsReady()) {
+#else
+	LL_RCC_LSI_Enable();
+	while (!LL_RCC_LSI_IsReady()) {
+#endif /* CONFIG_SOC_SERIES_STM32WBX */
+		/* Wait for LSI ready */
+	}
+
+	LL_RCC_SetLPTIMClockSource(LL_RCC_LPTIM1_CLKSOURCE_LSI);
+
+#else /* CONFIG_STM32_LPTIM_CLOCK_LSI */
+#ifdef LL_APB1_GRP1_PERIPH_PWR
+	/* Enable the power interface clock */
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+#endif /* LL_APB1_GRP1_PERIPH_PWR */
+	/* enable backup domain */
+	LL_PWR_EnableBkUpAccess();
+
+	/* enable LSE clock */
+	LL_RCC_LSE_DisableBypass();
+	LL_RCC_LSE_Enable();
+	while (!LL_RCC_LSE_IsReady()) {
+		/* Wait for LSE ready */
+	}
+	LL_RCC_SetLPTIMClockSource(LL_RCC_LPTIM1_CLKSOURCE_LSE);
+
+#endif /* CONFIG_STM32_LPTIM_CLOCK_LSI */
+
+	/* Clear the event flag and possible pending interrupt */
+	IRQ_CONNECT(DT_IRQN(DT_NODELABEL(lptim1)),
+		    DT_IRQ(DT_NODELABEL(lptim1), priority),
+		    lptim_irq_handler, 0, 0);
+	irq_enable(DT_IRQN(DT_NODELABEL(lptim1)));
+
+	/* configure the LPTIM1 counter */
+	LL_LPTIM_SetClockSource(LPTIM1, LL_LPTIM_CLK_SOURCE_INTERNAL);
+	/*
+	 * configure the LPTIM1 prescaler with default value of 8
+	 * this will allow sleep period from 0 to 16000ms
+	 */
+	LL_LPTIM_SetPrescaler(LPTIM1, LL_LPTIM_PRESCALER_DIV8);
+	LL_LPTIM_SetPolarity(LPTIM1, LL_LPTIM_OUTPUT_POLARITY_REGULAR);
+	LL_LPTIM_SetUpdateMode(LPTIM1, LL_LPTIM_UPDATE_MODE_IMMEDIATE);
+	LL_LPTIM_SetCounterMode(LPTIM1, LL_LPTIM_COUNTER_MODE_INTERNAL);
+	LL_LPTIM_DisableTimeout(LPTIM1);
+	/* counting start is initiated by software */
+	LL_LPTIM_TrigSw(LPTIM1);
+
+	/* LPTIM1 interrupt set-up before enabling */
+	/* no Compare match Interrupt */
+	LL_LPTIM_DisableIT_CMPM(LPTIM1);
+	LL_LPTIM_ClearFLAG_CMPM(LPTIM1);
+
+	/* Autoreload match Interrupt */
+	LL_LPTIM_EnableIT_ARRM(LPTIM1);
+	LL_LPTIM_ClearFLAG_ARRM(LPTIM1);
+	/* ARROK bit validates the write operation to ARR register */
+	LL_LPTIM_ClearFlag_ARROK(LPTIM1);
+	LL_LPTIM_DisableIT_ARROK(LPTIM1);
+#ifdef CONFIG_DEBUG
+	/* stop LPTIM1 during DEBUG */
+	LL_DBGMCU_APB1_GRP1_FreezePeriph(LL_DBGMCU_APB1_GRP1_LPTIM1_STOP);
+#endif
+	/*  Enable the LPTIM1 counter */
+	LL_LPTIM_Enable(LPTIM1);
+
+}
+#endif /* CONFIG_SYS_PM_POLICY_RESIDENCY_STM32 */
+
 /* Initialize STM32 Power */
 static int stm32_power_init(struct device *dev)
 {
@@ -124,6 +227,10 @@ static int stm32_power_init(struct device *dev)
 	/* enable Power clock */
 	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
 
+	/* LPTIM initalization */
+	if (IS_ENABLED(CONFIG_SYS_PM_POLICY_RESIDENCY_STM32)) {
+		stm32_lptim_init();
+	}
 	irq_unlock(ret);
 
 	return 0;
