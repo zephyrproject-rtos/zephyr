7bcc9f7b09514ca1e9d160b87146dc02049f9e12
Kumar Gala <kumar.gala@linaro.org>
2020-05-27 14:59:11 -0500

    stdint.h: Make {u}int64_t types consistent regardless of arch
    On 64-bit arch's like AARCH64, x86_64, or riscv-64 the way {u}int64_t
    will differ from how its defined on 32-bit arch's.  Do the same trick we
    do for {u}int32_t for {u}int64_t so that the type is the same regardless
    of what we are building for.
    
    This keeps things like %lld working the same regardless of 32-bit or
    64-bit arch.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- include/toolchain/zephyr_stdint.h
+++ include/toolchain/zephyr_stdint.h
@@ -24,10 +24,14 @@
 #undef __UINT32_TYPE__
 #undef __INT_LEAST32_TYPE__
 #undef __UINT_LEAST32_TYPE__
+#undef __INT64_TYPE__
+#undef __UINT64_TYPE__
 #define __INT32_TYPE__ int
 #define __UINT32_TYPE__ unsigned int
 #define __INT_LEAST32_TYPE__ __INT32_TYPE__
 #define __UINT_LEAST32_TYPE__ __UINT32_TYPE__
+#define __INT64_TYPE__ long long int
+#define __UINT64_TYPE__ unsigned long long int
 
 /*
  * The confusion also exists with __INTPTR_TYPE__ which is either an int
