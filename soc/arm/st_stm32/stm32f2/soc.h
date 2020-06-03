b10e95181a0747ecc21ebb92f493743136c622a4
Francois Ramu <francois.ramu@st.com>
2020-06-03 10:13:05 +0200

    soc: arm: stm32f2 add rtc feature on this serie
    This patch enables the rtc on the stm32f2 soc series
    from STMicroelectronics
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- soc/arm/st_stm32/stm32f2/soc.h
+++ soc/arm/st_stm32/stm32f2/soc.h
@@ -54,6 +54,12 @@
 #include <stm32f2xx_ll_wwdg.h>
 #endif
 
+#if defined(CONFIG_COUNTER_RTC_STM32)
+#include <stm32f2xx_ll_rtc.h>
+#include <stm32f2xx_ll_exti.h>
+#include <stm32f2xx_ll_pwr.h>
+#endif
+
 #ifdef CONFIG_ADC_STM32
 #include <stm32f2xx_ll_adc.h>
 #endif
