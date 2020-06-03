f4aa2eaa0b2c71e892ce95f6d85dfb9459f91f93
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2020-06-03 16:21:52 +0200

    [nrf noup] flash_map: remove PM related code
    Partition_manager related code was moved to
    flash_map_pm.h file in the nrf repository.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

--- include/storage/flash_map.h
+++ include/storage/flash_map.h
@@ -210,36 +210,7 @@ int flash_area_has_driver(const struct flash_area *fa);
 struct device *flash_area_get_device(const struct flash_area *fa);
 
 #if USE_PARTITION_MANAGER
-#include <pm_config.h>
-#include <sys/util.h>
-
-/* Aliases for zephyr - mcuboot/ncs style naming */
-#define image_0 mcuboot_primary
-#define image_1 mcuboot_secondary
-
-#if (CONFIG_SETTINGS_FCB || CONFIG_SETTINGS_NVS)
-#define storage settings_storage
-#elif CONFIG_FILE_SYSTEM_LITTLEFS
-#define storage littlefs_storage
-#endif
-
-#if (CONFIG_SETTINGS_FCB || CONFIG_SETTINGS_NVS) && CONFIG_FILE_SYSTEM_LITTLEFS
-#error "Not supported"
-#endif
-
-#define PM_ID(label) PM_##label##_ID
-
-#define FLASH_AREA_ID(label) PM_ID(label)
-
-#define FLASH_AREA_OFFSET(label) \
-	UTIL_CAT(PM_, UTIL_CAT(UTIL_CAT(PM_, UTIL_CAT(PM_ID(label), _LABEL)), _OFFSET))
-
-#define FLASH_AREA_SIZE(label) \
-	UTIL_CAT(PM_, UTIL_CAT(UTIL_CAT(PM_, UTIL_CAT(PM_ID(label), _LABEL)), _SIZE))
-
-#define FLASH_AREA_LABEL_EXISTS(label) \
-	IS_ENABLED(FLASH_AREA_SIZE(label))
-
+#include <flash_map_pm.h>
 #else
 
 #define FLASH_AREA_LABEL_EXISTS(label) \
