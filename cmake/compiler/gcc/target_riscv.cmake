f6a67ecc2f13c7c1efa1c47c32897b1e08e269d5
Stephanos Ioannidis <root@stephanos.io>
2020-06-16 16:34:43 +0900

    cmake: compiler: gcc: Fix linker flags for RISC-V
    The `-mabi`, `-march` and `-mcmodel` flags must be passed to the
    linker in order to facilitate proper selection of the target multilib.
    
    This commit also addresses the issue in which the `-mabi` and `-march`
    flags are omitted for linking because `target_ld_options` evaluates the
    two flags separately and they must be used in tandem in order for
    compilation to succeed.
    
    Note that this issue went unnoticed because other toolchain specs have
    a hack in which the target library directory is directly added to the
    library path using the `-L` option.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- cmake/compiler/gcc/target_riscv.cmake
+++ cmake/compiler/gcc/target_riscv.cmake
@@ -7,6 +7,7 @@ if(CONFIG_64BIT)
     string(CONCAT riscv_mabi  ${riscv_mabi} "64")
     string(CONCAT riscv_march ${riscv_march} "64ima")
     list(APPEND TOOLCHAIN_C_FLAGS -mcmodel=medany)
+    list(APPEND TOOLCHAIN_LD_FLAGS -mcmodel=medany)
 else()
     string(CONCAT riscv_mabi  "i" ${riscv_mabi} "32")
     string(CONCAT riscv_march ${riscv_march} "32ima")
@@ -31,4 +32,9 @@ if(CONFIG_COMPRESSED_ISA)
 endif()
 
 list(APPEND TOOLCHAIN_C_FLAGS -mabi=${riscv_mabi} -march=${riscv_march})
-list(APPEND TOOLCHAIN_LD_FLAGS -mabi=${riscv_mabi} -march=${riscv_march})
+
+# The double quotes below ensure that the `-mabi` and `-march` flags are
+# evaluated together by `target_ld_options`. This is necessary because
+# specifying only either one of the flags will cause compilation to fail and
+# that will effectively omit these flags.
+list(APPEND TOOLCHAIN_LD_FLAGS "-mabi=${riscv_mabi} -march=${riscv_march}")
