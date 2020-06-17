3f5a32a7d4f572d9ba0e55aa0cdbb975d8cf476c
the BASTION <thebasti0ncode@gmail.com>
2020-06-17 14:26:14 +0800

    boards: arm: add ruuvi_ruuvitag
    Adds ARM based Ruuvtag baord. This board is based on the nRF52832.
    
    Signed-off-by: the BASTION <thebasti0ncode@gmail.com>

---
+++ boards/arm/ruuvi_ruuvitag/board.cmake
@@ -0,0 +1,7 @@
+# Copyright (c) 2020 Ruuvi Innovations Ltd (Oy)
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(nrfjprog "--nrf-family=NRF52")
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
