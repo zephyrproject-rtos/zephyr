369f64f6ff0931d6932a8185e1572051a0391937
Jordan Yates <jordan.yates@data61.csiro.au>
2020-06-18 17:40:50 +1000

    sensor: iis2mdc: Add chip select flags
    Adds the chip select devicetree flags to the spi_cs_control instance.
    
    Signed-off-by: Jordan Yates <jordan.yates@data61.csiro.au>

--- drivers/sensor/iis2mdc/iis2mdc.c
+++ drivers/sensor/iis2mdc/iis2mdc.c
@@ -273,6 +273,7 @@ static const struct iis2mdc_config iis2mdc_dev_config = {
 #if DT_INST_SPI_DEV_HAS_CS_GPIOS(0)
 	.gpio_cs_port	    = DT_INST_SPI_DEV_CS_GPIOS_LABEL(0),
 	.cs_gpio	    = DT_INST_SPI_DEV_CS_GPIOS_PIN(0),
+	.cs_gpio_flags	    = DT_INST_SPI_DEV_CS_GPIOS_LABEL(0),
 
 	.spi_conf.cs        =  &iis2mdc_data.cs_ctrl,
 #else
