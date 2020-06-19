624fcdb82e2e49a264a537df289ea4e72a75a39f
Gerson Fernando Budke <nandojve@gmail.com>
2020-06-19 14:34:49 -0300

    samples: net: cloud: Introduce TagoIO IoT cloud http post
    Introduce TagoIO IoT Cloud HTTP post client example.  This explorer
    Zephyr network resources to demonstrate an end to end application.
    The TagoIO allows that any user can test on a easy way Ethernet, WIFI
    and Modem (PPP) with BSD sockets.  The example provides overlays to
    configure WIFI and Modem.
    
    The application consists an a pseudo temperature sensor that sends
    periodically data to TagoIO IoT Cloud platform.  The data can be
    visualized on a web browser dashboard, cellphone or tablet.  The
    steps to configure TagoIO are described on the example documentation.
    
    Special Variables:
     - CONFIG_TAGOIO_DEVICE_TOKEN   DEVID  token generated by TagoIO
     - CONFIG_TAGOIO_HTTP_WIFI_SSID SSID   when using WIFI
     - CONFIG_TAGOIO_HTTP_WIFI_PSK  PASSWD when using WIFI
     - CONFIG_MODEM_GSM_UART_NAME   UART   label when using MODEM
     - CONFIG_MODEM_GSM_APN         APN    when using MODEM
    
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

---
+++ samples/net/cloud/tagoio_http_post/src/wifi.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2020 Gerson Fernando Budke <nandojve@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <logging/log.h>
+LOG_MODULE_DECLARE(tagoio_http_post, CONFIG_TAGOIO_HTTP_POST_LOG_LEVEL);
+
+#include <net/wifi_mgmt.h>
+
+static int connected;
+static struct net_mgmt_event_callback wifi_shell_mgmt_cb;
+
+static void handle_wifi_connect_result(struct net_mgmt_event_callback *cb)
+{
+	const struct wifi_status *status = (const struct wifi_status *)
+					   cb->info;
+
+	if (status->status) {
+		LOG_ERR("Connection request failed (%d)", status->status);
+	} else {
+		LOG_INF("WIFI Connected");
+		connected = 1;
+	}
+}
+
+static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb,
+				    uint32_t mgmt_event, struct net_if *iface)
+{
+	switch (mgmt_event) {
+	case NET_EVENT_WIFI_CONNECT_RESULT:
+		handle_wifi_connect_result(cb);
+		break;
+	default:
+		break;
+	}
+}
+
+void wifi_connect(void)
+{
+	net_mgmt_init_event_callback(&wifi_shell_mgmt_cb,
+				     wifi_mgmt_event_handler,
+				     NET_EVENT_WIFI_CONNECT_RESULT);
+
+	net_mgmt_add_event_callback(&wifi_shell_mgmt_cb);
+
+	struct net_if *iface = net_if_get_default();
+	static struct wifi_connect_req_params cnx_params = {
+		.ssid = CONFIG_TAGOIO_HTTP_WIFI_SSID,
+		.ssid_length = 0,
+		.psk = CONFIG_TAGOIO_HTTP_WIFI_PSK,
+		.psk_length = 0,
+		.channel = 0,
+		.security = WIFI_SECURITY_TYPE_PSK,
+	};
+
+	cnx_params.ssid_length = strlen(CONFIG_TAGOIO_HTTP_WIFI_SSID);
+	cnx_params.psk_length = strlen(CONFIG_TAGOIO_HTTP_WIFI_PSK);
+
+	connected = 0;
+
+	LOG_INF("WIFI try connecting to %s...", CONFIG_TAGOIO_HTTP_WIFI_SSID);
+
+	if (net_mgmt(NET_REQUEST_WIFI_CONNECT, iface,
+		     &cnx_params, sizeof(struct wifi_connect_req_params))) {
+		return;
+	}
+
+	while (connected == 0) {
+		k_msleep(100);
+	};
+}
