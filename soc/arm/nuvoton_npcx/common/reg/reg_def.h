a28e2e5650c0ba9edb6eb8b42f7374394d875935
Mulin Chao <mlchao@nuvoton.com>
2020-06-24 19:13:43 +0800

    soc: arm: Add Nuvoton NPCX7M6FB SoC
    Initial support for Nuvoton NPCX7M6FB SoC of NPCX series which is a chip
    family of embedded controllers (EC) and targeted for a wide range of
    portable applications. We implemented the SoC skeleton in
    soc/arm/nuvoton_npcx since there're many chip families in Nuvoton and
    aim to different markets such as PC, General MCU, and Audio. The
    architectures and hardware modules are different between them. Hence, we
    suggest using the company name plus with chip series for better
    understanding.
    
    Signed-off-by: Mulin Chao <mlchao@nuvoton.com>

---
+++ soc/arm/nuvoton_npcx/common/reg/reg_def.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 Nuvoton Technology Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _NUVOTON_NPCX_REG_DEF_H
+#define _NUVOTON_NPCX_REG_DEF_H
+
+#endif /* _NUVOTON_NPCX_REG_DEF_H */
+
+
