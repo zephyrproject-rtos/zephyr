58488f37412dd8530a1e5c30a60e4f8696d59ace
Gerson Fernando Budke <nandojve@gmail.com>
2020-07-03 19:01:07 -0300

    boards: arm: atsamr21_xpro: Add PWM support
    This enables PWM and connects it to the LED0.  Tested with blinky_pwm
    and fade_led samples.
    
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

--- boards/arm/atsamr21_xpro/pinmux.c
+++ boards/arm/atsamr21_xpro/pinmux.c
@@ -95,6 +95,13 @@ static int board_pinmux_init(struct device *dev)
 #warning Pin mapping may not be configured
 #endif
 
+#if (ATMEL_SAM0_DT_TCC_CHECK(0, atmel_sam0_tcc_pwm) && \
+	defined(CONFIG_PWM_SAM0_TCC))
+
+	/* TCC0 on WO3=PA19 */
+	pinmux_pin_set(muxa, 19, PINMUX_FUNC_F);
+#endif
+
 #ifdef CONFIG_USB_DC_SAM0
 	/* USB DP on PA25, USB DM on PA24 */
 	pinmux_pin_set(muxa, 25, PINMUX_FUNC_G);
