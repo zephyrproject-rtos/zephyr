585d6d806ce72a77a922f1e952a0337a3430c09a
Abhishek Shah <abhishek.shah@broadcom.com>
2020-07-06 01:46:51 +0530

    soc: arm: viper: Add PCIe highmem OB memory in MMU table
    Add and mark PCIe highmem outbound memory as nGnRnE device memory
    in Viper SoC MMU configuration.
    Increase VA/PA bits to 36-bits to support the same.
    
    Signed-off-by: Abhishek Shah <abhishek.shah@broadcom.com>

--- soc/arm/bcm_vk/viper/mmu_regions.c
+++ soc/arm/bcm_vk/viper/mmu_regions.c
@@ -3,14 +3,25 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+#include <devicetree.h>
 #include <soc.h>
 #include <sys/util.h>
 #include <arch/arm/aarch64/arm_mmu.h>
+
+
+#define PCIE_OB_HIGHMEM_ADDR	DT_REG_ADDR_BY_NAME(DT_NODELABEL(pcie0_ep), \
+						    map_highmem)
+#define PCIE_OB_HIGHMEM_SIZE	DT_REG_SIZE_BY_NAME(DT_NODELABEL(pcie0_ep), \
+						    map_highmem)
 
 static const struct arm_mmu_region mmu_regions[] = {
 
 	MMU_REGION_FLAT_ENTRY("DEVICE_REGION",
 			      0x40000000, MB(512),
+			      MT_DEVICE_nGnRnE | MT_RW | MT_SECURE),
+
+	MMU_REGION_FLAT_ENTRY("PCIE_HIGH_OBMEM",
+			      PCIE_OB_HIGHMEM_ADDR, PCIE_OB_HIGHMEM_SIZE,
 			      MT_DEVICE_nGnRnE | MT_RW | MT_SECURE),
 
 	MMU_REGION_FLAT_ENTRY("DRAM0_S0",
