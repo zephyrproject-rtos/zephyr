75733dc3d823214118327739b7c2e2fcf1772c77
Andries Kruithof <Andries.Kruithof@nordicsemi.no>
2020-07-08 14:46:03 +0200

    [TOPIC-BLE-LLCP] Bluetooth: controller: llcp: create unique log names
    The module names for logging must be unique for each module,
    otherwise the linker will complain about duplicate symbols
    
    Signed-off-by: Andries Kruithof <Andries.Kruithof@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_llcp_enc.c
+++ subsys/bluetooth/controller/ll_sw/ull_llcp_enc.c
@@ -29,7 +29,7 @@
 #include "ull_llcp_internal.h"
 
 #define BT_DBG_ENABLED IS_ENABLED(CONFIG_BT_DEBUG_HCI_DRIVER)
-#define LOG_MODULE_NAME bt_ctlr_ull_llcp
+#define LOG_MODULE_NAME bt_ctlr_ull_llcp_enc
 #include "common/log.h"
 #include <soc.h>
 #include "hal/debug.h"
