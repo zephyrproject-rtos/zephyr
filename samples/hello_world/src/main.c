97eb38421b993ee6f6ff196a959712474409ab48
Andrew Boie <andrew.p.boie@intel.com>
2020-07-08 11:22:07 -0700

    hello world
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,8 +6,10 @@
 
 #include <zephyr.h>
 #include <sys/printk.h>
+#include <x86_mmu.h>
 
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+	z_x86_dump_page_tables(&z_x86_kernel_ptables);
 }
