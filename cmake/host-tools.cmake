ff9321842e67df160905673e65129bdeb4a77eb8
Wentong Wu <wentong.wu@intel.com>
2020-07-09 09:56:58 +0800

    cmake: host-tools fixes for DTC
    1. Fix the regular expression used to parse version numbers. This uses
    \. and . to parse literal dots; the second one matches any character.
    Just use [.] instead in both cases.
    
    2. Don't error out if the installed dtc is too old. It's optional, so
    we should just proceed without it. Print a warning instead, and make
    it so dts.cmake won't do anything with the installed dtc.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- cmake/host-tools.cmake
+++ cmake/host-tools.cmake
@@ -57,21 +57,24 @@ if(${DTC} STREQUAL DTC-NOTFOUND)
   message(FATAL_ERROR "Unable to find dtc")
 endif()
 
-# Parse the 'dtc --version' and make sure it is at least MIN_DTC_VERSION
+# Parse the 'dtc --version' output to find the installed version.
 set(MIN_DTC_VERSION 1.4.6)
 execute_process(
   COMMAND
   ${DTC} --version
   OUTPUT_VARIABLE dtc_version_output
   )
-string(REGEX MATCH "Version: DTC ([0-9]+\.[0-9]+.[0-9]+).*" out_var ${dtc_version_output})
-if(${CMAKE_MATCH_1} VERSION_LESS ${MIN_DTC_VERSION})
-  assert(0 "The detected dtc version is unsupported.                                 \n\
-    The version was found to be ${CMAKE_MATCH_1}                                   \n\
-    But the minimum supported version is ${MIN_DTC_VERSION}                        \n\
-    See https://docs.zephyrproject.org/latest/getting_started/                     \n\
-    for how to use the SDK's dtc alongside a custom toolchain."
-  )
+string(REGEX MATCH "Version: DTC ([0-9]+[.][0-9]+[.][0-9]+).*" out_var ${dtc_version_output})
+
+# Since it is optional, an outdated version is not an error. If an
+# outdated version is discovered, print a warning and proceed as if
+# DTC were not installed.
+if(${CMAKE_MATCH_1} VERSION_GREATER ${MIN_DTC_VERSION})
+  message(STATUS "Found dtc: ${DTC} (found suitable version \"${CMAKE_MATCH_1}\", minimum required is \"${MIN_DTC_VERSION}\")")
+else()
+  message(WARNING
+    "Could NOT find dtc: Found unsuitable version \"${CMAKE_MATCH_1}\", but required is at least \"${MIN_DTC_VERSION}\" (found ${DTC}). Optional devicetree error checking with dtc will not be performed.")
+  set(DTC DTC-NOTFOUND)
 endif()
 
 find_program(
