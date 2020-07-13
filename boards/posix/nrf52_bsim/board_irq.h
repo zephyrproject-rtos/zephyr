fd6fbe9c5f8b7bcad745c9c2f29970d733c6777a
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2020-07-13 09:52:38 +0200

    boards: Apply IRQ offload API change
    Switching to constant parameter on ISR handler.
    
    Fixes #27399
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- boards/posix/nrf52_bsim/board_irq.h
+++ boards/posix/nrf52_bsim/board_irq.h
@@ -15,9 +15,10 @@
 extern "C" {
 #endif
 
-void posix_isr_declare(unsigned int irq_p, int flags, void isr_p(void *),
-		       void *isr_param_p);
-void posix_irq_priority_set(unsigned int irq, unsigned int prio, uint32_t flags);
+void posix_isr_declare(unsigned int irq_p, int flags, void isr_p(const void *),
+		       const void *isr_param_p);
+void posix_irq_priority_set(unsigned int irq, unsigned int prio,
+			    uint32_t flags);
 
 /**
  * Configure a static interrupt.
@@ -42,8 +43,8 @@ void posix_irq_priority_set(unsigned int irq, unsigned int prio, uint32_t flags)
  */
 #define ARCH_IRQ_DIRECT_CONNECT(irq_p, priority_p, isr_p, flags_p) \
 { \
-	posix_isr_declare(irq_p, ISR_FLAG_DIRECT, (void (*)(void *))isr_p, \
-			  NULL); \
+	posix_isr_declare(irq_p, ISR_FLAG_DIRECT, \
+			  (void (*)(const void *))isr_p, NULL); \
 	posix_irq_priority_set(irq_p, priority_p, flags_p); \
 }
 
