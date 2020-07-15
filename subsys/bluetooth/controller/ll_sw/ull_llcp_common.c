89e4fa3aa31630b9ae585909c6a99ea8b537e1e1
Andries Kruithof <Andries.Kruithof@nordicsemi.no>
2020-07-15 15:37:24 +0200

    [TOPIC-BLE-LLCP] Bluetooth: controller: allow multiple version requests
    At the linked layer level we are only allowed to send one VERSION_IND
    per connection, but the host may request version information
    multiple times.
    We need to ensure that we fill out data correctly in case of
    multiple requests
    
    Signed-off-by: Andries Kruithof <Andries.Kruithof@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_llcp_common.c
+++ subsys/bluetooth/controller/ll_sw/ull_llcp_common.c
@@ -275,6 +275,7 @@ static void lp_comm_send_req(struct ull_cp_conn *conn, struct proc_ctx *ctx, u8_
 				ctx->state = LP_COMMON_STATE_WAIT_RX;
 			}
 		} else {
+			ctx->response_opcode = PDU_DATA_LLCTRL_TYPE_VERSION_IND;
 			lp_comm_complete(conn, ctx, evt, param);
 		}
 		break;
