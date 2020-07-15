753b39f8efcb8f7573c50b18e386acaca59e1e9c
Daniel Leung <daniel.leung@intel.com>
2020-07-15 13:57:36 -0700

    benchmarks: add an app for commonly used kernel funcs footprint
    This adds an app which utilizes common kernel functions as a
    starting point to gauge kernel footprint.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

---
+++ tests/benchmarks/footprints/src/libc.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <kernel.h>
+#include <zephyr.h>
+#include <ksched.h>
+
+#include "footprint.h"
+
+static const char const_string[] = "String!\n";
+static char new_string[32];
+
+void run_libc(void)
+{
+	int len;
+
+	len = strlen(const_string);
+	len = strnlen(const_string, len);
+
+	memset(new_string, 0, sizeof(new_string));
+	if (memcmp(const_string, new_string, 0) != 0) {
+		/* avoid unused return error */
+	}
+	if (memcmp(const_string, new_string, len) != 0) {
+		/* avoid unused return error */
+	}
+
+	strcpy(new_string, const_string);
+	strncpy(new_string, const_string, len);
+}
---
+++ tests/benchmarks/footprints/src/semaphore.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2017-2020 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <kernel.h>
+#include <zephyr.h>
+#include <ksched.h>
+
+#include "footprint.h"
+
+#define STACK_SIZE	512
+
+K_SEM_DEFINE(semaphore0, 0, 1);
+
+void thread_fn(void *p1, void *p2, void *p3)
+{
+	k_sem_give(&semaphore0);
+
+	k_sem_take(&semaphore0, K_FOREVER);
+}
+
+void run_semaphore(void)
+{
+	k_tid_t sem0_tid;
+	struct k_sem sem0;
+
+	k_sem_init(&sem0, 0, 1);
+
+	k_sem_give(&sem0);
+
+	k_sem_take(&sem0, K_FOREVER);
+
+	sem0_tid = k_thread_create(&my_thread, my_stack_area,
+				   STACK_SIZE, thread_fn,
+				   NULL, NULL, NULL,
+				   0, 0, K_NO_WAIT);
+
+	k_thread_join(sem0_tid, K_FOREVER);
+
+#ifdef CONFIG_USERSPACE
+	sem0_tid = k_thread_create(&my_thread, my_stack_area,
+				   STACK_SIZE, thread_fn,
+				   NULL, NULL, NULL,
+				   0, K_USER, K_FOREVER);
+
+	k_object_access_grant(&semaphore0, sem0_tid);
+
+	k_thread_start(sem0_tid);
+	k_thread_join(sem0_tid, K_FOREVER);
+#endif
+}
