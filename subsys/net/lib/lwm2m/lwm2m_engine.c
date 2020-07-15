00d12041f603364a0f04a4ce1894adc59cc527a2
Robert Lubos <robert.lubos@nordicsemi.no>
2020-07-15 14:32:51 +0200

    net: lwm2m: Initialize CoAP response on bootstrap finish
    During the bootstrap procedure, when Boostrap Finish was received, the
    response message was not initialized properly, resulting in a socket
    error (NULL pointer porovided) and the response not being sent.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/net/lib/lwm2m/lwm2m_engine.c
+++ subsys/net/lib/lwm2m/lwm2m_engine.c
@@ -3353,6 +3353,12 @@ static int handle_request(struct coap_packet *request,
 	} else if ((code & COAP_REQUEST_MASK) == COAP_METHOD_POST && r == 1 && \
 		   strncmp(options[0].value, "bs", options[0].len) == 0) {
 		engine_bootstrap_finish();
+
+		r = lwm2m_init_message(msg);
+		if (r < 0) {
+			goto error;
+		}
+
 		return 0;
 #endif
 	} else {
