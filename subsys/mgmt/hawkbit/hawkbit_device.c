ddd6a650e227056153af11f67ec57bb76722b1c1
NavinSankar Velliangiri <navin@linumiz.com>
2020-07-24 22:54:49 +0530

    mgmt: hawkbit: Add Hawkbit FOTA Support
    Add Hawkbit FOTA support
    
    Signed-off-by: NavinSankar Velliangiri <navin@linumiz.com>

---
+++ subsys/mgmt/hawkbit/hawkbit_device.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020 Linumiz
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include "hawkbit_device.h"
+#include <string.h>
+
+bool hawkbit_get_device_identity(char *id, int id_max_len)
+{
+	uint8_t hwinfo_id[DEVICE_ID_BIN_MAX_SIZE];
+	size_t length;
+
+	length = hwinfo_get_device_id(hwinfo_id, DEVICE_ID_BIN_MAX_SIZE);
+	if (length <= 0) {
+		return false;
+	}
+
+	memset(id, 0, id_max_len);
+	length = bin2hex(hwinfo_id, length, id, id_max_len - 1);
+
+	return length > 0;
+}
