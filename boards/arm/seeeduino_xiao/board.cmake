89eab0e440a8753723f023cb7250272f775772a6
Michael Hope <mlhx@google.com>
2020-07-26 10:22:46 +0200

    boards: arm: enable BOSSA support on Seeeduino XIAO
    Enable the BOSSA bootloader support on the Seeeduino XIAO and drop the
    no longer needed `--offset`.
    
    Signed-off-by: Michael Hope <mlhx@google.com>

--- boards/arm/seeeduino_xiao/board.cmake
+++ boards/arm/seeeduino_xiao/board.cmake
@@ -1,7 +1,5 @@
 # Copyright (c) 2020 Google LLC.
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(bossac "--offset=0x2000")
-
 include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
