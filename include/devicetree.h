327afd2c56343e14988b226a57a05a7bc9e48a8b
Peter Bigot <peter.bigot@nordicsemi.no>
2020-07-27 10:17:46 -0500

    devicetree: add a name for an invalid node identifier
    This can be used in factored code to determine that no devicetree node
    was available.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- include/devicetree.h
+++ include/devicetree.h
@@ -28,6 +28,15 @@
  * @}
  */
 
+/**
+ * @brief Name for an invalid node identifier
+ *
+ * This supports cases where factored macros can be invoked from paths where
+ * devicetree data may or may not be available.  It is a preprocessor identifier
+ * that does not match any valid devicetree node identifier.
+ */
+#define DT_INVALID_NODE _
+
 /*
  * Property suffixes
  * -----------------
