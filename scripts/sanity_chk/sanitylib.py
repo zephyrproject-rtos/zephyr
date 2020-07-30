555fc6dbd5640e3fa582177b17d5b2e24f51e417
Anas Nashif <anas.nashif@intel.com>
2020-07-30 07:23:54 -0400

    sanitycheck: fix only_tags usage
    We have been doing an AND comparison instead of an OR. AND does exclude
    way to many testcases where multiple tags are being used.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/sanity_chk/sanitylib.py
+++ scripts/sanity_chk/sanitylib.py
@@ -2844,7 +2844,7 @@ class TestSuite(DisablePyTestCollectionMixin):
                     discards[instance] = "Excluded tags per platform (exclude_tags)"
                     continue
 
-                if not tc.tags or (plat.only_tags and tc.tags - set(plat.only_tags)):
+                if plat.only_tags and not set(plat.only_tags) & tc.tags:
                     discards[instance] = "Excluded tags per platform (only_tags)"
                     continue
 
