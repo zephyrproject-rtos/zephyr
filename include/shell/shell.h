d3d504aab4b988df9d5b251192eb41d217866ad1
Andrew Boie <andrew.p.boie@intel.com>
2020-07-31 12:53:04 -0700

    shell: SHELL_DEFINE() create kernel stacks
    These shell threads run in supervisor mode.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- include/shell/shell.h
+++ include/shell/shell.h
@@ -671,7 +671,7 @@ extern void shell_print_stream(const void *user_ctx, const char *data,
 			     true, shell_print_stream);			      \
 	LOG_INSTANCE_REGISTER(shell, _name, CONFIG_SHELL_LOG_LEVEL);	      \
 	SHELL_STATS_DEFINE(_name);					      \
-	static K_THREAD_STACK_DEFINE(_name##_stack, CONFIG_SHELL_STACK_SIZE); \
+	static K_KERNEL_STACK_DEFINE(_name##_stack, CONFIG_SHELL_STACK_SIZE); \
 	static struct k_thread _name##_thread;				      \
 	static const Z_STRUCT_SECTION_ITERABLE(shell, _name) = {	      \
 		.default_prompt = _prompt,				      \
