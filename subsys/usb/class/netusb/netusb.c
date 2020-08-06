57084f32a13bebda80fd7934ae245945910828af
Emil Obalski <emil.obalski@nordicsemi.no>
2020-08-06 14:31:10 +0200

    samples: zperf: usb: Enable USB by the application.
    This commit allows let build zperf sample with overlay-netusb.conf.
    USB subsystem must be enabled by the application.
    
    Signed-off-by: Emil Obalski <emil.obalski@nordicsemi.no>

--- subsys/usb/class/netusb/netusb.c
+++ subsys/usb/class/netusb/netusb.c
@@ -125,17 +125,9 @@ bool netusb_enabled(void)
 
 static void netusb_init(struct net_if *iface)
 {
-	int ret;
-
 	static uint8_t mac[6] = { 0x00, 0x00, 0x5E, 0x00, 0x53, 0x00 };
 
 	LOG_DBG("netusb device initialization");
-
-	ret = usb_enable(NULL);
-	if (ret != 0) {
-		LOG_ERR("Failed to enable USB");
-		return;
-	}
 
 	netusb.iface = iface;
 
