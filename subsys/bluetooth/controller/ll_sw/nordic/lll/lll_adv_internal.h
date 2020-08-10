28238fef7129935a87ca816d14768b0a57bec5b8
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2020-08-10 20:05:16 +0200

    Bluetooth: controller: Add public scan_req_report function
    This will be also used by lll_adv_aux code.
    
    Signed-off-by: Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_internal.h
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_internal.h
@@ -74,3 +74,7 @@ lll_adv_sync_data_curr_get(struct lll_adv_sync *lll)
 bool lll_adv_scan_req_check(struct lll_adv *lll, struct pdu_adv *sr,
 			    uint8_t tx_addr, uint8_t *addr,
 			    uint8_t devmatch_ok, uint8_t *rl_idx);
+
+#if defined(CONFIG_BT_CTLR_SCAN_REQ_NOTIFY)
+int lll_adv_scan_req_report(struct pdu_adv *pdu_adv_rx, uint8_t rssi_ready);
+#endif
