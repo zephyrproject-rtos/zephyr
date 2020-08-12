c00b85b0e99b793923d585f88a3b1fb42e32b052
Nicholas Lowell <nlowell@lexmark.com>
2020-08-12 13:33:04 -0400

    posix: semaphore: use ZRESTRICT
    To better handle all possible definitions of restrict
    or __restrict keyword and avoid redefinition errors, use
    the ZRESTRICT pre-processor defined in toolchain.h
    
    Signed-off-by: Nicholas Lowell <nlowell@lexmark.com>

--- include/posix/semaphore.h
+++ include/posix/semaphore.h
@@ -14,10 +14,10 @@ extern "C" {
 #endif
 
 int sem_destroy(sem_t *semaphore);
-int sem_getvalue(sem_t *restrict semaphore, int *restrict value);
+int sem_getvalue(sem_t *ZRESTRICT semaphore, int *ZRESTRICT value);
 int sem_init(sem_t *semaphore, int pshared, unsigned int value);
 int sem_post(sem_t *semaphore);
-int sem_timedwait(sem_t *restrict semaphore, struct timespec *restrict abstime);
+int sem_timedwait(sem_t *ZRESTRICT semaphore, struct timespec *ZRESTRICT abstime);
 int sem_trywait(sem_t *semaphore);
 int sem_wait(sem_t *semaphore);
 
