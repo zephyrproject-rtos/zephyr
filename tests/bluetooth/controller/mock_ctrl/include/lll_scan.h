0947931a8130859f00f304d85b6e00795a27bbeb
Andries Kruithof <Andries.Kruithof@nordicsemi.no>
2020-09-02 15:15:14 +0200

    [TOPIC-BLE-LLCP] Bluetooth: controller: update HCI based on feedback
    The HCI routines are updated based on feedback on PR
    Added mockup routines
    Fixed compile issues after rebase
    Added comment that ull_llcp_hci.c needs to be re-evaluated before upmerge
    
    Signed-off-by: Andries Kruithof <Andries.Kruithof@nordicsemi.no>
    
    Update with comment

--- tests/bluetooth/controller/mock_ctrl/include/lll_scan.h
+++ tests/bluetooth/controller/mock_ctrl/include/lll_scan.h
@@ -1,14 +1,19 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 struct lll_scan {
 	struct ull_cp_conn *conn;
-	u8_t  init_addr[BDADDR_SIZE];
-	u8_t  adv_addr[BDADDR_SIZE];
-	u8_t adv_addr_type:1;
-	u16_t conn_timeout;
-	u8_t conn_ticks_slot;
-	u8_t connect_expire;
-	u8_t supervision_expire;
-	u8_t supervision_reload;
-	u8_t procedure_expire;
-	u8_t procedure_reload;
+	uint8_t  init_addr[BDADDR_SIZE];
+	uint8_t  adv_addr[BDADDR_SIZE];
+	uint8_t adv_addr_type:1;
+	uint16_t conn_timeout;
+	uint8_t conn_ticks_slot;
+	uint8_t connect_expire;
+	uint8_t supervision_expire;
+	uint8_t supervision_reload;
+	uint8_t procedure_expire;
+	uint8_t procedure_reload;
 };
