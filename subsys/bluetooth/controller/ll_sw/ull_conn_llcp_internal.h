215e91f575e625e6d0dd0ea8a376bb96f694d022
Andries Kruithof <Andries.Kruithof@nordicsemi.no>
2020-09-02 15:15:14 +0200

    [TOPIC-BLE-LLCP] Bluetooth: controller: update HCI based on feedback
    The HCI routines are updated based on feedback on PR
    Added mockup routines
    Fixed compile issues after rebase
    Added comment that ull_llcp_hci.c needs to be re-evaluated before upmerge
    
    Signed-off-by: Andries Kruithof <Andries.Kruithof@nordicsemi.no>
    
    Update with comment

--- subsys/bluetooth/controller/ll_sw/ull_conn_llcp_internal.h
+++ subsys/bluetooth/controller/ll_sw/ull_conn_llcp_internal.h
@@ -4,8 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/* Macro to convert time in us to connection interval units */
-#define RADIO_CONN_EVENTS(x, y) ((u16_t)(((x) + (y) - 1) / (y)))
+/*
+ * copied from ull_conn_internal.h
+ * some macros will need to be moved to other, more logical places
+ */
 
 /*
  * Macros to return correct Data Channel PDU time
@@ -36,51 +38,46 @@
 				     CODED_PHY_CRC_SIZE + \
 				     CODED_PHY_TERM2_SIZE) * 8)
 
-#define PKT_US(octets, phy) (((phy) & PHY_CODED) ?		   \
-			     (CODED_PHY_PREAMBLE_TIME_US + \
-			      FEC_BLOCK1_TIME_US + \
-			      FEC_BLOCK2_TIME_US(octets)) : \
-			     (((PREAMBLE_SIZE(phy) + \
-				ACCESS_ADDR_SIZE + \
-				PAYLOAD_OVERHEAD_SIZE + \
-				(octets) + \
-				CRC_SIZE) * 8) / \
-			      BIT(((phy) & 0x03) >> 1)))
 #else /* !CONFIG_BT_CTLR_PHY_CODED */
-#define PKT_US(octets, phy) ((((PREAMBLE_SIZE(phy)) +	\
-			       ACCESS_ADDR_SIZE + \
-			       PAYLOAD_OVERHEAD_SIZE + \
-			       (octets) + \
-			       CRC_SIZE) * 8) / \
-			      BIT(((phy) & 0x03) >> 1))
 #endif /* !CONFIG_BT_CTLR_PHY_CODED */
+
+#define ULL_HANDLE_NOT_CONNECTED 0xFFFF
 
 struct ull_cp_conn *ll_conn_acquire(void);
 void ll_conn_release(struct ull_cp_conn *conn);
-u16_t ll_conn_handle_get(struct ull_cp_conn *conn);
-struct ull_cp_conn *ll_conn_get(u16_t handle);
-struct ull_cp_conn *ll_connected_get(u16_t handle);
-// int ull_conn_init(void);
+uint16_t ll_conn_handle_get(struct ull_cp_conn *conn);
+struct ull_cp_conn *ll_conn_get(uint16_t handle);
+struct ull_cp_conn *ll_connected_get(uint16_t handle);
+uint16_t ull_conn_init(void);
 int ull_conn_reset(void);
-u8_t ull_conn_chan_map_cpy(u8_t *chan_map);
-void ull_conn_chan_map_set(u8_t *chan_map);
-u16_t ull_conn_default_tx_octets_get(void);
-u16_t ull_conn_default_tx_time_get(void);
-u8_t ull_conn_default_phy_tx_get(void);
-u8_t ull_conn_default_phy_rx_get(void);
+uint8_t ull_conn_chan_map_cpy(uint8_t *chan_map);
+void ull_conn_chan_map_set(uint8_t const *const chan_map);
+uint16_t ull_conn_default_tx_octets_get(void);
+uint16_t ull_conn_default_tx_time_get(void);
+void ull_conn_default_tx_octets_set(uint16_t tx_octets);
+void ull_conn_default_tx_time_set(uint16_t tx_time);
+uint8_t ull_conn_default_phy_tx_get(void);
+uint8_t ull_conn_default_phy_rx_get(void);
+void ull_conn_default_phy_tx_set(uint8_t tx);
+void ull_conn_default_phy_rx_set(uint8_t rx);
 void ull_conn_setup(memq_link_t *link, struct node_rx_hdr *rx);
 int ull_conn_rx(memq_link_t *link, struct node_rx_pdu **rx);
-int ull_conn_llcp(struct ull_cp_conn *conn, u32_t ticks_at_expire, u16_t lazy);
+int ull_conn_llcp(struct ull_cp_conn *conn, uint32_t ticks_at_expire, uint16_t lazy);
 void ull_conn_done(struct node_rx_event_done *done);
-void ull_conn_tx_demux(u8_t count);
-void ull_conn_tx_lll_enqueue(struct ull_cp_conn *conn, u8_t count);
+void ull_conn_tx_demux(uint8_t count);
+void ull_conn_tx_lll_enqueue(struct ull_cp_conn *conn, uint8_t count);
 void ull_conn_link_tx_release(void *link);
-u8_t ull_conn_ack_last_idx_get(void);
-memq_link_t *ull_conn_ack_peek(u8_t *ack_last, u16_t *handle,
+uint8_t ull_conn_ack_last_idx_get(void);
+memq_link_t *ull_conn_ack_peek(uint8_t *ack_last, uint16_t *handle,
 			       struct node_tx **tx);
-memq_link_t *ull_conn_ack_by_last_peek(u8_t last, u16_t *handle,
+memq_link_t *ull_conn_ack_by_last_peek(uint8_t last, uint16_t *handle,
 				       struct node_tx **tx);
 void *ull_conn_ack_dequeue(void);
-struct ull_cp_conn *ull_conn_tx_ack(u16_t handle, memq_link_t *link,
+struct ull_cp_conn *ull_conn_tx_ack(uint16_t handle, memq_link_t *link,
 				struct node_tx *tx);
-u8_t ull_conn_llcp_req(void *conn);
+uint8_t ull_conn_llcp_req(void *conn);
+
+void *ull_conn_tx_mem_acquire(void);
+void ull_conn_tx_mem_release(void *tx);
+uint8_t ull_conn_mfifo_get_tx(void *lll_tx);
+void ull_conn_mfifo_enqueue_tx(uint8_t idx);
--- tests/bluetooth/controller/mock_ctrl/include/kconfig.h
+++ tests/bluetooth/controller/mock_ctrl/include/kconfig.h
@@ -14,7 +14,7 @@
 #endif
 
 #define CONFIG_BT_CONN
-#define CONFIG_BT_MAX_CONN 2
+#define CONFIG_BT_MAX_CONN 4
 
 /* ensure that proper configuration is set */
 
@@ -51,13 +51,16 @@
 #define CONFIG_BT_CTLR_CONN_RSSI y
 #endif
 
+#define CONFIG_BT_CTLR_LLCP_CONN 4
+
 /* Kconfig Cheats */
 #define CONFIG_BT_LOG_LEVEL 1
 #define CONFIG_BT_CTLR_COMPANY_ID 0x1234
 #define CONFIG_BT_CTLR_SUBVERSION_NUMBER 0x5678
 #define CONFIG_NET_BUF_USER_DATA_SIZE 4096
 #define CONFIG_BT_CTLR_ASSERT_HANDLER y
-
+#define CONFIG_BT_CTLR_TX_BUFFERS 7
+#define CONFIG_BT_CTLR_TX_BUFFER_SIZE 27
 #define PROC_CTX_BUF_NUM 15
 #define TX_CTRL_BUF_NUM 2
 #define NTF_BUF_NUM 2
