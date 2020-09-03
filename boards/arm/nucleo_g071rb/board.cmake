89eed0329b57585b0b5c7ae336eb1dc1bbcb86bd
Erwan Gouriou <erwan.gouriou@linaro.org>
2020-09-03 10:12:39 +0200

    boards: nucleo_g071rb: Fix pyocd target
    In recent pyocd versions, `stm32g071rbtx` should be used as target
    name.
    
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- boards/arm/nucleo_g071rb/board.cmake
+++ boards/arm/nucleo_g071rb/board.cmake
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
-board_runner_args(pyocd "--target=stm32g071rb")
+board_runner_args(pyocd "--target=stm32g071rbtx")
 board_runner_args(pyocd "--flash-opt=-O reset_type=hw")
 board_runner_args(pyocd "--flash-opt=-O connect_mode=under-reset")
 
