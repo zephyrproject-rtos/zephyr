8f6e5ed98a473f34f6f7089169cfe940eed7ab34
Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
2020-09-03 14:57:45 +0300

    cmake: allow to specify compliler for DTS preprocessing
    MWDT CCAC compiler can't handle the dts file as gcc does, so
    allow to specify compliler for DTS preprocessing which is differ
    from ${CMAKE_C_COMPILER}.
    
    Use ARC GNU compiler for DTS preprocessing in case of MWDT
    toolchain.
    
    Signed-off-by: Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>

--- cmake/compiler/arcmwdt/generic.cmake
+++ cmake/compiler/arcmwdt/generic.cmake
@@ -2,6 +2,22 @@
 
 # Configures CMake for using ccac
 
+# MWDT compiler (CCAC) can't be used for preprocessing the DTS sources as it has
+# weird restrictions about file extensions. Synopsys Jira issue: P10019563-38578
+# Let's temporarily use GNU compiler instead.
+find_program(CMAKE_DTS_PREPROCESSOR arc-elf32-gcc)
+if (NOT CMAKE_DTS_PREPROCESSOR)
+  find_program(CMAKE_DTS_PREPROCESSOR arc-linux-gcc)
+endif()
+
+if (NOT CMAKE_DTS_PREPROCESSOR)
+  find_program(CMAKE_DTS_PREPROCESSOR gcc)
+endif()
+
+if(NOT CMAKE_DTS_PREPROCESSOR)
+  message(FATAL_ERROR "Zephyr was unable to find any GNU compiler (ARC or host one) for DTS preprocessing")
+endif()
+
 find_program(CMAKE_C_COMPILER ${CROSS_COMPILE}ccac PATH ${TOOLCHAIN_HOME} NO_DEFAULT_PATH)
 
 if(CMAKE_C_COMPILER STREQUAL CMAKE_C_COMPILER-NOTFOUND)
