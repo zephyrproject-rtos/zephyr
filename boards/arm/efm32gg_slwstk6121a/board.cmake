7f9f4e659714f5454ccccb310cb4f0faecc913f4
Thorvald Natvig <thorvald@natvig.com>
2020-09-05 09:32:04 -0700

    boards: Add SiLabs SLWSTK6121A (WGM160P)
    The WGM160P module combines the WF200 Wi-Fi transceiver with
    an EFM32GG11 MCU
    This code is based on the efm32gg_stk3701a board definitions
    
    Signed-off-by: Thorvald Natvig <thorvald@natvig.com>

---
+++ boards/arm/efm32gg_slwstk6121a/board.cmake
@@ -0,0 +1,7 @@
+# Copyright (c) 2019 Interay Solutions B.V.
+# Copyright (c) 2019 Oane Kingma
+# Copyright (c) 2020 Thorvald Natvig
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=EFM32GG11B820F2048")
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
