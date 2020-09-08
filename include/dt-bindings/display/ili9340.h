114ec6a2cc081afc1e084e5f998560fc0bd5c9e4
Gerard Marull-Paretas <gerard@teslabs.com>
2020-09-08 23:28:30 +0200

    drivers: display: ili9340: add support for configuring pixel format
    Move pixel format setting (RGB565/RGB888) to DeviceTree. Add support for
    changing pixel format at runtime.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

---
+++ include/dt-bindings/display/ili9340.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020 Teslabs Engineering S.L.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_DISPLAY_ILI9340_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_DISPLAY_ILI9340_H_
+
+/* Pixel formats */
+#define ILI9340_PIXEL_FORMAT_RGB565 0U
+#define ILI9340_PIXEL_FORMAT_RGB888 1U
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_DISPLAY_ILI9340_H_ */
