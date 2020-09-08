a64bc775666f12265d25bee3c94278e461ffb9af
Andries Kruithof <Andries.Kruithof@nordicsemi.no>
2020-09-08 12:16:04 +0200

    [TOPIC-BLE-LLCP] Bluetooth: Add data structures for connection
    Added data structures for HCI interfacing and setting connection
    
    Signed-off-by: Andries Kruithof <Andries.Kruithof@nordicsemi.no>

--- tests/bluetooth/controller/mock_ctrl/src/ull.c
+++ tests/bluetooth/controller/mock_ctrl/src/ull.c
@@ -6,6 +6,31 @@
 
 #include <zephyr/types.h>
 
+#include "util/mem.h"
+#include "util/memq.h"
+/*
+ * just a big number
+ */
+#define PDU_RX_POOL_SIZE 16384
+
+
+static struct {
+	void *free;
+	uint8_t pool[PDU_RX_POOL_SIZE];
+} mem_pdu_rx;
+
+/*
+ * just a big number
+ */
+#define LINK_RX_POOL_SIZE 16384
+
+static struct {
+	uint8_t quota_pdu; /* Number of un-utilized buffers */
+
+	void *free;
+	uint8_t pool[LINK_RX_POOL_SIZE];
+} mem_link_rx;
+
 
 
 void ull_ticker_status_give(uint32_t status, void *param)
@@ -31,4 +56,24 @@ void *ull_disable_unmark(void *param)
 int ull_disable(void *lll)
 {
 	return 0;
+}
+
+void *ll_rx_link_alloc(void)
+{
+	return mem_acquire(&mem_link_rx.free);
+}
+
+void ll_rx_link_release(void *link)
+{
+	mem_release(link, &mem_link_rx.free);
+}
+
+void *ll_rx_alloc(void)
+{
+	return mem_acquire(&mem_pdu_rx.free);
+}
+
+void ll_rx_release(void *node_rx)
+{
+	mem_release(node_rx, &mem_pdu_rx.free);
 }
