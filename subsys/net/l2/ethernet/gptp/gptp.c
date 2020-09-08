e3fd17072e95e8ca0fe689bf802e4ffa53faaf69
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2020-09-08 14:06:21 +0300

    net: gptp: Allow user to tweak priority1 and priority2 values
    Instead of hardcoding the priority1 and priority2 values used
    in BMCA, let the user tweak the values via Kconfig.
    
    Fixes #28151
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- subsys/net/l2/ethernet/gptp/gptp.c
+++ subsys/net/l2/ethernet/gptp/gptp.c
@@ -382,7 +382,15 @@ static void gptp_init_clock_ds(void)
 		GPTP_OFFSET_SCALED_LOG_VAR_UNKNOWN;
 
 	if (default_ds->gm_capable) {
-		default_ds->priority1 = GPTP_PRIORITY1_GM_CAPABLE;
+		/* The priority1 value cannot be 255 for GM capable
+		 * system.
+		 */
+		if (CONFIG_NET_GPTP_BMCA_PRIORITY1 ==
+		    GPTP_PRIORITY1_NON_GM_CAPABLE) {
+			default_ds->priority1 = GPTP_PRIORITY1_GM_CAPABLE;
+		} else {
+			default_ds->priority1 = CONFIG_NET_GPTP_BMCA_PRIORITY1;
+		}
 	} else {
 		default_ds->priority1 = GPTP_PRIORITY1_NON_GM_CAPABLE;
 	}
