bd7ee8649632ada6846c5872fd61e779bda19842
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2020-09-09 21:28:29 +0530

    drivers: Bluetooth: rpmsg: Add missing BT_QUIRK_NO_AUTO_DLE
    Add BT_QUIRK_NO_AUTO_DLE quirk option for HCI rpmsg driver
    when the Zephyr open source BLE controller is used.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- drivers/bluetooth/hci/rpmsg.c
+++ drivers/bluetooth/hci/rpmsg.c
@@ -198,6 +198,9 @@ static const struct bt_hci_driver drv = {
 	.open		= bt_rpmsg_open,
 	.send		= bt_rpmsg_send,
 	.bus		= BT_HCI_DRIVER_BUS_IPM,
+#if defined(BT_DRIVER_QUIRK_NO_AUTO_DLE)
+	.quirks         = BT_QUIRK_NO_AUTO_DLE,
+#endif
 };
 
 static int bt_rpmsg_init(const struct device *unused)
