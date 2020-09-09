cb1cf54406abafb4a781660161a93d5c363c7a1f
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2020-09-09 21:28:29 +0530

    drivers: Bluetooth: rpmsg: Fix typo in BT_QUIRK_NO_AUTO_DLE use
    Use CONFIG_BT_QUIRK_NO_AUTO_DLE as quirk option's
    conditional compilation check.
    
    Regression introduced in commit bd7ee8649632 ("drivers:
    Bluetooth: rpmsg: Add missing BT_QUIRK_NO_AUTO_DLE").
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- drivers/bluetooth/hci/rpmsg.c
+++ drivers/bluetooth/hci/rpmsg.c
@@ -198,7 +198,7 @@ static const struct bt_hci_driver drv = {
 	.open		= bt_rpmsg_open,
 	.send		= bt_rpmsg_send,
 	.bus		= BT_HCI_DRIVER_BUS_IPM,
-#if defined(BT_DRIVER_QUIRK_NO_AUTO_DLE)
+#if defined(CONFIG_BT_DRIVER_QUIRK_NO_AUTO_DLE)
 	.quirks         = BT_QUIRK_NO_AUTO_DLE,
 #endif
 };
