4ee863bc50469d90d3470eddd7cc7684ba1902dd
Lingao Meng <mengabc1086@gmail.com>
2020-09-14 17:00:11 +0800

    Bluetooth: Mesh: Fix RPL not check by proxy configuration
    Fixes proxy configuration not check by replay.
    Fixes secure beacon iv update not store new RPL.
    
    Signed-off-by: Lingao Meng <mengabc1086@gmail.com>

--- subsys/bluetooth/mesh/transport.h
+++ subsys/bluetooth/mesh/transport.h
@@ -101,6 +101,8 @@ void bt_mesh_trans_init(void);
 
 void bt_mesh_rpl_clear(void);
 
+bool bt_mesh_rpl_check(struct bt_mesh_net_rx *rx, struct bt_mesh_rpl **match);
+
 int bt_mesh_heartbeat_send(const struct bt_mesh_send_cb *cb, void *cb_data);
 
 int bt_mesh_app_key_get(const struct bt_mesh_subnet *subnet, uint16_t app_idx,
