4a35916f5efc9417cfa862546b741463e2f07537
Watson Zeng <zhiwei@synopsys.com>
2020-09-15 14:18:10 +0800

    tests: kernel: stack: fix for memcmp with wrong length
    fix for memcmp with wrong length
    
    Signed-off-by: Watson Zeng <zhiwei@synopsys.com>

--- tests/kernel/stack/stack/src/main.c
+++ tests/kernel/stack/stack/src/main.c
@@ -125,7 +125,7 @@ static void thread_entry_fn_single(void *p1, void *p2, void *p3)
 	for (i = STACK_LEN; i; i--) {
 		k_stack_pop((struct k_stack *)p1, &tmp[i - 1], K_NO_WAIT);
 	}
-	zassert_false(memcmp(tmp, data1, STACK_LEN),
+	zassert_false(memcmp(tmp, data1, sizeof(tmp)),
 		      "Push & Pop items does not match");
 
 	/* Push items from stack */
@@ -150,7 +150,7 @@ static void thread_entry_fn_dual(void *p1, void *p2, void *p3)
 		k_stack_push(p1, data1[i]);
 
 	}
-	zassert_false(memcmp(tmp, data2, STACK_LEN),
+	zassert_false(memcmp(tmp, data2, sizeof(tmp)),
 		      "Push & Pop items does not match");
 }
 
@@ -158,7 +158,7 @@ static void thread_entry_fn_isr(void *p1, void *p2, void *p3)
 {
 	/* Pop items from stack2 */
 	irq_offload(tIsr_entry_pop, (const void *)p2);
-	zassert_false(memcmp(data_isr, data2, STACK_LEN),
+	zassert_false(memcmp(data_isr, data2, sizeof(data_isr)),
 		      "Push & Pop items does not match");
 
 	/* Push items to stack1 */
@@ -203,7 +203,7 @@ static void test_single_stack_play(void)
 		k_stack_pop(&stack1, &tmp[i - 1], K_NO_WAIT);
 	}
 
-	zassert_false(memcmp(tmp, data2, STACK_LEN),
+	zassert_false(memcmp(tmp, data2, sizeof(tmp)),
 		      "Push & Pop items does not match");
 
 	/* Clear the spawn thread to avoid side effect */
@@ -232,7 +232,7 @@ static void test_dual_stack_play(void)
 		k_stack_pop(&stack1, &tmp[i], K_FOREVER);
 	}
 
-	zassert_false(memcmp(tmp, data1, STACK_LEN),
+	zassert_false(memcmp(tmp, data1, sizeof(tmp)),
 		      "Push & Pop items does not match");
 
 	/* Clear the spawn thread to avoid side effect */
@@ -263,7 +263,7 @@ static void test_isr_stack_play(void)
 	/* Pop items from stack1 */
 	irq_offload(tIsr_entry_pop, (const void *)&stack1);
 
-	zassert_false(memcmp(data_isr, data1, STACK_LEN),
+	zassert_false(memcmp(data_isr, data1, sizeof(data_isr)),
 		      "Push & Pop items does not match");
 
 	/* Clear the spawn thread to avoid side effect */
