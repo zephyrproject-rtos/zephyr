12e1fd653deab52cef5b1679ba631c35b2a55286
Robert Lubos <robert.lubos@nordicsemi.no>
2020-09-16 15:09:30 +0200

    net: lwm2m: Fix FOTA block transfer with opaque content-format
    This commit fixes PUSH FOTA when opaque content-format is used.
    
    This consists of the following fixes:
     * Moved `struct block_context` to a private header, so that it can be a
       part of `struct lwm2m_input_context`. This allows content decoders to
       make use of the block context data.
     * Removed faulty `get_length_left` function from the plain text
       decoder, and replace it with coap_packet_get_payload() to obtain the
       actual payload size.
     * Introduce `struct lwm2m_opaque_context` as a part of block context,
       which allows to keep track of opaque data download progress.
     * Simplify `lwm2m_write_handler_opaque()` function. It will now only
       make calls to `engine_get_opaque` - it's the decoder responsibility
       to update the opaque context according to it's content format (for
       instance TLV decoder should only update it with the actual opaque
       data size, not the whole TLV).
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/net/lib/lwm2m/lwm2m_engine.h
+++ subsys/net/lib/lwm2m/lwm2m_engine.h
@@ -102,7 +102,9 @@ int lwm2m_engine_get_resource(char *pathstr,
 void lwm2m_engine_get_binding(char *binding);
 
 size_t lwm2m_engine_get_opaque_more(struct lwm2m_input_context *in,
-				    uint8_t *buf, size_t buflen, bool *last_block);
+				    uint8_t *buf, size_t buflen,
+				    struct lwm2m_opaque_context *opaque,
+				    bool *last_block);
 
 int lwm2m_security_inst_id_to_index(uint16_t obj_inst_id);
 int lwm2m_security_index_to_inst_id(int index);
