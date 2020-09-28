0ae1b7628a28ddb417ac0bfb40af4987c19bd821
Hake Huang <hake.huang@oss.nxp.com>
2020-09-28 16:41:21 +0800

    debugger: rt1xxx series pyocd supporting update
    in pyocd-0.28 release pyocd can support below NXP rt1xxx
    mimxrt1010
    mimxrt1015
    mimxrt1060
    mimxrt1064
    
    user can upgrade latest pyocd with
    pip3 install pyocd -U
    
    Signed-off-by: Hake Huang <hake.huang@oss.nxp.com>

--- boards/arm/mimxrt1010_evk/board.cmake
+++ boards/arm/mimxrt1010_evk/board.cmake
@@ -4,5 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+board_runner_args(pyocd "--target=mimxrt1010")
 board_runner_args(jlink "--device=MIMXRT1011")
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
--- boards/arm/mimxrt1064_evk/board.cmake
+++ boards/arm/mimxrt1064_evk/board.cmake
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-board_runner_args(pyocd "--target=cortex_m")
+board_runner_args(pyocd "--target=mimxrt1064")
 board_runner_args(jlink "--device=MIMXRT1064")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
