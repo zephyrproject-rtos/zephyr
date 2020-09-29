668cbe19a34e38d72526efee7a726123dc39b2e3
Marcin Niestroj <m.niestroj@grinn-global.com>
2020-09-29 02:31:23 +0200

    drivers: wifi: esp: support power-gpios to enable ESP module
    Add power-gpios device-tree binding property to power on module before
    communicating with it. This pin is called CHIP_PU in case of ESP32{,-S2}
    and CHIP_EN in case of ESP8266. Dedicated reset pin is available only on
    the latter, however Espressif recommends (in ESP8266 Hardware Design
    Guidelines) to use CHIP_EN instead. Follow those recommendations and use
    power-gpios to reset chip if that is provided over device-tree.
    
    Configure power-gpios and reset-gpios as inactive by default, so that
    chip becomes ready after executing esp_reset() function, either if one
    or both are provided over device-tree.
    
    Signed-off-by: Marcin Niestroj <m.niestroj@grinn-global.com>

--- drivers/wifi/esp/esp.c
+++ drivers/wifi/esp/esp.c
@@ -29,6 +29,9 @@ LOG_MODULE_REGISTER(wifi_esp);
 
 /* pin settings */
 enum modem_control_pins {
+#if DT_INST_NODE_HAS_PROP(0, power_gpios)
+	ESP_POWER,
+#endif
 #if DT_INST_NODE_HAS_PROP(0, reset_gpios)
 	ESP_RESET,
 #endif
@@ -36,10 +39,15 @@ enum modem_control_pins {
 };
 
 static struct modem_pin modem_pins[] = {
+#if DT_INST_NODE_HAS_PROP(0, power_gpios)
+	MODEM_PIN(DT_INST_GPIO_LABEL(0, power_gpios),
+		  DT_INST_GPIO_PIN(0, power_gpios),
+		  DT_INST_GPIO_FLAGS(0, power_gpios) | GPIO_OUTPUT_INACTIVE),
+#endif
 #if DT_INST_NODE_HAS_PROP(0, reset_gpios)
 	MODEM_PIN(DT_INST_GPIO_LABEL(0, reset_gpios),
 		  DT_INST_GPIO_PIN(0, reset_gpios),
-		  DT_INST_GPIO_FLAGS(0, reset_gpios) | GPIO_OUTPUT),
+		  DT_INST_GPIO_FLAGS(0, reset_gpios) | GPIO_OUTPUT_INACTIVE),
 #endif
 };
 
@@ -788,7 +796,11 @@ static void esp_reset(struct esp_data *dev)
 		net_if_down(dev->net_iface);
 	}
 
-#if DT_INST_NODE_HAS_PROP(0, reset_gpios)
+#if DT_INST_NODE_HAS_PROP(0, power_gpios)
+	modem_pin_write(&dev->mctx, ESP_POWER, 0);
+	k_sleep(K_MSEC(100));
+	modem_pin_write(&dev->mctx, ESP_POWER, 1);
+#elif DT_INST_NODE_HAS_PROP(0, reset_gpios)
 	modem_pin_write(&dev->mctx, ESP_RESET, 1);
 	k_sleep(K_MSEC(100));
 	modem_pin_write(&dev->mctx, ESP_RESET, 0);
