cca85a495b35700b6edde810c15ca5e0317f9538
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2020-09-30 09:00:09 +0200

    [DNM] boards: Add support for TF-M in nRF5340 PDK and nRF9160 DK
    Provide the required settings so that it is possible to build with
    TF-M as the secure image for those boards.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- boards/arm/nrf9160dk_nrf9160/board.cmake
+++ boards/arm/nrf9160dk_nrf9160/board.cmake
@@ -1,5 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
+set(TFM_TARGET_PLATFORM "nrf9160dk_nrf9160")
+set(TFM_PUBLIC_KEY_FORMAT "full")
+
 board_runner_args(nrfjprog "--nrf-family=NRF91")
 board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
