780fa73b832f9d49f755b17af1176f503a6dad92
Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
2020-10-02 07:59:20 +0000

    settings_fcb: initialize rc variable
    Avoid warning about uninitialized variable.
    The for loop should do at least two iterations
    in a valid execution, hence initialize to an
    error value.
    
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>

--- subsys/settings/src/settings_fcb.c
+++ subsys/settings/src/settings_fcb.c
@@ -305,7 +305,7 @@ static int settings_fcb_save_priv(struct settings_store *cs, const char *name,
 	struct settings_fcb *cf = (struct settings_fcb *)cs;
 	struct fcb_entry_ctx loc;
 	int len;
-	int rc;
+	int rc = -EINVAL;
 	int i;
 	uint8_t wbs;
 
