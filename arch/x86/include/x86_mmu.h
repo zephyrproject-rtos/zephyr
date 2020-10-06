f6c64e92cecf2a963c192ffb8b66a2721106c895
Andrew Boie <andrew.p.boie@intel.com>
2020-10-06 16:04:40 -0700

    x86: fix arch_user_mode_enter locking
    This function iterates over the thread's memory domain
    and updates page tables based on it. We need to be holding
    z_mem_domain_lock while this happens.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- arch/x86/include/x86_mmu.h
+++ arch/x86/include/x86_mmu.h
@@ -119,7 +119,10 @@ extern uint8_t z_shared_kernel_page_start;
 /* Set up per-thread page tables just prior to entering user mode */
 void z_x86_thread_pt_init(struct k_thread *thread);
 
-/* Apply a memory domain policy to a set of thread page tables */
+/* Apply a memory domain policy to a set of thread page tables.
+ *
+ * Must be called with z_mem_domain_lock held.
+ */
 void z_x86_apply_mem_domain(struct k_thread *thread,
 			    struct k_mem_domain *mem_domain);
 #endif /* CONFIG_USERSPACE */
