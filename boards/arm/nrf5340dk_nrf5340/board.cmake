e2dc8982beeeca22aff07db6447d8376dce476b5
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2020-10-08 17:36:42 +0200

    boards: arm: Add support for TF-M in nRF5340 PDK
    Adding support for TF-M in the Nordic nRF5340
    Application MCU.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- boards/arm/nrf5340dk_nrf5340/board.cmake
+++ boards/arm/nrf5340dk_nrf5340/board.cmake
@@ -1,5 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
+# Set the corresponding TF-M target platform when building for the Non-Secure
+# version of the board (Application MCU).
+if(CONFIG_BOARD_NRF5340PDK_NRF5340_CPUAPPNS)
+  set(TFM_TARGET_PLATFORM "nordic_nrf/nrf5340pdk_nrf5340_cpuapp")
+  set(TFM_PUBLIC_KEY_FORMAT "full")
+endif()
+
 if((CONFIG_BOARD_NRF5340PDK_NRF5340_CPUAPP OR CONFIG_BOARD_NRF5340PDK_NRF5340_CPUAPPNS) OR
   (CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP OR CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPPNS))
 board_runner_args(nrfjprog "--nrf-family=NRF53" "--tool-opt=--coprocessor CP_APPLICATION")
