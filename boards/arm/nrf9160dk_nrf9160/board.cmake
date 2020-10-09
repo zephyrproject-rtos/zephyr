af5dc05dd7be4e1794f9f674681f352bccf7596c
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2020-10-09 14:17:56 +0200

    boards: arm: Add support for TF-M in nRF9160 DK
    Adding support for TF-M in the Nordic nRF9160 DK.
    Allow the TF-M integration samples to be built and
    executed for nRF9160 DK.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- boards/arm/nrf9160dk_nrf9160/board.cmake
+++ boards/arm/nrf9160dk_nrf9160/board.cmake
@@ -1,5 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
+# Set the corresponding TF-M target platform when building for the Non-Secure
+# version of the board (Application MCU).
+if(CONFIG_BOARD_NRF9160DK_NRF9160NS)
+  set(TFM_TARGET_PLATFORM "nordic_nrf/nrf9160dk_nrf9160")
+  set(TFM_PUBLIC_KEY_FORMAT "full")
+endif()
+
 board_runner_args(nrfjprog "--nrf-family=NRF91")
 board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
