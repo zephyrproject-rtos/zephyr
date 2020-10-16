0d1497948debc290330ceba4f2393bbe4bac8f36
Stephane D'Alu <sdalu@sdalu.com>
2020-10-16 19:01:00 +0200

    boards: Added cmsis-dap interface to openocd runner for NRF5
    Added openocd runner with cmsis-dap interface.
    Apply to nrf52840_mdk board.
    
    Signed-off-by: Stephane D'Alu <sdalu@sdalu.com>

--- boards/arm/nrf52840_mdk/board.cmake
+++ boards/arm/nrf52840_mdk/board.cmake
@@ -1,4 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(pyocd "--target=nrf52840")
+set(OPENOCD_NRF5_INTERFACE "cmsis-dap")
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
