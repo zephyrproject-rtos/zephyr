e2847552fc4e4b2dbb52578eb6b520acc771b94e
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2020-10-19 14:36:19 +0200

    drivers/sensor: Fix constant qualifier for lis2mdl PM function
    Found it via running coccinelle script in:
    e18fcbba5a4e63b54116f40ae5f0323d5237d329
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- drivers/sensor/lis2mdl/lis2mdl.c
+++ drivers/sensor/lis2mdl/lis2mdl.c
@@ -401,7 +401,7 @@ static int lis2mdl_set_power_state(struct lis2mdl_data *lis2mdl,
 	return status;
 }
 
-static int lis2mdl_pm_control(struct device *dev, uint32_t ctrl_command,
+static int lis2mdl_pm_control(const struct device *dev, uint32_t ctrl_command,
 				void *context, device_pm_cb cb, void *arg)
 {
 	struct lis2mdl_data *lis2mdl = dev->driver_data;
