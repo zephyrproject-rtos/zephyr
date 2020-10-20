e7436f7c55095acf20f3ad0c76a45d540ff33a34
Andy Ross <andrew.j.ross@intel.com>
2020-10-20 10:43:14 -0700

    samples/userspace/prod_consumer: Use a sys_heap
    This code used a sys_mem_pool directly.  Use a new-style heap instead
    to do the same thing.
    
    (Note that the usage is a little specious -- it allocates from the
    heap but doesn't appear to fill or check any data therein, just that
    the heap memory can be copied from the two memory domains.  It's
    unclear exactly what this is trying to demonstrate and we might want
    to improve the sample to do something less trivial.)
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- samples/userspace/prod_consumer/src/main.h
+++ samples/userspace/prod_consumer/src/main.h
@@ -9,13 +9,13 @@
 
 #include <kernel.h>
 #include <app_memory/app_memdomain.h>
-#include <sys/mempool.h>
+#include <sys/sys_heap.h>
 
 extern struct k_mem_partition shared_partition;
 #define SHARED_DATA	K_APP_DMEM(shared_partition)
 #define SHARED_BSS	K_APP_BMEM(shared_partition)
 
-extern struct sys_mem_pool shared_pool;
+extern struct sys_heap shared_pool;
 extern struct k_queue shared_queue_incoming;
 extern struct k_queue shared_queue_outgoing;
 
