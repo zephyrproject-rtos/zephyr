555cc4c7925aaaed68854fb1d408e855e6d8308b
Andrew Boie <andrew.p.boie@intel.com>
2020-10-23 13:19:34 -0700

    samples: prod_consumer: re-use default mem domain
    Memory domains can be very expensive to instantiate. Demonstrate
    how the default memory domain can be re-purposed for an
    application, much like how the main thread already is.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- samples/userspace/prod_consumer/src/app_b.c
+++ samples/userspace/prod_consumer/src/app_b.c
@@ -24,9 +24,6 @@ K_MEM_POOL_DEFINE(app_b_resource_pool, 32, 256, 4, 4);
  */
 K_APPMEM_PARTITION_DEFINE(app_b_partition);
 
-/* Memory domain for application B, set up and installed in app_a_entry() */
-static struct k_mem_domain app_b_domain;
-
 /* Global data used by application B. By tagging with APP_B_BSS or APP_B_DATA,
  * we ensure all this gets linked into the continuous region denoted by
  * app_b_partition.
@@ -79,20 +76,12 @@ static void processor_thread(void *p1, void *p2, void *p3)
 
 void app_b_entry(void *p1, void *p2, void *p3)
 {
-	struct k_mem_partition *parts[] = {
-#if Z_LIBC_PARTITION_EXISTS
-		&z_libc_partition,
-#endif
-		&app_b_partition, &shared_partition
-	};
-
-	/* Initialize a memory domain with the specified partitions
-	 * and add ourself to this domain. We need access to our own
-	 * partition, the shared partition, and any common libc partition
-	 * if it exists.
+	/* Much like how we are reusing the main thread as this application's
+	 * processor thread, we will re-use the default memory domain as the
+	 * domain for application B.
 	 */
-	k_mem_domain_init(&app_b_domain, ARRAY_SIZE(parts), parts);
-	k_mem_domain_add_thread(&app_b_domain, k_current_get());
+	k_mem_domain_add_partition(&k_mem_domain_default, &app_b_partition);
+	k_mem_domain_add_partition(&k_mem_domain_default, &shared_partition);
 
 	/* Assign a resource pool to serve for kernel-side allocations on
 	 * behalf of application A. Needed for k_queue_alloc_append().
