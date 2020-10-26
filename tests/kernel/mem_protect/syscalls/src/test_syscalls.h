cc6ac9d07e5351395de4333f613114627c9ee3b6
Wentong Wu <wentong.wu@intel.com>
2020-10-26 20:47:44 +0800

    tests: kernel: syscalls: add more than 6 arguments syscall test case
    Add more than 6 arguments syscall test case.
    
    Signed-off-by: Wentong Wu <wentong.wu@intel.com>

--- tests/kernel/mem_protect/syscalls/src/test_syscalls.h
+++ tests/kernel/mem_protect/syscalls/src/test_syscalls.h
@@ -16,6 +16,11 @@ __syscall int to_copy(char *dest);
 
 __syscall size_t string_nlen(char *src, size_t maxlen, int *err);
 
+__syscall unsigned int more_args(unsigned int arg1, unsigned int arg2,
+				 unsigned int arg3, unsigned int arg4,
+				 unsigned int arg5, unsigned int arg6,
+				 unsigned int arg7);
+
 #include <syscalls/test_syscalls.h>
 
 #endif /* _TEST_SYSCALLS_H_ */
