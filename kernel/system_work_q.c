db2363c3a08ab9242ec136e7ceca5a27a61ea8dd
Peter Bigot <peter.bigot@nordicsemi.no>
2020-10-28 11:24:05 -0500

    kernel: add new work queue implementation
    This commit provides a complete reimplementation of the work queue
    infrastructure intended to eliminate the race conditions and feature
    gaps in the existing implementation.
    
    Both bare and delayable work structures are supported.  Items can be
    submitted; delayable items can be scheduled for submission at a future
    time.  Items can be delayed, queued, and running all at the same time.
    A running item can also be canceling.
    
    The new implementation:
    * replaces "pending" with "busy" which identifies the active states;
    * supports canceling delayed and submitted items;
    * prevents resubmission of a item being canceled until cancellation
      completes;
    * supports waiting for cancellation to complete;
    * supports flushing a work item (waiting for the last submission to
      complete without preventing resubmission);
    * supports waiting for a queue to drain (only allows resubmission from
      the work thread);
    * supports stopping a work queue in conjunction with draining it;
    * prevents handler-reentrancy during resubmission.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- kernel/system_work_q.c
+++ kernel/system_work_q.c
@@ -14,19 +14,31 @@
 #include <kernel.h>
 #include <init.h>
 
-K_KERNEL_STACK_DEFINE(sys_work_q_stack, CONFIG_SYSTEM_WORKQUEUE_STACK_SIZE);
+static K_KERNEL_STACK_DEFINE(sys_work_q_stack,
+			     CONFIG_SYSTEM_WORKQUEUE_STACK_SIZE);
 
 struct k_work_q k_sys_work_q;
 
 static int k_sys_work_q_init(const struct device *dev)
 {
 	ARG_UNUSED(dev);
+	struct k_work_queue_config cfg = {
+		.name = "sysworkq",
+		.no_yield = IS_ENABLED(CONFIG_SYSTEM_WORKQUEUE_NO_YIELD),
+	};
 
+#ifdef CONFIG_KERNEL_WORK1
 	k_work_q_start(&k_sys_work_q,
 		       sys_work_q_stack,
 		       K_KERNEL_STACK_SIZEOF(sys_work_q_stack),
 		       CONFIG_SYSTEM_WORKQUEUE_PRIORITY);
 	k_thread_name_set(&k_sys_work_q.thread, "sysworkq");
+#else /* CONFIG_KERNEL_WORK1 */
+	k_work_queue_start(&k_sys_work_q,
+			    sys_work_q_stack,
+			    K_KERNEL_STACK_SIZEOF(sys_work_q_stack),
+			    CONFIG_SYSTEM_WORKQUEUE_PRIORITY, &cfg);
+#endif /* CONFIG_KERNEL_WORK1 */
 
 	return 0;
 }
