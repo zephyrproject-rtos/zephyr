fa08a7d5a714f68064135dc13d6bf69c72cb1e2f
Wentong Wu <wentong.wu@intel.com>
2020-10-30 11:33:13 +0800

    logging: add STP transport and raw data output support for syst
    Add STP transport support for MIPI SyS-T with Kconfig MIPI_SYST_STP,
    this is following SyS-T spec Section 7.
    And with Kconfig MIPI_SYST_RAW_DATA, add raw data output support for
    MIPI SyS-T protocol stack.
    
    Signed-off-by: Wentong Wu <wentong.wu@intel.com>

--- subsys/logging/mipi_syst/platform.h
+++ subsys/logging/mipi_syst/platform.h
@@ -13,11 +13,29 @@
 extern "C" {
 #endif
 
+#if defined(CONFIG_MIPI_SYST_STP)
+/*
+ * Structure generating STP protocol data
+ */
+struct stp_writer_data {
+	mipi_syst_u8   byteDone;
+	mipi_syst_u8   current;
+	mipi_syst_u16  master;
+	mipi_syst_u16  channel;
+	mipi_syst_u64  recordCount;
+	mipi_syst_u64  timestamp;
+};
+#endif
+
 #if defined(MIPI_SYST_PCFG_ENABLE_PLATFORM_STATE_DATA)
 /*
  * Platform specific SyS-T global state extension
  */
 struct mipi_syst_platform_state {
+#if defined(CONFIG_MIPI_SYST_STP)
+	struct stp_writer_data *stpWriter;
+#endif
+
 	void (*write_d8)(struct mipi_syst_handle *systh, mipi_syst_u8 v);
 	void (*write_d16)(struct mipi_syst_handle *systh, mipi_syst_u16 v);
 	void (*write_d32)(struct mipi_syst_handle *systh, mipi_syst_u32 v);
@@ -25,6 +43,8 @@ struct mipi_syst_platform_state {
 	void (*write_d64)(struct mipi_syst_handle *systh, mipi_syst_u64 v);
 #endif
 	void (*write_d32ts)(struct mipi_syst_handle *systh, mipi_syst_u32 v);
+	void (*write_d32mts)(struct mipi_syst_handle *systh, mipi_syst_u32 v);
+	void (*write_d64mts)(struct mipi_syst_handle *systh, mipi_syst_u64 v);
 	void (*write_flag)(struct mipi_syst_handle *systh);
 };
 
@@ -33,6 +53,10 @@ struct mipi_syst_platform_state {
  */
 struct mipi_syst_platform_handle {
 	mipi_syst_u32 flag;
+#if defined(CONFIG_MIPI_SYST_STP)
+	mipi_syst_u32 master;
+	mipi_syst_u32 channel;
+#endif
 	struct log_output *log_output;
 };
 
@@ -57,6 +81,12 @@ struct mipi_syst_platform_handle {
 #define MIPI_SYST_OUTPUT_D32TS(syst_handle, data) \
 	((syst_handle)->systh_header-> \
 		systh_platform.write_d32ts((syst_handle), (data)))
+#define MIPI_SYST_OUTPUT_D32MTS(syst_handle, data) \
+	((syst_handle)->systh_header-> \
+		systh_platform.write_d32mts((syst_handle), (data)))
+#define MIPI_SYST_OUTPUT_D64MTS(syst_handle, data) \
+	((syst_handle)->systh_header-> \
+		systh_platform.write_d64mts((syst_handle), (data)))
 #define MIPI_SYST_OUTPUT_FLAG(syst_handle) \
 	((syst_handle)->systh_header-> \
 		systh_platform.write_flag((syst_handle)))
