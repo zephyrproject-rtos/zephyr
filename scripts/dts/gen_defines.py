4e1d17964aaef61fb966515a8f81e813f44bae72
Martí Bolívar <marti.bolivar@nordicsemi.no>
2020-10-30 17:45:27 -0700

    scripts: dts: gen_defines: don't include descriptions
    Before we had a bindings index in the documentation, the generated
    header file was (somewhat unfortunately) often our best reference for
    what a particular binding or property within a binding ends up doing,
    so it made good sense to put the description in the generated file.
    
    Now that we have HTML documentation that's a bit more digestible than
    the generated file, though, we can just point users at that. Do that
    and remove the inline description from the generated file.
    
    This makes it possible to put C-style multiline comments in the
    descriptions themselves, which will be done in subsequent patches.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- scripts/dts/gen_defines.py
+++ scripts/dts/gen_defines.py
@@ -192,11 +192,19 @@ Binding (compatible = {node.matching_compat}):
 """
 
     if node.description:
-        # Indent description by two spaces
-        s += "\nDescription:\n" + \
-            "\n".join("  " + line for line in
-                      node.description.splitlines()) + \
-            "\n"
+        # We used to put descriptions in the generated file, but
+        # devicetree bindings now have pages in the HTML
+        # documentation. Let users who are accustomed to digging
+        # around in the generated file where to find the descriptions
+        # now.
+        #
+        # Keeping them here would mean that the descriptions
+        # themselves couldn't contain C multi-line comments, which is
+        # inconvenient when we want to do things like quote snippets
+        # of .dtsi files within the snippets themselves, or otherwise
+        # include the characters "*/" in order.
+        s += ("\n(Descriptions have moved to the Devicetree Bindings Index\n"
+              "in the documentation.)\n")
 
     out_comment(s)
 
