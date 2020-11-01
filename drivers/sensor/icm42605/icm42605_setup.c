cc56fb524758e6b0d9eaba86f129071b9ed99919
JuHyun Kim <jkim@invensense.com>
2020-11-01 05:12:57 -0800

    drivers: sensor: Initial driver of Invensense ICM42605
    Initial driver and sample application of
    TDK Invensense ICM42605 6-axis motion sensor.
    This driver provide DTS for nRF52 DK board DTS setting.
    
    Providing features are below.
    
    Sensor data streaming - Accel, gyro
    Tap, Double tap triggering.
    Set/Get FSR, ODR by set attr API
    Support multi instance feature.
    
    Signed-off-by: JuHyun Kim <jkim@invensense.com>

---
+++ drivers/sensor/icm42605/icm42605_setup.c
@@ -0,0 +1,447 @@
+/*
+ * Copyright (c) 2020 TDK Invensense
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <sys/byteorder.h>
+#include <drivers/sensor.h>
+#include <logging/log.h>
+
+#include "icm42605.h"
+#include "icm42605_reg.h"
+#include "icm42605_spi.h"
+
+LOG_MODULE_REGISTER(ICM42605, CONFIG_SENSOR_LOG_LEVEL);
+
+int icm42605_set_fs(const struct device *dev, uint16_t a_sf, uint16_t g_sf)
+{
+	uint8_t databuf;
+	int result;
+
+	result = inv_spi_read(REG_ACCEL_CONFIG0, &databuf, 1);
+	if (result) {
+		return result;
+	}
+	databuf &= ~BIT_ACCEL_FSR;
+
+	databuf |= a_sf;
+
+	result = inv_spi_single_write(REG_ACCEL_CONFIG0, &databuf);
+
+	result = inv_spi_read(REG_GYRO_CONFIG0, &databuf, 1);
+
+	if (result) {
+		return result;
+	}
+
+	databuf &= ~BIT_GYRO_FSR;
+	databuf |= g_sf;
+
+	result = inv_spi_single_write(REG_GYRO_CONFIG0, &databuf);
+
+	if (result) {
+		return result;
+	}
+
+	return 0;
+}
+
+int icm42605_set_odr(const struct device *dev, uint16_t a_rate, uint16_t g_rate)
+{
+	uint8_t databuf;
+	int result;
+
+	if (a_rate > 8000 || g_rate > 8000 ||
+	    a_rate < 1 || g_rate < 12) {
+		LOG_ERR("Not supported frequency");
+		return -ENOTSUP;
+	}
+
+	result = inv_spi_read(REG_ACCEL_CONFIG0, &databuf, 1);
+
+	if (result) {
+		return result;
+	}
+
+	databuf &= ~BIT_ACCEL_ODR;
+
+	if (a_rate > 4000) {
+		databuf |= BIT_ACCEL_ODR_8000;
+	} else if (a_rate > 2000) {
+		databuf |= BIT_ACCEL_ODR_4000;
+	} else if (a_rate > 1000) {
+		databuf |= BIT_ACCEL_ODR_2000;
+	} else if (a_rate > 500) {
+		databuf |= BIT_ACCEL_ODR_1000;
+	} else if (a_rate > 200) {
+		databuf |= BIT_ACCEL_ODR_500;
+	} else if (a_rate > 100) {
+		databuf |= BIT_ACCEL_ODR_200;
+	} else if (a_rate > 50) {
+		databuf |= BIT_ACCEL_ODR_100;
+	} else if (a_rate > 25) {
+		databuf |= BIT_ACCEL_ODR_50;
+	} else if (a_rate > 12) {
+		databuf |= BIT_ACCEL_ODR_25;
+	} else if (a_rate > 6) {
+		databuf |= BIT_ACCEL_ODR_12;
+	} else if (a_rate > 3) {
+		databuf |= BIT_ACCEL_ODR_6;
+	} else if (a_rate > 1) {
+		databuf |= BIT_ACCEL_ODR_3;
+	} else {
+		databuf |= BIT_ACCEL_ODR_1;
+	}
+
+	result = inv_spi_single_write(REG_ACCEL_CONFIG0, &databuf);
+
+	if (result) {
+		return result;
+	}
+
+	LOG_DBG("Write Accel ODR 0x%X", databuf);
+
+	result = inv_spi_read(REG_GYRO_CONFIG0, &databuf, 1);
+
+	if (result) {
+		return result;
+	}
+
+	databuf &= ~BIT_GYRO_ODR;
+
+	if (g_rate > 4000) {
+		databuf |= BIT_GYRO_ODR_8000;
+	} else if (g_rate > 2000) {
+		databuf |= BIT_GYRO_ODR_4000;
+	} else if (g_rate > 1000) {
+		databuf |= BIT_GYRO_ODR_2000;
+	} else if (g_rate > 500) {
+		databuf |= BIT_GYRO_ODR_1000;
+	} else if (g_rate > 200) {
+		databuf |= BIT_GYRO_ODR_500;
+	} else if (g_rate > 100) {
+		databuf |= BIT_GYRO_ODR_200;
+	} else if (g_rate > 50) {
+		databuf |= BIT_GYRO_ODR_100;
+	} else if (g_rate > 25) {
+		databuf |= BIT_GYRO_ODR_50;
+	} else if (g_rate > 12) {
+		databuf |= BIT_GYRO_ODR_25;
+	} else {
+		databuf |= BIT_GYRO_ODR_12;
+	}
+
+	LOG_DBG("Write GYRO ODR 0x%X", databuf);
+
+	result = inv_spi_single_write(REG_GYRO_CONFIG0, &databuf);
+	if (result) {
+		return result;
+	}
+
+	return result;
+}
+
+int icm42605_sensor_init(const struct device *dev)
+{
+	int result = 0;
+	uint8_t v;
+
+	result = inv_spi_read(REG_WHO_AM_I, &v, 1);
+
+	if (result) {
+		return result;
+	}
+
+	LOG_DBG("WHO AM I : 0x%X", v);
+
+	result = inv_spi_read(REG_DEVICE_CONFIG, &v, 1);
+
+	if (result) {
+		LOG_DBG("read REG_DEVICE_CONFIG_REG failed");
+		return result;
+	}
+
+	v |= BIT_SOFT_RESET;
+
+	result = inv_spi_single_write(REG_DEVICE_CONFIG, &v);
+
+	if (result) {
+		LOG_ERR("write REG_DEVICE_CONFIG failed");
+		return result;
+	}
+
+	/* Need at least 10ms after soft reset */
+	k_msleep(10);
+
+	v = BIT_GYRO_AFSR_MODE_HFS | BIT_ACCEL_AFSR_MODE_HFS | BIT_CLK_SEL_PLL;
+
+	result = inv_spi_single_write(REG_INTF_CONFIG1, &v);
+
+	if (result) {
+		LOG_ERR("write REG_INTF_CONFIG1 failed");
+		return result;
+	}
+
+	v = BIT_EN_DREG_FIFO_D2A |
+	    BIT_TMST_TO_REGS_EN |
+	    BIT_TMST_EN;
+
+	result = inv_spi_single_write(REG_TMST_CONFIG, &v);
+
+	if (result) {
+		LOG_ERR("Write REG_TMST_CONFIG failed");
+		return result;
+	}
+
+	result = inv_spi_read(REG_INTF_CONFIG0, &v, 1);
+
+	if (result) {
+		LOG_ERR("Read REG_INTF_CONFIG0 failed");
+		return result;
+	}
+
+	LOG_DBG("Read REG_INTF_CONFIG0 0x%X", v);
+
+	v |= BIT_UI_SIFS_DISABLE_I2C;
+
+	result = inv_spi_single_write(REG_INTF_CONFIG0, &v);
+
+	if (result) {
+		LOG_ERR("Write REG_INTF_CONFIG failed");
+		return result;
+	}
+
+	v = 0;
+	result = inv_spi_single_write(REG_INT_CONFIG1, &v);
+
+	if (result) {
+		return result;
+	}
+
+	result = inv_spi_single_write(REG_PWR_MGMT0, &v);
+
+	if (result) {
+		return result;
+	}
+
+	return 0;
+}
+
+int icm42605_turn_on_fifo(const struct device *dev)
+{
+	const struct icm42605_data *drv_data = dev->data;
+
+	uint8_t int0_en, fifo_en;
+	uint8_t burst_read[3];
+	int result;
+	uint8_t v = 0;
+
+	v = BIT_FIFO_MODE_BYPASS;
+	result = inv_spi_single_write(REG_FIFO_CONFIG, &v);
+	if (result) {
+		return result;
+	}
+
+	v = 0;
+	result = inv_spi_single_write(REG_FIFO_CONFIG1, &v);
+	if (result) {
+		return result;
+	}
+
+	result = inv_spi_read(REG_FIFO_COUNTH, burst_read, 2);
+	if (result) {
+		return result;
+	}
+
+	result = inv_spi_read(REG_FIFO_DATA, burst_read, 3);
+	if (result) {
+		return result;
+	}
+
+	v = BIT_FIFO_MODE_STREAM;
+	result = inv_spi_single_write(REG_FIFO_CONFIG, &v);
+	if (result) {
+		return result;
+	}
+
+	fifo_en |= (BIT_FIFO_ACCEL_EN  | BIT_FIFO_GYRO_EN | BIT_FIFO_WM_TH);
+
+	result = inv_spi_single_write(REG_FIFO_CONFIG1, &fifo_en);
+	if (result) {
+		return result;
+	}
+
+	int0_en = BIT_INT_UI_DRDY_INT1_EN;
+	result = inv_spi_single_write(REG_INT_SOURCE0, &int0_en);
+	if (result) {
+		return result;
+	}
+
+	if (drv_data->tap_en) {
+		v = BIT_TAP_ENABLE;
+		result = inv_spi_single_write(REG_APEX_CONFIG0, &v);
+		if (result) {
+			return result;
+		}
+
+		v = BIT_DMP_INIT_EN;
+		result = inv_spi_single_write(REG_SIGNAL_PATH_RESET, &v);
+		if (result) {
+			return result;
+		}
+
+		v = BIT_BANK_SEL_4;
+		result = inv_spi_single_write(REG_BANK_SEL, &v);
+		if (result) {
+			return result;
+		}
+
+		v = BIT_INT_STATUS_TAP_DET;
+		result = inv_spi_single_write(REG_INT_SOURCE6, &v);
+		if (result) {
+			return result;
+		}
+
+		v = BIT_BANK_SEL_0;
+		result = inv_spi_single_write(REG_BANK_SEL, &v);
+		if (result) {
+			return result;
+		}
+	}
+
+	LOG_DBG("turn on fifo done");
+	return 0;
+}
+
+int icm42605_turn_off_fifo(const struct device *dev)
+{
+	const struct icm42605_data *drv_data = dev->data;
+
+	uint8_t int0_en = 0;
+	uint8_t burst_read[3];
+	int result;
+	uint8_t v = 0;
+
+	v = BIT_FIFO_MODE_BYPASS;
+	result = inv_spi_single_write(REG_FIFO_CONFIG, &v);
+	if (result) {
+		return result;
+	}
+
+	v = 0;
+	result = inv_spi_single_write(REG_FIFO_CONFIG1, &v);
+	if (result) {
+		return result;
+	}
+
+	result = inv_spi_read(REG_FIFO_COUNTH, burst_read, 2);
+	if (result) {
+		return result;
+	}
+
+	result = inv_spi_read(REG_FIFO_DATA, burst_read, 3);
+	if (result) {
+		return result;
+	}
+
+	result = inv_spi_single_write(REG_INT_SOURCE0, &int0_en);
+	if (result) {
+		return result;
+	}
+
+	if (drv_data->tap_en) {
+		v = 0;
+		result = inv_spi_single_write(REG_APEX_CONFIG0, &v);
+		if (result) {
+			return result;
+		}
+
+		result = inv_spi_single_write(REG_SIGNAL_PATH_RESET, &v);
+		if (result) {
+			return result;
+		}
+
+		v = BIT_BANK_SEL_4;
+		result = inv_spi_single_write(REG_BANK_SEL, &v);
+		if (result) {
+			return result;
+		}
+
+		v = 0;
+		result = inv_spi_single_write(REG_INT_SOURCE6, &v);
+		if (result) {
+			return result;
+		}
+
+		v = BIT_BANK_SEL_0;
+		result = inv_spi_single_write(REG_BANK_SEL, &v);
+		if (result) {
+			return result;
+		}
+	}
+
+	return 0;
+}
+
+int icm42605_turn_on_sensor(const struct device *dev)
+{
+	struct icm42605_data *drv_data = dev->data;
+
+	uint8_t v = 0;
+	int result = 0;
+
+
+	if (drv_data->sensor_started) {
+		LOG_ERR("Sensor already started");
+		return -EALREADY;
+	}
+
+	icm42605_set_fs(dev, drv_data->accel_sf, drv_data->gyro_sf);
+
+	icm42605_set_odr(dev, drv_data->accel_hz, drv_data->gyro_hz);
+
+	v |= BIT_ACCEL_MODE_LNM;
+	v |= BIT_GYRO_MODE_LNM;
+
+	result = inv_spi_single_write(REG_PWR_MGMT0, &v);
+	if (result) {
+		return result;
+	}
+
+	/* Accelerometer sensor need at least 10ms startup time
+	 * Gyroscope sensor need at least 30ms startup time
+	 */
+	k_msleep(100);
+
+	icm42605_turn_on_fifo(dev);
+
+	drv_data->sensor_started = true;
+
+	return 0;
+}
+
+int icm42605_turn_off_sensor(const struct device *dev)
+{
+	uint8_t v = 0;
+	int result = 0;
+
+	result = inv_spi_read(REG_PWR_MGMT0, &v, 1);
+
+	v ^= BIT_ACCEL_MODE_LNM;
+	v ^= BIT_GYRO_MODE_LNM;
+
+	result = inv_spi_single_write(REG_PWR_MGMT0, &v);
+	if (result) {
+		return result;
+	}
+
+	/* Accelerometer sensor need at least 10ms startup time
+	 * Gyroscope sensor need at least 30ms startup time
+	 */
+	k_msleep(100);
+
+	icm42605_turn_off_fifo(dev);
+
+	return 0;
+}
---
+++ drivers/sensor/icm42605/icm42605_spi.c
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2020 TDK Invensense
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <device.h>
+#include <drivers/spi.h>
+#include <logging/log.h>
+#include <sys/__assert.h>
+
+LOG_MODULE_REGISTER(ICM42605, CONFIG_SENSOR_LOG_LEVEL);
+
+struct device *spi_dev;
+static struct spi_config *spi_cfg;
+
+int inv_spi_single_write(uint8_t reg, const uint8_t *data)
+{
+	int result;
+
+	const struct spi_buf buf[2] = {
+		{
+			.buf = &reg,
+			.len = 1,
+		},
+		{
+			.buf = data,
+			.len = 1,
+		}
+	};
+	const struct spi_buf_set tx = {
+		.buffers = buf,
+		.count = 2,
+	};
+
+	result = spi_write(spi_dev, spi_cfg, &tx);
+
+	if (result) {
+		return result;
+	}
+
+	return 0;
+}
+
+int inv_spi_read(uint8_t reg, uint8_t *data, size_t len)
+{
+	int result;
+	unsigned char tx_buffer[2] = { 0, };
+
+	tx_buffer[0] = 0x80 | reg;
+
+	const struct spi_buf tx_buf = {
+		.buf = tx_buffer,
+		.len = 1,
+	};
+	const struct spi_buf_set tx = {
+		.buffers = &tx_buf,
+		.count = 1,
+	};
+
+	struct spi_buf rx_buf[2] = {
+		{
+			.buf = tx_buffer,
+			.len = 1,
+		},
+		{
+			.buf = data,
+			.len = len,
+		}
+	};
+
+	const struct spi_buf_set rx = {
+		.buffers = rx_buf,
+		.count = 2,
+	};
+
+	result = spi_transceive(spi_dev, spi_cfg, &tx, &rx);
+
+	if (result) {
+		return result;
+	}
+
+	return 0;
+}
+
+int icm42605_spi_init(const struct device *spi_device,
+		      const struct spi_config *spi_config)
+{
+	__ASSERT_NO_MSG(spi_device);
+	__ASSERT_NO_MSG(spi_config);
+
+	spi_dev = spi_device;
+	spi_cfg = spi_config;
+
+	return 0;
+}
