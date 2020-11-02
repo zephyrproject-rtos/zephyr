8197f2350a1176c17c8386aa9f141e4fccce1954
Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>
2020-11-02 13:54:25 +0100

    Bluetooth: Audio: Build without Object Transfer Service
    Make it possible to build the audio shell when the Object Transfer
    Service (OTS) (and the Object Transfer Client) is not enabled.
    
    The affected parts are the Media Control Service and the media player.
    commands for this.  The change is done by adding "#ifdef-guards"
    around the OTS-related parts of the code and slight restructuring.
    
    Signed-off-by: Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>

--- subsys/bluetooth/host/audio/mpl.h
+++ subsys/bluetooth/host/audio/mpl.h
@@ -166,18 +166,24 @@ struct mpl_tseg_t {
 
 /* Tracks */
 struct mpl_track_t {
+#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
 	uint64_t             id;
+#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
 	char                 title[CONFIG_BT_MCS_TRACK_TITLE_MAX];
 	int32_t              duration;
 	struct mpl_tseg_t   *segment;
+#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
 	uint64_t             segments_id;
+#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
 	struct mpl_track_t  *prev;
 	struct mpl_track_t  *next;
 };
 
 /* Groups */
 struct mpl_group_t {
+#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
 	uint64_t             id;
+#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
 	char                 title[CONFIG_BT_MCS_GROUP_TITLE_MAX];
 	struct mpl_track_t  *track;
 	struct mpl_group_t  *parent;
@@ -188,7 +194,9 @@ struct mpl_group_t {
 /** @brief Media Player */
 struct mpl_mediaplayer_t {
 	char                name[CONFIG_BT_MCS_MEDIA_PLAYER_NAME_MAX];
+#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
 	uint64_t            icon_id;
+#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
 	char                icon_uri[CONFIG_BT_MCS_ICON_URI_MAX];
 	struct mpl_group_t *group;
 	int32_t             track_pos;
@@ -198,7 +206,9 @@ struct mpl_mediaplayer_t {
 	uint8_t             playing_order;
 	uint16_t            playing_orders_supported;
 	uint32_t            operations_supported;
+#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
 	uint64_t            search_results_id;
+#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
 	uint8_t             content_ctrl_id;
 };
 
@@ -216,6 +226,7 @@ struct mpl_op_ntf_t {
 	uint8_t result_code;
 };
 
+#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
 /** @brief Search control item */
 struct mpl_sci_t {
 	uint8_t len;                     /* Length of type and parameter */
@@ -228,12 +239,15 @@ struct mpl_search_t {
 	uint8_t len;
 	char    search[SEARCH_LEN_MAX]; /* Concatenated search control items */
 };	                                /* - (type, length, param) */
+#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
 
 int mpl_init(void);
 
 char *mpl_player_name_get(void);
 
+#ifdef CONFIG_BT_OTS
 uint64_t mpl_icon_id_get(void);
+#endif /* CONFIG_BT_OTS */
 
 char *mpl_icon_uri_get(void);
 
@@ -256,6 +270,7 @@ void mpl_playback_speed_cb(int8_t speed);
 int8_t mpl_seeking_speed_get(void);
 void mpl_seeking_speed_cb(int8_t speed);
 
+#ifdef CONFIG_BT_OTS
 uint64_t mpl_track_segments_id_get(void);
 
 uint64_t mpl_current_track_id_get(void);
@@ -272,6 +287,7 @@ void mpl_group_id_cb(uint64_t id);
 
 uint64_t mpl_parent_group_id_get(void);
 void mpl_parent_group_id_cb(uint64_t id);
+#endif /* CONFIG_BT_OTS */
 
 uint8_t mpl_playing_order_get(void);
 void mpl_playing_order_set(uint8_t order);
@@ -288,11 +304,13 @@ void mpl_operation_cb(struct mpl_op_ntf_t op_ntf);
 uint32_t mpl_operations_supported_get(void);
 void mpl_operations_supported_cb(uint32_t operations);
 
+#ifdef CONFIG_BT_OTS
 void mpl_scp_set(struct mpl_search_t search);
 void mpl_search_cb(uint8_t result_code);
 
 uint64_t mpl_search_results_id_get(void);
 void mpl_search_results_id_cb(uint64_t id);
+#endif /* CONFIG_BT_OTS */
 
 uint8_t mpl_content_ctrl_id_get(void);
 
