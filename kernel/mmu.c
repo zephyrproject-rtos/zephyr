ea6e4ad098d9337eebdb9c154dba0ebe7ab93986
Andrew Boie <andrew.p.boie@intel.com>
2020-11-04 13:31:14 -0800

    kernel: support non-identity RAM mapping
    Some platforms may have multiple RAM regions which are
    dis-continuous in the physical memory map. We really want
    these to be in a continuous virtual region, and we need to
    stop assuming that there is just one SRAM region that is
    identity-mapped.
    
    We no longer use CONFIG_SRAM_BASE_ADDRESS and CONFIG_SRAM_SIZE
    as the bounds of kernel RAM, and no longer assume in the core
    kernel that these are identity mapped at boot.
    
    Two new Kconfigs, CONFIG_KERNEL_VM_BASE and
    CONFIG_KERNEL_RAM_SIZE now indicate the bounds of this region
    in virtual memory.
    
    We are currently only memory-mapping physical device driver
    MMIO regions so we do not need virtual-to-physical calculations
    to re-map RAM yet. When the time comes an architecture interface
    will be defined for this.
    
    Platforms which just have one RAM region may continue to
    identity-map it.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- kernel/mmu.c
+++ kernel/mmu.c
@@ -20,14 +20,17 @@ LOG_MODULE_DECLARE(os);
 static struct k_spinlock mm_lock;
 
 /*
- * Overall virtual memory map. System RAM is identity-mapped:
+ * Overall virtual memory map. When the kernel starts, it is expected that all
+ * memory regions are mapped into one large virtual region at the beginning of
+ * CONFIG_KERNEL_VM_BASE. Unused virtual memory up to the limit noted by
+ * CONFIG_KERNEL_VM_SIZE may be used for runtime memory mappings.
  *
- * +--------------+ <- CONFIG_SRAM_BASE_ADDRESS
+ * +--------------+ <- CONFIG_KERNEL_VM_BASE
  * | Mapping for  |
  * | all RAM      |
  * |              |
  * |              |
- * +--------------+ <- CONFIG_SRAM_BASE_ADDRESS + CONFIG_SRAM_SIZE
+ * +--------------+ <- CONFIG_KERNEL_VM_BASE + CONFIG_KERNEL_RAM_SIZE
  * | Available    |    also the mapping limit as mappings grown downward
  * | virtual mem  |
  * |              |
@@ -39,7 +42,7 @@ static struct k_spinlock mm_lock;
  * | ...          |
  * +--------------+
  * | Mapping      |
- * +--------------+ <- CONFIG_SRAM_BASE_ADDRESS + CONFIG_KERNEL_VM_SIZE
+ * +--------------+ <- CONFIG_KERNEL_VM_BASE + CONFIG_KERNEL_VM_SIZE
  *
  * At the moment we just have one area for mappings and they are permanent.
  * This is under heavy development and may change.
@@ -50,21 +53,18 @@ static struct k_spinlock mm_lock;
   * z_mem_map() mappings start at the end of the address space, and grow
   * downward.
   *
-  * TODO: If we ever encounter a board with RAM in high enough memory
-  * such that there isn't room in the address space, define mapping_pos
-  * and mapping_limit such that we have mappings grow downward from the
-  * beginning of system RAM.
+  * All of this is under heavy development and is subject to change.
   */
 static uint8_t *mapping_pos =
-		(uint8_t *)((uintptr_t)CONFIG_SRAM_BASE_ADDRESS +
+		(uint8_t *)((uintptr_t)CONFIG_KERNEL_VM_BASE +
 			    (uintptr_t)CONFIG_KERNEL_VM_SIZE);
 
 /* Lower-limit of virtual address mapping. Immediately below this is the
  * permanent identity mapping for all SRAM.
  */
 static uint8_t *mapping_limit =
-	(uint8_t *)((uintptr_t)CONFIG_SRAM_BASE_ADDRESS +
-		    KB((size_t)CONFIG_SRAM_SIZE));
+	(uint8_t *)((uintptr_t)CONFIG_KERNEL_VM_BASE +
+		    (size_t)CONFIG_KERNEL_RAM_SIZE);
 
 size_t k_mem_region_align(uintptr_t *aligned_addr, size_t *aligned_size,
 			  uintptr_t phys_addr, size_t size, size_t align)
