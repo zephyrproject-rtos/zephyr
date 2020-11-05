74fd3a81435b431a7432df91f708a2821cbf5803
Peter Bigot <peter.bigot@nordicsemi.no>
2020-11-05 10:24:33 -0600

    libc: minimal: add maximum integer typedefs
    Use 64-bit as the maximum integer size.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- lib/libc/minimal/include/stdint.h
+++ lib/libc/minimal/include/stdint.h
@@ -17,6 +17,7 @@ extern "C" {
 #define INT16_MAX   __INT16_MAX__
 #define INT32_MAX   __INT32_MAX__
 #define INT64_MAX   __INT64_MAX__
+#define INTMAX_MAX  __INT64_MAX__
 
 #define INT8_MIN    (-INT8_MAX - 1)
 #define INT16_MIN   (-INT16_MAX - 1)
@@ -27,6 +28,7 @@ extern "C" {
 #define UINT16_MAX  __UINT16_MAX__
 #define UINT32_MAX  __UINT32_MAX__
 #define UINT64_MAX  __UINT64_MAX__
+#define UINTMAX_MAX __UINT64_MAX__
 
 #define INTPTR_MAX  __INTPTR_MAX__
 #define INTPTR_MIN  (-INTPTR_MAX - 1)
@@ -41,6 +43,7 @@ typedef __INT8_TYPE__		int8_t;
 typedef __INT16_TYPE__		int16_t;
 typedef __INT32_TYPE__		int32_t;
 typedef __INT64_TYPE__		int64_t;
+typedef __INT64_TYPE__		intmax_t;
 
 typedef __INT_FAST8_TYPE__	int_fast8_t;
 typedef __INT_FAST16_TYPE__	int_fast16_t;
@@ -56,6 +59,7 @@ typedef __UINT8_TYPE__		uint8_t;
 typedef __UINT16_TYPE__		uint16_t;
 typedef __UINT32_TYPE__		uint32_t;
 typedef __UINT64_TYPE__		uint64_t;
+typedef __UINT64_TYPE__		uintmax_t;
 
 typedef __UINT_FAST8_TYPE__	uint_fast8_t;
 typedef __UINT_FAST16_TYPE__	uint_fast16_t;
