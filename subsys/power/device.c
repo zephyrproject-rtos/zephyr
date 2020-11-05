99cb219edc6ec9cbabccb323541278ee94fe2d61
Kumar Gala <kumar.gala@linaro.org>
2020-11-05 05:43:02 -0600

    device: treat a NULL device_pm_control as device_pm_control_nop
    This is a step towards phasing out DEVICE_INIT and just having
    DEVICE_DEFINE.  To make it a little easier on users or DEVICE_DEFINE
    if they don't care about PM, they can just pass NULL for the
    pm_control_fn paramater.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- subsys/power/device.c
+++ subsys/power/device.c
@@ -160,7 +160,8 @@ void sys_pm_create_device_list(void)
 		const struct device *dev = &all_devices[pmi];
 
 		/* Ignore "device"s that don't support PM */
-		if (dev->device_pm_control == device_pm_control_nop) {
+		if ((dev->device_pm_control == NULL) ||
+		    (dev->device_pm_control == device_pm_control_nop)) {
 			continue;
 		}
 
