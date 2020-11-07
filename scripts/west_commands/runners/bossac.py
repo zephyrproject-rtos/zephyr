aa77428a841944c39e0df6d0e25ad41d568aa054
Gerson Fernando Budke <nandojve@gmail.com>
2020-11-07 01:08:53 -0300

    scripts: runners: bossac: Fix offset parameter
    The current bossac runner have multiple entries for flash offset
    parameter.  Remove offset parameter from command line and rework
    all infraestructure to get offset from device tree.  Add proper
    verification routines to validate configurations on board and
    device tree entries to fix SDK compatibility checks.
    
    Fixes #29312.
    
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

--- scripts/west_commands/runners/bossac.py
+++ scripts/west_commands/runners/bossac.py
@@ -1,29 +1,35 @@
 # Copyright (c) 2017 Linaro Limited.
+# Copyright (c) 2020 Gerson Fernando Budke <nandojve@gmail.com>
 #
 # SPDX-License-Identifier: Apache-2.0
 
 '''bossac-specific runner (flash only) for Atmel SAM microcontrollers.'''
 
-import subprocess
-
+import pathlib
+import pickle
 import platform
+import subprocess
+import sys
 
 from runners.core import ZephyrBinaryRunner, RunnerCaps, BuildConfiguration
+from zephyr_ext_common import ZEPHYR_SCRIPTS
+
+# This is needed to load edt.pickle files.
+sys.path.append(str(ZEPHYR_SCRIPTS / 'dts'))
+import edtlib  # pylint: disable=unused-import
 
 DEFAULT_BOSSAC_PORT = '/dev/ttyACM0'
 DEFAULT_BOSSAC_SPEED = '115200'
-
 
 class BossacBinaryRunner(ZephyrBinaryRunner):
     '''Runner front-end for bossac.'''
 
     def __init__(self, cfg, bossac='bossac', port=DEFAULT_BOSSAC_PORT,
-                 speed=DEFAULT_BOSSAC_SPEED, offset=None):
+                 speed=DEFAULT_BOSSAC_SPEED):
         super().__init__(cfg)
         self.bossac = bossac
         self.port = port
         self.speed = speed
-        self.offset = offset
 
     @classmethod
     def name(cls):
@@ -37,10 +43,6 @@ class BossacBinaryRunner(ZephyrBinaryRunner):
     def do_add_parser(cls, parser):
         parser.add_argument('--bossac', default='bossac',
                             help='path to bossac, default is bossac')
-        parser.add_argument('--offset', default=None,
-                            help='start erase/write/read/verify operation '
-                                 'at flash OFFSET; OFFSET must be aligned '
-                                 ' to a flash page boundary')
         parser.add_argument('--bossac-port', default=DEFAULT_BOSSAC_PORT,
                             help='serial port to use, default is ' +
                             DEFAULT_BOSSAC_PORT)
@@ -49,23 +51,9 @@ class BossacBinaryRunner(ZephyrBinaryRunner):
                             DEFAULT_BOSSAC_SPEED)
 
     @classmethod
-    def get_flash_offset(cls, cfg):
-        # Pull the bootloader size from the config
-        build_conf = BuildConfiguration(cfg.build_dir)
-        if build_conf['CONFIG_HAS_FLASH_LOAD_OFFSET']:
-            return build_conf['CONFIG_FLASH_LOAD_OFFSET']
-
-        return None
-
-    @classmethod
     def do_create(cls, cfg, args):
-        offset = args.offset
-
-        if offset is None:
-            offset = cls.get_flash_offset(cfg)
         return BossacBinaryRunner(cfg, bossac=args.bossac,
-                                  port=args.bossac_port, speed=args.speed,
-                                  offset=offset)
+                                  port=args.bossac_port, speed=args.speed)
 
     def read_help(self):
         """Run bossac --help and return the output as a list of lines"""
@@ -96,27 +84,65 @@ class BossacBinaryRunner(ZephyrBinaryRunner):
                 return True
         return False
 
-    def get_offset(self, supports_offset):
+    def is_partition_enabled(self):
+        build_conf = BuildConfiguration(self.cfg.build_dir)
+
+        if 'CONFIG_USE_DT_CODE_PARTITION' not in build_conf:
+            return False
+
+        return True
+
+    def get_chosen_code_partition_node(self):
+        # Get the EDT Node corresponding to the zephyr,code-partition
+        # chosen DT node
+
+        # Ensure the build directory has a compiled DTS file
+        # where we expect it to be.
+        b = pathlib.Path(self.cfg.build_dir)
+        edt_pickle = b / 'zephyr' / 'edt.pickle'
+        if not edt_pickle.is_file():
+            error_msg = "can't load devicetree; expected to find:" \
+	                + str(edt_pickle)
+
+            raise RuntimeError(error_msg)
+
+        # Load the devicetree.
+        with open(edt_pickle, 'rb') as f:
+            edt = pickle.load(f)
+
+        return edt.chosen_node('zephyr,code-partition')
+
+    def get_board_name(self):
+        build_conf = BuildConfiguration(self.cfg.build_dir)
+
+        if 'CONFIG_BOARD' not in build_conf:
+            return '<board>'
+
+        return build_conf['CONFIG_BOARD'][0].replace('"', '')
+
+    def get_dts_img_offset(self):
+        build_conf = BuildConfiguration(self.cfg.build_dir)
+
+        if build_conf['CONFIG_HAS_FLASH_LOAD_OFFSET']:
+            return build_conf['CONFIG_FLASH_LOAD_OFFSET']
+
+        return 0
+
+    def get_image_offset(self, supports_offset):
         """Validates and returns the flash offset"""
-        if supports_offset:
-            if self.offset is not None:
-                return self.offset
 
-            self.logger.warning(
-                'This version of BOSSA supports the --offset flag but' +
-                ' no offset was supplied. If flashing fails, then' +
-                ' please specify the size of the bootloader by adding' +
-                ' the --offset= flag to board_runner_args in board.cmake')
-            return self.offset
+        dts_img_offset = self.get_dts_img_offset()
 
-        if self.offset is not None:
-            self.logger.warning(
-                'This version of BOSSA does not support the --offset flag.' +
-                ' Please see' +
-                ' https://github.com/zephyrproject-rtos/sdk-ng/issues/234' +
-                ' which tracks updating the Zephyr SDK.')
+        if int(str(dts_img_offset), 16) > 0:
+            if not supports_offset:
+                old_sdk = 'This version of BOSSA does not support the' \
+                          ' --offset flag. Please upgrade to a newer Zephyr' \
+                          ' SDK version >= 0.12.0.'
+                raise RuntimeError(old_sdk)
 
-        return self.offset
+            return dts_img_offset
+
+        return None
 
     def set_serial_config(self):
         if platform.system() == 'Linux':
@@ -132,17 +158,35 @@ class BossacBinaryRunner(ZephyrBinaryRunner):
         cmd_flash = [self.bossac, '-p', self.port, '-R', '-e', '-w', '-v',
                      '-b', self.cfg.bin_file]
 
-        offset = self.get_offset(self.supports('--offset'))
+        dt_chosen_code_partition_nd = self.get_chosen_code_partition_node()
 
-        if offset:
-            cmd_flash += ['-o', '%s' % offset]
+        if self.is_partition_enabled():
+            if dt_chosen_code_partition_nd is None:
+                error_msg = 'The device tree zephyr,code-partition chosen' \
+                            ' node must be defined.'
+
+                raise RuntimeError(error_msg)
+
+            offset = self.get_image_offset(self.supports('--offset'))
+
+            if offset is not None and int(str(offset), 16) > 0:
+                cmd_flash += ['-o', '%s' % offset]
+
+        elif dt_chosen_code_partition_nd is not None:
+            error_msg = 'There is no CONFIG_USE_DT_CODE_PARTITION Kconfig' \
+                        ' defined at ' + self.get_board_name() + \
+                        '_defconfig file.\n This means that' \
+                        ' zephyr,code-partition device tree node should not' \
+                        ' be defined. Check Zephyr SAM-BA documentation.'
+
+            raise RuntimeError(error_msg)
 
         return cmd_flash
 
     def do_run(self, command, **kwargs):
         if platform.system() == 'Windows':
             msg = 'CAUTION: BOSSAC runner not support on Windows!'
-            raise NotImplementedError(msg)
+            raise RuntimeError(msg)
 
         self.require(self.bossac)
         self.set_serial_config()
