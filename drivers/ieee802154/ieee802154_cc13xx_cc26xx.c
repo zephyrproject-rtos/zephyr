3d85dd0973161c3d4073ab1f1e97f5804390637b
Christopher Friedt <chrisfriedt@gmail.com>
2020-11-09 12:15:32 -0500

    drivers: ieee802154: cc13xx_cc26xx: use multi-protocol radio patch
    This change enables the multi-protocol rf patch to be used for
    the cc13xx_cc26xx IEEE 802.15.4 2.4 GHz PHY, which allows both
    the 2.4 GHz and Sub GHz PHY to be used simultaneously.
    
    Eventually, BLE will also work simultaneously on 2.4 GHz (with
    arbitration).
    
    Fixes #29883
    
    Signed-off-by: Christopher Friedt <chrisfriedt@gmail.com>

--- drivers/ieee802154/ieee802154_cc13xx_cc26xx.c
+++ drivers/ieee802154/ieee802154_cc13xx_cc26xx.c
@@ -23,7 +23,7 @@ LOG_MODULE_REGISTER(ieee802154_cc13xx_cc26xx);
 #include <driverlib/rfc.h>
 #include <inc/hw_ccfg.h>
 #include <inc/hw_fcfg1.h>
-#include <rf_patches/rf_patch_cpe_ieee_802_15_4.h>
+#include <rf_patches/rf_patch_cpe_multi_protocol.h>
 
 #include <ti/drivers/rf/RF.h>
 
@@ -536,8 +536,8 @@ static int ieee802154_cc13xx_cc26xx_init(const struct device *dev)
 	RF_Params rf_params;
 	RF_EventMask reason;
 	RF_Mode rf_mode = {
-		.rfMode      = RF_MODE_IEEE_15_4,
-		.cpePatchFxn = &rf_patch_cpe_ieee_802_15_4,
+		.rfMode      = RF_MODE_MULTIPLE,
+		.cpePatchFxn = &rf_patch_cpe_multi_protocol,
 	};
 	struct ieee802154_cc13xx_cc26xx_data *drv_data = get_dev_data(dev);
 
