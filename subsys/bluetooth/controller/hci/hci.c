9624d9bc6ca77d8a27ebe87fe4c2d6e459941b98
Emil Gydesen <emil.gydesen@nordicsemi.no>
2020-11-10 13:51:28 +0100

    Bluetooth: Controller: Change node_rx_type to auto incrementing enum
    Instead of having explicit values for each entry in the enum that
    has to be updated for each new entry (thus requiring manual work
    which can also be error-prone), the enum will now be
    self-incrementing, to make it easier and safer to add new
    entries.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/controller/hci/hci.c
+++ subsys/bluetooth/controller/hci/hci.c
@@ -4661,7 +4661,7 @@ static void encode_control(struct node_rx_pdu *node_rx,
 #endif /* CONFIG_BT_HCI_MESH_EXT */
 
 #if defined(CONFIG_BT_CTLR_USER_EXT)
-	case NODE_RX_TYPE_USER_START ... NODE_RX_TYPE_USER_END:
+	case NODE_RX_TYPE_USER_START ... NODE_RX_TYPE_USER_END - 1:
 		hci_user_ext_encode_control(node_rx, pdu_data, buf);
 		return;
 #endif /* CONFIG_BT_CTLR_USER_EXT */
@@ -5053,7 +5053,7 @@ uint8_t hci_get_class(struct node_rx_pdu *node_rx)
 #endif /* CONFIG_BT_CONN */
 
 #if defined(CONFIG_BT_CTLR_USER_EXT)
-		case NODE_RX_TYPE_USER_START ... NODE_RX_TYPE_USER_END:
+		case NODE_RX_TYPE_USER_START ... NODE_RX_TYPE_USER_END - 1:
 			return hci_user_ext_get_class(node_rx);
 #endif /* CONFIG_BT_CTLR_USER_EXT */
 
