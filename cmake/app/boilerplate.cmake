bbaf00b8e444b52d8cece71048270dcb3be40b1b
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2020-11-12 13:15:15 +0100

    cmake: extend zephyr_file(<mode>) to support searching of config files
    This commit extends zephyr_file(<mode>) to support `CONF_FILES` which
    allows for finding and appending Zephyr Kconfig fragments and DTS
    overlays.
    
    Introducing mode:CONF_FILES allows have a uniform way of looking up
    <board>.conf and <board>.overlay, and ensure that extending such
    functionality keeps identical behaviour in shields, tests, and samples.
    
    Using zephyr_file(CONF_FILES) a lot of duplicated code can be removed.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/app/boilerplate.cmake
+++ cmake/app/boilerplate.cmake
@@ -302,28 +302,11 @@ foreach(root ${BOARD_ROOT})
         shield_dts_files
         ${shield_dir}/${s_path}
         )
+
       list(APPEND
         shield_dts_fixups
         ${shield_dir}/${s_dir}/dts_fixup.h
         )
-
-      # search for shield/boards/board.overlay file
-      if(EXISTS ${shield_dir}/${s_dir}/boards/${BOARD}.overlay)
-        # add shield/board overlay to the shield overlays list
-        list(APPEND
-          shield_dts_files
-          ${shield_dir}/${s_dir}/boards/${BOARD}.overlay
-          )
-      endif()
-
-      # search for shield/boards/shield/board.overlay file
-      if(EXISTS ${shield_dir}/${s_dir}/boards/${s}/${BOARD}.overlay)
-        # add shield/board overlay to the shield overlays list
-        list(APPEND
-          shield_dts_files
-          ${shield_dir}/${s_dir}/boards/${s}/${BOARD}.overlay
-          )
-      endif()
 
       # search for shield/shield.conf file
       if(EXISTS ${shield_dir}/${s_dir}/${s}.conf)
@@ -334,23 +317,14 @@ foreach(root ${BOARD_ROOT})
           )
       endif()
 
-      # search for shield/boards/board.conf file
-      if(EXISTS ${shield_dir}/${s_dir}/boards/${BOARD}.conf)
-        # add HW specific board.conf to the shield config list
-        list(APPEND
-          shield_conf_files
-          ${shield_dir}/${s_dir}/boards/${BOARD}.conf
-          )
-      endif()
-
-      # search for shield/boards/shield/board.conf file
-      if(EXISTS ${shield_dir}/${s_dir}/boards/${s}/${BOARD}.conf)
-        # add HW specific board.conf to the shield config list
-        list(APPEND
-          shield_conf_files
-          ${shield_dir}/${s_dir}/boards/${s}/${BOARD}.conf
-          )
-      endif()
+      zephyr_file(CONF_FILES ${shield_dir}/${s_dir}/boards
+                  DTS   shield_dts_files
+                  KCONF shield_conf_files
+      )
+      zephyr_file(CONF_FILES ${shield_dir}/${s_dir}/boards/${s}
+                  DTS   shield_dts_files
+                  KCONF shield_conf_files
+      )
     endforeach()
   endif()
 endforeach()
@@ -409,9 +383,7 @@ if(DEFINED CONF_FILE)
       if(NOT IS_ABSOLUTE ${CONF_FILE_DIR})
         set(CONF_FILE_DIR ${APPLICATION_SOURCE_DIR}/${CONF_FILE_DIR})
       endif()
-      if(EXISTS ${CONF_FILE_DIR}/boards/${BOARD}_${CMAKE_MATCH_1}.conf)
-        list(APPEND CONF_FILE ${CONF_FILE_DIR}/boards/${BOARD}_${CMAKE_MATCH_1}.conf)
-      endif()
+      zephyr_file(CONF_FILES ${CONF_FILE_DIR}/boards KCONF CONF_FILE BUILD ${CMAKE_MATCH_1})
     endif()
   endif()
 elseif(CACHED_CONF_FILE)
