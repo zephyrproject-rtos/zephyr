a28da5118d38bc84d8ff09fdaff64e1e7c2a4aae
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2020-11-16 16:55:29 +0100

    tfm: Configure TFM via Kconfig instead of Cmake
    Add Kconfig options that will be used by the module
    to call the function with the desired parameters.
    Refactor the tfm_integration samples and
    the supported boards.
    
    Update west.yml to bring in Cmake changes that use the new KConfigs.
    
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- boards/arm/nrf9160dk_nrf9160/board.cmake
+++ boards/arm/nrf9160dk_nrf9160/board.cmake
@@ -1,9 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
-# Set the corresponding TF-M target platform when building for the Non-Secure
-# version of the board (Application MCU).
 if(CONFIG_BOARD_NRF9160DK_NRF9160NS)
-  set(TFM_TARGET_PLATFORM "nordic_nrf/nrf9160dk_nrf9160")
   set(TFM_PUBLIC_KEY_FORMAT "full")
 endif()
 
