e15297e7d9e38f86f4f2d16e99a75925a711a4b6
Martin Åberg <martin.aberg@gaisler.com>
2020-11-18 15:45:45 +0100

    cmake: Preparation for SDK 0.12 and SPARC
    Zephyr SDK version 0.12 is distributed with SPARC cmake target support,
    while version 0.11.3 and 0.11.4 need target.cmake from the Zephyr repo.
    
    This commit has been tested with:
    - zephyr-sdk-0.11.3 (currently used in CI)
    - zephyr-sdk-0.11.4
    - zephyr-sdk-0.12.0-beta-2
    
    This commit is expected to be compatible with
    - zephyr-sdk-0.12.0 (not yet released)
    
    Signed-off-by: Martin Åberg <martin.aberg@gaisler.com>

--- cmake/toolchain/zephyr/target.cmake
+++ cmake/toolchain/zephyr/target.cmake
@@ -4,8 +4,8 @@ if(${SDK_VERSION} VERSION_LESS_EQUAL 0.11.2)
   # For backward compatibility with 0.11.1 and 0.11.2
   # we need to source files from Zephyr repo
   include(${CMAKE_CURRENT_LIST_DIR}/${SDK_MAJOR_MINOR}/target.cmake)
-elseif("${ARCH}" STREQUAL "sparc")
-  # SDK 0.11.3, 0.11.4 and 0.12.0-beta-1 does not have SPARC target support.
+elseif(("${ARCH}" STREQUAL "sparc") AND (${SDK_VERSION} VERSION_LESS 0.12))
+  # SDK 0.11.3, 0.11.4 does not have SPARC target support.
   include(${CMAKE_CURRENT_LIST_DIR}/${SDK_MAJOR_MINOR}/target.cmake)
 else()
   include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/target.cmake)
