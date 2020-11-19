0b230ae6395182d008c273f1d7798e012a1c2af8
Piotr Pryga <piotr.pryga@nordicsemi.no>
2020-11-19 02:12:54 -0800

    Bluetooth: controller: Add DF initialization to controller init sequence
    Direction Finding requires initalization of a set of registers
    in Radio peripheral. Also it requires information about antenna
    matrix unit that are provided by end user in DTS overlay.
    Implemented initialization step is responsible for validation of
    hardware information delivered by DTS and setting up DF related
    registers in Radio.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/lll_df.h
+++ subsys/bluetooth/controller/ll_sw/lll_df.h
@@ -22,5 +22,8 @@ struct lll_df_adv_cfg {
 /* @brief Max supported CTE length in 8us units */
 #define LLL_DF_MAX_CTE_LEN 20
 
+int lll_df_init(void);
+int lll_df_reset(void);
+
 /* Provides number of available antennas for Direction Finding */
 uint8_t lll_df_ant_num_get(void);
--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_df.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_df.c
@@ -8,6 +8,34 @@
 #include "hal/radio_df.h"
 #include "lll_df.h"
 
+/* @brief Function performs common steps for initialization and reset
+ * of Direction Finding LLL module.
+ *
+ * @return	Zero in case of success, other value in case of failure.
+ */
+static int init_reset(void);
+
+/* @brief Function performs Direction Finding initialization
+ *
+ * @return	Zero in case of success, other value in case of failure.
+ */
+int lll_df_init(void)
+{
+	radio_df_ant_configure();
+
+	return init_reset();
+}
+
+
+/* @brief Function performs Direction Finding reset
+ *
+ * @return	Zero in case of success, other value in case of failure.
+ */
+int lll_df_reset(void)
+{
+	return init_reset();
+}
+
 /* @brief Function provides number of available antennas.
  *
  * The number of antenna is hardware defined and it is provided via devicetree.
@@ -17,4 +45,9 @@
 uint8_t lll_df_ant_num_get(void)
 {
 	return radio_df_ant_num_get();
+}
+
+static int init_reset(void)
+{
+	return 0;
 }
