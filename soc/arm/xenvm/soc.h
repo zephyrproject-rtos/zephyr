b07065d3f325d1d6a0def9ce7f57876ed959a178
Volodymyr Babchuk <volodymyr_babchuk@epam.com>
2020-11-20 17:39:46 +0200

    arm: aarch64: add Xen virtual machine support
    This commit adds minimal support for running zephyr as Xen guest. It
    does not use xen PV console, which is somewhat hard to implement, as it
    depends on xenbus infrastructure. Instead SBSA-compatible PL011 uart is
    used.
    
    Signed-off-by: Volodymyr Babchuk <volodymyr_babchuk@epam.com>

---
+++ soc/arm/xenvm/soc.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef _SOC_H_
+#define _SOC_H_
+
+#include <sys/util.h>
+
+#ifndef _ASMLANGUAGE
+
+#include <device.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _SOC_H_ */
