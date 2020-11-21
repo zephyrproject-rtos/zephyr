a20b3307c485b856e8539bc6580e6e46ab4792a5
Peng Fan <peng.fan@nxp.com>
2020-11-21 20:38:45 +0800

    arm: qemu_cortex_a53: support running in NS world
    Support qemu_cortex_a53 runs in Normal World
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- boards/arm/qemu_cortex_a53/board.cmake
+++ boards/arm/qemu_cortex_a53/board.cmake
@@ -5,9 +5,16 @@ set(EMU_PLATFORM qemu)
 set(QEMU_ARCH aarch64)
 
 set(QEMU_CPU_TYPE_${ARCH} cortex-a53)
+
+if(CONFIG_ARMV8_A_NS)
+set(QEMU_MACH virt,gic-version=3)
+else()
+set(QEMU_MACH virt,secure=on,gic-version=3)
+endif()
+
 set(QEMU_FLAGS_${ARCH}
   -cpu ${QEMU_CPU_TYPE_${ARCH}}
   -nographic
-  -machine virt,secure=on,gic-version=3
+  -machine ${QEMU_MACH}
   )
 board_set_debugger_ifnset(qemu)
