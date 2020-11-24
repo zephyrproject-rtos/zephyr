zephyrproject-rtos/zephyr

commit d6c30eead0381aa72d56ea8025ad0275ff4f140e

nrfjprog.py: Fail if hex file has UICR data and no --erase
Inspect the hex file with intelhex, and fail if the hex file has any
contents in the UICR area(s).
family == 'NRF52' still always does --sectoranduicrerase, but this
option is not available on other families.
Add --force command line option to proceed with flashing instead of
failing.

Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

scripts/west_commands/runners/nrfjprog.py
@@ -9,21 +9,34 @@ import os
 import shlex
 import sys
 from re import fullmatch, escape
+from intelhex import IntelHex
 
 from runners.core import ZephyrBinaryRunner, RunnerCaps
+
+# Helper function for inspecting hex files.
+# has_region returns True if hex file has any contents in a specific region
+# region_filter is a callable that takes an address as argument and
+# returns True if that address is in the region in question
+def has_region(regions, hex_file):
+    try:
+        ih = IntelHex(hex_file)
+        return any((len(ih[rs:re]) > 0) for (rs, re) in regions)
+    except FileNotFoundError:
+        return False
 
 
 class NrfJprogBinaryRunner(ZephyrBinaryRunner):
     '''Runner front-end for nrfjprog.'''
 
     def __init__(self, cfg, family, softreset, snr, erase=False,
-        tool_opt=[]):
+        tool_opt=[], force=False):
         super().__init__(cfg)
         self.hex_ = cfg.hex_file
         self.family = family
         self.softreset = softreset
         self.snr = snr
         self.erase = erase
+        self.force = force
 
         self.tool_opt = []
         for opts in [shlex.split(opt) for opt in tool_opt]:
@@ -51,12 +64,15 @@ class NrfJprogBinaryRunner(ZephyrBinaryRunner):
         parser.add_argument('--tool-opt', default=[], action='append',
                             help='''Additional options for nrfjprog,
                             e.g. "--recover"''')
+        parser.add_argument('--force', required=False,
+                            action='store_true',
+                            help='Flash even if the result cannot be guaranteed.')
 
     @classmethod
     def do_create(cls, cfg, args):
         return NrfJprogBinaryRunner(cfg, args.nrf_family, args.softreset,
                                     args.snr, erase=args.erase,
-                                    tool_opt=args.tool_opt)
+                                    tool_opt=args.tool_opt, force=args.force)
 
     def ensure_snr(self):
         if not self.snr or "*" in self.snr:
@@ -151,10 +167,25 @@ class NrfJprogBinaryRunner(ZephyrBinaryRunner):
                 program_cmd
             ])
         else:
-            if self.family == 'NRF52':
+            if self.family == 'NRF51':
+                commands.append(program_cmd + ['--sectorerase'])
+            elif self.family == 'NRF52':
                 commands.append(program_cmd + ['--sectoranduicrerase'])
             else:
-                commands.append(program_cmd + ['--sectorerase'])
+                uicr = {
+                    'NRF53': ((0x00FF8000, 0x00FF8800),
+                              (0x01FF8000, 0x01FF8800)),
+                    'NRF91': ((0x00FF8000, 0x00FF8800),),
+                }[self.family]
+
+                if not self.force and has_region(uicr, self.hex_):
+                    # Hex file has UICR contents.
+                    raise RuntimeError(
+                        'The hex file contains data placed in the UICR, which '
+                        'needs a full erase before reprogramming. Run west '
+                        'flash again with --force or --erase.')
+                else:
+                    commands.append(program_cmd + ['--sectorerase'])
 
         if self.family == 'NRF52' and not self.softreset:
             commands.extend([

.github/workflows/doc-build.yml
@@ -32,7 +32,7 @@ jobs:
         pip3 install setuptools
         pip3 install -r scripts/requirements-doc.txt
         pip3 install west>=0.6.2
-        pip3 install pyelftools canopen progress
+        pip3 install pyelftools canopen progress intelhex
 
     - name: west setup
       run: |

.github/workflows/west_cmds.yml
@@ -55,7 +55,7 @@ jobs:
     - name: install pytest
       run: |
         pip3 install wheel
-        pip3 install pytest west pyelftools canopen progress mypy
+        pip3 install pytest west pyelftools canopen progress mypy intelhex
     - name: run pytest-win
       if: runner.os == 'Windows'
       run: |

commit cb620aa7d2ec9f2ea0fed4f5814716ee8e46bcda

runners: nrfjprog: fixes for nRF53 and --tool-opt
On nRF53, the network core should have --recover added to its nrfjprog
options. For consistency, apply the same change to the application
core. This implies not using --sectorerase on that family, since it
cannot be combined with --recover.

Additionally, commit d6c30eead0 ("nrfjprog.py: Fail if hex file has
UICR data and no --erase") broke the guarantee that --tool-opt is
provided last on the command which is used for flashing. This is
important to make sure that the user's wish overrides the settings in
the runner file and is a general practice within the flash/debug
runners. Restore the desired behavior.

Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

scripts/west_commands/runners/nrfjprog.py
@@ -163,8 +163,13 @@ class NrfJprogBinaryRunner(ZephyrBinaryRunner):
                              format(self.hex_) +
                              'Try enabling CONFIG_BUILD_OUTPUT_HEX.')
 
-        program_cmd = ['nrfjprog', '--program', self.hex_, '-f', self.family,
-                    '--snr', board_snr] + self.tool_opt
+        program_cmd = ['nrfjprog', '--program', self.hex_]
+        if self.family == 'NRF53':
+            # Due to multi-core related issues, flashing
+            # requires --recover on the network core. We just
+            # add it on both cores for simplicity and consistency.
+            program_cmd.append('--recover')
+        program_cmd += ['-f', self.family, '--snr', board_snr]
 
         self.logger.info('Flashing file: {}'.format(self.hex_))
         if self.erase:
@@ -173,13 +178,15 @@ class NrfJprogBinaryRunner(ZephyrBinaryRunner):
                  '--eraseall',
                  '-f', self.family,
                  '--snr', board_snr],
-                program_cmd
+                program_cmd + self.tool_opt
             ])
         else:
             if self.family == 'NRF51':
-                commands.append(program_cmd + ['--sectorerase'])
+                commands.append(program_cmd + ['--sectorerase'] +
+                                self.tool_opt)
             elif self.family == 'NRF52':
-                commands.append(program_cmd + ['--sectoranduicrerase'])
+                commands.append(program_cmd + ['--sectoranduicrerase'] +
+                                self.tool_opt)
             else:
                 uicr = {
                     'NRF53': ((0x00FF8000, 0x00FF8800),
@@ -193,8 +200,14 @@ class NrfJprogBinaryRunner(ZephyrBinaryRunner):
                         'The hex file contains data placed in the UICR, which '
                         'needs a full erase before reprogramming. Run west '
                         'flash again with --force or --erase.')
+
+                if self.family == 'NRF53':
+                    # --sectorerase cannot be combined with another erase
+                    # command, like --recover which is given above.
+                    commands.append(program_cmd + self.tool_opt)
                 else:
-                    commands.append(program_cmd + ['--sectorerase'])
+                    commands.append(program_cmd + ['--sectorerase'] +
+                                    self.tool_opt)
 
         if self.family == 'NRF52' and not self.softreset:
             commands.extend([

scripts/west_commands/tests/test_nrfjprog.py
@@ -115,39 +115,39 @@ EXPECTED_COMMANDS = {
 
     # NRF53:
     '3NNN':
-    (['nrfjprog', '--program', RC_KERNEL_HEX, '-f', 'NRF53', '--snr', TEST_DEF_SNR, '--sectorerase'],  # noqa: E501
+    (['nrfjprog', '--program', RC_KERNEL_HEX, '--recover', '-f', 'NRF53', '--snr', TEST_DEF_SNR],  # noqa: E501
      ['nrfjprog', '--pinreset', '-f', 'NRF53', '--snr', TEST_DEF_SNR]),
 
     '3NNY':
     (['nrfjprog', '--eraseall', '-f', 'NRF53', '--snr', TEST_DEF_SNR],
-     ['nrfjprog', '--program', RC_KERNEL_HEX, '-f', 'NRF53', '--snr', TEST_DEF_SNR],  # noqa: E501
+     ['nrfjprog', '--program', RC_KERNEL_HEX, '--recover', '-f', 'NRF53', '--snr', TEST_DEF_SNR],  # noqa: E501
      ['nrfjprog', '--pinreset', '-f', 'NRF53', '--snr', TEST_DEF_SNR]),
 
     '3NYN':
-    (['nrfjprog', '--program', RC_KERNEL_HEX, '-f', 'NRF53', '--snr', TEST_OVR_SNR, '--sectorerase'],  # noqa: E501
+    (['nrfjprog', '--program', RC_KERNEL_HEX, '--recover', '-f', 'NRF53', '--snr', TEST_OVR_SNR],  # noqa: E501
      ['nrfjprog', '--pinreset', '-f', 'NRF53', '--snr', TEST_OVR_SNR]),
 
     '3NYY':
     (['nrfjprog', '--eraseall', '-f', 'NRF53', '--snr', TEST_OVR_SNR],
-     ['nrfjprog', '--program', RC_KERNEL_HEX, '-f', 'NRF53', '--snr', TEST_OVR_SNR],  # noqa: E501
+     ['nrfjprog', '--program', RC_KERNEL_HEX, '--recover', '-f', 'NRF53', '--snr', TEST_OVR_SNR],  # noqa: E501
      ['nrfjprog', '--pinreset', '-f', 'NRF53', '--snr', TEST_OVR_SNR]),
 
     '3YNN':
-    (['nrfjprog', '--program', RC_KERNEL_HEX, '-f', 'NRF53', '--snr', TEST_DEF_SNR, '--sectorerase'],  # noqa: E501
+    (['nrfjprog', '--program', RC_KERNEL_HEX, '--recover', '-f', 'NRF53', '--snr', TEST_DEF_SNR],  # noqa: E501
      ['nrfjprog', '--reset', '-f', 'NRF53', '--snr', TEST_DEF_SNR]),
 
     '3YNY':
     (['nrfjprog', '--eraseall', '-f', 'NRF53', '--snr', TEST_DEF_SNR],
-     ['nrfjprog', '--program', RC_KERNEL_HEX, '-f', 'NRF53', '--snr', TEST_DEF_SNR],  # noqa: E501
+     ['nrfjprog', '--program', RC_KERNEL_HEX, '--recover', '-f', 'NRF53', '--snr', TEST_DEF_SNR],  # noqa: E501
      ['nrfjprog', '--reset', '-f', 'NRF53', '--snr', TEST_DEF_SNR]),
 
     '3YYN':
-    (['nrfjprog', '--program', RC_KERNEL_HEX, '-f', 'NRF53', '--snr', TEST_OVR_SNR, '--sectorerase'],  # noqa: E501
+    (['nrfjprog', '--program', RC_KERNEL_HEX, '--recover', '-f', 'NRF53', '--snr', TEST_OVR_SNR],  # noqa: E501
      ['nrfjprog', '--reset', '-f', 'NRF53', '--snr', TEST_OVR_SNR]),
 
     '3YYY':
     (['nrfjprog', '--eraseall', '-f', 'NRF53', '--snr', TEST_OVR_SNR],
-     ['nrfjprog', '--program', RC_KERNEL_HEX, '-f', 'NRF53', '--snr', TEST_OVR_SNR],  # noqa: E501
+     ['nrfjprog', '--program', RC_KERNEL_HEX, '--recover', '-f', 'NRF53', '--snr', TEST_OVR_SNR],  # noqa: E501
      ['nrfjprog', '--reset', '-f', 'NRF53', '--snr', TEST_OVR_SNR]),
 
     # NRF91: