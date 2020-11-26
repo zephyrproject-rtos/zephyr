e8bbcb928444f1ccb5cdae79d290df15e085737c
Armando Visconti <armando.visconti@st.com>
2020-11-26 18:40:25 +0100

    drivers/sensor: iis2dlpc: Move range Kconfig property into dts
    Converts iis2dlpc range options (2g, 4g, 8g, 16g) from Kconfigs
    to Device Tree.
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

--- drivers/sensor/iis2dlpc/iis2dlpc.c
+++ drivers/sensor/iis2dlpc/iis2dlpc.c
@@ -31,13 +31,12 @@ LOG_MODULE_REGISTER(IIS2DLPC, CONFIG_SENSOR_LOG_LEVEL);
  * @dev: Pointer to instance of struct device (I2C or SPI)
  * @range: Full scale range (2, 4, 8 and 16 G)
  */
-static int iis2dlpc_set_range(const struct device *dev, uint16_t range)
+static int iis2dlpc_set_range(const struct device *dev, uint8_t fs)
 {
 	int err;
 	struct iis2dlpc_data *iis2dlpc = dev->data;
 	const struct iis2dlpc_device_config *cfg = dev->config;
 	uint8_t shift_gain = 0U;
-	uint8_t fs = IIS2DLPC_FS_TO_REG(range);
 
 	err = iis2dlpc_full_scale_set(iis2dlpc->ctx, fs);
 
@@ -48,8 +47,7 @@ static int iis2dlpc_set_range(const struct device *dev, uint16_t range)
 	if (!err) {
 		/* save internally gain for optimization */
 		iis2dlpc->gain =
-			IIS2DLPC_FS_TO_GAIN(IIS2DLPC_FS_TO_REG(range),
-					    shift_gain);
+			IIS2DLPC_FS_TO_GAIN(fs, shift_gain);
 	}
 
 	return err;
@@ -146,7 +144,8 @@ static int iis2dlpc_config(const struct device *dev, enum sensor_channel chan,
 {
 	switch (attr) {
 	case SENSOR_ATTR_FULL_SCALE:
-		return iis2dlpc_set_range(dev, sensor_ms2_to_g(val));
+		return iis2dlpc_set_range(dev,
+				IIS2DLPC_FS_TO_REG(sensor_ms2_to_g(val)));
 	case SENSOR_ATTR_SAMPLING_FREQUENCY:
 		return iis2dlpc_set_odr(dev, val->val1);
 	default:
@@ -297,14 +296,11 @@ static int iis2dlpc_init(const struct device *dev)
 		return -EIO;
 	}
 
-	if (iis2dlpc_full_scale_set(iis2dlpc->ctx, IIS2DLPC_ACC_FS) < 0) {
+	LOG_INF("range is %d", cfg->range);
+	if (iis2dlpc_set_range(dev, IIS2DLPC_FS_TO_REG(cfg->range)) < 0) {
+		LOG_ERR("range init error %d", cfg->range);
 		return -EIO;
 	}
-
-	iis2dlpc->gain =
-		IIS2DLPC_FS_TO_GAIN(IIS2DLPC_ACC_FS,
-				    cfg->pm == IIS2DLPC_CONT_LOW_PWR_12bit ?
-				    IIS2DLPC_SHFT_GAIN_NOLP1 : 0);
 
 #ifdef CONFIG_IIS2DLPC_TRIGGER
 	if (iis2dlpc_init_interrupt(dev) < 0) {
@@ -377,6 +373,7 @@ static int iis2dlpc_init(const struct device *dev)
 const struct iis2dlpc_device_config iis2dlpc_cfg = {
 	.bus_name = DT_INST_BUS_LABEL(0),
 	.pm = CONFIG_IIS2DLPC_POWER_MODE,
+	.range = DT_INST_PROP(0, range),
 #ifdef CONFIG_IIS2DLPC_TRIGGER
 	.int_gpio_port = DT_INST_GPIO_LABEL(0, drdy_gpios),
 	.int_gpio_pin = DT_INST_GPIO_PIN(0, drdy_gpios),
