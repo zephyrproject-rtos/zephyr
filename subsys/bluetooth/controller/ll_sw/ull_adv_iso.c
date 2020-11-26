3efd26598b35199423222d1a7bc53407921fb616
Emil Gydesen <emil.gydesen@nordicsemi.no>
2020-11-26 14:03:27 +0100

    Bluetooth: controller: Rearrange functions in ull_adv_iso.c
    Rearranged the functions so the order of functions match
    the common pattern in other ULL files.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_adv_iso.c
+++ subsys/bluetooth/controller/ll_sw/ull_adv_iso.c
@@ -46,238 +46,16 @@ static struct {
 			CONFIG_BT_CTLR_ISO_TX_BUFFERS];
 } mem_iso_tx;
 
-static struct ll_adv_iso *adv_iso_by_bis_handle_get(uint16_t bis_handle)
-{
-	struct ll_adv_iso *adv_iso;
-	uint8_t idx;
 
-	adv_iso =  &ll_adv_iso[0];
-
-	for (idx = 0U; idx < CONFIG_BT_CTLR_ADV_SET; idx++, adv_iso++) {
-		if (adv_iso->is_created &&
-		    (adv_iso->bis_handle == bis_handle)) {
-			return adv_iso;
-		}
-	}
-
-	return NULL;
-}
-
-void *ll_iso_tx_mem_acquire(void)
-{
-	return mem_acquire(&mem_iso_tx.free);
-}
-
-void ll_iso_tx_mem_release(void *tx)
-{
-	mem_release(tx, &mem_iso_tx.free);
-}
-
-static int init_reset(void)
-{
-	/* Initialize conn pool. */
-	mem_init(ll_adv_iso, sizeof(struct ll_adv_iso),
-		 sizeof(ll_adv_iso) / sizeof(struct ll_adv_iso), &adv_iso_free);
-
-	/* Initialize tx pool. */
-	mem_init(mem_iso_tx.pool, CONFIG_BT_CTLR_ISO_TX_BUFFER_SIZE,
-		 CONFIG_BT_CTLR_ISO_TX_BUFFERS, &mem_iso_tx.free);
-
-	return 0;
-}
-
-int ull_adv_iso_init(void)
-{
-	int err;
-
-	err = init_reset();
-	if (err) {
-		return err;
-	}
-
-	return 0;
-}
-
-int ull_adv_iso_reset(void)
-{
-	int err;
-
-	/* Re-initialize the Tx mfifo */
-	MFIFO_INIT(iso_tx);
-
-	err = init_reset();
-	if (err) {
-		return err;
-	}
-
-	return 0;
-}
-
-int ll_iso_tx_mem_enqueue(uint16_t handle, void *tx)
-{
-	struct lll_tx *lll_tx;
-	struct ll_adv_iso *adv_iso;
-	uint8_t idx;
-
-	adv_iso = adv_iso_by_bis_handle_get(handle);
-	if (!adv_iso) {
-		return -EINVAL;
-	}
-
-	idx = MFIFO_ENQUEUE_GET(iso_tx, (void **) &lll_tx);
-	if (!lll_tx) {
-		return -ENOBUFS;
-	}
-
-	lll_tx->handle = handle;
-	lll_tx->node = tx;
-
-	MFIFO_ENQUEUE(iso_tx, idx);
-
-	return 0;
-}
-
-uint8_t ll_adv_iso_by_hci_handle_get(uint8_t hci_handle, uint8_t *handle)
-{
-	struct ll_adv_iso *adv_iso;
-	uint8_t idx;
-
-	adv_iso =  &ll_adv_iso[0];
-
-	for (idx = 0U; idx < CONFIG_BT_CTLR_ADV_SET; idx++, adv_iso++) {
-		if (adv_iso->is_created &&
-		    (adv_iso->hci_handle == hci_handle)) {
-			*handle = idx;
-			return 0;
-		}
-	}
-
-	return BT_HCI_ERR_UNKNOWN_ADV_IDENTIFIER;
-}
-
-uint8_t ll_adv_iso_by_hci_handle_new(uint8_t hci_handle, uint8_t *handle)
-{
-	struct ll_adv_iso *adv_iso, *adv_iso_empty;
-	uint8_t idx;
-
-	adv_iso = &ll_adv_iso[0];
-	adv_iso_empty = NULL;
-
-	for (idx = 0U; idx < CONFIG_BT_CTLR_ADV_SET; idx++, adv_iso++) {
-		if (adv_iso->is_created) {
-			if (adv_iso->hci_handle == hci_handle) {
-				return BT_HCI_ERR_CMD_DISALLOWED;
-			}
-		} else if (!adv_iso_empty) {
-			adv_iso_empty = adv_iso;
-			*handle = idx;
-		}
-	}
-
-	if (adv_iso_empty) {
-		memset(adv_iso_empty, 0, sizeof(*adv_iso_empty));
-		adv_iso_empty->hci_handle = hci_handle;
-		return 0;
-	}
-
-	return BT_HCI_ERR_MEM_CAPACITY_EXCEEDED;
-}
-
-
+static struct ll_adv_iso *adv_iso_by_bis_handle_get(uint16_t bis_handle);
+static uint32_t ull_adv_iso_start(struct ll_adv_iso *adv_iso,
+				  uint32_t ticks_anchor);
+static inline struct ll_adv_iso *ull_adv_iso_get(uint8_t handle);
+static int init_reset(void);
 static void ticker_cb(uint32_t ticks_at_expire, uint32_t remainder,
-		      uint16_t lazy, void *param)
-{
-	/* TODO: LLL support for ADV ISO */
-#if 0
-	static memq_link_t link;
-	static struct mayfly mfy = {0, 0, &link, NULL, lll_adv_iso_prepare};
-	static struct lll_prepare_param p;
-	struct ll_adv_iso *adv_iso = param;
-	struct lll_adv_iso *lll;
-	uint32_t ret;
-	uint8_t ref;
-
-	DEBUG_RADIO_PREPARE_A(1);
-
-	lll = &adv_iso->lll;
-
-	/* Increment prepare reference count */
-	ref = ull_ref_inc(&adv_iso->ull);
-	LL_ASSERT(ref);
-
-	/* Append timing parameters */
-	p.ticks_at_expire = ticks_at_expire;
-	p.remainder = remainder;
-	p.lazy = lazy;
-	p.param = lll;
-	mfy.param = &p;
-
-	/* Kick LLL prepare */
-	ret = mayfly_enqueue(TICKER_USER_ID_ULL_HIGH,
-			     TICKER_USER_ID_LLL, 0, &mfy);
-	LL_ASSERT(!ret);
-
-	DEBUG_RADIO_PREPARE_A(1);
-#endif
-}
-
-uint32_t ull_adv_iso_start(struct ll_adv_iso *adv_iso, uint32_t ticks_anchor)
-{
-	uint32_t ticks_slot_overhead;
-	uint32_t volatile ret_cb;
-	uint32_t iso_interval_us;
-	uint32_t slot_us;
-	uint32_t ret;
-
-	ull_hdr_init(&adv_iso->ull);
-
-	/* TODO: Calc slot_us */
-	slot_us = EVENT_OVERHEAD_START_US + EVENT_OVERHEAD_END_US;
-	slot_us += 1000;
-
-	adv_iso->evt.ticks_active_to_start = 0;
-	adv_iso->evt.ticks_xtal_to_start =
-		HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_XTAL_US);
-	adv_iso->evt.ticks_preempt_to_start =
-		HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_PREEMPT_MIN_US);
-	adv_iso->evt.ticks_slot = HAL_TICKER_US_TO_TICKS(slot_us);
-
-	if (IS_ENABLED(CONFIG_BT_CTLR_LOW_LAT)) {
-		ticks_slot_overhead = MAX(adv_iso->evt.ticks_active_to_start,
-					  adv_iso->evt.ticks_xtal_to_start);
-	} else {
-		ticks_slot_overhead = 0;
-	}
-
-	/* TODO: Calculate ISO interval */
-	/* iso_interval shall be at least SDU interval,
-	 * or integer multiple of SDU interval for unframed PDUs
-	 */
-	iso_interval_us = adv_iso->sdu_interval;
-
-	ret_cb = TICKER_STATUS_BUSY;
-	ret = ticker_start(TICKER_INSTANCE_ID_CTLR, TICKER_USER_ID_ULL_HIGH,
-			   (TICKER_ID_ADV_ISO_BASE + adv_iso->bis_handle),
-			   ticks_anchor, 0,
-			   HAL_TICKER_US_TO_TICKS(iso_interval_us),
-			   HAL_TICKER_REMAINDER(iso_interval_us),
-			   TICKER_NULL_LAZY,
-			   (ll_adv_iso->evt.ticks_slot + ticks_slot_overhead),
-			   ticker_cb, ll_adv_iso,
-			   ull_ticker_status_give, (void *)&ret_cb);
-	ret = ull_ticker_status_take(ret, &ret_cb);
-
-	return 0;
-}
-
-static inline struct ll_adv_iso *ull_adv_iso_get(uint8_t handle)
-{
-	if (handle >= CONFIG_BT_CTLR_ADV_SET) {
-		return NULL;
-	}
-
-	return &ll_adv_iso[handle];
-}
+		      uint16_t lazy, void *param);
+static void tx_lll_flush(void *param);
+static void ticker_op_stop_cb(uint32_t status, void *param);
 
 uint8_t ll_big_create(uint8_t big_handle, uint8_t adv_handle, uint8_t num_bis,
 		      uint32_t sdu_interval, uint16_t max_sdu,
@@ -394,6 +172,7 @@ uint8_t ll_big_create(uint8_t big_handle, uint8_t adv_handle, uint8_t num_bis,
 	}
 
 	/* Prepare BIG complete event */
+	/* TODO: Implement custom node_rx struct for optimization */
 	node_rx = (void *)&adv_iso->node_rx_complete;
 	node_rx->hdr.type = NODE_RX_TYPE_BIG_COMPLETE;
 	node_rx->hdr.handle = big_handle;
@@ -432,33 +211,6 @@ uint8_t ll_big_test_create(uint8_t big_handle, uint8_t adv_handle,
 	return BT_HCI_ERR_CMD_DISALLOWED;
 }
 
-
-static void tx_lll_flush(void *param)
-{
-	/* TODO: LLL support for ADV ISO */
-	/* TODO: Send terminate complete event to host */
-#if 0
-	/* TODO: Flush TX */
-	struct ll_adv_iso *lll = param;
-#endif
-}
-
-static void ticker_op_stop_cb(uint32_t status, void *param)
-{
-	uint32_t retval;
-	static memq_link_t link;
-	static struct mayfly mfy = {0, 0, &link, NULL, tx_lll_flush};
-
-	LL_ASSERT(status == TICKER_STATUS_SUCCESS);
-
-	mfy.param = param;
-
-	/* Flush pending tx PDUs in LLL (using a mayfly) */
-	retval = mayfly_enqueue(TICKER_USER_ID_ULL_LOW, TICKER_USER_ID_LLL, 0,
-				&mfy);
-	LL_ASSERT(!retval);
-}
-
 uint8_t ll_big_terminate(uint8_t big_handle, uint8_t reason, void **rx)
 {
 	struct ll_adv_iso *adv_iso;
@@ -489,4 +241,264 @@ uint8_t ll_big_terminate(uint8_t big_handle, uint8_t reason, void **rx)
 	*rx = node_rx;
 
 	return BT_HCI_ERR_SUCCESS;
+}
+
+int ull_adv_iso_init(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+int ull_adv_iso_reset(void)
+{
+	int err;
+
+	/* Re-initialize the Tx mfifo */
+	MFIFO_INIT(iso_tx);
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+void *ll_iso_tx_mem_acquire(void)
+{
+	return mem_acquire(&mem_iso_tx.free);
+}
+
+void ll_iso_tx_mem_release(void *tx)
+{
+	mem_release(tx, &mem_iso_tx.free);
+}
+
+int ll_iso_tx_mem_enqueue(uint16_t handle, void *tx)
+{
+	struct lll_tx *lll_tx;
+	struct ll_adv_iso *adv_iso;
+	uint8_t idx;
+
+	adv_iso = adv_iso_by_bis_handle_get(handle);
+	if (!adv_iso) {
+		return -EINVAL;
+	}
+
+	idx = MFIFO_ENQUEUE_GET(iso_tx, (void **) &lll_tx);
+	if (!lll_tx) {
+		return -ENOBUFS;
+	}
+
+	lll_tx->handle = handle;
+	lll_tx->node = tx;
+
+	MFIFO_ENQUEUE(iso_tx, idx);
+
+	return 0;
+}
+
+uint8_t ll_adv_iso_by_hci_handle_get(uint8_t hci_handle, uint8_t *handle)
+{
+	struct ll_adv_iso *adv_iso;
+	uint8_t idx;
+
+	adv_iso =  &ll_adv_iso[0];
+
+	for (idx = 0U; idx < CONFIG_BT_CTLR_ADV_SET; idx++, adv_iso++) {
+		if (adv_iso->is_created &&
+		    (adv_iso->hci_handle == hci_handle)) {
+			*handle = idx;
+			return 0;
+		}
+	}
+
+	return BT_HCI_ERR_UNKNOWN_ADV_IDENTIFIER;
+}
+
+uint8_t ll_adv_iso_by_hci_handle_new(uint8_t hci_handle, uint8_t *handle)
+{
+	struct ll_adv_iso *adv_iso, *adv_iso_empty;
+	uint8_t idx;
+
+	adv_iso = &ll_adv_iso[0];
+	adv_iso_empty = NULL;
+
+	for (idx = 0U; idx < CONFIG_BT_CTLR_ADV_SET; idx++, adv_iso++) {
+		if (adv_iso->is_created) {
+			if (adv_iso->hci_handle == hci_handle) {
+				return BT_HCI_ERR_CMD_DISALLOWED;
+			}
+		} else if (!adv_iso_empty) {
+			adv_iso_empty = adv_iso;
+			*handle = idx;
+		}
+	}
+
+	if (adv_iso_empty) {
+		memset(adv_iso_empty, 0, sizeof(*adv_iso_empty));
+		adv_iso_empty->hci_handle = hci_handle;
+		return 0;
+	}
+
+	return BT_HCI_ERR_MEM_CAPACITY_EXCEEDED;
+}
+
+static struct ll_adv_iso *adv_iso_by_bis_handle_get(uint16_t bis_handle)
+{
+	struct ll_adv_iso *adv_iso;
+	uint8_t idx;
+
+	adv_iso =  &ll_adv_iso[0];
+
+	for (idx = 0U; idx < CONFIG_BT_CTLR_ADV_SET; idx++, adv_iso++) {
+		if (adv_iso->is_created &&
+		    (adv_iso->bis_handle == bis_handle)) {
+			return adv_iso;
+		}
+	}
+
+	return NULL;
+}
+
+static uint32_t ull_adv_iso_start(struct ll_adv_iso *adv_iso,
+				  uint32_t ticks_anchor)
+{
+	uint32_t ticks_slot_overhead;
+	uint32_t volatile ret_cb;
+	uint32_t iso_interval_us;
+	uint32_t slot_us;
+	uint32_t ret;
+
+	ull_hdr_init(&adv_iso->ull);
+
+	/* TODO: Calc slot_us */
+	slot_us = EVENT_OVERHEAD_START_US + EVENT_OVERHEAD_END_US;
+	slot_us += 1000;
+
+	adv_iso->evt.ticks_active_to_start = 0;
+	adv_iso->evt.ticks_xtal_to_start =
+		HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_XTAL_US);
+	adv_iso->evt.ticks_preempt_to_start =
+		HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_PREEMPT_MIN_US);
+	adv_iso->evt.ticks_slot = HAL_TICKER_US_TO_TICKS(slot_us);
+
+	if (IS_ENABLED(CONFIG_BT_CTLR_LOW_LAT)) {
+		ticks_slot_overhead = MAX(adv_iso->evt.ticks_active_to_start,
+					  adv_iso->evt.ticks_xtal_to_start);
+	} else {
+		ticks_slot_overhead = 0;
+	}
+
+	/* TODO: Calculate ISO interval */
+	/* iso_interval shall be at least SDU interval,
+	 * or integer multiple of SDU interval for unframed PDUs
+	 */
+	iso_interval_us = adv_iso->sdu_interval;
+
+	ret_cb = TICKER_STATUS_BUSY;
+	ret = ticker_start(TICKER_INSTANCE_ID_CTLR, TICKER_USER_ID_ULL_HIGH,
+			   (TICKER_ID_ADV_ISO_BASE + adv_iso->bis_handle),
+			   ticks_anchor, 0,
+			   HAL_TICKER_US_TO_TICKS(iso_interval_us),
+			   HAL_TICKER_REMAINDER(iso_interval_us),
+			   TICKER_NULL_LAZY,
+			   (ll_adv_iso->evt.ticks_slot + ticks_slot_overhead),
+			   ticker_cb, ll_adv_iso,
+			   ull_ticker_status_give, (void *)&ret_cb);
+	ret = ull_ticker_status_take(ret, &ret_cb);
+
+	return 0;
+}
+
+static inline struct ll_adv_iso *ull_adv_iso_get(uint8_t handle)
+{
+	if (handle >= CONFIG_BT_CTLR_ADV_SET) {
+		return NULL;
+	}
+
+	return &ll_adv_iso[handle];
+}
+
+static int init_reset(void)
+{
+	/* Initialize conn pool. */
+	mem_init(ll_adv_iso, sizeof(struct ll_adv_iso),
+		 sizeof(ll_adv_iso) / sizeof(struct ll_adv_iso), &adv_iso_free);
+
+	/* Initialize tx pool. */
+	mem_init(mem_iso_tx.pool, CONFIG_BT_CTLR_ISO_TX_BUFFER_SIZE,
+		 CONFIG_BT_CTLR_ISO_TX_BUFFERS, &mem_iso_tx.free);
+
+	return 0;
+}
+
+
+static void ticker_cb(uint32_t ticks_at_expire, uint32_t remainder,
+		      uint16_t lazy, void *param)
+{
+	/* TODO: LLL support for ADV ISO */
+#if 0
+	static memq_link_t link;
+	static struct mayfly mfy = {0, 0, &link, NULL, lll_adv_iso_prepare};
+	static struct lll_prepare_param p;
+	struct ll_adv_iso *adv_iso = param;
+	struct lll_adv_iso *lll;
+	uint32_t ret;
+	uint8_t ref;
+
+	DEBUG_RADIO_PREPARE_A(1);
+
+	lll = &adv_iso->lll;
+
+	/* Increment prepare reference count */
+	ref = ull_ref_inc(&adv_iso->ull);
+	LL_ASSERT(ref);
+
+	/* Append timing parameters */
+	p.ticks_at_expire = ticks_at_expire;
+	p.remainder = remainder;
+	p.lazy = lazy;
+	p.param = lll;
+	mfy.param = &p;
+
+	/* Kick LLL prepare */
+	ret = mayfly_enqueue(TICKER_USER_ID_ULL_HIGH,
+			     TICKER_USER_ID_LLL, 0, &mfy);
+	LL_ASSERT(!ret);
+
+	DEBUG_RADIO_PREPARE_A(1);
+#endif
+}
+
+static void tx_lll_flush(void *param)
+{
+	/* TODO: LLL support for ADV ISO */
+	/* TODO: Send terminate complete event to host */
+#if 0
+	/* TODO: Flush TX */
+	struct ll_adv_iso *lll = param;
+#endif
+}
+
+static void ticker_op_stop_cb(uint32_t status, void *param)
+{
+	uint32_t retval;
+	static memq_link_t link;
+	static struct mayfly mfy = {0, 0, &link, NULL, tx_lll_flush};
+
+	LL_ASSERT(status == TICKER_STATUS_SUCCESS);
+
+	mfy.param = param;
+
+	/* Flush pending tx PDUs in LLL (using a mayfly) */
+	retval = mayfly_enqueue(TICKER_USER_ID_ULL_LOW, TICKER_USER_ID_LLL, 0,
+				&mfy);
+	LL_ASSERT(!retval);
 }
