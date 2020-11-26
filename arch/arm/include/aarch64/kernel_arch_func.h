bd46910d53f6153bcf9cfb805b337753287ecf31
Carlo Caione <ccaione@baylibre.com>
2020-11-26 12:57:36 +0100

    aarch64: userspace: Add support for page tables swapping
    Introduce the necessary routines to have the user thread stack correctly
    mapped and the functions to swap page tables on context switch.
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

--- arch/arm/include/aarch64/kernel_arch_func.h
+++ arch/arm/include/aarch64/kernel_arch_func.h
@@ -41,6 +41,7 @@ static inline void arch_switch(void *switch_to, void **switched_from)
 
 extern void z_arm64_fatal_error(z_arch_esf_t *esf, unsigned int reason);
 extern void z_arm64_userspace_enter(z_arch_esf_t *esf);
+extern void z_arm64_set_ttbr0(uintptr_t ttbr0);
 
 #endif /* _ASMLANGUAGE */
 
