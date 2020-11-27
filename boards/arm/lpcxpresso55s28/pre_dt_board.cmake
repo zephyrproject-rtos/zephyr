bfc09c0b8a84fef8c46a4febbe8c417d37be867d
Christian Taedcke <christian.taedcke@lemonbeat.com>
2020-11-27 10:12:39 +0100

    boards: arm: Add LPCXpresso55S28 dev board
    Add initial support for NXP LPCXpresso55S28 development board.
    
    Signed-off-by: Christian Taedcke <christian.taedcke@lemonbeat.com>

---
+++ boards/arm/lpcxpresso55s28/pre_dt_board.cmake
@@ -0,0 +1,5 @@
+# Copyright (c) 2020 Lemonbeat GmbH
+# SPDX-License-Identifier: Apache-2.0
+
+# Suppress "simple_bus_reg" on LPC boards as all GPIO ports use the same register.
+list(APPEND EXTRA_DTC_FLAGS "-Wno-simple_bus_reg")
