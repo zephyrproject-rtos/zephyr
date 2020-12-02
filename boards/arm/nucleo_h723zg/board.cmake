30cde5e5aee69499f6c1610ee7efbb7bdfb6f6f8
Erwan Gouriou <erwan.gouriou@linaro.org>
2020-12-02 16:43:39 +0100

    boards: nucleo_h723zg: Use openocd as default flash interface
    Since this board is fully supported on openocd,
    let's use openocd to comply with other boards and
    remove the dependency on stm32cubeprogrammer.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- boards/arm/nucleo_h723zg/board.cmake
+++ boards/arm/nucleo_h723zg/board.cmake
@@ -4,6 +4,6 @@ board_runner_args(openocd "--use-elf")
 board_runner_args(jlink "--device=STM32H723ZG" "--speed=4000")
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset=hw")
 
-include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
