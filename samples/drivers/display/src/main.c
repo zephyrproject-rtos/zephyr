fd63c5ff1f07410ea054f6ee916eaa1e0382fba3
Kim Bøndergaard <kibo@prevas.dk>
2020-12-09 09:00:25 +0100

    samples: drivers: display: Support for all solomon_ssd16xxfb based displays
    Currently the following shields will thus be supported
    
    	waveshare_epaper_gdeh0154a07
    	waveshare_epaper_gdeh0213b1
    	waveshare_epaper_gdeh0213b72
    	waveshare_epaper_gdeh029a1
    
    Signed-off-by: Kim Bøndergaard <kim@fam-boendergaard.dk>
    Signed-off-by: Kim Bøndergaard <kibo@prevas.dk>

--- samples/drivers/display/src/main.c
+++ samples/drivers/display/src/main.c
@@ -22,8 +22,8 @@ LOG_MODULE_REGISTER(sample, LOG_LEVEL_INF);
 #define DISPLAY_DEV_NAME DT_LABEL(DT_INST(0, solomon_ssd1306fb))
 #endif
 
-#if DT_NODE_HAS_STATUS(DT_INST(0, gooddisplay_gdeh0213b1), okay)
-#define DISPLAY_DEV_NAME DT_LABEL(DT_INST(0, gooddisplay_gdeh0213b1))
+#if DT_NODE_HAS_STATUS(DT_INST(0, solomon_ssd16xxfb), okay)
+#define DISPLAY_DEV_NAME DT_LABEL(DT_INST(0, solomon_ssd16xxfb))
 #endif
 
 #if DT_NODE_HAS_STATUS(DT_INST(0, sitronix_st7789v), okay)
