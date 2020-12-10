zephyrproject-rtos/zephyr

commit eca014115287bdb8a8a57355514fbd5e1d27b75b

Bluetooth: Mesh: Isolate cryptographic material
This is a major refactoring of the handling of the cryptographic
material of both the network and transport layers. The aim is to
encapsulate the key object manipulation, and improve overall modularity.

Pulls Applications and Subnets out of the bt_mesh and into separate
modules, with static storage types on the data. This has several
side-effects:
- The Config Server no longer operates directly on the bt_mesh.subs and
  bt_mesh.apps lists, but goes through a public configuration interface,
  following the pattern set in #27908.
- All iteration through the keys is done through iteration APIs
- Key resolution on RX and TX is centralized.
- Changes to the keys triggers events the other modules can register
  handlers for.
- Friendship credentials are stored in the lpn and friend structures.

Part of #27842.

Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

subsys/bluetooth/mesh/subnet.c
@@ -1,0 +1,660 @@
+/*
+ * Copyright (c) 2017 Intel Corporation
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <string.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <sys/atomic.h>
+#include <sys/util.h>
+#include <sys/byteorder.h>
+
+#include <net/buf.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/conn.h>
+#include <bluetooth/mesh.h>
+
+#define BT_DBG_ENABLED IS_ENABLED(CONFIG_BT_MESH_DEBUG_KEYS)
+#define LOG_MODULE_NAME bt_mesh_net_keys
+#include "common/log.h"
+
+#include "crypto.h"
+#include "adv.h"
+#include "mesh.h"
+#include "net.h"
+#include "lpn.h"
+#include "friend.h"
+#include "proxy.h"
+#include "transport.h"
+#include "access.h"
+#include "foundation.h"
+#include "beacon.h"
+#include "rpl.h"
+#include "settings.h"
+#include "prov.h"
+
+static struct bt_mesh_subnet subnets[CONFIG_BT_MESH_SUBNET_COUNT] = {
+	[0 ... (CONFIG_BT_MESH_SUBNET_COUNT - 1)] = {
+		.net_idx = BT_MESH_KEY_UNUSED,
+	},
+};
+
+static void subnet_evt(struct bt_mesh_subnet *sub, enum bt_mesh_key_evt evt)
+{
+	Z_STRUCT_SECTION_FOREACH(bt_mesh_subnet_cb, cb) {
+		cb->evt_handler(sub, evt);
+	}
+}
+
+uint8_t bt_mesh_net_flags(struct bt_mesh_subnet *sub)
+{
+	uint8_t flags = 0x00;
+
+	if (sub && (sub->kr_phase == BT_MESH_KR_PHASE_2)) {
+		flags |= BT_MESH_NET_FLAG_KR;
+	}
+
+	if (atomic_test_bit(bt_mesh.flags, BT_MESH_IVU_IN_PROGRESS)) {
+		flags |= BT_MESH_NET_FLAG_IVU;
+	}
+
+	return flags;
+}
+
+static void key_refresh(struct bt_mesh_subnet *sub, uint8_t new_phase)
+{
+	BT_DBG("Phase 0x%02x -> 0x%02x", sub->kr_phase, new_phase);
+
+	switch (new_phase) {
+	/* Added second set of keys */
+	case BT_MESH_KR_PHASE_1:
+		sub->kr_phase = new_phase;
+		subnet_evt(sub, BT_MESH_KEY_UPDATED);
+		break;
+	/* Now using new keys for TX */
+	case BT_MESH_KR_PHASE_2:
+		sub->kr_phase = new_phase;
+		subnet_evt(sub, BT_MESH_KEY_SWAPPED);
+		break;
+	/* Revoking keys */
+	case BT_MESH_KR_PHASE_3:
+	case BT_MESH_KR_NORMAL:
+		sub->kr_phase = BT_MESH_KR_NORMAL;
+		memcpy(&sub->keys[0], &sub->keys[1], sizeof(sub->keys[0]));
+		sub->keys[1].valid = 0U;
+		subnet_evt(sub, BT_MESH_KEY_REVOKED);
+		break;
+	}
+
+	if (IS_ENABLED(CONFIG_BT_SETTINGS)) {
+		BT_DBG("Storing Updated NetKey persistently");
+		bt_mesh_store_subnet(sub->net_idx);
+	}
+}
+
+void bt_mesh_kr_update(struct bt_mesh_subnet *sub, bool kr_flag, bool new_key)
+{
+	if (!new_key) {
+		return;
+	}
+
+	if (sub->kr_phase == BT_MESH_KR_PHASE_1) {
+		/* Bluetooth Mesh Profile Specification Section 3.10.4.1:
+		 * Can skip phase 2 if we get KR=0 on new key.
+		 */
+		key_refresh(sub, (kr_flag ? BT_MESH_KR_PHASE_2 :
+					    BT_MESH_KR_PHASE_3));
+	} else if (sub->kr_phase == BT_MESH_KR_PHASE_2 && !kr_flag) {
+		key_refresh(sub, BT_MESH_KR_PHASE_3);
+	}
+}
+
+static struct bt_mesh_subnet *subnet_alloc(uint16_t net_idx)
+{
+	struct bt_mesh_subnet *sub = NULL;
+
+	for (int i = 0; i < ARRAY_SIZE(subnets); i++) {
+		/* Check for already existing subnet */
+		if (subnets[i].net_idx == net_idx) {
+			return &subnets[i];
+		}
+
+		if (!sub && subnets[i].net_idx == BT_MESH_KEY_UNUSED) {
+			sub = &subnets[i];
+		}
+	}
+
+	return sub;
+}
+
+static void subnet_del(struct bt_mesh_subnet *sub)
+{
+	if (IS_ENABLED(CONFIG_BT_SETTINGS)) {
+		bt_mesh_clear_subnet(sub->net_idx);
+	}
+
+	bt_mesh_net_loopback_clear(sub->net_idx);
+
+	subnet_evt(sub, BT_MESH_KEY_DELETED);
+	(void)memset(sub, 0, sizeof(*sub));
+	sub->net_idx = BT_MESH_KEY_UNUSED;
+}
+
+static int msg_cred_create(struct bt_mesh_net_cred *cred, const uint8_t *p,
+			   size_t p_len, const uint8_t key[16])
+{
+	return bt_mesh_k2(key, p, p_len, &cred->nid, cred->enc, cred->privacy);
+}
+
+static int net_keys_create(struct bt_mesh_subnet_keys *keys,
+			   const uint8_t key[16])
+{
+	uint8_t p = 0;
+	int err;
+
+	err = msg_cred_create(&keys->msg, &p, 1, key);
+	if (err) {
+		BT_ERR("Unable to generate NID, EncKey & PrivacyKey");
+		return err;
+	}
+
+	memcpy(keys->net, key, 16);
+
+	BT_DBG("NID 0x%02x EncKey %s", keys->msg.nid,
+	       bt_hex(keys->msg.enc, 16));
+	BT_DBG("PrivacyKey %s", bt_hex(keys->msg.privacy, 16));
+
+	err = bt_mesh_k3(key, keys->net_id);
+	if (err) {
+		BT_ERR("Unable to generate Net ID");
+		return err;
+	}
+
+	BT_DBG("NetID %s", bt_hex(keys->net_id, 8));
+
+#if defined(CONFIG_BT_MESH_GATT_PROXY)
+	err = bt_mesh_identity_key(key, keys->identity);
+	if (err) {
+		BT_ERR("Unable to generate IdentityKey");
+		return err;
+	}
+
+	BT_DBG("IdentityKey %s", bt_hex(keys->identity, 16));
+#endif /* GATT_PROXY */
+
+	err = bt_mesh_beacon_key(key, keys->beacon);
+	if (err) {
+		BT_ERR("Unable to generate beacon key");
+		return err;
+	}
+
+	BT_DBG("BeaconKey %s", bt_hex(keys->beacon, 16));
+
+	keys->valid = 1U;
+
+	return 0;
+}
+
+uint8_t bt_mesh_subnet_add(uint16_t net_idx, const uint8_t key[16])
+{
+	struct bt_mesh_subnet *sub = NULL;
+	int err;
+
+	BT_DBG("0x%03x", net_idx);
+
+	sub = subnet_alloc(net_idx);
+	if (!sub) {
+		return STATUS_INSUFF_RESOURCES;
+	}
+
+	if (sub->net_idx == net_idx) {
+		if (memcmp(key, sub->keys[0].net, 16)) {
+			return STATUS_IDX_ALREADY_STORED;
+		}
+
+		return STATUS_SUCCESS;
+	}
+
+	err = net_keys_create(&sub->keys[0], key);
+	if (err) {
+		return STATUS_UNSPECIFIED;
+	}
+
+	sub->net_idx = net_idx;
+	sub->kr_phase = BT_MESH_KR_NORMAL;
+
+	if (IS_ENABLED(CONFIG_BT_MESH_GATT_PROXY)) {
+		sub->node_id = BT_MESH_NODE_IDENTITY_STOPPED;
+	} else {
+		sub->node_id = BT_MESH_NODE_IDENTITY_NOT_SUPPORTED;
+	}
+
+	subnet_evt(sub, BT_MESH_KEY_ADDED);
+
+	if (IS_ENABLED(CONFIG_BT_SETTINGS)) {
+		BT_DBG("Storing NetKey persistently");
+		bt_mesh_store_subnet(sub->net_idx);
+	}
+
+	return STATUS_SUCCESS;
+}
+
+bool bt_mesh_subnet_exists(uint16_t net_idx)
+{
+	return !!bt_mesh_subnet_get(net_idx);
+}
+
+uint8_t bt_mesh_subnet_update(uint16_t net_idx, const uint8_t key[16])
+{
+	struct bt_mesh_subnet *sub;
+	int err;
+
+	BT_DBG("0x%03x", net_idx);
+
+	sub = bt_mesh_subnet_get(net_idx);
+	if (!sub) {
+		return STATUS_INVALID_NETKEY;
+	}
+
+	/* The node shall successfully process a NetKey Update message on a
+	 * valid NetKeyIndex when the NetKey value is different and the Key
+	 * Refresh procedure has not been started, or when the NetKey value is
+	 * the same in Phase 1. The NetKey Update message shall generate an
+	 * error when the node is in Phase 2, or Phase 3.
+	 */
+	switch (sub->kr_phase) {
+	case BT_MESH_KR_NORMAL:
+		if (!memcmp(key, sub->keys[0].net, 16)) {
+			return STATUS_IDX_ALREADY_STORED;
+		}
+		break;
+	case BT_MESH_KR_PHASE_1:
+		if (!memcmp(key, sub->keys[1].net, 16)) {
+			return STATUS_SUCCESS;
+		}
+		__fallthrough;
+	case BT_MESH_KR_PHASE_2:
+	case BT_MESH_KR_PHASE_3:
+		return STATUS_CANNOT_UPDATE;
+	}
+
+	err = net_keys_create(&sub->keys[1], key);
+	if (err) {
+		return STATUS_CANNOT_UPDATE;
+	}
+
+	key_refresh(sub, BT_MESH_KR_PHASE_1);
+
+	return STATUS_SUCCESS;
+}
+
+uint8_t bt_mesh_subnet_del(uint16_t net_idx)
+{
+	struct bt_mesh_subnet *sub;
+
+	BT_DBG("0x%03x", net_idx);
+
+	sub = bt_mesh_subnet_get(net_idx);
+	if (!sub) {
+		/* This could be a retry of a previous attempt that had its
+		 * response lost, so pretend that it was a success.
+		 */
+		return STATUS_INVALID_NETKEY;
+	}
+
+	subnet_del(sub);
+
+	return STATUS_SUCCESS;
+}
+
+int bt_mesh_friend_cred_create(struct bt_mesh_net_cred *cred, uint16_t lpn_addr,
+			       uint16_t frnd_addr, uint16_t lpn_counter,
+			       uint16_t frnd_counter, const uint8_t key[16])
+{
+	uint8_t p[9];
+
+	p[0] = 0x01;
+	sys_put_be16(lpn_addr, p + 1);
+	sys_put_be16(frnd_addr, p + 3);
+	sys_put_be16(lpn_counter, p + 5);
+	sys_put_be16(frnd_counter, p + 7);
+
+	return msg_cred_create(cred, p, sizeof(p), key);
+}
+
+uint8_t bt_mesh_subnet_kr_phase_set(uint16_t net_idx, uint8_t *phase)
+{
+	/* Table in Bluetooth Mesh Profile Specification Section 4.2.14: */
+	const uint8_t valid_transitions[] = {
+		0x00, /* Normal phase: KR is started by key update */
+		BIT(BT_MESH_KR_PHASE_2) | BIT(BT_MESH_KR_PHASE_3), /* Phase 1 */
+		BIT(BT_MESH_KR_PHASE_3), /* Phase 2 */
+		/* Subnet is never in Phase 3 */
+	};
+	struct bt_mesh_subnet *sub;
+
+	BT_DBG("0x%03x", net_idx);
+
+	sub = bt_mesh_subnet_get(net_idx);
+	if (!sub) {
+		*phase = 0x00;
+		return STATUS_INVALID_NETKEY;
+	}
+
+	if (*phase == sub->kr_phase) {
+		return STATUS_SUCCESS;
+	}
+
+	if (sub->kr_phase < ARRAY_SIZE(valid_transitions) &&
+	    valid_transitions[sub->kr_phase] & BIT(*phase)) {
+		key_refresh(sub, *phase);
+
+		*phase = sub->kr_phase;
+
+		return STATUS_SUCCESS;
+	}
+
+	BT_WARN("Invalid KR transition: 0x%02x -> 0x%02x", sub->kr_phase,
+		*phase);
+
+	*phase = sub->kr_phase;
+
+	return STATUS_CANNOT_UPDATE;
+}
+
+uint8_t bt_mesh_subnet_kr_phase_get(uint16_t net_idx, uint8_t *phase)
+{
+	struct bt_mesh_subnet *sub;
+
+	sub = bt_mesh_subnet_get(net_idx);
+	if (!sub) {
+		*phase = BT_MESH_KR_NORMAL;
+		return STATUS_INVALID_NETKEY;
+	}
+
+	*phase = sub->kr_phase;
+
+	return STATUS_SUCCESS;
+}
+
+uint8_t bt_mesh_subnet_node_id_set(uint16_t net_idx,
+				   enum bt_mesh_feat_state node_id)
+{
+	struct bt_mesh_subnet *sub;
+
+	if (node_id == BT_MESH_FEATURE_NOT_SUPPORTED) {
+		return STATUS_CANNOT_SET;
+	}
+
+	sub = bt_mesh_subnet_get(net_idx);
+	if (!sub) {
+		return STATUS_INVALID_NETKEY;
+	}
+
+	if (!IS_ENABLED(CONFIG_BT_MESH_GATT_PROXY)) {
+		return STATUS_FEAT_NOT_SUPP;
+	}
+
+	if (node_id) {
+		bt_mesh_proxy_identity_start(sub);
+	} else {
+		bt_mesh_proxy_identity_stop(sub);
+	}
+
+	bt_mesh_adv_update();
+
+	return STATUS_SUCCESS;
+}
+
+uint8_t bt_mesh_subnet_node_id_get(uint16_t net_idx,
+				   enum bt_mesh_feat_state *node_id)
+{
+	struct bt_mesh_subnet *sub;
+
+	sub = bt_mesh_subnet_get(net_idx);
+	if (!sub) {
+		*node_id = 0x00;
+		return STATUS_INVALID_NETKEY;
+	}
+
+	*node_id = sub->node_id;
+
+	return STATUS_SUCCESS;
+}
+
+ssize_t bt_mesh_subnets_get(uint16_t net_idxs[], size_t max, off_t skip)
+{
+	size_t count = 0;
+
+	for (int i = 0; i < ARRAY_SIZE(subnets); i++) {
+		struct bt_mesh_subnet *sub = &subnets[i];
+
+		if (sub->net_idx == BT_MESH_KEY_UNUSED) {
+			continue;
+		}
+
+		if (skip) {
+			skip--;
+			continue;
+		}
+
+		if (count >= max) {
+			return -ENOMEM;
+		}
+
+		net_idxs[count++] = sub->net_idx;
+	}
+
+	return count;
+}
+
+struct bt_mesh_subnet *bt_mesh_subnet_get(uint16_t net_idx)
+{
+	for (int i = 0; i < ARRAY_SIZE(subnets); i++) {
+		struct bt_mesh_subnet *sub = &subnets[i];
+
+		if (sub->net_idx == net_idx) {
+			return sub;
+		}
+	}
+
+	return NULL;
+}
+
+int bt_mesh_subnet_set(uint16_t net_idx, uint8_t kr_phase,
+		       const uint8_t old_key[16], const uint8_t new_key[16])
+{
+	const uint8_t *keys[] = { old_key, new_key };
+	struct bt_mesh_subnet *sub;
+
+	sub = subnet_alloc(net_idx);
+	if (!sub) {
+		return -ENOMEM;
+	}
+
+	if (sub->net_idx == net_idx) {
+		return -EALREADY;
+	}
+
+	for (int i = 0; i < ARRAY_SIZE(keys); i++) {
+		if (!keys[i]) {
+			continue;
+		}
+
+		if (net_keys_create(&sub->keys[i], keys[i])) {
+			return -EIO;
+		}
+	}
+
+	sub->net_idx = net_idx;
+	sub->kr_phase = kr_phase;
+
+	if (IS_ENABLED(CONFIG_BT_MESH_GATT_PROXY)) {
+		sub->node_id = BT_MESH_NODE_IDENTITY_STOPPED;
+	} else {
+		sub->node_id = BT_MESH_NODE_IDENTITY_NOT_SUPPORTED;
+	}
+
+	/* Make sure we have valid beacon data to be sent */
+	bt_mesh_beacon_update(sub);
+
+	return 0;
+}
+
+struct bt_mesh_subnet *bt_mesh_subnet_find(int (*cb)(struct bt_mesh_subnet *sub,
+						     void *cb_data),
+					   void *cb_data)
+{
+	for (int i = 0; i < ARRAY_SIZE(subnets); i++) {
+		if (subnets[i].net_idx == BT_MESH_KEY_UNUSED) {
+			continue;
+		}
+
+		if (!cb || cb(&subnets[i], cb_data)) {
+			return &subnets[i];
+		}
+	}
+
+	return NULL;
+}
+
+size_t bt_mesh_subnet_foreach(void (*cb)(struct bt_mesh_subnet *sub))
+{
+	size_t count = 0;
+
+	for (int i = 0; i < ARRAY_SIZE(subnets); i++) {
+		if (subnets[i].net_idx == BT_MESH_KEY_UNUSED) {
+			continue;
+		}
+
+		cb(&subnets[i]);
+		count++;
+	}
+
+	return count;
+}
+
+struct bt_mesh_subnet *bt_mesh_subnet_next(struct bt_mesh_subnet *sub)
+{
+	if (sub) {
+		sub++;
+	} else {
+		sub = &subnets[0];
+	}
+
+	for (int i = 0; i < ARRAY_SIZE(subnets); i++, sub++) {
+		/* Roll over once we reach the end */
+		if (sub == &subnets[ARRAY_SIZE(subnets)]) {
+			sub = &subnets[0];
+		}
+
+		if (sub->net_idx != BT_MESH_KEY_UNUSED) {
+			return sub;
+		}
+	}
+
+	return NULL;
+}
+
+void bt_mesh_net_keys_reset(void)
+{
+	int i;
+
+	/* Delete all net keys, which also takes care of all app keys which
+	 * are associated with each net key.
+	 */
+	for (i = 0; i < ARRAY_SIZE(subnets); i++) {
+		struct bt_mesh_subnet *sub = &subnets[i];
+
+		if (sub->net_idx != BT_MESH_KEY_UNUSED) {
+			subnet_del(sub);
+		}
+	}
+}
+
+bool bt_mesh_net_cred_find(struct bt_mesh_net_rx *rx, struct net_buf_simple *in,
+			   struct net_buf_simple *out,
+			   bool (*cb)(struct bt_mesh_net_rx *rx,
+				      struct net_buf_simple *in,
+				      struct net_buf_simple *out,
+				      const struct bt_mesh_net_cred *cred))
+{
+	int i, j;
+
+	BT_DBG("");
+
+	rx->friend_cred = 1U;
+#if defined(CONFIG_BT_MESH_LOW_POWER)
+	if (bt_mesh_lpn_established()) {
+		rx->sub = bt_mesh.lpn.sub;
+
+		for (j = 0; j < ARRAY_SIZE(bt_mesh.lpn.cred); j++) {
+			if (!rx->sub->keys[j].valid) {
+				continue;
+			}
+
+			if (cb(rx, in, out, &bt_mesh.lpn.cred[j])) {
+				rx->new_key = (j > 0);
+				return true;
+			}
+		}
+
+		/* LPN Should only receive on the friendship credentials when in
+		 * a friendship.
+		 */
+		return false;
+	}
+#endif
+
+#if defined(CONFIG_BT_MESH_FRIEND)
+	/** Each friendship has unique friendship credentials */
+	for (i = 0; i < ARRAY_SIZE(bt_mesh.frnd); i++) {
+		struct bt_mesh_friend *frnd = &bt_mesh.frnd[i];
+
+		if (!frnd->established) {
+			continue;
+		}
+
+		if (!frnd->subnet) {
+			continue;
+		}
+
+		rx->sub = frnd->subnet;
+
+		for (j = 0; j < ARRAY_SIZE(frnd->cred); j++) {
+			if (!rx->sub->keys[j].valid) {
+				continue;
+			}
+
+			if (cb(rx, in, out, &frnd->cred[j])) {
+				rx->new_key = (j > 0);
+				return true;
+			}
+		}
+	}
+#endif
+
+	for (i = 0; i < ARRAY_SIZE(subnets); i++) {
+		rx->sub = &subnets[i];
+		if (rx->sub->net_idx == BT_MESH_KEY_UNUSED) {
+			continue;
+		}
+
+		for (j = 0; j < ARRAY_SIZE(rx->sub->keys); j++) {
+			if (!rx->sub->keys[j].valid) {
+				continue;
+			}
+
+			if (cb(rx, in, out, &rx->sub->keys[j].msg)) {
+				rx->new_key = (j > 0);
+				return true;
+			}
+		}
+	}
+
+	return false;
+}

include/bluetooth/mesh.h
@@ -22,5 +22,6 @@
 #include <bluetooth/mesh/health_cli.h>
 #include <bluetooth/mesh/proxy.h>
 #include <bluetooth/mesh/cdb.h>
+#include <bluetooth/mesh/cfg.h>
 
 #endif /* ZEPHYR_INCLUDE_BLUETOOTH_MESH_H_ */

include/bluetooth/mesh/access.h
@@ -41,6 +41,7 @@ extern "C" {
 #define BT_MESH_ADDR_RELAYS       0xfffe
 
 #define BT_MESH_KEY_UNUSED        0xffff
+#define BT_MESH_KEY_ANY           0xffff
 #define BT_MESH_KEY_DEV           0xfffe
 #define BT_MESH_KEY_DEV_LOCAL     BT_MESH_KEY_DEV
 #define BT_MESH_KEY_DEV_REMOTE    0xfffd

include/bluetooth/mesh/cfg.h
@@ -1,0 +1,320 @@
+/** @file
+ *  @brief Bluetooth Mesh Runtime Configuration APIs.
+ */
+
+/*
+ * Copyright (c) 2020 Nordic Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_BLUETOOTH_MESH_CFG_H_
+#define ZEPHYR_INCLUDE_BLUETOOTH_MESH_CFG_H_
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <sys/types.h>
+
+/**
+ * @brief Bluetooth Mesh Runtime Configuration API
+ * @defgroup bt_mesh_cfg Bluetooth Mesh Runtime Configuration
+ * @ingroup bt_mesh
+ * @{
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Bluetooth Mesh Feature states */
+enum bt_mesh_feat_state {
+	/** Feature is supported, but disabled. */
+	BT_MESH_FEATURE_DISABLED,
+	/** Feature is supported and enabled. */
+	BT_MESH_FEATURE_ENABLED,
+	/** Feature is not supported, and cannot be enabled. */
+	BT_MESH_FEATURE_NOT_SUPPORTED,
+};
+
+/**
+ * @brief Bluetooth Mesh Subnet Configuration
+ * @defgroup bt_mesh_cfg_subnet Bluetooth Mesh Subnet Configuration
+ * @{
+ */
+
+/** @brief Add a Subnet.
+ *
+ *  Adds a subnet with the given network index and network key to the list of
+ *  known Subnets. All messages sent on the given Subnet will be processed by
+ *  this node, and the node may send and receive Network Beacons on the given
+ *  Subnet.
+ *
+ *  @param net_idx Network index.
+ *  @param key     Root network key of the Subnet. All other keys are derived
+ *                 from this.
+ *
+ *  @retval STATUS_SUCCESS The Subnet was successfully added.
+ *  @retval STATUS_INSUFF_RESOURCES No room for this Subnet.
+ *  @retval STATUS_UNSPECIFIED The Subnet couldn't be created for some reason.
+ */
+uint8_t bt_mesh_subnet_add(uint16_t net_idx, const uint8_t key[16]);
+
+/** @brief Update the given Subnet.
+ *
+ *  Starts the Key Refresh procedure for this Subnet by adding a second set of
+ *  encryption keys. The Subnet will continue sending with the old key (but
+ *  receiving messages using both) until the Subnet enters Key Refresh phase 2.
+ *
+ *  This allows a network configurator to replace old network and application
+ *  keys for the entire network, effectively removing access for all nodes that
+ *  aren't given the new keys.
+ *
+ *  @param net_idx Network index.
+ *  @param key     New root network key of the Subnet.
+ *
+ *  @retval STATUS_SUCCESS The Subnet was updated with a second key.
+ *  @retval STATUS_INVALID_NETKEY The NetIdx is unknown.
+ *  @retval STATUS_IDX_ALREADY_STORED The @c key value is the same as the
+ *                                    current key.
+ *  @retval STATUS_CANNOT_UPDATE The Subnet cannot be updated for some reason.
+ */
+uint8_t bt_mesh_subnet_update(uint16_t net_idx, const uint8_t key[16]);
+
+/** @brief Delete a Subnet.
+ *
+ *  Removes the Subnet with the given network index from the node. The node will
+ *  stop sending Network Beacons with the given Subnet, and can no longer
+ *  process messages on this Subnet.
+ *
+ *  All Applications bound to this Subnet are also deleted.
+ *
+ *  @param net_idx Network index.
+ *
+ *  @retval STATUS_SUCCESS The Subnet was deleted.
+ *  @retval STATUS_INVALID_NETKEY The NetIdx is unknown.
+ */
+uint8_t bt_mesh_subnet_del(uint16_t net_idx);
+
+/** @brief Check whether a Subnet is known.
+ *
+ *  @param net_idx Network index
+ *
+ *  @return true if a Subnet with the given index exists, false otherwise.
+ */
+bool bt_mesh_subnet_exists(uint16_t net_idx);
+
+/** @brief Set the Subnet's Key Refresh phase.
+ *
+ *  The Key Refresh procedure is started by updating the Subnet keys through
+ *  @ref bt_mesh_subnet_update. This puts the Subnet in Key Refresh Phase 1.
+ *  Once all nodes have received the new Subnet key, Key Refresh Phase 2 can be
+ *  activated through this function to start transmitting with the new network
+ *  key. Finally, to revoke the old key, set the Key Refresh Phase to 3. This
+ *  removes the old keys from the node, and returns the Subnet back to normal
+ *  single-key operation with the new key set.
+ *
+ *  @param net_idx Network index.
+ *  @param phase   Pointer to the new Key Refresh phase. Will return the actual
+ *                 Key Refresh phase after updating.
+ *
+ *  @retval STATUS_SUCCESS The Key Refresh phase of the Subnet was successfully
+ *                         changed.
+ *  @retval STATUS_INVALID_NETKEY The NetIdx is unknown.
+ *  @retval STATUS_CANNOT_UPDATE The given phase change is invalid.
+ */
+uint8_t bt_mesh_subnet_kr_phase_set(uint16_t net_idx, uint8_t *phase);
+
+/** @brief Get the Subnet's Key Refresh phase.
+ *
+ *  @param net_idx Network index.
+ *  @param phase   Pointer to the Key Refresh variable to fill.
+ *
+ *  @retval STATUS_SUCCESS Successfully populated the @c phase variable.
+ *  @retval STATUS_INVALID_NETKEY The NetIdx is unknown.
+ */
+uint8_t bt_mesh_subnet_kr_phase_get(uint16_t net_idx, uint8_t *phase);
+
+/** @brief Set the Node Identity state of the Subnet.
+ *
+ *  The Node Identity state of a Subnet determines whether the Subnet advertises
+ *  connectable Node Identity beacons for Proxy Clients to connect to.
+ *  Once started, the Node Identity beacon runs for 60 seconds, or until it is
+ *  stopped.
+ *
+ *  This function serves the same purpose as @ref bt_mesh_proxy_identity_enable,
+ *  but only acts on a single Subnet.
+ *
+ *  GATT Proxy support must be enabled through
+ *  @option{CONFIG_BT_MESH_GATT_PROXY}.
+ *
+ *  @param net_idx Network index.
+ *  @param node_id New Node Identity state, must be either @ref
+ *                 BT_MESH_FEATURE_ENABLED or @ref BT_MESH_FEATURE_DISABLED.
+ *
+ *  @retval STATUS_SUCCESS Successfully set the Node Identity state of the
+ *                         Subnet.
+ *  @retval STATUS_INVALID_NETKEY The NetIdx is unknown.
+ *  @retval STATUS_FEAT_NOT_SUPP The Node Identity feature is not supported.
+ *  @retval STATUS_CANNOT_SET Couldn't set the Node Identity state.
+ */
+uint8_t bt_mesh_subnet_node_id_set(uint16_t net_idx,
+				   enum bt_mesh_feat_state node_id);
+
+/** @brief Get the Node Identity state of the Subnet.
+ *
+ *  @param net_idx Network index.
+ *  @param node_id Node Identity variable to fill.
+ *
+ *  @retval STATUS_SUCCESS Successfully populated the @c node_id variable.
+ *  @retval STATUS_INVALID_NETKEY The NetIdx is unknown.
+ */
+uint8_t bt_mesh_subnet_node_id_get(uint16_t net_idx,
+				   enum bt_mesh_feat_state *node_id);
+
+/** @brief Get a list of all known Subnet indexes.
+ *
+ *  Builds a list of all known Subnet indexes in the @c net_idxs array.
+ *  If the @c net_idxs array is smaller than the list of known Subnets, this
+ *  function fills all available entries and returns @c -ENOMEM. In this
+ *  case, the next @c max entries of the list can be read out by calling
+ *  @code
+ *  bt_mesh_subnets_get(list, max, max);
+ *  @endcode
+ *
+ *  Note that any changes to the Subnet list between calls to this function
+ *  could change the order and number of entries in the list.
+ *
+ *  @param net_idxs Array to fill.
+ *  @param max      Max number of indexes to return.
+ *  @param skip     Number of indexes to skip. Enables batched processing of the
+ *                  list.
+ *
+ *  @return The number of indexes added to the @c net_idxs array, or @c -ENOMEM
+ *          if the number of known Subnets exceeds the @c max parameter.
+ */
+ssize_t bt_mesh_subnets_get(uint16_t net_idxs[], size_t max, off_t skip);
+
+/**
+ * @}
+ */
+
+/**
+ * @brief Bluetooth Mesh Application Configuration
+ * @defgroup bt_mesh_cfg_app Bluetooth Mesh Application Configuration
+ * @{
+ */
+
+/** @brief Add an Application key.
+ *
+ *  Adds the Application with the given index to the list of known applications.
+ *  Allows the node to send and receive model messages encrypted with this
+ *  Application key.
+ *
+ *  Every Application is bound to a specific Subnet. The node must know the
+ *  Subnet the Application is bound to before it can add the Application.
+ *
+ *  @param app_idx Application index.
+ *  @param net_idx Network index the Application is bound to.
+ *  @param key     Application key value.
+ *
+ *  @retval STATUS_SUCCESS The Application was successfully added.
+ *  @retval STATUS_INVALID_NETKEY The NetIdx is unknown.
+ *  @retval STATUS_INSUFF_RESOURCES There's no room for storing this
+ *                                  Application.
+ *  @retval STATUS_INVALID_BINDING This AppIdx is already bound to another
+ *                                 Subnet.
+ *  @retval STATUS_IDX_ALREADY_STORED This AppIdx is already stored with a
+ *                                    different key value.
+ *  @retval STATUS_CANNOT_SET Cannot set the Application key for some reason.
+ */
+uint8_t bt_mesh_app_key_add(uint16_t app_idx, uint16_t net_idx,
+			    const uint8_t key[16]);
+
+/** @brief Update an Application key.
+ *
+ *  Update an Application with a second Application key, as part of the
+ *  Key Refresh procedure of the bound Subnet. The node will continue
+ *  transmitting with the old application key (but receiving on both) until the
+ *  Subnet enters Key Refresh phase 2. Once the Subnet enters Key Refresh phase
+ *  3, the old application key will be deleted.
+ *
+ *  @note The Application key can only be updated if the bound Subnet is in Key
+ *        Refresh phase 1.
+ *
+ *  @param app_idx Application index.
+ *  @param net_idx Network index the Application is bound to, or
+ *                 @ref BT_MESH_KEY_ANY to skip the binding check.
+ *  @param key     New key value.
+ *
+ *  @retval STATUS_SUCCESS The Application key was successfully updated.
+ *  @retval STATUS_INVALID_NETKEY The NetIdx is unknown.
+ *  @retval STATUS_INVALID_BINDING This AppIdx is not bound to the given NetIdx.
+ *  @retval STATUS_CANNOT_UPDATE The Application key cannot be updated for some
+ *                               reason.
+ *  @retval STATUS_IDX_ALREADY_STORED This AppIdx is already updated with a
+ *                                    different key value.
+ */
+uint8_t bt_mesh_app_key_update(uint16_t app_idx, uint16_t net_idx,
+			       const uint8_t key[16]);
+
+/** @brief Delete an Application key.
+ *
+ *  All models bound to this application will remove this binding.
+ *  All models publishing with this application will stop publishing.
+ *
+ *  @param app_idx Application index.
+ *  @param net_idx Network index.
+ *
+ *  @retval STATUS_SUCCESS The Application key was successfully deleted.
+ *  @retval STATUS_INVALID_NETKEY The NetIdx is unknown.
+ *  @retval STATUS_INVALID_BINDING This AppIdx is not bound to the given NetIdx.
+ */
+uint8_t bt_mesh_app_key_del(uint16_t app_idx, uint16_t net_idx);
+
+/** @brief Check if an Application key is known.
+ *
+ *  @param app_idx Application index.
+ *
+ *  @return true if the Application is known, false otherwise.
+ */
+bool bt_mesh_app_key_exists(uint16_t app_idx);
+
+/** @brief Get a list of all known Application key indexes.
+ *
+ *  Builds a list of all Application indexes for the given network index in the
+ *  @c app_idxs array. If the @c app_idxs array cannot fit all bound
+ *  Applications, this function fills all available entries and returns @c
+ *  -ENOMEM. In this case, the next @c max entries of the list can be read out
+ *  by calling
+ *  @code
+ *  bt_mesh_app_keys_get(net_idx, list, max, max);
+ *  @endcode
+ *
+ *  Note that any changes to the Application key list between calls to this
+ *  function could change the order and number of entries in the list.
+ *
+ *  @param net_idx  Network Index to get the Applications of, or @ref
+ *                  BT_MESH_KEY_ANY to get all Applications.
+ *  @param app_idxs Array to fill.
+ *  @param max      Max number of indexes to return.
+ *  @param skip     Number of indexes to skip. Enables batched processing of the
+ *                  list.
+ *
+ *  @return The number of indexes added to the @c app_idxs array, or @c -ENOMEM
+ *          if the number of known Applications exceeds the @c max parameter.
+ */
+ssize_t bt_mesh_app_keys_get(uint16_t net_idx, uint16_t app_idxs[], size_t max,
+			     off_t skip);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* ZEPHYR_INCLUDE_BLUETOOTH_MESH_CFG_H_ */

include/linker/common-rom.ld
@@ -102,6 +102,11 @@
 
 	Z_ITERABLE_SECTION_ROM(bt_gatt_service_static, 4)
 
+#if defined(CONFIG_BT_MESH)
+	Z_ITERABLE_SECTION_ROM(bt_mesh_subnet_cb, 4)
+	Z_ITERABLE_SECTION_ROM(bt_mesh_app_key_cb, 4)
+#endif
+
 #if defined(CONFIG_EC_HOST_CMD)
 	Z_ITERABLE_SECTION_ROM(ec_host_cmd_handler, 4)
 #endif

subsys/bluetooth/mesh/CMakeLists.txt
@@ -8,6 +8,8 @@ zephyr_library_sources_ifdef(CONFIG_BT_MESH
     adv.c
     beacon.c
     net.c
+    subnet.c
+    app_keys.c
     transport.c
     rpl.c
     crypto.c

commit 682a006aa2cf3a34446513a44d18c392d26518d5

Bluetooth: Mesh: Check LPN credentials before establishment
While the friend node considers the friendship established as soon as it
sends the friendship offer, the LPN only considers the friendship
established once it receives the first update after its poll. This
update is encrypted with the friendship credentials, which aren't
available, as they're protected by an if (lpn_established()) check.

Changes the check to lpn_waiting_update(), which makes the LPN check its
friendship credentials only when it's expecting a response to a poll
message. This is the only time the friend will send messages encrypted
with the friendship credentials.

This is a regression from #28511.

Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

subsys/bluetooth/mesh/subnet.c
@@ -592,7 +592,7 @@ bool bt_mesh_net_cred_find(struct bt_mesh_net_rx *rx, struct net_buf_simple *in,
 	BT_DBG("");
 
 #if defined(CONFIG_BT_MESH_LOW_POWER)
-	if (bt_mesh_lpn_established()) {
+	if (bt_mesh_lpn_waiting_update()) {
 		rx->sub = bt_mesh.lpn.sub;
 
 		for (j = 0; j < ARRAY_SIZE(bt_mesh.lpn.cred); j++) {