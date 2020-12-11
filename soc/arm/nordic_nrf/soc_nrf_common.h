c8b89184f8e7bcdd9c930adf30858b9c0530bed2
Martí Bolívar <marti.bolivar@nordicsemi.no>
2020-12-11 13:57:57 -0800

    soc: nrf: clean up soc.h headers
    Make a common header for common includes.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

---
+++ soc/arm/nordic_nrf/soc_nrf_common.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file Common soc.h include for Nordic nRF5 SoCs.
+ */
+
+#ifndef _ZEPHYR_SOC_ARM_NORDIC_NRF_SOC_NRF_COMMON_H_
+#define _ZEPHYR_SOC_ARM_NORDIC_NRF_SOC_NRF_COMMON_H_
+
+#ifndef _ASMLANGUAGE
+#include <nrfx.h>
+#include <devicetree.h>
+#endif /* !_ASMLANGUAGE */
+
+#endif
