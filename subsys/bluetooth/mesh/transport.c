1c731c714c47d520bffafc3525baf000bc3d518a
Michał Narajowski <michal.narajowski@codecoup.pl>
2020-12-11 14:59:39 +0100

    Bluetooth: Mesh: Refactor bt_mesh_model_recv
    Pass only msg context as it is the only thing used from net_rx.
    
    Signed-off-by: Michał Narajowski <michal.narajowski@codecoup.pl>

--- subsys/bluetooth/mesh/transport.c
+++ subsys/bluetooth/mesh/transport.c
@@ -768,7 +768,7 @@ static int sdu_recv(struct bt_mesh_net_rx *rx, uint8_t hdr, uint8_t aszmic,
 
 	LOG_DBG("Decrypted (AppIdx: 0x%03x)", rx->ctx.app_idx);
 
-	bt_mesh_model_recv(rx, sdu);
+	bt_mesh_model_recv(&rx->ctx, sdu);
 
 	return 0;
 }
