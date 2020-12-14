5dbd2521bde66321fbc3d2ffe3f5fb632b5073e3
Kumar Gala <kumar.gala@linaro.org>
2020-12-14 10:50:08 -0600

    drivers: lora: Convert drivers to new DT device macros
    Convert lora drivers from:
    
            DEVICE_AND_API_INIT -> DEVICE_DT_INST_DEFINE
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- drivers/lora/sx126x.c
+++ drivers/lora/sx126x.c
@@ -518,7 +518,6 @@ static const struct lora_driver_api sx126x_lora_api = {
 	.test_cw = sx12xx_lora_test_cw,
 };
 
-DEVICE_AND_API_INIT(sx126x_lora, DT_INST_LABEL(0),
-		    &sx126x_lora_init, NULL,
+DEVICE_DT_INST_DEFINE(0, &sx126x_lora_init, device_pm_control_nop, NULL,
 		    NULL, POST_KERNEL, CONFIG_LORA_INIT_PRIORITY,
 		    &sx126x_lora_api);
