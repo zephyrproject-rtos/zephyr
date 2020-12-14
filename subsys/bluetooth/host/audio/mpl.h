edf5694f07363a07355ea09a75fb5c7d6e1e57d5
Emil Gydesen <emil.gydesen@nordicsemi.no>
2020-12-14 09:48:23 +0100

    Bluetooth: Audio: Adds _TEMP_ infix to non-upstream OTS - REVERT ME
    This commit should be reverted once we merge the two
    implementations, but for now both OTS implementation
    depend on the same KConfig, causing an error on `bt init`
    as both OTS implementations try to register the same PSM.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/host/audio/mpl.h
+++ subsys/bluetooth/host/audio/mpl.h
@@ -180,24 +180,24 @@ struct mpl_tseg_t {
 
 /* Tracks */
 struct mpl_track_t {
-#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
+#if defined(CONFIG_BT_OTS_TEMP) || defined(CONFIG_BT_OTC)
 	uint64_t             id;
-#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
+#endif /* CONFIG_BT_OTS_TEMP || CONFIG_BT_OTC */
 	char                 title[CONFIG_BT_MCS_TRACK_TITLE_MAX];
 	int32_t              duration;
 	struct mpl_tseg_t   *segment;
-#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
+#if defined(CONFIG_BT_OTS_TEMP) || defined(CONFIG_BT_OTC)
 	uint64_t             segments_id;
-#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
+#endif /* CONFIG_BT_OTS_TEMP || CONFIG_BT_OTC */
 	struct mpl_track_t  *prev;
 	struct mpl_track_t  *next;
 };
 
 /* Groups */
 struct mpl_group_t {
-#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
+#if defined(CONFIG_BT_OTS_TEMP) || defined(CONFIG_BT_OTC)
 	uint64_t             id;
-#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
+#endif /* CONFIG_BT_OTS_TEMP || CONFIG_BT_OTC */
 	char                 title[CONFIG_BT_MCS_GROUP_TITLE_MAX];
 	struct mpl_track_t  *track;
 	struct mpl_group_t  *parent;
@@ -208,9 +208,9 @@ struct mpl_group_t {
 /** @brief Media Player */
 struct mpl_mediaplayer_t {
 	char                name[CONFIG_BT_MCS_MEDIA_PLAYER_NAME_MAX];
-#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
+#if defined(CONFIG_BT_OTS_TEMP) || defined(CONFIG_BT_OTC)
 	uint64_t            icon_id;
-#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
+#endif /* CONFIG_BT_OTS_TEMP || CONFIG_BT_OTC */
 	char                icon_uri[CONFIG_BT_MCS_ICON_URI_MAX];
 	struct mpl_group_t *group;
 	int32_t             track_pos;
@@ -220,9 +220,9 @@ struct mpl_mediaplayer_t {
 	uint8_t             playing_order;
 	uint16_t            playing_orders_supported;
 	uint32_t            operations_supported;
-#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
+#if defined(CONFIG_BT_OTS_TEMP) || defined(CONFIG_BT_OTC)
 	uint64_t            search_results_id;
-#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
+#endif /* CONFIG_BT_OTS_TEMP || CONFIG_BT_OTC */
 	uint8_t             content_ctrl_id;
 };
 
@@ -240,7 +240,7 @@ struct mpl_op_ntf_t {
 	uint8_t result_code;
 };
 
-#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
+#if defined(CONFIG_BT_OTS_TEMP) || defined(CONFIG_BT_OTC)
 /** @brief Search control item */
 struct mpl_sci_t {
 	uint8_t len;                     /* Length of type and parameter */
@@ -253,15 +253,15 @@ struct mpl_search_t {
 	uint8_t len;
 	char    search[SEARCH_LEN_MAX]; /* Concatenated search control items */
 };	                                /* - (type, length, param) */
-#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
+#endif /* CONFIG_BT_OTS_TEMP || CONFIG_BT_OTC */
 
 int mpl_init(void);
 
 char *mpl_player_name_get(void);
 
-#ifdef CONFIG_BT_OTS
+#ifdef CONFIG_BT_OTS_TEMP
 uint64_t mpl_icon_id_get(void);
-#endif /* CONFIG_BT_OTS */
+#endif /* CONFIG_BT_OTS_TEMP */
 
 char *mpl_icon_uri_get(void);
 
@@ -284,7 +284,7 @@ void mpl_playback_speed_cb(int8_t speed);
 int8_t mpl_seeking_speed_get(void);
 void mpl_seeking_speed_cb(int8_t speed);
 
-#ifdef CONFIG_BT_OTS
+#ifdef CONFIG_BT_OTS_TEMP
 uint64_t mpl_track_segments_id_get(void);
 
 uint64_t mpl_current_track_id_get(void);
@@ -305,7 +305,7 @@ void mpl_parent_group_id_cb(uint64_t id);
 /* For  IOP testing - set current group to be it's own parent */
 void mpl_test_unset_parent_group(void);
 #endif /* CONFIG_BT_DEBUG_MCS && CONFIG_BT_TESTING */
-#endif /* CONFIG_BT_OTS */
+#endif /* CONFIG_BT_OTS_TEMP */
 
 uint8_t mpl_playing_order_get(void);
 void mpl_playing_order_set(uint8_t order);
@@ -325,13 +325,13 @@ void mpl_operation_cb(struct mpl_op_ntf_t op_ntf);
 uint32_t mpl_operations_supported_get(void);
 void mpl_operations_supported_cb(uint32_t operations);
 
-#ifdef CONFIG_BT_OTS
+#ifdef CONFIG_BT_OTS_TEMP
 void mpl_scp_set(struct mpl_search_t search);
 void mpl_search_cb(uint8_t result_code);
 
 uint64_t mpl_search_results_id_get(void);
 void mpl_search_results_id_cb(uint64_t id);
-#endif /* CONFIG_BT_OTS */
+#endif /* CONFIG_BT_OTS_TEMP */
 
 uint8_t mpl_content_ctrl_id_get(void);
 
