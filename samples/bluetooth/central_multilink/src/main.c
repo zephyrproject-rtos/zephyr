0f74dbe3cfafef7f36b562257067453d43e45cb9
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2020-12-15 18:52:49 +0530

    samples: Bluetooth: Multiple central connections to peripherals
    Application demonstrating Bluetooth Low Energy Central role
    functionality by scanning for other devices and establishing
    connection to upto 62 peripherals with a strong enough
    signal.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

---
+++ samples/bluetooth/central_multilink/src/main.c
@@ -0,0 +1,14 @@
+/* main.c - Application main entry point */
+
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int init_central(void);
+
+void main(void)
+{
+	(void)init_central();
+}
