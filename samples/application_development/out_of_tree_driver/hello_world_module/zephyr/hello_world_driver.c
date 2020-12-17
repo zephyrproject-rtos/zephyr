e2e882ab3e1c94f842276536672f0988ad78c309
Kumar Gala <kumar.gala@linaro.org>
2020-12-17 09:31:31 -0600

    samples: convert DEVICE_AND_API_INIT to DEVICE_DEFINE
    Convert samples to DEVICE_{DT_}DEFINE instead of DEVICE_AND_API_INIT
    so we can deprecate DEVICE_AND_API_INIT in the future.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- samples/application_development/out_of_tree_driver/hello_world_module/zephyr/hello_world_driver.c
+++ samples/application_development/out_of_tree_driver/hello_world_module/zephyr/hello_world_driver.c
@@ -42,7 +42,7 @@ static inline void z_vrfy_hello_world_print(const struct device *dev)
 #endif /* CONFIG_USERSPACE */
 
 
-DEVICE_AND_API_INIT(hello_world, "CUSTOM_DRIVER",
-		    init, &data, NULL,
+DEVICE_DEFINE(hello_world, "CUSTOM_DRIVER",
+		    init, device_pm_control_nop, &data, NULL,
 		    PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
 		    &((struct hello_world_driver_api){ .print = print_impl }));
