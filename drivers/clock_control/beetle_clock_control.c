914a5dd4468e7231ad20156108834663b742c817
Kumar Gala <kumar.gala@linaro.org>
2020-12-18 13:20:53 -0600

    drivers: clock_control: Convert DEVICE_AND_API_INIT to DEVICE_DEFINE
    Convert driver(s) to DEVICE_DEFINE instead of DEVICE_AND_API_INIT
    so we can deprecate DEVICE_AND_API_INIT in the future.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- drivers/clock_control/beetle_clock_control.c
+++ drivers/clock_control/beetle_clock_control.c
@@ -242,8 +242,9 @@ static const struct beetle_clock_control_cfg_t beetle_cc_cfg = {
  * @brief Clock Control device init
  *
  */
-DEVICE_AND_API_INIT(clock_control_beetle, CONFIG_ARM_CLOCK_CONTROL_DEV_NAME,
+DEVICE_DEFINE(clock_control_beetle, CONFIG_ARM_CLOCK_CONTROL_DEV_NAME,
 		    &beetle_clock_control_init,
+		    device_pm_control_nop,
 		    NULL, &beetle_cc_cfg,
 		    PRE_KERNEL_1,
 		    CONFIG_CLOCK_CONTROL_BEETLE_DEVICE_INIT_PRIORITY,
