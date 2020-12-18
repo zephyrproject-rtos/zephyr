0bdf3d50a61a49420a3313bc4817d2f67b9a6984
Kumar Gala <kumar.gala@linaro.org>
2020-12-18 09:20:21 -0600

    drivers: pwm: Convert DEVICE_AND_API_INIT to DEVICE_DEFINE
    Convert driver(s) to DEVICE_DEFINE instead of DEVICE_AND_API_INIT
    so we can deprecate DEVICE_AND_API_INIT in the future.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- drivers/pwm/pwm_pca9685.c
+++ drivers/pwm/pwm_pca9685.c
@@ -168,10 +168,10 @@ static const struct pwm_pca9685_config pwm_pca9685_0_cfg = {
 static struct pwm_pca9685_drv_data pwm_pca9685_0_drvdata;
 
 /* This has to init after I2C master */
-DEVICE_AND_API_INIT(pwm_pca9685_0, CONFIG_PWM_PCA9685_0_DEV_NAME,
-			pwm_pca9685_init,
-			&pwm_pca9685_0_drvdata, &pwm_pca9685_0_cfg,
-			POST_KERNEL, CONFIG_PWM_PCA9685_INIT_PRIORITY,
-			&pwm_pca9685_drv_api_funcs);
+DEVICE_DEFINE(pwm_pca9685_0, CONFIG_PWM_PCA9685_0_DEV_NAME,
+		pwm_pca9685_init, device_pm_control_nop,
+		&pwm_pca9685_0_drvdata, &pwm_pca9685_0_cfg,
+		POST_KERNEL, CONFIG_PWM_PCA9685_INIT_PRIORITY,
+		&pwm_pca9685_drv_api_funcs);
 
 #endif /* CONFIG_PWM_PCA9685_0 */
