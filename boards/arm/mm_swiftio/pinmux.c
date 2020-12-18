8ce383401be85559105fb45c2c77600761270f49
Frank Li <lgl88911@163.com>
2020-12-18 22:29:32 +0800

    boards: mm_swiftio: modify to follow code style
    Move the unrelated whitespace and
    Modify line break alignment.
    
    Signed-off-by: Frank Li <lgl88911@163.com>

--- boards/arm/mm_swiftio/pinmux.c
+++ boards/arm/mm_swiftio/pinmux.c
@@ -20,12 +20,12 @@ static gpio_pin_config_t enet_gpio_config = {
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(usdhc1), okay) && CONFIG_DISK_ACCESS_USDHC1
 
 /*Drive Strength Field: R0(260 Ohm @ 3.3V, 150 Ohm@1.8V, 240 Ohm for DDR)
- *Speed Field: medium(100MHz)
- *Open Drain Enable Field: Open Drain Disabled
- *Pull / Keep Enable Field: Pull/Keeper Enabled
- *Pull / Keep Select Field: Pull
- *Pull Up / Down Config. Field: 47K Ohm Pull Up
- *Hyst. Enable Field: Hysteresis Enabled.
+ * Speed Field: medium(100MHz)
+ * Open Drain Enable Field: Open Drain Disabled
+ * Pull / Keep Enable Field: Pull/Keeper Enabled
+ * Pull / Keep Select Field: Pull
+ * Pull Up / Down Config. Field: 47K Ohm Pull Up
+ * Hyst. Enable Field: Hysteresis Enabled.
  */
 
 static void mm_swiftio_usdhc_pinmux(
@@ -33,17 +33,17 @@ static void mm_swiftio_usdhc_pinmux(
 	uint32_t speed, uint32_t strength)
 {
 	uint32_t cmd_data = IOMUXC_SW_PAD_CTL_PAD_SPEED(speed) |
-				IOMUXC_SW_PAD_CTL_PAD_SRE_MASK |
-				IOMUXC_SW_PAD_CTL_PAD_PKE_MASK |
-				IOMUXC_SW_PAD_CTL_PAD_PUE_MASK |
-				IOMUXC_SW_PAD_CTL_PAD_HYS_MASK |
-				IOMUXC_SW_PAD_CTL_PAD_PUS(1) |
-				IOMUXC_SW_PAD_CTL_PAD_DSE(strength);
+			    IOMUXC_SW_PAD_CTL_PAD_SRE_MASK |
+			    IOMUXC_SW_PAD_CTL_PAD_PKE_MASK |
+			    IOMUXC_SW_PAD_CTL_PAD_PUE_MASK |
+			    IOMUXC_SW_PAD_CTL_PAD_HYS_MASK |
+			    IOMUXC_SW_PAD_CTL_PAD_PUS(1) |
+			    IOMUXC_SW_PAD_CTL_PAD_DSE(strength);
 	uint32_t clk = IOMUXC_SW_PAD_CTL_PAD_SPEED(speed) |
-				IOMUXC_SW_PAD_CTL_PAD_SRE_MASK |
-				IOMUXC_SW_PAD_CTL_PAD_HYS_MASK |
-				IOMUXC_SW_PAD_CTL_PAD_PUS(0) |
-				IOMUXC_SW_PAD_CTL_PAD_DSE(strength);
+		       IOMUXC_SW_PAD_CTL_PAD_SRE_MASK |
+		       IOMUXC_SW_PAD_CTL_PAD_HYS_MASK |
+		       IOMUXC_SW_PAD_CTL_PAD_PUS(0) |
+		       IOMUXC_SW_PAD_CTL_PAD_DSE(strength);
 
 	if (nusdhc == 0) {
 		if (init) {
@@ -87,17 +87,17 @@ static void mm_swiftio_usdhc_pinmux(
 		}
 
 		IOMUXC_SetPinConfig(IOMUXC_GPIO_SD_B0_00_USDHC1_CMD,
-			cmd_data);
+				    cmd_data);
 		IOMUXC_SetPinConfig(IOMUXC_GPIO_SD_B0_01_USDHC1_CLK,
-			clk);
+				    clk);
 		IOMUXC_SetPinConfig(IOMUXC_GPIO_SD_B0_02_USDHC1_DATA0,
-			cmd_data);
+				    cmd_data);
 		IOMUXC_SetPinConfig(IOMUXC_GPIO_SD_B0_03_USDHC1_DATA1,
-			cmd_data);
+				    cmd_data);
 		IOMUXC_SetPinConfig(IOMUXC_GPIO_SD_B0_04_USDHC1_DATA2,
-			cmd_data);
+				    cmd_data);
 		IOMUXC_SetPinConfig(IOMUXC_GPIO_SD_B0_05_USDHC1_DATA3,
-			cmd_data);
+				    cmd_data);
 	}
 }
 #endif
@@ -150,22 +150,22 @@ static int mm_swiftio_init(const struct device *dev)
 #endif
 
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(lpi2c1), okay) && CONFIG_I2C
-		IOMUXC_SetPinMux(IOMUXC_GPIO_AD_B1_00_LPI2C1_SCL, 1);
-		IOMUXC_SetPinMux(IOMUXC_GPIO_AD_B1_01_LPI2C1_SDA, 1);
+	IOMUXC_SetPinMux(IOMUXC_GPIO_AD_B1_00_LPI2C1_SCL, 1);
+	IOMUXC_SetPinMux(IOMUXC_GPIO_AD_B1_01_LPI2C1_SDA, 1);
 
-		IOMUXC_SetPinConfig(IOMUXC_GPIO_AD_B1_00_LPI2C1_SCL,
-				IOMUXC_SW_PAD_CTL_PAD_PUS(3) |
-				IOMUXC_SW_PAD_CTL_PAD_PKE_MASK |
-				IOMUXC_SW_PAD_CTL_PAD_ODE_MASK |
-				IOMUXC_SW_PAD_CTL_PAD_SPEED(2) |
-				IOMUXC_SW_PAD_CTL_PAD_DSE(6));
+	IOMUXC_SetPinConfig(IOMUXC_GPIO_AD_B1_00_LPI2C1_SCL,
+			    IOMUXC_SW_PAD_CTL_PAD_PUS(3) |
+			    IOMUXC_SW_PAD_CTL_PAD_PKE_MASK |
+			    IOMUXC_SW_PAD_CTL_PAD_ODE_MASK |
+			    IOMUXC_SW_PAD_CTL_PAD_SPEED(2) |
+			    IOMUXC_SW_PAD_CTL_PAD_DSE(6));
 
-		IOMUXC_SetPinConfig(IOMUXC_GPIO_AD_B1_01_LPI2C1_SDA,
-				IOMUXC_SW_PAD_CTL_PAD_PUS(3) |
-				IOMUXC_SW_PAD_CTL_PAD_PKE_MASK |
-				IOMUXC_SW_PAD_CTL_PAD_ODE_MASK |
-				IOMUXC_SW_PAD_CTL_PAD_SPEED(2) |
-				IOMUXC_SW_PAD_CTL_PAD_DSE(6));
+	IOMUXC_SetPinConfig(IOMUXC_GPIO_AD_B1_01_LPI2C1_SDA,
+			    IOMUXC_SW_PAD_CTL_PAD_PUS(3) |
+			    IOMUXC_SW_PAD_CTL_PAD_PKE_MASK |
+			    IOMUXC_SW_PAD_CTL_PAD_ODE_MASK |
+			    IOMUXC_SW_PAD_CTL_PAD_SPEED(2) |
+			    IOMUXC_SW_PAD_CTL_PAD_DSE(6));
 #endif
 
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(lpi2c3), okay) && CONFIG_I2C
@@ -173,18 +173,18 @@ static int mm_swiftio_init(const struct device *dev)
 	IOMUXC_SetPinMux(IOMUXC_GPIO_AD_B1_06_LPI2C3_SDA, 1);
 
 	IOMUXC_SetPinConfig(IOMUXC_GPIO_AD_B1_07_LPI2C3_SCL,
-				IOMUXC_SW_PAD_CTL_PAD_PUS(3) |
-				IOMUXC_SW_PAD_CTL_PAD_PKE_MASK |
-				IOMUXC_SW_PAD_CTL_PAD_ODE_MASK |
-				IOMUXC_SW_PAD_CTL_PAD_SPEED(2) |
-				IOMUXC_SW_PAD_CTL_PAD_DSE(6));
+			    IOMUXC_SW_PAD_CTL_PAD_PUS(3) |
+			    IOMUXC_SW_PAD_CTL_PAD_PKE_MASK |
+			    IOMUXC_SW_PAD_CTL_PAD_ODE_MASK |
+			    IOMUXC_SW_PAD_CTL_PAD_SPEED(2) |
+			    IOMUXC_SW_PAD_CTL_PAD_DSE(6));
 
 	IOMUXC_SetPinConfig(IOMUXC_GPIO_AD_B1_06_LPI2C3_SDA,
-				IOMUXC_SW_PAD_CTL_PAD_PUS(3) |
-				IOMUXC_SW_PAD_CTL_PAD_PKE_MASK |
-				IOMUXC_SW_PAD_CTL_PAD_ODE_MASK |
-				IOMUXC_SW_PAD_CTL_PAD_SPEED(2) |
-				IOMUXC_SW_PAD_CTL_PAD_DSE(6));
+			    IOMUXC_SW_PAD_CTL_PAD_PUS(3) |
+			    IOMUXC_SW_PAD_CTL_PAD_PKE_MASK |
+			    IOMUXC_SW_PAD_CTL_PAD_ODE_MASK |
+			    IOMUXC_SW_PAD_CTL_PAD_SPEED(2) |
+			    IOMUXC_SW_PAD_CTL_PAD_DSE(6));
 #endif
 
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(usdhc1), okay) && CONFIG_DISK_ACCESS_USDHC1
