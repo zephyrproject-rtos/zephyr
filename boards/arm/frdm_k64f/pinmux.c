08ed9756f53b96240afdeab495107de5aebc69f8
Henrik Brix Andersen <hebad@vestas.com>
2020-12-21 10:32:45 +0100

    boards: arm: frdm_k64f: enable FlexTimer 0 as PWM
    Enable FlexTimer 0 (FTM0) as PWM and setup PTC1 as FTM0 channel 0 for
    use in PWM loopback test.
    
    Signed-off-by: Henrik Brix Andersen <hebad@vestas.com>

--- boards/arm/frdm_k64f/pinmux.c
+++ boards/arm/frdm_k64f/pinmux.c
@@ -122,7 +122,13 @@ static int frdm_k64f_pinmux_init(const struct device *dev)
 	pinmux_pin_set(portb, 10, PORT_PCR_MUX(kPORT_PinDisabledOrAnalog));
 #endif
 
-#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(ftm3), nxp_kinetis_ftm_pwm, okay) && CONFIG_PWM
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(ftm0), nxp_kinetis_ftm_pwm, okay) \
+	&& CONFIG_PWM
+	pinmux_pin_set(portc,  1, PORT_PCR_MUX(kPORT_MuxAlt4));
+#endif
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(ftm3), nxp_kinetis_ftm_pwm, okay) \
+	&& CONFIG_PWM
 	pinmux_pin_set(portc,  8, PORT_PCR_MUX(kPORT_MuxAlt3));
 	pinmux_pin_set(portc,  9, PORT_PCR_MUX(kPORT_MuxAlt3));
 #endif
