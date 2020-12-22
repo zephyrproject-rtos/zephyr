41fa347c2cac5a4bac688d9bbc6fef204f3606a7
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2020-12-22 09:56:24 +0530

    Bluetooth: controller: Refactor PA/LNA PPI configuration
    Refactor the implementation of PA/LNA PPI configuration
    which was done on every Tx or Rx as common code to be
    executed once per radio reset. And only setup the timeout
    and enabling of PPI at every Tx or Rx.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/radio/radio_nrf5_ppi.h
+++ subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/radio/radio_nrf5_ppi.h
@@ -264,9 +264,10 @@ static inline void hal_trigger_rateoverride_ppi_config(void)
 /******************************************************************************/
 #if defined(CONFIG_BT_CTLR_GPIO_PA_PIN) || defined(CONFIG_BT_CTLR_GPIO_LNA_PIN)
 
-#define HAL_ENABLE_PALNA_PPI 15
+#define HAL_ENABLE_PALNA_PPI  15
+#define HAL_DISABLE_PALNA_PPI 16
 
-static inline void hal_enable_palna_ppi_config(void)
+static inline void hal_palna_ppi_setup(void)
 {
 	nrf_ppi_channel_endpoint_setup(
 		NRF_PPI,
@@ -274,12 +275,6 @@ static inline void hal_enable_palna_ppi_config(void)
 		(uint32_t)&(EVENT_TIMER->EVENTS_COMPARE[2]),
 		(uint32_t)&(NRF_GPIOTE->TASKS_OUT[
 				CONFIG_BT_CTLR_PA_LNA_GPIOTE_CHAN]));
-}
-
-#define HAL_DISABLE_PALNA_PPI 16
-
-static inline void hal_disable_palna_ppi_config(void)
-{
 	nrf_ppi_channel_endpoint_setup(
 		NRF_PPI,
 		HAL_DISABLE_PALNA_PPI,
