4db7c433f2952574415d16e3ecb3b7b93584a0e1
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2020-12-26 13:57:46 +0530

    Bluetooth: controller: Fix missing node rx allocation for ISO establish
    Added missing allocation of node rx buffer required for
    generation of Broadcast ISO Sync Established.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_sync_types.h
+++ subsys/bluetooth/controller/ll_sw/ull_sync_types.h
@@ -9,19 +9,7 @@
 #define LL_SYNC_STATE_CREATED    0x02
 
 #if defined(CONFIG_BT_CTLR_SYNC_ISO)
-struct ll_sync_iso {
-	struct evt_hdr evt;
-	struct ull_hdr ull;
-	struct lll_sync_iso lll;
-
-	struct node_rx_hdr node_rx_lost;
-	struct node_rx_hdr node_rx_estab;
-};
-
-struct node_rx_sync_iso {
-	uint8_t status;
-	uint16_t interval;
-};
+struct ll_sync_iso;
 #endif /* CONFIG_BT_CTLR_SYNC_ISO */
 
 struct ll_sync_set {
@@ -47,7 +35,15 @@ struct ll_sync_set {
 	} node_rx_lost;
 
 #if defined(CONFIG_BT_CTLR_SYNC_ISO)
-	struct ll_sync_iso *sync_iso;
+	struct {
+		struct node_rx_hdr *node_rx_estab;
+
+		/* Non-Null when creating sync, reset in ISR context on
+		 * synchronisation state and checked in Thread context when
+		 * cancelling sync create, hence the volatile keyword.
+		 */
+		struct ll_sync_iso *volatile sync_iso;
+	} iso;
 #endif /* CONFIG_BT_CTLR_SYNC_ISO */
 };
 
@@ -57,3 +53,34 @@ struct node_rx_sync {
 	uint16_t interval;
 	uint8_t  sca;
 };
+
+#if defined(CONFIG_BT_CTLR_SYNC_ISO)
+struct ll_sync_iso {
+	struct evt_hdr evt;
+	struct ull_hdr ull;
+	struct lll_sync_iso lll;
+
+	/* Periodic Advertising Sync that contained the BIGInfo */
+	struct ll_sync_set *sync;
+
+	/* node rx type with memory aligned storage for sync lost reason.
+	 * HCI will reference the value using the pdu member of
+	 * struct node_rx_pdu.
+	 */
+	struct {
+		struct node_rx_hdr hdr;
+		union {
+			uint8_t pdu[0] __aligned(4);
+			struct {
+				uint8_t handle;
+				uint8_t reason;
+			};
+		};
+	} node_rx_lost;
+};
+
+struct node_rx_sync_iso {
+	uint8_t status;
+	uint16_t interval;
+};
+#endif /* CONFIG_BT_CTLR_SYNC_ISO */
