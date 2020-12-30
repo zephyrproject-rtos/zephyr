96c40ba8acfd74d499ee721520596ac105ad1a1a
Henrik Brix Andersen <hebad@vestas.com>
2020-12-30 22:30:09 +0100

    tests: drivers: pwm: api: run PWM tests in user space
    Run the PWM API tests in user space.
    
    Signed-off-by: Henrik Brix Andersen <hebad@vestas.com>

--- tests/drivers/pwm/pwm_api/src/main.c
+++ tests/drivers/pwm/pwm_api/src/main.c
@@ -8,15 +8,21 @@
 #include <zephyr.h>
 #include <ztest.h>
 
+const struct device *get_pwm_device(void);
 void test_pwm_usec(void);
 void test_pwm_cycle(void);
 void test_pwm_nsec(void);
 
 void test_main(void)
 {
+	const struct device *dev = get_pwm_device();
+
+	zassert_not_null(dev, "Cannot get PWM device");
+	k_object_access_grant(dev, k_current_get());
+
 	ztest_test_suite(pwm_basic_test,
-			 ztest_unit_test(test_pwm_usec),
-			 ztest_unit_test(test_pwm_nsec),
-			 ztest_unit_test(test_pwm_cycle));
+			 ztest_user_unit_test(test_pwm_usec),
+			 ztest_user_unit_test(test_pwm_nsec),
+			 ztest_user_unit_test(test_pwm_cycle));
 	ztest_run_test_suite(pwm_basic_test);
 }
