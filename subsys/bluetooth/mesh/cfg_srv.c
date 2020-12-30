zephyrproject-rtos/zephyr

commit 5374245dd66e630932267663546d5f1108aff9b5

Bluetooth: Mesh: split prov.c into two separate modules
Currently all provisioning procedure into common source
files call `prov.c`, that will not compile separately.

Add `BT_MESH_NODE` to control whether nodes are supported
and device provisioning is supported, this will be used in
provisioner role.

Add more provisioner OOB authentication method.

Signed-off-by: Lingao Meng <mengabc1086@gmail.com>

subsys/bluetooth/mesh/pb_gatt.c
@@ -8,11 +8,10 @@
  */
 #include <bluetooth/mesh.h>
 #include <bluetooth/conn.h>
-#include "prov.h"
 #include "net.h"
 #include "proxy.h"
 #include "adv.h"
-#include "prov_bearer.h"
+#include "prov.h"
 
 #define BT_DBG_ENABLED IS_ENABLED(CONFIG_BT_MESH_DEBUG_PROV)
 #define LOG_MODULE_NAME bt_mesh_pb_gatt

include/bluetooth/mesh/main.h
@@ -63,6 +63,33 @@ typedef enum {
 	BT_MESH_PROV_OOB_ON_DEV    = BIT(15),
 } bt_mesh_prov_oob_info_t;
 
+/** Device Capabilities. */
+struct bt_mesh_dev_capabilities {
+	/** Number of elements supported by the device */
+	uint8_t elem_count;
+
+	/** Supported algorithms and other capabilities */
+	uint16_t algorithms;
+
+	/** Supported public key types */
+	uint8_t pub_key_type;
+
+	/** Supported static OOB Types */
+	uint8_t static_oob;
+
+	/** Supported Output OOB Actions */
+	bt_mesh_output_action_t output_actions;
+
+	/** Supported Input OOB Actions */
+	bt_mesh_input_action_t input_actions;
+
+	/** Maximum size of Output OOB supported */
+	uint8_t output_size;
+
+	/** Maximum size in octets of Input OOB supported */
+	uint8_t input_size;
+};
+
 /** Provisioning properties & capabilities. */
 struct bt_mesh_prov {
 	/** The UUID that's used when advertising as unprovisioned */
@@ -92,6 +119,21 @@ struct bt_mesh_prov {
 	uint8_t        input_size;
 	/** Supported Input OOB Actions */
 	uint16_t       input_actions;
+
+	/** @brief Provisioning Capabilities.
+	 *
+	 *  This callback notifies the application that the provisioning capabilities
+	 *  of the unprovisioned device has been received.
+	 *
+	 *  The application can consequently call bt_mesh_auth_method_set_<*> to
+	 *  select suitable provisioning oob authentication method.
+	 *
+	 *  When this callback returns, the provisioner will start authentication with
+	 *  the chosen method.
+	 *
+	 *  @param cap capabilities supported by device.
+	 */
+	void         (*capabilities)(const struct bt_mesh_dev_capabilities *cap);
 
 	/** @brief Output of a number is requested.
 	 *
@@ -229,6 +271,89 @@ int bt_mesh_input_string(const char *str);
  *  @return Zero on success or (negative) error code otherwise.
  */
 int bt_mesh_input_number(uint32_t num);
+
+/** @brief Provide Device public key.
+ *
+ *  @param public_key Device public key.
+ *
+ *  @return Zero on success or (negative) error code otherwise.
+ */
+int bt_mesh_prov_remote_pub_key_set(const uint8_t public_key[64]);
+
+/** @brief Use Input OOB authentication.
+ *
+ *  Provisioner only.
+ *
+ *  Instruct the unprovisioned device to use the specified Input OOB
+ *  authentication action. When using @ref BT_MESH_PUSH, @ref BT_MESH_TWIST or
+ *  @ref BT_MESH_ENTER_NUMBER, the @ref bt_mesh_prov::output_number callback is
+ *  called with a random number that has to be entered on the unprovisioned
+ *  device.
+ *
+ *  When using @ref BT_MESH_ENTER_STRING, the @ref bt_mesh_prov::output_string
+ *  callback is called with a random string that has to be entered on the
+ *  unprovisioned device.
+ *
+ *  @param action Authentication action used by the unprovisioned device.
+ *  @param size Authentication size.
+ *
+ *  @return Zero on success or (negative) error code otherwise.
+ */
+int bt_mesh_auth_method_set_input(bt_mesh_input_action_t action, uint8_t size);
+
+/** @brief Use Output OOB authentication.
+ *
+ *  Provisioner only.
+ *
+ *  Instruct the unprovisioned device to use the specified Output OOB
+ *  authentication action. The @ref bt_mesh_prov::input callback will
+ *  be called.
+ *
+ *  When using @ref BT_MESH_BLINK, @ref BT_MESH_BEEP, @ref BT_MESH_VIBRATE
+ *  or @ref BT_MESH_DISPLAY_NUMBER, and the application has to call
+ *  @ref bt_mesh_input_number with the random number indicated by
+ *  the unprovisioned device.
+ *
+ *  When using @ref BT_MESH_DISPLAY_STRING, the application has to call
+ *  @ref bt_mesh_input_string with the random string displayed by the
+ *  unprovisioned device.
+ *
+ *  @param action Authentication action used by the unprovisioned device.
+ *  @param size Authentication size.
+ *
+ *  @return Zero on success or (negative) error code otherwise.
+ */
+int bt_mesh_auth_method_set_output(bt_mesh_output_action_t action, uint8_t size);
+
+/** @brief Use static OOB authentication.
+ *
+ *  Provisioner only.
+ *
+ *  Instruct the unprovisioned device to use static OOB authentication, and use
+ *  the given static authentication value when provisioning.
+ *
+ *  @param static_val Static OOB value.
+ *  @param size Static OOB value size.
+ *
+ *  @return Zero on success or (negative) error code otherwise.
+ */
+int bt_mesh_auth_method_set_static(const uint8_t *static_val, uint8_t size);
+
+/** @brief Don't use OOB authentication.
+ *
+ *  Provisioner only.
+ *
+ *  Don't use any authentication when provisioning new devices. This is the
+ *  default behavior.
+ *
+ *  @warning Not using any authentication exposes the mesh network to
+ *           impersonation attacks, where attackers can pretend to be the
+ *           unprovisioned device to gain access to the network. Authentication
+ *           is strongly encouraged.
+ *
+ *  @return Zero on success or (negative) error code otherwise.
+ */
+int bt_mesh_auth_method_set_none(void);
 
 /** @brief Enable specific provisioning bearers
  *

samples/bluetooth/mesh_provisioner/prj.conf
@@ -19,6 +19,7 @@ CONFIG_BT_MESH_CFG_CLI=y
 CONFIG_BT_MESH_HEALTH_CLI=y
 
 CONFIG_BT_MESH_PROVISIONER=y
+CONFIG_BT_MESH_PROV_DEVICE=n
 CONFIG_BT_MESH_CDB=y
 CONFIG_BT_MESH_CDB_NODE_COUNT=3
 CONFIG_BT_MESH_CDB_SUBNET_COUNT=3

subsys/bluetooth/mesh/CMakeLists.txt
@@ -26,6 +26,10 @@ zephyr_library_sources_ifdef(CONFIG_BT_MESH_FRIEND friend.c)
 
 zephyr_library_sources_ifdef(CONFIG_BT_MESH_PROV prov.c)
 
+zephyr_library_sources_ifdef(CONFIG_BT_MESH_PROV_DEVICE prov_device.c)
+
+zephyr_library_sources_ifdef(CONFIG_BT_MESH_PROVISIONER provisioner.c)
+
 zephyr_library_sources_ifdef(CONFIG_BT_MESH_PB_ADV pb_adv.c)
 
 zephyr_library_sources_ifdef(CONFIG_BT_MESH_PB_GATT pb_gatt.c)

subsys/bluetooth/mesh/Kconfig
@@ -21,6 +21,13 @@ if BT_MESH
 config BT_MESH_PROV
 	bool
 
+config BT_MESH_PROV_DEVICE
+	bool "Provisioning device role support"
+	depends on BT_MESH_PROV
+	default y
+	help
+	  Enable this option to allow the device to be provisioned into a mesh network.
+
 config BT_MESH_PB_ADV
 	bool "Provisioning support using the advertising bearer (PB-ADV)"
 	select BT_MESH_PROV
@@ -47,12 +54,14 @@ config BT_MESH_PB_ADV_RETRANS_TIMEOUT
 
 config BT_MESH_PROVISIONER
 	bool "Provisioner support"
-	select BT_MESH_PROV
+	depends on BT_MESH_PROV
+	depends on BT_MESH_PB_ADV
 	help
 	  Enable this option to have support for provisioning remote devices.
 
 config BT_MESH_CDB
 	bool "Mesh Configuration Database [EXPERIMENTAL]"
+	default y if BT_MESH_PROVISIONER
 
 if BT_MESH_CDB
 
@@ -652,6 +661,12 @@ config BT_MESH_DEBUG_PROV
 	bool "Provisioning debug"
 	help
 	  Use this option to enable Provisioning debug logs for the
+	  Bluetooth Mesh functionality.
+
+config BT_MESH_DEBUG_PROVISIONER
+	bool "Provisioner debug"
+	help
+	  Use this option to enable Provisioner debug logs for the
 	  Bluetooth Mesh functionality.
 
 config BT_MESH_DEBUG_ACCESS

subsys/bluetooth/mesh/beacon.c
@@ -233,7 +233,7 @@ static void update_beacon_observation(void)
 static void beacon_send(struct k_work *work)
 {
 	/* Don't send anything if we have an active provisioning link */
-	if (IS_ENABLED(CONFIG_BT_MESH_PB_ADV) && bt_prov_active()) {
+	if (IS_ENABLED(CONFIG_BT_MESH_PB_ADV) && bt_mesh_prov_active()) {
 		k_delayed_work_submit(&beacon_timer, K_SECONDS(CONFIG_BT_MESH_UNPROV_BEACON_INT));
 		return;
 	}

subsys/bluetooth/mesh/main.c
@@ -23,6 +23,7 @@
 #include "test.h"
 #include "adv.h"
 #include "prov.h"
+#include "provisioner.h"
 #include "net.h"
 #include "app_keys.h"
 #include "rpl.h"

subsys/bluetooth/mesh/pb_adv.c
@@ -13,11 +13,10 @@
 #include <net/buf.h>
 #include "host/testing.h"
 #include "net.h"
-#include "prov.h"
 #include "adv.h"
 #include "crypto.h"
 #include "beacon.h"
-#include "prov_bearer.h"
+#include "prov.h"
 
 #define BT_DBG_ENABLED IS_ENABLED(CONFIG_BT_MESH_DEBUG_PROV)
 #define LOG_MODULE_NAME bt_mesh_pb_adv
@@ -61,20 +60,20 @@
 #define RETRANSMITS_ACK        2
 
 enum {
-	LINK_ACTIVE,    /* Link has been opened */
-	LINK_ACK_RECVD, /* Ack for link has been received */
-	LINK_CLOSING,   /* Link is closing down */
-	LINK_INVALID,   /* Error occurred during provisioning */
-	ACK_PENDING,    /* An acknowledgment is being sent */
-	PROVISIONER,    /* The link was opened as provisioner */
+	ADV_LINK_ACTIVE,    	/* Link has been opened */
+	ADV_LINK_ACK_RECVD, 	/* Ack for link has been received */
+	ADV_LINK_CLOSING,   	/* Link is closing down */
+	ADV_LINK_INVALID,   	/* Error occurred during provisioning */
+	ADV_ACK_PENDING,    	/* An acknowledgment is being sent */
+	ADV_PROVISIONER,    	/* The link was opened as provisioner */
 
-	NUM_FLAGS,
+	ADV_NUM_FLAGS,
 };
 
 struct pb_adv {
 	uint32_t id; /* Link ID */
 
-	ATOMIC_DEFINE(flags, NUM_FLAGS);
+	ATOMIC_DEFINE(flags, ADV_NUM_FLAGS);
 
 	const struct prov_bearer_cb *cb;
 	void *cb_data;
@@ -190,7 +189,7 @@ static void reset_adv_link(void)
 
 	k_delayed_work_cancel(&link.prot_timer);
 
-	if (atomic_test_bit(link.flags, PROVISIONER)) {
+	if (atomic_test_bit(link.flags, ADV_PROVISIONER)) {
 		/* Clear everything except the retransmit and protocol timer
 		 * delayed work objects.
 		 */
@@ -236,19 +235,19 @@ static struct net_buf *adv_buf_create(uint8_t retransmits)
 static void ack_complete(uint16_t duration, int err, void *user_data)
 {
 	BT_DBG("xact 0x%x complete", (uint8_t)link.tx.pending_ack);
-	atomic_clear_bit(link.flags, ACK_PENDING);
+	atomic_clear_bit(link.flags, ADV_ACK_PENDING);
 }
 
 static bool ack_pending(void)
 {
-	return atomic_test_bit(link.flags, ACK_PENDING);
+	return atomic_test_bit(link.flags, ADV_ACK_PENDING);
 }
 
 static void prov_failed(uint8_t err)
 {
 	BT_DBG("%u", err);
 	link.cb->error(&pb_adv, link.cb_data, err);
-	atomic_set_bit(link.flags, LINK_INVALID);
+	atomic_set_bit(link.flags, ADV_LINK_INVALID);
 }
 
 static void prov_msg_recv(void)
@@ -262,7 +261,7 @@ static void prov_msg_recv(void)
 
 	gen_prov_ack_send(link.rx.id);
 
-	if (atomic_test_bit(link.flags, LINK_INVALID)) {
+	if (atomic_test_bit(link.flags, ADV_LINK_INVALID)) {
 		BT_WARN("Unexpected msg 0x%02x on invalidated link",
 			link.rx.buf->data[0]);
 		prov_failed(PROV_ERR_UNEXP_PDU);
@@ -290,7 +289,7 @@ static void gen_prov_ack_send(uint8_t xact_id)
 	};
 	const struct bt_mesh_send_cb *complete;
 	struct net_buf *buf;
-	bool pending = atomic_test_and_set_bit(link.flags, ACK_PENDING);
+	bool pending = atomic_test_and_set_bit(link.flags, ADV_ACK_PENDING);
 
 	BT_DBG("xact_id 0x%x", xact_id);
 
@@ -301,7 +300,7 @@ static void gen_prov_ack_send(uint8_t xact_id)
 
 	buf = adv_buf_create(RETRANSMITS_ACK);
 	if (!buf) {
-		atomic_clear_bit(link.flags, ACK_PENDING);
+		atomic_clear_bit(link.flags, ADV_ACK_PENDING);
 		return;
 	}
 
@@ -394,8 +393,8 @@ static void gen_prov_ack(struct prov_rx *rx, struct net_buf_simple *buf)
 	}
 
 	if (rx->xact_id == link.tx.id) {
-		/* Don't clear resending of LINK_CLOSE messages */
-		if (!atomic_test_bit(link.flags, LINK_CLOSING)) {
+		/* Don't clear resending of link_close messages */
+		if (!atomic_test_bit(link.flags, ADV_LINK_CLOSING)) {
 			prov_clear_tx();
 		}
 
@@ -489,14 +488,14 @@ static void gen_prov_ctl(struct prov_rx *rx, struct net_buf_simple *buf)
 		link_open(rx, buf);
 		break;
 	case LINK_ACK:
-		if (!atomic_test_bit(link.flags, LINK_ACTIVE)) {
+		if (!atomic_test_bit(link.flags, ADV_LINK_ACTIVE)) {
 			return;
 		}
 
 		link_ack(rx, buf);
 		break;
 	case LINK_CLOSE:
-		if (!atomic_test_bit(link.flags, LINK_ACTIVE)) {
+		if (!atomic_test_bit(link.flags, ADV_LINK_ACTIVE)) {
 			return;
 		}
 
@@ -531,7 +530,7 @@ static void gen_prov_recv(struct prov_rx *rx, struct net_buf_simple *buf)
 		return;
 	}
 
-	if (!atomic_test_bit(link.flags, LINK_ACTIVE) &&
+	if (!atomic_test_bit(link.flags, ADV_LINK_ACTIVE) &&
 	    gen_prov[GPCF(rx->gpc)].require_link) {
 		BT_DBG("Ignoring message that requires active link");
 		return;
@@ -572,24 +571,24 @@ static void prov_retransmit(struct k_work *work)
 
 	BT_DBG("");
 
-	if (!atomic_test_bit(link.flags, LINK_ACTIVE)) {
+	if (!atomic_test_bit(link.flags, ADV_LINK_ACTIVE)) {
 		BT_WARN("Link not active");
 		return;
 	}
 
 	/*
 	 * According to mesh profile spec (5.3.1.4.3), the close message should
-	 * be restransmitted at least three times. Retransmit the LINK_CLOSE
+	 * be restransmitted at least three times. Retransmit the link_close
 	 * message until CLOSING_TIMEOUT has elapsed.
 	 */
-	if (atomic_test_bit(link.flags, LINK_CLOSING)) {
+	if (atomic_test_bit(link.flags, ADV_LINK_CLOSING)) {
 		timeout_ms = CLOSING_TIMEOUT;
 	} else {
 		timeout_ms = TRANSACTION_TIMEOUT;
 	}
 
 	if (k_uptime_get() - link.tx.start > timeout_ms) {
-		if (atomic_test_bit(link.flags, LINK_CLOSING)) {
+		if (atomic_test_bit(link.flags, ADV_LINK_CLOSING)) {
 			close_link(PROV_BEARER_LINK_STATUS_SUCCESS);
 		} else {
 			BT_WARN("Giving up transaction");
@@ -732,7 +731,7 @@ static void link_open(struct prov_rx *rx, struct net_buf_simple *buf)
 		return;
 	}
 
-	if (atomic_test_bit(link.flags, LINK_ACTIVE)) {
+	if (atomic_test_bit(link.flags, ADV_LINK_ACTIVE)) {
 		/* Send another link ack if the provisioner missed the last */
 		if (link.id == rx->link_id) {
 			BT_DBG("Resending link ack");
@@ -750,7 +749,7 @@ static void link_open(struct prov_rx *rx, struct net_buf_simple *buf)
 	}
 
 	link.id = rx->link_id;
-	atomic_set_bit(link.flags, LINK_ACTIVE);
+	atomic_set_bit(link.flags, ADV_LINK_ACTIVE);
 	net_buf_simple_reset(link.rx.buf);
 
 	bearer_ctl_send(LINK_ACK, NULL, 0, false);
@@ -762,8 +761,8 @@ static void link_ack(struct prov_rx *rx, struct net_buf_simple *buf)
 {
 	BT_DBG("len %u", buf->len);
 
-	if (atomic_test_bit(link.flags, PROVISIONER)) {
-		if (atomic_test_and_set_bit(link.flags, LINK_ACK_RECVD)) {
+	if (atomic_test_bit(link.flags, ADV_PROVISIONER)) {
+		if (atomic_test_and_set_bit(link.flags, ADV_LINK_ACK_RECVD)) {
 			return;
 		}
 
@@ -805,7 +804,7 @@ void bt_mesh_pb_adv_recv(struct net_buf_simple *buf)
 	rx.xact_id = net_buf_simple_pull_u8(buf);
 	rx.gpc = net_buf_simple_pull_u8(buf);
 
-	if (atomic_test_bit(link.flags, LINK_ACTIVE) && link.id != rx.link_id) {
+	if (atomic_test_bit(link.flags, ADV_LINK_ACTIVE) && link.id != rx.link_id) {
 		return;
 	}
 
@@ -819,11 +818,11 @@ static int prov_link_open(const uint8_t uuid[16], k_timeout_t timeout,
 {
 	BT_DBG("uuid %s", bt_hex(uuid, 16));
 
-	if (atomic_test_and_set_bit(link.flags, LINK_ACTIVE)) {
+	if (atomic_test_and_set_bit(link.flags, ADV_LINK_ACTIVE)) {
 		return -EBUSY;
 	}
 
-	atomic_set_bit(link.flags, PROVISIONER);
+	atomic_set_bit(link.flags, ADV_PROVISIONER);
 
 	bt_rand(&link.id, sizeof(link.id));
 	link.tx.id = XACT_ID_MAX;
@@ -840,7 +839,7 @@ static int prov_link_open(const uint8_t uuid[16], k_timeout_t timeout,
 
 static int prov_link_accept(const struct prov_bearer_cb *cb, void *cb_data)
 {
-	if (atomic_test_bit(link.flags, LINK_ACTIVE)) {
+	if (atomic_test_bit(link.flags, ADV_LINK_ACTIVE)) {
 		return -EBUSY;
 	}
 
@@ -859,7 +858,7 @@ static int prov_link_accept(const struct prov_bearer_cb *cb, void *cb_data)
 
 static void prov_link_close(enum prov_bearer_link_status status)
 {
-	if (atomic_test_and_set_bit(link.flags, LINK_CLOSING)) {
+	if (atomic_test_and_set_bit(link.flags, ADV_LINK_CLOSING)) {
 		return;
 	}
 

commit d8dcf5790e32e6745909776409330c5fdc0af045

Bluetooth: Mesh: Add a send callback mechanism for proxy
Zephyr Bluetooth Mesh did not check whether the proxy message
was actually sent out, so that the response message could
not be received during reset. Although @trond tried to fix
this problem in PR(#26668), it did not solve the status
callback of the proxy sending message, so a new problem was
introduced after PR(#28457) merged.

`bt_mesh_prov_send(&buf, public_key_sent))`

This PR will try to solve the above problem, and will fix
the problem due to thread competition, and PR(26668) will
not be necessary.

Fixes: #31019

Signed-off-by: Lingao Meng <menglingao@xiaomi.com>

subsys/bluetooth/mesh/cfg_srv.c
@@ -1964,11 +1964,27 @@ send_list:
 	}
 }
 
+static void reset_send_start(uint16_t duration, int err, void *cb_data)
+{
+	if (err) {
+		BT_ERR("Sending Node Reset Status failed (err %d)", err);
+		bt_mesh_reset();
+	}
+}
+
+static void reset_send_end(int err, void *cb_data)
+{
+	bt_mesh_reset();
+}
+
 static void node_reset(struct bt_mesh_model *model,
 		       struct bt_mesh_msg_ctx *ctx,
 		       struct net_buf_simple *buf)
 {
-	static struct bt_mesh_proxy_idle_cb proxy_idle = {.cb = bt_mesh_reset};
+	static const struct bt_mesh_send_cb reset_cb = {
+		.start = reset_send_start,
+		.end = reset_send_end,
+	};
 
 	BT_MESH_MODEL_BUF_DEFINE(msg, OP_NODE_RESET_STATUS, 0);
 
@@ -1976,25 +1992,12 @@ static void node_reset(struct bt_mesh_model *model,
 	       ctx->net_idx, ctx->app_idx, ctx->addr, buf->len,
 	       bt_hex(buf->data, buf->len));
 
-
 	bt_mesh_model_msg_init(&msg, OP_NODE_RESET_STATUS);
 
-	/* Send the response first since we wont have any keys left to
-	 * send it later.
-	 */
-	if (bt_mesh_model_send(model, ctx, &msg, NULL, NULL)) {
+	if (bt_mesh_model_send(model, ctx, &msg, &reset_cb, NULL)) {
 		BT_ERR("Unable to send Node Reset Status");
-	}
-
-	if (!IS_ENABLED(CONFIG_BT_MESH_GATT_PROXY)) {
 		bt_mesh_reset();
-		return;
 	}
-
-	/* If the response goes to a proxy node, we'll wait for the sending to
-	 * complete before moving on.
-	 */
-	bt_mesh_proxy_on_idle(&proxy_idle);
 }
 
 static void send_friend_status(struct bt_mesh_model *model,

subsys/bluetooth/mesh/net.c
@@ -495,14 +495,13 @@ int bt_mesh_net_send(struct bt_mesh_net_tx *tx, struct net_buf *buf,
 	}
 
 	/* Deliver to GATT Proxy Clients if necessary. */
-	if (IS_ENABLED(CONFIG_BT_MESH_GATT_PROXY) &&
-	    bt_mesh_proxy_relay(&buf->b, tx->ctx->addr) &&
-	    BT_MESH_ADDR_IS_UNICAST(tx->ctx->addr)) {
-		/* Notify completion if this only went through the Mesh Proxy */
-		send_cb_finalize(cb, cb_data);
-
-		err = 0;
-		goto done;
+	if (IS_ENABLED(CONFIG_BT_MESH_GATT_PROXY)) {
+		if (!BT_MESH_ADDR_IS_UNICAST(tx->ctx->addr)) {
+			bt_mesh_proxy_relay(&buf->b, tx->ctx->addr, NULL, NULL);
+		} else if (bt_mesh_proxy_relay(&buf->b, tx->ctx->addr,
+						cb, cb_data)) {
+			goto done;
+		}
 	}
 
 	bt_mesh_adv_send(buf, cb, cb_data);
@@ -666,7 +665,7 @@ static void bt_mesh_net_relay(struct net_buf_simple *sbuf,
 	if (IS_ENABLED(CONFIG_BT_MESH_GATT_PROXY) &&
 	    (rx->friend_cred ||
 	     bt_mesh_gatt_proxy_get() == BT_MESH_GATT_PROXY_ENABLED)) {
-		bt_mesh_proxy_relay(&buf->b, rx->ctx.recv_dst);
+		bt_mesh_proxy_relay(&buf->b, rx->ctx.recv_dst, NULL, NULL);
 	}
 
 	if (relay_to_adv(rx->net_if) || rx->friend_cred) {

subsys/bluetooth/mesh/pb_gatt.c
@@ -17,10 +17,16 @@
 #define LOG_MODULE_NAME bt_mesh_pb_gatt
 #include "common/log.h"
 
+struct prov_bearer_send_cb {
+	prov_bearer_send_complete_t cb;
+	void *cb_data;
+};
+
 struct prov_link {
 	struct bt_conn *conn;
 	const struct prov_bearer_cb *cb;
 	void *cb_data;
+	struct prov_bearer_send_cb comp;
 	struct net_buf_simple *rx_buf;
 	struct k_delayed_work prot_timer;
 };
@@ -118,16 +124,30 @@ static int link_accept(const struct prov_bearer_cb *cb, void *cb_data)
 	return 0;
 }
 
+static void pb_gatt_send_end(int err, void *cb_data)
+{
+	if (link.comp.cb) {
+		link.comp.cb(err, cb_data);
+	}
+}
+
 static int buf_send(struct net_buf_simple *buf, prov_bearer_send_complete_t cb,
 		    void *cb_data)
 {
+	static const struct bt_mesh_send_cb pb_gatt_send_cb = {
+		.end = pb_gatt_send_end,
+	};
+
 	if (!link.conn) {
 		return -ENOTCONN;
 	}
 
+	link.comp.cb = cb;
+	link.comp.cb_data = cb_data;
+
 	k_delayed_work_submit(&link.prot_timer, PROTOCOL_TIMEOUT);
 
-	return bt_mesh_proxy_send(link.conn, BT_MESH_PROXY_PROV, buf);
+	return bt_mesh_pb_gatt_send(link.conn, buf, &pb_gatt_send_cb, cb_data);
 }
 
 static void clear_tx(void)

subsys/bluetooth/mesh/proxy.c
@@ -83,6 +83,16 @@ static void proxy_send_beacons(struct k_work *work);
 static bool prov_fast_adv;
 #endif
 
+static struct bt_mesh_proxy {
+	const struct bt_mesh_send_cb *cb;
+	void *cb_data;
+	atomic_t ref;
+} proxy_pool[CONFIG_BT_MESH_ADV_BUF_COUNT] = {
+	[0 ... (CONFIG_BT_MESH_ADV_BUF_COUNT - 1)] = {
+		.ref = ATOMIC_INIT(0),
+	},
+};
+
 static struct bt_mesh_proxy_client {
 	struct bt_conn *conn;
 	uint16_t filter[CONFIG_BT_MESH_PROXY_FILTER_SIZE];
@@ -106,8 +116,6 @@ static struct bt_mesh_proxy_client {
 	},
 };
 
-static sys_slist_t idle_waiters;
-static atomic_t pending_notifications;
 static uint8_t __noinit client_buf_data[CLIENT_BUF_SIZE * CONFIG_BT_MAX_CONN];
 
 /* Track which service is enabled */
@@ -116,6 +124,22 @@ static enum {
 	MESH_GATT_PROV,
 	MESH_GATT_PROXY,
 } gatt_svc = MESH_GATT_NONE;
+
+static struct bt_mesh_proxy *proxy_alloc(const struct bt_mesh_send_cb *cb,
+					void *cb_data)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(proxy_pool); i++) {
+		if (!atomic_get(&proxy_pool[i].ref)) {
+			proxy_pool[i].cb = cb;
+			proxy_pool[i].cb_data = cb_data;
+			return &proxy_pool[i];
+		}
+	}
+
+	return NULL;
+}
 
 static struct bt_mesh_proxy_client *find_client(struct bt_conn *conn)
 {
@@ -148,7 +172,8 @@ static void proxy_sar_timeout(struct k_work *work)
 static struct bt_mesh_subnet *beacon_sub;
 
 static int proxy_segment_and_send(struct bt_conn *conn, uint8_t type,
-				  struct net_buf_simple *msg);
+				  struct net_buf_simple *msg,
+				  struct bt_mesh_proxy *proxy);
 
 static int filter_set(struct bt_mesh_proxy_client *client,
 		      struct net_buf_simple *buf)
@@ -264,7 +289,8 @@ static void send_filter_status(struct bt_mesh_proxy_client *client,
 		return;
 	}
 
-	err = proxy_segment_and_send(client->conn, BT_MESH_PROXY_CONFIG, buf);
+	err = proxy_segment_and_send(client->conn, BT_MESH_PROXY_CONFIG,
+				     buf, NULL);
 	if (err) {
 		BT_ERR("Failed to send proxy cfg message (err %d)", err);
 	}
@@ -339,7 +365,7 @@ static int beacon_send(struct bt_conn *conn, struct bt_mesh_subnet *sub)
 	net_buf_simple_reserve(&buf, 1);
 	bt_mesh_beacon_create(sub, &buf);
 
-	return proxy_segment_and_send(conn, BT_MESH_PROXY_BEACON, &buf);
+	return proxy_segment_and_send(conn, BT_MESH_PROXY_BEACON, &buf, NULL);
 }
 
 static int send_beacon_cb(struct bt_mesh_subnet *sub, void *cb_data)
@@ -880,12 +906,22 @@ static bool client_filter_match(struct bt_mesh_proxy_client *client,
 	return false;
 }
 
-bool bt_mesh_proxy_relay(struct net_buf_simple *buf, uint16_t dst)
+bool bt_mesh_proxy_relay(struct net_buf_simple *buf, uint16_t dst,
+			 const struct bt_mesh_send_cb *cb, void *user_data)
 {
+	struct bt_mesh_proxy *proxy = NULL;
 	bool relayed = false;
 	int i;
 
 	BT_DBG("%u bytes to dst 0x%04x", buf->len, dst);
+
+	if (cb) {
+		proxy = proxy_alloc(cb, user_data);
+		if (!proxy) {
+			BT_ERR("No Proxy Object available");
+			return -ENOBUFS;
+		}
+	}
 
 	for (i = 0; i < ARRAY_SIZE(clients); i++) {
 		struct bt_mesh_proxy_client *client = &clients[i];
@@ -899,13 +935,20 @@ bool bt_mesh_proxy_relay(struct net_buf_simple *buf, uint16_t dst)
 			continue;
 		}
 
+		if (client->filter_type == PROV) {
+			BT_ERR("Invalid PDU type for Proxy Client");
+			return -EINVAL;
+		}
+
 		/* Proxy PDU sending modifies the original buffer,
 		 * so we need to make a copy.
 		 */
 		net_buf_simple_reserve(&msg, 1);
 		net_buf_simple_add_mem(&msg, buf->data, buf->len);
 
-		bt_mesh_proxy_send(client->conn, BT_MESH_PROXY_NET_PDU, &msg);
+		proxy_segment_and_send(client->conn, BT_MESH_PROXY_NET_PDU,
+					&msg, proxy);
+
 		relayed = true;
 	}
 
@@ -916,25 +959,26 @@ bool bt_mesh_proxy_relay(struct net_buf_simple *buf, uint16_t dst)
 
 static void notify_complete(struct bt_conn *conn, void *user_data)
 {
-	sys_snode_t *n;
+	struct bt_mesh_proxy *proxy = user_data;
 
-	if (atomic_dec(&pending_notifications) > 1) {
+	if (!proxy || atomic_dec(&proxy->ref) > 1) {
 		return;
 	}
 
 	BT_DBG("");
 
-	while ((n = sys_slist_get(&idle_waiters))) {
-		CONTAINER_OF(n, struct bt_mesh_proxy_idle_cb, n)->cb();
+	if (proxy->cb && proxy->cb->end) {
+		proxy->cb->end(0, proxy->cb_data);
 	}
 }
 
 static int proxy_send(struct bt_conn *conn, const void *data,
-		      uint16_t len)
+		      uint16_t len, struct bt_mesh_proxy *proxy)
 {
 	struct bt_gatt_notify_params params = {
 		.data = data,
 		.len = len,
+		.user_data = proxy,
 		.func = notify_complete,
 	};
 	int err;
@@ -953,19 +997,26 @@ static int proxy_send(struct bt_conn *conn, const void *data,
 #endif
 
 	if (!params.attr) {
-		return 0;
+		return -ENOENT;
 	}
 
 	err = bt_gatt_notify_cb(conn, &params);
-	if (!err) {
-		atomic_inc(&pending_notifications);
+	if (!err && proxy) {
+		if (atomic_inc(&proxy->ref)) {
+			return 0;
+		}
+
+		if (proxy->cb && proxy->cb->start) {
+			proxy->cb->start(0, 0, proxy->cb_data);
+		}
 	}
 
 	return err;
 }
 
 static int proxy_segment_and_send(struct bt_conn *conn, uint8_t type,
-				  struct net_buf_simple *msg)
+				  struct net_buf_simple *msg,
+				  struct bt_mesh_proxy *proxy)
 {
 	uint16_t mtu;
 
@@ -976,44 +1027,53 @@ static int proxy_segment_and_send(struct bt_conn *conn, uint8_t type,
 	mtu = bt_gatt_get_mtu(conn) - 3;
 	if (mtu > msg->len) {
 		net_buf_simple_push_u8(msg, PDU_HDR(SAR_COMPLETE, type));
-		return proxy_send(conn, msg->data, msg->len);
+		return proxy_send(conn, msg->data, msg->len, proxy);
 	}
 
 	net_buf_simple_push_u8(msg, PDU_HDR(SAR_FIRST, type));
-	proxy_send(conn, msg->data, mtu);
+	proxy_send(conn, msg->data, mtu, proxy);
 	net_buf_simple_pull(msg, mtu);
 
 	while (msg->len) {
 		if (msg->len + 1 < mtu) {
 			net_buf_simple_push_u8(msg, PDU_HDR(SAR_LAST, type));
-			proxy_send(conn, msg->data, msg->len);
+			proxy_send(conn, msg->data, msg->len, proxy);
 			break;
 		}
 
 		net_buf_simple_push_u8(msg, PDU_HDR(SAR_CONT, type));
-		proxy_send(conn, msg->data, mtu);
+		proxy_send(conn, msg->data, mtu, proxy);
 		net_buf_simple_pull(msg, mtu);
 	}
 
 	return 0;
 }
 
-int bt_mesh_proxy_send(struct bt_conn *conn, uint8_t type,
-		       struct net_buf_simple *msg)
+int bt_mesh_pb_gatt_send(struct bt_conn *conn, struct net_buf_simple *msg,
+			 const struct bt_mesh_send_cb *cb, void *user_data)
 {
+	struct bt_mesh_proxy *proxy = NULL;
 	struct bt_mesh_proxy_client *client = find_client(conn);
+
+	if (cb) {
+		proxy = proxy_alloc(cb, user_data);
+		if (!proxy) {
+			BT_ERR("No Proxy Object available");
+			return -ENOBUFS;
+		}
+	}
 
 	if (!client) {
 		BT_ERR("No Proxy Client found");
 		return -ENOTCONN;
 	}
 
-	if ((client->filter_type == PROV) != (type == BT_MESH_PROXY_PROV)) {
+	if (client->filter_type != PROV) {
 		BT_ERR("Invalid PDU type for Proxy Client");
 		return -EINVAL;
 	}
 
-	return proxy_segment_and_send(conn, type, msg);
+	return proxy_segment_and_send(conn, BT_MESH_PROXY_PROV, msg, proxy);
 }
 
 #if defined(CONFIG_BT_MESH_PB_GATT)
@@ -1362,14 +1422,4 @@ int bt_mesh_proxy_init(void)
 	bt_conn_cb_register(&conn_callbacks);
 
 	return 0;
-}
-
-void bt_mesh_proxy_on_idle(struct bt_mesh_proxy_idle_cb *cb)
-{
-	if (!atomic_get(&pending_notifications)) {
-		cb->cb();
-		return;
-	}
-
-	sys_slist_append(&idle_waiters, &cb->n);
 }

subsys/bluetooth/mesh/proxy.h
@@ -12,13 +12,8 @@
 #define BT_MESH_PROXY_PROV      0x03
 
 
-struct bt_mesh_proxy_idle_cb {
-	sys_snode_t n;
-	void (*cb)(void);
-};
-
-int bt_mesh_proxy_send(struct bt_conn *conn, uint8_t type,
-		       struct net_buf_simple *msg);
+int bt_mesh_pb_gatt_send(struct bt_conn *conn, struct net_buf_simple *msg,
+			 const struct bt_mesh_send_cb *cb, void *user_data);
 
 int bt_mesh_proxy_prov_enable(void);
 int bt_mesh_proxy_prov_disable(bool disconnect);
@@ -36,8 +31,8 @@ int bt_mesh_proxy_adv_start(void);
 void bt_mesh_proxy_identity_start(struct bt_mesh_subnet *sub);
 void bt_mesh_proxy_identity_stop(struct bt_mesh_subnet *sub);
 
-bool bt_mesh_proxy_relay(struct net_buf_simple *buf, uint16_t dst);
+bool bt_mesh_proxy_relay(struct net_buf_simple *buf, uint16_t dst,
+			 const struct bt_mesh_send_cb *cb, void *user_data);
 void bt_mesh_proxy_addr_add(struct net_buf_simple *buf, uint16_t addr);
 
 int bt_mesh_proxy_init(void);
-void bt_mesh_proxy_on_idle(struct bt_mesh_proxy_idle_cb *cb);