44bf4a124b3f318586251f7207fbfad4da1869c8
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2021-01-05 12:07:38 +0100

    cavs: (cosmetic) remove redundant LPRAM_* macros
    LPRAM_BASE and LPRAM_SIZE are duplicates of LP_SRAM_BASE and
    LP_SRAM_SIZE respectively. Remove them and use LP_SRAM_*
    consistently everywhere.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- soc/xtensa/intel_adsp/cavs_v20/include/soc/memory.h
+++ soc/xtensa/intel_adsp/cavs_v20/include/soc/memory.h
@@ -89,10 +89,6 @@
 /* size of the Interrupt Descriptor Table (IDT) */
 #define IDT_SIZE				0x2000
 
-/* low power ram where DMA buffers are typically placed */
-#define LPRAM_BASE (DT_REG_ADDR(DT_NODELABEL(sram1)))
-#define LPRAM_SIZE (DT_REG_SIZE(DT_NODELABEL(sram1)))
-
 /* bootloader */
 
 #define HP_SRAM_BASE	0xbe000000
@@ -220,5 +216,9 @@
 #define HOST_PAGE_SIZE		4096
 
 #define SRAM_BANK_SIZE                  (64 * 1024)
+
+/* low power ram where DMA buffers are typically placed */
+#define LP_SRAM_BASE (DT_REG_ADDR(DT_NODELABEL(sram1)))
+#define LP_SRAM_SIZE (DT_REG_SIZE(DT_NODELABEL(sram1)))
 
 #endif /* __INC_MEMORY_H */
