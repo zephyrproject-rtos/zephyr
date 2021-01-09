8dd9244795d37193aa0c34b3ba98584c66fb8aae
Rafael Dias Menezes <rdmeneze@gmail.com>
2021-01-09 14:18:08 +0100

    boards: arm: Add support for SiLabs EFM32PG1B SLSTK3401A board
    This commit adds support for Silicon Labs SLSTK3401A Pearl Gecko board.
    
    Signed-off-by: Rafael Dias Menezes <rdmeneze@gmail.com>

---
+++ boards/arm/efm32pg_stk3401a/board.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020 Rafael Dias Menezes <rdmeneze@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+#include "board.h"
+#include <drivers/gpio.h>
+#include <sys/printk.h>
+
+static int efm32pg_stk3401a_init(const struct device *dev)
+{
+	const struct device *bce_dev; /* Board Controller Enable Gpio Device */
+
+	ARG_UNUSED(dev);
+
+	/* Enable the board controller to be able to use the serial port */
+	bce_dev = device_get_binding(BC_ENABLE_GPIO_NAME);
+
+	if (!bce_dev) {
+		printk("Board controller gpio port was not found!\n");
+		return -ENODEV;
+	}
+
+	gpio_pin_configure(bce_dev, BC_ENABLE_GPIO_PIN, GPIO_OUTPUT_HIGH);
+
+	return 0;
+}
+
+/* needs to be done after GPIO driver init */
+SYS_INIT(efm32pg_stk3401a_init, PRE_KERNEL_1, CONFIG_BOARD_INIT_PRIORITY);
