2c904b379bf7e1f8301be37520e86d843672ff1e
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2021-01-12 14:03:04 +0100

    drivers/interrupt_controller: Adding QI support in VT-D
    Looks like it's mandatory to invalidate the Interrupt Entry Cache in
    VT-D and the only way to do so is to enable Queued Interface.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- drivers/interrupt_controller/intc_intel_vtd.h
+++ drivers/interrupt_controller/intc_intel_vtd.h
@@ -52,8 +52,59 @@ struct vtd_irte {
 #define IRTE_NUM 256
 #define IRTA_SIZE 7  /* size = 2^(X+1) where IRTA_SIZE is X 2^8 = 256 */
 
+#define QI_NUM 256 /* Which is the minimal number we can set for the queue */
+#define QI_SIZE 0 /* size = 2^(X+8) where QI_SIZE is X: 2^8 = 256 */
+#define QI_WIDTH 128
+
+struct qi_descriptor {
+	uint64_t low;
+	uint64_t high;
+};
+
+#define QI_TYPE_IEC 0x4UL
+
+union qi_iec_descriptor {
+	struct qi_descriptor desc;
+
+	struct iec_bits {
+		uint64_t type		: 4;
+		uint64_t granularity	: 1;
+		uint64_t _reserved_0	: 4;
+		uint64_t zero		: 3;
+		uint64_t _reserved_1	: 15;
+		uint64_t index_mask	: 5;
+		uint64_t interrupt_index: 16;
+		uint64_t _reserved_2	: 16;
+		uint64_t reserved;
+	} iec __packed;
+};
+
+#define QI_TYPE_WAIT 0x5UL
+
+union qi_wait_descriptor {
+	struct qi_descriptor desc;
+
+	struct wait_bits {
+		uint64_t type		: 4;
+		uint64_t interrupt_flag	: 1;
+		uint64_t status_write	: 1;
+		uint64_t fence_flag	: 1;
+		uint64_t page_req_drain	: 1;
+		uint64_t _reserved_0	: 1;
+		uint64_t zero		: 3;
+		uint64_t _reserved_1	: 20;
+		uint64_t status_data	: 32;
+		uint64_t reserved	: 2;
+		uint64_t address	: 62;
+	} wait __packed;
+};
+
+#define QI_WAIT_STATUS_INCOMPLETE 0x0UL
+#define QI_WAIT_STATUS_COMPLETE 0x1UL
+
 struct vtd_ictl_data {
 	struct vtd_irte irte[IRTE_NUM];
+	struct qi_descriptor qi[QI_NUM] __aligned(0x1000);
 	int irqs[IRTE_NUM];
 	int vectors[IRTE_NUM];
 	bool msi[IRTE_NUM];
@@ -61,6 +112,7 @@ struct vtd_ictl_data {
 	unsigned int fault_irq;
 	uintptr_t fault_record_reg;
 	uint16_t fault_record_num;
+	uint16_t qi_tail;
 	uint8_t fault_vector;
 };
 
