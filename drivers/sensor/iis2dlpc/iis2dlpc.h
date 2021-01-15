45d0508bdf90a8eb6d1e93d5bae6149f7b6db701
Armando Visconti <armando.visconti@st.com>
2021-01-15 17:42:43 +0100

    drivers/sensor: iis2dlpc: Add multi-instance support
    Make this driver multi-instance and use the new API.
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

--- drivers/sensor/iis2dlpc/iis2dlpc.h
+++ drivers/sensor/iis2dlpc/iis2dlpc.h
@@ -11,11 +11,18 @@
 #ifndef ZEPHYR_DRIVERS_SENSOR_IIS2DLPC_IIS2DLPC_H_
 #define ZEPHYR_DRIVERS_SENSOR_IIS2DLPC_IIS2DLPC_H_
 
-#include <drivers/spi.h>
 #include <drivers/gpio.h>
 #include <sys/util.h>
 #include <drivers/sensor.h>
 #include "iis2dlpc_reg.h"
+
+#if DT_ANY_INST_ON_BUS_STATUS_OKAY(spi)
+#include <drivers/spi.h>
+#endif /* DT_ANY_INST_ON_BUS_STATUS_OKAY(spi) */
+
+#if DT_ANY_INST_ON_BUS_STATUS_OKAY(i2c)
+#include <drivers/i2c.h>
+#endif /* DT_ANY_INST_ON_BUS_STATUS_OKAY(i2c) */
 
 union axis3bit16_t {
 	int16_t i16bit[3];
@@ -46,22 +53,41 @@ union axis3bit16_t {
 #define IIS2DLPC_SHIFT_PM1		4
 #define IIS2DLPC_SHIFT_PMOTHER		2
 
+#if DT_ANY_INST_ON_BUS_STATUS_OKAY(spi)
+struct iis2dlpc_spi_cfg {
+	struct spi_config spi_conf;
+	const char *cs_gpios_label;
+};
+#endif /* DT_ANY_INST_ON_BUS_STATUS_OKAY(spi) */
+
+union iis2dlpc_bus_cfg {
+#if DT_ANY_INST_ON_BUS_STATUS_OKAY(i2c)
+	uint16_t i2c_slv_addr;
+#endif
+
+#if DT_ANY_INST_ON_BUS_STATUS_OKAY(spi)
+	const struct iis2dlpc_spi_cfg *spi_cfg;
+#endif /* DT_ANY_INST_ON_BUS_STATUS_OKAY(spi) */
+};
+
 /**
- * struct iis2dlpc_device_config - iis2dlpc hw configuration
+ * struct iis2dlpc_dev_config - iis2dlpc hw configuration
  * @bus_name: Pointer to bus master identifier.
  * @pm: Power mode (lis2dh_powermode).
- * @int_gpio_port: Pointer to GPIO PORT identifier.
- * @int_gpio_pin: GPIO pin number connecter to sensor int pin.
+ * @irq_dev_name: Pointer to GPIO PORT identifier.
+ * @irq_pin: GPIO pin number connecter to sensor int pin.
  * @drdy_int: Sensor drdy int (int1/int2).
  */
-struct iis2dlpc_device_config {
+struct iis2dlpc_dev_config {
 	const char *bus_name;
+	int (*bus_init)(const struct device *dev);
+	const union iis2dlpc_bus_cfg bus_cfg;
 	iis2dlpc_mode_t pm;
 	uint8_t range;
 #ifdef CONFIG_IIS2DLPC_TRIGGER
-	const char *int_gpio_port;
-	uint8_t int_gpio_pin;
-	uint8_t int_gpio_flags;
+	const char *irq_dev_name;
+	gpio_pin_t irq_pin;
+	gpio_flags_t irq_flags;
 	uint8_t drdy_int;
 #ifdef CONFIG_IIS2DLPC_TAP
 	uint8_t tap_mode;
@@ -75,6 +101,7 @@ struct iis2dlpc_device_config {
 
 /* sensor data */
 struct iis2dlpc_data {
+	const struct device *dev;
 	const struct device *bus;
 	int16_t acc[3];
 
@@ -82,8 +109,15 @@ struct iis2dlpc_data {
 	uint16_t gain;
 
 	stmdev_ctx_t *ctx;
+
+#if DT_ANY_INST_ON_BUS_STATUS_OKAY(i2c)
+	stmdev_ctx_t ctx_i2c;
+#endif
+#if DT_ANY_INST_ON_BUS_STATUS_OKAY(spi)
+	stmdev_ctx_t ctx_spi;
+#endif
+
 #ifdef CONFIG_IIS2DLPC_TRIGGER
-	const struct device *dev;
 	const struct device *gpio;
 	uint8_t gpio_pin;
 	struct gpio_callback gpio_cb;
@@ -100,9 +134,9 @@ struct iis2dlpc_data {
 	struct k_work work;
 #endif /* CONFIG_IIS2DLPC_TRIGGER_GLOBAL_THREAD */
 #endif /* CONFIG_IIS2DLPC_TRIGGER */
-#if DT_INST_SPI_DEV_HAS_CS_GPIOS(0)
+#if DT_ANY_INST_ON_BUS_STATUS_OKAY(spi)
 	struct spi_cs_control cs_ctrl;
-#endif
+#endif /* DT_ANY_INST_ON_BUS_STATUS_OKAY(spi) */
 };
 
 int iis2dlpc_i2c_init(const struct device *dev);
