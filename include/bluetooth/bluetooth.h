90424efeb9409e00997ef22eaf2fbd7f682c50d8
Xeenych <xeenych@gmail.com>
2021-01-18 16:27:57 +0300

    bluetooth: update comment to LE Advertising Parameters
    Update description to maximum and minimum values of interval_min and interval_max fields of bt_le_adv_param struct according to Bluetooth Core Specification 5.2
     
    Signed-off-by: <Tsvetkov Alexey> <xeenych@gmail.com>

--- include/bluetooth/bluetooth.h
+++ include/bluetooth/bluetooth.h
@@ -530,7 +530,7 @@ struct bt_le_adv_param {
 	/** Maximum Advertising Interval (N * 0.625 milliseconds)
 	 * Minimum Advertising Interval shall be less than or equal to the
 	 * Maximum Advertising Interval. The Minimum Advertising Interval and
-         * Maximum Advertising Interval should not be the same value (as stated
+	 * Maximum Advertising Interval should not be the same value (as stated
 	 * in Bluetooth Core Spec 5.2, section 7.8.5)
 	 * Range: 0x0020 to 0x4000
 	 */
