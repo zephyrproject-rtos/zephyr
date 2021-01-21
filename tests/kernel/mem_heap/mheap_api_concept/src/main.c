c041fc5bd79153a7970a6abff51288f12b350280
Ying ming <mingx.ying@intel.com>
2021-01-21 09:49:58 +0800

    test: mempool : improve coverage to mempool
    Add some testcase to improve coverage of mempool.c
    
    Signed-off-by: Ying ming <mingx.ying@intel.com>

--- tests/kernel/mem_heap/mheap_api_concept/src/main.c
+++ tests/kernel/mem_heap/mheap_api_concept/src/main.c
@@ -6,12 +6,13 @@
 
 #include <ztest.h>
 extern void test_mheap_malloc_free(void);
-extern void test_mheap_malloc_align4(void);
-extern void test_mheap_min_block_size(void);
-extern void test_mheap_block_desc(void);
 extern void test_mheap_calloc(void);
-extern void test_mheap_block_release(void);
+extern void test_mheap_malloc_align4(void);
 extern void test_k_aligned_alloc(void);
+extern void test_sys_heap_mem_pool_assign(void);
+extern void test_malloc_in_isr(void);
+extern void test_malloc_in_thread(void);
+
 
 /**
  * @brief Heap tests
@@ -29,6 +30,9 @@ void test_main(void)
 			 ztest_unit_test(test_mheap_malloc_free),
 			 ztest_unit_test(test_mheap_calloc),
 			 ztest_unit_test(test_mheap_malloc_align4),
-			 ztest_unit_test(test_k_aligned_alloc));
+			 ztest_unit_test(test_k_aligned_alloc),
+			 ztest_unit_test(test_sys_heap_mem_pool_assign),
+			 ztest_unit_test(test_malloc_in_isr),
+			 ztest_unit_test(test_malloc_in_thread));
 	ztest_run_test_suite(mheap_api);
 }
--- tests/kernel/mem_heap/mheap_api_concept/src/test_mheap_api.c
+++ tests/kernel/mem_heap/mheap_api_concept/src/test_mheap_api.c
@@ -5,7 +5,38 @@
  */
 
 #include <ztest.h>
+#include <kernel_internal.h>
+#include <irq_offload.h>
 #include "test_mheap.h"
+
+#define STACK_SIZE (512 + CONFIG_TEST_EXTRA_STACKSIZE)
+#define OVERFLOW_SIZE    SIZE_MAX
+
+K_SEM_DEFINE(thread_sem, 0, 1);
+K_THREAD_STACK_DEFINE(tstack, STACK_SIZE);
+struct k_thread tdata;
+
+static void tIsr_malloc_and_free(void *data)
+{
+	ARG_UNUSED(data);
+	void *ptr;
+
+	ptr = (char *)z_thread_malloc(BLK_SIZE_MIN);
+	zassert_not_null(ptr, "bytes allocation failed from system pool");
+	k_free(ptr);
+}
+
+static void thread_entry(void *p1, void *p2, void *p3)
+{
+	void *ptr;
+
+	k_current_get()->resource_pool = NULL;
+
+	ptr = (char *)z_thread_malloc(BLK_SIZE_MIN);
+	zassert_is_null(ptr, "bytes allocation failed from system pool");
+
+	k_sem_give(&thread_sem);
+}
 
 /*test cases*/
 
@@ -52,6 +83,10 @@ void test_mheap_malloc_free(void)
 	}
 	/** TESTPOINT: If ptr is NULL, no operation is performed.*/
 	k_free(NULL);
+
+	/** TESTPOINT: Return NULL if fail.*/
+	block_fail = k_malloc(OVERFLOW_SIZE);
+	zassert_is_null(block_fail, NULL);
 }
 
 #define NMEMB   8
@@ -63,7 +98,9 @@ void test_mheap_malloc_free(void)
  *
  * @ingroup kernel_heap_tests
  *
- * @details The test validates k_calloc() API. The 8 blocks of memory of
+ * @details The test validates k_calloc() API. When requesting a
+ * huge size of space or a space larger than heap memory,
+ * the API will return NULL. The 8 blocks of memory of
  * size 16 bytes are allocated by k_calloc() API. When allocated using
  * k_calloc() the memory buffers have to be zeroed. Check is done, if the
  * blocks are memset to 0 and read/write is allowed. The test is then
@@ -75,8 +112,15 @@ void test_mheap_calloc(void)
 {
 	char *mem;
 
-	mem = k_calloc(NMEMB, SIZE);
+	/* Requesting a huge size to validate overflow */
+	mem = k_calloc(NMEMB, OVERFLOW_SIZE);
+	zassert_is_null(mem, "calloc operation failed");
 
+	/* Requesting a space large than heap memory lead to failure */
+	mem = k_calloc(NMEMB * 3, SIZE);
+	zassert_is_null(mem, "calloc operation failed");
+
+	mem = k_calloc(NMEMB, SIZE);
 	zassert_not_null(mem, "calloc operation failed");
 
 	/* Memory should be zeroed and not crash us if we read/write to it */
@@ -117,4 +161,66 @@ void test_k_aligned_alloc(void)
 	zassert_equal(0, (uintptr_t)r % 16,
 		"%p not 16-byte-aligned", r);
 	k_free(r);
+}
+
+/**
+ * @brief Validate allocation and free from system heap memory pool.
+
+ * @details Set heap memory as resource pool. It will success when alloc
+ * a block of memory smaller than the pool and will fail when alloc
+ * a block of memory larger than the pool.
+ *
+ * @ingroup kernel_heap_tests
+ *
+ * @see k_thread_system_pool_assign(), z_thread_malloc(), k_free()
+ */
+void test_sys_heap_mem_pool_assign(void)
+{
+	void *ptr;
+
+	k_thread_system_pool_assign(k_current_get());
+	ptr = (char *)z_thread_malloc(BLK_SIZE_MIN/2);
+	zassert_not_null(ptr, "bytes allocation failed from system pool");
+	k_free(ptr);
+
+	zassert_is_null((char *)z_thread_malloc(BLK_SIZE_MAX * 2),
+						"overflow check failed");
+}
+
+/**
+ * @brief Validate allocation and free from system heap memory pool
+ * in isr context.
+ *
+ * @details When in isr context, the kernel will successfully alloc a block of
+ * memory because in this situation, the kernel will assign the heap memory
+ * as resource pool.
+ *
+ * @ingroup kernel_heap_tests
+ *
+ * @see z_thread_malloc(), k_free()
+ */
+void test_malloc_in_isr(void)
+{
+	irq_offload((irq_offload_routine_t)tIsr_malloc_and_free, NULL);
+}
+
+/**
+ * @brief Validate allocation and free failure when thread's resource pool
+ * is not assigned.
+ *
+ * @details When a thread's resource pool is not assigned, alloc memory will fail.
+ *
+ * @ingroup kernel_heap_tests
+ *
+ * @see z_thread_malloc()
+ */
+void test_malloc_in_thread(void)
+{
+	k_tid_t tid = k_thread_create(&tdata, tstack, STACK_SIZE,
+			      thread_entry, NULL, NULL, NULL,
+			      0, 0, K_NO_WAIT);
+
+	k_sem_take(&thread_sem, K_FOREVER);
+
+	k_thread_abort(tid);
 }
