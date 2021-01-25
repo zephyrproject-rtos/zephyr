19c28c8128e6eb5d31a909883657069a868b37a6
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2021-01-25 17:19:20 +0200

    samples: net: syslog: Add info how to manually start net backend
    If CONFIG_LOG_BACKEND_NET_AUTOSTART is disabled, then the syslog
    network backend is not started automatically. This option is
    enabled by default but it might be that application needs to
    start the backend manually if the network is slow to start or
    some special routes needs to be set by the application.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

--- samples/net/syslog_net/src/main.c
+++ samples/net/syslog_net/src/main.c
@@ -9,8 +9,7 @@ LOG_MODULE_REGISTER(net_syslog, LOG_LEVEL_DBG);
 
 #include <zephyr.h>
 
-#include <net/net_core.h>
-#include <net/net_pkt.h>
+#include <logging/log_backend.h>
 
 #include <stdlib.h>
 
@@ -18,11 +17,31 @@ BUILD_ASSERT(IS_ENABLED(CONFIG_LOG_BACKEND_NET), "syslog backend not enabled");
 
 #define SLEEP_BETWEEN_PRINTS 3
 
+#if IS_ENABLED(CONFIG_LOG_BACKEND_NET)
+extern const struct log_backend *log_backend_net_get(void);
+#endif
+
 void main(void)
 {
 	int i, count, sleep;
 
 	LOG_DBG("Starting");
+
+	if (!IS_ENABLED(CONFIG_LOG_BACKEND_NET_AUTOSTART)) {
+		/* Example how to start the backend if autostart is disabled.
+		 * This is useful if the application needs to wait the network
+		 * to be fully up before the syslog-net is able to work.
+		 */
+		const struct log_backend *backend = log_backend_net_get();
+
+		if (!log_backend_is_active(backend)) {
+			if (backend->api->init != NULL) {
+				backend->api->init();
+			}
+
+			log_backend_activate(backend, NULL);
+		}
+	}
 
 	if (CONFIG_NET_SAMPLE_SEND_ITERATIONS) {
 		count = CONFIG_NET_SAMPLE_SEND_ITERATIONS;
