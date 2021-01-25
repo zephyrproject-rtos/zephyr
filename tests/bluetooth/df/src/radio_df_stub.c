a8c18ff5f4ace13fd8a296112d6521cbd7b19a7c
Piotr Pryga <piotr.pryga@nordicsemi.no>
2021-01-25 07:15:08 -0800

    tests: Bluetooth: df: Fix build error, missing antenna config in DTS
    The DF tests are implemented to be executed with nrf52_bsim platform.
    The nrf52_bsim platform does not include Direction Finding Extenstion.
    Due to that, radio_df.c compilation failed with error about missing
    antenna configuration in DTS.
    
    To solve the problem, I've changed nrf.cmake to include radio_df.c
    file when CONFIG_BT_CTLR_DF is defined and CONFIG_SOC_SERIES_BSIM_NRFXX
    is not definded.
    
    Thanks to that any other platform is not affected. The file will not
    build if there is no appropriate configuration or there are missing
    features in a hardwared.
    
    Unit tests have provided stub imlpementation or radio functionality.
    If nrf52_bsim has implemented Direction Finding Extension, the
    DF unit tests code will stil work and will not require additional
    changes. Also content of the file is not affected by contional
    compilation entries.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

---
+++ tests/bluetooth/df/src/radio_df_stub.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <stdint.h>
+
+/* Stub functitons that does nothing. Just to avoid liker complains. */
+void radio_df_ant_configure(void)
+{
+
+}
+
+uint8_t radio_df_ant_num_get(void)
+{
+	return 0;
+}
