30ed82526b09c733bcd8401eda89ec7a8e267317
Bob Recny <bob.recny@u-blox.com>
2021-01-26 17:47:36 -0800

    boards: add ubx_bmd340eval_nrf52840
    Add support for u-blox AG EVK BMD-340-EVAL
    On branch board_add_ubx_bmd340eval_nrf52840
    Changes to be committed:
    	new file:   boards/arm/ubx_bmd340eval_nrf52840/Kconfig
    	new file:   boards/arm/ubx_bmd340eval_nrf52840/Kconfig.board
    	new file:   boards/arm/ubx_bmd340eval_nrf52840/Kconfig.defconfig
    	new file:   boards/arm/ubx_bmd340eval_nrf52840/board.cmake
    	new file:   boards/arm/ubx_bmd340eval_nrf52840/doc/img/BMD-34-38-EVAL.png
    	new file:   boards/arm/ubx_bmd340eval_nrf52840/doc/img/bmd-340-eval_pin_out.png
    	new file:   boards/arm/ubx_bmd340eval_nrf52840/doc/index.rst
    	new file:   boards/arm/ubx_bmd340eval_nrf52840/ubx_bmd340eval_nrf52840.dts
    	new file:   boards/arm/ubx_bmd340eval_nrf52840/ubx_bmd340eval_nrf52840.yaml
    	new file:   boards/arm/ubx_bmd340eval_nrf52840/ubx_bmd340eval_nrf52840_defconfig
    Similar to Nordic Semiconductor nrf52840dk_nrf52840
    Tested with samples/basic/blinky
    
    Signed-off-by: Bob Recny <bob.recny@u-blox.com>

---
+++ boards/arm/ubx_bmd340eval_nrf52840/board.cmake
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(nrfjprog "--nrf-family=NRF52")
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
+board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
