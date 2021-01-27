70ab8feb8f2c63006298806892f175bef603a6ed
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2021-01-27 12:56:07 +0100

    logging: Add v2 support to RTT backend
    Add support for logging v2 to RTT backend.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

--- subsys/logging/log_backend_rtt.c
+++ subsys/logging/log_backend_rtt.c
@@ -295,11 +295,20 @@ static void sync_hexdump(const struct log_backend *const backend,
 				     timestamp, metadata, data, length);
 }
 
+static void process(const struct log_backend *const backend,
+		union log_msg2_generic *msg)
+{
+	uint32_t flags = log_backend_std_get_flags();
+
+	log_output_msg2_process(&log_output_rtt, &msg->log, flags);
+}
+
 const struct log_backend_api log_backend_rtt_api = {
-	.put = IS_ENABLED(CONFIG_LOG_IMMEDIATE) ? NULL : put,
-	.put_sync_string = IS_ENABLED(CONFIG_LOG_IMMEDIATE) ?
+	.process = IS_ENABLED(CONFIG_LOG2) ? process : NULL,
+	.put = IS_ENABLED(CONFIG_LOG_MODE_DEFERRED) ? put : NULL,
+	.put_sync_string = IS_ENABLED(CONFIG_LOG_MODE_IMMEDIATE) ?
 			sync_string : NULL,
-	.put_sync_hexdump = IS_ENABLED(CONFIG_LOG_IMMEDIATE) ?
+	.put_sync_hexdump = IS_ENABLED(CONFIG_LOG_MODE_IMMEDIATE) ?
 			sync_hexdump : NULL,
 	.panic = panic,
 	.init = log_backend_rtt_init,
