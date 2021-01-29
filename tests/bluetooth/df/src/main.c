0eb9c87b8d40d1cfa2dd578d438de20112bb0256
Piotr Pryga <piotr.pryga@nordicsemi.no>
2021-01-28 22:59:07 -0800

    tests: Bluetooth: df: add UT for enable CTE for periodic advertising
    Adds unit tests for verification of enable/disable CTE for periodic
    advertiging in controller.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

--- tests/bluetooth/df/src/main.c
+++ tests/bluetooth/df/src/main.c
@@ -8,6 +8,7 @@
 
 #include "common.h"
 #include "test_set_cl_cte_tx_params.h"
+#include "test_set_cl_cte_tx_enable.h"
 
 
 /*test case main entry*/
@@ -16,4 +17,6 @@ void test_main(void)
 	common_setup();
 	common_create_adv_set();
 	run_set_cl_cte_tx_params_tests();
+	common_delete_adv_set();
+	run_set_cl_cte_tx_enable_tests();
 }
