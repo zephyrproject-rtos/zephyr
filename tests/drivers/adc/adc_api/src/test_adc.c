7f50dd0a9a26e6d010e80d3411d35992a133b786
NavinSankar Velliangiri <navin@linumiz.com>
2021-01-29 14:13:13 +0530

    tests: drivers: adc: adc_api: Enable ADC test support
    Enable ADC test support for bmd_345_eval board
    
    Signed-off-by: NavinSankar Velliangiri <navin@linumiz.com>

--- tests/drivers/adc/adc_api/src/test_adc.c
+++ tests/drivers/adc/adc_api/src/test_adc.c
@@ -48,7 +48,8 @@
 	defined(CONFIG_BOARD_RUUVI_RUUVITAG) || \
 	defined(CONFIG_BOARD_BT510) || \
 	defined(CONFIG_BOARD_PINNACLE_100_DVK) || \
-	defined(CONFIG_BOARD_ARDUINO_NANO_33_BLE)
+	defined(CONFIG_BOARD_ARDUINO_NANO_33_BLE) || \
+	defined(CONFIG_BOARD_BMD_345_EVAL)
 
 #include <hal/nrf_saadc.h>
 #define ADC_DEVICE_NAME		DT_LABEL(DT_INST(0, nordic_nrf_saadc))
