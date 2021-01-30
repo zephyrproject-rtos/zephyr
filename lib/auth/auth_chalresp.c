70087d749c684de87fe8ba3910f703ba2d62acb8
Dean Gereaux <deang@goldenbits.com>
2021-01-29 16:17:54 -0800

    auth: Fixed problem with running unit test
    - Added back sys_rand_get() for CI tests.  This should not be used for
      production code, but needed for CI tests.
    - Updated serial sample YAML files to used ci.prf.conf file for
      CI purposes.
    
    Signed-off-by: Dean Gereaux <deang@goldenbits.com>

--- lib/auth/auth_chalresp.c
+++ lib/auth/auth_chalresp.c
@@ -541,8 +541,17 @@ static int auth_chalresp_client(struct authenticate_conn *auth_conn)
 	struct auth_chalresp_result server_result;
 	enum auth_status status;
 
-	/* generate random number as challenge */
+#if defined(CONFIG_ENTROPY_HAS_DRIVER)
+	/* Generate random number as challenge using a cryptographically secure random
+	 * number generator. */
 	sys_csrand_get(random_chal, sizeof(random_chal));
+#else
+	/**
+	 * WARNING!!  This rand function should only be used for testing. It is
+	 * not recommend for production use.
+	 */
+    sys_rand_get(random_chal, sizeof(random_chal));
+#endif
 
 	if (!auth_client_send_challenge(auth_conn, random_chal)) {
 		auth_lib_set_status(auth_conn, AUTH_STATUS_FAILED);
@@ -608,8 +617,18 @@ static int auth_chalresp_server(struct authenticate_conn *auth_conn)
 	enum auth_status status;
 	uint8_t random_chal[AUTH_CHALLENGE_LEN];
 
-	/* generate random number as challenge */
+
+#if defined(CONFIG_ENTROPY_HAS_DRIVER)
+    /* Generate random number as challenge using a cryptographically secure random
+	 * number generator. */
 	sys_csrand_get(random_chal, sizeof(random_chal));
+#else
+    /**
+     * WARNING!!  This rand function should only be used for testing. It is
+     * not recommend for production use.
+     */
+    sys_rand_get(random_chal, sizeof(random_chal));
+#endif
 
 	/* Wait for challenge from the Central */
 	if (!auth_server_recv_challenge(auth_conn, random_chal)) {
