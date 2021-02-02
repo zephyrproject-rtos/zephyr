752798665d92a4fba3f10a78f02e4506f55f5d77
Alexandre Bourdiol <alexandre.bourdiol@st.com>
2021-02-02 15:02:37 +0100

    soc: arm: st_stm32: add STM32WL support
    Add STM32WL soc serie
    
    Signed-off-by: Alexandre Bourdiol <alexandre.bourdiol@st.com>

---
+++ soc/arm/st_stm32/stm32wl/soc.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2020 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file SoC configuration macros for the STM32WL family processors.
+ *
+ * Based on reference manual:
+ *   STM32WLxx advanced ARM ® -based 32-bit MCUs
+ *
+ * Chapter 2.6: Memory organization
+ */
+
+
+#ifndef _STM32WL_SOC_H_
+#define _STM32WL_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <stm32wlxx.h>
+
+/* Add generated devicetree information and STM32 helper macros */
+#include <st_stm32_dt.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _STM32WL_SOC_H_ */
