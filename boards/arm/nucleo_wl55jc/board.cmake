755e3b540c8912c4e34337c9058a63ae71f077b1
Alexandre Bourdiol <alexandre.bourdiol@st.com>
2021-02-02 15:04:26 +0100

    boards: arm: add nucleo_wl55jc board support
    Add support of nucleo_wl55jc board
    
    Signed-off-by: Alexandre Bourdiol <alexandre.bourdiol@st.com>

---
+++ boards/arm/nucleo_wl55jc/board.cmake
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset=hw")
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
