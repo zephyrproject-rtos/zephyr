8e02c4cdf44db3a3c0112368a5865a0f653413a3
Pavlo Hamov <pasha.gamov@gmail.com>
2021-02-02 18:31:09 +0200

    shell: adc: support cc322xx
    Control cc322xx ADC from shell
    
    Signed-off-by: Pavlo Hamov <pasha.gamov@gmail.com>

--- drivers/adc/adc_shell.c
+++ drivers/adc/adc_shell.c
@@ -29,6 +29,8 @@
 #define DT_DRV_COMPAT st_stm32_adc
 #elif DT_HAS_COMPAT_STATUS_OKAY(nuvoton_npcx_adc)
 #define DT_DRV_COMPAT nuvoton_npcx_adc
+#elif DT_HAS_COMPAT_STATUS_OKAY(ti_cc32xx_adc)
+#define DT_DRV_COMPAT ti_cc32xx_adc
 #else
 #error No known devicetree compatible match for ADC shell
 #endif
