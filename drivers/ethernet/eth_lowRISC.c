e6e03dd2a80b6694fb1e589517207432c9e45338
akif <akif@recogni.com>
2021-02-03 11:28:56 -0800

    bugfix:: disable interrupts beginning of ISR

--- drivers/ethernet/eth_lowRISC.c
+++ drivers/ethernet/eth_lowRISC.c
@@ -209,6 +209,8 @@ static void lr_isr(struct device *dev)
    struct net_pkt *pkt = NULL;
    uint16_t vlan_tag = NET_VLAN_TAG_UNSPEC;
 
+   lr_eth_disable_irq(priv);
+
    if ((n = lr_eth_recv_size(priv)) > 0)
    {
 
