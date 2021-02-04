bd1d0b22b5590a57a05e51260284597464abe5ce
Enjia Mai <enjiax.mai@intel.com>
2021-02-04 14:42:13 +0800

    tests: ztest: fix error_hook test case fail in some board
    Enlarge the test thread stack size, to fix test case fail
    in some board which need more stack size for testing, when
    CONFIG_EXCEPTION_DEBUG is enabled.
    
    Signed-off-by: Enjia Mai <enjiax.mai@intel.com>

--- tests/ztest/error_hook/src/main.c
+++ tests/ztest/error_hook/src/main.c
@@ -9,7 +9,7 @@
 #include <syscall_handler.h>
 #include <ztest_error_hook.h>
 
-#define STACK_SIZE (512 + CONFIG_TEST_EXTRA_STACKSIZE)
+#define STACK_SIZE (1024 + CONFIG_TEST_EXTRA_STACKSIZE)
 #define THREAD_TEST_PRIORITY 5
 
 static K_THREAD_STACK_DEFINE(tstack, STACK_SIZE);
