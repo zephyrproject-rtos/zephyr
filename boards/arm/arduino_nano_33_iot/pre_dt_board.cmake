6fb13f63a3cc0109e51ae10359bea2a40343f283
Kumar Gala <kumar.gala@linaro.org>
2021-02-03 21:33:01 -0600

    dts: atmel/nxp: supress duplicate unit-address warning
    A number of SoCs have overlapping devices at the same unit address.
    Surpress the warning for those cases:
    
    * Atmel - pinmux@41004400 & gpio@41004400
    * Atmel - pinmux@41004480 & gpio@41004480
    * Atmel - pinmux@41008000 & gpio@41008000
    * NXP - flash@0 & gpio@0
    * NXP - syscon@0 & gpio@0
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- boards/arm/arduino_nano_33_iot/pre_dt_board.cmake
+++ boards/arm/arduino_nano_33_iot/pre_dt_board.cmake
@@ -3,3 +3,8 @@
 
 # SPI is implemented via sercom so node name isn't spi@...
 list(APPEND EXTRA_DTC_FLAGS "-Wno-spi_bus_bridge")
+
+# Suppress "unique_unit_address_if_enabled" to handle the following overlaps:
+# - /soc/pinmux@41004400 & /soc/gpio@41004400
+# - /soc/pinmux@41004480 & /soc/gpio@41004480
+list(APPEND EXTRA_DTC_FLAGS "-Wno-unique_unit_address_if_enabled")
