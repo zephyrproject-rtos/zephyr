2ac4944f8af9e0ebe5a739d4a583d83a7fe1bef2
Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>
2021-02-05 16:11:15 +0100

    Bluetooth: Audio: Test improvement - use AD array size
    Use new AD_SIZE value when setting up advertising in VCS and CCP.
    
    Signed-off-by: Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_audio/src/ccp_test.c
+++ tests/bluetooth/bsim_bt/bsim_test_audio/src/ccp_test.c
@@ -143,7 +143,7 @@ static void test_main(void)
 
 	printk("Audio Server: Bluetooth discovered\n");
 
-	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, 1, NULL, 0);
+	err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, ad, AD_SIZE, NULL, 0);
 	if (err) {
 		FAIL("Advertising failed to start (err %d)\n", err);
 		return;
