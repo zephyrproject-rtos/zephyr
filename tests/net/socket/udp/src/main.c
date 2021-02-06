2f02ca0b393c3c183fd38085cc3ac267a14c9c41
Hubert Miś <hubert.mis@nordicsemi.no>
2021-02-06 19:15:32 +0100

    tests: net: socket: SO_PROTOCOL socket option
    This patch adds tests to verify getting SO_PROTOCOL socket option for
    TCP, UDP, and TLS sockets.
    
    Signed-off-by: Hubert Miś <hubert.mis@nordicsemi.no>

--- tests/net/socket/udp/src/main.c
+++ tests/net/socket/udp/src/main.c
@@ -845,6 +845,35 @@ void test_so_rcvtimeo(void)
 	zassert_equal(rv, 0, "close failed");
 }
 
+void test_so_protocol(void)
+{
+	struct sockaddr_in bind_addr4;
+	struct sockaddr_in6 bind_addr6;
+	int sock1, sock2, rv;
+	int optval;
+	socklen_t optsize = sizeof(optval);
+
+	prepare_sock_udp_v4(CONFIG_NET_CONFIG_MY_IPV4_ADDR, 55555,
+			    &sock1, &bind_addr4);
+	prepare_sock_udp_v6(CONFIG_NET_CONFIG_MY_IPV6_ADDR, 55555,
+			    &sock2, &bind_addr6);
+
+	rv = getsockopt(sock1, SOL_SOCKET, SO_PROTOCOL, &optval, &optsize);
+	zassert_equal(rv, 0, "getsockopt failed (%d)", errno);
+	zassert_equal(optval, IPPROTO_UDP, "getsockopt got invalid protocol");
+	zassert_equal(optsize, sizeof(optval), "getsockopt got invalid size");
+
+	rv = getsockopt(sock2, SOL_SOCKET, SO_PROTOCOL, &optval, &optsize);
+	zassert_equal(rv, 0, "getsockopt failed (%d)", errno);
+	zassert_equal(optval, IPPROTO_UDP, "getsockopt got invalid protocol");
+	zassert_equal(optsize, sizeof(optval), "getsockopt got invalid size");
+
+	rv = close(sock1);
+	zassert_equal(rv, 0, "close failed");
+	rv = close(sock2);
+	zassert_equal(rv, 0, "close failed");
+}
+
 static void comm_sendmsg_with_txtime(int client_sock,
 				     struct sockaddr *client_addr,
 				     socklen_t client_addrlen,
@@ -1061,6 +1090,7 @@ void test_main(void)
 			 ztest_unit_test(test_so_priority),
 			 ztest_unit_test(test_so_txtime),
 			 ztest_unit_test(test_so_rcvtimeo),
+			 ztest_unit_test(test_so_protocol),
 			 ztest_unit_test(test_v4_sendmsg_recvfrom),
 			 ztest_user_unit_test(test_v4_sendmsg_recvfrom),
 			 ztest_unit_test(test_v4_sendmsg_recvfrom_no_aux_data),
