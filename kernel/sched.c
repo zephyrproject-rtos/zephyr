f16bf022bcaf7bb4050bb10766161b96820df599
Andy Ross <andrew.j.ross@intel.com>
2021-02-09 16:47:47 -0800

    kernel/sched: Add missing lock around waitq unpend calls
    The two calls to unpend a thread from a wait queue were inexplicably*
    unsynchronized, as James Harris discovered.  Rework them to call the
    lowest level primities so we can wrap the process inside the scheduler
    lock.
    
    Fixes #32136
    
    * I took a brief look.  What seems to have happened here is that these
      were originally synchronized via an implicit from an outer caller
      (remember the original Uniprocessor irq_lock() API is a recursive
      lock), and they were mostly implemented in terms of middle-level
      calls that were themselves locked.  So those got ported over to the
      newer spinlock but the outer wrapper layer got forgotten.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- kernel/sched.c
+++ kernel/sched.c
@@ -766,12 +766,32 @@ int z_pend_curr(struct k_spinlock *lock, k_spinlock_key_t key,
 	return z_swap(lock, key);
 }
 
+struct k_thread *z_unpend1_no_timeout(_wait_q_t *wait_q)
+{
+	struct k_thread *thread = NULL;
+
+	LOCKED(&sched_spinlock) {
+		thread = _priq_wait_best(&wait_q->waitq);
+
+		if (thread != NULL) {
+			unpend_thread_no_timeout(thread);
+		}
+	}
+
+	return thread;
+}
+
 struct k_thread *z_unpend_first_thread(_wait_q_t *wait_q)
 {
-	struct k_thread *thread = z_unpend1_no_timeout(wait_q);
+	struct k_thread *thread = NULL;
 
-	if (thread != NULL) {
-		(void)z_abort_thread_timeout(thread);
+	LOCKED(&sched_spinlock) {
+		thread = _priq_wait_best(&wait_q->waitq);
+
+		if (thread != NULL) {
+			unpend_thread_no_timeout(thread);
+			(void)z_abort_thread_timeout(thread);
+		}
 	}
 
 	return thread;
