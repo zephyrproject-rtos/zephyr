b3e6c5c847a38b0b6e3f1d87f8d35f5d3568aadc
Kumar Gala <kumar.gala@linaro.org>
2021-02-11 07:26:41 -0600

    pinmux: sifive: Convert SiFive pinmux to be devicetree based
    Add a simple pinctrl node for the IOF registers under the GPIO
    controller node to be used by the pinmux driver.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- boards/riscv/hifive1_revb/pinmux.c
+++ boards/riscv/hifive1_revb/pinmux.c
@@ -12,7 +12,9 @@ static int hifive1_revb_pinmux_init(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 
-	const struct device *p = device_get_binding(CONFIG_PINMUX_SIFIVE_0_NAME);
+	const struct device *p = DEVICE_DT_GET(DT_NODELABEL(pinctrl));
+
+	__ASSERT_NO_MSG(device_is_ready(p));
 
 #ifdef CONFIG_UART_SIFIVE
 #ifdef CONFIG_UART_SIFIVE_PORT_0
