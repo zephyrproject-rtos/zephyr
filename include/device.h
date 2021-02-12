5382a348c835931cb5b80861eec6bd375750ba86
Peter Bigot <peter.bigot@nordicsemi.no>
2021-02-12 06:19:08 -0600

    device: binding lookup should return null for unsupported names
    A null device name should map to a null device.  So should a name that
    is empty.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- include/device.h
+++ include/device.h
@@ -313,7 +313,8 @@ struct device {
  * it can use this function to retrieve the device structure of the lower level
  * driver by the name the driver exposes to the system.
  *
- * @param name device name to search for.
+ * @param name device name to search for.  A null pointer, or a pointer to an
+ * empty string, will cause NULL to be returned.
  *
  * @return pointer to device structure; NULL if not found or cannot be used.
  */
