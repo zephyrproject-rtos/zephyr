adac872a27d8d36fe8cbaebdf731d9fc73b95d29
Daniel Leung <daniel.leung@intel.com>
2021-02-12 14:03:06 -0800

    kernel: mmu: fix boot address translation macros
    The Z_BOOT_VIRT_TO_PHYS() and Z_BOOT_PHYS_TO_VIRT() address
    translation macros are flipped in their calculations.
    The calculation is supposed to be:
    
      virt = phys + ((KERNEL_VM_BASE + KERNEL_VM_OFFSET) -
                     SRAM_BASE_ADDRESS)
    
    So fix the them.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- kernel/include/mmu.h
+++ kernel/include/mmu.h
@@ -47,8 +47,8 @@
  * been remapped or paged out. Never use this unless you know exactly what you
  * are doing.
  */
-#define Z_BOOT_VIRT_TO_PHYS(virt) ((uintptr_t)(((uint8_t *)virt) + Z_VM_OFFSET))
-#define Z_BOOT_PHYS_TO_VIRT(phys) ((uint8_t *)(((uintptr_t)phys) - Z_VM_OFFSET))
+#define Z_BOOT_VIRT_TO_PHYS(virt) ((uintptr_t)(((uint8_t *)virt) - Z_VM_OFFSET))
+#define Z_BOOT_PHYS_TO_VIRT(phys) ((uint8_t *)(((uintptr_t)phys) + Z_VM_OFFSET))
 
 #ifdef CONFIG_ARCH_MAPS_ALL_RAM
 #define Z_FREE_VM_START	Z_BOOT_PHYS_TO_VIRT(Z_PHYS_RAM_END)
