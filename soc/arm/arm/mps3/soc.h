1e6716d9ff87858622e460fd7361d8451c156e51
Kumar Gala <kumar.gala@linaro.org>
2021-02-13 14:42:46 -0600

    arm: mps3_an547: Add SoC support for the AN547 on MPS3
    The AN547 is a Soft Macro Model implementation of the SSE-300 subsystem
    with Ethos-U55 and Cortex-M55 components targeting the MPS3 board.
    
    The SoC support is based on the AN521 MPS2+ support that already exists
    in Zephyr.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

---
+++ soc/arm/arm/mps3/soc.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2017-2021 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC_H_
+#define _SOC_H_
+
+#define __MPU_PRESENT 1
+
+#if defined(CONFIG_SOC_MPS3_AN547)
+#define __SAUREGION_PRESENT       1U        /* SAU regions present */
+#define __FPU_PRESENT             CONFIG_CPU_HAS_FPU
+#define __DSP_PRESENT             1U        /* DSP extension present */
+#define __MVE_PRESENT             1U        /* MVE extensions present */
+#define __MVE_FP                  1U        /* MVE floating point present */
+#endif
+
+#include <devicetree.h>
+
+#endif /* _SOC_H_ */
