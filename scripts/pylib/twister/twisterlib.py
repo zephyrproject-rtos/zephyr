fbd34dca1e619272a6489de32e7bb4ca9e049fc3
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-02-14 18:02:12 -0800

    twisterlib: add deprecated DT props to warnings_as_errors
    Use EXTRA_GEN_DEFINES_ARGS to error out on deprecated devicetree
    properties when warnings are treated as errors.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- scripts/pylib/twister/twisterlib.py
+++ scripts/pylib/twister/twisterlib.py
@@ -1993,8 +1993,10 @@ class CMake():
             ldflags = "-Wl,--fatal-warnings"
             cflags = "-Werror"
             aflags = "-Wa,--fatal-warnings"
+            gen_defines_args = "--err-on-deprecated-properties"
         else:
             ldflags = cflags = aflags = ""
+            gen_defines_args = ""
 
         logger.debug("Running cmake on %s for %s" % (self.source_dir, self.platform.name))
         cmake_args = [
@@ -2003,6 +2005,7 @@ class CMake():
             f'-DEXTRA_CFLAGS="{cflags}"',
             f'-DEXTRA_AFLAGS="{aflags}',
             f'-DEXTRA_LDFLAGS="{ldflags}"',
+            f'-DEXTRA_GEN_DEFINES_ARGS={gen_defines_args}',
             f'-G{self.generator}'
         ]
 
