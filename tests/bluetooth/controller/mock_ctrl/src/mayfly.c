c607c97f96a289002e6d9700d457fedac91ce225
Thomas Ebert Hansen <thoh@oticon.com>
2021-02-15 13:58:49 +0100

    tests: Bluetooth: Update controller mock
    Add missing functionality to the controller mock
    
    Signed-off-by: Thomas Ebert Hansen <thoh@oticon.com>

---
+++ tests/bluetooth/controller/mock_ctrl/src/mayfly.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021 Demant
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "memq.h"
+#include "mayfly.h"
+
+void mayfly_init(void)
+{
+}
+
+void mayfly_enable(uint8_t caller_id, uint8_t callee_id, uint8_t enable)
+{
+}
+
+uint32_t mayfly_enqueue(uint8_t caller_id, uint8_t callee_id, uint8_t chain,
+			struct mayfly *m)
+{
+	return 0;
+}
+
+void mayfly_run(uint8_t callee_id)
+{
+
+}
