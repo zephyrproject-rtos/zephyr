dff55623d212b1c203caa0903ed3b97ee7388240
Pavlo Hamov <pasha.gamov@gmail.com>
2021-02-17 17:11:02 +0200

    samples: watchdog: Add cc32xx support
    add DTS bindings and board overlay
    
    Signed-off-by: Pavlo Hamov <pasha.gamov@gmail.com>

--- samples/drivers/watchdog/src/main.c
+++ samples/drivers/watchdog/src/main.c
@@ -41,6 +41,8 @@
 #define WDT_NODE DT_INST(0, nxp_kinetis_wdog32)
 #elif DT_HAS_COMPAT_STATUS_OKAY(microchip_xec_watchdog)
 #define WDT_NODE DT_INST(0, microchip_xec_watchdog)
+#elif DT_HAS_COMPAT_STATUS_OKAY(ti_cc32xx_watchdog)
+#define WDT_NODE DT_INST(0, ti_cc32xx_watchdog)
 #endif
 
 #ifndef WDT_ALLOW_CALLBACK
