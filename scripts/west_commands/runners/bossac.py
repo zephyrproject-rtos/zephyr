62be07224815ebc79b6fa6ba45d7907007de45cb
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-02-23 13:43:33 -0800

    scripts: runners: bossac: handle edtlib ImportError
    Runners may not import anything outside of stdlib unless they handle
    the possible ImportError.
    
    Apply this rule to the bossac runner. The general pattern is to have a
    MISSING_REQUIREMENTS global that we handle at the beginning of
    do_run(), but in this case this isn't a missing pip package, but a
    misconfiguration. Just log a warning; it won't happen in practice.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- scripts/west_commands/runners/bossac.py
+++ scripts/west_commands/runners/bossac.py
@@ -9,14 +9,18 @@ import pathlib
 import pickle
 import platform
 import subprocess
-import sys
 
 from runners.core import ZephyrBinaryRunner, RunnerCaps, BuildConfiguration
-from zephyr_ext_common import ZEPHYR_SCRIPTS
 
 # This is needed to load edt.pickle files.
-sys.path.append(str(ZEPHYR_SCRIPTS / 'dts'))
-import edtlib  # pylint: disable=unused-import
+try:
+    import edtlib  # pylint: disable=unused-import
+    MISSING_EDTLIB = False
+except ImportError:
+    # This can happen when building the documentation for the
+    # runners package if edtlib is not on sys.path. This is fine
+    # to ignore in that case.
+    MISSING_EDTLIB = True
 
 DEFAULT_BOSSAC_PORT = '/dev/ttyACM0'
 DEFAULT_BOSSAC_SPEED = '115200'
@@ -185,6 +189,11 @@ class BossacBinaryRunner(ZephyrBinaryRunner):
         return cmd_flash
 
     def do_run(self, command, **kwargs):
+        if MISSING_EDTLIB:
+            self.logger.warning(
+                'could not import edtlib; something may be wrong with the '
+                'python environment')
+
         if platform.system() == 'Windows':
             msg = 'CAUTION: BOSSAC runner not support on Windows!'
             raise RuntimeError(msg)
