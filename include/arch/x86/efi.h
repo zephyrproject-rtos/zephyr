b1976fd84a8cf51f23b2d870fe457c32c534768a
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2021-02-25 13:16:53 +0100

    zefi: Add an EFI boot argument passing ACPI RSDP info
    If such table pointer is present with EFI system table, this will speed
    up ACPI initialization later on.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- include/arch/x86/efi.h
+++ include/arch/x86/efi.h
@@ -12,11 +12,17 @@
 
 #ifndef _ASMLANGUAGE
 
+struct efi_boot_arg {
+	void *acpi_rsdp;
+};
+
 #if defined(CONFIG_X86_EFI)
 
 /** @brief Initialize usage of EFI gathered information
+ *
+ * @param efi_arg The given pointer to EFI prepared boot argument
  */
-void efi_init(void);
+void efi_init(struct efi_boot_arg *efi_arg);
 
 #else /* CONFIG_X86_EFI */
 
