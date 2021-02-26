25578868126e9868be05dc48c9661636dc09eed2
Mulin Chao <mlchao@nuvoton.com>
2021-02-25 23:15:58 -0800

    pm: power: npcx: add console expired mechanism.
    This CL adds support for console expired mechanism. It implements the
    notification to power management module that the module for console is
    in use. If the interval that module doesn't receive any input message
    exceeds CONFIG_SOC_POWER_CONSOLE_EXPIRED_TIMEOUT, the power management
    module is allowed to enter deep sleep mode. This mechanism gives a
    window in which the users can organize console input.
    
    Signed-off-by: Mulin Chao <mlchao@nuvoton.com>

---
+++ soc/arm/nuvoton_npcx/common/soc_power.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021 Nuvoton Technology Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _NUVOTON_NPCX_SOC_POWER_H_
+#define _NUVOTON_NPCX_SOC_POWER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Receive whether the module for the console is in use.
+ *
+ * @return 1 if console is in use. Otherwise
+ * @return
+ *  - True if the console is in use.
+ *  - False otherwise,  the module for console is reday to enter low power mode.
+ */
+bool npcx_power_console_is_in_use(void);
+
+/**
+ * @brief Notify the power module that the module for the console is in use.
+ *
+ * Notify the power module that the module for the console is in use. It also
+ * extends expired time by CONFIG_UART_CONSOLE_INPUT_EXPIRED_TIMEOUT.
+ */
+void npcx_power_console_is_in_use_refresh(void);
+
+/**
+ * @brief Set expired time-out directly for the console is in use.
+ *
+ * @param timeout Expired time-out for the console is in use.
+ */
+void npcx_power_set_console_in_use_timeout(int64_t timeout);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _NUVOTON_NPCX_SOC_POWER_H_ */
