f205e94f9ef89be491b9bc6d6328c3918e7c445f
Johann Fischer <johann.fischer@nordicsemi.no>
2021-02-26 13:22:20 +0100

    include: move disk_access.h to storage
    Zephyr already has a directory for storage API relevant headers.
    Move disk_access.h header to include/storage where it fits better
    structurally.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- tests/subsys/fs/fat_fs_dual_drive/src/disk_access_test_drv.c
+++ tests/subsys/fs/fat_fs_dual_drive/src/disk_access_test_drv.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <zephyr/types.h>
 #include <sys/__assert.h>
-#include <disk/disk_access.h>
+#include <storage/disk_access.h>
 #include <errno.h>
 #include <init.h>
 #include <device.h>
