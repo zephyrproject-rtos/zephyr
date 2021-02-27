707f0391cbf1ab2110ee3fd5b812e3e180d703a1
Kumar Gala <kumar.gala@linaro.org>
2021-02-27 10:07:02 -0600

    devicetree: io-channels: add marcos to obtain the node identifier
    Added helper macros to obtain the node identifier from an
    io-channels phandle.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- include/devicetree/io-channels.h
+++ include/devicetree/io-channels.h
@@ -99,6 +99,72 @@ extern "C" {
 #define DT_IO_CHANNELS_LABEL(node_id) DT_IO_CHANNELS_LABEL_BY_IDX(node_id, 0)
 
 /**
+ *
+ * @brief Get the node identifier for the node referenced by an
+ *        io-channels property at an index
+ *
+ * Example devicetree fragment:
+ *
+ *     adc1: adc@... { ... };
+ *
+ *     adc2: adc@... { ... };
+ *
+ *     n: node {
+ *             io-channels = <&adc1 10>, <&adc2 20>;
+ *     };
+ *
+ * Example usage:
+ *
+ *     DT_IO_CHANNELS_CTLR_BY_IDX(DT_NODELABEL(n), 0) // DT_NODELABEL(adc1)
+ *     DT_IO_CHANNELS_CTLR_BY_IDX(DT_NODELABEL(n), 1) // DT_NODELABEL(adc2)
+ *
+ * @param node_id node identifier for a node with an io-channels property
+ * @param idx logical index into io-channels property
+ * @return the node identifier for the node referenced at index "idx"
+ * @see DT_PROP_BY_PHANDLE_IDX()
+ */
+#define DT_IO_CHANNELS_CTLR_BY_IDX(node_id, idx) \
+	DT_PHANDLE_BY_IDX(node_id, io_channels, idx)
+
+/**
+ * @brief Get the node identifier for the node referenced by an
+ *        io-channels property by name
+ *
+ * Example devicetree fragment:
+ *
+ *     adc1: adc@... { ... };
+ *
+ *     adc2: adc@... { ... };
+ *
+ *     n: node {
+ *             io-channels = <&adc1 10>, <&adc2 20>;
+ *             io-channel-names = "SENSOR", "BANDGAP";
+ *     };
+ *
+ * Example usage:
+ *
+ *  DT_IO_CHANNELS_CTLR_BY_NAME(DT_NODELABEL(n), sensor) // DT_NODELABEL(adc1)
+ *  DT_IO_CHANNELS_CTLR_BY_NAME(DT_NODELABEL(n), bandgap) // DT_NODELABEL(adc2)
+ *
+ * @param node_id node identifier for a node with an io-channels property
+ * @param name lowercase-and-underscores name of an io-channels element
+ *             as defined by the node's io-channel-names property
+ * @return the node identifier for the node referenced at the named element
+ * @see DT_PHANDLE_BY_NAME()
+ */
+#define DT_IO_CHANNELS_CTLR_BY_NAME(node_id, name) \
+	DT_PHANDLE_BY_NAME(node_id, io_channels, name)
+
+/**
+ * @brief Equivalent to DT_IO_CHANNELS_CTLR_BY_IDX(node_id, 0)
+ * @param node_id node identifier for a node with an io-channels property
+ * @return the node identifier for the node referenced at index 0
+ *         in the node's "io-channels" property
+ * @see DT_IO_CHANNELS_CTLR_BY_IDX()
+ */
+#define DT_IO_CHANNELS_CTLR(node_id) DT_IO_CHANNELS_CTLR_BY_IDX(node_id, 0)
+
+/**
  * @brief Get a label property from a DT_DRV_COMPAT instance's io-channels
  *        property at an index
  * @param inst DT_DRV_COMPAT instance number
@@ -127,6 +193,39 @@ extern "C" {
  * @return the label property of the node referenced at index 0
  */
 #define DT_INST_IO_CHANNELS_LABEL(inst) DT_INST_IO_CHANNELS_LABEL_BY_IDX(inst, 0)
+
+/**
+ * @brief Get the node identifier from a DT_DRV_COMPAT instance's io-channels
+ *        property at an index
+ *
+ * @param inst DT_DRV_COMPAT instance number
+ * @param idx logical index into io-channels property
+ * @return the node identifier for the node referenced at index "idx"
+ * @see DT_IO_CHANNELS_CTLR_BY_IDX()
+ */
+#define DT_INST_IO_CHANNELS_CTLR_BY_IDX(inst, idx) \
+	DT_IO_CHANNELS_CTLR_BY_IDX(DT_DRV_INST(inst), idx)
+
+/**
+ * @brief Get the node identifier from a DT_DRV_COMPAT instance's io-channels
+ *        property by name
+ * @param inst DT_DRV_COMPAT instance number
+ * @param name lowercase-and-underscores name of an io-channels element
+ *             as defined by the node's io-channel-names property
+ * @return the node identifier for the node referenced at the named element
+ * @see DT_IO_CHANNELS_CTLR_BY_NAME()
+ */
+#define DT_INST_IO_CHANNELS_CTLR_BY_NAME(inst, name) \
+	DT_IO_CHANNELS_CTLR_BY_NAME(DT_DRV_INST(inst), name)
+
+/**
+ * @brief Equivalent to DT_INST_IO_CHANNELS_CTLR_BY_IDX(inst, 0)
+ * @param inst DT_DRV_COMPAT instance number
+ * @return the node identifier for the node referenced at index 0
+ *         in the node's "io-channels" property
+ * @see DT_IO_CHANNELS_CTLR_BY_IDX()
+ */
+#define DT_INST_IO_CHANNELS_CTLR(inst) DT_INST_IO_CHANNELS_CTLR_BY_IDX(inst, 0)
 
 /**
  * @brief Get an io-channels specifier input cell at an index
