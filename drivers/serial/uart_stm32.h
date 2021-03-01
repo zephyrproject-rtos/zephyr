2dabaf32ea80b4b365e644220bf5f0dc0833be58
Erwan Gouriou <erwan.gouriou@linaro.org>
2021-03-01 11:59:57 +0100

    drivers/serial: stm32: convert dma to new DT_DMA helper macros
    New DT_DMA helper macors are available to access DMA node
    identifier. Use them
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- drivers/serial/uart_stm32.h
+++ drivers/serial/uart_stm32.h
@@ -29,7 +29,7 @@ struct uart_stm32_config {
 
 #ifdef CONFIG_UART_ASYNC_API
 struct uart_dma_stream {
-	const char *dma_name;
+	const struct device *dma_dev;
 	uint32_t dma_channel;
 	struct dma_config dma_cfg;
 	uint8_t priority;
@@ -60,8 +60,6 @@ struct uart_stm32_data {
 
 #ifdef CONFIG_UART_ASYNC_API
 	const struct device *uart_dev;
-	const struct device *dev_dma_tx;
-	const struct device *dev_dma_rx;
 	uart_callback_t async_cb;
 	void *async_user_data;
 	struct uart_dma_stream dma_rx;
