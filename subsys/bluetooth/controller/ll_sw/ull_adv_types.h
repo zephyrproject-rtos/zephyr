23f9a1ed1dd3dc34ce415bb3932d6612cb79babf
Wolfgang Puffitsch <wopu@demant.com>
2021-03-01 16:42:19 +0100

    Bluetooth: controller: Rename peer's address in advertising set struct
    Rename the peer's address in the advertising set struct from `id_addr`
    to `peer_addr` to clarify what the address refers to.
    
    Signed-off-by: Wolfgang Puffitsch <wopu@demant.com>

--- subsys/bluetooth/controller/ll_sw/ull_adv_types.h
+++ subsys/bluetooth/controller/ll_sw/ull_adv_types.h
@@ -37,8 +37,8 @@ struct ll_adv_set {
 
 #if defined(CONFIG_BT_CTLR_PRIVACY)
 	uint8_t  own_addr_type:2;
-	uint8_t  id_addr_type:1;
-	uint8_t  id_addr[BDADDR_SIZE];
+	uint8_t  peer_addr_type:1;
+	uint8_t  peer_addr[BDADDR_SIZE];
 #endif /* CONFIG_BT_CTLR_PRIVACY */
 
 #if defined(CONFIG_BT_CTLR_DF_ADV_CTE_TX)
