0d9da124cd827ed0d0a6f05a208cec6c1e724a93
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-03-02 10:31:35 -0800

    samples: mesh_demo: rework error and device handling
    The board specific interface in this sample is not handling errors.
    Fix that by making board_init() return a negative error code or zero.
    
    Rework the bbc_microbit specific board file by handling errors
    appropriately and avoiding device_get_binding() to move some device
    configuration errors to build time.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- samples/bluetooth/mesh_demo/src/board.h
+++ samples/bluetooth/mesh_demo/src/board.h
@@ -15,15 +15,16 @@ uint16_t board_set_target(void);
 void board_play(const char *str);
 
 #if defined(CONFIG_BOARD_BBC_MICROBIT)
-void board_init(uint16_t *addr);
+int board_init(uint16_t *addr);
 void board_play_tune(const char *str);
 void board_heartbeat(uint8_t hops, uint16_t feat);
 void board_other_dev_pressed(uint16_t addr);
 void board_attention(bool attention);
 #else
-static inline void board_init(uint16_t *addr)
+static inline int board_init(uint16_t *addr)
 {
 	*addr = NODE_ADDR;
+	return 0;
 }
 
 static inline void board_play_tune(const char *str)
