d860569d94b3ef5b6b40865921a409b1bd40278b
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2021-03-02 09:21:27 +0100

    tests: drivers: build_all: Add fake instance of GPIO device
    Such instance is needed for building drivers that use DEVICE_DT_GET()
    to get the pointer to the GPIO controller device structure.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- tests/drivers/build_all/src/main.c
+++ tests/drivers/build_all/src/main.c
@@ -6,6 +6,7 @@
 
 #include <zephyr.h>
 #include <sys/printk.h>
+#include <device.h>
 
 
 /*
@@ -18,3 +19,11 @@ void main(void)
 {
 	printk("Hello World!\n");
 }
+
+#if DT_NODE_EXISTS(DT_INST(0, vnd_gpio))
+/* Fake GPIO device, needed for building drivers that use DEVICE_DT_GET()
+ * to access GPIO controllers.
+ */
+DEVICE_DT_DEFINE(DT_INST(0, vnd_gpio), NULL, NULL, NULL, NULL,
+		 POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, NULL);
+#endif
