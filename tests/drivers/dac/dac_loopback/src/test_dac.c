41225ceac6ec149baab55ca9ac1933822897ee8b
Mikkel Jakobsen <mikkel.aunsbjerg@prevas.dk>
2021-03-02 12:28:32 +0100

    tests: drivers: dac_loopback: add frdm_f22f board support
    Add support for running the DAC loopback test case on the NXP
    FRDM-K22F development board.
    
    Signed-off-by: Mikkel Jakobsen <mikkel.aunsbjerg@prevas.dk>

--- tests/drivers/dac/dac_loopback/src/test_dac.c
+++ tests/drivers/dac/dac_loopback/src/test_dac.c
@@ -95,6 +95,21 @@
 #define ADC_ACQUISITION_TIME	ADC_ACQ_TIME_DEFAULT
 #define ADC_CHANNEL_ID		23
 
+#elif defined(CONFIG_BOARD_FRDM_K22F)
+
+/* DAC0 output is internally available on ADC0_SE23 */
+
+#define DAC_DEVICE_NAME		DT_LABEL(DT_NODELABEL(dac0))
+#define DAC_RESOLUTION		12
+#define DAC_CHANNEL_ID		0
+
+#define ADC_DEVICE_NAME		DT_LABEL(DT_NODELABEL(adc0))
+#define ADC_RESOLUTION		12
+#define ADC_GAIN		ADC_GAIN_1
+#define ADC_REFERENCE		ADC_REF_INTERNAL
+#define ADC_ACQUISITION_TIME	ADC_ACQ_TIME_DEFAULT
+#define ADC_CHANNEL_ID		23
+
 #else
 #error "Unsupported board."
 #endif
