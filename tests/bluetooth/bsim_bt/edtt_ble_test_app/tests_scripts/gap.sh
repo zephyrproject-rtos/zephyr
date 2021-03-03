eae73b460bea444dfdea7dafa6623de423525d2a
Alberto Escolar Piedras <alpi@oticon.com>
2021-03-03 09:35:00 +0100

    bsim_bt: tests: Fix relative paths search in scripts
    The way these scripts looked for the current working directory
    was not correct or reliable. With this change it should
    work properly.
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>

--- tests/bluetooth/bsim_bt/edtt_ble_test_app/tests_scripts/gap.sh
+++ tests/bluetooth/bsim_bt/edtt_ble_test_app/tests_scripts/gap.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # GAP regression tests based on the EDTTool
-CWD=`pwd`"/"`dirname $0`
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)"
 
 export SIMULATION_ID="edtt_gap"
 export TEST_FILE=${CWD}"/gap.test_list"
