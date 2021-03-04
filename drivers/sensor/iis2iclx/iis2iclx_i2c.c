b43370330babe592fc0c816812c9527d55e070d6
Armando Visconti <armando.visconti@st.com>
2021-03-04 11:37:36 +0100

    drivers/sensor: iis2iclx: fix multi-instance using new helpers
    This commit simplifies the Device Tree configuration by using
    the new helpers introduced in #30536.
    
    In particular:
        - get bus devices with DEVICE_DT_GET
        - get SPI information with SPI_CONFIG_DT_INST
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

--- drivers/sensor/iis2iclx/iis2iclx_i2c.c
+++ drivers/sensor/iis2iclx/iis2iclx_i2c.c
@@ -19,23 +19,21 @@
 
 LOG_MODULE_DECLARE(IIS2ICLX, CONFIG_SENSOR_LOG_LEVEL);
 
-static int iis2iclx_i2c_read(struct iis2iclx_data *data, uint8_t reg_addr,
+static int iis2iclx_i2c_read(const struct device *dev, uint8_t reg_addr,
 			       uint8_t *value, uint8_t len)
 {
-	const struct device *dev = data->dev;
 	const struct iis2iclx_config *cfg = dev->config;
 
-	return i2c_burst_read(data->bus, cfg->bus_cfg.i2c_slv_addr,
+	return i2c_burst_read(cfg->bus, cfg->bus_cfg.i2c_slv_addr,
 			      reg_addr, value, len);
 }
 
-static int iis2iclx_i2c_write(struct iis2iclx_data *data, uint8_t reg_addr,
+static int iis2iclx_i2c_write(const struct device *dev, uint8_t reg_addr,
 				uint8_t *value, uint8_t len)
 {
-	const struct device *dev = data->dev;
 	const struct iis2iclx_config *cfg = dev->config;
 
-	return i2c_burst_write(data->bus, cfg->bus_cfg.i2c_slv_addr,
+	return i2c_burst_write(cfg->bus, cfg->bus_cfg.i2c_slv_addr,
 			       reg_addr, value, len);
 }
 
@@ -43,11 +41,11 @@ int iis2iclx_i2c_init(const struct device *dev)
 {
 	struct iis2iclx_data *data = dev->data;
 
-	data->ctx_i2c.read_reg = (stmdev_read_ptr) iis2iclx_i2c_read,
-	data->ctx_i2c.write_reg = (stmdev_write_ptr) iis2iclx_i2c_write,
+	data->ctx_i2c.read_reg = (stmdev_read_ptr) iis2iclx_i2c_read;
+	data->ctx_i2c.write_reg = (stmdev_write_ptr) iis2iclx_i2c_write;
 
 	data->ctx = &data->ctx_i2c;
-	data->ctx->handle = data;
+	data->ctx->handle = (void *)dev;
 
 	return 0;
 }
