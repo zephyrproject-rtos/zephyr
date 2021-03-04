66f699b8ea6d0ac9d31f80c40394157d465e4f9c
Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>
2021-03-04 16:17:11 +0100

    Bluetooth: Audio: Add maintainers/codeowners
    Add Thalley (emil.gydesen@nordicsemi.no) and asbjornsabo
    (asbjorn.sabo@nordicsemi.no) as additional maintainers/codeowners for
    bluetooth audio.
    
    (Also adds empty audio.h file, that will later be expanded, so that
    the include/bluetooth/audio directory now listed in CODEOWNERS
    actually exists.)
    
    Signed-off-by: Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>

---
+++ include/bluetooth/audio/audio.h
@@ -0,0 +1,9 @@
+/** @file
+ *  @brief Header for Bluetooth le-audio
+ *
+ * Copyright (c) 2020 Nordic Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* For now, this file is a place holder */
