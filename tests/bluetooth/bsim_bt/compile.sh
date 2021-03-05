b65299d7fea2ab3022939d728256aaa6a8887d01
Thomas Ebert Hansen <thoh@oticon.com>
2021-03-05 09:24:23 +0100

    tests: Bluetooth: bsim: Build multiple variants of hci_test_app
    Build both the new and legacy variant of the hci_test_app.
    
    Signed-off-by: Thomas Ebert Hansen <thoh@oticon.com>

--- tests/bluetooth/bsim_bt/compile.sh
+++ tests/bluetooth/bsim_bt/compile.sh
@@ -58,6 +58,9 @@ app=tests/bluetooth/bsim_bt/bsim_test_app conf_file=prj_split_privacy.conf \
 app=tests/bluetooth/bsim_bt/bsim_test_advx compile
 app=tests/bluetooth/bsim_bt/bsim_test_iso compile
 app=tests/bluetooth/bsim_bt/bsim_test_audio compile
-app=tests/bluetooth/bsim_bt/edtt_ble_test_app/hci_test_app compile
+app=tests/bluetooth/bsim_bt/edtt_ble_test_app/hci_test_app conf_file=prj.conf \
+  compile
+app=tests/bluetooth/bsim_bt/edtt_ble_test_app/hci_test_app \
+  conf_file=prj_legacy.conf compile
 app=tests/bluetooth/bsim_bt/edtt_ble_test_app/gatt_test_app compile
 app=tests/bluetooth/bsim_bt/bsim_test_mesh compile
