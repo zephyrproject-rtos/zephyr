c5e75509ac3aa9ade42d6396797564a7023d10e6
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2021-03-08 12:21:01 +0530

    Bluetooth: controller: Add function to calculate channel identifier
    Added function to calculate channel identifier value
    required for Channel Selection Algorithm #2.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/lll_chan.h
+++ subsys/bluetooth/controller/ll_sw/lll_chan.h
@@ -6,5 +6,6 @@
 
 uint8_t lll_chan_sel_1(uint8_t *chan_use, uint8_t hop, uint16_t latency, uint8_t *chan_map,
 		    uint8_t chan_count);
+uint16_t lll_chan_id(uint8_t *access_addr);
 uint8_t lll_chan_sel_2(uint16_t counter, uint16_t chan_id, uint8_t *chan_map,
 		    uint8_t chan_count);
