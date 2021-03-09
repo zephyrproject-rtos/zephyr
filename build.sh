2dddceaf14d6049c69a1a6eeda1535018525d27e
shuiyihang <shuiyihang0981@163.com>
2021-03-09 10:57:31 +0800

    修改：增加自己的工程目录和自动编译脚本

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+if [ ! $1 ]; then
+    BOARDS='stm32f103_mini'
+elif [ "$1" = "-k" ]; then
+    BOARDS='stm32f103_mini'
+else
+    BOARDS=$1
+fi;
+if [ ! $2 ]; then
+        PROJECT="./samples/MyselfProject/SnakeGame/"
+else
+        PROJECT=$2
+fi;
+
+west build -b ${BOARDS}  ${PROJECT} -p auto
---
+++ samples/MyselfProject/SnakeGame/src/main.c
@@ -0,0 +1,17 @@
+#include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+
+
+
+
+
+
+void main(void)
+{
+    while(1)
+    {
+        
+    }
+}
