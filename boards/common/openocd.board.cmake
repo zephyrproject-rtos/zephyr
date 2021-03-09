56278397c9d46966313e6d2b4a75c7b0ed6b897e
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2021-03-09 16:02:19 +0100

    boards: common: add cortex-m specific command for clearing DHCSR
    Introduce a command in openocd.board.cmake for cortex-m
    targets, which clears the halt debug enable bit flag, upon
    openocd exit. This is required since openocd does not clear
    the state itself when it shuts down.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- boards/common/openocd.board.cmake
+++ boards/common/openocd.board.cmake
@@ -16,7 +16,18 @@ endif()
 set(OPENOCD_CMD_LOAD_DEFAULT "${OPENOCD_FLASH}")
 set(OPENOCD_CMD_VERIFY_DEFAULT "verify_image")
 
+# Cortex-M-specific halt debug enable bit clearing:
+# OpenOCD sets DHCSR.DEBUGEN to 1 to halt and flash the target.
+# As openOCD shutdown does not clear DEBUGEN, we introduce a runner
+# option to explicitly clear DEBUGEN (Halt Debug Enable bit) after
+# openOCD has finished flashing and resetting the target (but before
+# it shuts down).
+if(CONFIG_CPU_CORTEX_M)
+set(OPENOCD_CMD_HALT_DEBUG_EN_CLEAR "mww 0xE000EDF0 0xA05F0000")
+endif()
+
 board_finalize_runner_args(openocd
   --cmd-load "${OPENOCD_CMD_LOAD_DEFAULT}"
   --cmd-verify "${OPENOCD_CMD_VERIFY_DEFAULT}"
+  --cmd-post-reset "${OPENOCD_CMD_HALT_DEBUG_EN_CLEAR}"
   )
