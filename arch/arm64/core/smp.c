64dfa69681ff21062cf69fe285057e19cfd1f55a
Carlo Caione <ccaione@baylibre.com>
2021-03-09 14:18:19 +0100

    aarch64: Remove useless _curr_cpu struct
    Currently _curr_cpu is only used by the get_cpu macro to quickly access
    the cpu struct. This is not really necessary because we can access to
    the struct by directly referencing &(_kernel.cpus[cpu_num]) in assembly
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

--- arch/arm64/core/smp.c
+++ arch/arm64/core/smp.c
@@ -32,13 +32,8 @@ volatile struct {
 	char pad[] __aligned(L1_CACHE_BYTES);
 } arm64_cpu_init[CONFIG_MP_NUM_CPUS];
 
-/*
- * _curr_cpu is used to record the struct of _cpu_t of each cpu.
- * for efficient usage in assembly
- */
-volatile _cpu_t *_curr_cpu[CONFIG_MP_NUM_CPUS];
-
 extern void __start(void);
+
 /* Called from Zephyr initialization */
 void arch_start_cpu(int cpu_num, k_thread_stack_t *stack, int sz,
 		    arch_cpustart_t fn, void *arg)
@@ -46,7 +41,6 @@ void arch_start_cpu(int cpu_num, k_thread_stack_t *stack, int sz,
 	__ASSERT(sizeof(arm64_cpu_init[0]) == ARM64_CPU_INIT_SIZE,
 		 "ARM64_CPU_INIT_SIZE != sizeof(arm64_cpu_init[0]\n");
 
-	_curr_cpu[cpu_num] = &(_kernel.cpus[cpu_num]);
 	arm64_cpu_init[cpu_num].fn = fn;
 	arm64_cpu_init[cpu_num].arg = arg;
 	arm64_cpu_init[cpu_num].sp =
@@ -123,9 +117,6 @@ void arch_sched_ipi(void)
 static int arm64_smp_init(const struct device *dev)
 {
 	ARG_UNUSED(dev);
-
-	/* necessary master core init */
-	_curr_cpu[0] = &(_kernel.cpus[0]);
 
 	/*
 	 * SGI0 is use for sched ipi, this might be changed to use Kconfig
