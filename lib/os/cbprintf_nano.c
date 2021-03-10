5aecaa81a3c247becf21c73e19d7c869543c5fc8
Jennifer Williams <jennifer.m.williams@intel.com>
2021-03-10 06:14:44 +0200

    lib: os: various places fix missing final else
    The lib/os/ had several places missing final else
    statement in the if else if construct. This commit adds
    else {} or simple refactor to comply with coding guideline 15.7.
    - cbprintf_complete.c
    - cbprintf_nano.c
    - heap-validate.c
    - heap.c
    - onoff.c
    - p4wq.c
    - sem.c
    
    Also resolves the checkpatch issue of comments should align * on
    each line.
    
    Signed-off-by: Jennifer Williams <jennifer.m.williams@intel.com>

--- lib/os/cbprintf_nano.c
+++ lib/os/cbprintf_nano.c
@@ -204,6 +204,8 @@ start:
 			} else if (special == '+') {
 				prefix = "+";
 				min_width--;
+			} else {
+				;
 			}
 			data_len = convert_value(d, 10, 0, buf + sizeof(buf));
 			data = buf + sizeof(buf) - data_len;
