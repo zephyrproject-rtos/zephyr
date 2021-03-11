1a14bc89361b1e007a40a1980f4181a115a9bb72
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2021-03-11 15:10:42 +0100

    Bluetooth: Mesh: Add some actual behavior in sample
    Populates the onoff server stubs in the Bluetooth Mesh sample, and
    implements generic LED based hardware support to include more boards.
    
    Fixes #31031.
    
    Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

--- samples/bluetooth/mesh/src/board.h
+++ samples/bluetooth/mesh/src/board.h
@@ -6,23 +6,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#if defined(CONFIG_BOARD_BBC_MICROBIT)
 void board_output_number(bt_mesh_output_action_t action, uint32_t number);
 
 void board_prov_complete(void);
 
-void board_init(void);
-#else
-static inline void board_output_number(bt_mesh_output_action_t action,
-				       uint32_t number)
-{
-}
+int board_init(struct k_work *button_work);
 
-static inline void board_prov_complete(void)
-{
-}
-
-static inline void board_init(void)
-{
-}
-#endif
+void board_led_set(bool val);
