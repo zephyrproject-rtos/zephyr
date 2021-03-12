217cb13905187382595890783409c8bebbfb993b
Anas Nashif <anas.nashif@intel.com>
2021-03-12 13:13:22 -0500

    kernel: remove tickless idle
    This feature predated the tickless kernel and has been in legacy mode
    for a while. We now have no drivers or systems that do not support
    tickless, so remove this option and cleanup the code to only use
    tickless.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- drivers/timer/cortex_m_systick.c
+++ drivers/timer/cortex_m_systick.c
@@ -172,8 +172,7 @@ void sys_clock_set_timeout(int32_t ticks, bool idle)
 	 * the counter. (Note: we can assume if idle==true that
 	 * interrupts are already disabled)
 	 */
-	if (IS_ENABLED(CONFIG_TICKLESS_IDLE) && idle
-	    && ticks == K_TICKS_FOREVER) {
+	if (IS_ENABLED(CONFIG_TICKLESS_KERNEL) && idle && ticks == K_TICKS_FOREVER) {
 		SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
 		last_load = TIMER_STOPPED;
 		return;
