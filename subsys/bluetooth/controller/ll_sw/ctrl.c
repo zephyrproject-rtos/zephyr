0833f104b3a6a0575da1d669a22d6e4526a387a1
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2021-03-12 10:27:42 +0530

    Bluetooth: controller: legacy: Fix dev assert in CPR implementation
    Replace a development assertion in the implementation of
    Connection Parameter Request Procedure with an internal
    comment and handle transaction violation be ignoring the
    PDU.
    
    Relates to commit dec6140685b4 ("Bluetooth: controller: Fix
    dev assert in CPR implementation").
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ctrl.c
+++ subsys/bluetooth/controller/ll_sw/ctrl.c
@@ -3289,7 +3289,10 @@ isr_rx_conn_pkt_ctrl(struct radio_pdu_node_rx *node_rx,
 				_radio.conn_upd = conn;
 			}
 		} else {
-			LL_ASSERT(0);
+			/* Ignore duplicate request as peripheral is busy
+			 * processing the previously initiated connection
+			 * update request procedure.
+			 */
 		}
 		break;
 
