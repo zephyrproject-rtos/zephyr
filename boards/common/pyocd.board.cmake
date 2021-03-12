251c3078bec3c5a4245ef0b595eadcd9f8d80b77
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2021-03-12 16:24:00 +0100

    boards: common: pyocd: cortex-m specific command for clearing DHCSR
    Introduce a command in pyocd.board.cmake for ARM Cortex-m
    targets, which clears the halt debug enable bit flag, after
    flashing is complete and before pyocd exit. This is required
    since pyocd regular flashing command does not clear the state
    itself when it completes, nor when pyocd session shuts down.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

--- boards/common/pyocd.board.cmake
+++ boards/common/pyocd.board.cmake
@@ -1,5 +1,17 @@
 # SPDX-License-Identifier: Apache-2.0
 
+# Cortex-M-specific halt debug enable bit clearing:
+# OpenOCD sets DHCSR.DEBUGEN to 1 to halt and flash the target.
+# As openOCD shutdown does not clear DEBUGEN, we introduce a runner
+# option to explicitly clear DEBUGEN (Halt Debug Enable bit) after
+# openOCD has finished flashing and resetting the target (but before
+# it shuts down).
+if(CONFIG_CPU_CORTEX_M)
+set(PYOCD_CMD_HALT_DEBUG_EN_CLEAR "write32 0xE000EDF0 0xA05F0000")
+endif()
+
+
 board_set_flasher_ifnset(pyocd)
 board_set_debugger_ifnset(pyocd)
-board_finalize_runner_args(pyocd "--dt-flash=y")
+board_finalize_runner_args(pyocd "--dt-flash=y"
+    --post-flash "${PYOCD_CMD_HALT_DEBUG_EN_CLEAR}")
