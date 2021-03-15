451e026d6bbf76490e7cc347430b2c767b48a3f5
Thomas Ebert Hansen <thoh@oticon.com>
2021-03-15 11:49:45 +0100

    Bluetooth: controller: Setup LLL/CCM for master enc
    Calculate the SK and setup the CCM data structure.
    
    Signed-off-by: Thomas Ebert Hansen <thoh@oticon.com>

---
+++ tests/bluetooth/controller/mock_ctrl/src/ecb.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2021 Demant
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include "hal/ecb.h"
+
+void ecb_encrypt(uint8_t const *const key_le, uint8_t const *const clear_text_le, uint8_t * const cipher_text_le, uint8_t * const cipher_text_be)
+{
+}
