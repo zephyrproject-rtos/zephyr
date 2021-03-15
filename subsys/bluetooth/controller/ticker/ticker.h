c8460d6e329127e6100370d824baa9396438ec65
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2021-03-15 16:08:23 +0530

    Bluetooth: controller: Ticker next slot get to return lazy value
    Updated ticker implementation to return lazy value for a
    ticker when enumerating active tickers with time
    reservations.
    
    This is required to find offsets and to use the lazy value
    to correctly calculate event and payload counter values that
    needs to be filled into SyncInfo and BigInfo structures.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ticker/ticker.h
+++ subsys/bluetooth/controller/ticker/ticker.h
@@ -144,9 +144,13 @@ uint32_t ticker_stop_abs(uint8_t instance_index, uint8_t user_id, uint8_t ticker
 		      uint32_t ticks_at_stop, ticker_op_func fp_op_func,
 		      void *op_context);
 uint32_t ticker_next_slot_get(uint8_t instance_index, uint8_t user_id,
-			   uint8_t *ticker_id_head, uint32_t *ticks_current,
-			   uint32_t *ticks_to_expire,
-			   ticker_op_func fp_op_func, void *op_context);
+			      uint8_t *ticker_id, uint32_t *ticks_current,
+			      uint32_t *ticks_to_expire,
+			      ticker_op_func fp_op_func, void *op_context);
+uint32_t ticker_next_slot_get_ext(uint8_t instance_index, uint8_t user_id,
+				  uint8_t *ticker_id, uint32_t *ticks_current,
+				  uint32_t *ticks_to_expire, uint16_t *lazy,
+				  ticker_op_func fp_op_func, void *op_context);
 uint32_t ticker_job_idle_get(uint8_t instance_index, uint8_t user_id,
 			  ticker_op_func fp_op_func, void *op_context);
 void ticker_job_sched(uint8_t instance_index, uint8_t user_id);
