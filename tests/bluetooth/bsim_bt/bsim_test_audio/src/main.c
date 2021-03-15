c1741ed3f1005390f2eef563711e3df11afd98d0
Emil Gydesen <emil.gydesen@nordicsemi.no>
2021-03-15 11:13:25 +0100

    Bluetooth: Audio: Adds BASS BSIM tests
    Adds initial BASS BSIM tests.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_audio/src/main.c
+++ tests/bluetooth/bsim_bt/bsim_test_audio/src/main.c
@@ -17,6 +17,9 @@ extern struct bst_test_list *test_csis_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_csip_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_mcs_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_mcc_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_bass_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_bass_client_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_bass_broadcaster_install(struct bst_test_list *tests);
 
 
 bst_test_install_t test_installers[] = {
@@ -30,6 +33,9 @@ bst_test_install_t test_installers[] = {
 	test_csip_install,
 	test_mcs_install,
 	test_mcc_install,
+	test_bass_install,
+	test_bass_client_install,
+	test_bass_broadcaster_install,
 	NULL
 };
 
