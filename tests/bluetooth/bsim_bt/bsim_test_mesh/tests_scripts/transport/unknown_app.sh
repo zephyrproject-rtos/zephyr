400ccfa807431c0e1e0bc3aa14af5cfb61af8aed
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2021-03-16 15:33:15 +0100

    Bluetooth: Replace all bsim test scripts with testcase.yaml entries
    Replaces all bash scripts for the BabbleSim tests with testcase.yaml
    entries, where possible. As the EDTT tests need a python script to run
    in parallel, this has been left as-is, along with the framework scripts
    required to run them.
    
    Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/transport/unknown_app.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Copyright 2021 Nordic Semiconductor
-# SPDX-License-Identifier: Apache-2.0
-
-source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
-
-RunTest mesh_transport_unknown_app transport_tx_unknown_app transport_rx_none
