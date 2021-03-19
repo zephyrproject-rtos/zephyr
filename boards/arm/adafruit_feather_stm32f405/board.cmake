6ca934f3aab401069976d2c1acc00b83b78cc4ed
Richard Osterloh <richard.osterloh@gmail.com>
2021-03-19 11:55:43 +0000

    boards: adafruit_feather_stm32f405: OpenOCD support
    This board has pads for and SWD header as per
    https://learn.adafruit.com/adafruit-stm32f405-feather-express/pinouts#swd-port-3050618-16
    This commit addes OpenOCD support to the board
    
    Signed-off-by: Richard Osterloh <richard.osterloh@gmail.com>

--- boards/arm/adafruit_feather_stm32f405/board.cmake
+++ boards/arm/adafruit_feather_stm32f405/board.cmake
@@ -5,3 +5,4 @@ board_runner_args(jlink "--device=STM32F405RG" "--speed=4000")
 
 include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
