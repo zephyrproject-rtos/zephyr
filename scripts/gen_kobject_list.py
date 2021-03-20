2e934e0805cc6be9240e274aa7ed39e12bbf2ce9
Daniel Leung <daniel.leung@intel.com>
2021-03-20 12:02:59 -0700

    kernel: change metadata for priv_stacks to const
    The metadata array for priv_stacks doesn't change and
    can be marked const so this can put in ROM.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- scripts/gen_kobject_list.py
+++ scripts/gen_kobject_list.py
@@ -780,7 +780,7 @@ def write_gperf_table(fp, syms, objs, little_endian, static_begin, static_end):
                      " priv_stacks[%d][Z_KERNEL_STACK_LEN(CONFIG_PRIVILEGED_STACK_SIZE)];\n"
                      % stack_counter)
 
-            fp.write("static struct z_stack_data stack_data[%d] = {\n"
+            fp.write("static const struct z_stack_data stack_data[%d] = {\n"
                      % stack_counter)
             counter = 0
             for _, ko in objs.items():
