dbccc40102b44ae56188f6901004265ab07a0a47
Armando Visconti <armando.visconti@st.com>
2021-03-20 14:07:34 +0100

    drivers/sensor: lsm6dso: Add multi-instance support
    Make this driver multi-instance and use the new API.
    
    This commit makes use of some DT macro helpers
    In particular:
        - get bus devices with DEVICE_DT_GET
        - get SPI information with SPI_CONFIG_DT_INST
        - get drdy gpios with GPIO_DT_SPEC_GET
    
    Moreover the driver is now using the stmemsc common
    routines as requested in issue #33440 and it avoids
    the unnecessary declaration of both ctx_i2c and ctx_spi
    in the data structure.
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

--- drivers/sensor/lsm6dso/lsm6dso_i2c.c
+++ drivers/sensor/lsm6dso/lsm6dso_i2c.c
@@ -10,43 +10,28 @@
 
 #define DT_DRV_COMPAT st_lsm6dso
 
-#include <string.h>
-#include <drivers/i2c.h>
 #include <logging/log.h>
-
 #include "lsm6dso.h"
 
 #if DT_ANY_INST_ON_BUS_STATUS_OKAY(i2c)
 
 LOG_MODULE_DECLARE(LSM6DSO, CONFIG_SENSOR_LOG_LEVEL);
 
-static int lsm6dso_i2c_read(struct lsm6dso_data *data, uint8_t reg_addr,
-			    uint8_t *value, uint8_t len)
-{
-	const struct lsm6dso_config *cfg = data->dev->config;
-
-	return i2c_burst_read(data->bus, cfg->i2c_slv_addr,
-			      reg_addr, value, len);
-}
-
-static int lsm6dso_i2c_write(struct lsm6dso_data *data, uint8_t reg_addr,
-			     uint8_t *value, uint8_t len)
-{
-	const struct lsm6dso_config *cfg = data->dev->config;
-
-	return i2c_burst_write(data->bus, cfg->i2c_slv_addr,
-			       reg_addr, value, len);
-}
-
 int lsm6dso_i2c_init(const struct device *dev)
 {
 	struct lsm6dso_data *data = dev->data;
+	const struct lsm6dso_config *cfg = dev->config;
 
-	data->ctx_i2c.read_reg = (stmdev_read_ptr) lsm6dso_i2c_read,
-	data->ctx_i2c.write_reg = (stmdev_write_ptr) lsm6dso_i2c_write,
+	if (!device_is_ready(cfg->stmemsc_cfg.i2c.bus)) {
+		LOG_ERR("Cannot get pointer to bus device");
+		return -ENODEV;
+	}
 
-	data->ctx = &data->ctx_i2c;
-	data->ctx->handle = data;
+	/* Use generic stmemsc routine for read/write I2C bus */
+	data->ctx.read_reg = (stmdev_read_ptr) stmemsc_i2c_read,
+	data->ctx.write_reg = (stmdev_write_ptr) stmemsc_i2c_write,
+
+	data->ctx.handle = (void *)&cfg->stmemsc_cfg.i2c;
 
 	return 0;
 }
--- drivers/sensor/lsm6dso/lsm6dso_spi.c
+++ drivers/sensor/lsm6dso/lsm6dso_spi.c
@@ -10,117 +10,28 @@
 
 #define DT_DRV_COMPAT st_lsm6dso
 
-#include <string.h>
 #include "lsm6dso.h"
 #include <logging/log.h>
 
 #if DT_ANY_INST_ON_BUS_STATUS_OKAY(spi)
 
-#define LSM6DSO_SPI_READ		(1 << 7)
-
 LOG_MODULE_DECLARE(LSM6DSO, CONFIG_SENSOR_LOG_LEVEL);
-
-static int lsm6dso_spi_read(struct lsm6dso_data *data, uint8_t reg_addr,
-			    uint8_t *value, uint8_t len)
-{
-	const struct lsm6dso_config *cfg = data->dev->config;
-	const struct spi_config *spi_cfg = &cfg->spi_conf;
-	uint8_t buffer_tx[2] = { reg_addr | LSM6DSO_SPI_READ, 0 };
-	const struct spi_buf tx_buf = {
-			.buf = buffer_tx,
-			.len = 2,
-	};
-	const struct spi_buf_set tx = {
-		.buffers = &tx_buf,
-		.count = 1
-	};
-	const struct spi_buf rx_buf[2] = {
-		{
-			.buf = NULL,
-			.len = 1,
-		},
-		{
-			.buf = value,
-			.len = len,
-		}
-	};
-	const struct spi_buf_set rx = {
-		.buffers = rx_buf,
-		.count = 2
-	};
-
-
-	if (len > 64) {
-		return -EIO;
-	}
-
-	if (spi_transceive(data->bus, spi_cfg, &tx, &rx)) {
-		return -EIO;
-	}
-
-	return 0;
-}
-
-static int lsm6dso_spi_write(struct lsm6dso_data *data, uint8_t reg_addr,
-			     uint8_t *value, uint8_t len)
-{
-	const struct lsm6dso_config *cfg = data->dev->config;
-	const struct spi_config *spi_cfg = &cfg->spi_conf;
-	uint8_t buffer_tx[1] = { reg_addr & ~LSM6DSO_SPI_READ };
-	const struct spi_buf tx_buf[2] = {
-		{
-			.buf = buffer_tx,
-			.len = 1,
-		},
-		{
-			.buf = value,
-			.len = len,
-		}
-	};
-	const struct spi_buf_set tx = {
-		.buffers = tx_buf,
-		.count = 2
-	};
-
-
-	if (len > 64) {
-		return -EIO;
-	}
-
-	if (spi_write(data->bus, spi_cfg, &tx)) {
-		return -EIO;
-	}
-
-	return 0;
-}
 
 int lsm6dso_spi_init(const struct device *dev)
 {
 	struct lsm6dso_data *data = dev->data;
-
-	data->ctx_spi.read_reg = (stmdev_read_ptr) lsm6dso_spi_read;
-	data->ctx_spi.write_reg = (stmdev_write_ptr) lsm6dso_spi_write;
-
-	data->ctx = &data->ctx_spi;
-	data->ctx->handle = data;
-
-#if DT_INST_SPI_DEV_HAS_CS_GPIOS(0)
 	const struct lsm6dso_config *cfg = dev->config;
 
-	/* handle SPI CS thru GPIO if it is the case */
-	data->cs_ctrl.gpio_dev = device_get_binding(cfg->gpio_cs_port);
-	if (!data->cs_ctrl.gpio_dev) {
-		LOG_ERR("Unable to get GPIO SPI CS device");
+	if (!device_is_ready(cfg->stmemsc_cfg.spi.bus)) {
+		LOG_ERR("Cannot get pointer to bus device");
 		return -ENODEV;
 	}
 
-	data->cs_ctrl.gpio_pin = cfg->cs_gpio;
-	data->cs_ctrl.gpio_dt_flags = cfg->cs_gpio_flags;
-	data->cs_ctrl.delay = 0;
+	/* Use generic stmemsc routine for read/write SPI bus */
+	data->ctx.read_reg = (stmdev_read_ptr) stmemsc_spi_read;
+	data->ctx.write_reg = (stmdev_write_ptr) stmemsc_spi_write;
 
-	LOG_DBG("SPI GPIO CS configured on %s:%u",
-		cfg->gpio_cs_port, cfg->cs_gpio);
-#endif
+	data->ctx.handle = (void *)&cfg->stmemsc_cfg.spi;
 
 	return 0;
 }
