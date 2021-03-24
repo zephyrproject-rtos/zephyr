33a960b224ce845e54cd6b2cc19a2f6c133c7601
Maureen Helm <maureen.helm@nxp.com>
2021-03-23 21:54:46 -0500

    boards: arm: Configure frdm_k64f Arduino D9 pin as gpio
    Configures the pinmux for the Arduino D9 pin to allow using it as a
    gpio. This is needed by the adafruit_2_8_tft_touch_v2 shield for
    cmd-data-gpios.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- boards/arm/frdm_k64f/pinmux.c
+++ boards/arm/frdm_k64f/pinmux.c
@@ -166,6 +166,10 @@ static int frdm_k64f_pinmux_init(const struct device *dev)
 		       PORT_PCR_PE_MASK | PORT_PCR_PS_MASK);
 #endif
 
+#if CONFIG_SHIELD_ADAFRUIT_2_8_TFT_TOUCH_V2
+	pinmux_pin_set(portc,  4, PORT_PCR_MUX(kPORT_MuxAsGpio));
+#endif
+
 #if CONFIG_SHIELD_ADAFRUIT_WINC1500
 	/* IRQ, ENable, RST */
 	pinmux_pin_set(portc,  3, PORT_PCR_MUX(kPORT_MuxAsGpio));
