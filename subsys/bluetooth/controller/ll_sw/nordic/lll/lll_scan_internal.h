9995ec4c8b7311fc0e109fd37f0c2a3fad909803
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2021-03-25 17:32:57 +0100

    Bluetooth: controller: Use proper transmitWindowDelay for central
    On secondary advertising channel transmitWindowDelay depends on PHY
    used to create connection so we need to adjust it.
    
    Signed-off-by: Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan_internal.h
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan_internal.h
@@ -5,9 +5,9 @@
  */
 
 void lll_scan_prepare_connect_req(struct lll_scan *lll, struct pdu_adv *pdu_tx,
-				  uint8_t adv_tx_addr, uint8_t *adv_addr,
-				  uint8_t init_tx_addr, uint8_t *init_addr,
-				  uint32_t *conn_space_us);
+				  uint8_t phy, uint8_t adv_tx_addr,
+				  uint8_t *adv_addr, uint8_t init_tx_addr,
+				  uint8_t *init_addr, uint32_t *conn_space_us);
 
 bool lll_scan_adva_check(struct lll_scan *lll, uint8_t addr_type, uint8_t *addr,
 			 uint8_t rl_idx);
