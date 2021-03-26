558851867a8353b267a61603bc650724164a2496
Torbjörn Leksell <torbjorn.leksell@percepio.com>
2021-03-26 14:09:10 +0100

    Tracing: Timer tracing
    Add Timer tracing, default tracing hooks, and documentation.
    
    Signed-off-by: Torbjörn Leksell <torbjorn.leksell@percepio.com>

--- kernel/timer.c
+++ kernel/timer.c
@@ -98,6 +98,9 @@ void k_timer_init(struct k_timer *timer,
 
 	z_waitq_init(&timer->wait_q);
 	z_init_timeout(&timer->timeout);
+
+	SYS_PORT_TRACING_OBJ_INIT(k_timer, timer);
+
 	SYS_TRACING_OBJ_INIT(k_timer, timer);
 
 	timer->user_data = NULL;
@@ -109,6 +112,8 @@ void k_timer_init(struct k_timer *timer,
 void z_impl_k_timer_start(struct k_timer *timer, k_timeout_t duration,
 			  k_timeout_t period)
 {
+	SYS_PORT_TRACING_OBJ_FUNC(k_timer, start, timer);
+
 	if (K_TIMEOUT_EQ(duration, K_FOREVER)) {
 		return;
 	}
@@ -155,6 +160,8 @@ static inline void z_vrfy_k_timer_start(struct k_timer *timer,
 
 void z_impl_k_timer_stop(struct k_timer *timer)
 {
+	SYS_PORT_TRACING_OBJ_FUNC(k_timer, stop, timer);
+
 	int inactive = z_abort_timeout(&timer->timeout) != 0;
 
 	if (inactive) {
@@ -206,11 +213,15 @@ uint32_t z_impl_k_timer_status_sync(struct k_timer *timer)
 {
 	__ASSERT(!arch_is_in_isr(), "");
 
+	SYS_PORT_TRACING_OBJ_FUNC_ENTER(k_timer, status_sync, timer);
+
 	k_spinlock_key_t key = k_spin_lock(&lock);
 	uint32_t result = timer->status;
 
 	if (result == 0U) {
 		if (!z_is_inactive_timeout(&timer->timeout)) {
+			SYS_PORT_TRACING_OBJ_FUNC_BLOCKING(k_timer, status_sync, timer, K_FOREVER);
+
 			/* wait for timer to expire or stop */
 			(void)z_pend_curr(&lock, key, &timer->wait_q, K_FOREVER);
 
@@ -226,6 +237,11 @@ uint32_t z_impl_k_timer_status_sync(struct k_timer *timer)
 
 	timer->status = 0U;
 	k_spin_unlock(&lock, key);
+
+	/**
+	 * @note	New tracing hook
+	 */
+	SYS_PORT_TRACING_OBJ_FUNC_EXIT(k_timer, status_sync, timer, result);
 
 	return result;
 }
