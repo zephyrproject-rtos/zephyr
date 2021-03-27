27af48417e6a9f81e936fbc566ffcdde48155b21
Anas Nashif <anas.nashif@intel.com>
2021-03-27 10:33:33 -0400

    libc: snprintf/..: match implementation to declaration
    The identifiers used in the declaration and definition of a function
    shall be identical [MISRAC2012-RULE_8_3-b]
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- lib/libc/minimal/include/stdio.h
+++ lib/libc/minimal/include/stdio.h
@@ -32,19 +32,19 @@ typedef int  FILE;
 #define stderr ((FILE *) 3)
 
 int __printf_like(1, 2) printf(const char *_MLIBC_RESTRICT fmt, ...);
-int __printf_like(3, 4) snprintf(char *_MLIBC_RESTRICT s, size_t len,
+int __printf_like(3, 4) snprintf(char *_MLIBC_RESTRICT str, size_t len,
 				 const char *_MLIBC_RESTRICT fmt, ...);
-int __printf_like(2, 3) sprintf(char *_MLIBC_RESTRICT s,
+int __printf_like(2, 3) sprintf(char *_MLIBC_RESTRICT str,
 				const char *_MLIBC_RESTRICT fmt, ...);
 int __printf_like(2, 3) fprintf(FILE * _MLIBC_RESTRICT stream,
 				const char *_MLIBC_RESTRICT format, ...);
 
 
 int __printf_like(1, 0) vprintf(const char *_MLIBC_RESTRICT fmt, va_list list);
-int __printf_like(3, 0) vsnprintf(char *_MLIBC_RESTRICT s, size_t len,
+int __printf_like(3, 0) vsnprintf(char *_MLIBC_RESTRICT str, size_t len,
 				  const char *_MLIBC_RESTRICT fmt,
 				  va_list list);
-int __printf_like(2, 0) vsprintf(char *_MLIBC_RESTRICT s,
+int __printf_like(2, 0) vsprintf(char *_MLIBC_RESTRICT str,
 				 const char *_MLIBC_RESTRICT fmt, va_list list);
 int __printf_like(2, 0) vfprintf(FILE * _MLIBC_RESTRICT stream,
 				 const char *_MLIBC_RESTRICT format,
