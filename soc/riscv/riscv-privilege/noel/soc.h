bf43ad91bc48b8c37e65baa4e522c93139f3d403
Martin Åberg <martin.aberg@gaisler.com>
2021-03-29 16:36:30 +0200

    soc/riscv: Add support for the NOEL-V Processor
    This is a Zephyr SOC description for the NOEL-V RISC-V.
    
    Cobham Gaisler's NOEL-V RISC-V processor IP is available under GPL
    and commercial license and is described in more detail here:
      https://www.gaisler.com/NOELV
    
    The SOC uses the GRLIB APBUART serial driver "uart_apbuart.c".
    
    A separate commit adds board support.
    
    Signed-off-by: Martin Åberg <martin.aberg@gaisler.com>

---
+++ soc/riscv/riscv-privilege/noel/soc.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 Cobham Gaisler AB
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __RISCV_NOEL_SOC_H_
+#define __RISCV_NOEL_SOC_H_
+
+#include <soc_common.h>
+#include <devicetree.h>
+
+#define RISCV_MTIME_BASE             0xE000BFF8
+#define RISCV_MTIMECMP_BASE          0xE0004000
+
+#endif /* __RISCV_NOEL_SOC_H_ */
