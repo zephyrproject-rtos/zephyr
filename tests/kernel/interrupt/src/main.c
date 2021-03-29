c9c8cec3b6792165208493c5519d07b599458ed1
Enjia Mai <enjiax.mai@intel.com>
2021-03-29 22:27:13 +0800

    tests: interrupt: add test cases of direct interrupt for arch x86 and posix
    Add test cases of direct interrupt for arch x86 and posix.
    
    We register two direct interrupt at build time, then triggering
    interrupt and check if ISR handler has executed or not. We also
    check irq_enable and irq_disable works.
    
    Why we add an extra compiler option "-mgeneral-regs-only" to make
    it works in arch x86. because there might be some existing x87
    instructions executing inside interrupt context.
    
    Signed-off-by: Enjia Mai <enjiax.mai@intel.com>

--- tests/kernel/interrupt/src/main.c
+++ tests/kernel/interrupt/src/main.c
@@ -13,6 +13,7 @@ extern void test_isr_regular(void);
 extern void test_isr_offload_job_multiple(void);
 extern void test_isr_offload_job_identi(void);
 extern void test_isr_offload_job(void);
+extern void test_direct_interrupt(void);
 
 void test_main(void)
 {
@@ -23,7 +24,8 @@ void test_main(void)
 			ztest_unit_test(test_isr_regular),
 			ztest_unit_test(test_isr_offload_job_multiple),
 			ztest_unit_test(test_isr_offload_job_identi),
-			ztest_unit_test(test_isr_offload_job)
+			ztest_unit_test(test_isr_offload_job),
+			ztest_unit_test(test_direct_interrupt)
 			);
 	ztest_run_test_suite(interrupt_feature);
 }
