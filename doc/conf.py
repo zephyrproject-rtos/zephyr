cbe7a786350fb741df63dafcd1db00cc05d02b0e
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2021-03-30 10:30:00 +0200

    doc: remove commented options from config
    Remove options that are commented out from conf.py. It improves
    readability of the file.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- doc/conf.py
+++ doc/conf.py
@@ -38,9 +38,6 @@ except ImportError:
 
 # -- General configuration ------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -67,9 +64,6 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
 source_suffix = '.rst'
-
-# The encoding of source files.
-#source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
@@ -116,12 +110,6 @@ finally:
 # Usually you set "language" from the command line for these cases.
 language = None
 
-# There are two options for replacing |today|: either, you set today to some
-# non-false value, then it is used:
-#today = ''
-# Else, today_fmt is used as the format for a strftime call.
-#today_fmt = '%B %d, %Y'
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = ['_build']
@@ -140,18 +128,6 @@ else:
 # `name`
 
 default_role = 'any'
-# default_domain = 'cpp'
-
-# If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
-
-# If true, the current module name will be prepended to all description
-# unit titles (such as .. function::).
-#add_module_names = True
-
-# If true, sectionauthor and moduleauthor directives will be shown in the
-# output. They are ignored by default.
-#show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -160,12 +136,6 @@ pygments_style = 'sphinx'
 from lexer.DtsLexer import DtsLexer
 from sphinx.highlighting import lexers
 lexers['DTS'] = DtsLexer()
-
-# A list of ignored prefixes for module index sorting.
-#modindex_common_prefix = []
-
-# If true, keep warnings as "system message" paragraphs in the built documents.
-#keep_warnings = False
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -194,13 +164,6 @@ else:
 # "<project> v<release> documentation".
 html_title = "Zephyr Project Documentation"
 
-# This value determines the text for the permalink; it defaults to "¶".
-# Set it to None or the empty string to disable permalinks.
-#html_add_permalinks = ""
-
-# A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
-
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 html_logo = 'images/Zephyr-Kite-logo.png'
@@ -215,25 +178,9 @@ html_favicon = 'images/zp_favicon.png'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['{}/doc/_static'.format(ZEPHYR_BASE)]
 
-# Add any extra paths that contain custom files (such as robots.txt or
-# .htaccess) here, relative to this directory. These files are copied
-# directly to the root of the documentation.
-#html_extra_path = []
-
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 html_last_updated_fmt = '%b %d, %Y'
-
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-#html_use_smartypants =
-
-# Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
-
-# Additional templates that should be rendered to pages, maps page names to
-# template names.
-#html_additional_pages = {}
 
 # If false, no module index is generated.
 html_domain_indices = False
@@ -250,31 +197,10 @@ html_show_sourcelink = False
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 html_show_sphinx = False
 
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-# html_show_copyright = tags.has('development')
-
 # If true, license is shown in the HTML footer. Default is True.
 html_show_license = True
 
-# If true, an OpenSearch description file will be output, and all pages will
-# contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished HTML is served.
-#html_use_opensearch = ''
-
-# This is the file name suffix for HTML files (e.g. ".xhtml").
-#html_file_suffix = None
-
-# Language to be used for generating the HTML full-text search index.
-# Sphinx supports the following languages:
-#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
-#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-#html_search_language = 'en'
-
 sourcelink_suffix = '.txt'
-
-# A dictionary with options for the search language support, empty by default.
-# Now only 'ja' uses this config value
-#html_search_options = {'type': 'default'}
 
 # The name of a javascript file (relative to the configuration directory) that
 # implements a search results scorer. If empty, the default will be used.
@@ -443,17 +369,8 @@ html_redirect_pages = [
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
-
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
-
-# Additional stuff for the LaTeX preamble.
-'preamble': r'\setcounter{tocdepth}{2}',
-
-# Latex figure (float) alignment
-#'figure_align': 'htbp',
+    # Additional stuff for the LaTeX preamble.
+    'preamble': r'\setcounter{tocdepth}{2}',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
@@ -464,26 +381,6 @@ latex_documents = [
    u'many', 'manual'),
 ]
 
-# The name of an image file (relative to this directory) to place at the top of
-# the title page.
-#latex_logo = None
-
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-#latex_use_parts = False
-
-# If true, show page references after internal links.
-#latex_show_pagerefs = False
-
-# If true, show URL addresses after external links.
-#latex_show_urls = False
-
-# Documents to append as an appendix to all manuals.
-#latex_appendices = []
-
-# If false, no module index is generated.
-#latex_domain_indices = True
-
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
@@ -492,10 +389,6 @@ man_pages = [
     (master_doc, 'zephyr', u'Zephyr Project Documentation',
      [author], 1)
 ]
-
-# If true, show URL addresses after external links.
-#man_show_urls = False
-
 
 # -- Options for Texinfo output -------------------------------------------
 
@@ -507,18 +400,6 @@ texinfo_documents = [
    author, 'Zephyr', 'One line description of project.',
    'Miscellaneous'),
 ]
-
-# Documents to append as an appendix to all manuals.
-#texinfo_appendices = []
-
-# If false, no module index is generated.
-#texinfo_domain_indices = True
-
-# How to display URL addresses: 'footnote', 'no', or 'inline'.
-#texinfo_show_urls = 'footnote'
-
-# If true, do not generate a @detailmenu in the "Top" node's menu.
-#texinfo_no_detailmenu = False
 
 breathe_projects = {
     "Zephyr": "{}/doxygen/xml".format(ZEPHYR_BUILD),
