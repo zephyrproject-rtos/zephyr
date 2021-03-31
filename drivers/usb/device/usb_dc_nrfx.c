537d6ced87b8e6e09e608e70f1738bb2ba69520a
Peter Bigot <peter.bigot@nordicsemi.no>
2021-03-31 10:31:30 -0500

    usb: conversion of k_work API
    Replace all existing deprecated API with the recommended alternative.
    
    Be aware that this does not address architectural errors in the use
    of the work API.
    
    Fixes #34092
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- drivers/usb/device/usb_dc_nrfx.c
+++ drivers/usb/device/usb_dc_nrfx.c
@@ -1915,10 +1915,10 @@ static int usb_init(const struct device *arg)
 	(void)nrfx_power_init(&power_config);
 	nrfx_power_usbevt_init(&usbevt_config);
 
-	k_work_q_start(&usbd_work_queue,
-		usbd_work_queue_stack,
-		K_KERNEL_STACK_SIZEOF(usbd_work_queue_stack),
-		CONFIG_SYSTEM_WORKQUEUE_PRIORITY);
+	k_work_queue_start(&usbd_work_queue,
+			   usbd_work_queue_stack,
+			   K_KERNEL_STACK_SIZEOF(usbd_work_queue_stack),
+			   CONFIG_SYSTEM_WORKQUEUE_PRIORITY, NULL);
 
 	k_work_init(&ctx->usb_work, usbd_work_handler);
 
