d061c7d6abc3ebd92896ca24174a53321dbe9951
Shlomi Vaknin <shlomi.39sd@gmail.com>
2021-04-02 13:05:32 +0300

    fs: shell: add cat file command
    Add cat command to fs shell module.
    This command prints to the standard output
    the given files.
    
    Signed-off-by: Shlomi Vaknin <shlomi.39sd@gmail.com>

--- subsys/fs/shell.c
+++ subsys/fs/shell.c
@@ -334,6 +334,60 @@ static int cmd_read(const struct shell *shell, size_t argc, char **argv)
 	return 0;
 }
 
+static int cmd_cat(const struct shell *shell, size_t argc, char **argv)
+{
+	char path[MAX_PATH_LEN];
+	uint8_t buf[BUF_CNT];
+	struct fs_dirent dirent;
+	struct fs_file_t file;
+	int err;
+	ssize_t read;
+
+	fs_file_t_init(&file);
+
+	for (size_t i = 1; i < argc; ++i) {
+		create_abs_path(argv[i], path, sizeof(path));
+
+		err = fs_stat(path, &dirent);
+		if (err < 0) {
+			shell_error(shell, "Failed to obtain file %s (err: %d)",
+					path, err);
+			continue;
+		}
+
+		if (dirent.type != FS_DIR_ENTRY_FILE) {
+			shell_error(shell, "Note a file %s", path);
+			continue;
+		}
+
+		err = fs_open(&file, path, FS_O_READ);
+		if (err < 0) {
+			shell_error(shell, "Failed to open %s (%d)", path, err);
+			continue;
+		}
+
+		while (true) {
+			read = fs_read(&file, buf, sizeof(buf));
+			if (read <= 0) {
+				break;
+			}
+
+			for (int j = 0; j < read; j++) {
+				shell_fprintf(shell, SHELL_NORMAL, "%c", buf[j]);
+			}
+		}
+
+		if (read < 0) {
+			shell_error(shell, "Failed to read from file %s (err: %d)",
+				path, read);
+		}
+
+		fs_close(&file);
+	}
+
+	return 0;
+}
+
 static int cmd_statvfs(const struct shell *shell, size_t argc, char **argv)
 {
 	int err;
@@ -520,6 +574,9 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sub_fs,
 #endif
 	SHELL_CMD(pwd, NULL, "Print current working directory", cmd_pwd),
 	SHELL_CMD_ARG(read, NULL, "Read from file", cmd_read, 2, 255),
+	SHELL_CMD_ARG(cat, NULL,
+		"Concatenate files and print on the standard output",
+		cmd_cat, 2, 255),
 	SHELL_CMD_ARG(rm, NULL, "Remove file", cmd_rm, 2, 0),
 	SHELL_CMD_ARG(statvfs, NULL, "Show file system state", cmd_statvfs, 2, 0),
 	SHELL_CMD_ARG(trunc, NULL, "Truncate file", cmd_trunc, 2, 255),
