221ff1c1bab05bdaa35f7b19387165099827322d
Enjia Mai <enjiax.mai@intel.com>
2021-04-06 01:23:34 +0800

    tests: interrupt: add test cases of offloading job from isr
    Add 3 test cases to test offload job from isr, include:
    
    1. test_isr_offload_job_multiple()
    Validate the offloaded work executes immediately or not depends on its
    priority, and it offloads to different k_work.
    
    2. test_isr_offload_job_identi()
    Validate the offloaded work executes immediately or not depends on its
    priority, and it offloads to the identical k_work.
    
    3. test_isr_offload_job()
    Use dynamic interrupt instead of irq_offload() to verify the offloaded
    work.
    
    Signed-off-by: Enjia Mai <enjiax.mai@intel.com>

--- tests/kernel/interrupt/src/main.c
+++ tests/kernel/interrupt/src/main.c
@@ -9,13 +9,19 @@
 extern void test_isr_dynamic(void);
 extern void test_nested_isr(void);
 extern void test_prevent_interruption(void);
+extern void test_isr_offload_job_multiple(void);
+extern void test_isr_offload_job_identi(void);
+extern void test_isr_offload_job(void);
 
 void test_main(void)
 {
 	ztest_test_suite(interrupt_feature,
 			ztest_unit_test(test_isr_dynamic),
 			ztest_unit_test(test_nested_isr),
-			ztest_unit_test(test_prevent_interruption)
+			ztest_unit_test(test_prevent_interruption),
+			ztest_unit_test(test_isr_offload_job_multiple),
+			ztest_unit_test(test_isr_offload_job_identi),
+			ztest_unit_test(test_isr_offload_job)
 			);
 	ztest_run_test_suite(interrupt_feature);
 }
