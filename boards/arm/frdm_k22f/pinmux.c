79c54f8631e78c62120b376fb4d68b30cd49e5ff
Kumar Gala <kumar.gala@linaro.org>
2021-04-08 17:25:34 -0500

    boards: frdm_k22f: Fix SPI CS pinmux setting
    The chipselect is wired to PTC4 and is SPI0_PCS0.  Fix pinmux.c and
    board docs to correctly reflect this.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- boards/arm/frdm_k22f/pinmux.c
+++ boards/arm/frdm_k22f/pinmux.c
@@ -78,7 +78,7 @@ static int frdm_k22f_pinmux_init(const struct device *dev)
 
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(spi0), okay) && CONFIG_SPI
 	/* SPI0 CS0, SCK, SOUT, SIN */
-	pinmux_pin_set(portd, 4, PORT_PCR_MUX(kPORT_MuxAlt2));
+	pinmux_pin_set(portc, 4, PORT_PCR_MUX(kPORT_MuxAlt2));
 	pinmux_pin_set(portd, 1, PORT_PCR_MUX(kPORT_MuxAlt2));
 	pinmux_pin_set(portd, 2, PORT_PCR_MUX(kPORT_MuxAlt2));
 	pinmux_pin_set(portd, 3, PORT_PCR_MUX(kPORT_MuxAlt2));
