3e76b91a028fb44187e7e0f0bb565ef82470895c
Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>
2021-04-08 10:35:26 +0200

    Bluetooth: Audio - switch to upstream OTS server
    Use the upstream Object Transfer Service (OTS) server instead of the
    local (downstream) one.
    
    The local one was made because OTS did not exist upstream.  Since, an
    upstream version has been provided, to a large degree based upon the
    local one.
    
    After this, the local (downstream) OTS server may be removed.
    
    BT_OTS_TEMP -> BT_OTS in project file
    Also remove BT_DEBUG_OTS - new OTS does not have debug
    
    Signed-off-by: Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>

--- subsys/bluetooth/host/audio/mpl.h
+++ subsys/bluetooth/host/audio/mpl.h
@@ -182,24 +182,24 @@ struct mpl_tseg_t {
 
 /* Tracks */
 struct mpl_track_t {
-#if defined(CONFIG_BT_OTS_TEMP) || defined(CONFIG_BT_OTC)
+#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
 	uint64_t             id;
-#endif /* CONFIG_BT_OTS_TEMP || CONFIG_BT_OTC */
+#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
 	char                 title[CONFIG_BT_MCS_TRACK_TITLE_MAX];
 	int32_t              duration;
 	struct mpl_tseg_t   *segment;
-#if defined(CONFIG_BT_OTS_TEMP) || defined(CONFIG_BT_OTC)
+#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
 	uint64_t             segments_id;
-#endif /* CONFIG_BT_OTS_TEMP || CONFIG_BT_OTC */
+#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
 	struct mpl_track_t  *prev;
 	struct mpl_track_t  *next;
 };
 
 /* Groups */
 struct mpl_group_t {
-#if defined(CONFIG_BT_OTS_TEMP) || defined(CONFIG_BT_OTC)
+#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
 	uint64_t             id;
-#endif /* CONFIG_BT_OTS_TEMP || CONFIG_BT_OTC */
+#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
 	char                 title[CONFIG_BT_MCS_GROUP_TITLE_MAX];
 	struct mpl_track_t  *track;
 	struct mpl_group_t  *parent;
@@ -210,9 +210,9 @@ struct mpl_group_t {
 /** @brief Media Player */
 struct mpl_mediaplayer_t {
 	char                name[CONFIG_BT_MCS_MEDIA_PLAYER_NAME_MAX];
-#if defined(CONFIG_BT_OTS_TEMP) || defined(CONFIG_BT_OTC)
+#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
 	uint64_t            icon_id;
-#endif /* CONFIG_BT_OTS_TEMP || CONFIG_BT_OTC */
+#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
 	char                icon_uri[CONFIG_BT_MCS_ICON_URI_MAX];
 	struct mpl_group_t *group;
 	int32_t             track_pos;
@@ -222,9 +222,9 @@ struct mpl_mediaplayer_t {
 	uint8_t             playing_order;
 	uint16_t            playing_orders_supported;
 	uint32_t            operations_supported;
-#if defined(CONFIG_BT_OTS_TEMP) || defined(CONFIG_BT_OTC)
+#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
 	uint64_t            search_results_id;
-#endif /* CONFIG_BT_OTS_TEMP || CONFIG_BT_OTC */
+#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
 	uint8_t             content_ctrl_id;
 };
 
@@ -242,7 +242,7 @@ struct mpl_op_ntf_t {
 	uint8_t result_code;
 };
 
-#if defined(CONFIG_BT_OTS_TEMP) || defined(CONFIG_BT_OTC)
+#if defined(CONFIG_BT_OTS) || defined(CONFIG_BT_OTC)
 /** @brief Search control item */
 struct mpl_sci_t {
 	uint8_t len;                     /* Length of type and parameter */
@@ -255,15 +255,15 @@ struct mpl_search_t {
 	uint8_t len;
 	char    search[SEARCH_LEN_MAX]; /* Concatenated search control items */
 };	                                /* - (type, length, param) */
-#endif /* CONFIG_BT_OTS_TEMP || CONFIG_BT_OTC */
+#endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
 
 int mpl_init(void);
 
 char *mpl_player_name_get(void);
 
-#ifdef CONFIG_BT_OTS_TEMP
+#ifdef CONFIG_BT_OTS
 uint64_t mpl_icon_id_get(void);
-#endif /* CONFIG_BT_OTS_TEMP */
+#endif /* CONFIG_BT_OTS */
 
 char *mpl_icon_uri_get(void);
 
@@ -286,7 +286,7 @@ void mpl_playback_speed_cb(int8_t speed);
 int8_t mpl_seeking_speed_get(void);
 void mpl_seeking_speed_cb(int8_t speed);
 
-#ifdef CONFIG_BT_OTS_TEMP
+#ifdef CONFIG_BT_OTS
 uint64_t mpl_track_segments_id_get(void);
 
 uint64_t mpl_current_track_id_get(void);
@@ -307,7 +307,7 @@ void mpl_parent_group_id_cb(uint64_t id);
 /* For  IOP testing - set current group to be it's own parent */
 void mpl_test_unset_parent_group(void);
 #endif /* CONFIG_BT_DEBUG_MCS && CONFIG_BT_TESTING */
-#endif /* CONFIG_BT_OTS_TEMP */
+#endif /* CONFIG_BT_OTS */
 
 uint8_t mpl_playing_order_get(void);
 void mpl_playing_order_set(uint8_t order);
@@ -327,13 +327,13 @@ void mpl_operation_cb(struct mpl_op_ntf_t op_ntf);
 uint32_t mpl_operations_supported_get(void);
 void mpl_operations_supported_cb(uint32_t operations);
 
-#ifdef CONFIG_BT_OTS_TEMP
+#ifdef CONFIG_BT_OTS
 void mpl_scp_set(struct mpl_search_t search);
 void mpl_search_cb(uint8_t result_code);
 
 uint64_t mpl_search_results_id_get(void);
 void mpl_search_results_id_cb(uint64_t id);
-#endif /* CONFIG_BT_OTS_TEMP */
+#endif /* CONFIG_BT_OTS */
 
 uint8_t mpl_content_ctrl_id_get(void);
 
@@ -342,7 +342,7 @@ uint8_t mpl_content_ctrl_id_get(void);
 void mpl_debug_dump_state(void);
 #endif /* CONFIG_BT_DEBUG_MCS */
 
-struct ots_svc_inst_t *bt_mcs_get_ots(void);
+struct bt_ots *bt_mcs_get_ots(void);
 
 #ifdef __cplusplus
 }
