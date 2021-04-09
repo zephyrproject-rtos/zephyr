b9841ed02b7bb1a44b6aef0f86a6249628314309
Carlo Caione <ccaione@baylibre.com>
2021-04-09 16:39:46 +0200

    board: arm64: Add FVP Base RevC 2xAEMv8A board
    Add support for the FVP Base RevC 2xAEMv8A board to be emulated using
    the same FVP. For now the virtual platform is only exposing one core and
    the basic set of peripherals (GICv3, ARM arch timer, PL011, etc...).
    
    INFO    - Total complete:  256/ 256  100%  skipped:  933, failed:    0
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

---
+++ boards/arm64/fvp_base_revc_2xaemv8a/board.cmake
@@ -0,0 +1,17 @@
+# Copyright (c) 2021 Carlo Caione <ccaione@baylibre.com>
+# SPDX-License-Identifier: Apache-2.0
+
+set(EMU_PLATFORM armfvp)
+set(ARMFVP_BIN_NAME FVP_Base_RevC-2xAEMvA)
+
+set(ARMFVP_FLAGS
+  -C bp.secure_memory=0
+  -C cluster0.NUM_CORES=1
+  -C bp.refcounter.non_arch_start_at_default=1
+  -C bp.pl011_uart0.out_file=-
+  -C bp.pl011_uart0.unbuffered_output=1
+  -C bp.terminal_0.start_telnet=0
+  -C bp.vis.disable_visualisation=1
+  -C bp.vis.rate_limit-enable=0
+  -C gic_distributor.ARE-fixed-to-one=1
+  )
