9d044923613fc273f6a12180ceee0b092d580ce5
Kumar Gala <kumar.gala@linaro.org>
2021-04-09 07:59:51 -0500

    bluetooth: Fix build issue with <bluetooth/mesh/access.h>
    Add include <bluetooth/mesh/msg.h> into <bluetooth/mesh/access.h> to get
    definition of 'struct bt_mesh_msg_ctx'.
    
    We see this when trying to build the mesh_badge sample for the
    reel_board.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- include/bluetooth/mesh/access.h
+++ include/bluetooth/mesh/access.h
@@ -12,6 +12,7 @@
 
 #include <settings/settings.h>
 #include <sys/util.h>
+#include <bluetooth/mesh/msg.h>
 
 /* Internal macros used to initialize array members */
 #define BT_MESH_KEY_UNUSED_ELT_(IDX, _) BT_MESH_KEY_UNUSED,
