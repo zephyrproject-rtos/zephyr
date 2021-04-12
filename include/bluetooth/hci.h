b6217e1bec999c8133a21c34701d4bf0d3536099
Mark Wang <yichang.wang@nxp.com>
2021-04-12 20:47:07 +0800

    Bluetooth: hci: add HCI_Write_Class_Of_Device
    add the op code and the parameter structure.
    
    Signed-off-by: Mark Wang <yichang.wang@nxp.com>

--- include/bluetooth/hci.h
+++ include/bluetooth/hci.h
@@ -484,6 +484,11 @@ struct bt_hci_write_local_name {
 #define BT_BREDR_SCAN_INQUIRY                   0x01
 #define BT_BREDR_SCAN_PAGE                      0x02
 
+#define BT_HCI_OP_WRITE_CLASS_OF_DEVICE         BT_OP(BT_OGF_BASEBAND, 0x0024)
+struct bt_hci_cp_write_class_of_device {
+	uint8_t  class_of_device[3];
+} __packed;
+
 #define BT_TX_POWER_LEVEL_CURRENT               0x00
 #define BT_TX_POWER_LEVEL_MAX                   0x01
 #define BT_HCI_OP_READ_TX_POWER_LEVEL           BT_OP(BT_OGF_BASEBAND, 0x002d)
