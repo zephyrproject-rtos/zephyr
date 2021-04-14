b8fb353cd465102980ae5ca6ab2d4f398e13e474
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2021-04-14 13:35:29 +0200

    kernel: Move k_busy_wait from thread to timeout
    K_busy_wait is the only function from thread.c that is used when
    CONFIG_MULTITHREADING=n. Moving to timeout since it fits better there
    as it requires sys clock to be present.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

--- kernel/thread.c
+++ kernel/thread.c
@@ -120,44 +120,6 @@ bool z_is_thread_essential(void)
 	return (_current->base.user_options & K_ESSENTIAL) == K_ESSENTIAL;
 }
 
-#ifdef CONFIG_SYS_CLOCK_EXISTS
-void z_impl_k_busy_wait(uint32_t usec_to_wait)
-{
-	if (usec_to_wait == 0U) {
-		return;
-	}
-
-#if !defined(CONFIG_ARCH_HAS_CUSTOM_BUSY_WAIT)
-	uint32_t start_cycles = k_cycle_get_32();
-
-	/* use 64-bit math to prevent overflow when multiplying */
-	uint32_t cycles_to_wait = (uint32_t)(
-		(uint64_t)usec_to_wait *
-		(uint64_t)sys_clock_hw_cycles_per_sec() /
-		(uint64_t)USEC_PER_SEC
-	);
-
-	for (;;) {
-		uint32_t current_cycles = k_cycle_get_32();
-
-		/* this handles the rollover on an unsigned 32-bit value */
-		if ((current_cycles - start_cycles) >= cycles_to_wait) {
-			break;
-		}
-	}
-#else
-	arch_busy_wait(usec_to_wait);
-#endif /* CONFIG_ARCH_HAS_CUSTOM_BUSY_WAIT */
-}
-
-#ifdef CONFIG_USERSPACE
-static inline void z_vrfy_k_busy_wait(uint32_t usec_to_wait)
-{
-	z_impl_k_busy_wait(usec_to_wait);
-}
-#include <syscalls/k_busy_wait_mrsh.c>
-#endif /* CONFIG_USERSPACE */
-#endif /* CONFIG_SYS_CLOCK_EXISTS */
 
 #ifdef CONFIG_THREAD_CUSTOM_DATA
 void z_impl_k_thread_custom_data_set(void *value)
