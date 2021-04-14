zephyrproject-rtos/zephyr

commit 2ca0b0163fd1170d937ef8c52f34f46bb8038f59

Bluetooth: controller: legacy: Validate chan map and hop value
Add validation of channel map and hop increment value
received in CONNECT_IND PDU.

Zero bit count leads to controller assert or divide-by-zero
fault.

Hop increment shall be between 5 and 16 by BT Specification.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/ctrl.c
@@ -1054,7 +1054,13 @@ static inline u32_t isr_rx_adv(u8_t devmatch_ok, u8_t devmatch_id,
 		conn->data_chan_count =
 			util_ones_count_get(&conn->data_chan_map[0],
 					    sizeof(conn->data_chan_map));
+		if (conn->data_chan_count < 2) {
+			return 1;
+		}
 		conn->data_chan_hop = pdu_adv->connect_ind.hop;
+		if ((conn->data_chan_hop < 5) || (conn->data_chan_hop > 16)) {
+			return 1;
+		}
 		conn->conn_interval =
 			pdu_adv->connect_ind.interval;
 		conn_interval_us =

commit 277435b7515333799671967a5d54c2ed6232daa1

Bluetooth: controller: legacy: Fix channel map check regression
Fix channel map check regression introduced in
commit 2ca0b0163fd1 ("Bluetooth: controller: legacy:
Validate chan map and hop value"), that caused further
connection initiation to fail.

Relates to commit 94d5f0854e49 ("Bluetooth: controller:
fixing error re. all zero chmap in conn-ind").

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/ctrl.c
@@ -1037,7 +1037,6 @@ static inline u32_t isr_rx_adv(u8_t devmatch_ok, u8_t devmatch_id,
 
 		/* acquire the slave context from advertiser */
 		conn = _radio.advertiser.conn;
-		_radio.advertiser.conn = NULL;
 
 		/* Populate the slave context */
 		conn->handle = mem_index_get(conn, _radio.conn_pool,
@@ -1054,13 +1053,14 @@ static inline u32_t isr_rx_adv(u8_t devmatch_ok, u8_t devmatch_id,
 		conn->data_chan_count =
 			util_ones_count_get(&conn->data_chan_map[0],
 					    sizeof(conn->data_chan_map));
-		if (conn->data_chan_count < 2) {
-			return 1;
-		}
 		conn->data_chan_hop = pdu_adv->connect_ind.hop;
-		if ((conn->data_chan_hop < 5) || (conn->data_chan_hop > 16)) {
+		if ((conn->data_chan_count < 2) || (conn->data_chan_hop < 5) ||
+		    (conn->data_chan_hop > 16)) {
 			return 1;
 		}
+
+		_radio.advertiser.conn = NULL;
+
 		conn->conn_interval =
 			pdu_adv->connect_ind.interval;
 		conn_interval_us =