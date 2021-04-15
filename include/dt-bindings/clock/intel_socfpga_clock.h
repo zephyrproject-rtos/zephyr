34cc99a2d9997fdfd0c90b6fe996b0cb7db44866
Siew Chin Lim <elly.siew.chin.lim@intel.com>
2021-04-15 22:06:38 +0800

    drivers: clock_control: Add clock driver for Intel SoC FPGA Agilex
    Add clock driver for Intel SoC FPGA Agilex.
    
    Signed-off-by: Siew Chin Lim <elly.siew.chin.lim@intel.com>

---
+++ include/dt-bindings/clock/intel_socfpga_clock.h
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (C) 2021, Intel Corporation
+ *
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_INTEL_SOCFPGA_CLOCK_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_INTEL_SOCFPGA_CLOCK_H_
+
+#define INTEL_SOCFPGA_CLOCK_MPU        0
+#define INTEL_SOCFPGA_CLOCK_WDT        1
+#define INTEL_SOCFPGA_CLOCK_UART       2
+#define INTEL_SOCFPGA_CLOCK_MMC        3
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_INTEL_SOCFPGA_CLOCK_H_ */
