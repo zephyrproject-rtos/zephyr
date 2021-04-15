adf900f014f9077ff1c42256b46c208a846d219b
Piotr Pryga <piotr.pryga@nordicsemi.no>
2021-04-15 17:25:52 +0200

    tests: Bluetooth: df: Move UT code into connectionless_cte_tx sub dir
    Before thic commit the bluetooth/df unit tests subdirectory contained
    connectionless CTE TX verification. Due to the implementation of
    connectionless CTE RX new set of tests will be provided.
    The new functionality requires different: configuration and common
    setup code. Because of that, current test code was moved to
    connectionless_cte_tx subdirectory.
    Code shared by all DF tests is moved to common subdirectory.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

--- tests/bluetooth/df/connectionless_cte_tx/src/main.c
+++ tests/bluetooth/df/connectionless_cte_tx/src/main.c
@@ -1,20 +1,21 @@
 /*
- * Copyright (c) 2020 Nordic Semiconductor ASA
+ * Copyright (c) 2020-2021 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <stdint.h>
 
+#include <bt_common.h>
 #include "common.h"
 #include "test_set_cl_cte_tx_params.h"
 #include "test_set_cl_cte_tx_enable.h"
 
-
 /*test case main entry*/
 void test_main(void)
 {
-	common_setup();
+	ut_bt_setup();
+
 	common_create_adv_set();
 	run_set_cl_cte_tx_params_tests();
 	common_delete_adv_set();
