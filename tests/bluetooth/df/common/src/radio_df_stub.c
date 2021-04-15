bf7bf0bec9d54f05ba56aedf5ae4eeafe51cace3
Piotr Pryga <piotr.pryga@nordicsemi.no>
2021-04-15 19:50:28 +0200

    tests: Bluetooth: df: Add connectionless IQ sampling enable unit tests
    Add unit tests for connectionless IQ sampling functionality in
    controller.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

--- tests/bluetooth/df/common/src/radio_df_stub.c
+++ tests/bluetooth/df/common/src/radio_df_stub.c
@@ -4,6 +4,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include <stdint.h>
+#include <stddef.h>
+
+/* global variables that may be used to imitate real radio behaviour */
+static uint32_t g_iq_samples_amount;
+static uint8_t g_cte_status;
+static uint8_t g_pdu_switch_pattern;
 
 /* Stub functions that does nothing. Just to avoid liker complains. */
 
@@ -58,4 +64,47 @@ void radio_df_mode_set_aoa(void)
 
 void radio_df_mode_set_aod(void)
 {
+}
+
+/* CTE RX related functions */
+void radio_df_cte_rx_2us_switching(void)
+{
+}
+
+void radio_df_cte_rx_4us_switching(void)
+{
+}
+
+void radio_df_iq_data_packet_set(uint8_t *buffer, size_t len)
+{
+}
+
+void ut_radio_df_iq_samples_amount_set(uint32_t amount)
+{
+	g_iq_samples_amount = amount;
+}
+
+uint32_t radio_df_iq_samples_amount_get(void)
+{
+	return g_iq_samples_amount;
+}
+
+void ut_radio_df_cte_status_set(uint8_t status)
+{
+	g_cte_status = status;
+}
+
+uint8_t radio_df_cte_status_get(void)
+{
+	return g_cte_status;
+}
+
+void ut_radio_df_pdu_antenna_switch_pattern_set(uint8_t pattern)
+{
+	g_pdu_switch_pattern = pattern;
+}
+
+uint8_t radio_df_pdu_antenna_switch_pattern_get(void)
+{
+	return g_pdu_switch_pattern;
 }
