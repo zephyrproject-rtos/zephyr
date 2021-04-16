932e39e00aede52b318a9ed9e9486743b2bb9265
Flavio Ceolin <flavio.ceolin@intel.com>
2021-04-16 16:31:52 -0700

    toolchain: llvm: Find strip command
    Some targets require *strip* command. This command was not
    being set when selecting llvm toolchain.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- cmake/bintools/llvm/target.cmake
+++ cmake/bintools/llvm/target.cmake
@@ -11,6 +11,7 @@ find_program(CMAKE_AR      llvm-ar      ${find_program_clang_args}   )
 find_program(CMAKE_NM      llvm-nm      ${find_program_clang_args}   )
 find_program(CMAKE_OBJDUMP llvm-objdump ${find_program_clang_args}   )
 find_program(CMAKE_RANLIB  llvm-ranlib  ${find_program_clang_args}   )
+find_program(CMAKE_STRIP   llvm-strip   ${find_program_clang_args}   )
 find_program(CMAKE_OBJCOPY objcopy      ${find_program_binutils_args})
 find_program(CMAKE_READELF readelf      ${find_program_binutils_args})
 
