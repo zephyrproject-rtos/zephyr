707dc22fb06a60a69329c04a299a30d36fb160eb
Peter Bigot <peter.bigot@nordicsemi.no>
2021-04-16 11:48:50 -0500

    kernel: fix error in synchronous work cancellation return value
    The return value is documented to be true if the work was pending, but
    the implementation returned true only if the work was actually running
    (i.e. the caller had to wait).  It should also return true if
    scheduled or submitted work was cancelled.
    
    Note that this means the return value cannot be used to determine
    whether the call slept.
    
    Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>

--- kernel/work.c
+++ kernel/work.c
@@ -524,10 +524,13 @@ bool k_work_cancel_sync(struct k_work *work,
 
 	struct z_work_canceller *canceller = &sync->canceller;
 	k_spinlock_key_t key = k_spin_lock(&lock);
+	bool pending = (work_busy_get_locked(work) != 0U);
+	bool need_wait = false;
 
-	(void)cancel_async_locked(work);
-
-	bool need_wait = cancel_sync_locked(work, canceller);
+	if (pending) {
+		(void)cancel_async_locked(work);
+		need_wait = cancel_sync_locked(work, canceller);
+	}
 
 	k_spin_unlock(&lock, key);
 
@@ -535,7 +538,7 @@ bool k_work_cancel_sync(struct k_work *work,
 		k_sem_take(&canceller->sem, K_FOREVER);
 	}
 
-	return need_wait;
+	return pending;
 }
 
 /* Work has been dequeued and is about to be invoked by the work
@@ -983,10 +986,13 @@ bool k_work_cancel_delayable_sync(struct k_work_delayable *dwork,
 
 	struct z_work_canceller *canceller = &sync->canceller;
 	k_spinlock_key_t key = k_spin_lock(&lock);
+	bool pending = (work_delayable_busy_get_locked(dwork) != 0U);
+	bool need_wait = false;
 
-	(void)cancel_delayable_async_locked(dwork);
-
-	bool need_wait = cancel_sync_locked(&dwork->work, canceller);
+	if (pending) {
+		(void)cancel_delayable_async_locked(dwork);
+		need_wait = cancel_sync_locked(&dwork->work, canceller);
+	}
 
 	k_spin_unlock(&lock, key);
 
@@ -994,7 +1000,7 @@ bool k_work_cancel_delayable_sync(struct k_work_delayable *dwork,
 		k_sem_take(&canceller->sem, K_FOREVER);
 	}
 
-	return need_wait;
+	return pending;
 }
 
 bool k_work_flush_delayable(struct k_work_delayable *dwork,
