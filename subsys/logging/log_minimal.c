8cbda26cdd5fde0f0512f7da211cb45a483c3ffa
Flavio Ceolin <flavio.ceolin@intel.com>
2021-04-19 09:36:56 -0700

    logging: log_minimal: Fix violations to rule 10.4
    Logical AND operator accepts only unsigned type and operands.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- subsys/logging/log_minimal.c
+++ subsys/logging/log_minimal.c
@@ -29,7 +29,7 @@ static void minimal_hexdump_line_print(const char *data, size_t length)
 {
 	for (size_t i = 0U; i < HEXDUMP_BYTES_IN_LINE; i++) {
 		if (i < length) {
-			printk("%02x ", data[i] & 0xFF);
+			printk("%02x ", (unsigned char)data[i] & 0xFFu);
 		} else {
 			printk("   ");
 		}
