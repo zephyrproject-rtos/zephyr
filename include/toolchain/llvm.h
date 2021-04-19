1b95d96a720746d8456c079aaf2d44b5e65f3e77
Anas Nashif <anas.nashif@intel.com>
2021-04-19 00:12:57 -0400

    toolchain: add llvm abstraction
    Add abstraction for llvm to allow for toolchain customizations that are
    different from the gcc defaults.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- include/toolchain/llvm.h
+++ include/toolchain/llvm.h
@@ -7,18 +7,9 @@
 #ifndef ZEPHYR_INCLUDE_TOOLCHAIN_LLVM_H_
 #define ZEPHYR_INCLUDE_TOOLCHAIN_LLVM_H_
 
-#ifndef _LINKER
-#if defined(_ASMLANGUAGE)
-
-#include <toolchain/common.h>
-
-#else /* defined(_ASMLANGUAGE) */
 
 #define __no_optimization __attribute__((optnone))
-
 #include <toolchain/gcc.h>
-#endif /* _ASMLANGUAGE */
 
-#endif /* !_LINKER */
 
 #endif /* ZEPHYR_INCLUDE_TOOLCHAIN_LLVM_H_ */
