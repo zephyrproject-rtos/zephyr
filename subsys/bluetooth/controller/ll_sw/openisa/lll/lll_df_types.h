36043dd123b689f3f6e32e1b29c1c757fa9259b8
Piotr Pryga <piotr.pryga@nordicsemi.no>
2021-04-23 07:52:53 +0200

    Bluetooth: controller: Fix build error for non Nordic plaforms
    Direction finding is implemented only for Nordic link layer.
    Build was failing due to missing header files for Openisa (RiskV).
    Added dummy lll_df_types.h header file to OpenISA lower link layer.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

---
+++ subsys/bluetooth/controller/ll_sw/openisa/lll/lll_df_types.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* This is currently almost empty file to avoid build errors due to missing header files.
+ * The file shall be used if Direction Finding support is added to OpenISA lower link layer.
+ */
+
+/* Forward declaration of the type to avoid build errors in ULL code. */
+struct lll_df_sync;
