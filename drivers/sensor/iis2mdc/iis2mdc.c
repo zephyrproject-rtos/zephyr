87a8119105e5eea5bfc1c4a273489f03bc83452e
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2021-04-24 11:04:57 +0200

    drivers: sensor: iis2mdc: use GPIO_DT_SPEC_INST_GET
    Replace GPIO_DT_SPEC_GET for GPIO_DT_SPEC_INST_GET.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- drivers/sensor/iis2mdc/iis2mdc.c
+++ drivers/sensor/iis2mdc/iis2mdc.c
@@ -345,7 +345,7 @@ static int iis2mdc_init(const struct device *dev)
 
 #ifdef CONFIG_IIS2MDC_TRIGGER
 #define IIS2MDC_CFG_IRQ(inst) \
-	.gpio_drdy = GPIO_DT_SPEC_GET(DT_DRV_INST(inst), drdy_gpios),
+	.gpio_drdy = GPIO_DT_SPEC_INST_GET(inst, drdy_gpios),
 #else
 #define IIS2MDC_CFG_IRQ(inst)
 #endif /* CONFIG_IIS2MDC_TRIGGER */
