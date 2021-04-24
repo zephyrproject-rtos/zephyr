3ef8ca29de14a00e445f02abc4e8f73ab543cefd
Jordan Yates <jordan.yates@data61.csiro.au>
2021-04-24 13:01:11 +1000

    linker: devicetree_regions: autogen memory regions
    Adds a macro that automatically generate memory regions for all
    devicetree nodes with a compatible matching 'linkable-memory'.
    
    Signed-off-by: Jordan Yates <jordan.yates@data61.csiro.au>

--- include/linker/devicetree_regions.h
+++ include/linker/devicetree_regions.h
@@ -12,6 +12,16 @@
 	ORIGIN = DT_REG_ADDR(node),		       \
 	LENGTH = DT_REG_SIZE(node)
 
+/* Automatically generate a region from a devicetree node */
+#define _REGION_FROM_NODE(attr, node)				   \
+	COND_CODE_1(DT_NODE_HAS_PROP(node, label),		   \
+		    (_REGION_DECLARE(DT_LABEL(node), attr, node)), \
+		    (_REGION_DECLARE(DT_NODE_FULL_NAME(node), attr, node)))
+
+/* Read-Write memory region from a 'linkable-memory' compatible */
+#define _LINKABLE_MEMORY_DECLARE(inst) \
+	_REGION_FROM_NODE(rw, DT_INST(inst, linkable_memory))
+
 /**
  * @brief Generate a linker memory region from a devicetree node
  *
@@ -24,3 +34,9 @@
 	COND_CODE_1(DT_NODE_HAS_STATUS(node, okay),	  \
 		    (_REGION_DECLARE(name, attr, node)),  \
 		    ())
+
+/**
+ * @brief Generate a linker memory region for each 'linkable-memory' compatible
+ */
+#define DT_LINKABLE_MEMORY_REGIONS() \
+	DT_COMPAT_FOREACH_STATUS_OKAY(linkable_memory, _LINKABLE_MEMORY_DECLARE)
