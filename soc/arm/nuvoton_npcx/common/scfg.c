e5aa9b0e34990e1a8f322d93b90e52d8350ff691
Mulin Chao <mlchao@nuvoton.com>
2021-04-27 01:09:05 -0700

    soc: common: npcx: move power.c and registers.c to common folder.
    This CL moves power policy source file for all npcx series to common
    folder. It also abstracts the sources for checking register structures
    to common/registers.c to avoid the duplicated sources in the later npcx
    series.
    
    Signed-off-by: Mulin Chao <mlchao@nuvoton.com>

--- soc/arm/nuvoton_npcx/common/scfg.c
+++ soc/arm/nuvoton_npcx/common/scfg.c
@@ -214,13 +214,12 @@ static int npcx_scfg_init(const struct device *dev)
 {
 	struct scfg_reg *inst_scfg = HAL_SFCG_INST();
 
-#if defined(CONFIG_SOC_SERIES_NPCX7)
 	/*
 	 * Set bit 7 of DEVCNT again for npcx7 series. Please see Errata
 	 * for more information. It will be fixed in next chip.
 	 */
-	inst_scfg->DEVCNT |= BIT(7);
-#endif
+	if (IS_ENABLED(CONFIG_SOC_SERIES_NPCX7))
+		inst_scfg->DEVCNT |= BIT(7);
 
 	/* Change all pads whose default functionality isn't IO to GPIO */
 	npcx_pinctrl_mux_configure(def_alts, ARRAY_SIZE(def_alts), 0);
