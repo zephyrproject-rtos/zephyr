2d0b976ff58be70b04a7c1ff94353d04c852f9a6
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2021-04-27 15:33:45 +0200

    power: provide old reboot header for compatibility
    Make out-of-tree applications transition easier by providing the same
    header as before pointing to the new one. Once Zephyr modules are also
    migrated this header should produce a deprecation warning if included.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

---
+++ include/power/reboot.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_POWER_REBOOT_H_
+#define ZEPHYR_INCLUDE_POWER_REBOOT_H_
+
+/* NOTE: This header is kept for compatibility but will be deprecated soon */
+
+#include <sys/reboot.h>
+
+#endif /* ZEPHYR_INCLUDE_POWER_REBOOT_H_ */
