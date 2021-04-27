b6f9d212fb1d47633b083cafd8e2ee3500cec2ea
Mulin Chao <mlchao@nuvoton.com>
2021-04-27 01:20:40 -0700

    soc: npcx: add soc drivers for npcx9 series.
    This CL adds the soc drivers for npcx9 series. There are two soc part
    numbers, npcx9m3f and npcx9m6f, are introduced. We also move soc family
    configurations from /nuvoton_npcx/npcx7/Kconfig.soc to
    nuvoton_npcx/Kconfig.
    
    Signed-off-by: Jun Lin <CHLin56@nuvoton.com>
    Signed-off-by: Mulin Chao <mlchao@nuvoton.com>

---
+++ soc/arm/nuvoton_npcx/npcx9/soc.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2021 Nuvoton Technology Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <kernel.h>
+#include <device.h>
+#include <init.h>
+#include <soc.h>
+
+static int soc_init(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	return 0;
+}
+
+SYS_INIT(soc_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
