ab5cabbf7d17b5fdc42c75968b1031547cd69f76
Flavio Ceolin <flavio.ceolin@intel.com>
2021-04-26 22:37:42 -0700

    power: device_pm: Fix concurrence issues
    The sync API was using k_poll_signal and in certain conditions is
    possible multiple threads waiting on a signal leading to an undefined
    behavior.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- subsys/power/device_pm.c
+++ subsys/power/device_pm.c
@@ -39,7 +39,11 @@ static void device_pm_callback(const struct device *dev,
 			   DEVICE_PM_STATE_SUSPENDED);
 	}
 
-	k_work_submit(&dev->pm->work);
+	/*
+	 * This function returns the number of woken threads on success. There
+	 * is nothing we can do with this information. Just ignoring it.
+	 */
+	(void)k_condvar_broadcast(&dev->pm->condvar);
 }
 
 static void pm_work_handler(struct k_work *work)
@@ -48,7 +52,6 @@ static void pm_work_handler(struct k_work *work)
 					struct device_pm, work);
 	const struct device *dev = pm->dev;
 	int ret = 0;
-	uint8_t pm_state;
 
 	switch (atomic_get(&dev->pm->fsm_state)) {
 	case DEVICE_PM_STATE_ACTIVE:
@@ -60,7 +63,6 @@ static void pm_work_handler(struct k_work *work)
 						DEVICE_PM_SUSPEND_STATE,
 						device_pm_callback, NULL);
 		} else {
-			pm_state = DEVICE_PM_ACTIVE_STATE;
 			goto fsm_out;
 		}
 		break;
@@ -73,7 +75,6 @@ static void pm_work_handler(struct k_work *work)
 						DEVICE_PM_ACTIVE_STATE,
 						device_pm_callback, NULL);
 		} else {
-			pm_state = DEVICE_PM_SUSPEND_STATE;
 			goto fsm_out;
 		}
 		break;
@@ -86,17 +87,20 @@ static void pm_work_handler(struct k_work *work)
 	}
 
 	__ASSERT(ret == 0, "Set Power state error");
-
 	return;
 
 fsm_out:
-	k_poll_signal_raise(&dev->pm->signal, pm_state);
+	/*
+	 * This function returns the number of woken threads on success. There
+	 * is nothing we can do with this information. Just ignoring it.
+	 */
+	(void)k_condvar_broadcast(&dev->pm->condvar);
 }
 
 static int device_pm_request(const struct device *dev,
 			     uint32_t target_state, uint32_t pm_flags)
 {
-	int result, signaled = 0;
+	struct k_mutex request_mutex;
 
 	__ASSERT((target_state == DEVICE_PM_ACTIVE_STATE) ||
 			(target_state == DEVICE_PM_SUSPEND_STATE),
@@ -116,25 +120,19 @@ static int device_pm_request(const struct device *dev,
 		return 0;
 	}
 
-	k_work_submit(&dev->pm->work);
+	(void)k_work_schedule(&dev->pm->work, K_NO_WAIT);
 
 	/* Return in case of Async request */
 	if (pm_flags & DEVICE_PM_ASYNC) {
 		return 0;
 	}
 
-	/* Incase of Sync request wait for completion event */
-	do {
-		(void)k_poll(&dev->pm->event, 1, K_FOREVER);
-		k_poll_signal_check(&dev->pm->signal,
-						&signaled, &result);
-	} while (!signaled);
+	k_mutex_init(&request_mutex);
+	k_mutex_lock(&request_mutex, K_FOREVER);
+	(void)k_condvar_wait(&dev->pm->condvar, &request_mutex, K_FOREVER);
+	k_mutex_unlock(&request_mutex);
 
-	dev->pm->event.state = K_POLL_STATE_NOT_READY;
-	k_poll_signal_reset(&dev->pm->signal);
-
-
-	return result == target_state ? 0 : -EIO;
+	return target_state == atomic_get(&dev->pm->fsm_state) ? 0 : -EIO;
 }
 
 int device_pm_get(const struct device *dev)
@@ -180,9 +178,9 @@ void device_pm_enable(const struct device *dev)
 		dev->pm->dev = dev;
 		atomic_set(&dev->pm->fsm_state,
 			   DEVICE_PM_STATE_SUSPENDED);
-		k_work_init(&dev->pm->work, pm_work_handler);
+		k_work_init_delayable(&dev->pm->work, pm_work_handler);
 	} else {
-		k_work_submit(&dev->pm->work);
+		k_work_schedule(&dev->pm->work, K_NO_WAIT);
 	}
 	k_sem_give(&dev->pm->lock);
 }
@@ -195,6 +193,6 @@ void device_pm_disable(const struct device *dev)
 	k_sem_take(&dev->pm->lock, K_FOREVER);
 	dev->pm->enable = false;
 	/* Bring up the device before disabling the Idle PM */
-	k_work_submit(&dev->pm->work);
+	k_work_schedule(&dev->pm->work, K_NO_WAIT);
 	k_sem_give(&dev->pm->lock);
 }
