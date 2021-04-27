2c0cc38c4425330079ab2adeda0ac493b9586db7
Anas Nashif <anas.nashif@intel.com>
2021-04-27 16:21:25 -0400

    tracing: Use new macros with CTF based tracing
    Use new tracing macros to generate tracing output in CTF format.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- subsys/tracing/ctf/ctf_top.c
+++ subsys/tracing/ctf/ctf_top.c
@@ -22,7 +22,7 @@ static void _get_thread_name(struct k_thread *thread,
 	}
 }
 
-void sys_trace_thread_switched_out(void)
+void sys_trace_k_thread_switched_out(void)
 {
 	ctf_bounded_string_t name = { "unknown" };
 	struct k_thread *thread;
@@ -33,7 +33,7 @@ void sys_trace_thread_switched_out(void)
 	ctf_top_thread_switched_out((uint32_t)(uintptr_t)thread, name);
 }
 
-void sys_trace_thread_switched_in(void)
+void sys_trace_k_thread_switched_in(void)
 {
 	struct k_thread *thread;
 	ctf_bounded_string_t name = { "unknown" };
@@ -44,7 +44,7 @@ void sys_trace_thread_switched_in(void)
 	ctf_top_thread_switched_in((uint32_t)(uintptr_t)thread, name);
 }
 
-void sys_trace_thread_priority_set(struct k_thread *thread)
+void sys_trace_k_thread_priority_set(struct k_thread *thread)
 {
 	ctf_bounded_string_t name = { "unknown" };
 
@@ -53,7 +53,7 @@ void sys_trace_thread_priority_set(struct k_thread *thread)
 				    thread->base.prio, name);
 }
 
-void sys_trace_thread_create(struct k_thread *thread)
+void sys_trace_k_thread_create(struct k_thread *thread, size_t stack_size, int prio)
 {
 	ctf_bounded_string_t name = { "unknown" };
 
@@ -74,7 +74,7 @@ void sys_trace_thread_create(struct k_thread *thread)
 #endif
 }
 
-void sys_trace_thread_abort(struct k_thread *thread)
+void sys_trace_k_thread_abort(struct k_thread *thread)
 {
 	ctf_bounded_string_t name = { "unknown" };
 
@@ -82,7 +82,7 @@ void sys_trace_thread_abort(struct k_thread *thread)
 	ctf_top_thread_abort((uint32_t)(uintptr_t)thread, name);
 }
 
-void sys_trace_thread_suspend(struct k_thread *thread)
+void sys_trace_k_thread_suspend(struct k_thread *thread)
 {
 	ctf_bounded_string_t name = { "unknown" };
 
@@ -90,7 +90,7 @@ void sys_trace_thread_suspend(struct k_thread *thread)
 	ctf_top_thread_suspend((uint32_t)(uintptr_t)thread, name);
 }
 
-void sys_trace_thread_resume(struct k_thread *thread)
+void sys_trace_k_thread_resume(struct k_thread *thread)
 {
 	ctf_bounded_string_t name = { "unknown" };
 
@@ -99,7 +99,7 @@ void sys_trace_thread_resume(struct k_thread *thread)
 	ctf_top_thread_resume((uint32_t)(uintptr_t)thread, name);
 }
 
-void sys_trace_thread_ready(struct k_thread *thread)
+void sys_trace_k_thread_ready(struct k_thread *thread)
 {
 	ctf_bounded_string_t name = { "unknown" };
 
@@ -108,7 +108,12 @@ void sys_trace_thread_ready(struct k_thread *thread)
 	ctf_top_thread_ready((uint32_t)(uintptr_t)thread, name);
 }
 
-void sys_trace_thread_pend(struct k_thread *thread)
+void sys_trace_k_thread_start(struct k_thread *thread)
+{
+
+}
+
+void sys_trace_k_thread_pend(struct k_thread *thread)
 {
 	ctf_bounded_string_t name = { "unknown" };
 
@@ -116,7 +121,7 @@ void sys_trace_thread_pend(struct k_thread *thread)
 	ctf_top_thread_pend((uint32_t)(uintptr_t)thread, name);
 }
 
-void sys_trace_thread_info(struct k_thread *thread)
+void sys_trace_k_thread_info(struct k_thread *thread)
 {
 #if defined(CONFIG_THREAD_STACK_INFO)
 	ctf_bounded_string_t name = { "unknown" };
@@ -131,7 +136,7 @@ void sys_trace_thread_info(struct k_thread *thread)
 #endif
 }
 
-void sys_trace_thread_name_set(struct k_thread *thread)
+void sys_trace_k_thread_name_set(struct k_thread *thread, int ret)
 {
 	ctf_bounded_string_t name = { "unknown" };
 
@@ -163,54 +168,130 @@ void sys_trace_idle(void)
 	ctf_top_idle();
 }
 
-void sys_trace_void(unsigned int id)
-{
-	ctf_top_void(id);
-}
-
-void sys_trace_semaphore_init(struct k_sem *sem)
+/* Semaphore */
+void sys_trace_k_sem_init(struct k_sem *sem, int ret)
 {
 	ctf_top_semaphore_init(
-		(uint32_t)(uintptr_t)sem
+		(uint32_t)(uintptr_t)sem,
+		(int32_t)ret
 		);
 }
 
-void sys_trace_semaphore_take(struct k_sem *sem)
+void sys_trace_k_sem_take_enter(struct k_sem *sem, k_timeout_t timeout)
 {
-	ctf_top_semaphore_take(
-		(uint32_t)(uintptr_t)sem
+	ctf_top_semaphore_take_enter(
+		(uint32_t)(uintptr_t)sem,
+		k_ticks_to_ms_floor32((uint32_t)timeout.ticks)
 		);
 }
 
-void sys_trace_semaphore_give(struct k_sem *sem)
+
+void sys_trace_k_sem_take_blocking(struct k_sem *sem, k_timeout_t timeout)
 {
-	ctf_top_semaphore_give(
+	ctf_top_semaphore_take_blocking(
+		(uint32_t)(uintptr_t)sem,
+		k_ticks_to_ms_floor32((uint32_t)timeout.ticks)
+		);
+}
+
+void sys_trace_k_sem_take_exit(struct k_sem *sem, k_timeout_t timeout, int ret)
+{
+	ctf_top_semaphore_take_exit(
+		(uint32_t)(uintptr_t)sem,
+		k_ticks_to_ms_floor32((uint32_t)timeout.ticks),
+		(uint32_t)ret
+		);
+}
+
+void sys_trace_k_sem_reset(struct k_sem *sem)
+{
+	ctf_top_semaphore_reset(
 		(uint32_t)(uintptr_t)sem
 		);
 }
 
-void sys_trace_mutex_init(struct k_mutex *mutex)
+void sys_trace_k_sem_give_enter(struct k_sem *sem)
+{
+	ctf_top_semaphore_give_enter(
+		(uint32_t)(uintptr_t)sem
+		);
+}
+
+void sys_trace_k_sem_give_exit(struct k_sem *sem)
+{
+	ctf_top_semaphore_give_exit(
+		(uint32_t)(uintptr_t)sem
+		);
+}
+
+/* Mutex */
+void sys_trace_k_mutex_init(struct k_mutex *mutex, int ret)
 {
 	ctf_top_mutex_init(
+		(uint32_t)(uintptr_t)mutex,
+		(int32_t)ret
+		);
+}
+
+void sys_trace_k_mutex_lock_enter(struct k_mutex *mutex, k_timeout_t timeout)
+{
+	ctf_top_mutex_lock_enter(
+		(uint32_t)(uintptr_t)mutex,
+		k_ticks_to_ms_floor32((uint32_t)timeout.ticks)
+		);
+}
+
+void sys_trace_k_mutex_lock_blocking(struct k_mutex *mutex, k_timeout_t timeout)
+{
+	ctf_top_mutex_lock_blocking(
+		(uint32_t)(uintptr_t)mutex,
+		k_ticks_to_ms_floor32((uint32_t)timeout.ticks)
+		);
+}
+
+void sys_trace_k_mutex_lock_exit(struct k_mutex *mutex, k_timeout_t timeout, int ret)
+{
+	ctf_top_mutex_lock_exit(
+		(uint32_t)(uintptr_t)mutex,
+		k_ticks_to_ms_floor32((uint32_t)timeout.ticks),
+		(int32_t)ret
+		);
+}
+
+void sys_trace_k_mutex_unlock_enter(struct k_mutex *mutex)
+{
+	ctf_top_mutex_unlock_enter(
 		(uint32_t)(uintptr_t)mutex
 		);
 }
 
-void sys_trace_mutex_lock(struct k_mutex *mutex)
+void sys_trace_k_mutex_unlock_exit(struct k_mutex *mutex, int ret)
 {
-	ctf_top_mutex_lock(
-		(uint32_t)(uintptr_t)mutex
+	ctf_top_mutex_unlock_exit(
+		(uint32_t)(uintptr_t)mutex,
+		(int32_t)ret
 		);
 }
 
-void sys_trace_mutex_unlock(struct k_mutex *mutex)
+/* Timer */
+void sys_trace_k_timer_init(struct k_timer *timer, k_timer_expiry_t expiry_fn,
+			    k_timer_expiry_t stop_fn)
 {
-	ctf_top_mutex_unlock(
-		(uint32_t)(uintptr_t)mutex
-		);
 }
 
-void sys_trace_end_call(unsigned int id)
+void sys_trace_k_timer_start(struct k_timer *timer, k_timeout_t duration,
+			     k_timeout_t period)
 {
-	ctf_top_end_call(id);
+}
+
+void sys_trace_k_timer_stop(struct k_timer *timer)
+{
+}
+
+void sys_trace_k_timer_status_sync_blocking(struct k_timer *timer)
+{
+}
+
+void sys_trace_k_timer_status_sync_exit(struct k_timer *timer, uint32_t result)
+{
 }
