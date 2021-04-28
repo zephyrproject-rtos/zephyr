a591d7c21facbc4a6cc62c46a9da0636fde5e874
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-04-28 15:44:53 -0700

    runners: core: add **kwargs to other subprocess wrappers
    Just to make these match check_output().
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- scripts/west_commands/runners/core.py
+++ scripts/west_commands/runners/core.py
@@ -552,7 +552,7 @@ class ZephyrBinaryRunner(abc.ABC):
         else:
             self.logger.info(escaped)
 
-    def call(self, cmd: List[str]) -> int:
+    def call(self, cmd: List[str], **kwargs) -> int:
         '''Subclass subprocess.call() wrapper.
 
         Subclasses should use this method to run command in a
@@ -562,9 +562,9 @@ class ZephyrBinaryRunner(abc.ABC):
         self._log_cmd(cmd)
         if _DRY_RUN:
             return 0
-        return subprocess.call(cmd)
+        return subprocess.call(cmd, **kwargs)
 
-    def check_call(self, cmd: List[str]):
+    def check_call(self, cmd: List[str], **kwargs):
         '''Subclass subprocess.check_call() wrapper.
 
         Subclasses should use this method to run command in a
@@ -574,7 +574,7 @@ class ZephyrBinaryRunner(abc.ABC):
         self._log_cmd(cmd)
         if _DRY_RUN:
             return
-        subprocess.check_call(cmd)
+        subprocess.check_call(cmd, **kwargs)
 
     def check_output(self, cmd: List[str], **kwargs) -> bytes:
         '''Subclass subprocess.check_output() wrapper.
