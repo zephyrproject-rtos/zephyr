ef4ceca1a83c0471a97e1211c7b22e1b65985460
Wealian Liao <WHLIAO@nuvoton.com>
2021-04-29 17:11:39 +0800

    soc: npcx: Move ecst to npcx common folder
    NPCX series ROM code changes the chip basic setting by firmware binary
    header for loading the firmware from flash to RAM. Add the following to
    improve the ec firmware header setting:
    
    - Move the ECST which generates the firmware binary header to NPCX
    common folder. All the following NPCX series chips can use this.
    - Add ecst setting option in Kconfig.
    
    Signed-off-by: Yuval Peress <peress@chromium.org>
    Signed-off-by: Wealian Liao <WHLIAO@nuvoton.com>

--- boards/arm/npcx7m6fb_evb/board.cmake
+++ boards/arm/npcx7m6fb_evb/board.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
-set(TARGET_IMAGE_FILE  ${PROJECT_BINARY_DIR}/${CONFIG_KERNEL_BIN_NAME}_${BOARD}.bin)
+set(NPCX_IMAGE_FILE  ${PROJECT_BINARY_DIR}/${CONFIG_KERNEL_BIN_NAME}_${BOARD}.bin)
 
 set(TARGET_IMAGE_ADDR  ${CONFIG_FLASH_BASE_ADDRESS})
 set(TARGET_IMAGE_SIZE  ${CONFIG_FLASH_SIZE})
@@ -9,6 +9,6 @@ board_set_flasher_ifnset(openocd)
 board_set_debugger_ifnset(openocd)
 
 board_finalize_runner_args(openocd
-  --cmd-load "flash_npcx ${MONITOR_IMAGE_FILE} ${TARGET_IMAGE_FILE} ${TARGET_IMAGE_ADDR} ${TARGET_IMAGE_SIZE}"
+  --cmd-load "flash_npcx ${MONITOR_IMAGE_FILE} ${NPCX_IMAGE_FILE} ${TARGET_IMAGE_ADDR} ${TARGET_IMAGE_SIZE}"
   --cmd-verify "verify_npcx"
   )
