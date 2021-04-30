4d13c5a183409b0f9b420b23fb9866e6af5f83d4
Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>
2021-04-30 11:11:57 +0200

    Bluetooth: Audio: Media control: Rename functions
    Rename the mpl (media player) functions to media_proxy_pl (media_proxy
    player).
    
    Signed-off-by: Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>

--- subsys/bluetooth/host/audio/media_proxy.h
+++ subsys/bluetooth/host/audio/media_proxy.h
@@ -136,92 +136,91 @@ struct mpl_search_t {
 #endif /* CONFIG_BT_OTS || CONFIG_BT_OTC */
 
 
-
 /* PUBLIC API FOR PLAYERS */
 
-int mpl_init(void);
+int media_proxy_pl_init(void);
 
-char *mpl_player_name_get(void);
+char *media_proxy_pl_player_name_get(void);
 
 #ifdef CONFIG_BT_OTS
-uint64_t mpl_icon_id_get(void);
+uint64_t media_proxy_pl_icon_id_get(void);
 #endif /* CONFIG_BT_OTS */
 
-char *mpl_icon_uri_get(void);
+char *media_proxy_pl_icon_uri_get(void);
 
-void mpl_track_changed_cb(void);
+void media_proxy_pl_track_changed_cb(void);
 
-char *mpl_track_title_get(void);
-void mpl_track_title_cb(char *title);
+char *media_proxy_pl_track_title_get(void);
+void media_proxy_pl_track_title_cb(char *title);
 
-int32_t mpl_track_duration_get(void);
-void mpl_track_duration_cb(int32_t duration);
+int32_t media_proxy_pl_track_duration_get(void);
+void media_proxy_pl_track_duration_cb(int32_t duration);
 
-int32_t mpl_track_position_get(void);
-void mpl_track_position_set(int32_t position);
-void mpl_track_position_cb(int32_t position);
+int32_t media_proxy_pl_track_position_get(void);
+void media_proxy_pl_track_position_set(int32_t position);
+void media_proxy_pl_track_position_cb(int32_t position);
 
-int8_t mpl_playback_speed_get(void);
-void mpl_playback_speed_set(int8_t speed);
-void mpl_playback_speed_cb(int8_t speed);
+int8_t media_proxy_pl_playback_speed_get(void);
+void media_proxy_pl_playback_speed_set(int8_t speed);
+void media_proxy_pl_playback_speed_cb(int8_t speed);
 
-int8_t mpl_seeking_speed_get(void);
-void mpl_seeking_speed_cb(int8_t speed);
+int8_t media_proxy_pl_seeking_speed_get(void);
+void media_proxy_pl_seeking_speed_cb(int8_t speed);
 
 #ifdef CONFIG_BT_OTS
-uint64_t mpl_track_segments_id_get(void);
+uint64_t media_proxy_pl_track_segments_id_get(void);
 
-uint64_t mpl_current_track_id_get(void);
-void mpl_current_track_id_set(uint64_t id);
-void mpl_current_track_id_cb(uint64_t id);
+uint64_t media_proxy_pl_current_track_id_get(void);
+void media_proxy_pl_current_track_id_set(uint64_t id);
+void media_proxy_pl_current_track_id_cb(uint64_t id);
 
-uint64_t mpl_next_track_id_get(void);
-void mpl_next_track_id_set(uint64_t id);
-void mpl_next_track_id_cb(uint64_t id);
+uint64_t media_proxy_pl_next_track_id_get(void);
+void media_proxy_pl_next_track_id_set(uint64_t id);
+void media_proxy_pl_next_track_id_cb(uint64_t id);
 
-uint64_t mpl_group_id_get(void);
-void mpl_group_id_set(uint64_t id);
-void mpl_group_id_cb(uint64_t id);
+uint64_t media_proxy_pl_group_id_get(void);
+void media_proxy_pl_group_id_set(uint64_t id);
+void media_proxy_pl_group_id_cb(uint64_t id);
 
-uint64_t mpl_parent_group_id_get(void);
-void mpl_parent_group_id_cb(uint64_t id);
+uint64_t media_proxy_pl_parent_group_id_get(void);
+void media_proxy_pl_parent_group_id_cb(uint64_t id);
 #if defined(CONFIG_BT_DEBUG_MCS) && defined(CONFIG_BT_TESTING)
 /* For  IOP testing - set current group to be it's own parent */
-void mpl_test_unset_parent_group(void);
+void media_proxy_pl_test_unset_parent_group(void);
 #endif /* CONFIG_BT_DEBUG_MCS && CONFIG_BT_TESTING */
 #endif /* CONFIG_BT_OTS */
 
-uint8_t mpl_playing_order_get(void);
-void mpl_playing_order_set(uint8_t order);
-void mpl_playing_order_cb(uint8_t order);
+uint8_t media_proxy_pl_playing_order_get(void);
+void media_proxy_pl_playing_order_set(uint8_t order);
+void media_proxy_pl_playing_order_cb(uint8_t order);
 
-uint16_t mpl_playing_orders_supported_get(void);
+uint16_t media_proxy_pl_playing_orders_supported_get(void);
 
-uint8_t mpl_media_state_get(void);
+uint8_t media_proxy_pl_media_state_get(void);
 #if defined(CONFIG_BT_DEBUG_MCS) && defined(CONFIG_BT_TESTING)
-void mpl_test_media_state_set(uint8_t state);
+void media_proxy_pl_test_media_state_set(uint8_t state);
 #endif /* CONFIG_BT_DEBUG_MCS && CONFIG_BT_TESTING */
-void mpl_media_state_cb(uint8_t state);
+void media_proxy_pl_media_state_cb(uint8_t state);
 
-void mpl_operation_set(struct mpl_op_t operation);
-void mpl_operation_cb(struct mpl_op_ntf_t op_ntf);
+void media_proxy_pl_operation_set(struct mpl_op_t operation);
+void media_proxy_pl_operation_cb(struct mpl_op_ntf_t op_ntf);
 
-uint32_t mpl_operations_supported_get(void);
-void mpl_operations_supported_cb(uint32_t operations);
+uint32_t media_proxy_pl_operations_supported_get(void);
+void media_proxy_pl_operations_supported_cb(uint32_t operations);
 
 #ifdef CONFIG_BT_OTS
-void mpl_scp_set(struct mpl_search_t search);
-void mpl_search_cb(uint8_t result_code);
+void media_proxy_pl_scp_set(struct mpl_search_t search);
+void media_proxy_pl_search_cb(uint8_t result_code);
 
-uint64_t mpl_search_results_id_get(void);
-void mpl_search_results_id_cb(uint64_t id);
+uint64_t media_proxy_pl_search_results_id_get(void);
+void media_proxy_pl_search_results_id_cb(uint64_t id);
 #endif /* CONFIG_BT_OTS */
 
-uint8_t mpl_content_ctrl_id_get(void);
+uint8_t media_proxy_pl_content_ctrl_id_get(void);
 
 #if CONFIG_BT_DEBUG_MCS
 /* Output the mediaplayer's state information */
-void mpl_debug_dump_state(void);
+void media_proxy_pl_debug_dump_state(void);
 #endif /* CONFIG_BT_DEBUG_MCS */
 
 struct bt_ots *bt_mcs_get_ots(void);
