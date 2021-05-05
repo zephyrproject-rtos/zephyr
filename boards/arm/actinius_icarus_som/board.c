0febdbfba0ee2eac4adbc54ee179d32ee8b10aca
Alex Tsamakos <alex@actinius.com>
2021-05-06 01:36:59 +0200

    boards: arm: Add support for Actinius Icarus SoM (uC: Nordic nRF9160)
    Add support for the Actinius Icarus SoM (nRF9160).
    
    Signed-off-by: Alex Tsamakos <alex@actinius.com>

---
+++ boards/arm/actinius_icarus_som/board.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2021 Actinius
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+#include <drivers/gpio.h>
+
+#include <logging/log.h>
+LOG_MODULE_REGISTER(board_control, CONFIG_BOARD_ICARUS_SOM_LOG_LEVEL);
+
+#define SIM_SELECT_PIN 12
+
+static void select_sim(void)
+{
+	const struct device *port = device_get_binding(DT_LABEL(DT_NODELABEL(gpio0)));
+
+	if (!port) {
+		LOG_ERR("Could not get GPIO Device Binding");
+
+		return;
+	}
+
+	#ifdef CONFIG_BOARD_SELECT_SIM_EXTERNAL
+		gpio_pin_configure(port, SIM_SELECT_PIN, GPIO_OUTPUT_LOW);
+		LOG_INF("External SIM is selected");
+	#else
+		gpio_pin_configure(port, SIM_SELECT_PIN, GPIO_OUTPUT_HIGH);
+		LOG_INF("eSIM is selected");
+	#endif
+}
+
+static int board_actinius_icarus_som_init(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	select_sim();
+
+	return 0;
+}
+
+SYS_INIT(board_actinius_icarus_som_init, POST_KERNEL,
+	 CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
