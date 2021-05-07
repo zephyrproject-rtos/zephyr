8ff65f4087f1974273fa943d6a129363f1b5315d
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2021-05-07 11:41:09 +0200

    cmake: get: using NOT DEFINE BUILD_VERSION instead of NOT BUILD_VERSION
    If user is specifying `-DBUILD_VERSION=<value>` then the proper way to
    test is using `NOT DEFINED BUILD_VERSION`.
    
    The difference between `NOT DEFINED BUILD_VERSION` and
    `NOT BUILD_VERSION` is that a user specifying `-DBUILD_VERSION=0` will
    result in `NOT BUILD_VERSION` becoming true, whereas
    `NOT DEFINED BUILD_VERSION` would be false, which is the correct
    behavior here, as the user actually provided a specific BUILD_VERSION.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/git.cmake
+++ cmake/git.cmake
@@ -13,7 +13,7 @@
 
 # https://cmake.org/cmake/help/latest/module/FindGit.html
 find_package(Git QUIET)
-if(NOT BUILD_VERSION AND GIT_FOUND)
+if(NOT DEFINED BUILD_VERSION AND GIT_FOUND)
   execute_process(
     COMMAND ${GIT_EXECUTABLE} describe --abbrev=12 --always
     WORKING_DIRECTORY                ${ZEPHYR_BASE}
