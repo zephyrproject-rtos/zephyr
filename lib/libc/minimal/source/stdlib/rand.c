42023f8725139073276ad6f0fb1470102c06eee1
Yasushi SHOJI <yashi@spacecubics.com>
2021-05-08 23:52:33 +0900

    libc: minimal: Add rand() and srand()
    rand() and srand() are pseudo-random number generator functions
    defined in ISO C. This implementation uses the Linear Congruential
    Generator (LCG) algorithm with the following parameters, which are the
    same as used in GNU Libc "TYPE_0" algorithm.
    
      Modulus 2^31
      Multiplier 1103515245
      Increment 12345
      Output Bits 30..0
    
    Note that the default algorithm used by GNU Libc is not TYPE_0, and
    TYPE_0 should be selected first by an initstate() call as shown below.
    
    All global variables in a C library must be routed to a memory
    partition in order to be used by user-mode applications when
    CONFIG_USERSPACE is enabled.  Thus, srand_seed is marked as
    such. z_libc_partition is originally used by the Newlib C library but
    it's generic enough to be used by either the minimal libc or the
    newlib.
    
    All other functions in the Minimal C library, however, don't require
    global variables/states.  Unconditionally using z_libc_partition with
    minimal libc is selected might be a problem for applications utilizing
    many custom memory partitions on platforms with a limited number of
    MPU regions (eg. Cortex M0/M3). This commit introduces a kconfig
    option CONFIG_MINIMAL_LIBC_GLOBALS so that applications can disable
    the functions requiring the global variables and can use the minimal
    libc without a memory partition.
    
    Because this commit _does_ implement rand() and srand(), our coding
    guideline check on GitHub Action finds it as a violation.
    
        Error: lib/libc/minimal/include/stdlib.h:45:WARNING: Violation to
        rule 21.2 (Should not used a reserved identifier) - srand
    
    But this is false positive.
    
    The following is a simple test program for LCG with GNU Libc.
    
      #include <stdio.h>
      #include <stdlib.h>
    
      int main()
      {
              static char state[8];
    
              /* Switch GLIBC to use LCG/TYPE_0 generator type. */
              initstate(0, state, sizeof(state));
    
              srand(1);  /* Or any other value. */
              printf("%d\n", rand());
              printf("%d\n", rand());
    
              return 0;
      }
    
    See initstate(3p) for more detail about how to use LCG in GLIBC.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

---
+++ lib/libc/minimal/source/stdlib/rand.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdlib.h>
+#include <sys/libc-hooks.h>
+#include <app_memory/app_memdomain.h>
+
+#define LIBC_DATA K_APP_DMEM(z_libc_partition)
+
+#define OUTPUT_BITS (0x7fffffffU)
+#define MULTIPLIER (1103515245U)
+#define INCREMENT (12345U)
+
+static LIBC_DATA unsigned long srand_seed = 1;
+
+void srand(unsigned int s)
+{
+	srand_seed = s;
+}
+
+int rand(void)
+{
+	srand_seed = (MULTIPLIER * srand_seed + INCREMENT) & OUTPUT_BITS;
+
+	return srand_seed;
+}
