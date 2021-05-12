zephyrproject-rtos/zephyr

commit 5832e588e3664cc0abb76847ac3a5e4d1954392d

samples: bmg160: drop device_get_binding
Use DEVICE_DT_GET_ANY.

Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

samples/sensor/bmg160/src/main.c
@@ -168,14 +168,17 @@ static void test_trigger_mode(const struct device *bmg160)
 
 void main(void)
 {
-	const struct device *bmg160;
+	const struct device *bmg160 = DEVICE_DT_GET_ANY(bosch_bmg160);
 #if defined(CONFIG_BMG160_RANGE_RUNTIME)
 	struct sensor_value attr;
 #endif
 
-	bmg160 = device_get_binding("bmg160");
 	if (!bmg160) {
 		printf("Device not found.\n");
+		return;
+	}
+	if (!device_is_ready(bmg160)) {
+		printf("Device %s is not ready.\n", bmg160->name);
 		return;
 	}
 

commit 95b7381b34f5e7d981949dfc2d95c8b4fd04606b

samples: sensor: Fix logically dead code in bmg160 sample
Converts the bmg160 sample application to check the sensor device at
build time instead of runtime. This fixes a Coverity issue for logically
dead code introduced in commit 5832e588e3664cc0abb76847ac3a5e4d1954392d.

Fixes #35118
Coverity-CID: 235919

Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

samples/sensor/bmg160/src/main.c
@@ -17,6 +17,10 @@
 #define MAX_TEST_TIME	15000
 #define SLEEPTIME	300
 
+#if !DT_HAS_COMPAT_STATUS_OKAY(bosch_bmg160)
+#error "No bosch,bmg160 compatible node found in the device tree"
+#endif
+
 static void print_gyro_data(const struct device *bmg160)
 {
 	struct sensor_value val[3];
@@ -173,10 +177,6 @@ void main(void)
 	struct sensor_value attr;
 #endif
 
-	if (!bmg160) {
-		printf("Device not found.\n");
-		return;
-	}
 	if (!device_is_ready(bmg160)) {
 		printf("Device %s is not ready.\n", bmg160->name);
 		return;