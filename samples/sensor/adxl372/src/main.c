zephyrproject-rtos/zephyr

commit 3c7cb323a8aa98a29f47ffd79c723bb2fc3f2060

samples: adxl372: drop device_get_binding/DT_INST
Use DEVICE_DT_GET_ANY.

Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

samples/sensor/adxl372/src/main.c
@@ -48,10 +48,14 @@ void main(void)
 	int i;
 	char meter[200];
 
-	const struct device *dev = device_get_binding(DT_LABEL(DT_INST(0, adi_adxl372)));
+	const struct device *dev = DEVICE_DT_GET_ANY(adi_adxl372);
 
-	if (dev == NULL) {
-		printf("Could not get %s device\n", DT_LABEL(DT_INST(0, adi_adxl372)));
+	if (!dev) {
+		printf("Devicetree has no adi,adxl372 node\n");
+		return;
+	}
+	if (!device_is_ready(dev)) {
+		printf("Device %s is not ready\n", dev->name);
 		return;
 	}
 

commit 8f5ffc1751d08c6553cba04db826488f4ad8fe9d

samples: sensor: Fix logically dead code in adxl372 sample
Converts the adxl372 sample application to check the sensor device at
build time instead of runtime. This fixes a Coverity issue for logically
dead code introduced in commit 3c7cb323a8aa98a29f47ffd79c723bb2fc3f2060.

Fixes #35119
Coverity-CID: 235932

Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

samples/sensor/adxl372/src/main.c
@@ -10,6 +10,10 @@
 
 #define pow2(x) ((x) * (x))
 
+#if !DT_HAS_COMPAT_STATUS_OKAY(adi_adxl372)
+#error "No adi,adxl372 compatible node found in the device tree"
+#endif
+
 static double sqrt(double value)
 {
 	int i;
@@ -50,10 +54,6 @@ void main(void)
 
 	const struct device *dev = DEVICE_DT_GET_ANY(adi_adxl372);
 
-	if (!dev) {
-		printf("Devicetree has no adi,adxl372 node\n");
-		return;
-	}
 	if (!device_is_ready(dev)) {
 		printf("Device %s is not ready\n", dev->name);
 		return;