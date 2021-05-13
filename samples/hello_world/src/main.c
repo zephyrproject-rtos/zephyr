458e7393dd9790ab02081c38a8c810d33d69f895
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2021-05-13 15:30:41 +0200

    [DNM] samples: hello_world: quick test of device PM changes
    A quick test, DNM.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,9 +5,28 @@
  */
 
 #include <zephyr.h>
+#include <device.h>
 #include <sys/printk.h>
+
+extern const struct device *__pm_device_start[];
+extern const struct device *__pm_device_end[];
+
+extern const struct device __device_start[];
+extern const struct device __device_end[];
 
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	size_t pm_devices_cnt = __pm_device_end - __pm_device_start;
+
+	printk("%d devices provide power management\n", pm_devices_cnt);
+	for (size_t i = 0U; i < pm_devices_cnt; i++) {
+		printk("- %s\n", __pm_device_start[i]->name);
+	}
+
+	size_t devices_cnt = __device_end - __device_start;
+
+	printk("%d devices available\n", devices_cnt);
+	for (size_t i = 0U; i < devices_cnt; i++) {
+		printk("- %s\n", __device_start[i].name);
+	}
 }
