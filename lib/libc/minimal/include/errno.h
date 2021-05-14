75fe0301f654d4e6ae960e6319e16e982109a047
Carles Cufi <carles.cufi@nordicsemi.no>
2021-05-14 15:37:55 +0200

    libc: minimal: Add EOVERFLOW
    Add the EOVERFLOW error number, which is already in use by some of
    Zephyr's modules.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- lib/libc/minimal/include/errno.h
+++ lib/libc/minimal/include/errno.h
@@ -114,6 +114,7 @@ extern "C" {
 #define ETOOMANYREFS 129    /**< Too many references: can't splice */
 #define ENOTSUP 134         /**< Unsupported value */
 #define EILSEQ 138          /**< Illegal byte sequence */
+#define EOVERFLOW 139       /**< Value overflow */
 #define ECANCELED 140       /**< Operation canceled */
 
 #define EWOULDBLOCK EAGAIN /**< Operation would block */
