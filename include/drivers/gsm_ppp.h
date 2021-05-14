472f49c3fce60b8d54223aeffc4f94df27d67650
Yong Cong Sin <yongcong.sin@gmail.com>
2021-05-14 19:05:54 +0800

    drivers: modem: gsm_ppp: Add RTC query
    Added RTC query using AT+CCLK to the gsm_ppp driver.
    
    Signed-off-by: Yong Cong Sin <yongcong.sin@gmail.com>

--- include/drivers/gsm_ppp.h
+++ include/drivers/gsm_ppp.h
@@ -1,5 +1,7 @@
 /*
  * Copyright (c) 2020 Endian Technologies AB
+ * Copyright (c) 2020 Laird Connectivity
+ * Copyright (c) 2021 G-Technologies Sdn. Bhd.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -7,12 +9,29 @@
 #ifndef GSM_PPP_H_
 #define GSM_PPP_H_
 
+#ifdef CONFIG_NEWLIB_LIBC
+#include <time.h>
+#endif
+
 #define GSM_MODEM_DEVICE_NAME "modem_gsm"
 
 /** @cond INTERNAL_HIDDEN */
 struct device;
 void gsm_ppp_start(const struct device *dev);
 void gsm_ppp_stop(const struct device *dev);
+#ifdef CONFIG_NEWLIB_LIBC
+/**
+ * @brief Get the local time from the modem's real time clock.
+ *
+ * @param ts time structure
+ * @param offset The amount the local time is offset from GMT/UTC in seconds.
+ *
+ * @param 0 if successful
+ */
+int32_t gsm_ppp_get_local_time(const struct device *dev,
+			       struct tm *ts,
+			       int32_t *offset);
+#endif
 /** @endcond */
 
 #endif /* GSM_PPP_H_ */
