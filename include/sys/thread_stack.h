9440a1c2b8ae9b59b486b130758d5c6d4f892c10
Anas Nashif <anas.nashif@intel.com>
2021-05-17 12:40:01 -0400

    doc: kernel: thread stack is not the same as stack object
    Fix mixup of stack objects and thread stacks.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- include/sys/thread_stack.h
+++ include/sys/thread_stack.h
@@ -10,6 +10,14 @@
  * @brief Macros for declaring thread stacks
  */
 
+/**
+ * @brief Thread Stack APIs
+ * @ingroup kernel_apis
+ * @defgroup thread_stack_api Thread Stack APIs
+ * @{
+ * @}
+ */
+
 #ifndef ZEPHYR_INCLUDE_SYS_THREAD_STACK_H
 #define ZEPHYR_INCLUDE_SYS_THREAD_STACK_H
 
@@ -107,7 +115,7 @@ static inline char *z_stack_ptr_align(char *ptr)
 #define K_KERNEL_STACK_EXTERN(sym) extern k_thread_stack_t sym[]
 
 /**
- * @addtogroup stack_apis
+ * @addtogroup thread_stack_api
  * @{
  */
 
@@ -364,7 +372,7 @@ static inline char *Z_KERNEL_STACK_BUFFER(k_thread_stack_t *sym)
 #define K_THREAD_STACK_EXTERN(sym) extern k_thread_stack_t sym[]
 
 /**
- * @addtogroup stack_apis
+ * @addtogroup thread_stack_api
  * @{
  */
 
