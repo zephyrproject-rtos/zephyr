fac4201243f3ce093d68eaae21bd339c19156253
Jennifer Williams <jennifer.m.williams@intel.com>
2021-05-17 15:03:55 -0700

    lib: os: sem.c: fix coding guideline 15.7 missing comment
    The final else {} in the if...else if is missing required
    comment (non-empty, ';' is not sufficient). This adds a comment
    to comply with CG 15.7.
    
    Signed-off-by: Jennifer Williams <jennifer.m.williams@intel.com>

--- lib/os/sem.c
+++ lib/os/sem.c
@@ -75,7 +75,7 @@ int sys_sem_give(struct sys_sem *sem)
 	} else if (old_value >= sem->limit) {
 		return -EAGAIN;
 	} else {
-		;
+		; /* Non-empty final else in if ... else if struct */
 	}
 	return ret;
 }
