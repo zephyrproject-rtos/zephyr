f1a13a9fc5fcfc95c913372b331a10f4fbc8daba
Gerson Fernando Budke <nandojve@gmail.com>
2021-05-18 19:38:06 -0300

    drivers: serial: uart_sam0: Add pinctrl support
    Add Atmel sam0 sercom[uart] pinctrl bindings and implements pinctrl at
    driver level.  It changes all sam0 boards to use new feature and remove
    pinmux driver dependency for sercom[uart].  The samples that require a
    binding were update to keep consistency and avoid errors.
    
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

--- boards/arm/seeeduino_xiao/pinmux.c
+++ boards/arm/seeeduino_xiao/pinmux.c
@@ -17,28 +17,8 @@ static int board_pinmux_init(const struct device *dev)
 	__ASSERT_NO_MSG(device_is_ready(muxb));
 
 	ARG_UNUSED(dev);
-
-#if ATMEL_SAM0_DT_SERCOM_CHECK(4, atmel_sam0_uart) && defined(CONFIG_UART_SAM0)
-	/* SERCOM4 on RX=PB9/pad 1, TX=PB8/pad 0 */
-	pinmux_pin_set(muxb, 9, PINMUX_FUNC_D);
-	pinmux_pin_set(muxb, 8, PINMUX_FUNC_D);
-#endif
-
-#if ATMEL_SAM0_DT_SERCOM_CHECK(0, atmel_sam0_uart) && defined(CONFIG_UART_SAM0)
-#warning Pin mapping may not be configured
-#endif
-#if ATMEL_SAM0_DT_SERCOM_CHECK(1, atmel_sam0_uart) && defined(CONFIG_UART_SAM0)
-#warning Pin mapping may not be configured
-#endif
-#if ATMEL_SAM0_DT_SERCOM_CHECK(2, atmel_sam0_uart) && defined(CONFIG_UART_SAM0)
-#warning Pin mapping may not be configured
-#endif
-#if ATMEL_SAM0_DT_SERCOM_CHECK(3, atmel_sam0_uart) && defined(CONFIG_UART_SAM0)
-#warning Pin mapping may not be configured
-#endif
-#if ATMEL_SAM0_DT_SERCOM_CHECK(5, atmel_sam0_uart) && defined(CONFIG_UART_SAM0)
-#warning Pin mapping may not be configured
-#endif
+	ARG_UNUSED(muxa);
+	ARG_UNUSED(muxb);
 
 #if ATMEL_SAM0_DT_SERCOM_CHECK(2, atmel_sam0_i2c) && defined(CONFIG_I2C_SAM0)
 	/* SERCOM2 on SDA=PA08, SCL=PA09 */
