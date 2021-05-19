e90d94a7a520749077a624758d148c10a6e2a933
Yuriy Vynnychek <yura.vynnychek@telink-semi.com>
2021-05-19 08:46:09 +0300

    soc: riscv: telink_b91: new Telink B91 (TLSR9) SoC system
    A new RISC-V platform SoC for Telink B91 (TLSR9)
    
    Signed-off-by: Yuriy Vynnychek <yura.vynnychek@telink-semi.com>

---
+++ soc/riscv/riscv-privilege/telink_b91/soc.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 Telink Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __RISCV_TELINK_B91_SOC_H_
+#define __RISCV_TELINK_B91_SOC_H_
+
+#include <soc_common.h>
+#include <devicetree.h>
+
+/* Machine timer memory-mapped registers */
+#define RISCV_MTIME_BASE             0xE6000000
+#define RISCV_MTIMECMP_BASE          0xE6000008
+
+#endif /* __RISCV_TELINK_B91_SOC_H_ */
