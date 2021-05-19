21d616c7bcd9ad294b73c0c78cb9891597e45cb8
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2021-05-19 07:03:27 +0530

    Bluetooth: controller: Fix side effect in assertion
    Fix side effect in assertion when checking a volatile
    variable inside assert check.
    
    Fixes #32904, #32923.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_adv_sync.c
+++ subsys/bluetooth/controller/ll_sw/ull_adv_sync.c
@@ -907,6 +907,7 @@ static void mfy_sync_offset_get(void *param)
 		uint32_t volatile ret_cb;
 		uint32_t ticks_previous;
 		uint32_t ret;
+		bool success;
 
 		ticks_previous = ticks_current;
 
@@ -923,7 +924,8 @@ static void mfy_sync_offset_get(void *param)
 			}
 		}
 
-		LL_ASSERT(ret_cb == TICKER_STATUS_SUCCESS);
+		success = (ret_cb == TICKER_STATUS_SUCCESS);
+		LL_ASSERT(success);
 
 		LL_ASSERT((ticks_current == ticks_previous) || retry--);
 
