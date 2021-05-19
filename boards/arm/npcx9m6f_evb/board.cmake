03f77934cd2b09e46c34a40ff1881bcac9b004cf
Wealian Liao <WHLIAO@nuvoton.com>
2021-05-19 17:03:36 +0800

    soc: npcx: Add output npcx image in hex format
    To support west OpenOCD path image flashing, this generates an npcx hex
    image from the npcx BIN image. Moreover, this also changes the name of
    npcx output image by CONFIG_KERNEL_BIN_NAME.
    
    Signed-off-by: Wealian Liao <WHLIAO@nuvoton.com>

--- boards/arm/npcx9m6f_evb/board.cmake
+++ boards/arm/npcx9m6f_evb/board.cmake
@@ -1,14 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
-set(NPCX_IMAGE_FILE  ${PROJECT_BINARY_DIR}/${CONFIG_KERNEL_BIN_NAME}_${BOARD}.bin)
-
-set(TARGET_IMAGE_ADDR  ${CONFIG_FLASH_BASE_ADDRESS})
-set(TARGET_IMAGE_SIZE  ${CONFIG_FLASH_SIZE})
-
 board_set_flasher_ifnset(openocd)
 board_set_debugger_ifnset(openocd)
 
 board_finalize_runner_args(openocd
-  --cmd-load "flash_npcx ${MONITOR_IMAGE_FILE} ${NPCX_IMAGE_FILE} ${TARGET_IMAGE_ADDR} ${TARGET_IMAGE_SIZE}"
-  --cmd-verify "verify_npcx"
+  --cmd-load "npcx_write_image"
+  --cmd-verify "npcx_verify_image"
   )
