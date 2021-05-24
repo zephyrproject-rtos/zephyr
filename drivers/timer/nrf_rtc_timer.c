5221bb7d177adac2609813ddf6c99bca88c01239
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2021-05-24 11:46:07 +0200

    drivers: nrf_rtc_timer: Remove unnecessary locking
    As per description of the sys_clock_elapsed() function, "the kernel
    will call this with appropriate locking, the driver needs only provide
    an instantaneous answer". Remove then the unnecessary locking from the
    function, as it only adds an undesirable delay.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- drivers/timer/nrf_rtc_timer.c
+++ drivers/timer/nrf_rtc_timer.c
@@ -386,11 +386,7 @@ uint32_t sys_clock_elapsed(void)
 		return 0;
 	}
 
-	k_spinlock_key_t key = k_spin_lock(&lock);
-	uint32_t ret = counter_sub(counter(), last_count) / CYC_PER_TICK;
-
-	k_spin_unlock(&lock, key);
-	return ret;
+	return counter_sub(counter(), last_count) / CYC_PER_TICK;
 }
 
 uint32_t sys_clock_cycle_get_32(void)
