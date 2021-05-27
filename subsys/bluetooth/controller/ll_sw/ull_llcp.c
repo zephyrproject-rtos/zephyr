6411cd53b5f80c5a661865361f731a7aa0758026
Piotr Pryga <piotr.pryga@nordicsemi.no>
2021-05-28 00:30:44 +0200

    Bluetooth: controller: LLCP: Add API to enable or disable CTE req and rsp
    Add functions that allow:
    - start CTE request controle procedure.
    - enable possibility to response for CTE request procedures.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_llcp.c
+++ subsys/bluetooth/controller/ll_sw/ull_llcp.c
@@ -228,6 +228,11 @@ struct proc_ctx *llcp_create_local_procedure(enum llcp_proc proc)
 		llcp_lp_comm_init_proc(ctx);
 		break;
 #endif /* CONFIG_BT_CTLR_DATA_LENGTH */
+#if defined(CONFIG_BT_CTLR_DF_CONN_CTE_REQ)
+	case PROC_CTE_REQ:
+		llcp_lp_comm_init_proc(ctx);
+		break;
+#endif /* CONFIG_BT_CTLR_DF_CONN_CTE_REQ */
 	default:
 		/* Unknown procedure */
 		LL_ASSERT(0);
@@ -291,6 +296,11 @@ struct proc_ctx *llcp_create_remote_procedure(enum llcp_proc proc)
 		llcp_rp_comm_init_proc(ctx);
 		break;
 #endif /* CONFIG_BT_CTLR_DATA_LENGTH */
+#if defined(CONFIG_BT_CTLR_DF_CONN_CTE_REQ)
+	case PROC_CTE_REQ:
+		llcp_rp_comm_init_proc(ctx);
+		break;
+#endif /* CONFIG_BT_CTLR_DF_CONN_CTE_REQ */
 	default:
 		/* Unknown procedure */
 		LL_ASSERT(0);
@@ -732,6 +742,37 @@ void ull_cp_conn_param_req_neg_reply(struct ll_conn *conn, uint8_t error_code)
 		llcp_rp_conn_param_req_neg_reply(conn, ctx);
 	}
 }
+
+#if defined(CONFIG_BT_CTLR_DF_CONN_CTE_RSP)
+void ull_cp_cte_rsp_enable(struct ll_conn *conn, bool enable, uint8_t max_cte_len,
+			   uint8_t cte_types)
+{
+	conn->llcp.cte_rsp.is_enabled = enable;
+
+	if (enable) {
+		conn->llcp.cte_rsp.max_cte_len = max_cte_len;
+		conn->llcp.cte_rsp.cte_types = cte_types;
+	}
+}
+#endif /* CONFIG_BT_CTLR_DF_CONN_CTE_RSP */
+
+#if defined(CONFIG_BT_CTLR_DF_CONN_CTE_REQ)
+uint8_t ull_cp_cte_req(struct ll_conn *conn, uint8_t min_cte_len, uint8_t cte_type)
+{
+	struct proc_ctx *ctx;
+
+	ctx = llcp_create_local_procedure(PROC_CTE_REQ);
+	if (!ctx) {
+		return BT_HCI_ERR_CMD_DISALLOWED;
+	}
+
+	ctx->data.cte_req.min_len = min_cte_len;
+	ctx->data.cte_req.type = cte_type;
+	llcp_lr_enqueue(conn, ctx);
+
+	return BT_HCI_ERR_SUCCESS;
+}
+#endif /* CONFIG_BT_CTLR_DF_CONN_CTE_REQ */
 
 static bool pdu_is_expected(struct pdu_data *pdu, struct proc_ctx *ctx)
 {
