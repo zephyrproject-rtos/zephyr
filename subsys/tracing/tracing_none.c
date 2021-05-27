fecb289e3ce124cc3a400235db8980ad90c08397
Torbjörn Leksell <torbjorn.leksell@percepio.com>
2021-05-27 09:28:46 +0200

    Tracing: TRACING_NONE Compilation Fix
    Fixed several compilation errors that resulted from selecting
    TRACING without specifying a tracing system (Tracerecorder,
    CTF, Systemview). In this case (TRACING_NONE), some default trace hooks
    (in tracing.h) were incorrectly named resulting in compilation errors.
    The legacy sys_trace_isr_enter, sys_trace_isr_exit, and sys_trace_idle
    also caused problems since these were only given as defines, resulting
    in undefined reference errors since they are required by the assembly
    files calling these. To solve this issue I've added a stub file
    "tracing_none.c" (only compiled if TRACING_NONE) and declared the
    functions in tracing.h if no tracing system is selected.
    
    Signed-off-by: Torbjörn Leksell <torbjorn.leksell@percepio.com>

---
+++ subsys/tracing/tracing_none.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 Intel corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+#include <string.h>
+#include <kernel.h>
+
+void sys_trace_isr_enter(void) {}
+
+void sys_trace_isr_exit(void) {}
+
+void sys_trace_isr_exit_to_scheduler(void) {}
+
+void sys_trace_idle(void) {}
