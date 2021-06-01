5666e4d525fe98979f7d89624307726920d22867
Daniel Leung <daniel.leung@intel.com>
2021-06-01 13:17:22 -0700

    cmake: force GCC to emit DWARF version 4
    GCC 11 defaults to output DWARF version 5 which pyelftools cannot
    currently parse. So keep output at version 4 for the time being.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- cmake/compiler/gcc/compiler_flags.cmake
+++ cmake/compiler/gcc/compiler_flags.cmake
@@ -167,6 +167,10 @@ set_compiler_property(PROPERTY freestanding -ffreestanding)
 # Flag to enable debugging
 set_compiler_property(PROPERTY debug -g)
 
+# GCC 11 by default emits DWARF version 5 which cannot be parsed by
+# pyelftools. Can be removed once pyelftools supports v5.
+check_set_compiler_property(APPEND PROPERTY debug -gdwarf-4)
+
 set_compiler_property(PROPERTY no_common -fno-common)
 
 # GCC compiler flags for imacros. The specific header must be appended by user.
