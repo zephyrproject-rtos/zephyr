f004d52ac56a0475addd52933fdf3a1d48748c71
Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>
2021-06-03 11:41:31 +0200

    Bluetooth: Audio: Media control - function rename
    Rename scp_set to search_set for consistency with the operations_set
    for the media control point, and to make the name more meaningful.
    
    Signed-off-by: Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>

--- subsys/bluetooth/host/audio/media_proxy_internal.h
+++ subsys/bluetooth/host/audio/media_proxy_internal.h
@@ -119,7 +119,7 @@ void media_proxy_sctrl_operation_set(struct mpl_op_t operation);
 uint32_t media_proxy_sctrl_operations_supported_get(void);
 
 #ifdef CONFIG_BT_OTS
-void media_proxy_sctrl_scp_set(struct mpl_search_t search);
+void media_proxy_sctrl_search_set(struct mpl_search_t search);
 
 uint64_t media_proxy_sctrl_search_results_id_get(void);
 #endif /* CONFIG_BT_OTS */
