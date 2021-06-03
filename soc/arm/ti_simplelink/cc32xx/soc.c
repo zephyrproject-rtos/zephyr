457d089c7910aa304eae138536af1f0d2c1d292c
Pavlo Hamov <pasha.gamov@gmail.com>
2021-06-03 20:34:39 +0300

    soc: arm: cc32xx: Override Reboot implementation
    Support cold, hot reboots. Cold will reboot all periherals
    
    Signed-off-by: Pavlo Hamov <pasha.gamov@gmail.com>

--- soc/arm/ti_simplelink/cc32xx/soc.c
+++ soc/arm/ti_simplelink/cc32xx/soc.c
@@ -12,6 +12,12 @@
 #include <driverlib/rom_map.h>
 #include <driverlib/prcm.h>
 
+/* Overrides the weak ARM implementation */
+void sys_arch_reboot(int type)
+{
+	MAP_PRCMMCUReset(!!type);
+}
+
 static int ti_cc32xx_init(const struct device *arg)
 {
 	ARG_UNUSED(arg);
