413dff91b69bdeb6ab086922df2b0e232bd39731
Erik Brockhoff <erbr@oticon.com>
2021-06-11 12:39:41 +0200

    Bluetooth: controller: compile enc re. central/periph kconfig
    Only compile in code needed depending on central/periph config
    
    Signed-off-by: Erik Brockhoff <erbr@oticon.com>

--- subsys/bluetooth/controller/ll_sw/ull_llcp_remote.c
+++ subsys/bluetooth/controller/ll_sw/ull_llcp_remote.c
@@ -192,12 +192,12 @@ void ull_cp_priv_rr_rx(struct ll_conn *conn, struct proc_ctx *ctx, struct node_r
 	case PROC_VERSION_EXCHANGE:
 		rp_comm_rx(conn, ctx, rx);
 		break;
-#if defined (CONFIG_BT_CTLR_LE_ENC)
+#if defined (CONFIG_BT_CTLR_LE_ENC) && defined (CONFIG_BT_PERIPHERAL)
 	case PROC_ENCRYPTION_START:
 	case PROC_ENCRYPTION_PAUSE:
 		rp_enc_rx(conn, ctx, rx);
 		break;
-#endif /* CONFIG_BT_CTLR_LE_ENC */
+#endif /* CONFIG_BT_CTLR_LE_ENC && CONFIG_BT_PERIPHERAL */
 #ifdef CONFIG_BT_CTLR_PHY
 	case PROC_PHY_UPDATE:
 		rp_pu_rx(conn, ctx, rx);
@@ -270,12 +270,12 @@ static void rr_act_run(struct ll_conn *conn)
 	case PROC_VERSION_EXCHANGE:
 		rp_comm_run(conn, ctx, NULL);
 		break;
-#if defined (CONFIG_BT_CTLR_LE_ENC)
+#if defined (CONFIG_BT_CTLR_LE_ENC) && defined (CONFIG_BT_PERIPHERAL)
 	case PROC_ENCRYPTION_START:
 	case PROC_ENCRYPTION_PAUSE:
 		rp_enc_run(conn, ctx, NULL);
 		break;
-#endif /* CONFIG_BT_CTLR_LE_ENC */
+#endif /* CONFIG_BT_CTLR_LE_ENC && CONFIG_BT_PERIPHERAL */
 #ifdef CONFIG_BT_CTLR_PHY
 	case PROC_PHY_UPDATE:
 		rp_pu_run(conn, ctx, NULL);
