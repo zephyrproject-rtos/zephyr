588d22a755ae9f72e5da88106e67ed664c315f12
Henrik Brix Andersen <henrik@brixandersen.dk>
2021-06-14 22:58:56 +0200

    drivers: ht16k33: convert keyscan driver from gpio API to kscan API
    Convert the keyscan portion of the Holtek HT16K33 driver to adhere to
    the kscan API instead of the GPIO API.
    
    When this driver was introduced the kscan API was not present. The
    keyscan driver was therefore implemented as a GPIO interrupt driver.
    
    Signed-off-by: Henrik Brix Andersen <henrik@brixandersen.dk>

--- include/drivers/led/ht16k33.h
+++ include/drivers/led/ht16k33.h
@@ -8,40 +8,20 @@
 #ifndef ZEPHYR_INCLUDE_DRIVERS_LED_HT16K33_H_
 #define ZEPHYR_INCLUDE_DRIVERS_LED_HT16K33_H_
 
-#include <device.h>
-#include <zephyr/types.h>
+#include <drivers/kscan.h>
 
 /**
  * Register a HT16K33 keyscan device to be notified of relevant
  * keyscan events by the keyscan interrupt thread in the HT16K33
  * parent driver.
  *
- * @param parent      HT16K33 parent device.
- * @param child       HT16K33 keyscan child device.
- * @param keyscan_idx Index of the keyscan line handled by the keyscan
- *		      child device (0, 1, or 2).
+ * @param parent HT16K33 parent device.
+ * @param child HT16K33 child device.
+ * @param callback Keyscan callback function.
  * @return 0 if successful, negative errno code on failure.
  */
-int ht16k33_register_keyscan_device(const struct device *parent,
-				    const struct device *child,
-				    uint8_t keyscan_idx);
-
-/**
- * Check if a HT16K33 keyscan interrupt is pending.
- *
- * @param  parent HT16K33 parent device.
- * @return status != 0 if an interrupt is pending.
- */
-uint32_t ht16k33_get_pending_int(const struct device *parent);
-
-/**
- * Dispatch keyscan row data from a keyscan event to be handled by a
- * HT16K33 keyscan GPIO child device.
- *
- * @param child HT16K33 keyscan child device.
- * @param keys  Bitmask of key state for the row.
- */
-void ht16k33_process_keyscan_row_data(const struct device *child,
-				      uint32_t keys);
+int ht16k33_register_keyscan_callback(const struct device *parent,
+				      const struct device *child,
+				      kscan_callback_t callback);
 
 #endif /* ZEPHYR_INCLUDE_DRIVERS_LED_HT16K33_H_ */
