7b09d031fae3969fbe8647a57190f1fa21fabd8e
Felipe Neves <ryukokki.felipe@gmail.com>
2021-06-14 11:10:47 -0300

    arch: riscv: added support for custom initialization of gp register
    Plus added implementation for esp32c3 SoC.
    
    Signed-off-by: Felipe Neves <ryukokki.felipe@gmail.com>
    Signed-off-by: Felipe Neves <felipe.neves@espressif.com>

--- soc/riscv/esp32c3/soc.h
+++ soc/riscv/esp32c3/soc.h
@@ -41,6 +41,8 @@ extern STATUS esp32c3_rom_uart_tx_one_char(uint8_t chr);
 extern STATUS esp32c3_rom_uart_rx_one_char(uint8_t *chr);
 extern void esp32c3_rom_ets_set_user_start(uint32_t start);
 
+ulong_t __soc_get_gp_initial_value(void);
+
 #endif /* _ASMLANGUAGE */
 
 #endif /* __SOC_H__ */
