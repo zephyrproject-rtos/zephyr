bc1b563db344add038ba1eda0d75b4ff73579c6c
Johan Hedberg <johan.hedberg@intel.com>
2021-06-17 16:19:37 +0300

    Bluetooth: host: Remove confusing left-over comment
    This comment was previously associated with a stand-along net_buf_ref
    call. Now it's not clear what exactly it's referring to, and the
    additional reference to net_buf_put() is fairly self explanatory.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

--- subsys/bluetooth/host/hci_core.c
+++ subsys/bluetooth/host/hci_core.c
@@ -299,7 +299,6 @@ int bt_hci_cmd_send_sync(uint16_t opcode, struct net_buf *buf,
 	k_sem_init(&sync_sem, 0, 1);
 	cmd(buf)->sync = &sync_sem;
 
-	/* Make sure the buffer stays around until the command completes */
 	net_buf_put(&bt_dev.cmd_tx_queue, net_buf_ref(buf));
 
 	err = k_sem_take(&sync_sem, HCI_CMD_TIMEOUT);
