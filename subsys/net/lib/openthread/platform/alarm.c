ed76e6ae7219ced9de960b6b45422a2cf17abc53
Przemyslaw Bida <przemyslaw.bida@nordicsemi.no>
2021-06-18 13:24:20 +0200

    openthread: fix not working ot diag repeat command
    This commit fixes diag repeat command port by fixing issue with
    incorrectly handled repeat timer.
    
    Signed-off-by: Przemyslaw Bida <przemyslaw.bida@nordicsemi.no>

--- subsys/net/lib/openthread/platform/alarm.c
+++ subsys/net/lib/openthread/platform/alarm.c
@@ -16,6 +16,7 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
 #include <openthread/platform/alarm-milli.h>
 #include <openthread/platform/alarm-micro.h>
+#include <openthread/platform/diag.h>
 #include <openthread-system.h>
 
 #include <stdio.h>
@@ -52,7 +53,14 @@ void platformAlarmProcess(otInstance *aInstance)
 {
 	if (timer_ms_fired) {
 		timer_ms_fired = false;
-		otPlatAlarmMilliFired(aInstance);
+#if defined(CONFIG_OPENTHREAD_DIAG)
+		if (otPlatDiagModeGet()) {
+			otPlatDiagAlarmFired(aInstance);
+		} else
+#endif
+		{
+			otPlatAlarmMilliFired(aInstance);
+		}
 	}
 #if OPENTHREAD_CONFIG_PLATFORM_USEC_TIMER_ENABLE
 	if (timer_us_fired) {
