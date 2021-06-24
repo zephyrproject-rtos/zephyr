2ab09afd324290e883c5e7430eb4625e667f2221
Szymon Janc <szymon.janc@codecoup.pl>
2021-06-24 08:42:33 +0200

    Bluetooth: controller: Fix Chan Map Update tests
    This includes:
     Update of HCI tests
     Add missing test for slave role
     Verifying connection channel map for master role
    
    Signed-off-by: Szymon Janc <szymon.janc@codecoup.pl>

--- tests/bluetooth/controller/ctrl_chmu/src/main.c
+++ tests/bluetooth/controller/ctrl_chmu/src/main.c
@@ -49,11 +49,12 @@ static bool is_instant_reached(struct ll_conn *conn, uint16_t instant)
 void test_channel_map_update_mas_loc(void)
 {
 	uint8_t chm[5] = {0x00, 0x04, 0x05, 0x06, 0x00};
+	/* TODO should test setup set this to valid value? */
+	uint8_t defchm[5] = {};
 	uint8_t err;
 	struct node_tx *tx;
 	struct pdu_data *pdu;
 	uint16_t instant;
-
 	struct pdu_data_llctrl_chan_map_ind chmu_ind = {
 		.instant = 6,
 		.chm = {0x00, 0x04, 0x05, 0x06, 0x00},
@@ -85,7 +86,7 @@ void test_channel_map_update_mas_loc(void)
 	/* Release Tx */
 	ull_cp_release_tx(tx);
 
-	/* */
+	/* spin conn events */
 	while (!is_instant_reached(&conn, instant)) {
 		/* Prepare */
 		event_prepare(&conn);
@@ -98,6 +99,10 @@ void test_channel_map_update_mas_loc(void)
 
 		/* There should NOT be a host notification */
 		ut_rx_q_is_empty();
+
+		/* check if using old channel map */
+		zassert_mem_equal(conn.lll.data_chan_map, defchm,
+				sizeof(conn.lll.data_chan_map), "Channel map invalid");
 	}
 
 	/* Prepare */
@@ -112,9 +117,79 @@ void test_channel_map_update_mas_loc(void)
 	/* There should be no host notification */
 	ut_rx_q_is_empty();
 
-	/*TODO verify if new chm is in use... ? */
+	/* at this point new channel map shall be in use */
+	zassert_mem_equal(conn.lll.data_chan_map, chm,
+			sizeof(conn.lll.data_chan_map), "Channel map invalid");
 
 	zassert_equal(ctx_buffers_free(), PROC_CTX_BUF_NUM, "Free CTX buffers %d", ctx_buffers_free());
+}
+
+void test_channel_map_update_sla_rem(void)
+{
+	uint8_t chm[5] = {0x00, 0x04, 0x05, 0x06, 0x00};
+	/* TODO should test setup set this to valid value? */
+	uint8_t defchm[5] = {};
+	struct pdu_data_llctrl_chan_map_ind chmu_ind = {
+		.instant = 6,
+		.chm = {0x00, 0x04, 0x05, 0x06, 0x00},
+	};
+	uint16_t instant = 6;
+
+	/* Role */
+	test_set_role(&conn, BT_HCI_ROLE_SLAVE);
+
+	/* Connect */
+	ull_cp_state_set(&conn, ULL_CP_CONNECTED);
+
+	/* Prepare */
+	event_prepare(&conn);
+
+	/* Tx Queue should NOT have a LL Control PDU */
+	lt_rx_q_is_empty(&conn);
+
+	/* RX */
+	lt_tx(LL_CHAN_MAP_UPDATE_IND, &conn, &chmu_ind);
+
+	/* Done */
+	event_done(&conn);
+
+	/* spin conn events */
+	while (!is_instant_reached(&conn, instant)) {
+		/* Prepare */
+		event_prepare(&conn);
+
+		/* Tx Queue should NOT have a LL Control PDU */
+		lt_rx_q_is_empty(&conn);
+
+		/* Done */
+		event_done(&conn);
+
+		/* There should NOT be a host notification */
+		ut_rx_q_is_empty();
+
+		/* check if using old channel map */
+		zassert_mem_equal(conn.lll.data_chan_map, defchm,
+				sizeof(conn.lll.data_chan_map), "Channel map invalid");
+	}
+
+	/* Prepare */
+	event_prepare(&conn);
+
+	/* Tx Queue should NOT have a LL Control PDU */
+	lt_rx_q_is_empty(&conn);
+
+	/* Done */
+	event_done(&conn);
+
+	/* There should be no host notification */
+	ut_rx_q_is_empty();
+
+	/* at this point new channel map shall be in use */
+	zassert_mem_equal(conn.lll.data_chan_map, chm,
+			sizeof(conn.lll.data_chan_map), "Channel map invalid");
+
+	zassert_equal(ctx_buffers_free(), PROC_CTX_BUF_NUM, "Free CTX buffers %d", ctx_buffers_free());
+
 }
 
 void test_channel_map_update_sla_loc(void)
@@ -138,6 +213,7 @@ void test_main(void)
 {
 	ztest_test_suite(chmu,
 			 ztest_unit_test_setup_teardown(test_channel_map_update_mas_loc, setup, unit_test_noop),
+			 ztest_unit_test_setup_teardown(test_channel_map_update_sla_rem, setup, unit_test_noop),
 			 ztest_unit_test_setup_teardown(test_channel_map_update_sla_loc, setup, unit_test_noop)
 			);
 
