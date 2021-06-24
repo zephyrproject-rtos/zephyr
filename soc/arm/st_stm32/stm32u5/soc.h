ad1aea87cfc2d3981dd5e484d7916f2a600dd8bc
Erwan Gouriou <erwan.gouriou@linaro.org>
2021-06-24 09:57:58 +0200

    soc/arm: st_stm32: Add stm32u5 SoC series
    Add minimal support for STM32U5 series.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

---
+++ soc/arm/st_stm32/stm32u5/soc.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file SoC configuration macros for the STM32U5 family processors.
+ *
+ */
+
+
+#ifndef _STM32U5_SOC_H_
+#define _STM32U5_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <stm32u5xx.h>
+
+/* Add generated devicetree information and STM32 helper macros */
+#include <st_stm32_dt.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _STM32U5_SOC_H_ */
