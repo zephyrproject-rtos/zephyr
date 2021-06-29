e649e3386a276d94b33c560e1c4a20c2cab8cee0
Huifeng Zhang <Huifeng.Zhang@arm.com>
2021-06-29 15:07:55 +0800

    soc: arm64: arm: fvp_base_r: define a strong pm_cpu_on() function
    With this patch, zephyr can enable SMP directly. Otherwise
    zephyr needs TB-R to provide psci function.
    
    Signed-off-by: Huifeng Zhang <Huifeng.Zhang@arm.com>

--- soc/arm64/arm/fvp_aemv8r/soc.c
+++ soc/arm64/arm/fvp_aemv8r/soc.c
@@ -21,3 +21,16 @@ void z_arm64_el2_plat_init(void)
 }
 
 #endif
+
+#ifdef CONFIG_SOC_FVP_AEMV8R_SIMULATE_CPU_PM
+
+/*
+ * fvp can work on the mode that all core run together when it start.
+ * So an implementation for FVP is needed.
+ */
+int pm_cpu_on(unsigned long cpuid, uintptr_t entry_point)
+{
+	return 0;
+}
+
+#endif
