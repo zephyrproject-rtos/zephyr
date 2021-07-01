6c41bf6135fc8e48590689568e88b97d6228780f
Omkar Kulkarni <omkar.kulkarni@nordicsemi.no>
2021-07-01 14:33:53 +0200

    bsim_bt: mesh: persistence storage testing
    Adds support for testing of persistence storage
    for mesh Babblesim tests. Adds a sample test
    for testing provisioning data.
    
    Signed-off-by: Omkar Kulkarni <omkar.kulkarni@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_mesh/src/main.c
+++ tests/bluetooth/bsim_bt/bsim_test_mesh/src/main.c
@@ -7,17 +7,25 @@
 #include "bstests.h"
 #include "mesh_test.h"
 
-extern struct bst_test_list *
-test_transport_install(struct bst_test_list *tests);
-struct bst_test_list *test_friendship_install(struct bst_test_list *tests);
-struct bst_test_list *test_provision_install(struct bst_test_list *tests);
-struct bst_test_list *test_beacon_install(struct bst_test_list *tests);
+#if defined(CONFIG_SETTINGS)
+extern struct bst_test_list *test_persistence_install(struct bst_test_list *tests);
+#else
+extern struct bst_test_list *test_transport_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_friendship_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_provision_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_beacon_install(struct bst_test_list *tests);
+#endif
+
 
 bst_test_install_t test_installers[] = {
+#if defined(CONFIG_SETTINGS)
+	test_persistence_install,
+#else
 	test_transport_install,
 	test_friendship_install,
 	test_provision_install,
 	test_beacon_install,
+#endif
 	NULL
 };
 
