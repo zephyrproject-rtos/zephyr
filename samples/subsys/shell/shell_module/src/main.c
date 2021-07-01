caf4a8bcfa47d74f4e16eef81e1d5ac9185476b0
Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>
2021-07-01 11:00:15 +0200

    lib: getopt: rework and extend getopt library
    Getopt has been rework in this way that calling it does not require
    extra state parameter and its execution is thread safe.
    Global parameters describing the state of the getopt function have been
    made available to ensure full API compatibility in using this library.
    However, referencing these global variables directly is not thread
    safe. In order to get the state of the getopt function for the thread
    that is currently using it, call: getopt_state_get();
    
    Extended the library with getopt_long and getopt_long_only functions.
    
    Moved getopt libary from utils to posix.
    
    Signed-off-by: Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>

--- samples/subsys/shell/shell_module/src/main.c
+++ samples/subsys/shell/shell_module/src/main.c
@@ -100,7 +100,9 @@ static int cmd_demo_board(const struct shell *sh, size_t argc, char **argv)
 }
 
 #if defined CONFIG_SHELL_GETOPT
-static int cmd_demo_getopt(const struct shell *shell, size_t argc, char **argv)
+/* Thread save usage */
+static int cmd_demo_getopt_ts(const struct shell *sh, size_t argc,
+			      char **argv)
 {
 	struct getopt_state *state;
 	char *cvalue = NULL;
@@ -108,8 +110,8 @@ static int cmd_demo_getopt(const struct shell *shell, size_t argc, char **argv)
 	int bflag = 0;
 	int c;
 
-	while ((c = shell_getopt(shell, argc, argv, "abhc:")) != -1) {
-		state = shell_getopt_state_get(shell);
+	while ((c = getopt(argc, argv, "abhc:")) != -1) {
+		state = getopt_state_get();
 		switch (c) {
 		case 'a':
 			aflag = 1;
@@ -125,19 +127,19 @@ static int cmd_demo_getopt(const struct shell *shell, size_t argc, char **argv)
 			 * command handler to print help message. It must
 			 * be done explicitly.
 			 */
-			shell_help(shell);
+			shell_help(sh);
 			return SHELL_CMD_HELP_PRINTED;
 		case '?':
 			if (state->optopt == 'c') {
-				shell_print(shell,
+				shell_print(sh,
 					"Option -%c requires an argument.",
 					state->optopt);
 			} else if (isprint(state->optopt)) {
-				shell_print(shell,
+				shell_print(sh,
 					"Unknown option `-%c'.",
 					state->optopt);
 			} else {
-				shell_print(shell,
+				shell_print(sh,
 					"Unknown option character `\\x%x'.",
 					state->optopt);
 			}
@@ -147,7 +149,56 @@ static int cmd_demo_getopt(const struct shell *shell, size_t argc, char **argv)
 		}
 	}
 
-	shell_print(shell, "aflag = %d, bflag = %d", aflag, bflag);
+	shell_print(sh, "aflag = %d, bflag = %d", aflag, bflag);
+	return 0;
+}
+
+static int cmd_demo_getopt(const struct shell *sh, size_t argc,
+			      char **argv)
+{
+	char *cvalue = NULL;
+	int aflag = 0;
+	int bflag = 0;
+	int c;
+
+	while ((c = getopt(argc, argv, "abhc:")) != -1) {
+		switch (c) {
+		case 'a':
+			aflag = 1;
+			break;
+		case 'b':
+			bflag = 1;
+			break;
+		case 'c':
+			cvalue = optarg;
+			break;
+		case 'h':
+			/* When getopt is active shell is not parsing
+			 * command handler to print help message. It must
+			 * be done explicitly.
+			 */
+			shell_help(sh);
+			return SHELL_CMD_HELP_PRINTED;
+		case '?':
+			if (optopt == 'c') {
+				shell_print(sh,
+					"Option -%c requires an argument.",
+					optopt);
+			} else if (isprint(optopt)) {
+				shell_print(sh, "Unknown option `-%c'.",
+					optopt);
+			} else {
+				shell_print(sh,
+					"Unknown option character `\\x%x'.",
+					optopt);
+			}
+			return 1;
+		default:
+			break;
+		}
+	}
+
+	shell_print(sh, "aflag = %d, bflag = %d", aflag, bflag);
 	return 0;
 }
 #endif
@@ -322,8 +373,12 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sub_demo,
 	SHELL_CMD(ping, NULL, "Ping command.", cmd_demo_ping),
 	SHELL_CMD(board, NULL, "Show board name command.", cmd_demo_board),
 #if defined CONFIG_SHELL_GETOPT
-	SHELL_CMD(getopt, NULL,	"Cammand using getopt, looking for: \"abhc:\".",
-		  cmd_demo_getopt),
+	SHELL_CMD(getopt_thread_safe, NULL,
+		  "Cammand using getopt in thread safe way"
+		  " looking for: \"abhc:\".",
+		  cmd_demo_getopt_ts),
+	SHELL_CMD(getopt, NULL, "Cammand using getopt in non thread safe way"
+		  " looking for: \"abhc:\".\n", cmd_demo_getopt),
 #endif
 	SHELL_SUBCMD_SET_END /* Array terminated. */
 );
