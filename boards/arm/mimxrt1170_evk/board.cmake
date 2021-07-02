7d98dec4748a0c7deeb0c73408682c87dc8fad04
Lucien Zhao <lucien.zhao@nxp.com>
2021-07-02 16:29:18 +0800

    boards: arm: mimxrt1170_evk: Add rt1170_cm4 core debug way
    Add cm4 core debug way into board.cmake
    
    Signed-off-by: Lucien Zhao <lucien.zhao@nxp.com>

--- boards/arm/mimxrt1170_evk/board.cmake
+++ boards/arm/mimxrt1170_evk/board.cmake
@@ -4,8 +4,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+if(CONFIG_SOC_MIMXRT1176_CM7)
 board_runner_args(pyocd "--target=mimxrt1170_cm7")
 board_runner_args(jlink "--device=MIMXRT1176xxxA_M7" "--reset-after-load")
+endif()
+
+if(CONFIG_SOC_MIMXRT1176_CM4)
+board_runner_args(pyocd "--target=mimxrt1170_cm4")
+# Note: Please use JLINK above V7.50 (Only support run cm4 image when debugging due to default boot core on board is cm7 core)
+board_runner_args(jlink "--device=MIMXRT1176xxxA_M4")
+endif()
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
