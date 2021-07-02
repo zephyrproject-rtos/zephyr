12e2a4976909124ca54dbb5806811e4f56929f89
Emil Gydesen <emil.gydesen@nordicsemi.no>
2021-07-02 13:36:59 +0200

    Bluetooth: Audio: Add broadcast source test
    Add the inital test of the broadcast source that simply
    create a broadcast source. This will eventually replace
    the bass_broadcaster_test.c
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_audio/src/main.c
+++ tests/bluetooth/bsim_bt/bsim_test_audio/src/main.c
@@ -21,6 +21,7 @@ extern struct bst_test_list *test_media_controller_install(struct bst_test_list 
 extern struct bst_test_list *test_bass_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_bass_client_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_bass_broadcaster_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_broadcast_source_install(struct bst_test_list *tests);
 
 
 bst_test_install_t test_installers[] = {
@@ -38,6 +39,7 @@ bst_test_install_t test_installers[] = {
 	test_bass_install,
 	test_bass_client_install,
 	test_bass_broadcaster_install,
+	test_broadcast_source_install,
 	NULL
 };
 
