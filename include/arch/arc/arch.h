8d6fff4e7cf4c091dd7e3f7c1a0f32ad076683ff
Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>
2021-07-08 15:30:19 +0300

    ARC: MWDT: workaround paddr_t defined in both Zephyr and toolchain
    MWDT provides paddr_t type and it conflicts witn Zephyr definition:
    - Zephyr defines paddr_t as a uintptr_t
    - MWDT defines paddr_t as a unsigned long
    This causes extra warnings. However we can safely define
    paddr_t as a unsigned long for the case when MWDT toolchain is used as
    they are both unsighned, have same size and aligning.
    
    Signed-off-by: Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
    Signed-off-by: Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>

--- include/arch/arc/arch.h
+++ include/arch/arc/arch.h
@@ -29,7 +29,7 @@
 #include <arch/arc/v2/aux_regs.h>
 #include <arch/arc/v2/arcv2_irq_unit.h>
 #include <arch/arc/v2/asm_inline.h>
-#include <arch/common/addr_types.h>
+#include <arch/arc/arc_addr_types.h>
 #include <arch/arc/v2/error.h>
 
 #ifdef CONFIG_ARC_CONNECT
