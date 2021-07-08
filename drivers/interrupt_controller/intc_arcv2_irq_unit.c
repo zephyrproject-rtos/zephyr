zephyrproject-rtos/zephyr

commit cc2f0e9c08dcad59a4cc0117028a5fcf9d73f433

pm: use enum for device PM states
Move all PM_DEVICE_STATE_* definitions to an enum. The
PM_DEVICE_STATE_SET and PM_DEVICE_STATE_GET definitions have been kept
out of the enum since they do not represent any state. However, their
name has not been changed since they will be removed soon.

All drivers and tests have been adjusted accordingly.

Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

drivers/interrupt_controller/intc_arcv2_irq_unit.c
@@ -192,16 +192,17 @@ static int arc_v2_irq_unit_get_state(const struct device *dev)
  * @return operation result
  */
 static int arc_v2_irq_unit_device_ctrl(const struct device *dev,
-				       uint32_t ctrl_command, uint32_t *context,
+				       uint32_t ctrl_command,
+				       enum pm_device_state *state,
 				       pm_device_cb cb, void *arg)
 {
 	int ret = 0;
 	unsigned int key = arch_irq_lock();
 
 	if (ctrl_command == PM_DEVICE_STATE_SET) {
-		if (*((uint32_t *)context) == PM_DEVICE_STATE_SUSPEND) {
+		if (*state == PM_DEVICE_STATE_SUSPEND) {
 			ret = arc_v2_irq_unit_suspend(dev);
-		} else if (*((uint32_t *)context) == PM_DEVICE_STATE_ACTIVE) {
+		} else if (*state == PM_DEVICE_STATE_ACTIVE) {
 			ret = arc_v2_irq_unit_resume(dev);
 		}
 	} else if (ctrl_command == PM_DEVICE_STATE_GET) {

drivers/display/display_st7735r.c
@@ -68,7 +68,7 @@ struct st7735r_data {
 	uint16_t x_offset;
 	uint16_t y_offset;
 #ifdef CONFIG_PM_DEVICE
-	uint32_t pm_state;
+	enum pm_device_state pm_state;
 #endif
 };
 
@@ -520,14 +520,15 @@ static int st7735r_enter_sleep(struct st7735r_data *data)
 }
 
 static int st7735r_pm_control(const struct device *dev, uint32_t ctrl_command,
-			      uint32_t *state, pm_device_cb cb, void *arg)
+			      enum pm_device_state *state,
+			      pm_device_cb cb, void *arg)
 {
 	int ret = 0;
 	struct st7735r_data *data = (struct st7735r_data *)dev->data;
 
 	switch (ctrl_command) {
 	case PM_DEVICE_STATE_SET:
-		if (*((uint32_t *)context) == PM_DEVICE_STATE_ACTIVE) {
+		if (*state == PM_DEVICE_STATE_ACTIVE) {
 			ret = st7735r_exit_sleep(data);
 			if (ret < 0) {
 				return ret;

drivers/display/display_st7789v.c
@@ -53,7 +53,7 @@ struct st7789v_data {
 	uint16_t x_offset;
 	uint16_t y_offset;
 #ifdef CONFIG_PM_DEVICE
-	uint32_t pm_state;
+	enum pm_device_state pm_state;
 #endif
 };
 
@@ -409,7 +409,7 @@ static void st7789v_enter_sleep(struct st7789v_data *data)
 }
 
 static int st7789v_pm_control(const struct device *dev, uint32_t ctrl_command,
-				 uint32_t *state, pm_device_cb cb, void *arg)
+				 enum pm_device_state *state, pm_device_cb cb, void *arg)
 {
 	int ret = 0;
 	struct st7789v_data *data = (struct st7789v_data *)dev->data;

drivers/entropy/entropy_cc13xx_cc26xx.c
@@ -37,7 +37,7 @@ struct entropy_cc13xx_cc26xx_data {
 	bool constrained;
 #endif
 #ifdef CONFIG_PM_DEVICE
-	uint32_t pm_state;
+	enum device_pm_state pm_state;
 #endif
 };
 
@@ -268,7 +268,7 @@ static int post_notify_fxn(unsigned int eventType, uintptr_t eventArg,
 
 #ifdef CONFIG_PM_DEVICE
 static int entropy_cc13xx_cc26xx_set_power_state(const struct device *dev,
-						 uint32_t new_state)
+						 enum pm_device_state new_state)
 {
 	struct entropy_cc13xx_cc26xx_data *data = get_dev_data(dev);
 	int ret = 0;
@@ -294,14 +294,14 @@ static int entropy_cc13xx_cc26xx_set_power_state(const struct device *dev,
 
 static int entropy_cc13xx_cc26xx_pm_control(const struct device *dev,
 					    uint32_t ctrl_command,
-					    uint32_t *state, pm_device_cb cb,
+					    enum pm_device_state *state, pm_device_cb cb,
 					    void *arg)
 {
 	struct entropy_cc13xx_cc26xx_data *data = get_dev_data(dev);
 	int ret = 0;
 
 	if (ctrl_command == PM_DEVICE_STATE_SET) {
-		uint32_t new_state = *state;
+		enum pm_device_state new_state = *state;
 
 		if (new_state != data->pm_state) {
 			ret = entropy_cc13xx_cc26xx_set_power_state(dev,

drivers/ethernet/eth_mcux.c
@@ -186,7 +186,7 @@ void eth_mcux_phy_stop(struct eth_context *context);
 
 static int eth_mcux_device_pm_control(const struct device *dev,
 				      uint32_t command,
-				      uint32_t *state, pm_device_cb cb,
+				      enum pm_device_state *state, pm_device_cb cb,
 				      void *arg)
 {
 	struct eth_context *eth_ctx = (struct eth_context *)dev->data;

drivers/flash/spi_flash_at45.c
@@ -61,7 +61,7 @@ struct spi_flash_at45_data {
 	struct spi_cs_control spi_cs;
 	struct k_sem lock;
 #if IS_ENABLED(CONFIG_PM_DEVICE)
-	uint32_t pm_state;
+	enum pm_device_state pm_state;
 #endif
 };
 
@@ -629,7 +629,7 @@ static int spi_flash_at45_init(const struct device *dev)
 #if IS_ENABLED(CONFIG_PM_DEVICE)
 static int spi_flash_at45_pm_control(const struct device *dev,
 				     uint32_t ctrl_command,
-				     uint32_t *state, pm_device_cb cb,
+				     enum pm_device_state *state, pm_device_cb cb,
 				     void *arg)
 {
 	struct spi_flash_at45_data *dev_data = get_dev_data(dev);
@@ -637,7 +637,7 @@ static int spi_flash_at45_pm_control(const struct device *dev,
 	int err = 0;
 
 	if (ctrl_command == PM_DEVICE_STATE_SET) {
-		uint32_t new_state = *state;
+		enum pm_device_state new_state = *state;
 
 		if (new_state != dev_data->pm_state) {
 			switch (new_state) {

drivers/gpio/gpio_dw.c
@@ -461,7 +461,7 @@ static inline int gpio_dw_resume_from_suspend_port(const struct device *port)
 */
 static int gpio_dw_device_ctrl(const struct device *port,
 			       uint32_t ctrl_command,
-			       uint32_t *state, pm_device_cb cb, void *arg)
+			       enum pm_device_state *state, pm_device_cb cb, void *arg)
 {
 	int ret = 0;
 

drivers/gpio/gpio_dw.h
@@ -42,7 +42,7 @@ struct gpio_dw_runtime {
 #endif
 	sys_slist_t callbacks;
 #ifdef CONFIG_PM_DEVICE
-	uint32_t device_power_state;
+	enum pm_device_state device_power_state;
 #endif
 };
 

commit 2732c1a0d873659954d9dbcc8436e8f765d4be4c

drivers: intc_arcv2_irq_unit: Fix arc_v2_irq_unit_device_ctrl
Fix some leftovers from the pm_device_state changes.

Fixes build problem introduced in
cc2f0e9c08dcad59a4cc0117028a5fcf9d73f433.

Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

drivers/interrupt_controller/intc_arcv2_irq_unit.c
@@ -32,7 +32,8 @@ extern void *_VectorTable;
 #define _ARC_V2_IRQ_VECT_BASE _ARC_V2_IRQ_VECT_BASE_S
 #endif
 
-static uint32_t _arc_v2_irq_unit_device_power_state = PM_DEVICE_STATE_ACTIVE;
+static enum pm_device_state _arc_v2_irq_unit_device_power_state =
+	PM_DEVICE_STATE_ACTIVE;
 struct arc_v2_irq_unit_ctx {
 	uint32_t irq_ctrl; /* Interrupt Context Saving Control Register. */
 	uint32_t irq_vect_base; /* Interrupt Vector Base. */
@@ -176,7 +177,7 @@ static int arc_v2_irq_unit_resume(const struct device *dev)
  *
  * @return the power state of interrupt unit
  */
-static int arc_v2_irq_unit_get_state(const struct device *dev)
+static enum pm_device_state arc_v2_irq_unit_get_state(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 
@@ -206,13 +207,13 @@ static int arc_v2_irq_unit_device_ctrl(const struct device *dev,
 			ret = arc_v2_irq_unit_resume(dev);
 		}
 	} else if (ctrl_command == PM_DEVICE_STATE_GET) {
-		*((uint32_t *)context) = arc_v2_irq_unit_get_state(dev);
+		*state = arc_v2_irq_unit_get_state(dev);
 	}
 
 	arch_irq_unlock(key);
 
 	if (cb) {
-		cb(dev, ret, context, arg);
+		cb(dev, ret, state, arg);
 	}
 
 	return ret;