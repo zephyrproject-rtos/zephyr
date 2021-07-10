1f811f5b40b9100e3dadc90894cadb0bbaef1014
Scott Worley <scott.worley@microchip.com>
2021-07-10 10:39:23 -0400

    Microchip: MEC172x add board and update for minimal build
    Added minimal device tree and board files to build Microchip
    MEC172x. SOC layer stripped down to allow build for checking
    compilation only.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

---
+++ boards/arm/mec172xevb_assy6906/pinmux.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 Microchip Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <device.h>
+#include <init.h>
+#include <kernel.h>
+#include <drivers/pinmux.h>
+
+#include "soc.h"
+
+/*
+ * If PINMUX will be used this is where board_pinmux_init should be implemented
+ * and added to SYS_INIT.
+ */
