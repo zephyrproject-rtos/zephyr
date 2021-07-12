457fd3e7c6f78afb06199457de721dc408a8b248
Lingao Meng <menglingao@xiaomi.com>
2021-07-11 18:34:27 -0700

    Bluetooth: Mesh: Delete bt_mesh_proxy_prov_disable parameter.
    we will no longer need the additional `disconnect` parameter,
    such as we only process gatt database from disconnect handler.
    
    Signed-off-by: Lingao Meng <menglingao@xiaomi.com>

--- subsys/bluetooth/mesh/prov_device.c
+++ subsys/bluetooth/mesh/prov_device.c
@@ -634,6 +634,10 @@ int bt_mesh_prov_disable(bt_mesh_prov_bearer_t bearers)
 		return -EALREADY;
 	}
 
+	if (bt_mesh_prov_active()) {
+		return -EBUSY;
+	}
+
 	if (IS_ENABLED(CONFIG_BT_MESH_PB_ADV) &&
 	    (bearers & BT_MESH_PROV_ADV)) {
 		bt_mesh_beacon_disable();
@@ -642,7 +646,7 @@ int bt_mesh_prov_disable(bt_mesh_prov_bearer_t bearers)
 
 	if (IS_ENABLED(CONFIG_BT_MESH_PB_GATT) &&
 	    (bearers & BT_MESH_PROV_GATT)) {
-		bt_mesh_proxy_prov_disable(true);
+		(void)bt_mesh_proxy_prov_disable();
 	}
 
 	return 0;
