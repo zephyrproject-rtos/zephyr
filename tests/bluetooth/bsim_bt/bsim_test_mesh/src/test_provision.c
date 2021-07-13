zephyrproject-rtos/zephyr

commit 4e6cb116f87bb2ff0ece7d8f968adac4dd2c9b3d

Bluetooth: Mesh: transport: update delayable work
Switch to the new API. Adds early exits for the ack and retransmit
timers, and replaces a remaining_time() + submit() call with schedule().

Signed-off-by: Peter Bigot <peter.bigot@nordicsemi.no>
Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

subsys/bluetooth/mesh/transport.c
@@ -115,7 +115,7 @@ static struct seg_tx {
 			      friend_cred:1; /* Using Friend credentials */
 	const struct bt_mesh_send_cb *cb;
 	void                  *cb_data;
-	struct k_delayed_work retransmit;    /* Retransmit timer */
+	struct k_work_delayable retransmit;    /* Retransmit timer */
 } seg_tx[CONFIG_BT_MESH_TX_SEG_MSG_COUNT];
 
 static struct seg_rx {
@@ -133,7 +133,7 @@ static struct seg_rx {
 	uint8_t                     ttl;
 	uint32_t                    block;
 	uint32_t                    last;
-	struct k_delayed_work    ack;
+	struct k_work_delayable  ack;
 } seg_rx[CONFIG_BT_MESH_RX_SEG_MSG_COUNT];
 
 K_MEM_SLAB_DEFINE(segs, BT_MESH_APP_SEG_SDU_MAX, CONFIG_BT_MESH_SEG_BUFS, 4);
@@ -247,7 +247,7 @@ static void seg_tx_unblock_check(struct seg_tx *tx)
 		BT_DBG("Unblocked 0x%04x",
 		       (uint16_t)(blocked->seq_auth & TRANS_SEQ_ZERO_MASK));
 		blocked->blocked = false;
-		k_delayed_work_submit(&blocked->retransmit, K_NO_WAIT);
+		k_work_reschedule(&blocked->retransmit, K_NO_WAIT);
 	}
 }
 
@@ -255,7 +255,8 @@ static void seg_tx_reset(struct seg_tx *tx)
 {
 	int i;
 
-	k_delayed_work_cancel(&tx->retransmit);
+	/* If this call fails, the handler will exit early, as nack_count is 0. */
+	(void)k_work_cancel_delayable(&tx->retransmit);
 
 	tx->cb = NULL;
 	tx->cb_data = NULL;
@@ -315,9 +316,9 @@ static void schedule_retransmit(struct seg_tx *tx)
 	 * called this from inside bt_mesh_net_send), we should continue the
 	 * retransmit immediately, as we just freed up a tx buffer.
 	 */
-	k_delayed_work_submit(&tx->retransmit,
-			      tx->seg_o ? K_NO_WAIT :
-					  K_MSEC(SEG_RETRANSMIT_TIMEOUT(tx)));
+	k_work_reschedule(&tx->retransmit,
+			  tx->seg_o ? K_NO_WAIT :
+			  K_MSEC(SEG_RETRANSMIT_TIMEOUT(tx)));
 }
 
 static void seg_send_start(uint16_t duration, int err, void *user_data)
@@ -440,8 +441,8 @@ static void seg_tx_send_unacked(struct seg_tx *tx)
 
 end:
 	if (!tx->seg_pending) {
-		k_delayed_work_submit(&tx->retransmit,
-				      K_MSEC(SEG_RETRANSMIT_TIMEOUT(tx)));
+		k_work_reschedule(&tx->retransmit,
+				  K_MSEC(SEG_RETRANSMIT_TIMEOUT(tx)));
 	}
 
 	tx->sending = 0U;
@@ -859,8 +860,6 @@ static int trans_ack(struct bt_mesh_net_rx *rx, uint8_t hdr,
 		return -EINVAL;
 	}
 
-	k_delayed_work_cancel(&tx->retransmit);
-
 	while ((bit = find_lsb_set(ack))) {
 		if (tx->seg[bit - 1]) {
 			BT_DBG("seg %u/%u acked", bit - 1, tx->seg_n);
@@ -871,7 +870,11 @@ static int trans_ack(struct bt_mesh_net_rx *rx, uint8_t hdr,
 	}
 
 	if (tx->nack_count) {
-		seg_tx_send_unacked(tx);
+		/* According to the Bluetooth Mesh Profile specification,
+		 * section 3.5.3.3, we should reset the retransmit timer and
+		 * retransmit immediately when receiving a valid ack message:
+		 */
+		k_work_reschedule(&tx->retransmit, K_NO_WAIT);
 	} else {
 		BT_DBG("SDU TX complete");
 		seg_tx_complete(tx, 0);
@@ -1090,7 +1093,10 @@ static void seg_rx_reset(struct seg_rx *rx, bool full_reset)
 
 	BT_DBG("rx %p", rx);
 
-	k_delayed_work_cancel(&rx->ack);
+	/* If this fails, the handler will exit early on the next execution, as
+	 * it checks rx->in_use.
+	 */
+	(void)k_work_cancel_delayable(&rx->ack);
 
 	if (IS_ENABLED(CONFIG_BT_MESH_FRIEND) && rx->obo &&
 	    rx->block != BLOCK_COMPLETE(rx->seg_n)) {
@@ -1127,6 +1133,16 @@ static void seg_ack(struct k_work *work)
 	struct seg_rx *rx = CONTAINER_OF(work, struct seg_rx, ack);
 	int32_t timeout;
 
+	if (!rx->in_use || rx->block == BLOCK_COMPLETE(rx->seg_n)) {
+		/* Cancellation of this timer may have failed. If it fails as
+		 * part of seg_reset, in_use will be false.
+		 * If it fails as part of the processing of a fully received
+		 * SDU, the ack is already being sent from the receive handler,
+		 * and the timer based ack sending can be ignored.
+		 */
+		return;
+	}
+
 	BT_DBG("rx %p", rx);
 
 	if (k_uptime_get_32() - rx->last > (60 * MSEC_PER_SEC)) {
@@ -1144,7 +1160,7 @@ static void seg_ack(struct k_work *work)
 		 rx->block, rx->obo);
 
 	timeout = ack_timeout(rx);
-	k_delayed_work_submit(&rx->ack, K_MSEC(timeout));
+	k_work_schedule(&rx->ack, K_MSEC(timeout));
 }
 
 static inline bool sdu_len_is_ok(bool ctl, uint8_t seg_n)
@@ -1447,11 +1463,10 @@ found_rx:
 	/* Reset the Incomplete Timer */
 	rx->last = k_uptime_get_32();
 
-	if (!k_delayed_work_remaining_get(&rx->ack) &&
-	    !bt_mesh_lpn_established()) {
+	if (!bt_mesh_lpn_established()) {
 		int32_t timeout = ack_timeout(rx);
-
-		k_delayed_work_submit(&rx->ack, K_MSEC(timeout));
+		/* Should only start ack timer if it isn't running already: */
+		k_work_schedule(&rx->ack, K_MSEC(timeout));
 	}
 
 	/* Allocated segment here */
@@ -1487,7 +1502,10 @@ found_rx:
 
 	*pdu_type = BT_MESH_FRIEND_PDU_COMPLETE;
 
-	k_delayed_work_cancel(&rx->ack);
+	/* If this fails, the work handler will either exit early because the
+	 * block is fully received, or rx->in_use is false.
+	 */
+	(void)k_work_cancel_delayable(&rx->ack);
 	send_ack(net_rx->sub, net_rx->ctx.recv_dst, net_rx->ctx.addr,
 		 net_rx->ctx.send_ttl, seq_auth, rx->block, rx->obo);
 
@@ -1643,11 +1661,11 @@ void bt_mesh_trans_init(void)
 	int i;
 
 	for (i = 0; i < ARRAY_SIZE(seg_tx); i++) {
-		k_delayed_work_init(&seg_tx[i].retransmit, seg_retransmit);
+		k_work_init_delayable(&seg_tx[i].retransmit, seg_retransmit);
 	}
 
 	for (i = 0; i < ARRAY_SIZE(seg_rx); i++) {
-		k_delayed_work_init(&seg_rx[i].ack, seg_ack);
+		k_work_init_delayable(&seg_rx[i].ack, seg_ack);
 	}
 }
 

commit 8d41e029144eca28f369b01b4749e829b02373d6

Bluetooth: Mesh: Test provisioning multiple devices
Adds a BabbleSim test for provisioning multiple devices in a row. This
scenario had a regression in #33782, which is fixed in #35405.

Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

tests/bluetooth/bsim_bt/bsim_test_mesh/prj.conf
@@ -38,6 +38,6 @@ CONFIG_BT_MESH_PB_ADV=y
 CONFIG_BT_MESH_PROVISIONER=y
 CONFIG_BT_MESH_PROV_DEVICE=y
 CONFIG_BT_MESH_CDB=y
-CONFIG_BT_MESH_CDB_NODE_COUNT=3
+CONFIG_BT_MESH_CDB_NODE_COUNT=4
 
 CONFIG_BT_MESH_DEBUG=y

tests/bluetooth/bsim_bt/bsim_test_mesh/src/test_provision.c
@@ -17,18 +17,38 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
  *   Tests both the provisioner and device role in various scenarios.
  */
 
+#define PROV_MULTI_COUNT 3
+
+enum test_flags {
+	IS_PROVISIONER,
+
+	TEST_FLAGS,
+};
+
+static ATOMIC_DEFINE(flags, TEST_FLAGS);
+extern const struct bt_mesh_comp comp;
+extern const uint8_t test_net_key[16];
+extern uint global_device_nbr;
+
 /* Timeout semaphore */
 static struct k_sem prov_sem;
+static uint16_t prov_addr = 0x0002;
+static const uint8_t dev_key[16] = { 0x01, 0x02, 0x03, 0x04, 0x05 };
+static uint8_t dev_uuid[16] = { 0x6c, 0x69, 0x6e, 0x67, 0x61, 0x6f };
 
 #define WAIT_TIME 60 /*seconds*/
 
 static void test_device_init(void)
 {
+	/* Ensure that the UUID is unique: */
+	dev_uuid[6] = '0' + global_device_nbr;
+
 	bt_mesh_test_cfg_set(NULL, WAIT_TIME);
 }
 
 static void test_provisioner_init(void)
 {
+	atomic_set_bit(flags, IS_PROVISIONER);
 	bt_mesh_test_cfg_set(NULL, WAIT_TIME);
 }
 
@@ -36,24 +56,28 @@ static void unprovisioned_beacon(uint8_t uuid[16],
 				 bt_mesh_prov_oob_info_t oob_info,
 				 uint32_t *uri_hash)
 {
-	bt_mesh_provision_adv(uuid, 0, 0, 0);
+	if (!atomic_test_bit(flags, IS_PROVISIONER)) {
+		return;
+	}
+
+	bt_mesh_provision_adv(uuid, 0, prov_addr, 0);
 }
 
 static void prov_complete(uint16_t net_idx, uint16_t addr)
 {
-	k_sem_give(&prov_sem);
+	if (!atomic_test_bit(flags, IS_PROVISIONER)) {
+		k_sem_give(&prov_sem);
+	}
 }
 
 static void prov_node_added(uint16_t net_idx, uint8_t uuid[16], uint16_t addr,
 			    uint8_t num_elem)
 {
+	LOG_INF("Device 0x%04x provisioned", prov_addr);
+	prov_addr++;
 	k_sem_give(&prov_sem);
 }
 
-extern const struct bt_mesh_comp comp;
-extern const uint8_t test_net_key[16];
-static const uint8_t dev_key[16] = { 0x01, 0x02, 0x03, 0x04, 0x05 };
-static const uint8_t dev_uuid[16] = { 0x6c, 0x69, 0x6e, 0x67, 0x61, 0x6f };
 static struct bt_mesh_prov prov = {
 	.uuid = dev_uuid,
 	.unprovisioned_beacon = unprovisioned_beacon,
@@ -95,7 +119,8 @@ static void test_device_pb_adv_no_oob(void)
 
 	LOG_INF("Mesh initialized\n");
 
-	ASSERT_OK(k_sem_take(&prov_sem, K_SECONDS(20)),
+	/* Keep a long timeout so the prov multi case has time to finish: */
+	ASSERT_OK(k_sem_take(&prov_sem, K_SECONDS(40)),
 		  "Device provision fail");
 
 	PASS();
@@ -117,10 +142,32 @@ static void test_provisioner_pb_adv_no_oob(void)
 	err = bt_mesh_provision(test_net_key, 0, 0, 0, 0x0001, dev_key);
 	ASSERT_OK(err, "Provisioning failed (err %d)", err);
 
-	k_sem_take(&prov_sem, K_NO_WAIT);
-
-	ASSERT_OK(k_sem_take(&prov_sem, K_SECONDS(20)),
+	ASSERT_OK(k_sem_take(&prov_sem, K_SECONDS(5)),
 		  "Provisioner provision fail");
+
+	PASS();
+}
+
+/** @brief Verify that the provisioner can provision multiple devices in a row
+ */
+static void test_provisioner_pb_adv_multi(void)
+{
+	int err;
+
+	k_sem_init(&prov_sem, 0, 1);
+
+	bt_mesh_device_setup();
+
+	err = bt_mesh_cdb_create(test_net_key);
+	ASSERT_OK(err, "Failed to create CDB (err %d)\n", err);
+
+	err = bt_mesh_provision(test_net_key, 0, 0, 0, 0x0001, dev_key);
+	ASSERT_OK(err, "Provisioning failed (err %d)", err);
+
+	for (int i = 0; i < PROV_MULTI_COUNT; i++) {
+		ASSERT_OK(k_sem_take(&prov_sem, K_SECONDS(20)),
+			  "Provisioner provision #%d fail", i);
+	}
 
 	PASS();
 }
@@ -139,6 +186,8 @@ static const struct bst_test_instance test_connect[] = {
 
 	TEST_CASE(provisioner, pb_adv_no_oob,
 		  "Provisioner: pb-adv provisioning use no-oob method"),
+	TEST_CASE(provisioner, pb_adv_multi,
+		  "Provisioner: pb-adv provisioning multiple devices"),
 
 	BSTEST_END_MARKER
 };

tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/provision/pb_adv_multi.sh
@@ -1,0 +1,14 @@
+#!/usr/bin/env bash
+# Copyright 2021 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+# Provision 3 devices in succession:
+# Note that the number of devices must match the
+# PROV_MULTI_COUNT define in test_provision.c
+RunTest mesh_provision_pb_adv_multi \
+	prov_provisioner_pb_adv_multi \
+	prov_device_pb_adv_no_oob \
+	prov_device_pb_adv_no_oob \
+	prov_device_pb_adv_no_oob \