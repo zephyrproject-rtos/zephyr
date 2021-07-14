fd588b8e2bf93e2fc414d0f35ebb848dfcb6ab8d
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2021-07-14 10:35:42 +0530

    Bluetooth: Controller: Make adva and tgta check function file static
    The functions to check advertising and target address in a
    PDU are used only inside the same file, hence make them file
    static functions.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan_internal.h
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan_internal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2019 Nordic Semiconductor ASA
+ * Copyright (c) 2018-2021 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -8,11 +8,5 @@ void lll_scan_prepare_connect_req(struct lll_scan *lll, struct pdu_adv *pdu_tx,
 				  uint8_t phy, uint8_t adv_tx_addr,
 				  uint8_t *adv_addr, uint8_t init_tx_addr,
 				  uint8_t *init_addr, uint32_t *conn_space_us);
-
-bool lll_scan_adva_check(struct lll_scan *lll, uint8_t addr_type, uint8_t *addr,
-			 uint8_t rl_idx);
-
-bool lll_scan_tgta_check(struct lll_scan *lll, bool init, uint8_t addr_type,
-			 uint8_t *addr, uint8_t rl_idx, bool *dir_report);
 bool lll_scan_ext_tgta_check(struct lll_scan *lll, bool pri, bool is_init,
 			     struct pdu_adv *pdu, uint8_t rl_idx);
