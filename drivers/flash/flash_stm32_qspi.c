ebb70c07446a8acf9e82e1fd789ada6ad6694bfc
Dominik Ermel <dominik.ermel@nordicsemi.no>
2021-07-14 10:40:55 +0000

    drivers/flash/stm32_qspi: Add flash_get_size
    The commit adds implementation of flash_get_size.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- drivers/flash/flash_stm32_qspi.c
+++ drivers/flash/flash_stm32_qspi.c
@@ -755,6 +755,15 @@ flash_stm32_qspi_get_parameters(const struct device *dev)
 	return &flash_stm32_qspi_parameters;
 }
 
+static int flash_stm32_qspi_get_size(const struct device *dev, uint64_t *size)
+{
+	const struct flash_stm32_qspi_config *dev_cfg = dev->config;
+
+	*size = (uint64_t)dev_cfg->flash_size;
+
+	return 0;
+}
+
 static void flash_stm32_qspi_isr(const struct device *dev)
 {
 	struct flash_stm32_qspi_data *dev_data = dev->data;
@@ -881,6 +890,7 @@ static const struct flash_driver_api flash_stm32_qspi_driver_api = {
 	.write = flash_stm32_qspi_write,
 	.erase = flash_stm32_qspi_erase,
 	.get_parameters = flash_stm32_qspi_get_parameters,
+	.get_size = flash_stm32_qspi_get_size,
 #if defined(CONFIG_FLASH_PAGE_LAYOUT)
 	.page_layout = flash_stm32_qspi_pages_layout,
 #endif
