d3f6b54f8e9c112f92971a458422b5ace02c7f66
Scott Worley <scott.worley@microchip.com>
2021-07-19 14:14:20 -0400

    Microchip: MEC172x clock control driver
    Implement a clock control driver for Microchip MEC172x handling
    configuring the 32 KHz input sources for the PLL and peripheral-32k
    clock domains. MEC172x differs from MEC152x. MEC152x had one 32K source
    for both PLL and peripherals. MEC172x allows the two domains to use
    independent 32 KHz sources. Device tree updated to provide addresses
    of hardware used by the driver.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

--- soc/arm/microchip_mec/mec172x/soc.h
+++ soc/arm/microchip_mec/mec172x/soc.h
@@ -58,6 +58,7 @@
 
 /* common SoC API */
 #include "../common/soc_gpio.h"
+#include "../common/soc_pcr.h"
 #include "../common/soc_pins.h"
 #include "../common/soc_espi_channels.h"
 #include "../common/soc_espi_saf.h"
