fa22be54c3157c9bee43cd78d922154e5c3359ee
Jaxson Han <jaxson.han@arm.com>
2021-07-20 16:32:13 +0800

    include: arm64: Refine the mem alignment macros
    Add a new macro MEM_DOMAIN_ALIGN_AND_SIZE for mmu and mpu mem
    alignment.
    MEM_DOMAIN_ALIGN_AND_SIZE is
      - CONFIG_MMU_PAGE_SIZE, when mmu is enabled.
      - CONFIG_ARM_MPU_REGION_MIN_ALIGN_AND_SIZE when mpu enabled.
    
    Signed-off-by: Jaxson Han <jaxson.han@arm.com>

--- include/arch/arm64/mm.h
+++ include/arch/arm64/mm.h
@@ -8,8 +8,19 @@
 
 #if defined(CONFIG_ARM_MMU)
 #include <arch/arm64/arm_mmu.h>
+/*
+ * When mmu enabled, some section addresses need to be aligned with
+ * page size which is CONFIG_MMU_PAGE_SIZE
+ */
+#define MEM_DOMAIN_ALIGN_AND_SIZE CONFIG_MMU_PAGE_SIZE
 #elif defined(CONFIG_ARM_MPU)
 #include <arch/arm64/cortex_r/arm_mpu.h>
+/*
+ * When mpu enabled, some section addresses need to be aligned with
+ * mpu region min align size which is
+ * CONFIG_ARM_MPU_REGION_MIN_ALIGN_AND_SIZE
+ */
+#define MEM_DOMAIN_ALIGN_AND_SIZE CONFIG_ARM_MPU_REGION_MIN_ALIGN_AND_SIZE
 #endif
 
 #ifndef _ASMLANGUAGE
