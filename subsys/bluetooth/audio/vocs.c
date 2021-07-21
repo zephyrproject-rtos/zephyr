cce921dad52a3f7bab288d9f9d1e91bf7c46ce5d
Lingao Meng <menglingao@xiaomi.com>
2021-07-21 04:02:51 -0700

    Bluetooth: Fix Wrong format when debug
    Fix wformat build warning
    
    Signed-off-by: Lingao Meng <menglingao@xiaomi.com>

--- subsys/bluetooth/audio/vocs.c
+++ subsys/bluetooth/audio/vocs.c
@@ -85,7 +85,7 @@ static ssize_t read_location(struct bt_conn *conn, const struct bt_gatt_attr *at
 {
 	struct bt_vocs *inst = attr->user_data;
 
-	BT_DBG("0x%02x", inst->srv.location);
+	BT_DBG("0x%08x", inst->srv.location);
 	return bt_gatt_attr_read(conn, attr, buf, len, offset, &inst->srv.location,
 				 sizeof(inst->srv.location));
 }
