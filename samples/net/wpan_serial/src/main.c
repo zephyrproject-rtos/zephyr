2631dbd457cf13bbf7d8891c3d60f6ae52e14c63
Johann Fischer <johann.fischer@nordicsemi.no>
2021-07-22 14:41:08 +0200

    samples: wpan_serial: get CDC ACM UART device from devicetree
    Add app.overlay which contains  cdc-acm-uart node.
    Rework sample to get CDC ACM UART device from devicetree.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- samples/net/wpan_serial/src/main.c
+++ samples/net/wpan_serial/src/main.c
@@ -528,9 +528,9 @@ void main(void)
 
 	LOG_INF("Starting wpan_serial application");
 
-	dev = device_get_binding("CDC_ACM_0");
-	if (!dev) {
-		LOG_ERR("CDC ACM device not found");
+	dev = DEVICE_DT_GET_ONE(zephyr_cdc_acm_uart);
+	if (!device_is_ready(dev)) {
+		LOG_ERR("CDC ACM device not ready");
 		return;
 	}
 
