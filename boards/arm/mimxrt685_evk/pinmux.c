68649fd2f07c8518cbe55000f2eec645d340b5ad
Maureen Helm <maureen.helm@nxp.com>
2021-07-21 19:23:19 -0500

    boards: arm: Add PWM LEDs to the mimxrt685_evk board
    Adds support for PWM LEDs (red, green, blue) to the mimxrt685_evk board
    by adding devicetree nodes and aliases, and configuring the associated
    pinmuxes. The red PWM LED is disabled by default because it's connected
    to the same PWM channel as the blue PWM LED.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>

--- boards/arm/mimxrt685_evk/pinmux.c
+++ boards/arm/mimxrt685_evk/pinmux.c
@@ -698,6 +698,76 @@ static int mimxrt685_evk_pinmux_init(const struct device *dev)
 			IOPCTL_PIO_INV_DI);
 	/* PORT0 PIN27 (coords: B3) is configured as SCT0_OUT7 */
 	IOPCTL_PinMuxSet(IOPCTL, 0U, 27U, port0_pin27_config);
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(green_pwm_led), okay)
+	uint32_t port0_pin14_config = (
+			IOPCTL_PIO_FUNC3 |
+			/* Disable pull-up / pull-down function */
+			IOPCTL_PIO_PUPD_DI |
+			/* Enable pull-down function */
+			IOPCTL_PIO_PULLDOWN_EN |
+			/* Disable input buffer function */
+			IOPCTL_PIO_INBUF_DI |
+			/* Normal mode */
+			IOPCTL_PIO_SLEW_RATE_NORMAL |
+			/* Normal drive */
+			IOPCTL_PIO_FULLDRIVE_DI |
+			/* Analog mux is disabled */
+			IOPCTL_PIO_ANAMUX_DI |
+			/* Pseudo Output Drain is disabled */
+			IOPCTL_PIO_PSEDRAIN_DI |
+			/* Input function is not inverted */
+			IOPCTL_PIO_INV_DI);
+	/* PORT0 PIN14 (coords: B3) is configured as SCT0_OUT0 */
+	IOPCTL_PinMuxSet(IOPCTL, 0U, 14U, port0_pin14_config);
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(blue_pwm_led), okay)
+	uint32_t port0_pin26_config = (
+			IOPCTL_PIO_FUNC3 |
+			/* Disable pull-up / pull-down function */
+			IOPCTL_PIO_PUPD_DI |
+			/* Enable pull-down function */
+			IOPCTL_PIO_PULLDOWN_EN |
+			/* Disable input buffer function */
+			IOPCTL_PIO_INBUF_DI |
+			/* Normal mode */
+			IOPCTL_PIO_SLEW_RATE_NORMAL |
+			/* Normal drive */
+			IOPCTL_PIO_FULLDRIVE_DI |
+			/* Analog mux is disabled */
+			IOPCTL_PIO_ANAMUX_DI |
+			/* Pseudo Output Drain is disabled */
+			IOPCTL_PIO_PSEDRAIN_DI |
+			/* Input function is not inverted */
+			IOPCTL_PIO_INV_DI);
+	/* PORT0 PIN26 (coords: B3) is configured as SCT0_OUT6 */
+	IOPCTL_PinMuxSet(IOPCTL, 0U, 26U, port0_pin26_config);
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(red_pwm_led), okay)
+	uint32_t port0_pin31_config = (
+			IOPCTL_PIO_FUNC3 |
+			/* Disable pull-up / pull-down function */
+			IOPCTL_PIO_PUPD_DI |
+			/* Enable pull-down function */
+			IOPCTL_PIO_PULLDOWN_EN |
+			/* Disable input buffer function */
+			IOPCTL_PIO_INBUF_DI |
+			/* Normal mode */
+			IOPCTL_PIO_SLEW_RATE_NORMAL |
+			/* Normal drive */
+			IOPCTL_PIO_FULLDRIVE_DI |
+			/* Analog mux is disabled */
+			IOPCTL_PIO_ANAMUX_DI |
+			/* Pseudo Output Drain is disabled */
+			IOPCTL_PIO_PSEDRAIN_DI |
+			/* Input function is not inverted */
+			IOPCTL_PIO_INV_DI);
+	/* PORT0 PIN31 (coords: B3) is configured as SCT0_OUT6 */
+	IOPCTL_PinMuxSet(IOPCTL, 0U, 31U, port0_pin31_config);
+#endif
+
 #endif
 
 	return 0;
