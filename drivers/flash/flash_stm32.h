53274d7f6067de4adf12f4398606d4dc83c20ad4
Dominik Ermel <dominik.ermel@nordicsemi.no>
2021-07-23 14:20:49 +0000

    drivers/flash/flash_stm32: Add flash get_page_info API call
    The commit adds implementation of get_page_info, get_page_count
    and get_size to the flash driver.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- drivers/flash/flash_stm32.h
+++ drivers/flash/flash_stm32.h
@@ -220,6 +220,10 @@ int flash_stm32_block_erase_loop(const struct device *dev,
 
 int flash_stm32_wait_flash_idle(const struct device *dev);
 
+int flash_stm32_get_page_info(const struct device *dev, off_t offset, struct flash_page_info *fpi);
+ssize_t flash_stm32_get_page_count(const struct device *dev);
+ssize_t flash_stm32_get_size(const struct device *dev);
+
 #ifdef CONFIG_SOC_SERIES_STM32WBX
 int flash_stm32_check_status(const struct device *dev);
 #endif /* CONFIG_SOC_SERIES_STM32WBX */
