d159646e9b57086c43cb5d6a42f63eb71c9abc7d
Piotr Pryga <piotr.pryga@nordicsemi.no>
2021-07-23 10:07:50 +0200

    Bluetooth: controller: ull_df.c code re-arrange
    Code related with connected mode was in middle between connectionless
    RX and TX. Moved to the bottom of the file.
    
    Added missing compilation guards for ll_df_set_conn_cte_tx_params.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_df.c
+++ subsys/bluetooth/controller/ll_sw/ull_df.c
@@ -348,75 +348,6 @@ uint8_t ll_df_set_cl_cte_tx_enable(uint8_t adv_handle, uint8_t cte_enable)
 }
 #endif /* CONFIG_BT_CTLR_DF_ADV_CTE_TX */
 
-/* @brief Function sets CTE transmission parameters for a connection.
- *
- * @param[in]handle                     Connection handle.
- * @param[in]cte_types                  Bitfield holding information about
- *                                      allowed CTE types.
- * @param[in]switch_pattern_len         Number of antenna ids in switch pattern.
- * @param[in]ant_id                     Array of antenna identifiers.
- *
- * @return Status of command completion.
- */
-uint8_t ll_df_set_conn_cte_tx_params(uint16_t handle, uint8_t cte_types,
-				     uint8_t switch_pattern_len,
-				     uint8_t *ant_id)
-{
-	if (cte_types & BT_HCI_LE_AOD_CTE_RSP_1US ||
-	    cte_types & BT_HCI_LE_AOD_CTE_RSP_2US) {
-
-		if (!IS_ENABLED(CONFIG_BT_CTLR_DF_ANT_SWITCH_TX)) {
-			return BT_HCI_ERR_UNSUPP_FEATURE_PARAM_VAL;
-		}
-
-		if (switch_pattern_len < BT_HCI_LE_SWITCH_PATTERN_LEN_MIN ||
-		    switch_pattern_len > BT_HCI_LE_SWITCH_PATTERN_LEN_MAX ||
-		    !ant_id) {
-			return BT_HCI_ERR_UNSUPP_FEATURE_PARAM_VAL;
-		}
-	}
-
-	return BT_HCI_ERR_CMD_DISALLOWED;
-}
-
-/* @brief Function provides information about Direction Finding
- *        antennas switching and sampling related settings.
- *
- * @param[out]switch_sample_rates       Pointer to store available antennas
- *                                      switch-sampling configurations.
- * @param[out]num_ant                   Pointer to store number of available
- *                                      antennas.
- * @param[out]max_switch_pattern_len    Pointer to store maximum number of
- *                                      antennas ids in switch pattern.
- * @param[out]max_cte_len               Pointer to store maximum length of CTE
- *                                      in [8us] units.
- */
-void ll_df_read_ant_inf(uint8_t *switch_sample_rates,
-			uint8_t *num_ant,
-			uint8_t *max_switch_pattern_len,
-			uint8_t *max_cte_len)
-{
-	*switch_sample_rates = 0;
-	if (IS_ENABLED(CONFIG_BT_CTLR_DF_ANT_SWITCH_TX) &&
-	    IS_ENABLED(CONFIG_BT_CTLR_DF_ANT_SWITCH_1US)) {
-		*switch_sample_rates |= DF_AOD_1US_TX;
-	}
-
-	if (IS_ENABLED(CONFIG_BT_CTLR_DF_CTE_RX) &&
-	    IS_ENABLED(CONFIG_BT_CTLR_DF_CTE_RX_SAMPLE_1US)) {
-		*switch_sample_rates |= DF_AOD_1US_RX;
-	}
-
-	if (IS_ENABLED(CONFIG_BT_CTLR_DF_ANT_SWITCH_RX) &&
-	    IS_ENABLED(CONFIG_BT_CTLR_DF_CTE_RX_SAMPLE_1US)) {
-		*switch_sample_rates |= DF_AOA_1US;
-	}
-
-	*max_switch_pattern_len = BT_CTLR_DF_MAX_ANT_SW_PATTERN_LEN;
-	*num_ant = lll_df_ant_num_get();
-	*max_cte_len = LLL_DF_MAX_CTE_LEN;
-}
-
 #if defined(CONFIG_BT_CTLR_DF_SCAN_CTE_RX)
 /* @brief Function sets IQ sampling enabled or disabled.
  *
@@ -1009,3 +940,68 @@ static uint8_t cte_info_clear(struct ll_adv_set *adv, struct lll_df_adv_cfg *df_
 	return BT_HCI_ERR_SUCCESS;
 }
 #endif /* CONFIG_BT_CTLR_DF_ADV_CTE_TX */
+
+#if defined CONFIG_BT_CTLR_DF_CONN_CTE_RSP
+/* @brief Function sets CTE transmission parameters for a connection.
+ *
+ * @param handle             Connection handle.
+ * @param cte_types          Bitfield holding information about
+ *                           allowed CTE types.
+ * @param switch_pattern_len Number of antenna ids in switch pattern.
+ * @param ant_id             Array of antenna identifiers.
+ *
+ * @return Status of command completion.
+ */
+uint8_t ll_df_set_conn_cte_tx_params(uint16_t handle, uint8_t cte_types, uint8_t switch_pattern_len,
+				     uint8_t *ant_id)
+{
+	if (cte_types & BT_HCI_LE_AOD_CTE_RSP_1US || cte_types & BT_HCI_LE_AOD_CTE_RSP_2US) {
+		if (!IS_ENABLED(CONFIG_BT_CTLR_DF_ANT_SWITCH_TX)) {
+			return BT_HCI_ERR_UNSUPP_FEATURE_PARAM_VAL;
+		}
+
+		if (switch_pattern_len < BT_HCI_LE_SWITCH_PATTERN_LEN_MIN ||
+		    switch_pattern_len > BT_HCI_LE_SWITCH_PATTERN_LEN_MAX || !ant_id) {
+			return BT_HCI_ERR_UNSUPP_FEATURE_PARAM_VAL;
+		}
+	}
+
+	return BT_HCI_ERR_CMD_DISALLOWED;
+}
+#endif /* CONFIG_BT_CTLR_DF_CONN_CTE_RSP */
+
+/* @brief Function provides information about Direction Finding
+ *        antennas switching and sampling related settings.
+ *
+ * @param[out]switch_sample_rates       Pointer to store available antennas
+ *                                      switch-sampling configurations.
+ * @param[out]num_ant                   Pointer to store number of available
+ *                                      antennas.
+ * @param[out]max_switch_pattern_len    Pointer to store maximum number of
+ *                                      antennas ids in switch pattern.
+ * @param[out]max_cte_len               Pointer to store maximum length of CTE
+ *                                      in [8us] units.
+ */
+void ll_df_read_ant_inf(uint8_t *switch_sample_rates, uint8_t *num_ant,
+			uint8_t *max_switch_pattern_len, uint8_t *max_cte_len)
+{
+	*switch_sample_rates = 0;
+	if (IS_ENABLED(CONFIG_BT_CTLR_DF_ANT_SWITCH_TX) &&
+	    IS_ENABLED(CONFIG_BT_CTLR_DF_ANT_SWITCH_1US)) {
+		*switch_sample_rates |= DF_AOD_1US_TX;
+	}
+
+	if (IS_ENABLED(CONFIG_BT_CTLR_DF_CTE_RX) &&
+	    IS_ENABLED(CONFIG_BT_CTLR_DF_CTE_RX_SAMPLE_1US)) {
+		*switch_sample_rates |= DF_AOD_1US_RX;
+	}
+
+	if (IS_ENABLED(CONFIG_BT_CTLR_DF_ANT_SWITCH_RX) &&
+	    IS_ENABLED(CONFIG_BT_CTLR_DF_CTE_RX_SAMPLE_1US)) {
+		*switch_sample_rates |= DF_AOA_1US;
+	}
+
+	*max_switch_pattern_len = BT_CTLR_DF_MAX_ANT_SW_PATTERN_LEN;
+	*num_ant = lll_df_ant_num_get();
+	*max_cte_len = LLL_DF_MAX_CTE_LEN;
+}
