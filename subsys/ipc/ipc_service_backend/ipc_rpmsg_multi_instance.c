f0045cebe9160ee9a1d95c8271aa339c497ef68f
Carlo Caione <ccaione@baylibre.com>
2021-07-26 17:22:07 +0200

    ipc: Cleanup ipc_service and rpmsg_multi_instance code
    No functional changes. Only a bit of code cleanup and refactoring.
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

--- subsys/ipc/ipc_service_backend/ipc_rpmsg_multi_instance.c
+++ subsys/ipc/ipc_service_backend/ipc_rpmsg_multi_instance.c
@@ -9,54 +9,48 @@
 #include <ipc/rpmsg_multi_instance.h>
 
 #include <logging/log.h>
+#include <sys/util_macro.h>
 #include <zephyr.h>
 #include <device.h>
 
 LOG_MODULE_REGISTER(ipc_rpmsg_multi_instance, CONFIG_IPC_SERVICE_LOG_LEVEL);
 
-#define MASTER IS_ENABLED(CONFIG_RPMSG_MULTI_INSTANCE_MASTER)
+#define NUM_INSTANCES		CONFIG_RPMSG_MULTI_INSTANCES_NO
+#define NUM_ENDPOINTS		CONFIG_IPC_BACKEND_RPMSG_MI_NUM_ENDPOINTS_PER_INSTANCE
 
-#define NUM_INSTANCES  CONFIG_RPMSG_MULTI_INSTANCES_NO
-#define NUM_ENDPOINTS  CONFIG_IPC_BACKEND_RPMSG_MI_NUM_ENDPOINTS_PER_INSTANCE
+#define WORK_QUEUE_STACK_SIZE	CONFIG_IPC_BACKEND_RPMSG_MI_WORK_QUEUE_STACK_SIZE
 
-#define WORK_QUEUE_STACK_SIZE  CONFIG_IPC_BACKEND_RPMSG_MI_WORK_QUEUE_STACK_SIZE
+#define PRIO_INIT_VAL		INT_MAX
+#define INSTANCE_NAME_SIZE	16
 
-#define PRIO_INIT_VAL          INT_MAX
-#define INSTANCE_NAME_SIZE     16
+#define CH_NAME(idx, sub)	(CONFIG_RPMSG_MULTI_INSTANCE_ ## idx ## _IPM_ ## sub ## _NAME)
 
-K_THREAD_STACK_ARRAY_DEFINE(ipm_stack, NUM_INSTANCES, WORK_QUEUE_STACK_SIZE);
-#define CAT_4(a, b, c, d) (a##b##c##d)
+static char *ipm_rx_name[] = {
+	FOR_EACH_FIXED_ARG(CH_NAME, (,), RX, 1, 2, 3, 4, 5, 6, 7, 8),
+};
 
-#define IPM_CH_NAME(name, sub) char *name[] = {                         \
-		CAT_4(CONFIG, _RPMSG_MULTI_INSTANCE_1_IPM_, sub, _NAME), \
-		CAT_4(CONFIG, _RPMSG_MULTI_INSTANCE_2_IPM_, sub, _NAME), \
-		CAT_4(CONFIG, _RPMSG_MULTI_INSTANCE_3_IPM_, sub, _NAME), \
-		CAT_4(CONFIG, _RPMSG_MULTI_INSTANCE_4_IPM_, sub, _NAME), \
-		CAT_4(CONFIG, _RPMSG_MULTI_INSTANCE_5_IPM_, sub, _NAME), \
-		CAT_4(CONFIG, _RPMSG_MULTI_INSTANCE_6_IPM_, sub, _NAME), \
-		CAT_4(CONFIG, _RPMSG_MULTI_INSTANCE_7_IPM_, sub, _NAME), \
-		CAT_4(CONFIG, _RPMSG_MULTI_INSTANCE_8_IPM_, sub, _NAME), }
-
-IPM_CH_NAME(ipm_rx_name, RX);
-IPM_CH_NAME(ipm_tx_name, TX);
+static char *ipm_tx_name[] = {
+	FOR_EACH_FIXED_ARG(CH_NAME, (,), TX, 1, 2, 3, 4, 5, 6, 7, 8),
+};
 
 BUILD_ASSERT(ARRAY_SIZE(ipm_rx_name) >= NUM_INSTANCES, "Invalid configuration");
 BUILD_ASSERT(ARRAY_SIZE(ipm_tx_name) >= NUM_INSTANCES, "Invalid configuration");
 
-/* Internal definition of the structure ipc_ept*/
+K_THREAD_STACK_ARRAY_DEFINE(ipm_stack, NUM_INSTANCES, WORK_QUEUE_STACK_SIZE);
+
 struct ipc_ept {
-	const char *name;
 	struct rpmsg_mi_ept rpmsg_ep;
 	struct ipc_service_cb cb;
+	const char *name;
 	void *priv;
 };
 
 struct ipc_rpmsg_mi_instances {
+	struct ipc_ept endpoints[NUM_ENDPOINTS];
 	char name[INSTANCE_NAME_SIZE];
 	struct rpmsg_mi_ctx ctx;
-	struct ipc_ept endpoints[NUM_ENDPOINTS];
-	int prio;
 	bool is_initialized;
+	int prio;
 };
 
 static struct ipc_rpmsg_mi_instances instances[NUM_INSTANCES];
@@ -114,63 +108,66 @@ static int get_available_ept_slot(struct ipc_rpmsg_mi_instances *instance)
 
 static int register_ept(struct ipc_ept **ept, const struct ipc_ept_cfg *cfg)
 {
+	struct rpmsg_mi_ept_cfg ept_cfg = { 0 };
+	int i_idx, e_idx;
+
 	if (!cfg || !ept) {
 		return -EINVAL;
 	}
 
-	int i = get_available_instance(cfg);
-	int e_idx = get_available_ept_slot(&instances[i]);
-
-	if (i < 0) {
+	i_idx = get_available_instance(cfg);
+	if (i_idx < 0) {
 		LOG_ERR("Available instance not found");
 		return -EIO;
 	}
 
-	/** Initialization of the instance context is performed only once.
-	 *  When registering the first endpoint for the instance.
+	/* Initialization of the instance context is performed only once.
+	 * When registering the first endpoint for the instance.
 	 */
-	if (!instances[i].is_initialized) {
-		struct rpsmg_mi_ctx_cfg ctx_cfg = {0};
+	if (!instances[i_idx].is_initialized) {
+		struct rpsmg_mi_ctx_cfg ctx_cfg = { 0 };
 
-		snprintf(instances[i].name, INSTANCE_NAME_SIZE, "rpmsg_mi_%d", i);
-		ctx_cfg.name = instances[i].name;
-		ctx_cfg.ipm_stack_area = ipm_stack[i];
-		ctx_cfg.ipm_stack_size = K_THREAD_STACK_SIZEOF(ipm_stack[i]);
+		snprintf(instances[i_idx].name, INSTANCE_NAME_SIZE, "rpmsg_mi_%d", i_idx);
+
+		ctx_cfg.name = instances[i_idx].name;
+		ctx_cfg.ipm_stack_area = ipm_stack[i_idx];
+		ctx_cfg.ipm_stack_size = K_THREAD_STACK_SIZEOF(ipm_stack[i_idx]);
 		ctx_cfg.ipm_work_q_prio = cfg->prio;
-		ctx_cfg.ipm_thread_name = instances[i].name;
-		ctx_cfg.ipm_rx_name = ipm_rx_name[i];
-		ctx_cfg.ipm_tx_name = ipm_tx_name[i];
+		ctx_cfg.ipm_thread_name = instances[i_idx].name;
+		ctx_cfg.ipm_rx_name = ipm_rx_name[i_idx];
+		ctx_cfg.ipm_tx_name = ipm_tx_name[i_idx];
 
-		if (rpmsg_mi_ctx_init(&instances[i].ctx, &ctx_cfg) < 0) {
+		if (rpmsg_mi_ctx_init(&instances[i_idx].ctx, &ctx_cfg) < 0) {
 			LOG_ERR("Instance initialization failed");
 			return -EIO;
 		}
-		instances[i].is_initialized = true;
+
+		instances[i_idx].is_initialized = true;
 	}
 
+	e_idx = get_available_ept_slot(&instances[i_idx]);
 	if (e_idx < 0) {
 		LOG_ERR("No free slots to register endpoint %s", log_strdup(cfg->name));
 		return -EIO;
 	}
 
-	struct rpmsg_mi_ept_cfg ept_cfg = {0};
-
-	instances[i].endpoints[e_idx].priv = cfg->priv;
-	instances[i].endpoints[e_idx].cb = cfg->cb;
+	instances[i_idx].endpoints[e_idx].priv = cfg->priv;
+	instances[i_idx].endpoints[e_idx].cb = cfg->cb;
 
 	ept_cfg.cb = &cb;
-	ept_cfg.priv = &instances[i].endpoints[e_idx];
+	ept_cfg.priv = &instances[i_idx].endpoints[e_idx];
 	ept_cfg.name = cfg->name;
 
-	if (rpmsg_mi_ept_register(&instances[i].ctx,
-				  &instances[i].endpoints[e_idx].rpmsg_ep, &ept_cfg) < 0) {
+	if (rpmsg_mi_ept_register(&instances[i_idx].ctx,
+				  &instances[i_idx].endpoints[e_idx].rpmsg_ep, &ept_cfg) < 0) {
 		LOG_ERR("Register endpoint failed");
 		return -EIO;
 	}
 
-	instances[i].endpoints[e_idx].name = cfg->name;
-	instances[i].prio = cfg->prio;
-	*ept = &instances[i].endpoints[e_idx];
+	instances[i_idx].endpoints[e_idx].name = cfg->name;
+	instances[i_idx].prio = cfg->prio;
+
+	*ept = &instances[i_idx].endpoints[e_idx];
 
 	return 0;
 }
@@ -184,9 +181,11 @@ const static struct ipc_service_backend backend = {
 static int backend_init(const struct device *dev)
 {
 	ARG_UNUSED(dev);
+
 	for (size_t i = 0; i < NUM_INSTANCES; i++) {
 		instances[i].prio = PRIO_INIT_VAL;
 	}
+
 	return ipc_service_register_backend(&backend);
 }
 
