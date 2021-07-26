453980391d485b530d7e7df7c3abdf60a954a05e
Carlo Caione <ccaione@baylibre.com>
2021-07-26 17:22:07 +0200

    ipc: Cleanup ipc_service and rpmsg_multi_instance code
    No functional changes. Only a bit of code cleanup and refactoring.
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

--- subsys/ipc/rpmsg_multi_instance/rpmsg_multi_instance.c
+++ subsys/ipc/rpmsg_multi_instance/rpmsg_multi_instance.c
@@ -13,66 +13,9 @@
 #include <openamp/open_amp.h>
 #include <cache.h>
 
+#include "rpmsg_multi_instance.h"
+
 LOG_MODULE_REGISTER(rpmsg_multi_instance, CONFIG_RPMSG_MULTI_INSTANCE_LOG_LEVEL);
-
-#define SHM_DEVICE_NAME          "sram0.shm"
-#define IPM_MSG_ID               CONFIG_IPM_MSG_ID
-
-#define RPMSG_VQ_0          (0) /* TX virtqueue queue index */
-#define RPMSG_VQ_1          (1) /* RX virtqueue queue index */
-#define VRING_COUNT         (2) /* Number of used vring buffers. */
-#define IPC_INSTANCE_COUNT  (CONFIG_RPMSG_MULTI_INSTANCES_NO) /* Total number of IPC instances.*/
-
-/* Private macros. */
-#define VRING_DESC_SIZEOF(num) ((num) * (sizeof(struct vring_desc)))
-#define VRING_AVAIL_SIZEOF(num) (sizeof(struct vring_avail) + \
-						((num) * sizeof(uint16_t)) + sizeof(uint16_t))
-#define VRING_USED_SIZEOF(num) (sizeof(struct vring_used) + \
-						((num) * sizeof(struct vring_used_elem)) + \
-						sizeof(uint16_t))
-
-#define VRING_FIRST_SUM(num) (VRING_DESC_SIZEOF(num) + VRING_AVAIL_SIZEOF(num))
-
-
-/* Compute size of vring buffer based on its size and alignment. */
-#define VRING_SIZE_COMPUTE(vring_size, align) (ROUND_UP(VRING_FIRST_SUM((vring_size)), \
-					(align)) + VRING_USED_SIZEOF((vring_size)))
-/* Macro for calculating used memory by virtqueue buffers for remote device. */
-#define VIRTQUEUE_SIZE_GET(vring_size)  (RPMSG_BUFFER_SIZE * (vring_size))
-
-/* Macro for getting the size of shared memory occupied by single IPC instance. */
-#define SHMEM_INST_SIZE_GET(vring_size) (VDEV_STATUS_SIZE +  \
-				(VRING_COUNT * VIRTQUEUE_SIZE_GET((vring_size))) + \
-				(VRING_COUNT * VRING_SIZE_COMPUTE((vring_size), (VRING_ALIGNMENT))))
-
-/* Returns size of used shared memory consumed by all IPC instances*/
-#define SHMEM_CONSUMED_SIZE_GET(vring_size) (IPC_INSTANCE_COUNT * SHMEM_INST_SIZE_GET((vring_size)))
-
-/* Returns maximum allowable size of vring buffers to fit memory requirements. */
-#define VRING_SIZE_GET(shmem_size)                                       \
-					((SHMEM_CONSUMED_SIZE_GET(32)) < (shmem_size) ? 32 : \
-					(SHMEM_CONSUMED_SIZE_GET(16)) < (shmem_size) ? 16 :  \
-					(SHMEM_CONSUMED_SIZE_GET(8))  < (shmem_size) ? 8  :  \
-					(SHMEM_CONSUMED_SIZE_GET(4))  < (shmem_size) ? 4  :  \
-					(SHMEM_CONSUMED_SIZE_GET(2))  < (shmem_size) ? 2 : 1)
-
-/* Returns size of used shared memory of single instance in case of using
- * maximum allowable vring buffer size.
- */
-#define SHMEM_INST_SIZE_AUTOALLOC_GET(shmem_size) \
-					  (SHMEM_INST_SIZE_GET(VRING_SIZE_GET((shmem_size))))
-
-/* Returns start address of ipc instance in shared memory. It assumes that
- * maximum allowable vring buffer size is used.
- */
-#define SHMEM_INST_ADDR_AUTOALLOC_GET(shmem_addr, shmem_size, id) ((shmem_addr) + \
-					  ((id) * (SHMEM_INST_SIZE_AUTOALLOC_GET(shmem_size))))
-
-#if IS_ENABLED(CONFIG_RPMSG_MULTI_INSTANCE_MASTER)
-#define VIRTQUEUE_ID 0
-#else
-#define VIRTQUEUE_ID 1
-#endif
 
 static bool config_correct;
 static int instance;
@@ -116,16 +59,15 @@ static void virtio_set_features(struct virtio_device *vdev, uint32_t features)
 {
 	/* No need for implementation */
 }
-#endif
+#endif /* CONFIG_RPMSG_MULTI_INSTANCE_MASTER */
 
 static void virtio_notify(struct virtqueue *vq)
 {
+	struct rpmsg_mi_ctx *ctx = metal_container_of(vq->vq_dev, struct rpmsg_mi_ctx, vdev);
 	int status;
 
-	struct rpmsg_mi_ctx *ctx = metal_container_of(vq->vq_dev, struct rpmsg_mi_ctx, vdev);
-
 	if (ctx) {
-		status = ipm_send(ctx->ipm_tx_handle, 0, IPM_MSG_ID, NULL, 0);
+		status = ipm_send(ctx->ipm_tx_handle, 0, CONFIG_IPM_MSG_ID, NULL, 0);
 		if (status != 0) {
 			LOG_WRN("Failed to notify: %d", status);
 		}
@@ -138,7 +80,7 @@ const static struct virtio_dispatch dispatch = {
 #ifdef CONFIG_RPMSG_MULTI_INSTANCE_MASTER
 	.set_status = virtio_set_status,
 	.set_features = virtio_set_features,
-#endif
+#endif /* CONFIG_RPMSG_MULTI_INSTANCE_MASTER */
 	.notify = virtio_notify,
 };
 
@@ -167,7 +109,7 @@ int rpmsg_mi_configure_shm(struct rpmsg_mi_ctx *ctx, const struct rpsmg_mi_ctx_c
 	uint32_t shm_local_start_addr = shm_addr + VDEV_STATUS_SIZE;
 	uint32_t shm_local_size = shm_size - VDEV_STATUS_SIZE;
 
-	uint32_t rpmsg_reg_size = 2 * VIRTQUEUE_SIZE_GET(vring_size);
+	uint32_t rpmsg_reg_size = VRING_COUNT * VIRTQUEUE_SIZE_GET(vring_size);
 	uint32_t vring_region_size = VRING_SIZE_COMPUTE(vring_size, VRING_ALIGNMENT);
 
 	ctx->shm_status_reg_addr = shm_addr;
@@ -210,22 +152,22 @@ static int ept_cb(struct rpmsg_endpoint *ept, void *data, size_t len, uint32_t s
 		}
 		return 0;
 	}
+
 	if (mi_ep->cb->received) {
 		mi_ep->cb->received(data, len, mi_ep->priv);
 	}
+
 	return 0;
 }
 
 static void ns_bind_cb(struct rpmsg_device *rdev, const char *name, uint32_t dest)
 {
+	sys_snode_t *node;
 	struct rpmsg_virtio_device *p_rvdev = metal_container_of(rdev,
 								 struct rpmsg_virtio_device, rdev);
-
 	struct rpmsg_mi_ctx *ctx = metal_container_of(p_rvdev, struct rpmsg_mi_ctx, rvdev);
 
 	LOG_DBG("bind_cb endpoint: %s, for instance: %s", name ? log_strdup(name) : "", ctx->name);
-
-	sys_snode_t *node;
 
 	SYS_SLIST_FOR_EACH_NODE(&ctx->endpoints, node) {
 		struct rpmsg_mi_ept *ept = CONTAINER_OF(node, struct rpmsg_mi_ept, node);
@@ -245,6 +187,7 @@ static void ns_bind_cb(struct rpmsg_device *rdev, const char *name, uint32_t des
 				ept->bound = true;
 				ept->cb->bound(ept->priv);
 			}
+
 			break;
 		}
 	}
@@ -252,22 +195,24 @@ static void ns_bind_cb(struct rpmsg_device *rdev, const char *name, uint32_t des
 
 int rpmsg_mi_ctx_init(struct rpmsg_mi_ctx *ctx, const struct rpsmg_mi_ctx_cfg *cfg)
 {
+	struct metal_init_params metal_params = METAL_INIT_DEFAULTS;
+	uint8_t vring_size = VRING_SIZE_GET(SHM_SIZE);
+	struct metal_device *device;
+	int err;
+
 	if (!ctx || !cfg) {
 		return -EINVAL;
 	}
+
 	if (!config_correct) {
 		return -EIO;
 	}
 
 	LOG_DBG("RPMsg multiple instance initialization");
 
-	int                         err;
-	struct metal_init_params    metal_params = METAL_INIT_DEFAULTS;
-	struct metal_device         *device;
-
 	/* Start IPM workqueue */
 	k_work_queue_start(&ctx->ipm_work_q, cfg->ipm_stack_area,
-				 cfg->ipm_stack_size, cfg->ipm_work_q_prio, NULL);
+			   cfg->ipm_stack_size, cfg->ipm_work_q_prio, NULL);
 	k_thread_name_set(&ctx->ipm_work_q.thread, cfg->ipm_thread_name);
 	k_work_init(&ctx->ipm_work, ipm_callback_process);
 
@@ -322,9 +267,6 @@ int rpmsg_mi_ctx_init(struct rpmsg_mi_ctx *ctx, const struct rpsmg_mi_ctx_cfg *c
 	/* Register IPM callback. This cb executes when msg has come. */
 	ipm_register_callback(ctx->ipm_rx_handle, ipm_callback, ctx);
 
-	/* Virtquue setup */
-	uint8_t vring_size = VRING_SIZE_GET(SHM_SIZE);
-
 	ctx->vq[RPMSG_VQ_0] = virtqueue_allocate(vring_size);
 	if (!ctx->vq[RPMSG_VQ_0]) {
 		LOG_ERR("virtqueue_allocate failed to alloc vq[RPMSG_VQ_0]");
@@ -350,7 +292,7 @@ int rpmsg_mi_ctx_init(struct rpmsg_mi_ctx *ctx, const struct rpsmg_mi_ctx_cfg *c
 	ctx->rvrings[RPMSG_VQ_1].vq = ctx->vq[RPMSG_VQ_1];
 
 	ctx->vdev.role = IS_ENABLED(CONFIG_RPMSG_MULTI_INSTANCE_MASTER) ?
-								  RPMSG_MASTER : RPMSG_REMOTE;
+			 RPMSG_MASTER : RPMSG_REMOTE;
 
 	ctx->vdev.vrings_num = VRING_COUNT;
 	ctx->vdev.func = &dispatch;
@@ -379,10 +321,10 @@ int rpmsg_mi_ctx_init(struct rpmsg_mi_ctx *ctx, const struct rpsmg_mi_ctx_cfg *c
 	ctx->rdev = rpmsg_virtio_get_rpmsg_device(&ctx->rvdev);
 
 	instance++;
+
 	LOG_DBG("RPMsg multiple instance initialization done");
 
 	return 0;
-
 }
 
 int rpmsg_mi_ept_register(struct rpmsg_mi_ctx *ctx, struct rpmsg_mi_ept *ept,
@@ -399,6 +341,7 @@ int rpmsg_mi_ept_register(struct rpmsg_mi_ctx *ctx, struct rpmsg_mi_ept *ept,
 	ept->name = cfg->name;
 
 	sys_slist_append(&ctx->endpoints, &ept->node);
+
 	if (!IS_ENABLED(CONFIG_RPMSG_MULTI_INSTANCE_MASTER)) {
 		LOG_DBG("Remote - Create endpoint: %s", ept->name);
 
@@ -410,6 +353,7 @@ int rpmsg_mi_ept_register(struct rpmsg_mi_ctx *ctx, struct rpmsg_mi_ept *ept,
 			return err;
 		}
 	}
+
 	return 0;
 }
 
@@ -430,8 +374,10 @@ static bool rpmsg_mi_config_verify(void)
 static int rpmsg_mi_init(const struct device *dev)
 {
 	ARG_UNUSED(dev);
+
 	LOG_DBG("Initialization of RPMsg multiple instance");
 	config_correct = rpmsg_mi_config_verify();
+
 	return 0;
 }
 
