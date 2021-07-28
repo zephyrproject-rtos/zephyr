4ccf4a277574a2d84b575a7ff3d89085ee69c0e5
Dominik Ermel <dominik.ermel@nordicsemi.no>
2021-07-28 07:50:41 +0000

    drivers/flash/flash_stm32f7x: Add flash get_page_info API call
    The commit adds implementation of get_page_info, get_page_count
    and get_size to the flash driver.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- drivers/flash/flash_stm32f7x.c
+++ drivers/flash/flash_stm32f7x.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2018 Aurelien Jarno
  * Copyright (c) 2018 Yong Jin
+ * Copyright (c) 2021 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -238,3 +239,143 @@ void flash_stm32_page_layout(const struct device *dev,
 	*layout_size = ARRAY_SIZE(stm32f7_flash_layout);
 #endif
 }
+
+#if !defined(FLASH_SECTOR_TOTAL)
+#error "Unknown flash layout"
+#else
+
+/** Single bank or single bank layout in dual bank mode **/
+/* Counts */
+#define K256_COUNT	(3 * ((FLASH_SECTOR_TOTAL == 8) && (CONFIG_FLASH_SIZE == 1024)) +	\
+			 7 * (FLASH_SECTOR_TOTAL == 24))
+
+#define K128_COUNT	(1 * (((FLASH_SECTOR_TOTAL == 8) && (CONFIG_FLASH_SIZE == 1024)) ||	\
+			      (FLASH_SECTOR_TOTAL == 24)) +					\
+			 3 * ((FLASH_SECTOR_TOTAL == 8) && (CONFIG_FLASH_SIZE == 512)))
+
+#define K64_COUNT	(1 * ((FLASH_SECTOR_TOTAL == 8) && (CONFIG_FLASH_SIZE == 512)))
+
+#define K32_COUNT	(4 * (((FLASH_SECTOR_TOTAL == 8) && (CONFIG_FLASH_SIZE == 1024)) ||	\
+			      (FLASH_SECTOR_TOTAL == 24)) +					\
+			 2 * (FLASH_SECTOR_TOTAL == 2))
+
+#define K16_COUNT	(4 * (((FLASH_SECTOR_TOTAL == 8) && (CONFIG_FLASH_SIZE == 512)) ||	\
+			      (FLASH_SECTOR_TOTAL == 4)))
+
+#define KALL_COUNT	(K16_COUNT + K32_COUNT + K64_COUNT + K128_COUNT + K256_COUNT)
+
+#define KALL_SIZE	(K16_COUNT * KB(16) + K32_COUNT * KB(32) + K64_COUNT * KB(64) +		\
+			 K128_COUNT * KB(128) + K256_COUNT * KB(256))
+
+/* Masks */
+#define K256_MASK	(0x001c0000 * (K256_COUNT != 0))
+
+#define K128_MASK	(0x00020000 * (((FLASH_SECTOR_TOTAL == 8) &&				\
+					(CONFIG_FLASH_SIZE == 1024)) ||				\
+				       (FLASH_SECTOR_TOTAL == 24)) +				\
+			 0x00060000 * ((FLASH_SECTOR_TOTAL == 8) && (CONFIG_FLASH_SIZE == 512)))
+
+#define K64_MASK	(0x00010000 * (K64_COUNT != 0))
+
+#define K32_MASK	(0x00018000 * (K32_COUNT != 0))
+
+#define K16_MASK	(0x0000c000 * (K16_COUNT != 0))
+
+/** Second bank layout in dual bank mode **/
+#if FLASH_SECTOR_TOTAL == 24 && !defined(FLASH_OPTCR_nDBANK)
+#error "The 24 sectors allow single/dual bank mode and require FLASH_OPTCR_nDBANK"
+#endif
+/* In dual bank mode, the second bank layout is the same in size of flash but the number of pages
+ * dubles as sizes of all pages are divided by two; this means that istead of having N pages of
+ * size M we now have N * 2 pages of size M / 2.
+ */
+#define K128_COUNT_D	K256_COUNT
+#define K64_COUNT_D	K128_COUNT
+#define K16_COUNT_D	K32_COUNT
+#define KALL_COUNT_D	((K128_COUNT_D + K64_COUNT_D + K16_COUNT_D) * 1)
+
+/* Masks are also shifted right by one as now they mask bits for pages that are in size M / 2 */
+#define K128_MASK_D	(K256_MASK >> 1)
+#define K64_MASK_D	(K128_MASK >> 1)
+#define K16_MASK_D	(K32_MASK >> 1)
+/* Mask where second bank layout starts */
+#define BANK_MASK	0x00100000
+
+int flash_stm32_get_page_info(const struct device *dev, off_t off, struct flash_page_info *fpi)
+{
+	ARG_UNUSED(dev);
+
+	if (off < 0 || off >= KALL_SIZE) {
+		return -EINVAL;
+	}
+
+#if FLASH_SECTOR_TOTAL == 24
+	if (!(FLASH_STM32_REGS(dev)->OPTCR & FLASH_OPTCR_nDBANK)) {
+		/* Dual bank mode, dual layout */
+		/* RM0410, table 4: STM32F76xxx and STM32F77xxx in dual bank */
+		if (off & K128_MASK_D) {
+			fpi->offset = off & (K128_MASK_D | BANK_MASK);
+			fpi->size = KB(128);
+		} else if (off & K64_MASK_D) {
+			fpi->offset = off & (K64_MASK_D | BANK_MASK);
+			fpi->size = KB(64);
+		} else {
+			fpi->offset = off & (K16_MASK_D | BANK_MASK);
+			fpi->size = KB(16);
+		}
+		return 0;
+	}
+#endif
+
+	/* Single bank mode or single layout in dual bank bank mode */
+	/* RM0385, table 4: STM32F750xx */
+	/* RM0431, table 4: STM32F730xx */
+	/* RM0385, table 3: STM32F756xx and STM32F74xxx */
+	/* RM0410, table 3: STM32F76xxx and STM32F77xxx in single bank */
+	if (off & K256_MASK) {
+		fpi->offset = off & K256_MASK;
+		fpi->size = KB(256);
+	} else if (off & K128_MASK) {
+		fpi->offset = off & K128_MASK;
+		fpi->size = KB(128);
+	} else if (off & K64_MASK) {
+		fpi->offset = off & K64_MASK;
+		fpi->size = KB(64);
+	} else if (K32_MASK != 0) {
+		fpi->offset = off & K32_MASK;
+		fpi->size = KB(32);
+	} else if (K16_MASK != 0) {
+		fpi->offset = off & K16_MASK;
+		fpi->size = KB(16);
+	}
+
+	return 0;
+}
+
+ssize_t flash_stm32_get_page_count(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+#if FLASH_SECTOR_TOTAL == 24
+	if (!(FLASH_STM32_REGS(dev)->OPTCR & FLASH_OPTCR_nDBANK)) {
+		return KALL_COUNT_D;
+	}
+#endif
+
+	return KALL_COUNT;
+}
+
+ssize_t flash_stm32_get_size(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	/*
+	 * In case when FLASH_SECTOR_TOTAL == 24, the number of pages changes due to how the flash
+	 * gets divided, but the KALL_SIZE is the same as the division into pages does not change
+	 * the overall size.
+	 */
+	return KALL_SIZE;
+}
+
+
+#endif /* !define(FLASH_SECTOR_TOTAL) */
