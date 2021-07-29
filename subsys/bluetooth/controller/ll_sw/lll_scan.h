e884fa4844c102b445b2dc0147a4d50cb5c21ea2
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2021-07-29 17:10:59 +0530

    Bluetooth: Controller: Resume primary PDU scanning after Auxiliary PDU
    Add implementation to resume primary PDU scanning window
    after use of LLL scheduling to receive auxiliary PDUs.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/lll_scan.h
+++ subsys/bluetooth/controller/ll_sw/lll_scan.h
@@ -31,6 +31,7 @@ struct lll_scan {
 	uint16_t duration_expire;
 	uint8_t  phy:3;
 	uint8_t  is_adv_ind:1;
+	uint8_t  is_aux_sched:1;
 
 	/* temporary storage when aux scan was scheduled from LLL */
 	struct lll_scan_aux *lll_aux;
