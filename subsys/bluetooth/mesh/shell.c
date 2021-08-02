66a314e9e9d508ca9280c089ff255b6cbfe5826f
Lingao Meng <menglingao@xiaomi.com>
2021-08-02 17:26:07 +0800

    [nrf fromtree] Bluetooth: Mesh: Mark as internal function
    Marks funcs:`show_faults` as internal, avoiding conflict.
    
    Signed-off-by: Lingao Meng <menglingao@xiaomi.com>
    Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
    (cherry picked from commit d4361b2c0e26dca79b7e3954306a9c967dce80dd)

--- subsys/bluetooth/mesh/shell.c
+++ subsys/bluetooth/mesh/shell.c
@@ -133,7 +133,7 @@ BT_MESH_HEALTH_PUB_DEFINE(health_pub, CUR_FAULTS_MAX);
 static struct bt_mesh_cfg_cli cfg_cli = {
 };
 
-void show_faults(uint8_t test_id, uint16_t cid, uint8_t *faults, size_t fault_count)
+static void show_faults(uint8_t test_id, uint16_t cid, uint8_t *faults, size_t fault_count)
 {
 	size_t i;
 
