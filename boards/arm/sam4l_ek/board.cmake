46937a1629dee7f33ba10302bb3dde96209edc11
Gerson Fernando Budke <nandojve@gmail.com>
2021-08-05 19:20:18 -0300

    boards: sam/sam0: Add debuggers thread awareness
    Zephyr thread awareness is available but Atmel SAM/SAM0 boards don't
    have debuggers configuration.  Add thread awareness option to enable
    feature when user selects CONFIG_DEBUG_THREAD_INFO=y.
    
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

--- boards/arm/sam4l_ek/board.cmake
+++ boards/arm/sam4l_ek/board.cmake
@@ -4,4 +4,5 @@
 board_runner_args(jlink "--device=atsam4lc4c")
 board_runner_args(jlink "--speed=4000")
 board_runner_args(jlink "--reset-after-load")
+board_runner_args(jlink "-rtos GDBServer/RTOSPlugin_Zephyr")
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
--- boards/arm/sam4s_xplained/board.cmake
+++ boards/arm/sam4s_xplained/board.cmake
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--device=atsam4s16c" "--speed=4000")
+board_runner_args(jlink "-rtos GDBServer/RTOSPlugin_Zephyr")
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 
 include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
