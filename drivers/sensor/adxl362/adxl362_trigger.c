9c1cdfc66da7deda046022900216c2d107d6cd1d
Jordan Yates <jordan.yates@data61.csiro.au>
2021-08-07 13:29:33 +1000

    sensor: adxl362: convert to `_dt_spec`
    Convert adxl362 driver to use `spi_dt_spec` and `gpio_dt_spec`.
    
    Signed-off-by: Jordan Yates <jordan.yates@data61.csiro.au>

--- drivers/sensor/adxl362/adxl362_trigger.c
+++ drivers/sensor/adxl362/adxl362_trigger.c
@@ -116,16 +116,14 @@ int adxl362_trigger_set(const struct device *dev,
 
 int adxl362_init_interrupt(const struct device *dev)
 {
-	struct adxl362_data *drv_data = dev->data;
 	const struct adxl362_config *cfg = dev->config;
+	struct adxl362_data *drv_data = dev->data;
 	int ret;
 
 	k_mutex_init(&drv_data->trigger_mutex);
 
-	drv_data->gpio = device_get_binding(cfg->gpio_port);
-	if (drv_data->gpio == NULL) {
-		LOG_ERR("Failed to get pointer to %s device!",
-			cfg->gpio_port);
+	if (!device_is_ready(cfg->interrupt.port)) {
+		LOG_ERR("GPIO port %s not ready", cfg->interrupt.port->name);
 		return -EINVAL;
 	}
 
@@ -135,14 +133,13 @@ int adxl362_init_interrupt(const struct device *dev)
 		return -EFAULT;
 	}
 
-	gpio_pin_configure(drv_data->gpio, cfg->int_gpio,
-			   GPIO_INPUT | cfg->int_flags);
+	gpio_pin_configure_dt(&cfg->interrupt, GPIO_INPUT);
 
 	gpio_init_callback(&drv_data->gpio_cb,
 			   adxl362_gpio_callback,
-			   BIT(cfg->int_gpio));
+			   BIT(cfg->interrupt.pin));
 
-	if (gpio_add_callback(drv_data->gpio, &drv_data->gpio_cb) < 0) {
+	if (gpio_add_callback(cfg->interrupt.port, &drv_data->gpio_cb) < 0) {
 		LOG_ERR("Failed to set gpio callback!");
 		return -EIO;
 	}
@@ -161,8 +158,7 @@ int adxl362_init_interrupt(const struct device *dev)
 	drv_data->work.handler = adxl362_work_cb;
 #endif
 
-	gpio_pin_interrupt_configure(drv_data->gpio, cfg->int_gpio,
-				     GPIO_INT_EDGE_TO_ACTIVE);
+	gpio_pin_interrupt_configure_dt(&cfg->interrupt, GPIO_INT_EDGE_TO_ACTIVE);
 
 	return 0;
 }
