d85da555fa389ca68c59318575544133f06d9dad
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-08-10 15:47:57 -0700

    dts: fix 'grove,foo' compatibles by using 'seeed,grove-foo'
    Rename:
    
    - grove,light to seeed,grove-light
    - grove,temperature to seeed,grove-temperature
    
    The "grove" brand applies to a family of products by Seeed (sic):
    
    https://www.seeedstudio.com/category/Grove-c-1003.html
    
    Therefore we should use the existing vendor seeed.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- samples/sensor/grove_light/src/main.c
+++ samples/sensor/grove_light/src/main.c
@@ -13,7 +13,8 @@
 
 void main(void)
 {
-	const struct device *dev = device_get_binding(DT_LABEL(DT_INST(0, grove_light)));
+	const struct device *dev =
+		device_get_binding(DT_LABEL(DT_INST(0, seeed_grove_light)));
 
 	if (dev == NULL) {
 		printf("device not found.  aborting test.\n");
