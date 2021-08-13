4a8a1577efbe3976d143c19415be57735697036d
Lingao Meng <menglingao@xiaomi.com>
2021-08-13 14:10:45 +0800

    Bluetooth: Mesh: Redefine callback registration
    There is a problem with the previous method, that is,
    we use the same label(bt_mesh_subnet_cb_subnet_evt) and
    put it in the same section, which is not friendly for debugging.
    
    Signed-off-by: Lingao Meng <menglingao@xiaomi.com>

--- subsys/bluetooth/mesh/lpn.c
+++ subsys/bluetooth/mesh/lpn.c
@@ -1075,7 +1075,9 @@ static void subnet_evt(struct bt_mesh_subnet *sub, enum bt_mesh_key_evt evt)
 	}
 }
 
-BT_MESH_SUBNET_CB_DEFINE(subnet_evt);
+BT_MESH_SUBNET_CB_DEFINE(lpn) = {
+	.evt_handler = subnet_evt,
+};
 
 int bt_mesh_lpn_init(void)
 {
