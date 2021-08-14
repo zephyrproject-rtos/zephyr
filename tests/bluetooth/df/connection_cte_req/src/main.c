5a4e0dca31d4f5e64f359da17b5f25a4c8d1775d
Piotr Pryga <piotr.pryga@nordicsemi.no>
2021-08-15 00:23:18 +0200

    tests: Bluetooth: df: Add tests for enable CTE req and sampling HCI cmd
    Add unit tests for HCI commands:
    - HCI_LE_Set_Connection_CTE_Receive_Parameters,
    - HCI_LE_Connection_CTE_Request_Enable.
    Tests validate current implementation of commands handling in HCI/ULL.
    Handling of the HCI_LE_Connection_CTE_Request_Enable is not complete,
    thus tests don't validate positive behavior the functions.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

---
+++ tests/bluetooth/df/connection_cte_req/src/main.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <bt_common.h>
+
+#include "common.h"
+#include "test_cte_set_rx_params.h"
+#include "test_cte_req_enable.h"
+
+/*test case main entry*/
+void test_main(void)
+{
+	ut_bt_setup();
+
+	run_set_cte_rx_params_tests();
+	run_cte_request_enable_tests();
+}
