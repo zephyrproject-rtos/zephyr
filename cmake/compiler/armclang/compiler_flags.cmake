36bb00d1f5c4266a1857cb02c09710ac9c7e796f
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2021-08-15 23:14:21 +0200

    armclang: ARM Compiler C library support
    Support for ARM Compiler C library.
    
    This commit add support for the ARM Compiler C libary in:
    - Kconfig
    - libc/armstdc
    
    A new Kconfig symbol is added to allow a toolchain to specify if they
    support linking with the minimal C library.
    Also the CMake variable `TOOLCHAIN_HAS_NEWLIB` is exported to Kconfig
    so that CONFIG_NEWLIB_LIBS can only be enabled if the toolchain has
    newlib.
    
    The armclang toolchain selects the CMake scatter file generator and
    disables support for the LD linker template which is not supported by
    armlink.
    
    For the ARM Compiler C library, a corresponding lib/libc/armstc/ folder
    with a minimal implementation to work with the ARM Compiler C library
    is added.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/compiler/armclang/compiler_flags.cmake
+++ cmake/compiler/armclang/compiler_flags.cmake
@@ -4,3 +4,6 @@ include(${ZEPHYR_BASE}/cmake/compiler/clang/compiler_flags.cmake)
 # Required ASM flags when using armclang, this should be handled by CMake, but
 # fails because of: https://gitlab.kitware.com/cmake/cmake/-/issues/19963
 set_property(TARGET asm APPEND PROPERTY required "--target=${triple}")
+
+# Only the ARM Compiler C library is currently supported.
+set_compiler_property(PROPERTY nostdinc)
