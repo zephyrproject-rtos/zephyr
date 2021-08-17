efdf1fac5edf807538748348764531261ef353a5
HaiLong Yang <hailong.yang@brainco.cn>
2021-08-17 16:36:41 +0800

    ipc: rpmsg_service: add ipm stm32 hsem support
    Add ipm_send supprot for stm32 hsem mailbox.
    
    Signed-off-by: HaiLong Yang <hailong.yang@brainco.cn>

--- subsys/ipc/rpmsg_service/rpmsg_backend.c
+++ subsys/ipc/rpmsg_service/rpmsg_backend.c
@@ -131,6 +131,8 @@ static void virtio_notify(struct virtqueue *vq)
 	uint32_t current_core = sse_200_platform_get_cpu_id();
 
 	status = ipm_send(ipm_handle, 0, current_core ? 0 : 1, 0, 1);
+#elif defined(CONFIG_IPM_STM32_HSEM)
+	status = ipm_send(ipm_handle, 0, 0, NULL, 0);
 #else
 	uint32_t dummy_data = 0x55005500; /* Some data must be provided */
 
