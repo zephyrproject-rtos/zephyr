35ede513e480c69024c255cd1c0463d94eb27f66
Stephanos Ioannidis <root@stephanos.io>
2021-08-18 19:44:38 +0900

    tests: lib: cmsis_dsp: basicmath: Update F32 tests for 1.9.0
    This commit updates the basicmath F32 test patterns and implementations
    for the CMSIS-DSP 1.9.0.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- tests/lib/cmsis_dsp/basicmath/src/f32.c
+++ tests/lib/cmsis_dsp/basicmath/src/f32.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2020 Stephanos Ioannidis <root@stephanos.io>
- * Copyright (C) 2010-2020 ARM Limited or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Stephanos Ioannidis <root@stephanos.io>
+ * Copyright (C) 2010-2021 ARM Limited or its affiliates. All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -47,6 +47,7 @@ static void test_arm_add_f32(
 DEFINE_TEST_VARIANT4(arm_add_f32, 3, in_com1, in_com2, ref_add, 3);
 DEFINE_TEST_VARIANT4(arm_add_f32, 8, in_com1, in_com2, ref_add, 8);
 DEFINE_TEST_VARIANT4(arm_add_f32, 11, in_com1, in_com2, ref_add, 11);
+DEFINE_TEST_VARIANT4(arm_add_f32, long, in_com1, in_com2, ref_add, ARRAY_SIZE(in_com1));
 
 static void test_arm_sub_f32(
 	const uint32_t *input1, const uint32_t *input2, const uint32_t *ref,
@@ -79,6 +80,7 @@ static void test_arm_sub_f32(
 DEFINE_TEST_VARIANT4(arm_sub_f32, 3, in_com1, in_com2, ref_sub, 3);
 DEFINE_TEST_VARIANT4(arm_sub_f32, 8, in_com1, in_com2, ref_sub, 8);
 DEFINE_TEST_VARIANT4(arm_sub_f32, 11, in_com1, in_com2, ref_sub, 11);
+DEFINE_TEST_VARIANT4(arm_sub_f32, long, in_com1, in_com2, ref_sub, ARRAY_SIZE(in_com1));
 
 static void test_arm_mult_f32(
 	const uint32_t *input1, const uint32_t *input2, const uint32_t *ref,
@@ -111,6 +113,7 @@ static void test_arm_mult_f32(
 DEFINE_TEST_VARIANT4(arm_mult_f32, 3, in_com1, in_com2, ref_mult, 3);
 DEFINE_TEST_VARIANT4(arm_mult_f32, 8, in_com1, in_com2, ref_mult, 8);
 DEFINE_TEST_VARIANT4(arm_mult_f32, 11, in_com1, in_com2, ref_mult, 11);
+DEFINE_TEST_VARIANT4(arm_mult_f32, long, in_com1, in_com2, ref_mult, ARRAY_SIZE(in_com1));
 
 static void test_arm_negate_f32(
 	const uint32_t *input1, const uint32_t *ref, size_t length)
@@ -142,6 +145,7 @@ static void test_arm_negate_f32(
 DEFINE_TEST_VARIANT3(arm_negate_f32, 3, in_com1, ref_negate, 3);
 DEFINE_TEST_VARIANT3(arm_negate_f32, 8, in_com1, ref_negate, 8);
 DEFINE_TEST_VARIANT3(arm_negate_f32, 11, in_com1, ref_negate, 11);
+DEFINE_TEST_VARIANT3(arm_negate_f32, long, in_com1, ref_negate, ARRAY_SIZE(in_com1));
 
 static void test_arm_offset_f32(
 	const uint32_t *input1, float32_t scalar, const uint32_t *ref,
@@ -174,6 +178,7 @@ static void test_arm_offset_f32(
 DEFINE_TEST_VARIANT4(arm_offset_f32, 0p5_3, in_com1, 0.5f, ref_offset, 3);
 DEFINE_TEST_VARIANT4(arm_offset_f32, 0p5_8, in_com1, 0.5f, ref_offset, 8);
 DEFINE_TEST_VARIANT4(arm_offset_f32, 0p5_11, in_com1, 0.5f, ref_offset, 11);
+DEFINE_TEST_VARIANT4(arm_offset_f32, long, in_com1, 0.5f, ref_offset, ARRAY_SIZE(in_com1));
 
 static void test_arm_scale_f32(
 	const uint32_t *input1, float32_t scalar, const uint32_t *ref,
@@ -206,6 +211,7 @@ static void test_arm_scale_f32(
 DEFINE_TEST_VARIANT4(arm_scale_f32, 0p5_3, in_com1, 0.5f, ref_scale, 3);
 DEFINE_TEST_VARIANT4(arm_scale_f32, 0p5_8, in_com1, 0.5f, ref_scale, 8);
 DEFINE_TEST_VARIANT4(arm_scale_f32, 0p5_11, in_com1, 0.5f, ref_scale, 11);
+DEFINE_TEST_VARIANT4(arm_scale_f32, long, in_com1, 0.5f, ref_scale, ARRAY_SIZE(in_com1));
 
 static void test_arm_dot_prod_f32(
 	const uint32_t *input1, const uint32_t *input2, const uint32_t *ref,
@@ -239,6 +245,8 @@ static void test_arm_dot_prod_f32(
 DEFINE_TEST_VARIANT4(arm_dot_prod_f32, 3, in_com1, in_com2, ref_dot_prod_3, 3);
 DEFINE_TEST_VARIANT4(arm_dot_prod_f32, 8, in_com1, in_com2, ref_dot_prod_4, 8);
 DEFINE_TEST_VARIANT4(arm_dot_prod_f32, 11, in_com1, in_com2, ref_dot_prod_4n1, 11);
+DEFINE_TEST_VARIANT4(arm_dot_prod_f32, long, in_com1, in_com2, ref_dot_prod_long,
+		     ARRAY_SIZE(in_com1));
 
 static void test_arm_abs_f32(
 	const uint32_t *input1, const uint32_t *ref, size_t length)
@@ -270,6 +278,41 @@ static void test_arm_abs_f32(
 DEFINE_TEST_VARIANT3(arm_abs_f32, 3, in_com1, ref_abs, 3);
 DEFINE_TEST_VARIANT3(arm_abs_f32, 8, in_com1, ref_abs, 8);
 DEFINE_TEST_VARIANT3(arm_abs_f32, 11, in_com1, ref_abs, 11);
+DEFINE_TEST_VARIANT3(arm_abs_f32, long, in_com1, ref_abs, ARRAY_SIZE(in_com1));
+
+static void test_arm_clip_f32(
+	const uint32_t *input, const uint32_t *ref, float32_t min, float32_t max, size_t length)
+{
+	float32_t *output;
+
+	/* Allocate output buffer */
+	output = malloc(length * sizeof(float32_t));
+	zassert_not_null(output, ASSERT_MSG_BUFFER_ALLOC_FAILED);
+
+	/* Run test function */
+	arm_clip_f32((float32_t *)input, output, min, max, length);
+
+	/* Validate output */
+	zassert_true(
+		test_snr_error_f32(length, output, (float32_t *)ref,
+			SNR_ERROR_THRESH),
+		ASSERT_MSG_SNR_LIMIT_EXCEED);
+
+	zassert_true(
+		test_rel_error_f32(length, output, (float32_t *)ref,
+			REL_ERROR_THRESH),
+		"incorrect computation result");
+
+	/* Free output buffer */
+	free(output);
+}
+
+DEFINE_TEST_VARIANT5(arm_clip_f32, m0p5_m0p1, in_clip, ref_clip1,
+		     -0.5f, -0.1f, ARRAY_SIZE(ref_clip1));
+DEFINE_TEST_VARIANT5(arm_clip_f32, m0p5_0p5, in_clip, ref_clip2,
+		     -0.5f, 0.5f, ARRAY_SIZE(ref_clip2));
+DEFINE_TEST_VARIANT5(arm_clip_f32, 0p1_0p5, in_clip, ref_clip3,
+		     0.1f, 0.5f, ARRAY_SIZE(ref_clip3));
 
 void test_basicmath_f32(void)
 {
@@ -297,7 +340,18 @@ void test_basicmath_f32(void)
 		ztest_unit_test(test_arm_dot_prod_f32_11),
 		ztest_unit_test(test_arm_abs_f32_3),
 		ztest_unit_test(test_arm_abs_f32_8),
-		ztest_unit_test(test_arm_abs_f32_11)
+		ztest_unit_test(test_arm_abs_f32_11),
+		ztest_unit_test(test_arm_add_f32_long),
+		ztest_unit_test(test_arm_sub_f32_long),
+		ztest_unit_test(test_arm_mult_f32_long),
+		ztest_unit_test(test_arm_negate_f32_long),
+		ztest_unit_test(test_arm_offset_f32_long),
+		ztest_unit_test(test_arm_scale_f32_long),
+		ztest_unit_test(test_arm_dot_prod_f32_long),
+		ztest_unit_test(test_arm_abs_f32_long),
+		ztest_unit_test(test_arm_clip_f32_m0p5_m0p1),
+		ztest_unit_test(test_arm_clip_f32_m0p5_0p5),
+		ztest_unit_test(test_arm_clip_f32_0p1_0p5)
 		);
 
 	ztest_run_test_suite(basicmath_f32);
