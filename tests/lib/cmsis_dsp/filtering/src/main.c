5d370a24bd7787268e20fc6de9590df28687ff9f
Stephanos Ioannidis <root@stephanos.io>
2021-08-19 23:31:55 +0900

    tests: lib: cmsis_dsp: filtering: Add FIR F16 tests for 1.9.0
    This commit adds the filtering FIR F16 test patterns and
    implementations for the CMSIS-DSP 1.9.0.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- tests/lib/cmsis_dsp/filtering/src/main.c
+++ tests/lib/cmsis_dsp/filtering/src/main.c
@@ -16,6 +16,7 @@ extern void test_filtering_biquad_f64(void);
 extern void test_filtering_fir_q7(void);
 extern void test_filtering_fir_q15(void);
 extern void test_filtering_fir_q31(void);
+extern void test_filtering_fir_f16(void);
 extern void test_filtering_fir_f32(void);
 
 extern void test_filtering_misc_q7(void);
@@ -39,6 +40,9 @@ void test_main(void)
 	test_filtering_fir_q7();
 	test_filtering_fir_q15();
 	test_filtering_fir_q31();
+#ifdef CONFIG_CMSIS_DSP_FLOAT16
+	test_filtering_fir_f16();
+#endif
 	test_filtering_fir_f32();
 #endif
 
