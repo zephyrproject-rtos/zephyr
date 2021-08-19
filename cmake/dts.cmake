4e7b7524511642b84dc69c44c5005bdccb67907f
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-08-19 11:16:28 -0700

    cmake: dts: give each DTS_ROOT a vendor-prefixes.txt
    Now that the infrastructure we need is in place, we can allow every
    element of DTS_ROOT to have its own dts/bindings/vendor-prefixes.txt
    file instead of just special-casing ZEPHYR_BASE (which is always in
    DTS_ROOT).
    
    This treats all modules equally and eases out of tree management of
    downstream-specific vendor prefixes.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- cmake/dts.cmake
+++ cmake/dts.cmake
@@ -23,8 +23,9 @@ set(EDT_PICKLE                  ${PROJECT_BINARY_DIR}/edt.pickle)
 set(DEVICETREE_UNFIXED_H        ${PROJECT_BINARY_DIR}/include/generated/devicetree_unfixed.h)
 set(DEVICE_EXTERN_H             ${PROJECT_BINARY_DIR}/include/generated/device_extern.h)
 set(DTS_POST_CPP                ${PROJECT_BINARY_DIR}/${BOARD}.dts.pre.tmp)
-# A list of generally accepted vendor prefixes.
-set_ifndef(VENDOR_PREFIXES      ${ZEPHYR_BASE}/dts/bindings/vendor-prefixes.txt)
+# The location of a list of known vendor prefixes.
+# This is relative to each element of DTS_ROOT.
+set(VENDOR_PREFIXES             dts/bindings/vendor-prefixes.txt)
 
 set_ifndef(DTS_SOURCE ${BOARD_DIR}/${BOARD}.dts)
 
@@ -113,6 +114,11 @@ if(SUPPORTS_DTS)
         DTS_ROOT_BINDINGS
         ${bindings_path}
         )
+    endif()
+
+    set(vendor_prefixes ${dts_root}/${VENDOR_PREFIXES})
+    if(EXISTS ${vendor_prefixes})
+      list(APPEND EXTRA_GEN_DEFINES_ARGS --vendor-prefixes ${vendor_prefixes})
     endif()
   endforeach()
 
@@ -228,7 +234,6 @@ if(SUPPORTS_DTS)
   --device-header-out ${DEVICE_EXTERN_H}
   --dts-out ${ZEPHYR_DTS} # As a debugging aid
   --edt-pickle-out ${EDT_PICKLE}
-  --vendor-prefixes ${VENDOR_PREFIXES}
   ${EXTRA_GEN_DEFINES_ARGS}
   )
 
