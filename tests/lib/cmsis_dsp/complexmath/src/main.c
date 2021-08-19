bd28727fe21a725bd0f9659a632159399de6500f
Stephanos Ioannidis <root@stephanos.io>
2021-08-19 21:42:10 +0900

    tests: lib: cmsis_dsp: complexmath: Add F16 tests for 1.9.0
    This commit adds the complexmath F16 test patterns and implementations
    for the CMSIS-DSP 1.9.0.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- tests/lib/cmsis_dsp/complexmath/src/main.c
+++ tests/lib/cmsis_dsp/complexmath/src/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Stephanos Ioannidis <root@stephanos.io>
+ * Copyright (c) 2021 Stephanos Ioannidis <root@stephanos.io>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,11 +9,15 @@
 
 extern void test_complexmath_q15(void);
 extern void test_complexmath_q31(void);
+extern void test_complexmath_f16(void);
 extern void test_complexmath_f32(void);
 
 void test_main(void)
 {
 	test_complexmath_q15();
 	test_complexmath_q31();
+#ifdef CONFIG_CMSIS_DSP_FLOAT16
+	test_complexmath_f16();
+#endif
 	test_complexmath_f32();
 }
