292bffc2ca9f0656bf8c44c05590626dfb1fc1b7
Stephanos Ioannidis <root@stephanos.io>
2021-08-20 22:07:29 +0900

    tests: lib: cmsis_dsp: matrix: Update Binary Q31 tests for 1.9.0
    This commit updates the matrix Binary Q31 test patterns and
    implementations for the CMSIS-DSP 1.9.0.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- tests/lib/cmsis_dsp/matrix/src/binary_q31.c
+++ tests/lib/cmsis_dsp/matrix/src/binary_q31.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2020 Stephanos Ioannidis <root@stephanos.io>
- * Copyright (C) 2010-2020 ARM Limited or its affiliates. All rights reserved.
+ * Copyright (c) 2021 Stephanos Ioannidis <root@stephanos.io>
+ * Copyright (C) 2010-2021 ARM Limited or its affiliates. All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -30,6 +30,7 @@ static void test_op2(int op, const q31_t *input1, const q31_t *input2,
 	uint16_t *dims = (uint16_t *)in_dims;
 	q31_t *tmp1, *tmp2, *output;
 	uint16_t rows, internal, columns;
+	arm_status status;
 
 	arm_matrix_instance_q31 mat_in1;
 	arm_matrix_instance_q31 mat_in2;
@@ -76,11 +77,16 @@ static void test_op2(int op, const q31_t *input1, const q31_t *input2,
 		/* Run test function */
 		switch (op) {
 		case OP2_MULT:
-			arm_mat_mult_q31(&mat_in1, &mat_in2, &mat_out);
+			status = arm_mat_mult_q31(&mat_in1, &mat_in2,
+						  &mat_out);
 			break;
 		default:
 			zassert_unreachable("invalid operation");
 		}
+
+		/* Validate status */
+		zassert_equal(status, ARM_MATH_SUCCESS,
+			      ASSERT_MSG_INCORRECT_COMP_RESULT);
 
 		/* Increment output pointer */
 		mat_out.pData += (rows * columns);
@@ -113,6 +119,7 @@ static void test_op2c(int op, const q31_t *input1, const q31_t *input2,
 	uint16_t *dims = (uint16_t *)in_dims;
 	q31_t *tmp1, *tmp2, *output;
 	uint16_t rows, internal, columns;
+	arm_status status;
 
 	arm_matrix_instance_q31 mat_in1;
 	arm_matrix_instance_q31 mat_in2;
@@ -159,11 +166,16 @@ static void test_op2c(int op, const q31_t *input1, const q31_t *input2,
 		/* Run test function */
 		switch (op) {
 		case OP2C_CMPLX_MULT:
-			arm_mat_cmplx_mult_q31(&mat_in1, &mat_in2, &mat_out);
+			status = arm_mat_cmplx_mult_q31(&mat_in1, &mat_in2,
+							&mat_out);
 			break;
 		default:
 			zassert_unreachable("invalid operation");
 		}
+
+		/* Validate status */
+		zassert_equal(status, ARM_MATH_SUCCESS,
+			      ASSERT_MSG_INCORRECT_COMP_RESULT);
 
 		/* Increment output pointer */
 		mat_out.pData += (2 * rows * columns);
