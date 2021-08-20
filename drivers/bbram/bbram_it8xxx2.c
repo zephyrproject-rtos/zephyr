4711fd69982c65d9fe510d53c23022859180e188
Yuval Peress <peress@chromium.org>
2021-08-20 13:31:33 -0600

    drivers: bbram: Add it8xxx2 BBRAM driver implementation
    Note that the it8xxx2 does not support a status register so that
    functionality is omitted.
    
    Signed-off-by: Yuval Peress <peress@chromium.org>

---
+++ drivers/bbram/bbram_it8xxx2.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2021 Google Inc
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT ite_it8xxx2_bbram
+
+#include <drivers/bbram.h>
+#include <errono.h>
+#include <sys/util.h>
+
+#include <logging/log.h>
+LOG_MODULE_REGISTER(bbram, CONFIG_BBRAM_LOG_LEVEL);
+
+/** Device config */
+struct bbram_it8xxx2_config {
+	/** BBRAM base address */
+	uintptr_t base_addr;
+	/** BBRAM size (Unit:bytes) */
+	int size;
+};
+
+#define DRV_CONFIG(dev) ((const struct bbram_it8xxx2_config *)(dev)->config)
+
+static int bbram_it8xxx2_read(const struct device *dev, size_t offset, size_t size, uint8_t *data)
+{
+	const struct bbram_it8xxx2_config *config = DRV_CONFIG(dev);
+
+	if (size < 1 || offset + size > config->size) {
+		return -EFAULT;
+	}
+
+	bytecpy(data, ((volatile uint8_t *)config->base_addr + offset), size);
+	return 0;
+}
+
+static int bbram_it8xxx2_write(const struct device *dev, size_t offset, size_t size,
+			       const uint8_t *data)
+{
+	const struct bbram_npcx_config *config = DRV_CONFIG(dev);
+
+	if (size < 1 || offset + size > config->size) {
+		return -EFAULT;
+	}
+
+	bytecpy(((volatile uint8_t *)config->base_addr + offset), data, size);
+	return 0;
+}
+
+static const struct bbram_driver_api bbram_it8xxx2_driver_api = {
+	.read = bbram_it8xxx2_read,
+	.write = bbram_it8xxx2_write,
+};
+
+static int bbram_it8xxx2_init(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	return 0;
+}
+
+#define BBRAM_INIT(inst)                                                                           \
+	static struct {                                                                            \
+	} bbram_data_##inst;                                                                       \
+	static const struct bbram_it8xxx2_config bbram_cfg_##inst = {                              \
+		.base_addr = DT_INST_REG_ADDR(inst),                                               \
+		.size = DT_INST_REG_SIZE(inst),                                                    \
+	};                                                                                         \
+	DEVICE_DT_INST_DEFINE(inst, bbram_it8xxx2_init, NULL, &bbram_data_##inst,                  \
+			      &bbram_cfg_##inst, PRE_KERNEL_1, CONFIG_BBRAM_INIT_PRIORITY,         \
+			      &bbram_it8xxx2_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(BBRAM_INIT);
