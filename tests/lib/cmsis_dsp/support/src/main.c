a39874fe21f0af2b1bf1cccff0eb3d866fa9ee99
Stephanos Ioannidis <root@stephanos.io>
2021-08-22 00:26:44 +0900

    tests: lib: cmsis_dsp: support: Add F16 tests for 1.9.0
    This commit adds the support F16 test patterns and implementations for
    the CMSIS-DSP 1.9.0.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- tests/lib/cmsis_dsp/support/src/main.c
+++ tests/lib/cmsis_dsp/support/src/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Stephanos Ioannidis <root@stephanos.io>
+ * Copyright (c) 2021 Stephanos Ioannidis <root@stephanos.io>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -10,6 +10,7 @@
 extern void test_support_q7(void);
 extern void test_support_q15(void);
 extern void test_support_q31(void);
+extern void test_support_f16(void);
 extern void test_support_f32(void);
 extern void test_support_barycenter_f32(void);
 
@@ -18,6 +19,9 @@ void test_main(void)
 	test_support_q7();
 	test_support_q15();
 	test_support_q31();
+#ifdef CONFIG_CMSIS_DSP_FLOAT16
+	test_support_f16();
+#endif
 	test_support_f32();
 	test_support_barycenter_f32();
 }
