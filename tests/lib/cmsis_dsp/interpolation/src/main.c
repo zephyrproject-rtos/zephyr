aea96e453ce5625f8862fde3948654e11b485746
Stephanos Ioannidis <root@stephanos.io>
2021-08-22 19:00:55 +0900

    tests: lib: cmsis_dsp: interpolation: Add F16 tests for 1.9.0
    This commit adds the interpolation F16 test patterns and
    implementations for the CMSIS-DSP 1.9.0.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- tests/lib/cmsis_dsp/interpolation/src/main.c
+++ tests/lib/cmsis_dsp/interpolation/src/main.c
@@ -10,6 +10,7 @@
 extern void test_interpolation_q7(void);
 extern void test_interpolation_q15(void);
 extern void test_interpolation_q31(void);
+extern void test_interpolation_f16(void);
 extern void test_interpolation_f32(void);
 
 void test_main(void)
@@ -17,5 +18,8 @@ void test_main(void)
 	test_interpolation_q7();
 	test_interpolation_q15();
 	test_interpolation_q31();
+#ifdef CONFIG_CMSIS_DSP_FLOAT16
+	test_interpolation_f16();
+#endif
 	test_interpolation_f32();
 }
