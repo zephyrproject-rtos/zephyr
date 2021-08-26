b34c72d4996781c38c2b1b83e8cd19b0b91a914f
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2021-08-26 15:36:53 +0200

    Bluetooth: Mesh: Initialize UUID in shell's mod_pub_set
    PR #35774 introduced a uuid field in the bt_mesh_cfg_mod_pub structure.
    The shell does not initialize this pointer before passing it to the
    access layer. Add a line to initialize this pointer.
    
    Fixes #38016.
    
    Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

--- subsys/bluetooth/mesh/shell.c
+++ subsys/bluetooth/mesh/shell.c
@@ -1638,6 +1638,7 @@ static int mod_pub_set(const struct shell *shell, uint16_t addr, uint16_t mod_id
 	int err;
 
 	pub.addr = strtoul(argv[0], NULL, 0);
+	pub.uuid = NULL;
 	pub.app_idx = strtoul(argv[1], NULL, 0);
 	pub.cred_flag = str2bool(argv[2]);
 	pub.ttl = strtoul(argv[3], NULL, 0);
