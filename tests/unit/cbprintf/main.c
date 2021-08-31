59e298b16d018b8ec1e88c7d53f3fb104ed853cb
Nicolas Pitre <npitre@baylibre.com>
2021-08-31 16:05:18 -0400

    lib/os/cbprintf_nano.c: avoid sign extension on unsigned formats
    There might be a sign extension when a long is promoted to
    int_value_type and the former type is smaller than the later.
    This produces the wrong output if the specified format is unsigned.
    
    Let's avoid this problem by handling signed and unsigned cases
    explicitly. When the type already matches int_value_type then the
    compiler is smart enough to recognize the redundancy and removes
    unneeded duplications automatically, meaning that the code will stay
    small when code size matters.
    
    A similar issue also existed in the restricted %llu case.
    The fix is the same as above.
    
    Those fixes exposed wrong results in the printk.c test with %llx
    so fix that as well.
    
    Signed-off-by: Nicolas Pitre <npitre@baylibre.com>

--- tests/unit/cbprintf/main.c
+++ tests/unit/cbprintf/main.c
@@ -510,6 +510,7 @@ static void test_d_length(void)
 	int max = 1876543210;
 	long long svll = 123LL << 48;
 	long long svll2 = -2LL;
+	unsigned long long uvll = 4000000000LLU;
 	int rc;
 
 	TEST_PRF(&rc, "%d/%d", min, max);
@@ -526,22 +527,22 @@ static void test_d_length(void)
 		PRF_CHECK("46/-22", rc);
 	}
 
-	TEST_PRF(&rc, "%ld/%ld", (long)min, (long)max);
+	TEST_PRF(&rc, "%ld/%ld/%lu/", (long)min, (long)max, 4000000000UL);
 	if (IS_ENABLED(CONFIG_CBPRINTF_FULL_INTEGRAL)
 	    || (sizeof(long) <= 4)
 	    || IS_ENABLED(CONFIG_CBPRINTF_NANO)) {
-		PRF_CHECK("-1234567890/1876543210", rc);
+		PRF_CHECK("-1234567890/1876543210/4000000000/", rc);
 	} else {
-		PRF_CHECK("%ld/%ld", rc);
+		PRF_CHECK("%ld/%ld/%lu/", rc);
 	}
 
-	TEST_PRF(&rc, "/%lld/%lld/%lld/", svll, -svll, svll2);
+	TEST_PRF(&rc, "/%lld/%lld/%lld/%llu/", svll, -svll, svll2, uvll);
 	if (IS_ENABLED(CONFIG_CBPRINTF_FULL_INTEGRAL)) {
-		PRF_CHECK("/34621422135410688/-34621422135410688/-2/", rc);
+		PRF_CHECK("/34621422135410688/-34621422135410688/-2/4000000000/", rc);
 	} else if (IS_ENABLED(CONFIG_CBPRINTF_COMPLETE)) {
-		PRF_CHECK("/%lld/%lld/%lld/", rc);
+		PRF_CHECK("/%lld/%lld/%lld/%llu/", rc);
 	} else if (IS_ENABLED(CONFIG_CBPRINTF_NANO)) {
-		PRF_CHECK("/ERR/ERR/-2/", rc);
+		PRF_CHECK("/ERR/ERR/-2/4000000000/", rc);
 	} else {
 		zassert_true(false, "Missed case!");
 	}
