24eb1a03c930502e00b99d4d2b9f93d83bbb2064
MinSteve <min98st1@gmail.com>
2021-09-01 21:23:36 +0700

    Add necessary files for porting new board
    Porting new board need a SoC folder and board folder. These are definition for S32K144EVB

---
+++ boards/arm/s32k144_evb/board.cmake
@@ -0,0 +1,6 @@
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+#Link to debugger folder which use OpenOCD
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
---
+++ soc/arm/nxp_s32/s32k14x/soc.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021, Dung Tran
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+/* Add include for DTS generated information */
+#include <devicetree.h>
+
+#endif /* _SOC__H_ */
