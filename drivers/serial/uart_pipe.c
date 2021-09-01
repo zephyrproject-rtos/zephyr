033cfba6187c944105c9c9c71075827b9498e7a0
Pieter De Gendt <pieter.degendt@basalte.be>
2021-09-01 12:15:16 +0200

    uart_pipe: move driver from console to serial
    The uart_pipe driver is not dependent on any console driver,
    however a serial driver is required.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- drivers/serial/uart_pipe.c
+++ drivers/serial/uart_pipe.c
@@ -12,13 +12,13 @@
  */
 
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(uart_pipe, CONFIG_UART_CONSOLE_LOG_LEVEL);
+LOG_MODULE_REGISTER(uart_pipe);
 
 #include <zephyr/kernel.h>
 
 #include <zephyr/drivers/uart.h>
 
-#include <zephyr/drivers/console/uart_pipe.h>
+#include <zephyr/drivers/uart_pipe.h>
 #include <zephyr/sys/printk.h>
 
 static const struct device *uart_pipe_dev;
