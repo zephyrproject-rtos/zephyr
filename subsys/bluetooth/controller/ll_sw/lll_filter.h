b9a76b0cb0b2b2cbb8d3d089f288b3250f5e3c75
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2021-09-06 07:36:22 +0530

    Bluetooth: Controller: Minor filter accept list related refactoring
    Refactoring related to filter accept list, resolving list,
    and removed the redundant anon variable.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/lll_filter.h
+++ subsys/bluetooth/controller/ll_sw/lll_filter.h
@@ -10,6 +10,7 @@
 #define FAL_SIZE 8
 #endif /* CONFIG_BT_CTLR_FAL_SIZE */
 
+#define IRK_SIZE               16
 #define FILTER_IDX_NONE        0xFF
 #define LLL_FILTER_BITMASK_ALL (BIT(FAL_SIZE) - 1)
 
@@ -28,25 +29,26 @@ struct lll_filter {
 
 /* Filter Accept List peer list */
 struct lll_fal {
-	uint8_t      taken:1;
-	uint8_t      id_addr_type:1;
-	uint8_t      rl_idx;
+	uint8_t   taken:1;
+	uint8_t   id_addr_type:1;
+	uint8_t   rl_idx;
 	bt_addr_t id_addr;
 };
 
-struct lll_resolvelist {
-	uint8_t      taken:1;
-	uint8_t      rpas_ready:1;
-	uint8_t      pirk:1;
-	uint8_t      lirk:1;
-	uint8_t      dev:1;
-	uint8_t      fal:1;
+/* Resolve list */
+struct lll_resolve_list {
+	uint8_t   taken:1;
+	uint8_t   rpas_ready:1;
+	uint8_t   pirk:1;
+	uint8_t   lirk:1;
+	uint8_t   dev:1;
+	uint8_t   fal:1;
 
-	uint8_t      id_addr_type:1;
+	uint8_t   id_addr_type:1;
 	bt_addr_t id_addr;
 
-	uint8_t      local_irk[16];
-	uint8_t      pirk_idx;
+	uint8_t   local_irk[IRK_SIZE];
+	uint8_t   pirk_idx;
 	bt_addr_t curr_rpa;
 	bt_addr_t peer_rpa;
 	bt_addr_t *local_rpa;
@@ -55,7 +57,7 @@ struct lll_resolvelist {
 #endif
 };
 
-bool ull_filter_lll_lrpa_used(uint8_t rl_idx);
+extern bool ull_filter_lll_lrpa_used(uint8_t rl_idx);
 extern bt_addr_t *ull_filter_lll_lrpa_get(uint8_t rl_idx);
 extern uint8_t *ull_filter_lll_irks_get(uint8_t *count);
 extern uint8_t ull_filter_lll_rl_idx(bool fal, uint8_t devmatch_id);
@@ -63,7 +65,7 @@ extern uint8_t ull_filter_lll_rl_irk_idx(uint8_t irkmatch_id);
 extern bool ull_filter_lll_irk_in_fal(uint8_t rl_idx);
 extern struct lll_filter *ull_filter_lll_get(bool fal);
 extern struct lll_fal *ull_filter_lll_fal_get(void);
-extern struct lll_resolvelist *ull_filter_lll_resolvelist_get(void);
+extern struct lll_resolve_list *ull_filter_lll_resolve_list_get(void);
 extern bool ull_filter_lll_rl_idx_allowed(uint8_t irkmatch_ok, uint8_t rl_idx);
 extern bool ull_filter_lll_rl_addr_allowed(uint8_t id_addr_type, uint8_t *id_addr,
 					   uint8_t *rl_idx);
