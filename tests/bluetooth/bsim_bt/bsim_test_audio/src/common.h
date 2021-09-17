396f2c790edaab3f7ea9b0bc03e11685dfd126e3
Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>
2021-09-17 11:34:22 +0200

    Bluetooth: Audio: Test - add "PASSED" output to BabbleSim tests
    Redefine PASS macro to add "PASSED" to the output.  This makes passes
    easier to grep for by ensuring a consistent pattern in the output.
    
    Signed-off-by: Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_audio/src/common.h
+++ tests/bluetooth/bsim_bt/bsim_test_audio/src/common.h
@@ -52,7 +52,7 @@
 #define PASS(...) \
 	do { \
 		bst_result = Passed; \
-		bs_trace_info_time(1, __VA_ARGS__); \
+		bs_trace_info_time(1, "PASSED: " __VA_ARGS__); \
 	} while (0)
 
 #define AD_SIZE 1
