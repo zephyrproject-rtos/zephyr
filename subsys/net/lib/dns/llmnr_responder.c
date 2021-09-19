0a6d09a3fee540de032d370b302eb8b8e7120a76
Markus Fuchs <markus.fuchs@ch.sauter-bc.com>
2021-09-19 23:20:19 +0200

    net: dns: Make mdns and llmnr responders join their multicast groups
    As with IPv6, the mdns and llmnr responders should join their multicast
    groups for IPv4 instead of just adding the multicast address to the
    interface.
    
    Signed-off-by: Markus Fuchs <markus.fuchs@ch.sauter-bc.com>

--- subsys/net/lib/dns/llmnr_responder.c
+++ subsys/net/lib/dns/llmnr_responder.c
@@ -24,6 +24,7 @@ LOG_MODULE_REGISTER(net_llmnr_responder, CONFIG_LLMNR_RESPONDER_LOG_LEVEL);
 #include <net/net_pkt.h>
 #include <net/dns_resolve.h>
 #include <net/udp.h>
+#include <net/igmp.h>
 
 #include "dns_pack.h"
 #include "ipv6.h"
@@ -571,10 +572,10 @@ static void setup_ipv6_addr(struct sockaddr_in6 *local_addr)
 static void iface_ipv4_cb(struct net_if *iface, void *user_data)
 {
 	struct in_addr *addr = user_data;
-	struct net_if_mcast_addr *ifaddr;
+	int ret;
 
-	ifaddr = net_if_ipv4_maddr_add(iface, addr);
-	if (!ifaddr) {
+	ret = net_ipv4_igmp_join(iface, addr);
+	if (ret < 0) {
 		NET_DBG("Cannot add IPv4 multicast address to iface %p",
 			iface);
 	}
