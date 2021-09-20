9e20fcda7763246379d6e74b4b1ad56bf254550c
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2021-09-20 19:01:14 +0530

    Bluetooth: Controller: Fix Extended Adv Report directed addr type
    Fix Extended Advertising Report to return correct directed
    address type.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan_internal.h
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan_internal.h
@@ -8,7 +8,8 @@ void lll_scan_isr_resume(void *param);
 bool lll_scan_adva_check(struct lll_scan *lll, uint8_t addr_type, uint8_t *addr,
 			 uint8_t rl_idx);
 bool lll_scan_ext_tgta_check(struct lll_scan *lll, bool pri, bool is_init,
-			     struct pdu_adv *pdu, uint8_t rl_idx);
+			     struct pdu_adv *pdu, uint8_t rl_idx,
+			     bool *dir_report);
 void lll_scan_prepare_connect_req(struct lll_scan *lll, struct pdu_adv *pdu_tx,
 				  uint8_t phy, uint8_t adv_tx_addr,
 				  uint8_t *adv_addr, uint8_t init_tx_addr,
