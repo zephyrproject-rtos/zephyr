225dc76ff37a372a89da2121e6b5c5456ff530b6
Nicolai Glud <nicolai.glud@prevas.dk>
2021-09-20 12:55:57 +0200

    drivers: flash: added flexspi hyperflash.
    This enables accessing the hyperflash through the flash api.
    
    Added a feature to memc_mcux_flexspi that waits for flexspi bus to be
    quiet.
    
    Signed-off-by: Nicolai Glud <nicolai.glud@prevas.dk>

--- drivers/memc/memc_mcux_flexspi.h
+++ drivers/memc/memc_mcux_flexspi.h
@@ -8,6 +8,8 @@
 #include <sys/types.h>
 #include <fsl_flexspi.h>
 
+void memc_flexspi_wait_bus_idle(const struct device *dev);
+
 bool memc_flexspi_is_running_xip(const struct device *dev);
 
 int memc_flexspi_update_lut(const struct device *dev, uint32_t index,
