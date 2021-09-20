ff5030e2bfdf0ce8ab6c20ab98c927e8d53d42da
Ramiro Merello <rmerello@itba.edu.ar>
2021-09-19 23:14:43 -0400

    testing

--- tests/lib/json/src/main.c
+++ tests/lib/json/src/main.c
@@ -232,7 +232,7 @@ static void test_json_decoding(void)
 static void test_json_limits(void) 
 {
 	int ret = 0;
-	char encoded[] = "{	\"int_max\": 2147483647,"
+	char encoded[] = "{\"int_max\": 2147483647,"
 			 "\"int_min\": -2147483648"
 			 /* "	\"uint_min\": 4294967295" */
 			 "}";
