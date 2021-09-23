509a4fdd3d3fff6f90812663d73ca19ea73e5387
Erwan Gouriou <erwan.gouriou@linaro.org>
2021-09-23 17:01:01 +0200

    soc: stm32l0: Disable clock after write to DBGMCU registers
    Similarly to what was done on stm32g0, disable DBGMCU clock
    after operation to avoid conflict with openocd.
    
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- soc/arm/st_stm32/stm32l0/power.c
+++ soc/arm/st_stm32/stm32l0/power.c
@@ -88,6 +88,7 @@ static int stm32_power_init(const struct device *dev)
 	/* Enable the Debug Module during STOP mode */
 	LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_DBGMCU);
 	LL_DBGMCU_EnableDBGStopMode();
+	LL_APB2_GRP1_DisableClock(LL_APB2_GRP1_PERIPH_DBGMCU);
 #endif /* CONFIG_DEBUG */
 
 	return 0;
