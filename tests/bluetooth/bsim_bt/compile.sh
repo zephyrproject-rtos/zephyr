f86e3eff63adf4f2b585142ff553cd8bd3c110de
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2021-09-23 10:57:13 +0530

    tests: Bluetooth: bsim: Use separate DUT and TST EDTT builds
    Use separate DUT and TST builds in EDTT HCI tests so that
    the tester can send Data Length Requests with txOctets and
    maxTxTime as required by the test specification. TST build
    has HCI parameter checks disabled.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- tests/bluetooth/bsim_bt/compile.sh
+++ tests/bluetooth/bsim_bt/compile.sh
@@ -63,7 +63,10 @@ app=tests/bluetooth/bsim_bt/bsim_test_multiple compile
 app=tests/bluetooth/bsim_bt/bsim_test_advx compile
 app=tests/bluetooth/bsim_bt/bsim_test_iso compile
 app=tests/bluetooth/bsim_bt/bsim_test_audio compile
-app=tests/bluetooth/bsim_bt/edtt_ble_test_app/hci_test_app compile
+app=tests/bluetooth/bsim_bt/edtt_ble_test_app/hci_test_app \
+  conf_file=prj_dut.conf compile
+app=tests/bluetooth/bsim_bt/edtt_ble_test_app/hci_test_app \
+  conf_file=prj_tst.conf compile
 app=tests/bluetooth/bsim_bt/edtt_ble_test_app/gatt_test_app compile
 app=tests/bluetooth/bsim_bt/bsim_test_mesh compile
 app=tests/bluetooth/bsim_bt/bsim_test_mesh conf_file=prj_low_lat.conf compile
