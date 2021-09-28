a38fa6dae7269f56ce5eef055b311ff10bb05a5c
Nicolas Pitre <npitre@baylibre.com>
2021-09-27 21:43:02 -0400

    net_buf_alloc_len: make SMP safe
    Replace irq_lock() with a spin lock.
    
    Signed-off-by: Nicolas Pitre <npitre@baylibre.com>

--- subsys/net/buf.c
+++ subsys/net/buf.c
@@ -232,16 +232,16 @@ struct net_buf *net_buf_alloc_len(struct net_buf_pool *pool, size_t size,
 {
 	uint64_t end = sys_clock_timeout_end_calc(timeout);
 	struct net_buf *buf;
-	unsigned int key;
+	k_spinlock_key_t key;
 
 	__ASSERT_NO_MSG(pool);
 
 	NET_BUF_DBG("%s():%d: pool %p size %zu", func, line, pool, size);
 
-	/* We need to lock interrupts temporarily to prevent race conditions
+	/* We need to prevent race conditions
 	 * when accessing pool->uninit_count.
 	 */
-	key = irq_lock();
+	key = k_spin_lock(&pool->lock);
 
 	/* If there are uninitialized buffers we're guaranteed to succeed
 	 * with the allocation one way or another.
@@ -256,19 +256,19 @@ struct net_buf *net_buf_alloc_len(struct net_buf_pool *pool, size_t size,
 		if (pool->uninit_count < pool->buf_count) {
 			buf = k_lifo_get(&pool->free, K_NO_WAIT);
 			if (buf) {
-				irq_unlock(key);
+				k_spin_unlock(&pool->lock, key);
 				goto success;
 			}
 		}
 
 		uninit_count = pool->uninit_count--;
-		irq_unlock(key);
+		k_spin_unlock(&pool->lock, key);
 
 		buf = pool_get_uninit(pool, uninit_count);
 		goto success;
 	}
 
-	irq_unlock(key);
+	k_spin_unlock(&pool->lock, key);
 
 #if defined(CONFIG_NET_BUF_LOG) && (CONFIG_NET_BUF_LOG_LEVEL >= LOG_LEVEL_WRN)
 	if (K_TIMEOUT_EQ(timeout, K_FOREVER)) {
