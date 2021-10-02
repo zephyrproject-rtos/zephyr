2fcd8340c6c37a243f5234bccebbe50fb444ad8c
Gerson Fernando Budke <gerson.budke@atl-electronics.com>
2021-10-02 13:55:45 -0300

    boards: arm: gd32f403z_eval: Enable gd32isp runner
    Add support to gd32isp runner and update board documentation.
    
    Signed-off-by: Gerson Fernando Budke <gerson.budke@atl-electronics.com>

--- boards/arm/gd32f403z_eval/board.cmake
+++ boards/arm/gd32f403z_eval/board.cmake
@@ -2,3 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+
+board_runner_args(gd32isp "--device=GD32F403ZET6")
+include(${ZEPHYR_BASE}/boards/common/gd32isp.board.cmake)
