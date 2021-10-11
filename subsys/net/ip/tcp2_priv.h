471304d57619d68e113c61a56069b548159eeccf
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2021-10-11 13:29:47 +0300

    net: tcp: Fix macro conn_send_data_dump()
    Fixes error in macro parameter.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- subsys/net/ip/tcp2_priv.h
+++ subsys/net/ip/tcp2_priv.h
@@ -127,7 +127,7 @@
 		NET_DBG("conn: %p total=%zd, unacked_len=%d, "                 \
 			"send_win=%hu, mss=%hu",                               \
 			(_conn), net_pkt_get_len((_conn)->send_data),          \
-			conn->unacked_len, conn->send_win,                     \
+			_conn->unacked_len, _conn->send_win,                   \
 			(uint16_t)conn_mss((_conn)));                          \
 		NET_DBG("conn: %p send_data_timer=%hu, send_data_retries=%hu", \
 			(_conn),                                               \
