15d0253c1b21dcf7c7d83a06621f5a77750d39ff
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2021-10-11 15:06:24 +0200

    soc: nrf52: Add Kconfig option for enabling REG0 DC/DC converter
    Add a Kconfig option, similar to the one that is already available
    for nRF5340, that allows enabling the REG0 (VDDH) DC/DC converter
    in nRF52840. Make use of this option in Nordic boards: nRF52840 DK
    and nRF52840 Dongle.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- drivers/usb/device/usb_dc_nrfx.c
+++ drivers/usb/device/usb_dc_nrfx.c
@@ -1932,7 +1932,8 @@ static int usb_init(const struct device *arg)
 		.dcdcen = IS_ENABLED(CONFIG_SOC_DCDC_NRF52X) ||
 			  IS_ENABLED(CONFIG_SOC_DCDC_NRF53X_APP),
 #if NRFX_POWER_SUPPORTS_DCDCEN_VDDH
-		.dcdcenhv = IS_ENABLED(CONFIG_SOC_DCDC_NRF53X_HV),
+		.dcdcenhv = IS_ENABLED(CONFIG_SOC_DCDC_NRF52X_HV) ||
+			    IS_ENABLED(CONFIG_SOC_DCDC_NRF53X_HV),
 #endif
 	};
 
