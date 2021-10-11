712ef1b9140f52e88a0c69a6bbf7eb8cebf1491e
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2021-10-11 16:34:16 +0300

    net: tcp: Use BIT() macros
    Use BIT() macro to make code more readable and consistent.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- subsys/net/ip/tcp2_priv.h
+++ subsys/net/ip/tcp2_priv.h
@@ -167,14 +167,14 @@ struct tcphdr {
 } __packed;
 
 enum th_flags {
-	FIN = 1,
-	SYN = 1 << 1,
-	RST = 1 << 2,
-	PSH = 1 << 3,
-	ACK = 1 << 4,
-	URG = 1 << 5,
-	ECN = 1 << 6,
-	CWR = 1 << 7,
+	FIN = BIT(0),
+	SYN = BIT(1),
+	RST = BIT(2),
+	PSH = BIT(3),
+	ACK = BIT(4),
+	URG = BIT(5),
+	ECN = BIT(6),
+	CWR = BIT(7),
 };
 
 enum tcp_state {
