e58a243428e7ee77ae36cccb7d1b7cc8b5ccef97
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2021-10-13 12:02:01 +0300

    edac: tests: Increase code coverage
    Add test for better code coverage.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- tests/subsys/edac/ibecc/src/ibecc.c
+++ tests/subsys/edac/ibecc/src/ibecc.c
@@ -305,6 +305,18 @@ static void test_ibecc_error_inject_test_uc(void)
 }
 #endif
 
+/* Used only for code coverage */
+
+bool z_x86_do_kernel_nmi(const z_arch_esf_t *esf);
+
+static void test_trigger_nmi_handler(void)
+{
+	bool ret;
+
+	ret = z_x86_do_kernel_nmi(NULL);
+	zassert_false(ret, "Test that NMI handling fails");
+}
+
 void test_edac_dummy_api(void);
 
 void test_main(void)
@@ -314,6 +326,7 @@ void test_main(void)
 #endif
 
 	ztest_test_suite(ibecc,
+			 ztest_unit_test(test_trigger_nmi_handler),
 			 ztest_unit_test(test_ibecc_initialized),
 			 ztest_unit_test(test_ibecc_api),
 			 ztest_unit_test(test_edac_dummy_api),
