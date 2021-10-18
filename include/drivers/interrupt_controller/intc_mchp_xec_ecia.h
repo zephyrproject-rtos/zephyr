b83413b8befbb5925d1b0604e8d9d55ae873084e
Jay Vasanth <jay.vasanth@microchip.com>
2021-10-18 16:44:45 -0400

    Microchip: MEC172x: eSPI driver
    MEC172x eSPI driver, eSPI pin programming, interrupt updates related
    to eSPI and other updates for MEC172x eSPI driver.
    
    Signed-off-by: Jay Vasanth <jay.vasanth@microchip.com>

--- include/drivers/interrupt_controller/intc_mchp_xec_ecia.h
+++ include/drivers/interrupt_controller/intc_mchp_xec_ecia.h
@@ -29,12 +29,28 @@
 int mchp_xec_ecia_enable(int girq_id, int src);
 
 /**
+ * @brief enable EXTI interrupt for specific line specified by parameter
+ * encoded with MCHP_XEC_ECIA macro.
+ *
+ * @param ecia_info is GIRQ connection encoded with MCHP_XEC_ECIA
+ */
+int mchp_xec_ecia_info_enable(int ecia_info);
+
+/**
  * @brief disable EXTI interrupt for specific line
  *
  * @param girq_id is the GIRQ number (8 - 26)
  * @param src is the interrupt source in the GIRQ (0 - 31)
  */
 int mchp_xec_ecia_disable(int girq_id, int src);
+
+/**
+ * @brief disable EXTI interrupt for specific line specified by parameter
+ * encoded with MCHP_XEC_ECIA macro.
+ *
+ * @param ecia_info is GIRQ connection encoded with MCHP_XEC_ECIA
+ */
+int mchp_xec_ecia_info_disable(int ecia_info);
 
 
 /* callback for ECIA GIRQ interrupt source */
@@ -50,6 +66,16 @@ typedef void (*mchp_xec_ecia_callback_t) (int girq_id, int src, void *user);
  */
 int mchp_xec_ecia_set_callback(int girq_id, int src,
 			       mchp_xec_ecia_callback_t cb, void *data);
+
+/**
+ * @brief set GIRQn interrupt source callback
+ *
+ * @param ecia_info is GIRQ connection encoded with MCHP_XEC_ECIA
+ * @param cb user callback
+ * @param data user data
+ */
+int mchp_xec_ecia_info_set_callback(int ecia_info, mchp_xec_ecia_callback_t cb,
+				    void *data);
 
 /**
  * @brief set GIRQn interrupt source callback
@@ -140,5 +166,45 @@ uint32_t mchp_xec_ecia_girq_result(uint8_t girq_id);
  * @param nvic_num is 0 to maximum NVIC inputs for the chip.
  */
 void mchp_xec_ecia_nvic_clr_pend(uint32_t nvic_num);
+
+/* API using GIRQ parameters encoded with MCHP_XEC_ECIA */
+
+/** @brief enable or disable aggregated GIRQ output
+ *
+ * @param ecia_info is GIRQ connection encoded with MCHP_XEC_ECIA
+ * @param enable is flag to indicate enable(1) or disable(0)
+ */
+void mchp_xec_ecia_info_girq_aggr_en(int ecia_info, uint8_t enable);
+
+/** @brief clear GIRQ latched source status bit
+ *
+ * @param ecia_info is GIRQ connection encoded with MCHP_XEC_ECIA
+ */
+void mchp_xec_ecia_info_girq_src_clr(int ecia_info);
+
+/** @brief enable a source in a GIRQ
+ *
+ * @param ecia_info is GIRQ connection encoded with MCHP_XEC_ECIA
+ */
+void mchp_xec_ecia_info_girq_src_en(int ecia_info);
+
+/** @brief disable a source in a GIRQ
+ *
+ * @param ecia_info is GIRQ connection encoded with MCHP_XEC_ECIA
+ */
+void mchp_xec_ecia_info_girq_src_dis(int ecia_info);
+
+/** @brief Read GIRQ result register (bit-wise OR of enable and source)
+ *
+ * @param ecia_info is GIRQ connection encoded with MCHP_XEC_ECIA
+ * @return 32-bit unsigned result register value
+ */
+uint32_t mchp_xec_ecia_info_girq_result(int ecia_info);
+
+/** @brief Clear external NVIC input pending status given encoded ECIA info.
+ *
+ * @param ecia_info is GIRQ connection encoded with MCHP_XEC_ECIA
+ */
+void mchp_xec_ecia_info_nvic_clr_pend(int ecia_info);
 
 #endif /* ZEPHYR_DRIVERS_INTERRUPT_CONTROLLER_MCHP_XEC_ECIA_H_ */
