708bc5966ef70b2cccc4e64107c0188ed523a287
Felipe Neves <felipe.neves@espressif.com>
2021-10-18 17:19:25 -0300

    drivers: serial: add support for esp32c3
    into esp32_serial unified driver
    
    Signed-off-by: Felipe Neves <felipe.neves@espressif.com>

--- soc/riscv/esp32c3/soc.h
+++ soc/riscv/esp32c3/soc.h
@@ -40,6 +40,9 @@ extern void esp_rom_uart_attach(void);
 extern void esp_rom_uart_tx_wait_idle(uint8_t uart_no);
 extern STATUS esp_rom_uart_tx_one_char(uint8_t chr);
 extern STATUS esp_rom_uart_rx_one_char(uint8_t *chr);
+extern int esp_rom_gpio_matrix_in(uint32_t gpio, uint32_t signal_index, bool inverted);
+extern int esp_rom_gpio_matrix_out(uint32_t gpio, uint32_t signal_index,
+				bool out_invrted, bool out_enabled_inverted);
 extern void esp_rom_ets_set_user_start(uint32_t start);
 extern void esprv_intc_int_set_threshold(int priority_threshold);
 uint32_t soc_intr_get_next_source(void);
