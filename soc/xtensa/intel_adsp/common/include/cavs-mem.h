a121e423ab5369089de0c15380362372713485e6
Andy Ross <andrew.j.ross@intel.com>
2021-10-19 13:53:57 -0700

    soc/intel_adsp: Unify fake linker sections
    There are multiple "fake" (not part of image) sections needless linked
    to explicit addresses right now.  This should be cleaned up, but in
    the meantime let's at least put their definitions all in one place so
    they aren't cut/pasted into every platform.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- soc/xtensa/intel_adsp/common/include/cavs-mem.h
+++ soc/xtensa/intel_adsp/common/include/cavs-mem.h
@@ -9,5 +9,15 @@
 #define L2_SRAM_BASE (DT_REG_ADDR(DT_NODELABEL(sram0)))
 #define L2_SRAM_SIZE (DT_REG_SIZE(DT_NODELABEL(sram0)))
 
+/* These are fake section addresses used only in the linker scripts */
+#define IDT_BASE		(L2_SRAM_BASE + L2_SRAM_SIZE)
+#define IDT_SIZE		0x2000
+#define UUID_ENTRY_ELF_BASE	0x1FFFA000
+#define UUID_ENTRY_ELF_SIZE	0x6000
+#define LOG_ENTRY_ELF_BASE	0x20000000
+#define LOG_ENTRY_ELF_SIZE	0x2000000
+#define EXT_MANIFEST_ELF_BASE	(LOG_ENTRY_ELF_BASE + LOG_ENTRY_ELF_SIZE)
+#define EXT_MANIFEST_ELF_SIZE	0x2000000
+
 #endif /* _ZEPHYR_SOC_INTEL_ADSP_MEM */
 
