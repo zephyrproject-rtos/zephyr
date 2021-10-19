33bd649d4e872a02d8b3a2ef6e6d1fe269dfc3aa
Andy Ross <andrew.j.ross@intel.com>
2021-10-19 15:44:52 -0700

    soc/intel_adsp: Unify "RAM" region
    There was a divergent set of definitions for a "RAM" region for the
    benefit of memory layout in the platform headers.  In fact there was
    only one platform dependence (cAVS 1.5 has 32k instead of 64k
    reserved).  Put that into kconfig in a single place, and add a warning
    that this is a trap region with hidden dependencies in both Zephyr and
    SOF.  Good enough until we clean this up and make everything visible
    to the linker.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- soc/xtensa/intel_adsp/common/include/cavs-mem.h
+++ soc/xtensa/intel_adsp/common/include/cavs-mem.h
@@ -5,9 +5,14 @@
 #define _ZEPHYR_SOC_INTEL_ADSP_MEM
 
 #include <devicetree.h>
+#include <cavs-vectors.h>
 
 #define L2_SRAM_BASE (DT_REG_ADDR(DT_NODELABEL(sram0)))
 #define L2_SRAM_SIZE (DT_REG_SIZE(DT_NODELABEL(sram0)))
+
+/* Linker-usable RAM region */
+#define RAM_BASE (L2_SRAM_BASE + CONFIG_HP_SRAM_RESERVE + VECTOR_TBL_SIZE)
+#define RAM_SIZE (L2_SRAM_SIZE - CONFIG_HP_SRAM_RESERVE - VECTOR_TBL_SIZE)
 
 /* These are fake section addresses used only in the linker scripts */
 #define IDT_BASE		(L2_SRAM_BASE + L2_SRAM_SIZE)
@@ -20,4 +25,3 @@
 #define EXT_MANIFEST_ELF_SIZE	0x2000000
 
 #endif /* _ZEPHYR_SOC_INTEL_ADSP_MEM */
-
