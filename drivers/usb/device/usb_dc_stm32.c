4e6df08d2b16de3f1c5a353413460b0325f25adb
Erwan Gouriou <erwan.gouriou@linaro.org>
2021-10-19 16:58:30 +0200

    drivers/usb: stm23: Fix deprecated macro declaration
    Use of '__DEPRECATED_MACRO' was not compatible with the way macro
    is used later in this driver.
    Remove it and keep the warning as vector for deprecation information.
    
    Additionally, replace DT_NODE_HAS_PROP with DT_INST_PROP as
    using the former is not recommended with boolean properties.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- drivers/usb/device/usb_dc_stm32.c
+++ drivers/usb/device/usb_dc_stm32.c
@@ -44,8 +44,8 @@ LOG_MODULE_REGISTER(usb_dc_stm32);
 #elif DT_HAS_COMPAT_STATUS_OKAY(st_stm32_usb)
 #define DT_DRV_COMPAT st_stm32_usb
 #define USB_IRQ_NAME  usb
-#if DT_INST_NODE_HAS_PROP(0, enable_pin_remap)
-#define USB_ENABLE_PIN_REMAP	__DEPRECATED_MACRO DT_INST_PROP(0, enable_pin_remap)
+#if DT_INST_PROP(0, enable_pin_remap)
+#define USB_ENABLE_PIN_REMAP	DT_INST_PROP(0, enable_pin_remap)
 #warning "Property deprecated in favor of property 'remap-pa11-pa12' from 'st-stm32-pinctrl'"
 #endif
 #endif
