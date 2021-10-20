4a668ab23af977018bb443e21137edd404b2814c
Andy Ross <andrew.j.ross@intel.com>
2021-10-19 18:33:31 -0700

    soc/intel_adsp: Unify host window register interface
    These registers are identical on all platforms, the only difference
    being that cAVS 1.5 places them at a different address.
    
    Create a devicetree node to track the register block, and replace the
    platform header code with a global API defined once (it works like the
    pre-existing shim struct).
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- soc/xtensa/intel_adsp/cavs_v15/include/soc/memory.h
+++ soc/xtensa/intel_adsp/cavs_v15/include/soc/memory.h
@@ -52,10 +52,6 @@
 #define IPC_DSP_SIZE		0x00000080
 #define IPC_DSP_BASE(x)		(0x00001200 + x * IPC_DSP_SIZE)
 
-/* SRAM window for HOST */
-#define HOST_WIN_SIZE		0x00000008
-#define HOST_WIN_BASE(x)	(0x00001580 + x * HOST_WIN_SIZE)
-
 /* HP SRAM windows */
 
 /* window 3 */
