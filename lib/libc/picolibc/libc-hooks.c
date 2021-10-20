0522467cbfa5ae6bbb2adc98f68d88e42721ddd8
Keith Packard <keithp@keithp.com>
2021-10-19 21:49:15 -0700

    lib/os: Replace cbvprintf and printfcb family when using picolibc
    Picolibc already provides the functionality offered by cbprintf, so
    there's no reason to use the larger and less functional version included
    in zephyr.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- lib/libc/picolibc/libc-hooks.c
+++ lib/libc/picolibc/libc-hooks.c
@@ -193,6 +193,32 @@ int z_impl_zephyr_write_stdout(const void *buffer, int nbytes)
 	return nbytes;
 }
 
+#include <sys/cbprintf.h>
+
+struct cb_bits {
+	FILE f;
+	cbprintf_cb out;
+	void	*ctx;
+};
+
+static int cbputc(char c, FILE *_s)
+{
+	struct cb_bits *s = (struct cb_bits *) _s;
+
+	(*s->out) (c, s->ctx);
+	return 0;
+}
+
+int cbvprintf(cbprintf_cb out, void *ctx, const char *fp, va_list ap)
+{
+	struct cb_bits	s = {
+		.f = FDEV_SETUP_STREAM(cbputc, NULL, NULL, _FDEV_SETUP_WRITE),
+		.out = out,
+		.ctx = ctx,
+	};
+	return vfprintf(&s.f, fp, ap);
+}
+
 #ifndef CONFIG_POSIX_API
 int _read(int fd, char *buf, int nbytes)
 {
