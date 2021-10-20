59c1af94c30a90283ac0360d911074834020d1cf
Christopher Friedt <chrisfriedt@gmail.com>
2021-10-20 12:59:02 -0400

    kconfig: compile: add option to disable noinit
    The `__noinit` option is useful for
    1. preserving sram across reboots
    2. reducing the size of `.bss` and speeding-up init
    
    However, some systems (such as those with ECC memory) can
    fault / hang when reading from previously unwritten memory.
    
    Default `CONFIG_NOINIT` to `y` to preserve existing behaviour.
    
    Fixes #39598
    
    Signed-off-by: Christopher Friedt <chrisfriedt@gmail.com>

--- include/linker/section_tags.h
+++ include/linker/section_tags.h
@@ -13,10 +13,16 @@
 
 #if !defined(_ASMLANGUAGE)
 
-#define __noinit		__in_section_unique(_NOINIT_SECTION_NAME)
-#define __noinit_named(name)	__in_section_unique_named(_NOINIT_SECTION_NAME, name)
-#define __irq_vector_table	Z_GENERIC_SECTION(_IRQ_VECTOR_TABLE_SECTION_NAME)
-#define __sw_isr_table		Z_GENERIC_SECTION(_SW_ISR_TABLE_SECTION_NAME)
+#if defined(CONFIG_NOINIT)
+#define __noinit __in_section_unique(_NOINIT_SECTION_NAME)
+#define __noinit_named(name) __in_section_unique_named(_NOINIT_SECTION_NAME, name)
+#else
+#define __noinit
+#define __noinit_named(name)
+#endif
+
+#define __irq_vector_table     Z_GENERIC_SECTION(_IRQ_VECTOR_TABLE_SECTION_NAME)
+#define __sw_isr_table         Z_GENERIC_SECTION(_SW_ISR_TABLE_SECTION_NAME)
 
 #if defined(CONFIG_ARM)
 #define __kinetis_flash_config_section __in_section_unique(_KINETIS_FLASH_CONFIG_SECTION_NAME)
