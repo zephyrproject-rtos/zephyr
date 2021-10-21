91551e31b409706464395c751250b92da49d4380
Emir <emir.emiroguz@gmail.com>
2021-10-21 15:08:58 +0200

    just comment out

--- samples/sensor/bq35100/src/main.c
+++ samples/sensor/bq35100/src/main.c
@@ -72,15 +72,15 @@ void main(void)
 		
 		sensor_channel_get(dev, SENSOR_CHAN_GAUGE_VOLTAGE, &val);
 		printf("Voltage:       %1.3f V\n", sensor_value_to_double(&val));
-
+	
 		sensor_channel_get(dev, SENSOR_CHAN_GAUGE_AVG_CURRENT, &val);
-		printf("Current:       %i     A\n", val.val1);
+		printf("Current:       %i   mA\n", val.val1);
 
 		sensor_channel_get(dev, SENSOR_CHAN_GAUGE_STATE_OF_HEALTH, &val);
-		printf("StateOfHealth: %i     %%\n", val.val1);
+		printf("StateOfHealth: %i   %%\n\n", val.val1);
 
-		sensor_channel_get(dev, SENSOR_CHAN_GAUGE_ACCUMULATED_CAPACITY, &val);
-		printk("Acc Capacity : %i     uAh\n\n", val.val1);
+		/*sensor_channel_get(dev, SENSOR_CHAN_GAUGE_ACCUMULATED_CAPACITY, &val);
+		printk("Acc Capacity : %i     uAh\n\n", val.val1);*/
 
 		k_sleep(K_MSEC(3000));
 	}
