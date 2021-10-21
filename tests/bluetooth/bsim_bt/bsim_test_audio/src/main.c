fa1a7a723e17d795fc7d6a4ac52cf3fe64e86842
Emil Gydesen <emil.gydesen@nordicsemi.no>
2021-10-21 18:03:25 +0200

    Bluetooth: Audio: Add CSIS BSIM tests
    Add a couple of babblesim tests for the CSIS server
    and client.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_audio/src/main.c
+++ tests/bluetooth/bsim_bt/bsim_test_audio/src/main.c
@@ -10,12 +10,16 @@ extern struct bst_test_list *test_vcs_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_vcs_client_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_mics_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_mics_client_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_csis_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_csis_client_install(struct bst_test_list *tests);
 
 bst_test_install_t test_installers[] = {
 	test_vcs_install,
 	test_vcs_client_install,
 	test_mics_install,
 	test_mics_client_install,
+	test_csis_install,
+	test_csis_client_install,
 	NULL
 };
 
