43fa7ce53ff008889b9a537d8ba00fcb5d8b5bde
Maureen Helm <maureen.helm@intel.com>
2021-10-21 13:29:33 -0500

    drivers: console: Refactor drivers to use shared init priority
    Refactors all of the console drivers to use a shared driver class
    initialization priority configuration, CONFIG_CONSOLE_INIT_PRIORITY, to
    allow configuring console drivers separately from other devices. This is
    similar to other driver classes like I2C and SPI.
    
    The default is set to CONFIG_KERNEL_INIT_PRIORITY_DEFAULT to preserve
    the existing default initialization priority for most drivers.
    
    The driver-specific option, CONFIG_NATIVE_POSIX_CONSOLE_INIT_PRIORITY,
    is left intact because the native posix console driver needs to
    initialize after the UART console driver when both drivers are enabled.
    
    Signed-off-by: Maureen Helm <maureen.helm@intel.com>

--- drivers/console/rtt_console.c
+++ drivers/console/rtt_console.c
@@ -88,4 +88,4 @@ static int rtt_console_init(const struct device *d)
 	return 0;
 }
 
-SYS_INIT(rtt_console_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+SYS_INIT(rtt_console_init, PRE_KERNEL_1, CONFIG_CONSOLE_INIT_PRIORITY);
