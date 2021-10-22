b5086f07304412c10591877ee3d61bc260e97dd3
Pieter De Gendt <pieter.degendt@basalte.be>
2021-10-22 10:32:38 +0200

    net: coap: Fix size1 for block1 transfers
    According to RFC-7959:
    When uploading with coap block1 requests the server may respond
    with a size1 option (together with the response code 4.13).
    This to indicate the maximum size the server is able and willing
    to handle.
    
    This commit changes the total_size in the current block context
    being handled to the optional size1 option value from the server.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- subsys/net/lib/coap/coap.c
+++ subsys/net/lib/coap/coap.c
@@ -1008,7 +1008,10 @@ static int update_control_block1(struct coap_block_context *ctx,
 	}
 
 	ctx->block_size = GET_BLOCK_SIZE(block);
-	ctx->total_size = size;
+
+	if (size >= 0) {
+		ctx->total_size = size;
+	}
 
 	return 0;
 }
@@ -1046,16 +1049,13 @@ int coap_update_from_block(const struct coap_packet *cpkt,
 	size1 = coap_get_option_int(cpkt, COAP_OPTION_SIZE1);
 	size2 = coap_get_option_int(cpkt, COAP_OPTION_SIZE2);
 
-	size1 = size1 == -ENOENT ? 0 : size1;
-	size2 = size2 == -ENOENT ? 0 : size2;
-
 	if (is_request(cpkt)) {
 		r = update_control_block2(ctx, block2, size2);
 		if (r) {
 			return r;
 		}
 
-		return update_descriptive_block(ctx, block1, size1);
+		return update_descriptive_block(ctx, block1, size1 == -ENOENT ? 0 : size1);
 	}
 
 	r = update_control_block1(ctx, block1, size1);
@@ -1063,7 +1063,7 @@ int coap_update_from_block(const struct coap_packet *cpkt,
 		return r;
 	}
 
-	return update_descriptive_block(ctx, block2, size2);
+	return update_descriptive_block(ctx, block2, size2 == -ENOENT ? 0 : size2);
 }
 
 size_t coap_next_block(const struct coap_packet *cpkt,
