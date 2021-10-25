2b63086e752d5f0dea58fef709f57ad1b9487aac
Carlo Caione <ccaione@baylibre.com>
2021-10-25 17:40:28 +0200

    ipc: ipc_service: Add support for multiple instances
    The IPC service code is currently assuming that only one IPC instance
    does exist and the user can use the IPC service API to interface with
    that singleton instance.
    
    This is a huge limitation and this patch is trying to fix this
    assumption introducing three major changes to the IPC service API:
    
    - All the IPC instances are now supposed to be instantiated as a struct
      device. A new test is introduced to be used as skeleton for all the
      other backends.
    
    - ipc_service_register_backend() is now removed (because multiple
      backends are now supported at the same time).
    
    - All the other ipc_service_*() functions are now taking a struct device
      pointer as parameter to specify on which instance the user is going to
      act and operate.
    
    In this patch the documentation is also extended to better clarify the
    terminology used.
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

--- include/ipc/ipc_service_backend.h
+++ include/ipc/ipc_service_backend.h
@@ -25,41 +25,30 @@ extern "C" {
  *  This structure is used for configuration backend during registration.
  */
 struct ipc_service_backend {
-	/** @brief Name of the IPC backend. */
-	const char *name;
-
 	/** @brief Pointer to the function that will be used to send data to the endpoint.
 	 *
-	 *  @param ept Registered endpoint.
+	 *  @param instance Instance pointer.
+	 *  @param token Backend-specific token.
 	 *  @param data Pointer to the buffer to send.
 	 *  @param len Number of bytes to send.
 	 *
 	 *  @retval Status code.
 	 */
-	int (*send)(struct ipc_ept *ept, const void *data, size_t len);
+	int (*send)(const struct device *instance, void *token,
+		    const void *data, size_t len);
 
 	/** @brief Pointer to the function that will be used to register endpoints.
 	 *
-	 *  @param ept Endpoint object.
+	 *  @param instance Instance to register the endpoint onto.
+	 *  @param token Backend-specific token.
 	 *  @param cfg Endpoint configuration.
 	 *
 	 *  @retval Status code.
 	 */
-	int (*register_endpoint)(struct ipc_ept **ept, const struct ipc_ept_cfg *cfg);
+	int (*register_endpoint)(const struct device *instance,
+				 void **token,
+				 const struct ipc_ept_cfg *cfg);
 };
-
-/** @brief IPC backend registration.
- *
- *  Registration must be done before using IPC Service.
- *
- *  @param backend Configuration of the backend.
- *
- *  @retval -EALREADY The backend is already registered.
- *  @retval -EINVAL The backend configuration is incorrect.
- *  @retval Zero on success.
- *
- */
-int ipc_service_register_backend(const struct ipc_service_backend *backend);
 
 /**
  * @}
