77698e1c70dbf1260f9d20845f9565e234f9c73e
Bartosz Bilas <bartosz.bilas@hotmail.com>
2021-10-27 20:33:15 +0200

    drivers: spi: use dedicated init priority
    A couple of SPI drivers use CONFIG_KERNEL_INIT_PRIORITY_DEVICE
    as init priority for driver initialization. Let's change
    it to the dedicated CONFIG_SPI_INIT_PRIORITY to make it
    compatible with other ones.
    
    Signed-off-by: Bartosz Bilas <bartosz.bilas@hotmail.com>

--- drivers/spi/spi_mcux_lpspi.c
+++ drivers/spi/spi_mcux_lpspi.c
@@ -306,7 +306,7 @@ static const struct spi_driver_api spi_mcux_driver_api = {
 	DEVICE_DT_INST_DEFINE(n, &spi_mcux_init, NULL,			\
 			    &spi_mcux_data_##n,				\
 			    &spi_mcux_config_##n, POST_KERNEL,		\
-			    CONFIG_KERNEL_INIT_PRIORITY_DEVICE,		\
+			    CONFIG_SPI_INIT_PRIORITY,			\
 			    &spi_mcux_driver_api);			\
 									\
 	static void spi_mcux_config_func_##n(const struct device *dev)	\
