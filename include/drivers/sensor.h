efffca594ab8db3ea941acdb81416d118bfe9bfb
AzamLukman <73987401+AzamLukman@users.noreply.github.com>
2021-10-29 22:53:08 +0800

    Update sensor.h

--- include/drivers/sensor.h
+++ include/drivers/sensor.h
@@ -287,8 +287,6 @@ enum sensor_attribute {
 	SENSOR_ATTR_LOWER_THRESH,
 	/** Upper threshold for trigger. */
 	SENSOR_ATTR_UPPER_THRESH,
-	/** Threshold for any-motion (slope) trigger. */
-	SENSOR_ATTR_SLOPE_TH,
 	/**
 	* Duration for which the freefall value need to be
 	* outside the threshold for trigger to fire
