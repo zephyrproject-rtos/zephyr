18519ffe8d613eaed764570b0890d81af71b10d4
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2021-11-02 16:20:43 +0100

    tests: use common PM action callback naming
    The PM callback is no longer referenced as "pm_control" but
    "pm_action_cb", so reflect this new naming on the callbacks.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- tests/benchmarks/footprints/src/pm_device.c
+++ tests/benchmarks/footprints/src/pm_device.c
@@ -21,15 +21,15 @@ static int dummy_init(const struct device *dev)
 	return 0;
 }
 
-static int dummy_device_pm_ctrl(const struct device *dev,
-				enum pm_device_action action)
+static int dummy_device_pm_action(const struct device *dev,
+				  enum pm_device_action action)
 {
 	return 0;
 }
 
 /* Define a driver with and without power management enabled */
 DEVICE_DEFINE(dummy_pm_driver, DUMMY_PM_DRIVER_NAME, &dummy_init,
-		    dummy_device_pm_ctrl, NULL, NULL, APPLICATION,
+		    dummy_device_pm_action, NULL, NULL, APPLICATION,
 		    CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, NULL);
 
 DEVICE_DEFINE(dummy_driver, DUMMY_DRIVER_NAME, &dummy_init,
