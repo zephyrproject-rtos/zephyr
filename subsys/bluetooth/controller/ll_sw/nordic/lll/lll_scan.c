7a6666a0e7d6cb28197079e5f7de08f2a3694bf6
Morten Priess <mtpr@oticon.com>
2021-11-04 16:37:19 +0100

    Bluetooth: controller: Introduce preferred central conn spacing config
    Adds configuration BT_CTLR_SCHED_ADVANCED_CENTRAL_CONN_SPACING to enable
    per-application preferred central connection spacing. This is an
    extension of feature BT_CTLR_SCHED_ADVANCED, which when enabled, adds
    user defined spacing between existing, and new central initiated
    connections.
    
    This is crucial for ensuring that centrals acting as ACL connection
    based multi-channel streaming sources do not get overlapping
    connections, and that spacing allows full frame transfer without
    scheduling collision.
    
    Due to the simple, compile-time configuration, use is limited to
    specific applications in which the specified spacing is required.
    
    Signed-off-by: Morten Priess <mtpr@oticon.com>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan.c
@@ -50,6 +50,12 @@
 /* Maximum primary Advertising Radio Channels to scan */
 #define ADV_CHAN_MAX 3U
 
+#if defined(CONFIG_BT_CENTRAL) && defined(CONFIG_BT_CTLR_SCHED_ADVANCED)
+#define CONN_SPACING CONFIG_BT_CTLR_SCHED_ADVANCED_CENTRAL_CONN_SPACING
+#else
+#define CONN_SPACING 0U
+#endif /* CONFIG_BT_CENTRAL && CONFIG_BT_CTLR_SCHED_ADVANCED */
+
 static int init_reset(void);
 static int prepare_cb(struct lll_prepare_param *p);
 static int resume_prepare_cb(struct lll_prepare_param *p);
@@ -287,7 +293,8 @@ void lll_scan_prepare_connect_req(struct lll_scan *lll, struct pdu_adv *pdu_tx,
 		*conn_space_us = conn_offset_us;
 		pdu_tx->connect_ind.win_offset = sys_cpu_to_le16(0);
 	} else {
-		uint32_t win_offset_us = lll->conn_win_offset_us;
+		uint32_t win_offset_us = lll->conn_win_offset_us +
+					 CONN_SPACING;
 
 		while ((win_offset_us & ((uint32_t)1 << 31)) ||
 		       (win_offset_us < conn_offset_us)) {
