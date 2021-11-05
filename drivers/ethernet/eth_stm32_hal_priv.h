87438e4587b7ed2a884b823481bbd1e86b8e425b
Erwan Gouriou <erwan.gouriou@linaro.org>
2021-11-05 16:43:04 +0100

    drivers/ethernet: stm32_hal: use new pinctrl API
    Use the new pinctrl API to configure pins.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- drivers/ethernet/eth_stm32_hal_priv.h
+++ drivers/ethernet/eth_stm32_hal_priv.h
@@ -29,8 +29,7 @@ struct eth_stm32_hal_dev_cfg {
 #if !defined(CONFIG_SOC_SERIES_STM32H7X)
 	struct stm32_pclken pclken_ptp;
 #endif /* !defined(CONFIG_SOC_SERIES_STM32H7X) */
-	const struct soc_gpio_pinctrl *pinctrl;
-	size_t pinctrl_len;
+	const struct pinctrl_dev_config *pcfg;
 };
 
 /* Device run time data */
