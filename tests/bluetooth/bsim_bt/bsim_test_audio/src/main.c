64f177c560ed2daa686b1a4a4d04f2cdb8d475ad
Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>
2021-11-09 15:36:17 +0100

    Bluetooth: Audio: Media control BabbleSim tests
    This commit add the BabbleSim tests for the media control client, the
    media control service and the media proxy.  This is a part of the
    upmerge of the le-audio media control files.
    
    These tests have been developed and maintained along with the media
    control functionality.  The tests are believed to cover all
    implemented media control functionality.
    
    The commit is a pure copy of the files and content in the
    topic-le-audio branch, with the exception that some include paths have
    been modified to correspond to new locations of files and that the
    tests have been guarded with ifdefs.
    
    Signed-off-by: Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>
    
    test guards

--- tests/bluetooth/bsim_bt/bsim_test_audio/src/main.c
+++ tests/bluetooth/bsim_bt/bsim_test_audio/src/main.c
@@ -12,6 +12,9 @@ extern struct bst_test_list *test_mics_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_mics_client_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_csis_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_csis_client_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_mcs_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_mcc_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_media_controller_install(struct bst_test_list *tests);
 
 bst_test_install_t test_installers[] = {
 	test_vcs_install,
@@ -20,6 +23,9 @@ bst_test_install_t test_installers[] = {
 	test_mics_client_install,
 	test_csis_install,
 	test_csis_client_install,
+	test_mcs_install,
+	test_mcc_install,
+	test_media_controller_install,
 	NULL
 };
 
