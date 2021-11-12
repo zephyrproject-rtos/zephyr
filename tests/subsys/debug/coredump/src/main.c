b30423a242eef8899124f5cf74edd48ad7886e4c
Mark Holden <mholden@fb.com>
2021-11-12 13:21:43 -0800

    coredump: add support for RISC-V
    This adds the necessary bits in arch code, and Python scripts
    to enable coredump support for RISC-V
    
    Signed-off-by: Mark Holden <mholden@fb.com>

--- tests/subsys/debug/coredump/src/main.c
+++ tests/subsys/debug/coredump/src/main.c
@@ -6,10 +6,18 @@
 
 #include <zephyr.h>
 #include <sys/printk.h>
+#include <debug/coredump.h>
 
 void func_3(uint32_t *addr)
 {
-#if !defined(CONFIG_CPU_CORTEX_M)
+#if defined(CONFIG_BOARD_M2GL025_MIV) || defined(CONFIG_BOARD_HIFIVE1)
+	ARG_UNUSED(addr);
+	/* Call coredump() directly so Renode doesn't pause execution */
+	z_arch_esf_t esf;
+	struct k_thread kthread;
+
+	coredump(1, &esf, &kthread);
+#elif !defined(CONFIG_CPU_CORTEX_M)
 	/* For null pointer reference */
 	*addr = 0;
 #else
