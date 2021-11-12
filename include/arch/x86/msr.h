9a3a81395431f6751543330df43bb22e3a29c3ff
Abramo Bagnara <abramo.bagnara@bugseng.com>
2021-11-12 16:45:12 +0100

    coding guidelines: comply with MISRA C:2012 Rule 7.2
    MISRA C:2012 Rule 7.2 (A `u' or `U' suffix shall be applied to all
    integer constants that are represented in an unsigned type)
    
    Added missing `U' suffixes in constants that are involved in the
    analyzed build, plus a few more not to introduce inconsistencies
    with respect to nearby constants that are either unused in the
    build (but implicitly unsigned) or are used and are immediately
    converted to unsigned.
    
    Signed-off-by: Abramo Bagnara <abramo.bagnara@bugseng.com>

--- include/arch/x86/msr.h
+++ include/arch/x86/msr.h
@@ -24,7 +24,7 @@
 
 #define X86_X2APIC_BASE_MSR		0x00000800 /* .. thru 0x00000BFF */
 
-#define X86_EFER_MSR			0xC0000080
+#define X86_EFER_MSR			0xC0000080U
 #define X86_EFER_MSR_SCE		BIT(0)
 #define X86_EFER_MSR_LME		BIT(8)
 #define X86_EFER_MSR_NXE		BIT(11)
@@ -33,17 +33,17 @@
  *      47:32  Kernel CS (SS = CS+8)
  *      63:48  User CS (SS = CS+8)
  */
-#define X86_STAR_MSR			0xC0000081
+#define X86_STAR_MSR			0xC0000081U
 
 /* Location for system call entry point */
-#define X86_LSTAR_MSR			0xC0000082
+#define X86_LSTAR_MSR			0xC0000082U
 
 /* Low 32 bits in this MSR are the SYSCALL mask applied to EFLAGS */
-#define X86_FMASK_MSR			0xC0000084
+#define X86_FMASK_MSR			0xC0000084U
 
-#define X86_FS_BASE			0xC0000100
-#define X86_GS_BASE			0xC0000101
-#define X86_KERNEL_GS_BASE		0xC0000102
+#define X86_FS_BASE			0xC0000100U
+#define X86_GS_BASE			0xC0000101U
+#define X86_KERNEL_GS_BASE		0xC0000102U
 
 #ifndef _ASMLANGUAGE
 #ifdef __cplusplus
@@ -58,7 +58,7 @@ extern "C" {
 static inline void z_x86_msr_write(unsigned int msr, uint64_t data)
 {
 	uint32_t high = data >> 32;
-	uint32_t low = data & 0xFFFFFFFF;
+	uint32_t low = data & 0xFFFFFFFFU;
 
 	__asm__ volatile ("wrmsr" : : "c"(msr), "a"(low), "d"(high));
 }
