a92896be3a221f14d4e4376d5399948110b3e2b6
Johann Fischer <johann.fischer@nordicsemi.no>
2021-11-12 16:32:54 +0100

    tests: bluetooth: shell: use generic CDC ACM shield
    Remove CDC ACM UART devicetree and config overlay, and explicit
    USB support initialization in favor of generic CDC ACM shield.
    For shell support via USB CDC ACM, this example is to be built
    as follows:
    
    west build -b nrf52840dongle_nrf52840
      tests/bluetooth/shell -- -DSHIELD=cdc_acm_shell
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- tests/bluetooth/shell/src/main.c
+++ tests/bluetooth/shell/src/main.c
@@ -123,21 +123,6 @@ static void hrs_notify(void)
 
 void main(void)
 {
-#if DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_shell_uart), zephyr_cdc_acm_uart)
-	const struct device *dev;
-	uint32_t dtr = 0;
-
-	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
-	if (!device_is_ready(dev) || usb_enable(NULL)) {
-		return;
-	}
-
-	while (!dtr) {
-		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-		k_sleep(K_MSEC(100));
-	}
-#endif
-
 	printk("Type \"help\" for supported commands.");
 	printk("Before any Bluetooth commands you must `bt init` to initialize"
 	       " the stack.\n");
