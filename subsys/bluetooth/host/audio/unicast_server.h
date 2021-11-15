fbdeb7f3d5bc44c96441fd2fd241320c3b591426
Emil Gydesen <emil.gydesen@nordicsemi.no>
2021-11-15 13:21:23 +0100

    Bluetooth: Audio: Move unicast server callback to own file
    Add new unicast_server.c/h files that contains the unicast
    server callback structure.
    
    This change is used so that the callback struct
    is placed in a more semantically name file,
    as well as making it possible to use the callback
    from PACS as well as ASCS.
    
    This also renames the variable to better describe the
    context of the variable.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

---
+++ subsys/bluetooth/host/audio/unicast_server.h
@@ -0,0 +1,11 @@
+/*  Bluetooth Audio Unicast Server */
+
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <bluetooth/audio.h>
+
+extern const struct bt_audio_unicast_server_cb *unicast_server_cb;
