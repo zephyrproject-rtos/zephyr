35b761e3cae423a41dc021a2dee556a6bad780e9
Emil Gydesen <emil.gydesen@nordicsemi.no>
2021-11-22 11:45:01 +0100

    Bluetooth: Audio: Add bap prefix for functions in bap.c
    Add a bap_ prefix for all static functions in bap.c
    Add a bt_bap_prefeix for all non-static functions in bap.c
    
    This is to primarily rename the moved functions from
    endpoint.c, but also to make sure that it is
    easy to determine where the function is located.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/host/audio/bap_internal.h
+++ subsys/bluetooth/host/audio/bap_internal.h
@@ -22,3 +22,19 @@ int bt_bap_start(struct bt_audio_stream *stream);
 int bt_bap_stop(struct bt_audio_stream *stream);
 
 int bt_bap_release(struct bt_audio_stream *stream);
+
+bool bt_bap_ep_is_src(const struct bt_audio_ep *ep);
+
+void bt_bap_ep_set_state(struct bt_audio_ep *ep, uint8_t state);
+
+struct net_buf_simple *bt_bap_ep_create_pdu(uint8_t op);
+
+int bt_bap_ep_qos(struct bt_audio_ep *ep, struct net_buf_simple *buf,
+		  struct bt_codec_qos *qos);
+
+int bt_bap_ep_send(struct bt_conn *conn, struct bt_audio_ep *ep,
+		   struct net_buf_simple *buf);
+
+void bt_bap_ep_attach(struct bt_audio_ep *ep, struct bt_audio_stream *stream);
+
+void bt_bap_ep_detach(struct bt_audio_ep *ep, struct bt_audio_stream *stream);
