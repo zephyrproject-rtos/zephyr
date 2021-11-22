d60d47d2bdeb2c09e59d0022d7a1802f654bb755
Pieter De Gendt <pieter.degendt@basalte.be>
2021-11-22 12:02:12 +0100

    tests: drivers: counter: Generic seconds counter test
    This commit reworks the CMOS test to a generic seconds
    counter test.
    
    And it adds a test case for the iMX RT1064 SNVS RTC.
    
    Tested with mimxrt1064_evk
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- tests/drivers/counter/counter_seconds/src/main.c
+++ tests/drivers/counter/counter_seconds/src/main.c
@@ -11,26 +11,32 @@
  * Basic test to ensure that the clock is ticking at roughly 1Hz.
  */
 
+#ifdef CONFIG_COUNTER_CMOS
+#define DEVICE_LABEL "CMOS"
+#else
+#define DEVICE_LABEL DT_LABEL(DT_ALIAS(rtc_0))
+#endif
+
 #define DELAY_MS 1200	/* pause 1.2 seconds should always pass */
 #define MIN_BOUND 1	/* counter must report at least MIN_BOUND .. */
 #define MAX_BOUND 2	/* .. but at most MAX_BOUND seconds elapsed */
 
-void test_cmos_rate(void)
+void test_seconds_rate(void)
 {
-	const struct device *cmos;
+	const struct device *dev;
 	uint32_t start, elapsed;
 	int err;
 
-	cmos = device_get_binding("CMOS");
-	zassert_true(cmos != NULL, "can't find CMOS counter device");
+	dev = device_get_binding(DEVICE_LABEL);
+	zassert_true(dev != NULL, "can't find counter device");
 
-	err = counter_get_value(cmos, &start);
-	zassert_true(err == 0, "failed to read CMOS counter device");
+	err = counter_get_value(dev, &start);
+	zassert_true(err == 0, "failed to read counter device");
 
 	k_msleep(DELAY_MS);
 
-	err = counter_get_value(cmos, &elapsed);
-	zassert_true(err == 0, "failed to read CMOS counter device");
+	err = counter_get_value(dev, &elapsed);
+	zassert_true(err == 0, "failed to read counter device");
 	elapsed -= start;
 
 	zassert_true(elapsed >= MIN_BOUND, "busted minimum bound");
@@ -39,6 +45,6 @@ void test_cmos_rate(void)
 
 void test_main(void)
 {
-	ztest_test_suite(test_cmos_counter, ztest_unit_test(test_cmos_rate));
-	ztest_run_test_suite(test_cmos_counter);
+	ztest_test_suite(test_seconds_counter, ztest_unit_test(test_seconds_rate));
+	ztest_run_test_suite(test_seconds_counter);
 }
