30098c881f70f01a456b23a7a3d98ada4cbec0b7
Aleksander Wasaznik <aleksander.wasaznik@nordicsemi.no>
2021-11-22 13:25:06 +0100

    Bluetooth: Host: Detect deadlock in bt_att_req_alloc
    `gatt_req_alloc` will wait until a `req` is free (or until timeout).
    `req`s are freed on the BT RX thread in calls into bt_att_recv.
    
    When `gatt_req_alloc` called on the BT RX thread itself when there are
    no free `req`s, it will block the BT RX thread and deadlock. The
    deadlock lasts until timeout.
    
    This change detects this condition and returns the failure early.
    
    Fixes https://github.com/zephyrproject-rtos/zephyr/issues/39624 where
    if `bt_gatt_write` is called from BT RX thread (as can happen if it is
    called from a bluetooth callback), the BT RX thread can be blocked and
    prevented from processing the request responses and unblocking itself.
    This was the cause of a soft 30s deadlock until gatt_req_alloc timeouts.
    
    Signed-off-by: Aleksander Wasaznik <aleksander.wasaznik@nordicsemi.no>

--- subsys/bluetooth/host/hci_core.h
+++ subsys/bluetooth/host/hci_core.h
@@ -470,3 +470,8 @@ void bt_hci_synchronous_conn_complete(struct net_buf *buf);
 
 void bt_hci_le_df_connection_iq_report(struct net_buf *buf);
 void bt_hci_le_df_cte_req_failed(struct net_buf *buf);
+
+/** First thread that called bt_recv. NULL if not yet called.
+ *  Updated non-atomically; may be used only to compare to current thread id.
+ */
+extern k_tid_t bt_recv_thread_id;
