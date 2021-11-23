53adcece95c6a9a40abcbb18d229173d55022fcb
Erwan Gouriou <erwan.gouriou@linaro.org>
2021-11-23 14:38:24 +0100

    include/dt-bindings/pinctrl: stm32: Remove c pinmux api leftover
    Following definitive transition to dts pinmux then pinctrl api
    remove C pinmux api leftover definitions.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- include/dt-bindings/pinctrl/stm32f1-pinctrl.h
+++ include/dt-bindings/pinctrl/stm32f1-pinctrl.h
@@ -71,12 +71,6 @@
  * Applicable to STM32F1 series
  */
 
-/* Alternate functions */
-/* STM32F1 Pinmux doesn't use explicit alternate functions */
-/* These are kept for compatibility with other STM32 pinmux */
-#define STM32_AFR_MASK			0
-#define STM32_AFR_SHIFT			0
-
 /* Port Mode */
 #define STM32_MODE_INPUT		(0x0<<STM32_MODE_INOUT_SHIFT)
 #define STM32_MODE_OUTPUT		(0x1<<STM32_MODE_INOUT_SHIFT)
@@ -113,50 +107,5 @@
 #define STM32_PUPD_PULL_DOWN		(0x2<<STM32_PUPD_SHIFT)
 #define STM32_PUPD_MASK			0x3
 #define STM32_PUPD_SHIFT		7
-
-/* Alternate defines */
-/* IO pin functions are mostly common across STM32 devices. Notable
- * exception is STM32F1 as these MCUs do not have registers for
- * configuration of pin's alternate function. The configuration is
- * done implicitly by setting specific mode and config in MODE and CNF
- * registers for particular pin.
- */
-#define STM32_ALTERNATE			(STM32_MODE_OUTPUT | STM32_CNF_ALT_FUNC)
-
-#define STM32_PIN_USART_TX		(STM32_ALTERNATE | STM32_CNF_PUSH_PULL)
-#define STM32_PIN_USART_RX		(STM32_MODE_INPUT | STM32_CNF_IN_FLOAT)
-#define STM32_PIN_I2C			(STM32_ALTERNATE | STM32_CNF_OPEN_DRAIN)
-#define STM32_PIN_PWM			(STM32_ALTERNATE | STM32_CNF_PUSH_PULL)
-#define STM32_PIN_SPI_MASTER_SCK	(STM32_ALTERNATE | STM32_CNF_PUSH_PULL)
-#define STM32_PIN_SPI_SLAVE_SCK		(STM32_MODE_INPUT | STM32_CNF_IN_FLOAT)
-#define STM32_PIN_SPI_MASTER_MOSI	(STM32_ALTERNATE | STM32_CNF_PUSH_PULL)
-#define STM32_PIN_SPI_SLAVE_MOSI	(STM32_MODE_INPUT | STM32_CNF_IN_FLOAT)
-#define STM32_PIN_SPI_MASTER_MISO	(STM32_MODE_INPUT | STM32_CNF_IN_FLOAT)
-#define STM32_PIN_SPI_SLAVE_MISO	(STM32_ALTERNATE | STM32_CNF_PUSH_PULL)
-#define STM32_PIN_CAN_TX		(STM32_ALTERNATE | STM32_CNF_PUSH_PULL)
-#define STM32_PIN_CAN_RX		(STM32_MODE_INPUT  | STM32_PUPD_PULL_UP)
-
-/*
- * Reference manual (RM0008)
- * Section 25.3.1: Slave select (NSS) pin management
- *
- * Hardware NSS management:
- * - NSS output disabled: allows multimaster capability for devices operating
- *   in master mode.
- * - NSS output enabled: used only when the device operates in master mode.
- *
- * Software NSS management:
- * - External NSS pin remains free for other application uses.
- *
- */
-
-/* Hardware master NSS output disabled */
-#define STM32_PIN_SPI_MASTER_NSS	(STM32_MODE_INPUT | STM32_CNF_IN_FLOAT)
-/* Hardware master NSS output enabled */
-#define STM32_PIN_SPI_MASTER_NSS_OE	(STM32_MODE_OUTPUT | \
-						STM32_CNF_ALT_FUNC | \
-						STM32_CNF_PUSH_PULL)
-#define STM32_PIN_SPI_SLAVE_NSS		(STM32_MODE_INPUT | STM32_CNF_IN_FLOAT)
-#define STM32_PIN_USB			(STM32_MODE_INPUT | STM32_CNF_IN_PUPD)
 
 #endif	/* ZEPHYR_STM32_PINCTRLF1_H_ */
