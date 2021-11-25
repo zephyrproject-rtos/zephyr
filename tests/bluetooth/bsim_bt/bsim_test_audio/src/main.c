e9ee83b3c3fff631ed5b231f82690723c291b4fc
Emil Gydesen <emil.gydesen@nordicsemi.no>
2021-11-25 15:45:34 +0100

    Bluetooth: Audio: Add BASS BSIM tests
    Add the BASS babbleism tests.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_audio/src/main.c
+++ tests/bluetooth/bsim_bt/bsim_test_audio/src/main.c
@@ -19,6 +19,9 @@ extern struct bst_test_list *test_unicast_client_install(struct bst_test_list *t
 extern struct bst_test_list *test_unicast_server_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_broadcast_source_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_broadcast_sink_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_bass_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_bass_client_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_bass_broadcaster_install(struct bst_test_list *tests);
 
 bst_test_install_t test_installers[] = {
 	test_vcs_install,
@@ -34,6 +37,9 @@ bst_test_install_t test_installers[] = {
 	test_unicast_server_install,
 	test_broadcast_source_install,
 	test_broadcast_sink_install,
+	test_bass_install,
+	test_bass_client_install,
+	test_bass_broadcaster_install,
 	NULL
 };
 
