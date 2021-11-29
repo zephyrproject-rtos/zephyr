c761dd86215059ba24f5a8c8583307ba833c7729
Paul He <pawpawhe@gmail.com>
2021-11-30 00:16:12 +0800

    samples: prod_consumer: use app_shared.h instead of main.h
    Normally main.c file doesn't have a header file, because it doesn't
    need to declare any interfaces to other modules.
    
    In this sample, it's better to put the shared variables in one place,
    and provide the interfaces to other modules, such as app_a, app_b,
    and main.
    
    Signed-off-by: Paul He <pawpawhe@gmail.com>

--- samples/userspace/prod_consumer/src/app_shared.h
+++ samples/userspace/prod_consumer/src/app_shared.h
@@ -4,18 +4,23 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef APP_SHARED_H
+#define APP_SHARED_H
 
 #include <kernel.h>
 #include <app_memory/app_memdomain.h>
 #include <sys/sys_heap.h>
 
+#include "sample_driver.h"
+
+#define BLK_SIZE (SAMPLE_DRIVER_MSG_SIZE + sizeof(void *))
+
+#define HEAP_BYTES (BLK_SIZE * 16)
+
 extern struct k_mem_partition shared_partition;
-#define SHARED_DATA	K_APP_DMEM(shared_partition)
-#define SHARED_BSS	K_APP_BMEM(shared_partition)
 
 extern struct sys_heap shared_pool;
+extern uint8_t shared_pool_mem[HEAP_BYTES];
 extern struct k_queue shared_queue_incoming;
 extern struct k_queue shared_queue_outgoing;
 
