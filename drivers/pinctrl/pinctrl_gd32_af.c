b009cc2e1850b206b03581110555be9c36dd2913
HaiLong Yang <cameledyang@pm.me>
2021-11-29 18:31:43 +0800

    drivers: pinctrl: add gd32f350 gpio opspeed check
    Add compile time check for gd32f350 gpio opspeed.
    
    Signed-off-by: HaiLong Yang <cameledyang@pm.me>

--- drivers/pinctrl/pinctrl_gd32_af.c
+++ drivers/pinctrl/pinctrl_gd32_af.c
@@ -5,6 +5,7 @@
  */
 
 #include <drivers/pinctrl.h>
+#include <soc.h>
 
 BUILD_ASSERT((GD32_PUPD_NONE == GPIO_PUPD_NONE) &&
 	     (GD32_PUPD_PULLUP == GPIO_PUPD_PULLUP) &&
@@ -16,9 +17,15 @@ BUILD_ASSERT((GD32_OTYPE_PP == GPIO_OTYPE_PP) &&
 	     "pinctrl output type definitions != HAL definitions");
 
 BUILD_ASSERT((GD32_OSPEED_2MHZ == GPIO_OSPEED_2MHZ) &&
+#ifdef CONFIG_SOC_SERIES_GD32F3X0
+	     (GD32_OSPEED_10MHZ == GPIO_OSPEED_10MHZ) &&
+	     (GD32_OSPEED_50MHZ == GPIO_OSPEED_50MHZ) &&
+#else
 	     (GD32_OSPEED_25MHZ == GPIO_OSPEED_25MHZ) &&
 	     (GD32_OSPEED_50MHZ == GPIO_OSPEED_50MHZ) &&
-	     (GD32_OSPEED_200MHZ == GPIO_OSPEED_200MHZ),
+	     (GD32_OSPEED_200MHZ == GPIO_OSPEED_200MHZ) &&
+#endif /* CONFIG_SOC_SERIES_GD32F3X0 */
+	     1U,
 	     "pinctrl output speed definitions != HAL definitions");
 
 /** Utility macro that expands to the GPIO port address if it exists */
