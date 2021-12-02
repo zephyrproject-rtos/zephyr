f00a0a9e0669519d0cc7a2f729e1955fc66ddc36
Sylvio Alves <sylvio.alves@espressif.com>
2021-12-02 16:36:04 -0300

    drivers: pinmux: esp32: use rtcio to control pull up/down
    Set pinmux driver to use RTCIO interface for to handle
    dedicated IOs pull-up/down configuration.
    Without this, some GPIOS won't have pull-up enabled
    properly.
    
    Also, current implementation automatically disables
    output pin when input pin is set. It also performs the
    oppositte. This PR changes it only to enable what is
    requested, either input or output.
    
    Signed-off-by: Sylvio Alves <sylvio.alves@espressif.com>

--- drivers/pinmux/pinmux_esp32.c
+++ drivers/pinmux/pinmux_esp32.c
@@ -11,10 +11,24 @@
 #include <soc/soc.h>
 #include <hal/gpio_types.h>
 #include <hal/gpio_ll.h>
+#include <hal/rtc_io_hal.h>
 #include <soc.h>
 
 #include <errno.h>
 #include <drivers/pinmux.h>
+
+#ifndef SOC_GPIO_SUPPORT_RTC_INDEPENDENT
+#define SOC_GPIO_SUPPORT_RTC_INDEPENDENT 0
+#endif
+
+static inline bool rtc_gpio_is_valid_gpio(uint32_t gpio_num)
+{
+#if SOC_RTCIO_INPUT_OUTPUT_SUPPORTED
+	return (gpio_num < SOC_GPIO_PIN_COUNT && rtc_io_num_map[gpio_num] >= 0);
+#else
+	return false;
+#endif
+}
 
 static int pinmux_set(const struct device *dev, uint32_t pin, uint32_t func)
 {
@@ -48,11 +62,26 @@ static int pinmux_pullup(const struct device *dev, uint32_t pin, uint8_t func)
 
 	switch (func) {
 	case PINMUX_PULLUP_DISABLE:
-		gpio_ll_pullup_dis(&GPIO, pin);
+		if (!rtc_gpio_is_valid_gpio(pin) || SOC_GPIO_SUPPORT_RTC_INDEPENDENT) {
+			gpio_ll_pullup_dis(&GPIO, pin);
+			gpio_ll_pulldown_en(&GPIO, pin);
+		} else {
+#if SOC_RTCIO_INPUT_OUTPUT_SUPPORTED
+			rtcio_hal_pullup_disable(rtc_io_num_map[pin]);
+			rtcio_hal_pulldown_enable(rtc_io_num_map[pin]);
+#endif
+		}
 		break;
 	case PINMUX_PULLUP_ENABLE:
-		gpio_ll_pulldown_dis(&GPIO, pin);
-		gpio_ll_pullup_en(&GPIO, pin);
+		if (!rtc_gpio_is_valid_gpio(pin) || SOC_GPIO_SUPPORT_RTC_INDEPENDENT) {
+			gpio_ll_pulldown_dis(&GPIO, pin);
+			gpio_ll_pullup_en(&GPIO, pin);
+		} else {
+#if SOC_RTCIO_INPUT_OUTPUT_SUPPORTED
+			rtcio_hal_pulldown_disable(rtc_io_num_map[pin]);
+			rtcio_hal_pullup_enable(rtc_io_num_map[pin]);
+#endif
+		}
 		break;
 	default:
 		return -EINVAL;
@@ -67,11 +96,9 @@ static int pinmux_input(const struct device *dev, uint32_t pin, uint8_t func)
 
 	switch (func) {
 	case PINMUX_INPUT_ENABLED:
-		gpio_ll_output_disable(&GPIO, pin);
 		gpio_ll_input_enable(&GPIO, pin);
 		break;
 	case PINMUX_OUTPUT_ENABLED:
-		gpio_ll_input_disable(&GPIO, pin);
 		gpio_ll_output_enable(&GPIO, pin);
 		esp_rom_gpio_matrix_out(pin, SIG_GPIO_OUT_IDX, false, false);
 		break;
