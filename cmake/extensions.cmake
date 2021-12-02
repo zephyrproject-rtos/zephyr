zephyrproject-rtos/zephyr

commit 525fa76f4dd13d42549261c9fc846a150ee291bc

boards: xtensa: Use a CMake variable to set the rimage target name
Removes hardcoded logic in the west signing script that translates
Zephyr board names to rimage target names. Instead, use a cached CMake
variable set at the board level to define its respective rimage target
name. This eliminates the need to modify the west signing script when
new SOF-supported boards are introduced to Zephyr.

Signed-off-by: Maureen Helm <maureen.helm@intel.com>

cmake/extensions.cmake
@@ -825,6 +825,11 @@ function(board_finalize_runner_args runner)
   set_property(GLOBAL APPEND PROPERTY ZEPHYR_RUNNERS ${runner})
 endfunction()
 
+function(board_set_rimage_target target)
+  zephyr_check_cache(RIMAGE_TARGET)
+  set(RIMAGE_TARGET ${target} CACHE STRING "rimage target")
+endfunction()
+
 # Zephyr board revision:
 #
 # This section provides a function for revision checking.

boards/xtensa/intel_adsp_cavs15/board.cmake
@@ -2,3 +2,5 @@
 
 board_set_flasher_ifnset(misc-flasher)
 board_finalize_runner_args(misc-flasher)
+
+board_set_rimage_target(apl)

boards/xtensa/intel_adsp_cavs18/board.cmake
@@ -2,3 +2,5 @@
 
 board_set_flasher_ifnset(misc-flasher)
 board_finalize_runner_args(misc-flasher)
+
+board_set_rimage_target(cnl)

boards/xtensa/intel_adsp_cavs20/board.cmake
@@ -2,3 +2,5 @@
 
 board_set_flasher_ifnset(misc-flasher)
 board_finalize_runner_args(misc-flasher)
+
+board_set_rimage_target(icl)

boards/xtensa/intel_adsp_cavs25/board.cmake
@@ -2,3 +2,5 @@
 
 board_set_flasher_ifnset(misc-flasher)
 board_finalize_runner_args(misc-flasher)
+
+board_set_rimage_target(tgl)

boards/xtensa/nxp_adsp_imx8/board.cmake
@@ -2,3 +2,5 @@
 
 board_set_flasher_ifnset(misc-flasher)
 board_finalize_runner_args(misc-flasher)
+
+board_set_rimage_target(imx8)

boards/xtensa/nxp_adsp_imx8m/board.cmake
@@ -2,3 +2,5 @@
 
 board_set_flasher_ifnset(misc-flasher)
 board_finalize_runner_args(misc-flasher)
+
+board_set_rimage_target(imx8m)

boards/xtensa/nxp_adsp_imx8x/board.cmake
@@ -2,3 +2,5 @@
 
 board_set_flasher_ifnset(misc-flasher)
 board_finalize_runner_args(misc-flasher)
+
+board_set_rimage_target(imx8)

commit 381f447dd4b79f1ddcdc6285589ea6db97f98f1f

cmake: Fix invalid warning related to rimage
Fixes an issue on SOF-supported boards where the build system
incorrectly believed that the rimage target was changing when rerunning
CMake, even though the user did not change any configuration. This issue
was introduced in commit 525fa76f4dd13d42549261c9fc846a150ee291bc.

Create initial build dir:
$ west build --board intel_adsp_cavs18 samples/subsys/audio/sof

Re-run CMake with no arguments in that build dir:
$ cd build
$ cmake .

CMake Warning at /home/mhelm/zephyrproject/zephyr/cmake/extensions.cmake:2403 (message):
  The build directory must be cleaned pristinely when changing rimage target,

  Current value="", Ignored value="cnl"
Call Stack (most recent call first):
  /home/mhelm/zephyrproject/zephyr/cmake/extensions.cmake:829 (zephyr_check_cache)
  /home/mhelm/zephyrproject/zephyr/boards/xtensa/intel_adsp_cavs18/board.cmake:6 (board_set_rimage_target)
  /home/mhelm/zephyrproject/zephyr/cmake/app/boilerplate.cmake:635 (include)
  /home/mhelm/zephyrproject/zephyr/share/zephyr-package/cmake/ZephyrConfig.cmake:24 (include)
  /home/mhelm/zephyrproject/zephyr/share/zephyr-package/cmake/ZephyrConfig.cmake:40 (include_boilerplate)
  CMakeLists.txt:5 (find_package)

Signed-off-by: Maureen Helm <maureen.helm@intel.com>

cmake/extensions.cmake
@@ -826,8 +826,8 @@ function(board_finalize_runner_args runner)
 endfunction()
 
 function(board_set_rimage_target target)
-  zephyr_check_cache(RIMAGE_TARGET)
   set(RIMAGE_TARGET ${target} CACHE STRING "rimage target")
+  zephyr_check_cache(RIMAGE_TARGET)
 endfunction()
 
 # Zephyr board revision: