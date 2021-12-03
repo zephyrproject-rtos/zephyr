b6635456d1279dbc216ef02ea45f7cc624dad05d
Andy Ross <andrew.j.ross@intel.com>
2021-12-03 10:29:25 -0800

    soc/intel_adsp: Refactor, move CPU init into a separate header
    Move the very-early core initialization hooks (which are a mix of
    Xtensa architectural features and Intel-specific hardwareisms) into a
    separate header so they can be shared between the bootloader, main
    core, and MP cores.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- soc/xtensa/intel_adsp/common/soc_mp.c
+++ soc/xtensa/intel_adsp/common/soc_mp.c
@@ -23,6 +23,7 @@ LOG_MODULE_REGISTER(soc_mp, CONFIG_SOC_LOG_LEVEL);
 #include <adsp/io.h>
 #include <cavs-shim.h>
 #include <cavs-mem.h>
+#include <cpu_init.h>
 
 extern void z_sched_ipi(void);
 extern void z_smp_start_cpu(int id);
@@ -94,93 +95,11 @@ __asm__(".align 4                   \n\t"
 	"  call4 z_mp_entry         \n\t");
 BUILD_ASSERT(XCHAL_EXCM_LEVEL == 5);
 
-#define CxL1CCAP (*(volatile uint32_t *)0x9F080080)
-#define CxL1CCFG (*(volatile uint32_t *)0x9F080084)
-#define CxL1PCFG (*(volatile uint32_t *)0x9F080088)
-
-/* "Data/Instruction Cache Memory Way Count" fields */
-#define CxL1CCAP_DCMWC ((CxL1CCAP >> 16) & 7)
-#define CxL1CCAP_ICMWC ((CxL1CCAP >> 20) & 7)
-
-static ALWAYS_INLINE void enable_l1_cache(void)
-{
-	uint32_t reg;
-
-#ifdef CONFIG_SOC_SERIES_INTEL_CAVS_V25
-	/* First, on cAVS 2.5 we need to power the cache SRAM banks
-	 * on!  Write a bit for each cache way in the bottom half of
-	 * the L1CCFG register and poll the top half for them to turn
-	 * on.
-	 */
-	uint32_t dmask = BIT(CxL1CCAP_DCMWC) - 1;
-	uint32_t imask = BIT(CxL1CCAP_ICMWC) - 1;
-	uint32_t waymask = (imask << 8) | dmask;
-
-	CxL1CCFG = waymask;
-	while (((CxL1CCFG >> 16) & waymask) != waymask) {
-	}
-
-	/* Prefetcher also power gates, same interface */
-	CxL1PCFG = 1;
-	while ((CxL1PCFG & 0x10000) == 0) {
-	}
-#endif
-
-	/* Now set up the Xtensa CPU to enable the cache logic.  The
-	 * details of the fields are somewhat complicated, but per the
-	 * ISA ref: "Turning on caches at power-up usually consists of
-	 * writing a constant with bits[31:8] all 1’s to MEMCTL.".
-	 * Also set bit 0 to enable the LOOP extension instruction
-	 * fetch buffer.
-	 */
-#ifdef XCHAL_HAVE_ICACHE_DYN_ENABLE
-	reg = 0xffffff01;
-	__asm__ volatile("wsr %0, MEMCTL; rsync" :: "r"(reg));
-#endif
-
-	/* Likewise enable prefetching.  Sadly these values are not
-	 * architecturally defined by Xtensa (they're just documented
-	 * as priority hints), so this constant is just copied from
-	 * SOF for now.  If we care about prefetch priority tuning
-	 * we're supposed to ask Cadence I guess.
-	 */
-	reg = IS_ENABLED(CONFIG_SOC_SERIES_INTEL_CAVS_V25) ? 0x1038 : 0;
-	__asm__ volatile("wsr %0, PREFCTL; rsync" :: "r"(reg));
-
-	/* Finally we need to enable the cache in the Region
-	 * Protection Option "TLB" entries.  The hardware defaults
-	 * have this set to RW/uncached (2) everywhere.  We want
-	 * writeback caching (4) in the sixth mapping (the second of
-	 * two RAM mappings) and to mark all unused regions
-	 * inaccessible (15) for safety.  Note that there is a HAL
-	 * routine that does this (by emulating the older "cacheattr"
-	 * hardware register), but it generates significantly larger
-	 * code.
-	 */
-#ifdef CONFIG_SOC_SERIES_INTEL_CAVS_V25
-	/* Already set up by the ROM on older hardware. */
-	const uint8_t attribs[] = { 2, 15, 15, 15, 2, 4, 15, 15 };
-
-	for (int region = 0; region < 8; region++) {
-		reg = 0x20000000 * region;
-		__asm__ volatile("wdtlb %0, %1" :: "r"(attribs[region]), "r"(reg));
-	}
-#endif
-}
-
 void z_mp_entry(void)
 {
 	uint32_t reg;
 
-	enable_l1_cache();
-
-	/* Fix ATOMCTL to match CPU0.  Hardware defaults for S32C1I
-	 * use internal operations (and are thus presumably atomic
-	 * only WRT the local CPU!).  We need external transactions on
-	 * the shared bus.
-	 */
-	reg = 0x15;
-	__asm__ volatile("wsr %0, ATOMCTL" :: "r"(reg));
+	cpu_early_init();
 
 	/* We don't know what the boot ROM (on pre-2.5 DSPs) might
 	 * have touched and we don't care.  Make sure it's not in our
