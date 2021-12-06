988ae3044ec9eec30169a4c503a1ea8930e354dd
YuLong Yao <feilongphone@gmail.com>
2021-12-07 04:48:49 +0800

    soc: arm: gigadevice: add support for GD32e10x
    Add support for the E10x series.
    
    Signed-off-by: YuLong Yao <feilongphone@gmail.com>

---
+++ soc/arm/gigadevice/gd32e10x/soc.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021 YuLong Yao <feilongphone@gmail.com>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC_ARM_GIGADEVICE_GD32E10X_SOC_H_
+#define _SOC_ARM_GIGADEVICE_GD32E10X_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <gd32e10x.h>
+
+#endif /* _ASMLANGUAGE */
+
+#endif /* _SOC_ARM_GIGADEVICE_GD32E10X_SOC_H_ */
