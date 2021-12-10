286000a1d4dae74df700d94c544bfdf7a10b09bd
Silvano Cortesi <silvano.cortesi@hotmail.com>
2021-12-10 17:19:06 +0100

    drivers: sensor: max30101: Augment sample to be trigger based
    This commit augments the sample of the MAX30101 to be trigger based.
    
    Signed-off-by: Silvano Cortesi <silvano.cortesi@hotmail.com>

--- samples/sensor/max30101/src/main.c
+++ samples/sensor/max30101/src/main.c
@@ -8,9 +8,27 @@
 #include <drivers/sensor.h>
 #include <stdio.h>
 
+static struct sensor_value red;
+
+#ifdef CONFIG_MAX30101_TRIGGER
+static void trigger_handler_drdy(const struct device *dev, struct sensor_trigger *trig)
+{
+	if (sensor_sample_fetch(dev) < 0) {
+		printf("Can't readout new sensor value\n");
+		return;
+	}
+
+	if (sensor_channel_get(dev, SENSOR_CHAN_RED, &red) < 0) {
+		printf("Can't get a new sensor value\n");
+		return;
+	}
+	/* Print red PPG data */
+	printf("RED=%d\r\n", red.val1);
+}
+#endif
+
 void main(void)
 {
-	struct sensor_value red;
 	const struct device *dev = DEVICE_DT_GET_ANY(maxim_max30101);
 
 	if (dev == NULL) {
@@ -22,7 +40,15 @@ void main(void)
 		return;
 	}
 
+#ifdef CONFIG_MAX30101_TRIGGER
+	struct sensor_trigger trig;
+
+	trig.type = SENSOR_TRIG_DATA_READY;
+	sensor_trigger_set(dev, &trig, trigger_handler_drdy);
+#endif
+
 	while (1) {
+#ifndef CONFIG_MAX30101_TRIGGER
 		sensor_sample_fetch(dev);
 		sensor_channel_get(dev, SENSOR_CHAN_RED, &red);
 
@@ -30,5 +56,8 @@ void main(void)
 		printf("RED=%d\n", red.val1);
 
 		k_sleep(K_MSEC(20));
+#else
+		k_sleep(K_FOREVER);
+#endif
 	}
 }
