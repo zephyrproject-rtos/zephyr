680959373982584eb4fca09cdea464dfe59d262d
Andy Ross <andrew.j.ross@intel.com>
2021-12-10 21:33:44 -0800

    soc/intel_adsp: Linkage rework
    Lots of changes to the linkage, none major:
    
    + Remove all the manually-defined ELF program headers.  This was a big
      pain to maintain, and I finally figured out why we were doing this:
      it turns out to have been a workaround for the flags issue below.
    
    + Suppress the "empty loadable section" warnings at module generation.
      This turns out to be an objcopy issue, when you drop all the
      sections from an ELF program header.
    
    + Set section flags for NOLOAD sections manually.  Rimage is very
      strict about flags (even to the point of trying to suck in its own
      metadata section as program text).  This turns out to be really
      fragile, as the linker automatically sets flags on the output
      section based on the symbols placed in it.  Rather than needing to
      have one program header per section, or playing games in the
      assembly for section definition to make this all match, just set the
      flags expressly on the sections we know about on the objcopy command
      line.
    
    + Similarly drop the special memory regions with explicit faked
      "physical" addresses that were being used for non-loadable sections
      (e.g. .fw_metadata, .static_log_entries).  Just link them all after
      the rest of the image like other platforms do.
    
    + Clean up multiple levels of macro indirection for the manifest base
      address, which is ultimately coming from kconfig.  Now the magic
      numbers don't seem so magic.
    
    + Remove legacy symbol exports for "cacheattr" that we don't use
      anymore.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- soc/xtensa/intel_adsp/common/include/cavs-mem.h
+++ soc/xtensa/intel_adsp/common/include/cavs-mem.h
@@ -23,17 +23,13 @@
 #define HP_SRAM_WIN3_BASE (L2_SRAM_BASE + CONFIG_ADSP_WIN3_OFFSET)
 #define HP_SRAM_WIN3_SIZE 0x2000
 
-#define IMR_BOOT_LDR_MANIFEST_BASE CONFIG_IMR_MANIFEST_ADDR
-#define IMR_BOOT_LDR_TEXT_ENTRY_BASE (IMR_BOOT_LDR_MANIFEST_BASE + 0x6000)
-
-/* These are fake section addresses used only in the linker scripts */
-#define IDT_BASE		(L2_SRAM_BASE + L2_SRAM_SIZE)
-#define IDT_SIZE		0x2000
-#define UUID_ENTRY_ELF_BASE	0x1FFFA000
-#define UUID_ENTRY_ELF_SIZE	0x6000
-#define LOG_ENTRY_ELF_BASE	0x20000000
-#define LOG_ENTRY_ELF_SIZE	0x2000000
-#define EXT_MANIFEST_ELF_BASE	(LOG_ENTRY_ELF_BASE + LOG_ENTRY_ELF_SIZE)
-#define EXT_MANIFEST_ELF_SIZE	0x2000000
+/* The rimage tool produces two blob addresses we need to find: one is
+ * our bootloader code block which starts at its entry point, the
+ * other is the "manifest" containing the HP-SRAM data to unpack,
+ * which appears 24k earlier in the DMA'd file, and thus in IMR
+ * memory.  There's no ability to change this offset, it's a magic
+ * number from rimage we simply need to honor.
+ */
+#define IMR_BOOT_LDR_TEXT_ENTRY_BASE (CONFIG_IMR_MANIFEST_ADDR + 0x6000)
 
 #endif /* _ZEPHYR_SOC_INTEL_ADSP_MEM */
