75c8a0c9028a1cf4704b16e06f6953f5c3b282b4
Andy Ross <andrew.j.ross@intel.com>
2021-12-11 11:22:31 -0800

    soc/intel_adsp: Put a guard around CAVS_VERSION
    This is some SOF code that got imported into Zephyr.  But it's not a
    Zephyr API and Zephyr doesn't use it.  Unfortunately, the Zephyr
    definition is now the one actually used at runtime in SOF, so we can't
    remove it.
    
    Put a guard around the definition so nothing else uses it until we get
    it moved back home.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- soc/xtensa/intel_adsp/common/include/cavs/version.h
+++ soc/xtensa/intel_adsp/common/include/cavs/version.h
@@ -8,7 +8,14 @@
 #ifndef __CAVS_VERSION_H__
 #define __CAVS_VERSION_H__
 
-#include <autoconf.h>
+/* Note: this is not a Zephyr API and Zephyr doesn't use it
+ * (preferring the kconfig/devicetree mechanisms from which this is
+ * derived).  SOF uses the CAVS_VERSION symbol internally and needs it
+ * defined, and apparently that definition ended up here.  Really this
+ * should be moved to SOF proper, but until then let's make sure it
+ * doesn't spread:
+ */
+#ifdef CONFIG_SOF
 
 #define CAVS_VERSION_1_5 0x10500
 #define CAVS_VERSION_1_8 0x10800
@@ -25,5 +32,7 @@
 #elif CONFIG_SOC_SERIES_INTEL_CAVS_V25
 #define CAVS_VERSION CAVS_VERSION_2_5
 #endif
+
+#endif /* CONFIG_SOF */
 
 #endif /* __CAVS_VERSION_H__ */
