81dd3db3ccff410b60a328adeee28525b594064e
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2021-12-13 21:21:19 +0100

    pm: policy: residency: add compile time checks for timings
    Check that minimum residency time is greater than exit latency time for
    all CPUs power states at compile time.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- subsys/pm/policy/policy_residency.c
+++ subsys/pm/policy/policy_residency.c
@@ -14,6 +14,36 @@
 #include <logging/log.h>
 LOG_MODULE_DECLARE(pm, CONFIG_PM_LOG_LEVEL);
 
+/**
+ * Check CPU power state consistency.
+ *
+ * @param i Power state index.
+ * @param node_id CPU node identifier.
+ */
+#define CHECK_POWER_STATE_CONSISTENCY(i, node_id)			       \
+	BUILD_ASSERT(							       \
+		DT_PROP_BY_PHANDLE_IDX_OR(node_id, cpu_power_states, i,	       \
+					  min_residency_us, 0U) >=	       \
+		DT_PROP_BY_PHANDLE_IDX_OR(node_id, cpu_power_states, i,	       \
+					  exit_latency_us, 0U),		       \
+		"Found CPU power state with min_residency < exit_latency");
+
+/**
+ * @brief Check CPU power states consistency
+ *
+ * All states should have a minimum residency >= than the exit latency.
+ *
+ * @param node_id A CPU node identifier.
+ */
+#define CHECK_POWER_STATES_CONSISTENCY(node_id)				       \
+	UTIL_LISTIFY(DT_NUM_CPU_POWER_STATES(node_id),			       \
+		     CHECK_POWER_STATE_CONSISTENCY, node_id)		       \
+
+/* Check that all power states are consistent */
+COND_CODE_1(DT_NODE_EXISTS(DT_PATH(cpus)),
+	    (DT_FOREACH_CHILD(DT_PATH(cpus), CHECK_POWER_STATES_CONSISTENCY)),
+	    ())
+
 #define NUM_CPU_STATES(n) DT_NUM_CPU_POWER_STATES(n),
 #define CPU_STATES(n) (struct pm_state_info[])PM_STATE_INFO_LIST_FROM_DT_CPU(n),
 
@@ -47,8 +77,6 @@ struct pm_state_info pm_policy_next_state(uint8_t cpu, int32_t ticks)
 
 		min_residency = k_us_to_ticks_ceil32(state->min_residency_us);
 		exit_latency = k_us_to_ticks_ceil32(state->exit_latency_us);
-		__ASSERT(min_residency > exit_latency,
-			 "min_residency_us < exit_latency_us");
 
 		if ((ticks == K_TICKS_FOREVER) ||
 		    (ticks >= (min_residency + exit_latency))) {
