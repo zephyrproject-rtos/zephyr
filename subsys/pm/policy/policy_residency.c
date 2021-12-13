1ee22c25edc4e254dfc47037df081812453cee51
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2021-12-13 19:55:59 +0100

    pm: policy: residency: s/pm_min_residency/cpus_states
    Adjust pm_min_residency variable name to make it more clear.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- subsys/pm/policy/policy_residency.c
+++ subsys/pm/policy/policy_residency.c
@@ -19,7 +19,7 @@ LOG_MODULE_DECLARE(pm, CONFIG_PM_LOG_LEVEL);
 #define NUM_CPU_STATES(n) DT_NUM_CPU_POWER_STATES(n),
 #define CPU_STATES(n) (struct pm_state_info[])PM_STATE_INFO_LIST_FROM_DT_CPU(n),
 
-static const struct pm_state_info *pm_min_residency[] = {
+static const struct pm_state_info *cpus_states[] = {
 	DT_FOREACH_CHILD(DT_PATH(cpus), CPU_STATES)
 };
 
@@ -27,7 +27,7 @@ static int states_per_cpu[] = {
 	DT_FOREACH_CHILD(DT_PATH(cpus), NUM_CPU_STATES)
 };
 #else
-static const struct pm_state_info *pm_min_residency[CONFIG_MP_NUM_CPUS] = {};
+static const struct pm_state_info *cpus_states[CONFIG_MP_NUM_CPUS] = {};
 static int states_per_cpu[CONFIG_MP_NUM_CPUS] = {};
 #endif	/* DT_NODE_EXISTS(DT_PATH(cpus)) */
 
@@ -41,7 +41,7 @@ struct pm_state_info pm_policy_next_state(uint8_t cpu, int32_t ticks)
 
 	i = states_per_cpu[cpu];
 	const struct pm_state_info *states = (const struct pm_state_info *)
-		pm_min_residency[cpu];
+		cpus_states[cpu];
 
 	for (i = i - 1; i >= 0; i--) {
 		uint32_t min_residency, exit_latency;
