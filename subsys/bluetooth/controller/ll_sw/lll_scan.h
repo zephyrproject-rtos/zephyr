a951c232a648b4a2848f8f2cdf7ae639b34d7b3b
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2021-12-14 07:17:24 +0530

    Bluetooth: Controller: Advertising Extensions dynamic Tx power control
    Update Controller implementation of Advertising Extensions
    to inherit the set dynamic Tx power values of the primary
    channel PDU transmissions.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/lll_scan.h
+++ subsys/bluetooth/controller/ll_sw/lll_scan.h
@@ -68,10 +68,6 @@ struct lll_scan_aux {
 
 	uint32_t window_size_us;
 
-#if defined(CONFIG_BT_CTLR_TX_PWR_DYNAMIC_CONTROL)
-	int8_t tx_pwr_lvl;
-#endif /* CONFIG_BT_CTLR_TX_PWR_DYNAMIC_CONTROL */
-
 #if defined(CONFIG_BT_CENTRAL)
 	struct node_rx_pdu *node_conn_rx;
 #endif /* CONFIG_BT_CENTRAL */
--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_aux.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_aux.c
@@ -182,7 +182,7 @@ static int prepare_cb(struct lll_prepare_param *p)
 	radio_reset();
 
 #if defined(CONFIG_BT_CTLR_TX_PWR_DYNAMIC_CONTROL)
-	radio_tx_power_set(lll->tx_pwr_lvl);
+	radio_tx_power_set(lll_adv->tx_pwr_lvl);
 #else
 	radio_tx_power_set(RADIO_TXP_DEFAULT);
 #endif /* CONFIG_BT_CTLR_TX_PWR_DYNAMIC_CONTROL */
