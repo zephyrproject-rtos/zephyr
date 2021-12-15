abce92d867aad0475dc52963737a69289496c8d0
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2021-12-15 21:31:58 +0100

    cmake: zephyr toolchain code cleanup
    With the revert of commit 820d327b4618d587a9cae0d085f00c6ab9c81cf2 then
    some additional code can be cleaned up.
    
    This removes the final left-overs from Zephyr SDK 0.11.1 support and
    older.
    
    It further aligns message printing when including Zephyr SDK toolchain
    to other toolchain message printing.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
    (cherry picked from commit fb3a113eb844178235653ffd9facb39e94ff11d7)

--- cmake/toolchain/zephyr/host-tools.cmake
+++ cmake/toolchain/zephyr/host-tools.cmake
@@ -1,15 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-if(NOT DEFINED ZEPHYR_SDK_INSTALL_DIR)
-  # Until https://github.com/zephyrproject-rtos/zephyr/issues/4912 is
-  # resolved we use ZEPHYR_SDK_INSTALL_DIR to determine whether the user
-  # wants to use the Zephyr SDK or not.
-  return()
-endif()
-
-# Cache the Zephyr SDK install dir.
-set(ZEPHYR_SDK_INSTALL_DIR ${ZEPHYR_SDK_INSTALL_DIR} CACHE PATH "Zephyr SDK install directory")
-
-message(STATUS "Using toolchain: zephyr ${SDK_VERSION} (${ZEPHYR_SDK_INSTALL_DIR})")
-
 include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/host-tools.cmake)
+
+message(STATUS "Found host-tools: zephyr ${SDK_VERSION} (${ZEPHYR_SDK_INSTALL_DIR})")
