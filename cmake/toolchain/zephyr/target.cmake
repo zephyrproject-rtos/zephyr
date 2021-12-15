92a1ca61eb1ba94015a0226ac4e4e1907d480d19
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2021-12-15 21:40:48 +0100

    cmake: remove xtensa workaround in Zephyr toolchain code.
    Remove xtensa specific workaround as this code is now present in Zephyr
    SDK cmake code.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/toolchain/zephyr/target.cmake
+++ cmake/toolchain/zephyr/target.cmake
@@ -1,9 +1,3 @@
 # SPDX-License-Identifier: Apache-2.0
 
 include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/target.cmake)
-
-# Workaround, FIXME: Waiting for new SDK.
-if("${ARCH}" STREQUAL "xtensa")
-  set(SYSROOT_DIR ${TOOLCHAIN_HOME}/xtensa/${SOC_TOOLCHAIN_NAME}/${SYSROOT_TARGET})
-  set(CROSS_COMPILE ${TOOLCHAIN_HOME}/xtensa/${SOC_TOOLCHAIN_NAME}/${CROSS_COMPILE_TARGET}/bin/${CROSS_COMPILE_TARGET}-)
-endif()
