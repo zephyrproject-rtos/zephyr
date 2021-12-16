a610e8934d3c0299bba7fc94126ce5c5827d2c58
Boon Khai Ng <boon.khai.ng@intel.com>
2021-12-16 08:41:16 +0800

    dts: arm64: intel_socfpga: Add peripherals register map
    This patch is to add the peripheral register map in dts as syscon
    to cover the peripheral for UART, I2C, I2C_EMAC, and Timer SP
    addresses to be added in the MMU region. Due to the 4K alignment
    checking in MMU driver, the peripheral that is not 4K aligned
    will cause the kernel fall into kernel panic during MMU setup
    when the CONFIG_ASSERT option is turned on.
    
    Signed-off-by: Boon Khai Ng <boon.khai.ng@intel.com>

--- soc/arm64/intel_socfpga/agilex/mmu_regions.c
+++ soc/arm64/intel_socfpga/agilex/mmu_regions.c
@@ -20,10 +20,15 @@ static const struct arm_mmu_region mmu_regions[] = {
 			      DT_REG_ADDR(DT_NODELABEL(clock)),
 			      DT_REG_SIZE(DT_NODELABEL(clock)),
 			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
-
 	MMU_REGION_FLAT_ENTRY("UART0",
 			      DT_REG_ADDR(DT_NODELABEL(uart0)),
 			      DT_REG_SIZE(DT_NODELABEL(uart0)),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
+
+	/* Peripheral to cover UART, I2C, I2C EMAC and Timer SP */
+	MMU_REGION_FLAT_ENTRY("PERIPHERAL",
+			      DT_REG_ADDR(DT_NODELABEL(periph)),
+			      DT_REG_SIZE(DT_NODELABEL(periph)),
 			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
 
 	MMU_REGION_FLAT_ENTRY("GIC",
