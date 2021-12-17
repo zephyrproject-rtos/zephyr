2315259beadf84f8f86e876dd0f462076bb4822d
Bartosz Golaszewski <bartosz.golaszewski@huawei.com>
2021-12-17 18:07:34 +0100

    lvgl: kill LVGL_MEM_POOL_HEAP_KERNEL and LVGL_MEM_POOL_KERNEL
    Current lvgl code allows to use the kernel heap for dynamic memory
    allocation. The k_heap API doesn't however provide k_realloc() which
    will be needed in order to update lvgl to v8. Nico suggested there's
    no good reason for lvgl to use k_heap and it should stick to either
    the libc's allocator or depend on its own private sys_heap.
    
    The alternative would be to extend the k_heap API to provide k_realloc()
    but this may be tricky for several reasons and for now there would
    be a single user anyway.
    
    This removes the choice of using k_heap for lvgl and renames the user
    pool to SYS_HEAP in Kconfig and makes it the default option.
    
    The prj.conf for the lvgl sample is modifed to specify the number of
    memory pool blocks instead of the total size as the default block
    size is 2048 and it results in the same size of memory.
    
    Suggested-by: Nicolas Pitre <npitre@baylibre.com>
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@huawei.com>

--- lib/gui/lvgl/lv_conf.h
+++ lib/gui/lvgl/lv_conf.h
@@ -65,13 +65,7 @@ typedef short lv_coord_t;
 
 #define LV_MEMCPY_MEMSET_STD 1
 
-#ifdef CONFIG_LVGL_MEM_POOL_HEAP_KERNEL
-
-#define LV_MEM_CUSTOM_INCLUDE	"kernel.h"
-#define LV_MEM_CUSTOM_ALLOC	k_malloc
-#define LV_MEM_CUSTOM_FREE	k_free
-
-#elif defined(CONFIG_LVGL_MEM_POOL_HEAP_LIB_C)
+#if defined(CONFIG_LVGL_MEM_POOL_HEAP_LIB_C)
 
 #define LV_MEM_CUSTOM_INCLUDE	"stdlib.h"
 #define LV_MEM_CUSTOM_ALLOC	malloc
