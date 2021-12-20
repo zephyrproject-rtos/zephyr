b3c1eea2692524d6c3bd4fae6a1986da25c33eb6
Henrik Brix Andersen <hebad@vestas.com>
2021-12-20 22:27:39 +0100

    Revert "tests: Bluetooth: Mesh: Add tests to cover message publication"
    This reverts commit 9b99ab6896aa7bcc5f17faa707b50ce73f5a2b55.
    
    Signed-off-by: Henrik Brix Andersen <hebad@vestas.com>

--- tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/access/access_period.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Copyright 2021 Nordic Semiconductor
-# SPDX-License-Identifier: Apache-2.0
-
-source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
-
-RunTest mesh_access_periodic_publication \
-	access_tx_period access_rx_period
