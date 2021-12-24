ea665e210d81468cfdd4a353796cb35138539d1f
Hake Huang <hake.huang@oss.nxp.com>
2021-12-24 14:55:49 +0800

    soc: kconfig: pinctrl select in rt1xxx series
    select pinctrl by default in rt series
    
    Signed-off-by: Hake Huang <hake.huang@oss.nxp.com>

---
+++ soc/arm/nxp_imx/rt/pinctrl_soc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_ARM_NXP_IMX_RT_PINCTRL_SOC_H_
+#define ZEPHYR_SOC_ARM_NXP_IMX_RT_PINCTRL_SOC_H_
+
+#include <drivers/pinctrl/pinctrl_soc_mcux_rt_common.h>
+
+#endif /* ZEPHYR_SOC_ARM_NXP_IMX_RT_PINCTRL_SOC_H_ */
