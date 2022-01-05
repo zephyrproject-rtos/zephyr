ca3c2cf1f98e4ba1e37ed78febd20b54a5b0d183
Francois Ramu <francois.ramu@st.com>
2022-01-05 19:17:09 +0100

    drivers: spi: stm32 spi instance of stm32H7 mcu
    A generic function is used to give the register address to the DMA.
    The SPI of the stm32H7 serie has two data registers for Tx and Rx
    When the DMA is getting the address it differs between Rx and Tx.
    As the stm32cube/stm32h7xx/drivers/include/stm32h7xx_ll_spi.h
    has no such LL functions, the register address is get direclty.
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- drivers/spi/spi_ll_stm32.c
+++ drivers/spi/spi_ll_stm32.c
@@ -121,7 +121,7 @@ static int spi_stm32_dma_tx_load(const struct device *dev, const uint8_t *buf,
 		}
 	}
 
-	blk_cfg->dest_address = (uint32_t)LL_SPI_DMA_GetRegAddr(cfg->spi);
+	blk_cfg->dest_address = ll_func_dma_get_reg_addr(cfg->spi, SPI_STM32_DMA_TX);
 	/* fifo mode NOT USED there */
 	if (data->dma_tx.dst_addr_increment) {
 		blk_cfg->dest_addr_adj = DMA_ADDR_ADJ_INCREMENT;
@@ -180,7 +180,7 @@ static int spi_stm32_dma_rx_load(const struct device *dev, uint8_t *buf,
 		}
 	}
 
-	blk_cfg->source_address = (uint32_t)LL_SPI_DMA_GetRegAddr(cfg->spi);
+	blk_cfg->source_address = ll_func_dma_get_reg_addr(cfg->spi, SPI_STM32_DMA_RX);
 	if (data->dma_rx.src_addr_increment) {
 		blk_cfg->source_addr_adj = DMA_ADDR_ADJ_INCREMENT;
 	} else {
