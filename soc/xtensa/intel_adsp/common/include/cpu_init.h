97ada8bc04c1c38d395903f516f94a19afec0525
Andy Ross <andrew.j.ross@intel.com>
2022-01-07 05:09:39 -0800

    arch/xtensa: Promote adsp RPO/cache utilities to an arch API
    This is trick (mapping RAM twice so you can use alternate Region
    Protection Option addresses to control cacheability) is something any
    Xtensa hardware designer might productively choose to do.  And as it
    works really well, we should encourage that by making this a generic
    architecture feature for Zephyr.
    
    Now everything works by setting two kconfig values at the soc level
    defining the cached and uncached regions.  As long as these are
    correct, you can then use the new arch_xtensa_un/cached_ptr() APIs to
    convert between them and a ARCH_XTENSA_SET_RPO_TLB() macro that
    provides much smaller initialization code (in C!) than the HAL
    assembly macros.  The conversion routines have been generalized to
    support conversion between any two regions.
    
    Note that full KERNEL_COHERENCE still requires support from the
    platform linker script, that can't be made generic given the way
    Zephyr does linkage.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- soc/xtensa/intel_adsp/common/include/cpu_init.h
+++ soc/xtensa/intel_adsp/common/include/cpu_init.h
@@ -4,6 +4,7 @@
 #ifndef __INTEL_ADSP_CPU_INIT_H
 #define __INTEL_ADSP_CPU_INIT_H
 
+#include <arch/xtensa/cache.h>
 #include <xtensa/config/core-isa.h>
 
 #define CxL1CCAP (*(volatile uint32_t *)0x9F080080)
@@ -13,39 +14,6 @@
 /* "Data/Instruction Cache Memory Way Count" fields */
 #define CxL1CCAP_DCMWC ((CxL1CCAP >> 16) & 7)
 #define CxL1CCAP_ICMWC ((CxL1CCAP >> 20) & 7)
-
-/* Utilities to generate an unwrapped code sequence to set the RPO TLB
- * registers.  Pass the 8 region attributes as arguments, e.g.:
- *
- *     SET_RPO_TLB(2, 15, 15, 15, 2, 4, 15, 15);
- *
- * Note that cAVS 1.5 has the "translation" option that we don't use,
- * but still need to put an identity mapping in the high bits.  Also
- * per spec changing the current code region requires that WITLB be
- * followed by an ISYNC and that both instructions live in the same
- * cache line (two 3-byte instructions fit in an 8-byte aligned
- * region, so that's guaranteed not to cross a caceh line boundary).
- */
-#define SET_ONE_TLB(region, att) do {					\
-	uint32_t addr = region * 0x20000000U, attr = att;		\
-	if (XCHAL_HAVE_XLT_CACHEATTR) {					\
-		attr |= addr; /* RPO with translation */		\
-	}								\
-	if (region != (L2_SRAM_BASE >> 29)) {				\
-		__asm__ volatile("wdtlb %0, %1; witlb %0, %1"		\
-				 :: "r"(attr), "r"(addr));		\
-	} else {							\
-		__asm__ volatile("wdtlb %0, %1"				\
-				 :: "r"(attr), "r"(addr));		\
-		__asm__ volatile("j 1f; .align 8; 1:");			\
-		__asm__ volatile("witlb %0, %1; isync"			\
-				 :: "r"(attr), "r"(addr));		\
-	}								\
-} while (0)
-
-#define SET_RPO_TLB(...) do {				\
-	FOR_EACH_IDX(SET_ONE_TLB, (;), __VA_ARGS__);	\
-} while (0)
 
 /* Low-level CPU initialization.  Call this immediately after entering
  * C code to initialize the cache, protection and synchronization
@@ -98,15 +66,9 @@ static ALWAYS_INLINE void cpu_early_init(void)
 
 	/* Finally we need to enable the cache in the Region
 	 * Protection Option "TLB" entries.  The hardware defaults
-	 * have this set to RW/uncached (2) everywhere.  We want
-	 * writeback caching (4) in the sixth mapping (the second of
-	 * two RAM mappings) and to mark all unused regions
-	 * inaccessible (15) for safety.  Note that there is a HAL
-	 * routine that does this (by emulating the older "cacheattr"
-	 * hardware register), but it generates significantly larger
-	 * code.
+	 * have this set to RW/uncached everywhere.
 	 */
-	SET_RPO_TLB(2, 15, 15, 15, 2, 4, 15, 15);
+	ARCH_XTENSA_SET_RPO_TLB();
 
 	/* Initialize ATOMCTL: Hardware defaults for S32C1I use
 	 * "internal" operations, meaning they are atomic only WRT the
