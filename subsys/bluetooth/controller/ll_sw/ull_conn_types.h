cb29f29cc930c52e3cb5a59544a58096e4dd610e
Piotr Pryga <piotr.pryga@nordicsemi.no>
2022-01-08 12:51:23 +0100

    Bluetooth: Controller: Integrate CTE req in ULL with refactored LLCPs
    Integrate existing CTE request control procedure code in ULL with
    implementation of refactor LLCPs.
    The commit includes code responsible for:
    - enabling CTE request and scheduling its execution by refactored
      LLCPs framework,
    - running CTE request periodically
    - disabling CTE reqest in case it is running periodic
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_conn_types.h
+++ subsys/bluetooth/controller/ll_sw/ull_conn_types.h
@@ -422,6 +422,7 @@ struct llcp_struct {
 		/* Minimum requested CTE length in 8us units */
 		uint8_t min_cte_len;
 		uint16_t req_interval;
+		uint16_t req_expire;
 	} cte_req;
 #endif /* CONFIG_BT_CTLR_DF_CONN_CTE_REQ */
 #if defined(CONFIG_BT_CTLR_DF_CONN_CTE_RSP)
