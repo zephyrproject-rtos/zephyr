d0c95fe869d3571e741547dab7f27971eb680ade
Flavio Ceolin <flavio.ceolin@intel.com>
2022-01-12 15:11:09 -0800

    lib: crc32c_sw: Code guideline fixes
    Fixes violations code guideline related with essential types rules,
    basically it ensures computations are done in the destination precision.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>
    Signed-off-by: Abramo Bagnara <abramo.bagnara@bugseng.com>

--- lib/os/crc32c_sw.c
+++ lib/os/crc32c_sw.c
@@ -8,21 +8,21 @@
 
 /* crc table generated from polynomial 0x1EDC6F41UL (Castagnoli) */
 static const uint32_t crc32c_table[16] = {
-	0x00000000UL, 0x105EC76FUL, 0x20BD8EDEUL, 0x30E349B1UL,
-	0x417B1DBCUL, 0x5125DAD3UL, 0x61C69362UL, 0x7198540DUL,
-	0x82F63B78UL, 0x92A8FC17UL, 0xA24BB5A6UL, 0xB21572C9UL,
-	0xC38D26C4UL, 0xD3D3E1ABUL, 0xE330A81AUL, 0xF36E6F75UL
+	0x00000000U, 0x105EC76FU, 0x20BD8EDEU, 0x30E349B1U,
+	0x417B1DBCU, 0x5125DAD3U, 0x61C69362U, 0x7198540DU,
+	0x82F63B78U, 0x92A8FC17U, 0xA24BB5A6U, 0xB21572C9U,
+	0xC38D26C4U, 0xD3D3E1ABU, 0xE330A81AU, 0xF36E6F75U
 };
 
 /* This value needs to be XORed with the final crc value once crc for
  * the entire stream is calculated. This is a requirement of crc32c algo.
  */
-#define CRC32C_XOR_OUT	0xFFFFFFFFUL
+#define CRC32C_XOR_OUT	0xFFFFFFFFU
 
 /* The crc32c algorithm requires the below value as Init value at the
  * beginning of the stream.
  */
-#define CRC32C_INIT	0xFFFFFFFFUL
+#define CRC32C_INIT	0xFFFFFFFFU
 
 uint32_t crc32_c(uint32_t crc, const uint8_t *data,
 		 size_t len, bool first_pkt, bool last_pkt)
@@ -32,8 +32,8 @@ uint32_t crc32_c(uint32_t crc, const uint8_t *data,
 	}
 
 	for (size_t i = 0; i < len; i++) {
-		crc = crc32c_table[(crc ^ data[i]) & 0x0F] ^ (crc >> 4);
-		crc = crc32c_table[(crc ^ ((uint32_t)data[i] >> 4)) & 0x0F] ^ (crc >> 4);
+		crc = crc32c_table[(crc ^ data[i]) & 0x0FU] ^ (crc >> 4);
+		crc = crc32c_table[(crc ^ ((uint32_t)data[i] >> 4)) & 0x0FU] ^ (crc >> 4);
 	}
 
 	return last_pkt ? (crc ^ CRC32C_XOR_OUT) : crc;
