6b048ad80d88b211772981e94f7c6f1cc90810ae
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2022-01-12 15:52:48 +0100

    logging: Deprecate v1, default to v2
    Reduced logging mode selection to deferred, immediate, minimal and
    frontend. Decoupled logging version from mode and created CONFIG_LOG1
    which can be used to explicitly select deprecated version.
    
    From now on, chosing CONFIG_LOG_MODE_{IMMEDIATE,DEFERRED} will result
    in version2.
    
    Deprecated CONFIG_LOG2_MODE_{IMMEDIATE,DEFERRED} with cmake warning.
    
    Codebase adapted to those changes.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

--- tests/subsys/logging/log_api/src/mock_backend.c
+++ tests/subsys/logging/log_api/src/mock_backend.c
@@ -381,12 +381,12 @@ static void sync_hexdump(const struct log_backend *const backend,
 
 const struct log_backend_api mock_log_backend_api = {
 	.process = IS_ENABLED(CONFIG_LOG2) ? process : NULL,
-	.put = IS_ENABLED(CONFIG_LOG_MODE_DEFERRED) ? put : NULL,
-	.put_sync_string = IS_ENABLED(CONFIG_LOG_MODE_IMMEDIATE) ?
+	.put = IS_ENABLED(CONFIG_LOG1_DEFERRED) ? put : NULL,
+	.put_sync_string = IS_ENABLED(CONFIG_LOG1_IMMEDIATE) ?
 			sync_string : NULL,
-	.put_sync_hexdump = IS_ENABLED(CONFIG_LOG_MODE_IMMEDIATE) ?
+	.put_sync_hexdump = IS_ENABLED(CONFIG_LOG1_IMMEDIATE) ?
 			sync_hexdump : NULL,
 	.panic = panic,
 	.init = mock_init,
-	.dropped = IS_ENABLED(CONFIG_LOG_IMMEDIATE) ? NULL : dropped,
+	.dropped = IS_ENABLED(CONFIG_LOG_MODE_IMMEDIATE) ? NULL : dropped,
 };
