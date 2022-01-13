bfa7038018f4c4988e3a249eae4b45ed9757e085
Flavio Ceolin <flavio.ceolin@intel.com>
2022-01-12 21:21:06 -0800

    lib: sem: Code guideline fixes
    Fixes violations code guideline related with essential types rules:
    
    - use bool when the data nature is Boolean
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>
    Signed-off-by: Abramo Bagnara <abramo.bagnara@bugseng.com>

--- lib/os/sem.c
+++ lib/os/sem.c
@@ -22,7 +22,7 @@ static inline atomic_t bounded_dec(atomic_t *val, atomic_t minimum)
 		}
 
 		new_value = old_value - 1;
-	} while (atomic_cas(val, old_value, new_value) == 0);
+	} while (!atomic_cas(val, old_value, new_value));
 
 	return old_value;
 }
@@ -40,7 +40,7 @@ static inline atomic_t bounded_inc(atomic_t *val, atomic_t minimum,
 
 		new_value = old_value < minimum ?
 			    minimum + 1 : old_value + 1;
-	} while (atomic_cas(val, old_value, new_value) == 0U);
+	} while (!atomic_cas(val, old_value, new_value));
 
 	return old_value;
 }
