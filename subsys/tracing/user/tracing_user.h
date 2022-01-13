7467dc4c12b9b3646491e17baa99eaf282a00650
Wealian Liao <WHLIAO@nuvoton.com>
2022-01-13 10:55:15 +0800

    tracing_user: Add ISR nest level parameter
    For tracing_user, the sys_trace_isr_enter() & sys_trace_isr_exit()
    block any nest interrupts & most SMP interrupts for the user. It is
    hard to analyze the IRQ preemption(e.g., each IRQ counter and execution
    time). This commit adds ISR nest level for each CPU to the user instead
    of blocking user call back when nest interrupts.
    
    Signed-off-by: Wealian Liao <WHLIAO@nuvoton.com>

--- subsys/tracing/user/tracing_user.h
+++ subsys/tracing/user/tracing_user.h
@@ -16,8 +16,8 @@ extern "C" {
 
 void sys_trace_thread_switched_in_user(struct k_thread *thread);
 void sys_trace_thread_switched_out_user(struct k_thread *thread);
-void sys_trace_isr_enter_user(void);
-void sys_trace_isr_exit_user(void);
+void sys_trace_isr_enter_user(int nested_interrupts);
+void sys_trace_isr_exit_user(int nested_interrupts);
 void sys_trace_idle_user(void);
 
 void sys_trace_k_thread_switched_in(void);
