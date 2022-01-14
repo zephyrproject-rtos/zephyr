9b567aa9374f334eb1dffb2f6a5f0bcaca38cffb
Andy Ross <andrew.j.ross@intel.com>
2022-01-14 10:11:08 -0800

    tests/boards: Add intel_adsp board integration/smoke test
    As Zephyr begins to absorb drivers for these platforms that had
    previously been managed by the SOF app, there's a need for a rapid
    board-specific smoke test to use during development.
    
    This starts with the smp_boot_delay test (itself a unit test for a
    SOF-derived feature) and adds a host IPC case (that needs to match
    code in cavstool.py on the other side of the PCI bus!).
    
    It will grow more features over time as needed.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

---
+++ tests/boards/intel_adsp/smoke/src/tests.h
@@ -0,0 +1,24 @@
+/* Copyright (c) 2022 Intel Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_TESTS_INTEL_ADSP_TESTS_H
+#define ZEPHYR_TESTS_INTEL_ADSP_TESTS_H
+
+/* Helper to escape from infinite polling loops with a test failure
+ * instead of a hang.  Spins with a relaxation loop so that it works
+ * in interrupt context and doesn't stress shared resources like SRAM
+ */
+#define WAIT_FOR(expr) do {						\
+		int i;							\
+		for (i = 0; !(expr) && i < 10000; i++) {		\
+			for (volatile int j = 0; j < 1000; j++) {	\
+			}						\
+		}							\
+		zassert_true(i < 10000, "timeout waiting for %s", #expr); \
+	} while (0)
+
+void test_post_boot_ipi(void);
+void test_smp_boot_delay(void);
+void test_host_ipc(void);
+
+#endif /* ZEPHYR_TESTS_INTEL_ADSP_TESTS_H */
