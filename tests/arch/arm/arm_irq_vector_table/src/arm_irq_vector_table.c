4d963755f1d79fbe7fd073f235247ac1fcdfa592
Mahesh Mahadevan <mahesh.mahadevan@nxp.com>
2022-01-18 09:56:05 -0600

    tests: arch: arm_irq_vector_table: Update to run on MXRT595
    Update the custom vector table to add the OS Event timer
    interrupt which is used on RT595 as the kernel system timer
    
    Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

--- tests/arch/arm/arm_irq_vector_table/src/arm_irq_vector_table.c
+++ tests/arch/arm/arm_irq_vector_table/src/arm_irq_vector_table.c
@@ -227,7 +227,8 @@ vth __irq_vector_table _irq_vector_table[] = {
 	isr0, isr1, isr2, 0,
 	rtc_isr
 };
-#elif defined(CONFIG_SOC_SERIES_IMX_RT6XX) && defined(CONFIG_MCUX_OS_TIMER)
+#elif defined(CONFIG_SOC_SERIES_IMX_RT6XX) || defined(CONFIG_SOC_SERIES_IMX_RT5XX) && \
+	defined(CONFIG_MCUX_OS_TIMER)
 /* MXRT685 employs a OS Event timer to implement the Kernel system
  * timer, instead of the ARM Cortex-M SysTick. Therefore, a pointer to
  * the timer ISR needs to be added in the custom vector table to handle
