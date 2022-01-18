67bfa6cce59d303cdff4bc3ded2f2f36674ca43d
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-01-18 16:23:45 +0100

    drivers: sensor: drop DEV_DATA/DEV_CFG usage
    Stop using redundant DEV_DATA/DEV_CFG macros and use dev->data and
    dev->config instead.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- drivers/sensor/bmp388/bmp388.h
+++ drivers/sensor/bmp388/bmp388.h
@@ -187,9 +187,6 @@ struct bmp388_data {
 #endif /* CONFIG_BMP388_TRIGGER */
 };
 
-#define DEV_DATA(dev) ((struct bmp388_data *)dev->data)
-#define DEV_CFG(dev)  ((const struct bmp388_config *)dev->config)
-
 int bmp388_trigger_mode_init(const struct device *dev);
 int bmp388_trigger_set(const struct device *dev,
 		       const struct sensor_trigger *trig,
