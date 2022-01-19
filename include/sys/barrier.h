49ca5088a087157b67f1e9748b1056f2c37285cf
Alexandre Mergnat <amergnat@baylibre.com>
2022-01-19 16:34:55 +0100

    barrier: Introduce barriers API
    - Introduce a new preprocessor based API for barriers.
    
    - Add HAS_ARCH_MEMORY_BARRIER option for memory barrier:
    The architecture must select it to use its own specific
    memory barrier functions.
    
    Signed-off-by: Alexandre Mergnat <amergnat@baylibre.com>

---
+++ include/sys/barrier.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2022 Alexandre Mergnat <amergnat@baylibre.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Public interface for barriers.
+ */
+#ifndef ZEPHYR_INCLUDE_BARRIER_H_
+#define ZEPHYR_INCLUDE_BARRIER_H_
+
+#ifndef _ASMLANGUAGE
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Some architectures haven't their own implementation */
+#if !defined(CONFIG_ARCH_HAS_MEMORY_BARRIER)
+
+/**
+ * @defgroup barrier_apis Barriers API
+ * @ingroup kernel_apis
+ * @{
+ */
+
+/**
+ * @brief arch_mb - Memory Barrier
+ *
+ * A full system memory barrier. All memory operations before the mb() in the
+ * instruction stream will be committed before any operations after the mb()
+ * are committed.
+ */
+#define arch_mb()	__atomic_thread_fence(__ATOMIC_ACQ_REL)
+
+/**
+ * @brief arch_rmb - Read Memory Barrier
+ *
+ * Like arch_mb(), but only guarantees ordering between read accesses. That is,
+ * all read operations before an rmb() will be committed before any read
+ * operations after the rmb().
+ */
+#define arch_rmb()	__atomic_thread_fence(__ATOMIC_ACQUIRE)
+
+/**
+ * @brief arch_wmb - Write Memory Barrier
+ *
+ * Like arch_mb(), but only guarantees ordering between write accesses. That is,
+ * all write operations before a wmb() will be committed before any write
+ * operations after the wmb().
+ */
+#define arch_wmb()	__atomic_thread_fence(__ATOMIC_RELEASE)
+#endif /* !CONFIG_ARCH_HAS_MEMORY_BARRIER */
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* ZEPHYR_INCLUDE_BARRIER_H_ */
