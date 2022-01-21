d07d94331b706eb172dfc2d8adc663e4ace8f452
Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>
2022-01-21 11:32:34 +0100

    Bluetooth: OTC: Clean up internal header file
    Remove various unused defines, update comments, update inclusion guard
    
    Signed-off-by: Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>

--- subsys/bluetooth/services/ots/otc_internal.h
+++ subsys/bluetooth/services/ots/otc_internal.h
@@ -3,16 +3,12 @@
  *
  * For use with the Object Transfer Service Client (OTC)
  *
- * Copyright (c) 2020 - 2021 Nordic Semiconductor ASA
+ * Copyright (c) 2020 - 2022 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#ifndef ZEPHYR_INCLUDE_BLUETOOTH_HOST_AUDIO_OTC_INTERNAL_H_
-#define ZEPHYR_INCLUDE_BLUETOOTH_HOST_AUDIO_OTC_INTERNAL_H_
-
-/* TODO: Temporarily here - clean up, and move alongside the Object Transfer Service */
-
-#include <bluetooth/services/otc.h>
+#ifndef BT_GATT_OTC_INTERNAL_H_
+#define BT_GATT_OTC_INTERNAL_H_
 
 #ifdef __cplusplus
 extern "C" {
@@ -27,6 +23,7 @@ extern "C" {
  *
  * TODO: Update to also convert the non-SUCCESS values
  * TODO: The macro is called OLCP, but uses OACP success value
+ * TODO: See https://github.com/zephyrproject-rtos/zephyr/issues/41184
  * OTS does not use the value "0".
  */
 #define OLCP_RESULT_TO_ERROR(RESULT) \
@@ -38,25 +35,8 @@ extern "C" {
 #define OTS_PROPERTIES_LEN     (uint32_t)(sizeof(uint32_t))
 #define OTS_TYPE_MAX_LEN       (uint32_t)(sizeof(struct bt_ots_obj_type))
 
-#define OTS_MAX_OBJECT_META_SIZE       170
-
-/** PSM for Object Transfer Service. */
-#define OTS_L2CAP_PSM                  0x0025
-/**Start of the usable range of Object IDs (values 0 to 0xFF are reserved)*/
-#define OTS_ID_RANGE_START             0x000000000100
-/** Type UUID of the directory listing object. */
-#define OTS_DIR_LISTING_TYPE           0x2acb
-/** ID of the directory listing object. */
-#define OTS_DIR_LISTING_ID             0x000000000000
-/** Name of the directory listing object. */
-#define OTS_DIR_LISTING_NAME           "Directory_listing"
-
-/** Maximum size of the directory listing object. */
-#define BT_OTS_DIR_LISTING_MAX_SIZE \
-	(OTS_MAX_OBJECT_META_SIZE * BT_OTS_MAX_OBJ_CNT)
-
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* ZEPHYR_INCLUDE_BLUETOOTH_HOST_AUDIO_OTC_INTERNAL_H_ */
+#endif /* BT_GATT_OTC_INTERNAL_H_ */
