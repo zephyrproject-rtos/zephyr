5e3b35ba89ee8d1aec21bad33f0a587c4e4a4b78
Henrik Brix Andersen <hebad@vestas.com>
2022-01-21 18:19:22 +0100

    Revert "drivers: can: stm32: Change TX priority to chronological order"
    This reverts commit 82a1661c99c8557974e32ed295789b5aad9b42f9.
    
    Fixes: #26541
    
    Signed-off-by: Henrik Brix Andersen <hebad@vestas.com>

--- drivers/can/can_stm32.c
+++ drivers/can/can_stm32.c
@@ -481,11 +481,8 @@ static int can_stm32_init(const struct device *dev)
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(can2), okay)
 	master_can->FMR &= ~CAN_FMR_CAN2SB; /* Assign all filters to CAN2 */
 #endif
-
-	/* Set TX priority to chronological order */
-	can->MCR |= CAN_MCR_TXFP;
 	can->MCR &= ~CAN_MCR_TTCM & ~CAN_MCR_ABOM & ~CAN_MCR_AWUM &
-		    ~CAN_MCR_NART & ~CAN_MCR_RFLM;
+		    ~CAN_MCR_NART & ~CAN_MCR_RFLM & ~CAN_MCR_TXFP;
 #ifdef CONFIG_CAN_RX_TIMESTAMP
 	can->MCR |= CAN_MCR_TTCM;
 #endif
