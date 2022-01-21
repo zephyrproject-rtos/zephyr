649b7e0e639a8db16a41ead091817400fe0734b0
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-01-21 11:24:19 +0100

    pm: policy: new API to set maximum latency requirements
    Add a new API that allows to configure maximum latency requirements.
    When the policy manager computes the next state, it will check if the
    state brings too much latency based on requirements. This can be useful,
    for example, if a certain driver or the application want a system to
    respond fast, since any low power state that brings too much latency
    will not be used.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- subsys/pm/policy.c
+++ subsys/pm/policy.c
@@ -5,8 +5,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <kernel.h>
 #include <pm/pm.h>
 #include <pm/policy.h>
+#include <spinlock.h>
 #include <sys_clock.h>
 #include <sys/__assert.h>
 #include <sys/time_units.h>
@@ -15,6 +17,29 @@
 
 /** State lock reference counting */
 static atomic_t state_lock_cnt[PM_STATE_COUNT];
+
+/** Lock to synchronize access to the latency request list. */
+static struct k_spinlock latency_lock;
+/** List of maximum latency requests. */
+static sys_slist_t latency_reqs;
+/** Maximum CPU latency in ticks */
+static int32_t max_latency_ticks = K_TICKS_FOREVER;
+
+/** @brief Update maximum allowed latency. */
+static void update_max_latency(void)
+{
+	int32_t new_max_latency_ticks = K_TICKS_FOREVER;
+	struct pm_policy_latency_request *req;
+
+	SYS_SLIST_FOR_EACH_CONTAINER(&latency_reqs, req, node) {
+		if ((new_max_latency_ticks == K_TICKS_FOREVER) ||
+		    ((int32_t)req->value < new_max_latency_ticks)) {
+			new_max_latency_ticks = (int32_t)req->value;
+		}
+	}
+
+	max_latency_ticks = new_max_latency_ticks;
+}
 
 #ifdef CONFIG_PM_POLICY_DEFAULT
 const struct pm_state_info *pm_policy_next_state(uint8_t cpu, int32_t ticks)
@@ -34,6 +59,12 @@ const struct pm_state_info *pm_policy_next_state(uint8_t cpu, int32_t ticks)
 
 		min_residency = k_us_to_ticks_ceil32(state->min_residency_us);
 		exit_latency = k_us_to_ticks_ceil32(state->exit_latency_us);
+
+		/* skip state if it brings too much latency */
+		if ((max_latency_ticks != K_TICKS_FOREVER) &&
+		    (exit_latency >= max_latency_ticks)) {
+			continue;
+		}
 
 		if ((ticks == K_TICKS_FOREVER) ||
 		    (ticks >= (min_residency + exit_latency))) {
@@ -62,4 +93,38 @@ void pm_policy_state_lock_put(enum pm_state state)
 bool pm_policy_state_lock_is_active(enum pm_state state)
 {
 	return (atomic_get(&state_lock_cnt[state]) != 0);
+}
+
+void pm_policy_latency_request_add(struct pm_policy_latency_request *req,
+				   uint32_t value)
+{
+	req->value = k_us_to_ticks_ceil32(value);
+
+	k_spinlock_key_t key = k_spin_lock(&latency_lock);
+
+	sys_slist_append(&latency_reqs, &req->node);
+	update_max_latency();
+
+	k_spin_unlock(&latency_lock, key);
+}
+
+void pm_policy_latency_request_update(struct pm_policy_latency_request *req,
+				      uint32_t value)
+{
+	k_spinlock_key_t key = k_spin_lock(&latency_lock);
+
+	req->value = k_us_to_ticks_ceil32(value);
+	update_max_latency();
+
+	k_spin_unlock(&latency_lock, key);
+}
+
+void pm_policy_latency_request_remove(struct pm_policy_latency_request *req)
+{
+	k_spinlock_key_t key = k_spin_lock(&latency_lock);
+
+	(void)sys_slist_find_and_remove(&latency_reqs, &req->node);
+	update_max_latency();
+
+	k_spin_unlock(&latency_lock, key);
 }
