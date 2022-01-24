4b6ab8b7a1faa623f958f1c5a1d2e40a95ae734e
Erwan Gouriou <erwan.gouriou@linaro.org>
2022-01-24 09:41:42 +0100

    tests/drivers/clock_control: stm32h7: device_test: Clean up
    Cleanup the test a little bit to give some clarity.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- tests/drivers/clock_control/stm32_clock_configuration/stm32h7_devices/src/test_stm32_clock_configuration.c
+++ tests/drivers/clock_control/stm32_clock_configuration/stm32h7_devices/src/test_stm32_clock_configuration.c
@@ -25,7 +25,7 @@ static void test_sysclk_freq(void)
 
 static void test_spi_clk_config(void)
 {
-	struct stm32_pclken spi1_clk_cfg = {
+	struct stm32_pclken spi1_reg_clk_cfg = {
 		.enr = DT_CLOCKS_CELL_BY_NAME(DT_NODELABEL(spi1), reg, bits),
 		.bus = DT_CLOCKS_CELL_BY_NAME(DT_NODELABEL(spi1), reg, bus)
 	};
@@ -33,58 +33,61 @@ static void test_spi_clk_config(void)
 		.enr = DT_CLOCKS_CELL_BY_NAME(DT_NODELABEL(spi1), kernel, bits),
 		.bus = DT_CLOCKS_CELL_BY_NAME(DT_NODELABEL(spi1), kernel, bus)
 	};
-	uint32_t spi1_actual_clk_src, spi1_hal_clk_kernel_src, spi1_dt_clk_kernel_src;
+	uint32_t spi1_actual_clk_src, spi1_dt_ker_clk_src;
 	uint32_t spi1_dt_clk_freq, spi1_actual_clk_freq;
 	int r;
 
-	/* Test clock_on spi1_clck_cfg */
+	/* Test clock_on(reg_clk) */
 	r = clock_control_on(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
-				(clock_control_subsys_t) &spi1_clk_cfg);
-	zassert_true((r == 0), "Could not enable SPI Clk");
+				(clock_control_subsys_t) &spi1_reg_clk_cfg);
+	zassert_true((r == 0), "Could not enable SPI reg_clk");
 
-	zassert_true(__HAL_RCC_SPI1_IS_CLK_ENABLED(), "SPI1 clock should be on");
-	TC_PRINT("SPI1 Clock on\n");
+	zassert_true(__HAL_RCC_SPI1_IS_CLK_ENABLED(), "SPI1 reg_clk should be on");
+	TC_PRINT("SPI1 reg_clk on\n");
 
-	/* Test clock_on spi1_ker_clk_cfg */
+	/* Test clock_on(ker_clk) */
 	r = clock_control_on(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
 				(clock_control_subsys_t) &spi1_ker_clk_cfg);
 	zassert_true((r == 0), "Could not enable SPI ker_clk");
 	TC_PRINT("SPI1 ker_clk on\n");
 
-	/* Test clk_src */
-	spi1_dt_clk_kernel_src = DT_CLOCKS_CELL_BY_NAME(DT_NODELABEL(spi1), kernel, bus);
-	if (spi1_dt_clk_kernel_src == STM32_SRC_PLL1_Q) {
-		spi1_hal_clk_kernel_src = RCC_SPI123CLKSOURCE_PLL;
-	} else if (spi1_dt_clk_kernel_src == STM32_SRC_PLL3_P) {
-		spi1_hal_clk_kernel_src = RCC_SPI123CLKSOURCE_PLL3;
+	/* Test ker_clk source */
+	spi1_dt_ker_clk_src = DT_CLOCKS_CELL_BY_NAME(DT_NODELABEL(spi1), kernel, bus);
+	spi1_actual_clk_src = __HAL_RCC_GET_SPI1_SOURCE();
+
+	if (spi1_dt_ker_clk_src == STM32_SRC_PLL1_Q) {
+		zassert_equal(spi1_actual_clk_src, RCC_SPI123CLKSOURCE_PLL,
+				"Expected SPI src: PLLQ (%d). Actual SPI src: %d",
+				spi1_actual_clk_src, RCC_SPI123CLKSOURCE_PLL);
+	} else if (spi1_dt_ker_clk_src == STM32_SRC_PLL3_P) {
+		zassert_equal(spi1_actual_clk_src, RCC_SPI123CLKSOURCE_PLL3,
+				"Expected SPI src: PLLQ (%d). Actual SPI src: %d",
+				spi1_actual_clk_src, RCC_SPI123CLKSOURCE_PLL3);
 	} else {
-		zassert_true(1, "Unexpected clk src(%d)", spi1_dt_clk_kernel_src);
+		zassert_true(1, "Unexpected ker_clk src(%d)", spi1_dt_ker_clk_src);
 	}
 
-	spi1_actual_clk_src = __HAL_RCC_GET_SPI1_SOURCE();
-	zassert_equal(spi1_hal_clk_kernel_src, __HAL_RCC_GET_SPI1_SOURCE(),
-			"Expected SPI src: PLLQ (%d). Actual SPI src: %d",
-			spi1_hal_clk_kernel_src, __HAL_RCC_GET_SPI1_SOURCE());
-
-	/* Test get_rate */
+	/* Test get_rate(ker_clk) */
 	r = clock_control_get_rate(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
 				(clock_control_subsys_t) &spi1_ker_clk_cfg,
 				&spi1_dt_clk_freq);
 	zassert_true((r == 0), "Could not get SPI clk freq");
 
 	spi1_actual_clk_freq = HAL_RCCEx_GetPeriphCLKFreq(RCC_PERIPHCLK_SPI1);
-
 	zassert_equal(spi1_dt_clk_freq, spi1_actual_clk_freq,
 			"Expected SPI clk: (%d). Actual SPI clk: %d",
 			spi1_dt_clk_freq, spi1_actual_clk_freq);
 
-	/* Test clock_off */
+	/* Test clock_off(reg_clk) */
 	r = clock_control_off(DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE),
-				(clock_control_subsys_t) &spi1_clk_cfg);
-	zassert_true((r == 0), "Could not disable SPI Clk");
+				(clock_control_subsys_t) &spi1_reg_clk_cfg);
+	zassert_true((r == 0), "Could not disable SPI reg_clk");
 
-	zassert_true(!__HAL_RCC_SPI1_IS_CLK_ENABLED(), "SPI1 clock should be off");
-	TC_PRINT("SPI1 Clock off\n");
+	zassert_true(!__HAL_RCC_SPI1_IS_CLK_ENABLED(), "SPI1 reg_clk should be off");
+	TC_PRINT("SPI1 reg_clk off\n");
+
+	/* Test clock_off(ker_clk) */
+	/* Not supported today */
 }
 
 void test_main(void)
