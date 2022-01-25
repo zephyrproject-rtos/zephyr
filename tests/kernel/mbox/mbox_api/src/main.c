0184fdcf4848adcc6ce604a20fd890b1d03945da
Andy Ross <andrew.j.ross@intel.com>
2022-01-25 07:59:25 -0800

    test/kernel/mbox: Drop needless _1cpu_ from test cases
    This test is already running with CONFIG_MP_NUM_CPUS=1.  All those
    1cpu declarations are needless.  And some of them (like the init
    tests) have side effects that make it difficult to do things like
    "filter for only MP cases".
    
    (Indeed, this is a heavily MP-unsafe test; almost all cases written to
    rely on ordering between a parent thread and its child.  And that's
    doubly so for COHERENE platforms because lots and lots of the test
    objects are on stacks.  MP_NUM_CPUS=1 is definitely the right thing
    here.)
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- tests/kernel/mbox/mbox_api/src/main.c
+++ tests/kernel/mbox/mbox_api/src/main.c
@@ -35,25 +35,26 @@ extern void test_mbox_get_put_block_data(void);
 void test_main(void)
 {
 	ztest_test_suite(mbox_api,
-			 ztest_1cpu_unit_test(test_mbox_kinit),/*keep init first!*/
-			 ztest_1cpu_unit_test(test_mbox_kdefine),
+			 ztest_unit_test(test_mbox_kinit),/*keep init first!*/
+			 ztest_unit_test(test_mbox_kdefine),
 			 ztest_unit_test(test_enhance_capability),
 			 ztest_unit_test(test_define_multi_mbox),
-			 ztest_1cpu_unit_test(test_mbox_put_get_null),
+			 ztest_unit_test(test_mbox_put_get_null),
 			 ztest_unit_test(test_mbox_put_get_buffer),
-			 ztest_1cpu_unit_test(test_mbox_async_put_get_buffer),
+			 ztest_unit_test(test_mbox_async_put_get_buffer),
 			 ztest_unit_test(test_mbox_target_source_thread_buffer),
 			 ztest_unit_test(test_mbox_incorrect_receiver_tid),
-			 ztest_1cpu_unit_test(test_mbox_incorrect_transmit_tid),
-			 ztest_1cpu_unit_test(test_mbox_timed_out_mbox_get),
+			 ztest_unit_test(test_mbox_incorrect_transmit_tid),
+			 ztest_unit_test(test_mbox_timed_out_mbox_get),
 			 ztest_unit_test(test_mbox_msg_tid_mismatch),
-			 ztest_1cpu_unit_test(test_mbox_dispose_size_0_msg),
-			 ztest_1cpu_unit_test(test_mbox_async_put_to_waiting_get),
+			 ztest_unit_test(test_mbox_dispose_size_0_msg),
+			 ztest_unit_test(test_mbox_async_put_to_waiting_get),
 			 ztest_unit_test(
 				test_mbox_get_waiting_put_incorrect_tid),
-			 ztest_1cpu_unit_test(test_mbox_async_multiple_put),
+			 ztest_unit_test(test_mbox_async_multiple_put),
 			 ztest_unit_test(test_mbox_data_get_null),
 			 ztest_unit_test(test_mbox_get_put_block_data),
-			 ztest_1cpu_unit_test(test_mbox_multiple_waiting_get));
+			 ztest_unit_test(test_mbox_multiple_waiting_get)
+);
 	ztest_run_test_suite(mbox_api);
 }
