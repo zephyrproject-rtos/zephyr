c93efd274bb3195a0092abcba0d5dea1604342c6
Julien Massot <julien.massot@iot.bzh>
2022-01-27 10:12:55 +0100

    arch: arm: cortex_a_r: Add support to start in HYP mode
    The ARMv8-R processors always boot into Hyp mode (EL2)
    
    To enter EL1:
    Program the HACTLR register because it defaults
    to only allowing EL2 accesses. HACTLR controls
    whether EL1 can access memory region registers and CPUACTLR.
    Program the SPSR before entering EL1.
    Other registers default to allowing accesses at EL1 from reset.
    Set VBAR to the correct location for the vector table.
    Set ELR to point to the entry point of the EL1 code and call ERET.
    
    Signed-off-by: Julien Massot <julien.massot@iot.bzh>

--- include/arch/arm/aarch32/cortex_a_r/cpu.h
+++ include/arch/arm/aarch32/cortex_a_r/cpu.h
@@ -21,6 +21,7 @@
 #define MODE_IRQ	0x12
 #define MODE_SVC	0x13
 #define MODE_ABT	0x17
+#define MODE_HYP	0x1a
 #define MODE_UND	0x1b
 #define MODE_SYS	0x1f
 #define MODE_MASK	0x1f
@@ -46,4 +47,29 @@
 #define DFSR_WRITE_MASK		(1 << 11)
 #define DFSR_AXI_SLAVE_MASK	(1 << 12)
 
+/* Armv8-R AArch32 architecture profile */
+#define SCTLR_M_BIT		BIT(0)
+#define SCTLR_A_BIT		BIT(1)
+#define SCTLR_C_BIT		BIT(2)
+#define SCTLR_I_BIT		BIT(12)
+
+/* Hyp System Control Register */
+#define HSCTLR_RES1		(BIT(29) | BIT(28) | BIT(23) | \
+				 BIT(22) | BIT(18) | BIT(16) | \
+				 BIT(11) | BIT(4)  | BIT(3))
+
+/* Hyp Auxiliary Control Register */
+#define HACTLR_CPUACTLR         BIT(0)
+#define HACTLR_CDBGDCI          BIT(1)
+#define HACTLR_FLASHIFREGIONR   BIT(7)
+#define HACTLR_PERIPHPREGIONR   BIT(8)
+#define HACTLR_QOSR_BIT         BIT(9)
+#define HACTLR_BUSTIMEOUTR_BIT  BIT(10)
+#define HACTLR_INTMONR_BIT      BIT(12)
+#define HACTLR_ERR_BIT          BIT(13)
+
+#define HACTLR_INIT (HACTLR_ERR_BIT | HACTLR_INTMONR_BIT | \
+		     HACTLR_BUSTIMEOUTR_BIT | HACTLR_QOSR_BIT | \
+		     HACTLR_PERIPHPREGIONR | HACTLR_FLASHIFREGIONR | \
+		     HACTLR_CDBGDCI | HACTLR_CPUACTLR)
 #endif /* ZEPHYR_INCLUDE_ARCH_ARM_AARCH32_CORTEX_A_R_CPU_H_ */
