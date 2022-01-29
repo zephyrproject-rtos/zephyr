20ff55e234cccea801717723146aa94458348586
Lingao Meng <menglingao@xiaomi.com>
2022-01-29 09:30:12 +0800

    Bluetooth: host: Optimize L2CAP resource usage
    Making sure struct bt_l2cap_chan has absolutely no members related
    to dynamic channels.
    
    That way we ensure that there is no overhead for a build where only
    fixed channels are used.
    
    It's not enough that the dynamic channel-related members are put behind
    ifdefs - they should be completely moved out from the struct definition.
    
    Furthermore, the public l2cap.h header file already has a struct
    that's meant to be used for dynamic channels: struct bt_l2cap_le_chan!
    
    However, currently dynamic channel support is a mess - it's a mix
    between these two structs. The bt_l2cap_le_chan struct should really
    be an extension of the bt_l2cap_chan struct, i.e. the former should
    contain as a member the latter.
    
    Signed-off-by: Lingao Meng <menglingao@xiaomi.com>

--- subsys/bluetooth/host/avdtp.c
+++ subsys/bluetooth/host/avdtp.c
@@ -204,7 +204,7 @@ int bt_avdtp_connect(struct bt_conn *conn, struct bt_avdtp *session)
 	}
 
 	session->br_chan.chan.ops = &ops;
-	session->br_chan.chan.required_sec_level = BT_SECURITY_L2;
+	session->br_chan.required_sec_level = BT_SECURITY_L2;
 
 	return bt_l2cap_chan_connect(conn, &session->br_chan.chan,
 				     BT_L2CAP_PSM_AVDTP);
