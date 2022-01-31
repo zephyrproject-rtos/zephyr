1d61b94eb25b99a90f3911ac5e67a9eaf63783fe
Robert Lubos <robert.lubos@nordicsemi.no>
2022-01-31 16:09:01 +0100

    net: sockets: getaddrinfo: Fix possible crash when callback is delayed
    In case system workqueue processing is delayed for any reason, and
    resolver callback is executed after getaddrinfo() call already timed
    out, the system would crash as the callback makes use of the user data
    allocated on the stack within getaddrinfo() function.
    
    Prevent that, by cancelling the DNS request explicitly from the
    getaddrinfo() context, therefore preventing the resolver callback
    from being executed after the getaddrinfo() call ends.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/net/lib/sockets/getaddrinfo.c
+++ subsys/net/lib/sockets/getaddrinfo.c
@@ -51,6 +51,7 @@ struct getaddrinfo_state {
 	int status;
 	uint16_t idx;
 	uint16_t port;
+	uint16_t dns_id;
 	struct zsock_addrinfo *ai_arr;
 };
 
@@ -112,7 +113,7 @@ static int exec_query(const char *host, int family,
 		qtype = DNS_QUERY_TYPE_AAAA;
 	}
 
-	return dns_get_addr_info(host, qtype, NULL,
+	return dns_get_addr_info(host, qtype, &ai_state->dns_id,
 				 dns_resolve_cb, ai_state,
 				 CONFIG_NET_SOCKETS_DNS_TIMEOUT);
 }
@@ -198,6 +199,7 @@ int z_impl_z_zsock_getaddrinfo_internal(const char *host, const char *service,
 	ai_state.idx = 0U;
 	ai_state.port = htons(port);
 	ai_state.ai_arr = res;
+	ai_state.dns_id = 0;
 	k_sem_init(&ai_state.sem, 0, K_SEM_MAX_LIMIT);
 
 	/* If the family is AF_UNSPEC, then we query IPv4 address first */
@@ -213,6 +215,7 @@ int z_impl_z_zsock_getaddrinfo_internal(const char *host, const char *service,
 				     K_MSEC(CONFIG_NET_SOCKETS_DNS_TIMEOUT +
 					    100));
 		if (ret == -EAGAIN) {
+			(void)dns_cancel_addr_info(ai_state.dns_id);
 			return DNS_EAI_AGAIN;
 		}
 
@@ -241,6 +244,7 @@ int z_impl_z_zsock_getaddrinfo_internal(const char *host, const char *service,
 				&ai_state.sem,
 				K_MSEC(CONFIG_NET_SOCKETS_DNS_TIMEOUT + 100));
 			if (ret == -EAGAIN) {
+				(void)dns_cancel_addr_info(ai_state.dns_id);
 				return DNS_EAI_AGAIN;
 			}
 
