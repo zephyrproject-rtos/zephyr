370881e99dad7a5090c782446ad051db8f5647ce
Alexandre Mergnat <amergnat@baylibre.com>
2022-02-01 13:15:11 +0100

    fixup: barrier: Introduce barriers API
    blabla
    
    Signed-off-by: Alexandre Mergnat <amergnat@baylibre.com>

--- include/barrier.h
+++ include/barrier.h
@@ -17,32 +17,23 @@
 extern "C" {
 #endif
 
+/* Some architectures haven't their own implementation */
+#if !defined(CONFIG_ARCH_HAS_MEMORY_BARRIER) && !defined(__HAS_BUILTIN_MEMORY_BARRIER)
+
 /**
- * @defgroup barrier_apis Barriers API
+ * @defgroup default barrier_apis Barriers API
  * @ingroup kernel_apis
  * @{
  */
 
-#if !defined(nop) || defined(__DOXYGEN__)
 /**
- * @brief nop
- *
- * NOP instruction.
- */
-#define nop()		__asm__ volatile ("nop")
-#endif /* !nop */
-
-#if !defined(isb) || defined(__DOXYGEN__)
-/**
- * @brief isb
+ * @brief isb - Instruction Synchronization Barrier
  *
  * This is used to guarantee that any subsequent instructions are fetched, so
  * that privilege and access are checked with the current MMU configuration.
  */
-#define isb()
-#endif /* !isb */
+#define arch_isb()
 
-#if !defined(mb) || defined(__DOXYGEN__)
 /**
  * @brief mb - Memory Barrier
  *
@@ -50,10 +41,8 @@ extern "C" {
  * instruction stream will be committed before any operations after the mb()
  * are committed.
  */
-#define mb()		compiler_barrier()
-#endif /* !mb */
+#define arch_mb()		compiler_barrier()
 
-#if !defined(rmb) || defined(__DOXYGEN__)
 /**
  * @brief rmb - Read Memory Barrier
  *
@@ -61,10 +50,8 @@ extern "C" {
  * read operations before an rmb() will be committed before any read operations
  * after the rmb().
  */
-#define rmb()		mb()
-#endif /* !rmb */
+#define arch_rmb()		arch_mb()
 
-#if !defined(wmb) || defined(__DOXYGEN__)
 /**
  * @brief wmb - Write Memory Barrier
  *
@@ -72,12 +59,8 @@ extern "C" {
  * write operations before a wmb() will be committed before any write
  * operations after the wmb().
  */
-#define wmb()		mb()
-#endif /* !wmb */
+#define arch_wmb()		arch_mb()
 
-#if defined(CONFIG_SMP) || defined(__DOXYGEN__)
-
-#if !defined(smp_mb) || defined(__DOXYGEN__)
 /**
  * @brief smp_mb - SMP Memory Barrier
  *
@@ -86,42 +69,32 @@ extern "C" {
  * visible to all cores within the SMP system before any accesses after the
  * smp_mb().
  */
-#define smp_mb()	mb()
-#endif /* !smp_mb */
+#define arch_smp_mb()	arch_mb()
 
-#if !defined(smp_rmb) || defined(__DOXYGEN__)
 /**
  * @brief smp_rmb - SMP Read Memory Barrier
  *
  * Like smp_mb(), but only guarantees ordering between read accesses.
  */
-#define smp_rmb()	rmb()
-#endif /* !smp_rmb */
+#define arch_smp_rmb()	arch_rmb()
 
-#if !defined(smp_wmb) || defined(__DOXYGEN__)
 /**
  * @brief smp_wmb - SMP Write Memory Barrier
  *
  * Like smp_mb(), but only guarantees ordering between write accesses.
  */
-#define smp_wmb()	wmb()
-#endif /* !smp_wmb */
+#define arch_smp_wmb()	arch_wmb()
 
-#else /* !CONFIG_SMP */
+#endif /* !CONFIG_ARCH_HAS_MEMORY_BARRIER */
 
-#if !defined(smp_mb)
-#define smp_mb()	compiler_barrier()
-#endif /* !smp_mb */
-
-#if !defined(smp_rmb)
-#define smp_rmb()	compiler_barrier()
-#endif /* !smp_rmb */
-
-#if !defined(smp_wmb)
-#define smp_wmb()	compiler_barrier()
-#endif /* !smp_wmb */
-
-#endif /* CONFIG_SMP */
+#if !defined(CONFIG_SMP)
+#undef  arch_smp_mb
+#undef  arch_smp_rmb
+#undef  arch_smp_wmb
+#define arch_smp_mb()	arch_mb()
+#define arch_smp_rmb()	arch_rmb()
+#define arch_smp_wmb()	arch_wmb()
+#endif /* !CONFIG_SMP */
 
 /**
  * @}
