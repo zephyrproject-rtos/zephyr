b99a51aa36cfc29a3e7547c8ee319e7db5aa623b
Juha Heiskanen <juha.heiskanen@nordicsemi.no>
2022-02-01 02:45:52 -0800

    net: lwm2m: Fix Server Object SSID access
    Server Object SSID should only have Read access.
    LightweightM2M-1.1-int-256 confirmance test validate
    that write operation to SSID should return error.
    Overwrite SSID affect dead block for lwm2m engine and
    only reset will heal.
    
    Fix by adding bootstrap overwrite access for Security and
    Server object when bootstrap is active.
    
    Signed-off-by: Juha Heiskanen <juha.heiskanen@nordicsemi.no>

--- subsys/net/lib/lwm2m/lwm2m_engine.h
+++ subsys/net/lib/lwm2m/lwm2m_engine.h
@@ -104,6 +104,8 @@ int lwm2m_write_handler(struct lwm2m_engine_obj_inst *obj_inst,
 			struct lwm2m_engine_obj_field *obj_field,
 			struct lwm2m_message *msg);
 
+bool lwm2m_engine_bootstrap_override(struct lwm2m_ctx *client_ctx, struct lwm2m_obj_path *path);
+
 int lwm2m_discover_handler(struct lwm2m_message *msg, bool is_bootstrap);
 
 enum coap_block_size lwm2m_default_block_size(void);
