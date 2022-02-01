6a014cebd1999c5e3bd690629d0f01f279615ba7
Daniel Leung <daniel.leung@intel.com>
2022-02-01 12:40:49 -0800

    lib/os: cbprintf_packaged: add ability to use external formatter
    This adds to the cbprintf_packaged library to allow external
    formatters to be used by the way of callback. This will allow
    logging backends to use their own formatter for output if
    needed.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- include/sys/cbprintf.h
+++ include/sys/cbprintf.h
@@ -109,6 +109,27 @@ extern "C" {
  */
 typedef int (*cbprintf_cb)(/* int c, void *ctx */);
 
+/** @brief Signature for a external formatter function identical to cbvprintf.
+ *
+ * This function expects the following parameters:
+ *
+ * @param out the function used to emit each generated character.
+ *
+ * @param ctx a pointer to an object that provides context for the
+ * external formatter.
+ *
+ * @param fmt a standard ISO C format string with characters and
+ * conversion specifications.
+ *
+ * @param ap captured stack arguments corresponding to the conversion
+ * specifications found within @p fmt.
+ *
+ * @return vprintf like return values: the number of characters printed,
+ * or a negative error value returned from external formatter.
+ */
+typedef int (*cbvprintf_exteral_formatter_func)(cbprintf_cb out, void *ctx,
+						const char *fmt, va_list ap);
+
 /** @brief Determine if string must be packaged in run time.
  *
  * Static packaging can be applied if size of the package can be determined
@@ -286,11 +307,14 @@ int cbprintf_fsc_package(void *in_packaged,
 			 size_t len);
 
 /** @brief Generate the output for a previously captured format
- * operation.
+ * operation using an external formatter.
  *
  * @param out the function used to emit each generated character.
  *
- * @param ctx context provided when invoking out
+ * @param formatter external formatter function.
+ *
+ * @param ctx a pointer to an object that provides context for the
+ * external formatter.
  *
  * @param packaged the data required to generate the formatted output, as
  * captured by cbprintf_package() or cbvprintf_package(). The alignment
@@ -299,12 +323,13 @@ int cbprintf_fsc_package(void *in_packaged,
  * @note Memory indicated by @p packaged will be modified in a non-destructive
  * way, meaning that it could still be reused with this function again.
  *
- * @return the number of characters printed, or a negative error value
- * returned from invoking @p out.
+ * @return printf like return values: the number of characters printed,
+ * or a negative error value returned from external formatter.
  */
-int cbpprintf(cbprintf_cb out,
-	      void *ctx,
-	      void *packaged);
+int cbpprintf_external(cbprintf_cb out,
+		       cbvprintf_exteral_formatter_func formatter,
+		       void *ctx,
+		       void *packaged);
 
 /** @brief *printf-like output through a callback.
  *
@@ -361,6 +386,29 @@ int cbprintf(cbprintf_cb out, void *ctx, const char *format, ...);
  * returned from invoking @p out.
  */
 int cbvprintf(cbprintf_cb out, void *ctx, const char *format, va_list ap);
+
+/** @brief Generate the output for a previously captured format
+ * operation.
+ *
+ * @param out the function used to emit each generated character.
+ *
+ * @param ctx context provided when invoking out
+ *
+ * @param packaged the data required to generate the formatted output, as
+ * captured by cbprintf_package() or cbvprintf_package(). The alignment
+ * requirement on this data is the same as when it was initially created.
+ *
+ * @note Memory indicated by @p packaged will be modified in a non-destructive
+ * way, meaning that it could still be reused with this function again.
+ *
+ * @return the number of characters printed, or a negative error value
+ * returned from invoking @p out.
+ */
+static inline
+int cbpprintf(cbprintf_cb out, void *ctx, void *packaged)
+{
+	return cbpprintf_external(out, cbvprintf, ctx, packaged);
+}
 
 #ifdef CONFIG_CBPRINTF_LIBC_SUBSTS
 
