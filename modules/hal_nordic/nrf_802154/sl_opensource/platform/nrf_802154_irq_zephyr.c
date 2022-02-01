fa80b20cd575277669a6bdabe03f9e993df2d6b7
Andrzej Kuros <andrzej.kuros@nordicsemi.no>
2022-02-01 12:40:37 +0100

    [nrf fromlist] modules: hal_nordic: nrf_802154_irq ZLI support
    Parameter `prio` of `nrf_802154_irq_init` is now allowed to have
    negative value what means use of ZLI priority or highest possible
    priority if ZLIs are disabled.
    
    Zephyr PR: https://github.com/zephyrproject-rtos/zephyr/pull/42355
    
    Signed-off-by: Andrzej Kuros <andrzej.kuros@nordicsemi.no>
    (cherry picked from commit d143ae93cabf2979b4bc3d049145e108a7d7adcf)

--- modules/hal_nordic/nrf_802154/sl_opensource/platform/nrf_802154_irq_zephyr.c
+++ modules/hal_nordic/nrf_802154/sl_opensource/platform/nrf_802154_irq_zephyr.c
@@ -9,9 +9,16 @@
 #include <irq.h>
 #include <nrfx.h>
 
-void nrf_802154_irq_init(uint32_t irqn, uint32_t prio, nrf_802154_isr_t isr)
+void nrf_802154_irq_init(uint32_t irqn, int32_t prio, nrf_802154_isr_t isr)
 {
-	irq_connect_dynamic(irqn, prio, isr, NULL, 0);
+	uint32_t flags = 0U;
+
+	if (prio < 0) {
+		prio = 0;
+		flags |= IRQ_ZERO_LATENCY;
+	}
+
+	irq_connect_dynamic(irqn, prio, isr, NULL, flags);
 }
 
 void nrf_802154_irq_enable(uint32_t irqn)
