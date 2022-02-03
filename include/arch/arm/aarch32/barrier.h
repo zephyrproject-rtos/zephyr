02517b40436db6c040df372a804050f89441757c
Alexandre Mergnat <amergnat@baylibre.com>
2022-02-03 19:10:00 +0100

    fixup: barrier: Hook up the barrier API to the archs
    Signed-off-by: Alexandre Mergnat <amergnat@baylibre.com>

--- include/arch/arm/aarch32/barrier.h
+++ include/arch/arm/aarch32/barrier.h
@@ -9,40 +9,55 @@
 
 #ifndef _ASMLANGUAGE
 
-#ifndef CONFIG_HAS_BUILTIN_MEMORY_BARRIER
 /* Fallback on CMSIS */
 #if defined(CONFIG_CPU_CORTEX_M)
 #include <arch/arm/aarch32/cortex_m/cmsis.h>
 #elif defined(CONFIG_CPU_CORTEX_R) || defined(CONFIG_CPU_AARCH32_CORTEX_A)
 #include <arch/arm/aarch32/cortex_a_r/cmsis.h>
 #endif /* CONFIG_CPU_CORTEX_M | CONFIG_CPU_CORTEX_R | CONFIG_CPU_CORTEX_A */
-#endif /* !CONFIG_HAS_BUILTIN_MEMORY_BARRIER */
 
-#define arch_isb()		__ISB()
 
+#if defined(__BUILTIN_ISB)
+#define arch_isb() __BUILTIN_ISB()
+#else
+#define arch_isb() __ISB()
+#endif /* __BUILTIN_ISB */
+
+#if defined(__BUILTIN_DSB)
+#define arch_mb()		__BUILTIN_DSB()
+#define arch_rmb()		__BUILTIN_DSB()
+#define arch_wmb()		__BUILTIN_DSB()
+#else
 #define arch_mb()		__DSB()
-
 #define arch_rmb()		__DSB()
-
 #define arch_wmb()		__DSB()
+#endif /* __BUILTIN_DSB */
 
 #if defined(CONFIG_SMP)
 
+#if defined(__BUILTIN_DMB)
+#define arch_smp_mb()	__BUILTIN_DMB()
+#define arch_smp_rmb()	__BUILTIN_DMB()
+#define arch_smp_wmb()	__BUILTIN_DMB()
+#else
 #define arch_smp_mb()	__DMB()
-
 #define arch_smp_rmb()	__DMB()
-
 #define arch_smp_wmb()	__DMB()
+#endif /* __BUILTIN_DMB */
+
 
 #else /* !CONFIG_SMP */
 
 #define arch_smp_mb()    arch_mb()
-
 #define arch_smp_rmb()   arch_rmb()
-
 #define arch_smp_wmb()   arch_wmb()
 
 #endif /* CONFIG_SMP */
+
+
+#undef tmp_isb
+#undef tmp_dsb
+#undef tmp_dmb
 
 #endif /* !_ASMLANGUAGE */
 
