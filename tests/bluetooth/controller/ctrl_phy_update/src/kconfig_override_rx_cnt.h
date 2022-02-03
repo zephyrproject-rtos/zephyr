fe513fc159a1ebd7b614f6c4cb26496542f47daf
Andries Kruithof <Andries.Kruithof@nordicsemi.no>
2022-02-03 08:52:11 +0100

    Bluetooth: controller: llcp: serialize phy update notifications
    The PHY update procedure may trigger a DLE update in certain
    situations
    When a limited number of rx buffers is available there may not be
    enough for doing the phy update and dle update notifications
    at once.
    
    Fixes #41788
    
    Signed-off-by: Andries Kruithof <Andries.Kruithof@nordicsemi.no>

---
+++ tests/bluetooth/controller/ctrl_phy_update/src/kconfig_override_rx_cnt.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021 Demant
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifdef CONFIG_BT_CTLR_LLCP_CONN
+#undef CONFIG_BT_CTLR_LLCP_CONN
+#define CONFIG_BT_CTLR_LLCP_CONN 2
+#endif
