a807fff0850a9484b2eccd474eefa452278e0973
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2022-02-04 10:14:08 +0100

    arch/x86: Add a CPUID function to get initial APIC ID
    Depending on whether X2APIC is enabled or not, it will be safer to grab
    such ID from the right place.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

--- include/arch/x86/cpuid.h
+++ include/arch/x86/cpuid.h
@@ -11,13 +11,18 @@
 extern "C" {
 #endif
 
-#define CPUID_EXTENDED_FEATURES_LVL 7
+#define CPUID_BASIC_INFO_1			0x01
+#define CPUID_EXTENDED_FEATURES_LVL		0x07
+#define CPUID_EXTENDED_TOPOLOGY_ENUMERATION	0x0B
+#define CPUID_EXTENDED_TOPOLOGY_ENUMERATION_V2	0x1F
 
 /* Bits to check in CPUID extended features */
 #define CPUID_SPEC_CTRL_SSBD	BIT(31)
 #define CPUID_SPEC_CTRL_IBRS	BIT(26)
 
 uint32_t z_x86_cpuid_extended_features(void);
+
+uint8_t z_x86_cpuid_get_current_physical_apic_id(void);
 
 #ifdef __cplusplus
 }
