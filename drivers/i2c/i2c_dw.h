6d5ec5a0d533716acdad5168557669ddd9c2c141
Andrei-Edward Popa <andrei_edward.popa@upb.ro>
2022-02-06 18:32:20 +0200

    drivers: i2c: added slave support for DW
    added slave mode support for I2C designware chip
    
    Signed-off-by: Andrei-Edward Popa <andrei_edward.popa@upb.ro>

--- drivers/i2c/i2c_dw.h
+++ drivers/i2c/i2c_dw.h
@@ -118,6 +118,8 @@ struct i2c_dw_dev_config {
 	uint8_t			request_bytes;
 	uint8_t			xfr_flags;
 	bool			support_hs_mode;
+
+	struct i2c_slave_config *slave_cfg;
 };
 
 #define Z_REG_READ(__sz) sys_read##__sz
