741fe73f740ee976f81f1611a1edd0b131eb3305
Herman Berget <herman.berget@nordicsemi.no>
2022-02-08 16:03:42 +0100

    Bluetooth: Host: Add option to use GATT over EATT
    A channel option field has been added to the bt_gatt_*_params structs.
    This allows the application to choose wether Unenhanced ATT, Enhanced
    ATT, or Any ATT channel shall be used for the
    request/command/indication/notification.
    
    Signed-off-by: Herman Berget <herman.berget@nordicsemi.no>

--- subsys/bluetooth/host/att_internal.h
+++ subsys/bluetooth/host/att_internal.h
@@ -317,12 +317,18 @@ void bt_att_clear_out_of_sync_sent(struct bt_conn *conn);
 /* Check if BT_ATT_ERR_DB_OUT_OF_SYNC has been sent on the fixed ATT channel */
 bool bt_att_out_of_sync_sent_on_fixed(struct bt_conn *conn);
 
-
 typedef void (*bt_gatt_complete_func_t) (struct bt_conn *conn, void *user_data);
-void bt_att_set_tx_meta_data(struct net_buf *buf, bt_gatt_complete_func_t func, void *user_data);
+void bt_att_set_tx_meta_data(struct net_buf *buf, bt_gatt_complete_func_t func, void *user_data,
+			     enum bt_att_chan_opt chan_opt);
 void bt_att_increment_tx_meta_data_attr_count(struct net_buf *buf, uint16_t attr_count);
 
 bool bt_att_tx_meta_data_match(const struct net_buf *buf, bt_gatt_complete_func_t func,
-			       const void *user_data);
+			       const void *user_data, enum bt_att_chan_opt chan_opt);
 
 void bt_att_free_tx_meta_data(const struct net_buf *buf);
+
+#if defined(CONFIG_BT_EATT)
+#define BT_ATT_CHAN_OPT(_params) (_params)->chan_opt
+#else
+#define BT_ATT_CHAN_OPT(_params) BT_ATT_CHAN_OPT_UNENHANCED_ONLY
+#endif /* CONFIG_BT_EATT */
