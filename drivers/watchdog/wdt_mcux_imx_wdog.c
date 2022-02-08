41a63f48fdc81636b1ed875c574d27aaf0b09bc9
Daniel DeGrasse <daniel.degrasse@nxp.com>
2022-02-08 11:17:12 -0600

    samples: task_wdt: Increase min watchdog timeout
    MCUX watchdog timer supports min window of 500ms. Increase maximum task
    watchdog timeout window to 500ms by default, so that this sample can run
    on iMX RT SOCs. Also update iMX RT watchdog driver to reject timeout
    maximums under 500ms with a useful error message
    
    Fixes #40153
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- drivers/watchdog/wdt_mcux_imx_wdog.c
+++ drivers/watchdog/wdt_mcux_imx_wdog.c
@@ -13,7 +13,7 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(wdt_mcux_wdog);
 
-#define WDOG_TMOUT_SEC(x)  ((x * 2 / MSEC_PER_SEC) - 1)
+#define WDOG_TMOUT_SEC(x)  (((x * 2) / MSEC_PER_SEC) - 1)
 
 struct mcux_wdog_config {
 	WDOG_Type *base;
@@ -74,6 +74,11 @@ static int mcux_wdog_install_timeout(const struct device *dev,
 
 	WDOG_GetDefaultConfig(&data->wdog_config);
 	data->wdog_config.interruptTimeValue = 0U;
+
+	if (cfg->window.max < (MSEC_PER_SEC / 2)) {
+		LOG_ERR("Invalid window max, shortest window is 500ms");
+		return -EINVAL;
+	}
 
 	data->wdog_config.timeoutValue =
 		  WDOG_TMOUT_SEC(cfg->window.max);
