33c3d3db7cfb5e32c17f9ad6a546a4d489799abf
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2022-02-11 12:36:33 +0530

    samples: Bluetooth: Use custom interval for Periodic Advertising
    Use custom interval for Periodic Advertising.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- samples/bluetooth/periodic_adv/src/main.c
+++ samples/bluetooth/periodic_adv/src/main.c
@@ -6,10 +6,22 @@
 
 #include <bluetooth/bluetooth.h>
 
+#define BT_LE_EXT_ADV_CUSTOM \
+		BT_LE_ADV_PARAM(BT_LE_ADV_OPT_EXT_ADV | \
+				BT_LE_ADV_OPT_USE_NAME, \
+				0x20, \
+				0x20, \
+				NULL)
+
+#define BT_LE_PER_ADV_CUSTOM  \
+		BT_LE_PER_ADV_PARAM(0x10, \
+				    0x10, \
+				    BT_LE_PER_ADV_OPT_NONE)
+
 static uint8_t mfg_data[] = { 0xff, 0xff, 0x00 };
 
 static const struct bt_data ad[] = {
-	BT_DATA(BT_DATA_MANUFACTURER_DATA, mfg_data, 3),
+	BT_DATA(BT_DATA_MANUFACTURER_DATA, mfg_data, ARRAY_SIZE(mfg_data)),
 };
 
 void main(void)
@@ -27,14 +39,14 @@ void main(void)
 	}
 
 	/* Create a non-connectable non-scannable advertising set */
-	err = bt_le_ext_adv_create(BT_LE_EXT_ADV_NCONN_NAME, NULL, &adv);
+	err = bt_le_ext_adv_create(BT_LE_EXT_ADV_CUSTOM, NULL, &adv);
 	if (err) {
 		printk("Failed to create advertising set (err %d)\n", err);
 		return;
 	}
 
 	/* Set periodic advertising parameters */
-	err = bt_le_per_adv_set_param(adv, BT_LE_PER_ADV_DEFAULT);
+	err = bt_le_per_adv_set_param(adv, BT_LE_PER_ADV_CUSTOM);
 	if (err) {
 		printk("Failed to set periodic advertising parameters"
 		       " (err %d)\n", err);
