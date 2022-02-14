195bde0ffaa7bdaf01beea8b07e02b5fd83a20f0
Andy Ross <andrew.j.ross@intel.com>
2022-02-14 14:30:34 -0800

    arch: Add IRQ_OFFSET_NESTED feature
    The x86 and xtensa implementations of irq_offload() invoke synchronous
    interrupts on the local CPU, and are therefore safe to use from within
    an interrupt context.  This is a cheap and portable way to exercise
    nested interrupts, which are otherwise highly platform-dependent to
    test.  Add a kconfig to signal the capability.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- kernel/thread.c
+++ kernel/thread.c
@@ -890,9 +890,13 @@ K_SEM_DEFINE(offload_sem, 1, 1);
 
 void irq_offload(irq_offload_routine_t routine, const void *parameter)
 {
+#ifdef CONFIG_IRQ_OFFLOAD_NESTED
+	arch_irq_offload(routine, parameter);
+#else
 	k_sem_take(&offload_sem, K_FOREVER);
 	arch_irq_offload(routine, parameter);
 	k_sem_give(&offload_sem);
+#endif
 }
 #endif
 
