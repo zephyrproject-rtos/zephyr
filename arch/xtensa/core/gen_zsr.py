baa169276769a25693de27841d81332180ddfa1f
Andy Ross <andrew.j.ross@intel.com>
2022-02-14 13:13:07 -0800

    arch/xtensa: Rework irq_offload: automatic config, SMP-safe
    The Xtensa implementation of arch_irq_offload() required that the user
    select the correct interrupt manually, and would race with itself if
    invoked from separate CPUs (it was saved here by the main
    irq_offload() function which has a semaphore to serialize access).
    
    Use the new gen_zsr.py script to automatically detect the highest
    available software interrupt, and keep a per-CPU set of
    callback/parameter pointers.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- arch/xtensa/core/gen_zsr.py
+++ arch/xtensa/core/gen_zsr.py
@@ -37,6 +37,22 @@ for il in range(1, 1 + int(get("XCHAL_NUM_INTLEVELS"))):
     if il != int(get("XCHAL_DEBUGLEVEL")):
         maxint = max(il, maxint)
 
+# Find the highest priority software interrupt.  We'll use that for
+# arch_irq_offload().
+irqoff_level = -1
+irqoff_int = -1
+for sym, val in syms.items():
+    if val == "XTHAL_INTTYPE_SOFTWARE":
+        m = re.match(r"XCHAL_INT(\d+)_TYPE", sym)
+        if m:
+            intnum = int(m.group(1))
+            levelsym = f"XCHAL_INT{intnum}_LEVEL"
+            if levelsym in syms:
+                intlevel = int(syms[levelsym])
+                if intlevel > irqoff_level:
+                    irqoff_int = intnum
+                    irqoff_level = intlevel
+
 # Now emit our output header with the assignments we chose
 with open(outfile, "w") as f:
     f.write("/* Generated File, see gen_zsr.py */\n")
@@ -54,4 +70,9 @@ with open(outfile, "w") as f:
     f.write(f"# define ZSR_RFI_LEVEL {maxint}\n")
     f.write(f"# define ZSR_EPC EPC{maxint}\n")
     f.write(f"# define ZSR_EPS EPS{maxint}\n")
+
+    # And the irq offset interrupt
+    if irqoff_int >= 0:
+        f.write(f"# define ZSR_IRQ_OFFLOAD_INT {irqoff_int}\n")
+
     f.write("#endif\n")
