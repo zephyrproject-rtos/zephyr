3da4c7bc589e6d4aa2afb644d204c0f1ba9d4117
Chris Trowbridge <chris.trowbridge@lairdconnect.com>
2022-02-15 17:08:47 -0500

    drivers: ipm: Add support for NXP i.MX rev2 MU
    With a number of the i.MX SoCs (e.g., the i.MX8M Plus), NXP has moved to
    supporting the integrated Cortex-M cores with the MCUXpresso SDK (MCUX).
    As a result, certain Zephyr drivers (such as the IPM driver) need to be
    updated to utlize this new MCUX-based SDK.
    
    This change adds support for the MCUX Messaging Unit driver pulled in by
    this PR:
    https://github.com/zephyrproject-rtos/hal_nxp/pull/130
    
    Additionally, this change enables the new IPM_IMX_REV2 config for the
    mimx8ml8_m7 SoC target which utilizes this new revision of the driver.
    
    Signed-off-by: Chris Trowbridge <chris.trowbridge@lairdconnect.com>

--- drivers/ipm/ipm_imx.c
+++ drivers/ipm/ipm_imx.c
@@ -146,7 +146,11 @@ static void imx_mu_isr(const struct device *dev)
 
 	/* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F
 	 * Store immediate overlapping exception return operation
-	 * might vector to incorrect interrupt
+	 * might vector to incorrect interrupt. For Cortex-M7, if
+	 * core speed much faster than peripheral register write
+	 * speed, the peripheral interrupt flags may be still set
+	 * after exiting ISR, this results to the same error similar
+	 * with errata 838869.
 	 */
 #if (defined __CORTEX_M) && ((__CORTEX_M == 4U) || (__CORTEX_M == 7U))
 	__DSB();
