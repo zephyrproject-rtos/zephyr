f32f047b1ed8fc5f96495bb79446f2330f0df189
Carles Cufi <carles.cufi@nordicsemi.no>
2022-02-17 17:07:03 +0100

    kconfig: Rename the ZTEST stack size option to align with the rest
    All stack sizes should end with STACK_SIZE.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- subsys/testsuite/ztest/src/ztest_new.c
+++ subsys/testsuite/ztest/src/ztest_new.c
@@ -317,7 +317,7 @@ out:
 #define FAIL_FAST 0
 #endif
 
-K_THREAD_STACK_DEFINE(ztest_thread_stack, CONFIG_ZTEST_STACKSIZE + CONFIG_TEST_EXTRA_STACKSIZE);
+K_THREAD_STACK_DEFINE(ztest_thread_stack, CONFIG_ZTEST_STACK_SIZE + CONFIG_TEST_EXTRA_STACKSIZE);
 static ZTEST_BMEM int test_result;
 
 static void test_finalize(void)
