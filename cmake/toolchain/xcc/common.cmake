1358f02d5828020dee29f30628b896395b3cf507
Ederson de Souza <ederson.desouza@intel.com>
2022-02-17 15:32:27 -0800

    cmake: Add xcc-clang toolchain
    Instead of setting XCC_USE_CLANG=1, this patch adds xcc-clang toolchain
    that is basically xcc using the clang compiler.
    
    Initially, the new toolchain simply includes files from current xcc
    toolchain and (re)sets some variables. This should be a more scalable
    approach to diverge the toolchains in the future than placing
    "if($ENV{XCC_USE_CLANG})" at several places.
    
    It should also help to filter tests that run (or not) exclusively with
    the clang variant of XCC on twister.
    
    The XCC_USE_CLANG flag is documented as deprecated, and a message is
    emitted during build if still in use. Its new behaviour is to instruct
    Zephyr to use `xcc-clang` toolchain.
    
    Signed-off-by: Ederson de Souza <ederson.desouza@intel.com>

---
+++ cmake/toolchain/xcc/common.cmake
@@ -0,0 +1,26 @@
+# SPDX-License-Identifier: Apache-2.0
+
+set_ifndef(XTENSA_TOOLCHAIN_PATH "$ENV{XTENSA_TOOLCHAIN_PATH}")
+set(       XTENSA_TOOLCHAIN_PATH ${XTENSA_TOOLCHAIN_PATH} CACHE PATH "xtensa tools install directory")
+assert(    XTENSA_TOOLCHAIN_PATH "XTENSA_TOOLCHAIN_PATH is not set")
+
+if(NOT EXISTS ${XTENSA_TOOLCHAIN_PATH})
+  message(FATAL_ERROR "Nothing found at XTENSA_TOOLCHAIN_PATH: '${XTENSA_TOOLCHAIN_PATH}'")
+endif()
+
+set(TOOLCHAIN_HOME ${XTENSA_TOOLCHAIN_PATH}/$ENV{TOOLCHAIN_VER}/XtensaTools)
+
+set(LINKER ld)
+set(BINTOOLS gnu)
+
+set(CROSS_COMPILE_TARGET xt)
+set(SYSROOT_TARGET       xtensa-elf)
+
+set(CROSS_COMPILE  ${TOOLCHAIN_HOME}/bin/${CROSS_COMPILE_TARGET}-)
+set(SYSROOT_DIR    ${TOOLCHAIN_HOME}/${SYSROOT_TARGET})
+
+set(NOSYSDEF_CFLAG "")
+
+list(APPEND TOOLCHAIN_C_FLAGS -fms-extensions)
+
+set(TOOLCHAIN_HAS_NEWLIB OFF CACHE BOOL "True if toolchain supports newlib")
