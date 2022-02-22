659f23c01f00b639acf9496868ad4ab6d073fa9e
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-02-22 20:22:35 +0100

    device: remove deprecated SYS_DEVICE_DEFINE
    The macro was deprecated in favor of SYS_INIT.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- include/zephyr/device.h
+++ include/zephyr/device.h
@@ -95,21 +95,6 @@ typedef int16_t device_handle_t;
  */
 #define DEVICE_NAME_GET(name) _CONCAT(__device_, name)
 
-/**
- * @def SYS_DEVICE_DEFINE
- *
- * @brief Run an initialization function at boot at specified priority.
- *
- * @deprecated Use SYS_INIT() instead.
- *
- * @param drv_name A string name for the pseudo-device (unused).
- * @param init_fn Pointer to the function which should run at boot time.
- * @param level Initialization level to run the function in.
- * @param prio Function's priority within its initialization level.
- */
-#define SYS_DEVICE_DEFINE(drv_name, init_fn, level, prio)		\
-	__DEPRECATED_MACRO SYS_INIT(init_fn, level, prio)
-
 /* Node paths can exceed the maximum size supported by device_get_binding() in user mode,
  * so synthesize a unique dev_name from the devicetree node.
  *
