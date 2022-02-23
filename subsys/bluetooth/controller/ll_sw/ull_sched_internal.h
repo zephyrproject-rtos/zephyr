adca553f04601b343094497abdc59d4f3d6462dc
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2022-02-23 11:16:05 +0530

    Bluetooth: Controller: Rename use of mstr word to cen for Central
    Rename the use of mstr word in advanced scheduling
    implementation to cen as a representation for Central
    role.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_sched_internal.h
+++ subsys/bluetooth/controller/ll_sw/ull_sched_internal.h
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-int ull_sched_after_mstr_slot_get(uint8_t user_id, uint32_t ticks_slot_abs,
-				  uint32_t *ticks_anchor, uint32_t *us_offset);
+int ull_sched_after_cen_slot_get(uint8_t user_id, uint32_t ticks_slot_abs,
+				 uint32_t *ticks_anchor, uint32_t *us_offset);
 void ull_sched_mfy_win_offset_use(void *param);
 void ull_sched_mfy_free_win_offset_calc(void *param);
 void ull_sched_mfy_win_offset_select(void *param);
