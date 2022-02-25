9affc0153c2272f1f7c19af83140bc9866074291
Herman Berget <herman.berget@nordicsemi.no>
2022-02-25 11:29:05 +0100

    Bluetooth: Host: Add test for autoconnection of EATT channels
    Checks that CONFIG_BT_EATT_MAX EATT channels are created automatically
    after connection.
    
    Signed-off-by: Herman Berget <herman.berget@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_eatt/src/main.c
+++ tests/bluetooth/bsim_bt/bsim_test_eatt/src/main.c
@@ -7,10 +7,12 @@
 #include "bstests.h"
 
 extern struct bst_test_list *test_main_collision_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_main_autoconnect_install(struct bst_test_list *tests);
 
 bst_test_install_t test_installers[] = {
 	test_main_collision_install,
-	NULL
+	test_main_autoconnect_install,
+	NULL,
 };
 
 void main(void)
