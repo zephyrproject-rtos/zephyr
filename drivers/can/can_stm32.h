6fa9cbe6bbbd257a87e73930c5e814f2a58b6b7d
Henrik Brix Andersen <hebad@vestas.com>
2022-02-25 13:49:01 +0100

    drivers: can: stm32: only call state change callback if state is changed
    Only call the state change callback if the state has changed. Reuse the
    existing function for retrieving the CAN controller state instead of
    having the same code twice.
    
    Signed-off-by: Henrik Brix Andersen <hebad@vestas.com>

--- drivers/can/can_stm32.h
+++ drivers/can/can_stm32.h
@@ -62,6 +62,7 @@ struct can_stm32_data {
 	void *cb_arg[CONFIG_CAN_MAX_FILTER];
 	can_state_change_callback_t state_change_cb;
 	void *state_change_cb_data;
+	enum can_state state;
 };
 
 struct can_stm32_config {
