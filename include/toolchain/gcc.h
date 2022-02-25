fb92a654e30602d843eb006892f047e8fd4316bc
Daniel Leung <daniel.leung@intel.com>
2022-02-25 14:09:18 -0800

    toolchain: introduce macro TOOLCHAIN_HAS_C_AUTO_TYPE
    This introduces the macro TOOLCHAIN_HAS_C_AUTO_TYPE to
    indicate that the toolchain supports __auto_type. This is
    supported since GCC 4.9.0 and Clang 3.8.0.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- include/toolchain/gcc.h
+++ include/toolchain/gcc.h
@@ -26,6 +26,10 @@
 #define TOOLCHAIN_HAS_C_GENERIC 1
 #endif
 
+#if !defined(TOOLCHAIN_HAS_C_AUTO_TYPE) && (GCC_VERSION >= 40900)
+#define TOOLCHAIN_HAS_C_AUTO_TYPE 1
+#endif
+
 /*
  * Older versions of GCC do not define __BYTE_ORDER__, so it must be manually
  * detected and defined using arch-specific definitions.
--- include/toolchain/llvm.h
+++ include/toolchain/llvm.h
@@ -22,6 +22,7 @@
 
 #if CLANG_VERSION >= 30800
 #define TOOLCHAIN_HAS_C_GENERIC 1
+#define TOOLCHAIN_HAS_C_AUTO_TYPE 1
 #endif
 
 #include <toolchain/gcc.h>
