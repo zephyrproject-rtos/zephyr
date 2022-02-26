a78e61dc9a6f3d9c7b425b716e37e768229df278
Yonatan Schachter <yonatan.schachter@gmail.com>
2022-02-26 19:25:03 +0200

    soc: rpi_pico: Added panic handler
    Some pico-sdk drivers call a panic function, originally implemented
    as part of the Pico's C runtime. This commit adds a Zephyr compatible
    implementation of panic, so that those drivers could be compiled with
    Zephyr.
    
    Signed-off-by: Yonatan Schachter <yonatan.schachter@gmail.com>

--- soc/arm/rpi_pico/rp2/soc.c
+++ soc/arm/rpi_pico/rp2/soc.c
@@ -13,9 +13,12 @@
  * for the Raspberry Pi RP2040 family processor.
  */
 
+#include <stdio.h>
+
 #include <zephyr/kernel.h>
 #include <zephyr/init.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/fatal.h>
 
 #include <hardware/regs/resets.h>
 #include <hardware/clocks.h>
@@ -59,6 +62,20 @@ static int rp2040_init(const struct device *arg)
 	irq_unlock(key);
 
 	return 0;
+}
+
+/*
+ * Some pico-sdk drivers call panic on fatal error.
+ * This alternative implementation of panic handles the panic
+ * through Zephyr.
+ */
+void __attribute__((noreturn)) panic(const char *fmt, ...)
+{
+	va_list args;
+
+	va_start(args, fmt);
+	vprintf(fmt, args);
+	k_fatal_halt(K_ERR_CPU_EXCEPTION);
 }
 
 SYS_INIT(rp2040_init, PRE_KERNEL_1, 0);
