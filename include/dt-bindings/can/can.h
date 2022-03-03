3d6a7d3a9449ef8b74140a35adf6f9f1235657b7
YuLong Yao <feilongphone@gmail.com>
2022-03-03 20:07:09 +0800

    drivers: can: add iso and non-ISO type to can.h
    add iso and non-ISO type to can.h
    
    Signed-off-by: YuLong Yao <feilongphone@gmail.com>

---
+++ include/dt-bindings/can/can.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2022 YuLong Yao <feilongphone@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_CAN_CAN_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_CAN_CAN_H_
+
+/* CAN-FD STANDARD: ISO/BOSCH(Non-ISO) */
+#define CAN_FD_STANDARD_ISO 0
+#define CAN_FD_STANDARD_BOSCH 1
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CAN_CAN_H_ */
