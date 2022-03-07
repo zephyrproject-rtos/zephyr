638bf4aa8fd9d6d22c3050e794ea2c2f0ee7dc89
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-03-07 14:55:57 +0100

    soc: arm: bl5340_dvk: Fix bl5340 DVK pin assignments
    Fix the BL5340 DVK pin assignment using the nrf5340 DK pin.
    Regression from: e4260ac03fa6e2f1bc3770826e6dcb54021cab42.
    
    This also aligns the BL5340 DVK to use the GPIO forward module.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- boards/arm/bl5340_dvk/bl5340_dvk_cpunet_reset.c
+++ boards/arm/bl5340_dvk/bl5340_dvk_cpunet_reset.c
@@ -10,29 +10,11 @@
 #include <logging/log.h>
 
 #include <soc.h>
-#include <soc_secure.h>
 
 LOG_MODULE_REGISTER(bl5340_dvk_cpuapp, CONFIG_LOG_DEFAULT_LEVEL);
 
-/* TODO: This should come from DTS, possibly an overlay. */
-#define CPUNET_UARTE_PIN_TX 33
-#define CPUNET_UARTE_PIN_RX 32
-#define CPUNET_UARTE_PIN_RTS 11
-#define CPUNET_UARTE_PIN_CTS 10
-
 static void remoteproc_mgr_config(void)
 {
-#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) || defined(CONFIG_BUILD_WITH_TFM)
-	/* UARTE */
-	/* Assign specific GPIOs that will be used to get UARTE from
-	 * nRF5340 Network MCU.
-	 */
-	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_TX, NRF_GPIO_PIN_MCUSEL_NETWORK);
-	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_RX, NRF_GPIO_PIN_MCUSEL_NETWORK);
-	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_RTS, NRF_GPIO_PIN_MCUSEL_NETWORK);
-	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_CTS, NRF_GPIO_PIN_MCUSEL_NETWORK);
-#endif /* !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) || defined(CONFIG_BUILD_WITH_TFM) */
-
 #if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 	/* Retain nRF5340 Network MCU in Secure domain (bus
 	 * accesses by Network MCU will have Secure attribute set).
