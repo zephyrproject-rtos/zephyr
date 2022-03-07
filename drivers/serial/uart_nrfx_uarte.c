e519a42069e84e45d7f50ad65f523b4e4b97d79c
Yegor Yefremov <yegorslists@googlemail.com>
2022-03-07 22:53:16 +0100

    drivers: serial: fix typos
    Typos were found with codespell utility.
    
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>

--- drivers/serial/uart_nrfx_uarte.c
+++ drivers/serial/uart_nrfx_uarte.c
@@ -1077,7 +1077,7 @@ static void rx_timeout(struct k_timer *timer)
 	}
 
 	/* Check for current buffer being full.
-	 * if the UART receives characters before the the ENDRX is handled
+	 * if the UART receives characters before the ENDRX is handled
 	 * and the 'next' buffer is set up, then the SHORT between ENDRX and
 	 * STARTRX will mean that data will be going into to the 'next' buffer
 	 * until the ENDRX event gets a chance to be handled.
@@ -1102,7 +1102,7 @@ static void rx_timeout(struct k_timer *timer)
 				data->async->rx_timeout_slab;
 		}
 
-		/* If theres nothing left to report until the buffers are
+		/* If there's nothing left to report until the buffers are
 		 * switched then the timer can be stopped
 		 */
 		if (clipped) {
