2952b40cc00ede21fc3b785a8834b0af43141041
Nicolas Pitre <npitre@baylibre.com>
2022-03-07 13:24:38 -0500

    riscv: irq_offload: simpler implementation
    Get rid of all those global variables and IRQ locking.
    Use the regular IRQ exit path to let tests validate preemption properly.
    
    Signed-off-by: Nicolas Pitre <npitre@baylibre.com>

--- arch/riscv/core/irq_offload.c
+++ arch/riscv/core/irq_offload.c
@@ -1,31 +1,13 @@
 /*
- * Copyright (c) 2016 Jean-Paul Etienne <fractalclone@gmail.com>
+ * Copyright (c) 2022 BayLibre SAS
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <irq.h>
 #include <irq_offload.h>
 #include <arch/riscv/syscall.h>
 
-static irq_offload_routine_t offload_routine;
-static const void *offload_param;
-
-/*
- * Called by _enter_irq
- */
-void z_irq_do_offload(void)
-{
-	offload_routine(offload_param);
-}
-
 void arch_irq_offload(irq_offload_routine_t routine, const void *parameter)
 {
-	unsigned int key;
-
-	key = irq_lock();
-	offload_routine = routine;
-	offload_param = parameter;
-	arch_syscall_invoke0(RV_ECALL_IRQ_OFFLOAD);
-	irq_unlock(key);
+	arch_syscall_invoke2((uintptr_t)routine, (uintptr_t)parameter, RV_ECALL_IRQ_OFFLOAD);
 }
