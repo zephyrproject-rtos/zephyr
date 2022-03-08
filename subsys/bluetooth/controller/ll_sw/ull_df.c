4f7d3c2dab90b79967003e56308f60b13fd20a25
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-03-08 12:18:43 +0100

    Revert "[nrf fromtree] Bluetooth: Controller: df: refactor CTE REQ set disable"
    This reverts commit 7065446f75a3c4852dc7d39f1c1c26184cff6fa0.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_df.c
+++ subsys/bluetooth/controller/ll_sw/ull_df.c
@@ -1160,7 +1160,8 @@ uint8_t ll_df_set_conn_cte_req_enable(uint16_t handle, uint8_t enable,
 	}
 
 	if (!enable) {
-		ull_cp_cte_req_set_disable(conn);
+		conn->llcp.cte_req.is_enabled = false;
+		conn->llcp.cte_req.req_interval = 0U;
 
 		if (conn->llcp.cte_req.is_active) {
 			struct k_sem sem;
