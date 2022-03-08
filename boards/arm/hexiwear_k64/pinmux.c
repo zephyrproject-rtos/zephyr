2b8d397737d6f42fe83298e28cc07c18442f8355
Daniel DeGrasse <daniel.degrasse@nxp.com>
2022-03-08 15:20:10 -0600

    boards: remove pinmux usage for all kinetis boards
    Remove pinmux file where possible, and remove all pinmux driver usage
    where file cannot be removed. All kinetis boards no longer use pinmux
    driver.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- boards/arm/hexiwear_k64/pinmux.c
+++ boards/arm/hexiwear_k64/pinmux.c
@@ -5,34 +5,11 @@
  */
 
 #include <init.h>
-#include <drivers/pinmux.h>
 #include <drivers/gpio.h>
-#include <fsl_port.h>
 
 static int hexiwear_k64_pinmux_init(const struct device *dev)
 {
 	ARG_UNUSED(dev);
-
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(portb), okay)
-	__unused const struct device *portb =
-		DEVICE_DT_GET(DT_NODELABEL(portb));
-	__ASSERT_NO_MSG(device_is_ready(portb));
-#endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(portc), okay)
-	__unused const struct device *portc =
-		DEVICE_DT_GET(DT_NODELABEL(portc));
-	__ASSERT_NO_MSG(device_is_ready(portc));
-#endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(portd), okay)
-	__unused const struct device *portd =
-		DEVICE_DT_GET(DT_NODELABEL(portd));
-	__ASSERT_NO_MSG(device_is_ready(portd));
-#endif
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(porte), okay)
-	__unused const struct device *porte =
-		DEVICE_DT_GET(DT_NODELABEL(porte));
-	__ASSERT_NO_MSG(device_is_ready(porte));
-#endif
 
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(i2c0), okay) && CONFIG_I2C
 	const struct device *gpiob =
@@ -58,4 +35,4 @@ static int hexiwear_k64_pinmux_init(const struct device *dev)
 	return 0;
 }
 
-SYS_INIT(hexiwear_k64_pinmux_init, PRE_KERNEL_1, CONFIG_PINMUX_INIT_PRIORITY);
+SYS_INIT(hexiwear_k64_pinmux_init, PRE_KERNEL_1, CONFIG_APPLICATION_INIT_PRIORITY);
