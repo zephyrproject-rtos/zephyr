1019443f63a927200c0415d5de87dcdb46c726ed
Thomas Stranger <thomas.stranger@outlook.com>
2022-03-08 10:12:34 +0100

    drivers: w1 introduce 1-wire api and common code
    This commit introduces a new api for the Dallas 1-wire protocol.
    The api includes link functions for read and write operations on
    bit, byte, and block level, as well as functions to reset and
    lock the bus.
    
    The bus configuration is derived from the device tree and can be
    queried using w1_slave_count routine.
    
    Signed-off-by: Thomas Stranger <thomas.stranger@outlook.com>

---
+++ drivers/w1/w1_common.c
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2022 Thomas Stranger
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <device.h>
+#include <sys/crc.h>
+#include <zephyr/types.h>
+#include <drivers/w1.h>
+
+
+int z_impl_w1_read_block(const struct device *dev, uint8_t *buffer, size_t len)
+{
+	const struct w1_driver_api *api = dev->api;
+	int ret;
+
+	if (api->read_block != NULL) {
+		return api->read_block(dev, buffer, len);
+	}
+	for (int i = 0; i < len; ++i) {
+		ret = w1_read_byte(dev);
+		if (ret < 0) {
+			return ret;
+		}
+		buffer[i] = ret;
+	}
+
+	return 0;
+}
+
+int z_impl_w1_write_block(const struct device *dev, const uint8_t *buffer,
+			  size_t len)
+{
+	const struct w1_driver_api *api = dev->api;
+	int ret;
+
+	if (api->write_block != NULL) {
+		return api->write_block(dev, buffer, len);
+	}
+	for (int i = 0; i < len; ++i) {
+		ret = w1_write_byte(dev, buffer[i]);
+		if (ret < 0) {
+			return ret;
+		}
+	}
+
+	return 0;
+}
