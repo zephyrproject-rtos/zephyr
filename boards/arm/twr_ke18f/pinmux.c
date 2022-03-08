f34d8dc54178f02ad6241bd583474a9deb2bbb65
Daniel DeGrasse <daniel.degrasse@nxp.com>
2022-03-08 14:44:01 -0600

    boards: remove pinmux usage for dac32 driver
    dac32 driver supports pinctrl. Remove pinmux usage on kinetis boards
    with this IP.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- boards/arm/twr_ke18f/pinmux.c
+++ boards/arm/twr_ke18f/pinmux.c
@@ -39,10 +39,6 @@ static int twr_ke18f_pinmux_init(const struct device *dev)
 	__ASSERT_NO_MSG(device_is_ready(porte));
 #endif
 
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(dac0), okay) && CONFIG_DAC
-	pinmux_pin_set(porte, 9, PORT_PCR_MUX(kPORT_PinDisabledOrAnalog));
-#endif
-
 #ifdef CONFIG_BOARD_TWR_KE18F_FLEXIO_CLKOUT
 	int err;
 
