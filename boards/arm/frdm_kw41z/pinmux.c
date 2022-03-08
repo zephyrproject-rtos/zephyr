2a6f51ee8b507a4c9ed85bb1992ac728c39b8df7
Daniel DeGrasse <daniel.degrasse@nxp.com>
2022-03-08 14:55:56 -0600

    boards: remove tpm pinmux usage for kinetis boards
    tpm driver supports pinctrl. Remove pinmux usage for kinetis boards.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- boards/arm/frdm_kw41z/pinmux.c
+++ boards/arm/frdm_kw41z/pinmux.c
@@ -28,18 +28,6 @@ static int frdm_kw41z_pinmux_init(const struct device *dev)
 	__ASSERT_NO_MSG(device_is_ready(portc));
 #endif
 
-	/* Red, green, blue LEDs. Note the red LED and accel INT1 are both
-	 * wired to PTC1.
-	 */
-#if defined(CONFIG_PWM) && DT_NODE_HAS_STATUS(DT_NODELABEL(tpm0), okay)
-	pinmux_pin_set(portc,  1, PORT_PCR_MUX(kPORT_MuxAlt5));
-#endif
-
-#if defined(CONFIG_PWM) && DT_NODE_HAS_STATUS(DT_NODELABEL(tpm2), okay)
-	pinmux_pin_set(porta, 19, PORT_PCR_MUX(kPORT_MuxAlt5));
-	pinmux_pin_set(porta, 18, PORT_PCR_MUX(kPORT_MuxAlt5));
-#endif
-
 	return 0;
 }
 
