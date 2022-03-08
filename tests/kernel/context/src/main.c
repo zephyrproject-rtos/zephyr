502873041076f5d4e25edcde01319f8c7328fe14
Sylvio Alves <sylvio.alves@espressif.com>
2022-03-08 12:30:21 -0300

    tests: kernel: context: add esp32c3 irq value
    Allow esp32c3 soc to proper execute this kernel test.
    
    Signed-off-by: Sylvio Alves <sylvio.alves@espressif.com>

--- tests/kernel/context/src/main.c
+++ tests/kernel/context/src/main.c
@@ -77,6 +77,8 @@
 #define TICK_IRQ DT_IRQN(DT_INST(0, xlnx_ttcps))
 #elif defined(CONFIG_RCAR_CMT_TIMER)
 #define TICK_IRQ DT_IRQN(DT_INST(0, renesas_rcar_cmt))
+#elif defined(CONFIG_ESP32C3_SYS_TIMER)
+#define TICK_IRQ DT_IRQN(DT_NODELABEL(systimer0))
 #elif defined(CONFIG_CPU_CORTEX_M)
 /*
  * The Cortex-M use the SYSTICK exception for the system timer, which is
