e9b2c6ade2efd5a5dada926453a61d2fce3f6561
Dominik Ermel <dominik.ermel@nordicsemi.no>
2022-03-11 18:47:28 +0000

    mgmt/mcumgr/lib: Encode STAT command group responses with zcbor
    The commit moves STAT command group responses encoding to zcbor.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- subsys/mgmt/mcumgr/lib/cmd/stat_mgmt/src/stat_mgmt.c
+++ subsys/mgmt/mcumgr/lib/cmd/stat_mgmt/src/stat_mgmt.c
@@ -10,20 +10,47 @@
 
 #include <stats/stats.h>
 #include <mgmt/mgmt.h>
+#include <mgmt/mcumgr/buf.h>
 #include <stat_mgmt/stat_mgmt_config.h>
 #include <stat_mgmt/stat_mgmt.h>
 #include "cborattr/cborattr.h"
+#include <zcbor_common.h>
+#include <zcbor_encode.h>
 
 static struct mgmt_handler stat_mgmt_handlers[];
 
-typedef int stat_mgmt_foreach_entry_fn(CborEncoder *enc, struct stat_mgmt_entry *entry);
+typedef int stat_mgmt_foreach_entry_fn(zcbor_state_t *zse, struct stat_mgmt_entry *entry);
+
+static int
+stats_mgmt_count_plus_one(struct stats_hdr *hdr, void *arg, const char *name, uint16_t off)
+{
+	size_t *counter = arg;
+
+	(*counter)++;
+
+	return 0;
+}
+
+static int
+stat_mgmt_count(const char *group_name, size_t *counter)
+{
+	struct stats_hdr *hdr = stats_group_find(group_name);
+
+	if (hdr == NULL) {
+		return MGMT_ERR_ENOENT;
+	}
+
+	*counter = 0;
+
+	return stats_walk(hdr, stats_mgmt_count_plus_one, &counter);
+}
 
 static int
 stat_mgmt_walk_cb(struct stats_hdr *hdr, void *arg, const char *name, uint16_t off);
 
 struct stat_mgmt_walk_arg {
 	stat_mgmt_foreach_entry_fn *cb;
-	CborEncoder *enc;
+	zcbor_state_t *zse;
 };
 
 static int
@@ -47,15 +74,15 @@ stat_mgmt_walk_cb(struct stats_hdr *hdr, void *arg, const char *name, uint16_t o
 		entry.value = *(uint64_t *) stat_val;
 		break;
 	default:
-		return MGMT_ERR_EUNKNOWN;
+		return MGMT_ERR_EINVAL;
 	}
 	entry.name = name;
 
-	return walk_arg->cb(walk_arg->enc, &entry);
+	return walk_arg->cb(walk_arg->zse, &entry);
 }
 
 static int
-stat_mgmt_foreach_entry(const char *group_name, stat_mgmt_foreach_entry_fn *cb, CborEncoder *enc)
+stat_mgmt_foreach_entry(zcbor_state_t *zse, const char *group_name, stat_mgmt_foreach_entry_fn *cb)
 {
 	struct stat_mgmt_walk_arg walk_arg;
 	struct stats_hdr *hdr;
@@ -67,26 +94,19 @@ stat_mgmt_foreach_entry(const char *group_name, stat_mgmt_foreach_entry_fn *cb, 
 
 	walk_arg = (struct stat_mgmt_walk_arg) {
 		.cb = cb,
-		.enc = enc
+		.zse = zse
 	};
 
 	return stats_walk(hdr, stat_mgmt_walk_cb, &walk_arg);
 }
 
 static int
-stat_mgmt_cb_encode(CborEncoder *enc, struct stat_mgmt_entry *entry)
+stat_mgmt_cb_encode(zcbor_state_t *zse, struct stat_mgmt_entry *entry)
 {
-	CborError err;
+	bool ok = zcbor_tstr_put_term(zse, entry->name) &&
+		  zcbor_uint32_put(zse, entry->value);
 
-	err = 0;
-	err |= cbor_encode_text_stringz(enc, entry->name);
-	err |= cbor_encode_uint(enc, entry->value);
-
-	if (err != 0) {
-		return MGMT_ERR_ENOMEM;
-	}
-
-	return 0;
+	return ok ? MGMT_ERR_EOK : MGMT_ERR_ENOMEM;
 }
 
 /**
@@ -96,9 +116,10 @@ static int
 stat_mgmt_show(struct mgmt_ctxt *ctxt)
 {
 	char stat_name[STAT_MGMT_MAX_NAME_LEN];
-	CborEncoder map_enc;
 	CborError err;
-	int rc = MGMT_ERR_EOK;
+	zcbor_state_t *zse = ctxt->cnbe->zs;
+	bool ok;
+	size_t counter = 0;
 
 	struct cbor_attr_t attrs[] = {
 		{
@@ -115,25 +136,29 @@ stat_mgmt_show(struct mgmt_ctxt *ctxt)
 		return MGMT_ERR_EINVAL;
 	}
 
-	err |= cbor_encode_text_stringz(&ctxt->encoder, "rc");
-	err |= cbor_encode_int(&ctxt->encoder, MGMT_ERR_EOK);
-
-	err |= cbor_encode_text_stringz(&ctxt->encoder, "name");
-	err |= cbor_encode_text_stringz(&ctxt->encoder, stat_name);
-
-	err |= cbor_encode_text_stringz(&ctxt->encoder, "fields");
-	err |= cbor_encoder_create_map(&ctxt->encoder, &map_enc, CborIndefiniteLength);
-
-	if (err == 0) {
-		rc = stat_mgmt_foreach_entry(stat_name, stat_mgmt_cb_encode, &map_enc);
+	if (stat_mgmt_count(stat_name, &counter) != 0) {
+		return MGMT_ERR_EUNKNOWN;
 	}
 
-	err |= cbor_encoder_close_container(&ctxt->encoder, &map_enc);
-	if (rc || err != 0) {
+	ok = zcbor_tstr_put_lit(zse, "rc")		&&
+	     zcbor_int32_put(zse, MGMT_ERR_EOK)		&&
+	     zcbor_tstr_put_lit(zse, "name")		&&
+	     zcbor_tstr_put_term(zse, stat_name)	&&
+	     zcbor_tstr_put_lit(zse, "fields")		&&
+	     zcbor_map_start_encode(zse, counter);
+
+	if (ok) {
+		int rc = stat_mgmt_foreach_entry(zse, stat_name, stat_mgmt_cb_encode);
+
+		if (rc != MGMT_ERR_EOK) {
+			return rc;
+		}
+	} else {
 		return MGMT_ERR_ENOMEM;
 	}
 
-	return 0;
+	ok = zcbor_map_end_encode(zse, counter);
+	return ok ? MGMT_ERR_EOK : MGMT_ERR_ENOMEM;
 }
 
 /**
@@ -142,16 +167,26 @@ stat_mgmt_show(struct mgmt_ctxt *ctxt)
 static int
 stat_mgmt_list(struct mgmt_ctxt *ctxt)
 {
-	const struct stats_hdr *cur;
-	CborEncoder arr_enc;
-	CborError err;
+	const struct stats_hdr *cur = NULL;
+	zcbor_state_t *zse = ctxt->cnbe->zs;
+	bool ok;
+	size_t counter = 0;
 
-	err = CborNoError;
-	err |= cbor_encode_text_stringz(&ctxt->encoder, "rc");
-	err |= cbor_encode_int(&ctxt->encoder, MGMT_ERR_EOK);
-	err |= cbor_encode_text_stringz(&ctxt->encoder, "stat_list");
-	err |= cbor_encoder_create_array(&ctxt->encoder, &arr_enc, CborIndefiniteLength);
+	do {
+		cur = stats_group_get_next(cur);
+		if (cur != NULL) {
+			counter++;
+		}
+	} while (cur != NULL);
 
+	ok = zcbor_tstr_put_lit(zse, "rc")		&&
+	     zcbor_int32_put(zse, MGMT_ERR_EOK)		&&
+	     zcbor_tstr_put_lit(zse, "stat_list")	&&
+	     zcbor_list_start_encode(zse, counter);
+
+	if (!ok) {
+		return MGMT_ERR_ENOMEM;
+	}
 	/* Iterate the list of stat groups, encoding each group's name in the CBOR
 	 * array.
 	 */
@@ -159,11 +194,11 @@ stat_mgmt_list(struct mgmt_ctxt *ctxt)
 	do {
 		cur = stats_group_get_next(cur);
 		if (cur != NULL) {
-			err |= cbor_encode_text_stringz(&ctxt->encoder, cur->s_name);
+			ok = zcbor_tstr_put_term(zse, cur->s_name);
 		}
-	} while (err == 0 && cur != NULL);
+	} while (ok && cur != NULL);
 
-	if (err || cbor_encoder_close_container(&ctxt->encoder, &arr_enc) != 0) {
+	if (!ok || !zcbor_list_end_encode(zse, counter)) {
 		return MGMT_ERR_ENOMEM;
 	}
 
