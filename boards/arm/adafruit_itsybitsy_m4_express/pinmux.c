ff4a09652b09383c155b77d7e8c127ca39aa0a31
Gerson Fernando Budke <nandojve@gmail.com>
2022-03-13 12:30:30 -0300

    drivers: pwm: Add sam0 support to pinctrl
    This add support to pinctrl at Atmel sam0 tcc pwm driver. It updates
    all boards with new pinctrl groups format and drop pinmux entries.
    
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

--- boards/arm/adafruit_itsybitsy_m4_express/pinmux.c
+++ boards/arm/adafruit_itsybitsy_m4_express/pinmux.c
@@ -22,19 +22,6 @@ static int board_pinmux_init(const struct device *dev)
 		return -ENXIO;
 	}
 
-#if defined(CONFIG_PWM_SAM0_TCC)
-#if ATMEL_SAM0_DT_TCC_CHECK(0, atmel_sam0_tcc_pwm)
-	/* TCC0/WO[2] on PA22 (LED) */
-	pinmux_pin_set(muxa, 22, PINMUX_FUNC_G);
-#endif
-#if ATMEL_SAM0_DT_TCC_CHECK(1, atmel_sam0_tcc_pwm)
-	/* TCC1/WO[2] on PA18 (D7) */
-	pinmux_pin_set(muxa, 18, PINMUX_FUNC_F);
-	/* TCC1/WO[3] on PA19 (D9) */
-	pinmux_pin_set(muxa, 19, PINMUX_FUNC_F);
-#endif
-#endif
-
 	if (IS_ENABLED(CONFIG_USB_DC_SAM0)) {
 		/* USB DP on PA25, USB DM on PA24 */
 		pinmux_pin_set(muxa, 25, PINMUX_FUNC_H);
