1c325aab782e89586f00b2691fa2e87759ce3863
Emil Obalski <emil.obalski@nordicsemi.no>
2022-03-14 15:59:04 +0100

    ipc_service: Add new backend based on custom messaging buffer
    This commit adds ipc backend, that relies on simple
    inter core messaging buffer also added by this commit.
    
    This backend is configurable with DT overlay. Each
    ipc instance could be defined with ipc-icmsg commpatible.
    
    Signed-off-by: Emil Obalski <emil.obalski@nordicsemi.no>

---
+++ subsys/ipc/ipc_service/backends/ipc_icmsg.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ipc/ipc_icmsg_buf.h>
+
+enum icmsg_state {
+	ICMSG_STATE_OFF,
+	ICMSG_STATE_BUSY,
+	ICMSG_STATE_READY
+};
