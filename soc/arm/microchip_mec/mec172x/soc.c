e367d6befbff7e5297b1141f78b34c7000185d35
Jose Alberto Meza <jose.a.meza.arellano@intel.com>
2022-03-16 17:05:13 -0700

    soc: arm: microchip: mec172x: Add option to enable JTAG
    Set different options for MEC172x SoC debug via JTAG.
    
    Signed-off-by: Jose Alberto Meza <jose.a.meza.arellano@intel.com>

--- soc/arm/microchip_mec/mec172x/soc.c
+++ soc/arm/microchip_mec/mec172x/soc.c
@@ -12,9 +12,39 @@
 #include <zephyr/arch/cpu.h>
 #include <zephyr/arch/arm/aarch32/cortex_m/cmsis.h>
 
+
+#if defined(CONFIG_SOC_MEC172X_DEBUG_WITHOUT_TRACING) || \
+	defined(CONFIG_SOC_MEC172X_DEBUG_AND_TRACING)
+static uintptr_t ecs_regbase = DT_REG_ADDR(DT_NODELABEL(ecs));
+#endif
+
+static void configure_debug_interface(void)
+{
+#ifdef CONFIG_SOC_MEC172X_DEBUG_WITHOUT_TRACING
+	struct ecs_regs *regs = ((struct ecs_regs *)ecs_regbase);
+
+	regs->DEBUG_CTRL = (MCHP_ECS_DCTRL_DBG_EN |
+				MCHP_ECS_DCTRL_MODE_SWD);
+#elif defined(CONFIG_SOC_MEC172X_DEBUG_AND_TRACING)
+	struct ecs_regs *regs = ((struct ecs_regs *)ecs_regbase);
+
+	#if defined(CONFIG_SOC_MEC172X_DEBUG_AND_ETM_TRACING)
+		regs->ETM_CTRL = MCHP_ECS_ETM_CTRL_EN;
+		regs->DEBUG_CTRL = (MCHP_ECS_DCTRL_DBG_EN |
+				MCHP_ECS_DCTRL_MODE_SWD);
+	#elif defined(CONFIG_SOC_MEC172X_DEBUG_AND_SWV_TRACING)
+		regs->DEBUG_CTRL = (MCHP_ECS_DCTRL_DBG_EN |
+				MCHP_ECS_DCTRL_MODE_SWD_SWV);
+	#endif /* CONFIG_SOC_MEC172X_DEBUG_AND_TRACING */
+
+#endif /* CONFIG_SOC_MEC172X_DEBUG_WITHOUT_TRACING */
+}
+
 static int soc_init(const struct device *dev)
 {
 	ARG_UNUSED(dev);
+
+	configure_debug_interface();
 
 	return 0;
 }
