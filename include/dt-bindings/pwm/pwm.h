da058ce7386d137665e8e5b9f8f685f396d6efb6
Francois Ramu <francois.ramu@st.com>
2022-03-18 11:30:17 +0100

    include: drivers: pwm: extend the pwm flags to be soc specific
    Add 8 bits to the pwm_flags_t so that upper byte is reserved
    for non-standard but soc specific dts flags and keeping the low
    Byte for standard flags.
    Some of SoC like STM32 mcus can then define their own flags
    in their dt-bindings/pwm.
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- include/dt-bindings/pwm/pwm.h
+++ include/dt-bindings/pwm/pwm.h
@@ -11,6 +11,7 @@
  * The `PWM_POLARITY_*` flags are used with pwm_pin_set_cycles(),
  * pwm_pin_set_usec(), pwm_pin_set_nsec() or pwm_pin_configure_capture() to
  * specify the polarity of a PWM pin.
+ * The flags are on the lower 8bits of the pwm_flags_t
  * @{
  */
 /** PWM pin normal polarity (active-high pulse). */
