ecae7a9c7f1f99c998ed9428350a9a42d44edd5f
Andries Kruithof <andries.kruithof@nordicsemi.no>
2022-03-21 06:47:21 -0700

    Bluetooth: controller: llcp: terminate connection on collission
    As per bluetooth spec Vol. 6 part B section 5.3 we need to terminate
    the connection under given situation
    
    Signed-off-by: Andries Kruithof <andries.kruithof@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_llcp_remote.c
+++ subsys/bluetooth/controller/ll_sw/ull_llcp_remote.c
@@ -461,6 +461,7 @@ static void rr_st_disconnect(struct ll_conn *conn, uint8_t evt, void *param)
 static void rr_st_idle(struct ll_conn *conn, uint8_t evt, void *param)
 {
 	struct proc_ctx *ctx;
+	struct proc_ctx *ctx_local;
 
 	switch (evt) {
 	case RR_EVT_PREPARE:
@@ -493,7 +494,10 @@ static void rr_st_idle(struct ll_conn *conn, uint8_t evt, void *param)
 				 * Local incompatible procedure request is kept pending.
 				 */
 
-				/* Pause local incompatible procedure */
+				/*
+				 * Pause local incompatible procedure
+				 * in case we run a procedure with instant
+				 */
 				rr_set_collision(conn, with_instant);
 
 				/* Run remote procedure */
@@ -509,7 +513,7 @@ static void rr_st_idle(struct ll_conn *conn, uint8_t evt, void *param)
 				/* Run remote procedure */
 				rr_act_run(conn);
 				rr_set_state(conn, RR_STATE_ACTIVE);
-			} else if (with_instant && central && incompat == INCOMPAT_RESOLVABLE) {
+			} else if (central && incompat == INCOMPAT_RESOLVABLE) {
 				/* Central collision
 				 * => Send reject
 				 *
@@ -522,20 +526,24 @@ static void rr_st_idle(struct ll_conn *conn, uint8_t evt, void *param)
 
 				conn->llcp.remote.reject_opcode = pdu->llctrl.opcode;
 				rr_act_reject(conn);
-			} else if (!with_instant && central && incompat == INCOMPAT_RESOLVABLE) {
-				/* No collision with procedure without instant
-				 * => Run procedure
-				 */
-				rr_act_run(conn);
-				rr_set_state(conn, RR_STATE_ACTIVE);
-			} else if (with_instant && incompat == INCOMPAT_RESERVED) {
+			} else if (incompat == INCOMPAT_RESERVED) {
 				/* Protocol violation.
 				 * => Disconnect
-				 *
+				 * See Bluetooth Core Specification Version 5.3 Vol 6, Part B
+				 * section 5.3 (page 2879) for error codes
 				 */
 
-				/* TODO */
-				LL_ASSERT(0);
+				ctx_local = llcp_lr_peek(conn);
+
+				if (ctx_local->proc == ctx->proc ||
+				    (ctx_local->proc == PROC_CONN_UPDATE &&
+				     ctx->proc == PROC_CONN_PARAM_REQ)) {
+					conn->llcp_terminate.reason_final =
+						BT_HCI_ERR_LL_PROC_COLLISION;
+				} else {
+					conn->llcp_terminate.reason_final =
+						BT_HCI_ERR_DIFF_TRANS_COLLISION;
+				}
 			}
 		}
 		break;
