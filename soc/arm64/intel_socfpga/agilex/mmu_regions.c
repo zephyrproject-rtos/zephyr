cdbf0eae85554974ebb081c264e51040121c93ef
Alif Zakuan Yuslaimi <alif.zakuan.yuslaimi@intel.com>
2022-03-23 00:39:16 +0800

    dts: arm64: intel_socfpga: Add GPIO device tree
    This patch is to add GPIO device tree element and
    memory region to ready for GPIO driver bringup.
    
    Signed-off-by: Alif Zakuan Yuslaimi <alif.zakuan.yuslaimi@intel.com>

--- soc/arm64/intel_socfpga/agilex/mmu_regions.c
+++ soc/arm64/intel_socfpga/agilex/mmu_regions.c
@@ -50,6 +50,11 @@ static const struct arm_mmu_region mmu_regions[] = {
 			      DT_REG_ADDR_BY_IDX(DT_NODELABEL(gic), 1),
 			      DT_REG_SIZE_BY_IDX(DT_NODELABEL(gic), 1),
 			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
+
+	MMU_REGION_FLAT_ENTRY("PINMUX",
+			      DT_REG_ADDR(DT_NODELABEL(pinmux)),
+			      DT_REG_SIZE(DT_NODELABEL(pinmux)),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
 };
 
 const struct arm_mmu_config mmu_config = {
