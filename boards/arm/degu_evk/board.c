1b6c5096e6ad2846241c9fe03a487c4a727565fb
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-03-22 14:44:53 +0100

    boards: degu_evk: use regulator for 3V3 power rail
    Replace custom init code with the regulator infrastructure to enable the
    3V3 power rail.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- boards/arm/degu_evk/board.c
+++ boards/arm/degu_evk/board.c
@@ -11,16 +11,8 @@ static int board_degu_evk_init(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 
-	const struct device *gpio0 =
-		device_get_binding(DT_LABEL(DT_NODELABEL(gpio0)));
 	const struct device *gpio1 =
 		device_get_binding(DT_LABEL(DT_NODELABEL(gpio1)));
-
-	/*
-	 * Degu Evaluation Kit has a TPS22916C power switch.
-	 * It is connected to GPIO0_26 so we must enable it.
-	 */
-	gpio_pin_configure(gpio0, 26, GPIO_OUTPUT_HIGH);
 
 	/*
 	 * We must enable GPIO1_2 to use Secure Element.
