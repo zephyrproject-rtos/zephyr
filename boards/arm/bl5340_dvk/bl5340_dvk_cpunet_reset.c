zephyrproject-rtos/zephyr

commit 89f29c5dea2326950eecaeb592c1458140fc432e

boards: nrf53: Fix board pin MCU selection with TF-M enabled
Fix board pin MCU selection not working with TF-M enabled.
This fixed no log output from the network core when TF-M is enabled.

Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

boards/arm/bl5340_dvk/bl5340_dvk_cpunet_reset.c
@@ -10,51 +10,43 @@
 #include <logging/log.h>
 
 #include <soc.h>
+#include <soc_secure.h>
 
 LOG_MODULE_REGISTER(bl5340_dvk_cpuapp, CONFIG_LOG_DEFAULT_LEVEL);
 
-#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
-
-/* This should come from DTS, possibly an overlay. */
-#if defined(CONFIG_BOARD_BL5340_DVK_CPUAPP)
-#define CPUNET_UARTE_PIN_TX  8
-#define CPUNET_UARTE_PIN_RX  10
-#define CPUNET_UARTE_PORT_TRX NRF_P1
-#define CPUNET_UARTE_PIN_RTS 7
-#define CPUNET_UARTE_PIN_CTS 9
-#define CPUNET_UARTE_PORT_RCTS NRF_P1
-#endif
+/* TODO: This should come from DTS, possibly an overlay. */
+#define CPUNET_UARTE_PIN_TX 33
+#define CPUNET_UARTE_PIN_RX 32
+#define CPUNET_UARTE_PIN_RTS 11
+#define CPUNET_UARTE_PIN_CTS 10
 
 static void remoteproc_mgr_config(void)
 {
+#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) || defined(CONFIG_BUILD_WITH_TFM)
 	/* UARTE */
 	/* Assign specific GPIOs that will be used to get UARTE from
 	 * nRF5340 Network MCU.
 	 */
-	CPUNET_UARTE_PORT_TRX->PIN_CNF[CPUNET_UARTE_PIN_TX] =
-		GPIO_PIN_CNF_MCUSEL_NetworkMCU << GPIO_PIN_CNF_MCUSEL_Pos;
-	CPUNET_UARTE_PORT_TRX->PIN_CNF[CPUNET_UARTE_PIN_RX] =
-		GPIO_PIN_CNF_MCUSEL_NetworkMCU << GPIO_PIN_CNF_MCUSEL_Pos;
-	CPUNET_UARTE_PORT_RCTS->PIN_CNF[CPUNET_UARTE_PIN_RTS] =
-		GPIO_PIN_CNF_MCUSEL_NetworkMCU << GPIO_PIN_CNF_MCUSEL_Pos;
-	CPUNET_UARTE_PORT_RCTS->PIN_CNF[CPUNET_UARTE_PIN_CTS] =
-		GPIO_PIN_CNF_MCUSEL_NetworkMCU << GPIO_PIN_CNF_MCUSEL_Pos;
+	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_TX, NRF_GPIO_PIN_MCUSEL_NETWORK);
+	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_RX, NRF_GPIO_PIN_MCUSEL_NETWORK);
+	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_RTS, NRF_GPIO_PIN_MCUSEL_NETWORK);
+	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_CTS, NRF_GPIO_PIN_MCUSEL_NETWORK);
+#endif /* !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) || defined(CONFIG_BUILD_WITH_TFM) */
 
+#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 	/* Retain nRF5340 Network MCU in Secure domain (bus
 	 * accesses by Network MCU will have Secure attribute set).
 	 */
 	NRF_SPU->EXTDOMAIN[0].PERM = 1 << 4;
-}
 #endif /* !CONFIG_TRUSTED_EXECUTION_NONSECURE */
+}
 
 static int remoteproc_mgr_boot(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 
-#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 	/* Secure domain may configure permissions for the Network MCU. */
 	remoteproc_mgr_config();
-#endif /* !CONFIG_TRUSTED_EXECUTION_NONSECURE */
 
 #if !defined(CONFIG_TRUSTED_EXECUTION_SECURE)
 	/*

boards/arm/nrf5340dk_nrf5340/nrf5340_cpunet_reset.c
@@ -9,19 +9,15 @@
 #include <logging/log.h>
 
 #include <soc.h>
+#include <soc_secure.h>
 
 LOG_MODULE_REGISTER(nrf5340dk_nrf5340_cpuapp, CONFIG_LOG_DEFAULT_LEVEL);
 
-#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
-
-/* This should come from DTS, possibly an overlay. */
-#if defined(CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP)
-#define CPUNET_UARTE_PIN_TX  1
-#define CPUNET_UARTE_PIN_RX  0
-#define CPUNET_UARTE_PORT_TRX NRF_P1
+/* TODO: This should come from DTS, possibly an overlay. */
+#define CPUNET_UARTE_PIN_TX 33
+#define CPUNET_UARTE_PIN_RX 32
 #define CPUNET_UARTE_PIN_RTS 11
 #define CPUNET_UARTE_PIN_CTS 10
-#endif
 
 #if defined(CONFIG_BT_CTLR_DEBUG_PINS_CPUAPP)
 #include <../subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/debug.h>
@@ -31,37 +27,34 @@ LOG_MODULE_REGISTER(nrf5340dk_nrf5340_cpuapp, CONFIG_LOG_DEFAULT_LEVEL);
 
 static void remoteproc_mgr_config(void)
 {
+#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) || defined(CONFIG_BUILD_WITH_TFM)
 	/* UARTE */
 	/* Assign specific GPIOs that will be used to get UARTE from
 	 * nRF5340 Network MCU.
 	 */
-	CPUNET_UARTE_PORT_TRX->PIN_CNF[CPUNET_UARTE_PIN_TX] =
-		GPIO_PIN_CNF_MCUSEL_NetworkMCU << GPIO_PIN_CNF_MCUSEL_Pos;
-	CPUNET_UARTE_PORT_TRX->PIN_CNF[CPUNET_UARTE_PIN_RX] =
-		GPIO_PIN_CNF_MCUSEL_NetworkMCU << GPIO_PIN_CNF_MCUSEL_Pos;
-	NRF_P0->PIN_CNF[CPUNET_UARTE_PIN_RTS] =
-		GPIO_PIN_CNF_MCUSEL_NetworkMCU << GPIO_PIN_CNF_MCUSEL_Pos;
-	NRF_P0->PIN_CNF[CPUNET_UARTE_PIN_CTS] =
-		GPIO_PIN_CNF_MCUSEL_NetworkMCU << GPIO_PIN_CNF_MCUSEL_Pos;
+	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_TX, NRF_GPIO_PIN_MCUSEL_NETWORK);
+	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_RX, NRF_GPIO_PIN_MCUSEL_NETWORK);
+	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_RTS, NRF_GPIO_PIN_MCUSEL_NETWORK);
+	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_CTS, NRF_GPIO_PIN_MCUSEL_NETWORK);
 
 	/* Route Bluetooth Controller Debug Pins */
 	DEBUG_SETUP();
+#endif /* !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) || defined(CONFIG_BUILD_WITH_TFM) */
 
+#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 	/* Retain nRF5340 Network MCU in Secure domain (bus
 	 * accesses by Network MCU will have Secure attribute set).
 	 */
 	NRF_SPU->EXTDOMAIN[0].PERM = 1 << 4;
+#endif /* !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) */
 }
-#endif /* !CONFIG_TRUSTED_EXECUTION_NONSECURE */
 
 static int remoteproc_mgr_boot(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 
-#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 	/* Secure domain may configure permissions for the Network MCU. */
 	remoteproc_mgr_config();
-#endif /* !CONFIG_TRUSTED_EXECUTION_NONSECURE */
 
 #if !defined(CONFIG_TRUSTED_EXECUTION_SECURE)
 	/*

commit 762a852d3f2d1b869b96052c8587658090a0959f

soc: arm: bl5340_dvk: Fix bl5340 DVK pin assignments
Fix the BL5340 DVK pin assignment using the nrf5340 DK pin.
Regression from: 89f29c5dea2326950eecaeb592c1458140fc432e.

This also aligns the BL5340 DVK to use the GPIO forward module.

Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

boards/arm/bl5340_dvk/bl5340_dvk_cpuapp_common.dts
@@ -66,6 +66,14 @@
 		};
 	};
 
+	gpio_fwd: nrf-gpio-forwarder {
+		compatible = "nordic,nrf-gpio-forwarder";
+		status = "okay";
+		uart {
+			gpios = <&gpio1 8 0>, <&gpio1 10 0>, <&gpio1 7 0>, <&gpio1 9 0>;
+		};
+	};
+
 	/* These aliases are provided for compatibility with samples */
 	aliases {
 		led0 = &led1;

boards/arm/bl5340_dvk/bl5340_dvk_cpunet_reset.c
@@ -10,29 +10,11 @@
 #include <logging/log.h>
 
 #include <soc.h>
-#include <soc_secure.h>
 
 LOG_MODULE_REGISTER(bl5340_dvk_cpuapp, CONFIG_LOG_DEFAULT_LEVEL);
 
-/* TODO: This should come from DTS, possibly an overlay. */
-#define CPUNET_UARTE_PIN_TX 33
-#define CPUNET_UARTE_PIN_RX 32
-#define CPUNET_UARTE_PIN_RTS 11
-#define CPUNET_UARTE_PIN_CTS 10
-
 static void remoteproc_mgr_config(void)
 {
-#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) || defined(CONFIG_BUILD_WITH_TFM)
-	/* UARTE */
-	/* Assign specific GPIOs that will be used to get UARTE from
-	 * nRF5340 Network MCU.
-	 */
-	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_TX, NRF_GPIO_PIN_MCUSEL_NETWORK);
-	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_RX, NRF_GPIO_PIN_MCUSEL_NETWORK);
-	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_RTS, NRF_GPIO_PIN_MCUSEL_NETWORK);
-	soc_secure_gpio_pin_mcu_select(CPUNET_UARTE_PIN_CTS, NRF_GPIO_PIN_MCUSEL_NETWORK);
-#endif /* !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) || defined(CONFIG_BUILD_WITH_TFM) */
-
 #if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 	/* Retain nRF5340 Network MCU in Secure domain (bus
 	 * accesses by Network MCU will have Secure attribute set).