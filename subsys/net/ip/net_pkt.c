41496a79d2215ed2de1fba58500b5541305a8112
Robert Lubos <robert.lubos@nordicsemi.no>
2022-03-23 15:05:58 +0100

    net: pkt: Add net_pkt_rx_clone() function
    Add a function which allows to clone a packet, using RX packet poll to
    create the clone.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/net/ip/net_pkt.c
+++ subsys/net/ip/net_pkt.c
@@ -1808,19 +1808,21 @@ static void clone_pkt_attributes(struct net_pkt *pkt, struct net_pkt *clone_pkt)
 #endif
 }
 
-struct net_pkt *net_pkt_clone(struct net_pkt *pkt, k_timeout_t timeout)
+static struct net_pkt *net_pkt_clone_internal(struct net_pkt *pkt,
+					      struct k_mem_slab *slab,
+					      k_timeout_t timeout)
 {
 	size_t cursor_offset = net_pkt_get_current_offset(pkt);
 	struct net_pkt *clone_pkt;
 	struct net_pkt_cursor backup;
 
 #if NET_LOG_LEVEL >= LOG_LEVEL_DBG
-	clone_pkt = pkt_alloc_with_buffer(pkt->slab, net_pkt_iface(pkt),
+	clone_pkt = pkt_alloc_with_buffer(slab, net_pkt_iface(pkt),
 					  net_pkt_get_len(pkt),
 					  AF_UNSPEC, 0, timeout,
 					  __func__, __LINE__);
 #else
-	clone_pkt = pkt_alloc_with_buffer(pkt->slab, net_pkt_iface(pkt),
+	clone_pkt = pkt_alloc_with_buffer(slab, net_pkt_iface(pkt),
 					  net_pkt_get_len(pkt),
 					  AF_UNSPEC, 0, timeout);
 #endif
@@ -1862,6 +1864,16 @@ struct net_pkt *net_pkt_clone(struct net_pkt *pkt, k_timeout_t timeout)
 	NET_DBG("Cloned %p to %p", pkt, clone_pkt);
 
 	return clone_pkt;
+}
+
+struct net_pkt *net_pkt_clone(struct net_pkt *pkt, k_timeout_t timeout)
+{
+	return net_pkt_clone_internal(pkt, pkt->slab, timeout);
+}
+
+struct net_pkt *net_pkt_rx_clone(struct net_pkt *pkt, k_timeout_t timeout)
+{
+	return net_pkt_clone_internal(pkt, &rx_pkts, timeout);
 }
 
 struct net_pkt *net_pkt_shallow_clone(struct net_pkt *pkt, k_timeout_t timeout)
