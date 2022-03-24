5904e314fca72c8d1c19294b2f82eff66e26fa67
Carles Cufi <carles.cufi@nordicsemi.no>
2022-03-24 15:41:11 +0100

    boards: nrf: Fix jlink invocation strings
    The jlink executable expects a fully qualified IC name, and not just the
    family. Apply to all Nordic boards.
    
    Fixes #37294
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- boards/arm/bbc_microbit_v2/board.cmake
+++ boards/arm/bbc_microbit_v2/board.cmake
@@ -2,7 +2,7 @@
 
 board_runner_args(pyocd "--target=nrf52")
 board_runner_args(nrfjprog "--nrf-family=NRF52")
-board_runner_args(jlink "--device=nrf52" "--speed=4000")
+board_runner_args(jlink "--device=nRF52833_xxAA" "--speed=4000")
 
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
