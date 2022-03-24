935d08dd74f8882d1a501f56323002b9e686cf22
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2022-03-24 18:05:23 +0200

    soc/intel_adsp: boot: simplify the ifdef rules for cAVS versions
    Only cAVS15 is different in terms of definitions, so the ifdef logic can
    be simplified quite a bit. Also reorder some of the definitions to
    improve readability. No functional change.
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

--- soc/xtensa/intel_adsp/common/boot.c
+++ soc/xtensa/intel_adsp/common/boot.c
@@ -36,38 +36,23 @@
  * These probably want to migrate to devicetree.
  */
 
-#if defined(CONFIG_SOC_SERIES_INTEL_CAVS_V25)
-#define PLATFORM_INIT_HPSRAM
-#define PLATFORM_INIT_LPSRAM
-#define EBB_SEGMENT_SIZE          32
+#define LPSRAM_MASK(x)		 0x00000003
+#define SRAM_BANK_SIZE		(64 * 1024)
+#define HOST_PAGE_SIZE		4096
+#define EBB_SEGMENT_SIZE	32
+#define MANIFEST_SEGMENT_COUNT	3
 
-#elif defined(CONFIG_SOC_SERIES_INTEL_CAVS_V20)
-#define PLATFORM_INIT_HPSRAM
-#define PLATFORM_INIT_LPSRAM
-#define EBB_SEGMENT_SIZE          32
-
-#elif defined(CONFIG_SOC_SERIES_INTEL_CAVS_V18)
-#define PLATFORM_INIT_HPSRAM
-#define PLATFORM_INIT_LPSRAM
-#define EBB_SEGMENT_SIZE          32
-
-#elif defined(CONFIG_SOC_SERIES_INTEL_CAVS_V15)
-#define PLATFORM_INIT_LPSRAM
+#if defined(CONFIG_SOC_SERIES_INTEL_CAVS_V15)
 #define PLATFORM_DISABLE_L2CACHE_AT_BOOT
-
+#else
+#define PLATFORM_INIT_HPSRAM
 #endif
 
-#define LPSRAM_MASK(x) 0x00000003
-#define SRAM_BANK_SIZE (64 * 1024)
-#define HOST_PAGE_SIZE 4096
-#define EBB_SEGMENT_SIZE          32
 #define PLATFORM_INIT_LPSRAM
 
 #define PLATFORM_HPSRAM_EBB_COUNT (DT_REG_SIZE(DT_NODELABEL(sram0)) / SRAM_BANK_SIZE)
 BUILD_ASSERT((DT_REG_SIZE(DT_NODELABEL(sram0)) % SRAM_BANK_SIZE) == 0,
 	     "sram0 must be divisible by 64*1024 bank size.");
-
-#define MANIFEST_SEGMENT_COUNT 3
 
 extern void soc_trace_init(void);
 
