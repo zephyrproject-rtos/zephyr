975a79f6a7fa5e0c54a650c5b036afff75ad4e26
Carles Cufi <carles.cufi@nordicsemi.no>
2022-03-25 15:50:19 +0100

    boards: nrf: pyocd: Fix pyocd invocation strings
    A follow-up to #44181, this time fixing the pyocd target parameters to
    match the actual hardware on the board.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- boards/arm/bbc_microbit_v2/board.cmake
+++ boards/arm/bbc_microbit_v2/board.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(pyocd "--target=nrf52")
+board_runner_args(pyocd "--target=nrf52833")
 board_runner_args(nrfjprog "--nrf-family=NRF52")
 board_runner_args(jlink "--device=nRF52833_xxAA" "--speed=4000")
 
