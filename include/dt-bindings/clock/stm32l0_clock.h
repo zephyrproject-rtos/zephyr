bccf8afa59ccf30a2e710078f63bcac7c4815b00
Erwan Gouriou <erwan.gouriou@linaro.org>
2022-03-29 11:46:35 +0200

    include/dt-bindings/clock: stm32: Add bindings for 'common' series
    Add new scheme clock bindings for 'common' series:
    - stm32f1_clock.h > compatible with f0/f1/f3 series
    - stm32f4_clock.h > compatible with f2/f4/f7 series
    - stm32l0_clock.h > compatible with l0 series
    - stm32l1_clock.h > compatible with l1 series
    - stm32l4_clock.h > compatible with g4/l4/l5/wb series
    - stm32wl_clock.h > compatible with wl series
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

---
+++ include/dt-bindings/clock/stm32l0_clock.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2022 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_STM32L0_CLOCK_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_STM32L0_CLOCK_H_
+
+/** Peripheral clock sources */
+
+/** Bus clocks */
+#define STM32_CLOCK_BUS_IOP     0x02c
+#define STM32_CLOCK_BUS_AHB1    0x030
+#define STM32_CLOCK_BUS_APB2    0x034
+#define STM32_CLOCK_BUS_APB1    0x038
+
+#define STM32_PERIPH_BUS_MIN	STM32_CLOCK_BUS_IOP
+#define STM32_PERIPH_BUS_MAX	STM32_CLOCK_BUS_APB1
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_STM32L0_CLOCK_H_ */
