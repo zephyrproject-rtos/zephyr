35f9a5d715060cb6b0067f2b39cb1614e81933d1
Jimmy Brisson <jimmy.brisson@linaro.org>
2022-03-29 16:04:15 -0500

    cortex-m: Abort pending SVC when a thread is killed
    If an SVC was pending during the stack overflow, it will run
    after the return of the memory manage fault. To the SVC's misfortune of
    the SVC handler, the it's invariant, that PSP point to the
    hardware-stacked context is no longer valid. When the user has a
    k_sys_fatal_error_handler that tries to kill the thread that caused a
    stack overflow, this manifests as the svc reading the memory of whatever
    is on the stack after being adjusted by the mem manage fault handler, and
    that leads to unending, spurious hard faults, locking up the system.
    
    This patch prevents that.
    
    Signed-off-by: Jimmy Brisson <jimmy.brisson@linaro.org>

--- arch/arm/core/aarch32/cortex_m/thread_abort.c
+++ arch/arm/core/aarch32/cortex_m/thread_abort.c
@@ -40,6 +40,12 @@ void z_impl_k_thread_abort(k_tid_t thread)
 			 * is not an implicit scheduler invocation.
 			 */
 			SCB->ICSR |= SCB_ICSR_PENDSVSET_Msk;
+			/* Clear any system calls that may be pending
+			 * as they have a higher priority than the PendSV
+			 * handler and will check the stack of the thread
+			 * being aborted.
+			 */
+			SCB->SHCSR &= ~SCB_SHCSR_SVCALLPENDED_Msk;
 		}
 	}
 
