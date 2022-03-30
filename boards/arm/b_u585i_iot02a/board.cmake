333b7c05be7027c41f071cf9fdc6e60826c37bd3
Francois Ramu <francois.ramu@st.com>
2022-03-30 12:10:44 +0200

    boards: b_u585i_iot02a: Add a non-secure target
    Provide a _ns target for b_u585i_iot02a board.
    Flash partition is given as default for most applications,
    with the following sizes:
    MCUBoot: 208K
    Secure: 256K
    Non secure: 512K
    
    Tested using tfm_ipc sample.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- boards/arm/b_u585i_iot02a/board.cmake
+++ boards/arm/b_u585i_iot02a/board.cmake
@@ -1,4 +1,16 @@
 # SPDX-License-Identifier: Apache-2.0
+if(CONFIG_BUILD_WITH_TFM)
+  set(TFM_FLASH_BASE_ADDRESS 0x0C000000)
+
+  # Flash merged TF-M + Zephyr binary
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file "${CMAKE_BINARY_DIR}/tfm_merged.hex")
+
+  if (CONFIG_HAS_FLASH_LOAD_OFFSET)
+    MATH(EXPR TFM_HEX_BASE_ADDRESS_NS "${TFM_FLASH_BASE_ADDRESS}+${CONFIG_FLASH_LOAD_OFFSET}")
+  else()
+    set(TFM_HEX_BASE_ADDRESS_NS ${TFM_TFM_FLASH_BASE_ADDRESS})
+  endif()
+endif()
 
 board_runner_args(stm32cubeprogrammer "--erase" "--port=swd" "--reset=hw")
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset=hw")
