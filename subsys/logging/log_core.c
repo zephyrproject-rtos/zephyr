55e9e182d7e9a164a37d6ad14e9d7244f4203f88
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2022-03-31 13:25:41 +0200

    logging: Add timestamp getter function part of internal API
    Expose timestamp getter so it can be used, e.g. in the frontend.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

--- subsys/logging/log_core.c
+++ subsys/logging/log_core.c
@@ -160,6 +160,11 @@ static log_timestamp_t dummy_timestamp(void)
 	return 0;
 }
 
+log_timestamp_t z_log_timestamp(void)
+{
+	return timestamp_func();
+}
+
 uint32_t z_log_get_s_mask(const char *str, uint32_t nargs)
 {
 	char curr;
@@ -611,16 +616,16 @@ void log_core_init(void)
 	if (IS_ENABLED(CONFIG_LOG_RUNTIME_FILTERING)) {
 		z_log_runtime_filters_init();
 	}
+
+	if (IS_ENABLED(CONFIG_LOG_FRONTEND)) {
+		log_frontend_init();
+	}
 }
 
 void log_init(void)
 {
 	__ASSERT_NO_MSG(log_backend_count_get() < LOG_FILTERS_NUM_OF_SLOTS);
 	int i;
-
-	if (IS_ENABLED(CONFIG_LOG_FRONTEND)) {
-		log_frontend_init();
-	}
 
 	if (atomic_inc(&initialized) != 0) {
 		return;
