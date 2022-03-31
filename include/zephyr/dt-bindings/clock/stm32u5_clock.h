557505ffaf7bb365b506452f4b61c04c25d90e2c
Erwan Gouriou <erwan.gouriou@linaro.org>
2022-03-31 15:42:59 +0200

    include/dt-bindings/clock: stm32: Factorize Clock source binding accessors
    Rename and factorize clock source bindings accessors by moving them
    in common header file stm32_clock_control and remove them from
    include/dt-bindings/clock/stm32XY_clock.h files
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- include/zephyr/dt-bindings/clock/stm32u5_clock.h
+++ include/zephyr/dt-bindings/clock/stm32u5_clock.h
@@ -64,55 +64,20 @@
  * @param val Clock value (0, 1, ... 7).
  */
 
-#define STM32U5_CLOCK_REG_MASK    0xFFU
-#define STM32U5_CLOCK_REG_SHIFT   0U
-#define STM32U5_CLOCK_SHIFT_MASK  0x1FU
-#define STM32U5_CLOCK_SHIFT_SHIFT 8U
-#define STM32U5_CLOCK_MASK_MASK   0x7U
-#define STM32U5_CLOCK_MASK_SHIFT  13U
-#define STM32U5_CLOCK_VAL_MASK    0x7U
-#define STM32U5_CLOCK_VAL_SHIFT   16U
+#define STM32_CLOCK_REG_MASK    0xFFU
+#define STM32_CLOCK_REG_SHIFT   0U
+#define STM32_CLOCK_SHIFT_MASK  0x1FU
+#define STM32_CLOCK_SHIFT_SHIFT 8U
+#define STM32_CLOCK_MASK_MASK   0x7U
+#define STM32_CLOCK_MASK_SHIFT  13U
+#define STM32_CLOCK_VAL_MASK    0x7U
+#define STM32_CLOCK_VAL_SHIFT   16U
 
-#define STM32U5_CLOCK(val, mask, shift, reg)					\
-	((((reg) & STM32U5_CLOCK_REG_MASK) << STM32U5_CLOCK_REG_SHIFT) |	\
-	 (((shift) & STM32U5_CLOCK_SHIFT_MASK) << STM32U5_CLOCK_SHIFT_SHIFT) |	\
-	 (((mask) & STM32U5_CLOCK_MASK_MASK) << STM32U5_CLOCK_MASK_SHIFT) |	\
-	 (((val) & STM32U5_CLOCK_VAL_MASK) << STM32U5_CLOCK_VAL_SHIFT))
-
-
-/* Accessors for clock value */
-
-/**
- * @brief Obtain register field from clock configuration.
- *
- * @param clock clock bit field value.
- */
-#define STM32U5_CLOCK_REG_GET(clock) \
-	(((clock) >> STM32U5_CLOCK_REG_SHIFT) & STM32U5_CLOCK_REG_MASK)
-
-/**
- * @brief Obtain position field from clock configuration.
- *
- * @param clock Clock bit field value.
- */
-#define STM32U5_CLOCK_SHIFT_GET(clock) \
-	(((clock) >> STM32U5_CLOCK_SHIFT_SHIFT) & STM32U5_CLOCK_SHIFT_MASK)
-
-/**
- * @brief Obtain mask field from clock configuration.
- *
- * @param clock Clock bit field value.
- */
-#define STM32U5_CLOCK_MASK_GET(clock) \
-	(((clock) >> STM32U5_CLOCK_MASK_SHIFT) & STM32U5_CLOCK_MASK_MASK)
-
-/**
- * @brief Obtain value field from clock configuration.
- *
- * @param clock Clock bit field value.
- */
-#define STM32U5_CLOCK_VAL_GET(clock) \
-	(((clock) >> STM32U5_CLOCK_VAL_SHIFT) & STM32U5_CLOCK_VAL_MASK)
+#define STM32_CLOCK(val, mask, shift, reg)					\
+	((((reg) & STM32_CLOCK_REG_MASK) << STM32_CLOCK_REG_SHIFT) |	\
+	 (((shift) & STM32_CLOCK_SHIFT_MASK) << STM32_CLOCK_SHIFT_SHIFT) |	\
+	 (((mask) & STM32_CLOCK_MASK_MASK) << STM32_CLOCK_MASK_SHIFT) |	\
+	 (((val) & STM32_CLOCK_VAL_MASK) << STM32_CLOCK_VAL_SHIFT))
 
 /** @brief RCC_CCIPRx register offset (RM0456.pdf) */
 #define CCIPR1_REG		0xE0
@@ -121,37 +86,37 @@
 
 /** @brief Device clk sources selection helpers (RM0399.pdf) */
 /** CCIPR1 devices */
-#define USART1_SEL(val)		STM32U5_CLOCK(val, 3, 0, CCIPR1_REG)
-#define USART2_SEL(val)		STM32U5_CLOCK(val, 3, 2, CCIPR1_REG)
-#define USART3_SEL(val)		STM32U5_CLOCK(val, 3, 4, CCIPR1_REG)
-#define USART4_SEL(val)		STM32U5_CLOCK(val, 3, 6, CCIPR1_REG)
-#define USART5_SEL(val)		STM32U5_CLOCK(val, 3, 8, CCIPR1_REG)
-#define I2C1_SEL(val)		STM32U5_CLOCK(val, 3, 10, CCIPR1_REG)
-#define I2C2_SEL(val)		STM32U5_CLOCK(val, 3, 12, CCIPR1_REG)
-#define I2C4_SEL(val)		STM32U5_CLOCK(val, 3, 14, CCIPR1_REG)
-#define SPI2_SEL(val)		STM32U5_CLOCK(val, 3, 16, CCIPR1_REG)
-#define LPTIM2_SEL(val)		STM32U5_CLOCK(val, 3, 18, CCIPR1_REG)
-#define SPI1_SEL(val)		STM32U5_CLOCK(val, 3, 20, CCIPR1_REG)
-#define SYSTICK_SEL(val)	STM32U5_CLOCK(val, 3, 22, CCIPR1_REG)
-#define FDCAN1_SEL(val)		STM32U5_CLOCK(val, 3, 24, CCIPR1_REG)
-#define ICKLK_SEL(val)		STM32U5_CLOCK(val, 3, 26, CCIPR1_REG)
-#define TIMIC_SEL(val)		STM32U5_CLOCK(val, 7, 29, CCIPR1_REG)
+#define USART1_SEL(val)		STM32_CLOCK(val, 3, 0, CCIPR1_REG)
+#define USART2_SEL(val)		STM32_CLOCK(val, 3, 2, CCIPR1_REG)
+#define USART3_SEL(val)		STM32_CLOCK(val, 3, 4, CCIPR1_REG)
+#define USART4_SEL(val)		STM32_CLOCK(val, 3, 6, CCIPR1_REG)
+#define USART5_SEL(val)		STM32_CLOCK(val, 3, 8, CCIPR1_REG)
+#define I2C1_SEL(val)		STM32_CLOCK(val, 3, 10, CCIPR1_REG)
+#define I2C2_SEL(val)		STM32_CLOCK(val, 3, 12, CCIPR1_REG)
+#define I2C4_SEL(val)		STM32_CLOCK(val, 3, 14, CCIPR1_REG)
+#define SPI2_SEL(val)		STM32_CLOCK(val, 3, 16, CCIPR1_REG)
+#define LPTIM2_SEL(val)		STM32_CLOCK(val, 3, 18, CCIPR1_REG)
+#define SPI1_SEL(val)		STM32_CLOCK(val, 3, 20, CCIPR1_REG)
+#define SYSTICK_SEL(val)	STM32_CLOCK(val, 3, 22, CCIPR1_REG)
+#define FDCAN1_SEL(val)		STM32_CLOCK(val, 3, 24, CCIPR1_REG)
+#define ICKLK_SEL(val)		STM32_CLOCK(val, 3, 26, CCIPR1_REG)
+#define TIMIC_SEL(val)		STM32_CLOCK(val, 7, 29, CCIPR1_REG)
 /** CCIPR2 devices */
-#define MDF1_SEL(val)		STM32U5_CLOCK(val, 7, 0, CCIPR2_REG)
-#define SAI1_SEL(val)		STM32U5_CLOCK(val, 7, 5, CCIPR2_REG)
-#define SAI2_SEL(val)		STM32U5_CLOCK(val, 7, 8, CCIPR2_REG)
-#define SAE_SEL(val)		STM32U5_CLOCK(val, 1, 11, CCIPR2_REG)
-#define RNG_SEL(val)		STM32U5_CLOCK(val, 3, 12, CCIPR2_REG)
-#define SDMMC_SEL(val)		STM32U5_CLOCK(val, 1, 14, CCIPR2_REG)
-#define OCTOSPI_SEL(val)	STM32U5_CLOCK(val, 3, 20, CCIPR2_REG)
+#define MDF1_SEL(val)		STM32_CLOCK(val, 7, 0, CCIPR2_REG)
+#define SAI1_SEL(val)		STM32_CLOCK(val, 7, 5, CCIPR2_REG)
+#define SAI2_SEL(val)		STM32_CLOCK(val, 7, 8, CCIPR2_REG)
+#define SAE_SEL(val)		STM32_CLOCK(val, 1, 11, CCIPR2_REG)
+#define RNG_SEL(val)		STM32_CLOCK(val, 3, 12, CCIPR2_REG)
+#define SDMMC_SEL(val)		STM32_CLOCK(val, 1, 14, CCIPR2_REG)
+#define OCTOSPI_SEL(val)	STM32_CLOCK(val, 3, 20, CCIPR2_REG)
 /** CCIPR3 devices */
-#define LPUART1_SEL(val)	STM32U5_CLOCK(val, 7, 0, CCIPR3_REG)
-#define SPI3_SEL(val)		STM32U5_CLOCK(val, 3, 3, CCIPR3_REG)
-#define I2C3_SEL(val)		STM32U5_CLOCK(val, 3, 6, CCIPR3_REG)
-#define LPTIM34_SEL(val)	STM32U5_CLOCK(val, 3, 8, CCIPR3_REG)
-#define LPTIM1_SEL(val)		STM32U5_CLOCK(val, 3, 10, CCIPR3_REG)
-#define ADCDAC_SEL(val)		STM32U5_CLOCK(val, 7, 12, CCIPR3_REG)
-#define DAC1_SEL(val)		STM32U5_CLOCK(val, 1, 15, CCIPR3_REG)
-#define ADF1_SEL(val)		STM32U5_CLOCK(val, 7, 16, CCIPR3_REG)
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_STM32U5_CLOCK_H_ */
+#define LPUART1_SEL(val)	STM32_CLOCK(val, 7, 0, CCIPR3_REG)
+#define SPI3_SEL(val)		STM32_CLOCK(val, 3, 3, CCIPR3_REG)
+#define I2C3_SEL(val)		STM32_CLOCK(val, 3, 6, CCIPR3_REG)
+#define LPTIM34_SEL(val)	STM32_CLOCK(val, 3, 8, CCIPR3_REG)
+#define LPTIM1_SEL(val)		STM32_CLOCK(val, 3, 10, CCIPR3_REG)
+#define ADCDAC_SEL(val)		STM32_CLOCK(val, 7, 12, CCIPR3_REG)
+#define DAC1_SEL(val)		STM32_CLOCK(val, 1, 15, CCIPR3_REG)
+#define ADF1_SEL(val)		STM32_CLOCK(val, 7, 16, CCIPR3_REG)
