ae35c9e58a8bec8b20a7f063a1c26f677ce12684
Raphal <gael.raphal-stg@systerel.fr>
2022-03-31 17:08:05 +0200

    modifing copy/paste from nucleo_h745zi_q_m7 changing names folders and includes

---
+++ boards/arm/stm32h745i_disco/board.cmake
@@ -0,0 +1,8 @@
+# STM32H745I-Disco board configuration
+# Copyright (c) 2022 Systerel (https://www.systerel.fr)
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=STM32H745XI" "--speed=4000")
+
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
