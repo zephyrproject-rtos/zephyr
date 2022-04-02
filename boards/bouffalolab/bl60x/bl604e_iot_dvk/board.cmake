e94837e5b2a0f72edf77593f596ee7d022527e5f
Gerson Fernando Budke <nandojve@gmail.com>
2022-04-02 12:09:04 -0300

    boards: riscv: Introduce bl604e_iot_dvk
    Add initial version.
    
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

---
+++ boards/bouffalolab/bl60x/bl604e_iot_dvk/board.cmake
@@ -0,0 +1,20 @@
+# Copyright (c) 2022-2025 ATL Electronics
+#
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(openocd --cmd-pre-init "source [find bl60x.cfg]")
+
+board_runner_args(openocd --use-elf --no-load --no-init)
+board_runner_args(openocd --gdb-init "set mem inaccessible-by-default off")
+board_runner_args(openocd --gdb-init "set architecture riscv:rv32")
+board_runner_args(openocd --gdb-init "set remotetimeout 250")
+board_runner_args(openocd --gdb-init "set print asm-demangle on")
+board_runner_args(openocd --gdb-init "set backtrace limit 32")
+board_runner_args(openocd --gdb-init "mem 0x22008000 0x22014000 rw")
+board_runner_args(openocd --gdb-init "mem 0x42008000 0x42014000 rw")
+board_runner_args(openocd --gdb-init "mem 0x22014000 0x22020000 rw")
+board_runner_args(openocd --gdb-init "mem 0x42014000 0x42020000 rw")
+board_runner_args(openocd --gdb-init "mem 0x22020000 0x2203C000 rw")
+board_runner_args(openocd --gdb-init "mem 0x42020000 0x4203C000 rw")
+board_runner_args(openocd --gdb-init "mem 0x23000000 0x23400000 ro")
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
