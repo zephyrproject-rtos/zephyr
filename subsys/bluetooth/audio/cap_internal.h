9298c0f96fc09b855fe30b9e311a3df1656bdee3
Emil Gydesen <emil.gydesen@nordicsemi.no>
2022-04-05 16:37:26 +0200

    Bluetooth: Audio: Add CAP acceptor CCID check for TBS
    Add bt_cap_acceptor_ccid_exist that verifies that CCIDs
    from the CAP initiator are valid on the initiator based
    on what we have previously discovered.
    
    Verification from MCS is still a TODO.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

---
+++ subsys/bluetooth/audio/cap_internal.h
@@ -0,0 +1,12 @@
+/*  Bluetooth Audio Common Audio Profile internal header */
+
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/types.h>
+#include <bluetooth/conn.h>
+
+bool bt_cap_acceptor_ccid_exist(const struct bt_conn *conn, uint8_t ccid);
