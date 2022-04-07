23d4788afa88475535d745e79fd82d2245e1f7f8
Herman Berget <herman.berget@nordicsemi.no>
2022-04-07 15:51:47 +0200

    Bluetooth: Host: Add more tests for GATT client becoming change-aware
    For clients with both a single and multiple ATT channels:
     - Read a characteristic before reading the DB hash and then retrying
     - Read the DB hash and then do the reads
     - Retry the reads without reading the DB hash
    
    Signed-off-by: Herman Berget <herman.berget@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_gatt_caching/src/common.h
+++ tests/bluetooth/bsim_bt/bsim_test_gatt_caching/src/common.h
@@ -27,7 +27,7 @@
 
 extern enum bst_result_t bst_result;
 
-#define WAIT_TIME (30 * 1e6) /*seconds*/
+#define WAIT_TIME (60 * 1e6) /*seconds*/
 
 #define CREATE_FLAG(flag) static atomic_t flag = (atomic_t)false
 #define SET_FLAG(flag) (void)atomic_set(&flag, (atomic_t)true)
---
+++ tests/bluetooth/bsim_bt/bsim_test_gatt_caching/test_scripts/gatt_caching_db_hash_read_no_eatt.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Copyright 2022 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+simulation_id="gatt_caching_db_hash_read_no_eatt" \
+    client_id="gatt_client_db_hash_read_no_eatt" \
+    server_id="gatt_server_no_eatt" \
+    $(dirname "${BASH_SOURCE[0]}")/_run_test.sh
