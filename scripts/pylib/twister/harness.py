f99949184291405803fa1978d76ece3e8ca059a0
Anas Nashif <anas.nashif@intel.com>
2022-04-07 16:11:01 -0400

    twister: generate and verify test run id
    Set run id (md5 of instance and a random number) for each test and match
    what was set during build with the output on the screen. This will
    verify that we are evaluating the same test we have uploaded and not
    some previous output on the screen.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/pylib/twister/harness.py
+++ scripts/pylib/twister/harness.py
@@ -13,6 +13,8 @@ class Harness:
     FAULT = "ZEPHYR FATAL ERROR"
     RUN_PASSED = "PROJECT EXECUTION SUCCESSFUL"
     RUN_FAILED = "PROJECT EXECUTION FAILED"
+    run_id_pattern = r"RunID: (?P<run_id>.*)"
+
 
     def __init__(self):
         self.state = None
@@ -33,10 +35,14 @@ class Harness:
         self.ztest = False
         self.is_pytest = False
         self.detected_suite_names = []
+        self.run_id = None
+        self.matched_run_id = False
+        self.run_id_exists = False
 
     def configure(self, instance):
         config = instance.testcase.harness_config
         self.id = instance.testcase.id
+        self.run_id = instance.run_id
         if "ignore_faults" in instance.testcase.tags:
             self.fail_on_fault = False
 
@@ -48,6 +54,13 @@ class Harness:
             self.record = config.get('record', {})
 
     def process_test(self, line):
+
+        runid_match = re.search(self.run_id_pattern, line)
+        if runid_match:
+            run_id = runid_match.group("run_id")
+            self.run_id_exists = True
+            if run_id == str(self.run_id):
+                self.matched_run_id = True
 
         if self.RUN_PASSED in line:
             if self.fault:
