fe90e4932b4b4098883492a178c513a13af58026
Marcin Niestroj <m.niestroj@emb.dev>
2022-04-08 12:03:17 +0200

    wifi: eswifi: use NET_SOCKETS_OFFLOAD_PRIORITY
    Use NET_SOCKETS_OFFLOAD_PRIORITY instead of
    NET_SOCKETS_OFFLOAD_PRIORITY, so that by default offloaded sockets will
    be used instead of native sockets.
    
    Addiitonally this allows to select relative priority of offloaded TLS
    versus native TLS when used together with NET_SOCKETS_TLS_PRIORITY.
    
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

--- drivers/wifi/eswifi/eswifi_socket_offload.c
+++ drivers/wifi/eswifi/eswifi_socket_offload.c
@@ -571,7 +571,7 @@ static const struct socket_op_vtable eswifi_socket_fd_op_vtable = {
 };
 
 #ifdef CONFIG_NET_SOCKETS_OFFLOAD
-NET_SOCKET_REGISTER(eswifi, NET_SOCKET_DEFAULT_PRIO, AF_UNSPEC,
+NET_SOCKET_REGISTER(eswifi, CONFIG_NET_SOCKETS_OFFLOAD_PRIORITY, AF_UNSPEC,
 		    eswifi_socket_is_supported, eswifi_socket_create);
 #endif
 
