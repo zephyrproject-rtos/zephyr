7c583bbf8ff669b9d40e6936e5c27ec196464b08
Christopher Friedt <chrisfriedt@gmail.com>
2022-04-10 12:35:15 -0400

    lib: posix: support for pthread_attr_setstacksize
    Support pthread_attr_setstacksize(3).
    
    See https://pubs.opengroup.org/onlinepubs/7908799/xsh/pthread_attr_getstacksize.html
    
    Fixes #44722
    
    Signed-off-by: Christopher Friedt <chrisfriedt@gmail.com>

--- include/zephyr/posix/pthread.h
+++ include/zephyr/posix/pthread.h
@@ -64,6 +64,9 @@ struct posix_thread {
 /* Passed to pthread_once */
 #define PTHREAD_ONCE_INIT 1
 
+/* The minimum allowable stack size */
+#define PTHREAD_STACK_MIN Z_KERNEL_STACK_SIZE_ADJUST(0)
+
 /**
  * @brief Declare a pthread condition variable
  *
@@ -483,6 +486,7 @@ static inline int pthread_rwlockattr_init(pthread_rwlockattr_t *attr)
 }
 
 int pthread_attr_getstacksize(const pthread_attr_t *attr, size_t *stacksize);
+int pthread_attr_setstacksize(pthread_attr_t *attr, size_t stacksize);
 int pthread_attr_setschedpolicy(pthread_attr_t *attr, int policy);
 int pthread_attr_getschedpolicy(const pthread_attr_t *attr, int *policy);
 int pthread_attr_setdetachstate(pthread_attr_t *attr, int detachstate);
