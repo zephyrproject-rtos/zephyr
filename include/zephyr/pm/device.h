83f1e7d9d0ae9dc4e6b31a032b3d822db74b5b3b
Jordan Yates <jordan.yates@data61.csiro.au>
2022-04-11 21:24:58 +1000

    pm: device: add driver init helper
    Adds a helper function for initializing devices into the expected power
    state, through the devices `pm_device_action_cb_t`. This eliminates code
    duplication between the init functions and the PM callback.
    
    The expected device states in order of priority are:
     * No power applied to device, `OFF`
     * `zephyr,pm-device-runtime-auto` enabled, `SUSPEND`
     * Otherwise, `ACTIVE`
    
    Signed-off-by: Jordan Yates <jordan.yates@data61.csiro.au>

--- include/zephyr/pm/device.h
+++ include/zephyr/pm/device.h
@@ -581,6 +581,22 @@ int pm_device_power_domain_remove(const struct device *dev,
  * @retval false If device is not currently powered
  */
 bool pm_device_is_powered(const struct device *dev);
+
+/**
+ * @brief Setup a device driver into the lowest valid power mode
+ *
+ * This helper function is intended to be called at the end of a driver
+ * init function to automatically setup the device into the lowest power
+ * mode. It assumes that the device has been configured as if it is in
+ * @ref PM_DEVICE_STATE_OFF.
+ *
+ * @param dev Device instance.
+ * @param action_cb Device PM control callback function.
+ * @retval 0 On success.
+ * @retval -errno Error code from @a action_cb on failure.
+ */
+int pm_device_driver_init(const struct device *dev, pm_device_action_cb_t action_cb);
+
 #else
 static inline int pm_device_state_get(const struct device *dev,
 				      enum pm_device_state *state)
@@ -667,6 +683,19 @@ static inline bool pm_device_is_powered(const struct device *dev)
 	ARG_UNUSED(dev);
 	return true;
 }
+
+static inline int pm_device_driver_init(const struct device *dev, pm_device_action_cb_t action_cb)
+{
+	int rc;
+
+	/* When power management is not enabled, all drivers should initialise to active state */
+	rc = action_cb(dev, PM_DEVICE_ACTION_TURN_ON);
+	if (rc == 0) {
+		rc = action_cb(dev, PM_DEVICE_ACTION_RESUME);
+	}
+	return rc;
+}
+
 #endif /* CONFIG_PM_DEVICE */
 
 /** @} */
