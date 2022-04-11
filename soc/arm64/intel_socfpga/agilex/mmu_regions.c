6ae49985d7d26b80bc1af694cd7f6b09ac860a3b
Yuslaimi, Alif Zakuan <alif.zakuan.yuslaimi@intel.com>
2022-04-11 13:29:53 +0800

    soc: arm64: intel_socfpga: Device driver nodes restructuring
    This patch accommodates the device driver nodes restructuring in the
    device tree by assigning index number to each driver in the periph
    driver node.
    
    Signed-off-by: Yuslaimi, Alif Zakuan <alif.zakuan.yuslaimi@intel.com>

--- soc/arm64/intel_socfpga/agilex/mmu_regions.c
+++ soc/arm64/intel_socfpga/agilex/mmu_regions.c
@@ -9,17 +9,6 @@
 
 static const struct arm_mmu_region mmu_regions[] = {
 
-	/* System manager register that required by clock driver */
-	MMU_REGION_FLAT_ENTRY("SYSTEM_MANAGER",
-			      DT_REG_ADDR(DT_NODELABEL(sysmgr)),
-			      DT_REG_SIZE(DT_NODELABEL(sysmgr)),
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_DEFAULT_SECURE_STATE),
-
-	MMU_REGION_FLAT_ENTRY("RESET_MANAGER",
-			      DT_REG_ADDR(DT_NODELABEL(rstmgr)),
-			      DT_REG_SIZE(DT_NODELABEL(rstmgr)),
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_DEFAULT_SECURE_STATE),
-
 	MMU_REGION_FLAT_ENTRY("CLOCK",
 			      DT_REG_ADDR(DT_NODELABEL(clock)),
 			      DT_REG_SIZE(DT_NODELABEL(clock)),
@@ -37,8 +26,24 @@ static const struct arm_mmu_region mmu_regions[] = {
 
 	/* Peripheral to cover UART, I2C, I2C EMAC and Timer SP */
 	MMU_REGION_FLAT_ENTRY("PERIPHERAL",
-			      DT_REG_ADDR(DT_NODELABEL(periph)),
-			      DT_REG_SIZE(DT_NODELABEL(periph)),
+			      DT_REG_ADDR_BY_IDX(DT_NODELABEL(periph), 0),
+			      DT_REG_SIZE_BY_IDX(DT_NODELABEL(periph), 0),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_DEFAULT_SECURE_STATE),
+
+	MMU_REGION_FLAT_ENTRY("RESET_MANAGER",
+			      DT_REG_ADDR_BY_IDX(DT_NODELABEL(periph), 1),
+			      DT_REG_SIZE_BY_IDX(DT_NODELABEL(periph), 1),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_DEFAULT_SECURE_STATE),
+
+	/* System manager register that required by clock driver */
+	MMU_REGION_FLAT_ENTRY("SYSTEM_MANAGER",
+			      DT_REG_ADDR_BY_IDX(DT_NODELABEL(periph), 2),
+			      DT_REG_SIZE_BY_IDX(DT_NODELABEL(periph), 2),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_DEFAULT_SECURE_STATE),
+
+	MMU_REGION_FLAT_ENTRY("PINMUX",
+			      DT_REG_ADDR_BY_IDX(DT_NODELABEL(periph), 3),
+			      DT_REG_SIZE_BY_IDX(DT_NODELABEL(periph), 3),
 			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_DEFAULT_SECURE_STATE),
 
 	MMU_REGION_FLAT_ENTRY("SDMMC",
@@ -54,11 +59,6 @@ static const struct arm_mmu_region mmu_regions[] = {
 	MMU_REGION_FLAT_ENTRY("GIC",
 			      DT_REG_ADDR_BY_IDX(DT_NODELABEL(gic), 1),
 			      DT_REG_SIZE_BY_IDX(DT_NODELABEL(gic), 1),
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_DEFAULT_SECURE_STATE),
-
-	MMU_REGION_FLAT_ENTRY("PINMUX",
-			      DT_REG_ADDR(DT_NODELABEL(pinmux)),
-			      DT_REG_SIZE(DT_NODELABEL(pinmux)),
 			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_DEFAULT_SECURE_STATE),
 };
 
