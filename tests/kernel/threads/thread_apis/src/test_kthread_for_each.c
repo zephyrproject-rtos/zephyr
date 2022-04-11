ed0882e75f29666e42455f9375034fd50b6d1946
Peter Mitsis <peter.mitsis@intel.com>
2022-04-11 19:54:23 -0400

    kernel: update k_thread_state_str() API
    When threads are in more than one state at a time, k_thread_state_str()
    returns a string that lists each of its states delimited by a '+'.
    This in turn necessitates a change to the API that includes both a
    pointer to the buffer to use for the string and the size of the buffer.
    
    Signed-off-by: Peter Mitsis <peter.mitsis@intel.com>

--- tests/kernel/threads/thread_apis/src/test_kthread_for_each.c
+++ tests/kernel/threads/thread_apis/src/test_kthread_for_each.c
@@ -219,32 +219,50 @@ void test_k_thread_foreach_unlocked_null_cb(void)
  */
 void test_k_thread_state_str(void)
 {
+	char state_str[32];
+	const char *str;
 	k_tid_t tid = &tdata1;
 
 	tid->base.thread_state = 0;
-	zassert_true(strcmp(k_thread_state_str(tid), "") == 0, NULL);
+	str = k_thread_state_str(tid, state_str, sizeof(state_str));
+	zassert_true(strcmp(str, "") == 0, NULL);
 
 	tid->base.thread_state = _THREAD_DUMMY;
-	zassert_true(strcmp(k_thread_state_str(tid), "dummy") == 0, NULL);
+
+	str = k_thread_state_str(tid, NULL, sizeof(state_str));
+	zassert_true(strcmp(str, "") == 0, NULL);
+
+	str = k_thread_state_str(tid, state_str, 0);
+	zassert_true(strcmp(str, "") == 0, NULL);
+
+	str = k_thread_state_str(tid, state_str, sizeof(state_str));
+	zassert_true(strcmp(str, "dummy") == 0, NULL);
 
 	tid->base.thread_state = _THREAD_PENDING;
-	zassert_true(strcmp(k_thread_state_str(tid), "pending") == 0, NULL);
+	str = k_thread_state_str(tid, state_str, sizeof(state_str));
+	zassert_true(strcmp(str, "pending") == 0, NULL);
 
 	tid->base.thread_state = _THREAD_PRESTART;
-	zassert_true(strcmp(k_thread_state_str(tid), "prestart") == 0, NULL);
+	str = k_thread_state_str(tid, state_str, sizeof(state_str));
+	zassert_true(strcmp(str, "prestart") == 0, NULL);
 
 	tid->base.thread_state = _THREAD_DEAD;
-	zassert_true(strcmp(k_thread_state_str(tid), "dead") == 0, NULL);
+	str = k_thread_state_str(tid, state_str, sizeof(state_str));
+	zassert_true(strcmp(str, "dead") == 0, NULL);
 
 	tid->base.thread_state = _THREAD_SUSPENDED;
-	zassert_true(strcmp(k_thread_state_str(tid), "suspended") == 0, NULL);
+	str = k_thread_state_str(tid, state_str, sizeof(state_str));
+	zassert_true(strcmp(str, "suspended") == 0, NULL);
 
 	tid->base.thread_state = _THREAD_ABORTING;
-	zassert_true(strcmp(k_thread_state_str(tid), "aborting") == 0, NULL);
+	str = k_thread_state_str(tid, state_str, sizeof(state_str));
+	zassert_true(strcmp(str, "aborting") == 0, NULL);
 
 	tid->base.thread_state = _THREAD_QUEUED;
-	zassert_true(strcmp(k_thread_state_str(tid), "queued") == 0, NULL);
+	str = k_thread_state_str(tid, state_str, sizeof(state_str));
+	zassert_true(strcmp(str, "queued") == 0, NULL);
 
-	tid->base.thread_state = 0xFF;
-	zassert_true(strcmp(k_thread_state_str(tid), "unknown") == 0, NULL);
+	tid->base.thread_state = _THREAD_PENDING | _THREAD_SUSPENDED;
+	str = k_thread_state_str(tid, state_str, sizeof(state_str));
+	zassert_true(strcmp(str, "pending+suspended") == 0, NULL);
 }
