15ad99e67960e740d4f78fcc1f1972385a821fde
Steffen Jahnke <steffen.jahnke@eu.panasonic.com>
2022-04-14 13:56:37 +0200

    boards: Add PAN1781 evaluation board
    The PAN1781 evaluation board is a development tool for the nRF52820
    from Nordic Semiconductor.
    
    Signed-off-by: Steffen Jahnke <steffen.jahnke@eu.panasonic.com>

---
+++ boards/arm/pan1781_evb/board.cmake
@@ -0,0 +1,11 @@
+# PAN1781 EVB configuration
+
+# Copyright (c) 2022 Panasonic Industrial Devices Europe GmbH
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
+board_runner_args(pyocd "--target=nrf52820" "--frequency=4000000")
+
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
