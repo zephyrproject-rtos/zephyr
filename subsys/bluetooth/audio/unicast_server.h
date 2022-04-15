a22bbee33aa408944046b2923404264ab57e6c51
Emil Gydesen <emil.gydesen@nordicsemi.no>
2022-04-15 13:16:08 +0200

    Bluetooth: Audio: Add unicast server stream release
    Update bt_audio_stream_release to also support the unicast
    server.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/audio/unicast_server.h
+++ subsys/bluetooth/audio/unicast_server.h
@@ -9,3 +9,5 @@
 #include <zephyr/bluetooth/audio/audio.h>
 
 extern const struct bt_audio_unicast_server_cb *unicast_server_cb;
+
+int bt_unicast_server_release(struct bt_audio_stream *stream, bool cache);
