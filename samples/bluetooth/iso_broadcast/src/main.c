db84c7d6798935a033a3518901c91c12a615f6d5
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2022-04-22 18:16:27 +0530

    samples: Bluetooth: iso_broadcast: Updated to broadcast 2 streams
    Updated the iso_broadcast to configure and broadcast 2 BISes
    in the created BIG.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- samples/bluetooth/iso_broadcast/src/main.c
+++ samples/bluetooth/iso_broadcast/src/main.c
@@ -8,10 +8,11 @@
 #include <zephyr/bluetooth/iso.h>
 #include <zephyr/sys/byteorder.h>
 
+#define BUF_ALLOC_TIMEOUT (10) /* milliseconds */
 #define BIG_TERMINATE_TIMEOUT_US (60 * USEC_PER_SEC) /* microseconds */
 #define BIG_SDU_INTERVAL_US (10000)
 
-#define BIS_ISO_CHAN_COUNT 1
+#define BIS_ISO_CHAN_COUNT 2
 NET_BUF_POOL_FIXED_DEFINE(bis_tx_pool, BIS_ISO_CHAN_COUNT,
 			  BT_ISO_SDU_BUF_SIZE(CONFIG_BT_ISO_TX_MTU), 8, NULL);
 
@@ -45,7 +46,7 @@ static struct bt_iso_chan_ops iso_ops = {
 
 static struct bt_iso_chan_io_qos iso_tx_qos = {
 	.sdu = sizeof(uint32_t), /* bytes */
-	.rtn = 2,
+	.rtn = 1,
 	.phy = BT_GAP_LE_PHY_2M,
 };
 
@@ -53,31 +54,34 @@ static struct bt_iso_chan_qos bis_iso_qos = {
 	.tx = &iso_tx_qos,
 };
 
-static struct bt_iso_chan bis_iso_chan = {
-	.ops = &iso_ops,
-	.qos = &bis_iso_qos,
+static struct bt_iso_chan bis_iso_chan[] = {
+	{ .ops = &iso_ops, .qos = &bis_iso_qos, },
+	{ .ops = &iso_ops, .qos = &bis_iso_qos, },
 };
 
-static struct bt_iso_chan *bis[BIS_ISO_CHAN_COUNT] = { &bis_iso_chan };
+static struct bt_iso_chan *bis[] = {
+	&bis_iso_chan[0],
+	&bis_iso_chan[1],
+};
 
 static struct bt_iso_big_create_param big_create_param = {
 	.num_bis = BIS_ISO_CHAN_COUNT,
 	.bis_channels = bis,
 	.interval = BIG_SDU_INTERVAL_US, /* in microseconds */
-	.latency = 10, /* milliseconds */
+	.latency = 10, /* in milliseconds */
 	.packing = 0, /* 0 - sequential, 1 - interleaved */
 	.framing = 0, /* 0 - unframed, 1 - framed */
 };
 
 void main(void)
 {
+	uint32_t timeout_counter = INITIAL_TIMEOUT_COUNTER;
 	struct bt_le_ext_adv *adv;
 	struct bt_iso_big *big;
 	int err;
+
 	uint32_t iso_send_count = 0;
 	uint8_t iso_data[sizeof(iso_send_count)] = { 0 };
-	struct net_buf *buf;
-	uint32_t timeout_counter = INITIAL_TIMEOUT_COUNTER;
 
 	printk("Starting ISO Broadcast Demo\n");
 
@@ -137,17 +141,32 @@ void main(void)
 
 		k_sleep(K_USEC(big_create_param.interval));
 
-		buf = net_buf_alloc(&bis_tx_pool, K_FOREVER);
-		net_buf_reserve(buf, BT_ISO_CHAN_SEND_RESERVE);
-		sys_put_le32(++iso_send_count, iso_data);
-		net_buf_add_mem(buf, iso_data, sizeof(iso_data));
-		ret = bt_iso_chan_send(&bis_iso_chan, buf, seq_num++,
-				       BT_ISO_TIMESTAMP_NONE);
-		if (ret < 0) {
-			printk("Unable to broadcast data: %d", ret);
-			net_buf_unref(buf);
-			return;
+		for (uint8_t chan = 0U; chan < BIS_ISO_CHAN_COUNT; chan++) {
+			struct net_buf *buf;
+
+			buf = net_buf_alloc(&bis_tx_pool,
+					    K_MSEC(BUF_ALLOC_TIMEOUT));
+			if (!buf) {
+				printk("Data buffer allocate timeout on channel"
+				       " %u\n", chan);
+				return;
+			}
+			net_buf_reserve(buf, BT_ISO_CHAN_SEND_RESERVE);
+			sys_put_le32(iso_send_count, iso_data);
+			net_buf_add_mem(buf, iso_data, sizeof(iso_data));
+			ret = bt_iso_chan_send(&bis_iso_chan[chan], buf,
+					       seq_num, BT_ISO_TIMESTAMP_NONE);
+			if (ret < 0) {
+				printk("Unable to broadcast data on channel %u"
+				       " : %d", chan, ret);
+				net_buf_unref(buf);
+				return;
+			}
+
 		}
+
+		iso_send_count++;
+		seq_num++;
 
 		if ((iso_send_count % 100) == 0) {
 			printk("Sending value %u\n", iso_send_count);
