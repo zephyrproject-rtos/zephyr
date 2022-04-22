f868841a25fc7ab3fcdb81b22533240b712a2cfb
Morten Priess <mtpr@oticon.com>
2022-04-22 15:46:07 +0200

    Bluetooth: controller: Asynch stop of ISO resume ticker in disable
    Do not use ull_ticker_stop_with_mark for ISO one-shot resume ticker, but
    stop without checking result. If active this will stop it, otherwise it
    is ignored. Also, this prevents calling lll_disable twice.
    
    Signed-off-by: Morten Priess <mtpr@oticon.com>

--- subsys/bluetooth/controller/ll_sw/ull_conn_iso.c
+++ subsys/bluetooth/controller/ll_sw/ull_conn_iso.c
@@ -727,8 +727,9 @@ static void disable(uint16_t handle)
 
 	cig = ll_conn_iso_group_get(handle);
 
-	(void)ull_ticker_stop_with_mark(TICKER_ID_CONN_ISO_RESUME_BASE + handle,
-					cig, &cig->lll);
+	(void)ticker_stop(TICKER_INSTANCE_ID_CTLR, TICKER_USER_ID_THREAD,
+			  TICKER_ID_CONN_ISO_RESUME_BASE + handle, NULL,
+			  NULL);
 
 	err = ull_ticker_stop_with_mark(TICKER_ID_CONN_ISO_BASE + handle,
 					cig, &cig->lll);
