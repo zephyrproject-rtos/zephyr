bcdd5b630f891364848ca82771a37b5ed1c74a69
Enjia Mai <enjia.mai@intel.com>
2022-04-25 23:47:55 +0800

    boards: xtensa: Activate the intel_adsp west runner
    Make the intel_adsp west runner starting to work on all the
    intel_adsp boards. Changes include:
    
    1. Make the cavstool.py work as a service in remote host
       ADSP board and rename it to cavstool_server.py.
    
    2. Active the runner and adds a common board.cmake file to
       specify the default signing key for cavs boards.
    
    Signed-off-by: Enjia Mai <enjia.mai@intel.com>

---
+++ boards/common/intel_adsp.board.cmake
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(intel_adsp "--default-key=${RIMAGE_SIGN_KEY}")
+
+board_finalize_runner_args(intel_adsp)
--- boards/xtensa/intel_adsp_cavs25/board.cmake
+++ boards/xtensa/intel_adsp_cavs25/board.cmake
@@ -1,7 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_set_flasher_ifnset(misc-flasher)
-board_finalize_runner_args(misc-flasher)
+board_set_flasher_ifnset(intel_adsp)
+
+set(RIMAGE_SIGN_KEY otc_private_key_3k.pem)
 
 if(CONFIG_BOARD_INTEL_ADSP_CAVS25)
 board_set_rimage_target(tgl)
@@ -10,3 +11,5 @@ endif()
 if(CONFIG_BOARD_INTEL_ADSP_CAVS25_TGPH)
 board_set_rimage_target(tgl-h)
 endif()
+
+include(${ZEPHYR_BASE}/boards/common/intel_adsp.board.cmake)
