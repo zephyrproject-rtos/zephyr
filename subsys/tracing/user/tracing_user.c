fad15a14f71cd7026d09c1ccd8f4846f8db248af
Eugene Cohen <quic_egmc@quicinc.com>
2022-04-25 15:39:28 -0600

    tracing: add additional user tracing thread calls
    Add additional user tracing calls for thread states, names
    and priorities.
    
    add the following user tracing calls:
    
    sys_trace_thread_create_user
    sys_trace_thread_abort_user
    sys_trace_thread_suspend_user
    sys_trace_thread_resume_user
    sys_trace_thread_name_set_user
    sys_trace_thread_info_user
    sys_trace_thread_priority_set_user
    sys_trace_thread_sched_ready_user
    sys_trace_thread_pend_user
    
    Signed-off-by: Eugene Cohen <quic_egmc@quicinc.com>

--- subsys/tracing/user/tracing_user.c
+++ subsys/tracing/user/tracing_user.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020 Lexmark International, Inc.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -11,11 +12,45 @@
 
 static int nested_interrupts[CONFIG_MP_NUM_CPUS];
 
+void __weak sys_trace_thread_create_user(struct k_thread *thread) {}
+void __weak sys_trace_thread_abort_user(struct k_thread *thread) {}
+void __weak sys_trace_thread_suspend_user(struct k_thread *thread) {}
+void __weak sys_trace_thread_resume_user(struct k_thread *thread) {}
+void __weak sys_trace_thread_name_set_user(struct k_thread *thread) {}
 void __weak sys_trace_thread_switched_in_user(struct k_thread *thread) {}
 void __weak sys_trace_thread_switched_out_user(struct k_thread *thread) {}
+void __weak sys_trace_thread_info_user(struct k_thread *thread) {}
+void __weak sys_trace_thread_sched_ready_user(struct k_thread *thread) {}
+void __weak sys_trace_thread_pend_user(struct k_thread *thread) {}
+void __weak sys_trace_thread_priority_set_user(struct k_thread *thread, int prio) {}
 void __weak sys_trace_isr_enter_user(int nested_interrupts) {}
 void __weak sys_trace_isr_exit_user(int nested_interrupts) {}
 void __weak sys_trace_idle_user(void) {}
+
+void sys_trace_thread_create(struct k_thread *thread)
+{
+	sys_trace_thread_create_user(thread);
+}
+
+void sys_trace_thread_abort(struct k_thread *thread)
+{
+	sys_trace_thread_abort_user(thread);
+}
+
+void sys_trace_thread_suspend(struct k_thread *thread)
+{
+	sys_trace_thread_suspend_user(thread);
+}
+
+void sys_trace_thread_resume(struct k_thread *thread)
+{
+	sys_trace_thread_resume_user(thread);
+}
+
+void sys_trace_thread_name_set(struct k_thread *thread)
+{
+	sys_trace_thread_name_set_user(thread);
+}
 
 void sys_trace_k_thread_switched_in(void)
 {
@@ -37,6 +72,26 @@ void sys_trace_k_thread_switched_out(void)
 	sys_trace_thread_switched_out_user(z_current_get());
 
 	irq_unlock(key);
+}
+
+void sys_trace_thread_info(struct k_thread *thread)
+{
+	sys_trace_thread_info_user(thread);
+}
+
+void sys_trace_thread_sched_priority_set(struct k_thread *thread, int prio)
+{
+	sys_trace_thread_priority_set_user(thread, prio);
+}
+
+void sys_trace_thread_sched_ready(struct k_thread *thread)
+{
+	sys_trace_thread_sched_ready_user(thread);
+}
+
+void sys_trace_thread_pend(struct k_thread *thread)
+{
+	sys_trace_thread_pend_user(thread);
 }
 
 void sys_trace_isr_enter(void)
