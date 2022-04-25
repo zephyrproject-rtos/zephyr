045653719d740eb4c0f353f81da7a63462f3b111
Carlo Caione <ccaione@baylibre.com>
2022-04-25 10:45:51 +0200

    tests: Misc test fixes for fvp_base_revc_2xaemv8a
    Fix several tests for the fvp_base_revc_2xaemv8a board.
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

--- tests/ztest/error_hook/src/main.c
+++ tests/ztest/error_hook/src/main.c
@@ -120,7 +120,7 @@ __no_optimization static void trigger_fault_divide_zero(void)
 	defined(CONFIG_BOARD_QEMU_CORTEX_A53) || defined(CONFIG_SOC_QEMU_ARC) || \
 	defined(CONFIG_ARMV6_M_ARMV8_M_BASELINE) || \
 	defined(CONFIG_BOARD_QEMU_CORTEX_R5) || \
-	defined(CONFIG_BOARD_FVP_BASER_AEMV8R)
+	defined(CONFIG_BOARD_FVP_BASER_AEMV8R) || defined(CONFIG_BOARD_FVP_BASE_REVC_2XAEMV8A)
 	ztest_test_skip();
 #endif
 }
