ffa1532be249f4096cefdf302ce72d3610858d60
Francois Ramu <francois.ramu@st.com>
2022-04-26 16:51:40 +0200

    samples: drivers flash add a test case for stm32 octospi
    This add a new sample application to test and validate
    the stm32 octospi driver on the NOR octo-flash present on
    stm32 disco kits
    There should be a special case to erase the complete flash.
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- samples/drivers/spi_flash/src/main.c
+++ samples/drivers/spi_flash/src/main.c
@@ -22,6 +22,9 @@
 #elif DT_NODE_HAS_STATUS(DT_INST(0, st_stm32_qspi_nor), okay)
 #define FLASH_DEVICE DT_LABEL(DT_INST(0, st_stm32_qspi_nor))
 #define FLASH_NAME "JEDEC QSPI-NOR"
+#elif DT_NODE_HAS_STATUS(DT_INST(0, st_stm32_ospi_nor), okay)
+#define FLASH_DEVICE DT_LABEL(DT_INST(0, st_stm32_ospi_nor))
+#define FLASH_NAME "JEDEC OSPI-NOR"
 #else
 #error Unsupported flash driver
 #endif
@@ -67,6 +70,7 @@ void main(void)
 	 */
 	printf("\nTest 1: Flash erase\n");
 
+	/* full flash erase if FLASH_TEST_REGION_OFFSET = 0  FLASH_SECTOR_SIZE = flash size */
 	rc = flash_erase(flash_dev, FLASH_TEST_REGION_OFFSET,
 			 FLASH_SECTOR_SIZE);
 	if (rc != 0) {
