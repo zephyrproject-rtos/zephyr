7a49de12afb600c56555917e78c039e3949baf27
Emil Gydesen <emil.gydesen@nordicsemi.no>
2022-04-28 14:47:39 +0200

    Bluetooth: Audio: Implement CAP initiator discover
    This is the primary discovery of the CAS on the remote device,
    but does not include discovering the (optional) CSIS.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/audio/cap_initiator.c
+++ subsys/bluetooth/audio/cap_initiator.c
@@ -18,7 +18,19 @@ static const struct bt_cap_initiator_cb *cap_cb;
 
 int bt_cap_initiator_register_cb(const struct bt_cap_initiator_cb *cb)
 {
-	return -ENOSYS;
+	CHECKIF(cb == NULL) {
+		BT_DBG("cb is NULL");
+		return -EINVAL;
+	}
+
+	CHECKIF(cap_cb != NULL) {
+		BT_DBG("callbacks already registered");
+		return -EALREADY;
+	}
+
+	cap_cb = cb;
+
+	return 0;
 }
 
 #if defined(CONFIG_BT_AUDIO_BROADCAST_SOURCE)
@@ -44,10 +56,62 @@ int bt_cap_initiator_broadcast_audio_stop(struct bt_audio_broadcast_source *broa
 #endif /* CONFIG_BT_AUDIO_BROADCAST_SOURCE */
 
 #if defined(CONFIG_BT_AUDIO_UNICAST_CLIENT)
+static const struct bt_uuid *cas_uuid = BT_UUID_CAS;
+
+static struct {
+	struct bt_gatt_discover_params param;
+} bt_cap_unicast_clients[CONFIG_BT_MAX_CONN];
+
+static uint8_t cap_unicast_discover_cb(struct bt_conn *conn,
+				       const struct bt_gatt_attr *attr,
+				       struct bt_gatt_discover_params *params)
+{
+	int err;
+
+	params->func = NULL;
+
+	if (attr == NULL) {
+		err = -ENODATA;
+	} else {
+		err = 0;
+	}
+
+	if (cap_cb && cap_cb->discovery_complete) {
+		cap_cb->discovery_complete(conn, err);
+	}
+
+	return BT_GATT_ITER_STOP;
+}
 
 int bt_cap_initiator_unicast_discover(struct bt_conn *conn)
 {
-	return -ENOSYS;
+	struct bt_gatt_discover_params *param;
+	int err;
+
+	CHECKIF(conn == NULL) {
+		BT_DBG("NULL conn");
+		return -EINVAL;
+	}
+
+	param = &bt_cap_unicast_clients[bt_conn_index(conn)].param;
+
+	/* use param->func to tell if a client is "busy" */
+	if (param->func != NULL) {
+		return -EBUSY;
+	}
+
+	param->func = cap_unicast_discover_cb;
+	param->uuid = cas_uuid;
+	param->type = BT_GATT_DISCOVER_PRIMARY;
+	param->start_handle = BT_ATT_FIRST_ATTRIBUTE_HANDLE;
+	param->end_handle = BT_ATT_LAST_ATTRIBUTE_HANDLE;
+
+	err = bt_gatt_discover(conn, param);
+	if (err != 0) {
+		param->func = NULL;
+	}
+
+	return err;
 }
 
 int bt_cap_initiator_unicast_audio_start(const struct bt_cap_unicast_audio_start_param *param,
