c659765cf3e4c87c4b62a8d12b990b3b5f5bd5d6
Morten Priess <mtpr@oticon.com>
2022-04-29 09:36:45 +0200

    test: bluetooth: Fix hci_test_app iso_data_pool element size
    Size of ISO RX data pool did not account for the EDTT timestamp.
    
    Signed-off-by: Morten Priess <mtpr@oticon.com>

--- tests/bluetooth/bsim_bt/edtt_ble_test_app/hci_test_app/src/main.c
+++ tests/bluetooth/bsim_bt/edtt_ble_test_app/hci_test_app/src/main.c
@@ -21,6 +21,7 @@
 #include <zephyr/bluetooth/l2cap.h>
 #include <zephyr/bluetooth/hci_vs.h>
 #include <zephyr/bluetooth/hci_raw.h>
+#include <zephyr/bluetooth/iso.h>
 
 #include "edtt_driver.h"
 #include "bs_tracing.h"
@@ -199,7 +200,8 @@ NET_BUF_POOL_FIXED_DEFINE(data_pool, CONFIG_BT_CTLR_RX_BUFFERS + 14,
 static K_FIFO_DEFINE(data_queue);
 #if defined(CONFIG_BT_ISO)
 NET_BUF_POOL_FIXED_DEFINE(iso_data_pool, CONFIG_BT_ISO_RX_BUF_COUNT + 14,
-			  CONFIG_BT_ISO_RX_MTU + 4, NULL);
+			  BT_ISO_SDU_BUF_SIZE(CONFIG_BT_ISO_RX_MTU) +
+			  sizeof(uint32_t), 8, NULL);
 static K_FIFO_DEFINE(iso_data_queue);
 #endif /* CONFIG_BT_ISO */
 
