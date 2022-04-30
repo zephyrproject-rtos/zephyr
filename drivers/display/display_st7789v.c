694561448486020816f898aa004e8701a6028e28
Jonathan Gonzaga <jonathan.s.gonzaga@gmail.com>
2022-04-29 22:36:30 -0300

    drivers: display: display_st7789v: add check to existence of inverted-sck property
    If inverted-sck property exists, add SPI_MODE_CPOL
    
    Signed-off-by: Jonathan Gonzaga <jonathan.s.gonzaga@gmail.com>

--- drivers/display/display_st7789v.c
+++ drivers/display/display_st7789v.c
@@ -422,7 +422,7 @@ static const struct display_driver_api st7789v_api = {
 #define ST7789V_INIT(inst)							\
 	static const struct st7789v_config st7789v_config_ ## inst = {		\
 		.bus = SPI_DT_SPEC_INST_GET(inst, SPI_OP_MODE_MASTER |          \
-							  SPI_WORD_SET(8), 0),	\
+			SPI_WORD_SET(8) | (DT_INST_PROP(inst, inverted_sck) ? SPI_MODE_CPOL : 0), 0),	\
 		.cmd_data_gpio = GPIO_DT_SPEC_INST_GET(inst, cmd_data_gpios),	\
 		.reset_gpio = GPIO_DT_SPEC_INST_GET_OR(inst, reset_gpios, {}),	\
 		.vcom = DT_INST_PROP(inst, vcom),				\
