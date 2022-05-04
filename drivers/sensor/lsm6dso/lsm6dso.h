6c77989003fd3d4399d220b2d592cf84c907351e
Trent Piepho <trent.piepho@igorinstitute.com>
2022-05-03 22:05:20 -0700

    drivers/sensor: lsm6dso: Add support for lsm6dso32
    This sensor is virtually identical to the lsm6dso.  The only difference
    is the accelerometer ranges are double those of the lsm6dso.
    
    Use the same driver.  The difference is detected by using "st,lsm6dso32"
    as the first compatible entry, followed by "st,lsm6dso".
    
    An bit flag in the existing accel_range config field is used to check if
    the chip is the doubled range or not.
    
    Signed-off-by: Trent Piepho <trent.piepho@igorinstitute.com>

--- drivers/sensor/lsm6dso/lsm6dso.h
+++ drivers/sensor/lsm6dso/lsm6dso.h
@@ -52,6 +52,8 @@ struct lsm6dso_config {
 	} stmemsc_cfg;
 	uint8_t accel_pm;
 	uint8_t accel_odr;
+#define ACCEL_RANGE_DOUBLE	BIT(7)
+#define ACCEL_RANGE_MASK	BIT_MASK(6)
 	uint8_t accel_range;
 	uint8_t gyro_pm;
 	uint8_t gyro_odr;
