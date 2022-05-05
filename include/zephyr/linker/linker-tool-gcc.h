1a566648f46ea30fbb6bd31e17e74cff0ccc2c01
Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>
2022-05-05 19:42:24 +0400

    ARC: ARCv3: add HS5x support
    Add HS5x CPU support - ARCv3 32bit ISA.
    
    Signed-off-by: Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>
    Signed-off-by: Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>

--- include/zephyr/linker/linker-tool-gcc.h
+++ include/zephyr/linker/linker-tool-gcc.h
@@ -27,8 +27,10 @@
 #elif defined(CONFIG_ARM64)
 	OUTPUT_FORMAT("elf64-littleaarch64")
 #elif defined(CONFIG_ARC)
-	#if defined(CONFIG_64BIT)
+	#if defined(CONFIG_ISA_ARCV3) && defined(CONFIG_64BIT)
 		OUTPUT_FORMAT("elf64-littlearc")
+	#elif defined(CONFIG_ISA_ARCV3) && !defined(CONFIG_64BIT)
+		OUTPUT_FORMAT("elf32-littlearc64")
 	#else
 		OUTPUT_FORMAT("elf32-littlearc", "elf32-bigarc", "elf32-littlearc")
 	#endif
