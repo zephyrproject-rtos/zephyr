b25da6ed84d123a9bad3d4298f8b65207af94d11
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-05-06 11:25:47 +0200

    soc: arc: snps_nsim: delete C file with no code
    soc_config.c for snps_nsim had no actual code, just a bunch of includes.
    Deleting as it has no value (and triggered a compliance warning because
    file had no newline at EOF).
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- soc/arc/snps_nsim/soc_config.c
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2018 Synopsys, Inc. All rights reserved.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include <zephyr/device.h>
-#include <zephyr/init.h>
-#include "soc.h"
