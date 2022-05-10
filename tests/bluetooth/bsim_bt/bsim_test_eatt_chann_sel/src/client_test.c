ea1e1918be6cfc1002baf2dcab91c91e21c90017
Alexandru <68592963+CarbuneanuAlexandru@users.noreply.github.com>
2022-05-10 09:39:53 +0200

    Update tests/bluetooth/bsim_bt/bsim_test_eatt_chann_sel/src/client_test.c
    Co-authored-by: Emil Gydesen <Thalley@users.noreply.github.com>

--- tests/bluetooth/bsim_bt/bsim_test_eatt_chann_sel/src/client_test.c
+++ tests/bluetooth/bsim_bt/bsim_test_eatt_chann_sel/src/client_test.c
@@ -16,7 +16,7 @@ CREATE_FLAG(flag_subscribed);
 
 static struct bt_conn *g_conn;
 static uint16_t chrc_handle;
-static struct bt_uuid *test_svc_uuid = TEST_SERVICE_UUID;
+static const struct bt_uuid *test_svc_uuid = TEST_SERVICE_UUID;
 
 static void connected(struct bt_conn *conn, uint8_t err)
 {
