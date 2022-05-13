affc5ec6e84b3d9fb618c089873619f029cc9c08
Henrik Brix Andersen <hebad@vestas.com>
2022-05-13 21:32:03 +0200

    soc: arm: gigadevice: undefine CAN_MODE_NORMAL
    The GigaDevice HAL defines CAN_MODE_NORMAL, which conflicts with the
    zephyr/drivers/can.h header definition. Undefine it in soc.h.
    
    Signed-off-by: Henrik Brix Andersen <hebad@vestas.com>

--- soc/arm/gigadevice/gd32f4xx/soc.h
+++ soc/arm/gigadevice/gd32f4xx/soc.h
@@ -10,6 +10,9 @@
 
 #include <gd32f4xx.h>
 
+/* The GigaDevice HAL headers define this, but it conflicts with the Zephyr can.h */
+#undef CAN_MODE_NORMAL
+
 #endif /* _ASMLANGUAGE */
 
 #endif /* _SOC_ARM_GIGADEVICE_GD32F4XX_SOC_H_ */
