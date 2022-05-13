c5e09b69c402d2b591183291b6843442c3f41daf
Marcin Niestroj <m.niestroj@emb.dev>
2022-05-13 19:22:13 +0200

    modem: remove 'modem_pin' abstraction
    All drivers using 'modem_pin' abstraction were converted already, so remove
    its implementation now.
    
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

--- drivers/modem/modem_context.c
+++ drivers/modem/modem_context.c
@@ -128,22 +128,9 @@ struct modem_context *modem_context_from_id(int id)
 
 int modem_context_register(struct modem_context *ctx)
 {
-	int ret;
-
 	if (!ctx) {
 		return -EINVAL;
 	}
 
-	ret = modem_context_get(ctx);
-	if (ret < 0) {
-		return ret;
-	}
-
-	ret = modem_pin_init(ctx);
-	if (ret < 0) {
-		LOG_ERR("modem pin init error: %d", ret);
-		return ret;
-	}
-
-	return 0;
+	return modem_context_get(ctx);
 }
