f0403fde71c52a1fc0ddb1145debf407ccfc80cc
Aaron Massey <aaronmassey@google.com>
2022-05-18 12:46:13 -0600

    tests: Enable sbs_gauge driver native_posix
    The SBS Gauge drivers tests only executed directly on hardware before. In
    addition, the tests were flawed as they were only testing that behavior was
    failing.
    
    Enable the SBS Gauge emulator and fix driver tests so they execute
    correctly.
    
    TEST=twister -T tests/drivers/sensor/sbs_gauge
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>

--- tests/drivers/sensor/sbs_gauge/src/test_sbs_gauge.c
+++ tests/drivers/sensor/sbs_gauge/src/test_sbs_gauge.c
@@ -19,11 +19,12 @@ const struct device *get_fuel_gauge_device(void)
 
 void test_get_sensor_value(int16_t channel)
 {
+	/* Helper that only checks channel access success. */
 	struct sensor_value value;
 	const struct device *dev = get_fuel_gauge_device();
 
-	zassert_true(sensor_sample_fetch_chan(dev, channel) < 0, "Sample fetch failed");
-	zassert_true(sensor_channel_get(dev, channel, &value) < 0, "Get sensor value failed");
+	zassert_ok(sensor_sample_fetch_chan(dev, channel), "Sample fetch failed");
+	zassert_ok(sensor_channel_get(dev, channel, &value), "Get sensor value failed");
 }
 
 void test_get_sensor_value_not_supp(int16_t channel)
@@ -41,16 +42,6 @@ void test_get_gauge_voltage(void)
 void test_get_gauge_current(void)
 {
 	test_get_sensor_value(SENSOR_CHAN_GAUGE_AVG_CURRENT);
-}
-
-void test_get_stdby_current(void)
-{
-	test_get_sensor_value(SENSOR_CHAN_GAUGE_STDBY_CURRENT);
-}
-
-void test_get_max_load_current(void)
-{
-	test_get_sensor_value(SENSOR_CHAN_GAUGE_MAX_LOAD_CURRENT);
 }
 
 void test_get_temperature(void)
@@ -83,11 +74,6 @@ void test_get_full_avail_capacity(void)
 	test_get_sensor_value(SENSOR_CHAN_GAUGE_FULL_AVAIL_CAPACITY);
 }
 
-void test_get_average_power(void)
-{
-	test_get_sensor_value(SENSOR_CHAN_GAUGE_AVG_POWER);
-}
-
 void test_get_average_time_to_empty(void)
 {
 	test_get_sensor_value(SENSOR_CHAN_GAUGE_TIME_TO_EMPTY);
@@ -103,21 +89,6 @@ void test_get_cycle_count(void)
 	test_get_sensor_value(SENSOR_CHAN_GAUGE_CYCLE_COUNT);
 }
 
-void test_get_design_voltage(void)
-{
-	test_get_sensor_value(SENSOR_CHAN_GAUGE_DESIGN_VOLTAGE);
-}
-
-void test_get_desired_voltage(void)
-{
-	test_get_sensor_value(SENSOR_CHAN_GAUGE_DESIRED_VOLTAGE);
-}
-
-void test_get_desired_chg_current(void)
-{
-	test_get_sensor_value(SENSOR_CHAN_GAUGE_DESIRED_CHARGING_CURRENT);
-}
-
 void test_not_supported_channel(void)
 {
 	uint8_t channel;
@@ -127,4 +98,10 @@ void test_not_supported_channel(void)
 	}
 	/* SOH is not defined in the SBS 1.1 specifications */
 	test_get_sensor_value_not_supp(SENSOR_CHAN_GAUGE_STATE_OF_HEALTH);
+
+	/* These readings are not presently supported by the sbs_gauge driver. */
+	test_get_sensor_value_not_supp(SENSOR_CHAN_GAUGE_STDBY_CURRENT);
+	test_get_sensor_value_not_supp(SENSOR_CHAN_GAUGE_MAX_LOAD_CURRENT);
+	test_get_sensor_value_not_supp(SENSOR_CHAN_GAUGE_DESIRED_VOLTAGE);
+	test_get_sensor_value_not_supp(SENSOR_CHAN_GAUGE_DESIRED_CHARGING_CURRENT);
 }
