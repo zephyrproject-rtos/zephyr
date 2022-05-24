3d035a71fe56cf2d09e02e95ff9b9b8e18350985
Georgij Cernysiov <geo.cgv@gmail.com>
2022-05-24 20:17:03 +0200

    includes: shell: uart: fix legacy includes
    Adds `zephyr/...` prefix to `smp_shell.h` include.
    
    Signed-off-by: Georgij Cernysiov <geo.cgv@gmail.com>

--- include/zephyr/shell/shell_uart.h
+++ include/zephyr/shell/shell_uart.h
@@ -10,7 +10,7 @@
 #include <zephyr/shell/shell.h>
 #include <zephyr/sys/ring_buffer.h>
 #include <zephyr/sys/atomic.h>
-#include "mgmt/mcumgr/smp_shell.h"
+#include <zephyr/mgmt/mcumgr/smp_shell.h>
 
 #ifdef __cplusplus
 extern "C" {
