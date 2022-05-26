0fa9ce6755659aba0d28e79de87cfd7a9c39152b
Guo Lixin <lixinx.guo@intel.com>
2022-05-26 10:10:45 +0800

    twister: add console harness support for ztest
    Tests may need to be verified both in ztest and console, that could
    be confusing when selecting the harness.
    As most tests are written in ztest, so add console support for ztest
    and configure to use ztest harness directly.
    Also add some judgements to make sure that ztest and console are
    verified at the same time, and if there are failures, we can distinguish
    them in results.
    
    Signed-off-by: Guo Lixin <lixinx.guo@intel.com>
    Signed-off-by: Michał Barnaś <mb@semihalf.com>

--- scripts/pylib/twister/twisterlib/handlers.py
+++ scripts/pylib/twister/twisterlib/handlers.py
@@ -41,8 +41,8 @@ class HarnessImporter:
     def __init__(self, name):
         sys.path.insert(0, os.path.join(ZEPHYR_BASE, "scripts/pylib/twister/twisterlib"))
         module = __import__("harness")
-        if name:
-            my_class = getattr(module, name)
+        if name == "pytest":
+            my_class = getattr(module, "Pytest")
         else:
             my_class = getattr(module, "Test")
 
@@ -299,7 +299,10 @@ class BinaryHandler(Handler):
         elif harness.state:
             self.instance.status = harness.state
             if harness.state == "failed":
-                self.instance.reason = "Failed"
+                if not harness.console_state:
+                    self.instance.reason = "Console Failed"
+                else:
+                    self.instance.reason = "Failed"
         else:
             self.instance.status = "failed"
             self.instance.reason = "Timeout"
@@ -607,7 +610,10 @@ class DeviceHandler(Handler):
         if harness.state:
             self.instance.status = harness.state
             if harness.state == "failed":
-                self.instance.reason = "Failed"
+                if not harness.console_state:
+                    self.instance.reason = "Console Failed"
+                else:
+                    self.instance.reason = "Failed"
         elif not flash_error:
             self.instance.status = "error"
             self.instance.reason = "No Console Output(Timeout)"
@@ -784,7 +790,10 @@ class QEMUHandler(Handler):
             handler.instance.reason = "Timeout"
         elif out_state == "failed":
             handler.instance.status = "failed"
-            handler.instance.reason = "Failed"
+            if not harness.console_state:
+                handler.instance.reason = "Console Failed"
+            else:
+                handler.instance.reason = "Failed"
         elif out_state in ['unexpected eof', 'unexpected byte']:
             handler.instance.status = "failed"
             handler.instance.reason = out_state
