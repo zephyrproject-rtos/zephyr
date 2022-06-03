c3ca7ef6b7d4c4530b89fa382977d3ce6437fb3b
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2022-06-03 14:32:30 +0300

    lib: rb: Remove unneeded statement
    Remove unneeded statement since it is done in the beginning of the
    loop.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- lib/os/rb.c
+++ lib/os/rb.c
@@ -199,7 +199,6 @@ static void fix_extra_red(struct rbnode **stack, int stacksz)
 
 		if (parent_side != side) {
 			rotate(stack, stacksz);
-			node = stack[stacksz - 1];
 		}
 
 		/* Rotate the grandparent with parent, swapping colors */
