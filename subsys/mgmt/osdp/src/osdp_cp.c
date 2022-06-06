219ace696ac516eeb186a0ea7c1d112a85c68250
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2022-06-06 23:28:05 +0200

    mgmt/osdp: cp: Move set PD online status to a method
    When a PD is set online, we need to do other actions along with it. To
    make this easier in future, move cp_set_state(pd, OSDP_CP_STATE_ONLINE)
    to it's own method cp_set_online(pd).
    
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

--- subsys/mgmt/osdp/src/osdp_cp.c
+++ subsys/mgmt/osdp/src/osdp_cp.c
@@ -659,6 +659,18 @@ static void cp_flush_command_queue(struct osdp_pd *pd)
 	}
 }
 
+static inline void cp_set_state(struct osdp_pd *pd, enum osdp_cp_state_e state)
+{
+	pd->state = state;
+	CLEAR_FLAG(pd, PD_FLAG_AWAIT_RESP);
+}
+
+static inline void cp_set_online(struct osdp_pd *pd)
+{
+	cp_set_state(pd, OSDP_CP_STATE_ONLINE);
+	pd->tstamp = 0;
+}
+
 static inline void cp_set_offline(struct osdp_pd *pd)
 {
 	sc_deactivate(pd);
@@ -670,12 +682,6 @@ static inline void cp_reset_state(struct osdp_pd *pd)
 {
 	pd->state = OSDP_CP_STATE_INIT;
 	osdp_phy_state_reset(pd);
-}
-
-static inline void cp_set_state(struct osdp_pd *pd, enum osdp_cp_state_e state)
-{
-	pd->state = state;
-	CLEAR_FLAG(pd, PD_FLAG_AWAIT_RESP);
 }
 
 #ifdef CONFIG_OSDP_SC_ENABLED
@@ -854,7 +860,7 @@ static int state_update(struct osdp_pd *pd)
 			break;
 		}
 #endif /* CONFIG_OSDP_SC_ENABLED */
-		cp_set_state(pd, OSDP_CP_STATE_ONLINE);
+		cp_set_online(pd);
 		break;
 #ifdef CONFIG_OSDP_SC_ENABLED
 	case OSDP_CP_STATE_SC_INIT:
@@ -869,7 +875,7 @@ static int state_update(struct osdp_pd *pd)
 			if (ISSET_FLAG(pd, PD_FLAG_SC_SCBKD_DONE)) {
 				LOG_INF("SC Failed. Online without SC");
 				pd->sc_tstamp = osdp_millis_now();
-				cp_set_state(pd, OSDP_CP_STATE_ONLINE);
+				cp_set_online(pd);
 				break;
 			}
 			SET_FLAG(pd, PD_FLAG_SC_USE_SCBKD);
@@ -882,7 +888,7 @@ static int state_update(struct osdp_pd *pd)
 		if (pd->reply_id != REPLY_CCRYPT) {
 			LOG_ERR("CHLNG failed. Online without SC");
 			pd->sc_tstamp = osdp_millis_now();
-			cp_set_state(pd, OSDP_CP_STATE_ONLINE);
+			cp_set_online(pd);
 			break;
 		}
 		cp_set_state(pd, OSDP_CP_STATE_SC_SCRYPT);
@@ -894,7 +900,7 @@ static int state_update(struct osdp_pd *pd)
 		if (pd->reply_id != REPLY_RMAC_I) {
 			LOG_ERR("SCRYPT failed. Online without SC");
 			pd->sc_tstamp = osdp_millis_now();
-			cp_set_state(pd, OSDP_CP_STATE_ONLINE);
+			cp_set_online(pd);
 			break;
 		}
 		if (ISSET_FLAG(pd, PD_FLAG_SC_USE_SCBKD)) {
@@ -904,7 +910,7 @@ static int state_update(struct osdp_pd *pd)
 		}
 		LOG_INF("SC Active");
 		pd->sc_tstamp = osdp_millis_now();
-		cp_set_state(pd, OSDP_CP_STATE_ONLINE);
+		cp_set_online(pd);
 		break;
 	case OSDP_CP_STATE_SET_SCBK:
 		if (cp_cmd_dispatcher(pd, CMD_KEYSET) != 0) {
@@ -912,7 +918,7 @@ static int state_update(struct osdp_pd *pd)
 		}
 		if (pd->reply_id == REPLY_NAK) {
 			LOG_WRN("Failed to set SCBK; continue with SCBK-D");
-			cp_set_state(pd, OSDP_CP_STATE_ONLINE);
+			cp_set_online(pd);
 			break;
 		}
 		LOG_INF("SCBK set; restarting SC to verify new SCBK");
