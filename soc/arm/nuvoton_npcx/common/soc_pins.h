3468a02f72541fc1e19414e51ea27471d2b01bb4
Mulin Chao <mlchao@nuvoton.com>
2022-06-06 23:16:17 -0700

    npcx: scfg: pinmux: cleanup unused functions and macro
    Clean up unused pinmux function 'npcx_pinctrl_mux_configure()' and
    related 'NPCX_DT_IO_ALT*' macro functions.
    
    Please refer https://issuetracker.google.com/234861079 for more detail.
    
    Signed-off-by: Mulin Chao <mlchao@nuvoton.com>

--- soc/arm/nuvoton_npcx/common/soc_pins.h
+++ soc/arm/nuvoton_npcx/common/soc_pins.h
@@ -63,42 +63,6 @@ struct npcx_psl_in {
 };
 
 /**
- * @brief Select device pin-mux to I/O or its alternative functionality
- *
- * Example devicetree fragment:
- *    / {
- *		uart1: serial@400c4000 {
- *			//altfunc 0: PIN64.65, otherwise CR_SIN1 CR_SOUT1
- *			pinctrl = <&altc_uart1_sl2>;
- *			...
- *		};
- *
- *		kscan0: kscan@400a3000 {
- *			//altfunc 0: PIN31.xx PIN21.xx, otherwise KSO0-x KSI0-x
- *			pinctrl = <&alt7_no_ksi0_sl ...
- *			           &alt8_no_kso00_sl ...>;
- *			...
- *		};
- *	};
- *
- * Example usage:
- *    - Pinmux configuration list
- *         const struct npcx_alt alts_list[] = NPCX_DT_ALT_ITEMS_LIST(inst);
- *    - Change pinmux to UART:
- *         npcx_pinctrl_mux_configure(alts_list, ARRAY_SIZE(alts_list), 1);
- *    - Change pinmux back to GPIO64.65:
- *         npcx_pinctrl_mux_configure(alts_list, ARRAY_SIZE(alts_list), 0);
- *
- * Please refer more details in Table 3. (Pin Multiplexing Configuration).
- *
- * @param alts_list Pointer to pin-mux configuration list for specific device
- * @param alts_size Pin-mux configuration list size
- * @param altfunc 0: set pin-mux to GPIO, otherwise specific functionality
- */
-void npcx_pinctrl_mux_configure(const struct npcx_alt *alts_list,
-			uint8_t alts_size, int altfunc);
-
-/**
  * @brief Select i2c port pads of i2c controller
  *
  * @param controller i2c controller device
