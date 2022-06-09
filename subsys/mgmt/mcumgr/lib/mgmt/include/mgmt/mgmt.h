762d53f5c5831be1d1cd997900b7398d7229063e
Dominik Ermel <dominik.ermel@nordicsemi.no>
2022-06-09 12:03:28 +0000

    mgmt/mcumgr/lib: Add BUSY error code
    The commit reserves adds MGMT_ERR_EBUSY (10) error code and adds
    documentaiton for it.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- subsys/mgmt/mcumgr/lib/mgmt/include/mgmt/mgmt.h
+++ subsys/mgmt/mcumgr/lib/mgmt/include/mgmt/mgmt.h
@@ -52,6 +52,7 @@ extern "C" {
 #define MGMT_ERR_EMSGSIZE	7	/* Response too large. */
 #define MGMT_ERR_ENOTSUP	8	/* Command not supported. */
 #define MGMT_ERR_ECORRUPT	9	/* Corrupt */
+#define MGMT_ERR_EBUSY		10	/* Command blocked by processing of other command */
 #define MGMT_ERR_EPERUSER	256
 
 #define MGMT_HDR_SIZE		8
