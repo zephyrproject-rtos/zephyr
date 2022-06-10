6073101b576ddb7f963575f4b8d97fdc9fb0abe4
Sateesh Kotapati <Sateesh.Kotapati@silabs.com>
2022-06-10 14:04:29 +0530

    boards: Add support for Silicon Labs' EFR32BG22-SLTB010A board
    Incorporated review comments.
    
    Signed-off-by: Sateesh Kotapati <Sateesh.Kotapati@silabs.com>

--- boards/arm/efr32bg_sltb010a/board.c
+++ boards/arm/efr32bg_sltb010a/board.c
@@ -4,31 +4,25 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <init.h>
-#include <drivers/gpio.h>
-#include <sys/printk.h>
-#include <logging/log.h>
+#include <zephyr/init.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(efr32bg_sltb010a, LOG_LEVEL_DBG);
-
-/* This pin is used to enable the serial port using the board controller */
-#define VCOM_ENABLE_GPIO_NAME  "GPIO_A"
-#define VCOM_ENABLE_GPIO_PIN   5
+LOG_MODULE_REGISTER(efr32bg_sltb010a, CONFIG_BOARD_EFR32BG22_LOG_LEVEL);
 
 static int efr32bg_sltb010a_init(const struct device *dev)
 {
-	const struct device *vce_dev; /* Virtual COM Port Enable GPIO Device */
+	const struct gpio_dt_spec vcen_dev =
+		GPIO_DT_SPEC_GET(DT_ALIAS(vcomenable), gpios);
 
 	ARG_UNUSED(dev);
 
-	/* Enable the board controller to be able to use the serial port */
-	vce_dev = device_get_binding(VCOM_ENABLE_GPIO_NAME);
-	if (!vce_dev) {
+	if (!device_is_ready(vcen_dev.port)) {
 		LOG_ERR("Virtual COM Port Enable device was not found!\n");
 		return -ENODEV;
 	}
-
-	gpio_pin_configure(vce_dev, VCOM_ENABLE_GPIO_PIN, GPIO_OUTPUT_HIGH);
+	gpio_pin_configure_dt(&vcen_dev, GPIO_OUTPUT_HIGH);
 
 	return 0;
 }
