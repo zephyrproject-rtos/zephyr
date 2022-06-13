97aa927741502040ded0fd59c61ce33bd2d4f2b5
Wolter HV <wolterhv@gmx.de>
2022-06-13 09:25:03 +0100

    Progress update for draft PR.
    Restores `esp32.dtsi` to version in main.  Changes to this file were a
    draft but it's still quite messy and unguided.

--- drivers/adc/adc_esp32.c
+++ drivers/adc/adc_esp32.c
@@ -5,6 +5,21 @@
  */
 
 /*
+ * Zephyr - Espressif mapping
+ * adc_channel_setup - adc1_config_width, adc1_config_channel_atten
+ * adc_read - adc1_get_raw, adc2_get_raw
+ * adc_read_async - (not implemented)
+ * adc_atten_t - adc_gain
+ *
+ * Step 1: implement with non-hal calls.
+ * Step 2: implement with hal calls.
+ *
+ * The channel attenuation is not completely compatible with the concept of ADC
+ * gain, apparently. It seems to be something that gets passed on to the
+ * hardware.
+ */
+
+/*
  * adc_stm32.c has been used as a reference for this implementation.
  */
 
@@ -15,8 +30,7 @@
 #include <drivers/adc.h>
 
 /* From espressif */
-#include "hal/adc_hal.h"
-#include "hal/adc_hal_conf.h"
+#include <hal/adc_hal.h>
 
 #define LOG_LEVEL CONFIG_ADC_LOG_LEVEL
 #include <logging/log.h>
@@ -43,7 +57,47 @@ static const struct adc_driver_api api_esp32_driver_api = {
 #endif /* CONFIG_ADC_ASYNC */
 };
 
+enum adc_esp32_devid_e {
+	ADC1 = '1',
+	ADC2 = '2',
+	ADC_ID_INVALID
+};
+
+/* To house ESP32-specific stuff */
+struct adc_esp32_devconf {
+	adc_atten_t       atten; /* adc-specific (adc1_config_width) */
+	adc_bits_width_t  width; /* channel-specific (adc1_config_channel_atten) */
+	uint8_t channel_count; /* maps to adc1_channel_t or adc2_channel_t, must
+				  be set to the relevant ADCn_CHANNEL_MAX from
+				  the devicetree configuration. */
+	/* From single_read/adc/adc1_example_main.c */
+        /*
+	 * adc_bits_width_t width = ADC_WIDTH_BIT_12;
+	 * adc_channel_t channel = ADC_CHANNEL_6;
+	 * adc_atten_t atten = ADC_ATTEN_DB_11;
+         */
+};
+
 /* Implementation */
+
+static enum adc_esp32_devid helper_get_devid(const struct device  *dev)
+{
+	/* TODO: find a better way of identifying the device that doesn't rely
+	 * on strings. */
+	/* dev->name is either "ADC1" or "ADC2".  To check which ADC the device
+	 * corresponds to, it is only necessary to check character 3. */
+	switch (dev->name[3]) {
+	case '1':
+		return ADC1;
+		break;
+	case '2':
+		return ADC2;
+		break;
+	default:
+		return ADC_ID_INVALID;
+		break;
+	}
+}
 
 static int adc_esp32_init(const struct device *dev)
 {
@@ -54,9 +108,39 @@ static int adc_esp32_init(const struct device *dev)
 static int adc_esp32_channel_setup (const struct device *dev,
 				    const struct adc_channel_cfg *channel_cfg)
 {
-#error "Not implemented"
-	adc1_config_width();
-	adc1_config_channel_atten();
+	const struct adc_esp32_devconf  *devconf = dev->config;
+	enum adc_esp32_devid             id      = helper_get_devid(dev);
+
+	if (channel_cfg->channel_id >= devconf->channel_count) {
+		LOG_ERR("Channel %d is not valid", channel_cfg->channel_id);
+		return -EINVAL;
+	}
+
+	switch (id) {
+	case ADC1:
+		adc1_config_width(devconf->width);
+		adc1_config_channel_atten((adc1_channel_t) channel_cfg->channel_id,
+					  devconf->atten);
+		break;
+	case ADC2:
+		adc2_config_channel_atten((adc2_channel_t) channel_cfg->channel_id,
+					  devconf->atten);
+		break;
+	default:
+		/* The device name is invalid for some reason. */
+		return -EINVAL;
+		break;
+	}
+	/* At some point, a "characterisation" of the ADC may be needed. In the
+	 * example it is called with
+	 * esp_adc_cal_characterize. */
+	/* After this, all that is needed is calling adc1_get_raw or
+	 * adc2_get_raw. */
+	/* To convert a raw value to a physical quantity, use
+	 * esp_adc_cal_raw_to_voltage. */
+	/* In the adc_hal.h, the functions
+	 * adc_ll_set_atten and adc_ll_rtc_enable_channel are used, which
+	 * suggests that these may the HAL ways of setting up a channel. */
 	return 0;
 }
 
@@ -69,14 +153,48 @@ static int adc_esp32_channel_setup (const struct device *dev,
 static int adc_esp32_read          (const struct device *dev,
 			            const struct adc_sequence *sequence)
 {
-	int error;
+	enum adc_esp32_devid id = helper_get_devid(dev);
 
-	/* adc1_get_raw(); */
-	/* adc2_get_raw(); */
+	if (id == ADC_ID_INVALID) {
+		return -EINVAL;
+	}
+
+	/* the adc_sequence struct member "channels" is a 32-bit bitfield with
+	 * the channels to get a reading for. Possibly in this function we will
+	 * have a loop instead, that will get a reading for each channel. */
+	/* The STM32 returns an error when multiple channels are selected, and
+	 * that's how it gets away with doing just single channel reads. */
+	/* TODO: Find out whether the ESP32 supports reading multiple channels
+	 * at a time.  In the meantime, do what STM32 does. */
+	uint8_t index = find_lsb_set(sequence->channels) - 1;
+	if (sequence->channels > BIT(index)) {
+		LOG_ERR("Only single channel supported");
+		return -ENOTSUP;
+	}
+
+	if (sequence->calibrate) {
+		/* TODO: Find out how to calibrate. */
+		adc_esp32_calibrate();
+	}
+
+	/* TODO: Find out if there are equivalent HAL functions for the
+	 * readings.  For now, use the ESP-IDF functions. */
+	int reading;
+	switch (id) {
+	case ADC1:
+		reading = adc1_get_raw((adc1_channel_t) channel_cfg->channel_id);
+		break;
+	case ADC2:
+		reading = adc2_get_raw((adc2_channel_t) channel_cfg->channel_id);
+		break;
+	default:
+		/* Error already handled by helper_get_devid */
+		break;
+	}
 	/* Set resolution, according to channel */
 	sequence->buffer[chan_idx] = raw_value;
 
-	return error;
+	return 0;
 }
 
 #ifdef CONFIG_ADC_ASYNC
@@ -84,8 +202,13 @@ static int adc_esp32_read_async    (const struct device *dev,
 			            const struct adc_sequence *sequence,
 			            struct k_poll_signal *async)
 {
-#error "Not implemented"
-	return 0;
+	(void)(dev);
+	(void)(sequence);
+	(void)(async);
+
+	/* TODO: Find out whether this is supported. */
+
+	return -ENOTSUP;
 }
 #endif /* CONFIG_ADC_ASYNC */
 
