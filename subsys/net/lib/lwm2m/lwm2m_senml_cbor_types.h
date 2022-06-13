a97adbd9798ac01175edb04086158ae49d93f58b
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2022-06-13 12:19:35 +0200

    west.yml: Update zcbor to v. 0.5.1
    lwm2m: Regenerate lwm2m_senml code from the CDDL description.
    Reapply manual changes.
    Create a sh script to perform the regeneration.
    
    tests: zcbor_bulk: Adapt to zcbor 0.5.1
    zcbor_new_decode_state() now has no return value.
    
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

--- subsys/net/lib/lwm2m/lwm2m_senml_cbor_types.h
+++ subsys/net/lib/lwm2m/lwm2m_senml_cbor_types.h
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 /*
- * Generated using zcbor version 0.4.0
+ * Generated using zcbor version 0.5.1
  * https://github.com/zephyrproject-rtos/zcbor
  * Generated with a --default-max-qty of 99
  */
@@ -12,11 +12,12 @@
 #ifndef LWM2M_SENML_CBOR_TYPES_H__
 #define LWM2M_SENML_CBOR_TYPES_H__
 
-#include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <string.h>
 #include "zcbor_common.h"
+
 
 enum lwm2m_senml_cbor_key {
 	lwm2m_senml_cbor_key_bn = -2,
