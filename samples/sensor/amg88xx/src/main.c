cbdc74b965b9377171203f65e6af0024afa049aa
Kumar Gala <galak@kernel.org>
2022-06-13 17:43:43 -0500

    samples: amg88xx: Convert sample to use DEVICE_DT_GET_ONE
    Move to use DEVICE_DT_GET_ONE instead of device_get_binding as we
    work on phasing out use of DTS 'label' property.
    
    Signed-off-by: Kumar Gala <galak@kernel.org>

--- samples/sensor/amg88xx/src/main.c
+++ samples/sensor/amg88xx/src/main.c
@@ -51,11 +51,10 @@ void print_buffer(void *ptr, size_t l)
 void main(void)
 {
 	int ret;
-	const struct device *dev = device_get_binding(
-				DT_LABEL(DT_INST(0, panasonic_amg88xx)));
+	const struct device *dev = DEVICE_DT_GET_ONE(panasonic_amg88xx);
 
-	if (dev == NULL) {
-		printk("Could not get AMG88XX device\n");
+	if (!device_is_ready(dev)) {
+		printk("sensor: device not ready.\n");
 		return;
 	}
 
