be8c7e5a32c5f86b7d922dcf4e91158c63e31f38
Manuel Arguelles <manuel.arguelles@nxp.com>
2022-06-14 21:16:22 +0200

    tests: error_hook: skip div-by-zero on FVP BaseR
    Division-by-zero does not generate an exception on ARM FVP BaseR.
    
    Signed-off-by: Manuel Arguelles <manuel.arguelles@nxp.com>

--- tests/ztest/error_hook/src/main.c
+++ tests/ztest/error_hook/src/main.c
@@ -121,7 +121,8 @@ __no_optimization static void trigger_fault_divide_zero(void)
 	defined(CONFIG_BOARD_QEMU_CORTEX_A53) || defined(CONFIG_SOC_QEMU_ARC) || \
 	defined(CONFIG_ARMV6_M_ARMV8_M_BASELINE) || \
 	defined(CONFIG_BOARD_QEMU_CORTEX_R5) || \
-	defined(CONFIG_BOARD_FVP_BASER_AEMV8R) || defined(CONFIG_BOARD_FVP_BASE_REVC_2XAEMV8A)
+	defined(CONFIG_BOARD_FVP_BASER_AEMV8R) || defined(CONFIG_BOARD_FVP_BASE_REVC_2XAEMV8A) || \
+	defined(CONFIG_BOARD_FVP_BASER_AEMV8R_AARCH32)
 	ztest_test_skip();
 #endif
 }
