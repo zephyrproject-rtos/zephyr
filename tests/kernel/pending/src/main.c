74ed64139c2c4840bc1ae7eb06dda7f6d23ee6bc
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-06-14 10:50:56 +0200

    tests: remove redundant <zephyr/zephyr.h> includes
    Files including <zephyr/kernel.h> do not have to include
    <zephyr/zephyr.h>, a shim to <zephyr/kernel.h>.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- tests/kernel/pending/src/main.c
+++ tests/kernel/pending/src/main.c
@@ -5,7 +5,6 @@
  */
 
 #include <tc_util.h>
-#include <zephyr/zephyr.h>
 #include <ztest.h>
 #include <zephyr/kernel.h>
 #include <zephyr/kernel_structs.h>
--- tests/subsys/pm/power_mgmt/src/main.c
+++ tests/subsys/pm/power_mgmt/src/main.c
@@ -5,7 +5,6 @@
  */
 
 #include <zephyr/sys/printk.h>
-#include <zephyr/zephyr.h>
 #include <zephyr/types.h>
 #include <zephyr/pm/device.h>
 #include <zephyr/pm/device_runtime.h>
