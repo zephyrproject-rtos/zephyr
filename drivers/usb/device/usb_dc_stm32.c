64e42a8a40bdb8f347b7e69465676c5c9e2d892d
Aurelien Jarno <aurelien@aurel32.net>
2022-06-15 18:20:01 +0200

    drivers: usb: device: stm32: only disable the ULPI clock for USB FS
    On SoCs with two USB controller, Zephyr selects the FS one, and in that
    case the ULPI clock needs to be disabled in sleep mode for the
    controller to work.
    
    On SoCs with a single USB HS controller, this operation is not needed.
    
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- drivers/usb/device/usb_dc_stm32.c
+++ drivers/usb/device/usb_dc_stm32.c
@@ -399,6 +399,7 @@ static int usb_dc_stm32_init(void)
 #endif /* CONFIG_USB_DEVICE_SOF */
 
 #if defined(CONFIG_SOC_SERIES_STM32H7X)
+#if DT_HAS_COMPAT_STATUS_OKAY(st_stm32_otgfs)
 	/* The USB2 controller only works in FS mode, but the ULPI clock needs
 	 * to be disabled in sleep mode for it to work. For the USB1
 	 * controller, as it is an HS one, the clock is disabled in the common
@@ -406,6 +407,7 @@ static int usb_dc_stm32_init(void)
 	 */
 
 	LL_AHB1_GRP1_DisableClockSleep(LL_AHB1_GRP1_PERIPH_USB2OTGHSULPI);
+#endif
 
 	LL_PWR_EnableUSBVoltageDetector();
 
