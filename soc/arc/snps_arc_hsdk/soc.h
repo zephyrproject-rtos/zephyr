82ce3e566d2bfa02e95009082b5d9c469198ff4b
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-06-15 12:56:31 +0200

    soc: arc: snps_arch_hdsk: remove unused pinmux definitions
    Pinmux definitions were not used, so remove them.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- soc/arc/snps_arc_hsdk/soc.h
+++ soc/arc/snps_arc_hsdk/soc.h
@@ -28,31 +28,6 @@
 #include <zephyr/sys/util.h>
 #include <zephyr/random/rand32.h>
 
-/* PINMUX IO Hardware Functions */
-#define HSDK_PINMUX_FUNS		8
-
-#define HSDK_PINMUX_FUN0		0
-#define HSDK_PINMUX_FUN1		1
-#define HSDK_PINMUX_FUN2		2
-#define HSDK_PINMUX_FUN3		3
-#define HSDK_PINMUX_FUN4		4
-#define HSDK_PINMUX_FUN5		5
-#define HSDK_PINMUX_FUN6		6
-#define HSDK_PINMUX_FUN7		7
-
-/* PINMUX MAX SELS */
-#define HSDK_PINMUX_SELS		8
-
-#define HSDK_PINMUX_SEL0		0
-#define HSDK_PINMUX_SEL1		1
-#define HSDK_PINMUX_SEL2		2
-#define HSDK_PINMUX_SEL3		3
-#define HSDK_PINMUX_SEL4		4
-#define HSDK_PINMUX_SEL5		5
-#define HSDK_PINMUX_SEL6		6
-#define HSDK_PINMUX_SEL7		7
-
-
 #endif /* !_ASMLANGUAGE */
 
 #endif /* _SOC_H_ */
