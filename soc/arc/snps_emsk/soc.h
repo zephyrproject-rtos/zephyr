61350ca3e40625d8c30add12ed77f8865a9ac12d
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-06-15 16:20:34 +0200

    soc: arc: snps_emsk/nsim: remove unused definitions
    Remove definitions not used in any C unit throughout the tree.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- soc/arc/snps_emsk/soc.h
+++ soc/arc/snps_emsk/soc.h
@@ -16,22 +16,10 @@
 
 #include <zephyr/sys/util.h>
 
-#if defined(CONFIG_BOARD_EM_STARTERKIT_R23) && defined(CONFIG_SOC_EMSK_EM7D)
-#define IRQ_CORE_DMA_COMPLETE			22
-#define IRQ_CORE_DMA_ERROR			23
-#else /* CONFIG_BOARD_EM_STARTERKIT_R23 && CONFIG_SOC_EMSK_EM7D */
-#define IRQ_CORE_DMA_COMPLETE			20
-#define IRQ_CORE_DMA_ERROR			21
-#endif /* !(CONFIG_BOARD_EM_STARTERKIT_R23 && CONFIG_SOC_EMSK_EM7D) */
-
-
 #ifndef _ASMLANGUAGE
 
 #include <zephyr/sys/util.h>
 #include <zephyr/random/rand32.h>
-
-#define INT_ENABLE_ARC				~(0x00000001 << 8)
-#define INT_ENABLE_ARC_BIT_POS			(8)
 
 #endif /* !_ASMLANGUAGE */
 
