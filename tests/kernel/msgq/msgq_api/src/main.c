ff0471a59b1735c1b965eae678ab4fb7c7d1fd42
Stephanos Ioannidis <root@stephanos.io>
2022-06-17 18:34:17 +0900

    tests: kernel: msgq_api: Migrate to K_THREAD_STACK_DECLARE
    This commit updates all deprecated `K_THREAD_STACK_EXTERN` macro usages
    to use the `K_THREAD_STACK_DECLARE` macro instead.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- tests/kernel/msgq/msgq_api/src/main.c
+++ tests/kernel/msgq/msgq_api/src/main.c
@@ -12,6 +12,9 @@
  */
 
 #include <ztest.h>
+
+#include "test_msgq.h"
+
 extern void test_msgq_thread(void);
 extern void test_msgq_thread_overflow(void);
 extern void test_msgq_isr(void);
@@ -57,7 +60,7 @@ extern struct k_msgq kmsgq;
 extern struct k_msgq msgq;
 extern struct k_sem end_sema;
 extern struct k_thread tdata;
-K_THREAD_STACK_EXTERN(tstack);
+K_THREAD_STACK_DECLARE(tstack, STACK_SIZE);
 
 /*test case main entry*/
 void test_main(void)
