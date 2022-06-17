8fd777552e8be1b46886af9d0f15b8d8aeca8448
Anas Nashif <anas.nashif@intel.com>
2022-06-17 07:28:11 -0400

    arch: add arch_proc_id() to get processor ID
    Add a new interfaces for getting processor ID. On some of the
    architectures, implement by means of arch_curr_cpu().
    
    On xtensa, we get this via the PRID register.
    
    Doxygen string by Andy Ross.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- include/zephyr/arch/arm64/arch_inlines.h
+++ include/zephyr/arch/arm64/arch_inlines.h
@@ -23,5 +23,14 @@ static ALWAYS_INLINE int arch_exception_depth(void)
 	return (read_tpidrro_el0() & TPIDRROEL0_EXC_DEPTH) / TPIDRROEL0_EXC_UNIT;
 }
 
+static ALWAYS_INLINE uint32_t arch_proc_id(void)
+{
+	/*
+	 * Placeholder implementation to be replaced with an architecture
+	 * specific call to get processor ID
+	 */
+	return arch_curr_cpu()->id;
+}
+
 #endif /* !_ASMLANGUAGE */
 #endif /* ZEPHYR_INCLUDE_ARCH_ARM64_ARCH_INLINES_H */
