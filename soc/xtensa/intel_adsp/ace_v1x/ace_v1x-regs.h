8792dd800d54316e1ec0770d76fae2f6bdec3d84
Anas Nashif <anas.nashif@intel.com>
2022-06-21 18:36:24 -0400

    soc: ace1.x: use common code
    Do not duplicate code in new SoC and reuse code from intel_adsp/common.
    Move SRAM code into own file in common code and setup SRAM in soc for
    MTL platform.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- soc/xtensa/intel_adsp/ace_v1x/ace_v1x-regs.h
+++ soc/xtensa/intel_adsp/ace_v1x/ace_v1x-regs.h
@@ -1,8 +1,8 @@
 /* Copyright (c) 2022 Intel Corporation
  * SPDX-License-Identifier: Apache-2.0
  */
-#ifndef _ZEPHYR_SOC_INTEL_ADSP_ACE_V1X_REGS_H_
-#define _ZEPHYR_SOC_INTEL_ADSP_ACE_V1X_REGS_H_
+#ifndef ZEPHYR_SOC_INTEL_ADSP_ACE_v1x_REGS_H_
+#define ZEPHYR_SOC_INTEL_ADSP_ACE_v1x_REGS_H_
 
 #include <stdint.h>
 #include <xtensa/config/core-isa.h>
@@ -225,4 +225,4 @@ struct mtl_hpsram_regs {
 
 #define HPSRAM_REGS(x) ((volatile struct mtl_hpsram_regs* const)(0x17A800 + 0x0008 * (x)))
 
-#endif /* _ZEPHYR_SOC_INTEL_ADSP_ACE_V1X_REGS_H_ */
+#endif /* ZEPHYR_SOC_INTEL_ADSP_ACE_v1x_REGS_H_ */
