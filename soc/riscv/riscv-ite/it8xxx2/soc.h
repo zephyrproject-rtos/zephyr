bfa00d62dbc3806de261d5ffd45b268f840997bf
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2022-06-22 17:19:17 +0800

    ITE soc/it8xxx2: disable unused integrated cc module
    ITE EC chip it81202 and it81302 both have embedded integrated
    pd module (support two usbpd ports), this is different from
    standalone TCPC. To prevent cc pins leakage, we disable not
    active ITE USBPD port cc modules, then cc pins can be used
    as gpio if needed.
    
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- soc/riscv/riscv-ite/it8xxx2/soc.h
+++ soc/riscv/riscv-ite/it8xxx2/soc.h
@@ -12,6 +12,23 @@
 #define RISCV_RAM_BASE               CONFIG_SRAM_BASE_ADDRESS
 #define RISCV_RAM_SIZE               KB(CONFIG_SRAM_SIZE)
 
+/*
+ * This define gets the total number of USBPD ports available on the
+ * ITE EC chip from dtsi (include status disable). Both it81202 and
+ * it81302 support two USBPD ports.
+ */
+#define SOC_USBPD_ITE_PHY_PORT_COUNT \
+COND_CODE_1(DT_NODE_EXISTS(DT_INST(1, ite_it8xxx2_usbpd)), (2), (1))
+
+/*
+ * This define gets the number of active USB Power Delivery (USB PD)
+ * ports in use on the ITE microcontroller from dts (only status okay).
+ * The active port usage should follow the order of ITE TCPC port index,
+ * ex. if we're active only one ITE USB PD port, then the port should be
+ * 0x3700 (port0 register base), instead of 0x3800 (port1 register base).
+ */
+#define SOC_USBPD_ITE_ACTIVE_PORT_COUNT DT_NUM_INST_STATUS_OKAY(ite_it8xxx2_usbpd)
+
 #ifndef _ASMLANGUAGE
 void soc_interrupt_init(void);
 #endif
