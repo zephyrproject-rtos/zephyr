05688726d76e9d8656d4dca4480447c424b417c1
Dominik Ermel <dominik.ermel@nordicsemi.no>
2022-06-23 14:10:01 +0000

    [nrf noup] mgmt/mcumgr: Bootutil hooks to handle image-1
    The commit adds bootutil hook, for nrf5340, to allow it
    handling the non-accessible image-1/primary slot.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>
    (cherry picked from commit 482dcc7865f7d3a458f061a1d8276c5801eed1e4)

---
+++ subsys/mgmt/mcumgr/bootutil_hooks/nrf53_hooks.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include "bootutil/bootutil_public.h"
+
+int boot_read_swap_state_primary_slot_hook(int image_index,
+		struct boot_swap_state *state)
+{
+	if (image_index == 1) {
+		/* Pretend that primary slot of image 1 unpopulated */
+		state->magic = BOOT_MAGIC_UNSET;
+		state->swap_type = BOOT_SWAP_TYPE_NONE;
+		state->image_num = image_index;
+		state->copy_done = BOOT_FLAG_UNSET;
+		state->image_ok = BOOT_FLAG_UNSET;
+
+		/* Prevent bootutil from trying to obtain true info */
+		return 0;
+	}
+
+	return BOOT_HOOK_REGULAR;
+}
