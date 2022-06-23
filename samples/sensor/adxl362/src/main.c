a703d1245b128041bf48f3aa26c17e28977ce1fe
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2022-06-23 09:38:51 +0200

    ADXL362: Add separate activity/inactivity triggers
    This patch modifies the ADXL362 driver to use the new
    SENSOR_TRIG_MOTION trigger for activity detection
    and SENSOR_TRIG_STATIONARY for inactivity detection.
    
    Signed-off-by: Maximilian Deubel <maximilian.deubel@nordicsemi.no>

--- samples/sensor/adxl362/src/main.c
+++ samples/sensor/adxl362/src/main.c
@@ -22,8 +22,11 @@ static void trigger_handler(const struct device *dev,
 		}
 		k_sem_give(&sem);
 		break;
-	case SENSOR_TRIG_THRESHOLD:
-		printf("Threshold trigger\n");
+	case SENSOR_TRIG_MOTION:
+		printf("Motion trigger\n");
+		break;
+	case SENSOR_TRIG_STATIONARY:
+		printf("Stationary trigger\n");
 		break;
 	default:
 		printf("Unknown trigger\n");
@@ -43,7 +46,13 @@ void main(void)
 	if (IS_ENABLED(CONFIG_ADXL362_TRIGGER)) {
 		struct sensor_trigger trig = { .chan = SENSOR_CHAN_ACCEL_XYZ };
 
-		trig.type = SENSOR_TRIG_THRESHOLD;
+		trig.type = SENSOR_TRIG_MOTION;
+		if (sensor_trigger_set(dev, &trig, trigger_handler)) {
+			printf("Trigger set error\n");
+			return;
+		}
+
+		trig.type = SENSOR_TRIG_STATIONARY;
 		if (sensor_trigger_set(dev, &trig, trigger_handler)) {
 			printf("Trigger set error\n");
 			return;
