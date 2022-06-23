a6c9e4a55a5735d02767feb49ee0b93255adfbe3
Emil Gydesen <emil.gydesen@nordicsemi.no>
2022-06-23 15:14:00 +0200

    zephyr: Fix n=0 for utf8_lcpy
    The function used an assert if n was 0. Instead
    of using an assert, the function will now just
    not do anything. The documentation has also
    been updated to reflect this.
    
    The reasoning for this is that the strlcpy function
    this (sort of) implements for utf8 works the same way.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- include/zephyr/sys/util.h
+++ include/zephyr/sys/util.h
@@ -415,21 +415,23 @@ uint8_t u8_to_dec(char *buf, uint8_t buflen, uint8_t value);
  *
  * @param utf8_str NULL-terminated string
  *
- *  @return Pointer to the @p utf8_str
+ * @return Pointer to the @p utf8_str
  */
 char *utf8_trunc(char *utf8_str);
 
 /**
  * @brief Copies a UTF-8 encoded string from @p src to @p dst
  *
- * The resulting @p dst will always be NULL terminated, and the @p dst string
- * will always be properly UTF-8 truncated.
+ * The resulting @p dst will always be NULL terminated if @p n is larger than 0,
+ * and the @p dst string will always be properly UTF-8 truncated.
  *
  * @param dst The destination of the UTF-8 string.
  * @param src The source string
- * @param n   The size of the @p dst buffer. Shall not be 0.
+ * @param n   The size of the @p dst buffer. Maximum number of characters copied
+ *            is @p n - 1. If 0 nothing will be done, and the @p dst will not be
+ *            NULL terminated.
  *
- * return Pointer to the @p dst
+ * @return Pointer to the @p dst
  */
 char *utf8_lcpy(char *dst, const char *src, size_t n);
 
