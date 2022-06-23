c0c94f923dda91f7628bf248d6caeb98c11f16cc
Erwan Gouriou <erwan.gouriou@linaro.org>
2022-06-23 14:36:20 +0200

    include: stm32: clock_control: Ease usage of STM32_DT_CLOCKS macro
    STM32_DT_CLOCKS was designed to take a device tree node label name as
    argument: STM32_DT_CLOCKS(uart1)
    Change its implementation to take a node identifier instead:
    STM32_DT_CLOCKS(DT_NODELABEL(uart1)).
    
    This make its usage more flexible since the argument can now be extracted
    from other DT macros such as DT_PARENT. Then, the following can be done:
    STM32_DT_CLOCKS(DT_PARENT(child_node_label)).
    
    Since it is now possible implement STM32_DT_INST_CLOCKS using
    STM32_DT_CLOCKS.
    
    Finally, update existing STM32_DT_CLOCKS users and convert
    STM32_INST_CLOCK_INFO users to STM32_CLOCK_INFO.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- tests/drivers/clock_control/stm32_clock_configuration/stm32u5_devices/src/test_stm32_clock_configuration.c
+++ tests/drivers/clock_control/stm32_clock_configuration/stm32u5_devices/src/test_stm32_clock_configuration.c
@@ -35,7 +35,7 @@ static void test_sysclk_freq(void)
 
 static void test_spi_clk_config(void)
 {
-	static const struct stm32_pclken pclken[] = STM32_DT_CLOCKS(spi1);
+	static const struct stm32_pclken pclken[] = STM32_DT_CLOCKS(DT_NODELABEL(spi1));
 
 	uint32_t spi1_actual_clk_src;
 	uint32_t spi1_dt_clk_freq, spi1_actual_clk_freq;
