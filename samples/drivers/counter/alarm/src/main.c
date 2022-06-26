23e11dd707bb31d4bc1c81aa8d274dc1045ad2b7
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2022-06-27 01:55:07 +0900

    samples: counter: alarm: add support for GD32 boards
    Add support for boards that implements the GD32 SoCs.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- samples/drivers/counter/alarm/src/main.c
+++ samples/drivers/counter/alarm/src/main.c
@@ -35,6 +35,8 @@ struct counter_alarm_cfg alarm_cfg;
 #define TIMER DT_NODELABEL(ctimer0)
 #elif defined(CONFIG_COUNTER_NXP_S32_SYS_TIMER)
 #define TIMER DT_NODELABEL(stm0)
+#elif defined(CONFIG_COUNTER_TIMER_GD32)
+#define TIMER DT_NODELABEL(timer0)
 #endif
 
 static void test_counter_interrupt_fn(const struct device *counter_dev,
