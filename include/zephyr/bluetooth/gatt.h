c431cef06d98d8f4c0da0a6c16eb027f7f1a1d4a
Fredrik Danebjer <fredrik@danebjer.com>
2022-06-27 11:11:44 +0200

    Bluetooth: GATT: Add LE Secure Connections Permission Check
    Added possibility to enforce that a LE Secure Connection is required
    for reading and writing characteristics, i.e. providing a more fine
    grained check that allows services that requires secure connection
    to co-exists with those that doesn't.
    
    Signed-off-by: Lars Knudsen <larsgk@gmail.com>
    Signed-off-by: Fredrik Danebjer <fredrik@danebjer.com>

--- include/zephyr/bluetooth/gatt.h
+++ include/zephyr/bluetooth/gatt.h
@@ -72,6 +72,18 @@ enum {
 	 *  passed to write callback.
 	 */
 	BT_GATT_PERM_PREPARE_WRITE = BIT(6),
+
+	/** @brief Attribute read permission with LE Secure Connection encryption.
+	 *
+	 *  If set, requires that LE Secure Connections is used for read access.
+	 */
+	BT_GATT_PERM_READ_LESC = BIT(7),
+
+	/** @brief Attribute write permission with LE Secure Connection encryption.
+	 *
+	 *  If set, requires that LE Secure Connections is used for write access.
+	 */
+	BT_GATT_PERM_WRITE_LESC = BIT(8),
 };
 
 /** @def BT_GATT_ERR
@@ -151,7 +163,7 @@ struct bt_gatt_attr {
 	/** Attribute handle */
 	uint16_t handle;
 	/** Attribute permissions */
-	uint8_t perm;
+	uint16_t perm;
 };
 
 /** @brief GATT Service structure */
