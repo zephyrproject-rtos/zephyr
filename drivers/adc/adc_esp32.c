7cc34b0b3b0020da77b96ffab6a8f7178a48df5e
Wolter HV <wolterhv@gmx.de>
2022-06-28 21:49:39 +0100

    Various changes, see commit message body.
    *   Fix raw ADC reading inversion (workaround)
    *   Implement ESP-IDF raw voltage to millivolt conversion.
    *   Add more ESP32-specific functions to characterize ADC.

--- drivers/adc/adc_esp32.c
+++ drivers/adc/adc_esp32.c
@@ -19,46 +19,94 @@
 #include <logging/log.h>
 
 #include <hal/adc_hal.h>
+#include <esp_adc_cal.h>
 
 #include "drivers/adc/adc_esp32.h"
 
 #define LOG_LEVEL CONFIG_ADC_LOG_LEVEL
 LOG_MODULE_REGISTER(adc_esp32);
 
-#define ADC_ESP32_RESOLUTION_OFFSET     (9)
+
+/* Macro constants */
+
+#define ADC_ESP32_RESOLUTION_OFFSET        (9)
+#define ADC_ESP32_DEFAULT_VREF_INTERNAL (1100)  /* this is the default used in
+						   espressif:adc1_example_main.c */
+
+
+/* Macro functions */
 
 #define MIN(a, b)          (((a) < (b)) ? (a) : (b))
 #define MAX(a, b)          (((a) > (b)) ? (a) : (b))
 #define CLIP(min, max, x)  (MIN(MAX((min), (x)), (max)))
 #define ELEM_COUNT(arr)    (sizeof(arr)/sizeof(arr[0]))
 
-#define ADC_ESP32_LINTERP(x0, y0, x1, y1, x)				\
+#define LINTERP(x0, y0, x1, y1, x)					\
 ((y0) + (((y1)-(y0))*((x)-(x0)))/((x1)-(x0)))
 
-#define ADC_ESP32_DECLARE_CONST_DEVCONF(dev, devconf)			\
+#define DECLARE_DEVCONF(dev, devconf)					\
 const struct adc_esp32_conf *(devconf) =				\
 	(const struct adc_esp32_conf *) (dev)->config;
 
-#define ADC_ESP32_DECLARE_DEVDATA(dev, devdata)				\
+#define DECLARE_DEVDATA(dev, devdata)					\
 struct adc_esp32_data *(devdata) =					\
 	(struct adc_esp32_data *) (dev)->data;
+
+/* Declarations */
 
 struct adc_esp32_conf;
 struct adc_esp32_data;
 
-static int adc_esp32_init          (const struct device *dev);
-static int adc_esp32_channel_setup (const struct device *dev,
-				    const struct adc_channel_cfg *channel_cfg);
-static int adc_esp32_read          (const struct device *dev,
-			            const struct adc_sequence *sequence);
+static int8_t adc_esp32_atten_map[][2] = {
+	{ (int8_t) ADC_ESP32_ATTEN_0, (int8_t) ADC_ATTEN_DB_0   },
+	{ (int8_t) ADC_ESP32_ATTEN_1, (int8_t) ADC_ATTEN_DB_2_5 },
+	{ (int8_t) ADC_ESP32_ATTEN_2, (int8_t) ADC_ATTEN_DB_6   },
+	{ (int8_t) ADC_ESP32_ATTEN_3, (int8_t) ADC_ATTEN_DB_11  },
+};
+
+static uint8_t adc_esp32_resolution_map[][2] = {
+#if CONFIG_IDF_TARGET_ESP32
+	{(uint8_t) ADC_WIDTH_BIT_9,   9},
+	{(uint8_t) ADC_WIDTH_BIT_10, 10},
+	{(uint8_t) ADC_WIDTH_BIT_11, 11},
+	{(uint8_t) ADC_WIDTH_BIT_12, 12},
+#elif SOC_ADC_MAX_BITWIDTH == 12
+	{(uint8_t) ADC_WIDTH_BIT_12, 12},
+#elif SOC_ADC_MAX_BITWIDTH == 13
+	{(uint8_t) ADC_WIDTH_BIT_13, 13},
+#endif
+};
+
+/* API functions */
+
+static int adc_esp32_init		(const struct device *dev);
+static int adc_esp32_channel_setup	(const struct device *dev,
+					 const struct adc_channel_cfg *channel_cfg);
+static int adc_esp32_read		(const struct device *dev,
+					 const struct adc_sequence *sequence);
 #ifdef CONFIG_ADC_ASYNC
-static int adc_esp32_read_async    (const struct device *dev,
-			            const struct adc_sequence *sequence,
-			            struct k_poll_signal *async);
+static int adc_esp32_read_async		(const struct device *dev,
+					 const struct adc_sequence *sequence,
+					 struct k_poll_signal *async);
 #endif /* CONFIG_ADC_ASYNC */
 
-static int adc_esp32_validate_channel_id(const struct device  *dev,
+/* Helpers */
+
+static int validate_channel_id		(const struct device  *dev,
 					 const uint8_t         channel_id);
+static int validate_resolution		(const uint8_t resolution);
+static int encode_resolution		(const uint8_t      resolution,
+					 adc_bits_width_t  *esp32_resolution);
+static int decode_resolution		(const adc_bits_width_t   esp32_resolution,
+					 uint8_t                 *resolution);
+static int validate_attenuation		(const adc_atten_t  attenuation);
+static int encode_attenuation		(const enum adc_esp32_atten_e   atten,
+					 adc_atten_t                   *esp32_atten);
+static int decode_attenuation		(const adc_atten_t        esp32_atten,
+					 enum adc_esp32_atten_e  *atten);
+
+
+/* Definitions */
 
 static const struct adc_driver_api api_esp32_driver_api = {
 	.channel_setup = adc_esp32_channel_setup,
@@ -66,34 +114,7 @@ static const struct adc_driver_api api_esp32_driver_api = {
 #ifdef CONFIG_ADC_ASYNC
 	.read_async    = adc_esp32_read_async,
 #endif /* CONFIG_ADC_ASYNC */
-	.ref_internal  = 1100, /* this is the default used in
-			          espressif:adc1_example_main.c */
-};
-/*
- * According to espressif:adc1_example_main.c, adc2_vref_to_gpio is a function
- * used to measure the internal voltage reference.
- *
- * According to the espressif:esp_adc_cal_characterize function, the function
- * read_efuse_vref can be used to obtained a reading as well, but apparently
- * the so-called efuse is not always available, this can be checked with
- * check_efuse_vref.
- */
-
-/* To house ESP32-specific stuff */
-struct adc_esp32_conf {
-	adc_ll_num_t  adc_num;
-	uint8_t       channel_count; /* maps to adc1_channel_t or
-					adc2_channel_t, must be set to the
-					relevant ADCn_CHANNEL_MAX from the
-					devicetree configuration. */
-	/* TODO instead of channel count, we can have an array in the devicetree
-	 * description, and take its length */
-};
-
-struct adc_esp32_data {
-	uint16_t      mes_ref_internal; /* mV, measured, update on calibration */
-	uint16_t     *buffer;
-	adc_atten_t   atten[ADC_CHANNEL_MAX];
+	.ref_internal  = ADC_ESP32_DEFAULT_VREF_INTERNAL,
 };
 
 /*
@@ -106,45 +127,92 @@ struct adc_esp32_data {
  * - supported acquisition times
  * - whether differential is supported
  */
+struct adc_esp32_conf {
+	adc_ll_num_t  adc_num;
+	uint8_t       channel_count; /* maps to adc1_channel_t or
+					adc2_channel_t, must be set to the
+					relevant ADCn_CHANNEL_MAX from the
+					devicetree configuration. */
+	/* TODO instead of channel count, we can have an array in the devicetree
+	 * description, and take its length */
+};
 
+struct adc_esp32_data {
+	uint16_t                        meas_ref_internal; /* mV, measured,
+							     update on
+							     calibration */
+	uint16_t                       *buffer;
+	adc_atten_t                     atten[ADC_CHANNEL_MAX];
+	esp_adc_cal_characteristics_t   chars;
+};
 
-/* Exposed functions */
+/*
+ * According to espressif:adc1_example_main.c, adc2_vref_to_gpio is a function
+ * used to measure the internal voltage reference.
+ *
+ * According to the espressif:esp_adc_cal_characterize function, the function
+ * read_efuse_vref can be used to obtained a reading as well, but apparently
+ * the so-called efuse is not always available, this can be checked with
+ * check_efuse_vref.
+ */
+
+/* Extra ESP32 API functions */
+
+int adc_esp32_get_atten(const struct device     *dev,
+			const uint8_t            channel_id,
+			enum adc_esp32_atten_e  *atten)
+{
+	DECLARE_DEVDATA(dev, devdata);
+	adc_atten_t esp32_atten;
+	int         err;
+
+	err = validate_channel_id(dev, channel_id);
+	if (err < 0) {
+		return -EINVAL;
+	}
+
+	atten = NULL;
+	esp32_atten = devdata->atten[channel_id];
+
+	for (int i = 0; i < ELEM_COUNT(adc_esp32_atten_map); i++) {
+		if (esp32_atten == adc_esp32_atten_map[i][1]) {
+			*atten = adc_esp32_atten_map[i][0];
+		}
+	}
+	if (atten == NULL) {
+		LOG_ERR("invalid attenuation");
+		return -EINVAL;
+	}
+
+	return 0;
+}
 
 int adc_esp32_set_atten(const struct device           *dev,
 			const uint8_t                  channel_id,
 			const enum adc_esp32_atten_e   atten)
 {
-	ADC_ESP32_DECLARE_CONST_DEVCONF(dev, devconf);
-	ADC_ESP32_DECLARE_DEVDATA(dev, devdata);
+	DECLARE_DEVCONF(dev, devconf);
+	DECLARE_DEVDATA(dev, devdata);
 	int         err;
 	adc_atten_t esp32_atten;
 
-	err = adc_esp32_validate_channel_id(dev, channel_id);
+	err = validate_channel_id(dev, channel_id);
 	if (err < 0) {
-		return err;
+		return -EINVAL;
 	}
 
-	switch (atten) {
-	case ADC_ESP32_ATTEN_0:
-		esp32_atten = ADC_ATTEN_DB_0;
-		break;
-	case ADC_ESP32_ATTEN_1:
-		esp32_atten = ADC_ATTEN_DB_2_5;
-		break;
-	case ADC_ESP32_ATTEN_2:
-		esp32_atten = ADC_ATTEN_DB_6;
-		break;
-	case ADC_ESP32_ATTEN_3:
-		esp32_atten = ADC_ATTEN_DB_11;
-		break;
-	default:
+	esp32_atten = ADC_ATTEN_MAX;
+	for (int i = 0; i < ELEM_COUNT(adc_esp32_atten_map); i++) {
+		if (atten == adc_esp32_atten_map[i][0]) {
+			esp32_atten = adc_esp32_atten_map[i][1];
+		}
+	}
+	if (esp32_atten == ADC_ATTEN_MAX) {
 		LOG_ERR("invalid attenuation");
 		return -EINVAL;
-		break;
 	}
 
 	devdata->atten[channel_id] = esp32_atten;
-
 	adc_hal_set_atten(devconf->adc_num,
 			  (adc_channel_t) channel_id,
 			  devdata->atten[channel_id]);
@@ -152,53 +220,86 @@ int adc_esp32_set_atten(const struct device           *dev,
 	return 0;
 }
 
-int adc_esp32_raw_to_millivolts(const struct device  *dev,
-				const uint8_t         channel_id,
-				const uint8_t         resolution,
-				const int32_t         adc_ref_voltage,
-			        int32_t              *valp)
+int adc_esp32_update_meas_ref_internal(const struct device *dev)
 {
-	ADC_ESP32_DECLARE_DEVDATA(dev, devdata);
-	int err;
+	DECLARE_DEVDATA(dev, devdata);
 
-	err = adc_esp32_validate_channel_id(dev, channel_id);
-	if (err < 0) {
-		return err;
-	}
-
-	const int x0 =                 0; int y0;
-	const int x1 = (1 << resolution); int y1;
-
-	switch (devdata->atten[channel_id]) {
-	case ADC_ATTEN_DB_0:
-		y0 =  100;
-		y1 =  950;
-		break;
-	case ADC_ATTEN_DB_2_5:
-		y0 =  100;
-		y1 = 1250;
-		break;
-	case ADC_ATTEN_DB_6:
-		y0 =  150;
-		y1 = 1750;
-		break;
-	case ADC_ATTEN_DB_11:
-		y0 =  150;
-		y1 = 2450;
-		break;
-	default:
-		goto invalid_atten;
-		break;
-	}
-
-	*valp = MIN(INT32_MAX,
-		    (adc_ref_voltage * ADC_ESP32_LINTERP(x0, y0, x1, y1, *valp)))
-		/ 1100;
+	/* TODO add logic to actually update the measurement */
+	devdata->meas_ref_internal = ADC_ESP32_DEFAULT_VREF_INTERNAL;
 
 	return 0;
+}
 
-invalid_atten:
-	return -ENOTSUP;
+int adc_esp32_get_meas_ref_internal(const struct device  *dev,
+				    uint16_t             *meas_ref_internal)
+{
+	DECLARE_DEVDATA(dev, devdata);
+
+	*meas_ref_internal = devdata->meas_ref_internal;
+
+	return 0;
+}
+
+int adc_esp32_characterize_by_atten(const struct device           *dev,
+			            const uint8_t                  resolution,
+			            const enum adc_esp32_atten_e   atten)
+{
+	DECLARE_DEVCONF(dev, devconf);
+	DECLARE_DEVDATA(dev, devdata);
+	int			err;
+	adc_atten_t		esp32_atten;
+	adc_bits_width_t	esp32_resolution;
+
+	err = encode_attenuation(atten, &esp32_atten);
+	if (err < 0) {
+		return -EINVAL;
+	}
+
+	err = encode_resolution(resolution, &esp32_resolution);
+	if (err < 0) {
+		return -ENOTSUP;
+	}
+
+	esp_adc_cal_characterize( devconf->adc_num,
+				  esp32_atten,
+				  esp32_resolution,
+				  devdata->meas_ref_internal,
+				 &devdata->chars);
+	/* Instead of ADC_ESP32_DEFAULT_VREF_INTERNAL use adc2_vref_to_gpio for
+	 * a better estimate */
+
+	return 0;
+}
+
+int adc_esp32_characterize_by_channel(const struct device  *dev,
+				      const uint8_t         resolution,
+				      const uint8_t         channel_id)
+{
+	DECLARE_DEVDATA(dev, devdata);
+	int			err;
+	enum adc_esp32_atten_e  atten;
+
+	err = validate_channel_id(dev, channel_id);
+	if (err < 0) {
+		return -ENOTSUP;
+	}
+
+	err = decode_attenuation(devdata->atten[channel_id], &atten);
+	if (err < 0) {
+		return -EINVAL;
+	}
+
+	return adc_esp32_characterize_by_atten(dev, resolution, atten);
+}
+
+int adc_esp32_raw_to_millivolts(const struct device  *dev,
+			        int32_t              *value)
+{
+	DECLARE_DEVDATA(dev, devdata);
+
+	*value = esp_adc_cal_raw_to_voltage(*value, &devdata->chars);
+
+	return 0;
 }
 
 
@@ -206,7 +307,8 @@ invalid_atten:
 
 static int adc_esp32_init(const struct device *dev)
 {
-	ADC_ESP32_DECLARE_DEVDATA(dev, devdata);
+	DECLARE_DEVCONF(dev, devconf);
+	DECLARE_DEVDATA(dev, devdata);
 
 	LOG_DBG("initialising");
 
@@ -214,7 +316,15 @@ static int adc_esp32_init(const struct device *dev)
 		devdata->atten[ch_id] = ADC_ATTEN_DB_0;
 	}
 
-	/* adc_hal_init(); */
+	devdata->meas_ref_internal = ADC_ESP32_DEFAULT_VREF_INTERNAL;
+
+	adc_hal_init();
+
+	/* TODO this depends on the selected clock. Inverted seems to be the
+	 * default behaviour in ESP-IDF */
+	adc_ll_rtc_output_invert  (devconf->adc_num, true);
+	adc_ll_digi_output_invert (devconf->adc_num, true);
+
 	return 0;
 }
 
@@ -222,13 +332,13 @@ static int
 adc_esp32_channel_setup	(const struct device *dev,
 			 const struct adc_channel_cfg *channel_cfg)
 {
-	ADC_ESP32_DECLARE_CONST_DEVCONF(dev, devconf);
-	ADC_ESP32_DECLARE_DEVDATA(dev, devdata);
+	DECLARE_DEVCONF(dev, devconf);
+	DECLARE_DEVDATA(dev, devdata);
 	int err;
 
-	err = adc_esp32_validate_channel_id(dev, channel_cfg->channel_id);
+	err = validate_channel_id(dev, channel_cfg->channel_id);
 	if (err < 0) {
-		return err;
+		return -ENOTSUP;
 	}
 
 	if (channel_cfg->gain != ADC_GAIN_1) {
@@ -272,8 +382,11 @@ static int
 adc_esp32_read	(const struct device *dev,
 		 const struct adc_sequence *sequence)
 {
-	ADC_ESP32_DECLARE_CONST_DEVCONF(dev, devconf);
-	ADC_ESP32_DECLARE_DEVDATA(dev, devdata);
+	DECLARE_DEVCONF(dev, devconf);
+	DECLARE_DEVDATA(dev, devdata);
+	adc_bits_width_t  esp32_resolution;
+	int err;
+	int reading;
 
 	if ((size_t) devconf->channel_count > sequence->buffer_size) {
 		LOG_ERR("sequence buffer only has space for %u channel values, "
@@ -296,53 +409,32 @@ adc_esp32_read	(const struct device *dev,
 		return -ENOTSUP;
 	}
 
-	/* resolution in Zephyr is width in Espressif. */
-	/* ESP32 supports only resolutions of 9, 10, 11 and 12 bits, which are
-	 * mapped as follows:
-	 * zephyr_resolution : espressif_resolution
-	 * -           9 bit : 0
-	 * -          10 bit : 1
-	 * -          11 bit : 2
-	 * -          12 bit : 3
-	 * i.e., zephyr_resolution - 9 = espressif_resolution
-	 * Other ESP32XX versions may support up to 13 bits of resolution.
-	 */
-	int8_t offset_resolution =   (int8_t) sequence->resolution
-				   - ADC_ESP32_RESOLUTION_OFFSET;
-	adc_bits_width_t esp32_resolution =
-		(adc_bits_width_t) CLIP((int8_t) ADC_WIDTH_BIT_9,
-				        (int8_t) SOC_ADC_MAX_BITWIDTH,
-				        (int8_t) offset_resolution);
-	if (offset_resolution != (int8_t) esp32_resolution) {
-		LOG_ERR("resolution not supported, using nearest: %u bits",
-			(uint8_t) esp32_resolution);
-	}
-	adc_hal_rtc_set_output_format(devconf->adc_num,
-				      esp32_resolution);
-	/* adc_ll_digi_set_output_format() */ /* TODO support later */
-
 	if (sequence->calibrate) {
 		/* TODO: Find out how to calibrate. Possibly, resolution needs
 		 * to be applied before calibration. */
 		LOG_ERR("calibration is not supported");
+		return -ENOTSUP;
 	}
 
-	/* TODO: Find out if there are equivalent HAL functions for the
-	 * readings.  For now, use the ESP-IDF functions. */
-	int reading;
+	err = encode_resolution(sequence->resolution, &esp32_resolution);
+	if (err < 0) {
+		return -ENOTSUP;
+	}
+	adc_hal_rtc_set_output_format(devconf->adc_num, esp32_resolution);
+	/* adc_ll_digi_set_output_format() */ /* TODO support later */
 
 #ifdef CONFIG_IDF_TARGET_ESP32
 	adc_hal_hall_disable();
 	adc_hal_amp_disable();
-#endif /* CONFIG_IDF_TARGET_ESP32 */
+#endif /* ifdef CONFIG_IDF_TARGET_ESP32 */
 	adc_hal_set_controller(devconf->adc_num, ADC_CTRL_RTC);
 	adc_hal_convert(devconf->adc_num, channel_id, &reading);
 #if !CONFIG_IDF_TARGET_ESP32
 	adc_hal_rtc_reset();
-#endif /* !CONFIG_IDF_TARGET_ESP32 */
+#endif /* if !CONFIG_IDF_TARGET_ESP32 */
 
-	devdata->buffer = (uint16_t *) sequence->buffer;
-	devdata->buffer[channel_id] = reading;
+	devdata->buffer             = (uint16_t *) sequence->buffer;
+	devdata->buffer[channel_id] = (uint16_t)   reading;
 
 	return 0;
 }
@@ -357,6 +449,11 @@ adc_esp32_read_async	(const struct device *dev,
 	(void)(sequence);
 	(void)(async);
 
+	/*
+	 * TODO Look into the adc_ll_rtc_convert_is_done function, maybe this is
+	 * supported.
+	 */
+
 	return -ENOTSUP;
 }
 #endif /* CONFIG_ADC_ASYNC */
@@ -364,10 +461,10 @@ adc_esp32_read_async	(const struct device *dev,
 
 /* Utility functions */
 
-static int adc_esp32_validate_channel_id(const struct device  *dev,
+static int validate_channel_id(const struct device  *dev,
 					 const uint8_t         channel_id)
 {
-	ADC_ESP32_DECLARE_CONST_DEVCONF(dev, devconf);
+	DECLARE_DEVCONF(dev, devconf);
 	if (channel_id >= devconf->channel_count) {
 		LOG_ERR("unsupported channel id '%u'", channel_id);
 		return -ENOTSUP;
@@ -375,6 +472,81 @@ static int adc_esp32_validate_channel_id(const struct device  *dev,
 	return 0;
 }
 
+static int validate_resolution(const uint8_t resolution)
+{
+	for (int i = 0; i < ELEM_COUNT(adc_esp32_resolution_map); i++) {
+		if (resolution == adc_esp32_resolution_map[i][1]) {
+			return 0;
+		}
+	}
+	LOG_ERR("resolution not supported");
+	return -ENOTSUP;
+}
+
+static int validate_attenuation(const adc_atten_t esp32_atten)
+{
+	for (int i = 0; i < ELEM_COUNT(adc_esp32_atten_map); i++) {
+		if (esp32_atten == (adc_atten_t) adc_esp32_atten_map[i][1]) {
+			return 0;
+		}
+	}
+	LOG_ERR("attenuation is invalid");
+	return -EINVAL;
+}
+
+static int encode_attenuation	(const enum adc_esp32_atten_e   atten,
+				 adc_atten_t                   *esp32_atten)
+{
+	for (int i = 0; i < ELEM_COUNT(adc_esp32_atten_map); i++) {
+		if ((int8_t) atten == adc_esp32_atten_map[i][0]) {
+			*esp32_atten = (adc_atten_t) adc_esp32_atten_map[i][1];
+			return 0;
+		}
+	}
+	LOG_ERR("attenuation is invalid");
+	return -EINVAL;
+}
+
+static int encode_resolution(const uint8_t      resolution,
+			     adc_bits_width_t  *esp32_resolution)
+{
+	for (int i = 0; i < ELEM_COUNT(adc_esp32_resolution_map); i++) {
+		if (resolution == adc_esp32_resolution_map[i][1]) {
+			*esp32_resolution =
+				(adc_bits_width_t) adc_esp32_resolution_map[i][0];
+			return 0;
+		}
+	}
+	LOG_ERR("resolution not supported");
+	return -ENOTSUP;
+}
+
+static int decode_resolution(const adc_bits_width_t   esp32_resolution,
+				       uint8_t                 *resolution)
+{
+	resolution = NULL;
+	for (int i = 0; i < ELEM_COUNT(adc_esp32_resolution_map); i++) {
+		if ((uint8_t) esp32_resolution == adc_esp32_resolution_map[i][0]) {
+			*resolution = adc_esp32_resolution_map[i][1];
+			return 0;
+		}
+	}
+	LOG_ERR("resolution not supported");
+	return -ENOTSUP;
+}
+
+static int decode_attenuation	(const adc_atten_t        esp32_atten,
+				 enum adc_esp32_atten_e  *atten)
+{
+	for (int i = 0; i < ELEM_COUNT(adc_esp32_atten_map); i++) {
+		if (esp32_atten == (adc_atten_t) adc_esp32_atten_map[i][1]) {
+			*atten = (enum adc_esp32_atten_e) adc_esp32_atten_map[i][0];
+			return 0;
+		}
+	}
+	LOG_ERR("attenuation is invalid");
+	return -EINVAL;
+}
 
 /* Footer */
 
@@ -397,13 +569,5 @@ DEVICE_DT_INST_DEFINE(index,						\
 		      POST_KERNEL,			\
 		      CONFIG_ADC_INIT_PRIORITY,				\
 		      &api_esp32_driver_api);
-
-#ifndef _SYS_INIT_LEVEL_POST_KERNEL
-#	error "_SYS_INIT_LEVEL_POST_KERNEL is not defined"
-#endif
-
-#ifndef CONFIG_ADC_INIT_PRIORITY
-#	error "CONFIG_ADC_INIT_PRIORITY is not defined"
-#endif
 
 DT_INST_FOREACH_STATUS_OKAY(ESP32_ADC_INIT)
--- include/drivers/adc/adc_esp32.h
+++ include/drivers/adc/adc_esp32.h
@@ -1,8 +1,15 @@
-/* SPDX-License-Identifier: Apache-2.0 */
+/*
+ * Copyright (c) 2022 Wolter HV <wolterhv@gmx.de>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #ifndef ZEPHYR_INCLUDE_DRIVERS_ADC_ADC_ESP32_H_
 #define ZEPHYR_INCLUDE_DRIVERS_ADC_ADC_ESP32_H_
 
+/*!
+ * @brief Attenuation enum to decouple Zephyr API from ESP-IDF API.
+ */
 enum adc_esp32_atten_e {
 	ADC_ESP32_ATTEN_0 = 0,
 	ADC_ESP32_ATTEN_1,
@@ -11,33 +18,98 @@ enum adc_esp32_atten_e {
 	ADC_ESP32_ATTEN_COUNT,
 };
 
-/* This method sets attenuation */
-/*
- * | Atten. Ref. | Gain | Min volt. | Max. volt. |
- * |:-----------:+-----:+----------:+-----------:|
- * |      0      |  0,0 |       100 |        950 |
- * |      1      |  2,5 |       100 |       1250 |
- * |      2      |  6,0 |       150 |       1750 |
- * |      3      | 11,0 |       150 |       2450 |
+/*!
+ * @brief Gets the attenuation on an ADC channel.
+ *
+ * @param dev		the ADC device handle
+ * @param channel_id	the channel
+ * @param atten		the attenuation
+ *
+ * @retval 0		on success
+ * @retval -EINVAL	if channel_id is invalid
  */
-/* For now, assume linear progression between limits and do a simple
- * linear interpolation */
+int adc_esp32_get_atten(const struct device     *dev,
+			const uint8_t            channel_id,
+			enum adc_esp32_atten_e  *atten);
+
+/*!
+ * @brief Sets the attenuation on an ADC channel.
+ *
+ * @param dev		the ADC device handle
+ * @param channel_id	the channel
+ * @param atten		the attenuation
+ *
+ * @retval 0		on success
+ * @retval -EINVAL	if channel_id is invalid
+ * @retval -EINVAL	if attenuation is invalid
+ */
 int adc_esp32_set_atten(const struct device           *dev,
 			const uint8_t                  channel_id,
 			const enum adc_esp32_atten_e   atten);
 
+
+/*!
+ * @brief Updates the internal reference voltage measurement.
+ */
+int adc_esp32_update_meas_ref_internal(const struct device *dev);
+
+/*!
+ * @brief Sets the parameter to the value of the last reference voltage
+ * measurement.
+ */
+int adc_esp32_get_meas_ref_internal(const struct device  *dev,
+				    uint16_t             *meas_ref_internal);
+
+/*!
+ * @brief Characterizes the ADC channel for accurate conversions from raw values
+ * to millivolts.
+ */
+int adc_esp32_characterize_by_channel(const struct device  *dev,
+			              const uint8_t         resolution,
+			              const uint8_t         channel_id);
+
+int adc_esp32_characterize_by_atten(const struct device           *dev,
+			            const uint8_t                  resolution,
+			            const enum adc_esp32_atten_e   atten);
+/*!
+ * @brief Converts an ADC raw measurement into a value in millivolts.
+ *
+ * The converted value is stored at the location where the raw value was read
+ * from.
+ *
+ * Conversions are performed considering the following look-up table:
+ *
+ * | adc_esp32_atten_e | Gain / dB | Range start / mV | Range end / mV |
+ * |:-----------------:+----------:+-----------------:+---------------:|
+ * | ADC_ESP32_ATTEN_0 |       0,0 |              100 |            950 |
+ * | ADC_ESP32_ATTEN_1 |       2,5 |              100 |           1250 |
+ * | ADC_ESP32_ATTEN_2 |       6,0 |              150 |           1750 |
+ * | ADC_ESP32_ATTEN_3 |      11,0 |              150 |           2450 |
+ *
+ * Source: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/adc.html?highlight=adc#adc-attenuation
+ *
+ * Note: A linear correlation is assumed between the real voltage and the raw
+ * value, though this is not exactly true.
+ *
+ * @param dev			the ADC device handle
+ * @param channel_id		the channel
+ * @param resolution		the resolution / width
+ * @param adc_ref_voltage	the reference voltage
+ * @param valp			pointer to the raw value
+ *
+ * @retval 0		on success
+ * @retval -EINVAL	if channel_id is invalid
+ * @retval -EINVAL	if resolution is invalid
+ * @retval -EINVAL	if the reference voltage is negative
+ * @retval -EINVAL	if the raw value is out of range according to the
+ *			resolution
+ */
 /* At some point, a "characterisation" of the ADC may be needed. In the example
  * it is called with esp_adc_cal_characterize. */
 /* After this, all that is needed is calling adc1_get_raw or adc2_get_raw. */
 /* To convert a raw value to a physical quantity, use
  * esp_adc_cal_raw_to_voltage. */
-/* In the adc_hal.h, the functions adc_ll_set_atten and
- * adc_ll_rtc_enable_channel are used, which suggests that these may the HAL
- * ways of setting up a channel. */
 int adc_esp32_raw_to_millivolts(const struct device  *dev,
-				const uint8_t         channel_id,
-				const uint8_t         resolution,
-				const int32_t         adc_ref_voltage,
-			        int32_t              *valp);
+			        int32_t              *value);
 
 #endif /* ZEPHYR_INCLUDE_DRIVERS_ADC_ADC_ESP32_H_ */
