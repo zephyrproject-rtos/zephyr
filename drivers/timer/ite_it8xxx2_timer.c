835d9e14d7a23aaf4bb8b51a61aac7dcb008d952
Anas Nashif <anas.nashif@intel.com>
2022-06-28 17:58:40 -0400

    drivers: timer: provide timer irq to tests
    As with previous commit, make the timer irq a simple integer variable
    exported by the timer driver for the benefit of this one test
    (tests/kernel/context).
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- drivers/timer/ite_it8xxx2_timer.c
+++ drivers/timer/ite_it8xxx2_timer.c
@@ -37,6 +37,10 @@ BUILD_ASSERT(CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC == 32768,
 /* Busy wait high timer max count is 71.58min (base on clock source 1MHz) */
 #define BUSY_WAIT_TIMER_H_MAX_CNT 0xFFFFFFFFUL
 
+#if defined(CONFIG_TEST)
+const int32_t z_sys_timer_irq_for_test = DT_IRQ_BY_IDX(DT_NODELABEL(timer), 5, irq);
+#endif
+
 #ifdef CONFIG_SOC_IT8XXX2_PLL_FLASH_48M
 /*
  * One shot timer configurations
