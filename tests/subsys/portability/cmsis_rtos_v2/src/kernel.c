225609f1f61947bfc76d1a070f97e479acbccb2e
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2022-06-29 17:00:28 +0300

    tests: cmsis_rtos_v2: Remove unused variables
    Clean up dead code.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- tests/subsys/portability/cmsis_rtos_v2/src/kernel.c
+++ tests/subsys/portability/cmsis_rtos_v2/src/kernel.c
@@ -34,18 +34,14 @@ void get_version_check(const void *param)
 
 void lock_unlock_check(const void *arg)
 {
-	ARG_UNUSED(arg);
-
 	int32_t state_before_lock, state_after_lock, current_state;
-	uint32_t tick_freq, sys_timer_freq;
 
+	ARG_UNUSED(arg);
 
 	state_before_lock = osKernelLock();
 	if (k_is_in_isr()) {
 		zassert_true(state_before_lock == osErrorISR, NULL);
 	}
-	tick_freq = osKernelGetTickFreq();
-	sys_timer_freq = osKernelGetTickFreq();
 
 	state_after_lock = osKernelUnlock();
 	if (k_is_in_isr()) {
