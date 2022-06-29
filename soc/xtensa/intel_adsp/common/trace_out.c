ff2164974984dc3b7622e4cb96b28e892e24aaab
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2022-06-29 19:17:41 +0300

    [DO NOT MERGER] soc/intel_adsp: implement IPC4 mtrace layout
    This is a proof-of-concept that modifies the existing winstream
    backend to output logs according to alternative SRAM layout
    used by IPC4 SOF drivers.
    
    This patch is a partial implementation. Notably missing:
      - a IPC4 notification needs to be sent when new log data
        is added to the buffer (with some configurable thresholds
        that can be set by host)
      - the mtrace layout divides the SRAM window into multiple slots,
        this PR only uses a single slot
      - the slot configuration is hardcoded to expose a single
        debug slot, with no support for multicore debug

--- soc/xtensa/intel_adsp/common/trace_out.c
+++ soc/xtensa/intel_adsp/common/trace_out.c
@@ -10,8 +10,48 @@ struct k_spinlock trace_lock;
 
 static struct sys_winstream *winstream;
 
+#define SOF_MTRACE_DESCRIPTOR_SIZE	12 /* 3 x u32 */
+#define SOF_MTRACE_PAGE_SIZE		0x1000
+#define SOF_MTRACE_SLOT_SIZE		SOF_MTRACE_PAGE_SIZE
+
+static void mtrace_init(void)
+{
+	uint8_t *buf2 = z_soc_uncached_ptr((void *)HP_SRAM_WIN2_BASE);
+	uint8_t *toffset = (uint8_t*)buf2 + 4;
+	unsigned int boffset = SOF_MTRACE_SLOT_SIZE + 8;
+	volatile uint32_t *type = (uint32_t*) toffset;
+
+	/* FIXME: something zeros window2 after soc_trace_init(), so
+	 * this has to be redone here ondemand until issue rootcaused */
+	if (*type != 0x474f4c00) {
+		*type = 0x474f4c00;
+
+		winstream = sys_winstream_init(buf2 + boffset, HP_SRAM_WIN2_SIZE - boffset);
+	}
+}
+
+static void mtrace_update_dsp_writeptr(void)
+{
+	uint8_t *buf = z_soc_uncached_ptr((void *)HP_SRAM_WIN2_BASE);
+	unsigned int winstr_offset = SOF_MTRACE_SLOT_SIZE + 2 * sizeof(uint32_t);
+	unsigned int dspptr_offset = SOF_MTRACE_SLOT_SIZE + sizeof(uint32_t);
+	struct sys_winstream *winstream = (struct sys_winstream *)(buf + winstr_offset);
+	uint32_t pos = winstream->end + 4 * sizeof(uint32_t);
+	volatile uint32_t *dsp_ptr = (uint32_t*)(buf + dspptr_offset);
+
+	/* copy write pointer maintained by winstream to correct
+	 * place (as expected by driver) */
+	*dsp_ptr = pos;
+}
+
 void intel_adsp_trace_out(int8_t *str, size_t len)
 {
+	/* FIXME: the WIN2 area gets overwritten/zeroed
+	 *        some time after soc_trace_init(), so as
+	 *        a stopgap, keep reinitializing the WIN2
+	 *        headers */
+	mtrace_init();
+
 	if (len == 0) {
 		return;
 	}
@@ -25,12 +65,12 @@ void intel_adsp_trace_out(int8_t *str, size_t len)
 	__asm__ volatile("simcall" : "+r"(a2), "+r"(a3)
 			 : "r"(a4), "r"(a5)  : "memory");
 #endif
-
 	k_spinlock_key_t key = k_spin_lock(&trace_lock);
-
 	sys_winstream_write(winstream, str, len);
+	mtrace_update_dsp_writeptr();
 	k_spin_unlock(&trace_lock, key);
 }
+
 
 int arch_printk_char_out(int c)
 {
@@ -42,7 +82,5 @@ int arch_printk_char_out(int c)
 
 void soc_trace_init(void)
 {
-	void *buf = z_soc_uncached_ptr((void *)HP_SRAM_WIN3_BASE);
-
-	winstream = sys_winstream_init(buf, HP_SRAM_WIN3_SIZE);
+	mtrace_init();
 }
