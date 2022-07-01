92725e31028430b1b23eb487b6be834412ed0e3e
Flavio Ceolin <flavio.ceolin@intel.com>
2022-07-01 13:23:30 -0700

    soc: adsp: sram: Assorted cosmetic fixes
    General code style fixes.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- soc/xtensa/intel_adsp/common/sram.c
+++ soc/xtensa/intel_adsp/common/sram.c
@@ -14,7 +14,8 @@
 #include "manifest.h"
 
 
-#define LPSRAM_MASK(x)		 0x00000003
+#define DELAY_COUNT			256
+#define LPSRAM_MASK(x)		0x00000003
 #define SRAM_BANK_SIZE		(64 * 1024)
 #define EBB_SEGMENT_SIZE	32
 #if !defined(CONFIG_SOC_SERIES_INTEL_CAVS_V15)
@@ -25,37 +26,33 @@
 
 #define PLATFORM_HPSRAM_EBB_COUNT (DT_REG_SIZE(DT_NODELABEL(sram0)) / SRAM_BANK_SIZE)
 BUILD_ASSERT((DT_REG_SIZE(DT_NODELABEL(sram0)) % SRAM_BANK_SIZE) == 0,
-	     "sram0 must be divisible by 64*1024 bank size.");
+		"sram0 must be divisible by 64*1024 bank size.")
 
-/* function powers up a number of memory banks provided as an argument and
- * gates remaining memory banks
+/*
+ * Function powers up a number of memory banks provided as an argument
+ * and gates remaining memory banks
  */
 static __imr void hp_sram_pm_banks(uint32_t banks)
 {
 #ifdef PLATFORM_INIT_HPSRAM
-	int delay_count = 256;
-
-	uint32_t status;
-
-	uint32_t ebb_mask0, ebb_mask1, ebb_avail_mask0, ebb_avail_mask1;
-
-	uint32_t total_banks_count = PLATFORM_HPSRAM_EBB_COUNT;
+	uint32_t status, ebb_mask0, ebb_mask1, ebb_avail_mask0, ebb_avail_mask1,
+		total_banks_count = PLATFORM_HPSRAM_EBB_COUNT;
 
 	CAVS_SHIM.ldoctl = SHIM_LDOCTL_HPSRAM_LDO_ON;
 
-	/* add some delay before touch power register */
-	z_idelay(delay_count);
+	/* Add some delay before touch power register */
+	z_idelay(DELAY_COUNT);
 
-	/* bit masks reflect total number of available EBB (banks) in each
+	/*
+	 * bit masks reflect total number of available EBB (banks) in each
 	 * segment; current implementation supports 2 segments 0,1
 	 */
 	if (total_banks_count > EBB_SEGMENT_SIZE) {
 		ebb_avail_mask0 = (uint32_t)GENMASK(EBB_SEGMENT_SIZE - 1, 0);
 		ebb_avail_mask1 = (uint32_t)GENMASK(total_banks_count -
-		EBB_SEGMENT_SIZE - 1, 0);
+						    EBB_SEGMENT_SIZE - 1, 0);
 	} else {
-		ebb_avail_mask0 = (uint32_t)GENMASK(total_banks_count - 1,
-		0);
+		ebb_avail_mask0 = (uint32_t)GENMASK(total_banks_count - 1, 0);
 		ebb_avail_mask1 = 0;
 	}
 
@@ -76,24 +73,30 @@ static __imr void hp_sram_pm_banks(uint32_t banks)
 	CAVS_L2LM.hspgctl1 = (~ebb_mask1) & ebb_avail_mask1;
 	CAVS_L2LM.hsrmctl1 = (~ebb_mask1) & ebb_avail_mask1;
 
-	/* query the power status of first part of HP memory */
-	/* to check whether it has been powered up. A few    */
-	/* cycles are needed for it to be powered up         */
+	/*
+	 * Query the power status of first part of HP memory
+	 * to check whether it has been powered up. A few
+	 * cycles are needed for it to be powered up
+	 */
 	status = CAVS_L2LM.hspgists0;
 	while (status != ((~ebb_mask0) & ebb_avail_mask0)) {
-		z_idelay(delay_count);
+		z_idelay(DELAY_COUNT);
 		status = CAVS_L2LM.hspgists0;
 	}
-	/* query the power status of second part of HP memory */
-	/* and do as above code                               */
 
+	/*
+	 * Query the power status of second part of HP memory
+	 * and do as above code
+	 */
 	status = CAVS_L2LM.hspgists1;
+
 	while (status != ((~ebb_mask1) & ebb_avail_mask1)) {
-		z_idelay(delay_count);
+		z_idelay(DELAY_COUNT);
 		status = CAVS_L2LM.hspgists1;
 	}
-	/* add some delay before touch power register */
-	z_idelay(delay_count);
+
+	/* Add some delay before touch power register */
+	z_idelay(DELAY_COUNT);
 
 	CAVS_SHIM.ldoctl = SHIM_LDOCTL_HPSRAM_LDO_BYPASS;
 #endif
@@ -103,7 +106,8 @@ __imr void hp_sram_init(uint32_t memory_size)
 {
 	uint32_t ebb_in_use;
 
-	/* calculate total number of used SRAM banks (EBB)
+	/*
+	 * Calculate total number of used SRAM banks (EBB)
 	 * to power up only necessary banks
 	 */
 	ebb_in_use = ceiling_fraction(memory_size, SRAM_BANK_SIZE);
@@ -116,25 +120,27 @@ __imr void hp_sram_init(uint32_t memory_size)
 __imr void lp_sram_init(void)
 {
 #ifdef PLATFORM_INIT_LPSRAM
-	uint32_t timeout_counter, delay_count = 256;
+	uint32_t timeout_counter;
 
-	timeout_counter = delay_count;
+	timeout_counter = DELAY_COUNT;
 
 	CAVS_SHIM.ldoctl = SHIM_LDOCTL_LPSRAM_LDO_ON;
 
-	/* add some delay before writing power registers */
-	z_idelay(delay_count);
+	/* Add some delay before writing power registers */
+	z_idelay(DELAY_COUNT);
 
 	CAVS_SHIM.lspgctl = CAVS_SHIM.lspgists & ~LPSRAM_MASK(0);
 
-	/* add some delay before checking the status */
-	z_idelay(delay_count);
+	/* Add some delay before checking the status */
+	z_idelay(DELAY_COUNT);
 
-	/* query the power status of first part of LP memory */
-	/* to check whether it has been powered up. A few    */
-	/* cycles are needed for it to be powered up         */
+	/*
+	 * Query the power status of first part of LP memory
+	 * to check whether it has been powered up. A few
+	 * cycles are needed for it to be powered up
+	 */
 	while (CAVS_SHIM.lspgists && timeout_counter--) {
-		z_idelay(delay_count);
+		z_idelay(DELAY_COUNT);
 	}
 
 	CAVS_SHIM.ldoctl = SHIM_LDOCTL_LPSRAM_LDO_BYPASS;
