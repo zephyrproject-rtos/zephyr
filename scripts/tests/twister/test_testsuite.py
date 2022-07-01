e29c8df31b76e3a6b56b6a7f6c4c1b4bce08c276
Anas Nashif <anas.nashif@intel.com>
2022-07-01 05:43:45 -0400

    twister: adapt twister testsuite
    Adapt with new way for parsing testsuites.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/tests/twister/test_testsuite.py
+++ scripts/tests/twister/test_testsuite.py
@@ -13,7 +13,8 @@ import pytest
 ZEPHYR_BASE = os.getenv("ZEPHYR_BASE")
 sys.path.insert(0, os.path.join(ZEPHYR_BASE, "scripts/pylib/twister"))
 
-from twisterlib.testsuite import scan_file, ScanPathResult
+from twisterlib.testsuite import scan_file, ScanPathResult, TestSuite
+from twisterlib.error import TwisterException
 
 TESTDATA_5 = [
     ("testsuites/tests/test_ztest.c",
@@ -58,3 +59,54 @@ def test_scan_file(test_data, test_file, class_env, expected: ScanPathResult):
 
     result: ScanPathResult = scan_file(os.path.join(test_data, test_file))
     assert result == expected
+
+
+TESTDATA_4 = [
+    (ZEPHYR_BASE, '.', 'test_c', 'Tests should reference the category and subsystem with a dot as a separator.'),
+    (os.path.join(ZEPHYR_BASE, '/scripts/tests'), '.', '', 'Tests should reference the category and subsystem with a dot as a separator.'),
+]
+@pytest.mark.parametrize("testsuite_root, workdir, name, exception", TESTDATA_4)
+def test_get_unique_exception(testsuite_root, workdir, name, exception):
+    '''Test to check if tests reference the category and subsystem with a dot as a separator'''
+
+    with pytest.raises(TwisterException):
+        unique = TestSuite(testsuite_root, workdir, name)
+        assert unique == exception
+
+TESTDATA_3 = [
+        (
+            ZEPHYR_BASE + '/scripts/tests/twister/test_data/testsuites',
+            ZEPHYR_BASE + '/scripts/tests/twister/test_data/testsuites/tests/test_a',
+            '/scripts/tests/twister/test_data/testsuites/tests/test_a/test_a.check_1',
+            '/scripts/tests/twister/test_data/testsuites/tests/test_a/test_a.check_1'
+        ),
+        (
+            ZEPHYR_BASE,
+            ZEPHYR_BASE,
+            'test_a.check_1',
+            'test_a.check_1'
+        ),
+        (
+            ZEPHYR_BASE,
+            ZEPHYR_BASE + '/scripts/tests/twister/test_data/testsuites/test_b',
+            '/scripts/tests/twister/test_data/testsuites/test_b/test_b.check_1',
+            '/scripts/tests/twister/test_data/testsuites/test_b/test_b.check_1'
+        ),
+        (
+            os.path.join(ZEPHYR_BASE, '/scripts/tests'),
+            os.path.join(ZEPHYR_BASE, '/scripts/tests'),
+            'test_b.check_1',
+            'test_b.check_1'
+        ),
+        (
+            ZEPHYR_BASE,
+            ZEPHYR_BASE,
+            'test_a.check_1.check_2',
+            'test_a.check_1.check_2'
+        ),
+]
+@pytest.mark.parametrize("testsuite_root, suite_path, name, expected", TESTDATA_3)
+def test_get_unique(testsuite_root, suite_path, name, expected):
+    '''Test to check if the unique name is given for each testsuite root and workdir'''
+    suite = TestSuite(testsuite_root, suite_path, name)
+    assert suite.name == expected
