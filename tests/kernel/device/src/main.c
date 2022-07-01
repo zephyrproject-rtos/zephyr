e4414e64ace55fd2cac842033316610ce36fa55b
Guo Lixin <lixinx.guo@intel.com>
2022-07-01 16:29:08 +0800

    tests: kernel: device: move to new ztest API
    Move tests/kernel/device to new ztest API.
    
    Signed-off-by: Guo Lixin <lixinx.guo@intel.com>

--- tests/kernel/device/src/main.c
+++ tests/kernel/device/src/main.c
@@ -20,12 +20,6 @@
 #define MY_DRIVER_A     "my_driver_A"
 #define MY_DRIVER_B     "my_driver_B"
 
-extern void test_mmio_multiple(void);
-extern void test_mmio_toplevel(void);
-extern void test_mmio_single(void);
-extern void test_mmio_device_map(void);
-extern void test_mmio_multireg(void);
-
 /**
  * @brief Test cases to verify device objects
  *
@@ -54,7 +48,7 @@ extern void test_mmio_multireg(void);
  *
  * @see device_get_binding(), DEVICE_DEFINE()
  */
-void test_dummy_device(void)
+ZTEST(device, test_dummy_device)
 {
 	const struct device *dev;
 
@@ -80,7 +74,7 @@ void test_dummy_device(void)
  *
  * @see device_get_binding(), DEVICE_DEFINE()
  */
-static void test_dynamic_name(void)
+ZTEST_USER(device, test_dynamic_name)
 {
 	const struct device *mux;
 	char name[sizeof(DUMMY_PORT_2)];
@@ -98,7 +92,7 @@ static void test_dynamic_name(void)
  *
  * @see device_get_binding(), DEVICE_DEFINE()
  */
-static void test_bogus_dynamic_name(void)
+ZTEST_USER(device, test_bogus_dynamic_name)
 {
 	const struct device *mux;
 	char name[64];
@@ -115,7 +109,7 @@ static void test_bogus_dynamic_name(void)
  *
  * @see device_get_binding(), DEVICE_DEFINE()
  */
-static void test_null_dynamic_name(void)
+ZTEST_USER(device, test_null_dynamic_name)
 {
 	/* Supplying a NULL dynamic name may trigger a SecureFault and
 	 * lead to system crash in TrustZone enabled Non-Secure builds.
@@ -196,7 +190,7 @@ SYS_INIT(null_driver_init, POST_KERNEL, 0);
  *
  * @see k_is_pre_kernel()
  */
-void test_pre_kernel_detection(void)
+ZTEST(device, test_pre_kernel_detection)
 {
 	struct init_record *rpe = rp;
 
@@ -234,7 +228,7 @@ void test_pre_kernel_detection(void)
  *
  * @see z_device_get_all_static()
  */
-static void test_device_list(void)
+ZTEST(device, test_device_list)
 {
 	struct device const *devices;
 	size_t devcount = z_device_get_all_static(&devices);
@@ -258,7 +252,7 @@ extern unsigned int seq_priority_cnt;
  *
  * @ingroup kernel_device_tests
  */
-void test_device_init_level(void)
+ZTEST(device, test_device_init_level)
 {
 	bool seq_correct = true;
 
@@ -284,7 +278,7 @@ void test_device_init_level(void)
  *
  * @ingroup kernel_device_tests
  */
-void test_device_init_priority(void)
+ZTEST(device, test_device_init_priority)
 {
 	bool sequence_correct = true;
 
@@ -316,7 +310,7 @@ void test_device_init_priority(void)
  *
  * @ingroup kernel_device_tests
  */
-void test_abstraction_driver_common(void)
+ZTEST(device, test_abstraction_driver_common)
 {
 	const struct device *dev;
 	int ret;
@@ -350,22 +344,4 @@ void test_abstraction_driver_common(void)
  * @}
  */
 
-void test_main(void)
-{
-	ztest_test_suite(device,
-			 ztest_unit_test(test_device_list),
-			 ztest_unit_test(test_dummy_device),
-			 ztest_unit_test(test_pre_kernel_detection),
-			 ztest_user_unit_test(test_bogus_dynamic_name),
-			 ztest_user_unit_test(test_null_dynamic_name),
-			 ztest_user_unit_test(test_dynamic_name),
-			 ztest_unit_test(test_device_init_level),
-			 ztest_unit_test(test_device_init_priority),
-			 ztest_unit_test(test_abstraction_driver_common),
-			 ztest_unit_test(test_mmio_single),
-			 ztest_unit_test(test_mmio_multiple),
-			 ztest_unit_test(test_mmio_toplevel),
-			 ztest_unit_test(test_mmio_device_map),
-			 ztest_unit_test(test_mmio_multireg));
-	ztest_run_test_suite(device);
-}
+ZTEST_SUITE(device, NULL, NULL, NULL, NULL, NULL);
