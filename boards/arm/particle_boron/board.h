5f51cb0567d4a99139cb58217c2ebdabbebf18bc
Kumar Gala <galak@kernel.org>
2022-07-01 18:17:41 -0500

    boards: particle_boron: Use GPIO_DT_SPEC for gpio handling
    Convert gpio handling to use GPIO_DT_SPEC as this removes the use
    of DT_LABEL.
    
    Signed-off-by: Kumar Gala <galak@kernel.org>

--- boards/arm/particle_boron/board.h
+++ boards/arm/particle_boron/board.h
@@ -16,9 +16,4 @@
 #define V_INT_DETECT_GPIO_PIN	2
 #define V_INT_DETECT_GPIO_FLAGS	GPIO_ACTIVE_HIGH
 
-/* SKYWORKS SKY13351 antenna selection settings (only use vctl1) */
-#define ANT_UFLn_GPIO_NAME	DT_GPIO_LABEL(DT_INST(0, skyworks_sky13351), vctl1_gpios)
-#define ANT_UFLn_GPIO_FLAGS	DT_GPIO_FLAGS(DT_INST(0, skyworks_sky13351), vctl1_gpios)
-#define ANT_UFLn_GPIO_PIN	DT_GPIO_PIN(DT_INST(0, skyworks_sky13351), vctl1_gpios)
-
 #endif /* __INC_BOARD_H */
