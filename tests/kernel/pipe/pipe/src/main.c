af1e0c5a005b097b4cf5d6d12926b29cf315ed03
Guo Lixin <lixinx.guo@intel.com>
2022-07-01 11:55:49 +0800

    tests: pipe: move to new ztest API
    Move tests/kernel/pipe/pipe/ to use new ztest API.
    And make test case a little more independent.
    
    Signed-off-by: Guo Lixin <lixinx.guo@intel.com>

--- tests/kernel/pipe/pipe/src/main.c
+++ tests/kernel/pipe/pipe/src/main.c
@@ -6,47 +6,20 @@
 
 #include <ztest.h>
 
-extern void test_pipe_on_single_elements(void);
-extern void test_pipe_on_multiple_elements(void);
-extern void test_pipe_forever_wait(void);
-extern void test_pipe_timeout(void);
-extern void test_pipe_get_on_empty_pipe(void);
-extern void test_pipe_forever_timeout(void);
-extern void test_pipe_get_timeout(void);
-extern void test_pipe_get_invalid_size(void);
-extern void test_pipe_get_min_xfer(void);
-extern void test_pipe_put_min_xfer(void);
-extern void test_pipe_define_at_runtime(void);
-extern void test_pipe_flush(void);
-
 extern struct k_pipe test_pipe;
 extern struct k_pipe small_pipe;
 extern struct k_sem put_sem, get_sem, sync_sem, multiple_send_sem;
 extern struct k_stack stack_1;
 extern struct k_thread get_single_tid;
 
-/*test case main entry*/
-void test_main(void)
+static void *pipe_setup(void)
 {
 	k_thread_access_grant(k_current_get(),
 			      &test_pipe, &small_pipe, &put_sem, &get_sem,
 			      &sync_sem, &multiple_send_sem,
 			      &get_single_tid, &stack_1);
 
-	ztest_test_suite(test_pipe,
-			 ztest_user_unit_test(test_pipe_on_single_elements),
-			 ztest_user_unit_test(test_pipe_on_multiple_elements),
-			 ztest_user_unit_test(test_pipe_forever_wait),
-			 ztest_user_unit_test(test_pipe_timeout),
-			 ztest_user_unit_test(test_pipe_get_on_empty_pipe),
-			 ztest_user_unit_test(test_pipe_forever_timeout),
-			 ztest_user_unit_test(test_pipe_get_timeout),
-			 ztest_user_unit_test(test_pipe_get_invalid_size),
-			 ztest_user_unit_test(test_pipe_get_min_xfer),
-			 ztest_user_unit_test(test_pipe_put_min_xfer),
-			 ztest_unit_test(test_pipe_define_at_runtime),
-			 ztest_unit_test(test_pipe_flush)
-			 );
-
-	ztest_run_test_suite(test_pipe);
+	return NULL;
 }
+
+ZTEST_SUITE(pipe, NULL, pipe_setup, NULL, NULL, NULL);
