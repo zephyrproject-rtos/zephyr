aede9fcba3258b72dc532cd98a29e9389d2d2c1b
Piotr Kmiecik <piotrx.kmiecik@intel.com>
2022-07-01 08:49:40 +0200

    drivers/counter: return 64-bit value
    Extend the API to add the possibility
    to return value of 64-bit counter.
    
    Signed-off-by: Piotr Kmiecik <piotrx.kmiecik@intel.com>

--- drivers/counter/counter_handlers.c
+++ drivers/counter/counter_handlers.c
@@ -71,6 +71,15 @@ static inline int z_vrfy_counter_get_value(const struct device *dev,
 	Z_OOPS(Z_SYSCALL_MEMORY_WRITE(ticks, sizeof(*ticks)));
 	return z_impl_counter_get_value((const struct device *)dev, ticks);
 }
+
+static inline int z_vrfy_counter_get_value_64(const struct device *dev,
+					   uint64_t *ticks)
+{
+	Z_OOPS(Z_SYSCALL_DRIVER_COUNTER(dev, get_value_64));
+	Z_OOPS(Z_SYSCALL_MEMORY_WRITE(ticks, sizeof(*ticks)));
+	return z_impl_counter_get_value_64((const struct device *)dev, ticks);
+}
+
 #include <syscalls/counter_get_value_mrsh.c>
 
 static inline int z_vrfy_counter_set_channel_alarm(const struct device *dev,
