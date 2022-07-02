64affbc8776a770e37d27481b9a54e40a015a267
Jordan Yates <jordan.yates@data61.csiro.au>
2022-07-02 12:18:35 +1000

    tests: device: validate `SYS_INIT_NAMED`
    Check that `SYS_INIT_NAMED` allows multiple instances of the same
    initialisation function, as long as names are unique.
    
    Signed-off-by: Jordan Yates <jordan.yates@data61.csiro.au>

--- tests/kernel/device/src/main.c
+++ tests/kernel/device/src/main.c
@@ -236,6 +236,24 @@ ZTEST(device, test_device_list)
 	zassert_false((devcount == 0), NULL);
 }
 
+static int sys_init_counter;
+
+static int init_fn(const struct device *dev)
+{
+	sys_init_counter++;
+	return 0;
+}
+
+SYS_INIT(init_fn, APPLICATION, 0);
+SYS_INIT_NAMED(init1, init_fn, APPLICATION, 1);
+SYS_INIT_NAMED(init2, init_fn, APPLICATION, 2);
+SYS_INIT_NAMED(init3, init_fn, APPLICATION, 2);
+
+ZTEST(device, test_sys_init_multiple)
+{
+	zassert_equal(sys_init_counter, 4, "");
+}
+
 /* this is for storing sequence during initialization */
 extern int init_level_sequence[4];
 extern int init_priority_sequence[4];
