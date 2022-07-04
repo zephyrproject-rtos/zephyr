1cd97f11daa28027166021f7bdfb9250d8d63045
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-07-04 16:37:23 +0200

    include: zephyr: remove __ZEPHYR__
    CMake always injects __ZEPHYR__ to any code being built on Zephyr (e.g.
    a module, an application, etc.). Requiring an include that defines
    __ZEPHYR__ to determine if I'm on Zephyr is problematic as well: it
    requires to include a Zephyr header (which will exist if building for...
    Zephyr!).
    
    Note that this change leaves <zephyr/zephyr.h> in a questionable
    position: why shouldn't I just include <zephyr/kernel.h>?
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- include/zephyr/zephyr.h
+++ include/zephyr/zephyr.h
@@ -7,14 +7,6 @@
 #ifndef ZEPHYR_INCLUDE_ZEPHYR_H_
 #define ZEPHYR_INCLUDE_ZEPHYR_H_
 
-/*
- * Applications can identify whether they are built for Zephyr by
- * macro below. (It may be already defined by a makefile or toolchain.)
- */
-#ifndef __ZEPHYR__
-#define __ZEPHYR__
-#endif
-
 #include <zephyr/kernel.h>
 
 #endif /* ZEPHYR_INCLUDE_ZEPHYR_H_ */
