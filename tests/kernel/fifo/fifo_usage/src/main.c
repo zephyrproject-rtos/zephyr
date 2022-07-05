c25fb9ee1fce97501d2d82a19613f81cf9e555bb
Guo Lixin <lixinx.guo@intel.com>
2022-07-05 16:28:15 +0800

    tests: kernel: fifo_usage: move to new ztest API
    Move tests/kernel/fifo/fifo_usage/ to use new ztest API.
    
    Signed-off-by: Guo Lixin <lixinx.guo@intel.com>

--- tests/kernel/fifo/fifo_usage/src/main.c
+++ tests/kernel/fifo/fifo_usage/src/main.c
@@ -141,7 +141,7 @@ static void thread_entry_fn_isr(void *p1, void *p2, void *p3)
  * is returned back to Test Thread, it extracts all items from the fifo.
  * @see k_fifo_get(), k_fifo_is_empty(), k_fifo_put(), #K_FIFO_DEFINE(x)
  */
-static void test_single_fifo_play(void)
+ZTEST(fifo_usage, test_single_fifo_play)
 {
 	void *rx_data;
 	uint32_t i;
@@ -180,7 +180,7 @@ static void test_single_fifo_play(void)
  * Child Thread and so forth.
  * @see k_fifo_get(), k_fifo_is_empty(), k_fifo_put(), #K_FIFO_DEFINE(x)
  */
-static void test_dual_fifo_play(void)
+ZTEST(fifo_usage, test_dual_fifo_play)
 {
 	void *rx_data;
 	uint32_t i;
@@ -212,7 +212,7 @@ static void test_dual_fifo_play(void)
  * All the Push and Pop operations happen in ISR Context.
  * @see k_fifo_get(), k_fifo_is_empty(), k_fifo_put(), #K_FIFO_DEFINE(x)
  */
-static void test_isr_fifo_play(void)
+ZTEST(fifo_usage, test_isr_fifo_play)
 {
 	/* Init kernel objects */
 	k_sem_init(&end_sema, 0, 1);
@@ -239,12 +239,5 @@ static void test_isr_fifo_play(void)
  * @}
  */
 
-/*test case main entry*/
-void test_main(void)
-{
-	ztest_test_suite(test_fifo_usage,
-			 ztest_1cpu_unit_test(test_single_fifo_play),
-			 ztest_1cpu_unit_test(test_dual_fifo_play),
-			 ztest_1cpu_unit_test(test_isr_fifo_play));
-	ztest_run_test_suite(test_fifo_usage);
-}
+ZTEST_SUITE(fifo_usage, NULL, NULL,
+		ztest_simple_1cpu_before, ztest_simple_1cpu_after, NULL);
