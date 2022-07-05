e24cd9915a82e81d8144e6a8e30a01acc1ce9050
Bjarki Arge Andreasen <bjarkix123@gmail.com>
2022-07-05 13:00:26 +0200

    drivers/location: Add location API
    The Location API is designed to provide the essential location
    data which can be produced from sources like SUPL. GNSS, WIFI-
    location, cellular location, propriatary solutions etc.
    
    It is primed for use with the upcomming multi interface device
    model, and for implementation into the upcomming GNSS API
    
    Signed-off-by: Bjarki Arge Andreasen <bjarkix123@gmail.com>

---
+++ tests/drivers/location/src/main.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/zephyr.h>
+#include <zephyr/ztest.h>
+
+extern void test_location_api_position_get(void);
+extern void test_location_api_bearing_get(void);
+extern void test_location_api_speed_get(void);
+extern void test_location_api_altitude_get(void);
+
+extern void test_location_api_position_get_user(void);
+extern void test_location_api_bearing_get_user(void);
+extern void test_location_api_speed_get_user(void);
+extern void test_location_api_altitude_get_user(void);
+
+extern void test_location_api_event_handler_set(void);
+extern void test_location_api_event_handler_set_user(void);
+
+void test_main(void)
+{
+	ztest_test_suite(location_test_supervisor,
+			 ztest_unit_test(test_location_api_position_get),
+			 ztest_unit_test(test_location_api_bearing_get),
+			 ztest_unit_test(test_location_api_speed_get),
+			 ztest_unit_test(test_location_api_altitude_get),
+			 ztest_unit_test(test_location_api_event_handler_set));
+
+	ztest_test_suite(location_test_user,
+			 ztest_user_unit_test(test_location_api_position_get_user),
+			 ztest_user_unit_test(test_location_api_bearing_get_user),
+			 ztest_user_unit_test(test_location_api_speed_get_user),
+			 ztest_user_unit_test(test_location_api_altitude_get_user),
+			 ztest_user_unit_test(test_location_api_event_handler_set_user));
+
+	ztest_run_test_suite(location_test_supervisor);
+	ztest_run_test_suite(location_test_user);
+}
