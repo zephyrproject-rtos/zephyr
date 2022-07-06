e2638760655f0e28d018953268f06c2f80b6d86c
Kumar Gala <galak@kernel.org>
2022-07-05 21:19:05 -0500

    samples: x_nucleo_iks02a1: standard: Convert to use DEVICE_DT_GET_ONE
    Move to use DEVICE_DT_GET_ONE instead of device_get_binding as
    we work on phasing out use of DTS 'label' property.
    
    Signed-off-by: Kumar Gala <galak@kernel.org>

--- samples/shields/x_nucleo_iks02a1/standard/src/main.c
+++ samples/shields/x_nucleo_iks02a1/standard/src/main.c
@@ -167,21 +167,21 @@ void main(void)
 	struct sensor_value accel1[3], accel2[3];
 	struct sensor_value gyro[3];
 	struct sensor_value magn[3];
-	const struct device *iis2dlpc = device_get_binding(DT_LABEL(DT_INST(0, st_iis2dlpc)));
-	const struct device *iis2mdc = device_get_binding(DT_LABEL(DT_INST(0, st_iis2mdc)));
-	const struct device *ism330dhcx = device_get_binding(DT_LABEL(DT_INST(0, st_ism330dhcx)));
+	const struct device *iis2dlpc = DEVICE_DT_GET_ONE(st_iis2dlpc);
+	const struct device *iis2mdc = DEVICE_DT_GET_ONE(st_iis2mdc);
+	const struct device *ism330dhcx = DEVICE_DT_GET_ONE(st_ism330dhcx);
 	int cnt = 1;
 
-	if (iis2dlpc == NULL) {
-		printf("Could not get IIS2DLPC device\n");
+	if (!device_is_ready(iis2dlpc)) {
+		printk("%s: device not ready.\n", iis2dlpc->name);
 		return;
 	}
-	if (iis2mdc == NULL) {
-		printf("Could not get IIS2MDC Magn device\n");
+	if (!device_is_ready(iis2mdc)) {
+		printk("%s: device not ready.\n", iis2mdc->name);
 		return;
 	}
-	if (ism330dhcx == NULL) {
-		printf("Could not get ISM330DHCX IMU device\n");
+	if (!device_is_ready(ism330dhcx)) {
+		printk("%s: device not ready.\n", ism330dhcx->name);
 		return;
 	}
 
