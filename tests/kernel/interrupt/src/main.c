17b1c17634e7482eea4780ea1e57556fcc6a97d2
Enjia Mai <enjia.mai@intel.com>
2022-07-06 18:04:49 +0800

    tests: kernel: move the direct interrupt test to arch testing
    Move the direct interrupt test to tests/arch/x86/direct_isr. Two
    reasons:
    1. The direct interrupt is only for x86. It's arch-specific.
    2. And it need extra gcc option to pass the build, that will
    include testsuite number. Although it seems like we add a
    extra testsuite for it, actually we can reduce whole tests
    configuration in tests/kernel/interrupt. And also make this
    test more generic as it used to be.
    
    Signed-off-by: Enjia Mai <enjia.mai@intel.com>

--- tests/kernel/interrupt/src/main.c
+++ tests/kernel/interrupt/src/main.c
@@ -13,7 +13,6 @@ extern void test_isr_regular(void);
 extern void test_isr_offload_job_multiple(void);
 extern void test_isr_offload_job_identi(void);
 extern void test_isr_offload_job(void);
-extern void test_direct_interrupt(void);
 
 void test_main(void)
 {
@@ -24,8 +23,7 @@ void test_main(void)
 			ztest_unit_test(test_isr_regular),
 			ztest_unit_test(test_isr_offload_job_multiple),
 			ztest_unit_test(test_isr_offload_job_identi),
-			ztest_unit_test(test_isr_offload_job),
-			ztest_unit_test(test_direct_interrupt)
+			ztest_unit_test(test_isr_offload_job)
 			);
 	ztest_run_test_suite(interrupt_feature);
 }
