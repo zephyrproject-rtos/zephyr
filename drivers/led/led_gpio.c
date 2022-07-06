40542638fde4b4f5e460d9d927534805dad9d86e
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-07-06 10:57:53 +0200

    drivers: led: gpio: use DT_INST_FOREACH_CHILD_SEP_VARGS
    Avoid auxiliary macros by using DT_INST_FOREACH_CHILD_SEP_VARGS
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- drivers/led/led_gpio.c
+++ drivers/led/led_gpio.c
@@ -83,13 +83,10 @@ static const struct led_driver_api led_gpio_api = {
 	.set_brightness	= led_gpio_set_brightness,
 };
 
-#define LED_GPIO_DT_SPEC(led_node_id)				\
-	GPIO_DT_SPEC_GET(led_node_id, gpios),			\
-
 #define LED_GPIO_DEVICE(i)					\
 								\
-static const struct gpio_dt_spec gpio_dt_spec_##i[] = {	\
-	DT_INST_FOREACH_CHILD(i, LED_GPIO_DT_SPEC)		\
+static const struct gpio_dt_spec gpio_dt_spec_##i[] = {		\
+	DT_INST_FOREACH_CHILD_SEP_VARGS(i, GPIO_DT_SPEC_GET, (,), gpios) \
 };								\
 								\
 static const struct led_gpio_config led_gpio_config_##i = {	\
