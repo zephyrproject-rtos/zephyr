bead14a415fe6637e5b6a937a336fa1ec596e25d
Shaoan Li <shaoanx.li@intel.com>
2022-07-06 09:24:48 +0800

    tests: arch: arm: move test arm_interrupt to new ztest API
    Migrate the testsuite tests/arch/arm/arm_interrupt to the
    new ztest API.
    
    Signed-off-by: Shaoan Li <shaoanx.li@intel.com>

--- tests/arch/arm/arm_interrupt/src/main.c
+++ tests/arch/arm/arm_interrupt/src/main.c
@@ -6,17 +6,4 @@
 
 #include <ztest.h>
 
-extern void test_arm_null_pointer_exception(void);
-extern void test_arm_interrupt(void);
-extern void test_arm_user_interrupt(void);
-extern void test_arm_esf_collection(void);
-
-void test_main(void)
-{
-	ztest_test_suite(arm_interrupt,
-		ztest_unit_test(test_arm_null_pointer_exception),
-		ztest_unit_test(test_arm_interrupt),
-		ztest_unit_test(test_arm_esf_collection),
-		ztest_user_unit_test(test_arm_user_interrupt));
-	ztest_run_test_suite(arm_interrupt);
-}
+ZTEST_SUITE(arm_interrupt, NULL, NULL, NULL, NULL, NULL);
