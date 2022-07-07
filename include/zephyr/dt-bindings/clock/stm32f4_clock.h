d1b80bf01082b02a60b685f15a0d573038ad8033
Francois Ramu <francois.ramu@st.com>
2022-07-07 17:08:22 +0200

    dt-bindings/clock: stm32f4 peripheral clock source
    The STM32_SRC_xxx definitions which are needed
    for some peripherals of the stm32f4 soc.
    Only the minimum clock sources are listed.
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- include/zephyr/dt-bindings/clock/stm32f4_clock.h
+++ include/zephyr/dt-bindings/clock/stm32f4_clock.h
@@ -19,4 +19,21 @@
 #define STM32_PERIPH_BUS_MIN	STM32_CLOCK_BUS_AHB1
 #define STM32_PERIPH_BUS_MAX	STM32_CLOCK_BUS_APB3
 
+/** Peripheral clock sources */
+/* RM0386, 0390, 0402, 0430 § Dedicated Clock configuration register (RCC_DCKCFGRx) */
+
+/** Fixed clocks  */
+#define STM32_SRC_HSI		0x001
+#define STM32_SRC_LSE		0x002
+#define STM32_SRC_LSI		0x003
+/* #define STM32_SRC_HSI48	0x004 */
+/** System clock */
+/* #define STM32_SRC_SYSCLK	0x005 */
+/** Bus clock */
+#define STM32_SRC_PCLK		0x006
+/** PLL clock outputs */
+#define STM32_SRC_PLL_P	0x007
+#define STM32_SRC_PLL_Q	0x008
+#define STM32_SRC_PLL_R	0x009
+
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_STM32F4_CLOCK_H_ */
