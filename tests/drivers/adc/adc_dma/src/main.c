f6c39876600d2cb02b13a6240266e80e7fc0b06d
Hake Huang <hake.huang@oss.nxp.com>
2022-07-08 16:09:23 +0800

    tests: adc: migrate adc to ztest new
    move to ztest_new api for adc tests.
    some naming changes as well
    
    related: #47002
    
    Signed-off-by: Hake Huang <hake.huang@oss.nxp.com>

--- tests/drivers/adc/adc_dma/src/main.c
+++ tests/drivers/adc/adc_dma/src/main.c
@@ -8,17 +8,11 @@
 #include <zephyr/zephyr.h>
 #include <ztest.h>
 
-extern void test_adc_sample_one_channel(void);
-extern void test_adc_sample_two_channels(void);
-extern void test_adc_asynchronous_call(void);
-extern void test_adc_sample_with_interval(void);
-extern void test_adc_repeated_samplings(void);
-extern void test_adc_invalid_request(void);
 extern const struct device *get_adc_device(void);
 extern const struct device *get_count_device(void);
 extern struct k_poll_signal async_sig;
 
-void test_main(void)
+void *adc_dma_setup(void)
 {
 	k_object_access_grant(get_adc_device(), k_current_get());
 	k_object_access_grant(get_count_device(), k_current_get());
@@ -28,12 +22,7 @@ void test_main(void)
 	k_thread_system_pool_assign(k_current_get());
 #endif
 
-	ztest_test_suite(adc_dma_test,
-			 ztest_user_unit_test(test_adc_sample_one_channel),
-			 ztest_user_unit_test(test_adc_sample_two_channels),
-			 ztest_user_unit_test(test_adc_asynchronous_call),
-			 ztest_unit_test(test_adc_sample_with_interval),
-			 ztest_unit_test(test_adc_repeated_samplings),
-			 ztest_user_unit_test(test_adc_invalid_request));
-	ztest_run_test_suite(adc_dma_test);
+	return NULL;
 }
+
+ZTEST_SUITE(adc_dma, NULL, adc_dma_setup, NULL, NULL, NULL);
