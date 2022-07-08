3a32b75d5fca1e5f8d4a505a6f8cf966683de418
Peter Mitsis <peter.mitsis@intel.com>
2022-07-08 11:27:09 -0400

    kernel: pipes: rewrite pipes implementation
    This new implementation of pipes has a number of advantages over the
    previous.
      1. The schedule locking is eliminated both making it safer for SMP
         and allowing for pipes to be used from ISR context.
      2. The code used to be structured to have separate code for copying
         to/from a wating thread's buffer and the pipe buffer. This had
         unnecessary duplication that has been replaced with a simpler
         scatter-gather copy model.
      3. The manner in which the "working list" is generated has also been
         simplified. It no longer tries to use the thread's queuing node.
         Instead, the k_pipe_desc structure (whose instances are on the
         part of the k_thread structure) has been extended to contain
         additional fields including a node for use with a linked list. As
         this impacts the k_thread structure, pipes are now configurable
         in the kernel via CONFIG_PIPES.
    
    Fixes #47061
    
    Signed-off-by: Peter Mitsis <peter.mitsis@intel.com>

--- kernel/userspace.c
+++ kernel/userspace.c
@@ -492,9 +492,11 @@ static void unref_check(struct z_object *ko, uintptr_t index)
 	 * specifically needs to happen depends on the object type.
 	 */
 	switch (ko->type) {
+#ifdef CONFIG_PIPES
 	case K_OBJ_PIPE:
 		k_pipe_cleanup((struct k_pipe *)ko->name);
 		break;
+#endif
 	case K_OBJ_MSGQ:
 		k_msgq_cleanup((struct k_msgq *)ko->name);
 		break;
