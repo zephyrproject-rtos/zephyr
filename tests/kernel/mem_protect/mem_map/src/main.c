87f0b58efe7f889e21cca66b072f5aca7a45f606
Enjia Mai <enjia.mai@intel.com>
2022-07-09 15:48:40 +0800

    tests: kernel: mem_protect: move the mem map test to new ztest API
    Migrate the testsuite tests/kernel/mem_protect/mem_map to the new
    ztest API.
    
    Signed-off-by: Enjia Mai <enjia.mai@intel.com>

--- tests/kernel/mem_protect/mem_map/src/main.c
+++ tests/kernel/mem_protect/mem_map/src/main.c
@@ -47,7 +47,7 @@ void k_sys_fatal_error_handler(unsigned int reason, const z_arch_esf_t *pEsf)
  *
  * @ingroup kernel_memprotect_tests
  */
-void test_z_phys_map_rw(void)
+ZTEST(mem_map, test_z_phys_map_rw)
 {
 	uint8_t *mapped_rw, *mapped_ro;
 	uint8_t *buf = test_page + BUF_OFFSET;
@@ -90,14 +90,16 @@ static void transplanted_function(bool *executed)
 {
 	*executed = true;
 }
+#endif
 
 /**
  * Show that mapping with/without K_MEM_PERM_EXEC works as expected
  *
  * @ingroup kernel_memprotect_tests
  */
-void test_z_phys_map_exec(void)
+ZTEST(mem_map, test_z_phys_map_exec)
 {
+#ifndef SKIP_EXECUTE_TESTS
 	uint8_t *mapped_exec, *mapped_ro;
 	bool executed = false;
 	void (*func)(bool *executed);
@@ -129,20 +131,17 @@ void test_z_phys_map_exec(void)
 
 	printk("shouldn't get here\n");
 	ztest_test_fail();
-}
 #else
-void test_z_phys_map_exec(void)
-{
 	ztest_test_skip();
-}
 #endif /* SKIP_EXECUTE_TESTS */
+}
 
 /**
  * Show that memory mapping doesn't have unintended side effects
  *
  * @ingroup kernel_memprotect_tests
  */
-void test_z_phys_map_side_effect(void)
+ZTEST(mem_map, test_z_phys_map_side_effect)
 {
 	uint8_t *mapped;
 
@@ -171,7 +170,7 @@ void test_z_phys_map_side_effect(void)
  *
  * @ingroup kernel_memprotect_tests
  */
-void test_z_phys_unmap(void)
+ZTEST(mem_map, test_z_phys_unmap)
 {
 	uint8_t *mapped;
 
@@ -199,7 +198,7 @@ void test_z_phys_unmap(void)
  *
  * Does not exercise K_MEM_MAP_* control flags, just default behavior
  */
-void test_k_mem_map_unmap(void)
+ZTEST(mem_map_api, test_k_mem_map_unmap)
 {
 	size_t free_mem, free_mem_after_map, free_mem_after_unmap;
 	char *mapped, *last_mapped;
@@ -262,7 +261,7 @@ void test_k_mem_map_unmap(void)
 /**
  * Test that the "before" guard page is in place for k_mem_map().
  */
-void test_k_mem_map_guard_before(void)
+ZTEST(mem_map_api, test_k_mem_map_guard_before)
 {
 	uint8_t *mapped;
 
@@ -290,7 +289,7 @@ void test_k_mem_map_guard_before(void)
 /**
  * Test that the "after" guard page is in place for k_mem_map().
  */
-void test_k_mem_map_guard_after(void)
+ZTEST(mem_map_api, test_k_mem_map_guard_after)
 {
 	uint8_t *mapped;
 
@@ -315,7 +314,7 @@ void test_k_mem_map_guard_after(void)
 	ztest_test_fail();
 }
 
-void test_k_mem_map_exhaustion(void)
+ZTEST(mem_map_api, test_k_mem_map_exhaustion)
 {
 	/* With demand paging enabled, there is backing store
 	 * which extends available memory. However, we don't
@@ -427,7 +426,7 @@ static void user_function(void *p1, void *p2, void *p3)
  * Test that the allocated region will be only accessible to userspace when
  * K_MEM_PERM_USER is used.
  */
-void test_k_mem_map_user(void)
+ZTEST(mem_map_api, test_k_mem_map_user)
 {
 #ifdef CONFIG_USERSPACE
 	int ret;
@@ -485,7 +484,7 @@ void test_k_mem_map_user(void)
 }
 
 /* ztest main entry*/
-void test_main(void)
+void *mem_map_env_setup(void)
 {
 #ifdef CONFIG_DEMAND_PAGING
 	/* This test sets up multiple mappings of RAM pages, which is only
@@ -493,16 +492,8 @@ void test_main(void)
 	 */
 	k_mem_pin(test_page, sizeof(test_page));
 #endif
-	ztest_test_suite(test_mem_map,
-			ztest_unit_test(test_z_phys_map_rw),
-			ztest_unit_test(test_z_phys_map_exec),
-			ztest_unit_test(test_z_phys_map_side_effect),
-			ztest_unit_test(test_z_phys_unmap),
-			ztest_unit_test(test_k_mem_map_unmap),
-			ztest_unit_test(test_k_mem_map_guard_before),
-			ztest_unit_test(test_k_mem_map_guard_after),
-			ztest_unit_test(test_k_mem_map_exhaustion),
-			ztest_unit_test(test_k_mem_map_user)
-			);
-	ztest_run_test_suite(test_mem_map);
+	return NULL;
 }
+
+ZTEST_SUITE(mem_map, NULL, NULL, NULL, NULL, NULL);
+ZTEST_SUITE(mem_map_api, NULL, mem_map_env_setup, NULL, NULL, NULL);
