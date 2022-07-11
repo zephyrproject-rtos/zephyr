8be0999dfb8fdf9854c3c3e99e342d4419dff471
Piotr Pryga <piotr.pryga@nordicsemi.no>
2022-07-11 12:51:20 +0200

    tests: Bluetooth: df: connectionless cte tx migrate to new ZTEST API
    Migrate tests in connectionless_cte_tx to new ZTEST API.
    
    Remove main.c source file because is unncessary when automatic
    unit tests registration is available. Also former use of
    predicate to select unit tests for execution is removed
    because the tests are split into two distinct tests suites
    that has its't own setup and teardown phases.
    
    Signed-off-by: Piotr Pryga <piotr.pryga@nordicsemi.no>

--- tests/bluetooth/df/connectionless_cte_tx/src/test_set_cl_cte_tx_params.h
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2020 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/* Runs tests for HCI_LE_Set_Connectionless_CTE_Transmit_Parameters command */
-void run_set_cl_cte_tx_params_tests(void);
