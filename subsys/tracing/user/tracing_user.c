111750629b1b57f94850ce8d7087f81f31f4ab7d
Johann Fischer <johann.fischer@nordicsemi.no>
2022-07-13 17:11:05 +0200

    different subsystems: use unsigned int for irq_lock()
    irq_lock() returns an unsigned integer key.
    Generated by spatch using semantic patch
    scripts/coccinelle/irq_lock.cocci
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- subsys/tracing/user/tracing_user.c
+++ subsys/tracing/user/tracing_user.c
@@ -57,7 +57,7 @@ void sys_trace_k_thread_switched_in(void)
 /* FIXME: Limitation of the current x86 EFI cosnole implementation. */
 #if !defined(CONFIG_X86_EFI_CONSOLE) && !defined(CONFIG_UART_CONSOLE)
 
-	int key = irq_lock();
+	unsigned int key = irq_lock();
 
 	__ASSERT_NO_MSG(nested_interrupts[_current_cpu->id] == 0);
 	/* Can't use k_current_get as thread base and z_tls_current might be incorrect */
@@ -70,7 +70,7 @@ void sys_trace_k_thread_switched_in(void)
 void sys_trace_k_thread_switched_out(void)
 {
 #if !defined(CONFIG_X86_EFI_CONSOLE) && !defined(CONFIG_UART_CONSOLE)
-	int key = irq_lock();
+	unsigned int key = irq_lock();
 
 	__ASSERT_NO_MSG(nested_interrupts[_current_cpu->id] == 0);
 	/* Can't use k_current_get as thread base and z_tls_current might be incorrect */
@@ -102,7 +102,7 @@ void sys_trace_thread_pend(struct k_thread *thread)
 
 void sys_trace_isr_enter(void)
 {
-	int key = irq_lock();
+	unsigned int key = irq_lock();
 	_cpu_t *curr_cpu = _current_cpu;
 
 	sys_trace_isr_enter_user(nested_interrupts[curr_cpu->id]);
@@ -113,7 +113,7 @@ void sys_trace_isr_enter(void)
 
 void sys_trace_isr_exit(void)
 {
-	int key = irq_lock();
+	unsigned int key = irq_lock();
 	_cpu_t *curr_cpu = _current_cpu;
 
 	nested_interrupts[curr_cpu->id]--;
