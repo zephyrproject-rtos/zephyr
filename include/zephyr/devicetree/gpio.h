1fc243c73daf882c51ec4892e70f89b706708185
Kumar Gala <galak@kernel.org>
2022-07-13 06:00:53 -0500

    devicetree: gpio: deprecate DT_GPIO_LABEL variants
    Deprecate DT_GPIO_LABEL, DT_INST_GPIO_LABEL, DT_GPIO_LABEL_BY_IDX,
    and DT_INST_GPIO_LABEL_BY_IDX as we phase out 'label' property usage
    in favor of DT_GPIO_CTLR and variants.
    
    Signed-off-by: Kumar Gala <galak@kernel.org>

--- include/zephyr/devicetree/gpio.h
+++ include/zephyr/devicetree/gpio.h
@@ -66,6 +66,9 @@ extern "C" {
 	DT_GPIO_CTLR_BY_IDX(node_id, gpio_pha, 0)
 
 /**
+ * @deprecated If used to obtain a device instance with device_get_binding,
+ * consider using @c DEVICE_DT_GET(DT_GPIO_CTLR_BY_IDX(node, gpio_pha, idx)).
+ *
  * @brief Get a label property from a gpio phandle-array property
  *        at an index
  *
@@ -100,9 +103,12 @@ extern "C" {
  * @see DT_PHANDLE_BY_IDX()
  */
 #define DT_GPIO_LABEL_BY_IDX(node_id, gpio_pha, idx) \
-	DT_PROP(DT_GPIO_CTLR_BY_IDX(node_id, gpio_pha, idx), label)
+	DT_PROP(DT_GPIO_CTLR_BY_IDX(node_id, gpio_pha, idx), label) __DEPRECATED_MACRO
 
 /**
+ * @deprecated If used to obtain a device instance with device_get_binding,
+ * consider using @c DEVICE_DT_GET(DT_GPIO_CTLR(node, gpio_pha)).
+ *
  * @brief Equivalent to DT_GPIO_LABEL_BY_IDX(node_id, gpio_pha, 0)
  * @param node_id node identifier
  * @param gpio_pha lowercase-and-underscores GPIO property with
@@ -111,7 +117,7 @@ extern "C" {
  * @see DT_GPIO_LABEL_BY_IDX()
  */
 #define DT_GPIO_LABEL(node_id, gpio_pha) \
-	DT_GPIO_LABEL_BY_IDX(node_id, gpio_pha, 0)
+	DT_GPIO_LABEL_BY_IDX(node_id, gpio_pha, 0) __DEPRECATED_MACRO
 
 /**
  * @brief Get a GPIO specifier's pin cell at an index
@@ -225,6 +231,9 @@ extern "C" {
 	DT_GPIO_FLAGS_BY_IDX(node_id, gpio_pha, 0)
 
 /**
+ * @deprecated If used to obtain a device instance with device_get_binding,
+ * consider using @c DEVICE_DT_GET(DT_INST_GPIO_CTLR_BY_IDX(node, gpio_pha, idx)).
+ *
  * @brief Get a label property from a DT_DRV_COMPAT instance's GPIO
  *        property at an index
  * @param inst DT_DRV_COMPAT instance number
@@ -234,9 +243,12 @@ extern "C" {
  * @return the label property of the node referenced at index "idx"
  */
 #define DT_INST_GPIO_LABEL_BY_IDX(inst, gpio_pha, idx) \
-	DT_GPIO_LABEL_BY_IDX(DT_DRV_INST(inst), gpio_pha, idx)
+	DT_GPIO_LABEL_BY_IDX(DT_DRV_INST(inst), gpio_pha, idx) __DEPRECATED_MACRO
 
 /**
+ * @deprecated If used to obtain a device instance with device_get_binding,
+ * consider using @c DEVICE_DT_GET(DT_INST_GPIO_CTLR(node, gpio_pha)).
+ *
  * @brief Equivalent to DT_INST_GPIO_LABEL_BY_IDX(inst, gpio_pha, 0)
  * @param inst DT_DRV_COMPAT instance number
  * @param gpio_pha lowercase-and-underscores GPIO property with
@@ -244,7 +256,7 @@ extern "C" {
  * @return the label property of the node referenced at index 0
  */
 #define DT_INST_GPIO_LABEL(inst, gpio_pha) \
-	DT_INST_GPIO_LABEL_BY_IDX(inst, gpio_pha, 0)
+	DT_INST_GPIO_LABEL_BY_IDX(inst, gpio_pha, 0) __DEPRECATED_MACRO
 
 /**
  * @brief Get a DT_DRV_COMPAT instance's GPIO specifier's pin cell value
