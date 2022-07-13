6fdd5aac4cbc961edcae39e361fa9a33a7152e75
Kumar Gala <galak@kernel.org>
2022-07-13 13:26:02 -0500

    drivers: clock_control: beetle: Use devicetree for clock control
    Add simple clock control node in devicetree for beetle to handle
    relationship between drivers (uart, timers, gpio) and clock controller
    device.
    
    Signed-off-by: Kumar Gala <galak@kernel.org>

--- drivers/serial/uart_cmsdk_apb.c
+++ drivers/serial/uart_cmsdk_apb.c
@@ -126,10 +126,12 @@ static int uart_cmsdk_apb_init(const struct device *dev)
 
 #ifdef CONFIG_CLOCK_CONTROL
 	/* Enable clock for subsystem */
-	const struct device *clk =
-		device_get_binding(CONFIG_ARM_CLOCK_CONTROL_DEV_NAME);
-
+	const struct device *clk = DEVICE_DT_GET(DT_INST_CLOCKS_CTLR_BY_IDX(0, 1));
 	struct uart_cmsdk_apb_dev_data * const data = dev->data;
+
+	if (!device_is_ready(clk)) {
+		return -ENODEV;
+	}
 
 #ifdef CONFIG_SOC_SERIES_BEETLE
 	clock_control_on(clk, (clock_control_subsys_t *) &data->uart_cc_as);
