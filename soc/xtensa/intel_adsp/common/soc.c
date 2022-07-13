c759806076df6d97122707f884b3a7d426543126
Anas Nashif <anas.nashif@intel.com>
2022-07-13 17:51:21 -0400

    intel_adsp: common: build soc.c only for CAVS
    build soc.c only for platforms that can use it.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- soc/xtensa/intel_adsp/common/soc.c
+++ soc/xtensa/intel_adsp/common/soc.c
@@ -51,7 +51,6 @@ LOG_MODULE_REGISTER(soc);
 extern void soc_mp_init(void);
 #endif
 
-#ifdef CONFIG_INTEL_ADSP_CAVS
 static __imr void power_init_v15(void)
 {
 	/* HP domain clocked by PLL
@@ -137,4 +136,3 @@ static __imr int soc_init(const struct device *dev)
 }
 
 SYS_INIT(soc_init, PRE_KERNEL_1, 99);
-#endif /* CONFIG_INTEL_ADSP_CAVS */
