751037aa18d7c8c2073583e3899ab00a83414442
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2022-07-14 16:14:20 +0200

    Bluetooth: samples: Add HAP Hearing Aid sample application
    This adds sample application that can be used as reference
    implementation and for HAP qualification purposes.
    
    Signed-off-by: Mariusz Skamra <mariusz.skamra@codecoup.pl>

---
+++ samples/bluetooth/hap_ha/src/csip_set_member.c
@@ -0,0 +1,48 @@
+/** @file
+ *  @brief Bluetooth Coordinated Set Identifier Profile (CSIP) Set Member role.
+ *
+ *  Copyright (c) 2022 Codecoup
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/zephyr.h>
+#include <zephyr/sys/printk.h>
+
+#include <zephyr/bluetooth/audio/cap.h>
+#include <zephyr/bluetooth/audio/csis.h>
+
+#define CSIS_SIRK_DEBUG	{ 0xcd, 0xcc, 0x72, 0xdd, 0x86, 0x8c, 0xcd, 0xce, \
+			  0x22, 0xfd, 0xa1, 0x21, 0x09, 0x7d, 0x7d, 0x45 }
+
+static struct bt_csis *csis;
+
+static void csis_lock_changed_cb(struct bt_conn *conn, struct bt_csis *csis, bool locked)
+{
+	printk("Client %p %s the lock\n", conn, locked ? "locked" : "released");
+}
+
+static uint8_t sirk_read_req_cb(struct bt_conn *conn, struct bt_csis *csis)
+{
+	return BT_CSIS_READ_SIRK_REQ_RSP_ACCEPT;
+}
+
+static struct bt_csis_cb csis_cb = {
+	.lock_changed = csis_lock_changed_cb,
+	.sirk_read_req = sirk_read_req_cb,
+};
+
+int csip_set_member_init(void (*rsi_changed)(const uint8_t *rsi))
+{
+	struct bt_csis_register_param param = {
+		.set_size = 2,
+		.rank = CONFIG_HAP_HA_SET_RANK,
+		.lockable = false,
+		.set_sirk = CSIS_SIRK_DEBUG,
+		.cb = &csis_cb,
+	};
+
+	csis_cb.rsi_changed = rsi_changed;
+
+	return bt_cap_acceptor_register(&param, &csis);
+}
