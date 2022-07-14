82f74794f6dcbb831eef50513f4357da4b0af18d
li biao <biao1x.li@intel.com>
2022-07-14 16:56:58 +0800

    tests: lib: move sys_util to new ztest API
    Move test lib/sys_util to use new ztest API.
    
    Signed-off-by: li biao <biao1x.li@intel.com>

--- tests/lib/sys_util/src/main.c
+++ tests/lib/sys_util/src/main.c
@@ -3,20 +3,35 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
+#include <zephyr/sys/util.h>
+#include <zephyr/kernel.h>
 #include <ztest.h>
-
-extern void test_wait_for(void);
-
 /**
- * @defgroup sys_util_tests Sys Util Tests
- * @ingroup all_tests
+ * @addtogroup sys_util_tests
  * @{
- * @}
  */
 
-void test_main(void)
+/**
+ * @brief Test wait_for works as expected with typical use cases
+ *
+ * @see WAIT_FOR()
+ */
+ZTEST(sys_util, test_wait_for)
 {
-	ztest_test_suite(sys_util, ztest_unit_test(test_wait_for));
-	ztest_run_test_suite(sys_util);
+	uint32_t start, end, expected;
+
+	zassert_true(WAIT_FOR(true, 0, NULL), "true, no wait, NULL");
+	zassert_true(WAIT_FOR(true, 0, k_yield()), "true, no wait, yield");
+	zassert_false(WAIT_FOR(false, 0, k_yield()), "false, no wait, yield");
+	zassert_true(WAIT_FOR(true, 1, k_yield()), "true, 1usec, yield");
+	zassert_false(WAIT_FOR(false, 1, k_yield()), "false, 1usec, yield");
+	zassert_true(WAIT_FOR(true, 1000, k_yield()), "true, 1msec, yield");
+
+
+	expected = 1000*(sys_clock_hw_cycles_per_sec()/USEC_PER_SEC);
+	start = k_cycle_get_32();
+	zassert_false(WAIT_FOR(false, 1000, k_yield()), "true, 1msec, yield");
+	end = k_cycle_get_32();
+	zassert_true(end-start >= expected, "wait for 1ms");
 }
+ZTEST_SUITE(sys_util, NULL, NULL, NULL, NULL, NULL);
