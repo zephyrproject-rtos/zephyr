b0d73e54bac24ced4e7b86085aad6f84ad9c4fea
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-07-18 11:06:49 +0200

    tests: subsys: openthread: refactor test
    Use a test compatible so that test can create a DT-based device and
    provide a valid choice when building the test.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- tests/subsys/openthread/radio_stub.c
+++ tests/subsys/openthread/radio_stub.c
@@ -16,18 +16,10 @@
 /* OpenThread not enabled here */
 #define CONFIG_OPENTHREAD_L2_LOG_LEVEL LOG_LEVEL_DBG
 
-#ifndef CONFIG_NET_CONFIG_IEEE802154_DEV_NAME
-	#define CONFIG_NET_CONFIG_IEEE802154_DEV_NAME ""
-#endif /* CONFIG_NET_CONFIG_IEEE802154_DEV_NAME */
-
 #define CONFIG_OPENTHREAD_THREAD_PRIORITY 5
 #define OT_WORKER_PRIORITY K_PRIO_COOP(CONFIG_OPENTHREAD_THREAD_PRIORITY)
 #define CONFIG_NET_L2_OPENTHREAD 1
 #define CONFIG_OPENTHREAD_RADIO_WORKQUEUE_STACK_SIZE 512
-
-/* needed for stubbing device driver */
-const struct device *device_get_binding_stub(const char *name);
-#define device_get_binding device_get_binding_stub
 
 /* file itself */
 #include "radio.c"
