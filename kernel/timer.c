039219709820e70daf66a868bda06449dfcc4c25
Simon Hein <SHein@baumer.com>
2022-07-19 22:30:17 +0200

    kernel: comply to coding guidelines MISRA C:2012 Rule 14.4
    MISRA C:2012 Rule 14.4 (The controlling expression of an if statement
    and the controlling expression of an iteration-statement shall have
    essentially Boolean type.)
    
    Use `bool' instead of `int' to represent Boolean values.
    Use `do { ... } while (false)' instead of `do { ... } while (0)'.
    Use comparisons with zero instead of implicitly testing integers.
    
    This commit is a subset of the original commit:
    5d02614e34a86b549c7707d3d9f0984bc3a5f22a
    
    Signed-off-by: Simon Hein <SHein@baumer.com>

--- kernel/timer.c
+++ kernel/timer.c
@@ -161,7 +161,7 @@ void z_impl_k_timer_stop(struct k_timer *timer)
 {
 	SYS_PORT_TRACING_OBJ_FUNC(k_timer, stop, timer);
 
-	int inactive = z_abort_timeout(&timer->timeout) != 0;
+	bool inactive = (z_abort_timeout(&timer->timeout) != 0);
 
 	if (inactive) {
 		return;
