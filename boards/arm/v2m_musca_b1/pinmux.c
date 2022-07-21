d4ddbfd521fd94525699c24939ae44801bb02cf1
Kumar Gala <galak@kernel.org>
2022-07-21 05:44:53 -0500

    drivers: serial: pl011: Remove unused Kconfig symbols
    The driver has be DT_INST based for a while so the Kconfig
    symbols CONFIG_UART_PL011_PORT0<n> aren't used.  So lets
    remove them.
    
    Signed-off-by: Kumar Gala <galak@kernel.org>

--- boards/arm/v2m_musca_b1/pinmux.c
+++ boards/arm/v2m_musca_b1/pinmux.c
@@ -41,7 +41,7 @@ static void arm_musca_b1_pinmux_defaults(void)
 	scc[IOMUX_ALTF1_OUTSEL] = 0xffff;
 	scc[IOMUX_ALTF1_OENSEL] = 0xffff;
 
-#if defined(CONFIG_UART_PL011_PORT0)
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(uart0), okay)
 	/* clear bit 0/1 for GPIO0/1 to steer from ALTF1 */
 	scc[IOMUX_MAIN_INSEL] &= ~(BIT(0) | BIT(1));
 	scc[IOMUX_MAIN_OUTSEL] &= ~(BIT(0) | BIT(1));
