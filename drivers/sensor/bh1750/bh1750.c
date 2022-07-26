eb5492720ae4038ff0b2d54783fc56d2dcdc4f58
Lucas Heitzmann Gabrielli <heitzmann@gmail.com>
2022-07-26 10:49:42 -0300

    drivers: sensor: bh1759: Improve src documentation
    Include review sugestions and code compliance fixes.
    
    Signed-off-by: Lucas Heitzmann Gabrielli <heitzmann@gmail.com>

--- drivers/sensor/bh1750/bh1750.c
+++ drivers/sensor/bh1750/bh1750.c
@@ -7,55 +7,58 @@
 
 #define DT_DRV_COMPAT rohm_bh1750
 
-#include <logging/log.h>
+#include "bh1750.h"
+
 #include <drivers/i2c.h>
 #include <drivers/sensor.h>
-
-#include "bh1750.h"
+#include <logging/log.h>
 
 LOG_MODULE_REGISTER(BH1750, CONFIG_SENSOR_LOG_LEVEL);
 
 static int bh1750_read(const struct device *dev, uint8_t *byte, uint32_t count)
 {
 	const struct bh1750_config *cfg = dev->config;
+
 	return i2c_read(cfg->bus, byte, count, cfg->bus_addr);
 }
 
 static int bh1750_write(const struct device *dev, uint8_t byte)
 {
 	const struct bh1750_config *cfg = dev->config;
+
 	return i2c_write(cfg->bus, &byte, 1, cfg->bus_addr);
 }
 
 static int bh1750_attr_set(const struct device *dev, enum sensor_channel chan,
 			   enum sensor_attribute attr, const struct sensor_value *val)
 {
-	int ret;
-	uint8_t byte;
-
 	__ASSERT_NO_MSG(chan == SENSOR_CHAN_LIGHT || chan == SENSOR_CHAN_ALL);
 	__ASSERT_NO_MSG(attr == SENSOR_ATTR_OVERSAMPLING);
 
-	if (val->val1 < 31 || val->val1 > 254) {
-		LOG_ERR("Oversampling factor out of range [31, 254]: %d", val->val1);
+	int ret;
+	uint8_t byte;
+	struct bh1750_data *data = dev->data;
+
+	if (val->val1 < BH1750_MEAS_TIME_MIN || val->val1 > BH1750_MEAS_TIME_MAX) {
+		LOG_ERR("Oversampling factor out of range [%d, %d]: %d", BH1750_MEAS_TIME_MIN,
+			BH1750_MEAS_TIME_MAX, val->val1);
 		return -EINVAL;
 	}
 
 	byte = BH1750_MEAS_TIME_MSB | (((uint8_t)val->val1 >> 5) & 0x07);
 	ret = bh1750_write(dev, byte);
 	if (ret < 0) {
-		LOG_ERR("Failed to configure high part of oversampling factor.");
+		LOG_ERR("Failed to configure %s part of oversampling factor.", "high");
 		return ret;
 	}
 
 	byte = BH1750_MEAS_TIME_LSB | ((uint8_t)val->val1 & 0x1F);
 	ret = bh1750_write(dev, byte);
 	if (ret < 0) {
-		LOG_ERR("Failed to configure low part of oversampling factor.");
+		LOG_ERR("Failed to configure %s part of oversampling factor.", "low");
 		return ret;
 	}
 
-	struct bh1750_data *data = dev->data;
 	data->oversampling_factor = (uint8_t)val->val1;
 
 	return 0;
@@ -68,6 +71,7 @@ static int bh1750_attr_get(const struct device *dev, enum sensor_channel chan,
 	__ASSERT_NO_MSG(attr == SENSOR_ATTR_OVERSAMPLING);
 
 	struct bh1750_data *data = dev->data;
+
 	val->val1 = (int32_t)data->oversampling_factor;
 
 	return 0;
@@ -75,10 +79,12 @@ static int bh1750_attr_get(const struct device *dev, enum sensor_channel chan,
 
 static int bh1750_sample_fetch(const struct device *dev, enum sensor_channel chan)
 {
-	struct bh1750_data *data = dev->data;
-	int ret;
-
 	__ASSERT_NO_MSG(chan == SENSOR_CHAN_LIGHT);
+
+	int ret;
+	uint8_t buffer[2];
+	struct bh1750_data *data = dev->data;
+
 
 	ret = bh1750_write(dev, BH1750_SINGLE_MEASUREMENT);
 	if (ret < 0) {
@@ -88,13 +94,12 @@ static int bh1750_sample_fetch(const struct device *dev, enum sensor_channel cha
 
 	k_busy_wait(BH1750_TIME_FACTOR * (uint32_t)data->oversampling_factor);
 
-	uint8_t buffer[2];
 	ret = bh1750_read(dev, buffer, 2);
 	if (ret < 0) {
 		LOG_ERR("Unable to issue read command.");
 		return ret;
 	}
-	data->adc_count = ((uint16_t)buffer[0] << 8) | (uint16_t)buffer[1];
+	data->adc_count = sys_get_be16(buffer);
 
 	return 0;
 }
@@ -103,14 +108,40 @@ static int bh1750_channel_get(const struct device *dev, enum sensor_channel chan
 			      struct sensor_value *val)
 {
 	__ASSERT_NO_MSG(chan == SENSOR_CHAN_LIGHT);
+
+	/*
+	 * The conversion between the ADC count and the sensor value (in lux) is
+	 *
+	 * - High res:        value = count * 115 / (4 * oversampling_factor)
+	 * - Normal/low res:  value = count * 115 / (2 * oversampling_factor)
+	 *
+	 * To preserve as many significant digits as possible in the division,
+	 * we can first multiply by 115 and shift up to 9 bits (without risking
+	 * an overflow, since adc_count is a 16 bit value.  Thus:
+	 *
+	 * - High res:        value = (tmp >> 9) / 4 = tmp >> 11
+	 * - Normal/low res:  value = (tmp >> 9) / 2 = tmp >> 10
+	 *
+	 * Finally, the integer part (val->val1) can be straightforwardly
+	 * calculated with bit shifts.  The fractional part comes from the
+	 * shifted bits (11 or 10, depending on the resolution) downshifted and
+	 * multiplied by 10^6:
+	 *
+	 * val2 = ((tmp & 0x07FF) >> 11) * 1000000
+	 *      = (((tmp & 0x07FF) >> 11) << 6) * 15625
+	 *      = ((tmp & 0x07FF) * 15625) >> 5
+	 *
+	 * and similarly for norma/low resolution.
+	 *
+	 */
+
 	struct bh1750_data *data = dev->data;
-	uint32_t tmp = (uint32_t)data->adc_count * (230 << 8) / data->oversampling_factor;
+	uint32_t tmp = (uint32_t)data->adc_count * (115 << 9) / data->oversampling_factor;
+
 #if defined(CONFIG_BH1750_HIGH_RES)
-	/* tmp = lux * 2^11 */
 	val->val1 = (int32_t)(tmp >> 11);
 	val->val2 = (int32_t)((tmp & 0x07FF) * 15625) >> 5;
 #else
-	/* tmp = lux * 2^10 */
 	val->val1 = (int32_t)(tmp >> 10);
 	val->val2 = (int32_t)((tmp & 0x03FF) * 15625) >> 4;
 #endif
@@ -128,13 +159,13 @@ static int bh1750_init(const struct device *dev)
 {
 	struct bh1750_data *data = dev->data;
 	const struct bh1750_config *cfg = dev->config;
+	struct sensor_value tmp = {.val1 = data->oversampling_factor, .val2 = 0};
 
 	if (!device_is_ready(cfg->bus)) {
 		LOG_ERR("Bus device is not ready");
 		return -ENODEV;
 	}
 
-	struct sensor_value tmp = { .val1 = data->oversampling_factor, .val2 = 0 };
 	return bh1750_attr_set(dev, SENSOR_CHAN_ALL, SENSOR_ATTR_OVERSAMPLING, &tmp);
 }
 
