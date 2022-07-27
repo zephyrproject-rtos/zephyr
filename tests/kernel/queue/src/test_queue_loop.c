e73d7e0843f457ff9b166b52a33013daeeff8794
NingX Zhao <ningx.zhao@intel.com>
2022-07-27 14:44:20 +0800

    kernel: queue: move queue testcase to new ztest
    Move queue testcases to new ztest.
    
    Signed-off-by: NingX Zhao <ningx.zhao@intel.com>

--- tests/kernel/queue/src/test_queue_loop.c
+++ tests/kernel/queue/src/test_queue_loop.c
@@ -107,7 +107,7 @@ static void tqueue_read_write(struct k_queue *pqueue)
  * @see k_queue_append(), k_queue_get(),
  * k_queue_init(), k_queue_remove()
  */
-void test_queue_loop(void)
+ZTEST(queue_api_1cpu, test_queue_loop)
 {
 	k_queue_init(&queue);
 	for (int i = 0; i < LOOPS; i++) {
