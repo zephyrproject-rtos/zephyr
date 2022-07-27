9fc87639f0cb9c61ba7c9dd6e029382fbc0f8406
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-07-27 23:05:50 +0200

    soc: riscv: remove unused RISCV_RAM_BASE|SIZE definitions
    The definitions are not used anywhere in Zephyr nor in modules, so
    delete them.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- soc/riscv/riscv-privilege/andes_v5/ae350/soc.h
+++ soc/riscv/riscv-privilege/andes_v5/ae350/soc.h
@@ -17,10 +17,6 @@
 #define RISCV_MTIME_BASE             0xE6000000
 #define RISCV_MTIMECMP_BASE          0xE6000008
 
-/* lib-c hooks required RAM defined variables */
-#define RISCV_RAM_BASE               CONFIG_SRAM_BASE_ADDRESS
-#define RISCV_RAM_SIZE               KB(CONFIG_SRAM_SIZE)
-
 /* Include CSRs available for Andes V5 SoCs */
 #include "soc_v5.h"
 
