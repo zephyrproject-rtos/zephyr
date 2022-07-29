27d83d67d88fde38672410a27ca2c0d19bca243b
Anas Nashif <anas.nashif@intel.com>
2022-07-29 18:59:31 -0400

    intel_adsp: acev1x: move sram init code into own file
    Just to be consistent with other ADSP series.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- soc/xtensa/intel_adsp/ace_v1x/soc.c
+++ soc/xtensa/intel_adsp/ace_v1x/soc.c
@@ -30,34 +30,7 @@ extern void parse_manifest(void);
 #define HOST_PAGE_SIZE 4096
 
 #define MANIFEST_SEGMENT_COUNT 3
-#define DELAY_COUNT	       256
 
-__imr void hp_sram_init(void)
-{
-	uint32_t hpsram_ebb_quantity = mtl_hpsram_get_bank_count();
-	volatile uint32_t *l2hsbpmptr = (volatile uint32_t *)MTL_L2MM->l2hsbpmptr;
-	volatile uint8_t *status = (volatile uint8_t *)l2hsbpmptr + 4;
-	int idx;
-
-	for (idx = 0; idx < hpsram_ebb_quantity; ++idx) {
-		*(l2hsbpmptr + idx * 2) = 0;
-	}
-	for (idx = 0; idx < hpsram_ebb_quantity; ++idx) {
-		while (*(status + idx * 8) != 0) {
-			z_idelay(DELAY_COUNT);
-		}
-	}
-}
-
-__imr void lp_sram_init(void)
-{
-	uint32_t lpsram_ebb_quantity = mtl_lpsram_get_bank_count();
-	volatile uint32_t *l2usbpmptr = (volatile uint32_t *)MTL_L2MM->l2usbpmptr;
-
-	for (uint32_t idx = 0; idx < lpsram_ebb_quantity; ++idx) {
-		*(l2usbpmptr + idx * 2) = 0;
-	}
-}
 
 __imr void boot_core0(void)
 {
