663acea61122e736249e2d300944aa4ab8861636
Jan Kuliga <jtkuliga@gmail.com>
2022-07-31 02:33:12 +0200

    bme680: Rework setting the spi_mem_page accordingly to the datasheet
    According to the BME680 datasheet, only the spi_mem_page bit can be
    changed in its 8-bit register. Therefore we shouldn't just write 0s
    to the remaining bits, but update the register content in read-modify-write
    fashion instead.
    Rework the bme680_set_mem_page(), bme680_init() and register-access
    macros.
    
    Signed-off-by: Jan Kuliga <jtkuliga@gmail.com>

--- drivers/sensor/bme680/bme680_spi.c
+++ drivers/sensor/bme680/bme680_spi.c
@@ -27,30 +27,52 @@ static inline int bme680_set_mem_page(const struct device *dev, uint8_t addr)
 {
 	const struct bme680_config *config = dev->config;
 	struct bme680_data *data = dev->data;
-	uint8_t page;
+	uint8_t page = (addr > 0x7f) ? 0U : 1U;
 	int err = 0;
 
-	if (addr > 0x7f) {
-		page = BME680_MEM_PAGE1;
-	} else {
-		page = BME680_MEM_PAGE0;
-	}
-
 	if (data->mem_page != page) {
+		uint8_t buf[2];
 
-		data->mem_page = page;
-		uint8_t cmd[] = { BME680_REG_MEM_PAGE & BME680_SPI_WRITE_MSK, data->mem_page };
-		const struct spi_buf tx_buf = {
-			.buf = cmd,
-			.len = sizeof(cmd)
+		struct spi_buf tx_buf = {
+			.buf = &buf[0],
+			.len = 1,
 		};
 		const struct spi_buf_set tx = {
 			.buffers = &tx_buf,
-			.count = 1
+			.count = 1,
 		};
 
+		const struct spi_buf rx_buf[] = {
+			{ .buf = NULL, .len = 1 },
+			{ .buf = &buf[1], .len = 1 },
+		};
+		const struct spi_buf_set rx = {
+			.buffers = rx_buf,
+			.count = ARRAY_SIZE(rx_buf),
+		};
+
+		buf[0] = BME680_REG_STATUS | BME680_SPI_READ_BIT;
+		err = spi_transceive_dt(&config->bus.spi, &tx, &rx);
+		if (err < 0) {
+			return err;
+		}
+
+		if (data->mem_page == 1U) {
+			buf[1] &= ~BME680_SPI_MEM_PAGE_MSK;
+		} else {
+			buf[1] |= BME680_SPI_MEM_PAGE_MSK;
+		}
+
+		buf[0] = BME680_REG_STATUS & BME680_SPI_WRITE_MSK;
+		tx_buf.len = 2;
 		err = spi_write_dt(&config->bus.spi, &tx);
+		if (err < 0) {
+			return err;
+		}
+
+		data->mem_page = page;
 	}
+
 	return err;
 }
 
