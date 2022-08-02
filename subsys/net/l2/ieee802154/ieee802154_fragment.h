fec74af5d4db9e7295e0a141a0fda98fc0d099a6
Florian Grandel <jerico.dev@gmail.com>
2022-08-02 20:03:35 +0200

    net: l2: ieee802154: improve inline comments
    This change contains some merely editorial changes to inline comments
    plus updates references from the IEEE 802.15.4-2003 spec to
    IEEE 802.15.4-2006 which corresponds to the implementation level of
    the module.
    
    Signed-off-by: Florian Grandel <jerico.dev@gmail.com>

--- subsys/net/l2/ieee802154/ieee802154_fragment.h
+++ subsys/net/l2/ieee802154/ieee802154_fragment.h
@@ -56,9 +56,9 @@ void ieee802154_fragment_ctx_init(struct ieee802154_fragment_ctx *ctx,
  *  needs to be fragmented. Every fragment will have fragmentation header
  *  data size, data offset, data tag and payload.
  *
- *  @param Pointer to valid fragmentation context
- *  @param Pointer to valid buffer where to write the fragment
- *  @param bool true for IPHC compression, false for IPv6 dispatch header
+ *  @param ctx Pointer to valid fragmentation context
+ *  @param frame_buf Pointer to valid buffer where to write the fragment
+ *  @param ipch bool true for IPHC compression, false for IPv6 dispatch header
  *
  *  @return True in case of success, false otherwise
  */
@@ -77,7 +77,7 @@ void ieee802154_fragment(struct ieee802154_fragment_ctx *ctx,
  *  per data tag, data offset and data size. First packet is uncompressed
  *  immediately after reception.
  *
- *  @param Pointer to network fragment, which gets updated to full reassembled
+ *  @param pkt Pointer to network fragment, which gets updated to full reassembled
  *         packet when verdict is NET_CONTINUE
  *
  *  @return NET_CONTINUE reassembly done, pkt is complete
