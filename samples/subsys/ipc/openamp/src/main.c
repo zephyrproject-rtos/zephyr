fd082435f209e28ab76d8e5f8a51a76841192177
Kumar Gala <galak@kernel.org>
2022-08-03 13:50:49 -0500

    samples: openamp: Convert to use DEVICE_DT_GET
    Update sample to use DEVICE_DT_GET to remove usage of
    device_get_binding.
    
    Signed-off-by: Kumar Gala <galak@kernel.org>

--- samples/subsys/ipc/openamp/src/main.c
+++ samples/subsys/ipc/openamp/src/main.c
@@ -200,9 +200,9 @@ void app_task(void *arg1, void *arg2, void *arg3)
 	}
 
 	/* setup IPM */
-	ipm_handle = device_get_binding(CONFIG_OPENAMP_IPC_DEV_NAME);
-	if (ipm_handle == NULL) {
-		printk("device_get_binding failed to find device\n");
+	ipm_handle = DEVICE_DT_GET(DT_CHOSEN(zephyr_ipc));
+	if (!device_is_ready(ipm_handle)) {
+		printk("IPM device is not ready\n");
 		return;
 	}
 
