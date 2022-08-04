278e23b4b23c34a1bbabc816b28a2a2b56e92bd1
Meng xianglin <xianglinx.meng@intel.com>
2022-08-04 09:15:59 +0800

    tests: msgq_api: move to new ztest API
    test cases in tests/kernel/msgq/msgq_api/ are move to new ztest API
    
    Signed-off-by: Meng xianglin <xianglinx.meng@intel.com>

--- tests/kernel/msgq/msgq_api/src/main.c
+++ tests/kernel/msgq/msgq_api/src/main.c
@@ -15,39 +15,6 @@
 
 #include "test_msgq.h"
 
-extern void test_msgq_thread(void);
-extern void test_msgq_thread_overflow(void);
-extern void test_msgq_isr(void);
-extern void test_msgq_put_fail(void);
-extern void test_msgq_get_fail(void);
-extern void test_msgq_purge_when_put(void);
-extern void test_msgq_attrs_get(void);
-extern void test_msgq_alloc(void);
-extern void test_msgq_pend_thread(void);
-extern void test_msgq_empty(void);
-extern void test_msgq_full(void);
-#ifdef CONFIG_USERSPACE
-extern void test_msgq_user_thread(void);
-extern void test_msgq_user_thread_overflow(void);
-extern void test_msgq_user_put_fail(void);
-extern void test_msgq_user_get_fail(void);
-extern void test_msgq_user_attrs_get(void);
-extern void test_msgq_user_purge_when_put(void);
-#else
-#define dummy_test(_name) \
-	static void _name(void) \
-	{ \
-		ztest_test_skip(); \
-	}
-
-dummy_test(test_msgq_user_thread);
-dummy_test(test_msgq_user_thread_overflow);
-dummy_test(test_msgq_user_put_fail);
-dummy_test(test_msgq_user_get_fail);
-dummy_test(test_msgq_user_attrs_get);
-dummy_test(test_msgq_user_purge_when_put);
-#endif /* CONFIG_USERSPACE */
-
 #ifdef CONFIG_64BIT
 #define MAX_SZ	256
 #else
@@ -62,31 +29,13 @@ extern struct k_sem end_sema;
 extern struct k_thread tdata;
 K_THREAD_STACK_DECLARE(tstack, STACK_SIZE);
 
-/*test case main entry*/
-void test_main(void)
+void *msgq_api_setup(void)
 {
 	k_thread_access_grant(k_current_get(), &kmsgq, &msgq, &end_sema,
 			      &tdata, &tstack);
-
 	k_thread_heap_assign(k_current_get(), &test_pool);
-
-	ztest_test_suite(msgq_api,
-			 ztest_1cpu_unit_test(test_msgq_thread),
-			 ztest_unit_test(test_msgq_thread_overflow),
-			 ztest_user_unit_test(test_msgq_user_thread),
-			 ztest_user_unit_test(test_msgq_user_thread_overflow),
-			 ztest_unit_test(test_msgq_isr),
-			 ztest_1cpu_unit_test(test_msgq_put_fail),
-			 ztest_1cpu_unit_test(test_msgq_get_fail),
-			 ztest_user_unit_test(test_msgq_user_put_fail),
-			 ztest_user_unit_test(test_msgq_user_get_fail),
-			 ztest_unit_test(test_msgq_attrs_get),
-			 ztest_user_unit_test(test_msgq_user_attrs_get),
-			 ztest_1cpu_unit_test(test_msgq_purge_when_put),
-			 ztest_user_unit_test(test_msgq_user_purge_when_put),
-			 ztest_1cpu_unit_test(test_msgq_pend_thread),
-			 ztest_1cpu_unit_test(test_msgq_empty),
-			 ztest_1cpu_unit_test(test_msgq_full),
-			 ztest_unit_test(test_msgq_alloc));
-	ztest_run_test_suite(msgq_api);
+	return NULL;
 }
+ZTEST_SUITE(msgq_api, NULL, msgq_api_setup, NULL, NULL, NULL);
+ZTEST_SUITE(msgq_api_1cpu, NULL, msgq_api_setup,
+	    ztest_simple_1cpu_before, ztest_simple_1cpu_after, NULL);
