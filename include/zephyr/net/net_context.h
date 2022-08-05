e608e92112d3396a4483d07f1ad6bfb6c4f891cb
Florian Grandel <jerico.dev@gmail.com>
2022-08-05 18:35:59 +0200

    net: context: properly namespace can-related methods
    The net_context_set/get_filter_id() methods are CAN specific and should
    say so.
    
    Signed-off-by: Florian Grandel <jerico.dev@gmail.com>

--- include/zephyr/net/net_context.h
+++ include/zephyr/net/net_context.h
@@ -553,7 +553,7 @@ static inline void net_context_set_type(struct net_context *context,
  * @param filter_id CAN filter id
  */
 #if defined(CONFIG_NET_SOCKETS_CAN)
-static inline void net_context_set_filter_id(struct net_context *context,
+static inline void net_context_set_can_filter_id(struct net_context *context,
 					     int filter_id)
 {
 	NET_ASSERT(context);
@@ -561,7 +561,7 @@ static inline void net_context_set_filter_id(struct net_context *context,
 	context->can_filter_id = filter_id;
 }
 #else
-static inline void net_context_set_filter_id(struct net_context *context,
+static inline void net_context_set_can_filter_id(struct net_context *context,
 					     int filter_id)
 {
 	ARG_UNUSED(context);
@@ -579,14 +579,14 @@ static inline void net_context_set_filter_id(struct net_context *context,
  * @return Filter id of this network context
  */
 #if defined(CONFIG_NET_SOCKETS_CAN)
-static inline int net_context_get_filter_id(struct net_context *context)
+static inline int net_context_get_can_filter_id(struct net_context *context)
 {
 	NET_ASSERT(context);
 
 	return context->can_filter_id;
 }
 #else
-static inline int net_context_get_filter_id(struct net_context *context)
+static inline int net_context_get_can_filter_id(struct net_context *context)
 {
 	ARG_UNUSED(context);
 
