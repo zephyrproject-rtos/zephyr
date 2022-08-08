a2eb3284f0965cbe542e92270b3f5302146df29d
Henrik Brix Andersen <hebad@vestas.com>
2022-08-08 13:34:50 +0200

    drivers: ethernet: check if clock device is ready before accessing
    Add check for device_is_ready() before accessing clock control devices.
    
    Signed-off-by: Henrik Brix Andersen <hebad@vestas.com>

--- drivers/ethernet/eth_dwmac_stm32h7x.c
+++ drivers/ethernet/eth_dwmac_stm32h7x.c
@@ -50,6 +50,12 @@ int dwmac_bus_init(struct dwmac_priv *p)
 	int ret;
 
 	p->clock = DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE);
+
+	if (!device_is_ready(p->clock)) {
+		LOG_ERR("clock control device not ready");
+		return -ENODEV;
+	}
+
 	ret  = clock_control_on(p->clock, (clock_control_subsys_t *)&pclken);
 	ret |= clock_control_on(p->clock, (clock_control_subsys_t *)&pclken_tx);
 	ret |= clock_control_on(p->clock, (clock_control_subsys_t *)&pclken_rx);
