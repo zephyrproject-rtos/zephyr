429272275c70e1694c010afda603a6e2a33ee0f4
Lauren Murphy <lauren.murphy@intel.com>
2022-08-10 14:39:08 -0700

    boards, dts: fix namespace for intel adsp cavs, ace
    Fixes namespace for Intel ADSP CAVS and ACE boards.
    
    Signed-off-by: Lauren Murphy <lauren.murphy@intel.com>

--- include/zephyr/drivers/clock_control/clock_control_cavs.h
+++ include/zephyr/drivers/clock_control/clock_control_cavs.h
@@ -3,9 +3,9 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#ifndef CLOCK_CONTROL_CAVS_H_
-#define CLOCK_CONTROL_CAVS_H_
+#ifndef CLOCK_CONTROL_ADSP_H_
+#define CLOCK_CONTROL_ADSP_H_
 
 #include <cavs-clk.h>
 
-#endif /* CLOCK_CONTROL_CAVS_H_ */
+#endif /* CLOCK_CONTROL_ADSP_H_ */
