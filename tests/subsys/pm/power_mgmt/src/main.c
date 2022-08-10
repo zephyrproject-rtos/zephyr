d09ac568c771ee3e8428ee34dbc685ffd8a1300e
Yinfang Wang <yinfang.wang@intel.com>
2022-08-10 22:27:43 +0800

    tests: subsys: pm: move power_mgmt to new ztest API
    move power_mgmt to new ztest API.
    
    Signed-off-by: Yinfang Wang <yinfang.wang@intel.com>

--- tests/subsys/pm/power_mgmt/src/main.c
+++ tests/subsys/pm/power_mgmt/src/main.c
@@ -286,7 +286,7 @@ static void notify_pm_state_exit(enum pm_state state)
  *
  * @ingroup power_tests
  */
-void test_power_idle(void)
+ZTEST(power_management_1cpu, test_power_idle)
 {
 	TC_PRINT("give way to idle thread\n");
 	k_sleep(SLEEP_TIMEOUT);
@@ -311,7 +311,7 @@ static struct pm_notifier notifier = {
  *
  * @ingroup power_tests
  */
-void test_power_state_trans(void)
+ZTEST(power_management_1cpu, test_power_state_trans)
 {
 	int ret;
 
@@ -336,7 +336,7 @@ void test_power_state_trans(void)
  *
  * @details
  *  - device driver notify its power state change by pm_device_runtime_get and
- *    pm_device_runtime_put_async
+ *    pm_device_runtime_put_asyncpm_notifier_register
  *  - system inform device system power state change through device interface
  *    pm_action_cb
  *
@@ -345,7 +345,7 @@ void test_power_state_trans(void)
  *
  * @ingroup power_tests
  */
-void test_power_state_notification(void)
+ZTEST(power_management_1cpu, test_power_state_notification)
 {
 	int ret;
 	enum pm_device_state device_power_state;
@@ -370,9 +370,10 @@ void test_power_state_notification(void)
 	pm_device_state_get(device_dummy, &device_power_state);
 	zassert_equal(device_power_state, PM_DEVICE_STATE_SUSPENDED, NULL);
 	pm_notifier_unregister(&notifier);
+	testing_device_runtime = false;
 }
 
-void test_device_order(void)
+ZTEST(power_management_1cpu, test_device_order)
 {
 	device_a = DEVICE_DT_GET(DT_INST(0, test_device_pm));
 	zassert_not_null(device_a, "Failed to get device");
@@ -391,7 +392,7 @@ void test_device_order(void)
 /**
  * @brief Test the device busy APIs.
  */
-void test_busy(void)
+ZTEST(power_management_1cpu, test_busy)
 {
 	bool busy;
 
@@ -415,7 +416,7 @@ void test_busy(void)
 	zassert_false(busy, NULL);
 }
 
-void test_device_state_lock(void)
+ZTEST(power_management_1cpu, test_device_state_lock)
 {
 	pm_device_state_lock(device_a);
 	zassert_true(pm_device_state_is_locked(device_a), NULL);
@@ -430,18 +431,24 @@ void test_device_state_lock(void)
 	testing_device_lock = false;
 }
 
-void test_main(void)
+void power_management_1cpu_teardown(void *data)
+{
+	pm_notifier_unregister(&notifier);
+}
+
+static void *power_management_1cpu_setup(void)
 {
 	device_dummy = device_get_binding(DUMMY_DRIVER_NAME);
 	api = (struct dummy_driver_api *)device_dummy->api;
-
-	ztest_test_suite(power_management_test,
-			 ztest_1cpu_unit_test(test_power_idle),
-			 ztest_1cpu_unit_test(test_power_state_trans),
-			 ztest_1cpu_unit_test(test_device_order),
-			 ztest_1cpu_unit_test(test_device_state_lock),
-			 ztest_1cpu_unit_test(test_power_state_notification),
-			 ztest_1cpu_unit_test(test_busy));
-	ztest_run_test_suite(power_management_test);
-	pm_notifier_unregister(&notifier);
+	return NULL;
 }
+
+ZTEST(power_management_1cpu, test_power)
+{
+
+
+}
+
+ZTEST_SUITE(power_management_1cpu, NULL, power_management_1cpu_setup,
+			ztest_simple_1cpu_before, ztest_simple_1cpu_after,
+			power_management_1cpu_teardown);
