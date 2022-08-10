d772f822572194f03a47fd3ce27716ac120fc030
Anas Nashif <anas.nashif@intel.com>
2022-08-09 20:31:37 -0400

    intel_adsp: move memory windows to DTS proper
    Instead of just declaring the memory window register in DTS and have
    everything else all over the place (headers, Kconfig, etc.) this change
    defines the memory window instances in DTS and uses the device model to
    initialize the windows. Code is still part of the SoC, given that we do
    not have a driver subsystem suitable for this type of device yet.
    
    Move FW status to own workflow and separate from window setup.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

---
+++ soc/xtensa/intel_adsp/common/mem_window.c
@@ -0,0 +1,56 @@
+/* Copyright(c) 2022 Intel Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/arch/xtensa/cache.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+
+#include <adsp_memory.h>
+#include <adsp_shim.h>
+#include <mem_window.h>
+#include <soc.h>
+
+/* host windows */
+#define DMWBA(win_base) (win_base + 0x0)
+#define DMWLO(win_base) (win_base + 0x4)
+
+#define DT_DRV_COMPAT intel_adsp_mem_window
+
+#define WIN_SIZE(N)   (CONFIG_MEMORY_WIN_##N##_SIZE)
+#define WIN0_OFFSET DT_PROP(DT_NODELABEL(mem_window0), offset)
+#define WIN1_OFFSET WIN0_OFFSET + WIN_SIZE(0)
+#define WIN2_OFFSET WIN1_OFFSET + WIN_SIZE(1)
+#define WIN3_OFFSET WIN2_OFFSET + WIN_SIZE(2)
+
+#define WIN_OFFSET(N) (WIN##N##_OFFSET)
+
+__imr int mem_win_init(const struct device *dev)
+{
+	const struct mem_win_config *config = dev->config;
+
+	if (config->initialize) {
+		uint32_t *win = z_soc_uncached_ptr((void *)config->mem_base);
+		/* Software protocol: "firmware entered" has the value 5 */
+		win[0] = 5;
+	}
+
+	sys_write32(config->size | 0x7, DMWLO(config->base_addr));
+	sys_write32((config->mem_base | CAVS_DMWBA_READONLY | CAVS_DMWBA_ENABLE),
+		    DMWBA(config->base_addr));
+
+	return 0;
+}
+
+#define MEM_WIN_INIT(inst)                                                                         \
+	static const struct mem_win_config mem_win_config_##inst = {                               \
+		.base_addr = DT_INST_REG_ADDR(inst),                                               \
+		.size = WIN_SIZE(inst),                                                            \
+		.offset = WIN_OFFSET(inst),                                                        \
+		.mem_base = DT_REG_ADDR(DT_INST_PHANDLE(inst, memory)) + WIN_OFFSET(inst),         \
+		.initialize = DT_INST_PROP(inst, initialize),                                      \
+	};                                                                                         \
+	DEVICE_DT_INST_DEFINE(inst, mem_win_init, NULL, NULL, &mem_win_config_##inst, ARCH,        \
+			      CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, NULL);
+
+DT_INST_FOREACH_STATUS_OKAY(MEM_WIN_INIT)
