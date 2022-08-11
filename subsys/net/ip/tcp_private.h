15ee9e818cac9715341facd6e5a384d8a82e5a64
Robert Lubos <robert.lubos@nordicsemi.no>
2022-08-11 15:21:26 +0200

    net: tcp: Use default MSS value if no MSS option is present
    In case peer does not send the MSS option, the TCP stack should assume
    default peer MSS value of 536, as per RFC 1122:
    
        If an MSS option is not received at connection setup, TCP
        MUST assume a default send MSS of 536.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/net/ip/tcp_private.h
+++ subsys/net/ip/tcp_private.h
@@ -110,10 +110,12 @@
 #define conn_ack(_conn, _req) (_conn)->ack += (_req)
 #endif
 
-#define conn_mss(_conn)					\
-	((_conn)->recv_options.mss_found ?		\
-	 MIN((_conn)->recv_options.mss, \
-	 net_tcp_get_supported_mss(_conn)) : net_tcp_get_supported_mss(_conn))
+#define NET_TCP_DEFAULT_MSS 536
+
+#define conn_mss(_conn)							\
+	MIN((_conn)->recv_options.mss_found ? (_conn)->recv_options.mss	\
+					    : NET_TCP_DEFAULT_MSS,	\
+	    net_tcp_get_supported_mss(_conn))
 
 #define conn_state(_conn, _s)						\
 ({									\
