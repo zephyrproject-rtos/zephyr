3474ba919f830bbe35f5f0c21034252c8256d1c4
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2022-08-11 17:24:55 +0800

    ITE: drivers/i2c: FIFO2 can be selected to support channel of B or C
    FIFO2 can be selected to support channel of B or C by dtsi.
    
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>

--- soc/riscv/riscv-ite/common/chip_chipregs.h
+++ soc/riscv/riscv-ite/common/chip_chipregs.h
@@ -1762,6 +1762,7 @@ enum chip_pll_mode {
 /* 0x0F: SMBus FIFO Control 2 */
 #define IT8XXX2_SMB_BLKDS             BIT(4)
 #define IT8XXX2_SMB_FFEN              BIT(3)
+#define IT8XXX2_SMB_FFCHSEL2_B        0
 #define IT8XXX2_SMB_FFCHSEL2_C        BIT(0)
 /* 0x10: SMBus FIFO Status 2 */
 #define IT8XXX2_SMB_FIFO2_EMPTY       BIT(7)
@@ -1770,6 +1771,7 @@ enum chip_pll_mode {
 #define IT8XXX2_SMB_MAIF              BIT(7)
 #define IT8XXX2_SMB_MBCIF             BIT(6)
 #define IT8XXX2_SMB_MCIFI             BIT(2)
+#define IT8XXX2_SMB_MBIFI             BIT(1)
 #define IT8XXX2_SMB_MAIFI             BIT(0)
 /* 0x13: I2C Wr To Rd FIFO Interrupt Status */
 #define IT8XXX2_SMB_MCIFID            BIT(2)
