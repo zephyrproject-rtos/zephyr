da7df6f7eb1f0f1e71402bcf3a71eb0ef7b02423
Sagar Shah <sagar.shah@legrand.us>
2022-08-12 09:19:00 -0700

    net: coap_packet_parse() updated to return different values other than EINVAL.
    coap_packet_parse() now uses EINVAL for invalid input arguments, EBADMSG for
    malformed coap packet header, and EILSEQ for malformed coap options.
    
    Fixes: #48960
    
    Signed-off-by: sagar.shah@legrand.us

--- include/zephyr/net/coap.h
+++ include/zephyr/net/coap.h
@@ -360,7 +360,11 @@ const uint8_t *coap_packet_get_payload(const struct coap_packet *cpkt,
  * @param options Parse options and cache its details.
  * @param opt_num Number of options
  *
- * @return 0 in case of success or negative in case of error.
+ * @return 0 in case of success.
+ * @return -EINVAL in case of NULL ptr for cpkt and data, and invalid
+ * len value.
+ * @return -EBADMSG in case of issues with coap packet header data.
+ * @return -EILSEQ in case of issues parsing options.
  */
 int coap_packet_parse(struct coap_packet *cpkt, uint8_t *data, uint16_t len,
 		      struct coap_option *options, uint8_t opt_num);
