16fa7316b1a4c19af8f41ee1a59cb3fae0ac6096
Ming Shao <ming.shao@intel.com>
2022-08-12 17:34:57 +0800

    winstream: Add a gate-keeper test for winstream
    Currently, the Intel CAVS platforms use a winstream
    ring buffer design for log output. And use a dedicated
    tool called cavstool.py to extract logs from Intel CAVS
    boards. But it has some performance issue when cooperating
    with cavstool. The cavstool may lose logs or even extract
    nothing at all from the board if log output is frequent
    enough.
    
    This test is designed to be a gate-keeper for such
    scenario. If the log output is frequent enough and
    a wrap around happens, the test suite header info
    will be over-written and cause twister to consider
    this test fail. If winstream design is fixed in future,
    this test can pass.
    
    This test just output certain amount of logs frequently on
    CAVS15/18/25 platforms. It assumes nothing  about how logging
    is implemented on these platforms. Should the implementation
    be changed, this test should still pass.
    
    This test is dedicated to Intel CAVS platforms.
    Other platforms will skip this test.
    
    Signed-off-by: Ming Shao <ming.shao@intel.com>

---
+++ tests/lib/winstream/src/main.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2021 Golioth, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/ztest.h>
+#include <time.h>
+
+
+#include <adsp_memory.h>
+
+/* For winstream buffer size,
+ * refer to soc/xtensa/intel_adsp/common/trace_out.c
+ */
+#define TEST_WINSTREAM_BUFFER_SIZE HP_SRAM_WIN3_SIZE
+
+ZTEST(winstream, test_log_frequent)
+{
+	uint32_t i = 0;
+	/* Below log output has no delay, thus very frequent.
+	 * And we output for 2 full winstream buffer size to
+	 * ensure a wrap around happens. This way, the test suite
+	 * header info will be over-written if it is not extracted
+	 * yet. Thus leads to a twister failure because twister
+	 * relies on the test suite header info to determine test
+	 * pass or fail.
+	 * If the winstream is fixed, this case should pass.
+	 * This test is only meant for platforms with
+	 * CONFIG_WINSTREAM=y. Other platforms will skip it.
+	 */
+	while (i < TEST_WINSTREAM_BUFFER_SIZE * 2) {
+		TC_PRINT("M");
+		i++;
+	}
+	TC_PRINT("\n");
+
+}
+
+ZTEST_SUITE(winstream, NULL, NULL, NULL, NULL, NULL);
