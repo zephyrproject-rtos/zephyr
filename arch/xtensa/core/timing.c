941fb21d2f2070e4d2eb921d3ef60ac739ffcd64
Ederson de Souza <ederson.desouza@intel.com>
2022-08-17 13:48:57 -0700

    arch/xtensa/core: Fix timing API issues
    Two issues:
     - A unnecessary parentheses pair caused rounding errors (by truncating
       a small value before multiplying it).
     - arch_timing_cycles_to_ns_avg() wasn't actually converting the result
       to nanoseconds.
    
    Signed-off-by: Ederson de Souza <ederson.desouza@intel.com>

--- arch/xtensa/core/timing.c
+++ arch/xtensa/core/timing.c
@@ -43,13 +43,13 @@ uint64_t arch_timing_cycles_get(volatile timing_t *const start,
 
 uint64_t arch_timing_cycles_to_ns(uint64_t cycles)
 {
-	return cycles * (1000000000ULL / CONFIG_XTENSA_CCOUNT_HZ);
+	return cycles * 1000000000ULL / CONFIG_XTENSA_CCOUNT_HZ;
 }
 
 uint64_t arch_timing_cycles_to_ns_avg(uint64_t cycles, uint32_t count)
 {
 	/* Why is this an arch API?  This is just math! */
-	return cycles / (uint64_t) count;
+	return arch_timing_cycles_to_ns(cycles) / (uint64_t) count;
 }
 
 uint32_t arch_timing_freq_get_mhz(void)
