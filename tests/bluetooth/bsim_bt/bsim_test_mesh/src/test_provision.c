ec9480624c4f2c539cbe3b70ed77dad058cdcc16
Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>
2022-08-17 09:55:29 +0200

    tests: Bluetooth: Mesh: fix reprovisioning test with ext adv
    Extended advertiser has better latency without any delays than legacy one.
    If couple of config model commands start sequently then
    the first ones are received normally but the next one are transmitted in
    parallel with leftover communication (retransmissions) of the previous one.
    That causes collisions and broken test despite the functionality itself
    is ok. Adding delays between commands solves crossovering tx time.
    
    Signed-off-by: Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_mesh/src/test_provision.c
+++ tests/bluetooth/bsim_bt/bsim_test_mesh/src/test_provision.c
@@ -269,6 +269,7 @@ static void oob_auth_set(int test_step)
 	prov.input_size = oob_auth_test_vector[test_step].input_size;
 	prov.input_actions = oob_auth_test_vector[test_step].input_actions;
 }
+
 static void oob_device(bool use_oob_pk)
 {
 	int err = 0;
@@ -394,7 +395,7 @@ static void test_device_pb_adv_reprovision(void)
 	for (int i = 0; i < PROV_REPROV_COUNT; i++) {
 		/* Keep a long timeout so the prov multi case has time to finish: */
 		LOG_INF("Dev prov loop #%d, waiting for prov ...\n", i);
-		ASSERT_OK(k_sem_take(&prov_sem, K_SECONDS(5)));
+		ASSERT_OK(k_sem_take(&prov_sem, K_SECONDS(10)));
 	}
 
 	PASS();
@@ -561,18 +562,26 @@ static void test_provisioner_pb_adv_reprovision(void)
 						  &status));
 		ASSERT_EQUAL(0, status);
 
+		k_sleep(K_SECONDS(1));
+
 		ASSERT_OK(bt_mesh_cfg_mod_app_bind(0, current_dev_addr, current_dev_addr, 0x0,
 						   BT_MESH_MODEL_ID_HEALTH_SRV, &status));
 		ASSERT_EQUAL(0, status);
+
+		k_sleep(K_SECONDS(1));
 
 		ASSERT_OK(bt_mesh_cfg_mod_sub_add(0, current_dev_addr, current_dev_addr, 0xc000,
 						  BT_MESH_MODEL_ID_HEALTH_SRV, &status));
 		ASSERT_EQUAL(0, status);
 
+		k_sleep(K_SECONDS(1));
+
 		ASSERT_OK(bt_mesh_cfg_mod_pub_set(0, current_dev_addr, current_dev_addr,
 						  BT_MESH_MODEL_ID_HEALTH_SRV, &healthpub,
 						  &status));
 		ASSERT_EQUAL(0, status);
+
+		k_sleep(K_SECONDS(1));
 
 		ASSERT_OK(bt_mesh_cfg_node_reset(0, current_dev_addr, (bool *)&status));
 
