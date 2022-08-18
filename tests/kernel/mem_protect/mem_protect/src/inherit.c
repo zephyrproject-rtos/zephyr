253affd70172112ab2edd328805b0fbdba058f9a
Enjia Mai <enjia.mai@intel.com>
2022-08-19 00:27:17 +0800

    tests: kernel: move the memory protection test to new ztest API
    Migrate the testsuite tests/kernel/mem_protect/mem_protect to
    the new ztest API.
    
    Signed-off-by: Enjia Mai <enjia.mai@intel.com>

--- tests/kernel/mem_protect/mem_protect/src/inherit.c
+++ tests/kernel/mem_protect/mem_protect/src/inherit.c
@@ -102,17 +102,8 @@ static void test_thread_1_for_SU(void *p1, void *p2, void *p3)
  * @see k_mem_domain_init(), k_mem_domain_add_thread(),
  * k_thread_access_grant()
  */
-void test_permission_inheritance(void)
+ZTEST(mem_protect, test_permission_inheritance)
 {
-	int ret;
-
-	ret = k_mem_domain_init(&inherit_mem_domain,
-				ARRAY_SIZE(inherit_memory_partition_array),
-				inherit_memory_partition_array);
-	if (ret != 0) {
-		ztest_test_fail();
-	}
-
 	parent_tid = k_current_get();
 	k_mem_domain_add_thread(&inherit_mem_domain, parent_tid);
 
@@ -178,7 +169,7 @@ void parent_handler(void *p1, void *p2, void *p3)
  *
  * @see z_thread_heap_assign()
  */
-void test_inherit_resource_pool(void)
+ZTEST(mem_protect, test_inherit_resource_pool)
 {
 	k_sem_reset(&sync_sem);
 	k_thread_create(&parent_thr, parent_thr_stack,
@@ -193,3 +184,32 @@ void test_inherit_resource_pool(void)
 		     "Resource pool of the parent thread not inherited,"
 		     " by child thread");
 }
+
+void mem_protect_inhert_setup(void)
+{
+	int ret;
+
+	ret = k_mem_domain_init(&inherit_mem_domain,
+				ARRAY_SIZE(inherit_memory_partition_array),
+				inherit_memory_partition_array);
+	if (ret != 0) {
+		ztest_test_fail();
+	}
+}
+
+
+K_HEAP_DEFINE(test_mem_heap, TEST_HEAP_SIZE);
+
+void *mem_protect_setup(void)
+{
+	k_thread_priority_set(k_current_get(), -1);
+
+	k_thread_heap_assign(k_current_get(), &test_mem_heap);
+
+	mem_protect_inhert_setup();
+
+	return NULL;
+}
+
+ZTEST_SUITE(mem_protect, NULL, mem_protect_setup,
+		NULL, NULL, NULL);
