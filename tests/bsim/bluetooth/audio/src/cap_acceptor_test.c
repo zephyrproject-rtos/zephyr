46afaf59fb8dbf2de53f0338ac198004fc451f83
Emil Gydesen <emil.gydesen@nordicsemi.no>
2022-08-18 13:37:59 +0200

    tests: Bluetooth: Audio: Add CAP unicast start BSIM test
    The test only works partially until the controller and
    babblesim supports ISO, as it needs that in order to
    perform the unicast start procedure.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bsim/bluetooth/audio/src/cap_acceptor_test.c
+++ tests/bsim/bluetooth/audio/src/cap_acceptor_test.c
@@ -15,6 +15,11 @@
 
 extern enum bst_result_t bst_result;
 
+#define SINK_CONTEXT BT_AUDIO_CONTEXT_TYPE_UNSPECIFIED | \
+		     BT_AUDIO_CONTEXT_TYPE_MEDIA | \
+		     BT_AUDIO_CONTEXT_TYPE_CONVERSATIONAL
+#define SOURCE_CONTEXT BT_AUDIO_CONTEXT_TYPE_NOTIFICATIONS
+
 CREATE_FLAG(flag_broadcaster_found);
 CREATE_FLAG(flag_base_received);
 CREATE_FLAG(flag_pa_synced);
@@ -24,8 +29,16 @@ CREATE_FLAG(flag_pa_sync_lost);
 
 static struct bt_bap_broadcast_sink *g_broadcast_sink;
 static struct bt_cap_stream broadcast_sink_streams[CONFIG_BT_BAP_BROADCAST_SNK_STREAM_COUNT];
-static struct bt_bap_lc3_preset broadcast_preset_16_2_1 = BT_BAP_LC3_BROADCAST_PRESET_16_2_1(
-	BT_AUDIO_LOCATION_FRONT_LEFT, BT_AUDIO_CONTEXT_TYPE_UNSPECIFIED);
+static struct bt_bap_lc3_preset unicast_preset_16_2_1 =
+	BT_BAP_LC3_UNICAST_PRESET_16_2_1(BT_AUDIO_LOCATION_FRONT_LEFT,
+					   SINK_CONTEXT);
+static struct bt_bap_lc3_preset broadcast_preset_16_2_1 =
+	BT_BAP_LC3_BROADCAST_PRESET_16_2_1(BT_AUDIO_LOCATION_FRONT_LEFT,
+					   BT_AUDIO_CONTEXT_TYPE_UNSPECIFIED);
+
+static const struct bt_codec_qos_pref unicast_qos_pref =
+	BT_CODEC_QOS_PREF(true, BT_GAP_LE_PHY_2M, 0u, 60u, 20000u, 40000u, 20000u, 40000u);
+
 
 static K_SEM_DEFINE(sem_broadcast_started, 0U, ARRAY_SIZE(broadcast_sink_streams));
 static K_SEM_DEFINE(sem_broadcast_stopped, 0U, ARRAY_SIZE(broadcast_sink_streams));
@@ -36,6 +49,13 @@ static K_SEM_DEFINE(sem_broadcast_stopped, 0U, ARRAY_SIZE(broadcast_sink_streams
  */
 static const uint32_t bis_index_mask = BIT_MASK(ARRAY_SIZE(broadcast_sink_streams) + 1U);
 static uint32_t bis_index_bitfield;
+
+#define UNICAST_CHANNEL_COUNT_1 BIT(0)
+
+static struct bt_cap_stream unicast_streams[CONFIG_BT_ASCS_ASE_SNK_COUNT +
+					    CONFIG_BT_ASCS_ASE_SRC_COUNT];
+
+CREATE_FLAG(flag_unicast_stream_configured);
 
 static bool scan_recv_cb(const struct bt_le_scan_recv_info *info,
 			 struct net_buf_simple *ad,
@@ -185,6 +205,34 @@ static void recv_cb(struct bt_bap_stream *stream, const struct bt_iso_recv_info 
 static struct bt_bap_stream_ops broadcast_stream_ops = {
 	.started = started_cb, .stopped = stopped_cb, .recv = recv_cb};
 
+static void unicast_stream_enabled_cb(struct bt_bap_stream *stream)
+{
+	struct bt_bap_ep_info ep_info;
+	int err;
+
+	printk("Enabled: stream %p\n", stream);
+
+	err = bt_bap_ep_get_info(stream->ep, &ep_info);
+	if (err != 0) {
+		FAIL("Failed to get ep info: %d\n", err);
+		return;
+	}
+
+	if (ep_info.dir == BT_AUDIO_DIR_SINK) {
+		/* Automatically do the receiver start ready operation */
+		err = bt_bap_stream_start(stream);
+
+		if (err != 0) {
+			FAIL("Failed to start stream: %d\n", err);
+			return;
+		}
+	}
+}
+
+static struct bt_bap_stream_ops unicast_stream_ops = {
+	.enabled = unicast_stream_enabled_cb,
+};
+
 /* TODO: Expand with CAP service data */
 static const struct bt_data cap_acceptor_ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -193,15 +241,258 @@ static const struct bt_data cap_acceptor_ad[] = {
 
 static struct bt_csip_set_member_svc_inst *csip_set_member;
 
+static struct bt_bap_stream *unicast_stream_alloc(void)
+{
+	for (size_t i = 0; i < ARRAY_SIZE(unicast_streams); i++) {
+		struct bt_bap_stream *stream = &unicast_streams[i].bap_stream;
+
+		if (!stream->conn) {
+			return stream;
+		}
+	}
+
+	return NULL;
+}
+
+static int unicast_server_config(struct bt_conn *conn, const struct bt_bap_ep *ep,
+				 enum bt_audio_dir dir, const struct bt_codec *codec,
+				 struct bt_bap_stream **stream,
+				 struct bt_codec_qos_pref *const pref)
+{
+	printk("ASE Codec Config: conn %p ep %p dir %u\n", conn, ep, dir);
+
+	print_codec(codec);
+
+	*stream = unicast_stream_alloc();
+	if (*stream == NULL) {
+		printk("No streams available\n");
+
+		return -ENOMEM;
+	}
+
+	printk("ASE Codec Config stream %p\n", *stream);
+
+	SET_FLAG(flag_unicast_stream_configured);
+
+	*pref = unicast_qos_pref;
+
+	return 0;
+}
+
+static int unicast_server_reconfig(struct bt_bap_stream *stream, enum bt_audio_dir dir,
+				   const struct bt_codec *codec,
+				   struct bt_codec_qos_pref *const pref)
+{
+	printk("ASE Codec Reconfig: stream %p\n", stream);
+
+	print_codec(codec);
+
+	*pref = unicast_qos_pref;
+
+	/* We only support one QoS at the moment, reject changes */
+	return -ENOEXEC;
+}
+
+static int unicast_server_qos(struct bt_bap_stream *stream, const struct bt_codec_qos *qos)
+{
+	printk("QoS: stream %p qos %p\n", stream, qos);
+
+	print_qos(qos);
+
+	return 0;
+}
+
+static int unicast_server_enable(struct bt_bap_stream *stream, const struct bt_codec_data *meta,
+				 size_t meta_count)
+{
+	printk("Enable: stream %p meta_count %zu\n", stream, meta_count);
+
+	return 0;
+}
+
+static int unicast_server_start(struct bt_bap_stream *stream)
+{
+	printk("Start: stream %p\n", stream);
+
+	return 0;
+}
+
+static bool valid_metadata_type(uint8_t type, uint8_t len)
+{
+	switch (type) {
+	case BT_AUDIO_METADATA_TYPE_PREF_CONTEXT:
+	case BT_AUDIO_METADATA_TYPE_STREAM_CONTEXT:
+		if (len != 2) {
+			return false;
+		}
+
+		return true;
+	case BT_AUDIO_METADATA_TYPE_STREAM_LANG:
+		if (len != 3) {
+			return false;
+		}
+
+		return true;
+	case BT_AUDIO_METADATA_TYPE_PARENTAL_RATING:
+		if (len != 1) {
+			return false;
+		}
+
+		return true;
+	case BT_AUDIO_METADATA_TYPE_EXTENDED: /* 1 - 255 octets */
+	case BT_AUDIO_METADATA_TYPE_VENDOR: /* 1 - 255 octets */
+		if (len < 1) {
+			return false;
+		}
+
+		return true;
+	case BT_AUDIO_METADATA_TYPE_CCID_LIST: /* 2 - 254 octets */
+		if (len < 2) {
+			return false;
+		}
+
+		return true;
+	case BT_AUDIO_METADATA_TYPE_PROGRAM_INFO: /* 0 - 255 octets */
+	case BT_AUDIO_METADATA_TYPE_PROGRAM_INFO_URI: /* 0 - 255 octets */
+		return true;
+	default:
+		return false;
+	}
+}
+
+static int unicast_server_metadata(struct bt_bap_stream *stream, const struct bt_codec_data *meta,
+				   size_t meta_count)
+{
+	printk("Metadata: stream %p meta_count %zu\n", stream, meta_count);
+
+	for (size_t i = 0; i < meta_count; i++) {
+		if (!valid_metadata_type(meta->data.type, meta->data.data_len)) {
+			printk("Invalid metadata type %u or length %u\n",
+			       meta->data.type, meta->data.data_len);
+
+			return -EINVAL;
+		}
+	}
+
+	return 0;
+}
+
+static int unicast_server_disable(struct bt_bap_stream *stream)
+{
+	printk("Disable: stream %p\n", stream);
+
+	return 0;
+}
+
+static int unicast_server_stop(struct bt_bap_stream *stream)
+{
+	printk("Stop: stream %p\n", stream);
+
+	return 0;
+}
+
+static int unicast_server_release(struct bt_bap_stream *stream)
+{
+	printk("Release: stream %p\n", stream);
+
+	return 0;
+}
+
+static struct bt_bap_unicast_server_cb unicast_server_cbs = {
+	.config = unicast_server_config,
+	.reconfig = unicast_server_reconfig,
+	.qos = unicast_server_qos,
+	.enable = unicast_server_enable,
+	.start = unicast_server_start,
+	.metadata = unicast_server_metadata,
+	.disable = unicast_server_disable,
+	.stop = unicast_server_stop,
+	.release = unicast_server_release,
+};
+
+static void set_location(void)
+{
+	int err;
+
+	if (IS_ENABLED(CONFIG_BT_PAC_SNK_LOC)) {
+		err = bt_pacs_set_location(BT_AUDIO_DIR_SINK,
+					   BT_AUDIO_LOCATION_FRONT_CENTER);
+		if (err != 0) {
+			FAIL("Failed to set sink location (err %d)\n", err);
+			return;
+		}
+	}
+
+	if (IS_ENABLED(CONFIG_BT_PAC_SRC_LOC)) {
+		err = bt_pacs_set_location(BT_AUDIO_DIR_SOURCE,
+					   BT_AUDIO_LOCATION_FRONT_LEFT |
+						BT_AUDIO_LOCATION_FRONT_RIGHT);
+		if (err != 0) {
+			FAIL("Failed to set source location (err %d)\n", err);
+			return;
+		}
+	}
+
+	printk("Location successfully set\n");
+}
+
+static int set_supported_contexts(void)
+{
+	int err;
+
+	if (IS_ENABLED(CONFIG_BT_PAC_SNK)) {
+		err = bt_pacs_set_supported_contexts(BT_AUDIO_DIR_SINK, SINK_CONTEXT);
+		if (err != 0) {
+			printk("Failed to set sink supported contexts (err %d)\n",
+			       err);
+
+			return err;
+		}
+	}
+
+	if (IS_ENABLED(CONFIG_BT_PAC_SRC)) {
+		err = bt_pacs_set_supported_contexts(BT_AUDIO_DIR_SOURCE, SOURCE_CONTEXT);
+		if (err != 0) {
+			printk("Failed to set source supported contexts (err %d)\n",
+			       err);
+
+			return err;
+		}
+	}
+
+	printk("Supported contexts successfully set\n");
+
+	return 0;
+}
+
+static void set_available_contexts(void)
+{
+	int err;
+
+	err = bt_pacs_set_available_contexts(BT_AUDIO_DIR_SINK, SINK_CONTEXT);
+	if (IS_ENABLED(CONFIG_BT_PAC_SNK) && err != 0) {
+		FAIL("Failed to set sink available contexts (err %d)\n", err);
+		return;
+	}
+
+	err = bt_pacs_set_available_contexts(BT_AUDIO_DIR_SOURCE, SOURCE_CONTEXT);
+	if (IS_ENABLED(CONFIG_BT_PAC_SRC) && err != 0) {
+		FAIL("Failed to set source available contexts (err %d)\n", err);
+		return;
+	}
+
+	printk("Available contexts successfully set\n");
+}
+
 static void init(void)
 {
-	struct bt_csip_set_member_register_param csip_set_member_param = {
+	const struct bt_csip_set_member_register_param csip_set_member_param = {
 		.set_size = 3,
 		.rank = 1,
 		.lockable = true,
 		/* Using the CSIP_SET_MEMBER test sample SIRK */
 		.set_sirk = { 0xcd, 0xcc, 0x72, 0xdd, 0x86, 0x8c, 0xcd, 0xce,
-			0x22, 0xfd, 0xa1, 0x21, 0x09, 0x7d, 0x7d, 0x45 },
+			      0x22, 0xfd, 0xa1, 0x21, 0x09, 0x7d, 0x7d, 0x45 },
 	};
 
 	int err;
@@ -215,8 +506,7 @@ static void init(void)
 	printk("Bluetooth initialized\n");
 
 	if (IS_ENABLED(CONFIG_BT_CAP_ACCEPTOR_SET_MEMBER)) {
-		err = bt_cap_acceptor_register(&csip_set_member_param,
-					       &csip_set_member);
+		err = bt_cap_acceptor_register(&csip_set_member_param, &csip_set_member);
 		if (err != 0) {
 			FAIL("CAP acceptor failed to register (err %d)\n", err);
 			return;
@@ -224,8 +514,32 @@ static void init(void)
 	}
 
 	if (IS_ENABLED(CONFIG_BT_BAP_UNICAST_SERVER)) {
+		static struct bt_pacs_cap unicast_cap = {
+			.codec = &unicast_preset_16_2_1.codec,
+		};
+
+		err = bt_pacs_cap_register(BT_AUDIO_DIR_SINK, &unicast_cap);
+		if (err != 0) {
+			FAIL("Broadcast capability register failed (err %d)\n",
+			     err);
+
+			return;
+		}
+
+		err = bt_bap_unicast_server_register_cb(&unicast_server_cbs);
+		if (err != 0) {
+			FAIL("Failed to register unicast server callbacks (err %d)\n",
+			     err);
+
+			return;
+		}
+
+		for (size_t i = 0U; i < ARRAY_SIZE(unicast_streams); i++) {
+			bt_cap_stream_ops_register(&unicast_streams[i], &unicast_stream_ops);
+		}
+
 		err = bt_le_adv_start(BT_LE_ADV_CONN_NAME, cap_acceptor_ad,
-				ARRAY_SIZE(cap_acceptor_ad), NULL, 0);
+				      ARRAY_SIZE(cap_acceptor_ad), NULL, 0);
 		if (err != 0) {
 			FAIL("Advertising failed to start (err %d)\n", err);
 			return;
@@ -233,11 +547,11 @@ static void init(void)
 	}
 
 	if (IS_ENABLED(CONFIG_BT_BAP_BROADCAST_SINK)) {
-		static struct bt_pacs_cap cap = {
+		static struct bt_pacs_cap broadcast_cap = {
 			.codec = &broadcast_preset_16_2_1.codec,
 		};
 
-		err = bt_pacs_cap_register(BT_AUDIO_DIR_SINK, &cap);
+		err = bt_pacs_cap_register(BT_AUDIO_DIR_SINK, &broadcast_cap);
 		if (err != 0) {
 			FAIL("Broadcast capability register failed (err %d)\n",
 			     err);
@@ -256,6 +570,10 @@ static void init(void)
 						   &broadcast_stream_ops);
 		}
 	}
+
+	set_supported_contexts();
+	set_available_contexts();
+	set_location();
 }
 
 static void test_cap_acceptor_unicast(void)
