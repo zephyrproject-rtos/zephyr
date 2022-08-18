6866cd30e8ec9b277f66467e3e29882e26266c27
Emil Gydesen <emil.gydesen@nordicsemi.no>
2022-08-18 13:36:48 +0200

    Bluetooth: Audio: Add support for CAP unicast start in shell
    This modifies the audio.c shell module to support
    multiple connections.
    
    This adds support for the cap_initiator unicast-start command,
    which supports multiple endpoints on multiple devices.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/shell/bt.h
+++ subsys/bluetooth/shell/bt.h
@@ -31,6 +31,27 @@ extern struct bt_le_per_adv_sync *per_adv_syncs[CONFIG_BT_PER_ADV_SYNC_MAX];
 #endif /* CONFIG_BT_PER_ADV_SYNC */
 #endif /* CONFIG_BT_EXT_ADV */
 
+#if defined(CONFIG_BT_AUDIO)
+/* Must guard before including audio.h as audio.h uses Kconfigs guarded by
+ * CONFIG_BT_AUDIO
+ */
+#include <zephyr/bluetooth/audio/audio.h>
+#include <zephyr/bluetooth/audio/bap_lc3_preset.h>
+
+struct named_lc3_preset {
+	const char *name;
+	struct bt_bap_lc3_preset preset;
+};
+
+#if defined(CONFIG_BT_BAP_UNICAST_CLIENT)
+
+extern struct bt_bap_ep *snks[CONFIG_BT_MAX_CONN][CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT];
+extern struct bt_bap_ep *srcs[CONFIG_BT_MAX_CONN][CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SRC_COUNT];
+extern const struct named_lc3_preset *default_sink_preset;
+extern const struct named_lc3_preset *default_source_preset;
+#endif /* CONFIG_BT_BAP_UNICAST_CLIENT */
+#endif /* CONFIG_BT_AUDIO */
+
 void conn_addr_str(struct bt_conn *conn, char *addr, size_t len);
 
 #endif /* __BT_H */
