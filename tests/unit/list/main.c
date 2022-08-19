3f4906a7a6e329c7ebdadcf927a7f33c7e1849e1
NingX Zhao <ningx.zhao@intel.com>
2022-08-19 10:15:14 +0800

    tests: unit: list: move testcases to new ZTEST API
    Move testcases to new ZTEST API.
    
    Signed-off-by: NingX Zhao <ningx.zhao@intel.com>

--- tests/unit/list/main.c
+++ tests/unit/list/main.c
@@ -6,19 +6,4 @@
 
 #include <zephyr/ztest.h>
 
-extern void test_slist(void);
-extern void test_sflist(void);
-extern void test_dlist(void);
-extern void test_dlist2(void);
-
-void test_main(void)
-{
-	ztest_test_suite(dlist,
-			 ztest_unit_test(test_dlist),
-			 ztest_unit_test(test_dlist2),
-			 ztest_unit_test(test_slist),
-			 ztest_unit_test(test_sflist)
-			 );
-
-	ztest_run_test_suite(dlist);
-}
+ZTEST_SUITE(dlist_api, NULL, NULL, NULL, NULL, NULL);
