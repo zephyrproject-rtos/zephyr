3e17abeadc16e01bded17ecfbe1fd5f8e23bb819
Ahmed Moheb <ahmed.moheb@nordicsemi.no>
2022-08-23 11:35:55 +0200

    tests: bluetooth: host: Add UT for bt_keys_foreach_type()
    Unit test project for bt_keys_foreach_type().
    This part of subsys/bluetooth/host/keys.c unit testing.
    
    Signed-off-by: Ahmed Moheb <ahmed.moheb@nordicsemi.no>

--- tests/bluetooth/host/keys/mocks/keys_help_utils.h
+++ tests/bluetooth/host/keys/mocks/keys_help_utils.h
@@ -8,8 +8,15 @@
 
 /* BT (ID, Address) pair */
 struct id_addr_pair {
-	uint8_t id;			    /* ID */
-	bt_addr_le_t *addr;		/* Pointer to the address */
+	uint8_t id;	    /* ID */
+	bt_addr_le_t *addr; /* Pointer to the address */
+};
+
+/* BT Key (ID, Address, type) info */
+struct id_addr_type {
+	uint8_t id;	    /*	ID */
+	bt_addr_le_t *addr; /*	Pointer to the address	*/
+	int type;	    /*	Key type */
 };
 
 /* keys.c declarations */
@@ -21,6 +28,8 @@ uint32_t bt_keys_get_aging_counter_val(void);
 /* keys_help_utils.c declarations */
 void clear_key_pool(void);
 int fill_key_pool_by_id_addr(const struct id_addr_pair src[], int size, struct bt_keys *refs[]);
+int fill_key_pool_by_id_addr_type(const struct id_addr_type src[], int size,
+				  struct bt_keys *refs[]);
 bool check_key_pool_is_empty(void);
 
 /* Repeat test entries */
