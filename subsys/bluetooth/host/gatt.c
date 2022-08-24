97f9a16ed15f6486dff1f73ccee40830706ec374
Stephanos Ioannidis <root@stephanos.io>
2022-08-24 22:55:43 +0900

    Bluetooth: Host: GATT: Make addr param const in find_sc_cfg
    This commit adds the `const` qualifier to the `addr` parameter of the
    `find_sc_cfg` function because this parameter is and should never be
    modified within this function.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- subsys/bluetooth/host/gatt.c
+++ subsys/bluetooth/host/gatt.c
@@ -308,7 +308,7 @@ static struct db_hash {
 } db_hash;
 #endif
 
-static struct gatt_sc_cfg *find_sc_cfg(uint8_t id, bt_addr_le_t *addr)
+static struct gatt_sc_cfg *find_sc_cfg(uint8_t id, const bt_addr_le_t *addr)
 {
 	BT_DBG("id: %u, addr: %s", id, bt_addr_le_str(addr));
 
