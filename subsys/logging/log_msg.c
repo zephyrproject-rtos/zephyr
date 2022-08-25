fd18ad846e70acdbd0bf886f1ef8461340f30c23
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2022-08-25 15:51:11 +0200

    lib: os: cbprintf: Renamed flags used for conversion function
    At some point, package copy function was extended and renamed
    to cbprintf_package_convert. However, flags used by this
    function were not renamed and used contained COPY idiom.
    Deprecating flags with COPY and replacing them with flags
    with CONVERT idiom to match function which is utilizing them.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

--- subsys/logging/log_msg.c
+++ subsys/logging/log_msg.c
@@ -52,7 +52,7 @@ void z_impl_z_log_msg_static_create(const void *source,
 	struct log_msg *msg;
 
 	if (inlen > 0) {
-		uint32_t flags = CBPRINTF_PACKAGE_COPY_RW_STR;
+		uint32_t flags = CBPRINTF_PACKAGE_CONVERT_RW_STR;
 		uint16_t strl[4];
 		int len;
 
