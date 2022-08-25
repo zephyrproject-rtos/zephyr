7fb2e1ec4da6bc9c1dba017bc6dfb585fab5c59f
Florian Grandel <jerico.dev@gmail.com>
2022-08-26 00:38:37 +0200

    net: l2: ieee802154: add unittests
    This change introduces test coverage for IEEE 802.15.4 AF_PACKET support
    and IEEE 802.15.4 security operation.
    
    Signed-off-by: Florian Grandel <jerico.dev@gmail.com>

--- tests/net/ieee802154/l2/src/ieee802154_test.c
+++ tests/net/ieee802154/l2/src/ieee802154_test.c
@@ -10,12 +10,17 @@ LOG_MODULE_REGISTER(net_ieee802154_test, LOG_LEVEL_DBG);
 #include <zephyr/zephyr.h>
 #include <zephyr/ztest.h>
 
-#include <zephyr/net/net_core.h>
-#include "net_private.h"
 
+#include <zephyr/crypto/crypto.h>
+#include <zephyr/net/ethernet.h>
+#include <zephyr/net/ieee802154.h>
+#include <zephyr/net/ieee802154_mgmt.h>
+#include <zephyr/net/net_core.h>
 #include <zephyr/net/net_ip.h>
 #include <zephyr/net/net_pkt.h>
+#include <zephyr/net/socket.h>
 
+#include "net_private.h"
 #include <ieee802154_frame.h>
 #include <ipv6.h>
 
@@ -189,7 +194,7 @@ static bool test_ns_sending(struct ieee802154_pkt_test *t)
 	if (!ieee802154_validate_frame(net_pkt_data(current_pkt),
 				       net_pkt_get_len(current_pkt), &mpdu)) {
 		NET_ERR("*** Sent packet is not valid\n");
-		net_pkt_unref(current_pkt);
+		net_pkt_frag_unref(current_pkt->frags);
 
 		return false;
 	}
@@ -198,6 +203,184 @@ static bool test_ns_sending(struct ieee802154_pkt_test *t)
 	current_pkt->frags = NULL;
 
 	return true;
+}
+
+static bool test_dgram_packet_sending(struct sockaddr_ll *pkt_sll, uint32_t security_level)
+{
+	struct ieee802154_context *ctx = net_if_l2_data(iface);
+	int fd;
+	struct sockaddr_ll socket_sll = {0};
+	uint8_t payload[4] = {0x01, 0x02, 0x03, 0x04};
+	struct ieee802154_mpdu mpdu;
+	bool result = false;
+	struct ieee802154_security_params params = {
+		.key = {0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb,
+			0xcc, 0xcd, 0xce, 0xcf},
+		.key_len = 16U,
+		.key_mode = IEEE802154_KEY_ID_MODE_IMPLICIT,
+		.level = security_level,
+	};
+
+	if (net_mgmt(NET_REQUEST_IEEE802154_SET_SECURITY_SETTINGS, iface, &params,
+		     sizeof(struct ieee802154_security_params))) {
+		NET_ERR("*** Failed to set security settings\n");
+		goto out;
+	}
+
+	NET_INFO("- Sending DGRAM packet via AF_PACKET socket\n");
+	fd = socket(AF_PACKET, SOCK_DGRAM, ETH_P_IEEE802154);
+	if (fd < 0) {
+		NET_ERR("*** Failed to create DGRAM socket : %d\n", errno);
+		goto release_sec_ctx;
+	}
+
+	socket_sll.sll_ifindex = net_if_get_by_iface(iface);
+	socket_sll.sll_family = AF_PACKET;
+	socket_sll.sll_protocol = ETH_P_IEEE802154;
+	if (bind(fd, (const struct sockaddr *)&socket_sll, sizeof(struct sockaddr_ll))) {
+		NET_ERR("*** Failed to bind packet socket : %d\n", errno);
+		goto release_fd;
+	}
+
+	if (sendto(fd, payload, sizeof(payload), 0, (const struct sockaddr *)pkt_sll,
+		   sizeof(struct sockaddr_ll)) != sizeof(payload)) {
+		NET_ERR("*** Failed to send, errno %d\n", errno);
+		goto release_fd;
+	}
+
+	k_yield();
+	k_sem_take(&driver_lock, K_SECONDS(1));
+
+	if (!current_pkt->frags) {
+		NET_ERR("*** Could not send DGRAM packet\n");
+		goto release_fd;
+	}
+
+	pkt_hexdump(net_pkt_data(current_pkt), net_pkt_get_len(current_pkt));
+
+	if (!ieee802154_validate_frame(net_pkt_data(current_pkt),
+				       net_pkt_get_len(current_pkt), &mpdu)) {
+		NET_ERR("*** Sent packet is not valid\n");
+		goto release_frag;
+	}
+
+	net_pkt_lladdr_src(current_pkt)->addr = ctx->ext_addr;
+	net_pkt_lladdr_src(current_pkt)->len = IEEE802154_MAX_ADDR_LENGTH;
+	if (!ieee802154_decipher_data_frame(iface, current_pkt, &mpdu)) {
+		NET_ERR("*** Cannot decipher/authenticate packet\n");
+		goto release_frag;
+	}
+
+	if (memcmp(mpdu.payload, payload, sizeof(payload)) != 0) {
+		NET_ERR("*** Payload of sent packet is incorrect\n");
+		goto release_frag;
+	}
+
+	result = true;
+
+release_frag:
+	net_pkt_frag_unref(current_pkt->frags);
+	current_pkt->frags = NULL;
+release_fd:
+	close(fd);
+release_sec_ctx:
+	cipher_free_session(ctx->sec_ctx.enc.device, &ctx->sec_ctx.enc);
+	cipher_free_session(ctx->sec_ctx.dec.device, &ctx->sec_ctx.dec);
+out:
+	ctx->sec_ctx.level = IEEE802154_SECURITY_LEVEL_NONE;
+	return result;
+}
+
+static bool test_raw_packet_sending(void)
+{
+	struct ieee802154_context *ctx = net_if_l2_data(iface);
+	int fd;
+	struct sockaddr_ll socket_sll = {0};
+	struct msghdr msg = {0};
+	struct iovec io_vector;
+	uint8_t payload[20];
+	struct ieee802154_mpdu mpdu;
+	bool result = false;
+
+	NET_INFO("- Sending RAW packet via AF_PACKET socket\n");
+	fd = socket(AF_PACKET, SOCK_RAW, ETH_P_IEEE802154);
+	if (fd < 0) {
+		NET_ERR("*** Failed to create RAW socket : %d\n", errno);
+		goto out;
+	}
+
+	socket_sll.sll_ifindex = net_if_get_by_iface(iface);
+	socket_sll.sll_family = AF_PACKET;
+	socket_sll.sll_protocol = ETH_P_IEEE802154;
+	if (bind(fd, (const struct sockaddr *)&socket_sll, sizeof(struct sockaddr_ll))) {
+		NET_ERR("*** Failed to bind packet socket : %d\n", errno);
+		goto release_fd;
+	}
+
+	/* Construct raw packet payload, length and FCS gets added in the radio driver,
+	 * see https://github.com/linux-wpan/wpan-tools/blob/master/examples/af_packet_tx.c
+	 */
+	payload[0] = 0x01; /* Frame Control Field */
+	payload[1] = 0xc8; /* Frame Control Field */
+	payload[2] = 0x8b; /* Sequence number */
+	payload[3] = 0xff; /* Destination PAN ID 0xffff */
+	payload[4] = 0xff; /* Destination PAN ID */
+	payload[5] = 0x02; /* Destination short address 0x0002 */
+	payload[6] = 0x00; /* Destination short address */
+	payload[7] = 0x23; /* Source PAN ID 0x0023 */
+	payload[8] = 0x00; /* */
+	payload[9] = 0x60; /* Source extended address ae:c2:4a:1c:21:16:e2:60 */
+	payload[10] = 0xe2; /* */
+	payload[11] = 0x16; /* */
+	payload[12] = 0x21; /* */
+	payload[13] = 0x1c; /* */
+	payload[14] = 0x4a; /* */
+	payload[15] = 0xc2; /* */
+	payload[16] = 0xae; /* */
+	payload[17] = 0xAA; /* Payload */
+	payload[18] = 0xBB; /* */
+	payload[19] = 0xCC; /* */
+	io_vector.iov_base = payload;
+	io_vector.iov_len = sizeof(payload);
+	msg.msg_iov = &io_vector;
+	msg.msg_iovlen = 1;
+
+	if (sendmsg(fd, &msg, 0) != sizeof(payload)) {
+		NET_ERR("*** Failed to send, errno %d\n", errno);
+		goto release_fd;
+	}
+
+	k_yield();
+	k_sem_take(&driver_lock, K_SECONDS(1));
+
+	if (!current_pkt->frags) {
+		NET_ERR("*** Could not send RAW packet\n");
+		goto release_fd;
+	}
+
+	pkt_hexdump(net_pkt_data(current_pkt), net_pkt_get_len(current_pkt));
+
+	if (!ieee802154_validate_frame(net_pkt_data(current_pkt),
+				       net_pkt_get_len(current_pkt), &mpdu)) {
+		NET_ERR("*** Sent packet is not valid\n");
+		goto release_frag;
+	}
+
+	if (memcmp(mpdu.payload, &payload[17], 3) != 0) {
+		NET_ERR("*** Payload of sent packet is incorrect\n");
+		goto release_frag;
+	}
+
+	result = true;
+
+release_frag:
+	net_pkt_frag_unref(current_pkt->frags);
+	current_pkt->frags = NULL;
+release_fd:
+	close(fd);
+out:
+	ctx->sec_ctx.level = IEEE802154_SECURITY_LEVEL_NONE;
+	return result;
 }
 
 static bool test_ack_reply(struct ieee802154_pkt_test *t)
@@ -356,6 +539,60 @@ ZTEST(ieee802154_l2, test_parsing_sec_data_pkt)
 	ret = test_packet_parsing(&test_sec_data_pkt);
 
 	zassert_true(ret, "Secured data frame parsed");
+}
+
+ZTEST(ieee802154_l2, test_sending_broadcast_dgram_pkt)
+{
+	bool ret;
+	uint16_t dst_short_addr = htons(IEEE802154_BROADCAST_ADDRESS);
+	struct sockaddr_ll pkt_sll = {
+		.sll_halen = sizeof(dst_short_addr),
+		.sll_protocol = htons(ETH_P_IEEE802154),
+	};
+	memcpy(pkt_sll.sll_addr, &dst_short_addr, sizeof(dst_short_addr));
+
+	ret = test_dgram_packet_sending(&pkt_sll, IEEE802154_SECURITY_LEVEL_NONE);
+
+	zassert_true(ret, "Broadcast DGRAM packet sent");
+}
+
+ZTEST(ieee802154_l2, test_sending_authenticated_dgram_pkt)
+{
+	bool ret;
+	uint16_t dst_short_addr = htons(0x1234);
+	struct sockaddr_ll pkt_sll = {
+		.sll_halen = sizeof(dst_short_addr),
+		.sll_protocol = htons(ETH_P_IEEE802154),
+	};
+	memcpy(pkt_sll.sll_addr, &dst_short_addr, sizeof(dst_short_addr));
+
+	ret = test_dgram_packet_sending(&pkt_sll, IEEE802154_SECURITY_LEVEL_MIC_128);
+
+	zassert_true(ret, "Authenticated DGRAM packet sent");
+}
+
+ZTEST(ieee802154_l2, test_sending_encrypted_and_authenticated_dgram_pkt)
+{
+	bool ret;
+	uint8_t dst_ext_addr[8] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+	struct sockaddr_ll pkt_sll = {
+		.sll_halen = sizeof(dst_ext_addr),
+		.sll_protocol = htons(ETH_P_IEEE802154),
+	};
+	memcpy(pkt_sll.sll_addr, dst_ext_addr, sizeof(dst_ext_addr));
+
+	ret = test_dgram_packet_sending(&pkt_sll, IEEE802154_SECURITY_LEVEL_ENC_MIC_128);
+
+	zassert_true(ret, "Encrypted and authenticated DGRAM packet sent");
+}
+
+ZTEST(ieee802154_l2, test_sending_raw_pkt)
+{
+	bool ret;
+
+	ret = test_raw_packet_sending();
+
+	zassert_true(ret, "RAW packet sent");
 }
 
 ZTEST_SUITE(ieee802154_l2, NULL, test_init, NULL, NULL, NULL);
