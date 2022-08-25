58c032580ec851483b8f32e2ded4c2a005c08c5f
Enjia Mai <enjia.mai@intel.com>
2022-08-25 15:30:48 +0800

    tests: drivers: i2c: move the gy271 test to new ztest API
    Migrate the testsuite tests/drivers/i2c/i2c_api to the new
    ztest API.
    
    Signed-off-by: Enjia Mai <enjia.mai@intel.com>

--- tests/drivers/i2c/i2c_api/src/test_i2c.c
+++ tests/drivers/i2c/i2c_api/src/test_i2c.c
@@ -149,12 +149,14 @@ static int test_burst_gy271(void)
 	return TC_PASS;
 }
 
-void test_i2c_gy271(void)
+ZTEST(i2c_gy271, test_i2c_gy271)
 {
 	zassert_true(test_gy271() == TC_PASS, NULL);
 }
 
-void test_i2c_burst_gy271(void)
+ZTEST(i2c_gy271, test_i2c_burst_gy271)
 {
 	zassert_true(test_burst_gy271() == TC_PASS, NULL);
 }
+
+ZTEST_SUITE(i2c_gy271, NULL, NULL, NULL, NULL, NULL);
