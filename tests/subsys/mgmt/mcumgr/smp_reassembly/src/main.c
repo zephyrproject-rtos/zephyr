ffdff60e0acb455669c1c5826fb55609cbeeb109
Dominik Ermel <dominik.ermel@nordicsemi.no>
2022-08-30 13:59:25 +0000

    tests/mgmt/mcumgr/smp_reassembly: Drop zephyr_ prefix
    The mcumgr transport API drops zephyr_ prefixes.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- tests/subsys/mgmt/mcumgr/smp_reassembly/src/main.c
+++ tests/subsys/mgmt/mcumgr/smp_reassembly/src/main.c
@@ -12,25 +12,25 @@
 #include <mgmt/mgmt.h>
 #include "../../../../../../subsys/mgmt/mcumgr/smp_reassembly.h"
 
-static struct zephyr_smp_transport zst;
+static struct smp_transport smpt;
 static uint8_t buff[CONFIG_MCUMGR_BUF_SIZE];
 #define TEST_FRAME_SIZE 256
 
 static struct net_buf *backup;
 
-/* The function is called by zephyr_smp_reassembly_complete to pass a completed packet
+/* The function is called by smp_reassembly_complete to pass a completed packet
  * for further processing; since there is nothing to process, this stub will only backup
  * buffer the pointer to allow a test case to free it with use of the mcumgr net_buf
  * management.
  */
-void zephyr_smp_rx_req(struct zephyr_smp_transport *zst, struct net_buf *nb)
+void smp_rx_req(struct smp_transport *smpt, struct net_buf *nb)
 {
 	backup = nb;
 }
 
 ZTEST(smp_reassembly, test_first)
 {
-	zephyr_smp_reassembly_init(&zst);
+	smp_reassembly_init(&smpt);
 	struct mgmt_hdr *mh = (struct mgmt_hdr *)buff;
 	int frag_used;
 	int ret;
@@ -38,35 +38,35 @@ ZTEST(smp_reassembly, test_first)
 
 	/** First fragment errors **/
 	/* Len longer than netbuf error */
-	zassert_equal(-ENOSR, zephyr_smp_reassembly_collect(&zst, buff, CONFIG_MCUMGR_BUF_SIZE + 1),
+	zassert_equal(-ENOSR, smp_reassembly_collect(&smpt, buff, CONFIG_MCUMGR_BUF_SIZE + 1),
 		      "Expected -ENOSR error");
 	/* Len not enough to read expected size from header */
 	zassert_equal(-ENODATA,
-		      zephyr_smp_reassembly_collect(&zst, buff, sizeof(struct mgmt_hdr) - 1),
+		      smp_reassembly_collect(&smpt, buff, sizeof(struct mgmt_hdr) - 1),
 		      "Expected -ENODATA error");
 	/* Length extracted from header, plus size of header, is bigger than buffer */
 	mh->nh_len = sys_cpu_to_be16(CONFIG_MCUMGR_BUF_SIZE - sizeof(struct mgmt_hdr) + 1);
 	zassert_equal(-ENOSR,
-		      zephyr_smp_reassembly_collect(&zst, buff, sizeof(struct mgmt_hdr) + 1),
+		      smp_reassembly_collect(&smpt, buff, sizeof(struct mgmt_hdr) + 1),
 		      "Expected -ENOSR error");
 
 	/* Successfully alloc buffer */
 	mh->nh_len = sys_cpu_to_be16(TEST_FRAME_SIZE - sizeof(struct mgmt_hdr));
 	frag_used = 40;
 	expected = TEST_FRAME_SIZE - frag_used;
-	ret = zephyr_smp_reassembly_collect(&zst, buff, frag_used);
+	ret = smp_reassembly_collect(&smpt, buff, frag_used);
 	zassert_equal(expected, ret,
 		      "Expected is %d should be %d\n", ret, expected);
 
 	/* Force complete it, expected returned number of bytes missing */
-	ret = zephyr_smp_reassembly_complete(&zst, true);
+	ret = smp_reassembly_complete(&smpt, true);
 	zassert_equal(expected, ret,
 		      "Forced completion ret %d, but expected was %d\n", ret, expected);
 
 	/* Check fail due to lack of buffers: there is only one buffer and it already got passed
 	 * for processing by complete
 	 */
-	ret = zephyr_smp_reassembly_collect(&zst, buff, frag_used);
+	ret = smp_reassembly_collect(&smpt, buff, frag_used);
 	zassert_equal(-ENOMEM, ret,
 		      "Expected is %d should be %d\n", ret, expected);
 
@@ -88,11 +88,11 @@ ZTEST(smp_reassembly, test_drops)
 	mh->nh_len = sys_cpu_to_be16(TEST_FRAME_SIZE - sizeof(struct mgmt_hdr));
 	frag_used = 40;
 	expected = TEST_FRAME_SIZE - frag_used;
-	ret = zephyr_smp_reassembly_collect(&zst, buff, frag_used);
+	ret = smp_reassembly_collect(&smpt, buff, frag_used);
 	zassert_equal(expected, ret,
 		      "Expected is %d should be %d\n", ret, expected);
 
-	ret = zephyr_smp_reassembly_drop(&zst);
+	ret = smp_reassembly_drop(&smpt);
 	zassert_equal(0, ret,
 		      "Expected %d from drop, got %d", ret, expected);
 }
@@ -114,7 +114,7 @@ ZTEST(smp_reassembly, test_collection)
 	/* First fragment with header */
 	mh->nh_len = sys_cpu_to_be16(TEST_FRAME_SIZE - sizeof(struct mgmt_hdr));
 	frag = 40;
-	ret = zephyr_smp_reassembly_collect(&zst, buff, frag);
+	ret = smp_reassembly_collect(&smpt, buff, frag);
 	expected = TEST_FRAME_SIZE - frag;
 	zassert_equal(expected, ret,
 		      "Expected is %d should be %d\n", ret, expected);
@@ -122,29 +122,29 @@ ZTEST(smp_reassembly, test_collection)
 
 	/* Next fragment */
 	frag = 40;
-	ret = zephyr_smp_reassembly_collect(&zst, &buff[pkt_used], frag);
+	ret = smp_reassembly_collect(&smpt, &buff[pkt_used], frag);
 	pkt_used += frag;
 	expected = TEST_FRAME_SIZE - pkt_used;
 	zassert_equal(expected, ret,
 		      "Expected is %d should be %d\n", ret, expected);
 
 	/* Try to complete incomplete, no force */
-	ret = zephyr_smp_reassembly_complete(&zst, false);
+	ret = smp_reassembly_complete(&smpt, false);
 	zassert_equal(-ENODATA, ret,
 		      "Expected -ENODATA when completing incomplete buffer");
 
 	/* Last fragment */
-	ret = zephyr_smp_reassembly_collect(&zst, &buff[pkt_used], expected);
+	ret = smp_reassembly_collect(&smpt, &buff[pkt_used], expected);
 	zassert_equal(0, ret,
 		      "Expected is %d should be %d\n", ret, 0);
 
 	/* And overflow */
-	ret = zephyr_smp_reassembly_collect(&zst, buff, 1);
+	ret = smp_reassembly_collect(&smpt, buff, 1);
 	zassert_equal(-EOVERFLOW, ret,
 		      "Expected -EOVERFLOW, got %d\n", ret);
 
 	/* Complete successfully complete buffer */
-	ret = zephyr_smp_reassembly_complete(&zst, false);
+	ret = smp_reassembly_complete(&smpt, false);
 	zassert_equal(0, ret,
 		      "Expected 0 from complete, got %d\n", ret);
 
@@ -164,15 +164,15 @@ ZTEST(smp_reassembly, test_no_packet_started)
 	int ret;
 
 	/** Complete on non-started packet **/
-	ret = zephyr_smp_reassembly_complete(&zst, false);
+	ret = smp_reassembly_complete(&smpt, false);
 	zassert_equal(-EINVAL, ret,
 		      "Expected -EINVAL from complete, got %d", ret);
-	ret = zephyr_smp_reassembly_complete(&zst, true);
+	ret = smp_reassembly_complete(&smpt, true);
 	zassert_equal(-EINVAL, ret,
 		      "Expected -EINVAL from complete, got %d", ret);
 
 	/* Try to drop packet when there is none yet */
-	ret = zephyr_smp_reassembly_drop(&zst);
+	ret = smp_reassembly_drop(&smpt);
 	zassert_equal(-EINVAL, ret,
 		      "Expected -EINVAL, there is no packet started yet");
 }
@@ -186,20 +186,20 @@ ZTEST(smp_reassembly, test_ud)
 	void *p;
 
 	/* No packet started yet */
-	p = zephyr_smp_reassembly_get_ud(&zst);
+	p = smp_reassembly_get_ud(&smpt);
 	zassert_equal(p, NULL, "Expect NULL ud poiner");
 
 	/* After collecting first fragment */
 	mh->nh_len = sys_cpu_to_be16(TEST_FRAME_SIZE);
 	frag_used = 40;
 	expected = TEST_FRAME_SIZE - frag_used + sizeof(struct mgmt_hdr);
-	ret = zephyr_smp_reassembly_collect(&zst, buff, frag_used);
+	ret = smp_reassembly_collect(&smpt, buff, frag_used);
 	zassert_equal(expected, ret,
 		      "Expected is %d should be %d\n", ret, expected);
 
-	p = zephyr_smp_reassembly_get_ud(&zst);
+	p = smp_reassembly_get_ud(&smpt);
 	zassert_not_equal(p, NULL, "Expect non-NULL ud poiner");
-	zephyr_smp_reassembly_drop(&zst);
+	smp_reassembly_drop(&smpt);
 }
 
 ZTEST_SUITE(smp_reassembly, NULL, NULL, NULL, NULL, NULL);
