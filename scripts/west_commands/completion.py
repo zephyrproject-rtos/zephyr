a20f0d1922cc1e17a459abb274c96163c940608c
Carles Cufi <carles.cufi@nordicsemi.no>
2022-08-31 14:31:35 +0200

    west: Add completion support for zsh
    As part of the overall work on improving west integration with Zephyr,
    add support for zsh completion in the `west completion` command.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- scripts/west_commands/completion.py
+++ scripts/west_commands/completion.py
@@ -11,14 +11,41 @@ from west.commands import WestCommand
 # Relative to the folder where this script lives
 COMPLETION_REL_PATH = 'completion/west-completion'
 
+COMP_DESCRIPTION = '''\
+Output shell completion scripts for west.
+
+This command outputs completion scripts for different shells by printing them
+to stdout. Using the completion scripts:
+
+  bash:
+    # one-time
+    source <(west completion bash)
+    # permanent
+    west completion bash > ~/west-completion.bash
+    # edit your .bashrc or .bash_profile and add:
+    source $HOME/west-completion.bash
+
+  zsh:
+    # one-time
+    source <(west completion zsh)
+    # permanent (might require sudo)
+    west completion zsh > "${fpath[1]}/_west"
+
+positional arguments:
+  source_dir            application source directory
+  cmake_opt             extra options to pass to cmake; implies -c
+                        (these must come after "--" as shown above)
+'''
+
+
 class Completion(WestCommand):
 
     def __init__(self):
         super().__init__(
             'completion',
             # Keep this in sync with the string in west-commands.yml.
-            'display shell completion scripts',
-            'Display shell completion scripts.',
+            'output shell completion scripts',
+            COMP_DESCRIPTION,
             accepts_unknown_args=False)
 
     def do_add_parser(self, parser_adder):
@@ -30,10 +57,9 @@ class Completion(WestCommand):
 
         # Remember to update west-completion.bash if you add or remove
         # flags
-        parser.add_argument('shell', nargs=1, choices=['bash'],
-                            help='''Select the shell that which the completion
-                            script is intended for.
-                            Currently only bash is supported.''')
+        parser.add_argument('shell', nargs=1, choices=['bash', 'zsh'],
+                            help='''Shell that which the completion
+                            script is intended for.''')
         return parser
 
     def do_run(self, args, unknown_args):
