980c22c92db1d6f32ac9ba7ccaec66d3fcdc16cd
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2022-08-31 22:24:04 +0200

    cmake: move deprecated code to FindDeprecated.cmake
    Move deprecated XCC_USE_CLANG setting to deprecated module.
    
    Deprecated and move the use of CROSS_COMPILE to deprecated module as we
    have had ZEPHYR_TOOLCHAIN_VARIANT==cross-compile for a long time.
    
    Deprecated code is now loaded with a single line:
    > find_package(Deprecated COMPONENTS XCC_USE_CLANG CROSS_COMPILE)
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/modules/FindDeprecated.cmake
+++ cmake/modules/FindDeprecated.cmake
@@ -37,7 +37,34 @@ if("${Deprecated_FIND_COMPONENTS}" STREQUAL "")
   message(WARNING "find_package(Deprecated) missing required COMPONENTS keyword")
 endif()
 
-# Currently no code is deprecated.
+if("XCC_USE_CLANG" IN_LIST Deprecated_FIND_COMPONENTS)
+  list(REMOVE_ITEM Deprecated_FIND_COMPONENTS XCC_USE_CLANG)
+  # This code was deprecated after Zephyr v3.0.0
+  # Keep XCC_USE_CLANG behaviour for a while.
+  if(NOT DEFINED ZEPHYR_TOOLCHAIN_VARIANT)
+    set(ZEPHYR_TOOLCHAIN_VARIANT $ENV{ZEPHYR_TOOLCHAIN_VARIANT})
+  endif()
+
+  if ("${ZEPHYR_TOOLCHAIN_VARIANT}" STREQUAL "xcc"
+      AND "$ENV{XCC_USE_CLANG}" STREQUAL "1")
+    set(ZEPHYR_TOOLCHAIN_VARIANT xcc-clang)
+    message(DEPRECATION "XCC_USE_CLANG is deprecated. Please set ZEPHYR_TOOLCHAIN_VARIANT to 'xcc-clang'")
+  endif()
+endif()
+
+if("CROSS_COMPILE" IN_LIST Deprecated_FIND_COMPONENTS)
+  list(REMOVE_ITEM Deprecated_FIND_COMPONENTS CROSS_COMPILE)
+  # This code was deprecated after Zephyr v3.1.0
+  if(NOT DEFINED ZEPHYR_TOOLCHAIN_VARIANT)
+    set(ZEPHYR_TOOLCHAIN_VARIANT $ENV{ZEPHYR_TOOLCHAIN_VARIANT})
+  endif()
+
+  if(NOT ZEPHYR_TOOLCHAIN_VARIANT AND
+     (CROSS_COMPILE OR (DEFINED ENV{CROSS_COMPILE})))
+      set(ZEPHYR_TOOLCHAIN_VARIANT cross-compile)
+      message(DEPRECATION  "CROSS_COMPILE is deprecated. Please set ZEPHYR_TOOLCHAIN_VARIANT to 'cross-compile'")
+  endif()
+endif()
 
 if(NOT "${Deprecated_FIND_COMPONENTS}" STREQUAL "")
   message(STATUS "The following deprecated component(s) could not be found: "
