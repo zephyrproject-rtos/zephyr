74519c35b5f27c5e1d736ada039d7de59eeca011
Jiafei Pan <Jiafei.Pan@nxp.com>
2022-09-05 14:33:24 +0800

    board: mimx8m/n/p: use dts for mmu mapping
    Move MMU mapping for UART4 from mmu_region.c to dts.
    Removed UART2 mmu mapping as it is not used by any board.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- soc/arm64/nxp_imx/mimx8m/mmu_regions.c
+++ soc/arm64/nxp_imx/mimx8m/mmu_regions.c
@@ -6,6 +6,7 @@
 
 #include <zephyr/arch/arm64/arm_mmu.h>
 #include <zephyr/devicetree.h>
+#include <zephyr/linker/devicetree_regions.h>
 #include <zephyr/sys/util.h>
 
 static const struct arm_mmu_region mmu_regions[] = {
@@ -25,25 +26,17 @@ static const struct arm_mmu_region mmu_regions[] = {
 			      DT_REG_SIZE(DT_NODELABEL(ccm)),
 			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS),
 
-	MMU_REGION_FLAT_ENTRY("UART2",
-			      DT_REG_ADDR(DT_NODELABEL(uart2)),
-			      DT_REG_SIZE(DT_NODELABEL(uart2)),
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS),
-
 	MMU_REGION_FLAT_ENTRY("ANA_PLL",
 			      DT_REG_ADDR(DT_NODELABEL(ana_pll)),
 			      DT_REG_SIZE(DT_NODELABEL(ana_pll)),
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS),
-
-	MMU_REGION_FLAT_ENTRY("UART4",
-			      DT_REG_ADDR(DT_NODELABEL(uart4)),
-			      DT_REG_SIZE(DT_NODELABEL(uart4)),
 			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS),
 
 	MMU_REGION_FLAT_ENTRY("IOMUXC",
 			      DT_REG_ADDR(DT_NODELABEL(iomuxc)),
 			      DT_REG_SIZE(DT_NODELABEL(iomuxc)),
 			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS),
+
+	BUILD_DT_REGION_MMU(MMU_REGION_FLAT_ENTRY)
 };
 
 const struct arm_mmu_config mmu_config = {
