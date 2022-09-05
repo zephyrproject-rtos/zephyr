d00c1e78e789f7731e9a717312cc1871b5609839
Ahmed Moheb <ahmed.moheb@nordicsemi.no>
2022-09-05 16:43:26 +0200

    tests: bluetooth: host: Add UT for bt_keys_get_addr()
    Unit test project for bt_keys_get_addr().
    This part of subsys/bluetooth/host/buf.c unit testing
    
    Signed-off-by: Ahmed Moheb <ahmed.moheb@nordicsemi.no>

--- tests/bluetooth/host/keys/mocks/keys_help_utils.c
+++ tests/bluetooth/host/keys/mocks/keys_help_utils.c
@@ -9,6 +9,39 @@
 #include <host/keys.h>
 #include "keys_help_utils.h"
 
+void clear_key_pool(void)
+{
+	struct bt_keys *key_pool;
+
+	key_pool = bt_keys_get_key_pool();
+	memset(key_pool, 0x00, sizeof(struct bt_keys) * CONFIG_BT_MAX_PAIRED);
+}
+
+int fill_key_pool_by_id_addr(const struct id_addr_pair src[], int size, struct bt_keys *refs[])
+{
+	uint8_t id;
+	bt_addr_le_t *addr;
+	struct id_addr_pair const *params_vector;
+
+	if (!check_key_pool_is_empty()) {
+		printk("'%s' Error ! Keys pool isn't empty\n", __func__);
+		return -ENOSR;
+	}
+
+	for (size_t it = 0; it < size; it++) {
+		params_vector = &src[it];
+		id = params_vector->id;
+		addr = params_vector->addr;
+		refs[it] = bt_keys_get_addr(id, addr);
+		if (refs[it] == NULL) {
+			printk("'%s' Failed to add key %d to the keys pool\n", __func__, it);
+			return -ENOBUFS;
+		}
+	}
+
+	return 0;
+}
+
 bool check_key_pool_is_empty(void)
 {
 	int i;
--- tests/bluetooth/host/keys/mocks/keys_help_utils.h
+++ tests/bluetooth/host/keys/mocks/keys_help_utils.h
@@ -14,8 +14,13 @@ struct id_addr_pair {
 
 /* keys.c declarations */
 struct bt_keys *bt_keys_get_key_pool(void);
+#if IS_ENABLED(CONFIG_BT_KEYS_OVERWRITE_OLDEST)
+uint32_t bt_keys_get_aging_counter_val(void);
+#endif
 
 /* keys_help_utils.c declarations */
+void clear_key_pool(void);
+int fill_key_pool_by_id_addr(const struct id_addr_pair src[], int size, struct bt_keys *refs[]);
 bool check_key_pool_is_empty(void);
 
 /* Repeat test entries */
