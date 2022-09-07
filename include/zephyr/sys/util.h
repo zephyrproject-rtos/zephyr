3a9049412e8a26d99095759902e5ad67a1b0143c
Christopher Friedt <cfriedt@fb.com>
2022-09-07 13:09:46 -0400

    util: provide IS_ARRAY_ELEMENT, ARRAY_INDEX, ARRAY_INDEX_FLOOR
    `IS_ARRAY_ELEMENT(array, ptr)` behaves like `PART_OF_ARRAY()`
    except that it checks that `ptr` is aligned to an array-element
    boundary.
    
    `ARRAY_INDEX(array, ptr)` returns the index of `ptr`, where
    `ptr` is aligned to an array-element boundary.
    
    `ARRAY_INDEX_FLOOR(array, ptr)` returns the index of `ptr`,
    where `ptr` does not need to be aligned to an array-element
    boundary.
    
    If `CONFIG_ASSERT=y`, then `ARRAY_INDEX()` and
    `ARRAY_INDEX_FLOOR()` will trigger an assertion for invalid
    `ptr`.
    
    Signed-off-by: Christopher Friedt <cfriedt@fb.com>

--- include/zephyr/sys/util.h
+++ include/zephyr/sys/util.h
@@ -111,17 +111,80 @@ extern "C" {
 #endif /* __cplusplus */
 
 /**
+ * @brief Whether @p ptr is an element of @p array
+ *
+ * This macro can be seen as a slightly stricter version of @ref PART_OF_ARRAY
+ * in that it also ensures that @p ptr is aligned to an array-element boundary
+ * of @p array.
+ *
+ * In C, passing a pointer as @p array causes a compile error.
+ *
+ * @param array the array in question
+ * @param ptr the pointer to check
+ *
+ * @return 1 if @p ptr is part of @p array, 0 otherwise
+ */
+#define IS_ARRAY_ELEMENT(array, ptr)                                                               \
+	((ptr) && POINTER_TO_UINT(array) <= POINTER_TO_UINT(ptr) &&                          \
+	 POINTER_TO_UINT(ptr) < POINTER_TO_UINT(&(array)[ARRAY_SIZE(array)]) &&                    \
+	 (POINTER_TO_UINT(ptr) - POINTER_TO_UINT(array)) % sizeof((array)[0]) == 0)
+
+/**
+ * @brief Index of @p ptr within @p array
+ *
+ * With `CONFIG_ASSERT=y`, this macro will trigger a runtime assertion
+ * when @p ptr does not fall into the range of @p array or when @p ptr
+ * is not aligned to an array-element boundary of @p array.
+ *
+ * In C, passing a pointer as @p array causes a compile error.
+ *
+ * @param array the array in question
+ * @param ptr pointer to an element of @p array
+ *
+ * @return the array index of @p ptr within @p array, on success
+ */
+#define ARRAY_INDEX(array, ptr)                                                                    \
+	({                                                                                         \
+		__ASSERT_NO_MSG(IS_ARRAY_ELEMENT(array, ptr));                                     \
+		(__typeof__((array)[0]) *)(ptr) - (array);                                         \
+	})
+
+/**
  * @brief Check if a pointer @p ptr lies within @p array.
  *
  * In C but not C++, this causes a compile error if @p array is not an array
  * (e.g. if @p ptr and @p array are mixed up).
  *
- * @param ptr a pointer
  * @param array an array
+ * @param ptr a pointer
  * @return 1 if @p ptr is part of @p array, 0 otherwise
  */
-#define PART_OF_ARRAY(array, ptr) \
-	((ptr) && ((ptr) >= &array[0] && (ptr) < &array[ARRAY_SIZE(array)]))
+#define PART_OF_ARRAY(array, ptr)                                                                  \
+	((ptr) && POINTER_TO_UINT(array) <= POINTER_TO_UINT(ptr) &&                                \
+	 POINTER_TO_UINT(ptr) < POINTER_TO_UINT(&(array)[ARRAY_SIZE(array)]))
+
+/**
+ * @brief Array-index of @p ptr within @p array, rounded down
+ *
+ * This macro behaves much like @ref ARRAY_INDEX with the notable
+ * difference that it accepts any @p ptr in the range of @p array rather than
+ * exclusively a @p ptr aligned to an array-element boundary of @p array.
+ *
+ * With `CONFIG_ASSERT=y`, this macro will trigger a runtime assertion
+ * when @p ptr does not fall into the range of @p array.
+ *
+ * In C, passing a pointer as @p array causes a compile error.
+ *
+ * @param array the array in question
+ * @param ptr pointer to an element of @p array
+ *
+ * @return the array index of @p ptr within @p array, on success
+ */
+#define ARRAY_INDEX_FLOOR(array, ptr)                                                              \
+	({                                                                                         \
+		__ASSERT_NO_MSG(PART_OF_ARRAY(array, ptr));                                        \
+		(POINTER_TO_UINT(ptr) - POINTER_TO_UINT(array)) / sizeof((array)[0]);              \
+	})
 
 /**
  * @brief Get a pointer to a structure containing the element
