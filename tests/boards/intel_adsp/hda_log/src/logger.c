47cb3b357c061e33b79eed893c6e2ad1088a8d9b
Tom Burdick <thomas.burdick@intel.com>
2022-09-07 09:26:36 -0500

    logging: adsp hda backend refinements and additional test
    Additional testing showed that when using printk the logger would start
    sticking and spitting out nulls which is wrong. This made it appear as if
    the firmware had locked up. The issue seems to have been caused by the
    initial ipc message to read all the dma buffers on the host.
    
    Removing that, the issue seems to have been solved.
    
    This also improves the test case to ensure printk with LOG_PRINTK=y
    works as expected. It also adds a last log message between some
    timeouts of the flush timer length to ensure the padding and timer
    flush are working properly.
    
    Signed-off-by: Tom Burdick <thomas.burdick@intel.com>

--- tests/boards/intel_adsp/hda_log/src/logger.c
+++ tests/boards/intel_adsp/hda_log/src/logger.c
@@ -14,20 +14,48 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(hda_test, LOG_LEVEL_DBG);
 
+/* Define a prime length message string (13 bytes long when including the \0 terminator)
+ *
+ * This helps ensure most if not all messages are not going to land on a 128 byte boundary
+ * which is important to test the padding and wrapping feature
+ */
+#ifdef CONFIG_LOG_PRINTK
+#define FMT_STR "TEST:%06d\n"
+#else
+#define FMT_STR "TEST:%07d"
+#endif
+
+#define FMT_STR_LEN 13
+
+/* Now define the number of iterations such that we ensure a large number of wraps
+ * on the HDA ring.
+ */
+#define TEST_ITERATIONS ((CONFIG_LOG_BACKEND_ADSP_HDA_SIZE/FMT_STR_LEN)*200)
+
 ZTEST(intel_adsp_hda_log, test_hda_logger)
 {
-	TC_PRINT("Testing hda log backend\n");
+	TC_PRINT("Testing hda log backend, log buffer size %u, iterations %u\n",
+		CONFIG_LOG_BACKEND_ADSP_HDA_SIZE, TEST_ITERATIONS);
 
-	/* Wait a moment so the output isn't mangled */
+	/* Wait a moment so the output isn't mangled with the above printk */
 	k_msleep(100);
 
-	/* Ensure multiple wraps and many many logs are written */
-	for (int i = 0; i < 4096; i++) {
-		LOG_DBG("test hda log message %d", i);
+	for (int i = 0; i < TEST_ITERATIONS; i++) {
+		if (IS_ENABLED(CONFIG_LOG_PRINTK)) {
+			printk(FMT_STR, i);
+		} else {
+			LOG_INF(FMT_STR, i);
+		}
 	}
 
-	/* Wait another moment so dma may flush */
-	k_sleep(K_MSEC(100));
+	/* Wait for the flush to happen */
+	k_msleep(CONFIG_LOG_BACKEND_ADSP_HDA_FLUSH_TIME + 10);
+
+	/* Test the flush timer works by writing a short string */
+	LOG_INF("Timeout flush working if shown");
+
+	/* Wait again for the flush to happen */
+	k_msleep(CONFIG_LOG_BACKEND_ADSP_HDA_FLUSH_TIME + 10);
 }
 
 ZTEST_SUITE(intel_adsp_hda_log, NULL, NULL, NULL, NULL, NULL);
