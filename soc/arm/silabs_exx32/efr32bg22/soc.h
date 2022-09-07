59dd2af5be7b01043d0dd360d84eb967ac564667
Filip Kokosinski <fkokosinski@antmicro.com>
2022-09-07 11:06:30 +0200

    soc: silabs_exx32: Add support for SiLabs EFR32BG22 SoC
    This commit adds support for Silicon Labs EFR32BG22 SoC.
    
    Co-authored-by: Mateusz Sierszulski <msierszulski@antmicro.com>
    Signed-off-by: Filip Kokosinski <fkokosinski@antmicro.com>

---
+++ soc/arm/silabs_exx32/efr32bg22/soc.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021 Sateesh Kotapati
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Register access macros for the EFR32BG22 SoC
+ *
+ */
+
+#ifndef EFR32BG22_SOC_H_
+#define EFR32BG22_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <em_common.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* EFR32BG22_SOC_H_ */
