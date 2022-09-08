d801656f68166d50c7a9c1f4f95e8a1dabb49c76
Fabio Baltieri <fabiobaltieri@google.com>
2022-09-08 11:54:51 +0100

    samples: bt: hids: implement a sample report
    The peripheral_hids sample currently implements the HID descriptor and
    characteristics, but no notify report. Add some code for implementing a
    simple one button click report for boards that have sw0 defined, so that
    the sample actually does something beside connecting and enumerating the
    device.
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

--- samples/bluetooth/peripheral_hids/src/hog.c
+++ samples/bluetooth/peripheral_hids/src/hog.c
@@ -9,6 +9,7 @@
  */
 
 #include <zephyr/types.h>
+#include <zephyr/drivers/gpio.h>
 #include <stddef.h>
 #include <string.h>
 #include <errno.h>
@@ -163,4 +164,34 @@ BT_GATT_SERVICE_DEFINE(hog_svc,
 
 void hog_init(void)
 {
+}
+
+#define SW0_NODE DT_ALIAS(sw0)
+
+void hog_button_loop(void)
+{
+#if DT_NODE_HAS_STATUS(SW0_NODE, okay)
+	const struct gpio_dt_spec sw0 = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+
+	gpio_pin_configure_dt(&sw0, GPIO_INPUT);
+
+	for (;;) {
+		if (simulate_input) {
+			/* HID Report:
+			 * Byte 0: buttons (lower 3 bits)
+			 * Byte 1: X axis (int8)
+			 * Byte 2: Y axis (int8)
+			 */
+			int8_t report[3] = {0, 0, 0};
+
+			if (gpio_pin_get_dt(&sw0)) {
+				report[0] |= BIT(0);
+			}
+
+			bt_gatt_notify(NULL, &hog_svc.attrs[5],
+				       report, sizeof(report));
+		}
+		k_sleep(K_MSEC(100));
+	}
+#endif
 }
