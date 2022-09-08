32a11a2aa397b53becc35ca2bad75003e424d609
Fabio Baltieri <fabiobaltieri@google.com>
2022-09-08 11:37:54 +0100

    samples: bt: hids: set report id in the report_map
    The default report_map for peripheral_hids mouse sample descriptor is
    missing a report id field. This seems to be confusing Android hosts into
    somehow parsing reports incorrectly.
    
    Adding a report id seems to fix the problem and still works correctly on
    bluez hosts.
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

--- samples/bluetooth/peripheral_hids/src/hog.c
+++ samples/bluetooth/peripheral_hids/src/hog.c
@@ -61,6 +61,7 @@ static uint8_t report_map[] = {
 	0x05, 0x01, /* Usage Page (Generic Desktop Ctrls) */
 	0x09, 0x02, /* Usage (Mouse) */
 	0xA1, 0x01, /* Collection (Application) */
+	0x85, 0x01, /*	 Report Id (1) */
 	0x09, 0x01, /*   Usage (Pointer) */
 	0xA1, 0x00, /*   Collection (Physical) */
 	0x05, 0x09, /*     Usage Page (Button) */
