60345c1ddc11a0f5d06ba573c6ba28713abccea6
Ahmed Moheb <ahmed.moheb@nordicsemi.no>
2022-09-08 20:28:46 +0200

    tests: bluetooth: host: Add UT for bt_keys_update_usage()
    Unit test project for bt_keys_update_usage().
    This part of subsys/bluetooth/host/keys.c unit testing.
    
    Signed-off-by: Ahmed Moheb <ahmed.moheb@nordicsemi.no>

--- tests/bluetooth/host/keys/mocks/settings_store_expects.h
+++ tests/bluetooth/host/keys/mocks/settings_store_expects.h
@@ -21,3 +21,11 @@ void expect_single_call_settings_delete(void);
  *   - settings_save_one() to be called once with correct parameters
  */
 void expect_single_call_settings_save_one(const void *value);
+
+/*
+ *  Validate expected behaviour when settings_save_one() isn't called
+ *
+ *  Expected behaviour:
+ *   - settings_save_one() isn't called at all
+ */
+void expect_not_called_settings_save_one(void);
