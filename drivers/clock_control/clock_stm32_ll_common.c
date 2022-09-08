1816e0c7ea3bce463f67bebebe98ecd2cb99ba35
Francois Ramu <francois.ramu@st.com>
2022-09-08 15:42:58 +0200

    drivers: clock_control: stm32 fixed LSE system clock enable
    On some stm32 mcus, the LSE is enabled as system clock (LSESYS)
    only when the LSEON and LSERDY are both set.
    The bit LSESYSEN is set in the RCC BDCR register
    and the driver is waiting for the LSESYSRDY to be set.
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- drivers/clock_control/clock_stm32_ll_common.c
+++ drivers/clock_control/clock_stm32_ll_common.c
@@ -595,18 +595,18 @@ static void set_up_fixed_clock_sources(void)
 		LL_RCC_LSE_SetDriveCapability(STM32_LSE_DRIVING << RCC_BDCR_LSEDRV_Pos);
 #endif
 
+		/* Enable LSE Oscillator (32.768 kHz) */
+		LL_RCC_LSE_Enable();
+		while (!LL_RCC_LSE_IsReady()) {
+			/* Wait for LSE ready */
+		}
+
 #ifdef RCC_BDCR_LSESYSEN
 		LL_RCC_LSE_EnablePropagation();
 		/* Wait till LSESYS is ready */
 		while (!LL_RCC_LSE_IsPropagationReady()) {
 		}
 #endif /* RCC_BDCR_LSESYSEN */
-
-		/* Enable LSE Oscillator (32.768 kHz) */
-		LL_RCC_LSE_Enable();
-		while (!LL_RCC_LSE_IsReady()) {
-			/* Wait for LSE ready */
-		}
 
 		LL_PWR_DisableBkUpAccess();
 
