75346a47f9a322b355f6e5a9f53be4ab62bf1ba6
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2022-09-10 12:38:54 +0530

    Bluetooth: Controller: Fix Rx chan idx when skipping BIS subevents
    Fix missing implementation to correctly to use data channel
    index for subevents when previous subevent of previous BIS
    stream where skipped.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_sync_iso.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_sync_iso.c
@@ -596,6 +596,23 @@ isr_rx_find_subevent:
 		struct lll_sync_iso_stream *stream;
 		uint16_t handle;
 
+		if (skipped) {
+			/* Calculate the Access Address for the current BIS */
+			util_bis_aa_le32(lll->bis_curr, lll->seed_access_addr,
+					 access_addr);
+			data_chan_id = lll_chan_id(access_addr);
+
+			/* Skip channel indices for the previous BIS stream */
+			do {
+				/* Calculate the radio channel to use for ISO event */
+				(void)lll_chan_iso_subevent(data_chan_id,
+						lll->data_chan_map,
+						lll->data_chan_count,
+						&lll->data_chan_prn_s,
+						&lll->data_chan_remap_idx);
+			} while (--skipped);
+		}
+
 		handle = lll->stream_handle[lll->stream_curr];
 		stream = ull_sync_iso_lll_stream_get(handle);
 		if (lll->bis_curr == stream->bis_index) {
