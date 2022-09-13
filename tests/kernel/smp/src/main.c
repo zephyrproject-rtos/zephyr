da099bb3150e959b94b11a29fa54e2ca82cc48cf
Andy Ross <andyross@google.com>
2022-09-13 12:11:36 -0700

    tests/kernel/smp: Correct parameter name
    The k_sys_fatal_error_handler() function is declared in zephyr/fatal.h
    with a name of "esf" for the second parameter, not "pEsf".  For
    unknown reasons, this is showing up in CI as a documentation
    generation failure pointing at the (correct) header.
    
    Still, no reason not to synchronize.
    
    Signed-off-by: Andy Ross <andyross@google.com>

--- tests/kernel/smp/src/main.c
+++ tests/kernel/smp/src/main.c
@@ -659,7 +659,7 @@ ZTEST(smp, test_smp_ipi)
 }
 #endif
 
-void k_sys_fatal_error_handler(unsigned int reason, const z_arch_esf_t *pEsf)
+void k_sys_fatal_error_handler(unsigned int reason, const z_arch_esf_t *esf)
 {
 	static int trigger;
 
