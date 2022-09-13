a332ee317229c6801bada759f4816b5d90f00518
Guillaume Gautier <guillaume.gautier-ext@st.com>
2022-09-13 14:37:31 +0200

    drivers: clock_control: clock_stm32: Add support for lse bypass
    Add support for LSE bypass for all STM32 series
    
    Signed-off-by: Guillaume Gautier <guillaume.gautier-ext@st.com>

--- drivers/clock_control/clock_stm32_ll_common.c
+++ drivers/clock_control/clock_stm32_ll_common.c
@@ -616,6 +616,11 @@ static void set_up_fixed_clock_sources(void)
 		LL_RCC_LSE_SetDriveCapability(STM32_LSE_DRIVING << RCC_BDCR_LSEDRV_Pos);
 #endif
 
+		if (IS_ENABLED(STM32_LSE_BYPASS)) {
+			/* Configure LSE bypass */
+			LL_RCC_LSE_EnableBypass();
+		}
+
 		/* Enable LSE Oscillator (32.768 kHz) */
 		LL_RCC_LSE_Enable();
 		while (!LL_RCC_LSE_IsReady()) {
