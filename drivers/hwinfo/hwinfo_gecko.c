350465b6f43adfc14319afec8a5de593dc3b89f4
Mateusz Sierszulski <msierszulski@antmicro.com>
2022-09-14 09:13:22 +0200

    drivers: hwinfo: Fix Gecko hwinfo driver building
    do not build hwinfo driver for this board - it does not have support for it
    
    Signed-off-by: Mateusz Sierszulski <msierszulski@antmicro.com>

--- drivers/hwinfo/hwinfo_gecko.c
+++ drivers/hwinfo/hwinfo_gecko.c
@@ -37,25 +37,35 @@ int z_impl_hwinfo_get_reset_cause(uint32_t *cause)
 	uint32_t flags = 0;
 	uint32_t rmu_flags = RMU_ResetCauseGet();
 
+#ifdef RMU_RSTCAUSE_PORST
 	if (rmu_flags & RMU_RSTCAUSE_PORST) {
 		flags |= RESET_POR;
 	}
+#endif /* RMU_RSTCAUSE_PORST */
 
+#ifdef RMU_RSTCAUSE_EXTRST
 	if (rmu_flags & RMU_RSTCAUSE_EXTRST) {
 		flags |= RESET_PIN;
 	}
+#endif /* RMU_RSTCAUSE_EXTRST */
 
+#ifdef RMU_RSTCAUSE_SYSREQRST
 	if (rmu_flags & RMU_RSTCAUSE_SYSREQRST) {
 		flags |= RESET_SOFTWARE;
 	}
+#endif /* RMU_RSTCAUSE_SYSREQRST */
 
+#ifdef RMU_RSTCAUSE_LOCKUPRST
 	if (rmu_flags & RMU_RSTCAUSE_LOCKUPRST) {
 		flags |= RESET_CPU_LOCKUP;
 	}
+#endif /* RMU_RSTCAUSE_LOCKUPRST */
 
+#ifdef RMU_RSTCAUSE_WDOGRST
 	if (rmu_flags & RMU_RSTCAUSE_WDOGRST) {
 		flags |= RESET_WATCHDOG;
 	}
+#endif /* RMU_RSTCAUSE_WDOGRST */
 
 #ifdef RMU_RSTCAUSE_EM4WURST
 	if (rmu_flags & RMU_RSTCAUSE_EM4WURST) {
