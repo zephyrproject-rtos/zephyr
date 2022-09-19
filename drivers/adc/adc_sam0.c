c707f1ed28a83d9a0715ca08743d0e2112a3fa95
Kamil Serwus <kserwus@gmail.com>
2022-09-19 20:44:33 +0200

    drivers: adc: sam0: fix compliation for feature SAMs for C21.
    C21 doesn't have ADC_REFCTRL_REFSEL_AREFB and have different APBs.
    
    Signed-off-by: Kamil Serwus <kserwus@gmail.com>

--- drivers/adc/adc_sam0.c
+++ drivers/adc/adc_sam0.c
@@ -153,9 +153,11 @@ static int adc_sam0_channel_setup(const struct device *dev,
 	case ADC_REF_EXTERNAL0:
 		refctrl = ADC_REFCTRL_REFSEL_AREFA;
 		break;
+#ifdef ADC_REFCTRL_REFSEL_AREFB
 	case ADC_REF_EXTERNAL1:
 		refctrl = ADC_REFCTRL_REFSEL_AREFB;
 		break;
+#endif
 	default:
 		LOG_ERR("Selected reference is not valid");
 		return -EINVAL;
@@ -450,7 +452,7 @@ static int adc_sam0_init(const struct device *dev)
 #ifdef MCLK
 	GCLK->PCHCTRL[cfg->gclk_id].reg = cfg->gclk_mask | GCLK_PCHCTRL_CHEN;
 
-	MCLK->APBDMASK.reg |= cfg->mclk_mask;
+	MCLK_ADC |= cfg->mclk_mask;
 #else
 	PM->APBCMASK.bit.ADC_ = 1;
 
