a032bc86a4f1bf20e04749114c1c20de793b06aa
Henrik Brix Andersen <hebad@vestas.com>
2022-09-19 22:49:17 +0200

    drivers: can: provide default callback to can_send() if NULL
    Provide a default, internal callback to CAN controller drivers
    implementation of can_send() if no callback was provided by the caller.
    
    This allows for simplifying the CAN driver implementations of can_send() as
    these no longer need special handling for callback/no callback operation.
    
    Signed-off-by: Henrik Brix Andersen <hebad@vestas.com>

--- include/zephyr/drivers/can.h
+++ include/zephyr/drivers/can.h
@@ -348,6 +348,9 @@ typedef int (*can_set_mode_t)(const struct device *dev, can_mode_t mode);
 /**
  * @brief Callback API upon sending a CAN frame
  * See @a can_send() for argument description
+ *
+ * @note From a driver perspective `callback` will never be `NULL` as a default callback will be
+ * provided if none is provided by the caller. This allows for simplifying the driver handling.
  */
 typedef int (*can_send_t)(const struct device *dev,
 			  const struct can_frame *frame,
@@ -1094,15 +1097,6 @@ __syscall int can_set_bitrate(const struct device *dev, uint32_t bitrate);
 __syscall int can_send(const struct device *dev, const struct can_frame *frame,
 		       k_timeout_t timeout, can_tx_callback_t callback,
 		       void *user_data);
-
-static inline int z_impl_can_send(const struct device *dev, const struct can_frame *frame,
-				  k_timeout_t timeout, can_tx_callback_t callback,
-				  void *user_data)
-{
-	const struct can_driver_api *api = (const struct can_driver_api *)dev->api;
-
-	return api->send(dev, frame, timeout, callback, user_data);
-}
 
 /** @} */
 
