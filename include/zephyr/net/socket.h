d87d1be4c14cbc8fd4b4bde386608aa516530b3c
Robert Lubos <robert.lubos@nordicsemi.no>
2022-09-20 17:15:50 +0200

    net: sockets: Add options to control DSCP/ECN value
    Add new socket options IP_TOS and IPV6_TCLASS which allows to set
    DSCP/ECN values on a socket for an outgoing packet IPv4/IPv6 headers.
    
    The options are compatible with Linux behaviour, where both DSCP and ECN
    are set with a single socket option.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- include/zephyr/net/socket.h
+++ include/zephyr/net/socket.h
@@ -983,9 +983,16 @@ struct ifreq {
 /** sockopt: Disable TCP buffering (ignored, for compatibility) */
 #define TCP_NODELAY 1
 
+/* Socket options for IPPROTO_IP level */
+/** sockopt: Set or receive the Type-Of-Service value for an outgoing packet. */
+#define IP_TOS 1
+
 /* Socket options for IPPROTO_IPV6 level */
 /** sockopt: Don't support IPv4 access (ignored, for compatibility) */
 #define IPV6_V6ONLY 26
+
+/** sockopt: Set or receive the traffic class value for an outgoing packet. */
+#define IPV6_TCLASS 67
 
 /** sockopt: Socket priority */
 #define SO_PRIORITY 12
