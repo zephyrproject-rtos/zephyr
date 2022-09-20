2e0e5e27e87734e61604787376474990fa8f9429
Jonathan Rico <jonathan.rico@nordicsemi.no>
2022-09-20 14:23:24 +0200

    Bluetooth: host: add l2cap stress-test
    This test reproduces more-or-less #34600.
    
    It has a central that connects to multiple peripherals, opens one l2cap
    CoC channel per connection, and transmits a few SDUs largely exceeding
    the MPS of the channel.
    
    In this commit, the test doesn't pass, but when it passes (after the
    subsequent commits), error and warning messages are expected from the
    stack, as this is not the happy path.
    
    We can later debate on whether these particular error messages should
    be downgraded to debug.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>
    (cherry picked from commit 7a6872d83729360d2061957305e68250107c9e64)

--- tests/bluetooth/bsim_bt/compile.sh
+++ tests/bluetooth/bsim_bt/compile.sh
@@ -61,6 +61,7 @@ app=tests/bluetooth/bsim_bt/bsim_test_app conf_file=prj_split_low_lat.conf \
   compile
 app=tests/bluetooth/bsim_bt/bsim_test_multiple compile
 app=tests/bluetooth/bsim_bt/bsim_test_advx compile
+app=tests/bluetooth/bsim_bt/bsim_test_l2cap_stress compile
 app=tests/bluetooth/bsim_bt/bsim_test_iso compile
 app=tests/bluetooth/bsim_bt/bsim_test_audio compile
 app=tests/bluetooth/bsim_bt/edtt_ble_test_app/hci_test_app \
