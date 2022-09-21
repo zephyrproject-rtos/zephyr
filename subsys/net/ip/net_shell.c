ba9ed29f1d88860385151f3d73b2fc013f27fccc
Robert Lubos <robert.lubos@nordicsemi.no>
2022-09-21 17:28:10 +0200

    net: shell: Allow for ping arguments to be specified in hex
    Improve the parse_arg() helper function used with shell ping command,
    by allowing to parse arguments provided in hex.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/net/ip/net_shell.c
+++ subsys/net/ip/net_shell.c
@@ -4104,7 +4104,11 @@ static int parse_arg(size_t *i, size_t argc, char *argv[])
 	}
 
 	errno = 0;
-	res = strtol(str, &endptr, 10);
+	if (strncmp(str, "0x", 2) == 0) {
+		res = strtol(str, &endptr, 16);
+	} else {
+		res = strtol(str, &endptr, 10);
+	}
 
 	if (errno || (endptr == str)) {
 		return -1;
