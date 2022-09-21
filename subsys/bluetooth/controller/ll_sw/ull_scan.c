1bbe209a30fcdab037c8ac9b96c33ea2249665e0
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2022-09-21 09:52:08 +0530

    Bluetooth: Controller: Fix auxiliary scan stop assertion check
    Use a local variable to assert check on auxiliary context's
    release.
    Add missing volatile qualifier to the parent field of the
    auxiliary scan context.
    Under race condition a released aux context can be allocated
    for reception of chain PDU of a periodic sync role, hence
    fix the assertion check to consider that the released
    context can be allocated to other roles.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_scan.c
+++ subsys/bluetooth/controller/ll_sw/ull_scan.c
@@ -639,12 +639,21 @@ uint8_t ull_scan_disable(uint8_t handle, struct ll_scan_set *scan)
 
 		aux_scan = HDR_LLL2ULL(aux_scan_lll);
 		if (aux_scan == scan) {
+			void *parent;
+
 			err = ull_scan_aux_stop(aux);
 			if (err && (err != -EALREADY)) {
 				return BT_HCI_ERR_CMD_DISALLOWED;
 			}
 
-			LL_ASSERT(!aux->parent);
+			/* Use a local variable to assert on auxiliary context's
+			 * release.
+			 * Under race condition a released aux context can be
+			 * allocated for reception of chain PDU of a periodic
+			 * sync role.
+			 */
+			parent = aux->parent;
+			LL_ASSERT(!parent || (parent != aux_scan_lll));
 		}
 	}
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
