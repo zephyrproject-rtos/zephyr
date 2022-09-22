bab80595dfbe58a7e2e9fb4081bfa0c712c63722
Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>
2022-09-22 12:13:59 +0200

    tests: Bluetooth: Mesh: add proxy mix mesh bsim test
    The commit adds proxy mix advertiser test that checks
    mix of transmission between pb gatt, proxy and adv bearers.
    The exact test scenario is described in proxy_mixin.sh file.
    
    Additionally, the separate test configuration has been added.
    All bsim mesh tests have been implemented not considering that
    GATT frames are possible if PB-GATT or GATT Proxy are enabled.
    The solution to avoid this misimplementation is to create
    the separate configuration for tests with enabled
    PB-GATT and GATT Proxy features. The commit adds this as well.
    
    Signed-off-by: Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>
    Signed-off-by: Artur Dobrynin <artur.dobrynin@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_mesh/src/test_advertiser.c
+++ tests/bluetooth/bsim_bt/bsim_test_mesh/src/test_advertiser.c
@@ -8,6 +8,9 @@
 #include <zephyr/bluetooth/hci.h>
 #include "mesh_test.h"
 #include "mesh/adv.h"
+#include "mesh/net.h"
+#include "mesh/mesh.h"
+#include "mesh/foundation.h"
 
 #define LOG_MODULE_NAME test_adv
 
@@ -16,18 +19,40 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME, LOG_LEVEL_INF);
 
 #define WAIT_TIME 60 /*seconds*/
 
+enum bt_mesh_gatt_service {
+	MESH_SERVICE_PROVISIONING,
+	MESH_SERVICE_PROXY,
+};
+
 struct bt_mesh_test_adv {
-	uint8_t retr;
-	int64_t interval;
+	uint8_t retr;     /* number of retransmits of adv frame */
+	int64_t interval; /* interval of transmitted frames */
+};
+
+struct bt_mesh_test_gatt {
+	uint8_t transmits; /* number of frame (pb gatt or proxy beacon) transmits */
+	int64_t interval;  /* interval of transmitted frames */
+	enum bt_mesh_gatt_service service;
+};
+
+extern const struct bt_mesh_comp comp;
+
+static uint8_t test_prov_uuid[16] = { 0x6c, 0x69, 0x6e, 0x67, 0x61, 0xaa };
+
+static const struct bt_mesh_test_cfg adv_cfg = {
+	.addr = 0x0001,
+	.dev_key = { 0x01 },
 };
 
 static struct bt_mesh_send_cb send_cb;
-static struct bt_mesh_test_adv *xmit_param;
-static struct k_sem observer_sem;
+static struct bt_mesh_test_adv xmit_param;
 static const char txt_msg[] = "adv test";
 static const char cb_msg[] = "cb test";
 static int64_t tx_timestamp;
 static int seq_checker;
+static struct bt_mesh_test_gatt gatt_param;
+
+static K_SEM_DEFINE(observer_sem, 0, 1);
 
 static void test_tx_init(void)
 {
@@ -106,17 +131,107 @@ static void seq_end_cb(int err, void *cb_data)
 	}
 }
 
+static void parse_mesh_gatt_preamble(struct net_buf_simple *buf)
+{
+	ASSERT_EQUAL(0x0201, net_buf_simple_pull_be16(buf));
+	/* flags */
+	(void)net_buf_simple_pull_u8(buf);
+	ASSERT_EQUAL(0x0303, net_buf_simple_pull_be16(buf));
+}
+
+static void parse_mesh_pb_gatt_service(struct net_buf_simple *buf)
+{
+	/* Figure 7.1: PB-GATT Advertising Data */
+	/* mesh provisioning service */
+	ASSERT_EQUAL(0x2718, net_buf_simple_pull_be16(buf));
+	ASSERT_EQUAL(0x1516, net_buf_simple_pull_be16(buf));
+	/* mesh provisioning service */
+	ASSERT_EQUAL(0x2718, net_buf_simple_pull_be16(buf));
+}
+
+static void parse_mesh_proxy_service(struct net_buf_simple *buf)
+{
+	/* Figure 7.2: Advertising with Network ID (Identification Type 0x00) */
+	/* mesh proxy service */
+	ASSERT_EQUAL(0x2818, net_buf_simple_pull_be16(buf));
+	ASSERT_EQUAL(0x0c16, net_buf_simple_pull_be16(buf));
+	/* mesh proxy service */
+	ASSERT_EQUAL(0x2818, net_buf_simple_pull_be16(buf));
+	/* network ID */
+	ASSERT_EQUAL(0x00, net_buf_simple_pull_u8(buf));
+}
+
+static void gatt_scan_cb(const bt_addr_le_t *addr, int8_t rssi,
+			  uint8_t adv_type, struct net_buf_simple *buf)
+{
+	static int cnt;
+	static int64_t timestamp;
+
+	if (adv_type != BT_GAP_ADV_TYPE_ADV_IND) {
+		return;
+	}
+
+	parse_mesh_gatt_preamble(buf);
+
+	if (gatt_param.service == MESH_SERVICE_PROVISIONING) {
+		parse_mesh_pb_gatt_service(buf);
+	} else {
+		parse_mesh_proxy_service(buf);
+	}
+
+	if (cnt) {
+		int64_t delta = k_uptime_delta(&timestamp);
+
+		LOG_INF("rx: %s +%d ms", txt_msg, delta);
+
+		ASSERT_TRUE(delta >= gatt_param.interval &&
+			delta < (gatt_param.interval + 15));
+	} else {
+		timestamp = k_uptime_get();
+
+		LOG_INF("rx: %s +0 ms", txt_msg);
+	}
+
+	cnt++;
+
+	if (cnt == gatt_param.transmits) {
+		cnt = 0;
+		timestamp = 0;
+		LOG_INF("rx completed. stop observer.");
+		k_sem_give(&observer_sem);
+	}
+}
+
+static void rx_gatt_beacons(void)
+{
+	struct bt_le_scan_param scan_param = {
+		.type       = BT_HCI_LE_SCAN_PASSIVE,
+		.options    = BT_LE_SCAN_OPT_NONE,
+		.interval   = BT_MESH_ADV_SCAN_UNIT(1000),
+		.window     = BT_MESH_ADV_SCAN_UNIT(1000)
+	};
+	int err;
+
+	err = bt_le_scan_start(&scan_param, gatt_scan_cb);
+	ASSERT_FALSE(err && err != -EALREADY, "starting scan failed (err %d)", err);
+
+	err = k_sem_take(&observer_sem, K_SECONDS(20));
+	ASSERT_OK(err);
+
+	err = bt_le_scan_stop();
+	ASSERT_FALSE(err && err != -EALREADY, "stopping scan failed (err %d)", err);
+}
+
 static void xmit_scan_cb(const bt_addr_le_t *addr, int8_t rssi, uint8_t adv_type,
 			struct net_buf_simple *buf)
 {
 	uint8_t length;
 	static int cnt;
-	static int64_t buff_timestamp;
-	int64_t timestamp = k_uptime_get();
-	static int64_t delta;
+	static int64_t timestamp;
 
-	ASSERT_TRUE(xmit_param, "Xmit checker didn't get parameters");
-	ASSERT_EQUAL(BT_GAP_ADV_TYPE_ADV_NONCONN_IND, adv_type);
+	if (adv_type != BT_GAP_ADV_TYPE_ADV_NONCONN_IND) {
+		return;
+	}
 
 	length = net_buf_simple_pull_u8(buf);
 	ASSERT_EQUAL(buf->len, length);
@@ -127,23 +242,25 @@ static void xmit_scan_cb(const bt_addr_le_t *addr, int8_t rssi, uint8_t adv_type
 
 	ASSERT_EQUAL(0, memcmp(txt_msg, data, sizeof(txt_msg)));
 
-	cnt++;
+	if (cnt) {
+		int64_t delta = k_uptime_delta(&timestamp);
 
-	if (cnt > 1) {
-		delta = k_uptime_delta(&buff_timestamp);
+		LOG_INF("rx: %s +%d ms", txt_msg, delta);
 
-		ASSERT_TRUE(delta >= xmit_param->interval &&
-			delta < (xmit_param->interval + xmit_param->interval / 2));
+		ASSERT_TRUE(delta >= xmit_param.interval &&
+			delta < (xmit_param.interval + 15));
+	} else {
+		timestamp = k_uptime_get();
+
+		LOG_INF("rx: %s +0 ms", txt_msg);
 	}
 
-	LOG_INF("rx: %s +%d ms", txt_msg, delta);
+	cnt++;
 
-	buff_timestamp = timestamp;
-
-	if (cnt > xmit_param->retr) {
+	if (cnt > xmit_param.retr) {
 		cnt = 0;
-		delta = 0;
-		buff_timestamp = 0;
+		timestamp = 0;
+		LOG_INF("rx completed. stop observer.");
 		k_sem_give(&observer_sem);
 	}
 }
@@ -158,13 +275,11 @@ static void rx_xmit_adv(void)
 	};
 	int err;
 
-	k_sem_init(&observer_sem, 0, 1);
 	err = bt_le_scan_start(&scan_param, xmit_scan_cb);
 	ASSERT_FALSE(err && err != -EALREADY, "starting scan failed (err %d)", err);
 
-	err = k_sem_take(&observer_sem,
-		K_MSEC(((xmit_param->retr + 1) * xmit_param->interval) + 10));
-	ASSERT_OK(err, "Didn't receive adv in time");
+	err = k_sem_take(&observer_sem, K_SECONDS(20));
+	ASSERT_OK(err);
 
 	err = bt_le_scan_stop();
 	ASSERT_FALSE(err && err != -EALREADY, "stopping scan failed (err %d)", err);
@@ -185,7 +300,6 @@ static void test_tx_cb_single(void)
 	send_cb.start = single_start_cb;
 	send_cb.end = single_end_cb;
 
-	k_sem_init(&observer_sem, 0, 1);
 	net_buf_add_mem(buf, txt_msg, sizeof(txt_msg));
 	seq_checker = 0;
 	tx_timestamp = k_uptime_get();
@@ -200,14 +314,12 @@ static void test_tx_cb_single(void)
 
 static void test_rx_xmit(void)
 {
-	struct bt_mesh_test_adv param = {
-		.retr = 2,
-		.interval = 20
-	};
+	xmit_param.retr = 2;
+	xmit_param.interval = 20;
 
 	bt_init();
-	xmit_param = &param;
 	rx_xmit_adv();
+
 	PASS();
 }
 
@@ -219,7 +331,6 @@ static void test_tx_cb_multi(void)
 
 	bt_init();
 	adv_init();
-	k_sem_init(&observer_sem, 0, 1);
 
 	/* Allocate all network buffers. */
 	for (int i = 0; i < CONFIG_BT_MESH_ADV_BUF_COUNT; i++) {
@@ -262,6 +373,85 @@ static void test_tx_cb_multi(void)
 	PASS();
 }
 
+static void test_tx_proxy_mixin(void)
+{
+	static struct bt_mesh_prov prov = {
+		.uuid = test_prov_uuid,
+	};
+	uint8_t status;
+	int err;
+
+	/* Initialize mesh stack and enable pb gatt bearer to emit beacons. */
+	bt_mesh_device_setup(&prov, &comp);
+	err = bt_mesh_prov_enable(BT_MESH_PROV_GATT);
+	ASSERT_OK(err, "Failed to enable GATT provisioner");
+
+	/* Let the tester to measure an interval between advertisements.
+	 * The node should advertise pb gatt service with 100 msec interval.
+	 */
+	k_sleep(K_MSEC(1800));
+
+	LOG_INF("Provision device under test");
+	/* Provision dut and start gatt proxy beacons. */
+	bt_mesh_provision(test_net_key, 0, 0, 0, adv_cfg.addr, adv_cfg.dev_key);
+	/* Disable secured network beacons to exclude influence of them on proxy beaconing. */
+	ASSERT_OK(bt_mesh_cfg_cli_beacon_set(0, adv_cfg.addr, BT_MESH_BEACON_DISABLED, &status));
+	ASSERT_EQUAL(BT_MESH_BEACON_DISABLED, status);
+
+	/* Let the tester to measure an interval between advertisements.
+	 * The node should advertise proxy service with 1 second interval.
+	 */
+	k_sleep(K_MSEC(6000));
+
+	/* Send a mesh message while advertising proxy service.
+	 * Advertising the proxy service should be resumed after
+	 * finishing advertising the message.
+	 */
+	struct net_buf *buf = bt_mesh_adv_create(BT_MESH_ADV_DATA, BT_MESH_LOCAL_ADV,
+			BT_MESH_TRANSMIT(5, 20), K_NO_WAIT);
+	net_buf_add_mem(buf, txt_msg, sizeof(txt_msg));
+	bt_mesh_adv_send(buf, NULL, NULL);
+	k_sleep(K_MSEC(150));
+
+	/* Let the tester to measure an interval between advertisements again. */
+	k_sleep(K_MSEC(6000));
+
+	PASS();
+}
+
+static void test_rx_proxy_mixin(void)
+{
+	/* (total transmit duration) / (transmit interval) */
+	gatt_param.transmits = 1500 / 100;
+	gatt_param.interval = 100;
+	gatt_param.service = MESH_SERVICE_PROVISIONING;
+
+	bt_init();
+
+	/* Scan pb gatt beacons. */
+	rx_gatt_beacons();
+
+	/* Delay to provision dut */
+	k_sleep(K_MSEC(1000));
+
+	/* Scan proxy beacons. */
+	/* (total transmit duration) / (transmit interval) */
+	gatt_param.transmits = 5000 / 1000;
+	gatt_param.interval = 1000;
+	gatt_param.service = MESH_SERVICE_PROXY;
+	rx_gatt_beacons();
+
+	/* Scan adv data. */
+	xmit_param.retr = 5;
+	xmit_param.interval = 20;
+	rx_xmit_adv();
+
+	/* Scan proxy beacons again. */
+	rx_gatt_beacons();
+
+	PASS();
+}
+
 #define TEST_CASE(role, name, description)                     \
 	{                                                      \
 		.test_id = "adv_" #role "_" #name,             \
@@ -272,10 +462,13 @@ static void test_tx_cb_multi(void)
 	}
 
 static const struct bst_test_instance test_adv[] = {
-	TEST_CASE(tx, cb_single, "ADV: tx cb parameter checker"),
-	TEST_CASE(tx, cb_multi,  "ADV: tx cb sequence checker"),
+	TEST_CASE(tx, cb_single,   "ADV: tx cb parameter checker"),
+	TEST_CASE(tx, cb_multi,    "ADV: tx cb sequence checker"),
+	TEST_CASE(tx, proxy_mixin, "ADV: proxy mix-in gatt adv"),
 
-	TEST_CASE(rx, xmit,    "ADV: xmit checker"),
+	TEST_CASE(rx, xmit,        "ADV: xmit checker"),
+	TEST_CASE(rx, proxy_mixin, "ADV: proxy mix-in scanner"),
+
 	BSTEST_END_MARKER
 };
 
