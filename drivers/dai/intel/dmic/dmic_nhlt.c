39c2007b04ec12a6d1ff9eda67e15aa9de0582b9
Damian Nikodem <damian.nikodem@intel.com>
2022-09-23 09:13:35 +0200

    drivers: dmic: update dmic flow initialization
    DMIC configuration should allow the use of periodic start.
    
    Signed-off-by: Damian Nikodem <damian.nikodem@intel.com>

--- drivers/dai/intel/dmic/dmic_nhlt.c
+++ drivers/dai/intel/dmic/dmic_nhlt.c
@@ -436,15 +436,17 @@ int dai_dmic_set_config_nhlt(struct dai_intel_dmic *dmic, const void *bespoke_cf
 			val = fir_cfg_a[n]->fir_control;
 			bf1 = FIR_CONTROL_A_START_GET(val);
 			bf2 = FIR_CONTROL_A_ARRAY_START_EN_GET(val);
-			bf3 = FIR_CONTROL_A_DCCOMP_GET(val);
-			bf4 = FIR_CONTROL_A_MUTE_GET(val);
-			bf5 = FIR_CONTROL_A_STEREO_GET(val);
+			bf3 = FIR_CONTROL_A_PERIODIC_START_EN_GET(val);
+			bf4 = FIR_CONTROL_A_DCCOMP_GET(val);
+			bf5 = FIR_CONTROL_A_MUTE_GET(val);
+			bf6 = FIR_CONTROL_A_STEREO_GET(val);
 			LOG_DBG("dmic_set_config_nhlt(): FIR_CONTROL_A = %08x", val);
-			LOG_DBG("  start=%d, array_start_en=%d, dccomp=%d", bf1, bf2, bf3);
-			LOG_DBG("  mute=%d, stereo=%d", bf4, bf5);
+			LOG_DBG("  start=%d, array_start_en=%d, periodic_start_en=%d",
+				bf1, bf2, bf3);
+			LOG_DBG("  dccomp=%d, mute=%d, stereo=%d", bf4, bf5, bf6);
 			ref = FIR_CONTROL_A_START(bf1) | FIR_CONTROL_A_ARRAY_START_EN(bf2) |
-				FIR_CONTROL_A_DCCOMP(bf3) | FIR_CONTROL_A_MUTE(bf4) |
-				FIR_CONTROL_A_STEREO(bf5);
+				FIR_CONTROL_A_PERIODIC_START_EN(bf3) | FIR_CONTROL_A_DCCOMP(bf4) |
+				FIR_CONTROL_A_MUTE(bf5) | FIR_CONTROL_A_STEREO(bf6);
 
 			if (ref != val) {
 				LOG_ERR("dmic_set_config_nhlt(): illegal FIR_CONTROL = 0x%08x",
@@ -496,12 +498,14 @@ int dai_dmic_set_config_nhlt(struct dai_intel_dmic *dmic, const void *bespoke_cf
 			val = fir_cfg_b[n]->fir_control;
 			bf1 = FIR_CONTROL_B_START_GET(val);
 			bf2 = FIR_CONTROL_B_ARRAY_START_EN_GET(val);
-			bf3 = FIR_CONTROL_B_DCCOMP_GET(val);
+			bf3 = FIR_CONTROL_B_PERIODIC_START_EN_GET(val);
+			bf4 = FIR_CONTROL_B_DCCOMP_GET(val);
 			bf5 = FIR_CONTROL_B_MUTE_GET(val);
 			bf6 = FIR_CONTROL_B_STEREO_GET(val);
 			LOG_DBG("dmic_set_config_nhlt(): FIR_CONTROL_B = %08x", val);
-			LOG_DBG("  start=%d, array_start_en=%d, dccomp=%d", bf1, bf2, bf3);
-			LOG_DBG("  mute=%d, stereo=%d", bf5, bf6);
+			LOG_DBG("  start=%d, array_start_en=%d, periodic_start_en=%d",
+				bf1, bf2, bf3);
+			LOG_DBG("  dccomp=%d, mute=%d, stereo=%d", bf4, bf5, bf6);
 
 			/* Clear START, set MUTE */
 			fir_control = (val & ~FIR_CONTROL_B_START_BIT) | FIR_CONTROL_B_MUTE_BIT;
