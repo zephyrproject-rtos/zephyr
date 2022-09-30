e51590bd2a0a0b317eebca4658a52d0ab852be59
Jay Vasanth <jay.vasanth@microchip.com>
2022-09-30 17:12:39 -0400

    drivers: uart: microchip: add support for mec15xx
    update uart mchp xec driver to support mec15xx and add
    pinctrl support for mec15xx uart
    
    Signed-off-by: Jay Vasanth <jay.vasanth@microchip.com>

--- drivers/serial/uart_mchp_xec.c
+++ drivers/serial/uart_mchp_xec.c
@@ -24,15 +24,14 @@
 #include <zephyr/init.h>
 #include <zephyr/toolchain.h>
 #include <zephyr/linker/sections.h>
+#ifdef CONFIG_SOC_SERIES_MEC172X
 #include <zephyr/drivers/clock_control/mchp_xec_clock_control.h>
 #include <zephyr/drivers/interrupt_controller/intc_mchp_xec_ecia.h>
+#endif
 #include <zephyr/drivers/pinctrl.h>
 #include <zephyr/drivers/uart.h>
 #include <zephyr/sys/sys_io.h>
 #include <zephyr/spinlock.h>
-
-BUILD_ASSERT(IS_ENABLED(CONFIG_SOC_SERIES_MEC172X),
-	     "XEC UART driver only support MEC172x at this time");
 
 /* Clock source is 1.8432 MHz derived from PLL 48 MHz */
 #define XEC_UART_CLK_SRC_1P8M		0
@@ -194,6 +193,56 @@ struct uart_xec_dev_data {
 };
 
 static const struct uart_driver_api uart_xec_driver_api;
+
+#ifdef CONFIG_SOC_SERIES_MEC172X
+
+static void uart_clr_slp_en(const struct device *dev)
+{
+	struct uart_xec_device_config const *dev_cfg = dev->config;
+
+	z_mchp_xec_pcr_periph_sleep(dev_cfg->pcr_idx, dev_cfg->pcr_bitpos, 0);
+}
+
+static inline void uart_xec_girq_clr(const struct device *dev)
+{
+	struct uart_xec_device_config const *dev_cfg = dev->config;
+
+	mchp_soc_ecia_girq_src_clr(dev_cfg->girq_id, dev_cfg->girq_pos);
+}
+
+static inline void uart_xec_girq_en(uint8_t girq_idx, uint8_t girq_posn)
+{
+	mchp_xec_ecia_girq_src_en(girq_idx, girq_posn);
+}
+
+#else
+
+static void uart_clr_slp_en(const struct device *dev)
+{
+	struct uart_xec_device_config const *dev_cfg = dev->config;
+
+	if (dev_cfg->pcr_bitpos == MCHP_PCR2_UART0_POS) {
+		mchp_pcr_periph_slp_ctrl(PCR_UART0, 0);
+	} else if (dev_cfg->pcr_bitpos == MCHP_PCR2_UART1_POS) {
+		mchp_pcr_periph_slp_ctrl(PCR_UART1, 0);
+	} else {
+		mchp_pcr_periph_slp_ctrl(PCR_UART2, 0);
+	}
+}
+
+static inline void uart_xec_girq_clr(const struct device *dev)
+{
+	struct uart_xec_device_config const *dev_cfg = dev->config;
+
+	MCHP_GIRQ_SRC(dev_cfg->girq_id) = BIT(dev_cfg->girq_pos);
+}
+
+static inline void uart_xec_girq_en(uint8_t girq_idx, uint8_t girq_posn)
+{
+	MCHP_GIRQ_ENSET(girq_idx) = BIT(girq_posn);
+}
+
+#endif
 
 static void set_baud_rate(const struct device *dev, uint32_t baud_rate)
 {
@@ -363,11 +412,7 @@ static int uart_xec_init(const struct device *dev)
 	struct uart_xec_dev_data *dev_data = dev->data;
 	int ret;
 
-	ret = z_mchp_xec_pcr_periph_sleep(dev_cfg->pcr_idx,
-					  dev_cfg->pcr_bitpos, 0);
-	if (ret != 0) {
-		return ret;
-	}
+	uart_clr_slp_en(dev);
 
 	ret = pinctrl_apply_state(dev_cfg->pcfg, PINCTRL_STATE_DEFAULT);
 	if (ret != 0) {
@@ -750,7 +795,6 @@ static void uart_xec_irq_callback_set(const struct device *dev,
  */
 static void uart_xec_isr(const struct device *dev)
 {
-	const struct uart_xec_device_config * const dev_cfg = dev->config;
 	struct uart_xec_dev_data * const dev_data = dev->data;
 
 	if (dev_data->cb) {
@@ -758,7 +802,7 @@ static void uart_xec_isr(const struct device *dev)
 	}
 
 	/* clear ECIA GIRQ R/W1C status bit after UART status cleared */
-	mchp_xec_ecia_girq_src_clr(dev_cfg->girq_id, dev_cfg->girq_pos);
+	uart_xec_girq_clr(dev);
 }
 
 #endif /* CONFIG_UART_INTERRUPT_DRIVEN */
@@ -862,7 +906,7 @@ static const struct uart_driver_api uart_xec_driver_api = {
 			    uart_xec_isr, DEVICE_DT_INST_GET(n),	\
 			    0);						\
 		irq_enable(DT_INST_IRQN(n));				\
-		mchp_xec_ecia_girq_src_en(DT_INST_PROP_BY_IDX(n, girqs, 0), \
+		uart_xec_girq_en(DT_INST_PROP_BY_IDX(n, girqs, 0), \
 					  DT_INST_PROP_BY_IDX(n, girqs, 1)); \
 	}
 #else
