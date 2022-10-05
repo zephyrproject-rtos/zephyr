da44dcce6e73ee0196016475fb74130c831bb0b9
Ruibin Chang <Ruibin.Chang@ite.com.tw>
2022-10-05 14:05:43 +0800

    ITE drivers/interrupt_controller: add intc_ite_it8xxx2_v2 driver
    This driver is made for it82xx2 series.
    
    Signed-off-by: Ruibin Chang <Ruibin.Chang@ite.com.tw>

--- soc/riscv/riscv-ite/common/soc_common.h
+++ soc/riscv/riscv-ite/common/soc_common.h
@@ -27,7 +27,7 @@
 
 #ifndef _ASMLANGUAGE
 
-#if CONFIG_ITE_IT8XXX2_INTC
+#ifdef CONFIG_HAS_ITE_INTC
 /*
  * Save current interrupt state of soc-level into ier_setting[] with
  * disabling interrupt.
@@ -44,7 +44,7 @@ extern void ite_intc_irq_polarity_set(unsigned int irq, unsigned int flags);
 extern void ite_intc_isr_clear(unsigned int irq);
 void ite_intc_init(void);
 bool ite_intc_no_irq(void);
-#endif /* CONFIG_ITE_IT8XXX2_INTC */
+#endif /* CONFIG_HAS_ITE_INTC */
 
 #ifdef CONFIG_SOC_IT8XXX2_PLL_FLASH_48M
 void timer_5ms_one_shot(void);
