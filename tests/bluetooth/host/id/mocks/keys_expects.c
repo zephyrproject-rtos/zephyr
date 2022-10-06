e6356c2770839c482b6e7435aa961c25d0ee68b7
Ahmed Moheb <ahmed.moheb@nordicsemi.no>
2022-10-06 17:03:22 +0200

    tests: bluetooth: host: Add UT for bt_id_del()
    Unit test project for bt_id_del().
    This part of subsys/bluetooth/host/id.c unit testing.
    
    Signed-off-by: Ahmed Moheb <ahmed.moheb@nordicsemi.no>

--- tests/bluetooth/host/id/mocks/keys_expects.c
+++ tests/bluetooth/host/id/mocks/keys_expects.c
@@ -29,3 +29,24 @@ void expect_not_called_bt_keys_find_irk(void)
 	zassert_equal(bt_keys_find_irk_fake.call_count, 0, "'%s()' was called unexpectedly",
 		      func_name);
 }
+
+void expect_single_call_bt_keys_foreach_type(enum bt_keys_type type)
+{
+	const char *func_name = "bt_keys_foreach_type";
+
+	zassert_equal(bt_keys_foreach_type_fake.call_count, 1, "'%s()' was called more than once",
+		      func_name);
+
+	zassert_equal(bt_keys_foreach_type_fake.arg0_val, type,
+		      "'%s()' was called with incorrect '%s' value", func_name, "type");
+	zassert_not_null(bt_keys_foreach_type_fake.arg1_val,
+			 "'%s()' was called with incorrect '%s' value", func_name, "func");
+}
+
+void expect_not_called_bt_keys_foreach_type(void)
+{
+	const char *func_name = "bt_keys_foreach_type";
+
+	zassert_equal(bt_keys_foreach_type_fake.call_count, 0, "'%s()' was called unexpectedly",
+		      func_name);
+}
