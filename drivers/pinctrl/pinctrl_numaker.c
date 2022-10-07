d903cf800469b5468ff7284b8f16ff81cbccb09d
cyliangtw <cyliang@nuvoton.com>
2022-10-07 17:50:28 +0800

    NuMaker M46x MFP base address from pinctrl reg-addr

--- drivers/pinctrl/pinctrl_numaker.c
+++ drivers/pinctrl/pinctrl_numaker.c
@@ -24,8 +24,10 @@ static void configure_pin(nvt_pinctrl_pin_t pin)
     uint32_t port_index = PORT_INDEX(pin.pinMux);
     uint32_t mfp_cfg = MFP_CFG(pin.pinMux);
     uint32_t pin_cfg = pin.cfgData;
+    /* Get mfp_base, it should be == (&SYS->GPA_MFP0) in M467 */
+    uint32_t mfp_base = DT_REG_ADDR(DT_NODELABEL(pinctrl));
 
-    uint32_t *GPx_MFPx = ((uint32_t *) &SYS->GPA_MFP0) + port_index * 4 + (pin_index / 4);
+    uint32_t *GPx_MFPx = ((uint32_t *) mfp_base) + port_index * 4 + (pin_index / 4);
     uint32_t pinMask = NU_MFP_MASK(pin_index);
 
     // E.g.: SYS->GPA_MFP0  = (SYS->GPA_MFP0 & (~SYS_GPA_MFP0_PA0MFP_Msk) ) | SYS_GPA_MFP0_PA0MFP_SC0_CD  ;
