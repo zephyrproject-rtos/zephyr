36098ceef10c95bfcfff1899e8f3815075c806d1
Titouan Christophe <moiandme@gmail.com>
2022-10-07 12:11:51 +0200

    drivers: sensor: vl53l0x: add support for interrupt-gpios
    - Add a new optional device tree property `interrupt-gpios`
    - When this property is defined, sensor_sample_fetch() now waits for an
      interrupt from the sensor, rather than actively polling for completion
    - Add implementation for sensor_trigger_set()
    
    Signed-off-by: Titouan Christophe <moiandme@gmail.com>

--- drivers/sensor/vl53l0x/vl53l0x.c
+++ drivers/sensor/vl53l0x/vl53l0x.c
@@ -9,6 +9,7 @@
  */
 
 #include "vl53l0x_api.h"
+#include "vl53l0x_api_core.h"
 #include "vl53l0x_platform.h"
 
 #include <errno.h>
@@ -42,13 +43,20 @@ LOG_MODULE_REGISTER(VL53L0X, CONFIG_SENSOR_LOG_LEVEL);
 
 struct vl53l0x_config {
 	struct i2c_dt_spec i2c;
+	struct gpio_dt_spec interrupt;
 	struct gpio_dt_spec xshut;
 };
 
 struct vl53l0x_data {
+	const struct device *dev;
 	bool started;
 	VL53L0X_Dev_t vl53l0x;
 	VL53L0X_RangingMeasurementData_t measurement;
+	VL53L0X_DeviceModes current_mode;
+	struct gpio_callback interrupt_cb;
+	struct k_work interrupt_work;
+	struct k_sem wait_for_interrupt;
+	sensor_trigger_handler_t data_ready_handler;
 };
 
 static inline bool vl53l0x_supports_chan(enum sensor_channel chan)
@@ -63,7 +71,53 @@ static inline bool vl53l0x_supports_chan(enum sensor_channel chan)
 	}
 }
 
-static int vl53l0x_setup_single_shot(const struct device *dev)
+static inline bool vl53l0x_has_interrupt(const struct device *dev)
+{
+	const struct vl53l0x_config *const config = dev->config;
+
+	return config->interrupt.port != NULL;
+}
+
+static void vl53l0x_interrupt_callback(const struct device *port, struct gpio_callback *cb,
+				       uint32_t pin)
+{
+	struct vl53l0x_data *drv_data = CONTAINER_OF(cb, struct vl53l0x_data, interrupt_cb);
+
+	ARG_UNUSED(port);
+	ARG_UNUSED(pin);
+	k_work_submit(&drv_data->interrupt_work);
+	LOG_DBG("[%s] Got interrupt !", drv_data->dev->name);
+}
+
+static void vl53l0x_interrupt_work(struct k_work *work)
+{
+	struct sensor_trigger trig = {
+		.type = SENSOR_TRIG_DATA_READY,
+		.chan = SENSOR_CHAN_DISTANCE,
+	};
+	struct vl53l0x_data *drv_data = CONTAINER_OF(work, struct vl53l0x_data, interrupt_work);
+	uint32_t reason;
+	int r;
+
+	r = VL53L0X_GetInterruptMaskStatus(&drv_data->vl53l0x, &reason);
+	if (r) {
+		LOG_ERR("[%s] VL53L0X_GetInterruptMaskStatus failed", drv_data->dev->name);
+		return;
+	}
+
+	k_sem_give(&drv_data->wait_for_interrupt);
+
+	if (reason == VL53L0X_GPIOFUNCTIONALITY_NEW_MEASURE_READY && drv_data->data_ready_handler) {
+		drv_data->data_ready_handler(drv_data->dev, &trig);
+	}
+
+	r = VL53L0X_ClearInterruptMask(&drv_data->vl53l0x, 0);
+	if (r) {
+		LOG_ERR("[%s] VL53L0X_ClearInterruptMask failed", drv_data->dev->name);
+	}
+}
+
+static int vl53l0x_configure(const struct device *dev)
 {
 	struct vl53l0x_data *drv_data = dev->data;
 	int ret;
@@ -88,12 +142,6 @@ static int vl53l0x_setup_single_shot(const struct device *dev)
 					       &isApertureSpads);
 	if (ret) {
 		LOG_ERR("[%s] VL53L0X_PerformRefSpadManagement failed", dev->name);
-		goto exit;
-	}
-
-	ret = VL53L0X_SetDeviceMode(&drv_data->vl53l0x, VL53L0X_DEVICEMODE_SINGLE_RANGING);
-	if (ret) {
-		LOG_ERR("[%s] VL53L0X_SetDeviceMode failed", dev->name);
 		goto exit;
 	}
 
@@ -150,6 +198,19 @@ static int vl53l0x_setup_single_shot(const struct device *dev)
 
 exit:
 	return ret;
+}
+
+static int vl53l0x_set_ranging_mode(const struct device *dev, VL53L0X_DeviceModes mode)
+{
+	struct vl53l0x_data *drv_data = dev->data;
+	int ret = VL53L0X_SetDeviceMode(&drv_data->vl53l0x, mode);
+
+	if (ret) {
+		LOG_ERR("[%s] VL53L0X_SetDeviceMode failed", dev->name);
+		return ret;
+	}
+	drv_data->current_mode = mode;
+	return 0;
 }
 
 static int vl53l0x_start(const struct device *dev)
@@ -216,9 +277,31 @@ static int vl53l0x_start(const struct device *dev)
 		return -ENOTSUP;
 	}
 
-	ret = vl53l0x_setup_single_shot(dev);
+	ret = vl53l0x_configure(dev);
 	if (ret < 0) {
 		return -ENOTSUP;
+	}
+
+	ret = vl53l0x_set_ranging_mode(dev, VL53L0X_DEVICEMODE_SINGLE_RANGING);
+	if (ret) {
+		return -EIO;
+	}
+
+	if (vl53l0x_has_interrupt(dev)) {
+		r = VL53L0X_SetGpioConfig(&drv_data->vl53l0x, 0, VL53L0X_DEVICEMODE_SINGLE_RANGING,
+					  VL53L0X_GPIOFUNCTIONALITY_NEW_MEASURE_READY,
+					  VL53L0X_INTERRUPTPOLARITY_HIGH);
+		if (r < 0) {
+			LOG_ERR("[%s] Unable to setup interrupt config on device: %d", dev->name,
+				r);
+			return -EIO;
+		}
+
+		gpio_init_callback(&drv_data->interrupt_cb, vl53l0x_interrupt_callback,
+				   BIT(config->interrupt.pin));
+		gpio_add_callback(config->interrupt.port, &drv_data->interrupt_cb);
+		gpio_pin_interrupt_configure_dt(&config->interrupt, GPIO_INT_EDGE_TO_ACTIVE);
+		LOG_DBG("[%s] Interrupt configured", dev->name);
 	}
 
 	drv_data->started = true;
@@ -229,7 +312,6 @@ static int vl53l0x_start(const struct device *dev)
 static int vl53l0x_sample_fetch(const struct device *dev, enum sensor_channel chan)
 {
 	struct vl53l0x_data *drv_data = dev->data;
-	VL53L0X_Error ret;
 	int r;
 
 	if (!vl53l0x_supports_chan(chan)) {
@@ -243,9 +325,26 @@ static int vl53l0x_sample_fetch(const struct device *dev, enum sensor_channel ch
 		}
 	}
 
-	ret = VL53L0X_PerformSingleRangingMeasurement(&drv_data->vl53l0x, &drv_data->measurement);
-	if (ret < 0) {
-		LOG_ERR("[%s] Could not perform measurment (error=%d)", dev->name, ret);
+	if (drv_data->current_mode == VL53L0X_DEVICEMODE_SINGLE_RANGING) {
+		r = VL53L0X_StartMeasurement(&drv_data->vl53l0x);
+		if (r) {
+			LOG_ERR("[%s] VL53L0X_PerformSingleMeasurement failed", dev->name);
+			return -EIO;
+		}
+	}
+
+	if (vl53l0x_has_interrupt(dev)) {
+		LOG_DBG("[%s] Waiting for interrupt", dev->name);
+		k_sem_take(&drv_data->wait_for_interrupt, K_FOREVER);
+	} else {
+		LOG_DBG("[%s] Polling for measurement completion", dev->name);
+		VL53L0X_measurement_poll_for_completion(&drv_data->vl53l0x);
+	}
+
+	LOG_DBG("[%s] Getting measurement data", dev->name);
+	r = VL53L0X_GetRangingMeasurementData(&drv_data->vl53l0x, &drv_data->measurement);
+	if (r) {
+		LOG_ERR("[%s] VL53L0X_GetRangingMeasurementData failed", dev->name);
 		return -EINVAL;
 	}
 
@@ -305,7 +404,7 @@ static int vl53l0x_set_sampling_freq(const struct device *dev, const struct sens
 
 	timing = USEC_PER_SEC * USEC_PER_SEC / freq_microhertz;
 
-	r = VL53L0X_SetMeasurementTimingBudgetMicroSeconds(&drv_data->vl53l0x, (uint32_t) timing);
+	r = VL53L0X_SetMeasurementTimingBudgetMicroSeconds(&drv_data->vl53l0x, (uint32_t)timing);
 	if (r) {
 		LOG_ERR("[%s] Unable to set measurement timing budget", dev->name);
 		return r;
@@ -363,11 +462,54 @@ static int vl53l0x_attr_set(const struct device *dev, enum sensor_channel chan,
 	}
 }
 
+int vl53l0x_trigger_set(const struct device *dev, const struct sensor_trigger *trig,
+			sensor_trigger_handler_t handler)
+{
+	struct vl53l0x_data *drv_data = dev->data;
+	bool was_in_continuous_mode =
+		(drv_data->current_mode == VL53L0X_DEVICEMODE_CONTINUOUS_RANGING);
+	bool to_continuous_mode = (handler != NULL);
+
+	if (!vl53l0x_has_interrupt(dev)) {
+		return -ENOTSUP;
+	}
+
+	if (trig->type != SENSOR_TRIG_DATA_READY) {
+		return -ENOTSUP;
+	}
+
+	if (!vl53l0x_supports_chan(trig->chan)) {
+		return -ENOTSUP;
+	}
+
+	drv_data->data_ready_handler = handler;
+
+	if (!was_in_continuous_mode && to_continuous_mode) {
+		if (vl53l0x_set_ranging_mode(dev, VL53L0X_DEVICEMODE_CONTINUOUS_RANGING)) {
+			return -EIO;
+		}
+		if (VL53L0X_StartMeasurement(&drv_data->vl53l0x)) {
+			LOG_ERR("[%s] VL53L0X_StartMeasurement failed", dev->name);
+			return -EIO;
+		}
+	} else if (was_in_continuous_mode && !to_continuous_mode) {
+		if (VL53L0X_StopMeasurement(&drv_data->vl53l0x)) {
+			LOG_ERR("[%s] VL53L0X_StopMeasurement failed", dev->name);
+			return -EIO;
+		}
+		if (vl53l0x_set_ranging_mode(dev, VL53L0X_DEVICEMODE_SINGLE_RANGING)) {
+			return -EIO;
+		}
+	}
+	return 0;
+}
+
 static const struct sensor_driver_api vl53l0x_api_funcs = {
 	.sample_fetch = vl53l0x_sample_fetch,
 	.channel_get = vl53l0x_channel_get,
 	.attr_get = vl53l0x_attr_get,
 	.attr_set = vl53l0x_attr_set,
+	.trigger_set = vl53l0x_trigger_set,
 };
 
 static int vl53l0x_init(const struct device *dev)
@@ -375,6 +517,10 @@ static int vl53l0x_init(const struct device *dev)
 	int r;
 	struct vl53l0x_data *drv_data = dev->data;
 	const struct vl53l0x_config *const config = dev->config;
+
+	k_work_init(&drv_data->interrupt_work, vl53l0x_interrupt_work);
+	k_sem_init(&drv_data->wait_for_interrupt, 0, 1);
+	drv_data->dev = dev;
 
 	/* Initialize the HAL peripheral with the default sensor address,
 	 * ie. the address on power up
@@ -399,7 +545,16 @@ static int vl53l0x_init(const struct device *dev)
 	if (config->xshut.port) {
 		r = gpio_pin_configure_dt(&config->xshut, GPIO_OUTPUT);
 		if (r < 0) {
-			LOG_ERR("[%s] Unable to configure GPIO as output", dev->name);
+			LOG_ERR("[%s] Unable to configure xshut as output", dev->name);
+			return -EIO;
+		}
+	}
+
+	if (vl53l0x_has_interrupt(dev)) {
+		r = gpio_pin_configure_dt(&config->interrupt, GPIO_INPUT);
+		if (r < 0) {
+			LOG_ERR("[%s] Unable to configure interrupt as input", dev->name);
+			return -EIO;
 		}
 	}
 
@@ -425,6 +580,7 @@ static int vl53l0x_init(const struct device *dev)
 #define VL53L0X_INIT(inst)                                                                         \
 	static struct vl53l0x_config vl53l0x_##inst##_config = {                                   \
 		.i2c = I2C_DT_SPEC_INST_GET(inst),                                                 \
+		.interrupt = GPIO_DT_SPEC_INST_GET_OR(inst, interrupt_gpios, {}),                  \
 		.xshut = GPIO_DT_SPEC_INST_GET_OR(inst, xshut_gpios, {})};                         \
                                                                                                    \
 	static struct vl53l0x_data vl53l0x_##inst##_driver;                                        \
