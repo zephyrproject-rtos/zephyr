4707ceb65a1226f14b45b91767849003d27cadf6
Emil Gydesen <emil.gydesen@nordicsemi.no>
2022-10-09 13:35:47 +0200

    Bluetooth: Audio: Add separate API for VCP vol controller
    Add a separate API for the VCP volume controller (client).
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_audio/test_scripts/vcp.sh
+++ tests/bluetooth/bsim_bt/bsim_test_audio/test_scripts/vcp.sh
@@ -43,7 +43,7 @@ Execute ./bs_${BOARD}_tests_bluetooth_bsim_bt_bsim_test_audio_prj_conf \
   -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=0 -testid=vcp -rs=23
 
 Execute ./bs_${BOARD}_tests_bluetooth_bsim_bt_bsim_test_audio_prj_conf \
-  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=1 -testid=vcp_client -rs=46
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=1 -testid=vcp_vol_ctlr -rs=46
 
 # Simulation time should be larger than the WAIT_TIME in common.h
 Execute ./bs_2G4_phy_v1 -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} \
