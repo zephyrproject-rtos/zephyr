36ea41fad3f353ee7af1238b63474bde24e3e9ee
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2022-10-10 11:51:38 +0200

    samples: introduce 'with_mcuboot' sample to show sysbuild feature
    This sample is intended to show users how they can create a sample which
    has dedicated configuration files for extra images included in a build.
    
    MCUboot is used as example on how its default can be adjusted and
    MCUboot itself automatically be included in the build when using
    sysbuild.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

---
+++ samples/application_development/sysbuild/with_mcuboot/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/linker/linker-defs.h>
+
+void main(void)
+{
+	printk("Address of sample %p\n", (void *)__rom_region_start);
+	printk("Hello sysbuild with mcuboot! %s\n", CONFIG_BOARD);
+}
