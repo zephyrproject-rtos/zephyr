627c21ae27c54851823ae3c0280c9643c9d51d25
Christopher Friedt <cfriedt@meta.com>
2022-10-10 12:58:30 -0400

    include: net: http: move http methods to separate header
    Previously, HTTP method enumerations were only defined within
    the `http_parser.h`, which may not be ideal for all use cases.
    
    This commit moves the `enum http_method` definition to a
    dedicated header in a dedicated `http` subdirectory.
    
    Signed-off-by: Christopher Friedt <cfriedt@meta.com>

--- include/zephyr/net/http_parser.h
+++ include/zephyr/net/http_parser.h
@@ -45,6 +45,7 @@ typedef unsigned __int64 uint64_t;
 #include <zephyr/types.h>
 #include <stddef.h>
 #endif
+#include <zephyr/net/http/method.h>
 #include <zephyr/net/http_parser_state.h>
 #include <zephyr/net/http_parser_url.h>
 
@@ -88,42 +89,6 @@ struct http_parser_settings;
 typedef int (*http_data_cb)(struct http_parser *, const char *at,
 			    size_t length);
 typedef int (*http_cb)(struct http_parser *);
-
-enum http_method {
-	HTTP_DELETE = 0,
-	HTTP_GET = 1,
-	HTTP_HEAD = 2,
-	HTTP_POST = 3,
-	HTTP_PUT = 4,
-	HTTP_CONNECT = 5,
-	HTTP_OPTIONS = 6,
-	HTTP_TRACE = 7,
-	HTTP_COPY = 8,
-	HTTP_LOCK = 9,
-	HTTP_MKCOL = 10,
-	HTTP_MOVE = 11,
-	HTTP_PROPFIND = 12,
-	HTTP_PROPPATCH = 13,
-	HTTP_SEARCH = 14,
-	HTTP_UNLOCK = 15,
-	HTTP_BIND = 16,
-	HTTP_REBIND = 17,
-	HTTP_UNBIND = 18,
-	HTTP_ACL = 19,
-	HTTP_REPORT = 20,
-	HTTP_MKACTIVITY = 21,
-	HTTP_CHECKOUT = 22,
-	HTTP_MERGE = 23,
-	HTTP_MSEARCH = 24,
-	HTTP_NOTIFY = 25,
-	HTTP_SUBSCRIBE = 26,
-	HTTP_UNSUBSCRIBE = 27,
-	HTTP_PATCH = 28,
-	HTTP_PURGE = 29,
-	HTTP_MKCALENDAR = 30,
-	HTTP_LINK = 31,
-	HTTP_UNLINK = 32
-};
 
 enum http_parser_type { HTTP_REQUEST, HTTP_RESPONSE, HTTP_BOTH };
 
