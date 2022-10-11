8b49ec3290e38001c24ba1fde0cb0e0c78deba68
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2022-10-11 12:46:34 +0530

    tests: bsim: Bluetooth: Add test for auto connection parameter update
    Add BabbleSim test to cover the automatic preferred
    peripheral connection parameter requests across multiple
    simultaneous connections.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- tests/bsim/bluetooth/ll/compile.sh
+++ tests/bsim/bluetooth/ll/compile.sh
@@ -47,4 +47,6 @@ app=tests/bsim/bluetooth/ll/edtt/gatt_test_app \
 app=tests/bsim/bluetooth/ll/multiple_id compile
 app=tests/bsim/bluetooth/ll/throughput compile
 
+app=tests/bsim/bluetooth/ll/conn_param compile
+
 wait_for_background_jobs
