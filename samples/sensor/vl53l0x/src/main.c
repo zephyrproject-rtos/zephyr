ae178399ff0dacff4f269fa6cd05c1d6f37718d1
Titouan Christophe <moiandme@gmail.com>
2022-10-12 10:37:55 +0200

    samples: sensors: vl53l0x: update to use trigger mode if available
    The sample now demonstrates how to configure the sampling frequency, and
    how to gather data using the "new data ready" trigger. If this is not
    supported (ie the sensor has no interrupt pin wired), fallback to polling
    mode as before.
    
    Also reformat the sample with clang-format to adhere to the latest code
    format standard.
    
    Signed-off-by: Titouan Christophe <moiandme@gmail.com>

--- samples/sensor/vl53l0x/src/main.c
+++ samples/sensor/vl53l0x/src/main.c
@@ -4,38 +4,82 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/kernel.h>
+#include <stdio.h>
+
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
-#include <stdio.h>
+#include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+
+#define SAMPLING_FREQ 25
+
+void print_distance_and_prox(const struct device *dev)
+{
+	struct sensor_value value;
+	int ret;
+
+	ret = sensor_sample_fetch(dev);
+	if (ret) {
+		printf("sensor_sample_fetch failed ret %d\n", ret);
+		return;
+	}
+
+	ret = sensor_channel_get(dev, SENSOR_CHAN_PROX, &value);
+	printf("prox is %d\n", value.val1);
+
+	ret = sensor_channel_get(dev, SENSOR_CHAN_DISTANCE, &value);
+	printf("distance is %.3fm\n", sensor_value_to_double(&value));
+}
+
+void on_new_data_ready(const struct device *dev, const struct sensor_trigger *trig)
+{
+	ARG_UNUSED(trig);
+	print_distance_and_prox(dev);
+}
 
 void main(void)
 {
-	const struct device *const dev = DEVICE_DT_GET_ONE(st_vl53l0x);
-	struct sensor_value value;
 	int ret;
+	bool trigger_enabled = false;
+	const struct device *const dev = DEVICE_DT_GET_ONE(st_vl53l0x);
+	const struct sensor_trigger trigger = {
+		.chan = SENSOR_CHAN_DISTANCE,
+		.type = SENSOR_TRIG_DATA_READY,
+	};
+	const struct sensor_value sampling_freq = {
+		.val1 = SAMPLING_FREQ,
+		.val2 = 0,
+	};
 
 	if (!device_is_ready(dev)) {
 		printk("sensor: device not ready.\n");
 		return;
 	}
 
+	ret = sensor_attr_set(dev, SENSOR_CHAN_DISTANCE, SENSOR_ATTR_SAMPLING_FREQUENCY,
+			      &sampling_freq);
+	if (ret) {
+		printf("Unable to set sampling freq to %dHz\n", SAMPLING_FREQ);
+		return;
+	}
+
+	ret = sensor_trigger_set(dev, &trigger, on_new_data_ready);
+	switch (ret) {
+	case 0:
+		trigger_enabled = true;
+		printf("Trigger enabled\n");
+		break;
+	case -ENOTSUP:
+		printf("Sensor does not support trigger\n");
+		break;
+	default:
+		return;
+	}
+
 	while (1) {
-		ret = sensor_sample_fetch(dev);
-		if (ret) {
-			printk("sensor_sample_fetch failed ret %d\n", ret);
-			return;
+		if (!trigger_enabled) {
+			print_distance_and_prox(dev);
 		}
-
-		ret = sensor_channel_get(dev, SENSOR_CHAN_PROX, &value);
-		printk("prox is %d\n", value.val1);
-
-		ret = sensor_channel_get(dev,
-					 SENSOR_CHAN_DISTANCE,
-					 &value);
-		printf("distance is %.3fm\n", sensor_value_to_double(&value));
-
 		k_sleep(K_MSEC(1000));
 	}
 }
