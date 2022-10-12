43564cc301a71d0d55c89001e8e54034e54f42e0
Yuval Peress <peress@google.com>
2022-10-11 22:49:40 -0600

    dsp: Update tests for basicmath to use dsp subsystem
    Update the relevant tests in basicmath to use the dsp subsystem. Note
    that f16 is not updated since it's much more architecture specific and
    did not become a part of zdsp.
    
    Signed-off-by: Yuval Peress <peress@google.com>

--- tests/subsys/dsp/basicmath/src/q31.c
+++ tests/subsys/dsp/basicmath/src/q31.c
@@ -5,10 +5,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/dsp/dsp.h>
 #include <zephyr/ztest.h>
 #include <zephyr/kernel.h>
 #include <stdlib.h>
-#include <arm_math.h>
 #include "../../common/test_common.h"
 
 #include "q31.pat"
@@ -17,7 +17,7 @@
 #define ABS_ERROR_THRESH_Q31	((q31_t)4)
 #define ABS_ERROR_THRESH_Q63	((q63_t)(1 << 17))
 
-static void test_arm_add_q31(
+static void test_zdsp_add_q31(
 	const q31_t *input1, const q31_t *input2, const q31_t *ref, size_t length)
 {
 	q31_t *output;
@@ -27,7 +27,7 @@ static void test_arm_add_q31(
 	zassert_not_null(output, ASSERT_MSG_BUFFER_ALLOC_FAILED);
 
 	/* Run test function */
-	arm_add_q31(input1, input2, output, length);
+	zdsp_add_q31(input1, input2, output, length);
 
 	/* Validate output */
 	zassert_true(
@@ -42,15 +42,15 @@ static void test_arm_add_q31(
 	free(output);
 }
 
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_add_q31, 3, in_com1, in_com2, ref_add, 3);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_add_q31, 8, in_com1, in_com2, ref_add, 8);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_add_q31, 11, in_com1, in_com2, ref_add, 11);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_add_q31, possat, in_maxpos, in_maxpos, ref_add_possat, 9);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_add_q31, negsat, in_maxneg, in_maxneg, ref_add_negsat, 9);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_add_q31, long, in_com1, in_com2, ref_add,
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_add_q31, 3, in_com1, in_com2, ref_add, 3);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_add_q31, 8, in_com1, in_com2, ref_add, 8);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_add_q31, 11, in_com1, in_com2, ref_add, 11);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_add_q31, possat, in_maxpos, in_maxpos, ref_add_possat, 9);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_add_q31, negsat, in_maxneg, in_maxneg, ref_add_negsat, 9);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_add_q31, long, in_com1, in_com2, ref_add,
 		     ARRAY_SIZE(in_com1));
 
-static void test_arm_sub_q31(
+static void test_zdsp_sub_q31(
 	const q31_t *input1, const q31_t *input2, const q31_t *ref, size_t length)
 {
 	q31_t *output;
@@ -60,7 +60,7 @@ static void test_arm_sub_q31(
 	zassert_not_null(output, ASSERT_MSG_BUFFER_ALLOC_FAILED);
 
 	/* Run test function */
-	arm_sub_q31(input1, input2, output, length);
+	zdsp_sub_q31(input1, input2, output, length);
 
 	/* Validate output */
 	zassert_true(
@@ -75,15 +75,15 @@ static void test_arm_sub_q31(
 	free(output);
 }
 
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_sub_q31, 3, in_com1, in_com2, ref_sub, 3);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_sub_q31, 8, in_com1, in_com2, ref_sub, 8);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_sub_q31, 11, in_com1, in_com2, ref_sub, 11);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_sub_q31, possat, in_maxpos, in_maxneg, ref_sub_possat, 9);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_sub_q31, negsat, in_maxneg, in_maxpos, ref_sub_negsat, 9);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_sub_q31, long, in_com1, in_com2, ref_sub,
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_sub_q31, 3, in_com1, in_com2, ref_sub, 3);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_sub_q31, 8, in_com1, in_com2, ref_sub, 8);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_sub_q31, 11, in_com1, in_com2, ref_sub, 11);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_sub_q31, possat, in_maxpos, in_maxneg, ref_sub_possat, 9);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_sub_q31, negsat, in_maxneg, in_maxpos, ref_sub_negsat, 9);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_sub_q31, long, in_com1, in_com2, ref_sub,
 		     ARRAY_SIZE(in_com1));
 
-static void test_arm_mult_q31(
+static void test_zdsp_mult_q31(
 	const q31_t *input1, const q31_t *input2, const q31_t *ref, size_t length)
 {
 	q31_t *output;
@@ -93,7 +93,7 @@ static void test_arm_mult_q31(
 	zassert_not_null(output, ASSERT_MSG_BUFFER_ALLOC_FAILED);
 
 	/* Run test function */
-	arm_mult_q31(input1, input2, output, length);
+	zdsp_mult_q31(input1, input2, output, length);
 
 	/* Validate output */
 	zassert_true(
@@ -108,15 +108,15 @@ static void test_arm_mult_q31(
 	free(output);
 }
 
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_mult_q31, 3, in_com1, in_com2, ref_mult, 3);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_mult_q31, 8, in_com1, in_com2, ref_mult, 8);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_mult_q31, 11, in_com1, in_com2, ref_mult, 11);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_mult_q31, possat, in_maxneg2, in_maxneg2, ref_mult_possat,
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_mult_q31, 3, in_com1, in_com2, ref_mult, 3);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_mult_q31, 8, in_com1, in_com2, ref_mult, 8);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_mult_q31, 11, in_com1, in_com2, ref_mult, 11);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_mult_q31, possat, in_maxneg2, in_maxneg2, ref_mult_possat,
 		     9);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_mult_q31, long, in_com1, in_com2, ref_mult,
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_mult_q31, long, in_com1, in_com2, ref_mult,
 		     ARRAY_SIZE(in_com1));
 
-static void test_arm_negate_q31(
+static void test_zdsp_negate_q31(
 	const q31_t *input1, const q31_t *ref, size_t length)
 {
 	q31_t *output;
@@ -126,7 +126,7 @@ static void test_arm_negate_q31(
 	zassert_not_null(output, ASSERT_MSG_BUFFER_ALLOC_FAILED);
 
 	/* Run test function */
-	arm_negate_q31(input1, output, length);
+	zdsp_negate_q31(input1, output, length);
 
 	/* Validate output */
 	zassert_true(
@@ -141,14 +141,14 @@ static void test_arm_negate_q31(
 	free(output);
 }
 
-DEFINE_TEST_VARIANT3(basic_math_q31, arm_negate_q31, 3, in_com1, ref_negate, 3);
-DEFINE_TEST_VARIANT3(basic_math_q31, arm_negate_q31, 8, in_com1, ref_negate, 8);
-DEFINE_TEST_VARIANT3(basic_math_q31, arm_negate_q31, 11, in_com1, ref_negate, 11);
-DEFINE_TEST_VARIANT3(basic_math_q31, arm_negate_q31, possat, in_maxneg2, ref_negate_possat, 9);
-DEFINE_TEST_VARIANT3(basic_math_q31, arm_negate_q31, long, in_com1, ref_negate,
+DEFINE_TEST_VARIANT3(basic_math_q31, zdsp_negate_q31, 3, in_com1, ref_negate, 3);
+DEFINE_TEST_VARIANT3(basic_math_q31, zdsp_negate_q31, 8, in_com1, ref_negate, 8);
+DEFINE_TEST_VARIANT3(basic_math_q31, zdsp_negate_q31, 11, in_com1, ref_negate, 11);
+DEFINE_TEST_VARIANT3(basic_math_q31, zdsp_negate_q31, possat, in_maxneg2, ref_negate_possat, 9);
+DEFINE_TEST_VARIANT3(basic_math_q31, zdsp_negate_q31, long, in_com1, ref_negate,
 		     ARRAY_SIZE(in_com1));
 
-static void test_arm_offset_q31(
+static void test_zdsp_offset_q31(
 	const q31_t *input1, q31_t scalar, const q31_t *ref, size_t length)
 {
 	q31_t *output;
@@ -158,7 +158,7 @@ static void test_arm_offset_q31(
 	zassert_not_null(output, ASSERT_MSG_BUFFER_ALLOC_FAILED);
 
 	/* Run test function */
-	arm_offset_q31(input1, scalar, output, length);
+	zdsp_offset_q31(input1, scalar, output, length);
 
 	/* Validate output */
 	zassert_true(
@@ -173,17 +173,17 @@ static void test_arm_offset_q31(
 	free(output);
 }
 
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_offset_q31, 0p5_3, in_com1, 0x40000000, ref_offset, 3);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_offset_q31, 0p5_8, in_com1, 0x40000000, ref_offset, 8);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_offset_q31, 0p5_11, in_com1, 0x40000000, ref_offset, 11);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_offset_q31, possat, in_maxpos, 0x73333333,
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_offset_q31, 0p5_3, in_com1, 0x40000000, ref_offset, 3);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_offset_q31, 0p5_8, in_com1, 0x40000000, ref_offset, 8);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_offset_q31, 0p5_11, in_com1, 0x40000000, ref_offset, 11);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_offset_q31, possat, in_maxpos, 0x73333333,
 		     ref_offset_possat, 9);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_offset_q31, negsat, in_maxneg, 0x8ccccccd,
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_offset_q31, negsat, in_maxneg, 0x8ccccccd,
 		     ref_offset_negsat, 9);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_offset_q31, long, in_com1, 0x40000000, ref_offset,
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_offset_q31, long, in_com1, 0x40000000, ref_offset,
 		     ARRAY_SIZE(in_com1));
 
-static void test_arm_scale_q31(
+static void test_zdsp_scale_q31(
 	const q31_t *input1, q31_t scalar, const q31_t *ref, size_t length)
 {
 	q31_t *output;
@@ -193,7 +193,7 @@ static void test_arm_scale_q31(
 	zassert_not_null(output, ASSERT_MSG_BUFFER_ALLOC_FAILED);
 
 	/* Run test function */
-	arm_scale_q31(input1, scalar, 0, output, length);
+	zdsp_scale_q31(input1, scalar, 0, output, length);
 
 	/* Validate output */
 	zassert_true(
@@ -208,15 +208,15 @@ static void test_arm_scale_q31(
 	free(output);
 }
 
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_scale_q31, 0p5_3, in_com1, 0x40000000, ref_scale, 3);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_scale_q31, 0p5_8, in_com1, 0x40000000, ref_scale, 8);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_scale_q31, 0p5_11, in_com1, 0x40000000, ref_scale, 11);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_scale_q31, possat, in_maxneg2, 0x80000000,
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_scale_q31, 0p5_3, in_com1, 0x40000000, ref_scale, 3);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_scale_q31, 0p5_8, in_com1, 0x40000000, ref_scale, 8);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_scale_q31, 0p5_11, in_com1, 0x40000000, ref_scale, 11);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_scale_q31, possat, in_maxneg2, 0x80000000,
 		     ref_scale_possat, 9);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_scale_q31, long, in_com1, 0x40000000, ref_scale,
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_scale_q31, long, in_com1, 0x40000000, ref_scale,
 		     ARRAY_SIZE(in_com1));
 
-static void test_arm_dot_prod_q31(
+static void test_zdsp_dot_prod_q31(
 	const q31_t *input1, const q31_t *input2, const q63_t *ref, size_t length)
 {
 	q63_t *output;
@@ -226,7 +226,7 @@ static void test_arm_dot_prod_q31(
 	zassert_not_null(output, ASSERT_MSG_BUFFER_ALLOC_FAILED);
 
 	/* Run test function */
-	arm_dot_prod_q31(input1, input2, length, &output[0]);
+	zdsp_dot_prod_q31(input1, input2, length, &output[0]);
 
 	/* Validate output */
 	zassert_true(
@@ -241,13 +241,13 @@ static void test_arm_dot_prod_q31(
 	free(output);
 }
 
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_dot_prod_q31, 3, in_com1, in_com2, ref_dot_prod_3, 3);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_dot_prod_q31, 8, in_com1, in_com2, ref_dot_prod_4, 8);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_dot_prod_q31, 11, in_com1, in_com2, ref_dot_prod_4n1, 11);
-DEFINE_TEST_VARIANT4(basic_math_q31, arm_dot_prod_q31, long, in_com1, in_com2, ref_dot_prod_long,
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_dot_prod_q31, 3, in_com1, in_com2, ref_dot_prod_3, 3);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_dot_prod_q31, 8, in_com1, in_com2, ref_dot_prod_4, 8);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_dot_prod_q31, 11, in_com1, in_com2, ref_dot_prod_4n1, 11);
+DEFINE_TEST_VARIANT4(basic_math_q31, zdsp_dot_prod_q31, long, in_com1, in_com2, ref_dot_prod_long,
 		     ARRAY_SIZE(in_com1));
 
-static void test_arm_abs_q31(
+static void test_zdsp_abs_q31(
 	const q31_t *input1, const q31_t *ref, size_t length)
 {
 	q31_t *output;
@@ -257,7 +257,7 @@ static void test_arm_abs_q31(
 	zassert_not_null(output, ASSERT_MSG_BUFFER_ALLOC_FAILED);
 
 	/* Run test function */
-	arm_abs_q31(input1, output, length);
+	zdsp_abs_q31(input1, output, length);
 
 	/* Validate output */
 	zassert_true(
@@ -272,12 +272,12 @@ static void test_arm_abs_q31(
 	free(output);
 }
 
-DEFINE_TEST_VARIANT3(basic_math_q31, arm_abs_q31, 3, in_com1, ref_abs, 3);
-DEFINE_TEST_VARIANT3(basic_math_q31, arm_abs_q31, 8, in_com1, ref_abs, 8);
-DEFINE_TEST_VARIANT3(basic_math_q31, arm_abs_q31, 11, in_com1, ref_abs, 11);
-DEFINE_TEST_VARIANT3(basic_math_q31, arm_abs_q31, long, in_com1, ref_abs, ARRAY_SIZE(in_com1));
+DEFINE_TEST_VARIANT3(basic_math_q31, zdsp_abs_q31, 3, in_com1, ref_abs, 3);
+DEFINE_TEST_VARIANT3(basic_math_q31, zdsp_abs_q31, 8, in_com1, ref_abs, 8);
+DEFINE_TEST_VARIANT3(basic_math_q31, zdsp_abs_q31, 11, in_com1, ref_abs, 11);
+DEFINE_TEST_VARIANT3(basic_math_q31, zdsp_abs_q31, long, in_com1, ref_abs, ARRAY_SIZE(in_com1));
 
-static void test_arm_shift_q31(
+static void test_zdsp_shift_q31(
 	const q31_t *input1, const q31_t *ref, size_t length)
 {
 	q31_t *output;
@@ -287,7 +287,7 @@ static void test_arm_shift_q31(
 	zassert_not_null(output, ASSERT_MSG_BUFFER_ALLOC_FAILED);
 
 	/* Run test function */
-	arm_shift_q31(input1, 1, output, length);
+	zdsp_shift_q31(input1, 1, output, length);
 
 	/* Validate output */
 	zassert_true(
@@ -302,9 +302,9 @@ static void test_arm_shift_q31(
 	free(output);
 }
 
-DEFINE_TEST_VARIANT3(basic_math_q31, arm_shift_q31, rand, in_rand, ref_shift, 9);
-DEFINE_TEST_VARIANT3(basic_math_q31, arm_shift_q31, possat, in_maxpos, ref_shift_possat, 9);
-DEFINE_TEST_VARIANT3(basic_math_q31, arm_shift_q31, negsat, in_maxneg, ref_shift_negsat, 9);
+DEFINE_TEST_VARIANT3(basic_math_q31, zdsp_shift_q31, rand, in_rand, ref_shift, 9);
+DEFINE_TEST_VARIANT3(basic_math_q31, zdsp_shift_q31, possat, in_maxpos, ref_shift_possat, 9);
+DEFINE_TEST_VARIANT3(basic_math_q31, zdsp_shift_q31, negsat, in_maxneg, ref_shift_negsat, 9);
 
 static void test_arm_and_u32(
 	const uint32_t *input1, const uint32_t *input2, const uint32_t *ref, size_t length)
@@ -406,7 +406,7 @@ DEFINE_TEST_VARIANT4(basic_math_q31, arm_xor_u32, 3, in_bitwise1, in_bitwise2, r
 DEFINE_TEST_VARIANT4(basic_math_q31, arm_xor_u32, 8, in_bitwise1, in_bitwise2, ref_xor, 8);
 DEFINE_TEST_VARIANT4(basic_math_q31, arm_xor_u32, 11, in_bitwise1, in_bitwise2, ref_xor, 11);
 
-static void test_arm_clip_q31(
+static void test_zdsp_clip_q31(
 	const q31_t *input, const q31_t *ref, q31_t min, q31_t max, size_t length)
 {
 	q31_t *output;
@@ -416,7 +416,7 @@ static void test_arm_clip_q31(
 	zassert_not_null(output, ASSERT_MSG_BUFFER_ALLOC_FAILED);
 
 	/* Run test function */
-	arm_clip_q31(input, output, min, max, length);
+	zdsp_clip_q31(input, output, min, max, length);
 
 	/* Validate output */
 	zassert_true(
@@ -427,11 +427,11 @@ static void test_arm_clip_q31(
 	free(output);
 }
 
-DEFINE_TEST_VARIANT5(basic_math_q31, arm_clip_q31, c0000000_f3333333, in_clip, ref_clip1,
+DEFINE_TEST_VARIANT5(basic_math_q31, zdsp_clip_q31, c0000000_f3333333, in_clip, ref_clip1,
 		     0xc0000000, 0xf3333333, ARRAY_SIZE(ref_clip1));
-DEFINE_TEST_VARIANT5(basic_math_q31, arm_clip_q31, c0000000_40000000, in_clip, ref_clip2,
+DEFINE_TEST_VARIANT5(basic_math_q31, zdsp_clip_q31, c0000000_40000000, in_clip, ref_clip2,
 		     0xc0000000, 0x40000000, ARRAY_SIZE(ref_clip2));
-DEFINE_TEST_VARIANT5(basic_math_q31, arm_clip_q31, 0ccccccd_40000000, in_clip, ref_clip3,
+DEFINE_TEST_VARIANT5(basic_math_q31, zdsp_clip_q31, 0ccccccd_40000000, in_clip, ref_clip3,
 		     0x0ccccccd, 0x40000000, ARRAY_SIZE(ref_clip3));
 
 ZTEST_SUITE(basic_math_q31, NULL, NULL, NULL, NULL, NULL);
