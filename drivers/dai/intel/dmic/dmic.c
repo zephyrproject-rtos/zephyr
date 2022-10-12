ed661a6c6909b338035b026cfc101ddda65ab8eb
Adrian Warecki <adrian.warecki@intel.com>
2022-10-12 15:18:33 +0200

    dai: dmic: Add missing include header
    The code was missing #include <zephyr/kernel.h> which
    caused a compilation error. Missing header has been added.
    
    Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

--- drivers/dai/intel/dmic/dmic.c
+++ drivers/dai/intel/dmic/dmic.c
@@ -12,6 +12,7 @@ LOG_MODULE_REGISTER(LOG_DOMAIN);
 #include <errno.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <zephyr/kernel.h>
 #include <zephyr/spinlock.h>
 #include <zephyr/devicetree.h>
 
