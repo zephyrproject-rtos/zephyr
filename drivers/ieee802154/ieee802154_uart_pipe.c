5083ef5a3188b8d5800f3099f85f7497bde32c0d
Gerson Fernando Budke <nandojve@gmail.com>
2022-10-14 13:58:17 +0200

    drivers: ieee802154: Drop IEEE802154_MTU constant
    The IEEE802154_MTU is not a well know IEEE 802.15.4 term. It should be
    avoid inside drivers and L2 stack because the representation of MTU is
    not a constant value. The correct value should be calculated in the L2
    and exposed by a method to upper layers. Because of that, drivers
    should pass the equivalet MTU value to NET_DEVICE_DT_INST_DEFINE based
    on their own capabilities.
    
    For instance, the standard IEEE 802.15.4-2003/2006 devices should
    define MTU as IEEE802154_STANDARD_PSDU_SIZE less the number defined by
    IEEE802154_CRC_16_FCS_SIZE.
    
    Signed-off-by: Gerson Fernando Budke <nandojve@gmail.com>

--- drivers/ieee802154/ieee802154_uart_pipe.c
+++ drivers/ieee802154/ieee802154_uart_pipe.c
@@ -396,4 +396,5 @@ static struct ieee802154_radio_api upipe_radio_api = {
 NET_DEVICE_DT_INST_DEFINE(0, upipe_init, NULL, &upipe_context_data, NULL,
 			  CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &upipe_radio_api,
 			  IEEE802154_L2, NET_L2_GET_CTX_TYPE(IEEE802154_L2),
-			  125);
+			  (IEEE802154_STANDARD_PSDU_SIZE -
+			   IEEE802154_CRC_16_FCS_SIZE));
