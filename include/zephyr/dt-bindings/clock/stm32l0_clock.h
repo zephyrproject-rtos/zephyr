e3e50fe9f8ef747225366982d28ef85537374dc1
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-10-14 10:14:02 +0200

    dt-bindings: clock: stm32: fix STM32_CLOCK macro docs location
    Doxygen docs for a macro need to be placed together with the macro being
    documented (unless using `@def`).
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- include/zephyr/dt-bindings/clock/stm32l0_clock.h
+++ include/zephyr/dt-bindings/clock/stm32l0_clock.h
@@ -28,6 +28,15 @@
 /** Bus clock */
 #define STM32_SRC_PCLK		0x006
 
+#define STM32_CLOCK_REG_MASK    0xFFU
+#define STM32_CLOCK_REG_SHIFT   0U
+#define STM32_CLOCK_SHIFT_MASK  0x1FU
+#define STM32_CLOCK_SHIFT_SHIFT 8U
+#define STM32_CLOCK_MASK_MASK   0x7U
+#define STM32_CLOCK_MASK_SHIFT  13U
+#define STM32_CLOCK_VAL_MASK    0x7U
+#define STM32_CLOCK_VAL_SHIFT   16U
+
 /**
  * @brief STM32 clock configuration bit field.
  *
@@ -41,16 +50,6 @@
  * @param mask Mask for the RCC_CCIPRx field.
  * @param val Clock value (0, 1, ... 7).
  */
-
-#define STM32_CLOCK_REG_MASK    0xFFU
-#define STM32_CLOCK_REG_SHIFT   0U
-#define STM32_CLOCK_SHIFT_MASK  0x1FU
-#define STM32_CLOCK_SHIFT_SHIFT 8U
-#define STM32_CLOCK_MASK_MASK   0x7U
-#define STM32_CLOCK_MASK_SHIFT  13U
-#define STM32_CLOCK_VAL_MASK    0x7U
-#define STM32_CLOCK_VAL_SHIFT   16U
-
 #define STM32_CLOCK(val, mask, shift, reg)					\
 	((((reg) & STM32_CLOCK_REG_MASK) << STM32_CLOCK_REG_SHIFT) |		\
 	 (((shift) & STM32_CLOCK_SHIFT_MASK) << STM32_CLOCK_SHIFT_SHIFT) |	\
