419e5087cae868ca71af9a00e2bb28f88cc57bb2
TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>
2022-10-16 10:17:18 +0900

    boards: arduino_nano_33_ble: add pyocd support
    Add support to debugging and flashing with the pyocd.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>

--- boards/arm/arduino_nano_33_ble/board.cmake
+++ boards/arm/arduino_nano_33_ble/board.cmake
@@ -1,4 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
+
 include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
