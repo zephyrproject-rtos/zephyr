664975959807a27c6e4c5202919274fc91458d2a
Ahmed Moheb <ahmed.moheb@nordicsemi.no>
2022-10-16 11:28:12 +0200

    tests: bluetooth: host: Add UT for bt_le_oob_set_legacy_tk()
    Unit test project for bt_le_oob_set_legacy_tk().
    This part of subsys/bluetooth/host/id.c unit testing.
    
    Signed-off-by: Ahmed Moheb <ahmed.moheb@nordicsemi.no>

--- tests/bluetooth/host/id/mocks/smp_expects.h
+++ tests/bluetooth/host/id/mocks/smp_expects.h
@@ -21,3 +21,11 @@ void expect_single_call_bt_smp_le_oob_generate_sc_data(struct bt_le_oob_sc_data 
  *   - bt_smp_le_oob_generate_sc_data() isn't called at all
  */
 void expect_not_called_bt_smp_le_oob_generate_sc_data(void);
+
+/*
+ *  Validate expected behaviour when bt_smp_le_oob_set_tk() is called
+ *
+ *  Expected behaviour:
+ *   - bt_smp_le_oob_set_tk() to be called once with correct parameters
+ */
+void expect_single_call_bt_smp_le_oob_set_tk(struct bt_conn *conn, const uint8_t *tk);
