411234bdd6c688a4303b3965f5d914d654c5f7b2
Ahmed Moheb <ahmed.moheb@nordicsemi.no>
2022-10-17 10:27:56 +0200

    tests: bluetooth: host: Add UT for bt_le_oob_get_sc_data()
    Unit test project for bt_le_oob_get_sc_data().
    This part of subsys/bluetooth/host/id.c unit testing.
    
    Signed-off-by: Ahmed Moheb <ahmed.moheb@nordicsemi.no>

--- tests/bluetooth/host/id/mocks/smp_expects.c
+++ tests/bluetooth/host/id/mocks/smp_expects.c
@@ -54,3 +54,20 @@ void expect_single_call_bt_smp_le_oob_set_sc_data(struct bt_conn *conn,
 	zassert_equal_ptr(bt_smp_le_oob_set_sc_data_fake.arg2_val, oobd_remote,
 			  "'%s()' was called with incorrect '%s' value", func_name, "oobd_remote");
 }
+
+void expect_single_call_bt_smp_le_oob_get_sc_data(struct bt_conn *conn,
+						  const struct bt_le_oob_sc_data **oobd_local,
+						  const struct bt_le_oob_sc_data **oobd_remote)
+{
+	const char *func_name = "bt_smp_le_oob_get_sc_data";
+
+	zassert_equal(bt_smp_le_oob_get_sc_data_fake.call_count, 1,
+		      "'%s()' was called more than once", func_name);
+
+	zassert_equal_ptr(bt_smp_le_oob_get_sc_data_fake.arg0_val, conn,
+			  "'%s()' was called with incorrect '%s' value", func_name, "conn");
+	zassert_equal_ptr(bt_smp_le_oob_get_sc_data_fake.arg1_val, oobd_local,
+			  "'%s()' was called with incorrect '%s' value", func_name, "oobd_local");
+	zassert_equal_ptr(bt_smp_le_oob_get_sc_data_fake.arg2_val, oobd_remote,
+			  "'%s()' was called with incorrect '%s' value", func_name, "oobd_remote");
+}
