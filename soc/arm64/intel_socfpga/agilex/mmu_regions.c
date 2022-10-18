fafb46a1cb4c6a4f19319b133c986dcd3a233cf7
Mahesh Rao <mahesh.rao@intel.com>
2022-10-18 07:04:07 +0000

    soc: agilex: Updated mmu regions as per rebase to zephyr v3.2
    Updated the mmu regions  for Intel AGILEX SOC FPGA
    
    Signed-off-by: Mahesh Rao <mahesh.rao@intel.com>

--- soc/arm64/intel_socfpga/agilex/mmu_regions.c
+++ soc/arm64/intel_socfpga/agilex/mmu_regions.c
@@ -14,10 +14,6 @@ static const struct arm_mmu_region mmu_regions[] = {
 			      DT_REG_ADDR(DT_NODELABEL(clock)),
 			      DT_REG_SIZE(DT_NODELABEL(clock)),
 			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
-	MMU_REGION_FLAT_ENTRY("UART0",
-			      DT_REG_ADDR(DT_NODELABEL(uart0)),
-			      DT_REG_SIZE(DT_NODELABEL(uart0)),
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
 
 	/* Peripheral to cover UART, I2C, I2C EMAC and Timer SP */
 	MMU_REGION_FLAT_ENTRY("PERIPHERAL",
@@ -25,10 +21,6 @@ static const struct arm_mmu_region mmu_regions[] = {
 			      DT_REG_SIZE_BY_IDX(DT_NODELABEL(periph), 0),
 			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
 
-	MMU_REGION_FLAT_ENTRY("RESET_MANAGER",
-			      DT_REG_ADDR_BY_IDX(DT_NODELABEL(periph), 1),
-			      DT_REG_SIZE_BY_IDX(DT_NODELABEL(periph), 1),
-			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
 
 	/* System manager register that required by clock driver */
 	MMU_REGION_FLAT_ENTRY("SYSTEM_MANAGER",
