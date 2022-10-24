772ee7bd4d281562329bc4a6247496aff0e31ba9
Duong Vu Nam <duong.vunam@nxp.com>
2022-10-24 17:06:55 +0700

    arch: aarch32: add caches capability to Cortex-R52
    Support I/D cache for Cortex-R52 to run with cache on NXP S32Z/E.
    Make sure no data is present in the D-Cache before initializing mpu
    
    Signed-off-by: Duong Vu Nam <duong.vunam@nxp.com>

--- arch/arm/core/aarch32/mpu/arm_mpu.c
+++ arch/arm/core/aarch32/mpu/arm_mpu.c
@@ -350,10 +350,16 @@ int z_arm_mpu_init(void)
 	/* Clean and invalidate data cache if it is enabled and
 	 * that was not already done at boot
 	 */
+#if defined(CONFIG_CPU_AARCH32_CORTEX_R)
+	if (__get_SCTLR() & SCTLR_C_Msk) {
+		L1C_CleanInvalidateDCacheAll();
+	}
+#else
 #if !defined(CONFIG_INIT_ARCH_HW_AT_BOOT)
 	if (SCB->CCR & SCB_CCR_DC_Msk) {
 		SCB_CleanInvalidateDCache();
 	}
+#endif
 #endif
 #endif /* CONFIG_NOCACHE_MEMORY */
 
