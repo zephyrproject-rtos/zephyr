e31e3c0e02041762de7799ae99da795d378438bd
Sean Madigan <sean.madigan@nordicsemi.no>
2022-10-25 09:15:49 +0100

    Tests: Bluetooth: Add bt_disable babblesim test with gatt
    Add babblesim test for bt_disable which enables bt, starts a connection,
    GATT discovers, reads, writes then disables BT in a loop.
    
    Signed-off-by: Sean Madigan <sean.madigan@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_disable/src/main.c
+++ tests/bluetooth/bsim_bt/bsim_test_disable/src/main.c
@@ -7,9 +7,13 @@
 #include "bstests.h"
 
 extern struct bst_test_list *test_main_disable_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_gatt_server_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_gatt_client_install(struct bst_test_list *tests);
 
 bst_test_install_t test_installers[] = {
 	test_main_disable_install,
+	test_gatt_server_install,
+	test_gatt_client_install,
 	NULL
 };
 
