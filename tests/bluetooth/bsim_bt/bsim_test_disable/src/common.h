c3711ebc43c222bef9be074ab42d37fc02a9d02e
Sean Madigan <sean.madigan@nordicsemi.no>
2022-10-27 16:14:29 +0100

    Tests: Bluetooth: Add bt_disable babblesim test
    Add babblesim test for bt_disable which just enables and disables BT in
    a loop.
    
    Signed-off-by: Sean Madigan <sean.madigan@nordicsemi.no>

---
+++ tests/bluetooth/bsim_bt/bsim_test_disable/src/common.h
@@ -0,0 +1,43 @@
+/**
+ * Common functions and helpers for BSIM disable with GATT tests
+ *
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+#include "bs_types.h"
+#include "bs_tracing.h"
+#include "time_machine.h"
+#include "bstests.h"
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <errno.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+
+extern enum bst_result_t bst_result;
+
+#define WAIT_TIME (600 * 1e6) /*seconds*/
+
+#define FAIL(...) \
+	do { \
+		bst_result = Failed; \
+		bs_trace_error_time_line(__VA_ARGS__); \
+	} while (0)
+
+#define PASS(...) \
+	do { \
+		bst_result = Passed; \
+		bs_trace_info_time(1, __VA_ARGS__); \
+	} while (0)
+
+void test_tick(bs_time_t HW_device_time);
+void test_init(void);
