96f3d41c96c15856395f82ad1eba007f4f17e4c7
Siyuan Cheng <siyuanc@synopsys.com>
2022-10-31 15:14:21 +0800

    arc: add XY mem support
    The XY Memory is a feature commonly found in DSP processors to increase
    the DSP performance. The XY component allows a ARC processor to
    implicitly load source operands and store results into a closely coupled
    memory using a single instruction.
    
    Add XY memory for ARC EM9D/EM11D processors including em_starterkit,
    em_starterkit_em11d. emsdp_em9d, nsim_em, iotdk.
    
    Signed-off-by: Siyuan Cheng <siyuanc@synopsys.com>

--- boards/arc/em_starterkit/arc_mpu_regions.c
+++ boards/arc/em_starterkit/arc_mpu_regions.c
@@ -28,6 +28,21 @@ static struct arc_mpu_region mpu_regions[] = {
 			 REGION_KERNEL_RAM_ATTR | REGION_DYNAMIC),
 #endif
 
+#if DT_REG_SIZE(DT_INST(0, arc_xccm)) > 0
+	/* Region XCCM */
+	MPU_REGION_ENTRY("XCCM",
+			 DT_REG_ADDR(DT_INST(0, arc_xccm)),
+			 DT_REG_SIZE(DT_INST(0, arc_xccm)),
+			 REGION_KERNEL_RAM_ATTR | REGION_DYNAMIC),
+#endif
+#if DT_REG_SIZE(DT_INST(0, arc_yccm)) > 0
+	/* Region YCCM */
+	MPU_REGION_ENTRY("YCCM",
+			 DT_REG_ADDR(DT_INST(0, arc_yccm)),
+			 DT_REG_SIZE(DT_INST(0, arc_yccm)),
+			 REGION_KERNEL_RAM_ATTR | REGION_DYNAMIC),
+#endif
+
 #if DT_REG_SIZE(DT_INST(0, mmio_sram)) > 0
 	/* Region DDR RAM */
 	MPU_REGION_ENTRY("DDR RAM",
