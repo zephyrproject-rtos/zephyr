ae23b9281b032967558d5898c506c9971b491aef
Jaxson Han <jaxson.han@arm.com>
2022-11-01 15:32:50 +0800

    arch: arm64: Add stack check relevant variables to _cpu_arch_t
    Add three per-cpu variables for the convenience of quickly accessing.
    
    The safe_exception_stack stores the top of safe exception stack pointer.
    The current_stack_limit stores the current thread's priv stack limit.
    The corrputed_sp stores the priv sp or irq sp for the stack overflow
    case, or 0 for the normal case.
    
    Signed-off-by: Jaxson Han <jaxson.han@arm.com>

--- include/zephyr/arch/arm64/structs.h
+++ include/zephyr/arch/arm64/structs.h
@@ -12,6 +12,12 @@ struct _cpu_arch {
 #ifdef CONFIG_FPU_SHARING
 	struct k_thread *fpu_owner;
 #endif
+#ifdef CONFIG_ARM64_SAFE_EXCEPTION_STACK
+	uint64_t safe_exception_stack;
+	uint64_t current_stack_limit;
+	/* Saved the corrupted stack pointer when stack overflow, else 0 */
+	uint64_t corrupted_sp;
+#endif
 };
 
 #endif /* ZEPHYR_INCLUDE_ARM64_STRUCTS_H_ */
