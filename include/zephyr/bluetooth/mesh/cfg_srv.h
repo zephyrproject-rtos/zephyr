b08e9da9c1f2692a1109229524ef4820daa2378b
Lingao Meng <menglingao@xiaomi.com>
2022-11-02 19:31:26 +0800

    Bluetooth: Mesh: Add specific number of keys & groups
    Since Mesh Spec and Mesh model sayes, some model no need
    groups and no need keys, such as Config Server, but the
    current implementation, all model use same configuration,
    which cause some ram ext comsume.
    
    So that, change to specific way, but will consume more footprint size.
    
    Signed-off-by: Lingao Meng <menglingao@xiaomi.com>

--- include/zephyr/bluetooth/mesh/cfg_srv.h
+++ include/zephyr/bluetooth/mesh/cfg_srv.h
@@ -24,9 +24,10 @@ extern "C" {
 /**
  *  @brief Generic Configuration Server model composition data entry.
  */
-#define BT_MESH_MODEL_CFG_SRV                                                  \
-	BT_MESH_MODEL_CB(BT_MESH_MODEL_ID_CFG_SRV, bt_mesh_cfg_srv_op, NULL,   \
-			 NULL, &bt_mesh_cfg_srv_cb)
+#define BT_MESH_MODEL_CFG_SRV	\
+	BT_MESH_MODEL_CNT_CB(BT_MESH_MODEL_ID_CFG_SRV,	\
+			     bt_mesh_cfg_srv_op, NULL,	\
+			     NULL, 1, 0, &bt_mesh_cfg_srv_cb)
 
 /** @cond INTERNAL_HIDDEN */
 extern const struct bt_mesh_model_op bt_mesh_cfg_srv_op[];
