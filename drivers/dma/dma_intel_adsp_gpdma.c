8c0ae90c18f4e6cc6e5373205c1276c91401b961
Adrian Warecki <adrian.warecki@intel.com>
2022-11-04 10:13:03 +0100

    dma: gpdma: Read linear link position in status function
    Added reading of the linear link position in the status function.
    
    Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

--- drivers/dma/dma_intel_adsp_gpdma.c
+++ drivers/dma/dma_intel_adsp_gpdma.c
@@ -263,6 +263,20 @@ out:
 	return 0;
 }
 
+int intel_adsp_gpdma_get_status(const struct device *dev, uint32_t channel, struct dma_status *stat)
+{
+	uint32_t llp_l, llp_u;
+
+	if (channel >= DW_MAX_CHAN) {
+		return -EINVAL;
+	}
+
+	intel_adsp_gpdma_llp_read(dev, channel, &llp_l, &llp_u);
+	stat->total_copied = (llp_u << 32) | llp_l;
+
+	return dw_dma_get_status(dev, channel, stat);
+}
+
 static const struct dma_driver_api intel_adsp_gpdma_driver_api = {
 	.config = intel_adsp_gpdma_config,
 	.reload = intel_adsp_gpdma_copy,
@@ -270,7 +284,7 @@ static const struct dma_driver_api intel_adsp_gpdma_driver_api = {
 	.stop = intel_adsp_gpdma_stop,
 	.suspend = dw_dma_suspend,
 	.resume = dw_dma_resume,
-	.get_status = dw_dma_get_status,
+	.get_status = intel_adsp_gpdma_get_status,
 };
 
 #define INTEL_ADSP_GPDMA_CHAN_ARB_DATA(inst)				\
