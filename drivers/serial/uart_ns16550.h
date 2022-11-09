bbb2f8277a7e9320c9dee06e98cdb3801238f4dc
Daniel Leung <daniel.leung@intel.com>
2022-11-09 12:49:28 -0800

    uart: ns16550: move command macro to public header
    This moves the CMD_SET_DLF command macro to public header
    so application can use it without weird include path to
    include the "private" header file under drivers/serial.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- drivers/serial/uart_ns16550.h
+++ drivers/serial/uart_ns16550.h
@@ -11,6 +11,8 @@
 #ifndef ZEPHYR_DRIVERS_SERIAL_UART_NS16550_H_
 #define ZEPHYR_DRIVERS_SERIAL_UART_NS16550_H_
 
-#define CMD_SET_DLF	0x01
+#include <zephyr/drivers/serial/uart_ns16550.h>
+
+#warning this header is deprecated, include <zephyr/drivers/serial/uart_ns16550.h> instead
 
 #endif /* ZEPHYR_DRIVERS_SERIAL_UART_NS16550_H_ */
