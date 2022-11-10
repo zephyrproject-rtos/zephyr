69dd61d31945e5247304e9dfb1c87c70c66396f2
Stanley Huang <stanley@raytac.com>
2022-11-10 13:15:36 +0800

    boards: Change board's name to raytac_mdbt50q_db_40_nrf52840
    I change name to raytac_mdbt50q_db_40_nrf52840.

--- boards/arm/raytac_mdbt50q_db_40_nrf52840/pre_dt_board.cmake
+++ boards/arm/raytac_mdbt50q_db_40_nrf52840/pre_dt_board.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 Raytac Corporation
+# Copyright (c) 2022 Nordic Semiconductor
 # SPDX-License-Identifier: Apache-2.0
 
 # Suppress "unique_unit_address_if_enabled" to handle the following overlaps:
