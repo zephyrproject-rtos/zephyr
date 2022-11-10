6b5752d9cbd958dcd1ab6a88a8b92ed82f600b04
Morten Priess <mtpr@oticon.com>
2022-11-10 08:43:46 +0100

    Bluetooth: controller: HCI fixes for ISO central role CIG/CIS creation
    Fix issues with referencing invalid netbuf data and add missing
    parameter. Removed returning of handle in setup functions, as handles
    are  not available until commit.
    
    Signed-off-by: Morten Priess <mtpr@oticon.com>

--- subsys/bluetooth/controller/include/ll.h
+++ subsys/bluetooth/controller/include/ll.h
@@ -147,8 +147,7 @@ uint8_t ll_cig_parameters_open(uint8_t cig_id,
 uint8_t ll_cis_parameters_set(uint8_t cis_id,
 			      uint16_t c_sdu, uint16_t p_sdu,
 			      uint8_t c_phy, uint8_t p_phy,
-			      uint8_t c_rtn, uint8_t p_rtn,
-			      uint16_t *handle);
+			      uint8_t c_rtn, uint8_t p_rtn);
 uint8_t ll_cig_parameters_commit(uint8_t cig_id);
 uint8_t ll_cig_parameters_test_open(uint8_t cig_id,
 				    uint32_t c_interval,
@@ -160,12 +159,11 @@ uint8_t ll_cig_parameters_test_open(uint8_t cig_id,
 				    uint8_t packing,
 				    uint8_t framing,
 				    uint8_t num_cis);
-uint8_t ll_cis_parameters_test_set(uint8_t cis_id,
+uint8_t ll_cis_parameters_test_set(uint8_t cis_id, uint8_t nse,
 				   uint16_t c_sdu, uint16_t p_sdu,
 				   uint16_t c_pdu, uint16_t p_pdu,
 				   uint8_t c_phy, uint8_t p_phy,
-				   uint8_t c_bn, uint8_t p_bn,
-				   uint16_t *handle);
+				   uint8_t c_bn, uint8_t p_bn);
 uint8_t ll_configure_data_path(uint8_t data_path_dir,
 			       uint8_t data_path_id,
 			       uint8_t vs_config_len,
