4343f926ae835c6dddafd24d8e508308b24d11f3
Joakim Andersson <joakim.andersson@nordicsemi.no>
2022-11-11 12:15:27 +0100

    Bluetooth: host: Call RPA expired on disabled advertising sets
    Call the RPA expired callback on advertising sets even when they are
    not enabled.
    The RPA expired callback will now be called on advertising sets
    when the BT_ADV_RPA_VALID flag is cleared for the set.
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

--- subsys/bluetooth/host/adv.c
+++ subsys/bluetooth/host/adv.c
@@ -1639,7 +1639,7 @@ int bt_le_ext_adv_stop(struct bt_le_ext_adv *adv)
 	}
 
 	if (atomic_test_and_clear_bit(adv->flags, BT_ADV_LIMITED)) {
-		atomic_clear_bit(adv->flags, BT_ADV_RPA_VALID);
+		bt_id_adv_limited_stopped(adv);
 
 #if defined(CONFIG_BT_SMP)
 		bt_id_pending_keys_update();
@@ -2015,7 +2015,7 @@ void bt_hci_le_adv_set_terminated(struct net_buf *buf)
 	}
 
 	if (atomic_test_and_clear_bit(adv->flags, BT_ADV_LIMITED)) {
-		atomic_clear_bit(adv->flags, BT_ADV_RPA_VALID);
+		bt_id_adv_limited_stopped(adv);
 
 #if defined(CONFIG_BT_SMP)
 		bt_id_pending_keys_update();
