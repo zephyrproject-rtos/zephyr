0af2809263f633fcd9e711595a3a3e46dc6af8ca
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-11-11 13:36:58 +0100

    drivers: regulator: improve Doxygen
    The recently moved "consumer" API had multiple Doxygen issues. This
    patch improves the Doxygen docs, e.g. use `@retval`, remove redundant
    docs, improve parameter docs, etc.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- include/zephyr/drivers/regulator.h
+++ include/zephyr/drivers/regulator.h
@@ -108,13 +108,16 @@ static inline int regulator_disable(const struct device *dev)
 }
 
 /**
- * @brief Return the number of supported voltage levels
- * Returns the number of selectors, or negative errno. Selectors are
- * numbered starting at zero, and typically correspond to bitfields
- * in hardware registers.
+ * @brief Obtain the number of supported voltage levels.
  *
- * @param dev: Regulator device to count voltage levels for.
- * @return number of selectors, or negative errno.
+ * Selectors are numbered starting at zero, and typically correspond to
+ * bitfields in hardware registers.
+ *
+ * @param dev Regulator device instance.
+ *
+ * @retval selectors Number of selectors, if successful.
+ * @retval -ENOSYS If function is not implemented.
+ * @retval -errno In case of any other error.
  */
 static inline int regulator_count_voltages(const struct device *dev)
 {
@@ -129,13 +132,13 @@ static inline int regulator_count_voltages(const struct device *dev)
 }
 
 /**
- * @brief Return the number of supported regulator modes
- * Returns the number of supported regulator modes. Many regulators will only
- * support one mode. Regulator modes can be set and selected with
- * regulator_set_mode
+ * @brief Obtain the number of supported modes.
  *
- * @param dev: Regulator device to count supported regulator modes for
- * @return number of supported modes
+ * @param dev Regulator device instance.
+ *
+ * @return Number of supported modes.
+ *
+ * @see regulator_set_mode()
  */
 static inline int regulator_count_modes(const struct device *dev)
 {
@@ -150,13 +153,13 @@ static inline int regulator_count_modes(const struct device *dev)
 }
 
 /**
- * @brief Return supported voltage
- * Returns a voltage that can be passed to @ref regulator_set_voltage(), zero
- * if the selector code can't be used, or a negative errno.
+ * @brief Obtain supported voltages.
  *
- * @param dev: Regulator device to get voltage for.
- * @param selector: voltage selector code.
- * @return voltage level in uV, or zero if selector code can't be used.
+ * @param dev Regulator device instance.
+ * @param selector Voltage selector.
+ *
+ * @return voltage Voltage level in microvolts.
+ * @retval 0 If selector code can't be used.
  */
 static inline int regulator_list_voltages(const struct device *dev,
 					  unsigned int selector)
@@ -172,12 +175,15 @@ static inline int regulator_list_voltages(const struct device *dev,
 }
 
 /**
- * @brief Check if a voltage range can be supported.
+ * @brief Check if a voltage range is supported.
  *
- * @param dev: Regulator to check range against.
- * @param min_uV: Minimum voltage in microvolts
- * @param max_uV: maximum voltage in microvolts
- * @returns boolean or negative error code.
+ * @param dev Regulator device instance.
+ * @param min_uV Minimum voltage in microvolts.
+ * @param max_uV maximum voltage in microvolts.
+ *
+ * @retval 0 If successful.
+ * @retval -ENOSYS If function is not implemented.
+ * @retval -errno In case of any other error.
  */
 static inline int regulator_is_supported_voltage(const struct device *dev,
 						 int min_uV, int max_uV)
@@ -193,11 +199,19 @@ static inline int regulator_is_supported_voltage(const struct device *dev,
 }
 
 /**
- * @brief Set regulator output voltage.
- * Sets a regulator to the closest supported output voltage.
- * @param dev: Regulator to set voltage
- * @param min_uV: Minimum acceptable voltage in microvolts
- * @param max_uV: Maximum acceptable voltage in microvolts
+ * @brief Set output voltage.
+ *
+ * @note The output voltage will be configured to the closest supported output
+ * voltage. regulator_get_voltage() can be used to obtain the actual configured
+ * voltage.
+ *
+ * @param dev Regulator device instance.
+ * @param min_uV Minimum acceptable voltage in microvolts.
+ * @param max_uV Maximum acceptable voltage in microvolts.
+ *
+ * @retval 0 If successful.
+ * @retval -ENOSYS If function is not implemented.
+ * @retval -errno In case of any other error.
  */
 static inline int regulator_set_voltage(const struct device *dev, int min_uV,
 					int max_uV)
@@ -213,11 +227,11 @@ static inline int regulator_set_voltage(const struct device *dev, int min_uV,
 }
 
 /**
- * @brief Get regulator output voltage.
- * Returns the current regulator voltage in microvolts
+ * @brief Obtain output voltage.
  *
- * @param dev: Regulator to query
- * @return voltage level in uV
+ * @param dev Regulator device instance.
+ *
+ * @return Voltage level in microvolts.
  */
 static inline int regulator_get_voltage(const struct device *dev)
 {
@@ -232,12 +246,15 @@ static inline int regulator_get_voltage(const struct device *dev)
 }
 
 /**
- * @brief Set regulator output current limit
- * Sets current sink to desired output current.
- * @param dev: Regulator to set output current level
- * @param min_uA: minimum microamps
- * @param max_uA: maximum microamps
- * @return 0 on success, or errno on error
+ * @brief Set output current limit.
+ *
+ * @param dev Regulator device instance.
+ * @param min_uA Minimum acceptable current limit in microamps.
+ * @param max_uA Maximum acceptable current limit in microamps.
+ *
+ * @retval 0 If successful.
+ * @retval -ENOSYS If function is not implemented.
+ * @retval -errno In case of any other error.
  */
 static inline int regulator_set_current_limit(const struct device *dev,
 					      int min_uA, int max_uA)
@@ -253,10 +270,13 @@ static inline int regulator_set_current_limit(const struct device *dev,
 }
 
 /**
- * @brief Get regulator output current.
- * Note the current limit must have been set for this call to succeed.
- * @param dev: Regulator to query
- * @return current limit in uA, or errno
+ * @brief Get output current limit.
+ *
+ * @param dev Regulator device instance.
+ *
+ * @retval current Current limit in microamperes
+ * @retval -ENOSYS If function is not implemented.
+ * @retval -errno In case of any other error.
  */
 static inline int regulator_get_current_limit(const struct device *dev)
 {
@@ -271,14 +291,19 @@ static inline int regulator_get_current_limit(const struct device *dev)
 }
 
 /**
- * @brief Select mode of regulator
+ * @brief Set mode.
+ *
  * Regulators can support multiple modes in order to permit different voltage
  * configuration or better power savings. This API will apply a mode for
  * the regulator.
- * @param dev: regulator to switch mode for
- * @param mode: Mode to select for this regulator. Only modes present
- * in the regulator-allowed-modes property are permitted.
- * @return 0 on success, or errno on error
+ *
+ * @param dev Regulator device instance.
+ * @param mode Mode to select for this regulator. Only modes present in the
+ * `regulator-allowed-modes` devicetree property are permitted.
+ *
+ * @retval 0 If successful.
+ * @retval -ENOSYS If function is not implemented.
+ * @retval -errno In case of any other error.
  */
 static inline int regulator_set_mode(const struct device *dev, uint32_t mode)
 {
@@ -293,17 +318,19 @@ static inline int regulator_set_mode(const struct device *dev, uint32_t mode)
 }
 
 /**
- * @brief Set target voltage for regulator mode
- * Part of the extended regulator consumer API.
- * sets the target voltage for a given regulator mode. This mode does
- * not need to be the active mode. This API can be used to configure
- * voltages for a mode, then the regulator can be switched to that mode
- * with the regulator_set_mode api.
- * @param dev: regulator to set voltage for
- * @param mode: target mode to configure voltage for
- * @param min_uV: minimum voltage acceptable, in uV
- * @param max_uV: maximum voltage acceptable, in uV
- * @return 0 on success, or errno on error
+ * @brief Set target voltage for a regulator mode.
+ *
+ * @note Mode does not need to be the active mode. The regulator can be switched
+ * to that mode with regulator_set_mode().
+ *
+ * @param dev Regulator device instance.
+ * @param mode Regulator mode.
+ * @param min_uV Minimum acceptable voltage in microvolts.
+ * @param max_uV Maximum acceptable voltage in microvolts.
+ *
+ * @retval 0 If successful.
+ * @retval -ENOSYS If function is not implemented.
+ * @retval -errno In case of any other error.
  */
 static inline int regulator_set_mode_voltage(const struct device *dev,
 					     uint32_t mode, uint32_t min_uV,
@@ -320,14 +347,15 @@ static inline int regulator_set_mode_voltage(const struct device *dev,
 }
 
 /**
- * @brief Get target voltage for regulator mode
- * Part of the extended regulator consumer API.
- * gets the target voltage for a given regulator mode. This mode does
- * not need to be the active mode. This API can be used to read voltages
- * from a regulator mode other than the default.
- * @param dev: regulator to query voltage from
- * @param mode: target mode to query voltage from
- * @return voltage level in uV
+ * @brief Get target voltage for a regulator mode
+ *
+ * This API can be used to read voltages from a regulator mode other than the
+ * default. The given mode does not need to be active.
+ *
+ * @param dev Regulator device instance.
+ * @param mode Regulator mode.
+ *
+ * @return Voltage level in microvolts.
  */
 static inline int regulator_get_mode_voltage(const struct device *dev,
 					     uint32_t mode)
@@ -343,12 +371,14 @@ static inline int regulator_get_mode_voltage(const struct device *dev,
 }
 
 /**
- * @brief Disable regulator for a given mode
- * Part of the extended regulator consumer API.
- * Disables the regulator in a given mode. Does not implement the
- * onoff service, as this is incompatible with multiple mode operation
- * @param dev: regulator to disable
- * @param mode: mode to change regulator state in
+ * @brief Disable regulator for a given mode.
+ *
+ * @param dev Regulator device instance.
+ * @param mode Regulator mode.
+ *
+ * @retval 0 If successful.
+ * @retval -ENOSYS If function is not implemented.
+ * @retval -errno In case of any other error.
  */
 static inline int regulator_mode_disable(const struct device *dev,
 					 uint32_t mode)
@@ -364,12 +394,14 @@ static inline int regulator_mode_disable(const struct device *dev,
 }
 
 /**
- * @brief Enable regulator for a given mode
- * Part of the extended regulator consumer API.
- * Enables the regulator in a given mode. Does not implement the
- * onoff service, as this is incompatible with multiple mode operation
- * @param dev: regulator to enable
- * @param mode: mode to change regulator state in
+ * @brief Enable regulator for a given mode.
+ *
+ * @param dev Regulator device instance.
+ * @param mode Regulator mode.
+ *
+ * @retval 0 If successful.
+ * @retval -ENOSYS If function is not implemented.
+ * @retval -errno In case of any other error.
  */
 static inline int regulator_mode_enable(const struct device *dev,
 					uint32_t mode)
