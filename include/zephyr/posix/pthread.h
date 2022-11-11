3a56d276e951dd9edd2c6882022c34970ef15f24
Chris Friedt <cfriedt@meta.com>
2022-11-11 06:57:31 -0500

    posix: cond: abstract pthread_cond_t as uint32_t
    Consistent with the change of `pthread_t` from
    `struct posix_thread` to `uint32_t`, we can now also abstract
    `pthread_cond_t` as `uint32_t` and separate `struct posix_cond`
    as an implementation detail, hidden from POSIX API consumers.
    
    This change deprecates `PTHREAD_COND_DEFINE()` in favour of the
    (standardized) `PTHREAD_COND_INITIALIZER`.
    
    This change introduces `CONFIG_MAX_PTHREAD_COND_COUNT`.
    
    Signed-off-by: Chris Friedt <cfriedt@meta.com>

--- include/zephyr/posix/pthread.h
+++ include/zephyr/posix/pthread.h
@@ -37,6 +37,13 @@ extern "C" {
 #define PTHREAD_STACK_MIN Z_KERNEL_STACK_SIZE_ADJUST(0)
 
 /**
+ * @brief Declare a condition variable as initialized
+ *
+ * Initialize a condition variable with the default condition variable attributes.
+ */
+#define PTHREAD_COND_INITIALIZER (-1)
+
+/**
  * @brief Declare a pthread condition variable
  *
  * Declaration API for a pthread condition variable.  This is not a
@@ -44,35 +51,23 @@ extern "C" {
  * strategies for kernel objects.
  *
  * @param name Symbol name of the condition variable
+ * @deprecated Use @c PTHREAD_COND_INITIALIZER instead.
  */
-#define PTHREAD_COND_DEFINE(name)					\
-	struct pthread_cond name = {					\
-		.wait_q = Z_WAIT_Q_INIT(&name.wait_q),			\
-	}
+#define PTHREAD_COND_DEFINE(name) pthread_cond_t name = PTHREAD_COND_INITIALIZER
 
 /**
  * @brief POSIX threading compatibility API
  *
  * See IEEE 1003.1
  */
-static inline int pthread_cond_init(pthread_cond_t *cv,
-				    const pthread_condattr_t *att)
-{
-	ARG_UNUSED(att);
-	z_waitq_init(&cv->wait_q);
-	return 0;
-}
+int pthread_cond_init(pthread_cond_t *cv, const pthread_condattr_t *att);
 
 /**
  * @brief POSIX threading compatibility API
  *
  * See IEEE 1003.1
  */
-static inline int pthread_cond_destroy(pthread_cond_t *cv)
-{
-	ARG_UNUSED(cv);
-	return 0;
-}
+int pthread_cond_destroy(pthread_cond_t *cv);
 
 /**
  * @brief POSIX threading compatibility API
