43d4b4eebdb7e34db71d0df3325f97344e959b03
Morten Priess <mtpr@oticon.com>
2022-11-14 14:40:18 +0100

    Bluetooth: controller: CIS Central fixes from EBQ run
    Various fixes for issues found during EBQ run.
    
    Signed-off-by: Morten Priess <mtpr@oticon.com>

--- tests/bluetooth/controller/mock_ctrl/src/ull_peripheral_iso.c
+++ tests/bluetooth/controller/mock_ctrl/src/ull_peripheral_iso.c
@@ -56,11 +56,6 @@ uint8_t ull_peripheral_iso_setup(struct pdu_data_llctrl_cis_ind *ind,
 	return 0;
 }
 
-void ull_peripheral_iso_start(struct ll_conn *acl, uint32_t ticks_at_expire,
-			      uint16_t cis_handle)
-{
-}
-
 void ull_peripheral_iso_update_peer_sca(struct ll_conn *acl)
 {
 }
