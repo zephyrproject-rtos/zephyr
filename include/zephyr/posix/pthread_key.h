17fe89f9a044d997b1caf55d2ab42981e6dbb005
Chris Friedt <cfriedt@meta.com>
2022-11-16 16:09:01 -0500

    posix: key: abstract pthread_key_t as uint32_t
    Consistent with the change of `pthread_t`, `pthread_mutex_t`,
    and `pthread_cond_t` to `uint32_t`, we can now also abstract
    `pthread_key_t` as `uint32_t` and separate the implementation
    detail, hidden from POSIX API consumers.
    
    This change introduces `CONFIG_MAX_PTHREAD_KEY_COUNT`.
    
    Signed-off-by: Chris Friedt <cfriedt@meta.com>

--- include/zephyr/posix/pthread_key.h
+++ include/zephyr/posix/pthread_key.h
@@ -18,30 +18,7 @@ extern "C" {
 typedef uint32_t pthread_once_t;
 
 /* pthread_key */
-typedef void *pthread_key_t;
-
-typedef struct pthread_key_obj {
-	/* List of pthread_key_data objects that contain thread
-	 * specific data for the key
-	 */
-	sys_slist_t key_data_l;
-
-	/* Optional destructor that is passed to pthread_key_create() */
-	void (*destructor)(void *);
-} pthread_key_obj;
-
-typedef struct pthread_thread_data {
-	sys_snode_t node;
-
-	/* Key and thread specific data passed to pthread_setspecific() */
-	pthread_key_obj *key;
-	void *spec_data;
-} pthread_thread_data;
-
-typedef struct pthread_key_data {
-	sys_snode_t node;
-	pthread_thread_data thread_data;
-} pthread_key_data;
+typedef uint32_t pthread_key_t;
 
 #ifdef __cplusplus
 }
