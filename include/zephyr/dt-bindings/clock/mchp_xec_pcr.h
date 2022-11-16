e2266d14efcef411e86edeafd802ec99abef4e3a
Jay Vasanth <jay.vasanth@microchip.com>
2022-11-16 12:00:41 -0500

    drivers: clock: Microchip MEC172x clock control driver support all modes
    Fix Microchip XEC clock control driver single-ended XTAL2 pin
    initialization. Add support for external 32KHZ_IN pin as a
    clock source including PINTRL to switch the GPIO to 32KHZ_IN
    function. Add device tree option to disable internal silicon
    oscillator if it is not required by the configuration. Add
    device tree tuning options based on crystal and board layout.
    
    Signed-off-by: Jay Vasanth <jay.vasanth@microchip.com>

--- include/zephyr/dt-bindings/clock/mchp_xec_pcr.h
+++ include/zephyr/dt-bindings/clock/mchp_xec_pcr.h
@@ -7,29 +7,23 @@
 #ifndef ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_MCHP_XEC_H_
 #define ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_MCHP_XEC_H_
 
-/* PLL and Peripheral 32 KHz clock source */
-#define MCHP_XEC_CLK32K_SRC_SIL_OSC	0U
-#define MCHP_XEC_CLK32K_SRC_XTAL	1U
-#define MCHP_XEC_CLK32K_SRC_PIN		2U
+/* PLL 32KHz clock source VTR rail ON. */
+#define MCHP_XEC_PLL_CLK32K_SRC_SIL_OSC		0U
+#define MCHP_XEC_PLL_CLK32K_SRC_XTAL		1U
+#define MCHP_XEC_PLL_CLK32K_SRC_PIN		2U
 
-/* Crystal connection */
-#define MCHP_XEC_XTAL_PARALLEL		0U
-#define MCHP_XEC_XTAL_SINGLE_ENDED	1U
-
-/*
- * When the 32KHz pin goes down fall back
- * to either internal silicon oscillator
- * or crystal.
- */
-#define MCHP_XEC_PIN32K_FB_SIL_OSC	0U
-#define MCHP_XEC_PIN32K_FB_XTAL		1U
+/* Peripheral 32KHz clock source for VTR rail ON and off(VBAT operation) */
+#define MCHP_XEC_PERIPH_CLK32K_SRC_SO_SO	0U
+#define MCHP_XEC_PERIPH_CLK32K_SRC_XTAL_XTAL	1U
+#define MCHP_XEC_PERIPH_CLK32K_SRC_PIN_SO	2U
+#define MCHP_XEC_PERIPH_CLK32K_SRC_PIN_XTAL	3U
 
 /* clocks supported by the driver */
-#define MCHP_XEC_PCR_CLK_CORE		0
-#define MCHP_XEC_PCR_CLK_CPU		1
-#define MCHP_XEC_PCR_CLK_BUS		2
-#define MCHP_XEC_PCR_CLK_PERIPH		3
-#define MCHP_XEC_PCR_CLK_PERIPH_FAST	4
-#define MCHP_XEC_PCR_CLK_PERIPH_SLOW	5
+#define MCHP_XEC_PCR_CLK_CORE			0
+#define MCHP_XEC_PCR_CLK_CPU			1
+#define MCHP_XEC_PCR_CLK_BUS			2
+#define MCHP_XEC_PCR_CLK_PERIPH			3
+#define MCHP_XEC_PCR_CLK_PERIPH_FAST		4
+#define MCHP_XEC_PCR_CLK_PERIPH_SLOW		5
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_MCHP_XEC_H_ */
