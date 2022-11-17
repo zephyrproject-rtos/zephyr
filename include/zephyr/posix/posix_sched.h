7be31813daa3c8c72f10db7b25866f79fbd26f38
Chris Friedt <cfriedt@meta.com>
2022-11-16 21:02:05 -0500

    posix: sched: use newlib-compatible SCHED_RR FIFO definitions
    Zephyr's `SCHED_RR` and `SCHED_FIFO` definitions were slightly
    different than Newlib's. Additionally, the test had hard-coded
    magic numbers instead of using symbolic values.
    
    Signed-off-by: Chris Friedt <cfriedt@meta.com>

--- include/zephyr/posix/posix_sched.h
+++ include/zephyr/posix/posix_sched.h
@@ -11,14 +11,10 @@ extern "C" {
 #endif
 
 /* Cooperative scheduling policy */
-#ifndef SCHED_FIFO
-#define SCHED_FIFO 0
-#endif /* SCHED_FIFO */
+#define SCHED_FIFO 1
 
 /* Priority based preemptive scheduling policy */
-#ifndef SCHED_RR
-#define SCHED_RR 1
-#endif /* SCHED_RR */
+#define SCHED_RR 2
 
 struct sched_param {
 	int sched_priority;
