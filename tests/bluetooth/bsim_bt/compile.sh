47ceb5cf81a6154180c528aab89d13db516a2142
Stephanos Ioannidis <stephanos.ioannidis@nordicsemi.no>
2022-11-26 15:31:22 +0900

    tests: bluetooth: bsim_bt: Enable parallel compilation
    This commit updates the Bluetooth BSIM test compilation script to build
    multiple tests simultaneously in order to improve CPU utilisation.
    
    Signed-off-by: Stephanos Ioannidis <stephanos.ioannidis@nordicsemi.no>

--- tests/bluetooth/bsim_bt/compile.sh
+++ tests/bluetooth/bsim_bt/compile.sh
@@ -20,47 +20,49 @@ mkdir -p ${WORK_DIR}
 
 source ${ZEPHYR_BASE}/tests/bluetooth/bsim_bt/compile.source
 
-app=tests/bluetooth/bsim_bt/bsim_test_bond_overwrite_allowed compile
-app=tests/bluetooth/bsim_bt/bsim_test_bond_overwrite_denied compile
-app=tests/bluetooth/bsim_bt/bsim_test_notify compile
-app=tests/bluetooth/bsim_bt/bsim_test_notify_multiple compile
-app=tests/bluetooth/bsim_bt/bsim_test_eatt_notif conf_file=prj.conf compile
-app=tests/bluetooth/bsim_bt/bsim_test_gatt_caching compile
-app=tests/bluetooth/bsim_bt/bsim_test_eatt conf_file=prj_collision.conf compile
-app=tests/bluetooth/bsim_bt/bsim_test_eatt conf_file=prj_multiple_conn.conf compile
-app=tests/bluetooth/bsim_bt/bsim_test_eatt conf_file=prj_autoconnect.conf compile
+app=tests/bluetooth/bsim_bt/bsim_test_bond_overwrite_allowed compile &
+app=tests/bluetooth/bsim_bt/bsim_test_bond_overwrite_denied compile &
+app=tests/bluetooth/bsim_bt/bsim_test_notify compile &
+app=tests/bluetooth/bsim_bt/bsim_test_notify_multiple compile &
+app=tests/bluetooth/bsim_bt/bsim_test_eatt_notif conf_file=prj.conf compile &
+app=tests/bluetooth/bsim_bt/bsim_test_gatt_caching compile &
+app=tests/bluetooth/bsim_bt/bsim_test_eatt conf_file=prj_collision.conf compile &
+app=tests/bluetooth/bsim_bt/bsim_test_eatt conf_file=prj_multiple_conn.conf compile &
+app=tests/bluetooth/bsim_bt/bsim_test_eatt conf_file=prj_autoconnect.conf compile &
 app=tests/bluetooth/bsim_bt/bsim_test_app conf_file=prj_split.conf \
-  compile
+  compile &
 app=tests/bluetooth/bsim_bt/bsim_test_app conf_file=prj_split_privacy.conf \
-  compile
+  compile &
 app=tests/bluetooth/bsim_bt/bsim_test_app conf_file=prj_split_low_lat.conf \
-  compile
-app=tests/bluetooth/bsim_bt/bsim_test_multiple compile
-app=tests/bluetooth/bsim_bt/bsim_test_advx compile
-app=tests/bluetooth/bsim_bt/bsim_test_adv_chain compile
-app=tests/bluetooth/bsim_bt/bsim_test_gatt compile
-app=tests/bluetooth/bsim_bt/bsim_test_gatt_write compile
-app=tests/bluetooth/bsim_bt/bsim_test_l2cap compile
-app=tests/bluetooth/bsim_bt/bsim_test_l2cap_userdata compile
-app=tests/bluetooth/bsim_bt/bsim_test_l2cap_stress compile
-app=tests/bluetooth/bsim_bt/bsim_test_iso compile
+  compile &
+app=tests/bluetooth/bsim_bt/bsim_test_multiple compile &
+app=tests/bluetooth/bsim_bt/bsim_test_advx compile &
+app=tests/bluetooth/bsim_bt/bsim_test_adv_chain compile &
+app=tests/bluetooth/bsim_bt/bsim_test_gatt compile &
+app=tests/bluetooth/bsim_bt/bsim_test_gatt_write compile &
+app=tests/bluetooth/bsim_bt/bsim_test_l2cap compile &
+app=tests/bluetooth/bsim_bt/bsim_test_l2cap_userdata compile &
+app=tests/bluetooth/bsim_bt/bsim_test_l2cap_stress compile &
+app=tests/bluetooth/bsim_bt/bsim_test_iso compile &
 app=tests/bluetooth/bsim_bt/bsim_test_iso conf_file=prj_vs_dp.conf \
-  compile
-app=tests/bluetooth/bsim_bt/bsim_test_audio compile
+  compile &
+app=tests/bluetooth/bsim_bt/bsim_test_audio compile &
 app=tests/bluetooth/bsim_bt/edtt_ble_test_app/hci_test_app \
-  conf_file=prj_dut_llcp.conf compile
+  conf_file=prj_dut_llcp.conf compile &
 app=tests/bluetooth/bsim_bt/edtt_ble_test_app/hci_test_app \
-  conf_file=prj_tst_llcp.conf compile
+  conf_file=prj_tst_llcp.conf compile &
 app=tests/bluetooth/bsim_bt/edtt_ble_test_app/hci_test_app \
-  conf_file=prj_dut.conf compile
+  conf_file=prj_dut.conf compile &
 app=tests/bluetooth/bsim_bt/edtt_ble_test_app/hci_test_app \
-  conf_file=prj_tst.conf compile
+  conf_file=prj_tst.conf compile &
 app=tests/bluetooth/bsim_bt/edtt_ble_test_app/gatt_test_app \
-  conf_file=prj.conf compile
+  conf_file=prj.conf compile &
 app=tests/bluetooth/bsim_bt/edtt_ble_test_app/gatt_test_app \
-  conf_file=prj_llcp.conf compile
-app=tests/bluetooth/bsim_bt/bsim_test_mesh compile
-app=tests/bluetooth/bsim_bt/bsim_test_mesh conf_file=prj_low_lat.conf compile
-app=tests/bluetooth/bsim_bt/bsim_test_mesh conf_file=prj_pst.conf compile
-app=tests/bluetooth/bsim_bt/bsim_test_mesh conf_file=prj_gatt.conf compile
-app=tests/bluetooth/bsim_bt/bsim_test_disable compile
+  conf_file=prj_llcp.conf compile &
+app=tests/bluetooth/bsim_bt/bsim_test_mesh compile &
+app=tests/bluetooth/bsim_bt/bsim_test_mesh conf_file=prj_low_lat.conf compile &
+app=tests/bluetooth/bsim_bt/bsim_test_mesh conf_file=prj_pst.conf compile &
+app=tests/bluetooth/bsim_bt/bsim_test_mesh conf_file=prj_gatt.conf compile &
+app=tests/bluetooth/bsim_bt/bsim_test_disable compile &
+
+wait
