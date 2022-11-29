f048298c8c143908bf8f790778ad4a944540e54e
Emil Gydesen <emil.gydesen@nordicsemi.no>
2022-11-29 13:44:04 +0100

    Bluetooth: Audio: Add common audio shell print commands to audio.h
    Move common audio shell print functions into audio.h.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/audio/shell/audio.h
+++ subsys/bluetooth/audio/shell/audio.h
@@ -13,7 +13,13 @@
 #ifndef __AUDIO_H
 #define __AUDIO_H
 
+#include <stdio.h>
+
+#include <zephyr/bluetooth/audio/audio.h>
+#include <zephyr/bluetooth/audio/bap.h>
 #include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/shell/shell.h>
+#include <zephyr/sys/printk.h>
 
 extern struct bt_csip_set_member_svc_inst *svc_inst;
 
@@ -65,6 +71,101 @@ extern const struct named_lc3_preset *default_sink_preset;
 extern const struct named_lc3_preset *default_source_preset;
 #endif /* CONFIG_BT_BAP_UNICAST_CLIENT */
 #endif /* CONFIG_BT_BAP_UNICAST */
+
+static inline void print_qos(const struct shell *sh, const struct bt_codec_qos *qos)
+{
+#if defined(CONFIG_BT_BAP_BROADCAST_SOURCE) || defined(CONFIG_BT_BAP_UNICAST)
+	shell_print(sh,
+		    "QoS: interval %u framing 0x%02x phy 0x%02x sdu %u rtn %u latency %u pd %u",
+		    qos->interval, qos->framing, qos->phy, qos->sdu, qos->rtn, qos->latency,
+		    qos->pd);
+#else
+	shell_print(sh, "QoS: interval %u framing 0x%02x phy 0x%02x sdu %u rtn %u pd %u",
+		    qos->interval, qos->framing, qos->phy, qos->sdu, qos->rtn, qos->pd);
+#endif /* CONFIG_BT_BAP_BROADCAST_SOURCE || CONFIG_BT_BAP_UNICAST */
+}
+
+static inline void print_codec(const struct shell *sh, const struct bt_codec *codec)
+{
+	shell_print(sh, "codec 0x%02x cid 0x%04x vid 0x%04x", codec->id, codec->cid, codec->vid);
+
+#if CONFIG_BT_CODEC_MAX_DATA_COUNT > 0
+	shell_print(sh, "data_count %u", codec->data_count);
+	for (size_t i = 0U; i < codec->data_count; i++) {
+		shell_print(sh, "data #%u: type 0x%02x len %u", i, codec->data[i].data.type,
+			    codec->data[i].data.data_len);
+		shell_hexdump(sh, codec->data[i].data.data,
+			      codec->data[i].data.data_len - sizeof(codec->data[i].data.type));
+	}
+#endif /* CONFIG_BT_CODEC_MAX_DATA_COUNT > 0 */
+
+#if CONFIG_BT_CODEC_MAX_METADATA_COUNT > 0
+	shell_print(sh, "meta_count %u", codec->data_count);
+	for (size_t i = 0U; i < codec->meta_count; i++) {
+		shell_print(sh, "meta #%u: type 0x%02x len %u", i, codec->meta[i].data.type,
+			    codec->meta[i].data.data_len);
+		shell_hexdump(sh, codec->meta[i].data.data,
+			      codec->meta[i].data.data_len - sizeof(codec->meta[i].data.type));
+	}
+#endif /* CONFIG_BT_CODEC_MAX_METADATA_COUNT > 0 */
+}
+
+#if BROADCAST_SNK_SUBGROUP_CNT > 0
+static inline void print_base(const struct shell *sh, const struct bt_bap_base *base)
+{
+	uint8_t bis_indexes[BT_ISO_MAX_GROUP_ISO_COUNT] = {0};
+	/* "0xXX " requires 5 characters */
+	char bis_indexes_str[5 * ARRAY_SIZE(bis_indexes) + 1];
+	size_t index_count = 0;
+
+	for (size_t i = 0U; i < base->subgroup_count; i++) {
+		const struct bt_bap_base_subgroup *subgroup;
+
+		subgroup = &base->subgroups[i];
+
+		shell_print(sh, "Subgroup[%d]:", i);
+		print_codec(sh, &subgroup->codec);
+
+		for (size_t j = 0U; j < subgroup->bis_count; j++) {
+			const struct bt_bap_base_bis_data *bis_data;
+
+			bis_data = &subgroup->bis_data[j];
+
+			shell_print(sh, "BIS[%d] index 0x%02x", j, bis_data->index);
+			bis_indexes[index_count++] = bis_data->index;
+
+#if CONFIG_BT_CODEC_MAX_DATA_COUNT > 0
+			for (size_t k = 0U; k < bis_data->data_count; k++) {
+				const struct bt_codec_data *codec_data;
+
+				codec_data = &bis_data->data[k];
+
+				shell_print(sh, "data #%u: type 0x%02x len %u", k,
+					    codec_data->data.type, codec_data->data.data_len);
+				shell_hexdump(sh, codec_data->data.data,
+					      codec_data->data.data_len -
+						      sizeof(codec_data->data.type));
+			}
+#endif /* CONFIG_BT_CODEC_MAX_DATA_COUNT > 0 */
+		}
+	}
+
+	(void)memset(bis_indexes_str, 0, sizeof(bis_indexes_str));
+
+	/* Create space separated list of indexes as hex values */
+	for (size_t i = 0U; i < index_count; i++) {
+		char bis_index_str[6];
+
+		sprintf(bis_index_str, "0x%02x ", bis_indexes[i]);
+
+		strcat(bis_indexes_str, bis_index_str);
+		shell_print(sh, "[%d]: %s", i, bis_index_str);
+	}
+
+	shell_print(sh, "Possible indexes: %s", bis_indexes_str);
+}
+#endif /* BROADCAST_SNK_SUBGROUP_CNT > 0 */
+
 #endif /* CONFIG_BT_AUDIO */
 
 #endif /* __AUDIO_H */
