01926494a9aaf7567a3db4d9fedf00539c808e44
Manuel Arguelles <manuel.arguelles@nxp.com>
2022-11-29 09:51:33 +0700

    drivers: mdio: add support for NXP S32 NETC EMDIO
    Introduce NXP S32 NETC External MDIO controller driver. Driver supports
    a single instance, as current support is based on NXP S32Z/E SoCs.
    
    Signed-off-by: Manuel Arguelles <manuel.arguelles@nxp.com>

--- drivers/mdio/mdio_shell.c
+++ drivers/mdio/mdio_shell.c
@@ -18,6 +18,8 @@ LOG_MODULE_REGISTER(mdio_shell, CONFIG_LOG_DEFAULT_LEVEL);
 #define DT_DRV_COMPAT atmel_sam_mdio
 #elif DT_HAS_COMPAT_STATUS_OKAY(espressif_esp32_mdio)
 #define DT_DRV_COMPAT espressif_esp32_mdio
+#elif DT_HAS_COMPAT_STATUS_OKAY(nxp_s32_netc_emdio)
+#define DT_DRV_COMPAT nxp_s32_netc_emdio
 #else
 #error "No known devicetree compatible match for MDIO shell"
 #endif
