916e02563cf5742a0948c0ee9987083c32ce4d34
Kamil Piszczek <Kamil.Piszczek@nordicsemi.no>
2022-11-29 14:57:30 +0100

    bluetooth: host: smp: unauth bond overwrite on different identity
    Added a new Kconfig option for the Bluetooth Host to allow
    unauthenticated pairing attempts made by the peer where an
    unauthenticated bond already exists on other local identity.
    
    Signed-off-by: Kamil Piszczek <Kamil.Piszczek@nordicsemi.no>

--- subsys/bluetooth/host/smp.c
+++ subsys/bluetooth/host/smp.c
@@ -565,7 +565,10 @@ static bool update_keys_check(struct bt_smp *smp, struct bt_keys *keys)
 	if (!IS_ENABLED(CONFIG_BT_SMP_ALLOW_UNAUTH_OVERWRITE) &&
 	    (!(keys->flags & BT_KEYS_AUTHENTICATED)
 	     && smp->method == JUST_WORKS)) {
-		return false;
+		if (!IS_ENABLED(CONFIG_BT_ID_ALLOW_UNAUTH_OVERWRITE) ||
+		    (keys->id == smp->chan.chan.conn->id)) {
+			return false;
+		}
 	}
 
 	return true;
