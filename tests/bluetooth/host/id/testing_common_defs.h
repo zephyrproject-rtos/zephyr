7df78a37857a9e6240a225074b1074f8407689e2
Ahmed Moheb <ahmed.moheb@nordicsemi.no>
2022-11-30 12:46:46 +0200

    tests: bluetooth: host: Add UT for bt_id_create()
    Unit test project for bt_id_create().
    This part of subsys/bluetooth/host/id.c unit testing.
    
    Signed-off-by: Ahmed Moheb <ahmed.moheb@nordicsemi.no>

--- tests/bluetooth/host/id/testing_common_defs.h
+++ tests/bluetooth/host/id/testing_common_defs.h
@@ -7,5 +7,15 @@
 /** Bluetooth device "test" addresses */
 #define BT_ADDR ((bt_addr_t[]){{{0x0A, 0x89, 0x67, 0x45, 0x23, 0x01}}})
 
-/** Bluetooth LE device "test" public addresses */
+/** Bluetooth LE public device "test" addresses */
 #define BT_LE_ADDR ((bt_addr_le_t[]){{BT_ADDR_LE_PUBLIC, {{0x0A, 0x89, 0x67, 0x45, 0x23, 0x01}}}})
+
+/** Bluetooth LE device "test" static random addresses */
+#define BT_STATIC_RANDOM_LE_ADDR_1                                                                 \
+	((bt_addr_le_t[]){{BT_ADDR_LE_RANDOM, {{0x0A, 0x89, 0x67, 0x45, 0x23, 0xC1}}}})
+#define BT_STATIC_RANDOM_LE_ADDR_2                                                                 \
+	((bt_addr_le_t[]){{BT_ADDR_LE_RANDOM, {{0x0B, 0x89, 0x67, 0x45, 0x23, 0xC1}}}})
+
+/** Bluetooth LE device "test" random resolvable private addresses */
+#define BT_RPA_LE_ADDR                                                                             \
+	((bt_addr_le_t[]){{BT_ADDR_LE_RANDOM, {{0x0A, 0x89, 0x67, 0x45, 0x23, 0x41}}}})
