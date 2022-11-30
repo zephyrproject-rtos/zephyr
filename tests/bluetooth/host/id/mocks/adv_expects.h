af92e7f9d11cd56241f05bbbaf56e2a873000be5
Ahmed Moheb <ahmed.moheb@nordicsemi.no>
2022-11-30 10:58:13 +0200

    tests: bluetooth: host: Add UT for bt_id_add()
    Unit test project for bt_id_add().
    This part of subsys/bluetooth/host/id.c unit testing.
    
    Signed-off-by: Ahmed Moheb <ahmed.moheb@nordicsemi.no>
    
    # Conflicts:
    #	tests/bluetooth/host/id/CMakeLists.txt

--- tests/bluetooth/host/id/mocks/adv_expects.h
+++ tests/bluetooth/host/id/mocks/adv_expects.h
@@ -21,3 +21,19 @@ void expect_single_call_bt_le_adv_lookup_legacy(void);
  *   - bt_le_adv_lookup_legacy() isn't called at all
  */
 void expect_not_called_bt_le_adv_lookup_legacy(void);
+
+/*
+ *  Validate expected behaviour when bt_le_ext_adv_foreach() is called
+ *
+ *  Expected behaviour:
+ *   - bt_le_ext_adv_foreach() to be called once with correct parameters
+ */
+void expect_single_call_bt_le_ext_adv_foreach(void);
+
+/*
+ *  Validate expected behaviour when bt_le_ext_adv_foreach() isn't called
+ *
+ *  Expected behaviour:
+ *   - bt_le_ext_adv_foreach() isn't called at all
+ */
+void expect_not_called_bt_le_ext_adv_foreach(void);
