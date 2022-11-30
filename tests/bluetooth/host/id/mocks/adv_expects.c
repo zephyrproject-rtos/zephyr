5c04093a89d56203304306f7ba66847e45ea2233
Ahmed Moheb <ahmed.moheb@nordicsemi.no>
2022-11-30 10:58:13 +0200

    tests: bluetooth: host: Add UT for bt_id_add()
    Unit test project for bt_id_add().
    This part of subsys/bluetooth/host/id.c unit testing.
    
    Signed-off-by: Ahmed Moheb <ahmed.moheb@nordicsemi.no>
    
    # Conflicts:
    #	tests/bluetooth/host/id/CMakeLists.txt

--- tests/bluetooth/host/id/mocks/adv_expects.c
+++ tests/bluetooth/host/id/mocks/adv_expects.c
@@ -24,3 +24,22 @@ void expect_not_called_bt_le_adv_lookup_legacy(void)
 	zassert_equal(bt_le_adv_lookup_legacy_fake.call_count, 0, "'%s()' was called unexpectedly",
 		      func_name);
 }
+
+void expect_single_call_bt_le_ext_adv_foreach(void)
+{
+	const char *func_name = "bt_le_ext_adv_foreach";
+
+	zassert_equal(bt_le_ext_adv_foreach_fake.call_count, 1, "'%s()' was called more than once",
+		      func_name);
+
+	zassume_not_null(bt_le_ext_adv_foreach_fake.arg0_val,
+			 "'%s()' was called with incorrect '%s' value", func_name, "func");
+}
+
+void expect_not_called_bt_le_ext_adv_foreach(void)
+{
+	const char *func_name = "bt_le_ext_adv_foreach";
+
+	zassert_equal(bt_le_ext_adv_foreach_fake.call_count, 0, "'%s()' was called unexpectedly",
+		      func_name);
+}
