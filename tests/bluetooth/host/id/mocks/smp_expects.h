e1d0df2bb3772c3b3bc775fe512d571274ebdf49
Ahmed Moheb <ahmed.moheb@nordicsemi.no>
2022-11-30 11:25:05 +0200

    tests: bluetooth: host: Add UT for bt_le_oob_get_local()
    Unit test project for bt_le_oob_get_local().
    This part of subsys/bluetooth/host/id.c unit testing.
    
    Signed-off-by: Ahmed Moheb <ahmed.moheb@nordicsemi.no>
    
    # Conflicts:
    #	tests/bluetooth/host/id/CMakeLists.txt

---
+++ tests/bluetooth/host/id/mocks/smp_expects.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+/*
+ *  Validate expected behaviour when bt_smp_le_oob_generate_sc_data() is called
+ *
+ *  Expected behaviour:
+ *   - bt_smp_le_oob_generate_sc_data() to be called once with correct parameters
+ */
+void expect_single_call_bt_smp_le_oob_generate_sc_data(struct bt_le_oob_sc_data *le_sc_oob);
+
+/*
+ *  Validate expected behaviour when bt_smp_le_oob_generate_sc_data() isn't called
+ *
+ *  Expected behaviour:
+ *   - bt_smp_le_oob_generate_sc_data() isn't called at all
+ */
+void expect_not_called_bt_smp_le_oob_generate_sc_data(void);
