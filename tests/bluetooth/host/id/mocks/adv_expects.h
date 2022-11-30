41f28c885353ad4729ea99b442176bd3b092e7c8
Ahmed Moheb <ahmed.moheb@nordicsemi.no>
2022-11-30 10:48:29 +0200

    tests: bluetooth: host: Add UT for bt_id_scan_random_addr_check()
    Unit test project for bt_id_scan_random_addr_check().
    This part of subsys/bluetooth/host/id.c unit testing.
    
    Signed-off-by: Ahmed Moheb <ahmed.moheb@nordicsemi.no>
    
    # Conflicts:
    #	tests/bluetooth/host/id/CMakeLists.txt

---
+++ tests/bluetooth/host/id/mocks/adv_expects.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+/*
+ *  Validate expected behaviour when bt_le_adv_lookup_legacy() is called
+ *
+ *  Expected behaviour:
+ *   - bt_le_adv_lookup_legacy() to be called once with correct parameters
+ */
+void expect_single_call_bt_le_adv_lookup_legacy(void);
+
+/*
+ *  Validate expected behaviour when bt_le_adv_lookup_legacy() isn't called
+ *
+ *  Expected behaviour:
+ *   - bt_le_adv_lookup_legacy() isn't called at all
+ */
+void expect_not_called_bt_le_adv_lookup_legacy(void);
