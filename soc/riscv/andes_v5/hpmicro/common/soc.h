80f757661976959edaf07d17bba3920c6b36186a
Weiwei Guo <guoweiwei@syriusrobotics.com>
2022-12-01 18:38:15 +0800

    soc: riscv: andes_v5: add hpmicro soc support
    This is an risc-v core chip with dual-core processor.
    
    Signed-off-by: Weiwei Guo <guoweiwei@syriusrobotics.com>

---
+++ soc/riscv/andes_v5/hpmicro/common/soc.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2022 HPMicro
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+/**
+ * @brief Macros for the Andes HPMICRO platform
+ */
+
+#ifndef __RISCV_ANDES_HPMICRO_SOC_H_
+#define __RISCV_ANDES_HPMICRO_SOC_H_
+
+/* Include CSRs available for Andes V5 SoCs */
+#include "soc_v5.h"
+
+#endif /* __RISCV_ANDES_HPMICRO_SOC_H_ */
