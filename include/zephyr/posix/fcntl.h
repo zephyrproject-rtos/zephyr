ecf9a6a51cf520b8f782b9e37b48079da12a311f
Chris Friedt <cfriedt@meta.com>
2022-12-02 13:30:23 -0500

    posix: fcntl.h: define constants to agree with picolibc
    PicoLibC defines `O_CREAT` (really `_FCREAT`) as 0x0040.
    
    Otherwise, these constants are identical.
    
    Signed-off-by: Chris Friedt <cfriedt@meta.com>

--- include/zephyr/posix/fcntl.h
+++ include/zephyr/posix/fcntl.h
@@ -7,9 +7,14 @@
 #ifndef ZEPHYR_POSIX_FCNTL_H_
 #define ZEPHYR_POSIX_FCNTL_H_
 
-#define O_CREAT    0x0200
+#ifdef CONFIG_PICOLIBC
+#define O_CREAT 0x0040
+#else
+#define O_CREAT 0x0200
+#endif
+
 #define O_APPEND   0x0400
-#define O_EXCL     0x0800
+#define O_EXCL	   0x0800
 #define O_NONBLOCK 0x4000
 
 #define F_DUPFD 0
