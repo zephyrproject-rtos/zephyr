10ce9684c37ca96368d26a0673af9fffc95fc13f
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2022-12-07 11:03:13 +0100

    drivers: regulator: fix/improve usage of devicetree properties
    Most of devicetree properties for regulator, such as:
    
    - regulator-min/max-microvolt
    - regulator-min/max-microamp
    - regulator-allowed-modes
    - etc.
    
    Are meant to specify limits on what consumers may set. They are **NOT**
    meant to describe the hardware capabilities. For example, I could have a
    BUCK converter that supports 0-5V output voltage, but my circuit may
    only allow working on the 2.7-3.3V range.
    
    This patch reworks the API so that the API class layer manages this
    information. This is done by drivers collecting all such fields in a
    common configuration structure that is later accessed by the class
    layer. This simplifies drivers implementation. For example, if A
    consumer calls regulator_set_voltage() with a voltage that is supported
    but not allowed, driver code won't be called. Similarly, if a regulator
    is configured to be `always-on`, enable/disable driver code will never
    be called.
    
    Drivers have been adjusted. PCA9420 mode settings have been removed from
    devicetree in this commit as they are not actual modes but PMIC states.
    This will be refactored in a follow-up commit.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- drivers/regulator/regulator_common.c
+++ drivers/regulator/regulator_common.c
@@ -16,18 +16,29 @@ void regulator_common_data_init(const struct device *dev)
 
 int regulator_enable(const struct device *dev)
 {
+	const struct regulator_driver_api *api =
+		(const struct regulator_driver_api *)dev->api;
+	const struct regulator_common_config *config =
+		(struct regulator_common_config *)dev->config;
 	struct regulator_common_data *data =
 		(struct regulator_common_data *)dev->data;
 	int ret = 0;
+
+	/* enable not supported (always on) */
+	if (api->enable == NULL) {
+		return 0;
+	}
+
+	/* regulator must stay always on */
+	if  ((config->flags & REGULATOR_ALWAYS_ON) != 0U) {
+		return 0;
+	}
 
 	(void)k_mutex_lock(&data->lock, K_FOREVER);
 
 	data->refcnt++;
 
 	if (data->refcnt == 1) {
-		const struct regulator_driver_api *api =
-			(const struct regulator_driver_api *)dev->api;
-
 		ret = api->enable(dev);
 		if (ret < 0) {
 			data->refcnt--;
@@ -41,18 +52,29 @@ int regulator_enable(const struct device *dev)
 
 int regulator_disable(const struct device *dev)
 {
+	const struct regulator_driver_api *api =
+		(const struct regulator_driver_api *)dev->api;
+	const struct regulator_common_config *config =
+		(struct regulator_common_config *)dev->config;
 	struct regulator_common_data *data =
 		(struct regulator_common_data *)dev->data;
 	int ret = 0;
+
+	/* disable not supported (always on) */
+	if (api->disable == NULL) {
+		return 0;
+	}
+
+	/* regulator must stay always on */
+	if  ((config->flags & REGULATOR_ALWAYS_ON) != 0U) {
+		return 0;
+	}
 
 	(void)k_mutex_lock(&data->lock, K_FOREVER);
 
 	data->refcnt--;
 
 	if (data->refcnt == 0) {
-		const struct regulator_driver_api *api =
-			(const struct regulator_driver_api *)dev->api;
-
 		ret = api->disable(dev);
 		if (ret < 0) {
 			data->refcnt++;
@@ -67,7 +89,16 @@ int regulator_disable(const struct device *dev)
 bool regulator_is_supported_voltage(const struct device *dev, int32_t min_uv,
 				    int32_t max_uv)
 {
-	unsigned int volt_cnt = regulator_count_voltages(dev);
+	const struct regulator_common_config *config =
+		(struct regulator_common_config *)dev->config;
+	unsigned int volt_cnt;
+
+	/* voltage may not be allowed, even if supported */
+	if ((min_uv < config->min_uv) || (max_uv > config->max_uv)) {
+		return -EINVAL;
+	}
+
+	volt_cnt = regulator_count_voltages(dev);
 
 	for (unsigned int idx = 0U; idx < volt_cnt; idx++) {
 		int32_t volt_uv;
@@ -80,4 +111,70 @@ bool regulator_is_supported_voltage(const struct device *dev, int32_t min_uv,
 	}
 
 	return false;
+}
+
+int regulator_set_voltage(const struct device *dev, int32_t min_uv,
+			  int32_t max_uv)
+{
+	const struct regulator_common_config *config =
+		(struct regulator_common_config *)dev->config;
+	const struct regulator_driver_api *api =
+		(const struct regulator_driver_api *)dev->api;
+
+	if (api->set_voltage == NULL) {
+		return -ENOSYS;
+	}
+
+	/* voltage may not be allowed, even if supported */
+	if ((min_uv < config->min_uv) || (max_uv > config->max_uv)) {
+		return -EINVAL;
+	}
+
+	return api->set_voltage(dev, min_uv, max_uv);
+}
+
+int regulator_set_current_limit(const struct device *dev, int32_t min_ua,
+				int32_t max_ua)
+{
+	const struct regulator_common_config *config =
+		(struct regulator_common_config *)dev->config;
+	const struct regulator_driver_api *api =
+		(const struct regulator_driver_api *)dev->api;
+
+	if (api->set_current_limit == NULL) {
+		return -ENOSYS;
+	}
+
+	/* current limit may not be allowed, even if supported */
+	if ((min_ua < config->min_ua) || (max_ua > config->max_ua)) {
+		return -EINVAL;
+	}
+
+	return api->set_current_limit(dev, min_ua, max_ua);
+}
+
+int regulator_set_mode(const struct device *dev, regulator_mode_t mode)
+{
+	const struct regulator_common_config *config =
+		(struct regulator_common_config *)dev->config;
+	const struct regulator_driver_api *api =
+		(const struct regulator_driver_api *)dev->api;
+
+	if (api->set_mode == NULL) {
+		return -ENOSYS;
+	}
+
+	/* no mode restrictions */
+	if (config->allowed_modes_cnt == 0U) {
+		return api->set_mode(dev, mode);
+	}
+
+	/* check if mode is allowed, apply if it is */
+	for (uint8_t i = 0U; i < config->allowed_modes_cnt; i++) {
+		if (mode == config->allowed_modes[i]) {
+			return api->set_mode(dev, mode);
+		}
+	}
+
+	return -ENOTSUP;
 }
