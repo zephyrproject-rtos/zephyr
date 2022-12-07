2a02f4f9a918b6ab2a3e5c819c3dfb76825f0c21
Robert Lubos <robert.lubos@nordicsemi.no>
2022-12-07 13:35:00 +0100

    samples: net: big_http_download: Update URL for TLS download
    The old link has expired, the file is no longer avaiable to download.
    
    Update the download link, which now leads to a file in Ubuntu
    repositories for the recent LTS release, hopefully making the link valid
    for longer period.
    
    As the server ceritficate is signed by a different root CA, update the
    root CA as well.
    
    Finally, cleanup how the URL is handled in the sample - for TLS variant
    it'll always be provided with a Kconfig, hence no need to repeat it in
    the sample.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- samples/net/sockets/big_http_download/src/big_http_download.c
+++ samples/net/sockets/big_http_download/src/big_http_download.c
@@ -48,11 +48,7 @@ static char download_url[MAX_URL_LENGTH] =
 #if defined(CONFIG_SAMPLE_BIG_HTTP_DL_URL)
     CONFIG_SAMPLE_BIG_HTTP_DL_URL;
 #else
-#if !defined(CONFIG_NET_SOCKETS_SOCKOPT_TLS)
     "http://archive.ubuntu.com/ubuntu/dists/xenial/main/installer-amd64/current/images/hd-media/vmlinuz";
-#else
-    "https://www.7-zip.org/a/7z1805.exe";
-#endif /* !defined(CONFIG_NET_SOCKETS_SOCKOPT_TLS) */
 #endif /* defined(CONFIG_SAMPLE_BIG_HTTP_DL_URL) */
 /* Quick testing. */
 /*    "http://google.com/foo";*/
@@ -62,7 +58,8 @@ static uint8_t download_hash[32] =
 #if !defined(CONFIG_NET_SOCKETS_SOCKOPT_TLS)
 "\x33\x7c\x37\xd7\xec\x00\x34\x84\x14\x22\x4b\xaa\x6b\xdb\x2d\x43\xf2\xa3\x4e\xf5\x67\x6b\xaf\xcd\xca\xd9\x16\xf1\x48\xb5\xb3\x17";
 #else
-"\x64\x7a\x9a\x62\x11\x62\xcd\x7a\x50\x08\x93\x4a\x08\xe2\x3f\xf7\xc1\x13\x5d\x6f\x12\x61\x68\x9f\xd9\x54\xaa\x17\xd5\x0f\x97\x29";
+"\x3a\x07\x55\xdd\x1c\xfa\xb7\x1a\x24\xdd\x96\xdf\x34\x98\xc2\x9c"
+"\xd0\xac\xd1\x3b\x04\xf3\xd0\x8b\xf9\x33\xe8\x12\x86\xdb\x80\x2c";
 #endif /* !defined(CONFIG_NET_SOCKETS_SOCKOPT_TLS) */
 
 #define SSTRLEN(s) (sizeof(s) - 1)
