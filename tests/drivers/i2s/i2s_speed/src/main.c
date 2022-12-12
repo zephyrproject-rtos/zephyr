bfa6129302749f19fe5a97121ee7cffd10338766
Emilio Benavente <emilio.benavente@nxp.com>
2022-12-12 11:37:12 -0600

    tests: drivers: i2s: ZTest ordering issue fix
    The test was executing the expected functions
    out of order. I have created a config functions
    and explicitly declared them to be setup functions
    for this ZTest framework.
    
    Signed-off-by: Emilio Benavente <emilio.benavente@nxp.com>

--- tests/drivers/i2s/i2s_speed/src/main.c
+++ tests/drivers/i2s/i2s_speed/src/main.c
@@ -8,4 +8,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/ztest.h>
 
-ZTEST_SUITE(drivers_i2s_speed, NULL, NULL, NULL, NULL, NULL);
+extern void *test_i2s_speed_configure(void);
+extern void *test_i2s_speed_rxtx_configure(void);
+ZTEST_SUITE(drivers_i2s_speed, NULL, test_i2s_speed_configure, NULL, NULL, NULL);
+ZTEST_SUITE(drivers_i2s_speed_both_rxtx, NULL, test_i2s_speed_rxtx_configure, NULL, NULL, NULL);
