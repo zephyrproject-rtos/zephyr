bfcef7da8c851a7362c776010637086eefd507bc
Flavio Ceolin <flavio.ceolin@intel.com>
2022-12-13 21:49:52 +0000

    intel_adsp: ace20_lnl: Add L3 region definition
    Add L3_MEM* definitions to adsp_memory.h
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- soc/xtensa/intel_adsp/ace/include/intel_ace20_lnl/adsp_memory.h
+++ soc/xtensa/intel_adsp/ace/include/intel_ace20_lnl/adsp_memory.h
@@ -27,6 +27,12 @@
 #define RAM_BASE (L2_SRAM_BASE + CONFIG_HP_SRAM_RESERVE + VECTOR_TBL_SIZE)
 #define RAM_SIZE (L2_SRAM_SIZE - CONFIG_HP_SRAM_RESERVE - VECTOR_TBL_SIZE)
 
+/* L3 region (IMR), located in host memory */
+
+#define L3_MEM_BASE_ADDR (DT_REG_ADDR(DT_NODELABEL(imr1)))
+#define L3_MEM_SIZE (DT_REG_SIZE(DT_NODELABEL(imr1)))
+#define L3_MEM_PAGE_SIZE (DT_PROP(DT_NODELABEL(imr1), block_size))
+
 /* The rimage tool produces two blob addresses we need to find: one is
  * our bootloader code block which starts at its entry point, the
  * other is the "manifest" containing the HP-SRAM data to unpack,
