8977c004fbe2b9c854a226f42b61a428ebbc772d
Andries Kruithof <andries.kruithof@nordicsemi.no>
2022-12-16 09:48:26 +0100

    Bluetooth: controller: Kconfig for event length update
    Since the event length update is not necessarily an improvement
    in all situations a Kconfig option is added so that it can
    be disabled for the users that do not need it.
    
    Signed-off-by: Andries Kruithof <andries.kruithof@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_llcp_phy.c
+++ subsys/bluetooth/controller/ll_sw/ull_llcp_phy.c
@@ -303,7 +303,9 @@ static uint8_t pu_update_eff_times(struct ll_conn *conn, struct proc_ctx *ctx)
 	    (lll->dle.eff.max_rx_time > max_rx_time)) {
 		lll->dle.eff.max_tx_time = eff_tx_time;
 		lll->dle.eff.max_rx_time = eff_rx_time;
+#if defined(CONFIG_BT_CTLR_SLOT_RESERVATION_UPDATE)
 		lll->evt_len_upd = 1U;
+#endif /* CONFIG_BT_CTLR_SLOT_RESERVATION_UPDATE */
 		return 1U;
 	}
 
