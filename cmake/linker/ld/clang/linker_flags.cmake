7af5fc6f9b560707f75c19722d8ec38e7a68f72e
Nikolay Agishev <agishev@synopsys.com>
2022-12-22 15:46:04 +0400

    toolchain: Move extra warning options to toolchain abstraction
    Move extra warning option from generic twister script into
    compiler-dependent config files.
    ARCMWDT compiler doesn't support extra warning options ex.
    "-Wl,--fatal-warnings". To avoid build fails flag
    "disable_warnings_as_errors" should be passed to twister.
    This allows all warning messages and make atomatic test useles.
    
    Signed-off-by: Nikolay Agishev <agishev@synopsys.com>

--- cmake/linker/ld/clang/linker_flags.cmake
+++ cmake/linker/ld/clang/linker_flags.cmake
@@ -2,3 +2,6 @@
 if (NOT CONFIG_COVERAGE_GCOV)
   set_property(TARGET linker PROPERTY coverage --coverage)
 endif()
+
+# Extra warnings options for twister run
+set_property(TARGET linker PROPERTY ld_extra_warning_options -Wl,--fatal-warnings)
