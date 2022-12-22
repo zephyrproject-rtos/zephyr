616f7be261b6af5c4333a98b1879e7e0833bc390
Francois Ramu <francois.ramu@st.com>
2022-12-22 15:13:47 +0100

    drivers: flash: stm32 page layout size differs in stm32u5 or stm32l5
    Adjust the size of the stm32_flash_layout[] table depending on the
    bank configuration of the stm32u5 or stm32l5 devices.
    That will avoid div by zero error in flash_get_page_info()
    if the layout_size is not correct.
    Assign the *layout_size only once with the correct value : 3 or 1.
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- drivers/flash/flash_stm32l5x.c
+++ drivers/flash/flash_stm32l5x.c
@@ -360,10 +360,9 @@ void flash_stm32_page_layout(const struct device *dev,
 	static struct flash_pages_layout stm32_flash_layout[3];
 
 	*layout = stm32_flash_layout;
-	*layout_size = ARRAY_SIZE(stm32_flash_layout);
 
 	if (stm32_flash_layout[0].pages_count != 0) {
-		/* Short circuit calculation logic if already performed */
+		/* Short circuit calculation logic if already performed (size is known) */
 		return;
 	}
 
@@ -386,6 +385,8 @@ void flash_stm32_page_layout(const struct device *dev,
 		/* Bank2 */
 		stm32_flash_layout[2].pages_count = PAGES_PER_BANK;
 		stm32_flash_layout[2].pages_size = FLASH_PAGE_SIZE;
+
+		*layout_size = ARRAY_SIZE(stm32_flash_layout);
 	} else {
 		/*
 		 * For stm32l562xx & stm32l552xx with 512 flash or stm32u58x
@@ -404,6 +405,11 @@ void flash_stm32_page_layout(const struct device *dev,
 			stm32_flash_layout[0].pages_size = FLASH_PAGE_SIZE_128_BITS;
 #endif /* CONFIG_SOC_SERIES_STM32L5X */
 		}
-	}
 
+		/*
+		 * In this case the stm32_flash_layout table has one single element
+		 * when read by the flash_get_page_info()
+		 */
+		*layout_size = 1;
+	}
 }
