6cdef0894fe7aced7f58b1f34fa1ca2b691f56cb
Aaron Massey <aaronmassey@google.com>
2022-12-29 15:05:40 -0700

    drivers: fuel_gauge: kernel drivers use syscalls
    The fuel gauge API doesn't make use of syscalls with device drivers, which
    is not congruent with Zephyr's security model.
    
    Correct the fuel gauge API to only allow access to fuel gauge device
    drivers functionality via syscalls to keep with Zephyr's security model.
    
    Signed-off-by: Aaron Massey <aaronmassey@google.com>

--- include/zephyr/drivers/fuel_gauge.h
+++ include/zephyr/drivers/fuel_gauge.h
@@ -18,6 +18,7 @@
 extern "C" {
 #endif /* __cplusplus */
 
+#include <errno.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -162,6 +163,31 @@ struct fuel_gauge_set_property {
 };
 
 /**
+ * @typedef fuel_gauge_get_property_t
+ * @brief Callback API for getting a fuel_gauge property.
+ *
+ * See fuel_gauge_get_property() for argument description
+ */
+typedef int (*fuel_gauge_get_property_t)(const struct device *dev,
+					 struct fuel_gauge_get_property *props, size_t props_len);
+
+/**
+ * @typedef fuel_gauge_set_property_t
+ * @brief Callback API for setting a fuel_gauge property.
+ *
+ * See fuel_gauge_set_property() for argument description
+ */
+typedef int (*fuel_gauge_set_property_t)(const struct device *dev,
+					 struct fuel_gauge_set_property *props, size_t props_len);
+
+/* Caching is entirely on the onus of the client */
+
+__subsystem struct fuel_gauge_driver_api {
+	fuel_gauge_get_property_t get_property;
+	fuel_gauge_set_property_t set_property;
+};
+
+/**
  * @brief Fetch a battery fuel-gauge property
  *
  * @param dev Pointer to the battery fuel-gauge device
@@ -173,8 +199,21 @@ struct fuel_gauge_set_property {
  * @return return=0 if successful, return < 0 if getting all properties failed, return > 0 if some
  * properties failed where return=number of failing properties.
  */
-typedef int (*fuel_gauge_get_property_t)(const struct device *dev,
-					 struct fuel_gauge_get_property *props, size_t props_len);
+__syscall int fuel_gauge_get_prop(const struct device *dev, struct fuel_gauge_get_property *props,
+				  size_t props_len);
+
+static inline int z_impl_fuel_gauge_get_prop(const struct device *dev,
+					     struct fuel_gauge_get_property *props,
+					     size_t props_len)
+{
+	const struct fuel_gauge_driver_api *api = (const struct fuel_gauge_driver_api *)dev->api;
+
+	if (api->get_property == NULL) {
+		return -ENOSYS;
+	}
+
+	return api->get_property(dev, props, props_len);
+}
 
 /**
  * @brief Set a battery fuel-gauge property
@@ -188,15 +227,21 @@ typedef int (*fuel_gauge_get_property_t)(const struct device *dev,
  * @return return=0 if successful, return < 0 if setting all properties failed, return > 0 if some
  * properties failed where return=number of failing properties.
  */
-typedef int (*fuel_gauge_set_property_t)(const struct device *dev,
-					 struct fuel_gauge_set_property *props, size_t props_len);
+__syscall int fuel_gauge_set_prop(const struct device *dev, struct fuel_gauge_set_property *props,
+				  size_t props_len);
 
-/* Caching is entirely on the onus of the client */
+static inline int z_impl_fuel_gauge_set_prop(const struct device *dev,
+					     struct fuel_gauge_set_property *props,
+					     size_t props_len)
+{
+	const struct fuel_gauge_driver_api *api = (const struct fuel_gauge_driver_api *)dev->api;
 
-__subsystem struct fuel_gauge_driver_api {
-	fuel_gauge_get_property_t get_property;
-	fuel_gauge_set_property_t set_property;
-};
+	if (api->set_property == NULL) {
+		return -ENOSYS;
+	}
+
+	return api->set_property(dev, props, props_len);
+}
 
 /**
  * @}
@@ -205,5 +250,7 @@ __subsystem struct fuel_gauge_driver_api {
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
+
+#include <syscalls/fuel_gauge.h>
 
 #endif /* ZEPHYR_INCLUDE_DRIVERS_BATTERY_H_ */
