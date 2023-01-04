zephyrproject-rtos/zephyr

commit 74239d5b93ff700ddadf16a2dbe8f4f530c756b6

usb: dfu: Support DFU with WinUSB on Windows
DFU on Windows with WinUSB driver failed with "Lost device after RESET?"
error because WinUSB does not support host initiated resets. USB Device
Firmware Upgrade Specification Revision 1.1 attribute bitWillDetach can
be used to overcome WinUSB reset limitation. When bitWillDetach is set,
it is the device responsibility to detach and reattach itself to the bus
after receiving DFU_DETACH request.

Add and enable by default USB_DFU_WILL_DETACH configuration option,
because it is the only way to support WinUSB driver. WinUSB driver is
preferable because it can be automatically installed on Windows 8 and
later if USB device implements WCID descriptors.

Fixes: #49821

Signed-off-by: Tomasz Moń <tomasz.mon@nordicsemi.no>

subsys/usb/device/class/dfu/usb_dfu.c
@@ -79,9 +79,16 @@ LOG_MODULE_REGISTER(usb_dfu);
 #define DFU_DESC_ATTRIBUTES_CAN_UPLOAD 0
 #endif
 
+#if IS_ENABLED(CONFIG_USB_DFU_WILL_DETACH)
+#define DFU_DESC_ATTRIBUTES_WILL_DETACH DFU_ATTR_WILL_DETACH
+#else
+#define DFU_DESC_ATTRIBUTES_WILL_DETACH 0
+#endif
+
 #define DFU_DESC_ATTRIBUTES		(DFU_ATTR_CAN_DNLOAD | \
 					 DFU_DESC_ATTRIBUTES_CAN_UPLOAD |\
-					 DFU_DESC_ATTRIBUTES_MANIF_TOL)
+					 DFU_DESC_ATTRIBUTES_MANIF_TOL |\
+					 DFU_DESC_ATTRIBUTES_WILL_DETACH)
 
 static struct k_poll_event dfu_event;
 static struct k_poll_signal dfu_signal;
@@ -400,10 +407,31 @@ static void dfu_flash_write(uint8_t *data, size_t len)
 	LOG_DBG("bytes written 0x%x", flash_img_bytes_written(&dfu_data.ctx));
 }
 
+static void dfu_enter_idle(void)
+{
+	dfu_data.state = dfuIDLE;
+
+	/* Set the DFU mode descriptors to be used after reset */
+	dfu_config.usb_device_description = (uint8_t *) &dfu_mode_desc;
+	if (usb_set_config(dfu_config.usb_device_description)) {
+		LOG_ERR("usb_set_config failed during DFU idle entry");
+	}
+}
+
 static void dfu_timer_expired(struct k_timer *timer)
 {
 	if (dfu_data.state == appDETACH) {
-		dfu_data.state = appIDLE;
+		if (IS_ENABLED(CONFIG_USB_DFU_WILL_DETACH)) {
+			if (usb_dc_detach()) {
+				LOG_ERR("usb_dc_detach failed");
+			}
+			dfu_enter_idle();
+			if (usb_dc_attach()) {
+				LOG_ERR("usb_dc_attach failed");
+			}
+		} else {
+			dfu_data.state = appIDLE;
+		}
 	}
 }
 
@@ -655,8 +683,17 @@ static int dfu_class_handle_to_device(struct usb_setup_packet *setup,
 
 		/* Move to appDETACH state */
 		dfu_data.state = appDETACH;
-		/* Begin detach timeout timer */
-		timeout = MIN(setup->wValue, CONFIG_USB_DFU_DETACH_TIMEOUT);
+		if (IS_ENABLED(CONFIG_USB_DFU_WILL_DETACH)) {
+			/* Note: Detach should happen once the status stage
+			 * finishes but the USB device stack does not expose
+			 * such callback. Wait fixed time (ignore wValue) to
+			 * let device finish control transfer status stage.
+			 */
+			timeout = CONFIG_USB_DFU_DETACH_TIMEOUT;
+		} else {
+			/* Begin detach timeout timer */
+			timeout = MIN(setup->wValue, CONFIG_USB_DFU_DETACH_TIMEOUT);
+		}
 		k_timer_start(&dfu_timer, K_MSEC(timeout), K_FOREVER);
 		break;
 	default:
@@ -709,19 +746,11 @@ static void dfu_status_cb(struct usb_cfg_data *cfg,
 		break;
 	case USB_DC_RESET:
 		LOG_DBG("USB device reset detected, state %d", dfu_data.state);
-		/* Stop the appDETACH timeout timer */
-		k_timer_stop(&dfu_timer);
-		if (dfu_data.state == appDETACH) {
-			dfu_data.state = dfuIDLE;
-
-			/* Set the DFU mode descriptors to be used after
-			 * reset
-			 */
-			dfu_config.usb_device_description =
-				(uint8_t *) &dfu_mode_desc;
-			if (usb_set_config(dfu_config.usb_device_description)) {
-				LOG_ERR("usb_set_config failed during USB "
-					"device reset");
+		if (!IS_ENABLED(CONFIG_USB_DFU_WILL_DETACH)) {
+			/* Stop the appDETACH timeout timer */
+			k_timer_stop(&dfu_timer);
+			if (dfu_data.state == appDETACH) {
+				dfu_enter_idle();
 			}
 		}
 		break;

subsys/usb/device/class/dfu/Kconfig
@@ -18,8 +18,18 @@ config USB_DEVICE_DFU_PID
 	help
 	  USB device product ID in DFU mode. MUST be configured by vendor.
 
+config USB_DFU_WILL_DETACH
+	bool "Generate detach-attach sequence on DFU detach"
+	default y
+	help
+	  Enabling this option makes the device responsible for detaching
+	  itself from the bus after the DFU_DETACH request. Select this
+	  for compatibility with host drivers that cannot issue USB reset.
+	  DFU fails on Windows with WinUSB driver if this is not enabled.
+
 config USB_DFU_DETACH_TIMEOUT
 	int
+	default 100 if USB_DFU_WILL_DETACH
 	default 1000
 
 config USB_DFU_DEFAULT_POLLTIMEOUT

commit 33ffca2239ecbbde2f0aa213ac819a0b2201b61a

usb: dfu: Schedule timer handler from USB workqueue
Use k_work_delayable instead of k_timer in order to execute timeout in
USB workqueue context instead of ISR context. This fixes Will-Detach on
targets where usb_dc_detach() uses functions not allowed in ISR context,
e.g. nrfx usb_dc_detach() acquires mutex.

Fixes: 74239d5b93ff ("usb: dfu: Support DFU with WinUSB on Windows")

Signed-off-by: Tomasz Moń <tomasz.mon@nordicsemi.no>

subsys/usb/device/class/dfu/usb_dfu.c
@@ -91,7 +91,7 @@ LOG_MODULE_REGISTER(usb_dfu, CONFIG_USB_DEVICE_LOG_LEVEL);
 
 static struct k_poll_event dfu_event;
 static struct k_poll_signal dfu_signal;
-static struct k_timer dfu_timer;
+static struct k_work_delayable dfu_timer_work;
 
 static struct k_work dfu_work;
 
@@ -417,8 +417,10 @@ static void dfu_enter_idle(void)
 	}
 }
 
-static void dfu_timer_expired(struct k_timer *timer)
+static void dfu_timer_work_handler(struct k_work *item)
 {
+	ARG_UNUSED(item);
+
 	if (dfu_data.state == appDETACH) {
 		if (IS_ENABLED(CONFIG_USB_DFU_WILL_DETACH)) {
 			if (usb_dc_detach()) {
@@ -693,7 +695,7 @@ static int dfu_class_handle_to_device(struct usb_setup_packet *setup,
 			/* Begin detach timeout timer */
 			timeout = MIN(setup->wValue, CONFIG_USB_DFU_DETACH_TIMEOUT);
 		}
-		k_timer_start(&dfu_timer, K_MSEC(timeout), K_FOREVER);
+		k_work_reschedule_for_queue(&USB_WORK_Q, &dfu_timer_work, K_MSEC(timeout));
 		break;
 	default:
 		LOG_DBG("Unsupported bmRequestType 0x%02x bRequest 0x%02x",
@@ -747,7 +749,7 @@ static void dfu_status_cb(struct usb_cfg_data *cfg,
 		LOG_DBG("USB device reset detected, state %d", dfu_data.state);
 		if (!IS_ENABLED(CONFIG_USB_DFU_WILL_DETACH)) {
 			/* Stop the appDETACH timeout timer */
-			k_timer_stop(&dfu_timer);
+			k_work_cancel_delayable(&dfu_timer_work);
 			if (dfu_data.state == appDETACH) {
 				dfu_enter_idle();
 			}
@@ -904,7 +906,7 @@ static int usb_dfu_init(const struct device *dev)
 
 	k_work_init(&dfu_work, dfu_work_handler);
 	k_poll_signal_init(&dfu_signal);
-	k_timer_init(&dfu_timer, dfu_timer_expired, NULL);
+	k_work_init_delayable(&dfu_timer_work, dfu_timer_work_handler);
 
 #ifdef CONFIG_USB_DFU_REBOOT
 	k_work_init_delayable(&reboot_work, reboot_work_handler);