ab37d338b32c7103adf22137a4cef1c406172303
Erwan Gouriou <erwan.gouriou@linaro.org>
2023-01-05 10:25:15 +0100

    boards: stm32: fix --reset argument for stm32cubeprogrammer runner
    stm32cubeprogrammer runner takes a --reset-mode argument to specify the
    type of reset used in the flashing process.
    Though, argparse default configuration in python allows shortened command
    arguments and it happened that --reset was used on most boards instead
    of --reset-mode.
    This argparse configuration is being changed in order to prevent shortened
    command  args (see #53495). As a consequence all board configs using
    --reset should be updated to use the full length version.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- boards/arm/lora_e5_dev_board/board.cmake
+++ boards/arm/lora_e5_dev_board/board.cmake
@@ -4,7 +4,7 @@ board_runner_args(pyocd "--target=stm32wle5jcix")
 board_runner_args(pyocd "--flash-opt=-O reset_type=hw")
 board_runner_args(pyocd "--flash-opt=-O connect_mode=under-reset")
 board_runner_args(jlink "--device=STM32WLE5JC" "--speed=4000" "--reset-after-load")
-board_runner_args(stm32cubeprogrammer "--port=swd" "--reset=hw")
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(blackmagicprobe "--gdb-serial=/dev/ttyBmpGdb" "--connect-srst")
 
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
--- boards/arm/stm32f3_disco/board.cmake
+++ boards/arm/stm32f3_disco/board.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--device=STM32F303VC" "--speed=4000")
-board_runner_args(stm32cubeprogrammer "--port=swd" "--reset=hw")
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
