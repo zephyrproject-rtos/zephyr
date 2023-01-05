69cc659c927e9041240be9d953f09b284238b76c
Erwan Gouriou <erwan.gouriou@linaro.org>
2023-01-05 10:25:15 +0100

    boards: stm32: fix --reset argument for stm32cubeprogrammer runner
    stm32cubeprogrammer runner takes a --reset-mode argument to specify the
    type of reset used in the flashing process.
    Though, argparse default configuration in python allows shortened command
    arguments and it happened that --reset was used on most boards instead
    of --reset-mode.
    This argparse configuration is being changed in order to prevent shortened
    command  args (see #53495). As a consequence all board configs using
    --reset should be updated to use the full length version.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>

--- boards/arm/nucleo_h753zi/board.cmake
+++ boards/arm/nucleo_h753zi/board.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--device=STM32H753ZI" "--speed=4000")
-board_runner_args(stm32cubeprogrammer "--port=swd" "--reset=hw")
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
