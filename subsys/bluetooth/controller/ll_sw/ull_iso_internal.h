ff04132c597c8227186bba40b64b11bc74c2eeca
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2023-01-11 15:02:41 +0530

    Bluetooth: Controller: Define a internal BT_CTLR_ISO_TX_BUFFERS
    Define a internal BT_CTLR_ISO_TX_BUFFERS to allocate ISO Tx
    buffers used for Broadcast and Connected ISO.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_iso_internal.h
+++ subsys/bluetooth/controller/ll_sw/ull_iso_internal.h
@@ -4,6 +4,23 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#if defined(CONFIG_BT_CTLR_ADV_ISO) || defined(CONFIG_BT_CTLR_CONN_ISO)
+/* Calculate ISO PDU buffers required considering SDU fragmentation */
+/* FIXME: Calculation considering both Connected and Broadcast ISO PDU
+ *        fragmentation.
+ */
+#if defined(CONFIG_BT_CTLR_CONN_ISO)
+#define BT_CTLR_ISO_TX_BUFFERS (((CONFIG_BT_CTLR_CONN_ISO_SDU_LEN_MAX + \
+				  CONFIG_BT_CTLR_CONN_ISO_PDU_LEN_MAX - 1U) / \
+				 CONFIG_BT_CTLR_CONN_ISO_PDU_LEN_MAX) * \
+				CONFIG_BT_CTLR_ISO_TX_BUFFERS)
+#else /* !CONFIG_BT_CTLR_CONN_ISO */
+#define BT_CTLR_ISO_TX_BUFFERS CONFIG_BT_CTLR_ISO_TX_BUFFERS
+#endif /* !CONFIG_BT_CTLR_CONN_ISO */
+#else /* !CONFIG_BT_CTLR_ADV_ISO && !CONFIG_BT_CTLR_CONN_ISO */
+#define BT_CTLR_ISO_TX_BUFFERS 0
+#endif /* !CONFIG_BT_CTLR_ADV_ISO && !CONFIG_BT_CTLR_CONN_ISO */
+
 int ull_iso_init(void);
 int ull_iso_reset(void);
 struct ll_iso_datapath *ull_iso_datapath_alloc(void);
