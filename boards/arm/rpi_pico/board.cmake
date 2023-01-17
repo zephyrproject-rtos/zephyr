a48894b063ae654431168b22f74939f89ce474de
Fabio Baltieri <fabio.baltieri@gmail.com>
2023-01-17 22:55:03 +0000

    boards: drop explicit --gdb-serial
    Now that the well known udev BMP port is used automatically, drop it
    from the board files.
    
    Signed-off-by: Fabio Baltieri <fabio.baltieri@gmail.com>

--- boards/arm/rpi_pico/board.cmake
+++ boards/arm/rpi_pico/board.cmake
@@ -27,8 +27,6 @@ board_runner_args(openocd --cmd-pre-init "set_adapter_speed_if_not_set 2000")
 
 board_runner_args(jlink "--device=RP2040_M0_0")
 
-board_runner_args(blackmagicprobe "--gdb-serial=/dev/ttyBmpGdb")
-
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
