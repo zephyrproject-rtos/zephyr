cd59e74412e740ab003d11534d3ba552c9c3b486
Jeppe Odgaard <jeppe.odgaard@prevas.dk>
2023-01-17 10:37:54 +0100

    drivers: memc: add update clock function
    Add a function to update the flexspi bus clock. This is
    needed when write operations are done to the hyperflash.
    
    Signed-off-by: Jeppe Odgaard <jeppe.odgaard@prevas.dk>

--- drivers/memc/memc_mcux_flexspi.h
+++ drivers/memc/memc_mcux_flexspi.h
@@ -8,12 +8,23 @@
 #include <sys/types.h>
 #include <fsl_flexspi.h>
 
+enum memc_flexspi_clock_t {
+	/* Flexspi clock 332M, DDR mode, internal clock 166M. */
+	MEMC_FLEXSPI_CLOCK_166M,
+	/* Flexspi clock 83M, DDR mode, internal clock 42M. */
+	MEMC_FLEXSPI_CLOCK_42M,
+};
+
 void memc_flexspi_wait_bus_idle(const struct device *dev);
 
 bool memc_flexspi_is_running_xip(const struct device *dev);
 
 int memc_flexspi_update_lut(const struct device *dev, uint32_t index,
 		const uint32_t *cmd, uint32_t count);
+
+int memc_flexspi_update_clock(const struct device *dev,
+		flexspi_device_config_t *device_config,
+		flexspi_port_t port, enum memc_flexspi_clock_t clock);
 
 int memc_flexspi_set_device_config(const struct device *dev,
 		const flexspi_device_config_t *device_config,
