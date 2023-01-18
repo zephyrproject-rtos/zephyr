153596f82cb8b0f705a5878870c6e2691ce192c3
Daniel DeGrasse <daniel.degrasse@nxp.com>
2023-01-18 10:40:15 -0600

    drivers: display: update MCUX ELCDIF driver to use new lcdif binding
    Update MCUX ELCDIF driver to use new LCDIF bindings. This
    update also adds support for configuring the root clock of
    the ELCDIF module based on the pixel-clock property to the
    RT11xx SOC clock init, as this SOC series has this IP block
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- drivers/mipi_dsi/dsi_mcux.c
+++ drivers/mipi_dsi/dsi_mcux.c
@@ -81,7 +81,6 @@ static int dsi_mcux_attach(const struct device *dev,
 
 	mipi_dsi_dphy_bit_clk_hz = DSI_InitDphy((MIPI_DSI_Type *)&config->base,
 						&dphy_config, mipi_dsi_dphy_ref_clk_hz);
-
 	/* Init DPI interface. */
 	DSI_SetDpiConfig((MIPI_DSI_Type *)&config->base, &config->dpi_config, mdev->data_lanes,
 					mipi_dsi_dpi_clk_hz, mipi_dsi_dphy_bit_clk_hz);
@@ -187,13 +186,22 @@ static int display_mcux_mipi_dsi_init(const struct device *dev)
 			.bllpMode = DT_INST_ENUM_IDX(id, dpi_bllp_mode),			\
 			.pixelPayloadSize = DT_INST_PROP_BY_PHANDLE(id, nxp_lcdif, width),	\
 			.panelHeight = DT_INST_PROP_BY_PHANDLE(id, nxp_lcdif, height),		\
-			.polarityFlags = DT_INST_PROP_BY_PHANDLE_IDX(				\
-					id, nxp_lcdif, id, polarity) >> 2,			\
-			.hfp = DT_INST_PROP_BY_PHANDLE(id, nxp_lcdif, hfp),			\
-			.hbp = DT_INST_PROP_BY_PHANDLE(id, nxp_lcdif, hbp),			\
-			.hsw = DT_INST_PROP_BY_PHANDLE(id, nxp_lcdif, hsync),			\
-			.vfp = DT_INST_PROP_BY_PHANDLE(id, nxp_lcdif, vfp),			\
-			.vbp = DT_INST_PROP_BY_PHANDLE(id, nxp_lcdif, vbp),			\
+			.polarityFlags = (DT_PROP(DT_CHILD(DT_INST_PHANDLE(id, nxp_lcdif),	\
+					display_timings), hsync_active) ?			\
+					kDSI_DpiHsyncActiveHigh : kDSI_DpiHsyncActiveLow) |	\
+					(DT_PROP(DT_CHILD(DT_INST_PHANDLE(id, nxp_lcdif),	\
+					display_timings), vsync_active) ?			\
+					kDSI_DpiVsyncActiveHigh : kDSI_DpiVsyncActiveLow),	\
+			.hfp = DT_PROP(DT_CHILD(DT_INST_PHANDLE(id, nxp_lcdif),			\
+						display_timings), hfront_porch),		\
+			.hbp = DT_PROP(DT_CHILD(DT_INST_PHANDLE(id, nxp_lcdif),			\
+							display_timings), hback_porch),		\
+			.hsw = DT_PROP(DT_CHILD(DT_INST_PHANDLE(id, nxp_lcdif),			\
+							display_timings), hsync_len),		\
+			.vfp = DT_PROP(DT_CHILD(DT_INST_PHANDLE(id, nxp_lcdif),			\
+						display_timings), vfront_porch),		\
+			.vbp = DT_PROP(DT_CHILD(DT_INST_PHANDLE(id, nxp_lcdif),			\
+							display_timings), vback_porch),		\
 		},										\
 		.auto_insert_eotp = DT_INST_PROP(id, autoinsert_eotp),				\
 		.phy_clock = DT_INST_PROP(id, phy_clock),					\
