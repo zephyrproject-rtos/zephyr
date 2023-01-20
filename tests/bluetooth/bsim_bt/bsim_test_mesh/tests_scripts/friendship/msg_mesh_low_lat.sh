9e20933caeb293e910a27fcd250fb481a4c697fd
Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>
2023-01-20 10:53:43 +0100

    tests: Bluetooth: Mesh: use overlays for mesh bsim
    Add the overlays usage for bsim mesh configurations instead of
    multiple configurations that duplicate each other a lot.
    
    Signed-off-by: Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>

--- tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/friendship/msg_mesh_low_lat.sh
+++ tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/friendship/msg_mesh_low_lat.sh
@@ -5,7 +5,7 @@
 source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
 
 # Test communication between the LPN and a third mesh device
-conf=prj_low_lat_conf
+overlay=overlay_low_lat_conf
 RunTest mesh_friendship_msg_mesh_low_lat \
 	friendship_lpn_msg_mesh \
 	friendship_other_msg \
--- tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/transport/unicast_low_lat.sh
+++ tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/transport/unicast_low_lat.sh
@@ -4,5 +4,5 @@
 
 source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
 
-conf=prj_low_lat_conf
+overlay=overlay_low_lat_conf
 RunTest mesh_transport_unicast_low_lat transport_tx_unicast transport_rx_unicast
