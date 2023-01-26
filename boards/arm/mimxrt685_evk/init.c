f839c47a3a391ead3274dfa1d9d3d2301d546756
Daniel DeGrasse <daniel.degrasse@nxp.com>
2023-01-26 15:59:54 -0600

    boards: arm: mimimxrt685_evk_cm33: workaround to support sys_reboot
    On the RT685, the sys_reboot API does not function correctly. This is
    due to the fact that the external flash must be reset at power on. In
    order to configure the ROM to reset the flash, a OTP shadow register
    can be programmed with the GPIO port and pin of the flash reset pin.
    
    Program these bits at boot when the CONFIG_REBOOT symbol is set,
    so that sys_reboot works as expected.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- boards/arm/mimxrt685_evk/init.c
+++ boards/arm/mimxrt685_evk/init.c
@@ -43,6 +43,19 @@ static int mimxrt685_evk_init(const struct device *dev)
 #endif
 
 #endif
+
+#ifdef CONFIG_REBOOT
+	/*
+	 * The sys_reboot API calls NVIC_SystemReset. On the RT685, the warm
+	 * reset will not complete correctly unless the ROM toggles the
+	 * flash reset pin. We can control this behavior using the OTP shadow
+	 * register for OPT word BOOT_CFG1
+	 *
+	 * Set FLEXSPI_RESET_PIN_ENABLE=1, FLEXSPI_RESET_PIN= PIO2_12
+	 */
+	 OCOTP->OTP_SHADOW[97] = 0x314000;
+#endif /* CONFIG_REBOOT */
+
 	return 0;
 }
 
