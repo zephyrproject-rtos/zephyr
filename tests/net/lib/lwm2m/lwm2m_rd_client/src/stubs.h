f5555ac8164d085e79bf37ec1e537e47f7c7ea17
Andreas Chmielewski <andreas.chmielewski@grandcentrix.net>
2023-01-26 14:25:46 +0100

    [nrf fromtree] tests: lwm2m_rd_client: Added more fff tests
    If the rdclient is suspended or deregistered any network errors caused
    by lwm2m_engine and forwarded by socket_fault handler should not change
    the rd client state.
    
    Signed-off-by: Andreas Chmielewski <andreas.chmielewski@grandcentrix.net>
    Signed-off-by: Seppo Takalo <seppo.takalo@nordicsemi.no>
    (cherry picked from commit 72f7a05a8b51524960a3ea5b7114249b10e715ba)

--- tests/net/lib/lwm2m/lwm2m_rd_client/src/stubs.h
+++ tests/net/lib/lwm2m/lwm2m_rd_client/src/stubs.h
@@ -54,6 +54,7 @@ int lwm2m_get_u32_val(const struct lwm2m_obj_path *path, uint32_t *val);
 
 /* subsys/net/lib/lwm2m/lwm2m_engine.h */
 DECLARE_FAKE_VALUE_FUNC(int, lwm2m_socket_start, struct lwm2m_ctx *);
+int lwm2m_socket_start_fake_fail(struct lwm2m_ctx *client_ctx);
 DECLARE_FAKE_VALUE_FUNC(int, lwm2m_socket_close, struct lwm2m_ctx *);
 DECLARE_FAKE_VALUE_FUNC(int, lwm2m_close_socket, struct lwm2m_ctx *);
 DECLARE_FAKE_VALUE_FUNC(int, lwm2m_socket_suspend, struct lwm2m_ctx *);
@@ -61,6 +62,8 @@ DECLARE_FAKE_VALUE_FUNC(int, lwm2m_security_inst_id_to_index, uint16_t);
 DECLARE_FAKE_VALUE_FUNC(int, lwm2m_engine_connection_resume, struct lwm2m_ctx *);
 DECLARE_FAKE_VALUE_FUNC(int, lwm2m_push_queued_buffers, struct lwm2m_ctx *);
 DECLARE_FAKE_VOID_FUNC(lwm2m_engine_context_init, struct lwm2m_ctx *);
+void lwm2m_engine_context_init_fake1(struct lwm2m_ctx *client_ctx);
+void test_throw_network_error_from_engine(int err);
 DECLARE_FAKE_VOID_FUNC(lwm2m_engine_context_close, struct lwm2m_ctx *);
 DECLARE_FAKE_VALUE_FUNC(char *, lwm2m_sprint_ip_addr, const struct sockaddr *);
 char *lwm2m_sprint_ip_addr_fake_default(const struct sockaddr *addr);
