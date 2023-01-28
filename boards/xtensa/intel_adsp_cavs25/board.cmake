c94070b2a19c1ad595615699fcbee259b6313b7a
Marc Herbert <marc.herbert@intel.com>
2023-01-28 02:27:17 +0000

    west: sign: add new west config [rimage].extra-args and a default key
    Make RIMAGE_SIGN_KEY a CMake CACHE variable so `west sign` can find it
    and use it as a default value.
    
    Fixes https://github.com/thesofproject/sof/issues/6917
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- boards/xtensa/intel_adsp_cavs25/board.cmake
+++ boards/xtensa/intel_adsp_cavs25/board.cmake
@@ -7,7 +7,7 @@ endif()
 
 board_set_flasher_ifnset(intel_adsp)
 
-set(RIMAGE_SIGN_KEY otc_private_key_3k.pem)
+set(RIMAGE_SIGN_KEY "otc_private_key_3k.pem" CACHE STRING "default in cavs25/board.cmake")
 
 if(CONFIG_BOARD_INTEL_ADSP_CAVS25)
 board_set_rimage_target(tgl)
