189d8b8a5e8f6e7867da105a69036f475c7aa823
Chris Friedt <cfriedt@meta.com>
2023-02-04 14:52:58 -0500

    samples: net: socketpair: clean up sample app
    * use `read()` instead of `recv()`
    * use `write()` instead of `send()`
    * use `CONFIG_POSIX_API` and drop `<zephyr/posix/..>` prefix
    * use `perror()`
    * fix `Makefile.posix` to use `CC` instead of `CXX`
    * fix race condition which caused an unhandled EOF
      and infinite loop, by adding a readback / echo
    * Updated the docs to reflect the expected stdout
    * Improve doc clarity
    
    Signed-off-by: Chris Friedt <cfriedt@meta.com>

--- samples/net/sockets/socketpair/src/socketpair_example.c
+++ samples/net/sockets/socketpair/src/socketpair_example.c
@@ -3,37 +3,26 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#if defined(__ZEPHYR__) && !defined(CONFIG_ARCH_POSIX)
-
-#include <zephyr/net/socket.h>
-#include <zephyr/posix/pthread.h>
-#include <zephyr/sys/util.h>
-#include <zephyr/posix/unistd.h>
-
-#else
-
+#include <errno.h>
 #include <poll.h>
 #include <pthread.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-
-#define ARRAY_SIZE(x) (sizeof(x)/sizeof((x)[0]))
-
-#endif
-
-#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#ifdef __ZEPHYR__
+#include <zephyr/kernel.h>
+#endif
 
 #define NUM_SOCKETPAIRS 3
 #define NUM_REPITITIONS 3
 
-struct ctx {
+struct context {
 	int spair[2];
 	pthread_t thread;
-	char *name;
+	const char *name;
 };
 
 static const char *const names[] = {
@@ -42,173 +31,240 @@ static const char *const names[] = {
 	"Charlie",
 };
 
-#if defined(__ZEPHYR__) && !defined(CONFIG_ARCH_POSIX)
-
+#ifdef __ZEPHYR__
 #define STACK_SIZE (1024)
-static pthread_attr_t attr[NUM_SOCKETPAIRS];
 K_THREAD_STACK_ARRAY_DEFINE(stack, NUM_SOCKETPAIRS, STACK_SIZE);
-
 #endif
 
-static void hello(int fd, const char *name)
+static int hello(int fd, const char *name)
 {
-	/* write(2) should be used after #25443 */
-	int res = send(fd, name, strlen(name), 0);
+	int res;
+	char buf[32] = {0};
 
-	if (res != strlen(name)) {
-		printf("%s(): send: expected: %d actual: %d errno: %d\n",
-			__func__, (int)strlen(name), res, errno);
+	/* check for an echo of what is written */
+	res = write(fd, name, strlen(name));
+	if (res < 0) {
+		perror("write");
+	} else if (res != strlen(name)) {
+		printf("only wrote %d/%d bytes", res, (int)strlen(name));
+		return -EIO;
 	}
+
+	res = read(fd, buf, sizeof(buf) - 1);
+	if (res < 0) {
+		perror("read");
+	} else if (res != strlen(name)) {
+		printf("only read %d/%d bytes", res, (int)strlen(name));
+		return -EIO;
+	}
+
+	if (strncmp(buf, name, sizeof(buf)) != 0) {
+		printf("expected %s\n", name);
+		return -EINVAL;
+	}
+
+	return 0;
 }
 
 static void *fun(void *arg)
 {
-	struct ctx *const ctx = (struct ctx *)arg;
+	struct context *ctx = (struct context *)arg;
 	int fd = ctx->spair[1];
 	const char *name = ctx->name;
 
 	for (size_t i = 0; i < NUM_REPITITIONS; ++i) {
-		hello(fd, name);
-	}
-
-	close(fd);
-	printf("%s closed fd %d\n", name, fd);
-	ctx->spair[1] = -1;
-
-	return NULL;
-}
-
-static int fd_to_idx(int fd, struct ctx *ctx, size_t n)
-{
-	int r = -1;
-	size_t i;
-
-	for (i = 0; i < n; ++i) {
-		if (ctx[i].spair[0] == fd) {
-			r = i;
+		if (hello(fd, name) < 0) {
 			break;
 		}
 	}
 
-	return r;
+	return NULL;
 }
 
-#ifdef __ZEPHYR__
-void zephyr_app_main(void)
+static int fd_to_idx(int fd, const struct context *ctx, size_t n)
 {
-#else
-int main(int argc, char *argv[])
-{
-	(void) argc;
-	(void) argv;
-#endif
-
-	int r;
-	int fd;
-	int idx;
+	int res = -1;
 	size_t i;
-	size_t num_active;
-	char buf[32];
-	struct ctx ctx[NUM_SOCKETPAIRS] = {};
-	struct pollfd fds[NUM_SOCKETPAIRS] = {};
-	void *unused;
+
+	for (i = 0; i < n; ++i) {
+		if (ctx[i].spair[0] == fd) {
+			res = i;
+			break;
+		}
+	}
+
+	return res;
+}
+
+static int setup(struct context *ctx, size_t n)
+{
+	int res;
+#ifdef __ZEPHYR__
+	pthread_attr_t attr;
+	pthread_attr_t *attrp = &attr;
+#else
 	pthread_attr_t *attrp = NULL;
+#endif
 
-	for (i = 0; i < ARRAY_SIZE(ctx); ++i) {
+	for (size_t i = 0; i < n; ++i) {
 		ctx[i].name = (char *)names[i];
-		r = socketpair(AF_UNIX, SOCK_STREAM, 0, ctx[i].spair);
-		if (r != 0) {
-			printf("socketpair failed: %d\n", errno);
-			goto out;
+		res = socketpair(AF_UNIX, SOCK_STREAM, 0, ctx[i].spair);
+		if (res < 0) {
+			perror("socketpair");
+			return res;
 		}
 
-#if defined(__ZEPHYR__) && !defined(CONFIG_ARCH_POSIX)
+#ifdef __ZEPHYR__
 		/* Zephyr requires a non-NULL attribute for pthread_create */
-		attrp = &attr[i];
-		r = pthread_attr_init(attrp);
-		if (r != 0) {
-			printf("pthread_attr_init() failed: %d", r);
-			goto out;
+		res = pthread_attr_init(attrp);
+		if (res != 0) {
+			errno = res;
+			perror("pthread_attr_init");
+			return -res;
 		}
 
-		r = pthread_attr_setstack(attrp, &stack[i], STACK_SIZE);
-		if (r != 0) {
-			printf("pthread_attr_setstack() failed: %d", r);
-			goto out;
+		res = pthread_attr_setstack(&attr, &stack[i], STACK_SIZE);
+		if (res != 0) {
+			errno = res;
+			perror("pthread_attr_setstack");
+			return -res;
 		}
 #endif
 
-		r = pthread_create(&ctx[i].thread, attrp, fun, &ctx[i]);
-		if (r != 0) {
-			printf("pthread_create failed: %d\n", r);
-			goto out;
+		res = pthread_create(&ctx[i].thread, attrp, fun, &ctx[i]);
+		if (res != 0) {
+			errno = res;
+			perror("pthread_create");
+			return -res;
 		}
 
 		printf("%s: socketpair: %d <=> %d\n",
 			ctx[i].name, ctx[i].spair[0], ctx[i].spair[1]);
 	}
 
-	/* loop until all threads are done */
-	for (;;) {
+	return 0;
+}
 
-		/* count threads that are still running and fill in pollfds */
-		for (i = 0, num_active = 0; i < ARRAY_SIZE(ctx); ++i) {
-			if (ctx[i].spair[0] == -1) {
-				continue;
-			}
-			fds[num_active].fd = ctx[i].spair[0];
-			fds[num_active].events = POLLIN;
-			fds[num_active].revents = 0;
-			num_active++;
+static void teardown(struct context *ctx, size_t n)
+{
+	void *unused;
+
+	for (size_t i = 0; i < n; ++i) {
+		pthread_join(ctx[i].thread, &unused);
+
+		close(ctx[i].spair[0]);
+		ctx[i].spair[0] = -1;
+
+		close(ctx[i].spair[1]);
+		ctx[i].spair[1] = -1;
+	}
+}
+
+static void setup_poll(const struct context *ctx, struct pollfd *fds, size_t n)
+{
+	for (size_t i = 0; i < n; ++i) {
+		fds[i].fd = ctx[i].spair[0];
+		fds[i].events = POLLIN;
+		fds[i].revents = 0;
+	}
+}
+
+static int handle_poll_events(const struct context *ctx, struct pollfd *fds, size_t n,
+			      size_t n_events)
+{
+	int res;
+	int idx;
+	char buf[32];
+	size_t event = 0;
+
+	for (size_t i = 0; event < n_events && i < n; ++i) {
+		idx = fd_to_idx(fds[i].fd, ctx, n);
+		if (idx < 0) {
+			printf("failed to find fd %d in any active context\n", fds[i].fd);
+			continue;
 		}
 
-		if (num_active == 0) {
-			/* all threads are done */
-			break;
-		}
+		if ((fds[i].revents & POLLERR) != 0) {
+			printf("fd: %d: error\n", fds[i].fd);
+			return -EIO;
+		} else if ((fds[i].revents & POLLIN) != 0) {
+			memset(buf, '\0', sizeof(buf));
 
-		(void)poll(fds, num_active, -1);
-
-		for (size_t i = 0; i < num_active; ++i) {
-
-			fd = fds[i].fd;
-			idx = fd_to_idx(fd, ctx, ARRAY_SIZE(ctx));
-			if (idx < 0) {
-				printf("failed to map fd %d to index\n", fd);
-				continue;
+			/* echo back the same thing that was read */
+			res = read(fds[i].fd, buf, sizeof(buf));
+			if (res < 0) {
+				perror("read");
+				return -errno;
 			}
 
-			if ((fds[i].revents & POLLIN) != 0) {
-
-				memset(buf, '\0', sizeof(buf));
-
-				/* read(2) should be used after #25443 */
-				r = recv(fd, buf, sizeof(buf), 0);
-				printf("fd: %d: read %d bytes\n", fd, r);
+			printf("main: read '%s' on fd %d\n", buf, fds[i].fd);
+			if (strncmp(ctx[idx].name, buf, sizeof(buf)) != 0) {
+				printf("main: expected: '%s' actual: '%s'\n", ctx[idx].name, buf);
+				return -EINVAL;
 			}
 
-			if ((fds[i].revents & POLLERR) != 0) {
-				printf("fd: %d: error\n", fd);
+			res = write(fds[i].fd, buf, res);
+			if (res < 0) {
+				perror("write");
+				return -errno;
 			}
 
-			if ((fds[i].revents & POLLHUP) != 0) {
-				printf("fd: %d: hung up\n", fd);
-				close(ctx[idx].spair[0]);
-				printf("%s: closed fd %d\n", __func__,
-					ctx[idx].spair[0]);
-				pthread_join(ctx[idx].thread, &unused);
-				printf("joined %s\n", ctx[idx].name);
-				ctx[idx].spair[0] = -1;
-			}
+			++event;
 		}
 	}
 
-	printf("finished!\n");
+	if (event != n_events) {
+		printf("main: unhandled events remaining\n");
+		return -EINVAL;
+	}
+
+	return n_events;
+}
+
+#ifdef __ZEPHYR__
+void main(void)
+{
+#else
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+#endif
+
+	int res;
+	struct context ctx[NUM_SOCKETPAIRS] = {};
+	struct pollfd fds[NUM_SOCKETPAIRS] = {};
+
+	printf("setting-up\n");
+	res = setup(ctx, NUM_SOCKETPAIRS);
+	if (res < 0) {
+		goto out;
+	}
+
+	for (size_t n_events = NUM_SOCKETPAIRS * NUM_REPITITIONS; n_events > 0; n_events -= res) {
+
+		setup_poll(ctx, fds, NUM_SOCKETPAIRS);
+		res = poll(fds, NUM_SOCKETPAIRS, -1);
+		if (res < 0) {
+			perror("poll");
+			goto out;
+		}
+
+		res = handle_poll_events(ctx, fds, NUM_SOCKETPAIRS, res);
+		if (res < 0) {
+			goto out;
+		}
+	}
+
+	res = 0;
 
 out:
+	printf("tearing-down\n");
+	teardown(ctx, NUM_SOCKETPAIRS);
+
+	printf("%s\n", res == 0 ? "SUCCESS" : "FAILURE");
+
 #ifndef __ZEPHYR__
-	return 0;
-#else
-	return;
+	return res;
 #endif
 }
