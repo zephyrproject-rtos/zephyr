aa9061e613e9bf5f7c086106f55ffdd68b503a8d
Jonathan Rico <jonathan.rico@nordicsemi.no>
2023-02-06 15:24:14 +0100

    Bluetooth: host: add test for some GATT settings bugs
    This test is designed to check the soundness of GATT caching across
    reboots.
    
    It will fail if the fixes for #54173 and #54172 are not present.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

---
+++ tests/bluetooth/bsim_bt/bsim_test_gatt_settings/test_scripts/_compile.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Copyright 2023 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+bash_source_dir="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+
+# Read variable definitions output by _env.sh
+source "${bash_source_dir}/_env.sh"
+
+# Place yourself in the test's root (i.e. ./../)
+west build -b nrf52_bsim -d build_test && \
+    cp build_test/zephyr/zephyr.exe "${test_exe}"
