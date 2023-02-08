0ee6dea268feb11e85b78db90830493c19423fff
Daniel Leung <daniel.leung@intel.com>
2023-02-08 13:26:19 -0800

    tests: kernel: print FAILED when wrong faults caught
    For some kernel tests, faults and exceptions are expected.
    They are caught and the test would continue if the reasons
    for faults are as expected. However, when the unexpected
    reasons are encountered, the code simply prints a message
    and calls k_fatal_halt(). When running under twister,
    these messages are not the expected failed messages so
    twister will spin till timeout although the execution
    has already been halted. This adds another printk() before
    halt to signal twister that the test has failed and bails
    early.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- tests/kernel/mem_protect/stackprot/src/main.c
+++ tests/kernel/mem_protect/stackprot/src/main.c
@@ -19,6 +19,7 @@ void k_sys_fatal_error_handler(unsigned int reason, const z_arch_esf_t *esf)
 {
 	if (reason != K_ERR_STACK_CHK_FAIL) {
 		printk("wrong error type\n");
+		printk("PROJECT EXECUTION FAILED\n");
 		k_fatal_halt(reason);
 	}
 }
