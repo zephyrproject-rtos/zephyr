61d1ee239414d36f078134b335a7e5a701802924
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2023-02-09 07:53:08 +0530

    Bluetooth: Controller: Refactor duplicate aux_time_get implementation
    Refactor duplicate aux_time_get() implementation and reuse
    the same code.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_adv_aux.c
+++ subsys/bluetooth/controller/ll_sw/ull_adv_aux.c
@@ -52,8 +52,9 @@ static int init_reset(void);
 #if (CONFIG_BT_CTLR_ADV_AUX_SET > 0)
 static inline struct ll_adv_aux_set *aux_acquire(void);
 static inline void aux_release(struct ll_adv_aux_set *aux);
-static uint32_t aux_time_get(struct ll_adv_aux_set *aux, struct pdu_adv *pdu,
-			     struct pdu_adv *pdu_scan);
+static uint32_t aux_time_get(const struct ll_adv_aux_set *aux,
+			     const struct pdu_adv *pdu,
+			     uint8_t pdu_len, uint8_t pdu_scan_len);
 static uint8_t aux_time_update(struct ll_adv_aux_set *aux, struct pdu_adv *pdu,
 			       struct pdu_adv *pdu_scan);
 static void mfy_aux_offset_get(void *param);
@@ -2436,7 +2437,7 @@ uint32_t ull_adv_aux_evt_init(struct ll_adv_aux_set *aux,
 	pdu_scan = lll_adv_scan_rsp_peek(lll);
 
 	/* Calculate the PDU Tx Time and hence the radio event length */
-	time_us = aux_time_get(aux, pdu, pdu_scan);
+	time_us = aux_time_get(aux, pdu, pdu->len, pdu_scan->len);
 
 	/* TODO: active_to_start feature port */
 	aux->ull.ticks_active_to_start = 0;
@@ -2588,51 +2589,11 @@ struct ll_adv_aux_set *ull_adv_aux_get(uint8_t handle)
 uint32_t ull_adv_aux_time_get(const struct ll_adv_aux_set *aux, uint8_t pdu_len,
 			      uint8_t pdu_scan_len)
 {
-	const struct lll_adv_aux *lll_aux;
-	const struct lll_adv *lll;
 	const struct pdu_adv *pdu;
-	uint32_t time_us;
 
-	lll_aux = &aux->lll;
-	lll = lll_aux->adv;
+	pdu = lll_adv_aux_data_peek(&aux->lll);
 
-	if (IS_ENABLED(CONFIG_BT_CTLR_ADV_RESERVE_MAX) &&
-	    (lll->phy_s == PHY_CODED)) {
-		pdu_len = PDU_AC_EXT_PAYLOAD_OVERHEAD;
-		pdu_scan_len = PDU_AC_EXT_PAYLOAD_OVERHEAD;
-	}
-
-	/* NOTE: 16-bit values are sufficient for minimum radio event time
-	 *       reservation, 32-bit are used here so that reservations for
-	 *       whole back-to-back chaining of PDUs can be accomodated where
-	 *       the required microseconds could overflow 16-bits, example,
-	 *       back-to-back chained Coded PHY PDUs.
-	 */
-	time_us = PDU_AC_US(pdu_len, lll->phy_s, lll->phy_flags) +
-		  EVENT_OVERHEAD_START_US + EVENT_OVERHEAD_END_US;
-
-	pdu = lll_adv_aux_data_peek(lll_aux);
-	if ((pdu->adv_ext_ind.adv_mode & BT_HCI_LE_ADV_PROP_CONN) ==
-	    BT_HCI_LE_ADV_PROP_CONN) {
-		const uint16_t conn_req_us =
-			PDU_AC_MAX_US((INITA_SIZE + ADVA_SIZE + LLDATA_SIZE),
-				      lll->phy_s);
-		const uint16_t conn_rsp_us =
-			PDU_AC_US((PDU_AC_EXT_HEADER_SIZE_MIN + ADVA_SIZE +
-				   TARGETA_SIZE), lll->phy_s, lll->phy_flags);
-
-		time_us += EVENT_IFS_MAX_US * 2 + conn_req_us + conn_rsp_us;
-	} else if ((pdu->adv_ext_ind.adv_mode & BT_HCI_LE_ADV_PROP_SCAN) ==
-		   BT_HCI_LE_ADV_PROP_SCAN) {
-		const uint16_t scan_req_us  =
-			PDU_AC_MAX_US((SCANA_SIZE + ADVA_SIZE), lll->phy_s);
-		const uint16_t scan_rsp_us =
-			PDU_AC_US(pdu_scan_len, lll->phy_s, lll->phy_flags);
-
-		time_us += EVENT_IFS_MAX_US * 2 + scan_req_us + scan_rsp_us;
-	}
-
-	return time_us;
+	return aux_time_get(aux, pdu, pdu_len, pdu_scan_len);
 }
 
 void ull_adv_aux_offset_get(struct ll_adv_set *adv)
@@ -2877,12 +2838,22 @@ static inline void aux_release(struct ll_adv_aux_set *aux)
 	mem_release(aux, &adv_aux_free);
 }
 
-static uint32_t aux_time_get(struct ll_adv_aux_set *aux, struct pdu_adv *pdu,
-			     struct pdu_adv *pdu_scan)
+static uint32_t aux_time_get(const struct ll_adv_aux_set *aux,
+			     const struct pdu_adv *pdu,
+			     uint8_t pdu_len, uint8_t pdu_scan_len)
 {
-	struct lll_adv_aux *lll_aux;
-	struct lll_adv *lll;
+	const struct lll_adv_aux *lll_aux;
+	const struct lll_adv *lll;
 	uint32_t time_us;
+
+	lll_aux = &aux->lll;
+	lll = lll_aux->adv;
+
+	if (IS_ENABLED(CONFIG_BT_CTLR_ADV_RESERVE_MAX) &&
+	    (lll->phy_s == PHY_CODED)) {
+		pdu_len = PDU_AC_EXT_PAYLOAD_OVERHEAD;
+		pdu_scan_len = PDU_AC_EXT_PAYLOAD_OVERHEAD;
+	}
 
 	/* NOTE: 16-bit values are sufficient for minimum radio event time
 	 *       reservation, 32-bit are used here so that reservations for
@@ -2890,9 +2861,7 @@ static uint32_t aux_time_get(struct ll_adv_aux_set *aux, struct pdu_adv *pdu,
 	 *       the required microseconds could overflow 16-bits, example,
 	 *       back-to-back chained Coded PHY PDUs.
 	 */
-	lll_aux = &aux->lll;
-	lll = lll_aux->adv;
-	time_us = PDU_AC_US(pdu->len, lll->phy_s, lll->phy_flags) +
+	time_us = PDU_AC_US(pdu_len, lll->phy_s, lll->phy_flags) +
 		  EVENT_OVERHEAD_START_US + EVENT_OVERHEAD_END_US;
 
 	if ((pdu->adv_ext_ind.adv_mode & BT_HCI_LE_ADV_PROP_CONN) ==
@@ -2910,7 +2879,7 @@ static uint32_t aux_time_get(struct ll_adv_aux_set *aux, struct pdu_adv *pdu,
 		const uint16_t scan_req_us  =
 			PDU_AC_MAX_US((SCANA_SIZE + ADVA_SIZE), lll->phy_s);
 		const uint16_t scan_rsp_us =
-			PDU_AC_US(pdu_scan->len, lll->phy_s, lll->phy_flags);
+			PDU_AC_US(pdu_scan_len, lll->phy_s, lll->phy_flags);
 
 		time_us += EVENT_IFS_MAX_US * 2 + scan_req_us + scan_rsp_us;
 
@@ -2938,7 +2907,7 @@ static uint8_t aux_time_update(struct ll_adv_aux_set *aux, struct pdu_adv *pdu,
 	uint32_t time_us;
 	uint32_t ret;
 
-	time_us = aux_time_get(aux, pdu, pdu_scan);
+	time_us = aux_time_get(aux, pdu, pdu->len, pdu_scan->len);
 	time_ticks = HAL_TICKER_US_TO_TICKS(time_us);
 	if (aux->ull.ticks_slot > time_ticks) {
 		ticks_minus = aux->ull.ticks_slot - time_ticks;
