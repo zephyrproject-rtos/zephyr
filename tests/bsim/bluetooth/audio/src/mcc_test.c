ec79aa8c242822a0b32a8974f204ce9ee7b66280
Emil Gydesen <emil.gydesen@nordicsemi.no>
2023-02-10 15:20:28 +0100

    tests: Bluetooth: MCC: Add invalid testing of bt_mcc_otc_read_icon_object
    Add invalid testing of bt_mcc_otc_read_icon_object.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bsim/bluetooth/audio/src/mcc_test.c
+++ tests/bsim/bluetooth/audio/src/mcc_test.c
@@ -1530,6 +1530,14 @@ static void test_read_icon_obj(void)
 {
 	int err;
 
+	/* Invalid behavior */
+	err = bt_mcc_otc_read_icon_object(NULL);
+	if (err == 0) {
+		FAIL("bt_mcc_otc_read_icon_object did not fail with NULL conn");
+		return;
+	}
+
+	/* Valid behavior */
 	UNSET_FLAG(object_read);
 
 	err = bt_mcc_otc_read_icon_object(default_conn);
