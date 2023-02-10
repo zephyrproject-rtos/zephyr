32cd16cfac2f16a9fc8ea98b106aebf6e541c787
Emil Gydesen <emil.gydesen@nordicsemi.no>
2023-02-10 13:20:10 +0100

    Bluetooth: Audio: Add BT_MCS_VALID_OBJ_ID
    Add macro BT_MCS_VALID_OBJ_ID to verify the OTS Object
    ID before attempting any OTS operations.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/audio/mcs_internal.h
+++ subsys/bluetooth/audio/mcs_internal.h
@@ -16,6 +16,9 @@
 extern "C" {
 #endif
 
+/* This differs from BT_OTS_VALID_OBJ_ID as MCS does not use the directory list object */
+#define BT_MCS_VALID_OBJ_ID(id) (IN_RANGE((id), BT_OTS_OBJ_ID_MIN, BT_OTS_OBJ_ID_MAX))
+
 int bt_mcs_init(struct bt_ots_cb *ots_cbs);
 
 #ifdef __cplusplus
