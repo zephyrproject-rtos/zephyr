121c66b7ef9057de09ea9553213dc2db3955739d
Chris Friedt <cfriedt@meta.com>
2023-02-11 07:53:02 -0500

    tests: net: socketpair: put common includes in one header
    This serves to increase SNR in the test sources (i.e. to
    make test sources easier to read).
    
    Signed-off-by: Chris Friedt <cfriedt@meta.com>

--- tests/net/socket/socketpair/src/happy_path.c
+++ tests/net/socket/socketpair/src/happy_path.c
@@ -4,27 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifdef CONFIG_ARCH_POSIX
-#include <fcntl.h>
-#else
-#include <zephyr/posix/fcntl.h>
-#endif
-
-#include <zephyr/logging/log.h>
-LOG_MODULE_DECLARE(net_test, CONFIG_NET_SOCKETS_LOG_LEVEL);
-
-#include <string.h>
-#include <zephyr/net/socket.h>
-#include <zephyr/sys/util.h>
-#include <zephyr/posix/unistd.h>
-
-#include <zephyr/ztest_assert.h>
-
-#undef read
-#define read(fd, buf, len) zsock_recv(fd, buf, len, 0)
-
-#undef write
-#define write(fd, buf, len) zsock_send(fd, buf, len, 0)
+#include "_main.h"
 
 static void happy_path(
 	const int family, const char *family_s,
