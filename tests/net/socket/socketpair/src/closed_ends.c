dac9de4b0121a4cc07bd0541f57edcd3c1e90fcb
Chris Friedt <cfriedt@meta.com>
2023-02-11 20:45:26 -0500

    tests: net: socketpair: use do not alias read and write
    * do not define `read`, `write`, as different symbols
    * use `recv`, and `send` as defined by
      `CONFIG_NET_SOCKETS_POSIX_NAMES`
    * do not specify `(2)` as manual-page sections are
      somewhat irrelevant
    
    Note: we use `CONFIG_NET_SOCKETS_POSIX_NAMES` here, and
    `CONFIG_POSIX_API` in `samples/` to show that the socketpair
    API is available in both configurations.
    
    Signed-off-by: Chris Friedt <cfriedt@meta.com>

--- tests/net/socket/socketpair/src/closed_ends.c
+++ tests/net/socket/socketpair/src/closed_ends.c
@@ -12,13 +12,13 @@ ZTEST_USER(net_socketpair, close_one_end_and_write_to_the_other)
 
 	for (size_t i = 0; i < 2; ++i) {
 		res = socketpair(AF_UNIX, SOCK_STREAM, 0, sv);
-		zassert_equal(res, 0, "socketpair(2) failed: %d", errno);
+		zassert_equal(res, 0, "socketpair() failed: %d", errno);
 
 		res = close(sv[i]);
 		zassert_equal(res, 0, "close(sv[%u]) failed: %d", i, errno);
 
-		res = write(sv[(!i) & 1], "x", 1);
-		zassert_equal(res, -1, "expected write(2) to fail");
+		res = send(sv[(!i) & 1], "x", 1, 0);
+		zassert_equal(res, -1, "expected send() to fail");
 		zassert_equal(res, -1, "errno: expected: EPIPE actual: %d",
 			errno);
 
@@ -35,29 +35,28 @@ ZTEST_USER(net_socketpair, close_one_end_and_read_from_the_other)
 
 	for (size_t i = 0; i < 2; ++i) {
 		res = socketpair(AF_UNIX, SOCK_STREAM, 0, sv);
-		zassert_equal(res, 0, "socketpair(2) failed: %d", errno);
+		zassert_equal(res, 0, "socketpair() failed: %d", errno);
 
 		/* We want to write some bytes to the closing end of the
 		 * socket before it gets shut down, so that we can prove that
 		 * reading is possible from the other end still and that data
 		 * is not lost.
 		 */
-		res = write(sv[i], "xx", 2);
-		zassert_not_equal(res, -1, "write(2) failed: %d", errno);
-		zassert_equal(res, 2, "write(2) failed to write 2 bytes");
+		res = send(sv[i], "xx", 2, 0);
+		zassert_not_equal(res, -1, "send() failed: %d", errno);
+		zassert_equal(res, 2, "write() failed to write 2 bytes");
 
 		res = close(sv[i]);
 		zassert_equal(res, 0, "close(sv[%u]) failed: %d", i, errno);
 
 		memset(xx, 0, sizeof(xx));
-		res = read(sv[(!i) & 1], xx, sizeof(xx));
-		zassert_not_equal(res, -1, "read(2) failed: %d", errno);
+		res = recv(sv[(!i) & 1], xx, sizeof(xx), 0);
+		zassert_not_equal(res, -1, "read() failed: %d", errno);
 		zassert_equal(res, 2, "expected to read 2 bytes but read %d",
 			res);
 
-		res = read(sv[(!i) & 1], xx, sizeof(xx));
-		zassert_equal(res, 0,
-			"expected read(2) to succeed but read 0 bytes");
+		res = recv(sv[(!i) & 1], xx, sizeof(xx), 0);
+		zassert_equal(res, 0, "expected read() to succeed but read 0 bytes");
 
 		res = close(sv[(!i) & 1]);
 		zassert_equal(res, 0, "close(sv[%u]) failed: %d", i, errno);
--- tests/net/socket/socketpair/src/nonblock.c
+++ tests/net/socket/socketpair/src/nonblock.c
@@ -12,14 +12,14 @@ ZTEST_USER(net_socketpair, write_nonblock)
 	int sv[2] = {-1, -1};
 
 	res = socketpair(AF_UNIX, SOCK_STREAM, 0, sv);
-	zassert_equal(res, 0, "socketpair(2) failed: %d", errno);
+	zassert_equal(res, 0, "socketpair() failed: %d", errno);
 
 	for (size_t i = 0; i < 2; ++i) {
 		/* first, fill up the buffer */
 		for (size_t k = 0; k < CONFIG_NET_SOCKETPAIR_BUFFER_SIZE;
 			++k) {
-			res = write(sv[i], "x", 1);
-			zassert_equal(res, 1, "write(2) failed: %d", errno);
+			res = send(sv[i], "x", 1, 0);
+			zassert_equal(res, 1, "send() failed: %d", errno);
 		}
 
 		/* then set the O_NONBLOCK flag */
@@ -30,8 +30,8 @@ ZTEST_USER(net_socketpair, write_nonblock)
 		zassert_not_equal(res, -1, "fcntl() failed: %d", i, errno);
 
 		/* then, try to write one more byte */
-		res = write(sv[i], "x", 1);
-		zassert_equal(res, -1, "expected write to fail");
+		res = send(sv[i], "x", 1, 0);
+		zassert_equal(res, -1, "expected send to fail");
 		zassert_equal(errno, EAGAIN, "errno: expected: EAGAIN "
 			"actual: %d", errno);
 	}
@@ -47,7 +47,7 @@ ZTEST_USER(net_socketpair, read_nonblock)
 	char c;
 
 	res = socketpair(AF_UNIX, SOCK_STREAM, 0, sv);
-	zassert_equal(res, 0, "socketpair(2) failed: %d", errno);
+	zassert_equal(res, 0, "socketpair() failed: %d", errno);
 
 	for (size_t i = 0; i < 2; ++i) {
 		/* set the O_NONBLOCK flag */
@@ -58,8 +58,8 @@ ZTEST_USER(net_socketpair, read_nonblock)
 		zassert_not_equal(res, -1, "fcntl() failed: %d", i, errno);
 
 		/* then, try to read one byte */
-		res = read(sv[i], &c, 1);
-		zassert_equal(res, -1, "expected read to fail");
+		res = recv(sv[i], &c, 1, 0);
+		zassert_equal(res, -1, "expected recv() to fail");
 		zassert_equal(errno, EAGAIN, "errno: expected: EAGAIN "
 			"actual: %d", errno);
 	}
