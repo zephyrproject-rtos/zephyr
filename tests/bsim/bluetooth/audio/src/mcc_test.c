badebd501157c4f1b922f8c90e5e22dad9ccfdde
Emil Gydesen <emil.gydesen@nordicsemi.no>
2023-02-13 13:55:27 +0100

    tests: Bluetooth: MCC: Add invalid testing of otc_read_current_track_object
    Add invalid testing of bt_mcc_otc_read_current_track_object.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bsim/bluetooth/audio/src/mcc_test.c
+++ tests/bsim/bluetooth/audio/src/mcc_test.c
@@ -1890,6 +1890,14 @@ static void test_read_current_track_object(void)
 {
 	int err;
 
+	/* Invalid behavior */
+	err = bt_mcc_otc_read_current_track_object(NULL);
+	if (err == 0) {
+		FAIL("bt_mcc_otc_read_current_track_object did not fail with NULL conn");
+		return;
+	}
+
+	/* Valid behavior */
 	UNSET_FLAG(object_read);
 
 	err = bt_mcc_otc_read_current_track_object(default_conn);
