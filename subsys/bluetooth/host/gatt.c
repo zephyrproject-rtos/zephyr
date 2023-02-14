aed165df6197f87eabdec71cd9cc5300637b272a
Marek Pieta <Marek.Pieta@nordicsemi.no>
2023-02-14 10:45:14 +0100

    Bluetooth: host: Fix SC indication on encrypt change
    The SC indication can be sent only if the GATT Service Changed is
    enabled in the configuration. Change fixes build issues related to
    implicit sc_indicate declaration when Service Changed is disabled.
    
    Fixes: #54813
    
    Signed-off-by: Marek Pieta <Marek.Pieta@nordicsemi.no>

--- subsys/bluetooth/host/gatt.c
+++ subsys/bluetooth/host/gatt.c
@@ -5748,7 +5748,7 @@ void bt_gatt_encrypt_change(struct bt_conn *conn)
 
 	bt_gatt_foreach_attr(0x0001, 0xffff, update_ccc, &data);
 
-#if defined(CONFIG_BT_SETTINGS)
+#if defined(CONFIG_BT_SETTINGS) && defined(CONFIG_BT_GATT_SERVICE_CHANGED)
 	if (!bt_gatt_change_aware(conn, false)) {
 		/* Send a Service Changed indication if the current peer is
 		 * marked as change-unaware.
