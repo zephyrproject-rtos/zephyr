4683c8d4d5c1cc92581ad452d57bb5de0cc6c8d6
Daniel Leung <daniel.leung@intel.com>
2023-02-14 10:41:55 -0800

    linker: introduce profile xt-ld
    Xtensa toolchain has its own linker, xt-ld, which is based on
    binutils' ld. There are, of course, Xtensa specific options.
    But mostly it is based on old version of ld. It would be
    better to detach it from the ld profile to avoid any
    incompatible changes there.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- cmake/toolchain/xt-clang/generic.cmake
+++ cmake/toolchain/xt-clang/generic.cmake
@@ -5,5 +5,6 @@ include(${ZEPHYR_BASE}/cmake/toolchain/xcc/common.cmake)
 set(COMPILER xt-clang)
 set(CC clang)
 set(C++ clang++)
+set(LINKER xt-ld)
 
 message(STATUS "Found toolchain: xt-clang (${XTENSA_TOOLCHAIN_PATH})")
