cc482128755bb90f2111e569b763dbaeed0076e0
Benjamin Björnsson <benjamin.bjornsson@gmail.com>
2023-02-15 22:43:30 +0100

    soc: arm: st_stm32: stm32c0: Add STM32C0 Series support
    Add initial support of STM32C0 Series.
    
    Signed-off-by: Benjamin Björnsson <benjamin.bjornsson@gmail.com>

---
+++ soc/arm/st_stm32/stm32c0/soc.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023 Benjamin Björnsson <benjamin.bjornsson@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file SoC configuration macros for the STM32C0 family processors.
+ *
+ * Based on reference manual:
+ *   STM32C0X advanced ARM ® -based 32-bit MCUs
+ *
+ * Chapter 2.2: Memory organization
+ */
+
+
+#ifndef _STM32C0_SOC_H_
+#define _STM32C0_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <stm32c0xx.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _STM32C0_SOC_H_ */
