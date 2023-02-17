7dcf7ee6553bbd275965d6feafe952796470dc3c
Tom Burdick <thomas.burdick@intel.com>
2023-02-17 12:16:05 -0600

    rtio: Add callback op
    Adds a callback op to RTIO enabling C logic to be interspersed with
    I/O operations. This is not safe from userspace but allowable in kernel
    space.
    
    Signed-off-by: Tom Burdick <thomas.burdick@intel.com>

--- include/zephyr/rtio/rtio.h
+++ include/zephyr/rtio/rtio.h
@@ -52,9 +52,6 @@ extern "C" {
  * @}
  */
 
-
-struct rtio_iodev;
-
 /**
  * @brief RTIO API
  * @defgroup rtio_api RTIO API
@@ -121,6 +118,23 @@ struct rtio_iodev;
  * @}
  */
 
+/** @cond ignore */
+struct rtio;
+struct rtio_cqe;
+struct rtio_sqe;
+struct rtio_iodev;
+struct rtio_iodev_sqe;
+/** @endcond */
+
+/**
+ * @typedef rtio_callback_t
+ * @brief Callback signature for RTIO_OP_CALLBACK
+ * @param r RTIO context being used with the callback
+ * @param sqe Submission for the callback op
+ * @param arg0 Argument option as part of the sqe
+ */
+typedef void (*rtio_callback_t)(struct rtio *r, const struct rtio_sqe *sqe, void *arg0);
+
 /**
  * @brief A submission queue event
  */
@@ -147,7 +161,6 @@ struct rtio_sqe {
 		/** OP_TX, OP_RX */
 		struct {
 			uint32_t buf_len; /**< Length of buffer */
-
 			uint8_t *buf; /**< Buffer to use*/
 		};
 
@@ -159,11 +172,18 @@ struct rtio_sqe {
 
 		/** OP_CALLBACK */
 		struct {
-			void (*callback)(struct rtio *r, struct rtio_sqe *sqe, void *arg0);
-			void *arg0;
+			rtio_callback_t callback;
+			void *arg0; /**< Last argument given to callback */
 		};
 	};
 };
+
+
+/** @cond ignore */
+/* Ensure the rtio_sqe never grows beyond a common cacheline size of 64 bytes */
+BUILD_ASSERT(sizeof(struct rtio_sqe) <= 64);
+/** @endcond */
+
 
 /**
  * @brief Submission queue
@@ -195,8 +215,6 @@ struct rtio_cq {
 	struct rtio_cqe buffer[];
 };
 
-struct rtio;
-struct rtio_iodev_sqe;
 
 struct rtio_executor_api {
 	/**
@@ -341,11 +359,12 @@ struct rtio_iodev {
 /** An operation that transmits (writes) */
 #define RTIO_OP_TX (RTIO_OP_RX+1)
 
-/** An operation that transmits tiny writes */
+/** An operation that transmits tiny writes by copying the data to write */
 #define RTIO_OP_TINY_TX (RTIO_OP_TX+1)
 
-/** An operation that does some small functional work */
-#define RTIO_OP_FUNC (RTIO_OP_TINY_TX+1)
+/** An operation that calls a given function (callback) */
+#define RTIO_OP_CALLBACK (RTIO_OP_TINY_TX+1)
+
 
 
 /**
@@ -424,6 +443,28 @@ static inline void rtio_sqe_prep_tiny_write(struct rtio_sqe *sqe,
 	sqe->iodev = iodev;
 	sqe->tiny_buf_len = tiny_write_len;
 	memcpy(sqe->tiny_buf, tiny_write_data, tiny_write_len);
+	sqe->userdata = userdata;
+}
+
+/**
+ * @brief Prepare a callback op submission
+ *
+ * A somewhat special operation in that it may only be done in kernel mode.
+ *
+ * Used where general purpose logic is required in a queue of io operations to do
+ * transforms or logic.
+ */
+static inline void rtio_sqe_prep_callback(struct rtio_sqe *sqe,
+					  rtio_callback_t callback,
+					  void *arg0,
+					  void *userdata)
+{
+	sqe->op = RTIO_OP_CALLBACK;
+	sqe->prio = 0;
+	sqe->flags = 0;
+	sqe->iodev = NULL;
+	sqe->callback = callback;
+	sqe->arg0 = arg0;
 	sqe->userdata = userdata;
 }
 
