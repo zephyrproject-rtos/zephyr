626ec7413b6d38bad4ed0a3b1fe5424b183cdbd4
Anas Nashif <anas.nashif@intel.com>
2023-02-20 18:02:26 +0000

    Bluetooth: tests: move ctrl_le_ping to new ztest api
    Move to new ZTEST API, no functional changes.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/bluetooth/controller/ctrl_le_ping/src/main.c
+++ tests/bluetooth/controller/ctrl_le_ping/src/main.c
@@ -75,7 +75,7 @@ static void setup(void)
  *    |                            |<------------------|
  *    |                            |                   |
  */
-void test_ping_central_loc(void)
+ZTEST(bluetooth_ctrl_le_ping, test_ping_central_loc)
 {
 	uint8_t err;
 	struct node_tx *tx;
@@ -165,7 +165,7 @@ void test_ping_central_loc(void)
  *  ~~~~~~~~~~~~~~~~~ TERMINATE CONNECTION ~~~~~~~~~~~~~~
  *    |                            |                   |
  */
-void test_ping_central_loc_invalid_rsp(void)
+ZTEST(bluetooth_ctrl_le_ping, test_ping_central_loc_invalid_rsp)
 {
 	uint8_t err;
 	struct node_tx *tx;
@@ -266,7 +266,7 @@ void test_ping_central_loc_invalid_rsp(void)
  *    |                            |                   |
  *    |                            |                   |
  */
-void test_ping_periph_loc(void)
+ZTEST(bluetooth_ctrl_le_ping, test_ping_periph_loc)
 {
 	uint8_t err;
 	struct node_tx *tx;
@@ -319,7 +319,7 @@ void test_ping_periph_loc(void)
  *    |        |------------------>|
  *    |        |                   |
  */
-void test_ping_central_rem(void)
+ZTEST(bluetooth_ctrl_le_ping, test_ping_central_rem)
 {
 	struct node_tx *tx;
 
@@ -373,7 +373,7 @@ void test_ping_central_rem(void)
  *    |        |------------------>|
  *    |        |                   |
  */
-void test_ping_periph_rem(void)
+ZTEST(bluetooth_ctrl_le_ping, test_ping_periph_rem)
 {
 	struct node_tx *tx;
 
@@ -416,20 +416,4 @@ void test_ping_periph_rem(void)
 		      "Free CTX buffers %d", ctx_buffers_free());
 }
 
-void test_main(void)
-{
-	ztest_test_suite(ping,
-			 ztest_unit_test_setup_teardown(test_ping_central_loc, setup,
-							unit_test_noop),
-			 ztest_unit_test_setup_teardown(test_ping_central_loc_invalid_rsp, setup,
-							unit_test_noop),
-			 ztest_unit_test_setup_teardown(test_ping_periph_loc, setup,
-							unit_test_noop),
-			 ztest_unit_test_setup_teardown(test_ping_central_rem, setup,
-							unit_test_noop),
-			 ztest_unit_test_setup_teardown(test_ping_periph_rem, setup,
-							unit_test_noop)
-		);
-
-	ztest_run_test_suite(ping);
-}
+ZTEST_SUITE(bluetooth_ctrl_le_ping, NULL, NULL, setup, NULL, NULL);
