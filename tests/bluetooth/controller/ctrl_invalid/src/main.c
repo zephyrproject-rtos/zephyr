170ab2585bec0b37004bc31b50ac7f6bdd0bcb7a
Anas Nashif <anas.nashif@intel.com>
2023-02-20 18:03:56 +0000

    Bluetooth: tests: move ctrl_invalid to new ztest api
    Move to new ZTEST API, no functional changes.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/bluetooth/controller/ctrl_invalid/src/main.c
+++ tests/bluetooth/controller/ctrl_invalid/src/main.c
@@ -114,7 +114,7 @@ static void lt_tx_invalid_pdu_size(enum helper_pdu_opcode opcode, int adj_size)
 		      "Free CTX buffers %d", ctx_buffers_free());
 }
 
-void test_invalid_pdu_ignore_rx(void)
+ZTEST(bluetooth_ctrl_invalid, test_invalid_pdu_ignore_rx)
 {
 	/* Role */
 	test_set_role(&test_conn, BT_HCI_ROLE_PERIPHERAL);
@@ -181,11 +181,4 @@ void test_invalid_pdu_ignore_rx(void)
 	lt_tx_invalid_pdu_size(LL_CTE_RSP, 1);
 }
 
-void test_main(void)
-{
-	ztest_test_suite(invalid,
-			 ztest_unit_test_setup_teardown(test_invalid_pdu_ignore_rx, setup,
-							unit_test_noop));
-
-	ztest_run_test_suite(invalid);
-}
+ZTEST_SUITE(bluetooth_ctrl_invalid, NULL, NULL, setup, NULL, NULL);
