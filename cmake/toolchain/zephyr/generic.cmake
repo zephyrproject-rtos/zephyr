5e4907ee2756bc2db317fd352b0134d0ee5a3ace
Kumar Gala <kumar.gala@intel.com>
2023-02-21 17:03:42 +0000

    toolchain: rework Zephyr toolchain Kconfig
    Introduce cmake/toolchain/zephyr/Kconfig for any toolchain common
    properties that might exist for the toolchain.  As part of this
    introduce ZEPHYR_TOOLCHAIN_KCONFIG_DIR variable to use to reference
    the Kconfig file from the installed SDK.
    
    Signed-off-by: Kumar Gala <kumar.gala@intel.com>

--- cmake/toolchain/zephyr/generic.cmake
+++ cmake/toolchain/zephyr/generic.cmake
@@ -2,6 +2,6 @@
 
 include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/generic.cmake)
 
-set(TOOLCHAIN_KCONFIG_DIR ${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr)
+set(ZEPHYR_TOOLCHAIN_KCONFIG_DIR ${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr)
 
 message(STATUS "Found toolchain: zephyr ${SDK_VERSION} (${ZEPHYR_SDK_INSTALL_DIR})")
