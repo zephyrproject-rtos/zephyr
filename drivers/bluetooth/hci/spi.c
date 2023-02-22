e0b9c3d1e94df00ae16b612360cf6fe8c7bb54fb
Jonathan Rico <jonathan.rico@nordicsemi.no>
2023-02-22 08:45:15 +0100

    Bluetooth: HCI: Make driver stack sizes configurable
    This is necessary to prevent stack overflows when building with
    non-standard configurations (e.g. CONFIG_DEBUG).
    
    Adding them as hidden kconfig options to avoid the stuck kconfig syndrome.
    Users are free to redefine them in their app to force a value.
    
    The userchan.c driver is only built for posix, and the help text for
    ARCH_POSIX_RECOMMENDED_STACK_SIZE states that the real stack comes from
    somewhere else (the pthread stack), hence why it doesn't use the new
    kconfig options.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- drivers/bluetooth/hci/spi.c
+++ drivers/bluetooth/hci/spi.c
@@ -68,7 +68,7 @@ static K_SEM_DEFINE(sem_initialised, 0, 1);
 static K_SEM_DEFINE(sem_request, 0, 1);
 static K_SEM_DEFINE(sem_busy, 1, 1);
 
-static K_KERNEL_STACK_DEFINE(spi_rx_stack, 512);
+static K_KERNEL_STACK_DEFINE(spi_rx_stack, CONFIG_BT_DRV_RX_STACK_SIZE);
 static struct k_thread spi_rx_thread_data;
 
 #if defined(CONFIG_BT_HCI_DRIVER_LOG_LEVEL_DBG)
