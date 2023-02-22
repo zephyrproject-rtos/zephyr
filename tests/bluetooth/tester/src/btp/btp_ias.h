2676553845d0b2dd2897bdd2eef869745afc9e42
Szymon Janc <szymon.janc@codecoup.pl>
2023-02-22 16:33:27 +0100

    bluetooth: tester: Fix IAS service registration
    Make sure we don't send events if service was not registered.
    
    Signed-off-by: Szymon Janc <szymon.janc@codecoup.pl>

--- tests/bluetooth/tester/src/btp/btp_ias.h
+++ tests/bluetooth/tester/src/btp/btp_ias.h
@@ -10,7 +10,7 @@
 #include <stdint.h>
 
 /* events */
-#define BTP_IAS_EV_OUT_ALERT_ACTION	0x80
+#define BTP_IAS_EV_OUT_ALERT_ACTION 0x80
 struct btp_ias_alert_action_ev {
 	uint8_t alert_lvl;
 } __packed;
