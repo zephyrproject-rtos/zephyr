a67867e5a8cacb466522694a1c65ed76480a4368
Franciszek Zdobylak <fzdobylak@antmicro.com>
2023-02-24 10:12:03 +0100

    fs: ext2: create dir and file operations
    Introduced functions:
      - open
      - close
      - mkdir
    
    Signed-off-by: Franciszek Zdobylak <fzdobylak@antmicro.com>

--- subsys/fs/ext2/ext2_impl.c
+++ subsys/fs/ext2/ext2_impl.c
@@ -13,6 +13,7 @@
 #include "ext2.h"
 #include "ext2_impl.h"
 #include "ext2_struct.h"
+#include "ext2_diskops.h"
 
 LOG_MODULE_REGISTER(ext2, CONFIG_EXT2_LOG_LEVEL);
 
@@ -44,7 +45,7 @@ void  ext2_heap_free(void *ptr)
 
 /* Block operations --------------------------------------------------------- */
 
-struct ext2_block *ext2_get_block(struct ext2_data *fs, uint32_t block)
+static struct ext2_block *get_block_struct(void)
 {
 	int ret;
 	struct ext2_block *b;
@@ -58,39 +59,72 @@ struct ext2_block *ext2_get_block(struct ext2_data *fs, uint32_t block)
 	ret = k_mem_slab_alloc(&ext2_block_memory_slab, (void **)&b->data, K_NO_WAIT);
 	if (ret < 0) {
 		LOG_ERR("get block: alloc block memory error %d", ret);
-		goto fail_alloc;
+		k_mem_slab_free(&ext2_block_struct_slab, (void **)&b);
+		return NULL;
 	}
+	return b;
+}
 
+struct ext2_block *ext2_get_block(struct ext2_data *fs, uint32_t block)
+{
+	int ret;
+	struct ext2_block *b = get_block_struct();
+
+	if (!b) {
+		return NULL;
+	}
 	b->num = block;
+	b->flags = EXT2_BLOCK_ASSIGNED;
 	ret = fs->backend_ops->read_block(fs, b->data, block);
 	if (ret < 0) {
 		LOG_ERR("get block: read block error %d", ret);
-		goto fail_read;
+		ext2_drop_block(fs, b);
+		return NULL;
 	}
-
 	return b;
+}
 
-fail_read:
-	k_mem_slab_free(&ext2_block_memory_slab, (void **)&b->data);
-fail_alloc:
-	k_mem_slab_free(&ext2_block_struct_slab, (void **)&b);
-	return NULL;
+struct ext2_block *ext2_get_empty_block(struct ext2_data *fs)
+{
+	struct ext2_block *b = get_block_struct();
+
+	if (!b) {
+		return NULL;
+	}
+	b->num = 0;
+	b->flags = 0;
+	memset(b->data, 0, fs->block_size);
+	return b;
 }
 
 int ext2_sync_block(struct ext2_data *fs, struct ext2_block *b)
 {
 	int ret;
 
+	if (!(b->flags & EXT2_BLOCK_ASSIGNED)) {
+		return -EINVAL;
+	}
+
 	ret = fs->backend_ops->write_block(fs, b->data, b->num);
 	if (ret < 0) {
 		return ret;
 	}
+
+	b->flags &= ~EXT2_BLOCK_DIRTY;
 	return 0;
 }
 
-void ext2_drop_block(struct ext2_block *b)
+void ext2_drop_block(struct ext2_data *fs, struct ext2_block *b)
 {
-	if (b->data != NULL) {
+	if (b == NULL) {
+		return;
+	}
+
+	if (b->flags & EXT2_BLOCK_DIRTY) {
+		ext2_sync_block(fs, b);
+	}
+
+	if (b != NULL && b->data != NULL) {
 		k_mem_slab_free(&ext2_block_memory_slab, (void **)&b->data);
 		k_mem_slab_free(&ext2_block_struct_slab, (void **)&b);
 	}
@@ -110,6 +144,26 @@ void ext2_init_blocks_slab(struct ext2_data *fs)
 			CONFIG_EXT2_MAX_BLOCK_COUNT);
 }
 
+int ext2_assign_block_num(struct ext2_data *fs, struct ext2_block *b)
+{
+	int64_t new_block;
+
+	if (b->flags & EXT2_BLOCK_ASSIGNED) {
+		return -EINVAL;
+	}
+
+	/* Allocate block in the file system. */
+	new_block = ext2_alloc_block(fs);
+	if (new_block < 0) {
+		return new_block;
+	}
+
+	b->num = new_block;
+	b->flags |= EXT2_BLOCK_ASSIGNED;
+	return 0;
+}
+
+
 /* FS operations ------------------------------------------------------------ */
 
 int ext2_init_storage(struct ext2_data **fsp, const void *storage_dev, int flags)
@@ -123,6 +177,8 @@ int ext2_init_storage(struct ext2_data **fsp, const void *storage_dev, int flags
 	int64_t dev_size, write_size;
 
 	*fsp = fs;
+	fs->open_inodes = 0;
+	fs->flags = 0;
 
 	ret = ext2_init_disk_access_backend(fs, storage_dev, flags);
 	if (ret < 0) {
@@ -251,15 +307,11 @@ int ext2_init_fs(struct ext2_data *fs)
 		/* Update sblock fields set during the successful mount. */
 		EXT2_DATA_SBLOCK(fs)->s_state = EXT2_ERROR_FS;
 		EXT2_DATA_SBLOCK(fs)->s_mnt_count += 1;
-
-		ret = ext2_sync_block(fs, fs->sblock);
-		if (ret < 0) {
-			goto out;
-		}
+		fs->sblock->flags |= EXT2_BLOCK_DIRTY;
 	}
 	return 0;
 out:
-	ext2_drop_block(fs->sblock);
+	ext2_drop_block(fs, fs->sblock);
 	fs->sblock = NULL;
 	return ret;
 }
@@ -268,18 +320,27 @@ int ext2_close_fs(struct ext2_data *fs)
 {
 	int ret = 0;
 
-	if (!(fs->flags & EXT2_DATA_FLAGS_RO)) {
-		EXT2_DATA_SBLOCK(fs)->s_state = EXT2_VALID_FS;
-
-		ret = ext2_sync_block(fs, fs->sblock);
-		if (ret < 0) {
-			goto out;
+	/* Close all open inodes */
+	for (int32_t i = 0; i < fs->open_inodes; ++i) {
+		if (fs->inode_pool[i] != NULL) {
+			ext2_inode_drop(fs->inode_pool[i]);
 		}
 	}
 
-	ext2_drop_block(fs->sblock);
+	if (!(fs->flags & EXT2_DATA_FLAGS_RO)) {
+		EXT2_DATA_SBLOCK(fs)->s_state = EXT2_VALID_FS;
+		fs->sblock->flags |= EXT2_BLOCK_DIRTY;
+	}
+
+	/* free block group if it is fetched */
+	if (fs->bgroup != NULL) {
+		ext2_drop_block(fs, fs->bgroup->inode_table);
+		ext2_drop_block(fs, fs->bgroup->inode_bitmap);
+		ext2_drop_block(fs, fs->bgroup->block_bitmap);
+		ext2_drop_block(fs, fs->bgroup->block);
+	}
+	ext2_drop_block(fs, fs->sblock);
 	fs->sblock = NULL;
-out:
 	return ret;
 }
 
@@ -288,4 +349,557 @@ int ext2_close_struct(struct ext2_data *fs)
 	memset(fs, 0, sizeof(struct ext2_data));
 	initialized = false;
 	return 0;
+}
+
+/* Lookup ------------------------------------------------------------------- */
+
+/* Functions needed by lookup inode */
+static const char *skip_slash(const char *str);
+static char *strchrnul(const char *str, const char c);
+static int64_t find_dir_entry(struct ext2_inode *inode, const char *name, size_t len,
+		uint32_t *r_offset);
+
+int ext2_lookup_inode(struct ext2_data *fs, struct ext2_lookup_args *args)
+{
+	LOG_DBG("Looking for file %s", args->path);
+
+	int rc, ret = 0;
+	struct ext2_inode *cur_dir = NULL, *next = NULL;
+	static char name_buf[EXT2_MAX_FILE_NAME + 1];
+
+	/* Start looking from root directory of file system */
+	rc = ext2_inode_get(fs, EXT2_ROOT_INODE, &cur_dir);
+	if (rc < 0) {
+		ret = rc;
+		goto out;
+	}
+
+	/* There may be slash at the beginning of path */
+	const char *path = args->path;
+
+	path = skip_slash(path);
+
+	/* If path is empty then return root directory */
+	if (path[0] == '\0') {
+		args->inode = cur_dir;
+		cur_dir = NULL;
+		goto out;
+	}
+
+	for (;;) {
+		/* Get path component */
+		char *end = strchrnul(path, '/');
+		size_t len = end - path;
+
+		if (len > EXT2_MAX_FILE_NAME) {
+			ret = -ENAMETOOLONG;
+			goto out;
+		}
+
+		strncpy(name_buf, path, len);
+		name_buf[len] = '\0';
+
+		/* Search in current directory */
+		uint32_t dir_off = 0;
+		/* using 64 bit value to don't lose any information on error */
+		int64_t ino = find_dir_entry(cur_dir, name_buf, len, &dir_off);
+
+		const char *next_path = skip_slash(end);
+		bool last_entry = next_path[0] == '\0';
+
+		if (!last_entry) {
+			/*  prepare the next loop iteration */
+
+			if (ino < 0) {
+				/* next entry not found */
+				ret = -ENOENT;
+				goto out;
+			}
+
+			rc = ext2_inode_get(fs, ino, &next);
+			if (rc < 0) {
+				/* error while fetching next entry */
+				ret = rc;
+				goto out;
+			}
+
+			if (!(next->i_mode & EXT2_S_IFDIR)) {
+				/* path component should be directory */
+				ret = -ENOTDIR;
+				goto out;
+			}
+
+			/* Go to the next path component */
+			path = next_path;
+
+			/* Move to next directory */
+			ext2_inode_drop(cur_dir);
+			cur_dir = next;
+
+			next = NULL;
+			continue;
+		}
+
+		/* Last entry */
+
+		if (ino < 0 && !(args->flags & LOOKUP_ARG_CREATE)) {
+			/* entry not found but we need it */
+			ret = -ENOENT;
+			goto out;
+		}
+
+		if (ino > 0) {
+			rc = ext2_inode_get(fs, ino, &next);
+			if (rc < 0) {
+				ret = rc;
+				goto out;
+			}
+		}
+
+		/* Store parent directory and offset in parent directory */
+		if (args->flags & (LOOKUP_ARG_CREATE | LOOKUP_ARG_STAT | LOOKUP_ARG_UNLINK)) {
+			/* In create it will be valid only if we have found existing file */
+			args->offset = dir_off;
+			args->parent = cur_dir;
+			cur_dir = NULL;
+		}
+
+		/* Store name info */
+		if (args->flags & LOOKUP_ARG_CREATE) {
+			args->name_pos = path - args->path;
+			args->name_len = len;
+		}
+
+		/* Store found inode */
+		if (ino > 0) {
+			args->inode = next;
+			next = NULL;
+		}
+		goto out;
+	}
+
+out:
+	/* Always free that inodes.
+	 * If some of them is returned from function then proper pointer is set to NULL.
+	 */
+	ext2_inode_drop(cur_dir);
+	ext2_inode_drop(next);
+	return ret;
+}
+
+/* Return position of given char or end of string. */
+static char *strchrnul(const char *s, char c)
+{
+	while ((*s != c) && (*s != '\0')) {
+		s++;
+	}
+	return (char *) s;
+}
+
+static const char *skip_slash(const char *s)
+{
+	while ((*s == '/') && (*s != '\0')) {
+		s++;
+	}
+	return s;
+}
+
+/**
+ * @brief Find inode
+ *
+ * @note Inodes are 32 bit. When we return signed 64 bit number then we don't
+ *       lose any information.
+ *
+ * @param r_offset If not NULL then offset in directory of that entry is written here.
+ * @return Inode number or negative error code
+ */
+static int64_t find_dir_entry(struct ext2_inode *inode, const char *name, size_t len,
+		uint32_t *r_offset)
+{
+	int rc;
+	uint32_t block, block_off, offset = 0;
+	struct ext2_data *fs = inode->i_fs;
+
+	while (offset < inode->i_size) {
+		block = offset / fs->block_size;
+		block_off = offset % fs->block_size;
+
+		rc = ext2_fetch_inode_block(inode, block);
+		if (rc < 0) {
+			return rc;
+		}
+
+		struct ext2_disk_dentry *de =
+			(struct ext2_disk_dentry *)(inode_current_block_mem(inode) + block_off);
+
+		if (len == de->de_name_len && strncmp(de->de_name, name, len) == 0) {
+
+			if (r_offset) {
+				/* Return offset*/
+				*r_offset = offset;
+			}
+
+			return (int64_t)de->de_inode;
+		}
+
+		/* move to next directory entry */
+		offset += de->de_rec_len;
+	}
+
+	return -EINVAL;
+}
+
+/* Create files and directories */
+
+/* Allocate inode number and fill inode table with default values. */
+static int ext2_create_inode(struct ext2_data *fs, struct ext2_inode *parent,
+		struct ext2_inode *inode, int type)
+{
+	int32_t ino = ext2_alloc_inode(fs);
+
+	if (ino < 0) {
+		return ino;
+	}
+
+	/* Clear inode table entry */
+	int rc = ext2_clear_inode(fs, ino);
+
+	if (rc < 0) {
+		return rc;
+	}
+
+	/* fill inode with correct data */
+	inode->i_fs = fs;
+	inode->flags = 0;
+	inode->i_id = ino;
+	inode->i_size = 0;
+	inode->i_mode = type == FS_DIR_ENTRY_FILE ? EXT2_S_IFREG : EXT2_S_IFDIR;
+	inode->i_links_count = 0;
+	memset(inode->i_block, 0, 15 * 4);
+
+	if (type == FS_DIR_ENTRY_DIR) {
+		/* Block group current block is already fetched. We don't have to do it again.
+		 * (It was done above in clear_inode function.)
+		 */
+		fs->bgroup->block->flags |= EXT2_BLOCK_DIRTY;
+		current_disk_bgroup(fs->bgroup)->bg_used_dirs_count += 1;
+	}
+
+	rc = ext2_commit_inode(inode);
+	return rc;
+}
+
+static int ext2_add_direntry(struct ext2_inode *dir, struct ext2_disk_dentry *entry)
+{
+	LOG_DBG("Adding entry: {in=%d type=%d name_len=%d} to directory (in=%d)",
+			entry->de_inode, entry->de_file_type, entry->de_name_len, dir->i_id);
+
+	int rc = 0;
+	uint32_t block_size = dir->i_fs->block_size;
+	uint32_t entry_size = sizeof(struct ext2_disk_dentry) + entry->de_name_len;
+
+	if (entry_size > block_size) {
+		return -EINVAL;
+	}
+
+	/* Find last entry */
+	/* get last block and start from first entry on that block */
+	int last_blk = (dir->i_size / block_size) - 1;
+
+	rc = ext2_fetch_inode_block(dir, last_blk);
+	if (rc < 0) {
+		return rc;
+	}
+
+	uint32_t offset = 0;
+
+	struct ext2_disk_dentry *de = 0;
+	/* loop must be executed at least once, because block_size > 0 */
+	while (offset < block_size) {
+		de = (struct ext2_disk_dentry *)(inode_current_block_mem(dir) + offset);
+		if (offset + de->de_rec_len == block_size) {
+			break;
+		}
+		offset += de->de_rec_len;
+	}
+
+
+	uint32_t occupied = sizeof(struct ext2_disk_dentry) + de->de_name_len;
+
+	/* Align to 4 bytes */
+	occupied = ROUND_UP(occupied, 4);
+
+	LOG_DBG("Occupied: %d total: %d needed: %d", occupied, de->de_rec_len, entry_size);
+
+	if (de->de_rec_len - occupied >= entry_size) {
+		/* Entry fits into current block */
+		offset += occupied;
+		de->de_rec_len = occupied;
+		entry->de_rec_len = block_size - offset;
+
+	} else {
+		LOG_DBG("Allocating new block for directory");
+
+		/* Have to allocate new block */
+		rc = ext2_fetch_inode_block(dir, last_blk + 1);
+		if (rc < 0) {
+			return rc;
+		}
+
+		/* Increase size of directory */
+		dir->i_size += block_size;
+		rc = ext2_commit_inode(dir);
+		if (rc < 0) {
+			return rc;
+		}
+		rc = ext2_commit_inode_block(dir);
+		if (rc < 0) {
+			return rc;
+		}
+
+		/* New entry will start at offset 0 */
+		offset = 0;
+		entry->de_rec_len = block_size;
+	}
+
+	LOG_DBG("Writing entry {in=%d type=%d rec_len=%d name_len=%d} to block %d of inode %d",
+			entry->de_inode, entry->de_file_type, entry->de_rec_len, entry->de_name_len,
+			inode_current_block(dir)->num, dir->i_id);
+
+	memcpy(inode_current_block_mem(dir) + offset, entry, entry_size);
+
+	rc = ext2_commit_inode_block(dir);
+	return rc;
+}
+
+int ext2_create_file(struct ext2_inode *parent, struct ext2_inode *new_inode,
+		struct ext2_lookup_args *args)
+{
+	int rc, ret = 0;
+	struct ext2_data *fs = parent->i_fs;
+
+	rc = ext2_create_inode(fs, args->inode, new_inode, FS_DIR_ENTRY_FILE);
+	if (rc < 0) {
+		return rc;
+	}
+
+	struct ext2_disk_dentry *entry =
+		ext2_heap_alloc(sizeof(struct ext2_disk_dentry) + args->name_len);
+
+	if (entry == NULL) {
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	entry->de_inode = new_inode->i_id;
+	entry->de_name_len = args->name_len;
+	entry->de_file_type = EXT2_FT_REG_FILE;
+	memcpy(entry->de_name, args->path + args->name_pos, entry->de_name_len);
+
+	rc = ext2_add_direntry(parent, entry);
+	if (rc < 0) {
+		ret = rc;
+		goto out;
+	}
+
+	/* Successfully added to directory */
+	new_inode->i_links_count += 1;
+
+	rc = ext2_commit_inode(new_inode);
+	if (rc < 0) {
+		ret = rc;
+	}
+out:
+	ext2_heap_free(entry);
+	return ret;
+}
+
+int ext2_create_dir(struct ext2_inode *parent, struct ext2_inode *new_inode,
+		struct ext2_lookup_args *args)
+{
+	int rc, ret = 0;
+	struct ext2_data *fs = parent->i_fs;
+	uint32_t block_size = parent->i_fs->block_size;
+
+	rc = ext2_create_inode(fs, args->inode, new_inode, FS_DIR_ENTRY_DIR);
+	if (rc < 0) {
+		return rc;
+	}
+
+	/* Directory must have at least one block */
+	new_inode->i_size = block_size;
+
+	struct ext2_disk_dentry *entry =
+		ext2_heap_alloc(sizeof(struct ext2_disk_dentry) + args->name_len);
+
+	if (entry == NULL) {
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	entry->de_inode = new_inode->i_id;
+	entry->de_name_len = args->name_len;
+	entry->de_file_type = EXT2_FT_DIR;
+	memcpy(entry->de_name, args->path + args->name_pos, entry->de_name_len);
+
+	rc = ext2_add_direntry(parent, entry);
+	if (rc < 0) {
+		ret = rc;
+		goto out;
+	}
+
+	/* Successfully added to directory */
+	new_inode->i_links_count += 1;
+
+	/* Add "." directory entry */
+	entry->de_inode = new_inode->i_id;
+	entry->de_name_len = 1;
+	entry->de_file_type = EXT2_FT_DIR;
+	entry->de_rec_len = block_size;
+	memcpy(entry->de_name, ".", 1);
+
+	/* It has to be inserted manually */
+	rc = ext2_fetch_inode_block(new_inode, 0);
+	if (rc < 0) {
+		ret = rc;
+		goto out;
+	}
+
+	memcpy(inode_current_block_mem(new_inode), entry, sizeof(struct ext2_disk_dentry) + 1);
+
+	new_inode->i_links_count += 1;
+
+	/* Add ".." directory entry */
+	entry->de_inode = parent->i_id;
+	entry->de_name_len = 2;
+	entry->de_file_type = EXT2_FT_DIR;
+	memcpy(entry->de_name, "..", 2);
+
+	rc = ext2_add_direntry(new_inode, entry);
+	if (rc < 0) {
+		ret = rc;
+		goto out;
+	}
+
+	/* Successfully added to directory */
+	parent->i_links_count += 1;
+
+	rc = ext2_commit_inode_block(new_inode);
+	if (rc < 0) {
+		ret = rc;
+	}
+
+	rc = ext2_commit_inode_block(parent);
+	if (rc < 0) {
+		ret = rc;
+	}
+
+	/* Commit inodes after increasing link counts */
+	rc = ext2_commit_inode(new_inode);
+	if (rc < 0) {
+		ret = rc;
+	}
+
+	rc = ext2_commit_inode(parent);
+	if (rc < 0) {
+		ret = rc;
+	}
+out:
+	ext2_heap_free(entry);
+	return ret;
+}
+
+int ext2_inode_get(struct ext2_data *fs, uint32_t ino, struct ext2_inode **ret)
+{
+	struct ext2_inode *inode;
+
+	for (int i = 0; i < fs->open_inodes; ++i) {
+		inode = fs->inode_pool[i];
+
+		if (inode->i_id == ino) {
+			*ret = inode;
+			inode->i_ref++;
+			return 0;
+		}
+	}
+
+	if (fs->open_inodes >= MAX_INODES) {
+		return -ENOMEM;
+	}
+
+	inode = ext2_heap_alloc(sizeof(struct ext2_inode));
+	if (inode == NULL) {
+		return -ENOMEM;
+	}
+	memset(inode, 0, sizeof(struct ext2_inode));
+
+	if (ino != 0) {
+		int rc = ext2_fetch_inode(fs, ino, inode);
+
+		if (rc < 0) {
+			ext2_heap_free(inode);
+			return rc;
+		}
+	}
+
+	fs->inode_pool[fs->open_inodes] = inode;
+	fs->open_inodes++;
+
+	inode->i_fs = fs;
+	inode->i_ref = 1;
+	*ret = inode;
+	return 0;
+}
+
+int ext2_inode_drop(struct ext2_inode *inode)
+{
+	if (inode == NULL) {
+		return 0;
+	}
+
+	struct ext2_data *fs = inode->i_fs;
+
+	if (fs->open_inodes <= 0) {
+		LOG_WRN("All inodes should be already closed");
+		return 0;
+	}
+
+	inode->i_ref--;
+
+	/* Clean inode if that was last reference */
+	if (inode->i_ref == 0) {
+
+		/* find entry */
+		uint32_t offset = 0;
+
+		while (fs->inode_pool[offset] != inode && offset < MAX_INODES) {
+			offset++;
+		}
+
+		if (offset >= MAX_INODES) {
+			LOG_ERR("Inode structure at %p not in inode_pool", inode);
+			return -EINVAL;
+		}
+
+		ext2_inode_drop_blocks(inode);
+
+		ext2_heap_free(inode);
+
+		/* copy last open in place of freed inode */
+		uint32_t last = fs->open_inodes - 1;
+
+		fs->inode_pool[offset] = fs->inode_pool[last];
+		fs->open_inodes--;
+
+	}
+
+	return 0;
+}
+
+void ext2_inode_drop_blocks(struct ext2_inode *inode)
+{
+	for (int i = 0; i < 4; ++i) {
+		ext2_drop_block(inode->i_fs, inode->blocks[i]);
+	}
+	inode->flags &= ~INODE_FETCHED_BLOCK;
 }
