7ebb5f0e1554b21ab9b3b835d9dadc4f47ac75ad
Franciszek Zdobylak <fzdobylak@antmicro.com>
2023-02-24 10:12:05 +0100

    tests: fs: Move testfs_utils to common directory
    Move utils for file system tests into common directory to allow other
    tests to use them.
    
    Signed-off-by: Franciszek Zdobylak <fzdobylak@antmicro.com>

--- tests/subsys/fs/littlefs/src/testfs_lfs.h
+++ tests/subsys/fs/littlefs/src/testfs_lfs.h
@@ -8,7 +8,7 @@
 #define _ZEPHYR_TESTS_SUBSYS_FS_LITTLEFS_TESTFS_LFS_H_
 
 #include <zephyr/fs/fs.h>
-#include "testfs_util.h"
+#include "../../common/test_fs_util.h"
 
 #define TESTFS_MNT_POINT_SMALL "/sml"
 #define TESTFS_MNT_POINT_MEDIUM "/med"
