464f68b9e8fc25c183d58273ed3d16e73d4172c3
Daniel Leung <daniel.leung@intel.com>
2023-02-24 17:34:03 -0800

    toolchain: llvm: defer setting target triple to target.cmake
    Since kconfigs are not available when generic.cmake is parsed.
    Setting the target triple for x86 needs to be deferred to
    target.cmake as it needs to know whether CONFIG_64BIT is
    enabled. This also moves the ARM triple to target.cmake as
    triple is needed for target tools.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- cmake/toolchain/llvm/generic.cmake
+++ cmake/toolchain/llvm/generic.cmake
@@ -17,21 +17,6 @@ set(LLVM_TOOLCHAIN_PATH ${CLANG_ROOT_DIR} CACHE PATH "clang install directory")
 set(COMPILER clang)
 set(BINTOOLS llvm)
 
-if("${ARCH}" STREQUAL "arm")
-  set(triple arm-none-eabi)
-  set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nosys.specs")
-elseif("${ARCH}" STREQUAL "x86")
-  if(CONFIG_64BIT)
-    set(triple x86_64-pc-none-elf)
-  else()
-    set(triple i686-pc-none-elf)
-  endif()
-endif()
-
-set(CMAKE_C_COMPILER_TARGET   ${triple})
-set(CMAKE_ASM_COMPILER_TARGET ${triple})
-set(CMAKE_CXX_COMPILER_TARGET ${triple})
-
 set(TOOLCHAIN_HAS_NEWLIB OFF CACHE BOOL "True if toolchain supports newlib")
 
 list(APPEND TOOLCHAIN_C_FLAGS --config ${ZEPHYR_BASE}/cmake/toolchain/llvm/clang.cfg)
