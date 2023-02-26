5e9d3c75223fe9b008428b61654fd4252e30450e
Chris Friedt <cfriedt@meta.com>
2023-02-25 19:05:23 -0500

    posix: fnmatch: correct (many) compliance issues
    * add opening and closing braces around several blocks
    * convert dos line endings to unix
    * move assignments out of conditionals
    * ensure adequate spacing after control blocks
    
    Signed-off-by: Chris Friedt <cfriedt@meta.com>

--- include/zephyr/posix/fnmatch.h
+++ include/zephyr/posix/fnmatch.h
@@ -33,24 +33,24 @@
  *    @(#)fnmatch.h    8.1 (Berkeley) 6/2/93
  */
 
-#ifndef    _FNMATCH_H_
-#define    _FNMATCH_H_
+#ifndef _FNMATCH_H_
+#define _FNMATCH_H_
 
-#define    FNM_NOMATCH      1    /* Match failed. */
-#define    FNM_NOSYS        2    /* Function not implemented. */
-#define    FNM_NORES        3    /* Out of resources */
+#define FNM_NOMATCH 1 /* Match failed. */
+#define FNM_NOSYS   2 /* Function not implemented. */
+#define FNM_NORES   3 /* Out of resources */
 
-#define    FNM_NOESCAPE     0x01    /* Disable backslash escaping. */
-#define    FNM_PATHNAME     0x02    /* Slash must be matched by slash. */
-#define    FNM_PERIOD       0x04    /* Period must be matched by period. */
-#define    FNM_CASEFOLD     0x08    /* Pattern is matched case-insensitive */
-#define    FNM_LEADING_DIR  0x10    /* Ignore /<tail> after Imatch. */
+#define FNM_NOESCAPE	0x01 /* Disable backslash escaping. */
+#define FNM_PATHNAME	0x02 /* Slash must be matched by slash. */
+#define FNM_PERIOD	0x04 /* Period must be matched by period. */
+#define FNM_CASEFOLD	0x08 /* Pattern is matched case-insensitive */
+#define FNM_LEADING_DIR 0x10 /* Ignore /<tail> after Imatch. */
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int     fnmatch(const char *, const char *, int);
+int fnmatch(const char *, const char *, int);
 
 #ifdef __cplusplus
 }
