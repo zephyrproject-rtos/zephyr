f4bc619f2b1ea3786589fcb40973c28906bb16a1
Chris Friedt <cfriedt@meta.com>
2023-02-25 19:05:23 -0500

    posix: fnmatch: correct (many) compliance issues
    * add opening and closing braces around several blocks
    * convert dos line endings to unix
    * move assignments out of conditionals
    * ensure adequate spacing after control blocks
    
    Signed-off-by: Chris Friedt <cfriedt@meta.com>

--- lib/posix/fnmatch.c
+++ lib/posix/fnmatch.c
@@ -40,171 +40,222 @@
  */
 
 #include <ctype.h>
+#include <stdbool.h>
 #include <string.h>
 
 #include <zephyr/posix/fnmatch.h>
 
-#define EOS    '\0'
+#define EOS '\0'
 
 static inline int foldcase(int ch, int flags)
 {
 
-    if ((flags & FNM_CASEFOLD) != 0 && isupper(ch))
-        return tolower(ch);
-    return ch;
+	if ((flags & FNM_CASEFOLD) != 0 && isupper(ch)) {
+		return tolower(ch);
+	}
+
+	return ch;
 }
 
-#define    FOLDCASE(ch, flags)    foldcase((unsigned char)(ch), (flags))
+#define FOLDCASE(ch, flags) foldcase((unsigned char)(ch), (flags))
 
-static const char * rangematch(const char *pattern, int test, int flags)
+static const char *rangematch(const char *pattern, int test, int flags)
 {
-    int negate, ok, need;
-    char c, c2;
+	bool negate, ok, need;
+	char c, c2;
 
-    if (pattern == NULL)
-    {
-        return NULL;
-    }
+	if (pattern == NULL) {
+		return NULL;
+	}
 
-    /*
-     * A bracket expression starting with an unquoted circumflex
-     * character produces unspecified results (IEEE 1003.2-1992,
-     * 3.13.2).  This implementation treats it like '!', for
-     * consistency with the regular expression syntax.
-     * J.T. Conklin (conklin@ngai.kaleida.com)
-     */
-    if ((negate = (*pattern == '!' || *pattern == '^')) != 0)
-        ++pattern;
-    
-    need = 1;
-    for (ok = 0; (c = FOLDCASE(*pattern++, flags)) != ']' || need;) {
-        need = 0;
-        if (c == '/')
-            return (void *)-1;
-        if (c == '\\' && !(flags & FNM_NOESCAPE))
-            c = FOLDCASE(*pattern++, flags);
-        if (c == EOS)
-            return NULL;
-        if (*pattern == '-' 
-            && (c2 = FOLDCASE(*(pattern + 1), flags)) != EOS &&
-                c2 != ']') {
-            pattern += 2;
-            if (c2 == '\\' && !(flags & FNM_NOESCAPE))
-                c2 = FOLDCASE(*pattern++, flags);
-            if (c2 == EOS)
-                return NULL;
-            if (c <= test && test <= c2)
-                ok = 1;
-        } else if (c == test)
-            ok = 1;
-    }
-    return ok == negate ? NULL : pattern;
+	/*
+	 * A bracket expression starting with an unquoted circumflex
+	 * character produces unspecified results (IEEE 1003.2-1992,
+	 * 3.13.2).  This implementation treats it like '!', for
+	 * consistency with the regular expression syntax.
+	 * J.T. Conklin (conklin@ngai.kaleida.com)
+	 */
+	negate = *pattern == '!' || *pattern == '^';
+	if (negate) {
+		++pattern;
+	}
+
+	for (need = true, ok = false, c = FOLDCASE(*pattern++, flags); c != ']' || need;
+	     c = FOLDCASE(*pattern++, flags)) {
+		need = false;
+		if (c == '/') {
+			return (void *)-1;
+		}
+
+		if (c == '\\' && !(flags & FNM_NOESCAPE)) {
+			c = FOLDCASE(*pattern++, flags);
+		}
+
+		if (c == EOS) {
+			return NULL;
+		}
+
+		if (*pattern == '-') {
+			c2 = FOLDCASE(*(pattern + 1), flags);
+			if (c2 != EOS && c2 != ']') {
+				pattern += 2;
+				if (c2 == '\\' && !(flags & FNM_NOESCAPE)) {
+					c2 = FOLDCASE(*pattern++, flags);
+				}
+
+				if (c2 == EOS) {
+					return NULL;
+				}
+
+				if (c <= test && test <= c2) {
+					ok = true;
+				}
+			}
+		} else if (c == test) {
+			ok = true;
+		}
+	}
+
+	return ok == negate ? NULL : pattern;
 }
-
 
 static int fnmatchx(const char *pattern, const char *string, int flags, size_t recursion)
 {
-    const char *stringstart, *r;
-    char c, test;
+	const char *stringstart, *r;
+	char c, test;
 
-    if ((pattern == NULL) || (string == NULL))
-    {
-        return FNM_NOMATCH;
-    }
+	if (pattern == NULL || string == NULL) {
+		return FNM_NOMATCH;
+	}
 
-    if (recursion-- == 0)
-        return FNM_NORES;
+	if (recursion-- == 0) {
+		return FNM_NORES;
+	}
 
-    for (stringstart = string;;) {
-        switch (c = FOLDCASE(*pattern++, flags)) {
-        case EOS:
-            if ((flags & FNM_LEADING_DIR) && *string == '/')
-                return 0;
-            return *string == EOS ? 0 : FNM_NOMATCH;
-        case '?':
-            if (*string == EOS)
-                return FNM_NOMATCH;
-            if (*string == '/' && (flags & FNM_PATHNAME))
-                return FNM_NOMATCH;
-            if (*string == '.' && (flags & FNM_PERIOD) &&
-                (string == stringstart ||
-                ((flags & FNM_PATHNAME) && *(string - 1) == '/')))
-                return FNM_NOMATCH;
-            ++string;
-            break;
-        case '*':
-            c = FOLDCASE(*pattern, flags);
-            /* Collapse multiple stars. */
-            while (c == '*')
-                c = FOLDCASE(*++pattern, flags);
+	for (stringstart = string;;) {
+		c = FOLDCASE(*pattern++, flags);
+		switch (c) {
+		case EOS:
+			if ((flags & FNM_LEADING_DIR) && *string == '/') {
+				return 0;
+			}
 
-            if (*string == '.' && (flags & FNM_PERIOD) &&
-                (string == stringstart ||
-                ((flags & FNM_PATHNAME) && *(string - 1) == '/')))
-                return FNM_NOMATCH;
+			return *string == EOS ? 0 : FNM_NOMATCH;
+		case '?':
+			if (*string == EOS) {
+				return FNM_NOMATCH;
+			}
 
-            /* Optimize for pattern with * at end or before /. */
-            if (c == EOS) {
-                if (flags & FNM_PATHNAME)
-                    return (flags & FNM_LEADING_DIR) ||
-                        strchr(string, '/') == NULL ?
-                        0 : FNM_NOMATCH;
-                else
-                    return 0;
-            } else if (c == '/' && flags & FNM_PATHNAME) {
-                if ((string = strchr(string, '/')) == NULL)
-                    return FNM_NOMATCH;
-                break;
-            }
+			if (*string == '/' && (flags & FNM_PATHNAME)) {
+				return FNM_NOMATCH;
+			}
 
-            /* General case, use recursion. */
-            while ((test = FOLDCASE(*string, flags)) != EOS) {
-                int e;
-                switch ((e = fnmatchx(pattern, string,
-                    flags & ~FNM_PERIOD, recursion))) {
-                case FNM_NOMATCH:
-                    break;
-                default:
-                    return e;
-                }
-                if (test == '/' && flags & FNM_PATHNAME)
-                    break;
-                ++string;
-            }
-            return FNM_NOMATCH;
-        case '[':
-            if (*string == EOS)
-                return FNM_NOMATCH;
-            if (*string == '/' && flags & FNM_PATHNAME)
-                return FNM_NOMATCH;
-            if ((r = rangematch(pattern,
-                FOLDCASE(*string, flags), flags)) == NULL)
-                return FNM_NOMATCH;
-            if (r == (void *)-1) {
-                if (*string != '[')
-                    return FNM_NOMATCH;
-            } else
-                pattern = r;
-            ++string;
-            break;
-        case '\\':
-            if (!(flags & FNM_NOESCAPE)) {
-                if ((c = FOLDCASE(*pattern++, flags)) == EOS) {
-                    c = '\0';
-                    --pattern;
-                }
-            }
-            /* FALLTHROUGH */
-        default:
-            if (c != FOLDCASE(*string++, flags))
-                return FNM_NOMATCH;
-            break;
-        }
-    }
-    /* NOTREACHED */
+			if (*string == '.' && (flags & FNM_PERIOD) &&
+			    (string == stringstart ||
+			     ((flags & FNM_PATHNAME) && *(string - 1) == '/'))) {
+				return FNM_NOMATCH;
+			}
+
+			++string;
+			break;
+		case '*':
+			c = FOLDCASE(*pattern, flags);
+			/* Collapse multiple stars. */
+			while (c == '*') {
+				c = FOLDCASE(*++pattern, flags);
+			}
+
+			if (*string == '.' && (flags & FNM_PERIOD) &&
+			    (string == stringstart ||
+			     ((flags & FNM_PATHNAME) && *(string - 1) == '/'))) {
+				return FNM_NOMATCH;
+			}
+
+			/* Optimize for pattern with * at end or before /. */
+			if (c == EOS) {
+				if (flags & FNM_PATHNAME) {
+					return (flags & FNM_LEADING_DIR) ||
+							       strchr(string, '/') == NULL
+						       ? 0
+						       : FNM_NOMATCH;
+				} else {
+					return 0;
+				}
+			} else if (c == '/' && flags & FNM_PATHNAME) {
+				string = strchr(string, '/');
+				if (string == NULL) {
+					return FNM_NOMATCH;
+				}
+
+				break;
+			}
+
+			/* General case, use recursion. */
+			do {
+				test = FOLDCASE(*string, flags);
+				if (test == EOS) {
+					break;
+				}
+
+				int e = fnmatchx(pattern, string, flags & ~FNM_PERIOD, recursion);
+
+				if (e != FNM_NOMATCH) {
+					return e;
+				}
+
+				if (test == '/' && flags & FNM_PATHNAME) {
+					break;
+				}
+
+				++string;
+			} while (true);
+
+			return FNM_NOMATCH;
+		case '[':
+			if (*string == EOS) {
+				return FNM_NOMATCH;
+			}
+
+			if (*string == '/' && flags & FNM_PATHNAME) {
+				return FNM_NOMATCH;
+			}
+
+			r = rangematch(pattern, FOLDCASE(*string, flags), flags);
+			if (r == NULL) {
+				return FNM_NOMATCH;
+			}
+
+			if (r == (void *)-1) {
+				if (*string != '[') {
+					return FNM_NOMATCH;
+				}
+			} else {
+				pattern = r;
+			}
+
+			++string;
+			break;
+		case '\\':
+			if (!(flags & FNM_NOESCAPE)) {
+				c = FOLDCASE(*pattern++, flags);
+				if (c == EOS) {
+					c = '\0';
+					--pattern;
+				}
+			}
+			/* FALLTHROUGH */
+		default:
+			if (c != FOLDCASE(*string++, flags)) {
+				return FNM_NOMATCH;
+			}
+
+			break;
+		}
+	}
+	/* NOTREACHED */
 }
 
 int fnmatch(const char *pattern, const char *string, int flags)
 {
-    return fnmatchx(pattern, string, flags, 64);
+	return fnmatchx(pattern, string, flags, 64);
 }
