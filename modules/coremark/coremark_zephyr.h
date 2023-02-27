3176f9638a146d7d3c1f48b1ed0bc061d04ef8a2
Volodymyr Bondarchuk <volodymyr.bondarchuk@nordicsemi.no>
2023-02-27 14:25:47 +0100

    modules: coremark: add integration and porting layer
    Coremark porting layer and integration to zephyr build system added.
    
    Signed-off-by: Volodymyr Bondarchuk <volodymyr.bondarchuk@nordicsemi.no>

---
+++ modules/coremark/coremark_zephyr.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CORE_PORTME_ZEPHYR_H
+#define CORE_PORTME_ZEPHYR_H
+
+void coremark_run(void);
+
+#endif
