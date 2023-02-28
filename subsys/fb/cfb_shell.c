5d9db9dc65ba55c589e7b3718e03a8d908609f69
Simon Frank <simon.frank@lohmega.com>
2023-02-28 10:02:54 +0100

    fb: cfb: cfb_shell command to set font kerning
    Using `struct shell *shell` would be more consistent but triggers CI
    check on MISRA rule 5.7 "A Tag name shall be a unique identifier"
    
    Signed-off-by: Simon Frank <simon.frank@lohmega.com>

--- subsys/fb/cfb_shell.c
+++ subsys/fb/cfb_shell.c
@@ -235,6 +235,33 @@ static int cmd_set_font(const struct shell *shell, size_t argc, char *argv[])
 	return err;
 }
 
+static int cmd_set_kerning(const struct shell *sh, size_t argc, char *argv[])
+{
+	int err;
+	char *ep = NULL;
+	long kerning;
+
+	if (!dev) {
+		shell_error(sh, HELP_INIT);
+		return -ENODEV;
+	}
+
+	errno = 0;
+	kerning = strtol(argv[1], &ep, 10);
+	if (errno || ep == argv[1]) {
+		shell_error(sh, HELP_INIT);
+		return -EINVAL;
+	}
+
+	err = cfb_set_kerning(dev, kerning);
+	if (err) {
+		shell_error(sh, "Failed to set kerning err=%d", err);
+		return err;
+	}
+
+	return err;
+}
+
 static int cmd_invert(const struct shell *shell, size_t argc, char *argv[])
 {
 	int err;
@@ -468,6 +495,7 @@ SHELL_STATIC_SUBCMD_SET_CREATE(cfb_cmds,
 		  "<all, height, width, ppt, rows, cols>", NULL),
 	SHELL_CMD_ARG(get_fonts, NULL, HELP_NONE, cmd_get_fonts, 1, 0),
 	SHELL_CMD_ARG(set_font, NULL, "<idx>", cmd_set_font, 2, 0),
+	SHELL_CMD_ARG(set_kerning, NULL, "<kerning>", cmd_set_kerning, 2, 0),
 	SHELL_CMD_ARG(invert, NULL, HELP_NONE, cmd_invert, 1, 0),
 	SHELL_CMD_ARG(print, NULL, HELP_PRINT, cmd_print, 4, 0),
 	SHELL_CMD(scroll, &sub_cmd_scroll, "scroll a text in vertical or "
