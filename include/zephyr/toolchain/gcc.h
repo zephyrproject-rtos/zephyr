588f50c8a79ccfea83084fe57f4218173bf2e7f2
Andy Ross <andyross@google.com>
2023-03-01 07:53:36 -0800

    toolchain/gcc: Limit use of _Static_assert
    This C11 feature doesn't work when building C++ code, but the C++
    version is based on C++11's static_assert(), which we don't have in
    -std=c++98 either.
    
    Signed-off-by: Andy Ross <andyross@google.com>

--- include/zephyr/toolchain/gcc.h
+++ include/zephyr/toolchain/gcc.h
@@ -70,11 +70,14 @@
 #define BUILD_ASSERT(EXPR, MSG...) static_assert(EXPR, "" MSG)
 
 /*
- * GCC 4.6 and higher have the C11 _Static_assert built in, and its
+ * GCC 4.6 and higher have the C11 _Static_assert built in and its
  * output is easier to understand than the common BUILD_ASSERT macros.
+ * Don't use this in C++98 mode though (which we can hit, as
+ * static_assert() is not available)
  */
-#elif (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)) || \
-	(__STDC_VERSION__) >= 201100
+#elif !defined(__cplusplus) && \
+	((__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6)) ||	\
+	 (__STDC_VERSION__) >= 201100)
 #define BUILD_ASSERT(EXPR, MSG...) _Static_assert(EXPR, "" MSG)
 #else
 #define BUILD_ASSERT(EXPR, MSG...)
