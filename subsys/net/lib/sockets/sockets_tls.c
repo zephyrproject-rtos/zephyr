aa68987ed6dae471152cd407d46e0ce4b388a864
Vidar Lillebø <vidar.lillebo@nordicsemi.no>
2023-03-02 15:14:42 +0100

    mbedtls: Remove dependency on MBEDTLS_BUILTIN for MBEDTLS_DEBUG
    Allows using MBEDTLS_DEBUG functionality when not using MBEDTLS_BUILTIN.
    
    Signed-off-by: Vidar Lillebø <vidar.lillebo@nordicsemi.no>

--- subsys/net/lib/sockets/sockets_tls.c
+++ subsys/net/lib/sockets/sockets_tls.c
@@ -56,7 +56,7 @@ LOG_MODULE_REGISTER(net_sock_tls, CONFIG_NET_SOCKETS_LOG_LEVEL);
 #include "sockets_internal.h"
 #include "tls_internal.h"
 
-#if defined(CONFIG_MBEDTLS_BUILTIN)
+#if defined(CONFIG_MBEDTLS_DEBUG)
 #include <zephyr_mbedtls_priv.h>
 #endif
 
