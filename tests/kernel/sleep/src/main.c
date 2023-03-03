346b17d716ad5b502f08b422f3909a8ebed61f48
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-03-03 16:34:11 +0100

    util: Replace all POSIX arch busy_waits with Z_SPIN_DELAY
    A new Z_SPIN_DELAY() macro has been added which
    can be used to reduce a bit the amount of noise
    due to the POSIX arch need to break busy loops with
    k_busy_wait().
    Use it.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- tests/kernel/sleep/src/main.c
+++ tests/kernel/sleep/src/main.c
@@ -83,9 +83,7 @@ static void align_to_tick_boundary(void)
 	tick = k_uptime_get_32();
 	while (k_uptime_get_32() == tick) {
 		/* Busy wait to align to tick boundary */
-#if defined(CONFIG_ARCH_POSIX)
-		k_busy_wait(50);
-#endif
+		Z_SPIN_DELAY(50);
 	}
 
 }
