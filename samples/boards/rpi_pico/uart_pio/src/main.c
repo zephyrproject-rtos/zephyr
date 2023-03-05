c431c169056654913c9a108e48612f6d029532f5
Yonatan Schachter <yonatan.schachter@gmail.com>
2023-03-05 23:57:49 +0200

    samples: boards: rpi_pico: Added uart_pio sample
    Added a sample for the rpi_pico board showing how to use the UART
    over PIO driver.
    
    Signed-off-by: Yonatan Schachter <yonatan.schachter@gmail.com>

---
+++ samples/boards/rpi_pico/uart_pio/src/main.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2023 Yonatan Schachter
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/uart.h>
+
+void main(void)
+{
+	char data;
+	const struct device *uart0 = DEVICE_DT_GET(DT_NODELABEL(pio1_uart0));
+	const struct device *uart1 = DEVICE_DT_GET(DT_NODELABEL(pio1_uart1));
+
+	if (!device_is_ready(uart0)) {
+		return;
+	}
+
+	if (!device_is_ready(uart1)) {
+		return;
+	}
+
+	while (1) {
+		if (!uart_poll_in(uart0, &data)) {
+			uart_poll_out(uart0, data);
+		}
+
+		if (!uart_poll_in(uart1, &data)) {
+			uart_poll_out(uart1, data);
+		}
+	}
+}
