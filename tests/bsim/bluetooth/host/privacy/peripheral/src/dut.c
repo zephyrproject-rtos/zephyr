1c6e1036f6911d992551a8753a5e11dd68e6e4d3
Nithin Ramesh Myliattil <niym@demant.com>
2023-03-06 09:01:47 +0100

    subsys/bluetooth/host: Make rpa same for adv sets with same id
    Add check to see if RPA is already generated for adv sets
    with same id. If generated use the same address for all adv sets
    with same id else create new RPA.
    
    Signed-off-by: Nithin Ramesh Myliattil <niym@demant.com>

--- tests/bsim/bluetooth/host/privacy/peripheral/src/dut.c
+++ tests/bsim/bluetooth/host/privacy/peripheral/src/dut.c
@@ -14,18 +14,60 @@
 #include <zephyr/bluetooth/conn.h>
 #include <zephyr/toolchain/gcc.h>
 
+#include "common/bt_str.h"
+
+#define ADV_SET_INDEX_ONE   0x00
+#define ADV_SET_INDEX_TWO   0x01
+#define ADV_SET_INDEX_THREE 0x02
+
+static struct bt_le_ext_adv *adv_set[CONFIG_BT_EXT_ADV_MAX_ADV_SET];
+
+static const struct bt_data ad_id[] = {
+	BT_DATA_BYTES(BT_DATA_MANUFACTURER_DATA, ADV_SET_INDEX_ONE),
+	BT_DATA_BYTES(BT_DATA_MANUFACTURER_DATA, ADV_SET_INDEX_TWO),
+	BT_DATA_BYTES(BT_DATA_MANUFACTURER_DATA, ADV_SET_INDEX_THREE),
+};
+
 bool cb_rpa_expired(struct bt_le_ext_adv *adv)
 {
 	/* Return true to rotate the current RPA */
+	int err;
+	struct bt_le_ext_adv_info info;
+
+	err = bt_le_ext_adv_get_info(adv, &info);
+	if (err) {
+		return false;
+	}
+	printk("advertiser[%d] RPA %s\n", info.id, bt_addr_le_str(info.addr));
 	return true;
+}
+
+static void create_adv(struct bt_le_ext_adv **adv, int id)
+{
+	int err;
+	struct bt_le_adv_param params;
+	static struct bt_le_ext_adv_cb cb_adv;
+
+	cb_adv.rpa_expired = cb_rpa_expired;
+	memset(&params, 0, sizeof(struct bt_le_adv_param));
+
+	params.options |= BT_LE_ADV_OPT_EXT_ADV;
+	params.id = id;
+	params.sid = 0;
+	params.interval_min = BT_GAP_ADV_FAST_INT_MIN_1;
+	params.interval_max = BT_GAP_ADV_FAST_INT_MAX_1;
+
+	err = bt_le_ext_adv_create(&params, &cb_adv, adv);
+	if (err) {
+		FAIL("Failed to create advertiser (%d)\n", err);
+	}
 }
 
 void start_advertising(void)
 {
 	int err;
-	static struct bt_le_ext_adv_cb cb_adv;
-	struct bt_le_ext_adv_start_param start_params;
-	struct bt_le_ext_adv *adv;
+	int id_a;
+	int id_b;
 
 	/* Enable bluetooth */
 	err = bt_enable(NULL);
@@ -33,19 +75,36 @@ void start_advertising(void)
 		FAIL("Failed to enable bluetooth (err %d\n)", err);
 	}
 
-	/* Start non-connectable extended advertising with private address */
-	start_params.timeout = 0;
-	start_params.num_events = 0;
-	cb_adv.rpa_expired = cb_rpa_expired;
-
-	err = bt_le_ext_adv_create(BT_LE_EXT_ADV_CONN_NAME, &cb_adv, &adv);
-	if (err) {
-		FAIL("Failed to create advertising set (err %d)\n", err);
+	id_a = bt_id_create(NULL, NULL);
+	if (id_a < 0) {
+		FAIL("bt_id_create id_a failed (err %d)\n", id_a);
 	}
 
-	err = bt_le_ext_adv_start(adv, &start_params);
-	if (err) {
-		FAIL("Failed to enable periodic advertising (err %d)\n", err);
+	id_b = bt_id_create(NULL, NULL);
+	if (id_b < 0) {
+		FAIL("bt_id_create id_b failed (err %d)\n", id_b);
+	}
+
+	for (int i = 0; i < CONFIG_BT_EXT_ADV_MAX_ADV_SET; i++) {
+
+		if (i != ADV_SET_INDEX_THREE) {
+			/* Create advertising set 1 and 2 with same id */
+			create_adv(&adv_set[i], id_a);
+		} else {
+			/* Create advertising set 3 with different id */
+			create_adv(&adv_set[i], id_b);
+		}
+
+		/* Set extended advertising data */
+		err = bt_le_ext_adv_set_data(adv_set[i], &ad_id[i], 1, NULL, 0);
+		if (err) {
+			FAIL("Failed to set advertising data for set %d (err %d)\n", i, err);
+		}
+
+		err = bt_le_ext_adv_start(adv_set[i], BT_LE_EXT_ADV_START_DEFAULT);
+		if (err) {
+			FAIL("Failed to start advertising (err %d)\n", err);
+		}
 	}
 }
 
