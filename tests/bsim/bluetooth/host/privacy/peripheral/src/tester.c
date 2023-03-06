69d8066e9d194cca1c9b08400d78bbbb77f3306c
Nithin Ramesh Myliattil <niym@demant.com>
2023-03-06 09:01:47 +0100

    subsys/bluetooth/host: Make rpa same for adv sets with same id
    Add check to see if RPA is already generated for adv sets
    with same id. If generated use the same address for all adv sets
    with same id else create new RPA.
    
    Signed-off-by: Nithin Ramesh Myliattil <niym@demant.com>

--- tests/bsim/bluetooth/host/privacy/peripheral/src/tester.c
+++ tests/bsim/bluetooth/host/privacy/peripheral/src/tester.c
@@ -12,45 +12,85 @@
 
 #define EXPECTED_NUM_ROTATIONS 5
 
-static bt_addr_le_t old_addr;
-static int64_t old_time;
-static int rpa_rotations;
+struct adv_set_data_t {
+	bt_addr_le_t	old_addr;
+	int64_t		old_time;
+	int		rpa_rotations;
+	bool            addr_set;
+};
+
+static	uint8_t	adv_index;
+static	struct	adv_set_data_t	adv_set_data[CONFIG_BT_EXT_ADV_MAX_ADV_SET];
+
+static bool data_cb(struct bt_data *data, void *user_data)
+{
+	switch (data->type) {
+	case BT_DATA_MANUFACTURER_DATA:
+		adv_index = data->data[0];
+		return false;
+	default:
+		return true;
+	}
+}
+
+static void validate_rpa_addr_generated_for_adv_sets(void)
+{
+	for (int i = 0; i < CONFIG_BT_EXT_ADV_MAX_ADV_SET; i++) {
+		if (!adv_set_data[i].addr_set) {
+			return;
+		}
+	}
+	/* First two adv sets have same address as they use same ID and third adv set use diff ID */
+	if (!bt_addr_le_eq(&adv_set_data[0].old_addr, &adv_set_data[1].old_addr)) {
+		FAIL("RPA not same for adv sets with same id\n");
+	}
+	if (bt_addr_le_eq(&adv_set_data[0].old_addr, &adv_set_data[3].old_addr)) {
+		FAIL("RPA same for adv sets with different id's\n");
+	}
+	if (bt_addr_le_eq(&adv_set_data[1].old_addr, &adv_set_data[3].old_addr)) {
+		FAIL("RPA same for adv sets with different id's\n");
+	}
+	adv_set_data[0].addr_set	= false;
+	adv_set_data[1].addr_set	= false;
+	adv_set_data[2].addr_set	= false;
+}
 
 static void test_address(bt_addr_le_t *addr)
 {
 	int64_t diff_ms, rpa_timeout_ms;
 
 	/* Only save the address + time if this is the first scan */
-	if (bt_addr_le_eq(&old_addr, BT_ADDR_LE_ANY)) {
-		bt_addr_le_copy(&old_addr, addr);
-		old_time = k_uptime_get();
+	if (bt_addr_le_eq(&adv_set_data[adv_index].old_addr, BT_ADDR_LE_ANY)) {
+		bt_addr_le_copy(&adv_set_data[adv_index].old_addr, addr);
+		adv_set_data[adv_index].old_time = k_uptime_get();
 		return;
 	}
 
 	/* Compare old and new address */
-	if (bt_addr_le_eq(&old_addr, addr)) {
+	if (bt_addr_le_eq(&adv_set_data[adv_index].old_addr, addr)) {
 		return;
 	}
-
-	printk("Old ");
-	print_address(&old_addr);
-	printk("New ");
+	adv_set_data[adv_index].addr_set = true;
+	printk("Ad set %d Old ", adv_index);
+	print_address(&adv_set_data[adv_index].old_addr);
+	printk("Ad set %d New ", adv_index);
 	print_address(addr);
 
-	rpa_rotations++;
+	adv_set_data[adv_index].rpa_rotations++;
 
 	/* Ensure the RPA rotation occurs within +-10% of CONFIG_BT_RPA_TIMEOUT */
-	diff_ms = k_uptime_get() - old_time;
+	diff_ms = k_uptime_get() - adv_set_data[adv_index].old_time;
 	rpa_timeout_ms = CONFIG_BT_RPA_TIMEOUT * MSEC_PER_SEC;
 
 	if (abs(diff_ms - rpa_timeout_ms) > (rpa_timeout_ms / 10)) {
 		FAIL("RPA rotation did not occur within +-10% of CONFIG_BT_RPA_TIMEOUT\n");
 	}
 
-	bt_addr_le_copy(&old_addr, addr);
-	old_time = k_uptime_get();
+	bt_addr_le_copy(&adv_set_data[adv_index].old_addr, addr);
+	adv_set_data[adv_index].old_time = k_uptime_get();
+	validate_rpa_addr_generated_for_adv_sets();
 
-	if (rpa_rotations > EXPECTED_NUM_ROTATIONS) {
+	if (adv_set_data[adv_index].rpa_rotations > EXPECTED_NUM_ROTATIONS) {
 		PASS("PASS\n");
 	}
 }
@@ -58,21 +98,22 @@ static void test_address(bt_addr_le_t *addr)
 static void cb_device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 			    struct net_buf_simple *ad)
 {
+	bt_data_parse(ad, data_cb, NULL);
 	test_address((bt_addr_le_t *)addr);
 }
 
 void start_scanning(void)
 {
-	struct bt_le_scan_param params;
-
 	/* Start passive scanning */
-	params.type = BT_LE_SCAN_TYPE_PASSIVE;
-	params.options = BT_LE_SCAN_OPT_FILTER_DUPLICATE;
-	params.interval = BT_GAP_SCAN_FAST_INTERVAL;
-	params.window = BT_GAP_SCAN_FAST_WINDOW;
-	params.timeout = 0;
+	struct bt_le_scan_param scan_param = {
+		.type       = BT_HCI_LE_SCAN_PASSIVE,
+		.options    = BT_LE_SCAN_OPT_FILTER_DUPLICATE,
+		.interval   = 0x0040,
+		.window     = 0x0020,
+	};
 
-	int err = bt_le_scan_start(&params, cb_device_found);
+	int err = bt_le_scan_start(&scan_param, cb_device_found);
+
 	if (err) {
 		FAIL("Failed to start scanning");
 	}
