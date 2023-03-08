b1f4a3864c2f49af496d96f6d5434bdb6686c535
Anders Storrø <anders.storro@nordicsemi.no>
2023-03-08 12:17:09 +0100

    [nrf noup] Bluetooth: Mesh: Fix adv randomness bug
    Fixes issue where randomness can be removed for advertising sets that
    have to handle other adv types than the BT_MESH_FRIEND_ADV tag type.
    
    Signed-off-by: Anders Storrø <anders.storro@nordicsemi.no>
    Signed-off-by: Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>
    (cherry picked from commit 5b9a6457f7a3fdeee728da6cf8cf80fbb9e2947f)

--- subsys/bluetooth/mesh/adv_ext.c
+++ subsys/bluetooth/mesh/adv_ext.c
@@ -567,8 +567,10 @@ int bt_mesh_adv_enable(void)
 			return err;
 		}
 
-		if (IS_ENABLED(CONFIG_BT_LL_SOFTDEVICE) && adv->tag & BT_MESH_FRIEND_ADV) {
-			err = set_adv_randomness(adv->instance->handle, 0);
+		if (IS_ENABLED(CONFIG_BT_LL_SOFTDEVICE) &&
+		    IS_ENABLED(CONFIG_BT_MESH_ADV_EXT_FRIEND_SEPARATE) &&
+		    advs[i].tags == BT_MESH_ADV_TAG_BIT_FRIEND) {
+			err = set_adv_randomness(advs[i].instance->handle, 0);
 			if (err) {
 				LOG_ERR("Failed to set zero randomness: %d", err);
 			}
