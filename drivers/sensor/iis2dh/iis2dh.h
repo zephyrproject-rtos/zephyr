bf5c39a40a22e7bd7b96ecb131eeff9299bed97d
Maureen Helm <maureen.helm@intel.com>
2023-03-08 15:49:27 -0600

    drivers: sensor: iis2dh: Store sensor trigger as a pointer
    Fixes the iis2dh sensor driver to store the user-supplied sensor
    trigger as a pointer rather than a copy. This enables the trigger
    handler to use CONTAINER_OF to retrieve a context pointer when the
    trigger is embedded in a larger struct.
    
    Signed-off-by: Maureen Helm <maureen.helm@intel.com>

--- drivers/sensor/iis2dh/iis2dh.h
+++ drivers/sensor/iis2dh/iis2dh.h
@@ -65,6 +65,7 @@ struct iis2dh_data {
 	const struct device *dev;
 	struct gpio_callback gpio_cb;
 	sensor_trigger_handler_t drdy_handler;
+	const struct sensor_trigger *drdy_trig;
 #if defined(CONFIG_IIS2DH_TRIGGER_OWN_THREAD)
 	K_KERNEL_STACK_MEMBER(thread_stack, CONFIG_IIS2DH_THREAD_STACK_SIZE);
 	struct k_thread thread;
