ed8a4fb6a8b643568063b8470b4b010b94550858
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-03-09 13:23:56 +0100

    tests/bluetooth/bsim: Fail CI on build failure
    After 47ceb5cf81a, compile.sh stopped failing on build
    failures as failing jobs in the background would neither stop
    the script nor have their status propagated.
    Fix it.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- tests/bluetooth/bsim/host/compile.sh
+++ tests/bluetooth/bsim/host/compile.sh
@@ -20,38 +20,38 @@ mkdir -p ${WORK_DIR}
 
 source ${ZEPHYR_BASE}/tests/bluetooth/bsim/compile.source
 
-app=tests/bluetooth/bsim/host/adv/resume compile &
-app=tests/bluetooth/bsim/host/adv/resume conf_file=prj_2.conf compile &
-app=tests/bluetooth/bsim/host/adv/chain compile &
-app=tests/bluetooth/bsim/host/adv/periodic compile &
-app=tests/bluetooth/bsim/host/adv/periodic conf_file=prj_long_data.conf compile &
+app=tests/bluetooth/bsim/host/adv/resume compile
+app=tests/bluetooth/bsim/host/adv/resume conf_file=prj_2.conf compile
+app=tests/bluetooth/bsim/host/adv/chain compile
+app=tests/bluetooth/bsim/host/adv/periodic compile
+app=tests/bluetooth/bsim/host/adv/periodic conf_file=prj_long_data.conf compile
 
-app=tests/bluetooth/bsim/host/att/eatt conf_file=prj_collision.conf compile &
-app=tests/bluetooth/bsim/host/att/eatt conf_file=prj_multiple_conn.conf compile &
-app=tests/bluetooth/bsim/host/att/eatt conf_file=prj_autoconnect.conf compile &
-app=tests/bluetooth/bsim/host/att/eatt_notif conf_file=prj.conf compile &
-app=tests/bluetooth/bsim/host/att/mtu_update compile &
+app=tests/bluetooth/bsim/host/att/eatt conf_file=prj_collision.conf compile
+app=tests/bluetooth/bsim/host/att/eatt conf_file=prj_multiple_conn.conf compile
+app=tests/bluetooth/bsim/host/att/eatt conf_file=prj_autoconnect.conf compile
+app=tests/bluetooth/bsim/host/att/eatt_notif conf_file=prj.conf compile
+app=tests/bluetooth/bsim/host/att/mtu_update compile
 
-app=tests/bluetooth/bsim/host/gatt/caching compile &
-app=tests/bluetooth/bsim/host/gatt/general compile &
-app=tests/bluetooth/bsim/host/gatt/notify compile &
-app=tests/bluetooth/bsim/host/gatt/notify_multiple compile &
-app=tests/bluetooth/bsim/host/gatt/settings compile &
-app=tests/bluetooth/bsim/host/gatt/settings conf_file=prj_2.conf compile &
-app=tests/bluetooth/bsim/host/gatt/write compile &
+app=tests/bluetooth/bsim/host/gatt/caching compile
+app=tests/bluetooth/bsim/host/gatt/general compile
+app=tests/bluetooth/bsim/host/gatt/notify compile
+app=tests/bluetooth/bsim/host/gatt/notify_multiple compile
+app=tests/bluetooth/bsim/host/gatt/settings compile
+app=tests/bluetooth/bsim/host/gatt/settings conf_file=prj_2.conf compile
+app=tests/bluetooth/bsim/host/gatt/write compile
 
-app=tests/bluetooth/bsim/host/l2cap/general compile &
-app=tests/bluetooth/bsim/host/l2cap/userdata compile &
-app=tests/bluetooth/bsim/host/l2cap/stress compile &
+app=tests/bluetooth/bsim/host/l2cap/general compile
+app=tests/bluetooth/bsim/host/l2cap/userdata compile
+app=tests/bluetooth/bsim/host/l2cap/stress compile
 
-app=tests/bluetooth/bsim/host/misc/disable compile &
-app=tests/bluetooth/bsim/host/misc/multiple_id compile &
+app=tests/bluetooth/bsim/host/misc/disable compile
+app=tests/bluetooth/bsim/host/misc/multiple_id compile
 
-app=tests/bluetooth/bsim/host/privacy/central compile &
-app=tests/bluetooth/bsim/host/privacy/peripheral compile &
-app=tests/bluetooth/bsim/host/privacy/device compile &
+app=tests/bluetooth/bsim/host/privacy/central compile
+app=tests/bluetooth/bsim/host/privacy/peripheral compile
+app=tests/bluetooth/bsim/host/privacy/device compile
 
-app=tests/bluetooth/bsim/host/security/bond_overwrite_allowed compile &
-app=tests/bluetooth/bsim/host/security/bond_overwrite_denied compile &
+app=tests/bluetooth/bsim/host/security/bond_overwrite_allowed compile
+app=tests/bluetooth/bsim/host/security/bond_overwrite_denied compile
 
-wait
+wait_for_background_jobs
--- tests/bluetooth/bsim/mesh/compile.sh
+++ tests/bluetooth/bsim/mesh/compile.sh
@@ -20,16 +20,16 @@ mkdir -p ${WORK_DIR}
 
 source ${ZEPHYR_BASE}/tests/bluetooth/bsim/compile.source
 
-app=tests/bluetooth/bsim/mesh compile &
-app=tests/bluetooth/bsim/mesh conf_overlay=overlay_low_lat.conf compile &
-app=tests/bluetooth/bsim/mesh conf_overlay=overlay_pst.conf compile &
-app=tests/bluetooth/bsim/mesh conf_overlay=overlay_gatt.conf compile &
-app=tests/bluetooth/bsim/mesh conf_file=prj_mesh1d1.conf compile &
+app=tests/bluetooth/bsim/mesh compile
+app=tests/bluetooth/bsim/mesh conf_overlay=overlay_low_lat.conf compile
+app=tests/bluetooth/bsim/mesh conf_overlay=overlay_pst.conf compile
+app=tests/bluetooth/bsim/mesh conf_overlay=overlay_gatt.conf compile
+app=tests/bluetooth/bsim/mesh conf_file=prj_mesh1d1.conf compile
 app=tests/bluetooth/bsim/mesh \
-  conf_file=prj_mesh1d1.conf conf_overlay=overlay_pst.conf compile &
+  conf_file=prj_mesh1d1.conf conf_overlay=overlay_pst.conf compile
 app=tests/bluetooth/bsim/mesh \
-  conf_file=prj_mesh1d1.conf conf_overlay=overlay_gatt.conf compile &
+  conf_file=prj_mesh1d1.conf conf_overlay=overlay_gatt.conf compile
 app=tests/bluetooth/bsim/mesh \
-  conf_file=prj_mesh1d1.conf conf_overlay=overlay_low_lat.conf compile &
+  conf_file=prj_mesh1d1.conf conf_overlay=overlay_low_lat.conf compile
 
-wait
+wait_for_background_jobs
