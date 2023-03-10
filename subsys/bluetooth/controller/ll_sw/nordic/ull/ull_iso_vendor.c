b525db221c3430c66b882393965de4ee4388fb02
Wolfgang Puffitsch <wopu@demant.com>
2023-03-10 12:51:28 +0100

    Bluetooth: controller: Avoid the use of weak functions for ISO data path
    Avoid the use of weak functions and call the respective functions only
    if vendor-specific data path is supported. The weak dummy functions
    will not implement the desired behavior anyway. This change makes it
    explicit that these functions need to be implemented by the vendor.
    
    Signed-off-by: Wolfgang Puffitsch <wopu@demant.com>

--- subsys/bluetooth/controller/ll_sw/nordic/ull/ull_iso_vendor.c
+++ subsys/bluetooth/controller/ll_sw/nordic/ull/ull_iso_vendor.c
@@ -8,6 +8,8 @@
 #include <stdbool.h>
 
 #include <zephyr/bluetooth/hci.h>
+#include "isoal.h"
+#include "ull_iso_types.h"
 
 /* FIXME: Implement vendor specific data path configuration */
 static bool dummy;
@@ -18,6 +20,23 @@ bool ll_data_path_configured(uint8_t data_path_dir, uint8_t data_path_id)
 	ARG_UNUSED(data_path_id);
 
 	return dummy;
+}
+
+bool ll_data_path_source_create(uint16_t handle,
+				struct ll_iso_datapath *datapath,
+				isoal_source_pdu_alloc_cb *pdu_alloc,
+				isoal_source_pdu_write_cb *pdu_write,
+				isoal_source_pdu_emit_cb *pdu_emit,
+				isoal_source_pdu_release_cb *pdu_release)
+{
+	ARG_UNUSED(handle);
+	ARG_UNUSED(datapath);
+	ARG_UNUSED(pdu_alloc);
+	ARG_UNUSED(pdu_write);
+	ARG_UNUSED(pdu_emit);
+	ARG_UNUSED(pdu_release);
+
+	return false;
 }
 
 uint8_t ll_configure_data_path(uint8_t data_path_dir, uint8_t data_path_id,
