b8447521fd1648f4f396c540f18d7d9a8eea16a6
Franciszek Zdobylak <fzdobylak@antmicro.com>
2023-03-13 08:44:50 +0100

    tests: fs: ext2: fix after changes in mkfs
    Remove 'lost+found' dir in test that expects empty layout of directory.
    
    Signed-off-by: Franciszek Zdobylak <fzdobylak@antmicro.com>

--- tests/subsys/fs/ext2/src/test_dirops.c
+++ tests/subsys/fs/ext2/src/test_dirops.c
@@ -16,6 +16,19 @@ struct fs_mount_t *fs_dirops_test_mp = &testfs_mnt;
 
 ZTEST(ext2tests, test_dirops)
 {
+	struct fs_mount_t *mp = &testfs_mnt;
+	struct fs_dirent de;
+
+	zassert_equal(fs_mount(mp), 0, "Mount failed");
+
+	/* 'lost+found' directory is created automatically. test_fs_dirops expects empty root
+	 * directory hence we have to remove it before starting test.
+	 */
+	if (fs_stat("/sml/lost+found", &de) == 0) {
+		zassert_equal(fs_unlink("/sml/lost+found"), 0, "unlink failed");
+	}
+	zassert_equal(fs_unmount(mp), 0, "Unount failed");
+
 	/* Common dirops tests.
 	 * (File system is mounted and unmounted during that test.)
 	 */
