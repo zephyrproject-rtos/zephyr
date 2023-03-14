db4febc584c86ff5a1dd2addb45874a0ef676f45
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2023-03-14 12:37:49 +0000

    retention: Add bootloader configuration interface
    Adds a bootloader configuration interface which allows for a
    bootloader (e.g. MCUboot) to set configuration in a shared data
    area which is then read by the application.
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- scripts/ci/check_compliance.py
+++ scripts/ci/check_compliance.py
@@ -624,11 +624,13 @@ flagged.
         "BOOT_SERIAL_CDC_ACM",       # Used in (sysbuild-based) test
         "BOOT_SERIAL_ENTRANCE_GPIO", # Used in (sysbuild-based) test
         "BOOT_SERIAL_IMG_GRP_HASH",  # Used in documentation
-        "BOOT_SIGNATURE_KEY_FILE", # MCUboot setting used by sysbuild
+        "BOOT_SHARE_DATA",           # Used in Kconfig text
+        "BOOT_SHARE_BACKEND_RETENTION", # Used in Kconfig text
+        "BOOT_SIGNATURE_KEY_FILE",   # MCUboot setting used by sysbuild
         "BOOT_SIGNATURE_TYPE_ECDSA_P256", # MCUboot setting used by sysbuild
-        "BOOT_SIGNATURE_TYPE_ED25519", # MCUboot setting used by sysbuild
-        "BOOT_SIGNATURE_TYPE_NONE", # MCUboot setting used by sysbuild
-        "BOOT_SIGNATURE_TYPE_RSA", # MCUboot setting used by sysbuild
+        "BOOT_SIGNATURE_TYPE_ED25519",    # MCUboot setting used by sysbuild
+        "BOOT_SIGNATURE_TYPE_NONE",       # MCUboot setting used by sysbuild
+        "BOOT_SIGNATURE_TYPE_RSA",        # MCUboot setting used by sysbuild
         "BOOT_VALIDATE_SLOT0",       # Used in (sysbuild-based) test
         "BOOT_WATCHDOG_FEED",        # Used in (sysbuild-based) test
         "BTTESTER_LOG_LEVEL",  # Used in tests/bluetooth/tester
