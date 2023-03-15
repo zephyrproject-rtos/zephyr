4034894d136e65106754d63f09222a5ef09277cc
Kumar Gala <kumar.gala@intel.com>
2023-03-15 23:58:29 +0000

    armclang: fix compiler warnings with isprint()
    We get compile warnings of the form:
    
    drivers/console/uart_console.c:508:8: error: converting the result of
    '<<' to a boolean; did you mean
    '((__aeabi_ctype_table_ + 1)[(byte)] << 28) != 0'?
     [-Werror,-Wint-in-bool-context]
                    if (!isprint(byte)) {
                         ^
    
    Since isprint returns an int, change check to an explicit test against
    the return value.
    
    Signed-off-by: Kumar Gala <kumar.gala@intel.com>
    (cherry picked from commit 831bd2f841faa49008c4acd471bb019f44de1c9d)

--- subsys/shell/shell.c
+++ subsys/shell/shell.c
@@ -1049,7 +1049,7 @@ static void state_collect(const struct shell *shell)
 				break;
 
 			default:
-				if (isprint((int) data)) {
+				if (isprint((int) data) != 0) {
 					z_flag_history_exit_set(shell, true);
 					z_shell_op_char_insert(shell, data);
 				} else if (z_flag_echo_get(shell)) {
@@ -1557,7 +1557,7 @@ void shell_hexdump_line(const struct shell *shell, unsigned int offset,
 			char c = data[i];
 
 			shell_fprintf(shell, SHELL_NORMAL, "%c",
-				      isprint((int)c) ? c : '.');
+				      isprint((int)c) != 0 ? c : '.');
 		} else {
 			shell_fprintf(shell, SHELL_NORMAL, " ");
 		}
