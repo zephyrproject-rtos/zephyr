da04b42fa4b28127a300e309bcd27cfeb0dd7c37
Ole Sæther <ole.saether@nordicsemi.no>
2023-03-18 16:52:41 +0100

    samples: bluetooth: BTHome sensor template fix whitespace
    Whitespace fix.
    
    Signed-off-by: Ole Sæther <ole.saether@nordicsemi.no>
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- samples/bluetooth/bthome_sensor_template/src/main.c
+++ samples/bluetooth/bthome_sensor_template/src/main.c
@@ -8,13 +8,13 @@
 #include <zephyr/bluetooth/uuid.h>
 
 #define SERVICE_DATA_LEN        9
-#define SERVICE_UUID            0xfcd2		/* BTHome service UUID */
+#define SERVICE_UUID            0xfcd2      /* BTHome service UUID */
 #define IDX_TEMPL               4           /* Index of lo byte of temp in service data*/
 #define IDX_TEMPH               5           /* Index of hi byte of temp in service data*/
 
 #define ADV_PARAM BT_LE_ADV_PARAM(BT_LE_ADV_OPT_USE_IDENTITY, \
-					     BT_GAP_ADV_SLOW_INT_MIN, \
-					     BT_GAP_ADV_SLOW_INT_MAX, NULL)
+				  BT_GAP_ADV_SLOW_INT_MIN, \
+				  BT_GAP_ADV_SLOW_INT_MAX, NULL)
 
 
 static uint8_t service_data[SERVICE_DATA_LEN] = {
