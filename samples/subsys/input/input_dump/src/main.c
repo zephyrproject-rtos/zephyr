7c70250df2d3b75dcc1c575a8bf01f800ba47cd3
Fabio Baltieri <fabiobaltieri@google.com>
2023-03-19 21:29:17 +0000

    samples: input_dump: handle events with no device set
    Input events with no associated device structure are valid. Handle them
    in the sample by printing NULL instead of crashing trying to lookup the
    device name.
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

--- samples/subsys/input/input_dump/src/main.c
+++ samples/subsys/input/input_dump/src/main.c
@@ -10,7 +10,7 @@
 static void input_cb(struct input_event *evt)
 {
 	printf("input event: dev=%-16s %3s type=%2x code=%3d value=%d\n",
-	       evt->dev->name,
+	       evt->dev ? evt->dev->name : "NULL",
 	       evt->sync ? "SYN" : "",
 	       evt->type,
 	       evt->code,
