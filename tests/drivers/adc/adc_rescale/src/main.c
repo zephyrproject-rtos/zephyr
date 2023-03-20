e8b43aa86a732fd4df7b63fe049e4e5054de0b6c
Jason Yuan <jasonyuan@google.com>
2023-03-20 15:26:52 -0700

    test: adc: current sense shunt
    adds tests for the current sense shunt binding macro and rescaling
    function.
    
    Signed-off-by: Jason Yuan <jasonyuan@google.com>

--- tests/drivers/adc/adc_rescale/src/main.c
+++ tests/drivers/adc/adc_rescale/src/main.c
@@ -7,6 +7,7 @@
 #include <zephyr/drivers/adc.h>
 #include <zephyr/drivers/adc/adc_emul.h>
 #include <zephyr/drivers/adc/voltage_divider.h>
+#include <zephyr/drivers/adc/current_sense_shunt.h>
 #include <zephyr/kernel.h>
 #include <zephyr/ztest.h>
 
@@ -14,6 +15,7 @@
 #define MV_OUTPUT_EPS 10
 
 #define ADC_TEST_NODE_0 DT_NODELABEL(sensor0)
+#define ADC_TEST_NODE_1 DT_NODELABEL(sensor1)
 
 /**
  * @brief Get ADC emulated device
@@ -83,6 +85,46 @@ static int test_task_voltage_divider(void)
 ZTEST_USER(adc_rescale, test_adc_voltage_divider)
 {
 	zassert_true(test_task_voltage_divider() == TC_PASS);
+}
+
+/*
+ * test_adc_current_sensor
+ */
+static int test_task_current_sensor(void)
+{
+	int ret;
+	int32_t calculated_current = 0;
+	int32_t input_mv = 3000;
+	const struct current_sense_shunt_dt_spec adc_node_1 =
+		CURRENT_SENSE_SHUNT_DT_SPEC_GET(ADC_TEST_NODE_1);
+
+	ret = init_adc(&adc_node_1.port, input_mv);
+	zassert_equal(ret, 0, "Setting up of the second channel failed with code %d", ret);
+
+	struct adc_sequence sequence = {
+		.buffer = &calculated_current,
+		.buffer_size = sizeof(calculated_current),
+	};
+	adc_sequence_init_dt(&adc_node_1.port, &sequence);
+
+	ret = adc_read(adc_node_1.port.dev, &sequence);
+	zassert_equal(ret, 0, "adc_read() failed with code %d", ret);
+
+	ret = adc_raw_to_millivolts_dt(&adc_node_1.port, &calculated_current);
+	zassert_equal(ret, 0, "adc_raw_to_millivolts_dt() failed with code %d", ret);
+
+	current_sense_shunt_scale_dt(&adc_node_1, &calculated_current);
+
+	zassert_within(calculated_current, input_mv * 2, MV_OUTPUT_EPS,
+		       "%u != %u should have set value", calculated_current,
+		       input_mv * 2);
+
+	return TC_PASS;
+}
+
+ZTEST_USER(adc_rescale, test_adc_current_sensor)
+{
+	zassert_true(test_task_current_sensor() == TC_PASS);
 }
 
 void *adc_rescale_setup(void)
