fbc7a9e209397e0d8c487d91e5106fbe9f633cd6
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2023-03-22 14:30:11 +0100

    soc: arm: smartbond: Add support for extended sleep
    This enabled extended sleep for Renesas SmartBond(tm).
    
    Extended sleep is low power mode where ARM core is powered off and can
    be woken up by PDC. This is default sleep mode when CONFIG_PM is
    enabled.
    
    Signed-off-by: Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
    Signed-off-by: Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>

--- soc/renesas/smartbond/da1469x/soc.c
+++ soc/renesas/smartbond/da1469x/soc.c
@@ -6,6 +6,7 @@
 
 #include <zephyr/init.h>
 #include <zephyr/linker/linker-defs.h>
+#include <zephyr/logging/log.h>
 #include <string.h>
 #include <DA1469xAB.h>
 #include <da1469x_clock.h>
@@ -13,7 +14,9 @@
 #include <da1469x_pd.h>
 #include <da1469x_pdc.h>
 #include <da1469x_trimv.h>
-#include <cmsis_core.h>
+#include <da1469x_sleep.h>
+
+LOG_MODULE_REGISTER(soc, CONFIG_SOC_LOG_LEVEL);
 
 #define REMAP_ADR0_QSPI           0x2
 
@@ -108,8 +111,23 @@ static void z_renesas_configure_cache(void)
 
 void z_arm_platform_init(void)
 {
+#if defined(CONFIG_PM)
+	uint32_t *ivt;
+#endif
+
 #if defined(CONFIG_BOOTLOADER_MCUBOOT)
 	z_renesas_configure_cache();
+#endif
+
+#if defined(CONFIG_PM)
+	/* IVT is always placed in reserved space at the start of RAM which
+	 * is then remapped to 0x0 and retained. Generic reset handler is
+	 * changed to custom routine since next time ARM core is reset we
+	 * need to determine whether it was a regular reset or a wakeup from
+	 * extended sleep and ARM core state needs to be restored.
+	 */
+	ivt = (uint32_t *)_image_ram_start;
+	ivt[1] = (uint32_t)da1469x_wakeup_handler;
 #endif
 }
 
@@ -117,6 +135,7 @@ static int renesas_da1469x_init(void)
 {
 	/* Freeze watchdog until configured */
 	GPREG->SET_FREEZE_REG = GPREG_SET_FREEZE_REG_FRZ_SYS_WDOG_Msk;
+	SYS_WDOG->WATCHDOG_REG = SYS_WDOG_WATCHDOG_REG_WDOG_VAL_Msk;
 
 	/* Reset clock dividers to 0 */
 	CRG_TOP->CLK_AMBA_REG &= ~(CRG_TOP_CLK_AMBA_REG_HCLK_DIV_Msk |
