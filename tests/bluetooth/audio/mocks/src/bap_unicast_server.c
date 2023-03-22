zephyrproject-rtos/zephyr

commit be0ee36024a364e2fbfbb007dd18c4a37ca18b27

LE Audio: Refactor server ASE callbacks to return rsp object
From now app layer is able to return explicit response code and
reason that will appear in ASE Control Point notification.

Fixes issues of ASCS/SR/SPE/BI-(07/08/09)-C PTS test cases, where
PTS was receiving wrong response codes and reasons.

Signed-off-by: Magdalena Kasenberg <magdalena.kasenberg@codecoup.pl>

samples/bluetooth/unicast_audio_server/src/main.c
@@ -261,7 +261,7 @@ static struct bt_bap_stream *stream_alloc(enum bt_audio_dir dir)
 
 static int lc3_config(struct bt_conn *conn, const struct bt_bap_ep *ep, enum bt_audio_dir dir,
 		      const struct bt_codec *codec, struct bt_bap_stream **stream,
-		      struct bt_codec_qos_pref *const pref)
+		      struct bt_codec_qos_pref *const pref, struct bt_bap_ascs_rsp *rsp)
 {
 	printk("ASE Codec Config: conn %p ep %p dir %u\n", conn, ep, dir);
 
@@ -270,6 +270,7 @@ static int lc3_config(struct bt_conn *conn, const struct bt_bap_ep *ep, enum bt_
 	*stream = stream_alloc(dir);
 	if (*stream == NULL) {
 		printk("No streams available\n");
+		*rsp = BT_BAP_ASCS_RSP(BT_BAP_ASCS_RSP_CODE_NO_MEM, BT_BAP_ASCS_REASON_NONE);
 
 		return -ENOMEM;
 	}
@@ -291,7 +292,8 @@ static int lc3_config(struct bt_conn *conn, const struct bt_bap_ep *ep, enum bt_
 }
 
 static int lc3_reconfig(struct bt_bap_stream *stream, enum bt_audio_dir dir,
-			const struct bt_codec *codec, struct bt_codec_qos_pref *const pref)
+			const struct bt_codec *codec, struct bt_codec_qos_pref *const pref,
+			struct bt_bap_ascs_rsp *rsp)
 {
 	printk("ASE Codec Reconfig: stream %p\n", stream);
 
@@ -302,11 +304,14 @@ static int lc3_reconfig(struct bt_bap_stream *stream, enum bt_audio_dir dir,
 	lc3_decoder = NULL;
 #endif
 
+	*rsp = BT_BAP_ASCS_RSP(BT_BAP_ASCS_RSP_CODE_CONF_UNSUPPORTED, BT_BAP_ASCS_REASON_NONE);
+
 	/* We only support one QoS at the moment, reject changes */
 	return -ENOEXEC;
 }
 
-static int lc3_qos(struct bt_bap_stream *stream, const struct bt_codec_qos *qos)
+static int lc3_qos(struct bt_bap_stream *stream, const struct bt_codec_qos *qos,
+		   struct bt_bap_ascs_rsp *rsp)
 {
 	printk("QoS: stream %p qos %p\n", stream, qos);
 
@@ -323,7 +328,7 @@ static int lc3_qos(struct bt_bap_stream *stream, const struct bt_codec_qos *qos)
 }
 
 static int lc3_enable(struct bt_bap_stream *stream, const struct bt_codec_data *meta,
-		      size_t meta_count)
+		      size_t meta_count, struct bt_bap_ascs_rsp *rsp)
 {
 	printk("Enable: stream %p meta_count %u\n", stream, meta_count);
 
@@ -334,11 +339,15 @@ static int lc3_enable(struct bt_bap_stream *stream, const struct bt_codec_data *
 
 		if (freq < 0) {
 			printk("Error: Codec frequency not set, cannot start codec.");
+			*rsp = BT_BAP_ASCS_RSP(BT_BAP_ASCS_RSP_CODE_CONF_INVALID,
+					       BT_BAP_ASCS_REASON_CODEC_DATA);
 			return -1;
 		}
 
 		if (frame_duration_us < 0) {
 			printk("Error: Frame duration not set, cannot start codec.");
+			*rsp = BT_BAP_ASCS_RSP(BT_BAP_ASCS_RSP_CODE_CONF_INVALID,
+					       BT_BAP_ASCS_REASON_CODEC_DATA);
 			return -1;
 		}
 
@@ -351,6 +360,8 @@ static int lc3_enable(struct bt_bap_stream *stream, const struct bt_codec_data *
 
 		if (lc3_decoder == NULL) {
 			printk("ERROR: Failed to setup LC3 encoder - wrong parameters?\n");
+			*rsp = BT_BAP_ASCS_RSP(BT_BAP_ASCS_RSP_CODE_CONF_INVALID,
+					       BT_BAP_ASCS_REASON_CODEC_DATA);
 			return -1;
 		}
 	}
@@ -359,7 +370,7 @@ static int lc3_enable(struct bt_bap_stream *stream, const struct bt_codec_data *
 	return 0;
 }
 
-static int lc3_start(struct bt_bap_stream *stream)
+static int lc3_start(struct bt_bap_stream *stream, struct bt_bap_ascs_rsp *rsp)
 {
 	printk("Start: stream %p\n", stream);
 
@@ -425,14 +436,18 @@ static bool valid_metadata_type(uint8_t type, uint8_t len)
 }
 
 static int lc3_metadata(struct bt_bap_stream *stream, const struct bt_codec_data *meta,
-			size_t meta_count)
+			size_t meta_count, struct bt_bap_ascs_rsp *rsp)
 {
 	printk("Metadata: stream %p meta_count %u\n", stream, meta_count);
 
 	for (size_t i = 0; i < meta_count; i++) {
-		if (!valid_metadata_type(meta->data.type, meta->data.data_len)) {
+		const struct bt_codec_data *data = &meta[i];
+
+		if (!valid_metadata_type(data->data.type, data->data.data_len)) {
 			printk("Invalid metadata type %u or length %u\n",
-			       meta->data.type, meta->data.data_len);
+			       data->data.type, data->data.data_len);
+			*rsp = BT_BAP_ASCS_RSP(BT_BAP_ASCS_RSP_CODE_METADATA_REJECTED,
+					       data->data.type);
 
 			return -EINVAL;
 		}
@@ -441,21 +456,21 @@ static int lc3_metadata(struct bt_bap_stream *stream, const struct bt_codec_data
 	return 0;
 }
 
-static int lc3_disable(struct bt_bap_stream *stream)
+static int lc3_disable(struct bt_bap_stream *stream, struct bt_bap_ascs_rsp *rsp)
 {
 	printk("Disable: stream %p\n", stream);
 
 	return 0;
 }
 
-static int lc3_stop(struct bt_bap_stream *stream)
+static int lc3_stop(struct bt_bap_stream *stream, struct bt_bap_ascs_rsp *rsp)
 {
 	printk("Stop: stream %p\n", stream);
 
 	return 0;
 }
 
-static int lc3_release(struct bt_bap_stream *stream)
+static int lc3_release(struct bt_bap_stream *stream, struct bt_bap_ascs_rsp *rsp)
 {
 	printk("Release: stream %p\n", stream);
 	return 0;

subsys/bluetooth/audio/bap_unicast_server.c
@@ -58,6 +58,8 @@ int bt_bap_unicast_server_unregister_cb(const struct bt_bap_unicast_server_cb *c
 int bt_bap_unicast_server_reconfig(struct bt_bap_stream *stream, const struct bt_codec *codec)
 {
 	struct bt_bap_ep *ep;
+	struct bt_bap_ascs_rsp rsp = BT_BAP_ASCS_RSP(BT_BAP_ASCS_RSP_CODE_SUCCESS,
+						     BT_BAP_ASCS_REASON_NONE);
 	int err;
 
 	ep = stream->ep;
@@ -65,7 +67,7 @@ int bt_bap_unicast_server_reconfig(struct bt_bap_stream *stream, const struct bt
 	if (unicast_server_cb != NULL &&
 		unicast_server_cb->reconfig != NULL) {
 		err = unicast_server_cb->reconfig(stream, ep->dir, codec,
-						  &ep->qos_pref);
+						  &ep->qos_pref, &rsp);
 	} else {
 		err = -ENOTSUP;
 	}
@@ -108,11 +110,13 @@ int bt_bap_unicast_server_metadata(struct bt_bap_stream *stream, struct bt_codec
 				   size_t meta_count)
 {
 	struct bt_bap_ep *ep;
+	struct bt_bap_ascs_rsp rsp = BT_BAP_ASCS_RSP(BT_BAP_ASCS_RSP_CODE_SUCCESS,
+						     BT_BAP_ASCS_REASON_NONE);
 	int err;
 
 
 	if (unicast_server_cb != NULL && unicast_server_cb->metadata != NULL) {
-		err = unicast_server_cb->metadata(stream, meta, meta_count);
+		err = unicast_server_cb->metadata(stream, meta, meta_count, &rsp);
 	} else {
 		err = -ENOTSUP;
 	}
@@ -124,6 +128,7 @@ int bt_bap_unicast_server_metadata(struct bt_bap_stream *stream, struct bt_codec
 	}
 
 	if (err) {
+		LOG_ERR("Metadata failed: err %d, code %u, reason %u", err, rsp.code, rsp.reason);
 		return err;
 	}
 
@@ -136,15 +141,18 @@ int bt_bap_unicast_server_metadata(struct bt_bap_stream *stream, struct bt_codec
 int bt_bap_unicast_server_disable(struct bt_bap_stream *stream)
 {
 	struct bt_bap_ep *ep;
+	struct bt_bap_ascs_rsp rsp = BT_BAP_ASCS_RSP(BT_BAP_ASCS_RSP_CODE_SUCCESS,
+						     BT_BAP_ASCS_REASON_NONE);
 	int err;
 
 	if (unicast_server_cb != NULL && unicast_server_cb->disable != NULL) {
-		err = unicast_server_cb->disable(stream);
+		err = unicast_server_cb->disable(stream, &rsp);
 	} else {
 		err = -ENOTSUP;
 	}
 
 	if (err != 0) {
+		LOG_ERR("Disable failed: err %d, code %u, reason %u", err, rsp.code, rsp.reason);
 		return err;
 	}
 
@@ -164,15 +172,18 @@ int bt_bap_unicast_server_disable(struct bt_bap_stream *stream)
 
 int bt_bap_unicast_server_release(struct bt_bap_stream *stream)
 {
+	struct bt_bap_ascs_rsp rsp = BT_BAP_ASCS_RSP(BT_BAP_ASCS_RSP_CODE_SUCCESS,
+						     BT_BAP_ASCS_REASON_NONE);
 	int err;
 
 	if (unicast_server_cb != NULL && unicast_server_cb->release != NULL) {
-		err = unicast_server_cb->release(stream);
+		err = unicast_server_cb->release(stream, &rsp);
 	} else {
 		err = -ENOTSUP;
 	}
 
 	if (err != 0) {
+		LOG_ERR("Release failed: err %d, code %u, reason %u", err, rsp.code, rsp.reason);
 		return err;
 	}
 

include/zephyr/bluetooth/audio/bap.h
@@ -154,6 +154,62 @@ enum bt_bap_ascs_reason {
 	BT_BAP_ASCS_REASON_CIS = 0x0a,
 };
 
+/** @brief Structure storing values of fields of ASE Control Point notification. */
+struct bt_bap_ascs_rsp {
+	/** @brief Value of the Response Code field. */
+	enum bt_bap_ascs_rsp_code code;
+
+	/**
+	 * @brief Value of the Reason field.
+	 *
+	 * The meaning of this value depend on the Response Code field.
+	 */
+	union {
+		/**
+		 * @brief Response reason
+		 *
+		 * If the Response Code is one of the following:
+		 * - @ref BT_BAP_ASCS_RSP_CODE_CONF_UNSUPPORTED
+		 * - @ref BT_BAP_ASCS_RSP_CODE_CONF_REJECTED
+		 * - @ref BT_BAP_ASCS_RSP_CODE_CONF_INVALID
+		 * all values from @ref bt_bap_ascs_reason can be used.
+		 *
+		 * If the Response Code is one of the following:
+		 * - @ref BT_BAP_ASCS_RSP_CODE_SUCCESS
+		 * - @ref BT_BAP_ASCS_RSP_CODE_NOT_SUPPORTED
+		 * - @ref BT_BAP_ASCS_RSP_CODE_INVALID_LENGTH
+		 * - @ref BT_BAP_ASCS_RSP_CODE_INVALID_ASE
+		 * - @ref BT_BAP_ASCS_RSP_CODE_INVALID_ASE_STATE
+		 * - @ref BT_BAP_ASCS_RSP_CODE_INVALID_DIR
+		 * - @ref BT_BAP_ASCS_RSP_CODE_CAP_UNSUPPORTED
+		 * - @ref BT_BAP_ASCS_RSP_CODE_NO_MEM
+		 * - @ref BT_BAP_ASCS_RSP_CODE_UNSPECIFIED
+		 * only value @ref BT_BAP_ASCS_REASON_NONE shall be used.
+		 */
+		enum bt_bap_ascs_reason reason;
+
+		/**
+		 * @brief Response metadata type
+		 *
+		 * If the Response Code is one of the following:
+		 * - @ref BT_BAP_ASCS_RSP_CODE_METADATA_UNSUPPORTED
+		 * - @ref BT_BAP_ASCS_RSP_CODE_METADATA_REJECTED
+		 * - @ref BT_BAP_ASCS_RSP_CODE_METADATA_INVALID
+		 * the value of the Metadata Type shall be used.
+		 */
+		enum bt_audio_metadata_type metadata_type;
+	};
+};
+
+/**
+ * @brief Macro used to initialise the object storing values of ASE Control Point notification.
+ *
+ * @param c Response Code field
+ * @param r Reason field - @ref bt_bap_ascs_reason or @ref bt_audio_metadata_type (see notes in
+ *          @ref bt_bap_ascs_rsp).
+ */
+#define BT_BAP_ASCS_RSP(c, r) (struct bt_bap_ascs_rsp) { .code = c, .reason = r }
+
 /** @brief Abstract Audio Broadcast Source structure. */
 struct bt_bap_broadcast_source;
 
@@ -580,12 +636,14 @@ struct bt_bap_unicast_server_cb {
 	 * @param[out] stream  Pointer to stream that will be configured for the endpoint.
 	 * @param[out] pref    Pointer to a QoS preference object that shall be populated with
 	 *                     values. Invalid values will reject the codec configuration request.
+	 * @param[out] rsp     Object for the ASE operation response. Only used if the return
+	 *                     value is non-zero.
 	 *
 	 * @return 0 in case of success or negative value in case of error.
 	 */
 	int (*config)(struct bt_conn *conn, const struct bt_bap_ep *ep, enum bt_audio_dir dir,
 		      const struct bt_codec *codec, struct bt_bap_stream **stream,
-		      struct bt_codec_qos_pref *const pref);
+		      struct bt_codec_qos_pref *const pref, struct bt_bap_ascs_rsp *rsp);
 
 	/**
 	 * @brief Stream reconfig request callback
@@ -598,11 +656,14 @@ struct bt_bap_unicast_server_cb {
 	 * @param[in]  codec   Codec configuration.
 	 * @param[out] pref    Pointer to a QoS preference object that shall be populated with
 	 *                     values. Invalid values will reject the codec configuration request.
+	 * @param[out] rsp     Object for the ASE operation response. Only used if the return
+	 *                     value is non-zero.
 	 *
 	 * @return 0 in case of success or negative value in case of error.
 	 */
 	int (*reconfig)(struct bt_bap_stream *stream, enum bt_audio_dir dir,
-			const struct bt_codec *codec, struct bt_codec_qos_pref *const pref);
+			const struct bt_codec *codec, struct bt_codec_qos_pref *const pref,
+			struct bt_bap_ascs_rsp *rsp);
 
 	/**
 	 * @brief Stream QoS request callback
@@ -610,73 +671,86 @@ struct bt_bap_unicast_server_cb {
 	 * QoS callback is called whenever an Audio Stream Quality of
 	 * Service needs to be configured.
 	 *
-	 * @param stream  Stream object being reconfigured.
-	 * @param qos     Quality of Service configuration.
+	 * @param[in]  stream  Stream object being reconfigured.
+	 * @param[in]  qos     Quality of Service configuration.
+	 * @param[out] rsp     Object for the ASE operation response. Only used if the return
+	 *                     value is non-zero.
 	 *
 	 * @return 0 in case of success or negative value in case of error.
 	 */
-	int (*qos)(struct bt_bap_stream *stream, const struct bt_codec_qos *qos);
+	int (*qos)(struct bt_bap_stream *stream, const struct bt_codec_qos *qos,
+		   struct bt_bap_ascs_rsp *rsp);
 
 	/**
 	 * @brief Stream Enable request callback
 	 *
 	 * Enable callback is called whenever an Audio Stream is requested to be enabled to stream.
 	 *
-	 * @param stream      Stream object being enabled.
-	 * @param meta        Metadata entries
-	 * @param meta_count  Number of metadata entries
+	 * @param[in]  stream      Stream object being enabled.
+	 * @param[in]  meta        Metadata entries
+	 * @param[in]  meta_count  Number of metadata entries
+	 * @param[out] rsp         Object for the ASE operation response. Only used if the return
+	 *                         value is non-zero.
 	 *
 	 * @return 0 in case of success or negative value in case of error.
 	 */
 	int (*enable)(struct bt_bap_stream *stream, const struct bt_codec_data *meta,
-		      size_t meta_count);
+		      size_t meta_count, struct bt_bap_ascs_rsp *rsp);
 
 	/**
 	 * @brief Stream Start request callback
 	 *
 	 * Start callback is called whenever an Audio Stream is requested to start streaming.
 	 *
-	 * @param stream Stream object.
+	 * @param[in]  stream Stream object.
+	 * @param[out] rsp    Object for the ASE operation response. Only used if the return
+	 *                    value is non-zero.
 	 *
 	 * @return 0 in case of success or negative value in case of error.
 	 */
-	int (*start)(struct bt_bap_stream *stream);
+	int (*start)(struct bt_bap_stream *stream, struct bt_bap_ascs_rsp *rsp);
 
 	/**
 	 * @brief Stream Metadata update request callback
 	 *
 	 * Metadata callback is called whenever an Audio Stream is requested to update its metadata.
 	 *
-	 * @param stream       Stream object.
-	 * @param meta         Metadata entries
-	 * @param meta_count   Number of metadata entries
+	 * @param[in]  stream       Stream object.
+	 * @param[in]  meta         Metadata entries
+	 * @param[in]  meta_count   Number of metadata entries
+	 * @param[out] rsp          Object for the ASE operation response. Only used if the return
+	 *                          value is non-zero.
 	 *
 	 * @return 0 in case of success or negative value in case of error.
 	 */
 	int (*metadata)(struct bt_bap_stream *stream, const struct bt_codec_data *meta,
-			size_t meta_count);
+			size_t meta_count, struct bt_bap_ascs_rsp *rsp);
 
 	/**
 	 * @brief Stream Disable request callback
 	 *
 	 * Disable callback is called whenever an Audio Stream is requested to disable the stream.
 	 *
-	 * @param stream Stream object being disabled.
+	 * @param[in]  stream Stream object being disabled.
+	 * @param[out] rsp    Object for the ASE operation response. Only used if the return
+	 *                    value is non-zero.
 	 *
 	 * @return 0 in case of success or negative value in case of error.
 	 */
-	int (*disable)(struct bt_bap_stream *stream);
+	int (*disable)(struct bt_bap_stream *stream, struct bt_bap_ascs_rsp *rsp);
 
 	/**
 	 * @brief Stream Stop callback
 	 *
 	 * Stop callback is called whenever an Audio Stream is requested to stop streaming.
 	 *
-	 * @param stream Stream object.
+	 * @param[in]  stream Stream object.
+	 * @param[out] rsp    Object for the ASE operation response. Only used if the return
+	 *                    value is non-zero.
 	 *
 	 * @return 0 in case of success or negative value in case of error.
 	 */
-	int (*stop)(struct bt_bap_stream *stream);
+	int (*stop)(struct bt_bap_stream *stream, struct bt_bap_ascs_rsp *rsp);
 
 	/**
 	 * @brief Stream release callback
@@ -684,11 +758,13 @@ struct bt_bap_unicast_server_cb {
 	 * Release callback is called whenever a new Audio Stream needs to be released and thus
 	 * deallocated.
 	 *
-	 * @param stream Stream object.
+	 * @param[in]  stream Stream object.
+	 * @param[out] rsp    Object for the ASE operation response. Only used if the return
+	 *                    value is non-zero.
 	 *
 	 * @return 0 in case of success or negative value in case of error.
 	 */
-	int (*release)(struct bt_bap_stream *stream);
+	int (*release)(struct bt_bap_stream *stream, struct bt_bap_ascs_rsp *rsp);
 };
 
 /**

samples/bluetooth/hap_ha/src/bap_unicast_sr.c
@@ -189,7 +189,7 @@ static struct bt_bap_stream *stream_alloc(void)
 
 static int lc3_config(struct bt_conn *conn, const struct bt_bap_ep *ep, enum bt_audio_dir dir,
 		      const struct bt_codec *codec, struct bt_bap_stream **stream,
-		      struct bt_codec_qos_pref *const pref)
+		      struct bt_codec_qos_pref *const pref, struct bt_bap_ascs_rsp *rsp)
 {
 	printk("ASE Codec Config: conn %p ep %p dir %u\n", conn, ep, dir);
 
@@ -198,7 +198,7 @@ static int lc3_config(struct bt_conn *conn, const struct bt_bap_ep *ep, enum bt_
 	*stream = stream_alloc();
 	if (*stream == NULL) {
 		printk("No streams available\n");
-
+		*rsp = BT_BAP_ASCS_RSP(BT_BAP_ASCS_RSP_CODE_NO_MEM, BT_BAP_ASCS_REASON_NONE);
 		return -ENOMEM;
 	}
 
@@ -214,17 +214,21 @@ static int lc3_config(struct bt_conn *conn, const struct bt_bap_ep *ep, enum bt_
 }
 
 static int lc3_reconfig(struct bt_bap_stream *stream, enum bt_audio_dir dir,
-			const struct bt_codec *codec, struct bt_codec_qos_pref *const pref)
+			const struct bt_codec *codec, struct bt_codec_qos_pref *const pref,
+			struct bt_bap_ascs_rsp *rsp)
 {
 	printk("ASE Codec Reconfig: stream %p\n", stream);
 
 	print_codec(codec);
 
+	*rsp = BT_BAP_ASCS_RSP(BT_BAP_ASCS_RSP_CODE_CONF_UNSUPPORTED, BT_BAP_ASCS_REASON_NONE);
+
 	/* We only support one QoS at the moment, reject changes */
 	return -ENOEXEC;
 }
 
-static int lc3_qos(struct bt_bap_stream *stream, const struct bt_codec_qos *qos)
+static int lc3_qos(struct bt_bap_stream *stream, const struct bt_codec_qos *qos,
+		   struct bt_bap_ascs_rsp *rsp)
 {
 	printk("QoS: stream %p qos %p\n", stream, qos);
 
@@ -234,14 +238,14 @@ static int lc3_qos(struct bt_bap_stream *stream, const struct bt_codec_qos *qos)
 }
 
 static int lc3_enable(struct bt_bap_stream *stream, const struct bt_codec_data *meta,
-		      size_t meta_count)
+		      size_t meta_count, struct bt_bap_ascs_rsp *rsp)
 {
 	printk("Enable: stream %p meta_count %u\n", stream, meta_count);
 
 	return 0;
 }
 
-static int lc3_start(struct bt_bap_stream *stream)
+static int lc3_start(struct bt_bap_stream *stream, struct bt_bap_ascs_rsp *rsp)
 {
 	printk("Start: stream %p\n", stream);
 
@@ -308,14 +312,18 @@ static bool valid_metadata_type(uint8_t type, uint8_t len)
 }
 
 static int lc3_metadata(struct bt_bap_stream *stream, const struct bt_codec_data *meta,
-			size_t meta_count)
+			size_t meta_count, struct bt_bap_ascs_rsp *rsp)
 {
 	printk("Metadata: stream %p meta_count %u\n", stream, meta_count);
 
 	for (size_t i = 0; i < meta_count; i++) {
-		if (!valid_metadata_type(meta->data.type, meta->data.data_len)) {
+		const struct bt_codec_data *data = &meta[i];
+
+		if (!valid_metadata_type(data->data.type, data->data.data_len)) {
 			printk("Invalid metadata type %u or length %u\n",
-			       meta->data.type, meta->data.data_len);
+			       data->data.type, data->data.data_len);
+			*rsp = BT_BAP_ASCS_RSP(BT_BAP_ASCS_RSP_CODE_METADATA_REJECTED,
+					       data->data.type);
 
 			return -EINVAL;
 		}
@@ -324,21 +332,21 @@ static int lc3_metadata(struct bt_bap_stream *stream, const struct bt_codec_data
 	return 0;
 }
 
-static int lc3_disable(struct bt_bap_stream *stream)
+static int lc3_disable(struct bt_bap_stream *stream, struct bt_bap_ascs_rsp *rsp)
 {
 	printk("Disable: stream %p\n", stream);
 
 	return 0;
 }
 
-static int lc3_stop(struct bt_bap_stream *stream)
+static int lc3_stop(struct bt_bap_stream *stream, struct bt_bap_ascs_rsp *rsp)
 {
 	printk("Stop: stream %p\n", stream);
 
 	return 0;
 }
 
-static int lc3_release(struct bt_bap_stream *stream)
+static int lc3_release(struct bt_bap_stream *stream, struct bt_bap_ascs_rsp *rsp)
 {
 	printk("Release: stream %p\n", stream);
 	return 0;

subsys/bluetooth/audio/bap_broadcast_source.c
@@ -540,7 +540,7 @@ static bool valid_create_param(const struct bt_bap_broadcast_source_create_param
 		return false;
 	}
 
-	CHECKIF(!bt_audio_valid_qos(qos)) {
+	CHECKIF(bt_audio_verify_qos(qos) != BT_BAP_ASCS_REASON_NONE) {
 		LOG_DBG("param->qos is invalid");
 		return false;
 	}

subsys/bluetooth/audio/bap_stream.c
@@ -136,39 +136,39 @@ int bt_bap_ep_get_info(const struct bt_bap_ep *ep, struct bt_bap_ep_info *info)
 }
 
 #if defined(CONFIG_BT_BAP_UNICAST) || defined(CONFIG_BT_BAP_BROADCAST_SOURCE)
-bool bt_audio_valid_qos(const struct bt_codec_qos *qos)
+enum bt_bap_ascs_reason bt_audio_verify_qos(const struct bt_codec_qos *qos)
 {
 	if (qos->interval < BT_ISO_SDU_INTERVAL_MIN ||
 	    qos->interval > BT_ISO_SDU_INTERVAL_MAX) {
 		LOG_DBG("Interval not within allowed range: %u (%u-%u)", qos->interval,
 			BT_ISO_SDU_INTERVAL_MIN, BT_ISO_SDU_INTERVAL_MAX);
-		return false;
+		return BT_BAP_ASCS_REASON_INTERVAL;
 	}
 
 	if (qos->framing > BT_CODEC_QOS_FRAMED) {
 		LOG_DBG("Invalid Framing 0x%02x", qos->framing);
-		return false;
+		return BT_BAP_ASCS_REASON_FRAMING;
 	}
 
 	if (qos->phy != BT_CODEC_QOS_1M &&
 	    qos->phy != BT_CODEC_QOS_2M &&
 	    qos->phy != BT_CODEC_QOS_CODED) {
 		LOG_DBG("Invalid PHY 0x%02x", qos->phy);
-		return false;
+		return BT_BAP_ASCS_REASON_PHY;
 	}
 
 	if (qos->sdu > BT_ISO_MAX_SDU) {
 		LOG_DBG("Invalid SDU %u", qos->sdu);
-		return false;
+		return BT_BAP_ASCS_REASON_SDU;
 	}
 
 	if (qos->latency < BT_ISO_LATENCY_MIN ||
 	    qos->latency > BT_ISO_LATENCY_MAX) {
 		LOG_DBG("Invalid Latency %u", qos->latency);
-		return false;
+		return BT_BAP_ASCS_REASON_LATENCY;
 	}
 
-	return true;
+	return BT_BAP_ASCS_REASON_NONE;
 }
 
 int bt_bap_stream_send(struct bt_bap_stream *stream, struct net_buf *buf,
@@ -203,7 +203,8 @@ static bool bt_bap_stream_is_broadcast(const struct bt_bap_stream *stream)
 	       (IS_ENABLED(CONFIG_BT_BAP_BROADCAST_SINK) && bt_bap_ep_is_broadcast_snk(stream->ep));
 }
 
-bool bt_bap_stream_valid_qos(const struct bt_bap_stream *stream, const struct bt_codec_qos *qos)
+enum bt_bap_ascs_reason bt_bap_stream_verify_qos(const struct bt_bap_stream *stream,
+						 const struct bt_codec_qos *qos)
 {
 	const struct bt_codec_qos_pref *qos_pref = &stream->ep->qos_pref;
 
@@ -215,10 +216,10 @@ bool bt_bap_stream_valid_qos(const struct bt_bap_stream *stream, const struct bt
 	if (!IN_RANGE(qos->pd, qos_pref->pd_min, qos_pref->pd_max)) {
 		LOG_DBG("Presentation Delay not within range: min %u max %u pd %u",
 			qos_pref->pd_min, qos_pref->pd_max, qos->pd);
-		return false;
+		return BT_BAP_ASCS_REASON_PD;
 	}
 
-	return true;
+	return BT_BAP_ASCS_REASON_NONE;
 }
 
 void bt_bap_stream_detach(struct bt_bap_stream *stream)

subsys/bluetooth/audio/bap_stream.h
@@ -24,8 +24,9 @@ void bt_audio_codec_qos_to_iso_qos(struct bt_iso_chan_io_qos *io,
 
 void bt_bap_stream_detach(struct bt_bap_stream *stream);
 
-bool bt_audio_valid_qos(const struct bt_codec_qos *qos);
+enum bt_bap_ascs_reason bt_audio_verify_qos(const struct bt_codec_qos *qos);
 
-bool bt_bap_stream_valid_qos(const struct bt_bap_stream *stream, const struct bt_codec_qos *qos);
+enum bt_bap_ascs_reason bt_bap_stream_verify_qos(const struct bt_bap_stream *stream,
+						 const struct bt_codec_qos *qos);
 
 struct bt_iso_chan *bt_bap_stream_iso_chan_get(struct bt_bap_stream *stream);

commit ac5875a41acbc15d8d8d5a1bac1cacdafdacd5e7

tests: Bluetooth: ascs: Fix bap_unicast_server_cb mocks
This fixes merge conflict issue by adding missing function
parameters that were introduced in
be0ee36024a364e2fbfbb007dd18c4a37ca18b27.

Signed-off-by: Mariusz Skamra <mariusz.skamra@codecoup.pl>

tests/bluetooth/audio/ascs/src/main.c
@@ -566,10 +566,12 @@ static struct bt_bap_stream *stream_allocated;
 int unicast_server_cb_config_custom_fake(struct bt_conn *conn, const struct bt_bap_ep *ep,
 					 enum bt_audio_dir dir, const struct bt_codec *codec,
 					 struct bt_bap_stream **stream,
-					 struct bt_codec_qos_pref *const pref)
+					 struct bt_codec_qos_pref *const pref,
+					 struct bt_bap_ascs_rsp *rsp)
 {
 	*stream = stream_allocated;
 	*pref = qos_pref;
+	*rsp = BT_BAP_ASCS_RSP(BT_BAP_ASCS_RSP_CODE_SUCCESS, BT_BAP_ASCS_REASON_NONE);
 
 	bt_bap_stream_cb_register(*stream, &mock_bap_stream_ops);
 

tests/bluetooth/audio/mocks/include/bap_unicast_server.h
@@ -17,19 +17,24 @@ void mock_bap_unicast_server_cleanup(void);
 
 DECLARE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_config, struct bt_conn *,
 			const struct bt_bap_ep *, enum bt_audio_dir, const struct bt_codec *,
-			struct bt_bap_stream **, struct bt_codec_qos_pref *const);
+			struct bt_bap_stream **, struct bt_codec_qos_pref *const,
+			struct bt_bap_ascs_rsp *);
 DECLARE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_reconfig, struct bt_bap_stream *,
 			enum bt_audio_dir, const struct bt_codec *,
-			struct bt_codec_qos_pref *const);
+			struct bt_codec_qos_pref *const, struct bt_bap_ascs_rsp *);
 DECLARE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_qos, struct bt_bap_stream *,
-			const struct bt_codec_qos *);
+			const struct bt_codec_qos *, struct bt_bap_ascs_rsp *);
 DECLARE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_enable, struct bt_bap_stream *,
-			const struct bt_codec_data *, size_t);
-DECLARE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_start, struct bt_bap_stream *);
+			const struct bt_codec_data *, size_t, struct bt_bap_ascs_rsp *);
+DECLARE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_start, struct bt_bap_stream *,
+			struct bt_bap_ascs_rsp *);
 DECLARE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_metadata, struct bt_bap_stream *,
-			const struct bt_codec_data *, size_t);
-DECLARE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_disable, struct bt_bap_stream *);
-DECLARE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_stop, struct bt_bap_stream *);
-DECLARE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_release, struct bt_bap_stream *);
+			const struct bt_codec_data *, size_t, struct bt_bap_ascs_rsp *);
+DECLARE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_disable, struct bt_bap_stream *,
+			struct bt_bap_ascs_rsp *);
+DECLARE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_stop, struct bt_bap_stream *,
+			struct bt_bap_ascs_rsp *);
+DECLARE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_release, struct bt_bap_stream *,
+			struct bt_bap_ascs_rsp *);
 
 #endif /* MOCKS_BAP_UNICAST_SERVER_H_ */

tests/bluetooth/audio/mocks/src/bap_unicast_server.c
@@ -32,19 +32,25 @@ void mock_bap_unicast_server_cleanup(void)
 
 DEFINE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_config, struct bt_conn *,
 		       const struct bt_bap_ep *, enum bt_audio_dir, const struct bt_codec *,
-		       struct bt_bap_stream **, struct bt_codec_qos_pref *const);
+		       struct bt_bap_stream **, struct bt_codec_qos_pref *const,
+		       struct bt_bap_ascs_rsp *);
 DEFINE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_reconfig, struct bt_bap_stream *,
-		       enum bt_audio_dir, const struct bt_codec *, struct bt_codec_qos_pref *const);
+		       enum bt_audio_dir, const struct bt_codec *, struct bt_codec_qos_pref *const,
+		       struct bt_bap_ascs_rsp *);
 DEFINE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_qos, struct bt_bap_stream *,
-		       const struct bt_codec_qos *);
+		       const struct bt_codec_qos *, struct bt_bap_ascs_rsp *);
 DEFINE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_enable, struct bt_bap_stream *,
-		       const struct bt_codec_data *, size_t);
-DEFINE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_start, struct bt_bap_stream *);
+		       const struct bt_codec_data *, size_t, struct bt_bap_ascs_rsp *);
+DEFINE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_start, struct bt_bap_stream *,
+		       struct bt_bap_ascs_rsp *);
 DEFINE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_metadata, struct bt_bap_stream *,
-		       const struct bt_codec_data *, size_t);
-DEFINE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_disable, struct bt_bap_stream *);
-DEFINE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_stop, struct bt_bap_stream *);
-DEFINE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_release, struct bt_bap_stream *);
+		       const struct bt_codec_data *, size_t, struct bt_bap_ascs_rsp *);
+DEFINE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_disable, struct bt_bap_stream *,
+		       struct bt_bap_ascs_rsp *);
+DEFINE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_stop, struct bt_bap_stream *,
+		       struct bt_bap_ascs_rsp *);
+DEFINE_FAKE_VALUE_FUNC(int, mock_bap_unicast_server_cb_release, struct bt_bap_stream *,
+		       struct bt_bap_ascs_rsp *);
 
 const struct bt_bap_unicast_server_cb mock_bap_unicast_server_cb = {
 	.config = mock_bap_unicast_server_cb_config,