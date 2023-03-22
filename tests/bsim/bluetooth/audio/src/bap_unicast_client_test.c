11f5f3b080efb3ec9fed2d0e1647eea151b8fbfc
Emil Gydesen <emil.gydesen@nordicsemi.no>
2023-03-22 14:00:28 +0100

    Bluetooth: BAP: Move uni cli discov err to callback
    Move the err field from the discover parameters to
    the callback.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bsim/bluetooth/audio/src/bap_unicast_client_test.c
+++ tests/bsim/bluetooth/audio/src/bap_unicast_client_test.c
@@ -219,14 +219,15 @@ static void print_remote_codec(struct bt_codec *codec, int index, enum bt_audio_
 	print_codec(codec);
 }
 
-static void discover_sinks_cb(struct bt_conn *conn, struct bt_codec *codec, struct bt_bap_ep *ep,
+static void discover_sinks_cb(struct bt_conn *conn, int err, struct bt_codec *codec,
+			      struct bt_bap_ep *ep,
 			      struct bt_bap_unicast_client_discover_params *params)
 {
 	static bool codec_found;
 	static bool endpoint_found;
 
-	if (params->err != 0) {
-		FAIL("Discovery failed: %d\n", params->err);
+	if (err != 0) {
+		FAIL("Discovery failed: %d\n", err);
 		return;
 	}
 
@@ -258,14 +259,15 @@ static void discover_sinks_cb(struct bt_conn *conn, struct bt_codec *codec, stru
 	}
 }
 
-static void discover_sources_cb(struct bt_conn *conn, struct bt_codec *codec, struct bt_bap_ep *ep,
+static void discover_sources_cb(struct bt_conn *conn, int err, struct bt_codec *codec,
+				struct bt_bap_ep *ep,
 				struct bt_bap_unicast_client_discover_params *params)
 {
 	static bool codec_found;
 	static bool endpoint_found;
 
-	if (params->err != 0) {
-		FAIL("Discovery failed: %d\n", params->err);
+	if (err != 0) {
+		FAIL("Discovery failed: %d\n", err);
 		return;
 	}
 
