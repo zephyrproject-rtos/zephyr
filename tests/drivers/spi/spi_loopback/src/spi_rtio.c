06aa17f716ecdbd13ae37d3e297e821dd3f585e7
Tom Burdick <thomas.burdick@intel.com>
2023-03-22 09:32:17 -0500

    tests: SPI loopback with RTIO
    Adds the equivalent spi loopback tests using RTIO and a testplan that
    uses it. Adds a tdk robokit1 overlay to enable the NOCACHE option so
    that DMA transfers correctly work.
    
    Signed-off-by: Tom Burdick <thomas.burdick@intel.com>

--- tests/drivers/spi/spi_loopback/src/spi_rtio.c
+++ tests/drivers/spi/spi_loopback/src/spi_rtio.c
@@ -1,12 +1,13 @@
 /*
- * Copyright (c) 2017 Intel Corporation.
+ * Copyright (c) 2023 Intel Corporation.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include "zephyr/devicetree.h"
 #define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(spi_loopback);
+LOG_MODULE_REGISTER(spi_rtio_loopback);
 
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
@@ -14,6 +15,8 @@ LOG_MODULE_REGISTER(spi_loopback);
 #include <stdio.h>
 #include <zephyr/ztest.h>
 
+#include <zephyr/rtio/rtio.h>
+#include <zephyr/rtio/rtio_executor_simple.h>
 #include <zephyr/drivers/spi.h>
 
 #define SPI_FAST_DEV	DT_COMPAT_GET_ANY_STATUS_OKAY(test_spi_loopback_fast)
@@ -28,9 +31,11 @@ LOG_MODULE_REGISTER(spi_loopback);
 #define SPI_OP SPI_OP_MODE_MASTER | SPI_MODE_CPOL | MODE_LOOP | \
 	       SPI_MODE_CPHA | SPI_WORD_SET(8) | SPI_LINES_SINGLE
 
+static SPI_DT_IODEV_DEFINE(spi_fast, SPI_FAST_DEV, SPI_OP, 0);
+static SPI_DT_IODEV_DEFINE(spi_slow, SPI_FAST_DEV, SPI_OP, 0);
 
-static struct spi_dt_spec spi_fast = SPI_DT_SPEC_GET(SPI_FAST_DEV, SPI_OP, 0);
-static struct spi_dt_spec spi_slow = SPI_DT_SPEC_GET(SPI_SLOW_DEV, SPI_OP, 0);
+static RTIO_EXECUTOR_SIMPLE_DEFINE(rexec);
+RTIO_DEFINE(r, (struct rtio_executor *)&rexec, 8, 8);
 
 /* to run this test, connect MOSI pin to the MISO of the SPI */
 
@@ -64,6 +69,8 @@ static uint8_t buffer_print_rx[BUF_SIZE * 5 + 1];
 static uint8_t buffer_print_tx2[BUF2_SIZE * 5 + 1];
 static uint8_t buffer_print_rx2[BUF2_SIZE * 5 + 1];
 
+
+
 static void to_display_format(const uint8_t *src, size_t size, char *dst)
 {
 	size_t i;
@@ -74,38 +81,26 @@ static void to_display_format(const uint8_t *src, size_t size, char *dst)
 }
 
 /* test transferring different buffers on the same dma channels */
-static int spi_complete_multiple(struct spi_dt_spec *spec)
+static int spi_complete_multiple(struct rtio_iodev *spi_iodev)
 {
-	struct spi_buf tx_bufs[2];
-	const struct spi_buf_set tx = {
-		.buffers = tx_bufs,
-		.count = ARRAY_SIZE(tx_bufs)
-	};
-
-	tx_bufs[0].buf = buffer_tx;
-	tx_bufs[0].len = BUF_SIZE;
-
-	tx_bufs[1].buf = buffer2_tx;
-	tx_bufs[1].len = BUF2_SIZE;
-
-
-	struct spi_buf rx_bufs[2];
-	const struct spi_buf_set rx = {
-		.buffers = rx_bufs,
-		.count = ARRAY_SIZE(rx_bufs)
-	};
-
-	rx_bufs[0].buf = buffer_rx;
-	rx_bufs[0].len = BUF_SIZE;
-
-	rx_bufs[1].buf = buffer2_rx;
-	rx_bufs[1].len = BUF2_SIZE;
-
+	struct rtio_sqe *sqe;
+	struct rtio_cqe *cqe;
 	int ret;
 
-	LOG_INF("Start complete multiple");
+	sqe = rtio_sqe_acquire(&r);
+	rtio_sqe_prep_transceive(sqe, spi_iodev, RTIO_PRIO_NORM,
+				buffer_tx, buffer_rx, BUF_SIZE, NULL);
+	sqe->flags |= RTIO_SQE_TRANSACTION;
+	sqe = rtio_sqe_acquire(&r);
+	rtio_sqe_prep_transceive(sqe, spi_iodev, RTIO_PRIO_NORM,
+				 buffer2_tx, buffer2_rx, BUF2_SIZE, NULL);
 
-	ret = spi_transceive_dt(spec, &tx, &rx);
+	LOG_INF("Start complete multiple");
+	rtio_submit(&r, 1);
+	cqe = rtio_cqe_consume(&r);
+	ret = cqe->result;
+	rtio_cqe_release(&r);
+
 	if (ret) {
 		LOG_ERR("Code %d", ret);
 		zassert_false(ret, "SPI transceive failed");
@@ -135,34 +130,23 @@ static int spi_complete_multiple(struct spi_dt_spec *spec)
 	return 0;
 }
 
-static int spi_complete_loop(struct spi_dt_spec *spec)
+static int spi_complete_loop(struct rtio_iodev *spi_iodev)
 {
-	const struct spi_buf tx_bufs[] = {
-		{
-			.buf = buffer_tx,
-			.len = BUF_SIZE,
-		},
-	};
-	const struct spi_buf rx_bufs[] = {
-		{
-			.buf = buffer_rx,
-			.len = BUF_SIZE,
-		},
-	};
-	const struct spi_buf_set tx = {
-		.buffers = tx_bufs,
-		.count = ARRAY_SIZE(tx_bufs)
-	};
-	const struct spi_buf_set rx = {
-		.buffers = rx_bufs,
-		.count = ARRAY_SIZE(rx_bufs)
-	};
-
+	struct rtio_sqe *sqe;
+	struct rtio_cqe *cqe;
 	int ret;
+
+	sqe = rtio_sqe_acquire(&r);
+	rtio_sqe_prep_transceive(sqe, spi_iodev, RTIO_PRIO_NORM,
+				 buffer_tx,  buffer_rx, BUF_SIZE, NULL);
 
 	LOG_INF("Start complete loop");
 
-	ret = spi_transceive_dt(spec, &tx, &rx);
+	rtio_submit(&r, 1);
+	cqe = rtio_cqe_consume(&r);
+	ret = cqe->result;
+	rtio_cqe_release(&r);
+
 	if (ret) {
 		LOG_ERR("Code %d", ret);
 		zassert_false(ret, "SPI transceive failed");
@@ -183,41 +167,28 @@ static int spi_complete_loop(struct spi_dt_spec *spec)
 	return 0;
 }
 
-static int spi_null_tx_buf(struct spi_dt_spec *spec)
+static int spi_null_tx_buf(struct rtio_iodev *spi_iodev)
 {
 	static const uint8_t EXPECTED_NOP_RETURN_BUF[BUF_SIZE] = { 0 };
 
+	struct rtio_sqe *sqe;
+	struct rtio_cqe *cqe;
+	int ret;
+
 	(void)memset(buffer_rx, 0x77, BUF_SIZE);
 
-	const struct spi_buf tx_bufs[] = {
-		/* According to documentation, when sending NULL tx buf -
-		 *  NOP frames should be sent on MOSI line
-		 */
-		{
-			.buf = NULL,
-			.len = BUF_SIZE,
-		},
-	};
-	const struct spi_buf rx_bufs[] = {
-		{
-			.buf = buffer_rx,
-			.len = BUF_SIZE,
-		},
-	};
-	const struct spi_buf_set tx = {
-		.buffers = tx_bufs,
-		.count = ARRAY_SIZE(tx_bufs)
-	};
-	const struct spi_buf_set rx = {
-		.buffers = rx_bufs,
-		.count = ARRAY_SIZE(rx_bufs)
-	};
-
-	int ret;
+	sqe = rtio_sqe_acquire(&r);
+	rtio_sqe_prep_read(sqe, spi_iodev, RTIO_PRIO_NORM,
+				 buffer_rx, BUF_SIZE,
+				 NULL);
 
 	LOG_INF("Start null tx");
 
-	ret = spi_transceive_dt(spec, &tx, &rx);
+	rtio_submit(&r, 1);
+	cqe = rtio_cqe_consume(&r);
+	ret = cqe->result;
+	rtio_cqe_release(&r);
+
 	if (ret) {
 		LOG_ERR("Code %d", ret);
 		zassert_false(ret, "SPI transceive failed");
@@ -238,35 +209,30 @@ static int spi_null_tx_buf(struct spi_dt_spec *spec)
 	return 0;
 }
 
-static int spi_rx_half_start(struct spi_dt_spec *spec)
+static int spi_rx_half_start(struct rtio_iodev *spi_iodev)
 {
-	const struct spi_buf tx_bufs[] = {
-		{
-			.buf = buffer_tx,
-			.len = BUF_SIZE,
-		},
-	};
-	const struct spi_buf rx_bufs[] = {
-		{
-			.buf = buffer_rx,
-			.len = 8,
-		},
-	};
-	const struct spi_buf_set tx = {
-		.buffers = tx_bufs,
-		.count = ARRAY_SIZE(tx_bufs)
-	};
-	const struct spi_buf_set rx = {
-		.buffers = rx_bufs,
-		.count = ARRAY_SIZE(rx_bufs)
-	};
+	struct rtio_sqe *sqe;
+	struct rtio_cqe *cqe;
 	int ret;
+
+	sqe = rtio_sqe_acquire(&r);
+	rtio_sqe_prep_transceive(sqe, spi_iodev, RTIO_PRIO_NORM,
+				 buffer_tx, buffer_rx, 8, NULL);
+	sqe->flags |= RTIO_SQE_TRANSACTION;
+	sqe = rtio_sqe_acquire(&r);
+	rtio_sqe_prep_write(sqe, spi_iodev, RTIO_PRIO_NORM,
+				 &buffer_tx[8], BUF_SIZE-8, NULL);
+
 
 	LOG_INF("Start half start");
 
 	(void)memset(buffer_rx, 0, BUF_SIZE);
 
-	ret = spi_transceive_dt(spec, &tx, &rx);
+	rtio_submit(&r, 1);
+	cqe = rtio_cqe_consume(&r);
+	ret = cqe->result;
+	rtio_cqe_release(&r);
+
 	if (ret) {
 		LOG_ERR("Code %d", ret);
 		zassert_false(ret, "SPI transceive failed");
@@ -287,32 +253,10 @@ static int spi_rx_half_start(struct spi_dt_spec *spec)
 	return 0;
 }
 
-static int spi_rx_half_end(struct spi_dt_spec *spec)
+static int spi_rx_half_end(struct rtio_iodev *spi_iodev)
 {
-	const struct spi_buf tx_bufs[] = {
-		{
-			.buf = buffer_tx,
-			.len = BUF_SIZE,
-		},
-	};
-	const struct spi_buf rx_bufs[] = {
-		{
-			.buf = NULL,
-			.len = 8,
-		},
-		{
-			.buf = buffer_rx,
-			.len = 8,
-		},
-	};
-	const struct spi_buf_set tx = {
-		.buffers = tx_bufs,
-		.count = ARRAY_SIZE(tx_bufs)
-	};
-	const struct spi_buf_set rx = {
-		.buffers = rx_bufs,
-		.count = ARRAY_SIZE(rx_bufs)
-	};
+	struct rtio_sqe *sqe;
+	struct rtio_cqe *cqe;
 	int ret;
 
 	if (IS_ENABLED(CONFIG_SPI_STM32_DMA)) {
@@ -320,11 +264,30 @@ static int spi_rx_half_end(struct spi_dt_spec *spec)
 		return 0;
 	}
 
+	sqe = rtio_sqe_acquire(&r);
+	rtio_sqe_prep_write(sqe, spi_iodev, RTIO_PRIO_NORM,
+				 buffer_tx, 8, NULL);
+	sqe->flags |= RTIO_SQE_TRANSACTION;
+
+	sqe = rtio_sqe_acquire(&r);
+	rtio_sqe_prep_transceive(sqe, spi_iodev, RTIO_PRIO_NORM,
+				 &buffer_tx[8],
+				 buffer_rx, 8,
+				 NULL);
+
+	sqe = rtio_sqe_acquire(&r);
+	rtio_sqe_prep_write(sqe, spi_iodev, RTIO_PRIO_NORM,
+		&buffer_tx[16], BUF_SIZE-16, NULL);
+
 	LOG_INF("Start half end");
 
 	(void)memset(buffer_rx, 0, BUF_SIZE);
 
-	ret = spi_transceive_dt(spec, &tx, &rx);
+	rtio_submit(&r, 1);
+	cqe = rtio_cqe_consume(&r);
+	ret = cqe->result;
+	rtio_cqe_release(&r);
+
 	if (ret) {
 		LOG_ERR("Code %d", ret);
 		zassert_false(ret, "SPI transceive failed");
@@ -345,40 +308,10 @@ static int spi_rx_half_end(struct spi_dt_spec *spec)
 	return 0;
 }
 
-static int spi_rx_every_4(struct spi_dt_spec *spec)
+static int spi_rx_every_4(struct rtio_iodev *spi_iodev)
 {
-	const struct spi_buf tx_bufs[] = {
-		{
-			.buf = buffer_tx,
-			.len = BUF_SIZE,
-		},
-	};
-	const struct spi_buf rx_bufs[] = {
-		{
-			.buf = NULL,
-			.len = 4,
-		},
-		{
-			.buf = buffer_rx,
-			.len = 4,
-		},
-		{
-			.buf = NULL,
-			.len = 4,
-		},
-		{
-			.buf = buffer_rx + 4,
-			.len = 4,
-		},
-	};
-	const struct spi_buf_set tx = {
-		.buffers = tx_bufs,
-		.count = ARRAY_SIZE(tx_bufs)
-	};
-	const struct spi_buf_set rx = {
-		.buffers = rx_bufs,
-		.count = ARRAY_SIZE(rx_bufs)
-	};
+	struct rtio_sqe *sqe;
+	struct rtio_cqe *cqe;
 	int ret;
 
 	if (IS_ENABLED(CONFIG_SPI_STM32_DMA)) {
@@ -391,11 +324,37 @@ static int spi_rx_every_4(struct spi_dt_spec *spec)
 		return 0;
 	}
 
+	sqe = rtio_sqe_acquire(&r);
+	rtio_sqe_prep_write(sqe, spi_iodev, RTIO_PRIO_NORM,
+				 buffer_tx, 4,
+				 NULL);
+	sqe->flags |= RTIO_SQE_TRANSACTION;
+	sqe = rtio_sqe_acquire(&r);
+	rtio_sqe_prep_transceive(sqe, spi_iodev, RTIO_PRIO_NORM,
+				 &buffer_tx[4], buffer_rx, 4, NULL);
+	sqe->flags |= RTIO_SQE_TRANSACTION;
+	sqe = rtio_sqe_acquire(&r);
+	rtio_sqe_prep_write(sqe, spi_iodev, RTIO_PRIO_NORM,
+				 &buffer_tx[8], (BUF_SIZE - 8),
+				 NULL);
+	sqe->flags |= RTIO_SQE_TRANSACTION;
+	sqe = rtio_sqe_acquire(&r);
+	rtio_sqe_prep_transceive(sqe, spi_iodev, RTIO_PRIO_NORM,
+				 &buffer_tx[12], &buffer_rx[4], 4, NULL);
+	sqe->flags |= RTIO_SQE_TRANSACTION;
+	sqe = rtio_sqe_acquire(&r);
+	rtio_sqe_prep_write(sqe, spi_iodev, RTIO_PRIO_NORM,
+				 &buffer_tx[16], BUF_SIZE-16, NULL);
+
 	LOG_INF("Start every 4");
 
 	(void)memset(buffer_rx, 0, BUF_SIZE);
 
-	ret = spi_transceive_dt(spec, &tx, &rx);
+	rtio_submit(&r, 1);
+	cqe = rtio_cqe_consume(&r);
+	ret = cqe->result;
+	rtio_cqe_release(&r);
+
 	if (ret) {
 		LOG_ERR("Code %d", ret);
 		zassert_false(ret, "SPI transceive failed");
@@ -423,127 +382,13 @@ static int spi_rx_every_4(struct spi_dt_spec *spec)
 	return 0;
 }
 
-#if (CONFIG_SPI_ASYNC)
-static struct k_poll_signal async_sig = K_POLL_SIGNAL_INITIALIZER(async_sig);
-static struct k_poll_event async_evt =
-	K_POLL_EVENT_INITIALIZER(K_POLL_TYPE_SIGNAL,
-				 K_POLL_MODE_NOTIFY_ONLY,
-				 &async_sig);
-static K_SEM_DEFINE(caller, 0, 1);
-K_THREAD_STACK_DEFINE(spi_async_stack, STACK_SIZE);
-static int result = 1;
 
-static void spi_async_call_cb(struct k_poll_event *async_evt,
-			      struct k_sem *caller_sem,
-			      void *unused)
+ZTEST(spi_loopback_rtio, test_spi_loopback_rtio)
 {
-	int ret;
 
-	LOG_DBG("Polling...");
-
-	while (1) {
-		ret = k_poll(async_evt, 1, K_MSEC(200));
-		zassert_false(ret, "one or more events are not ready");
-
-		result = async_evt->signal->result;
-		k_sem_give(caller_sem);
-
-		/* Reinitializing for next call */
-		async_evt->signal->signaled = 0U;
-		async_evt->state = K_POLL_STATE_NOT_READY;
-	}
-}
-
-static int spi_async_call(struct spi_dt_spec *spec)
-{
-	const struct spi_buf tx_bufs[] = {
-		{
-			.buf = buffer_tx,
-			.len = BUF_SIZE,
-		},
-	};
-	const struct spi_buf rx_bufs[] = {
-		{
-			.buf = buffer_rx,
-			.len = BUF_SIZE,
-		},
-	};
-	const struct spi_buf_set tx = {
-		.buffers = tx_bufs,
-		.count = ARRAY_SIZE(tx_bufs)
-	};
-	const struct spi_buf_set rx = {
-		.buffers = rx_bufs,
-		.count = ARRAY_SIZE(rx_bufs)
-	};
-	int ret;
-
-	LOG_INF("Start async call");
-
-	ret = spi_transceive_signal(spec->bus, &spec->config, &tx, &rx, &async_sig);
-	if (ret == -ENOTSUP) {
-		LOG_DBG("Not supported");
-		return 0;
-	}
-
-	if (ret) {
-		LOG_ERR("Code %d", ret);
-		zassert_false(ret, "SPI transceive failed");
-		return -1;
-	}
-
-	k_sem_take(&caller, K_FOREVER);
-
-	if (result) {
-		LOG_ERR("Call code %d", ret);
-		zassert_false(result, "SPI transceive failed");
-		return -1;
-	}
-
-	LOG_INF("Passed");
-
-	return 0;
-}
-#endif
-
-static int spi_resource_lock_test(struct spi_dt_spec *lock_spec,
-				  struct spi_dt_spec *try_spec)
-{
-	lock_spec->config.operation |= SPI_LOCK_ON;
-
-	if (spi_complete_loop(lock_spec)) {
-		return -1;
-	}
-
-	if (spi_release_dt(lock_spec)) {
-		LOG_ERR("Deadlock now?");
-		zassert_false(1, "SPI release failed");
-		return -1;
-	}
-
-	if (spi_complete_loop(try_spec)) {
-		return -1;
-	}
-
-	return 0;
-}
-
-ZTEST(spi_loopback, test_spi_loopback)
-{
-#if (CONFIG_SPI_ASYNC)
-	struct k_thread async_thread;
-	k_tid_t async_thread_id;
-#endif
 	LOG_INF("SPI test on buffers TX/RX %p/%p", buffer_tx, buffer_rx);
 
-#if (CONFIG_SPI_ASYNC)
-	async_thread_id = k_thread_create(&async_thread,
-					  spi_async_stack, STACK_SIZE,
-					  (k_thread_entry_t)spi_async_call_cb,
-					  &async_evt, &caller, NULL,
-					  K_PRIO_COOP(7), 0, K_NO_WAIT);
-#endif
-	zassert_true(spi_is_ready_dt(&spi_slow), "Slow spi lookback device is not ready");
+	zassert_true(spi_is_ready_iodev(&spi_slow), "Slow spi lookback device is not ready");
 
 	LOG_INF("SPI test slow config");
 
@@ -553,14 +398,11 @@ ZTEST(spi_loopback, test_spi_loopback)
 	    spi_rx_half_start(&spi_slow) ||
 	    spi_rx_half_end(&spi_slow) ||
 	    spi_rx_every_4(&spi_slow)
-#if (CONFIG_SPI_ASYNC)
-	    || spi_async_call(&spi_slow)
-#endif
 	    ) {
 		goto end;
 	}
 
-	zassert_true(spi_is_ready_dt(&spi_fast), "Fast spi lookback device is not ready");
+	zassert_true(spi_is_ready_iodev(&spi_fast), "Fast spi lookback device is not ready");
 
 	LOG_INF("SPI test fast config");
 
@@ -570,24 +412,12 @@ ZTEST(spi_loopback, test_spi_loopback)
 	    spi_rx_half_start(&spi_fast) ||
 	    spi_rx_half_end(&spi_fast) ||
 	    spi_rx_every_4(&spi_fast)
-#if (CONFIG_SPI_ASYNC)
-	    || spi_async_call(&spi_fast)
-#endif
 	    ) {
-		goto end;
-	}
-
-	if (spi_resource_lock_test(&spi_slow, &spi_fast)) {
 		goto end;
 	}
 
 	LOG_INF("All tx/rx passed");
 end:
-#if (CONFIG_SPI_ASYNC)
-	k_thread_abort(async_thread_id);
-#else
-	;
-#endif
 }
 
 static void *spi_loopback_setup(void)
@@ -601,4 +431,4 @@ static void *spi_loopback_setup(void)
 	return NULL;
 }
 
-ZTEST_SUITE(spi_loopback, NULL, spi_loopback_setup, NULL, NULL, NULL);
+ZTEST_SUITE(spi_loopback_rtio, NULL, spi_loopback_setup, NULL, NULL, NULL);
