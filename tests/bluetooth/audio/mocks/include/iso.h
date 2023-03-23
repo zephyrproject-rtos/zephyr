67fa11f9d63b57590b07199872172f60a5aa3f0c
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2023-03-23 13:51:43 +0100

    Bluetooth: audio: ascs: Register ISO server on ASCS init
    This moves the ISO server registration to bt_ascs_init.
    When the ASCS gets cleaned up (the callbacks are unregistered), the ISO
    server gets unregistered.
    
    Signed-off-by: Mariusz Skamra <mariusz.skamra@codecoup.pl>

--- tests/bluetooth/audio/mocks/include/iso.h
+++ tests/bluetooth/audio/mocks/include/iso.h
@@ -14,11 +14,13 @@
 #define ISO_FFF_FAKES_LIST(FAKE)                                                                   \
 	FAKE(bt_iso_chan_send)                                                                     \
 	FAKE(bt_iso_server_register)                                                               \
+	FAKE(bt_iso_server_unregister)                                                             \
 	FAKE(bt_iso_chan_disconnect)                                                               \
 
 DECLARE_FAKE_VALUE_FUNC(int, bt_iso_chan_send, struct bt_iso_chan *, struct net_buf *, uint16_t,
 			uint32_t);
 DECLARE_FAKE_VALUE_FUNC(int, bt_iso_server_register, struct bt_iso_server *);
+DECLARE_FAKE_VALUE_FUNC(int, bt_iso_server_unregister, struct bt_iso_server *);
 DECLARE_FAKE_VALUE_FUNC(int, bt_iso_chan_disconnect, struct bt_iso_chan *);
 
 #endif /* MOCKS_ISO_H_ */
