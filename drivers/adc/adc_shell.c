2e20bfa115fa2dc91eab665637f638b71ae83a71
scott worley <scott.worley@microchip.com>
2023-03-24 07:22:55 -0400

    tests: adc: Microchip XEC fix ADC API test and ADC shell
    Microchip XEC ADC and ADC V2 driver were merged into one
    That PR did not change the ADC API test and ADC shell resulting
    in twister build failures. Fixed both ADC API test and ADC shell.
    
    Signed-off-by: scott worley <scott.worley@microchip.com>

--- drivers/adc/adc_shell.c
+++ drivers/adc/adc_shell.c
@@ -23,8 +23,6 @@
 #define DT_DRV_COMPAT ite_it8xxx2_adc
 #elif DT_HAS_COMPAT_STATUS_OKAY(microchip_xec_adc)
 #define DT_DRV_COMPAT microchip_xec_adc
-#elif DT_HAS_COMPAT_STATUS_OKAY(microchip_xec_adc_v2)
-#define DT_DRV_COMPAT microchip_xec_adc_v2
 #elif DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_adc)
 #define DT_DRV_COMPAT nordic_nrf_adc
 #elif DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_saadc)
