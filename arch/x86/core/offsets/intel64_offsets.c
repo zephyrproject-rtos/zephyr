3e9226d8342f6f98dccd18791fc2fff10f1511af
Peter Mitsis <peter.mitsis@intel.com>
2023-03-25 10:53:44 -0400

    arch: x86: Remove unused offset files
    Removes the files ia32_offsets.c and intel64_offsets.c as they
    are now empty and are no longer needed by the x86 version of
    the offsets.c file.
    
    Signed-off-by: Peter Mitsis <peter.mitsis@intel.com>

--- arch/x86/core/offsets/intel64_offsets.c
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2019 Intel Corp.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#ifndef _X86_OFFSETS_INC_
-#define _X86_OFFSETS_INC_
-
-#endif /* _X86_OFFSETS_INC_ */
--- arch/x86/core/offsets/offsets.c
+++ arch/x86/core/offsets/offsets.c
@@ -8,10 +8,5 @@
 #include <gen_offset.h>
 #include <kernel_offsets.h>
 
-#ifdef CONFIG_X86_64
-#include "intel64_offsets.c"
-#else
-#include "ia32_offsets.c"
-#endif
 
 GEN_ABS_SYM_END
