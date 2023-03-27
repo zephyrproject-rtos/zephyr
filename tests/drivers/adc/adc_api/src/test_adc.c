11c81021d662afe9ac73f490ad9f3d826acdf40e
Hein Wessels <heinwessels93@gmail.com>
2023-03-27 13:46:17 +0200

    tests: drivers: adc: add tests for batch_mode
    Expand the unit tests to also test batch_mode. From the outside
    the only aspect to be tested is that the callback is only called
    once, regardless of number of extra_samples or channels.
    
    Signed-off-by: Hein Wessels <heinwessels93@gmail.com>

--- tests/drivers/adc/adc_api/src/test_adc.c
+++ tests/drivers/adc/adc_api/src/test_adc.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2018 Nordic Semiconductor ASA
  * Copyright (c) 2016 Intel Corporation
+ * Copyright (c) 2023 Nobleo Technology
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -705,6 +706,68 @@ static int test_task_with_interval(void)
 ZTEST(adc_basic, test_adc_sample_with_interval)
 {
 	zassert_true(test_task_with_interval() == TC_PASS);
+}
+
+/*
+ * test_adc_sample_batch_mode
+ */
+static uint8_t test_batch_mode_callback_counter;
+
+static enum adc_action test_task_batch_mode_callback(const struct device *dev,
+						     const struct adc_sequence *sequence,
+						     uint16_t sampling_index)
+{
+	test_batch_mode_callback_counter++;
+	return ADC_ACTION_CONTINUE;
+}
+
+static int test_task_batch_mode(void)
+{
+	int ret;
+	const struct adc_sequence_options options = {
+		.interval_us     = 0,
+		.callback        = test_task_batch_mode_callback,
+		.extra_samplings = 1,  /* Non zero */
+		.batch_mode      = true,
+	};
+	const struct adc_sequence sequence = {
+		.options     = &options,
+#if defined(ADC_2ND_CHANNEL_ID)
+		/* Test with multiple channels if possible */
+		.channels    = BIT(ADC_1ST_CHANNEL_ID) |
+			       BIT(ADC_2ND_CHANNEL_ID),
+#else
+		.channels    = BIT(ADC_1ST_CHANNEL_ID),
+#endif /* defined(ADC_2ND_CHANNEL_ID) */
+		.buffer      = m_sample_buffer,
+		.buffer_size = sizeof(m_sample_buffer),
+		.resolution  = ADC_RESOLUTION,
+	};
+
+	const struct device *adc_dev = init_adc();
+
+	if (!adc_dev) {
+		return TC_FAIL;
+	}
+
+	test_batch_mode_callback_counter = 0;
+	ret = adc_read(adc_dev, &sequence);
+	zassert_equal(ret, 0, "adc_read() failed with code %d", ret);
+
+#if defined(ADC_2ND_CHANNEL_ID)
+	check_samples((1 + options.extra_samplings) * 2);
+#else
+	check_samples(1 + options.extra_samplings);
+#endif /* defined(ADC_2ND_CHANNEL_ID) */
+
+	zassert_equal(test_batch_mode_callback_counter, 1);
+
+	return TC_PASS;
+}
+
+ZTEST(adc_basic, test_adc_batch_mode)
+{
+	zassert_true(test_task_batch_mode() == TC_PASS);
 }
 
 /*
