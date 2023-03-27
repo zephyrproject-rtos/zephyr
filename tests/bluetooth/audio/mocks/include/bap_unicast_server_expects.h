64ccf9eb2ab096df90f5c9d4777e8393a6d3a465
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2023-03-27 16:00:53 +0200

    tests: Bluetooth: ascs: Add Sink ASE state machine test cases
    This adds another set of test cases to cover the ASE state
    transitions.
    
    Signed-off-by: Mariusz Skamra <mariusz.skamra@codecoup.pl>

--- tests/bluetooth/audio/mocks/include/bap_unicast_server_expects.h
+++ tests/bluetooth/audio/mocks/include/bap_unicast_server_expects.h
@@ -40,6 +40,30 @@ do {                                                                            
 		     zassert_unreachable("Not implemented");))                                     \
 } while (0)
 
+#define expect_bt_bap_unicast_server_cb_reconfig_called_once(_stream, _dir, _codec)                \
+do {                                                                                               \
+	const char *func_name = "bt_bap_unicast_server_cb.reconfig";                               \
+												   \
+	zassert_true(mock_bap_unicast_server_cb_reconfig_fake.call_count > 0,                      \
+		     "'%s()' was not called", func_name);                                          \
+	zassert_equal(1, mock_bap_unicast_server_cb_reconfig_fake.call_count,                      \
+		      "'%s()' was called more than once", func_name);                              \
+												   \
+	IF_NOT_EMPTY(_stream, (                                                                    \
+		     zassert_equal_ptr(_stream, mock_bap_unicast_server_cb_reconfig_fake.arg0_val, \
+				       "'%s()' was called with incorrect '%s' value",              \
+				       func_name, "stream");))                                     \
+												   \
+	IF_NOT_EMPTY(_dir, (                                                                       \
+		     zassert_equal(_dir, mock_bap_unicast_server_cb_reconfig_fake.arg1_val,        \
+				   "'%s()' was called with incorrect '%s' value",                  \
+				   func_name, "_dir");))                                           \
+												   \
+	IF_NOT_EMPTY(_codec, (                                                                     \
+		     /* TODO */                                                                    \
+		     zassert_unreachable("Not implemented");))                                     \
+} while (0)
+
 #define expect_bt_bap_unicast_server_cb_qos_called_once(_stream, _qos)                             \
 do {                                                                                               \
 	const char *func_name = "bt_bap_unicast_server_cb.qos";                                    \
@@ -68,6 +92,29 @@ do {                                                                            
 												   \
 	IF_NOT_EMPTY(_stream, (                                                                    \
 		     zassert_equal_ptr(_stream, mock_bap_unicast_server_cb_enable_fake.arg0_val,   \
+				       "'%s()' was called with incorrect '%s' value",              \
+				       func_name, "stream");))                                     \
+												   \
+	IF_NOT_EMPTY(_meta, (                                                                      \
+		     /* TODO */                                                                    \
+		     zassert_unreachable("Not implemented");))                                     \
+												   \
+	IF_NOT_EMPTY(_meta_count, (                                                                \
+		     /* TODO */                                                                    \
+		     zassert_unreachable("Not implemented");))                                     \
+} while (0)
+
+#define expect_bt_bap_unicast_server_cb_metadata_called_once(_stream, _meta, _meta_count)          \
+do {                                                                                               \
+	const char *func_name = "bt_bap_unicast_server_cb.enable";                                 \
+												   \
+	zassert_true(mock_bap_unicast_server_cb_metadata_fake.call_count > 0,                      \
+		     "'%s()' was not called", func_name);                                          \
+	zassert_equal(1, mock_bap_unicast_server_cb_metadata_fake.call_count,                      \
+		      "'%s()' was called more than once", func_name);                              \
+												   \
+	IF_NOT_EMPTY(_stream, (                                                                    \
+		     zassert_equal_ptr(_stream, mock_bap_unicast_server_cb_metadata_fake.arg0_val, \
 				       "'%s()' was called with incorrect '%s' value",              \
 				       func_name, "stream");))                                     \
 												   \
@@ -122,5 +169,13 @@ do {                                                                            
 				       "'%s()' was called with incorrect '%s' value",              \
 				       func_name, "stream");))                                     \
 } while (0)
+
+static void expect_bt_bap_unicast_server_cb_config_not_called(void)
+{
+	const char *func_name = "bt_bap_unicast_server_cb.config";
+
+	zassert_equal(0, mock_bap_unicast_server_cb_config_fake.call_count,
+		      "'%s()' was called unexpectedly", func_name);
+}
 
 #endif /* MOCKS_BAP_UNICAST_SERVER_EXPECTS_H_ */
