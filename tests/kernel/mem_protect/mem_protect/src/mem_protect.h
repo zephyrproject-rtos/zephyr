04fbd840317332ecf684721bdee58eff4d173876
Rick Tsao <rick592@andestech.com>
2023-03-28 10:52:32 +0800

    tests: mem_protect: Add support for configurable granularity of PMP
    Redefine MEM_REGION_ALLOC to make memory domain align with granularity
    of PMP.
    
    Signed-off-by: Rick Tsao <rick592@andestech.com>

--- tests/kernel/mem_protect/mem_protect/src/mem_protect.h
+++ tests/kernel/mem_protect/mem_protect/src/mem_protect.h
@@ -62,8 +62,8 @@ static inline void set_fault_valid(bool valid)
 #elif defined(CONFIG_ARM)
 #define MEM_REGION_ALLOC (Z_THREAD_MIN_STACK_ALIGN)
 #elif defined(CONFIG_RISCV)
-#if defined(CONFIG_PMP_NO_TOR) && defined(CONFIG_PMP_NO_NA4)
-#define MEM_REGION_ALLOC (8)
+#if defined(CONFIG_RISCV_PMP)
+#define MEM_REGION_ALLOC (CONFIG_PMP_GRANULARITY)
 #else
 #define MEM_REGION_ALLOC (4)
 #endif
