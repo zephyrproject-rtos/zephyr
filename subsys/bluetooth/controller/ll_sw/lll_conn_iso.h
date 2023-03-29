711b5dbce9d661bb7c4f4056b7b6aab16a23f60e
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2023-03-29 15:53:59 +0530

    Bluetooth: Controller: Fix implementation of CIS Null PDU Indicator use
    Fix implementation of CIS Null PDU Indicator use to meet the
    BT Specification statement, that, if the source of the data
    fails to provide a payload in time for a CIS subevent, then
    the link layer shall transmit a CIS Null PDU instead.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/lll_conn_iso.h
+++ subsys/bluetooth/controller/ll_sw/lll_conn_iso.h
@@ -39,7 +39,7 @@ struct lll_conn_iso_stream {
 	uint8_t sn:1;               /* Sequence number */
 	uint8_t nesn:1;             /* Next expected sequence number */
 	uint8_t cie:1;              /* Close isochronous event */
-	uint8_t empty:1;            /* 1 if CIS LLL has Tx-ed empty PDU */
+	uint8_t npi:1;              /* 1 if CIS LLL has Tx-ed Null PDU Indicator */
 	uint8_t flushed:1;          /* 1 if CIS LLL has been flushed */
 	uint8_t active:1;           /* 1 if CIS LLL is active */
 	uint8_t datapath_ready_rx:1;/* 1 if datapath for RX is ready */
