6719415cb0302137d9592004da8c8498e023ee3e
Keith Packard <keithp@keithp.com>
2023-03-28 21:24:23 -0700

    lib/libc: Move malloc from minimal to common library
    This will (eventually) permit use of a common malloc implementation with
    other C libraries, reducing the amount of Zephyr-specific code required
    to support each C library.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- doc/develop/languages/c/common_libc.rst
+++ doc/develop/languages/c/common_libc.rst
@@ -14,3 +14,34 @@ Time function
 This provides an implementation of the standard C function, :c:func:`time`,
 relying on the Zephyr function, :c:func:`clock_gettime`. This function can
 be enabled by selecting :kconfig:option:`COMMON_LIBC_TIME`.
+
+Dynamic Memory Management
+*************************
+
+The common dynamic memory management implementation can be enabled by
+selecting the :kconfig:option:`CONFIG_COMMON_LIBC_MALLOC` in the
+application configuration file.
+
+The common C library internally uses the :ref:`kernel memory heap API
+<heap_v2>` to manage the memory heap used by the standard dynamic memory
+management interface functions such as :c:func:`malloc` and :c:func:`free`.
+
+The internal memory heap is normally located in the ``.bss`` section. When
+userspace is enabled, however, it is placed in a dedicated memory partition
+called ``z_malloc_partition``, which can be accessed from the user mode
+threads. The size of the internal memory heap is specified by the
+:kconfig:option:`CONFIG_COMMON_LIBC_MALLOC_ARENA_SIZE`.
+
+The default heap size for applications using the common C library is zero
+(no heap). For other C library users, if there is an MMU present, then the
+default heap is 16kB. Otherwise, the heap uses all available memory.
+
+There are also separate controls to select :c:func:`calloc`
+(:kconfig:option:`COMMON_LIBC_CALLOC`) and :c:func:`reallocarray`
+(:kconfig:option:`COMMON_LIBC_REALLOCARRAY`). Both of these are enabled by
+default as that doesn't impact memory usage in applications not using them.
+
+The standard dynamic memory management interface functions implemented by
+the common C library are thread safe and may be simultaneously called by
+multiple threads. These functions are implemented in
+:file:`lib/libc/common/source/stdlib/malloc.c`.
--- lib/libc/common/source/stdlib/malloc.c
+++ lib/libc/common/source/stdlib/malloc.c
@@ -13,15 +13,16 @@
 #include <zephyr/app_memory/app_memdomain.h>
 #include <zephyr/sys/mutex.h>
 #include <zephyr/sys/sys_heap.h>
+#include <zephyr/sys/libc-hooks.h>
 #include <zephyr/types.h>
 
 #define LOG_LEVEL CONFIG_KERNEL_LOG_LEVEL
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(os, CONFIG_KERNEL_LOG_LEVEL);
 
-#ifdef CONFIG_MINIMAL_LIBC_MALLOC
+#ifdef CONFIG_COMMON_LIBC_MALLOC
 
-#if (CONFIG_MINIMAL_LIBC_MALLOC_ARENA_SIZE > 0)
+#if (CONFIG_COMMON_LIBC_MALLOC_ARENA_SIZE > 0)
 #ifdef CONFIG_USERSPACE
 K_APPMEM_PARTITION_DEFINE(z_malloc_partition);
 #define POOL_SECTION K_APP_DMEM_SECTION(z_malloc_partition)
@@ -29,7 +30,7 @@ K_APPMEM_PARTITION_DEFINE(z_malloc_partition);
 #define POOL_SECTION .bss
 #endif /* CONFIG_USERSPACE */
 
-#define HEAP_BYTES CONFIG_MINIMAL_LIBC_MALLOC_ARENA_SIZE
+#define HEAP_BYTES CONFIG_COMMON_LIBC_MALLOC_ARENA_SIZE
 
 Z_GENERIC_SECTION(POOL_SECTION) static struct sys_heap z_malloc_heap;
 Z_GENERIC_SECTION(POOL_SECTION) struct sys_mutex z_malloc_heap_mutex;
@@ -121,7 +122,7 @@ void *malloc(size_t size)
 {
 	ARG_UNUSED(size);
 
-	LOG_ERR("CONFIG_MINIMAL_LIBC_MALLOC_ARENA_SIZE is 0");
+	LOG_ERR("CONFIG_COMMON_LIBC_MALLOC_ARENA_SIZE is 0");
 	errno = ENOMEM;
 
 	return NULL;
@@ -139,9 +140,9 @@ void *realloc(void *ptr, size_t size)
 }
 #endif
 
-#endif /* CONFIG_MINIMAL_LIBC_MALLOC */
+#endif /* CONFIG_COMMON_LIBC_MALLOC */
 
-#ifdef CONFIG_MINIMAL_LIBC_CALLOC
+#ifdef CONFIG_COMMON_LIBC_CALLOC
 void *calloc(size_t nmemb, size_t size)
 {
 	void *ret;
@@ -159,19 +160,15 @@ void *calloc(size_t nmemb, size_t size)
 
 	return ret;
 }
-#endif /* CONFIG_MINIMAL_LIBC_CALLOC */
+#endif /* CONFIG_COMMON_LIBC_CALLOC */
 
-#ifdef CONFIG_MINIMAL_LIBC_REALLOCARRAY
+#ifdef CONFIG_COMMON_LIBC_REALLOCARRAY
 void *reallocarray(void *ptr, size_t nmemb, size_t size)
 {
-#if (CONFIG_MINIMAL_LIBC_MALLOC_ARENA_SIZE > 0)
 	if (size_mul_overflow(nmemb, size, &size)) {
 		errno = ENOMEM;
 		return NULL;
 	}
 	return realloc(ptr, size);
-#else
-	return NULL;
-#endif
 }
-#endif /* CONFIG_MINIMAL_LIBC_REALLOCARRAY */
+#endif /* CONFIG_COMMON_LIBC_REALLOCARRAY */
