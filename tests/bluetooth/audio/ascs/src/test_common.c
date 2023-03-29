0acc0504e9b0e3e214e7f42386921381779d04aa
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2023-03-29 10:27:12 +0200

    tests: Bluetooth: ascs: Stop using hardcoded ASE_ID in tests
    This adds ASE_ID getter function to be used in the tests. The function
    performs read operation on ASE charateristic. The characteristic value
    is ensured to be ASE Idle state characterisitic value, which carries
    information about ASE_ID value.
    
    Signed-off-by: Mariusz Skamra <mariusz.skamra@codecoup.pl>

--- tests/bluetooth/audio/ascs/src/test_common.c
+++ tests/bluetooth/audio/ascs/src/test_common.c
@@ -11,9 +11,11 @@
 #include <zephyr/bluetooth/conn.h>
 #include <zephyr/bluetooth/gatt.h>
 #include <zephyr/bluetooth/uuid.h>
+#include <zephyr/sys/util_macro.h>
 
 #include "bap_unicast_server.h"
 #include "bap_stream.h"
+#include "gatt_expects.h"
 #include "conn.h"
 
 #include "test_common.h"
@@ -82,6 +84,24 @@ uint8_t test_ase_get(const struct bt_uuid *uuid, int num_ase, ...)
 	return i;
 }
 
+uint8_t test_ase_id_get(const struct bt_gatt_attr *ase)
+{
+	const struct test_ase_chrc_value_hdr *hdr;
+	ssize_t ret;
+
+	ret = ase->read(NULL, ase, NULL, 0, 0);
+	zassert_false(ret < 0, "ase->read returned unexpected (err 0x%02x)", BT_GATT_ERR(ret));
+
+	expect_bt_gatt_attr_read_called_once(NULL, ase, EMPTY, EMPTY, 0, EMPTY, sizeof(*hdr));
+
+	hdr = bt_gatt_attr_read_fake.arg5_val;
+
+	/* Reset the mock state */
+	bt_gatt_attr_read_reset();
+
+	return hdr->ase_id;
+}
+
 static struct bt_bap_stream *stream_allocated;
 static const struct bt_codec_qos_pref qos_pref = BT_CODEC_QOS_PREF(true, BT_GAP_LE_PHY_2M,
 								   0x02, 10, 40000, 40000,
@@ -102,13 +122,14 @@ static int unicast_server_cb_config_custom_fake(struct bt_conn *conn, const stru
 	return 0;
 }
 
-void test_ase_control_client_config_codec(struct bt_conn *conn, struct bt_bap_stream *stream)
+void test_ase_control_client_config_codec(struct bt_conn *conn, uint8_t ase_id,
+					  struct bt_bap_stream *stream)
 {
 	const struct bt_gatt_attr *attr = test_ase_control_point_get();
 	const uint8_t buf[] = {
 		0x01,           /* Opcode = Config Codec */
 		0x01,           /* Number_of_ASEs */
-		0x01,           /* ASE_ID[0] */
+		ase_id,         /* ASE_ID[0] */
 		0x01,           /* Target_Latency[0] = Target low latency */
 		0x02,           /* Target_PHY[0] = LE 2M PHY */
 		0x06,           /* Codec_ID[0].Coding_Format = LC3 */
@@ -128,13 +149,13 @@ void test_ase_control_client_config_codec(struct bt_conn *conn, struct bt_bap_st
 	stream_allocated = NULL;
 }
 
-void test_ase_control_client_config_qos(struct bt_conn *conn)
+void test_ase_control_client_config_qos(struct bt_conn *conn, uint8_t ase_id)
 {
 	const struct bt_gatt_attr *attr = test_ase_control_point_get();
 	const uint8_t buf[] = {
 		0x02,                   /* Opcode = Config QoS */
 		0x01,                   /* Number_of_ASEs */
-		0x01,                   /* ASE_ID[0] */
+		ase_id,                 /* ASE_ID[0] */
 		0x01,                   /* CIG_ID[0] */
 		0x01,                   /* CIS_ID[0] */
 		0xFF, 0x00, 0x00,       /* SDU_Interval[0] */
@@ -151,13 +172,13 @@ void test_ase_control_client_config_qos(struct bt_conn *conn)
 	zassert_false(ret < 0, "attr->write returned unexpected (err 0x%02x)", BT_GATT_ERR(ret));
 }
 
-void test_ase_control_client_enable(struct bt_conn *conn)
+void test_ase_control_client_enable(struct bt_conn *conn, uint8_t ase_id)
 {
 	const struct bt_gatt_attr *attr = test_ase_control_point_get();
 	const uint8_t buf[] = {
 		0x03,                   /* Opcode = Enable */
 		0x01,                   /* Number_of_ASEs */
-		0x01,                   /* ASE_ID[0] */
+		ase_id,                 /* ASE_ID[0] */
 		0x00,                   /* Metadata_Length[0] */
 	};
 	ssize_t ret;
@@ -166,13 +187,13 @@ void test_ase_control_client_enable(struct bt_conn *conn)
 	zassert_false(ret < 0, "attr->write returned unexpected (err 0x%02x)", BT_GATT_ERR(ret));
 }
 
-void test_ase_control_client_disable(struct bt_conn *conn)
+void test_ase_control_client_disable(struct bt_conn *conn, uint8_t ase_id)
 {
 	const struct bt_gatt_attr *attr = test_ase_control_point_get();
 	const uint8_t buf[] = {
 		0x05,                   /* Opcode = Disable */
 		0x01,                   /* Number_of_ASEs */
-		0x01,                   /* ASE_ID[0] */
+		ase_id,                 /* ASE_ID[0] */
 	};
 	ssize_t ret;
 
@@ -180,13 +201,13 @@ void test_ase_control_client_disable(struct bt_conn *conn)
 	zassert_false(ret < 0, "attr->write returned unexpected (err 0x%02x)", BT_GATT_ERR(ret));
 }
 
-void test_ase_control_client_release(struct bt_conn *conn)
+void test_ase_control_client_release(struct bt_conn *conn, uint8_t ase_id)
 {
 	const struct bt_gatt_attr *attr = test_ase_control_point_get();
 	const uint8_t buf[] = {
 		0x08,                   /* Opcode = Disable */
 		0x01,                   /* Number_of_ASEs */
-		0x01,                   /* ASE_ID[0] */
+		ase_id,                 /* ASE_ID[0] */
 	};
 	ssize_t ret;
 
@@ -194,13 +215,13 @@ void test_ase_control_client_release(struct bt_conn *conn)
 	zassert_false(ret < 0, "attr->write returned unexpected (err 0x%02x)", BT_GATT_ERR(ret));
 }
 
-void test_ase_control_client_update_metadata(struct bt_conn *conn)
+void test_ase_control_client_update_metadata(struct bt_conn *conn, uint8_t ase_id)
 {
 	const struct bt_gatt_attr *attr = test_ase_control_point_get();
 	const uint8_t buf[] = {
 		0x07,                   /* Opcode = Update Metadata */
 		0x01,                   /* Number_of_ASEs */
-		0x01,                   /* ASE_ID[0] */
+		ase_id,                 /* ASE_ID[0] */
 		0x03,                   /* Metadata_Length[0] */
 		0x02, 0x02, 0x04,       /* Metadata[0] = Streaming Context (Media) */
 	};
