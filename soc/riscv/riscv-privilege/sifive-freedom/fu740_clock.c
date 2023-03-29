d43e3b95523b474839953b1017de1fc80fa7917b
Franciszek Zdobylak <fzdobylak@antmicro.com>
2023-03-29 07:36:21 +0200

    soc: riscv: sifive: fu740: add DDR clk
    Add code that initializes DDR clk alongside other clocks.
    
    Signed-off-by: Franciszek Zdobylak <fzdobylak@antmicro.com>

--- soc/riscv/riscv-privilege/sifive-freedom/fu740_clock.c
+++ soc/riscv/riscv-privilege/sifive-freedom/fu740_clock.c
@@ -15,10 +15,17 @@ BUILD_ASSERT(MHZ(1000) == DT_PROP(DT_NODELABEL(coreclk), clock_frequency),
 BUILD_ASSERT(KHZ(125125) == DT_PROP(DT_NODELABEL(pclk), clock_frequency),
 	"Unsupported PCLK frequency");
 
+static inline void wait_controller_cycle(void)
+{
+	/* HACK to get the '1 full controller clock cycle'. */
+	__asm__ volatile ("fence");
+}
+
 /*
  * Switch the clock source
  *   - core: to 1GHz PLL (CORE_PLL) from 26MHz oscillator (HFCLK)
  *   - peri: to 250MHz PLL (HFPCLKPLL) from HFCLK
+ *   - ddr:  to 923MHz PLL (DDRPLL) from HFCLK (half of the data rate)
  * on the HiFive Unmatched board.
  *
  * Note: Valid PLL VCO range is 2400MHz to 4800MHz
@@ -55,6 +62,34 @@ static int fu740_clock_init(const struct device *dev)
 	PRCI_REG(PRCI_HFPCLKPLLOUTDIV) = OUTDIV_PLLCKE(OUTDIV_PLLCKE_ENA);
 	PRCI_REG(PRCI_HFPCLKPLLSEL) = CLKSEL_SEL(CLKSEL_PLL);
 
+	PRCI_REG(PRCI_DDRPLLCFG) =
+		PLL_R(0) |   /* input divider: Fin / (0 + 1) = 26MHz */
+		PLL_F(70) |  /* VCO: 2 x (70 + 1) = 154 = 1872MHz */
+		PLL_Q(2) |   /* output divider: VCO / 2^2 = 936MHz */
+		PLL_RANGE(PLL_RANGE_18MHZ) |
+		PLL_BYPASS(PLL_BYPASS_DISABLE) |
+		PLL_FSE(PLL_FSE_INTERNAL);
+	while ((PRCI_REG(PRCI_DDRPLLCFG) & PLL_LOCK(1)) == 0)
+		;
+
+	PRCI_REG(PRCI_DDRPLLOUTDIV) |= OUTDIV_PLLCKE(OUTDIV_PLLCKE_ENA);
+
+	PRCI_REG(PRCI_DEVICESRESETN) |= DEVICERESETN_DDRCTRL;
+	wait_controller_cycle();
+
+	/* Release DDR reset */
+	PRCI_REG(PRCI_DEVICESRESETN) |= DEVICERESETN_DDRAXI |
+					DEVICERESETN_DDRAHB |
+					DEVICERESETN_DDRPHY;
+	wait_controller_cycle();
+
+	/*
+	 * These take like 16 cycles to actually propagate. We can't go sending stuff before they
+	 * come out of reset. So wait.
+	 */
+	for (int i = 0; i < 256; i++) {
+		__asm__ volatile ("nop");
+	}
 	return 0;
 }
 
