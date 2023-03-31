f90a479ee9b9d3bfbb2fe69a57f0daf8b8ce9076
Roman Dobrodii <rdobrodii@antmicro.com>
2023-03-31 17:14:06 +0200

    boards/arm/efr32_thunderboard: use silabs_commander for BG27
    Use Silabs Commander to flash BG27 chip since it can't use J-Link
    as of today.
    
    Signed-off-by: Roman Dobrodii <rdobrodii@antmicro.com>

--- boards/arm/efr32_thunderboard/board.cmake
+++ boards/arm/efr32_thunderboard/board.cmake
@@ -2,6 +2,10 @@
 
 if(CONFIG_BOARD_EFR32BG22_BRD4184A)
 board_runner_args(jlink "--device=EFR32BG22C224F512IM40" "--reset-after-load")
-endif() # CONFIG_BOARD_EFR32BG22_BRD4184A
-
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+
+elseif(CONFIG_BOARD_EFR32BG27_BRD2602A)
+board_runner_args(silabs_commander "--device=EFR32BG27C140F768IM40")
+include(${ZEPHYR_BASE}/boards/common/silabs_commander.board.cmake)
+
+endif()
