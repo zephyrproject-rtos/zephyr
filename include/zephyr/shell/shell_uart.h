9832a3edc055b40d0ec9427aec2777b06436b1b5
Tomasz Moń <tomasz.mon@nordicsemi.no>
2023-04-03 07:32:15 +0200

    shell: uart: Fix wait for DTR before TX
    Fail compilation when incorrect configuration is detected, i.e. when
    SHELL_BACKEND_SERIAL_CHECK_DTR is set but UART_LINE_CTRL is not set.
    
    Use periodic timer to wait for DTR instead of waiting in uart callback
    to prevent blocking caller workqueue and/or sleeping in ISR.
    
    DTR check was only ever supported with interrupt driven backend so add
    appropriate depends on to Kconfig.
    
    Fixes: 481acb93cf2c ("shell: uart: Add waiting on DTR signal before
    sending data")
    Fixes: #47120
    Fixes: #54705
    
    Signed-off-by: Tomasz Moń <tomasz.mon@nordicsemi.no>

--- include/zephyr/shell/shell_uart.h
+++ include/zephyr/shell/shell_uart.h
@@ -39,17 +39,23 @@ struct shell_uart_ctrl_blk {
 
 #define Z_UART_SHELL_RX_TIMER_PTR(_name) NULL
 
+#define Z_UART_SHELL_DTR_TIMER_DECLARE(_name) static struct k_timer _name##_dtr_timer
+#define Z_UART_SHELL_DTR_TIMER_PTR(_name) (&_name##_dtr_timer)
+
 #else /* CONFIG_SHELL_BACKEND_SERIAL_INTERRUPT_DRIVEN */
 #define Z_UART_SHELL_TX_RINGBUF_DECLARE(_name, _size) /* Empty */
 #define Z_UART_SHELL_RX_TIMER_DECLARE(_name) static struct k_timer _name##_timer
 #define Z_UART_SHELL_TX_RINGBUF_PTR(_name) NULL
 #define Z_UART_SHELL_RX_TIMER_PTR(_name) (&_name##_timer)
+#define Z_UART_SHELL_DTR_TIMER_DECLARE(_name) /* Empty */
+#define Z_UART_SHELL_DTR_TIMER_PTR(_name) NULL
 #endif /* CONFIG_SHELL_BACKEND_SERIAL_INTERRUPT_DRIVEN */
 
 /** @brief Shell UART transport instance structure. */
 struct shell_uart {
 	struct shell_uart_ctrl_blk *ctrl_blk;
 	struct k_timer *timer;
+	struct k_timer *dtr_timer;
 	struct ring_buf *tx_ringbuf;
 	struct ring_buf *rx_ringbuf;
 };
@@ -58,11 +64,13 @@ struct shell_uart {
 #define SHELL_UART_DEFINE(_name, _tx_ringbuf_size, _rx_ringbuf_size)	\
 	static struct shell_uart_ctrl_blk _name##_ctrl_blk;		\
 	Z_UART_SHELL_RX_TIMER_DECLARE(_name);				\
+	Z_UART_SHELL_DTR_TIMER_DECLARE(_name);				\
 	Z_UART_SHELL_TX_RINGBUF_DECLARE(_name, _tx_ringbuf_size);	\
 	RING_BUF_DECLARE(_name##_rx_ringbuf, _rx_ringbuf_size);		\
 	static const struct shell_uart _name##_shell_uart = {		\
 		.ctrl_blk = &_name##_ctrl_blk,				\
 		.timer = Z_UART_SHELL_RX_TIMER_PTR(_name),		\
+		.dtr_timer = Z_UART_SHELL_DTR_TIMER_PTR(_name),		\
 		.tx_ringbuf = Z_UART_SHELL_TX_RINGBUF_PTR(_name),	\
 		.rx_ringbuf = &_name##_rx_ringbuf,			\
 	};								\
