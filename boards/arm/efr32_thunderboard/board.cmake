5d4bc3bf9fff214e5d4741948febcb8762531b26
Krzysztof Boronski <kboronski@antmicro.com>
2023-04-04 13:43:47 +0200

    boards: arm: Renamed efr32bg22_sltb010a and updated docs
    Vendor's P/Ns are volatile and there isn't one yet assigned to the new
    BG27 board. Thunderboard-style boards are now all named after the number on
    printed on their PCBs.
    
    Signed-off-by: Krzysztof Boronski <kboronski@antmicro.com>

--- boards/arm/efr32_thunderboard/board.cmake
+++ boards/arm/efr32_thunderboard/board.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
-if(CONFIG_BOARD_EFR32BG_SLTB010A)
+if(CONFIG_BOARD_EFR32BG22_BRD4184A)
 board_runner_args(jlink "--device=EFR32BG22C224F512IM40" "--reset-after-load")
-endif() # CONFIG_BOARD_EFR32BG_SLTB010A
+endif() # CONFIG_BOARD_EFR32BG22_BRD4184A
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
