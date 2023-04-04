1590ef917a24e94b993ad9a141062f4b324718b7
Anas Nashif <anas.nashif@intel.com>
2023-04-04 11:00:38 +0000

    ztest: add macro for custom testcase result
    Some cases, especially those dealing with faults need to inject output
    manually, so provied a macro for special case.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- subsys/testsuite/include/zephyr/tc_util.h
+++ subsys/testsuite/include/zephyr/tc_util.h
@@ -189,6 +189,11 @@ static inline void print_nothing(const char *fmt, ...)
 	Z_TC_END_RESULT((result), __func__)
 #endif
 
+#ifndef TC_END_RESULT_CUSTOM
+#define TC_END_RESULT_CUSTOM(result, func)                           \
+	Z_TC_END_RESULT((result), func)
+#endif
+
 #ifndef TC_SUITE_PRINT
 #define TC_SUITE_PRINT(fmt, ...) PRINT_DATA(fmt, ##__VA_ARGS__)
 #endif
