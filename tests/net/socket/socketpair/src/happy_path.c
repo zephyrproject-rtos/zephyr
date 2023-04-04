232810161920b3635df117a019205f63ec6b3655
Anas Nashif <anas.nashif@intel.com>
2023-04-04 11:51:08 +0000

    tests: net: socketpair: prefix tests with test_
    tests need to be prefixed with test_ or else we will not be able to
    parse output correctly.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/net/socket/socketpair/src/happy_path.c
+++ tests/net/socket/socketpair/src/happy_path.c
@@ -119,7 +119,7 @@ static void happy_path(
 	zassert_equal(res, 0, "close failed");
 }
 
-ZTEST_USER(net_socketpair, AF_LOCAL__SOCK_STREAM__0)
+ZTEST_USER(net_socketpair, test_AF_LOCAL__SOCK_STREAM__0)
 {
 	happy_path(
 		AF_LOCAL, "AF_LOCAL",
@@ -128,7 +128,7 @@ ZTEST_USER(net_socketpair, AF_LOCAL__SOCK_STREAM__0)
 	);
 }
 
-ZTEST_USER(net_socketpair, AF_UNIX__SOCK_STREAM__0)
+ZTEST_USER(net_socketpair, test_AF_UNIX__SOCK_STREAM__0)
 {
 	happy_path(
 		AF_UNIX, "AF_UNIX",
