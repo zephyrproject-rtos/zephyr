6f24edf5c8ae5062c26cf4a344eefb9fd74c3e3b
Mykola Kvach <mykola_kvach@epam.com>
2023-04-04 19:29:45 +0300

    drivers: rcar_cpg: add generic Renesas functions for get/set rate
    Add associated tables of clocks and API for working with these tables,
    from this moment the relationship between clocks and their divider are
    built. After set rate of some Core clock, driver has to update all
    in/out rates of all childrens recursively. During get/set rate calls
    if out rate is unknown, we try to get parent in/out rates and its
    divider, in case when parent doesn't have valid in/out rates we get
    parent of parent and so on until we get parent with a valid in or out
    rates.
    
    Add generic Renesas functions for get/set rate of CPG.
    
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>

--- drivers/clock_control/clock_control_renesas_cpg_mssr.h
+++ drivers/clock_control/clock_control_renesas_cpg_mssr.h
@@ -7,6 +7,73 @@
 #ifndef ZEPHYR_DRIVERS_RENESAS_RENESAS_CPG_MSSR_H_
 #define ZEPHYR_DRIVERS_RENESAS_RENESAS_CPG_MSSR_H_
 
+#include <zephyr/spinlock.h>
+#include <zephyr/sys/sys_io.h>
+#include <zephyr/drivers/clock_control.h>
+
+#define CPG_NUM_DOMAINS 2
+
+struct cpg_clk_info_table {
+	uint32_t domain;
+	uint32_t module;
+	mem_addr_t offset;
+	uint32_t parent_id;
+
+	int64_t in_freq;
+	int64_t out_freq;
+
+	/* TODO: add setting of this field and add function for getting status */
+	enum clock_control_status status;
+
+	struct cpg_clk_info_table *parent;
+	struct cpg_clk_info_table *children_list;
+	struct cpg_clk_info_table *next_sibling;
+};
+
+struct rcar_cpg_mssr_data {
+	mem_addr_t base_addr;
+
+	struct cpg_clk_info_table *clk_info_table[CPG_NUM_DOMAINS];
+	const uint32_t clk_info_table_size[CPG_NUM_DOMAINS];
+
+	struct k_spinlock lock;
+
+	uint32_t (*get_div_helper)(uint32_t reg, uint32_t module);
+	int (*set_rate_helper)(uint32_t module, uint32_t *div, uint32_t *div_mask);
+};
+
+#define RCAR_CPG_NONE -1
+#define RCAR_CPG_KHZ(khz) ((khz) * 1000U)
+#define RCAR_CPG_MHZ(mhz) (RCAR_CPG_KHZ(mhz) * 1000U)
+
+#define RCAR_CORE_CLK_INFO_ITEM(id, off, par_id, in_frq)	\
+	{							\
+		.domain		= CPG_CORE,			\
+		.module		= id,				\
+		.offset		= off,				\
+		.parent_id	= par_id,			\
+		.in_freq	= in_frq,			\
+		.out_freq	= RCAR_CPG_NONE,		\
+		.status		= CLOCK_CONTROL_STATUS_UNKNOWN,	\
+		.parent		= NULL,				\
+		.children_list	= NULL,				\
+		.next_sibling	= NULL,				\
+	}
+
+#define RCAR_MOD_CLK_INFO_ITEM(id, par_id)			\
+	{							\
+		.domain		= CPG_MOD,			\
+		.module		= id,				\
+		.offset		= RCAR_CPG_NONE,		\
+		.parent_id	= par_id,			\
+		.in_freq	= RCAR_CPG_NONE,		\
+		.out_freq	= RCAR_CPG_NONE,		\
+		.status		= CLOCK_CONTROL_STATUS_UNKNOWN,	\
+		.parent		= NULL,				\
+		.children_list	= NULL,				\
+		.next_sibling	= NULL,				\
+	}
+
 #ifdef CONFIG_SOC_SERIES_RCAR_GEN3
 /* Software Reset Clearing Register offsets */
 #define SRSTCLR(i)      (0x940 + (i) * 4)
@@ -46,5 +113,18 @@ static const uint16_t srcr[] = {
 void rcar_cpg_write(uint32_t base_address, uint32_t reg, uint32_t val);
 
 int rcar_cpg_mstp_clock_endisable(uint32_t base_address, uint32_t module, bool enable);
+
+struct cpg_clk_info_table *rcar_cpg_find_clk_info_by_module_id(const struct device *dev,
+							       uint32_t domain,
+							       uint32_t id);
+
+void rcar_cpg_build_clock_relationship(const struct device *dev);
+
+void rcar_cpg_update_all_in_out_freq(const struct device *dev);
+
+int rcar_cpg_get_rate(const struct device *dev, clock_control_subsys_t sys, uint32_t *rate);
+
+int rcar_cpg_set_rate(const struct device *dev, clock_control_subsys_t sys,
+		      clock_control_subsys_rate_t rate);
 
 #endif /* ZEPHYR_DRIVERS_RENESAS_RENESAS_CPG_MSSR_H_ */
