bb98f48ae62aea5239c4def83c53422248513400
Krzysztof Boronski <kboronski@antmicro.com>
2023-04-05 15:21:57 +0200

    boards: arm: Create Thunderboard category
    The general structure of efr32b27_sltb010a board is shared by more than one
    board. This commit intrduces changes to the organization of board files,
    which aim to take that into account.
    
    Signed-off-by: Krzysztof Boronski <kboronski@antmicro.com>

--- boards/arm/efr32_thunderboard/board.cmake
+++ boards/arm/efr32_thunderboard/board.cmake
@@ -1,7 +1,7 @@
-# Copyright (c) 2021, Sateesh Kotapati
-#
 # SPDX-License-Identifier: Apache-2.0
-#
 
+if(CONFIG_BOARD_EFR32BG_SLTB010A)
 board_runner_args(jlink "--device=EFR32BG22C224F512IM40" "--reset-after-load")
+endif() # CONFIG_BOARD_EFR32BG_SLTB010A
+
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
