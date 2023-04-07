06de8d80e2fec50945baa5ace1a060914a0e4f1e
Thomas Stranger <thomas.stranger@outlook.com>
2023-04-07 12:04:24 +0200

    soc: arm: nordic_nrf: use aarch32 common nmi init definitions
    Remove per soc definitions that are already defined in the exact same
    way in include/zephyr/arch/arm/aarch32/nmi.h.
    
    Signed-off-by: Thomas Stranger <thomas.stranger@outlook.com>

--- soc/arm/nordic_nrf/nrf51/soc.c
+++ soc/arm/nordic_nrf/nrf51/soc.c
@@ -15,16 +15,10 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/init.h>
+#include <zephyr/arch/arm/aarch32/nmi.h>
 #include <hal/nrf_power.h>
 #include <soc/nrfx_coredep.h>
 #include <zephyr/logging/log.h>
-
-#ifdef CONFIG_RUNTIME_NMI
-extern void z_arm_nmi_init(void);
-#define NMI_INIT() z_arm_nmi_init()
-#else
-#define NMI_INIT()
-#endif
 
 #include <system_nrf51.h>
 #define LOG_LEVEL CONFIG_SOC_LOG_LEVEL
--- soc/arm/nordic_nrf/nrf91/soc.c
+++ soc/arm/nordic_nrf/nrf91/soc.c
@@ -15,15 +15,9 @@
 #include <zephyr/kernel.h>
 #include <zephyr/init.h>
 #include <zephyr/arch/arm/aarch32/cortex_m/cmsis.h>
+#include <zephyr/arch/arm/aarch32/nmi.h>
 #include <soc/nrfx_coredep.h>
 #include <zephyr/logging/log.h>
-
-#ifdef CONFIG_RUNTIME_NMI
-extern void z_arm_nmi_init(void);
-#define NMI_INIT() z_arm_nmi_init()
-#else
-#define NMI_INIT()
-#endif
 
 #if defined(CONFIG_SOC_NRF9160)
 #include <system_nrf9160.h>
