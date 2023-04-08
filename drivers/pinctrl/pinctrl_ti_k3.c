bb8684fb6fca5ae2aaef96080c2f34ef9f0c7242
Grant Ramsay <gramsay@enphaseenergy.com>
2023-04-09 08:49:04 +1200

    drivers: pinctrl: Convert TI K3 pinctrl driver to multi-instance
    K3 devices have multiple pinctrl peripherals
    
    Signed-off-by: Grant Ramsay <gramsay@enphaseenergy.com>

--- drivers/pinctrl/pinctrl_ti_k3.c
+++ drivers/pinctrl/pinctrl_ti_k3.c
@@ -9,25 +9,20 @@
 #include <zephyr/arch/cpu.h>
 #include <zephyr/drivers/pinctrl.h>
 
-#define PINCTRL_NODE DT_NODELABEL(pinctrl)
-
-static struct pinctrl_ti_k3_dev_data {
+struct pinctrl_ti_k3_dev_data {
 	DEVICE_MMIO_RAM;
-} pinctrl_ti_k3_dev;
+};
 
-static struct pinctrl_ti_k3_cfg_data {
+struct pinctrl_ti_k3_cfg_data {
 	DEVICE_MMIO_ROM;
-} pinctrl_ti_k3_cfg = {
-	DEVICE_MMIO_ROM_INIT(PINCTRL_NODE)
 };
 
 int pinctrl_configure_pins(const pinctrl_soc_pin_t *pins, uint8_t pin_cnt, uintptr_t reg)
 {
 	ARG_UNUSED(reg);
-	const struct device *dev = DEVICE_DT_GET(PINCTRL_NODE);
-	uintptr_t virt_reg_base = DEVICE_MMIO_GET(dev);
 
 	for (uint8_t i = 0; i < pin_cnt; i++) {
+		uintptr_t virt_reg_base = DEVICE_MMIO_GET(pins[i].dev);
 		sys_write32(pins[i].value, virt_reg_base + pins[i].offset);
 	}
 
@@ -40,11 +35,20 @@ static int pinctrl_ti_k3_init(const struct device *dev)
 	return 0;
 }
 
-DEVICE_DT_DEFINE(PINCTRL_NODE,
-		 pinctrl_ti_k3_init,
-		 NULL,
-		 &pinctrl_ti_k3_dev,
-		 &pinctrl_ti_k3_cfg,
-		 PRE_KERNEL_1,
-		 CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
-		 NULL);
+#define PINCTRL_TI_K3_INIT(n)                           \
+	static struct pinctrl_ti_k3_dev_data            \
+		pinctrl_ti_k3_dev_##n = {};             \
+	static struct pinctrl_ti_k3_cfg_data            \
+		pinctrl_ti_k3_cfg_##n = {               \
+		DEVICE_MMIO_ROM_INIT(DT_DRV_INST(n))    \
+	};                                              \
+	DEVICE_DT_INST_DEFINE(n,                        \
+		pinctrl_ti_k3_init,                     \
+		NULL,                                   \
+		&pinctrl_ti_k3_dev_##n,                 \
+		&pinctrl_ti_k3_cfg_##n,                 \
+		PRE_KERNEL_1,                           \
+		CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,    \
+		NULL);
+
+DT_INST_FOREACH_STATUS_OKAY(PINCTRL_TI_K3_INIT)
--- soc/arm64/ti_sitara/pinctrl_soc.h
+++ soc/arm64/ti_sitara/pinctrl_soc.h
@@ -12,6 +12,7 @@ extern "C" {
 #endif
 
 struct pinctrl_soc_pin {
+	const struct device *dev;
 	uint32_t offset;
 	uint32_t value;
 };
@@ -20,6 +21,7 @@ typedef struct pinctrl_soc_pin pinctrl_soc_pin_t;
 
 #define TI_SITARA_DT_PIN(node_id)				\
 	{							\
+		.dev = DEVICE_DT_GET(DT_PARENT(node_id)),	\
 		.offset = DT_PROP_BY_IDX(node_id, pinmux, 0),	\
 		.value = DT_PROP_BY_IDX(node_id, pinmux, 1)	\
 	},
