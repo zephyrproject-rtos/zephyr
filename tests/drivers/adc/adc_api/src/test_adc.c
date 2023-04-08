e25c687d638a4b79559565f0df2b0efa34c7c9a6
Benjamin Björnsson <benjamin.bjornsson@gmail.com>
2023-04-08 21:32:15 +0200

    tests: drivers: adc: adc_api: Move compat nuvoton,npcx-adc to DT
    Move channel description to devicetree for nuvoton,npcx-adc
    compatible to make the test more readable.
    
    Signed-off-by: Benjamin Björnsson <benjamin.bjornsson@gmail.com>

--- tests/drivers/adc/adc_api/src/test_adc.c
+++ tests/drivers/adc/adc_api/src/test_adc.c
@@ -338,16 +338,6 @@
 #define ADC_1ST_CHANNEL_INPUT	MCUX_LPADC_CH0A
 #define ADC_2ND_CHANNEL_INPUT	MCUX_LPADC_CH1A
 
-#elif defined(CONFIG_BOARD_NPCX7M6FB_EVB) || \
-	defined(CONFIG_BOARD_NPCX9M6F_EVB)
-#define ADC_DEVICE_NODE		DT_INST(0, nuvoton_npcx_adc)
-#define ADC_RESOLUTION		10
-#define ADC_GAIN		ADC_GAIN_1
-#define ADC_REFERENCE		ADC_REF_INTERNAL
-#define ADC_ACQUISITION_TIME	ADC_ACQ_TIME_DEFAULT
-#define ADC_1ST_CHANNEL_ID	0
-#define ADC_2ND_CHANNEL_ID	2
-
 #endif
 
 /* Invalid value that is not supposed to be written by the driver. It is used
