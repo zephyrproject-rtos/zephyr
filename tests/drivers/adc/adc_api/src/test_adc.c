25ca3629071f4c6b262283d5f29a116ad196c641
Benjamin Björnsson <benjamin.bjornsson@gmail.com>
2023-04-08 21:38:35 +0200

    tests: drivers: adc: adc_api: Move compat microchip,xec-adc to DT
    Move channel description to devicetree for microchip,xec-adc
    compatible to make the test more readable.
    
    Signed-off-by: Benjamin Björnsson <benjamin.bjornsson@gmail.com>

--- tests/drivers/adc/adc_api/src/test_adc.c
+++ tests/drivers/adc/adc_api/src/test_adc.c
@@ -311,17 +311,6 @@
 #define ADC_1ST_CHANNEL_ID	0
 #define ADC_2ND_CHANNEL_ID	1
 
-#elif defined(CONFIG_BOARD_MEC15XXEVB_ASSY6853) || \
-	defined(CONFIG_BOARD_MEC1501MODULAR_ASSY6885) || \
-	defined(CONFIG_BOARD_MEC172XEVB_ASSY6906)
-#define ADC_DEVICE_NODE		DT_INST(0, microchip_xec_adc)
-#define ADC_RESOLUTION		12
-#define ADC_GAIN		ADC_GAIN_1
-#define ADC_REFERENCE		ADC_REF_INTERNAL
-#define ADC_ACQUISITION_TIME	ADC_ACQ_TIME_DEFAULT
-#define ADC_1ST_CHANNEL_ID	4
-#define ADC_2ND_CHANNEL_ID	5
-
 #endif
 
 /* Invalid value that is not supposed to be written by the driver. It is used
