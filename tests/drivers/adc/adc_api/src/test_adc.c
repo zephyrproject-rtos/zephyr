65a974e07f45394d3fd0e52b8f4ebece8af14f59
Benjamin Björnsson <benjamin.bjornsson@gmail.com>
2023-04-08 21:40:10 +0200

    tests: drivers: adc: adc_api: Move compat nxp,kinetis-adc12 to DT
    Move channel description to devicetree for nxp,kinetis-adc12
    compatible to make the test more readable.
    
    Signed-off-by: Benjamin Björnsson <benjamin.bjornsson@gmail.com>

--- tests/drivers/adc/adc_api/src/test_adc.c
+++ tests/drivers/adc/adc_api/src/test_adc.c
@@ -302,15 +302,6 @@
 #define ADC_1ST_CHANNEL_ID	12
 #define ADC_2ND_CHANNEL_ID	15
 
-#elif defined(CONFIG_BOARD_TWR_KE18F)
-#define ADC_DEVICE_NODE		DT_INST(0, nxp_kinetis_adc12)
-#define ADC_RESOLUTION		12
-#define ADC_GAIN		ADC_GAIN_1
-#define ADC_REFERENCE		ADC_REF_INTERNAL
-#define ADC_ACQUISITION_TIME	ADC_ACQ_TIME_DEFAULT
-#define ADC_1ST_CHANNEL_ID	0
-#define ADC_2ND_CHANNEL_ID	1
-
 #endif
 
 /* Invalid value that is not supposed to be written by the driver. It is used
