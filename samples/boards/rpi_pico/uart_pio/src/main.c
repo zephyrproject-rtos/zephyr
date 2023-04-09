1c399a58018d9d916e8df1d4c6a0384fdc4839e5
Ionut Catalin Pavel <iocapa@iocapa.com>
2023-04-09 14:59:40 +0300

    samples: boards: rpi_pico: Added PIO shell uart example
    Simple example showing how to use the PIO based uart as a
    console / log backend.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>
    Signed-off-by: Yonatan Schachter <yonatan.schachter@gmail.com>
    Signed-off-by: Ionut Catalin Pavel <iocapa@iocapa.com>

--- samples/boards/rpi_pico/uart_pio/src/main.c
+++ samples/boards/rpi_pico/uart_pio/src/main.c
@@ -1,36 +1,19 @@
 /*
+ * Copyright (c) 2023 Ionut Pavel <iocapa@iocapa.com>
  * Copyright (c) 2023 Yonatan Schachter
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/uart.h>
 
-int main(void)
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main);
+
+void main(void)
 {
-	char data;
-	const struct device *uart0 = DEVICE_DT_GET(DT_NODELABEL(pio1_uart0));
-	const struct device *uart1 = DEVICE_DT_GET(DT_NODELABEL(pio1_uart1));
-
-	if (!device_is_ready(uart0)) {
-		return 0;
-	}
-
-	if (!device_is_ready(uart1)) {
-		return 0;
-	}
-
 	while (1) {
-		if (!uart_poll_in(uart0, &data)) {
-			uart_poll_out(uart0, data);
-		}
-
-		if (!uart_poll_in(uart1, &data)) {
-			uart_poll_out(uart1, data);
-		}
+		k_msleep(1000);
+		LOG_INF("One second passed...");
 	}
-
-	return 0;
 }
