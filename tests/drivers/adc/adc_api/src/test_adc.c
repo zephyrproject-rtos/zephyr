3d93116b1ceb8c9b3f200302bc5c0f0c449e291e
Benjamin Björnsson <benjamin.bjornsson@gmail.com>
2023-04-10 16:38:00 +0200

    tests: drivers: adc: adc_api: Move compat raspberrypi,pico-adc to DT
    Move channel description to devicetree for raspberrypi,pico-adc
    compatible to make the test more readable.
    
    Signed-off-by: Benjamin Björnsson <benjamin.bjornsson@gmail.com>

--- tests/drivers/adc/adc_api/src/test_adc.c
+++ tests/drivers/adc/adc_api/src/test_adc.c
@@ -394,17 +394,6 @@
 #define ADC_1ST_CHANNEL_ID	0
 #define ADC_2ND_CHANNEL_ID  1
 
-#elif defined(CONFIG_BOARD_RPI_PICO) || \
-	defined(CONFIG_BOARD_ADAFRUIT_KB2040) || \
-	defined(CONFIG_BOARD_SPARKFUN_PRO_MICRO_RP2040)
-#define ADC_DEVICE_NODE		DT_INST(0, raspberrypi_pico_adc)
-#define ADC_RESOLUTION		12
-#define ADC_GAIN		ADC_GAIN_1
-#define ADC_REFERENCE		ADC_REF_INTERNAL
-#define ADC_ACQUISITION_TIME	ADC_ACQ_TIME_DEFAULT
-#define ADC_1ST_CHANNEL_ID	0
-#define ADC_2ND_CHANNEL_ID	1
-
 #endif
 
 /* Invalid value that is not supposed to be written by the driver. It is used
