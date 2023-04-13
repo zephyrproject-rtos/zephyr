e51ee225c5dc56e9014ab65d87d9119475b08aed
Thomas Stranger <thomas.stranger@outlook.com>
2023-04-13 09:47:53 +0200

    drivers: clock_control: stm32 common: implement get_status api
    The get_status api allows to query whether clock is turned on or off.
    Like the get_rate call, the status of gated clocks (for specific
    peripherals), as well as domain clock sources(HSE, PCLK, PLL_P,..)
    can be checked this way.
    
    Signed-off-by: Thomas Stranger <thomas.stranger@outlook.com>

--- drivers/clock_control/clock_stm32_ll_common.c
+++ drivers/clock_control/clock_stm32_ll_common.c
@@ -418,10 +418,36 @@ static int stm32_clock_control_get_subsys_rate(const struct device *clock,
 	return 0;
 }
 
+static enum clock_control_status stm32_clock_control_get_status(const struct device *dev,
+								clock_control_subsys_t sub_system)
+{
+	struct stm32_pclken *pclken = (struct stm32_pclken *)sub_system;
+
+	ARG_UNUSED(dev);
+
+	if (IN_RANGE(pclken->bus, STM32_PERIPH_BUS_MIN, STM32_PERIPH_BUS_MAX) == true) {
+		/* Gated clocks */
+		if ((sys_read32(DT_REG_ADDR(DT_NODELABEL(rcc)) + pclken->bus) & pclken->enr)
+		    == pclken->enr) {
+			return CLOCK_CONTROL_STATUS_ON;
+		} else {
+			return CLOCK_CONTROL_STATUS_OFF;
+		}
+	} else {
+		/* Domain clock sources */
+		if (enabled_clock(pclken->bus) == 0) {
+			return CLOCK_CONTROL_STATUS_ON;
+		} else {
+			return CLOCK_CONTROL_STATUS_OFF;
+		}
+	}
+}
+
 static struct clock_control_driver_api stm32_clock_control_api = {
 	.on = stm32_clock_control_on,
 	.off = stm32_clock_control_off,
 	.get_rate = stm32_clock_control_get_subsys_rate,
+	.get_status = stm32_clock_control_get_status,
 	.configure = stm32_clock_control_configure,
 };
 
