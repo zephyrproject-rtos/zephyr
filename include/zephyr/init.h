2d0912b4d6942fbdfacfaf51b8bbc5b15b9aaefe
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2023-04-13 10:14:19 +0200

    init/device: add missing __noasan
    __noasan attribute was accidentally removed by
    dac487dda6137affe04755fd0d5a64c5461941a6. Add it back as it is causing
    some address sanitizer issues when using the LLVM toolchain.
    
    Details on why this is needed can be found in
    e834fd9e1933d1a2e54853cc9ec15423ed7a6534.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- include/zephyr/init.h
+++ include/zephyr/init.h
@@ -151,7 +151,7 @@ struct init_entry {
  */
 #define SYS_INIT_NAMED(name, init_fn_, level, prio)                            \
 	static const Z_DECL_ALIGN(struct init_entry)                           \
-		Z_INIT_ENTRY_SECTION(level, prio) __used                       \
+		Z_INIT_ENTRY_SECTION(level, prio) __used __noasan              \
 		Z_INIT_ENTRY_NAME(name) = {                                    \
 			.init_fn = {.sys = (init_fn_)},                        \
 	}
