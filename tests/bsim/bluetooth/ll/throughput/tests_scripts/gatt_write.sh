614b970c59ebdfa463862f07848ea54ba141fb58
Jonathan Rico <jonathan.rico@nordicsemi.no>
2023-04-13 16:32:04 +0200

    tests: Bluetooth: move host/gatt/write bsim test to ll
    The pass criteria for the test very much depends on the controller's
    implementation.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- tests/bsim/bluetooth/ll/throughput/tests_scripts/gatt_write.sh
+++ tests/bsim/bluetooth/ll/throughput/tests_scripts/gatt_write.sh
@@ -4,17 +4,16 @@
 
 source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
 
-# Multiple connection between two devices with multiple peripheral identity
-simulation_id="gatt_write"
+simulation_id="ll-throughput"
 verbosity_level=2
 EXECUTE_TIMEOUT=60
 
 cd ${BSIM_OUT_PATH}/bin
 
-Execute ./bs_${BOARD}_tests_bsim_bluetooth_host_gatt_write_prj_conf \
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_ll_throughput_prj_conf \
   -v=${verbosity_level} -s=${simulation_id} -d=0 -testid=central
 
-Execute ./bs_${BOARD}_tests_bsim_bluetooth_host_gatt_write_prj_conf\
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_ll_throughput_prj_conf\
   -v=${verbosity_level} -s=${simulation_id} -d=1 -testid=peripheral
 
 Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
