2b5d3740a6188a3b3d2ccdd1704e2523d6f20821
Lawrence King <lawrencek52@gmail.com>
2023-04-15 11:43:56 -0400

    libc: minimal: math: sqrt: fix Incorrect minimal libc sqrt
    Fixes #55962
    
    Changed initial guess from a simple x/3 to dividing the
    exponent by 2. This makes large or small numbers like 10e10
    and 01e-10 converge in a few loops. Added a loop counter to
    ensure that the algorithm breaks out of the loop in the case
    that the algorithm doesn't converge (toggling between two numbers).
    Tested with a range of numbers between 10e10 and 10e-10.
    
    Signed-off-by: Lawrence King <lawrencek52@gmail.com>

--- lib/libc/minimal/source/math/sqrt.c
+++ lib/libc/minimal/source/math/sqrt.c
@@ -5,19 +5,43 @@
  */
 
 #include <math.h>
+#include <stdint.h>
+#include <zephyr/sys/util.h>
 
-double sqrt(double value)
+#define MAX_D_ITTERATIONS	8  /* usually converges in 5 loops */
+				   /* this ensures we break out of the loop */
+#define MAX_D_ERROR_COUNT	5  /* when result almost converges, stop */
+#define EXP_MASK64	GENMASK64(62, 52)
+
+double sqrt(double square)
 {
-	double sqrt = value / 3;
 	int i;
+	int64_t	exponent;
+	double root;
+	double last;
+	int64_t *p_square = (int64_t *)&square;
+	int64_t *p_root = (int64_t *)&root;
+	int64_t *p_last = (int64_t *)&last;
 
-	if (value <= 0) {
-		return 0;
+	if (square <= 0.0) {
+		return 0.0;
 	}
 
-	for (i = 0; i < 6; i++) {
-		sqrt = (sqrt + value / sqrt) / 2;
-	}
+	/* we need a good starting guess so that this will converge quickly,
+	 * we can do this by dividing the exponent part of the float by 2
+	 * this assumes IEEE-754 format doubles
+	 */
+	exponent = ((*p_square & EXP_MASK64)>>52)-1023;
+	exponent /= 2;
+	*p_root = (*p_square & ~EXP_MASK64) | (exponent+1023)<<52;
 
-	return sqrt;
+	for (i = 0; i < MAX_D_ITTERATIONS; i++) {
+		last = root;
+		root = (root + square / root) * 0.5;
+		/* if (llabs(*p_root-*p_last)<MAX_D_ERROR_COUNT) break; */
+		if ((*p_root ^ *p_last) < MAX_D_ERROR_COUNT) {
+			break;
+		}
+	}
+	return root;
 }
--- lib/libc/minimal/source/math/sqrtf.c
+++ lib/libc/minimal/source/math/sqrtf.c
@@ -5,24 +5,43 @@
  */
 
 #include <math.h>
+#include <stdint.h>
+#include <zephyr/sys/util.h>
 
-#define MINDIFF 2.25e-308
+#define MAX_D_ITTERATIONS	6  /* usually converges in 4 loops */
+				   /* this ensures we break out of the loop */
+#define MAX_D_ERROR_COUNT	3  /* when result almost converges, stop */
+#define EXP_MASK32	GENMASK(30, 23)
 
 float sqrtf(float square)
 {
-	float root, last, diff;
+	int i;
+	float root;
+	float last;
+	int32_t	exponent;
+	int32_t *p_square = (int32_t *)&square;
+	int32_t *p_root = (int32_t *)&root;
+	int32_t *p_last = (int32_t *)&last;
 
-	root = square / 3.0;
-
-	if (square <= 0) {
-		return 0;
+	if (square <= 0.0f) {
+		return 0.0f;
 	}
 
-	do {
-		last = root;
-		root = (root + square / root) / 2.0;
-		diff = root - last;
-	} while (diff > MINDIFF || diff < -MINDIFF);
+	/* we need a good starting guess so that this will converge quickly,
+	 * we can do this by dividing the exponent part of the float by 2
+	 * this assumes IEEE-754 format doubles
+	 */
+	exponent = ((*p_square & EXP_MASK32)>>23)-127;
+	exponent /= 2;
+	*p_root = (*p_square & ~EXP_MASK32) | (exponent+127) << 23;
 
+	for (i = 0; i < MAX_F_ITTERATIONS; i++) {
+		last = root;
+		root = (root + square / root) * 0.5f;
+		//if (labs(*p_root - *p_last) < MAX_F_ERROR_COUNT) break;
+		if ((*p_root ^ *p_last) < MAX_F_ERROR_COUNT) {
+			break;
+		}
+	}
 	return root;
 }
