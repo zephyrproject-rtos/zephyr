822fd4782f6bd8cf4efa493dae2d53bd78717d6b
Krzysztof Kopyściński <krzysztof.kopyscinski@codecoup.pl>
2023-04-17 11:47:04 +0200

    Tests: Bluetooth: Mesh: Add persistent storage test for SAR TX/RX
    This tests if SAR RX/TX configuaration is saved in and loaded from
    persistent storage correctly.
    
    Signed-off-by: Krzysztof Kopyściński <krzysztof.kopyscinski@codecoup.pl>

--- tests/bsim/bluetooth/mesh/src/main.c
+++ tests/bsim/bluetooth/mesh/src/main.c
@@ -14,6 +14,7 @@ extern struct bst_test_list *test_provision_pst_install(struct bst_test_list *te
 #if defined(CONFIG_BT_MESH_V1d1)
 extern struct bst_test_list *test_dfu_install(struct bst_test_list *test);
 extern struct bst_test_list *test_blob_pst_install(struct bst_test_list *test);
+extern struct bst_test_list *test_sar_pst_install(struct bst_test_list *test);
 #endif /* defined(CONFIG_BT_MESH_V1d1) */
 #elif defined(CONFIG_BT_MESH_GATT_PROXY)
 extern struct bst_test_list *test_adv_install(struct bst_test_list *test);
@@ -46,6 +47,7 @@ bst_test_install_t test_installers[] = {
 	test_provision_pst_install,
 	test_dfu_install,
 	test_blob_pst_install,
+	test_sar_pst_install,
 #endif /* defined(CONFIG_BT_MESH_V1d1) */
 #elif defined(CONFIG_BT_MESH_GATT_PROXY)
 	test_adv_install,
