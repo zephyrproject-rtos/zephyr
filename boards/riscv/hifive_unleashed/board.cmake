fc63792eeb5c74ce1f9a2f60fa9e3b45c7f122ca
Mateusz Holenko <mholenko@antmicro.com>
2023-04-18 10:12:07 +0200

    hifive_unleashed: Add RENODE_UART variable to board.cmake
    Set RENODE_UART variable describing which UART device
    should be used when running Robot Framework test on the platform.
    
    Signed-off-by: Mateusz Hołenko <mholenko@antmicro.com>

--- boards/riscv/hifive_unleashed/board.cmake
+++ boards/riscv/hifive_unleashed/board.cmake
@@ -2,6 +2,7 @@
 
 set(SUPPORTED_EMU_PLATFORMS renode)
 set(RENODE_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/support/hifive_unleashed.resc)
+set(RENODE_UART sysbus.uart0)
 set(OPENOCD_USE_LOAD_IMAGE NO)
 
 board_runner_args(openocd "--config=${BOARD_DIR}/support/openocd_hifive_unleashed.cfg")
