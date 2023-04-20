85c7ff21af77cf7c3288a1a9ffbfb99d74c73dd6
Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>
2023-04-20 13:19:32 +0300

    soc: arm64: nxp_imx: Add common directory
    This commit introduces the common/ directory which should
    contain all definitions required by NXP's arm64-based
    SoC's. This is required mostly for the soc.h header without
    which the build process of Zephyr's SOF module fails for
    i.MX93.
    
    Signed-off-by: Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>

---
+++ soc/arm64/nxp_imx/common/include/soc.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2023 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_NXP_IMX_COMMON_SOC_H_
+#define ZEPHYR_SOC_NXP_IMX_COMMON_SOC_H_
+
+/* TODO: populate me when need be */
+
+
+#endif /* ZEPHYR_SOC_NXP_IMX_COMMON_SOC_H_ */
