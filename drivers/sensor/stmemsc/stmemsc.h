b4f31747f6a35192326c747050b8b4a2b7a5915b
Armando Visconti <armando.visconti@st.com>
2023-04-21 13:45:54 +0200

    drivers/sensor: stmemsc: add macros to populate stmdev_ctx_t structure
    Add STMEMSC_CTX I2C/SPI/I3C macros that can be reused among all STMEMSC
    based ST sensor drivers to populate the stmdev_ctx_t stucture.
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

--- drivers/sensor/stmemsc/stmemsc.h
+++ drivers/sensor/stmemsc/stmemsc.h
@@ -15,12 +15,25 @@
 #include <zephyr/drivers/i3c.h>
 #include <zephyr/drivers/spi.h>
 
+
 static inline void stmemsc_mdelay(uint32_t millisec)
 {
 	k_msleep(millisec);
 }
 
 #ifdef CONFIG_I2C
+/*
+ * Populate the stmdev_ctx_t structure pointed by stmdev_ctx_ptr with
+ * stmemsc i2c APIs.
+ */
+#define STMEMSC_CTX_I2C(stmdev_ctx_ptr)					\
+	.ctx = {							\
+		.read_reg = (stmdev_read_ptr) stmemsc_i2c_read,		\
+		.write_reg = (stmdev_write_ptr) stmemsc_i2c_write,	\
+		.mdelay = (stmdev_mdelay_ptr) stmemsc_mdelay,		\
+		.handle = (void *)stmdev_ctx_ptr			\
+	}
+
 int stmemsc_i2c_read(const struct i2c_dt_spec *stmemsc,
 		     uint8_t reg_addr, uint8_t *value, uint8_t len);
 int stmemsc_i2c_write(const struct i2c_dt_spec *stmemsc,
@@ -28,6 +41,18 @@ int stmemsc_i2c_write(const struct i2c_dt_spec *stmemsc,
 #endif
 
 #ifdef CONFIG_I3C
+/*
+ * Populate the stmdev_ctx_t structure pointed by stmdev_ctx_ptr with
+ * stmemsc i3c APIs.
+ */
+#define STMEMSC_CTX_I3C(stmdev_ctx_ptr)					\
+	.ctx = {							\
+		.read_reg = (stmdev_read_ptr) stmemsc_i3c_read,		\
+		.write_reg = (stmdev_write_ptr) stmemsc_i3c_write,	\
+		.mdelay = (stmdev_mdelay_ptr) stmemsc_mdelay,		\
+		.handle = (void *)stmdev_ctx_ptr			\
+	}
+
 int stmemsc_i3c_read(void *stmemsc,
 		     uint8_t reg_addr, uint8_t *value, uint8_t len);
 int stmemsc_i3c_write(void *stmemsc,
@@ -35,6 +60,18 @@ int stmemsc_i3c_write(void *stmemsc,
 #endif
 
 #ifdef CONFIG_SPI
+/*
+ * Populate the stmdev_ctx_t structure pointed by stmdev_ctx_ptr with
+ * stmemsc spi APIs.
+ */
+#define STMEMSC_CTX_SPI(stmdev_ctx_ptr)					\
+	.ctx = {							\
+		.read_reg = (stmdev_read_ptr) stmemsc_spi_read,		\
+		.write_reg = (stmdev_write_ptr) stmemsc_spi_write,	\
+		.mdelay = (stmdev_mdelay_ptr) stmemsc_mdelay,		\
+		.handle = (void *)stmdev_ctx_ptr			\
+	}
+
 int stmemsc_spi_read(const struct spi_dt_spec *stmemsc,
 		     uint8_t reg_addr, uint8_t *value, uint8_t len);
 int stmemsc_spi_write(const struct spi_dt_spec *stmemsc,
