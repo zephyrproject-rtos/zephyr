2689e16670eba3aa6ba24cb1fbabb1d3d21685bf
Hein Wessels <heinwessels93@gmail.com>
2023-04-21 16:18:40 +0200

    net: sntp: support building without NET_SOCKETS_POSIX_NAMES
    Fixes the compiler error that occurs when attempting to build
    the SNTP library with CONFIG_NET_SOCKETS_POSIX_NAMES=n, while
    using native_posix.
    
    Closes #57133
    
    Co-authored-by: Chris Friedt <cfriedt@meta.com>
    Signed-off-by: Hein Wessels <heinwessels93@gmail.com>

--- samples/net/sockets/sntp_client/src/main.c
+++ samples/net/sockets/sntp_client/src/main.c
@@ -31,7 +31,7 @@ int main(void)
 	memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(SNTP_PORT);
-	inet_pton(AF_INET, SERVER_ADDR, &addr.sin_addr);
+	zsock_inet_pton(AF_INET, SERVER_ADDR, &addr.sin_addr);
 
 	rv = sntp_init(&ctx, (struct sockaddr *) &addr,
 		       sizeof(struct sockaddr_in));
@@ -58,7 +58,7 @@ int main(void)
 	memset(&addr6, 0, sizeof(addr6));
 	addr6.sin6_family = AF_INET6;
 	addr6.sin6_port = htons(SNTP_PORT);
-	inet_pton(AF_INET6, SERVER_ADDR6, &addr6.sin6_addr);
+	zsock_inet_pton(AF_INET6, SERVER_ADDR6, &addr6.sin6_addr);
 
 	rv = sntp_init(&ctx, (struct sockaddr *) &addr6,
 		       sizeof(struct sockaddr_in6));
