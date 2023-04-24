a013ea49af723c9f266d900fe50432eb08a56dd2
Girisha Dengi <girisha.dengi@intel.com>
2023-04-24 15:15:20 +0000

    drivers: pm_cpu_ops: Add support for multiple PSCI versions
    Add support for multiple ARM PSCI versions based on DT_DRV_COMPAT
    string. As of now PSCI versions 0_2 and 1_1 are supported.
    
    Signed-off-by: Girisha Dengi <girisha.dengi@intel.com>

--- drivers/pm_cpu_ops/pm_cpu_ops_psci.c
+++ drivers/pm_cpu_ops/pm_cpu_ops_psci.c
@@ -1,6 +1,8 @@
 /*
  * Copyright 2020 Carlo Caione <ccaione@baylibre.com>
  *
+ * Copyright (c) 2023, Intel Corporation.
+ *
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -19,7 +21,11 @@ LOG_MODULE_REGISTER(psci);
 #include <zephyr/drivers/pm_cpu_ops.h>
 #include "pm_cpu_ops_psci.h"
 
-static struct psci psci_data;
+/* PSCI device configuration object macro. */
+#define DEV_CFG(_dev) ((const struct psci_config_t *)(_dev)->config)
+
+/* PSCI data object. */
+static struct psci_data_t psci_data;
 
 static int psci_to_dev_err(int ret)
 {
@@ -80,7 +86,27 @@ int pm_system_off(void)
 	return psci_to_dev_err(ret);
 }
 
-int pm_system_reset(unsigned char reset)
+/**
+ * This function checks whether the given ID is supported or not, using PSCI_FEATURES command.
+ * PSCI_FEATURES is supported from version 1.0 onwards.
+ */
+static int psci_features_check(unsigned long function_id)
+{
+	int result = 0;
+
+	/* PSCI_FEATURES function ID is supported from PSCI 1.0 onwards. */
+	if (PSCI_VERSION_MAJOR(psci_data.ver) >= 1) {
+		result = psci_data.invoke_psci_fn(PSCI_FN_NATIVE(1_0, PSCI_FEATURES), function_id,
+						  0, 0);
+	} else {
+		LOG_ERR("Function ID %lx not supported", function_id);
+		result = -ENOTSUP;
+	}
+
+	return result;
+}
+
+int pm_system_reset(unsigned char reset_type)
 {
 	int ret;
 
@@ -88,11 +114,13 @@ int pm_system_reset(unsigned char reset)
 		return -EINVAL;
 	}
 
-	if (reset == SYS_WARM_RESET) {
-		ret = psci_data.invoke_psci_fn(PSCI_0_2_FN64_SYSTEM_RESET2, 0, 0, 0);
-	} else if (reset == SYS_COLD_RESET) {
-		ret = psci_data.invoke_psci_fn(PSCI_0_2_FN_SYSTEM_RESET, 0, 0, 0);
+	if ((reset_type == SYS_WARM_RESET) &&
+	    (!psci_features_check(PSCI_FN_NATIVE(1_1, SYSTEM_RESET2)))) {
+		ret = psci_data.invoke_psci_fn(PSCI_FN_NATIVE(1_1, SYSTEM_RESET2), 0, 0, 0);
+	} else if (reset_type == SYS_COLD_RESET) {
+		ret = psci_data.invoke_psci_fn(PSCI_FN_NATIVE(0_2, SYSTEM_RESET), 0, 0, 0);
 	} else {
+		LOG_ERR("Invalid system reset type issued");
 		return -EINVAL;
 	}
 
@@ -127,16 +155,14 @@ static uint32_t psci_get_version(void)
 	return psci_data.invoke_psci_fn(PSCI_0_2_FN_PSCI_VERSION, 0, 0, 0);
 }
 
-static int set_conduit_method(void)
+static int set_conduit_method(const struct device *dev)
 {
-	const char *method;
+	const struct psci_config_t *dev_config = DEV_CFG(dev);
 
-	method = DT_PROP(DT_INST(0, DT_DRV_COMPAT), method);
-
-	if (!strcmp("hvc", method)) {
+	if (!strcmp("hvc", dev_config->method)) {
 		psci_data.conduit = SMCCC_CONDUIT_HVC;
 		psci_data.invoke_psci_fn = __invoke_psci_fn_hvc;
-	} else if (!strcmp("smc", method)) {
+	} else if (!strcmp("smc", dev_config->method)) {
 		psci_data.conduit = SMCCC_CONDUIT_SMC;
 		psci_data.invoke_psci_fn = __invoke_psci_fn_smc;
 	} else {
@@ -174,13 +200,38 @@ static int psci_init(const struct device *dev)
 {
 	psci_data.conduit = SMCCC_CONDUIT_NONE;
 
-	if (set_conduit_method()) {
+	if (set_conduit_method(dev)) {
 		return -ENOTSUP;
 	}
 
 	return psci_detect();
 }
 
-DEVICE_DT_INST_DEFINE(0, psci_init, NULL,
-	&psci_data, NULL, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
-	NULL);
+/**
+ * Each PSCI interface versions have different DT compatible strings like arm,psci-0.2,
+ * arm,psci-1.1 and so on. However, the same driver can be used for all the versions with
+ * the below mentioned DT method where we need to #undef the default version arm,psci-0.2
+ * and #define the required version like arm,psci-1.0.
+ */
+#define PSCI_DEFINE(inst, ver)						\
+	static const struct psci_config_t psci_config_##inst##ver = {	\
+		.method = DT_PROP(DT_DRV_INST(inst), method)		\
+	};								\
+	DEVICE_DT_INST_DEFINE(inst,					\
+			&psci_init,					\
+			NULL,						\
+			&psci_data,					\
+			&psci_config_##inst##ver,				\
+			PRE_KERNEL_1,					\
+			CONFIG_KERNEL_INIT_PRIORITY_DEVICE,		\
+			NULL);
+
+#define PSCI_0_2_INIT(n) PSCI_DEFINE(n, PSCI_0_2)
+#undef DT_DRV_COMPAT
+#define DT_DRV_COMPAT arm_psci_0_2
+DT_INST_FOREACH_STATUS_OKAY(PSCI_0_2_INIT)
+
+#define PSCI_1_1_INIT(n) PSCI_DEFINE(n, PSCI_1_1)
+#undef DT_DRV_COMPAT
+#define DT_DRV_COMPAT arm_psci_1_1
+DT_INST_FOREACH_STATUS_OKAY(PSCI_1_1_INIT)
--- drivers/pm_cpu_ops/psci_shell.c
+++ drivers/pm_cpu_ops/psci_shell.c
@@ -1,13 +1,10 @@
 /*
- * Copyright (c) 2015 Intel Corporation.
+ * Copyright (c) 2023 Intel Corporation.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/shell/shell.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
 #include <zephyr/drivers/pm_cpu_ops.h>
 
 static int cmd_reboot_warm(const struct shell *shctx, size_t argc, char **argv)
@@ -18,6 +15,9 @@ static int cmd_reboot_warm(const struct shell *shctx, size_t argc, char **argv)
 	int ret = 0;
 
 	ret = pm_system_reset(SYS_WARM_RESET);
+	if (ret != 0) {
+		shell_error(shctx, "Failed to perform system warm reset");
+	}
 
 	return ret;
 }
@@ -30,14 +30,18 @@ static int cmd_reboot_cold(const struct shell *shctx, size_t argc, char **argv)
 	int ret = 0;
 
 	ret = pm_system_reset(SYS_COLD_RESET);
+	if (ret != 0) {
+		shell_error(shctx, "Failed to perform system cold reset");
+	}
 
 	return ret;
 }
 
-SHELL_STATIC_SUBCMD_SET_CREATE(sub_reboot,
-		SHELL_CMD_ARG(warm, NULL, "psci warm", cmd_reboot_warm, 1, 0),
-		SHELL_CMD_ARG(cold, NULL, "psci cold", cmd_reboot_cold, 1, 0),
-		SHELL_SUBCMD_SET_END /* Array terminated. */
-		);
+SHELL_STATIC_SUBCMD_SET_CREATE(
+	sub_reboot,
+	SHELL_CMD_ARG(warm, NULL, "System warm reset. Usage: <psci warm>", cmd_reboot_warm, 1, 0),
+	SHELL_CMD_ARG(cold, NULL, "System cold reset. Usage: <psci cold>", cmd_reboot_cold, 1, 0),
+	SHELL_SUBCMD_SET_END /* Array terminated. */
+);
 
-SHELL_CMD_REGISTER(psci, &sub_reboot, "PSCI commands", NULL);
+SHELL_CMD_REGISTER(psci, &sub_reboot, "ARM PSCI interface commands", NULL);
