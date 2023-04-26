cf6be82dc3c74aef4a197a476e599ac0dcd33b46
Peter Mitsis <peter.mitsis@intel.com>
2023-04-26 13:30:45 -0400

    tests: Update sched benchmark test
    As z_pend_curr_irqlock() is no longer used in the code base, it makes
    little sense to perform a micro-benchmark on it.
    
    Signed-off-by: Peter Mitsis <peter.mitsis@intel.com>

--- tests/benchmarks/sched/src/main.c
+++ tests/benchmarks/sched/src/main.c
@@ -8,19 +8,19 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/wait_q.h>
 #include <ksched.h>
+#include <zephyr/spinlock.h>
 
 /* This is a scheduler microbenchmark, designed to measure latencies
  * of specific low level scheduling primitives independent of overhead
  * from application or API abstractions.  It works very simply: a main
  * thread creates a "partner" thread at a higher priority, the partner
- * then sleeps using z_pend_curr_irqlock().  From this initial
- * state:
+ * then sleeps using z_pend_curr().  From this initial state:
  *
  * 1. The main thread calls z_unpend_first_thread()
  * 2. The main thread calls z_ready_thread()
  * 3. The main thread calls k_yield()
  *    (the kernel switches to the partner thread)
- * 4. The partner thread then runs and calls z_pend_curr_irqlock() again
+ * 4. The partner thread then runs and calls z_pend_curr() again
  *    (the kernel switches to the main thread)
  * 5. The main thread returns from k_yield()
  *
@@ -35,6 +35,8 @@
 
 static K_THREAD_STACK_DEFINE(partner_stack, 1024);
 static struct k_thread partner_thread;
+
+static struct k_spinlock lock;
 
 _wait_q_t waitq;
 
@@ -79,9 +81,9 @@ static void partner_fn(void *arg1, void *arg2, void *arg3)
 	printk("Running %p\n", k_current_get());
 
 	while (true) {
-		unsigned int key = irq_lock();
+		k_spinlock_key_t key = k_spin_lock(&lock);
 
-		z_pend_curr_irqlock(key, &waitq, K_FOREVER);
+		z_pend_curr(&lock, key, &waitq, K_FOREVER);
 		stamp(PARTNER_AWAKE_PENDING);
 	}
 }
