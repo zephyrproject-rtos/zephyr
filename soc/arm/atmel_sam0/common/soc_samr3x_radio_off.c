26184d50371901d3373cd9c8b03f0d3f28129222
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2023-04-26 17:42:24 +0200

    soc: arm: atmel_sam0: improve radio off handling for samr3x
    Improve the radio off code, mainly:
    
    - Compile the file only if necessary, ie, LORA radio not in use
    - Use pin information from DT, so that we do not need to hardcode pins
      and can switch to dt-spec APIs.
    - Improve error handling, includes, etc.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

---
+++ soc/arm/atmel_sam0/common/soc_samr3x_radio_off.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2021 Argentum Systems Ltd.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/init.h>
+
+static int radio_off_setup(void)
+{
+	int ret;
+	const struct gpio_dt_spec reset = GPIO_DT_SPEC_GET(DT_NODELABEL(lora), reset_gpios);
+	const struct gpio_dt_spec cs = GPIO_DT_SPEC_GET(DT_NODELABEL(sercom4), cs_gpios);
+
+	if (!gpio_is_ready_dt(&reset) || !gpio_is_ready_dt(&cs)) {
+		return -ENODEV;
+	}
+
+	ret = gpio_pin_configure_dt(&reset, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return ret;
+	}
+
+	ret = gpio_pin_configure_dt(&cs, GPIO_OUTPUT_INACTIVE);
+	if (ret < 0) {
+		return ret;
+	}
+
+	return 0;
+}
+
+SYS_INIT(radio_off_setup, PRE_KERNEL_2, 0);
