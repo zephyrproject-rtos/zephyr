b65613e79cb2577e2f417db552b9daf13de93d03
Georges Oates_Larsen <georges.larsen@nordicsemi.no>
2023-04-26 16:30:23 -0700

    net: conn_mgr: Support Auto-Down
    To reduce the amount of boiler-plate needed in applications, this commit
    grants conn_mgr the ability to automatically take ifaces that have given
    up on connecting into the admin-down state.
    
    Tests adjusted as appropriate.
    
    This behavior can be disabled globally by disabling
    NET_CONNECTION_MANAGER_AUTO_IF_DOWN, or disabled per-iface using the
    CONN_MGR_IF_NO_AUTO_DOWN flag.
    
    Signed-off-by: Georges Oates_Larsen <georges.larsen@nordicsemi.no>

--- subsys/net/conn_mgr/conn_mgr_connectivity.c
+++ subsys/net/conn_mgr/conn_mgr_connectivity.c
@@ -48,6 +48,8 @@ out:
 	return status;
 }
 
+static void conn_mgr_conn_if_auto_admin_down(struct net_if *iface);
+
 int conn_mgr_if_disconnect(struct net_if *iface)
 {
 	struct conn_mgr_conn_binding *binding;
@@ -76,6 +78,16 @@ int conn_mgr_if_disconnect(struct net_if *iface)
 
 out:
 	k_mutex_unlock(binding->mutex);
+
+	/* Since the connectivity implementation will not automatically attempt to reconnect after
+	 * a call to conn_mgr_if_disconnect, conn_mgr_conn_if_auto_admin_down should be called.
+	 *
+	 * conn_mgr_conn_handle_iface_down will only call conn_mgr_conn_if_auto_admin_down if
+	 * persistence is disabled. To ensure conn_mgr_conn_if_auto_admin_down is called in all
+	 * cases, we must call it directly from here. If persistence is disabled, this will result
+	 * in conn_mgr_conn_if_auto_admin_down being called twice, but that is not an issue.
+	 */
+	conn_mgr_conn_if_auto_admin_down(iface);
 
 	return status;
 }
@@ -263,6 +275,61 @@ static void conn_mgr_conn_handle_iface_admin_up(struct net_if *iface)
 	}
 }
 
+/**
+ * @brief Take the provided iface admin-down.
+ *
+ * Called automatically by conn_mgr when an iface has lost connection and will not attempt to
+ * regain it.
+ *
+ * @param iface - The iface to take admin-down
+ */
+static void conn_mgr_conn_if_auto_admin_down(struct net_if *iface)
+{
+	/* NOTE: This will be double-fired for ifaces that are both non-persistent
+	 * and are being directly requested to disconnect, since both of these conditions
+	 * separately trigger conn_mgr_conn_if_auto_admin_down.
+	 *
+	 * This is fine, because net_if_down is idempotent, but if you are adding other
+	 * behaviors to this function, bear it in mind.
+	 */
+
+	/* Ignore ifaces that don't have connectivity implementations */
+	if (!conn_mgr_if_is_bound(iface)) {
+		return;
+	}
+
+	/* Take the iface admin-down if AUTO_DOWN is enabled */
+	if (IS_ENABLED(CONFIG_NET_CONNECTION_MANAGER_AUTO_IF_DOWN) &&
+	    !conn_mgr_if_get_flag(iface, CONN_MGR_IF_NO_AUTO_DOWN)) {
+		net_if_down(iface);
+	}
+}
+
+/**
+ * @brief Perform automated behaviors in response to any iface that loses oper-up state.
+ *
+ * This is how conn_mgr_conn automatically takes such ifaces admin-down if they are not persistent.
+ *
+ * @param iface - The iface which lost oper-up state.
+ */
+static void conn_mgr_conn_handle_iface_down(struct net_if *iface)
+{
+	/* Ignore ifaces that don't have connectivity implementations */
+	if (!conn_mgr_if_is_bound(iface)) {
+		return;
+	}
+
+	/* If the iface is persistent, we expect it to try to reconnect, so nothing else to do */
+	if (conn_mgr_if_get_flag(iface, CONN_MGR_IF_PERSISTENT)) {
+		return;
+	}
+
+	/* Otherwise, we do not expect the iface to reconnect, and we should call
+	 * conn_mgr_conn_if_auto_admin_down
+	 */
+	conn_mgr_conn_if_auto_admin_down(iface);
+}
+
 static struct net_mgmt_event_callback conn_mgr_conn_iface_cb;
 static void conn_mgr_conn_iface_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event,
 					struct net_if *iface)
@@ -272,10 +339,43 @@ static void conn_mgr_conn_iface_handler(struct net_mgmt_event_callback *cb, uint
 	}
 
 	switch (mgmt_event) {
-	case NET_EVENT_IF_UP:
+	case NET_EVENT_IF_DOWN:
+		conn_mgr_conn_handle_iface_down(iface);
+		break;
+	case NET_EVENT_IF_ADMIN_UP:
 		conn_mgr_conn_handle_iface_admin_up(iface);
 		break;
 	}
+}
+
+static struct net_mgmt_event_callback conn_mgr_conn_self_cb;
+static void conn_mgr_conn_self_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event,
+				       struct net_if *iface)
+{
+	if ((mgmt_event & CONN_MGR_CONN_SELF_EVENTS_MASK) != mgmt_event) {
+		return;
+	}
+
+	switch (NET_MGMT_GET_COMMAND(mgmt_event)) {
+	case NET_EVENT_CONN_CMD_IF_FATAL_ERROR:
+		if (cb->info) {
+			NET_ERR("Fatal connectivity error on iface %d (%p). Reason: %d.",
+				net_if_get_by_iface(iface), iface, *((int *)cb->info)
+			);
+		} else {
+			NET_ERR("Unknown fatal connectivity error on iface %d (%p).",
+				net_if_get_by_iface(iface), iface
+			);
+		}
+	__fallthrough;
+	case NET_EVENT_CONN_CMD_IF_TIMEOUT:
+		/* If a timeout or fatal error occurs, we do not expect the iface to try to
+		 * reconnect, so call conn_mgr_conn_if_auto_admin_down.
+		 */
+		conn_mgr_conn_if_auto_admin_down(iface);
+		break;
+	}
+
 }
 
 void conn_mgr_conn_init(void)
@@ -304,6 +404,10 @@ void conn_mgr_conn_init(void)
 	net_mgmt_init_event_callback(&conn_mgr_conn_iface_cb, conn_mgr_conn_iface_handler,
 				     CONN_MGR_CONN_IFACE_EVENTS_MASK);
 	net_mgmt_add_event_callback(&conn_mgr_conn_iface_cb);
+
+	net_mgmt_init_event_callback(&conn_mgr_conn_self_cb, conn_mgr_conn_self_handler,
+				     CONN_MGR_CONN_SELF_EVENTS_MASK);
+	net_mgmt_add_event_callback(&conn_mgr_conn_self_cb);
 
 	/* Trigger any initial automated behaviors for ifaces */
 	STRUCT_SECTION_FOREACH(conn_mgr_conn_binding, binding) {
