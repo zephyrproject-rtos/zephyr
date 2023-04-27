0bd67c9b44b1c1f67225cb3973739b2e9ba5853c
Peter Rowley <perowle@microsoft.com>
2023-04-27 14:43:33 -0700

    drivers: fuel_gauge: sbs_gauge: Add support for block registers
    The block registers (chemistry, manufacturer name, device name) were
    not implemented. Implemented by adding a new api interface for
    retrieving block properties. fuel_gauge_get_block_property has been
    added, and uses a memory buffer allocated in the app in order to
    store the fuel gauge information.
    
    Signed-off-by: Peter Rowley <perowle@microsoft.com>

--- drivers/fuel_gauge/fuel_gauge_syscall_handlers.c
+++ drivers/fuel_gauge/fuel_gauge_syscall_handlers.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2023 Google LLC
+ * Copyright (C) Microsoft Corporation.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -26,6 +27,30 @@ static inline int z_vrfy_fuel_gauge_get_prop(const struct device *dev,
 }
 
 #include <syscalls/fuel_gauge_get_prop_mrsh.c>
+
+static inline int z_vrfy_fuel_gauge_get_block_prop(const struct device *dev,
+						    struct fuel_gauge_get_block_property *prop,
+						    void *dst, size_t dst_len)
+{
+	char k_buf[dst_len];
+	struct fuel_gauge_get_block_property k_prop;
+
+	Z_OOPS(Z_SYSCALL_DRIVER_FUEL_GAUGE(dev, get_block_property));
+
+	Z_OOPS(z_user_from_copy(&k_prop, prop, 
+				sizeof(struct fuel_gauge_get_block_property)));
+
+	int ret = z_impl_fuel_gauge_get_block_prop(dev, &k_prop, k_buf, dst_len);
+
+	Z_OOPS(z_user_to_copy(dst, k_buf, dst_len));
+
+	Z_OOPS(z_user_to_copy(prop, &k_prop,
+			      sizeof(struct fuel_gauge_get_block_property)));
+
+	return ret;
+}
+
+#include <syscalls/fuel_gauge_get_block_prop_mrsh.c>
 
 static inline int z_vrfy_fuel_gauge_set_prop(const struct device *dev,
 					     struct fuel_gauge_set_property *props,
