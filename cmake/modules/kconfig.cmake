e49a2e774b7fd7555c5a8918bf89bbeaeddb0551
Gerard Marull-Paretas <gerard@teslabs.com>
2023-04-28 18:26:23 +0200

    cmake: modules: add support for board extensions
    Board extensions are board fragments that may be present in any board
    root folder, under `${BOARD_ROOT}/extensions/${BOARD}`. The board
    extension directory may contain Kconfig fragments and/or devicetree
    overlays.  Board extensions are automatically loaded and applied on top
    of board files, before anything else. There is no guarantee on which
    order extensions are applied, in case multiple exist.
    
    Board extensions may be useful in the context of Zephyr modules, such as
    `example-application`. In some situations, certain hardware description
    or choices can not be added in the upstream Zephyr context, but they do
    in a downstream project, where custom bindings or driver classes can
    also be created. This feature may also be useful in development phases,
    when the board skeleton lives upstream, but other board features are not
    yet present.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- cmake/modules/kconfig.cmake
+++ cmake/modules/kconfig.cmake
@@ -84,6 +84,7 @@ if(EXTRA_CONF_FILE)
   string(REPLACE " " ";" EXTRA_CONF_FILE_AS_LIST "${EXTRA_CONF_FILE_EXPANDED}")
 endif()
 
+zephyr_file(CONF_FILES ${BOARD_EXTENSION_DIRS} KCONF board_extension_conf_files)
 
 # DTS_ROOT_BINDINGS is a semicolon separated list, this causes
 # problems when invoking kconfig_target since semicolon is a special
@@ -252,6 +253,7 @@ set(
   merge_config_files
   ${BOARD_DEFCONFIG}
   ${BOARD_REVISION_CONFIG}
+  ${board_extension_conf_files}
   ${CONF_FILE_AS_LIST}
   ${shield_conf_files}
   ${EXTRA_CONF_FILE_AS_LIST}
