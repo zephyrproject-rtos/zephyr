f6901c4621c8d65793513fa9e877608c56c44f81
Kumar Gala <kumar.gala@intel.com>
2023-04-28 09:27:46 -0500

    samples, tests: cleanup void main usage.
    Some samples, tests got missed in the switch from void main() to
    int main().  Cleanup those samples/tests to use int main().
    
    Signed-off-by: Kumar Gala <kumar.gala@intel.com>

--- samples/subsys/usb_c/source/src/main.c
+++ samples/subsys/usb_c/source/src/main.c
@@ -313,7 +313,7 @@ bool port1_policy_check(const struct device *dev,
 }
 /* usbc.rst check end */
 
-void main(void)
+int main(void)
 {
 	const struct device *usbc_port1;
 
@@ -321,7 +321,7 @@ void main(void)
 	usbc_port1 = DEVICE_DT_GET(PORT1_NODE);
 	if (!device_is_ready(usbc_port1)) {
 		LOG_ERR("PORT1 device not ready");
-		return;
+		return 0;
 	}
 
 	/* usbc.rst register start */
@@ -389,4 +389,6 @@ void main(void)
 		/* Arbitrary delay */
 		k_msleep(10);
 	}
+
+	return 0;
 }
