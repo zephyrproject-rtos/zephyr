c510ff222e6a6e27f29dc8754347225f002b3df7
Tom Burdick <thomas.burdick@intel.com>
2023-04-28 15:52:16 -0500

    rtio: Use mpsc for submission and completion queue
    Rather than the rings, which weren't shared between userspace and kernel
    space in Zephyr like they are in Linux with io_uring, use atomic mpsc
    queues for submission and completion queues.
    
    Most importantly this removes a potential head of line blocker in the
    submission queue as the sqe would be held until a task is completed.
    
    As additional bonuses this avoids some additional locks and restrictions
    about what can be submitted and where. It also removes the need for
    two executors as all chains/transactions are done concurrently.
    
    Lastly this opens up the possibility for a common pool of sqe's to
    allocate from potentially saving lots of memory.
    
    Signed-off-by: Tom Burdick <thomas.burdick@intel.com>

--- tests/subsys/rtio/rtio_api/src/rtio_iodev_test.h
+++ tests/subsys/rtio/rtio_api/src/rtio_iodev_test.h
@@ -17,8 +17,8 @@ struct rtio_iodev_test_data {
 	struct k_timer timer;
 
 	/* Currently executing sqe */
-	struct rtio_iodev_sqe *iodev_sqe;
-	const struct rtio_sqe *sqe;
+	struct rtio_iodev_sqe *txn_head;
+	struct rtio_iodev_sqe *txn_curr;
 
 	/* Count of submit calls */
 	atomic_t submit_count;
@@ -34,7 +34,7 @@ static void rtio_iodev_test_next(struct rtio_iodev *iodev)
 	/* The next section must be serialized to ensure single consumer semantics */
 	k_spinlock_key_t key = k_spin_lock(&data->lock);
 
-	if (data->iodev_sqe != NULL) {
+	if (data->txn_head != NULL) {
 		goto out;
 	}
 
@@ -43,9 +43,13 @@ static void rtio_iodev_test_next(struct rtio_iodev *iodev)
 	if (next != NULL) {
 		struct rtio_iodev_sqe *next_sqe = CONTAINER_OF(next, struct rtio_iodev_sqe, q);
 
-		data->iodev_sqe = next_sqe;
-		data->sqe = next_sqe->sqe;
+		TC_PRINT("next task in queue %p\n", (void *)next_sqe);
+
+		data->txn_head = next_sqe;
+		data->txn_curr = next_sqe;
 		k_timer_start(&data->timer, K_MSEC(10), K_NO_WAIT);
+	} else {
+		TC_PRINT("no more tasks in the queue\n");
 	}
 
 out:
@@ -55,40 +59,46 @@ out:
 static void rtio_iodev_timer_fn(struct k_timer *tm)
 {
 	struct rtio_iodev_test_data *data = CONTAINER_OF(tm, struct rtio_iodev_test_data, timer);
-	struct rtio_iodev_sqe *iodev_sqe = data->iodev_sqe;
-	struct rtio_iodev *iodev = (struct rtio_iodev *)iodev_sqe->sqe->iodev;
+	struct rtio_iodev_sqe *iodev_sqe = data->txn_curr;
+	struct rtio_iodev *iodev = (struct rtio_iodev *)data->txn_head->sqe.iodev;
 
-	if (data->sqe->op == RTIO_OP_RX) {
+	if (iodev_sqe->sqe.op == RTIO_OP_RX) {
 		uint8_t *buf;
 		uint32_t buf_len;
 
 		int rc = rtio_sqe_rx_buf(iodev_sqe, 16, 16, &buf, &buf_len);
 
 		if (rc != 0) {
+			iodev_sqe = data->txn_head;
+			data->txn_head = NULL;
+			data->txn_curr = NULL;
 			rtio_iodev_sqe_err(iodev_sqe, rc);
+			rtio_iodev_test_next(iodev);
 			return;
 		}
 
 		for (int i = 0; i < 16; ++i) {
-			buf[i] = ((uint8_t *)iodev_sqe->sqe->userdata)[i];
+			buf[i] = ((uint8_t *)iodev_sqe->sqe.userdata)[i];
 		}
 	}
 
-	if (data->sqe->flags & RTIO_SQE_TRANSACTION) {
-		data->sqe = rtio_spsc_next(data->iodev_sqe->r->sq, data->sqe);
-		k_timer_start(&data->timer, K_MSEC(10), K_NO_WAIT);
+	if (iodev_sqe->sqe.flags & RTIO_SQE_TRANSACTION) {
+		data->txn_curr = rtio_txn_next(data->txn_curr);
+		TC_PRINT("iodev_sqe %p marked transaction, next %p\n", iodev_sqe, data->txn_curr);
+		k_timer_start(tm, K_MSEC(10), K_NO_WAIT);
 		return;
 	}
 
-	data->iodev_sqe = NULL;
-	data->sqe = NULL;
-	rtio_iodev_sqe_ok(iodev_sqe, 0);
+	iodev_sqe = data->txn_head;
+	data->txn_head = NULL;
+	data->txn_curr = NULL;
 	rtio_iodev_test_next(iodev);
+	rtio_iodev_sqe_ok(iodev_sqe, 0);
 }
 
 static void rtio_iodev_test_submit(struct rtio_iodev_sqe *iodev_sqe)
 {
-	struct rtio_iodev *iodev = (struct rtio_iodev *)iodev_sqe->sqe->iodev;
+	struct rtio_iodev *iodev = (struct rtio_iodev *)iodev_sqe->sqe.iodev;
 	struct rtio_iodev_test_data *data = iodev->data;
 
 	atomic_inc(&data->submit_count);
@@ -108,7 +118,8 @@ void rtio_iodev_test_init(struct rtio_iodev *test)
 	struct rtio_iodev_test_data *data = test->data;
 
 	rtio_mpsc_init(&test->iodev_sq);
-	data->iodev_sqe = NULL;
+	data->txn_head = NULL;
+	data->txn_curr = NULL;
 	k_timer_init(&data->timer, rtio_iodev_timer_fn, NULL);
 }
 
