ff0c6b9c803891254106793e02d398aef116ef15
Christopher Friedt <cfriedt@meta.com>
2023-04-29 07:50:28 -0400

    tests: posix: pthread: init pthread_attr_t on each iteration
    The `test_pthread_descriptor_leak` test was causing a kernel
    panic on some platforms. Initially, it was not clear why.
    
    The usual cases were examined - race conditions, stack sizes,
    etc. Still no luck.
    
    As it turns out, recycling a thread stack (or at least the
    `pthread_attr_t`) in-place does not work on some platforms,
    and we need to reinitialize the `pthread_attr_t` and set
    set the stack property again prior to calling
    `pthread_create()`.
    
    Signed-off-by: Christopher Friedt <cfriedt@meta.com>

--- tests/posix/common/src/pthread.c
+++ tests/posix/common/src/pthread.c
@@ -588,11 +588,10 @@ ZTEST(posix_apis, test_pthread_descriptor_leak)
 	pthread_t pthread1;
 	pthread_attr_t attr;
 
-	zassert_ok(pthread_attr_init(&attr));
-	zassert_ok(pthread_attr_setstack(&attr, &stack_e[0][0], STACKS));
-
 	/* If we are leaking descriptors, then this loop will never complete */
 	for (size_t i = 0; i < CONFIG_MAX_PTHREAD_COUNT * 2; ++i) {
+		zassert_ok(pthread_attr_init(&attr));
+		zassert_ok(pthread_attr_setstack(&attr, &stack_e[0][0], STACKS));
 		zassert_ok(pthread_create(&pthread1, &attr, create_thread1, NULL),
 			   "unable to create thread %zu", i);
 		zassert_ok(pthread_join(pthread1, &unused), "unable to join thread %zu", i);
