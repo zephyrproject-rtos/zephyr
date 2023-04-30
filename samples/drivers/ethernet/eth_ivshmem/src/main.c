36dcdb2ce07473c7b81d1ff8909fd08c043405ee
Grant Ramsay <gramsay@enphaseenergy.com>
2023-04-30 14:40:29 +1200

    samples: drivers: ethernet: Add Jailhouse IVSHMEM Ethernet sample
    Add an IVSHMEM Ethernet sample to communicate between Zephyr
    and Linux root cell within the Jailhouse hypervisor.
    
    The supported configuration runs the Jailhouse hypervisor
    within QEMU Cortex-A53.
    
    Signed-off-by: Grant Ramsay <gramsay@enphaseenergy.com>

---
+++ samples/drivers/ethernet/eth_ivshmem/src/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2023 Enphase Energy
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+
+void main(void)
+{
+	/* This sample uses the shell */
+	k_sleep(K_FOREVER);
+}
