9d069ce86a4e48ece42f59298b3b6b216af6fd9c
Grant Ramsay <gramsay@enphaseenergy.com>
2023-04-30 14:34:35 +1200

    drivers: virtualization: Add interface for ivshmem-v2
    ivshmem-v2 is primarily used for IPC in the Jailhouse hypervisor
    
    Signed-off-by: Grant Ramsay <gramsay@enphaseenergy.com>

--- drivers/virtualization/virt_ivshmem.h
+++ drivers/virtualization/virt_ivshmem.h
@@ -13,8 +13,29 @@
 #include <zephyr/drivers/pcie/msi.h>
 #endif
 
+#define PCIE_CONF_CMDSTAT_INTX_DISABLE	0x0400
+#define PCIE_CONF_INTR_PIN(x)		(((x) >> 8) & 0xFFu)
+
+#define IVSHMEM_CFG_ID			0x00
+#define IVSHMEM_CFG_NEXT_CAP		0x01
+#define IVSHMEM_CFG_LENGTH		0x02
+#define IVSHMEM_CFG_PRIV_CNTL		0x03
+#define IVSHMEM_PRIV_CNTL_ONESHOT_INT	BIT(0)
+#define IVSHMEM_CFG_STATE_TAB_SZ	0x04
+#define IVSHMEM_CFG_RW_SECTION_SZ	0x08
+#define IVSHMEM_CFG_OUTPUT_SECTION_SZ	0x10
+#define IVSHMEM_CFG_ADDRESS		0x18
+
+#define IVSHMEM_INT_ENABLE		BIT(0)
+
 #define IVSHMEM_PCIE_REG_BAR_IDX	0
+#define IVSHMEM_PCIE_MSI_X_BAR_IDX	1
 #define IVSHMEM_PCIE_SHMEM_BAR_IDX	2
+
+#define PCIE_INTX_PIN_MIN		1
+#define PCIE_INTX_PIN_MAX		4
+
+#define INTX_IRQ_UNUSED			UINT32_MAX
 
 struct ivshmem_param {
 	const struct device *dev;
@@ -32,6 +53,14 @@ struct ivshmem {
 	struct ivshmem_param params[CONFIG_IVSHMEM_MSI_X_VECTORS];
 	uint16_t n_vectors;
 #endif
+#ifdef CONFIG_IVSHMEM_V2
+	bool ivshmem_v2;
+	uint32_t max_peers;
+	size_t rw_section_size;
+	size_t rw_section_offset;
+	size_t output_section_size;
+	size_t output_section_offset;
+#endif
 };
 
 struct ivshmem_reg {
@@ -40,6 +69,26 @@ struct ivshmem_reg {
 	uint32_t iv_position;
 	uint32_t doorbell;
 };
+
+#ifdef CONFIG_IVSHMEM_V2
+
+struct ivshmem_v2_reg {
+	uint32_t id;
+	uint32_t max_peers;
+	uint32_t int_control;
+	uint32_t doorbell;
+	uint32_t state;
+};
+
+struct ivshmem_cfg {
+	struct intx_info {
+		uint32_t irq;
+		uint32_t priority;
+		uint32_t flags;
+	} intx_info[PCIE_INTX_PIN_MAX];
+};
+
+#endif /* CONFIG_IVSHMEM_V2 */
 
 #define IVSHMEM_GEN_DOORBELL(i, v) ((i << 16) | (v & 0xFFFF))
 
