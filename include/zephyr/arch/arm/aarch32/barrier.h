66572a384ef276d69f03577809bef08ee8e12237
Carlo Caione <ccaione@baylibre.com>
2023-05-02 09:35:30 +0200

    barriers: arm: Use CMSIS instead of built-in functions
    For ARM (AArch32) we fall back on the CMSIS implementation that it is
    supposedly more refined.
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

---
+++ include/zephyr/arch/arm/aarch32/barrier.h
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2023 Carlo Caione <ccaione@baylibre.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_BARRIER_ARM_H_
+#define ZEPHYR_INCLUDE_BARRIER_ARM_H_
+
+#ifndef ZEPHYR_INCLUDE_SYS_BARRIER_H_
+#error Please include <zephyr/sys/barrier.h>
+#endif
+
+#if defined(CONFIG_CPU_CORTEX_M)
+#include <zephyr/arch/arm/aarch32/cortex_m/cmsis.h>
+#else
+#include <zephyr/arch/arm/aarch32/cortex_a_r/cmsis.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static ALWAYS_INLINE void z_barrier_dmem_fence_full(void)
+{
+	__DMB();
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_BARRIER_ARM_H_ */
