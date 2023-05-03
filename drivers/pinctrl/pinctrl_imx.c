3d6c29c817c2967c6871c083afed9e8c6b7a8fdb
Manoel Brunnen <mb@lee-brunnen.de>
2023-05-03 13:48:39 +0200

    drivers: pinctrl: imx: Add braces to if bodies
    Putting the bodies in if statements inside braces increases the
    readability.
    
    Signed-off-by: Manoel Brunnen <mb@lee-brunnen.de>

--- drivers/pinctrl/pinctrl_imx.c
+++ drivers/pinctrl/pinctrl_imx.c
@@ -36,12 +36,14 @@ int pinctrl_configure_pins(const pinctrl_soc_pin_t *pins, uint8_t pin_cnt,
 		sys_write32(IOMUXC1_SW_MUX_CTL_PAD_MUX_MODE(mux_mode) |
 			IOMUXC1_SW_MUX_CTL_PAD_SION(MCUX_IMX_INPUT_ENABLE(pin_ctrl_flags)),
 			(mem_addr_t)mux_register);
-		if (input_register)
+		if (input_register) {
 			sys_write32(IOMUXC1_SELECT_INPUT_DAISY(input_daisy),
-					(mem_addr_t)input_register);
-		if (config_register)
+				    (mem_addr_t)input_register);
+		}
+		if (config_register) {
 			sys_write32(pin_ctrl_flags & (~(0x1 << MCUX_IMX_INPUT_ENABLE_SHIFT)),
-					(mem_addr_t)config_register);
+				    (mem_addr_t)config_register);
+		}
 #else
 		*((volatile uint32_t *)((uintptr_t)mux_register)) =
 			IOMUXC_SW_MUX_CTL_PAD_MUX_MODE(mux_mode) |
