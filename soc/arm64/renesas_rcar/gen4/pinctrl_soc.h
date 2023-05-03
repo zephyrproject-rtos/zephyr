e7c486f41c4a122e0ea243af62f2a81795d8ed05
Mykola Kvach <mykola_kvach@epam.com>
2023-05-03 10:56:46 +0300

    soc: arm64: add support of pinctrl for r8a779f
    Add support of pinctrl for r8a779f.
    
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>

---
+++ soc/arm64/renesas_rcar/gen4/pinctrl_soc.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2024 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_ARM64_RENESAS_RCAR_GEN4_PINCTRL_SOC_H_
+#define ZEPHYR_SOC_ARM64_RENESAS_RCAR_GEN4_PINCTRL_SOC_H_
+#include <pinctrl_rcar.h>
+
+#endif /* ZEPHYR_SOC_ARM64_RENESAS_RCAR_GEN4_PINCTRL_SOC_H_ */
