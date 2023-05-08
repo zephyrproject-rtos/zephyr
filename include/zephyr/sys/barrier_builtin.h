4e2b8b1dd12cf8a00b8856662f7f253760fd81d5
Carlo Caione <ccaione@baylibre.com>
2023-05-08 13:52:21 +0200

    barriers: builtin: Check for GCC-ism
    __atomic_thread_fence() is GCC-ism. When supported by the compiler
    (C11), use atomic_thread_fence() instead.
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

--- include/zephyr/sys/barrier_builtin.h
+++ include/zephyr/sys/barrier_builtin.h
@@ -19,12 +19,22 @@ extern "C" {
 
 static ALWAYS_INLINE void z_barrier_dmem_fence_full(void)
 {
+#if defined(__GNUC__)
+	/* GCC-ism */
 	__atomic_thread_fence(__ATOMIC_SEQ_CST);
+#else
+	atomic_thread_fence(memory_order_seq_cst);
+#endif
 }
 
 static ALWAYS_INLINE void z_barrier_dsync_fence_full(void)
 {
+#if defined(__GNUC__)
+	/* GCC-ism */
 	__atomic_thread_fence(__ATOMIC_SEQ_CST);
+#else
+	atomic_thread_fence(memory_order_seq_cst);
+#endif
 }
 
 static ALWAYS_INLINE void z_barrier_isync_fence_full(void)
