a17487f40ff77226a2a148d3d679e6606ce4ebe9
Maciej Sobkowski <msobkowski@antmicro.com>
2023-05-09 23:47:55 +0200

    soc: arm: ambiq: apollo4: Add support for Apollo4 Plus SoC
    Add all required parts (new SoC family/series, device tree) for
    the Ambiq Apollo4 Plus SoC.
    
    Signed-off-by: Maciej Sobkowski <msobkowski@antmicro.com>

---
+++ soc/arm/ambiq/apollo4x/soc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023 Antmicro <www.antmicro.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __SOC_H__
+#define __SOC_H__
+
+#include <apollo4p.h>
+
+#endif /* __SOC_H__ */
