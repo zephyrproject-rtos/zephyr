c81e21b2c185bac4cd9ba1d19f42eeb77aa29187
Benjamin Lemouzy <blemouzy@centralp.fr>
2023-05-11 14:32:08 +0200

    drivers: led: gpio: add software blinking
    Add new CONFIG_LED_GPIO_BLINK to add GPIO LEDs software blinking.
    
    Signed-off-by: Benjamin Lemouzy <blemouzy@centralp.fr>

--- drivers/led/led_gpio.c
+++ drivers/led/led_gpio.c
@@ -24,17 +24,37 @@ struct led_gpio_config {
 	const struct gpio_dt_spec *led;
 };
 
+#ifdef CONFIG_LED_GPIO_BLINK
+struct led_gpio_blink_data {
+	uint32_t delay_on;
+	uint32_t delay_off;
+	struct k_timer timer;
+};
+#endif /* CONFIG_LED_GPIO_BLINK */
+
+struct led_gpio_led_data {
+	bool is_on;
+#ifdef CONFIG_LED_GPIO_BLINK
+	struct led_gpio_blink_data blink;
+#endif /* CONFIG_LED_GPIO_BLINK */
+};
+
+struct led_gpio_data {
+	struct led_gpio_led_data *led;
+};
+
 static int led_gpio_set_brightness(const struct device *dev, uint32_t led, uint8_t value)
 {
-
 	const struct led_gpio_config *config = dev->config;
 	const struct gpio_dt_spec *led_gpio;
+	struct led_gpio_data *data = dev->data;
 
 	if ((led >= config->num_leds) || (value > 100)) {
 		return -EINVAL;
 	}
 
 	led_gpio = &config->led[led];
+	data->led[led].is_on = value > 0;
 
 	return gpio_pin_set_dt(led_gpio, value > 0);
 }
@@ -49,9 +69,59 @@ static int led_gpio_off(const struct device *dev, uint32_t led)
 	return led_gpio_set_brightness(dev, led, 0);
 }
 
+#ifdef CONFIG_LED_GPIO_BLINK
+static int led_gpio_blink(const struct device *dev, uint32_t led, uint32_t delay_on,
+			  uint32_t delay_off)
+{
+	const struct led_gpio_config *config = dev->config;
+	struct led_gpio_data *data = dev->data;
+
+	if (led >= config->num_leds) {
+		return -EINVAL;
+	}
+
+	data->led[led].blink.delay_on = delay_on;
+	data->led[led].blink.delay_off = delay_off;
+	if ((delay_on != 0) && (delay_off != 0)) {
+		k_timer_start(&data->led[led].blink.timer, K_MSEC(delay_on), K_NO_WAIT);
+	} else {
+		k_timer_stop(&data->led[led].blink.timer);
+	}
+
+	return 0;
+}
+
+static void blink_timeout(struct k_timer *timer)
+{
+	const struct device *dev = k_timer_user_data_get(timer);
+	const struct led_gpio_config *config = dev->config;
+	struct led_gpio_data *data = dev->data;
+	struct led_gpio_blink_data *blink_data =
+		CONTAINER_OF(timer, struct led_gpio_blink_data, timer);
+	struct led_gpio_led_data *led_data =
+		CONTAINER_OF(blink_data, struct led_gpio_led_data, blink);
+	uint32_t led = ARRAY_INDEX(data->led, led_data);
+
+	if (led >= config->num_leds) {
+		return;
+	}
+
+	if (led_data->is_on) {
+		led_gpio_off(dev, led);
+		k_timer_start(timer, K_MSEC(led_data->blink.delay_off), K_NO_WAIT);
+	} else {
+		led_gpio_on(dev, led);
+		k_timer_start(timer, K_MSEC(led_data->blink.delay_on), K_NO_WAIT);
+	}
+}
+#endif /* CONFIG_LED_GPIO_BLINK */
+
 static int led_gpio_init(const struct device *dev)
 {
 	const struct led_gpio_config *config = dev->config;
+#ifdef CONFIG_LED_GPIO_BLINK
+	struct led_gpio_data *data = dev->data;
+#endif /* CONFIG_LED_GPIO_BLINK */
 	int err = 0;
 
 	if (!config->num_leds) {
@@ -72,6 +142,11 @@ static int led_gpio_init(const struct device *dev)
 			LOG_ERR("%s: GPIO device not ready", dev->name);
 			err = -ENODEV;
 		}
+
+#ifdef CONFIG_LED_GPIO_BLINK
+		k_timer_init(&data->led[i].blink.timer, blink_timeout, NULL);
+		k_timer_user_data_set(&data->led[i].blink.timer, (void *)dev);
+#endif /* CONFIG_LED_GPIO_BLINK */
 	}
 
 	return err;
@@ -80,6 +155,9 @@ static int led_gpio_init(const struct device *dev)
 static const struct led_driver_api led_gpio_api = {
 	.on		= led_gpio_on,
 	.off		= led_gpio_off,
+#ifdef CONFIG_LED_GPIO_BLINK
+	.blink		= led_gpio_blink,
+#endif /* CONFIG_LED_GPIO_BLINK */
 	.set_brightness	= led_gpio_set_brightness,
 };
 
@@ -94,8 +172,16 @@ static const struct led_gpio_config led_gpio_config_##i = {	\
 	.led		= gpio_dt_spec_##i,			\
 };								\
 								\
+static struct led_gpio_led_data led_gpio_led_data_##i[ARRAY_SIZE(gpio_dt_spec_##i)] = {	\
+	{.is_on = false},			\
+};								\
+								\
+static struct led_gpio_data led_gpio_data_##i = {	\
+	.led = led_gpio_led_data_##i,	\
+};								\
+								\
 DEVICE_DT_INST_DEFINE(i, &led_gpio_init, NULL,			\
-		      NULL, &led_gpio_config_##i,		\
+		      &led_gpio_data_##i, &led_gpio_config_##i,		\
 		      POST_KERNEL, CONFIG_LED_INIT_PRIORITY,	\
 		      &led_gpio_api);
 
