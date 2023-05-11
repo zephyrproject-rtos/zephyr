c9c9054bfdf814f446c074dbb6c503cf7445d57e
Erwan Gouriou <erwan.gouriou@st.com>
2023-05-11 11:22:41 +0200

    boards: nucleo_wba52cg: Enable flash and debug using OpenOCD
    OpenOCD can now be used to flash and debug nucleo_wba52cg.
    However it required use of STMicroelectronics OpenOCD fork.
    
    Add instructions on how to use it.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@st.com>

--- boards/arm/nucleo_wba52cg/board.cmake
+++ boards/arm/nucleo_wba52cg/board.cmake
@@ -1,3 +1,7 @@
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 
+set(OPENOCD "/local/mcu/tools/openocd/src/openocd" CACHE FILEPATH "" FORCE)
+set(OPENOCD_DEFAULT_PATH /local/mcu/tools/openocd/tcl)
+
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
