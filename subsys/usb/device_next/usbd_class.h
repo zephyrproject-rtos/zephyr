00da7bdd8dcb56b8ce05a0e07cde45a6035d2d2f
Johann Fischer <johann.fischer@nordicsemi.no>
2023-05-15 11:52:25 +0200

    usb: device_next: do a little more cleanup on shutdown
    At the shutdown of USB device stack we have to cleanup and
    remove all registered class instances and string descriptors
    from a configuration.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- subsys/usb/device_next/usbd_class.h
+++ subsys/usb/device_next/usbd_class.h
@@ -93,4 +93,15 @@ struct usbd_class_node *usbd_class_get_by_ep(struct usbd_contex *uds_ctx,
 struct usbd_class_node *usbd_class_get_by_req(struct usbd_contex *uds_ctx,
 					      uint8_t request);
 
+/**
+ * @brief Remove all registered class instances from a configuration
+ *
+ * @param[in] uds_ctx Pointer to device context
+ * @param[in] cfg     Configuration number (bConfigurationValue)
+ *
+ * @return 0 on success, other values on fail.
+ */
+int usbd_class_remove_all(struct usbd_contex *const uds_ctx,
+			  const uint8_t cfg);
+
 #endif /* ZEPHYR_INCLUDE_USBD_CLASS_H */
