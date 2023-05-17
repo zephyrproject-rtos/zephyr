b6af41cd1463644ae3f334f5148a22f1ba078614
Rihards Skuja <rihards.s@origin-robotics.com>
2023-05-17 12:34:47 +0300

    drivers: sensor: ntc_thermistor: simplify binary search cmp method a bit
    Remove redundant comparisons that are already implied by previous ones.
    
    Signed-off-by: Rihards Skuja <rihards.s@origin-robotics.com>

--- drivers/sensor/ntc_thermistor/ntc_thermistor_calc.c
+++ drivers/sensor/ntc_thermistor/ntc_thermistor_calc.c
@@ -56,12 +56,11 @@ int ntc_compensation_compare_ohm(const struct ntc_type *type, const void *key, c
 		}
 	} else if (ntc_key->ohm == element_val->ohm) {
 		sgn = 0;
-	} else if (ntc_key->ohm < element_val->ohm) {
+	} else {
 		if (element_idx == (type->n_comp / 2) - 1) {
 			sgn = 0;
 		} else {
-			if (element_idx != (type->n_comp / 2) - 1 &&
-			    ntc_key->ohm > type->comp[element_idx + 1].ohm) {
+			if (ntc_key->ohm > type->comp[element_idx + 1].ohm) {
 				sgn = 0;
 			} else {
 				sgn = 1;
