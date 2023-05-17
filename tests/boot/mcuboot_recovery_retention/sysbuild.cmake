fbb26dc4a914f46a36101e718ca20a543712b9bf
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2023-05-17 13:59:28 +0200

    cmake: align Zephyr module variable to EXTRA_ZEPHYR_MODULES
    Align Zephyr modules with other user facing variables where settings
    can be defined or extended, meaning Zephyr modules now supports:
    ZEPHYR_MODULES and EXTRA_ZEPHYR_MODULES.
    
    Support for ZEPHYR_EXTRA_MODULES is kept foir backward compatibility.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- tests/boot/mcuboot_recovery_retention/sysbuild.cmake
+++ tests/boot/mcuboot_recovery_retention/sysbuild.cmake
@@ -1,1 +1,1 @@
-set(mcuboot_ZEPHYR_EXTRA_MODULES "${CMAKE_CURRENT_LIST_DIR}/test_module" CACHE INTERNAL "test_module directory")
+set(mcuboot_EXTRA_ZEPHYR_MODULES "${CMAKE_CURRENT_LIST_DIR}/test_module" CACHE INTERNAL "test_module directory")
