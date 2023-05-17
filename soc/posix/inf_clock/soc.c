202bb569519e0e2e2f96d20d669a87479b32a7f2
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-05-17 15:08:34 +0200

    soc posix: Refactor native_tasks into own file
    The native_tasks logic is unrelated to the remaining
    of the soc logic (which handles CPU start/stopping)
    refactor it out into its own file.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- soc/posix/inf_clock/soc.c
+++ soc/posix/inf_clock/soc.c
@@ -220,39 +220,6 @@ void posix_boot_cpu(void)
 }
 
 /**
- * @brief Run the set of special native tasks corresponding to the given level
- *
- * @param level One of _NATIVE_*_LEVEL as defined in soc.h
- */
-void run_native_tasks(int level)
-{
-	extern void (*__native_PRE_BOOT_1_tasks_start[])(void);
-	extern void (*__native_PRE_BOOT_2_tasks_start[])(void);
-	extern void (*__native_PRE_BOOT_3_tasks_start[])(void);
-	extern void (*__native_FIRST_SLEEP_tasks_start[])(void);
-	extern void (*__native_ON_EXIT_tasks_start[])(void);
-	extern void (*__native_tasks_end[])(void);
-
-	static void (**native_pre_tasks[])(void) = {
-		__native_PRE_BOOT_1_tasks_start,
-		__native_PRE_BOOT_2_tasks_start,
-		__native_PRE_BOOT_3_tasks_start,
-		__native_FIRST_SLEEP_tasks_start,
-		__native_ON_EXIT_tasks_start,
-		__native_tasks_end
-	};
-
-	void (**fptr)(void);
-
-	for (fptr = native_pre_tasks[level]; fptr < native_pre_tasks[level+1];
-		fptr++) {
-		if (*fptr) { /* LCOV_EXCL_BR_LINE */
-			(*fptr)();
-		}
-	}
-}
-
-/**
  * Clean up all memory allocated by the SOC and POSIX core
  *
  * This function can be called from both HW and SW threads
