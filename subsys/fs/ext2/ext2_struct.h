07775ce25ce06082c1d648fb99910f59835a6048
Franciszek Zdobylak <fzdobylak@antmicro.com>
2023-05-22 11:24:06 +0200

    fs: ext2: Use disk structs to access data on disk
    This commits changes how data on the disk is accessed. There are disk
    structures which are packed and their fields are stored in little endian
    byte order. To use data in the program structures it has to be translated
    from little endian to cpu endianness.
    
    Signed-off-by: Franciszek Zdobylak <fzdobylak@antmicro.com>

--- subsys/fs/ext2/ext2_struct.h
+++ subsys/fs/ext2/ext2_struct.h
@@ -56,7 +56,7 @@ struct ext2_disk_superblock {
 	uint32_t s_journal_dev;
 	uint32_t s_last_orphan;
 	uint8_t  s_padding[788];
-};
+} __packed;
 
 struct ext2_disk_bgroup {
 	uint32_t bg_block_bitmap;
@@ -67,7 +67,7 @@ struct ext2_disk_bgroup {
 	uint16_t bg_used_dirs_count;
 	uint16_t bg_pad;
 	uint8_t bg_reserved[12];
-};
+} __packed;
 
 struct ext2_disk_inode {
 	uint16_t i_mode;
@@ -88,24 +88,47 @@ struct ext2_disk_inode {
 	uint32_t i_dir_acl;
 	uint32_t i_faddr;
 	uint8_t i_osd2[12];
-};
+} __packed;
 
-struct ext2_disk_dentry {
+struct ext2_disk_direntry {
 	uint32_t de_inode;
 	uint16_t de_rec_len;
 	uint8_t de_name_len;
 	uint8_t de_file_type;
 	char de_name[];
-};
-
-/* Max size of directory entry */
-#define MAX_DIRENTRY_SIZE (sizeof(struct ext2_disk_dentry) + UINT8_MAX)
+} __packed;
 
 /* Program structures ------------------------------------------------------- */
 
+struct ext2_superblock {
+	uint32_t s_inodes_count;
+	uint32_t s_blocks_count;
+	uint32_t s_free_blocks_count;
+	uint32_t s_free_inodes_count;
+	uint32_t s_first_data_block;
+	uint32_t s_log_block_size;
+	uint32_t s_log_frag_size;
+	uint32_t s_blocks_per_group;
+	uint32_t s_frags_per_group;
+	uint32_t s_inodes_per_group;
+	uint16_t s_mnt_count;
+	uint16_t s_max_mnt_count;
+	uint16_t s_magic;
+	uint16_t s_state;
+	uint16_t s_errors;
+	uint32_t s_creator_os;
+	uint32_t s_rev_level;
+	uint32_t s_first_ino;
+	uint16_t s_inode_size;
+	uint16_t s_block_group_nr;
+	uint32_t s_feature_compat;
+	uint32_t s_feature_incompat;
+	uint32_t s_feature_ro_compat;
+};
+
 #define EXT2_BLOCK_NUM_SIZE (sizeof(uint32_t))
-#define EXT2_NEXT_DISK_DIRENTRY(de) \
-	((struct ext2_disk_dentry *)(((uint8_t *)(de)) + (de)->de_rec_len))
+#define EXT2_DISK_DIRENTRY_BY_OFFSET(addr, offset) \
+	((struct ext2_disk_direntry *)(((uint8_t *)(addr)) + (offset)))
 
 #define EXT2_BLOCK_ASSIGNED BIT(0)
 
@@ -115,7 +138,6 @@ struct ext2_block {
 	uint8_t *data;
 } __aligned(sizeof(void *));
 
-#define BGROUP_BLOCK(bg) ((struct ext2_disk_bgroup *)(bg)->block->data)
 #define BGROUP_INODE_TABLE(bg) ((struct ext2_disk_inode *)(bg)->inode_table->data)
 #define BGROUP_INODE_BITMAP(bg) ((uint8_t *)(bg)->inode_bitmap->data)
 #define BGROUP_BLOCK_BITMAP(bg) ((uint8_t *)(bg)->block_bitmap->data)
@@ -123,21 +145,20 @@ struct ext2_block {
 struct ext2_bgroup {
 	struct ext2_data *fs;       /* pointer to file system data */
 
-	struct ext2_block *block;        /* block of current block group */
 	struct ext2_block *inode_table;  /* fetched block of inode table */
 	struct ext2_block *inode_bitmap; /* inode bitmap */
 	struct ext2_block *block_bitmap; /* block bitmap */
 
-	uint32_t num;               /* number of described block group */
-	uint32_t num_in_block;      /* number of group in fetched block */
-	uint32_t bgroup_block;      /* number of fetched block (relative) */
+	int32_t num;                /* number of described block group */
 	uint32_t inode_table_block; /* number of fetched block (relative) */
-};
 
-static inline struct ext2_disk_bgroup *current_disk_bgroup(struct ext2_bgroup *bg)
-{
-	return &BGROUP_BLOCK(bg)[bg->num_in_block];
-}
+	uint32_t bg_block_bitmap;
+	uint32_t bg_inode_bitmap;
+	uint32_t bg_inode_table;
+	uint16_t bg_free_blocks_count;
+	uint16_t bg_free_inodes_count;
+	uint16_t bg_used_dirs_count;
+};
 
 /* Flags for inode */
 #define INODE_FETCHED_BLOCK BIT(0)
@@ -171,6 +192,17 @@ static inline uint8_t *inode_current_block_mem(struct ext2_inode *inode)
 	return (uint8_t *)inode_current_block(inode)->data;
 }
 
+struct ext2_direntry {
+	uint32_t de_inode;
+	uint16_t de_rec_len;
+	uint8_t de_name_len;
+	uint8_t de_file_type;
+	char de_name[];
+};
+
+/* Max size of directory entry that could be allocated from heap. */
+#define MAX_DIRENTRY_SIZE (sizeof(struct ext2_direntry) + UINT8_MAX)
+
 /* Structure common for files and directories representation */
 struct ext2_file {
 	struct ext2_inode *f_inode;
@@ -180,10 +212,6 @@ struct ext2_file {
 
 #define EXT2_DATA_FLAGS_RO  BIT(0)
 #define EXT2_DATA_FLAGS_ERR BIT(1)
-
-/* Accessing superblock disk structure (it is at some offset in stored block) */
-#define EXT2_DATA_SBLOCK(fs) \
-	((struct ext2_disk_superblock *)((uint8_t *)(fs)->sblock->data + (fs)->sblock_offset))
 
 struct ext2_data;
 
@@ -199,8 +227,8 @@ struct ext2_backend_ops {
 #define MAX_INODES (CONFIG_MAX_FILES + 2)
 
 struct ext2_data {
-	struct ext2_block *sblock; /* superblock */
-	struct ext2_bgroup bgroup; /* block group */
+	struct ext2_superblock sblock; /* superblock */
+	struct ext2_bgroup bgroup;     /* block group */
 
 	int32_t open_inodes;
 	int32_t open_files;
