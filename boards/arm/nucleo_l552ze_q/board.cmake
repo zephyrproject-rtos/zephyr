a7251405cecaacb2c8d873a4225d44e3ac5edc7d
Scott Laboe <scottyl2.718@gmail.com>
2023-05-24 15:51:20 +0000

    Added jlink runner for STM32 Nucleo L552ZE_Q board
    Signed-off-by: Scott Laboe <scottyl2.718@gmail.com>

--- boards/arm/nucleo_l552ze_q/board.cmake
+++ boards/arm/nucleo_l552ze_q/board.cmake
@@ -16,6 +16,8 @@ set_ifndef(BOARD_DEBUG_RUNNER pyocd)
 set_ifndef(BOARD_FLASH_RUNNER pyocd)
 
 board_runner_args(pyocd "--target=stm32l552zetxq")
+board_runner_args(jlink "--device=STM32L552ZE" "--speed=4000" "--reset-after-load")
 
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
