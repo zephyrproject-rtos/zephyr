1d7029d095d138102baa3415b8a132c34a68c248
Tristan Honscheid <honscheid@google.com>
2023-05-24 15:39:12 -0600

    sensors: Create `sensor trig` subcommand
    This PR relocates the sensor trigger sample application from the
    `sensor_shell` sample to a subcommand in the actual sensor shell. The
    subcommand has a UI for enabling a given trigger on a given sensor.
    A built-in handler for the data_ready trigger is included that prints
    the latest data to the log. Currently, only `SENSOR_TRIG_DATA_READY` is
    supported but the groundwork is there to add others. Tested on a
    `tdk_robokit1` board.
    
    Signed-off-by: Tristan Honscheid <honscheid@google.com>

--- samples/sensor/sensor_shell/src/main.c
+++ samples/sensor/sensor_shell/src/main.c
@@ -4,28 +4,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <stdlib.h>
-#include <zephyr/drivers/sensor.h>
-#include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
-#include <zephyr/sys/iterable_sections.h>
-
-#include "trigger.h"
-
-LOG_MODULE_REGISTER(app);
+/*
+ * This sample app launches a shell. Interact with it using the `sensor` command. See
+ * `drivers/sensor/sensor_shell.c`. There is nothing to do in the main thread.
+ */
 
 int main(void)
 {
-	if (IS_ENABLED(CONFIG_INIT_TRIG_DATA_READY)) {
-		STRUCT_SECTION_FOREACH(sensor_info, sensor)
-		{
-			struct sensor_trigger trigger = {
-				.chan = SENSOR_CHAN_ALL,
-				.type = SENSOR_TRIG_DATA_READY,
-			};
-			sensor_trigger_set(sensor->dev, &trigger,
-					   sensor_shell_data_ready_trigger_handler);
-		}
-	}
 	return 0;
 }
