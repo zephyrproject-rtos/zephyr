c3cb5bd37600b96c67b14af3df23e8dcc4cf70ef
Dmitrii Golovanov <dmitrii.golovanov@intel.com>
2023-05-24 10:55:34 +0200

    boards: intel: ish: Improve Simics support
    Improve Simics support for `boards/intel/ish/intel_ish_5_8_0`
    for better integration with the simulator.
    
    Signed-off-by: Dmitrii Golovanov <dmitrii.golovanov@intel.com>

--- boards/intel/ish/board.cmake
+++ boards/intel/ish/board.cmake
@@ -1,9 +1,8 @@
+# Copyright (c) 2023-2024 Intel Corporation
+#
 # SPDX-License-Identifier: Apache-2.0
 
-set(SUPPORTED_EMU_PLATFORMS simics)
-
 if(CONFIG_BOARD_INTEL_ISH_5_8_0)
-  board_emu_args(simics "project=$ENV{SIMICS_PROJECT}")
   board_emu_args(simics "zephyr_elf=${APPLICATION_BINARY_DIR}/zephyr/${KERNEL_ELF_NAME}")
   board_emu_args(simics "zephyr_start_address=${CONFIG_SRAM_BASE_ADDRESS}")
   include(${ZEPHYR_BASE}/boards/common/simics.board.cmake)
