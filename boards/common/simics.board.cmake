9ea6f4741d9a028e6d35b53121b763180f7d0e9d
Dmitrii Golovanov <dmitrii.golovanov@intel.com>
2023-05-24 10:55:34 +0200

    boards: intel: ish: Improve Simics support
    Improve Simics support for `boards/intel/ish/intel_ish_5_8_0`
    for better integration with the simulator.
    
    Signed-off-by: Dmitrii Golovanov <dmitrii.golovanov@intel.com>

--- boards/common/simics.board.cmake
+++ boards/common/simics.board.cmake
@@ -1,5 +1,7 @@
-# Copyright (c) 2023 Intel Corporation
+# Copyright (c) 2023-2024 Intel Corporation
 #
 # SPDX-License-Identifier: Apache-2.0
+
+set(SUPPORTED_EMU_PLATFORMS simics)
 
 board_finalize_emu_args(simics)
