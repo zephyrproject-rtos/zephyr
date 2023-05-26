1ec513606d79f31b72092a50ffa4e65f5b4f3e8f
Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>
2023-05-26 13:23:35 +0100

    ARC: MWDT: rework GNU helper tools usage
    As of today MWDT toolchain in case of Zephyr may use few
    tools from GNU toolchain - GNU compiler for DTS preprocessing
    and objcopy for section renaming.
    
    Currently we were trying to find any GNU compiler & objcopy
    which start to cause compatibility issues for new targets -
    i.e. not every objcopy knows new ARC targets.
    
    Let's use ARC GNU tools from Zephyr SDK as we still usually
    require it when building with MWDT for other tools like DTC
    (device tree compiler)
    
    Signed-off-by: Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>
    Signed-off-by: Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>

--- cmake/compiler/arcmwdt/generic.cmake
+++ cmake/compiler/arcmwdt/generic.cmake
@@ -2,21 +2,8 @@
 
 # Configures CMake for using ccac
 
-# MWDT compiler (CCAC) can't be used for preprocessing the DTS sources as it has
-# weird restrictions about file extensions. Synopsys Jira issue: P10019563-38578
-# Let's temporarily use GNU compiler instead.
-find_program(CMAKE_DTS_PREPROCESSOR arc-elf32-gcc)
-if (NOT CMAKE_DTS_PREPROCESSOR)
-  find_program(CMAKE_DTS_PREPROCESSOR arc-linux-gcc)
-endif()
-
-if (NOT CMAKE_DTS_PREPROCESSOR)
-  find_program(CMAKE_DTS_PREPROCESSOR gcc)
-endif()
-
-if(NOT CMAKE_DTS_PREPROCESSOR)
-  message(FATAL_ERROR "Zephyr was unable to find any GNU compiler (ARC or host one) for DTS preprocessing")
-endif()
+find_program(CMAKE_DTS_PREPROCESSOR ${ZEPHYR_SDK_CROSS_COMPILE}gcc PATHS ${ZEPHYR_SDK_INSTALL_DIR} NO_DEFAULT_PATH)
+message(STATUS "Found dts preprocessor: ${CMAKE_DTS_PREPROCESSOR} (Zephyr SDK ${SDK_VERSION})")
 
 find_program(CMAKE_C_COMPILER ${CROSS_COMPILE}ccac PATHS ${TOOLCHAIN_HOME} NO_DEFAULT_PATH)
 find_program(CMAKE_LLVM_COV ${CROSS_COMPILE}llvm-cov PATHS ${TOOLCHAIN_HOME} NO_DEFAULT_PATH)
