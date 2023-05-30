9384dcfa458fae43df2f3ef900b22c8a04b1c4b5
Andrii Bilynskyi <andrii.bilynskyi@telink-semi.com>
2023-05-30 15:02:00 +0300

    samples: boards: tlsr9518adk80d: Improve sample
    More precise task period calculation.
    
    Signed-off-by: Andrii Bilynskyi <andrii.bilynskyi@telink-semi.com>

--- samples/boards/tlsr9518adk80d_retention/basic/src/main.c
+++ samples/boards/tlsr9518adk80d_retention/basic/src/main.c
@@ -13,21 +13,23 @@ int main(void)
 	uint32_t cnt = 0;
 
 	printk("retention demo start\n");
-	/* Artificial delay (~2S) to have a possibility to flash using ICEman
-	 * - press and hold both reset buttons (flash and MCU)
-	 * - release MCU reset
-	 * - release flash reset
-	 * - start ICEman
-	 */
-	for (volatile uint32_t i = 0; i < 8000000; i++) {
+	/******************************************************************************
+	 * Artificial delay (~2S @ 48MHz) to have a possibility to flash using ICEman *
+	 * - press and hold both reset buttons (flash and MCU)                        *
+	 * - release MCU reset                                                        *
+	 * - release flash reset                                                      *
+	 * - start ICEman                                                             *
+	 ******************************************************************************/
+	for (volatile uint32_t i = 0; i < 7410838; i++) {
 		__asm("nop");
 	}
 	printk("ICEman guard finished\n");
 
 	while (1) {
-		printk("cnt: %u %llu\n", cnt, sys_clock_cycle_get_64());
-		cnt++;
-		k_msleep(SLEEP_TIME_MS);
+		uint64_t active_time_ms = k_uptime_get();
+
+		printk("cnt: %u, mtimer: %llu\n", cnt++, sys_clock_cycle_get_64());
+		k_sleep(K_TIMEOUT_ABS_MS(active_time_ms + SLEEP_TIME_MS));
 	}
 	return 0;
 }
