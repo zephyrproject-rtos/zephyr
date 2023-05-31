b17f0195ba4926fa7c0adbc0d93e86f60c8411e4
Dmytro Semenets <dmytro_semenets@epam.com>
2023-05-31 14:39:34 +0300

    drivers: xen: add xen version hypercall
    Xen API contains hypercall, which allows domains to identify Xen
    version, that is currently used on the system. It can be used to check
    if current version is supported by Zephyr or to change behavior of the
    drivers or services.
    
    Signed-off-by: Dmytro Semenets <dmytro_semenets@epam.com>
    Signed-off-by: Dmytro Firsov <dmytro_firsov@epam.com>

--- include/zephyr/arch/arm64/hypercall.h
+++ include/zephyr/arch/arm64/hypercall.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /*
- * Copyright (c) 2021-2023 EPAM Systems
+ * Copyright (c) 2021-2025 EPAM Systems
  */
 
 #ifndef ZEPHYR_INCLUDE_ARCH_ARM64_HYPERCALL_H_
@@ -13,6 +13,7 @@ int HYPERVISOR_event_channel_op(int op, void *param);
 int HYPERVISOR_hvm_op(int op, void *param);
 int HYPERVISOR_memory_op(int op, void *param);
 int HYPERVISOR_grant_table_op(int op, void *uop, unsigned int count);
+int HYPERVISOR_xen_version(int op, void *param);
 
 #ifdef CONFIG_XEN_DOM0
 int HYPERVISOR_domctl(void *param);
