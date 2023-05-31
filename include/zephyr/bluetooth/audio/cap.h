80c9668fc1832733c5395ac516936149c37c8df0
Emil Gydesen <emil.gydesen@nordicsemi.no>
2023-05-31 14:47:51 +0200

    Bluetooth: CAP: Add Initiator cancel procedure
    Add function to cancel any current proecedure. This is useful
    in cases where the connection to one or more acceptors is lost
    or if some acceptor does not respond to our requests for whatever
    reason.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- include/zephyr/bluetooth/audio/cap.h
+++ include/zephyr/bluetooth/audio/cap.h
@@ -78,10 +78,12 @@ struct bt_cap_initiator_cb {
 	 *
 	 * @param unicast_group  The unicast group pointer supplied to
 	 *                       bt_cap_initiator_unicast_audio_start().
-	 * @param err            0 if success, else BT_GATT_ERR() with a
-	 *                       specific ATT (BT_ATT_ERR_*) error code.
+	 * @param err            0 if success, BT_GATT_ERR() with a
+	 *                       specific ATT (BT_ATT_ERR_*) error code or -ECANCELED if cancelled
+	 *                       by bt_cap_initiator_unicast_audio_cancel().
 	 * @param conn           Pointer to the connection where the error
-	 *                       occurred. NULL if @p err is 0.
+	 *                       occurred. NULL if @p err is 0 or if cancelled by
+	 *                       bt_cap_initiator_unicast_audio_cancel()
 	 */
 	void (*unicast_start_complete)(struct bt_bap_unicast_group *unicast_group,
 				       int err, struct bt_conn *conn);
@@ -89,10 +91,12 @@ struct bt_cap_initiator_cb {
 	/**
 	 * @brief Callback for bt_cap_initiator_unicast_audio_update().
 	 *
-	 * @param err            0 if success, else BT_GATT_ERR() with a
-	 *                       specific ATT (BT_ATT_ERR_*) error code.
+	 * @param err            0 if success, BT_GATT_ERR() with a
+	 *                       specific ATT (BT_ATT_ERR_*) error code or -ECANCELED if cancelled
+	 *                       by bt_cap_initiator_unicast_audio_cancel().
 	 * @param conn           Pointer to the connection where the error
-	 *                       occurred. NULL if @p err is 0.
+	 *                       occurred. NULL if @p err is 0 or if cancelled by
+	 *                       bt_cap_initiator_unicast_audio_cancel()
 	 */
 	void (*unicast_update_complete)(int err, struct bt_conn *conn);
 
@@ -107,10 +111,12 @@ struct bt_cap_initiator_cb {
 	 *
 	 * @param unicast_group  The unicast group pointer supplied to
 	 *                       bt_cap_initiator_unicast_audio_stop().
-	 * @param err            0 if success, else BT_GATT_ERR() with a
-	 *                       specific ATT (BT_ATT_ERR_*) error code.
+	 * @param err            0 if success, BT_GATT_ERR() with a
+	 *                       specific ATT (BT_ATT_ERR_*) error code or -ECANCELED if cancelled
+	 *                       by bt_cap_initiator_unicast_audio_cancel().
 	 * @param conn           Pointer to the connection where the error
-	 *                       occurred. NULL if @p err is 0.
+	 *                       occurred. NULL if @p err is 0 or if cancelled by
+	 *                       bt_cap_initiator_unicast_audio_cancel()
 	 */
 	void (*unicast_stop_complete)(struct bt_bap_unicast_group *unicast_group,
 				      int err, struct bt_conn *conn);
@@ -268,6 +274,30 @@ int bt_cap_initiator_unicast_audio_update(const struct bt_cap_unicast_audio_upda
  * @return 0 on success or negative error value on failure.
  */
 int bt_cap_initiator_unicast_audio_stop(struct bt_bap_unicast_group *unicast_group);
+
+/** @brief Cancel any current Common Audio Profile procedure
+ *
+ * This will stop the current procedure from continuing and making it possible to run a new
+ * Common Audio Profile procedure.
+ *
+ * It is recommended to do this if any existing procedure take longer time than expected, which
+ * could indicate a missing response from the Common Audio Profile Acceptor.
+ *
+ * This does not send any requests to any Common Audio Profile Acceptors involved with the current
+ * procedure, and thus notifications from the Common Audio Profile Acceptors may arrive after this
+ * has been called. It is thus recommended to either only use this if a procedure has stalled, or
+ * wait a short while before starting any new Common Audio Profile procedure after this has been
+ * called to avoid getting notifications from the cancelled procedure. The wait time depends on
+ * the connection interval, the number of devices in the previous procedure and the behavior of the
+ * Common Audio Profile Acceptors.
+ *
+ * The respective callbacks of the procedure will be called as part of this with the connection
+ * pointer set to 0 and the err value set to -ECANCELED.
+ *
+ * @retval 0 on success
+ * @retval -EALREADY if no procedure is active
+ */
+int bt_cap_initiator_unicast_audio_cancel(void);
 
 struct bt_cap_initiator_broadcast_stream_param {
 	/** Audio stream */
