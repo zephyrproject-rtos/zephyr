7d4fe17691d389f21d25cf46c02a412eb10fad8e
Dmytro Semenets <dmytro_semenets@epam.com>
2023-05-31 14:43:54 +0300

    drivers: xen: dom0: add Xen sysctl hypercall
    This hypercall can be used get some information about physical machine
    and running guests:
    
    - sysctl hypercall "xen_sysctl_getphysinfo" allows read information about
    physical machine: number CPUs, memory sizes, hardware capabilities, etc.
    
    - sysctl hypercall "xen_sysctl_getdomaininfolist" returns array of domain
    info structures that provide information about particular domain(s).
    
    Signed-off-by: Dmytro Semenets <dmytro_semenets@epam.com>
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>
    Signed-off-by: Dmytro Firsov <dmytro_firsov@epam.com>

---
+++ drivers/xen/dom0/sysctl.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2025 EPAM Systems
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/arch/arm64/hypercall.h>
+#include <zephyr/xen/dom0/sysctl.h>
+#include <zephyr/xen/generic.h>
+#include <zephyr/xen/public/xen.h>
+
+static int do_sysctl(xen_sysctl_t *sysctl)
+{
+	sysctl->interface_version = XEN_SYSCTL_INTERFACE_VERSION;
+	return HYPERVISOR_sysctl(sysctl);
+}
+
+int xen_sysctl_physinfo(struct xen_sysctl_physinfo *info)
+{
+	int ret;
+	xen_sysctl_t sysctl = {
+		.cmd = XEN_SYSCTL_physinfo,
+	};
+
+	if (!info) {
+		return -EINVAL;
+	}
+
+	ret = do_sysctl(&sysctl);
+	if (ret < 0) {
+		return ret;
+	}
+	*info = sysctl.u.physinfo;
+
+	return ret;
+}
+
+int xen_sysctl_getdomaininfo(struct xen_domctl_getdomaininfo *domaininfo,
+			     uint16_t first, uint16_t num)
+{
+	int ret;
+	xen_sysctl_t sysctl = {
+		.cmd = XEN_SYSCTL_getdomaininfolist,
+		.u.getdomaininfolist.first_domain = first,
+		.u.getdomaininfolist.max_domains  = num,
+	};
+
+	if (!domaininfo || !num) {
+		return -EINVAL;
+	}
+	set_xen_guest_handle(sysctl.u.getdomaininfolist.buffer, domaininfo);
+
+	ret = do_sysctl(&sysctl);
+	if (ret < 0) {
+		return ret;
+	}
+
+	return sysctl.u.getdomaininfolist.num_domains;
+}
