27ee295820f23273ccdc5dcfa0c14f0fb61efaf3
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2023-06-01 18:02:22 +0300

    [DNM] libc: minimal: add minimal wchar.h header
    Currently MIPI_SYST_LIB requires a full C-library implementation
    just because of few definitions it needs from wchar.h.
    
    It's quite inconvenient to have to change libc implementation
    just to enable MIPI Sys-T logs, so this patch provides a minimal
    wchar.h definitions to cver needs of MIPI_SYST_LIB.
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

---
+++ lib/libc/minimal/include/wchar.h
@@ -0,0 +1,24 @@
+/* wchar.h */
+
+/*
+ *  Copyright(c) 2023 Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_WCHAR_H_
+#define ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_WCHAR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define WCHAR_MAX 0xFFFFU
+
+typedef int wint_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_WCHAR_H_ */
