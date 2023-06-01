ff649274c864f8e3d1504a47b18df98f538cbae2
Chirag Kochar <chirag.kochar@intel.com>
2023-06-01 17:33:03 +0000

    samples: basic: blinky: DO NOT UPSTREAM! Added prints for regtest
    This commit is for intel usage and code is not to be upstreamed.
    Enabled CONSOLE & PRINTS in GPIO blinky sample application for regtest.
    Change loop to run for finite cycle to complete the sample app.
    
    Signed-off-by: Chirag Kochar <chirag.kochar@intel.com>

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Intel Corporation
+ * Copyright (c) 2016-2023 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,6 +9,9 @@
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
+
+/* Toggle count */
+#define GPIO_TOGGLE_COUNT 10
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
@@ -22,22 +25,30 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 int main(void)
 {
 	int ret;
+	uint32_t count = GPIO_TOGGLE_COUNT;
 
+	printk("GPIO blinky application started\n");
 	if (!gpio_is_ready_dt(&led)) {
+		printk("GPIO not ready\n");
 		return 0;
 	}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
+		printk("GPIO pin configuration failed\n");
 		return 0;
 	}
 
-	while (1) {
+	printk("GPIO toggle started for %d times\n", GPIO_TOGGLE_COUNT);
+	while (count) {
 		ret = gpio_pin_toggle_dt(&led);
 		if (ret < 0) {
+			printk("GPIO pin toggle failed\n");
 			return 0;
 		}
 		k_msleep(SLEEP_TIME_MS);
+		count--;
 	}
+	printk("GPIO blinky Application completed!!!\n");
 	return 0;
 }
