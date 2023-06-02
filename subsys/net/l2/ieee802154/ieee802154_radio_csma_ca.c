ffcae5f0297189ff8591deff91ac11d5dc7fb439
Florian Grandel <fgrandel@code-for-humans.de>
2023-06-02 19:34:52 +0200

    net: l2: ieee802154: separate CCA and retransmission
    The IEEE 802.15.4 standard clearly separates clear channel assessment
    from retransmission. This separation of concern was not represented in
    the current channel access vs. retransmission implementation which
    resulted in considerable duplication of code and logic.
    
    This change removes the duplication of logic and encapsulates the
    resulting functions in a private API that may only be used from within
    Zephyr's native L2 layer.
    
    Signed-off-by: Florian Grandel <fgrandel@code-for-humans.de>

--- subsys/net/l2/ieee802154/ieee802154_radio_csma_ca.c
+++ subsys/net/l2/ieee802154/ieee802154_radio_csma_ca.c
@@ -9,122 +9,64 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(net_ieee802154_csma, CONFIG_NET_L2_IEEE802154_LOG_LEVEL);
 
-#include <zephyr/net/net_core.h>
-#include <zephyr/net/net_if.h>
 #include <zephyr/net/ieee802154.h>
-
-#include <zephyr/sys/util.h>
+#include <zephyr/net/ieee802154_radio.h>
+#include <zephyr/net/net_if.h>
 #include <zephyr/random/rand32.h>
+#include <zephyr/toolchain.h>
 
-#include <stdlib.h>
 #include <errno.h>
+#include <stdlib.h>
 
-#include "ieee802154_frame.h"
+#include "ieee802154_priv.h"
 #include "ieee802154_utils.h"
-#include "ieee802154_radio_utils.h"
 
 BUILD_ASSERT(CONFIG_NET_L2_IEEE802154_RADIO_CSMA_CA_MIN_BE <=
 		     CONFIG_NET_L2_IEEE802154_RADIO_CSMA_CA_MAX_BE,
 	     "The CSMA/CA min backoff exponent must be less or equal max backoff exponent.");
 
 /* See section 6.2.5.1. */
-static inline int unslotted_csma_ca_radio_send(struct net_if *iface, struct net_pkt *pkt,
-					       struct net_buf *frag)
+static inline int unslotted_csma_ca_channel_access(struct net_if *iface)
 {
 	uint8_t be = CONFIG_NET_L2_IEEE802154_RADIO_CSMA_CA_MIN_BE;
-	struct ieee802154_context *ctx = net_if_l2_data(iface);
 	uint32_t symbol_period, turnaround_time;
-	uint8_t remaining_attempts;
-	bool ack_required;
 	bool is_subg_phy;
-	uint8_t nb = 0U;
-	int ret;
-
-	NET_DBG("frag %p", frag);
 
 	is_subg_phy = ieee802154_get_hw_capabilities(iface) & IEEE802154_HW_SUB_GHZ;
 	/* TODO: Move symbol period calculation to radio driver. */
 	symbol_period = IEEE802154_PHY_SYMBOL_PERIOD(is_subg_phy);
 	turnaround_time = IEEE802154_PHY_A_TURNAROUND_TIME(is_subg_phy);
 
-	/* See section 6.7.4.4 - Retransmissions. */
-	remaining_attempts = CONFIG_NET_L2_IEEE802154_RADIO_TX_RETRIES + 1;
+	for (uint8_t nb = 0U; nb <= CONFIG_NET_L2_IEEE802154_RADIO_CSMA_CA_MAX_BO; nb++) {
+		int ret;
 
-	while (remaining_attempts) {
-		remaining_attempts--;
+		if (be) {
+			uint8_t bo_n = sys_rand32_get() & ((1 << be) - 1);
 
-		while (1) {
-			if (be) {
-				uint8_t bo_n = sys_rand32_get() & ((1 << be) - 1);
-
-				/* TODO: k_busy_wait() is too inaccurate on many platforms, the
-				 * radio API should expose a precise radio clock instead (which may
-				 * fall back to k_busy_wait() if the radio does not have a clock).
-				 */
-				k_busy_wait(bo_n * IEEE802154_A_UNIT_BACKOFF_PERIOD_US(
-							   turnaround_time, symbol_period));
-			}
-
-			ret = ieee802154_cca(iface);
-			if (ret == 0) {
-				/* Channel is idle -> CSMA Success */
-				break;
-			} else if (ret != -EBUSY) {
-				/* CCA exited with failure code. */
-				return -EIO;
-			}
-
-			/* Channel is not idle -> CSMA Backoff */
-			be = MIN(be + 1, CONFIG_NET_L2_IEEE802154_RADIO_CSMA_CA_MAX_BE);
-			nb++;
-
-			if (nb > CONFIG_NET_L2_IEEE802154_RADIO_CSMA_CA_MAX_BO) {
-				return -EIO;
-			}
-		}
-
-		ack_required = prepare_for_ack(ctx, pkt, frag);
-
-		if (!ack_required) {
-			/* See section 6.7.4.4: "A device that sends a frame with the AR field set
-			 * to indicate no acknowledgment requested may assume that the transmission
-			 * was successfully received and shall not perform the retransmission
-			 * procedure."
+			/* TODO: k_busy_wait() is too inaccurate on many platforms, the
+			 * radio API should expose a precise radio clock instead (which may
+			 * fall back to k_busy_wait() if the radio does not have a clock).
 			 */
-			remaining_attempts = 0;
+			k_busy_wait(bo_n * IEEE802154_A_UNIT_BACKOFF_PERIOD_US(turnaround_time,
+									       symbol_period));
 		}
 
-		ret = ieee802154_tx(iface, IEEE802154_TX_MODE_DIRECT,
-				    pkt, frag);
-		if (ret) {
-			continue;
+		ret = ieee802154_cca(iface);
+		if (ret == 0) {
+			/* Channel is idle -> CSMA Success */
+			return 0;
+		} else if (ret != -EBUSY) {
+			/* CCA exited with failure code -> CSMA Abort */
+			return -EIO;
 		}
 
-		ret = wait_for_ack(iface, ack_required);
-		if (!ret) {
-			break;
-		}
+		/* Channel is busy -> CSMA Backoff */
+		be = MIN(be + 1, CONFIG_NET_L2_IEEE802154_RADIO_CSMA_CA_MAX_BE);
 	}
 
-	return ret;
+	/* Channel is still busy after max backoffs -> CSMA Failure */
+	return -EBUSY;
 }
 
-static enum net_verdict csma_ca_radio_handle_ack(struct net_if *iface,
-						 struct net_pkt *pkt)
-{
-	struct ieee802154_context *ctx = net_if_l2_data(iface);
-
-	if (IS_ENABLED(CONFIG_NET_L2_IEEE802154_RADIO_CSMA_CA) &&
-	    ieee802154_get_hw_capabilities(iface) & IEEE802154_HW_CSMA) {
-		return NET_OK;
-	}
-
-	return handle_ack(ctx, pkt);
-}
-
-/* Declare the public Radio driver function used by the HW drivers */
-FUNC_ALIAS(unslotted_csma_ca_radio_send,
-	   ieee802154_radio_send, int);
-
-FUNC_ALIAS(csma_ca_radio_handle_ack,
-	   ieee802154_radio_handle_ack, enum net_verdict);
+/* Declare the public channel access algorithm function used by L2. */
+FUNC_ALIAS(unslotted_csma_ca_channel_access, ieee802154_wait_for_clear_channel, int);
