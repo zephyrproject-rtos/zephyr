d882f72a844386e09cb853b1229f7ce5856a691e
Andriy Gelman <andriy.gelman@gmail.com>
2023-06-04 21:36:17 -0400

    drivers: pwm: Add driver for xmc4xxx using ccu4 module
    Adds driver for pwm on xmc4xxx using Capture Compare Unit 4 (CCU4)
    module. There are four CCU4 with each one having four channels
    Thus it's possible to have up to 16 pwm output signals. The output of
    each channel can only be connected to a specific port/pin. The possible
    connection and gpio configurations are defined using pinctrl.
    
    The CCU4 module also has a capture mode. Capture support will be added
    in the future.
    
    Signed-off-by: Andriy Gelman <andriy.gelman@gmail.com>

--- soc/arm/infineon_xmc/4xxx/soc.c
+++ soc/arm/infineon_xmc/4xxx/soc.c
@@ -31,7 +31,11 @@ void z_arm_platform_init(void)
 	temp |= PMU_FLASH_WS;
 	FLASH0->FCON = temp;
 
-	XMC_SCU_CLOCK_SetSleepConfig(XMC_SCU_CLOCK_SLEEP_MODE_CONFIG_SYSCLK_FPLL);
+	XMC_SCU_CLOCK_SetSleepConfig(XMC_SCU_CLOCK_SLEEP_MODE_CONFIG_SYSCLK_FPLL
+#ifdef CONFIG_PWM_XMC4XXX_CCU4
+				     | XMC_SCU_CLOCK_SLEEP_MODE_CONFIG_ENABLE_CCU
+#endif
+	);
 
 	/* configure PLL & system clock */
 	SystemCoreClockSetup();
