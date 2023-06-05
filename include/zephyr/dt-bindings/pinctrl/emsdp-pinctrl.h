8ea22115b0e55cf34f9f99744293f8c834d0d159
Siyuan Cheng <siyuanc@synopsys.com>
2023-06-05 14:56:36 +0800

    drivers: pinctrl_emsdp: fix definition location
    Mux Control Register Index are internals of driver, now
    moved from dt-binding header to driver itself.
    
    Signed-off-by: Siyuan Cheng <siyuanc@synopsys.com>

--- include/zephyr/dt-bindings/pinctrl/emsdp-pinctrl.h
+++ include/zephyr/dt-bindings/pinctrl/emsdp-pinctrl.h
@@ -7,13 +7,6 @@
 #ifndef ZEPHYR_INCLUDE_DT_BINDINGS_PINCTRL_EMSDP_PINCTRL_H_
 #define ZEPHYR_INCLUDE_DT_BINDINGS_PINCTRL_EMSDP_PINCTRL_H_
 
-/**
- * Mux Control Register Index
- */
-#define PMOD_MUX_CTRL 0 /*!< 32-bits, offset 0x0 */
-
-#define ARDUINO_MUX_CTRL 4 /*!< 32-bits, offset 0x4 */
-
 #define PMOD_A              0
 #define PMOD_B              1
 #define PMOD_C              2
@@ -37,7 +30,7 @@
 #define ARDUINO_PIN_AD3     20
 #define ARDUINO_PIN_AD4     21
 #define ARDUINO_PIN_AD5     22
-#define INNER_CONNECT       23
+#define UNMUXED_PIN       23
 
 #define PMOD_GPIO           0
 #define PMOD_UARTA          1
