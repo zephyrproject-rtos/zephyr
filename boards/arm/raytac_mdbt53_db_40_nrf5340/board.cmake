f86489c1832566120c00f99fb1ab031e91b7a902
Stanley Huang <stanley@raytac.com>
2023-06-05 17:00:00 +0800

    boards: arm: add raytac_mdbt53_db_40_nrf5340
    Adds new raytac_mdbt53_db_40_nrf5340 board.
    
    Signed-off-by: Stanley Huang <stanley@raytac.com>

---
+++ boards/arm/raytac_mdbt53_db_40_nrf5340/board.cmake
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: Apache-2.0
+
+if(CONFIG_BOARD_RAYTAC_MDBT53_DB_40_NRF5340_CPUAPP OR CONFIG_BOARD_RAYTAC_MDBT53_DB_40_NRF5340_CPUAPP_NS)
+  board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
+endif()
+
+if(CONFIG_BOARD_RAYTAC_MDBT53_DB_40_NRF5340_CPUNET)
+  board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
+endif()
+
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
