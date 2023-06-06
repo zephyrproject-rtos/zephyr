b9411b75df77a58fac9e9ba71815c4143ed351ad
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2023-06-06 23:36:14 +0530

    net: ethernet: Fix build and doc error
    The API should be defined independent of L2_PTP define, this fixes build
    error and also the doc error, as the next function will have two doxygen
    headers.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- include/zephyr/net/ethernet.h
+++ include/zephyr/net/ethernet.h
@@ -1021,6 +1021,12 @@ static inline int net_eth_get_ptp_port(struct net_if *iface)
  */
 #if defined(CONFIG_NET_L2_PTP)
 void net_eth_set_ptp_port(struct net_if *iface, int port);
+#else
+static inline void net_eth_set_ptp_port(struct net_if *iface, int port)
+{
+	ARG_UNUSED(iface);
+	ARG_UNUSED(port);
+}
 #endif /* CONFIG_NET_L2_PTP */
 
 /**
