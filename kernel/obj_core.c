5b1a9752bd650f0b824f54a3c73c77232eeccbb9
Peter Mitsis <peter.mitsis@intel.com>
2023-06-09 14:23:25 -0400

    kernel: Add initial k_obj_core_stats infrastructure
    Adds the infrastructure to integrate statistics into
    the object core.
    
    Signed-off-by: Peter Mitsis <peter.mitsis@intel.com>

--- kernel/obj_core.c
+++ kernel/obj_core.c
@@ -26,6 +26,9 @@ void k_obj_core_init(struct k_obj_core *obj_core, struct k_obj_type *type)
 {
 	obj_core->node.next = NULL;
 	obj_core->type = type;
+#ifdef CONFIG_OBJ_CORE_STATS
+	obj_core->stats = NULL;
+#endif
 }
 
 void k_obj_core_link(struct k_obj_core *obj_core)
@@ -117,3 +120,236 @@ int k_obj_type_walk_unlocked(struct k_obj_type *type,
 
 	return status;
 }
+
+#ifdef CONFIG_OBJ_CORE_STATS
+int k_obj_core_stats_register(struct k_obj_core *obj_core, void *stats,
+			      size_t stats_len)
+{
+	__ASSERT((obj_core != NULL) && (stats != NULL), "NULL parameter");
+
+	k_spinlock_key_t  key = k_spin_lock(&lock);
+
+	__ASSERT(obj_core->type != NULL, "Object core not initialized");
+
+	if (obj_core->type->stats_desc == NULL) {
+
+		/* Object type not configured for statistics. */
+
+		k_spin_unlock(&lock, key);
+		return -ENOTSUP;
+	}
+
+	if (obj_core->type->stats_desc->raw_size != stats_len) {
+
+		/* Buffer size mismatch */
+
+		k_spin_unlock(&lock, key);
+		return -EINVAL;
+	}
+
+	obj_core->stats = stats;
+	k_spin_unlock(&lock, key);
+
+	return 0;
+}
+
+int k_obj_core_stats_deregister(struct k_obj_core *obj_core)
+{
+	__ASSERT((obj_core != NULL), "NULL parameter");
+
+	k_spinlock_key_t  key = k_spin_lock(&lock);
+
+	__ASSERT(obj_core->type != NULL, "Object core not initialized");
+
+	if (obj_core->type->stats_desc == NULL) {
+
+		/* Object type not configured  for statistics. */
+
+		k_spin_unlock(&lock, key);
+		return -ENOTSUP;
+	}
+
+	obj_core->stats = NULL;
+	k_spin_unlock(&lock, key);
+
+	return 0;
+}
+
+int k_obj_core_stats_raw(struct k_obj_core *obj_core, void *stats,
+			 size_t stats_len)
+{
+	int  rv;
+	struct k_obj_core_stats_desc *desc;
+
+	__ASSERT((obj_core != NULL) && (stats != NULL), "NULL parameter");
+
+	k_spinlock_key_t  key = k_spin_lock(&lock);
+
+	__ASSERT(obj_core->type != NULL, "Object core not initialized");
+
+	desc = obj_core->type->stats_desc;
+	if ((desc == NULL) || (desc->raw == NULL)) {
+
+		/* The object type is not configured for this operation */
+
+		k_spin_unlock(&lock, key);
+		return -ENOTSUP;
+	}
+
+	if ((desc->raw_size != stats_len) || (obj_core->stats == NULL)) {
+
+		/*
+		 * Either the size of the stats buffer is wrong or
+		 * the kernel object was not registered for statistics.
+		 */
+
+		k_spin_unlock(&lock, key);
+		return -EINVAL;
+	}
+
+	rv = desc->raw(obj_core, stats);
+	k_spin_unlock(&lock, key);
+
+	return rv;
+}
+
+int k_obj_core_stats_query(struct k_obj_core *obj_core, void *stats,
+			   size_t stats_len)
+{
+	int  rv;
+	struct k_obj_core_stats_desc *desc;
+
+	__ASSERT((obj_core != NULL) && (stats != NULL), "NULL parameter");
+
+	k_spinlock_key_t  key = k_spin_lock(&lock);
+
+	__ASSERT(obj_core->type != NULL, "Object core not initialized");
+
+	desc = obj_core->type->stats_desc;
+	if ((desc == NULL) || (desc->query == NULL)) {
+
+		/* The object type is not configured for this operation */
+
+		k_spin_unlock(&lock, key);
+		return -ENOTSUP;
+	}
+
+	if ((desc->query_size != stats_len) || (obj_core->stats == NULL)) {
+
+		/*
+		 * Either the size of the stats buffer is wrong or
+		 * the kernel object was not registered for statistics.
+		 */
+
+		k_spin_unlock(&lock, key);
+		return -EINVAL;
+	}
+
+	rv = desc->query(obj_core, stats);
+	k_spin_unlock(&lock, key);
+
+	return rv;
+}
+
+int k_obj_core_stats_reset(struct k_obj_core *obj_core)
+{
+	int  rv;
+	struct k_obj_core_stats_desc *desc;
+
+	__ASSERT((obj_core != NULL), "NULL parameter");
+
+	k_spinlock_key_t  key = k_spin_lock(&lock);
+
+	__ASSERT(obj_core->type != NULL, "Object core not initialized");
+
+	desc = obj_core->type->stats_desc;
+	if ((desc == NULL) || (desc->reset == NULL)) {
+
+		/* The object type is not configured for this operation */
+
+		k_spin_unlock(&lock, key);
+		return -ENOTSUP;
+	}
+
+	if (obj_core->stats == NULL) {
+
+		/* This kernel object is not configured for statistics */
+
+		k_spin_unlock(&lock, key);
+		return -EINVAL;
+	}
+
+	rv = desc->reset(obj_core);
+	k_spin_unlock(&lock, key);
+
+	return rv;
+}
+
+int k_obj_core_stats_disable(struct k_obj_core *obj_core)
+{
+	int  rv;
+	struct k_obj_core_stats_desc *desc;
+
+	__ASSERT((obj_core != NULL), "NULL parameter");
+
+	k_spinlock_key_t  key = k_spin_lock(&lock);
+
+	__ASSERT(obj_core->type != NULL, "Object core not initialized");
+
+	desc = obj_core->type->stats_desc;
+	if ((desc == NULL) || (desc->disable == NULL)) {
+
+		/* The object type is not configured for this operation */
+
+		k_spin_unlock(&lock, key);
+		return -ENOTSUP;
+	}
+
+	if (obj_core->stats == NULL) {
+
+		/* This kernel object is not configured for statistics */
+
+		k_spin_unlock(&lock, key);
+		return -EINVAL;
+	}
+
+	rv = desc->disable(obj_core);
+	k_spin_unlock(&lock, key);
+
+	return rv;
+}
+
+int k_obj_core_stats_enable(struct k_obj_core *obj_core)
+{
+	int  rv;
+	struct k_obj_core_stats_desc *desc;
+
+	__ASSERT((obj_core != NULL), "NULL parameter");
+
+	k_spinlock_key_t  key = k_spin_lock(&lock);
+
+	__ASSERT(obj_core->type != NULL, "Object core not initialized");
+
+	desc = obj_core->type->stats_desc;
+	if ((desc == NULL) || (desc->enable == NULL)) {
+
+		/* The object type is not configured for this operation */
+
+		k_spin_unlock(&lock, key);
+		return -ENOTSUP;
+	}
+
+	if (obj_core->stats == NULL) {
+
+		/* This kernel object is not configured for statistics */
+
+		k_spin_unlock(&lock, key);
+		return -EINVAL;
+	}
+
+	rv = desc->enable(obj_core);
+	k_spin_unlock(&lock, key);
+
+	return rv;
+}
+#endif
