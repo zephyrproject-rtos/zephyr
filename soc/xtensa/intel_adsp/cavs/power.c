a508598dfefec48eccfc27e6e9a2468d41276fe4
Jaska Uimonen <jaska.uimonen@linux.intel.com>
2023-06-13 14:17:21 +0300

    soc: xtensa: intel_adsp: fix memory bank shutdown
    Amount of memory banks should vary depending on platform, otherwise
    power down sequence might result in ipc timeout as the memory used by
    the ipc itself is shutdown.
    
    Lift the needed defines from sram.c to adsp_memory.h and shorten the
    names to avoid collision with sof code. No functional change to sram.c.
    
    Signed-off-by: Jaska Uimonen <jaska.uimonen@linux.intel.com>

--- soc/xtensa/intel_adsp/cavs/power.c
+++ soc/xtensa/intel_adsp/cavs/power.c
@@ -15,6 +15,7 @@
 #include <zephyr/cache.h>
 #include <cpu_init.h>
 
+#include <adsp_memory.h>
 #include <adsp_shim.h>
 #include <adsp_clk.h>
 #include <cavs-idc.h>
@@ -36,7 +37,6 @@ LOG_MODULE_REGISTER(soc);
 #ifdef CONFIG_PM_POLICY_CUSTOM
 #define SRAM_ALIAS_BASE		0x9E000000
 #define SRAM_ALIAS_MASK		0xFF000000
-#define EBB_BANKS_IN_SEGMENT	32
 #define SRAM_ALIAS_OFFSET	0x20000000
 
 #define L2_INTERRUPT_NUMBER     4
@@ -81,11 +81,12 @@ __weak void pm_state_set(enum pm_state state, uint8_t substate_id)
 		soc_cpus_active[cpu] = false;
 		sys_cache_data_flush_and_invd_all();
 		if (cpu == 0) {
-			uint32_t ebb = EBB_BANKS_IN_SEGMENT;
+			uint32_t hpsram_mask[HPSRAM_SEGMENTS];
 			/* turn off all HPSRAM banks - get a full bitmap */
-			uint32_t hpsram_mask = (1 << ebb) - 1;
+			for (int i = 0; i < HPSRAM_SEGMENTS; i++)
+				hpsram_mask[i] = HPSRAM_MEMMASK(i);
 			/* do power down - this function won't return */
-			power_down_cavs(true, uncache_to_cache(&hpsram_mask));
+			power_down_cavs(true, uncache_to_cache(&hpsram_mask[0]));
 		} else {
 			z_xt_ints_on(core_desc[cpu].intenable);
 			k_cpu_idle();
