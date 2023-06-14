14200f82dbaf47a6da688d5e3ee08c69b4248f1f
Théo Battrel <theo.battrel@nordicsemi.no>
2023-06-14 13:46:26 +0200

    Bluetooth: Tests: New bsim test for ID
    Add a new BabbleSim test checking that the value of the Bluetooth
    identity stored in the settings is correct.
    
    Signed-off-by: Théo Battrel <theo.battrel@nordicsemi.no>

--- tests/bsim/bluetooth/host/compile.sh
+++ tests/bsim/bluetooth/host/compile.sh
@@ -65,4 +65,6 @@ app=tests/bsim/bluetooth/host/security/bond_overwrite_denied compile
 app=tests/bsim/bluetooth/host/security/ccc_update compile
 app=tests/bsim/bluetooth/host/security/ccc_update conf_file=prj_2.conf compile
 
+app=tests/bsim/bluetooth/host/id/settings compile
+
 wait_for_background_jobs
