0eb8db3c6f07753e9846c9e966350f256071f771
Andrii Bilynskyi <andrii.bilynskyi@telink-semi.com>
2023-06-16 16:32:41 +0300

    samples: net: openthread: cli: Update sample
    - Get rid of deprecated interface
    - Print additional data
    
    Signed-off-by: Andrii Bilynskyi <andrii.bilynskyi@telink-semi.com>

--- samples/net/openthread/cli/src/main.c
+++ samples/net/openthread/cli/src/main.c
@@ -12,20 +12,31 @@ LOG_MODULE_REGISTER(ot_main, LOG_LEVEL_DBG);
 #include <zephyr/net/openthread.h>
 #include <openthread/thread.h>
 
-static void ot_satate_changed(uint32_t flags, void *context)
+static void ot_satate_changed(otChangedFlags flags,
+	struct openthread_context *ot_context, void *user_data)
 {
-	struct openthread_context *ot_context = context;
-
 	if (flags & OT_CHANGED_THREAD_ROLE) {
 		switch (otThreadGetDeviceRole(ot_context->instance)) {
 		case OT_DEVICE_ROLE_CHILD:
 			LOG_INF("OT child");
+			LOG_INF("OT Short address: %04x",
+				otLinkGetShortAddress(ot_context->instance));
+			LOG_HEXDUMP_INF(otLinkGetExtendedAddress(ot_context->instance),
+				OT_EXT_ADDRESS_SIZE, "OT Extended address:");
 			break;
 		case OT_DEVICE_ROLE_ROUTER:
 			LOG_INF("OT router");
+			LOG_INF("OT Short address: %04x",
+				otLinkGetShortAddress(ot_context->instance));
+			LOG_HEXDUMP_INF(otLinkGetExtendedAddress(ot_context->instance),
+				OT_EXT_ADDRESS_SIZE, "OT Extended address:");
 			break;
 		case OT_DEVICE_ROLE_LEADER:
 			LOG_INF("OT leader");
+			LOG_INF("OT Short address: %04x",
+				otLinkGetShortAddress(ot_context->instance));
+			LOG_HEXDUMP_INF(otLinkGetExtendedAddress(ot_context->instance),
+				OT_EXT_ADDRESS_SIZE, "OT Extended address:");
 			break;
 		case OT_DEVICE_ROLE_DISABLED:
 			LOG_INF("OT disabled");
@@ -45,9 +56,13 @@ void main(void)
 	LOG_INF("***** OpenThread CLI on Zephyr *****");
 #ifndef CONFIG_OPENTHREAD_MANUAL_START
 	LOG_INF("OT channel     %u",     CONFIG_OPENTHREAD_CHANNEL);
-	LOG_INF("OT pan id      %0x04x", CONFIG_OPENTHREAD_PANID);
+	LOG_INF("OT pan id      %04x",   CONFIG_OPENTHREAD_PANID);
 	LOG_INF("OT pan ext id  %s",     CONFIG_OPENTHREAD_XPANID);
 	LOG_INF("OT network key %s",     CONFIG_OPENTHREAD_NETWORKKEY);
 #endif /* CONFIG_OPENTHREAD_MANUAL_START */
-	openthread_set_state_changed_cb(ot_satate_changed);
+	static struct openthread_state_changed_cb ot_state_cahnge = {
+		.state_changed_cb = ot_satate_changed
+	};
+
+	openthread_state_changed_cb_register(openthread_get_default_context(), &ot_state_cahnge);
 }
