be7e1ea0cbbb21e004e29b8428747de09a777c49
Przemyslaw Bida <przemyslaw.bida@nordicsemi.no>
2023-06-19 13:14:05 +0200

    bsim: net: Add delay to echo client in ot bsim tests.
    This change ensures that openthread network topology will be formed
    properly, by delaing start of echo client application.
    
    Signed-off-by: Przemyslaw Bida <przemyslaw.bida@nordicsemi.no>

--- tests/bsim/net/sockets/echo_test/tests_scripts/echo_test_ot.sh
+++ tests/bsim/net/sockets/echo_test/tests_scripts/echo_test_ot.sh
@@ -18,13 +18,13 @@ EXECUTE_TIMEOUT=100
 cd ${BSIM_OUT_PATH}/bin
 
 Execute ./bs_${BOARD}_tests_bsim_net_sockets_echo_test_prj_conf_overlay-ot_conf \
-  -v=${verbosity_level} -s=${simulation_id} -d=0 -RealEncryption=1 \
+  -v=${verbosity_level} -s=${simulation_id} -start_offset=2e6 -d=0 -RealEncryption=1 \
   -testid=echo_client
 
 Execute ./bs_${BOARD}_samples_net_sockets_echo_server_prj_conf_overlay-ot_conf\
   -v=${verbosity_level} -s=${simulation_id} -d=1 -RealEncryption=1 \
 
 Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
-  -D=2 -sim_length=26e6 -argschannel -at=40 -argsmain $@
+  -D=2 -sim_length=40e6 -argschannel -at=40 -argsmain $@
 
 wait_for_background_jobs #Wait for all programs in background and return != 0 if any fails
