5ef91c22c3d21efb68937ea5196ab922e934bd3a
Prashanth S <slpp95prashanth@yahoo.com>
2023-06-22 10:21:40 +0530

    soc: ti_k3: Add TI J721E SoC R5
    Add initial SoC support for the TI J721E SoC series Cortex-R5 core.
    
    TRM for J721e https://www.ti.com/lit/zip/spruil1
    File: spruil1c.pdf
    
    Signed-off-by: Prashanth S <slpp95prashanth@yahoo.com>

---
+++ soc/arm/ti_k3/j721e/soc.h
@@ -0,0 +1,10 @@
+/* Copyright (C) 2023 BeagleBoard.org Foundation
+ * Copyright (C) 2023 S Prashanth
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _TI_K3_J721E_R5_SOC_H_
+#define _TI_K3_J721E_R5_SOC_H_
+
+#endif /* _TI_K3_J721E_R5_SOC_H_ */
