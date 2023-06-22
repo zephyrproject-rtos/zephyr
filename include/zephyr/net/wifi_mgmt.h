a71bff7f49f78aa3d8526ab71e4c9709e9bc51aa
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2023-06-23 00:49:04 +0530

    net: wifi: Fix power save timeout data type
    This should be an unsigned integer. Also, add a comment to explain this
    feature.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- include/zephyr/net/wifi_mgmt.h
+++ include/zephyr/net/wifi_mgmt.h
@@ -203,7 +203,13 @@ struct wifi_ps_params {
 	unsigned short listen_interval;
 	enum wifi_ps_wakeup_mode wakeup_mode;
 	enum wifi_ps_mode mode;
-	int timeout_ms;
+	/* This is the time out to wait after sending a TX packet
+	 * before going back to power save (in ms) to receive any replies
+	 * from the AP. Zero means this feature is disabled.
+	 *
+	 * It's a tradeoff between power consumption and latency.
+	 */
+	unsigned int timeout_ms;
 	enum ps_param_type type;
 	enum wifi_config_ps_param_fail_reason fail_reason;
 };
