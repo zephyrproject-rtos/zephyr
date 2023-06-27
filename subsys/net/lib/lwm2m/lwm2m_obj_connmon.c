7ae4e2472808648e3c13e887c04de2a0306eceb6
Jarno Lämsä <jarno.lamsa@nordicsemi.no>
2023-06-27 10:52:12 +0300

    net: lib: lwm2m: Use int16_t for signal strength
    The signal strength for the connectivity monitor was
    defined as int8_t, however this was too small for
    LTE RSRP values, which has range [-140,-44].
    
    Signed-off-by: Jarno Lämsä <jarno.lamsa@nordicsemi.no>

--- subsys/net/lib/lwm2m/lwm2m_obj_connmon.c
+++ subsys/net/lib/lwm2m/lwm2m_obj_connmon.c
@@ -84,7 +84,7 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
 /* resource state variables */
 static int8_t net_bearer;
-static int8_t rss;
+static int16_t rss;
 static uint8_t link_quality;
 static uint32_t cellid;
 static uint16_t mnc;
@@ -99,7 +99,7 @@ static struct lwm2m_engine_obj connmon;
 static struct lwm2m_engine_obj_field fields[] = {
 	OBJ_FIELD_DATA(CONNMON_NETWORK_BEARER_ID, R, U8),
 	OBJ_FIELD_DATA(CONNMON_AVAIL_NETWORK_BEARER_ID, R, U8),
-	OBJ_FIELD_DATA(CONNMON_RADIO_SIGNAL_STRENGTH, R, S8),
+	OBJ_FIELD_DATA(CONNMON_RADIO_SIGNAL_STRENGTH, R, S16),
 	OBJ_FIELD_DATA(CONNMON_LINK_QUALITY, R, U8),
 	OBJ_FIELD_DATA(CONNMON_IP_ADDRESSES, R, STRING),
 	OBJ_FIELD_DATA(CONNMON_ROUTER_IP_ADDRESSES, R_OPT, STRING),
