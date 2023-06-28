a456f2c9db5dda303a4edddbe48955989380b0fd
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2023-06-28 11:40:51 +0200

    cmake: python module cleanup.
    CMake 3.16 find_python3 module introduced `Python3_EXECUTABLE` as
    artifact specifier.
    This allows Zephyr to cleanup its Python detection mechanism and thus
    remove the Zephyr specific `PYTHON_PREFER` variable, which is now
    deprecated.
    
    This further improves the Python detection mechanism in Zephyr as it
    allows users to specify Python3_EXECUTABLE and thereby follows CMake
    documentation.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/modules/FindDeprecated.cmake
+++ cmake/modules/FindDeprecated.cmake
@@ -114,6 +114,18 @@ if("PRJ_BOARD" IN_LIST Deprecated_FIND_COMPONENTS)
                       "replaced with board Kconfig fragments instead.")
 endif()
 
+if("PYTHON_PREFER" IN_LIST Deprecated_FIND_COMPONENTS)
+  # This code was deprecated after Zephyr v3.4.0
+  list(REMOVE_ITEM Deprecated_FIND_COMPONENTS PYTHON_PREFER)
+  if(DEFINED PYTHON_PREFER)
+    message(DEPRECATION "'PYTHON_PREFER' variable is deprecated. Please use "
+                        "Python3_EXECUTABLE instead.")
+    if(NOT DEFINED Python3_EXECUTABLE)
+      set(Python3_EXECUTABLE ${PYTHON_PREFER})
+    endif()
+  endif()
+endif()
+
 if(NOT "${Deprecated_FIND_COMPONENTS}" STREQUAL "")
   message(STATUS "The following deprecated component(s) could not be found: "
                  "${Deprecated_FIND_COMPONENTS}")
