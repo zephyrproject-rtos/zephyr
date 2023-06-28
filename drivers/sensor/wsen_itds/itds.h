80f68c5cef0c89f0cdb7f96e8d77eb1c207376a9
Matthias Hauser <Matthias.Hauser@we-online.de>
2023-06-28 14:13:30 +0200

    drivers: sensor: fetch all channels on WSEN_ITDS sensor
    fetch all channels on WSEN_ITDS sensor
    
    Signed-off-by: Matthias Hauser <Matthias.Hauser@we-online.de>

--- drivers/sensor/wsen_itds/itds.h
+++ drivers/sensor/wsen_itds/itds.h
@@ -106,7 +106,7 @@ struct itds_device_data {
 	struct k_work work;
 #endif
 	int16_t samples[ITDS_SAMPLE_SIZE];
-	int16_t temprature;
+	int16_t temperature;
 	uint16_t scale;
 	enum operation_mode op_mode;
 	const struct device *dev;
