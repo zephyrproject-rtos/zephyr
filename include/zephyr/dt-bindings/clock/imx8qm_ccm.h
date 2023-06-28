7bdacfda9e48a0e89ff44e1583a9a2a6cbef830f
Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>
2023-06-28 14:53:58 +0300

    nxp: mimx8qm: Enable clock control support
    This commit introduces all necessary changes to enable clock
    control on i.MX8QM.
    
    Signed-off-by: Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>

---
+++ include/zephyr/dt-bindings/clock/imx8qm_ccm.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2023 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_IMX8QM_CCM_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_IMX8QM_CCM_H_
+
+/* note: the values of these defines need to match the indexes
+ * in the array of clocks for the clocks they represent.
+ */
+#define IMX8QM_CCM_ADMA_LPUART0_CLOCK 0
+#define IMX8QM_CCM_ADMA_LPUART1_CLOCK 1
+#define IMX8QM_CCM_ADMA_LPUART2_CLOCK 2
+
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_IMX8QM_CCM_H_ */
