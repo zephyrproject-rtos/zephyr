2c317283a32552af3f5efc577cd9fceccc1db413
Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>
2023-06-28 11:28:16 +0300

    nxp: mimx9: Switch to using CCM Rev3 clock controller
    This commit introduces the necessary changes to the i.MX93
    files required to make use of the CCM Rev3 driver.
    
    Signed-off-by: Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>

---
+++ include/zephyr/dt-bindings/clock/imx93_ccm.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2023 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_IMX93_CCM_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_IMX93_CCM_H_
+
+/* note: the values of these defines need to match the indexes
+ * in the array of clocks for the clocks they represent.
+ */
+#define IMX93_CCM_LPUART1_CLOCK 0
+#define IMX93_CCM_LPUART2_CLOCK 1
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_IMX93_CCM_H_ */
