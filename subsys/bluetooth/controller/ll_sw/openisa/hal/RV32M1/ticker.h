01314b2e434e5759327bcf78c38089e6750c54c4
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2023-06-30 08:18:53 +0530

    Bluetooth: Controller: Make openisa and test hal ticker.h consistent
    Make openisa and unit test hal ticker.h consistent with
    nrf hal ticker.h.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/openisa/hal/RV32M1/ticker.h
+++ subsys/bluetooth/controller/ll_sw/openisa/hal/RV32M1/ticker.h
@@ -6,36 +6,52 @@
  */
 
 #define HAL_TICKER_CNTR_CLK_FREQ_HZ 32768U
+#define HAL_TICKER_CNTR_CLK_UNIT_FS 30517578125UL
 
+/* Macro defining the minimum counter compare offset */
 #define HAL_TICKER_CNTR_CMP_OFFSET_MIN 2
 
+/* Macro defining the max. counter update latency in ticks */
 #define HAL_TICKER_CNTR_SET_LATENCY 1
+
+/* Macro defines the h/w supported most significant bit */
+#define HAL_TICKER_CNTR_MSBIT 31
+
+/* Macro defining the HW supported counter bits */
+#define HAL_TICKER_CNTR_MASK 0xFFFFFFFF
+
 /*
  * When the LPTMR is enabled, the first increment will take an additional
  * one or two prescaler clock cycles due to synchronization logic.
  */
 
+/* Macro to translate microseconds to tick units.
+ * NOTE: This returns the floor value.
+ */
 #define HAL_TICKER_US_TO_TICKS(x) \
 	( \
-		((uint32_t)(((uint64_t) (x) * 1000000000UL) / 30517578125UL)) \
-		& HAL_TICKER_CNTR_MASK \
+		((uint32_t)(((uint64_t) (x) * 1000000000UL) / \
+			    HAL_TICKER_CNTR_CLK_UNIT_FS)) & \
+		HAL_TICKER_CNTR_MASK \
 	)
 
+/* Macro to translate tick units to microseconds. */
+#define HAL_TICKER_TICKS_TO_US(x) \
+	( \
+		((uint32_t)(((uint64_t)(x) * HAL_TICKER_CNTR_CLK_UNIT_FS) / \
+		 1000000000UL)) \
+	)
+
+/* Macro returning remainder in picoseconds (to fit in 32-bits) */
 #define HAL_TICKER_REMAINDER(x) \
 	( \
 		( \
 			((uint64_t) (x) * 1000000000UL) \
-			- ((uint64_t)HAL_TICKER_US_TO_TICKS(x) * 30517578125UL) \
+			- ((uint64_t)HAL_TICKER_US_TO_TICKS(x) * \
+			   HAL_TICKER_CNTR_CLK_UNIT_FS) \
 		) \
 		/ 1000UL \
 	)
-
-#define HAL_TICKER_TICKS_TO_US(x) \
-	((uint32_t)(((uint64_t)(x) * 30517578125UL) / 1000000000UL))
-
-#define HAL_TICKER_CNTR_MSBIT 31
-
-#define HAL_TICKER_CNTR_MASK 0xFFFFFFFF
 
 /* Macro defining the remainder resolution/range
  * ~ 1000000 * HAL_TICKER_TICKS_TO_US(1)
