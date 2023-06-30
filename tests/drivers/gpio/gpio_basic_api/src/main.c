081c4647cdc8d20cf9d64ae6114ebeb81ba4dd7f
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-06-30 16:32:00 +0200

    test gpio_basic_api: Enable for nrf52_bsim
    Add DT and kconfig overlays for the nrf52_bsim so this test can be run
    in this board too.
    This simulated board supports short-circuiting pins
    either programmatically or thru configuration files,
    but it does not support a pull in an input being able to drag
    a non driven output, so we need to skip the pull test.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- tests/drivers/gpio/gpio_basic_api/src/main.c
+++ tests/drivers/gpio/gpio_basic_api/src/main.c
@@ -14,6 +14,8 @@
 #include "device_imx.h"
 #elif defined(CONFIG_BOARD_MIMXRT1050_EVK)
 #include <fsl_iomuxc.h>
+#elif defined(CONFIG_BOARD_NRF52_BSIM)
+#include <NRF_GPIO.h>
 #endif
 
 static void board_setup(void)
@@ -87,6 +89,14 @@ static void board_setup(void)
 	zassert_true(device_is_ready(dev), "GPIO dev is not ready");
 	int rc = gpio_add_callback(dev, &gpio_emul_callback);
 	__ASSERT(rc == 0, "gpio_add_callback() failed: %d", rc);
+#elif defined(CONFIG_BOARD_NRF52_BSIM)
+	static bool done;
+
+	if (!done) {
+		done = true;
+		/* This functions allows to programmatically short-circuit SOC GPIO pins */
+		nrf_gpio_backend_register_short(1, PIN_OUT, 1, PIN_IN);
+	}
 #endif
 }
 
