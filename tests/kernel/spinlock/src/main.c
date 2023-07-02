089f49a4997ff9d2183f4a997f116afe9a019ae8
Christopher Friedt <cfriedt@meta.com>
2023-07-02 10:58:38 +0200

    tests: kernel: spinlock: add test for k_spin_trylock()
    Add a testcase to exercise two cases:
    * when `k_spin_trylock()` fails (lock is busy)
    * when `k_spin_trylock()` succeeds (lock is acquired)
    
    We use the same machinery for checking for a recursive mutex
    as `k_spin_lock()`.
    
    Signed-off-by: Christopher Friedt <cfriedt@meta.com>

--- tests/kernel/spinlock/src/main.c
+++ tests/kernel/spinlock/src/main.c
@@ -18,6 +18,8 @@ struct k_thread cpu1_thread;
 static struct k_spinlock bounce_lock;
 
 volatile int bounce_owner, bounce_done;
+volatile int trylock_failures;
+volatile int trylock_successes;
 
 /**
  * @brief Tests for spinlock
@@ -53,8 +55,9 @@ ZTEST(spinlock, test_spinlock_basic)
 	zassert_true(!l.locked, "Spinlock failed to unlock");
 }
 
-void bounce_once(int id)
+void bounce_once(int id, bool trylock)
 {
+	int ret;
 	int i, locked;
 	k_spinlock_key_t key;
 
@@ -63,7 +66,16 @@ void bounce_once(int id)
 	 */
 	locked = 0;
 	for (i = 0; i < 10000; i++) {
-		key = k_spin_lock(&bounce_lock);
+		if (trylock) {
+			ret = k_spin_trylock(&bounce_lock, &key);
+			if (ret == -EBUSY) {
+				trylock_failures++;
+				continue;
+			}
+			trylock_successes++;
+		} else {
+			key = k_spin_lock(&bounce_lock);
+		}
 
 		if (bounce_owner != id) {
 			locked = 1;
@@ -100,7 +112,7 @@ void cpu1_fn(void *p1, void *p2, void *p3)
 	ARG_UNUSED(p3);
 
 	while (1) {
-		bounce_once(4321);
+		bounce_once(4321, false);
 	}
 }
 
@@ -122,7 +134,7 @@ ZTEST(spinlock, test_spinlock_bounce)
 	k_busy_wait(10);
 
 	for (i = 0; i < 10000; i++) {
-		bounce_once(1234);
+		bounce_once(1234, false);
 	}
 
 	bounce_done = 1;
@@ -171,6 +183,44 @@ ZTEST(spinlock, test_spinlock_mutual_exclusion)
 	k_spin_unlock(&lock_runtime, key);
 
 	zassert_true(!lock_runtime.locked, "Spinlock failed to unlock");
+}
+
+void trylock_fn(void *p1, void *p2, void *p3)
+{
+	ARG_UNUSED(p1);
+	ARG_UNUSED(p2);
+	ARG_UNUSED(p3);
+
+	while (1) {
+		bounce_once(4321, true);
+	}
+}
+
+/**
+ * @brief Test k_spin_trylock()
+ *
+ * @ingroup kernel_spinlock_tests
+ *
+ * @see k_spin_trylock()
+ */
+ZTEST(spinlock, test_trylock)
+{
+	int i;
+
+	k_thread_create(&cpu1_thread, cpu1_stack, CPU1_STACK_SIZE,
+			trylock_fn, NULL, NULL, NULL,
+			0, 0, K_NO_WAIT);
+
+	k_busy_wait(10);
+
+	for (i = 0; i < 10000; i++) {
+		bounce_once(1234, true);
+	}
+
+	bounce_done = 1;
+
+	zassert_true(trylock_failures > 0);
+	zassert_true(trylock_successes > 0);
 }
 
 ZTEST_SUITE(spinlock, NULL, NULL, NULL, NULL, NULL);
