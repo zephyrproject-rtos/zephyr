zephyrproject-rtos/zephyr

commit 5a4c9a2112a06fab89c6fb99a7621bdb1ebe2bdd

Bluetooth: BAP: Unicast client shall support 0 or > 1 endpoints
As per table 4.2 in BAP v1.0.1, the BAP unicast client shall
support at least 2 (> 1) endpoints in either direction if it
supports that direction.

Since Kconfig does not support such requirements (0 || > 1),
this is done using a BUILD_ASSERT.

Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

subsys/bluetooth/audio/Kconfig.bap
@@ -93,7 +93,7 @@ config BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT
 
 config BT_BAP_UNICAST_CLIENT_ASE_SRC_COUNT
 	int "Basic Audio Profile ASE Source count"
-	default 1
+	default 2
 	range 0 255
 	help
 	  This option enables caching a number of Audio Stream Endpoint Source

subsys/bluetooth/audio/bap_unicast_client.c
@@ -37,6 +37,14 @@ BUILD_ASSERT(CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT > 0 ||
 	     "CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT or "
 	     "CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SRC_COUNT shall be non-zero");
 
+BUILD_ASSERT(CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT == 0 ||
+		     CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT > 1,
+	     "CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT shall be either 0 or > 1");
+
+BUILD_ASSERT(CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SRC_COUNT == 0 ||
+		     CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SRC_COUNT > 1,
+	     "CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SRC_COUNT shall be either 0 or > 1");
+
 LOG_MODULE_REGISTER(bt_bap_unicast_client, CONFIG_BT_BAP_UNICAST_CLIENT_LOG_LEVEL);
 
 #define PAC_DIR_UNUSED(dir) ((dir) != BT_AUDIO_DIR_SINK && (dir) != BT_AUDIO_DIR_SOURCE)

samples/bluetooth/tmap_central/prj.conf
@@ -39,8 +39,8 @@ CONFIG_BT_TBS_SUPPORTED_FEATURES=3
 CONFIG_BT_ISO_TX_BUF_COUNT=2
 CONFIG_BT_ISO_MAX_CHAN=2
 CONFIG_BT_BAP_UNICAST_CLIENT_GROUP_STREAM_COUNT=1
-CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT=1
-CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SRC_COUNT=1
+CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT=2
+CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SRC_COUNT=2
 CONFIG_BT_GATT_DYNAMIC_DB=y
 
 CONFIG_BT_EXT_ADV=y

commit e6653e4c17a913379cd1e1e733859b8a74f19b24

Bluetooth: BAP: Remove SNK and SRC count asserts
This removes asserts introduced in
5a4c9a2112a06fab89c6fb99a7621bdb1ebe2bdd.

An application does not need to support all configurations, hence
CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT and
CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SRC_COUNT should be configurable
to value 1.

Signed-off-by: Magdalena Kasenberg <magdalena.kasenberg@codecoup.pl>

subsys/bluetooth/audio/Kconfig.bap
@@ -109,7 +109,7 @@ config BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT
 
 config BT_BAP_UNICAST_CLIENT_ASE_SRC_COUNT
 	int "Basic Audio Profile ASE Source count"
-	default 2
+	default 1
 	range 0 255
 	help
 	  This option enables caching a number of Audio Stream Endpoint Source

subsys/bluetooth/audio/bap_unicast_client.c
@@ -37,14 +37,6 @@ BUILD_ASSERT(CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT > 0 ||
 	     "CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT or "
 	     "CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SRC_COUNT shall be non-zero");
 
-BUILD_ASSERT(CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT == 0 ||
-		     CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT > 1,
-	     "CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SNK_COUNT shall be either 0 or > 1");
-
-BUILD_ASSERT(CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SRC_COUNT == 0 ||
-		     CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SRC_COUNT > 1,
-	     "CONFIG_BT_BAP_UNICAST_CLIENT_ASE_SRC_COUNT shall be either 0 or > 1");
-
 LOG_MODULE_REGISTER(bt_bap_unicast_client, CONFIG_BT_BAP_UNICAST_CLIENT_LOG_LEVEL);
 
 #define PAC_DIR_UNUSED(dir) ((dir) != BT_AUDIO_DIR_SINK && (dir) != BT_AUDIO_DIR_SOURCE)