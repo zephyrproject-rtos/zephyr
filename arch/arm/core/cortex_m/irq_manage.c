9b739bc0f2f98a6bd6a6578b2c1937b67ce1aea7
Huifeng Zhang <Huifeng.Zhang@arm.com>
2023-07-04 19:46:20 +0800

    arch: arm: Separate common irq_manage and isr_wrapper code
    There are too many differences between Cortex-A/R and Cortex-M on irq
    code, e.g. Cortex-A/R use GIC and Cortex-M uses NVIC. For reducing
    the complexity and easier to maintain, this commit separates irq_manage.c
    and isr_wrapper.S into two different parts based on the architecture.
    
    This commit also Removes the part related to the option
    'CONFIG_ARM_SECURE_FIRMWARE' in 'cortex_a_r/irq_manage.c' because
    this code is written for the Cortex-M architecture.
    
    Signed-off-by: Huifeng Zhang <Huifeng.Zhang@arm.com>

--- arch/arm/core/cortex_m/irq_manage.c
+++ arch/arm/core/cortex_m/irq_manage.c
@@ -6,7 +6,7 @@
 
 /**
  * @file
- * @brief ARM Cortex-A, Cortex-M and Cortex-R interrupt management
+ * @brief ARM Cortex-M interrupt management
  *
  *
  * Interrupt management: enabling/disabling and dynamic ISR
@@ -16,12 +16,7 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/arch/cpu.h>
-#if defined(CONFIG_CPU_CORTEX_M)
 #include <cmsis_core.h>
-#elif defined(CONFIG_CPU_AARCH32_CORTEX_A) \
-	|| defined(CONFIG_CPU_AARCH32_CORTEX_R)
-#include <zephyr/drivers/interrupt_controller/gic.h>
-#endif
 #include <zephyr/sys/__assert.h>
 #include <zephyr/sys/barrier.h>
 #include <zephyr/toolchain.h>
@@ -33,7 +28,6 @@
 
 extern void z_arm_reserved(void);
 
-#if defined(CONFIG_CPU_CORTEX_M)
 #define NUM_IRQS_PER_REG 32
 #define REG_FROM_IRQ(irq) (irq / NUM_IRQS_PER_REG)
 #define BIT_FROM_IRQ(irq) (irq % NUM_IRQS_PER_REG)
@@ -96,53 +90,6 @@ void z_arm_irq_priority_set(unsigned int irq, unsigned int prio, uint32_t flags)
 	NVIC_SetPriority((IRQn_Type)irq, prio);
 }
 
-#elif defined(CONFIG_CPU_AARCH32_CORTEX_A) \
-	|| defined(CONFIG_CPU_AARCH32_CORTEX_R)
-/*
- * For Cortex-A and Cortex-R cores, the default interrupt controller is the ARM
- * Generic Interrupt Controller (GIC) and therefore the architecture interrupt
- * control functions are mapped to the GIC driver interface.
- *
- * When a custom interrupt controller is used (i.e.
- * CONFIG_ARM_CUSTOM_INTERRUPT_CONTROLLER is enabled), the architecture
- * interrupt control functions are mapped to the SoC layer in
- * `include/arch/arm/aarch32/irq.h`.
- */
-
-#if !defined(CONFIG_ARM_CUSTOM_INTERRUPT_CONTROLLER)
-void arch_irq_enable(unsigned int irq)
-{
-	arm_gic_irq_enable(irq);
-}
-
-void arch_irq_disable(unsigned int irq)
-{
-	arm_gic_irq_disable(irq);
-}
-
-int arch_irq_is_enabled(unsigned int irq)
-{
-	return arm_gic_irq_is_enabled(irq);
-}
-
-/**
- * @internal
- *
- * @brief Set an interrupt's priority
- *
- * The priority is verified if ASSERT_ON is enabled. The maximum number
- * of priority levels is a little complex, as there are some hardware
- * priority levels which are reserved: three for various types of exceptions,
- * and possibly one additional to support zero latency interrupts.
- */
-void z_arm_irq_priority_set(unsigned int irq, unsigned int prio, uint32_t flags)
-{
-	arm_gic_irq_set_priority(irq, prio, flags);
-}
-#endif /* !CONFIG_ARM_CUSTOM_INTERRUPT_CONTROLLER */
-
-#endif /* CONFIG_CPU_CORTEX_M */
-
 void z_arm_fatal_error(unsigned int reason, const z_arch_esf_t *esf);
 
 /**
@@ -163,10 +110,7 @@ void z_irq_spurious(const void *unused)
 #ifdef CONFIG_PM
 void _arch_isr_direct_pm(void)
 {
-#if defined(CONFIG_ARMV6_M_ARMV8_M_BASELINE) \
-	|| defined(CONFIG_ARMV7_R) \
-	|| defined(CONFIG_AARCH32_ARMV8_R) \
-	|| defined(CONFIG_ARMV7_A)
+#if defined(CONFIG_ARMV6_M_ARMV8_M_BASELINE)
 	unsigned int key;
 
 	/* irq_lock() does what we want for this CPU */
@@ -186,10 +130,7 @@ void _arch_isr_direct_pm(void)
 		z_pm_save_idle_exit();
 	}
 
-#if defined(CONFIG_ARMV6_M_ARMV8_M_BASELINE) \
-	|| defined(CONFIG_ARMV7_R) \
-	|| defined(CONFIG_AARCH32_ARMV8_R) \
-	|| defined(CONFIG_ARMV7_A)
+#if defined(CONFIG_ARMV6_M_ARMV8_M_BASELINE)
 	irq_unlock(key);
 #elif defined(CONFIG_ARMV7_M_ARMV8_M_MAINLINE)
 	__asm__ volatile("cpsie i" : : : "memory");
