886325cc1d2dc3d1f03dea661fb3451d6d7a7af5
Harshil Bhatt <harshilbhatt2001@gmail.com>
2023-07-05 01:36:49 +0530

    posix: add tests for pthread_barrierattr
    add test coverage for init, destroy, setpshared and getpshared
    
    Signed-off-by: Harshil Bhatt <harshilbhatt2001@gmail.com>

--- tests/posix/common/src/pthread.c
+++ tests/posix/common/src/pthread.c
@@ -743,3 +743,31 @@ ZTEST(posix_apis, test_sched_policy)
 		}
 	}
 }
+
+ZTEST(posix_apis, test_posix_pthread_barrier)
+{
+	int ret, pshared;
+	pthread_barrierattr_t attr;
+
+	ret = pthread_barrierattr_init(&attr);
+	zassert_equal(ret, 0, "pthread_barrierattr_init failed");
+
+	ret = pthread_barrierattr_getpshared(&attr, &pshared);
+	zassert_equal(ret, 0, "pthread_barrierattr_getpshared failed");
+	zassert_equal(pshared, PTHREAD_PROCESS_PRIVATE, "pshared attribute not set correctly");
+
+	ret = pthread_barrierattr_setpshared(&attr, PTHREAD_PROCESS_PRIVATE);
+	zassert_equal(ret, 0, "pthread_barrierattr_setpshared failed");
+
+	ret = pthread_barrierattr_setpshared(&attr, PTHREAD_PROCESS_PUBLIC);
+	zassert_equal(ret, 0, "pthread_barrierattr_setpshared failed");
+
+	ret = pthread_barrierattr_getpshared(&attr, &pshared);
+	zassert_equal(pshared, PTHREAD_PROCESS_PUBLIC, "pshared attribute not retrieved correctly");
+
+	ret = pthread_barrierattr_setpshared(&attr, 42);
+	zassert_equal(ret, -EINVAL, "pthread_barrierattr_setpshared did not return EINVAL");
+
+	ret = pthread_barrierattr_destroy(&attr);
+	zassert_equal(ret, 0, "pthread_barrierattr_destroy failed");
+}
