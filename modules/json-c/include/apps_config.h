d395173ae90df80021d6b55dc8024c624d0533bf
Parthiban Nallathambi <parthiban@linumiz.com>
2023-07-05 00:21:01 +0530

    modules: add json-c external module
    add json-c as external module and define CMake ane Kconfig
    compialtion control from zephyr tree.
    
    Commit also includes generated header files which are essential
    for compilation. config.h typically controls the available
    support in the toolchain and the current choice with zephyr
    is done using picolibc.
    
    Signed-off-by: Parthiban Nallathambi <parthiban@linumiz.com>

---
+++ modules/json-c/include/apps_config.h
@@ -0,0 +1,8 @@
+
+/* Define to 1 if you have the <sys/resource.h> header file. */
+#define HAVE_SYS_RESOURCE_H
+
+/* Define if you have the `getrusage' function. */
+#define HAVE_GETRUSAGE
+
+/* #undef HAVE_JSON_TOKENER_GET_PARSE_END */
