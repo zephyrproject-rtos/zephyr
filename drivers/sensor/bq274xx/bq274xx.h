b64062b3e1564f25b66813a091eca1ef576c8aff
Fabio Baltieri <fabiobaltieri@google.com>
2023-07-06 22:29:31 +0100

    sensor: bq274xx: add support for bq27427
    The current ID for BQ274XX_DEVICE_ID is actually the one for the
    BQ27421. The driver seems to work with the BQ27427 as well, at least the
    common and extended commands are the same, so add that variant as well,
    rename the existing one and print the currently read ID when the ID
    check fails.
    
    The configuration registers have a different offset though, so add a
    register offset table and make the device rcognize the right one un
    runtime based on the device ID.
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

--- drivers/sensor/bq274xx/bq274xx.h
+++ drivers/sensor/bq274xx/bq274xx.h
@@ -13,7 +13,8 @@
 /*** General Constant ***/
 #define BQ274XX_UNSEAL_KEY_A 0x8000 /* Unseal code one on BQ27441-G1A and similar */
 #define BQ274XX_UNSEAL_KEY_B 0x8000 /* Unseal code two on BQ27441-G1A and similar */
-#define BQ274XX_DEVICE_ID  0x0421 /* Default device ID */
+#define BQ27421_DEVICE_ID  0x0421
+#define BQ27427_DEVICE_ID  0x0427
 
 /*** Standard Commands ***/
 #define BQ274XX_CMD_CONTROL_LOW    0x00 /* Control() low register */
@@ -68,18 +69,19 @@
 #define BQ274XX_EXT_BLKDAT_END                 0x5F /* BlockData_end() */
 #define BQ274XX_EXT_CHECKSUM                   0x60 /* BlockDataCheckSum() */
 #define BQ274XX_EXT_DATA_CONTROL               0x61 /* BlockDataControl() */
-#define BQ274XX_EXT_BLKDAT_DESIGN_CAP_HIGH     0x4A /* BlockData */
-#define BQ274XX_EXT_BLKDAT_DESIGN_CAP_LOW      0x4B
-#define BQ274XX_EXT_BLKDAT_DESIGN_ENR_HIGH     0x4C
-#define BQ274XX_EXT_BLKDAT_DESIGN_ENR_LOW      0x4D
-#define BQ274XX_EXT_BLKDAT_TERMINATE_VOLT_HIGH 0x50
-#define BQ274XX_EXT_BLKDAT_TERMINATE_VOLT_LOW  0x51
-#define BQ274XX_EXT_BLKDAT_TAPERRATE_HIGH      0x5B
-#define BQ274XX_EXT_BLKDAT_TAPERRATE_LOW       0x5C
+#define BQ274XX_EXT_BLKDAT_HIGH(off)           (BQ274XX_EXT_BLKDAT_START + off)
+#define BQ274XX_EXT_BLKDAT_LOW(off)            (BQ274XX_EXT_BLKDAT_START + off + 1)
 
-#define BQ274XX_DELAY 1000
+/* Hold the register offset for a device variant. */
+struct bq274xx_regs {
+	uint8_t dm_design_capacity;
+	uint8_t dm_design_energy;
+	uint8_t dm_terminate_voltage;
+	uint8_t dm_taper_rate;
+};
 
 struct bq274xx_data {
+	const struct bq274xx_regs *regs;
 	bool configured;
 	uint16_t voltage;
 	int16_t avg_current;
