cf76b821c89317553f6f7d46f627af7246d65c60
Girisha Dengi <girisha.dengi@intel.com>
2023-07-06 13:59:13 +0000

    soc: arm64: Add agilex5 soc folder and its configurations
    Add Agilex5 soc folder, MMU table and its configurations for
    Intel SoC FPGA Agilex5 platform for initial bring up.
    Add ARM Cortex-a76 and Cortex-a55 HMP cluster type.
    
    Signed-off-by: Teik Heng Chong <teik.heng.chong@intel.com>
    Signed-off-by: Girisha Dengi <girisha.dengi@intel.com>

---
+++ soc/arm64/intel_socfpga/agilex5/mmu_regions.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022, Intel Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/arch/cpu.h>
+#include <zephyr/arch/arm64/arm_mmu.h>
+
+static const struct arm_mmu_region mmu_regions[] = {
+
+	/* System manager register that required by clock driver */
+	MMU_REGION_FLAT_ENTRY("SYSTEM_MANAGER",
+			      DT_REG_ADDR(DT_NODELABEL(sysmgr)),
+			      DT_REG_SIZE(DT_NODELABEL(sysmgr)),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_DEFAULT_SECURE_STATE),
+
+	MMU_REGION_FLAT_ENTRY("PINMUX",
+			      DT_REG_ADDR_BY_IDX(DT_NODELABEL(pinmux), 0),
+			      DT_REG_SIZE_BY_IDX(DT_NODELABEL(pinmux), 0),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
+
+	MMU_REGION_FLAT_ENTRY("GIC_0",
+			      DT_REG_ADDR_BY_IDX(DT_NODELABEL(gic), 0),
+			      DT_REG_SIZE_BY_IDX(DT_NODELABEL(gic), 0),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_DEFAULT_SECURE_STATE),
+
+	MMU_REGION_FLAT_ENTRY("GIC_1",
+			      DT_REG_ADDR_BY_IDX(DT_NODELABEL(gic), 1),
+			      DT_REG_SIZE_BY_IDX(DT_NODELABEL(gic), 1),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_DEFAULT_SECURE_STATE),
+
+	MMU_REGION_FLAT_ENTRY("GIC_ITS",
+			      DT_REG_ADDR(DT_NODELABEL(its)),
+			      DT_REG_SIZE(DT_NODELABEL(its)),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_DEFAULT_SECURE_STATE),
+};
+
+const struct arm_mmu_config mmu_config = {
+	.num_regions = ARRAY_SIZE(mmu_regions),
+	.mmu_regions = mmu_regions,
+};
