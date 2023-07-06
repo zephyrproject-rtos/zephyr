eb709905ae6e7a6c57dfaa825a13ec8af02dcffb
Marcin Gasiorek <marcin.gasiorek@nordicsemi.no>
2023-07-06 14:17:51 +0200

    net: pkt_filter: Introduce additional hooks for pkt_filter
    The additional hooks provide infrastructure to construct
    rules on another network stack levels. Main benefit of this
    approach is packets are pre-parsed and e.g. IP filter is
    easier to implement. These hooks are equivalent of prerouting
    and local_in in linux's netfilter.
    
    Signed-off-by: Marcin Gasiorek <marcin.gasiorek@nordicsemi.no>

--- subsys/net/ip/ipv4.c
+++ subsys/net/ip/ipv4.c
@@ -302,6 +302,15 @@ enum net_verdict net_ipv4_input(struct net_pkt *pkt)
 		goto drop;
 	}
 
+	net_pkt_set_ipv4_ttl(pkt, hdr->ttl);
+
+	net_pkt_set_family(pkt, PF_INET);
+
+	if (!net_pkt_filter_ip_recv_ok(pkt)) {
+		/* drop the packet */
+		return NET_DROP;
+	}
+
 	if ((!net_ipv4_is_my_addr((struct in_addr *)hdr->dst) &&
 	     !net_ipv4_is_addr_mcast((struct in_addr *)hdr->dst) &&
 	     !(hdr->proto == IPPROTO_UDP &&
@@ -325,10 +334,6 @@ enum net_verdict net_ipv4_input(struct net_pkt *pkt)
 			goto drop;
 		}
 	}
-
-	net_pkt_set_ipv4_ttl(pkt, hdr->ttl);
-
-	net_pkt_set_family(pkt, PF_INET);
 
 	if (IS_ENABLED(CONFIG_NET_IPV4_FRAGMENT)) {
 		/* Check if this is a fragmented packet, and if so, handle reassembly */
