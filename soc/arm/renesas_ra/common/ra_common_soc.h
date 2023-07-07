4fb718f6e1f731e561bb420b9ae8f6af2f55906b
TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>
2023-07-07 15:27:40 +0900

    soc: arm: add support for Renesas RA4M1 series SoC
    Add essential support for RA4M1 Series.
    It only defines `r7fa4m1ab3cfm` currently.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>

---
+++ soc/arm/renesas_ra/common/ra_common_soc.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2023 TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_ARM_RENESAS_RA_COMMON_RA_COMMON_SOC_H_
+#define ZEPHYR_SOC_ARM_RENESAS_RA_COMMON_RA_COMMON_SOC_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_SOC_ARM_RENESAS_RA_COMMON_RA_COMMON_SOC_H_ */
