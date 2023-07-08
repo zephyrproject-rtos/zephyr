9e8ad216e9e5ed5084d3c2f5d43e319934c60544
Yong Cong Sin <ycsin@meta.com>
2023-07-08 22:33:31 +0800

    posix: signal: implement sigfillset
    Implementation and ztest for sigfillset.
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>

--- tests/posix/common/src/signal.c
+++ tests/posix/common/src/signal.c
@@ -23,3 +23,14 @@ ZTEST(posix_apis, test_signal_emptyset)
 		zassert_equal(set.sig[i], 0u, "set.sig[%d] is not empty: 0x%lx", i, set.sig[i]);
 	}
 }
+
+ZTEST(posix_apis, test_signal_fillset)
+{
+	sigset_t set = (sigset_t){0};
+
+	zassert_ok(sigfillset(&set));
+
+	for (int i = 0; i < ARRAY_SIZE(set.sig); i++) {
+		zassert_equal(set.sig[i], -1, "set.sig[%d] is not filled: 0x%lx", i, set.sig[i]);
+	}
+}
