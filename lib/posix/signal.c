8011449542674d7fb10a5d7c09df390bbcb581c4
Yong Cong Sin <ycsin@meta.com>
2023-07-08 22:45:27 +0800

    posix: signal: implement sigaddset
    Implementation and ztest for sigaddset.
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>

--- lib/posix/signal.c
+++ lib/posix/signal.c
@@ -3,7 +3,22 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+#include <errno.h>
+
 #include <zephyr/posix/signal.h>
+
+#define SIGNO_WORD_IDX(_signo) (signo / BITS_PER_LONG)
+#define SIGNO_WORD_BIT(_signo) (signo & BIT_MASK(LOG2(BITS_PER_LONG)))
+
+static inline bool signo_valid(int signo)
+{
+	return ((signo > 0) && (signo < _NSIG));
+}
+
+static inline bool signo_is_rt(int signo)
+{
+	return ((signo >= SIGRTMIN) && (signo <= SIGRTMAX));
+}
 
 int sigemptyset(sigset_t *set)
 {
@@ -16,6 +31,18 @@ int sigfillset(sigset_t *set)
 	for (int i = 0; i < ARRAY_SIZE(set->sig); i++) {
 		set->sig[i] = -1;
 	}
+
+	return 0;
+}
+
+int sigaddset(sigset_t *set, int signo)
+{
+	if (!signo_valid(signo)) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	WRITE_BIT(set->sig[SIGNO_WORD_IDX(signo)], SIGNO_WORD_BIT(signo), 1);
 
 	return 0;
 }
