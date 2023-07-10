5ea3f9a24e32826d3f40cd9fd55e0cba044b2c70
Fabio Baltieri <fabiobaltieri@google.com>
2023-07-10 15:29:35 +0000

    drivers: pcie: initialize after interrupt controllers
    PCIe devices refer to interrupt nodes, but are initialized with the same
    priority, making the sequence depending on the linking order. Add a new
    symbol and set it to one unit after intc to ensure that the
    initialization sequence is stable.
    
    Found with:
    
    west build -p -b qemu_cortex_a53 \
    	samples/drivers/virtualization/ivshmem/doorbell \
    	-DCONFIG_CHECK_INIT_PRIORITIES=y
    ...
    ERROR: /soc/pcie@4010000000 PRE_KERNEL_1 40 <
    	/soc/interrupt-controller@8000000/its@8080000 POST_KERNEL 40
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

--- drivers/pcie/host/pcie_ecam.c
+++ drivers/pcie/host/pcie_ecam.c
@@ -403,7 +403,7 @@ static const struct pcie_ctrl_driver_api pcie_ecam_api = {
 			      &pcie_ecam_data##n,				\
 			      &pcie_ecam_config##n,				\
 			      PRE_KERNEL_1,					\
-			      CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,		\
+			      CONFIG_PCIE_INIT_PRIORITY,			\
 			      &pcie_ecam_api);
 
 DT_INST_FOREACH_STATUS_OKAY(PCIE_ECAM_INIT)
