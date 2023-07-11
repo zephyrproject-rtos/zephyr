2f3a8171ba76dbfa59d8ed5478c99459f32e541e
Michael Zimmermann <sigmaepsilon92@gmail.com>
2023-07-11 14:12:18 +0200

    arch/posix: make posix_fuzz_buf const
    The original buffer is const as well and we're not allowed to modify it.
    
    Signed-off-by: Michael Zimmermann <sigmaepsilon92@gmail.com>

--- boards/posix/native_posix/main.c
+++ boards/posix/native_posix/main.c
@@ -127,7 +127,7 @@ int main(int argc, char *argv[])
  * "long enough" to handle the event and reach a quiescent state
  * again)
  */
-uint8_t *posix_fuzz_buf;
+const uint8_t *posix_fuzz_buf;
 size_t posix_fuzz_sz;
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t sz)
@@ -142,7 +142,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t sz)
 	/* Provide the fuzz data to Zephyr as an interrupt, with
 	 * "DMA-like" data placed into posix_fuzz_buf/sz
 	 */
-	posix_fuzz_buf = (void *)data;
+	posix_fuzz_buf = data;
 	posix_fuzz_sz = sz;
 	hw_irq_ctrl_set_irq(CONFIG_ARCH_POSIX_FUZZ_IRQ);
 
