71244acd227d6fe40f55d3dd23a1f5490122bb57
L Lakshmanan <l-lakshmanan@ti.com>
2023-07-11 12:08:13 +0530

    board: ti_am62x_sk_m4: Added board files for TI AM62X SK
    Added configuration and documentation files for the AM62x board
    M4 core.
    
    Signed-off-by: L Lakshmanan <l-lakshmanan@ti.com>

--- soc/arm/ti_k3/am62x_m4/soc.c
+++ soc/arm/ti_k3/am62x_m4/soc.c
@@ -11,6 +11,11 @@
 #include <soc.h>
 
 #define ADDR_TRANSLATE_RAT_BASE_ADDR		(0x044200000u)
+#define PINCTRL_BASE_ADDR			(0x4080000u)
+#define KICK0_UNLOCK_VAL			(0x68EF3490U)
+#define KICK1_UNLOCK_VAL			(0xD172BC5AU)
+#define CSL_MCU_PADCONFIG_LOCK0_KICK0_OFFSET	(0x1008)
+#define CSL_MCU_PADCONFIG_LOCK1_KICK0_OFFSET	(0x5008)
 
 static struct address_trans_region_config region_config[] = {
 	{
@@ -39,10 +44,29 @@ static struct address_trans_region_config region_config[] = {
  */
 };
 
+static void mmr_unlock(void)
+{
+	uint32_t baseAddr = PINCTRL_BASE_ADDR;
+	uintptr_t kickAddr;
+
+	/* Lock 0 */
+	kickAddr = baseAddr + CSL_MCU_PADCONFIG_LOCK0_KICK0_OFFSET;
+	sys_write32(KICK0_UNLOCK_VAL, kickAddr);   /* KICK 0 */
+	kickAddr = kickAddr + sizeof(uint32_t *);
+	sys_write32(KICK1_UNLOCK_VAL, kickAddr);   /* KICK 1 */
+
+	/* Lock 1 */
+	kickAddr = baseAddr + CSL_MCU_PADCONFIG_LOCK1_KICK0_OFFSET;
+	sys_write32(KICK0_UNLOCK_VAL, kickAddr);   /* KICK 0 */
+	kickAddr = kickAddr + sizeof(uint32_t *);
+	sys_write32(KICK1_UNLOCK_VAL, kickAddr);   /* KICK 1 */
+}
+
 static int am62x_m4_init(void)
 {
 	sys_mm_drv_ti_rat_init(
 		region_config, ADDR_TRANSLATE_RAT_BASE_ADDR, ARRAY_SIZE(region_config));
+	mmr_unlock();
 	return 0;
 }
 
