013d72c47061dcd02d37d9eed252cb781e42a032
Jonathan Rico <jonathan.rico@nordicsemi.no>
2023-07-12 17:26:25 +0200

    arch: posix: break debugger on fatal error
    Add option to raise a SIGTRAP on fatal error, making the debugger break
    instead of exiting.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- arch/posix/core/fatal.c
+++ arch/posix/core/fatal.c
@@ -13,9 +13,15 @@
 #include <zephyr/logging/log_ctrl.h>
 #include <zephyr/arch/posix/posix_soc_if.h>
 
+extern void nsi_raise_sigtrap(void);
+
 FUNC_NORETURN void arch_system_halt(unsigned int reason)
 {
 	ARG_UNUSED(reason);
+
+	if (IS_ENABLED(CONFIG_ARCH_POSIX_TRAP_ON_FATAL)) {
+		nsi_raise_sigtrap();
+	}
 
 	posix_print_error_and_exit("Exiting due to fatal error\n");
 	CODE_UNREACHABLE; /* LCOV_EXCL_LINE */
