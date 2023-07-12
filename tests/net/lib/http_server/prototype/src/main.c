21e945f22639844759b89c7fce048ce56afcdaf5
Emna Rekik <emna.rekik007@gmail.com>
2023-07-12 22:35:14 +0100

    tests: net: lib: http_server: Add test function for http2
    This test ensures HTTP/1.1 backward compatibility
    and verify HTTP 1.1 to 2.0 upgrade
    Fixes #59690
    Fixes #59670
    
    Signed-off-by: Emna Rekik <emna.rekik007@gmail.com>

--- tests/net/lib/http_server/prototype/src/main.c
+++ tests/net/lib/http_server/prototype/src/main.c
@@ -4,11 +4,25 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/ztest.h>
-#include <zephyr/net/socket.h>
 #include <string.h>
 #include <zephyr/net/http/server.h>
+#include <zephyr/net/socket.h>
+#include <zephyr/ztest.h>
 #include "server_functions.h"
+
+#define SUPPORT_BACKWARD_COMPATIBILITY 1
+#define SUPPORT_HTTP2_UPGRADE 2
+#define BUFFER_SIZE 256
+#define STACK_SIZE 8192
+#define MY_IPV4_ADDR "127.0.0.1"
+#define PORT 8000
+#define TIMEOUT 1000
+
+static struct k_sem server_sem;
+
+static K_THREAD_STACK_DEFINE(server_stack, STACK_SIZE);
+
+static struct k_thread server_thread;
 
 static const unsigned char inc_file[] = {
 #include "index.html.gz.inc"
@@ -29,6 +43,147 @@ static const unsigned char compressed_inc_file[] = {
 
 ZTEST_SUITE(server_function_tests, NULL, NULL, NULL, NULL, NULL);
 
+static void server_thread_fn(void *arg0, void *arg1, void *arg2)
+{
+	const int server_fd = POINTER_TO_INT(arg0);
+	const int test_support = POINTER_TO_INT(arg1);
+
+	int r;
+	int client_fd;
+	socklen_t addrlen;
+	char addrstr[BUFFER_SIZE];
+	struct sockaddr_in sa;
+	int support = 0;
+	const char *preface = "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n";
+
+	k_thread_name_set(k_current_get(), "server");
+
+	memset(&sa, 0, sizeof(sa));
+	addrlen = sizeof(sa);
+
+	k_sem_give(&server_sem);
+
+	client_fd = accept_new_client(server_fd);
+
+	if (test_support == SUPPORT_BACKWARD_COMPATIBILITY) {
+
+		memset(addrstr, '\0', sizeof(addrstr));
+		r = recv(client_fd, addrstr, sizeof(addrstr), 0);
+		zassert_not_equal(r, -1, "recv() failed (%d)", errno);
+
+		if (strncmp(addrstr, preface, strlen(preface)) != 0)
+			support = SUPPORT_BACKWARD_COMPATIBILITY;
+
+		zassert_equal(support, SUPPORT_BACKWARD_COMPATIBILITY,
+			      "Server does not supprt backward compatibility");
+	} else if (test_support == SUPPORT_HTTP2_UPGRADE) {
+
+		memset(addrstr, '\0', sizeof(addrstr));
+		r = recv(client_fd, addrstr, sizeof(addrstr), 0);
+		zassert_not_equal(r, -1, "recv() failed (%d)", errno);
+
+		if (strncmp(addrstr, preface, strlen(preface)) == 0)
+			support = SUPPORT_HTTP2_UPGRADE;
+
+		zassert_equal(support, SUPPORT_HTTP2_UPGRADE,
+			      "Server does not supprt HTTP1 to HTTP2 upgrade");
+	}
+	r = close(client_fd);
+	zassert_not_equal(r, -1, "close() failed on the server fd (%d)", errno);
+}
+
+static void test_common(int test_support)
+{
+	int r;
+	int client_fd;
+	int proto = IPPROTO_TCP;
+	char *addrstrp;
+	k_tid_t server_thread_id;
+	struct sockaddr_in sa;
+	char addrstr[INET_ADDRSTRLEN];
+
+	k_sem_init(&server_sem, 0, 1);
+
+	struct http2_server_config config;
+	struct http2_server_ctx ctx;
+
+	config.port = PORT;
+	config.address_family = AF_INET;
+
+	int server_fd = http2_server_init(&ctx, &config);
+
+	zassert_true(server_fd >= 0, "Failed to create server socket");
+
+	server_thread_id = k_thread_create(
+	    &server_thread, server_stack, STACK_SIZE, server_thread_fn,
+	    INT_TO_POINTER(server_fd), INT_TO_POINTER(test_support), NULL,
+	    K_PRIO_PREEMPT(8), 0, K_NO_WAIT);
+
+	r = k_sem_take(&server_sem, K_MSEC(TIMEOUT));
+	zassert_equal(0, r, "failed to synchronize with server thread (%d)", r);
+
+	k_thread_name_set(k_current_get(), "client");
+
+	r = socket(AF_INET, SOCK_STREAM, proto);
+	zassert_not_equal(r, -1, "failed to create client socket (%d)", errno);
+	client_fd = r;
+
+	sa.sin_family = AF_INET;
+	sa.sin_port = htons(PORT);
+
+	r = inet_pton(AF_INET, MY_IPV4_ADDR, &sa.sin_addr.s_addr);
+	zassert_not_equal(-1, r, "inet_pton() failed (%d)", errno);
+	zassert_not_equal(0, r, "%s is not a valid IPv4 address", MY_IPV4_ADDR);
+	zassert_equal(1, r, "inet_pton() failed to convert %s", MY_IPV4_ADDR);
+
+	memset(addrstr, '\0', sizeof(addrstr));
+	addrstrp =
+	    (char *)inet_ntop(AF_INET, &sa.sin_addr, addrstr, sizeof(addrstr));
+	zassert_not_equal(addrstrp, NULL, "inet_ntop() failed (%d)", errno);
+
+	r = connect(client_fd, (struct sockaddr *)&sa, sizeof(sa));
+	zassert_not_equal(r, -1, "failed to connect (%d)", errno);
+
+	if (test_support == SUPPORT_BACKWARD_COMPATIBILITY) {
+
+		char *http1_request = "GET / HTTP/1.1\r\n\r\n";
+
+		r = send(client_fd, http1_request, strlen(http1_request), 0);
+		zassert_not_equal(r, -1, "send() failed (%d)", errno);
+	} else if (test_support == SUPPORT_HTTP2_UPGRADE) {
+
+		char *http2_upgrade_request =
+		    "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n";
+
+		r = send(client_fd, http2_upgrade_request,
+			 strlen(http2_upgrade_request), 0);
+		zassert_not_equal(r, -1, "send() failed (%d)", errno);
+	}
+
+	memset(addrstr, 0, sizeof(addrstr));
+	r = recv(client_fd, addrstr, sizeof(addrstr), 0);
+	zassert_not_equal(r, -1, "recv() failed (%d)", errno);
+
+	r = close(client_fd);
+	zassert_not_equal(-1, r, "close() failed on the client fd (%d)", errno);
+
+	r = close(server_fd);
+	zassert_not_equal(-1, r, "close() failed on the server fd (%d)", errno);
+
+	r = k_thread_join(&server_thread, K_FOREVER);
+	zassert_equal(0, r, "k_thread_join() failed (%d)", r);
+}
+
+ZTEST(server_function_tests, test_http2_upgrade)
+{
+	test_common(SUPPORT_HTTP2_UPGRADE);
+}
+
+ZTEST(server_function_tests, test_backward_compatibility)
+{
+	test_common(SUPPORT_BACKWARD_COMPATIBILITY);
+}
+
 ZTEST(server_function_tests, test_gen_gz_inc_file)
 {
 	const char *data;
@@ -38,11 +193,14 @@ ZTEST(server_function_tests, test_gen_gz_inc_file)
 	len = sizeof(inc_file);
 	len = len - 2;
 
-	zassert_equal(len, sizeof(compressed_inc_file), "Invalid compressed file size");
+	zassert_equal(len, sizeof(compressed_inc_file),
+		      "Invalid compressed file size");
 
 	int result = memcmp(data, compressed_inc_file, len);
 
-	zassert_equal(result, 0, "inc_file and compressed_inc_file contents are not identical.");
+	zassert_equal(
+	    result, 0,
+	    "inc_file and compressed_inc_file contents are not identical");
 }
 
 ZTEST(server_function_tests, test_http2_support_ipv6)
@@ -56,7 +214,8 @@ ZTEST(server_function_tests, test_http2_support_ipv6)
 	int server_fd = http2_server_init(&ctx, &config);
 
 	/* Check that the function returned a valid file descriptor */
-	zassert_true(server_fd >= 0, "Failed to initialize HTTP/2 server with IPv6");
+	zassert_true(server_fd >= 0,
+		     "Failed to initialize HTTP/2 server with IPv6");
 
 	close(server_fd);
 }
@@ -72,16 +231,18 @@ ZTEST(server_function_tests, test_http2_support_ipv4)
 	int server_fd = http2_server_init(&ctx, &config);
 
 	/* Check that the function returned a valid file descriptor */
-	zassert_true(server_fd >= 0, "Failed to initialize HTTP/2 server with IPv4");
+	zassert_true(server_fd >= 0,
+		     "Failed to initialize HTTP/2 server with IPv4");
 
 	close(server_fd);
 }
 
 ZTEST(server_function_tests, test_http2_server_stop)
 {
-	int sem_count = http2_server_stop(NULL, 0, NULL);
+	int sem_count = http2_server_stop();
 
-	zassert_equal(sem_count, 1, "Semaphore should have one token after 'quit' command");
+	zassert_equal(sem_count, 1,
+		      "Semaphore should have one token after 'quit' command");
 }
 
 ZTEST(server_function_tests, test_http2_server_init)
@@ -102,31 +263,41 @@ ZTEST(server_function_tests, test_http2_server_init)
 
 ZTEST(server_function_tests, test_parse_http2_frames)
 {
-	unsigned char buffer[] = {0x00, 0x00, 0x0c, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03,
-				  0x00, 0x00, 0x00, 0x64, 0x00, 0x04, 0x00, 0x00, 0xff, 0xff, 0x00,
-				  0x00, 0x21, 0x01, 0x05, 0x00, 0x00, 0x00, 0x01, 0x82, 0x84, 0x86,
-				  0x41, 0x8a, 0x0b, 0xe2, 0x5c, 0x0b, 0x89, 0x70, 0xdc, 0x78, 0x0f,
-				  0x03, 0x53, 0x03, 0x2a, 0x2f, 0x2a, 0x90, 0x7a, 0x8a, 0xaa, 0x69,
-				  0xd2, 0x9a, 0xc4, 0xc0, 0x57, 0x68, 0x0b, 0x83};
+	unsigned char buffer[] = {
+	    0x00, 0x00, 0x0c, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03,
+	    0x00, 0x00, 0x00, 0x64, 0x00, 0x04, 0x00, 0x00, 0xff, 0xff, 0x00,
+	    0x00, 0x21, 0x01, 0x05, 0x00, 0x00, 0x00, 0x01, 0x82, 0x84, 0x86,
+	    0x41, 0x8a, 0x0b, 0xe2, 0x5c, 0x0b, 0x89, 0x70, 0xdc, 0x78, 0x0f,
+	    0x03, 0x53, 0x03, 0x2a, 0x2f, 0x2a, 0x90, 0x7a, 0x8a, 0xaa, 0x69,
+	    0xd2, 0x9a, 0xc4, 0xc0, 0x57, 0x68, 0x0b, 0x83};
 	struct http2_frame frames[2];
 	unsigned int buffer_len = ARRAY_SIZE(buffer);
 	unsigned int frame_count;
 
 	/* Test: Buffer with exactly two frames */
 	frame_count = parse_http2_frames(buffer, buffer_len, frames);
-	zassert_equal(frame_count, 2, "Expected 2 frames for the given payload");
+	zassert_equal(frame_count, 2,
+		      "Expected 2 frames for the given payload");
 
 	/* Validate frame details for the 1st frame */
-	zassert_equal(frames[0].length, 0x0C, "Expected length for the 1st frame doesn't match");
-	zassert_equal(frames[0].type, 0x04, "Expected type for the 1st frame doesn't match");
-	zassert_equal(frames[0].flags, 0x00, "Expected flags for the 1st frame doesn't match");
-	zassert_equal(frames[0].stream_identifier, 0x00,
-		      "Expected stream_identifier for the 1st frame doesn't match");
+	zassert_equal(frames[0].length, 0x0C,
+		      "Expected length for the 1st frame doesn't match");
+	zassert_equal(frames[0].type, 0x04,
+		      "Expected type for the 1st frame doesn't match");
+	zassert_equal(frames[0].flags, 0x00,
+		      "Expected flags for the 1st frame doesn't match");
+	zassert_equal(
+	    frames[0].stream_identifier, 0x00,
+	    "Expected stream_identifier for the 1st frame doesn't match");
 
 	/* Validate frame details for the 2nd frame */
-	zassert_equal(frames[1].length, 0x21, "Expected length for the 2nd frame doesn't match");
-	zassert_equal(frames[1].type, 0x01, "Expected type for the 2nd frame doesn't match");
-	zassert_equal(frames[1].flags, 0x05, "Expected flags for the 2nd frame doesn't match");
-	zassert_equal(frames[1].stream_identifier, 0x01,
-		      "Expected stream_identifier for the 2nd frame doesn't match");
+	zassert_equal(frames[1].length, 0x21,
+		      "Expected length for the 2nd frame doesn't match");
+	zassert_equal(frames[1].type, 0x01,
+		      "Expected type for the 2nd frame doesn't match");
+	zassert_equal(frames[1].flags, 0x05,
+		      "Expected flags for the 2nd frame doesn't match");
+	zassert_equal(
+	    frames[1].stream_identifier, 0x01,
+	    "Expected stream_identifier for the 2nd frame doesn't match");
 }
