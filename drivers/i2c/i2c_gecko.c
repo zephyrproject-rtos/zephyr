bc0572b3d267ba09fc437aa17f49874e43ce025f
Kai Meinhard <kaimeinhard@hotmail.de>
2023-07-13 08:16:43 +0200

    i2c: add target mode to Silicon Labs Gecko I2C driver
    This commits adds the ability to use Silicon Lab chips as an I2C
    target. This could be used together with the EEPROM target
    driver.
    
    Signed-off-by: Kai Meinhard <kaimeinhard@hotmail.de>

--- drivers/i2c/i2c_gecko.c
+++ drivers/i2c/i2c_gecko.c
@@ -36,11 +36,18 @@ struct i2c_gecko_config {
 #else
 	uint8_t loc;
 #endif
+#if defined(CONFIG_I2C_TARGET)
+	void (*irq_config_func)(const struct device *dev);
+#endif
 };
 
 struct i2c_gecko_data {
 	struct k_sem device_sync_sem;
 	uint32_t dev_config;
+#if defined(CONFIG_I2C_TARGET)
+	struct i2c_target_config *target_cfg;
+	volatile bool target_read;
+#endif
 };
 
 void i2c_gecko_config_pins(const struct device *dev,
@@ -76,6 +83,9 @@ static int i2c_gecko_configure(const struct device *dev,
 {
 	I2C_TypeDef *base = DEV_BASE(dev);
 	struct i2c_gecko_data *data = dev->data;
+#if defined(CONFIG_I2C_TARGET)
+	const struct i2c_gecko_config *config = dev->config;
+#endif
 	I2C_Init_TypeDef i2cInit = I2C_INIT_DEFAULT;
 	uint32_t baudrate;
 
@@ -99,6 +109,11 @@ static int i2c_gecko_configure(const struct device *dev,
 
 	data->dev_config = dev_config_raw;
 	i2cInit.freq = baudrate;
+
+#if defined(CONFIG_I2C_TARGET)
+	i2cInit.master = 0;
+	BUS_RegBitWrite(&(config->base->CTRL), _I2C_CTRL_AUTOACK_SHIFT, 1);
+#endif
 
 	I2C_Init(base, &i2cInit);
 
@@ -189,10 +204,102 @@ static int i2c_gecko_init(const struct device *dev)
 	return 0;
 }
 
+#if defined(CONFIG_I2C_TARGET)
+static int i2c_gecko_target_register(const struct device *dev, struct i2c_target_config *cfg)
+{
+	const struct i2c_gecko_config *config = dev->config;
+	struct i2c_gecko_data *data = dev->data;
+
+	data->target_cfg = cfg;
+
+	I2C_SlaveAddressSet(config->base, cfg->address << _I2C_SADDR_ADDR_SHIFT);
+	I2C_SlaveAddressMaskSet(config->base, _I2C_SADDRMASK_SADDRMASK_MASK);
+
+	I2C_IntDisable(config->base, _I2C_IEN_MASK);
+	I2C_IntEnable(config->base, I2C_IEN_ADDR | I2C_IEN_RXDATAV | I2C_IEN_SSTOP);
+
+	config->irq_config_func(dev);
+
+	return 0;
+}
+
+static int i2c_gecko_target_unregister(const struct device *dev, struct i2c_target_config *cfg)
+{
+	const struct i2c_gecko_config *config = dev->config;
+	struct i2c_gecko_data *data = dev->data;
+
+	data->target_cfg = NULL;
+
+	I2C_IntDisable(config->base, _I2C_IEN_MASK);
+
+	return 0;
+}
+#endif
+
 static const struct i2c_driver_api i2c_gecko_driver_api = {
 	.configure = i2c_gecko_configure,
 	.transfer = i2c_gecko_transfer,
+#if defined(CONFIG_I2C_TARGET)
+	.target_register = i2c_gecko_target_register,
+	.target_unregister = i2c_gecko_target_unregister,
+#endif
 };
+
+#if defined(CONFIG_I2C_TARGET)
+static void i2c_gecko_isr(const struct device *dev)
+{
+	const struct i2c_gecko_config *config = dev->config;
+	struct i2c_gecko_data *data = dev->data;
+	uint32_t status;
+	uint32_t rx_data;
+	uint8_t tx_byte;
+
+	status = config->base->IF;
+
+	if (status & I2C_IF_ADDR && status & I2C_IF_RXDATAV) {
+		/* Address Match */
+		rx_data = config->base->RXDATA;
+		if (rx_data & 0x1) {
+			data->target_read = true;
+
+			data->target_cfg->callbacks->read_requested(data->target_cfg, &tx_byte);
+			config->base->TXDATA = tx_byte;
+
+			I2C_IntEnable(config->base, I2C_IEN_BUSHOLD);
+		} else {
+			data->target_read = false;
+			data->target_cfg->callbacks->write_requested(data->target_cfg);
+		}
+		I2C_IntClear(config->base, I2C_IF_ADDR);
+		I2C_IntClear(config->base, I2C_IF_RXDATAV);
+	} else if (status & I2C_IF_RXDATAV) {
+		if (data->target_read == true) {
+			LOG_ERR("Unexpected Data");
+		} else {
+			rx_data = config->base->RXDATA;
+			data->target_cfg->callbacks->write_received(data->target_cfg, rx_data);
+		}
+		I2C_IntClear(config->base, I2C_IF_RXDATAV);
+	} else if (data->target_read && status & I2C_IF_BUSHOLD && !(status & I2C_IF_SSTOP)) {
+		data->target_cfg->callbacks->read_processed(data->target_cfg, &tx_byte);
+		config->base->TXDATA = tx_byte;
+
+		I2C_IntClear(config->base, I2C_IF_BUSHOLD);
+	}
+
+	if (status & I2C_IF_SSTOP) {
+		if (config->base->STATUS & I2C_STATUS_RXDATAV) {
+			LOG_ERR("Still valid data");
+		}
+		/* Stop received, reception is ended */
+		data->target_cfg->callbacks->stop(data->target_cfg);
+
+		I2C_IntClear(config->base, I2C_IF_SSTOP);
+		I2C_IntClear(config->base, I2C_IF_BUSHOLD);
+		I2C_IntDisable(config->base, I2C_IEN_BUSHOLD);
+	}
+}
+#endif
 
 #ifdef CONFIG_SOC_GECKO_HAS_INDIVIDUAL_PIN_LOCATION
 #define I2C_LOC_DATA(idx) \
@@ -208,25 +315,39 @@ static const struct i2c_driver_api i2c_gecko_driver_api = {
 	.loc = DT_INST_PROP_BY_IDX(idx, location_scl, 0)
 #endif
 
-#define I2C_INIT(idx) \
-I2C_VALIDATE_LOC(idx); \
-static const struct i2c_gecko_config i2c_gecko_config_##idx = { \
-	.base = (I2C_TypeDef *)DT_INST_REG_ADDR(idx), \
-	.clock = cmuClock_I2C##idx, \
-	.pin_sda = {DT_INST_PROP_BY_IDX(idx, location_sda, 1), \
-		DT_INST_PROP_BY_IDX(idx, location_sda, 2), gpioModeWiredAnd, 1}, \
-	.pin_scl = {DT_INST_PROP_BY_IDX(idx, location_scl, 1), \
-		DT_INST_PROP_BY_IDX(idx, location_scl, 2), gpioModeWiredAnd, 1}, \
-	I2C_LOC_DATA(idx), \
-	.bitrate = DT_INST_PROP(idx, clock_frequency), \
-}; \
-\
-static struct i2c_gecko_data i2c_gecko_data_##idx; \
-\
-I2C_DEVICE_DT_INST_DEFINE(idx, i2c_gecko_init, \
-		 NULL, \
-		 &i2c_gecko_data_##idx, &i2c_gecko_config_##idx, \
-		 POST_KERNEL, CONFIG_I2C_INIT_PRIORITY, \
-		 &i2c_gecko_driver_api);
+#if defined(CONFIG_I2C_TARGET)
+#define GECKO_I2C_IRQ_DEF(idx)  static void i2c_gecko_config_func_##idx(const struct device *dev);
+#define GECKO_I2C_IRQ_DATA(idx) .irq_config_func = i2c_gecko_config_func_##idx,
+#define GECKO_I2C_IRQ_HANDLER(idx)                                                                 \
+	static void i2c_gecko_config_func_##idx(const struct device *dev)                          \
+	{                                                                                          \
+		IRQ_CONNECT(DT_INST_IRQ(idx, irq), DT_INST_IRQ(idx, priority), i2c_gecko_isr,      \
+			    DEVICE_DT_INST_GET(idx), 0);                                           \
+		irq_enable(DT_INST_IRQ(idx, irq));                                                 \
+	}
+#else
+#define GECKO_I2C_IRQ_HANDLER(idx)
+#define GECKO_I2C_IRQ_DEF(idx)
+#define GECKO_I2C_IRQ_DATA(idx)
+#endif
+
+#define I2C_INIT(idx)                                                                              \
+	I2C_VALIDATE_LOC(idx);                                                                     \
+	GECKO_I2C_IRQ_DEF(idx)                                                                     \
+	static const struct i2c_gecko_config i2c_gecko_config_##idx = {                            \
+		.base = (I2C_TypeDef *)DT_INST_REG_ADDR(idx),                                      \
+		.clock = cmuClock_I2C##idx,                                                        \
+		.pin_sda = {DT_INST_PROP_BY_IDX(idx, location_sda, 1),                             \
+			    DT_INST_PROP_BY_IDX(idx, location_sda, 2), gpioModeWiredAnd, 1},    \
+		.pin_scl = {DT_INST_PROP_BY_IDX(idx, location_scl, 1),                             \
+			    DT_INST_PROP_BY_IDX(idx, location_scl, 2), gpioModeWiredAnd, 1},    \
+		I2C_LOC_DATA(idx),                                                                 \
+		.bitrate = DT_INST_PROP(idx, clock_frequency),                                     \
+		GECKO_I2C_IRQ_DATA(idx)};                                                          \
+	static struct i2c_gecko_data i2c_gecko_data_##idx;                                         \
+	I2C_DEVICE_DT_INST_DEFINE(idx, i2c_gecko_init, NULL, &i2c_gecko_data_##idx,                \
+				  &i2c_gecko_config_##idx, POST_KERNEL, CONFIG_I2C_INIT_PRIORITY,  \
+				  &i2c_gecko_driver_api);                                          \
+	GECKO_I2C_IRQ_HANDLER(idx)
 
 DT_INST_FOREACH_STATUS_OKAY(I2C_INIT)
