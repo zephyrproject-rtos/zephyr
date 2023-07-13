18636af6d6658cf66e5f055901ffe2515116d329
Carlo Caione <ccaione@baylibre.com>
2023-07-13 09:03:26 +0200

    arm64: Add frame-pointer based stack unwinding
    Add the frame-pointer based stack unwinding on ARM64.
    
    This is a typical output with the feature enabled:
    
    *** Booting Zephyr OS build zephyr-v3.4.0-1029-gae22ff648c16 ***
    E: ELR_ELn: 0x00000000400011c8
    E: ESR_ELn: 0x0000000096000046
    E:   EC:  0x25 (Data Abort taken without a change in Exception level)
    E:   IL:  0x1
    E:   ISS: 0x46
    E: FAR_ELn: 0x0000000000000000
    E: TPIDRRO: 0x0100000040011650
    E: x0:  0x0000000000000000  x1:  0x0000000000000003
    E: x2:  0x0000000000000003  x3:  0x000000004005c6a0
    E: x4:  0x0000000000000000  x5:  0x000000004005c7f0
    E: x6:  0x0000000048000000  x7:  0x0000000048000000
    E: x8:  0x0000000000000005  x9:  0x0000000000000000
    E: x10: 0x0000000000000000  x11: 0x0000000000000000
    E: x12: 0x0000000000000000  x13: 0x0000000000000000
    E: x14: 0x0000000000000000  x15: 0x0000000000000000
    E: x16: 0x0000000040004290  x17: 0x0000000000000000
    E: x18: 0x0000000000000000  lr:  0x0000000040001208
    E:
    E: backtrace  0: fp: 0x000000004005c690 lr: 0x0000000040001270
    E: backtrace  1: fp: 0x000000004005c6b0 lr: 0x0000000040001290
    E: backtrace  2: fp: 0x000000004005c7d0 lr: 0x0000000040004ac0
    E: backtrace  3: fp: 0x000000004005c7f0 lr: 0x00000000400013a4
    E: backtrace  4: fp: 0x000000004005c800 lr: 0x0000000000000000
    E:
    E: >>> ZEPHYR FATAL ERROR 0: CPU exception on CPU 0
    E: Current thread: 0x40011310 (unknown)
    E: Halting system
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

--- include/zephyr/arch/arm64/exc.h
+++ include/zephyr/arch/arm64/exc.h
@@ -47,10 +47,13 @@ struct __esf {
 	uint64_t lr;
 	uint64_t spsr;
 	uint64_t elr;
+#ifdef CONFIG_ARM64_ENABLE_FRAME_POINTER
+	uint64_t fp;
+#endif
 #ifdef CONFIG_ARM64_SAFE_EXCEPTION_STACK
 	uint64_t sp;
 #endif
-} __aligned(16);
+} __packed __aligned(16);
 
 typedef struct __esf z_arch_esf_t;
 
