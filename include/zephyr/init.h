122b8e40ae18ad434b56fb7940bd4fcec9123ea1
Jordan Yates <jordan.yates@data61.csiro.au>
2023-07-15 21:52:17 +1000

    init: add sub-priority to internal ordering
    Add a sub-priority field to `Z_INIT_ENTRY_SECTION`, which is used to
    ensure that multiple drivers running at the same init priority still
    run in an order that respects their devicetree dependencies.
    
    This is primarily useful when multiple instances of the same device are
    instantiated that can depend on each other, for example power domains
    and i2c muxes.
    
    Signed-off-by: Jordan Yates <jordan.yates@data61.csiro.au>

--- include/zephyr/init.h
+++ include/zephyr/init.h
@@ -128,10 +128,12 @@ struct init_entry {
  * @brief Init entry section.
  *
  * Each init entry is placed in a section with a name crafted so that it allows
- * linker scripts to sort them according to the specified level/priority.
+ * linker scripts to sort them according to the specified
+ * level/priority/sub-priority.
  */
-#define Z_INIT_ENTRY_SECTION(level, prio)                                      \
-	__attribute__((__section__(".z_init_" #level STRINGIFY(prio)"_")))
+#define Z_INIT_ENTRY_SECTION(level, prio, sub_prio)                           \
+	__attribute__((__section__(                                           \
+		".z_init_" #level STRINGIFY(prio)"_" STRINGIFY(sub_prio)"_")))
 
 /** @endcond */
 
@@ -186,7 +188,7 @@ struct init_entry {
  */
 #define SYS_INIT_NAMED(name, init_fn_, level, prio)                            \
 	static const Z_DECL_ALIGN(struct init_entry)                           \
-		Z_INIT_ENTRY_SECTION(level, prio) __used __noasan              \
+		Z_INIT_ENTRY_SECTION(level, prio, 0) __used __noasan           \
 		Z_INIT_ENTRY_NAME(name) = {                                    \
 			.init_fn = {.sys = (init_fn_)},                        \
 			.dev = NULL,                                           \
