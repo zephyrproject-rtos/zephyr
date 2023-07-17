85955f4f450c05976ac63f07565ff394d19e4299
Declan Snyder <declan.snyder@nxp.com>
2023-07-17 14:34:15 -0500

    dts: bindings: Add NXP ENET bindings
    Add bindings for compatibles related to NXP ENET IP.
    
    Signed-off-by: Declan Snyder <declan.snyder@nxp.com>

---
+++ include/zephyr/dt-bindings/ethernet/nxp_enet.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2023 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_NXP_ENET_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_NXP_ENET_H_
+
+#define NXP_ENET_MII_MODE	0
+#define NXP_ENET_RMII_MODE	1
+#define NXP_ENET_INVALID_MII_MODE 100
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_NXP_ENET_H_ */
