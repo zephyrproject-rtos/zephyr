260de6910db0d0220d12492fb6eeb224636916d7
Chirag Kochar <chirag.kochar@intel.com>
2023-07-18 14:09:18 +0800

    soc: arm64: intel_socfpga: Memory mapped pinmux register access.
    Add memory mapped pinmux register access for INTEL AGILEX SOC FPGA.
    
    Signed-off-by: Chirag Kochar <chirag.kochar@intel.com>

--- soc/arm64/intel_socfpga/agilex/mmu_regions.c
+++ soc/arm64/intel_socfpga/agilex/mmu_regions.c
@@ -26,6 +26,12 @@ static const struct arm_mmu_region mmu_regions[] = {
 			      DT_REG_SIZE(DT_NODELABEL(uart0)),
 			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
 
+	MMU_REGION_FLAT_ENTRY("PINMUX",
+			      DT_REG_ADDR(DT_NODELABEL(pinmux)),
+			      DT_REG_SIZE(DT_NODELABEL(pinmux)),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
+
+
 	MMU_REGION_FLAT_ENTRY("GIC",
 			      DT_REG_ADDR_BY_IDX(DT_NODELABEL(gic), 0),
 			      DT_REG_SIZE_BY_IDX(DT_NODELABEL(gic), 0),
