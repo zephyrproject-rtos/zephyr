43c549305bdb0770a3fbebe7347b3da8b2d9ceec
Yong Cong Sin <ycsin@meta.com>
2023-07-18 13:00:59 +0800

    posix: signal: check RTSIG limit
    Use build assert to make sure that the realtime signal
    constants are configured properly in the Kconfig.
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>

--- lib/posix/signal.c
+++ lib/posix/signal.c
@@ -13,6 +13,9 @@
 #define SIGNO_WORD_IDX(_signo) (signo / BITS_PER_LONG)
 #define SIGNO_WORD_BIT(_signo) (signo & BIT_MASK(LOG2(BITS_PER_LONG)))
 
+BUILD_ASSERT(CONFIG_POSIX_LIMITS_RTSIG_MAX >= 0);
+BUILD_ASSERT(CONFIG_POSIX_RTSIG_MAX >= CONFIG_POSIX_LIMITS_RTSIG_MAX);
+
 static inline bool signo_valid(int signo)
 {
 	return ((signo > 0) && (signo < _NSIG));
