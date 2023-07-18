833900066d0a8e0d7072a842dabbd5f0d904ded6
Benjamin Cabé <benjamin@zephyrproject.org>
2023-07-18 14:41:43 +0200

    input: doc: document event definitions
    Added missing documentation to event types, key codes, etc.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- include/zephyr/dt-bindings/input/input-event-codes.h
+++ include/zephyr/dt-bindings/input/input-event-codes.h
@@ -22,12 +22,12 @@
  * @anchor INPUT_EV_CODES
  * @{
  */
-#define INPUT_EV_KEY 0x01
-#define INPUT_EV_REL 0x02
-#define INPUT_EV_ABS 0x03
-#define INPUT_EV_MSC 0x04
-#define INPUT_EV_VENDOR_START 0xf0
-#define INPUT_EV_VENDOR_STOP 0xff
+#define INPUT_EV_KEY 0x01               /**< Key event */
+#define INPUT_EV_REL 0x02               /**< Relative coordinate event */
+#define INPUT_EV_ABS 0x03               /**< Absolute coordinate event */
+#define INPUT_EV_MSC 0x04               /**< Miscellaneous event */
+#define INPUT_EV_VENDOR_START 0xf0      /**< Vendor specific event start */
+#define INPUT_EV_VENDOR_STOP 0xff       /**< Vendor specific event stop */
 /** @} */
 
 /**
@@ -35,72 +35,73 @@
  * @anchor INPUT_KEY_CODES
  * @{
  */
-#define INPUT_KEY_0 11
-#define INPUT_KEY_1 2
-#define INPUT_KEY_2 3
-#define INPUT_KEY_3 4
-#define INPUT_KEY_4 5
-#define INPUT_KEY_5 6
-#define INPUT_KEY_6 7
-#define INPUT_KEY_7 8
-#define INPUT_KEY_8 9
-#define INPUT_KEY_9 10
-#define INPUT_KEY_A 30
-#define INPUT_KEY_B 48
-#define INPUT_KEY_C 46
-#define INPUT_KEY_D 32
-#define INPUT_KEY_E 18
-#define INPUT_KEY_F 33
-#define INPUT_KEY_G 34
-#define INPUT_KEY_H 35
-#define INPUT_KEY_I 23
-#define INPUT_KEY_J 36
-#define INPUT_KEY_K 37
-#define INPUT_KEY_L 38
-#define INPUT_KEY_M 50
-#define INPUT_KEY_N 49
-#define INPUT_KEY_O 24
-#define INPUT_KEY_P 25
-#define INPUT_KEY_Q 16
-#define INPUT_KEY_R 19
-#define INPUT_KEY_S 31
-#define INPUT_KEY_T 20
-#define INPUT_KEY_U 22
-#define INPUT_KEY_V 47
-#define INPUT_KEY_VOLUMEDOWN 114
-#define INPUT_KEY_VOLUMEUP 115
-#define INPUT_KEY_W 17
-#define INPUT_KEY_X 45
-#define INPUT_KEY_Y 21
-#define INPUT_KEY_Z 44
+#define INPUT_KEY_0 11                  /**< 0 Key */
+#define INPUT_KEY_1 2                   /**< 1 Key */
+#define INPUT_KEY_2 3                   /**< 2 Key */
+#define INPUT_KEY_3 4                   /**< 3 Key */
+#define INPUT_KEY_4 5                   /**< 4 Key */
+#define INPUT_KEY_5 6                   /**< 5 Key */
+#define INPUT_KEY_6 7                   /**< 6 Key */
+#define INPUT_KEY_7 8                   /**< 7 Key */
+#define INPUT_KEY_8 9                   /**< 8 Key */
+#define INPUT_KEY_9 10                  /**< 9 Key */
+#define INPUT_KEY_A 30                  /**< A Key */
+#define INPUT_KEY_B 48                  /**< B Key */
+#define INPUT_KEY_C 46                  /**< C Key */
+#define INPUT_KEY_D 32                  /**< D Key */
+#define INPUT_KEY_E 18                  /**< E Key */
+#define INPUT_KEY_F 33                  /**< F Key */
+#define INPUT_KEY_G 34                  /**< G Key */
+#define INPUT_KEY_H 35                  /**< H Key */
+#define INPUT_KEY_I 23                  /**< I Key */
+#define INPUT_KEY_J 36                  /**< J Key */
+#define INPUT_KEY_K 37                  /**< K Key */
+#define INPUT_KEY_L 38                  /**< L Key */
+#define INPUT_KEY_M 50                  /**< M Key */
+#define INPUT_KEY_N 49                  /**< N Key */
+#define INPUT_KEY_O 24                  /**< O Key */
+#define INPUT_KEY_P 25                  /**< P Key */
+#define INPUT_KEY_Q 16                  /**< Q Key */
+#define INPUT_KEY_R 19                  /**< R Key */
+#define INPUT_KEY_S 31                  /**< S Key */
+#define INPUT_KEY_T 20                  /**< T Key */
+#define INPUT_KEY_U 22                  /**< U Key */
+#define INPUT_KEY_V 47                  /**< V Key */
+#define INPUT_KEY_VOLUMEDOWN 114        /**< Volume Down Key */
+#define INPUT_KEY_VOLUMEUP 115          /**< Volume Up Key */
+#define INPUT_KEY_W 17                  /**< W Key */
+#define INPUT_KEY_X 45                  /**< X Key */
+#define INPUT_KEY_Y 21                  /**< Y Key */
+#define INPUT_KEY_Z 44                  /**< Z Key */
 /** @} */
+
 
 /**
  * @name Input event BTN codes.
  * @anchor INPUT_BTN_CODES
  * @{
  */
-#define INPUT_BTN_DPAD_DOWN 0x221
-#define INPUT_BTN_DPAD_LEFT 0x222
-#define INPUT_BTN_DPAD_RIGHT 0x223
-#define INPUT_BTN_DPAD_UP 0x220
-#define INPUT_BTN_EAST 0x131
-#define INPUT_BTN_LEFT 0x110
-#define INPUT_BTN_MIDDLE 0x112
-#define INPUT_BTN_MODE 0x13c
-#define INPUT_BTN_NORTH 0x133
-#define INPUT_BTN_RIGHT 0x111
-#define INPUT_BTN_SELECT 0x13a
-#define INPUT_BTN_SOUTH 0x130
-#define INPUT_BTN_START 0x13b
-#define INPUT_BTN_THUMBL 0x13d
-#define INPUT_BTN_THUMBR 0x13e
-#define INPUT_BTN_TL 0x136
-#define INPUT_BTN_TL2 0x138
-#define INPUT_BTN_TOUCH 0x14a
-#define INPUT_BTN_TR 0x137
-#define INPUT_BTN_TR2 0x139
-#define INPUT_BTN_WEST 0x134
+#define INPUT_BTN_DPAD_DOWN 0x221       /**< Directional pad Down */
+#define INPUT_BTN_DPAD_LEFT 0x222       /**< Directional pad Left */
+#define INPUT_BTN_DPAD_RIGHT 0x223      /**< Directional pad Right */
+#define INPUT_BTN_DPAD_UP 0x220         /**< Directional pad Up */
+#define INPUT_BTN_EAST 0x131            /**< East button */
+#define INPUT_BTN_LEFT 0x110            /**< Left button */
+#define INPUT_BTN_MIDDLE 0x112          /**< Middle button */
+#define INPUT_BTN_MODE 0x13c            /**< Mode button */
+#define INPUT_BTN_NORTH 0x133           /**< North button */
+#define INPUT_BTN_RIGHT 0x111           /**< Right button */
+#define INPUT_BTN_SELECT 0x13a          /**< Select button */
+#define INPUT_BTN_SOUTH 0x130           /**< South button */
+#define INPUT_BTN_START 0x13b           /**< Start button */
+#define INPUT_BTN_THUMBL 0x13d          /**< Left thumbstick button */
+#define INPUT_BTN_THUMBR 0x13e          /**< Right thumbstick button */
+#define INPUT_BTN_TL 0x136              /**< Left trigger (L1) */
+#define INPUT_BTN_TL2 0x138             /**< Left trigger 2 (L2) */
+#define INPUT_BTN_TOUCH 0x14a           /**< Touchscreen touch */
+#define INPUT_BTN_TR 0x137              /**< Right trigger (R1) */
+#define INPUT_BTN_TR2 0x139             /**< Right trigger 2 (R2) */
+#define INPUT_BTN_WEST 0x134            /**< West button */
 /** @} */
 
 /**
@@ -108,12 +109,12 @@
  * @anchor INPUT_ABS_CODES
  * @{
  */
-#define INPUT_ABS_RX 0x03
-#define INPUT_ABS_RY 0x04
-#define INPUT_ABS_RZ 0x05
-#define INPUT_ABS_X 0x00
-#define INPUT_ABS_Y 0x01
-#define INPUT_ABS_Z 0x02
+#define INPUT_ABS_RX 0x03               /**< Absolute rotation around X axis */
+#define INPUT_ABS_RY 0x04               /**< Absolute rotation around Y axis */
+#define INPUT_ABS_RZ 0x05               /**< Absolute rotation around Z axis */
+#define INPUT_ABS_X 0x00                /**< Absolute X coordinate */
+#define INPUT_ABS_Y 0x01                /**< Absolute Y coordinate */
+#define INPUT_ABS_Z 0x02                /**< Absolute Z coordinate */
 /** @} */
 
 /**
@@ -121,12 +122,12 @@
  * @anchor INPUT_REL_CODES
  * @{
  */
-#define INPUT_REL_RX 0x03
-#define INPUT_REL_RY 0x04
-#define INPUT_REL_RZ 0x05
-#define INPUT_REL_X 0x00
-#define INPUT_REL_Y 0x01
-#define INPUT_REL_Z 0x02
+#define INPUT_REL_RX 0x03               /**< Relative rotation around X axis */
+#define INPUT_REL_RY 0x04               /**< Relative rotation around Y axis */
+#define INPUT_REL_RZ 0x05               /**< Relative rotation around Z axis */
+#define INPUT_REL_X 0x00                /**< Relative X coordinate */
+#define INPUT_REL_Y 0x01                /**< Relative Y coordinate */
+#define INPUT_REL_Z 0x02                /**< Relative Z coordinate */
 /** @} */
 
 /**
@@ -134,7 +135,7 @@
  * @anchor INPUT_MSC_CODES
  * @{
  */
-#define INPUT_MSC_SCAN 0x04
+#define INPUT_MSC_SCAN 0x04             /**< Scan code */
 /** @} */
 
 /** @} */
