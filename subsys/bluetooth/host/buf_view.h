e9e5f51d35a3781ae8254b4cd0f313947802a33e
Jonathan Rico <jonathan.rico@nordicsemi.no>
2023-07-18 10:39:44 +0200

    Bluetooth: host: Introduce "view" buffer concept
    Instead of allocating segments/fragments and copying data into them, we
    allocate segments as "views" (or slices) into the original buffer.
    
    The view also gives access to the headroom of the original buffer, allowing
    lower layers to push their headers.
    
    We choose not to allow multiple views into the same buffer as the headroom
    of a view would overlap with the data of the previous view.
    
    We mark a buffer as locked (or "in-view") by temporarily setting its
    headroom to zero. This effectively stops create_view because the requested
    headroom is not available.
    
    Each layer that does some kind of fragmentation and wants to use views for
    that needs to maintain a buffer pool (bufsize 0, count = max views) and a
    metadata array (size = max views) for the view mechanism to work.
    
    Maximum number of views: number of parallel buffers from the upper layer,
    e.g. number of L2CAP channels for L2CAP segmentation or number of ACL
    connections for HCI fragmentation.
    
    Reason for the change:
    1. prevent deadlocks or (ATT/SMP) requests timing out
    2. save time (zero-copy)
    3. save memory (gets rid of frag pools)
    
    L2CAP CoC: would either allocate from the `alloc_seg` application callback,
    or worse _steal_ from the same pool, or allocate from the global ACL pool.
    
    Conn/HCI: would either allocate from `frag_pool` or the global ACL pool.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>
    Co-authored-by: Aleksander Wasaznik <aleksander.wasaznik@nordicsemi.no>

---
+++ subsys/bluetooth/host/buf_view.h
@@ -0,0 +1,85 @@
+/** @file
+ *  @brief Bluetooth "view" buffer abstraction
+ */
+
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SUBSYS_BLUETOOTH_HOST_BUF_VIEW_H_
+#define ZEPHYR_SUBSYS_BLUETOOTH_HOST_BUF_VIEW_H_
+
+#include <zephyr/net/buf.h>
+
+
+struct bt_buf_view_meta {
+	struct net_buf *parent;
+	/* saves the data pointers while the parent buffer is locked. */
+	struct net_buf_simple backup;
+};
+
+/** @internal
+ *
+ *  @brief Create a "view" or "window" into an existing buffer.
+ *  - enforces one active view at a time per-buffer
+ *  -> this restriction enables prepending data (ie. for headers)
+ *  - forbids appending data to the view
+ *  - pulls the size of the view from said buffer.
+ *
+ *  The "virtual buffer" that is generated has to be allocated from a buffer
+ *  pool. This is to allow refcounting and attaching a destroy callback. The
+ *  configured size of the buffers in that pool should be zero-length.
+ *
+ *  The user-data size is application-dependent, but should be minimized to save
+ *  memory. user_data is not used by the view API.
+ *
+ *  The view mechanism needs to store extra metadata in order to unlock the
+ *  original buffer when the view is destroyed.
+ *
+ *  The storage and allocation of the view buf pool and the view metadata is the
+ *  application's responsibility.
+ *
+ *  @note The `headroom` param is only used for __ASSERT(). The idea is that
+ *  it's easier to debug a headroom assert failure at allocation time, rather
+ *  than later down the line when a lower layer tries to add its headers and
+ *  fails.
+ *
+ *  @param view         Uninitialized "View" buffer
+ *  @param parent       Buffer data is pulled from into `view`
+ *  @param len          Amount to pull
+ *  @param meta         Uninitialized metadata storage
+ *
+ *  @return view if the operation was successful. NULL on error.
+ */
+struct net_buf *bt_buf_make_view(struct net_buf *view,
+				 struct net_buf *parent,
+				 size_t len,
+				 struct bt_buf_view_meta *meta);
+
+/** @internal
+ *
+ *  @brief Check if if `parent` has view.
+ *
+ *  If `parent` has been passed to @ref bt_buf_make_view() and the resulting
+ *  view buffer has not been destroyed.
+ */
+bool bt_buf_has_view(const struct net_buf *parent);
+
+/** @internal
+ *
+ *  @brief Destroy the view buffer
+ *
+ *  Equivalent of @ref net_buf_destroy.
+ *  It is mandatory to call this from the view pool's `destroy` callback.
+ *
+ *  This frees the parent buffer, and allows calling @ref bt_buf_make_view again.
+ *  The metadata is also freed for re-use.
+ *
+ *  @param view View to destroy
+ *  @param meta Meta that was given to @ref bt_buf_make_view
+ */
+void bt_buf_destroy_view(struct net_buf *view, struct bt_buf_view_meta *meta);
+
+#endif /* ZEPHYR_SUBSYS_BLUETOOTH_HOST_BUF_VIEW_H_ */
