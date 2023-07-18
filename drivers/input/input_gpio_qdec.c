a624f07cfc80caa12cf896f8cc0cf18e5fc19d1f
Fabio Baltieri <fabiobaltieri@google.com>
2023-07-18 11:28:40 +0000

    drivers: input: add few missing gpio_add_callback return check
    Add few missing check on gpio_add_callback and gpio_add_callback_dt
    calls, fixes a coverity warning.
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

--- drivers/input/input_gpio_qdec.c
+++ drivers/input/input_gpio_qdec.c
@@ -204,7 +204,11 @@ static int gpio_qdec_init(const struct device *dev)
 			return ret;
 		}
 
-		gpio_add_callback_dt(gpio, &data->gpio_cb);
+		ret = gpio_add_callback_dt(gpio, &data->gpio_cb);
+		if (ret < 0) {
+			LOG_ERR("Could not set gpio callback");
+			return ret;
+		}
 	}
 
 	data->prev_step = gpio_qdec_get_step(dev);
