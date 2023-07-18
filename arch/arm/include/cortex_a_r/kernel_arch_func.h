2011dee2b39b66755e7de2c0829d6e2e03fbdc34
Huifeng Zhang <Huifeng.Zhang@arm.com>
2023-07-18 13:22:52 +0800

    arch: arm: cortex_a_r: introduce USE_SWITCH
    This commit introduce 'USE_SWITCH' feature into cortex-A/R(aarch32)
    architecture
    
    For introducing USE_SWITCH, the exception entry and exit are unified via
    `z_arm_cortex_ar_enter_exc` and `z_arm_cortex_ar_exit_exc`. All
    exceptions including ISR are using this way to enter and exit exception
    handler.
    
    Differentiate exception depth and interrupt depth. Allow doing
    context switch when exception depth greater than 1 but not allow doing
    this when interrupt depth greater than 1.
    
    Currently, USE_SWITCH doesn't support FPU_SHARING and USERSPACE.
    
    Signed-off-by: Huifeng Zhang <Huifeng.Zhang@arm.com>

--- arch/arm/include/cortex_a_r/kernel_arch_func.h
+++ arch/arm/include/cortex_a_r/kernel_arch_func.h
@@ -30,11 +30,29 @@ static ALWAYS_INLINE void arch_kernel_init(void)
 {
 }
 
+#ifndef CONFIG_USE_SWITCH
+
 static ALWAYS_INLINE void
 arch_thread_return_value_set(struct k_thread *thread, unsigned int value)
 {
 	thread->arch.swap_return_value = value;
 }
+
+#else
+
+static ALWAYS_INLINE void arch_switch(void *switch_to, void **switched_from)
+{
+	extern void z_arm_context_switch(struct k_thread *new,
+					struct k_thread *old);
+
+	struct k_thread *new = switch_to;
+	struct k_thread *old = CONTAINER_OF(switched_from, struct k_thread,
+					    switch_handle);
+
+	z_arm_context_switch(new, old);
+}
+
+#endif
 
 extern FUNC_NORETURN void z_arm_userspace_enter(k_thread_entry_t user_entry,
 					       void *p1, void *p2, void *p3,
