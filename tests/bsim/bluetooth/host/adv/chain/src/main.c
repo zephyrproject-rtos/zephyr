9de3e62fb2928ddcec35b4c6505d2b9734992850
Andries Kruithof <andries.kruithof@nordicsemi.no>
2023-07-18 11:13:31 +0200

    Test: Bluetooth: controller: add CI testing for advertising chaining
    Update babblesim tests for testing chaining for advertising to ensure that
    we spill over to the next PDU
    
    Signed-off-by: Andries Kruithof <andries.kruithof@nordicsemi.no>

--- tests/bsim/bluetooth/host/adv/chain/src/main.c
+++ tests/bsim/bluetooth/host/adv/chain/src/main.c
@@ -34,8 +34,12 @@
 #define NAME_LEN 30
 #define BT_AD_DATA_NAME_SIZE     (sizeof(CONFIG_BT_DEVICE_NAME) - 1U + 2U)
 #define BT_AD_DATA_MFG_DATA_SIZE (254U + 2U)
+/*
+ * for testing chaining the manufacturing data is duplicated, hence DATA_LEN needs to
+ * add twice the size for this element
+ */
 #define DATA_LEN                 MIN((BT_AD_DATA_NAME_SIZE + \
-				      BT_AD_DATA_MFG_DATA_SIZE), \
+				      BT_AD_DATA_MFG_DATA_SIZE + BT_AD_DATA_MFG_DATA_SIZE), \
 				     CONFIG_BT_CTLR_ADV_DATA_LEN_MAX)
 
 static K_SEM_DEFINE(sem_recv, 0, 1);
@@ -94,6 +98,7 @@ static void scan_recv(const struct bt_le_scan_recv_info *info,
 
 	data_len = buf->len;
 	if (data_len != DATA_LEN) {
+		printk("Received datalength: %d\n", data_len);
 		return;
 	}
 
@@ -101,11 +106,13 @@ static void scan_recv(const struct bt_le_scan_recv_info *info,
 	bt_data_parse(buf, data_cb, name);
 
 	if (strcmp(name, CONFIG_BT_DEVICE_NAME)) {
+		printk("Wrong name %s\n", name);
 		return;
 	}
 
 	for (uint8_t i = 0; i < sid_count; i++) {
 		if (sid[i] == info->sid) {
+			printk("Received SID %d\n", info->sid);
 			return;
 		}
 	}
@@ -113,6 +120,7 @@ static void scan_recv(const struct bt_le_scan_recv_info *info,
 	sid[sid_count++] = info->sid;
 
 	if (sid_count < CONFIG_BT_EXT_ADV_MAX_ADV_SET) {
+		printk("Received advertising sets: %d\n", sid_count);
 		return;
 	}
 
