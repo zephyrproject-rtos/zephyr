46e2b8baea6fbb5fe5d85294a45de14c441fac3b
Daniel Leung <daniel.leung@intel.com>
2023-07-18 17:44:58 -0700

    xtensa: allow arch-specific arch_spin_relax() with more NOPs
    This adds a Kconfig to introduce the Xtensa specific
    arch_spin_relax() which can do more NOPs. Some Xtensa SoCs
    may need more NOPs after failure to lock a spinlock,
    especially under SMP. This gives the bus extra time to
    propagate the RCW transactions among CPUs.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- arch/xtensa/core/xtensa-asm2.c
+++ arch/xtensa/core/xtensa-asm2.c
@@ -455,3 +455,16 @@ int z_xtensa_irq_is_enabled(unsigned int irq)
 
 	return (ie & (1 << irq)) != 0U;
 }
+
+#ifdef CONFIG_XTENSA_MORE_SPIN_RELAX_NOPS
+/* Some compilers might "optimize out" (i.e. remove) continuous NOPs.
+ * So force no optimization to avoid that.
+ */
+__no_optimization
+void arch_spin_relax(void)
+{
+#define NOP1(_, __) __asm__ volatile("nop.n;");
+	LISTIFY(CONFIG_XTENSA_NUM_SPIN_RELAX_NOPS, NOP1, (;))
+#undef NOP1
+}
+#endif /* CONFIG_XTENSA_MORE_SPIN_RELAX_NOPS */
