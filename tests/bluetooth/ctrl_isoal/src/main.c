cd59d4f291540089700dc1c04e7dd82f8de345a5
Nirosharn Amarasinghe <niag@demant.com>
2023-07-19 12:20:42 +0200

    [nrf fromtree] tests: bluetooth: controller: ISO-AL selection of TX time stamps
    Unit tests for TX time stamp related decisions.
    
    Signed-off-by: Nirosharn Amarasinghe <niag@demant.com>
    (cherry picked from commit 2260b651b65be683fc1e82aafeef166d97dde64a)
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>
    (cherry picked from commit e95ce690ab6b5789f47871e98e549026143790d2)

--- tests/bluetooth/ctrl_isoal/src/main.c
+++ tests/bluetooth/ctrl_isoal/src/main.c
@@ -37,7 +37,7 @@ DEFINE_FFF_GLOBALS;
 
 ZTEST_SUITE(test_rx_basics, NULL, NULL, isoal_test_rx_common_before, NULL, NULL);
 ZTEST_SUITE(test_rx_unframed, NULL, NULL, isoal_test_rx_common_before, NULL, NULL);
-ZTEST_SUITE(test_rx_framed, NULL, NULL, NULL, isoal_test_rx_common_before, NULL);
+ZTEST_SUITE(test_rx_framed, NULL, NULL, isoal_test_rx_common_before, NULL, NULL);
 
 ZTEST_SUITE(test_tx_basics, NULL, NULL, isoal_test_tx_common_before, NULL, NULL);
 ZTEST_SUITE(test_tx_unframed, NULL, NULL, isoal_test_tx_common_before, NULL, NULL);
