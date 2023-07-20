900214ea32df803e2fc7295829b62ecafe937a70
Grzegorz Chwierut <grzegorz.chwierut@nordicsemi.no>
2023-07-20 17:30:30 +0200

    tests: pytest: Fix for pytest warning
    PytestAssertRewriteWarning is raised when pytest-twister-harness
    plugin is called from twister. Call register method to get rid
    of that warning
    
    Signed-off-by: Grzegorz Chwierut <grzegorz.chwierut@nordicsemi.no>

--- scripts/pylib/pytest-twister-harness/src/twister_harness/fixtures/__init__.py
+++ scripts/pylib/pytest-twister-harness/src/twister_harness/fixtures/__init__.py
@@ -1,3 +1,8 @@
 # Copyright (c) 2023 Nordic Semiconductor ASA
 #
 # SPDX-License-Identifier: Apache-2.0
+
+import pytest
+
+pytest.register_assert_rewrite('twister_harness.fixtures.dut')
+pytest.register_assert_rewrite('twister_harness.fixtures.mcumgr')
