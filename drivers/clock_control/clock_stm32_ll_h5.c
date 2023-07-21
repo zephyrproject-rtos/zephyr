305423ccb81821c83a616933d4d96a303ca2abbe
Kacper Dalach <dalachowsky@gmail.com>
2023-07-21 08:32:24 +0200

    drivers: clock_control: stm32h5: Add MCO configuration
    STM32H5 series lacked support for MCO configuration.
    Added SOC_SERIES_STM32H5X to approperiate kconfig MCO source
    configurations. Added new MCO sources from H5 series and
    updated the clock_stm32_ll_h5.c with MCO configuration.
    
    Signed-off-by: Kacper Dalach <dalachowsky@gmail.com>

--- drivers/clock_control/clock_stm32_ll_h5.c
+++ drivers/clock_control/clock_stm32_ll_h5.c
@@ -18,6 +18,7 @@
 #include <zephyr/drivers/clock_control.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/drivers/clock_control/stm32_clock_control.h>
+#include "clock_stm32_ll_mco.h"
 
 /* Macros to fill up prescaler values */
 #define z_hsi_divider(v) LL_RCC_HSI_DIV_ ## v
@@ -751,6 +752,9 @@ int stm32_clock_control_init(const struct device *dev)
 
 	/* Update CMSIS variable */
 	SystemCoreClock = CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC;
+
+	/* configure MCO1/MCO2 based on Kconfig */
+	stm32_clock_control_mco_init();
 
 	return 0;
 }
