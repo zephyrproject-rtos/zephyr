ce07834aec81645e5b2448b833eab03d601156f2
Thomas Ebert Hansen <thoh@oticon.com>
2023-07-21 16:14:15 +0200

    tests: Bluetooth: Test invalid enc. pause
    Add test to verify that
    1) A peripheral role cannot initiate an encryption pause
    2) A peripheral role rejects a LL_PAUSE_ENC_REQ while a CIS is
       established
    
    Signed-off-by: Thomas Ebert Hansen <thoh@oticon.com>

--- tests/bluetooth/controller/mock_ctrl/src/ull_conn_iso.c
+++ tests/bluetooth/controller/mock_ctrl/src/ull_conn_iso.c
@@ -44,8 +44,8 @@
 static struct ll_conn_iso_group cig = { 0 };
 static struct ll_conn_iso_stream cis = { 0 };
 
-struct ll_conn_iso_stream *ll_conn_iso_stream_get_by_acl(struct ll_conn *conn,
-							 uint16_t *cis_iter)
+__weak struct ll_conn_iso_stream *ll_conn_iso_stream_get_by_acl(struct ll_conn *conn,
+								uint16_t *cis_iter)
 {
 	return NULL;
 }
