8f03ca5f2feeae0366f75fdaf338de57b5fbca90
Daniel Gaston Ochoa <dgastonochoa@gmail.com>
2023-07-21 20:00:43 +0100

    Simple STM32 SPI driver unit test
    Signed-off-by: Daniel Gaston Ochoa <dgastonochoa@gmail.com>

--- tests/drivers/spi/spi_stm32/src/main.c
+++ tests/drivers/spi/spi_stm32/src/main.c
@@ -6,10 +6,64 @@
 
 #include <zephyr/types.h>
 #include <zephyr/ztest.h>
+#include <zephyr/fff.h>
+#include <zephyr/drivers/spi.h>
 
-ZTEST(spi_stm32, test_one)
+#include "stm32_spi_iface.h"
+#include "spi_ll_stm32.h"
+
+#include "mocks/stm32_spi_iface_mocks.h"
+
+#include "spi_setup.h"
+
+DEFINE_FFF_GLOBALS;
+
+extern int spi_stm32_transceive(const struct device *dev,
+                                const struct spi_config *config,
+                                const struct spi_buf_set *tx_bufs,
+                                const struct spi_buf_set *rx_bufs);
+
+static void reset_fakes(void* args)
 {
-	zassert_true(1);
+    RESET_FAKE(ll_func_is_active_master_transfer);
+    FFF_RESET_HISTORY();
 }
 
-ZTEST_SUITE(spi_stm32, NULL, NULL, NULL, NULL, NULL);
+static void setup_test_case(void* args)
+{
+    // These mocks must always return true or the transceive function will
+    // loop for ever waiting for them.
+    ll_func_is_active_master_transfer_fake.return_val = true;
+    ll_func_tx_is_empty_fake.return_val = true;
+    ll_func_rx_is_not_empty_fake.return_val = true;
+}
+
+static void* setup_test_suite(void)
+{
+    reset_fakes(NULL);
+    return NULL;
+}
+
+ZTEST_SUITE(test_spi_common,
+            NULL,
+            setup_test_suite,
+            setup_test_case,
+            reset_fakes,
+            NULL);
+
+
+ZTEST(test_spi_common, test_transceive_can_set_cpol0)
+{
+    spi_setup_t sps = spi_setup_create(1);
+    sps.cfg.operation &= ~SPI_MODE_CPOL;
+
+    spi_stm32_transceive(&sps.spi, &sps.cfg, &sps.tx_bufs, &sps.rx_bufs);
+
+    zassert_equal(ll_func_set_polarity_fake.call_count, 1, NULL);
+    zassert_equal_ptr(ll_func_set_polarity_fake.arg0_val,
+                      spi_setup_get_native_dev(&sps),
+                      NULL);
+    zassert_equal(ll_func_set_polarity_fake.arg1_val, STM32_SPI_CPOL_0, NULL);
+
+    spi_setup_free(&sps);
+}
