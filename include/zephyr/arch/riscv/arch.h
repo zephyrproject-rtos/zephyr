84b86d9b0cc9e3768bdbbe8ee042b795d3f969fb
Yong Cong Sin <ycsin@meta.com>
2023-07-25 17:50:40 +0800

    soc: riscv: Add ability to use custom sys_io functions
    Add Kconfig RISCV_SOC_HAS_CUSTOM_SYS_IO symbol so that a riscv
    SoC can set to specify that it has a custom implementation for
    sys_io functions.
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>

--- include/zephyr/arch/riscv/arch.h
+++ include/zephyr/arch/riscv/arch.h
@@ -18,8 +18,8 @@
 #include <zephyr/arch/riscv/thread.h>
 #include <zephyr/arch/riscv/exp.h>
 #include <zephyr/arch/riscv/irq.h>
+#include <zephyr/arch/riscv/sys_io.h>
 #include <zephyr/arch/common/sys_bitops.h>
-#include <zephyr/arch/common/sys_io.h>
 #include <zephyr/arch/common/ffs.h>
 #if defined(CONFIG_USERSPACE)
 #include <zephyr/arch/riscv/syscall.h>
