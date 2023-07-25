1f34a1241d92ab90513316ecd0c75d93d14ea9fd
Christopher Friedt <cfriedt@meta.com>
2023-07-24 20:51:00 -0400

    libc: common: add support for iso c11 threads
    This change capitalizes on newly added support for dynamic
    thread stacks and the existing pthread support to provide
    an implementation of the ISO C11 `<threads.h>` API.
    
    Signed-off-by: Christopher Friedt <cfriedt@meta.com>

---
+++ lib/libc/common/include/machine/_threads.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023, Meta
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_LIB_LIBC_COMMON_INCLUDE_MACHINE__THREADS_H_
+#define ZEPHYR_LIB_LIBC_COMMON_INCLUDE_MACHINE__THREADS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef int thrd_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_LIB_LIBC_COMMON_INCLUDE_MACHINE__THREADS_H_ */
