3925f1868353a2d263ea39669088acb53d322345
Fabio Baltieri <fabiobaltieri@google.com>
2023-07-27 16:04:45 +0000

    runners: harmonize misc-flasher template with the other runners
    Make the misc-flasher template work like all the other runners:
    
    - use board_set_flasher_ifnset so that it's used automatically if
      specified first
    - rename the file to match the runner name so one could use something
      like:
    
    board_runner_args(misc-flasher "...")
    include(${ZEPHYR_BASE}/boards/common/misc-flasher.board.cmake)
    
    Keep the old file for compatibility with out of tree boards.
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

---
+++ boards/common/misc-flasher.board.cmake
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# Please use this sparingly and only when if your setup is exotic and
+# you're willing to handle requests for help. E.g. your "board" is a
+# core on a special-purpose SoC which requires a complicated script to
+# network boot.
+
+board_set_flasher_ifnset(misc-flasher)
+board_finalize_runner_args(misc-flasher)
