a7cf7eb3936baef93472cf72171c87f83a726b33
Jonathan Rico <jonathan.rico@nordicsemi.no>
2023-07-27 11:21:04 +0200

    Bluetooth: l2cap: change `connected` cb call condition for ECRED
    This was recently refactored (in #58440). But it introduced a bug in which
    some channels were connected but not the whole list asked for by the peer.
    
    In that case, `result` will not be `SUCCESS` but we still want to call the
    `connected` callback as the peer will consider those channels to be
    connected when we send the response.
    
    The symptom is that EATT channels are being instantiated, but not
    considered connected (ie. usable by the stack).
    
    Also introduce a test that has asymmetric channel resources (5 on central
    vs 2 on peripheral) to reproduce the bug.
    
    Fixes #60212
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- tests/bsim/bluetooth/host/att/eatt/src/main.c
+++ tests/bsim/bluetooth/host/att/eatt/src/main.c
@@ -9,11 +9,13 @@
 extern struct bst_test_list *test_main_collision_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_main_autoconnect_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_main_reconfigure_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_main_lowres_install(struct bst_test_list *tests);
 
 bst_test_install_t test_installers[] = {
 	test_main_collision_install,
 	test_main_autoconnect_install,
 	test_main_reconfigure_install,
+	test_main_lowres_install,
 	NULL,
 };
 
---
+++ tests/bsim/bluetooth/host/att/eatt/tests_scripts/lowres.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Copyright (c) 2023 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
+
+simulation_id="lowres"
+verbosity_level=2
+EXECUTE_TIMEOUT=20
+
+cd ${BSIM_OUT_PATH}/bin
+
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_host_att_eatt_prj_autoconnect_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=0 -testid=central_lowres
+
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_host_att_eatt_prj_lowres_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=1 -testid=peripheral_lowres
+
+Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
+  -D=2 -sim_length=200e6 $@
+
+wait_for_background_jobs
