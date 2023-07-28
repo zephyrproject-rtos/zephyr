f7d0dbaf406140f63fbee225bcc1cc9eba9d6e57
Gerard Marull-Paretas <gerard@teslabs.com>
2023-07-28 15:17:19 +0200

    soc: arm: rt6xx: implement custom reboot hook
    It looks like extra magic is required to reboot on rt6xx SoCs. Right
    now, this was part of board code, let's move it to a custom hook in SoC
    code.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

---
+++ soc/arm/nxp_imx/rt6xx/reboot.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023 Nordic Semiconductor ASA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/sys/reboot.h>
+#include <zephyr/arch/arm/aarch32/cortex_m/cmsis.h>
+#include <zephyr/toolchain.h>
+
+#include <fsl_common.h>
+
+void z_sys_reboot(enum sys_reboot_type type)
+{
+	ARG_UNUSED(type);
+
+	/*
+	 * On the RT685, the warm reset will not complete correctly unless the
+	 * ROM toggles the flash reset pin. We can control this behavior using
+	 * the OTP shadow register for OPT word BOOT_CFG1
+	 *
+	 * Set FLEXSPI_RESET_PIN_ENABLE=1, FLEXSPI_RESET_PIN= PIO2_12
+	 */
+	OCOTP->OTP_SHADOW[97] = 0x314000;
+
+	NVIC_SystemReset();
+
+	CODE_UNREACHABLE;
+}
