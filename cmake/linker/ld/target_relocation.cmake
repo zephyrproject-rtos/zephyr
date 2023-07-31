623fb0ee8137e60452d2dcf3fc8826de22c0302c
Carles Cufi <carles.cufi@nordicsemi.no>
2023-07-31 16:30:12 +0200

    build: relocation: Fix long command-line invocations
    For applications relocating big parts of the code with many sections,
    builds were failing on Windows due to hitting the max command-line
    length on that platform.
    Fix this by using a file to store the dictionary passed to the python
    script.
    
    Fixes https://github.com/zephyrproject-rtos/zephyr/issues/60994.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- cmake/linker/ld/target_relocation.cmake
+++ cmake/linker/ld/target_relocation.cmake
@@ -10,6 +10,14 @@ macro(toolchain_ld_relocation)
        "${PROJECT_BINARY_DIR}/include/generated/linker_sram_bss_relocate.ld")
   set(MEM_RELOCATION_CODE "${PROJECT_BINARY_DIR}/code_relocation.c")
   set(MEM_REGION_DEFAULT_RAM RAM)
+  set(DICT_FILE "${PROJECT_BINARY_DIR}/relocation_dict.txt")
+
+  file(GENERATE
+  OUTPUT
+    ${DICT_FILE}
+  CONTENT
+    $<TARGET_PROPERTY:code_data_relocation_target,COMPILE_DEFINITIONS>
+  )
 
   add_custom_command(
     OUTPUT ${MEM_RELOCATION_CODE} ${MEM_RELOCATION_LD}
@@ -18,7 +26,7 @@ macro(toolchain_ld_relocation)
     ${ZEPHYR_BASE}/scripts/build/gen_relocate_app.py
     $<$<BOOL:${CMAKE_VERBOSE_MAKEFILE}>:--verbose>
     -d ${APPLICATION_BINARY_DIR}
-    -i \"$<TARGET_PROPERTY:code_data_relocation_target,COMPILE_DEFINITIONS>\"
+    -i ${DICT_FILE}
     -o ${MEM_RELOCATION_LD}
     -s ${MEM_RELOCATION_SRAM_DATA_LD}
     -b ${MEM_RELOCATION_SRAM_BSS_LD}
