42dc26038dd65ee1795704145caa113df88895fe
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2023-08-02 16:58:01 +0800

    ITE: drivers/i2c/target: Disable the timeout setting
    Disable the timeout setting when both the clock and data are
    in a low state. This allows for I2C host clock stretching
    without a timeout limit.
    
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>

--- soc/riscv/riscv-ite/common/chip_chipregs.h
+++ soc/riscv/riscv-ite/common/chip_chipregs.h
@@ -1347,6 +1347,9 @@ enum chip_pll_mode {
 /* 0x13: Nack Status */
 #define IT8XXX2_I2C_NST_CNS           BIT(7)
 #define IT8XXX2_I2C_NST_ID_NACK       BIT(3)
+/* 0x18: Timeout and Arbiter Status */
+#define IT8XXX2_I2C_SCL_TIMEOUT_EN    BIT(7)
+#define IT8XXX2_I2C_SDA_TIMEOUT_EN    BIT(6)
 /* 0x19: Error Status */
 #define IT8XXX2_I2C_ERR_ST_DEV1_EIRQ  BIT(0)
 /* 0x1B: Finish Status */
