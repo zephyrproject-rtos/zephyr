260737f47913dec6e596c7468e421131ce41dea2
Piotr Golyzniak <piotr.golyzniak@nordicsemi.no>
2023-08-04 12:40:19 +0200

    scripts: pytest: rename SimulatorAdapterBase
    Change name of SimulatorAdapterBase class to more meaningful like
    BinaryAdapterBase. This changes is also made to be more align with
    Twister terminology.
    
    Signed-off-by: Piotr Golyzniak <piotr.golyzniak@nordicsemi.no>

--- scripts/pylib/pytest-twister-harness/src/twister_harness/device/factory.py
+++ scripts/pylib/pytest-twister-harness/src/twister_harness/device/factory.py
@@ -10,7 +10,7 @@ from typing import Type
 from twister_harness.device.device_adapter import DeviceAdapter
 from twister_harness.device.hardware_adapter import HardwareAdapter
 from twister_harness.device.qemu_adapter import QemuAdapter
-from twister_harness.device.simulator_adapter import (
+from twister_harness.device.binary_adapter import (
     CustomSimulatorAdapter,
     NativeSimulatorAdapter,
     UnitSimulatorAdapter,
