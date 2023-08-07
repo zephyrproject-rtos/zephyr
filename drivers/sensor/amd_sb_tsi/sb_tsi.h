bcac61fb828a0f89bf7c23aa64bf0c628f819eee
Paweł Anikiel <pan@semihalf.com>
2023-08-07 12:06:30 +0000

    drivers: sensor: Add driver for SB-TSI
    Add a driver for the SB Temperature Sensor Interface. This is an I2C
    temperature sensor on AMD SoCs.
    
    Signed-off-by: Paweł Anikiel <pan@semihalf.com>

---
+++ drivers/sensor/amd_sb_tsi/sb_tsi.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023 Google LLC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_SENSOR_SB_TSI_SB_TSI_H_
+#define ZEPHYR_DRIVERS_SENSOR_SB_TSI_SB_TSI_H_
+
+#define SB_TSI_TEMP_INT 0x01
+#define SB_TSI_TEMP_DEC 0x10
+#define SB_TSI_TEMP_DEC_SHIFT 5
+#define SB_TSI_TEMP_DEC_SCALE 8
+
+#endif
