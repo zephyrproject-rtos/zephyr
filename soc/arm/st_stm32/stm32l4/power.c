4eaad9d33b707042fb4f68731763f392c226319b
Gerard Marull-Paretas <gerard@teslabs.com>
2023-08-07 17:52:47 +0200

    soc: arm: st_stm32: l4: add support for sys_poweroff
    Convert SOFT_OFF handler to sys_poweroff hook.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- soc/arm/st_stm32/stm32l4/power.c
+++ soc/arm/st_stm32/stm32l4/power.c
@@ -27,19 +27,6 @@ LOG_MODULE_DECLARE(soc, CONFIG_SOC_LOG_LEVEL);
 #define RCC_STOP_WAKEUPCLOCK_SELECTED LL_RCC_STOP_WAKEUPCLOCK_HSI
 #endif
 
-void enter_ultra_low_power_mode(void)
-{
-	/* Configure CPU core */
-	/* Enable CPU deep sleep mode */
-	LL_LPM_EnableDeepSleep();
-	LL_DBGMCU_DisableDBGStandbyMode();
-	/* Enter ultra_low-power mode */
-
-	for (;;) {
-		k_cpu_idle();
-	}
-}
-
 void set_mode_stop(uint8_t substate_id)
 {
 	/* ensure the proper wake-up system clock */
@@ -67,20 +54,6 @@ void set_mode_stop(uint8_t substate_id)
 	}
 }
 
-void set_mode_standby(void)
-{
-	/* Select standby mode */
-	LL_PWR_SetPowerMode(LL_PWR_MODE_STANDBY);
-	enter_ultra_low_power_mode();
-}
-
-void set_mode_shutdown(void)
-{
-	/* Select shutdown mode */
-	LL_PWR_SetPowerMode(LL_PWR_MODE_SHUTDOWN);
-	enter_ultra_low_power_mode();
-}
-
 /* Invoke Low Power/System Off specific Tasks */
 void pm_state_set(enum pm_state state, uint8_t substate_id)
 {
@@ -93,10 +66,10 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 		k_cpu_idle();
 		break;
 	case PM_STATE_STANDBY:
-		set_mode_standby();
-		break;
-	case PM_STATE_SOFT_OFF:
-		set_mode_shutdown();
+		LL_PWR_SetPowerMode(LL_PWR_MODE_STANDBY);
+		LL_LPM_EnableDeepSleep();
+		LL_DBGMCU_DisableDBGStandbyMode();
+		k_cpu_idle();
 		break;
 	default:
 		LOG_DBG("Unsupported power state %u", state);
@@ -128,9 +101,6 @@ void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
 		irq_unlock(0);
 		break;
 	case PM_STATE_STANDBY:
-		__fallthrough;
-	case PM_STATE_SOFT_OFF:
-		/* We should not get there */
 		__fallthrough;
 	case PM_STATE_SUSPEND_TO_RAM:
 		__fallthrough;
