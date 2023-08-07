0317b4cfa54e8b706d9750e49e3d3988f332c71b
Anas Nashif <anas.nashif@intel.com>
2023-08-07 14:34:19 +0000

    sys_io: rename z_soc_sys_ -> platform_sysio_
    Move from private hook names to internal using a common prefix for
    platform hooks.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- include/zephyr/arch/common/sys_io.h
+++ include/zephyr/arch/common/sys_io.h
@@ -65,53 +65,53 @@ static ALWAYS_INLINE void sys_write64(uint64_t data, mem_addr_t addr)
 
 #else
 
-extern uint8_t z_soc_sys_read8(mem_addr_t addr);
-extern void z_soc_sys_write8(uint8_t data, mem_addr_t addr);
-extern uint16_t z_soc_sys_read16(mem_addr_t addr);
-extern void z_soc_sys_write16(uint16_t data, mem_addr_t addr);
-extern uint32_t z_soc_sys_read32(mem_addr_t addr);
-extern void z_soc_sys_write32(uint32_t data, mem_addr_t addr);
-extern uint64_t z_soc_sys_read64(mem_addr_t addr);
-extern void z_soc_sys_write64(uint64_t data, mem_addr_t addr);
+extern uint8_t platform_sysio_read8(mem_addr_t addr);
+extern void platform_sysio_write8(uint8_t data, mem_addr_t addr);
+extern uint16_t platform_sysio_read16(mem_addr_t addr);
+extern void platform_sysio_write16(uint16_t data, mem_addr_t addr);
+extern uint32_t platform_sysio_read32(mem_addr_t addr);
+extern void platform_sysio_write32(uint32_t data, mem_addr_t addr);
+extern uint64_t platform_sysio_read64(mem_addr_t addr);
+extern void platform_sysio_write64(uint64_t data, mem_addr_t addr);
 
 static ALWAYS_INLINE uint8_t sys_read8(mem_addr_t addr)
 {
-	return z_soc_sys_read8(addr);
+	return platform_sysio_read8(addr);
 }
 
 static ALWAYS_INLINE void sys_write8(uint8_t data, mem_addr_t addr)
 {
-	return z_soc_sys_write8(data, addr);
+	return platform_sysio_write8(data, addr);
 }
 
 static ALWAYS_INLINE uint16_t sys_read16(mem_addr_t addr)
 {
-	return z_soc_sys_read16(addr);
+	return platform_sysio_read16(addr);
 }
 
 static ALWAYS_INLINE void sys_write16(uint16_t data, mem_addr_t addr)
 {
-	return z_soc_sys_write16(data, addr);
+	return platform_sysio_write16(data, addr);
 }
 
 static ALWAYS_INLINE uint32_t sys_read32(mem_addr_t addr)
 {
-	return z_soc_sys_read32(addr);
+	return platform_sysio_read32(addr);
 }
 
 static ALWAYS_INLINE void sys_write32(uint32_t data, mem_addr_t addr)
 {
-	return z_soc_sys_write32(data, addr);
+	return platform_sysio_write32(data, addr);
 }
 
 static ALWAYS_INLINE uint64_t sys_read64(mem_addr_t addr)
 {
-	return z_soc_sys_read64(addr);
+	return platform_sysio_read64(addr);
 }
 
 static ALWAYS_INLINE void sys_write64(uint64_t data, mem_addr_t addr)
 {
-	return z_soc_sys_write64(data, addr);
+	return platform_sysio_write64(data, addr);
 }
 
 #endif /* CONFIG_PLATFORM_HAS_CUSTOM_SYS_IO */
