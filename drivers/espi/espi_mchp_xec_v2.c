efc30638aa6f550f823863816a030afd495b26ce
Flavio Ceolin <flavio.ceolin@intel.com>
2023-08-06 23:30:29 -0700

    espi: mchp_xec_v2: Fix possible buffer overflow
    Check the packet lenght in flash_write operation beforeSigned-off-by
    copying it to an internal buffer.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- drivers/espi/espi_mchp_xec_v2.c
+++ drivers/espi/espi_mchp_xec_v2.c
@@ -524,6 +524,11 @@ static int espi_xec_flash_write(const struct device *dev,
 
 	LOG_DBG("%s", __func__);
 
+	if (sizeof(target_mem) < pckt->len) {
+		LOG_ERR("Packet length is too big");
+		return -ENOMEM;
+	}
+
 	if (!(regs->FCSTS & MCHP_ESPI_FC_STS_CHAN_EN)) {
 		LOG_ERR("Flash channel is disabled");
 		return -EIO;
