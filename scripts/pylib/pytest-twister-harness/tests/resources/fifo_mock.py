20dccb773cb993a0410df14881d142bcbad02d8f
Piotr Golyzniak <piotr.golyzniak@nordicsemi.no>
2023-08-07 11:57:54 +0200

    scripts: pytest: add Shell helper unit tests
    Add unit tests dedicated for Shell helper sample class.
    Add shell_simulator_script.py script to simulate shell application
    behavior.
    
    Signed-off-by: Piotr Golyzniak <piotr.golyzniak@nordicsemi.no>

--- scripts/pylib/pytest-twister-harness/tests/resources/fifo_mock.py
+++ scripts/pylib/pytest-twister-harness/tests/resources/fifo_mock.py
@@ -9,29 +9,7 @@ import threading
 import time
 from argparse import ArgumentParser
 
-content = """
-The Zen of Python, by Tim Peters
-
-Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested.
-Sparse is better than dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
-Although never is often better than *right* now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
-"""
+from zen_of_python import zen_of_python
 
 
 class FifoFile:
@@ -90,7 +68,7 @@ def main():
     logger.info('Start')
 
     with FifoFile(write_path, 'wb') as wf, FifoFile(read_path, 'rb'):
-        for line in content.split('\n'):
+        for line in zen_of_python:
             wf.write(f'{line}\n'.encode('utf-8'))
     time.sleep(1)  # give a moment for external programs to collect all outputs
     return 0
