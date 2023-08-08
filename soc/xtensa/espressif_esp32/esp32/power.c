42c8067b6f851135ec12ee14f150e172dbc57ca9
Gerard Marull-Paretas <gerard@teslabs.com>
2023-08-08 10:29:38 +0200

    soc: riscv|xtensa: esp32: add support for sys_poweroff
    Implement the sys_poweroff hook for all ESP32 SoCs based on the previous
    SOFT_OFF implementation.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- soc/xtensa/espressif_esp32/esp32/power.c
+++ soc/xtensa/espressif_esp32/esp32/power.c
@@ -21,17 +21,6 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 	ARG_UNUSED(substate_id);
 
 	switch (state) {
-	case PM_STATE_SOFT_OFF:
-		/*
-		 * Isolate GPIO12 pin from external circuits. This is needed for modules
-		 * which have an external pull-up resistor on GPIO12 (such as ESP32-WROVER)
-		 * to minimize current consumption.
-		 */
-		rtcio_hal_isolate(RTCIO_GPIO12_CHANNEL);
-		/* Forces RTC domain to be always on */
-		esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
-		esp_deep_sleep_start();
-		break;
 	case PM_STATE_STANDBY:
 		intenable = XTENSA_RSR("INTENABLE");
 		__asm__ volatile ("waiti 0");
@@ -48,9 +37,6 @@ void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
 	ARG_UNUSED(substate_id);
 
 	switch (state) {
-	case PM_STATE_SOFT_OFF:
-		/* Nothing to do. */
-		break;
 	case PM_STATE_STANDBY:
 		z_xt_ints_on(intenable);
 		esp_light_sleep_start();
