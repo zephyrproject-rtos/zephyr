6c5531ab23bac62ee5b1f5cd9aa492f605690968
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-08-09 17:29:19 +0200

    COVERAGE: Fix COVERAGE_GCOV dependencies
    CONFIG_COVERAGE has been incorrectly used to
    change other kconfig options (stack sizes, etc)
    code defaults, as well as some samples behaviour,
    which should not have dependend on it.
    
    Instead those should have depended on COVERAGE_GCOV,
    which, being the one which adds special code and
    temporary RAM storage for embedded targets,
    require changes to many features.
    
    When building for the native targets, all this was
    unnecessary.
    
    => Fix the dependency.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- samples/net/sockets/echo_server/src/common.h
+++ samples/net/sockets/echo_server/src/common.h
@@ -8,7 +8,7 @@
 
 #define MY_PORT 4242
 #if defined(CONFIG_NET_SOCKETS_SOCKOPT_TLS) || defined(CONFIG_NET_TCP) || \
-	defined(CONFIG_COVERAGE)
+	defined(CONFIG_COVERAGE_GCOV)
 #define STACK_SIZE 4096
 #else
 #define STACK_SIZE 2048
