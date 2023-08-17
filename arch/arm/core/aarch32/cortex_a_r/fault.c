7265a97bc4a73cbedfc4e7b573ab259ea21a7432
Manuel Argüelles <manuel.arguelles@nxp.com>
2023-08-17 12:24:20 +0700

    arm: cortex_r: support recoverable data abort
    Add support for processing the Fault Status Registers and recoverable
    data abort for Armv8-R AArch32.
    
    Based on Arm Architecture Reference Manual Supplement Armv8, for the
    Armv8-R AArch32 architecture profile (ARM DDI 0568).
    
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

--- arch/arm/core/aarch32/cortex_a_r/fault.c
+++ arch/arm/core/aarch32/cortex_a_r/fault.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2020 Stephanos Ioannidis <root@stephanos.io>
  * Copyright (c) 2018 Lexmark International, Inc.
+ * Copyright 2023 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -52,7 +53,7 @@ static uint32_t dump_fault(uint32_t status, uint32_t addr)
 {
 	uint32_t reason = K_ERR_CPU_EXCEPTION;
 	/*
-	 * Dump fault status and, if applicable, tatus-specific information.
+	 * Dump fault status and, if applicable, status-specific information.
 	 * Note that the fault address is only displayed for the synchronous
 	 * faults because it is unpredictable for asynchronous faults.
 	 */
@@ -60,10 +61,6 @@ static uint32_t dump_fault(uint32_t status, uint32_t addr)
 	case FSR_FS_ALIGNMENT_FAULT:
 		reason = K_ERR_ARM_ALIGNMENT_FAULT;
 		LOG_ERR("Alignment Fault @ 0x%08x", addr);
-		break;
-	case FSR_FS_BACKGROUND_FAULT:
-		reason = K_ERR_ARM_BACKGROUND_FAULT;
-		LOG_ERR("Background Fault @ 0x%08x", addr);
 		break;
 	case FSR_FS_PERMISSION_FAULT:
 		reason = K_ERR_ARM_PERMISSION_FAULT;
@@ -89,6 +86,21 @@ static uint32_t dump_fault(uint32_t status, uint32_t addr)
 		reason = K_ERR_ARM_DEBUG_EVENT;
 		dump_debug_event();
 		break;
+#if defined(CONFIG_AARCH32_ARMV8_R)
+	case FSR_FS_TRANSLATION_FAULT:
+		reason = K_ERR_ARM_TRANSLATION_FAULT;
+		LOG_ERR("Translation Fault @ 0x%08x", addr);
+		break;
+	case FSR_FS_UNSUPPORTED_EXCLUSIVE_ACCESS_FAULT:
+		reason = K_ERR_ARM_UNSUPPORTED_EXCLUSIVE_ACCESS_FAULT;
+		LOG_ERR("Unsupported Exclusive Access Fault @ 0x%08x", addr);
+		break;
+#else
+	case FSR_FS_BACKGROUND_FAULT:
+		reason = K_ERR_ARM_BACKGROUND_FAULT;
+		LOG_ERR("Background Fault @ 0x%08x", addr);
+		break;
+#endif
 	default:
 		LOG_ERR("Unknown (%u)", status);
 	}
@@ -226,7 +238,11 @@ bool z_arm_fault_prefetch(z_arch_esf_t *esf)
 
 	/* Read and parse Instruction Fault Status Register (IFSR) */
 	uint32_t ifsr = __get_IFSR();
+#if defined(CONFIG_AARCH32_ARMV8_R)
+	uint32_t fs = ifsr & IFSR_STATUS_Msk;
+#else
 	uint32_t fs = ((ifsr & IFSR_FS1_Msk) >> 6) | (ifsr & IFSR_FS0_Msk);
+#endif
 
 	/* Read Instruction Fault Address Register (IFAR) */
 	uint32_t ifar = __get_IFAR();
@@ -289,13 +305,19 @@ bool z_arm_fault_data(z_arch_esf_t *esf)
 
 	/* Read and parse Data Fault Status Register (DFSR) */
 	uint32_t dfsr = __get_DFSR();
+#if defined(CONFIG_AARCH32_ARMV8_R)
+	uint32_t fs = dfsr & DFSR_STATUS_Msk;
+#else
 	uint32_t fs = ((dfsr & DFSR_FS1_Msk) >> 6) | (dfsr & DFSR_FS0_Msk);
+#endif
 
 	/* Read Data Fault Address Register (DFAR) */
 	uint32_t dfar = __get_DFAR();
 
 #if defined(CONFIG_USERSPACE)
-	if ((fs == FSR_FS_BACKGROUND_FAULT)
+	if ((fs == COND_CODE_1(CONFIG_AARCH32_ARMV8_R,
+				(FSR_FS_TRANSLATION_FAULT),
+				(FSR_FS_BACKGROUND_FAULT)))
 			|| (fs == FSR_FS_PERMISSION_FAULT)) {
 		if (memory_fault_recoverable(esf)) {
 			return false;
