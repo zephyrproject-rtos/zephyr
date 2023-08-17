c313d7ba5114fba424f178b9d331422c5585d62e
Anders Storrø <anders.storro@nordicsemi.no>
2023-08-17 13:07:44 +0200

    Bluetooth: Mesh: Add ext timeout in PB for OOB I/O
    Adds separate extended protocol timeout of 120 sec in provisioning
    implementation when OOB method Input or Output is used. This
    complies with recommendation in the mesh 1.1 protocol spec (5.4.4).
    
    Signed-off-by: Anders Storrø <anders.storro@nordicsemi.no>

--- subsys/bluetooth/mesh/pb_gatt.c
+++ subsys/bluetooth/mesh/pb_gatt.c
@@ -95,7 +95,7 @@ int bt_mesh_pb_gatt_recv(struct bt_conn *conn, struct net_buf_simple *buf)
 		return -EINVAL;
 	}
 
-	k_work_reschedule(&link.prot_timer, PROTOCOL_TIMEOUT);
+	k_work_reschedule(&link.prot_timer, bt_mesh_prov_protocol_timeout_get());
 
 	link.cb->recv(&bt_mesh_pb_gatt, link.cb_data, buf);
 
@@ -111,7 +111,7 @@ int bt_mesh_pb_gatt_start(struct bt_conn *conn)
 	}
 
 	link.conn = bt_conn_ref(conn);
-	k_work_reschedule(&link.prot_timer, PROTOCOL_TIMEOUT);
+	k_work_reschedule(&link.prot_timer, bt_mesh_prov_protocol_timeout_get());
 
 	link.cb->link_opened(&bt_mesh_pb_gatt, link.cb_data);
 
@@ -142,7 +142,7 @@ int bt_mesh_pb_gatt_cli_start(struct bt_conn *conn)
 	}
 
 	link.conn = bt_conn_ref(conn);
-	k_work_reschedule(&link.prot_timer, PROTOCOL_TIMEOUT);
+	k_work_reschedule(&link.prot_timer, bt_mesh_prov_protocol_timeout_get());
 
 	return 0;
 }
@@ -218,7 +218,7 @@ static int buf_send(struct net_buf_simple *buf, prov_bearer_send_complete_t cb,
 	link.comp.cb = cb;
 	link.comp.cb_data = cb_data;
 
-	k_work_reschedule(&link.prot_timer, PROTOCOL_TIMEOUT);
+	k_work_reschedule(&link.prot_timer, bt_mesh_prov_protocol_timeout_get());
 
 	return bt_mesh_proxy_msg_send(link.conn, BT_MESH_PROXY_PROV,
 				      buf, buf_send_end, NULL);
