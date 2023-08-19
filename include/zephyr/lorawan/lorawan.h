dbb43834f7afbdf3e98b5ee2f0dd443987e9f010
romain pelletant <romain.pelletant@fullfreqs.com>
2023-08-19 19:26:03 +0200

    lorawan: add devicetime request support
    Add devicetime request support.
    Update lorawan sample
    Related to Issue #55072
    
    Co-authored-by: Jan Kowalewski <jkowalewski@cthings.co>
    
    Signed-off-by: romain pelletant <romain.pelletant@fullfreqs.com>
    Signed-off-by: Jan Kowalewski <jkowalewski@cthings.co>

--- include/zephyr/lorawan/lorawan.h
+++ include/zephyr/lorawan/lorawan.h
@@ -108,6 +108,7 @@ enum lorawan_message_type {
  */
 enum lorawan_dl_flags {
 	LORAWAN_DATA_PENDING = BIT(0),
+	LORAWAN_TIME_UPDATED = BIT(1),
 };
 
 /**
@@ -377,6 +378,32 @@ void lorawan_get_payload_sizes(uint8_t *max_next_payload_size,
  * @return 0 if successful, negative errno otherwise
  */
 int lorawan_set_region(enum lorawan_region region);
+
+/**
+ * @brief Request for time according to DeviceTimeReq MAC cmd
+ *
+ * Append MAC DevTimeReq command. It will be processed on next send
+ * message or force sending empty message to request time immediately.
+ *
+ * @param force_request Immediately send an empty message to execute the request
+ * @return 0 if successful, negative errno otherwise
+ */
+int lorawan_request_device_time(bool force_request);
+
+/**
+ * @brief Retrieve the current time from LoRaWAN stack updated by
+ * DeviceTimeAns on MAC layer.
+ *
+ * This function uses the GPS epoch format, as used in all LoRaWAN services.
+ *
+ * The GPS epoch started on 1980-01-06T00:00:00Z, but has since diverged
+ * from UTC, as it does not consider corrections like leap seconds.
+ *
+ * @param gps_time Synchronized time in GPS epoch format truncated to 32-bit.
+ *
+ * @return 0 if successful, -EAGAIN if the clock is not yet synchronized.
+ */
+int lorawan_device_time_get(uint32_t *gps_time);
 
 #ifdef CONFIG_LORAWAN_APP_CLOCK_SYNC
 
--- samples/subsys/lorawan/class_a/src/main.c
+++ samples/subsys/lorawan/class_a/src/main.c
@@ -27,12 +27,11 @@ LOG_MODULE_REGISTER(lorawan_class_a);
 
 char data[] = {'h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'};
 
-static void dl_callback(uint8_t port, uint8_t flags,
-			int16_t rssi, int8_t snr,
-			uint8_t len, const uint8_t *hex_data)
+static void dl_callback(uint8_t port, uint8_t flags, int16_t rssi, int8_t snr, uint8_t len,
+			const uint8_t *hex_data)
 {
-	LOG_INF("Port %d, Pending %d, RSSI %ddB, SNR %ddBm",
-			port, flags & LORAWAN_DATA_PENDING, rssi, snr);
+	LOG_INF("Port %d, Pending %d, RSSI %ddB, SNR %ddBm, Time %d", port,
+		flags & LORAWAN_DATA_PENDING, rssi, snr, !!(flags & LORAWAN_TIME_UPDATED));
 	if (hex_data) {
 		LOG_HEXDUMP_INF(hex_data, len, "Payload: ");
 	}
