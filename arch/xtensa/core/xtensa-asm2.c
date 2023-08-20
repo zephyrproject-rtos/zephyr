0429fff0d7a1eaa018a012009ecfde7992cf63c8
YuLong Yao <feilongphone@gmail.com>
2023-08-20 16:33:11 +0800

    arch: xtensa: Implement arch_float_enable&disable
    Every arch must have arch_float_enable&disable functions.
    
    Signed-off-by: YuLong Yao <feilongphone@gmail.com>

--- arch/xtensa/core/xtensa-asm2.c
+++ arch/xtensa/core/xtensa-asm2.c
@@ -210,6 +210,20 @@ static inline void *return_to(void *interrupted)
 	return z_arch_get_next_switch_handle(interrupted);
 }
 
+#if defined(CONFIG_FPU) && defined(CONFIG_FPU_SHARING)
+int arch_float_disable(struct k_thread *thread)
+{
+	/* xtensa always has FPU enabled so cannot be disabled */
+	return -ENOTSUP;
+}
+
+int arch_float_enable(struct k_thread *thread, unsigned int options)
+{
+	/* xtensa always has FPU enabled so nothing to do here */
+	return 0;
+}
+#endif /* CONFIG_FPU && CONFIG_FPU_SHARING */
+
 /* The wrapper code lives here instead of in the python script that
  * generates _xtensa_handle_one_int*().  Seems cleaner, still kind of
  * ugly.
