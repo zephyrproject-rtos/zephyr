e79ec0a22e24d34e61f662a811f072814522140c
Serhii Salamakha <serhii.salamakha@gmail.com>
2023-08-21 14:38:54 +0300

    drivers: spi: dtsi: update spi driver for B91 and B92
    Updated spi_b91 driver to spi_b9x for B91 and B92
    
    Signed-off-by: Serhii Salamakha <serhii.salamakha@gmail.com>

--- drivers/spi/spi_b9x.c
+++ drivers/spi/spi_b9x.c
@@ -1,10 +1,14 @@
 /*
- * Copyright (c) 2021 Telink Semiconductor
+ * Copyright (c) 2021-2023 Telink Semiconductor
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#if CONFIG_SOC_RISCV_TELINK_B91
 #define DT_DRV_COMPAT telink_b91_spi
+#elif CONFIG_SOC_RISCV_TELINK_B92
+#define DT_DRV_COMPAT telink_b92_spi
+#endif
 
 /*  Redefine 'spi_read' and 'spi_write' functions names from HAL */
 #define spi_read    hal_spi_read
@@ -29,22 +33,22 @@ LOG_MODULE_REGISTER(spi_telink);
 
 
 /* SPI configuration structure */
-struct spi_b91_cfg {
+struct spi_b9x_cfg {
 	uint8_t peripheral_id;
 	gpio_pin_e cs_pin[CHIP_SELECT_COUNT];
 	const struct pinctrl_dev_config *pcfg;
 };
-#define SPI_CFG(dev)                    ((struct spi_b91_cfg *) ((dev)->config))
+#define SPI_CFG(dev)                    ((struct spi_b9x_cfg *) ((dev)->config))
 
 /* SPI data structure */
-struct spi_b91_data {
+struct spi_b9x_data {
 	struct spi_context ctx;
 };
-#define SPI_DATA(dev)                   ((struct spi_b91_data *) ((dev)->data))
+#define SPI_DATA(dev)                   ((struct spi_b9x_data *) ((dev)->data))
 
 
 /* disable hardware cs flow control */
-static void spi_b91_hw_cs_disable(const struct spi_b91_cfg *config)
+static void spi_b9x_hw_cs_disable(const struct spi_b9x_cfg *config)
 {
 	gpio_pin_e pin;
 
@@ -55,28 +59,34 @@ static void spi_b91_hw_cs_disable(const struct spi_b91_cfg *config)
 
 		/* if CS pin is defined in device tree */
 		if (pin != 0) {
+#if CONFIG_SOC_RISCV_TELINK_B91
 			if (config->peripheral_id == PSPI_MODULE) {
-				/* disable CS pin for PSPI */
 				pspi_cs_pin_dis(pin);
 			} else {
-				/* disable CS pin for MSPI */
 				hspi_cs_pin_dis(pin);
 			}
+#elif CONFIG_SOC_RISCV_TELINK_B92
+			if (config->peripheral_id == LSPI_MODULE) {
+				/* Note: lspi_cs_pin_dis has not added to SPI driver for B92 */
+			} else {
+				gspi_cs_pin_dis(pin);
+			}
+#endif
 		}
 	}
 }
 
 /* config cs flow control: hardware or software */
-static bool spi_b91_config_cs(const struct device *dev,
+static bool spi_b9x_config_cs(const struct device *dev,
 			      const struct spi_config *config)
 {
-	pspi_csn_pin_def_e cs_pin = 0;
-	const struct spi_b91_cfg *b91_config = SPI_CFG(dev);
+	gpio_pin_e cs_pin = 0;
+	const struct spi_b9x_cfg *b9x_config = SPI_CFG(dev);
 
 	/* software flow control */
 	if (spi_cs_is_gpio(config)) {
 		/* disable all hardware CS pins */
-		spi_b91_hw_cs_disable(b91_config);
+		spi_b9x_hw_cs_disable(b9x_config);
 		return true;
 	}
 
@@ -91,7 +101,7 @@ static bool spi_b91_config_cs(const struct device *dev,
 	/* loop through all cs pins: cs0, cs1 and cs2 */
 	for (int cs_id = 0; cs_id < CHIP_SELECT_COUNT; cs_id++) {
 		/* get cs pin defined in device tree */
-		cs_pin = b91_config->cs_pin[cs_id];
+		cs_pin = b9x_config->cs_pin[cs_id];
 
 		/*  if cs pin is not defined for the selected slave, return error */
 		if ((cs_pin == 0) && (cs_id == config->slave)) {
@@ -101,22 +111,38 @@ static bool spi_b91_config_cs(const struct device *dev,
 
 		/* disable cs pin if it is defined and is not requested */
 		if ((cs_pin != 0) && (cs_id != config->slave)) {
-			if (b91_config->peripheral_id == PSPI_MODULE) {
+#if CONFIG_SOC_RISCV_TELINK_B91
+			if (b9x_config->peripheral_id == PSPI_MODULE) {
 				pspi_cs_pin_dis(cs_pin);
 			} else {
 				hspi_cs_pin_dis(cs_pin);
 			}
+#elif CONFIG_SOC_RISCV_TELINK_B92
+			if (b9x_config->peripheral_id == LSPI_MODULE) {
+				/* Note: lspi_cs_pin_dis has not added to SPI driver for B92 */
+			} else {
+				gspi_cs_pin_dis(cs_pin);
+			}
+#endif
 		}
 
 		/* enable cs pin if it is defined and is requested */
-		if ((cs_pin != 0) && (cs_id == config->slave)) {
-			if (b91_config->peripheral_id == PSPI_MODULE) {
-				pspi_set_pin_mux(cs_pin);
+        if ((cs_pin != 0) && (cs_id == config->slave)) {
+#if CONFIG_SOC_RISCV_TELINK_B91
+			if (b9x_config->peripheral_id == PSPI_MODULE) {
 				pspi_cs_pin_en(cs_pin);
 			} else {
-				hspi_set_pin_mux(cs_pin);
 				hspi_cs_pin_en(cs_pin);
 			}
+#elif CONFIG_SOC_RISCV_TELINK_B92
+			if (b9x_config->peripheral_id == LSPI_MODULE) {
+				/* Note: lspi_cs_pin_en has not added to SPI driver for B92,
+				         lspi_cs_pin_en must call lspi_set_pin_mux */
+				lspi_set_pin_mux(cs_pin);
+			} else {
+				gspi_cs_pin_en(cs_pin);
+			}
+#endif
 		}
 	}
 
@@ -124,24 +150,28 @@ static bool spi_b91_config_cs(const struct device *dev,
 }
 
 /* get spi transaction length */
-static uint32_t spi_b91_get_txrx_len(const struct spi_buf_set *tx_bufs,
+static uint32_t spi_b9x_get_txrx_len(const struct spi_buf_set *tx_bufs,
 				     const struct spi_buf_set *rx_bufs)
 {
 	uint32_t len_tx = 0;
 	uint32_t len_rx = 0;
-	const struct spi_buf *tx_buf = tx_bufs->buffers;
-	const struct spi_buf *rx_buf = rx_bufs->buffers;
 
 	/* calculate tx len */
-	for (int i = 0; i < tx_bufs->count; i++) {
-		len_tx += tx_buf->len;
-		tx_buf++;
+	if (tx_bufs) {
+		const struct spi_buf *tx_buf = tx_bufs->buffers;
+		for (int i = 0; i < tx_bufs->count; i++) {
+			len_tx += tx_buf->len;
+			tx_buf++;
+		}
 	}
 
 	/* calculate rx len */
-	for (int i = 0; i < rx_bufs->count; i++) {
-		len_rx += rx_buf->len;
-		rx_buf++;
+	if (rx_bufs) {
+		const struct spi_buf *rx_buf = rx_bufs->buffers;
+		for (int i = 0; i < rx_bufs->count; i++) {
+			len_rx += rx_buf->len;
+			rx_buf++;
+		}
 	}
 
 	return MAX(len_tx, len_rx);
@@ -149,7 +179,7 @@ static uint32_t spi_b91_get_txrx_len(const struct spi_buf_set *tx_bufs,
 
 /* process tx data */
 _attribute_ram_code_sec_
-static void spi_b91_tx(uint8_t peripheral_id, struct spi_context *ctx, uint8_t len)
+static void spi_b9x_tx(uint8_t peripheral_id, struct spi_context *ctx, uint8_t len)
 {
 	uint8_t tx;
 
@@ -160,23 +190,18 @@ static void spi_b91_tx(uint8_t peripheral_id, struct spi_context *ctx, uint8_t l
 			tx = 0;
 		}
 		spi_context_update_tx(ctx, 1, 1);
-		while (reg_spi_fifo_state(peripheral_id) & FLD_SPI_TXF_FULL) {
-		};
-		reg_spi_wr_rd_data(peripheral_id, i % 4) = tx;
+		hal_spi_write(peripheral_id, &tx, 1);
 	}
 }
 
 /* process rx data */
 _attribute_ram_code_sec_
-static void spi_b91_rx(uint8_t peripheral_id, struct spi_context *ctx, uint8_t len)
+static void spi_b9x_rx(uint8_t peripheral_id, struct spi_context *ctx, uint8_t len)
 {
 	uint8_t rx = 0;
 
 	for (int i = 0; i < len; i++) {
-		while (reg_spi_fifo_state(peripheral_id) & FLD_SPI_RXF_EMPTY) {
-		};
-		rx = reg_spi_wr_rd_data(peripheral_id, i % 4);
-
+		hal_spi_read(peripheral_id, &rx, 1);
 		if (spi_context_rx_buf_on(ctx)) {
 			*ctx->rx_buf = rx;
 		}
@@ -186,10 +211,10 @@ static void spi_b91_rx(uint8_t peripheral_id, struct spi_context *ctx, uint8_t l
 
 /* SPI transceive internal */
 _attribute_ram_code_sec_
-static void spi_b91_txrx(const struct device *dev, uint32_t len)
+static void spi_b9x_txrx(const struct device *dev, uint32_t len)
 {
 	unsigned int chunk_size = SPI_WR_RD_CHUNK_SIZE_MAX;
-	struct spi_b91_cfg *cfg = SPI_CFG(dev);
+	struct spi_b9x_cfg *cfg = SPI_CFG(dev);
 	struct spi_context *ctx = &SPI_DATA(dev)->ctx;
 
 	/* prepare SPI module */
@@ -206,26 +231,31 @@ static void spi_b91_txrx(const struct device *dev, uint32_t len)
 		}
 
 		/* write bytes */
-		spi_b91_tx(cfg->peripheral_id, ctx, chunk_size);
+		spi_b9x_tx(cfg->peripheral_id, ctx, chunk_size);
 
 		/* read bytes */
 		if (len <= SPI_WR_RD_CHUNK_SIZE_MAX) {
 			/* read all bytes if len is less than chunk size */
-			spi_b91_rx(cfg->peripheral_id, ctx, chunk_size);
+			spi_b9x_rx(cfg->peripheral_id, ctx, chunk_size);
 		} else if (i == 0) {
 			/* head, read 1 byte less than is sent */
-			spi_b91_rx(cfg->peripheral_id, ctx, chunk_size - 1);
+			spi_b9x_rx(cfg->peripheral_id, ctx, chunk_size - 1);
 		} else if ((len - i) > SPI_WR_RD_CHUNK_SIZE_MAX) {
 			/* body, read so many bytes as is sent*/
-			spi_b91_rx(cfg->peripheral_id, ctx, chunk_size);
+			spi_b9x_rx(cfg->peripheral_id, ctx, chunk_size);
 		} else {
 			/* tail, read the rest bytes */
-			spi_b91_rx(cfg->peripheral_id, ctx, chunk_size + 1);
+			spi_b9x_rx(cfg->peripheral_id, ctx, chunk_size + 1);
 		}
 
 		/* clear TX and RX fifo */
+#if CONFIG_SOC_RISCV_TELINK_B91
 		BM_SET(reg_spi_fifo_state(cfg->peripheral_id), FLD_SPI_TXF_CLR);
 		BM_SET(reg_spi_fifo_state(cfg->peripheral_id), FLD_SPI_RXF_CLR);
+#elif CONFIG_SOC_RISCV_TELINK_B92
+		BM_SET(reg_spi_status(cfg->peripheral_id), FLD_SPI_TXF_CLR_LEVEL);
+		BM_SET(reg_spi_status(cfg->peripheral_id), FLD_SPI_RXF_CLR_LEVEL);
+#endif
 	}
 
 	/* wait fot SPI is ready */
@@ -237,8 +267,8 @@ static void spi_b91_txrx(const struct device *dev, uint32_t len)
 }
 
 /* Check for supported configuration */
-static bool spi_b91_is_config_supported(const struct spi_config *config,
-					struct spi_b91_cfg *b91_config)
+static bool spi_b9x_is_config_supported(const struct spi_config *config,
+					struct spi_b9x_cfg *b9x_config)
 {
 	if (config->operation & SPI_HALF_DUPLEX) {
 		LOG_ERR("Half-duplex not supported");
@@ -274,12 +304,14 @@ static bool spi_b91_is_config_supported(const struct spi_config *config,
 		if ((config->operation & SPI_LINES_MASK) == SPI_LINES_OCTAL) {
 			LOG_ERR("SPI lines Octal is not supported");
 			return false;
-		} else if (((config->operation & SPI_LINES_MASK) ==
-			    SPI_LINES_QUAD) &&
-			   (b91_config->peripheral_id == PSPI_MODULE)) {
+		}
+#if CONFIG_SOC_RISCV_TELINK_B91 
+		else if (((config->operation & SPI_LINES_MASK) == SPI_LINES_QUAD)
+				&& (b9x_config->peripheral_id == PSPI_MODULE)) {
 			LOG_ERR("SPI lines Quad is not supported by PSPI");
 			return false;
 		}
+#endif
 	}
 
 	/* check for slave configuration */
@@ -292,22 +324,26 @@ static bool spi_b91_is_config_supported(const struct spi_config *config,
 }
 
 /* SPI configuration */
-static int spi_b91_config(const struct device *dev,
+static int spi_b9x_config(const struct device *dev,
 			  const struct spi_config *config)
 {
 	int status = 0;
 	spi_mode_type_e mode = SPI_MODE0;
-	struct spi_b91_cfg *b91_config = SPI_CFG(dev);
-	struct spi_b91_data *b91_data = SPI_DATA(dev);
-	uint8_t clk_src = b91_config->peripheral_id == PSPI_MODULE ? sys_clk.pclk : sys_clk.hclk;
+	struct spi_b9x_cfg *b9x_config = SPI_CFG(dev);
+	struct spi_b9x_data *b9x_data = SPI_DATA(dev);
+#if CONFIG_SOC_RISCV_TELINK_B91
+	uint8_t clk_src = b9x_config->peripheral_id == PSPI_MODULE ? sys_clk.pclk : sys_clk.hclk;
+#elif CONFIG_SOC_RISCV_TELINK_B92
+	uint8_t clk_src = sys_clk.hclk;
+#endif
 
 	/* check for unsupported configuration */
-	if (!spi_b91_is_config_supported(config, b91_config)) {
+	if (!spi_b9x_is_config_supported(config, b9x_config)) {
 		return -ENOTSUP;
 	}
 
 	/* config slave selection (CS): hw or sw */
-	if (!spi_b91_config_cs(dev, config)) {
+	if (!spi_b9x_config_cs(dev, config)) {
 		return -ENOTSUP;
 	}
 
@@ -327,44 +363,45 @@ static int spi_b91_config(const struct device *dev,
 	}
 
 	/* init SPI master */
-	spi_master_init(b91_config->peripheral_id,
+	spi_master_init(b9x_config->peripheral_id,
 			clk_src * 1000000 / (2 * config->frequency) - 1, mode);
-	spi_master_config(b91_config->peripheral_id, SPI_NOMAL);
+	spi_master_config(b9x_config->peripheral_id, SPI_NOMAL);
 
 	/* set lines configuration */
 	if (IS_ENABLED(CONFIG_SPI_EXTENDED_MODES)) {
 		uint32_t lines = config->operation & SPI_LINES_MASK;
 
 		if (lines == SPI_LINES_SINGLE) {
-			spi_set_io_mode(b91_config->peripheral_id,
-					SPI_SINGLE_MODE);
+			spi_set_io_mode(b9x_config->peripheral_id, SPI_SINGLE_MODE);
 		} else if (lines == SPI_LINES_DUAL) {
-			spi_set_io_mode(b91_config->peripheral_id,
-					SPI_DUAL_MODE);
+			spi_set_io_mode(b9x_config->peripheral_id, SPI_DUAL_MODE);
 		} else if (lines == SPI_LINES_QUAD) {
-			spi_set_io_mode(b91_config->peripheral_id,
-					HSPI_QUAD_MODE);
+#if CONFIG_SOC_RISCV_TELINK_B91
+			spi_set_io_mode(b9x_config->peripheral_id, HSPI_QUAD_MODE);
+#elif CONFIG_SOC_RISCV_TELINK_B92
+			spi_set_io_mode(b9x_config->peripheral_id, SPI_QUAD_MODE);
+#endif
 		}
 	}
 
 	/* configure pins */
-	status = pinctrl_apply_state(b91_config->pcfg, PINCTRL_STATE_DEFAULT);
+	status = pinctrl_apply_state(b9x_config->pcfg, PINCTRL_STATE_DEFAULT);
 	if (status < 0) {
 		LOG_ERR("Failed to configure SPI pins");
 		return status;
 	}
 
 	/* save context config */
-	b91_data->ctx.config = config;
+	b9x_data->ctx.config = config;
 
 	return 0;
 }
 
 /* API implementation: init */
-static int spi_b91_init(const struct device *dev)
+static int spi_b9x_init(const struct device *dev)
 {
 	int err;
-	struct spi_b91_data *data = SPI_DATA(dev);
+	struct spi_b9x_data *data = SPI_DATA(dev);
 
 	err = spi_context_cs_configure_all(&data->ctx);
 	if (err < 0) {
@@ -377,17 +414,17 @@ static int spi_b91_init(const struct device *dev)
 }
 
 /* API implementation: transceive */
-static int spi_b91_transceive(const struct device *dev,
+static int spi_b9x_transceive(const struct device *dev,
 			      const struct spi_config *config,
 			      const struct spi_buf_set *tx_bufs,
 			      const struct spi_buf_set *rx_bufs)
 {
 	int status = 0;
-	struct spi_b91_data *data = SPI_DATA(dev);
-	uint32_t txrx_len = spi_b91_get_txrx_len(tx_bufs, rx_bufs);
+	struct spi_b9x_data *data = SPI_DATA(dev);
+	uint32_t txrx_len = spi_b9x_get_txrx_len(tx_bufs, rx_bufs);
 
 	/* set configuration */
-	status = spi_b91_config(dev, config);
+	status = spi_b9x_config(dev, config);
 	if (status) {
 		return status;
 	}
@@ -402,7 +439,7 @@ static int spi_b91_transceive(const struct device *dev,
 	}
 
 	/* transceive data */
-	spi_b91_txrx(dev, txrx_len);
+	spi_b9x_txrx(dev, txrx_len);
 
 	/* if cs is defined: software cs control, set active false */
 	if (spi_cs_is_gpio(config)) {
@@ -418,7 +455,7 @@ static int spi_b91_transceive(const struct device *dev,
 
 #ifdef CONFIG_SPI_ASYNC
 /* API implementation: transceive_async */
-static int spi_b91_transceive_async(const struct device *dev,
+static int spi_b9x_transceive_async(const struct device *dev,
 				    const struct spi_config *config,
 				    const struct spi_buf_set *tx_bufs,
 				    const struct spi_buf_set *rx_bufs,
@@ -437,10 +474,10 @@ static int spi_b91_transceive_async(const struct device *dev,
 #endif /* CONFIG_SPI_ASYNC */
 
 /* API implementation: release */
-static int spi_b91_release(const struct device *dev,
+static int spi_b9x_release(const struct device *dev,
 			   const struct spi_config *config)
 {
-	struct spi_b91_data *data = SPI_DATA(dev);
+	struct spi_b9x_data *data = SPI_DATA(dev);
 
 	if (!spi_context_configured(&data->ctx, config)) {
 		return -EINVAL;
@@ -452,26 +489,26 @@ static int spi_b91_release(const struct device *dev,
 }
 
 /* SPI driver APIs structure */
-static struct spi_driver_api spi_b91_api = {
-	.transceive = spi_b91_transceive,
-	.release = spi_b91_release,
+static struct spi_driver_api spi_b9x_api = {
+	.transceive = spi_b9x_transceive,
+	.release = spi_b9x_release,
 #ifdef CONFIG_SPI_ASYNC
-	.transceive_async = spi_b91_transceive_async,
+	.transceive_async = spi_b9x_transceive_async,
 #endif /* CONFIG_SPI_ASYNC */
 };
 
 /* SPI driver registration */
-#define SPI_B91_INIT(inst)						  \
+#define SPI_B9X_INIT(inst)						  \
 									  \
 	PINCTRL_DT_INST_DEFINE(inst);					  \
 									  \
-	static struct spi_b91_data spi_b91_data_##inst = {		  \
-		SPI_CONTEXT_INIT_LOCK(spi_b91_data_##inst, ctx),	  \
-		SPI_CONTEXT_INIT_SYNC(spi_b91_data_##inst, ctx),	  \
+	static struct spi_b9x_data spi_b9x_data_##inst = {		  \
+		SPI_CONTEXT_INIT_LOCK(spi_b9x_data_##inst, ctx),	  \
+		SPI_CONTEXT_INIT_SYNC(spi_b9x_data_##inst, ctx),	  \
 		SPI_CONTEXT_CS_GPIOS_INITIALIZE(DT_DRV_INST(inst), ctx)	  \
 	};								  \
 									  \
-	static struct spi_b91_cfg spi_b91_cfg_##inst = {		  \
+	static struct spi_b9x_cfg spi_b9x_cfg_##inst = {		  \
 		.peripheral_id = DT_INST_ENUM_IDX(inst, peripheral_id),	  \
 		.cs_pin[0] = DT_INST_STRING_TOKEN(inst, cs0_pin),	  \
 		.cs_pin[1] = DT_INST_STRING_TOKEN(inst, cs1_pin),	  \
@@ -479,12 +516,12 @@ static struct spi_driver_api spi_b91_api = {
 		.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(inst),		  \
 	};								  \
 									  \
-	DEVICE_DT_INST_DEFINE(inst, spi_b91_init,			  \
+	DEVICE_DT_INST_DEFINE(inst, spi_b9x_init,			  \
 			      NULL,					  \
-			      &spi_b91_data_##inst,			  \
-			      &spi_b91_cfg_##inst,			  \
+			      &spi_b9x_data_##inst,			  \
+			      &spi_b9x_cfg_##inst,			  \
 			      POST_KERNEL,				  \
 			      CONFIG_SPI_INIT_PRIORITY,			  \
-			      &spi_b91_api);
+			      &spi_b9x_api);
 
-DT_INST_FOREACH_STATUS_OKAY(SPI_B91_INIT)
+DT_INST_FOREACH_STATUS_OKAY(SPI_B9X_INIT)
