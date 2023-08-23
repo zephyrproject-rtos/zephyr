8cc456c1a140836311426b80f8773f25b897d403
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-08-23 13:42:12 +0200

    native boards: cmake: Move nsi_config generation to shared file
    Move the generation of the native simulator build
    configuration into its own file, shared between
    the native simulator boards.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

---
+++ boards/posix/common/natsim_config.cmake
@@ -0,0 +1,22 @@
+# Copyright (c) 2023 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+set(zephyr_build_path ${CMAKE_BINARY_DIR}/zephyr)
+
+set(nsi_config_content
+  ${nsi_config_content}
+  "NSI_BUILD_OPTIONS:=$<JOIN:$<TARGET_PROPERTY:native_simulator,INTERFACE_COMPILE_OPTIONS>,\ >"
+  "NSI_BUILD_PATH:=${zephyr_build_path}/NSI"
+  "NSI_CC:=${CMAKE_C_COMPILER}"
+  "NSI_EMBEDDED_CPU_SW:=${zephyr_build_path}/${KERNEL_ELF_NAME}"
+  "NSI_EXE:=${zephyr_build_path}/${KERNEL_EXE_NAME}"
+  "NSI_EXTRA_SRCS:=$<JOIN:$<TARGET_PROPERTY:native_simulator,INTERFACE_SOURCES>,\ >"
+  "NSI_LINK_OPTIONS:=$<JOIN:$<TARGET_PROPERTY:native_simulator,INTERFACE_LINK_OPTIONS>,\ >"
+  "NSI_PATH:=${NSI_DIR}/"
+)
+
+string(REPLACE ";" "\n" nsi_config_content "${nsi_config_content}")
+
+file(GENERATE OUTPUT "${zephyr_build_path}/NSI/nsi_config"
+  CONTENT "${nsi_config_content}"
+)
