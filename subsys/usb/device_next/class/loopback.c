c169715f09f4e407f2c52c22a8d3cb187bafffc7
Johann Fischer <johann.fischer@nordicsemi.no>
2023-08-25 01:12:07 +0200

    usb: device_next: loopback: fixup left over from early state
    Class instance must not call usbd_ep_ctrl_enqueue().
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- subsys/usb/device_next/class/loopback.c
+++ subsys/usb/device_next/class/loopback.c
@@ -216,8 +216,6 @@ static int lb_control_to_host(struct usbd_class_node *c_nd,
 			      const struct usb_setup_packet *const setup,
 			      struct net_buf *const buf)
 {
-	struct usbd_contex *uds_ctx = c_nd->data->uds_ctx;
-
 	if (setup->RequestType.recipient != USB_REQTYPE_RECIPIENT_DEVICE) {
 		errno = -ENOTSUP;
 		return 0;
@@ -226,7 +224,6 @@ static int lb_control_to_host(struct usbd_class_node *c_nd,
 	if (setup->bRequest == LB_VENDOR_REQ_IN) {
 		net_buf_add_mem(buf, lb_buf,
 				MIN(sizeof(lb_buf), setup->wLength));
-		usbd_ep_ctrl_enqueue(uds_ctx, buf);
 
 		LOG_WRN("Device-to-Host, wLength %u | %zu", setup->wLength,
 			MIN(sizeof(lb_buf), setup->wLength));
