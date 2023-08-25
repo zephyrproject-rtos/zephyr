9ab243d671ad09d8c148aa49586c717c7745e99b
Ederson de Souza <ederson.desouza@intel.com>
2023-08-25 13:58:52 -0700

    doc: Generate develop/api/overview.rst API table from doxygen
    A new extension, api_overview.py, is used to, leveraging doxygen's
    Python module doxmlparser, parse the doxygen generated XML files.
    All groups ('defgroup' and 'addtogroup' tags) are collected, alongside
    their 'version' and 'since' info.
    
    From there, a new Sphinx directive `api-overview-table` is populated,
    including the name of the group, and if available, their 'since' and
    'version' information.
    
    Signed-off-by: Ederson de Souza <ederson.desouza@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- doc/conf.py
+++ doc/conf.py
@@ -95,6 +95,7 @@ extensions = [
     "sphinx_togglebutton",
     "zephyr.external_content",
     "zephyr.domain",
+    "zephyr.api_overview",
 ]
 
 # Only use SVG converter when it is really needed, e.g. LaTeX.
@@ -364,6 +365,9 @@ linkcheck_timeout = 30
 linkcheck_workers = 10
 linkcheck_anchors = False
 
+# -- Options for zephyr.api_overview --------------------------------------
+
+api_overview_doxygen_base_url = "../../doxygen/html"
 
 def setup(app):
     # theme customizations
