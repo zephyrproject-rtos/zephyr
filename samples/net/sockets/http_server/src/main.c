c2eed4c7642297768defac9569cfbce473459f9b
Emna Rekik <emna.rekik007@gmail.com>
2023-08-27 19:56:30 +0100

    samples: net: sockets: http_server: Support Static Ressources
    Support Static Ressources
    
    Signed-off-by: Emna Rekik <emna.rekik007@gmail.com>

--- samples/net/sockets/http_server/src/main.c
+++ samples/net/sockets/http_server/src/main.c
@@ -6,24 +6,43 @@
 
 #include <stdio.h>
 
-#include <zephyr/net/http/server.h>
-
-#if defined(__ZEPHYR__) || defined(CONFIG_POSIX_API)
-
 #include <zephyr/kernel.h>
+#include <zephyr/net/http/server.h>
+#include <zephyr/net/http/service.h>
+#include <zephyr/net/net_ip.h>
 
-#elif defined(__linux__)
+static uint16_t test_http_service_port = htons(CONFIG_NET_HTTP_SERVER_SERVICE_PORT);
+HTTP_SERVICE_DEFINE(test_http_service, CONFIG_NET_CONFIG_MY_IPV4_ADDR, &test_http_service_port, 1,
+		    10, NULL);
 
-#include <netinet/in.h>
+static uint8_t index_html_gz[] = {
+#include "index.html.gz.inc"
+};
 
-#endif
+struct http_resource_detail_static index_html_gz_resource_detail = {
+	.common = {
+			.type = HTTP_RESOURCE_TYPE_STATIC,
+			.bitmask_of_supported_http_methods = GET,
+		},
+	.static_data = index_html_gz,
+	.static_data_len = sizeof(index_html_gz),
+};
+
+HTTP_RESOURCE_DEFINE(index_html_gz_resource, test_http_service, "/",
+		     &index_html_gz_resource_detail);
+
+struct http_resource_detail_rest add_two_numbers_detail = {
+	.common = {
+			.type = HTTP_RESOURCE_TYPE_REST,
+			.bitmask_of_supported_http_methods = POST,
+		},
+};
+
+HTTP_RESOURCE_DEFINE(add_two_numbers, test_http_service, "/add", &add_two_numbers_detail);
 
 int main(void)
 {
 	struct http_server_ctx ctx;
-
-	ctx.config.port = 8080;
-	ctx.config.address_family = AF_INET;
 
 	int server_fd = http_server_init(&ctx);
 
