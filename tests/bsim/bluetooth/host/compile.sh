eb5d3fa4c84a74dd834161554b7104c0e891f5b4
Emil Gydesen <emil.gydesen@nordicsemi.no>
2023-08-28 14:03:30 +0200

    tests: bsim: Bluetooth: ISO: Add host CIS tests
    Add a simple central and peripheral CIS test that
    exercises the ISO host API for unicast ISO (CIS).
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bsim/bluetooth/host/compile.sh
+++ tests/bsim/bluetooth/host/compile.sh
@@ -45,6 +45,8 @@ app=tests/bsim/bluetooth/host/gatt/settings conf_file=prj_2.conf compile
 app=tests/bsim/bluetooth/host/gatt/ccc_store compile
 app=tests/bsim/bluetooth/host/gatt/ccc_store conf_file=prj_2.conf compile
 
+app=tests/bsim/bluetooth/host/iso/cis compile
+
 app=tests/bsim/bluetooth/host/l2cap/general compile
 app=tests/bsim/bluetooth/host/l2cap/userdata compile
 app=tests/bsim/bluetooth/host/l2cap/stress compile
