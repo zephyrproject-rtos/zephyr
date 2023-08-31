zephyrproject-rtos/zephyr

commit 994d897c71af561252f0d9b6efcb941584c5f9d9

soc: xtensa: intel_adsp: cavs: fix power_down_cavs() signature
The second argument 'uint32_t *hpsram_pg_mask' must be a cached
pointer and this needs to be reflected in function prototype.

Fixes sparse warning:
/zep_workspace/zephyr/soc/xtensa/intel_adsp/cavs/power.c:106:63:
warning: incorrect type in argument 2 (different address spaces)

Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

soc/xtensa/intel_adsp/cavs/power.c
@@ -69,7 +69,7 @@ static struct core_state core_desc[CONFIG_MP_MAX_NUM_CPUS] = {{0}};
  * @param hpsram_pg_mask pointer to memory segments power gating mask
  * (each bit corresponds to one ebb)
  */
-extern void power_down_cavs(bool disable_lpsram, uint32_t *hpsram_pg_mask);
+extern void power_down_cavs(bool disable_lpsram, uint32_t __sparse_cache * hpsram_pg_mask);
 
 static inline void __sparse_cache *uncache_to_cache(void *address)
 {
@@ -95,7 +95,7 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 			};
 			struct imr_layout *imr_layout = (struct imr_layout *)L3_MEM_BASE_ADDR;
 
-			__ASSERT_NO_MSG(arch_xtensa_is_ptr_uncached(L3_MEM_BASE_ADDR));
+			__ASSERT_NO_MSG(arch_xtensa_is_ptr_uncached((void *)L3_MEM_BASE_ADDR));
 			imr_layout->imr_state.header = hdr;
 
 #ifdef CONFIG_ADSP_POWER_DOWN_HPSRAM

commit 44e2136325e10236a7e38b26342339400dc93f97

soc: xtensa: intel_adsp: cavs: fix assert on L3_MEM_BASE_ADDR
The assert on L3_MEM_BASE_ADDR is incorrect, we need must convert
to uncached before use.

Fixes: 994d897c71af ("soc: xtensa: intel_adsp: cavs: fix
  power_down_cavs() signature")
Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

soc/xtensa/intel_adsp/cavs/power.c
@@ -93,9 +93,10 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 				.adsp_imr_magic = ADSP_IMR_MAGIC_VALUE,
 				.imr_restore_vector = rom_entry,
 			};
-			struct imr_layout *imr_layout = (struct imr_layout *)L3_MEM_BASE_ADDR;
+			struct imr_layout *imr_layout =
+				z_soc_uncached_ptr((__sparse_force void __sparse_cache *)
+						   L3_MEM_BASE_ADDR);
 
-			__ASSERT_NO_MSG(arch_xtensa_is_ptr_uncached((void *)L3_MEM_BASE_ADDR));
 			imr_layout->imr_state.header = hdr;
 
 #ifdef CONFIG_ADSP_POWER_DOWN_HPSRAM