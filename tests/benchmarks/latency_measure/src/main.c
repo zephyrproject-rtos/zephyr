9bc9fcab8663e112c3e0330164d0098272e0301e
Peter Mitsis <peter.mitsis@intel.com>
2023-09-01 14:27:33 -0400

    tests: latency_measure: Update context switch
    Updates both the preemptive and cooperative thread context switch
    benchmark tests so that not only will they share the same
    infrastructure, but they can both get the context switch times
    when switching from ...
      1. Kernel thread to kernel thread
      2. Kernel thread to user thread
      3. User thread to kernel thread
      4. User thread to user thread
    
    Signed-off-by: Peter Mitsis <peter.mitsis@intel.com>

--- tests/benchmarks/latency_measure/src/main.c
+++ tests/benchmarks/latency_measure/src/main.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012-2015 Wind River Systems, Inc.
+ * Copyright (c) 2023 Intel Corporation.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,29 +10,61 @@
  * This file contains the main testing module that invokes all the tests.
  */
 
+#include <zephyr/kernel.h>
 #include <zephyr/timestamp.h>
+#include <zephyr/app_memory/app_memdomain.h>
 #include "utils.h"
 #include <zephyr/tc_util.h>
 
+#define NUM_ITERATIONS  10000
+
 #define STACK_SIZE (1024 + CONFIG_TEST_EXTRA_STACK_SIZE)
 
-uint32_t tm_off; /* time necessary to read the time */
+#ifdef CONFIG_USERSPACE
+#define  BENCH_BMEM  K_APP_BMEM(bench_mem_partition)
+#else
+#define  BENCH_BMEM
+#endif
+
+uint32_t tm_off;
+
+BENCH_BMEM struct timestamp_data  timestamp;
+
+#ifdef CONFIG_USERSPACE
+K_APPMEM_PARTITION_DEFINE(bench_mem_partition);
+#endif
+
+K_THREAD_STACK_DEFINE(start_stack, START_STACK_SIZE);
+K_THREAD_STACK_DEFINE(alt_stack, START_STACK_SIZE);
+
+K_SEM_DEFINE(pause_sem, 0, 1);
+
+struct k_thread  start_thread;
+struct k_thread  alt_thread;
+
 int error_count; /* track number of errors */
 
-extern void thread_switch_yield(void);
+extern void thread_switch_yield(uint32_t num_iterations, bool is_cooperative);
 extern void int_to_thread(void);
 extern void int_to_thread_evt(void);
 extern void sema_test_signal(void);
 extern void mutex_lock_unlock(void);
-extern int coop_ctx_switch(void);
-extern int sema_test(void);
 extern int sema_context_switch(void);
 extern int suspend_resume(void);
 extern void heap_malloc_free(void);
 
-void test_thread(void *arg1, void *arg2, void *arg3)
+static void test_thread(void *arg1, void *arg2, void *arg3)
 {
 	uint32_t freq;
+
+	ARG_UNUSED(arg1);
+	ARG_UNUSED(arg2);
+	ARG_UNUSED(arg3);
+
+#ifdef CONFIG_USERSPACE
+	k_mem_domain_add_partition(&k_mem_domain_default,
+				   &bench_mem_partition);
+#endif
 
 	timing_init();
 
@@ -42,9 +75,11 @@ void test_thread(void *arg1, void *arg2, void *arg3)
 	TC_START("Time Measurement");
 	TC_PRINT("Timing results: Clock frequency: %u MHz\n", freq);
 
-	thread_switch_yield();
+	/* Preemptive threads context switching */
+	thread_switch_yield(NUM_ITERATIONS, false);
 
-	coop_ctx_switch();
+	/* Cooperative threads context switching */
+	thread_switch_yield(NUM_ITERATIONS, true);
 
 	int_to_thread();
 
@@ -63,7 +98,8 @@ void test_thread(void *arg1, void *arg2, void *arg3)
 	TC_END_REPORT(error_count);
 }
 
-K_THREAD_DEFINE(test_thread_id, STACK_SIZE, test_thread, NULL, NULL, NULL, K_PRIO_PREEMPT(10), 0, 0);
+K_THREAD_DEFINE(test_thread_id, STACK_SIZE, test_thread, NULL, NULL, NULL,
+		K_PRIO_PREEMPT(10), 0, 0);
 
 int main(void)
 {
