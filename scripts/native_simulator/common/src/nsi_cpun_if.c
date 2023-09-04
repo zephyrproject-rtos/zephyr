9dcb747dd05761c9f7ca8985b28646ed3304d743
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-09-04 15:20:35 +0200

    native simulator: Get latest from upstream
    Align with native_simulator's upstream main
    64b8be6311a61df2c63987813e8058560c8dc49c
    
    Which includes:
     * 64b8be6 Make: Give option to add extra localization parameters
     * 05bc3ce Generalize code for N CPUs/MCUs
     * 489069b Makefile: Generalize for N embedded images
     * a4c817e runner->embedded trampolines: Improve definitions
     * 8d8dd29 nsi_utils: Provide debracket macro
     * 6b4956e Add optional embedded test hook definition
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- scripts/native_simulator/common/src/nsi_cpun_if.c
+++ scripts/native_simulator/common/src/nsi_cpun_if.c
@@ -11,48 +11,10 @@
  * for ex., nsif_cpun_boot(4) -> nsif_cpu4_boot()
  */
 
-#define FUNCT(i, pre, post) \
-	pre##i##post
-
-#define F_TABLE(pre, post)    \
-	FUNCT(0, pre, post), \
-	FUNCT(1, pre, post), \
-	FUNCT(2, pre, post), \
-	FUNCT(3, pre, post), \
-	FUNCT(4, pre, post), \
-	FUNCT(5, pre, post), \
-	FUNCT(6, pre, post), \
-	FUNCT(7, pre, post), \
-	FUNCT(8, pre, post), \
-	FUNCT(9, pre, post), \
-	FUNCT(10, pre, post), \
-	FUNCT(11, pre, post), \
-	FUNCT(12, pre, post), \
-	FUNCT(13, pre, post), \
-	FUNCT(14, pre, post), \
-	FUNCT(15, pre, post)
-
-#define TRAMPOLINES(pre, post)             \
-	void pre ## n ## post(int n)       \
-	{                                  \
-		void(*fptrs[])(void) = {   \
-			F_TABLE(pre, post) \
-		};                         \
-		fptrs[n]();                \
-	}
-
-#define TRAMPOLINES_i(pre, post)             \
-	int pre ## n ## post(int n)          \
-	{                                    \
-		int(*fptrs[])(void) = {      \
-			F_TABLE(pre, post)   \
-		};                           \
-		return fptrs[n]();           \
-	}
-
 TRAMPOLINES(nsif_cpu, _pre_cmdline_hooks)
 TRAMPOLINES(nsif_cpu, _pre_hw_init_hooks)
 TRAMPOLINES(nsif_cpu, _boot)
-TRAMPOLINES_i(nsif_cpu, _cleanup)
+TRAMPOLINES_i_(nsif_cpu, _cleanup)
 TRAMPOLINES(nsif_cpu, _irq_raised)
 TRAMPOLINES(nsif_cpu, _irq_raised_from_sw)
+TRAMPOLINES_i_vp(nsif_cpu, _test_hook)
