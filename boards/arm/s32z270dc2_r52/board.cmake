44d5dd06bf98b317a8763f8aafcf71f82e8765b0
Manuel Argüelles <manuel.arguelles@nxp.com>
2023-09-05 10:43:59 +0700

    s32z270dc2_r52: make nxp_s32dbg the default runner
    Make NXP S32 Debug Probe runner the default on this board and keep
    Lauterbach TRACE32 runner as an alternative.
    
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

--- boards/arm/s32z270dc2_r52/board.cmake
+++ boards/arm/s32z270dc2_r52/board.cmake
@@ -1,19 +1,24 @@
-# Copyright 2022 NXP
+# Copyright 2022-2023 NXP
 # SPDX-License-Identifier: Apache-2.0
-
-board_set_flasher_ifnset(trace32)
-board_set_debugger_ifnset(trace32)
 
 board_runner_args(trace32
   "--startup-args"
-    "elfFile=${PROJECT_BINARY_DIR}/${KERNEL_ELF_NAME}"
-    "thumb=no"
+  "elfFile=${PROJECT_BINARY_DIR}/${KERNEL_ELF_NAME}"
+  "rtu=${CONFIG_NXP_S32_RTU_INDEX}"
 )
 
-if(CONFIG_BOARD_S32Z270DC2_RTU0_R52)
-board_runner_args(trace32 "rtu=0")
-elseif(CONFIG_BOARD_S32Z270DC2_RTU1_R52)
-board_runner_args(trace32 "rtu=1")
+board_runner_args(nxp_s32dbg
+  "--soc-family-name" "s32z2e2"
+  "--soc-name" "S32Z270"
+)
+
+if(CONFIG_DCLS)
+  board_runner_args(trace32 "lockstep=yes")
+  board_runner_args(nxp_s32dbg "--core-name" "R52_${CONFIG_NXP_S32_RTU_INDEX}_0_LS")
+else()
+  board_runner_args(trace32 "lockstep=no")
+  board_runner_args(nxp_s32dbg "--core-name" "R52_${CONFIG_NXP_S32_RTU_INDEX}_0")
 endif()
 
+include(${ZEPHYR_BASE}/boards/common/nxp_s32dbg.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/trace32.board.cmake)
