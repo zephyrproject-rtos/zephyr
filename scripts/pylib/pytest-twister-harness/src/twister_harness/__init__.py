f6185221440b7fe9f0c1b17702c8231105edc06b
Grzegorz Chwierut <grzegorz.chwierut@nordicsemi.no>
2023-09-08 17:50:24 +0200

    twister: pytest: Move fixtures to one file
    Fixtures in pytest-twister-harness plugin are moved to one
    file to simplify adding new fixtures in the future - no
    need to add pytest_plugins entry and register asserts.
    Moved also shell fixture from sample dir, because that
    fixture can be reused in new tests.
    
    Signed-off-by: Grzegorz Chwierut <grzegorz.chwierut@nordicsemi.no>

--- scripts/pylib/pytest-twister-harness/src/twister_harness/__init__.py
+++ scripts/pylib/pytest-twister-harness/src/twister_harness/__init__.py
@@ -5,7 +5,7 @@
 # flake8: noqa
 
 from twister_harness.device.device_adapter import DeviceAdapter
-from twister_harness.fixtures.mcumgr import MCUmgr
+from twister_harness.helpers.mcumgr import MCUmgr
 from twister_harness.helpers.shell import Shell
 
 __all__ = ['DeviceAdapter', 'MCUmgr', 'Shell']
