85299b31fa23568b66c2fc2667902436c757bf69
Najumon B.A <najumon.ba@intel.com>
2023-09-11 08:36:34 +0530

    drivers: intc_loapic: fixes for clang format issue
    fixes for error and warning reported by latest clang format tool
    
    Signed-off-by: Najumon B.A <najumon.ba@intel.com>

--- drivers/interrupt_controller/intc_ioapic_priv.h
+++ drivers/interrupt_controller/intc_ioapic_priv.h
@@ -12,30 +12,30 @@
 
 /* IO APIC direct register offsets */
 
-#define IOAPIC_IND 0x00   /* Index Register */
-#define IOAPIC_DATA 0x10  /* IO window (data) - pc.h */
+#define IOAPIC_IND   0x00 /* Index Register */
+#define IOAPIC_DATA  0x10 /* IO window (data) - pc.h */
 #define IOAPIC_IRQPA 0x20 /* IRQ Pin Assertion Register */
-#define IOAPIC_EOI 0x40   /* EOI Register */
+#define IOAPIC_EOI   0x40 /* EOI Register */
 
 /* IO APIC indirect register offset */
 
-#define IOAPIC_ID 0x00     /* IOAPIC ID */
-#define IOAPIC_VERS 0x01   /* IOAPIC Version */
-#define IOAPIC_ARB 0x02    /* IOAPIC Arbitration ID */
-#define IOAPIC_BOOT 0x03   /* IOAPIC Boot Configuration */
+#define IOAPIC_ID     0x00 /* IOAPIC ID */
+#define IOAPIC_VERS   0x01 /* IOAPIC Version */
+#define IOAPIC_ARB    0x02 /* IOAPIC Arbitration ID */
+#define IOAPIC_BOOT   0x03 /* IOAPIC Boot Configuration */
 #define IOAPIC_REDTBL 0x10 /* Redirection Table (24 * 64bit) */
 
 /* Interrupt delivery type */
 
 #define IOAPIC_DT_APIC 0x0 /* APIC serial bus */
-#define IOAPIC_DT_FS 0x1   /* Front side bus message*/
+#define IOAPIC_DT_FS   0x1 /* Front side bus message*/
 
 /* Version register bits */
 
 #define IOAPIC_MRE_MASK 0x00ff0000 /* Max Red. entry mask */
-#define IOAPIC_MRE_POS 16
-#define IOAPIC_PRQ 0x00008000      /* this has IRQ reg */
-#define IOAPIC_VERSION 0x000000ff  /* version number */
+#define IOAPIC_MRE_POS  16
+#define IOAPIC_PRQ      0x00008000 /* this has IRQ reg */
+#define IOAPIC_VERSION  0x000000ff /* version number */
 
 /* Redirection table entry bits: upper 32 bit */
 
--- drivers/interrupt_controller/intc_loapic.c
+++ drivers/interrupt_controller/intc_loapic.c
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-
 #define DT_DRV_COMPAT intel_loapic
 
 /*
@@ -29,31 +28,31 @@
 /* Local APIC Version Register Bits */
 
 #define LOAPIC_VERSION_MASK 0x000000ff /* LO APIC Version mask */
-#define LOAPIC_MAXLVT_MASK 0x00ff0000  /* LO APIC Max LVT mask */
-#define LOAPIC_PENTIUM4 0x00000014     /* LO APIC in Pentium4 */
-#define LOAPIC_LVT_PENTIUM4 5	  /* LO APIC LVT - Pentium4 */
-#define LOAPIC_LVT_P6 4		       /* LO APIC LVT - P6 */
-#define LOAPIC_LVT_P5 3		       /* LO APIC LVT - P5 */
+#define LOAPIC_MAXLVT_MASK  0x00ff0000 /* LO APIC Max LVT mask */
+#define LOAPIC_PENTIUM4     0x00000014 /* LO APIC in Pentium4 */
+#define LOAPIC_LVT_PENTIUM4 5          /* LO APIC LVT - Pentium4 */
+#define LOAPIC_LVT_P6       4          /* LO APIC LVT - P6 */
+#define LOAPIC_LVT_P5       3          /* LO APIC LVT - P5 */
 
 /* Local APIC Vector Table Bits */
 
 #define LOAPIC_VECTOR 0x000000ff /* vectorNo */
-#define LOAPIC_MODE 0x00000700   /* delivery mode */
-#define LOAPIC_FIXED 0x00000000  /* delivery mode: FIXED */
-#define LOAPIC_SMI 0x00000200    /* delivery mode: SMI */
-#define LOAPIC_NMI 0x00000400    /* delivery mode: NMI */
-#define LOAPIC_EXT 0x00000700    /* delivery mode: ExtINT */
-#define LOAPIC_IDLE 0x00000000   /* delivery status: Idle */
-#define LOAPIC_PEND 0x00001000   /* delivery status: Pend */
-#define LOAPIC_HIGH 0x00000000   /* polarity: High */
-#define LOAPIC_LOW 0x00002000    /* polarity: Low */
+#define LOAPIC_MODE   0x00000700 /* delivery mode */
+#define LOAPIC_FIXED  0x00000000 /* delivery mode: FIXED */
+#define LOAPIC_SMI    0x00000200 /* delivery mode: SMI */
+#define LOAPIC_NMI    0x00000400 /* delivery mode: NMI */
+#define LOAPIC_EXT    0x00000700 /* delivery mode: ExtINT */
+#define LOAPIC_IDLE   0x00000000 /* delivery status: Idle */
+#define LOAPIC_PEND   0x00001000 /* delivery status: Pend */
+#define LOAPIC_HIGH   0x00000000 /* polarity: High */
+#define LOAPIC_LOW    0x00002000 /* polarity: Low */
 #define LOAPIC_REMOTE 0x00004000 /* remote IRR */
-#define LOAPIC_EDGE 0x00000000   /* trigger mode: Edge */
-#define LOAPIC_LEVEL 0x00008000  /* trigger mode: Level */
+#define LOAPIC_EDGE   0x00000000 /* trigger mode: Edge */
+#define LOAPIC_LEVEL  0x00008000 /* trigger mode: Level */
 
 /* Local APIC Spurious-Interrupt Register Bits */
 
-#define LOAPIC_ENABLE 0x100	/* APIC Enabled */
+#define LOAPIC_ENABLE        0x100 /* APIC Enabled */
 #define LOAPIC_FOCUS_DISABLE 0x200 /* Focus Processor Checking */
 
 #if CONFIG_LOAPIC_SPURIOUS_VECTOR_ID == -1
@@ -62,18 +61,16 @@
 #define LOAPIC_SPURIOUS_VECTOR_ID CONFIG_LOAPIC_SPURIOUS_VECTOR_ID
 #endif
 
-#define LOAPIC_SSPND_BITS_PER_IRQ  1  /* Just the one for enable disable*/
-#define LOAPIC_SUSPEND_BITS_REQD (ROUND_UP((LOAPIC_IRQ_COUNT * LOAPIC_SSPND_BITS_PER_IRQ), 32))
+#define LOAPIC_SSPND_BITS_PER_IRQ 1 /* Just the one for enable disable*/
+#define LOAPIC_SUSPEND_BITS_REQD  (ROUND_UP((LOAPIC_IRQ_COUNT * LOAPIC_SSPND_BITS_PER_IRQ), 32))
 #ifdef CONFIG_PM_DEVICE
 #include <zephyr/pm/device.h>
-__pinned_bss
-uint32_t loapic_suspend_buf[LOAPIC_SUSPEND_BITS_REQD / 32] = {0};
+__pinned_bss uint32_t loapic_suspend_buf[LOAPIC_SUSPEND_BITS_REQD / 32] = {0};
 #endif
 
 DEVICE_MMIO_TOPLEVEL(LOAPIC_REGS_STR, DT_DRV_INST(0));
 
-__pinned_func
-void send_eoi(void)
+__pinned_func void send_eoi(void)
 {
 	x86_write_xapic(LOAPIC_EOI, 0);
 }
@@ -83,10 +80,10 @@ void send_eoi(void)
  *
  * Called from early assembly layer (e.g., crt0.S).
  */
-__pinned_func
-void z_loapic_enable(unsigned char cpu_number)
+__pinned_func void z_loapic_enable(unsigned char cpu_number)
 {
 	int32_t loApicMaxLvt; /* local APIC Max LVT */
+
 	DEVICE_MMIO_TOPLEVEL_MAP(LOAPIC_REGS_STR, K_MEM_CACHE_NONE);
 
 #ifndef CONFIG_X2APIC
@@ -111,8 +108,7 @@ void z_loapic_enable(unsigned char cpu_number)
 	 * x2APIC access is not enabled until the next step (if at all).
 	 */
 
-	x86_write_xapic(LOAPIC_SVR,
-			x86_read_xapic(LOAPIC_SVR) | LOAPIC_ENABLE);
+	x86_write_xapic(LOAPIC_SVR, x86_read_xapic(LOAPIC_SVR) | LOAPIC_ENABLE);
 
 #ifdef CONFIG_X2APIC
 	/*
@@ -121,6 +117,7 @@ void z_loapic_enable(unsigned char cpu_number)
 	 */
 
 	uint64_t msr = z_x86_msr_read(X86_APIC_BASE_MSR);
+
 	msr |= X86_APIC_BASE_MSR_X2APIC;
 	z_x86_msr_write(X86_APIC_BASE_MSR, msr);
 #endif
@@ -131,7 +128,7 @@ void z_loapic_enable(unsigned char cpu_number)
 
 #ifndef CONFIG_X2APIC
 	/* Flat model */
-	x86_write_loapic(LOAPIC_DFR, 0xffffffff);  /* no DFR in x2APIC mode */
+	x86_write_loapic(LOAPIC_DFR, 0xffffffff); /* no DFR in x2APIC mode */
 #endif
 
 	x86_write_loapic(LOAPIC_TPR, 0x0);
@@ -142,17 +139,17 @@ void z_loapic_enable(unsigned char cpu_number)
 
 	/* set LINT0: extInt, high-polarity, edge-trigger, not-masked */
 
-	x86_write_loapic(LOAPIC_LINT0, (x86_read_loapic(LOAPIC_LINT0) &
-		~(LOAPIC_MODE | LOAPIC_LOW |
-		  LOAPIC_LEVEL | LOAPIC_LVT_MASKED)) |
-		(LOAPIC_EXT | LOAPIC_HIGH | LOAPIC_EDGE));
+	x86_write_loapic(LOAPIC_LINT0,
+			 (x86_read_loapic(LOAPIC_LINT0) &
+			  ~(LOAPIC_MODE | LOAPIC_LOW | LOAPIC_LEVEL | LOAPIC_LVT_MASKED)) |
+				 (LOAPIC_EXT | LOAPIC_HIGH | LOAPIC_EDGE));
 
 	/* set LINT1: NMI, high-polarity, edge-trigger, not-masked */
 
-	x86_write_loapic(LOAPIC_LINT1, (x86_read_loapic(LOAPIC_LINT1) &
-		~(LOAPIC_MODE | LOAPIC_LOW |
-		  LOAPIC_LEVEL | LOAPIC_LVT_MASKED)) |
-		(LOAPIC_NMI | LOAPIC_HIGH | LOAPIC_EDGE));
+	x86_write_loapic(LOAPIC_LINT1,
+			 (x86_read_loapic(LOAPIC_LINT1) &
+			  ~(LOAPIC_MODE | LOAPIC_LOW | LOAPIC_LEVEL | LOAPIC_LVT_MASKED)) |
+				 (LOAPIC_NMI | LOAPIC_HIGH | LOAPIC_EDGE));
 
 	/* lock the Local APIC interrupts */
 
@@ -169,7 +166,7 @@ void z_loapic_enable(unsigned char cpu_number)
 
 #if CONFIG_LOAPIC_SPURIOUS_VECTOR
 	x86_write_loapic(LOAPIC_SVR, (x86_read_loapic(LOAPIC_SVR) & 0xFFFFFF00) |
-		     (LOAPIC_SPURIOUS_VECTOR_ID & 0xFF));
+					     (LOAPIC_SPURIOUS_VECTOR_ID & 0xFF));
 #endif
 
 	/* discard a pending interrupt if any */
@@ -182,16 +179,13 @@ void z_loapic_enable(unsigned char cpu_number)
  * The local APIC is initialized via z_loapic_enable() long before the
  * kernel runs through its device initializations, so this is unneeded.
  */
-__boot_func
-static int loapic_init(const struct device *unused)
+__boot_func static int loapic_init(const struct device *unused)
 {
 	ARG_UNUSED(unused);
 	return 0;
 }
 
-
-__pinned_func
-uint32_t z_loapic_irq_base(void)
+__pinned_func uint32_t z_loapic_irq_base(void)
 {
 	return z_ioapic_num_rtes();
 }
@@ -201,12 +195,11 @@ uint32_t z_loapic_irq_base(void)
  *
  * This associates an IRQ with the desired vector in the IDT.
  */
-__boot_func
-void z_loapic_int_vec_set(unsigned int irq, /* IRQ number of the interrupt */
-				  unsigned int vector /* vector to copy into the LVT */
-				  )
+__boot_func void z_loapic_int_vec_set(unsigned int irq,   /* IRQ number of the interrupt */
+				      unsigned int vector /* vector to copy into the LVT */
+)
 {
-	unsigned int oldLevel;   /* previous interrupt lock level */
+	unsigned int oldLevel; /* previous interrupt lock level */
 
 	/*
 	 * The following mappings are used:
@@ -225,8 +218,7 @@ void z_loapic_int_vec_set(unsigned int irq, /* IRQ number of the interrupt */
 
 	oldLevel = irq_lock();
 	x86_write_loapic(LOAPIC_TIMER + (irq * 0x10),
-		     (x86_read_loapic(LOAPIC_TIMER + (irq * 0x10)) &
-		      ~LOAPIC_VECTOR) | vector);
+			 (x86_read_loapic(LOAPIC_TIMER + (irq * 0x10)) & ~LOAPIC_VECTOR) | vector);
 	irq_unlock(oldLevel);
 }
 
@@ -237,10 +229,9 @@ void z_loapic_int_vec_set(unsigned int irq, /* IRQ number of the interrupt */
  *
  * This routine clears the interrupt mask bit in the LVT for the specified IRQ
  */
-__pinned_func
-void z_loapic_irq_enable(unsigned int irq)
+__pinned_func void z_loapic_irq_enable(unsigned int irq)
 {
-	unsigned int oldLevel;   /* previous interrupt lock level */
+	unsigned int oldLevel; /* previous interrupt lock level */
 
 	/*
 	 * See the comments in _LoApicLvtVecSet() regarding IRQ to LVT mappings
@@ -251,8 +242,7 @@ void z_loapic_irq_enable(unsigned int irq)
 
 	oldLevel = irq_lock();
 	x86_write_loapic(LOAPIC_TIMER + (irq * 0x10),
-		     x86_read_loapic(LOAPIC_TIMER + (irq * 0x10)) &
-		     ~LOAPIC_LVT_MASKED);
+			 x86_read_loapic(LOAPIC_TIMER + (irq * 0x10)) & ~LOAPIC_LVT_MASKED);
 	irq_unlock(oldLevel);
 }
 
@@ -263,10 +253,9 @@ void z_loapic_irq_enable(unsigned int irq)
  *
  * This routine clears the interrupt mask bit in the LVT for the specified IRQ
  */
-__pinned_func
-void z_loapic_irq_disable(unsigned int irq)
+__pinned_func void z_loapic_irq_disable(unsigned int irq)
 {
-	unsigned int oldLevel;   /* previous interrupt lock level */
+	unsigned int oldLevel; /* previous interrupt lock level */
 
 	/*
 	 * See the comments in _LoApicLvtVecSet() regarding IRQ to LVT mappings
@@ -277,11 +266,9 @@ void z_loapic_irq_disable(unsigned int irq)
 
 	oldLevel = irq_lock();
 	x86_write_loapic(LOAPIC_TIMER + (irq * 0x10),
-		     x86_read_loapic(LOAPIC_TIMER + (irq * 0x10)) |
-		     LOAPIC_LVT_MASKED);
+			 x86_read_loapic(LOAPIC_TIMER + (irq * 0x10)) | LOAPIC_LVT_MASKED);
 	irq_unlock(oldLevel);
 }
-
 
 /**
  * @brief Find the currently executing interrupt vector, if any
@@ -310,8 +297,7 @@ void z_loapic_irq_disable(unsigned int irq)
  * @return The vector of the interrupt that is currently being processed, or -1
  * if no IRQ is being serviced.
  */
-__pinned_func
-int z_irq_controller_isr_vector_get(void)
+__pinned_func int z_irq_controller_isr_vector_get(void)
 {
 	int pReg, block;
 
@@ -323,14 +309,12 @@ int z_irq_controller_isr_vector_get(void)
 		if (pReg != 0) {
 			return (block * 32) + (find_msb_set(pReg) - 1);
 		}
-
 	}
 	return -1;
 }
 
 #ifdef CONFIG_PM_DEVICE
-__pinned_func
-static int loapic_suspend(const struct device *port)
+__pinned_func static int loapic_suspend(const struct device *port)
 {
 	volatile uint32_t lvt; /* local vector table entry value */
 	int loapic_irq;
@@ -349,8 +333,7 @@ static int loapic_suspend(const struct device *port)
 			lvt = x86_read_loapic(LOAPIC_TIMER + (loapic_irq * 0x10));
 
 			if ((lvt & LOAPIC_LVT_MASKED) == 0U) {
-				sys_bitfield_set_bit((mem_addr_t)loapic_suspend_buf,
-					loapic_irq);
+				sys_bitfield_set_bit((mem_addr_t)loapic_suspend_buf, loapic_irq);
 			}
 		}
 	}
@@ -358,8 +341,7 @@ static int loapic_suspend(const struct device *port)
 	return 0;
 }
 
-__pinned_func
-int loapic_resume(const struct device *port)
+__pinned_func int loapic_resume(const struct device *port)
 {
 	int loapic_irq;
 
@@ -374,12 +356,11 @@ int loapic_resume(const struct device *port)
 
 		if (_irq_to_interrupt_vector[z_loapic_irq_base() + loapic_irq]) {
 			/* Configure vector and enable the required ones*/
-			z_loapic_int_vec_set(loapic_irq,
-				_irq_to_interrupt_vector[z_loapic_irq_base() +
-							 loapic_irq]);
+			z_loapic_int_vec_set(
+				loapic_irq,
+				_irq_to_interrupt_vector[z_loapic_irq_base() + loapic_irq]);
 
-			if (sys_bitfield_test_bit((mem_addr_t) loapic_suspend_buf,
-							loapic_irq)) {
+			if (sys_bitfield_test_bit((mem_addr_t)loapic_suspend_buf, loapic_irq)) {
 				z_loapic_irq_enable(loapic_irq);
 			}
 		}
@@ -389,12 +370,10 @@ int loapic_resume(const struct device *port)
 }
 
 /*
-* Implements the driver control management functionality
-* the *context may include IN data or/and OUT data
-*/
-__pinned_func
-static int loapic_pm_action(const struct device *dev,
-			    enum pm_device_action action)
+ * Implements the driver control management functionality
+ * the *context may include IN data or/and OUT data
+ */
+__pinned_func static int loapic_pm_action(const struct device *dev, enum pm_device_action action)
 {
 	int ret = 0;
 
@@ -415,13 +394,12 @@ static int loapic_pm_action(const struct device *dev,
 
 PM_DEVICE_DEFINE(loapic, loapic_pm_action);
 
-DEVICE_DEFINE(loapic, "loapic", loapic_init, PM_DEVICE_GET(loapic), NULL, NULL,
-	      PRE_KERNEL_1, CONFIG_INTC_INIT_PRIORITY, NULL);
+DEVICE_DEFINE(loapic, "loapic", loapic_init, PM_DEVICE_GET(loapic), NULL, NULL, PRE_KERNEL_1,
+	      CONFIG_INTC_INIT_PRIORITY, NULL);
 
 #if CONFIG_LOAPIC_SPURIOUS_VECTOR
 extern void z_loapic_spurious_handler(void);
 
-NANO_CPU_INT_REGISTER(z_loapic_spurious_handler, NANO_SOFT_IRQ,
-		      LOAPIC_SPURIOUS_VECTOR_ID >> 4,
+NANO_CPU_INT_REGISTER(z_loapic_spurious_handler, NANO_SOFT_IRQ, LOAPIC_SPURIOUS_VECTOR_ID >> 4,
 		      LOAPIC_SPURIOUS_VECTOR_ID, 0);
 #endif
