88648ff0349c9a64c98bacf168326e96df073610
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-09-11 17:25:38 +0200

    native SOC: Rename option NATIVE_SIMULATOR_CPU_N to MCU_N
    To be more accurate, as this option represents a microcontroller
    number, not a CPU number.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- boards/posix/nrf52_bsim/irq_handler.c
+++ boards/posix/nrf52_bsim/irq_handler.c
@@ -43,7 +43,7 @@ static inline void vector_to_irq(int irq_nbr, int *may_swap)
 	}
 
 	bs_trace_raw_time(6, "Vectoring to irq %i (%s)\n", irq_nbr,
-			  hw_irq_ctrl_get_name(CONFIG_NATIVE_SIMULATOR_CPU_N, irq_nbr));
+			  hw_irq_ctrl_get_name(CONFIG_NATIVE_SIMULATOR_MCU_N, irq_nbr));
 
 	sys_trace_isr_enter();
 
@@ -70,7 +70,7 @@ static inline void vector_to_irq(int irq_nbr, int *may_swap)
 	sys_trace_isr_exit();
 
 	bs_trace_raw_time(7, "Irq %i (%s) ended\n", irq_nbr,
-			  hw_irq_ctrl_get_name(CONFIG_NATIVE_SIMULATOR_CPU_N, irq_nbr));
+			  hw_irq_ctrl_get_name(CONFIG_NATIVE_SIMULATOR_MCU_N, irq_nbr));
 }
 
 /**
@@ -86,7 +86,7 @@ void posix_irq_handler(void)
 	uint64_t irq_lock;
 	int irq_nbr;
 	static int may_swap;
-	const int cpu_n = CONFIG_NATIVE_SIMULATOR_CPU_N;
+	const int cpu_n = CONFIG_NATIVE_SIMULATOR_MCU_N;
 
 	irq_lock = hw_irq_ctrl_get_current_lock(cpu_n);
 
@@ -146,7 +146,7 @@ void posix_irq_handler_im_from_sw(void)
 	 * pending we go immediately into irq_handler() to vector into its
 	 * handler
 	 */
-	if (hw_irq_ctrl_get_highest_prio_irq(CONFIG_NATIVE_SIMULATOR_CPU_N) != -1) {
+	if (hw_irq_ctrl_get_highest_prio_irq(CONFIG_NATIVE_SIMULATOR_MCU_N) != -1) {
 		if (!posix_is_cpu_running()) { /* LCOV_EXCL_BR_LINE */
 			/* LCOV_EXCL_START */
 			posix_print_error_and_exit("programming error: %s "
@@ -191,7 +191,7 @@ void posix_irq_handler_im_from_sw(void)
  */
 unsigned int posix_irq_lock(void)
 {
-	return hw_irq_ctrl_change_lock(CONFIG_NATIVE_SIMULATOR_CPU_N, true);
+	return hw_irq_ctrl_change_lock(CONFIG_NATIVE_SIMULATOR_MCU_N, true);
 }
 
 /**
@@ -205,27 +205,27 @@ unsigned int posix_irq_lock(void)
  */
 void posix_irq_unlock(unsigned int key)
 {
-	hw_irq_ctrl_change_lock(CONFIG_NATIVE_SIMULATOR_CPU_N, key);
+	hw_irq_ctrl_change_lock(CONFIG_NATIVE_SIMULATOR_MCU_N, key);
 }
 
 void posix_irq_full_unlock(void)
 {
-	hw_irq_ctrl_change_lock(CONFIG_NATIVE_SIMULATOR_CPU_N, false);
+	hw_irq_ctrl_change_lock(CONFIG_NATIVE_SIMULATOR_MCU_N, false);
 }
 
 void posix_irq_enable(unsigned int irq)
 {
-	hw_irq_ctrl_enable_irq(CONFIG_NATIVE_SIMULATOR_CPU_N, irq);
+	hw_irq_ctrl_enable_irq(CONFIG_NATIVE_SIMULATOR_MCU_N, irq);
 }
 
 void posix_irq_disable(unsigned int irq)
 {
-	hw_irq_ctrl_disable_irq(CONFIG_NATIVE_SIMULATOR_CPU_N, irq);
+	hw_irq_ctrl_disable_irq(CONFIG_NATIVE_SIMULATOR_MCU_N, irq);
 }
 
 int posix_irq_is_enabled(unsigned int irq)
 {
-	return hw_irq_ctrl_is_irq_enabled(CONFIG_NATIVE_SIMULATOR_CPU_N, irq);
+	return hw_irq_ctrl_is_irq_enabled(CONFIG_NATIVE_SIMULATOR_MCU_N, irq);
 }
 
 int posix_get_current_irq(void)
@@ -266,7 +266,7 @@ void posix_isr_declare(unsigned int irq_p, int flags, void isr_p(const void *),
  */
 void posix_irq_priority_set(unsigned int irq, unsigned int prio, uint32_t flags)
 {
-	hw_irq_ctrl_prio_set(CONFIG_NATIVE_SIMULATOR_CPU_N, irq, prio);
+	hw_irq_ctrl_prio_set(CONFIG_NATIVE_SIMULATOR_MCU_N, irq, prio);
 }
 
 /**
@@ -279,7 +279,7 @@ void posix_irq_priority_set(unsigned int irq, unsigned int prio, uint32_t flags)
  */
 void posix_sw_set_pending_IRQ(unsigned int IRQn)
 {
-	hw_irq_ctrl_raise_im_from_sw(CONFIG_NATIVE_SIMULATOR_CPU_N, IRQn);
+	hw_irq_ctrl_raise_im_from_sw(CONFIG_NATIVE_SIMULATOR_MCU_N, IRQn);
 }
 
 /**
@@ -288,7 +288,7 @@ void posix_sw_set_pending_IRQ(unsigned int IRQn)
  */
 void posix_sw_clear_pending_IRQ(unsigned int IRQn)
 {
-	hw_irq_ctrl_clear_irq(CONFIG_NATIVE_SIMULATOR_CPU_N, IRQn);
+	hw_irq_ctrl_clear_irq(CONFIG_NATIVE_SIMULATOR_MCU_N, IRQn);
 }
 
 #ifdef CONFIG_IRQ_OFFLOAD
