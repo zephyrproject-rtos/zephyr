e260d036866bfd06f402dc65c499628cc561622d
Anas Nashif <anas.nashif@intel.com>
2023-09-13 12:03:10 +0000

    init: introduce soc and board hooks
    Introduce soc and board hooks to replace arch specific code
    and replace usages of SYS_INIT for platform initialization.
    
    include/zephyr/platform/hooks.h introduces the hooks to be implemented
    by boards and SoCs.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- kernel/init.c
+++ kernel/init.c
@@ -25,6 +25,7 @@
 #include <zephyr/device.h>
 #include <zephyr/init.h>
 #include <zephyr/linker/linker-defs.h>
+#include <zephyr/platform/hooks.h>
 #include <ksched.h>
 #include <kthread.h>
 #include <zephyr/sys/dlist.h>
@@ -516,6 +517,13 @@ static void bg_thread_main(void *unused1, void *unused2, void *unused3)
 	z_sys_post_kernel = true;
 
 	z_sys_init_run_level(INIT_LEVEL_POST_KERNEL);
+#if CONFIG_SOC_LATE_INIT_HOOK
+	soc_late_init_hook();
+#endif
+#if CONFIG_BOARD_LATE_INIT_HOOK
+	board_late_init_hook();
+#endif
+
 #if defined(CONFIG_STACK_POINTER_RANDOM) && (CONFIG_STACK_POINTER_RANDOM != 0)
 	z_stack_adjust_initialized = 1;
 #endif /* CONFIG_STACK_POINTER_RANDOM */
@@ -757,6 +765,12 @@ FUNC_NORETURN void z_cstart(void)
 	/* do any necessary initialization of static devices */
 	z_device_state_init();
 
+#if CONFIG_SOC_EARLY_INIT_HOOK
+	soc_early_init_hook();
+#endif
+#if CONFIG_BOARD_EARLY_INIT_HOOK
+	board_early_init_hook();
+#endif
 	/* perform basic hardware initialization */
 	z_sys_init_run_level(INIT_LEVEL_PRE_KERNEL_1);
 #if defined(CONFIG_SMP)
