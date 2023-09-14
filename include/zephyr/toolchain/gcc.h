1a232277f94cc0332aff27657174828c620e9ca7
Gerard Marull-Paretas <gerard@teslabs.com>
2023-09-14 11:51:56 +0200

    toolchain: do not allow including toolchain-specific headers
    Toolchain utilities need to be used through the <zephyr/toolchain.h>
    parent header, where the right header is chosen.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- include/zephyr/toolchain/gcc.h
+++ include/zephyr/toolchain/gcc.h
@@ -7,6 +7,10 @@
 #ifndef ZEPHYR_INCLUDE_TOOLCHAIN_GCC_H_
 #define ZEPHYR_INCLUDE_TOOLCHAIN_GCC_H_
 
+#ifndef ZEPHYR_INCLUDE_TOOLCHAIN_H_
+#error Please do not include toolchain-specific headers directly, use <zephyr/toolchain.h> instead
+#endif
+
 /**
  * @file
  * @brief GCC toolchain abstraction
