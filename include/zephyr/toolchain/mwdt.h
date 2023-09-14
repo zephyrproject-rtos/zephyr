9660b91db3ed88283c76b8f28f6e16f0be009ae7
Gerard Marull-Paretas <gerard@teslabs.com>
2023-09-14 11:51:56 +0200

    toolchain: do not allow including toolchain-specific headers
    Toolchain utilities need to be used through the <zephyr/toolchain.h>
    parent header, where the right header is chosen.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- include/zephyr/toolchain/mwdt.h
+++ include/zephyr/toolchain/mwdt.h
@@ -8,6 +8,10 @@
 #ifndef ZEPHYR_INCLUDE_TOOLCHAIN_MWDT_H_
 #define ZEPHYR_INCLUDE_TOOLCHAIN_MWDT_H_
 
+#ifndef ZEPHYR_INCLUDE_TOOLCHAIN_H_
+#error Please do not include toolchain-specific headers directly, use <zephyr/toolchain.h> instead
+#endif
+
 #ifndef _LINKER
 #if defined(_ASMLANGUAGE)
 
