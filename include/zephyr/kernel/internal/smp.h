1b13c73aad10a9f659df26f92599f29fa0722586
Anas Nashif <anas.nashif@intel.com>
2023-09-18 11:34:06 -0400

    kernel: move internal smp calls to a internal header
    Those APIs are internal and should not be part of the main public API
    header.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

---
+++ include/zephyr/kernel/internal/smp.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_KERNEL_INTERNAL_SMP_H_
+#define ZEPHYR_INCLUDE_KERNEL_INTERNAL_SMP_H_
+
+struct k_thread;
+
+/**
+ * @internal
+ */
+#ifdef CONFIG_SOF
+void z_smp_thread_init(void *arg, struct k_thread *thread);
+void z_smp_thread_swap(void);
+#endif
+
+#endif
