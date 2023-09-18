0a49ef79c66eabcf6b3bc3b29299310ea41e8b57
Emil Gydesen <emil.gydesen@nordicsemi.no>
2023-09-18 11:22:23 +0200

    tests: Bluetooth: Audio: Codec meta_get unit tests
    Add unit tests for the bt_audio_codec_meta_get_* functions.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bluetooth/audio/codec/src/main.c
+++ tests/bluetooth/audio/codec/src/main.c
@@ -9,6 +9,7 @@
 #include <zephyr/fff.h>
 #include <zephyr/bluetooth/audio/audio.h>
 #include <zephyr/bluetooth/audio/bap_lc3_preset.h>
+#include <zephyr/sys/byteorder.h>
 
 DEFINE_FFF_GLOBALS;
 
@@ -70,4 +71,150 @@ ZTEST(audio_codec_test_suite, test_bt_audio_codec_cfg_get_frame_blocks_per_sdu)
 
 	ret = bt_audio_codec_cfg_get_frame_blocks_per_sdu(&preset.codec_cfg, true);
 	zassert_equal(ret, 1u, "unexpected return value %d", ret);
+}
+
+ZTEST(audio_codec_test_suite, test_bt_audio_codec_meta_get_pref_context)
+{
+	const uint8_t metadata[] = {
+		BT_AUDIO_CODEC_DATA(BT_AUDIO_METADATA_TYPE_PREF_CONTEXT,
+				    BT_BYTES_LIST_LE16(BT_AUDIO_CONTEXT_TYPE_UNSPECIFIED |
+						       BT_AUDIO_CONTEXT_TYPE_MEDIA)),
+	};
+	int ret;
+
+	ret = bt_audio_codec_meta_get_pref_context(metadata, ARRAY_SIZE(metadata));
+	zassert_equal(ret, 0x0005, "unexpected return value %d", ret);
+}
+
+ZTEST(audio_codec_test_suite, test_bt_audio_codec_meta_get_stream_context)
+{
+	const uint8_t metadata[] = {
+		BT_AUDIO_CODEC_DATA(BT_AUDIO_METADATA_TYPE_STREAM_CONTEXT,
+				    BT_BYTES_LIST_LE16(BT_AUDIO_CONTEXT_TYPE_UNSPECIFIED |
+						       BT_AUDIO_CONTEXT_TYPE_MEDIA)),
+	};
+	int ret;
+
+	ret = bt_audio_codec_meta_get_stream_context(metadata, ARRAY_SIZE(metadata));
+	zassert_equal(ret, 0x0005, "Unexpected return value %d", ret);
+}
+
+ZTEST(audio_codec_test_suite, test_bt_audio_codec_meta_get_program_info)
+{
+	const uint8_t expected_data[] = {'P', 'r', 'o', 'g', 'r', 'a', 'm', ' ',
+					 'I', 'n', 'f', 'o'};
+	const uint8_t metadata[] = {
+		BT_AUDIO_CODEC_DATA(BT_AUDIO_METADATA_TYPE_PROGRAM_INFO,
+				    'P', 'r', 'o', 'g', 'r', 'a', 'm', ' ', 'I', 'n', 'f', 'o'),
+	};
+	const uint8_t *program_data;
+	int ret;
+
+	ret = bt_audio_codec_meta_get_program_info(metadata, ARRAY_SIZE(metadata), &program_data);
+	zassert_equal(ret, ARRAY_SIZE(expected_data), "Unexpected return value %d", ret);
+	zassert_mem_equal(expected_data, program_data, ARRAY_SIZE(expected_data));
+}
+
+ZTEST(audio_codec_test_suite, test_bt_audio_codec_meta_get_stream_lang)
+{
+	const uint32_t expected_data = sys_get_le24((uint8_t[]){'e', 'n', 'g'});
+	const uint8_t metadata[] = {
+		BT_AUDIO_CODEC_DATA(BT_AUDIO_METADATA_TYPE_STREAM_LANG, 'e', 'n', 'g'),
+	};
+	int ret;
+
+	ret = bt_audio_codec_meta_get_stream_lang(metadata, ARRAY_SIZE(metadata));
+	zassert_equal(ret, expected_data, "Unexpected return value %d", ret);
+}
+
+ZTEST(audio_codec_test_suite, test_bt_audio_codec_meta_get_ccid_list)
+{
+	const uint8_t expected_data[] = {0x05, 0x10, 0x15};
+	const uint8_t metadata[] = {
+		BT_AUDIO_CODEC_DATA(BT_AUDIO_METADATA_TYPE_CCID_LIST, 0x05, 0x10, 0x15),
+	};
+	const uint8_t *ccid_list;
+	int ret;
+
+	ret = bt_audio_codec_meta_get_ccid_list(metadata, ARRAY_SIZE(metadata), &ccid_list);
+	zassert_equal(ret, ARRAY_SIZE(expected_data), "Unexpected return value %d", ret);
+	zassert_mem_equal(expected_data, ccid_list, ARRAY_SIZE(expected_data));
+}
+
+ZTEST(audio_codec_test_suite, test_bt_audio_codec_meta_get_parental_rating)
+{
+	const uint8_t metadata[] = {
+		BT_AUDIO_CODEC_DATA(BT_AUDIO_METADATA_TYPE_PARENTAL_RATING,
+				    BT_AUDIO_PARENTAL_RATING_AGE_10_OR_ABOVE),
+	};
+	int ret;
+
+	ret = bt_audio_codec_meta_get_parental_rating(metadata, ARRAY_SIZE(metadata));
+	zassert_equal(ret, 0x07, "Unexpected return value %d", ret);
+}
+
+ZTEST(audio_codec_test_suite, test_bt_audio_codec_meta_get_program_info_uri)
+{
+	const uint8_t expected_data[] = {'e', 'x', 'a', 'm', 'p', 'l', 'e', '.', 'c', 'o', 'm'};
+	const uint8_t metadata[] = {
+		BT_AUDIO_CODEC_DATA(BT_AUDIO_METADATA_TYPE_PROGRAM_INFO_URI,
+				    'e', 'x', 'a', 'm', 'p', 'l', 'e', '.', 'c', 'o', 'm'),
+	};
+	const uint8_t *program_info_uri;
+	int ret;
+
+	ret = bt_audio_codec_meta_get_program_info_uri(metadata, ARRAY_SIZE(metadata),
+						       &program_info_uri);
+	zassert_equal(ret, ARRAY_SIZE(expected_data), "Unexpected return value %d", ret);
+	zassert_mem_equal(expected_data, program_info_uri, ARRAY_SIZE(expected_data));
+}
+
+ZTEST(audio_codec_test_suite, test_bt_audio_codec_meta_get_audio_active_state)
+{
+	const uint8_t metadata[] = {
+		BT_AUDIO_CODEC_DATA(BT_AUDIO_METADATA_TYPE_AUDIO_STATE,
+				    BT_AUDIO_ACTIVE_STATE_ENABLED),
+	};
+	int ret;
+
+	ret = bt_audio_codec_meta_get_audio_active_state(metadata, ARRAY_SIZE(metadata));
+	zassert_equal(ret, 0x01, "Unexpected return value %d", ret);
+}
+
+ZTEST(audio_codec_test_suite, test_bt_audio_codec_meta_get_broadcast_audio_immediate_rendering_flag)
+{
+	const uint8_t metadata[] = {0x01, BT_AUDIO_METADATA_TYPE_BROADCAST_IMMEDIATE};
+	int ret;
+
+	ret = bt_audio_codec_meta_get_broadcast_audio_immediate_rendering_flag(
+		metadata, ARRAY_SIZE(metadata));
+	zassert_equal(ret, 0, "Unexpected return value %d", ret);
+}
+
+ZTEST(audio_codec_test_suite, test_bt_audio_codec_meta_get_extended)
+{
+	const uint8_t expected_data[] = {0x00, 0x01, 0x02, 0x03};
+	const uint8_t metadata[] = {
+		BT_AUDIO_CODEC_DATA(BT_AUDIO_METADATA_TYPE_EXTENDED, 0x00, 0x01, 0x02, 0x03),
+	};
+	const uint8_t *extended_meta;
+	int ret;
+
+	ret = bt_audio_codec_meta_get_extended(metadata, ARRAY_SIZE(metadata), &extended_meta);
+	zassert_equal(ret, ARRAY_SIZE(expected_data), "Unexpected return value %d", ret);
+	zassert_mem_equal(expected_data, extended_meta, ARRAY_SIZE(expected_data));
+}
+
+ZTEST(audio_codec_test_suite, test_bt_audio_codec_meta_get_vendor)
+{
+	const uint8_t expected_data[] = {0x00, 0x01, 0x02, 0x03};
+	const uint8_t metadata[] = {
+		BT_AUDIO_CODEC_DATA(BT_AUDIO_METADATA_TYPE_VENDOR, 0x00, 0x01, 0x02, 0x03),
+	};
+	const uint8_t *vendor_meta;
+	int ret;
+
+	ret = bt_audio_codec_meta_get_vendor(metadata, ARRAY_SIZE(metadata), &vendor_meta);
+	zassert_equal(ret, ARRAY_SIZE(expected_data), "Unexpected return value %d", ret);
+	zassert_mem_equal(expected_data, vendor_meta, ARRAY_SIZE(expected_data));
 }
