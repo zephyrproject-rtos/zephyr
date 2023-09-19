zephyrproject-rtos/zephyr

commit b572e8216aa258891cb06a17b25b5770637bb337

net: wifi: Add scan extension for specifying channels
Add scan extension to allow limiting the scanning to specific channels.
Only 20 MHz channel specifications are allowed presently.

Signed-off-by: Sachin D Kulkarni <sachin.kulkarni@nordicsemi.no>

include/zephyr/net/wifi_mgmt.h
@@ -215,6 +215,21 @@ struct wifi_scan_params {
 	 * signifies that there is no restriction on the number of scan results to be returned.
 	 */
 	uint16_t max_bss_cnt;
+	/** Channel information array indexed on Wi-Fi frequency bands and channels within that
+	 * band.
+	 * E.g. to scan channel 6 and 11 on the 2.4 GHz band, channel 36 on the 5 GHz band:
+	 * @code{.c}
+	 *     chan[WIFI_FREQ_BAND_2_4_GHZ][0] = 6;
+	 *     chan[WIFI_FREQ_BAND_2_4_GHZ][1] = 11;
+	 *     chan[WIFI_FREQ_BAND_5_GHZ][0] = 36;
+	 * @endcode
+	 *
+	 *  This list specifies the channels to be __considered for scan__. The underlying
+	 *  Wi-Fi chip can silently omit some channels due to various reasons such as channels
+	 *  not conforming to regulatory restrictions etc. The invoker of the API should
+	 *  ensure that the channels specified follow regulatory rules.
+	 */
+	uint16_t chan[WIFI_FREQ_BAND_MAX + 1][WIFI_CHANNEL_MAX];
 };
 
 /** Wi-Fi scan result, each result is provided to the net_mgmt_event_callback

include/zephyr/net/wifi_utils.h
@@ -59,7 +59,7 @@ int wifi_utils_parse_scan_bands(char *scan_bands_str, uint8_t *band_map);
 /**
  * @brief Convert a string containing a list of SSIDs to an array of SSID strings.
  *
- * @details The function will parse a string which specifies Wi-Fi SSIDs.
+ * @details The function will parse a string which specifies Wi-Fi SSIDs
  * as a comma separated string and convert it to an array.
  *
  * @param scan_ssids_str List of SSIDs expressed as a comma separated list.
@@ -70,6 +70,37 @@ int wifi_utils_parse_scan_bands(char *scan_bands_str, uint8_t *band_map);
  */
 int wifi_utils_parse_scan_ssids(char *scan_ssids_str,
 				char ssids[][WIFI_SSID_MAX_LEN + 1]);
+
+
+/**
+ * @brief Convert a string containing a specification of scan channels to an array.
+ *
+ * @details The function will parse a string which specifies channels to be scanned
+ * as a string and convert it to an array.
+ *
+ * The channel string has to be formatted using the colon (:), comma(,), hyphen (-) and
+ * underscore (_) delimiters as follows:
+ *	- A colon identifies the value preceding it as a band. A band value
+ *	  (2: 2.4 GHz, 5: 5 GHz 6: 6 GHz) has to precede the channels in that band (e.g. 2: etc)
+ *	- Hyphens (-) are used to identify channel ranges (e.g. 2-7, 32-48 etc)
+ *	- Commas are used to separate channel values within a band. Channels can be specified
+ *	  as individual values (2,6,48 etc) or channel ranges using hyphens (1-14, 32-48 etc)
+ *	- Underscores (_) are used to specify multiple band-channel sets (e.g. 2:1,2_5:36,40 etc)
+ *	- No spaces should be used anywhere, i.e. before/after commas,
+ *	  before/after hyphens etc.
+ *
+ * An example channel specification specifying channels in the 2.4 GHz and 5 GHz bands is
+ * as below:
+ *	2:1,5,7,9-11_5:36-48,100,163-167
+ *
+ * @param scan_chan_str List of channels expressed in the format described above.
+ * @param chan Pointer to an array where the parsed channels are to be stored.
+ *
+ * @retval 0 on success.
+ * @retval -errno value in case of failure.
+ */
+int wifi_utils_parse_scan_chan(char *scan_chan_str,
+			       uint16_t chan[][WIFI_CHANNEL_MAX]);
 
 /**
  * @}

subsys/net/l2/wifi/Kconfig
@@ -95,6 +95,24 @@ config WIFI_MGMT_SCAN_MAX_BSS_CNT
 	help
 	  Maximum number of scan results to return. 0 represents unlimited number of BSSes.
 
+config WIFI_MGMT_SCAN_CHAN
+	string "Scan on specific channels"
+	default ""
+	help
+	 Formatted string which specifies channels to be scanned. The channel string has to be formatted
+	 using the colon (:), comma(,), hyphen (-) and space ( ) delimiters as follows:
+	 - A colon identifies the value preceding it as a band. A band value
+	   (2: 2.4 GHz, 5: 5 GHz 6: 6 GHz) has to precede the channels in that band (e.g. 2: etc)
+	 - Hyphens are used to identify channel ranges (e.g. 2-7, 32-48 etc)
+	 - Commas are used to separate channel values within a band. Channels can be specified
+	   as individual values (2,6,48 etc) or channel ranges using hyphens (1-14, 32-48 etc)
+	 - Spaces are used to specify multiple band-channel sets (e.g. 2:1,2 5:36,40 etc)
+	 - No spaces should be used anywhere else, i.e. before/after commas,
+	   before/after hyphens.
+	   An example channel specification specifying channels in the 2.4 GHz and 5 GHz bands is
+	   as below:
+	   2:1,5,7,9-11_5:36-48,100,163-167
+
 config WIFI_NM
 	bool "Wi-Fi Network manager support"
 	help

subsys/net/l2/wifi/wifi_mgmt.c
@@ -104,6 +104,8 @@ static int wifi_scan(uint32_t mgmt_request, struct net_if *iface,
 	const struct device *dev = net_if_get_device(iface);
 	struct wifi_scan_params *params = data;
 	const struct wifi_mgmt_ops *const wifi_mgmt_api = get_wifi_api(iface);
+	bool chan_specified = false;
+	uint8_t i = 0;
 
 	if (wifi_mgmt_api == NULL || wifi_mgmt_api->scan == NULL) {
 		return -ENOTSUP;
@@ -144,6 +146,21 @@ static int wifi_scan(uint32_t mgmt_request, struct net_if *iface,
 			params->max_bss_cnt = CONFIG_WIFI_MGMT_SCAN_MAX_BSS_CNT;
 		}
 
+		for (i = 0; i <= WIFI_FREQ_BAND_MAX; i++) {
+			if (params->chan[i][0]) {
+				chan_specified = true;
+				break;
+			}
+		}
+
+		if ((!chan_specified) && strlen(CONFIG_WIFI_MGMT_SCAN_CHAN))  {
+			if (wifi_utils_parse_scan_chan(CONFIG_WIFI_MGMT_SCAN_CHAN,
+						       params->chan)) {
+				NET_ERR("Incorrect value(s) in CONFIG_WIFI_MGMT_SCAN_CHAN: %s",
+					CONFIG_WIFI_MGMT_SCAN_CHAN);
+				return -EINVAL;
+			}
+		}
 	}
 
 	return wifi_mgmt_api->scan(dev, params, scan_result_cb);

subsys/net/l2/wifi/wifi_shell.c
@@ -473,11 +473,12 @@ static int wifi_scan_args_to_params(const struct shell *sh,
 					       {"dwell_time_passive", required_argument, 0, 'p'},
 					       {"ssids", required_argument, 0, 's'},
 					       {"max_bss", required_argument, 0, 'm'},
+					       {"chans", required_argument, 0, 'c'},
 					       {0, 0, 0, 0}};
 	int opt_index = 0;
 	int val;
 
-	while ((opt = getopt_long(argc, argv, "t:b:a:p:s:m:", long_options, &opt_index)) != -1) {
+	while ((opt = getopt_long(argc, argv, "t:b:a:p:s:m:c:", long_options, &opt_index)) != -1) {
 		state = getopt_state_get();
 		switch (opt) {
 		case 't':
@@ -531,6 +532,14 @@ static int wifi_scan_args_to_params(const struct shell *sh,
 			}
 
 			params->max_bss_cnt = val;
+			break;
+		case 'c':
+			if (wifi_utils_parse_scan_chan(optarg, params->chan)) {
+				shell_fprintf(sh,
+					      SHELL_ERROR,
+					      "Invalid band or channel value(s)\n");
+				return -ENOEXEC;
+			}
 			break;
 		case '?':
 			shell_fprintf(sh, SHELL_ERROR, "Invalid option or option usage: %s\n",
@@ -1269,7 +1278,8 @@ SHELL_STATIC_SUBCMD_SET_CREATE(wifi_commands,
 		    "[-a, --dwell_time_active <val_in_ms>] : Active scan dwell time (in ms) on a channel. Range 5 ms to 1000 ms.\n"
 		    "[-p, --dwell_time_passive <val_in_ms>] : Passive scan dwell time (in ms) on a channel. Range 10 ms to 1000 ms.\n"
 		    "[-s, --ssids <Comma separate list of SSIDs>] : SSID list to scan for.\n"
-		    "[-m, --max_bss <val>] : Maximum BSSes to scan for. Range 1 - 65535.",
+		    "[-m, --max_bss <val>] : Maximum BSSes to scan for. Range 1 - 65535.\n"
+		    "[-c, --chans <Comma separated list of channel ranges>] : Channels to be scanned. The channels must be specified in the form band1:chan1,chan2_band2:chan3,..etc. band1, band2 must be valid band values and chan1, chan2, chan3 must be specified as a list of comma separated values where each value is either a single channel or a channel range specified as chan_start-chan_end. Each band channel set has to be separated by a _. For example, a valid channel specification can be 2:1,6-11,14_5:36,149-165,44",
 		  cmd_wifi_scan),
 	SHELL_CMD(statistics, NULL, "Wi-Fi interface statistics", cmd_wifi_stats),
 	SHELL_CMD(status, NULL, "Status of the Wi-Fi interface", cmd_wifi_status),

subsys/net/l2/wifi/wifi_utils.c
@@ -13,27 +13,202 @@ LOG_MODULE_REGISTER(net_wifi_utils, CONFIG_NET_L2_WIFI_MGMT_LOG_LEVEL);
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include <zephyr/kernel.h>
 #include <zephyr/net/net_core.h>
 #include <zephyr/net/wifi.h>
 #include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/wifi_utils.h>
 
 /* Ensure 'strtok_r' is available even with -std=c99. */
 char *strtok_r(char *str, const char *delim, char **saveptr);
 
+static const uint16_t valid_5g_chans_20mhz[] = {32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 96, 100,
+	104, 108, 112, 116, 120, 124, 128, 132, 136, 140, 144, 149, 153, 157, 159, 161,
+	163, 165, 167, 169, 171, 173, 175, 177};
+
 static enum wifi_frequency_bands wifi_utils_map_band_str_to_idx(char *band_str)
 {
+	enum wifi_frequency_bands band = WIFI_FREQ_BAND_UNKNOWN;
+
 	if (!strcmp(band_str, "2")) {
-		return WIFI_FREQ_BAND_2_4_GHZ;
+		band = WIFI_FREQ_BAND_2_4_GHZ;
 	} else if (!strcmp(band_str, "5")) {
-		return WIFI_FREQ_BAND_5_GHZ;
+		band = WIFI_FREQ_BAND_5_GHZ;
 	} else if (!strcmp(band_str, "6")) {
-		return WIFI_FREQ_BAND_6_GHZ;
-	} else {
-		NET_ERR("Unknown band value: %s", band_str);
-		return WIFI_FREQ_BAND_UNKNOWN;
+		band = WIFI_FREQ_BAND_6_GHZ;
 	}
+
+	return band;
+}
+
+
+static bool wifi_utils_validate_chan_2g(uint16_t chan)
+{
+	if ((chan >= 1) && (chan <= 14)) {
+		return true;
+	}
+
+	return false;
+}
+
+
+static bool wifi_utils_validate_chan_5g(uint16_t chan)
+{
+	uint16_t i;
+
+	for (i = 0; i < ARRAY_SIZE(valid_5g_chans_20mhz); i++) {
+		if (chan == valid_5g_chans_20mhz[i]) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+
+static bool wifi_utils_validate_chan_6g(uint16_t chan)
+{
+	if (((chan >= 1) && (chan <= 233) && (!((chan - 1)%4))) ||
+	    (chan == 2)) {
+		return true;
+	}
+
+	return false;
+}
+
+
+static bool wifi_utils_validate_chan(uint8_t band,
+				     uint16_t chan)
+{
+	bool result = false;
+
+	switch (band) {
+	case WIFI_FREQ_BAND_2_4_GHZ:
+		result = wifi_utils_validate_chan_2g(chan);
+		break;
+	case WIFI_FREQ_BAND_5_GHZ:
+		result = wifi_utils_validate_chan_5g(chan);
+		break;
+	case WIFI_FREQ_BAND_6_GHZ:
+		result = wifi_utils_validate_chan_6g(chan);
+		break;
+	default:
+		NET_ERR("Unknown band: %d", band);
+		break;
+	}
+
+	return result;
+}
+
+
+static int wifi_utils_get_all_chans_in_range(uint16_t chan_start,
+		uint16_t chan_end,
+		uint16_t chan[][WIFI_CHANNEL_MAX],
+		uint8_t band_idx,
+		uint8_t *chan_idx)
+{
+	uint16_t i;
+	bool start = false;
+	bool end = false;
+	uint8_t idx;
+
+	if (!wifi_utils_validate_chan(band_idx, chan_start)) {
+		NET_ERR("Invalid channel value %d in band %d", chan_start, band_idx);
+		return -EINVAL;
+	}
+
+	if (!wifi_utils_validate_chan(band_idx, chan_end)) {
+		NET_ERR("Invalid channel value %d in band %d", chan_end, band_idx);
+		return -EINVAL;
+	}
+
+	if (chan_end < chan_start) {
+		NET_ERR("Channel range end (%d) cannot be less than start (%d)",
+			chan_end,
+			chan_start);
+		return -EINVAL;
+	}
+
+	switch (band_idx) {
+	case WIFI_FREQ_BAND_2_4_GHZ:
+		idx = *chan_idx;
+
+		for (i = chan_start; i <= chan_end; i++) {
+			chan[band_idx][idx++] = i;
+		}
+
+		*chan_idx = idx;
+
+		break;
+	case WIFI_FREQ_BAND_5_GHZ:
+		idx = *chan_idx;
+
+		for (i = 0; i < ARRAY_SIZE(valid_5g_chans_20mhz); i++) {
+			if (valid_5g_chans_20mhz[i] == chan_start) {
+				start = true;
+			}
+
+			if (valid_5g_chans_20mhz[i] == chan_end) {
+				end = true;
+			}
+
+			if (start) {
+				chan[band_idx][idx++] = valid_5g_chans_20mhz[i];
+			}
+
+			if (end) {
+				*chan_idx = idx;
+				break;
+			}
+		}
+
+		break;
+	case WIFI_FREQ_BAND_6_GHZ:
+		idx = *chan_idx;
+
+		i = chan_start;
+
+		while (i <= chan_end) {
+			chan[band_idx][idx++] = i;
+
+			if (i == 1) {
+				i++;
+			} else if (i == 2) {
+				i += 3;
+			} else {
+				i += 4;
+			}
+		}
+
+		*chan_idx = idx;
+	default:
+		NET_ERR("Unknown band value: %d", band_idx);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+
+static int wifi_utils_validate_chan_str(char *chan_str)
+{
+	uint8_t i;
+
+	if ((!chan_str) || (!strlen(chan_str))) {
+		NET_ERR("Null or empty channel string\n");
+		return -EINVAL;
+	}
+
+	for (i = 0; i < strlen(chan_str); i++) {
+		if (!isdigit((int)chan_str[i])) {
+			NET_ERR("Invalid character in channel string %c\n", chan_str[i]);
+			return -EINVAL;
+		}
+	}
+
+	return 0;
 }
 
 
@@ -97,6 +272,130 @@ int wifi_utils_parse_scan_ssids(char *scan_ssids_str,
 		strcpy(&ssids[i++][0], ssid);
 
 		ssid = strtok_r(NULL, ",", &ctx);
+	}
+
+	return 0;
+}
+
+
+int wifi_utils_parse_scan_chan(char *scan_chan_str,
+			       uint16_t chan[][WIFI_CHANNEL_MAX])
+{
+	char band_str[WIFI_UTILS_MAX_BAND_STR_LEN] = {0};
+	char chan_str[WIFI_UTILS_MAX_CHAN_STR_LEN] = {0};
+	enum wifi_frequency_bands band = WIFI_FREQ_BAND_UNKNOWN;
+	uint16_t band_str_start_idx = 0;
+	uint16_t chan_str_start_idx = 0;
+	uint8_t chan_idx = 0;
+	uint16_t chan_start = 0;
+	uint16_t chan_val = 0;
+	uint16_t i = 0;
+	bool valid_band = false;
+	bool valid_chan = false;
+
+	while (scan_chan_str[i] != '\0') {
+		if (scan_chan_str[i] != ':') {
+			i++;
+			continue;
+		}
+
+		if (((i - band_str_start_idx) <= 0) ||
+		    ((i - band_str_start_idx) > WIFI_UTILS_MAX_BAND_STR_LEN)) {
+			NET_ERR("Invalid band value %s",
+				&scan_chan_str[band_str_start_idx]);
+			return -EINVAL;
+		}
+
+		strncpy(band_str,
+			&scan_chan_str[band_str_start_idx],
+			(i - band_str_start_idx));
+
+		band = wifi_utils_map_band_str_to_idx(band_str);
+
+		if (band == WIFI_FREQ_BAND_UNKNOWN) {
+			NET_ERR("Unsupported band value: %s", band_str);
+			return -EINVAL;
+		}
+
+		i++;
+		chan_idx = 0;
+		chan_str_start_idx = i;
+		valid_band = true;
+
+		while (1) {
+			if ((scan_chan_str[i] != ',') &&
+			    (scan_chan_str[i] != '_') &&
+			    (scan_chan_str[i] != '-') &&
+			    (scan_chan_str[i] != '\0')) {
+				i++;
+				continue;
+			}
+
+			if ((i - chan_str_start_idx) >
+			    WIFI_UTILS_MAX_CHAN_STR_LEN) {
+				NET_ERR("Invalid chan value %s",
+					&scan_chan_str[chan_str_start_idx]);
+				return -EINVAL;
+			}
+
+			strncpy(chan_str,
+				&scan_chan_str[chan_str_start_idx],
+				(i - chan_str_start_idx));
+
+			if (wifi_utils_validate_chan_str(chan_str)) {
+				NET_ERR("Channel string validation failed");
+				return -EINVAL;
+			}
+
+			chan_val = atoi(chan_str);
+
+			memset(chan_str, 0, sizeof(chan_str));
+
+			if (chan_start) {
+				if (wifi_utils_get_all_chans_in_range(chan_start,
+								      chan_val,
+								      chan,
+								      band,
+								      &chan_idx)) {
+					NET_ERR("Channel range invalid");
+					return -EINVAL;
+				}
+
+				chan_start = 0;
+			} else {
+				if (!wifi_utils_validate_chan(band,
+							      chan_val)) {
+					NET_ERR("Invalid channel %d", chan_val);
+					return -EINVAL;
+				}
+
+				chan[band][chan_idx++] = chan_val;
+			}
+
+			valid_chan = true;
+
+			if (scan_chan_str[i] == '_') {
+				band_str_start_idx = ++i;
+				break;
+			} else if (scan_chan_str[i] == ',') {
+				chan_str_start_idx = ++i;
+			} else if (scan_chan_str[i] == '-') {
+				chan_start = chan_val;
+				chan_str_start_idx = ++i;
+			} else if (scan_chan_str[i] == '\0') {
+				break;
+			}
+		}
+	}
+
+	if (!valid_band) {
+		NET_ERR("No valid band found");
+		return -EINVAL;
+	}
+
+	if (!valid_chan) {
+		NET_ERR("No valid channel found");
+		return -EINVAL;
 	}
 
 	return 0;

commit 157b9762c9c9e8ac24f9fa6253fc26b82081381f

net: wifi: Add toggle for extensions to scan specific channels
The feature uses lots of RAM and breaks samples/net/wifi on ESP32.
Add option to turn off this feature.

Fixes: b572e8216aa258891cb06a17b25b5770637bb337

Signed-off-by: Kent Gustavsson <kent@minoris.se>

include/zephyr/net/wifi_mgmt.h
@@ -252,7 +252,9 @@ struct wifi_scan_params {
 	 *  not conforming to regulatory restrictions etc. The invoker of the API should
 	 *  ensure that the channels specified follow regulatory rules.
 	 */
+#ifdef CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT
 	uint8_t chan[WIFI_FREQ_BAND_MAX + 1][WIFI_CHANNEL_MAX];
+#endif /* CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT */
 };
 
 /** Wi-Fi scan result, each result is provided to the net_mgmt_event_callback

include/zephyr/net/wifi_utils.h
@@ -72,6 +72,7 @@ int wifi_utils_parse_scan_ssids(char *scan_ssids_str,
 				char ssids[][WIFI_SSID_MAX_LEN + 1]);
 
 
+#ifdef CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT
 /**
  * @brief Convert a string containing a specification of scan channels to an array.
  *
@@ -102,6 +103,7 @@ int wifi_utils_parse_scan_ssids(char *scan_ssids_str,
 int wifi_utils_parse_scan_chan(char *scan_chan_str,
 			       uint8_t chan[][WIFI_CHANNEL_MAX]);
 
+#endif /* CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT */
 /**
  * @}
  */

subsys/net/l2/wifi/Kconfig
@@ -96,9 +96,15 @@ config WIFI_MGMT_SCAN_MAX_BSS_CNT
 	help
 	  Maximum number of scan results to return. 0 represents unlimited number of BSSes.
 
+config WIFI_MGMT_SCAN_CHAN_SUPPORT
+	bool "Scan on specific channels support"
+	help
+	  Enabling the scanning of individual channels. This option consumes a lot of memory.
+
 config WIFI_MGMT_SCAN_CHAN
 	string "Scan on specific channels"
 	default ""
+	depends on WIFI_MGMT_SCAN_CHAN_SUPPORT
 	help
 	 Formatted string which specifies channels to be scanned. The channel string has to be formatted
 	 using the colon (:), comma(,), hyphen (-) and space ( ) delimiters as follows:

subsys/net/l2/wifi/wifi_mgmt.c
@@ -104,8 +104,10 @@ static int wifi_scan(uint32_t mgmt_request, struct net_if *iface,
 	const struct device *dev = net_if_get_device(iface);
 	struct wifi_scan_params *params = data;
 	const struct wifi_mgmt_ops *const wifi_mgmt_api = get_wifi_api(iface);
+#ifdef CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT
 	bool chan_specified = false;
-	uint8_t i = 0;
+	uint8_t band = 0;
+#endif /* CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT */
 
 	if (wifi_mgmt_api == NULL || wifi_mgmt_api->scan == NULL) {
 		return -ENOTSUP;
@@ -145,9 +147,9 @@ static int wifi_scan(uint32_t mgmt_request, struct net_if *iface,
 		if (!params->max_bss_cnt) {
 			params->max_bss_cnt = CONFIG_WIFI_MGMT_SCAN_MAX_BSS_CNT;
 		}
-
-		for (i = 0; i <= WIFI_FREQ_BAND_MAX; i++) {
-			if (params->chan[i][0]) {
+#ifdef CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT
+		for (band = 0; band <= WIFI_FREQ_BAND_MAX; band++) {
+			if (params->chan[band][0]) {
 				chan_specified = true;
 				break;
 			}
@@ -161,6 +163,7 @@ static int wifi_scan(uint32_t mgmt_request, struct net_if *iface,
 				return -EINVAL;
 			}
 		}
+#endif /* CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT */
 	}
 
 	return wifi_mgmt_api->scan(dev, params, scan_result_cb);

subsys/net/l2/wifi/wifi_shell.c
@@ -556,13 +556,17 @@ static int wifi_scan_args_to_params(const struct shell *sh,
 			opt_num++;
 			break;
 		case 'c':
+#ifdef CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT
 			if (wifi_utils_parse_scan_chan(optarg, params->chan)) {
 				shell_fprintf(sh,
 					      SHELL_ERROR,
 					      "Invalid band or channel value(s)\n");
 				return -ENOEXEC;
 			}
-
+#else
+			shell_fprintf(sh, SHELL_ERROR, "This feature isn't enabled\n");
+			return -ENOEXEC;
+#endif /* CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT */
 			opt_num++;
 			break;
 		case 'h':
@@ -1653,7 +1657,9 @@ SHELL_STATIC_SUBCMD_SET_CREATE(wifi_commands,
 		    "[-p, --dwell_time_passive <val_in_ms>] : Passive scan dwell time (in ms) on a channel. Range 10 ms to 1000 ms.\n"
 		    "[-s, --ssids <Comma separate list of SSIDs>] : SSID list to scan for.\n"
 		    "[-m, --max_bss <val>] : Maximum BSSes to scan for. Range 1 - 65535.\n"
+#ifdef CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT
 		    "[-c, --chans <Comma separated list of channel ranges>] : Channels to be scanned. The channels must be specified in the form band1:chan1,chan2_band2:chan3,..etc. band1, band2 must be valid band values and chan1, chan2, chan3 must be specified as a list of comma separated values where each value is either a single channel or a channel range specified as chan_start-chan_end. Each band channel set has to be separated by a _. For example, a valid channel specification can be 2:1,6-11,14_5:36,149-165,44\n"
+#endif /* CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT */
 		    "[-h, --help] : Print out the help for the scan command.",
 		  cmd_wifi_scan),
 	SHELL_CMD(statistics, NULL, "Wi-Fi interface statistics", cmd_wifi_stats),

subsys/net/l2/wifi/wifi_utils.c
@@ -24,9 +24,11 @@ LOG_MODULE_REGISTER(net_wifi_utils, CONFIG_NET_L2_WIFI_MGMT_LOG_LEVEL);
 /* Ensure 'strtok_r' is available even with -std=c99. */
 char *strtok_r(char *str, const char *delim, char **saveptr);
 
+#ifdef CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT
 static const uint16_t valid_5g_chans_20mhz[] = {32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 96, 100,
 	104, 108, 112, 116, 120, 124, 128, 132, 136, 140, 144, 149, 153, 157, 159, 161,
 	163, 165, 167, 169, 171, 173, 175, 177};
+#endif /* CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT */
 
 static enum wifi_frequency_bands wifi_utils_map_band_str_to_idx(char *band_str)
 {
@@ -43,7 +45,7 @@ static enum wifi_frequency_bands wifi_utils_map_band_str_to_idx(char *band_str)
 	return band;
 }
 
-
+#ifdef CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT
 static bool wifi_utils_validate_chan_2g(uint16_t chan)
 {
 	if ((chan >= 1) && (chan <= 14)) {
@@ -52,7 +54,6 @@ static bool wifi_utils_validate_chan_2g(uint16_t chan)
 
 	return false;
 }
-
 
 static bool wifi_utils_validate_chan_5g(uint16_t chan)
 {
@@ -67,7 +68,6 @@ static bool wifi_utils_validate_chan_5g(uint16_t chan)
 	return false;
 }
 
-
 static bool wifi_utils_validate_chan_6g(uint16_t chan)
 {
 	if (((chan >= 1) && (chan <= 233) && (!((chan - 1)%4))) ||
@@ -77,7 +77,6 @@ static bool wifi_utils_validate_chan_6g(uint16_t chan)
 
 	return false;
 }
-
 
 static bool wifi_utils_validate_chan(uint8_t band,
 				     uint16_t chan)
@@ -101,7 +100,6 @@ static bool wifi_utils_validate_chan(uint8_t band,
 
 	return result;
 }
-
 
 static int wifi_utils_get_all_chans_in_range(uint8_t chan_start,
 		uint8_t chan_end,
@@ -192,7 +190,6 @@ static int wifi_utils_get_all_chans_in_range(uint8_t chan_start,
 	return 0;
 }
 
-
 static int wifi_utils_validate_chan_str(char *chan_str)
 {
 	uint8_t i;
@@ -211,103 +208,6 @@ static int wifi_utils_validate_chan_str(char *chan_str)
 
 	return 0;
 }
-
-
-int wifi_utils_parse_scan_bands(char *scan_bands_str, uint8_t *band_map)
-{
-	char parse_str[WIFI_MGMT_BAND_STR_SIZE_MAX + 1];
-	char *band_str = NULL;
-	char *ctx = NULL;
-	enum wifi_frequency_bands band = WIFI_FREQ_BAND_UNKNOWN;
-	int len;
-
-	if (!scan_bands_str) {
-		return -EINVAL;
-	}
-
-	len = strlen(scan_bands_str);
-
-	if (len > WIFI_MGMT_BAND_STR_SIZE_MAX) {
-		NET_ERR("Band string (%s) size (%d) exceeds maximum allowed value (%d)",
-			scan_bands_str,
-			len,
-			WIFI_MGMT_BAND_STR_SIZE_MAX);
-		return -EINVAL;
-	}
-
-	strncpy(parse_str, scan_bands_str, len);
-	parse_str[len] = '\0';
-
-	band_str = strtok_r(parse_str, ",", &ctx);
-
-	while (band_str) {
-		band = wifi_utils_map_band_str_to_idx(band_str);
-
-		if (band == WIFI_FREQ_BAND_UNKNOWN) {
-			NET_ERR("Invalid band value: %s", band_str);
-			return -EINVAL;
-		}
-
-		*band_map |= (1 << band);
-
-		band_str = strtok_r(NULL, ",", &ctx);
-	}
-
-	return 0;
-}
-
-int wifi_utils_parse_scan_ssids(char *scan_ssids_str,
-				char ssids[][WIFI_SSID_MAX_LEN + 1])
-{
-	char parse_str[(WIFI_MGMT_SCAN_SSID_FILT_MAX * (WIFI_SSID_MAX_LEN + 1)) + 1];
-	char *ssid = NULL;
-	char *ctx = NULL;
-	uint8_t i = 0;
-	int len;
-
-	if (!scan_ssids_str) {
-		return -EINVAL;
-	}
-
-	len = strlen(scan_ssids_str);
-
-	if (len > (WIFI_MGMT_SCAN_SSID_FILT_MAX * (WIFI_SSID_MAX_LEN + 1))) {
-		NET_ERR("SSID string (%s) size (%d) exceeds maximum allowed value (%d)",
-			scan_ssids_str,
-			len,
-			(WIFI_MGMT_SCAN_SSID_FILT_MAX * (WIFI_SSID_MAX_LEN + 1)));
-		return -EINVAL;
-	}
-
-	strncpy(parse_str, scan_ssids_str, len);
-	parse_str[len] = '\0';
-
-	ssid = strtok_r(parse_str, ",", &ctx);
-
-	while (ssid) {
-		if (strlen(ssid) > WIFI_SSID_MAX_LEN) {
-			NET_ERR("SSID length (%zu) exceeds maximum value (%d) for SSID %s",
-				strlen(ssid),
-				WIFI_SSID_MAX_LEN,
-				ssid);
-			return -EINVAL;
-		}
-
-		if (i >= WIFI_MGMT_SCAN_SSID_FILT_MAX) {
-			NET_WARN("Exceeded maximum allowed (%d) SSIDs. Ignoring SSIDs %s onwards",
-				 WIFI_MGMT_SCAN_SSID_FILT_MAX,
-				 ssid);
-			break;
-		}
-
-		strcpy(&ssids[i++][0], ssid);
-
-		ssid = strtok_r(NULL, ",", &ctx);
-	}
-
-	return 0;
-}
-
 
 int wifi_utils_parse_scan_chan(char *scan_chan_str,
 			       uint8_t chan[][WIFI_CHANNEL_MAX])
@@ -427,6 +327,102 @@ int wifi_utils_parse_scan_chan(char *scan_chan_str,
 	if (!valid_chan) {
 		NET_ERR("No valid channel found");
 		return -EINVAL;
+	}
+
+	return 0;
+}
+#endif /* CONFIG_WIFI_MGMT_SCAN_CHAN_SUPPORT */
+
+int wifi_utils_parse_scan_bands(char *scan_bands_str, uint8_t *band_map)
+{
+	char parse_str[WIFI_MGMT_BAND_STR_SIZE_MAX + 1];
+	char *band_str = NULL;
+	char *ctx = NULL;
+	enum wifi_frequency_bands band = WIFI_FREQ_BAND_UNKNOWN;
+	int len;
+
+	if (!scan_bands_str) {
+		return -EINVAL;
+	}
+
+	len = strlen(scan_bands_str);
+
+	if (len > WIFI_MGMT_BAND_STR_SIZE_MAX) {
+		NET_ERR("Band string (%s) size (%d) exceeds maximum allowed value (%d)",
+			scan_bands_str,
+			len,
+			WIFI_MGMT_BAND_STR_SIZE_MAX);
+		return -EINVAL;
+	}
+
+	strncpy(parse_str, scan_bands_str, len);
+	parse_str[len] = '\0';
+
+	band_str = strtok_r(parse_str, ",", &ctx);
+
+	while (band_str) {
+		band = wifi_utils_map_band_str_to_idx(band_str);
+
+		if (band == WIFI_FREQ_BAND_UNKNOWN) {
+			NET_ERR("Invalid band value: %s", band_str);
+			return -EINVAL;
+		}
+
+		*band_map |= (1 << band);
+
+		band_str = strtok_r(NULL, ",", &ctx);
+	}
+
+	return 0;
+}
+
+int wifi_utils_parse_scan_ssids(char *scan_ssids_str,
+				char ssids[][WIFI_SSID_MAX_LEN + 1])
+{
+	char parse_str[(WIFI_MGMT_SCAN_SSID_FILT_MAX * (WIFI_SSID_MAX_LEN + 1)) + 1];
+	char *ssid = NULL;
+	char *ctx = NULL;
+	uint8_t i = 0;
+	int len;
+
+	if (!scan_ssids_str) {
+		return -EINVAL;
+	}
+
+	len = strlen(scan_ssids_str);
+
+	if (len > (WIFI_MGMT_SCAN_SSID_FILT_MAX * (WIFI_SSID_MAX_LEN + 1))) {
+		NET_ERR("SSID string (%s) size (%d) exceeds maximum allowed value (%d)",
+			scan_ssids_str,
+			len,
+			(WIFI_MGMT_SCAN_SSID_FILT_MAX * (WIFI_SSID_MAX_LEN + 1)));
+		return -EINVAL;
+	}
+
+	strncpy(parse_str, scan_ssids_str, len);
+	parse_str[len] = '\0';
+
+	ssid = strtok_r(parse_str, ",", &ctx);
+
+	while (ssid) {
+		if (strlen(ssid) > WIFI_SSID_MAX_LEN) {
+			NET_ERR("SSID length (%zu) exceeds maximum value (%d) for SSID %s",
+				strlen(ssid),
+				WIFI_SSID_MAX_LEN,
+				ssid);
+			return -EINVAL;
+		}
+
+		if (i >= WIFI_MGMT_SCAN_SSID_FILT_MAX) {
+			NET_WARN("Exceeded maximum allowed (%d) SSIDs. Ignoring SSIDs %s onwards",
+				 WIFI_MGMT_SCAN_SSID_FILT_MAX,
+				 ssid);
+			break;
+		}
+
+		strcpy(&ssids[i++][0], ssid);
+
+		ssid = strtok_r(NULL, ",", &ctx);
 	}
 
 	return 0;