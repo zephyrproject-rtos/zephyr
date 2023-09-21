093e4508c182328e6ac821a94d19e9cd2ade64a8
Amrith Venkat Kesavamoorthi <amrith@mr-beam.org>
2023-09-21 16:46:59 +0200

    drivers: display: GC9A01A: resolve compliance checks
    Update yaml liniting and C formatting
    
    Signed-off-by: Amrith Venkat Kesavamoorthi <amrith@mr-beam.org>

--- drivers/display/display_gc9a01a.c
+++ drivers/display/display_gc9a01a.c
@@ -4,9 +4,9 @@
  * Copyright (c) 2020 Teslabs Engineering S.L.
  * Copyright (c) 2021 Krivorot Oleg <krivorot.oleg@gmail.com>
  * Copyright (c) 2023 Mr Beam Lasers GmbH.
- * Copyright (c) 2023 Amrith Venkat Kesavamoorthi <amrith@mr-beam.org> 
+ * Copyright (c) 2023 Amrith Venkat Kesavamoorthi <amrith@mr-beam.org>
  * SPDX-License-Identifier: Apache-2.0
- * 
+ *
  * @see https://www.buydisplay.com/download/ic/GC9A01A.pdf
  */
 
@@ -34,23 +34,25 @@ struct gc9a01a_data {
  */
 int gc9a01a_regs_init(const struct device *dev)
 {
-	
-    const struct gc9a01a_config *config = dev->config;
+
+	const struct gc9a01a_config *config = dev->config;
 	const struct gc9a01a_regs *regs = config->regs;
 
-    //https://github.com/jakkra/ZSWatch init logic updated from this repo
-	const uint8_t *addr=regs->reg_arr;
+	// https://github.com/jakkra/ZSWatch init logic updated from this repo
+	const uint8_t *addr = regs->reg_arr;
 	uint8_t cmd, x, numArgs;
-    while ((cmd = *addr++) > 0) {
-        x = *addr++;
-        numArgs = x & 0x7F;
-        gc9a01a_transmit(dev, cmd, addr, numArgs);
-        addr += numArgs;
-        if (x & 0x80) {
-            k_msleep(GC9A01A_SLEEP_OUT_TIME+30); // 30ms on top of the 120ms sleepout time to account for any manufacturing defects.
-        }
-    }
-    return 0;
+	while ((cmd = *addr++) > 0) {
+		x = *addr++;
+		numArgs = x & 0x7F;
+		gc9a01a_transmit(dev, cmd, addr, numArgs);
+		addr += numArgs;
+		if (x & 0x80) {
+			k_msleep(GC9A01A_SLEEP_OUT_TIME +
+				 30); // 30ms on top of the 120ms sleepout time to account for any
+				      // manufacturing defects.
+		}
+	}
+	return 0;
 }
 
 /**
@@ -62,14 +64,13 @@ int gc9a01a_regs_init(const struct device *dev)
  * @param tx_len Length of buffer to be sent.
  * @return 0 on success, errno otherwise.
  */
-int gc9a01a_transmit(const struct device *dev, uint8_t cmd, const void *tx_data,
-		     size_t tx_len)
+int gc9a01a_transmit(const struct device *dev, uint8_t cmd, const void *tx_data, size_t tx_len)
 {
 	const struct gc9a01a_config *config = dev->config;
 
 	int r;
 	struct spi_buf tx_buf;
-	struct spi_buf_set tx_bufs = { .buffers = &tx_buf, .count = 1U };
+	struct spi_buf_set tx_bufs = {.buffers = &tx_buf, .count = 1U};
 
 	/* send command */
 	tx_buf.buf = &cmd;
@@ -120,7 +121,7 @@ static int gc9a01a_exit_sleep(const struct device *dev)
  * @brief To turn off the sleep mode.
  *
  * @param dev GC9A01A device instance.
- * @return 0 when succesful, errno otherwise.
+ * @return 0 when successful, errno otherwise.
  */
 static int gc9a01a_hw_reset(const struct device *dev)
 {
@@ -143,7 +144,7 @@ static int gc9a01a_hw_reset(const struct device *dev)
  * @brief To recover from the sleep mode
  *
  * @param dev GC9A01A device instance.
- * @return 0 when succesful, errno otherwise.
+ * @return 0 when successful, errno otherwise.
  */
 static int gc9a01a_display_blanking_off(const struct device *dev)
 {
@@ -155,7 +156,7 @@ static int gc9a01a_display_blanking_off(const struct device *dev)
  * @brief To enter into DISPLAY OFF mode
  *
  * @param dev GC9A01A device instance.
- * @return 0 when succesful, errno otherwise.
+ * @return 0 when successful, errno otherwise.
  */
 static int gc9a01a_display_blanking_on(const struct device *dev)
 {
@@ -164,14 +165,14 @@ static int gc9a01a_display_blanking_on(const struct device *dev)
 }
 
 /**
- * @brief To set the pixel format 
+ * @brief To set the pixel format
  *
  * @param dev GC9A01A device instance.
  * @param pixel_format Display pixel format
- * @return 0 when succesful, errno otherwise.
+ * @return 0 when successful, errno otherwise.
  */
 static int gc9a01a_set_pixel_format(const struct device *dev,
-			 const enum display_pixel_format pixel_format)
+				    const enum display_pixel_format pixel_format)
 {
 	struct gc9a01a_data *data = dev->data;
 
@@ -206,7 +207,7 @@ static int gc9a01a_set_pixel_format(const struct device *dev,
  *
  * @param dev GC9A01A device instance.
  * @param orientation Display orientation
- * @return 0 when succesful, errno otherwise.
+ * @return 0 when successful, errno otherwise.
  */
 static int gc9a01a_set_orientation(const struct device *dev,
 				   const enum display_orientation orientation)
@@ -219,11 +220,11 @@ static int gc9a01a_set_orientation(const struct device *dev,
 	if (orientation == DISPLAY_ORIENTATION_NORMAL) { // works 0
 		tx_data |= 0;
 	} else if (orientation == DISPLAY_ORIENTATION_ROTATED_90) { // works CW 90
-		tx_data |= GC9A01A_MADCTL_MV | GC9A01A_MADCTL_MY ;
+		tx_data |= GC9A01A_MADCTL_MV | GC9A01A_MADCTL_MY;
 	} else if (orientation == DISPLAY_ORIENTATION_ROTATED_180) { // works CW 180
 		tx_data |= GC9A01A_MADCTL_MY | GC9A01A_MADCTL_MX | GC9A01A_MADCTL_MH;
 	} else if (orientation == DISPLAY_ORIENTATION_ROTATED_270) { // works CW 270
-		tx_data |= GC9A01A_MADCTL_MV | GC9A01A_MADCTL_MX ;
+		tx_data |= GC9A01A_MADCTL_MV | GC9A01A_MADCTL_MX;
 	}
 
 	r = gc9a01a_transmit(dev, GC9A01A_MADCTL, &tx_data, 1U);
@@ -240,7 +241,7 @@ static int gc9a01a_set_orientation(const struct device *dev,
  * @brief To do overall display device configuration.
  *
  * @param dev GC9A01A device instance.
- * @return 0 when succesful, errno otherwise.
+ * @return 0 when successful, errno otherwise.
  */
 static int gc9a01a_configure(const struct device *dev)
 {
@@ -298,28 +299,26 @@ static int gc9a01a_configure(const struct device *dev)
  *
  * @param dev GC9A01A device instance.
  * @param brightness Brightness in steps of 1/256.
- * @return 0 when succesful, errno otherwise.
+ * @return 0 when successful, errno otherwise.
  */
-static int gc9a01a_set_brightness(const struct device *dev,
-				  const uint8_t brightness)
+static int gc9a01a_set_brightness(const struct device *dev, const uint8_t brightness)
 {
 	const struct gc9a01a_config *config = dev->config;
-	uint32_t step=config->backlight.period/100;
-	int ret = pwm_set_pulse_dt(&config->backlight,brightness*step/2.55);
+	uint32_t step = config->backlight.period / 100;
+	int ret = pwm_set_pulse_dt(&config->backlight, brightness * step / 2.55);
 
-	if(ret){
+	if (ret) {
 		LOG_ERR("Failed to set pulse width");
 		return ret;
 	}
 	return 0;
-
 }
 
 /**
  * @brief To initialize the peripherals associated with the display.
  *
  * @param dev GC9A01A device instance.
- * @return 0 when succesful, errno otherwise.
+ * @return 0 when successful, errno otherwise.
  */
 static int gc9a01a_init(const struct device *dev)
 {
@@ -327,7 +326,7 @@ static int gc9a01a_init(const struct device *dev)
 
 	int r;
 
-	if (!spi_is_ready_dt(&config->spi)) { 
+	if (!spi_is_ready_dt(&config->spi)) {
 		LOG_ERR("SPI device is not ready");
 		return -ENODEV;
 	}
@@ -374,7 +373,7 @@ static int gc9a01a_init(const struct device *dev)
 		return r;
 	}
 
-	r=gc9a01a_set_brightness(dev,50);
+	r = gc9a01a_set_brightness(dev, 50);
 	if (r < 0) {
 		LOG_ERR("Could not set brightness (%d)", r);
 		return r;
@@ -391,11 +390,10 @@ static int gc9a01a_init(const struct device *dev)
  * @param y	end point of the window.
  * @param w	width point of the window.
  * @param h	height point of the window.
- * @return 0 when succesful, errno otherwise.
+ * @return 0 when successful, errno otherwise.
  */
-static int gc9a01a_set_mem_area(const struct device *dev, const uint16_t x,
-				const uint16_t y, const uint16_t w,
-				const uint16_t h)
+static int gc9a01a_set_mem_area(const struct device *dev, const uint16_t x, const uint16_t y,
+				const uint16_t w, const uint16_t h)
 {
 	int r;
 	uint16_t spi_data[2];
@@ -424,13 +422,11 @@ static int gc9a01a_set_mem_area(const struct device *dev, const uint16_t x,
  * @param x	start point of the window.
  * @param y	end point of the window.
  * @param desc pointer to the buffer descriptor.
- * @param buf pointer to the buffer to be sent 
- * @return 0 when succesful, errno otherwise.
+ * @param buf pointer to the buffer to be sent
+ * @return 0 when successful, errno otherwise.
  */
-static int gc9a01a_write(const struct device *dev, const uint16_t x,
-			 const uint16_t y,
-			 const struct display_buffer_descriptor *desc,
-			 const void *buf)
+static int gc9a01a_write(const struct device *dev, const uint16_t x, const uint16_t y,
+			 const struct display_buffer_descriptor *desc, const void *buf)
 {
 	const struct gc9a01a_config *config = dev->config;
 	struct gc9a01a_data *data = dev->data;
@@ -444,12 +440,10 @@ static int gc9a01a_write(const struct device *dev, const uint16_t x,
 	uint16_t write_h;
 
 	__ASSERT(desc->width <= desc->pitch, "Pitch is smaller than width");
-	__ASSERT((desc->pitch * data->bytes_per_pixel * desc->height) <=
-			 desc->buf_size,
+	__ASSERT((desc->pitch * data->bytes_per_pixel * desc->height) <= desc->buf_size,
 		 "Input buffer to small");
 
-	LOG_DBG("Writing %dx%d (w,h) @ %dx%d (x,y)", desc->width, desc->height,
-		x, y);
+	LOG_DBG("Writing %dx%d (w,h) @ %dx%d (x,y)", desc->width, desc->height, x, y);
 	r = gc9a01a_set_mem_area(dev, x, y, desc->width, desc->height);
 	if (r < 0) {
 		return r;
@@ -495,11 +489,10 @@ static int gc9a01a_write(const struct device *dev, const uint16_t x,
  * @param x	start point of the window.
  * @param y	end point of the window.
  * @param desc pointer to the buffer descriptor.
- * @param buf pointer to the buffer to be read 
+ * @param buf pointer to the buffer to be read
  * @return Not supported
  */
-static int gc9a01a_read(const struct device *dev, const uint16_t x,
-			const uint16_t y,
+static int gc9a01a_read(const struct device *dev, const uint16_t x, const uint16_t y,
 			const struct display_buffer_descriptor *desc, void *buf)
 {
 	LOG_ERR("Reading not supported");
@@ -525,8 +518,7 @@ static void *gc9a01a_get_framebuffer(const struct device *dev)
  * @param contrast  Contrast in steps of 1/256.
  * @return Not supported
  */
-static int gc9a01a_set_contrast(const struct device *dev,
-				const uint8_t contrast)
+static int gc9a01a_set_contrast(const struct device *dev, const uint8_t contrast)
 {
 	LOG_ERR("Set contrast not supported");
 	return -ENOTSUP;
@@ -547,8 +539,7 @@ static void gc9a01a_get_capabilities(const struct device *dev,
 
 	memset(capabilities, 0, sizeof(struct display_capabilities));
 
-	capabilities->supported_pixel_formats =
-		PIXEL_FORMAT_RGB_565 | PIXEL_FORMAT_RGB_888;
+	capabilities->supported_pixel_formats = PIXEL_FORMAT_RGB_565 | PIXEL_FORMAT_RGB_888;
 	capabilities->current_pixel_format = data->pixel_format;
 
 	if (data->orientation == DISPLAY_ORIENTATION_NORMAL ||
@@ -577,37 +568,31 @@ static const struct display_driver_api gc9a01a_api = {
 	.set_orientation = gc9a01a_set_orientation,
 };
 
-
 #define INST_DT_GC9A01A(n) DT_INST(n, waveshare_gc9a01a)
 
-#define GC9A01A_INIT(n,t)												\
-	GC9A01A_REGS_INIT(n);												\
-	static const struct gc9a01a_config gc9a01a_config_##n = { 			\
-		.spi = SPI_DT_SPEC_GET(INST_DT_GC9A01A(n), 						\
-			SPI_OP_MODE_MASTER | SPI_WORD_SET(8),						\
-			0 ) , 														\
-		.cmd_data= GPIO_DT_SPEC_GET(INST_DT_GC9A01A(n),					\
-			cmd_data_gpios),											\
-		.reset = GPIO_DT_SPEC_GET_OR(INST_DT_GC9A01A(n),        		\
-			reset_gpios, {0}),    										\
-		.backlight = PWM_DT_SPEC_GET(DT_PROP(INST_DT_GC9A01A(n), backlight_gpios)),\
-		.pixel_format = DT_PROP(INST_DT_GC9A01A(n), pixel_format), 		\
-		.rotation= DT_PROP(INST_DT_GC9A01A(n), rotation),        		\
-		.x_resolution = DT_PROP(INST_DT_GC9A01A(n), width),            	\
-		.y_resolution = DT_PROP(INST_DT_GC9A01A(n), height),            \
-		.inversion = DT_PROP(INST_DT_GC9A01A(n), display_inversion),	\
-		.regs = &gc9a01a_regs_##n,                                   	\
-		.regs_init_fn = gc9a01a_regs_init,                            	\
-	};                                           						\
-	static struct gc9a01a_data gc9a01a_data_##n;						\
-	DEVICE_DT_DEFINE(INST_DT_GC9A01A(n), gc9a01a_init,                  \
-			    NULL, &gc9a01a_data_##n,                           		\
-			    &gc9a01a_config_##n, POST_KERNEL,                 		\
-			    CONFIG_DISPLAY_INIT_PRIORITY, &gc9a01a_api)
+#define GC9A01A_INIT(n, t)                                                                         \
+	GC9A01A_REGS_INIT(n);                                                                      \
+	static const struct gc9a01a_config gc9a01a_config_##n = {                                  \
+		.spi = SPI_DT_SPEC_GET(INST_DT_GC9A01A(n), SPI_OP_MODE_MASTER | SPI_WORD_SET(8),   \
+				       0),                                                         \
+		.cmd_data = GPIO_DT_SPEC_GET(INST_DT_GC9A01A(n), cmd_data_gpios),                  \
+		.reset = GPIO_DT_SPEC_GET_OR(INST_DT_GC9A01A(n), reset_gpios, {0}),                \
+		.backlight = PWM_DT_SPEC_GET(DT_PROP(INST_DT_GC9A01A(n), backlight_gpios)),        \
+		.pixel_format = DT_PROP(INST_DT_GC9A01A(n), pixel_format),                         \
+		.rotation = DT_PROP(INST_DT_GC9A01A(n), rotation),                                 \
+		.x_resolution = DT_PROP(INST_DT_GC9A01A(n), width),                                \
+		.y_resolution = DT_PROP(INST_DT_GC9A01A(n), height),                               \
+		.inversion = DT_PROP(INST_DT_GC9A01A(n), display_inversion),                       \
+		.regs = &gc9a01a_regs_##n,                                                         \
+		.regs_init_fn = gc9a01a_regs_init,                                                 \
+	};                                                                                         \
+	static struct gc9a01a_data gc9a01a_data_##n;                                               \
+	DEVICE_DT_DEFINE(INST_DT_GC9A01A(n), gc9a01a_init, NULL, &gc9a01a_data_##n,                \
+			 &gc9a01a_config_##n, POST_KERNEL, CONFIG_DISPLAY_INIT_PRIORITY,           \
+			 &gc9a01a_api)
 
-
-#define DT_INST_FOREACH_GC9A01A_STATUS_OKAY                         \
-	LISTIFY(DT_NUM_INST_STATUS_OKAY(waveshare_gc9a01a), GC9A01A_INIT,(;))
+#define DT_INST_FOREACH_GC9A01A_STATUS_OKAY                                                        \
+	LISTIFY(DT_NUM_INST_STATUS_OKAY(waveshare_gc9a01a), GC9A01A_INIT, (;))
 
 #ifdef CONFIG_GC9A01A
 DT_INST_FOREACH_GC9A01A_STATUS_OKAY;
