f7ae33c02068737afd1ae989548f86394a1584a9
Benjamin Cabé <benjamin@zephyrproject.org>
2023-09-25 14:13:20 +0200

    doc: extensions: Add method to get vcs link to report issue
    This adds a vcs_link_get_open_issue_url Jinja filter to craft the URL
    allowing to report an issue for a given Sphinx page
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- doc/_extensions/zephyr/vcs_link.py
+++ doc/_extensions/zephyr/vcs_link.py
@@ -8,10 +8,14 @@ SPDX-License-Identifier: Apache-2.0
 Introduction
 ============
 
-This Sphinx extension can be used to obtain the VCS URL for a given Sphinx page.
+This Sphinx extension can be used to obtain various VCS URLs for a given Sphinx page.
 This is useful, for example, when adding features like "Open on GitHub" on top
-of pages. The extension installs a Jinja filter which can be used on the
-template to obtain VCS page URLs.
+of pages.
+The extension installs the following Jinja filter:
+
+* ``vcs_link_get_blob_url``: Returns a URL to the source of a page in the VCS.
+* ``vcs_link_get_edit_url``: Returns a URL to edit the given page in the VCS.
+* ``vcs_link_get_open_issue_url``: Returns a URL to open a new issue regarding the given page.
 
 Configuration options
 =====================
@@ -26,7 +30,9 @@ Configuration options
 from functools import partial
 import os
 import re
+from textwrap import dedent
 from typing import Optional
+from urllib.parse import quote
 
 from sphinx.application import Sphinx
 
@@ -70,6 +76,40 @@ def vcs_link_get_url(app: Sphinx, pagename: str, mode: str = "blob") -> Optional
     )
 
 
+def vcs_link_get_open_issue_url(app: Sphinx, pagename: str, sha1: str) -> Optional[str]:
+    """Link to open a new Github issue regarding "pagename" with title, body, and
+    labels already pre-filled with useful information.
+
+    Args:
+        app: Sphinx instance.
+        pagename: Page name (path).
+
+    Returns:
+        URL to open a new issue if applicable, None otherwise.
+    """
+
+    if not os.path.isfile(app.env.project.doc2path(pagename)):
+        return None
+
+    title = quote(f"[doc] Issue with {pagename}")
+    labels = quote("area: Documentation")
+    body = quote(
+        dedent(
+            f"""\
+    << Please describe the issue here >>
+
+    **Environment**
+
+    * Page: `{pagename}`
+    * Version: {app.config.vcs_link_version}
+    * SHA-1: {sha1}
+    """
+        )
+    )
+
+    return f"{app.config.vcs_link_base_url}/issues/new?title={title}&labels={labels}&body={body}"
+
+
 def add_jinja_filter(app: Sphinx):
     if app.builder.name != "html":
         return
@@ -80,6 +120,10 @@ def add_jinja_filter(app: Sphinx):
 
     app.builder.templates.environment.filters["vcs_link_get_edit_url"] = partial(
         vcs_link_get_url, app, mode="edit"
+    )
+
+    app.builder.templates.environment.filters["vcs_link_get_open_issue_url"] = partial(
+        vcs_link_get_open_issue_url, app
     )
 
 
