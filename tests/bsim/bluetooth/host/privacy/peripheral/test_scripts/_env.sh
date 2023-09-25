233149a5ec945c650ba91ce002112a73c6b90594
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-09-25 14:34:24 +0200

    tests BT privacy: Simplify a bit scripts
    Reduce the unnecessary hidding and indirection,
    as it makes the scripts more difficult to follow
    for no benefit.
    If somebody wants to debug these tests, they'll want
    to see the sim_id, and won't benefit from going around
    opening files needlessly.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- tests/bsim/bluetooth/host/privacy/peripheral/test_scripts/_env.sh
+++ tests/bsim/bluetooth/host/privacy/peripheral/test_scripts/_env.sh
@@ -5,10 +5,7 @@
 set -eu
 bash_source_dir="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 
-test_name="$(basename "$(realpath "$bash_source_dir/..")")"
 bsim_bin="${BSIM_OUT_PATH}/bin"
-verbosity_level=2
 BOARD="${BOARD:-nrf52_bsim}"
-simulation_id="$test_name"
 central_exe="${bsim_bin}/bs_${BOARD}_tests_bsim_bluetooth_host_privacy_peripheral_prj_conf"
 peripheral_exe="${central_exe}"
