3b8a27887487ad40121ecea4494272d349d7a84d
Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>
2023-09-26 12:11:00 +0200

    Bluetooth: Mesh: fix static oob setting
    According to specification it is possible to provide
    static oob value with any length. Mesh should trim or
    append\prepend by zeroes if it is longer\shorter than
    required value.
    
    Signed-off-by: Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>

--- subsys/bluetooth/mesh/prov_device.c
+++ subsys/bluetooth/mesh/prov_device.c
@@ -183,9 +183,14 @@ static void prov_start(const uint8_t *data)
 	}
 
 	if (atomic_test_bit(bt_mesh_prov_link.flags, OOB_STATIC_KEY)) {
-		memcpy(bt_mesh_prov_link.auth + auth_size - bt_mesh_prov->static_val_len,
-			bt_mesh_prov->static_val, bt_mesh_prov->static_val_len);
-		memset(bt_mesh_prov_link.auth, 0, auth_size - bt_mesh_prov->static_val_len);
+
+		uint8_t tail_size = bt_mesh_prov->static_val_len < auth_size
+					    ? auth_size - bt_mesh_prov->static_val_len
+					    : 0;
+
+		memcpy(bt_mesh_prov_link.auth + tail_size, bt_mesh_prov->static_val,
+		       tail_size ? bt_mesh_prov->static_val_len : auth_size);
+		memset(bt_mesh_prov_link.auth, 0, tail_size);
 	}
 }
 
