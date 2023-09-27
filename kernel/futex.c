7b036330b020f12d203becd17038a669c71623fc
Anas Nashif <anas.nashif@intel.com>
2023-09-27 10:49:28 +0000

    syscall: rename z_object_find -> k_object_find
    Rename internal API to not use z_/Z_.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- kernel/futex.c
+++ kernel/futex.c
@@ -16,7 +16,7 @@ static struct z_futex_data *k_futex_find_data(struct k_futex *futex)
 {
 	struct k_object *obj;
 
-	obj = z_object_find(futex);
+	obj = k_object_find(futex);
 	if (obj == NULL || obj->type != K_OBJ_FUTEX) {
 		return NULL;
 	}
