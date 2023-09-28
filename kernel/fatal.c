711c17182f401101390ce30a132c531aade01bd9
Flavio Ceolin <flavio.ceolin@intel.com>
2023-09-28 16:17:06 -0700

    kernel: s/k_current_get/_current
    Running inside kernel we can use _current instead of
    k_current_get that can lead to additional function call
    checks.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- kernel/fatal.c
+++ kernel/fatal.c
@@ -101,7 +101,7 @@ void z_fatal_error(unsigned int reason, const z_arch_esf_t *esf)
 	 */
 	unsigned int key = arch_irq_lock();
 	struct k_thread *thread = IS_ENABLED(CONFIG_MULTITHREADING) ?
-			k_current_get() : NULL;
+			_current : NULL;
 
 	/* twister looks for the "ZEPHYR FATAL ERROR" string, don't
 	 * change it without also updating twister
