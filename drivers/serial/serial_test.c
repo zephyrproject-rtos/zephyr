2625e53407b5dbcebb22187d844e2bcd211bf808
Bjarki Arge Andreasen <baa@trackunit.com>
2023-09-28 17:30:24 +0200

    drivers: serial: serial_test: Patch irq_isr set to undefined
    This commit fixes an error where the irq_isr callback is set
    to an undefined variable instead of NULL.
    
    Signed-off-by: Bjarki Arge Andreasen <baa@trackunit.com>

--- drivers/serial/serial_test.c
+++ drivers/serial/serial_test.c
@@ -318,7 +318,7 @@ static int serial_vnd_callback_set(const struct device *dev, uart_callback_t cal
 	}
 
 #if defined(CONFIG_UART_EXCLUSIVE_API_CALLBACKS) && defined(CONFIG_UART_INTERRUPT_DRIVEN)
-	data->irq_isr = cb;
+	data->irq_isr = NULL;
 #endif
 
 	if (callback == NULL && data->read_buf) {
