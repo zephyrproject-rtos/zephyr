3b12e3ec1f4ccaed390959f5339a4dd6ce76be2b
Erwan Gouriou <erwan.gouriou@st.com>
2023-10-02 15:10:08 +0200

    boards: nucleo_wb55rg: Use stm32cubeprogrammer as default runner
    When playing with PM related applications, stm32cubeprogrammer is
    useful to allow flashing even when SoC is in Stop mode.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@st.com>

--- boards/arm/nucleo_wb55rg/board.cmake
+++ boards/arm/nucleo_wb55rg/board.cmake
@@ -2,6 +2,6 @@
 board_runner_args(pyocd "--target=stm32wb55rgvx")
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
