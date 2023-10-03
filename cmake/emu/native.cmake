931586178862ed484387c8b32f923bfc286c95a2
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-10-03 10:53:57 +0200

    cmake run_native: Fix target dependency
    The run_native (and therefore run) targets did not depend
    on the proper thing for native_simulator based targets
    
    Fix it.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- cmake/emu/native.cmake
+++ cmake/emu/native.cmake
@@ -3,7 +3,9 @@
 add_custom_target(run_native
   COMMAND
   ${APPLICATION_BINARY_DIR}/zephyr/${KERNEL_EXE_NAME}
-  DEPENDS ${logical_target_for_zephyr_elf}
+  DEPENDS
+    ${logical_target_for_zephyr_elf}
+    $<$<TARGET_EXISTS:native_runner_executable>:native_runner_executable>
   WORKING_DIRECTORY ${APPLICATION_BINARY_DIR}
   USES_TERMINAL
   )
