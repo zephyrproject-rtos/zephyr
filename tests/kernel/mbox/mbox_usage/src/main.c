1393929ca889748391e31f51fa89b5f0735c4500
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2023-10-04 15:34:10 +0300

    tests: k_mbox: Simplify code and check return code
    k_mbox_get() / k_mbox_put() get timeout as is, so there is no need for
    extra code. Also check that k_mbox_put() actually sends message.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- tests/kernel/mbox/mbox_usage/src/main.c
+++ tests/kernel/mbox/mbox_usage/src/main.c
@@ -32,6 +32,7 @@ static enum mmsg_type {
 static void msg_sender(struct k_mbox *pmbox, k_timeout_t timeout)
 {
 	static struct k_mbox_msg mmsg;
+	int ret;
 
 	(void)memset(&mmsg, 0, sizeof(mmsg));
 
@@ -41,13 +42,9 @@ static void msg_sender(struct k_mbox *pmbox, k_timeout_t timeout)
 		mmsg.info = PUT_GET_NULL;
 		mmsg.size = 0;
 		mmsg.tx_data = NULL;
-		if (K_TIMEOUT_EQ(timeout, K_FOREVER)) {
-			k_mbox_put(pmbox, &mmsg, K_FOREVER);
-		} else if (K_TIMEOUT_EQ(timeout, K_NO_WAIT)) {
-			k_mbox_put(pmbox, &mmsg, K_NO_WAIT);
-		} else {
-			k_mbox_put(pmbox, &mmsg, timeout);
-		}
+
+		ret = k_mbox_put(pmbox, &mmsg, timeout);
+		zassert_ok(ret, "k_mbox_put() failed, ret %d", ret);
 		break;
 	default:
 		break;
@@ -59,20 +56,20 @@ static void msg_receiver(struct k_mbox *pmbox, k_tid_t thd_id,
 {
 	static struct k_mbox_msg mmsg;
 	static char rxdata[MAIL_LEN];
+	int ret;
 
 	switch (info_type) {
 	case PUT_GET_NULL:
 		mmsg.size = sizeof(rxdata);
 		mmsg.rx_source_thread = thd_id;
+
+		ret = k_mbox_get(pmbox, &mmsg, rxdata, timeout);
 		if (K_TIMEOUT_EQ(timeout, K_FOREVER)) {
-			zassert_true(k_mbox_get(pmbox, &mmsg,
-				     rxdata, K_FOREVER) == 0, NULL);
+			zassert_ok(ret, "k_mbox_get() ret %d", ret);
 		} else if (K_TIMEOUT_EQ(timeout, K_NO_WAIT)) {
-			zassert_false(k_mbox_get(pmbox, &mmsg,
-				      rxdata, K_NO_WAIT) == 0, NULL);
+			zassert_false(ret == 0, "k_mbox_get() ret %d", ret);
 		} else {
-			zassert_true(k_mbox_get(pmbox, &mmsg,
-				     rxdata, timeout) == 0, NULL);
+			zassert_ok(ret, "k_mbox_get() ret %d", ret);
 		}
 		break;
 	default:
