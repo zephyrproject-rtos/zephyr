db8855aaa3f32d3ba1f4e4e9066eb953c4406e25
Jun Lin <CHLin56@nuvoton.com>
2023-10-04 13:22:41 +0800

    driver: crypto: SHA: npcx: change to support npcx4
    The pre-alloacted size of the buffer for the SHA ROM API code increases
    in npcx4 chip. This commit adds a new property context-buffer-size to
    sha0 DT node in npcx9 and npcx4 separately. The driver can pre-allocate
    buffer with the correct size based on the property.
    
    Signed-off-by: Jun Lin <CHLin56@nuvoton.com>

--- drivers/crypto/crypto_npcx_sha.c
+++ drivers/crypto/crypto_npcx_sha.c
@@ -14,7 +14,7 @@
 LOG_MODULE_REGISTER(sha_npcx, CONFIG_CRYPTO_LOG_LEVEL);
 
 #define NPCX_HASH_CAPS_SUPPORT	(CAP_SEPARATE_IO_BUFS | CAP_SYNC_OPS)
-#define NPCX_SHA256_HANDLE_SIZE 212
+#define NPCX_SHA256_HANDLE_SIZE DT_INST_PROP(0, context_buffer_size)
 #define NPCX_SHA_MAX_SESSION	1
 
 /* The status code returns from Nuvoton Cryptographic Library ROM APIs */
