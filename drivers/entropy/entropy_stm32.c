009025d45d384fd1eacf3669c9a859627058b684
Carles Cufi <carles.cufi@nordicsemi.no>
2023-10-05 12:55:24 +0200

    [nrf fromtree] Bluetooth: Controller: Remove legacy BT_CTLR_FAST_ENC option
    When the legacy LLCP implementation was removed this Kconfig option was
    mistakenly left over. Remove it now with all its users.
    
    Fixes #63212.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    (cherry picked from commit a5e7ba356465cdf0b7f3387f5b46ddc016339250)
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>
    (cherry picked from commit 62cc15553b6fc153b945c515a11a4976fbd22763)

--- drivers/entropy/entropy_stm32.c
+++ drivers/entropy/entropy_stm32.c
@@ -377,13 +377,7 @@ static void pool_filling_work_handler(struct k_work *work)
 	}
 }
 
-#if defined(CONFIG_BT_CTLR_FAST_ENC)
-#define __fast __attribute__((optimize("Ofast")))
-#else
-#define __fast
-#endif
-
-__fast static uint16_t rng_pool_get(struct rng_pool *rngp, uint8_t *buf,
+static uint16_t rng_pool_get(struct rng_pool *rngp, uint8_t *buf,
 	uint16_t len)
 {
 	uint32_t last  = rngp->last;
@@ -448,7 +442,6 @@ __fast static uint16_t rng_pool_get(struct rng_pool *rngp, uint8_t *buf,
 
 	return len;
 }
-#undef __fast
 
 static int rng_pool_put(struct rng_pool *rngp, uint8_t byte)
 {
