f4106fd9c02862389deac0f25a07fef3cf26d616
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2023-10-09 17:18:34 +0300

    net: shell: Print v4-mapping-to-v6 address properly
    The remote address of the connection is checked whether
    it is v4-mapping-to-v6 address in which case we should
    print it such.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- subsys/net/lib/shell/net_shell.c
+++ subsys/net/lib/shell/net_shell.c
@@ -99,10 +99,21 @@ void get_addresses(struct net_context *context,
 			 net_sprint_ipv4_addr(
 				 net_sin_ptr(&context->local)->sin_addr),
 			 ntohs(net_sin_ptr(&context->local)->sin_port));
-		snprintk(addr_remote, remote_len, "%s:%d",
-			 net_sprint_ipv4_addr(
-				 &net_sin(&context->remote)->sin_addr),
-			 ntohs(net_sin(&context->remote)->sin_port));
+
+		/* Check if we need to print the v4-mapping-to-v6 address */
+		if (IS_ENABLED(CONFIG_NET_IPV4_MAPPING_TO_IPV6) &&
+		    net_sin(&context->remote)->sin_family == AF_INET6 &&
+		    net_ipv6_addr_is_v4_mapped(&net_sin6(&context->remote)->sin6_addr)) {
+			snprintk(addr_remote, remote_len, "[%s]:%d",
+				 net_sprint_ipv6_addr(
+					 &net_sin6(&context->remote)->sin6_addr),
+				 ntohs(net_sin6(&context->remote)->sin6_port));
+		} else {
+			snprintk(addr_remote, remote_len, "%s:%d",
+				 net_sprint_ipv4_addr(
+					 &net_sin(&context->remote)->sin_addr),
+				 ntohs(net_sin(&context->remote)->sin_port));
+		}
 
 	} else if (context->local.family == AF_UNSPEC) {
 		snprintk(addr_local, local_len, "AF_UNSPEC");
