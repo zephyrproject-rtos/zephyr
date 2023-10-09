546a64065773841af565771fe968f5c72ab3a2f3
Ioannis Karachalios <ioannis.karachalios.px@renesas.com>
2023-10-09 10:58:56 +0300

    drivers: dma: smartbond: Support DMA accelerator.
    Add support for the DMA engine.
    
    Signed-off-by: Ioannis Karachalios <ioannis.karachalios.px@renesas.com>

---
+++ include/zephyr/drivers/dma/dma_smartbond.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2023 Renesas Electronics Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef DMA_SMARTBOND_H_
+#define DMA_SMARTBOND_H_
+
+/**
+ * @brief Vendror-specific DMA peripheral triggering sources.
+ *
+ * A valid triggering source should be provided when DMA
+ * is configured for peripheral to peripheral or memory to peripheral
+ * transactions.
+ */
+enum dma_smartbond_trig_mux {
+	DMA_SMARTBOND_TRIG_MUX_SPI   = 0x0,
+	DMA_SMARTBOND_TRIG_MUX_SPI2  = 0x1,
+	DMA_SMARTBOND_TRIG_MUX_UART  = 0x2,
+	DMA_SMARTBOND_TRIG_MUX_UART2 = 0x3,
+	DMA_SMARTBOND_TRIG_MUX_I2C   = 0x4,
+	DMA_SMARTBOND_TRIG_MUX_I2C2  = 0x5,
+	DMA_SMARTBOND_TRIG_MUX_USB   = 0x6,
+	DMA_SMARTBOND_TRIG_MUX_UART3 = 0x7,
+	DMA_SMARTBOND_TRIG_MUX_PCM   = 0x8,
+	DMA_SMARTBOND_TRIG_MUX_SRC   = 0x9,
+	DMA_SMARTBOND_TRIG_MUX_GPADC = 0xC,
+	DMA_SMARTBOND_TRIG_MUX_SDADC = 0xD,
+	DMA_SMARTBOND_TRIG_MUX_NONE  = 0xF
+};
+
+#endif /* DMA_SMARTBOND_H_ */
