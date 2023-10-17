41ef97640a089357647a402bc3fe5f8b3bdee7bb
Adrien Bruant <adrien.bruant@aalberts-hfc.com>
2023-10-17 15:13:51 +0200

    drivers: bbram: stm32-bbram: port to stm32wl
    On STM32WL, the backup memory is defined as part of the TAMP peripheral.
    This seems to be a deviation from the stm32 family where this memory is
    defined as part of the RTC.
    
    The STM32WL reference manual shows that tamp_pclk is connected to
    rtc_pclk. This means that the clock required to run the TAMP peripheral
    is the same as the RTC's. A quick port of BBRAM on STM32WL is achieved
    by instanciating the bbram device as a child of the RTC and by modifying
    the address offset to the first backup register from the rtc base
    address.
    
    Signed-off-by: Adrien Bruant <adrien.bruant@aalberts-hfc.com>

--- drivers/bbram/bbram_stm32.c
+++ drivers/bbram/bbram_stm32.c
@@ -14,7 +14,14 @@
 LOG_MODULE_REGISTER(bbram, CONFIG_BBRAM_LOG_LEVEL);
 
 #define STM32_BKP_REG_BYTES		 4
-#define STM32_BKP_REG_OFFSET		 0x50
+#ifdef TAMP
+/* If a SoC has a TAMP peripherals, then the backup registers are defined there,
+ * not in the RTC.
+ */
+#define STM32_BKP_REG_OFFSET		 (TAMP_BASE + offsetof(TAMP_TypeDef, BKP0R) - RTC_BASE)
+#else
+#define STM32_BKP_REG_OFFSET		 offsetof(RTC_TypeDef, BKP0R)
+#endif
 #define STM32_BKP_REG_INDEX(offset)	 ((offset) >> 2)
 #define STM32_BKP_REG_BYTE_INDEX(offset) ((offset)&0x3UL)
 #define STM32_BKP_REG(i)		 (((volatile uint32_t *)config->base_addr)[(i)])
