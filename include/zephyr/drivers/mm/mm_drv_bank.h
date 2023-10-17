9c2e1d72461ff6d66e3551ec384c3037dcb0366b
Daniel Leung <daniel.leung@intel.com>
2023-10-17 14:45:54 -0700

    mm: polish doxygen doc on memory banks driver APIs
    () Put the memory bank APIs under the memory management group.
    () Document the struct sys_mm_drv_bank.
    () Put proper in,out for function parameters.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- include/zephyr/drivers/mm/mm_drv_bank.h
+++ include/zephyr/drivers/mm/mm_drv_bank.h
@@ -9,9 +9,10 @@
  * @brief Memory Banks Driver APIs
  *
  * This contains generic APIs to be used by a system-wide memory management
- * driver to track page usage within memory banks. It is incumbent upon the
- * caller to ensure that proper locking is used to protect the data when
- * using these APIs.
+ * driver to track page usage within memory banks.
+ *
+ * @note The caller of these functions needs to ensure proper locking
+ *       to protect the data when using these APIs.
  */
 
 #ifndef ZEPHYR_INCLUDE_DRIVERS_MM_DRV_BANK_H
@@ -21,9 +22,31 @@
 #include <zephyr/sys/mem_stats.h>
 #include <stdint.h>
 
+/**
+ * @brief Memory Banks Driver APIs
+ * @defgroup mm_drv_bank_apis Memory Banks Driver APIs
+ *
+ * This contains APIs for a system-wide memory management driver to
+ * track page usage within memory banks.
+ *
+ * @note The caller of these functions needs to ensure proper locking
+ *       to protect the data when using these APIs.
+ *
+ * @ingroup memory_management
+ * @{
+ */
+
+/**
+ * @brief Information about memory banks.
+ */
 struct sys_mm_drv_bank {
+	/** Number of unmapped pages. */
 	uint32_t  unmapped_pages;
+
+	/** Number of mapped pages. */
 	uint32_t  mapped_pages;
+
+	/** Maximum number of mapped pages since last counter reset. */
 	uint32_t  max_mapped_pages;
 };
 
@@ -36,8 +59,8 @@ struct sys_mm_drv_bank {
  * it will start with all pages mapped. In next phase of driver initialization
  * unused pages will be unmapped.
  *
- * @param bank Pointer to the memory bank structure used for tracking
- * @param bank_pages Number of pages in the memory bank
+ * @param[in,out] bank Pointer to the memory bank structure used for tracking
+ * @param[in] bank_pages Number of pages in the memory bank
  */
 void sys_mm_drv_bank_init(struct sys_mm_drv_bank *bank, uint32_t bank_pages);
 
@@ -47,7 +70,7 @@ void sys_mm_drv_bank_init(struct sys_mm_drv_bank *bank, uint32_t bank_pages);
  * This function is used to update the number of mapped pages within the
  * specified memory bank.
  *
- * @param bank Pointer to the memory bank's data structure
+ * @param[in,out] bank Pointer to the memory bank's data structure
  *
  * @return The number of pages mapped within the memory bank
  */
@@ -59,7 +82,7 @@ uint32_t sys_mm_drv_bank_page_mapped(struct sys_mm_drv_bank *bank);
  * This function is used to update the number of unmapped pages within the
  * specified memory bank.
  *
- * @param bank Pointer to the memory bank's data structure
+ * @param[in,out] bank Pointer to the memory bank's data structure
  *
  * @return The number of unmapped pages within the memory bank
  */
@@ -72,7 +95,7 @@ uint32_t sys_mm_drv_bank_page_unmapped(struct sys_mm_drv_bank *bank);
  * the specified memory bank to the current number of pages mapped in
  * that memory bank.
  *
- * @param bank Pointer to the memory bank's data structure
+ * @param[in,out] bank Pointer to the memory bank's data structure
  */
 void sys_mm_drv_bank_stats_reset_max(struct sys_mm_drv_bank *bank);
 
@@ -81,10 +104,14 @@ void sys_mm_drv_bank_stats_reset_max(struct sys_mm_drv_bank *bank);
  *
  * This routine extracts the system memory stats from the memory bank.
  *
- * @param bank Pointer to the memory bank's data structure
- * @param stats Pointer to memory into which to copy the system memory stats
+ * @param[in] bank Pointer to the memory bank's data structure
+ * @param[in,out] stats Pointer to memory into which to copy the system memory stats
  */
 void sys_mm_drv_bank_stats_get(struct sys_mm_drv_bank *bank,
 			       struct sys_memory_stats *stats);
+
+/**
+ * @}
+ */
 
 #endif /* ZEPHYR_INCLUDE_DRIVERS_MM_DRV_BANK_H */
