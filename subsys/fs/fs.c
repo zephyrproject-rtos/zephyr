07c1e49f20a1b7cdd0b2b08ca10d9a90183cffc0
Dominik Ermel <dominik.ermel@nordicsemi.no>
2023-10-19 16:53:17 +0000

    fs: Clarify description of fs_open when no access bits given
    Add warning that file opened without R/W flags will have no read/write
    access.
    Remove suggestion for using fs_open to check if file exists.
    Clarify -ENOENT return reason.
    
    Fixes #64030
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- subsys/fs/fs.c
+++ subsys/fs/fs.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2018 Intel Corporation.
  * Copyright (c) 2020 Peter Bigot Consulting, LLC
- * Copyright (c) 2020 Nordic Semiconductor ASA
+ * Copyright (c) 2020-2024 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
