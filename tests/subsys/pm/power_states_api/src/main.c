f077745e1ce33b6a6a572db33456d9d98a067cff
Flavio Ceolin <flavio.ceolin@intel.com>
2023-10-19 11:15:09 -0700

    tests: pm: Test pm_state_cpu_get_all
    Add test for pm_state_cpu_get_all API
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- tests/subsys/pm/power_states_api/src/main.c
+++ tests/subsys/pm/power_states_api/src/main.c
@@ -18,8 +18,24 @@ static enum pm_state states[] = {PM_STATE_SUSPEND_TO_IDLE,
 static enum pm_state wrong_states[] = {PM_STATE_SUSPEND_TO_DISK,
 		PM_STATE_SUSPEND_TO_RAM, PM_STATE_SUSPEND_TO_RAM};
 
+void pm_state_set(enum pm_state state, uint8_t substate_id)
+{
+	ARG_UNUSED(substate_id);
+	ARG_UNUSED(state);
+}
+
+void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
+{
+	ARG_UNUSED(state);
+	ARG_UNUSED(substate_id);
+
+	irq_unlock(0);
+}
+
 ZTEST(power_states_1cpu, test_power_states)
 {
+	uint8_t ret;
+	const struct pm_state_info *cpu_states;
 	enum pm_state dts_states[] =
 		PM_STATE_LIST_FROM_DT_CPU(DT_NODELABEL(cpu0));
 	struct pm_state_info dts_infos[] =
@@ -36,6 +52,15 @@ ZTEST(power_states_1cpu, test_power_states)
 
 	zassert_false(memcmp(wrong_states, dts_states, sizeof(dts_states)) == 0,
 		     "Invalid pm-states array");
+
+	ret = pm_state_cpu_get_all(CONFIG_MP_MAX_NUM_CPUS + 1, &cpu_states);
+	zassert_true(ret == 0, "Invalid pm_state_cpu_get_all return");
+
+	ret = pm_state_cpu_get_all(0U, &cpu_states);
+	zassert_true(ret == dts_states_len,
+		     "Invalid number of pm states");
+	zassert_true(memcmp(cpu_states, dts_infos, sizeof(dts_infos)) == 0,
+		     "Invalid pm_state_info array");
 }
 
 ZTEST_SUITE(power_states_1cpu, NULL, NULL, ztest_simple_1cpu_before,
