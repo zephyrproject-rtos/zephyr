dc7f3df0895b97addbf7ee94aca5f37b577164b2
Florian Grandel <fgrandel@code-for-humans.de>
2023-10-21 00:17:54 +0200

    kernel: time: header to represent generic time concepts
    The header files `zephyr/sys/time_units.h` and `zephyr/sys_clock.h` had
    both, generic time conversion and system clock specific utilities. This
    change creates a header file that will provide generic utilities to
    represent and convert time to be used independently from the system
    clock.
    
    The newly created `zephyr/sys_time.h` header must not depend on any
    other header files except for low level generic type declarations, the
    toolchain and low level utilities so that it can be included from
    `time_units.h` and `sys_clock.h` without include loops.
    
    The header is named `zephyr/sys_time.h` to avoid confusion with the
    POSIX `sys/time.h` header. The chosen name is consistent with existing
    sys_* headers.
    
    Signed-off-by: Florian Grandel <fgrandel@code-for-humans.de>

---
+++ include/zephyr/sys_time.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2014-2015 Wind River Systems, Inc.
+ * Copyright (c) 2023 Florian Grandel, Zephyr Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_SYS_TIME_H_
+#define ZEPHYR_INCLUDE_SYS_TIME_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @file
+ * @brief Types needed to represent and convert time in general.
+ *
+ * @defgroup timeutil_general_time_apis General Time Representation and Conversion Helpers
+ * @ingroup timeutil_apis
+ *
+ * @brief Various helper APIs for representing and converting between time
+ * units.
+ *
+ * @{
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_SYS_TIME_H_ */
