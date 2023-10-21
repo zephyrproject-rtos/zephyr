e858e0fcbd072141f8412a27d4f39a4f52aeea30
Chen Xingyu <hi@xingrz.me>
2023-10-21 15:14:30 +0800

    dts/soc: riscv: ch32v: Add WCH CH56x SoCs
    Based on QingKe V3A core, CH569/5 are members of the CH56x series.
    
    Signed-off-by: Chen Xingyu <hi@xingrz.me>

---
+++ include/zephyr/dt-bindings/interrupt-controller/ch56x-int.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023 Chen Xingyu <hi@xingrz.me>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DT_CH56X_INT_H_
+#define ZEPHYR_DT_CH56X_INT_H_
+
+#define CH56X_INT_WDOG   16 2
+#define CH56X_INT_TMR0   17 3
+#define CH56X_INT_GPIO   18 4
+#define CH56X_INT_SPI0   19 5
+#define CH56X_INT_USBSS  20 6
+#define CH56X_INT_LINK   21 7
+#define CH56X_INT_TMR1   22 8
+#define CH56X_INT_TMR2   23 9
+#define CH56X_INT_UART0  24 10
+#define CH56X_INT_USBHS  25 11
+#define CH56X_INT_EMMC   26 12
+#define CH56X_INT_DVP    27 13
+#define CH56X_INT_HSPI   28 14
+#define CH56X_INT_SPI1   29 15
+#define CH56X_INT_UART1  30 16
+#define CH56X_INT_UART2  31 17
+#define CH56X_INT_UART3  32 18
+#define CH56X_INT_SERDES 33 19
+#define CH56X_INT_ETH    34 20
+#define CH56X_INT_PMT    35 21
+#define CH56X_INT_ECDC   36 22
+
+#endif /* ZEPHYR_DT_CH56X_INT_H_ */
