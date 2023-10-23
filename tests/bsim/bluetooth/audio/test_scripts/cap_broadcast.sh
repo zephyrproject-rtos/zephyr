aef6544ad2f7909322e69d62dc32f5626b29a3dc
Emil Gydesen <emil.gydesen@nordicsemi.no>
2023-10-23 19:55:16 +0200

    tests: Bluetooth: Audio: Add BSIM sync API
    Add mechanism and API to sync devices via the babblesim
    backchannels.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bsim/bluetooth/audio/test_scripts/cap_broadcast.sh
+++ tests/bsim/bluetooth/audio/test_scripts/cap_broadcast.sh
@@ -15,10 +15,10 @@ cd ${BSIM_OUT_PATH}/bin
 printf "\n\n======== Running CAP broadcast test =========\n\n"
 
 Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_prj_conf \
-  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=0 -testid=cap_initiator_broadcast -rs=46
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=0 -testid=cap_initiator_broadcast -rs=46 -D=2
 
 Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_prj_conf \
-  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=1 -testid=cap_acceptor_broadcast -rs=23
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=1 -testid=cap_acceptor_broadcast -rs=23 -D=2
 
 # Simulation time should be larger than the WAIT_TIME in common.h
 Execute ./bs_2G4_phy_v1 -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} \
