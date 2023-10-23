5d577241ea5b018f54e2688f0db4c2f3e6b4110f
Emil Gydesen <emil.gydesen@nordicsemi.no>
2023-10-23 19:55:16 +0200

    tests: Bluetooth: Audio: Add BSIM sync API
    Add mechanism and API to sync devices via the babblesim
    backchannels.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bsim/bluetooth/audio/test_scripts/ias.sh
+++ tests/bsim/bluetooth/audio/test_scripts/ias.sh
@@ -15,10 +15,10 @@ cd ${BSIM_OUT_PATH}/bin
 printf "\n\n======== Running IAS main (API) test =========\n\n"
 
 Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_prj_conf \
-  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=0 -testid=ias -rs=23
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=0 -testid=ias -rs=23 -D=2
 
 Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_prj_conf \
-  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=1 -testid=ias_client -rs=6
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=1 -testid=ias_client -rs=6 -D=2
 
 # Simulation time should be larger than the WAIT_TIME in common.h
 Execute ./bs_2G4_phy_v1 -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} \
