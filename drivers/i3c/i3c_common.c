9ddc94e0d49f0e2a499bc5200ec4d275e4c1546c
Ryan McClelland <ryanmcclelland@meta.com>
2023-10-28 11:12:35 -0700

    drivers: i3c: specify start addr when searching for a free addr
    For example, if a driver needed to reserve address before it does a
    ENTDAA, it would need to get free address in a loop, but the get
    free address func would return the same address everytime. It needs
    the start address, which would be the last free address it go, to
    be passed in to get the next free address.
    
    Signed-off-by: Ryan McClelland <ryanmcclelland@meta.com>

--- drivers/i3c/i3c_common.c
+++ drivers/i3c/i3c_common.c
@@ -157,13 +157,13 @@ bool i3c_addr_slots_is_free(struct i3c_addr_slots *slots,
 	return (status == I3C_ADDR_SLOT_STATUS_FREE);
 }
 
-uint8_t i3c_addr_slots_next_free_find(struct i3c_addr_slots *slots)
+uint8_t i3c_addr_slots_next_free_find(struct i3c_addr_slots *slots, uint8_t start_addr)
 {
 	uint8_t addr;
 	enum i3c_addr_slot_status status;
 
 	/* Addresses 0 to 7 are reserved. So start at 8. */
-	for (addr = 8; addr < I3C_MAX_ADDR; addr++) {
+	for (addr = MAX(start_addr, 8); addr < I3C_MAX_ADDR; addr++) {
 		status = i3c_addr_slots_status(slots, addr);
 		if (status == I3C_ADDR_SLOT_STATUS_FREE) {
 			return addr;
@@ -252,7 +252,7 @@ int i3c_determine_default_addr(struct i3c_device_desc *target, uint8_t *addr)
 				} else {
 					/* address is not free, get the next one */
 					*addr = i3c_addr_slots_next_free_find(
-						&data->attached_dev.addr_slots);
+						&data->attached_dev.addr_slots, 0);
 				}
 			} else {
 				/* Use the init dynamic address as it's DA, but the RR will need to
@@ -281,7 +281,7 @@ int i3c_determine_default_addr(struct i3c_device_desc *target, uint8_t *addr)
 			} else {
 				/* pick a DA to use */
 				*addr = i3c_addr_slots_next_free_find(
-					&data->attached_dev.addr_slots);
+					&data->attached_dev.addr_slots, 0);
 			}
 		}
 	} else {
@@ -488,7 +488,7 @@ int i3c_dev_list_daa_addr_helper(struct i3c_addr_slots *addr_slots,
 	/*
 	 * Find the next available address.
 	 */
-	dyn_addr = i3c_addr_slots_next_free_find(addr_slots);
+	dyn_addr = i3c_addr_slots_next_free_find(addr_slots, 0);
 
 	if (dyn_addr == 0U) {
 		/* No free addresses available */
