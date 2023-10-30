5bb958a9d04f45775f7238e915d00048583caba4
Bartosz Bilas <b.bilas@grinn-global.com>
2023-10-30 12:55:22 +0100

    drivers: regulator: add MAX20335 driver
    Add a MAX20335 MFD subdriver to manage the built-in PMIC.
    
    Signed-off-by: Bartosz Bilas <b.bilas@grinn-global.com>

---
+++ include/zephyr/dt-bindings/regulator/max20335.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023 Grinn
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_REGULATOR_MAX20335_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_REGULATOR_MAX20335_H_
+
+/**
+ * @defgroup regulator_max20335 MAX20335 Devicetree helpers.
+ * @ingroup regulator_interface
+ * @{
+ */
+
+/**
+ * @name MAX20335 Regulator modes
+ * @{
+ */
+/** LDO mode */
+#define MAX20335_LDO_MODE 0
+/** Load switch mode */
+#define MAX20335_LOAD_SWITCH_MODE 1
+/** @} */
+
+/** @} */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_REGULATOR_MAX20335_H_*/
