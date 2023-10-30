86ce406ed555d32a10748f7ffef08aca07f6a05d
Chen Xingyu <hi@xingrz.me>
2023-10-30 14:05:23 +0800

    dts/soc: riscv: ch32v: Add syscon node for WCH CH56x
    This commit describes the system control block of the CH56x in DTS. This
    block will be utilized in future drivers for functions such as pin
    remapping, clock control, and interrupt management.
    
    Additionally, macros has been added to facilitate easy access to the block.
    
    Signed-off-by: Chen Xingyu <hi@xingrz.me>

--- soc/riscv/riscv-privileged/wch_ch32v/soc.h
+++ soc/riscv/riscv-privileged/wch_ch32v/soc.h
@@ -8,4 +8,19 @@
 
 #include <soc_common.h>
 
+#ifndef _ASMLANGUAGE
+
+#include <zephyr/device.h>
+
+#define CH32V_SYS_BASE DT_REG_ADDR(DT_NODELABEL(syscon))
+
+#if defined(CONFIG_SOC_CH56X)
+#include <syscon_regs_ch56x.h>
+#endif
+
+void ch32v_sys_unlock(void);
+void ch32v_sys_relock(void);
+
+#endif /* !_ASMLANGUAGE */
+
 #endif /* __SOC_H__ */
