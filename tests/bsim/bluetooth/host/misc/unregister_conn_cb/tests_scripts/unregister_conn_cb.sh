3d9f76b8e5e8a8f80e107aee4e86aa4cf85ed9ec
huajiang zheng <huajiang.zheng@nxp.com>
2023-10-31 15:01:49 +0800

    Bluetooth: Host: add unregister connection callback function
    [Description]
    tests: shell: Restart bt will register the same connection callback twice.
    Callback next node point to itself, when link established callback function
    loop infinitely.
    [Fix]
    Unregister the previous callback to avoid register repeatedly.
    [Test]
    After bt init/disable times, create connection successfully.
    
    Signed-off-by: huajiang zheng <huajiang.zheng@nxp.com>

---
+++ tests/bsim/bluetooth/host/misc/unregister_conn_cb/tests_scripts/unregister_conn_cb.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Copyright (c) 2024 NXP
+# Copyright (c) 2024 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
+
+#Unregister connection callbacks : A central device scans for and connects to a peripheral
+#after registered connection callbacks.When the connection state changes, few printing
+#will be printed and the flag_is_connected will be changed by the callback function.
+#After unregister the connection callbacks, reconnect to peer device, then no printing
+#neither flag change can be found, callback function was unregistered as expected
+simulation_id="unregister_conn_cb"
+verbosity_level=2
+EXECUTE_TIMEOUT=20
+
+cd ${BSIM_OUT_PATH}/bin
+
+bsim_exe=./bs_nrf52_bsim_tests_bsim_bluetooth_host_misc_unregister_conn_cb_prj_conf
+
+Execute "${bsim_exe}" -v=${verbosity_level} -s=${simulation_id} -d=0 -testid=central -rs=420
+Execute "${bsim_exe}" -v=${verbosity_level} -s=${simulation_id} -d=1 -testid=peripheral -rs=100
+
+Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} -D=2 -sim_length=30e6 $@
+
+wait_for_background_jobs
