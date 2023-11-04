237ec65ad365dd027552da3823da2fcc414b5138
Manuel Argüelles <manuel.arguelles@nxp.com>
2023-11-04 08:54:51 +0700

    intc: nxp_s32: use instance-based DT macros
    At present, many of the NXP S32 shim drivers do not make use of
    devicetree instance-based macros because the NXP S32 HAL relies on an
    index-based approach, requiring knowledge of the peripheral instance
    index during both compilation and runtime, and this index might not
    align with the devicetree instance index.
    
    The proposed solution in this patch eliminates this limitation by
    determining the peripheral instance index during compilation
    through macrobatics.
    
    Note that for some peripheral instances is needed to define the
    HAL macros of the peripheral base address because there are gaps
    in the instances or there are SoCs with a single instance.
    
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

--- soc/arm/nxp_s32/s32ze/soc.h
+++ soc/arm/nxp_s32/s32ze/soc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 NXP
+ * Copyright 2022-2023 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,5 +9,10 @@
 
 /* Do not let CMSIS to handle GIC */
 #define __GIC_PRESENT 0
+
+/* Aliases for peripheral base addresses */
+
+/* SIUL2 */
+#define IP_SIUL2_2_BASE         0U  /* instance does not exist on this SoC */
 
 #endif /* _NXP_S32_S32ZE_SOC_H_ */
