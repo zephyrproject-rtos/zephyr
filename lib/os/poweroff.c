dba44fc432c50207853c32358037f1cc445f0a5a
Bjarki Arge Andreasen <bjarki@arge-andreasen.me>
2023-11-05 19:29:42 +0100

    sys: poweroff: Add wakeup source configuration
    Configure all enabled wakeup sources right before powering
    down as part of the poweroff() API.
    
    Signed-off-by: Bjarki Arge Andreasen <bjarki@arge-andreasen.me>
    (cherry picked from pull/64814)

--- lib/os/poweroff.c
+++ lib/os/poweroff.c
@@ -5,10 +5,15 @@
 
 #include <zephyr/irq.h>
 #include <zephyr/sys/poweroff.h>
+#include <zephyr/sys/wakeup_source.h>
 
 void sys_poweroff(void)
 {
 	(void)irq_lock();
+
+#ifdef CONFIG_WAKEUP_SOURCE
+	z_sys_wakeup_sources_configure();
+#endif
 
 	z_sys_poweroff();
 }
