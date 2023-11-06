bf76e02d3900faab791bd4fef18faed459f12040
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2023-11-06 14:08:27 +0800

    ITE: drivers/i2c: Add a property for I2C located channel
    Add a property for I2C channel switch selection. This property will
    write to the SMBxxCHS register according to the I2C node you selected,
    which can make channel swapping.
    
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>

--- include/zephyr/dt-bindings/i2c/it8xxx2-i2c.h
+++ include/zephyr/dt-bindings/i2c/it8xxx2-i2c.h
@@ -20,4 +20,12 @@
 #define CGC_OFFSET_SMBB		((IT8XXX2_ECPM_CGCTRL4R_OFF << 8) | 0x08)
 #define CGC_OFFSET_SMBA		((IT8XXX2_ECPM_CGCTRL4R_OFF << 8) | 0x04)
 
+/* I2C channel switch selection */
+#define I2C_CHA_LOCATE		0
+#define I2C_CHB_LOCATE		1
+#define I2C_CHC_LOCATE		2
+#define I2C_CHD_LOCATE		3
+#define I2C_CHE_LOCATE		4
+#define I2C_CHF_LOCATE		5
+
 #endif	/* ZEPHYR_INCLUDE_DT_BINDINGS_I2C_IT8XXX2_H_ */
