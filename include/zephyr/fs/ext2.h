180c25019d52d531ea608d005acfa097c3c4ebbe
Benjamin Cabé <benjamin@zephyrproject.org>
2023-11-09 16:40:26 +0100

    doc: ext2: Fix doxygen documentation for ext2fs
    Moved incorrectly placed struct field doxygen comments and added
    some missing documentation tags.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- include/zephyr/fs/ext2.h
+++ include/zephyr/fs/ext2.h
@@ -4,34 +4,66 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/**
+ * @file
+ * @brief ext2 file system API
+ */
 #ifndef ZEPHYR_INCLUDE_FS_EXT2_H_
 #define ZEPHYR_INCLUDE_FS_EXT2_H_
 
 #include <stdint.h>
 
-/** @brief Configuration used to format ext2 file system.
+/**
+ * @defgroup ext2 ext2 file system API
+ * @ingroup file_system_storage
+ * @{
+ */
+
+/**
+ * @brief Configuration used to format ext2 file system.
  *
- * If a field is set to 0 then default value is used.
- * (In volume name the first cell of an array must be 0 to use default value.)
- *
- * @param block_size Requested size of block.
- * @param fs_size Requested size of file system. If 0 then whole available memory is used.
- * @param bytes_per_inode Requested memory for one inode. It is used to calculate number of inodes
- *	in created file system.
- * @param uuid UUID for created file system. Used when set_uuid is true.
- * @param volume_name Name for created file system.
- * @param set_uuid If true then UUID from that structure is used in created file system.
- *	If false then UUID (ver4) is generated.
+ * @note If a field is set to 0 then default value is used (see @ref
+ * FS_EXT2_DECLARE_DEFAULT_CONFIG).
  */
 struct ext2_cfg {
+	/**
+	 * Requested block size.
+	 */
 	uint32_t block_size;
-	uint32_t fs_size; /* Number of blocks that we want to take. */
+	/**
+	 * Requested file system size (expressed in blocks).
+	 */
+	uint32_t fs_size;
+	/**
+	 * Requested memory for each inode.
+	 *
+	 * This is used to calculate number of inodes in created file system.
+	 */
 	uint32_t bytes_per_inode;
+	/**
+	 * UUID for created file system.
+	 *
+	 * Used when @ref set_uuid field is set to true.
+	 */
 	uint8_t uuid[16];
-	uint8_t volume_name[17]; /* If first byte is 0 then name ext2" is given. */
+	/**
+	 * Name for created file system.
+	 *
+	 * @note If first byte is 0, the name "ext2" is given.
+	 */
+	uint8_t volume_name[17];
+	/**
+	 * Flag indicating if UUID should be set.
+	 *
+	 * If true then UUID from @ref uuid field is used in created file system.
+	 * If false then UUID (ver4) is generated.
+	 */
 	bool set_uuid;
 };
 
+/**
+ * @brief Default configuration for new ext2 file system.
+ */
 #define FS_EXT2_DECLARE_DEFAULT_CONFIG(name)			\
 	static struct ext2_cfg name = {				\
 		.block_size = 1024,				\
@@ -41,5 +73,8 @@ struct ext2_cfg {
 		.set_uuid = false,				\
 	}
 
+/**
+ * @}
+ */
 
 #endif /* ZEPHYR_INCLUDE_FS_EXT2_H_ */
