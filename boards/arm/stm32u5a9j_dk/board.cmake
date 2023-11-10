053c6b29cc8589120eda54f78f7beff5f966c91a
Abderrahmane Jarmouni <abderrahmane.jarmouni-ext@st.com>
2023-11-10 18:00:47 +0100

    boards: arm: stm32u5a9j-dk: add OpenOCD support
    Add support for debugging with OpenOCD and GDB
    
    Signed-off-by: Abderrahmane Jarmouni <abderrahmane.jarmouni-ext@st.com>

--- boards/arm/stm32u5a9j_dk/board.cmake
+++ boards/arm/stm32u5a9j_dk/board.cmake
@@ -3,5 +3,9 @@
 
 board_runner_args(stm32cubeprogrammer "--erase" "--port=swd" "--reset-mode=hw")
 
+board_runner_args(openocd "--tcl-port=6666")
+board_runner_args(openocd --cmd-pre-init "gdb_report_data_abort enable")
+board_runner_args(openocd "--no-halt")
+
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
-# FIXME: openocd runner not yet available.
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
