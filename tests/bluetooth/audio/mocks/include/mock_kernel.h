b6525340b55a7f5012299c3b1a81e8ae85246ba7
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2023-11-13 13:02:07 +0100

    tests: audio: mocks: Add k_work_schedule mock implementation
    This adds k_work_schedule mock implementation that will be used in
    bluetooth audio unit tests.
    
    Signed-off-by: Mariusz Skamra <mariusz.skamra@codecoup.pl>

--- tests/bluetooth/audio/mocks/include/mock_kernel.h
+++ tests/bluetooth/audio/mocks/include/mock_kernel.h
@@ -15,7 +15,6 @@ void mock_kernel_init(void);
 void mock_kernel_cleanup(void);
 
 DECLARE_FAKE_VALUE_FUNC(k_ticks_t, z_timeout_remaining, const struct _timeout *);
-DECLARE_FAKE_VALUE_FUNC(int, k_work_schedule, struct k_work_delayable *, k_timeout_t);
 DECLARE_FAKE_VALUE_FUNC(bool, k_work_cancel_delayable_sync, struct k_work_delayable *,
 			struct k_work_sync *);
 DECLARE_FAKE_VALUE_FUNC(int, k_sem_take, struct k_sem *, k_timeout_t);
--- tests/bluetooth/audio/mocks/src/kernel.c
+++ tests/bluetooth/audio/mocks/src/kernel.c
@@ -6,6 +6,7 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/sys_clock.h>
 #include <zephyr/sys/slist.h>
 
 #include "mock_kernel.h"
@@ -13,14 +14,12 @@
 /* List of fakes used by this unit tester */
 #define FFF_FAKES_LIST(FAKE)                                                                       \
 	FAKE(z_timeout_remaining)                                                                  \
-	FAKE(k_work_schedule)                                                                      \
 	FAKE(k_work_cancel_delayable_sync)                                                         \
 
 /* List of k_work items to be worked. */
 static sys_slist_t work_pending;
 
 DEFINE_FAKE_VALUE_FUNC(k_ticks_t, z_timeout_remaining, const struct _timeout *);
-DEFINE_FAKE_VALUE_FUNC(int, k_work_schedule, struct k_work_delayable *, k_timeout_t);
 DEFINE_FAKE_VALUE_FUNC(bool, k_work_cancel_delayable_sync, struct k_work_delayable *,
 		       struct k_work_sync *);
 DEFINE_FAKE_VALUE_FUNC(int, k_sem_take, struct k_sem *, k_timeout_t);
@@ -33,18 +32,48 @@ void k_work_init_delayable(struct k_work_delayable *dwork, k_work_handler_t hand
 
 int k_work_reschedule(struct k_work_delayable *dwork, k_timeout_t delay)
 {
+	bool on_list = false;
+	struct k_work *work;
+
+	dwork->timeout.dticks = delay.ticks;
+
+	/* Determine whether the work item is queued already. */
+	SYS_SLIST_FOR_EACH_CONTAINER(&work_pending, work, node) {
+		on_list = work == &dwork->work;
+		if (on_list) {
+			break;
+		}
+	}
+
+	if (dwork->timeout.dticks == 0) {
+		dwork->work.handler(&dwork->work);
+		if (on_list) {
+			(void)sys_slist_remove(&work_pending, NULL, &dwork->work.node);
+		}
+	} else if (!on_list) {
+		sys_slist_append(&work_pending, &dwork->work.node);
+	}
+
+	return 0;
+}
+
+int k_work_schedule(struct k_work_delayable *dwork, k_timeout_t delay)
+{
 	struct k_work *work;
 
 	/* Determine whether the work item is queued already. */
 	SYS_SLIST_FOR_EACH_CONTAINER(&work_pending, work, node) {
 		if (work == &dwork->work) {
-			dwork->timeout.dticks = delay.ticks;
 			return 0;
 		}
 	}
 
 	dwork->timeout.dticks = delay.ticks;
-	sys_slist_append(&work_pending, &dwork->work.node);
+	if (dwork->timeout.dticks == 0) {
+		dwork->work.handler(&dwork->work);
+	} else {
+		sys_slist_append(&work_pending, &dwork->work.node);
+	}
 
 	return 0;
 }
