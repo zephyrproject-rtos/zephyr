82d29d49925400d2e80940649b134d7ac1ed748d
Fabio Baltieri <fabiobaltieri@google.com>
2023-11-14 14:34:45 +0000

    ci: compliance: only run sorted check on text files
    The sorted check code crashes on binary files. Add a check on file type
    and only process text ones.
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

--- scripts/ci/check_compliance.py
+++ scripts/ci/check_compliance.py
@@ -1174,6 +1174,11 @@ class KeepSorted(ComplianceTest):
     MARKER = "zephyr-keep-sorted"
 
     def check_file(self, file, fp):
+        mime_type = magic.from_file(file, mime=True)
+
+        if not mime_type.startswith("text/"):
+            return
+
         lines = []
         in_block = False
 
