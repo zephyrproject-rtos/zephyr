927445325c78157a9f4b35a5fd8431fd7f390688
Manuel Argüelles <manuel.arguelles@nxp.com>
2023-11-14 21:16:33 +0700

    soc: nxp_s32: s32ze: include device headers in soc.h
    To simplify the inclusion of device headers in common code for
    NXP S32 devices, make sure all SoCs are including their respective
    device headers. This PR adds the missing headers for S32Z/E.
    
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

--- soc/arm/nxp_s32/s32ze/soc.h
+++ soc/arm/nxp_s32/s32ze/soc.h
@@ -10,6 +10,12 @@
 /* Do not let CMSIS to handle GIC */
 #define __GIC_PRESENT 0
 
+#if defined(CONFIG_SOC_S32Z27_R52)
+#include <S32Z2.h>
+#else
+#error "SoC not supported"
+#endif
+
 /* Aliases for peripheral base addresses */
 
 /* SIUL2 */
