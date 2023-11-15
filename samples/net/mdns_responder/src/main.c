a30c888e5f3c6af94087c7e3f31cb8fc8833e3e3
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2023-11-15 17:47:06 +0200

    samples: net: mdns_responder: Add VLAN support
    Add virtual LAN support to the sample so that it is easier to
    test the multiple mDNS listener feature implemented in previous
    commit.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- samples/net/mdns_responder/src/main.c
+++ samples/net/mdns_responder/src/main.c
@@ -14,6 +14,15 @@ LOG_MODULE_REGISTER(net_mdns_responder_sample, LOG_LEVEL_DBG);
 
 extern void service(void);
 
+#if defined(CONFIG_NET_VLAN)
+int init_vlan(void);
+#else
+static inline int init_vlan(void)
+{
+	return 0;
+}
+#endif /* CONFIG_NET_VLAN */
+
 /*
  * Note that mDNS support requires no application interaction with zephyr,
  * beyond optional runtime hostname configuration calls and setting
@@ -26,6 +35,7 @@ extern void service(void);
 int main(void)
 {
 	LOG_INF("Waiting mDNS queries...");
+	init_vlan();
 	service();
 	return 0;
 }
