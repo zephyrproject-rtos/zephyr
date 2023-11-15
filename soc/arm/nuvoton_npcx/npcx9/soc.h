edecc5ec470f9b33d1232ffa236a8fabfc5b4d59
Mulin Chao <mlchao@nuvoton.com>
2023-11-14 19:20:32 -0800

    soc: arm: npcx: fix clock reference of APB4/FIU1 buses
    This CL fixes the clock reference of APB4/FIU1 buses by introducing new
    Kconfig options.
    
    Signed-off-by: Mulin Chao <mlchao@nuvoton.com>

--- soc/arm/nuvoton_npcx/npcx9/soc.h
+++ soc/arm/nuvoton_npcx/npcx9/soc.h
@@ -56,10 +56,4 @@
 #include <soc_pins.h>
 #include <soc_power.h>
 
-/* NPCX9 Clock definitions */
-#if DT_NODE_HAS_PROP(DT_NODELABEL(pcc), apb4_prescaler)
-/* APB4 clock divider if supported */
-#define APB4DIV_VAL (DT_PROP(DT_NODELABEL(pcc), apb4_prescaler) - 1)
-#endif
-
 #endif /* _NUVOTON_NPCX_SOC_H_ */
