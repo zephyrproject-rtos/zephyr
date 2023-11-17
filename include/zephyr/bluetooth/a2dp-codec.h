582367156182917189b5c51f4e779d7a71e33b02
Benjamin Cabé <benjamin@zephyrproject.org>
2023-11-17 17:16:00 +0100

    bluetooth: a2dp: Add missing Doxygen comments
    Added missing Doxygen comments to various macros.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- include/zephyr/bluetooth/a2dp-codec.h
+++ include/zephyr/bluetooth/a2dp-codec.h
@@ -26,36 +26,76 @@
 extern "C" {
 #endif
 
-/* Sampling Frequency */
-#define A2DP_SBC_SAMP_FREQ_16000 BIT(7)
-#define A2DP_SBC_SAMP_FREQ_32000 BIT(6)
-#define A2DP_SBC_SAMP_FREQ_44100 BIT(5)
-#define A2DP_SBC_SAMP_FREQ_48000 BIT(4)
+/**
+ * @name Sampling Frequency
+ * @{
+ */
+#define A2DP_SBC_SAMP_FREQ_16000 BIT(7) /**< 16 kHz */
+#define A2DP_SBC_SAMP_FREQ_32000 BIT(6) /**< 32 kHz */
+#define A2DP_SBC_SAMP_FREQ_44100 BIT(5) /**< 44.1 kHz */
+#define A2DP_SBC_SAMP_FREQ_48000 BIT(4) /**< 48 kHz */
+/** @} */
 
-/* Channel Mode */
-#define A2DP_SBC_CH_MODE_MONO  BIT(3)
-#define A2DP_SBC_CH_MODE_DUAL  BIT(2)
-#define A2DP_SBC_CH_MODE_STREO BIT(1)
-#define A2DP_SBC_CH_MODE_JOINT BIT(0)
+/**
+ * @name Channel Mode
+ * @{
+ */
+#define A2DP_SBC_CH_MODE_MONO  BIT(3) /**< Mono */
+#define A2DP_SBC_CH_MODE_DUAL  BIT(2) /**< Dual Channel */
+#define A2DP_SBC_CH_MODE_STREO BIT(1) /**< Stereo */
+#define A2DP_SBC_CH_MODE_JOINT BIT(0) /**< Joint Stereo */
+/** @} */
 
-/* Block Length */
-#define A2DP_SBC_BLK_LEN_4  BIT(7)
-#define A2DP_SBC_BLK_LEN_8  BIT(6)
-#define A2DP_SBC_BLK_LEN_12 BIT(5)
-#define A2DP_SBC_BLK_LEN_16 BIT(4)
+/**
+ * @name Block Length
+ * @{
+ */
+#define A2DP_SBC_BLK_LEN_4  BIT(7) /**< 4 blocks */
+#define A2DP_SBC_BLK_LEN_8  BIT(6) /**< 8 blocks */
+#define A2DP_SBC_BLK_LEN_12 BIT(5) /**< 12 blocks */
+#define A2DP_SBC_BLK_LEN_16 BIT(4) /**< 16 blocks */
+/** @} */
 
-/* Subbands */
-#define A2DP_SBC_SUBBAND_4 BIT(3)
-#define A2DP_SBC_SUBBAND_8 BIT(2)
+/**
+ * @name Subbands
+ * @{
+ */
+#define A2DP_SBC_SUBBAND_4 BIT(3) /**< 4 subbands */
+#define A2DP_SBC_SUBBAND_8 BIT(2) /**< 8 subbands */
+/** @} */
 
-/* Allocation Method */
-#define A2DP_SBC_ALLOC_MTHD_SNR      BIT(1)
-#define A2DP_SBC_ALLOC_MTHD_LOUDNESS BIT(0)
+/**
+ * @name Bit pool Allocation Method
+ * @{
+ */
+#define A2DP_SBC_ALLOC_MTHD_SNR      BIT(1) /**< Allocate based on loudness of the subband signal */
+#define A2DP_SBC_ALLOC_MTHD_LOUDNESS BIT(0) /**< Allocate based on the signal-to-noise ratio */
+/** @} */
 
+/**
+ * Gets the sampling rate from a codec preset
+ * @param preset Codec preset
+ */
 #define BT_A2DP_SBC_SAMP_FREQ(preset)    ((preset->config[0] >> 4) & 0x0f)
+/**
+ * Gets the channel mode from a codec preset
+ * @param preset Codec preset
+ */
 #define BT_A2DP_SBC_CHAN_MODE(preset)    ((preset->config[0]) & 0x0f)
+/**
+ * Gets the block length from a codec preset
+ * @param preset Codec preset
+ */
 #define BT_A2DP_SBC_BLK_LEN(preset)      ((preset->config[1] >> 4) & 0x0f)
+/**
+ * Gets the number subbands from a codec preset
+ * @param preset Codec preset
+ */
 #define BT_A2DP_SBC_SUB_BAND(preset)     ((preset->config[1] >> 2) & 0x03)
+/**
+ * Gets the bitpool allocation method from a codec preset
+ * @param preset Codec preset
+ */
 #define BT_A2DP_SBC_ALLOC_MTHD(preset)   ((preset->config[1]) & 0x03)
 
 /** @brief SBC Codec */
