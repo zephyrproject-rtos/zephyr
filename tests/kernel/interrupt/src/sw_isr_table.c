a28da922dbb88db15200c8bdec3312ae1ec8b259
Yong Cong Sin <ycsin@meta.com>
2023-11-20 17:59:44 +0800

    tests: interrupt: refactor the sw_isr_irq_parent_table tests
    Split the `sw_isr_irq_parent_table` test into one that tests
    the IRQ table index function which is generic and should
    always be tested, and one that tests multi-instance -related
    functions which only work in interrupt controller drivers that
    has multi-instance implementation.
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>

--- tests/kernel/interrupt/src/sw_isr_table.c
+++ tests/kernel/interrupt/src/sw_isr_table.c
@@ -10,27 +10,59 @@
 
 extern const struct _irq_parent_entry _lvl2_irq_list[];
 
+#if DT_HAS_COMPAT_STATUS_OKAY(sifive_plic_1_0_0)
+#define INTC_SUPPORTS_MULTI_INSTANCE 1
+#endif
+
 /**
- * @brief Test sw_isr_table function
+ * @brief Test sw_isr_table index-related function(s)
  *
  * @details Validates that:
- * - z_get_sw_isr_device_from_irq() returns the parent interrupt controller for an IRQN
- * - z_get_sw_isr_irq_from_device() returns the IRQN of a parent interrupt controller
  * - z_get_sw_isr_table_idx() returns the corresponding SW ISR table index for an IRQN
  */
-ZTEST(interrupt_feature, test_sw_isr_irq_parent_table)
+ZTEST(interrupt_feature, test_sw_isr_irq_parent_table_idx)
 {
-	const struct device *parent_dev;
 	unsigned int parent_irq;
 	unsigned int parent_isr_offset;
-	const struct device *test_dev;
 	unsigned int test_irq;
 	unsigned int test_isr_offset;
 
 	for (size_t i = 0; i < CONFIG_NUM_2ND_LEVEL_AGGREGATORS; i++) {
-		parent_dev = _lvl2_irq_list[i].dev;
 		parent_irq = _lvl2_irq_list[i].irq;
 		parent_isr_offset = _lvl2_irq_list[i].offset;
+
+		for (unsigned int local_irq = 0;
+		     local_irq < BIT_MASK(CONFIG_2ND_LEVEL_INTERRUPT_BITS); local_irq++) {
+			test_irq = irq_to_level_2(local_irq) | parent_irq;
+			test_isr_offset = z_get_sw_isr_table_idx(test_irq);
+			zassert_equal(parent_isr_offset + local_irq, test_isr_offset,
+				      "expected offset: %d, got: %d", parent_isr_offset + local_irq,
+				      test_isr_offset);
+		}
+	}
+}
+
+/**
+ * @brief Test sw_isr_table device-related function(s)
+ *
+ * This test only works on driver that supports multi-instances
+ *
+ * @details Validates that:
+ * - z_get_sw_isr_device_from_irq() returns the parent interrupt controller for an IRQN
+ * - z_get_sw_isr_irq_from_device() returns the IRQN of a parent interrupt controller
+ */
+ZTEST(interrupt_feature, test_sw_isr_irq_parent_table_dev)
+{
+	const struct device *parent_dev;
+	unsigned int parent_irq;
+	const struct device *test_dev;
+	unsigned int test_irq;
+
+	Z_TEST_SKIP_IFNDEF(INTC_SUPPORTS_MULTI_INSTANCE);
+
+	for (size_t i = 0; i < CONFIG_NUM_2ND_LEVEL_AGGREGATORS; i++) {
+		parent_dev = _lvl2_irq_list[i].dev;
+		parent_irq = _lvl2_irq_list[i].irq;
 
 		for (unsigned int local_irq = 0;
 		     local_irq < BIT_MASK(CONFIG_2ND_LEVEL_INTERRUPT_BITS); local_irq++) {
@@ -43,14 +75,5 @@ ZTEST(interrupt_feature, test_sw_isr_irq_parent_table)
 		test_irq = z_get_sw_isr_irq_from_device(parent_dev);
 		zassert_equal(parent_irq, test_irq, "expected offset: %d, got: %d", parent_irq,
 			      test_irq);
-
-		for (unsigned int local_irq = 0;
-		     local_irq < BIT_MASK(CONFIG_2ND_LEVEL_INTERRUPT_BITS); local_irq++) {
-			test_irq = irq_to_level_2(local_irq) | parent_irq;
-			test_isr_offset = z_get_sw_isr_table_idx(test_irq);
-			zassert_equal(parent_isr_offset + local_irq, test_isr_offset,
-				      "expected offset: %d, got: %d", parent_isr_offset + local_irq,
-				      test_isr_offset);
-		}
 	}
 }
