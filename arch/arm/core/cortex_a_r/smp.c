4d8d5e945babfd64e18db667686aeb67fdb7cd5d
Stan Skowronek <stan@corellium.com>
2023-11-20 07:25:45 -0500

    arch: cortex_ar: Do not send IPIs to invalid MPIDR
    Fix check for uninitialized target CPU MPIDR when sending IPIs.
    
    Signed-off-by: Stan Skowronek <stan@corellium.com>

--- arch/arm/core/cortex_a_r/smp.c
+++ arch/arm/core/cortex_a_r/smp.c
@@ -223,7 +223,7 @@ static void broadcast_ipi(unsigned int ipi)
 		uint32_t target_mpidr = cpu_map[i];
 		uint8_t aff0;
 
-		if (mpidr == target_mpidr || mpidr == INV_MPID) {
+		if (mpidr == target_mpidr || target_mpidr == INV_MPID) {
 			continue;
 		}
 
