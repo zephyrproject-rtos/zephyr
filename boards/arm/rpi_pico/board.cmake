967f135f2401aafc7e44da47120e178b353f953f
TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>
2023-11-22 07:34:27 +0900

    boards: arm: rpi_pico: add pyocd runner configuration
    Adding configuration for support pyocd.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>

--- boards/arm/rpi_pico/board.cmake
+++ boards/arm/rpi_pico/board.cmake
@@ -27,8 +27,10 @@ board_runner_args(openocd --cmd-pre-init "set_adapter_speed_if_not_set 2000")
 
 board_runner_args(jlink "--device=RP2040_M0_0")
 board_runner_args(uf2 "--board-id=RPI-RP2")
+board_runner_args(pyocd "--target=rp2040")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
