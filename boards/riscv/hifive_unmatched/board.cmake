4a502b1f25632fe1709036ce57a5fcf7678001a7
Yong Cong Sin <ycsin@meta.com>
2023-11-22 18:12:06 +0800

    boards: riscv: hifive_unmatched: add Renode simulation support
    Add Renode simulation support for `hifive_unmatched`, the
    script is basically copied over from `hifive_unleashed`, with
    very minor edits.
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>

--- boards/riscv/hifive_unmatched/board.cmake
+++ boards/riscv/hifive_unmatched/board.cmake
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
+set(SUPPORTED_EMU_PLATFORMS renode)
+set(RENODE_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/support/hifive_unmatched.resc)
+set(RENODE_UART sysbus.uart0)
+
 set(OPENOCD_USE_LOAD_IMAGE NO)
 
 board_runner_args(openocd "--config=${BOARD_DIR}/support/openocd_hifive_unmatched.cfg")
