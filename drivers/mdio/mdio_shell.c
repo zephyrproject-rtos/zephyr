3131ade7354f32e2108e76d8bccb6c8313873ba0
Manuel Argüelles <manuel.arguelles@nxp.com>
2023-11-25 12:06:39 +0700

    drivers: mdio: add support for NXP S32 GMAC
    MDIO controller is part of GMAC and it requires GMAC ethernet driver to
    initialize first because it will reset the whole GMAC hw block during
    initialization. Both C22 and C45 APIs are supported.
    
    Co-authored-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>
    Signed-off-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>

--- drivers/mdio/mdio_shell.c
+++ drivers/mdio/mdio_shell.c
@@ -21,6 +21,8 @@ LOG_MODULE_REGISTER(mdio_shell, CONFIG_LOG_DEFAULT_LEVEL);
 #define DT_DRV_COMPAT espressif_esp32_mdio
 #elif DT_HAS_COMPAT_STATUS_OKAY(nxp_s32_netc_emdio)
 #define DT_DRV_COMPAT nxp_s32_netc_emdio
+#elif DT_HAS_COMPAT_STATUS_OKAY(nxp_s32_gmac_mdio)
+#define DT_DRV_COMPAT nxp_s32_gmac_mdio
 #elif DT_HAS_COMPAT_STATUS_OKAY(adi_adin2111_mdio)
 #define DT_DRV_COMPAT adi_adin2111_mdio
 #elif DT_HAS_COMPAT_STATUS_OKAY(smsc_lan91c111_mdio)
