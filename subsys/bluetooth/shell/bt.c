8d9506ab425224350070a635360725e483bc2498
Kyra Lengfeld <kyra.lengfeld@nordicsemi.no>
2023-11-27 12:32:01 +0100

    Bluetooth: Host: Align return lines of bt shell helper function phy2str
    As other helper functions were introduced for the LE Power Control
    Request Feature (see CONFIG_BT_TRANSMIT_POWER_CONTROL), it was noticed
    that the return statements of switch cases should be on the next line
    generally. This will align phy2str with the rest in bt.c.
    
    Signed-off-by: Kyra Lengfeld <kyra.lengfeld@nordicsemi.no>

--- subsys/bluetooth/shell/bt.c
+++ subsys/bluetooth/shell/bt.c
@@ -99,10 +99,14 @@ static const char *phy2str(uint8_t phy)
 {
 	switch (phy) {
 	case 0: return "No packets";
-	case BT_GAP_LE_PHY_1M: return "LE 1M";
-	case BT_GAP_LE_PHY_2M: return "LE 2M";
-	case BT_GAP_LE_PHY_CODED: return "LE Coded";
-	default: return "Unknown";
+	case BT_GAP_LE_PHY_1M:
+		return "LE 1M";
+	case BT_GAP_LE_PHY_2M:
+		return "LE 2M";
+	case BT_GAP_LE_PHY_CODED:
+		return "LE Coded";
+	default:
+		return "Unknown";
 	}
 }
 #endif
