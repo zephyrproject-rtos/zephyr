1ee2e6748d8de2711ce2bcbd6750a8560c35d751
Robert Lubos <robert.lubos@nordicsemi.no>
2023-11-28 17:48:03 +0100

    tests: net: tcp: Add small delay when checking context dereference
    Since the TCP context (and thus net_context) is not dereferenced
    directly from the test thread anymore, add a small delay so that TCP
    work queue have a chance to run, before checking net_context count or
    reusing the port on new allocation.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- tests/net/socket/tcp/src/main.c
+++ tests/net/socket/tcp/src/main.c
@@ -1060,12 +1060,10 @@ ZTEST(net_socket_tcp, test_connect_timeout)
 
 	test_close(c_sock);
 
-	/* If we have preemptive option set, then sleep here in order to allow
-	 * other part of the system to run and update itself.
+	/* Sleep here in order to allow other part of the system to run and
+	 * update itself.
 	 */
-	if (IS_ENABLED(CONFIG_NET_TC_THREAD_PREEMPTIVE)) {
-		k_sleep(K_MSEC(10));
-	}
+	k_sleep(K_MSEC(10));
 
 	/* After the client socket closing, the context count should be 0 */
 	net_context_foreach(calc_net_context, &count_after);
