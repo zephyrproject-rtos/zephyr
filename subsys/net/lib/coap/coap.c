8fe9c16b3890e76f5790c5bc1c8fbf25498abe05
Pieter De Gendt <pieter.degendt@basalte.be>
2023-11-29 16:53:09 +0100

    net: lib: coap: Introduce net mgmt events for CoAP
    Allow users to register net mgmt events callbacks for CoAP events.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- subsys/net/lib/coap/coap.c
+++ subsys/net/lib/coap/coap.c
@@ -23,6 +23,7 @@ LOG_MODULE_REGISTER(net_coap, CONFIG_COAP_LOG_LEVEL);
 #include <zephyr/net/net_ip.h>
 #include <zephyr/net/net_core.h>
 #include <zephyr/net/coap.h>
+#include <zephyr/net/coap_mgmt.h>
 
 #define COAP_PATH_ELEM_DELIM '/'
 #define COAP_PATH_ELEM_QUERY '?'
@@ -1844,6 +1845,25 @@ void coap_observer_init(struct coap_observer *observer,
 	net_ipaddr_copy(&observer->addr, addr);
 }
 
+static inline void coap_observer_raise_event(struct coap_resource *resource,
+					     struct coap_observer *observer,
+					     uint32_t mgmt_event)
+{
+#ifdef CONFIG_NET_MGMT_EVENT_INFO
+	const struct net_event_coap_observer net_event = {
+		.resource = resource,
+		.observer = observer,
+	};
+
+	net_mgmt_event_notify_with_info(mgmt_event, NULL, (void *)&net_event, sizeof(net_event));
+#else
+	ARG_UNUSED(resource);
+	ARG_UNUSED(observer);
+
+	net_mgmt_event_notify(mgmt_event, NULL);
+#endif
+}
+
 bool coap_register_observer(struct coap_resource *resource,
 			    struct coap_observer *observer)
 {
@@ -1856,13 +1876,21 @@ bool coap_register_observer(struct coap_resource *resource,
 		resource->age = 2;
 	}
 
+	coap_observer_raise_event(resource, observer, NET_EVENT_COAP_OBSERVER_ADDED);
+
 	return first;
 }
 
 bool coap_remove_observer(struct coap_resource *resource,
 			  struct coap_observer *observer)
 {
-	return sys_slist_find_and_remove(&resource->observers, &observer->list);
+	if (!sys_slist_find_and_remove(&resource->observers, &observer->list)) {
+		return false;
+	}
+
+	coap_observer_raise_event(resource, observer, NET_EVENT_COAP_OBSERVER_REMOVED);
+
+	return true;
 }
 
 static bool sockaddr_equal(const struct sockaddr *a,
