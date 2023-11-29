bbbf671ef88cdf8c3b064fc79a38eec317a780c6
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-11-29 18:46:52 +0100

    [nrf fromtree] samples/subsys/logging/multidomain: Reduce sysbuild boilerplate
    Reduce the sysbuild boilerplate required for
    the nrf5340bsim targets
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
    (cherry picked from commit b02fc27464c46076a62e2401e51ce7ae0302de95)
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- samples/subsys/logging/multidomain/sysbuild.cmake
+++ samples/subsys/logging/multidomain/sysbuild.cmake
@@ -15,24 +15,6 @@ ExternalZephyrProject_Add(
 	BOARD       ${SB_CONFIG_NET_CORE_BOARD}
 )
 
-if("${BOARD}" STREQUAL "nrf5340bsim_nrf5340_cpuapp")
-	# For the simulated board, the application core build will produce the final executable
-	# for that, we give it the path to the netcore image
-	set(NET_LIBRARY_PATH ${CMAKE_BINARY_DIR}/${NET_APP}/zephyr/zephyr.elf)
-	set_property(TARGET ${DEFAULT_IMAGE} APPEND_STRING PROPERTY CONFIG
-		"CONFIG_NATIVE_SIMULATOR_EXTRA_IMAGE_PATHS=\"${NET_LIBRARY_PATH}\"\n"
-	)
+native_simulator_set_child_images(${DEFAULT_IMAGE} ${NET_APP})
 
-	# Let's build the net core library first
-	add_dependencies(${DEFAULT_IMAGE} ${NET_APP})
-
-	# Let's meet users expectations of finding the final executable in zephyr/zephyr.exe
-	add_custom_target(final_executable
-		ALL
-		COMMAND
-		${CMAKE_COMMAND} -E copy
-			${CMAKE_BINARY_DIR}/${DEFAULT_IMAGE}/zephyr/zephyr.exe
-			${CMAKE_BINARY_DIR}/zephyr/zephyr.exe
-		DEPENDS ${DEFAULT_IMAGE}
-	)
-endif()
+native_simulator_set_final_executable(${DEFAULT_IMAGE})
