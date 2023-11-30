6178a815ea834a6bc69d8a42e9b78222f23a053a
Aleksander Wasaznik <aleksander.wasaznik@nordicsemi.no>
2023-11-30 15:25:44 +0100

    Bluetooth: testlib: remove duplicate testlib att/read_fill_buf
    This updates the test att/read_fill_buf to use the global testlib
    instead of its own duplicated one.
    
    Signed-off-by: Aleksander Wasaznik <aleksander.wasaznik@nordicsemi.no>

--- tests/bsim/bluetooth/host/att/read_fill_buf/testlib/security.h
+++
@@ -1,7 +0,0 @@
-/* Copyright (c) 2023 Nordic Semiconductor ASA
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include <zephyr/bluetooth/conn.h>
-
-int bt_testlib_secure(struct bt_conn *conn, bt_security_t new_minimum);
