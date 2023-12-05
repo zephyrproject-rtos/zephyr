32e6326d1a21d96d5859e0dba8b8c3415254dce4
Andrej Butok <andrey.butok@nxp.com>
2023-12-05 15:27:51 +0100

    boards: mimxrt1040_evk: add LinkServer and Pyocd
    Add LinkServer and Pyocd runners for the MIMXRT1040-EVK board.
    
    Signed-off-by: Andrej Butok <andrey.butok@nxp.com>

--- boards/arm/mimxrt1040_evk/board.cmake
+++ boards/arm/mimxrt1040_evk/board.cmake
@@ -5,5 +5,9 @@
 #
 
 board_runner_args(jlink "--device=MIMXRT1042XXX6B")
+board_runner_args(linkserver "--device=MIMXRT1042xxxxB:MIMXRT1040-EVK")
+board_runner_args(pyocd "--target=MIMXRT1042XJM5B")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
