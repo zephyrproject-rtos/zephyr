cc48290fb95c181675c5abd1a326638e8690db6a
Daniel DeGrasse <daniel.degrasse@nxp.com>
2023-12-05 22:27:04 +0000

    include: audio: dmic: fixup parse_channel_map implementation
    Fixup parse_channel_map implementation. The previous implementation of
    this function did not use the "channel_map" values passed in by the
    user, and would not return correct values for the PDM hardware
    controller or L/R value. This function is not being used in tree, so
    correcting the implementation to align with documentation should have
    minimal affect.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- include/zephyr/audio/dmic.h
+++ include/zephyr/audio/dmic.h
@@ -231,8 +231,8 @@ static inline void dmic_parse_channel_map(uint32_t channel_map_lo,
 	channel_map = (channel < 8) ? channel_map_lo : channel_map_hi;
 	channel_map >>= ((channel & BIT_MASK(3)) * 4U);
 
-	*pdm = (channel >> 1) & BIT_MASK(3);
-	*lr = (enum pdm_lr) (channel & BIT(0));
+	*pdm = (channel_map >> 1) & BIT_MASK(3);
+	*lr = (enum pdm_lr) (channel_map & BIT(0));
 }
 
 /**
