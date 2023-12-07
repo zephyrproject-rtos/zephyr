b9360488f73c59814639583f285491c463f8027b
Emil Gydesen <emil.gydesen@nordicsemi.no>
2023-12-07 15:21:45 +0100

    Bluetooth: CAP: Commander change volume mute procedure
    Adds the CAP Commander Change Volume mute procedure.
    This procedure changes the volume mute on one or more
    CAP Acceptors.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- include/zephyr/bluetooth/audio/cap.h
+++ include/zephyr/bluetooth/audio/cap.h
@@ -695,6 +695,18 @@ struct bt_cap_commander_cb {
 	 */
 	void (*volume_changed)(struct bt_conn *conn, int err);
 
+	/**
+	 * @brief Callback for bt_cap_commander_change_volume_mute_state().
+	 *
+	 * @param conn           Pointer to the connection where the error
+	 *                       occurred. NULL if @p err is 0 or if cancelled by
+	 *                       bt_cap_commander_cancel()
+	 * @param err            0 on success, BT_GATT_ERR() with a
+	 *                       specific ATT (BT_ATT_ERR_*) error code or -ECANCELED if cancelled
+	 *                       by bt_cap_commander_cancel().
+	 */
+	void (*volume_mute_changed)(struct bt_conn *conn, int err);
+
 #if defined(CONFIG_BT_VCP_VOL_CTLR_VOCS)
 	/**
 	 * @brief Callback for bt_cap_commander_change_volume_offset().
