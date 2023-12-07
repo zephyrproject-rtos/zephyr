b4a11748af19ce7a4eb7c0817f456a360f4cbb19
Lukasz Mrugala <lukaszx.mrugala@intel.com>
2023-12-07 13:56:38 +0000

    scripts: tests: twister: TestInstance tests refactor
    Prepares the test_testinstance.py for expansion of its tests
    
    Signed-off-by: Lukasz Mrugala <lukaszx.mrugala@intel.com>

--- scripts/tests/twister/test_testinstance.py
+++ scripts/tests/twister/test_testinstance.py
@@ -15,13 +15,14 @@ from unittest import mock
 
 ZEPHYR_BASE = os.getenv("ZEPHYR_BASE")
 sys.path.insert(0, os.path.join(ZEPHYR_BASE, "scripts/pylib/twister"))
+
 from twisterlib.testinstance import TestInstance
 from twisterlib.error import BuildError
 from twisterlib.runner import TwisterRunner
 from expr_parser import reserved
 
 
-TESTDATA_1 = [
+TESTDATA_PART_1 = [
     (False, False, "console", "na", "qemu", False, [], (False, True)),
     (False, False, "console", "native", "qemu", False, [], (False, True)),
     (True, False, "console", "native", "nsim", False, [], (True, False)),
@@ -30,14 +31,30 @@ TESTDATA_1 = [
     (False, False, "sensor", "na", "", False, [], (True, False)),
     (False, True, "sensor", "native", "", True, [], (True, False)),
 ]
-@pytest.mark.parametrize("build_only, slow, harness, platform_type, platform_sim, device_testing,fixture, expected", TESTDATA_1)
-def test_check_build_or_run(class_testplan, all_testsuites_dict, platforms_list, build_only, slow, harness, platform_type, platform_sim, device_testing, fixture, expected):
+@pytest.mark.parametrize(
+    "build_only, slow, harness, platform_type, platform_sim, device_testing,fixture, expected",
+    TESTDATA_PART_1
+)
+def test_check_build_or_run(
+    class_testplan,
+    all_testsuites_dict,
+    platforms_list,
+    build_only,
+    slow,
+    harness,
+    platform_type,
+    platform_sim,
+    device_testing,
+    fixture,
+    expected
+):
     """" Test to check the conditions for build_only and run scenarios
     Scenario 1: Test when different parameters are passed, build_only and run are set correctly
     Scenario 2: Test if build_only is enabled when the OS is Windows"""
 
     class_testplan.testsuites = all_testsuites_dict
-    testsuite = class_testplan.testsuites.get('scripts/tests/twister/test_data/testsuites/tests/test_a/test_a.check_1')
+    testsuite = class_testplan.testsuites.get('scripts/tests/twister/test_data/testsuites/tests/'
+                                              'test_a/test_a.check_1')
     print(testsuite)
 
     class_testplan.platforms = platforms_list
@@ -57,27 +74,52 @@ def test_check_build_or_run(class_testplan, all_testsuites_dict, platforms_list,
         run = testinstance.check_runnable()
         assert not run
 
-TESTDATA_2 = [
-    (True, True, True, ["demo_board_2"], "native", None, '\nCONFIG_COVERAGE=y\nCONFIG_COVERAGE_DUMP=y\nCONFIG_ASAN=y\nCONFIG_UBSAN=y'),
-    (True, False, True, ["demo_board_2"], "native", None, '\nCONFIG_COVERAGE=y\nCONFIG_COVERAGE_DUMP=y\nCONFIG_ASAN=y'),
-    (False, False, True, ["demo_board_2"], 'native', None, '\nCONFIG_COVERAGE=y\nCONFIG_COVERAGE_DUMP=y'),
-    (True, False, True, ["demo_board_2"], 'mcu', None, '\nCONFIG_COVERAGE=y\nCONFIG_COVERAGE_DUMP=y'),
+TESTDATA_PART_2 = [
+    (True, True, True, ["demo_board_2"], "native",
+     None, '\nCONFIG_COVERAGE=y\nCONFIG_COVERAGE_DUMP=y\nCONFIG_ASAN=y\nCONFIG_UBSAN=y'),
+    (True, False, True, ["demo_board_2"], "native",
+     None, '\nCONFIG_COVERAGE=y\nCONFIG_COVERAGE_DUMP=y\nCONFIG_ASAN=y'),
+    (False, False, True, ["demo_board_2"], 'native',
+     None, '\nCONFIG_COVERAGE=y\nCONFIG_COVERAGE_DUMP=y'),
+    (True, False, True, ["demo_board_2"], 'mcu',
+     None, '\nCONFIG_COVERAGE=y\nCONFIG_COVERAGE_DUMP=y'),
     (False, False, False, ["demo_board_2"], 'native', None, ''),
     (False, False, True, ['demo_board_1'], 'native', None, ''),
     (True, False, False, ["demo_board_2"], 'native', None, '\nCONFIG_ASAN=y'),
     (False, True, False, ["demo_board_2"], 'native', None, '\nCONFIG_UBSAN=y'),
-    (False, False, False, ["demo_board_2"], 'native', ["CONFIG_LOG=y"], 'CONFIG_LOG=y'),
-    (False, False, False, ["demo_board_2"], 'native', ["arch:x86_demo:CONFIG_LOG=y"], 'CONFIG_LOG=y'),
-    (False, False, False, ["demo_board_2"], 'native', ["arch:arm_demo:CONFIG_LOG=y"], ''),
-    (False, False, False, ["demo_board_2"], 'native', ["platform:demo_board_2:CONFIG_LOG=y"], 'CONFIG_LOG=y'),
-    (False, False, False, ["demo_board_2"], 'native', ["platform:demo_board_1:CONFIG_LOG=y"], ''),
+    (False, False, False, ["demo_board_2"], 'native',
+     ["CONFIG_LOG=y"], 'CONFIG_LOG=y'),
+    (False, False, False, ["demo_board_2"], 'native',
+     ["arch:x86_demo:CONFIG_LOG=y"], 'CONFIG_LOG=y'),
+    (False, False, False, ["demo_board_2"], 'native',
+     ["arch:arm_demo:CONFIG_LOG=y"], ''),
+    (False, False, False, ["demo_board_2"], 'native',
+     ["platform:demo_board_2:CONFIG_LOG=y"], 'CONFIG_LOG=y'),
+    (False, False, False, ["demo_board_2"], 'native',
+     ["platform:demo_board_1:CONFIG_LOG=y"], ''),
 ]
 
-@pytest.mark.parametrize("enable_asan, enable_ubsan, enable_coverage, coverage_platform, platform_type, extra_configs, expected_content", TESTDATA_2)
-def test_create_overlay(class_testplan, all_testsuites_dict, platforms_list, enable_asan, enable_ubsan, enable_coverage, coverage_platform, platform_type, extra_configs, expected_content):
+@pytest.mark.parametrize(
+    'enable_asan, enable_ubsan, enable_coverage, coverage_platform, platform_type,'
+    ' extra_configs, expected_content',
+    TESTDATA_PART_2
+)
+def test_create_overlay(
+    class_testplan,
+    all_testsuites_dict,
+    platforms_list,
+    enable_asan,
+    enable_ubsan,
+    enable_coverage,
+    coverage_platform,
+    platform_type,
+    extra_configs,
+    expected_content
+):
     """Test correct content is written to testcase_extra.conf based on if conditions."""
     class_testplan.testsuites = all_testsuites_dict
-    testcase = class_testplan.testsuites.get('scripts/tests/twister/test_data/testsuites/samples/test_app/sample_test.app')
+    testcase = class_testplan.testsuites.get('scripts/tests/twister/test_data/testsuites/samples/'
+                                             'test_app/sample_test.app')
 
     if extra_configs:
         testcase.extra_configs = extra_configs
@@ -92,7 +134,8 @@ def test_create_overlay(class_testplan, all_testsuites_dict, platforms_list, ena
 def test_calculate_sizes(class_testplan, all_testsuites_dict, platforms_list):
     """ Test Calculate sizes method for zephyr elf"""
     class_testplan.testsuites = all_testsuites_dict
-    testcase = class_testplan.testsuites.get('scripts/tests/twister/test_data/testsuites/samples/test_app/sample_test.app')
+    testcase = class_testplan.testsuites.get('scripts/tests/twister/test_data/testsuites/samples/'
+                                             'test_app/sample_test.app')
     class_testplan.platforms = platforms_list
     platform = class_testplan.get_platform("demo_board_2")
     testinstance = TestInstance(testcase, platform, class_testplan.env.outdir)
@@ -100,14 +143,34 @@ def test_calculate_sizes(class_testplan, all_testsuites_dict, platforms_list):
     with pytest.raises(BuildError):
         assert testinstance.calculate_sizes() == "Missing/multiple output ELF binary"
 
-TESTDATA_3 = [
-    ('CONFIG_ARCH_HAS_THREAD_LOCAL_STORAGE and CONFIG_TOOLCHAIN_SUPPORTS_THREAD_LOCAL_STORAGE and not (CONFIG_TOOLCHAIN_ARCMWDT_SUPPORTS_THREAD_LOCAL_STORAGE and CONFIG_USERSPACE)', ['kconfig']),
-    ('(dt_compat_enabled("st,stm32-flash-controller") or dt_compat_enabled("st,stm32h7-flash-controller")) and dt_label_with_parent_compat_enabled("storage_partition", "fixed-partitions")', ['dts']),
-    ('((CONFIG_FLASH_HAS_DRIVER_ENABLED and not CONFIG_TRUSTED_EXECUTION_NONSECURE) and dt_label_with_parent_compat_enabled("storage_partition", "fixed-partitions")) or (CONFIG_FLASH_HAS_DRIVER_ENABLED and CONFIG_TRUSTED_EXECUTION_NONSECURE and dt_label_with_parent_compat_enabled("slot1_ns_partition", "fixed-partitions"))', ['dts', 'kconfig']),
-    ('((CONFIG_CPU_AARCH32_CORTEX_R or CONFIG_CPU_CORTEX_M) and CONFIG_CPU_HAS_FPU and TOOLCHAIN_HAS_NEWLIB == 1) or CONFIG_ARCH_POSIX', ['full'])
+TESTDATA_PART_3 = [
+    (
+        'CONFIG_ARCH_HAS_THREAD_LOCAL_STORAGE and' \
+        ' CONFIG_TOOLCHAIN_SUPPORTS_THREAD_LOCAL_STORAGE and' \
+        ' not (CONFIG_TOOLCHAIN_ARCMWDT_SUPPORTS_THREAD_LOCAL_STORAGE and CONFIG_USERSPACE)',
+        ['kconfig']
+    ),
+    (
+        '(dt_compat_enabled("st,stm32-flash-controller") or' \
+        ' dt_compat_enabled("st,stm32h7-flash-controller")) and' \
+        ' dt_label_with_parent_compat_enabled("storage_partition", "fixed-partitions")',
+        ['dts']
+    ),
+    (
+        '((CONFIG_FLASH_HAS_DRIVER_ENABLED and not CONFIG_TRUSTED_EXECUTION_NONSECURE) and' \
+        ' dt_label_with_parent_compat_enabled("storage_partition", "fixed-partitions")) or' \
+        ' (CONFIG_FLASH_HAS_DRIVER_ENABLED and CONFIG_TRUSTED_EXECUTION_NONSECURE and' \
+        ' dt_label_with_parent_compat_enabled("slot1_ns_partition", "fixed-partitions"))',
+        ['dts', 'kconfig']
+    ),
+    (
+        '((CONFIG_CPU_AARCH32_CORTEX_R or CONFIG_CPU_CORTEX_M) and' \
+        ' CONFIG_CPU_HAS_FPU and TOOLCHAIN_HAS_NEWLIB == 1) or CONFIG_ARCH_POSIX',
+        ['full']
+    )
 ]
 
-@pytest.mark.parametrize("filter_expr, expected_stages", TESTDATA_3)
+@pytest.mark.parametrize("filter_expr, expected_stages", TESTDATA_PART_3)
 def test_which_filter_stages(filter_expr, expected_stages):
     logic_keys = reserved.keys()
     stages = TwisterRunner.get_cmake_filter_stages(filter_expr, logic_keys)
