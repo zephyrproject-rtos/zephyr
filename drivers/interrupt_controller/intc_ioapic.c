2684c06cc38bbda3c1300ca809695a5b2943b609
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2023-12-08 15:15:14 +0200

    drivers: intc_ioapic: Fix get ioapic_id
    Information about IOAPIC can be located not in the first
    DMAR Hardware Unit Definition subtable. Iterate them all.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- drivers/interrupt_controller/intc_ioapic.c
+++ drivers/interrupt_controller/intc_ioapic.c
@@ -127,9 +127,6 @@ static uint16_t ioapic_id;
 
 static bool get_vtd(void)
 {
-	union acpi_dmar_id dmar_id;
-	int inst_cnt;
-
 	if (!device_is_ready(vtd)) {
 		return false;
 	}
@@ -138,14 +135,7 @@ static bool get_vtd(void)
 		return true;
 	}
 
-	/* Assume only one PCH in system (say client platform). */
-	if (!acpi_drhd_get(ACPI_DMAR_SCOPE_TYPE_IOAPIC, NULL, &dmar_id, &inst_cnt, 1u)) {
-		return false;
-	}
-
-	ioapic_id = dmar_id.raw;
-
-	return true;
+	return acpi_dmar_ioapic_get(&ioapic_id) == 0;
 }
 #endif /* CONFIG_INTEL_VTD_ICTL && !INTEL_VTD_ICTL_XAPIC_PASSTHROUGH */
 
