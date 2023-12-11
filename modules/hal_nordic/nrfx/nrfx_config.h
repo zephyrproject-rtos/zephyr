04e4e6e5e97ff1d72959e46a0f26d727aaeaecd5
Witold Lukasik <witold.lukasik@nordicsemi.no>
2023-12-11 17:37:22 +0100

    modules: hal_nordic: nrfx: add support for nRF54L15 SOC
    Add config files for nRF54L15.
    
    Signed-off-by: Witold Lukasik <witold.lukasik@nordicsemi.no>

--- modules/hal_nordic/nrfx/nrfx_config.h
+++ modules/hal_nordic/nrfx/nrfx_config.h
@@ -698,6 +698,8 @@
     #include <nrfx_config_nrf5340_network.h>
 #elif defined(NRF9120_XXAA) || defined(NRF9160_XXAA)
     #include <nrfx_config_nrf91.h>
+#elif defined(NRF54L15_ENGA_XXAA) && defined(NRF_APPLICATION)
+    #include <nrfx_config_nrf54l15_enga_application.h>
 #else
     #error "Unknown device."
 #endif
