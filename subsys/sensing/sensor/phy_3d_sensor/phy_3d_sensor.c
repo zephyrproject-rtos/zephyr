5058355c1b405a81cfeb9a83f0b132aed8479149
Zhang Lixu <lixu.zhang@intel.com>
2023-12-11 16:56:10 +0800

    sensing: add rtio implement for sensing subsys
    1. add function sensing_iodev_submit and defined struct
       sensing_submit_config
    2. fix the issue of phy_3d_sensor
    
    Signed-off-by: Zhang Lixu <lixu.zhang@intel.com>

--- subsys/sensing/sensor/phy_3d_sensor/phy_3d_sensor.c
+++ subsys/sensing/sensor/phy_3d_sensor/phy_3d_sensor.c
@@ -102,18 +102,21 @@ static int phy_3d_sensor_init(const struct device *dev)
 {
 	const struct phy_3d_sensor_config *cfg = dev->config;
 	struct phy_3d_sensor_data *data = dev->data;
+	int i;
 
-	switch (cfg->sensor_type) {
-	case SENSING_SENSOR_TYPE_MOTION_ACCELEROMETER_3D:
-		data->custom = &custom_accel;
+	for (i = 0; i < cfg->sensor_num; i++) {
+		switch (cfg->sensor_types[i]) {
+		case SENSING_SENSOR_TYPE_MOTION_ACCELEROMETER_3D:
+			data->customs[i] = &custom_accel;
 		break;
-	case SENSING_SENSOR_TYPE_MOTION_GYROMETER_3D:
-		data->custom = &custom_gyro;
+		case SENSING_SENSOR_TYPE_MOTION_GYROMETER_3D:
+			data->customs[i] = &custom_gyro;
 		break;
-	default:
-		LOG_ERR("phy_3d_sensor doesn't support sensor type %d",
-				cfg->sensor_type);
-		return -ENOTSUP;
+		default:
+			LOG_ERR("phy_3d_sensor doesn't support sensor type %d",
+			cfg->sensor_types[i]);
+			return -ENOTSUP;
+		}
 	}
 
 	LOG_INF("%s: Underlying device: %s", dev->name, cfg->hw_dev->name);
@@ -126,38 +129,12 @@ static int phy_3d_sensor_attr_set_hyst(const struct device *dev,
 		const struct sensor_value *val)
 {
 	const struct phy_3d_sensor_config *cfg = dev->config;
-	struct phy_3d_sensor_data *data = dev->data;
-	int index = chan - SENSOR_CHAN_PRIV_START;
-	int i, min = 0;
 
-	if (index >= 0 && index < ARRAY_SIZE(data->sensitivities)) {
-		data->sensitivities[index] = *val;
-	} else if (index == SENSING_SENSITIVITY_INDEX_ALL) {
-		for (i = 0; i < ARRAY_SIZE(data->sensitivities); ++i) {
-			data->sensitivities[i] = *val;
-		}
-		min = ARRAY_SIZE(data->sensitivities) - 1;
+	if (chan == SENSOR_CHAN_ACCEL_XYZ || chan == SENSOR_CHAN_GYRO_XYZ) {
+		return sensor_attr_set(cfg->hw_dev, chan, SENSOR_ATTR_SLOPE_TH, val);
 	} else {
-		LOG_ERR("%s: set sensitivity: invalid index: %d",
-				dev->name, index);
-		return -EINVAL;
+		return sensor_attr_set(cfg->hw_dev, chan, SENSOR_ATTR_HYSTERESIS, val);
 	}
-
-	for (i = min + 1; i < ARRAY_SIZE(data->sensitivities); ++i) {
-		if ((data->sensitivities[i].val1 <
-					data->sensitivities[min].val1) ||
-				(data->sensitivities[i].val1 ==
-				 data->sensitivities[min].val1 &&
-				 data->sensitivities[i].val2 <
-				 data->sensitivities[min].val2)) {
-			min = i;
-		}
-	}
-
-	chan = data->custom->chan_all;
-
-	return sensor_attr_set(cfg->hw_dev, chan, SENSOR_ATTR_SLOPE_TH,
-			&data->sensitivities[min]);
 }
 
 static int phy_3d_sensor_attr_set(const struct device *dev,
@@ -166,7 +143,6 @@ static int phy_3d_sensor_attr_set(const struct device *dev,
 		const struct sensor_value *val)
 {
 	const struct phy_3d_sensor_config *cfg = dev->config;
-	struct phy_3d_sensor_data *data = dev->data;
 	int ret = 0;
 
 	switch (attr) {
@@ -175,7 +151,6 @@ static int phy_3d_sensor_attr_set(const struct device *dev,
 		break;
 
 	default:
-		chan = data->custom->chan_all;
 		ret = sensor_attr_set(cfg->hw_dev, chan, attr, val);
 		break;
 	}
@@ -187,8 +162,10 @@ static int phy_3d_sensor_attr_set(const struct device *dev,
 static int phy_3d_sensor_submit(const struct device *dev,
 		struct rtio_iodev_sqe *sqe)
 {
+	struct sensing_submit_config *config = (struct sensing_submit_config *)sqe->sqe.iodev->data;
 	const struct phy_3d_sensor_config *cfg = dev->config;
 	struct phy_3d_sensor_data *data = dev->data;
+	const struct phy_3d_sensor_custom *custom = data->customs[config->info_index];
 	struct sensing_sensor_value_3d_q31 *sample;
 	struct sensor_value value[PHY_3D_SENSOR_CHANNEL_NUM];
 	uint32_t buffer_len;
@@ -201,14 +178,14 @@ static int phy_3d_sensor_submit(const struct device *dev,
 		return ret;
 	}
 
-	ret = sensor_sample_fetch_chan(cfg->hw_dev, data->custom->chan_all);
+	ret = sensor_sample_fetch_chan(cfg->hw_dev, custom->chan_all);
 	if (ret) {
 		LOG_ERR("%s: sample fetch failed: %d", dev->name, ret);
 		rtio_iodev_sqe_err(sqe, ret);
 		return ret;
 	}
 
-	ret = sensor_channel_get(cfg->hw_dev, data->custom->chan_all, value);
+	ret = sensor_channel_get(cfg->hw_dev, custom->chan_all, value);
 	if (ret) {
 		LOG_ERR("%s: channel get failed: %d", dev->name, ret);
 		rtio_iodev_sqe_err(sqe, ret);
@@ -216,12 +193,11 @@ static int phy_3d_sensor_submit(const struct device *dev,
 	}
 
 	for (i = 0; i < ARRAY_SIZE(value); ++i) {
-		sample->readings[0].v[i] =
-			data->custom->sensor_value_to_q31(&value[i]);
+		sample->readings[0].v[i] = custom->sensor_value_to_q31(&value[i]);
 	}
 
 	sample->header.reading_count = 1;
-	sample->shift = data->custom->shift;
+	sample->shift = custom->shift;
 
 	LOG_DBG("%s: Sample data:\t x: %d, y: %d, z: %d",
 			dev->name,
@@ -238,6 +214,7 @@ static const struct sensor_driver_api phy_3d_sensor_api = {
 	.submit = phy_3d_sensor_submit,
 };
 
+/* To be removed */
 static const struct sensing_sensor_register_info phy_3d_sensor_reg = {
 	.flags = SENSING_SENSOR_FLAG_REPORT_ON_CHANGE,
 	.sample_size = sizeof(struct sensing_sensor_value_3d_q31),
@@ -245,18 +222,26 @@ static const struct sensing_sensor_register_info phy_3d_sensor_reg = {
 	.version.value = SENSING_SENSOR_VERSION(0, 8, 0, 0),
 };
 
-#define SENSING_PHY_3D_SENSOR_DT_DEFINE(_inst)				\
-	static struct phy_3d_sensor_data _CONCAT(data, _inst);		\
-	static const struct phy_3d_sensor_config _CONCAT(cfg, _inst) = {\
-		.hw_dev = DEVICE_DT_GET(				\
-				DT_PHANDLE(DT_DRV_INST(_inst),		\
-				underlying_device)),			\
-		.sensor_type = DT_PROP_BY_IDX(DT_DRV_INST(_inst), sensor_types, 0),\
-	};								\
-	SENSING_SENSORS_DT_INST_DEFINE(_inst, &phy_3d_sensor_reg, NULL,	\
-		&phy_3d_sensor_init, NULL,				\
-		&_CONCAT(data, _inst), &_CONCAT(cfg, _inst),		\
-		POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY,		\
+#define SENSING_PHY_3D_SENSOR_DT_DEFINE(_inst)						\
+	static struct rtio_iodev_sqe _CONCAT(sqes, _inst)[DT_INST_PROP_LEN(_inst,	\
+			sensor_types)];							\
+	static const struct phy_3d_sensor_custom *_CONCAT(customs, _inst)		\
+			[DT_INST_PROP_LEN(_inst, sensor_types)];			\
+	static struct phy_3d_sensor_data _CONCAT(data, _inst) = {			\
+		.sqes = _CONCAT(sqes, _inst),						\
+		.customs = _CONCAT(customs, _inst),					\
+	};										\
+	static const struct phy_3d_sensor_config _CONCAT(cfg, _inst) = {		\
+		.hw_dev = DEVICE_DT_GET(						\
+				DT_PHANDLE(DT_DRV_INST(_inst),				\
+				underlying_device)),					\
+		.sensor_num = DT_INST_PROP_LEN(_inst, sensor_types),			\
+		.sensor_types = DT_PROP(DT_DRV_INST(_inst), sensor_types),		\
+	};										\
+	SENSING_SENSORS_DT_INST_DEFINE(_inst, &phy_3d_sensor_reg, NULL,			\
+		&phy_3d_sensor_init, NULL,						\
+		&_CONCAT(data, _inst), &_CONCAT(cfg, _inst),				\
+		POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY,				\
 		&phy_3d_sensor_api);
 
 DT_INST_FOREACH_STATUS_OKAY(SENSING_PHY_3D_SENSOR_DT_DEFINE);
