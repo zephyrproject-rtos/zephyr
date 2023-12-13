d9c4e0b2f83cf03e91e54453c2d88dfb80ac1038
Nikolay Agishev <agishev@synopsys.com>
2023-12-13 15:50:44 +0300

    ARCMWDT: Add headers for POSIX compatibility
    Add ENODATA errno code, wich is not presented in ARCMWDT headers
    Add PATH_MAX define
    
    Signed-off-by: Nikolay Agishev <agishev@synopsys.com>

---
+++ lib/libc/arcmwdt/include/limits.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2023 Synopsys
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef LIB_LIBC_ARCMWDT_INCLUDE_LIMITS_H_
+#define LIB_LIBC_ARCMWDT_INCLUDE_LIMITS_H_
+
+#include_next <limits.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define PATH_MAX 256
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LIB_LIBC_ARCMWDT_INCLUDE_LIMITS_H_ */
