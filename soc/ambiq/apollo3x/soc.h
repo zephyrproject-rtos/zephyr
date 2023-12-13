80e3acba8f155cb64e6378b964e936fb059d867e
Hao Luo <hluo@ambiq.com>
2023-12-13 15:30:00 +0800

    soc: arm: ambiq: apollo3: Add support for Apollo3 Blue Plus SoC
    Add all required parts (new SoC family/series, device tree) for
    the Ambiq Apollo3 Blue Plus SoC.
    
    Signed-off-by: Hao Luo <hluo@ambiq.com>

---
+++ soc/ambiq/apollo3x/soc.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023 Ambiq Micro Inc. <www.ambiq.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __SOC_H__
+#define __SOC_H__
+
+#if defined(CONFIG_SOC_APOLLO3P_BLUE)
+#include <apollo3p.h>
+#elif defined(CONFIG_SOC_APOLLO3_BLUE)
+#include <apollo3.h>
+#endif
+
+#endif /* __SOC_H__ */
