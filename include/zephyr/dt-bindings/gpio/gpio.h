21e9918260437d921480027ae0d9c4149e8572a7
Mahesh Mahadevan <mahesh.mahadevan@nxp.com>
2023-12-13 16:54:55 -0600

    include: gpio: Add GPIO_INT_WAKEUP flag
    Add a flag to configure the GPIO pin interrupt as a
    wakeup source for the system.
    
    Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

--- include/zephyr/dt-bindings/gpio/gpio.h
+++ include/zephyr/dt-bindings/gpio/gpio.h
@@ -81,6 +81,10 @@
 
 /* Note: Bits 15 downto 8 are reserved for SoC specific flags. */
 
+/** Configures GPIO interrupt to wakeup the system from low power mode.
+ */
+#define GPIO_INT_WAKEUP         (1u << 28)
+
 /**
  * @}
  */
