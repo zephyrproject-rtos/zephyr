3dd04b79a53f01edeb1405a84725e157179a447b
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2023-12-14 15:21:10 +0100

    tests: Bluetooth: tester: Add initial support for HAP
    This adds initial support for Hearing Aid Profile BTP service commands.
    
    Signed-off-by: Mariusz Skamra <mariusz.skamra@codecoup.pl>

--- tests/bluetooth/tester/src/btp/bttester.h
+++ tests/bluetooth/tester/src/btp/bttester.h
@@ -120,3 +120,6 @@ uint8_t tester_unregister_mcp(void);
 
 uint8_t tester_init_mcs(void);
 uint8_t tester_unregister_mcs(void);
+
+uint8_t tester_init_hap(void);
+uint8_t tester_unregister_hap(void);
