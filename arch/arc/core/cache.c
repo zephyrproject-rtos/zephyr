a0bc892781a5583e9964ff05a1393ce1f3ffe88d
Benedikt Schmidt <benedikt.schmidt@embedded-solutions.at>
2023-12-18 10:49:10 +0100

    arch: arc: use sys_cache instead of arch-function for enabling the cache
    Use sys_cache_data_enable instead of arch_dcache_enable to enable
    the cache. This will ensure that CONFIG_CACHE_MANAGEMENT
    is considered correctly.
    
    Signed-off-by: Benedikt Schmidt <benedikt.schmidt@embedded-solutions.at>

--- arch/arc/core/cache.c
+++ arch/arc/core/cache.c
@@ -218,8 +218,7 @@ int arch_icache_flush_and_invd_range(void *addr, size_t size)
 
 static int init_dcache(void)
 {
-
-	arch_dcache_enable();
+	sys_cache_data_enable();
 
 #if defined(CONFIG_DCACHE_LINE_SIZE_DETECT)
 	init_dcache_line_size();
