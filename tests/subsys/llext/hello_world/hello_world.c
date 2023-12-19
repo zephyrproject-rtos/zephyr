61036d6c9a6b57dc68bffca12df54b52f7426722
Luca Burelli <l.burelli@arduino.cc>
2023-12-19 17:05:29 +0100

    llext: enlarge test coverage
    This commit adds code that verifies that the llext linker is able to
    link variables with different linkage types and that they are placed
    in the correct sections. A successful test should print the sequence
    of numbers from 1 to 6 on the console, and the addresses of the
    variables grouped together by memory location.
    
    This currently fails on both Xtensa and ARM targets, since the llext
    linker ignores everything but global functions.
    
    With qemu_xtensa:
    	Testing number sequence: 1 0 6 4 6 6
    	.text variables: 0xc8d8 0
    	.data variables: 0xda24 0
    	.bss  variables: 0 0
    
    With qemu_cortex_r5:
    	Testing number sequence: 1 2 6 4 6 6
    	.text variables: 0xe75c 0
    	.data variables: 0xe74c 0
    	.bss  variables: 0 0
    
    Signed-off-by: Luca Burelli <l.burelli@arduino.cc>

--- tests/subsys/llext/hello_world/hello_world.c
+++ tests/subsys/llext/hello_world/hello_world.c
@@ -16,11 +16,34 @@
 
 extern void printk(char *fmt, ...);
 
-static const uint32_t number = 42;
+static const uint32_t static_const = 1; /* .text, file-local linkage */
+static uint32_t static_var = 2;         /* .data, file-local linkage */
+uint32_t static_bss; /* = 3 */          /* .bss,  file-local linkage */
 
-void hello_world(void)
+const uint32_t global_const = 4;        /* .text, global linkage */
+uint32_t global_var = 5;                /* .data, global linkage */
+uint32_t global_bss; /* = 6 */          /* .bss,  global linkage */
+
+extern void hello_world(void)
 {
 	printk("hello world\n");
-	printk("A number is %lu\n", number);
+
+	/* Set BSS variables to their expected values */
+	static_bss = 3;
+	global_bss = 6;
+
+	/* Print all defined variables in sequence */
+	printk("Testing number sequence:");
+	printk(" %lu", static_const);
+	printk(" %lu", static_var);
+	printk(" %lu", static_bss);
+	printk(" %lu", global_const);
+	printk(" %lu", global_var);
+	printk(" %lu\n", global_bss);
+
+	/* Print linked variable addresses by group */
+	printk(".text variables: %p %p\n", &static_const, &global_const);
+	printk(".data variables: %p %p\n", &static_var, &global_var);
+	printk(".bss  variables: %p %p\n", &static_bss, &global_bss);
 }
 LL_EXTENSION_SYMBOL(hello_world);
