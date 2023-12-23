b6bc79d215cda84b4441836c9b96939ef1b662f5
Yong Cong Sin <ycsin@meta.com>
2023-12-23 17:35:24 +0800

    [nrf fromtree] soc: riscv: add support for Renode Virt RISCV32 SoC
    Add a beef-ed up version of Renode's `riscv_virt` SoC
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>
    (cherry picked from commit c328a38a94962cc64325b525fad8013331291e35)
    (cherry picked from commit c69028760cda83bad337c9acecda745e1a15b359)

---
+++ soc/riscv/riscv-privileged/renode_virt/soc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023 Meta
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __RISCV32_RENODE_SOC_H_
+#define __RISCV32_RENODE_SOC_H_
+
+#include <soc_common.h>
+
+#endif /* __RISCV32_RENODE_SOC_H_ */
