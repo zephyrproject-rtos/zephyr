18659820d9a6d23f815acbcf79e76668a5761686
Pisit Sawangvonganan <pisit@ndrsolution.com>
2023-12-25 00:10:03 +0700

    drivers: rtc: set 'rtc_driver_api' as 'static const'
    This change marks each instance of the 'rtc_driver_api' as 'static const'.
    The rationale is that 'rtc_driver_api' is used for declaring internal
    module interfaces and is not intended to be modified at runtime.
    By using 'static const', we ensure immutability, leading to usage of only
    .rodata and a reduction in the .data area.
    
    Signed-off-by: Pisit Sawangvonganan <pisit@ndrsolution.com>

--- drivers/rtc/rtc_ll_stm32.c
+++ drivers/rtc/rtc_ll_stm32.c
@@ -390,7 +390,7 @@ static int rtc_stm32_get_calibration(const struct device *dev, int32_t *calibrat
 #endif
 #endif /* CONFIG_RTC_CALIBRATION */
 
-struct rtc_driver_api rtc_stm32_driver_api = {
+static const struct rtc_driver_api rtc_stm32_driver_api = {
 	.set_time = rtc_stm32_set_time,
 	.get_time = rtc_stm32_get_time,
 	/* RTC_ALARM not supported */
