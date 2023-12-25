810bdf83d85c35bf392fa4e25e46621fcf8ca5e1
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2023-12-26 01:00:21 +0530

    hostap: Add a connect/disconnect event helpers
    These can be abstracted from the core WPA supplicant.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- modules/hostap/src/supp_events.h
+++ modules/hostap/src/supp_events.h
@@ -52,6 +52,8 @@ int supplicant_send_wifi_mgmt_event(const char *ifname,
 int supplicant_generate_state_event(const char *ifname,
 				    enum net_event_supplicant_cmd event,
 				    int status);
+int supplicant_send_wifi_mgmt_conn_event(void *ctx, int status_code);
+int supplicant_send_wifi_mgmt_disc_event(void *ctx, int reason_code);
 
 #define REASON_CODE_LEN 18
 #define NM_WIFI_EVENT_STR_LEN 64
