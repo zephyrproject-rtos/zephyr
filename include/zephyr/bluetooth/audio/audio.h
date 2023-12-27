d6046b8b2fc38864e53231fe9f7e44c289d34188
Emil Gydesen <emil.gydesen@nordicsemi.no>
2023-12-27 13:48:46 +0100

    Bluetooth: Audio: Move assigned numbers from lc3.h to audio.h
    The assigned numbers are not specific to LC3, so they should be
    defined in the more general audio.h header file.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- include/zephyr/bluetooth/audio/audio.h
+++ include/zephyr/bluetooth/audio/audio.h
@@ -41,6 +41,240 @@ extern "C" {
 
 #define BT_AUDIO_BROADCAST_CODE_SIZE             16
 
+/**
+ * @brief Codec capability types
+ *
+ * Used to build and parse codec capabilities as specified in the PAC specification.
+ * Source is assigned numbers for Generic Audio, bluetooth.com.
+ */
+enum bt_audio_codec_cap_type {
+	/** Supported sampling frequencies */
+	BT_AUDIO_CODEC_CAP_TYPE_FREQ = 0x01,
+
+	/** Supported frame durations */
+	BT_AUDIO_CODEC_CAP_TYPE_DURATION = 0x02,
+
+	/** Supported audio channel counts */
+	BT_AUDIO_CODEC_CAP_TYPE_CHAN_COUNT = 0x03,
+
+	/** Supported octets per codec frame */
+	BT_AUDIO_CODEC_CAP_TYPE_FRAME_LEN = 0x04,
+
+	/** Supported maximum codec frames per SDU  */
+	BT_AUDIO_CODEC_CAP_TYPE_FRAME_COUNT = 0x05,
+};
+
+/** @brief Supported frequencies bitfield */
+enum bt_audio_codec_cap_freq {
+	/** 8 Khz sampling frequency */
+	BT_AUDIO_CODEC_CAP_FREQ_8KHZ = BIT(0),
+
+	/** 11.025 Khz sampling frequency */
+	BT_AUDIO_CODEC_CAP_FREQ_11KHZ = BIT(1),
+
+	/** 16 Khz sampling frequency */
+	BT_AUDIO_CODEC_CAP_FREQ_16KHZ = BIT(2),
+
+	/** 22.05 Khz sampling frequency */
+	BT_AUDIO_CODEC_CAP_FREQ_22KHZ = BIT(3),
+
+	/** 24 Khz sampling frequency */
+	BT_AUDIO_CODEC_CAP_FREQ_24KHZ = BIT(4),
+
+	/** 32 Khz sampling frequency */
+	BT_AUDIO_CODEC_CAP_FREQ_32KHZ = BIT(5),
+
+	/** 44.1 Khz sampling frequency */
+	BT_AUDIO_CODEC_CAP_FREQ_44KHZ = BIT(6),
+
+	/** 48 Khz sampling frequency */
+	BT_AUDIO_CODEC_CAP_FREQ_48KHZ = BIT(7),
+
+	/** 88.2 Khz sampling frequency */
+	BT_AUDIO_CODEC_CAP_FREQ_88KHZ = BIT(8),
+
+	/** 96 Khz sampling frequency */
+	BT_AUDIO_CODEC_CAP_FREQ_96KHZ = BIT(9),
+
+	/** 176.4 Khz sampling frequency */
+	BT_AUDIO_CODEC_CAP_FREQ_176KHZ = BIT(10),
+
+	/** 192 Khz sampling frequency */
+	BT_AUDIO_CODEC_CAP_FREQ_192KHZ = BIT(11),
+
+	/** 384 Khz sampling frequency */
+	BT_AUDIO_CODEC_CAP_FREQ_384KHZ = BIT(12),
+
+	/** Any frequency capability */
+	BT_AUDIO_CODEC_CAP_FREQ_ANY =
+		(BT_AUDIO_CODEC_CAP_FREQ_8KHZ | BT_AUDIO_CODEC_CAP_FREQ_11KHZ |
+		 BT_AUDIO_CODEC_CAP_FREQ_16KHZ | BT_AUDIO_CODEC_CAP_FREQ_22KHZ |
+		 BT_AUDIO_CODEC_CAP_FREQ_24KHZ | BT_AUDIO_CODEC_CAP_FREQ_32KHZ |
+		 BT_AUDIO_CODEC_CAP_FREQ_44KHZ | BT_AUDIO_CODEC_CAP_FREQ_48KHZ |
+		 BT_AUDIO_CODEC_CAP_FREQ_88KHZ | BT_AUDIO_CODEC_CAP_FREQ_96KHZ |
+		 BT_AUDIO_CODEC_CAP_FREQ_176KHZ | BT_AUDIO_CODEC_CAP_FREQ_192KHZ |
+		 BT_AUDIO_CODEC_CAP_FREQ_384KHZ),
+};
+
+/** @brief Supported frame durations bitfield */
+enum bt_audio_codec_cap_frame_dur {
+	/** 7.5 msec frame duration capability */
+	BT_AUDIO_CODEC_CAP_DURATION_7_5 = BIT(0),
+
+	/** 10 msec frame duration capability */
+	BT_AUDIO_CODEC_CAP_DURATION_10 = BIT(1),
+
+	/** Any frame duration capability */
+	BT_AUDIO_CODEC_CAP_DURATION_ANY =
+		(BT_AUDIO_CODEC_CAP_DURATION_7_5 | BT_AUDIO_CODEC_CAP_DURATION_10),
+
+	/**
+	 * @brief 7.5 msec preferred frame duration capability.
+	 *
+	 * This shall only be set if @ref BT_AUDIO_CODEC_CAP_DURATION_7_5 is also set, and if @ref
+	 * BT_AUDIO_CODEC_CAP_DURATION_PREFER_10 is not set.
+	 */
+	BT_AUDIO_CODEC_CAP_DURATION_PREFER_7_5 = BIT(4),
+
+	/**
+	 * @brief 10 msec preferred frame duration capability
+	 *
+	 * This shall only be set if @ref BT_AUDIO_CODEC_CAP_DURATION_10 is also set, and if @ref
+	 * BT_AUDIO_CODEC_CAP_DURATION_PREFER_7_5 is not set.
+	 */
+	BT_AUDIO_CODEC_CAP_DURATION_PREFER_10 = BIT(5),
+};
+
+enum bt_audio_codec_cap_chan_count {
+	/** Supporting 1 channel */
+	BT_AUDIO_CODEC_CAP_CHAN_COUNT_1 = BIT(0),
+
+	/** Supporting 2 channel */
+	BT_AUDIO_CODEC_CAP_CHAN_COUNT_2 = BIT(1),
+
+	/** Supporting 3 channel */
+	BT_AUDIO_CODEC_CAP_CHAN_COUNT_3 = BIT(2),
+
+	/** Supporting 4 channel */
+	BT_AUDIO_CODEC_CAP_CHAN_COUNT_4 = BIT(3),
+
+	/** Supporting 5 channel */
+	BT_AUDIO_CODEC_CAP_CHAN_COUNT_5 = BIT(4),
+
+	/** Supporting 6 channel */
+	BT_AUDIO_CODEC_CAP_CHAN_COUNT_6 = BIT(5),
+
+	/** Supporting 7 channel */
+	BT_AUDIO_CODEC_CAP_CHAN_COUNT_7 = BIT(6),
+
+	/** Supporting 8 channel */
+	BT_AUDIO_CODEC_CAP_CHAN_COUNT_8 = BIT(7),
+
+	/** Supporting all channels */
+	BT_AUDIO_CODEC_CAP_CHAN_COUNT_ANY =
+		(BT_AUDIO_CODEC_CAP_CHAN_COUNT_1 | BT_AUDIO_CODEC_CAP_CHAN_COUNT_2 |
+		 BT_AUDIO_CODEC_CAP_CHAN_COUNT_3 | BT_AUDIO_CODEC_CAP_CHAN_COUNT_4 |
+		 BT_AUDIO_CODEC_CAP_CHAN_COUNT_5 | BT_AUDIO_CODEC_CAP_CHAN_COUNT_6 |
+		 BT_AUDIO_CODEC_CAP_CHAN_COUNT_7 | BT_AUDIO_CODEC_CAP_CHAN_COUNT_8),
+};
+
+/** Minimum supported channel counts */
+#define BT_AUDIO_CODEC_CAP_CHAN_COUNT_MIN 1
+/** Maximum supported channel counts */
+#define BT_AUDIO_CODEC_CAP_CHAN_COUNT_MAX 8
+
+/**
+ *  @brief Channel count support capability
+ *
+ *  Macro accepts variable number of channel counts.
+ *  The allowed channel counts are defined by specification and have to be in range from
+ *  @ref BT_AUDIO_CODEC_CAP_CHAN_COUNT_MIN to @ref BT_AUDIO_CODEC_CAP_CHAN_COUNT_MAX inclusive.
+ *
+ *  Example to support 1 and 3 channels:
+ *    BT_AUDIO_CODEC_CAP_CHAN_COUNT_SUPPORT(1, 3)
+ */
+#define BT_AUDIO_CODEC_CAP_CHAN_COUNT_SUPPORT(...)                                                 \
+	((enum bt_audio_codec_cap_chan_count)((FOR_EACH(BIT, (|), __VA_ARGS__)) >> 1))
+
+struct bt_audio_codec_octets_per_codec_frame {
+	/** Minimum number of octets supported per codec frame */
+	uint16_t min;
+	/** Maximum number of octets supported per codec frame */
+	uint16_t max;
+};
+
+/**
+ * @brief Codec configuration types
+ *
+ * Used to build and parse codec configurations as specified in the ASCS and BAP specifications.
+ * Source is assigned numbers for Generic Audio, bluetooth.com.
+ */
+enum bt_audio_codec_cfg_type {
+	/** Sampling frequency */
+	BT_AUDIO_CODEC_CFG_FREQ = 0x01,
+
+	/** Frame duration */
+	BT_AUDIO_CODEC_CFG_DURATION = 0x02,
+
+	/** Audio channel allocation */
+	BT_AUDIO_CODEC_CFG_CHAN_ALLOC = 0x03,
+
+	/** Octets per codec frame */
+	BT_AUDIO_CODEC_CFG_FRAME_LEN = 0x04,
+
+	/** Codec frame blocks per SDU */
+	BT_AUDIO_CODEC_CFG_FRAME_BLKS_PER_SDU = 0x05,
+};
+
+enum bt_audio_codec_cfg_freq {
+	/** 8 Khz codec sampling frequency */
+	BT_AUDIO_CODEC_CFG_FREQ_8KHZ = 0x01,
+
+	/** 11.025 Khz codec sampling frequency */
+	BT_AUDIO_CODEC_CFG_FREQ_11KHZ = 0x02,
+
+	/** 16 Khz codec sampling frequency */
+	BT_AUDIO_CODEC_CFG_FREQ_16KHZ = 0x03,
+
+	/** 22.05 Khz codec sampling frequency */
+	BT_AUDIO_CODEC_CFG_FREQ_22KHZ = 0x04,
+
+	/** 24 Khz codec sampling frequency */
+	BT_AUDIO_CODEC_CFG_FREQ_24KHZ = 0x05,
+
+	/** 32 Khz codec sampling frequency */
+	BT_AUDIO_CODEC_CFG_FREQ_32KHZ = 0x06,
+
+	/** 44.1 Khz codec sampling frequency */
+	BT_AUDIO_CODEC_CFG_FREQ_44KHZ = 0x07,
+
+	/** 48 Khz codec sampling frequency */
+	BT_AUDIO_CODEC_CFG_FREQ_48KHZ = 0x08,
+
+	/** 88.2 Khz codec sampling frequency */
+	BT_AUDIO_CODEC_CFG_FREQ_88KHZ = 0x09,
+
+	/** 96 Khz codec sampling frequency */
+	BT_AUDIO_CODEC_CFG_FREQ_96KHZ = 0x0a,
+
+	/** 176.4 Khz codec sampling frequency */
+	BT_AUDIO_CODEC_CFG_FREQ_176KHZ = 0x0b,
+
+	/** 192 Khz codec sampling frequency */
+	BT_AUDIO_CODEC_CFG_FREQ_192KHZ = 0x0c,
+
+	/** 384 Khz codec sampling frequency */
+	BT_AUDIO_CODEC_CFG_FREQ_384KHZ = 0x0d,
+};
+
+enum bt_audio_codec_cfg_frame_dur {
+	/** 7.5 msec Frame Duration configuration */
+	BT_AUDIO_CODEC_CFG_DURATION_7_5 = 0x00,
+
+	/** 10 msec Frame Duration configuration */
+	BT_AUDIO_CODEC_CFG_DURATION_10 = 0x01,
+};
+
 /** @brief Audio Context Type for Generic Audio
  *
  * These values are defined by the Generic Audio Assigned Numbers, bluetooth.com
