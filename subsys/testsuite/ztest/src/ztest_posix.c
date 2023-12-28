832d34547e805e6863f24634f93c5a6eefbefe9d
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2023-12-28 11:22:52 +0100

    ztest native extensions: Set standard source macro appropriately
    This file uses strtok_r which is an extension to the the
    std C library. Let's explicity select one of the extensions
    which includes it instead of relaying on somebody having
    set it for this file somewhere else.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- subsys/testsuite/ztest/src/ztest_posix.c
+++ subsys/testsuite/ztest/src/ztest_posix.c
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#undef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200809L
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "cmdline.h" /* native_posix command line options header */
