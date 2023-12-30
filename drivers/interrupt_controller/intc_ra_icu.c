4b26c3cc7c3a7f52867aeb3b1826af3efc5312fa
TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>
2023-12-30 21:34:56 +0900

    drivers: interrupt_controller: ra_icu: minor cleanups
    - Corrected indentation with clang-format
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>

--- drivers/interrupt_controller/intc_ra_icu.c
+++ drivers/interrupt_controller/intc_ra_icu.c
@@ -121,5 +121,4 @@ int ra_icu_irq_disconnect_dynamic(unsigned int irq, unsigned int priority,
 	return 0;
 }
 
-DEVICE_DT_INST_DEFINE(0, NULL, NULL, NULL, NULL, PRE_KERNEL_1, CONFIG_INTC_INIT_PRIORITY,
-		      NULL);
+DEVICE_DT_INST_DEFINE(0, NULL, NULL, NULL, NULL, PRE_KERNEL_1, CONFIG_INTC_INIT_PRIORITY, NULL);
