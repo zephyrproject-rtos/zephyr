c0d7d844ebad596699a901d84388e469d422bd89
Abderrahmane Jarmouni <abderrahmane.jarmouni-ext@st.com>
2023-12-31 17:41:30 +0100

    soc: arm: st_stm32: common: wake-up source subsystem
    Add support for wake-up source subsystem proposed in pr64814.
    We introduce macros for parsing & storing DT wake-up pins config in C
    structs.
    Then we implement wake-up source subsys' function for configuring a
    wake-up pin with a GPIO as a source.
    
    Signed-off-by: Abderrahmane Jarmouni <abderrahmane.jarmouni-ext@st.com>

---
+++ soc/st/stm32/common/wkup_pins_ctrl.h
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2024 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * STM32 SoC specific definitions for wake-up pins config
+ */
+
+#ifndef ZEPHYR_SOC_ARM_ST_STM32_COMMON_WKUP_PINS_CTRL_H_
+#define ZEPHYR_SOC_ARM_ST_STM32_COMMON_WKUP_PINS_CTRL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Some stm32 devices do not have all the LL_PWR_WAKEUP_PINx */
+#define LL_PWR_WAKEUP_PIN0 0
+#ifndef LL_PWR_WAKEUP_PIN3
+#define LL_PWR_WAKEUP_PIN3 0
+#endif /* LL_PWR_WAKEUP_PIN3 */
+#ifndef LL_PWR_WAKEUP_PIN5
+#define LL_PWR_WAKEUP_PIN5 0
+#endif /* LL_PWR_WAKEUP_PIN5 */
+#ifndef LL_PWR_WAKEUP_PIN6
+#define LL_PWR_WAKEUP_PIN6 0
+#endif /* LL_PWR_WAKEUP_PIN6 */
+#ifndef LL_PWR_WAKEUP_PIN7
+#define LL_PWR_WAKEUP_PIN7 0
+#endif /* LL_PWR_WAKEUP_PIN7 */
+#ifndef LL_PWR_WAKEUP_PIN8
+#define LL_PWR_WAKEUP_PIN8 0
+#endif /* LL_PWR_WAKEUP_PIN8 */
+
+/* Some stm32 devices do not have all the LL_PWR_GPIO_X */
+#ifndef LL_PWR_GPIO_A
+#define LL_PWR_GPIO_A 0
+#endif /* LL_PWR_GPIO_A */
+#ifndef LL_PWR_GPIO_B
+#define LL_PWR_GPIO_B 0
+#endif /* LL_PWR_WGPIO_B */
+#ifndef LL_PWR_GPIO_C
+#define LL_PWR_GPIO_C 0
+#endif /* LL_PWR_GPIO_C */
+#ifndef LL_PWR_GPIO_D
+#define LL_PWR_GPIO_D 0
+#endif /* LL_PWR_GPIO_D */
+#ifndef LL_PWR_GPIO_E
+#define LL_PWR_GPIO_E 0
+#endif /* LL_PWR_GPIO_E */
+#ifndef LL_PWR_GPIO_F
+#define LL_PWR_GPIO_F 0
+#endif /* LL_PWR_GPIO_F */
+#ifndef LL_PWR_GPIO_G
+#define LL_PWR_GPIO_G 0
+#endif /* LL_PWR_GPIO_G */
+#ifndef LL_PWR_GPIO_H
+#define LL_PWR_GPIO_H 0
+#endif /* LL_PWR_GPIO_H */
+#ifndef LL_PWR_GPIO_I
+#define LL_PWR_GPIO_I 0
+#endif /* LL_PWR_GPIO_I */
+#ifndef LL_PWR_GPIO_J
+#define LL_PWR_GPIO_J 0
+#endif /* LL_PWR_GPIO_J */
+
+/* Some STM32U5 SoCs do not have all the LL_PWR_GPIO_PORTX */
+#if defined(CONFIG_SOC_SERIES_STM32U5X)
+#ifndef LL_PWR_GPIO_PORTF
+#define LL_PWR_GPIO_PORTF 0
+#endif /* LL_PWR_GPIO_PORTF */
+#ifndef LL_PWR_GPIO_PORTI
+#define LL_PWR_GPIO_PORTI 0
+#endif /* LL_PWR_GPIO_PORTI */
+#ifndef LL_PWR_GPIO_PORTJ
+#define LL_PWR_GPIO_PORTJ 0
+#endif /* LL_PWR_GPIO_PORTJ */
+#endif /* CONFIG_SOC_SERIES_STM32U5X */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_SOC_ARM_ST_STM32_COMMON_WKUP_PINS_CTRL_H_ */
--- soc/st/stm32/stm32l4x/poweroff.c
+++ soc/st/stm32/stm32l4x/poweroff.c
@@ -11,8 +11,24 @@
 #include <stm32_ll_pwr.h>
 #include <stm32_ll_system.h>
 
+#ifdef CONFIG_WAKEUP_SOURCE
+#if DT_HAS_COMPAT_STATUS_OKAY(st_stm32_pwr)
+#define DT_DRV_COMPAT st_stm32_pwr
+#define PWR_STM32_WKUP_PINS_PULL_CFG DT_INST_PROP_OR(0, wkup_pins_pull, 0)
+#endif
+#endif /* CONFIG_WAKEUP_SOURCE */
+
 void z_sys_poweroff(void)
 {
+#ifdef CONFIG_WAKEUP_SOURCE
+#if PWR_STM32_WKUP_PINS_PULL_CFG
+	LL_PWR_EnablePUPDCfg();
+#else
+	LL_PWR_DisablePUPDCfg();
+#endif /* PWR_STM32_WKUP_PINS_PULL_CFG */
+	LL_PWR_ClearFlag_WU();
+#endif /* CONFIG_WAKEUP_SOURCE */
+
 	LL_PWR_SetPowerMode(LL_PWR_MODE_SHUTDOWN);
 	LL_LPM_EnableDeepSleep();
 	LL_DBGMCU_DisableDBGStandbyMode();
