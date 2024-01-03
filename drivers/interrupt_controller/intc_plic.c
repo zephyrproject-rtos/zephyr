13faa390f1fd7eaafbc55e0ff9ab2c6bec4dc1e5
Gerard Marull-Paretas <gerard@teslabs.com>
2024-01-03 12:39:36 +0100

    arch: riscv: introduce RISCV_PRIVILEGED
    Introduce a new arch level Kconfig option to signal the implementation
    of the RISCV Privileged ISA spec. This replaces
    SOC_FAMILY_RISCV_PRIVILEGED, because this is not a SoC specific
    property, nor a SoC family.
    
    Note that the SoC family naming scheme will be fixed in upcoming
    commits.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- drivers/interrupt_controller/intc_plic.c
+++ drivers/interrupt_controller/intc_plic.c
@@ -228,7 +228,7 @@ static void plic_irq_enable_set_state(uint32_t irq, bool enable)
  * @brief Enable a riscv PLIC-specific interrupt line
  *
  * This routine enables a RISCV PLIC-specific interrupt line.
- * riscv_plic_irq_enable is called by SOC_FAMILY_RISCV_PRIVILEGED
+ * riscv_plic_irq_enable is called by RISCV_PRIVILEGED
  * arch_irq_enable function to enable external interrupts for
  * IRQS level == 2, whenever CONFIG_RISCV_HAS_PLIC variable is set.
  *
@@ -243,7 +243,7 @@ void riscv_plic_irq_enable(uint32_t irq)
  * @brief Disable a riscv PLIC-specific interrupt line
  *
  * This routine disables a RISCV PLIC-specific interrupt line.
- * riscv_plic_irq_disable is called by SOC_FAMILY_RISCV_PRIVILEGED
+ * riscv_plic_irq_disable is called by RISCV_PRIVILEGED
  * arch_irq_disable function to disable external interrupts, for
  * IRQS level == 2, whenever CONFIG_RISCV_HAS_PLIC variable is set.
  *
