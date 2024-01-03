4bd927d24f8028777534a0f9be73aed47b71ac88
Gerard Marull-Paretas <gerard@teslabs.com>
2024-01-03 15:36:49 +0100

    soc: riscv: ite: reorganize SoC folder
    Follow the vendor structure [1]:
    
    - Family: ITE Embedded Controller SoCs
    - Series: IT8XXX2
    - SoCs: IT81202BX, IT81202CX, etc.
    
    [1]: https://www.ite.com.tw/en/product/category?cid=1
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- tests/drivers/gpio/gpio_ite_it8xxx2_v2/include/chip_chipregs.h
+++ tests/drivers/gpio/gpio_ite_it8xxx2_v2/include/chip_chipregs.h
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <../soc/riscv/riscv-ite/common/chip_chipregs.h>
+#include <../soc/riscv/ite_ec/common/chip_chipregs.h>
 
 /*
  * Macros for emulated hardware registers access.
