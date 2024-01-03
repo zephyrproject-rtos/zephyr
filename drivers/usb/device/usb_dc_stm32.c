730abef01abcb39892ed82e81acb1dcfaf0b3876
Francois Ramu <francois.ramu@st.com>
2024-01-03 12:58:58 +0100

    drivers: usb: stm32U5 usb device controller
    Like the stm32H5, stm32u5 usb device has an independent
    power supply, but control bit is PWR_SVMCR_USV.
    The control bit for the stm32H5 is PWR_USBSCR_USB33SV (no change)
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- drivers/usb/device/usb_dc_stm32.c
+++ drivers/usb/device/usb_dc_stm32.c
@@ -217,13 +217,14 @@ static int usb_dc_stm32_clock_enable(void)
 		return -ENODEV;
 	}
 
-#ifdef PWR_USBSCR_USB33SV
+#if defined(PWR_USBSCR_USB33SV) || defined(PWR_SVMCR_USV)
+
 	/*
 	 * VDDUSB independent USB supply (PWR clock is on)
 	 * with LL_PWR_EnableVDDUSB function (higher case)
 	 */
 	LL_PWR_EnableVDDUSB();
-#endif /* PWR_USBSCR_USB33SV */
+#endif /* PWR_USBSCR_USB33SV or PWR_SVMCR_USV */
 
 	if (DT_INST_NUM_CLOCKS(0) > 1) {
 		if (clock_control_configure(clk, (clock_control_subsys_t)&pclken[1],
