01178318dc38a5b301fdb122d2ecadfed0c7814d
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2024-01-05 12:38:49 +0100

    tests bsim/bt/ll: Split build script in 3
    The CIS and connected tests are building quite many
    apps on their own.
    Let's split them in their separate sub-scripts,
    so we don't parallelize too many builds,
    and users have more granurality if they only
    want to build a subset.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- tests/bsim/bluetooth/ll/compile.sh
+++ tests/bsim/bluetooth/ll/compile.sh
@@ -18,31 +18,17 @@ mkdir -p ${WORK_DIR}
 
 source ${ZEPHYR_BASE}/tests/bsim/compile.source
 
+${ZEPHYR_BASE}/tests/bsim/bluetooth/ll/conn/compile.sh
+${ZEPHYR_BASE}/tests/bsim/bluetooth/ll/cis/compile.sh
+
 app=tests/bsim/bluetooth/ll/advx compile
 app=tests/bsim/bluetooth/ll/advx \
   conf_overlay=overlay-ticker_expire_info.conf compile
-
-app=tests/bsim/bluetooth/ll/conn conf_file=prj_split.conf compile
-app=tests/bsim/bluetooth/ll/conn conf_file=prj_split_privacy.conf compile
-app=tests/bsim/bluetooth/ll/conn conf_file=prj_split_low_lat.conf compile
-app=tests/bsim/bluetooth/ll/conn conf_file=prj_split_single_timer.conf compile
 
 app=tests/bsim/bluetooth/ll/bis compile
 app=tests/bsim/bluetooth/ll/bis \
   conf_overlay=overlay-ticker_expire_info.conf compile
 app=tests/bsim/bluetooth/ll/bis conf_file=prj_vs_dp.conf compile
-
-app=tests/bsim/bluetooth/ll/cis conf_overlay=overlay.conf compile
-app=tests/bsim/bluetooth/ll/cis conf_overlay=overlay-acl_first.conf compile
-app=tests/bsim/bluetooth/ll/cis conf_overlay=overlay-legacy_adv.conf compile
-app=tests/bsim/bluetooth/ll/cis conf_overlay=overlay-legacy_adv_acl_first.conf compile
-app=tests/bsim/bluetooth/ll/cis conf_overlay=overlay-acl_group.conf compile
-app=tests/bsim/bluetooth/ll/cis conf_overlay=overlay-acl_group_acl_first.conf compile
-app=tests/bsim/bluetooth/ll/cis conf_overlay=overlay-peripheral_cis.conf compile
-app=tests/bsim/bluetooth/ll/cis conf_overlay=overlay-acl_first_ft_per_skip_2_se.conf compile
-app=tests/bsim/bluetooth/ll/cis conf_overlay=overlay-acl_first_ft_per_skip_4_se.conf compile
-app=tests/bsim/bluetooth/ll/cis conf_overlay=overlay-acl_first_ft_cen_skip_2_se.conf compile
-app=tests/bsim/bluetooth/ll/cis conf_overlay=overlay-acl_first_ft_cen_skip_4_se.conf compile
 
 app=tests/bsim/bluetooth/ll/edtt/hci_test_app \
   conf_file=prj_dut_llcp.conf compile
