573fe77fd269944fe57d6d9539908f2818a6cba5
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2024-01-05 11:24:18 +0100

    tests bsim bt: Add basic connection test with HCI UART controllers
    Add a basic connection test between two devices,
    in which the controllers are in separate nrf52_bsim devices
    connected over UART to the devices running the host+app.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- tests/bsim/bluetooth/compile.sh
+++ tests/bsim/bluetooth/compile.sh
@@ -28,5 +28,8 @@ ${ZEPHYR_BASE}/tests/bsim/bluetooth/audio_samples/compile.sh
 ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/compile.sh
 ${ZEPHYR_BASE}/tests/bsim/bluetooth/ll/compile.sh
 ${ZEPHYR_BASE}/tests/bsim/bluetooth/mesh/compile.sh
+if [ ${BOARD} == "nrf52_bsim" ]; then
+	${ZEPHYR_BASE}/tests/bsim/bluetooth/hci_uart/compile.sh
+fi
 
 wait_for_background_jobs
