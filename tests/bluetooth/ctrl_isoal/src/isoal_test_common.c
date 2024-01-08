ec869c65367bd72f0d386beb1c57f8d7bc59d6e4
Dominik Ermel <dominik.ermel@nordicsemi.no>
2024-01-08 12:29:52 +0000

    Revert "[nrf fromtree] test: bluetooth: controller: Updated ISO-AL tests to cover endianness"
    This reverts commit 78f271634db1df8383b6e32e46e750c428e264fc.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- tests/bluetooth/ctrl_isoal/src/isoal_test_common.c
+++ tests/bluetooth/ctrl_isoal/src/isoal_test_common.c
@@ -23,7 +23,6 @@
 #include <zephyr/kernel.h>
 
 #include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/sys/byteorder.h>
 
 #include "util/memq.h"
 
@@ -110,7 +109,7 @@ void isoal_test_create_unframed_pdu(uint8_t llid,
 uint16_t isoal_test_insert_segment(bool sc, bool cmplt, uint32_t time_offset, uint8_t *dataptr,
 				uint8_t length, struct isoal_pdu_rx *pdu_meta)
 {
-	uint8_t seg_hdr[PDU_ISO_SEG_HDR_SIZE + PDU_ISO_SEG_TIMEOFFSET_SIZE];
+	struct pdu_iso_sdu_sh seg_hdr;
 	uint16_t pdu_payload_size;
 	uint8_t hdr_write_size;
 	uint16_t pdu_data_loc;
@@ -123,13 +122,12 @@ uint16_t isoal_test_insert_segment(bool sc, bool cmplt, uint32_t time_offset, ui
 	zassert_true(pdu_payload_size <= TEST_RX_PDU_PAYLOAD_MAX,
 		"pdu_payload_size (%d)", pdu_payload_size);
 
-	/* Write header independent of endian dependent structures */
-	WRITE_BIT(seg_hdr[0], 0, sc); /* sc */
-	WRITE_BIT(seg_hdr[0], 1, cmplt); /* cmplt */
-	seg_hdr[1] = length + (sc ? 0 : PDU_ISO_SEG_TIMEOFFSET_SIZE);
+	seg_hdr.sc = sc;
+	seg_hdr.cmplt = cmplt;
+	seg_hdr.len = length + (sc ? 0 : PDU_ISO_SEG_TIMEOFFSET_SIZE);
 
 	if (!sc) {
-		sys_put_le24(time_offset, &seg_hdr[PDU_ISO_SEG_HDR_SIZE]);
+		seg_hdr.timeoffset = time_offset;
 	}
 
 	memcpy(&pdu_meta->pdu->payload[pdu_meta->pdu->len], &seg_hdr, hdr_write_size);
