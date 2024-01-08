b8d42f0f7be3a22acb6f511d9536c6fa2f5ee6d5
Dominik Ermel <dominik.ermel@nordicsemi.no>
2024-01-08 12:30:10 +0000

    Revert "[nrf fromtree] Bluetooth: Controller: Use DIV_ROUND_UP macro in HAL ticker"
    This reverts commit f5d51232a709225b77d6a2e9bfe25213e74637d5.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- tests/bluetooth/controller/mock_ctrl/include/hal/ticker_vendor_hal.h
+++ tests/bluetooth/controller/mock_ctrl/include/hal/ticker_vendor_hal.h
@@ -37,8 +37,9 @@
  */
 #define HAL_TICKER_US_TO_TICKS_CEIL(x) \
 	( \
-		DIV_ROUND_UP(((uint64_t) (x) * HAL_TICKER_FSEC_PER_USEC), \
-			     HAL_TICKER_CNTR_CLK_UNIT_FSEC) & \
+		((uint32_t)(((((uint64_t) (x) * HAL_TICKER_FSEC_PER_USEC) + \
+			      HAL_TICKER_CNTR_CLK_UNIT_FSEC - 1U)) / \
+			    HAL_TICKER_CNTR_CLK_UNIT_FSEC)) & \
 		HAL_TICKER_CNTR_MASK \
 	)
 
