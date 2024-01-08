8ea8caaba3494afbfa68892eda98a55461ce0050
Dominik Ermel <dominik.ermel@nordicsemi.no>
2024-01-08 12:30:08 +0000

    Revert "[nrf fromtree] Bluetooth: Controller: Fix order of preempt timeout requested flag"
    This reverts commit 8246e66a8ecaa438b87a025d7d02312ad239a0ce.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll.c
@@ -835,17 +835,11 @@ static void ticker_start_op_cb(uint32_t status, void *param)
 	ARG_UNUSED(param);
 	LL_ASSERT(status == TICKER_STATUS_SUCCESS);
 
-	/* Increase preempt requested count before acknowledging that the
-	 * ticker start operation for the preempt timeout has been handled.
-	 */
-	LL_ASSERT(preempt_req == preempt_ack);
-	preempt_req++;
-
-	/* Increase preempt start ack count, to acknowledge that the ticker
-	 * start operation has been handled.
-	 */
 	LL_ASSERT(preempt_start_req != preempt_start_ack);
 	preempt_start_ack++;
+
+	LL_ASSERT(preempt_req == preempt_ack);
+	preempt_req++;
 }
 
 static uint32_t preempt_ticker_start(struct lll_event *first,
@@ -860,11 +854,7 @@ static uint32_t preempt_ticker_start(struct lll_event *first,
 	uint32_t preempt_to;
 	uint32_t ret;
 
-	/* Do not request to start preempt timeout if already requested.
-	 *
-	 * Check if there is pending preempt timeout start requested or if
-	 * preempt timeout ticker has already been scheduled.
-	 */
+	/* Do not request to start preempt timeout if already requested */
 	if ((preempt_start_req != preempt_start_ack) ||
 	    (preempt_req != preempt_ack)) {
 		uint32_t diff;
