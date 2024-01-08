ade49f081df6fab0db6288bad25be4f5eff7ef3a
Jakub Zymelka <jakub.zymelka@nordicsemi.no>
2024-01-08 09:45:46 +0100

    modules: hal_nordic: nrfx: update API version to 3.2.0
    Updated API version enables multi-instance GPIOTE driver.
    Additionally obsolete symbol that was used to specify
    API version in the past was removed.
    Affected drivers have been adjusted and appropriate changes
    in affected files have been made.
    
    Signed-off-by: Jakub Zymelka <jakub.zymelka@nordicsemi.no>

--- drivers/pwm/pwm_nrf_sw.c
+++ drivers/pwm/pwm_nrf_sw.c
@@ -62,6 +62,7 @@ struct pwm_config {
 		NRF_RTC_Type *rtc;
 		NRF_TIMER_Type *timer;
 	};
+	nrfx_gpiote_t gpiote[PWM_0_MAP_SIZE];
 	uint8_t psel_ch[PWM_0_MAP_SIZE];
 	uint8_t initially_inverted;
 	uint8_t map_size;
@@ -123,6 +124,7 @@ static int pwm_nrf_sw_set_cycles(const struct device *dev, uint32_t channel,
 	const struct pwm_config *config = dev->config;
 	NRF_TIMER_Type *timer = pwm_config_timer(config);
 	NRF_RTC_Type *rtc = pwm_config_rtc(config);
+	NRF_GPIOTE_Type *gpiote;
 	struct pwm_data *data = dev->data;
 	uint32_t ppi_mask;
 	uint8_t active_level;
@@ -161,6 +163,7 @@ static int pwm_nrf_sw_set_cycles(const struct device *dev, uint32_t channel,
 		}
 	}
 
+	gpiote = config->gpiote[channel].p_reg;
 	psel_ch = config->psel_ch[channel];
 	gpiote_ch = data->gpiote_ch[channel];
 	ppi_chs = data->ppi_ch[channel];
@@ -186,7 +189,7 @@ static int pwm_nrf_sw_set_cycles(const struct device *dev, uint32_t channel,
 					  :  active_level);
 
 		/* clear GPIOTE config */
-		nrf_gpiote_te_default(NRF_GPIOTE, gpiote_ch);
+		nrf_gpiote_te_default(gpiote, gpiote_ch);
 
 		/* No PWM generation for this channel. */
 		data->pulse_cycles[channel] = 0U;
@@ -235,7 +238,7 @@ static int pwm_nrf_sw_set_cycles(const struct device *dev, uint32_t channel,
 	}
 
 	/* Configure GPIOTE - toggle task with proper initial output value. */
-	NRF_GPIOTE->CONFIG[gpiote_ch] =
+	gpiote->CONFIG[gpiote_ch] =
 		(GPIOTE_CONFIG_MODE_Task << GPIOTE_CONFIG_MODE_Pos) |
 		((uint32_t)psel_ch << 8) |
 		(GPIOTE_CONFIG_POLARITY_Toggle << GPIOTE_CONFIG_POLARITY_Pos) |
@@ -256,9 +259,9 @@ static int pwm_nrf_sw_set_cycles(const struct device *dev, uint32_t channel,
 	pulse_end_task = period_end_task = nrf_gpiote_out_task_get(gpiote_ch);
 #endif
 	uint32_t pulse_end_task_address =
-		nrf_gpiote_task_address_get(NRF_GPIOTE, pulse_end_task);
+		nrf_gpiote_task_address_get(gpiote, pulse_end_task);
 	uint32_t period_end_task_address =
-		nrf_gpiote_task_address_get(NRF_GPIOTE, period_end_task);
+		nrf_gpiote_task_address_get(gpiote, period_end_task);
 
 	if (USE_RTC) {
 		uint32_t clear_task_address =
@@ -359,7 +362,8 @@ static int pwm_nrf_sw_init(const struct device *dev)
 			}
 		}
 
-		err = nrfx_gpiote_channel_alloc(&data->gpiote_ch[i]);
+		err = nrfx_gpiote_channel_alloc(&config->gpiote[i],
+						&data->gpiote_ch[i]);
 		if (err != NRFX_SUCCESS) {
 			/* Do not free allocated resource. It is a fatal condition,
 			 * system requires reconfiguration.
@@ -402,8 +406,14 @@ static int pwm_nrf_sw_init(const struct device *dev)
 	((DT_GPIO_FLAGS_BY_IDX(_node_id, _prop, _idx) & GPIO_ACTIVE_LOW) \
 	 ? BIT(_idx) : 0) |
 
+#define GPIOTE_AND_COMMA(_node_id, _prop, _idx) \
+	NRFX_GPIOTE_INSTANCE(NRF_DT_GPIOTE_INST_BY_IDX(_node_id, _prop, _idx)),
+
 static const struct pwm_config pwm_nrf_sw_0_config = {
 	COND_CODE_1(USE_RTC, (.rtc), (.timer)) = GENERATOR_ADDR,
+	.gpiote = {
+		DT_INST_FOREACH_PROP_ELEM(0, channel_gpios, GPIOTE_AND_COMMA)
+	},
 	.psel_ch = {
 		DT_INST_FOREACH_PROP_ELEM(0, channel_gpios, PSEL_AND_COMMA)
 	},
