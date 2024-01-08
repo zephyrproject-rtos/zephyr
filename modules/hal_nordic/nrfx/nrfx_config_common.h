21b4cea8524e9b34c164d7bb1833de2c014d5b37
Jakub Zymelka <jakub.zymelka@nordicsemi.no>
2024-01-08 09:45:46 +0100

    modules: hal_nordic: nrfx: update API version to 3.2.0
    Updated API version enables multi-instance GPIOTE driver.
    Additionally obsolete symbol that was used to specify
    API version in the past was removed.
    Affected drivers have been adjusted and appropriate changes
    in affected files have been made.
    
    Signed-off-by: Jakub Zymelka <jakub.zymelka@nordicsemi.no>

--- modules/hal_nordic/nrfx/nrfx_config_common.h
+++ modules/hal_nordic/nrfx/nrfx_config_common.h
@@ -18,7 +18,7 @@
 
 /** @brief Symbol specifying minor version of the nrfx API to be used. */
 #ifndef NRFX_CONFIG_API_VER_MINOR
-#define NRFX_CONFIG_API_VER_MINOR 0
+#define NRFX_CONFIG_API_VER_MINOR 2
 #endif
 
 /** @brief Symbol specifying micro version of the nrfx API to be used. */
