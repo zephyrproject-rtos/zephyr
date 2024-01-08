202aacfe4539e29aa018e90bb61d288f9a87660f
Jai Arora <infolinesoni@gmail.com>
2024-01-09 00:19:55 +0530

    posix: Adds test for clock_getcpuclockid()
    Adds test for clock_getcpuclockid() to
    make sure clock_id is equal to CLOCK_PROCESS_CPUTIME_ID
    after a successful call
    
    Fixes #59954
    
    Signed-off-by: Jai Arora <infolinesoni@gmail.com>

--- tests/posix/common/src/clock.c
+++ tests/posix/common/src/clock.c
@@ -217,10 +217,11 @@ ZTEST(clock, test_realtime)
 ZTEST(clock, test_clock_getcpuclockid)
 {
 	int ret = 0;
-	clockid_t clock_id;
+	clockid_t clock_id = CLOCK_INVALID;
 
 	ret = clock_getcpuclockid((pid_t)0, &clock_id);
 	zassert_equal(ret, 0, "POSIX clock_getcpuclock id failed");
+	zassert_equal(clock_id, CLOCK_PROCESS_CPUTIME_ID, "POSIX clock_getcpuclock id failed");
 
 	ret = clock_getcpuclockid((pid_t)2482, &clock_id);
 	zassert_equal(ret, EPERM, "POSIX clock_getcpuclock id failed");
