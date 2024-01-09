35ac4cece4c8be25515b27ab8ec3833fd21fdc3b
Yong Cong Sin <ycsin@meta.com>
2024-01-09 22:57:44 +0800

    tests: posix: semaphore: refactor `test_semaphore`
    Localize a few public variables and refactor the test and
    functions so that they are reusable.
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>

--- tests/posix/common/src/semaphore.c
+++ tests/posix/common/src/semaphore.c
@@ -13,16 +13,15 @@
 #include <zephyr/sys/util.h>
 #include <zephyr/ztest.h>
 
-static sem_t sema;
-static void *dummy_sem;
-
 static void *child_func(void *p1)
 {
-	zassert_equal(sem_post(&sema), 0, "sem_post failed");
+	sem_t *sem = (sem_t *)p1;
+
+	zassert_equal(sem_post(sem), 0, "sem_post failed");
 	return NULL;
 }
 
-ZTEST(posix_apis, test_semaphore)
+static void semaphore_test(sem_t *sem)
 {
 	pthread_t thread1, thread2;
 	int val, ret;
@@ -31,28 +30,23 @@ ZTEST(posix_apis, test_semaphore)
 	/* TESTPOINT: Check if sema value is less than
 	 * CONFIG_SEM_VALUE_MAX
 	 */
-	zassert_equal(sem_init(&sema, 0, (CONFIG_SEM_VALUE_MAX + 1)), -1,
+	zassert_equal(sem_init(sem, 0, (CONFIG_SEM_VALUE_MAX + 1)), -1,
 		      "value larger than %d\n", CONFIG_SEM_VALUE_MAX);
 	zassert_equal(errno, EINVAL);
 
-	zassert_equal(sem_init(&sema, 0, 0), 0, "sem_init failed");
-
-	/* TESTPOINT: Call sem_post with invalid kobject */
-	zassert_equal(sem_post(dummy_sem), -1, "sem_post of"
-		      " invalid semaphore object didn't fail");
-	zassert_equal(errno, EINVAL);
+	zassert_equal(sem_init(sem, 0, 0), 0, "sem_init failed");
 
 	/* TESTPOINT: Check if semaphore value is as set */
-	zassert_equal(sem_getvalue(&sema, &val), 0);
+	zassert_equal(sem_getvalue(sem, &val), 0);
 	zassert_equal(val, 0);
 
 	/* TESTPOINT: Check if sema is acquired when it
 	 * is not available
 	 */
-	zassert_equal(sem_trywait(&sema), -1);
+	zassert_equal(sem_trywait(sem), -1);
 	zassert_equal(errno, EAGAIN);
 
-	ret = pthread_create(&thread1, NULL, child_func, NULL);
+	ret = pthread_create(&thread1, NULL, child_func, sem);
 	zassert_equal(ret, 0, "Thread creation failed");
 
 	zassert_equal(clock_gettime(CLOCK_REALTIME, &abstime), 0,
@@ -63,38 +57,54 @@ ZTEST(posix_apis, test_semaphore)
 	/* TESPOINT: Wait for 5 seconds and acquire sema given
 	 * by thread1
 	 */
-	zassert_equal(sem_timedwait(&sema, &abstime), 0);
+	zassert_equal(sem_timedwait(sem, &abstime), 0);
 
 	/* TESTPOINT: Semaphore is already acquired, check if
 	 * no semaphore is available
 	 */
-	zassert_equal(sem_timedwait(&sema, &abstime), -1);
+	zassert_equal(sem_timedwait(sem, &abstime), -1);
 	zassert_equal(errno, ETIMEDOUT);
 
-	/* TESTPOINT: sem_destroy with invalid kobject */
-	zassert_equal(sem_destroy(dummy_sem), -1, "invalid"
-		      " semaphore is destroyed");
-	zassert_equal(errno, EINVAL);
-
-	zassert_equal(sem_destroy(&sema), 0, "semaphore is not destroyed");
+	zassert_equal(sem_destroy(sem), 0, "semaphore is not destroyed");
 
 	/* TESTPOINT: Initialize sema with 1 */
-	zassert_equal(sem_init(&sema, 0, 1), 0, "sem_init failed");
-	zassert_equal(sem_getvalue(&sema, &val), 0);
+	zassert_equal(sem_init(sem, 0, 1), 0, "sem_init failed");
+	zassert_equal(sem_getvalue(sem, &val), 0);
 	zassert_equal(val, 1);
 
-	zassert_equal(sem_destroy(&sema), -1, "acquired semaphore"
+	zassert_equal(sem_destroy(sem), -1, "acquired semaphore"
 		      " is destroyed");
 	zassert_equal(errno, EBUSY);
 
 	/* TESTPOINT: take semaphore which is initialized with 1 */
-	zassert_equal(sem_trywait(&sema), 0);
+	zassert_equal(sem_trywait(sem), 0);
 
-	zassert_equal(pthread_create(&thread2, NULL, child_func, NULL), 0,
+	zassert_equal(pthread_create(&thread2, NULL, child_func, sem), 0,
 		      "Thread creation failed");
 
 	/* TESTPOINT: Wait and acquire semaphore till thread2 gives */
-	zassert_equal(sem_wait(&sema), 0, "sem_wait failed");
+	zassert_equal(sem_wait(sem), 0, "sem_wait failed");
+
+	/* Make sure the threads are terminated */
+	zassert_ok(pthread_join(thread1, NULL));
+	zassert_ok(pthread_join(thread2, NULL));
+}
+
+ZTEST(posix_apis, test_semaphore)
+{
+	sem_t sema;
+
+	/* TESTPOINT: Call sem_post with invalid kobject */
+	zassert_equal(sem_post(NULL), -1, "sem_post of"
+		      " invalid semaphore object didn't fail");
+	zassert_equal(errno, EINVAL);
+
+	/* TESTPOINT: sem_destroy with invalid kobject */
+	zassert_equal(sem_destroy(NULL), -1, "invalid"
+		      " semaphore is destroyed");
+	zassert_equal(errno, EINVAL);
+
+	semaphore_test(&sema);
 }
 
 unsigned int nsem_get_ref_count(sem_t *sem);
