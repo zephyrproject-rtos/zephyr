e2bcc9307023067b679099f946205b1985b980e9
Mykola Kvach <mykola_kvach@epam.com>
2024-01-09 17:56:58 +0200

    soc: renesas_rcar: make common pinctrl header for both arm and arm64 archs
    Make common pinctrl header for both arm and arm64 archs in order to avoid
    code duplicates.
    
    Signed-off-by: Mykola Kvach <mykola_kvach@epam.com>

--- soc/renesas/rcar/rcar_gen3/r7/pinctrl_soc.h
+++ soc/renesas/rcar/rcar_gen3/r7/pinctrl_soc.h
@@ -7,6 +7,6 @@
 
 #ifndef ZEPHYR_SOC_ARM_RENESAS_RCAR_GEN3_PINCTRL_SOC_H_
 #define ZEPHYR_SOC_ARM_RENESAS_RCAR_GEN3_PINCTRL_SOC_H_
-#include <pinctrl_rcar.h>
+#include <zephyr/drivers/pinctrl/pinctrl_rcar_common.h>
 
 #endif /* ZEPHYR_SOC_ARM_RENESAS_RCAR_GEN3_PINCTRL_SOC_H_ */
