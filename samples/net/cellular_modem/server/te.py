22bf48d3310681115f6d968ed51992358748580d
Bjarki Arge Andreasen <bjarki@arge-andreasen.me>
2024-01-09 09:12:38 +0100

    samples: net: cellular_modem: server: Limit packets per session
    The server has a feature which receives and validates packets
    within a UDP session, where it responds with the amount of
    valid packets received once modem stop sending to it. The
    response count is stored in a uint8, but there was no limit on
    the amount of packets per session, so if more than 255 packets
    where received, the server would crash :(
    
    This patch fixes the issue.
    
    Signed-off-by: Bjarki Arge Andreasen <bjarki@arge-andreasen.me>

--- samples/net/cellular_modem/server/te.py
+++ samples/net/cellular_modem/server/te.py
@@ -8,15 +8,21 @@ from te_udp_receive import TEUDPReceive
 udp_echo = TEUDPEcho()
 udp_receive = TEUDPReceive()
 
+def terminate():
+    udp_echo.stop()
+    udp_receive.stop()
+    print("stopped")
+
+udp_echo.signal_terminated(terminate)
+udp_receive.signal_terminated(terminate)
+
 udp_echo.start()
 udp_receive.start()
 
 print("started")
 
 def terminate_handler(a, b):
-    udp_echo.stop()
-    udp_receive.stop()
-    print("stopped")
+    terminate()
 
 signal.signal(signal.SIGTERM, terminate_handler)
 signal.signal(signal.SIGINT, terminate_handler)
