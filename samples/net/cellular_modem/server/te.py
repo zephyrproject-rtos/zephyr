4baabc8814035693e9e75b6e57faa28e644ed806
Bjarki Arge Andreasen <bjarki@arge-andreasen.me>
2024-01-09 11:43:19 +0100

    samples: net: cellular_modem: server: Add UDP transmit feature
    This addition adds a feature to the test server to dump a
    configurable amount of data at configurable packet size and
    packet interval, with predefined pseudo random data, to a
    UDP socket.
    
    This test can be used to test packet througput, stability, and
    handling and recovery if data is received to fast (DDOS attacks
    for example)
    
    Signed-off-by: Bjarki Arge Andreasen <bjarki@arge-andreasen.me>

--- samples/net/cellular_modem/server/te.py
+++ samples/net/cellular_modem/server/te.py
@@ -4,20 +4,25 @@
 import signal
 from te_udp_echo import TEUDPEcho
 from te_udp_receive import TEUDPReceive
+from te_udp_transmit import TEUDPTransmit
 
 udp_echo = TEUDPEcho()
 udp_receive = TEUDPReceive()
+udp_transmit = TEUDPTransmit()
 
 def terminate():
     udp_echo.stop()
     udp_receive.stop()
+    udp_transmit.stop()
     print("stopped")
 
 udp_echo.signal_terminated(terminate)
 udp_receive.signal_terminated(terminate)
+udp_transmit.signal_terminated(terminate)
 
 udp_echo.start()
 udp_receive.start()
+udp_transmit.start()
 
 print("started")
 
