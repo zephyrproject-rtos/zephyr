b9720babe23ddcaf4b497c7a1798ae752b653241
Gerard Marull-Paretas <gerard@teslabs.com>
2024-01-10 13:39:29 +0100

    [nrf fromtree] soc: riscv: cleanup usage/definition of MCAUSE IRQ flag
    The MCAUSE register has the "Interrupt" flag defined defined at XLEN-1
    position (31 for 32-bit, 63 for 64-bit). This is not an SoC specific
    option, and there's no need to expose it publicly.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>
    (cherry picked from commit a364420b30f96df97cc7a50b4674eaee6ce2e918)
    (cherry picked from commit 66f9e1e7be188e61f3b3d3a2e028e9a897675d68)

--- soc/riscv/espressif_esp32/esp32c3/soc.h
+++ soc/riscv/espressif_esp32/esp32c3/soc.h
@@ -21,9 +21,6 @@
 #define RISCV_MACHINE_TIMER_IRQ 7 /* Machine Timer Interrupt */
 #define RISCV_MACHINE_EXT_IRQ 11 /* Machine External Interrupt */
 
-/* Interrupt Mask */
-#define SOC_MCAUSE_IRQ_MASK (1 << 31)
-
 /* Exception code Mask */
 #define SOC_MCAUSE_EXP_MASK 0x7FFFFFFF
 
