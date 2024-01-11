6fa5d1e6a5d2739313f019ce5a7533d06600a37f
Jonathan Rico <jonathan.rico@nordicsemi.no>
2024-01-11 18:18:10 +0100

    Bluetooth: L2CAP: fix net buf frags handling
    Fix the handling of buffers with fragments. What seems to have been
    broken is the metadata reference that was not passed to the next frag in
    the chain.
    
    Add a test to regression too.
    
    The main user of this is IPSP.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- tests/bsim/bluetooth/host/compile.sh
+++ tests/bsim/bluetooth/host/compile.sh
@@ -66,6 +66,7 @@ app=tests/bsim/bluetooth/host/l2cap/credits compile
 app=tests/bsim/bluetooth/host/l2cap/credits conf_file=prj_ecred.conf compile
 app=tests/bsim/bluetooth/host/l2cap/credits_seg_recv compile
 app=tests/bsim/bluetooth/host/l2cap/credits_seg_recv conf_file=prj_ecred.conf compile
+app=tests/bsim/bluetooth/host/l2cap/frags compile
 app=tests/bsim/bluetooth/host/l2cap/send_on_connect compile
 app=tests/bsim/bluetooth/host/l2cap/send_on_connect conf_file=prj_ecred.conf compile
 
