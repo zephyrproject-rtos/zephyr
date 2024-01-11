4f3ca23fb15bbe4433c5668c5b7bbfba1e130093
Jonathan Rico <jonathan.rico@nordicsemi.no>
2024-01-11 17:37:11 +0100

    Bluetooth: L2CAP: Prepend SDU header immediately
    Previously it was not always possible to prepend the header.
    
    It was not possible if the application neglected to reserve the space
    for headers.  This is bad because it forces a buffer segment allocation
    even if the buffer had enough room for the headers. E.g. a payload of 10
    bytes in a netbuf of 30 bytes would have been segmented.
    
    We now explicitly reject the buffer if it does not have the headroom.
    
    This allows us to do a nice thing; simplify L2CAP segmentation.
    
    We convert the SDU from the application into a PDU payload, by
    prepending the SDU header, i.e. the SDU length in the original buffer.
    
    This PDU payload is ready to be chunked into PDUs without having to keep
    track of where in the SDU we are. This has the effect of removing a
    bunch of logic in the segmentation machine.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>
    Signed-off-by: Aleksander Wasaznik <aleksander.wasaznik@nordicsemi.no>

--- tests/bsim/bluetooth/host/l2cap/credits_seg_recv/src/main.c
+++ tests/bsim/bluetooth/host/l2cap/credits_seg_recv/src/main.c
@@ -21,7 +21,8 @@ CREATE_FLAG(flag_l2cap_connected);
 #define L2CAP_MTU (2 * SDU_LEN)
 
 /* Only one SDU transmitted or received at a time */
-NET_BUF_POOL_DEFINE(sdu_pool, 1, L2CAP_MTU, CONFIG_BT_CONN_TX_USER_DATA_SIZE, NULL);
+NET_BUF_POOL_DEFINE(sdu_pool, 1, BT_L2CAP_SDU_BUF_SIZE(L2CAP_MTU),
+		    CONFIG_BT_CONN_TX_USER_DATA_SIZE, NULL);
 
 static uint8_t tx_data[SDU_LEN];
 static uint16_t rx_cnt;
