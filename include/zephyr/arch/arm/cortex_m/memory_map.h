259b3d0095f0a10d9104c74d52d4209ab50fe8ba
Duy Nguyen <duy.nguyen.xa@renesas.com>
2024-01-11 03:21:07 +0000

    arch: arm: Add initial support for Cortex-M85 Core
    Add initial support for the Cortex-M85 Core which is an implementation
    of the Armv8.1-M mainline architecture.
    
    The support is based on the Cortex-M55 support that already exists in
    Zephyr.
    
    Signed-off-by: Duy Nguyen <duy.nguyen.xa@renesas.com>

--- include/zephyr/arch/arm/cortex_m/memory_map.h
+++ include/zephyr/arch/arm/cortex_m/memory_map.h
@@ -67,7 +67,8 @@
 #define _PPB_INT_RSVD_2           0xE000F000
 #elif defined(CONFIG_CPU_CORTEX_M23) || \
 	defined(CONFIG_CPU_CORTEX_M33) || \
-	defined(CONFIG_CPU_CORTEX_M55)
+	defined(CONFIG_CPU_CORTEX_M55) || \
+	defined(CONFIG_CPU_CORTEX_M85)
 #define _PPB_INT_RSVD_0           0xE0000000
 #define _PPB_INT_SCS              0xE000E000
 #define _PPB_INT_SCB              0xE000ED00
@@ -88,7 +89,8 @@
 #define _PPB_EXT_PPB              0xE0042000
 #define _PPB_EXT_ROM_TABLE        0xE00FF000
 #define _PPB_EXT_END_ADDR         0xE00FFFFF
-#elif defined(CONFIG_CPU_CORTEX_M33) || defined(CONFIG_CPU_CORTEX_M55)
+#elif defined(CONFIG_CPU_CORTEX_M33) || defined(CONFIG_CPU_CORTEX_M55) \
+	|| defined(CONFIG_CPU_CORTEX_M85)
 #undef  _PPB_EXT_BASE_ADDR
 #define _PPB_EXT_BASE_ADDR        0xE0044000
 #define _PPB_EXT_ROM_TABLE        0xE00FF000
