283c7e09d5b5f40a3a7940f502abd674ce76b39e
23131dw <2241061863@qq.com>
2024-01-12 07:17:25 -0800

    modules:paddlelite: add paddlelite support for zephyr
    Zephyr implements support for Paddle Lite. The selected Paddle Lite version is 2.6,
    and the selected hardware platform is RK3568, which is the ARM64 we have adapted before.
    
    Signed-off-by: 23131dw <2241061863@qq.com>

---
+++ samples/modules/paddlelite/src/image_classification.cc
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2023 HNU-ESNL
+ * Copyright 2023 YNU-SOFTWARE
+ * Copyright 2023 PaddlePaddle
+ * Copyright 2023 openEuler SIG-Zephyr
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+#include <lite/api/paddle_api.h>
+using namespace std;
+#include <zephyr/kernel.h>
+#include <zephyr/timing/timing.h>
+using namespace paddle::lite_api;
+#include <zephyr/arch/arm64/arm_mmu.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/sys/util.h>
+
+int64_t ShapeProduction(const shape_t& shape) {
+  int64_t res = 1;
+  for (auto i : shape) res *= i;
+  return res;
+}
+
+void RunModel() {
+std::string model_dir = " ";
+MobileConfig config;
+config.set_model_from_file(model_dir);
+std::shared_ptr<PaddlePredictor> predictor =
+    CreatePaddlePredictor<MobileConfig>(config);
+  // 3. Prepare input data
+  std::unique_ptr<Tensor> input_tensor(std::move(predictor->GetInput(0)));
+  input_tensor->Resize({1, 3, 224, 224});
+  auto* data = input_tensor->mutable_data<float>();
+  for (int i = 0; i < ShapeProduction(input_tensor->shape()); ++i) {
+    data[i] = 1;
+  }
+
+/*
+  timing_t start_time, end_time;
+  uint64_t total_cycles;
+  uint64_t total_ns;
+  timing_init();
+  timing_start();
+  start_time = timing_counter_get();
+  // 4. Run predictor
+  predictor->Run();
+  end_time = timing_counter_get();
+  total_cycles = timing_cycles_get(&start_time, &end_time);
+  total_ns = timing_cycles_to_ns(total_cycles);
+  timing_stop();
+  cout<<"total_ns is "<<total_ns<<endl;
+*/
+  // 4. Run predictor
+  predictor->Run();
+  // 5. Get output
+  std::unique_ptr<const Tensor> output_tensor(
+      std::move(predictor->GetOutput(0)));
+  std::cout << "Output shape " << output_tensor->shape()[1] << std::endl;
+  for (int i = 0; i < ShapeProduction(output_tensor->shape()); i += 100) {
+    std::cout << "Output[" << i << "]: " << output_tensor->data<float>()[i]
+              << std::endl;
+  }
+}
+
+int main(void) {
+  RunModel();
+  return 0;
+}
