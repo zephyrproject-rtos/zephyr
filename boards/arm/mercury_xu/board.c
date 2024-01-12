3042cb877a5337e791d25a83a20c2480430a8bd8
Jan Bylicki <jbylicki@antmicro.com>
2024-01-12 11:12:19 +0100

    boards: arm: mercury_xu: Define the pinctrl instance
    Define the pinctrl-based pin controller instance
    for the Mercury XU board and remove the old implementation
    
    Signed-off-by: Jan Bylicki <jbylicki@antmicro.com>

--- boards/arm/mercury_xu/board.c
+++ boards/arm/mercury_xu/board.c
@@ -6,24 +6,9 @@
 
 #include <zephyr/arch/cpu.h>
 #include <zephyr/init.h>
-#define MIO_PIN_18	0xff180048
-#define MIO_PIN_19	0xff18004c
-#define MIO_PIN_38	0xff180098
-#define MIO_PIN_39	0xff18009c
-
-#define MIO_DEFAULT	0x0
-#define MIO_UART0	0xc0
 
 static int mercury_xu_init(void)
 {
-	/* pinmux settings for uart */
-	sys_write32(MIO_UART0, MIO_PIN_38);
-	sys_write32(MIO_UART0, MIO_PIN_39);
-
-	/* disable misleading pinmux */
-	sys_write32(MIO_DEFAULT, MIO_PIN_18);
-	sys_write32(MIO_DEFAULT, MIO_PIN_19);
-
 	return 0;
 }
 
