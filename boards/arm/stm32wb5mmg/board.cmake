036ac1c2bd0208f491a4f859e632351703c9a94c
Javad Rahimipetroudi <javad.rahimipetroudi@mind.be>
2024-01-12 08:28:50 +0100

    boards: arm: stm32wb5mmg: add Bluetooth support
    This patch introduces the Bluetooth Low Energy (BLE) feature to the board.
    The board utilizes the STM32WB5MMG as the BLE module. However, As there
    was no BLE controller available for this module. Therefore, a board
    support package has been added to enable the STM32WB5MMG module to act as
    a BLE controller. This is achieved by running Zephyr's hci_uart example on
    the STM32WB5MMG module which enables communication with the main
    microcontroller over the H:4 HCI transport protocol. So, users must first
    build the BLE controller for the BLE module and upload it via on board
    ST-Link,then they can uses Zephyr Bluetooth demos on the development board
    Note that there was no hardware flow control wiring available on the
    board, which is why it has been disabled in the both main board and BLE
    module Device Tree.
    
    Signed-off-by: Javad Rahimipetroudi <javad.rahimipetroudi@mind.be>

---
+++ boards/arm/stm32wb5mmg/board.cmake
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+board_runner_args(pyocd "--target=stm32wb55vgyx")
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
