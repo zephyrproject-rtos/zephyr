874513439b04ba6326e71f60f169a1333cb35c13
Gerard Marull-Paretas <gerard@teslabs.com>
2024-01-16 15:13:16 +0100

    soc: riscv: sifive_freedom: move PRCI base address to prci.h
    Instead of soc.h. This likely needs to be DT-ized at some point.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- soc/riscv/sifive_freedom/u700/soc.h
+++ soc/riscv/sifive_freedom/u700/soc.h
@@ -11,9 +11,6 @@
 #ifndef __RISCV_SIFIVE_FREEDOM_U700_SOC_H_
 #define __RISCV_SIFIVE_FREEDOM_U700_SOC_H_
 
-/* Clock controller. */
-#define PRCI_BASE_ADDR               0x10000000
-
 /* On FU740, peripherals are clocked by PCLK. */
 #define SIFIVE_PERIPHERAL_CLOCK_FREQUENCY \
 	DT_PROP(DT_NODELABEL(pclk), clock_frequency)
