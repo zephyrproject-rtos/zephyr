2a8b25660a2d92636bd5ce3c49611b0d8f2ba8f5
Yves Vandervennet <yves.vandervennet@nxp.com>
2024-01-16 09:21:24 -0600

    boards: mimxrt1020_evk: enable linkserver support
    - adds the definitions in the board.cmake file
    - updates documentation
    
    Signed-off-by: Yves Vandervennet <yves.vandervennet@nxp.com>

--- boards/arm/mimxrt1020_evk/board.cmake
+++ boards/arm/mimxrt1020_evk/board.cmake
@@ -6,6 +6,8 @@
 
 board_runner_args(pyocd "--target=mimxrt1020")
 board_runner_args(jlink "--device=MIMXRT1021xxx5A")
+board_runner_args(linkserver  "--device=MIMXRT1021xxxxx:EVK-MIMXRT1020")
 
+include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
