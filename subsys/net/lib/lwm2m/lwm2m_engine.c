9eee8d2be594a23eca102e21790e2d6db65ec2de
Juha Ylinen <juha.ylinen@nordicsemi.no>
2024-01-17 14:02:34 +0200

    net: lwm2m: Allow send operations when sleeping
    Add new kconfig CONFIG_LWM2M_QUEUE_MODE_NO_MSG_BUFFERING.
    
    When enabled and device is sleeping, Reqistration Update
    message is skipped and messages from send operation and
    notifications are sent right away.
    
    Reqistration update message is also skipped when
    lwm2m_engine resumes from pause state.
    
    Signed-off-by: Juha Ylinen <juha.ylinen@nordicsemi.no>

--- subsys/net/lib/lwm2m/lwm2m_engine.c
+++ subsys/net/lib/lwm2m/lwm2m_engine.c
@@ -244,6 +244,7 @@ int lwm2m_push_queued_buffers(struct lwm2m_ctx *client_ctx)
 			break;
 		}
 		msg = SYS_SLIST_CONTAINER(msg_node, msg, node);
+		msg->pending->t0 = k_uptime_get();
 		sys_slist_append(&msg->ctx->pending_sends, &msg->node);
 	}
 #endif
