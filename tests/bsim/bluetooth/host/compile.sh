65d49cd4345208d23bdd8a8af89d4c5d8fec7203
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2024-01-17 09:51:37 +0100

    tests/bsim/bt/host: Split build script in 6
    There are quite many BT host test images being built.
    Today these are all built in parallel, causing a quite
    high load.
    
    Let's split them in their separate sub-scripts,
    so we don't parallelize too many builds,
    and users have more granularity if they only
    want to build a subset.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- tests/bsim/bluetooth/host/compile.sh
+++ tests/bsim/bluetooth/host/compile.sh
@@ -19,58 +19,13 @@ mkdir -p ${WORK_DIR}
 
 source ${ZEPHYR_BASE}/tests/bsim/compile.source
 
-app=tests/bsim/bluetooth/host/adv/resume compile
-app=tests/bsim/bluetooth/host/adv/resume conf_file=prj_2.conf compile
-app=tests/bsim/bluetooth/host/adv/chain compile
-app=tests/bsim/bluetooth/host/adv/extended conf_file=prj_advertiser.conf compile
-app=tests/bsim/bluetooth/host/adv/extended conf_file=prj_scanner.conf compile
-app=tests/bsim/bluetooth/host/adv/periodic compile
-app=tests/bsim/bluetooth/host/adv/periodic conf_file=prj_long_data.conf compile
-app=tests/bsim/bluetooth/host/adv/encrypted/css_sample_data compile
-app=tests/bsim/bluetooth/host/adv/encrypted/ead_sample compile
-
-app=tests/bsim/bluetooth/host/att/eatt conf_file=prj_lowres.conf compile
-app=tests/bsim/bluetooth/host/att/eatt conf_file=prj_collision.conf compile
-app=tests/bsim/bluetooth/host/att/eatt conf_file=prj_multiple_conn.conf compile
-app=tests/bsim/bluetooth/host/att/eatt conf_file=prj_autoconnect.conf compile
-app=tests/bsim/bluetooth/host/att/eatt_notif conf_file=prj.conf compile
-app=tests/bsim/bluetooth/host/att/mtu_update compile
-app=tests/bsim/bluetooth/host/att/read_fill_buf/client compile
-app=tests/bsim/bluetooth/host/att/read_fill_buf/server compile
-app=tests/bsim/bluetooth/host/att/retry_on_sec_err/client compile
-app=tests/bsim/bluetooth/host/att/retry_on_sec_err/server compile
-app=tests/bsim/bluetooth/host/att/sequential/dut compile
-app=tests/bsim/bluetooth/host/att/sequential/tester compile
-app=tests/bsim/bluetooth/host/att/pipeline/dut compile
-app=tests/bsim/bluetooth/host/att/pipeline/tester compile
-app=tests/bsim/bluetooth/host/att/long_read compile
-app=tests/bsim/bluetooth/host/att/open_close compile
-
-app=tests/bsim/bluetooth/host/gatt/authorization compile
-app=tests/bsim/bluetooth/host/gatt/caching compile
-app=tests/bsim/bluetooth/host/gatt/general compile
-app=tests/bsim/bluetooth/host/gatt/notify compile
-app=tests/bsim/bluetooth/host/gatt/notify_multiple compile
-app=tests/bsim/bluetooth/host/gatt/settings compile
-app=tests/bsim/bluetooth/host/gatt/settings conf_file=prj_2.conf compile
-app=tests/bsim/bluetooth/host/gatt/ccc_store compile
-app=tests/bsim/bluetooth/host/gatt/ccc_store conf_file=prj_2.conf compile
-app=tests/bsim/bluetooth/host/gatt/sc_indicate compile
+${ZEPHYR_BASE}/tests/bsim/bluetooth/host/adv/compile.sh
+${ZEPHYR_BASE}/tests/bsim/bluetooth/host/att/compile.sh
+${ZEPHYR_BASE}/tests/bsim/bluetooth/host/gatt/compile.sh
+${ZEPHYR_BASE}/tests/bsim/bluetooth/host/l2cap/compile.sh
+${ZEPHYR_BASE}/tests/bsim/bluetooth/host/security/compile.sh
 
 app=tests/bsim/bluetooth/host/iso/cis compile
-
-app=tests/bsim/bluetooth/host/l2cap/general compile
-app=tests/bsim/bluetooth/host/l2cap/userdata compile
-app=tests/bsim/bluetooth/host/l2cap/stress compile
-app=tests/bsim/bluetooth/host/l2cap/split/dut compile
-app=tests/bsim/bluetooth/host/l2cap/split/tester compile
-app=tests/bsim/bluetooth/host/l2cap/credits compile
-app=tests/bsim/bluetooth/host/l2cap/credits conf_file=prj_ecred.conf compile
-app=tests/bsim/bluetooth/host/l2cap/credits_seg_recv compile
-app=tests/bsim/bluetooth/host/l2cap/credits_seg_recv conf_file=prj_ecred.conf compile
-app=tests/bsim/bluetooth/host/l2cap/frags compile
-app=tests/bsim/bluetooth/host/l2cap/send_on_connect compile
-app=tests/bsim/bluetooth/host/l2cap/send_on_connect conf_file=prj_ecred.conf compile
 
 app=tests/bsim/bluetooth/host/misc/disable compile
 app=tests/bsim/bluetooth/host/misc/disconnect/dut compile
@@ -84,15 +39,6 @@ app=tests/bsim/bluetooth/host/privacy/peripheral compile
 app=tests/bsim/bluetooth/host/privacy/peripheral conf_file=prj_rpa_sharing.conf compile
 app=tests/bsim/bluetooth/host/privacy/device compile
 app=tests/bsim/bluetooth/host/privacy/legacy compile
-
-app=tests/bsim/bluetooth/host/security/bond_overwrite_allowed compile
-app=tests/bsim/bluetooth/host/security/bond_overwrite_denied compile
-app=tests/bsim/bluetooth/host/security/bond_per_connection compile
-app=tests/bsim/bluetooth/host/security/ccc_update compile
-app=tests/bsim/bluetooth/host/security/ccc_update conf_file=prj_2.conf compile
-app=tests/bsim/bluetooth/host/security/id_addr_update/central compile
-app=tests/bsim/bluetooth/host/security/id_addr_update/peripheral compile
-app=tests/bsim/bluetooth/host/security/security_changed_callback compile
 
 app=tests/bsim/bluetooth/host/id/settings compile
 
