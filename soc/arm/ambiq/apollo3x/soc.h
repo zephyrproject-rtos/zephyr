46b0cd64a0993ab30e10d6e7c4375fba7a7561fc
Ian Stadler <istadler@semios.com>
2024-01-17 16:22:27 -0800

    boards: arm: apollo3p_blue: Added new board
    Initial support for the apollo3p blue hardware
    
    Signed-off-by: Ian Stadler <istadler@semios.com>

---
+++ soc/arm/ambiq/apollo3x/soc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024 Semios Inc. <www.semios.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __SOC_H__
+#define __SOC_H__
+
+#include <apollo3p.h>
+
+#endif /* __SOC_H__ */
