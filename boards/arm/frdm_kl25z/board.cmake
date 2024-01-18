441b1b7de8a897650a99c3e6cf7819e3957ce15f
Yves Vandervennet <yves.vandervennet@nxp.com>
2024-01-18 14:16:04 -0600

    boards: frdm_kl25z: enable support for linkserver
    - adds the definitions in the board.cmake file
    - updates documentation
    
    Signed-off-by: Yves Vandervennet <yves.vandervennet@nxp.com>

--- boards/arm/frdm_kl25z/board.cmake
+++ boards/arm/frdm_kl25z/board.cmake
@@ -1,7 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--device=MKL25Z128xxx4")
+board_runner_args(linkserver  "--device=MKL25Z128xxx4:FRDM-KL25Z")
 board_runner_args(pyocd "--target=kl25z")
 
+include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
