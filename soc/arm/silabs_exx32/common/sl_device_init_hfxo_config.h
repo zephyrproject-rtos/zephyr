a0929f748c0331aabbc5e81f956aee41633575e2
Sateesh Kotapati <sateesh.kotapati@silabs.com>
2024-01-19 15:21:57 +0530

    gecko: service files updated | Update to GSDK 4.4.0
    Updated the files present in device_init, hfxo_manager, power_manager
    and sleeptimer folder as per latest version of gecko_sdk.
    Added SL_DEVICE_INIT_HFXO_PRECISION in sl_device_init_hfxo_config.
    
    Signed-off-by: Sateesh Kotapati <sateesh.kotapati@silabs.com>

--- soc/arm/silabs_exx32/common/sl_device_init_hfxo_config.h
+++ soc/arm/silabs_exx32/common/sl_device_init_hfxo_config.h
@@ -12,5 +12,6 @@
 #define SL_DEVICE_INIT_HFXO_MODE           cmuHfxoOscMode_Crystal
 #define SL_DEVICE_INIT_HFXO_FREQ           DT_PROP(DT_NODELABEL(clk_hfxo), clock_frequency)
 #define SL_DEVICE_INIT_HFXO_CTUNE          DT_PROP(DT_NODELABEL(clk_hfxo), ctune)
+#define SL_DEVICE_INIT_HFXO_PRECISION      DT_PROP(DT_NODELABEL(clk_hfxo), precision)
 
 #endif /* SL_DEVICE_INIT_HFXO_CONFIG_H */
