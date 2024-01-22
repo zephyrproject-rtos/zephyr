d5b68bad12d42de67fc92f5c2d20bedfe02a0e54
Stine Åkredalen <stine.akredalen@nordicsemi.no>
2024-01-22 04:56:04 -0800

    tests: Bluetooth: Mesh: add proxy solicitation bsim test
    * Test that beacon is received after solicitation
    * Test for replay attack
    
    Signed-off-by: Stine Åkredalen <stine.akredalen@nordicsemi.no>

---
+++ tests/bsim/bluetooth/mesh/tests_scripts/proxy_sol/sol_beacon_rcvd.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Copyright 2024 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+# Test procedure:
+# 1. Initialize and configure tester and iut instances.
+# 3. Tester sends a solicitation PDU.
+# 4. Tester scans for beacons and expects to receive at least one private net ID advertisement.
+overlay="overlay_pst_conf_overlay_gatt_conf"
+RunTest proxy_sol_beacon_rcvd \
+	proxy_sol_tester_beacon_rcvd \
+	proxy_sol_iut_beacon_send
+
+overlay="overlay_pst_conf_overlay_gatt_conf_overlay_psa_conf"
+RunTest proxy_sol_beacon_rcvd_psa \
+	proxy_sol_tester_beacon_rcvd \
+	proxy_sol_iut_beacon_send
