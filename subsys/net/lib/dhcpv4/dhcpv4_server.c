5c8e8e21b020d19eb1e05c186dd576ab71d83ce9
Robert Lubos <robert.lubos@nordicsemi.no>
2024-01-25 11:14:44 +0100

    [nrf fromtree] net: dhcpv4_server: Improve address pool range validation
    Not only check if the address pool belongs to the same subnet as the
    server, but also that it does not overlap with the server address -
    otherwise the server might end up assigning its own address.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
    (cherry picked from commit a147ac9a47dee34f16ebd26a3d0af59afbb7cb39)

--- subsys/net/lib/dhcpv4/dhcpv4_server.c
+++ subsys/net/lib/dhcpv4/dhcpv4_server.c
@@ -1224,6 +1224,13 @@ int net_dhcpv4_server_start(struct net_if *iface, struct in_addr *base_addr)
 		return -EINVAL;
 	}
 
+	if ((htonl(server_addr->s_addr) >= htonl(base_addr->s_addr)) &&
+	    (htonl(server_addr->s_addr) <
+	     htonl(base_addr->s_addr) + CONFIG_NET_DHCPV4_SERVER_ADDR_COUNT)) {
+		LOG_ERR("Address pool overlaps with server address.");
+		return -EINVAL;
+	}
+
 	netmask = net_if_ipv4_get_netmask(iface);
 	if (net_ipv4_is_addr_unspecified(&netmask)) {
 		LOG_ERR("Failed to obtain subnet mask.");
