zephyrproject-rtos/zephyr

commit 2438dbb613731f1abb90be9e1b4c7bd529d46574

init: add missing initialization of dev pointer in SYS_INIT macro
When the -Werror=missing-field-initializers is enabled, the compiler
complains about missing initialization of dev pointer in the
init_entry struct when using the SYS_INIT[_NAMED] macro.
This commit adds explicit assignment of NULL to it.

Signed-off-by: Michał Barnaś <barnas@google.com>

include/zephyr/init.h
@@ -202,10 +202,10 @@ struct init_entry {
  *
  * @see SYS_INIT()
  */
-#define SYS_INIT_NAMED(name, init_fn_, level, prio)                            \
-	static const Z_DECL_ALIGN(struct init_entry)                           \
-		Z_INIT_ENTRY_SECTION(level, prio, 0) __used __noasan           \
-		Z_INIT_ENTRY_NAME(name) = {.init_fn = {.sys = (init_fn_)}}
+#define SYS_INIT_NAMED(name, init_fn_, level, prio)                                       \
+	static const Z_DECL_ALIGN(struct init_entry)                                      \
+		Z_INIT_ENTRY_SECTION(level, prio, 0) __used __noasan                      \
+		Z_INIT_ENTRY_NAME(name) = {.init_fn = {.sys = (init_fn_)}, .dev = NULL}
 
 /** @} */
 

commit d3c8f8f96b75e2bee9f90670eb222f7594aa4dc0

init: adjust the SYS_INIT dev field init to play nice with older compilers
Fix a build error with certain older Cadence XCC toolchains. These
are used e.g. for Intel Tiger Lake products for the audio DSP (thhe oldest
platform supported in Zephyr upstream for the audio DSP).

To keep all compilers happy, use C89 style initializers.

Error:
lib/os/p4wq.c:216: error: unknown field ‘dev’ specified in initializer
lib/os/p4wq.c:216: warning: missing braces around initializer
lib/os/p4wq.c:216: warning: (near initialization for
  ‘__init_static_init.<anonymous>’)

Compiler version XCC RG-2017.8-linux (xt-xcc version 12.0.8)

Fixes: 2438dbb613731f ("init: add missing initialization of dev pointer
in SYS_INIT macro")
Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

include/zephyr/init.h
@@ -205,7 +205,7 @@ struct init_entry {
 #define SYS_INIT_NAMED(name, init_fn_, level, prio)                                       \
 	static const Z_DECL_ALIGN(struct init_entry)                                      \
 		Z_INIT_ENTRY_SECTION(level, prio, 0) __used __noasan                      \
-		Z_INIT_ENTRY_NAME(name) = {.init_fn = {.sys = (init_fn_)}, .dev = NULL}
+		Z_INIT_ENTRY_NAME(name) = {{ (init_fn_) }, { NULL } }
 
 /** @} */
 