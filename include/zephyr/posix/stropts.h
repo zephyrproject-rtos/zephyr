10a02b0bb203b92984903fdc8ab2db727fba2f59
Abhinav Srivastava <atg271@gmail.com>
2024-01-26 03:37:36 -0500

    posix: add headers for stropts
    Minimal header for stropts
    
    Signed-off-by: Abhinav Srivastava <atg271@gmail.com>

---
+++ include/zephyr/posix/stropts.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2024 Abhinav Srivastava
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_POSIX_STROPTS_H_
+#define ZEPHYR_INCLUDE_POSIX_STROPTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct strbuf {
+	int maxlen;
+	int len;
+	char *buf;
+};
+
+int putmsg(int fildes, const struct strbuf *ctlptr, const struct strbuf *dataptr, int flags);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_POSIX_STROPTS_H_ */
