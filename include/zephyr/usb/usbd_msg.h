2960b59f5db16796ab29a675c70c0d1daf149dbd
Johann Fischer <johann.fischer@nordicsemi.no>
2024-01-26 18:11:37 +0100

    usb: device_next: implementation of USB device support notification
    The implementation uses the system workqueue and a callback provided
    and registered by the application. The application callback is called in
    the context of the workqueue. Notification messages are stored in a
    queue and delivered to the callback in sequence.
    
    We cannot call application callback directly from the USB device stack
    thread because the behavior of arbitrary code provided by the
    application is unpredictable, and we do not want it to be executed in
    the same context where all events from the device controller are
    handled.
    
    Nor can we use the ZBUS subsystem directly. ZBUS offloads responsibility
    for defined behavior to the observers and application, and does not
    provide any way for the publisher to enforce defined behavior and
    execution context.
    ZBUS listener would be called from the USB thread context and is not
    acceptable. ZBUS subscriber does not provide delivery guarantee and
    cached message can be overwritten. ZBUS message subscriber has
    cumbersome global configuration and buffers that are too complicated to
    handle from USB configuration, and even if we would use it, defined
    behavior is not possible because of how listener and subscriber are
    handled.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

---
+++ include/zephyr/usb/usbd_msg.h
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief USB support message types and structure
+ */
+
+#ifndef ZEPHYR_INCLUDE_USBD_MSG_H_
+#define ZEPHYR_INCLUDE_USBD_MSG_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup usbd_msg_api USB device core API
+ * @ingroup usb
+ * @{
+ */
+
+/**
+ * @brief USB device support message types
+ *
+ * The first set of message types map to event types from the UDC driver API.
+ */
+enum usbd_msg_type {
+	/** VBUS ready message (optional) */
+	USBD_MSG_VBUS_READY,
+	/** VBUS removed message (optional) */
+	USBD_MSG_VBUS_REMOVED,
+	/** Device resume message */
+	USBD_MSG_RESUME,
+	/** Device suspended message */
+	USBD_MSG_SUSPEND,
+	/** Bus reset detected */
+	USBD_MSG_RESET,
+	/** Non-correctable UDC error message  */
+	USBD_MSG_UDC_ERROR,
+	/** Unrecoverable device stack error message  */
+	USBD_MSG_STACK_ERROR,
+	/** Maximum number of message types */
+	USBD_MSG_MAX_NUMBER,
+};
+
+
+static const char *const usbd_msg_type_list[] = {
+	"VBUS ready",
+	"VBUS removed",
+	"Device resumed",
+	"Device suspended",
+	"Bus reset",
+	"Controller error",
+	"Stack error",
+};
+
+BUILD_ASSERT(ARRAY_SIZE(usbd_msg_type_list) == USBD_MSG_MAX_NUMBER,
+	     "Number of entries in usbd_msg_type_list is not equal to USBD_MSG_MAX_NUMBER");
+
+/**
+ * @brief USB device message
+ */
+struct usbd_msg {
+	/** Message type */
+	enum usbd_msg_type type;
+	/** Message status, value or data */
+	union {
+		int status;
+		const struct device *dev;
+	};
+};
+
+/**
+ * @brief Callback type definition for USB device message delivery
+ *
+ * The implementation uses the system workqueue, and a callback provided and
+ * registered by the application. The application callback is called in the
+ * context of the system workqueue. Notification messages are stored in a queue
+ * and delivered to the callback in sequence.
+ *
+ * @param[in] msg Pointer to USB device message
+ */
+typedef void (*usbd_msg_cb_t)(const struct usbd_msg *const msg);
+
+/**
+ * @brief Returns the message type as a constant string
+ *
+ * @param[in] type USBD message type
+ *
+ * @return Message type as a constant string
+ */
+static inline const char *usbd_msg_type_string(const enum usbd_msg_type type)
+{
+	if (type >= 0 && type < USBD_MSG_MAX_NUMBER) {
+		return usbd_msg_type_list[type];
+	}
+
+	return "?";
+}
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_USBD_MSG_H_ */
