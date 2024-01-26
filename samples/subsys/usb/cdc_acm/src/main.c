ae08fe99aeda7187e1a59a910e5508fc49dfefad
Johann Fischer <johann.fischer@nordicsemi.no>
2024-01-26 18:12:09 +0100

    samples: cdc_acm: use USB notification messages to detect DTR
    Use USB notification messages to detect DTR and baudrate updates.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- samples/subsys/usb/cdc_acm/src/main.c
+++ samples/subsys/usb/cdc_acm/src/main.c
@@ -26,6 +26,8 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(cdc_acm_echo, LOG_LEVEL_INF);
 
+const struct device *const uart_dev = DEVICE_DT_GET_ONE(zephyr_cdc_acm_uart);
+
 #define RING_BUF_SIZE 1024
 uint8_t ring_buffer[RING_BUF_SIZE];
 
@@ -33,14 +35,46 @@ struct ring_buf ringbuf;
 
 static bool rx_throttled;
 
+static inline void print_baudrate(const struct device *dev)
+{
+	uint32_t baudrate;
+	int ret;
+
+	ret = uart_line_ctrl_get(dev, UART_LINE_CTRL_BAUD_RATE, &baudrate);
+	if (ret) {
+		LOG_WRN("Failed to get baudrate, ret code %d", ret);
+	} else {
+		LOG_INF("Baudrate %u", baudrate);
+	}
+}
+
 #if defined(CONFIG_USB_DEVICE_STACK_NEXT)
 static struct usbd_contex *sample_usbd;
+K_SEM_DEFINE(dtr_sem, 0, 1);
+
+static void sample_msg_cb(const struct usbd_msg *msg)
+{
+	LOG_INF("USBD message: %s", usbd_msg_type_string(msg->type));
+
+	if (msg->type == USBD_MSG_CDC_ACM_CONTROL_LINE_STATE) {
+		uint32_t dtr = 0U;
+
+		uart_line_ctrl_get(msg->dev, UART_LINE_CTRL_DTR, &dtr);
+		if (dtr) {
+			k_sem_give(&dtr_sem);
+		}
+	}
+
+	if (msg->type == USBD_MSG_CDC_ACM_LINE_CODING) {
+		print_baudrate(msg->dev);
+	}
+}
 
 static int enable_usb_device_next(void)
 {
 	int err;
 
-	sample_usbd = sample_usbd_init_device(NULL);
+	sample_usbd = sample_usbd_init_device(sample_msg_cb);
 	if (sample_usbd == NULL) {
 		LOG_ERR("Failed to initialize USB device");
 		return -ENODEV;
@@ -121,12 +155,9 @@ static void interrupt_handler(const struct device *dev, void *user_data)
 
 int main(void)
 {
-	const struct device *dev;
-	uint32_t baudrate, dtr = 0U;
 	int ret;
 
-	dev = DEVICE_DT_GET_ONE(zephyr_cdc_acm_uart);
-	if (!device_is_ready(dev)) {
+	if (!device_is_ready(uart_dev)) {
 		LOG_ERR("CDC ACM device not ready");
 		return 0;
 	}
@@ -146,8 +177,13 @@ int main(void)
 
 	LOG_INF("Wait for DTR");
 
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
+	k_sem_take(&dtr_sem, K_FOREVER);
+#else
 	while (true) {
-		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		uint32_t dtr = 0U;
+
+		uart_line_ctrl_get(uart_dev, UART_LINE_CTRL_DTR, &dtr);
 		if (dtr) {
 			break;
 		} else {
@@ -155,16 +191,17 @@ int main(void)
 			k_sleep(K_MSEC(100));
 		}
 	}
+#endif
 
 	LOG_INF("DTR set");
 
 	/* They are optional, we use them to test the interrupt endpoint */
-	ret = uart_line_ctrl_set(dev, UART_LINE_CTRL_DCD, 1);
+	ret = uart_line_ctrl_set(uart_dev, UART_LINE_CTRL_DCD, 1);
 	if (ret) {
 		LOG_WRN("Failed to set DCD, ret code %d", ret);
 	}
 
-	ret = uart_line_ctrl_set(dev, UART_LINE_CTRL_DSR, 1);
+	ret = uart_line_ctrl_set(uart_dev, UART_LINE_CTRL_DSR, 1);
 	if (ret) {
 		LOG_WRN("Failed to set DSR, ret code %d", ret);
 	}
@@ -172,16 +209,13 @@ int main(void)
 	/* Wait 100ms for the host to do all settings */
 	k_msleep(100);
 
-	ret = uart_line_ctrl_get(dev, UART_LINE_CTRL_BAUD_RATE, &baudrate);
-	if (ret) {
-		LOG_WRN("Failed to get baudrate, ret code %d", ret);
-	} else {
-		LOG_INF("Baudrate detected: %d", baudrate);
-	}
-
-	uart_irq_callback_set(dev, interrupt_handler);
+#ifndef CONFIG_USB_DEVICE_STACK_NEXT
+	print_baudrate(uart_dev);
+#endif
+	uart_irq_callback_set(uart_dev, interrupt_handler);
 
 	/* Enable rx interrupts */
-	uart_irq_rx_enable(dev);
+	uart_irq_rx_enable(uart_dev);
+
 	return 0;
 }
