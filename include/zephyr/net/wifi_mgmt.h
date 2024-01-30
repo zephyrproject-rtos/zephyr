a9cc922969af78c55a116a7f1b2210a94e11f51c
Kapil Bhatt <kapil.bhatt@nordicsemi.no>
2024-01-30 14:26:13 +0530

    net: wifi: Fix disconnect reason codes
    The shell is printing "connected" while passing
    the wrong password to connect command. If the status
    value is 0, then only shell will print "connected."
    The wrong password will disconnect the connection
    with an unspecified reason code.
    
    If the default value of the unspecified disconnect
    reason code is 0, then status will print "connected."
    By changing the default value to WIFI_STATUS_CONN_LAST_STATUS,
    it will print the correct reason, like a failed request
    with reason code.
    
    Signed-off-by: Kapil Bhatt <kapil.bhatt@nordicsemi.no>

--- include/zephyr/net/wifi_mgmt.h
+++ include/zephyr/net/wifi_mgmt.h
@@ -379,6 +379,10 @@ enum wifi_conn_status {
 	WIFI_STATUS_CONN_TIMEOUT,
 	/** Connection failed - AP not found */
 	WIFI_STATUS_CONN_AP_NOT_FOUND,
+	/** Last connection status */
+	WIFI_STATUS_CONN_LAST_STATUS,
+	/** Connection disconnected status */
+	WIFI_STATUS_DISCONN_FIRST_STATUS = WIFI_STATUS_CONN_LAST_STATUS,
 };
 
 /** Wi-Fi disconnect reason codes. To be overlaid on top of \ref wifi_status
@@ -386,7 +390,7 @@ enum wifi_conn_status {
  */
 enum wifi_disconn_reason {
 	/** Unspecified reason */
-	WIFI_REASON_DISCONN_UNSPECIFIED = 0,
+	WIFI_REASON_DISCONN_UNSPECIFIED = WIFI_STATUS_DISCONN_FIRST_STATUS,
 	/** Disconnected due to user request */
 	WIFI_REASON_DISCONN_USER_REQUEST,
 	/** Disconnected due to AP leaving */
