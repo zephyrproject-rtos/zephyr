61bb0260dc6915a991f6621a48591ad04cba082d
Yong Cong Sin <ycsin@meta.com>
2024-01-30 15:19:24 +0800

    cmake: generated_file_directories: set PROJECT_BINARY_DIR conditionally
    Set `PROJECT_BINARY_DIR` conditionally and created
    `INCLUDE_GENERATED_REL_DIR` & `INCLUDE_GENERATED_REL_DIR` for
    use in other files.
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>

--- cmake/modules/generated_file_directories.cmake
+++ cmake/modules/generated_file_directories.cmake
@@ -6,8 +6,10 @@ include_guard(GLOBAL)
 # for placing generated files.
 #
 # Outcome:
-# - BINARY_DIR_INCLUDE is set to ${PROJECT_BINARY_DIR}/include
+# - BINARY_DIR is set to ${PROJECT_BINARY_DIR} or ${CMAKE_BINARY_DIR}
+# - BINARY_DIR_INCLUDE is set to ${BINARY_DIR}/include
 # - BINARY_DIR_INCLUDE_GENERATED is set to ${BINARY_DIR_INCLUDE}/generated
+# - BINARY_DIR_INCLUDE_GENERATED_H is set to ${BINARY_DIR_INCLUDE_GENERATED}
 # - BINARY_DIR_INCLUDE_GENERATED is a directory
 #
 # Required variables:
@@ -19,6 +21,14 @@ include_guard(GLOBAL)
 # Optional environment variables:
 # None
 
-set(BINARY_DIR_INCLUDE ${PROJECT_BINARY_DIR}/include)
+set(INCLUDE_GENERATED_REL_DIR include/generated)
+set(INCLUDE_GENERATED_REL_DIR_H ${INCLUDE_GENERATED_REL_DIR})
+if (PROJECT_BINARY_DIR)
+  set(BINARY_DIR ${PROJECT_BINARY_DIR})
+else()
+  set(BINARY_DIR ${CMAKE_BINARY_DIR})
+endif()
+set(BINARY_DIR_INCLUDE ${BINARY_DIR}/include)
 set(BINARY_DIR_INCLUDE_GENERATED ${BINARY_DIR_INCLUDE}/generated)
+set(BINARY_DIR_INCLUDE_GENERATED_H ${BINARY_DIR_INCLUDE_GENERATED})
 file(MAKE_DIRECTORY ${BINARY_DIR_INCLUDE_GENERATED})
