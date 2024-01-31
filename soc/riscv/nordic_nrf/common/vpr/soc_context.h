6b31b5aa424d763051d7a47b69702f10a437542d
Gerard Marull-Paretas <gerard@teslabs.com>
2024-01-31 19:46:00 +0100

    [nrf fromtree] soc: riscv: nordic_nrf: add initial support for VPR core
    Add initial support for the VPR RISC-V core found in the new nRF54 SoCs.
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>
    (cherry picked from commit ba16e3dd13e5d16d038c816defebad62ef8249a1)

---
+++ soc/riscv/nordic_nrf/common/vpr/soc_context.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2024 Nordic Semiconductor ASA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef SOC_RISCV_NORDIC_NRF_COMMON_VPR_SOC_CONTEXT_H_
+#define SOC_RISCV_NORDIC_NRF_COMMON_VPR_SOC_CONTEXT_H_
+
+#define SOC_ESF_MEMBERS unsigned long minttresh
+#define SOC_ESF_INIT    0
+
+#endif /* SOC_RISCV_NORDIC_NRF_COMMON_VPR_SOC_CONTEXT_H_ */
