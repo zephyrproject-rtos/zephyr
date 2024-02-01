d3189cf7078f92c5a0d8f1d8b8238528892aa611
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2024-02-01 11:01:38 +0100

    tests: bsim: bluetooth: mesh: Add advertiser suspend test
    Add a test that checks that both advertisers, the legacy and the
    extended behaves identically when the stack is suspended, in particular:
    - `bt_mesh_send_cb.end` callback is called with error code `0` for the
      advertisement that the advertiser already pushed to the ble host
      (called `bt_mesh_send_cb.start`),
    - `bt_mesh_send_cb.start` callback with error `-ENODEV` is called for
      every advertisement that was pushed to the mesh advertiser using
      `bt_mesh_adv_send` function before the stack was suspended,
    - `bt_mesh_adv_create` returns NULL when attempting to create a new
      advertisement while the stack is suspended.
    
    The `bt_mesh_adv_disable` is called from the work because calling it
    from the `bt_mesh_send_cb.start` callback will cause a deadlock.
    
    Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

--- tests/bsim/bluetooth/mesh/src/main.c
+++ tests/bsim/bluetooth/mesh/src/main.c
@@ -26,6 +26,7 @@ extern struct bst_test_list *test_beacon_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_transport_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_friendship_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_suspend_install(struct bst_test_list *test);
+extern struct bst_test_list *test_adv_install(struct bst_test_list *test);
 #else
 extern struct bst_test_list *test_transport_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_friendship_install(struct bst_test_list *tests);
@@ -63,6 +64,7 @@ bst_test_install_t test_installers[] = {
 	test_transport_install,
 	test_friendship_install,
 	test_suspend_install,
+	test_adv_install,
 #else
 	test_transport_install,
 	test_friendship_install,
