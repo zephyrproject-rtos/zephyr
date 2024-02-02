28268c493834cb485f3b685986e6236e6cd21841
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-02-02 11:38:15 +0000

    boards: arm: nrf5340_audio_dk_nrf5340: Convert to v2
    Converts the board to hwmv2
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- boards/nordic_nrf/nrf5340_audio_dk/board.cmake
+++ boards/nordic_nrf/nrf5340_audio_dk/board.cmake
@@ -6,9 +6,7 @@ endif()
 
 if(CONFIG_BOARD_NRF5340_AUDIO_DK_NRF5340_CPUAPP OR CONFIG_BOARD_NRF5340_AUDIO_DK_NRF5340_CPUAPP_NS)
   board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
-endif()
-
-if(CONFIG_BOARD_NRF5340_AUDIO_DK_NRF5340_CPUNET)
+elseif(CONFIG_BOARD_NRF5340_AUDIO_DK_NRF5340_CPUNET)
   board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
 endif()
 
