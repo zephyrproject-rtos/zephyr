7fa21aa04dddfa02741d7642e852b9ddddaf5203
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-02-02 11:37:57 +0000

    boards: arm: nrf52dk: Convert to v2
    Converts the board to hwmv2
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- boards/boards_legacy/arm/nrf52dk_nrf52832/pre_dt_board.cmake
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2022 Nordic Semiconductor
-# SPDX-License-Identifier: Apache-2.0
-
-# Suppress "unique_unit_address_if_enabled" to handle the following overlaps:
-# - power@40000000 & clock@40000000 & bprot@40000000
-# - acl@4001e000 & flash-controller@4001e000
-list(APPEND EXTRA_DTC_FLAGS "-Wno-unique_unit_address_if_enabled")
