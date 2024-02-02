fee24f1416f035fce67fc67e80ac56fbbb8f5d20
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-02-02 16:50:11 +0200

    toolchain: do not use posix C code for asm language in gcc.h
    Toolchain headers can be include in assembler files causing compilation
    error for posix architecture. The fix involves adding
    `!defined(_ASMLANGUAGE)` condition to exclude including posix C code in
    this case.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- include/zephyr/toolchain/gcc.h
+++ include/zephyr/toolchain/gcc.h
@@ -103,7 +103,7 @@
 #define FUNC_ALIAS(real_func, new_alias, return_type) \
 	return_type new_alias() ALIAS_OF(real_func)
 
-#if defined(CONFIG_ARCH_POSIX)
+#if defined(CONFIG_ARCH_POSIX) && !defined(_ASMLANGUAGE)
 #include <zephyr/arch/posix/posix_trace.h>
 
 /*let's not segfault if this were to happen for some reason*/
