9b20f27d6a3e89b1be6e74a8dc69c082a9ecab6b
Erwan Gouriou <erwan.gouriou@st.com>
2024-02-05 17:52:44 +0100

    boards: Convert arduino_giga_r1 to HWM v2
    Port the board to HWMv2.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@st.com>

--- boards/arduino/arduino_giga_r1/board.cmake
+++ boards/arduino/arduino_giga_r1/board.cmake
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
-if(CONFIG_BOARD_ARDUINO_GIGA_R1_M7)
+if(CONFIG_BOARD_ARDUINO_GIGA_R1_STM32H747XX_M7)
 board_runner_args(jlink "--device=STM32H747XI_M7" "--speed=4000")
 board_runner_args(openocd "--config=${BOARD_DIR}/support/openocd_arduino_giga_r1_m7.cfg")
 board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
-elseif(CONFIG_BOARD_ARDUINO_GIGA_R1_M4)
+elseif(CONFIG_BOARD_ARDUINO_GIGA_R1_STM32H747XX_M4)
 board_runner_args(jlink "--device=STM32H747XI_M4" "--speed=4000")
 board_runner_args(openocd "--config=${BOARD_DIR}/support/openocd_arduino_giga_r1_m4.cfg")
 board_runner_args(openocd --target-handle=_CHIPNAME.cpu1)
