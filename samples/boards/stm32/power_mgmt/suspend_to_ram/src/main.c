dfdaf29f5b413084bdd69eaa1b6d99afeeaeb492
Guillaume Gautier <guillaume.gautier-ext@st.com>
2024-02-05 11:46:36 +0100

    samples: boards: stm32: pm: s2ram: add spi
    Add SPI transfer in the suspend to ram samples to test SPI PM.
    
    Signed-off-by: Guillaume Gautier <guillaume.gautier-ext@st.com>

--- samples/boards/stm32/power_mgmt/suspend_to_ram/src/main.c
+++ samples/boards/stm32/power_mgmt/suspend_to_ram/src/main.c
@@ -13,7 +13,10 @@
 #include <zephyr/pm/device_runtime.h>
 #include <zephyr/drivers/adc.h>
 #include <zephyr/drivers/entropy.h>
+#include <zephyr/drivers/spi.h>
 #include <string.h>
+#include <stdio.h>
+#include <assert.h>
 
 #define SLEEP_TIME_STOP0_MS	800
 #define SLEEP_TIME_STOP1_MS	1500
@@ -21,7 +24,7 @@
 #define SLEEP_TIME_BUSY_MS	2000
 
 static const struct gpio_dt_spec led =
-	GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+	GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
 
 #if !DT_NODE_EXISTS(DT_PATH(zephyr_user)) || \
 	!DT_NODE_HAS_PROP(DT_PATH(zephyr_user), io_channels)
@@ -42,6 +45,86 @@ const struct device *rng_dev;
 #define BUFFER_LENGTH	3
 
 static uint8_t entropy_buffer[BUFFER_LENGTH] = {0};
+
+#define SPI_TEST_DEV	DT_COMPAT_GET_ANY_STATUS_OKAY(test_spi_loopback)
+
+#define FRAME_SIZE (8)
+
+#define SPI_OP(frame_size) SPI_OP_MODE_MASTER | SPI_MODE_CPOL | \
+	       SPI_MODE_CPHA | SPI_WORD_SET(frame_size) | SPI_LINES_SINGLE
+
+static struct spi_dt_spec spi_test_dev = SPI_DT_SPEC_GET(SPI_TEST_DEV, SPI_OP(FRAME_SIZE), 0);
+
+#define SPI_BUF_SIZE 18
+
+static const char spi_tx_data[SPI_BUF_SIZE] = "0123456789abcdef-\0";
+static __aligned(32) char spi_buffer_tx[SPI_BUF_SIZE] __used;
+static __aligned(32) char spi_buffer_rx[SPI_BUF_SIZE] __used;
+
+static uint8_t spi_buffer_print_tx[SPI_BUF_SIZE * 5 + 1];
+static uint8_t spi_buffer_print_rx[SPI_BUF_SIZE * 5 + 1];
+
+static void to_display_format(const uint8_t *src, size_t size, char *dst)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++) {
+		sprintf(dst + 5 * i, "0x%02x,", src[i]);
+	}
+}
+
+static int spi_test(void)
+{
+	const struct spi_buf tx_bufs[] = {
+		{
+			.buf = spi_buffer_tx,
+			.len = SPI_BUF_SIZE,
+		},
+	};
+	const struct spi_buf rx_bufs[] = {
+		{
+			.buf = spi_buffer_rx,
+			.len = SPI_BUF_SIZE,
+		},
+	};
+	const struct spi_buf_set tx = {
+		.buffers = tx_bufs,
+		.count = ARRAY_SIZE(tx_bufs)
+	};
+	const struct spi_buf_set rx = {
+		.buffers = rx_bufs,
+		.count = ARRAY_SIZE(rx_bufs)
+	};
+
+	int ret;
+
+	ret = spi_transceive_dt(&spi_test_dev, &tx, &rx);
+	if (ret) {
+		printk("SPI transceive failed: %d\n", ret);
+		return ret;
+	}
+
+	if (memcmp(spi_buffer_tx, spi_buffer_rx, SPI_BUF_SIZE)) {
+		to_display_format(spi_buffer_tx, SPI_BUF_SIZE, spi_buffer_print_tx);
+		to_display_format(spi_buffer_rx, SPI_BUF_SIZE, spi_buffer_print_rx);
+		printk("Buffer contents are different\n");
+		printk("tx: %s\n", spi_buffer_print_tx);
+		printk("rx: %s\n", spi_buffer_print_rx);
+		return -1;
+	}
+
+	return 0;
+}
+
+static void spi_setup(void)
+{
+	memset(spi_buffer_tx, 0, sizeof(spi_buffer_tx));
+	memcpy(spi_buffer_tx, spi_tx_data, sizeof(spi_tx_data));
+
+	if (!spi_is_ready_dt(&spi_test_dev)) {
+		printk("Fast spi lookback device is not ready\n");
+	}
+}
 
 static int adc_test(void)
 {
@@ -122,6 +205,17 @@ void print_buf(uint8_t *buffer)
 	printk("\n");
 }
 
+static void loop(void)
+{
+	gpio_pin_set_dt(&led, 1);
+	adc_test();
+	if (!IS_ENABLED(CONFIG_PM_S2RAM)) {
+		spi_test();
+	}
+	k_busy_wait(SLEEP_TIME_BUSY_MS*1000);
+	gpio_pin_set_dt(&led, 0);
+}
+
 int main(void)
 {
 	__ASSERT_NO_MSG(gpio_is_ready_dt(&led));
@@ -131,20 +225,17 @@ int main(void)
 		printk("error: random device not ready");
 	}
 
+	spi_setup();
+
 	printk("Device ready\n");
 
 	while (true) {
 		gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-		adc_test();
-		k_busy_wait(SLEEP_TIME_BUSY_MS*1000);
-		gpio_pin_set_dt(&led, 0);
+		loop();
 		k_msleep(SLEEP_TIME_STOP0_MS);
 		printk("Exit Stop0\n");
 
-		gpio_pin_set_dt(&led, 1);
-		adc_test();
-		k_busy_wait(SLEEP_TIME_BUSY_MS*1000);
-		gpio_pin_set_dt(&led, 0);
+		loop();
 		k_msleep(SLEEP_TIME_STOP1_MS);
 		printk("Exit Stop1\n");
 
@@ -153,9 +244,7 @@ int main(void)
 		printk("Sync entropy: ");
 		print_buf(entropy_buffer);
 
-		gpio_pin_set_dt(&led, 1);
-		adc_test();
-		k_busy_wait(SLEEP_TIME_BUSY_MS*1000);
+		loop();
 		gpio_pin_configure_dt(&led, GPIO_DISCONNECTED);
 		k_msleep(SLEEP_TIME_STANDBY_MS);
 		printk("Exit Standby\n");
