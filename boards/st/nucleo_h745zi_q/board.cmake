91f9198dc4f6bee483deec031bea3a0fb53ab28e
Erwan Gouriou <erwan.gouriou@st.com>
2024-02-05 17:54:26 +0100

    boards: Convert nucleo_h745zi_q to HWM v2
    Port the board to HWMv2.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@st.com>

--- boards/st/nucleo_h745zi_q/board.cmake
+++ boards/st/nucleo_h745zi_q/board.cmake
@@ -1,9 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--device=STM32H745ZI" "--speed=4000")
-if(CONFIG_BOARD_NUCLEO_H745ZI_Q_M7)
+if(CONFIG_BOARD_NUCLEO_H745ZI_Q_STM32H745XX_M7)
 board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
-elseif(CONFIG_BOARD_NUCLEO_H745ZI_Q_M4)
+elseif(CONFIG_BOARD_NUCLEO_H745ZI_Q_STM32H745XX_M4)
 board_runner_args(openocd --target-handle=_CHIPNAME.cpu1)
 endif()
 
