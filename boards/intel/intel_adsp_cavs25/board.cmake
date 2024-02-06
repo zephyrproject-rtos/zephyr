689cc2bdb8b1e2f42dfcfe0c8b17ef14be3733ed
Dmitrii Golovanov <dmitrii.golovanov@intel.com>
2024-02-06 16:50:42 +0100

    boards: xtensa: intel_adsp_cavs25_tgph: change to board variant
    Change `intel_adsp_cavs25_tgph` board definition to be HWMv2 board
    variant `intel_adsp_cavs25/intel_tgl_adsp/tgph`
    
    Signed-off-by: Dmitrii Golovanov <dmitrii.golovanov@intel.com>

--- boards/intel/intel_adsp_cavs25/board.cmake
+++ boards/intel/intel_adsp_cavs25/board.cmake
@@ -13,7 +13,7 @@ if(CONFIG_BOARD_INTEL_ADSP_CAVS25)
 board_set_rimage_target(tgl)
 endif()
 
-if(CONFIG_BOARD_INTEL_ADSP_CAVS25_TGPH)
+if(CONFIG_BOARD_INTEL_ADSP_CAVS25_INTEL_TGL_ADSP_TGPH)
 board_set_rimage_target(tgl-h)
 endif()
 
