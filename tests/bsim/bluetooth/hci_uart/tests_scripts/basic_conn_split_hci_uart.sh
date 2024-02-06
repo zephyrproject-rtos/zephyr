07084e0e2d1feb87e29c331a111a5160394358cb
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2024-02-06 14:12:49 +0100

    tests: bsim: hci_uart: Rename to hci_uart test name
    Rename uart and uart_async bsim test to reflect
    testing of hci_uart sample using UART driver
    variants under bsim simulations.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- tests/bsim/bluetooth/hci_uart/tests_scripts/basic_conn_split_hci_uart.sh
+++ tests/bsim/bluetooth/hci_uart/tests_scripts/basic_conn_split_hci_uart.sh
@@ -7,8 +7,8 @@ source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
 # Basic connection test: a central connects to a peripheral and expects a
 # notification, using the split controller (ULL LLL)
 # Both central and peripheral hosts have their controllers in a separate device
-# connected over UART. The controller is the HCI UART async sample.
-simulation_id="basic_conn_split_uart_async"
+# connected over UART. The controller is the HCI UART sample.
+simulation_id="basic_conn_split_hci_uart"
 verbosity_level=2
 EXECUTE_TIMEOUT=10
 
@@ -21,22 +21,22 @@ UART_CEN=${UART_DIR}/central
 # Note the host+app devices are NOT connected to the phy, only the controllers are.
 
 # Peripheral app + host :
-Execute ./bs_${BOARD}_tests_bsim_bluetooth_ll_conn_prj_split_uart_conf \
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_ll_conn_prj_split_hci_uart_conf \
   -v=${verbosity_level} -s=${simulation_id} -d=10 -nosim -RealEncryption=0 \
   -testid=peripheral -rs=23 -uart1_fifob_rxfile=${UART_PER}.rx -uart1_fifob_txfile=${UART_PER}.tx
 
 # Peripheral controller:
-Execute ./bs_${BOARD}_samples_bluetooth_hci_uart_async_prj_conf \
+Execute ./bs_${BOARD}_samples_bluetooth_hci_uart_prj_conf \
   -v=${verbosity_level} -s=${simulation_id} -d=0 -RealEncryption=0 \
-  -rs=23 -uart1_fifob_rxfile=${UART_PER}.tx -uart1_fifob_txfile=${UART_PER}.rx
+  -rs=23 -uart1_fifob_rxfile=${UART_PER}.tx -uart1_fifob_txfile=${UART_PER}.rx \
 
 # Central app + host
-Execute ./bs_${BOARD}_tests_bsim_bluetooth_ll_conn_prj_split_uart_conf\
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_ll_conn_prj_split_hci_uart_conf\
   -v=${verbosity_level} -s=${simulation_id} -d=11 -nosim -RealEncryption=0 \
   -testid=central -rs=6 -uart1_fifob_rxfile=${UART_CEN}.rx -uart1_fifob_txfile=${UART_CEN}.tx
 
 # Central controller:
-Execute ./bs_${BOARD}_samples_bluetooth_hci_uart_async_prj_conf \
+Execute ./bs_${BOARD}_samples_bluetooth_hci_uart_prj_conf \
   -v=${verbosity_level} -s=${simulation_id} -d=1 -RealEncryption=0 \
   -rs=23 -uart1_fifob_rxfile=${UART_CEN}.tx -uart1_fifob_txfile=${UART_CEN}.rx
 
