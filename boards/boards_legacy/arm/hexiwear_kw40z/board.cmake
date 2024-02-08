d101792195207685cc36d173cd68d969eb2b8b62
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-02-08 13:29:34 -0600

    boards: nxp: convert hexiwear to hwmv2
    Convert hexiwear to hardware model v2. As multiple SOCs exist on this
    board, the hexiwear_k64 and hexiwear_kw40z boards are combined into one
    target.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- boards/boards_legacy/arm/hexiwear_kw40z/board.cmake
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-board_runner_args(jlink "--device=MKW40Z160xxx4")
-board_runner_args(pyocd "--target=kw40z4")
-
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
