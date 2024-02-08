0cc9bcc41a0179d961122545a37595ca9b8905ee
Christopher Friedt <cfriedt@meta.com>
2024-02-08 07:02:30 -0800

    logging: rename log2_generic to log_generic
    The log2 prefix was deprecated and changed to log some time ago,
    but log2_generic() seems to have been inadvertantly left with
    the old prefix.
    
    Rename log2_generic() to log_generic() to follow suit.
    
    Signed-off-by: Christopher Friedt <cfriedt@meta.com>

--- include/zephyr/logging/log_core.h
+++ include/zephyr/logging/log_core.h
@@ -501,7 +501,7 @@ void z_log_printf_arg_checker(const char *fmt, ...)
 }
 
 /**
- * @brief Writes a generic log message to the logging v2.
+ * @brief Write a generic log message.
  *
  * @note This function is intended to be used when porting other log systems.
  *
@@ -509,7 +509,7 @@ void z_log_printf_arg_checker(const char *fmt, ...)
  * @param fmt            String to format.
  * @param ap             Pointer to arguments list.
  */
-static inline void log2_generic(uint8_t level, const char *fmt, va_list ap)
+static inline void log_generic(uint8_t level, const char *fmt, va_list ap)
 {
 	z_log_msg_runtime_vcreate(Z_LOG_LOCAL_DOMAIN_ID, NULL, level,
 				   NULL, 0, 0, fmt, ap);
