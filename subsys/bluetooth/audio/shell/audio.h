233552d3b84c52816225685f3d72b93c999574ad
Emil Gydesen <emil.gydesen@nordicsemi.no>
2024-02-09 10:57:18 +0100

    Bluetooth: BAP: Shell: Minor refactor of LC3 encoder
    This commit renames a few variables and changes the
    type and adds some additional checks.
    
    It moves stream specific data to the shell stream
    struct, but still keeps the global values that were used
    to initialize the LC3 encoder
    
    The purpose of this is to better allow for a future LC3 decoder
    without any clashes in names or the like.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/audio/shell/audio.h
+++ subsys/bluetooth/audio/shell/audio.h
@@ -79,6 +79,12 @@ struct shell_stream {
 	struct bt_cap_stream stream;
 	struct bt_audio_codec_cfg codec_cfg;
 	struct bt_audio_codec_qos qos;
+#if defined(CONFIG_LIBLC3)
+	uint32_t lc3_freq_hz;
+	uint32_t lc3_frame_duration_us;
+	uint16_t lc3_octets_per_frame;
+	uint8_t lc3_frames_per_sdu;
+#endif /* CONFIG_LIBLC3 */
 #if defined(CONFIG_BT_AUDIO_TX)
 	int64_t connected_at_ticks; /* The uptime tick measured when stream was connected */
 	uint16_t seq_num;
