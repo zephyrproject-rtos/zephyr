cc3076be1db3ed528b8e5f2a9f4d3f608689f812
Bjarki Arge Andreasen <bjarki@arge-andreasen.me>
2024-02-15 07:03:00 +0100

    modem: cmux: Prevent NULL deref in dlci_open_handler
    Prevent unlikely item == NULL scenario (CID 323102) and remove
    superflous cmux == NULL check (CID 334903)
    
    Signed-off-by: Bjarki Arge Andreasen <bjarki@arge-andreasen.me>

--- subsys/modem/modem_cmux.c
+++ subsys/modem/modem_cmux.c
@@ -1015,12 +1015,15 @@ struct modem_pipe_api modem_cmux_dlci_pipe_api = {
 
 static void modem_cmux_dlci_open_handler(struct k_work *item)
 {
-	struct k_work_delayable *dwork = k_work_delayable_from_work(item);
-	struct modem_cmux_dlci *dlci = CONTAINER_OF(dwork, struct modem_cmux_dlci, open_work);
+	struct k_work_delayable *dwork;
+	struct modem_cmux_dlci *dlci;
 
-	if (dlci == NULL) {
+	if (item == NULL) {
 		return;
 	}
+
+	dwork = k_work_delayable_from_work(item);
+	dlci = CONTAINER_OF(dwork, struct modem_cmux_dlci, open_work);
 
 	dlci->state = MODEM_CMUX_DLCI_STATE_OPENING;
 
