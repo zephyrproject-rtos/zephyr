16337e8fed6063d7a1c57e34a8609dac610c1c37
Marc Herbert <marc.herbert@intel.com>
2024-02-16 05:46:39 +0000

    cmake/compiler/gcc: use -flto only for gcc >= 4.5.0
    Fixes commit 26c8776c70ea ("buildsystem: Add an option to enable LTO")
    
    Fixes the following error when compiling offsets.c
    
      xt-xcc ERROR parsing -fno-lto:  unknown flag
    
    LTO options were introduced in gcc 4.5.0, compare:
    
    https://gcc.gnu.org/onlinedocs/gcc-4.4.6/gcc/Option-Summary.html
    https://gcc.gnu.org/onlinedocs/gcc-4.5.0/gcc/Option-Summary.html
    
    Signed-off-by: Marc Herbert <marc.herbert@intel.com>

--- cmake/compiler/gcc/compiler_flags.cmake
+++ cmake/compiler/gcc/compiler_flags.cmake
@@ -21,8 +21,10 @@ endif()
 set_compiler_property(PROPERTY optimization_speed -O2)
 set_compiler_property(PROPERTY optimization_size  -Os)
 
-set_compiler_property(PROPERTY optimization_lto -flto)
-set_compiler_property(PROPERTY prohibit_lto -fno-lto)
+if(CMAKE_C_COMPILER_VERSION GREATER_EQUAL "4.5.0")
+  set_compiler_property(PROPERTY optimization_lto -flto)
+  set_compiler_property(PROPERTY prohibit_lto -fno-lto)
+endif()
 
 #######################################################
 # This section covers flags related to warning levels #
