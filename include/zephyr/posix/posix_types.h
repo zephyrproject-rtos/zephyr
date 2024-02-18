0447737feb014354d8ea3dc300cecd39fd890701
Christopher Friedt <cfriedt@meta.com>
2024-02-18 10:37:44 -0500

    posix: mutexattr: improvements for pthread_mutexattr_t
    Previously, pthread_mutexattr_gettype() and
    pthread_mutexattr_settype() were non-conformant and also
    less safe, as they would not check whether a pthread_mutexattr_t
    had been initialized prior to manipulating them. Furthermore,
    they would potentially dereference NULL pointers.
    
    Additionally, move the pthread_mutexattr_init() and
    pthread_mutexattr_destroy() functions to the library, and add
    some level of checking to them so that they are more than simply
    static inline / no-op calls.
    
    Lastly, reduce the size of struct pthread_mutexattr to only
    what is necessary (one byte should suffice).
    
    Signed-off-by: Christopher Friedt <cfriedt@meta.com>

--- include/zephyr/posix/posix_types.h
+++ include/zephyr/posix/posix_types.h
@@ -60,7 +60,8 @@ typedef struct k_sem sem_t;
 typedef uint32_t pthread_mutex_t;
 
 struct pthread_mutexattr {
-	int type;
+	unsigned char type: 2;
+	bool initialized: 1;
 };
 #if defined(CONFIG_MINIMAL_LIBC) || defined(CONFIG_PICOLIBC) || defined(CONFIG_ARMCLANG_STD_LIBC) \
 	|| defined(CONFIG_ARCMWDT_LIBC)
