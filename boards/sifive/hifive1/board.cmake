7ab7426223699cce36d0cdcca29dcbd56db75e5c
Filip Kokosinski <fkokosinski@antmicro.com>
2024-02-19 14:12:27 +0100

    boards: convert SiFive HiFive1 Rev. B to Zephyr HWMv2
    This commit converts the SiFive HiFive1 Rev. B board (`hifive1_revb`
    target) to the Zephyr Hardware Model v2.
    
    Signed-off-by: Filip Kokosinski <fkokosinski@antmicro.com>

--- boards/sifive/hifive1/board.cmake
+++ boards/sifive/hifive1/board.cmake
@@ -12,11 +12,18 @@ set(QEMU_FLAGS_${ARCH}
   -machine sifive_e
   )
 
-board_set_flasher_ifnset(hifive1)
-board_finalize_runner_args(hifive1)
-
-board_runner_args(openocd --cmd-load "hifive1-load")
-board_runner_args(openocd --cmd-reset-halt "hifive1-reset-halt")
-board_runner_args(openocd --cmd-post-verify "hifive1-post-verify")
-
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+if("${BOARD_REVISION}" STREQUAL "A")
+  board_set_flasher_ifnset(hifive1)
+  board_finalize_runner_args(hifive1)
+  board_runner_args(openocd --cmd-load "hifive1-load")
+  board_runner_args(openocd --cmd-reset-halt "hifive1-reset-halt")
+  board_runner_args(openocd --cmd-post-verify "hifive1-post-verify")
+  include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+elseif("${BOARD_REVISION}" STREQUAL "B")
+  board_runner_args(jlink "--device=FE310")
+  board_runner_args(jlink "--iface=JTAG")
+  board_runner_args(jlink "--speed=4000")
+  board_runner_args(jlink "--tool-opt=-jtagconf -1,-1")
+  board_runner_args(jlink "--tool-opt=-autoconnect 1")
+  include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+endif()
