f2d4787fe1773be7d4ef0b6ffa0540553aa0da83
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-02-19 08:48:01 +0000

    boards: riscv: neorv32: Convert to v2
    Converts the board to hwmv2
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- boards/boards_legacy/riscv/neorv32/revision.cmake
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2021 Henrik Brix Andersen <henrik@brixandersen.dk>
-# SPDX-License-Identifier: Apache-2.0
-
-board_check_revision(
-  FORMAT MAJOR.MINOR.PATCH
-  DEFAULT_REVISION 1.8.6
-)
