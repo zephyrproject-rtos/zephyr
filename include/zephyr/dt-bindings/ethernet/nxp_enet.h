8f7bb6c1e918a45f542a2c094d5e0ae073d34ead
Stanislav Poboril <stanislav.poboril@nxp.com>
2024-02-19 16:15:07 +0100

    dts: bindings: Add RGMII mode definition to NXP ENET bindings
    Add RGMII mode definition to be used in bindings related to NXP ENET_1G IP.
    
    Fixes: #66348
    
    Signed-off-by: Stanislav Poboril <stanislav.poboril@nxp.com>

--- include/zephyr/dt-bindings/ethernet/nxp_enet.h
+++ include/zephyr/dt-bindings/ethernet/nxp_enet.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 NXP
+ * Copyright 2023-2024 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,6 +9,7 @@
 
 #define NXP_ENET_MII_MODE	0
 #define NXP_ENET_RMII_MODE	1
+#define NXP_ENET_RGMII_MODE	2
 #define NXP_ENET_INVALID_MII_MODE 100
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_NXP_ENET_H_ */
