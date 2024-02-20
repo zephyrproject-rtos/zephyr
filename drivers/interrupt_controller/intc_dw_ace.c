291770cbf93641948cc1b5a9809efc9abe6d4e67
Yong Cong Sin <ycsin@meta.com>
2024-02-20 12:05:18 +0800

    drivers: intc_dw_ace: convert to use instance-based macros
    Define the DRV_COMPAT and use instance-based device DT macros.
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>

--- drivers/interrupt_controller/intc_dw_ace.c
+++ drivers/interrupt_controller/intc_dw_ace.c
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#define DT_DRV_COMPAT intel_ace_intc
+
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/irq_nextlevel.h>
@@ -68,7 +70,7 @@
  *     ACE_INTC[core_id].irq_inten_l |= interrupt_bit;
  */
 
-#define ACE_INTC ((volatile struct dw_ictl_registers *)DT_REG_ADDR(DT_NODELABEL(ace_intc)))
+#define ACE_INTC ((volatile struct dw_ictl_registers *)DT_INST_REG_ADDR(0))
 
 static inline bool is_dw_irq(uint32_t irq)
 {
@@ -175,6 +177,6 @@ static const struct dw_ace_v1_ictl_driver_api dw_ictl_ace_v1x_apis = {
 #endif
 };
 
-DEVICE_DT_DEFINE(DT_NODELABEL(ace_intc), dw_ace_init, NULL, NULL, NULL,
+DEVICE_DT_INST_DEFINE(0, dw_ace_init, NULL, NULL, NULL,
 		 PRE_KERNEL_1, CONFIG_INTC_INIT_PRIORITY,
 		 &dw_ictl_ace_v1x_apis);
