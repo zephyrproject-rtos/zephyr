07994ab5d7ed091d112f0117f421675a25712317
Pisit Sawangvonganan <pisit@ndrsolution.com>
2024-02-21 00:40:00 +0700

    bluetooth: fix typo in (common, crypto, services, shell)
    Utilize a code spell-checking tool to scan for and correct spelling errors
    in all files within the subsys/bluetooth/(common, crypto, service, shell).
    
    Signed-off-by: Pisit Sawangvonganan <pisit@ndrsolution.com>

--- subsys/bluetooth/shell/bt.c
+++ subsys/bluetooth/shell/bt.c
@@ -2897,7 +2897,7 @@ static int cmd_read_local_tx_power(const struct shell *sh, size_t argc, char *ar
 		}
 		err = bt_conn_le_get_tx_power_level(default_conn, &tx_power_level);
 		if (err) {
-			shell_print(sh, "Commad returned error error %d", err);
+			shell_print(sh, "Command returned error error %d", err);
 			return err;
 		}
 		if (tx_power_level.current_level == unachievable_current_level) {
