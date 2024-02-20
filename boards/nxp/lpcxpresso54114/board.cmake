d8cfa6fb291604a19040aa8a31977849578d40b7
David Leach <david.leach@nxp.com>
2024-02-20 10:29:47 -0600

    boards: nxp: convert lpcxpresso54114 to hwmv2
    Convert lpcxpresso54114
    
    Signed-off-by: David Leach <david.leach@nxp.com>

---
+++ boards/nxp/lpcxpresso54114/board.cmake
@@ -0,0 +1,13 @@
+#
+# Copyright (c) 2017, NXP
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+if(BOARD_LPCXPRESSO54114_LPC54114_M4 OR CONFIG_SECOND_CORE_MCUX)
+  board_runner_args(jlink "--device=LPC54114J256_M4" "--reset-after-load")
+elseif(BOARD_LPCXPRESSO54114_LPC54114_M0)
+  board_runner_args(jlink "--device=LPC54114J256_M0" "--reset-after-load")
+endif()
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
