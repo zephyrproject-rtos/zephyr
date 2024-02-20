15c8fd8a960684cb680754f522a6bab411c53019
Pisit Sawangvonganan <pisit@ndrsolution.com>
2024-02-21 00:40:00 +0700

    bluetooth: fix typo in (common, crypto, services, shell)
    Utilize a code spell-checking tool to scan for and correct spelling errors
    in all files within the subsys/bluetooth/(common, crypto, service, shell).
    
    Signed-off-by: Pisit Sawangvonganan <pisit@ndrsolution.com>

--- subsys/bluetooth/crypto/bt_crypto.h
+++ subsys/bluetooth/crypto/bt_crypto.h
@@ -127,7 +127,7 @@ int bt_crypto_h6(const uint8_t w[16], const uint8_t key_id[4], uint8_t res[16]);
 int bt_crypto_h7(const uint8_t salt[16], const uint8_t w[16], uint8_t res[16]);
 
 /**
- * @brief Cryptograhic Toolbox function h8
+ * @brief Cryptographic Toolbox function h8
  *
  * Defined in Core Vol. 6, part E 1.1.1.
  *
