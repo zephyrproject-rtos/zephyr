e40a2f1507da206f5c32307261701b3902207126
Gerard Marull-Paretas <gerard@teslabs.com>
2024-02-21 17:48:59 +0100

    samples: drivers: mbox: allow to enable RX/TX
    This can be useful to provide multiple setups, eg unidirectional
    ping-pong.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- samples/drivers/mbox/src/main.c
+++ samples/drivers/mbox/src/main.c
@@ -9,23 +9,25 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/mbox.h>
 
+#ifdef CONFIG_RX_ENABLED
 static void callback(const struct device *dev, uint32_t channel,
 		     void *user_data, struct mbox_msg *data)
 {
 	printk("Pong (on channel %d)\n", channel);
 }
+#endif
 
 int main(void)
 {
-	struct mbox_channel tx_channel;
-	struct mbox_channel rx_channel;
 	const struct device *dev;
 
 	printk("Hello from APP\n");
 
 	dev = DEVICE_DT_GET(DT_NODELABEL(mbox));
 
-	mbox_init_channel(&tx_channel, dev, CONFIG_TX_CHANNEL_ID);
+#ifdef CONFIG_RX_ENABLED
+	struct mbox_channel rx_channel;
+
 	mbox_init_channel(&rx_channel, dev, CONFIG_RX_CHANNEL_ID);
 
 	if (mbox_register_callback(&rx_channel, callback, NULL)) {
@@ -37,6 +39,12 @@ int main(void)
 		printk("mbox_set_enable() error\n");
 		return 0;
 	}
+#endif /* CONFIG_RX_ENABLED */
+
+#ifdef CONFIG_TX_ENABLED
+	struct mbox_channel tx_channel;
+
+	mbox_init_channel(&tx_channel, dev, CONFIG_TX_CHANNEL_ID);
 
 	while (1) {
 		k_sleep(K_MSEC(2000));
@@ -48,5 +56,6 @@ int main(void)
 			return 0;
 		}
 	}
+#endif /* CONFIG_TX_ENABLED */
 	return 0;
 }
