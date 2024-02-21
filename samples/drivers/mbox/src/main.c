fdd4cbee5e08c04c725340774831a4d14d03841d
Gerard Marull-Paretas <gerard@teslabs.com>
2024-02-21 17:59:22 +0100

    [nrf fromlist] samples: drivers: mbox: misc cleanups/enhancements
    - Include cleanup
    - s/NET/REMOTE (remote core may have nothing to do with NET)
    - Improved error reporting
    
    Upstream PR: https://github.com/zephyrproject-rtos/zephyr/pull/69303
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>
    (cherry picked from commit cf67d54ed49d896112a285044a12a28b7dfacff0)
    (cherry picked from commit 56cc531bd5ba1ea1f22e726f9cbd9b72247cc8fb)

--- samples/drivers/mbox/src/main.c
+++ samples/drivers/mbox/src/main.c
@@ -4,10 +4,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/mbox.h>
+#include <zephyr/sys/printk.h>
 
 #ifdef CONFIG_RX_ENABLED
 static void callback(const struct device *dev, uint32_t channel,
@@ -19,24 +18,25 @@ static void callback(const struct device *dev, uint32_t channel,
 
 int main(void)
 {
-	const struct device *dev;
+	int ret;
+	const struct device *const dev = DEVICE_DT_GET(DT_NODELABEL(mbox));
 
 	printk("Hello from APP\n");
-
-	dev = DEVICE_DT_GET(DT_NODELABEL(mbox));
 
 #ifdef CONFIG_RX_ENABLED
 	struct mbox_channel rx_channel;
 
 	mbox_init_channel(&rx_channel, dev, CONFIG_RX_CHANNEL_ID);
 
-	if (mbox_register_callback(&rx_channel, callback, NULL)) {
-		printk("mbox_register_callback() error\n");
+	ret = mbox_register_callback(&rx_channel, callback, NULL);
+	if (ret < 0) {
+		printk("Could not register callback (%d)\n", ret);
 		return 0;
 	}
 
-	if (mbox_set_enabled(&rx_channel, 1)) {
-		printk("mbox_set_enable() error\n");
+	ret = mbox_set_enabled(&rx_channel, true);
+	if (ret < 0) {
+		printk("Could not enable RX channel %d (%d)\n", rx_channel.id, ret);
 		return 0;
 	}
 #endif /* CONFIG_RX_ENABLED */
@@ -51,8 +51,9 @@ int main(void)
 
 		printk("Ping (on channel %d)\n", tx_channel.id);
 
-		if (mbox_send(&tx_channel, NULL) < 0) {
-			printk("mbox_send() error\n");
+		ret = mbox_send(&tx_channel, NULL);
+		if (ret < 0) {
+			printk("Could not send (%d)\n", ret);
 			return 0;
 		}
 	}
