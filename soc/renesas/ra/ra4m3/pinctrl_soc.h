6b1e87d82cc41f86148f5ab0a4595b26bebd6260
Piotr Rak <piotr.rak@gmail.com>
2024-02-22 02:15:16 +0100

    soc: renesas ra: RA4M3 series R7FA4M3AF3CFB.
    Signed-off-by: Piotr Rak <piotr.rak@gmail.com>

---
+++ soc/renesas/ra/ra4m3/pinctrl_soc.h
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2024 Piotr Rak
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "../common/pinctrl_ra.h"
---
+++ soc/renesas/ra/ra4m3/soc.h
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2024 Piotr Rak
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "../common/ra_common_soc.h"
