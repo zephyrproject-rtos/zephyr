3ced037b647dc90a61e39b95453cef4ee4da19cc
Anas Nashif <anas.nashif@intel.com>
2024-02-22 13:51:15 -0500

    kernel: split cpu_mask handling into own file
    In an effort to cleanup sched.c, move sections of code that can be
    compiled in based on options into own files. CPU mask here is managed by
    a kconfig and is not widely used (SMP affinity on multicore systems).
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

---
+++ kernel/cpu_mask.c
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2018,2024 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <zephyr/kernel.h>
+#include <ksched.h>
+#include <zephyr/spinlock.h>
+
+extern struct k_spinlock sched_spinlock;
+
+
+# ifdef CONFIG_SMP
+/* Right now we use a two byte for this mask */
+BUILD_ASSERT(CONFIG_MP_MAX_NUM_CPUS <= 16, "Too many CPUs for mask word");
+# endif
+
+
+static int cpu_mask_mod(k_tid_t thread, uint32_t enable_mask, uint32_t disable_mask)
+{
+	int ret = 0;
+
+#ifdef CONFIG_SCHED_CPU_MASK_PIN_ONLY
+	__ASSERT(z_is_thread_prevented_from_running(thread),
+		 "Running threads cannot change CPU pin");
+#endif
+
+	K_SPINLOCK(&sched_spinlock) {
+		if (z_is_thread_prevented_from_running(thread)) {
+			thread->base.cpu_mask |= enable_mask;
+			thread->base.cpu_mask  &= ~disable_mask;
+		} else {
+			ret = -EINVAL;
+		}
+	}
+
+#if defined(CONFIG_ASSERT) && defined(CONFIG_SCHED_CPU_MASK_PIN_ONLY)
+		int m = thread->base.cpu_mask;
+
+		__ASSERT((m == 0) || ((m & (m - 1)) == 0),
+			 "Only one CPU allowed in mask when PIN_ONLY");
+#endif
+
+	return ret;
+}
+
+int k_thread_cpu_mask_clear(k_tid_t thread)
+{
+	return cpu_mask_mod(thread, 0, 0xffffffff);
+}
+
+int k_thread_cpu_mask_enable_all(k_tid_t thread)
+{
+	return cpu_mask_mod(thread, 0xffffffff, 0);
+}
+
+int k_thread_cpu_mask_enable(k_tid_t thread, int cpu)
+{
+	return cpu_mask_mod(thread, BIT(cpu), 0);
+}
+
+int k_thread_cpu_mask_disable(k_tid_t thread, int cpu)
+{
+	return cpu_mask_mod(thread, 0, BIT(cpu));
+}
+
+int k_thread_cpu_pin(k_tid_t thread, int cpu)
+{
+	int ret;
+
+	ret = k_thread_cpu_mask_clear(thread);
+	if (ret == 0) {
+		return k_thread_cpu_mask_enable(thread, cpu);
+	}
+	return ret;
+}
