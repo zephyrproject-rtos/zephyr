edcb610994982929d9df1a3ff6dd28c99dd398d7
Piotr Rak <piotr.rak@gmail.com>
2024-02-22 02:20:07 +0100

    boards: renesas: EK-RA4M3 preliminary support
    Supports:
     - samples/basic/minimal
     - blinky is very slow since clocks aren't set properly
    
    Signed-off-by: Piotr Rak <piotr.rak@gmail.com>

---
+++ boards/renesas/ek_ra4m3/board.cmake
@@ -0,0 +1,12 @@
+# Copyright (c) 2024 Piotr Rak
+# SPDX-License-Identifier: Apache-2.0
+
+board_set_debugger_ifnset(jlink)
+board_set_flasher_ifnset(jlink)
+
+board_runner_args(jlink
+  "--device=R7FA4M3AF"
+  "--speed=4000"
+  "--iface=SWD"
+  "--reset-after-load")
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
