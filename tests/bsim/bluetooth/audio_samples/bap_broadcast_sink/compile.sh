e9215fae5b415d8c967c916de05f71f7308ff5de
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2024-02-23 12:14:49 +0100

    tests: bsim: Bluetooth: broadcast_audio_source interleaved/sequential
    Test broadcast audio source sample in interleaved and
    sequential packing.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- tests/bsim/bluetooth/audio_samples/bap_broadcast_sink/compile.sh
+++ tests/bsim/bluetooth/audio_samples/bap_broadcast_sink/compile.sh
@@ -10,13 +10,20 @@ set -ue
 source ${ZEPHYR_BASE}/tests/bsim/compile.source
 
 if [ "${BOARD_TS}" == "nrf5340bsim_nrf5340_cpuapp" ]; then
-  app=samples/bluetooth/bap_broadcast_source sysbuild=1 compile
+  app=samples/bluetooth/bap_broadcast_source conf_overlay=overlay-sequential.conf \
+    sysbuild=1 compile
+  app=samples/bluetooth/bap_broadcast_source conf_overlay=overlay-interleaved.conf \
+    sysbuild=1 compile
   app=tests/bsim/bluetooth/audio_samples/bap_broadcast_sink sysbuild=1 \
     conf_file=${ZEPHYR_BASE}/samples/bluetooth/bap_broadcast_sink/prj.conf \
     exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
 else
-  app=samples/bluetooth/bap_broadcast_source conf_overlay=overlay-bt_ll_sw_split.conf \
-    exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
+  app=samples/bluetooth/bap_broadcast_source \
+    conf_overlay='overlay-bt_ll_sw_split.conf;overlay-sequential.conf' \
+    exe_name=bs_${BOARD_TS}_${app}_prj_conf_overlay-sequential_conf sysbuild=1 compile
+  app=samples/bluetooth/bap_broadcast_source \
+    conf_overlay='overlay-bt_ll_sw_split.conf;overlay-interleaved.conf' \
+    exe_name=bs_${BOARD_TS}_${app}_prj_conf_overlay-interleaved_conf sysbuild=1 compile
   app=tests/bsim/bluetooth/audio_samples/bap_broadcast_sink \
     conf_file=${ZEPHYR_BASE}/samples/bluetooth/bap_broadcast_sink/prj.conf \
     conf_overlay=${ZEPHYR_BASE}/samples/bluetooth/bap_broadcast_sink/overlay-bt_ll_sw_split.conf \
