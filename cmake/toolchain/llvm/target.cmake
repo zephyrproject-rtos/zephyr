d54381263effd42b5bd2d4f6861c40aaf50c33a7
Piotr Rak <piotr.rak@gmail.com>
2024-02-25 18:50:32 +0100

    cmake: llvm: Remove common code in target selection
    Signed-off-by: Piotr Rak <piotr.rak@gmail.com>

--- cmake/toolchain/llvm/target.cmake
+++ cmake/toolchain/llvm/target.cmake
@@ -6,33 +6,12 @@ elseif(CONFIG_LLVM_USE_LLD)
   set(LINKER lld)
 endif()
 
-if("${ARCH}" STREQUAL "arm")
-  if(DEFINED CONFIG_ARMV8_M_MAINLINE)
-    # ARMv8-M mainline is ARMv7-M with additional features from ARMv8-M.
-    set(triple armv8m.main-none-eabi)
-  elseif(DEFINED CONFIG_ARMV8_M_BASELINE)
-    # ARMv8-M baseline is ARMv6-M with additional features from ARMv8-M.
-    set(triple armv8m.base-none-eabi)
-  elseif(DEFINED CONFIG_ARMV7_M_ARMV8_M_MAINLINE)
-    # ARMV7_M_ARMV8_M_MAINLINE means that ARMv7-M or backward compatible ARMv8-M
-    # processor is used.
-    set(triple armv7m-none-eabi)
-  elseif(DEFINED CONFIG_ARMV6_M_ARMV8_M_BASELINE)
-    # ARMV6_M_ARMV8_M_BASELINE means that ARMv6-M or ARMv8-M supporting the
-    # Baseline implementation processor is used.
-    set(triple armv6m-none-eabi)
-  else()
-    # Default ARM target supported by all processors.
-    set(triple arm-none-eabi)
-  endif()
+include(${ZEPHYR_BASE}/cmake/compiler/clang/flag-target.cmake)
 
+clang_target_flag("${ARCH}" triple)
+
+if("${ARCH}" STREQUAL "arm")
   set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nosys.specs")
-elseif("${ARCH}" STREQUAL "x86")
-  if(CONFIG_64BIT)
-    set(triple x86_64-pc-none-elf)
-  else()
-    set(triple i686-pc-none-elf)
-  endif()
 endif()
 
 if(DEFINED triple)
