d09611e871f2b248d9ccc96eb722e297c34f97ba
Piotr Rak <piotr.rak@gmail.com>
2024-02-25 18:33:45 +0100

    cmake: clang: Fix -mfpu selection for armv8.1m targets
    `-mfpu=auto` flag is accepted only by gcc, clang needs to specify it
    directly
    
    Signed-off-by: Piotr Rak <piotr.rak@gmail.com>

--- cmake/compiler/clang/target_arm.cmake
+++ cmake/compiler/clang/target_arm.cmake
@@ -11,7 +11,13 @@ endif()
 list(APPEND ARM_C_FLAGS -mabi=aapcs)
 
 if(CONFIG_FPU)
-  list(APPEND ARM_C_FLAGS   -mfpu=${GCC_M_FPU})
+  if (CONFIG_CPU_CORTEX_M AND "${GCC_M_FPU}" STREQUAL "auto")
+    # The clang compiler doesn't allow specify -mfpu=auto unlike gcc
+    # It's specified like that for arch armv8.1m-main cores in gcc-m-fpu.cmake
+    list(APPEND ARM_C_FLAGS   -mfpu=fp-armv8-fullfp16-${PRECISION_TOKEN}d16)
+  else()
+    list(APPEND ARM_C_FLAGS   -mfpu=${GCC_M_FPU})
+  endif()
 
   if(CONFIG_DCLS AND NOT CONFIG_FP_HARDABI)
     # If the processor is equipped with VFP and configured in DCLS topology,
@@ -25,7 +31,11 @@ if(CONFIG_FPU)
   elseif(CONFIG_FP_SOFTABI)
     list(APPEND ARM_C_FLAGS   -mfloat-abi=softfp)
   endif()
-endif()
+else()
+  if (CONFIG_ARMV8_M_MAINLINE)
+    list(APPEND ARM_C_FLAGS   -mfpu=none)
+  endif() # CONFIG_CORTEX_M AND CONFIG_ARMV8_1_M_MAINLINE
+endif() # CONFIG_FPU
 
 if(CONFIG_FP16)
   # Clang only supports IEEE 754-2008 format for __fp16. It's enabled by
