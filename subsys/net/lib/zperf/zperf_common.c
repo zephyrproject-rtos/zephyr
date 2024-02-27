c278fbe981abda27609e009bf43cb630f9f1d216
Maochen Wang <maochen.wang@nxp.com>
2024-02-27 14:33:36 +0800

    net: zperf: Fix TOS option not working in zperf
    When the zperf command is called with '-S' option which means IP_TOS
    for IPv4 and IPV6_TCLASS for IPv6, an error is printed and the
    setting does not work. The socket option handling was changed by
    commit 77e522a5a243('net: context: Refactor option setters'), but the
    callers of option setters were not changed. This causes the IP_TOS
    or IPV6_TCLASS option failed to set. The fix is to use uint8_t to
    store the value of the -S option.
    
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- subsys/net/lib/zperf/zperf_common.c
+++ subsys/net/lib/zperf/zperf_common.c
@@ -109,7 +109,7 @@ int zperf_get_ipv4_addr(char *host, struct in_addr *addr)
 	return 0;
 }
 
-int zperf_prepare_upload_sock(const struct sockaddr *peer_addr, int tos,
+int zperf_prepare_upload_sock(const struct sockaddr *peer_addr, uint8_t tos,
 			      int priority, int proto)
 {
 	socklen_t addrlen = peer_addr->sa_family == AF_INET6 ?
