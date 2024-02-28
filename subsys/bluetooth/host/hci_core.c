955334708036b3ad6f794c427c0c125c356e8176
Emil Gydesen <emil.gydesen@nordicsemi.no>
2024-02-28 14:51:47 +0100

    Bluetooth: ISO: Add CONFIG_BT_ISO_{RX/TX}
    Add 2 new Kconfig promptless options that are shorthand
    for whether the ISO configuration can support RX and TX.
    
    This also applies these new options as guards for existing
    and missing code pieces.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/host/hci_core.c
+++ subsys/bluetooth/host/hci_core.c
@@ -3266,10 +3266,14 @@ static int le_init_iso(void)
 
 		net_buf_unref(rsp);
 	} else if (IS_ENABLED(CONFIG_BT_CONN_TX)) {
-		LOG_WRN("Read Buffer Size V2 command is not supported. "
-			"No ISO TX buffers will be available");
+		if (IS_ENABLED(CONFIG_BT_ISO_TX)) {
+			LOG_WRN("Read Buffer Size V2 command is not supported. "
+				"No ISO TX buffers will be available");
+		}
 
-		/* Read LE Buffer Size */
+		/* Read LE Buffer Size in the case that we support ACL without TX ISO (e.g. if we
+		 * only support ISO sync receiver).
+		 */
 		err = bt_hci_cmd_send_sync(BT_HCI_OP_LE_READ_BUFFER_SIZE,
 					   NULL, &rsp);
 		if (err) {
