8957e92a12856bfc25ce553a1816584101f852be
Tom Burdick <thomas.burdick@intel.com>
2024-03-01 10:17:27 -0600

    llext: Rework hello_world test case to be "simple"
    The simple test is there to test the API and simple extensions in
    unison. Hello world was intended to be the first not the only extension
    being tested.
    
    Also refactors the entry thread to allow for usermode potentially by
    passing the pointer to the function symbol rather than having it look it
    up directly.
    
    Signed-off-by: Tom Burdick <thomas.burdick@intel.com>

---
+++ tests/subsys/llext/simple/src/hello_world_ext.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2023 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
+ * This very simple hello world C code can be used as a test case for building
+ * probably the simplest loadable extension. It requires a single symbol be
+ * linked, section relocation support, and the ability to export and call out to
+ * a function.
+ */
+
+#include <stdint.h>
+#include <zephyr/llext/symbol.h>
+#include <zephyr/sys/printk.h>
+
+static const uint32_t number = 42;
+
+void test_entry(void)
+{
+	printk("hello world\n");
+	printk("A number is %u\n", number);
+}
+LL_EXTENSION_SYMBOL(test_entry);
