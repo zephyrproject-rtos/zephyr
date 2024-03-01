2afd4e2114cef39bcce5fec3d55a771747ec2871
Erwan Gouriou <erwan.gouriou@st.com>
2024-03-01 14:25:56 +0100

    boards: st: stm32g0316: Use openocd as debug interface.
    Default pyocd configuration doesn't allow debugging on this board.
    Now that Zephyr openocd version support stm32g0 series, use it instead
    of pyocd.
    This commit provides working openocd configuration for this board
    an update documentation.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@st.com>

--- boards/st/stm32g0316_disco/board.cmake
+++ boards/st/stm32g0316_disco/board.cmake
@@ -3,6 +3,7 @@ board_runner_args(pyocd "--target=stm32g031j6mx")
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(jlink "--device=STM32G031J6" "--speed=4000")
 
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
