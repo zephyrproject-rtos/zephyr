3564824b8b428413fb0544844a3f628ea16d5d2f
Aleksander Wasaznik <aleksander.wasaznik@nordicsemi.no>
2024-03-04 15:08:06 +0100

    Bluetooth: tests: bsim: host/adv/resume2
    This test is an attempt at formalizing at least part of the behavior
    described in commit 6a79c3deae7d3a09965a6e71fc7da863aaa721fa.
    
    Signed-off-by: Aleksander Wasaznik <aleksander.wasaznik@nordicsemi.no>

---
+++ tests/bsim/bluetooth/host/adv/resume2/connectable/_build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2024 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+dotslash="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+bin_dir="${BSIM_OUT_PATH}/bin"
+BOARD="${BOARD:-nrf52_bsim}"
+
+cd "${dotslash}"
+
+compile_path="${bin_dir}/bs_${BOARD}_"
+compile_path+="$(realpath --relative-to "$(west topdir)"/zephyr prj.conf | tr /. _)"
+
+west build -b nrf52_bsim
+cp -v build/zephyr/zephyr.exe "${compile_path}"
---
+++ tests/bsim/bluetooth/host/adv/resume2/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2024 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+exec "$(dirname "${BASH_SOURCE[0]}")/run.py"
