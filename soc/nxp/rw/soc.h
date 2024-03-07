7f3a741c5898fc6aba01199937d4830b609e5097
Declan Snyder <declan.snyder@nxp.com>
2024-03-07 16:26:09 -0600

    soc: nxp: Add RW SOC Family
    Add SOC definition for NXP RW Family
    
    Signed-off-by: Declan Snyder <declan.snyder@nxp.com>

---
+++ soc/nxp/rw/soc.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2022-2023 NXP
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#ifndef _ASMLANGUAGE
+#include <zephyr/sys/util.h>
+#include <fsl_common.h>
+
+/* Add include for DTS generated information */
+#include <zephyr/devicetree.h>
+
+#endif /* !_ASMLANGUAGE */
+
+
+#ifdef CONFIG_MEMC
+uint32_t flexspi_clock_set_freq(uint32_t clock_name, uint32_t rate);
+#endif
+
+
+#endif /* _SOC__H_ */
