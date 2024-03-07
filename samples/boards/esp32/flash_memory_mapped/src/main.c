2bc8c77b4ece62e23160b899d95c43082012fc5a
Sylvio Alves <sylvio.alves@espressif.com>
2024-03-06 23:56:25 -0300

    samples: esp32: update to hal_espressif v5.1
    Modify and reorganize SoC to meet updated hal.
    
    Signed-off-by: Sylvio Alves <sylvio.alves@espressif.com>

--- samples/boards/esp32/flash_memory_mapped/src/main.c
+++ samples/boards/esp32/flash_memory_mapped/src/main.c
@@ -10,7 +10,7 @@
 #include <zephyr/drivers/flash.h>
 #include <zephyr/storage/flash_map.h>
 
-#include <esp_spi_flash.h>
+#include <spi_flash_mmap.h>
 #include <soc.h>
 
 #include <zephyr/logging/log.h>
