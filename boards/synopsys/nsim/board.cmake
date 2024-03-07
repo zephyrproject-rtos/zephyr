3b26426b64bd21b5abaabfd5a55dae09f6bd5002
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-03-07 08:43:26 +0000

    boards: snps: nsim: Fix board usage for filenames
    Fixes an issue where the board name variable has changed in hwmv2
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- boards/synopsys/nsim/board.cmake
+++ boards/synopsys/nsim/board.cmake
@@ -1,15 +1,18 @@
 # SPDX-License-Identifier: Apache-2.0
 set(SUPPORTED_EMU_PLATFORMS nsim)
 
+string(SUBSTRING "${BOARD_IDENTIFIER}" 1 -1 NSIM_BASE_FILENAME)
+string(REPLACE "/" "_" NSIM_BASE_FILENAME "${NSIM_BASE_FILENAME}")
+
 if(NOT (CONFIG_SOC_NSIM_HS_SMP OR CONFIG_SOC_NSIM_HS6X_SMP OR CONFIG_SOC_NSIM_HS5X_SMP))
   board_set_flasher_ifnset(arc-nsim)
   board_set_debugger_ifnset(arc-nsim)
 
-  set(NSIM_PROPS "${BOARD}.props")
+  set(NSIM_PROPS "${NSIM_BASE_FILENAME}.props")
   board_runner_args(arc-nsim "--props=${NSIM_PROPS}")
 endif()
 
-string(REPLACE "nsim" "mdb" MDB_ARGS "${BOARD}.args")
+string(REPLACE "nsim" "mdb" MDB_ARGS "${NSIM_BASE_FILENAME}.args")
 
 board_runner_args(mdb-nsim "--cores=${CONFIG_MP_MAX_NUM_CPUS}" "--nsim_args=${MDB_ARGS}")
 board_runner_args(mdb-hw "--cores=${CONFIG_MP_MAX_NUM_CPUS}")
