f085a4d07f71dbeeb398b95a37bdb0dd09c75273
Anas Nashif <anas.nashif@intel.com>
2024-03-07 18:18:36 -0500

    kernel: sched: remove unused prototype: z_is_thread_time_slicing
    A prototype not used anywhere.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- kernel/include/ksched.h
+++ kernel/include/ksched.h
@@ -39,7 +39,6 @@ BUILD_ASSERT(K_LOWEST_APPLICATION_THREAD_PRIO
 
 void z_sched_init(void);
 void z_move_thread_to_end_of_prio_q(struct k_thread *thread);
-int z_is_thread_time_slicing(struct k_thread *thread);
 void z_unpend_thread_no_timeout(struct k_thread *thread);
 struct k_thread *z_unpend1_no_timeout(_wait_q_t *wait_q);
 int z_pend_curr(struct k_spinlock *lock, k_spinlock_key_t key,
