2023268f90e591d890c3bf209151a06b2ee5ea65
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2024-03-08 13:26:21 +0100

    soc: nordic: Refactor soc_secure.h
    The soc_secure_* function are used by the non-secure application
    to access hardware resources which are mapped as secure.
    Using these functions for hardware resources mapped as non-secure
    is missleading.
    
    We have some soc_secure_* functions which read FICR values.
    In nRF91 and nRF53 platforms this made sense since FICR
    has hardware fixed mapping as secure.
    For nRF54 though the FICR has hardware fixed mapping as non-secure.
    
    This change refactors the soc_secure.h to exclude the functions
    which read FICR values from being included when FICR is mapped as
    non-secure.
    
    Also updates the hwinfo and ieee802154 drivers to adjust to this change.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

--- soc/nordic/common/soc_secure.h
+++ soc/nordic/common/soc_secure.h
@@ -15,6 +15,25 @@ void soc_secure_gpio_pin_mcu_select(uint32_t pin_number, nrf_gpio_pin_sel_t mcu)
 
 int soc_secure_mem_read(void *dst, void *src, size_t len);
 
+#else /* defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) */
+#if NRF_GPIO_HAS_SEL
+static inline void soc_secure_gpio_pin_mcu_select(uint32_t pin_number, nrf_gpio_pin_sel_t mcu)
+{
+	nrf_gpio_pin_control_select(pin_number, mcu);
+}
+#endif /* NRF_GPIO_HAS_SEL */
+
+static inline int soc_secure_mem_read(void *dst, void *src, size_t len)
+{
+	(void)memcpy(dst, src, len);
+	return 0;
+}
+
+#endif /* defined CONFIG_TRUSTED_EXECUTION_NONSECURE */
+
+/* Include these soc_secure_* functions only when the FICR is mapped as secure only */
+#if defined(NRF_FICR_S)
+#if defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 #if defined(CONFIG_SOC_HFXO_CAP_INTERNAL)
 static inline uint32_t soc_secure_read_xosc32mtrim(void)
 {
@@ -41,31 +60,18 @@ static inline void soc_secure_read_deviceid(uint32_t deviceid[2])
 }
 
 #else /* defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) */
-
-static inline int soc_secure_mem_read(void *dst, void *src, size_t len)
-{
-	(void)memcpy(dst, src, len);
-	return 0;
-}
-
-#if NRF_GPIO_HAS_SEL
-static inline void soc_secure_gpio_pin_mcu_select(uint32_t pin_number,
-						  nrf_gpio_pin_sel_t mcu)
-{
-	nrf_gpio_pin_control_select(pin_number, mcu);
-}
-#endif /* NRF_GPIO_HAS_SEL */
-
 #if defined(CONFIG_SOC_HFXO_CAP_INTERNAL)
 static inline uint32_t soc_secure_read_xosc32mtrim(void)
 {
-	return NRF_FICR->XOSC32MTRIM;
+	return NRF_FICR_S->XOSC32MTRIM;
 }
 #endif /* defined(CONFIG_SOC_HFXO_CAP_INTERNAL) */
 
 static inline void soc_secure_read_deviceid(uint32_t deviceid[2])
 {
-	deviceid[0] = nrf_ficr_deviceid_get(NRF_FICR, 0);
-	deviceid[1] = nrf_ficr_deviceid_get(NRF_FICR, 1);
+	deviceid[0] = nrf_ficr_deviceid_get(NRF_FICR_S, 0);
+	deviceid[1] = nrf_ficr_deviceid_get(NRF_FICR_S, 1);
 }
+
 #endif /* defined CONFIG_TRUSTED_EXECUTION_NONSECURE */
+#endif /* defined(NRF_FICR_S) */
