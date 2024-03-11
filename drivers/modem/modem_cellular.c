zephyrproject-rtos/zephyr

commit 080d51bb367836b73021fdd7ade3e2c4c54a0435

modem: modem_cellular: CID 338067
incorrect size passed to strncpy()

Signed-off-by: Bjarki Arge Andreasen <bjarki@arge-andreasen.me>

drivers/modem/modem_cellular.c
@@ -370,7 +370,7 @@ static void modem_cellular_chat_on_imsi(struct modem_chat *chat, char **argv, ui
 {
 	struct modem_cellular_data *data = (struct modem_cellular_data *)user_data;
 
-	strncpy(data->imsi, (char *)argv[1], sizeof(data->imsi));
+	strncpy(data->imsi, argv[1], sizeof(data->imsi) - 1);
 }
 
 static bool modem_cellular_is_registered(struct modem_cellular_data *data)

commit a7dd127b7ce1b0f1a4580d64c899268c7c663616

modem: modem_cellular: increase imsi size
Fixes an issue introduced with `080d51bb`

Signed-off-by: Lucas Denefle <lucas.denefle@converge.io>

drivers/modem/modem_cellular.c
@@ -28,7 +28,7 @@ LOG_MODULE_REGISTER(modem_cellular, CONFIG_MODEM_LOG_LEVEL);
 
 #define MODEM_CELLULAR_DATA_IMEI_LEN         (16)
 #define MODEM_CELLULAR_DATA_MODEL_ID_LEN     (65)
-#define MODEM_CELLULAR_DATA_IMSI_LEN         (22)
+#define MODEM_CELLULAR_DATA_IMSI_LEN         (23)
 #define MODEM_CELLULAR_DATA_ICCID_LEN        (22)
 #define MODEM_CELLULAR_DATA_MANUFACTURER_LEN (65)
 #define MODEM_CELLULAR_DATA_FW_VERSION_LEN   (65)