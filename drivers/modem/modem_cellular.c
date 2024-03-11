zephyrproject-rtos/zephyr

commit ae951f5f0215b8b8c57a5fed97d773770e7ed16f

modem: modem_cellular: CID 338140
incorrect size passed to strncpy

Signed-off-by: Bjarki Arge Andreasen <bjarki@arge-andreasen.me>

drivers/modem/modem_cellular.c
@@ -325,7 +325,7 @@ static void modem_cellular_chat_on_cgmi(struct modem_chat *chat, char **argv, ui
 		return;
 	}
 
-	strncpy(data->manufacturer, argv[1], sizeof(data->manufacturer));
+	strncpy(data->manufacturer, argv[1], sizeof(data->manufacturer) - 1);
 }
 
 static void modem_cellular_chat_on_cgmr(struct modem_chat *chat, char **argv, uint16_t argc,

commit 974c63103f5351d7df9accab887843489aaae20c

modem: modem_cellular: increase manufacturer size
Fixes an issue introduced with `ae951f5f`

Signed-off-by: Lucas Denefle <lucas.denefle@converge.io>

drivers/modem/modem_cellular.c
@@ -30,7 +30,7 @@ LOG_MODULE_REGISTER(modem_cellular, CONFIG_MODEM_LOG_LEVEL);
 #define MODEM_CELLULAR_DATA_MODEL_ID_LEN     (64)
 #define MODEM_CELLULAR_DATA_IMSI_LEN         (22)
 #define MODEM_CELLULAR_DATA_ICCID_LEN        (22)
-#define MODEM_CELLULAR_DATA_MANUFACTURER_LEN (64)
+#define MODEM_CELLULAR_DATA_MANUFACTURER_LEN (65)
 #define MODEM_CELLULAR_DATA_FW_VERSION_LEN   (65)
 
 /* Magic constants */