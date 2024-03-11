zephyrproject-rtos/zephyr

commit b668296a50a17132cc911fac3009ef8ee01652d2

modem: modem_cellular: CID 338073
incorrect size passed to strncpy()

Signed-off-by: Bjarki Arge Andreasen <bjarki@arge-andreasen.me>

drivers/modem/modem_cellular.c
@@ -301,7 +301,7 @@ static void modem_cellular_chat_on_imei(struct modem_chat *chat, char **argv, ui
 		return;
 	}
 
-	strncpy(data->imei, argv[1], sizeof(data->imei));
+	strncpy(data->imei, argv[1], sizeof(data->imei) - 1);
 }
 
 static void modem_cellular_chat_on_cgmm(struct modem_chat *chat, char **argv, uint16_t argc,

commit 02adba1ee295ddcacacf6ab1c11a1bb1e5dfd3b5

modem: modem_cellular: increase imei size
Fixes an issue introduced with `b668296a`

Signed-off-by: Lucas Denefle <lucas.denefle@converge.io>

drivers/modem/modem_cellular.c
@@ -26,7 +26,7 @@ LOG_MODULE_REGISTER(modem_cellular, CONFIG_MODEM_LOG_LEVEL);
 #define MODEM_CELLULAR_PERIODIC_SCRIPT_TIMEOUT \
 	K_MSEC(CONFIG_MODEM_CELLULAR_PERIODIC_SCRIPT_MS)
 
-#define MODEM_CELLULAR_DATA_IMEI_LEN         (15)
+#define MODEM_CELLULAR_DATA_IMEI_LEN         (16)
 #define MODEM_CELLULAR_DATA_MODEL_ID_LEN     (65)
 #define MODEM_CELLULAR_DATA_IMSI_LEN         (22)
 #define MODEM_CELLULAR_DATA_ICCID_LEN        (22)