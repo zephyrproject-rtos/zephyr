be654f38b3b05fc4338fd47f388a6c3807607899
Samy Francelet <samy.francelet@ik.me>
2024-03-13 00:55:49 +0100

    drivers: eswifi: Added config option for MAX_DATA_SIZE
    Default MAX_DATA_SIZE might be too small depending on application (e.g.
    handling the scan result with 12 APs detected)
    Added config option for MAX_DATA_SIZE and warning for the user
    
    Signed-off-by: Samy Francelet <samy.francelet@ik.me>

--- drivers/wifi/eswifi/eswifi_core.c
+++ drivers/wifi/eswifi/eswifi_core.c
@@ -141,6 +141,12 @@ int eswifi_at_cmd_rsp(struct eswifi_dev *eswifi, char *cmd, char **rsp)
 		return -EIO;
 	}
 
+	if (len == CONFIG_WIFI_ESWIFI_MAX_DATA_SIZE) {
+		LOG_WRN("Buffer might be too small for response!");
+		LOG_WRN("See CONFIG_WIFI_ESWIFI_MAX_DATA_SIZE (in build: %d)",
+			CONFIG_WIFI_ESWIFI_MAX_DATA_SIZE);
+	}
+
 	/*
 	 * Check response, format should be "\r\n[DATA]\r\nOK\r\n>"
 	 * Data is in arbitrary format (not only ASCII)
