604fd09c8c413e33940b2660cc8ba7c682741bd0
Gerard Marull-Paretas <gerard@teslabs.com>
2024-03-12 12:03:16 +0100

    boards: nordic: nrf54h20dk: add custom JLink reset scheme
    A custom reset scheme is required for the nRF54H20 SoC so that debug
    works out of the box. This magic will eventually be part of JLink.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- boards/nordic/nrf54h20dk/board.cmake
+++ boards/nordic/nrf54h20dk/board.cmake
@@ -3,13 +3,12 @@
 include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
 
 if(CONFIG_BOARD_NRF54H20DK_NRF54H20_CPUAPP OR CONFIG_BOARD_NRF54H20DK_NRF54H20_CPURAD)
-  if(CONFIG_BOARD_NRF54H20DK_NRF54H20_CPURAD)
-    set(
-      JLINK_TOOL_OPT
-      "-jlinkscriptfile ${CMAKE_CURRENT_LIST_DIR}/support/nrf54h20_cpurad.JLinkScript"
-    )
+  if(CONFIG_BOARD_NRF54H20DK_NRF54H20_CPUAPP)
+    set(JLINKSCRIPTFILE ${CMAKE_CURRENT_LIST_DIR}/support/nrf54h20_cpuapp.JLinkScript)
+  else()
+    set(JLINKSCRIPTFILE ${CMAKE_CURRENT_LIST_DIR}/support/nrf54h20_cpurad.JLinkScript)
   endif()
 
-  board_runner_args(jlink "--device=CORTEX-M33" "--speed=4000" "--tool-opt=${JLINK_TOOL_OPT}")
+  board_runner_args(jlink "--device=CORTEX-M33" "--speed=4000" "--tool-opt=-jlinkscriptfile ${JLINKSCRIPTFILE}")
   include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 endif()
