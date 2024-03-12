9b2200664917228826b47737ee6ee0e146ad594d
Erwan Gouriou <erwan.gouriou@st.com>
2024-03-12 14:29:45 +0100

    boards: st: nucleo_wba: Fix openocd configuration
    Using Zephyr SDK 0.16.5, openocd can be used to debug WBA based boards.
    Fix documentation and clean up board.cmake configuration for
    nucleo_wba52cg from a local configuration which shouldn't have been
    upstreamed
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@st.com>

--- boards/st/nucleo_wba52cg/board.cmake
+++ boards/st/nucleo_wba52cg/board.cmake
@@ -1,7 +1,4 @@
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 
-set(OPENOCD "/local/mcu/tools/openocd/src/openocd" CACHE FILEPATH "" FORCE)
-set(OPENOCD_DEFAULT_PATH /local/mcu/tools/openocd/tcl)
-
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
