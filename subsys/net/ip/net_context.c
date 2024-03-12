4c45c3e1b20e3f15be8ac9c6fa40afc111638e7e
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-03-12 16:30:24 +0900

    net: context: Fix the ICMP error on raw
    This commit applies the issues detected in UDP to recv_raw() as
    well. Please refer to the previous commit log for details.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>
    (cherry picked from commit 7d1edd1fcb3516f0b8b8c07fe55086675d3e382f)

--- subsys/net/ip/net_context.c
+++ subsys/net/ip/net_context.c
@@ -2512,17 +2512,30 @@ static int recv_raw(struct net_context *context,
 
 	ARG_UNUSED(timeout);
 
-	context->recv_cb = cb;
-
+	/* If the context already has a connection handler, it means it's
+	 * already registered. In that case, all we have to do is 1) update
+	 * the callback registered in the net_context and 2) update the
+	 * user_data using net_conn_update().
+	 *
+	 * The callback function passed to net_conn_update() must be the same
+	 * function as the one passed to net_conn_register(), not the callback
+	 * set for the net context passed by recv_raw().
+	 */
 	if (context->conn_handler) {
-		net_conn_unregister(context->conn_handler);
-		context->conn_handler = NULL;
+		context->recv_cb = cb;
+		ret = net_conn_update(context->conn_handler,
+				      net_context_raw_packet_received,
+				      user_data,
+				      NULL, 0);
+		return ret;
 	}
 
 	ret = bind_default(context);
 	if (ret) {
 		return ret;
 	}
+
+	context->recv_cb = cb;
 
 	ret = net_conn_register(net_context_get_proto(context),
 				net_context_get_family(context),
