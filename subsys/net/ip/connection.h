bc1af7a9979dbc279dae90a76ea87ebcc63910a3
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-03-13 19:27:41 +0900

    net: conn: Add internal function for update connection
    This commit adds the new internal function for update the callback,
    user data, remote address, and port for a registered connection
    handle.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>
    (cherry picked from commit 46ca624be41d6f0029d2d499c2473e718f09f869)

--- subsys/net/ip/connection.h
+++ subsys/net/ip/connection.h
@@ -157,6 +157,24 @@ static inline int net_conn_unregister(struct net_conn_handle *handle)
 #endif
 
 /**
+ * @brief Update the callback, user data, remote address, and port
+ * for a registered connection handle.
+ *
+ * @param handle A handle registered with net_conn_register()
+ * @param cb Callback to be called
+ * @param user_data User data supplied by caller.
+ * @param remote_addr Remote address
+ * @param remote_port Remote port
+ *
+ * @return Return 0 if the the change succeed, <0 otherwise.
+ */
+int net_conn_update(struct net_conn_handle *handle,
+		    net_conn_cb_t cb,
+		    void *user_data,
+		    const struct sockaddr *remote_addr,
+		    uint16_t remote_port);
+
+/**
  * @brief Called by net_core.c when a network packet is received.
  *
  * @param pkt Network packet holding received data
