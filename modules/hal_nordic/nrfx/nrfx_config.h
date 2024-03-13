72c385198c287a8af1d372c845600896858b58a1
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2024-03-13 17:29:33 +0100

    modules: hal_nordic: Remove support for nRF54H20 EngA
    This was a preview revision of the SoC that will no longer
    be supported.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- modules/hal_nordic/nrfx/nrfx_config.h
+++ modules/hal_nordic/nrfx/nrfx_config.h
@@ -947,12 +947,6 @@
     #include <nrfx_config_nrf54h20_radiocore.h>
 #elif defined(NRF54H20_XXAA) && defined(NRF_PPR)
     #include <nrfx_config_nrf54h20_ppr.h>
-#elif defined(NRF54H20_ENGA_XXAA) && defined(NRF_APPLICATION)
-    #include <nrfx_config_nrf54h20_enga_application.h>
-#elif defined(NRF54H20_ENGA_XXAA) && defined(NRF_RADIOCORE)
-    #include <nrfx_config_nrf54h20_enga_radiocore.h>
-#elif defined(NRF54H20_ENGA_XXAA) && defined(NRF_PPR)
-    #include <nrfx_config_nrf54h20_enga_ppr.h>
 #elif (defined(NRF54L15_XXAA) || defined(NRF54L15_ENGA_XXAA)) && defined(NRF_APPLICATION)
     #include <nrfx_config_nrf54l15_enga_application.h>
 #elif defined(NRF9120_XXAA) || defined(NRF9160_XXAA)
