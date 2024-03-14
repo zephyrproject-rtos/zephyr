0289105758805e78ed3b2f60073ff90fbbaac3c8
Francois Ramu <francois.ramu@st.com>
2024-03-14 12:44:55 +0100

    samples: drivers: include the stm32 xspi node for running the samples
    The stm32 xspi node is another possible multi-spi for connecting
    external NOR flash memories.
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- samples/drivers/spi_flash/src/main.c
+++ samples/drivers/spi_flash/src/main.c
@@ -26,7 +26,9 @@
 #endif
 #define SPI_FLASH_SECTOR_SIZE        4096
 
-#if defined(CONFIG_FLASH_STM32_OSPI) || defined(CONFIG_FLASH_STM32_QSPI)
+#if defined(CONFIG_FLASH_STM32_OSPI) || \
+	defined(CONFIG_FLASH_STM32_QSPI) || \
+	defined(CONFIG_FLASH_STM32_XSPI)
 #define SPI_FLASH_MULTI_SECTOR_TEST
 #endif
 
