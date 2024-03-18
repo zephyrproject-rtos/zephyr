zephyrproject-rtos/zephyr

commit f78a0810667b3b17706596a0ad2c53eb53a9400b

drivers: ethernet: dsa_ksz8xxx: use NET_DEVICE_DT_DEFINE_INSTANCE
Move to using NET_DEVICE_DT_DEFINE_INSTANCE instead of
NET_DEVICE_INIT_INSTANCE as the driver is devicetree based and it
lets us remove DT_LABEL usage in the driver itself.

Signed-off-by: Kumar Gala <galak@kernel.org>

drivers/ethernet/dsa_ksz8xxx.c
@@ -1064,8 +1064,7 @@ static struct dsa_api dsa_api_f = {
 	const struct dsa_slave_config dsa_0_slave_##slave##_config = {     \
 		.mac_addr = DT_PROP_OR(slave, local_mac_address, {0})      \
 	};                                                                 \
-	NET_DEVICE_INIT_INSTANCE(dsa_slave_port_##slave,                   \
-	DT_LABEL(slave),                                                   \
+	NET_DEVICE_DT_DEFINE_INSTANCE(slave,                               \
 	n,                                                                 \
 	dsa_port_init,                                                     \
 	NULL,                                                              \

commit 6d3dc270eb705a72e3f48bbe2bbab170ef53bc79

net: dsa: Fix DSA driver for KSZ8xxx to correctly initialize LAN devices
The commit "drivers: ethernet: dsa_ksz8xxx: use
NET_DEVICE_DT_DEFINE_INSTANCE"
(SHA1: f78a0810667b3b17706596a0ad2c53eb53a9400b) replaced
NET_DEVICE_INIT_INSTANCE() with NET_DEVICE_DT_DEFINE_INSTANCE() to
facilitate the removal of deprecated (from Zephyr's 3.2 release)
DT_LABEL() macro.

Unfortunately, the per LAN port initialization is necessary for correct
operation of the DSA driver - otherwise following errors were visible
when LLDP DSA sample (samples/net/dsa/src) was run on ip_k66f board:

<wrn> net_if: iface 0x20001440 is down
<inf> net_dsa_lldp_sample: LLDP pkt recv -> lan1
<inf> net_dsa_lldp_sample:   CHASSIS ID:     38:05:43:69:XX:ZZ
<inf> net_dsa_lldp_sample:   PORT ID:        38:05:43:69:XX:ZZ
<inf> net_dsa_lldp_sample:   TTL:            120s
<inf> net_dsa_lldp_sample:   SYSTEM NAME:    mtt
<err> net_dsa_lldp_sample: Failed to send, errno 115

The fix is to use again NET_DEVICE_INIT_INSTANCE() with "lan"X name
assigned (to avoid too long names when recommended DT_PROP() is
used instead of DT_LABEL()).

Fixes: f78a0810667b3b17706596a0ad2c53eb53a9400b

Signed-off-by: Lukasz Majewski <lukma@denx.de>

drivers/ethernet/dsa_ksz8xxx.c
@@ -1061,11 +1061,14 @@ static struct dsa_api dsa_api_f = {
  * For simple cases it is just good enough.
  */
 
+#define KSZ8XXX_STR(x)		#x
+#define KSZ8XXX_XSTR(x)	KSZ8XXX_STR(x)
 #define NET_SLAVE_DEVICE_INIT_INSTANCE(slave, n)                           \
 	const struct dsa_slave_config dsa_0_slave_##slave##_config = {     \
 		.mac_addr = DT_PROP_OR(slave, local_mac_address, {0})      \
 	};                                                                 \
-	NET_DEVICE_DT_DEFINE_INSTANCE(slave,                               \
+	NET_DEVICE_INIT_INSTANCE(dsa_slave_port_##slave,                   \
+	"lan" KSZ8XXX_XSTR(n),                                             \
 	n,                                                                 \
 	dsa_port_init,                                                     \
 	NULL,                                                              \