364875bf14ded865a329fab5d743c25a9100b9ed
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2024-03-18 16:16:40 +0200

    modem: backends: uart: register their own log modules
    Instead of both using the same modem_backend_uart, for better visibility.
    
    Signed-off-by: Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>

--- subsys/modem/backends/modem_backend_uart_isr.c
+++ subsys/modem/backends/modem_backend_uart_isr.c
@@ -7,7 +7,7 @@
 #include "modem_backend_uart_isr.h"
 
 #include <zephyr/logging/log.h>
-LOG_MODULE_DECLARE(modem_backend_uart, CONFIG_MODEM_MODULES_LOG_LEVEL);
+LOG_MODULE_REGISTER(modem_backend_uart_isr, CONFIG_MODEM_MODULES_LOG_LEVEL);
 
 #include <string.h>
 
