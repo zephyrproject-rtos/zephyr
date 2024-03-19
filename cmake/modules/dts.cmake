cc54548856f37691e8416c04835f0f2378c026be
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-03-19 09:14:12 +0000

    cmake: Add support for socs folder and Kconfig fragments
    Adds support for a new ``socs`` folder that can be placed in
    application folders and functions similar to the ``boards``
    folder, but works for SoCs instead of boards
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- cmake/modules/dts.cmake
+++ cmake/modules/dts.cmake
@@ -123,7 +123,8 @@ set(DTS_CMAKE                   ${PROJECT_BINARY_DIR}/dts.cmake)
 set(VENDOR_PREFIXES             dts/bindings/vendor-prefixes.txt)
 
 if(NOT DEFINED DTS_SOURCE)
-  zephyr_build_string(dts_board_string BOARD ${BOARD} BOARD_IDENTIFIER ${BOARD_IDENTIFIER} MERGE)
+  zephyr_build_string(dts_board_string BOARD ${BOARD} QUALIFIERS ${BOARD_IDENTIFIER} MERGE)
+
   foreach(str ${dts_board_string})
     if(EXISTS ${BOARD_DIR}/${str}.dts)
       set(DTS_SOURCE ${BOARD_DIR}/${str}.dts)
@@ -136,9 +137,10 @@ if(EXISTS ${DTS_SOURCE})
   # We found a devicetree. Check for a board revision overlay.
   if(DEFINED BOARD_REVISION)
     zephyr_build_string(dts_board_string BOARD ${BOARD}
-                                         BOARD_IDENTIFIER ${BOARD_IDENTIFIER}
+                                         QUALIFIERS ${BOARD_IDENTIFIER}
                                          BOARD_REVISION ${BOARD_REVISION}
     )
+
     if(EXISTS ${BOARD_DIR}/${dts_board_string}.overlay)
       list(APPEND DTS_SOURCE ${BOARD_DIR}/${dts_board_string}.overlay)
     endif()
