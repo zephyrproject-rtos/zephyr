4d3f188035062d65e4f98566e7bacb5fdf803a47
Jiafei Pan <Jiafei.Pan@nxp.com>
2024-03-19 17:01:19 +0800

    tests: counter_basic_api: add imx93 evk support
    Add i.MX 93 EVK support for counter_basic_api test application.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- tests/drivers/counter/counter_basic_api/src/test_counter.c
+++ tests/drivers/counter/counter_basic_api/src/test_counter.c
@@ -57,6 +57,9 @@ static const struct device *const devices[] = {
 	DEVS_FOR_DT_COMPAT(microchip_xec_timer)
 	DEVS_FOR_DT_COMPAT(nxp_imx_epit)
 	DEVS_FOR_DT_COMPAT(nxp_imx_gpt)
+#ifdef CONFIG_COUNTER_MCUX_TPM
+	DEVS_FOR_DT_COMPAT(nxp_tpm_timer)
+#endif
 	DEVS_FOR_DT_COMPAT(renesas_smartbond_timer)
 #ifdef CONFIG_COUNTER_MCUX_CTIMER
 	DEVS_FOR_DT_COMPAT(nxp_lpc_ctimer)
