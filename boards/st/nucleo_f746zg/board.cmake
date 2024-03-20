39ac02b8f5db31ad231bd443cfcdc46f4889d6d6
Abderrahmane Jarmouni <abderrahmane.jarmouni-ext@st.com>
2024-03-20 15:54:00 +0100

    board: arm: nucleo_f746zg: flashing
    Add the possibility to flash nucleo-f746zg board using west
    STM32CubeProgrammer runner.
    
    Signed-off-by: Abderrahmane Jarmouni <abderrahmane.jarmouni-ext@st.com>

--- boards/st/nucleo_f746zg/board.cmake
+++ boards/st/nucleo_f746zg/board.cmake
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(jlink "--device=STM32F746ZG" "--speed=4000")
 
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
