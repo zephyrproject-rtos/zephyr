8a78fcd03bfca5faf36dc9657196aa6ba3ecc75f
Chun-Chieh Li <ccli8@nuvoton.com>
2024-03-20 15:33:56 +0800

    boards: numaker_pfm_m467: change default runner to pyocd
    PyOCD is preferred for better support.
    
    Signed-off-by: Chun-Chieh Li <ccli8@nuvoton.com>

--- boards/nuvoton/numaker_pfm_m467/board.cmake
+++ boards/nuvoton/numaker_pfm_m467/board.cmake
@@ -3,7 +3,7 @@
 board_runner_args(pyocd "--target=m467hjhae")
 board_runner_args(nulink "-f")
 
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nulink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/canopen.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
