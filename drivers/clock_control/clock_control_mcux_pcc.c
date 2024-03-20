672511b85d41f9c22297098f31d96c2331c9fef9
Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>
2024-03-20 16:39:58 +0200

    drivers: clock: clock_control_mcux_pcc: rework clock ID computation
    In the case of some SOCs simply using the PCC base and clock
    offset to build the clock ID passed to the HAL is not sufficient
    since the clock ID may also contain some flags (e.g: see the case of
    i.MX8ULP). For such SOCs, we change the semantic of the value passed
    from the DTS (which is simply called "name", thus allowing us to
    interpret it differently from SOC to SOC) to an index in a LUT
    used to fetch the clock ID that can be safely passed to the HAL.
    
    For compatibility reasons, we also keep the old way and use it
    for SOCs which don't define such a LUT. The driver will decide
    which method to use based on the LUT size (i.e: if 0, use the
    old way, if != 0 use the new way).
    
    Signed-off-by: Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>

--- drivers/clock_control/clock_control_mcux_pcc.c
+++ drivers/clock_control/clock_control_mcux_pcc.c
@@ -20,6 +20,8 @@ LOG_MODULE_REGISTER(clock_control_mcux_pcc);
 
 struct mcux_pcc_config {
 	uint32_t base_address;
+	uint32_t *clocks;
+	uint32_t clock_num;
 };
 
 #define DEV_BASE(dev) (((struct mcux_pcc_config *)(dev->config))->base_address)
@@ -27,25 +29,60 @@ struct mcux_pcc_config {
 #define MAKE_PCC_REGADDR(base, offset) ((base) + (offset))
 #endif
 
-static inline clock_ip_name_t clock_ip(const struct device *dev,
-				       clock_control_subsys_t sub_system)
+static inline int get_clock_encoding(const struct device *dev,
+				     clock_control_subsys_t sub_system,
+				     uint32_t *clock_encoding)
 {
-	uint32_t offset = POINTER_TO_UINT(sub_system);
+	const struct mcux_pcc_config *cfg;
+	uint32_t clock_name;
 
-	return MAKE_PCC_REGADDR(DEV_BASE(dev), offset);
+	cfg = dev->config;
+	clock_name = POINTER_TO_UINT(sub_system);
+
+	if (!cfg->clock_num) {
+		*clock_encoding = MAKE_PCC_REGADDR(DEV_BASE(dev), clock_name);
+		return 0;
+	}
+
+	/* sanity check */
+	if (clock_name >= cfg->clock_num) {
+		return -EINVAL;
+	}
+
+	*clock_encoding = cfg->clocks[clock_name];
+
+	return 0;
 }
 
 static int mcux_pcc_on(const struct device *dev,
 		       clock_control_subsys_t sub_system)
 {
-	CLOCK_EnableClock(clock_ip(dev, sub_system));
+	uint32_t clock_encoding;
+	int ret;
+
+	ret = get_clock_encoding(dev, sub_system, &clock_encoding);
+	if (ret < 0) {
+		return ret;
+	}
+
+	CLOCK_EnableClock(clock_encoding);
+
 	return 0;
 }
 
 static int mcux_pcc_off(const struct device *dev,
 			clock_control_subsys_t sub_system)
 {
-	CLOCK_DisableClock(clock_ip(dev, sub_system));
+	uint32_t clock_encoding;
+	int ret;
+
+	ret = get_clock_encoding(dev, sub_system, &clock_encoding);
+	if (ret < 0) {
+		return ret;
+	}
+
+	CLOCK_DisableClock(clock_encoding);
+
 	return 0;
 }
 
@@ -53,7 +90,16 @@ static int mcux_pcc_get_rate(const struct device *dev,
 			       clock_control_subsys_t sub_system,
 			       uint32_t *rate)
 {
-	*rate = CLOCK_GetIpFreq(clock_ip(dev, sub_system));
+	uint32_t clock_encoding;
+	int ret;
+
+	ret = get_clock_encoding(dev, sub_system, &clock_encoding);
+	if (ret < 0) {
+		return ret;
+	}
+
+	*rate = CLOCK_GetIpFreq(clock_encoding);
+
 	return 0;
 }
 
@@ -63,9 +109,18 @@ static const struct clock_control_driver_api mcux_pcc_api = {
 	.get_rate = mcux_pcc_get_rate,
 };
 
+/* this is empty for SOCs which don't need a translation from
+ * the clock ID passed through the DTS and the clock ID encoding
+ * from the HAL. For these SOCs, the clock ID will be built based
+ * on the value passed from the DTS and the PCC base.
+ */
+static uint32_t clocks[] = {};
+
 #define MCUX_PCC_INIT(inst)						\
 	static const struct mcux_pcc_config mcux_pcc##inst##_config = {	\
-		.base_address = DT_INST_REG_ADDR(inst)			\
+		.base_address = DT_INST_REG_ADDR(inst),			\
+		.clocks = clocks,					\
+		.clock_num = ARRAY_SIZE(clocks),			\
 	};								\
 									\
 	DEVICE_DT_INST_DEFINE(inst,					\
