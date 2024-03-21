e64fe90a8bfaf25f412134e6881f14e878102633
Robert Lubos <robert.lubos@nordicsemi.no>
2024-03-21 11:27:49 +0100

    Revert "[nrf fromtree] Bluetooth: BAP: Add support for transparent coding format"
    This reverts commit 60eeedf060525bfa5a18816f61f762caa3d5fcae.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- include/zephyr/bluetooth/audio/audio.h
+++ include/zephyr/bluetooth/audio/audio.h
@@ -209,7 +209,6 @@ enum bt_audio_metadata_type {
 	((struct bt_audio_codec_cfg){                                                              \
 		/* Use HCI data path as default, can be overwritten by application */              \
 		.path_id = BT_ISO_DATA_PATH_HCI,                                                   \
-		.ctlr_transcode = false,                                                           \
 		.id = _id,                                                                         \
 		.cid = _cid,                                                                       \
 		.vid = _vid,                                                                       \
@@ -232,7 +231,6 @@ enum bt_audio_metadata_type {
 	((struct bt_audio_codec_cap){                                                              \
 		/* Use HCI data path as default, can be overwritten by application */              \
 		.path_id = BT_ISO_DATA_PATH_HCI,                                                   \
-		.ctlr_transcode = false,                                                           \
 		.id = (_id),                                                                       \
 		.cid = (_cid),                                                                     \
 		.vid = (_vid),                                                                     \
@@ -318,12 +316,6 @@ struct bt_audio_codec_cap {
 	 * vendor specific ID.
 	 */
 	uint8_t path_id;
-	/** Whether or not the local controller should transcode
-	 *
-	 * This effectively sets the coding format for the ISO data path to @ref
-	 * BT_HCI_CODING_FORMAT_TRANSPARENT if false, else uses the @ref bt_audio_codec_cfg.id.
-	 */
-	bool ctlr_transcode;
 	/** Codec ID */
 	uint8_t id;
 	/** Codec Company ID */
@@ -352,12 +344,6 @@ struct bt_audio_codec_cfg {
 	 * vendor specific ID.
 	 */
 	uint8_t path_id;
-	/** Whether or not the local controller should transcode
-	 *
-	 * This effectively sets the coding format for the ISO data path to @ref
-	 * BT_HCI_CODING_FORMAT_TRANSPARENT if false, else uses the @ref bt_audio_codec_cfg.id.
-	 */
-	bool ctlr_transcode;
 	/** Codec ID */
 	uint8_t  id;
 	/** Codec Company ID */
