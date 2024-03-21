a5f255d6040a5672f2f766b383074645ec2e848b
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2024-03-21 15:12:13 +0100

    tests/bsim sh scripts: Reduce boilerplate
    Remove the boilerplate which is not necessary beacuse
    it is already provided by the common scripts
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- tests/bsim/bluetooth/host/privacy/peripheral/test_scripts/_compile.sh
+++ tests/bsim/bluetooth/host/privacy/peripheral/test_scripts/_compile.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 # Copyright 2023 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
-
 set -eu
-
 : "${ZEPHYR_BASE:?ZEPHYR_BASE must be defined}"
 
-WORK_DIR="${WORK_DIR:-${ZEPHYR_BASE}/bsim_out}"
 INCR_BUILD=1
 
 source ${ZEPHYR_BASE}/tests/bsim/compile.source
