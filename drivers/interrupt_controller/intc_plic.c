aa225b7e526e690d6cc154da9268e102a42b7926
Robert Lubos <robert.lubos@nordicsemi.no>
2024-03-21 11:27:36 +0100

    Revert "[nrf fromtree] arch: riscv: introduce RISCV_PRIVILEGED"
    This reverts commit 61f8380f5404337122b30dbb0ea0172c0222f4e9.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- drivers/interrupt_controller/intc_plic.c
+++ drivers/interrupt_controller/intc_plic.c
@@ -135,7 +135,7 @@ static int riscv_plic_is_edge_irq(const struct device *dev, uint32_t local_irq)
  * @brief Enable a riscv PLIC-specific interrupt line
  *
  * This routine enables a RISCV PLIC-specific interrupt line.
- * riscv_plic_irq_enable is called by RISCV_PRIVILEGED
+ * riscv_plic_irq_enable is called by SOC_FAMILY_RISCV_PRIVILEGED
  * arch_irq_enable function to enable external interrupts for
  * IRQS level == 2, whenever CONFIG_RISCV_HAS_PLIC variable is set.
  *
@@ -161,7 +161,7 @@ void riscv_plic_irq_enable(uint32_t irq)
  * @brief Disable a riscv PLIC-specific interrupt line
  *
  * This routine disables a RISCV PLIC-specific interrupt line.
- * riscv_plic_irq_disable is called by RISCV_PRIVILEGED
+ * riscv_plic_irq_disable is called by SOC_FAMILY_RISCV_PRIVILEGED
  * arch_irq_disable function to disable external interrupts, for
  * IRQS level == 2, whenever CONFIG_RISCV_HAS_PLIC variable is set.
  *
