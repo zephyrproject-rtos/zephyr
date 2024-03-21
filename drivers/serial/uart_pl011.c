a8e53f54ddee4085682158032e54bbd0b1cecf34
Richard Wheatley <richard.wheatley@ambiq.com>
2024-03-21 17:26:09 -0500

    drivers: serial: UART PL011 Ambiq Update
    Add PM support for Ambiq UART
    Address apollo4x PM Mode in UART
    
    Signed-off-by: Richard Wheatley <richard.wheatley@ambiq.com>

--- drivers/serial/uart_pl011.c
+++ drivers/serial/uart_pl011.c
@@ -378,13 +378,13 @@ static int pl011_init(const struct device *dev)
 
 #define PL011_GET_COMPAT_CLK_QUIRK_0(n)					\
 	COND_CODE_1(DT_NODE_HAS_COMPAT(DT_DRV_INST(n), ambiq_uart),	\
-		    (clk_enable_ambiq_uart),				\
-		    PL011_GET_COMPAT_QUIRK_NONE(n))
+			(clk_enable_ambiq_uart),				\
+			PL011_GET_COMPAT_QUIRK_NONE(n))
 
 #define PL011_GET_COMPAT_PWR_QUIRK_0(n)					\
 	COND_CODE_1(DT_NODE_HAS_COMPAT(DT_DRV_INST(n), ambiq_uart),	\
-		    (pwr_on_ambiq_uart_##n),				\
-		    PL011_GET_COMPAT_QUIRK_NONE(n))
+			(pwr_on_ambiq_uart_##n),				\
+			PL011_GET_COMPAT_QUIRK_NONE(n))
 
 #ifdef CONFIG_UART_INTERRUPT_DRIVEN
 void pl011_isr(const struct device *dev)
@@ -441,9 +441,8 @@ void pl011_isr(const struct device *dev)
 	static struct pl011_data pl011_data_port_##n = {	\
 		.baud_rate = DT_INST_PROP(n, current_speed),	\
 	};							\
-								\
 	DEVICE_DT_INST_DEFINE(n, &pl011_init,			\
-			NULL,					\
+			PM_DEVICE_DT_INST_GET(n),					\
 			&pl011_data_port_##n,			\
 			&pl011_cfg_port_##n,			\
 			PRE_KERNEL_1,				\
