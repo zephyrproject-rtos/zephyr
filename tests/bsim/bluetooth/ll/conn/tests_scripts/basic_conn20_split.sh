ccacb580f425a4d8509ef40d4f1af99e7140899c
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2024-03-21 14:29:55 +0100

    tests/bsim/bluetooth/ll/conn: Use common scripts
    Use the common functions instead of
    replicating functionality and do a minor cleanup
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- tests/bsim/bluetooth/ll/conn/tests_scripts/basic_conn20_split.sh
+++ tests/bsim/bluetooth/ll/conn/tests_scripts/basic_conn20_split.sh
@@ -1,26 +1,13 @@
 #!/usr/bin/env bash
 # Copyright 2018 Oticon A/S
 # SPDX-License-Identifier: Apache-2.0
+source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
 
 # Basic connection test: a central connects to a peripheral
 # using the split controller (ULL LLL) - central disconnects and reconnects 20 times
 simulation_id="basic_conn20_split"
 verbosity_level=2
-process_ids=""; exit_code=0
-
-function Execute(){
-  if [ ! -f $1 ]; then
-    echo -e "  \e[91m`pwd`/`basename $1` cannot be found (did you forget to\
- compile it?)\e[39m"
-    exit 1
-  fi
-  timeout 55 $@ & process_ids="$process_ids $!"
-}
-
-: "${BSIM_OUT_PATH:?BSIM_OUT_PATH must be defined}"
-
-#Give a default value to BOARD if it does not have one yet:
-BOARD="${BOARD:-nrf52_bsim}"
+EXECUTE_TIMEOUT=60
 
 cd ${BSIM_OUT_PATH}/bin
 
@@ -35,7 +22,4 @@ Execute ./bs_${BOARD}_tests_bsim_bluetooth_ll_conn_prj_split_conf\
 Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
   -D=2 -sim_length=100e6 $@
 
-for process_id in $process_ids; do
-  wait $process_id || let "exit_code=$?"
-done
-exit $exit_code #the last exit code != 0
+wait_for_background_jobs
