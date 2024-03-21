ace45501b26f60339144c62041788b9a7622725b
Robert Lubos <robert.lubos@nordicsemi.no>
2024-03-21 11:26:27 +0100

    Revert "[nrf fromtree] settings: NVS: add entry into cache after writing"
    This reverts commit 6ad928353c157b954ce3f8642846cba685c2a509.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/settings/src/settings_nvs.c
+++ subsys/settings/src/settings_nvs.c
@@ -216,13 +216,10 @@ static int settings_nvs_save(struct settings_store *cs, const char *name,
 	delete = ((value == NULL) || (val_len == 0));
 
 #if CONFIG_SETTINGS_NVS_NAME_CACHE
-	bool name_in_cache = false;
-
 	name_id = settings_nvs_cache_match(cf, name, rdname, sizeof(rdname));
 	if (name_id != NVS_NAMECNT_ID) {
 		write_name_id = name_id;
 		write_name = false;
-		name_in_cache = true;
 		goto found;
 	}
 #endif
@@ -254,6 +251,9 @@ static int settings_nvs_save(struct settings_store *cs, const char *name,
 		}
 
 		if (!delete) {
+#if CONFIG_SETTINGS_NVS_NAME_CACHE
+			settings_nvs_cache_add(cf, name, name_id);
+#endif
 			write_name_id = name_id;
 			write_name = false;
 		}
@@ -321,12 +321,6 @@ found:
 			return rc;
 		}
 	}
-
-#if CONFIG_SETTINGS_NVS_NAME_CACHE
-	if (!name_in_cache) {
-		settings_nvs_cache_add(cf, name, write_name_id);
-	}
-#endif
 
 	return 0;
 }
