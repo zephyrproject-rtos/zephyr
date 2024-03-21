0e27d1c6d90e44dbbe36a8392a4dffb0d8228169
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2024-03-21 14:07:44 +0100

    tests/bsim bt host/iso/*: Test scripts cleanup
    Use the common scripts to perform common functions
    and do a cleanup.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- tests/bsim/bluetooth/host/iso/cis/tests_scripts/_compile.sh
+++ tests/bsim/bluetooth/host/iso/cis/tests_scripts/_compile.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 # Copyright 2023 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
-
 set -eu
-bash_source_dir="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 
-source "${bash_source_dir}/_env.sh"
-
-: "${BSIM_OUT_PATH:?BSIM_OUT_PATH must be defined}"
-: "${BSIM_COMPONENTS_PATH:?BSIM_COMPONENTS_PATH must be defined}"
 : "${ZEPHYR_BASE:?ZEPHYR_BASE must be defined}"
 
 WORK_DIR="${WORK_DIR:-${ZEPHYR_BASE}/bsim_out}"
-BOARD="${BOARD:-nrf52_bsim}"
-BOARD_ROOT="${BOARD_ROOT:-${ZEPHYR_BASE}}"
 INCR_BUILD=1
-mkdir -p ${WORK_DIR}
 
-west build -b nrf52_bsim
-cp build/zephyr/zephyr.exe $central_exe
+source ${ZEPHYR_BASE}/tests/bsim/compile.source
+
+app="$(guess_test_relpath)" compile
+
+wait_for_background_jobs
