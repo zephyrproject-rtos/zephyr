2cd5c845edd3dfd322647cceda3b8460b9db20c4
Robert Lubos <robert.lubos@nordicsemi.no>
2024-03-21 11:26:24 +0100

    Revert "[nrf fromlist] samples: drivers: mbox: misc cleanups/enhancements"
    This reverts commit fdd4cbee5e08c04c725340774831a4d14d03841d.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- samples/drivers/mbox/src/main.c
+++ samples/drivers/mbox/src/main.c
@@ -4,9 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/mbox.h>
-#include <zephyr/sys/printk.h>
 
 #ifdef CONFIG_RX_ENABLED
 static void callback(const struct device *dev, uint32_t channel,
@@ -18,25 +19,24 @@ static void callback(const struct device *dev, uint32_t channel,
 
 int main(void)
 {
-	int ret;
-	const struct device *const dev = DEVICE_DT_GET(DT_NODELABEL(mbox));
+	const struct device *dev;
 
 	printk("Hello from APP\n");
+
+	dev = DEVICE_DT_GET(DT_NODELABEL(mbox));
 
 #ifdef CONFIG_RX_ENABLED
 	struct mbox_channel rx_channel;
 
 	mbox_init_channel(&rx_channel, dev, CONFIG_RX_CHANNEL_ID);
 
-	ret = mbox_register_callback(&rx_channel, callback, NULL);
-	if (ret < 0) {
-		printk("Could not register callback (%d)\n", ret);
+	if (mbox_register_callback(&rx_channel, callback, NULL)) {
+		printk("mbox_register_callback() error\n");
 		return 0;
 	}
 
-	ret = mbox_set_enabled(&rx_channel, true);
-	if (ret < 0) {
-		printk("Could not enable RX channel %d (%d)\n", rx_channel.id, ret);
+	if (mbox_set_enabled(&rx_channel, 1)) {
+		printk("mbox_set_enable() error\n");
 		return 0;
 	}
 #endif /* CONFIG_RX_ENABLED */
@@ -51,9 +51,8 @@ int main(void)
 
 		printk("Ping (on channel %d)\n", tx_channel.id);
 
-		ret = mbox_send(&tx_channel, NULL);
-		if (ret < 0) {
-			printk("Could not send (%d)\n", ret);
+		if (mbox_send(&tx_channel, NULL) < 0) {
+			printk("mbox_send() error\n");
 			return 0;
 		}
 	}
