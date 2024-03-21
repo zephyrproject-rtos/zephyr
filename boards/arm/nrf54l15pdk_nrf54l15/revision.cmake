5cd9582cdcf3adc85ad7dce76e1665f19ff3f0bc
Robert Lubos <robert.lubos@nordicsemi.no>
2024-03-21 11:27:39 +0100

    Revert "[nrf fromtree] boards: arm: add Nordic nrf54l15dk_nrf54l15"
    This reverts commit 9dc7ca71bea83594f529f7e9f28aed035052e35f.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- boards/arm/nrf54l15pdk_nrf54l15/revision.cmake
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (c) 2024 Nordic Semiconductor ASA
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-board_check_revision(FORMAT MAJOR.MINOR.PATCH
-                     VALID_REVISIONS 0.2.0
-                     DEFAULT_REVISION 0.2.0)
