d34351d9945435fd29ff5b2ecdf4805cf784f779
Daniel Leung <daniel.leung@intel.com>
2024-03-22 14:11:49 -0700

    kernel: align thread stack size declaration
    When thread stack is defined as an array, K_THREAD_STACK_LEN()
    is used to calculate the size for each stack in the array.
    However, standalone thread stack has its size calculated by
    Z_THREAD_STACK_SIZE_ADJUST() instead. Depending on the arch
    alignment requirement, they may not be the same... which
    could cause some confusions. So align them both to use
    K_THREAD_STACK_LEN().
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- tests/kernel/fatal/exception/src/main.c
+++ tests/kernel/fatal/exception/src/main.c
@@ -465,7 +465,7 @@ static void *fatal_setup(void)
 
 	obj_size = K_THREAD_STACK_SIZEOF(overflow_stack);
 #if defined(CONFIG_USERSPACE)
-	obj_size = Z_THREAD_STACK_SIZE_ADJUST(obj_size);
+	obj_size = K_THREAD_STACK_LEN(obj_size);
 #endif
 
 	k_mem_region_align(&pin_addr, &pin_size,
@@ -477,7 +477,7 @@ static void *fatal_setup(void)
 
 	obj_size = K_THREAD_STACK_SIZEOF(alt_stack);
 #if defined(CONFIG_USERSPACE)
-	obj_size = Z_THREAD_STACK_SIZE_ADJUST(obj_size);
+	obj_size = K_THREAD_STACK_LEN(obj_size);
 #endif
 
 	k_mem_region_align(&pin_addr, &pin_size,
