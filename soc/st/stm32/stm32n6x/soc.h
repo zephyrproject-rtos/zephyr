7e1459882276757e7993e24c6dd33957bb5050fc
Guillaume Gautier <guillaume.gautier-ext@st.com>
2024-03-22 11:15:40 +0100

    soc: st: stm32: add stm32n6 series
    Add STM32N6 series
    
    Signed-off-by: Guillaume Gautier <guillaume.gautier-ext@st.com>

---
+++ soc/st/stm32/stm32n6x/soc.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file SoC configuration macros for the STM32N6 family processors.
+ *
+ */
+
+
+#ifndef _STM32N6_SOC_H_
+#define _STM32N6_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <stm32n6xx.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _STM32N6_SOC_H_ */
