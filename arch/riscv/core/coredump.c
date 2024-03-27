2f5938ec76284914393ca7b4bf27264d10440544
Aleksandar Cecaric <aleksandar.cecaric@nextsilicon.com>
2024-03-27 11:19:32 +0100

    arch: riscv: update coredump for 64BIT RISCV
    Add RISCV 64bit registers and parse them in coredump script.
    
    Signed-off-by: Aleksandar Cecaric <aleksandar.cecaric@nextsilicon.com>

--- arch/riscv/core/coredump.c
+++ arch/riscv/core/coredump.c
@@ -7,9 +7,35 @@
 #include <string.h>
 #include <zephyr/debug/coredump.h>
 
+#ifndef CONFIG_64BIT
 #define ARCH_HDR_VER 1
+#else
+#define ARCH_HDR_VER 2
+#endif
 
 struct riscv_arch_block {
+#ifdef CONFIG_64BIT
+	struct {
+		uint64_t ra;
+		uint64_t tp;
+		uint64_t t0;
+		uint64_t t1;
+		uint64_t t2;
+		uint64_t a0;
+		uint64_t a1;
+		uint64_t a2;
+		uint64_t a3;
+		uint64_t a4;
+		uint64_t a5;
+		uint64_t a6;
+		uint64_t a7;
+		uint64_t t3;
+		uint64_t t4;
+		uint64_t t5;
+		uint64_t t6;
+		uint64_t pc;
+	} r;
+#else /* !CONFIG_64BIT */
 	struct {
 		uint32_t ra;
 		uint32_t tp;
@@ -32,6 +58,7 @@ struct riscv_arch_block {
 #endif /* !CONFIG_RISCV_ISA_RV32E */
 		uint32_t pc;
 	} r;
+#endif /* CONFIG_64BIT */
 } __packed;
 
 /*
