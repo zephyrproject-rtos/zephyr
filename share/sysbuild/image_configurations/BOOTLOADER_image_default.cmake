0b9c1b03e02a1ded44601cad76eca8e40e189053
Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>
2024-03-28 13:11:44 +0100

    sysbuild: add VPR launcher bootloader
    Extend sysbuild bootloader mechanism with a new bootloader:
    VPR launcher, which takes minimal sample and adds `nordic-ppr{-xip}`
    snippet.
    This way, PPR can be easily used with examples (and tests) that do not
    require any action on APP side.
    
    Signed-off-by: Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>

--- share/sysbuild/image_configurations/BOOTLOADER_image_default.cmake
+++ share/sysbuild/image_configurations/BOOTLOADER_image_default.cmake
@@ -5,25 +5,27 @@
 # This sysbuild CMake file sets the sysbuild controlled settings as properties
 # on Zephyr MCUboot / bootloader image.
 
-set(keytypes CONFIG_BOOT_SIGNATURE_TYPE_NONE
-             CONFIG_BOOT_SIGNATURE_TYPE_RSA
-             CONFIG_BOOT_SIGNATURE_TYPE_ECDSA_P256
-             CONFIG_BOOT_SIGNATURE_TYPE_ED25519)
+if(SB_CONFIG_BOOTLOADER_MCUBOOT)
+  set(keytypes CONFIG_BOOT_SIGNATURE_TYPE_NONE
+               CONFIG_BOOT_SIGNATURE_TYPE_RSA
+               CONFIG_BOOT_SIGNATURE_TYPE_ECDSA_P256
+               CONFIG_BOOT_SIGNATURE_TYPE_ED25519)
 
-if(SB_CONFIG_BOOT_SIGNATURE_TYPE_NONE)
-  set(keytype CONFIG_BOOT_SIGNATURE_TYPE_NONE)
-elseif(SB_CONFIG_BOOT_SIGNATURE_TYPE_RSA)
-  set(keytype CONFIG_BOOT_SIGNATURE_TYPE_RSA)
-elseif(SB_CONFIG_BOOT_SIGNATURE_TYPE_ECDSA_P256)
-  set(keytype CONFIG_BOOT_SIGNATURE_TYPE_ECDSA_P256)
-elseif(SB_CONFIG_BOOT_SIGNATURE_TYPE_ED25519)
-  set(keytype CONFIG_BOOT_SIGNATURE_TYPE_ED25519)
-endif()
-
-foreach(loopkeytype ${keytypes})
-  if("${loopkeytype}" STREQUAL "${keytype}")
-    set_config_bool(${ZCMAKE_APPLICATION} ${loopkeytype} y)
-  else()
-    set_config_bool(${ZCMAKE_APPLICATION} ${loopkeytype} n)
+  if(SB_CONFIG_BOOT_SIGNATURE_TYPE_NONE)
+    set(keytype CONFIG_BOOT_SIGNATURE_TYPE_NONE)
+  elseif(SB_CONFIG_BOOT_SIGNATURE_TYPE_RSA)
+    set(keytype CONFIG_BOOT_SIGNATURE_TYPE_RSA)
+  elseif(SB_CONFIG_BOOT_SIGNATURE_TYPE_ECDSA_P256)
+    set(keytype CONFIG_BOOT_SIGNATURE_TYPE_ECDSA_P256)
+  elseif(SB_CONFIG_BOOT_SIGNATURE_TYPE_ED25519)
+    set(keytype CONFIG_BOOT_SIGNATURE_TYPE_ED25519)
   endif()
-endforeach()
+
+  foreach(loopkeytype ${keytypes})
+    if("${loopkeytype}" STREQUAL "${keytype}")
+      set_config_bool(${ZCMAKE_APPLICATION} ${loopkeytype} y)
+    else()
+      set_config_bool(${ZCMAKE_APPLICATION} ${loopkeytype} n)
+    endif()
+  endforeach()
+endif()
