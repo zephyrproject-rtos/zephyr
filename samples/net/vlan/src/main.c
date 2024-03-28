e2fcdab8fb36513391a6ddf573f8140f701565bb
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2024-03-28 13:52:29 +0200

    samples: net: vlan: Support all three network interfaces
    The VLAN sample application only had configuration support for
    the Ethernet interface and one VLAN interface, and left the one
    VLAN interface unconfigured. Fix this by adding config support for
    all three network interfaces created by the sample.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- samples/net/vlan/src/main.c
+++ samples/net/vlan/src/main.c
@@ -36,12 +36,69 @@ static void iface_cb(struct net_if *iface, void *user_data)
 	ud->second = iface;
 }
 
-static int init_app(void)
+static int setup_iface(struct net_if *iface, struct net_if *vlan,
+		       const char *ipv6_addr, const char *ipv4_addr,
+		       const char *netmask, uint16_t vlan_tag)
 {
-	struct net_if *iface;
 	struct net_if_addr *ifaddr;
 	struct in_addr addr4;
 	struct in6_addr addr6;
+	int ret;
+
+	ret = net_eth_vlan_enable(iface, vlan_tag);
+	if (ret < 0) {
+		LOG_ERR("Cannot enable VLAN for tag %d (%d)", vlan_tag, ret);
+	}
+
+	if (IS_ENABLED(CONFIG_NET_IPV6)) {
+		if (net_addr_pton(AF_INET6, ipv6_addr, &addr6)) {
+			LOG_ERR("Invalid address: %s", ipv6_addr);
+			return -EINVAL;
+		}
+
+		ifaddr = net_if_ipv6_addr_add(vlan, &addr6,
+					      NET_ADDR_MANUAL, 0);
+		if (!ifaddr) {
+			LOG_ERR("Cannot add %s to interface %p",
+				ipv6_addr, vlan);
+			return -EINVAL;
+		}
+	}
+
+	if (IS_ENABLED(CONFIG_NET_IPV4)) {
+		if (net_addr_pton(AF_INET, ipv4_addr, &addr4)) {
+			LOG_ERR("Invalid address: %s", ipv4_addr);
+			return -EINVAL;
+		}
+
+		ifaddr = net_if_ipv4_addr_add(vlan, &addr4,
+					      NET_ADDR_MANUAL, 0);
+		if (!ifaddr) {
+			LOG_ERR("Cannot add %s to interface %p",
+				ipv4_addr, vlan);
+			return -EINVAL;
+		}
+
+		if (netmask && netmask[0]) {
+			struct in_addr nm;
+
+			if (net_addr_pton(AF_INET, netmask, &nm)) {
+				LOG_ERR("Invalid netmask: %s", ipv4_addr);
+				return -EINVAL;
+			}
+
+			net_if_ipv4_set_netmask_by_addr(vlan, &addr4, &nm);
+		}
+	}
+
+	LOG_DBG("Interface %p VLAN tag %d setup done.", vlan, vlan_tag);
+
+	return 0;
+}
+
+static int init_app(void)
+{
+	struct net_if *iface;
 	struct ud ud;
 	int ret;
 
@@ -51,43 +108,31 @@ static int init_app(void)
 		return -ENOENT;
 	}
 
+	memset(&ud, 0, sizeof(ud));
+
 	net_if_foreach(iface_cb, &ud);
 
-	ret = net_eth_vlan_enable(iface, CONFIG_SAMPLE_VLAN_TAG);
+	ret = setup_iface(iface, ud.first,
+			  CONFIG_NET_SAMPLE_IFACE2_MY_IPV6_ADDR,
+			  CONFIG_NET_SAMPLE_IFACE2_MY_IPV4_ADDR,
+			  CONFIG_NET_SAMPLE_IFACE2_MY_IPV4_NETMASK,
+			  CONFIG_NET_SAMPLE_IFACE2_VLAN_TAG);
 	if (ret < 0) {
-		LOG_ERR("Cannot enable VLAN for tag %d (%d)",
-			CONFIG_SAMPLE_VLAN_TAG, ret);
+		return ret;
 	}
 
-	ret = net_eth_vlan_enable(iface, CONFIG_SAMPLE_VLAN_TAG_2);
+	ret = setup_iface(iface, ud.second,
+			  CONFIG_NET_SAMPLE_IFACE3_MY_IPV6_ADDR,
+			  CONFIG_NET_SAMPLE_IFACE3_MY_IPV4_ADDR,
+			  CONFIG_NET_SAMPLE_IFACE3_MY_IPV4_NETMASK,
+			  CONFIG_NET_SAMPLE_IFACE3_VLAN_TAG);
 	if (ret < 0) {
-		LOG_ERR("Cannot enable VLAN for tag %d (%d)",
-			CONFIG_SAMPLE_VLAN_TAG_2, ret);
+		return ret;
 	}
 
-	if (net_addr_pton(AF_INET6, CONFIG_SAMPLE_IPV6_ADDR_2, &addr6)) {
-		LOG_ERR("Invalid address: %s", CONFIG_SAMPLE_IPV6_ADDR_2);
-		return -EINVAL;
-	}
-
-	ifaddr = net_if_ipv6_addr_add(ud.second, &addr6, NET_ADDR_MANUAL, 0);
-	if (!ifaddr) {
-		LOG_ERR("Cannot add %s to interface %p",
-			CONFIG_SAMPLE_IPV6_ADDR_2, ud.second);
-		return -EINVAL;
-	}
-
-	if (net_addr_pton(AF_INET, CONFIG_SAMPLE_IPV4_ADDR_2, &addr4)) {
-		LOG_ERR("Invalid address: %s", CONFIG_SAMPLE_IPV4_ADDR_2);
-		return -EINVAL;
-	}
-
-	ifaddr = net_if_ipv4_addr_add(ud.second, &addr4, NET_ADDR_MANUAL, 0);
-	if (!ifaddr) {
-		LOG_ERR("Cannot add %s to interface %p",
-			CONFIG_SAMPLE_IPV4_ADDR_2, ud.second);
-		return -EINVAL;
-	}
+	/* Bring up the VLAN interface automatically */
+	net_if_up(ud.first);
+	net_if_up(ud.second);
 
 	return ret;
 }
