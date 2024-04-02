4ad6a2f9e8d3c6eabac2462631eaa7262f59472c
Daniel Baluta <daniel.baluta@nxp.com>
2024-04-02 17:28:59 +0300

    soc: imxrt6xx: Introduce initial SoC support for HIFI4 DSP
    Add SoC definitions and a linker file for the HIFI4 DSP
    found on the NXP i.MXRT685 microcontroller.
    
    Signed-off-by: Vit Stanicek <vit.stanicek@nxp.com>
    Signed-off-by: Daniel Baluta <daniel.baluta@nxp.com>

---
+++ soc/nxp/imxrt/imxrt6xx/adsp/include/soc.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2024 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_IMX_SOC_H
+#define __INC_IMX_SOC_H
+
+#include <string.h>
+#include <errno.h>
+
+#include <zephyr/sys/sys_io.h>
+
+#include <adsp/cache.h>
+#include <pinctrl_soc.h>
+
+#endif /* __INC_IMX_SOC_H */
