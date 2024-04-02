dcd3b86ebccfbb6bf74ef7ef19cf1146e7788f68
Dmitrii Golovanov <dmitrii.golovanov@intel.com>
2024-04-02 11:35:15 +0200

    twister: blackbox: pylint no-name-in-module warnings
    Ignore no-name-in-module pylint warnings.
    
    Signed-off-by: Dmitrii Golovanov <dmitrii.golovanov@intel.com>

--- scripts/tests/twister_blackbox/test_output.py
+++ scripts/tests/twister_blackbox/test_output.py
@@ -14,6 +14,7 @@ import pytest
 import sys
 import json
 
+# pylint: disable=no-name-in-module
 from conftest import ZEPHYR_BASE, TEST_DATA, testsuite_filename_mock, clear_log_in_test
 from twisterlib.testplan import TestPlan
 
