7fc15542281c98d2206fed3b7cb50496f2fd8232
Frederic Pillon <frederic.pillon@st.com>
2024-04-04 11:30:08 +0200

    boards: st: add steval_stwinbx1 support
    Add board support for steval_stwinbx1
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

---
+++ boards/st/steval_stwinbx1/board.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2024 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/init.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/drivers/uart.h>
+
+#if defined(CONFIG_CONSOLE) && defined(CONFIG_UART_CONSOLE)
+#if DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart)
+/*
+ * Enable console on USB CDC_ACM
+ */
+static int steval_stwinbx1_usb_console_init(void)
+{
+	const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+
+	if (!device_is_ready(dev)) {
+		return -ENODEV;
+	}
+	return (usb_enable(NULL));
+}
+
+/* needs to be done at Application */
+SYS_INIT(steval_stwinbx1_usb_console_init, APPLICATION,
+	 CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
+#endif /* DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart) */
+#endif /* defined(CONFIG_CONSOLE) && defined (CONFIG_UART_CONSOLE) */
