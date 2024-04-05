ca4e5d9c9872a7ff27737fb4f2125507525e75c3
Gerard Marull-Paretas <gerard@teslabs.com>
2024-04-05 12:12:50 +0200

    cmake: use INTERFACE_SOURCES in zephyr_syscall_header
    Instead of INTERFACE_INCLUDE_DIRECTORIES, because header files are used,
    not directories. Headers were added to both, target sources and include
    directories.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- cmake/modules/extensions.cmake
+++ cmake/modules/extensions.cmake
@@ -1634,10 +1634,6 @@ function(zephyr_syscall_header)
       syscalls_interface INTERFACE
       ${header_file}
     )
-    target_include_directories(
-      syscalls_interface INTERFACE
-      ${header_file}
-    )
     add_dependencies(
       syscalls_interface
       ${header_file}
