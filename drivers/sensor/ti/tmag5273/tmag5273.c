f5d0be068ccf002e43178ac97916f67c59c9fb79
Oliver Barta <oliver.barta@grandcentrix.net>
2024-04-09 08:43:44 +0200

    drivers: sensor: tmag5273: initialize to suspended state
    This avoids waiting for an initial conversion to complete in continous
    mode. The result will be lost anyway when sleep mode is entered. In either
    case it saves one I2C transaction during initialization when runtime PM
    is enabled.
    
    Signed-off-by: Oliver Barta <oliver.barta@grandcentrix.net>

--- drivers/sensor/ti/tmag5273/tmag5273.c
+++ drivers/sensor/ti/tmag5273/tmag5273.c
@@ -1029,8 +1029,13 @@ static inline int tmag5273_init_device_config(const struct device *dev)
 
 	drv_data->dev_conf_2 = regdata;
 
-	if (drv_cfg->operation_mode == TMAG5273_DT_OPER_MODE_CONTINUOUS) {
-		regdata |= TMAG5273_OPERATING_MODE_CONTINUOUS;
+	if (IS_ENABLED(CONFIG_PM_DEVICE_RUNTIME)) {
+		regdata |= TMAG5273_OPERATING_MODE_SLEEP;
+		pm_device_init_suspended(dev);
+	} else {
+		if (drv_cfg->operation_mode == TMAG5273_DT_OPER_MODE_CONTINUOUS) {
+			regdata |= TMAG5273_OPERATING_MODE_CONTINUOUS;
+		}
 	}
 
 	/* Note: I2C glitch filter enabled by default */
@@ -1041,8 +1046,12 @@ static inline int tmag5273_init_device_config(const struct device *dev)
 		return -EIO;
 	}
 
-	if (drv_cfg->operation_mode == TMAG5273_DT_OPER_MODE_CONTINUOUS) {
-		k_usleep(TMAG5273_T_START_MEASURE_US + drv_data->conversion_time_us);
+	if (IS_ENABLED(CONFIG_PM_DEVICE_RUNTIME)) {
+		k_usleep(TMAG5273_T_GO_SLEEP_US);
+	} else {
+		if (drv_cfg->operation_mode == TMAG5273_DT_OPER_MODE_CONTINUOUS) {
+			k_usleep(TMAG5273_T_START_MEASURE_US + drv_data->conversion_time_us);
+		}
 	}
 
 	return 0;
