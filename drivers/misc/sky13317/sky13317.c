94aebc707edf51a69dfb919e1ca1a00e243ee07f
Ayush Singh <ayushdevel1325@gmail.com>
2024-04-11 21:59:50 +0530

    drivers: misc: sky13317: Add sky13317 antenna
    - sky13317 is used in cc1352p1_launchxl, beagleconnect_freedom and
      beagleplay_cc1352.
    - Remove unused files
    - Update maintainers
    
    Signed-off-by: Ayush Singh <ayushdevel1325@gmail.com>

--- drivers/misc/sky13317/sky13317.c
+++ drivers/misc/sky13317/sky13317.c
@@ -49,7 +49,7 @@ const RFCC26XX_HWAttrsV2 RFCC26XX_hwAttrs = {
 
 PINCTRL_DT_INST_DEFINE(0);
 DEVICE_DT_INST_DEFINE(0, board_antenna_init, NULL, NULL, NULL,
-					  POST_KERNEL, CONFIG_BOARD_ANTENNA_INIT_PRIO, NULL);
+					  POST_KERNEL, CONFIG_SKY13317_INIT_PRIO, NULL);
 
 static const struct pinctrl_dev_config *ant_pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(0);
 static const struct gpio_dt_spec ant_gpios[] = {
