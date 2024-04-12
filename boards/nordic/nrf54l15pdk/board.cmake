666866e9cdb0dafb9e6d10e7bc19e568510b8a81
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2024-04-12 14:13:12 +0200

    [nrf noup] boards: add nrf54l15/nrf54l15/cpuapp/ns as board variant
    This commit adds the nRF54l15//nrf54l15/cpuapp/ns board variant to
    sdk-zephyr. It allows to build nRF54l15pdk with TF-M in nRF Connect SDK.
    
    This is done as a nrf noup patch as the out-of-tree board extension
    feature is not available. When zephyrproject-rtos/zephyr#69548 is
    completed, then this commit can be reverted and the variant be placed in
    sdk-nrf repository.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
    (cherry picked from commit c8cc6634d2bd1c03e150342b7b0568c0eb191c92)

--- boards/nordic/nrf54l15pdk/board.cmake
+++ boards/nordic/nrf54l15pdk/board.cmake
@@ -7,6 +7,14 @@ elseif (CONFIG_SOC_NRF54L15_ENGA_CPUFLPR)
   board_runner_args(jlink "--speed=4000")
 endif()
 
+if(BOARD_NRF54L15PDK_NRF54L15_CPUAPP_NS)
+  set(TFM_PUBLIC_KEY_FORMAT "full")
+endif()
+
+if(CONFIG_TFM_FLASH_MERGED_BINARY)
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
+endif()
+
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
