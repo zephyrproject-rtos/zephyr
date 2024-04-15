1758a3204dd9627f78974cd0815a6df2c9519974
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-04-15 09:23:00 +0100

    [nrf fromtree] cmake: modules: boards: Add normalised variables
    Adds a new variable NORMALIZED_BOARD_QUALIFIERS which contains
    the board qualifiers in file-name format, this allows for
    constructing strings in applications (e.g. for folder names) prior
    to Zephyr being found for things like APPLICATION_CONFIG_DIR. Also
    adds NORMALIZED_BOARD_TARGET.
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>
    (cherry picked from commit a2ed8162c8c42a4301264709e9e476ef9eaa0552)

--- cmake/modules/boards.cmake
+++ cmake/modules/boards.cmake
@@ -21,13 +21,18 @@
 # Outcome:
 # The following variables will be defined when this CMake module completes:
 #
-# - BOARD:                Board, without revision field.
-# - BOARD_REVISION:       Board revision
-# - BOARD_DIR:            Board directory with the implementation for selected board
-# - ARCH_DIR:             Arch dir for extracted from selected board
-# - BOARD_ROOT:           BOARD_ROOT with ZEPHYR_BASE appended
-# - BOARD_EXTENSION_DIRS: List of board extension directories (If
-#                         BOARD_EXTENSIONS is not explicitly disabled)
+# - BOARD:                       Board, without revision field.
+# - BOARD_REVISION:              Board revision
+# - BOARD_QUALIFIERS:            Board qualifiers
+# - NORMALIZED_BOARD_QUALIFIERS: Board qualifiers in lower-case format where slashes have been
+#                                replaced with underscores
+# - NORMALIZED_BOARD_TARGET:     Board target in lower-case format where slashes have been
+#                                replaced with underscores
+# - BOARD_DIR:                   Board directory with the implementation for selected board
+# - ARCH_DIR:                    Arch dir for extracted from selected board
+# - BOARD_ROOT:                  BOARD_ROOT with ZEPHYR_BASE appended
+# - BOARD_EXTENSION_DIRS:        List of board extension directories (If
+#                                BOARD_EXTENSIONS is not explicitly disabled)
 #
 # The following targets will be defined when this CMake module completes:
 # - board: when invoked, a list of valid boards will be printed
@@ -336,7 +341,12 @@ endif()
 if(DEFINED BOARD_QUALIFIERS)
   string(REGEX REPLACE "^/" "qualifiers: " board_message_qualifiers "${BOARD_QUALIFIERS}")
   set(board_message "${board_message}, ${board_message_qualifiers}")
+
+  string(REPLACE "/" "_" NORMALIZED_BOARD_QUALIFIERS "${BOARD_QUALIFIERS}")
 endif()
+
+set(NORMALIZED_BOARD_TARGET "${BOARD}${BOARD_QUALIFIERS}")
+string(REPLACE "/" "_" NORMALIZED_BOARD_TARGET "${NORMALIZED_BOARD_TARGET}")
 
 message(STATUS "${board_message}")
 
