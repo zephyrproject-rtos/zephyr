846768530207fa699ee147464df53ba1777f125c
Nikolay Agishev <agishev@synopsys.com>
2024-04-16 20:06:12 +0300

    ***SHOULD_BE_REVERTED*** Fixup WFI for tests only!

--- arch/riscv/core/cpu_idle.c
+++ arch/riscv/core/cpu_idle.c
@@ -7,6 +7,23 @@
 #include <zephyr/irq.h>
 #include <zephyr/tracing/tracing.h>
 
+#ifdef CONFIG_SOC_SERIES_NSIM_RV_RMX500
+
+void __weak arch_cpu_idle(void)
+{
+	sys_trace_idle();
+	irq_unlock(MSTATUS_IEN);
+	__asm__ volatile("wfi");
+}
+
+void __weak arch_cpu_atomic_idle(unsigned int key)
+{
+	sys_trace_idle();
+	irq_unlock(key);
+	__asm__ volatile("wfi");
+}
+
+#else
 void __weak arch_cpu_idle(void)
 {
 	sys_trace_idle();
@@ -20,3 +37,4 @@ void __weak arch_cpu_atomic_idle(unsigned int key)
 	__asm__ volatile("wfi");
 	irq_unlock(key);
 }
+#endif /* SOC_SERIES_NSIM_RV_RMX500 */
