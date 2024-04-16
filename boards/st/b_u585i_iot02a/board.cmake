a6400b42feeb2cc44aacb14d7b29370602cb9ce2
Mario Paja <mariopaja@hotmail.com>
2024-04-16 18:03:24 +0200

    boards: st: b_u585_iot02a: add segger jlink support
    This PR adds the possibility to use the Segger JLink debugger with
    b_u858_iot02a board.
    
    Signed-off-by: Mario Paja <mariopaja@hotmail.com>

--- boards/st/b_u585i_iot02a/board.cmake
+++ boards/st/b_u585i_iot02a/board.cmake
@@ -19,7 +19,10 @@ board_runner_args(openocd "--tcl-port=6666")
 board_runner_args(openocd --cmd-pre-init "gdb_report_data_abort enable")
 board_runner_args(openocd "--no-halt")
 
+board_runner_args(jlink "--device=STM32U585AI" "--reset-after-load")
+
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 # FIXME: openocd runner requires use of STMicro openocd fork.
 # Check board documentation for more details.
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
