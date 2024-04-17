cc2c0bafd9a68bcd17170f02483cfae41a6903e1
Chen Xingyu <hi@xingrz.me>
2024-04-18 01:26:00 +0800

    boards: nucleo_f030r8: Utilize probe-rs
    This commit introduce probe-rs runner to nucleo_f030r8 board for
    demonstration.
    
    Signed-off-by: Chen Xingyu <hi@xingrz.me>

--- boards/st/nucleo_f030r8/board.cmake
+++ boards/st/nucleo_f030r8/board.cmake
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--device=STM32F030R8" "--speed=4000")
+board_runner_args(probe-rs "--chip=STM32F030R8Tx")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/probe-rs.board.cmake)
