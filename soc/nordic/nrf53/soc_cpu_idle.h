743b2439516a6e59738cea38e08589760fb188c4
Szymon Janc <szymon.janc@codecoup.pl>
2024-04-17 10:21:48 +0200

    Revert "arch: arm: cortex_m: Convert cpu_idle from ASM to C"
    This reverts commit 4760aad353a88c4778ad6bd15720bc8a9a2b4162.
    
    This is causing hard faults on (at least) bttester app running
    on nRF52840.
    
    *** Booting Zephyr OS build v3.6.0-2542-g4760aad353a8 ***
    [00:00:00.249,908] <dbg> bttester: tester_init: Initializing tester
    ASSERTION FAIL [!terminate] @ WEST_TOPDIR/zephyr/kernel/sched.c:529
    	aborted _current back from dead
    [00:00:00.250,396] <err> os: ***** HARD FAULT *****
    [00:00:00.250,427] <err> os:   Fault escalation (see below)
    [00:00:00.250,427] <err> os: ARCH_EXCEPT with reason 4
    
    [00:00:00.250,457] <err> os: r0/a1:  0x00000004  r1/a2:  0x00000211
          r2/a3:  0x00000004
    [00:00:00.250,457] <err> os: r3/a4:  0x00000000 r12/ip:  0x00000014 r14/lr:
          0x0003a089
    [00:00:00.250,488] <err> os:  xpsr:  0x61000000
    [00:00:00.250,488] <err> os: Faulting instruction address (r15/pc):
          0x0003e102
    [00:00:00.250,518] <err> os: >>> ZEPHYR FATAL ERROR 4: Kernel panic on
          CPU 0
    [00:00:00.250,579] <err> os: Current thread: 0x20003128 (main)
    [00:00:08.830,963] <err> os: Halting system
    
    Signed-off-by: Szymon Janc <szymon.janc@codecoup.pl>

--- soc/nordic/nrf53/soc_cpu_idle.h
+++ soc/nordic/nrf53/soc_cpu_idle.h
@@ -8,22 +8,19 @@
  * @file SoC extensions of cpu_idle.S for the Nordic Semiconductor nRF53 processors family.
  */
 
-#define SOC_ON_EXIT_CPU_IDLE_4 \
-	__NOP(); \
-	__NOP(); \
-	__NOP(); \
-	__NOP();
-#define SOC_ON_EXIT_CPU_IDLE_8 \
-	SOC_ON_EXIT_CPU_IDLE_4 \
-	SOC_ON_EXIT_CPU_IDLE_4
+
+#if defined(_ASMLANGUAGE)
 
 #if defined(CONFIG_SOC_NRF53_ANOMALY_168_WORKAROUND_FOR_EXECUTION_FROM_RAM)
 #define SOC_ON_EXIT_CPU_IDLE \
-	SOC_ON_EXIT_CPU_IDLE_8; \
-	SOC_ON_EXIT_CPU_IDLE_8; \
-	SOC_ON_EXIT_CPU_IDLE_8; \
-	__NOP(); \
-	__NOP();
+	.rept 26; \
+	nop; \
+	.endr
 #elif defined(CONFIG_SOC_NRF53_ANOMALY_168_WORKAROUND)
-#define SOC_ON_EXIT_CPU_IDLE SOC_ON_EXIT_CPU_IDLE_8
+#define SOC_ON_EXIT_CPU_IDLE \
+	.rept 8; \
+	nop; \
+	.endr
 #endif
+
+#endif /* _ASMLANGUAGE */
