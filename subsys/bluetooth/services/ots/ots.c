37fe52a7f241871fb68760f78d778a95cfddc638
Szymon Janc <szymon.janc@codecoup.pl>
2024-04-19 08:43:31 +0200

    bluetooth: OTS: Delay indications to system workqueue
    OTS requires that indications about state change are sent after
    response to ATT Write.
    
    OTS Specification p.4.4.4.: "An OLCP or OACP operation is started
    when the ATT Write Response is received from the Object Server as
    a result of the Object Client writing an Op Code to a control point
    to perform some desired action. The control point operation ends
    when the Object Server sends an indication to the Object Client
     with the Op Code set to Response Code."
    
    Signed-off-by: Szymon Janc <szymon.janc@codecoup.pl>

--- subsys/bluetooth/services/ots/ots.c
+++ subsys/bluetooth/services/ots/ots.c
@@ -442,6 +442,22 @@ void *bt_ots_svc_decl_get(struct bt_ots *ots)
 }
 #endif
 
+static void oacp_indicate_work_handler(struct k_work *work)
+{
+	struct bt_gatt_ots_indicate *ind = CONTAINER_OF(work, struct bt_gatt_ots_indicate, work);
+	struct bt_ots *ots = CONTAINER_OF(ind, struct bt_ots, oacp_ind);
+
+	bt_gatt_indicate(NULL, &ots->oacp_ind.params);
+}
+
+static void olcp_indicate_work_handler(struct k_work *work)
+{
+	struct bt_gatt_ots_indicate *ind = CONTAINER_OF(work, struct bt_gatt_ots_indicate, work);
+	struct bt_ots *ots = CONTAINER_OF(ind, struct bt_ots, olcp_ind);
+
+	bt_gatt_indicate(NULL, &ots->olcp_ind.params);
+}
+
 int bt_ots_init(struct bt_ots *ots,
 		     struct bt_ots_init_param *ots_init)
 {
@@ -505,6 +521,9 @@ int bt_ots_init(struct bt_ots *ots,
 	if (IS_ENABLED(CONFIG_BT_OTS_DIR_LIST_OBJ)) {
 		bt_ots_dir_list_init(&ots->dir_list, ots->obj_manager);
 	}
+
+	k_work_init(&ots->oacp_ind.work, oacp_indicate_work_handler);
+	k_work_init(&ots->olcp_ind.work, olcp_indicate_work_handler);
 
 	LOG_DBG("Initialized OTS");
 
