46dfa3527dd31327beba002122e1f55ff58476c8
Glenn Andrews <glenn.andrews.42@gmail.com>
2024-04-20 20:43:43 -0700

    Lib: SMF Modify HSM operation for UML-Style transitions
    Modify the SMF such that state transitions from parent states choose the
    correct Least Common Ancestor based on the transition source rather than
    the current state.
    
    SMF set as experimental.
    
    Signed-off-by: Glenn Andrews <glenn.andrews.42@gmail.com>

--- tests/lib/smf/src/test_lib_smf.h
+++ tests/lib/smf/src/test_lib_smf.h
@@ -10,6 +10,6 @@
 void test_smf_flat(void);
 void test_smf_hierarchical(void);
 void test_smf_hierarchical_5_ancestors(void);
-void test_smf_initial_transitions(void);
+void test_smf_self_transition(void);
 
 #endif /* ZEPHYR_TEST_LIB_SMF_H_ */
