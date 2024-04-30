f1b95e4ddd3cc87c96bc2cc76f62190c0eb9e4d0
Gerard Marull-Paretas <gerard@teslabs.com>
2024-04-30 11:04:29 +0200

    Revert "[nrf fromlist] soc: nordic: Refactor soc_secure.h"
    This reverts commit a1ff51de6be0cf34387a1e6fea3811196768101d.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- soc/arm/nordic_nrf/common/soc_secure.h
+++ soc/arm/nordic_nrf/common/soc_secure.h
@@ -9,44 +9,20 @@
 #include <hal/nrf_ficr.h>
 
 #if defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
-int soc_secure_mem_read(void *dst, void *src, size_t len);
 #if NRF_GPIO_HAS_SEL
 void soc_secure_gpio_pin_mcu_select(uint32_t pin_number, nrf_gpio_pin_sel_t mcu);
 #endif
 
-#else /* defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) */
-static inline int soc_secure_mem_read(void *dst, void *src, size_t len)
-{
-	(void)memcpy(dst, src, len);
-	return 0;
-}
-#if NRF_GPIO_HAS_SEL
-static inline void soc_secure_gpio_pin_mcu_select(uint32_t pin_number, nrf_gpio_pin_sel_t mcu)
-{
-	nrf_gpio_pin_control_select(pin_number, mcu);
-}
-#endif /* NRF_GPIO_HAS_SEL */
+int soc_secure_mem_read(void *dst, void *src, size_t len);
 
-#endif /* defined CONFIG_TRUSTED_EXECUTION_NONSECURE */
-
-/* Include these soc_secure_* functions only when the FICR is mapped as secure only */
-#if defined(NRF_FICR_S)
-#if defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
-static inline void soc_secure_read_deviceid(uint32_t deviceid[2])
-{
-	int err;
-
-	err = soc_secure_mem_read(deviceid, (void *)&NRF_FICR_S->INFO.DEVICEID,
-				  2 * sizeof(uint32_t));
-	__ASSERT(err == 0, "Secure read error (%d)", err);
-}
 #if defined(CONFIG_SOC_HFXO_CAP_INTERNAL)
 static inline uint32_t soc_secure_read_xosc32mtrim(void)
 {
 	uint32_t xosc32mtrim;
 	int err;
 
-	err = soc_secure_mem_read(&xosc32mtrim, (void *)&NRF_FICR_S->XOSC32MTRIM,
+	err = soc_secure_mem_read(&xosc32mtrim,
+				  (void *)&NRF_FICR_S->XOSC32MTRIM,
 				  sizeof(xosc32mtrim));
 	__ASSERT(err == 0, "Secure read error (%d)", err);
 
@@ -54,18 +30,42 @@ static inline uint32_t soc_secure_read_xosc32mtrim(void)
 }
 #endif /* defined(CONFIG_SOC_HFXO_CAP_INTERNAL) */
 
-#else /* defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) */
 static inline void soc_secure_read_deviceid(uint32_t deviceid[2])
 {
-	deviceid[0] = nrf_ficr_deviceid_get(NRF_FICR_S, 0);
-	deviceid[1] = nrf_ficr_deviceid_get(NRF_FICR_S, 1);
+	int err;
+
+	err = soc_secure_mem_read(deviceid,
+				 (void *)&NRF_FICR_S->INFO.DEVICEID,
+				 2 * sizeof(uint32_t));
+	__ASSERT(err == 0, "Secure read error (%d)", err);
 }
+
+#else /* defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) */
+
+static inline int soc_secure_mem_read(void *dst, void *src, size_t len)
+{
+	(void)memcpy(dst, src, len);
+	return 0;
+}
+
+#if NRF_GPIO_HAS_SEL
+static inline void soc_secure_gpio_pin_mcu_select(uint32_t pin_number,
+						  nrf_gpio_pin_sel_t mcu)
+{
+	nrf_gpio_pin_control_select(pin_number, mcu);
+}
+#endif /* NRF_GPIO_HAS_SEL */
+
 #if defined(CONFIG_SOC_HFXO_CAP_INTERNAL)
 static inline uint32_t soc_secure_read_xosc32mtrim(void)
 {
-	return NRF_FICR_S->XOSC32MTRIM;
+	return NRF_FICR->XOSC32MTRIM;
 }
 #endif /* defined(CONFIG_SOC_HFXO_CAP_INTERNAL) */
 
+static inline void soc_secure_read_deviceid(uint32_t deviceid[2])
+{
+	deviceid[0] = nrf_ficr_deviceid_get(NRF_FICR, 0);
+	deviceid[1] = nrf_ficr_deviceid_get(NRF_FICR, 1);
+}
 #endif /* defined CONFIG_TRUSTED_EXECUTION_NONSECURE */
-#endif /* defined(NRF_FICR_S) */
