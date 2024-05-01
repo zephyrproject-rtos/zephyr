c07a87abbb8509e19cd7882639c9e3d0d65a4618
Emil Gydesen <emil.gydesen@nordicsemi.no>
2024-05-01 11:46:47 +0200

    tests: Bluetooth: Audio: Add BSIM test of the CAP samples
    Adds babblesim tests of the CAP Acceptor and the CAP Initiator
    samples for unicast.
    
    This simply checks if at least one stream is connected
    and is sending (empty) data.
    
    This modifies the samples to send data as well as counting the
    receive ISO data packets.
    Ideally the TX would be superflous to verify that ISO is working,
    but a missing feature in the Zephyr LL makes it a requirement.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bsim/bluetooth/audio_samples/compile.sh
+++ tests/bsim/bluetooth/audio_samples/compile.sh
@@ -22,6 +22,16 @@ if [ "${BOARD_TS}" == "nrf5340bsim_nrf5340_cpuapp" ]; then
   app=tests/bsim/bluetooth/audio_samples/bap_broadcast_sink sysbuild=1 \
     conf_file=${ZEPHYR_BASE}/samples/bluetooth/bap_broadcast_sink/prj.conf \
     exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
+  app=tests/bsim/bluetooth/audio_samples/cap/initiator \
+    sample=${ZEPHYR_BASE}/samples/bluetooth/cap_initiator \
+    conf_file=${sample}/prj.conf \
+    conf_overlay=${sample}/boards/nrf5340_audio_dk_nrf5340_cpuapp.conf \
+    exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
+  app=tests/bsim/bluetooth/audio_samples/cap/acceptor \
+    sample=${ZEPHYR_BASE}/samples/bluetooth/cap_acceptor \
+    conf_file=${sample}/prj.conf \
+    conf_overlay=${sample}/boards/nrf5340_audio_dk_nrf5340_cpuapp.conf \
+    exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
 else
   app=samples/bluetooth/bap_unicast_server conf_overlay=overlay-bt_ll_sw_split.conf \
     exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
@@ -35,6 +45,16 @@ else
   app=tests/bsim/bluetooth/audio_samples/bap_broadcast_sink \
     conf_file=${ZEPHYR_BASE}/samples/bluetooth/bap_broadcast_sink/prj.conf \
     conf_overlay=${ZEPHYR_BASE}/samples/bluetooth/bap_broadcast_sink/overlay-bt_ll_sw_split.conf \
+    exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
+  app=tests/bsim/bluetooth/audio_samples/cap/initiator \
+    sample=${ZEPHYR_BASE}/samples/bluetooth/cap_initiator \
+    conf_file=${sample}/prj.conf \
+    conf_overlay=${sample}/overlay-bt_ll_sw_split.conf \
+    exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
+  app=tests/bsim/bluetooth/audio_samples/cap/acceptor \
+    sample=${ZEPHYR_BASE}/samples/bluetooth/cap_acceptor \
+    conf_file=${sample}/prj.conf \
+    conf_overlay=${sample}/overlay-bt_ll_sw_split.conf \
     exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
 fi
 
