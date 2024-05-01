09161a4db22e06d2b71b48c1893453e7c2516791
Pieter De Gendt <pieter.degendt@basalte.be>
2024-05-01 11:45:12 +0200

    include: drivers: Add missing __subsystem tags
    Add __subsystem tags to driver API structs.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- include/zephyr/drivers/video.h
+++ include/zephyr/drivers/video.h
@@ -249,7 +249,7 @@ typedef int (*video_api_set_signal_t)(const struct device *dev,
 				      enum video_endpoint_id ep,
 				      struct k_poll_signal *signal);
 
-struct video_driver_api {
+__subsystem struct video_driver_api {
 	/* mandatory callbacks */
 	video_api_set_format_t set_format;
 	video_api_get_format_t get_format;
