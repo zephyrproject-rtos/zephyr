a64ea4bd912823e4bcfb13d3929f7a7f03383a58
Hess Nathan <nhess@baumer.com>
2024-05-02 12:55:30 +0200

    coding guidelines: comply with MISRA Rule 12.1.
    -added parentheses verifying lack of ambiguities
    
    Signed-off-by: Hess Nathan <nhess@baumer.com>

--- subsys/logging/log_minimal.c
+++ subsys/logging/log_minimal.c
@@ -43,7 +43,7 @@ static void minimal_hexdump_line_print(const char *data, size_t length)
 		if (i < length) {
 			unsigned char c = data[i];
 
-			printk("%c", isprint((int)c) != 0 ? c : '.');
+			printk("%c", (isprint((int)c) != 0) ? c : '.');
 		} else {
 			printk(" ");
 		}
