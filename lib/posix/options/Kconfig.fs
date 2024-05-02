24b8168a06ba865ee7c17a67fb91ac14b7e0991d
Chris Friedt <cfriedt@tenstorrent.com>
2024-05-02 19:26:41 -0400

    posix: fs: create a separate Kconfig option for fsync
    Create CONFIG_POSIX_FSYNC to match the spec.
    
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

--- lib/posix/options/Kconfig.fs
+++ lib/posix/options/Kconfig.fs
@@ -10,10 +10,19 @@ menuconfig POSIX_FS
 	help
 	  This enables POSIX style file system related APIs.
 
+if POSIX_FS
+
 config POSIX_MAX_OPEN_FILES
 	int "Maximum number of open file descriptors"
 	default 16
-	depends on POSIX_FS
 	help
 	  Maximum number of open files. Note that this setting
 	  is additionally bounded by CONFIG_POSIX_MAX_FDS.
+
+config POSIX_FSYNC
+	bool "Support for fsync()"
+	default y
+	help
+	  This enables fsync() support.
+
+endif # POSIX_FS
