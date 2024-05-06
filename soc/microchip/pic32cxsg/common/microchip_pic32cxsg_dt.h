3b01cf5d6a4a2efe29a5d425747e6fd675692c15
Zephyr DeveloperMichael <michael.sherwood@microchip.com>
2024-05-06 09:38:00 -0700

    Adding support for Microchip PIC32CX SG devices

---
+++ soc/microchip/pic32cxsg/common/microchip_pic32cxsg_dt.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2024 Microchip
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/** @file
+ * @brief Microchip PIC32CXSG MCU family devicetree helper macros
+ */
+
+#ifndef _MICROCHIP_PIC32CXSG_DT_H_
+#define _MICROCHIP_PIC32CXSG_DT_H_
+
+/* Helper macro to get MCLK register address for corresponding
+ * that has corresponding clock enable bit.
+ */
+#define MCLK_MASK_DT_INT_REG_ADDR(n) \
+	(DT_REG_ADDR(DT_INST_PHANDLE_BY_NAME(n, clocks, mclk)) + \
+	 DT_INST_CLOCKS_CELL_BY_NAME(n, mclk, offset))
+
+/* Helper macros for use with Microchip PIC32CXSG DMAC controller
+ * return 0xff as default value if there is no 'dmas' property
+ */
+#define MICROCHIP_PIC32CXSG_DT_INST_DMA_CELL(n, name, cell)		\
+	COND_CODE_1(DT_INST_NODE_HAS_PROP(n, dmas),		\
+		    (DT_INST_DMAS_CELL_BY_NAME(n, name, cell)),	\
+		    (0xff))
+#define MICROCHIP_PIC32CXSG_DT_INST_DMA_TRIGSRC(n, name) \
+	MICROCHIP_PIC32CXSG0_DT_INST_DMA_CELL(n, name, trigsrc)
+#define MICROCHIP_PIC32CXSG_DT_INST_DMA_CHANNEL(n, name) \
+	MICROCHIP_PIC32CXSG0_DT_INST_DMA_CELL(n, name, channel)
+#define MICROCHIP_PIC32CXSG_DT_INST_DMA_CTLR(n, name)			\
+	COND_CODE_1(DT_INST_NODE_HAS_PROP(n, dmas),		\
+		    (DT_INST_DMAS_CTLR_BY_NAME(n, name)),	\
+		    (DT_INVALID_NODE))
+
+
+/* Use to check if a sercom 'n' is enabled for a given 'compat' */
+#define MICROCHIP_PIC32CXSG_DT_SERCOM_CHECK(n, compat) \
+	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(sercom##n), compat, okay)
+
+/* Use to check if TCC 'n' is enabled for a given 'compat' */
+#define MICROCHIP_PIC32CXSG_DT_TCC_CHECK(n, compat) \
+	DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(tcc##n), compat, okay)
+
+/* Common macro for use to set HCLK_FREQ_HZ */
+#define MICROCHIP_PIC32CXSG_DT_CPU_CLK_FREQ_HZ \
+	DT_PROP(DT_PATH(cpus, cpu_0), clock_frequency)
+
+#endif /* _MICROCHIP_PIC32CXSG_SOC_DT_H_ */
---
+++ soc/microchip/pic32cxsg/pic32cxsg41/soc.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2024 Microchip
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC_MICROCHIP_PIC32CXSG41_SOC_H_
+#define _SOC_MICROCHIP_PIC32CXSG41_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#define DONT_USE_CMSIS_INIT
+
+#include <zephyr/types.h>
+
+#if defined(CONFIG_SOC_PIC32CX1025SG41100)
+#include <pic32cx1025sg41100.h>
+#elif defined(CONFIG_SOC_PIC32CX1025SG41128)
+#include <pic32cx1025sg41128.h>
+
+#else
+#error Library does not support the specified device.
+#endif
+
+#endif /* _ASMLANGUAGE */
+
+#include "sercom_fixup_pic32cxsg.h"
+#include "tc_fixup_pic32cxsg.h"
+#include "gmac_fixup_pic32cxsg.h"
+#include "adc_fixup_pic32cxsg.h"
+#include "../common/soc_port.h"
+#include "../common/microchip_pic32cxsg_dt.h"
+
+#define SOC_MICROCHIP_PIC32CXSG_OSC32K_FREQ_HZ 32768
+#define SOC_MICROCHIP_PIC32CXSG_DFLL48_FREQ_HZ 48000000
+
+/** Processor Clock (HCLK) Frequency */
+#define SOC_MICROCHIP_PIC32CXSG_HCLK_FREQ_HZ CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
+/** Master Clock (MCK) Frequency */
+#define SOC_MICROCHIP_PIC32CXSG_MCK_FREQ_HZ SOC_MICROCHIP_PIC32CXSG_HCLK_FREQ_HZ
+#define SOC_MICROCHIP_PIC32CXSG_GCLK0_FREQ_HZ SOC_MICROCHIP_PIC32CXSG_MCK_FREQ_HZ
+#define SOC_MICROCHIP_PIC32CXSG_GCLK2_FREQ_HZ 48000000
+
+#endif /* _SOC_MICROCHIP_PIC32CXSG41_SOC_H_ */
