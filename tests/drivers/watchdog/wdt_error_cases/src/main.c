48ee6fcae0571681ec5f892616e90679eae8c104
Sebastian Głąb <sebastian.glab@nordicsemi.no>
2024-05-07 13:28:29 +0200

    tests: drivers: watchdog: wdt_error_cases: Run test on nRF54H20
    Add overaly files for Application and Radio cores.
    
    Configure test according to watchdog features
    available on nRF54H20.
    
    Signed-off-by: Sebastian Głąb <sebastian.glab@nordicsemi.no>

--- tests/drivers/watchdog/wdt_error_cases/src/main.c
+++ tests/drivers/watchdog/wdt_error_cases/src/main.c
@@ -42,7 +42,7 @@
 #define DEFAULT_WINDOW_MIN (0U)
 
 /* Align tests to the specific target: */
-#if defined(CONFIG_SOC_NRF54L15)
+#if defined(CONFIG_SOC_NRF54L15) || defined(CONFIG_SOC_NRF54H20)
 #define WDT_TEST_FLAGS                                                                             \
 	(WDT_DISABLE_SUPPORTED | WDT_FLAG_RESET_SOC_SUPPORTED |                                    \
 	 WDT_FLAG_ONLY_ONE_TIMEOUT_VALUE_SUPPORTED | WDT_OPT_PAUSE_IN_SLEEP_SUPPORTED |            \
