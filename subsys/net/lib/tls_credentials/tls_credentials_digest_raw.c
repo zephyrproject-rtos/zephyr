5845c7588515960c39271cc29745a4bc367d274e
Valerio Setti <vsetti@baylibre.com>
2024-05-07 13:19:49 +0200

    tls_credentials: remove TinyCrypt implementation of digest_raw
    Moving toward the adoption of PSA crypto APIs, TinyCrypt is going
    to be deprecated so we can remove it in favor of newer APIs.
    
    Moreover tls_credentials is mostly meant to store data in the
    protected storage meaning that there is a TFM implementation
    providing this service. The same TFM can of course be used to
    compute sha256 hashing thus saving memory footprint on the NS
    side by removing TC.
    
    Signed-off-by: Valerio Setti <vsetti@baylibre.com>

--- subsys/net/lib/tls_credentials/tls_credentials_digest_raw.c
+++ subsys/net/lib/tls_credentials/tls_credentials_digest_raw.c
@@ -16,46 +16,7 @@
 #include "tls_internal.h"
 #include "tls_credentials_digest_raw.h"
 
-/* Grab mbedTLS headers if they are available so that we can check whether SHA256 is supported */
-
-#if defined(CONFIG_MBEDTLS)
-#if !defined(CONFIG_MBEDTLS_CFG_FILE)
-#include "mbedtls/config.h"
-#else
-#include CONFIG_MBEDTLS_CFG_FILE
-#endif /* CONFIG_MBEDTLS_CFG_FILE */
-#endif /* CONFIG_MBEDTLS */
-
-#if defined(CONFIG_TINYCRYPT_SHA256) && defined(CONFIG_BASE64)
-
-#include <tinycrypt/sha256.h>
-#include <zephyr/sys/base64.h>
-
-int credential_digest_raw(struct tls_credential *credential, void *dest, size_t *len)
-{
-	int err = 0;
-	size_t written = 0;
-	struct tc_sha256_state_struct sha_state;
-	uint8_t digest_buf[TC_SHA256_DIGEST_SIZE];
-
-	/* Compute digest. */
-	(void)tc_sha256_init(&sha_state);
-	(void)tc_sha256_update(&sha_state, credential->buf, credential->len);
-	(void)tc_sha256_final(digest_buf, &sha_state);
-
-	/* Attempt to encode digest to destination.
-	 * Will return -ENOMEM if there is not enough space in the destination buffer.
-	 */
-	err = base64_encode(dest, *len, &written, digest_buf, sizeof(digest_buf));
-	*len = err ? 0 : written;
-
-	/* Clean up. */
-	memset(&sha_state, 0, sizeof(sha_state));
-	memset(digest_buf, 0, sizeof(digest_buf));
-	return err;
-}
-
-#elif defined(CONFIG_PSA_WANT_ALG_SHA_256) && defined(CONFIG_BASE64)
+#if defined(CONFIG_PSA_WANT_ALG_SHA_256) && defined(CONFIG_BASE64)
 
 #include <psa/crypto.h>
 
