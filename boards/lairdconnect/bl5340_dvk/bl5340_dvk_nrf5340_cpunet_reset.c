688107b4a1a26f2f1df58444d5268a35042c1731
Jędrzej Ciupis <jedrzej.ciupis@nordicsemi.no>
2024-05-07 13:04:51 +0200

    [nrf fromlist] boards: control the network CPU directly
    Board initialization code should not use the network CPU management. It
    acts as a permanent request for the network CPU, which causes its users
    to be unable to turn it off. Instead, let the board initialization code
    control the network CPU directly. It sets initial state of the network
    core but does not have any impact on sharing the network CPU as a
    resource between its users.
    
    Upstream PR: https://github.com/zephyrproject-rtos/zephyr/pull/72412
    
    Signed-off-by: Jędrzej Ciupis <jedrzej.ciupis@nordicsemi.no>

--- boards/lairdconnect/bl5340_dvk/bl5340_dvk_nrf5340_cpunet_reset.c
+++ boards/lairdconnect/bl5340_dvk/bl5340_dvk_nrf5340_cpunet_reset.c
@@ -10,7 +10,7 @@
 #include <zephyr/logging/log.h>
 
 #include <soc.h>
-#include <nrf53_cpunet_mgmt.h>
+#include <hal/nrf_reset.h>
 
 LOG_MODULE_REGISTER(bl5340_dvk_cpuapp, CONFIG_LOG_DEFAULT_LEVEL);
 
@@ -50,7 +50,7 @@ static int remoteproc_mgr_boot(void)
 	 */
 
 	/* Release the Network MCU, 'Release force off signal' */
-	nrf53_cpunet_enable(true);
+	nrf_reset_network_force_off(NRF_RESET, false);
 
 	LOG_DBG("Network MCU released.");
 #endif /* !CONFIG_TRUSTED_EXECUTION_SECURE */
