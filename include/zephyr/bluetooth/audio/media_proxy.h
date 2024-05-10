c547079cc2552f2974fa3e50e1d6717b11733481
Emil Gydesen <emil.gydesen@nordicsemi.no>
2024-05-10 11:56:30 +0200

    Bluetooth: Audio: Upgrade from experimental to unstable
    This commit upgrades the LE audio API and implementation
    from experimental to unstable.
    
    LE Audio have existing for quite a few years in Zephyr
    and is not going anywhere, but at the same time it still
    has significant effort remaining before it can be
    considered stable.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- include/zephyr/bluetooth/audio/media_proxy.h
+++ include/zephyr/bluetooth/audio/media_proxy.h
@@ -11,6 +11,9 @@
  *
  * @defgroup bt_media_proxy Media Proxy
  *
+ * @since 3.0
+ * @version 0.8.0
+ *
  * @ingroup bluetooth
  * @{
  *
@@ -31,9 +34,6 @@
  * application, or it may be a Media Control Service relaying requests
  * from a remote Media Control Client. There may be either local or
  * remote control, or both, or even multiple instances of each.
- *
- * [Experimental] Users should note that the APIs can change
- * as a part of ongoing development.
  */
 
 #include <stdint.h>
