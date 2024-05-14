83132542fc430f817db5becf228337e74879465f
Adrien Ricciardi <aricciardi@baylibre.com>
2024-05-14 19:07:00 +0200

    [nrf fromtree] lib: crc: Add OpenPGP CRC-24
    Implementation is based on the section 6.1 of the RFC 4880.
    
    Signed-off-by: Adrien Ricciardi <aricciardi@baylibre.com>
    (cherry picked from commit 863f6affefa8b055e18d19ca84ef652ed4dfc59d)
    (cherry picked from commit 3038780e75077ccb481e209122f8b81dc66578e4)

--- lib/crc/crc_shell.c
+++ lib/crc/crc_shell.c
@@ -28,6 +28,7 @@ static const char *const crc_types[] = {
 	[CRC16_ANSI] = "16_ansi",
 	[CRC16_CCITT] = "16_ccitt",
 	[CRC16_ITU_T] = "16_itu_t",
+	[CRC24_PGP] = "24_pgp",
 	[CRC32_C] = "32_c",
 	[CRC32_IEEE] = "32_ieee",
 };
