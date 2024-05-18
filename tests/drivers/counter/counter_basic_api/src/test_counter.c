a090123b79b91d07ce3b48fe0260583c5a1b837c
Jérémy LOCHE - MAKEEN Energy <jlh@makeenenergy.com>
2024-05-18 12:11:16 +0200

    sample: test: counter: enable colibri_imx7d
    Enable counter sample for colibri_imx7d using gpt2.
    Enable counter test using OCRAM and gpt2.
    
    Signed-off-by: Jérémy LOCHE - MAKEEN Energy <jlh@makeenenergy.com>

--- tests/drivers/counter/counter_basic_api/src/test_counter.c
+++ tests/drivers/counter/counter_basic_api/src/test_counter.c
@@ -57,6 +57,7 @@ static const struct device *const devices[] = {
 	DEVS_FOR_DT_COMPAT(microchip_xec_timer)
 	DEVS_FOR_DT_COMPAT(nxp_imx_epit)
 	DEVS_FOR_DT_COMPAT(nxp_imx_gpt)
+	DEVS_FOR_DT_COMPAT(nxp_imx7d_gpt)
 #ifdef CONFIG_COUNTER_MCUX_TPM
 	DEVS_FOR_DT_COMPAT(nxp_tpm_timer)
 #endif
