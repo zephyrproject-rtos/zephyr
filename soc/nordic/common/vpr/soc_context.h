ea8db7636df6f4000c3f954255d28a3c7768bd93
Robert Lubos <robert.lubos@nordicsemi.no>
2024-05-20 12:21:33 +0200

    Revert "[nrf fromtree] soc: nordic: vpr: fix soc isr sw stacking."
    This reverts commit a0e4011234b9fb049c6d0e10d67cceb8b7e91024.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- soc/nordic/common/vpr/soc_context.h
+++ soc/nordic/common/vpr/soc_context.h
@@ -6,17 +6,7 @@
 #ifndef SOC_RISCV_NORDIC_NRF_COMMON_VPR_SOC_CONTEXT_H_
 #define SOC_RISCV_NORDIC_NRF_COMMON_VPR_SOC_CONTEXT_H_
 
-#define SOC_ESF_MEMBERS						\
-	unsigned long minttresh;				\
-	unsigned long sp_align;					\
-	unsigned long padding0;					\
-	unsigned long padding1;					\
-	unsigned long padding2
-
-#define SOC_ESF_INIT						\
-	0,							\
-	0,							\
-	0,							\
-	0
+#define SOC_ESF_MEMBERS unsigned long minttresh
+#define SOC_ESF_INIT    0
 
 #endif /* SOC_RISCV_NORDIC_NRF_COMMON_VPR_SOC_CONTEXT_H_ */
