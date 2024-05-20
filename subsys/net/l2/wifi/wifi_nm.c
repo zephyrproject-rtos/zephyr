2b6bbe0018535e2e7ef38c3d7e14125eed218c5d
Robert Lubos <robert.lubos@nordicsemi.no>
2024-05-20 12:21:47 +0200

    Revert "[nrf fromtree] net: l2: wifi: Protect processing of nm data"
    This reverts commit 570c8c21ab04bcda1059cbb2af3ee872d4a6d364.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/net/l2/wifi/wifi_nm.c
+++ subsys/net/l2/wifi/wifi_nm.c
@@ -10,9 +10,6 @@ LOG_MODULE_REGISTER(wifi_nm, CONFIG_WIFI_NM_LOG_LEVEL);
 #include <zephyr/kernel.h>
 #include <zephyr/net/wifi_nm.h>
 
-/* Used to protect nm data */
-static K_MUTEX_DEFINE(wifi_nm_lock);
-
 struct wifi_nm_instance *wifi_nm_get_instance(const char *name)
 {
 	STRUCT_SECTION_FOREACH(wifi_nm_instance, nm) {
@@ -30,16 +27,13 @@ struct wifi_nm_instance *wifi_nm_get_instance_iface(struct net_if *iface)
 		return false;
 	}
 
-	k_mutex_lock(&wifi_nm_lock, K_FOREVER);
 	STRUCT_SECTION_FOREACH(wifi_nm_instance, nm) {
 		for (int i = 0; i < CONFIG_WIFI_NM_MAX_MANAGED_INTERFACES; i++) {
 			if (nm->mgd_ifaces[i] == iface) {
-				k_mutex_unlock(&wifi_nm_lock);
 				return nm;
 			}
 		}
 	}
-	k_mutex_unlock(&wifi_nm_lock);
 
 	return NULL;
 }
@@ -54,15 +48,12 @@ int wifi_nm_register_mgd_iface(struct wifi_nm_instance *nm, struct net_if *iface
 		return -ENOTSUP;
 	}
 
-	k_mutex_lock(&wifi_nm_lock, K_FOREVER);
 	for (int i = 0; i < CONFIG_WIFI_NM_MAX_MANAGED_INTERFACES; i++) {
 		if (!nm->mgd_ifaces[i]) {
 			nm->mgd_ifaces[i] = iface;
-			k_mutex_unlock(&wifi_nm_lock);
 			return 0;
 		}
 	}
-	k_mutex_unlock(&wifi_nm_lock);
 
 	return -ENOMEM;
 }
@@ -73,15 +64,12 @@ int wifi_nm_unregister_mgd_iface(struct wifi_nm_instance *nm, struct net_if *ifa
 		return -EINVAL;
 	}
 
-	k_mutex_lock(&wifi_nm_lock, K_FOREVER);
 	for (int i = 0; i < CONFIG_WIFI_NM_MAX_MANAGED_INTERFACES; i++) {
 		if (nm->mgd_ifaces[i] == iface) {
 			nm->mgd_ifaces[i] = NULL;
-			k_mutex_unlock(&wifi_nm_lock);
 			return 0;
 		}
 	}
-	k_mutex_unlock(&wifi_nm_lock);
 
 	return -ENOENT;
 }
