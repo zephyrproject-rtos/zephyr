d59168eecbe14c7c1d29eb0a8243ae36b1da9354
Raffael Rostagno <raffael.rostagno@espressif.com>
2024-05-21 11:41:15 -0300

    drivers: ledc: Clock source update to support ESP32C6
    Clock source SCLK added for C6 on LEDC
    
    Signed-off-by: Raffael Rostagno <raffael.rostagno@espressif.com>

--- drivers/pwm/pwm_led_esp32.c
+++ drivers/pwm/pwm_led_esp32.c
@@ -124,7 +124,11 @@ static int pwm_led_esp32_calculate_max_resolution(struct pwm_ledc_esp32_channel_
 	 * Max duty resolution can be obtained with
 	 * max_res = log2(CLK_FREQ/FREQ)
 	 */
+#if SOC_LEDC_SUPPORT_APB_CLOCK
 	uint64_t clock_freq = channel->clock_src == LEDC_APB_CLK ? APB_CLK_FREQ : REF_CLK_FREQ;
+#elif SOC_LEDC_SUPPORT_PLL_DIV_CLOCK
+	uint64_t clock_freq = SCLK_CLK_FREQ;
+#endif
 	uint32_t max_precision_n = clock_freq/channel->freq;
 
 	for (uint8_t i = 0; i <= SOC_LEDC_TIMER_BIT_WIDTH; i++) {
@@ -159,7 +163,9 @@ static int pwm_led_esp32_timer_config(struct pwm_ledc_esp32_channel_config *chan
 	 * If the given frequency doesn't support it, we move to the next clock source.
 	 */
 
+#if SOC_LEDC_SUPPORT_APB_CLOCK
 	channel->clock_src = LEDC_APB_CLK;
+#endif
 	if (!pwm_led_esp32_calculate_max_resolution(channel)) {
 		return 0;
 	}
@@ -194,6 +200,7 @@ static int pwm_led_esp32_timer_set(const struct device *dev,
 	__ASSERT_NO_MSG(channel->freq > 0);
 
 	switch (channel->clock_src) {
+#if SOC_LEDC_SUPPORT_APB_CLOCK
 	case LEDC_APB_CLK:
 		/** This expression comes from ESP32 Espressif's Technical Reference
 		 * Manual chapter 13.2.2 Timers.
@@ -201,6 +208,12 @@ static int pwm_led_esp32_timer_set(const struct device *dev,
 		 */
 		prescaler = ((uint64_t) APB_CLK_FREQ << 8) / channel->freq / precision;
 	break;
+#endif
+#if SOC_LEDC_SUPPORT_PLL_DIV_CLOCK
+	case LEDC_SCLK:
+		prescaler = ((uint64_t) SCLK_CLK_FREQ << 8) / channel->freq / precision;
+	break;
+#endif
 #if SOC_LEDC_SUPPORT_REF_TICK
 	case LEDC_REF_TICK:
 		prescaler = ((uint64_t) REF_CLK_FREQ << 8) / channel->freq / precision;
@@ -245,7 +258,11 @@ static int pwm_led_esp32_get_cycles_per_sec(const struct device *dev,
 		return -EINVAL;
 	}
 
+#if SOC_LEDC_SUPPORT_APB_CLOCK
 	*cycles = channel->clock_src == LEDC_APB_CLK ? APB_CLK_FREQ : REF_CLK_FREQ;
+#elif SOC_LEDC_SUPPORT_PLL_DIV_CLOCK
+	*cycles = SCLK_CLK_FREQ;
+#endif
 
 	return 0;
 }
@@ -338,6 +355,12 @@ static const struct pwm_driver_api pwm_led_esp32_api = {
 
 PINCTRL_DT_INST_DEFINE(0);
 
+#if SOC_LEDC_SUPPORT_APB_CLOCK
+	#define CLOCK_SOURCE	LEDC_APB_CLK
+#elif SOC_LEDC_SUPPORT_PLL_DIV_CLOCK
+	#define CLOCK_SOURCE	LEDC_SCLK
+#endif
+
 #define CHANNEL_CONFIG(node_id)                                                \
 	{                                                                      \
 		.idx = DT_REG_ADDR(node_id),                                   \
@@ -346,7 +369,7 @@ PINCTRL_DT_INST_DEFINE(0);
 		.speed_mode = DT_REG_ADDR(node_id) < SOC_LEDC_CHANNEL_NUM      \
 				      ? LEDC_LOW_SPEED_MODE                    \
 				      : !LEDC_LOW_SPEED_MODE,                  \
-		.clock_src = LEDC_APB_CLK,                                     \
+		.clock_src = CLOCK_SOURCE,                                     \
 	},
 
 static struct pwm_ledc_esp32_channel_config channel_config[] = {
