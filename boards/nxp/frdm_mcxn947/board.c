ee9513b62af3840da4e9c5b35758da8ae7009cde
Emilio Benavente <emilio.benavente@nxp.com>
2024-05-22 21:15:48 +0000

    boards: nxp: frdm_mcxn947: Enable lpmtr for frdm_mcxn947
    Enabled one instance of lptmr
    running from the 16k FRO.
    
    Signed-off-by: Emilio Benavente <emilio.benavente@nxp.com>

--- boards/nxp/frdm_mcxn947/board.c
+++ boards/nxp/frdm_mcxn947/board.c
@@ -298,6 +298,10 @@ static int frdm_mcxn947_init(void)
 	SPC_EnableActiveModeAnalogModules(SPC0, (kSPC_controlCmp0 | kSPC_controlCmp0Dac));
 #endif
 
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(lptmr0), okay)
+	CLOCK_SetupClk16KClocking(kCLOCK_Clk16KToVsys);
+#endif
+
 	/* Set SystemCoreClock variable. */
 	SystemCoreClock = CLOCK_INIT_CORE_CLOCK;
 
