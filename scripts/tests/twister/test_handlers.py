3329e8e18c18bb2f98fe1f7e646e85f4f6be08ee
Robert Lubos <robert.lubos@nordicsemi.no>
2024-05-25 10:02:11 +0200

    Revert "[nrf fromlist] twister: Allow to run sysbuild with native_sim"
    This reverts commit 33c8345d3bdc6280f252afd5eab638a4267dc114.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- scripts/tests/twister/test_handlers.py
+++ scripts/tests/twister/test_handlers.py
@@ -448,7 +448,7 @@ TESTDATA_4 = [
       '--log-file=build_dir/valgrind.log', '--track-origins=yes',
       'generator', 'run_renode_test']),
     (False, True, False, 123, None, ['generator', 'run', '--seed=123']),
-    (False, False, False, None, ['ex1', 'ex2'], ['build_dir/zephyr/zephyr.exe', 'ex1', 'ex2']),
+    (False, False, False, None, ['ex1', 'ex2'], ['bin', 'ex1', 'ex2']),
 ]
 
 @pytest.mark.parametrize(
@@ -474,7 +474,6 @@ def test_binaryhandler_create_command(
     handler.seed = seed
     handler.extra_test_args = extra_args
     handler.build_dir = 'build_dir'
-    handler.instance.testsuite.sysbuild = False
 
     command = handler._create_command(robot_test)
 
@@ -1479,7 +1478,7 @@ TESTDATA_19 = [
     TESTDATA_19,
     ids=['domains build dir', 'self build dir']
 )
-def test_qemuhandler_get_default_domain_build_dir(
+def test_qemuhandler_get_sysbuild_build_dir(
     mocked_instance,
     self_sysbuild,
     self_build_dir,
@@ -1496,7 +1495,7 @@ def test_qemuhandler_get_default_domain_build_dir(
     handler.build_dir = self_build_dir
 
     with mock.patch('domains.Domains.from_file', from_file_mock):
-        result = handler.get_default_domain_build_dir()
+        result = handler._get_sysbuild_build_dir()
 
     assert result == expected
 
@@ -2018,7 +2017,7 @@ def test_qemuhandler_handle(
     harness = mock.Mock(state=harness_state)
     handler_options_west_flash = []
 
-    domain_build_dir = os.path.join('sysbuild', 'dummydir')
+    sysbuild_build_dir = os.path.join('sysbuild', 'dummydir')
     command = ['generator_cmd', '-C', os.path.join('cmd', 'path'), 'run']
 
     handler.options = mock.Mock(
@@ -2031,7 +2030,7 @@ def test_qemuhandler_handle(
     handler._final_handle_actions = mock.Mock(return_value=None)
     handler._create_command = mock.Mock(return_value=command)
     handler._set_qemu_filenames = mock.Mock(side_effect=mock_filenames)
-    handler.get_default_domain_build_dir = mock.Mock(return_value=domain_build_dir)
+    handler._get_sysbuild_build_dir = mock.Mock(return_value=sysbuild_build_dir)
     handler.terminate = mock.Mock()
 
     unlink_mock = mock.Mock()
