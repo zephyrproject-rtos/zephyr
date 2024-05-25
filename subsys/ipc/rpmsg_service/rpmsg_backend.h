26ceb9fe150cb9b66a7e553f9963011373931bc7
Robert Lubos <robert.lubos@nordicsemi.no>
2024-05-25 10:02:17 +0200

    Revert "[nrf noup] tree-wide: support NCS Partition Manager (PM) definitions"
    This reverts commit 91b6032820a140a6722f0838544f55d69b99a3e1.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/ipc/rpmsg_service/rpmsg_backend.h
+++ subsys/ipc/rpmsg_service/rpmsg_backend.h
@@ -13,35 +13,8 @@
 extern "C" {
 #endif
 
-#if CONFIG_PARTITION_MANAGER_ENABLED
-
-#include "pm_config.h"
-
-#if defined(PM_RPMSG_NRF53_SRAM_ADDRESS) || defined(PM__RPMSG_NRF53_SRAM_ADDRESS)
-
-#if defined(PM_RPMSG_NRF53_SRAM_ADDRESS)
-#define VDEV_START_ADDR PM_RPMSG_NRF53_SRAM_ADDRESS
-#define VDEV_SIZE PM_RPMSG_NRF53_SRAM_SIZE
-#else
-/* The current image is a child image in a different domain than the image
- * which defined the required values. To reach the values of the parent domain
- * we use the 'PM__' variant of the define.
- */
-#define VDEV_START_ADDR PM__RPMSG_NRF53_SRAM_ADDRESS
-#define VDEV_SIZE PM__RPMSG_NRF53_SRAM_SIZE
-#endif /* defined(PM_RPMSG_NRF53_SRAM_ADDRESS) */
-
-#else
 #define VDEV_START_ADDR		DT_REG_ADDR(DT_CHOSEN(zephyr_ipc_shm))
 #define VDEV_SIZE		DT_REG_SIZE(DT_CHOSEN(zephyr_ipc_shm))
-#endif /* defined(PM_RPMSG_NRF53_SRAM_ADDRESS) || defined(PM__RPMSG_NRF53_SRAM_ADDRESS) */
-
-#else
-
-#define VDEV_START_ADDR		DT_REG_ADDR(DT_CHOSEN(zephyr_ipc_shm))
-#define VDEV_SIZE		DT_REG_SIZE(DT_CHOSEN(zephyr_ipc_shm))
-
-#endif /* CONFIG_PARTITION_MANAGER_ENABLED */
 
 #define VDEV_STATUS_ADDR	VDEV_START_ADDR
 #define VDEV_STATUS_SIZE	0x400
