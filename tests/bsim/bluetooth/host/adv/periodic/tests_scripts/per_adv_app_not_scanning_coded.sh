f675b2a1e5e4c8c98083e60c3054a7d7811f308c
Rubin Gerritsen <rubin.gerritsen@nordicsemi.no>
2024-05-26 22:38:29 +0200

    tests: Bluetooth: Add coverage for Coded PHY sync establishment
    Adds test coverage for the case where the host starts the
    scanner automatically upon sync creation.
    This test covers there bug where we previously did not start
    scanning on Coded PHY.
    
    Signed-off-by: Rubin Gerritsen <rubin.gerritsen@nordicsemi.no>

---
+++ tests/bsim/bluetooth/host/adv/periodic/tests_scripts/per_adv_app_not_scanning_coded.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Copyright (c) 2024 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+# Periodic advertising sync test where the host starts scanning
+# automatically because the application didn't start it.
+# The advertiser is using Coded PHY as primary PHY.
+
+source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
+
+simulation_id="per_adv_app_not_scanning_coded"
+verbosity_level=2
+
+cd ${BSIM_OUT_PATH}/bin
+
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_host_adv_periodic_prj_coded_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=0 -RealEncryption=0 \
+  -testid=per_adv_advertiser_coded_phy -rs=23
+
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_host_adv_periodic_prj_coded_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=1 -RealEncryption=0 \
+  -testid=per_adv_syncer_app_not_scanning -rs=6
+
+Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
+  -D=2 -sim_length=20e6 $@
+
+wait_for_background_jobs
