803da2e2c884ee1eafcaf0c41c2138f8cf4d0732
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2024-05-26 22:39:30 +0200

    net: doc: zperf.h: Add missing doxygen comments
    The zperp.h header file was missing doxygen comments.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- include/zephyr/net/zperf.h
+++ include/zephyr/net/zperf.h
@@ -24,6 +24,8 @@
 extern "C" {
 #endif
 
+/** @cond INTERNAL_HIDDEN */
+
 enum zperf_status {
 	ZPERF_SESSION_STARTED,
 	ZPERF_SESSION_PERIODIC_RESULT,
@@ -51,17 +53,20 @@ struct zperf_download_params {
 	char if_name[IFNAMSIZ];
 };
 
+/** @endcond */
+
+/** Performance results */
 struct zperf_results {
-	uint32_t nb_packets_sent;
-	uint32_t nb_packets_rcvd;
-	uint32_t nb_packets_lost;
-	uint32_t nb_packets_outorder;
-	uint64_t total_len;
-	uint64_t time_in_us;
-	uint32_t jitter_in_us;
-	uint64_t client_time_in_us;
-	uint32_t packet_size;
-	uint32_t nb_packets_errors;
+	uint32_t nb_packets_sent;     /**< Number of packets sent */
+	uint32_t nb_packets_rcvd;     /**< Number of packets received */
+	uint32_t nb_packets_lost;     /**< Number of packets lost */
+	uint32_t nb_packets_outorder; /**< Number of packets out of order */
+	uint64_t total_len;           /**< Total length of the transferred data */
+	uint64_t time_in_us;          /**< Total time of the transfer in microseconds */
+	uint32_t jitter_in_us;        /**< Jitter in microseconds */
+	uint64_t client_time_in_us;   /**< Client connection time in microseconds */
+	uint32_t packet_size;         /**< Packet size */
+	uint32_t nb_packets_errors;   /**< Number of packet errors */
 };
 
 /**
