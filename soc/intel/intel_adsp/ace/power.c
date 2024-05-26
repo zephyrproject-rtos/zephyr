d94e5d549de82673fd98b5685915fac9d741b691
Flavio Ceolin <flavio.ceolin@intel.com>
2024-05-26 22:39:39 +0200

    intel_adsp/ace: pm: Remove unnecessary cache flush
    soc_cpus_active is not in cached memory.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- soc/intel/intel_adsp/ace/power.c
+++ soc/intel/intel_adsp/ace/power.c
@@ -195,7 +195,6 @@ void power_gate_entry(uint32_t core_id)
 	}
 
 	soc_cpus_active[core_id] = false;
-	sys_cache_data_flush_range(soc_cpus_active, sizeof(soc_cpus_active));
 	k_cpu_idle();
 
 	/* It is unlikely we get in here, but when this happens
