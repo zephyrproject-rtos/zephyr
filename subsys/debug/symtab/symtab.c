8b2a9802c4cfa2aca9c1f817e7ce13e7377bd0e4
Yong Cong Sin <ycsin@meta.com>
2024-05-27 21:38:55 +0800

    build: symtab: Add a dummy entry at the end for the binary search
    The `symtab_find_symbol_name()` is using an adapted binary
    search function to get the entry between 2 addresses, we need
    to add a dummy entry at the end so that the search function
    can remain simple and straightforward without doing
    out-of-bound checks:
    
       20  \
           |
           |
       50  x
           |
           |
       90  x
        .  |
        .  |
        .  |
    dummy  /
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>

--- subsys/debug/symtab/symtab.c
+++ subsys/debug/symtab/symtab.c
@@ -5,6 +5,7 @@
  */
 
 #include <string.h>
+#include <stdbool.h>
 
 #include <zephyr/debug/symtab.h>
 
@@ -19,22 +20,25 @@ const char *const symtab_find_symbol_name(uintptr_t addr, uint32_t *offset)
 {
 	const struct symtab_info *const symtab = symtab_get();
 	const uint32_t symbol_offset = addr - symtab->start_addr;
-	uint32_t left = 0, right = symtab->length - 1;
+	uint32_t left = 0, right = symtab->length;
 	uint32_t ret_offset = 0;
 	const char *ret_name = "?";
 
-	while (left <= right) {
-		uint32_t mid = left + (right - left) / 2;
+	/* No need to search if the address is out-of-bound */
+	if (symbol_offset < symtab->entries[symtab->length].offset) {
+		while (left <= right) {
+			uint32_t mid = left + (right - left) / 2;
 
-		if ((symbol_offset >= symtab->entries[mid].offset) &&
-		    (symbol_offset < symtab->entries[mid + 1].offset)) {
-			ret_offset = symbol_offset - symtab->entries[mid].offset;
-			ret_name = symtab->entries[mid].name;
-			break;
-		} else if (symbol_offset < symtab->entries[mid].offset) {
-			right = mid - 1;
-		} else {
-			left = mid + 1;
+			if ((symbol_offset >= symtab->entries[mid].offset) &&
+			(symbol_offset < symtab->entries[mid + 1].offset)) {
+				ret_offset = symbol_offset - symtab->entries[mid].offset;
+				ret_name = symtab->entries[mid].name;
+				break;
+			} else if (symbol_offset < symtab->entries[mid].offset) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
 		}
 	}
 
