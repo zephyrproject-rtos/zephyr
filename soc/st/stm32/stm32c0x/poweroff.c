ffe6f934920db980e2b3ce8a66e8f581fc731986
Romain Pelletant <romainp@kickmaker.net>
2024-05-27 23:22:14 +0200

    soc: stm32c0: add poweroff mode
    Add poweroff mode support for STM32C0
    Fixes #73371
    
    Signed-off-by: Romain Pelletant <romainp@kickmaker.net>

---
+++ soc/st/stm32/stm32c0x/poweroff.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2024 Kickmaker
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/poweroff.h>
+#include <zephyr/toolchain.h>
+
+#include <stm32_ll_cortex.h>
+#include <stm32_ll_pwr.h>
+
+void z_sys_poweroff(void)
+{
+	LL_PWR_SetPowerMode(LL_PWR_MODE_SHUTDOWN);
+	LL_LPM_EnableDeepSleep();
+
+	k_cpu_idle();
+
+	CODE_UNREACHABLE;
+}
