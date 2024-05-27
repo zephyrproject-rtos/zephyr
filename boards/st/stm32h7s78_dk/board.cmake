89a6179ab659ba48531aa34845cdffdad0ec4508
Francois Ramu <francois.ramu@st.com>
2024-05-27 11:18:52 +0200

    boards: arm: stm32h7s78 disco kit for XiP on external flash
    Add the memory attributes to XiP in MemoryMapped mode on the
    external octo-flash of the stm32h7s78_dk board.
    Use the STM32Cube programmer to flash with the corresponding
    external loader for XiP mode.
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- boards/st/stm32h7s78_dk/board.cmake
+++ boards/st/stm32h7s78_dk/board.cmake
@@ -2,6 +2,10 @@
 
 # keep first
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+if(CONFIG_XIP AND (CONFIG_STM32_MEMMAP OR CONFIG_BOOTLOADER_MCUBOOT))
+  board_runner_args(stm32cubeprogrammer "--extload=MX66UW1G45G_STM32H7S78-DK.stldr")
+endif()
+
 board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
 
 # keep first
