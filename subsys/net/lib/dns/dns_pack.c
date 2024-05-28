c6559af8a3a5c49ab5c84ede9ab8f06e67959e45
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2024-05-28 16:40:12 +0300

    net: dns: Add ANY query resource type
    Don't give an error for ANY type record.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- subsys/net/lib/dns/dns_pack.c
+++ subsys/net/lib/dns/dns_pack.c
@@ -560,7 +560,8 @@ int dns_unpack_query(struct dns_msg_t *dns_msg, struct net_buf *buf,
 	if (query_type != DNS_RR_TYPE_A && query_type != DNS_RR_TYPE_AAAA
 		&& query_type != DNS_RR_TYPE_PTR
 		&& query_type != DNS_RR_TYPE_SRV
-		&& query_type != DNS_RR_TYPE_TXT) {
+		&& query_type != DNS_RR_TYPE_TXT
+		&& query_type != DNS_RR_TYPE_ANY) {
 		return -EINVAL;
 	}
 
