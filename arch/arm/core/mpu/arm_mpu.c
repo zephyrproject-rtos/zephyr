ffbd9070d15623137f894f211a6b0b6d1063ec93
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2024-05-29 14:26:29 +0200

    [nrf fromlist] arch: arm: core: mpu: allow non-ARM memory attributes
    Memory region defined in devicetree can have attributes
    that are not intended to be parsed by MPU library,
    but might be valid for other components.
    
    Upstream PR: https://github.com/zephyrproject-rtos/zephyr/pull/73545
    
    Signed-off-by: Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
    (cherry picked from commit b5d3b1fe2f16548a99854261881d165c91ac323f)

--- arch/arm/core/mpu/arm_mpu.c
+++ arch/arm/core/mpu/arm_mpu.c
@@ -130,12 +130,10 @@ static int mpu_configure_regions_from_dt(uint8_t *reg_index)
 			break;
 #endif
 		default:
-			/* Either the specified `ATTR_MPU_*` attribute does not
-			 * exists or the `REGION_*_ATTR` macro is not defined
-			 * for that attribute.
+			/* Attribute other than ARM-specific is set.
+			 * This region should not be configured in MPU.
 			 */
-			LOG_ERR("Invalid attribute for the region\n");
-			return -EINVAL;
+			continue;
 		}
 #if defined(CONFIG_ARMV7_R)
 		region_conf.size = size_to_mpu_rasr_size(region[idx].dt_size);
