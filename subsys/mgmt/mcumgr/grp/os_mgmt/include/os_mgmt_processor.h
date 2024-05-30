5bf240b956facfe2c3a35742b72f2ff7d1400b9e
Alp Sayin <alpsayin@gmail.com>
2024-05-30 11:22:43 +0100

    tests: microblaze: test specific configuration additions for MicroBlaze
    Internal references: FWRIVERHD-5201
    
    Signed-off-by: Alp Sayin <alpsayin@gmail.com>

--- subsys/mgmt/mcumgr/grp/os_mgmt/include/os_mgmt_processor.h
+++ subsys/mgmt/mcumgr/grp/os_mgmt/include/os_mgmt_processor.h
@@ -154,6 +154,8 @@ extern "C" {
 #define PROCESSOR_NAME "xtensa"
 #elif defined(CONFIG_SPARC)
 #define PROCESSOR_NAME "sparc"
+#elif defined(CONFIG_MICROBLAZE)
+#define PROCESSOR_NAME "microblaze"
 #endif
 
 #ifndef PROCESSOR_NAME
