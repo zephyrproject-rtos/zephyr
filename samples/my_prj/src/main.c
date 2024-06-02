863a170114428042f21d3f294967b5ecb588de2b
alien <ali.sarlak@protonmail.com>
2024-06-02 05:57:36 +0000

    adding support for artemis nano

---
+++ samples/my_prj/src/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Special application!!!! %s\n", CONFIG_BOARD_TARGET);
+	return 0;
+}
