af752de85584e3ac163010443ea407fb5cfc70b0
Seppo Takalo <seppo.takalo@nordicsemi.no>
2024-06-03 11:20:55 +0300

    [nrf fromtree] net: lwm2m: Block-Wise response NUM field fix
    When calculating the offset for blockwise writes,
    we should not advance the block_ctx->current field
    past the block boundary.
    It causes CoAP layer to reply with the next NUM field
    instead of the current one being processed.
    
    Signed-off-by: Seppo Takalo <seppo.takalo@nordicsemi.no>
    (cherry picked from commit 9e615429d5dfc37feec87fd512b26a804b16fe36)
    (cherry picked from commit 2980109b8eda847e22561b65544ff35bdb28d7df)

--- subsys/net/lib/lwm2m/lwm2m_message_handling.c
+++ subsys/net/lib/lwm2m/lwm2m_message_handling.c
@@ -1004,7 +1004,7 @@ static int lwm2m_write_handler_opaque(struct lwm2m_engine_obj_inst *obj_inst,
 				return ret;
 			}
 		}
-		if (msg->in.block_ctx) {
+		if (msg->in.block_ctx && !last_pkt_block) {
 			msg->in.block_ctx->ctx.current += len;
 		}
 	}
