d5ff0cc82062e374287d37610a92330396198143
frei tycho <tfrei@baumer.com>
2024-06-03 15:04:51 +0000

    include: sys: added missing parenthesis
    - added missing parenthesis around macro argument expansion
    
    Signed-off-by: frei tycho <tfrei@baumer.com>

--- include/zephyr/sys/util.h
+++ include/zephyr/sys/util.h
@@ -789,7 +789,7 @@ static inline void mem_xor_128(uint8_t dst[16], const uint8_t src1[16], const ui
 /* This is used in linker scripts so need to avoid type casting there */
 #define KB(x) ((x) << 10)
 #else
-#define KB(x) (((size_t)x) << 10)
+#define KB(x) (((size_t)(x)) << 10)
 #endif
 /** @brief Number of bytes in @p x mebibytes */
 #define MB(x) (KB(x) << 10)
