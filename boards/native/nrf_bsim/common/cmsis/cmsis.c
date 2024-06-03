ec0cad597b4b3ea8bdf13b7b945c713e3221511d
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2024-06-03 11:08:23 +0200

    [nrf fromtree] boards nrf_bsim: Add NVIC_GetEnableIRQ()
    Provide a replacement for CMSIS' NVIC_GetEnableIRQ()
    as some applications use it.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
    (cherry picked from commit 68adc77cbdb168838e76a63412dd1fb71d7d1c57)

--- boards/native/nrf_bsim/common/cmsis/cmsis.c
+++ boards/native/nrf_bsim/common/cmsis/cmsis.c
@@ -35,6 +35,11 @@ void NVIC_EnableIRQ(IRQn_Type IRQn)
 	hw_irq_ctrl_enable_irq(CONFIG_NATIVE_SIMULATOR_MCU_N, IRQn);
 }
 
+uint32_t NVIC_GetEnableIRQ(IRQn_Type IRQn)
+{
+	return hw_irq_ctrl_is_irq_enabled(CONFIG_NATIVE_SIMULATOR_MCU_N, IRQn);
+}
+
 void NVIC_SetPriority(IRQn_Type IRQn, uint32_t priority)
 {
 	hw_irq_ctrl_prio_set(CONFIG_NATIVE_SIMULATOR_MCU_N, IRQn, priority);
