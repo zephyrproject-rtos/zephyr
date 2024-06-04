ae3af8bddc7cfc81acfcb8db9e5721197d4d89bb
Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>
2024-06-04 09:07:46 +0200

    task_wdt: Feed hardware watchdog only when its started
    Previously the schedule_next_timeout() function was feeding the hardware
    watchdog irrespective of whether or not it was started. This is now
    fixed.
    
    Signed-off-by: Balaji Srinivasan <balaji.srinivasan@nordicsemi.no>

--- subsys/task_wdt/task_wdt.c
+++ subsys/task_wdt/task_wdt.c
@@ -79,7 +79,7 @@ static void schedule_next_timeout(int64_t current_ticks)
 	k_timer_start(&timer, K_TIMEOUT_ABS_TICKS(next_timeout), K_FOREVER);
 
 #ifdef CONFIG_TASK_WDT_HW_FALLBACK
-	if (hw_wdt_dev) {
+	if (hw_wdt_started) {
 		wdt_feed(hw_wdt_dev, hw_wdt_channel);
 	}
 #endif
