28068bba2a1362fd6d3cec8d3618f1d14ea67266
Chris Friedt <cfriedt@tenstorrent.com>
2024-06-08 08:39:56 -0400

    posix: fs: implement fstat() via zvfs_fstat()
    Route the fstat() call (part of POSIX_FILE_SYSTEM) to
    zvfs_fstat() so that other types of file descriptors can also
    supply file status information.
    
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

--- include/zephyr/sys/fdtable.h
+++ include/zephyr/sys/fdtable.h
@@ -166,6 +166,7 @@ enum {
 	ZFD_IOCTL_POLL_UPDATE,
 	ZFD_IOCTL_POLL_OFFLOAD,
 	ZFD_IOCTL_SET_LOCK,
+	ZFD_IOCTL_STAT,
 
 	/* Codes above 0x5400 and below 0x5500 are reserved for termios, FIO, etc */
 	ZFD_IOCTL_FIONREAD = 0x541B,
