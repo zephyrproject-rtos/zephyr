d1a1fbaccfed1021e372ff39013e78fee7199676
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2024-06-10 22:12:56 +0200

    Bluetooth: Controller: nRF54Lx: Use NRF_GRTC for radio scheduling
    Use NRF_GRTC, Global real-time counter, instead of NRF_RTC.
    
    NRF_GRTC is present in a different power domain in the SoC.
    Also, NRF_GRTC provide microsecond resolution timing units,
    in the future use of NRF_TIMER for packet timer can also be
    replaced.
    
    Use of NRF_RTC would keep Radio power domain ON in addition
    to already ON NRF_GRTC's power domain, hence switch to using
    NRF_GRTC on nRF54Lx SoC to have lower power consumptions.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll.c
@@ -8,8 +8,6 @@
 #include <stdbool.h>
 #include <errno.h>
 
-#include <hal/nrf_rtc.h>
-
 #include <zephyr/toolchain.h>
 
 #include <soc.h>
@@ -120,8 +118,13 @@ static void rtc0_nrf5_isr(const void *arg)
 	lll_prof_enter_ull_high();
 
 	/* On compare0 run ticker worker instance0 */
+#if defined(CONFIG_BT_CTLR_NRF_GRTC)
+	if (NRF_GRTC->EVENTS_COMPARE[10]) {
+		nrf_grtc_event_clear(NRF_GRTC, NRF_GRTC_EVENT_COMPARE_10);
+#else /* !CONFIG_BT_CTLR_NRF_GRTC */
 	if (NRF_RTC->EVENTS_COMPARE[0]) {
 		nrf_rtc_event_clear(NRF_RTC, NRF_RTC_EVENT_COMPARE_0);
+#endif  /* !CONFIG_BT_CTLR_NRF_GRTC */
 
 		ticker_trigger(0);
 	}
