fd39fab3d5d11a592578c528446283b712d85c9c
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2024-06-10 17:04:10 +0200

    ci: bsim tests: Move CI steps to separate scripts
    So they can be easily run locally by users.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

---
+++ tests/bsim/ci.uart.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Copyright 2024 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+# This script runs the Babblesim CI UART tests.
+# It can also be run locally.
+# Note it will produce its output in ${ZEPHYR_BASE}/bsim_out/
+
+export ZEPHYR_BASE="${ZEPHYR_BASE:-${PWD}}"
+cd ${ZEPHYR_BASE}
+
+set -uex
+
+echo "UART: Single device tests"
+${ZEPHYR_BASE}/scripts/twister -T tests/drivers/uart/ --force-color --inline-logs -v -M \
+  -p nrf52_bsim --fixture gpio_loopback -- -uart0_loopback
+
+echo "UART: Multi device tests"
+WORK_DIR=${ZEPHYR_BASE}/bsim_uart nice tests/bsim/drivers/uart/compile.sh
+RESULTS_FILE=${ZEPHYR_BASE}/bsim_out/bsim_results.uart.52.xml \
+SEARCH_PATH=tests/bsim/drivers/uart/ tests/bsim/run_parallel.sh
