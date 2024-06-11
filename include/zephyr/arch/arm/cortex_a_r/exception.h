605a5e151638c5a03f2cd7c9e8404d9fcff49b3f
Emil Gydesen <emil.gydesen@nordicsemi.no>
2024-06-11 17:44:13 +0200

    arch: arm: Fix missing include for sys_define_gpr_with_alias
    The sys_define_gpr_with_alias macro was defined by arch.h,
    but was used by the two exception.h files that did not include it.
    
    Attempting to include arch.h from exception.h would resolve
    in a recursive include, so the macro was simply moved to the
    files that used it.
    
    An alternative could have been to define a new file that defined it,
    that could have been included by exception.h
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- include/zephyr/arch/arm/cortex_a_r/exception.h
+++ include/zephyr/arch/arm/cortex_a_r/exception.h
@@ -54,6 +54,9 @@ struct __extra_esf_info {
 };
 #endif /* CONFIG_EXTRA_EXCEPTION_INFO */
 
+/* ARM GPRs are often designated by two different names */
+#define sys_define_gpr_with_alias(name1, name2) union { uint32_t name1, name2; }
+
 struct arch_esf {
 #if defined(CONFIG_EXTRA_EXCEPTION_INFO)
 	struct __extra_esf_info extra_info;
