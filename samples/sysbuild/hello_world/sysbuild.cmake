e8a7c177a973b7e3380ba5eb920f08cd66a211c1
Gerard Marull-Paretas <gerard@teslabs.com>
2024-06-12 16:15:29 +0200

    [nrf noup] samples: sysbuild: hello_world: support PM on nRF53
    PM support is still required for nRF53 in the context of NCS.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>
    (cherry picked from commit b42e02dfde50c7fec35eabaf69f68eaad107fa08)

--- samples/sysbuild/hello_world/sysbuild.cmake
+++ samples/sysbuild/hello_world/sysbuild.cmake
@@ -11,5 +11,12 @@ ExternalZephyrProject_Add(
   BOARD ${SB_CONFIG_REMOTE_BOARD}
 )
 
+if(SB_CONFIG_SOC_SERIES_NRF53X)
+  set_property(GLOBAL APPEND PROPERTY PM_DOMAINS CPUNET)
+  set_property(GLOBAL APPEND PROPERTY PM_CPUNET_IMAGES remote)
+  set_property(GLOBAL PROPERTY DOMAIN_APP_CPUNET remote)
+  set(CPUNET_PM_DOMAIN_DYNAMIC_PARTITION remote CACHE INTERNAL "")
+endif()
+
 add_dependencies(hello_world remote)
 sysbuild_add_dependencies(FLASH hello_world remote)
