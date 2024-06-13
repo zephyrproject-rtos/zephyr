2d32c0e47dac3ba17b5388d5376ba76b280031b6
Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>
2024-06-13 19:06:19 +0300

    Drivers: DAI: Intel: Reduce traces dai_dmic_start()
    This patch removes the traces printed inside spinlock protected
    code. The DMIC capture start code is timing critical. Traces
    leave an unknown delay into start sequence, so it's better to not
    produce them.
    
    Signed-off-by: Seppo Ingalsuo <seppo.ingalsuo@linux.intel.com>

--- drivers/dai/intel/dmic/dmic.c
+++ drivers/dai/intel/dmic/dmic.c
@@ -553,7 +553,6 @@ static void dai_dmic_start(struct dai_intel_dmic *dmic)
 
 	/* enable port */
 	key = k_spin_lock(&dmic->lock);
-	LOG_DBG("dmic_start(), dai_index = %d", dmic->dai_config_params.dai_index);
 	dmic->startcount = 0;
 
 	/* Compute unmute ramp gain update coefficient. */
@@ -570,15 +569,11 @@ static void dai_dmic_start(struct dai_intel_dmic *dmic)
 #ifdef CONFIG_SOC_SERIES_INTEL_ADSP_ACE
 		dai_dmic_update_bits(dmic, dmic_base[i] + CIC_CONTROL,
 				     CIC_CONTROL_SOFT_RESET, 0);
-
-		LOG_INF("dmic_start(), cic 0x%08x",
-			dai_dmic_read(dmic, dmic_base[i] + CIC_CONTROL));
 #endif
 
 		mic_a = dmic->enable[i] & 1;
 		mic_b = (dmic->enable[i] & 2) >> 1;
 		start_fir = dmic->enable[i] > 0;
-		LOG_INF("dmic_start(), pdm%d mic_a = %u, mic_b = %u", i, mic_a, mic_b);
 
 		/* If both microphones are needed start them simultaneously
 		 * to start them in sync. The reset may be cleared for another
