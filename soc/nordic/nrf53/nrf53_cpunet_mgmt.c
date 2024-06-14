7bb99f9305a1ad53e148f717d936c579cf1c7d9e
Gerard Marull-Paretas <gerard@teslabs.com>
2024-06-14 11:10:52 +0200

    soc: nordic: nrf53: add option to enable cpunet
    When not using BT, users may want to enable the cpunet core. Until now,
    this has been done at board level (so duplicating unnecessary code)
    using CONFIG_BOARD_ENABLE_CPUNET. The board-level options were usually
    enabled automatically for BT, however, this was unnecessary as BT driver
    already takes care of the setup.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- soc/nordic/nrf53/nrf53_cpunet_mgmt.c
+++ soc/nordic/nrf53/nrf53_cpunet_mgmt.c
@@ -17,6 +17,7 @@
 #include <zephyr/sys/onoff.h>
 
 #include <hal/nrf_reset.h>
+#include <hal/nrf_spu.h>
 
 static struct onoff_manager cpunet_mgr;
 
@@ -66,3 +67,30 @@ void nrf53_cpunet_enable(bool on)
 
 	__ASSERT_NO_MSG(ret >= 0);
 }
+
+#ifdef CONFIG_SOC_NRF53_CPUNET_ENABLE
+static int nrf53_cpunet_init(void)
+{
+#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
+	/* Retain nRF5340 Network MCU in Secure domain (bus
+	 * accesses by Network MCU will have Secure attribute set).
+	 */
+	nrf_spu_extdomain_set((NRF_SPU_Type *)DT_REG_ADDR(DT_NODELABEL(spu)), 0, true, false);
+#endif /* !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE) */
+
+#if !defined(CONFIG_TRUSTED_EXECUTION_SECURE)
+	/*
+	 * Building Zephyr with CONFIG_TRUSTED_EXECUTION_SECURE=y implies
+	 * building also a Non-Secure image. The Non-Secure image will, in
+	 * this case do the remainder of actions to properly configure and
+	 * boot the Network MCU.
+	 */
+
+	nrf53_cpunet_enable(true);
+#endif /* !CONFIG_TRUSTED_EXECUTION_SECURE */
+
+	return 0;
+}
+
+SYS_INIT(nrf53_cpunet_init, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
+#endif /* CONFIG_SOC_NRF53_CPUNET_ENABLE */
