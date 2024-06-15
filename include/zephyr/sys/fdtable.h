49ac1912b29d858af6059901c2eb291e17e34a49
Chris Friedt <cfriedt@tenstorrent.com>
2024-06-15 10:04:55 -0400

    net: sockets: move select() implementation to zvfs
    Move the implementation of zsock_select() to zvfs_select(). This
    allows other types of file descriptors to also make use of
    select() functionality even when the network subsystem is not
    enabled.
    
    Additionally, it partially removes a dependency cycle between
    posix and networking by moving functionality into a mutual
    dependency.
    
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

--- include/zephyr/sys/fdtable.h
+++ include/zephyr/sys/fdtable.h
@@ -8,6 +8,7 @@
 
 #include <stdarg.h>
 #include <sys/types.h>
+
 /* FIXME: For native_posix ssize_t, off_t. */
 #include <zephyr/fs/fs.h>
 #include <zephyr/sys/mutex.h>
@@ -207,9 +208,23 @@ struct zvfs_pollfd {
 
 __syscall int zvfs_poll(struct zvfs_pollfd *fds, int nfds, int poll_timeout);
 
-struct zsock_fd_set {
+struct zvfs_fd_set {
 	uint32_t bitset[(CONFIG_ZVFS_OPEN_MAX + 31) / 32];
 };
+
+#define ZVFS_FD_SETSIZE (sizeof(((struct zvfs_fd_set *)0)->bitset) * 8)
+
+void ZVFS_FD_CLR(int fd, struct zvfs_fd_set *fdset);
+int ZVFS_FD_ISSET(int fd, struct zvfs_fd_set *fdset);
+void ZVFS_FD_SET(int fd, struct zvfs_fd_set *fdset);
+void ZVFS_FD_ZERO(struct zvfs_fd_set *fdset);
+
+struct timespec;
+__syscall int zvfs_select(int nfds, struct zvfs_fd_set *ZRESTRICT readfds,
+			  struct zvfs_fd_set *ZRESTRICT writefds,
+			  struct zvfs_fd_set *ZRESTRICT errorfds,
+			  const struct timeval *ZRESTRICT timeout);
+
 /**
  * Request codes for fd_op_vtable.ioctl().
  *
@@ -238,5 +253,7 @@ enum {
 #ifdef __cplusplus
 }
 #endif
+
+#include <zephyr/syscalls/fdtable.h>
 
 #endif /* ZEPHYR_INCLUDE_SYS_FDTABLE_H_ */
