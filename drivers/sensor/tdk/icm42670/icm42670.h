ca31dbc0bc883e3245518c82a713ce7583276d77
Lucas Tamborrino <lucas.tamborrino@espressif.com>
2024-06-17 08:57:15 -0300

    drivers: sensor: icm42670: Add I2C bus support
    Add bus interface so the driver can support both
    SPI and I2C bus.
    
    Signed-off-by: Lucas Tamborrino <lucas.tamborrino@espressif.com>

--- drivers/sensor/tdk/icm42670/icm42670.h
+++ drivers/sensor/tdk/icm42670/icm42670.h
@@ -11,7 +11,44 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/spi.h>
+#include <zephyr/drivers/i2c.h>
 #include <zephyr/kernel.h>
+
+#define ICM42670_BUS_SPI DT_HAS_COMPAT_ON_BUS_STATUS_OKAY(invensense_icm42670, spi)
+#define ICM42670_BUS_I2C DT_HAS_COMPAT_ON_BUS_STATUS_OKAY(invensense_icm42670, i2c)
+
+union icm42670_bus {
+#if ICM42670_BUS_SPI
+	struct spi_dt_spec spi;
+#endif
+#if ICM42670_BUS_I2C
+	struct i2c_dt_spec i2c;
+#endif
+};
+
+typedef int (*icm42670_bus_check_fn)(const union icm42670_bus *bus);
+typedef int (*icm42670_reg_read_fn)(const union icm42670_bus *bus,
+				  uint16_t reg, uint8_t *data, size_t size);
+typedef int (*icm42670_reg_write_fn)(const union icm42670_bus *bus,
+				   uint16_t reg, uint8_t data);
+
+typedef int (*icm42670_reg_update_fn)(const union icm42670_bus *bus,
+				   uint16_t reg, uint8_t mask, uint8_t data);
+
+struct icm42670_bus_io {
+	icm42670_bus_check_fn check;
+	icm42670_reg_read_fn read;
+	icm42670_reg_write_fn write;
+	icm42670_reg_update_fn update;
+};
+
+#if ICM42670_BUS_SPI
+extern const struct icm42670_bus_io icm42670_bus_io_spi;
+#endif
+
+#if ICM42670_BUS_I2C
+extern const struct icm42670_bus_io icm42670_bus_io_i2c;
+#endif
 
 struct icm42670_data {
 	int16_t accel_x;
@@ -45,7 +82,8 @@ struct icm42670_data {
 };
 
 struct icm42670_config {
-	struct spi_dt_spec spi;
+	union icm42670_bus bus;
+	const struct icm42670_bus_io *bus_io;
 	struct gpio_dt_spec gpio_int;
 };
 
