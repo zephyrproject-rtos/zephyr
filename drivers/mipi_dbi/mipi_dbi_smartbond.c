d5332532e5299c4a87dd3f43f07262397dcccc5b
Ioannis Karachalios <ioannis.karachalios.px@renesas.com>
2024-06-20 17:34:28 +0300

    drivers: mipi_dbi: smartbond: Unused symbol
    Suppress warning thrown when
    the reset line is not used.
    
    Signed-off-by: Ioannis Karachalios <ioannis.karachalios.px@renesas.com>

--- drivers/mipi_dbi/mipi_dbi_smartbond.c
+++ drivers/mipi_dbi/mipi_dbi_smartbond.c
@@ -509,7 +509,7 @@ static int mipi_dbi_smartbond_pm_action(const struct device *dev, enum pm_device
 
 static int mipi_dbi_smartbond_init(const struct device *dev)
 {
-	const struct mipi_dbi_smartbond_config *config = dev->config;
+	__unused const struct mipi_dbi_smartbond_config *config = dev->config;
 	struct mipi_dbi_smartbond_data *data = dev->data;
 	int ret;
 
