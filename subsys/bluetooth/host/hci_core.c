3b726dee5309201cae59a8e5b71aaf774b053451
Ali Hozhabri <ali.hozhabri@st.com>
2024-06-20 14:52:45 +0200

    Bluetooth: Host: Fix HCI command timeout usage
    Fix Bluetooth initialization problem caused by PR#72090
    for at least ST boards that are using BlueNRG BLE modules.
    
    For more information, please refer to issue #74528.
    
    Signed-off-by: Ali Hozhabri <ali.hozhabri@st.com>

--- subsys/bluetooth/host/hci_core.c
+++ subsys/bluetooth/host/hci_core.c
@@ -4634,7 +4634,7 @@ int bt_configure_data_path(uint8_t dir, uint8_t id, uint8_t vs_config_len,
 static bool process_pending_cmd(k_timeout_t timeout)
 {
 	if (!k_fifo_is_empty(&bt_dev.cmd_tx_queue)) {
-		if (k_sem_take(&bt_dev.ncmd_sem, K_NO_WAIT) == 0) {
+		if (k_sem_take(&bt_dev.ncmd_sem, timeout) == 0) {
 			hci_core_send_cmd();
 			return true;
 		}
