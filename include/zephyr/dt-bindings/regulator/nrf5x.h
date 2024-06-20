50b0bbf51d873def07fc8fb147692bdddf9fb666
Gerard Marull-Paretas <gerard@teslabs.com>
2024-06-20 10:11:16 +0200

    [nrf fromtree] dts: bindings: regulator: add nordic,nrf5[2]x-regulator[-hv]
    Some Nordic SoCs, like nRF52 contain an internal regulator for the main
    SoC supply. Depending on the SoC, the regulator can have multiple
    configurations (e.g. single/double stage), which mainly depends on
    supporting "high-voltage" mode or not (VDDH pin supply). This patch adds
    bindings for nRF5X regulator and nRF52X HV regulator.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>
    (cherry picked from commit 02a30c1c2bdce312c7f99dd08756e9d99e3a7e17)

---
+++ include/zephyr/dt-bindings/regulator/nrf5x.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_REGULATOR_NRF5X_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_REGULATOR_NRF_H_
+
+/**
+ * @defgroup regulator_nrf5x nRF5X regulator devicetree helpers.
+ * @ingroup regulator_interface
+ * @{
+ */
+
+/**
+ * @name nRF5X regulator modes
+ * @{
+ */
+/** LDO mode */
+#define NRF5X_REG_MODE_LDO 0
+/** DC/DC mode */
+#define NRF5X_REG_MODE_DCDC 1
+/** @} */
+
+/** @} */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_REGULATOR_NRF5X_H_*/
