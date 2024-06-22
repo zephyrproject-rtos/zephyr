81952e31f4229bc7049a1023854a53db48ebdf49
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2024-06-22 16:19:42 +0200

    tests: drivers: spi: loopback: Configurable large buffer
    In order to allow fiting test in targets with smaller memory make
    large buffer configurable through Kconfig.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- tests/drivers/spi/spi_loopback/src/spi.c
+++ tests/drivers/spi/spi_loopback/src/spi.c
@@ -58,7 +58,7 @@ static struct spi_dt_spec spi_slow = SPI_DT_SPEC_GET(SPI_SLOW_DEV, SPI_OP(FRAME_
 #define STACK_SIZE (512 + CONFIG_TEST_EXTRA_STACK_SIZE)
 #define BUF_SIZE 18
 #define BUF2_SIZE 36
-#define BUF3_SIZE 8192
+#define BUF3_SIZE CONFIG_SPI_LARGE_BUFFER_SIZE
 
 #if CONFIG_NOCACHE_MEMORY
 #define __NOCACHE	__attribute__((__section__(".nocache")))
