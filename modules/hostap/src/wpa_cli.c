76923b0d464607aba88a727f980fec033243614b
Fengming Ye <frank.ye@nxp.com>
2024-06-24 12:08:32 +0900

    [nxp noup] hostap: add hostapd cli shell command
    Add hostapd_cli shell command for test.
    
    Signed-off-by: Fengming Ye <frank.ye@nxp.com>

--- modules/hostap/src/wpa_cli.c
+++ modules/hostap/src/wpa_cli.c
@@ -13,6 +13,9 @@
 #include <zephyr/init.h>
 
 #include "wpa_cli_zephyr.h"
+#ifdef CONFIG_WIFI_NM_HOSTAPD_AP
+#include "hostapd_cli_zephyr.h"
+#endif
 
 static int cmd_wpa_cli(const struct shell *sh,
 			  size_t argc,
@@ -32,6 +35,26 @@ static int cmd_wpa_cli(const struct shell *sh,
 	return zephyr_wpa_ctrl_zephyr_cmd(argc - 1, &argv[1]);
 }
 
+#ifdef CONFIG_WIFI_NM_HOSTAPD_AP
+static int cmd_hostapd_cli(const struct shell *sh,
+			   size_t argc,
+			   const char *argv[])
+{
+	ARG_UNUSED(sh);
+
+	if (argc == 1) {
+		shell_error(sh, "Missing argument");
+		return -EINVAL;
+	}
+
+	argv[argc] = "interactive";
+	argc++;
+
+	/* Remove hostapd_cli from the argument list */
+	return zephyr_hostapd_ctrl_zephyr_cmd(argc - 1, &argv[1]);
+}
+#endif
+
 /* Persisting with "wpa_cli" naming for compatibility with Wi-Fi
  * certification applications and scripts.
  */
@@ -39,3 +62,9 @@ SHELL_CMD_REGISTER(wpa_cli,
 		   NULL,
 		   "wpa_cli commands (only for internal use)",
 		   cmd_wpa_cli);
+#ifdef CONFIG_WIFI_NM_HOSTAPD_AP
+SHELL_CMD_REGISTER(hostapd_cli,
+		   NULL,
+		   "hostapd_cli commands (only for internal use)",
+		   cmd_hostapd_cli);
+#endif
