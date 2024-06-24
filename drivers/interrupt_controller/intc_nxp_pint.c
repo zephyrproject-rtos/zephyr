405ab182a9749501f22df4455f9f2fbc1189adc1
Vit Stanicek <vit.stanicek@nxp.com>
2024-06-24 13:19:08 +0200

    drivers: intc_nxp_pint: Decouple from fsl_power.h
    Add drivers/interrupt_controller/intc_nxp_pint/power.h abstracting
    EnableDeepSleepIRQ and DisableDeepSleepIRQ invocations from
    intc_nxp_pint.c. Modify intc_nxp_pint.c to use that file.
    
    fsl_power.c and fsl_power.h can't be built on the
    mimxrt685_evk/mimxrt685s/hifi4 target, so it's excluded from it in hal_nxp.
    
    Signed-off-by: Vit Stanicek <vit.stanicek@nxp.com>

--- drivers/interrupt_controller/intc_nxp_pint.c
+++ drivers/interrupt_controller/intc_nxp_pint.c
@@ -12,7 +12,8 @@
 #include <zephyr/drivers/interrupt_controller/nxp_pint.h>
 
 #include <fsl_inputmux.h>
-#include <fsl_power.h>
+
+#include "intc_nxp_pint/power.h"
 
 #define DT_DRV_COMPAT nxp_pint
 
@@ -94,14 +95,8 @@ int nxp_pint_pin_enable(uint8_t pin, enum nxp_pint_trigger trigger, bool wake)
 	 * driver handles the IRQ
 	 */
 	PINT_PinInterruptConfig(pint_base, slot, trigger, NULL);
-#if !(defined(FSL_FEATURE_POWERLIB_EXTEND) && (FSL_FEATURE_POWERLIB_EXTEND != 0))
-	if (wake) {
-		EnableDeepSleepIRQ(pint_irq_cfg[slot].irq);
-	} else {
-		DisableDeepSleepIRQ(pint_irq_cfg[slot].irq);
-		irq_enable(pint_irq_cfg[slot].irq);
-	}
-#endif
+	nxp_pint_pin_deep_sleep_irq(pint_irq_cfg[slot].irq, wake);
+
 	return 0;
 }
 
