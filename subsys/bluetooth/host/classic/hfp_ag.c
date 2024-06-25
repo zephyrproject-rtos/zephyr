45af7e8cd22259c3b3deb7b15a9dbe9d51a66a99
Lyle Zhu <lyle.zhu@nxp.com>
2024-06-25 12:30:24 +0800

    Bluetooth: Classic: HF_AG: Fix incorrect status judgment
    Wrong condition of AG status is used for status
    checking.
    
    Fixes #74726.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- subsys/bluetooth/host/classic/hfp_ag.c
+++ subsys/bluetooth/host/classic/hfp_ag.c
@@ -2241,7 +2241,7 @@ int bt_hfp_ag_remote_terminate(struct bt_hfp_ag *ag)
 		return -ENOTCONN;
 	}
 
-	if ((ag->call_state != BT_HFP_CALL_ACTIVE) || (ag->call_state != BT_HFP_CALL_HOLD)) {
+	if ((ag->call_state != BT_HFP_CALL_ACTIVE) && (ag->call_state != BT_HFP_CALL_HOLD)) {
 		hfp_ag_unlock(ag);
 		return -EINVAL;
 	}
