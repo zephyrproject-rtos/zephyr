5b422b0859d25603860dfd77d22666d7e110138c
alien <ali.sarlak@protonmail.com>
2024-06-26 22:52:47 +0000

    update document

--- boards/sparkfun/artemis_nano/board.cmake
+++ boards/sparkfun/artemis_nano/board.cmake
@@ -1,18 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
 if(BOARD STREQUAL "artemis_nano")
     board_runner_args(sp_artemis "--baud-rate=115200" "--serial-port=/dev/ttyUSB0")
     include(${ZEPHYR_BASE}/boards/common/sp_artemis.board.cmake)
 endif()
->>>>>>> d737658ec14 (before modification for west flash)
->>>>>>> a53dd08c5d9 (fixing the west flash)
-
-board_runner_args(pyocd "--target=cortex_m" "--frequency=4000000" "--probe=/dev/ttyUSB0")
-
-include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/trace32.board.cmake)
