c4922d55f692910cc8b577cd52deb5f32c6fe2e9
Umamaheswaran <maheswaran.sg@gmail.com>
2024-06-26 13:45:27 +0530

    IMU_RTC RTC alone integrated step count problem

--- samples/IMU_RTC_Integration/src/main.c
+++ samples/IMU_RTC_Integration/src/main.c
@@ -16,6 +16,8 @@
 
 #define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
 #include <zephyr/logging/log.h>
+#include "RTC.h"
+#include "acc_gyro.h"
 LOG_MODULE_REGISTER(app);
 
 lv_ui guider_ui;
@@ -35,7 +37,8 @@ void update_time_in_screen(lv_ui *ui,Time const *time,Date const *date);
 void update_stepcount_in_screen(lv_ui *ui,uint32_t steps);
 
 
-
+Time time;
+Date date;
 char num_str[5];
 void num_to_string(char *str,uint16_t num)
 {
@@ -77,20 +80,43 @@ void update_stepcount_in_screen(lv_ui *ui,uint32_t steps)
 }
 int main(void)
 {
+	uint16_t steps;
 	const struct device *display_dev;
 	uint16_t step = 10;
+    float ax, ay, az; // Variables to store accelerometer data (x, y, z)
+    float gx, gy, gz; // Variables to store gyroscope data (x, y, z)
+    float mx, my, mz; // Variables to store magnetometer data (x, y, z)
 	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+
 	if (!device_is_ready(display_dev)) {
 		LOG_ERR("Device not ready, aborting test");
 		return 0;
 	}
+	const struct device * dev = DEVICE_DT_GET(DT_NODELABEL(i2c0));
+    // configure the mpu9250 for accelerometer and gyroscope
+    mpu9250_config(ACC_GYRO);
+	// DS3231_getHours(dev,DS3231_ADDR);
 	setup_ui(&guider_ui);
    	events_init(&guider_ui);
-	// lv_task_handler();
+	  DS3231_setSeconds(dev,DS3231_ADDR,00);
+      DS3231_setHours(dev,DS3231_ADDR,1);
+      DS3231_setMinutes(dev,DS3231_ADDR,40);
+	  DS3231_setHourMode(dev,DS3231_ADDR,CLOCK_H24);
 	display_blanking_off(display_dev);
 	while (1) {
+		time.hour = DS3231_getHours(dev,DS3231_ADDR);;
+		time.minutes = DS3231_getMinutes(dev,DS3231_ADDR);
+		date.day =26;
+		date.month = 6 ;
+		date.year =24 ;
+		update_time_in_screen(&guider_ui,&time,&date);
+		accel_xyz(ACC_GYRO, &ax, &ay, &az);
+		steps = readStepDetection(ax, ay, az);
+		printf("Time %d : %d\n",time.hour,time.minutes);
+		printf("Steps : %d\n",steps);
+		update_stepcount_in_screen(&guider_ui,steps);
 		lv_task_handler();
-		update_stepcount_in_screen(&guider_ui,step);
 		// k_sleep(K_MSEC(10));
 	}
+
 }
