84329a55c8013ec3efff45e6af23382728f23f6e
Raffael Rostagno <raffael.rostagno@espressif.com>
2024-06-26 17:17:52 -0300

    irq: esp32: Interrupts priority configuration
    Allows configuring interrupts priority in the device tree.
    
    Signed-off-by: Raffael Rostagno <raffael.rostagno@espressif.com>

--- drivers/gpio/gpio_esp32.c
+++ drivers/gpio/gpio_esp32.c
@@ -468,8 +468,8 @@ static int gpio_esp32_init(const struct device *dev)
 	static bool isr_connected;
 
 	if (!isr_connected) {
-		esp_intr_alloc(DT_IRQN(DT_NODELABEL(gpio0)),
-			0,
+		esp_intr_alloc(DT_IRQ_BY_IDX(DT_NODELABEL(gpio0), 0, irq),
+			esp_intr_level_to_flags(DT_IRQ_BY_IDX(DT_NODELABEL(gpio0), 0, priority)),
 			(ISR_HANDLER)gpio_esp32_isr,
 			(void *)dev,
 			NULL);
