d95de447c2fc60e1e62095ca66de44a203dab0ef
Pieter De Gendt <pieter.degendt@basalte.be>
2024-06-27 16:35:11 +0200

    test: Apply change that clang-format should report
    This commit is a test to be catched by CI compliance.
    
    DO NOT MERGE
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -25,14 +25,12 @@ int main(void)
 	int ret;
 	bool led_state = true;
 
-	if (!gpio_is_ready_dt(&led)) {
+	if (!gpio_is_ready_dt(&led))
 		return 0;
-	}
 
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
+	if (ret < 0)
 		return 0;
-	}
 
 	while (1) {
 		ret = gpio_pin_toggle_dt(&led);
