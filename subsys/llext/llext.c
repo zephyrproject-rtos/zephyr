3c97bd486e5cbd6bb27499d9eae3160263d9fd67
Luca Burelli <l.burelli@arduino.cc>
2024-06-28 14:35:15 +0200

    llext: harmonize error codes
    This patch changes the error codes returned by the ELF subsystem to be
    more consistent with the standard error descriptions. In particular:
    
    - issues with the ELF file are now reported as -ENOEXEC;
    - valid but unsupported edge cases are reported as -ENOTSUP;
    - failures in searching for an entry are reported as -ENOENT.
    
    Signed-off-by: Luca Burelli <l.burelli@arduino.cc>

--- subsys/llext/llext.c
+++ subsys/llext/llext.c
@@ -215,7 +215,7 @@ int llext_call_fn(struct llext *ext, const char *sym_name)
 
 	fn = llext_find_sym(&ext->exp_tab, sym_name);
 	if (fn == NULL) {
-		return -EINVAL;
+		return -ENOENT;
 	}
 	fn();
 
