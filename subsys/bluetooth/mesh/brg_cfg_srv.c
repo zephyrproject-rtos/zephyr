e10708863007e8829ab0a68e8a9f540ecb235a10
Ingar Kulbrandstad <ingar.kulbrandstad@nordicsemi.no>
2024-06-28 13:37:44 +0200

    Bluetooth: Mesh: Bridge Configuration Client/Server API
    Adding documentation and function calles for the API's
    in Bridge Configuration Client model and Bridge
    Configuration Server model.
    
    Signed-off-by: Ingar Kulbrandstad <ingar.kulbrandstad@nordicsemi.no>

---
+++ subsys/bluetooth/mesh/brg_cfg_srv.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/bluetooth/mesh.h>
+
+#define LOG_LEVEL CONFIG_BT_MESH_MODEL_LOG_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(bt_mesh_brg_cfg_srv);
+
+const struct bt_mesh_model_op _bt_mesh_brg_cfg_srv_op[] = {
+	BT_MESH_MODEL_OP_END,
+};
+
+static int brg_cfg_srv_init(const struct bt_mesh_model *model)
+{
+	const struct bt_mesh_model *config_srv =
+		bt_mesh_model_find(bt_mesh_model_elem(model), BT_MESH_MODEL_ID_CFG_SRV);
+
+	if (config_srv == NULL) {
+		LOG_ERR("Bridge Configuration Server only allowed in primary element");
+		return -EINVAL;
+	}
+
+
+	bt_mesh_model_extend(model, config_srv);
+
+	return 0;
+}
+
+const struct bt_mesh_model_cb _bt_mesh_brg_cfg_srv_cb = {
+	.init = brg_cfg_srv_init,
+};
