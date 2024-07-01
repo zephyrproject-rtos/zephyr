8e85ac8c4f65926195af6ab37d9d433c65d6933a
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2024-07-01 12:07:33 +0200

    Revert "net: sockets: move select() implementation to zvfs"
    This reverts commit 49ac1912b29d858af6059901c2eb291e17e34a49.
    
    PR #73978 introduced a regression.
    Unfortunately this PR cannot be reverted without reverting also
    Let's revert both PRs to stabilize main again towards the 3.7 release.
    
    For more details on the issue see
    https://github.com/zephyrproject-rtos/zephyr/issues/75205
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- subsys/net/lib/sockets/sockets_select.c
+++ subsys/net/lib/sockets/sockets_select.c
@@ -4,12 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <stdbool.h>
-
 #include <zephyr/kernel.h>
 #include <zephyr/internal/syscall_handler.h>
 #include <zephyr/sys/math_extras.h>
 #include <zephyr/net/socket.h>
+#include "sockets_internal.h"
 
 /* Get size, in elements, of an array within a struct. */
 #define STRUCT_MEMBER_ARRAY_SIZE(type, field) ARRAY_SIZE(((type *)0)->field)
@@ -21,12 +20,7 @@
 	bit_mask = 1 << b_idx; \
 	}
 
-/** Number of file descriptors which can be added to zsock_fd_set */
-#define ZVFS_FD_SETSIZE (sizeof(((struct zvfs_fd_set *)0)->bitset) * 8)
-
-int zvfs_poll_internal(struct zvfs_pollfd *fds, int nfds, k_timeout_t timeout);
-
-void ZVFS_FD_ZERO(struct zvfs_fd_set *set)
+void ZSOCK_FD_ZERO(zsock_fd_set *set)
 {
 	int i;
 
@@ -35,11 +29,11 @@ void ZVFS_FD_ZERO(struct zvfs_fd_set *set)
 	}
 }
 
-int ZVFS_FD_ISSET(int fd, struct zvfs_fd_set *set)
+int ZSOCK_FD_ISSET(int fd, zsock_fd_set *set)
 {
 	uint32_t word_idx, bit_mask;
 
-	if (fd < 0 || fd >= ZVFS_FD_SETSIZE) {
+	if (fd < 0 || fd >= ZSOCK_FD_SETSIZE) {
 		return 0;
 	}
 
@@ -48,11 +42,11 @@ int ZVFS_FD_ISSET(int fd, struct zvfs_fd_set *set)
 	return (set->bitset[word_idx] & bit_mask) != 0U;
 }
 
-void ZVFS_FD_CLR(int fd, struct zvfs_fd_set *set)
+void ZSOCK_FD_CLR(int fd, zsock_fd_set *set)
 {
 	uint32_t word_idx, bit_mask;
 
-	if (fd < 0 || fd >= ZVFS_FD_SETSIZE) {
+	if (fd < 0 || fd >= ZSOCK_FD_SETSIZE) {
 		return;
 	}
 
@@ -61,11 +55,11 @@ void ZVFS_FD_CLR(int fd, struct zvfs_fd_set *set)
 	set->bitset[word_idx] &= ~bit_mask;
 }
 
-void ZVFS_FD_SET(int fd, struct zvfs_fd_set *set)
+void ZSOCK_FD_SET(int fd, zsock_fd_set *set)
 {
 	uint32_t word_idx, bit_mask;
 
-	if (fd < 0 || fd >= ZVFS_FD_SETSIZE) {
+	if (fd < 0 || fd >= ZSOCK_FD_SETSIZE) {
 		return;
 	}
 
@@ -74,19 +68,17 @@ void ZVFS_FD_SET(int fd, struct zvfs_fd_set *set)
 	set->bitset[word_idx] |= bit_mask;
 }
 
-int z_impl_zvfs_select(int nfds, struct zvfs_fd_set *ZRESTRICT readfds,
-		       struct zvfs_fd_set *ZRESTRICT writefds,
-		       struct zvfs_fd_set *ZRESTRICT exceptfds,
-		       const struct timeval *ZRESTRICT timeout)
+int z_impl_zsock_select(int nfds, zsock_fd_set *readfds, zsock_fd_set *writefds,
+			zsock_fd_set *exceptfds, struct zsock_timeval *timeout)
 {
-	struct zvfs_pollfd pfds[CONFIG_ZVFS_POLL_MAX];
+	struct zsock_pollfd pfds[CONFIG_NET_SOCKETS_POLL_MAX];
 	k_timeout_t poll_timeout;
 	int i, res;
 	int num_pfds = 0;
 	int num_selects = 0;
 	int fd_no = 0;
 
-	for (i = 0; i < STRUCT_MEMBER_ARRAY_SIZE(struct zvfs_fd_set, bitset); i++) {
+	for (i = 0; i < STRUCT_MEMBER_ARRAY_SIZE(zsock_fd_set, bitset); i++) {
 		uint32_t bit_mask = 1U;
 		uint32_t read_mask = 0U, write_mask = 0U, except_mask = 0U;
 		uint32_t ored_mask;
@@ -119,15 +111,15 @@ int z_impl_zvfs_select(int nfds, struct zvfs_fd_set *ZRESTRICT readfds,
 				}
 
 				if (read_mask & bit_mask) {
-					events |= ZVFS_POLLIN;
+					events |= ZSOCK_POLLIN;
 				}
 
 				if (write_mask & bit_mask) {
-					events |= ZVFS_POLLOUT;
+					events |= ZSOCK_POLLOUT;
 				}
 
 				if (except_mask & bit_mask) {
-					events |= ZVFS_POLLPRI;
+					events |= ZSOCK_POLLPRI;
 				}
 
 				pfds[num_pfds].fd = fd_no;
@@ -142,24 +134,25 @@ int z_impl_zvfs_select(int nfds, struct zvfs_fd_set *ZRESTRICT readfds,
 	if (timeout == NULL) {
 		poll_timeout = K_FOREVER;
 	} else {
-		poll_timeout = K_USEC(timeout->tv_sec * USEC_PER_SEC + timeout->tv_usec);
+		poll_timeout =
+			K_USEC(timeout->tv_sec * 1000000UL + timeout->tv_usec);
 	}
 
-	res = zvfs_poll_internal(pfds, num_pfds, poll_timeout);
+	res = zsock_poll_internal(pfds, num_pfds, poll_timeout);
 	if (res == -1) {
 		return -1;
 	}
 
 	if (readfds != NULL) {
-		ZVFS_FD_ZERO(readfds);
+		ZSOCK_FD_ZERO(readfds);
 	}
 
 	if (writefds != NULL) {
-		ZVFS_FD_ZERO(writefds);
+		ZSOCK_FD_ZERO(writefds);
 	}
 
 	if (exceptfds != NULL) {
-		ZVFS_FD_ZERO(exceptfds);
+		ZSOCK_FD_ZERO(exceptfds);
 	}
 
 	for (i = 0; i < num_pfds && res > 0; i++) {
@@ -176,21 +169,21 @@ int z_impl_zvfs_select(int nfds, struct zvfs_fd_set *ZRESTRICT readfds,
 		 * So, unlike poll(), a single invalid fd aborts the entire
 		 * select().
 		 */
-		if (revents & ZVFS_POLLNVAL) {
+		if (revents & ZSOCK_POLLNVAL) {
 			errno = EBADF;
 			return -1;
 		}
 
-		if (revents & ZVFS_POLLIN) {
+		if (revents & ZSOCK_POLLIN) {
 			if (readfds != NULL) {
-				ZVFS_FD_SET(fd, readfds);
+				ZSOCK_FD_SET(fd, readfds);
 				num_selects++;
 			}
 		}
 
-		if (revents & ZVFS_POLLOUT) {
+		if (revents & ZSOCK_POLLOUT) {
 			if (writefds != NULL) {
-				ZVFS_FD_SET(fd, writefds);
+				ZSOCK_FD_SET(fd, writefds);
 				num_selects++;
 			}
 		}
@@ -198,14 +191,14 @@ int z_impl_zvfs_select(int nfds, struct zvfs_fd_set *ZRESTRICT readfds,
 		/* It's unclear if HUP/ERR belong here. At least not ignore
 		 * them. Zephyr doesn't use HUP and barely use ERR so far.
 		 */
-		if (revents & (ZVFS_POLLPRI | ZVFS_POLLHUP | ZVFS_POLLERR)) {
+		if (revents & (ZSOCK_POLLPRI | ZSOCK_POLLHUP | ZSOCK_POLLERR)) {
 			if (exceptfds != NULL) {
-				ZVFS_FD_SET(fd, exceptfds);
+				ZSOCK_FD_SET(fd, exceptfds);
 				num_selects++;
 			}
 
 			if (writefds != NULL) {
-				ZVFS_FD_SET(fd, writefds);
+				ZSOCK_FD_SET(fd, writefds);
 				num_selects++;
 			}
 		}
@@ -217,18 +210,19 @@ int z_impl_zvfs_select(int nfds, struct zvfs_fd_set *ZRESTRICT readfds,
 }
 
 #ifdef CONFIG_USERSPACE
-static int z_vrfy_zvfs_select(int nfds, struct zvfs_fd_set *ZRESTRICT readfds,
-			      struct zvfs_fd_set *ZRESTRICT writefds,
-			      struct zvfs_fd_set *ZRESTRICT exceptfds,
-			      const struct timeval *ZRESTRICT timeout)
+static int z_vrfy_zsock_select(int nfds, zsock_fd_set *readfds,
+			       zsock_fd_set *writefds,
+			       zsock_fd_set *exceptfds,
+			       struct zsock_timeval *timeout)
 {
-	struct zvfs_fd_set *readfds_copy = NULL, *writefds_copy = NULL, *exceptfds_copy = NULL;
-	struct timeval *to = NULL;
+	zsock_fd_set *readfds_copy = NULL, *writefds_copy = NULL,
+		*exceptfds_copy = NULL;
+	struct zsock_timeval *timeval = NULL;
 	int ret = -1;
 
 	if (readfds) {
-		readfds_copy =
-			k_usermode_alloc_from_copy((void *)readfds, sizeof(struct zvfs_fd_set));
+		readfds_copy = k_usermode_alloc_from_copy((void *)readfds,
+						      sizeof(zsock_fd_set));
 		if (!readfds_copy) {
 			errno = ENOMEM;
 			goto out;
@@ -236,8 +230,8 @@ static int z_vrfy_zvfs_select(int nfds, struct zvfs_fd_set *ZRESTRICT readfds,
 	}
 
 	if (writefds) {
-		writefds_copy =
-			k_usermode_alloc_from_copy((void *)writefds, sizeof(struct zvfs_fd_set));
+		writefds_copy = k_usermode_alloc_from_copy((void *)writefds,
+						       sizeof(zsock_fd_set));
 		if (!writefds_copy) {
 			errno = ENOMEM;
 			goto out;
@@ -245,8 +239,8 @@ static int z_vrfy_zvfs_select(int nfds, struct zvfs_fd_set *ZRESTRICT readfds,
 	}
 
 	if (exceptfds) {
-		exceptfds_copy =
-			k_usermode_alloc_from_copy((void *)exceptfds, sizeof(struct zvfs_fd_set));
+		exceptfds_copy = k_usermode_alloc_from_copy((void *)exceptfds,
+							sizeof(zsock_fd_set));
 		if (!exceptfds_copy) {
 			errno = ENOMEM;
 			goto out;
@@ -254,39 +248,41 @@ static int z_vrfy_zvfs_select(int nfds, struct zvfs_fd_set *ZRESTRICT readfds,
 	}
 
 	if (timeout) {
-		to = k_usermode_alloc_from_copy((void *)timeout, sizeof(*to));
-		if (!to) {
+		timeval = k_usermode_alloc_from_copy((void *)timeout,
+						 sizeof(struct zsock_timeval));
+		if (!timeval) {
 			errno = ENOMEM;
 			goto out;
 		}
 	}
 
-	ret = z_impl_zvfs_select(nfds, readfds_copy, writefds_copy, exceptfds_copy, to);
+	ret = z_impl_zsock_select(nfds, readfds_copy, writefds_copy,
+				  exceptfds_copy, timeval);
 
 	if (ret >= 0) {
 		if (readfds_copy) {
 			k_usermode_to_copy((void *)readfds, readfds_copy,
-					   sizeof(struct zvfs_fd_set));
+				       sizeof(zsock_fd_set));
 		}
 
 		if (writefds_copy) {
 			k_usermode_to_copy((void *)writefds, writefds_copy,
-					   sizeof(struct zvfs_fd_set));
+				       sizeof(zsock_fd_set));
 		}
 
 		if (exceptfds_copy) {
 			k_usermode_to_copy((void *)exceptfds, exceptfds_copy,
-					   sizeof(struct zvfs_fd_set));
+				       sizeof(zsock_fd_set));
 		}
 	}
 
 out:
-	k_free(to);
+	k_free(timeval);
 	k_free(readfds_copy);
 	k_free(writefds_copy);
 	k_free(exceptfds_copy);
 
 	return ret;
 }
-#include <zephyr/syscalls/zvfs_select_mrsh.c>
+#include <zephyr/syscalls/zsock_select_mrsh.c>
 #endif
