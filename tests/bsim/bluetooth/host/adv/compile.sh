32594fee11a8fb019362d3ff5f3f9fa7519322f4
Jonathan Rico <jonathan.rico@nordicsemi.no>
2024-07-02 11:46:43 +0200

    Bluetooth: remove host/adv/resume test
    This test verifies a convoluted interaction between the scanner and the
    resumable advertiser feature in the host.
    
    That feature is going away, see #72567.
    
    Prepare that work by removing this test that's in the way.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- tests/bsim/bluetooth/host/adv/compile.sh
+++ tests/bsim/bluetooth/host/adv/compile.sh
@@ -10,8 +10,6 @@ set -ue
 
 source ${ZEPHYR_BASE}/tests/bsim/compile.source
 
-app=tests/bsim/bluetooth/host/adv/resume compile
-app=tests/bsim/bluetooth/host/adv/resume conf_file=prj_2.conf compile
 app=tests/bsim/bluetooth/host/adv/resume2/connectable compile
 app=tests/bsim/bluetooth/host/adv/resume2/connecter compile
 app=tests/bsim/bluetooth/host/adv/resume2/dut compile
