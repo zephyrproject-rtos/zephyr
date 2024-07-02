2d9cb00ba0a9b35e20be7af30ae9205bab8501fc
Emil Gydesen <emil.gydesen@nordicsemi.no>
2024-07-02 13:30:51 +0200

    Bluetooth: GMAP: Add missing documentation in gmap_lc3_presets.h
    Add missing documentation for some defintions in gmap_lc3_presets.h
    Also added missing C++ guards, group and version.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- include/zephyr/bluetooth/audio/gmap_lc3_preset.h
+++ include/zephyr/bluetooth/audio/gmap_lc3_preset.h
@@ -1,7 +1,8 @@
-/** @file
- *  @brief Header for Bluetooth GMAP LC3 presets.
+/**
+ * @file
+ * @brief Header for Bluetooth GMAP LC3 presets.
  *
- * Copyright (c) 2023 Nordic Semiconductor ASA
+ * Copyright (c) 2023-2024 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,17 +10,34 @@
 #ifndef ZEPHYR_INCLUDE_BLUETOOTH_AUDIO_GMAP_LC3_PRESET_
 #define ZEPHYR_INCLUDE_BLUETOOTH_AUDIO_GMAP_LC3_PRESET_
 
+/**
+ * @brief Gaming Audio Profile (GMAP) LC3 Presets
+ *
+ * @defgroup bt_gmap_lc3_preset Gaming Audio Profile (GMAP) LC3 Presets
+ *
+ * @since 3.5
+ * @version 0.8.0
+ *
+ * @ingroup bluetooth
+ * @{
+ *
+ * These APIs provide presets for codec configuration and codec QoS based on values supplied by the
+ * codec configurations from table 3.16 in the GMAP v1.0 specification
+ */
+
 #include <zephyr/bluetooth/audio/audio.h>
 #include <zephyr/bluetooth/audio/bap_lc3_preset.h>
 #include <zephyr/bluetooth/audio/lc3.h>
 
-/* GMAP LC3 unicast presets defined by table 3.16 in the GMAP v1.0 specification */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /**
- *  @brief Helper to declare LC3 32_1_gr codec configuration
+ * @brief Helper to declare LC3 32_1_gr codec configuration
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_32_1_GR(_loc, _stream_context)                                          \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_32KHZ,                 \
@@ -28,10 +46,10 @@
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(7500u, 60U, 1U, 15U, 10000U))
 
 /**
- *  @brief Helper to declare LC3 32_2_gr codec configuration
+ * @brief Helper to declare LC3 32_2_gr codec configuration
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_32_2_GR(_loc, _stream_context)                                          \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_32KHZ,                 \
@@ -40,10 +58,10 @@
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(10000u, 80U, 1U, 20U, 10000U))
 
 /**
- *  @brief Helper to declare LC3 48_1_gr codec configuration
+ * @brief Helper to declare LC3 48_1_gr codec configuration
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_48_1_GR(_loc, _stream_context)                                          \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_48KHZ,                 \
@@ -52,12 +70,12 @@
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(7500u, 75U, 1U, 15U, 10000U))
 
 /**
- *  @brief Helper to declare LC3 48_2_gr codec configuration
+ * @brief Helper to declare LC3 48_2_gr codec configuration
  *
  *  Mandatory to support as both unicast client and server
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_48_2_GR(_loc, _stream_context)                                          \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_48KHZ,                 \
@@ -66,10 +84,10 @@
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(10000u, 100U, 1U, 20U, 10000U))
 
 /**
- *  @brief Helper to declare LC3 48_3_gr codec configuration
+ * @brief Helper to declare LC3 48_3_gr codec configuration
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_48_3_GR(_loc, _stream_context)                                          \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_48KHZ,                 \
@@ -78,12 +96,12 @@
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(7500u, 90U, 1U, 15U, 10000U))
 
 /**
- *  @brief Helper to declare LC3 48_4_gr codec configuration
+ * @brief Helper to declare LC3 48_4_gr codec configuration
  *
  *  Mandatory to support as unicast server
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_48_4_GR(_loc, _stream_context)                                          \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_48KHZ,                 \
@@ -92,10 +110,10 @@
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(10000u, 120U, 1U, 20U, 10000U))
 
 /**
- *  @brief Helper to declare LC3 16_1_gs codec configuration
+ * @brief Helper to declare LC3 16_1_gs codec configuration
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_16_1_GS(_loc, _stream_context)                                          \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_16KHZ,                 \
@@ -104,10 +122,10 @@
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(7500u, 30U, 1U, 15U, 60000U))
 
 /**
- *  @brief Helper to declare LC3 16_2_gs codec configuration
+ * @brief Helper to declare LC3 16_2_gs codec configuration
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_16_2_GS(_loc, _stream_context)                                          \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_16KHZ,                 \
@@ -116,10 +134,10 @@
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(10000u, 40U, 1U, 20U, 60000U))
 
 /**
- *  @brief Helper to declare LC3 32_1_gs codec configuration
+ * @brief Helper to declare LC3 32_1_gs codec configuration
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_32_1_GS(_loc, _stream_context)                                          \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_32KHZ,                 \
@@ -128,10 +146,10 @@
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(7500u, 60U, 1U, 15U, 60000U))
 
 /**
- *  @brief Helper to declare LC3 32_2_gs codec configuration
+ * @brief Helper to declare LC3 32_2_gs codec configuration
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_32_2_GS(_loc, _stream_context)                                          \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_32KHZ,                 \
@@ -140,10 +158,10 @@
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(10000u, 80U, 1U, 20U, 60000U))
 
 /**
- *  @brief Helper to declare LC3 48_1_gs codec configuration
+ * @brief Helper to declare LC3 48_1_gs codec configuration
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_48_1_GS(_loc, _stream_context)                                          \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_48KHZ,                 \
@@ -152,10 +170,10 @@
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(7500u, 75U, 1U, 15U, 60000U))
 
 /**
- *  @brief Helper to declare LC3 48_2_gs codec configuration
+ * @brief Helper to declare LC3 48_2_gs codec configuration
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_48_2_GS(_loc, _stream_context)                                          \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_48KHZ,                 \
@@ -166,10 +184,10 @@
 /* GMAP LC3 broadcast presets defined by table 3.22 in the GMAP v1.0 specification */
 
 /**
- *  @brief Helper to declare LC3 48_1_g codec configuration
+ * @brief Helper to declare LC3 48_1_g codec configuration
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_48_1_G(_loc, _stream_context)                                           \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_48KHZ,                 \
@@ -178,10 +196,10 @@
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(7500u, 75U, 1U, 8U, 10000U))
 
 /**
- *  @brief Helper to declare LC3 48_2_g codec configuration
+ * @brief Helper to declare LC3 48_2_g codec configuration
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_48_2_G(_loc, _stream_context)                                           \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_48KHZ,                 \
@@ -190,10 +208,10 @@
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(10000u, 100U, 1U, 10U, 10000U))
 
 /**
- *  @brief Helper to declare LC3 48_3_g codec configuration
+ * @brief Helper to declare LC3 48_3_g codec configuration
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_48_3_G(_loc, _stream_context)                                           \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_48KHZ,                 \
@@ -202,15 +220,20 @@
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(7500u, 90U, 1U, 8U, 10000U))
 
 /**
- *  @brief Helper to declare LC3 48_4_g codec configuration
+ * @brief Helper to declare LC3 48_4_g codec configuration
  *
- *  @param _loc             Audio channel location bitfield (@ref bt_audio_location)
- *  @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
+ * @param _loc             Audio channel location bitfield (@ref bt_audio_location)
+ * @param _stream_context  Stream context (``BT_AUDIO_CONTEXT_*``)
  */
 #define BT_GMAP_LC3_PRESET_48_4_G(_loc, _stream_context)                                           \
 	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_48KHZ,                 \
 						    BT_AUDIO_CODEC_CFG_DURATION_10, _loc, 120u, 1, \
 						    _stream_context),                              \
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(10000u, 120U, 1U, 10U, 10000U))
+
+#ifdef __cplusplus
+}
+#endif
+/** @} */
 
 #endif /* ZEPHYR_INCLUDE_BLUETOOTH_AUDIO_GMAP_LC3_PRESET_ */
