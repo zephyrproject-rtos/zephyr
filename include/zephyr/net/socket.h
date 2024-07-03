614b4e4584f68d10ae7877750be35fdefc314124
Yong Cong Sin <ycsin@meta.com>
2024-07-03 09:47:05 +0800

    net: socket: fix `sys/_timespec.h` not found error
    When building `native_sim` with CPP & POSIX enabled, the
    POSIX's `unistd.h` header causes a compilation error as
    `sys/_timespec.h` doesn't exist in host gcc lib.
    
    Remove the 'zephyr/posix' prefix, and rely on
    `CMakeLists.txt`s to do the routing accordingly:
    - zephyr/lib/CMakeLists.txt:L11
    - zephyr/lib/posix/options/CMakeLists.txt:L8
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>

--- include/zephyr/net/socket.h
+++ include/zephyr/net/socket.h
@@ -1403,21 +1403,11 @@ struct net_socket_register {
  * zephyr/net/socket.h header file.
  */
 #if defined(CONFIG_POSIX_API)
-#if !defined(ZEPHYR_INCLUDE_POSIX_ARPA_INET_H_)
-#include <zephyr/posix/arpa/inet.h>
-#endif
-#if !defined(ZEPHYR_INCLUDE_POSIX_NETDB_H_)
-#include <zephyr/posix/netdb.h>
-#endif
-#if !defined(ZEPHYR_INCLUDE_POSIX_UNISTD_H_)
-#include <zephyr/posix/unistd.h>
-#endif
-#if !defined(ZEPHYR_INCLUDE_POSIX_POLL_H_)
-#include <zephyr/posix/poll.h>
-#endif
-#if !defined(ZEPHYR_INCLUDE_POSIX_SYS_SOCKET_H_)
-#include <zephyr/posix/sys/socket.h>
-#endif
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <poll.h>
+#include <sys/socket.h>
 #endif /* CONFIG_POSIX_API */
 
 #endif /* ZEPHYR_INCLUDE_NET_SOCKET_H_ */
