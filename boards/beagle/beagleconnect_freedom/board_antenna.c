0558a1dfafff764dade565ea892a210c984a92d3
Ayush Singh <ayush@beagleboard.org>
2024-07-04 15:49:00 +0530

    boards: beagle: beagleconnect_freedom: Fix formatting
    - Fix formatting in board_antenna.c by using the standard config in
      Zephyr.
    - Reduce diff with cc1352p1_launchxl/board_antenna.c as much as possible
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- boards/beagle/beagleconnect_freedom/board_antenna.c
+++ boards/beagle/beagleconnect_freedom/board_antenna.c
@@ -1,9 +1,9 @@
-/* SPDX-License-Identifier: Apache-2.0
- *
+/*
  * Copyright (c) 2021 Florin Stancu
  * Copyright (c) 2021 Jason Kridner, BeagleBoard.org Foundation
  * Copyright (c) 2024 Ayush Singh <ayush@beagleboard.org>
  *
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 /*
@@ -33,14 +33,13 @@ static int board_antenna_init(const struct device *dev);
 static void board_cc13xx_rf_callback(RF_Handle client, RF_GlobalEvent events, void *arg);
 
 const RFCC26XX_HWAttrsV2 RFCC26XX_hwAttrs = {
-	.hwiPriority        = INT_PRI_LEVEL7,
-	.swiPriority        = 0,
+	.hwiPriority = INT_PRI_LEVEL7,
+	.swiPriority = 0,
 	.xoscHfAlwaysNeeded = true,
 	/* RF driver callback for custom antenna switching */
 	.globalCallback = board_cc13xx_rf_callback,
 	/* Subscribe to events */
-	.globalEventMask = (RF_GlobalEventRadioSetup |
-			RF_GlobalEventRadioPowerDown),
+	.globalEventMask = (RF_GlobalEventRadioSetup | RF_GlobalEventRadioPowerDown),
 };
 
 PINCTRL_DT_INST_DEFINE(0);
@@ -73,7 +72,7 @@ static int board_antenna_init(const struct device *dev)
  */
 static void board_cc13xx_rf_callback(RF_Handle client, RF_GlobalEvent events, void *arg)
 {
-	bool    sub1GHz   = false;
+	bool sub1GHz = false;
 	uint8_t loDivider = 0;
 	int i;
 
@@ -84,17 +83,17 @@ static void board_cc13xx_rf_callback(RF_Handle client, RF_GlobalEvent events, vo
 
 	if (events & RF_GlobalEventRadioSetup) {
 		/* Decode the current PA configuration. */
-		RF_TxPowerTable_PAType paType = (RF_TxPowerTable_PAType)
-			RF_getTxPower(client).paType;
+		RF_TxPowerTable_PAType paType =
+			(RF_TxPowerTable_PAType)RF_getTxPower(client).paType;
 		/* Decode the generic argument as a setup command. */
 		RF_RadioSetup *setupCommand = (RF_RadioSetup *)arg;
 
 		switch (setupCommand->common.commandNo) {
-		case (CMD_RADIO_SETUP):
-		case (CMD_BLE5_RADIO_SETUP):
+		case CMD_RADIO_SETUP:
+		case CMD_BLE5_RADIO_SETUP:
 			loDivider = RF_LODIVIDER_MASK & setupCommand->common.loDivider;
 			break;
-		case (CMD_PROP_RADIO_DIV_SETUP):
+		case CMD_PROP_RADIO_DIV_SETUP:
 			loDivider = RF_LODIVIDER_MASK & setupCommand->prop_div.loDivider;
 			break;
 		default:
