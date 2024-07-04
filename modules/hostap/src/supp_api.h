54205b1fc26a6392efd3ba2f109e1139a367c49d
Gang Li <gang.li_1@nxp.com>
2024-07-04 11:22:17 +0900

    [nxp noup] hostap: add AP configuration cmd support
    Implement AP configuration parameter operations.
    
    Signed-off-by: Gang Li <gang.li_1@nxp.com>

--- modules/hostap/src/supp_api.h
+++ modules/hostap/src/supp_api.h
@@ -268,6 +268,15 @@ int hapd_state(const struct device *dev, int *state);
  * @return 0 for OK; -1 for ERROR
  */
 int supplicant_ap_bandwidth(const struct device *dev, struct wifi_ap_config_params *params);
+
+/**
+ * @brief Wi-Fi AP configuration parameter.
+ *
+ * @param dev Wi-Fi device
+ * @param params AP parameters
+ * @return 0 for OK; -1 for ERROR
+ */
+int supplicant_ap_config_params(const struct device *dev, struct wifi_ap_config_params *params);
 #endif
 
 /**
