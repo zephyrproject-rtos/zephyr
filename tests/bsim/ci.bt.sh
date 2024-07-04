b2c2c2eb517f121234bf6f8401a0fa2f3b7b4e20
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2024-07-04 05:43:13 +0200

    Revert "tests/bsim: Provisionally disable the nrf54l15 BT tests"
    This reverts commit 3d729986e062f9b64f4b888a31e97f7012cc28e0.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- tests/bsim/ci.bt.sh
+++ tests/bsim/ci.bt.sh
@@ -35,12 +35,9 @@ TESTS_FILE=tests/bsim/bluetooth/tests.nrf5340bsim_nrf5340_cpuapp.txt \
 tests/bsim/run_parallel.sh
 
 # nrf54l15bsim/nrf54l15/cpuapp set:
-# We provisionally disable the nrf54l15 BT tests in CI due to instability issues in the
-# controller for this platform. See https://github.com/zephyrproject-rtos/zephyr/issues/74635
-# This should be reverted once the underlaying issue is fixed
-#nice tests/bsim/bluetooth/compile.nrf54l15bsim_nrf54l15_cpuapp.sh
+nice tests/bsim/bluetooth/compile.nrf54l15bsim_nrf54l15_cpuapp.sh
 
-#BOARD=nrf54l15bsim/nrf54l15/cpuapp \
-#RESULTS_FILE=${ZEPHYR_BASE}/bsim_out/bsim_results.bt.54l15_cpuapp.xml \
-#TESTS_FILE=tests/bsim/bluetooth/tests.nrf54l15bsim_nrf54l15_cpuapp.txt \
-#tests/bsim/run_parallel.sh
+BOARD=nrf54l15bsim/nrf54l15/cpuapp \
+RESULTS_FILE=${ZEPHYR_BASE}/bsim_out/bsim_results.bt.54l15_cpuapp.xml \
+TESTS_FILE=tests/bsim/bluetooth/tests.nrf54l15bsim_nrf54l15_cpuapp.txt \
+tests/bsim/run_parallel.sh
