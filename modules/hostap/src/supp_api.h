20b175cbd9fbdb79bd1d0679f203e25d3704581b
Maochen Wang <maochen.wang@nxp.com>
2024-07-05 18:19:19 +0800

    [nxp noup] hostap: add wps pbc and pin for AP mode
    add wps pbc and pin for AP mode.
    
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- modules/hostap/src/supp_api.h
+++ modules/hostap/src/supp_api.h
@@ -277,6 +277,25 @@ int supplicant_ap_bandwidth(const struct device *dev, struct wifi_ap_config_para
  * @return 0 for OK; -1 for ERROR
  */
 int supplicant_ap_config_params(const struct device *dev, struct wifi_ap_config_params *params);
+
+#ifdef CONFIG_WIFI_NM_HOSTAPD_WPS
+/** Start AP wps pbc connection
+ *
+ * @param dev Pointer to the device structure for the driver instance.
+ *
+ * @return 0 if ok, < 0 if error
+ */
+int supplicant_ap_wps_pbc(const struct device *dev);
+
+/** Set or get AP wps pin
+ *
+ * @param dev Pointer to the device structure for the driver instance
+ * @param params wps pin operarion and pin number
+ *
+ * @return 0 if ok, < 0 if error
+ */
+int supplicant_ap_wps_pin(const struct device *dev, struct wifi_wps_pin_params *params);
+#endif
 #endif
 
 /**
