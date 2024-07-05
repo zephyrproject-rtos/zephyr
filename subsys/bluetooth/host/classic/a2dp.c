64f38fc348f664c5df6663aff730a1740c3cff6c
Mark Wang <yichang.wang@nxp.com>
2024-07-05 19:01:19 +0800

    Bluetooth: A2DP: Fix mistake parameter
    After configuration, the stream instance is valid and stream's
    local_ep valid. bt_a2dp_stream_establish's parameter is stream
    too. So in bt_a2dp_stream_establish, stream->local_ep should be
    used to tell lower level (AVDTP) the sep.
    set_config_param is used by copy-paste mistake.
    
    Signed-off-by: Mark Wang <yichang.wang@nxp.com>

--- subsys/bluetooth/host/classic/a2dp.c
+++ subsys/bluetooth/host/classic/a2dp.c
@@ -726,7 +726,7 @@ int bt_a2dp_stream_establish(struct bt_a2dp_stream *stream)
 	a2dp->open_param.req.func = bt_a2dp_open_cb;
 	a2dp->open_param.acp_stream_ep_id = stream->remote_ep != NULL ?
 		stream->remote_ep->sep.sep_info.id : stream->remote_ep_id;
-	a2dp->open_param.sep = a2dp->set_config_param.sep;
+	a2dp->open_param.sep = &stream->local_ep->sep;
 	return bt_avdtp_open(&a2dp->session, &a2dp->open_param);
 }
 
