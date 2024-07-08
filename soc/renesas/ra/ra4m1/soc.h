9a344b3bfc7f28cdd12b6dbd1ae974117fe13fc4
Thao Luong <thao.luong.uw@renesas.com>
2024-07-08 22:05:45 +0700

    soc: renesas: Add initial support for RA4M1
    Add support for Renesas RA4M1 and change the name of existed support
    folder for Arduino R4 into RA4M1_arduino
    
    Signed-off-by: Quy Tran <quy.tran.pz@renesas.com>
    Signed-off-by: Thao Luong <thao.luong.uw@renesas.com>

--- soc/renesas/ra/ra4m1/soc.h
+++ soc/renesas/ra/ra4m1/soc.h
@@ -1,7 +1,16 @@
 /*
- * Copyright (c) 2023 TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>
+ * Copyright (c) 2024 Renesas Electronics Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "../common/ra_common_soc.h"
+/**
+ * @file SoC configuration macros for the Renesas RA4M1 family MCU
+ */
+
+#ifndef ZEPHYR_SOC_RENESAS_RA4M1_SOC_H_
+#define ZEPHYR_SOC_RENESAS_RA4M1_SOC_H_
+
+#include <bsp_api.h>
+
+#endif /* ZEPHYR_SOC_RENESAS_RA4M1_SOC_H_ */
