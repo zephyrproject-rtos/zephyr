9e3e8085af0db66e58247a689e5b828ea5ce81d9
Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>
2024-07-08 16:14:14 +0100

    tests: posix: headers: fix build error when minimal libc is used
    The `tests/posix/headers/portability.posix.headers.without_posix_api`
    test fails to build in case of toolchain which uses minimal libc by
    default (for example ARC MWDT toolchain).
    
    It can be easily fixed by providing sighal.h handler in minimal libc
    
    Signed-off-by: Evgeniy Paltsev <PaltsevEvgeniy@gmail.com>
    Signed-off-by: Eugeniy Paltsev <Eugeniy.Paltsev@synopsys.com>

---
+++ lib/libc/minimal/include/signal.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024 Synopsys
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_SIGNAL_H_
+#define ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_SIGNAL_H_
+
+#include <zephyr/posix/signal.h>
+
+#endif /* ZEPHYR_LIB_LIBC_MINIMAL_INCLUDE_SIGNAL_H_ */
