6477e79f44964372539266976f86834ba057847e
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2024-07-11 15:19:46 +0200

    Bluetooth: Controller: Introduce ticker reschedule with jitter
    Introduce ticker reschedule with jitter so that role like
    BIG can start after overlapping ACL and receive subsequent
    subevents.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ticker/ticker.c
+++ subsys/bluetooth/controller/ticker/ticker.c
@@ -1278,6 +1278,7 @@ void ticker_worker(void *param)
 	node = &instance->nodes[0];
 
 	while (ticker_id_head != TICKER_NULL) {
+		uint32_t ticks_to_expire_minus;
 		struct ticker_node *ticker;
 		uint32_t ticks_to_expire;
 		uint8_t must_expire_skip;
@@ -1360,10 +1361,23 @@ void ticker_worker(void *param)
 			must_expire_skip = 1U;
 		}
 
+		/* Pick the ticker expiry latency, to calculate ticks_at_expire
+		 */
+		ticks_to_expire_minus = ticker->ticks_to_expire_minus;
+
 #if defined(CONFIG_BT_TICKER_EXT)
 		if (ticker->ext_data) {
+			/* Pick the drift, to give it in the ticker_cb */
 			ticks_drift = ticker->ext_data->ticks_drift;
 			ticker->ext_data->ticks_drift = 0U;
+
+			/* Revert back drift so that next expiry maintains the
+			 * average periodic interval.
+			 */
+			if (ticker->ext_data->is_jitter_in_window) {
+				ticker->ticks_to_expire_minus += ticks_drift;
+			}
+
 			/* Mark node as not re-scheduling */
 			ticker->ext_data->reschedule_state =
 				TICKER_RESCHEDULE_STATE_NONE;
@@ -1378,6 +1392,10 @@ void ticker_worker(void *param)
 #else  /* CONFIG_BT_TICKER_LOW_LAT ||
 	* CONFIG_BT_TICKER_SLOT_AGNOSTIC
 	*/
+		/* Pick the ticker expiry latency, to calculate ticks_at_expire
+		 */
+		ticks_to_expire_minus = ticker->ticks_to_expire_minus;
+
 		ticks_drift = 0U;
 #endif /* CONFIG_BT_TICKER_LOW_LAT ||
 	* CONFIG_BT_TICKER_SLOT_AGNOSTIC
@@ -1391,7 +1409,7 @@ void ticker_worker(void *param)
 
 			ticks_at_expire = (instance->ticks_current +
 					   ticks_expired -
-					   ticker->ticks_to_expire_minus) &
+					   ticks_to_expire_minus) &
 					   HAL_TICKER_CNTR_MASK;
 
 #if defined(CONFIG_BT_TICKER_EXT_EXPIRE_INFO)
@@ -1700,7 +1718,8 @@ static inline uint32_t ticker_job_node_update(struct ticker_instance *instance,
 	 */
 	struct ticker_ext *ext_data = ticker->ext_data;
 
-	if (ext_data && ext_data->ticks_slot_window != 0U) {
+	if (ext_data && ext_data->ticks_slot_window &&
+	    !ext_data->is_jitter_in_window) {
 		ext_data->ticks_drift =
 			user_op->params.update.ticks_drift_plus -
 			user_op->params.update.ticks_drift_minus;
@@ -2516,7 +2535,8 @@ static uint8_t ticker_job_reschedule_in_window(struct ticker_instance *instance)
 			 */
 			if (window_end_ticks > (ticks_start_offset +
 						ticks_slot)) {
-				if (!ticker_resched->ticks_slot) {
+				if (!ticker_resched->ticks_slot ||
+				    ext_data->is_jitter_in_window) {
 					/* Place at start of window */
 					ticks_to_expire = window_start_ticks;
 				} else {
@@ -2542,6 +2562,9 @@ static uint8_t ticker_job_reschedule_in_window(struct ticker_instance *instance)
 						 ticks_slot))) {
 				/* Re-scheduled node fits before this node */
 				break;
+			} else {
+				/* Not inside the window */
+				ticks_to_expire = 0U;
 			}
 
 			/* We din't find a valid slot for re-scheduling - try
@@ -2552,9 +2575,14 @@ static uint8_t ticker_job_reschedule_in_window(struct ticker_instance *instance)
 					      ticker_next->ticks_slot;
 			ticks_to_expire_offset = 0U;
 
-			if (!ticker_resched->ticks_slot) {
-				/* Try at the end of the next node */
-				ticks_to_expire = window_start_ticks;
+			if (!ticker_resched->ticks_slot ||
+			    ext_data->is_jitter_in_window) {
+				if (!ext_data->is_jitter_in_window ||
+				    (window_start_ticks <= (ticks_slot_window -
+							   ticks_slot))) {
+					/* Try at the end of the next node */
+					ticks_to_expire = window_start_ticks;
+				}
 			} else {
 				/* Try at the end of the slot window. This
 				 * ensures that ticker with slot window and that
