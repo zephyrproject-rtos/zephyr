e633de53f8956aace95554ff14d34036849170f1
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2024-07-12 04:18:38 +0200

    Bluetooth: Controller: Introduce BT_CTLR_PERIPHERAL_RESERVE_MAX
    Introduce BT_CTLR_PERIPHERAL_RESERVE_MAX Kconfig option so
    that disabling this option will use minimum time reservation
    and exercise the peripheral connection event continuation
    using is_abort_cb mechanism.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_peripheral.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_peripheral.c
@@ -89,7 +89,8 @@ void lll_periph_prepare(void *param)
 	}
 
 	/* Invoke common pipeline handling of prepare */
-	err = lll_prepare(lll_is_abort_cb, lll_conn_abort_cb, prepare_cb, 0, p);
+	err = lll_prepare(lll_conn_is_abort_cb, lll_conn_abort_cb, prepare_cb,
+			  0U, p);
 	LL_ASSERT(!err || err == -EINPROGRESS);
 }
 
