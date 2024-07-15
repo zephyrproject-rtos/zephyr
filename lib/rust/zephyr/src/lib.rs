6646068931d865de8c1cf9bb888036b844421683
David Brown <david.brown@linaro.org>
2024-07-15 13:04:52 -0600

    rust: Basic `zephyr` crate
    The initial support crate for zephyr use within a Rust application.
    This crate does two simple things:
    
    - Processes the .config file generated for this build, and ensures that
      CONFIG_RUST is enabled.
    - Provide a hanging Rust panic handler.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

---
+++ lib/rust/zephyr/src/lib.rs
@@ -0,0 +1,22 @@
+// Copyright (c) 2024 Linaro LTD
+// SPDX-License-Identifier: Apache-2.0
+
+//! Zephyr application support for Rust
+//!
+//! This crates provides the core functionality for applications written in Rust that run on top of
+//! Zephyr.
+
+#![no_std]
+
+// Ensure that Rust is enabled.
+#[cfg(not(CONFIG_RUST))]
+compile_error!("CONFIG_RUST must be set to build Rust in Zephyr");
+
+use core::panic::PanicInfo;
+
+/// Override rust's panic.  This simplistic initial version just hangs in a loop.
+#[panic_handler]
+fn panic(_ :&PanicInfo) -> ! {
+    loop {
+    }
+}
