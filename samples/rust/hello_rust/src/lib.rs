9834f123b3f6f02ebee09946024f0e40559adcd4
David Brown <david.brown@linaro.org>
2024-07-15 13:10:52 -0600

    rust: Simple rust hello_world application
    Create the Rust equivalent of the hello_world application.  This shows
    the use of printk from Rust as well as accessing a string value from
    Kconfig.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

---
+++ samples/rust/hello_rust/src/lib.rs
@@ -0,0 +1,16 @@
+// Copyright (c) 2024 Linaro LTD
+// SPDX-License-Identifier: Apache-2.0
+
+#![no_std]
+
+use zephyr::printkln;
+
+// Reference the Zephyr crate so that the panic handler gets used.  This is only needed if no
+// symbols from the crate are directly used.
+extern crate zephyr;
+
+#[no_mangle]
+extern "C" fn rust_main() {
+    printkln!("Hello world from Rust on {}",
+              zephyr::kconfig::CONFIG_BOARD);
+}
