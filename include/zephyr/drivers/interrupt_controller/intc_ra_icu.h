eceab3a25e095abba7cda163523fdfd6a70cfbae
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2024-07-15 17:39:28 +0900

    drivers: intc: renesas_ra: Handle interrupts based on event numbers
    In the Renesas RA, interrupt numbers are not fixed because the number
    and features are set in the ICU settings.
    
    It wraps the NVIC interrupt functions so that they can be manipulated
    by the INTERRUPT ID instead of the IRQ number.
    
    Internally, it looks up the interrupt number from the event number and
    uses that number to set the interrupt for the NVIC.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- include/zephyr/drivers/interrupt_controller/intc_ra_icu.h
+++ include/zephyr/drivers/interrupt_controller/intc_ra_icu.h
@@ -1,18 +1,36 @@
 /*
- * Copyright (c) 2023 TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>
+ * Copyright (c) 2023 - 2024 TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/dt-bindings/interrupt-controller/renesas-ra-icu.h>
-
 #ifndef ZEPHYR_DRIVERS_INTERRUPT_CONTROLLER_INTC_RA_ICU_H_
 #define ZEPHYR_DRIVERS_INTERRUPT_CONTROLLER_INTC_RA_ICU_H_
 
-#define RA_ICU_FLAG_EVENT_OFFSET  8
-#define RA_ICU_FLAG_EVENT_MASK    (BIT_MASK(8) << RA_ICU_FLAG_EVENT_OFFSET)
-#define RA_ICU_FLAG_INTCFG_OFFSET 16
-#define RA_ICU_FLAG_INTCFG_MASK   (BIT_MASK(8) << RA_ICU_FLAG_INTCFG_OFFSET)
+/**
+ * The Renesas RA ICU is an interrupt controller that works with the NVIC.
+ * In the Renesas RA, interrupt numbers are not assigned to specific functions;
+ * instead, the ICU settings associate the numbers and functions.
+ *
+ * This specification is complex and cannot be supported by the original
+ * interrupt * handling mechanism, so we introduce unique solutions for
+ * isr_table generation.
+ *
+ * Specifically, the following points differ from the standard.
+ *
+ * 1. Interrupt handlers are set in the isr_table in the order they are found
+ *    during linking. At the same time, a lookup table is generated, which
+ *    associates event numbers with interrupts.
+ *
+ * 2. All arguments that specify IRQs in IRQ APIs, such as `irq_enable,
+ *  ` are interpreted as event numbers. The IRQ APIs implemented in the ICU
+ *    resolve this using the lookup table above. In other words, the API does
+ *    not use interrupt numbers but specifies all events by number.
+ *
+ * The interrupt controller uses NVIC, so we provide a wrapper as a driver API that
+ * looks up the IRQ number from the event number.
+ * Now, we can use it in the same way as a normal interrupt on Zephyr.
+ */
 
 enum icu_irq_mode {
 	ICU_FALLING,
@@ -21,22 +39,13 @@ enum icu_irq_mode {
 	ICU_LOW_LEVEL,
 };
 
-typedef void (*ra_isr_handler)(const void *);
+void ra_icu_event_enable(unsigned int event);
+void ra_icu_event_disable(unsigned int event);
+int ra_icu_event_is_enabled(unsigned int event);
+void ra_icu_event_priority_set(unsigned int event, unsigned int prio, uint32_t flags);
 
-extern void ra_icu_clear_int_flag(unsigned int irqn);
-
-extern int ra_icu_query_available_irq(uint32_t event);
-extern int ra_icu_query_exists_irq(uint32_t event);
-
-extern void ra_icu_query_irq_config(unsigned int irq, uint32_t *intcfg, ra_isr_handler *pisr,
-				    const void **cbarg);
-
-extern int ra_icu_irq_connect_dynamic(unsigned int irq, unsigned int priority,
-				      void (*routine)(const void *parameter), const void *parameter,
-				      uint32_t flags);
-
-extern int ra_icu_irq_disconnect_dynamic(unsigned int irq, unsigned int priority,
-					 void (*routine)(const void *parameter),
-					 const void *parameter, uint32_t flags);
+void ra_icu_event_clear_flag(unsigned int event);
+void ra_icu_event_query_config(unsigned int event, uint32_t *intcfg);
+void ra_icu_event_configure(unsigned int event, uint32_t intcfg);
 
 #endif /* ZEPHYR_DRIVERS_INTERRUPT_CONTROLLER_INTC_RA_ICU_H_ */
