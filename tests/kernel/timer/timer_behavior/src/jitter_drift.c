cee1950b1dd1a839bd071f839e391fbe73c69823
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2024-07-15 11:35:04 +0200

    tests: kernel: timer: behavior: Do not check drift for non-periodic case
    When timer is not periodic but next timer is started from expiration
    callback of the previous timer then it is expected that there will
    be aggregated error. Check for aggregated error only if test is running
    with periodic timers.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- tests/kernel/timer/timer_behavior/src/jitter_drift.c
+++ tests/kernel/timer/timer_behavior/src/jitter_drift.c
@@ -333,8 +333,15 @@ static void do_test_using(void (*sample_collection_fn)(void), const char *mechan
 		     "Standard deviation (in microseconds) outside expected bound");
 
 	/* Validate the timer drift (accuracy over time) is within a configurable bound */
-	zassert_true(abs(time_diff_us) < CONFIG_TIMER_TEST_MAX_DRIFT,
+	if (strcmp(mechanism, "builtin") == 0) {
+		/* There should be no drift only when periodic timer is used but when new timer
+		 * is started from the callback of the previous timer then new time setting is
+		 * relative to that moment and not to the expiration of previous timer and drift
+		 * will occur.
+		 */
+		zassert_true(abs(time_diff_us) < CONFIG_TIMER_TEST_MAX_DRIFT,
 		     "Drift (in microseconds) outside expected bound");
+	}
 }
 
 ZTEST(timer_jitter_drift, test_jitter_drift_timer_period)
