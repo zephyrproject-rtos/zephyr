01ca425314539b4305ebeda2acbe770f29c93c93
Benjamin Lemouzy <blemouzy@centralp.fr>
2024-07-16 10:46:13 +0200

    jwt: add functions to parse a token
    - Add a new function jwt_parse_payload to parse token payload
    - Add a new function jwt_verify to verify token header and signature
    
    Signed-off-by: Benjamin Lemouzy <blemouzy@centralp.fr>

--- subsys/jwt/jwt_legacy_rsa.c
+++ subsys/jwt/jwt_legacy_rsa.c
@@ -54,3 +54,31 @@ int jwt_sign_impl(struct jwt_builder *builder, const unsigned char *der_key, siz
 			      &sig_len_out, csprng_wrapper, NULL);
 	return res;
 }
+
+int jwt_verify_impl(struct jwt_builder *builder, const unsigned char *der_key, size_t der_key_len,
+		    const unsigned char *sig, size_t sig_size)
+{
+	int res;
+	mbedtls_pk_context ctx;
+
+	mbedtls_pk_init(&ctx);
+
+	res = mbedtls_pk_parse_key(&ctx, der_key, der_key_len, NULL, 0, csprng_wrapper, NULL);
+	if (res != 0) {
+		return res;
+	}
+
+	uint8_t hash[32];
+
+	/*
+	 * The '0' indicates to mbedtls to do a SHA256, instead of
+	 * 224.
+	 */
+	res = mbedtls_sha256(builder->base, builder->buf - builder->base, hash, 0);
+	if (res != 0) {
+		return res;
+	}
+
+	res = mbedtls_pk_verify(&ctx, MBEDTLS_MD_SHA256, hash, sizeof(hash), sig, sig_size);
+	return res;
+}
