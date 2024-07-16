a050bcba21bf7085f4bc19bc2b87723e6b21bac0
Sebastian Głąb <sebastian.glab@nordicsemi.no>
2024-07-16 08:31:31 +0200

    [nrf fromtree]  drivers: mbox: nrf: Change VEVIFs and BELLBOARD nomenclature
    Renaming 'LOCAL' to 'RX' and 'REMOTE' to 'TX'.
    This seems more descriptive and intuitive to use.
    
    Signed-off-by: Jakub Zymelka <jakub.zymelka@nordicsemi.no>
    (cherry picked from commit 8091e9383852fff6634827c51aefea82ea748ee3)
    
    Signed-off-by: Sebastian Głąb <sebastian.glab@nordicsemi.no>

--- drivers/mbox/mbox_nrf_bellboard_tx.c
+++ drivers/mbox/mbox_nrf_bellboard_tx.c
@@ -3,20 +3,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define DT_DRV_COMPAT nordic_nrf_bellboard_remote
+#define DT_DRV_COMPAT nordic_nrf_bellboard_tx
 
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/mbox.h>
 
 #include <haly/nrfy_bellboard.h>
 
-struct mbox_bellboard_remote_conf {
+struct mbox_bellboard_tx_conf {
 	NRF_BELLBOARD_Type *bellboard;
 };
 
-static int bellboard_remote_send(const struct device *dev, uint32_t id, const struct mbox_msg *msg)
+static int bellboard_tx_send(const struct device *dev, uint32_t id, const struct mbox_msg *msg)
 {
-	const struct mbox_bellboard_remote_conf *config = dev->config;
+	const struct mbox_bellboard_tx_conf *config = dev->config;
 
 	if (id >= BELLBOARD_TASKS_TRIGGER_MaxCount) {
 		return -EINVAL;
@@ -31,32 +31,32 @@ static int bellboard_remote_send(const struct device *dev, uint32_t id, const st
 	return 0;
 }
 
-static int bellboard_remote_mtu_get(const struct device *dev)
+static int bellboard_tx_mtu_get(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 
 	return 0;
 }
 
-static uint32_t bellboard_remote_max_channels_get(const struct device *dev)
+static uint32_t bellboard_tx_max_channels_get(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 
 	return BELLBOARD_TASKS_TRIGGER_MaxCount;
 }
 
-static const struct mbox_driver_api bellboard_remote_driver_api = {
-	.send = bellboard_remote_send,
-	.mtu_get = bellboard_remote_mtu_get,
-	.max_channels_get = bellboard_remote_max_channels_get,
+static const struct mbox_driver_api bellboard_tx_driver_api = {
+	.send = bellboard_tx_send,
+	.mtu_get = bellboard_tx_mtu_get,
+	.max_channels_get = bellboard_tx_max_channels_get,
 };
 
 #define BELLBOARD_REMOTE_DEFINE(inst)                                                              \
-	static const struct mbox_bellboard_remote_conf conf##inst = {                              \
+	static const struct mbox_bellboard_tx_conf conf##inst = {                                  \
 		.bellboard = (NRF_BELLBOARD_Type *)DT_INST_REG_ADDR(inst),                         \
 	};                                                                                         \
                                                                                                    \
 	DEVICE_DT_INST_DEFINE(inst, NULL, NULL, NULL, &conf##inst, POST_KERNEL,                    \
-			      CONFIG_MBOX_INIT_PRIORITY, &bellboard_remote_driver_api);
+			      CONFIG_MBOX_INIT_PRIORITY, &bellboard_tx_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(BELLBOARD_REMOTE_DEFINE)
