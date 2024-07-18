d56de3b12277a9caf41a4565737a9efe9cdf38f7
Manuel Argüelles <manuel.arguelles@nxp.com>
2024-07-18 09:20:15 +0700

    drivers: watchdog: convert NXP SWT to native driver
    Convert NXP SWT watchdog driver to a native driver and extend the
    SWT supported functionalities and configuration options.
    
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

--- soc/nxp/s32/s32ze/soc.h
+++ soc/nxp/s32/s32ze/soc.h
@@ -25,21 +25,6 @@
 /* LINFlexD*/
 #define IP_LINFLEX_12_BASE      IP_MSC_0_LIN_BASE
 
-/* SWT */
-#define IP_SWT_0_BASE           IP_CE_SWT_0_BASE
-#define IP_SWT_1_BASE           IP_CE_SWT_1_BASE
-#define IP_SWT_2_BASE           IP_RTU0__SWT_0_BASE
-#define IP_SWT_3_BASE           IP_RTU0__SWT_1_BASE
-#define IP_SWT_4_BASE           IP_RTU0__SWT_2_BASE
-#define IP_SWT_5_BASE           IP_RTU0__SWT_3_BASE
-#define IP_SWT_6_BASE           IP_RTU0__SWT_4_BASE
-#define IP_SWT_7_BASE           IP_RTU1__SWT_0_BASE
-#define IP_SWT_8_BASE           IP_RTU1__SWT_1_BASE
-#define IP_SWT_9_BASE           IP_RTU1__SWT_2_BASE
-#define IP_SWT_10_BASE          IP_RTU1__SWT_3_BASE
-#define IP_SWT_11_BASE          IP_RTU1__SWT_4_BASE
-#define IP_SWT_12_BASE          IP_SMU__SWT_BASE
-
 /* STM */
 #define IP_STM_0_BASE           IP_CE_STM_0_BASE
 #define IP_STM_1_BASE           IP_CE_STM_1_BASE
