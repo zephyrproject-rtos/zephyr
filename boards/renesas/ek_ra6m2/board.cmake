7ed7460d223e63cda89fbcffc802d83b579ba744
Duy Phuong Hoang. Nguyen <duy.nguyen.xa@renesas.com>
2024-07-18 19:02:49 +0700

    boards: renesas: Initial support Renesas EK-RA6M2 board
    - Initial support for EK-RA6M2 board
    
    Signed-off-by: Duy Phuong Hoang. Nguyen <duy.nguyen.xa@renesas.com>

---
+++ boards/renesas/ek_ra6m2/board.cmake
@@ -0,0 +1,6 @@
+# Copyright (c) 2024 Renesas Electronics Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=R7FA6M2AF")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
