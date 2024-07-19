8f14bb74efc7c59b2c089d3ddc4c0fa7c111ca75
Mario Jaun <mario.jaun@zuehlke.com>
2024-07-19 15:23:29 +0200

    support more types in rust dts

--- scripts/dts/gen_dts_rust.py
+++ scripts/dts/gen_dts_rust.py
@@ -38,6 +38,8 @@ def generate_dts_rust(edt):
 
 
 def generate_node_struct(node):
+    phandle_arrays = {}
+
     yield f'struct DtNode{node.dep_ordinal} {{\n'
 
     for child_name, child_node in node.children.items():
@@ -52,25 +54,42 @@ def generate_node_struct(node):
             rust_type = 'u32'
         if property_object.spec.type == 'string':
             rust_type = 'str'
+        if property_object.spec.type == 'uint8-array':
+            rust_type = '[u8]'
         if property_object.spec.type == 'array':
             rust_type = '[u32]'
         if property_object.spec.type == 'string-array':
             rust_type = '[str]'
-        if property_object.spec.type == 'phandle' or property_object.spec.type == 'path':
+        if property_object.spec.type in ('phandle', 'path'):
             rust_type = f'&DtNode{property_object.val.dep_ordinal}'
+        if property_object.spec.type == 'phandles':
+            rust_type = '(' + ', '.join(f'&DtNode{n.dep_ordinal}' for n in property_object.val) + ')'
         if property_object.spec.type == 'phandle-array':
-            pass
+            pha_types = []
+            for pha_entry in property_object.val:
+                pha_type = f'DtNode{node.dep_ordinal}Pha{len(phandle_arrays)}'
+                pha_types.append(pha_type)
+                phandle_arrays[pha_type] = pha_entry
+            rust_type = '(' + ', '.join('&' + pha_id for pha_id in pha_types) + ')'
 
         if rust_type:
             yield f'    {replace_chars(property_name)}: {rust_type},\n'
         else:
             yield f'    // {replace_chars(property_name)} ({property_object.spec.type})\n'
 
-
     yield '}\n\n'
+
+    for pha_type, pha_entry in phandle_arrays.items():
+        yield f'struct {pha_type} {{\n'
+        yield f'    controller: &DtNode{pha_entry.controller.dep_ordinal},\n'
+        for cell_name in pha_entry.data.keys():
+            yield f'    {replace_chars(cell_name)}: u32,\n'
+        yield '}\n\n'
 
 
 def generate_node_instance(node):
+    phandle_arrays = {}
+
     for label in node.labels:
         yield f'// label("{label}")\n'
     for alias in node.aliases:
@@ -90,7 +109,7 @@ def generate_node_instance(node):
             yield f'    {replace_chars(property_name)}: {property_object.val},\n'
         elif property_object.spec.type == 'string':
             yield f'    {replace_chars(property_name)}: "{property_object.val}",\n'
-        elif property_object.spec.type == 'array':
+        elif property_object.spec.type in ('uint8-array', 'array'):
             yield f'    {replace_chars(property_name)}: ['
             yield ', '.join(str(val) for val in property_object.val)
             yield '],\n'
@@ -98,12 +117,33 @@ def generate_node_instance(node):
             yield f'    {replace_chars(property_name)}: ['
             yield ', '.join(f'"{val}"' for val in property_object.val)
             yield '],\n'
-        elif property_object.spec.type == 'phandle' or property_object.spec.type == 'path':
+        elif property_object.spec.type in ('phandle', 'path'):
             yield f'    {replace_chars(property_name)}: "DT_NODE_{property_object.val.dep_ordinal}",\n'
+        elif property_object.spec.type == 'phandles':
+            yield f'    {replace_chars(property_name)}: ('
+            yield ', '.join(f'DT_NODE_{n.dep_ordinal}' for n in property_object.val)
+            yield '),\n'
+        elif property_object.spec.type == 'phandle-array':
+            pha_ids = []
+            for pha_entry in property_object.val:
+                pha_inst = f'DT_NODE_{node.dep_ordinal}_PHA_{len(phandle_arrays)}'
+                pha_type = f'&DtNode{node.dep_ordinal}Pha{len(phandle_arrays)}'
+                pha_ids.append(pha_inst)
+                phandle_arrays[pha_inst] = (pha_type, pha_entry)
+            yield f'    {replace_chars(property_name)}: (' + ', '.join(pha_ids) + '),\n'
         else:
             yield f'    // {replace_chars(property_name)} ({property_object.spec.type})\n'
 
     yield '};\n\n'
+
+    for pha_inst, (pha_type, pha_entry) in phandle_arrays.items():
+        if pha_entry.name:
+            yield f'// {pha_entry.name}\n'
+        yield f'const {pha_inst}: {pha_type} = {pha_type} {{\n'
+        yield f'    controller: DT_NODE_{pha_entry.controller.dep_ordinal},\n'
+        for cell_name, cell_value in pha_entry.data.items():
+            yield f'    {replace_chars(cell_name)}: {cell_value},\n'
+        yield '};\n\n'
 
 
 def replace_chars(text: str):
