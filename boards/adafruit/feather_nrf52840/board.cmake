b6ed4ba76168afd2ed84be6687dcee80a690b6b9
Jacob Winther <jacob@9.nz>
2024-07-21 21:03:27 +1200

    boards: Add Adafruit Feather nrf52840 Sense
    Add support for board based on the Express variant.
    Board uses uf2 as it has no swd adaptor.
    
    Signed-off-by: Jacob Winther <jacob@9.nz>

--- boards/adafruit/feather_nrf52840/board.cmake
+++ boards/adafruit/feather_nrf52840/board.cmake
@@ -2,6 +2,9 @@
 
 board_runner_args(jlink "--device=nRF52840_xxAA" "--speed=4000")
 board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
+if(CONFIG_BOARD_ADAFRUIT_FEATHER_NRF52840_SENSE)
+include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
+endif()
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
