e324a7e15f53bfbec736c41dfe95923702bf9806
Jordan Yates <jordan@embeint.com>
2024-07-21 12:37:40 +1000

    cmake: git.cmake: helpers for working with hashes
    Add two git helpers, the first for getting the git commit hash of the
    latest commit in a directory, the second to shorten it to a form usable
    in a `uint32_t`.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- cmake/git.cmake
+++ cmake/git.cmake
@@ -29,3 +29,57 @@ function(git_describe dir output)
     endif()
   endif()
 endfunction()
+
+# Usage:
+#   git_commit_hash(<dir> <output>)
+#
+# Helper function to extract the full 160bit git commit hash associated with a directory.
+# output is set to the output of `git rev-parse --short=40 HEAD` as run from dir.
+#
+function(git_commit_hash dir output)
+  if(GIT_FOUND)
+    execute_process(
+      COMMAND ${GIT_EXECUTABLE} rev-parse --short=40 HEAD
+      WORKING_DIRECTORY                ${dir}
+      OUTPUT_VARIABLE                  commit_hash
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_STRIP_TRAILING_WHITESPACE
+      ERROR_VARIABLE                   stderr
+      RESULT_VARIABLE                  return_code
+    )
+    if(return_code)
+      message(WARNING "git rev-parse failed: ${stderr}")
+    elseif(NOT "${stderr}" STREQUAL "")
+      message(WARNING "git rev-parse warned: ${stderr}")
+    else()
+      # Save output
+      set(${output} ${commit_hash} PARENT_SCOPE)
+    endif()
+  endif()
+endfunction()
+
+# Usage:
+#   git_commit_hash_short(<full_hash> <short_hash> <short_hash_int>)
+#
+# Helper function to truncate a full GIT commit hash to a value that
+# fits in a uint32_t. short_hash_str is set to the first 8 characters of
+# full_hash_str, while short_hash_uint32 is the decimal value equivalent of
+# 0x${short_hash_str}
+#
+function(git_commit_hash_short full_hash_str short_hash_str short_hash_uint32)
+  # Input validation
+  string(LENGTH "${full_hash_str}" full_hash_str_len)
+  if(full_hash_str_len LESS 8)
+    message(FATAL_ERROR "Expected ${full_hash_str} to be at least 8 characters")
+  endif()
+  if(NOT "${full_hash_str}" MATCHES "^[0-9a-fA-F]+$")
+    message(FATAL_ERROR "Expected ${full_hash_str} to be hexadecimal string")
+  endif()
+  # Extract first 8 characters of hash
+  string(SUBSTRING ${full_hash_str} 0 8 short_str)
+  # Convert to a decimal value
+  math(EXPR short_int "0x${short_str}" OUTPUT_FORMAT DECIMAL)
+  # Save output
+  set(${short_hash_str} ${short_str} PARENT_SCOPE)
+  set(${short_hash_uint32} ${short_int} PARENT_SCOPE)
+endfunction()
