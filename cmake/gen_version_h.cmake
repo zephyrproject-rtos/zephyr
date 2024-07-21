ad43cddd6d43df6e8c5260b501fa9de3bb4a36d0
Jordan Yates <jordan@embeint.com>
2024-07-21 12:36:30 +1000

    cmake: git.cmake: common git helpers
    Move generic git functions into a dedicated file under `cmake/`.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- cmake/gen_version_h.cmake
+++ cmake/gen_version_h.cmake
@@ -2,31 +2,18 @@
 
 cmake_minimum_required(VERSION 3.20.0)
 
+include(${ZEPHYR_BASE}/cmake/git.cmake)
+
 if(VERSION_TYPE STREQUAL KERNEL)
   set(BUILD_VERSION_NAME BUILD_VERSION)
 else()
   set(BUILD_VERSION_NAME ${VERSION_TYPE}_BUILD_VERSION)
 endif()
 
+cmake_path(GET VERSION_FILE PARENT_PATH work_dir)
+
 if(NOT DEFINED ${BUILD_VERSION_NAME})
-  cmake_path(GET VERSION_FILE PARENT_PATH work_dir)
-  find_package(Git QUIET)
-  if(GIT_FOUND)
-    execute_process(
-      COMMAND ${GIT_EXECUTABLE} describe --abbrev=12 --always
-      WORKING_DIRECTORY                ${work_dir}
-      OUTPUT_VARIABLE                  ${BUILD_VERSION_NAME}
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      ERROR_STRIP_TRAILING_WHITESPACE
-      ERROR_VARIABLE                   stderr
-      RESULT_VARIABLE                  return_code
-    )
-    if(return_code)
-      message(STATUS "git describe failed: ${stderr}")
-    elseif(NOT "${stderr}" STREQUAL "")
-      message(STATUS "git describe warned: ${stderr}")
-    endif()
-  endif()
+  git_describe(${work_dir} ${BUILD_VERSION_NAME})
 endif()
 
 include(${ZEPHYR_BASE}/cmake/modules/version.cmake)
---
+++ cmake/git.cmake
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: Apache-2.0
+
+find_package(Git QUIET)
+
+# Usage:
+#   git_describe(<dir> <output>)
+#
+# Helper function to get a short GIT desciption associated with a directory.
+# OUTPUT is set to the output of `git describe --abbrev=12 --always` as run
+# from DIR.
+#
+function(git_describe DIR OUTPUT)
+  if(GIT_FOUND)
+    execute_process(
+      COMMAND ${GIT_EXECUTABLE} describe --abbrev=12 --always
+      WORKING_DIRECTORY                ${DIR}
+      OUTPUT_VARIABLE                  DESCRIPTION
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_STRIP_TRAILING_WHITESPACE
+      ERROR_VARIABLE                   stderr
+      RESULT_VARIABLE                  return_code
+    )
+    if(return_code)
+      message(STATUS "git describe failed: ${stderr}")
+    elseif(NOT "${stderr}" STREQUAL "")
+      message(STATUS "git describe warned: ${stderr}")
+    else()
+      # Save output
+      set(${OUTPUT} ${DESCRIPTION} PARENT_SCOPE)
+    endif()
+  endif()
+endfunction()
