1bd34ac70ac088a709e642d0b51bf2b6f8927b52
David Brown <david.brown@linaro.org>
2024-07-23 09:15:45 -0600

    samples: rust: Use printkln macro
    Remove the unsafe code calling into the C printk, instead using the new
    `printkln` macro for emit a formatted message.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

--- samples/rust/hello_rust/src/lib.rs
+++ samples/rust/hello_rust/src/lib.rs
@@ -3,24 +3,14 @@
 
 #![no_std]
 
+use zephyr::printkln;
+
 // Reference the Zephyr crate so that the panic handler gets used.  This is only needed if no
 // symbols from the crate are directly used.
 extern crate zephyr;
 
 #[no_mangle]
 extern "C" fn rust_main() {
-    // Until we have allocation, converting a Rust string into a C string is a bit awkward.
-    // This works because the kconfig values are const.
-    const BOARD_SRC: &'static str = zephyr::kconfig::CONFIG_BOARD;
-    let mut board = [0u8; BOARD_SRC.len() + 1];
-    board[..BOARD_SRC.len()].clone_from_slice(BOARD_SRC.as_bytes());
-    board[BOARD_SRC.len()] = 0;
-
-    unsafe {
-        printk("Hello world from Rust on %s\n\0".as_ptr(), board.as_ptr());
-    }
-}
-
-extern "C" {
-    fn printk(msg: *const u8, arg1: *const u8);
+    printkln!("Hello world from Rust on {}",
+              zephyr::kconfig::CONFIG_BOARD);
 }
