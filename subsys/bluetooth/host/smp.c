ccd33168a716685ff6ddd18253ee4aea60bbd728
Lyle Zhu <lyle.zhu@nxp.com>
2024-07-25 10:56:14 +0800

    Bluetooth: LE: SMP: Set CT2 bit by default
    Set CT2 bit to auth_req field by default if
    the BR is enabled.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- subsys/bluetooth/host/smp.c
+++ subsys/bluetooth/host/smp.c
@@ -2640,6 +2640,10 @@ static uint8_t get_auth(struct bt_smp *smp, uint8_t auth)
 		auth &= ~BT_SMP_AUTH_KEYPRESS;
 	}
 
+#if defined(CONFIG_BT_CLASSIC)
+	auth |= BT_SMP_AUTH_CT2;
+#endif /* CONFIG_BT_CLASSIC */
+
 	return auth;
 }
 
