aaf40b539efbfb6e2a43d5e7a38ef4b26b6e58e5
David Brown <david.brown@linaro.org>
2024-07-26 08:42:31 -0600

    rust: Create zephyr-sys for low-level bindings
    With a lot of work done by Mario Jaun <mario.jaun@zuehlke.com>.
    
    Use the rust-bindgen tool to extract bindings from the Zephyr header files.
    The tool is run with the exact configuration of the current build, and
    therefore the bindings will match the current target.  The ifdefs and such
    are not translated into the Rust code, and this must be generated live for
    each build.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

---
+++ lib/rust/zephyr-sys/wrapper.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2024 Linaro LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
+ * This file is the seed point for bindgen.  This determines every header that will be visited for
+ * binding generation.  It should include any Zephyr headers we need bindings for.  The driver in
+ * build.rs will also select which functions need generation, which will determine the types that
+ * are output.
+ */
+
+#ifdef RUST_BINDGEN
+/* errno is coming from somewhere in Zephyr's build.  Add the symbol when running bindgen so that it
+ * is defined here.
+ */
+extern int errno;
+#endif
+
+/* First, make sure we have all of our config settings. */
+#include <zephyr/autoconf.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
