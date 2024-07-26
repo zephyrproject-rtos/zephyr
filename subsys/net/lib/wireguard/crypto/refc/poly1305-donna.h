691ae476e0874205fab2ea76ccbae6492d80aa1b
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2024-07-26 17:06:37 +0300

    net: wireguard: Add crypto support
    The crypto code is taken from wireguard-lwip project at
    https://github.com/smartalock/wireguard-lwip
    and is BSD-3-Clause and MIT licensed code.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

---
+++ subsys/net/lib/wireguard/crypto/refc/poly1305-donna.h
@@ -0,0 +1,18 @@
+// Taken from https://github.com/floodyberry/poly1305-donna - public domain or MIT
+// Copyright (c) 2014 Andrew Moon
+
+#ifndef POLY1305_DONNA_H
+#define POLY1305_DONNA_H
+
+#include <stddef.h>
+
+typedef struct poly1305_context {
+	size_t aligner;
+	unsigned char opaque[136];
+} poly1305_context;
+
+void poly1305_init(poly1305_context *ctx, const unsigned char key[32]);
+void poly1305_update(poly1305_context *ctx, const unsigned char *m, size_t bytes);
+void poly1305_finish(poly1305_context *ctx, unsigned char mac[16]);
+
+#endif /* POLY1305_DONNA_H */
