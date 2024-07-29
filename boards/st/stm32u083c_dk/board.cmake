4215078617b5dfd2c64fdb19e14e272944e70b72
IBEN EL HADJ MESSAOUD Marwa <marwa.ibenelhadjmessaoud-ext@st.com>
2024-07-29 18:17:25 +0200

    boards: st: Add stm32u083c_dk board support
    Add board support for stm32u083c_dk
    
    Signed-off-by: IBEN EL HADJ MESSAOUD Marwa <marwa.ibenelhadjmessaoud-ext@st.com>

---
+++ boards/st/stm32u083c_dk/board.cmake
@@ -0,0 +1,9 @@
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+
+board_runner_args(pyocd "--target=stm32u083mctx")
+
+board_runner_args(jlink "--device=STM32U083C-DK" "--reset-after-load")
+
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
