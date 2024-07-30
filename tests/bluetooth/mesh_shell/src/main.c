71b29f47969a02b4a172bb715b9b91bef46d433f
Håvard Reierstad <haavard.reierstad@nordicsemi.no>
2024-07-30 14:58:22 +0200

    Bluetooth: Mesh: Add brg_cfg_cli commands to shell
    Adds the Bridge Configuration Client commands to Mesh shell.
    
    Unifies documentation for shell Kconfig options.
    
    Signed-off-by: Håvard Reierstad <haavard.reierstad@nordicsemi.no>

--- tests/bluetooth/mesh_shell/src/main.c
+++ tests/bluetooth/mesh_shell/src/main.c
@@ -41,6 +41,10 @@ static struct bt_mesh_od_priv_proxy_cli od_priv_proxy_cli;
 struct bt_mesh_large_comp_data_cli large_comp_data_cli;
 #endif
 
+#if defined(CONFIG_BT_MESH_BRG_CFG_CLI)
+static struct bt_mesh_brg_cfg_cli brg_cfg_cli;
+#endif
+
 BT_MESH_SHELL_HEALTH_PUB_DEFINE(health_pub);
 
 static const struct bt_mesh_model root_models[] = {
@@ -105,6 +109,9 @@ static const struct bt_mesh_model root_models[] = {
 #endif
 #if defined(CONFIG_BT_MESH_OD_PRIV_PROXY_SRV)
 	BT_MESH_MODEL_OD_PRIV_PROXY_SRV,
+#endif
+#if defined(CONFIG_BT_MESH_BRG_CFG_CLI)
+	BT_MESH_MODEL_BRG_CFG_CLI(&brg_cfg_cli),
 #endif
 };
 
