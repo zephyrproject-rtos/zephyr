54a40754c497fdb30bf5b7ef49317ee5bc0ee71c
Mario Jaun <mario.jaun@zuehlke.com>
2024-07-30 16:48:54 +0200

    rust: add functions to allocate kernel objects
    Add helper functions to allocate and free kernel objects.
    The thread stacks should use the dynamic thread stack pool
    and other kernel objects should use the threads resource
    pool.
    
    Signed-off-by: Mario Jaun <mario.jaun@zuehlke.com>

--- lib/rust/zephyr/src/heap.rs
+++ lib/rust/zephyr/src/heap.rs
@@ -2,7 +2,36 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use core::alloc::{GlobalAlloc, Layout};
-use core::ffi::c_void;
+use core::ffi::{c_int, c_uint, c_void};
+use crate::errno::{check_ptr_mut, ENOMEM, ErrnoResult};
+
+pub(crate) trait KernelObject {
+    fn get_type_id() -> c_uint;
+}
+
+impl KernelObject for zephyr_sys::k_thread {
+    fn get_type_id() -> c_uint {
+        zephyr_sys::k_objects_K_OBJ_THREAD
+    }
+}
+
+impl KernelObject for zephyr_sys::k_mutex {
+    fn get_type_id() -> c_uint {
+        zephyr_sys::k_objects_K_OBJ_MUTEX
+    }
+}
+
+pub(crate) unsafe fn object_alloc<T: KernelObject>() -> ErrnoResult<*mut T> {
+    check_ptr_mut(zephyr_sys::k_object_alloc(T::get_type_id()) as *mut T, ENOMEM)
+}
+
+pub(crate) unsafe fn object_free<T: KernelObject>(obj: *mut T) {
+    zephyr_sys::k_object_free(obj as *mut c_void);
+}
+
+pub(crate) unsafe fn thread_stack_alloc(size: usize, flags: u32) -> ErrnoResult<*mut zephyr_sys::k_thread_stack_t> {
+    check_ptr_mut(zephyr_sys::k_thread_stack_alloc(size, flags as c_int), ENOMEM)
+}
 
 extern "C" {
     static mut rust_heap: zephyr_sys::k_heap;
