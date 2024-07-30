7087a7f2ee6fb3c7f4588dd34bbac378f877c72f
Pisit Sawangvonganan <pisit@ndrsolution.com>
2024-07-31 02:32:47 +0700

    ztest: fix typo
    Utilize a code spell-checking tool to scan for and correct spelling errors
    in `subsys/testsuite/ztest/src/ztest.c` file.
    
    Signed-off-by: Pisit Sawangvonganan <pisit@ndrsolution.com>

--- subsys/testsuite/ztest/src/ztest.c
+++ subsys/testsuite/ztest/src/ztest.c
@@ -948,7 +948,7 @@ static void __ztest_show_suite_summary_oneline(struct ztest_suite_node *suite)
 	struct ztest_unit_test *test = NULL;
 	unsigned int suite_duration_worst_ms = 0;
 
-	/** summary of disctinct run  */
+	/** summary of distinct run  */
 	while (((test = z_ztest_get_next_test(suite->name, test)) != NULL)) {
 		distinct_total++;
 		suite_duration_worst_ms += test->stats->duration_worst_ms;
@@ -1022,7 +1022,7 @@ static void __ztest_show_suite_summary_verbose(struct ztest_suite_node *suite)
 		}
 
 		if (flush_frequency % 3 == 0) {
-			/** Reduce the flush frequencey a bit to speed up the output */
+			/** Reduce the flush frequency a bit to speed up the output */
 			flush_log();
 		}
 		flush_frequency++;
@@ -1286,7 +1286,7 @@ static int cmd_shuffle(const struct shell *sh, size_t argc, char **argv)
 			val = atoi(state->optarg);
 			if (val < 1) {
 				shell_fprintf(sh, SHELL_ERROR,
-					      "Invalid number of suite interations\n");
+					      "Invalid number of suite iterations\n");
 				return -ENOEXEC;
 			}
 			suite_iter = val;
@@ -1296,7 +1296,7 @@ static int cmd_shuffle(const struct shell *sh, size_t argc, char **argv)
 			val = atoi(state->optarg);
 			if (val < 1) {
 				shell_fprintf(sh, SHELL_ERROR,
-					      "Invalid number of case interations\n");
+					      "Invalid number of case iterations\n");
 				return -ENOEXEC;
 			}
 			case_iter = val;
