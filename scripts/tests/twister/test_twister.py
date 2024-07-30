deeec3393479c4da84eaec1576765369a39d42d8
Wilfried Chauveau <wilfried.chauveau@arm.com>
2024-07-30 17:21:38 +0100

    scripts: twister: Solve deprecation notice when running tests
    CONF_FILE, DTC_OVERLAY_FILE and OVERLAY_CONFIG are deprecated but still
    used by the tests causing warnings when running them.
    
    This adds a test_data specific to validate the emission of the warning,
    and removes the offending args from the other test_data files.
    
    Signed-off-by: Wilfried Chauveau <wilfried.chauveau@arm.com>

--- scripts/tests/twister/test_twister.py
+++ scripts/tests/twister/test_twister.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # Copyright (c) 2020 Intel Corporation
+# Copyright (c) 2024 Arm Limited (or its affiliates). All rights reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
 """
@@ -8,6 +9,7 @@ This test file contains foundational testcases for Twister tool
 
 import os
 import sys
+import mock
 import pytest
 
 from pathlib import Path
@@ -51,17 +53,24 @@ def test_incorrect_schema(filename, schema, test_data):
 
 def test_testsuite_config_files():
     """ Test to validate conf and overlay files are extracted properly """
-    filename = Path(ZEPHYR_BASE) / "scripts/tests/twister/test_data/testsuites/tests/test_config/test_data.yaml"
+    filename = Path(ZEPHYR_BASE) / "scripts/tests/twister/test_data/test_data_with_deprecation_warnings.yaml"
     schema = scl.yaml_load(Path(ZEPHYR_BASE) / "scripts/schemas/twister/testsuite-schema.yaml")
     data = TwisterConfigParser(filename, schema)
     data.load()
 
-    # Load and validate the specific scenario from testcases.yaml
-    scenario = data.get_scenario("test_config.main")
-    assert scenario
+    with mock.patch('warnings.warn') as warn_mock:
+        # Load and validate the specific scenario from testcases.yaml
+        scenario = data.get_scenario("test_config.main")
+        assert scenario
 
-    # CONF_FILE, DTC_OVERLAY_FILE, OVERLAY_CONFIG fields should be stripped out
-    # of extra_args. Other fields should remain untouched.
+        # CONF_FILE, DTC_OVERLAY_FILE, OVERLAY_CONFIG fields should be stripped out
+        # of extra_args. Other fields should remain untouched.
+        warn_mock.assert_called_once_with("Do not specify CONF_FILE, OVERLAY_CONFIG, or "
+                                          "DTC_OVERLAY_FILE in extra_args. This feature is "
+                                          "deprecated and will soon result in an error. Use "
+                                          "extra_conf_files, extra_overlay_confs or "
+                                          "extra_dtc_overlay_files YAML fields instead", DeprecationWarning)
+
     assert scenario["extra_args"] == ["UNRELATED1=abc", "UNRELATED2=xyz"]
 
     # Check that all conf files have been assembled in the correct order
