10408a7b13162355e4dac848fe997d3362a44ccf
David Brown <david.brown@linaro.org>
2024-07-31 15:22:24 -0600

    samples: rust: Change philosophers to use sys::Mutex
    This eliminates the unsafe operations here.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

--- samples/rust/philosophers_rust/src/lib.rs
+++ samples/rust/philosophers_rust/src/lib.rs
@@ -3,20 +3,16 @@
 
 #![no_std]
 
-use core::ptr::addr_of_mut;
-
 use zephyr::{
     printkln,
     kernel_stack_define,
     kernel_thread_define,
+    sys_mutex_define,
 };
+use zephyr::object::KobjInit;
 use zephyr_sys::{
     k_timeout_t,
-    k_mutex_init,
-    k_mutex_lock,
-    k_mutex_unlock,
     k_msleep,
-    k_mutex,
 };
 
 #[no_mangle]
@@ -25,16 +21,22 @@ extern "C" fn rust_main() {
               zephyr::kconfig::CONFIG_BOARD);
 
     // Slightly awkward
+    /*
     unsafe {
         for i in 0..FORKS.len() {
             k_mutex_init(addr_of_mut!(FORKS[i]));
         }
     }
+    */
+    FORK1.init();
+    let fork = FORK1.get();
 
+    printkln!("Pre fork");
     // PHIL_THREAD.simple_spawn(PHIL_STACK.token(), phil_thread);
     PHIL_THREAD.spawn(PHIL_STACK.token(), move || {
         phil_thread(1);
     });
+    printkln!("Post fork");
 
     // Steal the fork long enough to make this interesting.
     unsafe {
@@ -43,16 +45,15 @@ extern "C" fn rust_main() {
         printkln!("Done with sleep in main");
     }
     loop {
-        unsafe {
-            printkln!("stealing fork");
-            k_mutex_lock(addr_of_mut!(FORKS[0]), FOREVER);
-            printkln!("fork stolen");
+        printkln!("stealing fork");
+        fork.lock(FOREVER);
+        printkln!("fork stolen");
             // k_busy_wait(1_000_000);
-            k_msleep(1_000);
-            printkln!("Main: giving up fork");
-            k_mutex_unlock(addr_of_mut!(FORKS[0]));
-            k_msleep(1);
-        }
+        unsafe { k_msleep(1_000) };
+        printkln!("Main: giving up fork");
+
+        fork.unlock();
+        unsafe { k_msleep(1) };
     }
 }
 
@@ -60,20 +61,22 @@ const FOREVER: k_timeout_t = k_timeout_t { ticks: !0 };
 
 fn phil_thread(n: usize) {
     printkln!("Child {} started", n);
+    let fork = FORK1.get();
     // Print out periodically.
     loop {
-        unsafe {
-            k_mutex_lock(addr_of_mut!(FORKS[0]), FOREVER);
-            printkln!("Child eat");
-            k_mutex_unlock(addr_of_mut!(FORKS[0]));
-            k_msleep(500);
-        }
+        fork.lock(FOREVER);
+        printkln!("Child eat");
+        fork.unlock();
+        unsafe { k_msleep(500) };
     }
 }
 
+/*
 #[used]
 #[link_section = concat!(".", "_k_mutex", ".static.", "FORKS_")]
 static mut FORKS: [k_mutex; 6] = unsafe { core::mem::zeroed() };
+*/
+sys_mutex_define!(FORK1);
 
 kernel_stack_define!(PHIL_STACK, 4096);
 kernel_thread_define!(PHIL_THREAD);
