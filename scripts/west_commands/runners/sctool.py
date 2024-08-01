6a07a4abd3d64011cdd40553cf809fb309ce6fd8
Andrii Bilynskyi <andrii.bilynskyi@telink-semi.com>
2024-08-01 16:40:41 +0300

    soc: riscv: telink_w91: Add N22 binaries
    Automatically download N22 core binaries from Telink Wiki during
    west build
    
    Signed-off-by: Andrii Bilynskyi <andrii.bilynskyi@telink-semi.com>

--- scripts/west_commands/runners/sctool.py
+++ scripts/west_commands/runners/sctool.py
@@ -10,12 +10,13 @@ from runners.core import ZephyrBinaryRunner, RunnerCaps, BuildConfiguration
 class SCTOOLBinaryRunner(ZephyrBinaryRunner):
     '''Runner front-end for Senscomm Flash Tool.'''
 
-    def __init__(self, cfg, sctool_path, usb_port, address, erase=False):
+    def __init__(self, cfg, sctool_path, usb_port, address, erase=False, update_n22=False):
         super().__init__(cfg)
         self.sctool_path = sctool_path
         self.usb_port = usb_port
         self.address = address
         self.erase = bool(erase)
+        self.update_n22 = bool(update_n22)
 
     @classmethod
     def name(cls):
@@ -33,6 +34,8 @@ class SCTOOLBinaryRunner(ZephyrBinaryRunner):
             help='USB CDC to which device is attached')
         parser.add_argument('--address', default='0x80000000',
             help='start flash address to write')
+        parser.add_argument("--update-n22", action="store_true",
+            help="upadet N22 FW")
 
     @classmethod
     def do_create(cls, cfg, args):
@@ -41,7 +44,7 @@ class SCTOOLBinaryRunner(ZephyrBinaryRunner):
         else:
             sctool_path = os.getenv('TELINK_SCTOOL_BASE_DIR')
 
-        return SCTOOLBinaryRunner(cfg, sctool_path, args.usb_port, args.address, args.erase)
+        return SCTOOLBinaryRunner(cfg, sctool_path, args.usb_port, args.address, args.erase, args.update_n22)
 
     def do_run(self, command, **kwargs):
         self.require(self.sctool_path + '/sctool')
@@ -62,6 +65,7 @@ class SCTOOLBinaryRunner(ZephyrBinaryRunner):
             exit()
         # get flash size
         flash_size = hex(build_conf['CONFIG_FLASH_SIZE'] * 1024)
+        print('Flashing D25 FW...')
         # erase flash
         if self.erase:
             erase = subprocess.Popen(['./sctool', '-p', self.usb_port, '-da', 'da/da.ram.bin', '-b', '115200', '--manual', 'flash_erase', '0x80000000', flash_size], cwd=self.sctool_path)
@@ -73,3 +77,21 @@ class SCTOOLBinaryRunner(ZephyrBinaryRunner):
         flash = subprocess.Popen(['./sctool', '-p', self.usb_port, '-da', 'da/da.ram.bin', '--after', 'hw_reset', '-b', '115200', '--manual', 'flash_write', self.address, bin_file], cwd=self.sctool_path)
         if flash.wait():
             exit()
+        print('D25 FW done!')
+
+        if self.update_n22:
+            print('Flashing N22 FW...')
+            # check N22 firmware
+            n22_fw_bin = os.path.dirname(bin_file) + '/n22.bin'
+            if not os.path.isfile(n22_fw_bin):
+                print(f'N22 FW not exist {n22_fw_bin}')
+                exit()
+            # erase N22 area
+            erase = subprocess.Popen(['./sctool', '-p', self.usb_port, '-da', 'da/da.ram.bin', '-b', '115200', '--manual', 'flash_erase', '0x80300000', '0x100000'], cwd=self.sctool_path)
+            if erase.wait():
+                exit()
+            # flash N22 FW
+            flash = subprocess.Popen(['./sctool', '-p', self.usb_port, '-da', 'da/da.ram.bin', '--after', 'hw_reset', '-b', '115200', '--manual', 'flash_write', '0x80300000', n22_fw_bin], cwd=self.sctool_path)
+            if flash.wait():
+                exit()
+            print('N22 FW done!')
