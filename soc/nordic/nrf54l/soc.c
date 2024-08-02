fcbc410dc39b33d7c9b6116752ff87325d128271
Karol Lasończyk <karol.lasonczyk@nordicsemi.no>
2024-08-02 15:30:35 +0200

    soc: Add support for nRF54L20 SoC
    Introduce nRF54L20 entries in soc directory.
    
    Signed-off-by: Karol Lasończyk <karol.lasonczyk@nordicsemi.no>

--- soc/nordic/nrf54l/soc.c
+++ soc/nordic/nrf54l/soc.c
@@ -12,6 +12,8 @@
  * for the Nordic Semiconductor nRF54L family processor.
  */
 
+#include <zephyr/devicetree.h>
+#include <zephyr/dt-bindings/regulator/nrf5x.h>
 #include <zephyr/kernel.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/init.h>
