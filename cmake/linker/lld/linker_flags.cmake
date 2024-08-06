aaaeff89cf4cce0b3183a36be294de0cd472c8cd
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2024-08-06 10:13:31 +0200

    cmake: improve Zephyr link phase
    Zephyr is a bare metal build where standard libs are disabled.
    
    This means that c and runtime libraries must manually be linked in.
    
    This has generally been handled by using CMake's link libraries handling
    but the issue with that is both de-duplication but also library link
    order.
    
    Standard libraries must be linked at last location to ensure symbols
    are always available, however this is not optimal with
    target_link_libraries() because this would ultimately require every
    library to know the c library to link with, which is not desired.
    
    Therefore, setup standard C and runtime library linking in linker
    CMake files for toolchains where this is required.
    
    This commit expands the principle introduced with toolchain abstraction,
    see PR#24851.
    
    This means that a toolchain implementation may specify standard C,
    runtime, C++, etc libraries, as well as their link order.
    Because a property approach is used, then Zephyr modules, such as the
    Picolibc module can adjust such properties.
    
    An optional `zephyr_linker_finalize()` macro is called at the end of
    Zephyr's CMakeList process and can be used by the toolchain
    implementation to define the final linker invocation.
    
    This aligns the linker handling flow to the principle introduced in
    PR#24851 and improves the flexibility and robustness of Zephyr build
    system.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/linker/lld/linker_flags.cmake
+++ cmake/linker/lld/linker_flags.cmake
@@ -1,4 +1,6 @@
 # Copyright (c) 2022 Google LLC
+# Copyright (c) 2024 Nordic Semiconductor
+#
 # SPDX-License-Identifier: Apache-2.0
 
 # Since lld is a drop in replacement for ld, we can just use ld's flags as a base
@@ -6,7 +8,7 @@
 include(${ZEPHYR_BASE}/cmake/linker/ld/linker_flags.cmake OPTIONAL)
 
 if(NOT CONFIG_MINIMAL_LIBCPP AND NOT CONFIG_NATIVE_LIBRARY AND NOT CONFIG_EXTERNAL_MODULE_LIBCPP)
-  set_property(TARGET linker PROPERTY cpp_base -lc++ ${LINKERFLAGPREFIX},-z,norelro)
+  set_property(TARGET linker PROPERTY cpp_base ${LINKERFLAGPREFIX},-z,norelro)
 endif()
 
 # Force LLVM to use built-in lld linker
