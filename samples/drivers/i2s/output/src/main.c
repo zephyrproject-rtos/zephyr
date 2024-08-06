850a617908860479adc6e63be8963fa814650f55
mario.paja <mario.paja@zal.aero>
2024-08-06 15:01:25 +0200

    drivers: i2s: add sai support for stm32u5xx
    This PR adds initial sai support for STM32u5xx
    
    Signed-off-by: Mario Paja <mario.paja@zal.aero>

--- samples/drivers/i2s/output/src/main.c
+++ samples/drivers/i2s/output/src/main.c
@@ -13,14 +13,12 @@
 
 /* The data represent a sine wave */
 static int16_t data[SAMPLE_NO] = {
-	  3211,   6392,   9511,  12539,  15446,  18204,  20787,  23169,
-	 25329,  27244,  28897,  30272,  31356,  32137,  32609,  32767,
-	 32609,  32137,  31356,  30272,  28897,  27244,  25329,  23169,
-	 20787,  18204,  15446,  12539,   9511,   6392,   3211,      0,
-	 -3212,  -6393,  -9512, -12540, -15447, -18205, -20788, -23170,
-	-25330, -27245, -28898, -30273, -31357, -32138, -32610, -32767,
-	-32610, -32138, -31357, -30273, -28898, -27245, -25330, -23170,
-	-20788, -18205, -15447, -12540,  -9512,  -6393,  -3212,     -1,
+	3211,   6392,   9511,   12539,  15446,  18204,  20787,  23169,  25329,  27244,  28897,
+	30272,  31356,  32137,  32609,  32767,  32609,  32137,  31356,  30272,  28897,  27244,
+	25329,  23169,  20787,  18204,  15446,  12539,  9511,   6392,   3211,   0,      -3212,
+	-6393,  -9512,  -12540, -15447, -18205, -20788, -23170, -25330, -27245, -28898, -30273,
+	-31357, -32138, -32610, -32767, -32610, -32138, -31357, -30273, -28898, -27245, -25330,
+	-23170, -20788, -18205, -15447, -12540, -9512,  -6393,  -3212,  -1,
 };
 
 /* Fill buffer with sine wave on left channel, and sine wave shifted by
@@ -33,10 +31,10 @@ static void fill_buf(int16_t *tx_block, int att)
 
 	for (int i = 0; i < SAMPLE_NO; i++) {
 		/* Left channel is sine wave */
-		tx_block[2 * i] = data[i] / (1 << att);
+		tx_block[2 * i] = data[i];
 		/* Right channel is same sine wave, shifted by 90 degrees */
 		r_idx = (i + (ARRAY_SIZE(data) / 4)) % ARRAY_SIZE(data);
-		tx_block[2 * i + 1] = data[r_idx] / (1 << att);
+		tx_block[2 * i + 1] = data[i];
 	}
 }
 
@@ -44,17 +42,16 @@ static void fill_buf(int16_t *tx_block, int att)
 #define BLOCK_SIZE (2 * sizeof(data))
 
 #ifdef CONFIG_NOCACHE_MEMORY
-	#define MEM_SLAB_CACHE_ATTR __nocache
+#define MEM_SLAB_CACHE_ATTR __nocache
 #else
-	#define MEM_SLAB_CACHE_ATTR
+#define MEM_SLAB_CACHE_ATTR
 #endif /* CONFIG_NOCACHE_MEMORY */
 
-static char MEM_SLAB_CACHE_ATTR __aligned(WB_UP(32))
-	_k_mem_slab_buf_tx_0_mem_slab[(NUM_BLOCKS) * WB_UP(BLOCK_SIZE)];
+static char MEM_SLAB_CACHE_ATTR
+	__aligned(WB_UP(32)) _k_mem_slab_buf_tx_0_mem_slab[(NUM_BLOCKS)*WB_UP(BLOCK_SIZE)];
 
-static STRUCT_SECTION_ITERABLE(k_mem_slab, tx_0_mem_slab) =
-	Z_MEM_SLAB_INITIALIZER(tx_0_mem_slab, _k_mem_slab_buf_tx_0_mem_slab,
-				WB_UP(BLOCK_SIZE), NUM_BLOCKS);
+static STRUCT_SECTION_ITERABLE(k_mem_slab, tx_0_mem_slab) = Z_MEM_SLAB_INITIALIZER(
+	tx_0_mem_slab, _k_mem_slab_buf_tx_0_mem_slab, WB_UP(BLOCK_SIZE), NUM_BLOCKS);
 
 int main(void)
 {
@@ -62,12 +59,19 @@ int main(void)
 	struct i2s_config i2s_cfg;
 	int ret;
 	uint32_t tx_idx;
-	const struct device *dev_i2s = DEVICE_DT_GET(DT_ALIAS(i2s_tx));
+	const struct device *sai1_a = DEVICE_DT_GET(DT_ALIAS(sai1a));
+	const struct device *sai1_b = DEVICE_DT_GET(DT_ALIAS(sai1b));
 
-	if (!device_is_ready(dev_i2s)) {
+	if (!device_is_ready(sai1_a)) {
 		printf("I2S device not ready\n");
 		return -ENODEV;
 	}
+
+	if (!device_is_ready(sai1_b)) {
+		printf("I2S device not ready\n");
+		return -ENODEV;
+	}
+
 	/* Configure I2S stream */
 	i2s_cfg.word_size = 16U;
 	i2s_cfg.channels = 2U;
@@ -76,10 +80,14 @@ int main(void)
 	i2s_cfg.block_size = BLOCK_SIZE;
 	i2s_cfg.timeout = 2000;
 	/* Configure the Transmit port as Master */
-	i2s_cfg.options = I2S_OPT_FRAME_CLK_MASTER
-			| I2S_OPT_BIT_CLK_MASTER;
+	i2s_cfg.options = I2S_OPT_FRAME_CLK_MASTER | I2S_OPT_BIT_CLK_MASTER;
 	i2s_cfg.mem_slab = &tx_0_mem_slab;
-	ret = i2s_configure(dev_i2s, I2S_DIR_TX, &i2s_cfg);
+	ret = i2s_configure(sai1_a, I2S_DIR_TX, &i2s_cfg);
+	if (ret < 0) {
+		printf("Failed to configure I2S stream\n");
+		return ret;
+	}
+	ret = i2s_configure(sai1_b, I2S_DIR_TX, &i2s_cfg);
 	if (ret < 0) {
 		printf("Failed to configure I2S stream\n");
 		return ret;
@@ -87,8 +95,7 @@ int main(void)
 
 	/* Prepare all TX blocks */
 	for (tx_idx = 0; tx_idx < NUM_BLOCKS; tx_idx++) {
-		ret = k_mem_slab_alloc(&tx_0_mem_slab, &tx_block[tx_idx],
-				       K_FOREVER);
+		ret = k_mem_slab_alloc(&tx_0_mem_slab, &tx_block[tx_idx], K_FOREVER);
 		if (ret < 0) {
 			printf("Failed to allocate TX block\n");
 			return ret;
@@ -98,27 +105,49 @@ int main(void)
 
 	tx_idx = 0;
 	/* Send first block */
-	ret = i2s_write(dev_i2s, tx_block[tx_idx++], BLOCK_SIZE);
+	ret = i2s_write(sai1_a, tx_block[tx_idx], BLOCK_SIZE);
+	if (ret < 0) {
+		printf("Could not write TX buffer %d\n", tx_idx);
+		return ret;
+	}
+	ret = i2s_write(sai1_b, tx_block[tx_idx++], BLOCK_SIZE);
 	if (ret < 0) {
 		printf("Could not write TX buffer %d\n", tx_idx);
 		return ret;
 	}
 	/* Trigger the I2S transmission */
-	ret = i2s_trigger(dev_i2s, I2S_DIR_TX, I2S_TRIGGER_START);
+	ret = i2s_trigger(sai1_a, I2S_DIR_TX, I2S_TRIGGER_START);
+	if (ret < 0) {
+		printf("Could not trigger I2S tx\n");
+		return ret;
+	}
+	ret = i2s_trigger(sai1_b, I2S_DIR_TX, I2S_TRIGGER_START);
 	if (ret < 0) {
 		printf("Could not trigger I2S tx\n");
 		return ret;
 	}
 
-	for (; tx_idx < NUM_BLOCKS; ) {
-		ret = i2s_write(dev_i2s, tx_block[tx_idx++], BLOCK_SIZE);
+	for (; tx_idx < NUM_BLOCKS;) {
+		ret = i2s_write(sai1_a, tx_block[tx_idx], BLOCK_SIZE);
+		if (ret < 0) {
+			printf("Could not write TX buffer %d\n", tx_idx);
+			return ret;
+		}
+		ret = i2s_write(sai1_b, tx_block[tx_idx++], BLOCK_SIZE);
 		if (ret < 0) {
 			printf("Could not write TX buffer %d\n", tx_idx);
 			return ret;
 		}
 	}
+
 	/* Drain TX queue */
-	ret = i2s_trigger(dev_i2s, I2S_DIR_TX, I2S_TRIGGER_DRAIN);
+	ret = i2s_trigger(sai1_a, I2S_DIR_TX, I2S_TRIGGER_DRAIN);
+	if (ret < 0) {
+		printf("Could not trigger I2S tx\n");
+		return ret;
+	}
+
+	ret = i2s_trigger(sai1_b, I2S_DIR_TX, I2S_TRIGGER_DRAIN);
 	if (ret < 0) {
 		printf("Could not trigger I2S tx\n");
 		return ret;
