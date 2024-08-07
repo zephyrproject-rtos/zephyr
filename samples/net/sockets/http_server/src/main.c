495cdd3ba460bbf68c1412b7d3f0ac55c5ea8ebb
Matt Rodgers <mrodgers@witekio.com>
2024-08-07 17:25:35 +0100

    samples: net: http_server: demonstrate dynamic GET and POST
    Previously the dynamic resource in the http_server sample simply echo'd
    the received data back to the sender. This doesn't demonstrate how the
    dynamic handlers would be used in a more typical use case, where the
    application may need to wait for the full request before acting on it or
    creating a response.
    
    Therefore two new dynamic resources have bee added: a '/led' resource
    which can be used to control the board's LED (if present) via a POST
    request, and a '/uptime' resource which can be used to query the kernel
    uptime via a GET request.
    
    The static resources are updated with some javascript to support
    demonstrating the new dynamic resouces via a web browser.
    
    Signed-off-by: Matt Rodgers <mrodgers@witekio.com>

--- samples/net/sockets/http_server/src/main.c
+++ samples/net/sockets/http_server/src/main.c
@@ -6,6 +6,7 @@
  */
 
 #include <stdio.h>
+#include <inttypes.h>
 
 #include <zephyr/kernel.h>
 #include <zephyr/net/tls_credentials.h>
@@ -13,20 +14,27 @@
 #include <zephyr/net/http/service.h>
 #include <zephyr/net/net_ip.h>
 #include <zephyr/net/socket.h>
+#include "zephyr/device.h"
+#include <zephyr/drivers/led.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(net_http_server_sample, LOG_LEVEL_DBG);
+
+static const struct device *leds_dev = DEVICE_DT_GET_ANY(gpio_leds);
 
 static uint8_t index_html_gz[] = {
 #include "index.html.gz.inc"
 };
 
-#if defined(CONFIG_NET_SAMPLE_HTTP_SERVICE)
-static uint16_t test_http_service_port = CONFIG_NET_SAMPLE_HTTP_SERVER_SERVICE_PORT;
-HTTP_SERVICE_DEFINE(test_http_service, CONFIG_NET_CONFIG_MY_IPV4_ADDR, &test_http_service_port, 1,
-		    10, NULL);
+static uint8_t main_js_gz[] = {
+#include "main.js.gz.inc"
+};
 
-struct http_resource_detail_static index_html_gz_resource_detail = {
+static uint8_t uptime_buf[256];
+static uint8_t led_buf[256];
+static uint8_t echo_buf[1024];
+
+static struct http_resource_detail_static index_html_gz_resource_detail = {
 	.common = {
 			.type = HTTP_RESOURCE_TYPE_STATIC,
 			.bitmask_of_supported_http_methods = BIT(HTTP_GET),
@@ -37,13 +45,19 @@ struct http_resource_detail_static index_html_gz_resource_detail = {
 	.static_data_len = sizeof(index_html_gz),
 };
 
-HTTP_RESOURCE_DEFINE(index_html_gz_resource, test_http_service, "/",
-		     &index_html_gz_resource_detail);
+static struct http_resource_detail_static main_js_gz_resource_detail = {
+	.common = {
+			.type = HTTP_RESOURCE_TYPE_STATIC,
+			.bitmask_of_supported_http_methods = BIT(HTTP_GET),
+			.content_encoding = "gzip",
+			.content_type = "text/javascript",
+		},
+	.static_data = main_js_gz,
+	.static_data_len = sizeof(main_js_gz),
+};
 
-static uint8_t recv_buffer[1024];
-
-static int dyn_handler(struct http_client_ctx *client, enum http_data_status status,
-		       uint8_t *buffer, size_t len, void *user_data)
+static int echo_handler(struct http_client_ctx *client, enum http_data_status status,
+			uint8_t *buffer, size_t len, void *user_data)
 {
 #define MAX_TEMP_PRINT_LEN 32
 	static char print_str[MAX_TEMP_PRINT_LEN];
@@ -75,20 +89,134 @@ static int dyn_handler(struct http_client_ctx *client, enum http_data_status sta
 	return len;
 }
 
-struct http_resource_detail_dynamic dyn_resource_detail = {
+static struct http_resource_detail_dynamic echo_resource_detail = {
 	.common = {
-		.type = HTTP_RESOURCE_TYPE_DYNAMIC,
-		.bitmask_of_supported_http_methods =
-			BIT(HTTP_GET) | BIT(HTTP_POST),
-	},
-	.cb = dyn_handler,
-	.data_buffer = recv_buffer,
-	.data_buffer_len = sizeof(recv_buffer),
+			.type = HTTP_RESOURCE_TYPE_DYNAMIC,
+			.bitmask_of_supported_http_methods = BIT(HTTP_GET) | BIT(HTTP_POST),
+		},
+	.cb = echo_handler,
+	.data_buffer = echo_buf,
+	.data_buffer_len = sizeof(echo_buf),
 	.user_data = NULL,
 };
 
-HTTP_RESOURCE_DEFINE(dyn_resource, test_http_service, "/dynamic",
-		     &dyn_resource_detail);
+static int uptime_handler(struct http_client_ctx *client, enum http_data_status status,
+			  uint8_t *buffer, size_t len, void *user_data)
+{
+	static bool response_sent;
+
+	LOG_DBG("Uptime handler status %d, response_sent %d", status, response_sent);
+
+	switch (status) {
+	case HTTP_SERVER_DATA_ABORTED: {
+		response_sent = false;
+		return 0;
+	}
+
+	case HTTP_SERVER_DATA_MORE: {
+		/* A payload is not expected with the GET request. Ignore any data and wait until
+		 * final callback before sending response
+		 */
+		return 0;
+	}
+
+	case HTTP_SERVER_DATA_FINAL: {
+		if (response_sent) {
+			/* Response already sent, return 0 to indicate to server that the callback
+			 * does not need to be called again.
+			 */
+			response_sent = false;
+			return 0;
+		}
+
+		response_sent = true;
+		return snprintf(buffer, sizeof(uptime_buf), "%" PRId64, k_uptime_get());
+	}
+	default: {
+		LOG_WRN("Unexpected status %d", status);
+		return -1;
+	}
+	}
+}
+
+static struct http_resource_detail_dynamic uptime_resource_detail = {
+	.common = {
+			.type = HTTP_RESOURCE_TYPE_DYNAMIC,
+			.bitmask_of_supported_http_methods = BIT(HTTP_GET),
+		},
+	.cb = uptime_handler,
+	.data_buffer = uptime_buf,
+	.data_buffer_len = sizeof(uptime_buf),
+	.user_data = NULL,
+};
+
+static void led_control(bool on)
+{
+	LOG_INF("POST request turning LED %s", on ? "on" : "off");
+
+	if (leds_dev != NULL) {
+		if (on) {
+			led_on(leds_dev, 0);
+		} else {
+			led_off(leds_dev, 0);
+		}
+	}
+}
+
+static void parse_led_post(uint8_t *buf, size_t len)
+{
+	if ((len == sizeof("led=on") - 1) && (0 == memcmp("led=on", buf, len))) {
+		led_control(true);
+	} else if ((len == sizeof("led=off") - 1) && (0 == memcmp("led=off", buf, len))) {
+		led_control(false);
+	} else {
+		LOG_WRN("Unexpected POST payload");
+	}
+}
+
+static int led_handler(struct http_client_ctx *client, enum http_data_status status,
+		       uint8_t *buffer, size_t len, void *user_data)
+{
+	static uint8_t post_payload_buf[32];
+	static size_t cursor;
+
+	LOG_DBG("LED handler status %d, size %zu", status, len);
+
+	if (status == HTTP_SERVER_DATA_ABORTED) {
+		cursor = 0;
+		return 0;
+	}
+
+	if (len + cursor > sizeof(post_payload_buf)) {
+		cursor = 0;
+		return -ENOMEM;
+	}
+
+	/* Copy payload to our buffer. Note that even for a small payload, it may arrive split into
+	 * chunks (e.g. if the header size was such that the whole HTTP request exceeds the size of
+	 * the client buffer).
+	 */
+	memcpy(post_payload_buf + cursor, buffer, len);
+	cursor += len;
+
+	if (status == HTTP_SERVER_DATA_FINAL) {
+		parse_led_post(post_payload_buf, cursor);
+		cursor = 0;
+	}
+
+	return 0;
+}
+
+static struct http_resource_detail_dynamic led_resource_detail = {
+	.common = {
+			.type = HTTP_RESOURCE_TYPE_DYNAMIC,
+			.bitmask_of_supported_http_methods = BIT(HTTP_POST),
+		},
+	.cb = led_handler,
+	.data_buffer = led_buf,
+	.data_buffer_len = sizeof(led_buf),
+	.user_data = NULL,
+};
 
 #if defined(CONFIG_NET_SAMPLE_WEBSOCKET_SERVICE)
 extern int ws_setup(int ws_socket, void *user_data);
@@ -111,6 +239,24 @@ struct http_resource_detail_websocket ws_resource_detail = {
 HTTP_RESOURCE_DEFINE(ws_resource, test_http_service, "/", &ws_resource_detail);
 
 #endif /* CONFIG_NET_SAMPLE_WEBSOCKET_SERVICE */
+
+#if defined(CONFIG_NET_SAMPLE_HTTP_SERVICE)
+static uint16_t test_http_service_port = CONFIG_NET_SAMPLE_HTTP_SERVER_SERVICE_PORT;
+HTTP_SERVICE_DEFINE(test_http_service, CONFIG_NET_CONFIG_MY_IPV4_ADDR, &test_http_service_port, 1,
+		    10, NULL);
+
+HTTP_RESOURCE_DEFINE(index_html_gz_resource, test_http_service, "/",
+		     &index_html_gz_resource_detail);
+
+HTTP_RESOURCE_DEFINE(main_js_gz_resource, test_http_service, "/main.js",
+		     &main_js_gz_resource_detail);
+
+HTTP_RESOURCE_DEFINE(echo_resource, test_http_service, "/dynamic", &echo_resource_detail);
+
+HTTP_RESOURCE_DEFINE(uptime_resource, test_http_service, "/uptime", &uptime_resource_detail);
+
+HTTP_RESOURCE_DEFINE(led_resource, test_http_service, "/led", &led_resource_detail);
+
 #endif /* CONFIG_NET_SAMPLE_HTTP_SERVICE */
 
 #if defined(CONFIG_NET_SAMPLE_HTTPS_SERVICE)
@@ -128,18 +274,17 @@ HTTPS_SERVICE_DEFINE(test_https_service, CONFIG_NET_CONFIG_MY_IPV4_ADDR,
 		     &test_https_service_port, 1, 10, NULL,
 		     sec_tag_list_verify_none, sizeof(sec_tag_list_verify_none));
 
-static struct http_resource_detail_static index_html_gz_resource_detail_https = {
-	.common = {
-			.type = HTTP_RESOURCE_TYPE_STATIC,
-			.bitmask_of_supported_http_methods = BIT(HTTP_GET),
-			.content_encoding = "gzip",
-		},
-	.static_data = index_html_gz,
-	.static_data_len = sizeof(index_html_gz),
-};
-
 HTTP_RESOURCE_DEFINE(index_html_gz_resource_https, test_https_service, "/",
-		     &index_html_gz_resource_detail_https);
+		     &index_html_gz_resource_detail);
+
+HTTP_RESOURCE_DEFINE(main_js_gz_resource_https, test_https_service, "/main.js",
+		     &main_js_gz_resource_detail);
+
+HTTP_RESOURCE_DEFINE(echo_resource_https, test_https_service, "/dynamic", &echo_resource_detail);
+
+HTTP_RESOURCE_DEFINE(uptime_resource_https, test_https_service, "/uptime", &uptime_resource_detail);
+
+HTTP_RESOURCE_DEFINE(led_resource_https, test_https_service, "/led", &led_resource_detail);
 
 #endif /* CONFIG_NET_SAMPLE_HTTPS_SERVICE */
 
