f216c434d01c7b9c540fd13fdbb55ffe8b1b276c
Jimmy Zheng <jimmyzhe@andestech.com>
2024-08-07 09:48:58 +0800

    soc: gd32: gd32vf103: keep the mcause.interrupt by SOC-specific context
    For Nuclei ECLIC, the interrupt level (mintstatus.MIL) is restored from
    the previous interrupt level (mcause.MPIL) only if mcause.interrupt is set.
    This behavior is not defined in the RISC-V CLIC spec.
    If an ISR causes a context switch and mcause.interrupt is not set in the
    next context (e.g. the next context is yielded from ecall), interrupts will
    be masked after MRET because the interrupt level is not restored.
    
    Use SOC-specific context to set mcause.interrupt to ensure the interrupt
    level is restored correctly.
    
    Signed-off-by: Jimmy Zheng <jimmyzhe@andestech.com>

---
+++ soc/gd/gd32/gd32vf103/soc_context.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024 Andes Technology Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef SOC_RISCV_GD32_GD32VF103_SOC_CONTEXT_H
+#define SOC_RISCV_GD32_GD32VF103_SOC_CONTEXT_H
+
+#ifdef CONFIG_RISCV_SOC_CONTEXT_SAVE
+
+	#define SOC_ESF_MEMBERS
+
+	#define SOC_ESF_INIT
+
+#endif  /* CONFIG_RISCV_SOC_CONTEXT_SAVE */
+
+#endif  /* SOC_RISCV_GD32_GD32VF103_SOC_CONTEXT_H */
