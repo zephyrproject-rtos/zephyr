8a8f2814cd033bef652670246db2369047a1fc50
Matt Rodgers <mrodgers@witekio.com>
2024-08-07 17:25:35 +0100

    samples: net: http_server: demonstrate dynamic GET and POST
    Previously the dynamic resource in the http_server sample simply echo'd
    the received data back to the sender. This doesn't demonstrate how the
    dynamic handlers would be used in a more typical use case, where the
    application may need to wait for the full request before acting on it or
    creating a response.
    
    Therefore two new dynamic resources have bee added: a '/led' resource
    which can be used to control the board's LED (if present) via a POST
    request, and a '/uptime' resource which can be used to query the kernel
    uptime via a GET request.
    
    The static resources are updated with some javascript to support
    demonstrating the new dynamic resouces via a web browser.
    
    Signed-off-by: Matt Rodgers <mrodgers@witekio.com>

---
+++ samples/net/sockets/http_server/src/static_web_resources/main.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2024, Witekio
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+async function fetchUptime()
+{
+	try {
+		const response = await fetch("/uptime");
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`);
+		}
+
+		const json = await response.json();
+		const uptime = document.getElementById("uptime");
+		uptime.innerHTML = "Uptime: " + json + " milliseconds"
+	} catch (error) {
+		console.error(error.message);
+	}
+}
+
+async function postLed(body)
+{
+	try {
+		const response = await fetch("/led", {method : "POST", body : body});
+		if (!response.ok) {
+			throw new Error(`Response satus: ${response.status}`);
+		}
+	} catch (error) {
+		console.error(error.message);
+	}
+}
+
+window.addEventListener("DOMContentLoaded", (ev) => {
+	/* Fetch the uptime once per second */
+	setInterval(fetchUptime, 1000);
+
+	/* POST to the LED endpoint when the buttons are pressed */
+	const led_on_btn = document.getElementById("led_on");
+	led_on_btn.addEventListener("click", (event) => {
+		console.log("led_on clicked");
+		postLed("led=on");
+	})
+
+	const led_off_btn = document.getElementById("led_off");
+	led_off_btn.addEventListener("click", (event) => {
+		console.log("led_off clicked");
+		postLed("led=off");
+	})
+})
