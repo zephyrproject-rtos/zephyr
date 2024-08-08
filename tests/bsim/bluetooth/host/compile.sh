3b39788af4744abc5516991f3baea4b4aa339346
Jonathan Rico <jonathan.rico@nordicsemi.no>
2024-08-08 15:07:55 +0200

    Bluetooth: Host: Add disconnection during TX fragmentation test
    Verifies that we don't leak connection references when the peer goes out
    of range whilst we are fragmenting and sending data to the controller.
    
    Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

--- tests/bsim/bluetooth/host/compile.sh
+++ tests/bsim/bluetooth/host/compile.sh
@@ -31,6 +31,7 @@ run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/misc/hfc/compile.sh
 run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/misc/hfc_multilink/compile.sh
 app=tests/bsim/bluetooth/host/misc/unregister_conn_cb compile
 run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/misc/sample_test/compile.sh
+run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/misc/acl_tx_frag/compile.sh
 
 run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/privacy/central/compile.sh
 run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/privacy/peripheral/compile.sh
