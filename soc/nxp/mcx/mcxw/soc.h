8f1c992b49bf4104c68df4e846f0fc03ccedb433
Declan Snyder <declan.snyder@nxp.com>
2024-08-08 14:37:15 -0500

    soc: nxp: Add MCXW71
    Add MCXW71 SOC, which inherits some qualitiies
    of kinetis heritage platforms.
    
    Signed-off-by: Declan Snyder <declan.snyder@nxp.com>

---
+++ soc/nxp/mcx/mcxw/soc.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2023 NXP
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#include <fsl_port.h>
+
+#define PORT_MUX_GPIO kPORT_MuxAsGpio
+
+#endif /* _SOC__H_ */
