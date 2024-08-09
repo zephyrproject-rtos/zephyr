4a82b3e91627262e976d972d6c57d8333aaac2c0
Richard Wheatley <richard.wheatley@ambiq.com>
2024-08-09 08:51:34 -0500

    drivers: update AMBIQ drivers to use proper base address
    REG_X_BASEADDR will be removed from all hal files.
    This forces the use of the peripheral base address
    Define MSPI_PORT macro for chip drivers
    
    Signed-off-by: Richard Wheatley <richard.wheatley@ambiq.com>

--- drivers/memc/memc_mspi_aps6404l.c
+++ drivers/memc/memc_mspi_aps6404l.c
@@ -428,8 +428,7 @@ static int memc_mspi_aps6404l_init(const struct device *psram)
 #define MEMC_MSPI_APS6404L(n)                                                                     \
 	static const struct memc_mspi_aps6404l_config                                             \
 	memc_mspi_aps6404l_config_##n = {                                                         \
-		.port               = (DT_REG_ADDR(DT_INST_BUS(n)) - REG_MSPI_BASEADDR) /         \
-					(DT_REG_SIZE(DT_INST_BUS(n)) * 4),                        \
+		.port               = MSPI_PORT(n),                                               \
 		.mem_size           = DT_INST_PROP(n, size) / 8,                                  \
 		.bus                = DEVICE_DT_GET(DT_INST_BUS(n)),                              \
 		.dev_id             = MSPI_DEVICE_ID_DT_INST(n),                                  \
