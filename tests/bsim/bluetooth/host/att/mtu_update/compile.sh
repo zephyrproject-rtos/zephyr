6a4040905694a12fdfb77330ffad2571f0f6fbef
Luis Ubieda <luisf@croxel.com>
2024-08-10 20:18:53 -0400

    tests: bsim: bluetooth: Refactor MTU Update test
    To isolate Central and Peripheral in two separate builds.
    
    Signed-off-by: Luis Ubieda <luisf@croxel.com>

--- tests/bsim/bluetooth/host/att/mtu_update/compile.sh
+++ tests/bsim/bluetooth/host/att/mtu_update/compile.sh
@@ -8,6 +8,7 @@ set -eu
 INCR_BUILD=1
 source ${ZEPHYR_BASE}/tests/bsim/compile.source
 
-app="$(guess_test_relpath)" compile
+app="$(guess_test_relpath)" conf_file=prj_central.conf compile
+app="$(guess_test_relpath)" conf_file=prj_peripheral.conf compile
 
 wait_for_background_jobs
