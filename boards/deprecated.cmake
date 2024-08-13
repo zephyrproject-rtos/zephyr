57358e61aa3721c54bae0abc6f1111c975c6dcd1
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2024-08-13 10:32:15 +0200

    cmake: deprecated.cmake: Link to deprecation instructions
    Let's link to the deprecated instructions.
    To increase visibility and hopefully avoid developers forgetting to
    do so.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- boards/deprecated.cmake
+++ boards/deprecated.cmake
@@ -1,12 +1,17 @@
 # SPDX-License-Identifier: Apache-2.0
 
-# This file contains boards in Zephyr which has been replaced with a new board
+# This file contains boards in Zephyr which have been replaced with a new board
 # name.
 # This allows the system to automatically change the board while at the same
 # time prints a warning to the user, that the board name is deprecated.
 #
 # To add a board rename, add a line in following format:
 # set(<old_board_name>_DEPRECATED <new_board_name>)
+#
+# When adding board aliases here, remember to add a mention in the corresponding GitHub issue
+# tracking the removal of API/options
+# https://docs.zephyrproject.org/latest/develop/api/api_lifecycle.html#deprecated,
+# so these aliases are eventually removed
 
 set(96b_carbon_DEPRECATED
     96b_carbon/stm32f401xe
