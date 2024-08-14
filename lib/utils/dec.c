115672b3cb6c551e73b57c930783461007e642df
Eric Schikschneit <eschik123@yahoo.com>
2024-08-14 16:35:20 -0500

    utils: Expand capability beyond u8, int to ASCII
    This commit will allow users to convert integers with
    different base number systems into ASCII format for use
    with other zephyr subsystems such as CFB.
    
    Signed-off-by: Eric Schikschneit <eschik123@yahoo.com>

--- lib/utils/dec.c
+++ lib/utils/dec.c
@@ -3,31 +3,55 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
+#include <limits.h>
+#include <string.h>
 #include <zephyr/sys/util.h>
 
 uint8_t u8_to_dec(char *buf, uint8_t buflen, uint8_t value)
 {
-	uint8_t divisor = 100;
-	uint8_t num_digits = 0;
-	uint8_t digit;
+	/* Assume base 10 */
+	return (uint8_t)itoa_util(value, buf, buflen, 10);
+}
 
-	while ((buflen > 0) && (divisor > 0)) {
-		digit = value / divisor;
-		if ((digit != 0) || (divisor == 1) || (num_digits != 0)) {
-			*buf = digit + (char)'0';
-			buf++;
-			buflen--;
-			num_digits++;
-		}
+int itoa_util(int value, char *buf, size_t buflen, int base)
+{
+	size_t num_digits = 0;
+	char buffer[sizeof(value) * CHAR_BIT + 2];
+	static const char digits[37] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-		value -= digit * divisor;
-		divisor /= 10;
+	/* Check for valid buffer */
+	if (buf == NULL) {
+		return 0;
 	}
 
-	if (buflen != 0) {
+	/* Check for valid base */
+	if (base < 2 || base > 36) {
 		*buf = '\0';
+		return 0;
 	}
 
-	return num_digits;
+	char *p = &buffer[sizeof(buffer) - 1];
+	*p = '\0';
+
+	int abs_val = value < 0 ? value : -value;
+
+	do {
+		*(--p) = digits[-(abs_val % base)];
+		abs_val /= base;
+	} while (abs_val);
+
+	if (value < 0) {
+		*(--p) = '-';
+	}
+
+	num_digits = &buffer[sizeof(buffer)] - p;
+	if (num_digits > buflen) {
+		/* Buffer too short */
+		/* Only return requested number of bytes */
+		memcpy(buf, p, buflen);
+		return buflen;
+	}
+	memcpy(buf, p, num_digits);
+	/* ignore \0 termination character */
+	return num_digits -= 1;
 }
