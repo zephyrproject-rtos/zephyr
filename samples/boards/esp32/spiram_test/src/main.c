adbf2192dfd6bf0051a28e12a609c0cce81be441
Lucas Tamborrino <lucas.tamborrino@espressif.com>
2024-08-14 08:03:56 -0300

    samples: boards: esp32: spiram test
    Simplifies psram test by using smh API.
    
    Signed-off-by: Lucas Tamborrino <lucas.tamborrino@espressif.com>

--- samples/boards/esp32/spiram_test/src/main.c
+++ samples/boards/esp32/spiram_test/src/main.c
@@ -7,81 +7,30 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <soc/soc_memory_layout.h>
-
-static int check_allocated_memory(int *m1, size_t size)
-{
-	int ret = 0;
-
-	if (size < CONFIG_ESP_HEAP_MIN_EXTRAM_THRESHOLD) {
-		if (!esp_ptr_internal(m1)) {
-			printk("Memory allocation is not within"
-					" specified bounds\n");
-			ret = -1;
-		}
-	} else {
-		if (!esp_ptr_external_ram(m1)) {
-			printk("Memory allocation is not within"
-					" specified bounds\n");
-			ret = -1;
-		}
-	}
-	return ret;
-}
-
-static int test_heap_caps(size_t size)
-{
-	int *m1, *m2;
-	int cnt = 0, err = 0;
-
-	m2 = NULL;
-	/* Allocated as much as we can, and create linked list */
-	while ((m1 = k_malloc(size))) {
-		if (check_allocated_memory(m1, size) == -1) {
-			err = -1;
-			goto ret;
-		}
-		*(int **) m1 = m2;
-		m2 = m1;
-		cnt++;
-	}
-
-	/* Free all allocated memory */
-	while (m2) {
-		m1 = *(int **) m2;
-		k_free(m2);
-		m2 = m1;
-	}
-	/* Confirm that allocation can succeed now */
-	m1 = k_malloc(size);
-	if (check_allocated_memory(m1, size) == -1) {
-		err = -1;
-		goto ret;
-	}
-	if (!m1) {
-		err = -1;
-	} else {
-		k_free(m1);
-		printk("mem test ok! %d\n", cnt);
-	}
-ret:
-	return err;
-}
+#include <zephyr/multi_heap/shared_multi_heap.h>
 
 int main(void)
 {
-	int err = test_heap_caps(10001);
+	int *m_ext, *m_int;
 
-	if (err == -1) {
+	m_ext = shared_multi_heap_aligned_alloc(SMH_REG_ATTR_EXTERNAL, 32, 4096);
+
+	if (!esp_ptr_external_ram(m_ext)) {
 		printk("SPIRAM mem test failed\n");
 	} else {
 		printk("SPIRAM mem test pass\n");
 	}
 
-	err = test_heap_caps(1001);
-	if (err == -1) {
+	shared_multi_heap_free(m_ext);
+
+	m_int = k_malloc(4096);
+	if (!esp_ptr_internal(m_int)) {
 		printk("Internal mem test failed\n");
 	} else {
 		printk("Internal mem test pass\n");
 	}
+
+	k_free(m_int);
+
 	return 0;
 }
