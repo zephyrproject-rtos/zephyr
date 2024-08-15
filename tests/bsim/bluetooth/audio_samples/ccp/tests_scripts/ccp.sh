7298e03cfb9f6a82f0afa92403887d67ab97434e
Emil Gydesen <emil.gydesen@nordicsemi.no>
2024-08-15 15:10:58 +0200

    Bluetooth: CCP: Initial CCP Client implemenation
    Added initial CCP client implementation that simply
    does discovery of TBS on a remote CCP server.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bsim/bluetooth/audio_samples/ccp/tests_scripts/ccp.sh
+++ tests/bsim/bluetooth/audio_samples/ccp/tests_scripts/ccp.sh
@@ -14,7 +14,10 @@ cd ${BSIM_OUT_PATH}/bin
 Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_audio_samples_ccp_call_control_server_prj_conf \
   -v=${verbosity_level} -s=${simulation_id} -d=0 -RealEncryption=1 -testid=ccp_call_control_server
 
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_audio_samples_ccp_call_control_client_prj_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=1 -RealEncryption=1 -testid=ccp_call_control_client
+
 Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
-  -D=1 -sim_length=20e6 $@ -argschannel -at=40
+  -D=2 -sim_length=20e6 $@ -argschannel -at=40
 
 wait_for_background_jobs #Wait for all programs in background and return != 0 if any fails
