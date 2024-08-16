534219c7c007e335f84ada8ce291a92589bec01a
Flavio Ceolin <flavio.ceolin@intel.com>
2024-08-16 15:25:55 -0700

    intel_adsp: ace: Fix undefined reference
    /zephyr/soc/intel/intel_adsp/ace/power.c:46:9: warning: implicit
    declaration of function 'cache_data_flush_range'; did you mean
    'sys_cache_data_flush_range'? [-Wimplicit-function-declaration]
       46 |         cache_data_flush_range((__sparse_force void *)
          |         ^~~~~~~~~~~~~~~~~~~~~~
          |         sys_cache_data_flush_range
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- soc/intel/intel_adsp/ace/power.c
+++ soc/intel/intel_adsp/ace/power.c
@@ -43,7 +43,7 @@ __imr void power_init(void)
 #if CONFIG_SOC_INTEL_ACE15_MTPM
 	*((__sparse_force uint32_t *)sys_cache_cached_ptr_get(&adsp_pending_buffer)) =
 		INTEL_ADSP_ACE15_MAGIC_KEY;
-	cache_data_flush_range((__sparse_force void *)
+	sys_cache_data_flush_range((__sparse_force void *)
 			sys_cache_cached_ptr_get(&adsp_pending_buffer),
 			sizeof(adsp_pending_buffer));
 #endif /* CONFIG_SOC_INTEL_ACE15_MTPM */
