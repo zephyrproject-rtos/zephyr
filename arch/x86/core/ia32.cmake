42362c6fcc17d922ddda8efb8625f4b374c63abb
Yong Cong Sin <ycsin@meta.com>
2024-08-16 15:59:09 +0800

    subsys/profiling: relocate stack unwind backends
    Relocate stack unwind backends from `arch/` to perf's
    `backends/` folder, just like logging/shell/..
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>
    Signed-off-by: Yong Cong Sin <yongcong.sin@gmail.com>

--- arch/x86/core/ia32.cmake
+++ arch/x86/core/ia32.cmake
@@ -25,8 +25,6 @@ zephyr_library_sources_ifdef(CONFIG_GDBSTUB		ia32/gdbstub.c)
 
 zephyr_library_sources_ifdef(CONFIG_DEBUG_COREDUMP	ia32/coredump.c)
 
-zephyr_library_sources_ifdef(CONFIG_PROFILING_PERF	ia32/perf.c)
-
 zephyr_library_sources_ifdef(
   CONFIG_X86_USE_THREAD_LOCAL_STORAGE
   ia32/tls.c
