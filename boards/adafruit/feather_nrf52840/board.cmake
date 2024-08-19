f38b3002e22d916a9448bb61301badd0b67b4007
Jacob Winther <jacob@9.nz>
2024-08-19 20:53:11 +1200

    boards: adafruit feather nrf52840 convert to variants
    Update all versions of adafruit feather nrf52840 to be variants rather
    than separate boards. The aim is to provide a more consistent approach
    and to remove some unnecessary Kconfig redundancy.
    
    This commit also tries to stanardise on using a uf2 variant for uf2
    support as most boards with uf2 have swd headers so its helpful to
    have support for both options.
    
    This commit seperates the two common dts setups into dtsi to avoid
    duplicating the somewhat cryptic flash layouts. I'm not sure if there
    is a better way to do this.
    
    Signed-off-by: Jacob Winther <jacob@9.nz>

--- boards/adafruit/feather_nrf52840/board.cmake
+++ boards/adafruit/feather_nrf52840/board.cmake
@@ -2,10 +2,7 @@
 
 board_runner_args(jlink "--device=nRF52840_xxAA" "--speed=4000")
 board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
-if(CONFIG_BOARD_ADAFRUIT_FEATHER_NRF52840_EXPRESS_UF2)
-include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
-endif()
-if(CONFIG_BOARD_ADAFRUIT_FEATHER_NRF52840_SENSE)
+if(CONFIG_BUILD_OUTPUT_UF2)
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
 endif()
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
