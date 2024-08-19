e0363f252d09acd907c92bfc112d69ff3919587c
Bjarki Arge Andreasen <bjarki.andreasen@nordicsemi.no>
2024-08-19 13:03:10 +0200

    sensor: mcux_acmp: namespace driver and kconfigs
    The mcux_acmp will get support by the comparator subsystem. To avoid
    namespace clashes, namespace the driver, kconfigs and use the
    MCUX_ACMP config solely to select the MCUX SDK driver.
    
    Signed-off-by: Bjarki Arge Andreasen <bjarki.andreasen@nordicsemi.no>

--- drivers/sensor/nxp/mcux_acmp/mcux_acmp.c
+++ drivers/sensor/nxp/mcux_acmp/mcux_acmp.c
@@ -45,9 +45,9 @@ struct mcux_acmp_config {
 	CMP_Type *base;
 	acmp_filter_config_t filter;
 	const struct pinctrl_dev_config *pincfg;
-#ifdef CONFIG_MCUX_ACMP_TRIGGER
+#ifdef CONFIG_SENSOR_MCUX_ACMP_TRIGGER
 	void (*irq_config_func)(const struct device *dev);
-#endif /* CONFIG_MCUX_ACMP_TRIGGER */
+#endif /* CONFIG_SENSOR_MCUX_ACMP_TRIGGER */
 	bool high_speed : 1;
 	bool unfiltered : 1;
 	bool output : 1;
@@ -61,7 +61,7 @@ struct mcux_acmp_data {
 #if MCUX_ACMP_HAS_DISCRETE_MODE
 	acmp_discrete_mode_config_t discrete_config;
 #endif
-#ifdef CONFIG_MCUX_ACMP_TRIGGER
+#ifdef CONFIG_SENSOR_MCUX_ACMP_TRIGGER
 	const struct device *dev;
 	sensor_trigger_handler_t rising_handler;
 	const struct sensor_trigger *rising_trigger;
@@ -69,7 +69,7 @@ struct mcux_acmp_data {
 	const struct sensor_trigger *falling_trigger;
 	struct k_work work;
 	volatile uint32_t status;
-#endif /* CONFIG_MCUX_ACMP_TRIGGER */
+#endif /* CONFIG_SENSOR_MCUX_ACMP_TRIGGER */
 	bool cout;
 };
 
@@ -370,7 +370,7 @@ static int mcux_acmp_channel_get(const struct device *dev,
 	return 0;
 }
 
-#ifdef CONFIG_MCUX_ACMP_TRIGGER
+#ifdef CONFIG_SENSOR_MCUX_ACMP_TRIGGER
 static int mcux_acmp_trigger_set(const struct device *dev,
 				 const struct sensor_trigger *trig,
 				 sensor_trigger_handler_t handler)
@@ -431,7 +431,7 @@ static void mcux_acmp_isr(const struct device *dev)
 
 	k_work_submit(&data->work);
 }
-#endif /* CONFIG_MCUX_ACMP_TRIGGER */
+#endif /* CONFIG_SENSOR_MCUX_ACMP_TRIGGER */
 
 static int mcux_acmp_init(const struct device *dev)
 {
@@ -462,7 +462,7 @@ static int mcux_acmp_init(const struct device *dev)
 	/* Disable DAC */
 	ACMP_SetDACConfig(config->base, NULL);
 
-#ifdef CONFIG_MCUX_ACMP_TRIGGER
+#ifdef CONFIG_SENSOR_MCUX_ACMP_TRIGGER
 	data->dev = dev;
 	k_work_init(&data->work, mcux_acmp_trigger_work_handler);
 
@@ -470,7 +470,7 @@ static int mcux_acmp_init(const struct device *dev)
 	ACMP_EnableInterrupts(config->base,
 			      kACMP_OutputRisingInterruptEnable |
 			      kACMP_OutputFallingInterruptEnable);
-#endif /* CONFIG_MCUX_ACMP_TRIGGER */
+#endif /* CONFIG_SENSOR_MCUX_ACMP_TRIGGER */
 
 	ACMP_Enable(config->base, true);
 
@@ -480,9 +480,9 @@ static int mcux_acmp_init(const struct device *dev)
 static const struct sensor_driver_api mcux_acmp_driver_api = {
 	.attr_set = mcux_acmp_attr_set,
 	.attr_get = mcux_acmp_attr_get,
-#ifdef CONFIG_MCUX_ACMP_TRIGGER
+#ifdef CONFIG_SENSOR_MCUX_ACMP_TRIGGER
 	.trigger_set = mcux_acmp_trigger_set,
-#endif /* CONFIG_MCUX_ACMP_TRIGGER */
+#endif /* CONFIG_SENSOR_MCUX_ACMP_TRIGGER */
 	.sample_fetch = mcux_acmp_sample_fetch,
 	.channel_get = mcux_acmp_channel_get,
 };
@@ -503,7 +503,7 @@ static const struct mcux_acmp_config mcux_acmp_config_##n = {		\
 	config_func_init						\
 }
 
-#ifdef CONFIG_MCUX_ACMP_TRIGGER
+#ifdef CONFIG_SENSOR_MCUX_ACMP_TRIGGER
 #define MCUX_ACMP_CONFIG_FUNC(n)					\
 	static void mcux_acmp_config_func_##n(const struct device *dev) \
 	{								\
@@ -517,12 +517,12 @@ static const struct mcux_acmp_config mcux_acmp_config_##n = {		\
 	.irq_config_func = mcux_acmp_config_func_##n
 #define MCUX_ACMP_INIT_CONFIG(n)					\
 	MCUX_ACMP_DECLARE_CONFIG(n, MCUX_ACMP_CONFIG_FUNC_INIT(n))
-#else /* !CONFIG_MCUX_ACMP_TRIGGER */
+#else /* !CONFIG_SENSOR_MCUX_ACMP_TRIGGER */
 #define MCUX_ACMP_CONFIG_FUNC(n)
 #define MCUX_ACMP_CONFIG_FUNC_INIT
 #define MCUX_ACMP_INIT_CONFIG(n)					\
 	MCUX_ACMP_DECLARE_CONFIG(n, MCUX_ACMP_CONFIG_FUNC_INIT)
-#endif /* !CONFIG_MCUX_ACMP_TRIGGER */
+#endif /* !CONFIG_SENSOR_MCUX_ACMP_TRIGGER */
 
 #define MCUX_ACMP_INIT(n)						\
 	static struct mcux_acmp_data mcux_acmp_data_##n;		\
