486555e8590d22632e26f214bd95faed5014aaff
Duy Nguyen <duy.nguyen.xa@renesas.com>
2024-08-19 12:28:00 +0700

    soc: renesas: rx: Initial support for RX130 SOC
    Minimal SOC layer support for Renesas RX SOC
    This SOC is using Renesas RXv1 core
    
    Signed-off-by: Duy Nguyen <duy.nguyen.xa@renesas.com>

---
+++ soc/renesas/rx/rx130/soc.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024 Renesas Electronics Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @brief SOC header file for Renesas RX SOC series
+ */
+
+#ifndef _SOC_H_
+#define _SOC_H_
+
+#endif /* _SOC_H_ */
