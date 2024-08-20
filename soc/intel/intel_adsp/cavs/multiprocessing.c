d379a33505eb891178b87cc187a0bdcca117eeb7
Pisit Sawangvonganan <pisit@ndrsolution.com>
2024-08-21 03:40:43 +0700

    style: soc: comply with MISRA C:2012 Rule 15.6
    Add missing braces to comply with MISRA C:2012 Rule 15.6 and
    also following Zephyr's style guideline.
    
    Signed-off-by: Pisit Sawangvonganan <pisit@ndrsolution.com>

--- soc/intel/intel_adsp/cavs/multiprocessing.c
+++ soc/intel/intel_adsp/cavs/multiprocessing.c
@@ -82,10 +82,11 @@ void soc_start_core(int cpu_num)
 	 * initialization, the next pm state is set to ACTIVE. This way we can determine
 	 * whether the core is being turned on again or for the first time.
 	 */
-	if (pm_state_next_get(cpu_num)->state == PM_STATE_ACTIVE)
+	if (pm_state_next_get(cpu_num)->state == PM_STATE_ACTIVE) {
 		lpsram[1] = z_soc_mp_asm_entry;
-	else
+	} else {
 		lpsram[1] = dsp_restore_vector;
+	}
 #else
 	lpsram[1] = z_soc_mp_asm_entry;
 #endif
