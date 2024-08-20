374562be3c790e8f297162d4b63577a87fca0e8b
Pisit Sawangvonganan <pisit@ndrsolution.com>
2024-08-21 01:28:13 +0700

    style: arch: comply with MISRA C:2012 Rule 15.6
    Add missing braces to comply with MISRA C:2012 Rule 15.6 and
    also following Zephyr's style guideline.
    
    Signed-off-by: Pisit Sawangvonganan <pisit@ndrsolution.com>

--- arch/arm64/core/fatal.c
+++ arch/arm64/core/fatal.c
@@ -322,8 +322,9 @@ static bool z_arm64_stack_corruption_check(struct arch_esf *esf, uint64_t esr, u
 static bool is_recoverable(struct arch_esf *esf, uint64_t esr, uint64_t far,
 			   uint64_t elr)
 {
-	if (!esf)
+	if (!esf) {
 		return false;
+	}
 
 #ifdef CONFIG_USERSPACE
 	for (int i = 0; i < ARRAY_SIZE(exceptions); i++) {
@@ -380,8 +381,9 @@ void z_arm64_fatal_error(unsigned int reason, struct arch_esf *esf)
 
 			dump_esr(esr, &dump_far);
 
-			if (dump_far)
+			if (dump_far) {
 				LOG_ERR("FAR_ELn: 0x%016llx", far);
+			}
 
 			LOG_ERR("TPIDRRO: 0x%016llx", read_tpidrro_el0());
 #endif /* CONFIG_EXCEPTION_DEBUG */
