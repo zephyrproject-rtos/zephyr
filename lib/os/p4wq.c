6a64490c3452ec6a1b2fa9169b76ccf44cfd98cb
Pisit Sawangvonganan <pisit@ndrsolution.com>
2024-08-21 03:09:47 +0700

    style: lib: comply with MISRA C:2012 Rule 15.6
    Add missing braces to comply with MISRA C:2012 Rule 15.6 and
    also following Zephyr's style guideline.
    
    Signed-off-by: Pisit Sawangvonganan <pisit@ndrsolution.com>

--- lib/os/p4wq.c
+++ lib/os/p4wq.c
@@ -176,8 +176,9 @@ static int static_init(void)
 			if (pp->flags & K_P4WQ_USER_CPU_MASK) {
 				int ret = k_thread_cpu_mask_clear(&pp->threads[i]);
 
-				if (ret < 0)
+				if (ret < 0) {
 					LOG_ERR("Couldn't clear CPU mask: %d", ret);
+				}
 			}
 #endif
 		}
@@ -196,8 +197,9 @@ void k_p4wq_enable_static_thread(struct k_p4wq *queue, struct k_thread *thread,
 		while ((i = find_lsb_set(cpu_mask))) {
 			int ret = k_thread_cpu_mask_enable(thread, i - 1);
 
-			if (ret < 0)
+			if (ret < 0) {
 				LOG_ERR("Couldn't set CPU mask for %u: %d", i, ret);
+			}
 			cpu_mask &= ~BIT(i - 1);
 		}
 	}
