8af4d66afdc3865096017174284bd1c9e546eb73
Karol Lasończyk <karol.lasonczyk@nordicsemi.no>
2024-08-21 13:45:55 +0200

    boards: nrf: Add nRF54L15 DK board
    Add board with production version of the nRF54L15 SoC.
    
    Signed-off-by: Karol Lasończyk <karol.lasonczyk@nordicsemi.no>

---
+++ boards/nordic/nrf54l15dk/board.cmake
@@ -0,0 +1,11 @@
+# Copyright (c) 2024 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+if(CONFIG_SOC_NRF54L15_CPUAPP)
+  board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
+elseif(CONFIG_SOC_NRF54L15_CPUFLPR)
+  board_runner_args(jlink "--speed=4000")
+endif()
+
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
