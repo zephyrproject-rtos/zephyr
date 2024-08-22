0e8daaec466a86022364a0bd8cd1377aa2d43584
Hyunsu Yoon <piedroconti@naver.com>
2024-08-22 20:49:19 +0900

    dts: formatting: Apply automatic formatting
    This commit reflects changes made by the automatic formatter when saving the file.
    Seeking review on whether these formatting changes are appropriate for this file.
    
    Signed-off-by: Hyunsu Yoon <piedroconti@naver.com>

--- include/zephyr/dt-bindings/input/keymap.h
+++ include/zephyr/dt-bindings/input/keymap.h
@@ -7,7 +7,7 @@
 #ifndef ZEPHYR_INCLUDE_DT_BINDINGS_INPUT_KEYMAP_H_
 #define ZEPHYR_INCLUDE_DT_BINDINGS_INPUT_KEYMAP_H_
 
-#define MATRIX_KEY(row, col, code) \
+#define MATRIX_KEY(row, col, code)                                                                 \
 	((((row) & 0xff) << 24) | (((col) & 0xff) << 16) | ((code) & 0xffff))
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_INPUT_KEYMAP_H_ */
