36023426114b28cdd4fc5a696c71e670d55d492f
Tomáš Juřena <jurenatomas@gmail.com>
2024-08-22 13:17:42 +0200

    drivers: sensor: ti: ina230: Add support for INA236
    This commit adds support for INA236 into the existing INA230 driver.
    These two chips are similar enough to share most of the code.
    The device can be defined the same way as INA230 and we only have
    the extra option to select the high-precision mode.
    
    ```
    ina236: ina236@40 {
    		status = "okay";
    		compatible = "ti,ina236", "ti,ina230";
    		reg = <0x40>;
    		adc-mode = "Bus and shunt voltage continuous";
    		vbus-conversion-time-us = <1100>;
    		vshunt-conversion-time-us = <1100>;
    		avg-count = <1>;
    		current-lsb-microamps = <1000>;
    		rshunt-micro-ohms = <15000>;
    		alert-gpios = <&gpiod 0 GPIO_ACTIVE_LOW>;
    		high-precision;
    	};
    ```
    
    Signed-off-by: Tomáš Juřena <jurenatomas@gmail.com>

--- drivers/sensor/ti/ina23x/ina230.h
+++ drivers/sensor/ti/ina23x/ina230.h
@@ -23,14 +23,16 @@
 #include <zephyr/kernel.h>
 #endif
 
-#define INA230_REG_CONFIG     0x00
-#define INA230_REG_SHUNT_VOLT 0x01
-#define INA230_REG_BUS_VOLT   0x02
-#define INA230_REG_POWER      0x03
-#define INA230_REG_CURRENT    0x04
-#define INA230_REG_CALIB      0x05
-#define INA230_REG_MASK       0x06
-#define INA230_REG_ALERT      0x07
+#define INA230_REG_CONFIG          0x00
+#define INA230_REG_SHUNT_VOLT      0x01
+#define INA230_REG_BUS_VOLT        0x02
+#define INA230_REG_POWER           0x03
+#define INA230_REG_CURRENT         0x04
+#define INA230_REG_CALIB           0x05
+#define INA230_REG_MASK            0x06
+#define INA230_REG_ALERT           0x07
+#define INA236_REG_MANUFACTURER_ID 0x3E
+#define INA236_REG_DEVICE_ID       0x3F
 
 struct ina230_data {
 	const struct device *dev;
@@ -43,7 +45,7 @@ struct ina230_data {
 	struct k_work work;
 	sensor_trigger_handler_t handler_alert;
 	const struct sensor_trigger *trig_alert;
-#endif  /* CONFIG_INA230_TRIGGER */
+#endif /* CONFIG_INA230_TRIGGER */
 };
 
 struct ina230_config {
@@ -51,17 +53,18 @@ struct ina230_config {
 	uint16_t config;
 	uint32_t current_lsb;
 	uint16_t cal;
+	uint8_t power_scale;
+	uint32_t uv_lsb;
 #ifdef CONFIG_INA230_TRIGGER
 	bool trig_enabled;
 	uint16_t mask;
 	const struct gpio_dt_spec alert_gpio;
 	uint16_t alert_limit;
-#endif  /* CONFIG_INA230_TRIGGER */
+#endif /* CONFIG_INA230_TRIGGER */
 };
 
 int ina230_trigger_mode_init(const struct device *dev);
-int ina230_trigger_set(const struct device *dev,
-		       const struct sensor_trigger *trig,
+int ina230_trigger_set(const struct device *dev, const struct sensor_trigger *trig,
 		       sensor_trigger_handler_t handler);
 
 #endif /* ZEPHYR_DRIVERS_SENSOR_INA23X_INA230_H_ */
