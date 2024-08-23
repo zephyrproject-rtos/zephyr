5de1e2972f0b1304671a990b6cc9510819c15932
Bartosz Miller <bartosz.miller@nordicsemi.no>
2024-08-23 08:47:32 +0200

    tests: drivers: Implement tests for nrf2 clock control api Update the existing base and add new test cases
    Signed-off-by: Bartosz Miller <bartosz.miller@nordicsemi.no>

--- tests/drivers/clock_control/nrf2_clock_control/src/main.c
+++ tests/drivers/clock_control/nrf2_clock_control/src/main.c
@@ -21,28 +21,80 @@ const struct nrf_clock_spec test_clk_specs_fll16m[] = {
 	{
 		.frequency = MHZ(16),
 		.accuracy = 20000,
-		.precision = 0,
+		.precision = NRF_CLOCK_CONTROL_PRECISION_DEFAULT,
 	},
 	{
 		.frequency = MHZ(16),
 		.accuracy = 5000,
-		.precision = 0,
+		.precision = NRF_CLOCK_CONTROL_PRECISION_DEFAULT,
 	},
 	{
 		.frequency = MHZ(16),
 		.accuracy = 30,
-		.precision = 0,
+		.precision = NRF_CLOCK_CONTROL_PRECISION_DEFAULT,
 	},
 };
 
-static const struct test_clk_context test_clk_contexts[] = {
+static const struct test_clk_context fll16m_test_clk_contexts[] = {
 	{
 		.clk_dev = DEVICE_DT_GET(DT_NODELABEL(fll16m)),
 		.clk_specs = test_clk_specs_fll16m,
 		.clk_specs_size = ARRAY_SIZE(test_clk_specs_fll16m),
 	},
 };
+
+const struct nrf_clock_spec test_clk_specs_hsfll[] = {
+	{
+		.frequency = MHZ(128),
+		.accuracy = 0,
+		.precision = NRF_CLOCK_CONTROL_PRECISION_DEFAULT,
+	},
+	{
+		.frequency = MHZ(320),
+		.accuracy = 0,
+		.precision = NRF_CLOCK_CONTROL_PRECISION_DEFAULT,
+	},
+	{
+		.frequency = MHZ(64),
+		.accuracy = 0,
+		.precision = NRF_CLOCK_CONTROL_PRECISION_DEFAULT,
+	},
+};
+
+static const struct test_clk_context cpuapp_hsfll_test_clk_contexts[] = {
+	{
+		.clk_dev = DEVICE_DT_GET(DT_NODELABEL(cpuapp_hsfll)),
+		.clk_specs = test_clk_specs_hsfll,
+		.clk_specs_size = ARRAY_SIZE(test_clk_specs_hsfll),
+	},
+};
 #endif
+
+const struct nrf_clock_spec test_clk_specs_lfclk[] = {
+	{
+		.frequency = 32768,
+		.accuracy = 0,
+		.precision = NRF_CLOCK_CONTROL_PRECISION_DEFAULT,
+	},
+	{
+		.frequency = 32768,
+		.accuracy = 30,
+		.precision = NRF_CLOCK_CONTROL_PRECISION_DEFAULT,
+	},
+	{
+		.frequency = 32768,
+		.accuracy = 30,
+		.precision = NRF_CLOCK_CONTROL_PRECISION_HIGH,
+	},
+};
+
+static const struct test_clk_context lfclk_test_clk_contexts[] = {
+	{
+		.clk_dev = DEVICE_DT_GET(DT_NODELABEL(lfclk)),
+		.clk_specs = test_clk_specs_lfclk,
+		.clk_specs_size = ARRAY_SIZE(test_clk_specs_lfclk),
+	},
+};
 
 static void test_request_release_clock_spec(const struct device *clk_dev,
 					    const struct nrf_clock_spec *clk_spec)
@@ -52,6 +104,7 @@ static void test_request_release_clock_spec(const struct device *clk_dev,
 	struct onoff_client cli;
 	uint32_t rate;
 
+	TC_PRINT("Clock under test: %s\n", clk_dev->name);
 	sys_notify_init_spinwait(&cli.notify);
 	ret = nrf_clock_control_request(clk_dev, clk_spec, &cli);
 	zassert_between_inclusive(ret, 0, 2);
@@ -59,6 +112,7 @@ static void test_request_release_clock_spec(const struct device *clk_dev,
 		ret = sys_notify_fetch_result(&cli.notify, &res);
 		k_yield();
 	} while (ret == -EAGAIN);
+	TC_PRINT("Res: %d\n", res);
 	zassert_ok(ret);
 	zassert_ok(res);
 	ret = clock_control_get_rate(clk_dev, NULL, &rate);
@@ -69,24 +123,53 @@ static void test_request_release_clock_spec(const struct device *clk_dev,
 	zassert_equal(ret, ONOFF_STATE_ON);
 }
 
-ZTEST(nrf2_clock_control, test_request)
+static void test_clock_control_request(const struct test_clk_context *clk_contexts,
+				       size_t contexts_size)
 {
 	const struct test_clk_context *clk_context;
 	size_t clk_specs_size;
 	const struct device *clk_dev;
 	const struct nrf_clock_spec *clk_spec;
 
-	for (size_t i = 0; i < ARRAY_SIZE(test_clk_contexts); i++) {
-		clk_context = &test_clk_contexts[i];
+	for (size_t i = 0; i < contexts_size; i++) {
+		clk_context = &clk_contexts[i];
 		clk_specs_size = clk_context->clk_specs_size;
 
 		for (size_t u = 0; u < clk_specs_size; u++) {
 			clk_dev = clk_context->clk_dev;
 			clk_spec = &clk_context->clk_specs[u];
 
+			TC_PRINT("Applying clock (%s) spec: frequency %d, accuracy %d, precision "
+				 "%d\n",
+				 clk_dev->name, clk_spec->frequency, clk_spec->accuracy,
+				 clk_spec->precision);
 			test_request_release_clock_spec(clk_dev, clk_spec);
 		}
 	}
+}
+
+#if CONFIG_BOARD_NRF54H20DK_NRF54H20_CPUAPP
+ZTEST(nrf2_clock_control, test_cpuapp_hsfll_control)
+{
+
+	TC_PRINT("APP DOMAIN HSFLL test\n");
+	/* Wait for the DVFS init to complete */
+	k_msleep(3000);
+	test_clock_control_request(cpuapp_hsfll_test_clk_contexts,
+				   ARRAY_SIZE(cpuapp_hsfll_test_clk_contexts));
+}
+#endif
+
+ZTEST(nrf2_clock_control, test_fll16m_control)
+{
+	TC_PRINT("FLL16M test\n");
+	test_clock_control_request(fll16m_test_clk_contexts, ARRAY_SIZE(fll16m_test_clk_contexts));
+}
+
+ZTEST(nrf2_clock_control, test_lfclk_control)
+{
+	TC_PRINT("LFCLK test\n");
+	test_clock_control_request(lfclk_test_clk_contexts, ARRAY_SIZE(lfclk_test_clk_contexts));
 }
 
 ZTEST_SUITE(nrf2_clock_control, NULL, NULL, NULL, NULL, NULL);
