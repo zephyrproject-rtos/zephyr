2c555aab8af6a819805285e79f670b2f71021dd1
Jordan Yates <jordan@embeint.com>
2024-08-26 20:30:26 +1000

    sensor: current_amp: request calibration
    Request calibration on ADC samples to improve the accuracy of the ADC
    outputs and therefore the final measured current.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- drivers/sensor/current_amp/current_amp.c
+++ drivers/sensor/current_amp/current_amp.c
@@ -152,6 +152,7 @@ static int current_init(const struct device *dev)
 
 	data->sequence.buffer = &data->raw;
 	data->sequence.buffer_size = sizeof(data->raw);
+	data->sequence.calibrate = true;
 
 	return 0;
 }
