88d421b58a81c0ddb19ae51c573fc898bfb99ab1
Rodrigo Peixoto <rodrigopex@gmail.com>
2024-08-27 08:21:16 -0300

    zbus: add HEAP_MEM_POOL_ADD_SIZE_ZBUS
    ZBus currently does not have a `HEAP_MEM_POOL_ADD_SIZE_ZBUS` when using
    message subscribers or runtime observers, forcing the developer to set that
    manually. Adding the configuration option to zbus would improve its
    usability and make zbus easier to use. With this feature, zbus aligns with
    the other Zephyr subsystems' heap memory allocation approach.
    
    Signed-off-by: Rodrigo Peixoto <rodrigopex@gmail.com>

--- subsys/zbus/zbus.c
+++ subsys/zbus/zbus.c
@@ -26,8 +26,6 @@ static struct k_spinlock obs_slock;
 NET_BUF_POOL_HEAP_DEFINE(_zbus_msg_subscribers_pool, CONFIG_ZBUS_MSG_SUBSCRIBER_NET_BUF_POOL_SIZE,
 			 sizeof(struct zbus_channel *), NULL);
 
-BUILD_ASSERT(K_HEAP_MEM_POOL_SIZE > 0, "MSG_SUBSCRIBER feature requires heap memory pool.");
-
 static inline struct net_buf *_zbus_create_net_buf(struct net_buf_pool *pool, size_t size,
 						   k_timeout_t timeout)
 {
