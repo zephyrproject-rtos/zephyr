37cb2eb906b3bda9844bced0d46df4ea5090f165
Kapil Bhatt <kapil.bhatt@nordicsemi.no>
2024-08-27 15:19:31 +0530

    hostap: Add rts threshold set api support
    Add api to set rts threshold.
    
    Signed-off-by: Kapil Bhatt <kapil.bhatt@nordicsemi.no>

--- modules/hostap/src/supp_api.h
+++ modules/hostap/src/supp_api.h
@@ -168,6 +168,15 @@ int supplicant_filter(const struct device *dev, struct wifi_filter_info *filter)
  */
 int supplicant_channel(const struct device *dev, struct wifi_channel_info *channel);
 
+/**
+ * @brief Set Wi-Fi RTS threshold
+ *
+ * @param dev Wi-Fi interface handle to use
+ * @param rts_threshold RTS threshold to set
+ * @return 0 for OK; -1 for ERROR
+ */
+int supplicant_set_rts_threshold(const struct device *dev, unsigned int rts_threshold);
+
 #ifdef CONFIG_WIFI_NM_WPA_SUPPLICANT_WNM
 /** Send bss transition query
  *
