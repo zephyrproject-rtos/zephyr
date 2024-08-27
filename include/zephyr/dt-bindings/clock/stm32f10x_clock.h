9925ce792ea9a3c5a744c4ef17bcfb8265d7d171
Joakim Andersson <joerchan@gmail.com>
2024-08-27 18:48:24 +0200

    include: dt-bindings: Add MCO select and prescaler macros for CFGR reg
    Add MCO select and prescaler macros to set bits in the RCC CFGRx
    register.
    The set of supported boards are chosen to replace what is currently
    supported in Kconfig.
    
    Signed-off-by: Joakim Andersson <joerchan@gmail.com>

--- include/zephyr/dt-bindings/clock/stm32f10x_clock.h
+++ include/zephyr/dt-bindings/clock/stm32f10x_clock.h
@@ -18,4 +18,9 @@
 #define STM32_SRC_PLL3CLK           (STM32_SRC_PLL2CLK + 1)
 #define STM32_SRC_EXT_HSE           (STM32_SRC_PLL3CLK + 1)
 
+/** CFGR1 devices */
+#undef MCO1_SEL /* Need to redefine generic F1 MCO_SEL for connectivity line devices. */
+#define MCO1_SEL(val)           STM32_MCO_CFGR(val, 0xF, 24, CFGR1_REG)
+/* No MCO prescaler support on STM32F1 series. */
+
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_STM32F10X_CLOCK_H_ */
