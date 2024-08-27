92aaa4b8e8b4d887a1c92175cdcb41659e8eb3d8
Simen E. Sørensen <simen_sor91@hotmail.com>
2024-08-27 10:42:59 +0200

    boards: common: esp32: Replace deprecated command
    - OpenOCD v0.12.0-esp32-20230921 from ESP-IDF complains about use of the
      deprecated command `flushregs` and suggests
      `maintenance flush register-cache` instead.
    
    Signed-off-by: Simen E. Sørensen <simen_sor91@hotmail.com>

--- boards/common/esp32.board.cmake
+++ boards/common/esp32.board.cmake
@@ -6,7 +6,7 @@ board_set_debugger_ifnset(openocd)
 board_runner_args(openocd  --no-init --no-halt --no-targets --no-load)
 board_runner_args(openocd  --gdb-init "set remote hardware-watchpoint-limit 2")
 
-board_runner_args(openocd  --gdb-init "flushregs")
+board_runner_args(openocd  --gdb-init "maintenance flush register-cache")
 board_runner_args(openocd  --gdb-init "mon reset halt")
 board_runner_args(openocd  --gdb-init "thb main")
 
