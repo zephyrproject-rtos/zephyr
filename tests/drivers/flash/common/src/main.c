13462bedbb70934eda8b359f6ae21f1116c0b10f
Bartosz Miller <bartosz.miller@nordicsemi.no>
2024-08-27 09:16:35 +0200

    tests: drivers: Extend coverage for SPI MEMORY driver
    Cover more SPI flash memeory driver api methods
    Extend testing with nrf54h20 and nrf54l15
    
    Signed-off-by: Bartosz Miller <bartosz.miller@nordicsemi.no>

--- tests/drivers/flash/common/src/main.c
+++ tests/drivers/flash/common/src/main.c
@@ -11,26 +11,26 @@
 #include <zephyr/storage/flash_map.h>
 
 #if defined(CONFIG_NORDIC_QSPI_NOR)
-#define TEST_AREA_DEV_NODE	DT_INST(0, nordic_qspi_nor)
+#define TEST_AREA_DEV_NODE DT_INST(0, nordic_qspi_nor)
 #elif defined(CONFIG_SPI_NOR)
-#define TEST_AREA_DEV_NODE	DT_INST(0, jedec_spi_nor)
+#define TEST_AREA_DEV_NODE DT_INST(0, jedec_spi_nor)
 #else
-#define TEST_AREA	storage_partition
+#define TEST_AREA storage_partition
 #endif
 
 /* TEST_AREA is only defined for configurations that realy on
  * fixed-partition nodes.
  */
 #ifdef TEST_AREA
-#define TEST_AREA_OFFSET	FIXED_PARTITION_OFFSET(TEST_AREA)
-#define TEST_AREA_SIZE		FIXED_PARTITION_SIZE(TEST_AREA)
-#define TEST_AREA_MAX		(TEST_AREA_OFFSET + TEST_AREA_SIZE)
-#define TEST_AREA_DEVICE	FIXED_PARTITION_DEVICE(TEST_AREA)
+#define TEST_AREA_OFFSET FIXED_PARTITION_OFFSET(TEST_AREA)
+#define TEST_AREA_SIZE   FIXED_PARTITION_SIZE(TEST_AREA)
+#define TEST_AREA_MAX    (TEST_AREA_OFFSET + TEST_AREA_SIZE)
+#define TEST_AREA_DEVICE FIXED_PARTITION_DEVICE(TEST_AREA)
 
 #elif defined(TEST_AREA_DEV_NODE)
 
-#define TEST_AREA_DEVICE	DEVICE_DT_GET(TEST_AREA_DEV_NODE)
-#define TEST_AREA_OFFSET	0xff000
+#define TEST_AREA_DEVICE DEVICE_DT_GET(TEST_AREA_DEV_NODE)
+#define TEST_AREA_OFFSET 0xff000
 
 #if DT_NODE_HAS_PROP(TEST_AREA_DEV_NODE, size_in_bytes)
 #define TEST_AREA_MAX DT_PROP(TEST_AREA_DEV_NODE, size_in_bytes)
@@ -42,10 +42,9 @@
 #error "Unsupported configuraiton"
 #endif
 
-#define EXPECTED_SIZE	512
+#define EXPECTED_SIZE 512
 
-#if !defined(CONFIG_FLASH_HAS_EXPLICIT_ERASE) &&		\
-	!defined(CONFIG_FLASH_HAS_NO_EXPLICIT_ERASE)
+#if !defined(CONFIG_FLASH_HAS_EXPLICIT_ERASE) && !defined(CONFIG_FLASH_HAS_NO_EXPLICIT_ERASE)
 #error There is no flash device enabled or it is missing Kconfig options
 #endif
 
@@ -66,11 +65,20 @@ static void *flash_driver_setup(void)
 	if (IS_ENABLED(CONFIG_FLASH_HAS_EXPLICIT_ERASE)) {
 		const struct flash_parameters *fparams = flash_get_parameters(flash_dev);
 
+		rc = flash_get_page_count(flash_dev);
+		zassert_not_equal(rc, 0);
+		TC_PRINT("Memory pages count: %d\n", rc);
+
 		erase_value = fparams->erase_value;
 		ebw_required = flash_params_get_erase_cap(fparams) & FLASH_ERASE_C_EXPLICIT;
 		/* For tests purposes use page (in nrf_qspi_nor page = 64 kB) */
-		flash_get_page_info_by_offs(flash_dev, TEST_AREA_OFFSET,
-					    &page_info);
+		flash_get_page_info_by_offs(flash_dev, TEST_AREA_OFFSET, &page_info);
+
+		rc = flash_get_page_info_by_idx(flash_dev, page_info.index, &page_info);
+		zassert_ok(rc);
+		TC_PRINT("Test page info: (start_offset %ld, size %d, index %d)\n",
+			 page_info.start_offset, (uint8_t)page_info.size, page_info.index);
+
 	} else {
 		TC_PRINT("No devices with erase requirement present\n");
 		erase_value = 0x55;
@@ -78,12 +86,10 @@ static void *flash_driver_setup(void)
 		page_info.size = TEST_AREA_MAX - TEST_AREA_OFFSET;
 	}
 
-
 	/* Check if test region is not empty */
 	uint8_t buf[EXPECTED_SIZE];
 
-	rc = flash_read(flash_dev, TEST_AREA_OFFSET,
-			buf, EXPECTED_SIZE);
+	rc = flash_read(flash_dev, TEST_AREA_OFFSET, buf, EXPECTED_SIZE);
 	zassert_equal(rc, 0, "Cannot read flash");
 
 	/* Fill test buffer with random data */
@@ -113,9 +119,8 @@ static void *flash_driver_setup(void)
 		if (!is_buf_clear) {
 			/* Erase a nb of pages aligned to the EXPECTED_SIZE */
 			rc = flash_erase(flash_dev, page_info.start_offset,
-					(page_info.size *
-					((EXPECTED_SIZE + page_info.size - 1)
-					/ page_info.size)));
+					 (page_info.size *
+					  ((EXPECTED_SIZE + page_info.size - 1) / page_info.size)));
 
 			zassert_equal(rc, 0, "Flash memory not properly erased");
 		}
@@ -134,19 +139,16 @@ ZTEST(flash_driver, test_read_unaligned_address)
 	if (IS_ENABLED(CONFIG_FLASH_HAS_EXPLICIT_ERASE) && ebw_required) {
 		start = page_info.start_offset;
 		/* Erase a nb of pages aligned to the EXPECTED_SIZE */
-		rc = flash_erase(flash_dev, page_info.start_offset,
-				(page_info.size *
-				((EXPECTED_SIZE + page_info.size - 1)
-				/ page_info.size)));
+		rc = flash_erase(
+			flash_dev, page_info.start_offset,
+			(page_info.size * ((EXPECTED_SIZE + page_info.size - 1) / page_info.size)));
 
 		zassert_equal(rc, 0, "Flash memory not properly erased");
 	} else {
 		start = TEST_AREA_OFFSET;
 	}
 
-	rc = flash_write(flash_dev,
-			 start,
-			 expected, EXPECTED_SIZE);
+	rc = flash_write(flash_dev, start, expected, EXPECTED_SIZE);
 	zassert_equal(rc, 0, "Cannot write to flash");
 
 	/* read buffer length*/
@@ -159,22 +161,21 @@ ZTEST(flash_driver, test_read_unaligned_address)
 				buf[buf_o - 1] = canary;
 				buf[buf_o + len] = canary;
 				memset(buf + buf_o, 0, len);
-				rc = flash_read(flash_dev,
-						start + ad_o,
-						buf + buf_o, len);
+				rc = flash_read(flash_dev, start + ad_o, buf + buf_o, len);
 				zassert_equal(rc, 0, "Cannot read flash");
-				zassert_equal(memcmp(buf + buf_o,
-						expected + ad_o,
-						len),
-					0, "Flash read failed at len=%d, "
-					"ad_o=%d, buf_o=%d", len, ad_o, buf_o);
+				zassert_equal(memcmp(buf + buf_o, expected + ad_o, len), 0,
+					      "Flash read failed at len=%d, "
+					      "ad_o=%d, buf_o=%d",
+					      len, ad_o, buf_o);
 				/* check buffer guards */
 				zassert_equal(buf[buf_o - 1], canary,
-					"Buffer underflow at len=%d, "
-					"ad_o=%d, buf_o=%d", len, ad_o, buf_o);
+					      "Buffer underflow at len=%d, "
+					      "ad_o=%d, buf_o=%d",
+					      len, ad_o, buf_o);
 				zassert_equal(buf[buf_o + len], canary,
-					"Buffer overflow at len=%d, "
-					"ad_o=%d, buf_o=%d", len, ad_o, buf_o);
+					      "Buffer overflow at len=%d, "
+					      "ad_o=%d, buf_o=%d",
+					      len, ad_o, buf_o);
 			}
 		}
 	}
@@ -188,29 +189,24 @@ ZTEST(flash_driver, test_flash_fill)
 
 	if (IS_ENABLED(CONFIG_FLASH_HAS_EXPLICIT_ERASE) && ebw_required) {
 		/* Erase a nb of pages aligned to the EXPECTED_SIZE */
-		rc = flash_erase(flash_dev, page_info.start_offset,
-				(page_info.size *
-				((EXPECTED_SIZE + page_info.size - 1)
-				/ page_info.size)));
+		rc = flash_erase(
+			flash_dev, page_info.start_offset,
+			(page_info.size * ((EXPECTED_SIZE + page_info.size - 1) / page_info.size)));
 
 		zassert_equal(rc, 0, "Flash memory not properly erased");
 	} else {
-		rc = flash_fill(flash_dev, 0x55, page_info.start_offset,
-				(page_info.size *
-				((EXPECTED_SIZE + page_info.size - 1)
-				/ page_info.size)));
+		rc = flash_fill(
+			flash_dev, 0x55, page_info.start_offset,
+			(page_info.size * ((EXPECTED_SIZE + page_info.size - 1) / page_info.size)));
 		zassert_equal(rc, 0, "Leveling memory with fill failed\n");
 	}
 
 	/* Fill the device with 0xaa */
 	rc = flash_fill(flash_dev, 0xaa, page_info.start_offset,
-			(page_info.size *
-			((EXPECTED_SIZE + page_info.size - 1)
-			/ page_info.size)));
+			(page_info.size * ((EXPECTED_SIZE + page_info.size - 1) / page_info.size)));
 	zassert_equal(rc, 0, "Fill failed\n");
 
-	rc = flash_read(flash_dev, TEST_AREA_OFFSET,
-			buf, EXPECTED_SIZE);
+	rc = flash_read(flash_dev, TEST_AREA_OFFSET, buf, EXPECTED_SIZE);
 	zassert_equal(rc, 0, "Cannot read flash");
 
 	for (i = 0; i < EXPECTED_SIZE; i++) {
@@ -227,22 +223,18 @@ ZTEST(flash_driver, test_flash_flatten)
 	int rc;
 	off_t i;
 
-	rc = flash_flatten(flash_dev, page_info.start_offset,
-			   (page_info.size *
-			   ((EXPECTED_SIZE + page_info.size - 1)
-			   / page_info.size)));
+	rc = flash_flatten(
+		flash_dev, page_info.start_offset,
+		(page_info.size * ((EXPECTED_SIZE + page_info.size - 1) / page_info.size)));
 
 	zassert_equal(rc, 0, "Flash not leveled not properly erased");
 
 	/* Fill the device with 0xaa */
 	rc = flash_fill(flash_dev, 0xaa, page_info.start_offset,
-			(page_info.size *
-			((EXPECTED_SIZE + page_info.size - 1)
-			/ page_info.size)));
+			(page_info.size * ((EXPECTED_SIZE + page_info.size - 1) / page_info.size)));
 	zassert_equal(rc, 0, "Fill failed\n");
 
-	rc = flash_read(flash_dev, TEST_AREA_OFFSET,
-			buf, EXPECTED_SIZE);
+	rc = flash_read(flash_dev, TEST_AREA_OFFSET, buf, EXPECTED_SIZE);
 	zassert_equal(rc, 0, "Cannot read flash");
 
 	for (i = 0; i < EXPECTED_SIZE; i++) {
