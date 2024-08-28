bde2525d9d5bf91edbcb31d15016f6d86b2a0f8a
Bjarki Arge Andreasen <bjarki.andreasen@nordicsemi.no>
2024-08-28 09:37:22 +0200

    Revert "[nrf fromlist] soc: nordic: nrf54h: Move HSFLL trimming to clock_control driver"
    This reverts commit ec07b19e4c85ea1306885db8c3bfc913492f9946.
    
    Signed-off-by: Bjarki Arge Andreasen <bjarki.andreasen@nordicsemi.no>

--- soc/nordic/nrf54h/soc.c
+++ soc/nordic/nrf54h/soc.c
@@ -10,12 +10,23 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
+#include <hal/nrf_hsfll.h>
 #include <hal/nrf_lrcconf.h>
 #include <hal/nrf_spu.h>
 #include <hal/nrf_memconf.h>
 #include <soc/nrfx_coredep.h>
 
 LOG_MODULE_REGISTER(soc, CONFIG_SOC_LOG_LEVEL);
+
+#if defined(NRF_APPLICATION)
+#define HSFLL_NODE DT_NODELABEL(cpuapp_hsfll)
+#elif defined(NRF_RADIOCORE)
+#define HSFLL_NODE DT_NODELABEL(cpurad_hsfll)
+#endif
+
+#define FICR_ADDR_GET(node_id, name)                                           \
+	DT_REG_ADDR(DT_PHANDLE_BY_NAME(node_id, nordic_ficrs, name)) +         \
+		DT_PHA_BY_NAME(node_id, nordic_ficrs, name, offset)
 
 #define SPU_INSTANCE_GET(p_addr)                                               \
 	((NRF_SPU_Type *)((p_addr) & (ADDRESS_REGION_Msk |                     \
@@ -67,12 +78,47 @@ static void power_domain_init(void)
 #endif
 }
 
+static int trim_hsfll(void)
+{
+#if defined(HSFLL_NODE)
+
+	NRF_HSFLL_Type *hsfll = (NRF_HSFLL_Type *)DT_REG_ADDR(HSFLL_NODE);
+	nrf_hsfll_trim_t trim = {
+		.vsup = sys_read32(FICR_ADDR_GET(HSFLL_NODE, vsup)),
+		.coarse = sys_read32(FICR_ADDR_GET(HSFLL_NODE, coarse)),
+		.fine = sys_read32(FICR_ADDR_GET(HSFLL_NODE, fine))
+	};
+
+	LOG_DBG("Trim: HSFLL VSUP: 0x%.8x", trim.vsup);
+	LOG_DBG("Trim: HSFLL COARSE: 0x%.8x", trim.coarse);
+	LOG_DBG("Trim: HSFLL FINE: 0x%.8x", trim.fine);
+
+	nrf_hsfll_clkctrl_mult_set(hsfll,
+				   DT_PROP(HSFLL_NODE, clock_frequency) /
+					   DT_PROP(DT_CLOCKS_CTLR(HSFLL_NODE), clock_frequency));
+	nrf_hsfll_trim_set(hsfll, &trim);
+
+	nrf_hsfll_task_trigger(hsfll, NRF_HSFLL_TASK_FREQ_CHANGE);
+	/* HSFLL task frequency change needs to be triggered twice to take effect.*/
+	nrf_hsfll_task_trigger(hsfll, NRF_HSFLL_TASK_FREQ_CHANGE);
+
+	LOG_DBG("NRF_HSFLL->TRIM.VSUP = %d", hsfll->TRIM.VSUP);
+	LOG_DBG("NRF_HSFLL->TRIM.COARSE = %d", hsfll->TRIM.COARSE);
+	LOG_DBG("NRF_HSFLL->TRIM.FINE = %d", hsfll->TRIM.FINE);
+
+#endif /* defined(HSFLL_NODE) */
+
+	return 0;
+}
+
 static int nordicsemi_nrf54h_init(void)
 {
 	sys_cache_instr_enable();
 	sys_cache_data_enable();
 
 	power_domain_init();
+
+	trim_hsfll();
 
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(ccm030), okay)
 	/* DMASEC is set to non-secure by default, which prevents CCM from
