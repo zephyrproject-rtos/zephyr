9a9e252d1677c047929f5602eace209123fafe3e
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2024-08-30 22:15:40 +0200

    cmake: linker: move toolchain_ld_<base|cpp> to linker flag property
    Transition the linker flag for the toolchain_ld_base and
    toolchain_ld_cpp macros to linker flag properties.
    This work follows the toolchain abstraction started in PR#24851.
    
    toolchain_ld_base() was intended for base linker flags, but has slowly
    become a 'set-any-linker-flag-here' and thus having several
    `if(<check>)` or `<func>_ifdef(<check> ...)`.
    
    Move the check to the top-level Zephyr CMakeLists.txt file, so that it
    becomes cleaner which part is responsible for setting a value, and then
    move the actual value (the linker flag) definition to the linker flag
    property location.
    
    It also helps adding support for new linkers, as it becomes clearer
    which linker flags Zephyr always expects, for example `base` and
    `cpp_base`, as well as those settings which are targeting for a given
    purpose, such as linker sort alignment.
    
    It also makes it clearer when those are used, for example in top-level
    CMakeLists.txt with CONFIG_LINKER_SORT_BY_ALIGNMENT compared to this
    information being buried in a linker support macro.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/linker/arcmwdt/linker_flags.cmake
+++ cmake/linker/arcmwdt/linker_flags.cmake
@@ -1,4 +1,10 @@
+# Copyright (c) 2024 Nordic Semiconductor
+#
 # SPDX-License-Identifier: Apache-2.0
+
+set_property(TARGET linker PROPERTY cpp_base -Hcplus)
 
 # Extra warnings options for twister run
 set_property(TARGET linker PROPERTY warnings_as_errors -Wl,--fatal-warnings)
+
+check_set_linker_property(TARGET linker PROPERTY sort_alignment -Wl,--sort-section=alignment)
--- cmake/linker/xt-ld/linker_flags.cmake
+++ cmake/linker/xt-ld/linker_flags.cmake
@@ -1,3 +1,21 @@
+# Copyright (c) 2024 Nordic Semiconductor
+#
 # SPDX-License-Identifier: Apache-2.0
 
+check_set_linker_property(TARGET linker PROPERTY base
+                          ${LINKERFLAGPREFIX},--gc-sections
+                          ${LINKERFLAGPREFIX},--build-id=none
+)
+
+if(NOT CONFIG_MINIMAL_LIBCPP AND NOT CONFIG_NATIVE_LIBRARY AND NOT CONFIG_EXTERNAL_MODULE_LIBCPP)
+  set_property(TARGET linker PROPERTY cpp_base -lstdc++)
+endif()
+
 set_property(TARGET linker PROPERTY partial_linking "-r")
+
+check_set_linker_property(TARGET linker PROPERTY no_relax ${LINKERFLAGPREFIX},--no-relax)
+)
+check_set_linker_property(TARGET linker PROPERTY sort_alignment
+                          ${LINKERFLAGPREFIX},--sort-common=descending
+                          ${LINKERFLAGPREFIX},--sort-section=alignment
+)
