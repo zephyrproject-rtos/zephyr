7c4d450764129ed84fd3436f367cd3ac44862d54
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2024-08-30 22:15:40 +0200

    cmake: linker: move toolchain_ld_<base|cpp> to linker flag property
    Transition the linker flag for the toolchain_ld_base and
    toolchain_ld_cpp macros to linker flag properties.
    This work follows the toolchain abstraction started in PR#24851.
    
    toolchain_ld_base() was intended for base linker flags, but has slowly
    become a 'set-any-linker-flag-here' and thus having several
    `if(<check>)` or `<func>_ifdef(<check> ...)`.
    
    Move the check to the top-level Zephyr CMakeLists.txt file, so that it
    becomes cleaner which part is responsible for setting a value, and then
    move the actual value (the linker flag) definition to the linker flag
    property location.
    
    It also helps adding support for new linkers, as it becomes clearer
    which linker flags Zephyr always expects, for example `base` and
    `cpp_base`, as well as those settings which are targeting for a given
    purpose, such as linker sort alignment.
    
    It also makes it clearer when those are used, for example in top-level
    CMakeLists.txt with CONFIG_LINKER_SORT_BY_ALIGNMENT compared to this
    information being buried in a linker support macro.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/linker/ld/target_base.cmake
+++ cmake/linker/ld/target_base.cmake
@@ -3,39 +3,4 @@
 # See root CMakeLists.txt for description and expectations of these macros
 
 macro(toolchain_ld_base)
-  # TOOLCHAIN_LD_FLAGS comes from compiler/gcc/target.cmake
-  # LINKERFLAGPREFIX comes from linker/ld/target.cmake
-  zephyr_ld_options(
-    ${TOOLCHAIN_LD_FLAGS}
-  )
-
-  zephyr_ld_options(
-    ${LINKERFLAGPREFIX},--gc-sections
-    ${LINKERFLAGPREFIX},--build-id=none
-  )
-
-  # Sort the common symbols and each input section by alignment
-  # in descending order to minimize padding between these symbols.
-  zephyr_ld_option_ifdef(
-    CONFIG_LINKER_SORT_BY_ALIGNMENT
-    ${LINKERFLAGPREFIX},--sort-common=descending
-    ${LINKERFLAGPREFIX},--sort-section=alignment
-  )
-
-  if (NOT CONFIG_LINKER_USE_RELAX)
-    zephyr_ld_options(
-      ${LINKERFLAGPREFIX},--no-relax
-    )
-  endif()
-
-  if(CONFIG_CPP AND (CMAKE_C_COMPILER_ID STREQUAL "Clang"))
-    # GNU ld complains when used with llvm/clang:
-    #   error: section: init_array is not contiguous with other relro sections
-    #
-    # So do not create RELRO program header.
-    zephyr_link_libraries(
-      -Wl,-z,norelro
-    )
-  endif()
-
 endmacro()
--- cmake/linker/lld/target_cpp.cmake
+++ cmake/linker/lld/target_cpp.cmake
@@ -3,9 +3,4 @@
 # See root CMakeLists.txt for description and expectations of these macros
 
 macro(toolchain_ld_cpp)
-
-  zephyr_link_libraries(
-    -lc++
-  )
-
 endmacro()
