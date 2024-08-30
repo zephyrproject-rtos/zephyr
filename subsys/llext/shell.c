918e3a3c59c020fee187dc60ccb85eeb535ad5be
Adam Wojasinski <awojasinski@baylibre.com>
2024-08-30 12:49:11 +0200

    llext: Add shell command for loading LLEXT from a filesystem
    This patch extends LLEXT shell commands with support for loading
    LLEXT from a filesystem. Use of the command requires absolute path
    to the llext file.
    
    Example use:
    `llext load_llext hello_world /lfs/hello_world.llext`
    
    Signed-off-by: Adam Wojasinski <awojasinski@baylibre.com>

--- subsys/llext/shell.c
+++ subsys/llext/shell.c
@@ -11,27 +11,35 @@
 #include <zephyr/llext/elf.h>
 #include <zephyr/llext/llext.h>
 #include <zephyr/llext/buf_loader.h>
+#include <zephyr/llext/fs_loader.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(llext_shell, CONFIG_LLEXT_LOG_LEVEL);
 
 #define LLEXT_LIST_HELP "List loaded extensions and their size in memory"
 
-#define LLEXT_LOAD_HEX_HELP								\
-	"Load an elf file encoded in hex directly from the shell input. Syntax:\n"	\
+#define LLEXT_LOAD_HEX_HELP                                                                        \
+	"Load an elf file encoded in hex directly from the shell input. Syntax:\n"                 \
 	"<ext_name> <ext_hex_string>"
 
-#define LLEXT_UNLOAD_HELP								\
-	"Unload an extension by name. Syntax:\n"					\
+#define LLEXT_UNLOAD_HELP                                                                          \
+	"Unload an extension by name. Syntax:\n"                                                   \
 	"<ext_name>"
 
-#define LLEXT_LIST_SYMBOLS_HELP								\
-	"List extension symbols. Syntax:\n"						\
+#define LLEXT_LIST_SYMBOLS_HELP                                                                    \
+	"List extension symbols. Syntax:\n"                                                        \
 	"<ext_name>"
 
-#define LLEXT_CALL_FN_HELP								\
-	"Call extension function with prototype void fn(void). Syntax:\n"		\
+#define LLEXT_CALL_FN_HELP                                                                         \
+	"Call extension function with prototype void fn(void). Syntax:\n"                          \
 	"<ext_name> <function_name>"
+
+#ifdef CONFIG_FILE_SYSTEM
+#define LLEXT_LOAD_FS_HELP                                                                         \
+	"Load an elf file directly from filesystem. Syntax:\n"                                     \
+	"<ext_name> <ext_llext_file_name>"
+
+#endif /* CONFIG_FILE_SYSTEM */
 
 static int cmd_llext_list_symbols(const struct shell *sh, size_t argc, char *argv[])
 {
@@ -182,10 +190,44 @@ static int cmd_llext_call_fn(const struct shell *sh, size_t argc, char *argv[])
 	return 0;
 }
 
+#ifdef CONFIG_FILE_SYSTEM
+static int cmd_llext_load_fs(const struct shell *sh, size_t argc, char *argv[])
+{
+	int res;
+	struct fs_dirent dirent;
+
+	res = fs_stat(argv[2], &dirent);
+	if (res) {
+		shell_error(sh, "Failed to obtain file %s, return code %d\n", argv[2], res);
+		return res;
+	}
+
+	if (dirent.type != FS_DIR_ENTRY_FILE) {
+		shell_error(sh, "Not a file %s", argv[2]);
+		return -ENOEXEC;
+	}
+
+	struct llext_fs_loader fs_loader = LLEXT_FS_LOADER(argv[2]);
+	struct llext_loader *ldr = &fs_loader.loader;
+	struct llext_load_param ldr_parm = LLEXT_LOAD_PARAM_DEFAULT;
+	struct llext *ext;
+
+	res = llext_load(ldr, argv[1], &ext, &ldr_parm);
+	if (res < 0) {
+		shell_print(sh, "Failed to load extension %s, return code %d\n", ext->name, res);
+		return -ENOEXEC;
+	}
+	shell_print(sh, "Successfully loaded extension %s, addr %p\n", ext->name, ext);
+	return 0;
+}
+#endif
 
 /* clang-format off */
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_llext,
 	SHELL_CMD(list, NULL, LLEXT_LIST_HELP, cmd_llext_list),
+#ifdef CONFIG_FILE_SYSTEM
+	SHELL_CMD_ARG(load_llext, NULL, LLEXT_LOAD_FS_HELP, cmd_llext_load_fs, 3, 0),
+#endif
 	SHELL_CMD_ARG(load_hex, NULL, LLEXT_LOAD_HEX_HELP, cmd_llext_load_hex, 3, 0),
 	SHELL_CMD_ARG(unload, &msub_llext_name, LLEXT_UNLOAD_HELP, cmd_llext_unload, 2, 0),
 	SHELL_CMD_ARG(list_symbols, &msub_llext_name, LLEXT_LIST_SYMBOLS_HELP,
