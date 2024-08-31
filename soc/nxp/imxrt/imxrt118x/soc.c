04d9c2295cf3cbb7f6a742cc4e14b74efe7253db
Lucien Zhao <lucien.zhao@nxp.com>
2024-08-31 14:28:06 +0800

    dts: arm: nxp: nxp_rt118x: add acmp instances
    enable acmp clock in rt118x/soc.c file
    
    add instances and enable clock for rt118x
    
    Signed-off-by: Lucien Zhao <lucien.zhao@nxp.com>

--- soc/nxp/imxrt/imxrt118x/soc.c
+++ soc/nxp/imxrt/imxrt118x/soc.c
@@ -269,6 +269,20 @@ static ALWAYS_INLINE void clock_init(void)
 
 #endif /* CONFIG_COUNTER_MCUX_GPT */
 
+#ifdef CONFIG_MCUX_ACMP
+
+#if (DT_NODE_HAS_STATUS(DT_NODELABEL(acmp1), okay)  \
+	|| DT_NODE_HAS_STATUS(DT_NODELABEL(acmp2), okay) \
+	|| DT_NODE_HAS_STATUS(DT_NODELABEL(acmp3), okay) \
+	|| DT_NODE_HAS_STATUS(DT_NODELABEL(acmp4), okay))
+	/* Configure ACMP using MuxSysPll3Out */
+	rootCfg.mux = kCLOCK_ACMP_ClockRoot_MuxSysPll3Out;
+	rootCfg.div = 2;
+	CLOCK_SetRootClock(kCLOCK_Root_Acmp, &rootCfg);
+#endif
+
+#endif /* CONFIG_MCUX_ACMP */
+
 	/* Keep core clock ungated during WFI */
 	CCM->LPCG[1].LPM0 = 0x33333333;
 	CCM->LPCG[1].LPM1 = 0x33333333;
