41c6483ef5dc39d1991afad6451b8e1f3384f64e
Darcy Lu <darcy_lu@realsil.com.cn>
2024-09-06 10:05:01 +0800

    soc: add realtek RTS5817
    Add support for the rts5817 MCU
    
    Signed-off-by: Darcy Lu <darcy_lu@realsil.com.cn>

---
+++ soc/realtek/fingerprint/rts5817/soc.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024 Realtek Semiconductor, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC_H_
+#define _SOC_H_
+
+#include <cmsis_core_m_defaults.h>
+#include <cachel1_armv7.h>
+
+#endif /* _SOC_H_ */
