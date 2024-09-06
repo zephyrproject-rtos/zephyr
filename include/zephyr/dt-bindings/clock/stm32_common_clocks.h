6c7f0c0b6c5beab9acd009795d0d1be905fe37a2
Joakim Andersson <joerchan@gmail.com>
2024-09-06 11:24:34 +0200

    include: Add STM32_MCO_CFGR bit encoding and decoding
    Add macro for encoding MCO bit values into the CFGR register
    of RCC. Add macros for decoding and extracting the different fields.
    
    Signed-off-by: Joakim Andersson <joerchan@gmail.com>

--- include/zephyr/dt-bindings/clock/stm32_common_clocks.h
+++ include/zephyr/dt-bindings/clock/stm32_common_clocks.h
@@ -15,4 +15,34 @@
 /** Dummy: Add a specifier when no selection is possible */
 #define NO_SEL			0xFF
 
+/** STM32 MCO configuration values */
+#define STM32_MCO_CFGR_REG_MASK     0xFFFFU
+#define STM32_MCO_CFGR_REG_SHIFT    0U
+#define STM32_MCO_CFGR_SHIFT_MASK   0x3FU
+#define STM32_MCO_CFGR_SHIFT_SHIFT  16U
+#define STM32_MCO_CFGR_MASK_MASK    0x1FU
+#define STM32_MCO_CFGR_MASK_SHIFT   22U
+#define STM32_MCO_CFGR_VAL_MASK     0x1FU
+#define STM32_MCO_CFGR_VAL_SHIFT    27U
+
+/**
+ * @brief STM32 MCO configuration register bit field
+ *
+ * @param reg    Offset to RCC register holding MCO configuration
+ * @param shift    Position of field within RCC register (= field LSB's index)
+ * @param mask    Mask of register field in RCC register
+ * @param val    Clock configuration field value (0~0x1F)
+ *
+ * @note 'reg' range:    0x0~0xFFFF    [ 00 : 15 ]
+ * @note 'shift' range:    0~63        [ 16 : 21 ]
+ * @note 'mask' range:    0x00~0x1F    [ 22 : 26 ]
+ * @note 'val' range:    0x00~0x1F    [ 27 : 31 ]
+ *
+ */
+#define STM32_MCO_CFGR(val, mask, shift, reg)                        \
+	((((reg) & STM32_MCO_CFGR_REG_MASK) << STM32_MCO_CFGR_REG_SHIFT) |        \
+	 (((shift) & STM32_MCO_CFGR_SHIFT_MASK) << STM32_MCO_CFGR_SHIFT_SHIFT) |    \
+	 (((mask) & STM32_MCO_CFGR_MASK_MASK) << STM32_MCO_CFGR_MASK_SHIFT) |        \
+	 (((val) & STM32_MCO_CFGR_VAL_MASK) << STM32_MCO_CFGR_VAL_SHIFT))
+
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_STM32_COMMON_CLOCKS_H_ */
