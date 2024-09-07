403d517279b6d039202b1ccfe2e5930d8f55802e
Abderrahmane Jarmouni <git@jarmouni.me>
2024-09-07 15:22:33 +0200

    boards: stm32: add flashing with stm32cubeprog for all
    Add STM32CubeProgrammer where it is not available, & make it
    default flashing method by placing it 1st in board.cmake
    
    Before this change, the west runner that's selected when calling
    'westflash' without the '--runner ...' option is kind of arbitrary
    & depends on which runner is 1st in board.cmake file.
    
    Another reason this change is needed is that STM32CubeProg is the only
    available way to check/modify Option Bytes, and to flash code to
    external Flash on many STM32 boards.
    
    This change does not affect 'west debug' behavior, which defaults to
    the debugger (OpenOCD/JLink) that is included first in board.cmake
    
    Signed-off-by: Abderrahmane Jarmouni <git@jarmouni.me>

--- boards/st/nucleo_l496zg/board.cmake
+++ boards/st/nucleo_l496zg/board.cmake
@@ -1,6 +1,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
+# keep first
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(jlink "--device=STM32L496ZG" "--speed=4000")
 
+# keep first
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
--- boards/st/steval_stwinbx1/board.cmake
+++ boards/st/steval_stwinbx1/board.cmake
@@ -13,6 +13,7 @@ board_runner_args(openocd "--tcl-port=6666")
 board_runner_args(openocd --cmd-pre-init "gdb_report_data_abort enable")
 board_runner_args(openocd "--no-halt")
 
+# keep first
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
