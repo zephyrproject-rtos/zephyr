23156596c53fdbf4dfb9b17f4882f57d521e0602
Abderrahmane Jarmouni <git@jarmouni.me>
2024-09-07 15:22:33 +0200

    boards: stm32: add flashing with stm32cubeprog for all
    Add STM32CubeProgrammer where it is not available, & make it
    default flashing method by placing it 1st in board.cmake
    
    Before this change, the west runner that's selected when calling
    'westflash' without the '--runner ...' option is kind of arbitrary
    & depends on which runner is 1st in board.cmake file.
    
    Another reason this change is needed is that STM32CubeProg is the only
    available way to check/modify Option Bytes, and to flash code to
    external Flash on many STM32 boards.
    
    This change does not affect 'west debug' behavior, which defaults to
    the debugger (OpenOCD/JLink) that is included first in board.cmake
    
    Signed-off-by: Abderrahmane Jarmouni <git@jarmouni.me>

--- boards/st/nucleo_l552ze_q/board.cmake
+++ boards/st/nucleo_l552ze_q/board.cmake
@@ -1,4 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
+
 if(CONFIG_BUILD_WITH_TFM)
   set(FLASH_BASE_ADDRESS_S 0x0C000000)
 
@@ -12,10 +13,15 @@ if(CONFIG_BUILD_WITH_TFM)
   endif()
 endif()
 
+# keep first
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+
 set_ifndef(BOARD_DEBUG_RUNNER pyocd)
 set_ifndef(BOARD_FLASH_RUNNER pyocd)
 
 board_runner_args(pyocd "--target=stm32l552zetxq")
 
+# keep first
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
