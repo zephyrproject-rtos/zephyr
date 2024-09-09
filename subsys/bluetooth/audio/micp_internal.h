a0d1161c5566fdc42e060f3f9aa4e05c6234935b
Emil Gydesen <emil.gydesen@nordicsemi.no>
2024-09-09 11:42:25 +0200

    Bluetooth: MICP: Replace busy bool with atomic
    Replace the busy boolean flag with an atomic value.
    This prevents any race conditions with the MICP client implementation.
    
    This also adds a missing check for the discovery procedure.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/audio/micp_internal.h
+++ subsys/bluetooth/audio/micp_internal.h
@@ -18,6 +18,13 @@
 #include <zephyr/autoconf.h>
 #include <zephyr/bluetooth/conn.h>
 #include <zephyr/bluetooth/gatt.h>
+#include <zephyr/sys/atomic.h>
+
+enum bt_micp_mic_ctlr_flag {
+	BT_MICP_MIC_CTLR_FLAG_BUSY,
+
+	BT_MICP_MIC_CTLR_FLAG_NUM_FLAGS, /* keep as last */
+};
 
 struct bt_micp_mic_ctlr {
 	uint16_t start_handle;
@@ -26,7 +33,6 @@ struct bt_micp_mic_ctlr {
 	struct bt_gatt_subscribe_params mute_sub_params;
 	struct bt_gatt_discover_params mute_sub_disc_params;
 
-	bool busy;
 	uint8_t mute_val_buf[1]; /* Mute value is a single octet */
 	struct bt_gatt_write_params write_params;
 	struct bt_gatt_read_params read_params;
@@ -37,6 +43,8 @@ struct bt_micp_mic_ctlr {
 	uint8_t aics_inst_cnt;
 	struct bt_aics *aics[CONFIG_BT_MICP_MIC_CTLR_MAX_AICS_INST];
 #endif /* CONFIG_BT_MICP_MIC_CTLR_AICS */
+
+	ATOMIC_DEFINE(flags, BT_MICP_MIC_CTLR_FLAG_NUM_FLAGS);
 };
 
 #endif /* ZEPHYR_INCLUDE_BLUETOOTH_AUDIO_MICP_INTERNAL_ */
