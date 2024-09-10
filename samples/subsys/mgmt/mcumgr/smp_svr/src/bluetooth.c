4c2bd909605842781153d30c516d30767a203842
Mateusz Kapala <mateusz.kapala@nordicsemi.no>
2024-09-10 13:58:46 +0200

    samples: subsys: mgmt: mcumgr: smp_svr: Use SMP_BT_SVC_UUID_VAL define
    Used the SMP_BT_SVC_UUID_VAL define exposed in the smp_bt header
    in the smp_svr sample instead of hardcoding its value.
    
    Signed-off-by: Mateusz Kapala <mateusz.kapala@nordicsemi.no>

--- samples/subsys/mgmt/mcumgr/smp_svr/src/bluetooth.c
+++ samples/subsys/mgmt/mcumgr/smp_svr/src/bluetooth.c
@@ -19,9 +19,7 @@ static struct k_work advertise_work;
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-	BT_DATA_BYTES(BT_DATA_UUID128_ALL,
-		      0x84, 0xaa, 0x60, 0x74, 0x52, 0x8a, 0x8b, 0x86,
-		      0xd3, 0x4c, 0xb7, 0x1d, 0x1d, 0xdc, 0x53, 0x8d),
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, SMP_BT_SVC_UUID_VAL),
 };
 
 static const struct bt_data sd[] = {
