e6506619ca7acee754d9bfb02bf026fa5b03f9d7
Anas Nashif <anas.nashif@intel.com>
2024-09-10 09:42:33 -0400

    soc: gd: move init code from SYS_INIT to hooks
    Replace SYS_INIT with SoC hooks and adapt SoC init code
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- soc/gd/gd32/gd32f3x0/soc.c
+++ soc/gd/gd32/gd32f3x0/soc.c
@@ -7,11 +7,7 @@
 #include <zephyr/init.h>
 #include <soc.h>
 
-static int gd32f3x0_init(void)
+void soc_early_init_hook(void)
 {
 	SystemInit();
-
-	return 0;
 }
-
-SYS_INIT(gd32f3x0_init, PRE_KERNEL_1, 0);
