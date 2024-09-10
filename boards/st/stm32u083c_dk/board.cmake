f4f3a3752d71eca1ff9b8d668114aeb3c3db6076
Abderrahmane Jarmouni <git@jarmouni.me>
2024-09-11 00:30:21 +0200

    boards: st: update board.cmake
    Update forgotten boards following PR 75284
    
    Signed-off-by: Abderrahmane Jarmouni <git@jarmouni.me>

--- boards/st/stm32u083c_dk/board.cmake
+++ boards/st/stm32u083c_dk/board.cmake
@@ -1,9 +1,11 @@
+# keep first
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 
 board_runner_args(pyocd "--target=stm32u083mctx")
 
 board_runner_args(jlink "--device=STM32U083C-DK" "--reset-after-load")
 
+# keep first
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
