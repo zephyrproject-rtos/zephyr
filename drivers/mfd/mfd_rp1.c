8d45d57333cbc30cbbdae75fc77094db7099df68
Junho Lee <junho@tsnlab.com>
2024-09-12 16:15:08 +0900

    Remove unnecessary properties from rp1 driver

--- drivers/mfd/mfd_rp1.c
+++ drivers/mfd/mfd_rp1.c
@@ -14,14 +14,13 @@
 #include <zephyr/sys/device_mmio.h>
 
 struct mfd_rp1_config {
-	const struct device *pci_dev;
-	uintptr_t cfg_addr;
+	uintptr_t cfg_phys_addr;
 	size_t cfg_size;
 };
 
 struct mfd_rp1_data {
+	mm_reg_t cfg_addr;
 };
-
 
 static int mfd_rp1_init(const struct device *dev)
 {
@@ -34,11 +33,7 @@ static int mfd_rp1_init(const struct device *dev)
 #define MFD_RP1_INIT(n)                                                                            \
 	static struct mfd_rp1_data mfd_rp1_data_##n;                                               \
                                                                                                    \
-	static const struct mfd_rp1_config mfd_rp1_cfg_##n = {                                     \
-		.pci_dev = DEVICE_DT_GET(DT_PARENT(DT_DRV_INST(n))),                               \
-		.cfg_addr = DT_INST_REG_ADDR(n),                                                   \
-		.cfg_size = DT_INST_REG_SIZE(n),                                                   \
-	};                                                                                         \
+	static const struct mfd_rp1_config mfd_rp1_cfg_##n;                                        \
                                                                                                    \
 	DEVICE_DT_INST_DEFINE(n, mfd_rp1_init, NULL, &mfd_rp1_data_##n, &mfd_rp1_cfg_##n,          \
 			      POST_KERNEL, 98, NULL);
