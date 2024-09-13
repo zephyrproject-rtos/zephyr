8a05a965f2e1d35f3de2335f0f7c126636ab6359
Fengming Ye <frank.ye@nxp.com>
2024-09-13 17:21:52 +0900

    [nxp toup] hostap: add IEEE80211R support
    Add 80211R support in hostap.
    Add cmd wifi connect option '-R' to enable 80211R.
    
    Signed-off-by: Fengming Ye <frank.ye@nxp.com>

--- include/zephyr/net/wifi_mgmt.h
+++ include/zephyr/net/wifi_mgmt.h
@@ -500,6 +500,8 @@ struct wifi_connect_req_params {
 	const uint8_t *eap_password;
 	/** eap passwd length */
 	uint8_t eap_passwd_length; /* Max 128 */
+	/** Fast BSS Transition used */
+	int ft_used;
 };
 
 /** @brief Wi-Fi connect result codes. To be overlaid on top of \ref wifi_status
