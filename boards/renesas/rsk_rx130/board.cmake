cea3bd2ea4317df8e52b80201d76e0c430bac13c
Phi Tran <phi.tran.jg@bp.renesas.com>
2024-09-13 15:32:09 +0700

    scripts: west: flash: Add support for .mot file format
    Add support for .mot file flash using west flash command
    The RX build output .mot as binary file to flash into
    board
    
    Signed-off-by: Phi Tran <phi.tran.jg@bp.renesas.com>
    Signed-off-by: Duy Nguyen <duy.nguyen.xa@renesas.com>

--- boards/renesas/rsk_rx130/board.cmake
+++ boards/renesas/rsk_rx130/board.cmake
@@ -3,6 +3,5 @@
 
 # options after "--tool-opt=" are directly passed to the tool. So instead of "--iface=JTAG" you could also write "--tool-opt=-if JTAG"
 board_runner_args(jlink "--device=R5F51308" "--iface=FINE" "--speed=1000" "--tool-opt=-jtagconf -1,-1 -autoconnect 1")
-board_runner_args(jlink "--use-mot")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
