58ae31e23e843b27195c4f5e8e90b5a9d6805062
Tomasz Leman <tomasz.m.leman@intel.com>
2024-09-16 17:56:16 +0200

    soc: intel_adsp: ace: Use DT macros instead of hardcoded values
    Replace hardcoded register addresses and values in
    asm_memory_management.h with Devicetree (DT) macros for LPSRAM
    power-down operations. This change ensures that register addresses and
    bank counts are dynamically obtained from the Devicetree, improving code
    portability and reducing the risk of errors due to manual updates.
    
    - Removed hardcoded LSPGCTL address definitions.
    - Updated m_ace_lpsram_power_down_entire macro to use DT_NODELABEL to
      fetch LPSRAM bank count and control register address
    - Adjusted bit field extraction logic to align with the updated register
      information from the Devicetree.
    
    This commit aligns with the ongoing effort to utilize Devicetree for
    hardware abstraction and to facilitate easier maintenance and updates to
    the codebase.
    
    Signed-off-by: Tomasz Leman <tomasz.m.leman@intel.com>

--- soc/intel/intel_adsp/ace/asm_memory_management.h
+++ soc/intel/intel_adsp/ace/asm_memory_management.h
@@ -9,33 +9,34 @@
 #ifndef __ZEPHYR_ACE_LIB_ASM_MEMORY_MANAGEMENT_H__
 #define __ZEPHYR_ACE_LIB_ASM_MEMORY_MANAGEMENT_H__
 
-/* These definitions should be placed elsewhere, but I can't find a good place for them. */
-#define LSPGCTL				0x71D80
-#define LSPGCTL_HIGH			((LSPGCTL >> 4) & 0xff00)
-#define LSPGCTL_LOW			((LSPGCTL >> 4) & 0xff)
-
 #include <zephyr/devicetree.h>
 
 #ifdef _ASMLANGUAGE
 
 .macro m_ace_lpsram_power_down_entire ax, ay, az, au
-	movi \au, 8 /* LPSRAM_EBB_QUANTITY */
-	movi \az, LSPGCTL_LOW
-	addmi \az, \az, LSPGCTL_HIGH
-	slli \az, \az, 4
+	/* Retrieve the LPSRAM bank count from the ACE_L2MCAP register */
+	movi \az, DT_REG_ADDR(DT_NODELABEL(hsbcap))
+	l32i \az, \az, 0
+	/* Extract the 4-bit bank count field starting from bit 8 */
+	extui \au, \az, 8, 4
 
-	movi \ay, 1
+	movi \ay, 1       /* Power down command */
+
+	/* Get the address of the LPSRAM control register from the Devicetree */
+	movi \az, DT_REG_ADDR(DT_NODELABEL(lsbpm))
 2 :
+	/* Issue the power down command to the current LPSRAM bank */
 	s8i \ay, \az, 0
 	memw
-
 1 :
+	/* Poll the status register to confirm the power down command has taken effect */
 	l8ui \ax, \az, 4
 	bne \ax, \ay, 1b
 
-	addi \az, \az, 8
-	addi \au, \au, -1
-	bnez \au, 2b
+	/* Move to the next LPSRAM bank control register */
+	addi \az, \az, DT_REG_SIZE(DT_NODELABEL(lsbpm))
+	addi \au, \au, -1 /* Decrement bank count */
+	bnez \au, 2b      /* If banks are left, continue loop */
 .endm
 
 .macro m_ace_hpsram_power_down_entire ax, ay, az, au
