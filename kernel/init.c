83eaa7d28ed821fbf68f6b75756fda77e5fde7a4
Yong Cong Sin <ycsin@meta.com>
2024-09-18 00:07:25 +0800

    kernel: remove `CONFIG_MP_NUM_CPUS`
    `CONFIG_MP_NUM_CPUS` has been deprecated for more than 2
    releases, it's time to remove it.
    
    Updated all usage of `CONFIG_MP_NUM_CPUS` to
    `CONFIG_MP_MAX_NUM_CPUS`
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>
    Signed-off-by: Yong Cong Sin <yongcong.sin@gmail.com>

--- kernel/init.c
+++ kernel/init.c
@@ -41,9 +41,6 @@
 #include <zephyr/internal/syscall_handler.h>
 LOG_MODULE_REGISTER(os, CONFIG_KERNEL_LOG_LEVEL);
 
-BUILD_ASSERT(CONFIG_MP_NUM_CPUS == CONFIG_MP_MAX_NUM_CPUS,
-	     "CONFIG_MP_NUM_CPUS and CONFIG_MP_MAX_NUM_CPUS need to be set the same");
-
 /* the only struct z_kernel instance */
 __pinned_bss
 struct z_kernel _kernel;
