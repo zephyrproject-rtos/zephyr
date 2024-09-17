3aa05c8ce97b0fc6abddb76026dc9a8173226257
Jérôme Pouiller <jerome.pouiller@silabs.com>
2024-09-17 15:20:06 +0200

    boards: silabs: Uniformize board.cmake
    All the board.cmake contains more or less the same data. This patch
    applies the cosmetics changes to align the structure of all these files.
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- boards/silabs/starter_kits/slstk3401a/board.cmake
+++ boards/silabs/starter_kits/slstk3401a/board.cmake
@@ -1,8 +1,5 @@
-#
 # Copyright (c) 2020, Rafael Dias Menezes <rdmeneze@gmail.com>
-#
 # SPDX-License-Identifier: Apache-2.0
-#
 
 board_runner_args(jlink "--device=EFM32PG1BxxxF256")
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
