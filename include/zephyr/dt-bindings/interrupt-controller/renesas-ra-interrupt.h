b288bac6004f2a6ef8e68e24ddd13cc7957cf473
Tran Van Quy <quy.tran.pz@renesas.com>
2024-09-18 13:29:22 +0700

    dts: renesas: Update interrupt ID in dts for Renesas RA8 series
    Update interrupt for Renesas RA8 series, to use with dts python script.
    The target is to handle the interrupt ID allocation in build time
    
    Signed-off-by: Tran Van Quy <quy.tran.pz@renesas.com>

---
+++ include/zephyr/dt-bindings/interrupt-controller/renesas-ra-interrupt.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024 Renesas Electronics Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DT_BINDINGS_INTERRUPT_CONTROLLER_RENESAS_RA_INTERRUPT_H_
+#define ZEPHYR_DT_BINDINGS_INTERRUPT_CONTROLLER_RENESAS_RA_INTERRUPT_H_
+
+/* When using macros in the device tree, the interrupt numbers will be
+ * automatically filled in by the script.
+ */
+#define RA_ICU_UNSPECIFIED (0xff)
+
+#endif /* ZEPHYR_DT_BINDINGS_INTERRUPT_CONTROLLER_RENESAS_RA_INTERRUPT_H_ */
