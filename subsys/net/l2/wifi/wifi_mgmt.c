ce174fda4e7ecf6a0bc4cd9d7439f3dfaa5ca784
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-09-18 11:22:07 -0500

    Revert "[nxp noup] hostap: add set RTS threshold cmd support"
    This reverts commit 0bf59842600ad9ccff9c455052e364f0412edf24.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- subsys/net/l2/wifi/wifi_mgmt.c
+++ subsys/net/l2/wifi/wifi_mgmt.c
@@ -655,26 +655,6 @@ static int wifi_ap_config_params(uint32_t mgmt_request, struct net_if *iface,
 
 NET_MGMT_REGISTER_REQUEST_HANDLER(NET_REQUEST_WIFI_AP_CONFIG_PARAM, wifi_ap_config_params);
 
-static int wifi_ap_set_rts_threshold(uint32_t mgmt_request, struct net_if *iface,
-				  void *data, size_t len)
-{
-	const struct device *dev = net_if_get_device(iface);
-	const struct wifi_mgmt_ops *const wifi_mgmt_api = get_wifi_api(iface);
-	unsigned int *rts_threshold = data;
-
-	if (wifi_mgmt_api == NULL || wifi_mgmt_api->set_rts_threshold == NULL) {
-		return -ENOTSUP;
-	}
-
-	if (!data || len != sizeof(*rts_threshold)) {
-		return -EINVAL;
-	}
-
-	return wifi_mgmt_api->set_rts_threshold(dev, *rts_threshold);
-}
-
-NET_MGMT_REGISTER_REQUEST_HANDLER(NET_REQUEST_WIFI_AP_RTS_THRESHOLD, wifi_ap_set_rts_threshold);
-
 static int wifi_iface_status(uint32_t mgmt_request, struct net_if *iface,
 			  void *data, size_t len)
 {
