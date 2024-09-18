435e8252d221626f54272d3a88de1ab2331e5075
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-09-18 11:22:10 -0500

    Revert "[nxp noup] subsys: support WPA2 and WPA3 mixed PWE support"
    This reverts commit 34f93d562a75f242cad12562dc21a3c4f83914d5.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- subsys/net/l2/wifi/wifi_shell.c
+++ subsys/net/l2/wifi/wifi_shell.c
@@ -574,8 +574,7 @@ static int __wifi_args_to_params(const struct shell *sh, size_t argc, char *argv
 			}
 			break;
 		case 'e':
-			if (params->security != WIFI_SECURITY_TYPE_SAE ||
-				params->security != WIFI_SECURITY_TYPE_WPA_AUTO_PERSONAL) {
+			if (params->security != WIFI_SECURITY_TYPE_SAE) {
 				PR_ERROR("PWE not supported for security type %s\n",
 						 wifi_security_txt(params->security));
 				return -EINVAL;
