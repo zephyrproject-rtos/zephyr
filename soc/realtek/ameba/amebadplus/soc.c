e21fd8fbe9e9fbf6d33085f527453107d5661267
zjian zhang <zjian_zhang@realsil.com.cn>
2024-09-19 21:27:27 +0800

    soc: add realtek amebadplus SOC integration
    Add initial version of Amebadplus Soc integration
    
    Signed-off-by: zjian zhang <zjian_zhang@realsil.com.cn>

---
+++ soc/realtek/ameba/amebadplus/soc.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2024 Realtek Semiconductor Corp.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <soc.h>
+#include <ameba_soc.h>
+
+#include <zephyr/device.h>
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <zephyr/cache.h>
+
+void z_arm_reset(void);
+
+IMAGE2_ENTRY_SECTION
+RAM_START_FUNCTION Img2EntryFun0 = {z_arm_reset, NULL, /* BOOT_RAM_WakeFromPG, */
+				    (uint32_t)NewVectorTable};
+
+void soc_early_init_hook(void)
+{
+	/*
+	 * Cache is enabled by default at reset, disable it before
+	 * sys_cache*-functions can enable them.
+	 */
+	Cache_Enable(DISABLE);
+	sys_cache_instr_enable();
+	sys_cache_data_enable();
+
+	XTAL_INIT();
+
+	if (SYSCFG_CHIPType_Get() == CHIP_TYPE_ASIC_POSTSIM) { /* Only Asic need OSC Calibration */
+		OSC4M_Init();
+		OSC4M_Calibration(30000);
+		if ((((BOOT_Reason()) & AON_BIT_RSTF_DSLP) == FALSE)) {
+			OSC131K_Calibration(30000); /* PPM=30000=3% */ /* 7.5ms */
+		}
+	}
+}
