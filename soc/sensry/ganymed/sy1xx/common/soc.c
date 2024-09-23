9dd24dca6472521258086cbaa1094769bbab3ccc
Sven Ginka <s.ginka@sensry.de>
2024-09-23 16:31:20 +0200

    soc: sensry: udma, pad renaming
    Before that fix the names for UDMA could be misleading.
    With that fix the namespace is clear and easy to follow.
    Same applies for peripheral addresses and pad config.
    
    Signed-off-by: Sven Ginka <s.ginka@sensry.de>

--- soc/sensry/ganymed/sy1xx/common/soc.c
+++ soc/sensry/ganymed/sy1xx/common/soc.c
@@ -15,33 +15,33 @@ LOG_MODULE_REGISTER(soc);
 #include "soc.h"
 
 /* ITC */
-#define ARCHI_ITC_MASK_OFFSET       0x0
-#define ARCHI_ITC_MASK_SET_OFFSET   0x4
-#define ARCHI_ITC_MASK_CLR_OFFSET   0x8
-#define ARCHI_ITC_STATUS_OFFSET     0xc
-#define ARCHI_ITC_STATUS_SET_OFFSET 0x10
-#define ARCHI_ITC_STATUS_CLR_OFFSET 0x14
-#define ARCHI_ITC_ACK_OFFSET        0x18
-#define ARCHI_ITC_ACK_SET_OFFSET    0x1c
-#define ARCHI_ITC_ACK_CLR_OFFSET    0x20
-#define ARCHI_ITC_FIFO_OFFSET       0x24
+#define SY1XX_ARCHI_ITC_MASK_OFFSET       0x0
+#define SY1XX_ARCHI_ITC_MASK_SET_OFFSET   0x4
+#define SY1XX_ARCHI_ITC_MASK_CLR_OFFSET   0x8
+#define SY1XX_ARCHI_ITC_STATUS_OFFSET     0xc
+#define SY1XX_ARCHI_ITC_STATUS_SET_OFFSET 0x10
+#define SY1XX_ARCHI_ITC_STATUS_CLR_OFFSET 0x14
+#define SY1XX_ARCHI_ITC_ACK_OFFSET        0x18
+#define SY1XX_ARCHI_ITC_ACK_SET_OFFSET    0x1c
+#define SY1XX_ARCHI_ITC_ACK_CLR_OFFSET    0x20
+#define SY1XX_ARCHI_ITC_FIFO_OFFSET       0x24
 
 void sys_arch_reboot(int type)
 {
 	ARG_UNUSED(type);
 }
 
-#define ARCHI_REF_CLOCK (32768)
-#define ARCHI_PER_CLOCK (125000000)
+#define SY1XX_ARCHI_REF_CLOCK (32768)
+#define SY1XX_ARCHI_PER_CLOCK (125000000)
 
-uint32_t soc_get_rts_clock_frequency(void)
+uint32_t sy1xx_soc_get_rts_clock_frequency(void)
 {
-	return ARCHI_REF_CLOCK;
+	return SY1XX_ARCHI_REF_CLOCK;
 }
 
-uint32_t soc_get_peripheral_clock(void)
+uint32_t sy1xx_soc_get_peripheral_clock(void)
 {
-	return ARCHI_PER_CLOCK;
+	return SY1XX_ARCHI_PER_CLOCK;
 }
 
 void riscv_clic_irq_priority_set(uint32_t irq, uint32_t prio, uint32_t flags)
@@ -51,17 +51,18 @@ void riscv_clic_irq_priority_set(uint32_t irq, uint32_t prio, uint32_t flags)
 
 void soc_enable_irq(uint32_t idx)
 {
-	uint32_t current = sys_read32(ARCHI_FC_ITC_ADDR + ARCHI_ITC_MASK_SET_OFFSET);
+	uint32_t current = sys_read32(SY1XX_ARCHI_FC_ITC_ADDR + SY1XX_ARCHI_ITC_MASK_SET_OFFSET);
 
-	sys_write32(current | (1 << (idx & 0x1f)), ARCHI_FC_ITC_ADDR + ARCHI_ITC_MASK_SET_OFFSET);
+	sys_write32(current | (1 << (idx & 0x1f)),
+		    SY1XX_ARCHI_FC_ITC_ADDR + SY1XX_ARCHI_ITC_MASK_SET_OFFSET);
 }
 
 void soc_disable_irq(uint32_t idx)
 {
-	uint32_t current = sys_read32(ARCHI_FC_ITC_ADDR + ARCHI_ITC_MASK_CLR_OFFSET);
+	uint32_t current = sys_read32(SY1XX_ARCHI_FC_ITC_ADDR + SY1XX_ARCHI_ITC_MASK_CLR_OFFSET);
 
 	sys_write32(current & (~(1 << (idx & 0x1f))),
-		    ARCHI_FC_ITC_ADDR + ARCHI_ITC_MASK_CLR_OFFSET);
+		    SY1XX_ARCHI_FC_ITC_ADDR + SY1XX_ARCHI_ITC_MASK_CLR_OFFSET);
 }
 
 /*
@@ -82,10 +83,10 @@ void soc_interrupt_init(void)
  *
  * @return 0
  */
-static int soc_sy1xx_init(void)
+static int sy1xx_soc_init(void)
 {
 
 	return 0;
 }
 
-SYS_INIT(soc_sy1xx_init, PRE_KERNEL_1, 0);
+SYS_INIT(sy1xx_soc_init, PRE_KERNEL_1, 0);
