b92966c047e2af7da281d8671a8d8eabae75c025
Jordan Yates <jordan@embeint.com>
2024-09-23 21:35:38 +1000

    doc: get output folder from environment
    Pull the output build folder from the environment, instead of hardcoding
    the HTML output folder, which is wrong for the latex build.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- doc/conf.py
+++ doc/conf.py
@@ -8,7 +8,7 @@ import re
 import textwrap
 
 ZEPHYR_BASE = Path(__file__).resolve().parents[1]
-ZEPHYR_BUILD = Path(os.environ.get("DOCS_HTML_DIR")).resolve()
+ZEPHYR_BUILD = Path(os.environ.get("OUTPUT_DIR")).resolve()
 
 # Add the '_extensions' directory to sys.path, to enable finding Sphinx
 # extensions within.
