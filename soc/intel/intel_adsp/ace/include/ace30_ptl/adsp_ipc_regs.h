5a1f72aad4e036e0459ad8502c1406fba6b9ee83
Grzegorz Bernat <grzegorzx.bernat@intel.com>
2024-09-23 15:28:53 +0200

    soc: intel: Fix problems with the formatter
    No functional changes were made in this update.
    Only code formatting issues were corrected.
    
    This commit is necessary to preserve Git history
    continuity for future changes involving the switch from ace30_ptl to ace30.
    
    Signed-off-by: Grzegorz Bernat <grzegorzx.bernat@intel.com>

--- soc/intel/intel_adsp/ace/include/ace30_ptl/adsp_ipc_regs.h
+++ soc/intel/intel_adsp/ace/include/ace30_ptl/adsp_ipc_regs.h
@@ -44,8 +44,8 @@ struct intel_adsp_ipc {
  * This clears BUSY on the other side of the connection in IDR register.
  */
 #define INTEL_ADSP_IPC_ACE1X_TDA_DONE 0
-#define INTEL_ADSP_IPC_BUSY BIT(31)
-#define INTEL_ADSP_IPC_DONE BIT(31)
+#define INTEL_ADSP_IPC_BUSY           BIT(31)
+#define INTEL_ADSP_IPC_DONE           BIT(31)
 
 #define INTEL_ADSP_IPC_CTL_TBIE BIT(0)
 #define INTEL_ADSP_IPC_CTL_IDIE BIT(1)
--- soc/intel/intel_adsp/ace/include/ace30_ptl/adsp_power.h
+++ soc/intel/intel_adsp/ace/include/ace30_ptl/adsp_power.h
@@ -16,45 +16,45 @@
 
 /* Power Control register - controls the power domain operations. */
 struct ace_pwrctl {
-	uint16_t rsvd4      : 5;
-	uint16_t wphstpg    : 1;
-	uint16_t wphubhppg  : 1;
-	uint16_t wpdspulppg : 1;
-	uint16_t wpioxpg    : 2;
-	uint16_t rsvd11     : 2;
-	uint16_t wpmlpg     : 1;
-	uint16_t rsvd14     : 2;
-	uint16_t phubulppg  : 1;
+	uint16_t rsvd4: 5;
+	uint16_t wphstpg: 1;
+	uint16_t wphubhppg: 1;
+	uint16_t wpdspulppg: 1;
+	uint16_t wpioxpg: 2;
+	uint16_t rsvd11: 2;
+	uint16_t wpmlpg: 1;
+	uint16_t rsvd14: 2;
+	uint16_t phubulppg: 1;
 };
 
 struct ace_pwrctl2 {
-	uint16_t wpdsphpxpg : 5;
-	uint16_t rsvd15     : 11;
+	uint16_t wpdsphpxpg: 5;
+	uint16_t rsvd15: 11;
 };
 
-#define ACE_PWRCTL ((volatile struct ace_pwrctl *) &ACE_DfPMCCU.dfpwrctl)
-#define ACE_PWRCTL2 ((volatile struct ace_pwrctl2 *) &ACE_DfPMCCU.dfpwrctl2)
+#define ACE_PWRCTL  ((volatile struct ace_pwrctl *)&ACE_DfPMCCU.dfpwrctl)
+#define ACE_PWRCTL2 ((volatile struct ace_pwrctl2 *)&ACE_DfPMCCU.dfpwrctl2)
 
 /* Power Status register - reports the power domain status. */
 struct ace_pwrsts {
-	uint16_t rsvd4     : 5;
-	uint16_t hstpgs    : 1;
-	uint16_t hubhppgs  : 1;
-	uint16_t dspulppgs : 1;
-	uint16_t ioxpgs    : 2;
-	uint16_t rsvd11    : 2;
-	uint16_t mlpgs     : 1;
-	uint16_t rsvd14    : 2;
-	uint16_t hubulppgs : 1;
+	uint16_t rsvd4: 5;
+	uint16_t hstpgs: 1;
+	uint16_t hubhppgs: 1;
+	uint16_t dspulppgs: 1;
+	uint16_t ioxpgs: 2;
+	uint16_t rsvd11: 2;
+	uint16_t mlpgs: 1;
+	uint16_t rsvd14: 2;
+	uint16_t hubulppgs: 1;
 };
 
 struct ace_pwrsts2 {
-	uint16_t dsphpxpgs : 5;
-	uint16_t rsvd15    : 11;
+	uint16_t dsphpxpgs: 5;
+	uint16_t rsvd15: 11;
 };
 
-#define ACE_PWRSTS ((volatile struct ace_pwrsts *) &ACE_DfPMCCU.dfpwrsts)
-#define ACE_PWRSTS2 ((volatile struct ace_pwrsts2 *) &ACE_DfPMCCU.dfpwrsts2)
+#define ACE_PWRSTS  ((volatile struct ace_pwrsts *)&ACE_DfPMCCU.dfpwrsts)
+#define ACE_PWRSTS2 ((volatile struct ace_pwrsts2 *)&ACE_DfPMCCU.dfpwrsts2)
 
 /**
  * @brief Power up a specific CPU.
