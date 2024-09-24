20e33abd7bba0ccaf30558f13b3fd72d9d985989
Emil Gydesen <emil.gydesen@nordicsemi.no>
2024-09-24 18:13:13 +0200

    Bluetooth: CAP: Add support for handling ASE errors
    If we get an error/rejection from the CAP acceptor when
    performing the Unicast Audio Start or Stop procedure then
    we need to abort the procedure and let the application determine
    what the next step is.
    
    This change triggered a corner case when connecting to multiple
    CAP acceptors as the CAP initiatior. This was also fixed as part
    of this.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bluetooth/audio/cap_initiator/uut/bap_unicast_client.c
+++ tests/bluetooth/audio/cap_initiator/uut/bap_unicast_client.c
@@ -12,13 +12,17 @@
 
 #include <zephyr/bluetooth/audio/audio.h>
 #include <zephyr/bluetooth/audio/bap.h>
+#include <zephyr/bluetooth/conn.h>
 #include <zephyr/bluetooth/hci_types.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/sys/slist.h>
 #include <sys/errno.h>
 
 #include "bap_endpoint.h"
+#include "bap_iso.h"
 #include "ztest_assert.h"
+
+static struct bt_bap_unicast_client_cb *unicast_client_cb;
 
 bool bt_bap_ep_is_unicast_client(const struct bt_bap_ep *ep)
 {
@@ -38,6 +42,11 @@ int bt_bap_unicast_client_config(struct bt_bap_stream *stream,
 		break;
 	default:
 		return -EINVAL;
+	}
+
+	if (unicast_client_cb != NULL && unicast_client_cb->config != NULL) {
+		unicast_client_cb->config(stream, BT_BAP_ASCS_RSP_CODE_SUCCESS,
+					  BT_BAP_ASCS_REASON_NONE);
 	}
 
 	stream->ep->status.state = BT_BAP_EP_STATE_CODEC_CONFIGURED;
@@ -73,6 +82,11 @@ int bt_bap_unicast_client_qos(struct bt_conn *conn, struct bt_bap_unicast_group 
 
 	SYS_SLIST_FOR_EACH_CONTAINER(&group->streams, stream, _node) {
 		if (stream->conn == conn) {
+			if (unicast_client_cb != NULL && unicast_client_cb->qos != NULL) {
+				unicast_client_cb->qos(stream, BT_BAP_ASCS_RSP_CODE_SUCCESS,
+						       BT_BAP_ASCS_REASON_NONE);
+			}
+
 			stream->ep->status.state = BT_BAP_EP_STATE_QOS_CONFIGURED;
 
 			if (stream->ops != NULL && stream->ops->qos_set != NULL) {
@@ -98,6 +112,11 @@ int bt_bap_unicast_client_enable(struct bt_bap_stream *stream, const uint8_t met
 		return -EINVAL;
 	}
 
+	if (unicast_client_cb != NULL && unicast_client_cb->enable != NULL) {
+		unicast_client_cb->enable(stream, BT_BAP_ASCS_RSP_CODE_SUCCESS,
+					  BT_BAP_ASCS_REASON_NONE);
+	}
+
 	stream->ep->status.state = BT_BAP_EP_STATE_ENABLING;
 
 	if (stream->ops != NULL && stream->ops->enabled != NULL) {
@@ -120,6 +139,11 @@ int bt_bap_unicast_client_metadata(struct bt_bap_stream *stream, const uint8_t m
 		break;
 	default:
 		return -EINVAL;
+	}
+
+	if (unicast_client_cb != NULL && unicast_client_cb->metadata != NULL) {
+		unicast_client_cb->metadata(stream, BT_BAP_ASCS_RSP_CODE_SUCCESS,
+					    BT_BAP_ASCS_REASON_NONE);
 	}
 
 	if (stream->ops != NULL && stream->ops->metadata_updated != NULL) {
@@ -173,6 +197,11 @@ int bt_bap_unicast_client_start(struct bt_bap_stream *stream)
 		return -EINVAL;
 	}
 
+	if (unicast_client_cb != NULL && unicast_client_cb->start != NULL) {
+		unicast_client_cb->start(stream, BT_BAP_ASCS_RSP_CODE_SUCCESS,
+					 BT_BAP_ASCS_REASON_NONE);
+	}
+
 	stream->ep->status.state = BT_BAP_EP_STATE_STREAMING;
 
 	if (stream->ops != NULL && stream->ops->started != NULL) {
@@ -202,6 +231,11 @@ int bt_bap_unicast_client_disable(struct bt_bap_stream *stream)
 	 * client implementation fakes the behavior of it and always calls the disabled callback
 	 * when leaving the streaming state in a non-release manner
 	 */
+
+	if (unicast_client_cb != NULL && unicast_client_cb->disable != NULL) {
+		unicast_client_cb->disable(stream, BT_BAP_ASCS_RSP_CODE_SUCCESS,
+					   BT_BAP_ASCS_REASON_NONE);
+	}
 
 	/* Disabled sink ASEs go directly to the QoS configured state */
 	if (stream->ep->dir == BT_AUDIO_DIR_SINK) {
@@ -245,6 +279,11 @@ int bt_bap_unicast_client_stop(struct bt_bap_stream *stream)
 		return -EINVAL;
 	}
 
+	if (unicast_client_cb != NULL && unicast_client_cb->stop != NULL) {
+		unicast_client_cb->stop(stream, BT_BAP_ASCS_RSP_CODE_SUCCESS,
+					BT_BAP_ASCS_REASON_NONE);
+	}
+
 	stream->ep->status.state = BT_BAP_EP_STATE_QOS_CONFIGURED;
 
 	if (stream->ops != NULL && stream->ops->stopped != NULL) {
@@ -253,6 +292,30 @@ int bt_bap_unicast_client_stop(struct bt_bap_stream *stream)
 
 	if (stream->ops != NULL && stream->ops->qos_set != NULL) {
 		stream->ops->qos_set(stream);
+	}
+
+	/* If the stream can be disconnected, BAP will disconnect the stream once it reaches the
+	 * QoS Configured state. We simulator that behavior here, and if the stream is disconnected,
+	 * then the Unicast Server will set any paired stream to the QoS Configured state
+	 * autonomously as well.
+	 */
+	if (bt_bap_stream_can_disconnect(stream)) {
+		struct bt_bap_ep *pair_ep = bt_bap_iso_get_paired_ep(stream->ep);
+
+		if (pair_ep != NULL && pair_ep->stream != NULL) {
+			struct bt_bap_stream *pair_stream = pair_ep->stream;
+
+			pair_stream->ep->status.state = BT_BAP_EP_STATE_QOS_CONFIGURED;
+
+			if (pair_stream->ops != NULL && pair_stream->ops->stopped != NULL) {
+				pair_stream->ops->stopped(pair_stream,
+							  BT_HCI_ERR_LOCALHOST_TERM_CONN);
+			}
+
+			if (pair_stream->ops != NULL && pair_stream->ops->qos_set != NULL) {
+				pair_stream->ops->qos_set(pair_stream);
+			}
+		}
 	}
 
 	return 0;
@@ -277,12 +340,24 @@ int bt_bap_unicast_client_release(struct bt_bap_stream *stream)
 		return -EINVAL;
 	}
 
+	if (unicast_client_cb != NULL && unicast_client_cb->release != NULL) {
+		unicast_client_cb->release(stream, BT_BAP_ASCS_RSP_CODE_SUCCESS,
+					   BT_BAP_ASCS_REASON_NONE);
+	}
+
 	stream->ep->status.state = BT_BAP_EP_STATE_IDLE;
 	bt_bap_stream_reset(stream);
 
 	if (stream->ops != NULL && stream->ops->released != NULL) {
 		stream->ops->released(stream);
 	}
+
+	return 0;
+}
+
+int bt_bap_unicast_client_register_cb(struct bt_bap_unicast_client_cb *cb)
+{
+	unicast_client_cb = cb;
 
 	return 0;
 }
