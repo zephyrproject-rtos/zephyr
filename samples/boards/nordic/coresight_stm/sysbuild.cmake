fb7b779d288dd7f513da0ae1f80439bac6e3498c
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2024-09-25 14:30:41 +0200

    samples: boards: nordic: coresight_stm: Add flpr and ppr support
    Add flpr and ppr core to the sample.
    
    Since there are only 2 UARTs available on DK, local UART configuration
    is using only cpuapp and cpurad.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- samples/boards/nordic/coresight_stm/sysbuild.cmake
+++ samples/boards/nordic/coresight_stm/sysbuild.cmake
@@ -1,16 +1,28 @@
 # Copyright (c) 2024 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 
-if("${SB_CONFIG_RAD_CORE_BOARD}" STREQUAL "")
-	message(FATAL_ERROR
-	"Target ${BOARD} not supported for this sample. "
-	"There is no remote board selected in Kconfig.sysbuild")
-endif()
-
-set(RAD_APP remote)
+set(REMOTE_APP remote)
 
 ExternalZephyrProject_Add(
-	APPLICATION ${RAD_APP}
-	SOURCE_DIR  ${APP_DIR}/${RAD_APP}
-	BOARD       ${SB_CONFIG_RAD_CORE_BOARD}
+	APPLICATION ${REMOTE_APP}_rad
+	SOURCE_DIR  ${APP_DIR}/${REMOTE_APP}
+	BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpurad
 )
+
+# There are sample configurations which do not use PPR.
+if(SB_CONFIG_APP_CPUPPR_RUN)
+	ExternalZephyrProject_Add(
+		APPLICATION ${REMOTE_APP}_ppr
+		SOURCE_DIR  ${APP_DIR}/${REMOTE_APP}
+		BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpuppr
+	)
+endif()
+
+# There are sample configurations which do not use FLPR.
+if(SB_CONFIG_APP_CPUFLPR_RUN)
+	ExternalZephyrProject_Add(
+		APPLICATION ${REMOTE_APP}_flpr
+		SOURCE_DIR  ${APP_DIR}/${REMOTE_APP}
+		BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpuflpr
+	)
+endif()
