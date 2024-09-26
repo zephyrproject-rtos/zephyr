8810b1160b7c0517e3195b057747b63d80ee254b
Michał Stasiak <michal.stasiak@nordicsemi.no>
2024-09-26 15:43:43 +0200

    modules: hal_nordic: nrfx: add nRF54L20 FLPR core
    Added configuration files for nRF54L20 FLPR core.
    
    Signed-off-by: Michał Stasiak <michal.stasiak@nordicsemi.no>

--- modules/hal_nordic/nrfx/nrfx_config.h
+++ modules/hal_nordic/nrfx/nrfx_config.h
@@ -1056,6 +1056,8 @@
     #include <nrfx_config_nrf54l15_enga_flpr.h>
 #elif (defined(NRF54L20_XXAA) || defined(NRF54L20_ENGA_XXAA)) && defined(NRF_APPLICATION)
     #include <nrfx_config_nrf54l20_enga_application.h>
+#elif (defined(NRF54L20_XXAA) || defined(NRF54L20_ENGA_XXAA)) && defined(NRF_FLPR)
+    #include <nrfx_config_nrf54l20_enga_flpr.h>
 #elif defined(NRF9120_XXAA) || defined(NRF9160_XXAA)
     #include <nrfx_config_nrf91.h>
 #elif defined(NRF9230_ENGB_XXAA) && defined(NRF_APPLICATION)
