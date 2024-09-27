8d691cc90cde295899a159359ddebd187fda461e
Andrew Featherstone <andrew.featherstone@gmail.com>
2024-09-27 19:03:10 +0100

    boards: raspberrypi: rpi_pico2: add OpenOCD runner configuration
    Add OpenOCD debugger support.
    For now we will need a RaspberryPi forked version of OpenOCD.
    
    https://github.com/raspberrypi/openocd
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>
    Signed-off-by: Andrew Featherstone <andrew.featherstone@gmail.com>

--- boards/raspberrypi/rpi_pico2/board.cmake
+++ boards/raspberrypi/rpi_pico2/board.cmake
@@ -1,5 +1,18 @@
 # SPDX-License-Identifier: Apache-2.0
 
+if ("${RPI_PICO_DEBUG_ADAPTER}" STREQUAL "")
+	set(RPI_PICO_DEBUG_ADAPTER "cmsis-dap")
+endif()
+
+board_runner_args(openocd --cmd-pre-init "source [find interface/${RPI_PICO_DEBUG_ADAPTER}.cfg]")
+board_runner_args(openocd --cmd-pre-init "transport select swd")
+board_runner_args(openocd --cmd-pre-init "source [find target/rp2350.cfg]")
+
+# The adapter speed is expected to be set by interface configuration.
+# But if not so, set 2000 to adapter speed.
+board_runner_args(openocd --cmd-pre-init "set_adapter_speed_if_not_set 2000")
+
 board_runner_args(uf2 "--board-id=RP2350")
 
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
