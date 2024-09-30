bd116148c8f1054f956338bcb603e1fb51a451c9
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2024-09-30 16:05:34 +0200

    dts: nordic: refactor bindings helper symbols for SAADC
    Split header files containing symbols denoting SAADC inputs
    so that only supported inputs can be used for given device.
    
    Signed-off-by: Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>

---
+++ include/zephyr/dt-bindings/adc/nrf-saadc-v2.h
@@ -0,0 +1,14 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_ADC_NRF_SAADC_V2_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_ADC_NRF_SAADC_V2_H_
+
+#include <zephyr/dt-bindings/adc/nrf-saadc.h>
+
+#define NRF_SAADC_VDD 9
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_ADC_NRF_SAADC_V2_H_ */
