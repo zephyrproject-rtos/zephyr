b96e86cea6105e558d6f88dd516e186f16330836
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-10-01 13:43:13 -0500

    include: zephyr: devicetree: add devicetree helpers for clock management
    Add devicetree helpers for clock management support. These helpers
    expose access to the new devicetree macros needed for the clock management
    subsystem.
    
    Also add testcases for these helpers to the devicetree lib test
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- include/zephyr/devicetree.h
+++ include/zephyr/devicetree.h
@@ -5558,6 +5558,7 @@
 /* have these last so they have access to all previously defined macros */
 #include <zephyr/devicetree/io-channels.h>
 #include <zephyr/devicetree/clocks.h>
+#include <zephyr/devicetree/clock_management.h>
 #include <zephyr/devicetree/gpio.h>
 #include <zephyr/devicetree/spi.h>
 #include <zephyr/devicetree/dma.h>
