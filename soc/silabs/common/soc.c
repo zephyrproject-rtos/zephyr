955aca6c09ee2766e887f6a4e24a1f9d450eda6b
Aksel Skauge Mellbye <aksel.mellbye@silabs.com>
2024-10-02 22:30:28 +0200

    soc: silabs: Initialize clock manager HAL from DT
    Swap from the deprecated device_init_* functions to clock manager
    for clock tree configuration. Populate config headers using
    device tree representation of clock tree and oscillator config.
    
    Signed-off-by: Aksel Skauge Mellbye <aksel.mellbye@silabs.com>

--- soc/silabs/common/soc.c
+++ soc/silabs/common/soc.c
@@ -21,8 +21,7 @@
 
 #ifdef CONFIG_SOC_GECKO_DEV_INIT
 #include <sl_device_init_dcdc.h>
-#include <sl_device_init_dpll.h>
-#include <sl_device_init_hfxo.h>
+#include <sl_clock_manager_init.h>
 
 #ifdef CONFIG_PM
 #include <sl_hfxo_manager.h>
@@ -217,8 +216,7 @@ void soc_early_init_hook(void)
 	if (DT_HAS_COMPAT_STATUS_OKAY(silabs_series2_dcdc)) {
 		sl_device_init_dcdc();
 	}
-	sl_device_init_hfxo();
-	sl_device_init_dpll();
+	sl_clock_manager_init();
 
 #ifdef CONFIG_PM
 	sl_power_manager_init();
