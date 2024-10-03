bc84506522ba631b497dfb280f5e0c495dff2251
RAJAGOPALAN GANGADHARAN <g.raju2000@gmail.com>
2024-10-03 20:07:03 +0530

    [WIP][Draft] Usage of fdtable for std streams to facilitate file usage
    Signed-off-by: RAJAGOPALAN-GANGADHARAN <g.raju2000@gmail.com>
    
    [WIP]Fix warnings treated as errors during twister run
    
    Signed-off-by: RAJAGOPALAN-GANGADHARAN <g.raju2000@gmail.com>

--- lib/libc/minimal/source/stdout/stdout_console.c
+++ lib/libc/minimal/source/stdout/stdout_console.c
@@ -8,8 +8,28 @@
 
 #include <stdio.h>
 #include <zephyr/sys/libc-hooks.h>
+#include <zephyr/sys/fdtable.h>
 #include <zephyr/internal/syscall_handler.h>
+#include <sys/_stdvtables.h>
+#include <stdlib.h>
+
 #include <string.h>
+
+// Defined in stdio
+FILE* _stdin;
+FILE* _stdout;
+FILE* _stderr;
+
+int stdin_fd = 0;
+int stdout_fd = 1;
+int stderr_fd = 2;
+
+// Alternative to setting FIle to int if fdtable is not used. Good for memory eh?
+// #if !defined(CONFIG_FDTABLE)
+// 	static struct fd_entry stdin_simple_entry = {.obj = (void*)&stdin_fd, .vtable = NULL};
+//     static struct fd_entry stdout_simple_entry = {.obj = (void*)&stdout_fd, .vtable = NULL};
+//     static struct fd_entry stderr_simple_entry = {.obj = (void*)&stderr_fd, .vtable = NULL};
+// #endif
 
 static int _stdout_hook_default(int c)
 {
@@ -22,12 +42,65 @@ static int (*_stdout_hook)(int c) = _stdout_hook_default;
 
 void __stdout_hook_install(int (*hook)(int c))
 {
+#if defined(CONFIG_FDTABLE)
+	_stdin = zvfs_get_fd_entry(stdin_fd);
+	_stdout = zvfs_get_fd_entry(stdout_fd);
+	_stderr = zvfs_get_fd_entry(stderr_fd);
+#else
+	// fdtable not availabe fallback to primitive read and write.
+    _stdin = &stdin_fd;
+    _stdout = &stdout_fd;
+    _stderr = &stderr_fd;
+#endif
+
 	_stdout_hook = hook;
 }
 
+#if defined(CONFIG_FDTABLE)
+
+static ssize_t stdin_read_vmeth(void *obj, void *buffer, size_t count)
+{
+	return fputc('c', stdout);
+}
+
+static ssize_t stdout_write_vmeth(void *obj, const void *buffer, size_t count)
+{
+	int c = *((int *) buffer);
+	return _stdout_hook(c);
+}
+
+static ssize_t stderr_write_vmeth(void *obj, const void *buffer, size_t count)
+{
+	return 10;
+}
+
+const struct fd_op_vtable stdin_fd_op_vtable = {
+	.read = stdin_read_vmeth,
+};
+
+const struct fd_op_vtable stdout_fd_op_vtable = {
+	.write = stdout_write_vmeth,
+};
+
+const struct fd_op_vtable stderr_fd_op_vtable = {
+	.write = stderr_write_vmeth,
+};
+
+#endif
+
 int z_impl_zephyr_fputc(int c, FILE *stream)
 {
-	return ((stream == stdout) || (stream == stderr)) ? _stdout_hook(c) : EOF;
+#if defined(CONFIG_FDTABLE)
+	if(stream->vtable != NULL) {
+		return stream->vtable->write(stream->obj, &c, 1);
+	}
+#else
+	// usage of std streams without fdtable
+	if(stream != NULL && (stream == stdout || stream == stderr))
+		return _stdout_hook(c);
+#endif
+	// EOF for any other cases
+	return EOF;
 }
 
 #ifdef CONFIG_USERSPACE
