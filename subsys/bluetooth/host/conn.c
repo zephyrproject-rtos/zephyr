257c5e6affbfe74d7eea7005ba77c1d7d7aac85c
Jordan Yates <jordan@embeint.com>
2024-10-04 17:02:55 +1000

    bluetooth: conn: increase log level of timeout ignore
    Notifying users that the supplied timeout is being ignored is worthy of
    a higher log level than debug. Any such usage should be changed at the
    application level.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/bluetooth/host/conn.c
+++ subsys/bluetooth/host/conn.c
@@ -1581,7 +1581,7 @@ struct net_buf *bt_conn_create_pdu_timeout(struct net_buf_pool *pool,
 
 	if (!K_TIMEOUT_EQ(timeout, K_NO_WAIT) &&
 	    k_current_get() == k_work_queue_thread_get(&k_sys_work_q)) {
-		LOG_DBG("Timeout discarded. No blocking in syswq.");
+		LOG_WRN("Timeout discarded. No blocking in syswq.");
 		timeout = K_NO_WAIT;
 	}
 
