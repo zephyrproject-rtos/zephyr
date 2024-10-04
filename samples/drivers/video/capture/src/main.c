77630569cfedfde16bdfc07379934fac047c2e92
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-10-04 16:22:31 -0500

    samples: drivers: video: capture: don't apply format unless needed
    Do not apply format setting unless needed. Also, correct the check for
    the RGB565 format setting- the zephyr display API treats RGB565 and
    BGR565 as big endian, so the format needed here is BGR565.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- samples/drivers/video/capture/src/main.c
+++ samples/drivers/video/capture/src/main.c
@@ -36,10 +36,14 @@ static inline int display_setup(const struct device *const display_dev, const ui
 	/* Set display pixel format to match the one in use by the camera */
 	switch (pixfmt) {
 	case VIDEO_PIX_FMT_RGB565:
-		ret = display_set_pixel_format(display_dev, PIXEL_FORMAT_RGB_565);
+		if (capabilities.current_pixel_format != PIXEL_FORMAT_BGR_565) {
+			ret = display_set_pixel_format(display_dev, PIXEL_FORMAT_BGR_565);
+		}
 		break;
 	case VIDEO_PIX_FMT_XRGB32:
-		ret = display_set_pixel_format(display_dev, PIXEL_FORMAT_ARGB_8888);
+		if (capabilities.current_pixel_format != PIXEL_FORMAT_ARGB_8888) {
+			ret = display_set_pixel_format(display_dev, PIXEL_FORMAT_ARGB_8888);
+		}
 		break;
 	default:
 		return -ENOTSUP;
