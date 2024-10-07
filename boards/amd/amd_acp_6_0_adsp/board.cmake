c5c3ae03955f8136bd606dee7bc5cb9038be0edb
DINESHKUMAR <DineshKumar.Kalva@amd.com>
2024-10-07 16:27:18 +0530

    Add myself and dineshkumar and my co as codeowners for ACP_6_0 related files for SOF with Zephyr OS.
    Signed-off-by: DINESHKUMAR <DineshKumar.Kalva@amd.com

--- boards/amd/amd_acp_6_0_adsp/board.cmake
+++ boards/amd/amd_acp_6_0_adsp/board.cmake
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
-if(CONFIG_SOC_ACP_6_0)
     board_set_flasher_ifnset(misc-flasher)
     board_finalize_runner_args(misc-flasher)
-    
-    board_set_rimage_target(acp_6_0)
-endif()
+
+    board_set_rimage_target(rmb)
