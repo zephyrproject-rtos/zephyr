57a6aa3cf64c75eb054f700ca380b2bf32f73152
Francois Ramu <francois.ramu@st.com>
2024-10-07 17:30:28 +0200

    samples: boards: stm32 power_mgt stdby and shutdown
    Polarity low is wrong : wakeup pin 2 is active low
    so the wakeup must be detected on the rising edge
    This is the default wakeup pin2 config.
    Apply the recommended sequence to enter low power standby
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- samples/boards/st/power_mgmt/standby_shutdown/src/main.c
+++ samples/boards/st/power_mgmt/standby_shutdown/src/main.c
@@ -42,16 +42,19 @@ static struct gpio_callback button_cb_data;
 
 void config_wakeup_features(void)
 {
-	/* Configure wake-up features */
-	/* WKUP2(PC13) only , - active low, pull-up */
-	/* Set pull-ups for standby modes */
-	LL_PWR_EnableGPIOPullUp(LL_PWR_GPIO_C, LL_PWR_GPIO_BIT_13);
-	LL_PWR_IsWakeUpPinPolarityLow(LL_PWR_WAKEUP_PIN2);
-	/* Enable pin pull up configurations and wakeup pins */
-	LL_PWR_EnablePUPDCfg();
+	/*
+	 * Configure wake-up features : WKUP2(PC13) only
+	 *
+	 * The Following Wakeup sequence is highly recommended prior to each Standby mode entry
+	 * mainly when using more than one wakeup source this is to not miss any wakeup event:
+	 * - Disable all used wakeup sources,
+	 * - Clear all related wakeup flags,
+	 * - Re-enable all used wakeup sources,
+	 * - Enter the Standby mode.
+	 */
+	LL_PWR_DisableWakeUpPin(LL_PWR_WAKEUP_PIN2);
+	LL_PWR_ClearFlag_WU(); /* Clear wakeup flags */
 	LL_PWR_EnableWakeUpPin(LL_PWR_WAKEUP_PIN2);
-	/* Clear wakeup flags */
-	LL_PWR_ClearFlag_WU();
 }
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb,
