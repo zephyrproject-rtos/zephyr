29e82d2f23b099ead21a045a869818e24552de87
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2024-10-07 21:41:25 +0200

    cmake: add linker_libraries.cmake for Cadence Xtensa linker
    Follow-up: #78320
    
    Create linker_libraries.cmake for the Cadence Xtensa xt-ld linker to
    ensure correct linking of runtime and C libraries as well as correct
    link order.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

---
+++ cmake/linker/xt-ld/linker_libraries.cmake
@@ -0,0 +1,8 @@
+# Copyright (c) 2024 Nordic Semiconductor
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set_linker_property(NO_CREATE PROPERTY c_library    "-lc")
+set_linker_property(NO_CREATE PROPERTY rt_library   "-lgcc")
+set_linker_property(NO_CREATE PROPERTY c++_library  "-lstdc++")
+set_linker_property(PROPERTY link_order_library "c;rt")
