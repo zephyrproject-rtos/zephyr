7fc6307b543a7bbde0386b788e1881eff81f78f6
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2024-10-08 14:42:50 +0300

    samples: net: echo-server: Add USB CDC NCM support
    Add support for USB cdc_ncm to echo-server application.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- samples/net/sockets/echo_server/src/usb.c
+++ samples/net/sockets/echo_server/src/usb.c
@@ -10,8 +10,32 @@ LOG_MODULE_DECLARE(net_echo_server_sample, LOG_LEVEL_DBG);
 #include <zephyr/usb/usb_device.h>
 #include <zephyr/net/net_config.h>
 
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
+#include <sample_usbd.h>
+
+static struct usbd_context *sample_usbd;
+
+static int enable_usb_device_next(void)
+{
+	int err;
+
+	sample_usbd = sample_usbd_init_device(NULL);
+	if (sample_usbd == NULL) {
+		return -ENODEV;
+	}
+
+	err = usbd_enable(sample_usbd);
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+#endif /* CONFIG_USB_DEVICE_STACK_NEXT */
+
 int init_usb(void)
 {
+#if defined(CONFIG_USB_DEVICE_STACK)
 	int ret;
 
 	ret = usb_enable(NULL);
@@ -19,6 +43,13 @@ int init_usb(void)
 		LOG_ERR("Cannot enable USB (%d)", ret);
 		return ret;
 	}
+#endif /* CONFIG_USB_DEVICE_STACK */
+
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
+	if (enable_usb_device_next()) {
+		return 0;
+	}
+#endif /* CONFIG_USB_DEVICE_STACK_NEXT */
 
 	(void)net_config_init_app(NULL, "Initializing network");
 
