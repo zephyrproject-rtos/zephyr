f7e8a8717b1e64b7a295d4c0563e22563d29be0e
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2024-10-08 07:48:19 +0200

    tests: bsim: bluetooth: host: att: Add ATT timeout test
    This commit tests that the host correctly disconnects from peer when ATT
    timeout happens.
    
    Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

---
+++ tests/bsim/bluetooth/host/att/timeout/compile.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Copyright 2024 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+: "${ZEPHYR_BASE:?ZEPHYR_BASE must be defined}"
+
+INCR_BUILD=1
+
+source ${ZEPHYR_BASE}/tests/bsim/compile.source
+
+app="$(guess_test_relpath)" compile
+
+wait_for_background_jobs
