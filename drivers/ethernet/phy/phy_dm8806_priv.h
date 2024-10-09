37c76e4d083751f67d44d90ac7e205d73b4c57aa
Robert Slawinski <robert.slawinski1@gmail.com>
2024-10-09 12:29:57 +0200

    drivers: dm8806: link speed change interrupt handling
    On the interrupt handling, one thread per driver instance is involved
    into monitoring the semaphor, sends inside the gpio callback triggered
    by the gpio interrupt. Each time, when the link parameters are change,
    the DM8806 is generating the gpio interrupt. After getting semaphor,
    the application callback function which was linked during initialization
    process is called to get the new link parameters with standard API
    calls
    
    Signed-off-by: Robert Slawinski <robert.slawinski1@gmail.com>

--- drivers/ethernet/phy/phy_dm8806_priv.h
+++ drivers/ethernet/phy/phy_dm8806_priv.h
@@ -40,8 +40,81 @@
 /* Link status mask. */
 #define LINK_STATUS_MASK          0x1u
 
+/* Switch Engine Registers */
+/* Address Table Control And Status Register PHY Address */
+#define ADDR_TAB_CTRL_STAT_PHY_ADDR 0x15u
+/* Address Table Control And Status Register Register SAddress */
+#define ADDR_TAB_CTRL_STAT_REG_ADDR 0x10u
+
+/* Address Table Access bussy flag offset */
+#define ATB_S_OFFSET  0xf
+/* Address Table Command Result flag offset */
+#define ATB_CR_OFFSET 0xd
+/* Address Table Command Result flag mask */
+#define ATB_CR_MASK   0x3
+
+/* Unicast Address Table Index*/
+#define UNICAST_ADDR_TAB   (1 << 0 | 1 << 1)
+/* Multicast Address Table Index*/
+#define MULTICAST_ADDR_TAB (1 << 0)
+/* IGMP Table Index*/
+#define IGMP_ADDR_TAB      (1 << 1)
+
+/* Read a entry with sequence number of address table */
+#define ATB_CMD_READ   (1 << 2 | 1 << 3 | 1 << 4)
+/* Write a entry with MAC address */
+#define ATB_CMD_WRITE  (1 << 2)
+/* Delete a entry with MAC address */
+#define ATB_CMD_DELETE (1 << 3)
+/* Search a entry with MAC address */
+#define ATB_CMD_SEARCH (1 << 2 | 1 << 3)
+/* Clear one or more than one entries with Port or FID */
+#define ATB_CMD_CLEAR  (1 << 4)
+
+/* Address Table Data 0 PHY Address */
+#define ADDR_TAB_DATA0_PHY_ADDR 0x15u
+/* Address Table Data 0 Register Address */
+#define ADDR_TAB_DATA0_REG_ADDR 0x11u
+/* Port number or port map mask*/
+#define ATB_PORT_MASK           0x1f
+
+/* Address Table Data 1 PHY Address */
+#define ADDR_TAB_DATA1_PHY_ADDR 0x15u
+/* Address Table Data 1 Register Address */
+#define ADDR_TAB_DATA1_REG_ADDR 0x12u
+
+/* Address Table Data 2 PHY Address */
+#define ADDR_TAB_DATA2_PHY_ADDR 0x15u
+/* Address Table Data 2 Register Address */
+#define ADDR_TAB_DATA2_REG_ADDR 0x13u
+
+/* Address Table Data 3 PHY Address */
+#define ADDR_TAB_DATA3_PHY_ADDR 0x15u
+/* Address Table Data 3 Register Address */
+#define ADDR_TAB_DATA3_REG_ADDR 0x14u
+
+/* Address Table Data 4 PHY Address */
+#define ADDR_TAB_DATA4_PHY_ADDR 0x15u
+/* Address Table Data 4 Register Address */
+#define ADDR_TAB_DATA4_REG_ADDR 0x15u
+
+/* WoL Control Register PHY Address */
+#define WOLL_CTRL_REG_PHY_ADDR 0x15u
+/* WoL Control Register Register Address */
+#define WOLL_CTRL_REG_REG_ADDR 0x1bu
+
 /* PHY address 0x18h */
 #define PHY_ADDRESS_18H 0x18u
+
+/* Interrupt Status Register PHY Address. */
+#define INT_STAT_PHY_ADDR 0x18u
+/* Interrupt Status Register Register Address. */
+#define INT_STAT_REG_ADDR 0x18u
+
+/* Interrupt Mask & Control Register PHY Address. */
+#define INT_MASK_CTRL_PHY_ADDR 0x18u
+/* Interrupt Mask & Control Register Register Address. */
+#define INT_MASK_CTRL_REG_ADDR 0x19u
 
 #define PORT5_MAC_CONTROL     0x15u
 /* Port 5 Force Speed control bit */
