b48a97d9cf8d94f63852facd09d30c83b72f4a2a
Fengming Ye <frank.ye@nxp.com>
2024-10-09 18:13:41 +0900

    [nxp noup] remove hapd_state
    Remove hapd_state as it is rejected.
    
    Signed-off-by: Fengming Ye <frank.ye@nxp.com>

--- modules/hostap/src/supp_api.h
+++ modules/hostap/src/supp_api.h
@@ -291,15 +291,6 @@ int supplicant_wps_config(const struct device *dev, struct wifi_wps_config_param
 #ifdef CONFIG_AP
 #ifdef CONFIG_WIFI_NM_HOSTAPD_AP
 /**
- * @brief Get Wi-Fi AP Status
- *
- * @param dev Wi-Fi device
- * @param params AP status
- * @return 0 for OK; -1 for ERROR
- */
-int hapd_state(const struct device *dev, int *state);
-
-/**
  * @brief Wi-Fi AP configuration parameter.
  *
  * @param dev Wi-Fi device
@@ -307,11 +298,6 @@ int hapd_state(const struct device *dev, int *state);
  * @return 0 for OK; -1 for ERROR
  */
 int supplicant_ap_config_params(const struct device *dev, struct wifi_ap_config_params *params);
-#else
-static inline int hapd_state(const struct device *dev, int *state)
-{
-	return -EINVAL;
-}
 #endif
 
 #ifdef CONFIG_WIFI_NM_HOSTAPD_WPS
