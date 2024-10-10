18f508144e4ffc50c13f02866ba4bf47335d35ea
Adam Cavender <adam.cavender@nordicsemi.no>
2024-10-10 11:10:25 +0100

    bluetooth: host: CS support for various HCI commands
    Adds HCI support for:
    - LE CS Security Enable
    - LE CS Procedure Enable
    - LE CS Set Procedure Parameters
    - LE CS Set Channel Classification
    - LE CS Read Local Supported Capabilities
    - LE CS Write Cached Remote Supported Capabilities
    - LE CS Write Cached Remote FAE Table
    
    Signed-off-by: Adam Cavender <adam.cavender@nordicsemi.no>

--- subsys/bluetooth/host/hci_core.h
+++ subsys/bluetooth/host/hci_core.h
@@ -542,6 +542,8 @@ void bt_hci_le_past_received_v2(struct net_buf *buf);
 void bt_hci_le_cs_read_remote_supported_capabilities_complete(struct net_buf *buf);
 void bt_hci_le_cs_read_remote_fae_table_complete(struct net_buf *buf);
 void bt_hci_le_cs_config_complete_event(struct net_buf *buf);
+void bt_hci_le_cs_security_enable_complete(struct net_buf *buf);
+void bt_hci_le_cs_procedure_enable_complete(struct net_buf *buf);
 void bt_hci_le_cs_subevent_result(struct net_buf *buf);
 void bt_hci_le_cs_subevent_result_continue(struct net_buf *buf);
 void bt_hci_le_cs_test_end_complete(struct net_buf *buf);
