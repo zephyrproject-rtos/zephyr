197163ef79e43443788ea10fadb94f2878dbd95a
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-10-10 11:04:34 -0500

    Revert "[nxp fromtree] hostap: add dispatch for hostapd and wpa_supplicant wpa_msg"
    This reverts commit 299a70a9a206d13f450afe2323278b6d04c8296d.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- modules/hostap/src/supp_main.h
+++ modules/hostap/src/supp_main.h
@@ -23,9 +23,6 @@ struct wpa_global *zephyr_get_default_supplicant_context(void);
 struct wpa_supplicant *zephyr_get_handle_by_ifname(const char *ifname);
 #ifdef CONFIG_WIFI_NM_HOSTAPD_AP
 struct hostapd_iface *zephyr_get_hapd_handle_by_ifname(const char *ifname);
-void wpa_supplicant_msg_send(void *ctx, int level, enum wpa_msg_type type, const char *txt,
-			     size_t len);
-void hostapd_msg_send(void *ctx, int level, enum wpa_msg_type type, const char *buf, size_t len);
 #endif
 struct wpa_supplicant_event_msg {
 #ifdef CONFIG_WIFI_NM_HOSTAPD_AP
