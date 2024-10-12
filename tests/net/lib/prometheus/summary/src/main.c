0f1ccd3185e0b9dc9f301fef1effca8cef570ea9
Mustafa Abdullah Kus <mustafa.kus@sparsetechnology.com>
2024-10-12 23:17:07 +0300

    test: net: lib: add prometheus library tests
    Tests for prometheus library support.
    It contains integration and unit test
    each metric types.
    
    Signed-off-by: Mustafa Abdullah Kus <mustafa.kus@sparsetechnology.com>

---
+++ tests/net/lib/prometheus/summary/src/main.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2024 Mustafa Abdullah Kus, Sparse Technology
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/ztest.h>
+
+#include <zephyr/net/prometheus/summary.h>
+
+struct prometheus_metric test_summary_metric = {
+	.type = PROMETHEUS_SUMMARY,
+	.name = "test_summary",
+	.description = "Test summary",
+	.num_labels = 1,
+	.labels = {{
+		.key = "test",
+		.value = "summary",
+	}},
+};
+
+PROMETHEUS_SUMMARY_DEFINE(test_summary_m, &test_summary_metric);
+
+/**
+ * @brief Test prometheus_summary_observe
+ *
+ * @details The test shall observe the histogram value by 1 and check if the
+ * value is incremented correctly.
+ *
+ * @details The test shall observe the histogram value by 2 and check if the
+ * value is incremented correctly.
+ */
+ZTEST(test_summary, test_summary_observe)
+{
+	int ret;
+
+	zassert_equal(test_summary_m.sum, 0, "Histogram value is not 0");
+
+	ret = prometheus_summary_observe(&test_summary_m, 1);
+	zassert_ok(ret, "Error observing histogram");
+
+	zassert_equal(test_summary_m.sum, 1, "Histogram value is not 1");
+
+	ret = prometheus_summary_observe(&test_summary_m, 2);
+	zassert_ok(ret, "Error observing histogram");
+
+	zassert_equal(test_summary_m.sum, 3, "Histogram value is not 3");
+}
+
+ZTEST_SUITE(test_summary, NULL, NULL, NULL, NULL, NULL);
