eb9eff70185d0c3d1fd0901f539530a21782bbe5
DineshKumar Kalva <DineshKumar.Kalva@amd.com>
2024-10-14 15:03:27 +0530

    west: sign: add support for AMD acp_6_0_adsp board.
    Add support for signing acp_6_0 SOF with Zephyr images with rimage.
    
    Signed-off-by: DineshKumar Kalva <DineshKumar.Kalva@amd.com>

--- scripts/west_commands/sign.py
+++ scripts/west_commands/sign.py
@@ -470,7 +470,7 @@ class RimageSigner(Signer):
         kernel_name = build_conf.get('CONFIG_KERNEL_BIN_NAME', 'zephyr')
 
         # TODO: make this a new sign.py --bootloader option.
-        if target in ('imx8', 'imx8m', 'imx8ulp', 'imx95'):
+        if target in ('imx8', 'imx8m', 'imx8ulp', 'imx95', 'rmb'):
             bootloader = None
             kernel = str(b / 'zephyr' / f'{kernel_name}.elf')
             out_bin = str(b / 'zephyr' / f'{kernel_name}.ri')
