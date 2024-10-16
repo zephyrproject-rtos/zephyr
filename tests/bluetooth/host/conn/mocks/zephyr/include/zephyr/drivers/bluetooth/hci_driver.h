9072c16869121748a259cf3c5339a8734fa8efda
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2024-10-16 14:03:59 +0200

    tests: bluetooth: host: conn: Add bt_conn_le_create unit test
    Add a test that checks behavior of
    CONFIG_BT_CONN_CHECK_NULL_BEFORE_CREATE Kconfig option.
    
    Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

---
+++ tests/bluetooth/host/conn/mocks/zephyr/include/zephyr/drivers/bluetooth/hci_driver.h
@@ -0,0 +1,11 @@
+/* Stub for Bluetooth HCI driver API used to hide real hci_driver.h. */
+
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DRIVERS_BLUETOOTH_HCI_DRIVER_H_
+#define ZEPHYR_INCLUDE_DRIVERS_BLUETOOTH_HCI_DRIVER_H_
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_BLUETOOTH_HCI_DRIVER_H_ */
