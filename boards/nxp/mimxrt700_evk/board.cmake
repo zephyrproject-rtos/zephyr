adbcb1ef25174ea6c4f9abb7fca9d6ffd8dbeb69
Iuliana Prodan <iuliana.prodan@nxp.com>
2024-10-17 02:10:23 +0300

    boards: nxp: imxrt700: Add i.MXRT700 HiFi1 board support
    Add board support for the HiFi1 DSP core from NXP i.MXRT700.
    
    Signed-off-by: Iuliana Prodan <iuliana.prodan@nxp.com>

--- boards/nxp/mimxrt700_evk/board.cmake
+++ boards/nxp/mimxrt700_evk/board.cmake
@@ -7,6 +7,9 @@
 if(CONFIG_SOC_MIMXRT798S_HIFI4)
   board_runner_args(jlink "--device=MIMXRT798S_hifi4" "--reset-after-load")
   board_runner_args(linkserver  "--device=MIMXRT798S:EVK-MIMXRT798")
+elseif(CONFIG_SOC_MIMXRT798S_HIFI1)
+  board_runner_args(jlink "--device=MIMXRT798S_hifi1" "--reset-after-load")
+  board_runner_args(linkserver  "--device=MIMXRT798S:EVK-MIMXRT798")
 endif()
 
 include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
