438308507b22576089348f3589bec9e02a02836b
Alexander pinheiro paschoaletto <1222703@isep.ipp.pt>
2024-10-18 13:24:56 +0100

    samples: edf: adding EDF sample
    adding a simple EDF sample.
    
    Signed-off-by: Alexander pinheiro paschoaletto <1222703@isep.ipp.pt>

--- samples/edf/src/lib/helper.h
+++ samples/edf/src/lib/helper.h
@@ -18,12 +18,7 @@
 #define INACTIVE           -1
 #define MSEC_TO_CYC(msec)  k_ms_to_cyc_near32(msec)
 #define MSEC_TO_USEC(msec) (msec * USEC_PER_MSEC)
-
-#ifdef CONFIG_TIMER_HAS_64BIT_CYCLE_COUNTER
-#define clock() k_cycle_get_64()
-#else
-#define clock() k_cycle_get_32()
-#endif
+#define CYC_TO_MSEC(cyc)   k_cyc_to_ms_floor32(cyc)
 
 typedef struct {
 	int id;
@@ -35,5 +30,16 @@ typedef struct {
 	struct k_msgq queue;
 	struct k_timer timer;
 } edf_t;
+
+void cycle(int id, uint32_t wcet, int32_t deadline)
+{
+	printf("[%d-%d-", id, CYC_TO_MSEC(deadline));
+	k_busy_wait(wcet / 5);
+	for (int i = 0; i < 4; i++) {
+		printf("%d-", id);
+		k_busy_wait(wcet / 5);
+	}
+	printf("%d]-", id);
+}
 
 #endif
