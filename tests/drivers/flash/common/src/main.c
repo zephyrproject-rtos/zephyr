8738e0a0228efa4f91e53c4b94f4411eaf9f5a3c
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2024-10-18 10:16:08 +0200

    samples|tests: drivers: flash: Add support for "jedec,mspi-nor" devices
    Extend several flash samples and tests so that they can also be used
    with "jedec,mspi-nor" devices.
    Add configurations needed for the nrf54h20dk/nrf54h20/cpuapp target.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- tests/drivers/flash/common/src/main.c
+++ tests/drivers/flash/common/src/main.c
@@ -14,6 +14,8 @@
 #define TEST_AREA_DEV_NODE	DT_INST(0, nordic_qspi_nor)
 #elif defined(CONFIG_SPI_NOR)
 #define TEST_AREA_DEV_NODE	DT_INST(0, jedec_spi_nor)
+#elif defined(CONFIG_FLASH_MSPI_NOR)
+#define TEST_AREA_DEV_NODE	DT_INST(0, jedec_mspi_nor)
 #else
 #define TEST_AREA	storage_partition
 #endif
