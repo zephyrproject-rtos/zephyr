9643ca20e9bc8df28c02aac923c3a794bbef6f5f
Carles Cufi <carles.cufi@nordicsemi.no>
2024-10-21 17:13:13 +0200

    nordic: Remove the nRF54H20 Engineering B
    The production version of the nRF54H20 SoC is now available, so remove
    the initial Engineering B (EngB) preview version.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- modules/hal_nordic/nrfx/nrfx_config.h
+++ modules/hal_nordic/nrfx/nrfx_config.h
@@ -1048,13 +1048,13 @@
     #include <nrfx_config_nrf5340_application.h>
 #elif defined(NRF5340_XXAA_NETWORK)
     #include <nrfx_config_nrf5340_network.h>
-#elif (defined(NRF54H20_XXAA) || defined(NRF54H20_ENGB_XXAA)) && defined(NRF_APPLICATION)
+#elif defined(NRF54H20_XXAA) && defined(NRF_APPLICATION)
     #include <nrfx_config_nrf54h20_application.h>
-#elif (defined(NRF54H20_XXAA) || defined(NRF54H20_ENGB_XXAA)) && defined(NRF_RADIOCORE)
+#elif defined(NRF54H20_XXAA) && defined(NRF_RADIOCORE)
     #include <nrfx_config_nrf54h20_radiocore.h>
-#elif (defined(NRF54H20_XXAA) || defined(NRF54H20_ENGB_XXAA)) && defined(NRF_PPR)
+#elif defined(NRF54H20_XXAA) && defined(NRF_PPR)
     #include <nrfx_config_nrf54h20_ppr.h>
-#elif (defined(NRF54H20_XXAA) || defined(NRF54H20_ENGB_XXAA)) && defined(NRF_FLPR)
+#elif defined(NRF54H20_XXAA) && defined(NRF_FLPR)
     #include <nrfx_config_nrf54h20_flpr.h>
 #elif defined(NRF54L15_XXAA) && defined(NRF_APPLICATION)
     #include <nrfx_config_nrf54l15_application.h>
