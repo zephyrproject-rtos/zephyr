c46cb3250c538142af302c8821818436baa787b0
Emil Gydesen <emil.gydesen@nordicsemi.no>
2024-10-22 13:07:53 +0200

    Bluetooth: CCP: Client: Add support for get bearer uci
    Add support for getting the remote bearer UCI.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bluetooth/audio/ccp_call_control_client/uut/tbs_client.c
+++ tests/bluetooth/audio/ccp_call_control_client/uut/tbs_client.c
@@ -7,6 +7,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <zephyr/autoconf.h>
 #include <zephyr/bluetooth/audio/tbs.h>
 #include <zephyr/bluetooth/conn.h>
 #include <zephyr/sys/util_macro.h>
@@ -34,6 +35,7 @@ int bt_tbs_client_discover(struct bt_conn *conn)
 	return 0;
 }
 
+#if defined(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)
 int bt_tbs_client_read_bearer_provider_name(struct bt_conn *conn, uint8_t inst_index)
 {
 	if (conn == NULL) {
@@ -46,3 +48,19 @@ int bt_tbs_client_read_bearer_provider_name(struct bt_conn *conn, uint8_t inst_i
 
 	return 0;
 }
+#endif /* CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME */
+
+#if defined(CONFIG_BT_TBS_CLIENT_BEARER_UCI)
+int bt_tbs_client_read_bearer_uci(struct bt_conn *conn, uint8_t inst_index)
+{
+	if (conn == NULL) {
+		return -ENOTCONN;
+	}
+
+	if (tbs_cbs != NULL && tbs_cbs->bearer_provider_name != NULL) {
+		tbs_cbs->bearer_uci(conn, 0, inst_index, "bearer UCI");
+	}
+
+	return 0;
+}
+#endif /* CONFIG_BT_TBS_CLIENT_BEARER_UCI */
