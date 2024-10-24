zephyrproject-rtos/zephyr

commit f987e8c6f0a49b04a1184b1a36612612482e3d24

runners: jlink: Make port selectable for J-Link IP
If multiple J-Links with IP support are used,
they can be selected with different ports.
The actual implementation is just using the default port.
Make the port selectable with <ip>:<port>.

Signed-off-by: Michael Arnold <marnold@baumer.com>

scripts/west_commands/runners/jlink.py
@@ -28,7 +28,7 @@ DEFAULT_JLINK_GDB_PORT = 2331
 
 def is_ip(ip):
     try:
-        ipaddress.ip_address(ip)
+        ipaddress.ip_address(ip.split(':')[0])
     except ValueError:
         return False
     return True

commit 3aaf317f1100b0d9d8c64413e729d186aad851d3

scripts: west_commands: runners: Fix jlink is_ip
Commit f987e8c6f0a49b04a1184b1a36612612482e3d24 introduced a regression
where the is_ip check fails if no --id is passed as an argument.

Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

scripts/west_commands/runners/jlink.py
@@ -30,6 +30,8 @@ DEFAULT_JLINK_GDB_PORT = 2331
 DEFAULT_JLINK_RTT_PORT = 19021
 
 def is_ip(ip):
+    if not ip:
+        return False
     try:
         ipaddress.ip_address(ip.split(':')[0])
     except ValueError: