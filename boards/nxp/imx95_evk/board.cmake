7814cecc2cc51dbcc490dc5af4fa6ff304e2cfc5
Yangbo Lu <yangbo.lu@nxp.com>
2024-10-25 09:04:40 +0200

    boards: nxp: imx95_evk: support generating/flashing bootable firmware
    Supported NXP Secure Provisioning SDK (SPSDK) generating
    and flashing bootable firmware for imx95_evk_mimx9596_m7/a55.
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- boards/nxp/imx95_evk/board.cmake
+++ boards/nxp/imx95_evk/board.cmake
@@ -3,3 +3,19 @@
 if (CONFIG_SOF AND CONFIG_BOARD_IMX95_EVK_MIMX9596_M7_DDR)
   board_set_rimage_target(imx95)
 endif()
+
+if(CONFIG_BOARD_NXP_SPSDK_IMAGE OR (DEFINED ENV{USE_NXP_SPSDK_IMAGE}
+  AND "$ENV{USE_NXP_SPSDK_IMAGE}" STREQUAL "y"))
+  board_set_flasher_ifnset(spsdk)
+
+  board_runner_args(spsdk "--family=mimx9596")
+  board_runner_args(spsdk "--bootloader=${CMAKE_BINARY_DIR}/zephyr/imx95-19x19-lpddr5-evk-boot-firmware-0.1/imx-boot-imx95-19x19-lpddr5-evk-sd.bin-flash_all")
+  board_runner_args(spsdk "--flashbin=${CMAKE_BINARY_DIR}/zephyr/flash.bin")
+  if(CONFIG_CPU_CORTEX_A55)
+    board_runner_args(spsdk "--containers=two")
+  else()
+    board_runner_args(spsdk "--containers=one")
+  endif()
+
+  include(${ZEPHYR_BASE}/boards/common/spsdk.board.cmake)
+endif()
