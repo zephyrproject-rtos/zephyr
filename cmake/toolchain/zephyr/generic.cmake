4706c356635edc82dcdd96f9b3aec5318fcc085f
Stephanos Ioannidis <root@stephanos.io>
2024-10-26 07:56:24 +0900

    cmake: toolchain: Introduce TOOLCHAIN_HAS_GLIBCXX
    This commit introduces `TOOLCHAIN_HAS_GLIBCXX` CMake variable, which is
    set to `y` when GNU C++ Standard Library aka. libstdc++ is available.
    
    This helps filter libstdc++-specific Kconfig and tests in a more refined
    manner.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- cmake/toolchain/zephyr/generic.cmake
+++ cmake/toolchain/zephyr/generic.cmake
@@ -6,4 +6,7 @@ include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/generic.cmake)
 
 set(TOOLCHAIN_KCONFIG_DIR ${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr)
 
+# Zephyr SDK < 0.17.1 does not set TOOLCHAIN_HAS_GLIBCXX
+set(TOOLCHAIN_HAS_GLIBCXX ON CACHE BOOL "True if toolchain supports libstdc++")
+
 message(STATUS "Found toolchain: zephyr-sdk-gnu ${SDK_VERSION} (${ZEPHYR_SDK_INSTALL_DIR})")
