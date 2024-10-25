309895646d61e21d4f987ca13d3a960bef39659e
Stephanos Ioannidis <root@stephanos.io>
2024-10-26 07:56:24 +0900

    cmake: toolchain: Introduce TOOLCHAIN_HAS_GLIBCXX
    This commit introduces `TOOLCHAIN_HAS_GLIBCXX` CMake variable, which is
    set to `y` when GNU C++ Standard Library aka. libstdc++ is available.
    
    This helps filter libstdc++-specific Kconfig and tests in a more refined
    manner.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- cmake/toolchain/host/generic.cmake
+++ cmake/toolchain/host/generic.cmake
@@ -5,5 +5,6 @@ set(LINKER ld)
 set(BINTOOLS host-gnu)
 
 set(TOOLCHAIN_HAS_NEWLIB OFF CACHE BOOL "True if toolchain supports newlib")
+set(TOOLCHAIN_HAS_GLIBCXX ON CACHE BOOL "True if toolchain supports libstdc++")
 
 message(STATUS "Found toolchain: host (gcc/ld)")
