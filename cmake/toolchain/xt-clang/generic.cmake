184fef5b5a1e2fd21b2dca048e00a84081cc36b6
Stephanos Ioannidis <root@stephanos.io>
2024-10-26 08:37:18 +0900

    cmake: toolchain: Introduce TOOLCHAIN_HAS_LIBCXX
    This commit introduces `TOOLCHAIN_HAS_LIBCXX` CMake variable, which is set
    to `y` when LLVM C++ Standard Library aka. libc++ is available.
    
    This helps filter libc++-specific Kconfig and tests in a more refined
    manner.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- cmake/toolchain/xt-clang/generic.cmake
+++ cmake/toolchain/xt-clang/generic.cmake
@@ -7,4 +7,6 @@ set(CC clang)
 set(C++ clang++)
 set(LINKER xt-ld)
 
+set(TOOLCHAIN_HAS_LIBCXX ON CACHE BOOL "True if toolchain supports libc++")
+
 message(STATUS "Found toolchain: xt-clang (${XTENSA_TOOLCHAIN_PATH})")
