3e7b1aaf15a3c65df4c05087de3a412bfb3bf6ae
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2024-10-25 10:27:41 +0200

    samples: Bluetooth: broadcaster_multiple: Refactor for reuse
    Refactor the sample so that multiple advertising setup can
    be reused in other sample if required. Bluetooth enable can
    be in the sample that reuses the advertising setup.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- samples/bluetooth/broadcaster_multiple/src/main.c
+++ samples/bluetooth/broadcaster_multiple/src/main.c
@@ -5,12 +5,22 @@
  */
 
 #include <zephyr/sys/printk.h>
+#include <zephyr/bluetooth/bluetooth.h>
 
 int broadcaster_multiple(void);
 
 int main(void)
 {
+	int err;
+
 	printk("Starting Multiple Broadcaster Demo\n");
+
+	/* Initialize the Bluetooth Subsystem */
+	err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return 0;
+	}
 
 	(void)broadcaster_multiple();
 
