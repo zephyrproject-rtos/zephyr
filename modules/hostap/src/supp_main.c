96777b01f903cf3be5c72be8ebf878bd47544a26
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-10-28 09:39:00 -0500

    Revert "[nxp toup] samples: net: wifi: add sap crypto enterprise for zephyr"
    This reverts commit b871e208a4096f27a31e40e8d3fbfcdd9926c66b.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- modules/hostap/src/supp_main.c
+++ modules/hostap/src/supp_main.c
@@ -113,9 +113,6 @@ static const struct wifi_mgmt_ops mgmt_ap_ops = {
 	.dpp_dispatch = hapd_dpp_dispatch,
 #endif /* CONFIG_WIFI_NM_WPA_SUPPLICANT_DPP */
 	.ap_config_params = supplicant_ap_config_params,
-#if defined CONFIG_WIFI_NM_HOSTAPD_CRYPTO_ENTERPRISE
-	.enterprise_creds = supplicant_add_enterprise_creds,
-#endif
 };
 
 DEFINE_WIFI_NM_INSTANCE(hostapd, &mgmt_ap_ops);
