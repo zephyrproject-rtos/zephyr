0fe04db36d9aa964de835aff8e8b07d181c47dcb
Zihao Gao <gaozihao@xiaomi.com>
2024-10-30 10:45:02 +0800

    Bluetooth: AVRCP: Add SDP attributes.
    This patch add SDP records for both CT and TG role.
    
    The SDP attribute would be registered according to the configuration.
    
    OBEX and Browsing commands are optional and yet not supported.
    
    SDP registration is implemented at AVRCP level to simplify the
    workload of the upper layer. We assume the App can have limited
    knowledge on SDP structures.
    
    Signed-off-by: Zihao Gao <gaozihao@xiaomi.com>

--- subsys/bluetooth/host/classic/avrcp_internal.h
+++ subsys/bluetooth/host/classic/avrcp_internal.h
@@ -9,4 +9,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#define AVCTP_VER_1_4 (0x0104u)
+#define AVRCP_VER_1_6 (0x0106u)
+
+#define AVRCP_CAT_1 BIT(0) /* Player/Recorder */
+#define AVRCP_CAT_2 BIT(1) /* Monitor/Amplifier */
+#define AVRCP_CAT_3 BIT(2) /* Tuner */
+#define AVRCP_CAT_4 BIT(3) /* Menu */
+
 int bt_avrcp_init(void);
