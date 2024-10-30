cb73824b94a5d013668d4b09269c4aba426596ca
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2024-10-30 11:56:14 +0100

    cmake: set minimal C++ linker properties in minimal C++ CMake impl
    The property based toolchain integration allows a cleaner design by
    letting the toolchain define its properties and values and let CMake
    implementation of Zephyr provided C and C++ libraries adjust those
    properties when minimal C or C++ libraries are used.
    
    This commit moves handling of C++ linker library properties into
    the minimal C++ CMake implementation.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- cmake/linker/lld/linker_libraries.cmake
+++ cmake/linker/lld/linker_libraries.cmake
@@ -8,7 +8,6 @@ set_linker_property(NO_CREATE TARGET linker PROPERTY rt_library "")
 set_linker_property(TARGET linker PROPERTY c++_library "-lc++;-lc++abi")
 
 if(CONFIG_CPP
-   AND NOT CONFIG_MINIMAL_LIBCPP
    AND NOT CONFIG_NATIVE_LIBRARY
    # When new link principle is fully introduced, then the below condition can
    # be removed, and instead the external module c++ should use:
