2dabb149652dd8bba90e0a082267fffbebb4b14a
Gopalkrishna N Biradar <gopalnbiradar7@gmail.com>
2024-10-30 17:40:21 +0530

    fix: Skip mkdir for FatFS root in settings API
    Signed-off-by: Gopalkrishna N Biradar <gopalnbiradar7@gmail.com>

--- subsys/settings/src/settings_file.c
+++ subsys/settings/src/settings_file.c
@@ -532,24 +532,30 @@ static int mkdir_if_not_exists(const char *path)
 
 static int mkdir_for_file(const char *file_path)
 {
-	char dir_path[SETTINGS_FILE_NAME_MAX];
-	int err;
+    char dir_path[SETTINGS_FILE_NAME_MAX];
+    int err;
 
-	for (size_t i = 0; file_path[i] != '\0'; i++) {
-		if (i > 0 && file_path[i] == '/') {
-			dir_path[i] = '\0';
+    for (size_t i = 0; file_path[i] != '\0'; i++) {
+        if (i > 0 && file_path[i] == '/') {
+            dir_path[i] = '\0';
 
-			err = mkdir_if_not_exists(dir_path);
-			if (err) {
-				return err;
-			}
-		}
+            // Skip mkdir for FatFS root directories (ending with ':')
+            if (strrchr(dir_path, ':') == &dir_path[strlen(dir_path) - 1]) {
+                LOG_DBG("FatFS root directory detected, skipping mkdir for path: %s",
+                        dir_path);
+            } else {
+                err = mkdir_if_not_exists(dir_path);
+                if (err) {
+                    return err;
+                }
+            }
+        }
+        dir_path[i] = file_path[i];
+    }
 
-		dir_path[i] = file_path[i];
-	}
-
-	return 0;
+    return 0;
 }
+
 
 int settings_backend_init(void)
 {
