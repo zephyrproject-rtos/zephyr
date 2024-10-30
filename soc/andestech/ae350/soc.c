5b239129f3da9365bd6c8587c4952ba601351cf5
Yong Cong Sin <yongcong.sin@gmail.com>
2024-10-30 22:42:27 +0800

    soc: andestech: run pma_init_per_core() with soc_per_core_init_hook()
    The function `pma_init_per_core()`, as its name suggest, should be
    run from every core, so call it from `soc_per_core_init_hook()`
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>
    Signed-off-by: Yong Cong Sin <yongcong.sin@gmail.com>

---
+++ soc/andestech/ae350/soc.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024 Meta Platforms
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "pma.h"
+
+#ifdef CONFIG_SOC_PER_CORE_INIT_HOOK
+void soc_per_core_init_hook(void)
+{
+#ifdef CONFIG_SOC_ANDES_V5_PMA
+	pma_init_per_core();
+#endif /* SOC_ANDES_V5_PMA */
+}
+#endif /* CONFIG_SOC_PER_CORE_INIT_HOOK */
