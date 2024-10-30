e90ed4379a4425d965b1ebdf81b0e41e7e439b55
Zihao Gao <gaozihao@xiaomi.com>
2024-10-30 10:45:02 +0800

    Bluetooth: AVCTP: allow to create and send AVCTP message
    This patch defines the message format for general AVCTP.
    They would be further called by AVRCP layer.
    
    Signed-off-by: Zihao Gao <gaozihao@xiaomi.com>

--- subsys/bluetooth/host/classic/avctp_internal.h
+++ subsys/bluetooth/host/classic/avctp_internal.h
@@ -11,6 +11,28 @@
 
 #define BT_L2CAP_PSM_AVCTP 0x0017
 
+typedef enum __packed {
+	BT_AVCTP_IPID_NONE = 0b0,
+	BT_AVCTP_IPID_INVALID = 0b1,
+} bt_avctp_ipid_t;
+
+typedef enum __packed {
+	BT_AVCTP_CMD = 0b0,
+	BT_AVCTP_RESPONSE = 0b1,
+} bt_avctp_cr_t;
+
+typedef enum __packed {
+	BT_AVCTP_PKT_TYPE_SINGLE = 0b00,
+} bt_avctp_pkt_type_t;
+
+struct bt_avctp_header {
+	uint8_t ipid: 1;     /* Invalid Profile Identifier (1), otherwise zero (0) */
+	uint8_t cr: 1;       /* Command(0) or Respone(1) */
+	uint8_t pkt_type: 2; /* Set to zero (00) for single L2CAP packet */
+	uint8_t tid: 4;      /* Transaction label */
+	uint16_t pid;        /* Profile Identifier */
+} __packed;
+
 struct bt_avctp;
 
 struct bt_avctp_ops_cb {
@@ -38,3 +60,10 @@ int bt_avctp_connect(struct bt_conn *conn, struct bt_avctp *session);
 
 /* AVCTP disconnect */
 int bt_avctp_disconnect(struct bt_avctp *session);
+
+/* Create AVCTP PDU */
+struct net_buf *bt_avctp_create_pdu(struct bt_avctp *session, bt_avctp_cr_t cr,
+				    bt_avctp_ipid_t ipid, uint8_t *tid, uint16_t pid);
+
+/* Send AVCTP PDU */
+int bt_avctp_send(struct bt_avctp *session, struct net_buf *buf);
