f959bb2597606da9c067dfcaa73d48fa3d757c4a
Arkadiusz Cholewinski <arkadiuszx.cholewinski@intel.com>
2024-10-31 16:25:19 +0100

    tests: pm: Adding tests based on new approach to PM testing.
    Introduces two scripts abstract_power_monitor.py and pwsh_stm32l562.py.
    Adds three new power management pytests based on a new approach.
    
    Signed-off-by: Arkadiusz Cholewinski <arkadiuszx.cholewinski@intel.com>

---
+++ tests/subsys/pm/power_states/pytest/test_power_states.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2024 Intel Corporation.
+# SPDX-License-Identifier: Apache-2.0
+from current_analyzer import current_RMS
+
+def test_power_states(current_measurement_output):
+    data = current_measurement_output
+    # Empirical RMS values in mA with descriptive keys
+    rms_expected = {
+        "k_cpu_idle":57.0,  # K_cpu_idle
+        "state 0": 4.0,  # State 0
+        "state 1": 1.3,  # State 1
+        "state 2": 0.27, # State 2
+        "active": 92    # Active
+    }
+
+    value_in_amps = current_RMS(data)
+    value_in_amps = value_in_amps[0:4]
+
+    # Convert to milliamps
+    value_in_miliamps = [value * 1e4 for value in value_in_amps]
+
+    def print_state_rms(state_label, rms_expected_value, rms_measured_value):
+        tolerance = rms_expected_value / 10
+
+        print(f"{state_label}:")
+        print(f"Expected RMS: {rms_expected_value:.2f} mA")
+        print(f"Tolerance: {tolerance:.2f} mA")
+        print(f"Current RMS: {rms_measured_value:.2f} mA")
+
+        assert (rms_expected_value - tolerance) < rms_measured_value < (rms_expected_value + tolerance), \
+            f"RMS value out of tolerance for {state_label}"
+
+    # Define the state labels
+    state_labels = ["k_cpu_idle", "state 0", "state 1", "state 2", "active"]
+
+    # Loop through the states and print RMS values
+    for state_label, rms_value in zip(state_labels, value_in_miliamps):
+        print_state_rms(state_label, rms_expected[state_label], rms_value)
