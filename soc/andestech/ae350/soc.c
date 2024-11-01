1400da56955ff272d3b21b8082123b222b12d394
Yong Cong Sin <ycsin@meta.com>
2024-11-01 14:13:29 +0800

    soc: andestech: refactor out soc_early_init_hook() from pma.c
    Refactor out the `soc_early_init_hook()` function from `pma.c` to
    `soc.c` which is always compiled so that it can be extended to run
    other init functions easily in the future. Then, restore the function
    in `pma.c` to `pma_init()`.
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>

--- soc/andestech/ae350/soc.c
+++ soc/andestech/ae350/soc.c
@@ -14,3 +14,12 @@ void soc_per_core_init_hook(void)
 #endif /* SOC_ANDES_V5_PMA */
 }
 #endif /* CONFIG_SOC_PER_CORE_INIT_HOOK */
+
+#ifdef CONFIG_SOC_EARLY_INIT_HOOK
+void soc_early_init_hook(void)
+{
+#ifdef CONFIG_SOC_ANDES_V5_PMA
+	pma_init();
+#endif /* CONFIG_SOC_ANDES_V5_PMA */
+}
+#endif /* CONFIG_SOC_EARLY_INIT_HOOK */
