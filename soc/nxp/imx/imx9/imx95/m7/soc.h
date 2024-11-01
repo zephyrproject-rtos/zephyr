5662d27211e174736db14f3258d8dba3221868d6
Yangbo Lu <yangbo.lu@nxp.com>
2024-11-01 15:44:06 +0800

    soc: nxp: imx95: support M7 soc init for NETC
    Added support for soc init for NETC.
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- soc/nxp/imx/imx9/imx95/m7/soc.h
+++ soc/nxp/imx/imx9/imx95/m7/soc.h
@@ -9,4 +9,9 @@
 
 #include <fsl_device_registers.h>
 
+/* IRQSTEER used for NETC MSGINTR2 interrupt */
+#if defined(CONFIG_ETH_NXP_IMX_NETC) && (DT_CHILD_NUM_STATUS_OKAY(DT_NODELABEL(netc)) != 0)
+#define NETC_MSGINTR_IRQ IRQSTEER_0_IRQn
+#endif
+
 #endif /* _SOC_NXP_IMX_IMX95_M7_SOC_H_ */
