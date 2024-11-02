811bd3b84aa9f67ac6ed0e5a5d71740529fb6086
Pieter De Gendt <pieter.degendt@basalte.be>
2024-11-02 16:46:56 +0100

    scripts: west_commands: Add packages command
    Add a new west command as helper for package management of Zephyr
    and modules.
    
    The first package manager to get supported is pip, where west projects
    can list individual packages or requirements files in their module file.
    
    A convenience --install argument is added to install the packages instead.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- scripts/zephyr_module.py
+++ scripts/zephyr_module.py
@@ -161,6 +161,19 @@ mapping:
          type: seq
          sequence:
             - type: str
+  package-managers:
+    required: false
+    type: map
+    mapping:
+      pip:
+        required: false
+        type: map
+        mapping:
+          requirement-files:
+            required: false
+            type: seq
+            sequence:
+              - type: str
 '''
 
 MODULE_YML_PATH = PurePath('zephyr/module.yml')
