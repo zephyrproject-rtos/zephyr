21475774fc412fed7273408de65ee69bcdd1b84e
Carles Cufi <carles.cufi@nordicsemi.no>
2024-11-06 18:42:45 +0100

    boards: nordic: nRF54L15DK: Add basic support for the L05 and L10 ICs
    The nRF54L05 and nRF54L10 are identical to the nRF54L15 except for their
    memory sizes. Add support for emulating those ICs on the nRF54L15DK.
    This commit only adds support for the main application core. Support for
    the FLPR core may be added later.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- boards/nordic/nrf54l15dk/board.cmake
+++ boards/nordic/nrf54l15dk/board.cmake
@@ -1,9 +1,11 @@
 # Copyright (c) 2024 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 
-if(CONFIG_SOC_NRF54L15_CPUAPP)
+if(CONFIG_SOC_NRF54L05_CPUAPP OR CONFIG_SOC_NRF54L10_CPUAPP OR
+		CONFIG_SOC_NRF54L15_CPUAPP)
   board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
-elseif(CONFIG_SOC_NRF54L15_CPUFLPR)
+elseif(CONFIG_SOC_NRF54L05_CPUFLPR OR CONFIG_SOC_NRF54L10_CPUFLPR OR
+		CONFIG_SOC_NRF54L15_CPUFLPR)
   board_runner_args(jlink "--speed=4000")
 endif()
 
