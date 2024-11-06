fcdc7f99d1d8644225fd65c9e7a5e267677fbe81
Eric Ackermann <eric.ackermann@cispa.de>
2024-11-06 18:28:49 +0100

    boards: openhwgroup: add CVA6 on Arty A7 board
    Adds support for the CVA6 CPU on an Arty A7 FPGA board
    (https://github.com/openhwgroup/cva6).
    The SoC currently contains the CVA6 CPU, interrupt controllers (CLINT and PLIC),
    UART, a SPI for booting from SD, a boot ROM, a GPIO and the lowRISC ethernet subsystem.
    Two slightly different versions of the board are added, with a 32-bit
    and a 64-bit configuration of CVA6, respectively.
    
    Signed-off-by: Eric Ackermann <eric.ackermann@cispa.de>

---
+++ boards/openhwgroup/cv64a6_arty_a7_100/board.cmake
@@ -0,0 +1,8 @@
+# Copyright 2024 CISPA Helmholtz Center for Information Security gGmbH
+# SPDX-License-Identifier: Apache-2.0
+board_runner_args(openocd "--config=${BOARD_DIR}/support/ariane.cfg")
+board_runner_args(openocd "--use-elf")
+board_runner_args(openocd "--verify")
+board_runner_args(openocd "--cmd-pre-init=riscv.cpu configure -work-area-phys 0x8f000000 -work-area-size 16780000")
+
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
