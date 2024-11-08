acc7c22ed3f3ae3443f02a9f6d7c0266d2b76c21
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2024-11-08 19:07:06 +0800

    soc: ITE: Add ITE it51xxx SoC
    Add support for ITE it51xxx SoC.
    NOTE: it51526aw is not support RISCV_ISA_EXT_C.
    
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>

--- soc/ite/ec/common/soc_common.h
+++ soc/ite/ec/common/soc_common.h
@@ -43,11 +43,10 @@ uint32_t chip_get_pll_freq(void);
 void chip_pll_ctrl(enum chip_pll_mode mode);
 void riscv_idle(enum chip_pll_mode mode, unsigned int key);
 
-#ifdef CONFIG_SOC_IT8XXX2_CPU_IDLE_GATING
+/* Functions for managing the CPU idle state */
 void chip_permit_idle(void);
 void chip_block_idle(void);
 bool cpu_idle_not_allowed(void);
-#endif
 
 #endif /* !_ASMLANGUAGE */
 
