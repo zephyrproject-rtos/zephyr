7c4b6f58a94ba8c40b3abb5c62c21d9970a845ee
Titouan Christophe <moiandme@gmail.com>
2024-11-10 18:54:33 +0100

    usb: device_next: add new MIDI 2.0 device class
    This adds a new USB device class (based on usb/device_next) that implements
    revision 2.0 of the MIDIStreaming interface, a sub-class of the USB audio
    device class. In practice, the MIDI interface is much more simple and has
    little in common with Audio, so it makes sense to have it as a separate
    class driver.
    
    MIDI inputs and outputs are configured through the device tree, under a
    node `compatible = "zephyr,usb-midi"`. As per the USB-MIDI2.0 spec,
    a single usb-midi interface can convey up to 16 Universal MIDI groups,
    comprising 16 channels each. Data is carried from/to the host via
    so-called Group Terminals, that are organized in Group Terminal Blocks.
    They are represented as children of the usb-midi interface in the device
    tree.
    
    From the Zephyr application programmer perspective, MIDI data is exchanged
    with the host through the device associated with the `zephyr,usb-midi`
    interface, using the following API:
    
    * Send a Universal MIDI Packet to the host: `usb_midi_send(device, pkt)`
    * Universal MIDI Packets from the host are delivered to the function passed
      in `usb_midi_set_ops(device, &{.rx_packet_cb = handler})`
    
    Compliant USB-MIDI 2.0 devices are required to expose a USB-MIDI1.0
    interface as alt setting 0, and the 2.0 interface on alt setting 1.
    To avoid the extra complexity of generating backward compatible USB
    descriptors and translating Universal MIDI Packets from/to the old
    USB-MIDI1.0 format, this driver generates an empty MIDI1.0 interface
    (without any input/output); and therefore will only be able to exchange
    MIDI data when the host has explicitely enabled MIDI2.0 (alt setting 1).
    
    This implementation is based on the following documents, which are referred
    to in the inline comments:
    
    * `midi20`:
        Universal Serial Bus Device Class Definition for MIDI Devices
        Release 2.0
        https://www.usb.org/sites/default/files/USB%20MIDI%20v2_0.pdf
    * `ump112`:
        Universal MIDI Packet (UMP) Format and MIDI 2.0 Protocol
          With MIDI 1.0 Protocol in UMP Format
        Document Version 1.1.2
        https://midi.org/universal-midi-packet-ump-and-midi-2-0-protocol-specification
    
    Signed-off-by: Titouan Christophe <moiandme@gmail.com>

---
+++ include/zephyr/usb/class/usbd_midi2.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2024 Titouan Christophe
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_USB_CLASS_USBD_MIDI_H_
+#define ZEPHYR_INCLUDE_USB_CLASS_USBD_MIDI_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief USB MIDI 2.0 class device API
+ * @defgroup usb_midi USB MIDI 2.0 Class device API
+ * @ingroup usb
+ * @since 4.1
+ * @version 0.1.0
+ * @see midi20: "Universal Serial Bus Device Class Definition for MIDI Devices"
+ *              Document Release 2.0 (May 5, 2020)
+ * @{
+ */
+
+#include <zephyr/device.h>
+#include <zephyr/audio/midi.h>
+
+/**
+ * @brief      MIDI2 application event handlers
+ */
+struct usbd_midi_ops {
+	/**
+	 * @brief Callback type for incoming Universal MIDI Packets from host
+	 * @param[in]  dev   The MIDI2 device receiving the packet
+	 * @param[in]  ump   The received packet in Universal MIDI Packet format
+	 */
+	void (*rx_packet_cb)(const struct device *dev, const struct midi_ump ump);
+
+	/**
+	 * @brief Callback type for MIDI2 interface runtime status change
+	 * @param[in]  dev    The MIDI2 device
+	 * @param[in]  ready  True if the interface is enabled by the host
+	 */
+	void (*ready_cb)(const struct device *dev, const bool ready);
+};
+
+/**
+ * @brief      Send a Universal MIDI Packet to the host
+ * @param[in]  dev   The MIDI2 device
+ * @param[in]  ump   The packet to send, in Universal MIDI Packet format
+ * @return     0 on success, all other values should be treated as error
+ *             -EIO if USB MIDI 2.0 is not enabled by the host
+ *             -ENOBUFS if there is no space in the TX buffer
+ */
+int usbd_midi_send(const struct device *dev, const struct midi_ump ump);
+
+/**
+ * @brief Set the application event handlers on a USB MIDI device
+ * @param[in]  dev   The MIDI2 device
+ * @param[in]  ops   The event handlers. Pass NULL to reset all callbacks
+ */
+void usbd_midi_set_ops(const struct device *dev, const struct usbd_midi_ops *ops);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
