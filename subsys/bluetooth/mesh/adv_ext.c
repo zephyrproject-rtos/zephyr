bcac572ab477b4421063bfaf9d8a5cf833dc5100
Lingao Meng <menglingao@xiaomi.com>
2024-11-13 16:23:04 +0800

    Bluetooth: Mesh: Remove ADV_FLAG_PROXY_START, due to meanless
    The first why introduce ADV_FLAG_PROXY_START
    see here(https://github.com/zephyrproject-rtos/zephyr/pull/58826)
    
    The main core change is to mark ADV_FLAG_PROXY as moving to the last
    enabled proxy instead of before.
    
    But introducing the ADV_FLAG_PROXY_START flag seems meaningless.
    
    Signed-off-by: Lingao Meng <menglingao@xiaomi.com>

--- subsys/bluetooth/mesh/adv_ext.c
+++ subsys/bluetooth/mesh/adv_ext.c
@@ -48,8 +48,6 @@ enum {
 	ADV_FLAG_SENT,
 	/** Currently performing proxy advertising */
 	ADV_FLAG_PROXY,
-	/** The proxy has been start, but maybe pending. */
-	ADV_FLAG_PROXY_START,
 	/** The send-call has been pending. */
 	ADV_FLAG_SCHEDULE_PENDING,
 	/** Custom adv params have been set, we need to update the parameters on
@@ -261,7 +259,6 @@ static const char * const adv_tag_to_str[] = {
 static bool schedule_send_with_mask(struct bt_mesh_ext_adv *ext_adv, int ignore_mask)
 {
 	if (atomic_test_and_clear_bit(ext_adv->flags, ADV_FLAG_PROXY)) {
-		atomic_clear_bit(ext_adv->flags, ADV_FLAG_PROXY_START);
 		(void)bt_le_ext_adv_stop(ext_adv->instance);
 
 		atomic_clear_bit(ext_adv->flags, ADV_FLAG_ACTIVE);
@@ -301,7 +298,6 @@ static void send_pending_adv(struct k_work *work)
 
 		atomic_clear_bit(ext_adv->flags, ADV_FLAG_ACTIVE);
 		atomic_clear_bit(ext_adv->flags, ADV_FLAG_PROXY);
-		atomic_clear_bit(ext_adv->flags, ADV_FLAG_PROXY_START);
 
 		if (ext_adv->adv) {
 			struct bt_mesh_adv_ctx ctx = ext_adv->adv->ctx;
@@ -345,8 +341,6 @@ static void send_pending_adv(struct k_work *work)
 	    !(ext_adv->tags & BT_MESH_ADV_TAG_BIT_PROXY)) {
 		return;
 	}
-
-	atomic_set_bit(ext_adv->flags, ADV_FLAG_PROXY_START);
 
 	if (!bt_mesh_adv_gatt_send()) {
 		atomic_set_bit(ext_adv->flags, ADV_FLAG_PROXY);
@@ -491,7 +485,7 @@ static void connected(struct bt_le_ext_adv *instance,
 {
 	struct bt_mesh_ext_adv *ext_adv = gatt_adv_get();
 
-	if (atomic_test_and_clear_bit(ext_adv->flags, ADV_FLAG_PROXY_START)) {
+	if (atomic_test_and_clear_bit(ext_adv->flags, ADV_FLAG_PROXY)) {
 		atomic_clear_bit(ext_adv->flags, ADV_FLAG_ACTIVE);
 		(void)schedule_send(ext_adv);
 	}
