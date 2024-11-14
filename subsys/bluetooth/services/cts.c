aae851fdb560b70327acedeae29236e1fcf90bbb
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2024-11-14 19:47:42 +0100

    bluetooth: CTS: Fix includes to avoid build error with some libCs
    Remove unnecessary include in header and source file.
    
    gmtime_r() is an extension to the C library, and therefore one
    needs to explicitly ask for its prototype to have it exposed.
    This is done by defining _POSIX_C_SOURCE so let's do so.
    
    These two changes fix build errors with some libCs.
    Tested with pico, newlib, minimal and the host glibc.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
    (cherry picked from commit a785548df6353dafe362579af6859d3d02458dbb)

--- subsys/bluetooth/services/cts.c
+++ subsys/bluetooth/services/cts.c
@@ -7,9 +7,17 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
+#undef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200809L /* To get gmtime_r()'s prototype */
+
+#ifdef CONFIG_BT_CTS_HELPER_API
+#include <time.h>
+#include <zephyr/sys/timeutil.h>
+#endif
+
 #include <stdbool.h>
 #include <zephyr/sys/byteorder.h>
-#include <zephyr/posix/time.h>
 
 #include <zephyr/bluetooth/conn.h>
 #include <zephyr/bluetooth/gatt.h>
@@ -25,9 +33,6 @@ LOG_MODULE_REGISTER(cts, CONFIG_BT_CTS_LOG_LEVEL);
 static const struct bt_cts_cb *cts_cb;
 
 #ifdef CONFIG_BT_CTS_HELPER_API
-
-#include <time.h>
-#include <zephyr/sys/timeutil.h>
 
 int bt_cts_time_to_unix_ms(const struct bt_cts_time_format *ct_time, int64_t *unix_ms)
 {
