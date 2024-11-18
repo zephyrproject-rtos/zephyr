ef2446b022637c03831d74bb6aaf28f0aaa36698
Andrew Featherstone <andrew.featherstone@gmail.com>
2024-11-18 22:11:49 +0000

    soc: rp2350: Add initial support for the Hazard3 cores
    The RP2350 SoC series contain two Hazard3 cores, which use the RISC-V
    instruction set. Define a new CPU cluster (`hazard3`), which is intended
    to be used with the two Hazard3 cores 'plugged in' to the two 'sockets'
    in the RP2350 series SoCs.
    
    Update the linker script to support linking against the correct
    (ISA-specific) linker script, and to generate a correct IMAGE_DEF for
    the target ISA.
    
    Signed-off-by: Andrew Featherstone <andrew.featherstone@gmail.com>

--- soc/raspberrypi/rpi_pico/common/soc.h
+++ soc/raspberrypi/rpi_pico/common/soc.h
@@ -6,12 +6,15 @@
  */
 
 /**
- * @file SoC configuration macros for the Raspberry Pi RP2040 family processors
+ * @file SoC configuration macros for the Raspberry Pi Pico family processors
+ * (RP2040 and RP235xx).
  */
 
 #ifndef _RPI_PICO_COMMON_SOC_H_
 #define _RPI_PICO_COMMON_SOC_H_
 
+#ifdef CONFIG_CPU_CORTEX_M
 #include <cmsis_core_m_defaults.h>
+#endif
 
 #endif /* _RPI_PICO_COMMON_SOC_H_ */
