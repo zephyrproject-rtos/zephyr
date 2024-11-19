d25f8bda7ad2b3934050c65225bb5e5a58366a40
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2024-11-19 15:53:52 +0100

    lib: os: poweroff: Add sys_poweroff_ready function
    Existing sys_poweroff cannot return. In multi-core targets it is
    possible that core indicates readiness for power off but system
    is waken up. In that case call to the poweroff function returns.
    Add sys_poweroff_ready() which can be called in that case.
    Function may return. It calls same z_sys_poweroff_prepare when
    CONFIG_POWEROFF_PREPARE=y and additionally calls
    z_sys_poweroff_unprepare if z_sys_poweroff_ready() returns.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- include/zephyr/sys/poweroff.h
+++ include/zephyr/sys/poweroff.h
@@ -29,6 +29,15 @@ extern "C" {
  */
 FUNC_NORETURN void z_sys_poweroff(void);
 
+/**
+ * @brief System power off ready hook.
+ *
+ * This function needs to be implemented in the platform code. It must put the core
+ * in the state which may lead to the power off of the system but it can return
+ * if powering off is interrupted.
+ */
+void z_sys_poweroff_ready(void);
+
 /** @endcond */
 
 /**
@@ -44,6 +53,19 @@ FUNC_NORETURN void z_sys_poweroff(void);
 FUNC_NORETURN void sys_poweroff(void);
 
 /**
+ * @brief Put core in "ready for power off" state.
+ *
+ * Function can be used in multi-core platforms to put a core into a state which
+ * may result in power off (if all other cores goes to the power off state) but
+ * it may also return if system is waken up before power off is reached.
+ *
+ * @retval 0 if the core was waken up from power off ready state.
+ * @retval negative if @ref z_sys_poweroff_prepare or @ref z_sys_poweroff_unprepare
+ * returned error.
+ */
+int sys_poweroff_ready(void);
+
+/**
  * @brief Prepare the power off.
  *
  * Platform specific code which can be executed before interrupts are locked and
@@ -52,9 +74,20 @@ FUNC_NORETURN void sys_poweroff(void);
  *
  * @retval 0 if preparation was successful.
  * @retval negative if error occurred. Note that @ref sys_poweroff cannot return so error
- * does not stop powering off.
+ * can only interrupt powering off when @ref sys_poweroff_ready is used.
  */
 int z_sys_poweroff_prepare(void);
+
+/**
+ * @brief Revert preparation to the power off.
+ *
+ * Platform specific code which can be executed if the core returns from @ref z_sys_poweroff_ready.
+ * Called by @ref sys_poweroff_ready when @kconfig{CONFIG_POWEROFF_PREPARE} is enabled.
+ *
+ * @retval 0 if reverting was successful.
+ * @retval negative if error occurred.
+ */
+int z_sys_poweroff_unprepare(void);
 
 /** @} */
 
--- lib/os/poweroff.c
+++ lib/os/poweroff.c
@@ -16,3 +16,31 @@ void sys_poweroff(void)
 
 	z_sys_poweroff();
 }
+
+int sys_poweroff_ready(void)
+{
+	uint32_t key;
+
+	if (IS_ENABLED(CONFIG_POWEROFF_PREPARE)) {
+		int err = z_sys_poweroff_prepare();
+
+		if (err < 0) {
+			return err;
+		}
+	}
+
+	key = irq_lock();
+
+	z_sys_poweroff_ready();
+	irq_unlock(key);
+
+	if (IS_ENABLED(CONFIG_POWEROFF_PREPARE)) {
+		int err = z_sys_poweroff_unprepare();
+
+		if (err < 0) {
+			return err;
+		}
+	}
+
+	return 0;
+}
