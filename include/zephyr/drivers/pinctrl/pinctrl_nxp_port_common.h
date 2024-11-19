42cc35f9414203e35d56e1587e7a99daabcb705d
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-11-19 12:51:04 -0600

    soc: nxp: consolidate nxp port pinctrl headers
    NXP PORT IP instantiations often have different features absent, IE
    input buffer, open drain, or slew rate support. Check if the relevant
    PCR register bitmasks are defined in the common pin control file, and
    define the bitmasks to 0x0 (no effect) if they are not. This allows us
    to further consolidate the pinctrl_soc.h headers for SOCs using the PORT
    IP.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- include/zephyr/drivers/pinctrl/pinctrl_nxp_port_common.h
+++ include/zephyr/drivers/pinctrl/pinctrl_nxp_port_common.h
@@ -6,50 +6,49 @@
 
 /*
  * @file
- * NXP Kinetis SOC specific helpers for pinctrl driver
+ * NXP PORT SOC specific helpers for pinctrl driver
  */
 
 
-#ifndef ZEPHYR_SOC_ARM_NXP_KINETIS_COMMON_PINCTRL_SOC_H_
-#define ZEPHYR_SOC_ARM_NXP_KINETIS_COMMON_PINCTRL_SOC_H_
+#ifndef ZEPHYR_INCLUDE_DRIVERS_PINCTRL_PINCTRL_NXP_PORT_COMMON_H_
+#define ZEPHYR_INCLUDE_DRIVERS_PINCTRL_PINCTRL_NXP_PORT_COMMON_H_
 
 /** @cond INTERNAL_HIDDEN */
 
 #include <zephyr/devicetree.h>
 #include <zephyr/types.h>
 
+/* Include SOC headers, so we get definitions for PCR bitmasks */
+#include <soc.h>
+
 #ifdef __cplusplus
 extern "C" {
+#endif
+
+/*
+ * Some PORT IP instantiations lack certain features, include input buffers,
+ * open drain, and slew rate. If masks aren't defined for these bitfields,
+ * define them to have no effect
+ */
+#ifndef PORT_PCR_IBE_MASK /* Input buffer enable */
+#define PORT_PCR_IBE_MASK 0x0
+#define PORT_PCR_IBE(x) 0x0
+#endif
+
+#ifndef PORT_PCR_SRE_MASK /* Slew rate */
+#define PORT_PCR_SRE_MASK 0x0
+#define PORT_PCR_SRE(x) 0x0
+#endif
+
+#ifndef PORT_PCR_ODE_MASK /* Open drain */
+#define PORT_PCR_ODE_MASK 0x0
+#define PORT_PCR_ODE(x) 0x0
 #endif
 
 
 typedef uint32_t pinctrl_soc_pin_t;
 
-/* Kinetis KW/KL/KE series does not support open drain. Define macros to have no effect
- * Note: KW22 and KW24 do support open drain, rest of KW series does not
- */
-/* clang-format off */
-#if (defined(CONFIG_SOC_SERIES_KINETIS_KWX) &&                                                     \
-	!(defined(CONFIG_SOC_MKW24D5) || defined(CONFIG_SOC_MKW22D5))) ||                          \
-	defined(CONFIG_SOC_SERIES_KINETIS_KL2X) || defined(CONFIG_SOC_SERIES_KINETIS_KE1XF) ||     \
-	defined(CONFIG_SOC_SERIES_KE1XZ)
-#define PORT_PCR_ODE(x)   0x0
-#define PORT_PCR_ODE_MASK 0x0
-#endif
-/* clang-format on */
-
-/* Kinetis KE series does not support slew rate. Define macros to have no effect */
-#if defined(CONFIG_SOC_SERIES_KINETIS_KE1XF) || defined(CONFIG_SOC_SERIES_KE1XZ)
-#define PORT_PCR_SRE(x)   0x0
-#define PORT_PCR_SRE_MASK 0x0
-#endif
-
-#if !(defined(CONFIG_SOC_SERIES_MCXA))
-#define PORT_PCR_IBE(x)   0x0
-#define PORT_PCR_IBE_MASK 0x0
-#endif
-
-#define Z_PINCTRL_KINETIS_PINCFG(node_id)                                                          \
+#define Z_PINCTRL_NXP_PORT_PINCFG(node_id)                                                         \
 	(PORT_PCR_DSE(DT_ENUM_IDX(node_id, drive_strength)) |                                      \
 	 PORT_PCR_PS(DT_PROP(node_id, bias_pull_up)) |                                             \
 	 PORT_PCR_PE(DT_PROP(node_id, bias_pull_up)) |                                             \
@@ -59,12 +58,12 @@ typedef uint32_t pinctrl_soc_pin_t;
 	 PORT_PCR_IBE(DT_PROP(node_id, input_enable)) |                                            \
 	 PORT_PCR_PFE(DT_PROP(node_id, nxp_passive_filter)))
 
-#define Z_PINCTRL_KINETIS_PCR_MASK                                                                 \
+#define Z_PINCTRL_NXP_PORT_PCR_MASK                                                                \
 	(PORT_PCR_MUX_MASK | PORT_PCR_DSE_MASK | PORT_PCR_ODE_MASK | PORT_PCR_PFE_MASK |           \
 	 PORT_PCR_IBE_MASK | PORT_PCR_SRE_MASK | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK)
 
 #define Z_PINCTRL_STATE_PIN_INIT(group, pin_prop, idx)                                             \
-	DT_PROP_BY_IDX(group, pin_prop, idx) | Z_PINCTRL_KINETIS_PINCFG(group),
+	DT_PROP_BY_IDX(group, pin_prop, idx) | Z_PINCTRL_NXP_PORT_PINCFG(group),
 
 #define Z_PINCTRL_STATE_PINS_INIT(node_id, prop)                                                   \
 	{DT_FOREACH_CHILD_VARGS(DT_PHANDLE(node_id, prop), DT_FOREACH_PROP_ELEM, pinmux,           \
@@ -76,4 +75,4 @@ typedef uint32_t pinctrl_soc_pin_t;
 
 /** @endcond */
 
-#endif /* ZEPHYR_SOC_ARM_NXP_KINETIS_COMMON_PINCTRL_SOC_H_ */
+#endif /* ZEPHYR_INCLUDE_DRIVERS_PINCTRL_PINCTRL_NXP_PORT_COMMON_H_ */
