534916c2253e2aecdf32811cd66a519c0b969304
Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>
2024-11-20 14:45:37 +0100

    modules: hal_nordic: nrfx: cleanup
    * Move all KConfig configuration from CMake to `nrfx_kconfig.h`
    * Move macro utils from `nrfx_kconfig.h` to `nrfx_zephyr_utils.h`
    * Move GRTC channels allocation from `nrfx_kconfig.h` to
      `nrfx_reserved_resources.h`
    
    Signed-off-by: Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>

--- modules/hal_nordic/nrfx/nrfx_kconfig.h
+++ modules/hal_nordic/nrfx/nrfx_kconfig.h
@@ -7,9 +7,6 @@
 #ifndef NRFX_KCONFIG_H__
 #define NRFX_KCONFIG_H__
 
-#include <zephyr/devicetree.h>
-#include <zephyr/sys/util_macro.h>
-
 /*
  * These are mappings of Kconfig options enabling nrfx drivers and particular
  * peripheral instances to the corresponding symbols used inside of nrfx.
@@ -967,85 +964,31 @@
 	CONFIG_NRF52_ANOMALY_109_WORKAROUND_EGU_INSTANCE
 #endif
 
-#if defined(CONFIG_SOC_SERIES_BSIM_NRFXX)
-#include "nrfx_config_bsim.h"
+#ifdef CONFIG_SOC_NRF54LX_SKIP_CLOCK_CONFIG
+#define NRF_SKIP_CLOCK_CONFIGURATION
+#endif
+
+#ifdef CONFIG_SOC_NRF54LX_DISABLE_FICR_TRIMCNF
+#define NRF_DISABLE_FICR_TRIMCNF
+#endif
+
+#ifdef CONFIG_SOC_NRF54LX_SKIP_GLITCHDETECTOR_DISABLE
+#define NRF_SKIP_GLITCHDETECTOR_DISABLE
 #endif
 
 /*
- * For chips with TrustZone support, MDK provides CMSIS-Core peripheral
- * accessing symbols in two flavors, with secure and non-secure base address
- * mappings. Their names contain the suffix _S or _NS, respectively.
- * Because nrfx HALs and drivers require these peripheral accessing symbols
- * without any suffixes, the following macro is provided that will translate
- * their names according to the kind of the target that is built.
+ * Inject code to skip TAMPC setup for nRF54L20 and nRF54L09. It is not supported for now.
+ * It needs to be removed when support is provided.
  */
-#if defined(NRF_TRUSTZONE_NONSECURE)
-#define NRF_PERIPH(P) P##_NS
-#else
-#define NRF_PERIPH(P) P##_S
+#if defined(CONFIG_SOC_NRF54L20_ENGA_CPUAPP) || defined(CONFIG_SOC_NRF54L09_ENGA_CPUAPP)
+#define NRF_SKIP_TAMPC_SETUP
 #endif
 
-#define NRFX_CONFIG_BIT_DT(node_id, prop, idx) BIT(DT_PROP_BY_IDX(node_id, prop, idx))
-#define NRFX_CONFIG_MASK_DT(node_id, prop) \
-	(COND_CODE_1(DT_NODE_HAS_PROP(node_id, prop), \
-		(DT_FOREACH_PROP_ELEM_SEP(node_id, prop, NRFX_CONFIG_BIT_DT, (|))), \
-		(0)))
-
-/* If the GRTC system timer driver is to be used, prepare definitions required
- * by the nrfx_grtc driver (NRFX_GRTC_CONFIG_ALLOWED_CC_CHANNELS_MASK and
- * NRFX_GRTC_CONFIG_NUM_OF_CC_CHANNELS) based on information from devicetree.
- */
-#if DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_grtc)
-#define NRFX_GRTC_CONFIG_ALLOWED_CC_CHANNELS_MASK \
-	(NRFX_CONFIG_MASK_DT(DT_INST(0, nordic_nrf_grtc), owned_channels) & \
-	 ~NRFX_CONFIG_MASK_DT(DT_INST(0, nordic_nrf_grtc), child_owned_channels))
-#define NRFX_GRTC_CONFIG_NUM_OF_CC_CHANNELS \
-	(DT_PROP_LEN_OR(DT_INST(0, nordic_nrf_grtc), owned_channels, 0) - \
-	 DT_PROP_LEN_OR(DT_INST(0, nordic_nrf_grtc), child_owned_channels, 0))
-#endif /* DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_grtc) */
-
-/* If global of local DPPIC peripherals are used, provide the following macro
- * definitions required by the interconnect/apb layer:
- * - NRFX_DPPI_PUB_CONFIG_ALLOWED_CHANNELS_MASK_BY_INST_NUM(inst_num)
- * - NRFX_DPPI_SUB_CONFIG_ALLOWED_CHANNELS_MASK_BY_INST_NUM(inst_num)
- * - NRFX_DPPI_PUB_OR_SUB_MASK(inst_num)
- * - NRFX_DPPI_CHANNELS_SINGLE_VAR_NAME_BY_INST_NUM(inst_num)
- * - NRFX_INTERCONNECT_APB_GLOBAL_DPPI_DEFINE
- * - NRFX_INTERCONNECT_APB_LOCAL_DPPI_DEFINE
- * based on information from devicetree.
- */
 #if	DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_dppic_global) || \
 	DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_dppic_local)
 #ifndef NRFX_DPPI_ENABLED
 #define NRFX_DPPI_ENABLED 1
 #endif
-/* Source (publish) channels masks generation. */
-#define NRFX_DPPI_PUB_CONFIG_ALLOWED_CHANNELS_MASK_BY_INST_NUM(inst_num) \
-	NRFX_CONFIG_MASK_DT(DT_NODELABEL(_CONCAT(dppic, inst_num)), source_channels)
-
-/* Sink (subscribe) channels masks generation. */
-#define NRFX_DPPI_SUB_CONFIG_ALLOWED_CHANNELS_MASK_BY_INST_NUM(inst_num) \
-	NRFX_CONFIG_MASK_DT(DT_NODELABEL(_CONCAT(dppic, inst_num)), sink_channels)
-
-#define NRFX_DPPI_PUB_OR_SUB_MASK(inst_num) \
-	UTIL_OR(DT_NODE_HAS_PROP(DT_NODELABEL(_CONCAT(dppic, inst_num)), source_channels), \
-		DT_NODE_HAS_PROP(DT_NODELABEL(_CONCAT(dppic, inst_num)), sink_channels))
-
-/* Variables names generation. */
-#define NRFX_CONFIG_DPPI_CHANNELS_ENTRY_NAME(node_id) _CONCAT(_CONCAT(m_, node_id), _channels)
-#define NRFX_DPPI_CHANNELS_SINGLE_VAR_NAME_BY_INST_NUM(inst_num) \
-	NRFX_CONFIG_DPPI_CHANNELS_ENTRY_NAME(DT_NODELABEL(_CONCAT(dppic, inst_num)))
-
-/* Variables entries generation. */
-#define NRFX_CONFIG_DPPI_CHANNELS_ENTRY(node_id) \
-	static nrfx_atomic_t NRFX_CONFIG_DPPI_CHANNELS_ENTRY_NAME(node_id) \
-		__attribute__((used)) = \
-		NRFX_CONFIG_MASK_DT(node_id, source_channels) | \
-		NRFX_CONFIG_MASK_DT(node_id, sink_channels);
-#define NRFX_INTERCONNECT_APB_GLOBAL_DPPI_DEFINE \
-	DT_FOREACH_STATUS_OKAY(nordic_nrf_dppic_global, NRFX_CONFIG_DPPI_CHANNELS_ENTRY)
-#define NRFX_INTERCONNECT_APB_LOCAL_DPPI_DEFINE \
-	DT_FOREACH_STATUS_OKAY(nordic_nrf_dppic_local, NRFX_CONFIG_DPPI_CHANNELS_ENTRY)
 #endif /* DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_dppic_global) || ... */
 
 /* If local or global DPPIC peripherals are used, provide the following macro
