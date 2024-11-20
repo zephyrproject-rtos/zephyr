761f27583173a2dc56a3f4f59b0ab8ba59d6221f
Yong Cong Sin <ycsin@meta.com>
2024-11-21 01:55:47 +0800

    arch: riscv: streamline fatal handling code
    `CONFIG_EXTRA_EXCEPTION_INFO` that was added in #78065 doesn't
    seem necessary, as we were already storing and printing the
    callee-saved-registers before that. All `CONFIG_EXTRA_EXCEPTION_INFO`
    does in RISCV is to add an additional `_callee_saved_t *csf` in the
    `struct arch_esf`, which overhead is negligible to what's being enabled
    by `CONFIG_EXCEPTION_DEBUG`.
    
    Let's remove `CONFIG_EXTRA_EXCEPTION_INFO`, and have that extra
    `_callee_saved_t *csf` in the `struct arch_esf` as long as
    `CONFIG_EXCEPTION_DEBUG` is enabled.
    
    TL;DR: it doesn't make sense to not enable `CONFIG_EXTRA_EXCEPTION_INFO`
    when `CONFIG_EXCEPTION_DEBUG` is enabled, so let's merge them.
    
    Then, since `*csf` is always available in the `struct arch_esf` when
    `CONFIG_EXCEPTION_DEBUG=y`, we can simply rely on that pointer in
    `z_riscv_fatal_error()` instead of an additional argument in
    `z_riscv_fatal_error_csf()`, rendering the latter redundant and thus
    can be removed.
    
    Additionally, save the callee-saved registers before jumping to
    to `z_riscv_fault()`, so that callee-saved-registers are printed on
    generic CPU exception as well.
    
    Signed-off-by: Yong Cong Sin <yongcong.sin@gmail.com>
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>

--- arch/riscv/core/fatal.c
+++ arch/riscv/core/fatal.c
@@ -80,12 +80,7 @@ const char *z_riscv_mcause_str(unsigned long cause)
 FUNC_NORETURN void z_riscv_fatal_error(unsigned int reason,
 				       const struct arch_esf *esf)
 {
-	z_riscv_fatal_error_csf(reason, esf, NULL);
-}
-
-FUNC_NORETURN void z_riscv_fatal_error_csf(unsigned int reason, const struct arch_esf *esf,
-					   const _callee_saved_t *csf)
-{
+	__maybe_unused _callee_saved_t *csf = NULL;
 	unsigned long mcause;
 
 	__asm__ volatile("csrr %0, mcause" : "=r" (mcause));
@@ -122,6 +117,8 @@ FUNC_NORETURN void z_riscv_fatal_error_csf(unsigned int reason, const struct arc
 		EXCEPTION_DUMP("   mepc: " PR_REG, esf->mepc);
 		EXCEPTION_DUMP("mstatus: " PR_REG, esf->mstatus);
 		EXCEPTION_DUMP("");
+
+		csf = esf->csf;
 	}
 
 	if (csf != NULL) {
@@ -247,6 +244,13 @@ FUNC_NORETURN void arch_syscall_oops(void *ssf_ptr)
 void z_impl_user_fault(unsigned int reason)
 {
 	struct arch_esf *oops_esf = _current->syscall_frame;
+
+#ifdef CONFIG_EXCEPTION_DEBUG
+	/* csf isn't populated in the syscall frame */
+	if (oops_esf != NULL) {
+		oops_esf->csf = NULL;
+	}
+#endif /* CONFIG_EXCEPTION_DEBUG */
 
 	if (((_current->base.user_options & K_USER) != 0) &&
 		reason != K_ERR_STACK_CHK_FAIL) {
