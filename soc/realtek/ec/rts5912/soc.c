b1d100f78cd6b48ce97b355b301dd9bd7be42595
Lin Yu-Cheng <lin_yu_cheng@realtek.com>
2024-11-22 16:54:13 +0800

    soc: realrek: ec: Add Realtek RTS5912 SoC
    Add support for Realtek RTS5912 embedded controller (EC).
    
    Signed-off-by: Lin Yu-Cheng <lin_yu_cheng@realtek.com>

---
+++ soc/realtek/ec/rts5912/soc.c
@@ -0,0 +1,28 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (c) 2024 Realtek Semiconductor Corporation, SIBG-SD7
+ * Author: Lin Yu-Cheng <lin_yu_cheng@realtek.com> / Titan Chen
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/init.h>
+
+#if defined(CONFIG_RTS5912_ON_ENTER_CPU_IDLE_HOOK)
+bool z_arm_on_enter_cpu_idle(void)
+{
+	/* Returning false prevent device goes to sleep mode */
+	return false;
+}
+#endif
+
+/**
+ * @brief Perform basic hardware initialization at boot.
+ *
+ * This needs to be run from the very beginning.
+ */
+void soc_early_init_hook(void)
+{
+	/* Apply device related preinit configuration */
+}
