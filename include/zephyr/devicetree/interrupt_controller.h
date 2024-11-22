b5e5935647c7e8bc5b38c693f15f780c76d5e095
James Roy <rruuaanng@outlook.com>
2024-11-22 15:24:53 +0800

    devicetree: Add DT_INTC_FLAGS_BY_IDX macro
    Add 'DT_INTC_FLAGS_BY_IDX' macro to get cell members
    named "sense" and "priority".
    
    Signed-off-by: James Roy <rruuaanng@outlook.com>

--- include/zephyr/devicetree/interrupt_controller.h
+++ include/zephyr/devicetree/interrupt_controller.h
@@ -50,6 +50,49 @@ extern "C" {
 #define DT_INST_INTC_GET_AGGREGATOR_LEVEL(inst) DT_INTC_GET_AGGREGATOR_LEVEL(DT_DRV_INST(inst))
 
 /**
+ * @brief Get a interrupt controller specifier's flags cell
+ *
+ * This macro expects interrupt specifiers with cells named "sense" | "priority".
+ * If there is no "sense" or "priority" cell in the interrupt specifier, zero is returned.
+ * Refer to the node's binding to check specifier cell names if necessary.
+ *
+ * Example devicetree fragment:
+ *
+ *     intc1: interrupt-controller@... {
+ *             compatible = "vnd,interrupt";
+ *             #interrupt-cells = <2>;
+ *     };
+ *
+ *     intc2: interrupt-controller@... {
+ *             compatible = "vnd,interrupt";
+ *             #interrupt-cells = <2>;
+ *     };
+ *
+ *     n: node {
+ *             interrupts-extended = <&intc1 3 2 15>,
+ *                                   <&intc2 4 3 16>;
+ *     };
+ *
+ * Bindings fragment for the vnd,interrupt compatible:
+ *
+ *     interrupt-cells:
+ *       - irq
+ *       - sense
+ *       - priority
+ *
+ * Example usage:
+ *
+ *     DT_INTC_FLAGS_BY_IDX(DT_NODELABEL(n), 0) // ((2 << 8) | (15 << 0))
+ *     DT_INTC_FLAGS_BY_IDX(DT_NODELABEL(n), 1) // ((3 << 8) | (16 << 0))
+ *
+ * @param inst instance of an interrupt controller
+ * @return the flags cell value at index "idx", or zero if there is none
+ */
+#define DT_INTC_FLAGS_BY_IDX(inst, idx) \
+	((DT_INST_IRQ_BY_IDX(inst, idx, sense) << 8) \
+	| (DT_INST_IRQ_BY_IDX(inst, idx, priority)) << 0)
+
+/**
  * @}
  */
 
