190d11e6333d19575dd28a2554f2ef204ff0c0ba
Johann Fischer <johann.fischer@nordicsemi.no>
2024-11-22 12:56:57 +0100

    samples: hci_uart: fix UART IRQ API usage
    uart_irq_update() should always be called in IRQ processing callback.
    From API documentation:
    "This function should be called the first thing in the ISR. Calling
    uart_irq_rx_ready(), uart_irq_tx_ready(), uart_irq_tx_complete()
    allowed only after this."
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- samples/bluetooth/hci_uart/src/main.c
+++ samples/bluetooth/hci_uart/src/main.c
@@ -232,17 +232,19 @@ static void bt_uart_isr(const struct device *unused, void *user_data)
 	ARG_UNUSED(unused);
 	ARG_UNUSED(user_data);
 
-	if (!(uart_irq_rx_ready(hci_uart_dev) ||
-	      uart_irq_tx_ready(hci_uart_dev))) {
-		LOG_DBG("spurious interrupt");
-	}
+	while (uart_irq_update(hci_uart_dev) && uart_irq_is_pending(hci_uart_dev)) {
+		if (!(uart_irq_rx_ready(hci_uart_dev) ||
+		      uart_irq_tx_ready(hci_uart_dev))) {
+			LOG_DBG("spurious interrupt");
+		}
 
-	if (uart_irq_tx_ready(hci_uart_dev)) {
-		tx_isr();
-	}
+		if (uart_irq_tx_ready(hci_uart_dev)) {
+			tx_isr();
+		}
 
-	if (uart_irq_rx_ready(hci_uart_dev)) {
-		rx_isr();
+		if (uart_irq_rx_ready(hci_uart_dev)) {
+			rx_isr();
+		}
 	}
 }
 
