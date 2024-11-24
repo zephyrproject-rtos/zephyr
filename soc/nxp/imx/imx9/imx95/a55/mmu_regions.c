38391bbeebc54cd23c770b1ab1e0557f663fd68f
Manuel Argüelles <manuel.arguelles@nxp.com>
2024-11-24 14:34:55 +0700

    dts: bindings: rename nxp,kinetis-lpuart compatible
    Rename "nxp,kinetis-lpuart" compatible to "nxp,lpuart" to remove the
    device family from its name.
    
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

--- soc/nxp/imx/imx9/imx95/a55/mmu_regions.c
+++ soc/nxp/imx/imx9/imx95/a55/mmu_regions.c
@@ -21,7 +21,7 @@ static const struct arm_mmu_region mmu_regions[] = {
 	MMU_REGION_DT_COMPAT_FOREACH_FLAT_ENTRY(nxp_mbox_imx_mu,
 						(MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS))
 
-	MMU_REGION_DT_COMPAT_FOREACH_FLAT_ENTRY(nxp_kinetis_lpuart,
+	MMU_REGION_DT_COMPAT_FOREACH_FLAT_ENTRY(nxp_lpuart,
 						(MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_NS))
 
 };
