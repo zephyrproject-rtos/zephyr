4881ff9df34d14da78aacea063a82fd3d8f99e8c
Tomas Choat <trc@ixys.no>
2024-11-26 10:30:37 +0100

    net: lib: ptp: fix cpp compilation errors
    Compilation errors in ptp library when using C++ compiler.
    First error is due to 'class' keyword being reserved in C++
    and 'class' is used as a variable name in ptp library.
    Second error is due to a flexible array member in a struct
    is not placed at the end.
    
    Signed-off-by: Tomas Choat <trc@ixys.no>

--- subsys/net/lib/ptp/clock.c
+++ subsys/net/lib/ptp/clock.c
@@ -262,7 +262,7 @@ const struct ptp_clock *ptp_clock_init(void)
 	dds->n_ports = 0;
 	dds->time_receiver_only = IS_ENABLED(CONFIG_PTP_TIME_RECEIVER_ONLY) ? true : false;
 
-	dds->clk_quality.class = dds->time_receiver_only ? 255 : 248;
+	dds->clk_quality.cls = dds->time_receiver_only ? 255 : 248;
 	dds->clk_quality.accuracy = CONFIG_PTP_CLOCK_ACCURACY;
 	/* 0xFFFF means that value has not been computed - IEEE 1588-2019 7.6.3.3 */
 	dds->clk_quality.offset_scaled_log_variance = 0xFFFF;
