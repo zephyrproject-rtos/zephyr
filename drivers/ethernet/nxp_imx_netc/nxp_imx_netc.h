f0e106900fd11624910a988f71901aad49a0c0c2
Yangbo Lu <yangbo.lu@nxp.com>
2024-11-26 17:07:09 +0800

    drivers: ethernet: add DSA driver for NXP i.MX NETC
    NXP i.MX NETC is a TSN capable Ethernet IP. It may support
    ENETC MACs, or/and multiple ports switch.
    
    The ENETC MAC was handled by eth_nxp_imx_netc driver.
    This DSA driver is to support NETC switch device.
    Current driver supports DSA with limitation that only switch
    function is available without management via DSA master port.
    DSA master port support is TODO work.
    
    Take i.MX RT1180 NETC hardware as an example.
    
                    +--------+                  +--------+
                    | ENETC1 |                  | ENETC0 |
                    |        |                  |        |
                    | Pseudo |                  |  1G    |
                    |  MAC   |                  |  MAC   |
                    +--------+                  +--------+
                        | zero copy interface       |
    +-------------- +--------+----------------+     |
    |               | Pseudo |                |     |
    |               |  MAC   |                |     |
    |               |        |                |     |
    |               | Port 4 |                |     |
    |               +--------+                |     |
    |           SWITCH       CORE             |     |
    +--------+ +--------+ +--------+ +--------+     |
    | Port 0 | | Port 1 | | Port 2 | | Port 3 |     |
    |        | |        | |        | |        |     |
    |  1G    | |  1G    | |  1G    | |  1G    |     |
    |  MAC   | |  MAC   | |  MAC   | |  MAC   |     |
    +--------+-+--------+-+--------+-+--------+     |
        |          |          |          |          |
    NETC External Interfaces (4 switch ports, 1 end-point port)
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

---
+++ drivers/ethernet/nxp_imx_netc/nxp_imx_netc.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2025 NXP
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __NXP_IMX_NETC_H__
+#define __NXP_IMX_NETC_H__
+
+#define NETC_BD_ALIGN 128
+
+/* Get phy mode from dts. Default RMII for i.MXRT1180 ENETC which hasn't added the property. */
+#define NETC_PHY_MODE(node_id)                                                                     \
+	(DT_ENUM_HAS_VALUE(node_id, phy_connection_type, mii)                                      \
+		 ? kNETC_MiiMode                                                                   \
+		 : (DT_ENUM_HAS_VALUE(node_id, phy_connection_type, rmii)                          \
+			    ? kNETC_RmiiMode                                                       \
+			    : (DT_ENUM_HAS_VALUE(node_id, phy_connection_type, rgmii)              \
+				       ? kNETC_RgmiiMode                                           \
+				       : (DT_ENUM_HAS_VALUE(node_id, phy_connection_type, gmii)    \
+						  ? kNETC_GmiiMode                                 \
+						  : kNETC_RmiiMode))))
+
+/* Helper macros to convert from Zephyr PHY speed to NETC speed/duplex types */
+#define PHY_TO_NETC_SPEED(x)                                                                       \
+	(PHY_LINK_IS_SPEED_1000M(x)                                                                \
+		 ? kNETC_MiiSpeed1000M                                                             \
+		 : (PHY_LINK_IS_SPEED_100M(x) ? kNETC_MiiSpeed100M : kNETC_MiiSpeed10M))
+
+#define PHY_TO_NETC_DUPLEX_MODE(x)                                                                 \
+	(PHY_LINK_IS_FULL_DUPLEX(x) ? kNETC_MiiFullDuplex : kNETC_MiiHalfDuplex)
+
+#endif
