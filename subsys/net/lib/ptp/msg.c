dcd74cb5b3e6c501d0f5000d586da2d839bfb74d
Tomas Choat <trc@ixys.no>
2024-11-26 10:43:22 +0100

    net: lib: ptp: fix follow up msg bug
    When sending follow up messages, the pre send function
    does not insert the correct time stamp in the message.
    Issue is fixed by calling the pre send timestamp function.
    
    Signed-off-by: Tomas Choat <trc@ixys.no>

--- subsys/net/lib/ptp/msg.c
+++ subsys/net/lib/ptp/msg.c
@@ -328,6 +328,7 @@ void ptp_msg_pre_send(struct ptp_msg *msg)
 		msg_port_id_pre_send(&msg->pdelay_resp.req_port_id);
 		break;
 	case PTP_MSG_FOLLOW_UP:
+		msg_timestamp_pre_send(&msg->follow_up.precise_origin_timestamp);
 		break;
 	case PTP_MSG_DELAY_RESP:
 		msg_timestamp_pre_send(&msg->delay_resp.receive_timestamp);
