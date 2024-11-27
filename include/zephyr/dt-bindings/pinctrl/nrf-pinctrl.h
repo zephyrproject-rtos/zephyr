6cb5fc1444d8c1d374dffcf89a6237f8c6d0c2d1
Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>
2024-11-27 16:00:56 +0100

    drivers: pinctrl: nrf: Add support for TDM peripheral
    Add support for configuring pins of the nRF TDM peripheral.
    
    Signed-off-by: Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>

--- include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h
+++ include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h
@@ -172,6 +172,20 @@
 #define NRF_FUN_GRTC_CLKOUT_FAST 55U
 /** GRTC slow clock output */
 #define NRF_FUN_GRTC_CLKOUT_32K  56U
+/** TDM SCK in master mode */
+#define NRF_FUN_TDM_SCK_M        71U
+/** TDM SCK in slave mode */
+#define NRF_FUN_TDM_SCK_S        72U
+/** TDM LRCK in master mode */
+#define NRF_FUN_TDM_FSYNC_M      73U
+/** TDM LRCK in slave mode */
+#define NRF_FUN_TDM_FSYNC_S      74U
+/** TDM SDIN */
+#define NRF_FUN_TDM_SDIN         75U
+/** TDM SDOUT */
+#define NRF_FUN_TDM_SDOUT        76U
+/** TDM MCK */
+#define NRF_FUN_TDM_MCK          77U
 
 /** @} */
 
