f483616be0051f5e8e34bedd013fd1f45249bb89
Andy Ross <andyross@google.com>
2024-11-26 17:45:12 -0800

    soc/mediatek: Ruffify python scripts
    These in-tree scripts fail the new ruff checks.  Clean things up so
    modifications can merge.
    
    Signed-off-by: Andy Ross <andyross@google.com>

--- soc/mediatek/mt8xxx/gen_img.py
+++ soc/mediatek/mt8xxx/gen_img.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # Copyright 2023 The ChromiumOS Authors
 # SPDX-License-Identifier: Apache-2.0
-import sys
 import struct
+import sys
+
 import elftools.elf.elffile
 import elftools.elf.sections
 
@@ -19,12 +20,10 @@ import elftools.elf.sections
 #
 # No padding or uninterpreted bytes.
 
-FILE_MAGIC = 0xe463be95
+FILE_MAGIC = 0xE463BE95
 
 elf_file = sys.argv[1]
 out_file = sys.argv[2]
-
-ef = elftools.elf.elffile.ELFFile(open(elf_file, "rb"))
 
 sram = bytearray()
 dram = bytearray()
@@ -34,11 +33,13 @@ dram = bytearray()
 # SOCs, but it's always a <=1M region in 0x4xxxxxxx.  Just use what we
 # get, but validate that it fits.
 sram_block = 0
+
+
 def sram_off(addr):
     global sram_block
     if addr < 0x40000000 or addr >= 0x50000000:
         return -1
-    block = addr & ~0xfffff
+    block = addr & ~0xFFFFF
     assert sram_block in (0, block)
 
     sram_block = block
@@ -46,44 +47,53 @@ def sram_off(addr):
     assert off < 0x100000
     return off
 
+
 # Similar heuristics: current platforms put DRAM either at 0x60000000
 # or 0x90000000 with no more than 16M of range
 def dram_off(addr):
-    if (addr >> 28 not in [6, 9]) or (addr & 0x0f000000 != 0):
+    if (addr >> 28 not in [6, 9]) or (addr & 0x0F000000 != 0):
         return -1
-    return addr & 0xffffff
+    return addr & 0xFFFFFF
 
-for seg in ef.iter_segments():
-    h = seg.header
-    if h.p_type == "PT_LOAD":
-        soff = sram_off(h.p_paddr)
-        doff = dram_off(h.p_paddr)
-        if soff >= 0:
-            buf = sram
-            off = soff
-        elif doff >= 0:
-            buf = dram
-            off = doff
-        else:
-            print(f"Invalid PT_LOAD address {h.p_paddr:x}")
-            sys.exit(1)
 
-        dat = seg.data()
-        end = off + len(dat)
-        if end > len(buf):
-            buf.extend(b'\x00' * (end - len(buf)))
+def read_elf(efile):
+    ef = elftools.elf.elffile.ELFFile(efile)
 
-        # pylint: disable=consider-using-enumerate
-        for i in range(len(dat)):
-            buf[i + off] = dat[i]
+    for seg in ef.iter_segments():
+        h = seg.header
+        if h.p_type == "PT_LOAD":
+            soff = sram_off(h.p_paddr)
+            doff = dram_off(h.p_paddr)
+            if soff >= 0:
+                buf = sram
+                off = soff
+            elif doff >= 0:
+                buf = dram
+                off = doff
+            else:
+                print(f"Invalid PT_LOAD address {h.p_paddr:x}")
+                sys.exit(1)
 
-for sec in ef.iter_sections():
-    if isinstance(sec, elftools.elf.sections.SymbolTableSection):
-        for sym in sec.iter_symbols():
-            if sym.name == "mtk_adsp_boot_entry":
-                boot_vector = sym.entry['st_value']
+            dat = seg.data()
+            end = off + len(dat)
+            if end > len(buf):
+                buf.extend(b'\x00' * (end - len(buf)))
 
-of = open(out_file, "wb")
-of.write(struct.pack("<III", FILE_MAGIC, len(sram), boot_vector))
-of.write(sram)
-of.write(dram)
+            # pylint: disable=consider-using-enumerate
+            for i in range(len(dat)):
+                buf[i + off] = dat[i]
+
+    for sec in ef.iter_sections():
+        if isinstance(sec, elftools.elf.sections.SymbolTableSection):
+            for sym in sec.iter_symbols():
+                if sym.name == "mtk_adsp_boot_entry":
+                    boot_vector = sym.entry['st_value']
+
+    with open(out_file, "wb") as of:
+        of.write(struct.pack("<III", FILE_MAGIC, len(sram), boot_vector))
+        of.write(sram)
+        of.write(dram)
+
+
+with open(elf_file, "rb") as f:
+    read_elf(f)
