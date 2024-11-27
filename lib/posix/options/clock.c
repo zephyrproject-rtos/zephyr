61cec5855809202bfaaaba2320b348a8aa2bedbd
Ilya Tagunov <Ilya.Tagunov@synopsys.com>
2024-11-27 07:10:23 +0000

    posix: clock: partially reformat __z_clock_nanosleep
    Apply clang-format to the offending lines to make it happy.
    
    Signed-off-by: Ilya Tagunov <Ilya.Tagunov@synopsys.com>

--- lib/posix/options/clock.c
+++ lib/posix/options/clock.c
@@ -216,11 +216,9 @@ static int __z_clock_nanosleep(clockid_t clock_id, int flags, const struct times
 		return -1;
 	}
 
-	if ((flags & TIMER_ABSTIME) == 0 &&
-	    unlikely(rqtp->tv_sec >= ULLONG_MAX / NSEC_PER_SEC)) {
-
-		ns = rqtp->tv_nsec + NSEC_PER_SEC
-			+ (uint64_t)k_sleep(K_SECONDS(rqtp->tv_sec - 1)) * NSEC_PER_MSEC;
+	if ((flags & TIMER_ABSTIME) == 0 && unlikely(rqtp->tv_sec >= ULLONG_MAX / NSEC_PER_SEC)) {
+		ns = rqtp->tv_nsec + NSEC_PER_SEC +
+		     (uint64_t)k_sleep(K_SECONDS(rqtp->tv_sec - 1)) * NSEC_PER_MSEC;
 	} else {
 		ns = (uint64_t)rqtp->tv_sec * NSEC_PER_SEC + rqtp->tv_nsec;
 	}
