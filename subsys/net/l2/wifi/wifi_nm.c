b5007b51904d26ef6d01a07a596aa3fbdc2998ec
Maochen Wang <maochen.wang@nxp.com>
2024-11-28 11:37:17 +0800

    net: wifi: avoid adding duplicate mgd_ifaces
    Add iface and type check to avoid adding duplicate mgd_ifaces.
    
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- subsys/net/l2/wifi/wifi_nm.c
+++ subsys/net/l2/wifi/wifi_nm.c
@@ -115,6 +115,12 @@ int wifi_nm_register_mgd_type_iface(struct wifi_nm_instance *nm,
 
 	k_mutex_lock(&wifi_nm_lock, K_FOREVER);
 	for (int i = 0; i < CONFIG_WIFI_NM_MAX_MANAGED_INTERFACES; i++) {
+		if ((nm->mgd_ifaces[i].iface == iface)
+			&& (nm->mgd_ifaces[i].type == BIT(type))) {
+			k_mutex_unlock(&wifi_nm_lock);
+			return 0;
+		}
+
 		if (!nm->mgd_ifaces[i].iface) {
 			nm->mgd_ifaces[i].iface = iface;
 			nm->mgd_ifaces[i].type = BIT(type);
