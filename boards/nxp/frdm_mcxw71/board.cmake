46c4a9b7fe6c4ca8fc0ae33f27458be2cb75c5ba
Andrej Butok <andrey.butok@nxp.com>
2024-12-02 14:02:53 +0100

    boards: nxp: frdm_mcxw71: Add LinkServer runner support
    Adds the LinkServer runner support for the FRDM-MCXW71 board.
    
    Signed-off-by: Andrej Butok <andrey.butok@nxp.com>

--- boards/nxp/frdm_mcxw71/board.cmake
+++ boards/nxp/frdm_mcxw71/board.cmake
@@ -1,6 +1,8 @@
 # Copyright 2024 NXP
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(linkserver "--device=MCXW716CxxxA:FRDM-MCXW71")
 board_runner_args(jlink "--device=mcxw716" "--reset-after-load")
 
+include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
