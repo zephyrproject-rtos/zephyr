edf996112c9312f4bedf82021da9750f39233033
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2024-12-02 09:50:28 +0100

    [nrf fromlist] drivers: clock_control_nrf2_common: Add resetting of the on-off services
    If an on-off service records an error while transitioning to the on
    state, it stays in the error state and does not allow new requests
    to be made until its state is reset. Add resetting of the services
    associated with particular clock controllers so that requests can
    be retried after failures.
    
    Upstream PR #: 89952
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- drivers/clock_control/clock_control_nrf2_hsfll.c
+++ drivers/clock_control/clock_control_nrf2_hsfll.c
@@ -140,7 +140,7 @@ static int api_request_hsfll(const struct device *dev,
 	struct onoff_manager *mgr = hsfll_find_mgr(dev, spec);
 
 	if (mgr) {
-		return onoff_request(mgr, cli);
+		return clock_config_request(mgr, cli);
 	}
 
 	return -EINVAL;
