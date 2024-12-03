79e63e1cfd576ec596ba7fa4468cb85d0d65b896
Armin Kessler <ake@espros.com>
2024-12-03 13:08:39 +0100

    scripts: twister: add esp32 runner
    Adds support for the `esp32` runner when using the hardware-map feature.
    
    Signed-off-by: Armin Kessler <ake@espros.com>

--- scripts/pylib/twister/twisterlib/handlers.py
+++ scripts/pylib/twister/twisterlib/handlers.py
@@ -566,10 +566,14 @@ class DeviceHandler(Handler):
 
                 board_id = hardware.probe_id or hardware.id
                 product = hardware.product
+                serial_port = hardware.serial
                 if board_id is not None:
                     if runner in ("pyocd", "nrfjprog", "nrfutil"):
                         command_extra_args.append("--dev-id")
                         command_extra_args.append(board_id)
+                    elif runner == "esp32":
+                        command_extra_args.append("--esp-device")
+                        command_extra_args.append(serial_port)
                     elif (
                         runner == "openocd"
                         and product == "STM32 STLink"
