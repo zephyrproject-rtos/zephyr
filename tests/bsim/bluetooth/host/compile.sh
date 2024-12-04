fcee29d051a34a1173b102473e90958227bcd568
Artur Dobrynin <artur.dobrynin@nordicsemi.no>
2024-12-04 10:37:33 +0100

    tests: bsim: bluetooth: host: use twister to compile tests
    Bluetooth host bsim tests are compiled with shell scripts, but it is
    possible to use twister, which supports compilation (but not running
    at the moment). Compiling with twister requires fewer shell scripts,
    provides cleaner interface and more flexible configuration.
    
    Signed-off-by: Artur Dobrynin <artur.dobrynin@nordicsemi.no>

--- tests/bsim/bluetooth/host/compile.sh
+++ tests/bsim/bluetooth/host/compile.sh
@@ -9,37 +9,7 @@ set -ue
 
 : "${ZEPHYR_BASE:?ZEPHYR_BASE must be set to point to the zephyr root directory}"
 
-source ${ZEPHYR_BASE}/tests/bsim/compile.source
+#Set a default value to BOARD if it does not have one yet
+BOARD="${BOARD:-nrf52_bsim/native}"
 
-${ZEPHYR_BASE}/tests/bsim/bluetooth/host/adv/compile.sh
-app=tests/bsim/bluetooth/host/central compile
-${ZEPHYR_BASE}/tests/bsim/bluetooth/host/att/compile.sh
-${ZEPHYR_BASE}/tests/bsim/bluetooth/host/gatt/compile.sh
-${ZEPHYR_BASE}/tests/bsim/bluetooth/host/l2cap/compile.sh
-${ZEPHYR_BASE}/tests/bsim/bluetooth/host/security/compile.sh
-
-app=tests/bsim/bluetooth/host/iso/cis compile
-app=tests/bsim/bluetooth/host/iso/bis compile
-app=tests/bsim/bluetooth/host/iso/frag compile
-app=tests/bsim/bluetooth/host/iso/frag_2 compile
-
-app=tests/bsim/bluetooth/host/misc/disable compile
-run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/misc/disconnect/compile.sh
-app=tests/bsim/bluetooth/host/misc/conn_stress/central compile
-app=tests/bsim/bluetooth/host/misc/conn_stress/peripheral compile
-run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/misc/hfc/compile.sh
-run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/misc/hfc_multilink/compile.sh
-app=tests/bsim/bluetooth/host/misc/unregister_conn_cb compile
-run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/misc/sample_test/compile.sh
-run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/misc/acl_tx_frag/compile.sh
-
-run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/privacy/central/compile.sh
-run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/privacy/peripheral/compile.sh
-run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/privacy/device/compile.sh
-run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/privacy/legacy/compile.sh
-
-run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/id/settings/compile.sh
-run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/scan/start_stop/compile.sh
-run_in_background ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/scan/slow/compile.sh
-
-wait_for_background_jobs
+west twister -T ${ZEPHYR_BASE}/tests/bsim/bluetooth/host/ -p ${BOARD}
