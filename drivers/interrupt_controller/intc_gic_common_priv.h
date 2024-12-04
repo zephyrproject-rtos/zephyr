6be456faad4fd3a50122c2650ebfd2490b88d2a0
Adam Openshaw <quic_adamo@quicinc.com>
2024-12-03 17:27:58 -0800

    drivers: gicv3: Add Support for Extended SPI
    Added support to gicv3 driver to utilize the
    extended SPI MMIO registers introduced in
    GICv3.1 for the extended SPI range.
    
    Documentation for the Extended Shared
    Peripheral Interrupts extension can be found
    in the ARM General Interrupt Controller
    Architecture Specification:
    
    https://developer.arm.com/documentation/ihi0069/latest/
    
    Signed-off-by: Adam Openshaw <quic_adamo@quicinc.com>

--- drivers/interrupt_controller/intc_gic_common_priv.h
+++ drivers/interrupt_controller/intc_gic_common_priv.h
@@ -21,6 +21,20 @@
 #define GIC_DIST_IGROUPMODR 0x0d00
 #define GIC_DIST_SGIR       0x0f00
 
+/* GICv3.1 Support for Extended SPI Range */
+#define GIC_ESPI_START        4096
+#define GIC_ESPI_END          5119
+#define GIC_DIST_IGROUPRnE    0x1000
+#define GIC_DIST_ISENABLERnE  0x1200
+#define GIC_DIST_ICENABLERnE  0x1400
+#define GIC_DIST_ISPENDRnE    0x1600
+#define GIC_DIST_ICPENDRnE    0x1800
+#define GIC_DIST_ISACTIVERnE  0x1a00
+#define GIC_DIST_ICACTIVERnE  0x1c00
+#define GIC_DIST_IPRIORITYRnE 0x2000
+#define GIC_DIST_ICFGRnE      0x3000
+#define GIC_DIST_IGROUPMODRnE 0x3400
+
 /* GICD GICR common access macros */
 #define IGROUPR(base, n)    (base + GIC_DIST_IGROUPR + (n) * 4)
 #define ISENABLER(base, n)  (base + GIC_DIST_ISENABLER + (n) * 4)
@@ -32,6 +46,16 @@
 #define ICFGR(base, n)      (base + GIC_DIST_ICFGR + (n) * 4)
 #define IGROUPMODR(base, n) (base + GIC_DIST_IGROUPMODR + (n) * 4)
 
+/* GICD Extended SPI (GICv3.1) common access macros */
+#define IGROUPRnE(base, n)    (base + GIC_DIST_IGROUPRnE + (n) * 4)
+#define ISENABLERnE(base, n)  (base + GIC_DIST_ISENABLERnE + (n) * 4)
+#define ICENABLERnE(base, n)  (base + GIC_DIST_ICENABLERnE + (n) * 4)
+#define ISPENDRnE(base, n)    (base + GIC_DIST_ISPENDRnE + (n) * 4)
+#define ICPENDRnE(base, n)    (base + GIC_DIST_ICPENDRnE + (n) * 4)
+#define IPRIORITYRnE(base, n) (base + GIC_DIST_IPRIORITYRnE + n)
+#define ICFGRnE(base, n)      (base + GIC_DIST_ICFGRnE + (n) * 4)
+#define IGROUPMODRnE(base, n) (base + GIC_DIST_IGROUPMODRnE + (n) * 4)
+
 /*
  * selects redistributor SGI_base for current core for PPI and SGI
  * selects distributor base for SPI
@@ -40,8 +64,10 @@
 
 #if CONFIG_GIC_VER <= 2
 #define GET_DIST_BASE(intid) GIC_DIST_BASE
+#define GIC_IS_ESPI(intid)   0
 #else
 #define GET_DIST_BASE(intid)                                                                       \
 	((intid < GIC_SPI_INT_BASE) ? (gic_get_rdist() + GICR_SGI_BASE_OFF) : GIC_DIST_BASE)
+#define GIC_IS_ESPI(intid) ((intid >= GIC_ESPI_START) && (intid <= GIC_ESPI_END))
 #endif
 #endif /* ZEPHYR_INCLUDE_DRIVERS_INTC_GIC_COMMON_PRIV_H */
