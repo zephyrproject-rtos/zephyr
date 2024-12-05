4b443ffd5c261dda160b34f23abff80b9afe89bb
Lingao Meng <menglingao@xiaomi.com>
2024-12-05 10:00:02 +0800

    tests: bsim: Add testcase for bluetooth mesh extended advertiser
    Add some configuration for test adv_ext.c
    
    Signed-off-by: Lingao Meng <menglingao@xiaomi.com>
    
    Signed-off-by: Lingao Meng <menglingao@xiaomi.com>

--- tests/bsim/bluetooth/mesh/tests_scripts/advertiser/reverse_order.sh
+++ tests/bsim/bluetooth/mesh/tests_scripts/advertiser/reverse_order.sh
@@ -10,5 +10,8 @@ RunTest mesh_adv_reverse_order adv_tx_reverse_order adv_rx_receive_order
 overlay=overlay_workq_sys_conf
 RunTest mesh_adv_reverse_order_workq adv_tx_reverse_order adv_rx_receive_order
 
+overlay="overlay_multi_adv_sets_conf"
+RunTest mesh_adv_reverse_order_multi_adv_sets adv_tx_reverse_order adv_rx_receive_order
+
 overlay=overlay_psa_conf
 RunTest mesh_adv_reverse_order_psa adv_tx_reverse_order adv_rx_receive_order
