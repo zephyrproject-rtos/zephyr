c81c6bedb0ba54cdefaaf15fe3dbaea7bc1cd8be
Carles Cufi <carles.cufi@nordicsemi.no>
2024-12-05 16:08:36 +0100

    boards: nordic: nRF54L15DK: Add debug configuration for FLPR
    Add the required code for `west debug` to work the FLPR
    core over JLink in the nRF54L 05, 10 and 15 devices.
    
    Note that this requries an external J-Link probe, it will not work with
    the on-board (OB) probe soldered on the DK.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- boards/nordic/nrf54l15dk/board.cmake
+++ boards/nordic/nrf54l15dk/board.cmake
@@ -6,7 +6,8 @@ if(CONFIG_SOC_NRF54L05_CPUAPP OR CONFIG_SOC_NRF54L10_CPUAPP OR
   board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
 elseif(CONFIG_SOC_NRF54L05_CPUFLPR OR CONFIG_SOC_NRF54L10_CPUFLPR OR
 		CONFIG_SOC_NRF54L15_CPUFLPR)
-  board_runner_args(jlink "--speed=4000")
+  set(JLINKSCRIPTFILE ${CMAKE_CURRENT_LIST_DIR}/support/nrf54l_05_10_15_cpuflpr.JLinkScript)
+  board_runner_args(jlink "--device=RISC-V" "--speed=4000" "-if SW" "--tool-opt=-jlinkscriptfile ${JLINKSCRIPTFILE}")
 endif()
 
 include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
