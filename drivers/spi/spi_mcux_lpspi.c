f868ca3f293b341f74da9e9d5943f33a3e894e5f
Declan Snyder <declan.snyder@nxp.com>
2024-12-05 13:37:44 -0600

    dts: bindings: nxp,lpspi: Add fifo size property
    Add properties for describing RX and TX fifo sizes.
    
    Signed-off-by: Declan Snyder <declan.snyder@nxp.com>

--- drivers/spi/spi_mcux_lpspi.c
+++ drivers/spi/spi_mcux_lpspi.c
@@ -70,6 +70,8 @@ struct spi_mcux_config {
 	uint32_t transfer_delay;
 	const struct pinctrl_dev_config *pincfg;
 	lpspi_pin_config_t data_pin_config;
+	uint8_t tx_fifo_size;
+	uint8_t rx_fifo_size;
 };
 
 struct spi_mcux_data {
@@ -877,6 +879,8 @@ static int spi_mcux_init(const struct device *dev)
 					   DT_INST_PROP(n, transfer_delay)),                       \
 		.pincfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),                                       \
 		.data_pin_config = DT_INST_ENUM_IDX(n, data_pin_config),                           \
+		.rx_fifo_size = (uint8_t)DT_INST_PROP(n, rx_fifo_size),                            \
+		.tx_fifo_size = (uint8_t)DT_INST_PROP(n, tx_fifo_size),                            \
 	};                                                                                         \
                                                                                                    \
 	static struct spi_mcux_data spi_mcux_data_##n = {                                          \
