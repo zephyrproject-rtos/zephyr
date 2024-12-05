cc014202c1610abbc2cb3e953b831d4ab9c64f3e
Lingao Meng <menglingao@xiaomi.com>
2024-12-05 10:00:02 +0800

    tests: bsim: Add testcase for bluetooth mesh extended advertiser
    Add some configuration for test adv_ext.c
    
    Signed-off-by: Lingao Meng <menglingao@xiaomi.com>
    
    Signed-off-by: Lingao Meng <menglingao@xiaomi.com>

--- tests/bsim/bluetooth/mesh/tests_scripts/advertiser/tx_cb_multi.sh
+++ tests/bsim/bluetooth/mesh/tests_scripts/advertiser/tx_cb_multi.sh
@@ -10,5 +10,8 @@ RunTest mesh_adv_tx_cb_multi adv_tx_cb_multi
 overlay=overlay_workq_sys_conf
 RunTest mesh_adv_tx_cb_multi_workq adv_tx_cb_multi
 
+overlay="overlay_multi_adv_sets_conf"
+RunTest mesh_adv_tx_cb_multi_multi_adv_sets adv_tx_cb_multi
+
 overlay=overlay_psa_conf
 RunTest mesh_adv_tx_cb_multi_psa adv_tx_cb_multi
