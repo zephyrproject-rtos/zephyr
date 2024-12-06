79ccfb17a3383a1e5accad6f4b7302eb016dc726
Gang Li <gang.li_1@nxp.com>
2024-12-06 07:13:17 +0100

    [nxp fromtree] net: wifi: add "bandwidth" parameter to "wifi ap enable" command
    Add "bandwidth" parameter to "wifi ap enable" command.
    Add "ht_capab" and "vht_capab" parameters to "wifi ap config" command.
    
    Signed-off-by: Gang Li <gang.li_1@nxp.com>
    
    (cherry picked from upstream PR: zephyrproject-rtos/zephyr#82113)

--- include/zephyr/net/wifi_mgmt.h
+++ include/zephyr/net/wifi_mgmt.h
@@ -579,6 +579,8 @@ struct wifi_connect_req_params {
 	uint8_t domain_match_length; /* Max 64 */
 	/** Hidden SSID configure */
 	uint8_t ignore_broadcast_ssid;
+	/** Parameter used for frequency band */
+	enum wifi_frequency_bandwidths bandwidth;
 };
 
 /** @brief Wi-Fi connect result codes. To be overlaid on top of \ref wifi_status
@@ -1008,6 +1010,7 @@ struct wifi_channel_info {
 
 /** @cond INTERNAL_HIDDEN */
 #define WIFI_AP_STA_MAX_INACTIVITY (LONG_MAX - 1)
+#define WIFI_AP_IEEE_80211_CAPAB_MAX_LEN 64
 /** @endcond */
 
 /** @brief Wi-Fi AP configuration parameter */
@@ -1018,6 +1021,14 @@ struct wifi_ap_config_params {
 	uint32_t max_inactivity;
 	/** Parameter used for setting maximum number of stations */
 	uint32_t max_num_sta;
+	/** Parameter used for frequency band */
+	enum wifi_frequency_bandwidths bandwidth;
+#if defined(CONFIG_WIFI_NM_HOSTAPD_AP)
+	/** Parameter used for setting HT capabilities */
+	char ht_capab[WIFI_AP_IEEE_80211_CAPAB_MAX_LEN + 1];
+	/** Parameter used for setting VHT capabilities */
+	char vht_capab[WIFI_AP_IEEE_80211_CAPAB_MAX_LEN + 1];
+#endif
 };
 
 #ifdef CONFIG_WIFI_NM_WPA_SUPPLICANT_DPP
