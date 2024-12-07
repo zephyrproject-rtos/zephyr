804bed7335b4cb5bd6f8425093c27d4c012392e6
David Leach <david.leach@nxp.com>
2024-12-06 20:21:10 -0600

    [nxp toup] boards: frdm_rw612: move TF-M NS board support
    Move the TF-M board support for ns targets from nxp-zsdk.
    
    Signed-off-by: David Leach <david.leach@nxp.com>

--- boards/nxp/frdm_rw612/board.cmake
+++ boards/nxp/frdm_rw612/board.cmake
@@ -7,3 +7,8 @@ board_runner_args(linkserver  "--device=RW612:FRDM-RW612")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
+
+if(CONFIG_BUILD_WITH_TFM)
+  # Flash merged TF-M + Zephyr binary
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
+endif()
