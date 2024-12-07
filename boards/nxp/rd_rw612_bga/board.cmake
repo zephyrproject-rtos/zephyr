54f83285476b42bb000cdb8a4fd68a9b111c73b3
David Leach <david.leach@nxp.com>
2024-12-06 20:51:19 -0600

    [nxp toup] boards: rd_rw612_bga: move TFM support from nxp-zsdk
    Move the TFM support files from the nxp-zsdk rd_rw612_bga board
    folder.
    
    Signed-off-by: David Leach <david.leach@nxp.com>

--- boards/nxp/rd_rw612_bga/board.cmake
+++ boards/nxp/rd_rw612_bga/board.cmake
@@ -1,4 +1,4 @@
-# Copyright 2022-2023 NXP
+# Copyright 2022-2024 NXP
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--device=RW612" "--reset-after-load")
@@ -7,3 +7,8 @@ board_runner_args(linkserver  "--device=RW612:RDRW612")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
+
+if(CONFIG_BUILD_WITH_TFM)
+  # Flash merged TF-M + Zephyr binary
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
+endif()
