9b0809c8e4ad8bf9e531cae8a894e35edf16f62d
Etienne Raquin <eraquin@kalrayinc.com>
2024-12-09 17:36:50 +0100

    drivers: can: stm32: fdcan: Fix IRQ connect by name on multi level
    Fix STM32 canfd driver interrupt connect by name when using multi level
    interrupts, by using new DT_INST_IRQN_BY_NAME helper macro
    
    Signed-off-by: Etienne Raquin <eraquin@kalrayinc.com>

--- drivers/can/can_stm32_fdcan.c
+++ drivers/can/can_stm32_fdcan.c
@@ -560,14 +560,14 @@ static const struct can_mcan_ops can_stm32fd_ops = {
 static void config_can_##inst##_irq(void)                                      \
 {                                                                              \
 	LOG_DBG("Enable CAN" #inst " IRQ");                                    \
-	IRQ_CONNECT(DT_INST_IRQ_BY_NAME(inst, int0, irq),                      \
+	IRQ_CONNECT(DT_INST_IRQN_BY_NAME(inst, int0),                          \
 		    DT_INST_IRQ_BY_NAME(inst, int0, priority),                 \
 		    can_mcan_line_0_isr, DEVICE_DT_INST_GET(inst), 0);         \
-	irq_enable(DT_INST_IRQ_BY_NAME(inst, int0, irq));                      \
-	IRQ_CONNECT(DT_INST_IRQ_BY_NAME(inst, int1, irq),                      \
+	irq_enable(DT_INST_IRQN_BY_NAME(inst, int0));                          \
+	IRQ_CONNECT(DT_INST_IRQN_BY_NAME(inst, int1),                          \
 		    DT_INST_IRQ_BY_NAME(inst, int1, priority),                 \
 		    can_mcan_line_1_isr, DEVICE_DT_INST_GET(inst), 0);         \
-	irq_enable(DT_INST_IRQ_BY_NAME(inst, int1, irq));                      \
+	irq_enable(DT_INST_IRQN_BY_NAME(inst, int1));                          \
 }
 
 #define CAN_STM32FD_CFG_INST(inst)					\
