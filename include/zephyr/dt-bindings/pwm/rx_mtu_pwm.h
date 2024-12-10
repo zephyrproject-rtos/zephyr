0f92f30f04f3f2da7d88d3ebf9b56ed16b3e92bc
Phi Tran <phi.tran.jg@bp.renesas.com>
2024-12-10 18:39:34 +0700

    drivers: pwm: Add support for PWM driver on RSK_RX130_512KB
    This is initial commit to support PWM driver
    on Renesas RX130 with MTU modules.
    
    Signed-off-by: Phi Tran <phi.tran.jg@bp.renesas.com>

---
+++ include/zephyr/dt-bindings/pwm/rx_mtu_pwm.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2025 Renesas Electronics Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_PWM_RX_MTU_PWM_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_PWM_RX_MTU_PWM_H_
+
+/* PWM SOURCE DIVIDER */
+#define RX_MTU_PWM_SOURCE_DIV_1  0
+#define RX_MTU_PWM_SOURCE_DIV_4  1
+#define RX_MTU_PWM_SOURCE_DIV_16 2
+#define RX_MTU_PWM_SOURCE_DIV_64 3
+
+/* PWM IO */
+#define RX_MTIOCxA 0
+#define RX_MTIOCxB 1
+#define RX_MTIOCxC 2
+#define RX_MTIOCxD 3
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_PWM_RX_MTU_PWM_H_ */
