264a72ce46b0d1bd3b150386637d3db0cd6bca5b
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2024-12-11 14:47:44 +0100

    devicetree: Add DT_PROP_LAST macro
    Add macro for getting the last element of an array type property.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- include/zephyr/devicetree.h
+++ include/zephyr/devicetree.h
@@ -899,6 +899,17 @@
 	DT_CAT5(node_id, _P_, prop, _IDX_, idx)
 
 /**
+ * @brief Get the last element of an array type property
+ *
+ * @param node_id node identifier
+ * @param prop lowercase-and-underscores property name
+ *
+ * @return a representation of the last element of the property
+ */
+#define DT_PROP_LAST(node_id, prop) \
+	DT_PROP_BY_IDX(node_id, prop, UTIL_DEC(DT_PROP_LEN(node_id, prop)))
+
+/**
  * @brief Like DT_PROP(), but with a fallback to @p default_value
  *
  * If the value exists, this expands to DT_PROP(node_id, prop).
