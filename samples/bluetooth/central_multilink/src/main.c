9b081f81e4cadf61387188035f17146909b955b8
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2024-12-13 05:36:32 +0100

    samples: Bluetooth: Update central multilink and peripheral identity
    Update central multilink sample to use continuous scanning
    to speed up connection setup.
    
    Update peripheral identity sample to be configurable for
    bsim testing as multiple devices with configurable count
    of supported connections.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- samples/bluetooth/central_multilink/src/main.c
+++ samples/bluetooth/central_multilink/src/main.c
@@ -8,10 +8,10 @@
 
 #include <stdint.h>
 
-int init_central(uint8_t iterations);
+int init_central(uint8_t max_conn, uint8_t iterations);
 
 int main(void)
 {
-	(void)init_central(CONFIG_SAMPLE_CONN_ITERATIONS);
+	(void)init_central(CONFIG_BT_MAX_CONN, CONFIG_SAMPLE_CONN_ITERATIONS);
 	return 0;
 }
