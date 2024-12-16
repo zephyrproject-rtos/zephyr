f4825033bc845c37348c7b61427212faa0f55879
Jordan Yates <jordan@embeint.com>
2024-12-16 13:39:37 +1000

    sensor: remove PM state checks from API functions
    Calling sensor API functions on devices not in `PM_DEVICE_STATE_ACTIVE`
    is a violation of the PM API. Adding manual checks inside of drivers
    complicates the drivers and increases ROM footprint for no additional
    benefit.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- drivers/sensor/bosch/bmp388/bmp388.c
+++ drivers/sensor/bosch/bmp388/bmp388.c
@@ -195,15 +195,6 @@ static int bmp388_attr_set(const struct device *dev,
 {
 	int ret;
 
-#ifdef CONFIG_PM_DEVICE
-	enum pm_device_state state;
-
-	(void)pm_device_state_get(dev, &state);
-	if (state != PM_DEVICE_STATE_ACTIVE) {
-		return -EBUSY;
-	}
-#endif
-
 	switch (attr) {
 #ifdef CONFIG_BMP388_ODR_RUNTIME
 	case SENSOR_ATTR_SAMPLING_FREQUENCY:
@@ -232,15 +223,6 @@ static int bmp388_sample_fetch(const struct device *dev,
 	int ret = 0;
 
 	__ASSERT_NO_MSG(chan == SENSOR_CHAN_ALL);
-
-#ifdef CONFIG_PM_DEVICE
-	enum pm_device_state state;
-
-	(void)pm_device_state_get(dev, &state);
-	if (state != PM_DEVICE_STATE_ACTIVE) {
-		return -EBUSY;
-	}
-#endif
 
 	pm_device_busy_set(dev);
 
