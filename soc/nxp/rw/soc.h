62d3baf0963b75d84fc256611b8679092c37e617
Xavier Razavet <xavier.razavet@nxp.com>
2024-12-17 09:54:15 +0100

    drivers: Narrow Band Unit interruption driver creation
    Creation of the new zephyr\soc\nxp\common\nxp_nbu.c driver which manage
    the interruption of the NBU. This modification is mandatory to support a
    coex application which includes Bluetooth and 802.15.4 on the same
    narrow band path.
    
    Signed-off-by: Xavier Razavet <xavier.razavet@nxp.com>

--- soc/nxp/rw/soc.h
+++ soc/nxp/rw/soc.h
@@ -16,11 +16,8 @@
 
 #endif /* !_ASMLANGUAGE */
 
-#define ble_hci_handler         BLE_MCI_WAKEUP0_DriverIRQHandler
-#define ble_wakeup_done_handler BLE_MCI_WAKEUP_DONE0_DriverIRQHandler
-
-#define hdlc_rcp_if_handler             BLE_MCI_WAKEUP0_DriverIRQHandler
-#define hdlc_rcp_if_wakeup_done_handler BLE_MCI_WAKEUP_DONE0_DriverIRQHandler
+#define nbu_handler             BLE_MCI_WAKEUP0_DriverIRQHandler
+#define nbu_wakeup_done_handler BLE_MCI_WAKEUP_DONE0_DriverIRQHandler
 
 /* Wrapper Function to deal with SDK differences in power API */
 static inline void EnableDeepSleepIRQ(IRQn_Type irq)
