c8a62a163ec91245ef32529eb1c54b64289ef4ed
Jilay Pandya <jilay.pandya@outlook.com>
2024-12-17 11:24:59 +0100

    tests: stepper_api: create a scaffold to generalize tests
    This commit creates a scaffold for executing stepper_api tests
    on the existing step_dir drivers.
    
    Signed-off-by: Jilay Pandya <jilay.pandya@outlook.com>

--- tests/drivers/stepper/stepper_api/src/main.c
+++ tests/drivers/stepper/stepper_api/src/main.c
@@ -7,6 +7,9 @@
 #include <zephyr/ztest.h>
 #include <zephyr/drivers/stepper.h>
 
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(stepper_api, CONFIG_STEPPER_LOG_LEVEL);
+
 struct stepper_fixture {
 	const struct device *dev;
 	stepper_event_callback_t callback;
@@ -19,7 +22,9 @@ void *user_data_received;
 static void stepper_print_event_callback(const struct device *dev, enum stepper_event event,
 					 void *user_data)
 {
+	const struct device *dev_callback = user_data;
 	user_data_received = user_data;
+
 	switch (event) {
 	case STEPPER_EVENT_STEPS_COMPLETED:
 		k_poll_signal_raise(&stepper_signal, STEPPER_EVENT_STEPS_COMPLETED);
@@ -36,12 +41,15 @@ static void stepper_print_event_callback(const struct device *dev, enum stepper_
 	default:
 		break;
 	}
+
+	LOG_DBG("Event %d, %s called for %s, expected for %s\n", event, __func__,
+	dev_callback->name, dev->name);
 }
 
 static void *stepper_setup(void)
 {
 	static struct stepper_fixture fixture = {
-		.dev = DEVICE_DT_GET(DT_NODELABEL(motor_1)),
+		.dev = DEVICE_DT_GET(DT_ALIAS(stepper)),
 		.callback = stepper_print_event_callback,
 	};
 
@@ -65,10 +73,10 @@ ZTEST_SUITE(stepper, NULL, stepper_setup, stepper_before, NULL, NULL);
 
 ZTEST_F(stepper, test_micro_step_res)
 {
-	(void)stepper_set_micro_step_res(fixture->dev, 2);
 	enum stepper_micro_step_resolution res;
 	(void)stepper_get_micro_step_res(fixture->dev, &res);
-	zassert_equal(res, 2, "Micro step resolution not set correctly");
+	zassert_equal(res, DT_PROP(DT_ALIAS(stepper), micro_step_res),
+		      "Micro step resolution not set correctly");
 }
 
 ZTEST_F(stepper, test_actual_position)
@@ -86,7 +94,7 @@ ZTEST_F(stepper, test_target_position)
 	(void)stepper_set_microstep_interval(fixture->dev, 10000);
 
 	/* Pass the function name as user data */
-	(void)stepper_set_event_callback(fixture->dev, fixture->callback, &fixture);
+	(void)stepper_set_event_callback(fixture->dev, fixture->callback, (void *)fixture->dev);
 
 	(void)stepper_move_to(fixture->dev, pos);
 
@@ -99,5 +107,5 @@ ZTEST_F(stepper, test_target_position)
 	zassert_equal(result, STEPPER_EVENT_STEPS_COMPLETED, "Signal not set");
 	(void)stepper_get_actual_position(fixture->dev, &pos);
 	zassert_equal(pos, 100u, "Target position should be %d but is %d", 100u, pos);
-	zassert_equal(user_data_received, &fixture, "User data not received");
+	zassert_equal(user_data_received, fixture->dev, "User data not received");
 }
