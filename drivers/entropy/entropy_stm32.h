80014246dc16043211ad1628a61765f81cc8eb54
Mathieu Choplain <mathieu.choplain@st.com>
2024-12-18 09:35:07 +0100

    drivers: entropy: stm32: add support for STM32WB09 TRNG
    Add support for the STM32WB09-specific TRNG IP in STM32 entropy driver.
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain@st.com>

--- drivers/entropy/entropy_stm32.h
+++ drivers/entropy/entropy_stm32.h
@@ -29,14 +29,23 @@ static inline void ll_rng_enable_it(RNG_TypeDef *RNGx)
 	/* Silence "unused" warning on IRQ-less hardware*/
 	ARG_UNUSED(RNGx);
 #if !IRQLESS_TRNG
-	LL_RNG_EnableIT(RNGx);
+#	if defined(CONFIG_SOC_STM32WB09XX)
+		LL_RNG_EnableEnErrorIrq(RNGx);
+		LL_RNG_EnableEnFfFullIrq(RNGx);
+#	else
+		LL_RNG_EnableIT(RNGx);
+#	endif
 #endif /* !IRQLESS_TRNG */
 }
 
 static inline uint32_t ll_rng_is_active_seis(RNG_TypeDef *RNGx)
 {
 #if defined(CONFIG_SOC_SERIES_STM32WB0X)
-	return LL_RNG_IsActiveFlag_FAULT(RNGx);
+#	if defined(CONFIG_SOC_STM32WB09XX)
+		return LL_RNG_IsActiveFlag_ENTROPY_ERR(RNGx);
+#	else
+		return LL_RNG_IsActiveFlag_FAULT(RNGx);
+#	endif
 #else
 	return LL_RNG_IsActiveFlag_SEIS(RNGx);
 #endif /* CONFIG_SOC_SERIES_STM32WB0X */
@@ -45,7 +54,11 @@ static inline uint32_t ll_rng_is_active_seis(RNG_TypeDef *RNGx)
 static inline void ll_rng_clear_seis(RNG_TypeDef *RNGx)
 {
 #if defined(CONFIG_SOC_SERIES_STM32WB0X)
-	LL_RNG_ClearFlag_FAULT(RNGx);
+#	if defined(CONFIG_SOC_STM32WB09XX)
+		LL_RNG_SetResetHealthErrorFlags(RNGx, 1);
+#	else
+		LL_RNG_ClearFlag_FAULT(RNGx);
+#	endif
 #else
 	LL_RNG_ClearFlag_SEIS(RNGx);
 #endif /* CONFIG_SOC_SERIES_STM32WB0X */
@@ -68,7 +81,11 @@ static inline uint32_t ll_rng_is_active_secs(RNG_TypeDef *RNGx)
 static inline uint32_t ll_rng_is_active_drdy(RNG_TypeDef *RNGx)
 {
 #if defined(CONFIG_SOC_SERIES_STM32WB0X)
-	return LL_RNG_IsActiveFlag_RNGRDY(RNGx);
+#	if defined(CONFIG_SOC_STM32WB09XX)
+		return LL_RNG_IsActiveFlag_VAL_READY(RNGx);
+#	else
+		return LL_RNG_IsActiveFlag_RNGRDY(RNGx);
+#	endif
 #else
 	return LL_RNG_IsActiveFlag_DRDY(RNGx);
 #endif /* CONFIG_SOC_SERIES_STM32WB0X */
@@ -77,7 +94,11 @@ static inline uint32_t ll_rng_is_active_drdy(RNG_TypeDef *RNGx)
 static inline uint16_t ll_rng_read_rand_data(RNG_TypeDef *RNGx)
 {
 #if defined(CONFIG_SOC_SERIES_STM32WB0X)
-	return LL_RNG_ReadRandData16(RNGx);
+#	if defined(CONFIG_SOC_STM32WB09XX)
+		return (uint16_t)LL_RNG_GetRndVal(RNGx);
+#	else
+		return LL_RNG_ReadRandData16(RNGx);
+#	endif
 #else
 	return (uint16_t)LL_RNG_ReadRandData32(RNGx);
 #endif /* CONFIG_SOC_SERIES_STM32WB0X */
