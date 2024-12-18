888b9c000e111ce679f16d1081078dd19f9ae1fc
Björn Bergman <bjorn.bergman@iar.com>
2024-12-18 13:57:52 +0100

    cmake: Fix compilation options for kobject_hash*.c
    Rework how the compilation-options for the gperf generated
    kobject_hash*.c files are put together to fix problems with SHELL:
    and cmake-list separators handling.
    
    zephyr_get_compile_options_for_lang_as_string() returns the set of
    options as a cmake generator expression string which is cumbersome to
    edit. This caused the command line for the IAR toolchain to have broken
    SHELL: entries, and other some command line entries being postfixed by
     "gnu".
    
    This also adds CMake compiler properties for no_function_sections and
    no_data_sections options
    
    Signed-off-by: Björn Bergman <bjorn.bergman@iar.com>

--- cmake/compiler/arcmwdt/compiler_flags.cmake
+++ cmake/compiler/arcmwdt/compiler_flags.cmake
@@ -210,3 +210,9 @@ set_compiler_property(PROPERTY warning_shadow_variables)
 
 set_compiler_property(PROPERTY no_builtin -fno-builtin)
 set_compiler_property(PROPERTY no_builtin_malloc -fno-builtin-malloc)
+
+# Compiler flag for placing functions in their own sections:
+set_compiler_property(PROPERTY no_function_sections "-fno-function-sections")
+
+# Compiler flag for placing variables in their own sections:
+set_compiler_property(PROPERTY no_data_sections "-fno-data-sections")
--- cmake/compiler/compiler_flags_template.cmake
+++ cmake/compiler/compiler_flags_template.cmake
@@ -149,3 +149,9 @@ set_compiler_property(PROPERTY specs)
 
 # Compiler flag for defining preinclude files.
 set_compiler_property(PROPERTY include_file)
+
+# Compiler flag for placing functions in their own sections:
+set_compiler_property(PROPERTY no_function_sections)
+
+# Compiler flag for placing variables in their own sections:
+set_compiler_property(PROPERTY no_data_sections)
