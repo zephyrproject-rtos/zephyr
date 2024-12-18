b3a6c822508dba96bb034154af64f0ee2b7be969
Jiafei Pan <Jiafei.Pan@nxp.com>
2024-12-18 16:22:54 +0800

    boards: imx95_evk_a55: add spsdk west runner support
    "west build" will build flash.bin which contains two containers, primary
    container contains ddr firmware, oei image, system manager image and spl.
    secondary container contains tf-a bl31 and zephyr image.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- boards/nxp/imx95_evk/board.cmake
+++ boards/nxp/imx95_evk/board.cmake
@@ -11,6 +11,11 @@ if(CONFIG_BOARD_NXP_SPSDK_IMAGE OR (DEFINED ENV{USE_NXP_SPSDK_IMAGE}
   board_runner_args(spsdk "--family=mimx9596")
   board_runner_args(spsdk "--bootloader=${CMAKE_BINARY_DIR}/zephyr/imx95-19x19-lpddr5-evk-boot-firmware-0.1/imx-boot-imx95-19x19-lpddr5-evk-sd.bin-flash_all")
   board_runner_args(spsdk "--flashbin=${CMAKE_BINARY_DIR}/zephyr/flash.bin")
+  if(CONFIG_CPU_CORTEX_A55)
+    board_runner_args(spsdk "--containers=two")
+  else()
+    board_runner_args(spsdk "--containers=one")
+  endif()
 
   include(${ZEPHYR_BASE}/boards/common/spsdk.board.cmake)
 endif()
