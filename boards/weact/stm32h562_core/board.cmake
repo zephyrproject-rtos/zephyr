90705ce86d23675bc8d334455423d47f98421a16
filipembedded <filipstojanovic.mas@gmail.com>
2024-12-19 21:13:10 +0100

    Add Zephyr support for weact stm32h562 board

---
+++ boards/weact/stm32h562_core/board.cmake
@@ -0,0 +1,4 @@
+include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
