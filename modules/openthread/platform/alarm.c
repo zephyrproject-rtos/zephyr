053654386aea6a2bab9d2daf24d179d454bef1e4
David Leach <david.leach@nxp.com>
2024-12-20 22:04:24 -0600

    Revert "[nxp fromtree] modules: openthread: Add of spinel hdlc rcp host interface"
    This reverts commit 43c62676c552ed519801c23c10d551eba095ea8a.
    
    Signed-off-by: David Leach <david.leach@nxp.com>

--- modules/openthread/platform/alarm.c
+++ modules/openthread/platform/alarm.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2018 Nordic Semiconductor ASA
- * Copyright (c) 2024 NXP.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -132,10 +131,3 @@ uint16_t otPlatTimeGetXtalAccuracy(void)
 {
 	return otPlatRadioGetCslAccuracy(NULL);
 }
-
-#ifdef CONFIG_HDLC_RCP_IF
-uint64_t otPlatTimeGet(void)
-{
-	return k_ticks_to_us_floor64(k_uptime_ticks());
-}
-#endif
