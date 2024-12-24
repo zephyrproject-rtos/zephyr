14d6fb1c16da4373b8ebc17420946c02c97b4507
Xudong Zheng <7pkvm5aw@slicealias.com>
2024-12-23 22:34:58 -0500

    entropy: rpi_pico: implement entropy driver for RP2350
    Use get_rand_64() from Pico SDK for entropy.
    
    Signed-off-by: Xudong Zheng <7pkvm5aw@slicealias.com>

---
+++ drivers/entropy/entropy_rpi_pico.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2024 Xudong Zheng
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string.h>
+#include <pico/rand.h>
+#include <zephyr/drivers/entropy.h>
+#include <zephyr/spinlock.h>
+
+#define DT_DRV_COMPAT raspberrypi_pico_rng
+
+static struct k_spinlock entropy_lock;
+
+static int entropy_rpi_pico_get_entropy(const struct device *dev, uint8_t *buf, uint16_t len)
+{
+	k_spinlock_key_t key = k_spin_lock(&entropy_lock);
+
+	__ASSERT_NO_MSG(buf != NULL);
+	uint8_t *buf_bytes = buf;
+
+	while (len > 0) {
+		uint64_t value = get_rand_64();
+		uint64_t to_copy = MIN(sizeof(value), len);
+
+		memcpy(buf_bytes, &value, to_copy);
+		buf_bytes += to_copy;
+		len -= to_copy;
+	}
+
+	k_spin_unlock(&entropy_lock, key);
+	return 0;
+}
+
+static DEVICE_API(entropy, entropy_rpi_pico_api_funcs) = {
+	.get_entropy = entropy_rpi_pico_get_entropy
+};
+
+static int entropy_rpi_pico_init(const struct device *dev)
+{
+	return 0;
+}
+
+DEVICE_DT_INST_DEFINE(0,
+		    entropy_rpi_pico_init, NULL, NULL, NULL,
+		    PRE_KERNEL_1, CONFIG_ENTROPY_INIT_PRIORITY,
+		    &entropy_rpi_pico_api_funcs);
