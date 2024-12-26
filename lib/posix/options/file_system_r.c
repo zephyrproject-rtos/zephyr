96489c4a4fe7564b2f8f0d5b139a0070a67b036d
Chris Friedt <cfriedt@tenstorrent.com>
2024-12-26 05:17:08 -0500

    posix: options: fs: separate file_system_r to its own file
    Move the functionality of POSIX_FILE_SYSTEM_R to its own
    compilation unit and remove the unnecessary dependency on
    POSIX_FILE_SYSTEM.
    
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

---
+++ lib/posix/options/file_system_r.c
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2024 Tenstorrent AI ULC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#undef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200809L
+
+#include "fs_priv.h"
+
+#include <errno.h>
+#include <limits.h>
+#include <string.h>
+
+#include <zephyr/fs/fs.h>
+#include <zephyr/posix/posix_features.h>
+#include <zephyr/posix/dirent.h>
+
+int readdir_r(DIR *dirp, struct dirent *entry, struct dirent **result)
+{
+	int rc;
+	struct fs_dirent de;
+	struct posix_fs_desc *const ptr = dirp;
+
+	if (result == NULL) {
+		return EINVAL;
+	}
+
+	if (entry == NULL) {
+		*result = NULL;
+		return EINVAL;
+	}
+
+	if (dirp == NULL) {
+		*result = NULL;
+		return EBADF;
+	}
+
+	rc = fs_readdir(&ptr->dir, &de);
+	if (rc < 0) {
+		*result = NULL;
+		return -rc;
+	}
+
+	strncpy(entry->d_name, de.name, MIN(sizeof(entry->d_name), sizeof(de.name)));
+	entry->d_name[sizeof(entry->d_name) - 1] = '\0';
+
+	if (entry->d_name[0] == '\0') {
+		*result = NULL;
+		return 0;
+	}
+
+	*result = entry;
+	return 0;
+}
--- lib/posix/options/fs.c
+++ lib/posix/options/fs.c
@@ -331,40 +331,6 @@ struct dirent *readdir(DIR *dirp)
 	return &pdirent;
 }
 
-#ifdef CONFIG_POSIX_FILE_SYSTEM_R
-int readdir_r(DIR *dirp, struct dirent *entry, struct dirent **result)
-{
-	struct dirent *dir;
-
-	errno = 0;
-
-	dir = readdir(dirp);
-	if (dir == NULL) {
-		int error = errno;
-
-		if (error != 0) {
-			if (result != NULL) {
-				*result = NULL;
-			}
-
-			return 0;
-		} else {
-			return error;
-		}
-	}
-
-	if (entry != NULL) {
-		memcpy(entry, dir, sizeof(struct dirent));
-	}
-
-	if (result != NULL) {
-		*result = entry;
-	}
-
-	return 0;
-}
-#endif /* CONFIG_POSIX_FILE_SYSTEM_R */
-
 /**
  * @brief Rename a file.
  *
