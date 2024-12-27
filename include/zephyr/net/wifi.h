c1782420d59930f376ff82e489de05c74cbf0da6
Maochen Wang <maochen.wang@nxp.com>
2024-12-27 15:51:05 +0800

    hostap: add WPA3 enterprise security type
    Change Wi-Fi suiteb type into WPA3 enterprise security type, it
    includes suiteB, suiteB-192 and WPA3 enterprise only mode.
    Support setting WPA3 enterprise only mode, which should use
    cipher_config->key_mgmt as WPA-EAP-SHA256, and the AKM in RSN
    IE will show 00-0F-AC:5.
    
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- include/zephyr/net/wifi.h
+++ include/zephyr/net/wifi.h
@@ -108,12 +108,26 @@ enum wifi_eap_type {
 	WIFI_EAP_TYPE_MSCHAPV2 = 26,
 };
 
-/** @brief Enterprise security WPA3 suiteb types. */
-enum wifi_suiteb_type {
-	/** suiteb. */
-	WIFI_SUITEB = 1,
-	/** suiteb-192. */
-	WIFI_SUITEB_192,
+/** @brief WPA3 Enterprise security types.
+ *
+ * See Section#3 in WFA WPA3 specification v3.4:
+ * https://www.wi-fi.org/file/wpa3-specification for details.
+ */
+enum wifi_wpa3_enterprise_type {
+	/** No WPA3 enterprise, either WPA2 Enterprise or personal mode */
+	WIFI_WPA3_ENTERPRISE_NA = 0,
+	/** WPA3 enterprise Suite-B (PMFR + WPA3-Suite-B). */
+	WIFI_WPA3_ENTERPRISE_SUITEB = 1,
+	/** WPA3 enterprise Suite-B-192 (PMFR + WPA3-Suite-B-192). */
+	WIFI_WPA3_ENTERPRISE_SUITEB_192,
+	/** WPA3 enterprise only (PMFR + WPA2-ENT disabled). */
+	WIFI_WPA3_ENTERPRISE_ONLY,
+
+	/** @cond INTERNAL_HIDDEN */
+	__WIFI_WPA3_ENTERPRISE_AFTER_LAST,
+	WIFI_WPA3_ENTERPRISE_MAX = __WIFI_WPA3_ENTERPRISE_AFTER_LAST - 1,
+	WIFI_WPA3_ENTERPRISE_UNKNOWN
+	/** @endcond */
 };
 
 enum wifi_eap_tls_cipher_type {
