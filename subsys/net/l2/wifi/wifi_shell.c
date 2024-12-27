980586ab19f44a70d2ed730cb7359ee0062b4d7a
Maochen Wang <maochen.wang@nxp.com>
2024-12-27 16:15:04 +0800

    net: wifi: support printing WPA3 enterprise in scan result
    Support printing WPA3 enterprise type in scan result for more
    accurate display, including the suiteb, suiteb-192 and WPA3
    enterprise only.
    
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- subsys/net/l2/wifi/wifi_shell.c
+++ subsys/net/l2/wifi/wifi_shell.c
@@ -191,18 +191,20 @@ static void handle_wifi_scan_result(struct net_mgmt_event_callback *cb)
 	context.scan_result++;
 
 	if (context.scan_result == 1U) {
-		PR("\n%-4s | %-32s %-5s | %-13s | %-4s | %-15s | %-17s | %-8s\n",
+		PR("\n%-4s | %-32s %-5s | %-13s | %-4s | %-20s | %-17s | %-8s\n",
 		   "Num", "SSID", "(len)", "Chan (Band)", "RSSI", "Security", "BSSID", "MFP");
 	}
 
 	strncpy(ssid_print, entry->ssid, sizeof(ssid_print) - 1);
 	ssid_print[sizeof(ssid_print) - 1] = '\0';
 
-	PR("%-4d | %-32s %-5u | %-4u (%-6s) | %-4d | %-15s | %-17s | %-8s\n",
+	PR("%-4d | %-32s %-5u | %-4u (%-6s) | %-4d | %-20s | %-17s | %-8s\n",
 	   context.scan_result, ssid_print, entry->ssid_length, entry->channel,
 	   wifi_band_txt(entry->band),
 	   entry->rssi,
-	   wifi_security_txt(entry->security),
+	   ((entry->wpa3_ent_type) ?
+		wifi_wpa3_enterprise_txt(entry->wpa3_ent_type)
+		 : wifi_security_txt(entry->security)),
 	   ((entry->mac_length) ?
 		   net_sprint_ll_addr_buf(entry->mac, WIFI_MAC_ADDR_LEN,
 					  mac_string_buf,
