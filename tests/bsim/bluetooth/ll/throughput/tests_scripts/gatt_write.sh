36e63bc7c98eae11903449ce2abec10326d81928
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2024-12-27 06:02:01 +0100

    tests: bsim: Bluetooth: nrRF53/54L: Throughput and multiple_id testing
    Enable throughput and multiple_id testing on nRF5340bsim and
    nrf54l15bsim.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- tests/bsim/bluetooth/ll/throughput/tests_scripts/gatt_write.sh
+++ tests/bsim/bluetooth/ll/throughput/tests_scripts/gatt_write.sh
@@ -11,10 +11,12 @@ EXECUTE_TIMEOUT=240
 cd ${BSIM_OUT_PATH}/bin
 
 Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_ll_throughput_prj_conf \
-  -v=${verbosity_level} -s=${simulation_id} -d=0 -testid=central
+  -v=${verbosity_level} -s=${simulation_id} -RealEncryption=1 -d=0 \
+  -testid=central
 
 Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_ll_throughput_prj_conf\
-  -v=${verbosity_level} -s=${simulation_id} -d=1 -testid=peripheral
+  -v=${verbosity_level} -s=${simulation_id} -RealEncryption=1 -d=1 \
+  -testid=peripheral
 
 Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
   -D=2 -sim_length=60e6 $@ -argschannel -at=40
