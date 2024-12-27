0633a76f311023548501273b6158b3cf85c84942
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2024-12-27 06:02:01 +0100

    tests: bsim: Bluetooth: nrRF53: Enable BT Mesh testing
    Enable BT Mesh testing on nRF5340bsim.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- tests/bsim/bluetooth/mesh/compile.sh
+++ tests/bsim/bluetooth/mesh/compile.sh
@@ -10,20 +10,27 @@ set -ue
 
 source ${ZEPHYR_BASE}/tests/bsim/compile.source
 
-app=tests/bsim/bluetooth/mesh compile
-app=tests/bsim/bluetooth/mesh conf_overlay=overlay_pst.conf compile
-app=tests/bsim/bluetooth/mesh conf_overlay=overlay_gatt.conf compile
-app=tests/bsim/bluetooth/mesh conf_overlay=overlay_low_lat.conf compile
-app=tests/bsim/bluetooth/mesh conf_overlay=overlay_psa.conf compile
-app=tests/bsim/bluetooth/mesh conf_overlay=overlay_workq_sys.conf compile
-app=tests/bsim/bluetooth/mesh conf_overlay="overlay_pst.conf;overlay_psa.conf" compile
-app=tests/bsim/bluetooth/mesh conf_overlay="overlay_gatt.conf;overlay_psa.conf" compile
-app=tests/bsim/bluetooth/mesh conf_overlay="overlay_gatt.conf;overlay_workq_sys.conf" compile
-app=tests/bsim/bluetooth/mesh conf_overlay="overlay_low_lat.conf;overlay_psa.conf" compile
-app=tests/bsim/bluetooth/mesh conf_overlay="overlay_gatt.conf;overlay_low_lat.conf" compile
-app=tests/bsim/bluetooth/mesh conf_overlay="overlay_pst.conf;overlay_gatt.conf" compile
-app=tests/bsim/bluetooth/mesh \
+app=tests/bsim/bluetooth/mesh sysbuild=1 compile
+app=tests/bsim/bluetooth/mesh sysbuild=1 conf_overlay=overlay_pst.conf compile
+app=tests/bsim/bluetooth/mesh sysbuild=1 conf_overlay=overlay_gatt.conf compile
+app=tests/bsim/bluetooth/mesh sysbuild=1 conf_overlay=overlay_low_lat.conf compile
+app=tests/bsim/bluetooth/mesh sysbuild=1 conf_overlay=overlay_psa.conf compile
+app=tests/bsim/bluetooth/mesh sysbuild=1 conf_overlay=overlay_workq_sys.conf compile
+app=tests/bsim/bluetooth/mesh sysbuild=1 \
+  conf_overlay="overlay_pst.conf;overlay_psa.conf" compile
+app=tests/bsim/bluetooth/mesh sysbuild=1 \
+  conf_overlay="overlay_gatt.conf;overlay_psa.conf" compile
+app=tests/bsim/bluetooth/mesh sysbuild=1 \
+  conf_overlay="overlay_gatt.conf;overlay_workq_sys.conf" compile
+app=tests/bsim/bluetooth/mesh sysbuild=1 \
+  conf_overlay="overlay_low_lat.conf;overlay_psa.conf" compile
+app=tests/bsim/bluetooth/mesh sysbuild=1 \
+  conf_overlay="overlay_gatt.conf;overlay_low_lat.conf" compile
+app=tests/bsim/bluetooth/mesh sysbuild=1 \
+  conf_overlay="overlay_pst.conf;overlay_gatt.conf" compile
+app=tests/bsim/bluetooth/mesh sysbuild=1 \
   conf_overlay="overlay_pst.conf;overlay_gatt.conf;overlay_psa.conf" compile
-app=tests/bsim/bluetooth/mesh \
+app=tests/bsim/bluetooth/mesh sysbuild=1 \
   conf_overlay="overlay_pst.conf;overlay_gatt.conf;overlay_workq_sys.conf" compile
+
 wait_for_background_jobs
