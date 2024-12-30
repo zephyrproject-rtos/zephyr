f02719edc3c5000a13a76696c190e711448e29cb
Martin Jäger <martin@libre.solar>
2024-12-30 13:42:40 +0100

    samples: net: openthread: shell: add sample application
    Add simple application to test Thread and IEEE 802.15.4 using the
    OpenThread shell.
    
    Signed-off-by: Martin Jäger <martin@libre.solar>

---
+++ samples/net/openthread/shell/src/main.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024 A Labs GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int main(void)
+{
+	/* Nothing to do here. The shell is automatically started in the background. */
+
+	return 0;
+}
