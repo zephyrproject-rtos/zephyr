58710374264d02b976f69b42c643a1d6b32cc603
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-12-30 08:40:01 +0000

    mgmt: mcumgr: grp: os_mgmt: Add error code for invalid responses
    Adds a new error code that can be used to signify that a query was
    valid but the response was not valid
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- include/zephyr/mgmt/mcumgr/grp/os_mgmt/os_mgmt.h
+++ include/zephyr/mgmt/mcumgr/grp/os_mgmt/os_mgmt.h
@@ -47,6 +47,9 @@ enum os_mgmt_err_code_t {
 
 	/** RTC command failed */
 	OS_MGMT_ERR_RTC_COMMAND_FAILED,
+
+	/** Query was recognized but there is no valid value for the response. */
+	OS_MGMT_ERR_QUERY_RESPONSE_VALUE_NOT_VALID,
 };
 
 /* Bitmask values used by the os info command handler. Note that the width of this variable is
