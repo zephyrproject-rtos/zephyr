76b7bb23d670ec83bfe9c78602593cd30216e58b
mmankal <171184446+mmankal@users.noreply.github.com>
2024-12-30 01:53:36 -0800

    Tsi shell (#9)
    * FIR-442: M85 Banner and Logging
    
    * FIR-442: M85 Banner and Logging
    
    * @FIR-445: M85 -TSI shell
    
    * @FIR 445 - TSI -shell; corrected trailing whitespaces
    
    * FIR 445 TSI-shell; README.rst -trailing space corrected
    
    * FIR445 - TSI-shell; main.c trailing space correction
    
    * FIR 445 TSI-shell - minor updates
    
    ---------
    
    Co-authored-by: M.Mankal-TSI <mmankali@wssim0.tsavoritesi.net>

--- tsi_core/README.rst
+++ tsi_core/README.rst
@@ -14,8 +14,8 @@ Building and Running
 This application can be built as follows:
 .. zephyr-app-commands::
    :zephyr-app:
-   :host-os: unix
-   :board: ek_tsi_skyp/m85 
+   :host-os:unix
+   :board:tsi/skyp/m85
    :goals: run
    :compact:
 
@@ -26,10 +26,10 @@ Sample Output
         Tsavorite Scalable Intelligence
 
     |||||||||||||||||||||||||||||||||||||
-    ||||||||||||||||||||||||||||||||||||| 
+    |||||||||||||||||||||||||||||||||||||
     ||||||                          |||||
-    ||||||                          ||||| 
-    |||||||||||||||||   |||||       ||||| 
+    ||||||                          |||||
+    |||||||||||||||||   |||||       |||||
     |||||||||||||||||   |||||       |||||
                ||||||   |||||
     ||||||     ||||||   |||||      ||||||
@@ -41,6 +41,9 @@ Sample Output
                 |||||   |||||
                   |||   |||
                         |
-[00:00:00.012,000] <inf> m85: Test Platform: tsi/skyp/m85
-[00:00:00.013,000] <wrn> m85: Testing on FPGA
+[00:00:00.012 000] <inf> m85: Test Platform: tsi/skyp/m85
+[00:00:00.013 000] <wrn> m85: Testing on FPGA
 TSI Logging enabled and printk is functional
+TSI:~$
+TSI:~$
+
--- tsi_core/src/main.c
+++ tsi_core/src/main.c
@@ -1,8 +1,7 @@
 /*
- * Copyright (c) 2024-2025 TSI   
+ * Copyright (c) 2024-2025 TSI
  *
  */
-
 
 #include <stddef.h>
 #include <stdio.h>
@@ -13,23 +12,21 @@
 #include <zephyr/logging/log_output.h>
 
 #define DATA_MAX_DLEN 8
-#define LOG_MODULE_NAME m85 
+#define LOG_MODULE_NAME m85
 LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 #define PRINT_TSI_BOOT_BANNER()  printk("        Tsavorite Scalable Intelligence \n")
 
 int main(void)
 {
-
 	/* TSI banner */
 	PRINT_TSI_BOOT_BANNER();
 	printf("\n");
-	printf("    |||||||||||||||||||||||||||||||||||||\n");;
-	printf("    ||||||||||||||||||||||||||||||||||||| \n");
+	printf("    |||||||||||||||||||||||||||||||||||||\n");
+	printf("    |||||||||||||||||||||||||||||||||||||\n");
 	printf("    ||||||                          |||||\n");
-	printf("    ||||||                          ||||| \n");
-	printf("    |||||||||||||||||   |||||       ||||| \n");
+	printf("    ||||||                          |||||\n");
 	printf("    |||||||||||||||||   |||||       |||||\n");
-	printf("               ||||||   |||||\n");
+  printf("               ||||||   |||||\n");
 	printf("    ||||||     ||||||   |||||      ||||||\n");
 	printf("     ||||||||  ||||||   |||||   ||||||||\n");
 	printf("       ||||||||||||||   ||||||||||||||\n");
@@ -39,10 +36,10 @@ int main(void)
 	printf("                |||||   |||||\n");
 	printf("                  |||   |||\n");
 	printf("                        |\n");
-
-        LOG_INF("Test Platform: %s",CONFIG_BOARD_TARGET);
-        LOG_WRN("Testing on FPGA");
-	printk("TSI Logging enabled and printk is functional\n");
+  
+  LOG_INF("Test Platform: %s", CONFIG_BOARD_TARGET);
+	LOG_WRN("Testing on FPGA");
+	printk("TSI Logging enabled & printk is functional\n");
 
 	return 0;
 }
