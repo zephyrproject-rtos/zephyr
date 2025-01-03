280507d0c65d58c579da12c002859c2cbcf7292b
Jordan Yates <jordan@embeint.com>
2025-01-03 10:29:32 +1000

    boards: openocd: `--cmd-erase` for STM32 series
    Add the appropriate mass erase command for STM32 SoC families.
    Family to command mapping taken from:
      https://openocd.org/doc/html/Flash-Commands.html
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- boards/common/openocd.board.cmake
+++ boards/common/openocd.board.cmake
@@ -13,6 +13,26 @@ else()
   set_ifndef(OPENOCD_FLASH "flash write_image erase")
 endif()
 
+# SoC specific mass erase options
+if(CONFIG_SOC_SERIES_STM32L0X OR CONFIG_SOC_SERIES_STM32L1X)
+  board_runner_args(openocd "--cmd-erase=stm32l1x mass_erase 0")
+elseif(CONFIG_SOC_SERIES_STM32L4X OR
+       CONFIG_SOC_SERIES_STM32L5X OR
+       CONFIG_SOC_SERIES_STM32U5X OR
+       CONFIG_SOC_SERIES_STM32WBX OR
+       CONFIG_SOC_SERIES_STM32G0X OR
+       CONFIG_SOC_SERIES_STM32G4X)
+  board_runner_args(openocd "--cmd-erase=stm32l4x mass_erase 0")
+elseif(CONFIG_SOC_SERIES_STM32F0X OR
+       CONFIG_SOC_SERIES_STM32F1X OR
+       CONFIG_SOC_SERIES_STM32F3X)
+  board_runner_args(openocd "--cmd-erase=stm32f1x mass_erase 0")
+elseif(CONFIG_SOC_SERIES_STM32F2X OR
+       CONFIG_SOC_SERIES_STM32F4X OR
+       CONFIG_SOC_SERIES_STM32F7X)
+  board_runner_args(openocd "--cmd-erase=stm32f2x mass_erase 0")
+endif()
+
 set(OPENOCD_CMD_LOAD_DEFAULT "${OPENOCD_FLASH}")
 set(OPENOCD_CMD_VERIFY_DEFAULT "verify_image")
 
