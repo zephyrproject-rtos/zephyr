60884cc326050885ade6afb091295d6532f7bf23
Johann Fischer <johann.fischer@nordicsemi.no>
2025-01-07 14:56:16 +0100

    usb: host: move representation of a USB device to UHC header
    This will allow the controller driver to access information about the
    device in the future.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- subsys/usb/host/usbh_device.h
+++ subsys/usb/host/usbh_device.h
@@ -11,23 +11,6 @@
 #include <zephyr/usb/usbh.h>
 #include <zephyr/drivers/usb/uhc.h>
 
-/* USB device state */
-enum usb_device_state {
-	USB_STATE_NOTCONNECTED,
-	USB_STATE_DEFAULT,
-	USB_STATE_ADDRESSED,
-	USB_STATE_CONFIGURED,
-};
-
-/* Host support view of a USB device */
-struct usb_device {
-	struct usbh_contex *ctx;
-	struct usb_device_descriptor dev_desc;
-	enum usb_device_state state;
-	uint8_t actual_cfg;
-	uint8_t addr;
-};
-
 /* Callback type to be used for e.g. synchronous requests */
 typedef int (*usbh_udev_cb_t)(struct usb_device *const udev,
 			      struct uhc_transfer *const xfer);
