e7a14b39f27842948a7b084abdb068e5f922dc97
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2025-01-08 11:15:56 +0100

    [nrf noup] boards: nordic: nrf7002dk: Bring back NS variants
    Non-secure variants for nRF7002 DK were removed from upstream
    in commit 10d49736cffa14d3798e615e70d58b3be8ee2cfc. Revert these
    changes downstream, so that the NS variants are still available.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>
    (cherry picked from commit c1643faea535faad9caf5852960c2d9cb09db101)

--- boards/nordic/nrf7002dk/board.cmake
+++ boards/nordic/nrf7002dk/board.cmake
@@ -1,10 +1,24 @@
 # Copyright (c) 2024 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
 
-if(CONFIG_BOARD_NRF7002DK_NRF5340_CPUAPP OR CONFIG_BOARD_NRF7002DK_NRF5340_CPUAPP_NRF7001)
+if(CONFIG_BOARD_NRF7002DK_NRF5340_CPUAPP_NS OR
+   CONFIG_BOARD_NRF7002DK_NRF5340_CPUAPP_NRF7001_NS)
+  set(TFM_PUBLIC_KEY_FORMAT "full")
+endif()
+
+if(CONFIG_BOARD_NRF7002DK_NRF5340_CPUAPP OR
+   CONFIG_BOARD_NRF7002DK_NRF5340_CPUAPP_NS OR
+   CONFIG_BOARD_NRF7002DK_NRF5340_CPUAPP_NRF7001 OR
+   CONFIG_BOARD_NRF7002DK_NRF5340_CPUAPP_NRF7001_NS)
   board_runner_args(nrfutil "--ext-mem-config-file=${BOARD_DIR}/support/nrf7002dk_spi_nrfutil_config.json")
   board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
-elseif(CONFIG_BOARD_NRF7002DK_NRF5340_CPUNET)
+endif()
+
+if(CONFIG_TFM_FLASH_MERGED_BINARY)
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file "${CMAKE_BINARY_DIR}/zephyr/tfm_merged.hex")
+endif()
+
+if(CONFIG_BOARD_NRF7002DK_NRF5340_CPUNET)
   board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
 endif()
 
