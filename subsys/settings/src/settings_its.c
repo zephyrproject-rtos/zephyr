8de7e13893874bd073e65d3745f6dddb8db7c7d5
Sean Kyer <Sean.Kyer@analog.com>
2025-01-09 16:25:05 -0800

    subsystem: settings: Add ITS backend
    Add settings_its backend in order for a non-secure
    Bluetooth application to be able to save Bluetooth
    settings to persistent storage.
    
    Signed-off-by: Sean Kyer <Sean.Kyer@analog.com>

---
+++ subsys/settings/src/settings_its.c
@@ -0,0 +1,248 @@
+/*
+ * Copyright (c) 2025 Analog Devices, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <psa/internal_trusted_storage.h>
+#include <zephyr/settings/settings.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/psa/key_ids.h>
+
+LOG_MODULE_DECLARE(settings, CONFIG_SETTINGS_LOG_LEVEL);
+
+K_MUTEX_DEFINE(worker_mutex);
+static struct k_work_delayable worker;
+
+struct setting_entry {
+	char name[SETTINGS_MAX_NAME_LEN];
+	char value[SETTINGS_MAX_VAL_LEN];
+	size_t val_len;
+};
+
+static struct setting_entry entries[CONFIG_SETTINGS_TFM_ITS_NUM_ENTRIES];
+static int entries_count;
+
+static int settings_its_load(struct settings_store *cs, const struct settings_load_arg *arg);
+static int settings_its_save(struct settings_store *cs, const char *name, const char *value,
+			     size_t val_len);
+
+static const struct settings_store_itf settings_its_itf = {
+	.csi_load = settings_its_load,
+	.csi_save = settings_its_save,
+};
+
+static struct settings_store default_settings_its = {.cs_itf = &settings_its_itf};
+
+static int increment_uid(psa_storage_uid_t *uid)
+{
+	if (*uid < ZEPHYR_PSA_TFM_ITS_KEY_ID_RANGE_BEGIN ||
+	    *uid >= (ZEPHYR_PSA_TFM_ITS_KEY_ID_RANGE_BEGIN +
+		     ZEPHYR_PSA_TFM_ITS_KEY_ID_RANGE_SIZE)) {
+		LOG_ERR("UID out of range! %lld > %d", *uid,
+			ZEPHYR_PSA_TFM_ITS_KEY_ID_RANGE_BEGIN +
+				ZEPHYR_PSA_TFM_ITS_KEY_ID_RANGE_SIZE);
+		return -EINVAL;
+	}
+
+	(*uid)++;
+	return 0;
+}
+
+static int store_entries(void)
+{
+	int err;
+	psa_status_t status;
+	psa_storage_uid_t uid = ZEPHYR_PSA_TFM_ITS_KEY_ID_RANGE_BEGIN;
+	size_t remaining = sizeof(entries);
+	size_t chunk_size = CONFIG_TFM_ITS_MAX_ASSET_SIZE;
+	const uint8_t *metadata_bytes = (const uint8_t *)&entries;
+
+	/*
+	 * Each ITS UID is treated like a sector. Data is written to each ITS node until
+	 * that node is full, before incrementing the UID. This is done to minimize the
+	 * number of allocated ITS nodes and to avoid wasting allocated bytes.
+	 */
+	while (remaining > 0) {
+		size_t write_size = (remaining > chunk_size) ? chunk_size : remaining;
+
+		status = psa_its_set(uid, write_size, metadata_bytes, PSA_STORAGE_FLAG_NONE);
+		if (status) {
+			LOG_ERR("Error storing %d bytes of metadata! Bytes Remaining: %d, status: "
+				"%d",
+				write_size, remaining, status);
+			return status;
+		}
+
+		metadata_bytes += write_size;
+		remaining -= write_size;
+		err = increment_uid(&uid);
+		if (err) {
+			return err;
+		}
+	}
+
+	LOG_DBG("ITS entries stored successfully - bytes_saved: %d num_entries: %d max_uid: %lld",
+		sizeof(entries), entries_count, uid);
+
+	return 0;
+}
+
+static int load_entries(void)
+{
+	int err;
+	psa_status_t status;
+	size_t bytes_read;
+	psa_storage_uid_t uid = ZEPHYR_PSA_TFM_ITS_KEY_ID_RANGE_BEGIN;
+	size_t remaining = sizeof(entries);
+	size_t chunk_size = CONFIG_TFM_ITS_MAX_ASSET_SIZE;
+	uint8_t *metadata_bytes = (uint8_t *)&entries;
+
+	/*
+	 * Each ITS UID is treated like a sector. Data is written to each ITS node until
+	 * that node is full, before incrementing the UID. This is done to minimize the
+	 * number of allocated ITS nodes and to avoid wasting allocated bytes.
+	 */
+	while (remaining > 0) {
+		size_t to_read = (remaining > chunk_size) ? chunk_size : remaining;
+
+		status = psa_its_get(uid, 0, to_read, metadata_bytes, &bytes_read);
+		if (status) {
+			return status;
+		}
+
+		metadata_bytes += bytes_read;
+		remaining -= bytes_read;
+		err = increment_uid(&uid);
+		if (err) {
+			return err;
+		}
+	}
+
+	for (int i = 0; i < CONFIG_SETTINGS_TFM_ITS_NUM_ENTRIES; i++) {
+		if (strnlen(entries[i].name, SETTINGS_MAX_NAME_LEN) != 0) {
+			entries_count++;
+		}
+	}
+
+	LOG_DBG("ITS entries restored successfully - bytes_loaded: %d, num_entries: %d",
+		sizeof(entries), entries_count);
+
+	return 0;
+}
+
+/* void *back_end is the index of the entry in metadata entries struct */
+static ssize_t settings_its_read_fn(void *back_end, void *data, size_t len)
+{
+	LOG_DBG("ITS Read - index: %d", *(int *)back_end);
+
+	if (*(int *)back_end < 0 || *(int *)back_end >= CONFIG_SETTINGS_TFM_ITS_NUM_ENTRIES) {
+		LOG_ERR("Invalid index %d in ITS metadata", *(int *)back_end);
+		return 0;
+	}
+
+	memcpy(data, entries[*(int *)back_end].value, len);
+
+	/*
+	 * Callback expects return value of the number of bytes read
+	 */
+	return entries[*(int *)back_end].val_len;
+}
+
+static int settings_its_load(struct settings_store *cs, const struct settings_load_arg *arg)
+{
+	int ret;
+
+	for (int i = 0; i < entries_count; i++) {
+
+		if (strnlen(entries[i].name, SETTINGS_MAX_NAME_LEN) != 0) {
+
+			/*
+			 * Pass the key to the settings handler with it's index as an argument,
+			 * to be read during callback function later.
+			 */
+			ret = settings_call_set_handler(entries[i].name, entries[i].val_len,
+							settings_its_read_fn, (void *)&i,
+							(void *)arg);
+			if (ret) {
+				return ret;
+			}
+		}
+	}
+
+	return 0;
+}
+
+static int settings_its_save(struct settings_store *cs, const char *name, const char *value,
+			     size_t val_len)
+{
+	if (entries_count >= CONFIG_SETTINGS_TFM_ITS_NUM_ENTRIES) {
+		LOG_ERR("%s: Max settings reached: %d", __func__,
+			CONFIG_SETTINGS_TFM_ITS_NUM_ENTRIES);
+		return -ENOMEM;
+	}
+
+	if (val_len > SETTINGS_MAX_VAL_LEN) {
+		LOG_ERR("%s: Invalid settings size - val_len: %d", __func__, val_len);
+		return -EINVAL;
+	}
+
+	int index;
+
+	/* Search metadata to see if entry already exists */
+	for (index = 0; index < CONFIG_SETTINGS_TFM_ITS_NUM_ENTRIES; index++) {
+		if (strncmp(entries[index].name, name, SETTINGS_MAX_NAME_LEN) == 0) {
+			break;
+		} else if (strnlen(entries[index].name, SETTINGS_MAX_NAME_LEN) == 0) {
+			/* New setting being entered */
+			entries_count++;
+			break;
+		}
+	}
+
+	LOG_DBG("ITS Save - index %d: name %s, val_len %d", index, name, val_len);
+
+	/* Update metadata */
+	strncpy(entries[index].name, name, SETTINGS_MAX_NAME_LEN);
+	memcpy(entries[index].value, value, val_len);
+	entries[index].val_len = val_len;
+
+	k_work_schedule(&worker, K_MSEC(CONFIG_SETTINGS_TFM_ITS_LAZY_PERSIST_DELAY_MS));
+
+	return 0;
+}
+
+void worker_persist_entries_struct_fn(struct k_work *work)
+{
+	k_mutex_lock(&worker_mutex, K_FOREVER);
+	store_entries();
+	k_mutex_unlock(&worker_mutex);
+}
+
+int settings_backend_init(void)
+{
+	psa_status_t status;
+
+	/* Load ITS metadata */
+	status = load_entries();
+
+	/* If resource DNE, we need to allocate it */
+	if (status == PSA_ERROR_DOES_NOT_EXIST) {
+		status = store_entries();
+		if (status) {
+			LOG_ERR("Error storing metadata in %s: (status %d)", __func__, status);
+			return -EIO;
+		}
+	} else if (status) {
+		LOG_ERR("Error loading metadata in %s: (status %d)", __func__, status);
+		return -EIO;
+	}
+
+	settings_dst_register(&default_settings_its);
+	settings_src_register(&default_settings_its);
+
+	k_work_init_delayable(&worker, worker_persist_entries_struct_fn);
+
+	return 0;
+}
