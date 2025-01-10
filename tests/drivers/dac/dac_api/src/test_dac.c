175cf738bbb147299258ec291d262a70be84c9bb
Ibe Van de Veire <ibe.vandeveire@basalte.be>
2025-01-10 16:29:42 +0100

    test: drivers: dac: Add support for SAM4S soc
    Add support for sam4s_xplained board in dac_api test.
    
    Signed-off-by: Ibe Van de Veire <ibe.vandeveire@basalte.be>

--- tests/drivers/dac/dac_api/src/test_dac.c
+++ tests/drivers/dac/dac_api/src/test_dac.c
@@ -85,8 +85,8 @@
 #define DAC_RESOLUTION		12
 #define DAC_CHANNEL_ID		0
 
-#elif defined(CONFIG_BOARD_SAM_E70_XPLAINED) || \
-	defined(CONFIG_BOARD_SAM_V71_XULT)
+#elif defined(CONFIG_BOARD_SAM_E70_XPLAINED) || defined(CONFIG_BOARD_SAM_V71_XULT) ||              \
+	defined(CONFIG_BOARD_SAM4S_XPLAINED)
 
 #define DAC_DEVICE_NODE		DT_NODELABEL(dacc)
 #define DAC_RESOLUTION		12
