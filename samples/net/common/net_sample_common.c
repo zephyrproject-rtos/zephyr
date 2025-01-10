fd31c7339bd8f25bb67a326f79a368698332b54e
Ryan Erickson <ryan.erickson@ezurio.com>
2025-01-10 08:31:13 -0600

    samples: net: http_get: add support for hl7800 modem
    Add overlay and small adjustments for using the hl7800 modem.
    Works out of the box with pinnacle_100_dvk and mg100.
    
    Signed-off-by: Ryan Erickson <ryan.erickson@ezurio.com>

--- samples/net/common/net_sample_common.c
+++ samples/net/common/net_sample_common.c
@@ -10,7 +10,11 @@ LOG_MODULE_REGISTER(net_samples_common, LOG_LEVEL_DBG);
 #include <zephyr/net/conn_mgr_connectivity.h>
 
 #if defined(CONFIG_NET_CONNECTION_MANAGER)
+#if defined(CONFIG_NET_SAMPLE_COMMON_WAIT_DNS_SERVER_ADDITION)
+#define L4_EVENT_MASK (NET_EVENT_DNS_SERVER_ADD | NET_EVENT_L4_DISCONNECTED)
+#else
 #define L4_EVENT_MASK (NET_EVENT_L4_CONNECTED | NET_EVENT_L4_DISCONNECTED)
+#endif
 
 static struct net_mgmt_event_callback l4_cb;
 static K_SEM_DEFINE(network_connected, 0, 1);
@@ -19,7 +23,11 @@ static void l4_event_handler(struct net_mgmt_event_callback *cb, uint32_t event,
 			     struct net_if *iface)
 {
 	switch (event) {
+#if defined(CONFIG_NET_SAMPLE_COMMON_WAIT_DNS_SERVER_ADDITION)
+	case NET_EVENT_DNS_SERVER_ADD:
+#else
 	case NET_EVENT_L4_CONNECTED:
+#endif
 		LOG_INF("Network connectivity established and IP address assigned");
 		k_sem_give(&network_connected);
 		break;
