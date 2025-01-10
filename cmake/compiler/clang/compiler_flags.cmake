6e803d8f75bb0a97d35c496f1b222e49a4cfdc0b
Tom Hughes <tomhughes@chromium.org>
2025-01-10 15:17:47 -0800

    cmake clang/compiler_flags.cmake: Re-enable -Wunused-variable for clang
    All warnings in the code base have been resolved.
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>

--- cmake/compiler/clang/compiler_flags.cmake
+++ cmake/compiler/clang/compiler_flags.cmake
@@ -108,7 +108,6 @@ check_set_compiler_property(PROPERTY warning_extended
                             -Wno-unused-function
                             -Wno-initializer-overrides
                             -Wno-section
-                            -Wno-unused-variable
                             -Wno-gnu
 )
 
