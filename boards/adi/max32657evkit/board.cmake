be3bb1cbc7db92a518114f3145dccd9fe0cd9999
Sadik Ozer <sadik.ozer@analog.com>
2025-01-13 15:40:29 +0300

    boards: adi: Enable jlink runner for MAX32657
    Enable JLink runner for MAX32657
    
    Signed-off-by: Sadik Ozer <sadik.ozer@analog.com>

--- boards/adi/max32657evkit/board.cmake
+++ boards/adi/max32657evkit/board.cmake
@@ -1,7 +1,8 @@
 # Copyright (c) 2024-2025 Analog Devices, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(openocd --cmd-pre-init "source [find interface/jlink.cfg]")
-board_runner_args(openocd --cmd-pre-init "source [find target/max32657.cfg]")
+board_runner_args(jlink "--device=MAX32657" "--reset-after-load")
 
+include(${ZEPHYR_BASE}/boards/common/openocd-adi-max32.boards.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
