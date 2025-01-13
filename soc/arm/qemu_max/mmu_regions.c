f518533c8a7005aa546707522e019c319e43c743
Guoqi Xie <xgqman@hnu.edu.cn>
2025-01-13 10:40:45 +0800

    boards: qemu: Add support for qemu_max board
    What is changed?
    - Added a new qemu_max board for QEMU emulation. This includes the addition of
      board-specific files such as Kconfig, defconfig, board.yaml, and DTS files under `boards/qemu/max`.
    - Added support for multi-core SMP with QEMU through the `qemu_max_qemu_max_smp`
      configuration and associated DTS and YAML files.
    - Updated SoC-specific configurations under `soc/arm/qemu_max`, including Kconfig,
      SoC-specific settings, and memory regions in `mmu_regions.c`.
    - Added device tree include file `qemu-virt-max.dtsi` for enhanced compatibility
      with the qemu_max board.
    
    Why do we need this change?
    - The qemu_max board supports the hardware features of ARMv8.1+,
      while the existing qemu_cortex_a53 does not.
    
    Signed-off-by: Guoqi Xie <xgqman@hnu.edu.cn>

---
+++ soc/arm/qemu_max/mmu_regions.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2024-2025 HNU-ESNL: Guoqi Xie, Charlie, Xingyu Hu and etc.;
+ * Copyright 2024-2025 openEuler SIG-Zephyr
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/arch/arm64/arm_mmu.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/sys/util.h>
+
+static const struct arm_mmu_region mmu_regions[] = {
+
+	MMU_REGION_FLAT_ENTRY("GIC",
+			      DT_REG_ADDR_BY_IDX(DT_INST(0, arm_gic), 0),
+			      DT_REG_SIZE_BY_IDX(DT_INST(0, arm_gic), 0),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
+
+	MMU_REGION_FLAT_ENTRY("GIC",
+			      DT_REG_ADDR_BY_IDX(DT_INST(0, arm_gic), 1),
+			      DT_REG_SIZE_BY_IDX(DT_INST(0, arm_gic), 1),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
+
+	MMU_REGION_FLAT_ENTRY("UART0",
+			      DT_REG_ADDR(DT_INST(0, arm_pl011)),
+			      DT_REG_SIZE(DT_INST(0, arm_pl011)),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
+
+#if defined(CONFIG_ZVM)
+	MMU_REGION_FLAT_ENTRY("UART1",
+			      DT_REG_ADDR(DT_INST(0, pl011)),
+			      DT_REG_SIZE(DT_INST(0, pl011)),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
+
+	MMU_REGION_FLAT_ENTRY("UART2",
+			      DT_REG_ADDR(DT_INST(1, pl011)),
+			      DT_REG_SIZE(DT_INST(1, pl011)),
+			      MT_DEVICE_nGnRnE | MT_P_RW_U_NA | MT_DEFAULT_SECURE_STATE),
+#endif
+
+};
+
+const struct arm_mmu_config mmu_config = {
+	.num_regions = ARRAY_SIZE(mmu_regions),
+	.mmu_regions = mmu_regions,
+};
