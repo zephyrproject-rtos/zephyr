e64f814ba0912896fca5eb38688351d19c47f93b
Gerard Marull-Paretas <gerard.marull@nordicsemi.no>
2025-01-13 12:23:28 +0100

    device: deprecate device_init
    Just use device_get(). This allows to keep client code the same
    regardless of when a device is initialized.
    
    Signed-off-by: Gerard Marull-Paretas <gerard.marull@nordicsemi.no>

--- include/zephyr/device.h
+++ include/zephyr/device.h
@@ -790,22 +790,6 @@ size_t z_device_get_all_static(const struct device **devices);
 __syscall bool device_is_ready(const struct device *dev);
 
 /**
- * @brief Initialize a device.
- *
- * A device whose initialization was deferred (by marking it as
- * ``zephyr,deferred-init`` on devicetree) needs to be initialized manually via
- * this call. Note that only devices whose initialization was deferred can be
- * initialized via this call - one can not try to initialize a non
- * initialization deferred device that failed initialization with this call.
- *
- * @param dev device to be initialized.
- *
- * @retval -ENOENT If device was not found - or isn't a deferred one.
- * @retval -errno For other errors.
- */
-__syscall int device_init(const struct device *dev);
-
-/**
  * Get a device.
  *
  * When getting a device, its usage count will be increased and, if not yet
@@ -819,6 +803,27 @@ __syscall int device_init(const struct device *dev);
  * indicate the current reference count.
  */
 __syscall int device_get(const struct device *dev);
+
+/**
+ * @brief Initialize a device.
+ *
+ * A device whose initialization was deferred (by marking it as
+ * ``zephyr,deferred-init`` on devicetree) needs to be initialized manually via
+ * this call. Note that only devices whose initialization was deferred can be
+ * initialized via this call - one can not try to initialize a non
+ * initialization deferred device that failed initialization with this call.
+ *
+ * @param dev device to be initialized.
+ *
+ * @deprecated Use device_get() instead.
+ *
+ * @retval -ENOENT If device was not found - or isn't a deferred one.
+ * @retval -errno For other errors.
+ */
+static inline __deprecated int device_init(const struct device *dev)
+{
+	return device_get(dev);
+}
 
 /**
  * @}
