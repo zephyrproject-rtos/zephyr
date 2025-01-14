7d625b535a8d0fcf4fa6c4cf25a4d1790eb3c439
Stine Akredalen <stine.akredalen@nordicsemi.no>
2025-01-14 21:10:28 +0100

    Bluetooth: host: Add overlays for bsim tests
    Removed similar prj.conf files and added overlays instead to avoid
    duplicate code.
    Renamed some test files for consistency.
    
    Signed-off-by: Stine Akredalen <stine.akredalen@nordicsemi.no>

--- tests/bsim/bluetooth/host/l2cap/stress/tests_scripts/l2cap_nofrag.sh
+++ tests/bsim/bluetooth/host/l2cap/stress/tests_scripts/l2cap_nofrag.sh
@@ -9,7 +9,7 @@ simulation_id="l2cap_stress_nofrag"
 verbosity_level=2
 EXECUTE_TIMEOUT=240
 
-bsim_exe=./bs_${BOARD_TS}_tests_bsim_bluetooth_host_l2cap_stress_prj_nofrag_conf
+bsim_exe=./bs_${BOARD_TS}_tests_bsim_bluetooth_host_l2cap_stress_prj_conf_overlay-nofrag_conf
 
 cd ${BSIM_OUT_PATH}/bin
 
