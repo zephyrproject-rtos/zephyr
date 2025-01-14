1b536c2d2afef31fc6e7a5c930f29f1dc6d1e700
Jiawei Yang <jiawei.yang_1@nxp.com>
2025-01-14 18:41:00 +0800

    Sample: bluetooth: Enable MIMXRT1170 EVKB for peripheral_ht
    Enable MIMXRT1170 EVKB for peripheral_ht.
    
    Signed-off-by: Jiawei Yang <jiawei.yang_1@nxp.com>

---
+++ modules/hal_nxp/bt_controller/no_blobs/no_blobs.h
@@ -0,0 +1,15 @@
+/*
+ *  Copyright 2025 NXP
+ *
+ *  SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef __UART_BT_FW_H__
+#define __UART_BT_FW_H__
+
+
+const unsigned char *bt_fw_bin;
+unsigned int bt_fw_bin_len;
+
+#endif /* __UART_BT_FW_H__ */
