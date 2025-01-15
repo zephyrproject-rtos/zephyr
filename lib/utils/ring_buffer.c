e7178e259ad527fc1ff16f5007385e4343cb4a9e
Nicolas Pitre <npitre@baylibre.com>
2025-01-15 17:26:05 -0500

    ring_buffer: factorize almost identical code
    Factorize almost identical code. May even improve performance due to
    CPU cache locality.
    
    Signed-off-by: Nicolas Pitre <npitre@baylibre.com>

--- lib/utils/ring_buffer.c
+++ lib/utils/ring_buffer.c
@@ -9,46 +9,45 @@
 #include <zephyr/sys/ring_buffer.h>
 #include <string.h>
 
-uint32_t ring_buf_put_claim(struct ring_buf *buf, uint8_t **data, uint32_t size)
+uint32_t ring_buf_area_claim(struct ring_buf *buf, struct ring_buf_index *ring,
+			     uint8_t **data, uint32_t size)
 {
-	uint32_t free_space, wrap_size;
+	uint32_t wrap_size;
 	int32_t base;
 
-	base = buf->put_base;
-	wrap_size = buf->put_head - base;
+	base = ring->base;
+	wrap_size = ring->head - base;
 	if (unlikely(wrap_size >= buf->size)) {
-		/* put_base is not yet adjusted */
+		/* ring->base is not yet adjusted */
 		wrap_size -= buf->size;
 		base += buf->size;
 	}
 	wrap_size = buf->size - wrap_size;
-
-	free_space = ring_buf_space_get(buf);
-	size = MIN(size, free_space);
 	size = MIN(size, wrap_size);
 
-	*data = &buf->buffer[buf->put_head - base];
-	buf->put_head += size;
+	*data = &buf->buffer[ring->head - base];
+	ring->head += size;
 
 	return size;
 }
 
-int ring_buf_put_finish(struct ring_buf *buf, uint32_t size)
+int ring_buf_area_finish(struct ring_buf *buf, struct ring_buf_index *ring,
+			 uint32_t size)
 {
-	uint32_t finish_space, wrap_size;
+	uint32_t claimed_size, wrap_size;
 
-	finish_space = buf->put_head - buf->put_tail;
-	if (unlikely(size > finish_space)) {
+	claimed_size = ring->head - ring->tail;
+	if (unlikely(size > claimed_size)) {
 		return -EINVAL;
 	}
 
-	buf->put_tail += size;
-	buf->put_head = buf->put_tail;
+	ring->tail += size;
+	ring->head = ring->tail;
 
-	wrap_size = buf->put_tail - buf->put_base;
+	wrap_size = ring->tail - ring->base;
 	if (unlikely(wrap_size >= buf->size)) {
-		/* we wrapped: adjust put_base */
-		buf->put_base += buf->size;
+		/* we wrapped: adjust ring->base */
+		ring->base += buf->size;
 	}
 
 	return 0;
@@ -77,51 +76,6 @@ uint32_t ring_buf_put(struct ring_buf *buf, const uint8_t *data, uint32_t size)
 	ARG_UNUSED(err);
 
 	return total_size;
-}
-
-uint32_t ring_buf_get_claim(struct ring_buf *buf, uint8_t **data, uint32_t size)
-{
-	uint32_t available_size, wrap_size;
-	int32_t base;
-
-	base = buf->get_base;
-	wrap_size = buf->get_head - base;
-	if (unlikely(wrap_size >= buf->size)) {
-		/* get_base is not yet adjusted */
-		wrap_size -= buf->size;
-		base += buf->size;
-	}
-	wrap_size = buf->size - wrap_size;
-
-	available_size = ring_buf_size_get(buf);
-	size = MIN(size, available_size);
-	size = MIN(size, wrap_size);
-
-	*data = &buf->buffer[buf->get_head - base];
-	buf->get_head += size;
-
-	return size;
-}
-
-int ring_buf_get_finish(struct ring_buf *buf, uint32_t size)
-{
-	uint32_t finish_space, wrap_size;
-
-	finish_space = buf->get_head - buf->get_tail;
-	if (unlikely(size > finish_space)) {
-		return -EINVAL;
-	}
-
-	buf->get_tail += size;
-	buf->get_head = buf->get_tail;
-
-	wrap_size = buf->get_tail - buf->get_base;
-	if (unlikely(wrap_size >= buf->size)) {
-		/* we wrapped: adjust get_base */
-		buf->get_base += buf->size;
-	}
-
-	return 0;
 }
 
 uint32_t ring_buf_get(struct ring_buf *buf, uint8_t *data, uint32_t size)
