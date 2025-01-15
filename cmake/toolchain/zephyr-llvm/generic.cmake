a20b45e4dfdbe02b6c6ce1cbfc3a0c0d50059d79
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2025-01-15 11:37:25 +0100

    fixup! cmake: Add 'zephyr-llvm' toolchain variant

--- cmake/toolchain/zephyr-llvm/generic.cmake
+++ cmake/toolchain/zephyr-llvm/generic.cmake
@@ -4,4 +4,4 @@ include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/llvm/generic.cmake)
 
 set(TOOLCHAIN_KCONFIG_DIR ${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr)
 
-message(STATUS "Found toolchain: zephyr-sdk-llvm ${SDK_VERSION} (${ZEPHYR_SDK_INSTALL_DIR})")
+message(STATUS "Found toolchain: zephyr-llvm ${SDK_VERSION} (${ZEPHYR_SDK_INSTALL_DIR})")
