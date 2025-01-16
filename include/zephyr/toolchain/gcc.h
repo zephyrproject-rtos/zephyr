f040627d6a0188c445db79a08b2cdd81f58bd6f5
Joel Holdsworth <jholdsworth@nvidia.com>
2025-01-16 11:26:48 -0800

    toolchain: gcc: Added symbol attributes for OpenRISC
    A common set of macros: GTEXT(), GDATA(), WTEXT() and WDATA() is defined
    for Arm, Arm66, MIPS, Nios II, RISC-V and Xtensa. This patch adds OpenRISC
    to this set.
    
    Signed-off-by: Joel Holdsworth <jholdsworth@nvidia.com>

--- include/zephyr/toolchain/gcc.h
+++ include/zephyr/toolchain/gcc.h
@@ -370,7 +370,7 @@ do {                                                                    \
 
 #if defined(CONFIG_ARM) || defined(CONFIG_NIOS2) || defined(CONFIG_RISCV) \
 	|| defined(CONFIG_XTENSA) || defined(CONFIG_ARM64) \
-	|| defined(CONFIG_MIPS)
+	|| defined(CONFIG_MIPS) || defined(CONFIG_OPENRISC)
 #define GTEXT(sym) .global sym; .type sym, %function
 #define GDATA(sym) .global sym; .type sym, %object
 #define WTEXT(sym) .weak sym; .type sym, %function
