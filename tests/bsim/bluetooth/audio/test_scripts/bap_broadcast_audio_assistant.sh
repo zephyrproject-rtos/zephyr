3dadce90a0be7fb85e052c240b3d12adb4eadc1a
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2025-01-16 07:08:22 +0100

    tests: bsim: Bluetooth: Workaround overlapping ACL in BAP broadcast sink
    Workaround by using `-start_offset` to ensure that the ACL
    established does not overlap the sink being established.
    
    The behavior between nrf52_bsim and nrf54l15bsim is
    different where in the two device are starting to
    advertising at the same time in nrf54l15bsim where as
    there is an offset already in nrf52_bsim test.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- tests/bsim/bluetooth/audio/test_scripts/bap_broadcast_audio_assistant.sh
+++ tests/bsim/bluetooth/audio/test_scripts/bap_broadcast_audio_assistant.sh
@@ -24,7 +24,8 @@ Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_audio_prj_conf \
 
 Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_audio_prj_conf \
   -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=2 \
-  -testid=broadcast_source -RealEncryption=1 -rs=69 -D=3
+  -testid=broadcast_source -RealEncryption=1 -rs=69 -D=3 \
+  -start_offset=5e3
 
 # Simulation time should be larger than the WAIT_TIME in common.h
 Execute ./bs_2G4_phy_v1 -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -D=3 -sim_length=60e6 $@
