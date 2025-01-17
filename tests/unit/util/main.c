716e6bfd2086e0573a21c8f1746c6b1500dd4da0
Théo Battrel <theo.battrel@nordicsemi.no>
2025-01-17 15:59:33 +0100

    Tests: Util: Add unit tests for `util_eq`/`util_memeq`
    Add two unit tests for the newly added functions `util_eq` and
    `util_memeq`.
    
    Signed-off-by: Théo Battrel <theo.battrel@nordicsemi.no>

--- tests/unit/util/main.c
+++ tests/unit/util/main.c
@@ -918,4 +918,55 @@ ZTEST(util, test_utf8_lcpy_null_termination)
 	zassert_str_equal(dest_str, expected_result, "Failed to truncate");
 }
 
+ZTEST(util, test_util_eq)
+{
+	uint8_t src1[16];
+	uint8_t src2[16];
+
+	bool mem_area_matching_1;
+	bool mem_area_matching_2;
+
+	memset(src1, 0, sizeof(src1));
+	memset(src2, 0, sizeof(src2));
+
+	for (size_t i = 0U; i < 16; i++) {
+		src1[i] = 0xAB;
+		src2[i] = 0xAB;
+	}
+
+	src1[15] = 0xCD;
+	src2[15] = 0xEF;
+
+	mem_area_matching_1 = util_eq(src1, sizeof(src1), src2, sizeof(src2));
+	mem_area_matching_2 = util_eq(src1, sizeof(src1) - 1, src2, sizeof(src2) - 1);
+
+	zassert_false(mem_area_matching_1);
+	zassert_true(mem_area_matching_2);
+}
+
+ZTEST(util, test_util_memeq)
+{
+	uint8_t src1[16];
+	uint8_t src2[16];
+	uint8_t src3[16];
+
+	bool mem_area_matching_1;
+	bool mem_area_matching_2;
+
+	memset(src1, 0, sizeof(src1));
+	memset(src2, 0, sizeof(src2));
+
+	for (size_t i = 0U; i < 16; i++) {
+		src1[i] = 0xAB;
+		src2[i] = 0xAB;
+		src3[i] = 0xCD;
+	}
+
+	mem_area_matching_1 = util_memeq(src1, src2, sizeof(src1));
+	mem_area_matching_2 = util_memeq(src1, src3, sizeof(src1));
+
+	zassert_true(mem_area_matching_1);
+	zassert_false(mem_area_matching_2);
+}
+
 ZTEST_SUITE(util, NULL, NULL, NULL, NULL, NULL);
