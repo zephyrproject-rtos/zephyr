0ea2870ae817a3145aa7d18143e7ef504d09fa47
Fabio Baltieri <fabiobaltieri@google.com>
2025-01-19 00:38:58 +0000

    drivers: drop few redundant device_is_ready
    Drop few redundant device_is_ready for functions that are only used as
    argument to shell_device_filter, as shell_device_filter checks for that
    alrady.
    
    Suggested-by: Yishai Jaffe <yishai1999@gmail.com>
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

--- drivers/led/led_shell.c
+++ drivers/led/led_shell.c
@@ -330,14 +330,14 @@ cmd_write_channels(const struct shell *sh, size_t argc, char **argv)
 	return err;
 }
 
-static bool device_is_led_and_ready(const struct device *dev)
+static bool device_is_led(const struct device *dev)
 {
-	return device_is_ready(dev) && DEVICE_API_IS(led, dev);
+	return DEVICE_API_IS(led, dev);
 }
 
 static void device_name_get(size_t idx, struct shell_static_entry *entry)
 {
-	const struct device *dev = shell_device_filter(idx, device_is_led_and_ready);
+	const struct device *dev = shell_device_filter(idx, device_is_led);
 
 	entry->syntax = (dev != NULL) ? dev->name : NULL;
 	entry->handler = NULL;
