52695a664d194e9cbfb3d270f5429cf1eecbf67e
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2025-01-20 12:18:32 +0100

    DNM: Make hci ipc driver fail allocating buf for inc ACL data
    Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

--- tests/bsim/bluetooth/host/l2cap/stress/tests_scripts/l2cap.sh
+++ tests/bsim/bluetooth/host/l2cap/stress/tests_scripts/l2cap.sh
@@ -2,6 +2,8 @@
 # Copyright (c) 2022 Nordic Semiconductor
 # SPDX-License-Identifier: Apache-2.0
 
+BOARD="nrf5340bsim/nrf5340/cpuapp"
+#BOARD="nrf52_bsim"
 source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
 
 # EATT test
@@ -16,12 +18,12 @@ cd ${BSIM_OUT_PATH}/bin
 Execute "${bsim_exe}" -v=${verbosity_level} -s=${simulation_id} -d=0 -testid=central -rs=43
 
 Execute "${bsim_exe}" -v=${verbosity_level} -s=${simulation_id} -d=1 -testid=peripheral -rs=42
-Execute "${bsim_exe}" -v=${verbosity_level} -s=${simulation_id} -d=2 -testid=peripheral -rs=10
-Execute "${bsim_exe}" -v=${verbosity_level} -s=${simulation_id} -d=3 -testid=peripheral -rs=23
-Execute "${bsim_exe}" -v=${verbosity_level} -s=${simulation_id} -d=4 -testid=peripheral -rs=7884
-Execute "${bsim_exe}" -v=${verbosity_level} -s=${simulation_id} -d=5 -testid=peripheral -rs=230
-Execute "${bsim_exe}" -v=${verbosity_level} -s=${simulation_id} -d=6 -testid=peripheral -rs=9
+#Execute "${bsim_exe}" -v=${verbosity_level} -s=${simulation_id} -d=2 -testid=peripheral -rs=10
+#Execute "${bsim_exe}" -v=${verbosity_level} -s=${simulation_id} -d=3 -testid=peripheral -rs=23
+#Execute "${bsim_exe}" -v=${verbosity_level} -s=${simulation_id} -d=4 -testid=peripheral -rs=7884
+#Execute "${bsim_exe}" -v=${verbosity_level} -s=${simulation_id} -d=5 -testid=peripheral -rs=230
+#Execute "${bsim_exe}" -v=${verbosity_level} -s=${simulation_id} -d=6 -testid=peripheral -rs=9
 
-Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} -D=7 -sim_length=400e6 $@
+Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} -D=2 -sim_length=400e6 $@
 
 wait_for_background_jobs
