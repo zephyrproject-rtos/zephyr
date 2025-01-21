8983a610a39a553009c69f8be1a4eea8db434d1f
Pisit Sawangvonganan <pisit@ndrsolution.com>
2025-01-21 12:05:30 +0700

    drivers: udc: stm32: fix DOUT stage handling for `st_stm32_usb`
    In the device that has `DT_DRV_COMPAT` equal to `st_stm32_usb`,
    its behavior differs from `st_stm32_otghs` and `st_stm32_otgfs`
    due to the underlying `HAL_PCD_IRQHandler`.
    As a result, calling `usbd_ctrl_feed_dout` for the DOUT stage is not
    compatible with the `st_stm32_usb` device.
    
    Instead of calling `usbd_ctrl_feed_dout`, we still require flushing
    the TX FIFO to the host.
    This is achieved by calling `HAL_PCD_EP_Receive` with `len` = `0`.
    
    Signed-off-by: Pisit Sawangvonganan <pisit@ndrsolution.com>

--- drivers/usb/udc/udc_stm32.c
+++ drivers/usb/udc/udc_stm32.c
@@ -194,6 +194,14 @@ void HAL_PCD_SOFCallback(PCD_HandleTypeDef *hpcd)
 	udc_submit_event(priv->dev, UDC_EVT_SOF, 0);
 }
 
+static void udc_stm32_flush_tx_fifo(const struct device *dev)
+{
+	struct udc_stm32_data *priv = udc_get_private(dev);
+	struct udc_ep_config *cfg = udc_get_ep_cfg(dev, USB_CONTROL_EP_OUT);
+
+	HAL_PCD_EP_Receive(&priv->pcd, cfg->addr, NULL, 0);
+}
+
 static int udc_stm32_tx(const struct device *dev, uint8_t ep,
 			struct net_buf *buf)
 {
@@ -230,7 +238,11 @@ static int udc_stm32_tx(const struct device *dev, uint8_t ep,
 		/* Wait for an empty package from the host.
 		 * This also flushes the TX FIFO to the host.
 		 */
-		usbd_ctrl_feed_dout(dev, 0);
+		if (DT_HAS_COMPAT_STATUS_OKAY(st_stm32_usb)) {
+			udc_stm32_flush_tx_fifo(dev);
+		} else {
+			usbd_ctrl_feed_dout(dev, 0);
+		}
 	}
 
 	return 0;
