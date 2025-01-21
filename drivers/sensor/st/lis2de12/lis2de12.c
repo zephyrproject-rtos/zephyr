db92e822ebededdd233af47bd9083039fab157b2
Mayank Narang <narang.may77@gmail.com>
2025-01-21 23:51:32 +0530

    drivers: sensor: lis2de12: fix read accel via spi
    The lis2de12 sensor driver spi interface was calling spi read api.
    This leads to a single byte operation on reading acceleration data
    which is a multi byte operation. Fix it by adding a call to spi read
    incr api instead. Added a length check to handle both single and multi
    byte read properly.
    
    Signed-off-by: Mayank Narang <narang.may77@gmail.com>

--- drivers/sensor/st/lis2de12/lis2de12.c
+++ drivers/sensor/st/lis2de12/lis2de12.c
@@ -435,7 +435,7 @@ static int lis2de12_init(const struct device *dev)
 
 #define LIS2DE12_CONFIG_SPI(inst)						\
 	{									\
-		STMEMSC_CTX_SPI(&lis2de12_config_##inst.stmemsc_cfg),		\
+		STMEMSC_CTX_SPI_INCR(&lis2de12_config_##inst.stmemsc_cfg),		\
 		.stmemsc_cfg = {						\
 			.spi = SPI_DT_SPEC_INST_GET(inst, LIS2DE12_SPI_OP, 0),	\
 		},								\
