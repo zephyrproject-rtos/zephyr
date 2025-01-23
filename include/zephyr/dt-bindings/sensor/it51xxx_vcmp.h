8d590f883d902d67b85d34fc468f2b5aa3add1df
Yunshao Chiang <Yunshao.Chiang@ite.com.tw>
2025-01-23 15:29:53 +0800

    drivers: sensor: Add it51xxx_evb adc voltage comparator driver
    Add voltage comparator driver for ITE it51xxx chip.
    
    Signed-off-by: Yunshao Chiang <Yunshao.Chiang@ite.com.tw>

---
+++ include/zephyr/dt-bindings/sensor/it51xxx_vcmp.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2025 ITE Technology Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_SENSOR_IT51XXX_VCMP_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_SENSOR_IT51XXX_VCMP_H_
+
+/**
+ * @name it51xxx voltage comparator channel references
+ * @{
+ */
+
+#define VCMP_CHANNEL_0   0
+#define VCMP_CHANNEL_1   1
+#define VCMP_CHANNEL_2   2
+#define VCMP_CHANNEL_CNT 3
+
+/** @} */
+
+/**
+ * @name it51xxx voltage comparator scan period for "all comparator channel"
+ * @{
+ */
+
+#ifdef CONFIG_VCMP_IT51XXX_SCAN_PERIOD_100US
+#define VCMP_SCAN_PERIOD 0x1
+#elif CONFIG_VCMP_IT51XXX_SCAN_PERIOD_200US
+#define VCMP_SCAN_PERIOD 0x2
+#elif CONFIG_VCMP_IT51XXX_SCAN_PERIOD_400US
+#define VCMP_SCAN_PERIOD 0x3
+#elif CONFIG_VCMP_IT51XXX_SCAN_PERIOD_600US
+#define VCMP_SCAN_PERIOD 0x4
+#elif CONFIG_VCMP_IT51XXX_SCAN_PERIOD_800US
+#define VCMP_SCAN_PERIOD 0x5
+#elif CONFIG_VCMP_IT51XXX_SCAN_PERIOD_1MS
+#define VCMP_SCAN_PERIOD 0x6
+#elif CONFIG_VCMP_IT51XXX_SCAN_PERIOD_1_5MS
+#define VCMP_SCAN_PERIOD 0x7
+#elif CONFIG_VCMP_IT51XXX_SCAN_PERIOD_2MS
+#define VCMP_SCAN_PERIOD 0x8
+#elif CONFIG_VCMP_IT51XXX_SCAN_PERIOD_2_5MS
+#define VCMP_SCAN_PERIOD 0x9
+#elif CONFIG_VCMP_IT51XXX_SCAN_PERIOD_3MS
+#define VCMP_SCAN_PERIOD 0xA
+#elif CONFIG_VCMP_IT51XXX_SCAN_PERIOD_4MS
+#define VCMP_SCAN_PERIOD 0xB
+#elif CONFIG_VCMP_IT51XXX_SCAN_PERIOD_5MS
+#define VCMP_SCAN_PERIOD 0xC
+#endif
+
+/** @} */
+
+/**
+ * @name it51xxx voltage comparator interrupt trigger mode
+ * @{
+ */
+
+#define IT51XXX_VCMP_LESS_OR_EQUAL 0
+#define IT51XXX_VCMP_GREATER       1
+#define IT51XXX_VCMP_UNDEFINED     0xffff
+
+/** @} */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_SENSOR_IT51XXX_VCMP_H_ */
