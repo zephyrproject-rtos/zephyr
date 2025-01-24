a8a02fa6de15ea45475381890a7eedb1bd35b10e
Aksel Skauge Mellbye <aksel.mellbye@silabs.com>
2025-01-24 11:11:36 +0100

    drivers: adc: iadc_gecko: Use pinctrl for bus allocation
    Make use of ABUS support in the pinctrl driver to allocate
    analog buses, rather than hard-coding bus 0 in the ADC driver.
    
    Signed-off-by: Aksel Skauge Mellbye <aksel.mellbye@silabs.com>

--- drivers/adc/iadc_gecko.c
+++ drivers/adc/iadc_gecko.c
@@ -7,6 +7,7 @@
 #define DT_DRV_COMPAT silabs_gecko_iadc
 
 #include <zephyr/drivers/adc.h>
+#include <zephyr/drivers/pinctrl.h>
 
 #include <em_iadc.h>
 #include <em_cmu.h>
@@ -43,6 +44,7 @@ struct adc_gecko_data {
 struct adc_gecko_config {
 	IADC_Config_t config;
 	IADC_TypeDef *base;
+	const struct pinctrl_dev_config *pcfg;
 	void (*irq_cfg_func)(void);
 };
 
@@ -277,80 +279,6 @@ static int adc_gecko_read_async(const struct device *dev,
 }
 #endif
 
-static void adc_gecko_gpio_busalloc_pos(IADC_PosInput_t input)
-{
-	uint32_t port = ((input << _IADC_SCAN_PINPOS_SHIFT) &
-			_IADC_SCAN_PORTPOS_MASK) >> _IADC_SCAN_PORTPOS_SHIFT;
-	uint32_t pin = ((input << _IADC_SCAN_PINPOS_SHIFT) &
-			_IADC_SCAN_PINPOS_MASK) >> _IADC_SCAN_PINPOS_SHIFT;
-
-	switch (port) {
-	case _IADC_SCAN_PORTPOS_PORTA:
-		if (pin & 1) {
-			GPIO->ABUSALLOC |= GPIO_ABUSALLOC_AODD0_ADC0;
-		} else {
-			GPIO->ABUSALLOC |= GPIO_ABUSALLOC_AEVEN0_ADC0;
-		}
-		break;
-
-	case _IADC_SCAN_PORTPOS_PORTB:
-		if (pin & 1) {
-			GPIO->BBUSALLOC |= GPIO_BBUSALLOC_BODD0_ADC0;
-		} else {
-			GPIO->BBUSALLOC |= GPIO_BBUSALLOC_BEVEN0_ADC0;
-		}
-		break;
-
-	case _IADC_SCAN_PORTPOS_PORTC:
-	case _IADC_SCAN_PORTPOS_PORTD:
-		if (pin & 1) {
-			GPIO->CDBUSALLOC |= GPIO_CDBUSALLOC_CDODD0_ADC0;
-		} else {
-			GPIO->CDBUSALLOC |= GPIO_CDBUSALLOC_CDEVEN0_ADC0;
-		}
-		break;
-
-	default:
-	}
-}
-
-static void adc_gecko_gpio_busalloc_neg(IADC_NegInput_t input)
-{
-	uint32_t port = ((input << _IADC_SCAN_PINNEG_SHIFT) &
-			_IADC_SCAN_PORTNEG_MASK) >> _IADC_SCAN_PORTNEG_SHIFT;
-	uint32_t pin = ((input << _IADC_SCAN_PINNEG_SHIFT) &
-			_IADC_SCAN_PINNEG_MASK) >> _IADC_SCAN_PINNEG_SHIFT;
-
-	switch (port) {
-	case _IADC_SCAN_PORTNEG_PORTA:
-		if (pin & 1) {
-			GPIO->ABUSALLOC |= GPIO_ABUSALLOC_AODD0_ADC0;
-		} else {
-			GPIO->ABUSALLOC |= GPIO_ABUSALLOC_AEVEN0_ADC0;
-		}
-		break;
-
-	case _IADC_SCAN_PORTNEG_PORTB:
-		if (pin & 1) {
-			GPIO->BBUSALLOC |= GPIO_BBUSALLOC_BODD0_ADC0;
-		} else {
-			GPIO->BBUSALLOC |= GPIO_BBUSALLOC_BEVEN0_ADC0;
-		}
-		break;
-
-	case _IADC_SCAN_PORTNEG_PORTC:
-	case _IADC_SCAN_PORTNEG_PORTD:
-		if (pin & 1) {
-			GPIO->CDBUSALLOC |= GPIO_CDBUSALLOC_CDODD0_ADC0;
-		} else {
-			GPIO->CDBUSALLOC |= GPIO_CDBUSALLOC_CDEVEN0_ADC0;
-		}
-		break;
-
-	default:
-	}
-}
-
 static int adc_gecko_channel_setup(const struct device *dev,
 				const struct adc_channel_cfg *channel_cfg)
 {
@@ -424,10 +352,6 @@ static int adc_gecko_channel_setup(const struct device *dev,
 		return -ENOTSUP;
 	}
 
-	/* Setup GPIO xBUSALLOC registers if channel uses GPIO pin */
-	adc_gecko_gpio_busalloc_pos(channel_config->input_positive);
-	adc_gecko_gpio_busalloc_neg(channel_config->input_negative);
-
 	channel_config->initialized = true;
 	LOG_DBG("Channel setup succeeded!");
 
@@ -436,6 +360,7 @@ static int adc_gecko_channel_setup(const struct device *dev,
 
 static int adc_gecko_init(const struct device *dev)
 {
+	int err;
 	const struct adc_gecko_config *config = dev->config;
 	struct adc_gecko_data *data = dev->data;
 
@@ -443,6 +368,11 @@ static int adc_gecko_init(const struct device *dev)
 
 	/* Select clock for IADC */
 	CMU_ClockSelectSet(cmuClock_IADCCLK, cmuSelect_FSRCO);  /* FSRCO - 20MHz */
+
+	err = pinctrl_apply_state(config->pcfg, PINCTRL_STATE_DEFAULT);
+	if (err < 0 && err != -ENOENT) {
+		return err;
+	}
 
 	data->dev = dev;
 
@@ -463,11 +393,13 @@ static DEVICE_API(adc, api_gecko_adc_driver_api) = {
 };
 
 #define GECKO_IADC_INIT(n)						\
+	PINCTRL_DT_INST_DEFINE(n);					\
 									\
 	static void adc_gecko_config_func_##n(void);			\
 									\
 	const static struct adc_gecko_config adc_gecko_config_##n = {	\
 		.base = (IADC_TypeDef *)DT_INST_REG_ADDR(n),\
+		.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),		\
 		.irq_cfg_func = adc_gecko_config_func_##n,		\
 	};								\
 	static struct adc_gecko_data adc_gecko_data_##n = {		\
