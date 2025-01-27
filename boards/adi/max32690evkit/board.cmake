ed4069c387de6e81f6c2adffed14863bb01adbea
Sadik Ozer <sadik.ozer@analog.com>
2025-01-27 14:41:41 +0300

    boards: adi: Centralize openocd debugger configuration
    Add a common openocd configuration files for MAX32 MCUs
    to simplify maintanence of them.
    After this update it will be easy to add/remove new command
    
    Signed-off-by: Sadik Ozer <sadik.ozer@analog.com>

--- boards/adi/max32690evkit/board.cmake
+++ boards/adi/max32690evkit/board.cmake
@@ -1,9 +1,8 @@
-# Copyright (c) 2023-2024 Analog Devices, Inc.
+# Copyright (c) 2023-2025 Analog Devices, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(openocd --cmd-pre-init "source [find interface/cmsis-dap.cfg]")
-board_runner_args(openocd --cmd-pre-init "source [find target/max32690.cfg]")
 board_runner_args(jlink "--device=MAX32690" "--reset-after-load")
 
+include(${ZEPHYR_BASE}/boards/common/openocd-adi-max32.boards.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
