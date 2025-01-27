412a226dc26b2a350b1292b56062b1beecefd0d9
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2025-01-27 11:20:50 +0100

    boards: native_sim: Detect attempt to configure not existing int
    Similar to e770128c2. Prevent overrunning the irq vector table.
    This is not happening today in tree, but coverity thinks it
    does. Checking for it to prevent it is not a bad idea
    anyhow, so let's do it.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- boards/native/native_sim/irq_handler.c
+++ boards/native/native_sim/irq_handler.c
@@ -256,6 +256,11 @@ void posix_isr_declare(unsigned int irq_p, int flags, void isr_p(const void *),
  */
 void posix_irq_priority_set(unsigned int irq, unsigned int prio, uint32_t flags)
 {
+	if (irq >= N_IRQS) {
+		posix_print_error_and_exit("Attempted to configure not existent interrupt %u\n",
+					   irq);
+		return;
+	}
 	hw_irq_ctrl_prio_set(irq, prio);
 }
 
