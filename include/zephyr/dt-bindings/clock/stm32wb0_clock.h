3b8b266821ccddb9590b8b72217de5858ee08763
Etienne Carriere <etienne.carriere@st.com>
2025-01-27 12:06:16 +0100

    include: zephyr: dt-bindings: clock: factorize STM32_* clock source macros
    Factorize definitions of the macros used to describe in the STM32
    boards device tree the RCC register dedicated to clock source selection
    configuration, instead of replicating the macros per SoC header file.
    For that purpose, use DT cell bit fields that matches all SoCs.
    
    This change also factorizes STM32_MCO_CFGR_* macros that use the same
    packing layout for defining selection of the MCO clocks and for MCO
    pre-scaling factor on SoCs that support the feature.
    
    By the way, reorder argument description in macro inline description
    comment in a standard way (first argument first).
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- include/zephyr/dt-bindings/clock/stm32wb0_clock.h
+++ include/zephyr/dt-bindings/clock/stm32wb0_clock.h
@@ -27,34 +27,6 @@
 #define STM32_PERIPH_BUS_MIN	STM32_CLOCK_BUS_AHB0
 #define STM32_PERIPH_BUS_MAX	STM32_CLOCK_BUS_APB2
 
-#define STM32_CLOCK_REG_MASK	(0xFFFFU)
-#define STM32_CLOCK_REG_SHIFT	(0U)
-#define STM32_CLOCK_SHIFT_MASK  (0x3FU)
-#define STM32_CLOCK_SHIFT_SHIFT (16U)
-#define STM32_CLOCK_MASK_MASK   (0x1FU)
-#define STM32_CLOCK_MASK_SHIFT  (22U)
-#define STM32_CLOCK_VAL_MASK    STM32_CLOCK_MASK_MASK
-#define STM32_CLOCK_VAL_SHIFT   (27U)
-
-/**
- * @brief STM32 clock configuration bit field
- *
- * @param reg	Offset to target configuration register in RCC
- * @param shift	Position of field within RCC register (= field LSB's index)
- * @param mask	Mask of field in RCC register
- * @param val	Field value
- *
- * @note 'reg' range:	0x0~0xFFFF	[ 00 : 15 ]
- * @note 'shift' range:	0~63		[ 16 : 21 ]
- * @note 'mask' range:	0x00~0x1F	[ 22 : 26 ]
- * @note 'val' range:	0x00~0x1F	[ 27 : 31 ]
- */
-#define STM32_DOMAIN_CLOCK(val, mask, shift, reg)				\
-	((((reg) & STM32_CLOCK_REG_MASK) << STM32_CLOCK_REG_SHIFT) |		\
-	 (((shift) & STM32_CLOCK_SHIFT_MASK) << STM32_CLOCK_SHIFT_SHIFT) |	\
-	 (((mask) & STM32_CLOCK_MASK_MASK) << STM32_CLOCK_MASK_SHIFT) |		\
-	 (((val) & STM32_CLOCK_VAL_MASK) << STM32_CLOCK_VAL_SHIFT))
-
 /** @brief RCC_CFGR register offset */
 #define CFGR_REG	0x08
 
