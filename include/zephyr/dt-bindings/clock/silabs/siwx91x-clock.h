ffb1c0de616e207ef027cabc62b1b8a4129c39fe
Jérôme Pouiller <jerome.pouiller@silabs.com>
2025-01-29 22:12:19 +0100

    drivers: clock: Add dumb clock driver for SiWx91x
    This driver is mostly the initial seed for further implementation of a
    real clock driver.
    
    It doesn't allow the user to choose the clock source for the various
    peripherals. The driver hardcodes some sane values.
    
    Note that for now, the driver snps,designware-i2c does not support
    "clocks" attribute. So this patch hardcode the clock configuration in
    the init of the clock driver.
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

---
+++ include/zephyr/dt-bindings/clock/silabs/siwx91x-clock.h
@@ -0,0 +1,16 @@
+/* Copyright (c) 2024 Silicon Laboratories Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_SILABS_SIWX91X_CLOCK_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_SILABS_SIWX91X_CLOCK_H_
+
+#define SIWX91X_CLK_ULP_UART      0
+#define SIWX91X_CLK_ULP_I2C       1
+#define SIWX91X_CLK_ULP_DMA       2
+#define SIWX91X_CLK_UART1         3
+#define SIWX91X_CLK_UART2         4
+#define SIWX91X_CLK_I2C0          5
+#define SIWX91X_CLK_I2C1          6
+#define SIWX91X_CLK_DMA0          7
+
+#endif
