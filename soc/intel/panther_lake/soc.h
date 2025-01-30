5c7e12b53ac3c30cd9e390c877f1639899ea0f80
Anisetti Avinash Krishna <anisetti.avinash.krishna@intel.com>
2025-01-30 10:13:40 +0530

    boards: intel: Added PTL board support
    Added PTL-H board support, PTL SOC and panther_lake.dtsi
    
    Signed-off-by: Anisetti Avinash Krishna <anisetti.avinash.krishna@intel.com>

---
+++ soc/intel/panther_lake/soc.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 Intel Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Board configuration macros for the Panther Lake SoC
+ *
+ * This header file is used to specify and describe soc-level aspects for
+ * the 'Panther Lake' SoC.
+ */
+
+#ifndef __SOC_H_
+#define __SOC_H_
+
+#include <zephyr/sys/util.h>
+
+#ifndef _ASMLANGUAGE
+#include <zephyr/device.h>
+#include <zephyr/random/random.h>
+#endif
+
+#if DT_ON_BUS(DT_CHOSEN(zephyr_console), pcie)
+#include <zephyr/drivers/pcie/pcie.h>
+#define X86_SOC_EARLY_SERIAL_PCIDEV DT_REG_ADDR(DT_CHOSEN(zephyr_console))
+#else
+#define X86_SOC_EARLY_SERIAL_MMIO8_ADDR DT_REG_ADDR(DT_CHOSEN(zephyr_console))
+#endif
+
+#endif /* __SOC_H_ */
