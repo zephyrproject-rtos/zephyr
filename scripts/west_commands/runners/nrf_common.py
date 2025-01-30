043f086b94f9e8f1eefdefa6eaf8532f6e5578cb
Carles Cufi <carles.cufi@nordicsemi.no>
2025-01-30 18:55:00 +0100

    scripts: runners: nrf: Default to soft reset for the nRF52 series
    The Nordic nRF52 series have a peculiarity that is not shared with any
    other Nordic families of SoCs: the reset pin can be reconfigured as a
    regular GPIO. This has an unintended consequence: if the user has
    reconfigured the pin in Devicetree to be a GPIO, `west flash` will
    override that and configure the IC to use it as a reset pin, and the
    firmware at boot won't be able to switch it back to GPIO, because that
    requires a UICR erase. This behavior is very confusing to users, because
    the GPIO does not work at all, since it is now just a reset line.
    
    With this patch, `west flash` defaults to using soft reset instead of
    pin reset for the nRF52 family of devices, to avoid overwriting the
    reset pin configuration that the user includes in the image.
    
    In order to be able to continue to use pin reset for users that so
    desire it, a new option `--pinreset` is added that forces the use of pin
    reset. The existing `--softreset` option is left exactly as it was, but
    it is now applicable only to families other than the nRF52.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- scripts/west_commands/runners/nrf_common.py
+++ scripts/west_commands/runners/nrf_common.py
@@ -78,13 +78,14 @@ def _get_suit_starter():
 class NrfBinaryRunner(ZephyrBinaryRunner):
     '''Runner front-end base class for nrf tools.'''
 
-    def __init__(self, cfg, family, softreset, dev_id, erase=False,
+    def __init__(self, cfg, family, softreset, pinreset, dev_id, erase=False,
                  reset=True, tool_opt=None, force=False, recover=False):
         super().__init__(cfg)
         self.hex_ = cfg.hex_file
         # The old --nrf-family options takes upper-case family names
         self.family = family.lower() if family else None
         self.softreset = softreset
+        self.pinreset = pinreset
         self.dev_id = dev_id
         self.erase = bool(erase)
         self.reset = bool(reset)
@@ -117,9 +118,14 @@ class NrfBinaryRunner(ZephyrBinaryRunner):
                                      'NRF54H', 'NRF91', 'NRF92'],
                             help='''MCU family; still accepted for
                             compatibility only''')
+        # Not using a mutual exclusive group for softreset and pinreset due to
+        # the way dump_runner_option_help() works in run_common.py
         parser.add_argument('--softreset', required=False,
                             action='store_true',
-                            help='use reset instead of pinreset')
+                            help='use softreset instead of pinreset')
+        parser.add_argument('--pinreset', required=False,
+                            action='store_true',
+                            help='use pinreset instead of softreset')
         parser.add_argument('--snr', required=False, dest='dev_id',
                             help='obsolete synonym for -i/--dev-id')
         parser.add_argument('--force', required=False,
@@ -412,13 +418,18 @@ class NrfBinaryRunner(ZephyrBinaryRunner):
 
 
     def reset_target(self):
-        if self.family == 'nrf52' and not self.softreset:
+        # Default to soft reset on nRF52 only, because ICs in these series can
+        # reconfigure the reset pin as a regular GPIO
+        default = "RESET_SYSTEM" if self.family == 'nrf52' else "RESET_PIN"
+        kind = ("RESET_SYSTEM" if self.softreset else "RESET_PIN" if
+               self.pinreset else default)
+
+        if self.family == 'nrf52' and kind == "RESET_PIN":
+            # Write to the UICR enabling nRESET in the corresponding pin
             self.exec_op('pinreset-enable')
 
-        if self.softreset:
-            self.exec_op('reset', kind="RESET_SYSTEM")
-        else:
-            self.exec_op('reset', kind="RESET_PIN")
+        self.logger.debug(f'Reset kind: {kind}')
+        self.exec_op('reset', kind=kind)
 
     @abc.abstractmethod
     def do_require(self):
@@ -487,6 +498,10 @@ class NrfBinaryRunner(ZephyrBinaryRunner):
 
     def do_run(self, command, **kwargs):
         self.do_require()
+
+        if self.softreset and self.pinreset:
+            raise RuntimeError('Options --softreset and --pinreset are mutually '
+                               'exclusive.')
 
         self.ensure_output('hex')
         if IntelHex is None:
