a08a101f3db7ab7a24b544bcc4c25bd9669393c5
Benjamin Cabé <benjamin@zephyrproject.org>
2025-01-31 10:26:16 +0100

    boards: samr34_xpro: add pre_dt_board.cmake
    Add pre-devicetree CMake configuration for SAMR34 Xplained Pro board to
    suppress SPI bus bridge warnings promoted to errors in weekly CI run.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

---
+++ boards/atmel/sam0/samr34_xpro/pre_dt_board.cmake
@@ -0,0 +1,5 @@
+# Copyright (c) 2021 Linaro Limited
+# SPDX-License-Identifier: Apache-2.0
+
+# SPI is implemented via sercom so node name isn't spi@...
+list(APPEND EXTRA_DTC_FLAGS "-Wno-spi_bus_bridge")
