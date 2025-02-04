0bc2e4d3dd1593cede94303b8d3a45be3d3f4517
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2025-02-04 10:40:53 +0100

    tracing: Add sys_trace_idle_exit call
    Add new tracing API which is called when core is exiting from idle.
    Current implementation is using it to track CPU load. Implementation
    in tracing_none is now weak so it can be used if given backend does
    not support new API call.
    
    When CONFIG_CPU_LOAD is enabled then sys_trace_idle also calls a
    hook which stores the timing information when CPU entered idle.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- include/zephyr/tracing/tracing.h
+++ include/zephyr/tracing/tracing.h
@@ -2718,6 +2718,12 @@
 
 #if defined(CONFIG_PERCEPIO_TRACERECORDER)
 #include "tracing_tracerecorder.h"
+
+/**
+ * @brief Called when the cpu exits the idle state
+ */
+void sys_trace_idle_exit(void);
+
 #else
 /**
  * @brief Called when entering an ISR
@@ -2738,6 +2744,12 @@ void sys_trace_isr_exit_to_scheduler(void);
  * @brief Called when the cpu enters the idle state
  */
 void sys_trace_idle(void);
+
+/**
+ * @brief Called when the cpu exits the idle state
+ */
+void sys_trace_idle_exit(void);
+
 #endif /* CONFIG_PERCEPIO_TRACERECORDER */
 
 /**
