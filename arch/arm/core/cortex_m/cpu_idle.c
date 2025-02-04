f857cb47eabb66dd4247c998f7bb1243e6fe8ad8
Wilfried Chauveau <wilfried.chauveau@arm.com>
2025-02-04 19:06:36 +0000

    arch: arm: cortex_m: Apply clang-format on cortex_m related code
    This commit updates cortex_m related code to align it with the rules from
    .clang-format. This is done to simplify future changes in these files as
    we are about to implement use_switch support.
    
    Some rules conflict with checkpatch and therefore some small part of the
    code locally disable clang-format.
    
    Signed-off-by: Wilfried Chauveau <wilfried.chauveau@arm.com>

--- arch/arm/core/cortex_m/cpu_idle.c
+++ arch/arm/core/cortex_m/cpu_idle.c
@@ -30,27 +30,31 @@ void z_arm_cpu_idle_init(void)
 #if defined(CONFIG_ARM_ON_EXIT_CPU_IDLE)
 #define ON_EXIT_IDLE_HOOK SOC_ON_EXIT_CPU_IDLE
 #else
-#define ON_EXIT_IDLE_HOOK do {} while (false)
+#define ON_EXIT_IDLE_HOOK                                                                          \
+	do {                                                                                       \
+	} while (false)
 #endif
 
 #if defined(CONFIG_ARM_ON_ENTER_CPU_IDLE_HOOK)
-#define SLEEP_IF_ALLOWED(wait_instr) do { \
-	/* Skip the wait instr if on_enter_cpu_idle returns false */ \
-	if (z_arm_on_enter_cpu_idle()) { \
-		/* Wait for all memory transaction to complete */ \
-		/* before entering low power state. */ \
-		__DSB(); \
-		wait_instr(); \
-		/* Inline the macro provided by SoC-specific code */ \
-		ON_EXIT_IDLE_HOOK; \
-	} \
-} while (false)
+#define SLEEP_IF_ALLOWED(wait_instr)                                                               \
+	do {                                                                                       \
+		/* Skip the wait instr if on_enter_cpu_idle returns false */                       \
+		if (z_arm_on_enter_cpu_idle()) {                                                   \
+			/* Wait for all memory transaction to complete */                          \
+			/* before entering low power state. */                                     \
+			__DSB();                                                                   \
+			wait_instr();                                                              \
+			/* Inline the macro provided by SoC-specific code */                       \
+			ON_EXIT_IDLE_HOOK;                                                         \
+		}                                                                                  \
+	} while (false)
 #else
-#define SLEEP_IF_ALLOWED(wait_instr) do { \
-	__DSB(); \
-	wait_instr(); \
-	ON_EXIT_IDLE_HOOK; \
-} while (false)
+#define SLEEP_IF_ALLOWED(wait_instr)                                                               \
+	do {                                                                                       \
+		__DSB();                                                                           \
+		wait_instr();                                                                      \
+		ON_EXIT_IDLE_HOOK;                                                                 \
+	} while (false)
 #endif
 
 #ifndef CONFIG_ARCH_HAS_CUSTOM_CPU_IDLE
--- include/zephyr/arch/arm/cortex_m/memory_map.h
+++ include/zephyr/arch/arm/cortex_m/memory_map.h
@@ -18,8 +18,8 @@
 #include <zephyr/sys/util.h>
 
 /* 0x00000000 -> 0x1fffffff: Code in ROM [0.5 GB] */
-#define _CODE_BASE_ADDR           0x00000000
-#define _CODE_END_ADDR            0x1FFFFFFF
+#define _CODE_BASE_ADDR 0x00000000
+#define _CODE_END_ADDR  0x1FFFFFFF
 
 /* 0x20000000 -> 0x3fffffff: SRAM [0.5GB] */
 #define _SRAM_BASE_ADDR           0x20000000
@@ -38,78 +38,77 @@
 #define _PERI_END_ADDR            0x5FFFFFFF
 
 /* 0x60000000 -> 0x9fffffff: external RAM [1GB] */
-#define _ERAM_BASE_ADDR           0x60000000
-#define _ERAM_END_ADDR            0x9FFFFFFF
+#define _ERAM_BASE_ADDR 0x60000000
+#define _ERAM_END_ADDR  0x9FFFFFFF
 
 /* 0xa0000000 -> 0xdfffffff: external devices [1GB] */
-#define _EDEV_BASE_ADDR           0xA0000000
-#define _EDEV_END_ADDR            0xDFFFFFFF
+#define _EDEV_BASE_ADDR 0xA0000000
+#define _EDEV_END_ADDR  0xDFFFFFFF
 
 /* 0xe0000000 -> 0xffffffff: varies by processor (see below) */
 
 /* 0xe0000000 -> 0xe00fffff: private peripheral bus */
 /* 0xe0000000 -> 0xe003ffff: internal [256KB] */
-#define _PPB_INT_BASE_ADDR        0xE0000000
-#if defined(CONFIG_CPU_CORTEX_M0) || defined(CONFIG_CPU_CORTEX_M0PLUS) || \
+#define _PPB_INT_BASE_ADDR 0xE0000000
+#if defined(CONFIG_CPU_CORTEX_M0) || defined(CONFIG_CPU_CORTEX_M0PLUS) ||                          \
 	defined(CONFIG_CPU_CORTEX_M1)
-#define _PPB_INT_RSVD_0           0xE0000000
-#define _PPB_INT_DWT              0xE0001000
-#define _PPB_INT_BPU              0xE0002000
-#define _PPB_INT_RSVD_1           0xE0003000
-#define _PPB_INT_SCS              0xE000E000
-#define _PPB_INT_RSVD_2           0xE000F000
-#elif defined(CONFIG_CPU_CORTEX_M3) || defined(CONFIG_CPU_CORTEX_M4) || defined(CONFIG_CPU_CORTEX_M7)
-#define _PPB_INT_ITM              0xE0000000
-#define _PPB_INT_DWT              0xE0001000
-#define _PPB_INT_FPB              0xE0002000
-#define _PPB_INT_RSVD_1           0xE0003000
-#define _PPB_INT_SCS              0xE000E000
-#define _PPB_INT_RSVD_2           0xE000F000
-#elif defined(CONFIG_CPU_CORTEX_M23) || \
-	defined(CONFIG_CPU_CORTEX_M33) || \
-	defined(CONFIG_CPU_CORTEX_M55) || \
-	defined(CONFIG_CPU_CORTEX_M85)
-#define _PPB_INT_RSVD_0           0xE0000000
-#define _PPB_INT_SCS              0xE000E000
-#define _PPB_INT_SCB              0xE000ED00
-#define _PPB_INT_RSVD_1           0xE002E000
+#define _PPB_INT_RSVD_0 0xE0000000
+#define _PPB_INT_DWT    0xE0001000
+#define _PPB_INT_BPU    0xE0002000
+#define _PPB_INT_RSVD_1 0xE0003000
+#define _PPB_INT_SCS    0xE000E000
+#define _PPB_INT_RSVD_2 0xE000F000
+#elif defined(CONFIG_CPU_CORTEX_M3) || defined(CONFIG_CPU_CORTEX_M4) ||                            \
+	defined(CONFIG_CPU_CORTEX_M7)
+#define _PPB_INT_ITM    0xE0000000
+#define _PPB_INT_DWT    0xE0001000
+#define _PPB_INT_FPB    0xE0002000
+#define _PPB_INT_RSVD_1 0xE0003000
+#define _PPB_INT_SCS    0xE000E000
+#define _PPB_INT_RSVD_2 0xE000F000
+#elif defined(CONFIG_CPU_CORTEX_M23) || defined(CONFIG_CPU_CORTEX_M33) ||                          \
+	defined(CONFIG_CPU_CORTEX_M55) || defined(CONFIG_CPU_CORTEX_M85)
+#define _PPB_INT_RSVD_0 0xE0000000
+#define _PPB_INT_SCS    0xE000E000
+#define _PPB_INT_SCB    0xE000ED00
+#define _PPB_INT_RSVD_1 0xE002E000
 #else
 #error Unknown CPU
 #endif
-#define _PPB_INT_END_ADDR         0xE003FFFF
+#define _PPB_INT_END_ADDR 0xE003FFFF
 
 /* 0xe0000000 -> 0xe00fffff: private peripheral bus */
 /* 0xe0040000 -> 0xe00fffff: external [768K] */
-#define _PPB_EXT_BASE_ADDR        0xE0040000
-#if defined(CONFIG_CPU_CORTEX_M0) || defined(CONFIG_CPU_CORTEX_M0PLUS) \
-	|| defined(CONFIG_CPU_CORTEX_M1) || defined(CONFIG_CPU_CORTEX_M23)
+#define _PPB_EXT_BASE_ADDR 0xE0040000
+#if defined(CONFIG_CPU_CORTEX_M0) || defined(CONFIG_CPU_CORTEX_M0PLUS) ||                          \
+	defined(CONFIG_CPU_CORTEX_M1) || defined(CONFIG_CPU_CORTEX_M23)
 #elif defined(CONFIG_CPU_CORTEX_M3) || defined(CONFIG_CPU_CORTEX_M4)
-#define _PPB_EXT_TPIU             0xE0040000
-#define _PPB_EXT_ETM              0xE0041000
-#define _PPB_EXT_PPB              0xE0042000
-#define _PPB_EXT_ROM_TABLE        0xE00FF000
-#define _PPB_EXT_END_ADDR         0xE00FFFFF
-#elif defined(CONFIG_CPU_CORTEX_M33) || defined(CONFIG_CPU_CORTEX_M55) \
-	|| defined(CONFIG_CPU_CORTEX_M85)
-#undef  _PPB_EXT_BASE_ADDR
-#define _PPB_EXT_BASE_ADDR        0xE0044000
-#define _PPB_EXT_ROM_TABLE        0xE00FF000
-#define _PPB_EXT_END_ADDR         0xE00FFFFF
+#define _PPB_EXT_TPIU      0xE0040000
+#define _PPB_EXT_ETM       0xE0041000
+#define _PPB_EXT_PPB       0xE0042000
+#define _PPB_EXT_ROM_TABLE 0xE00FF000
+#define _PPB_EXT_END_ADDR  0xE00FFFFF
+#elif defined(CONFIG_CPU_CORTEX_M33) || defined(CONFIG_CPU_CORTEX_M55) ||                          \
+	defined(CONFIG_CPU_CORTEX_M85)
+#undef _PPB_EXT_BASE_ADDR
+#define _PPB_EXT_BASE_ADDR 0xE0044000
+#define _PPB_EXT_ROM_TABLE 0xE00FF000
+#define _PPB_EXT_END_ADDR  0xE00FFFFF
 #elif defined(CONFIG_CPU_CORTEX_M7)
-#define _PPB_EXT_BASE_ADDR        0xE0040000
-#define _PPB_EXT_RSVD_TPIU        0xE0040000
-#define _PPB_EXT_ETM              0xE0041000
-#define _PPB_EXT_CTI              0xE0042000
-#define _PPB_EXT_PPB              0xE0043000
-#define _PPB_EXT_PROC_ROM_TABLE   0xE00FE000
-#define _PPB_EXT_PPB_ROM_TABLE    0xE00FF000
+#define _PPB_EXT_BASE_ADDR      0xE0040000
+#define _PPB_EXT_RSVD_TPIU      0xE0040000
+#define _PPB_EXT_ETM            0xE0041000
+#define _PPB_EXT_CTI            0xE0042000
+#define _PPB_EXT_PPB            0xE0043000
+#define _PPB_EXT_PROC_ROM_TABLE 0xE00FE000
+#define _PPB_EXT_PPB_ROM_TABLE  0xE00FF000
 #else
 #error Unknown CPU
 #endif
-#define _PPB_EXT_END_ADDR         0xE00FFFFF
+#define _PPB_EXT_END_ADDR 0xE00FFFFF
 
 /* 0xe0100000 -> 0xffffffff: vendor-specific [0.5GB-1MB or 511MB]  */
-#define _VENDOR_BASE_ADDR         0xE0100000
-#define _VENDOR_END_ADDR          0xFFFFFFFF
+#define _VENDOR_BASE_ADDR 0xE0100000
+#define _VENDOR_END_ADDR  0xFFFFFFFF
 
 #endif /* ZEPHYR_INCLUDE_ARCH_ARM_CORTEX_M_MEMORY_MAP_H_ */
