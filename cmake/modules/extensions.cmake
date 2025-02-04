9517de34e7b8477abe2b2a8cd10dc47b5fc112db
Björn Bergman <bjorn.bergman@iar.com>
2025-02-04 17:30:13 +0100

    cmake: linker generator: Functions for input from prev linker pass
    Add methods for the linker-script generator to pick up information
    from previus linkerpasses:
     * zephyr_linker_include_generated(FILE f.cmake [PASS X]) cmake-includes
       f.cmake in the relevant PASSes. This allows scripts to generate any
       content (e.g. sections or section configs) as if it was generated by
       the main cmake machiery. This is intended to cover the case of
       ld-script snippets generated by e.g. gen_app_partitions.py
     * @FOO[,undef:v]@ in an value or expression in the zephyr_linker_*
       inputs. This looks for a defintion of FOO (se below), and uses its
       value if found. If FOO is not defined, the whole @@ thing is left
       untouched, unless ,undef: is used, in which case v is used instead.
     * zephyr_linker_include_generated(FILE f.h [PASS x]) greps for f.h
       for #define FOO value to be accessed as @FOO@
     * zephyr_linker_include_var(VAR FOO VALUE V) allows the main
       cmake-script to set a @FOO@ variable.
    
    Note that the #define support is VERY basic, and does not use a proper
    pre-processor. It works for the current use of files generated by e.g.
    gen_kobject_placeholders.py but is not a general solution.
    
    Signed-off-by: Björn Bergman <bjorn.bergman@iar.com>

--- cmake/modules/extensions.cmake
+++ cmake/modules/extensions.cmake
@@ -5252,7 +5252,7 @@ endfunction()
 #   zephyr_linker_section_configure(SECTION <section> [ALIGN <alignment>]
 #                                   [PASS [NOT] <name>] [PRIO <no>] [SORT <sort>]
 #                                   [MIN_SIZE <minimum size>] [MAX_SIZE <maximum size>]
-#                                   [ANY] [FIRST] [KEEP] [INPUT <input>]
+#                                   [ANY] [FIRST] [KEEP] [INPUT <input>] [SYMBOLS [<start>[<end>]]]
 #   )
 #
 # Configure an output section with additional input sections.
@@ -5292,6 +5292,7 @@ endfunction()
 # ANY                 : ANY section flag in scatter file.
 #                       The FLAGS and ANY arguments only has effect for scatter files.
 # INPUT <input>       : Input section name or list of input section names.
+#
 function(zephyr_linker_section_configure)
   set(options     "ANY;FIRST;KEEP")
   set(single_args "ALIGN;OFFSET;PRIO;SECTION;SORT;MIN_SIZE;MAX_SIZE")
@@ -5362,6 +5363,59 @@ function(zephyr_linker_symbol)
 endfunction()
 
 # Usage:
+#   zephyr_linker_include_generated(FILE <name> [PASS <pass>])
+#
+# Add file that is generated at build-time to be included when running the
+# linker script generator.
+#
+# FILE <name>      : File to be included. *.h are grepped for #defines for
+#                    @variables@ and *.cmake are passed to include()
+# PASS [NOT] <pass>: Rule for which PASSES to include file.
+#                    see zephyr_linker_section PASS
+#
+function(zephyr_linker_include_generated)
+  set(single_args "FILE")
+  set(multi_args "PASS")
+  cmake_parse_arguments(INCLUDE "" "${single_args}" "${multi_args}" ${ARGN})
+  if(DEFINED INCLUDE_PASS)
+    zephyr_linker_check_pass_param("zephyr_linker_include_generated" "${INCLUDE_PASS}")
+  endif()
+  set(INCLUDE)
+  zephyr_linker_arg_val_list(INCLUDE "${single_args}")
+  zephyr_linker_arg_val_list(INCLUDE "${multi_args}")
+  string(REPLACE ";" "\;" INCLUDE "${INCLUDE}")
+  set_property(TARGET linker
+               APPEND PROPERTY INCLUDES "{${INCLUDE}}")
+endfunction()
+
+# Usage:
+#   zephyr_linker_include_var(VAR <name> [VALUE <value>])
+#
+# Save the value of <name> for when the generator is running at build-time.
+# If VALUE isn't set, the current value of the variable is used
+#
+# VAR <name>       : Variable to be set
+# VALUE <value>    : The value
+#
+function(zephyr_linker_include_var)
+  set(single_args "VAR;VALUE")
+  cmake_parse_arguments(VAR "" "${single_args}" "" ${ARGN})
+  if(NOT DEFINED VAR_VAR)
+    message(FATAL_ERROR "zephyr_linker_include_var(${ARGV0} ...) must have VAR <variable> ")
+  endif()
+  if(NOT DEFINED VAR_VALUE)
+    if(DEFINED ${VAR_VAR})
+      set(VAR_VALUE ${${VAR_VAR}})
+    else()
+      message(FATAL_ERROR "zephyr_linker_include_var(${ARGV0} ...) value not set ")
+    endif()
+  endif()
+
+  set_property(TARGET linker
+               APPEND PROPERTY VARIABLES "set(${VAR_VAR} \"${VAR_VALUE}\" CACHE INTERNAL \"\")")
+endfunction()
+
+# Usage:
 #   zephyr_linker_generate_linker_settings_file([FILE <name>] [STRING <string>])
 #
 # Add the content generated so far by the zephyr_linker_* functions to a file
@@ -5373,6 +5427,9 @@ endfunction()
 function(zephyr_linker_generate_linker_settings_file)
   set(single_args "FILE;STRING")
   cmake_parse_arguments(GEN "" "${single_args}" "" ${ARGN})
+  get_property(list_of_variables TARGET linker PROPERTY VARIABLES)
+  list(JOIN list_of_variables "\n" VARS)
+
   set(CONTENT
          "set(FORMAT \"$<TARGET_PROPERTY:linker,FORMAT>\" CACHE INTERNAL \"\")\n
           set(ENTRY \"$<TARGET_PROPERTY:linker,ENTRY>\" CACHE INTERNAL \"\")\n
@@ -5382,6 +5439,7 @@ function(zephyr_linker_generate_linker_settings_file)
           set(SECTION_SETTINGS \"$<TARGET_PROPERTY:linker,SECTION_SETTINGS>\" CACHE INTERNAL \"\")\n
           set(SYMBOLS \"$<TARGET_PROPERTY:linker,SYMBOLS>\" CACHE INTERNAL \"\")\n
           set(INCLUDES \"$<TARGET_PROPERTY:linker,INCLUDES>\" CACHE INTERNAL \"\")\n
+          ${VARS}
          ")
   if(DEFINED GEN_FILE)
     file(GENERATE OUTPUT ${GEN_FILE} CONTENT "${CONTENT}")
