22de0b3732476c8b16a4c11a7c1724f57918e790
alperen sener <alperen.sener@nordicsemi.no>
2025-02-06 13:30:48 +0100

    bluetooth: host/crypto: fix the psa crypto init for host
    psa_crypto_init was bounded to CONFIG_BT_HOST_CRYPTO_PRNG and
    used to be called under prng_init. Updating the ifdef condition
    and appropriating the function name for crypto init.
    
    Also it is better to make sure psa_crypto_init called by host.
    
    Signed-off-by: alperen sener <alperen.sener@nordicsemi.no>

--- subsys/bluetooth/host/hci_core.c
+++ subsys/bluetooth/host/hci_core.c
@@ -3287,11 +3287,9 @@ static int common_init(void)
 	read_supported_commands_complete(rsp);
 	net_buf_unref(rsp);
 
-	if (IS_ENABLED(CONFIG_BT_HOST_CRYPTO_PRNG)) {
-		/* Initialize the PRNG so that it is safe to use it later
-		 * on in the initialization process.
-		 */
-		err = prng_init();
+	if (IS_ENABLED(CONFIG_BT_HOST_CRYPTO)) {
+		/* Initialize crypto for host */
+		err = bt_crypto_init();
 		if (err) {
 			return err;
 		}
