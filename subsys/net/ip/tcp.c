73d225e4434d627d16fbb6759942bfc223847b8b
Noah Olson <noah@wavelynx.com>
2025-02-05 17:31:20 -0700

    net: tcp: fix ECONNREFUSED not reported by zsock_connect
    When a TCP connection is refused during zsock_connect, errno is set
    to -ENOTCONN, but errno should be set to -ECONNREFUSED. This change
    causes the ECONNREFUSED status to be propagated from tcp_in to
    net_tcp_connect, which eventually causes errno to be set
    to -ECONNREFUSED.
    
    Signed-off-by: Noah Olson <noah@wavelynx.com>

--- subsys/net/ip/tcp.c
+++ subsys/net/ip/tcp.c
@@ -2853,6 +2853,7 @@ static enum net_verdict tcp_in(struct tcp *conn, struct net_pkt *pkt)
 		net_stats_update_tcp_seg_rst(net_pkt_iface(pkt));
 		do_close = true;
 		close_status = -ECONNRESET;
+		conn->rst_received = true;
 
 		/* If we receive RST and ACK for the sent SYN, it means
 		 * that there is no socket listening the port we are trying
@@ -3951,7 +3952,11 @@ int net_tcp_connect(struct net_context *context,
 
 	if (!IS_ENABLED(CONFIG_NET_TEST_PROTOCOL)) {
 		if (conn->state == TCP_UNUSED || conn->state == TCP_CLOSED) {
-			ret = -ENOTCONN;
+			if (conn->rst_received) {
+				ret = -ECONNREFUSED;
+			} else {
+				ret = -ENOTCONN;
+			}
 			goto out_unref;
 		} else if ((K_TIMEOUT_EQ(timeout, K_NO_WAIT)) &&
 			   conn->state != TCP_ESTABLISHED) {
@@ -3964,7 +3969,11 @@ int net_tcp_connect(struct net_context *context,
 				tcp_conn_close(conn, -ETIMEDOUT);
 			}
 
-			ret = -ETIMEDOUT;
+			if (conn->rst_received) {
+				ret = -ECONNREFUSED;
+			} else {
+				ret = -ETIMEDOUT;
+			}
 			goto out_unref;
 		}
 		conn->in_connect = false;
