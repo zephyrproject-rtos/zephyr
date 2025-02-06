ca1fa6d16243f93109bbcf5cb584ea3ad9de8215
Jordan Yates <jordan@embeint.com>
2025-02-06 11:22:57 +1000

    tests: bsim: bluetooth: central: conn timeout under load
    Validate that the connection timeout code path works as expected when
    the command buffer pool is under stress.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- tests/bsim/bluetooth/host/central/src/main.c
+++ tests/bsim/bluetooth/host/central/src/main.c
@@ -7,6 +7,7 @@
 #include "bstests.h"
 #include "babblekit/testcase.h"
 #include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/hci.h>
 
 /* Include conn_internal for the purpose of checking reference counts. */
 #include "host/conn_internal.h"
@@ -42,7 +43,7 @@ static struct bt_conn_cb conn_cb = {
 	.connected = connected_cb,
 };
 
-static void test_central_connect_timeout_with_timeout(uint32_t timeout_ms)
+static void test_central_connect_timeout_with_timeout(uint32_t timeout_ms, bool stack_load)
 {
 	int err;
 	struct bt_conn *conn;
@@ -62,6 +63,7 @@ static void test_central_connect_timeout_with_timeout(uint32_t timeout_ms)
 		.window_coded = 0,
 		.timeout = timeout_ms / 10,
 	};
+	struct net_buf *bufs[CONFIG_BT_BUF_CMD_TX_COUNT];
 
 	k_sem_reset(&sem_failed_to_connect);
 
@@ -69,6 +71,21 @@ static void test_central_connect_timeout_with_timeout(uint32_t timeout_ms)
 
 	err = bt_conn_le_create(&peer, &create_param, BT_LE_CONN_PARAM_DEFAULT, &conn);
 	TEST_ASSERT(err == 0, "Failed starting initiator (err %d)", err);
+
+	if (stack_load) {
+		/* Claim all the buffers so that the stack cannot handle the timeout */
+		for (int i = 0; i < CONFIG_BT_BUF_CMD_TX_COUNT; i++) {
+			bufs[i] = bt_hci_cmd_create(BT_HCI_LE_ADV_ENABLE, 0);
+			TEST_ASSERT(bufs[i] != NULL, "Failed to claim all command buffers");
+		}
+		/* Hold all the buffers until after we expect the connection to timeout */
+		err = k_sem_take(&sem_failed_to_connect, K_MSEC(expected_conn_timeout_ms + 50));
+		TEST_ASSERT(err == -EAGAIN, "Callback ran with no buffers available", err);
+		/* Release all the buffers back to the stack */
+		for (int i = 0; i < CONFIG_BT_BUF_CMD_TX_COUNT; i++) {
+			net_buf_unref(bufs[i]);
+		}
+	}
 
 	err = k_sem_take(&sem_failed_to_connect, K_MSEC(2 * expected_conn_timeout_ms));
 	TEST_ASSERT(err == 0, "Failed getting connected timeout within %d s (err %d)",
@@ -96,8 +113,9 @@ static void test_central_connect_timeout(void)
 	err = bt_enable(NULL);
 	TEST_ASSERT(err == 0, "Can't enable Bluetooth (err %d)", err);
 
-	test_central_connect_timeout_with_timeout(0);
-	test_central_connect_timeout_with_timeout(1000);
+	test_central_connect_timeout_with_timeout(0, false);
+	test_central_connect_timeout_with_timeout(1000, false);
+	test_central_connect_timeout_with_timeout(2000, true);
 
 	TEST_PASS("Correct timeout");
 }
