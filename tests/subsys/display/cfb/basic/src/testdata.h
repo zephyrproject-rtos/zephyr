ef861d726d211dcc738da3f011455d316f9e0bd4
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2025-02-07 22:35:48 +0900

    fb: cfb: Improve handling of missing characters in font
    Characters not present in the font were replaced with a space character.
    This is changed to draw **tofu** instead.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- tests/subsys/display/cfb/basic/src/testdata.h
+++ tests/subsys/display/cfb/basic/src/testdata.h
@@ -12,13 +12,20 @@
 extern uint32_t diagonal3224[];
 extern uint32_t kerning_3_12rectspace1016[];
 extern uint32_t kerning_3_2rectspace1016[];
+extern uint32_t kerning_3_2tofu1016[];
 extern uint32_t kerning_3_rightclip_1_2rectspace1016[];
+extern uint32_t kerning_3_rightclip_1_2tofu1016[];
 extern uint32_t outside_bottom_left[];
+extern uint32_t outside_bottom_left_tofu1016[];
 extern uint32_t outside_bottom_right[];
+extern uint32_t outside_bottom_right_tofu1016[];
 extern uint32_t outside_top_left[];
+extern uint32_t outside_top_left_tofu1016[];
 extern uint32_t outside_top_right[];
+extern uint32_t outside_top_right_tofu1016[];
 extern uint32_t rectspace1016[];
 extern uint32_t rectspace1123[];
+extern uint32_t tofu1016[];
 extern uint32_t circle10[];
 
 #endif /* TESTS_SUBSYS_DISPLAY_CFB_DRAW_TEXT_AND_PRINT_SRC_TESTDATA_H__ */
