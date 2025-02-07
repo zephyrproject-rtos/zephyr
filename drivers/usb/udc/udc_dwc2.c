c6e3843f09b21b7a838e4a634244ddb0e6b0a0cf
Tomasz Moń <tomasz.mon@nordicsemi.no>
2025-02-07 08:35:02 +0100

    drivers: udc: Do not allow lock/unlock to fail
    USB stack does not check api->lock() and api->unlock() return value and
    all UDC drivers block without timeout in its lock() and unlock() api
    implementations. There is no realistic way to handle lock() and unlock()
    errors without making USB device stack API unnecessarily complex.
    
    Remove the return type from lock() and unlock() to make it clear that
    the functions must not fail.
    
    Signed-off-by: Tomasz Moń <tomasz.mon@nordicsemi.no>

--- drivers/usb/udc/udc_dwc2.c
+++ drivers/usb/udc/udc_dwc2.c
@@ -2316,14 +2316,14 @@ static int dwc2_driver_preinit(const struct device *dev)
 	return 0;
 }
 
-static int udc_dwc2_lock(const struct device *dev)
+static void udc_dwc2_lock(const struct device *dev)
 {
-	return udc_lock_internal(dev, K_FOREVER);
+	udc_lock_internal(dev, K_FOREVER);
 }
 
-static int udc_dwc2_unlock(const struct device *dev)
+static void udc_dwc2_unlock(const struct device *dev)
 {
-	return udc_unlock_internal(dev);
+	udc_unlock_internal(dev);
 }
 
 static void dwc2_on_bus_reset(const struct device *dev)
