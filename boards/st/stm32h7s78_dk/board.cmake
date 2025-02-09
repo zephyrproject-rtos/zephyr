f3bed112b07b403ef7129e76f049b2677b1227c0
Abderrahmane JARMOUNI <git@jarmouni.me>
2025-02-09 01:19:47 +0100

    boards: stm32h7s78-dk: add memory partitions for mcuboot
    Add memory partitions for mcuboot and storage
    Also add config to program external XSPI Flash
    
    Signed-off-by: Abderrahmane JARMOUNI <git@jarmouni.me>

--- boards/st/stm32h7s78_dk/board.cmake
+++ boards/st/stm32h7s78_dk/board.cmake
@@ -1,7 +1,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # keep first
+if(CONFIG_STM32_MEMMAP)
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+board_runner_args(stm32cubeprogrammer "--extload=MX66UW1G45G_STM32H7S78-DK.stldr")
+else()
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+endif()
+
 board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
 
 # keep first
