9ae310b9234b88941abcfcc06fceb0aa2f8abd69
Mahesh Mahadevan <mahesh.mahadevan@nxp.com>
2025-02-10 16:46:51 -0600

    soc: nxp_mxrt7xx: Fix cache implementation for CPU0
    This SoC has an external XCACHE controller for CPU0
    instruction and data bus.
    Add code to enable the data cache. Instruction cache
    is already enabled by SystemInit.
    
    Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

--- soc/nxp/imxrt/imxrt7xx/cm33/soc.c
+++ soc/nxp/imxrt/imxrt7xx/cm33/soc.c
@@ -15,7 +15,17 @@
 #include <zephyr/init.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/linker/sections.h>
+#include <zephyr/cache.h>
 #include <soc.h>
+
+void soc_early_init_hook(void)
+{
+	/* Enable data cache */
+	sys_cache_data_enable();
+
+	__ISB();
+	__DSB();
+}
 
 #ifdef CONFIG_SOC_RESET_HOOK
 
