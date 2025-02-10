3133de07ef17f3212b4613e100db42dd78546140
Benjamin Cabé <benjamin@zephyrproject.org>
2025-02-10 12:05:04 +0100

    samples: led_strip: Increase LED color intensity to full brightness
    Due to what looks like an oversight, the LED color values in the LED
    strip sample were not set to full brightness, i.e. 0x0f instead of 0xff.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- samples/drivers/led/led_strip/src/main.c
+++ samples/drivers/led/led_strip/src/main.c
@@ -32,9 +32,9 @@ LOG_MODULE_REGISTER(main);
 #define RGB(_r, _g, _b) { .r = (_r), .g = (_g), .b = (_b) }
 
 static const struct led_rgb colors[] = {
-	RGB(0x0f, 0x00, 0x00), /* red */
-	RGB(0x00, 0x0f, 0x00), /* green */
-	RGB(0x00, 0x00, 0x0f), /* blue */
+	RGB(0xff, 0x00, 0x00), /* red */
+	RGB(0x00, 0xff, 0x00), /* green */
+	RGB(0x00, 0x00, 0xff), /* blue */
 };
 
 static struct led_rgb pixels[STRIP_NUM_PIXELS];
