0dd0ebf51c8832f7132a4dbfb5e315f81045fa84
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2025-02-14 12:15:54 +0200

    tests: net: utils: Add tests for IP address string with mask
    Add tests that will verify that IP address with an optional mask
    string is properly parsed.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- tests/net/utils/src/main.c
+++ tests/net/utils/src/main.c
@@ -863,6 +863,435 @@ ZTEST(test_utils_fn, test_addr_parse)
 #endif
 }
 
+#if defined(CONFIG_NET_IPV4) && defined(CONFIG_NET_IPV6)
+static const char *check_ipaddr(const char *addresses)
+{
+	do {
+		char addr_str[NET_IPV6_ADDR_LEN + 4 + 1];
+		char expecting[NET_IPV6_ADDR_LEN + 4 + 1];
+		const char *orig = addresses;
+		struct sockaddr_storage addr;
+		struct sockaddr_storage mask;
+		uint8_t mask_len;
+		int ret;
+
+		memset(&addr, 0, sizeof(addr));
+		mask_len = 0;
+
+		memset(addr_str, 0, sizeof(addr_str));
+		memset(expecting, 0, sizeof(expecting));
+
+		addresses = net_ipaddr_parse_mask(addresses, strlen(addresses),
+						  (struct sockaddr *)&addr, &mask_len);
+		zassert_not_null(addresses, "Invalid parse, expecting \"%s\"", orig);
+
+		strncpy(expecting, orig,
+			*addresses == '\0' ? strlen(orig) : addresses - orig - 1);
+
+		(void)net_addr_ntop(addr.ss_family,
+				    &net_sin((struct sockaddr *)&addr)->sin_addr,
+				    addr_str, sizeof(addr_str));
+
+		ret = net_mask_len_to_netmask(addr.ss_family, mask_len,
+					      (struct sockaddr *)&mask);
+		zassert_equal(ret, 0, "Failed to convert mask %d", mask_len);
+
+		ret = net_netmask_to_mask_len(addr.ss_family,
+					      (struct sockaddr *)&mask,
+					      &mask_len);
+		zassert_equal(ret, 0, "Failed to convert mask %s",
+			      net_sprint_addr(addr.ss_family,
+					      (const void *)&net_sin(
+						      ((struct sockaddr *)&mask))->sin_addr));
+
+		if (net_sin((struct sockaddr *)&mask)->sin_addr.s_addr != 0) {
+			int addr_len = strlen(addr_str);
+
+			snprintk(addr_str + addr_len,
+				 sizeof(addr_str) - addr_len,
+				 "/%d", mask_len);
+		}
+
+		zassert_mem_equal(addr_str, expecting,
+				  *addresses == '\0' ? strlen(orig) : addresses - orig - 1,
+				  "Address mismatch, expecing %s, got %s (len %d)\n",
+				  expecting, addr_str, addresses - orig - 1);
+	} while (addresses != NULL && *addresses != '\0');
+
+	return addresses;
+}
+#endif /* CONFIG_NET_IPV4 && CONFIG_NET_IPV6 */
+
+ZTEST(test_utils_fn, test_addr_parse_mask)
+{
+	struct sockaddr addr;
+	uint8_t mask_len;
+	const char *next;
+	int i;
+#if defined(CONFIG_NET_IPV4)
+	static const struct {
+		const char *address;
+		int len;
+		struct sockaddr_in result;
+		uint8_t mask_len;
+		const char *verdict;
+	} parse_ipv4_entries[] = {
+		{
+			.address = "192.0.2.1:80",
+			.len = sizeof("192.0.2.1:80") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "192.0.2.2",
+			.len = sizeof("192.0.2.2") - 1,
+			.result = {
+				.sin_family = AF_INET,
+				.sin_addr = {
+					.s4_addr[0] = 192,
+					.s4_addr[1] = 0,
+					.s4_addr[2] = 2,
+					.s4_addr[3] = 2
+				}
+			},
+			.verdict = "",
+		},
+		{
+			.address = "192.0.2.3/foobar",
+			.len = sizeof("192.0.2.3/foobar") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "127.0.0.42,1.2.3.4",
+			.len = sizeof("127.0.0.42,1.2.3.4") - 1,
+			.result = {
+				.sin_family = AF_INET,
+				.sin_addr = {
+					.s4_addr[0] = 127,
+					.s4_addr[1] = 0,
+					.s4_addr[2] = 0,
+					.s4_addr[3] = 42
+				}
+			},
+			.mask_len = 32,
+			.verdict = &"127.0.0.42,1.2.3.4"[11],
+		},
+		{
+			.address = "127.0.0.42/8,1.2.3.4",
+			.len = sizeof("127.0.0.42/8,1.2.3.4") - 1,
+			.result = {
+				.sin_family = AF_INET,
+				.sin_addr = {
+					.s4_addr[0] = 127,
+					.s4_addr[1] = 0,
+					.s4_addr[2] = 0,
+					.s4_addr[3] = 42
+				}
+			},
+			.mask_len = 8,
+			.verdict = &"127.0.0.42/8,1.2.3.4"[13],
+		},
+		{
+			.address = "192.0.2.3:80/foobar",
+			.len = sizeof("192.0.2.3:80/foobar") - 1,
+			.verdict = false
+		},
+		{
+			.address = "192.168.1.1:65536/foobar",
+			.len = sizeof("192.168.1.1:65536") - 1,
+			.verdict = false
+		},
+		{
+			.address = "192.0.2.3:80/foobar",
+			.len = sizeof("192.0.2.3") - 1,
+			.result = {
+				.sin_family = AF_INET,
+				.sin_port = 0,
+				.sin_addr = {
+					.s4_addr[0] = 192,
+					.s4_addr[1] = 0,
+					.s4_addr[2] = 2,
+					.s4_addr[3] = 3
+				}
+			},
+			.verdict = "",
+		},
+		{
+			.address = "192.0.2.3:80/foobar",
+			.len = sizeof("192.0.2.3:80") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "a.b.c.d",
+			.verdict = false
+		},
+	};
+#endif
+#if defined(CONFIG_NET_IPV6)
+	static const struct {
+		const char *address;
+		int len;
+		struct sockaddr_in6 result;
+		uint8_t mask_len;
+		const char *verdict;
+	} parse_ipv6_entries[] = {
+		{
+			.address = "2001:db8::2",
+			.len = sizeof("2001:db8::2") - 1,
+			.result = {
+				.sin6_family = AF_INET6,
+				.sin6_addr = {
+					.s6_addr16[0] = ntohs(0x2001),
+					.s6_addr16[1] = ntohs(0xdb8),
+					.s6_addr16[3] = 0,
+					.s6_addr16[4] = 0,
+					.s6_addr16[5] = 0,
+					.s6_addr16[6] = 0,
+					.s6_addr16[7] = ntohs(2)
+				}
+			},
+			.verdict = "",
+		},
+		{
+			.address = "2001:db8::a/barfoo",
+			.len = sizeof("2001:db8::a/barfoo") - 8,
+			.result = {
+				.sin6_family = AF_INET6,
+				.sin6_port = 0,
+				.sin6_addr = {
+					.s6_addr16[0] = ntohs(0x2001),
+					.s6_addr16[1] = ntohs(0xdb8),
+					.s6_addr16[3] = 0,
+					.s6_addr16[4] = 0,
+					.s6_addr16[5] = 0,
+					.s6_addr16[6] = 0,
+					.s6_addr16[7] = ntohs(0xa)
+				}
+			},
+			.verdict = "",
+		},
+		{
+			.address = "2001:db8::a",
+			.len = sizeof("2001:db8::a") - 1,
+			.result = {
+				.sin6_family = AF_INET6,
+				.sin6_port = 0,
+				.sin6_addr = {
+					.s6_addr16[0] = ntohs(0x2001),
+					.s6_addr16[1] = ntohs(0xdb8),
+					.s6_addr16[3] = 0,
+					.s6_addr16[4] = 0,
+					.s6_addr16[5] = 0,
+					.s6_addr16[6] = 0,
+					.s6_addr16[7] = ntohs(0xa)
+				}
+			},
+			.verdict = "",
+		},
+		{
+			.address = "[2001:db8:3:4:5:6:7:8]:65535",
+			.len = sizeof("[2001:db8:3:4:5:6:7:8]:65535") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "[::]:0",
+			.len = sizeof("[::]:0") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "2001:db8::42",
+			.len = sizeof("2001:db8::42") - 1,
+			.result = {
+				.sin6_family = AF_INET6,
+				.sin6_port = 0,
+				.sin6_addr = {
+					.s6_addr16[0] = ntohs(0x2001),
+					.s6_addr16[1] = ntohs(0xdb8),
+					.s6_addr16[3] = 0,
+					.s6_addr16[4] = 0,
+					.s6_addr16[5] = 0,
+					.s6_addr16[6] = 0,
+					.s6_addr16[7] = ntohs(0x42)
+				}
+			},
+			.verdict = "",
+		},
+		{
+			.address = "[2001:db8::192.0.2.1]:80000",
+			.len = sizeof("[2001:db8::192.0.2.1]:80000") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "[2001:db8::1]:80",
+			.len = sizeof("[2001:db8::1") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "[2001:db8::1]:65536",
+			.len = sizeof("[2001:db8::1]:65536") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "[2001:db8::1]:80",
+			.len = sizeof("2001:db8::1") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "[2001:db8::1]:a",
+			.len = sizeof("[2001:db8::1]:a") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "[2001:db8::1]:10-12",
+			.len = sizeof("[2001:db8::1]:10-12") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "[2001:db8::]:80/url/continues",
+			.len = sizeof("[2001:db8::]") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "[2001:db8::200]:080",
+			.len = sizeof("[2001:db8:433:2]:80000") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "[2001:db8::]:8080/another/url",
+			.len = sizeof("[2001:db8::]:8080/another/url") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "[2001:db8::1",
+			.len = sizeof("[2001:db8::1") - 1,
+			.verdict = NULL,
+		},
+		{
+			.address = "[2001:db8::1]:-1",
+			.len = sizeof("[2001:db8::1]:-1") - 1,
+			.verdict = NULL,
+		},
+		{
+			/* Valid although user probably did not mean this */
+			.address = "2001:db8::1:80",
+			.len = sizeof("2001:db8::1:80") - 1,
+			.result = {
+				.sin6_family = AF_INET6,
+				.sin6_addr = {
+					.s6_addr16[0] = ntohs(0x2001),
+					.s6_addr16[1] = ntohs(0xdb8),
+					.s6_addr16[3] = 0,
+					.s6_addr16[4] = 0,
+					.s6_addr16[5] = 0,
+					.s6_addr16[6] = ntohs(0x01),
+					.s6_addr16[7] = ntohs(0x80)
+				}
+			},
+			.verdict = "",
+		},
+		{
+			.address = "2001:db8::1/64,2001:db8::2",
+			.len = sizeof("2001:db8::1/64,2001:db8::2") - 1,
+			.result = {
+				.sin6_family = AF_INET6,
+				.sin6_addr = {
+					.s6_addr16[0] = ntohs(0x2001),
+					.s6_addr16[1] = ntohs(0xdb8),
+					.s6_addr16[2] = 0,
+					.s6_addr16[3] = 0,
+					.s6_addr16[4] = 0,
+					.s6_addr16[5] = 0,
+					.s6_addr16[6] = 0,
+					.s6_addr16[7] = ntohs(0x01)
+				}
+			},
+			.mask_len = 64,
+			.verdict = &"2001:db8::1/64,2001:db8::2"[14],
+		},
+	};
+#endif
+
+#if defined(CONFIG_NET_IPV4)
+	for (i = 0; i < ARRAY_SIZE(parse_ipv4_entries) - 1; i++) {
+		(void)memset(&addr, 0, sizeof(addr));
+		mask_len = 0;
+
+		next = net_ipaddr_parse_mask(
+			parse_ipv4_entries[i].address,
+			parse_ipv4_entries[i].len,
+			&addr, &mask_len);
+		if (next != parse_ipv4_entries[i].verdict) {
+			printk("IPv4 entry [%d] \"%s\" failed\n", i,
+				parse_ipv4_entries[i].address);
+			printk("Points to \"%s\" but should point to \"%s\"\n",
+			       next == NULL ? "NULL" : next,
+			       parse_ipv4_entries[i].verdict == NULL ?
+			       "NULL" : parse_ipv4_entries[i].verdict);
+			zassert_true(false, "failure");
+		}
+
+		if (next != NULL && *next == '\0') {
+			zassert_true(
+				net_ipv4_addr_cmp(
+				      &net_sin(&addr)->sin_addr,
+				      &parse_ipv4_entries[i].result.sin_addr),
+				parse_ipv4_entries[i].address);
+			zassert_true(net_sin(&addr)->sin_port == 0,
+				     "IPv4 port");
+			zassert_true(net_sin(&addr)->sin_family ==
+				     parse_ipv4_entries[i].result.sin_family,
+				     "IPv4 family");
+		}
+	}
+#endif
+#if defined(CONFIG_NET_IPV6)
+	for (i = 0; i < ARRAY_SIZE(parse_ipv6_entries) - 1; i++) {
+		(void)memset(&addr, 0, sizeof(addr));
+		mask_len = 0;
+
+		next = net_ipaddr_parse_mask(
+			parse_ipv6_entries[i].address,
+			parse_ipv6_entries[i].len,
+			&addr, &mask_len);
+		if (next != parse_ipv6_entries[i].verdict) {
+			printk("IPv6 entry [%d] \"%s\" failed\n", i,
+			       parse_ipv6_entries[i].address);
+			zassert_true(false, "failure");
+		}
+
+		if (next != NULL && *next == '\0') {
+			zassert_true(
+				net_ipv6_addr_cmp(
+				      &net_sin6(&addr)->sin6_addr,
+				      &parse_ipv6_entries[i].result.sin6_addr),
+				parse_ipv6_entries[i].address);
+			zassert_true(net_sin6(&addr)->sin6_port == 0,
+				     "IPv6 port");
+			zassert_true(net_sin6(&addr)->sin6_family ==
+				     parse_ipv6_entries[i].result.sin6_family,
+				     "IPv6 family");
+		}
+	}
+#endif
+
+#if defined(CONFIG_NET_IPV4) && defined(CONFIG_NET_IPV6)
+	static const char * const addresses[] = {
+		"2001:db8::1/64,192.0.2.1,2001:db8::2,192.0.2.2/24",
+		"2001:db8::1/64 192.0.2.1 2001:db8::2 192.0.2.2/24",
+		"2001:db8::1/64 192.0.2.1,2001:db8::2 192.0.2.2/24",
+		NULL
+	};
+
+	i = 0;
+
+	while (addresses[i] != NULL) {
+		next = check_ipaddr(addresses[i]);
+		zassert_true(next != NULL && *next == '\0',
+			     "Invalid parse, expecting \"\", got %p\n", next);
+		i++;
+	}
+#endif /* CONFIG_NET_IPV4 && CONFIG_NET_IPV6 */
+}
+
 static uint16_t calc_chksum_ref(uint16_t sum, const uint8_t *data, size_t len)
 {
 	const uint8_t *end;
