ec3f85ad2dd7ffec0ae63cbd7bcf577ec791265b
Jori Rintahaka <jori.rintahaka@silabs.com>
2025-02-14 15:04:59 +0200

    drivers: bluetooth: hci: refactor silabs controller init
    Refactor the controller init to use the new init routines the
    controller now provides. Instead of using a hardcoded set of
    features, the features are now initialized based on Kconfig
    values that the application selects.
    
    Add config files under modules/hal_silabs, used by the controller
    init routines in the external module hal_silabs to determine which
    features to enable based on Kconfig options. This brings the
    controller feature configurability to roughly parity with the
    Simplicity SDK.
    
    Signed-off-by: Jori Rintahaka <jori.rintahaka@silabs.com>
    Signed-off-by: Aksel Skauge Mellbye <aksel.mellbye@silabs.com>

---
+++ modules/hal_silabs/simplicity_sdk/config/ll/sl_bluetooth.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025 Silicon Laboratories Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * This configuration header is used by the Silicon Labs Bluetooth Controller init
+ * functions. It is provided for compatibility only.
+ */
+
+#ifndef SL_BLUETOOTH_H
+#define SL_BLUETOOTH_H
+
+/* This is a dummy file for compatibility */
+
+#endif
