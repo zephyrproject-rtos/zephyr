ff1aba456f428f4be9618fc63fd074cda444887b
Jiawei Yang <jiawei.yang_1@nxp.com>
2025-02-17 14:39:51 +0800

    modules: hal_nxp: Add empty file to support CONFIG_BUILD_ONLY_NO_BLOBS
    Add empty file to support building with CONFIG_BUILD_ONLY_NO_BLOBS
    option.
    
    Signed-off-by: Jiawei Yang <jiawei.yang_1@nxp.com>

---
+++ modules/hal_nxp/bt_controller/src/no_blobs.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2024 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __UART_BT_FW_H__
+#define __UART_BT_FW_H__
+
+
+const unsigned char *bt_fw_bin;
+unsigned int bt_fw_bin_len;
+
+#endif /* __UART_BT_FW_H__ */
