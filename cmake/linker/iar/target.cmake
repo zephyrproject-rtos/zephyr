5d6ffca1881d7661d0a3ab6024509f9cf66da4ac
Robin Kastberg <robin.kastberg@iar.com>
2025-02-18 15:15:34 +0000

    iar: toolchain: Implement IAR static init routine
    This will implement a way of doing static initialization
    the "IAR" way. This is done by calling __iar_data_init3
    which handles all static initialization that is mentioned
    in the linker file "initialize by copy".
    
    Signed-off-by: Robin Kastberg <robin.kastberg@iar.com>

--- cmake/linker/iar/target.cmake
+++ cmake/linker/iar/target.cmake
@@ -10,6 +10,10 @@ find_program(CMAKE_LINKER
   NO_DEFAULT_PATH
 )
 
+if(CONFIG_IAR_DATA_INIT)
+  zephyr_linker_section(NAME .iar.init_table KVMA RAM_REGION GROUP RODATA_REGION)
+endif()
+
 add_custom_target(${IAR_LINKER})
 set(ILINK_THUMB_CALLS_WARNING_SUPPRESSED)
 set(IAR_LIB_USED)
@@ -61,6 +65,8 @@ macro(configure_linker_script linker_script_gen linker_pass_define)
       ${STEERING_FILE_ARG}
       -DCONFIG_LINKER_LAST_SECTION_ID=${CONFIG_LINKER_LAST_SECTION_ID}
       -DCONFIG_LINKER_LAST_SECTION_ID_PATTERN=${CONFIG_LINKER_LAST_SECTION_ID_PATTERN}
+      -DCONFIG_IAR_DATA_INIT=${CONFIG_IAR_DATA_INIT}
+      -DCONFIG_IAR_ZEPHYR_INIT=${CONFIG_IAR_ZEPHYR_INIT}
       -DOUT_FILE=${CMAKE_CURRENT_BINARY_DIR}/${linker_script_gen}
       ${IAR_LIB_USED}
       -P ${ZEPHYR_BASE}/cmake/linker/iar/config_file_script.cmake
