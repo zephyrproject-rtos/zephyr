7475236cf897834b68979ccf123d868af825d8b9
Benjamin Cabé <benjamin@zephyrproject.org>
2025-02-18 07:40:53 +0100

    twister: Add --keep-artifacts option
    Introduce a new command-line option `--keep-artifacts` in twister that
    allows users to specify which artifacts should be preserved
    during test cleanup in addition to the default set.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>
    Co-authored-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/pylib/twister/twisterlib/environment.py
+++ scripts/pylib/twister/twisterlib/environment.py
@@ -578,6 +578,11 @@ structure in the main Zephyr tree: boards/<vendor>/<board_name>/""")
         which only removes artifacts of passing tests. If you wish to
         remove all artificats including those of failed tests, use 'all'.""")
 
+    parser.add_argument(
+        "--keep-artifacts", action="append", default=[],
+        help="""Keep specified artifacts when cleaning up at runtime. Multiple invocations
+        are possible."""
+    )
     test_xor_generator.add_argument(
         "-N", "--ninja", action="store_true",
         default=not any(a in sys.argv for a in ("-k", "--make")),
