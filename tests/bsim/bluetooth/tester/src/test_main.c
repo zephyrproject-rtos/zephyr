c017841fbb59acadfd6ad56c8357e0d06eb40984
Emil Gydesen <emil.gydesen@nordicsemi.no>
2025-02-21 14:41:39 +0100

    tests: Bluetooth: Add BT Tester GAP smoke test
    Add a babblesim test of the BT Tester doing a GAP smoke test
    connecting 2 BT testers using BTP.
    
    The purpose of this is to further increase the test coverage
    of the BT Tester in CI, as it is only being built, and runtime
    errors are typically not caught.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

---
+++ tests/bsim/bluetooth/tester/src/test_main.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include <stddef.h>
+
+#include "bstests.h"
+
+extern struct bst_test_list *test_gap_central_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_gap_peripheral_install(struct bst_test_list *tests);
+
+bst_test_install_t test_installers[] = {
+	test_gap_central_install,
+	test_gap_peripheral_install,
+	NULL,
+};
+
+/* bst_main will be called by the BT tester's `main` function */
