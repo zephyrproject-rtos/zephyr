46773cac10715f21e4632809ac56eaf9f77c0f45
Cla Mattia Galliard <cla-mattia.galliard@zuehlke.com>
2025-02-21 10:59:16 +0100

    net: icmp: Send replies with timeout `K_NO_WAIT`
    This ensure system stays operational in icmp flood situation.
    
    Signed-off-by: Cla Mattia Galliard <cla-mattia.galliard@zuehlke.com>

--- subsys/net/ip/icmpv6.c
+++ subsys/net/ip/icmpv6.c
@@ -169,7 +169,7 @@ static int icmpv6_handle_echo_request(struct net_icmp_ctx *ctx,
 		net_sprint_ipv6_addr(src),
 		net_sprint_ipv6_addr(&ip_hdr->src));
 
-	if (net_send_data(reply) < 0) {
+	if (net_try_send_data(reply, K_NO_WAIT) < 0) {
 		goto drop;
 	}
 
@@ -319,7 +319,7 @@ int net_icmpv6_send_error(struct net_pkt *orig, uint8_t type, uint8_t code,
 		net_sprint_ipv6_addr(src),
 		net_sprint_ipv6_addr(&ip_hdr->src));
 
-	if (net_send_data(pkt) >= 0) {
+	if (net_try_send_data(pkt, K_NO_WAIT) >= 0) {
 		net_stats_update_icmp_sent(net_pkt_iface(pkt));
 		return 0;
 	}
