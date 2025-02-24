ea4a2289685c817cc148c8724882beb2b453e0dd
Lukasz Mrugala <lukaszx.mrugala@intel.com>
2025-02-24 18:00:52 +0000

    twister: Remove unrecognised sections test
    The section lists that govern whether the ELF
    section is recognised or not are out of date.
    
    A programming bug has rendered the unrecognised
    section check unrunnable for years.
    
    Thus, the practically dead code of the
    unrecognised section check is removed
    in this commit.
    
    Signed-off-by: Lukasz Mrugala <lukaszx.mrugala@intel.com>

--- scripts/pylib/twister/twisterlib/runner.py
+++ scripts/pylib/twister/twisterlib/runner.py
@@ -1793,7 +1793,6 @@ class ProjectBuilder(FilterBuilder):
             instance.metrics["used_rom"] = 0
             instance.metrics["available_rom"] = 0
             instance.metrics["available_ram"] = 0
-            instance.metrics["unrecognized"] = []
         return build_result
 
     @staticmethod
@@ -1809,13 +1808,11 @@ class ProjectBuilder(FilterBuilder):
                 instance.metrics["used_rom"] = size_calc.get_used_rom()
                 instance.metrics["available_rom"] = size_calc.get_available_rom()
                 instance.metrics["available_ram"] = size_calc.get_available_ram()
-                instance.metrics["unrecognized"] = size_calc.unrecognized_sections()
             else:
                 instance.metrics["used_ram"] = 0
                 instance.metrics["used_rom"] = 0
                 instance.metrics["available_rom"] = 0
                 instance.metrics["available_ram"] = 0
-                instance.metrics["unrecognized"] = []
             instance.metrics["handler_time"] = instance.execution_time
 
 class TwisterRunner:
@@ -1891,7 +1888,6 @@ class TwisterRunner:
                 else:
                     inst.metrics.update(self.instances[inst.name].metrics)
                     inst.metrics["handler_time"] = inst.execution_time
-                    inst.metrics["unrecognized"] = []
                     self.instances[inst.name] = inst
 
             print("")
