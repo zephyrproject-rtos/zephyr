silverlee35/z_example

modules: Add lora_basics_modem module
This module provides:
* The cmake files for LoRa Basics Modem
* The "HAL" described here:
  https://github.com/Lora-net/SWL2001/blob/master/lbm_lib/PORTING_GUIDE.md
* Reimplementations of the logging functions in smtc_modem_hal_dbg_trace.h

Signed-off-by: Félix Piédallu <felix@piedallu.me>

modules/lora_basics_modem/regions.cmake
@@ -1,0 +1,32 @@
+# Copyright (c) 2024 Semtech Corporation - Félix Piédallu
+# SPDX-License-Identifier: Apache-2.0
+
+set(LBM_SMTC_REGIONS_DIR ${LBM_SMTC_MODEM_CORE_DIR}/lr1mac/src/smtc_real/src/)
+
+# Regional parameters version
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_RP2_101 RP2_101)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_RP2_103 RP2_103)
+
+# enabled regions
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_AS_923 REGION_AS_923)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_AU_915 REGION_AU_915)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_CN_470 REGION_CN_470)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_CN_470_RP_1_0 REGION_CN_470_RP_1_0)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_EU_868 REGION_EU_868)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_IN_865 REGION_IN_865)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_KR_920 REGION_KR_920)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_RU_864 REGION_RU_864)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_US_915 REGION_US_915)
+# the underscore missing here is intentional!
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_WW_2G4 REGION_WW2G4 WW2G4_SINGLE_DATARATE)
+
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_AS_923 ${LBM_SMTC_REGIONS_DIR}/region_as_923.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_AU_915 ${LBM_SMTC_REGIONS_DIR}/region_au_915.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_CN_470 ${LBM_SMTC_REGIONS_DIR}/region_cn_470.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_CN_470_RP_1_0 ${LBM_SMTC_REGIONS_DIR}/region_cn_470_rp_1_0.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_EU_868 ${LBM_SMTC_REGIONS_DIR}/region_eu_868.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_IN_865 ${LBM_SMTC_REGIONS_DIR}/region_in_865.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_KR_920 ${LBM_SMTC_REGIONS_DIR}/region_kr_920.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_RU_864 ${LBM_SMTC_REGIONS_DIR}/region_ru_864.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_US_915 ${LBM_SMTC_REGIONS_DIR}/region_us_915.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_US_915 ${LBM_SMTC_REGIONS_DIR}/region_ww2g4.c)

modules/lora_basics_modem/sx126x.cmake
@@ -1,0 +1,30 @@
+# Copyright (c) 2024 Semtech Corporation - Félix Piédallu
+# SPDX-License-Identifier: Apache-2.0
+
+# Used by LBM
+zephyr_compile_definitions(SX126X)
+zephyr_compile_definitions(SX126X_TRANSCEIVER)
+zephyr_compile_definitions(SX126X_DISABLE_WARNINGS)
+zephyr_compile_definitions_ifdef(CONFIG_DT_HAS_SEMTECH_SX1261_NEW_ENABLED SX1261)
+zephyr_compile_definitions_ifdef(CONFIG_DT_HAS_SEMTECH_SX1262_NEW_ENABLED SX1262)
+zephyr_compile_definitions_ifdef(CONFIG_DT_HAS_SEMTECH_SX1268_NEW_ENABLED SX1268)
+
+# Allow modem options
+set(ALLOW_CSMA_BUILD true)
+
+set(LBM_SX126X_LIB_DIR ${LBM_RADIO_DRIVERS_DIR}/sx126x_driver/src)
+zephyr_include_directories(${LBM_SX126X_LIB_DIR})
+
+#-----------------------------------------------------------------------------
+# Radio specific sources
+#-----------------------------------------------------------------------------
+zephyr_library_sources(
+  ${LBM_SX126X_LIB_DIR}/lr_fhss_mac.c
+  ${LBM_SX126X_LIB_DIR}/sx126x.c
+  ${LBM_SX126X_LIB_DIR}/sx126x_driver_version.c
+  ${LBM_SX126X_LIB_DIR}/sx126x_lr_fhss.c
+)
+
+# Used later
+set(LBM_RAL_SOURCES ${LBM_SMTC_MODEM_CORE_DIR}/smtc_ral/src/ral_sx126x.c)
+set(LBM_RALF_SOURCES ${LBM_SMTC_MODEM_CORE_DIR}/smtc_ralf/src/ralf_sx126x.c)

zephyrproject-rtos/zephyr

modules/lora_basics_modem/regions.cmake
@@ -1,0 +1,32 @@
+# Copyright (c) 2024 Semtech Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+set(LBM_SMTC_REGIONS_DIR ${LBM_SMTC_MODEM_CORE_DIR}/lr1mac/src/smtc_real/src/)
+
+# Regional parameters version
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_RP2_101 RP2_101)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_RP2_103 RP2_103)
+
+# enabled regions
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_AS_923 REGION_AS_923)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_AU_915 REGION_AU_915)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_CN_470 REGION_CN_470)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_CN_470_RP_1_0 REGION_CN_470_RP_1_0)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_EU_868 REGION_EU_868)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_IN_865 REGION_IN_865)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_KR_920 REGION_KR_920)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_RU_864 REGION_RU_864)
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_US_915 REGION_US_915)
+# the underscore missing here is intentional!
+zephyr_library_compile_definitions_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_WW_2G4 REGION_WW2G4 WW2G4_SINGLE_DATARATE)
+
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_AS_923 ${LBM_SMTC_REGIONS_DIR}/region_as_923.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_AU_915 ${LBM_SMTC_REGIONS_DIR}/region_au_915.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_CN_470 ${LBM_SMTC_REGIONS_DIR}/region_cn_470.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_CN_470_RP_1_0 ${LBM_SMTC_REGIONS_DIR}/region_cn_470_rp_1_0.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_EU_868 ${LBM_SMTC_REGIONS_DIR}/region_eu_868.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_IN_865 ${LBM_SMTC_REGIONS_DIR}/region_in_865.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_KR_920 ${LBM_SMTC_REGIONS_DIR}/region_kr_920.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_RU_864 ${LBM_SMTC_REGIONS_DIR}/region_ru_864.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_US_915 ${LBM_SMTC_REGIONS_DIR}/region_us_915.c)
+zephyr_library_sources_ifdef(CONFIG_LORA_BASICS_MODEM_REGION_WW_2G4 ${LBM_SMTC_REGIONS_DIR}/region_ww2g4.c)

modules/lora_basics_modem/sx126x.cmake
@@ -1,0 +1,30 @@
+# Copyright (c) 2024 Semtech Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+# Used by LBM
+zephyr_library_compile_definitions(SX126X)
+zephyr_library_compile_definitions(SX126X_TRANSCEIVER)
+zephyr_library_compile_definitions(SX126X_DISABLE_WARNINGS)
+zephyr_library_compile_definitions_ifdef(CONFIG_DT_HAS_SEMTECH_SX1261_NEW_ENABLED SX1261)
+zephyr_library_compile_definitions_ifdef(CONFIG_DT_HAS_SEMTECH_SX1262_NEW_ENABLED SX1262)
+zephyr_library_compile_definitions_ifdef(CONFIG_DT_HAS_SEMTECH_SX1268_NEW_ENABLED SX1268)
+
+# Allow modem options
+set(ALLOW_CSMA_BUILD true)
+
+set(LBM_SX126X_LIB_DIR ${LBM_RADIO_DRIVERS_DIR}/sx126x_driver/src)
+zephyr_include_directories(${LBM_SX126X_LIB_DIR})
+
+#-----------------------------------------------------------------------------
+# Radio specific sources
+#-----------------------------------------------------------------------------
+zephyr_library_sources(
+  ${LBM_SX126X_LIB_DIR}/lr_fhss_mac.c
+  ${LBM_SX126X_LIB_DIR}/sx126x.c
+  ${LBM_SX126X_LIB_DIR}/sx126x_driver_version.c
+  ${LBM_SX126X_LIB_DIR}/sx126x_lr_fhss.c
+)
+
+# Used later
+set(LBM_RAL_SOURCES ${LBM_SMTC_MODEM_CORE_DIR}/smtc_ral/src/ral_sx126x.c)
+set(LBM_RALF_SOURCES ${LBM_SMTC_MODEM_CORE_DIR}/smtc_ralf/src/ralf_sx126x.c)