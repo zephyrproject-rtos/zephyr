f19532cdcf4aece77d06588af2aede26be42dc0d
Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
2025-02-27 14:27:56 +0530

    soc: amd: Add support for AMD Versal NET RPU
    Add support for the RPU, real-time processing unit on Versal NET SoC.
    It is based on Cortext-R52 processor.
    
    The patch contains initial wiring and configuration for generic board
    with OCM(1MB) and DDR(2G) memories, cpu, interrupt controller, global
    timer and UART.
    
    versalnet.dtsi contains common peripherals integrated into Versal NET
    SoC, and versalnet_r52.dtsi has peripherals which are private to
    Cortex-R52 processor.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
    Signed-off-by: Mubin Sayyed <mubin.sayyed@amd.com>

---
+++ soc/xlnx/versalnet/soc.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2025 Advanced Micro Devices, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC_XLNX_VERSALNET_SOC_H_
+#define _SOC_XLNX_VERSALNET_SOC_H_
+
+/* Define CMSIS configurations */
+#define __GIC_PRESENT 0
+#define __TIM_PRESENT 0
+
+#endif /* _SOC_XLNX_VERSALNET_SOC_H_ */
