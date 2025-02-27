869e55a9cb58a77cef87ac9b4cd44e87949ad040
Khoa Nguyen <khoa.nguyen.xh@renesas.com>
2025-02-27 16:58:20 +0700

    drivers: flash: Update driver flash to support Flash-HP for RA4L1
    Update source flash driver to support Flash-HP for RA4L1
    
    Signed-off-by: Khoa Nguyen <khoa.nguyen.xh@renesas.com>

--- drivers/flash/soc_flash_renesas_ra_hp.h
+++ drivers/flash/soc_flash_renesas_ra_hp.h
@@ -8,27 +8,66 @@
 #define ZEPHYR_DRIVERS_FLASH_SOC_FLASH_RENESAS_RA_HP_H_
 
 #include <zephyr/drivers/flash.h>
+#include <zephyr/sys/atomic.h>
 #include <instances/r_flash_hp.h>
 #include <api/r_flash_api.h>
 #include <zephyr/drivers/flash/ra_flash_api_extensions.h>
 
-#define CHECK_EQ(val1, val2)                 ((val1) == (val2) ? 1 : 0)
-#define GET_SIZE(COND, value, default_value) ((COND) ? (value) : (default_value))
+#define FLASH_HP_CF_START_ADDRESS DT_REG_ADDR(DT_NODELABEL(flash0))
+#define FLASH_HP_DF_START_ADDRESS DT_REG_ADDR(DT_NODELABEL(flash1))
 
-#define FLASH_HP_CF_BLOCK_8KB_SIZE  BSP_FEATURE_FLASH_HP_CF_REGION0_BLOCK_SIZE
-#define FLASH_HP_CF_BLOCK_32KB_SIZE BSP_FEATURE_FLASH_HP_CF_REGION1_BLOCK_SIZE
-#define FLASH_HP_DF_BLOCK_SIZE      BSP_FEATURE_FLASH_HP_DF_BLOCK_SIZE
-#define FLASH_HP_DF_START           BSP_FEATURE_FLASH_DATA_FLASH_START
+#define FLASH_HP_CF_SIZE DT_REG_SIZE(DT_NODELABEL(flash0))
+#define FLASH_HP_DF_SIZE DT_REG_SIZE(DT_NODELABEL(flash1))
 
-#define FLASH_HP_CF_BLOCK_8KB_LOW_START  (0)
-#define FLASH_HP_CF_BLOCK_8KB_LOW_END    (7)
-#define FLASH_HP_CF_BLOCK_8KB_HIGH_START (70)
-#define FLASH_HP_CF_BLOCK_8KB_HIGH_END   (77)
+#define FLASH_HP_VERSION DT_PROP(DT_PARENT(DT_NODELABEL(flash0)), flash_hardware_version)
 
-#define FLASH_HP_CF_BLOCK_32KB_LINEAR_START (8)
-#define FLASH_HP_CF_BLOCK_32KB_LINEAR_END   (DT_PROP(DT_NODELABEL(flash), block_32kb_linear_end))
+#if (FLASH_HP_VERSION == 40)
 
-#define FLASH_HP_DF_BLOCK_END (DT_REG_SIZE(DT_NODELABEL(flash1)) / FLASH_HP_DF_BLOCK_SIZE)
+#define FLASH_HP_CF_REGION0_BLOCKS_COUNT                                                           \
+	DT_PHA_BY_IDX(DT_NODELABEL(flash0), erase_blocks, 0, pages_count)
+#define FLASH_HP_CF_REGION0_BLOCK_SIZE                                                             \
+	DT_PHA_BY_IDX(DT_NODELABEL(flash0), erase_blocks, 0, pages_size)
+#define FLASH_HP_CF_REGION0_SIZE (FLASH_HP_CF_REGION0_BLOCKS_COUNT * FLASH_HP_CF_REGION0_BLOCK_SIZE)
+
+BUILD_ASSERT(FLASH_HP_CF_REGION0_BLOCK_SIZE == BSP_FEATURE_FLASH_HP_CF_REGION0_BLOCK_SIZE,
+	     "erase-block-size expected to be equal with block size");
+
+#define FLASH_HP_CF_REGION1_BLOCKS_COUNT                                                           \
+	DT_PHA_BY_IDX(DT_NODELABEL(flash0), erase_blocks, 1, pages_count)
+#define FLASH_HP_CF_REGION1_BLOCK_SIZE                                                             \
+	DT_PHA_BY_IDX(DT_NODELABEL(flash0), erase_blocks, 1, pages_size)
+
+BUILD_ASSERT(FLASH_HP_CF_REGION1_BLOCK_SIZE == BSP_FEATURE_FLASH_HP_CF_REGION1_BLOCK_SIZE,
+	     "erase-block-size expected to be equal with block size");
+
+#define FLASH_HP_CF_LAYOUT_SIZE (2UL)
+
+#define FLASH_HP_CF_END_BLOCK (FLASH_HP_CF_REGION0_BLOCKS_COUNT + FLASH_HP_CF_REGION1_BLOCKS_COUNT)
+
+#elif (FLASH_HP_VERSION == 4)
+
+#define FLASH_HP_CF_REGION0_BLOCKS_COUNT                                                           \
+	DT_PHA_BY_IDX(DT_NODELABEL(flash0), erase_blocks, 0, pages_count)
+#define FLASH_HP_CF_REGION0_BLOCK_SIZE                                                             \
+	DT_PHA_BY_IDX(DT_NODELABEL(flash0), erase_blocks, 0, pages_size)
+#define FLASH_HP_CF_REGION0_SIZE (FLASH_HP_CF_REGION0_BLOCKS_COUNT * FLASH_HP_CF_REGION0_BLOCK_SIZE)
+
+BUILD_ASSERT(FLASH_HP_CF_REGION0_BLOCK_SIZE == BSP_FEATURE_FLASH_HP_CF_REGION0_BLOCK_SIZE,
+	     "erase-block-size expected to be equal with block size");
+
+#define FLASH_HP_CF_LAYOUT_SIZE (1UL)
+
+#define FLASH_HP_CF_END_BLOCK FLASH_HP_CF_REGION0_BLOCKS_COUNT
+
+#endif
+
+#define FLASH_HP_DF_LAYOUT_SIZE  (1UL)
+#define FLASH_HP_DF_BLOCK_SIZE   DT_PROP(DT_NODELABEL(flash1), erase_block_size)
+#define FLASH_HP_DF_BLOCKS_COUNT (FLASH_HP_DF_SIZE / FLASH_HP_DF_BLOCK_SIZE)
+#define FLASH_HP_DF_END_BLOCK    FLASH_HP_DF_BLOCKS_COUNT
+
+BUILD_ASSERT(FLASH_HP_DF_BLOCK_SIZE == BSP_FEATURE_FLASH_HP_DF_BLOCK_SIZE,
+	     "erase-block-size expected to be equal with block size");
 
 #if defined(CONFIG_FLASH_EX_OP_ENABLED)
 #define FLASH_HP_FCU_CONFIG_SET_BPS     (0x1300A1C0U)
@@ -47,16 +86,17 @@ enum flash_region {
 	DATA_FLASH,
 };
 
-typedef void (*irq_config_func_t)(const struct device *dev);
+#if defined(CONFIG_FLASH_RENESAS_RA_HP_BGO)
+#define FLASH_FLAG_ERASE_COMPLETE BIT(0)
+#define FLASH_FLAG_WRITE_COMPLETE BIT(1)
+#define FLASH_FLAG_GET_ERROR      BIT(2)
+#endif /* CONFIG_FLASH_RENESAS_RA_HP_BGO */
 
 struct flash_hp_ra_controller {
 	struct st_flash_hp_instance_ctrl flash_ctrl;
 	struct k_sem ctrl_sem;
 	struct st_flash_cfg fsp_config;
-};
-
-struct flash_hp_ra_controller_config {
-	irq_config_func_t irq_config;
+	atomic_t flags;
 };
 
 struct flash_hp_ra_data {
@@ -68,11 +108,6 @@ struct flash_hp_ra_data {
 
 struct flash_hp_ra_config {
 	struct flash_parameters flash_ra_parameters;
-};
-
-struct event_flash {
-	volatile bool erase_complete;
-	volatile bool write_complete;
 };
 
 #if defined(CONFIG_FLASH_RENESAS_RA_HP_WRITE_PROTECT)
