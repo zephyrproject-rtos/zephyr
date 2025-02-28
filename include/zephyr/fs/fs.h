7775c7d4c8c63e519256cd7476b636219421a574
Jakub Michalski <jmichalski@antmicro.com>
2025-02-28 15:47:47 +0100

    fs: implement zephyr fs api for virtiofs
    This commit implements zephyr filesystem operations for virtiofs
    
    Signed-off-by: Jakub Michalski <jmichalski@antmicro.com>
    Signed-off-by: Filip Kokosinski <fkokosinski@antmicro.com>

--- include/zephyr/fs/fs.h
+++ include/zephyr/fs/fs.h
@@ -61,6 +61,9 @@ enum {
 	/** Identifier for in-tree Ext2 file system. */
 	FS_EXT2,
 
+	/** Identifier for in-tree Virtiofs file system. */
+	FS_VIRTIOFS,
+
 	/** Base identifier for external file systems. */
 	FS_TYPE_EXTERNAL_BASE,
 };
