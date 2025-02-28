cd09d42e36746d716ee1860d5c8d3ba97b54ccb6
Sudan Landge <sudan.landge@arm.com>
2025-02-28 16:39:43 +0000

    arch: arm: Add PXN support for  Armv8.1-M
    What is the change?
    This commit adds support for Armv8.1-M MPU architecture's PXN attribute.
    This includes support for configuring MPU regions with PXN via
    custom mpu_config, devicetree and static mpu_config.
    The existing MPU region attribute macros are updated to retain existing
    behaviour with this change with an addition of REGION_RAM_ATTR_PXN to be
    used if one needs to configure a RAM region with PXN MPU attribute.
    
    Why do we need this change?
    Armv8.1-M architecture introduced a new MPU region attribute called
    Privilege eXecute Never (PXN).
    If an MPU region is configured with the PXN attribute set and if the,
    processor attempts to execute code in this region while at
    privileged level, a Memory Management Fault exception is triggered.
    
    This ensures that specific tasks are executed only in unprivileged mode
    and helps in preventing secure privilege escalation attacks.
    
    Signed-off-by: Sudan Landge <sudan.landge@arm.com>

--- include/zephyr/dt-bindings/memory-attr/memory-attr-arm.h
+++ include/zephyr/dt-bindings/memory-attr/memory-attr-arm.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2023 Carlo Caione <ccaione@baylibre.com>
+ * Copyright 2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -18,23 +19,25 @@
  * This is legacy and it should NOT be extended further. If new MPU region
  * types must be added, these must rely on the generic memory attributes.
  */
-#define DT_MEM_ARM_MASK			DT_MEM_ARCH_ATTR_MASK
-#define DT_MEM_ARM_GET(x)		((x) & DT_MEM_ARM_MASK)
-#define DT_MEM_ARM(x)			((x) << DT_MEM_ARCH_ATTR_SHIFT)
+#define DT_MEM_ARM_MASK   DT_MEM_ARCH_ATTR_MASK
+#define DT_MEM_ARM_GET(x) ((x) & DT_MEM_ARM_MASK)
+#define DT_MEM_ARM(x)     ((x) << DT_MEM_ARCH_ATTR_SHIFT)
 
-#define  ATTR_MPU_RAM			BIT(0)
-#define  ATTR_MPU_RAM_NOCACHE		BIT(1)
-#define  ATTR_MPU_FLASH			BIT(2)
-#define  ATTR_MPU_PPB			BIT(3)
-#define  ATTR_MPU_IO			BIT(4)
-#define  ATTR_MPU_EXTMEM		BIT(5)
+#define ATTR_MPU_RAM         BIT(0)
+#define ATTR_MPU_RAM_NOCACHE BIT(1)
+#define ATTR_MPU_FLASH       BIT(2)
+#define ATTR_MPU_PPB         BIT(3)
+#define ATTR_MPU_IO          BIT(4)
+#define ATTR_MPU_EXTMEM      BIT(5)
+#define ATTR_MPU_RAM_PXN     BIT(6)
 
-#define DT_MEM_ARM_MPU_RAM		DT_MEM_ARM(ATTR_MPU_RAM)
-#define DT_MEM_ARM_MPU_RAM_NOCACHE	DT_MEM_ARM(ATTR_MPU_RAM_NOCACHE)
-#define DT_MEM_ARM_MPU_FLASH		DT_MEM_ARM(ATTR_MPU_FLASH)
-#define DT_MEM_ARM_MPU_PPB		DT_MEM_ARM(ATTR_MPU_PPB)
-#define DT_MEM_ARM_MPU_IO		DT_MEM_ARM(ATTR_MPU_IO)
-#define DT_MEM_ARM_MPU_EXTMEM		DT_MEM_ARM(ATTR_MPU_EXTMEM)
-#define DT_MEM_ARM_MPU_UNKNOWN		DT_MEM_ARCH_ATTR_UNKNOWN
+#define DT_MEM_ARM_MPU_RAM         DT_MEM_ARM(ATTR_MPU_RAM)
+#define DT_MEM_ARM_MPU_RAM_NOCACHE DT_MEM_ARM(ATTR_MPU_RAM_NOCACHE)
+#define DT_MEM_ARM_MPU_FLASH       DT_MEM_ARM(ATTR_MPU_FLASH)
+#define DT_MEM_ARM_MPU_PPB         DT_MEM_ARM(ATTR_MPU_PPB)
+#define DT_MEM_ARM_MPU_IO          DT_MEM_ARM(ATTR_MPU_IO)
+#define DT_MEM_ARM_MPU_EXTMEM      DT_MEM_ARM(ATTR_MPU_EXTMEM)
+#define DT_MEM_ARM_MPU_RAM_PXN     DT_MEM_ARM(ATTR_MPU_RAM_PXN)
+#define DT_MEM_ARM_MPU_UNKNOWN     DT_MEM_ARCH_ATTR_UNKNOWN
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_MEM_ATTR_ARM_H_ */
