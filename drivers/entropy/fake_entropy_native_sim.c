a0f51b672e23b250dba0398dddaff10d80501545
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2025-02-28 14:38:36 +0100

    drivers entropy: fake_entropy_native_posix rename to _native_sim
    Rename the driver files, binding and kconfig options, while deprecating
    the old binding and kconfig options.
    
    Uses in tree are replaced.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- drivers/entropy/fake_entropy_native_sim.c
+++ drivers/entropy/fake_entropy_native_sim.c
@@ -3,15 +3,21 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  *
- * Pseudo-random entropy generator for the ARCH_POSIX architecture:
- * Following the principle of reproducibility of the native_posix board
+ * Pseudo-random entropy generator for native simulator based target boards:
+ * Following the principle of reproducibility of the native_sim board
  * this entropy device will always generate the same random sequence when
  * initialized with the same seed
  *
  * This entropy source should only be used for testing.
  */
 
+#include <zephyr/devicetree.h>
+#if DT_HAS_COMPAT_STATUS_OKAY(zephyr_native_posix_rng)
 #define DT_DRV_COMPAT zephyr_native_posix_rng
+#warning "zephyr,native-posix-rng is deprecated in favor of zephyr,native-sim-rng"
+#else
+#define DT_DRV_COMPAT zephyr_native_sim_rng
+#endif
 
 #include <zephyr/device.h>
 #include <zephyr/drivers/entropy.h>
@@ -21,7 +27,7 @@
 #include <string.h>
 #include <zephyr/arch/posix/posix_trace.h>
 #include "soc.h"
-#include "cmdline.h" /* native_posix command line options header */
+#include "cmdline.h" /* native_sim command line options header */
 #include "nsi_host_trampolines.h"
 #include "fake_entropy_native_bottom.h"
 
@@ -29,7 +35,7 @@ static unsigned int seed = 0x5678;
 static bool seed_random;
 static bool seed_set;
 
-static int entropy_native_posix_get_entropy(const struct device *dev,
+static int entropy_native_sim_get_entropy(const struct device *dev,
 					    uint8_t *buffer,
 					    uint16_t length)
 {
@@ -55,26 +61,26 @@ static int entropy_native_posix_get_entropy(const struct device *dev,
 	return 0;
 }
 
-static int entropy_native_posix_get_entropy_isr(const struct device *dev,
+static int entropy_native_sim_get_entropy_isr(const struct device *dev,
 						uint8_t *buf,
 						uint16_t len, uint32_t flags)
 {
 	ARG_UNUSED(flags);
 
 	/*
-	 * entropy_native_posix_get_entropy() is also safe for ISRs
+	 * entropy_native_sim_get_entropy() is also safe for ISRs
 	 * and always produces data.
 	 */
-	entropy_native_posix_get_entropy(dev, buf, len);
+	entropy_native_sim_get_entropy(dev, buf, len);
 
 	return len;
 }
 
-static int entropy_native_posix_init(const struct device *dev)
+static int entropy_native_sim_init(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 	if (seed_set || seed_random ||
-	    IS_ENABLED(CONFIG_FAKE_ENTROPY_NATIVE_POSIX_SEED_BY_DEFAULT)) {
+	    IS_ENABLED(CONFIG_FAKE_ENTROPY_NATIVE_SIM_SEED_BY_DEFAULT)) {
 		entropy_native_seed(seed, seed_random);
 	}
 	posix_print_warning("WARNING: "
@@ -82,16 +88,16 @@ static int entropy_native_posix_init(const struct device *dev)
 	return 0;
 }
 
-static DEVICE_API(entropy, entropy_native_posix_api_funcs) = {
-	.get_entropy     = entropy_native_posix_get_entropy,
-	.get_entropy_isr = entropy_native_posix_get_entropy_isr
+static DEVICE_API(entropy, entropy_native_sim_api_funcs) = {
+	.get_entropy     = entropy_native_sim_get_entropy,
+	.get_entropy_isr = entropy_native_sim_get_entropy_isr
 };
 
 DEVICE_DT_INST_DEFINE(0,
-		    entropy_native_posix_init, NULL,
+		    entropy_native_sim_init, NULL,
 		    NULL, NULL,
 		    PRE_KERNEL_1, CONFIG_ENTROPY_INIT_PRIORITY,
-		    &entropy_native_posix_api_funcs);
+		    &entropy_native_sim_api_funcs);
 
 static void seed_was_set(char *argv, int offset)
 {
