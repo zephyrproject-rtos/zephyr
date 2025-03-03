ce54b90e326f4235e8323c4f8468f139e64dcafa
Lyle Zhu <lyle.zhu@nxp.com>
2025-03-03 16:51:26 +0800

    Bluetooth: tester: Set setting `BREDR` and `SSP` if classic is enabled
    Set the supported settings `BTP_GAP_SETTINGS_BREDR` and
    `BTP_GAP_SETTINGS_SSP` if the Bluetooth classic is enabled.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- tests/bluetooth/tester/src/btp_gap.c
+++ tests/bluetooth/tester/src/btp_gap.c
@@ -286,6 +286,10 @@ static uint8_t controller_info(const void *cmd, uint16_t cmd_len,
 	supported_settings |= BIT(BTP_GAP_SETTINGS_LE);
 	supported_settings |= BIT(BTP_GAP_SETTINGS_ADVERTISING);
 	supported_settings |= BIT(BTP_GAP_SETTINGS_EXTENDED_ADVERTISING);
+	if (IS_ENABLED(CONFIG_BT_CLASSIC)) {
+		supported_settings |= BIT(BTP_GAP_SETTINGS_BREDR);
+		supported_settings |= BIT(BTP_GAP_SETTINGS_SSP);
+	}
 
 	rp->supported_settings = sys_cpu_to_le32(supported_settings);
 	rp->current_settings = sys_cpu_to_le32(current_settings);
