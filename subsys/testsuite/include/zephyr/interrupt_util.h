ac4c70322dbffac3d3e201506d0435f5ab1e8363
pooky1004 <hounjoung@tsnlab.com>
2025-03-05 15:17:34 +0900

    drivers: interrupt_controller: subsys: add trigger_irq for topst_vcp45
    add trigger_irq for topst vcp45 to
    subsys/test/suite/include/zephyr/interrupt util.h file.
    
    Signed-off-by: Hounjoung Rim <hounjoung@tsnlab.com>

--- subsys/testsuite/include/zephyr/interrupt_util.h
+++ subsys/testsuite/include/zephyr/interrupt_util.h
@@ -213,6 +213,17 @@ static inline void trigger_irq(int irq)
 	z_vim_arm_enter_irq(irq);
 }
 
+#elif defined(CONFIG_CPU_CORTEX_R5) && defined(CONFIG_TIC)
+
+#include <zephyr/dt-bindings/interrupt-controller/tcc-tic.h>
+
+extern void z_tic_arm_enter_irq(int);
+
+static inline void trigger_irq(int irq)
+{
+	z_tic_arm_enter_irq(irq);
+}
+
 #else
 /* So far, Nios II does not support this */
 #define NO_TRIGGER_FROM_SW
