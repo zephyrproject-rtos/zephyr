zephyrproject-rtos/zephyr

commit 88830a3b25959a273213275e09eaa487fa79a5fd

uart: ns16550: Enable clock if defined
Currently, ns16550 allows the user to specify a clock attribute in the
Device Tree. This node is use to retrieve the internal frequency of the
hardware block.

However, ns16550 expects the clock is already initialised and it doesn't
try to enable it. Let's fix that issue.

Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

drivers/serial/uart_ns16550.c
@@ -573,9 +573,7 @@ static int uart_ns16550_configure(const struct device *dev,
 	const struct uart_ns16550_dev_config * const dev_cfg = dev->config;
 	uint8_t mdc = 0U, c;
 	uint32_t pclk = 0U;
-
-	/* temp for return value if error occurs in this locked region */
-	int ret = 0;
+	int ret;
 
 	k_spinlock_key_t key = k_spin_lock(&dev_data->lock);
 
@@ -619,6 +617,11 @@ static int uart_ns16550_configure(const struct device *dev,
 	} else {
 		if (!device_is_ready(dev_cfg->clock_dev)) {
 			ret = -EINVAL;
+			goto out;
+		}
+
+		ret = clock_control_on(dev_cfg->clock_dev, dev_cfg->clock_subsys);
+		if (ret != 0 && ret != -EALREADY) {
 			goto out;
 		}
 
@@ -722,6 +725,7 @@ static int uart_ns16550_configure(const struct device *dev,
 
 	/* disable interrupts  */
 	ns16550_outbyte(dev_cfg, IER(dev), 0x00);
+	ret = 0;
 
 out:
 	k_spin_unlock(&dev_data->lock, key);

commit bdaae84467c406a8e484b0ec63e2042fc7e94f35

drivers: serial: ns16550: allow clock_control_on returning ENOSYS
Fixes a regression caused by: 88830a3b
The ns16550 driver should handle possibility of
clock_control_on being a stub (i.e. returning ENOSYS)

Signed-off-by: Dominik Lau <dlau@antmicro.com>

drivers/serial/uart_ns16550.c
@@ -621,7 +621,7 @@ static int uart_ns16550_configure(const struct device *dev,
 		}
 
 		ret = clock_control_on(dev_cfg->clock_dev, dev_cfg->clock_subsys);
-		if (ret != 0 && ret != -EALREADY) {
+		if (ret != 0 && ret != -EALREADY && ret != -ENOSYS) {
 			goto out;
 		}
 