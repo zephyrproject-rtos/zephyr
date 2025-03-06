46293f6767564e8ed67de169b8f2b9dd32f31d31
Carles Cufi <carles.cufi@nordicsemi.no>
2025-03-06 15:38:09 +0100

    boards: nrf: Default to nRF Util for flashing
    Nordic has a new tool, nRF Util (`nrfutil`) which is replacing the
    previous set of command-line tools (particularly `nrfjprog`) that Zephyr
    had been using by default in many of the boards to flash the firmware
    onto the device.
    
    Since nRF Util is already mature and is going to be replacing the existing
    tools, including `nrfjprog`, it is now a good time to change the default
    flash tool to reflect this fact.
    
    Note that `nrfjprog` support is not being removed at all, but this may
    be a breaking change for users that do not have nRF Util installed. For
    this reason we also add a note in the migration guide.
    
    More info:
    https://devzone.nordicsemi.com/nordic/nordic-blog/b/blog/posts/nrf-util-unified-command-line-utility
    
    Installation:
    https://www.nordicsemi.com/Products/Development-tools/nrf-util
    
    Documentation:
    https://docs.nordicsemi.com/bundle/nrfutil/page/README.html
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- boards/u-blox/ubx_evkninab1/board.cmake
+++ boards/u-blox/ubx_evkninab1/board.cmake
@@ -6,6 +6,7 @@
 board_runner_args(nrfjprog "--nrf-family=NRF52")
 board_runner_args(jlink "--device=nRF52832_xxAA" "--speed=4000")
 board_runner_args(pyocd "--target=nrf52832" "--frequency=4000000")
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
