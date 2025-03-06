b2fe2e64dccbd9b4e4383f9f0a883833cf1fd2dd
Carles Cufi <carles.cufi@nordicsemi.no>
2025-03-06 15:38:09 +0100

    boards: nrf: Default to nRF Util for flashing
    Nordic has a new tool, nRF Util (`nrfutil`) which is replacing the
    previous set of command-line tools (particularly `nrfjprog`) that Zephyr
    had been using by default in many of the boards to flash the firmware
    onto the device.
    
    Since nRF Util is already mature and is going to be replacing the existing
    tools, including `nrfjprog`, it is now a good time to change the default
    flash tool to reflect this fact.
    
    Note that `nrfjprog` support is not being removed at all, but this may
    be a breaking change for users that do not have nRF Util installed. For
    this reason we also add a note in the migration guide.
    
    More info:
    https://devzone.nordicsemi.com/nordic/nordic-blog/b/blog/posts/nrf-util-unified-command-line-utility
    
    Installation:
    https://www.nordicsemi.com/Products/Development-tools/nrf-util
    
    Documentation:
    https://docs.nordicsemi.com/bundle/nrfutil/page/README.html
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- boards/panasonic/pan1783/board.cmake
+++ boards/panasonic/pan1783/board.cmake
@@ -8,5 +8,6 @@ if(CONFIG_SOC_NRF5340_CPUNET_QKAA)
   board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
 endif()
 
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
