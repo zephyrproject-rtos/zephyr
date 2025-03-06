d3a6cdc5d4a6a5bf75447ae5aa5a6733d577d5f7
Hoang Nguyen <hoang.nguyen.jx@bp.renesas.com>
2025-03-06 14:38:35 +0700

    soc: renesas: Add support for Renesas RZ/A2M
    Add support for Renesas RZ/A2M
    
    Signed-off-by: Hoang Nguyen <hoang.nguyen.jx@bp.renesas.com>
    Signed-off-by: Binh Nguyen <binh.nguyen.xw@renesas.com>

---
+++ soc/renesas/rz/rza2m/soc.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2025 Renesas Electronics Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_RENESAS_RZA2M_SOC_H_
+#define ZEPHYR_SOC_RENESAS_RZA2M_SOC_H_
+
+/*
+ * The following definitions are required for the inclusion of the CMSIS
+ * Common Peripheral Access Layer for aarch32 Cortex-A CPUs:
+ */
+
+#define __CORTEX_A 9U
+
+#endif /* ZEPHYR_SOC_RENESAS_RZA2M_SOC_H_ */
