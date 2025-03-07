c412ee459782d4f1aa089d40eb12c1e8e9e04a67
Qiang Zhao <qiang.zhao@nxp.com>
2025-03-07 09:19:52 +0530

    drivers: firmware: scmi: add cpu domain protocol
    Added helpers for NXP SCMI cpu dmomain protocol.
    
    Signed-off-by: Qiang Zhao <qiang.zhao@nxp.com>

---
+++ soc/nxp/imx/imx9/imx95/scmi_cpu_soc.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_NXP_IMX95_SCMI_CPU_SOC_H_
+#define ZEPHYR_NXP_IMX95_SCMI_CPU_SOC_H_
+
+#define CPU_IDX_M33P        0U
+#define CPU_IDX_M7P         1U
+#define CPU_IDX_A55C0       2U
+#define CPU_IDX_A55C1       3U
+#define CPU_IDX_A55C2       4U
+#define CPU_IDX_A55C3       5U
+#define CPU_IDX_A55C4       6U
+#define CPU_IDX_A55C5       7U
+#define CPU_IDX_A55P        8U
+#define CPU_NUM_IDX         9U
+
+#define CPU_SLEEP_MODE_RUN      0U
+#define CPU_SLEEP_MODE_WAIT     1U
+#define CPU_SLEEP_MODE_STOP     2U
+#define CPU_SLEEP_MODE_SUSPEND  3U
+
+#endif /* ZEPHYR_NXP_IMX95_SCMI_CPU_SOC_H_ */
