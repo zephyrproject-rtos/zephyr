5c05fd5d2ad5d6f7cdb8b0d3fcdcfa14a7ab0e80
Josuah Demangeon <me@josuah.net>
2025-03-09 18:21:48 +0000

    drivers: video: api: do not wait the timeout
    Fail immediately during tests rather than letting K_FOREVER trigger a CI
    timeout.
    
    Signed-off-by: Josuah Demangeon <me@josuah.net>

--- tests/drivers/video/api/src/video_emul.c
+++ tests/drivers/video/api/src/video_emul.c
@@ -157,7 +157,7 @@ ZTEST(video_common, test_video_vbuf)
 	zexpect_ok(video_set_format(rx_dev, VIDEO_EP_OUT, &fmt));
 
 	/* Allocate a buffer, assuming prj.conf gives enough memory for it */
-	vbuf = video_buffer_alloc(fmt.pitch * fmt.height, K_FOREVER);
+	vbuf = video_buffer_alloc(fmt.pitch * fmt.height, K_NO_WAIT);
 	zexpect_not_null(vbuf);
 
 	/* Start the virtual hardware */
