fb111410308fa56e19d6eb30935c993bb5db4257
Michael D Sherwood <michael.sherwood@microchip.com>
2025-03-11 10:46:55 -0700

    boards: microchip Addition of pic32cxsg board
    Addition of pic32cxsg SG41 Curiosity Ultra board
    
    Signed-off-by: Michael D Sherwood <michael.sherwood@microchip.com>

---
+++ boards/microchip/pic32cxsg41_cult/board_jlink.cmake
@@ -0,0 +1,5 @@
+# Copyright (c) 2024 Microchip
+# SPDX-License-Identifier: Apache-2.0
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+board_finalize_runner_args(jlink "--device=pic32cx1025sg41128")
