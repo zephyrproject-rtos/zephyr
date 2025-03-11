f91537b20905ec4654021ee82ce461e6efa19b40
Aasim Shaik <aasim.shaik@silabs.com>
2025-03-11 11:50:33 +0100

    Scripts: Security enablement during west build
    Added changes to automate the signing of zephyr security
    using customtool during the west build process.
    
    Signed-off-by: Aasim Shaik <aasim.shaik@silabs.com>

--- boards/silabs/radio_boards/siwx917_rb4338a/board.cmake
+++ boards/silabs/radio_boards/siwx917_rb4338a/board.cmake
@@ -1,8 +1,14 @@
 # Copyright (c) 2024 Silicon Laboratories Inc.
 # SPDX-License-Identifier: Apache-2.0
+# Set the flash file based on signing is enabled or not.
 
+if(CONFIG_SIWX91X_SILABS_COMMANDER_SIGN)
+    set(FLASH_FILE ${PROJECT_BINARY_DIR}/${KERNEL_NAME}_signed.bin.rps)
+else()
+    set(FLASH_FILE ${PROJECT_BINARY_DIR}/${KERNEL_BIN_NAME}.rps)
+endif()
 board_runner_args(silabs_commander "--device=SiWG917M111GTBA" "--file-type=bin"
-    "--file=${PROJECT_BINARY_DIR}/${KERNEL_BIN_NAME}.rps")
+    "--file=${FLASH_FILE}")
 include(${ZEPHYR_BASE}/boards/common/silabs_commander.board.cmake)
 
 # It is not possible to load/flash a firmware using JLink, but it is possible to
