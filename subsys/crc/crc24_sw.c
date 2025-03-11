050f8da7476b99f180360a450df9817716718a59
Duy Vo <duy.vo.xc@bp.renesas.com>
2025-03-11 16:01:26 +0700

    crc: initial support for CRC subsystem
    Migrate support from crc library to new crc subsystem
    Add hardware acclerator backend for crc subsystem
    
    Signed-off-by: Duy Vo <duy.vo.xc@bp.renesas.com>

--- subsys/crc/crc24_sw.c
+++ subsys/crc/crc24_sw.c
@@ -9,15 +9,13 @@
 
 #include <zephyr/sys/crc.h>
 
-#define CRC24_PGP_POLY 0x01864cfbU
-
-uint32_t crc24_pgp(const uint8_t *data, size_t len)
+uint32_t __weak crc24_pgp(const uint8_t *data, size_t len)
 {
 	return crc24_pgp_update(CRC24_PGP_INITIAL_VALUE, data, len) & CRC24_FINAL_VALUE_MASK;
 }
 
 /* CRC-24 implementation from the section 6.1 of the RFC 4880 */
-uint32_t crc24_pgp_update(uint32_t crc, const uint8_t *data, size_t len)
+uint32_t __weak crc24_pgp_update(uint32_t crc, const uint8_t *data, size_t len)
 {
 	int i;
 
