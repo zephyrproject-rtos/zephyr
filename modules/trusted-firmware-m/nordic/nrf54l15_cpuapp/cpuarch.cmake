6d4c7770ed70aeaafe73d67827ac192eac2efe5c
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2025-03-11 14:52:16 +0100

    boards: Add non-secure target for nrf54L15dk
    This adds the nrf54l15dk/nrf54l15/cpuapp/ns board variant to
    Zephyr. It allows to build applications for this target.
    
    This is an initial support for the non secure target which allows
    building and running tfm_ipc and config_build.
    
    This is NOT full support of the non secure target in upstream
    Zephyr.
    
    There are important limitations, such as:
    - The hardware Crypto accelerator is not supported and thus the non
      secur target is NOT secure for production applicatiions in upstream
      Zephyr.
    - The BL2 is not supported, so no DFU is supported with this support
    
    Most of the code chagnes here are taken from nRF Connect SDK
    in order to avoid having noups there.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

---
+++ modules/trusted-firmware-m/nordic/nrf54l15_cpuapp/cpuarch.cmake
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2025, Nordic Semiconductor ASA.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set(PLATFORM_PATH platform/ext/target/nordic_nrf)
+
+include(${PLATFORM_PATH}/common/nrf54l15/cpuarch.cmake)
