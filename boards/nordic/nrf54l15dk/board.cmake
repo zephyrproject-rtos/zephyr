141c3cb8b1c6a5bb1ee9dcacf1a8adbc8f3625f0
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2025-03-11 14:52:16 +0100

    boards: Add non-secure target for nrf54L15dk
    This adds the nrf54l15dk/nrf54l15/cpuapp/ns board variant to
    Zephyr. It allows to build applications for this target.
    
    This is an initial support for the non secure target which allows
    building and running tfm_ipc and config_build.
    
    This is NOT full support of the non secure target in upstream
    Zephyr.
    
    There are important limitations, such as:
    - The hardware Crypto accelerator is not supported and thus the non
      secur target is NOT secure for production applicatiions in upstream
      Zephyr.
    - The BL2 is not supported, so no DFU is supported with this support
    
    Most of the code chagnes here are taken from nRF Connect SDK
    in order to avoid having noups there.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

--- boards/nordic/nrf54l15dk/board.cmake
+++ boards/nordic/nrf54l15dk/board.cmake
@@ -10,5 +10,13 @@ elseif(CONFIG_SOC_NRF54L05_CPUFLPR OR CONFIG_SOC_NRF54L10_CPUFLPR)
   board_runner_args(jlink "--device=RISC-V" "--speed=4000" "-if SW" "--tool-opt=-jlinkscriptfile ${JLINKSCRIPTFILE}")
 endif()
 
+if(CONFIG_BOARD_NRF54L15DK_NRF54L15_CPUAPP_NS)
+  set(TFM_PUBLIC_KEY_FORMAT "full")
+endif()
+
+if(CONFIG_TFM_FLASH_MERGED_BINARY)
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
+endif()
+
 include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
