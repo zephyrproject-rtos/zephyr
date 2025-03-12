1aa58a88031eb0983f82be5835bcd49cdcff5342
Maxjta <411572327@qq.com>
2025-03-12 10:39:19 +0800

    drvier: modify at32 clock control driver

--- drivers/clock_control/clock_control_at32.c
+++ drivers/clock_control/clock_control_at32.c
@@ -14,7 +14,9 @@
 #include <zephyr/drivers/clock_control.h>
 #include <zephyr/drivers/clock_control/at32_clock_control.h>
 
-//#include <at32_regs.h>
+#include <at32_crm.h>
+#include <at32_flash.h>
+#include <at32_pwc.h>
 
 /** offset (from id cell) */
 #define AT32_CLOCK_ID_OFFSET(id) (((id) >> 6U) & 0xFFU)
@@ -134,40 +136,70 @@ static DEVICE_API(clock_control, clock_control_at32_api) = {
 int at32_clock_control_init(const struct device *dev)
 {
 	int clk_div = 0;
-	/* select pll as system clock source */
+	/* set hick as system clock */
     crm_sysclk_switch(CRM_SCLK_HICK);
-	
+
     /* wait till pll is used as system clock source */
     while(crm_sysclk_switch_status_get() != CRM_SCLK_HICK){
     }
 	
+	crm_periph_clock_enable(CRM_PWC_PERIPH_CLOCK, TRUE);
+#if defined(PWC_LDO_OUTPUT)
+    pwc_ldo_output_voltage_set(PWC_LDO_OUTPUT_MAX);
+#endif
 	/* disable pll */
     crm_clock_source_enable(CRM_CLOCK_SOURCE_PLL, FALSE);
+#if defined(FLASH_WAIT_CYCLE)
+    if(CPU_FREQ > 192000000){
+	    flash_psr_set(FLASH_WAIT_CYCLE_6);	
+	}
+	else if(CPU_FREQ > 160000000){
+        flash_psr_set(FLASH_WAIT_CYCLE_5);	
+	}
+	else if(CPU_FREQ > 128000000){
+        flash_psr_set(FLASH_WAIT_CYCLE_4);	
+	}
+	else if(CPU_FREQ > 96000000){
+        flash_psr_set(FLASH_WAIT_CYCLE_3);	
+	}
+	else if(CPU_FREQ > 64000000){
+        flash_psr_set(FLASH_WAIT_CYCLE_2);	
+	}
+	else{
+        flash_psr_set(FLASH_WAIT_CYCLE_1);	
+	}
+#endif
+    if (IS_ENABLED(AT32_HEXT_ENABLED)){
+        crm_clock_source_enable(CRM_CLOCK_SOURCE_HEXT, TRUE);
 
-    flash_psr_set(FLASH_WAIT_CYCLE_6);
-	
-    crm_clock_source_enable(CRM_CLOCK_SOURCE_HEXT, TRUE);
-
-    /* wait till hext is ready */
-    while(crm_hext_stable_wait() == ERROR)
-    {
+        /* wait till hext is ready */
+        while(crm_hext_stable_wait() == ERROR){
+        }
     }
-	
-	/* config pll clock resource */
-    crm_pll_config(CRM_PLL_SOURCE_HEXT, 
-	    DT_PROP(DT_NODELABEL(pll), mul_ns), 
-	    DT_PROP(DT_NODELABEL(pll), div_ms), 
-	    DT_PROP(DT_NODELABEL(pll), div_fp));
-	
-	crm_pllu_div_set(DT_PROP(DT_NODELABEL(pll), div_fu));
-	crm_pllu_output_set(TRUE);
+    if(IS_ENABLED(AT32_SYSCLK_SRC_PLL)){
+		/* config pll clock resource */
+		if(IS_ENABLED(AT32_PLL_SRC_HEXT)){
+			crm_pll_config(CRM_PLL_SOURCE_HEXT, 
+				DT_PROP(DT_NODELABEL(pll), mul_ns), 
+				DT_PROP(DT_NODELABEL(pll), div_ms), 
+				DT_PROP(DT_NODELABEL(pll), div_fp));
+		}
+		else if(IS_ENABLED(AT32_PLL_SRC_HICK)){
+			crm_pll_config(CRM_PLL_SOURCE_HICK, 
+				DT_PROP(DT_NODELABEL(pll), mul_ns), 
+				DT_PROP(DT_NODELABEL(pll), div_ms), 
+				DT_PROP(DT_NODELABEL(pll), div_fp));
+		}
+#if defined(CRM_PLL_FU)	
+		crm_pllu_div_set(DT_PROP(DT_NODELABEL(pll), div_fu));
+		crm_pllu_output_set(TRUE);
+#endif
+		/* enable pll */
+		crm_clock_source_enable(CRM_CLOCK_SOURCE_PLL, TRUE);
 
-	/* enable pll */
-    crm_clock_source_enable(CRM_CLOCK_SOURCE_PLL, TRUE);
-
-    /* wait till pll is ready */
-    while(crm_flag_get(CRM_PLL_STABLE_FLAG) != SET)
-    {
+		/* wait till pll is ready */
+		while(crm_flag_get(CRM_PLL_STABLE_FLAG) != SET){
+		}
     }
 	
     /* config ahbclk */
@@ -198,12 +230,29 @@ int at32_clock_control_init(const struct device *dev)
     /* enable auto step mode */
     crm_auto_step_mode_enable(TRUE);
 
-    /* select pll as system clock source */
-    crm_sysclk_switch(CRM_SCLK_PLL);
+    if(IS_ENABLED(AT32_SYSCLK_SRC_PLL)){
+		/* select pll as system clock source */
+		crm_sysclk_switch(CRM_SCLK_PLL);
 
-    /* wait till pll is used as system clock source */
-    while(crm_sysclk_switch_status_get() != CRM_SCLK_PLL)
-    {
+		/* wait till pll is used as system clock source */
+		while(crm_sysclk_switch_status_get() != CRM_SCLK_PLL){
+		}
+    }
+	else if(IS_ENABLED(AT32_SYSCLK_SRC_HEXT)){
+		/* select hext as system clock source */
+		crm_sysclk_switch(CRM_SCLK_HEXT);
+
+		/* wait till hext is used as system clock source */
+		while(crm_sysclk_switch_status_get() != CRM_SCLK_HEXT){
+		}
+    }
+	else {
+		/* select hick as system clock source */
+		crm_sysclk_switch(CRM_SCLK_HICK);
+
+		/* wait till hick is used as system clock source */
+		while(crm_sysclk_switch_status_get() != CRM_SCLK_HICK){
+		}
     }
 
     /* disable auto step mode */
--- include/zephyr/drivers/clock_control/at32_clock_control.h
+++ include/zephyr/drivers/clock_control/at32_clock_control.h
@@ -32,4 +32,39 @@
  */
 #define AT32_CLOCK_CONTROLLER DEVICE_DT_GET(DT_NODELABEL(cctl))
 
+/* To enable use of IS_ENABLED utility macro, these symbols
+ * should not be defined directly using DT_SAME_NODE.
+ */
+#define DT_CRM_CLOCKS_CTRL	DT_CLOCKS_CTLR(DT_NODELABEL(crm))
+#if DT_SAME_NODE(DT_CRM_CLOCKS_CTRL, DT_NODELABEL(pll))
+#define AT32_SYSCLK_SRC_PLL	1
+#endif
+#if DT_SAME_NODE(DT_CRM_CLOCKS_CTRL, DT_NODELABEL(clk_hick))
+#define AT32_SYSCLK_SRC_HICK	1
+#endif
+#if DT_SAME_NODE(DT_CRM_CLOCKS_CTRL, DT_NODELABEL(clk_hext))
+#define AT32_SYSCLK_SRC_HEXT	1
+#endif
+
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(pll), okay) && \
+	DT_NODE_HAS_PROP(DT_NODELABEL(pll), clocks)
+#define DT_PLL_CLOCKS_CTRL	DT_CLOCKS_CTLR(DT_NODELABEL(pll))
+#if DT_SAME_NODE(DT_PLL_CLOCKS_CTRL, DT_NODELABEL(clk_hick))
+#define AT32_PLL_SRC_HICK	1
+#endif
+#if DT_SAME_NODE(DT_PLL_CLOCKS_CTRL, DT_NODELABEL(clk_hext))
+#define AT32_PLL_SRC_HEXT	1
+#endif
+#endif
+
+#if DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(clk_hext), fixed_clock, okay)
+#define AT32_HEXT_ENABLED	1
+#define AT32_HEXT_FREQ		DT_PROP(DT_NODELABEL(clk_hext), clock_frequency)
+#elif DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(clk_hext), at_at32_hext, okay)
+#define AT32_HEXT_ENABLED	1
+#define AT32_HEXT_FREQ		DT_PROP(DT_NODELABEL(clk_hext), clock_frequency)
+#else
+#define AT32_HEXT_ENABLED		0
+#endif
+
 #endif /* ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_AT32_H_ */
