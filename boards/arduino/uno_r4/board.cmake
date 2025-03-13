ca36f8a4f15624aaa376f4b9adf676aa1efec7ae
Benjamin Cabé <benjamin@zephyrproject.org>
2025-03-13 18:39:13 +0100

    boards: arduino: use variants for Arduino Uno R4 Minima and WiFi
    Use variants instead of making these two different boards.
    Deprecate old board names
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- boards/arduino/uno_r4/board.cmake
+++ boards/arduino/uno_r4/board.cmake
@@ -1,6 +1,12 @@
 # Copyright (c) 2023 TOKITA Hiroshi <tokita.hiroshi@fujitsu.com>
 # SPDX-License-Identifier: Apache-2.0
 
+if(NOT ("${BOARD_QUALIFIERS}" MATCHES "minima"
+      OR "${BOARD_QUALIFIERS}" MATCHES "wifi"))
+  message(FATAL_ERROR "Please specify a board variant for the arduino_uno_r4:\n"
+                      "arduino_uno_r4/r7fa4m1ab3cfm/minima or arduino_uno_r4/r7fa4m1ab3cfm/wifi\n")
+endif()
+
 board_runner_args(pyocd "--target=r7fa4m1ab")
 
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
