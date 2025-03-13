5e5b2b3e08db0af86099e5a17bb3eb0579a8eac2
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2025-03-13 11:33:37 +0200

    net: Change the net_linkaddr struct to not use pointers
    Previously the net_linkaddr struct had pointers to the link address.
    This is error prone and difficult to handle if cloning the packet as
    those pointers can point to wrong place. Mitigate this issue by
    allocating the space for link address in net_linkaddr struct. This will
    increase the size of the net_pkt by 4 octets for IEEE 802.15.4 where the
    link address length is 8, but there no increase in size if link address
    is 6 bytes like in Ethernet/Wi-Fi.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- subsys/net/lib/capture/cooked.c
+++ subsys/net/lib/capture/cooked.c
@@ -446,10 +446,10 @@ void net_capture_data(struct net_capture_cooked *ctx,
 	 */
 	net_pkt_set_ll_proto_type(pkt, ptype);
 
-	net_pkt_lladdr_src(pkt)->addr = NULL;
+	memset(net_pkt_lladdr_src(pkt)->addr, 0, sizeof(net_pkt_lladdr_src(pkt)->addr));
 	net_pkt_lladdr_src(pkt)->len = 0U;
 	net_pkt_lladdr_src(pkt)->type = NET_LINK_DUMMY;
-	net_pkt_lladdr_dst(pkt)->addr = NULL;
+	memset(net_pkt_lladdr_dst(pkt)->addr, 0, sizeof(net_pkt_lladdr_dst(pkt)->addr));
 	net_pkt_lladdr_dst(pkt)->len = 0U;
 	net_pkt_lladdr_dst(pkt)->type = NET_LINK_DUMMY;
 
