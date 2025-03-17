f94e0404cd7370ae8ccaaba0ad71b53c8d9b5d93
Henrik Brix Andersen <henrik@brixandersen.dk>
2025-03-17 20:32:58 +0000

    boards: others: neorv32: add minimalboot board variant
    Add a MinimalBoot board variant for the NEORV32 board. The software
    configuration of this board matches the NEORV32 "MinimalBoot" processor
    template.
    
    Signed-off-by: Henrik Brix Andersen <henrik@brixandersen.dk>

--- boards/others/neorv32/board.cmake
+++ boards/others/neorv32/board.cmake
@@ -6,5 +6,5 @@ include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 
 if("${BOARD_QUALIFIERS}" STREQUAL "/neorv32")
   message(FATAL_ERROR "Please specify a variant for the neorv32 board "
-                      "(e.g. neorv32/neorv32/up5kdemo)")
+                      "(e.g. neorv32/neorv32/minimalboot or neorv32/neorv32/up5kdemo)")
 endif()
