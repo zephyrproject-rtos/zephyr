273d60164d40794aad1d09cd63a35a7f233e37de
Sayooj K Karun <sayooj@aerlync.com>
2025-03-17 16:58:55 +0530

    net: sockets: IPv6_MULTICAST_LOOP Support
    Add support for the IPV6_MULTICAST_LOOP socket option.
    This option allows enabling or disabling the consumption of multicast
    packets by the sender. By default, the socket option will be enabled.
    
    Co-authored-by: Pieter De Gendt <pieter.degendt@gmail.com>
    
    Signed-off-by: Sayooj K Karun <sayooj@aerlync.com>

--- subsys/net/lib/sockets/sockets_inet.c
+++ subsys/net/lib/sockets/sockets_inet.c
@@ -2,6 +2,7 @@
  * Copyright (c) 2017 Linaro Limited
  * Copyright (c) 2021 Nordic Semiconductor
  * Copyright (c) 2023 Arm Limited (or its affiliates). All rights reserved.
+ * Copyright (c) 2025 Aerlync Labs Inc.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -2080,6 +2081,18 @@ int zsock_getsockopt_ctx(struct net_context *ctx, int level, int optname,
 			}
 
 			return 0;
+
+		case IPV6_MULTICAST_LOOP:
+			ret = net_context_get_option(ctx,
+						     NET_OPT_IPV6_MCAST_LOOP,
+						     optval, optlen);
+			if (ret < 0) {
+				errno = -ret;
+				return -1;
+			}
+
+			return 0;
+
 		}
 
 		break;
@@ -2722,6 +2735,17 @@ int zsock_setsockopt_ctx(struct net_context *ctx, int level, int optname,
 			}
 
 			break;
+
+		case IPV6_MULTICAST_LOOP:
+			ret = net_context_set_option(ctx,
+						     NET_OPT_IPV6_MCAST_LOOP,
+						     optval, optlen);
+			if (ret < 0) {
+				errno = -ret;
+				return -1;
+			}
+
+			return 0;
 		}
 
 		break;
