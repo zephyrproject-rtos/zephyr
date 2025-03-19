7b27c27a8953e9666a035d4e0b3d441ec78adadf
David Leach <david.leach@nxp.com>
2025-03-19 16:40:15 -0500

    Revert "[nxp fromlist] drivers: entropy: mcux_trng: Add PM device action"
    This reverts commit 576889eaad2e478f942b22e0f2f35389090d0770.
    
    Signed-off-by: David Leach <david.leach@nxp.com>

--- drivers/entropy/entropy_mcux_trng.c
+++ drivers/entropy/entropy_mcux_trng.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2017 Linaro Limited.
- * Copyright 2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -10,7 +9,6 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/entropy.h>
 #include <zephyr/random/random.h>
-#include <zephyr/pm/device.h>
 #include <zephyr/init.h>
 
 #include "fsl_trng.h"
@@ -55,29 +53,8 @@ static int entropy_mcux_trng_init(const struct device *dev)
 	return 0;
 }
 
-#ifdef CONFIG_PM_DEVICE
-static int entropy_mcux_trng_pm_action(const struct device *dev, enum pm_device_action action)
-{
-	switch (action) {
-	case PM_DEVICE_ACTION_RESUME:
-		break;
-	case PM_DEVICE_ACTION_SUSPEND:
-		break;
-	case PM_DEVICE_ACTION_TURN_OFF:
-		break;
-	case PM_DEVICE_ACTION_TURN_ON:
-		entropy_mcux_trng_init(dev);
-		break;
-	default:
-		return -ENOTSUP;
-	}
-	return 0;
-}
-#endif /*CONFIG_PM_DEVICE*/
-
-PM_DEVICE_DT_INST_DEFINE(0, entropy_mcux_trng_pm_action);
 DEVICE_DT_INST_DEFINE(0,
-		    entropy_mcux_trng_init, PM_DEVICE_DT_INST_GET(0), NULL,
+		    entropy_mcux_trng_init, NULL, NULL,
 		    &entropy_mcux_config,
 		    PRE_KERNEL_1, CONFIG_ENTROPY_INIT_PRIORITY,
 		    &entropy_mcux_trng_api_funcs);
