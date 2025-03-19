c98ace0714ec16531ec9b45d26a772c9432a103f
Dominik Ermel <dominik.ermel@nordicsemi.no>
2025-03-19 15:01:08 +0000

    modules: mbedtls: Expose MBEDTLS_RSA_C
    Allow enabling MBEDTLS_RSA_C without key exchange enabled.
    This allows to use RSA without enabling x509 support too.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- modules/mbedtls/configs/config-tls-generic.h
+++ modules/mbedtls/configs/config-tls-generic.h
@@ -368,6 +368,11 @@
 #define MBEDTLS_MD_C
 #endif
 
+#if defined(CONFIG_MBEDTLS_RSA_C)
+#define MBEDTLS_RSA_C
+#define MBEDTLS_PKCS1_V21
+#endif
+
 /* Automatic dependencies */
 
 #if defined(MBEDTLS_KEY_EXCHANGE_DHE_PSK_ENABLED) || \
@@ -379,7 +384,6 @@
     defined(MBEDTLS_KEY_EXCHANGE_RSA_ENABLED) || \
     defined(MBEDTLS_KEY_EXCHANGE_DHE_RSA_ENABLED) || \
     defined(MBEDTLS_KEY_EXCHANGE_ECDHE_RSA_ENABLED)
-#define MBEDTLS_RSA_C
 #define MBEDTLS_PKCS1_V15
 #define MBEDTLS_PKCS1_V21
 #endif
