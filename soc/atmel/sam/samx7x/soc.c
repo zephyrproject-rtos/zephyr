0b6ed41db8c1df97e4f97755285bcf96f26365af
Perry Hung <perry@mosi.io>
2025-03-19 15:21:41 -0700

    soc: atmel: samx7x: implement main oscillator bypass mode
    Enable support for using an external clock as the main clock.
    When bypass mode is enabled, the external crystal oscillator is bypassed,
    and the main clock is directly driven by an external clock signal.
    
    Signed-off-by: Perry Hung <perry@mosi.io>

--- soc/atmel/sam/samx7x/soc.c
+++ soc/atmel/sam/samx7x/soc.c
@@ -57,7 +57,9 @@ static ALWAYS_INLINE void clock_init(void)
 		 * this optimization is not deemed
 		 * critical now.
 		 */
-		soc_pmc_switch_mainck_to_xtal(false, 0xff);
+		bool bypass = IS_ENABLED(CONFIG_SOC_ATMEL_SAM_EXT_MAINCK_BYPASS);
+
+		soc_pmc_switch_mainck_to_xtal(bypass, 0xff);
 	}
 
 	/*
