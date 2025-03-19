9564780564986734c9832696aaf0dc34f7272023
Fabian Blatz <fabianblatz@gmail.com>
2025-03-19 11:08:54 +0100

    drivers: input: sdl_touch: Associate display with instance
    Make the zephyr,input-sdl-touch driver be multi instance and add the
    possibility to associate it with a display. The input events are only
    emitted if the events occured on this display.
    
    Signed-off-by: Fabian Blatz <fabianblatz@gmail.com>

--- drivers/input/input_sdl_touch_bottom.c
+++ drivers/input/input_sdl_touch_bottom.c
@@ -8,9 +8,40 @@
 #include <SDL.h>
 #include "input_sdl_touch_bottom.h"
 
+static bool event_targets_display(SDL_Event *event, struct sdl_input_data *data)
+{
+	SDL_Window *window = NULL;
+	const void *display_dev;
+
+	if (!data->display_dev) {
+		return true;
+	}
+
+	if (event->type == SDL_MOUSEBUTTONDOWN || event->type == SDL_MOUSEBUTTONUP) {
+		window = SDL_GetWindowFromID(event->button.windowID);
+	} else if (event->type == SDL_MOUSEMOTION) {
+		window = SDL_GetWindowFromID(event->motion.windowID);
+	} else {
+		return false;
+	}
+
+	if (!window) {
+		return false;
+	}
+
+	/* Get the zephyr display associated with the window */
+	display_dev = SDL_GetWindowData(window, "zephyr_display");
+
+	return !display_dev || display_dev == data->display_dev;
+}
+
 static int sdl_filter(void *arg, SDL_Event *event)
 {
 	struct sdl_input_data *data = arg;
+
+	if (!event_targets_display(event, data)) {
+		return 1;
+	}
 
 	switch (event->type) {
 	case SDL_MOUSEBUTTONUP:
