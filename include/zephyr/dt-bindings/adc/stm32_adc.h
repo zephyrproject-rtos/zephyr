767409e4ecfba657002fc4970d0866ddfb9ecbba
James Roy <rruuaanng@outlook.com>
2025-03-20 14:22:49 +0800

    style: Inconsistent macro names changed
    Fix incorrect header file pre-macro names in
    'include/zephyr/dt-bindings'.
    
    Signed-off-by: James Roy <rruuaanng@outlook.com>

--- include/zephyr/dt-bindings/adc/stm32_adc.h
+++ include/zephyr/dt-bindings/adc/stm32_adc.h
@@ -3,8 +3,8 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_STM32_ADC_H_
-#define ZEPHYR_INCLUDE_DT_BINDINGS_STM32_ADC_H_
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_ADC_STM32_ADC_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_ADC_STM32_ADC_H_
 
 #include <zephyr/dt-bindings/adc/adc.h>
 
@@ -64,4 +64,4 @@
 	STM32_ADC(resolution, reg_val, STM32_ADC_RES_MASK, STM32_ADC_RES_SHIFT, \
 		  STM32_ADC_RES_REG)
 
-#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_STM32_ADC_H_ */
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_ADC_STM32_ADC_H_ */
