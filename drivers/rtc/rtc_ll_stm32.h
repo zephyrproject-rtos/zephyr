6e2859b6db7d63e33e2872c4de1e5a3557f860c2
Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
2025-03-22 00:57:39 +0100

    drivers: rtc: rtc_ll_stm32: update to new EXTI API
    integrate EXTI driver
    
    Co-authored-by: Mathieu CHOPLAIN <mathieu.choplain@st.com>
    Signed-off-by: Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>

--- drivers/rtc/rtc_ll_stm32.h
+++ drivers/rtc/rtc_ll_stm32.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2024 STMicroelectronics
+ * Copyright (c) 2025 Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -35,31 +36,6 @@
 #define RTC_STM32_ALRM_DATEWEEKDAYSEL_WEEKDAY	LL_RTC_ALMA_DATEWEEKDAYSEL_WEEKDAY
 #define RTC_STM32_ALRM_DATEWEEKDAYSEL_DATE	LL_RTC_ALMA_DATEWEEKDAYSEL_DATE
 
-static inline void ll_func_exti_enable_rtc_alarm_it(uint32_t exti_line)
-{
-#if defined(CONFIG_SOC_SERIES_STM32H7X) && defined(CONFIG_CPU_CORTEX_M4)
-	LL_C2_EXTI_EnableIT_0_31(exti_line);
-	LL_EXTI_EnableRisingTrig_0_31(exti_line);
-#elif defined(CONFIG_SOC_SERIES_STM32U5X) || defined(CONFIG_SOC_SERIES_STM32WBAX)
-	/* in STM32U5 & STM32WBAX series, RTC Alarm event is not routed to EXTI */
-#else
-	LL_EXTI_EnableIT_0_31(exti_line);
-	LL_EXTI_EnableRisingTrig_0_31(exti_line);
-#endif /* CONFIG_SOC_SERIES_STM32H7X and CONFIG_CPU_CORTEX_M4 */
-}
-
-static inline void ll_func_exti_clear_rtc_alarm_flag(uint32_t exti_line)
-{
-#if defined(CONFIG_SOC_SERIES_STM32H7X) && defined(CONFIG_CPU_CORTEX_M4)
-	LL_C2_EXTI_ClearFlag_0_31(exti_line);
-#elif defined(CONFIG_SOC_SERIES_STM32U5X) || defined(CONFIG_SOC_SERIES_STM32WBAX)
-	/* in STM32U5 & STM32WBAX series, RTC Alarm event is not routed to EXTI */
-#elif DT_HAS_COMPAT_STATUS_OKAY(st_stm32g0_exti)
-	LL_EXTI_ClearRisingFlag_0_31(exti_line);
-#else
-	LL_EXTI_ClearFlag_0_31(exti_line);
-#endif /* CONFIG_SOC_SERIES_STM32H7X and CONFIG_CPU_CORTEX_M4 */
-}
 #endif /* CONFIG_RTC_ALARM && !CONFIG_SOC_SERIES_STM32WB0X */
 
 #endif	/* ZEPHYR_DRIVERS_RTC_RTC_LL_STM32_H_ */
