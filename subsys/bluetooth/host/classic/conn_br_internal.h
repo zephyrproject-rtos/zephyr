22ea55c7a0aaeda173ff6a3b484ca54e83f8f103
Lyle Zhu <lyle.zhu@nxp.com>
2025-03-21 20:59:37 +0800

    Bluetooth: Classic: Move classic functions from conn.c to conn_br.c
    Move functions `bt_sco_cleanup()`, `bt_conn_create_br()`,
    `bt_hci_connect_br_cancel()`, and `bt_conn_get_dst_br()` from `conn.c`
    to `conn_br.c`.
    
    Change the function `bt_hci_connect_br_cancel()` to internal public
    function.
    
    Add a header file `conn_br_internal.h`. And declare the function
    `bt_hci_connect_br_cancel()` in the header file.
    
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

---
+++ subsys/bluetooth/host/classic/conn_br_internal.h
@@ -0,0 +1,12 @@
+/** @file
+ *  @brief Internal APIs for Bluetooth classic connection handling.
+ */
+
+/*
+ * Copyright (c) 2015 Intel Corporation
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int bt_hci_connect_br_cancel(struct bt_conn *conn);
