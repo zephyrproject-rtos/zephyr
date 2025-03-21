39b9e54ded9982cf3ba1cde24cf4b95608efcd17
Ryan Chu <ryan.chu@nordicsemi.no>
2025-03-21 15:03:00 +0100

    bluetooth: host: Report status of Channel Sounding complete events
    If the HCI status of a complete event is not BT_HCI_ERR_SUCCESS,
    the remaining parameters could be invalid.
    
    In this case, the params is passed as NULL pointer to the callbacks.
    - LE CS Read Remote Supported Capabilities Complete event
    - LE CS Read Remote FAE Table Complete event
    - LE CS Config Complete event
    - LE CS Security Enable Complete event
    - LE CS Procedure Enable Complete event
    
    This change avoids forwarding the invalid fileds to the applications.
    
    Signed-off-by: Ryan Chu <ryan.chu@nordicsemi.no>

--- subsys/bluetooth/host/conn_internal.h
+++ subsys/bluetooth/host/conn_internal.h
@@ -495,20 +495,25 @@ void notify_subrate_change(struct bt_conn *conn,
 			   struct bt_conn_le_subrate_changed params);
 
 void notify_remote_cs_capabilities(struct bt_conn *conn,
-			   struct bt_conn_le_cs_capabilities params);
+				   uint8_t status,
+				   struct bt_conn_le_cs_capabilities *params);
 
 void notify_remote_cs_fae_table(struct bt_conn *conn,
-			   struct bt_conn_le_cs_fae_table params);
+				uint8_t status,
+				struct bt_conn_le_cs_fae_table *params);
 
-void notify_cs_config_created(struct bt_conn *conn, struct bt_conn_le_cs_config *params);
+void notify_cs_config_created(struct bt_conn *conn,
+			      uint8_t status,
+			      struct bt_conn_le_cs_config *params);
 
 void notify_cs_config_removed(struct bt_conn *conn, uint8_t config_id);
 
 void notify_cs_subevent_result(struct bt_conn *conn, struct bt_conn_le_cs_subevent_result *result);
 
-void notify_cs_security_enable_available(struct bt_conn *conn);
+void notify_cs_security_enable_available(struct bt_conn *conn, uint8_t status);
 
 void notify_cs_procedure_enable_available(struct bt_conn *conn,
+					  uint8_t status,
 					  struct bt_conn_le_cs_procedure_enable_complete *params);
 
 #if defined(CONFIG_BT_SMP)
