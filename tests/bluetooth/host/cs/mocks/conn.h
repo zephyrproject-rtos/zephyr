c9240cc99f939a8b42cc62725e1c3037d848105e
Ryan Chu <ryan.chu@nordicsemi.no>
2025-03-21 15:03:00 +0100

    bluetooth: host: Report status of Channel Sounding complete events
    If the HCI status of a complete event is not BT_HCI_ERR_SUCCESS,
    the remaining parameters could be invalid.
    
    In this case, the params is passed as NULL pointer to the callbacks.
    - LE CS Read Remote Supported Capabilities Complete event
    - LE CS Read Remote FAE Table Complete event
    - LE CS Config Complete event
    - LE CS Security Enable Complete event
    - LE CS Procedure Enable Complete event
    
    This change avoids forwarding the invalid fileds to the applications.
    
    Signed-off-by: Ryan Chu <ryan.chu@nordicsemi.no>

--- tests/bluetooth/host/cs/mocks/conn.h
+++ tests/bluetooth/host/cs/mocks/conn.h
@@ -25,13 +25,14 @@
 DECLARE_FAKE_VOID_FUNC(bt_conn_unref, struct bt_conn *);
 DECLARE_FAKE_VALUE_FUNC(struct bt_conn *, bt_conn_lookup_handle, uint16_t, enum bt_conn_type);
 DECLARE_FAKE_VOID_FUNC(notify_remote_cs_capabilities, struct bt_conn *,
-		       struct bt_conn_le_cs_capabilities);
+		       uint8_t, struct bt_conn_le_cs_capabilities *);
 DECLARE_FAKE_VOID_FUNC(notify_remote_cs_fae_table, struct bt_conn *,
-		       struct bt_conn_le_cs_fae_table);
-DECLARE_FAKE_VOID_FUNC(notify_cs_config_created, struct bt_conn *, struct bt_conn_le_cs_config *);
+		       uint8_t, struct bt_conn_le_cs_fae_table *);
+DECLARE_FAKE_VOID_FUNC(notify_cs_config_created, struct bt_conn *,
+		       uint8_t, struct bt_conn_le_cs_config *);
 DECLARE_FAKE_VOID_FUNC(notify_cs_config_removed, struct bt_conn *, uint8_t);
 DECLARE_FAKE_VOID_FUNC(notify_cs_subevent_result, struct bt_conn *,
 		       struct bt_conn_le_cs_subevent_result *);
-DECLARE_FAKE_VOID_FUNC(notify_cs_security_enable_available, struct bt_conn *);
+DECLARE_FAKE_VOID_FUNC(notify_cs_security_enable_available, struct bt_conn *, uint8_t);
 DECLARE_FAKE_VOID_FUNC(notify_cs_procedure_enable_available, struct bt_conn *,
-		       struct bt_conn_le_cs_procedure_enable_complete *);
+		       uint8_t, struct bt_conn_le_cs_procedure_enable_complete *);
