dd1c3948ca63bc2b427df75d66e11dbb2857c089
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2025-03-25 13:32:40 +0100

    fixup! Bluetooth: Controller: Fix incorrect CIS payload count at CIS Estab
    Fix incorrect payload count at CIS Establish due to existing
    CIG event overlapping the ACL event at the instant when the
    CIS gets the active flag set.
    
    The overlapping CIG event picked up the new CIS that had its
    active flag set in the current CIG event instead of at the
    actual CIS offset which is in the next CIG event.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_peripheral_iso.c
+++ subsys/bluetooth/controller/ll_sw/ull_peripheral_iso.c
@@ -259,7 +259,7 @@ uint8_t ull_peripheral_iso_acquire(struct ll_conn *acl,
 	cis->lll.active = 0U;
 
 #if !defined(CONFIG_BT_CTLR_JIT_SCHEDULING)
-	cis->lll.established = 0U;
+	cis->lll.prepared = 0U;
 #endif /* !CONFIG_BT_CTLR_JIT_SCHEDULING */
 
 	cis->lll.handle = LLL_HANDLE_INVALID;
