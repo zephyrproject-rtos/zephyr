d0cd8657a84e3fb0324a776786f472a4154a7ae1
Håvard Reierstad <haavard.reierstad@nordicsemi.no>
2025-03-25 08:07:44 +0100

    Bluetooth: Mesh: Fix typo in prov bsim
    Changes Brearer --> Bearer.
    
    Signed-off-by: Håvard Reierstad <haavard.reierstad@nordicsemi.no>

--- tests/bsim/bluetooth/mesh/tests_scripts/provision/prov_oob_auth_ib_pk.sh
+++ tests/bsim/bluetooth/mesh/tests_scripts/provision/prov_oob_auth_ib_pk.sh
@@ -7,14 +7,14 @@ source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
 # Test provisioning with OOB authentication and with inband public key
 RunTest mesh_prov_pb_adv_oob_auth \
 	prov_device_oob_auth prov_provisioner_oob_auth \
-	-- -argstest prov-brearer=1
+	-- -argstest prov-bearer=1
 
 overlay=overlay_gatt_conf_overlay_psa_conf
 RunTest mesh_prov_pb_gatt_oob_auth \
 	prov_device_oob_auth prov_provisioner_oob_auth \
-	-- -argstest prov-brearer=2
+	-- -argstest prov-bearer=2
 
 overlay=overlay_psa_conf
 RunTest mesh_prov_pb_adv_oob_auth_psa \
 	prov_device_oob_auth prov_provisioner_oob_auth \
-	-- -argstest prov-brearer=1
+	-- -argstest prov-bearer=1
