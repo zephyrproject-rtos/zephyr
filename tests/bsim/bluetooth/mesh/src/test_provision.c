07f53b53c87647554873d495eb45b150ef9a64b8
Håvard Reierstad <haavard.reierstad@nordicsemi.no>
2025-03-25 08:07:44 +0100

    Bluetooth: Mesh: Fix typo in prov bsim
    Changes Brearer --> Bearer.
    
    Signed-off-by: Håvard Reierstad <haavard.reierstad@nordicsemi.no>

--- tests/bsim/bluetooth/mesh/src/test_provision.c
+++ tests/bsim/bluetooth/mesh/src/test_provision.c
@@ -109,7 +109,7 @@ static void test_args_parse(int argc, char *argv[])
 			.dest = &prov_bearer,
 			.type = 'i',
 			.name = "{invalid, PB-ADV, PB-GATT}",
-			.option = "prov-brearer",
+			.option = "prov-bearer",
 			.descript = "Provisioning bearer that is to be used."
 		},
 	};
--- tests/bsim/bluetooth/mesh/tests_scripts/provision/prov_no_oob.sh
+++ tests/bsim/bluetooth/mesh/tests_scripts/provision/prov_no_oob.sh
@@ -7,16 +7,16 @@ source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
 RunTest mesh_prov_pb_adv_on_oob \
 	prov_device_no_oob \
 	prov_provisioner_no_oob \
-	-- -argstest prov-brearer=1
+	-- -argstest prov-bearer=1
 
 overlay=overlay_gatt_conf_overlay_psa_conf
 RunTest mesh_prov_pb_gatt_on_oob \
 	prov_device_no_oob \
 	prov_provisioner_no_oob \
-	-- -argstest prov-brearer=2
+	-- -argstest prov-bearer=2
 
 overlay=overlay_psa_conf
 RunTest mesh_prov_pb_adv_on_oob_psa \
 	prov_device_no_oob \
 	prov_provisioner_no_oob \
-	-- -argstest prov-brearer=1
+	-- -argstest prov-bearer=1
