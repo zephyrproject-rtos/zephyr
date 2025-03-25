f908bbc0a2f3a8b5175c838c19faf7c5c373312c
Ederson de Souza <ederson.desouza@intel.com>
2025-03-25 12:19:43 -0700

    arch/x86: Indirect Branch Tracking support
    Indirect Branch Tracking (IBT) is one of the capabilities provided by
    Intel Control-flow Enforcement Technology (CET), aimed at defending
    against Jump/Call Oriented Programming.
    
    This patch enables it for x86 (32-bit, 64-bit support coming in future
    patches):
    
        - Add relevant Kconfigs (everything is behind X86_CET);
        - Code to enable it;
        - Enable compiler flags to enable it;
        - Add `endbr32` instructions to asm code, where needed.
    
    Points in the code where an indirect branch is expected to land need
    special instructions that tell the CPU they are valid indirect branch
    targets. Those are added by the compiler, so toolchain support is
    necessary. Note that any code added to the final ELF also need those
    markers, such as libc or libgcc.
    
    Finally, tests added to ensure IBT behaves sanely.
    
    Signed-off-by: Ederson de Souza <ederson.desouza@intel.com>

---
+++ tests/arch/x86/cet/src/main.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2025 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/ztest.h>
+#include <zephyr/arch/exception.h>
+
+#define IV_CTRL_PROTECTION_EXCEPTION 21
+
+#define CTRL_PROTECTION_ERRORCODE_ENDBRANCH 3
+
+extern int should_work(int a);
+extern int should_not_work(int a);
+
+static bool expect_fault;
+static int expect_code;
+
+void k_sys_fatal_error_handler(unsigned int reason, const struct arch_esf *pEsf)
+{
+	if (expect_fault) {
+		zassert_equal(z_x86_exception_vector, IV_CTRL_PROTECTION_EXCEPTION,
+					  "unexpected exception");
+		zassert_equal(pEsf->errorCode, expect_code, "unexpected error code");
+		printk("fatal error expected as part of test case\n");
+		expect_fault = false;
+	} else {
+		printk("fatal error was unexpected, aborting\n");
+		TC_END_REPORT(TC_FAIL);
+		k_fatal_halt(reason);
+	}
+}
+
+/* Round trip to trick optimisations and ensure the calls are indirect */
+int do_call(int (*func)(int), int a)
+{
+	return func(a);
+}
+
+ZTEST(cet, test_ibt)
+{
+	zassert_equal(do_call(should_work, 1), 2, "should_work failed");
+
+	expect_fault = true;
+	expect_code = CTRL_PROTECTION_ERRORCODE_ENDBRANCH;
+	do_call(should_not_work, 1);
+	zassert_unreachable("should_not_work did not fault");
+}
+
+ZTEST_SUITE(cet, NULL, NULL, NULL, NULL, NULL);
