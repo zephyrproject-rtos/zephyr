acb410d3651e7723ee3b3032c50c3e5d2d022867
Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
2025-03-26 10:22:22 +0100

    boards: nordic: Add initial support for nRF54L10 with TF-M
    This adds the nrf54l15dk/nrf54l10/cpuapp/ns board variant to
    Zephyr. It allows to build applications for this target.
    
    This is an initial support for the non secure target which allows
    building and running tfm_ipc and config_build.
    
    This is NOT full support of the non secure target in upstream
    Zephyr.
    
    There are important limitations, such as:
      - The hardware Crypto accelerator is not supported and thus the non
        secur target is NOT secure for production applications in upstream
        Zephyr.
      - The BL2 is not supported, so no DFU is supported with this support
    
    Most of the code changes here are taken from nRF Connect SDK
    in order to avoid having noups there.
    
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

---
+++ modules/trusted-firmware-m/nordic/nrf54l10_cpuapp/config.cmake
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2025, Nordic Semiconductor ASA.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set(NRF_SOC_VARIANT nrf54l10 CACHE STRING "nRF SoC Variant")
+
+include(${PLATFORM_PATH}/common/nrf54l10/config.cmake)
