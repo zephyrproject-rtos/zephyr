46b30f997e7af79cc597c82e088d64ecd393d95d
Mahesh Mahadevan <mahesh.mahadevan@nxp.com>
2025-03-26 11:45:15 -0500

    Revert "[nxp noup] soc: nxp: rw: Add support for Power Mode 3"
    This reverts commit c2468cfd7be1405ba95a9d3063d01251e9d052b6.
    
    Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

--- soc/nxp/rw/power.c
+++ soc/nxp/rw/power.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 NXP
+ * Copyright 2023, NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -11,8 +11,6 @@
 #include "fsl_power.h"
 
 #include <zephyr/logging/log.h>
-#include <zephyr/drivers/timer/system_timer.h>
-
 LOG_MODULE_DECLARE(soc, CONFIG_SOC_LOG_LEVEL);
 
 /* Active mode */
@@ -27,8 +25,6 @@ LOG_MODULE_DECLARE(soc, CONFIG_SOC_LOG_LEVEL);
 #define POWER_MODE4		4
 
 #define NODE_ID DT_INST(0, nxp_pdcfg_power)
-
-extern void clock_init(void);
 
 power_sleep_config_t slp_cfg;
 
@@ -59,81 +55,6 @@ static void pin1_isr(const struct device *dev)
 	POWER_DisableWakeup(DT_IRQN(DT_NODELABEL(pin1)));
 }
 #endif
-
-#ifdef CONFIG_MPU
-
-#define MPU_NUM_REGIONS 8
-
-typedef struct mpu_conf {
-	uint32_t CTRL;
-	uint32_t RNR;
-	uint32_t RBAR[MPU_NUM_REGIONS];
-	uint32_t RLAR[MPU_NUM_REGIONS];
-	uint32_t RBAR_A1;
-	uint32_t RLAR_A1;
-	uint32_t RBAR_A2;
-	uint32_t RLAR_A2;
-	uint32_t RBAR_A3;
-	uint32_t RLAR_A3;
-	uint32_t MAIR0;
-	uint32_t MAIR1;
-} mpu_conf;
-mpu_conf saved_mpu_conf;
-
-static void save_mpu_state(void)
-{
-	uint32_t index;
-
-	/*
-	 * MPU is divided in `MPU_NUM_REGIONS` regions.
-	 * Here we save those regions configuration to be restored after PM3 entry
-	 */
-	for (index = 0U; index < MPU_NUM_REGIONS; index++) {
-		MPU->RNR = index;
-		saved_mpu_conf.RBAR[index] = MPU->RBAR;
-		saved_mpu_conf.RLAR[index] = MPU->RLAR;
-	}
-
-	saved_mpu_conf.CTRL = MPU->CTRL;
-	saved_mpu_conf.RNR = MPU->RNR;
-	saved_mpu_conf.RBAR_A1 = MPU->RBAR_A1;
-	saved_mpu_conf.RLAR_A1 = MPU->RLAR_A1;
-	saved_mpu_conf.RBAR_A2 = MPU->RBAR_A2;
-	saved_mpu_conf.RLAR_A2 = MPU->RLAR_A2;
-	saved_mpu_conf.RBAR_A3 = MPU->RBAR_A3;
-	saved_mpu_conf.RLAR_A3 = MPU->RLAR_A3;
-	saved_mpu_conf.MAIR0 = MPU->MAIR0;
-	saved_mpu_conf.MAIR1 = MPU->MAIR1;
-}
-
-static void restore_mpu_state(void)
-{
-	uint32_t index;
-
-	for (index = 0U; index < MPU_NUM_REGIONS; index++) {
-		MPU->RNR = index;
-		MPU->RBAR = saved_mpu_conf.RBAR[index];
-		MPU->RLAR = saved_mpu_conf.RLAR[index];
-	}
-
-	MPU->RNR = saved_mpu_conf.RNR;
-	MPU->RBAR_A1 = saved_mpu_conf.RBAR_A1;
-	MPU->RLAR_A1 = saved_mpu_conf.RLAR_A1;
-	MPU->RBAR_A2 = saved_mpu_conf.RBAR_A2;
-	MPU->RLAR_A2 = saved_mpu_conf.RLAR_A2;
-	MPU->RBAR_A3 = saved_mpu_conf.RBAR_A3;
-	MPU->RLAR_A3 = saved_mpu_conf.RLAR_A3;
-	MPU->MAIR0 = saved_mpu_conf.MAIR0;
-	MPU->MAIR1 = saved_mpu_conf.MAIR1;
-
-	/*
-	 * CTRL register must be restored last in case MPU was enabled,
-	 * because some MPU registers can't be programmed while the MPU is enabled
-	 */
-	MPU->CTRL = saved_mpu_conf.CTRL;
-}
-
-#endif /* CONFIG_MPU */
 
 /* Invoke Low Power/System Off specific Tasks */
 __weak void pm_state_set(enum pm_state state, uint8_t substate_id)
@@ -171,28 +92,6 @@ __weak void pm_state_set(enum pm_state state, uint8_t substate_id)
 		break;
 	case PM_STATE_SUSPEND_TO_IDLE:
 		POWER_EnterPowerMode(POWER_MODE2, &slp_cfg);
-		break;
-	case PM_STATE_STANDBY:
-#ifdef CONFIG_MPU
-		/* Save MPU state before entering PM3 */
-		save_mpu_state();
-#endif /* CONFIG_MPU */
-
-		POWER_EnableWakeup(DT_IRQN(DT_NODELABEL(rtc)));
-		if (POWER_EnterPowerMode(POWER_MODE3, &slp_cfg)) {
-#ifdef CONFIG_MPU
-			/* Restore MPU as is lost after PM3 exit*/
-			restore_mpu_state();
-#endif /* CONFIG_MPU */
-			clock_init();
-
-			sys_clock_idle_exit();
-		}
-
-		/* Clear the RTC wakeup bits */
-		POWER_ClearWakeupStatus(DT_IRQN(DT_NODELABEL(rtc)));
-		POWER_DisableWakeup(DT_IRQN(DT_NODELABEL(rtc)));
-
 		break;
 	default:
 		LOG_DBG("Unsupported power state %u", state);
@@ -243,8 +142,4 @@ void nxp_rw6xx_power_init(void)
 	IRQ_CONNECT(DT_IRQN(DT_NODELABEL(pin1)), DT_IRQ(DT_NODELABEL(pin1), priority), pin1_isr,
 		    NULL, 0);
 #endif
-
-	/* Clear the RTC wakeup bits */
-	POWER_ClearWakeupStatus(DT_IRQN(DT_NODELABEL(rtc)));
-	POWER_DisableWakeup(DT_IRQN(DT_NODELABEL(rtc)));
 }
