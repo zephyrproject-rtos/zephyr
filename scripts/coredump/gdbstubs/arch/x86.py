f8cf908af8bfdaadb802feca80a5468a081c8775
Mark Holden <mholden@meta.com>
2025-03-27 15:59:40 -0700

    coredump: x86 python compliance fix
    Move comon code into shared file to make compliance tool happy
    
    Signed-off-by: Mark Holden <mholden@meta.com>

--- scripts/coredump/gdbstubs/arch/x86.py
+++ scripts/coredump/gdbstubs/arch/x86.py
@@ -9,6 +9,7 @@ import logging
 import struct
 
 from gdbstubs.gdbstub import GdbStub
+from x86_shared import ExceptionVectors
 
 
 logger = logging.getLogger("gdbstub")
@@ -32,32 +33,6 @@ class RegNum():
     ES = 13
     FS = 14
     GS = 15
-
-
-class ExceptionVectors():
-    # Matches arch/x86/include/kernel_arch_data.h
-    IV_DIVIDE_ERROR = 0
-    IV_DEBUG = 1
-    IV_NON_MASKABLE_INTERRUPT = 2
-    IV_BREAKPOINT = 3
-    IV_OVERFLOW = 4
-    IV_BOUND_RANGE = 5
-    IV_INVALID_OPCODE = 6
-    IV_DEVICE_NOT_AVAILABLE = 7
-    IV_DOUBLE_FAULT = 8
-    IV_COPROC_SEGMENT_OVERRUN = 9
-    IV_INVALID_TSS = 10
-    IV_SEGMENT_NOT_PRESENT = 11
-    IV_STACK_FAULT = 12
-    IV_GENERAL_PROTECTION = 13
-    IV_PAGE_FAULT = 14
-    IV_RESERVED = 15
-    IV_X87_FPU_FP_ERROR = 16
-    IV_ALIGNMENT_CHECK = 17
-    IV_MACHINE_CHECK = 18
-    IV_SIMD_FP = 19
-    IV_VIRT_EXCEPTION = 20
-    IV_SECURITY_EXCEPTION = 30
 
 
 class GdbStub_x86(GdbStub):
