23b61fdbd24efda5eef70cb5a281d95ccb4e198d
Loek Le Blansch <loek.le-blansch.pv@renesas.com>
2025-03-28 13:07:23 +0100

    revert "build: support newlines in syscall decls"
    This reverts commit 1993ea019b15857786b624b889198e32862ada59 because the
    `parse_syscalls.py` script has been modified to not output whitespace
    characters other than spaces in the generated JSON file.
    
    Signed-off-by: Loek Le Blansch <loek.le-blansch.pv@renesas.com>

--- scripts/build/gen_syscalls.py
+++ scripts/build/gen_syscalls.py
@@ -201,7 +201,6 @@ class SyscallParseException(Exception):
 
 
 def typename_split(item):
-    item = item.strip().replace("\n", " ")
     if "[" in item:
         raise SyscallParseException(
             "Please pass arrays to syscalls as pointers, unable to process '%s'" %
@@ -414,7 +413,7 @@ def analyze_fn(match_group, fn, userspace_only):
         if args == "void" or len(args) == 0:
             args = []
         else:
-            args = [typename_split(a) for a in args.split(",")]
+            args = [typename_split(a.strip()) for a in args.split(",")]
 
         func_type, func_name = typename_split(func)
     except SyscallParseException:
