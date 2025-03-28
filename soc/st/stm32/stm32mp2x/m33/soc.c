e1304e1cefa2fe57534ffbc06c8b50ca408da994
Youssef Zini <youssef.zini@savoirfairelinux.com>
2025-03-28 13:43:08 +0100

    soc: stm32: add initial soc support for stm32mp2x
    Add initial soc support for the stm32mp2x series, including
    initial Kconfig entries, default configuration files, and minimal SoC
    initialization code. This enables Zephyr to recognize and build for the
    stm32mp2x series, taking the stm32mp257f_ev1 as a baseline.
    
    Includes:
    - Kconfig and defconfig files for SoC selection and defaults
    - soc.c and soc.h for minimal initialization and integration of cm33
    - CMakeLists.txt for build system integration
    - soc.yml update to register the new SoC
    
    Signed-off-by: Youssef Zini <youssef.zini@savoirfairelinux.com>

---
+++ soc/st/stm32/stm32mp2x/m33/soc.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2025 Savoir-faire Linux, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief System/hardware module for STM32MP2 processor
+ */
+
+#include <cmsis_core.h>
+#include <soc.h>
+#include <zephyr/device.h>
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+
+/**
+ * @brief Perform basic hardware initialization at boot.
+ *
+ * This needs to be run from the very beginning.
+ */
+void soc_early_init_hook(void)
+{
+	/* Update CMSIS SystemCoreClock variable (HCLK) */
+	SystemCoreClock = 400000000;
+}
