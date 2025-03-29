dc18de0e0b16571463d837b0b628079bff42d158
Ederson de Souza <ederson.desouza@intel.com>
2025-03-28 17:22:47 -0700

    arch/x86: Support shadow stack on nested IRQs
    Nested interrupts are supported, on the normal stack, by creating a
    stack whose size is a multiple of CONFIG_ISR_DEPTH, and updating the
    pointer used by Interrupt Stack Table (IST) to point to a new base,
    inside the "oversized" stack.
    
    The same approach is used for the shadow stack: shadow stack size is
    multiplied by CONFIG_ISR_DEPTH, and the pointer to the stack on the
    shadow stack pointer table is update to point to the next base.
    
    Signed-off-by: Ederson de Souza <ederson.desouza@intel.com>

--- arch/x86/core/offsets/intel64_offsets.c
+++ arch/x86/core/offsets/intel64_offsets.c
@@ -45,6 +45,9 @@ GEN_OFFSET_SYM(x86_tss64_t, ist2);
 GEN_OFFSET_SYM(x86_tss64_t, ist6);
 GEN_OFFSET_SYM(x86_tss64_t, ist7);
 GEN_OFFSET_SYM(x86_tss64_t, cpu);
+#ifdef CONFIG_X86_CET_SHADOW_STACK
+GEN_OFFSET_SYM(x86_tss64_t, shstk_addr);
+#endif
 #ifdef CONFIG_USERSPACE
 GEN_OFFSET_SYM(x86_tss64_t, psp);
 GEN_OFFSET_SYM(x86_tss64_t, usp);
