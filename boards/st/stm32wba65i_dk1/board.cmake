7dfbc3446803dd7e41da4bcebc2c86b5b7393493
Etienne Carriere <etienne.carriere@st.com>
2025-03-31 12:27:51 +0200

    boards: st: Add stm32wba65i-dk1
    Add stm32wba65i-dk1 board support with UART console, LEDs, joystick keys
    using ADC channel 6.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

---
+++ boards/st/stm32wba65i_dk1/board.cmake
@@ -0,0 +1,7 @@
+# Copyright (c) 2025 STMicroelectronics
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
