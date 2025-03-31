85c13f30360e024ea81dd4db7d299aa34241f4f5
Peggy Chen <peggy.chen@cienet.com>
2025-03-31 08:10:03 +0800

    drivers: sensor: bma4xx: Implement streaming APIs
    Add a streaming implementation for the BMA4XX using both
    FIFO watermark and FIFO full interrupts. A batch duration of
    3,000 have been used for verification.
    
    Signed-off-by: Peggy Chen <peggy.chen@cienet.com>

--- drivers/sensor/bosch/bma4xx/bma4xx.c
+++ drivers/sensor/bosch/bma4xx/bma4xx.c
@@ -15,12 +15,13 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/pm/device.h>
 #include <zephyr/rtio/work.h>
-#include <zephyr/drivers/sensor_clock.h>
 
-LOG_MODULE_REGISTER(bma4xx, CONFIG_SENSOR_LOG_LEVEL);
 #include "bma4xx.h"
+#include "bma4xx_interrupt.h"
 #include "bma4xx_decoder.h"
 #include "bma4xx_rtio.h"
+
+LOG_MODULE_REGISTER(bma4xx, CONFIG_SENSOR_LOG_LEVEL);
 
 /**
  * @brief Helper for converting m/s^2 offset values into register values
@@ -112,8 +113,7 @@ static int bma4xx_odr_to_reg(uint32_t microhertz, uint8_t *reg_val)
 }
 
 /**
- * Set the sensor's acceleration offset (per axis). Use bma4xx_commit_nvm() to save these
- * offsets to nonvolatile memory so they are automatically set during power-on-reset.
+ * Set the sensor's acceleration offset (per axis).
  */
 static int bma4xx_attr_set_odr(const struct sensor_value *val,
 			       struct bma4xx_runtime_config *new_config)
@@ -226,6 +226,17 @@ static int bma4xx_attr_set(const struct device *dev, enum sensor_channel chan,
 			res = -ENOTSUP;
 		}
 		break;
+	case SENSOR_CHAN_ALL:
+		if (attr == SENSOR_ATTR_BATCH_DURATION) {
+			if (val->val1 < 0) {
+				return -EINVAL;
+			}
+			new_config.batch_ticks = val->val1;
+		} else {
+			LOG_ERR("Unsupported attribute");
+			res = -EINVAL;
+		}
+		break;
 	default:
 		LOG_ERR("Unsupported channel");
 		res = -EINVAL;
@@ -252,7 +263,7 @@ static int bma4xx_chip_init(const struct device *dev)
 	/* Sensor bus-specific initialization */
 	status = cfg->bus_init(dev);
 	if (status) {
-		LOG_ERR("bus_init failed: %d", status);
+		LOG_ERR("Failed to initialize bus: %d", status);
 		return status;
 	}
 
@@ -275,31 +286,26 @@ static int bma4xx_chip_init(const struct device *dev)
 		return status;
 	}
 
-	k_sleep(K_USEC(1000));
+	if (IS_ENABLED(CONFIG_BMA4XX_STREAM)) {
+		status = bma4xx_init_interrupt(dev);
+		if (status != 0) {
+			LOG_ERR("Failed to initialize bma4xx interrupt");
+			return status;
+		}
+	}
 
 	/* Default is: range = +/-4G, ODR = 100 Hz, BWP = "NORM_AVG4" */
 	bma4xx->cfg.accel_fs_range = BMA4XX_RANGE_4G;
 	bma4xx->cfg.accel_bwp = BMA4XX_BWP_NORM_AVG4;
 	bma4xx->cfg.accel_odr = BMA4XX_ODR_100;
 
-	/* Switch to performance power mode */
-	status = bma4xx->hw_ops->update_reg(dev, BMA4XX_REG_ACCEL_CONFIG, BMA4XX_BIT_ACC_PERF_MODE,
-					    BMA4XX_BIT_ACC_PERF_MODE);
+	status = bma4xx_configure(dev, &bma4xx->cfg);
 	if (status) {
-		LOG_ERR("Could not enable performance power save mode: %d", status);
+		LOG_ERR("Failed to initialize bma4xx trigger");
 		return status;
 	}
 
-	/* Enable accelerometer */
-	status =
-		bma4xx->hw_ops->update_reg(dev, BMA4XX_REG_POWER_CTRL, BMA4XX_BIT_POWER_CTRL_ACC_EN,
-					   BMA4XX_BIT_POWER_CTRL_ACC_EN);
-	if (status) {
-		LOG_ERR("Could not enable accel: %d", status);
-		return status;
-	}
-
-	return status;
+	return 0;
 }
 
 /*
@@ -308,8 +314,8 @@ static int bma4xx_chip_init(const struct device *dev)
 
 static DEVICE_API(sensor, bma4xx_driver_api) = {
 	.attr_set = bma4xx_attr_set,
-	.submit = bma4xx_submit,
 	.get_decoder = bma4xx_get_decoder,
+	.submit = bma4xx_submit,
 };
 
 /*
@@ -343,9 +349,18 @@ static DEVICE_API(sensor, bma4xx_driver_api) = {
 #define BMA4XX_DEFINE_BUS(inst)                                                                    \
 	COND_CODE_1(DT_INST_ON_BUS(inst, spi), (BMA4XX_CONFIG_SPI(inst)), (BMA4XX_CONFIG_I2C(inst)))
 
+#define BMA4XX_DT_CONFIG_INIT(inst)                                                                \
+	{                                                                                          \
+		.fifo_en = IS_ENABLED(CONFIG_BMA4XX_STREAM),                                       \
+		.batch_ticks = 0,                                                                  \
+		.interrupt1_fifo_wm = false,                                                       \
+		.interrupt1_fifo_full = false,                                                     \
+	}
+
 #define BMA4XX_DEFINE_DATA(inst)                                                                   \
 	BMA4XX_DEFINE_RTIO(inst);                                                                  \
 	static struct bma4xx_data bma4xx_driver_##inst = {                                         \
+		.cfg = BMA4XX_DT_CONFIG_INIT(inst),                                                \
 		.r = &bma4xx_rtio_##inst,                                                          \
 		.iodev = &bma4xx_iodev_##inst,                                                     \
 	};
@@ -357,7 +372,10 @@ static DEVICE_API(sensor, bma4xx_driver_api) = {
 #define BMA4XX_INIT(inst)                                                                          \
 	BMA4XX_DEFINE_DATA(inst);                                                                  \
                                                                                                    \
-	static const struct bma4xx_config bma4xx_config_##inst = {BMA4XX_DEFINE_BUS(inst)};        \
+	static const struct bma4xx_config bma4xx_config_##inst = {                                 \
+		BMA4XX_DEFINE_BUS(inst)                                                            \
+			IF_ENABLED(CONFIG_BMA4XX_STREAM, ( \
+		.gpio_interrupt = GPIO_DT_SPEC_INST_GET_OR(inst, int1_gpios, {0}),))};         \
                                                                                                    \
 	SENSOR_DEVICE_DT_INST_DEFINE(inst, bma4xx_chip_init, NULL, &bma4xx_driver_##inst,          \
 				     &bma4xx_config_##inst, POST_KERNEL,                           \
