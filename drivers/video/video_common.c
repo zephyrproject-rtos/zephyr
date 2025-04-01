541f190152fecd71935e87341315e3a604cf5038
Josuah Demangeon <me@josuah.net>
2025-04-01 17:52:38 +0000

    drivers: video: common: add shared implementation for imagers
    Imagers, also known as image sensors, have drivers that look very similar.
    Add common implementation that fits most image sensors, which can be
    bypassed wherever this does not make the implementation simpler.
    
    Signed-off-by: Josuah Demangeon <me@josuah.net>

--- drivers/video/video_common.c
+++ drivers/video/video_common.c
@@ -301,3 +301,196 @@ int video_write_cci_multi(const struct i2c_dt_spec *i2c, const struct video_reg 
 
 	return 0;
 }
+
+/* Common implementation for imagers (a.k.a. image sensor) drivers */
+
+int video_imager_set_mode(const struct device *dev, const struct video_imager_mode *mode)
+{
+	const struct video_imager_config *cfg = dev->config;
+	struct video_imager_data *data = dev->data;
+	int ret;
+
+	if (data->mode == mode) {
+		LOG_DBG("%s is arlready in the mode requested", dev->name);
+		return 0;
+	}
+
+	/* Write each register table to the device */
+	for (int i = 0; i < ARRAY_SIZE(mode->regs) && mode->regs[i] != NULL; i++) {
+		ret = cfg->write_multi(&cfg->i2c, mode->regs[i]);
+		if (ret != 0) {
+			LOG_ERR("Could not set %s to mode %p, %u FPS", dev->name, mode, mode->fps);
+			return ret;
+		}
+	}
+
+	data->mode = mode;
+
+	return 0;
+}
+
+int video_imager_set_frmival(const struct device *dev, enum video_endpoint_id ep,
+			     struct video_frmival *frmival)
+{
+	const struct video_imager_config *cfg = dev->config;
+	struct video_imager_data *data = dev->data;
+	struct video_frmival_enum fie = {.format = &data->fmt, .discrete = *frmival};
+
+	if (ep != VIDEO_EP_OUT && ep != VIDEO_EP_ALL) {
+		return -EINVAL;
+	}
+
+	video_closest_frmival(dev, ep, &fie);
+
+	return video_imager_set_mode(dev, &cfg->modes[data->fmt_id][fie.index]);
+}
+
+int video_imager_get_frmival(const struct device *dev, enum video_endpoint_id ep,
+			     struct video_frmival *frmival)
+{
+	struct video_imager_data *data = dev->data;
+
+	if (ep != VIDEO_EP_OUT && ep != VIDEO_EP_ALL) {
+		return -EINVAL;
+	}
+
+	frmival->numerator = 1;
+	frmival->denominator = data->mode->fps;
+
+	return 0;
+}
+
+int video_imager_enum_frmival(const struct device *dev, enum video_endpoint_id ep,
+			      struct video_frmival_enum *fie)
+{
+	const struct video_imager_config *cfg = dev->config;
+	const struct video_imager_mode *modes;
+	size_t fmt_id = 0;
+	int ret;
+
+	if (ep != VIDEO_EP_OUT && ep != VIDEO_EP_ALL) {
+		return -EINVAL;
+	}
+
+	ret = video_format_caps_index(cfg->fmts, fie->format, &fmt_id);
+	if (ret != 0) {
+		LOG_ERR("Format '%s' %ux%u not found for %s",
+			VIDEO_FOURCC_TO_STR(fie->format->pixelformat),
+			fie->format->width, fie->format->height, dev->name);
+		return ret;
+	}
+
+	modes = cfg->modes[fmt_id];
+
+	for (int i = 0;; i++) {
+		if (modes[i].fps == 0) {
+			return -EINVAL;
+		}
+
+		if (i == fie->index) {
+			fie->type = VIDEO_FRMIVAL_TYPE_DISCRETE;
+			fie->discrete.numerator = 1;
+			fie->discrete.denominator = modes[i].fps;
+			break;
+		}
+	}
+
+	return 0;
+}
+
+int video_imager_set_fmt(const struct device *const dev, enum video_endpoint_id ep,
+			 struct video_format *fmt)
+{
+	const struct video_imager_config *cfg = dev->config;
+	struct video_imager_data *data = dev->data;
+	size_t fmt_id;
+	int ret;
+
+	if (ep != VIDEO_EP_OUT && ep != VIDEO_EP_ALL) {
+		LOG_ERR("Only the output endpoint is supported for %s", dev->name);
+		return -EINVAL;
+	}
+
+	ret = video_format_caps_index(cfg->fmts, fmt, &fmt_id);
+	if (ret != 0) {
+		LOG_ERR("Format '%s' %ux%u not found for device %s",
+			VIDEO_FOURCC_TO_STR(fmt->pixelformat), fmt->width, fmt->height, dev->name);
+		return ret;
+	}
+
+	ret = video_imager_set_mode(dev, &cfg->modes[fmt_id][0]);
+	if (ret != 0) {
+		return ret;
+	}
+
+	data->fmt_id = fmt_id;
+	data->fmt = *fmt;
+
+	return 0;
+}
+
+int video_imager_get_fmt(const struct device *dev, enum video_endpoint_id ep,
+			 struct video_format *fmt)
+{
+	struct video_imager_data *data = dev->data;
+
+	if (ep != VIDEO_EP_OUT && ep != VIDEO_EP_ALL) {
+		return -EINVAL;
+	}
+
+	*fmt = data->fmt;
+
+	return 0;
+}
+
+int video_imager_get_caps(const struct device *dev, enum video_endpoint_id ep,
+			  struct video_caps *caps)
+{
+	const struct video_imager_config *cfg = dev->config;
+
+	if (ep != VIDEO_EP_OUT && ep != VIDEO_EP_ALL) {
+		return -EINVAL;
+	}
+
+	caps->format_caps = cfg->fmts;
+
+	return 0;
+}
+
+int video_imager_init(const struct device *dev, const struct video_reg *init_regs,
+		      int default_fmt_idx)
+{
+	const struct video_imager_config *cfg = dev->config;
+	struct video_format fmt;
+	int ret;
+
+	__ASSERT_NO_MSG(cfg->modes != NULL);
+	__ASSERT_NO_MSG(cfg->fmts != NULL);
+
+	if (!device_is_ready(cfg->i2c.bus)) {
+		LOG_ERR("I2C bus device %s is not ready", cfg->i2c.bus->name);
+		return -ENODEV;
+	}
+
+	if (init_regs != NULL) {
+		ret = cfg->write_multi(&cfg->i2c, init_regs);
+		if (ret != 0) {
+			LOG_ERR("Could not set %s initial registers", dev->name);
+			return ret;
+		}
+	}
+
+	fmt.pixelformat = cfg->fmts[default_fmt_idx].pixelformat;
+	fmt.width = cfg->fmts[default_fmt_idx].width_max;
+	fmt.height = cfg->fmts[default_fmt_idx].height_max;
+	fmt.pitch = fmt.width * video_bits_per_pixel(fmt.pixelformat) / BITS_PER_BYTE;
+
+	ret = video_set_format(dev, VIDEO_EP_OUT, &fmt);
+	if (ret != 0) {
+		LOG_ERR("Failed to set %s to default format '%s' %ux%u",
+			dev->name, VIDEO_FOURCC_TO_STR(fmt.pixelformat), fmt.width, fmt.height);
+		return ret;
+	}
+
+	return 0;
+}
--- drivers/video/video_common.h
+++ drivers/video/video_common.h
@@ -135,4 +135,126 @@ int video_read_cci_reg(const struct i2c_dt_spec *i2c, uint32_t reg_addr, uint32_
  */
 int video_write_cci_multi(const struct i2c_dt_spec *i2c, const struct video_reg *regs);
 
+/** @} */
+
+/**
+ * @defgroup video_imager Video Imager (image sensor) shared implementation
+ *
+ * This API is targeting image sensor driver developers.
+ *
+ * It provides a common implementation that only requires implementing a table of
+ * @ref video_imager_mode that lists the various resolution, frame rates and associated I2C
+ * configuration registers.
+ *
+ * The @c video_imager_... functions can be submitted directly to the @rev video_api.
+ * If a driver also needs to do extra work before or after applying a mode, it is possible
+ * to provide a custom wrapper or skip these default implementation altogether.
+ *
+ * @{
+ */
+
+/**
+ * @brief Table entry for an imaging mode of the sensor device.
+ *
+ * AA mode can be applied to an imager to configure a particular framerate, resolution, and pixel
+ * format. The index of the table of modes is meant to match the index of the table of formats.
+ */
+struct video_imager_mode {
+	/* FPS for this mode */
+	uint16_t fps;
+	/* Multiple lists of registers to allow sharing common sets of registers across modes. */
+	const struct video_reg *regs[3];
+};
+
+/**
+ * @brief A video imager device is expected to have dev->data point to this structure.
+ *
+ * In order to support custom data structure, it is possible to store an extra pointer
+ * in the dev->config struct. See existing drivers for an example.
+ */
+struct video_imager_config {
+	/** List of all formats supported by this sensor */
+	const struct video_format_cap *fmts;
+	/** Array of modes tables, one table per format cap lislted by "fmts" */
+	const struct video_imager_mode **modes;
+	/** I2C device to write the registers to */
+	struct i2c_dt_spec i2c;
+	/** Write a table of registers onto the device */
+	int (*write_multi)(const struct i2c_dt_spec *i2c, const struct video_reg *regs);
+	/** Reference to a ; */
+	struct video_imager_data *data;
+};
+
+/**
+ * @brief A video imager device is expected to have dev->data point to this structure.
+ *
+ * In order to support custom data structure, it is possible to store an extra pointer
+ * in the dev->config struct. See existing drivers for an example.
+ */
+struct video_imager_data {
+	/** Index of the currently active format in both modes[] and fmts[] */
+	int fmt_id;
+	/** Currently active video format */
+	struct video_format fmt;
+	/** Currently active operating mode as defined above */
+	const struct video_imager_mode *mode;
+};
+
+/**
+ * @brief Initialize one row of a @struct video_format_cap with fixed width and height.
+ *
+ * The minimum and maximum are the same for both width and height fields.
+ * @param
+ */
+#define VIDEO_IMAGER_FORMAT_CAP(pixfmt, width, height)                                             \
+	{                                                                                          \
+		.width_min = (width), .width_max = (width), .width_step = 0,                       \
+		.height_min = (height), .height_max = (height), .height_step = 0,                  \
+		.pixelformat = (pixfmt),                                                           \
+	}
+
+/**
+ * @brief Set the operating mode of the imager as defined in @ref video_imager_mode.
+ *
+ * If the default immplementation for the video API are used, there is no need to explicitly call
+ * this function in the image sensor driver.
+ *
+ * @param dev Device that has a struct video_imager in @c dev->data.
+ * @param mode The mode to apply to the image sensor.
+ * @return 0 if successful, or negative error number otherwise.
+ */
+int video_imager_set_mode(const struct device *dev, const struct video_imager_mode *mode);
+
+/** @brief Default implementation for image drivers frame interval selection */
+int video_imager_set_frmival(const struct device *dev, enum video_endpoint_id ep,
+			     struct video_frmival *frmival);
+/** @brief Default implementation for image drivers frame interval query */
+int video_imager_get_frmival(const struct device *dev, enum video_endpoint_id ep,
+			     struct video_frmival *frmival);
+/** @brief Default implementation for image drivers frame interval enumeration */
+int video_imager_enum_frmival(const struct device *dev, enum video_endpoint_id ep,
+			      struct video_frmival_enum *fie);
+/** @brief Default implementation for image drivers format selection */
+int video_imager_set_fmt(const struct device *const dev, enum video_endpoint_id ep,
+			 struct video_format *fmt);
+/** @brief Default implementation for image drivers format query */
+int video_imager_get_fmt(const struct device *dev, enum video_endpoint_id ep,
+			 struct video_format *fmt);
+/** @brief Default implementation for image drivers format capabilities */
+int video_imager_get_caps(const struct device *dev, enum video_endpoint_id ep,
+			  struct video_caps *caps);
+
+/**
+ * Initialize an imager by loading init_regs onto the device, and setting the default format.
+ *
+ * @param dev Device that has a struct video_imager in @c dev->data.
+ * @param init_regs If non-NULL, table of registers to configure at init.
+ * @param default_fmt_idx Default format index to apply at init.
+ * @return 0 if successful, or negative error number otherwise.
+ */
+int video_imager_init(const struct device *dev, const struct video_reg *init_regs,
+		      int default_fmt_idx);
+
+/** @} */
+
 #endif /* ZEPHYR_DRIVERS_VIDEO_COMMON_H_ */
