5a754176d229a7073745a225d3e31791f37248e3
Hugues Fruchet <hugues.fruchet@foss.st.com>
2025-04-02 10:09:26 +0200

    drivers: memc: stm32 xspi: add psram linker section
    Add stm32_psram PSRAM linker section.
    
    Signed-off-by: Hugues Fruchet <hugues.fruchet@foss.st.com>

--- include/zephyr/linker/section_tags.h
+++ include/zephyr/linker/section_tags.h
@@ -47,6 +47,7 @@
 #define __imx_boot_container_section Z_GENERIC_SECTION(_IMX_BOOT_CONTAINER_SECTION_NAME)
 #define __stm32_sdram1_section Z_GENERIC_SECTION(_STM32_SDRAM1_SECTION_NAME)
 #define __stm32_sdram2_section Z_GENERIC_SECTION(_STM32_SDRAM2_SECTION_NAME)
+#define __stm32_psram_section Z_GENERIC_SECTION(_STM32_PSRAM_SECTION_NAME)
 #define __stm32_backup_sram_section Z_GENERIC_SECTION(_STM32_BACKUP_SRAM_SECTION_NAME)
 #endif /* CONFIG_ARM */
 
