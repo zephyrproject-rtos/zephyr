24f0197399855471c5bec58af52d95a3fe0d8fdc
Parthiban Nallathambi <parthiban@linumiz.com>
2025-04-04 15:45:59 +0530

    lp-mspm0l2228: remove flash protection
    flash protection support is not added as part of openocd in the
    upstream, which results in flashing failure when using openocd
    
    remove flash control and use configuration from G3507.
    
    Signed-off-by: Parthiban Nallathambi <parthiban@linumiz.com>

--- boards/ti/lp_mspm0l2228/board.cmake
+++ boards/ti/lp_mspm0l2228/board.cmake
@@ -1,13 +1,5 @@
-# SPDX-License-Identifier: Apache-2.0
-
-board_runner_args(openocd "--cmd-pre-load=reset init")
-# unprotect sectors before running
-board_runner_args(openocd "--cmd-pre-load=flash protect 0 0 last off")
-board_runner_args(openocd "--cmd-pre-load=flash erase_sector 0 0 last")
-
-#board_runner_args(openocd "--cmd-reset-halt=flash info 1")
-
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+# # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--device=MSPM0L2228" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
