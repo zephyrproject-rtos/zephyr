9f862b31979f93eb6836bf0f39d0abbfbf26fa47
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2025-04-04 10:03:52 +0100

    boards: nordic: nrf5340dk: Add QSPI nrfutil config
    Adds a configuration file which is used with nrfutil to allow
    programming QSPI on the device
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- boards/nordic/nrf5340dk/board.cmake
+++ boards/nordic/nrf5340dk/board.cmake
@@ -5,6 +5,7 @@ if(CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP_NS)
 endif()
 
 if(CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP OR CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP_NS)
+  board_runner_args(nrfutil "--ext-mem-config-file=${BOARD_DIR}/support/nrf5340dk_qspi_nrfutil_config.json")
   board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
 endif()
 
