5cb9cf3f176a6efeb0b616ddf310603e82c97e98
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2025-04-04 10:09:05 +0100

    boards: nordic: nrf52840dk: Add QSPI nrfutil config
    Adds a configuration file which is used with nrfutil to allow
    programming QSPI on the device
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- boards/nordic/nrf52840dk/board.cmake
+++ boards/nordic/nrf52840dk/board.cmake
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
+if(CONFIG_SOC_NRF52840_QIAA)
+  board_runner_args(nrfutil "--ext-mem-config-file=${BOARD_DIR}/support/nrf52840dk_qspi_nrfutil_config.json")
+endif()
+
 board_runner_args(jlink "--device=nRF52840_xxAA" "--speed=4000")
 board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
 include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
