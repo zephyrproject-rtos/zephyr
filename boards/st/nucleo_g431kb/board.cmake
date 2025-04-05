5d41ff116189bfd06346adce96a5d6787f1481a1
Kuba Gryglak <gryglak.kuba@protonmail.com>
2025-04-05 18:46:24 +0200

    boards: st: add support for JLink in STM32G4 Nucleo board.cmake files
    Add missing support for J-Link in board.cmake configuration
    for Nucleo G4 boards so they can be
    flashed and debugged using J-Link.
    
    Signed-off-by: Kuba Gryglak <gryglak.kuba@protonmail.com>

--- boards/st/nucleo_g431kb/board.cmake
+++ boards/st/nucleo_g431kb/board.cmake
@@ -3,8 +3,10 @@
 # keep first
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(pyocd "--target=stm32g431kbtx")
+board_runner_args(jlink "--device=STM32G431KB" "--speed=4000")
 
 # keep first
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
