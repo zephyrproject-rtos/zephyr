04d9130d4da1f0e6e72d1ec52d9a6a8fda1b9023
Emil Gydesen <emil.gydesen@nordicsemi.no>
2025-04-07 16:20:24 +0200

    tests: Bluetooth: Tester: PBP BSIM test
    Adds BSIM testing of the PBP features of the BT Tester.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bsim/bluetooth/tester/src/test_main.c
+++ tests/bsim/bluetooth/tester/src/test_main.c
@@ -17,6 +17,8 @@ extern struct bst_test_list *test_cap_central_install(struct bst_test_list *test
 extern struct bst_test_list *test_cap_peripheral_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_gap_central_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_gap_peripheral_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_pbp_sink_install(struct bst_test_list *tests);
+extern struct bst_test_list *test_pbp_source_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_vcp_central_install(struct bst_test_list *tests);
 extern struct bst_test_list *test_vcp_peripheral_install(struct bst_test_list *tests);
 
@@ -31,6 +33,8 @@ bst_test_install_t test_installers[] = {
 	test_cap_peripheral_install,
 	test_gap_central_install,
 	test_gap_peripheral_install,
+	test_pbp_sink_install,
+	test_pbp_source_install,
 	test_vcp_central_install,
 	test_vcp_peripheral_install,
 	NULL,
