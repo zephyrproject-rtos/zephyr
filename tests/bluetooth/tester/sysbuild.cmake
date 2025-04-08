ac86261e1d66bc3117ee12c32b751ebadbc046c7
Emil Gydesen <emil.gydesen@nordicsemi.no>
2025-04-08 15:02:30 +0200

    tests: Bluetooth: Tester: Default to use overlay-bt_ll_sw_split.conf
    Default the BT Tester to use the overlay-bt_ll_sw_split.conf for all
    Zephyr based configurations.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bluetooth/tester/sysbuild.cmake
+++ tests/bluetooth/tester/sysbuild.cmake
@@ -13,13 +13,8 @@ if(SB_CONFIG_NET_CORE_IMAGE_HCI_IPC)
 		BOARD       ${SB_CONFIG_NET_CORE_BOARD}
 	)
 
-	set(${NET_APP}_CONF_FILE
-	 ${NET_APP_SRC_DIR}/nrf5340_cpunet_iso-bt_ll_sw_split.conf
-	 CACHE INTERNAL ""
-	)
-
 	set(${NET_APP}_EXTRA_CONF_FILE
-	 ${APP_DIR}/hci_ipc_cpunet.conf
+	 ${APP_DIR}/overlay-bt_ll_sw_split.conf
 	 CACHE INTERNAL ""
 	)
 
