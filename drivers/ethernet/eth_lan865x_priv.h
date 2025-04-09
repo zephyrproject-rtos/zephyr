495dd0d690b311c02835bb14b128f6783fe7bb03
Ramya T <ramya.t@microchip.com>
2025-04-09 23:06:33 +0530

    drivers: ethernet: lan865x: Move OA TC6 specific code into OA TC6 library
    OA TC6 specific code implementation is moved from lan865x driver to
    OA TC6 generic library so that all the 10Base-T1S MAC-PHY drivers can
    use the APIs from OA TC6 library. A new menuconfig option is created
    for selecting/adding the OA TC6 generic library support for the
    MAC-PHY drivers.
    
    Signed-off-by: Ramya T <ramya.t@microchip.com>
    Signed-off-by: Parthiban Veerasooran <parthiban.veerasooran@microchip.com>

--- drivers/ethernet/eth_lan865x_priv.h
+++ drivers/ethernet/eth_lan865x_priv.h
@@ -49,7 +49,6 @@ struct lan865x_config {
 	struct spi_dt_spec spi;
 	struct gpio_dt_spec interrupt;
 	struct gpio_dt_spec reset;
-	int32_t timeout;
 
 	/* MAC */
 	bool tx_cut_through_mode; /* 1 - tx cut through, 0 - Store and forward */
@@ -57,20 +56,10 @@ struct lan865x_config {
 };
 
 struct lan865x_data {
-	struct net_if *iface;
-	struct gpio_callback gpio_int_callback;
-	struct k_sem tx_rx_sem;
-	struct k_sem int_sem;
 	struct oa_tc6 *tc6;
 	uint16_t chip_id;
 	uint8_t silicon_rev;
 	uint8_t mac_address[6];
-	bool iface_initialized;
-	bool reset;
-
-	K_KERNEL_STACK_MEMBER(thread_stack, CONFIG_ETH_LAN865X_IRQ_THREAD_STACK_SIZE);
-	struct k_thread thread;
-	k_tid_t tid_int;
 };
 
 #endif /* ETH_LAN865X_PRIV_H__ */
