f8416de09ea1cf24eaa21862c8c391641f786954
Luis Ubieda <luisf@croxel.com>
2025-04-10 13:07:22 -0400

    sensor: adxl345: fix: Overriding of ODR setting in DTS property
    This patch fixes previous overriding of ODR setting through DTS (it
    would always be 25-Hz, irrespective of what the DTS property said).
    
    While doing so, create dt-binding enum to improve settings clarity.
    
    Signed-off-by: Luis Ubieda <luisf@croxel.com>

---
+++ include/zephyr/dt-bindings/sensor/adxl345.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2025 Croxel Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_ADI_ADX345_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_ADI_ADX345_H_
+
+/**
+ * @defgroup ADXL345 ADI DT Options
+ * @ingroup sensor_interface
+ * @{
+ */
+
+/**
+ * @defgroup ADXL345_ODR Output Rate options
+ * @{
+ */
+#define ADXL345_DT_ODR_12_5		7
+#define ADXL345_DT_ODR_25		8
+#define ADXL345_DT_ODR_50		9
+#define ADXL345_DT_ODR_100		10
+#define ADXL345_DT_ODR_200		11
+#define ADXL345_DT_ODR_400		12
+/** @} */
+
+/** @} */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_ADI_ADX345_H_ */
