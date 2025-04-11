246ae52721dc58333858956349e3f97cf873c245
Dima Nikiforov <vnikiforov@berkeley.edu>
2025-04-10 18:54:27 -0700

    add additional pytorch model examples

--- samples/chipyard/executorch/generate_pte.sh
+++ samples/chipyard/executorch/generate_pte.sh
@@ -2,12 +2,15 @@ script_dir=$(dirname $(realpath $0))
 
 function gen_pte(){
     echo "Generating PTE file for model..."
-    python ${script_dir}/model/gen_pte.py --pte ${script_dir}/model/model.pte
-    # export pte to header file
+    # Forward any additional arguments ($@) to the gen_pte.py script.
+    python ${script_dir}/model/gen_pte.py --pte ${script_dir}/model/model.pte "$@"
+    
+    # Export the generated PTE to a header file.
     python ${script_dir}/model/pte_to_header.py --pte ${script_dir}/model/model.pte --outfile ${script_dir}/executor_runner/model_pte.c
 
     echo "Generated ${script_dir}/model/model.pte"
     echo "Generated ${script_dir}/executor_runner/model_pte.c"
 }
 
-gen_pte
+# Pass all command-line arguments to the function.
+gen_pte "$@"
