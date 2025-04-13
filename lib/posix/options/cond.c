3e1ec61706f22934ca3199a32a23d725b675b28e
Chris Friedt <cfriedt@tenstorrent.com>
2025-04-13 11:20:24 -0400

    posix: implement the POSIX_CLOCK_SELECTION Option Group
    Implement the POSIX_CLOCK_SELECTION Option Group.
    
    This was mostly already done, but compiled / linked in the wrong places.
    
    E.g. pthread_condattr_getclock() and pthread_condattr_setclock() were
    in pthread.c and part of POSIX_THREADS_BASE. clock_nanosleep() was in
    clock.c and part of POSIX_TIMERS.
    
    This change builds them as part of clock_selection.c with
    CONFIG_POSIX_CLOCK_SELECTION.
    
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

--- lib/posix/options/cond.c
+++ lib/posix/options/cond.c
@@ -276,36 +276,4 @@ int pthread_condattr_destroy(pthread_condattr_t *att)
 	return 0;
 }
 
-int pthread_condattr_getclock(const pthread_condattr_t *ZRESTRICT att,
-		clockid_t *ZRESTRICT clock_id)
-{
-	struct posix_condattr *const attr = (struct posix_condattr *)att;
-
-	if ((attr == NULL) || !attr->initialized) {
-		LOG_DBG("%s %s initialized", "attribute", "not");
-		return EINVAL;
-	}
-
-	*clock_id = attr->clock;
-
-	return 0;
-}
-
-int pthread_condattr_setclock(pthread_condattr_t *att, clockid_t clock_id)
-{
-	struct posix_condattr *const attr = (struct posix_condattr *)att;
-
-	if (clock_id != CLOCK_REALTIME && clock_id != CLOCK_MONOTONIC) {
-		return -EINVAL;
-	}
-
-	if ((attr == NULL) || !attr->initialized) {
-		LOG_DBG("%s %s initialized", "attribute", "not");
-		return EINVAL;
-	}
-
-	attr->clock = clock_id;
-
-	return 0;
-}
 SYS_INIT(pthread_cond_pool_init, PRE_KERNEL_1, 0);
