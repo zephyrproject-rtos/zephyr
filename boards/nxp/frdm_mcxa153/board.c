130a60fb668921bf86448ec39a573b89f384ef60
Neil Chen <cheng.chen_1@nxp.com>
2025-04-14 11:09:22 +0800

    boards: nxp: frdm_mcxa153: Support lpcmp for NXP frdm_mcxa153 board
    Support lpcmp for NXP frdm_mcxa153 board.
    
    Signed-off-by: Neil Chen <cheng.chen_1@nxp.com>

--- boards/nxp/frdm_mcxa153/board.c
+++ boards/nxp/frdm_mcxa153/board.c
@@ -97,6 +97,12 @@ void board_early_init_hook(void)
 	CLOCK_EnableClock(kCLOCK_GateADC0);
 #endif
 
+#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(lpcmp0))
+	CLOCK_AttachClk(kFRO12M_to_CMP0);
+	CLOCK_SetClockDiv(kCLOCK_DivCMP0_FUNC, 1U);
+	SPC_EnableActiveModeAnalogModules(SPC0, (kSPC_controlCmp0 | kSPC_controlCmp0Dac));
+#endif
+
 #if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(lpuart0))
 	CLOCK_SetClockDiv(kCLOCK_DivLPUART0, 1u);
 	CLOCK_AttachClk(kFRO12M_to_LPUART0);
