e60070d91b5023ebdad539e21ed8426a47c1c96a
Nicholas Lowell <Nicholas.Lowell@lexmark.com>
2025-04-14 10:15:42 -0400

    posix: net: move static inline gethostname() to lib definition
    static inline gethostname() in unistd.h can cause declaration collisions.
    we should just move it to a normal function definition like the
    rest of the network functions.
    
    Signed-off-by: Nicholas Lowell <Nicholas.Lowell@lexmark.com>

--- lib/posix/options/net.c
+++ lib/posix/options/net.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 
 #include <zephyr/net/net_if.h>
+#include <zephyr/net/socket.h>
 #include <zephyr/posix/arpa/inet.h>
 #include <zephyr/posix/netinet/in.h>
 #include <zephyr/posix/net/if.h>
@@ -214,6 +215,11 @@ int getaddrinfo(const char *host, const char *service, const struct zsock_addrin
 struct hostent *gethostent(void)
 {
 	return NULL;
+}
+
+int gethostname(char *buf, size_t len)
+{
+	return zsock_gethostname(buf, len);
 }
 
 int getnameinfo(const struct sockaddr *addr, socklen_t addrlen, char *host, socklen_t hostlen,
