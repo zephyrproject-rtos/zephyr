zephyrproject-rtos/zephyr

commit b084e0d4cd8b4e0dc6764d02e2fe687a81aff23b

doc: _scripts: boards: fix condition for revision
Revision might be `0` so update the condition to not inadvertently
exclude boards with such revision number.

Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

doc/_scripts/gen_boards_catalog.py
@@ -138,7 +138,7 @@ def gather_board_devicetrees(twister_out_dir):
                 revision = board_info.get('revision', '')
 
                 board_target = board_name
-                if revision:
+                if revision is not None:
                     board_target = f"{board_target}@{revision}"
                 if qualifier:
                     board_target = f"{board_target}/{qualifier}"

commit bb31ccebac6b7fea3db5a1c72cc3579a77bd28cc

doc: _scripts: boards: fix for boards without revisions
Commit b084e0d introduced a pretty bad regression for boards without
revisions. Fix by correctly checking for actually undefined revisions

Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

doc/_scripts/gen_boards_catalog.py
@@ -163,7 +163,7 @@ def gather_board_build_info(twister_out_dir):
                 revision = board_info.get('revision', '')
 
                 board_target = board_name
-                if revision is not None:
+                if revision != '':
                     board_target = f"{board_target}@{revision}"
                 if qualifier:
                     board_target = f"{board_target}/{qualifier}"