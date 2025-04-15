9de3d6bf643455493dad32eda0c496cb97ee221a
Henrik Lindblom <henrik.lindblom@vaisala.com>
2025-04-15 11:20:11 +0300

    soc: stm32: use cache peripheral driver
    Use the Zephyr cache API in soc initialization code instead of calling the
    HAL directly. The change does not modify the pre-existing cache settings,
    just changes the path they are enabled.
    
    Signed-off-by: Henrik Lindblom <henrik.lindblom@vaisala.com>

--- soc/st/stm32/stm32wbax/soc.c
+++ soc/st/stm32/stm32wbax/soc.c
@@ -11,10 +11,10 @@
 
 #include <zephyr/device.h>
 #include <zephyr/init.h>
+#include <zephyr/cache.h>
 #include <stm32_ll_bus.h>
 #include <stm32_ll_pwr.h>
 #include <stm32_ll_rcc.h>
-#include <stm32_ll_icache.h>
 #include <zephyr/arch/cpu.h>
 #include <zephyr/irq.h>
 #include <zephyr/logging/log.h>
@@ -31,9 +31,7 @@ LOG_MODULE_REGISTER(soc);
  */
 void stm32wba_init(void)
 {
-	/* Enable instruction cache in 1-way (direct mapped cache) */
-	LL_ICACHE_SetMode(LL_ICACHE_1WAY);
-	LL_ICACHE_Enable();
+	sys_cache_instr_enable();
 #ifdef CONFIG_STM32_FLASH_PREFETCH
 	__HAL_FLASH_PREFETCH_BUFFER_ENABLE();
 #endif
