a639bfc702b3c672eac31bf39a3f2645b3661d0f
Tavish Naruka <t-naruka@ispace-inc.com>
2025-04-16 18:16:55 +0900

    soc: ti: add TI Hercules TMS570LC43x SoC
    Adds new SoC. The vendor provides a tool (halcogen) to
    generate code including SoC init and errata handling code, which
    is also included here after adapting it to coding style and reducing
    dependencies. As for erratas, the applicable one at this stage is PLL
    startup errata.
    
    We also have CORTEX-R5#7 (ARM ID-780125), but we did not implement it
    due to several reasons:
    1. did not implement it in the past codebase
    2. Halcogen code does not implement it
    3. This is Cortex R5 errata, Xilinx R5 implementation has this note
    https://adaptivesupport.amd.com/s/article/65878
    
    Signed-off-by: Tavish Naruka <t-naruka@ispace-inc.com>

---
+++ soc/ti/hercules/ti_tms570lc43x/soc.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2025 ispace, inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __TMS570_SOC_H__
+#define __TMS570_SOC_H__
+
+/* Do not let CMSIS to handle GIC and Timer */
+#include <stdint.h>
+#define __GIC_PRESENT 0
+
+#endif /* __TMS570_SOC_H__ */
