AmbiqMicro/ambiqzephyr

boards: Add support for the Ambiq Apollo510 EVB board
This commit adds support for the Ambiq Apollo510 Evaluation Board.

Signed-off-by: Hao Luo <hluo@ambiq.com>

boards/ambiq/apollo510_evb/board.cmake
@@ -1,0 +1,6 @@
+# Copyright (c) 2025 Ambiq Micro Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=AMAP54KK-KBR" "--iface=swd" "--speed=1000")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)

zephyrproject-rtos/zephyr

boards/ambiq/apollo510_evb/board.cmake
@@ -1,0 +1,6 @@
+# Copyright (c) 2025 Ambiq Micro Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=AP510NFA-CBR" "--iface=swd" "--speed=1000")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)