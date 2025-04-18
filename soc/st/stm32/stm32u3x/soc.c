ac55ce2bcd2b908631b5c05b5e9a533bb7ca0369
Khaoula Bidani <khaoula.bidani-ext@st.com>
2025-04-18 12:25:56 +0200

    soc: st: stm32: Add series stm32u3
    Add STM32U3 familly support
    
    Signed-off-by: Khaoula Bidani <khaoula.bidani-ext@st.com>

---
+++ soc/st/stm32/stm32u3x/soc.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2025 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief System/hardware module for STM32U3 processor
+ */
+
+#include <zephyr/device.h>
+#include <zephyr/cache.h>
+#include <zephyr/init.h>
+#include <stm32_ll_bus.h>
+#include <stm32_ll_system.h>
+#include <zephyr/logging/log.h>
+
+#include <cmsis_core.h>
+
+#define LOG_LEVEL CONFIG_SOC_LOG_LEVEL
+LOG_MODULE_REGISTER(soc);
+
+/**
+ * @brief Perform basic hardware initialization at boot.
+ *
+ * This needs to be run from the very beginning.
+ */
+void soc_early_init_hook(void)
+{
+	/* Enable ART Accelerator prefetch */
+	sys_cache_instr_enable();
+
+	/* Update CMSIS SystemCoreClock variable (HCLK) */
+	/* The MSIS is used as system clock source after startup from reset,configured at 12 MHz. */
+	SystemCoreClock = 12000000;
+
+	/* Enable power controller bus clock for other drivers */
+	LL_AHB1_GRP2_EnableClock(LL_AHB1_GRP2_PERIPH_PWR);
+}
