d7e6b0ed2d0a9b6cc0a321f7af3e36fdddea395c
Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
2025-04-19 11:00:13 +0200

    tests: drivers: interrupt_controller: add new test for STM32 EXTI
    add exti driver tests for stm32 platform
    
    Signed-off-by: Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>

---
+++ tests/drivers/interrupt_controller/intc_exti_stm32/src/main.c
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2025 Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/ztest.h>
+
+ZTEST_SUITE(intc_exti_stm32, NULL, NULL, NULL, NULL, NULL);
