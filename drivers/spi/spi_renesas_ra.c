463f5181926a74cfbd3be5c5575ea53853f51379
Khoa Nguyen <khoa.nguyen.xh@renesas.com>
2025-04-22 10:15:19 +0700

    drivers: Update dtc transfer info alignment
    Update dtc transfer info alignment for Renesas drivers
    
    Signed-off-by: Khoa Nguyen <khoa.nguyen.xh@renesas.com>

--- drivers/spi/spi_renesas_ra.c
+++ drivers/spi/spi_renesas_ra.c
@@ -41,14 +41,14 @@ struct ra_spi_data {
 	/* RX */
 	struct st_transfer_instance rx_transfer;
 	struct st_dtc_instance_ctrl rx_transfer_ctrl;
-	struct st_transfer_info rx_transfer_info;
+	struct st_transfer_info rx_transfer_info DTC_TRANSFER_INFO_ALIGNMENT;
 	struct st_transfer_cfg rx_transfer_cfg;
 	struct st_dtc_extended_cfg rx_transfer_cfg_extend;
 
 	/* TX */
 	struct st_transfer_instance tx_transfer;
 	struct st_dtc_instance_ctrl tx_transfer_ctrl;
-	struct st_transfer_info tx_transfer_info;
+	struct st_transfer_info tx_transfer_info DTC_TRANSFER_INFO_ALIGNMENT;
 	struct st_transfer_cfg tx_transfer_cfg;
 	struct st_dtc_extended_cfg tx_transfer_cfg_extend;
 #endif
