ccf8d3dd711d1d06d8a03c6674644a9e53054d7c
Tony Han <tony.han@microchip.com>
2025-04-22 16:58:36 +0800

    soc: microchip: sama7g5: sam: update MMU setting for sama7g5 TRNG
    Update TRNG registers with strong ordered, read and write access.
    
    Signed-off-by: Tony Han <tony.han@microchip.com>

--- soc/microchip/sam/sama7g5/soc.c
+++ soc/microchip/sam/sama7g5/soc.c
@@ -35,6 +35,10 @@ static const struct arm_mmu_region mmu_regions[] = {
 
 	MMU_REGION_FLAT_ENTRY("sckc", SCKC_BASE_ADDRESS, 0x4,
 			      MT_STRONGLY_ORDERED | MPERM_R | MPERM_W),
+
+	IF_ENABLED(DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(trng)),
+		   (MMU_REGION_FLAT_ENTRY("trng", TRNG_BASE_ADDRESS, 0x100,
+					  MT_STRONGLY_ORDERED | MPERM_R | MPERM_W),))
 };
 
 const struct arm_mmu_config mmu_config = {
