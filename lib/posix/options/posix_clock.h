e75f285bd14accde523352d6bc5c53310f259303
Chris Friedt <cfriedt@tenstorrent.com>
2025-04-23 08:15:17 -0400

    posix: add timespec_is_valid() private internal function
    Add a common private function timespec_is_valid() that
    can be used to check if a timespec object is valid, and
    use that consistently in lib/posix/options.
    
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

--- lib/posix/options/posix_clock.h
+++ lib/posix/options/posix_clock.h
@@ -8,8 +8,19 @@
 #ifndef ZEPHYR_LIB_POSIX_POSIX_CLOCK_H_
 #define ZEPHYR_LIB_POSIX_POSIX_CLOCK_H_
 
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <time.h>
+
+#include <zephyr/sys_clock.h>
+#include <zephyr/sys/__assert.h>
+
+static inline bool timespec_is_valid(const struct timespec *ts)
+{
+	__ASSERT_NO_MSG(ts != NULL);
+	return (ts->tv_nsec >= 0) && (ts->tv_nsec < NSEC_PER_SEC);
+}
 
 uint32_t timespec_to_clock_timeoutms(clockid_t clock_id, const struct timespec *abstime);
 uint32_t timespec_to_timeoutms(const struct timespec *abstime);
