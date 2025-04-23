9a739e6301a725fef785e4e4402bb5c570ec986a
Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>
2025-04-23 12:18:08 +0300

    tests: bluetooth: mesh: test lpn communication after friendship
    Commit extends the communication test between the LPN and
    the third mesh device by checking that LPN and the third mesh
    device can exchange data normally after friendship termination.
    
    Signed-off-by: Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>

--- tests/bsim/bluetooth/mesh/tests_scripts/friendship/msg_mesh.sh
+++ tests/bsim/bluetooth/mesh/tests_scripts/friendship/msg_mesh.sh
@@ -4,8 +4,24 @@
 
 source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
 
-# Test communication between the LPN and a third mesh device
+# Test communication between the LPN and a third mesh device.
+# Test scenario:
+# 1. LPN and Friend establish friendship.
+# 2. LPN sends unsegmented data to the third mesh device and receives unsegmented data back.
+# 3. LPN sends segmented data to the third mesh device and receives segmented data back.
+# 4. LPN publishes unsegmented data to the third device with friendship credentials.
+#    Friend should relay data to the third device with network credentials.
+#    The third mesh receives published data.
+# 5. LPN terminates friendship  and gets an event about termination.
+# 6. LPN sends unsegmented data to the third device outside of friendship and
+#    receives unsegmented data back.
 RunTest mesh_friendship_msg_mesh \
+	friendship_lpn_msg_mesh \
+	friendship_other_msg \
+	friendship_friend_est
+
+overlay=overlay_lpn_scan_on_conf
+RunTest mesh_friendship_msg_mesh_lpn_scan_on \
 	friendship_lpn_msg_mesh \
 	friendship_other_msg \
 	friendship_friend_est
