5086beebde9a8c6958fa59ac879bc1062966c44f
Alain Volmat <alain.volmat@foss.st.com>
2025-04-24 14:05:19 +0200

    drivers: video: introduction of the stm32 DCMIPP driver
    The STM32 Digital Camera Memory Interface Pixel Processor (DCMIPP)
    is a multi-pipeline camera interface allowing to capture
    and process frames from parallel or CSI interfaces depending on its
    version.
    Current commit introduces the support of the 3 pipes version with
    CSI input.
    
    Signed-off-by: Alain Volmat <alain.volmat@foss.st.com>

---
+++ drivers/video/video_stm32_dcmipp.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025 STMicroelectronics.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_VIDEO_DCMIPP_H_
+#define ZEPHYR_INCLUDE_VIDEO_DCMIPP_H_
+
+#include <zephyr/drivers/video-controls.h>
+
+#define VIDEO_CID_STM32_DCMIPP_ISP_HDEC	(VIDEO_CID_PRIVATE_BASE + 0)
+#define VIDEO_CID_STM32_DCMIPP_ISP_VDEC	(VIDEO_CID_PRIVATE_BASE + 1)
+
+#endif /* ZEPHYR_INCLUDE_VIDEO_DCMIPP_H_ */
