4bd2df85f2fe25614a0a1854455afa4923e12405
Bjarki Arge Andreasen <bjarki.andreasen@nordicsemi.no>
2025-04-25 13:07:23 +0200

    drivers: input: rm pm sys mgmt build assert
    Remove check for PM_DEVICE_RUNTIME being enabled if
    PM_DEVICE_SYSTEM_MANAGED is enabled as the latter option will be
    removed, hence this case is not possible.
    
    Signed-off-by: Bjarki Arge Andreasen <bjarki.andreasen@nordicsemi.no>

--- drivers/input/input_realtek_rts5912_kbd.c
+++ drivers/input/input_realtek_rts5912_kbd.c
@@ -210,9 +210,6 @@ PM_DEVICE_DT_INST_DEFINE(0, input_kbd_matrix_pm_action);
 DEVICE_DT_INST_DEFINE(0, &rts5912_kbd_init, PM_DEVICE_DT_INST_GET(0), &rts5912_kbd_data_0,
 		      &rts5912_kbd_cfg_0, POST_KERNEL, CONFIG_INPUT_INIT_PRIORITY, NULL);
 
-BUILD_ASSERT(!IS_ENABLED(CONFIG_PM_DEVICE_SYSTEM_MANAGED) || IS_ENABLED(CONFIG_PM_DEVICE_RUNTIME),
-	     "CONFIG_PM_DEVICE_RUNTIME must be enabled when using CONFIG_PM_DEVICE_SYSTEM_MANAGED");
-
 BUILD_ASSERT(DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) == 1,
 	     "only one realtek,rts5912-kbd compatible node can be supported");
 BUILD_ASSERT(IN_RANGE(DT_INST_PROP(0, row_size), 1, 9), "invalid row-size");
