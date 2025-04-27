feca886d090b2b343eb4ae3dcdacece577e69d6e
Tony Han <tony.han@microchip.com>
2025-04-27 10:04:28 +0800

    drivers: mfd: add FLEXCOM drivers for Microchip SAM devices
    The FLEXCOM offers several serial communication protocols that are
    managed by the three sub-modules USART, SPI, and TWI (I2C).
    
    Signed-off-by: Tony Han <tony.han@microchip.com>

---
+++ include/zephyr/dt-bindings/mfd/mfd_mchp_sam_flexcom.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_MFD_MCHP_SAM_FLEXCOMM_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_MFD_MCHP_SAM_FLEXCOMM_H_
+
+/**
+ * @brief FLEXCOM Operating Mode.
+ */
+#define SAM_FLEXCOM_MODE_NO_COM 0
+#define SAM_FLEXCOM_MODE_USART  1
+#define SAM_FLEXCOM_MODE_SPI    2
+#define SAM_FLEXCOM_MODE_TWI    3
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_MFD_MCHP_SAM_FLEXCOMM_H_ */
