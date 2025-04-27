4cc288a2c568bfcc7abbf09e0b00c1f044daf0eb
Parthiban Nallathambi <parthiban@linumiz.com>
2025-04-27 18:47:18 +0530

    drivers: i2c: init suspended when PM_DEVICE enabled
    disable the controller when PM_DEVICE is not enabled, this
    will help disable the device in both PM_DEVICE enabled case
    and also when it's not enabled.
    
    Signed-off-by: Parthiban Nallathambi <parthiban@linumiz.com>

--- drivers/i2c/i2c_mspm0.c
+++ drivers/i2c/i2c_mspm0.c
@@ -811,9 +811,11 @@ static int i2c_mspm0_init(const struct device *dev)
 		DL_I2C_disableController(config->base);
 	}
 
-	if (!IS_ENABLED(CONFIG_PM_DEVICE)) {
-		DL_I2C_enableController(config->base);
-	}
+#ifdef CONFIG_PM_DEVICE
+	pm_device_init_suspended(dev);
+#else
+	DL_I2C_enableController(config->base);
+#endif
 
 #ifdef CONFIG_PM_DEVICE_RUNTIME
 	(void)pm_device_runtime_enable(dev);
