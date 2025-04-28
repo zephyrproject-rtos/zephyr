36d0ba6b43593abe646801215e1269f3937d2731
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2025-04-28 10:46:53 +0100

    boards: nordic: nrf5340_audio_dk: Replace SYS_INIT with board hooks
    Updates to use board hooks since SYS_INIT should not be used
    in boards
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- boards/nordic/nrf5340_audio_dk/nrf5340_audio_dk_config.c
+++ boards/nordic/nrf5340_audio_dk/nrf5340_audio_dk_config.c
@@ -11,7 +11,7 @@
 
 LOG_MODULE_REGISTER(nrf5340_audio_dk_nrf5340_cpuapp, CONFIG_LOG_DEFAULT_LEVEL);
 
-static int core_config(void)
+void board_late_init_hook(void)
 {
 	nrf_gpiote_latency_t latency;
 
@@ -21,8 +21,4 @@ static int core_config(void)
 		LOG_DBG("Setting gpiote latency to low power");
 		nrf_gpiote_latency_set(NRF_GPIOTE, NRF_GPIOTE_LATENCY_LOWPOWER);
 	}
-
-	return 0;
 }
-
-SYS_INIT(core_config, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEVICE);
