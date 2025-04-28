46331561800305a62e18dcc4c56a3152c1a59363
Sergei Ovchinnikov <sergei.ovchinnikov@nordicsemi.no>
2025-04-28 09:11:30 +0200

    drivers: npm13xx: rename npm1300 to npm13xx
    Rename npm1300 to npm13xx in function names, documentation, etc. where
    applicable for all the npm13xx drivers
    
    Signed-off-by: Sergei Ovchinnikov <sergei.ovchinnikov@nordicsemi.no>

--- include/zephyr/dt-bindings/regulator/npm13xx.h
+++ include/zephyr/dt-bindings/regulator/npm13xx.h
@@ -8,31 +8,31 @@
 #define ZEPHYR_INCLUDE_DT_BINDINGS_REGULATOR_NPM13XX_H_
 
 /**
- * @defgroup regulator_npm1300 NPM1300 Devicetree helpers.
+ * @defgroup regulator_npm13xx nPM13xx Devicetree helpers.
  * @ingroup regulator_interface
  * @{
  */
 
 /**
- * @name NPM1300 Regulator modes
+ * @name nPM13xx Regulator modes
  * @{
  */
 /* Buck modes */
-#define NPM1300_BUCK_MODE_AUTO 0x00U
-#define NPM1300_BUCK_MODE_PWM  0x01U
-#define NPM1300_BUCK_MODE_PFM  0x04U
+#define NPM13XX_BUCK_MODE_AUTO 0x00U
+#define NPM13XX_BUCK_MODE_PWM  0x01U
+#define NPM13XX_BUCK_MODE_PFM  0x04U
 
 /* LDSW / LDO modes */
-#define NPM1300_LDSW_MODE_LDO  0x02U
-#define NPM1300_LDSW_MODE_LDSW 0x03U
+#define NPM13XX_LDSW_MODE_LDO  0x02U
+#define NPM13XX_LDSW_MODE_LDSW 0x03U
 
 /* GPIO control configuration */
-#define NPM1300_GPIO_CHAN_NONE 0x00U
-#define NPM1300_GPIO_CHAN_0    0x01U
-#define NPM1300_GPIO_CHAN_1    0x02U
-#define NPM1300_GPIO_CHAN_2    0x03U
-#define NPM1300_GPIO_CHAN_3    0x04U
-#define NPM1300_GPIO_CHAN_4    0x05U
+#define NPM13XX_GPIO_CHAN_NONE 0x00U
+#define NPM13XX_GPIO_CHAN_0    0x01U
+#define NPM13XX_GPIO_CHAN_1    0x02U
+#define NPM13XX_GPIO_CHAN_2    0x03U
+#define NPM13XX_GPIO_CHAN_3    0x04U
+#define NPM13XX_GPIO_CHAN_4    0x05U
 
 /** @} */
 
