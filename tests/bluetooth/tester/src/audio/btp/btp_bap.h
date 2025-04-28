4080489d0671dcdf70a48e40c52e6fb78ed7d600
Emil Gydesen <emil.gydesen@nordicsemi.no>
2025-04-28 17:16:02 +0200

    tests: Bluetooth: Tester: Fix synced typos
    Fixed typos syned -> synced and syced -> synced.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bluetooth/tester/src/audio/btp/btp_bap.h
+++ tests/bluetooth/tester/src/audio/btp/btp_bap.h
@@ -253,7 +253,7 @@ struct btp_bap_bis_found_ev {
 } __packed;
 
 #define BTP_BAP_EV_BIS_SYNCED			0x86
-struct btp_bap_bis_syned_ev {
+struct btp_bap_bis_synced_ev {
 	bt_addr_le_t address;
 	uint8_t broadcast_id[BT_AUDIO_BROADCAST_ID_SIZE];
 	uint8_t bis_id;
