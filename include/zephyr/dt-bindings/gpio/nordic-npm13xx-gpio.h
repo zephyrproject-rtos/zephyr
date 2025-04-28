89b838363381c8c6517c2dd98774cf86db22be3a
Sergei Ovchinnikov <sergei.ovchinnikov@nordicsemi.no>
2025-04-28 09:11:30 +0200

    drivers: npm13xx: rename npm1300 to npm13xx
    Rename npm1300 to npm13xx in function names, documentation, etc. where
    applicable for all the npm13xx drivers
    
    Signed-off-by: Sergei Ovchinnikov <sergei.ovchinnikov@nordicsemi.no>

--- include/zephyr/dt-bindings/gpio/nordic-npm13xx-gpio.h
+++ include/zephyr/dt-bindings/gpio/nordic-npm13xx-gpio.h
@@ -7,8 +7,8 @@
 #define ZEPHYR_INCLUDE_DT_BINDINGS_GPIO_NORDIC_NPM13XX_GPIO_H_
 
 /**
- * @brief nPM1300-specific GPIO Flags
- * @defgroup gpio_interface_npm1300 nPM1300-specific GPIO Flags
+ * @brief nPM13xx-specific GPIO Flags
+ * @defgroup gpio_interface_npm13xx nPM13xx-specific GPIO Flags
  *
  * The drive flags are encoded in the 8 upper bits of @ref gpio_dt_flags_t as
  * follows:
@@ -23,74 +23,74 @@
  */
 
 /**
- * @name nPM1300 GPIO drive strength flags
- * @brief nPM1300 GPIO drive strength flags
+ * @name nPM13xx GPIO drive strength flags
+ * @brief nPM13xx GPIO drive strength flags
  * @{
  */
 
 /** @cond INTERNAL_HIDDEN */
 /** Drive mode field mask */
-#define NPM1300_GPIO_DRIVE_MSK 0x0100U
+#define NPM13XX_GPIO_DRIVE_MSK 0x0100U
 /** @endcond */
 
 /** 1mA drive */
-#define NPM1300_GPIO_DRIVE_1MA (0U << 8U)
+#define NPM13XX_GPIO_DRIVE_1MA (0U << 8U)
 /** 6mA drive */
-#define NPM1300_GPIO_DRIVE_6MA (1U << 8U)
+#define NPM13XX_GPIO_DRIVE_6MA (1U << 8U)
 
 /** @} */
 
 /**
- * @name nPM1300 GPIO debounce flags
- * @brief nPM1300 GPIO debounce flags
+ * @name nPM13xx GPIO debounce flags
+ * @brief nPM13xx GPIO debounce flags
  * @{
  */
 
 /** @cond INTERNAL_HIDDEN */
 /** Debounce field mask */
-#define NPM1300_GPIO_DEBOUNCE_MSK 0x0200U
+#define NPM13XX_GPIO_DEBOUNCE_MSK 0x0200U
 /** @endcond */
 
 /** Normal drive */
-#define NPM1300_GPIO_DEBOUNCE_OFF (0U << 9U)
+#define NPM13XX_GPIO_DEBOUNCE_OFF (0U << 9U)
 /** High drive */
-#define NPM1300_GPIO_DEBOUNCE_ON  (1U << 9U)
+#define NPM13XX_GPIO_DEBOUNCE_ON  (1U << 9U)
 
 /** @} */
 
 /**
- * @name nPM1300 GPIO watchdog reset flags
- * @brief nPM1300 GPIO watchdog reset flags
+ * @name nPM13xx GPIO watchdog reset flags
+ * @brief nPM13xx GPIO watchdog reset flags
  * @{
  */
 
 /** @cond INTERNAL_HIDDEN */
 /** watchdog reset field mask */
-#define NPM1300_GPIO_WDT_RESET_MSK 0x0400U
+#define NPM13XX_GPIO_WDT_RESET_MSK 0x0400U
 /** @endcond */
 
 /** Off */
-#define NPM1300_GPIO_WDT_RESET_OFF (0U << 10U)
+#define NPM13XX_GPIO_WDT_RESET_OFF (0U << 10U)
 /** On */
-#define NPM1300_GPIO_WDT_RESET_ON  (1U << 10U)
+#define NPM13XX_GPIO_WDT_RESET_ON  (1U << 10U)
 
 /** @} */
 
 /**
- * @name nPM1300 GPIO power loss warning flags
- * @brief nPM1300 GPIO power loss warning flags
+ * @name nPM13xx GPIO power loss warning flags
+ * @brief nPM13xx GPIO power loss warning flags
  * @{
  */
 
 /** @cond INTERNAL_HIDDEN */
 /** power loss warning field mask */
-#define NPM1300_GPIO_PWRLOSSWARN_MSK 0x0800U
+#define NPM13XX_GPIO_PWRLOSSWARN_MSK 0x0800U
 /** @endcond */
 
 /** Off */
-#define NPM1300_GPIO_PWRLOSSWARN_OFF (0U << 11U)
+#define NPM13XX_GPIO_PWRLOSSWARN_OFF (0U << 11U)
 /** On */
-#define NPM1300_GPIO_PWRLOSSWARN_ON (1U << 11U)
+#define NPM13XX_GPIO_PWRLOSSWARN_ON (1U << 11U)
 
 /** @} */
 
