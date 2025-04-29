b1114ad021e595d3f51d1a67389719dd8db1bb63
Parthiban Nallathambi <parthiban@linumiz.com>
2025-04-29 17:24:00 +0530

    samples: ti/mspm0: add custom power policy in button sample
    add custom power policy in button to control not to put the SoC
    in low power mode in init of the application.
    
    Signed-off-by: Parthiban Nallathambi <parthiban@linumiz.com>

--- samples/boards/ti/mspm0/button/src/main.c
+++ samples/boards/ti/mspm0/button/src/main.c
@@ -35,6 +35,7 @@ void button_pressed(const struct device *dev, struct gpio_callback *cb,
 	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
 }
 
+static bool main_init = false;
 int main(void)
 {
 	int ret;
@@ -65,6 +66,8 @@ int main(void)
 	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
 
 	printk("Press the button\n");
+	main_init = true;
+
 	k_sleep(K_FOREVER);
 
 	return 0;
@@ -83,3 +86,57 @@ static int ti_mspm0l2xxx_bor_init(void)
 }
 
 SYS_INIT(ti_mspm0l2xxx_bor_init, APPLICATION, 0);
+
+#include <zephyr/pm/policy.h>
+#include <zephyr/sys_clock.h>
+#include <zephyr/pm/device.h>
+
+extern int32_t max_latency_cyc;
+
+const struct pm_state_info *pm_policy_next_state(uint8_t cpu, int32_t ticks)
+{
+	int64_t cyc = -1;
+	uint8_t num_cpu_states;
+	const struct pm_state_info *cpu_states;
+
+	if (main_init == false)
+		return NULL;
+
+#ifdef CONFIG_PM_NEED_ALL_DEVICES_IDLE
+	if (pm_device_is_any_busy()) {
+		return NULL;
+	}
+#endif
+
+	if (ticks != K_TICKS_FOREVER) {
+		cyc = k_ticks_to_cyc_ceil32(ticks);
+	}
+
+	num_cpu_states = pm_state_cpu_get_all(cpu, &cpu_states);
+
+	for (int16_t i = (int16_t)num_cpu_states - 1; i >= 0; i--) {
+		const struct pm_state_info *state = &cpu_states[i];
+		uint32_t min_residency_cyc, exit_latency_cyc;
+
+		/* check if there is a lock on state + substate */
+		if (pm_policy_state_lock_is_active(state->state, state->substate_id)) {
+			continue;
+		}
+
+		min_residency_cyc = k_us_to_cyc_ceil32(state->min_residency_us);
+		exit_latency_cyc = k_us_to_cyc_ceil32(state->exit_latency_us);
+
+		/* skip state if it brings too much latency */
+		if ((max_latency_cyc >= 0) &&
+		    (exit_latency_cyc >= max_latency_cyc)) {
+			continue;
+		}
+
+		if ((cyc < 0) ||
+		    (cyc >= (min_residency_cyc + exit_latency_cyc))) {
+			return state;
+		}
+	}
+
+	return NULL;
+}
