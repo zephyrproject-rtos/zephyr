9581b88ac5122eb5af2f32991a2078a8e69522f8
Emil Gydesen <emil.gydesen@nordicsemi.no>
2025-04-30 18:01:45 +0200

    tests: Bluetooth: Split CAP and GMAP AC tests into separate scripts
    This commit splits the CAP and GMAP audio configuration (AC)
    tests into separate scripts. The purpose of this is to reduce
    the runtime of the individual scripts (some, like the cap_ac_11,
    tests takes a long time to run all combinations).
    This split also improves support for running them in parallel.
    
    An additional, positive, side effect of this is that the logs will
    also be smaller per run.
    
    The new scripts can be run directly, or via the e.g. _cap_ac_X.sh
    scripts that run entire audio configuration for all presets.
    
    The design of how the parameters are passed around, are based
    on variable instead of function arguments. Effectively they can
    be used for the same thing, but variables have the advantage
    that it is easier to deal with longer names,
    and that any of the test scripts can be called with additional
    arguments that can, when using variables, easily be forwarded
    to the call to Execute for the PHY in the
    _ac_common.sh.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

---
+++ tests/bsim/bluetooth/audio/test_scripts/audio_config_tests/_ac_common.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+# Copyright (c) 2025 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
+
+VERBOSITY_LEVEL=2
+EXECUTE_TIMEOUT=60
+BSIM_EXE=./bs_${BOARD_TS}_tests_bsim_bluetooth_audio_prj_conf
+
+cd ${BSIM_OUT_PATH}/bin
+
+function Execute_ac() {
+    if [[ -n $ac_tx_preset ]]; then
+        ac_tx_preset_arg="${ac_tx_preset_arg_name} ${ac_tx_preset}"
+    else
+        ac_tx_preset_arg=""
+    fi
+
+    if [[ -n $ac_rx_preset ]]; then
+        ac_rx_preset_arg="source_preset ${ac_rx_preset}"
+    else
+        ac_rx_preset_arg=""
+    fi
+
+
+    sim_id="${ac_profile}_ac_${ac_config}_${ac_tx_preset}_${ac_rx_preset}"
+
+    printf "\n\n======== Running %s AC_%s with %s %s =========\n\n" \
+        "${ac_profile}" "${ac_config}" "${ac_tx_preset_arg}" "${ac_rx_preset_arg}"
+
+    let ac_device_count=1+${ac_acc_cnt}
+    ac_ini_name=${ac_ini_name}_${ac_config}
+
+    Execute ${BSIM_EXE} -v=${VERBOSITY_LEVEL} -s=${sim_id} -d=0 -testid=${ac_ini_name} \
+        -RealEncryption=1 -rs=23 -D=${ac_device_count} \
+        -argstest ${ac_tx_preset_arg} ${ac_rx_preset_arg}
+
+    for i in $(seq 1 $ac_acc_cnt); do
+        let rs=${i}*7 # ensure unique random seed value per acceptor
+
+        Execute ${BSIM_EXE} -v=${VERBOSITY_LEVEL} -s=${sim_id} -d=${i} -testid=${ac_acc_name} \
+            -RealEncryption=1 -rs=${rs} -D=${ac_device_count}
+    done
+
+    Execute ./bs_2G4_phy_v1 -v=${VERBOSITY_LEVEL} -s=${sim_id} -D=${ac_device_count} \
+        -sim_length=60e6 $@
+
+    wait_for_background_jobs
+}
+
+function Execute_cap_unicast_ac() {
+    ac_profile="CAP" ac_ini_name=cap_initiator_ac ac_acc_name=cap_acceptor_unicast \
+        ac_tx_preset_arg_name=sink_preset Execute_ac $@
+}
+
+function Execute_cap_broadcast_ac() {
+    ac_profile="CAP" ac_ini_name=cap_initiator_ac ac_acc_name=cap_acceptor_broadcast \
+        ac_tx_preset_arg_name=preset Execute_ac $@
+}
+
+function Execute_gmap_unicast_ac() {
+    ac_profile="GMAP" ac_ini_name=gmap_ugg_ac ac_acc_name=gmap_ugt \
+        ac_tx_preset_arg_name=sink_preset Execute_ac $@
+}
+
+function Execute_gmap_broadcast_ac() {
+    ac_profile="GMAP" ac_ini_name=gmap_ugg_ac ac_acc_name=cap_acceptor_broadcast \
+        ac_tx_preset_arg_name=broadcast_preset Execute_ac $@
+}
---
+++ tests/bsim/bluetooth/audio/test_scripts/audio_config_tests/cap_ac_7_i_16_1_1.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Copyright (c) 2025 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "$0")/_ac_common.sh
+
+ac_config=7_i ac_tx_preset=16_1_1 ac_rx_preset=16_1_1 ac_acc_cnt=1 Execute_cap_unicast_ac $@
