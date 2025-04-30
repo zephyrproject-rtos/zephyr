60e9f9958297fcf9eb55b30b62af247738500ab3
Emil Gydesen <emil.gydesen@nordicsemi.no>
2025-04-30 18:01:45 +0200

    tests: Bluetooth: Split CAP and GMAP AC tests into separate scripts
    This commit splits the CAP and GMAP audio configuration (AC)
    tests into separate scripts. The purpose of this is to reduce
    the runtime of the individual scripts (some, like the cap_ac_11,
    tests takes a long time to run all combinations).
    This split also improves support for running them in parallel.
    
    An additional, positive, side effect of this is that the logs will
    also be smaller per run.
    
    The new scripts can be run directly, or via the e.g. _cap_ac_X.sh
    scripts that run entire audio configuration for all presets.
    
    The design of how the parameters are passed around, are based
    on variable instead of function arguments. Effectively they can
    be used for the same thing, but variables have the advantage
    that it is easier to deal with longer names,
    and that any of the test scripts can be called with additional
    arguments that can, when using variables, easily be forwarded
    to the call to Execute for the PHY in the
    _ac_common.sh.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

---
+++ tests/bsim/bluetooth/audio/test_scripts/audio_config_tests/gmap_ac_8_ii_48_1_gr_48_1_gs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Copyright (c) 2025 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "$0")/_ac_common.sh
+
+ac_config=8_ii ac_tx_preset=48_1_gr ac_rx_preset=48_1_gs ac_acc_cnt=2 Execute_gmap_unicast_ac $@
