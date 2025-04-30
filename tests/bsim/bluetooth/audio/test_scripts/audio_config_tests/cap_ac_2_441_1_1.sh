f108c289d5d45441fa6d180b2ad04e1ffed092c3
Emil Gydesen <emil.gydesen@nordicsemi.no>
2025-04-30 18:01:45 +0200

    tests: Bluetooth: Split CAP and GMAP AC tests into separate scripts
    This commit splits the CAP and GMAP audio configuration (AC)
    tests into separate scripts. The purpose of this is to reduce
    the runtime of the individual scripts (some, like the cap_ac_11,
    tests takes a long time to run all combinations).
    This split also improves support for running them in parallel.
    
    An additional, positive, side effect of this is that the logs will
    also be smaller per run.
    
    The new scripts can be run directly, or via the e.g. _cap_ac_X.sh
    scripts that run entire audio configuration for all presets.
    
    The design of how the parameters are passed around, are based
    on variable instead of function arguments. Effectively they can
    be used for the same thing, but variables have the advantage
    that it is easier to deal with longer names,
    and that any of the test scripts can be called with additional
    arguments that can, when using variables, easily be forwarded
    to the call to Execute for the PHY in the
    _ac_common.sh.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

---
+++ tests/bsim/bluetooth/audio/test_scripts/audio_config_tests/cap_ac_2_441_1_1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Copyright (c) 2025 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "$0")/_ac_common.sh
+
+# bap_stream_rx.c:66): ISO receive lost
+# https://github.com/zephyrproject-rtos/zephyr/issues/84303
+# ac_config=2 ac_rx_preset=441_1_1 ac_acc_cnt=1 Execute_cap_unicast_ac $@
---
+++ tests/bsim/bluetooth/audio/test_scripts/audio_config_tests/cap_ac_8_i_441_1_1.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Copyright (c) 2025 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "$0")/_ac_common.sh
+
+# ASSERTION FAIL [err == ((isoal_status_t) 0x00)] @
+# zephyr/subsys/bluetooth/controller/hci/hci_driver.c:489
+# https://github.com/zephyrproject-rtos/zephyr/issues/83586
+# ac_config=8_i ac_tx_preset=441_1_1 ac_rx_preset=441_1_1 ac_acc_cnt=1 Execute_cap_unicast_ac $@
