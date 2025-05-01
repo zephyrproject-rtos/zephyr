37e28604f355d36f2ab1a8044ef92226dd303e30
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2025-05-01 11:49:29 +0200

    cpuconf: Boot the radiocore from the app in soc_late_init_hook
    Boot the radiocore from the app in soc_late_init_hook.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

--- soc/nordic/nrf54h/soc.c
+++ soc/nordic/nrf54h/soc.c
@@ -21,6 +21,7 @@
 #include <hal/nrf_nfct.h>
 #include <soc/nrfx_coredep.h>
 #include <soc_lrcconf.h>
+#include <zephyr/drivers/firmware/nrf_ironside/cpuconf.h>
 
 LOG_MODULE_REGISTER(soc, CONFIG_SOC_LOG_LEVEL);
 
@@ -41,6 +42,8 @@ sys_snode_t soc_node;
 				      ADDRESS_SECURITY_Msk |                   \
 				      ADDRESS_DOMAIN_Msk |                     \
 				      ADDRESS_BUS_Msk)))
+
+#define DT_NODELABEL_CPURAD_SLOT0_PARTITION DT_NODELABEL(cpurad_slot0_partition)
 
 static void power_domain_init(void)
 {
@@ -149,6 +152,30 @@ void soc_early_init_hook(void)
 		nrf_nfct_pad_config_enable_set(NRF_NFCT, false);
 	}
 }
+
+#if defined(CONFIG_SOC_NRF54H20_CPURAD_ENABLE)
+void soc_late_init_hook(void)
+{
+	int err;
+
+	/* The msg will be used for communication prior to IPC
+	 * communication being set up. But at this moment no such
+	 * communication is required.
+	 */
+	uint8_t *msg = NULL;
+	size_t msg_size = 0;
+
+	void *radiocore_address =
+		(void *)(DT_REG_ADDR(DT_GPARENT(DT_NODELABEL_CPURAD_SLOT0_PARTITION)) +
+				 DT_REG_ADDR(DT_NODELABEL_CPURAD_SLOT0_PARTITION));
+
+	/* Don't wait as this is not yet supported. */
+	bool cpu_wait = false;
+
+	err = ironside_cpuconf(NRF_PROCESSOR_RADIOCORE, radiocore_address, cpu_wait, msg, msg_size);
+	__ASSERT(err == 0, "err was %d", err);
+}
+#endif
 
 void arch_busy_wait(uint32_t time_us)
 {
