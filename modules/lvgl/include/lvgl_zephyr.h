0be94452a9c244bcd14c246547c79472fcaf5e94
Abderrahmane JARMOUNI <git@jarmouni.me>
2025-05-02 23:54:53 +0200

    modules: lvgl: add multi-display support in Zephyr
    Add multi-display support in Zephyr ontop of LVGL which already supports
    it.
    
    This change allows for creating buffers and structures for each display
    statically and automatically from deviceTree info, and call LV init
    routines for each display
    
    Signed-off-by: Abderrahmane JARMOUNI <git@jarmouni.me>

--- modules/lvgl/include/lvgl_zephyr.h
+++ modules/lvgl/include/lvgl_zephyr.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2024 Fabian Blatz <fabianblatz@gmail.com>
+ * Copyright (c) 2025 Abderrahmane JARMOUNI
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -8,6 +9,14 @@
 #define ZEPHYR_MODULES_LVGL_ZEPHYR_H_
 
 #include <zephyr/kernel.h>
+
+#if DT_ZEPHYR_DISPLAYS_COUNT == 0
+#error Could not find "zephyr,display" chosen property, or "zephyr,displays" compatible node in DT
+#endif /* DT_ZEPHYR_DISPLAYS_COUNT == 0 */
+
+#define LV_DISPLAY_IDX_MACRO(i, _) _##i
+
+#define LV_DISPLAYS_IDX_LIST LISTIFY(DT_ZEPHYR_DISPLAYS_COUNT, LV_DISPLAY_IDX_MACRO, (,))
 
 #ifdef __cplusplus
 extern "C" {
