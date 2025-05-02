eef8d78e4f3aa4ca75b95b33d17cce85e88e72d2
Lothar Felten <lothar.felten@gmail.com>
2025-05-02 13:47:12 +0200

    boards: lilygo: ttgo_t3stm32: initial support
    adds initial support for Lilygo TTGO T3-STM32 board
    
    Signed-off-by: Lothar Felten <lothar.felten@gmail.com>

---
+++ boards/lilygo/ttgo_t3stm32/board.cmake
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(stm32flash "--baud-rate=115200" "--start-addr=0x08000000" "--device=/dev/ttyACM0")
+
+include(${ZEPHYR_BASE}/boards/common/stm32flash.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
