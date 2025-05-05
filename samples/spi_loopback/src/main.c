3664b597aff780b3fb87e53e546a587d95fd8ff7
shrimahaalakshmi <mahaalakshmi@mindgrovetech.in>
2025-05-05 11:49:59 +0530

    Working code for MPW sdk

--- samples/spi_loopback/src/main.c
+++ samples/spi_loopback/src/main.c
@@ -14,6 +14,7 @@ int main()
     const struct device *const dev = DEVICE_DT_GET(DT_NODELABEL(spi1));
     struct spi_config config;
     config.operation = SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8);
+    k_busy_wait(100);
     // uint8_t tx_buff[3] = {0x12, 0x34, 0x56};
     // uint16_t tx_buff[3] = {0x1234, 0xabcd, 0x5678};
     //uint32_t tx_buff[3] = {0x12345678, 0xabcdefaa, 0xafcefbca};
@@ -21,22 +22,28 @@ int main()
     // uint8_t rx_buff[3];
     // uint16_t rx_buff[3];
     //uint32_t rx_buff[3];
-    uint8_t tx_buff[12000];
-    uint8_t rx_buff[12000];
+    uint8_t tx_buff[4];
+    uint8_t rx_buff[4];
 	int len = sizeof(tx_buff) / sizeof(tx_buff[0]);
-    for(int i = 0;i<1200;i++){
+
+    for(int i = 0;i<4;i++){
         tx_buff[i] = i;
+        printf("\nTransmitted Data [%d] = %d", i, tx_buff[i]);
     }
-    printf("len :%d\n", len);
+    printf("\nlen :%d", len);
 
     struct spi_buf tx_buf = { .buf = tx_buff, .len = len };
 	struct spi_buf_set tx_bufs = { .buffers = &tx_buf, .count = 1};
 
     struct spi_buf rx_buf = { .buf = rx_buff, .len = len};
     struct spi_buf_set rx_bufs = { .buffers = &rx_buf, .count = 1};
-    
+
     int ret = spi_transceive(dev, &config, &tx_bufs, &rx_bufs);
-    
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("\nReceived Data [%d] = %d", i, rx_buff[i]);
+    }
     printf("completed transmission\n");
 
 }
