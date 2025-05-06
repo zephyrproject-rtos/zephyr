b2471cf84874e39c056ed1fdd2d830b8112acf77
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2025-05-06 09:37:37 +0200

    tests: boards: Fix zassert string
    Fix wrong parameter used in zassert macros.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- tests/boards/intel_adsp/cache/src/main.c
+++ tests/boards/intel_adsp/cache/src/main.c
@@ -19,46 +19,46 @@ ZTEST(adsp_cache, test_adsp_cache_flush_inv_all)
 	*uncached = 40;
 
 	/* Just some sanity checks */
-	zassert_equal(*cached, 42, NULL);
-	zassert_equal(*uncached, 40, NULL);
+	zassert_equal(*cached, 42);
+	zassert_equal(*uncached, 40);
 
 	sys_cache_data_flush_and_invd_all();
 
 	/* After sys_cache_data_flush_and_invd_all(), uncached should be updated */
-	zassert_equal(*cached, 42, NULL);
-	zassert_equal(*uncached, 42, NULL);
+	zassert_equal(*cached, 42);
+	zassert_equal(*uncached, 42);
 
 	/* Flush and invalidate again, this time to check the invalidate part */
 	sys_cache_data_flush_and_invd_all();
 	*uncached = 80;
 
 	/* As cache is invalid, cached should be updated with uncached new value */
-	zassert_equal(*cached, 80, NULL);
-	zassert_equal(*uncached, 80, NULL);
+	zassert_equal(*cached, 80);
+	zassert_equal(*uncached, 80);
 
 	*cached = 82;
 
 	/* Only cached should have changed */
-	zassert_equal(*cached, 82, NULL);
-	zassert_equal(*uncached, 80, NULL);
+	zassert_equal(*cached, 82);
+	zassert_equal(*uncached, 80);
 
 	sys_cache_data_flush_all();
 
 	/* After sys_cache_data_flush_all(), uncached should be updated */
-	zassert_equal(*cached, 82, NULL);
-	zassert_equal(*uncached, 82, NULL);
+	zassert_equal(*cached, 82);
+	zassert_equal(*uncached, 82);
 
 	*uncached = 100;
 
 	/* As cache is not invalid, only uncached should be updated */
-	zassert_equal(*cached, 82, NULL);
-	zassert_equal(*uncached, 100, NULL);
+	zassert_equal(*cached, 82);
+	zassert_equal(*uncached, 100);
 
 	sys_cache_data_invd_all();
 
 	/* Now, cached should be updated */
-	zassert_equal(*cached, 100, NULL);
-	zassert_equal(*uncached, 100, NULL);
+	zassert_equal(*cached, 100);
+	zassert_equal(*uncached, 100);
 }
 
 ZTEST_SUITE(adsp_cache, NULL, NULL, NULL, NULL, NULL);
