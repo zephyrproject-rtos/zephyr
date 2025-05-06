84c7d6c9fe9f04461e701c4d68cecbbc4feae61a
Parthiban Nallathambi <parthiban@linumiz.com>
2025-05-06 17:22:14 +0530

    samples: ti/mspm0/shudown: add new sample for ACTIVE_LOW
    add a new sample for ACTIVE_LOW as it needs special care
    when setting up the pin to wake i.e to be called twice.
    
    - once before DL_SYSCTL_releaseShutdownIO
    - once before DL_SYSCTL_setPowerPolicySHUTDOWN
    
    before patch changes needs to be applied to test the ACTIVE_LOW
    logic to disable the release IO logic from power.c as we are
    handling this in application code.
    
    git diff
    diff --git a/soc/ti/mspm0/mspm0l122x_l222x/power.c b/soc/ti/mspm0/mspm0l122x_l222x/power.c
    index 62ddcf360a2..1a3c26f36af 100644
    --- a/soc/ti/mspm0/mspm0l122x_l222x/power.c
    +++ b/soc/ti/mspm0/mspm0l122x_l222x/power.c
    @@ -95,4 +95,4 @@ static int ti_mspm0l2xxx_pm_init(void)
    
            return 0;
     }
    -SYS_INIT(ti_mspm0l2xxx_pm_init, POST_KERNEL, 0);
    +//SYS_INIT(ti_mspm0l2xxx_pm_init, POST_KERNEL, 0);
    
    Signed-off-by: Parthiban Nallathambi <parthiban@linumiz.com>

---
+++ samples/boards/ti/mspm0/shutdown-low/src/low.c
@@ -0,0 +1,130 @@
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/hwinfo.h>
+#include <zephyr/drivers/gpio.h>
+
+#define SW1_NODE	DT_ALIAS(sw2)
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW1_NODE, gpios, {0});
+
+static int config_wake(bool enable)
+{
+	int ret;
+
+	if (!gpio_is_ready_dt(&button)) {
+		printk("Error: button device %s is not ready\n",
+				button.port->name);
+		return 0;
+	}
+
+	if (enable) {
+		ret = gpio_pin_configure_dt(&button, GPIO_INPUT | GPIO_INT_WAKEUP);
+		if (ret != 0) {
+			printk("Error %d: failed to configure %s pin %d\n",
+					ret, button.port->name, button.pin);
+			return 0;
+		}
+	} else {
+		ret = gpio_pin_configure_dt(&button, GPIO_DISCONNECTED | GPIO_INT_WAKEUP);
+		if (ret != 0) {
+			printk("Unable to disable wakeup: %d\n", ret);
+		}
+	}
+
+	return 0;
+}
+
+static int pm_init(void)
+{
+	int ret;
+	uint32_t rst_cause;
+
+	ret = hwinfo_get_reset_cause(&rst_cause);
+	if (ret != 0) {
+		return ret;
+	}
+
+	if (RESET_LOW_POWER_WAKE == rst_cause)
+	{
+		ret = config_wake(true);
+		DL_SYSCTL_releaseShutdownIO();
+	}
+
+	return 0;
+}
+SYS_INIT(pm_init, POST_KERNEL, 0);
+
+static bool main_init = false;
+int main(void)
+{
+	int ret;
+	uint32_t rst_cause;
+	uint8_t blink;
+
+	ret = hwinfo_get_reset_cause(&rst_cause);
+	if (ret != 0) {
+		return ret;
+	}
+
+	if (RESET_LOW_POWER_WAKE == rst_cause)
+	{
+		ret = config_wake(false);
+		printk("woke from shutdown\n");
+		k_msleep(1000);
+	}
+
+	ret = config_wake(true);
+	main_init = true;
+	k_sleep(K_FOREVER);
+}
+
+#include <zephyr/pm/policy.h>
+#include <zephyr/sys_clock.h>
+#include <zephyr/pm/device.h>
+
+extern int32_t max_latency_cyc;
+const struct pm_state_info *pm_policy_next_state(uint8_t cpu, int32_t ticks)
+{
+	int64_t cyc = -1;
+	uint8_t num_cpu_states;
+	const struct pm_state_info *cpu_states;
+
+	if (main_init == false)
+		return NULL;
+
+#ifdef CONFIG_PM_NEED_ALL_DEVICES_IDLE
+	if (pm_device_is_any_busy()) {
+		return NULL;
+	}
+#endif
+
+	if (ticks != K_TICKS_FOREVER) {
+		cyc = k_ticks_to_cyc_ceil32(ticks);
+	}
+
+	num_cpu_states = pm_state_cpu_get_all(cpu, &cpu_states);
+
+	for (int16_t i = (int16_t)num_cpu_states - 1; i >= 0; i--) {
+		const struct pm_state_info *state = &cpu_states[i];
+		uint32_t min_residency_cyc, exit_latency_cyc;
+
+		/* check if there is a lock on state + substate */
+		if (pm_policy_state_lock_is_active(state->state, state->substate_id)) {
+			continue;
+		}
+
+		min_residency_cyc = k_us_to_cyc_ceil32(state->min_residency_us);
+		exit_latency_cyc = k_us_to_cyc_ceil32(state->exit_latency_us);
+
+		/* skip state if it brings too much latency */
+		if ((max_latency_cyc >= 0) &&
+		    (exit_latency_cyc >= max_latency_cyc)) {
+			continue;
+		}
+
+		if ((cyc < 0) ||
+		    (cyc >= (min_residency_cyc + exit_latency_cyc))) {
+			return state;
+		}
+	}
+
+	return NULL;
+}
