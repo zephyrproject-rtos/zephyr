zephyrproject-rtos/zephyr

commit 4f54dcc23477d6497c180a14441183a2ae4da0c9

drivers: pwm: ti: Fix arbitrary invalid pulse width and period
Driverlib api DL_Timer_initCaptureCombinedMode enables the Zero and Load
event to the capture block, which inturn captures timing so there is
inconsistancy in input signal width and period calculation.

Signed-off-by: Saravanan Sekar <saravanan@linumiz.com>

drivers/pwm/pwm_mspm0.c
@@ -131,7 +131,7 @@ static int mspm0_pwm_set_cycles(const struct device *dev, uint32_t channel,
 		data->period = period_cycles >> 1;
 	}
 
-	DL_Timer_setLoadValue(config->base, data->period);
+	DL_Timer_setTimerCount(config->base, data->period);
 	DL_Timer_setCaptureCompareValue(config->base,
 					data->pulse_cycle[channel],
 					config->cc_idx[channel]);
@@ -168,6 +168,42 @@ static int mspm0_pwm_get_cycles_per_sec(const struct device *dev,
 }
 
 #ifdef CONFIG_PWM_CAPTURE
+#define MSPM0_CTRCTL_CAC_CCCTL_ACOND(x) (x << 10)
+
+static void mspm0_set_combined_mode(const struct pwm_mspm0_config *config,
+				    struct pwm_mspm0_data *data)
+{
+	DL_Timer_setLoadValue(config->base, data->period);
+	DL_Timer_setCaptureCompareInput(config->base, 0,
+			((config->cc_idx[0] & 0x1) ? 		\
+			  DL_TIMER_CC_IN_SEL_CCPX : DL_TIMER_CC_IN_SEL_CCP0),
+			config->cc_idx[0]);
+
+	DL_Timer_setCaptureCompareInput(config->base, 0,
+				GPTIMER_IFCTL_01_ISEL_CCPX_INPUT_PAIR,
+				(config->cc_idx[0] ^ 1));
+
+	DL_Timer_setCaptureCompareCtl(config->base,
+				DL_TIMER_CC_MODE_CAPTURE,
+				(DL_TIMER_CC_CCOND_TRIG_FALL),
+				config->cc_idx[0]);
+
+	DL_Timer_setCaptureCompareCtl(config->base,
+				DL_TIMER_CC_MODE_CAPTURE,
+				(DL_TIMER_CC_CCOND_TRIG_RISE),
+				(config->cc_idx[0] ^ 1));
+
+	DL_Timer_setCCPDirection(config->base, DL_TIMER_CC0_INPUT);
+
+	DL_Timer_setCounterControl(config->base,
+				   DL_TIMER_CZC_CCCTL0_ZCOND,
+				   (MSPM0_CTRCTL_CAC_CCCTL_ACOND(config->cc_idx[0])),
+				   DL_TIMER_CLC_CCCTL0_LCOND);
+
+	DL_Timer_setCounterRepeatMode(config->base, DL_TIMER_REPEAT_MODE_ENABLED);
+	DL_Timer_setCounterMode(config->base, DL_TIMER_COUNT_MODE_DOWN);
+}
+
 static void mspm0_setup_capture(const struct device *dev,
 				const struct pwm_mspm0_config *config,
 				struct pwm_mspm0_data *data)
@@ -180,11 +216,7 @@ static void mspm0_setup_capture(const struct device *dev,
 
 		DL_Timer_initCaptureMode(config->base, &cc_cfg);
 	} else {
-		DL_Timer_CaptureCombinedConfig cc_cfg = { 0 };
-		cc_cfg.inputChan = config->cc_idx[0];
-		cc_cfg.period = data->period;
-
-		DL_Timer_initCaptureCombinedMode(config->base, &cc_cfg);
+		mspm0_set_combined_mode(config, data);
 	}
 
 	DL_Timer_enableClock(config->base);
@@ -279,8 +311,9 @@ static int mspm0_capture_enable(const struct device *dev, uint32_t channel)
 		return -EBUSY;
 	}
 
-	DL_TimerG_setTimerCount(config->base, data->period);
+	DL_Timer_setTimerCount(config->base, data->period);
 	DL_Timer_startCounter(config->base);
+	DL_Timer_clearInterruptStatus(config->base, intr_mask);
 	DL_Timer_enableInterrupt(config->base, intr_mask);
 
 	k_mutex_unlock(&data->lock);
@@ -391,7 +424,11 @@ static void mspm0_cc_isr(const struct device *dev)
 
 	/* Timer reached zero no pwm signal is detected */
 	case DL_TIMERG_IIDX_ZERO:
-		data->is_synced = false;
+		if (data->callback &&
+		    !(data->flags & PWM_CAPTURE_MODE_CONTINUOUS)) {
+			data->callback(dev, 0, 0, 0, -ERANGE, data->user_data);
+			DL_Timer_stopCounter(config->base);
+		}
 		__fallthrough;
 
 	default:
@@ -401,7 +438,7 @@ static void mspm0_cc_isr(const struct device *dev)
 	if (data->flags & PWM_CAPTURE_TYPE_PERIOD) {
 		cc1 =  DL_Timer_getCaptureCompareValue(
 						config->base,
-						!(config->cc_idx[0]));
+						config->cc_idx[0] ^ 0x1);
 	}
 
 	/* ignore the unsynced counter value for pwm mode */
@@ -424,8 +461,9 @@ static void mspm0_cc_isr(const struct device *dev)
 		data->is_synced = false;
 	}
 
-	period = ((data->last_sample - cc1 + data->period) % data->period);
-	pulse = ((data->last_sample - cc0 + data->period) % data->period);
+
+	period = ((data->last_sample - cc1 + UINT16_MAX) % UINT16_MAX);
+	pulse = ((data->last_sample - cc0 + UINT16_MAX) % UINT16_MAX);
 	if (data->callback && period) {
 		data->callback(dev, 0, period, pulse, 0, data->user_data);
 	}

boards/ti/lp_mspm0l2228/lp_mspm0l2228_mspm0l2228.dts
@@ -19,7 +19,7 @@
 		sw1 = &btn1;
 		pwm-led0 = &pwm_led0;
 		counter = &countera0;
-		capture = &pwm4;
+		capture = &pwm12;
 	};
 
 	chosen {
@@ -196,16 +196,16 @@
 	status = "okay";
 };
 
-&gpt4 {
+&gpt12 {
 	status = "okay";
 };
 
 &pwm4 {
-	status = "okay";
+	status = "disabled";
 	pinctrl-0 = <&timg4c1_pb3>;
 	pinctrl-names = "default";
 	ti,cc-index = <1>;
-	ti,clk-prescaler = <255>;
+	ti,clk-prescaler = <99>;
 	ti,clk-div = <1>;
 	ti,cc-mode = "PULSE_WIDTH";
 	ti,period = <65535>;
@@ -216,10 +216,21 @@
 	pinctrl-0 = <&timg0c1_pa24>, <&timg0c0_pb0>;
 	pinctrl-names = "default";
 	ti,cc-index = <0 1>;
-	ti,clk-prescaler = <255>;
+	ti,clk-prescaler = <0>;
 	ti,clk-div = <1>;
 	ti,pwm-mode = "EDGE_ALIGN";
-	ti,period = <1000>;
+	ti,period = <1600>;
+};
+
+&pwm12 {
+	status = "okay";
+	pinctrl-0 = <&timg12c1_pa25>;
+	pinctrl-names = "default";
+	ti,cc-index = <1>;
+	ti,clk-prescaler = <0>;
+	ti,clk-div = <1>;
+	ti,cc-mode = "PULSE_WIDTH";
+	ti,period = <65535>;
 };
 
 &countera0 {

dts/arm/ti/mspm0l122x_l222x/mspm0lx22x-pinctrl.dtsi
@@ -157,4 +157,10 @@
 	/omit-if-no-ref/ tima0c0_pa8: tima0c0_pa8{
 		pinmux = <MSP_PINMUX(30, MSPM0_PIN_FUNCTION_5)>;
 	};
+
+	/omit-if-no-ref/ timg12c1_pa25: timg12c1_pa25 {
+		pinmux = <MSP_PINMUX(69, MSPM0_PIN_FUNCTION_4)>;
+		input-enable;
+		bias-pull-down;
+	};
 };

dts/arm/ti/mspm0l122x_l222x/mspm0lx22x.dtsi
@@ -379,6 +379,20 @@
 				status = "disabled";
 			};
 		};
+
+		gpt12: timg@40870000 {
+			compatible = "ti,mspm0g1x0x_g3x0x-timer";
+			reg = <0x40870000 0x2000>;
+			status = "disabled";
+
+			pwm12: pwm12 {
+				compatible = "ti,mspm0g1x0x_g3x0x-timer-pwm";
+				clocks = <&clkmux MSPM0_CLOCK_BUS_ULPCLK>;
+				interrupts = <2 0>;
+				status = "disabled";
+				#pwm-cells = <0x3>;
+			};
+		};
 	};
 };
 

commit 37b088e6017d57a738db08efb50a3a6450834077

drivers: pwm: fix regression on pwm output not working
PWM duty cycle is changed with timer enabled, use of DL_Timer_setTimerCount
during timer running is undefined behaviour.

Fixes: 4f54dcc2347 (drivers: pwm: ti: Fix arbitrary invalid pulse width)
Signed-off-by: Saravanan Sekar <saravanan@linumiz.com>

drivers/pwm/pwm_mspm0.c
@@ -131,7 +131,7 @@ static int mspm0_pwm_set_cycles(const struct device *dev, uint32_t channel,
 		data->period = period_cycles >> 1;
 	}
 
-	DL_Timer_setTimerCount(config->base, data->period);
+	DL_Timer_setLoadValue(config->base, data->period);
 	DL_Timer_setCaptureCompareValue(config->base,
 					data->pulse_cycle[channel],
 					config->cc_idx[channel]);