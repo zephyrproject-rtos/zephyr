f370a1b50357616cb05a0a35558692b7e4d89c02
Martin Hoff <martin.hoff@silabs.com>
2025-05-07 16:16:24 +0200

    soc: silabs: workaround for NULL address assignment
    Since the RAM start address for the siwg_917 SoC is set to 0x0, nothing
    prevents placing a RAM function or data at address 0x0. This can create
    a NULL pointer error (or a failed assertion in tests) when the function
    is called or the data is accessed. This commit adds a workaround to
    prevent the RAM start address from being used by reserving the address
    0x0 with a dummy function.
    
    LINKER_KEEP is used to prevent the compiler from optimizing this
    function away.
    
    __ramfunc is used to place this function in RAM. It also has the
    advantage of being moved with the ramfunc section if
    CONFIG_SARAM_VECTOR_TABLE is set.
    
    Limitation: In the case of multiple __ramfunc functions, we need to
    ensure that null_pointer_function is the first to be placed in RAM. This
    can be achieved by modifying the ramfunc linker script. It is not done
    in this commit.
    
    Signed-off-by: Martin Hoff <martin.hoff@silabs.com>

--- soc/silabs/silabs_siwx91x/siwg917/soc.c
+++ soc/silabs/silabs_siwx91x/siwg917/soc.c
@@ -16,6 +16,17 @@ void soc_early_init_hook(void)
 	SystemInit();
 }
 
+/* Reserve the address 0x0 at the start of the RAM. LINKER_KEEP is used to prevent the compiler from
+ * optimizing this function away. __ramfunc is used to place this function in RAM. It also has the
+ * advantage of being moved with the ramfunc section if CONFIG_SRAM_VECTOR_TABLE is set.
+ * TODO: In case of multiple __ramfunc functions, we need to ensure that null_pointer_function
+ * is the first to be placed in RAM. This can be achieved by modifying the ramfunc linker script.
+ */
+__ramfunc void null_pointer_function(void)
+{
+}
+LINKER_KEEP(null_pointer_function);
+
 /* SiWx917's bootloader requires IRQn 32 to hold payload's entry point address. */
 extern void z_arm_reset(void);
 Z_ISR_DECLARE_DIRECT(32, 0, z_arm_reset);
