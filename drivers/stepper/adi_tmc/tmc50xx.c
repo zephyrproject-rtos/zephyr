4c38b16f03320679f1fef4b9c3d68c6ddb08f410
Dipak Shetty <shetty.dipak@gmx.com>
2025-05-08 23:27:57 +0200

    drivers: stepper: adi_tmc: tmc5xxxx: fixed standstill detection
    The standstill detection logic now stands corrected. Moving would
    be indicated when the standstill bit is not zero.
    
    Signed-off-by: Dipak Shetty <shetty.dipak@gmx.com>

--- drivers/stepper/adi_tmc/tmc50xx.c
+++ drivers/stepper/adi_tmc/tmc50xx.c
@@ -341,7 +341,7 @@ static int tmc50xx_stepper_is_moving(const struct device *dev, bool *is_moving)
 		return -EIO;
 	}
 
-	*is_moving = (FIELD_GET(TMC5XXX_DRV_STATUS_STST_BIT, reg_value) == 1U);
+	*is_moving = (FIELD_GET(TMC5XXX_DRV_STATUS_STST_BIT, reg_value) != 1U);
 	LOG_DBG("Stepper motor controller %s is moving: %d", dev->name, *is_moving);
 	return 0;
 }
