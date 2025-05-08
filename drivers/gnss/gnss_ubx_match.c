be19fd2a32ea5637258a962c1ca9634bc5c831ed
Luis Ubieda <luisf@croxel.com>
2025-05-07 20:29:12 -0400

    ubx: Refactor codebase in order to improve usability
    During testing and code inspection, there were various anti-patterns
    on this (and U-Blox driver) codebase(s), including obfuscation, and
    lack of data validation. This made it increasingly difficult to
    introduce further variants of u-blox GNSS modems.
    
    With this patch, both the UBX modem and the M8 driver have been
    refactored to ease the reliability and maintainability of these
    codebases. Here are some highlights:
    
    WRT UBX modem:
    - Helper macros to easily create UBX frames, (including checksum
    calculation), at compile time; thus, making it easier to extend UBX
    commands.
    - Logic validation by the inclusion of the modem_ubx testsuite, used to
    refactor the code through TDD.
    - Ability to receive unsolicited messages, in order to enable U-Blox
    drivers to rely on modem_ubx to transceive all commands, and avoid
    hopping between modem_ubx and modem_chat.
    
    WRT M8 driver:
    - Remove GNSS specific protocol header files. Instead, unify them under
    modem/ubx/protocol.h. Background: After a survey and looking at ubxlib
    SDK I conclude the UBX protocol is by definition a GNSS protocol (there
    are non-GNSS u-blox modems, but they're not interfaced through UBX
    protocol).
    - Establish pattern to create and send/receive commands using new
    foundations on modem ubx.
    - Remove dependency of Modem chat, and instead use UBX unsolicited
    messages to get Navigation and Satellites data.
    - Switch from the auto-baudrate detection pattern to a pattern of
    transitioning between an initial known baudrate to a desired baudrate,
    in order to improve initialization time.
    - Add dts property to configure default fix-rate.
    
    Signed-off-by: Luis Ubieda <luisf@croxel.com>

---
+++ drivers/gnss/gnss_ubx_match.c
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2025 Croxel Inc.
+ * Copyright (c) 2025 CogniPilot Foundation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/drivers/gnss.h>
+#include <zephyr/drivers/gnss/gnss_publish.h>
+
+#include "gnss_ubx_match.h"
+
+void gnss_ubx_match_pvt_callback(struct modem_ubx *ubx, const struct ubx_frame *frame,
+				 size_t len, void *user_data)
+{
+	if (len >= UBX_FRM_SZ(sizeof(struct ubx_nav_pvt))) {
+		struct gnss_ubx_match_data *data = user_data;
+		const struct device *dev = data->gnss;
+		const struct ubx_nav_pvt *nav_pvt =
+			(const struct ubx_nav_pvt *)frame->payload_and_checksum;
+
+		if (nav_pvt->time.valid.date &&
+		    nav_pvt->time.valid.time &&
+		    nav_pvt->time.valid.time_fully_resolved) {
+
+			enum gnss_fix_quality fix_quality = GNSS_FIX_QUALITY_INVALID;
+			enum gnss_fix_status fix_status = GNSS_FIX_STATUS_NO_FIX;
+
+			if (nav_pvt->flags.gnss_fix_ok &&
+			    !nav_pvt->nav.flags3.invalid_lon_height_hmsl) {
+
+				switch (nav_pvt->fix_type) {
+				case UBX_NAV_FIX_TYPE_DR:
+				case UBX_NAV_FIX_TYPE_GNSS_DR_COMBINED:
+					fix_quality = GNSS_FIX_QUALITY_ESTIMATED;
+					fix_status = GNSS_FIX_STATUS_ESTIMATED_FIX;
+					break;
+				case UBX_NAV_FIX_TYPE_2D:
+				case UBX_NAV_FIX_TYPE_3D:
+					fix_quality = GNSS_FIX_QUALITY_GNSS_SPS;
+					fix_status = GNSS_FIX_STATUS_GNSS_FIX;
+					break;
+				default:
+					break;
+				}
+			}
+
+			struct gnss_data gnss_data = {
+				.info = {
+					.satellites_cnt = nav_pvt->nav.num_sv,
+					.hdop = nav_pvt->nav.pdop * 10,
+					.geoid_separation = (nav_pvt->nav.height -
+							     nav_pvt->nav.hmsl),
+					.fix_status = fix_status,
+					.fix_quality = fix_quality,
+				},
+				.nav_data = {
+					.latitude = (int64_t)nav_pvt->nav.latitude * 100,
+					.longitude = (int64_t)nav_pvt->nav.longitude * 100,
+					.bearing = (((nav_pvt->nav.head_motion < 0) ?
+						     (nav_pvt->nav.head_motion + (360 * 100000)) :
+						     (nav_pvt->nav.head_motion)) / 100),
+					.speed = nav_pvt->nav.ground_speed,
+					.altitude = nav_pvt->nav.hmsl,
+				},
+				.utc = {
+					.hour = nav_pvt->time.hour,
+					.minute = nav_pvt->time.minute,
+					.millisecond = (nav_pvt->time.second * 1000) +
+						       (nav_pvt->time.nano / 1000000),
+					.month_day = nav_pvt->time.day,
+					.month = nav_pvt->time.month,
+					.century_year = (nav_pvt->time.year % 100),
+				},
+			};
+
+			gnss_publish_data(dev, &gnss_data);
+		}
+	}
+}
+
+#if CONFIG_GNSS_SATELLITES
+void gnss_ubx_match_satellite_callback(struct modem_ubx *ubx, const struct ubx_frame *frame,
+				       size_t len, void *user_data)
+{
+	const struct ubx_nav_sat *ubx_sat = (const struct ubx_nav_sat *)frame->payload_and_checksum;
+	int num_satellites = (len - UBX_FRM_SZ_WITHOUT_PAYLOAD - sizeof(struct ubx_nav_sat)) /
+			     sizeof(struct ubx_nav_sat_info);
+
+	if (num_satellites > 0 && (num_satellites == ubx_sat->num_sv)) {
+		struct gnss_ubx_match_data *data = user_data;
+		const struct device *dev = data->gnss;
+
+		num_satellites = MIN(num_satellites, data->satellites.size);
+
+		for (size_t i = 0 ; i < num_satellites ; i++) {
+			enum gnss_system gnss_system = 0;
+
+			switch (ubx_sat->sat[i].gnss_id) {
+			case UBX_GNSS_ID_GPS:
+				gnss_system = GNSS_SYSTEM_GPS;
+				break;
+			case UBX_GNSS_ID_SBAS:
+				gnss_system = GNSS_SYSTEM_SBAS;
+				break;
+			case UBX_GNSS_ID_GALILEO:
+				gnss_system = GNSS_SYSTEM_GALILEO;
+				break;
+			case UBX_GNSS_ID_BEIDOU:
+				gnss_system = GNSS_SYSTEM_BEIDOU;
+				break;
+			case UBX_GNSS_ID_QZSS:
+				gnss_system = GNSS_SYSTEM_QZSS;
+				break;
+			case UBX_GNSS_ID_GLONASS:
+				gnss_system = GNSS_SYSTEM_GLONASS;
+				break;
+			default:
+				break;
+			}
+
+			struct gnss_satellite sat = {
+				/** TODO: Determine how to determine PRN from UBX sat info.
+				 * For now passing SV_ID.
+				 */
+				.prn = ubx_sat->sat[i].sv_id,
+				.snr = ubx_sat->sat[i].cno,
+				.elevation = ubx_sat->sat[i].elevation,
+				.azimuth = ubx_sat->sat[i].azimuth,
+				.system = gnss_system,
+				.is_tracked = ubx_sat->sat[i].flags.sv_used,
+			};
+
+			data->satellites.data[i] = sat;
+		}
+
+		gnss_publish_satellites(dev, data->satellites.data, num_satellites);
+	}
+}
+#endif
+
+void gnss_ubx_match_init(struct gnss_ubx_match_data *data,
+			 const struct gnss_ubx_match_config *config)
+{
+	data->gnss = config->gnss;
+#if CONFIG_GNSS_SATELLITES
+	data->satellites.data = config->satellites.buf;
+	data->satellites.size = config->satellites.size;
+#endif
+}
