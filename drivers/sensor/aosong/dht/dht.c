zephyrproject-rtos/zephyr

commit c9be1a991b18287a02f485279113e86dc6d3f176

drivers: sensor: dht: Fix sensor read issue on esp32_devkitc_wrover
The DHT[11/22] sensor driver fails to read data due to an extra
"gpio_pin_set_dt" that is occurs prior to waiting for the sensor active
response. This leads to consistent failed reads on esp32 (devkitc,
wrover). After removing this line reads are consistently successful.

Signed-off-by: Omar Naffaa <omarnaffaa.on@gmail.com>

drivers/sensor/aosong/dht/dht.c
@@ -82,8 +82,6 @@ static int dht_sample_fetch(const struct device *dev,
 
 	k_busy_wait(DHT_START_SIGNAL_DURATION);
 
-	gpio_pin_set_dt(&cfg->dio_gpio, false);
-
 	/* switch to DIR_IN to read sensor signals */
 	gpio_pin_configure_dt(&cfg->dio_gpio, GPIO_INPUT);
 

commit 337451e78ad3e375430ecddc42e6ae6f2de8eed7

driver: sensor: aosong,dht: fix sensor protocol broken since change #83192
A recent change 9eeb60c improperly removed a critical line in the driver.

This commit replaces that line and moves an irq_lock to above any pin

value manipulation since the timing is sensitive with the DHT protocol.

This should also fix the timing issue 9eeb60c attempted to fix.

Signed-off-by: John Shelton <moosery@gmail.com>

drivers/sensor/aosong/dht/dht.c
@@ -67,20 +67,25 @@ static int dht_sample_fetch(const struct device *dev,
 	uint8_t buf[5];
 	unsigned int i, j;
 
-#if defined(CONFIG_DHT_LOCK_IRQS)
-	int lock;
-#endif
-
 	__ASSERT_NO_MSG(chan == SENSOR_CHAN_ALL);
+
+#if defined(CONFIG_DHT_LOCK_IRQS)
+	/* Get the lock before any pin interaction since irq_lock has a */
+	/* potential of causing delays itself.                          */
+	int lock = irq_lock();
+#endif
 
 	/* assert to send start signal */
 	gpio_pin_set_dt(&cfg->dio_gpio, true);
 
-#if defined(CONFIG_DHT_LOCK_IRQS)
-	lock = irq_lock();
-#endif
-
 	k_busy_wait(DHT_START_SIGNAL_DURATION);
+
+	/* set the state back to logic LOW (voltage HIGH) so that the   */
+	/* subsequent call to dht_measure_signal duration waits for the */
+	/* DHT sensor to set it back to HIGH (voltage LOW).             */
+	/* Failure to do this would cause that subsequent call to       */
+	/* return immediately.                                          */
+	gpio_pin_set_dt(&cfg->dio_gpio, false);
 
 	/* switch to DIR_IN to read sensor signals */
 	gpio_pin_configure_dt(&cfg->dio_gpio, GPIO_INPUT);