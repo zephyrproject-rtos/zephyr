31605f7406448036efe7bd247c38365b46b63b26
Etienne Carriere <etienne.carriere@st.com>
2025-05-09 12:22:04 +0200

    boards: st: stm32wba65i_dk1: Add ns variant for TF-M support
    Add variant ns to stm32wba65i_dk1 board to embed TF-M in the SoC
    secure world. The flash layout is synced with the layout defined
    in Zephyr TF-M integration of platform STM32WBA65I.
    
    Successfully tested against a few samples and test samples:
    - samples/tfm_integration/psa_crypto
    - samples/tfm_integration/psa_protected_storage
    - samples/tfm_integration/tfm_ipc
    - samples/tfm_integration/tfm_regression_test
    - samples/tfm_integration/tfm_secure_partition
    - tests/subsys/secure_storage/psa/crypto
    - tests/subsys/secure_storage/psa/its
      (with CONFIG_TFM_ITS_MAX_ASSET_SIZE_OVERRIDE=y
      and CONFIG_TFM_ITS_MAX_ASSET_SIZE=256)
    
    The platforms do not yet support samples/tfm_integration/tfm_psa_test.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- boards/st/stm32wba65i_dk1/board.cmake
+++ boards/st/stm32wba65i_dk1/board.cmake
@@ -1,7 +1,32 @@
 # Copyright (c) 2025 STMicroelectronics
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+if(CONFIG_BUILD_WITH_TFM)
+  set(FLASH_BASE_ADDRESS_S 0x0C000000)
+
+  # Flash merged TF-M + Zephyr binary
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
+
+  if (CONFIG_HAS_FLASH_LOAD_OFFSET)
+    MATH(EXPR TFM_HEX_BASE_ADDRESS_NS "${FLASH_BASE_ADDRESS_S}+${CONFIG_FLASH_LOAD_OFFSET}")
+  else()
+    set(TFM_HEX_BASE_ADDRESS_NS ${TFM_FLASH_BASE_ADDRESS_S})
+  endif()
+
+  # System entry point is TF-M vector, located 1kByte after tfm_fmw_partition in DTS
+  get_target_property(TFM_FWM_NODE_NAME devicetree_target "DT_NODELABEL|slot0_secure_partition")
+  string(REGEX REPLACE ".*@([^@]+)$" "\\1" TFM_FWM_OFFSET "${TFM_FWM_NODE_NAME}")
+  if(NOT TFM_FWM_OFFSET)
+    message(FATAL_ERROR "Could not find TF-M firmware offset from node label slot0_secure_partition")
+  endif()
+  math(EXPR TFM_FWM_BOOT_ADDR "0x${TFM_FWM_OFFSET}+${FLASH_BASE_ADDRESS_S}+0x400")
+
+  board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw"
+    "--erase" "--start-address=${TFM_FWM_BOOT_ADDR}"
+  )
+else()
+  board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+endif()
 
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
