7a7f28ef51d3d369054e76931bb87d504bbd52d3
Alain Volmat <alain.volmat@foss.st.com>
2025-05-09 22:34:45 +0200

    dts: bindings: addition of RaspberryPi CSI 22 pins camera connector
    Add RaspberryPi CSI 22 pins camera connector description.
    
    Signed-off-by: Alain Volmat <alain.volmat@foss.st.com>

---
+++ include/zephyr/dt-bindings/gpio/raspberrypi-csi-22pins-connector.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2025 STMicroelectronics
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef INCLUDE_ZEPHYR_DT_BINDINGS_GPIO_RASPBERRYPI_CSI_22PINS_CONNECTOR_H_
+#define INCLUDE_ZEPHYR_DT_BINDINGS_GPIO_RASPBERRYPI_CSI_22PINS_CONNECTOR_H_
+
+/**
+ * @name CSI 22 pins camera connector pinout
+ * @{
+ */
+#define CSI_22PINS_IO0		17	/**< GPIO0 */
+#define CSI_22PINS_IO1		18	/**< GPIO1 */
+#define CSI_22PINS_I2C_SCL	20	/**< I2C clock pin */
+#define CSI_22PINS_I2C_SDA	21	/**< I2C data pin */
+/** @} */
+
+#endif /* INCLUDE_ZEPHYR_DT_BINDINGS_GPIO_RASPBERRYPI_CSI_22PINS_CONNECTOR_H_ */
