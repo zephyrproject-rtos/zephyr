99e9619b1ee3c55bcb61f4ac6615f9d3277a7524
S Mohamed Fiaz <fiaz.mohamed@silabs.com>
2025-05-10 16:13:53 +0530

    soc: silabs: siwx91x: Add siwx91x Power Manager driver
    This commit enables the Power Manager driver
    support for the siwx91x device.
    
    Signed-off-by: S Mohamed Fiaz <fiaz.mohamed@silabs.com>

--- soc/silabs/silabs_siwx91x/siwg917/soc.c
+++ soc/silabs/silabs_siwx91x/siwg917/soc.c
@@ -10,10 +10,28 @@
 #include <zephyr/sw_isr_table.h>
 
 #include "em_device.h"
+#ifdef CONFIG_WISECONNECT_NETWORK_STACK
+#include "sli_siwx917_soc.h"
+#endif
+#include "sl_si91x_power_manager.h"
 
 void soc_early_init_hook(void)
 {
+	__maybe_unused sl_power_peripheral_t peripheral_config = {};
+	__maybe_unused sl_power_ram_retention_config_t ram_configuration = {
+		.configure_ram_banks = false,
+		.m4ss_ram_size_kb = (DT_REG_SIZE(DT_NODELABEL(sram0)) / 1024),
+		.ulpss_ram_size_kb = 4,
+	};
 	SystemInit();
+#if IS_ENABLED(CONFIG_SOC_SIWX91X_PM_BACKEND_PMGR)
+	sli_si91x_platform_init();
+	sl_si91x_power_manager_init();
+	sl_si91x_power_manager_remove_peripheral_requirement(&peripheral_config);
+	sl_si91x_power_manager_configure_ram_retention(&ram_configuration);
+	sl_si91x_power_manager_add_ps_requirement(SL_SI91X_POWER_MANAGER_PS4);
+	sl_si91x_power_manager_set_clock_scaling(SL_SI91X_POWER_MANAGER_PERFORMANCE);
+#endif
 }
 
 /* SiWx917's bootloader requires IRQn 32 to hold payload's entry point address. */
