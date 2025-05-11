416457b6dcff92a26efa99e11345b020302ff620
Josuah Demangeon <me@josuah.net>
2025-05-11 17:55:38 +0000

    samples: drivers: video: capture: convert #ifdef into if()
    Convert #ifdef CONFIG_TEST into if (IS_ENABLED(CONFIG_TEST)) to improve
    readability.
    
    Signed-off-by: Josuah Demangeon <me@josuah.net>

--- samples/drivers/video/capture/src/main.c
+++ samples/drivers/video/capture/src/main.c
@@ -207,10 +207,10 @@ int main(void)
 		video_set_ctrl(video_dev, &ctrl);
 	}
 
-#ifdef CONFIG_TEST
-	ctrl.id = VIDEO_CID_TEST_PATTERN;
-	video_set_ctrl(video_dev, &ctrl);
-#endif
+	if (IS_ENABLED(CONFIG_TEST)) {
+		ctrl.id = VIDEO_CID_TEST_PATTERN;
+		video_set_ctrl(video_dev, &ctrl);
+	}
 
 #if DT_HAS_CHOSEN(zephyr_display)
 	const struct device *const display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
