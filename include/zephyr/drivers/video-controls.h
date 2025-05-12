be136f823e167df345f77605bc24f12a1061449e
Alain Volmat <alain.volmat@foss.st.com>
2025-05-12 13:38:45 +0200

    video: add LINK_FREQUENCY ctrl and a helper to retrieve it
    Add a read-only ctrl VIDEO_CID_LINK_FREQUENCY to indicate
    to a sink the frequency at which a device streams data over
    CSI2. Since not all source device currently provide the
    LINK_FREQUENCY control, add a helper function to retrieve
    it, with a fall-back by doing an approximate via the
    PIXEL_RATE control.
    
    Signed-off-by: Alain Volmat <alain.volmat@foss.st.com>

--- include/zephyr/drivers/video-controls.h
+++ include/zephyr/drivers/video-controls.h
@@ -368,6 +368,9 @@ enum video_camera_orientation {
 /** Selection of the type of test pattern to represent */
 #define VIDEO_CID_TEST_PATTERN (VIDEO_CID_IMAGE_PROC_CLASS_BASE + 3)
 
+/** Link frequency, applicable for the CSI2 based devices. This control is read-only. */
+#define VIDEO_CID_LINK_FREQUENCY (VIDEO_CID_IMAGE_PROC_CLASS_BASE + 4)
+
 /**
  * @}
  */
