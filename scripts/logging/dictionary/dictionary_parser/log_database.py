e6a9b9cd8951cc8d941a5d9b615b204d1968f92d
Keith Packard <keithp@keithp.com>
2025-05-12 14:58:37 -0400

    scripts/logging: Add RX config detection
    The RX arch uses the CONFIG_RX kconfig symbol
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- scripts/logging/dictionary/dictionary_parser/log_database.py
+++ scripts/logging/dictionary/dictionary_parser/log_database.py
@@ -55,6 +55,9 @@ ARCHS = {
     "riscv" : {
         "kconfig": "CONFIG_RISCV",
     },
+    "rx" : {
+        "kconfig": "CONFIG_RX",
+    },
     "xtensa" : {
         "kconfig": "CONFIG_XTENSA",
     },
