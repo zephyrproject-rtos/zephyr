3f3e8893a4050328eef4dcd91ac5e8130d277611
Jiafei Pan <Jiafei.Pan@nxp.com>
2025-05-14 17:32:40 +0800

    boards: imx93_evk: refine jlink runner
    The ogirnal design is using a J-Link script, then the path of image
    is saved into the script file during building, it will has issue
    when generate twister artifacts and copy it to another path for
    testing as path is changed. so refine it to use new parameter of
    J-Link runner parameter "--flash-sram" to fix this issue.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- boards/nxp/imx93_evk/board.cmake
+++ boards/nxp/imx93_evk/board.cmake
@@ -5,7 +5,7 @@
 
 if(CONFIG_SOC_MIMX9352_A55)
 
-board_runner_args(jlink "--device=MIMX9352_A55_0" "--no-reset" "--flash-script=${CMAKE_BINARY_DIR}/zephyr/runner.jlinkscript")
+board_runner_args(jlink "--device=MIMX9352_A55_0" "--no-reset" "--flash-sram")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 
