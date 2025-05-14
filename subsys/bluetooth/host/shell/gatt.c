aaafb8f98189ff42b1a577aed57b8b1da1ca3b9e
Alex Apostolu <apostolu240@gmail.com>
2025-05-13 21:08:02 -0400

    Bluetooth: Host: Use macro for nanoseconds per second
    Use the macro NSEC_PER_SEC defined in sys_clock.h for nanoseconds per
    second when updating write stats.
    
    Signed-off-by: Alex Apostolu <apostolu240@gmail.com>

--- subsys/bluetooth/host/shell/gatt.c
+++ subsys/bluetooth/host/shell/gatt.c
@@ -27,6 +27,7 @@
 #include <zephyr/sys/__assert.h>
 #include <zephyr/sys/byteorder.h>
 #include <zephyr/sys/time_units.h>
+#include <zephyr/sys_clock.h>
 #include <zephyr/sys/util.h>
 #include <sys/types.h>
 
@@ -61,14 +62,14 @@ static void update_write_stats(uint16_t len)
 	/* if last data rx-ed was greater than 1 second in the past,
 	 * reset the metrics.
 	 */
-	if (delta > 1000000000) {
+	if (delta > NSEC_PER_SEC) {
 		write_stats.len = 0U;
 		write_stats.rate = 0U;
 		cycle_stamp = k_cycle_get_32();
 	} else {
 		write_stats.len += len;
 		write_stats.rate = ((uint64_t)write_stats.len << 3) *
-			1000000000U / delta;
+			NSEC_PER_SEC / delta;
 	}
 }
 
