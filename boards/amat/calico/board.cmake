60bd4096ab4d082146ecd949152f1760013af7e2
Greg Gallagher <ggallagher@Mac.lan>
2025-05-15 10:39:55 -0400

    boards: calico: Add new board calico
    Add applied materials boards folder and first board calico.
    
    Signed-off-by: Greg Gallagher <ggallagher@Mac.lan>

---
+++ boards/amat/calico/board.cmake
@@ -0,0 +1,6 @@
+# Copyright (c) 2023 Antmicro <www.antmicro.com>
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=AMA4B2KP-KXR" "--iface=swd" "--speed=1000 --autoconnect=1")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
