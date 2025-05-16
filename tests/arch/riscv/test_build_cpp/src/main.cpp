65ccd3a3385513ccd873e2846795744b82f8be73
Tom Hughes <tomhughes@chromium.org>
2025-05-16 13:21:31 -0700

    tests: riscv: Add test to verify C++ compilation
    Using clang to build for RISC-V with these configs:
    
    CONFIG_CPP=y
    CONFIG_USERSPACE=n
    CONFIG_SMP=n
    CONFIG_FPU_SHARING=n
    
    results in the following warnings:
    
    include/zephyr/arch/riscv/structs.h:11:1: error: empty struct has size 0
    in C, size 1 in C++ [-Werror,-Wextern-c-compat]
    11 | struct _cpu_arch {
    | ^
    
    include/zephyr/arch/riscv/thread.h:68:1: error: empty struct has size 0
    in C, size 1 in C++ [-Werror,-Wextern-c-compat]
    68 | struct _thread_arch {
    | ^
    
    This commit adds a test with those configs to verify compilation
    succeeds. Although gcc doesn't emit these warnings, we can test with gcc
    as well with a BUILD_ASSERT in include/zephyr/arch/riscv/thread.h and
    include/zephyr/arch/riscv/structs.h.
    
    Signed-off-by: Tom Hughes <tomhughes@chromium.org>

---
+++ tests/arch/riscv/test_build_cpp/src/main.cpp
@@ -0,0 +1,13 @@
+/*
+* Copyright (c) 2025 Google
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/toolchain.h>
+
+BUILD_ASSERT(CONFIG_CPP, "C++ must be enabled");
+
+int main(void)
+{
+	return 0;
+}
