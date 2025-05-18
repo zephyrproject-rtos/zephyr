9d0ff1cd588b1837d242514883db820569d619e8
Muhammed Asif <asifp3104@gmail.com>
2025-05-18 21:57:37 +0530

    samples: Watchdog: Added support for early warning callback
    - Added the changes for initialising a user early warning callback
    
    Signed-off-by: Muhammed Asif <asifp3104@gmail.com>

--- samples/drivers/watchdog/src/main.c
+++ samples/drivers/watchdog/src/main.c
@@ -19,7 +19,7 @@
  * To use this sample the devicetree's /aliases must have a 'watchdog0' property.
  */
 #if DT_HAS_COMPAT_STATUS_OKAY(st_stm32_window_watchdog)
-#define WDT_MAX_WINDOW  100U
+#define WDT_MAX_WINDOW 100U
 #elif DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_wdt)
 /* Nordic supports a callback, but it has 61.2 us to complete before
  * the reset occurs, which is too short for this sample to do anything
@@ -29,16 +29,16 @@
 #elif DT_HAS_COMPAT_STATUS_OKAY(raspberrypi_pico_watchdog)
 #define WDT_ALLOW_CALLBACK 0
 #elif DT_HAS_COMPAT_STATUS_OKAY(gd_gd32_wwdgt)
-#define WDT_MAX_WINDOW 24U
-#define WDT_MIN_WINDOW 18U
+#define WDT_MAX_WINDOW    24U
+#define WDT_MIN_WINDOW    18U
 #define WDG_FEED_INTERVAL 12U
 #elif DT_HAS_COMPAT_STATUS_OKAY(intel_tco_wdt)
 #define WDT_ALLOW_CALLBACK 0
-#define WDT_MAX_WINDOW 3000U
+#define WDT_MAX_WINDOW     3000U
 #elif DT_HAS_COMPAT_STATUS_OKAY(nxp_fs26_wdog)
-#define WDT_MAX_WINDOW  1024U
-#define WDT_MIN_WINDOW	320U
-#define WDT_OPT 0
+#define WDT_MAX_WINDOW    1024U
+#define WDT_MIN_WINDOW    320U
+#define WDT_OPT           0
 #define WDG_FEED_INTERVAL (WDT_MIN_WINDOW + ((WDT_MAX_WINDOW - WDT_MIN_WINDOW) / 4))
 #elif DT_HAS_COMPAT_STATUS_OKAY(renesas_ra_wdt)
 #define WDT_ALLOW_CALLBACK 0
@@ -52,11 +52,11 @@
 #endif
 
 #ifndef WDT_MAX_WINDOW
-#define WDT_MAX_WINDOW  1000U
+#define WDT_MAX_WINDOW 1000U
 #endif
 
 #ifndef WDT_MIN_WINDOW
-#define WDT_MIN_WINDOW  0U
+#define WDT_MIN_WINDOW 0U
 #endif
 
 #ifndef WDG_FEED_INTERVAL
@@ -95,8 +95,7 @@ static void wdt_ew_callback(const struct device *wdt_dev, int channel_id)
 	}
 
 	wdt_feed(wdt_dev, channel_id);
-
-	printk("Fed watchdog after watchdog window opened... \n");
+	printk("Fed watchdog after watchdog window opened...\n");
 	handled_event = true;
 }
 #endif /* WDT_EARLY_WARNING_INTERRUPT_SUPPORT*/
@@ -142,6 +141,9 @@ int main(void)
 		/* IWDG driver for STM32 doesn't support callback */
 		printk("Callback support rejected, continuing anyway\n");
 		wdt_config.callback = NULL;
+#if WDT_EARLY_WARNING_INTERRUPT_SUPPORT
+		wdt_config.ew_callback = NULL;
+#endif /* WDT_EARLY_WARNING_INTERRUPT_SUPPORT*/
 		wdt_channel_id = wdt_install_timeout(wdt, &wdt_config);
 	}
 	if (wdt_channel_id < 0) {
