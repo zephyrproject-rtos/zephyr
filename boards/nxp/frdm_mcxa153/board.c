8cd24d5948026847d415228ac7ee5652bd90c1df
Neil Chen <cheng.chen_1@nxp.com>
2025-05-19 13:53:32 +0800

    boards: nxp: frdm_mcxa153: Support wwdt for NXP frdm_mcxa153 board
    Support watchdog for NXP frdm_mcxa153 board.
    Test it using samples/drivers/watchdog.
    
    Signed-off-by: Neil Chen <cheng.chen_1@nxp.com>

--- boards/nxp/frdm_mcxa153/board.c
+++ boards/nxp/frdm_mcxa153/board.c
@@ -189,6 +189,10 @@ void board_early_init_hook(void)
 	RESET_ReleasePeripheralReset(kPORT3_RST_SHIFT_RSTn);
 #endif
 
+#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(wwdt0))
+	CLOCK_SetClockDiv(kCLOCK_DivWWDT0, 1u);
+#endif
+
 	/* Set SystemCoreClock variable. */
 	SystemCoreClock = CLOCK_INIT_CORE_CLOCK;
 }
