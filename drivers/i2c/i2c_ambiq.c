7f77bda1f98c0599d70a0963aa0d64291a102ec4
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2025-05-20 11:51:58 +0200

    drivers/i2c/i2c_ambiq: Fix code compliance issues
    Fix 4 issues detected by checkpatch
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- drivers/i2c/i2c_ambiq.c
+++ drivers/i2c/i2c_ambiq.c
@@ -124,9 +124,11 @@ static int i2c_ambiq_read(const struct device *dev, struct i2c_msg *hdr_msg,
 			return -E2BIG;
 		}
 #if defined(CONFIG_SOC_SERIES_APOLLO3X)
-		trans.ui32Instr = (*(uint32_t *)hdr_msg->buf) & (0xFFFFFFFF >> (32 - (hdr_msg->len * 8)));
+		trans.ui32Instr = (*(uint32_t *)hdr_msg->buf)
+				   & (0xFFFFFFFFUL >> (32 - (hdr_msg->len * 8)));
 #else
-		trans.ui64Instr = (*(uint64_t *)hdr_msg->buf) & (0xFFFFFFFFFFFFFFFF >> (64 - (hdr_msg->len * 8)));
+		trans.ui64Instr = (*(uint64_t *)hdr_msg->buf)
+				   & (0xFFFFFFFFFFFFFFFFULL >> (64 - (hdr_msg->len * 8)));
 #endif
 		trans.ui32InstrLen = hdr_msg->len;
 	}
@@ -177,9 +179,11 @@ static int i2c_ambiq_write(const struct device *dev, struct i2c_msg *hdr_msg,
 			return -E2BIG;
 		}
 #if defined(CONFIG_SOC_SERIES_APOLLO3X)
-		trans.ui32Instr = (*(uint32_t *)hdr_msg->buf) & (0xFFFFFFFF >> (32 - (hdr_msg->len * 8)));
+		trans.ui32Instr = (*(uint32_t *)hdr_msg->buf)
+				   & (0xFFFFFFFFUL >> (32 - (hdr_msg->len * 8)));
 #else
-		trans.ui64Instr = (*(uint64_t *)hdr_msg->buf) & (0xFFFFFFFFFFFFFFFF >> (64 - (hdr_msg->len * 8)));
+		trans.ui64Instr = (*(uint64_t *)hdr_msg->buf)
+				   & (0xFFFFFFFFFFFFFFFFULL >> (64 - (hdr_msg->len * 8)));
 #endif
 		trans.ui32InstrLen = hdr_msg->len;
 	}
