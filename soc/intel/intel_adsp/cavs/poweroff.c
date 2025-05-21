49caa89e133c19215878086983a02108ca291dc4
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2025-05-21 11:34:43 -0700

    soc: intel: intel_adsp: Add sys_poweroff()
    Add the sys_poweroff() implementation for Intel ADSPs. This will be
    invoked to when the host sends the IPC to transition the DSP to D3.
    Currently, the IPC response for D3 takes ~100ms (measured the ACE15 based
    MTL laptop) because we perform the actions for D3 in the idle context.
    Invoking sys_poweroff() from the IPC context will speed up the transition
    and reduce the transition time to ~1ms resulting in a 100-fold reduction
    compared to the current implementation.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

---
+++ soc/intel/intel_adsp/cavs/poweroff.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/sys/poweroff.h>
+#include <adsp_power.h>
+
+void z_sys_poweroff(void)
+{
+        intel_adsp_power_off();
+
+        CODE_UNREACHABLE;
+}
