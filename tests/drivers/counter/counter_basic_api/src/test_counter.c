ffc2f1d36170e90026579445e6638f817d332e90
Joel Guittet <joelguittet@gmail.com>
2025-05-21 21:43:11 +0200

    drivers: counter: introduce counter node in esp32 timers
    Add counter device tree node to the esp32 timers.
    
    Signed-off-by: Joel Guittet <joelguittet@gmail.com>

--- tests/drivers/counter/counter_basic_api/src/test_counter.c
+++ tests/drivers/counter/counter_basic_api/src/test_counter.c
@@ -103,7 +103,7 @@ static const struct device *const devices[] = {
 	DEVS_FOR_DT_COMPAT(xlnx_xps_timer_1_00_a)
 #endif
 #ifdef CONFIG_COUNTER_TMR_ESP32
-	DEVS_FOR_DT_COMPAT(espressif_esp32_timer)
+	DEVS_FOR_DT_COMPAT(espressif_esp32_counter)
 #endif
 #ifdef CONFIG_COUNTER_NXP_S32_SYS_TIMER
 	DEVS_FOR_DT_COMPAT(nxp_s32_sys_timer)
