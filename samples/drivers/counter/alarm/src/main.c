0133f9baca5233326e34e5783ab657b5a8bd1a09
Joel Guittet <joelguittet@gmail.com>
2025-05-21 21:43:11 +0200

    drivers: counter: fix counter node in esp32 timers
    Fix counter device tree node of the esp32 timers in the counter sample
    and test app.
    
    Signed-off-by: Joel Guittet <joelguittet@gmail.com>

--- samples/drivers/counter/alarm/src/main.c
+++ samples/drivers/counter/alarm/src/main.c
@@ -36,7 +36,7 @@ struct counter_alarm_cfg alarm_cfg;
 #elif defined(CONFIG_COUNTER_XLNX_AXI_TIMER)
 #define TIMER DT_INST(0, xlnx_xps_timer_1_00_a)
 #elif defined(CONFIG_COUNTER_TMR_ESP32)
-#define TIMER DT_NODELABEL(timer0)
+#define TIMER DT_INST(0, espressif_esp32_counter)
 #elif defined(CONFIG_COUNTER_MCUX_CTIMER)
 #define TIMER DT_NODELABEL(ctimer0)
 #elif defined(CONFIG_COUNTER_NXP_S32_SYS_TIMER)
