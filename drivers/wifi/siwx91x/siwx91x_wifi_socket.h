16a5004a3f9595b62cfcd84ee5e8be84abe79269
Jérôme Pouiller <jerome.pouiller@silabs.com>
2025-05-22 13:27:25 +0200

    drivers: wifi: siwx91x: Fix siwx91x_wifi_socket.h syntax
    Functions in siwx91x_wifi_socket.h are not declared static. So it create
    duplicated symbols if the file is included twice.
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- drivers/wifi/siwx91x/siwx91x_wifi_socket.h
+++ drivers/wifi/siwx91x/siwx91x_wifi_socket.h
@@ -21,20 +21,20 @@ void siwx91x_sock_init(struct net_if *iface);
 
 #else /* CONFIG_WIFI_SILABS_SIWX91X_NET_STACK_OFFLOAD */
 
-enum offloaded_net_if_types siwx91x_get_type(void)
+static inline enum offloaded_net_if_types siwx91x_get_type(void)
 {
 	assert(0);
 }
 
-void siwx91x_on_join_ipv4(struct siwx91x_dev *sidev)
+static inline void siwx91x_on_join_ipv4(struct siwx91x_dev *sidev)
 {
 }
 
-void siwx91x_on_join_ipv6(struct siwx91x_dev *sidev)
+static inline void siwx91x_on_join_ipv6(struct siwx91x_dev *sidev)
 {
 }
 
-void siwx91x_sock_init(struct net_if *iface)
+static inline void siwx91x_sock_init(struct net_if *iface)
 {
 }
 
