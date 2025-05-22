55361b2241ffd33d9506ecf6849e734904f27666
Jérôme Pouiller <jerome.pouiller@silabs.com>
2025-05-22 14:08:57 +0200

    drivers: wifi: siwx91x: Align the ways errors are managed
    Unify the way the error code are tested:
      - sl_status_t is an alias to an int. The compiler won't complain if an
        int is assigned or compared to a sl_status_t. In the other hand,
        "int" is well known to manage error code. So, just use "int"
        everywhere.
      - Always name the return code "ret"
      - Unless it is meaningful, test the value of ret against 0
        (SL_STATUS_OK is just a convoluted way to say 0).
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- drivers/wifi/siwx91x/siwx91x_wifi_socket.c
+++ drivers/wifi/siwx91x/siwx91x_wifi_socket.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2023 Antmicro
- * Copyright (c) 2024 Silicon Laboratories Inc.
+ * Copyright (c) 2024-2025 Silicon Laboratories Inc.
  * SPDX-License-Identifier: Apache-2.0
  */
 #include <zephyr/net/net_offload.h>
