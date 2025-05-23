7b7c17f119fa913b570cab4d9b6785a5c104bc67
Emil Lindqvist <emil@lindq.gr>
2025-05-23 12:11:24 +0200

    modem_cellular: lara_r6: disable modem's own LWM2M client
    U-blox LARA-R6 LWM2M client is enabled by default. Not only causes this
    the modem to connect to U-blox's server on its own, it also for some
    reason causes the modem to reply "Destination unreachable" to DNS answers
    from DNS requests that we send
    
    Signed-off-by: Emil Lindqvist <emil@lindq.gr>

--- drivers/modem/modem_cellular.c
+++ drivers/modem/modem_cellular.c
@@ -2178,6 +2178,11 @@ MODEM_CHAT_SCRIPT_DEFINE(u_blox_lara_r6_set_baudrate_chat_script,
  * which works well
  */
 MODEM_CHAT_SCRIPT_CMDS_DEFINE(u_blox_lara_r6_init_chat_script_cmds,
+			      /* Nope, absolutely not, do not connect to anything on your own.
+			       * In addition, LWM2M causes our own DNS request answer to not
+			       * reach us
+			       */
+			      MODEM_CHAT_SCRIPT_CMD_RESP_MULT("AT+ULWM2M=1", allow_match),
 			      MODEM_CHAT_SCRIPT_CMD_RESP("AT+CFUN=4", ok_match),
 			      MODEM_CHAT_SCRIPT_CMD_RESP("AT+CMEE=1", ok_match),
 			      MODEM_CHAT_SCRIPT_CMD_RESP("AT+CREG=1", ok_match),
