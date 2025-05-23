c6629e3cab1dd00ab221783e65d79f6fb3ff76b5
Rahul Gurram <rahul.gurram@silabs.com>
2025-05-23 14:44:03 +0530

    drivers: wifi: siwx91x: DNS NetStack Application
    To support the SiWx917 DNS Network Stack Application
    protocol offloading, we need to integrate the Silicon Labs
    APIs, so adding the corresponding source files and include
    files in the kconfig and CMakelist
    
    Signed-off-by: Rahul Gurram <rahul.gurram@silabs.com>

--- soc/silabs/silabs_siwx91x/siwg917/nwp.c
+++ soc/silabs/silabs_siwx91x/siwg917/nwp.c
@@ -139,6 +139,9 @@ static void siwx91x_configure_network_stack(sl_si91x_boot_configuration_t *boot_
 	if (IS_ENABLED(CONFIG_WIFI_SILABS_SIWX91X_MDNS_RESPONDER)) {
 		boot_config->tcp_ip_feature_bit_map |= SL_SI91X_TCP_IP_FEAT_MDNSD;
 	}
+	if (IS_ENABLED(CONFIG_WIFI_SILABS_SIWX91X_DNS_CLIENT)) {
+		boot_config->tcp_ip_feature_bit_map |= SL_SI91X_TCP_IP_FEAT_DNS_CLIENT;
+	}
 	if (IS_ENABLED(CONFIG_WIFI_SILABS_SIWX91X_MQTT_CLIENT)) {
 		boot_config->ext_tcp_ip_feature_bit_map |= SL_SI91X_EXT_EMB_MQTT_ENABLE;
 	}
