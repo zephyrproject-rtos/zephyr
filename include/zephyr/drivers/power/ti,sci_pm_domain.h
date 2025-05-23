4be3febcf3d8d2f40d979ab6f99fe1a23417b930
Dave Joseph <d-joseph@ti.com>
2025-05-23 15:03:38 +0530

    drivers: power_domain: Power domain TISCI driver support
    Support added for power domain regulation using TISCI added for
    devices using the binding ti,sci-pm-domain.
    This driver relies on the TISCI layer to make calls to the
    device manager core to perform power management.
    
    Signed-off-by: Dave Joseph <d-joseph@ti.com>

---
+++ include/zephyr/drivers/power/ti,sci_pm_domain.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025 Texas Instruments Incorporated
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __DT_BINDINGS_TISCI_PM_DOMAIN_H
+#define __DT_BINDINGS_TISCI_PM_DOMAIN_H
+
+#define TISCI_PD_EXCLUSIVE 1
+#define TISCI_PD_SHARED    0
+
+#endif /* __DT_BINDINGS_TISCI_PM_DOMAIN_H */
