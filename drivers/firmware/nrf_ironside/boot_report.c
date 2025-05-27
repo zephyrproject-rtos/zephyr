d6fc5db59991b481654c8350b37b244c27aa8b84
Håkon Amundsen <haakon.amundsen@nordicsemi.no>
2025-05-27 07:13:02 +0200

    drivers: firmware: add IRONside boot report
    The boot report is used to pass information from IRONside to a CPU
    being booted.
    
    Integrate the boot report to the update service sample.
    
    Signed-off-by: Håkon Amundsen <haakon.amundsen@nordicsemi.no>

---
+++ drivers/firmware/nrf_ironside/boot_report.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <errno.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/firmware/nrf_ironside/boot_report.h>
+
+#define IRONSIDE_SE_BOOT_REPORT_ADDR DT_REG_ADDR(DT_NODELABEL(ironside_se_boot_report))
+
+int ironside_boot_report_get(const struct ironside_boot_report **report)
+{
+	const struct ironside_boot_report *tmp_report = (void *)IRONSIDE_SE_BOOT_REPORT_ADDR;
+
+	if (tmp_report->magic != IRONSIDE_BOOT_REPORT_MAGIC) {
+		return -EINVAL;
+	}
+
+	*report = tmp_report;
+
+	return 0;
+}
