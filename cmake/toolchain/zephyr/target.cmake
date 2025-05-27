1d88edcac9d0d41e7604f04e145fab1bdc0d86da
Anas Nashif <anas.nashif@intel.com>
2025-05-26 22:12:59 -0400

    cmake: add support enhanced variants with multiple compilers
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- cmake/toolchain/zephyr/target.cmake
+++ cmake/toolchain/zephyr/target.cmake
@@ -1,3 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
-include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/target.cmake)
+if(TOOLCHAIN_VARIANT_COMPILER STREQUAL "gnu" OR
+   TOOLCHAIN_VARIANT_COMPILER STREQUAL "default")
+  set(TOOLCHAIN_VARIANT_COMPILER gnu CACHE STRING "Variant compiler being used")
+  include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/gnu/target.cmake)
+elseif (TOOLCHAIN_VARIANT_COMPILER STREQUAL "llvm")
+  set(TOOLCHAIN_VARIANT_COMPILER llvm CACHE STRING "Variant compiler being used")
+  include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/llvm/target.cmake)
+else()
+  message(FATAL_ERROR "Unsupported TOOLCHAIN_VARIANT_COMPILER: ${TOOLCHAIN_VARIANT_COMPILER}")
+endif()
