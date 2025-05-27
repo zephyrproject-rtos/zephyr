a241b637f57207e1dc334fb71690fc11b6d533fe
Anas Nashif <anas.nashif@intel.com>
2025-05-26 22:12:59 -0400

    cmake: toolchain: support multiple compiler types for Zephyr SDK
    Support specifying compiler type for Zephyr SDK in the
    ZEPHYR_TOOLCHAIN_VARIANT variable.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- cmake/toolchain/zephyr/generic.cmake
+++ cmake/toolchain/zephyr/generic.cmake
@@ -1,10 +1,21 @@
 # SPDX-License-Identifier: Apache-2.0
 
-include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/generic.cmake)
+if(TOOLCHAIN_VARIANT_COMPILER STREQUAL "gnu" OR
+    NOT DEFINED TOOLCHAIN_VARIANT_COMPILER)
+  include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/gnu/generic.cmake)
+  set(TOOLCHAIN_VARIANT_COMPILER "gnu" CACHE STRING "compiler used by the toolchain variant" FORCE)
 
-set(TOOLCHAIN_KCONFIG_DIR ${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr)
+  set(TOOLCHAIN_KCONFIG_DIR ${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr)
+  # Zephyr SDK < 0.17.1 does not set TOOLCHAIN_HAS_GLIBCXX
+  set(TOOLCHAIN_HAS_GLIBCXX ON CACHE BOOL "True if toolchain supports libstdc++")
+  message(STATUS "Found toolchain: zephyr ${SDK_VERSION} (${ZEPHYR_SDK_INSTALL_DIR})")
+elseif(TOOLCHAIN_VARIANT_COMPILER STREQUAL "llvm")
+  include(${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr/llvm/generic.cmake)
+  set(TOOLCHAIN_VARIANT_COMPILER "llvm" CACHE STRING "compiler used by the toolchain variant" FORCE)
 
-# Zephyr SDK < 0.17.1 does not set TOOLCHAIN_HAS_GLIBCXX
-set(TOOLCHAIN_HAS_GLIBCXX ON CACHE BOOL "True if toolchain supports libstdc++")
+  set(TOOLCHAIN_KCONFIG_DIR ${ZEPHYR_SDK_INSTALL_DIR}/cmake/zephyr)
 
-message(STATUS "Found toolchain: zephyr-sdk-gnu ${SDK_VERSION} (${ZEPHYR_SDK_INSTALL_DIR})")
+  message(STATUS "Found toolchain: zephyr ${SDK_VERSION} (${ZEPHYR_SDK_INSTALL_DIR})")
+else()
+  message(FATAL_ERROR "Unsupported TOOLCHAIN_VARIANT_COMPILER: ${TOOLCHAIN_VARIANT_COMPILER}")
+endif()
