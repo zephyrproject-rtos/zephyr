69fed9c483a5c85337fe80fc7c547ec38c106c8e
Rahul Gurram <rahul.gurram@silabs.com>
2025-05-27 10:10:27 +0530

    drivers: wifi: siwx91x: Muliple TLS version
    To support the SiWx917 Multiple TLS version,
    Need to enable the SSL version bitmap
    in the intilization
    
    Signed-off-by: Rahul Gurram <rahul.gurram@silabs.com>

--- soc/silabs/silabs_siwx91x/siwg917/nwp.c
+++ soc/silabs/silabs_siwx91x/siwg917/nwp.c
@@ -158,6 +158,10 @@ static void siwx91x_configure_network_stack(sl_si91x_boot_configuration_t *boot_
 		boot_config->ext_tcp_ip_feature_bit_map |=
 			SL_SI91X_EXT_TCP_IP_FEAT_SSL_THREE_SOCKETS;
 	}
+	if (IS_ENABLED(CONFIG_WIFI_SILABS_SIWX91X_SSL_VERSIONS_SUPPORT)) {
+		boot_config->ext_custom_feature_bit_map |=
+			SL_SI91X_EXT_FEAT_SSL_VERSIONS_SUPPORT;
+	}
 	boot_config->ext_tcp_ip_feature_bit_map |= SL_SI91X_EXT_TCP_IP_WINDOW_SCALING;
 	boot_config->ext_tcp_ip_feature_bit_map |= SL_SI91X_EXT_TCP_IP_TOTAL_SELECTS(10);
 
