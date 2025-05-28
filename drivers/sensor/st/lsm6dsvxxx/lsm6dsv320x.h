9d2f90dd15cb733af6701163a65527601dd38a5c
Armando Visconti <armando.visconti@st.com>
2025-05-28 17:33:27 +0200

    drivers/sensor/st: add support to LSM6DSVXXX IMU family
    This driver is currently only supporting the polling-mode read_and_decode
    APIs (both blocking and non-blocking).
    
    The driver implements a chip_api structure which has to be used to
    provide device specific callbacks. The only lsm6dsvxxx family device
    currently supported is lsm6dsv320x.
    
    More information about LSM6DSV16X:
    https://www.st.com/resource/en/datasheet/lsm6dsv320x.pdf
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

---
+++ drivers/sensor/st/lsm6dsvxxx/lsm6dsv320x.h
@@ -0,0 +1,23 @@
+/* ST Microelectronics LSM6DSVXXX family IMU sensor
+ *
+ * Copyright (c) 2025 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Datasheet:
+ * https://www.st.com/resource/en/datasheet/lsm6dsv320x.pdf
+ */
+#ifndef ZEPHYR_DRIVERS_SENSOR_LSM6DSV320X_H_
+#define ZEPHYR_DRIVERS_SENSOR_LSM6DSV320X_H_
+
+#include <stdint.h>
+#include <stmemsc.h>
+
+#include "lsm6dsvxxx.h"
+#include "lsm6dsv320x_reg.h"
+
+#include <zephyr/drivers/sensor.h>
+
+extern const struct lsm6dsvxxx_chip_api st_lsm6dsv320x_chip_api;
+
+#endif /* ZEPHYR_DRIVERS_SENSOR_LSM6DSV320X_H_ */
