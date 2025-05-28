5da61875c57cb67108cd2e33fcc4aaf949ca2145
Mihira Madhava Bollapragada <madhava@ti.com>
2025-05-28 11:01:15 +0530

    boards/soc: ti: am261x: add AM261x LaunchPad support
    Complete support for TI AM261x LaunchPad with AM2612 SoC including:
    
    - Board definition and configuration files
    - Device tree support for AM261x family and AM2612
    - SoC support under new ti/am2x directory structure
    - Kconfig integration for AM2x family
    - Add logic to free MSS_RCM registers to allow RTI clock config
    
    NOTE about Davinci GPIO in dts:
    Davinci GPIO currently supports only 32 pins. the AM261x GPIO consists
    of 144 pins. these pins are organised in 9 banks with each 16 pins.
    Also, these 16 bit banks are grouped into 32 bit ones, resulting in 5
    groups, banks(0,1), banks(2,3), banks(4,5), bank(6,7) and bank(8).
    
    Hence, the current implementation splits each GPIO into these
    bank-groups which are also referred as offsets in the Linux world.
    
    Calculations to see which bank and which pin to be used may be done
    like, group = pin /32, pin_number = pin % 32.
    
    Signed-off-by: Mihira Madhava Bollapragada <madhava@ti.com>
    Signed-off-by: Shreyas Shankar <s-shankar@ti.com>

---
+++ soc/ti/am26x/am261x/r5/soc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025 Texas Instruments Incorporated
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _TI_AM261X_SOC_H_
+#define _TI_AM261X_SOC_H_
+
+#include <zephyr/drivers/interrupt_controller/intc_vim.h>
+
+#endif /* _TI_AM261X_SOC_H_ */
