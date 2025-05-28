4d7e3445769290e5665d9ca4dd9cf8e74afeb7b9
Axel Le Bourhis <axel.lebourhis@nxp.com>
2025-05-28 17:09:37 +0200

    [nxp noup] soc: nxp: rw: enable els_pkc crypto acceleration for ble applications
    Now that the bluetooth host switched to mbedtls and psa for crypto,
    we can enable the els_pkc crypto acceleration on this platform.
    
    Signed-off-by: Axel Le Bourhis <axel.lebourhis@nxp.com>

---
+++ soc/nxp/rw/ble_els_pkc_mbedtls_psa_config.h
@@ -0,0 +1,13 @@
+/*!
+ * Copyright 2025 NXP
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef ELS_PKC_MBEDTLS_PSA_CONFIG_H
+#define ELS_PKC_MBEDTLS_PSA_CONFIG_H
+
+#define PSA_CRYPTO_DRIVER_ELS_PKC
+#define MBEDTLS_PSA_CRYPTO_DRIVERS
+
+#endif /* ELS_PKC_MBEDTLS_PSA_CONFIG_H */
