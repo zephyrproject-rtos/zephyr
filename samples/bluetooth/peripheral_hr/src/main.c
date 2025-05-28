c0b900a0bf46879f5c29b46d2f9ef2f8a089543d
Stefan Jaritz <stefan.jaritz@gmail.com>
2025-05-28 20:17:11 +0200

    samples: bluetooth: fix advertising type for ext adv
    fix:
    	- different adv variables for normal and ext. adv
    
    Signed-off-by: Stefan Jaritz <stefan.jaritz@gmail.com>

--- samples/bluetooth/peripheral_hr/src/main.c
+++ samples/bluetooth/peripheral_hr/src/main.c
@@ -33,6 +33,18 @@ static const struct bt_data ad[] = {
 static const struct bt_data sd[] = {
 	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
 };
+
+static struct bt_le_adv_param adv_param = {
+	.id = BT_ID_DEFAULT,
+	.sid = 0U,
+	.secondary_max_skip = 0U,
+	.options = (BT_LE_ADV_OPT_EXT_ADV | BT_LE_ADV_OPT_CONN | BT_LE_ADV_OPT_CODED),
+	.interval_min = BT_GAP_ADV_FAST_INT_MIN_2,
+	.interval_max = BT_GAP_ADV_FAST_INT_MAX_2,
+	.peer = NULL,
+};
+
+static struct bt_le_ext_adv *adv;
 #endif /* !CONFIG_BT_EXT_ADV */
 
 /* Use atomic variable, 2 bits for connection and disconnection state */
@@ -205,10 +217,9 @@ static int start_adv(void)
 		printk("Advertising failed to start (err %d)\n", err);
 		return 0;
 	}
-
 #else  /* CONFIG_BT_EXT_ADV */
 	printk("Starting Extended Advertising (connectable and non-scannable)\n");
-	err = bt_le_ext_adv_start(ad, BT_LE_EXT_ADV_START_DEFAULT);
+	err = bt_le_ext_adv_start(adv, BT_LE_EXT_ADV_START_DEFAULT);
 	if (err) {
 		printk("Failed to start extended advertising set (err %d)\n", err);
 		return 0;
@@ -243,17 +254,6 @@ int main(void)
 	}
 
 #else  /* CONFIG_BT_EXT_ADV */
-	struct bt_le_adv_param adv_param = {
-		.id = BT_ID_DEFAULT,
-		.sid = 0U,
-		.secondary_max_skip = 0U,
-		.options = (BT_LE_ADV_OPT_EXT_ADV | BT_LE_ADV_OPT_CONN | BT_LE_ADV_OPT_CODED),
-		.interval_min = BT_GAP_ADV_FAST_INT_MIN_2,
-		.interval_max = BT_GAP_ADV_FAST_INT_MAX_2,
-		.peer = NULL,
-	};
-	struct bt_le_ext_adv *adv;
-
 	printk("Creating a Coded PHY connectable non-scannable advertising set\n");
 	err = bt_le_ext_adv_create(&adv_param, NULL, &adv);
 	if (err) {
