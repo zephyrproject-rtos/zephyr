19af3cf53098543f5a88cce7d89a924b49fa7944
Grzegorz Chwierut <grzegorz.chwierut@nordicsemi.no>
2025-05-29 15:49:29 +0200

    [nrf fromtree] west: runners: Add ncs-provision to west flash command
    Added automatic KMU key provisioning, when keyfile.json
    file exists in the build directory.
    This enables automated key provisioning during the
    flashing process to enable testing nRF54L aplications using Twister.
    Only applicable on nrfutil runner.
    
    Signed-off-by: Grzegorz Chwierut <grzegorz.chwierut@nordicsemi.no>
    (cherry picked from commit db3c344af9eb395a3dcd65e623d1b12436fefb5e)

--- scripts/west_commands/runners/nrfutil.py
+++ scripts/west_commands/runners/nrfutil.py
@@ -147,6 +147,8 @@ class NrfUtilBinaryRunner(NrfBinaryRunner):
             cmd += ['--reset-kind', _op['kind']]
         elif op_type == 'erase':
             cmd.append(f'--{_op["kind"]}')
+        elif op_type == 'x-provision-keys':
+            cmd += ['--key-file', _op['keyfile']]
 
         cmd += ['--core', op['core']] if op.get('core') else []
         cmd += ['--x-family', f'{self.family}']
