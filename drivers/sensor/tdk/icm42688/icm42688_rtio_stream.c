13c3ab7cda00375e7865f1324fe2781ab4554ac3
Benjamin Perseghetti <bperseghetti@rudislabs.com>
2025-05-29 15:22:51 -0400

    Temp commits rebased.
    Signed-off-by: Benjamin Perseghetti <bperseghetti@rudislabs.com>

--- drivers/sensor/tdk/icm42688/icm42688_rtio_stream.c
+++ drivers/sensor/tdk/icm42688/icm42688_rtio_stream.c
@@ -149,8 +149,11 @@ static void icm42688_fifo_count_cb(struct rtio *r, const struct rtio_sqe *sqe, v
 	 * result
 	 */
 	struct rtio_sqe *write_fifo_addr = rtio_sqe_acquire(r);
+	__ASSERT_NO_MSG(write_fifo_addr != NULL);
 	struct rtio_sqe *read_fifo_data = rtio_sqe_acquire(r);
+	__ASSERT_NO_MSG(read_fifo_data != NULL);
 	struct rtio_sqe *complete_op = rtio_sqe_acquire(r);
+	__ASSERT_NO_MSG(complete_op != NULL);
 	const uint8_t reg_addr = REG_SPI_READ_BIT | FIELD_GET(REG_ADDRESS_MASK, REG_FIFO_DATA);
 
 	rtio_sqe_prep_tiny_write(write_fifo_addr, spi_iodev, RTIO_PRIO_NORM, &reg_addr, 1, NULL);
@@ -172,6 +175,7 @@ icm42688_get_read_config_trigger(const struct sensor_read_config *cfg,
 			return &cfg->triggers[i];
 		}
 	}
+	LOG_DBG("Unsupported trigger (%d)", trig);
 	return NULL;
 }
 
@@ -199,12 +203,12 @@ static void icm42688_int_status_cb(struct rtio *r, const struct rtio_sqe *sqr, v
 	struct sensor_stream_trigger *fifo_ths_cfg =
 		icm42688_get_read_config_trigger(read_config, SENSOR_TRIG_FIFO_WATERMARK);
 	bool has_fifo_ths_trig = fifo_ths_cfg != NULL &&
-				 FIELD_GET(BIT_INT_STATUS_FIFO_THS, drv_data->int_status) != 0;
+				 FIELD_GET(BIT_FIFO_THS_INT, drv_data->int_status) != 0;
 
 	struct sensor_stream_trigger *fifo_full_cfg =
 		icm42688_get_read_config_trigger(read_config, SENSOR_TRIG_FIFO_FULL);
 	bool has_fifo_full_trig = fifo_full_cfg != NULL &&
-				  FIELD_GET(BIT_INT_STATUS_FIFO_FULL, drv_data->int_status) != 0;
+				  FIELD_GET(BIT_FIFO_FULL_INT, drv_data->int_status) != 0;
 
 	if (!has_fifo_ths_trig && !has_fifo_full_trig) {
 		LOG_DBG("No FIFO trigger is configured");
