zephyrproject-rtos/zephyr

commit c31640239c7f3c50908a099f0f7cbfb64b458b8c

soc: nxp: s32k3: fix erratum ERR011573
Due to erratum ERR011573, speculative accesses might be performed
to normal memory unmapped in the MPU. This can be avoided by using
MPU region 0 to cover all unmapped memory and make this region
execute-never and inaccessible.

Fixes #89852

Co-authored-by: Peter van der Perk <peter.vanderperk@nxp.com>
Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

soc/nxp/s32/s32k3/mpu_regions.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 NXP
+ * Copyright 2023, 2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -13,6 +13,13 @@ extern char _rom_attr[];
 #endif
 
 static struct arm_mpu_region mpu_regions[] = {
+
+	/* ERR011573: use first region to prevent speculative access in entire memory space */
+	{
+		.name = "BACKGROUND",
+		.base = 0,
+		.attr = {REGION_4G | MPU_RASR_XN_Msk | P_NA_U_NA_Msk},
+	},
 
 	/* Keep before CODE region so it can be overlapped by SRAM CODE in non-XIP systems */
 	{

commit 5866d6e7edc5bffd0803cabb1879c9bf4223f61b

soc: nxp: s32k3: configure missing mpu regions
This fixes a regression introduced in c31640239c7 where all regions
except Flash and RAM where left unmapped. Before introducing region
0 that prevents speculative access to the entire memory space, we
were relying on the architectural background map to access them.

Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

dts/arm/nxp/nxp_s32k344_m7.dtsi
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 NXP
+ * Copyright 2023-2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -8,6 +8,7 @@
 #include <mem.h>
 #include <zephyr/dt-bindings/clock/nxp_s32k344_clock.h>
 #include <zephyr/dt-bindings/i2c/i2c.h>
+#include <zephyr/dt-bindings/memory-attr/memory-attr-arm.h>
 
 / {
 	cpus {
@@ -45,12 +46,14 @@
 			compatible = "zephyr,memory-region", "arm,itcm";
 			reg = <0x00000000 DT_SIZE_K(64)>;
 			zephyr,memory-region = "ITCM";
+			zephyr,memory-attr = <( DT_MEM_ARM(ATTR_MPU_FLASH) )>;
 		};
 
 		dtcm: memory@20000000 {
 			compatible = "zephyr,memory-region", "arm,dtcm";
 			reg = <0x20000000 DT_SIZE_K(128)>;
 			zephyr,memory-region = "DTCM";
+			zephyr,memory-attr = <( DT_MEM_ARM(ATTR_MPU_RAM) )>;
 		};
 
 		sram0_1: sram0_1@20400000 {

soc/nxp/s32/s32k3/mpu_regions.c
@@ -12,11 +12,16 @@
 extern char _rom_attr[];
 #endif
 
+#define REGION_PERIPHERAL_BASE_ADDRESS 0x40000000
+#define REGION_PERIPHERAL_SIZE         REGION_512M
+#define REGION_PPB_BASE_ADDRESS        0xE0000000
+#define REGION_PPB_SIZE                REGION_1M
+
 static struct arm_mpu_region mpu_regions[] = {
 
 	/* ERR011573: use first region to prevent speculative access in entire memory space */
 	{
-		.name = "BACKGROUND",
+		.name = "UNMAPPED",
 		.base = 0,
 		.attr = {REGION_4G | MPU_RASR_XN_Msk | P_NA_U_NA_Msk},
 	},
@@ -42,6 +47,18 @@ static struct arm_mpu_region mpu_regions[] = {
 		.attr = {(uint32_t)_rom_attr},
 	},
 #endif
+
+	{
+		.name = "PERIPHERALS",
+		.base = REGION_PERIPHERAL_BASE_ADDRESS,
+		.attr = REGION_IO_ATTR(REGION_PERIPHERAL_SIZE),
+	},
+
+	{
+		.name = "PPB",
+		.base = REGION_PPB_BASE_ADDRESS,
+		.attr = REGION_PPB_ATTR(REGION_PPB_SIZE),
+	},
 };
 
 const struct arm_mpu_config mpu_config = {