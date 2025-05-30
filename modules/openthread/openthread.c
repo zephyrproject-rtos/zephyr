25682feeb55d8707a98bd01debbbc1b83218eb75
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2025-05-30 13:09:12 +0200

    openthread: Fix OpenThread independence from Zephyr network.
    If CONFIG_OPENTHREAD_SYS_INIT is enabled, OpenThread initialisation
    should also consider running OpenThread automatically if
    CONFIG_OPENTHREAD_MANUAL_START is disabled.
    
    Removed also dependency on the `net_bytes_from_str` functions from
    the openthread.h file. Now, in the OpenThread module, there is an
    additional `openthread_utils.c/.h` file that can implement useful
    utilities for the OpenThread platform. Currently, it implements
    the `bytes_from_str` function to use it instead of
    `net_bytes_from_str`.
    
    Signed-off-by: Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>

--- modules/openthread/openthread.c
+++ modules/openthread/openthread.c
@@ -21,6 +21,7 @@ LOG_MODULE_REGISTER(net_openthread_platform, CONFIG_OPENTHREAD_PLATFORM_LOG_LEVE
 #include "platform/platform-zephyr.h"
 
 #include <openthread.h>
+#include <openthread_utils.h>
 
 #include <openthread/child_supervision.h>
 #include <openthread/cli.h>
@@ -182,7 +183,10 @@ static bool ot_setup_default_configuration(void)
 		return false;
 	}
 
-	net_bytes_from_str(xpanid.m8, 8, (char *)OT_XPANID);
+	if (bytes_from_str(xpanid.m8, 8, (char *)OT_XPANID) != 0) {
+		LOG_ERR("Failed to parse extended PAN ID");
+		return false;
+	}
 	error = otThreadSetExtendedPanId(openthread_instance, &xpanid);
 	if (error != OT_ERROR_NONE) {
 		LOG_ERR("Failed to set %s [%d]", "ext PAN ID", error);
@@ -190,7 +194,11 @@ static bool ot_setup_default_configuration(void)
 	}
 
 	if (strlen(OT_NETWORKKEY)) {
-		net_bytes_from_str(networkKey.m8, OT_NETWORK_KEY_SIZE, (char *)OT_NETWORKKEY);
+		if (bytes_from_str(networkKey.m8, OT_NETWORK_KEY_SIZE, (char *)OT_NETWORKKEY) !=
+		    0) {
+			LOG_ERR("Failed to parse network key");
+			return false;
+		}
 		error = otThreadSetNetworkKey(openthread_instance, &networkKey);
 		if (error != OT_ERROR_NONE) {
 			LOG_ERR("Failed to set %s [%d]", "network key", error);
@@ -485,5 +493,18 @@ void openthread_mutex_unlock(void)
 }
 
 #ifdef CONFIG_OPENTHREAD_SYS_INIT
-SYS_INIT(openthread_init, POST_KERNEL, CONFIG_OPENTHREAD_SYS_INIT_PRIORITY);
+static int openthread_sys_init(void)
+{
+	int error = openthread_init();
+
+	if (error == 0) {
+#ifndef CONFIG_OPENTHREAD_MANUAL_START
+		error = openthread_run();
+#endif
+	}
+
+	return error;
+}
+
+SYS_INIT(openthread_sys_init, POST_KERNEL, CONFIG_OPENTHREAD_SYS_INIT_PRIORITY);
 #endif /* CONFIG_OPENTHREAD_SYS_INIT */
