2c6ac88af6156a76ca2e5436bf82b3bb4bba86e3
Armando Visconti <armando.visconti@st.com>
2025-06-05 18:37:33 +0200

    drivers/sensor: lsm6dsvxxx: add streaming capabality
    Add read_and_decode streaming APIs support.
    
    Triggers supported:
        - SENSOR_TRIG_FIFO_WATERMARK
        - SENSOR_TRIG_FIFO_FULL
        - SENSOR_TRIG_DATA_READY
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

--- drivers/sensor/st/lsm6dsvxxx/lsm6dsv320x.h
+++ drivers/sensor/st/lsm6dsvxxx/lsm6dsv320x.h
@@ -19,5 +19,7 @@
 #include <zephyr/drivers/sensor.h>
 
 extern const struct lsm6dsvxxx_chip_api st_lsm6dsv320x_chip_api;
+extern const int8_t st_lsm6dsv320x_accel_bit_shift[];
+extern const int32_t st_lsm6dsv320x_accel_scaler[];
 
 #endif /* ZEPHYR_DRIVERS_SENSOR_LSM6DSV320X_H_ */
