88d5b16eae39ecf23bafa2fef7880182601df7cf
Wenbin Zhang <freey7955@gmail.com>
2025-06-06 23:10:34 +0800

    drivers: spi_ll_stm32: Add LOG to indicate that DMA cannot be enabled
    Add LOG to indicate the reason why DMA cannot be enabled
    
    Signed-off-by: Wenbin Zhang <freey7955@gmail.com>

--- drivers/spi/spi_ll_stm32.c
+++ drivers/spi/spi_ll_stm32.c
@@ -1121,6 +1121,8 @@ static int transceive_dma(const struct device *dev,
 #ifdef CONFIG_DCACHE
 	if ((tx_bufs != NULL && !spi_buf_set_in_nocache(tx_bufs)) ||
 		(rx_bufs != NULL && !spi_buf_set_in_nocache(rx_bufs))) {
+		LOG_ERR("SPI buffers are not in non-cacheable memory,"
+			" SPI DMA transfer is not supported");
 		return -EFAULT;
 	}
 #endif /* CONFIG_DCACHE */
