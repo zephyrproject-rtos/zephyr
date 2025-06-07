40189787abae2d7fa485466f1c67bf675c5b9452
Russ Webber <russ@sentryrobotics.au>
2025-06-07 14:50:16 +1000

    drivers: sensors: as5600: change to raw reading
    Changed to use the raw reading rather than the scaled value, as that's
    what the angle calculation is expecting. Added log messages that
    communicate magnet sense state.
    
    Signed-off-by: Russ Webber <russ@sentryrobotics.au>

--- drivers/sensor/ams/ams_as5600/ams_as5600.c
+++ drivers/sensor/ams/ams_as5600/ams_as5600.c
@@ -11,15 +11,24 @@
 #include <zephyr/sys/util.h>
 #include <zephyr/device.h>
 #include <zephyr/init.h>
+#include <zephyr/sys/byteorder.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/logging/log.h>
+
 LOG_MODULE_REGISTER(ams_as5600, CONFIG_SENSOR_LOG_LEVEL);
 
-#define AS5600_ANGLE_REGISTER_H 0x0E
+#define AS5600_ANGLE_REGISTER_H	0x0E
+#define AS5600_ANGLE_REGISTER_RAW_H	0x0C
+#define AS5600_STATUS_REGISTER	0x0B
 #define AS5600_FULL_ANGLE		360
 #define AS5600_PULSES_PER_REV	4096
 #define AS5600_MILLION_UNIT	1000000
+
+#define AS5600_STATUS_MH_BIT	(3) /* Magnet too strong */
+#define AS5600_STATUS_ML_BIT	(4) /* Magnet too weak */
+#define AS5600_STATUS_MD_BIT	(5) /* Magnet detected */
+
 
 struct as5600_dev_cfg {
 	struct i2c_dt_spec i2c_port;
@@ -35,18 +44,43 @@ static int as5600_fetch(const struct device *dev, enum sensor_channel chan)
 	struct as5600_dev_data *dev_data = dev->data;
 	const struct as5600_dev_cfg *dev_cfg = dev->config;
 
-	uint8_t read_data[2] = {0, 0};
-	uint8_t angle_reg = AS5600_ANGLE_REGISTER_H;
+	uint8_t status;
+	int err = i2c_reg_read_byte_dt(&dev_cfg->i2c_port,
+					AS5600_STATUS_REGISTER,
+					&status);
 
-	int err = i2c_write_read_dt(&dev_cfg->i2c_port,
-						&angle_reg,
-						1,
-						&read_data,
-						sizeof(read_data));
+	if (err != 0) {
+		LOG_ERR("Failed to read status register: %d", err);
+		return err;
+	}
+
+	/* Check if the magnet is present */
+	if (!(status & BIT(AS5600_STATUS_MD_BIT))) {
+		LOG_WRN("Magnet not detected.");
+		return -ENODATA;
+	}
+
+	/* Check if the magnet is too strong or too weak */
+	if (status & BIT(AS5600_STATUS_MH_BIT)) {
+		LOG_WRN("Magnet too strong.");
+		return -ENODATA;
+	}
+
+	if (status & BIT(AS5600_STATUS_ML_BIT)) {
+		LOG_WRN("Magnet too weak.");
+		return -ENODATA;
+	}
+
+	uint8_t buffer[2] = {0, 0};
+
+	err = i2c_burst_read_dt(&dev_cfg->i2c_port,
+				AS5600_ANGLE_REGISTER_RAW_H,
+				(uint8_t *) &buffer,
+				sizeof(buffer));
 
 	/* invalid readings preserves the last good value */
 	if (!err) {
-		dev_data->position = ((uint16_t)read_data[0] << 8) | read_data[1];
+		dev_data->position = sys_get_be16(buffer);
 	}
 
 	return err;
