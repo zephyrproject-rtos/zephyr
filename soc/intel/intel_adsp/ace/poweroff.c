9472b3a9536b2cddf58e31b9e6e0828ada358b82
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2025-06-09 13:40:18 -0700

    soc: intel: intel_adsp: poweroff: Fix power off
    Allow a system resume in case of IMR context restore.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- soc/intel/intel_adsp/ace/poweroff.c
+++ soc/intel/intel_adsp/ace/poweroff.c
@@ -5,6 +5,7 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/pm/pm.h>
 #include <zephyr/sys/poweroff.h>
 
 #include <adsp_power.h>
@@ -13,5 +14,8 @@ void z_sys_poweroff(void)
 {
         intel_adsp_power_off();
 
-        CODE_UNREACHABLE;
+/* if IMR restore is enabled, we should perform a system resume */
+#ifdef CONFIG_ADSP_IMR_CONTEXT_SAVE
+        pm_system_resume();
+#endif
 }
