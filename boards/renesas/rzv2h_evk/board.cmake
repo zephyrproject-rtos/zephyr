23bf54f3309c977e629eecd33657f551dd6043df
Tien Nguyen <tien.nguyen.zg@renesas.com>
2025-06-10 17:18:33 +0700

    boards: renesas: Add support for Renesas RZ/V2H-EVK R8 core
    Add support for Renesas RZ/V2H-EVK R8 core
    
    Signed-off-by: Tien Nguyen <tien.nguyen.zg@renesas.com>

--- boards/renesas/rzv2h_evk/board.cmake
+++ boards/renesas/rzv2h_evk/board.cmake
@@ -5,4 +5,8 @@ if(CONFIG_BOARD_RZV2H_EVK_R9A09G057H44GBG_CM33)
   board_runner_args(jlink "--device=R9A09G057H44_M33_0")
 endif()
 
+if (CONFIG_BOARD_RZV2H_EVK_R9A09G057H44GBG_CR8_0)
+  board_runner_args(jlink "--device=R9A09G057H44_R8_0")
+endif()
+
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
