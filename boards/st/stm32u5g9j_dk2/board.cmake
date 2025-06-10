813fd267162a8f917bb6ec90c1d0dc91ae9a7b68
Mathias Markussen <mathias.markussen@st.com>
2025-06-10 15:12:56 +0200

    board: st: stm32u5g9j-dk2: Add HSPI external flash
    Add external HSPI flash on STM32U5G9J-DK2
    Use external loader to flash it with STM32CubeProgrammer
    
    Signed-off-by: Mathias Markussen <mathias.markussen@st.com>

--- boards/st/stm32u5g9j_dk2/board.cmake
+++ boards/st/stm32u5g9j_dk2/board.cmake
@@ -3,6 +3,9 @@
 
 # keep first
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+if(CONFIG_STM32_MEMMAP)
+board_runner_args(stm32cubeprogrammer "--extload=MX66LM1G45G_STM32U5G9J-DK2.stldr")
+endif()
 
 board_runner_args(openocd "--tcl-port=6666")
 board_runner_args(openocd --cmd-pre-init "gdb_report_data_abort enable")
