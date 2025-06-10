b68c5aaea339eec57bfb0bd5ab98d01dec189707
Cristian Bulacu <cristian.bulacu@nxp.com>
2025-06-10 09:40:57 +0300

    samples: net: openthread: border_router: Add OTBR sample application.
    Enabled Wi-Fi shell alongside OpenThread application and added specific
    configuration for Border Router enablement.
    
    Signed-off-by: Cristian Bulacu <cristian.bulacu@nxp.com>

---
+++ samples/net/openthread/border_router/src/main.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int main(void)
+{
+	/* Nothing to do here. The Border Router is automatically started in the background. */
+
+	return 0;
+}
