ff805ae376a74826bf74f0b4412737119c651a51
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2025-06-10 15:24:49 +0300

    net: dns-sd: Add internal record validator function
    Allow network shell DNS module to verify that the DNS SD
    record is a valid one. Currently this is not exposed to
    applications.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- subsys/net/lib/dns/dns_sd.h
+++ subsys/net/lib/dns/dns_sd.h
@@ -158,6 +158,13 @@ int dns_sd_handle_service_type_enum(const struct dns_sd_rec *service,
 	const struct in_addr *addr4, const struct in6_addr *addr6,
 	uint8_t *buf, uint16_t buf_size);
 
+/**
+ * @brief Check if DNS-SD record is a valid one.
+ *
+ * @param rec the record to check
+ */
+bool dns_sd_rec_is_valid(const struct dns_sd_rec *rec);
+
 #ifdef __cplusplus
 };
 #endif
