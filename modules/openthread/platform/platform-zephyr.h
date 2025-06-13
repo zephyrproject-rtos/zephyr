2402ecffcc16729f18f8b17cc6a000f1c42a12d8
Cristian Bulacu <cristian.bulacu@nxp.com>
2025-06-13 11:26:42 +0300

    modules: openthread: Add initial border router support.
    Adding minimal implementation to support Border Router POC.
    
    Signed-off-by: Cristian Bulacu <cristian.bulacu@nxp.com>

--- modules/openthread/platform/platform-zephyr.h
+++ modules/openthread/platform/platform-zephyr.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2018 Nordic Semiconductor ASA
+ * Copyright 2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -116,5 +117,9 @@ int notify_new_rx_frame(struct net_pkt *pkt);
  * Notify OpenThread task about new tx message.
  */
 int notify_new_tx_frame(struct net_pkt *pkt);
+
+#if defined(CONFIG_OPENTHREAD_ZEPHYR_BORDER_ROUTER)
+otError infra_if_init(otInstance *instance, struct net_if *ail_iface);
+#endif /* CONFIG_OPENTHREAD_ZEPHYR_BORDER_ROUTER */
 
 #endif /* PLATFORM_ZEPHYR_H_ */
