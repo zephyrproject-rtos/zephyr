c87ca883a77579f389298539c7b42fd2ceac9f70
Benjamin Cabé <benjamin@zephyrproject.org>
2025-06-14 21:00:23 +0200

    drivers: w1: shell: fix write_bit function
    write_bit should write a bit, not a byte.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- drivers/w1/w1_shell.c
+++ drivers/w1/w1_shell.c
@@ -176,7 +176,7 @@ static int cmd_w1_write_bit(const struct shell *sh, size_t argc, char **argv)
 	}
 
 	(void)w1_lock_bus(dev);
-	ret = w1_write_byte(dev, (bool)input);
+	ret = w1_write_bit(dev, (bool)input);
 	if (ret < 0) {
 		shell_error(sh, "Failed to write bit [%d]", ret);
 	}
