cd88e448f7f8d374a65aea5573f6536732860e7c
Krzysztof Sychla <ksychla@antmicro.com>
2025-06-16 13:18:22 +0200

    tracing: Allow disabling idle traces
    This change allows for enabling/disabling the idle traces by setting the
    CONFIG_TRACING_IDLE config.
    
    Signed-off-by: Krzysztof Sychla <ksychla@antmicro.com>

--- subsys/tracing/ctf/ctf_top.c
+++ subsys/tracing/ctf/ctf_top.c
@@ -189,7 +189,9 @@ void sys_trace_isr_exit_to_scheduler(void)
 
 void sys_trace_idle(void)
 {
+#ifdef CONFIG_TRACING_IDLE
 	ctf_top_idle();
+#endif
 	if (IS_ENABLED(CONFIG_CPU_LOAD)) {
 		cpu_load_on_enter_idle();
 	}
