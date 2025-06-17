ee22fff02d0eebd471bfb4bb8bcaf3db9646b3d2
Mathias Markussen <mathias.markussen@st.com>
2025-06-17 09:52:02 +0200

    drivers: display: st: allow-ltdc-callback-override
    Allow overriding the LTDC callback for manual handling or extension
    In some cases, the default handling may not be desired, for example
    triggering handling of the next frame/block once the transfer has passed
    a certain line.
    
    Signed-off-by: Mathias Markussen <mathias.markussen@st.com>

---
+++ drivers/display/display_stm32_ltdc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2025, STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _ZEPHYR_DRIVERS_DISPLAY_STM32_LTDC_H_
+#define _ZEPHYR_DRIVERS_DISPLAY_STM32_LTDC_H_
+
+void stm32_ltdc_global_isr(const struct device *dev);
+
+#endif /* _ZEPHYR_DRIVERS_DISPLAY_STM32_LTDC_H_ */
