0c87a3956907fc1217450132d1e272fd51695328
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2025-06-18 10:19:29 +0200

    soc: intel_adsp: gather debug window indices in one place
    All debug window slot indices should be defined in
    adsp_debug_window.h, move them there and use them consistently.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- subsys/logging/backends/log_backend_adsp_mtrace.c
+++ subsys/logging/backends/log_backend_adsp_mtrace.c
@@ -66,16 +66,17 @@ struct adsp_debug_slot {
 
 static void mtrace_init(void)
 {
-	if (ADSP_DW->descs[0].type == MTRACE_LOGGING_SLOT_TYPE(MTRACE_CORE)) {
+	if (ADSP_DW->descs[ADSP_DW_SLOT_NUM_MTRACE].type == MTRACE_LOGGING_SLOT_TYPE(MTRACE_CORE)) {
 		return;
 	}
 
-	ADSP_DW->descs[0].type = MTRACE_LOGGING_SLOT_TYPE(MTRACE_CORE);
+	ADSP_DW->descs[ADSP_DW_SLOT_NUM_MTRACE].type = MTRACE_LOGGING_SLOT_TYPE(MTRACE_CORE);
 }
 
 static size_t mtrace_out(int8_t *str, size_t len, size_t *space_left)
 {
-	struct adsp_debug_slot *slot = (struct adsp_debug_slot *)(ADSP_DW->slots[0]);
+	struct adsp_debug_slot *slot = (struct adsp_debug_slot *)
+		ADSP_DW->slots[ADSP_DW_SLOT_NUM_MTRACE];
 	uint8_t *data = slot->data;
 	uint32_t r = slot->host_ptr;
 	uint32_t w = slot->dsp_ptr;
