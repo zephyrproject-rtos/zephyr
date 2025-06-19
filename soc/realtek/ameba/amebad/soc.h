72b15aa4a27293fb0cdc0e9b2bb18799233fb6d3
zjian zhang <zjian_zhang@realsil.com.cn>
2025-06-19 10:11:01 +0800

    soc: add realtek amebad SOC integration
    Add initial version of Amebad Soc integration
    
    Signed-off-by: zjian zhang <zjian_zhang@realsil.com.cn>

---
+++ soc/realtek/ameba/amebad/soc.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024 Realtek Semiconductor Corp.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_REALTEK_AMEBA_AMEBAD_H_
+#define ZEPHYR_SOC_REALTEK_AMEBA_AMEBAD_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <zephyr/sys/util.h>
+#include "cmsis_cpu.h"
+
+#endif /* _ASMLANGUAGE */
+
+#endif /* ZEPHYR_SOC_REALTEK_AMEBA_AMEBAD_H_ */
