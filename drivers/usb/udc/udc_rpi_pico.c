daaf0f2859f29fb73603757941214bf8d479a9d8
Dmitrii Sharshakov <d3dx12.xx@gmail.com>
2025-06-19 11:22:36 +0200

    drivers: udc_rpi_pico: move control endpoint enable/disable
    Move control endpoint enable/disable calls to udc_enable()/udc_disable().
    It does not change much during USB device support initialization, but
    it seems to resolve an issue when starting from RAM, though the real
    cause is unknown.
    
    Signed-off-by: Dmitrii Sharshakov <d3dx12.xx@gmail.com>

--- drivers/usb/udc/udc_rpi_pico.c
+++ drivers/usb/udc/udc_rpi_pico.c
@@ -1044,6 +1044,18 @@ static int udc_rpi_pico_enable(const struct device *dev)
 			    USB_USB_PWR_VBUS_DETECT_OVERRIDE_EN_BITS, (mm_reg_t)&base->pwr);
 	}
 
+	if (udc_ep_enable_internal(dev, USB_CONTROL_EP_OUT,
+				   USB_EP_TYPE_CONTROL, 64, 0)) {
+		LOG_ERR("Failed to enable control endpoint");
+		return -EIO;
+	}
+
+	if (udc_ep_enable_internal(dev, USB_CONTROL_EP_IN,
+				   USB_EP_TYPE_CONTROL, 64, 0)) {
+		LOG_ERR("Failed to enable control endpoint");
+		return -EIO;
+	}
+
 	/* Enable an interrupt per EP0 transaction */
 	sys_write32(USB_SIE_CTRL_EP0_INT_1BUF_BITS, (mm_reg_t)&base->sie_ctrl);
 
@@ -1082,6 +1094,16 @@ static int udc_rpi_pico_disable(const struct device *dev)
 {
 	const struct rpi_pico_config *config = dev->config;
 
+	if (udc_ep_disable_internal(dev, USB_CONTROL_EP_OUT)) {
+		LOG_ERR("Failed to disable control endpoint");
+		return -EIO;
+	}
+
+	if (udc_ep_disable_internal(dev, USB_CONTROL_EP_IN)) {
+		LOG_ERR("Failed to disable control endpoint");
+		return -EIO;
+	}
+
 	config->irq_disable_func(dev);
 	LOG_DBG("Disable device %p", dev);
 
@@ -1093,18 +1115,6 @@ static int udc_rpi_pico_init(const struct device *dev)
 	const struct rpi_pico_config *config = dev->config;
 	const struct pinctrl_dev_config *const pcfg = config->pcfg;
 	int err;
-
-	if (udc_ep_enable_internal(dev, USB_CONTROL_EP_OUT,
-				   USB_EP_TYPE_CONTROL, 64, 0)) {
-		LOG_ERR("Failed to enable control endpoint");
-		return -EIO;
-	}
-
-	if (udc_ep_enable_internal(dev, USB_CONTROL_EP_IN,
-				   USB_EP_TYPE_CONTROL, 64, 0)) {
-		LOG_ERR("Failed to enable control endpoint");
-		return -EIO;
-	}
 
 	if (pcfg != NULL) {
 		err = pinctrl_apply_state(pcfg, PINCTRL_STATE_DEFAULT);
@@ -1120,16 +1130,6 @@ static int udc_rpi_pico_init(const struct device *dev)
 static int udc_rpi_pico_shutdown(const struct device *dev)
 {
 	const struct rpi_pico_config *config = dev->config;
-
-	if (udc_ep_disable_internal(dev, USB_CONTROL_EP_OUT)) {
-		LOG_ERR("Failed to disable control endpoint");
-		return -EIO;
-	}
-
-	if (udc_ep_disable_internal(dev, USB_CONTROL_EP_IN)) {
-		LOG_ERR("Failed to disable control endpoint");
-		return -EIO;
-	}
 
 	return clock_control_off(config->clk_dev, config->clk_sys);
 }
