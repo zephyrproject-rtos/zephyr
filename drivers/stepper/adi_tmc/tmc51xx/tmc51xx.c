40dcf3c16dce44e5580e15f1d57569e26e9e1a48
Jilay Pandya <jilay.pandya@outlook.com>
2025-06-19 14:21:14 +0200

    drivers: stepper: tmc5xxx: introduce stop function
    Introduce stop function in tmc5xxx
    
    Signed-off-by: Jilay Pandya <jilay.pandya@outlook.com>

--- drivers/stepper/adi_tmc/tmc51xx/tmc51xx.c
+++ drivers/stepper/adi_tmc/tmc51xx/tmc51xx.c
@@ -781,6 +781,23 @@ static int tmc51xx_init(const struct device *dev)
 	return 0;
 }
 
+static int tmc51xx_stepper_stop(const struct device *dev)
+{
+	int err;
+
+	err = tmc51xx_write(dev, TMC51XX_RAMPMODE, TMC5XXX_RAMPMODE_POSITIVE_VELOCITY_MODE);
+	if (err != 0) {
+		return -EIO;
+	}
+
+	err = tmc51xx_write(dev, TMC51XX_VMAX, 0);
+	if (err != 0) {
+		return -EIO;
+	}
+
+	return 0;
+}
+
 static DEVICE_API(stepper, tmc51xx_api) = {
 	.enable = tmc51xx_stepper_enable,
 	.disable = tmc51xx_stepper_disable,
@@ -792,6 +809,7 @@ static DEVICE_API(stepper, tmc51xx_api) = {
 	.get_actual_position = tmc51xx_stepper_get_actual_position,
 	.move_to = tmc51xx_stepper_move_to,
 	.run = tmc51xx_stepper_run,
+	.stop = tmc51xx_stepper_stop,
 	.set_event_callback = tmc51xx_stepper_set_event_callback,
 };
 
