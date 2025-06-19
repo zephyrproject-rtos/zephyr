893b4e6474fc8b9298eee0ed8ad1408ffd9ddaf9
Alessandro Manganaro <alessandro.manganaro@st.com>
2025-06-19 13:55:51 +0200

    soc: st: stm32: stm32wbax pm enhancements
    soc power management rework to support power states
    removing dependency on ST system clock manager (SCM).
    
    Signed-off-by: Alessandro Manganaro <alessandro.manganaro@st.com>

--- soc/st/stm32/stm32wbax/power.c
+++ soc/st/stm32/stm32wbax/power.c
@@ -10,6 +10,10 @@
 #include <zephyr/init.h>
 #include <zephyr/arch/common/pm_s2ram.h>
 #include <zephyr/drivers/timer/system_timer.h>
+#include <zephyr/arch/arm/cortex_m/fpu.h>
+#include <kernel_arch_func.h>
+
+#include <cmsis_core.h>
 
 #include <stm32wbaxx_ll_bus.h>
 #include <stm32wbaxx_ll_cortex.h>
@@ -18,69 +22,80 @@
 #include <stm32wbaxx_ll_system.h>
 #include <clock_control/clock_stm32_ll_common.h>
 
-#ifdef CONFIG_BT_STM32WBA
-#include "scm.h"
-#endif
-
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_DECLARE(soc, CONFIG_SOC_LOG_LEVEL);
 
-void stm32_power_init(void);
 
-static void set_mode_stop(uint8_t substate_id)
-{
-
-	LL_PWR_ClearFlag_STOP();
-	LL_RCC_ClearResetFlags();
-
-	/* Erratum 2.2.15:
-	 * Disabling ICACHE is required before entering stop mode
-	 */
-	sys_cache_instr_disable();
-
-#ifdef CONFIG_BT_STM32WBA
-	scm_setwaitstates(LP);
-#endif
-	/* Set SLEEPDEEP bit of Cortex System Control Register */
-	LL_LPM_EnableDeepSleep();
-
-	while (LL_PWR_IsActiveFlag_ACTVOS() == 0) {
-	}
-
-	switch (substate_id) {
-	case 1: /* enter STOP0 mode */
-		LL_PWR_SetPowerMode(LL_PWR_MODE_STOP0);
-		break;
-	case 2: /* enter STOP1 mode */
-		LL_PWR_SetPowerMode(LL_PWR_MODE_STOP1);
-		break;
-	default:
-		LOG_DBG("Unsupported power state substate-id %u", substate_id);
-		break;
-	}
-}
+#define HSE_ON ((READ_BIT(RCC->CR, RCC_CR_HSEON) == RCC_CR_HSEON) ? 1UL : 0UL)
 
 #if defined(CONFIG_PM_S2RAM)
-static int suspend_to_ram(void)
+struct fpu_ctx_full fpu_state;
+SCB_Type backup_scb;
+static uint32_t ram_waitstates_backup;
+static uint32_t flash_latency_backup;
+
+void stm32_power_init(void);
+
+static void scb_suspend(SCB_Type *backup)
 {
+	backup->ICSR = SCB->ICSR;
+	backup->VTOR = SCB->VTOR;
+	backup->AIRCR = SCB->AIRCR;
+	backup->SCR = SCB->SCR;
+	backup->CCR = SCB->CCR;
+	memcpy((uint32_t *) backup->SHPR, (uint32_t *)SCB->SHPR, sizeof(SCB->SHPR));
+	backup->SHCSR = SCB->SHCSR;
+	backup->CFSR = SCB->CFSR;
+	backup->HFSR = SCB->HFSR;
+	backup->DFSR = SCB->DFSR;
+	backup->MMFAR = SCB->MMFAR;
+	backup->BFAR = SCB->BFAR;
+	backup->AFSR = SCB->AFSR;
+	backup->CPACR = SCB->CPACR;
+}
+static void scb_resume(SCB_Type *backup)
+{
+	SCB->ICSR = backup->ICSR;
+	SCB->VTOR = backup->VTOR;
+	SCB->AIRCR = backup->AIRCR;
+	SCB->SCR = backup->SCR;
+	SCB->CCR = backup->CCR;
+	memcpy((uint32_t *)SCB->SHPR, (uint32_t *)backup->SHPR, sizeof(SCB->SHPR));
+	SCB->SHCSR = backup->SHCSR;
+	SCB->CFSR = backup->CFSR;
+	SCB->HFSR = backup->HFSR;
+	SCB->DFSR = backup->DFSR;
+	SCB->MMFAR = backup->MMFAR;
+	SCB->BFAR = backup->BFAR;
+	SCB->AFSR = backup->AFSR;
+	SCB->CPACR = backup->CPACR;
+}
+
+static int low_power_mode(void)
+{
+	uint32_t basepri;
+
 	LL_LPM_EnableDeepSleep();
 
 	while (LL_PWR_IsActiveFlag_ACTVOS() == 0) {
-	}
+	};
 
-	/* Select mode entry : WFE or WFI and enter the CPU selected mode */
-	k_cpu_idle();
+	/* MCU enters in low-power modes, allowing any interrupt to skip WFI */
+	basepri = __get_BASEPRI();
+	__set_BASEPRI(0);
+	__ISB();
+	__DSB();
+	__WFI();
 
-	return 0;
+	/* MCU exits from low power mode and set back the basepri */
+	__set_BASEPRI(basepri);
+
+	return -EBUSY;
 }
 
-static void set_mode_suspend_to_ram(void)
+static void set_mode_suspend_to_ram_enter(void)
 {
-	/* Enable SRAM full retention */
-	LL_PWR_SetSRAM1SBRetention(LL_PWR_SRAM1_SB_FULL_RETENTION);
-	LL_PWR_SetSRAM2SBRetention(LL_PWR_SRAM2_SB_FULL_RETENTION);
-
 	/* Enable RTC wakeup
 	 * This configures an internal pin that generates an event to wakeup the system
 	 */
@@ -97,29 +112,123 @@ static void set_mode_suspend_to_ram(void)
 	/* Select standby mode */
 	LL_PWR_SetPowerMode(LL_PWR_MODE_STANDBY);
 
+	/* Save FPU state and configuration */
+	z_arm_save_fp_context(&fpu_state);
+	scb_suspend(&backup_scb);
 	/* Save context and enter Standby mode */
-	arch_pm_s2ram_suspend(suspend_to_ram);
+	arch_pm_s2ram_suspend(low_power_mode);
+}
 
-	/* Execution is restored at this point after wake up */
+static void set_mode_suspend_to_ram_exit(void)
+{
+#if defined(CONFIG_ARM_MPU)
+	z_arm_mpu_init();
+	/* Configure static memory map. This will program MPU regions,
+	 * to set up access permissions for fixed memory sections, such
+	 * as Application Memory or No-Cacheable SRAM area.
+	 *
+	 * This function is invoked once, upon system initialization.
+	 */
+	z_arm_configure_static_mpu_regions();
+#endif /* CONFIG_ARM_MPU */
+	scb_resume(&backup_scb);
+	z_arm_restore_fp_context(&fpu_state);
+
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_RCC_RAMCFG_CLK_ENABLE();
+
 	/* Restore system clock as soon as we exit standby mode */
 	stm32_clock_control_standby_exit();
+
+	if (LL_PWR_IsActiveFlag_SB() || !HSE_ON) {
+		stm32_clock_control_init(NULL);
+	}
+	/* Resume configuration */
+	stm32wba_init();
+	stm32_power_init();
 }
 #endif
+
+static void set_mode_stop_enter(uint8_t substate_id)
+{
+	LL_PWR_ClearFlag_STOP();
+	LL_RCC_ClearResetFlags();
+
+	/* Erratum 2.2.15:
+	 * Disabling ICACHE is required before entering stop mode
+	 */
+	sys_cache_instr_disable();
+	/* If stop mode is greater than Stop0 (id = 1), manage flash latency and RAMCFG */
+	if (substate_id > 1) {
+		flash_latency_backup = __HAL_FLASH_GET_LATENCY();
+		if (flash_latency_backup < FLASH_LATENCY_1) {
+			/* Set flash latency to 1, since system will restart from HSI16 */
+			__HAL_FLASH_SET_LATENCY(FLASH_LATENCY_1);
+			while (__HAL_FLASH_GET_LATENCY() != FLASH_LATENCY_1) {
+			};
+		}
+		ram_waitstates_backup = READ_BIT(RAMCFG_SRAM1->CR, RAMCFG_CR_WSC);
+		MODIFY_REG(RAMCFG_SRAM1->CR, RAMCFG_CR_WSC, RAMCFG_WAITSTATE_1);
+		MODIFY_REG(RAMCFG_SRAM2->CR, RAMCFG_CR_WSC, RAMCFG_WAITSTATE_1);
+	}
+	switch (substate_id) {
+	case 1:
+		LL_PWR_SetPowerMode(LL_PWR_MODE_STOP0);
+		break;
+	case 2:
+		LL_PWR_SetPowerMode(LL_PWR_MODE_STOP1);
+		break;
+	default:
+		LOG_DBG("Unsupported power state substate-id %u", substate_id);
+		return;
+	}
+
+	low_power_mode();
+}
+
+static void set_mode_stop_exit(uint8_t substate_id)
+{
+	if (LL_PWR_IsActiveFlag_STOP() || !HSE_ON) {
+		/* Reconfigure the clock */
+		stm32_clock_control_init(NULL);
+	} else {
+		/* Stop mode skipped */
+		if (substate_id > 1) {
+			__HAL_FLASH_SET_LATENCY(flash_latency_backup);
+			while (__HAL_FLASH_GET_LATENCY() != flash_latency_backup) {
+			};
+			MODIFY_REG(RAMCFG_SRAM1->CR, RAMCFG_CR_WSC, ram_waitstates_backup);
+			MODIFY_REG(RAMCFG_SRAM2->CR, RAMCFG_CR_WSC, ram_waitstates_backup);
+		}
+	}
+
+	/* Erratum 2.2.15:
+	 * Enable ICACHE when exiting stop mode
+	 */
+	sys_cache_instr_enable();
+
+	LL_LPM_DisableSleepOnExit();
+	LL_LPM_EnableSleep();
+}
 
 /* Invoke Low Power/System Off specific Tasks */
 void pm_state_set(enum pm_state state, uint8_t substate_id)
 {
 	switch (state) {
 	case PM_STATE_SUSPEND_TO_IDLE:
-		set_mode_stop(substate_id);
-
-		/* Select mode entry : WFE or WFI and enter the CPU selected mode */
-		k_cpu_idle();
-
+		__disable_irq();
+		set_mode_stop_enter(substate_id);
+		/* Resume here */
+		set_mode_stop_exit(substate_id);
+		__enable_irq();
 		break;
 #if defined(CONFIG_PM_S2RAM)
 	case PM_STATE_SUSPEND_TO_RAM:
-		set_mode_suspend_to_ram();
+		__disable_irq();
+		set_mode_suspend_to_ram_enter();
+		/* Resume here */
+		set_mode_suspend_to_ram_exit();
+		__enable_irq();
 		break;
 #endif
 	default:
@@ -131,54 +240,6 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 /* Handle SOC specific activity after Low Power Mode Exit */
 void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
 {
-#ifdef CONFIG_BT_STM32WBA
-	if (LL_PWR_IsActiveFlag_STOP() == 1U) {
-		scm_setup();
-	} else {
-		scm_setwaitstates(RUN);
-	}
-#endif
-
-	switch (state) {
-	case PM_STATE_SUSPEND_TO_IDLE:
-		if (substate_id <= 2) {
-			/* Erratum 2.2.15:
-			 * Enable ICACHE when exiting stop mode
-			 */
-			sys_cache_instr_enable();
-
-			LL_LPM_DisableSleepOnExit();
-			LL_LPM_EnableSleep();
-		} else {
-			LOG_DBG("Unsupported power substate-id %u",
-							substate_id);
-		}
-		break;
-	case PM_STATE_SUSPEND_TO_RAM:
-#if defined(CONFIG_PM_S2RAM)
-		stm32wba_init();
-		stm32_power_init();
-
-		LL_LPM_DisableSleepOnExit();
-		LL_LPM_EnableSleep();
-#else
-		LOG_DBG("Suspend to RAM needs CONFIG_PM_S2RAM to be enabled");
-#endif
-		break;
-	case PM_STATE_STANDBY:
-		__fallthrough;
-	case PM_STATE_SUSPEND_TO_DISK:
-		__fallthrough;
-	default:
-		LOG_DBG("Unsupported power state %u", state);
-		break;
-	}
-
-	/* When BLE is enabled, clock restoration is performed by SCM */
-#if !defined(CONFIG_BT_STM32WBA)
-	stm32_clock_control_init(NULL);
-#endif
-
 	/*
 	 * System is now in active mode.
 	 * Reenable interrupts which were disabled
@@ -191,10 +252,6 @@ void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
 void stm32_power_init(void)
 {
 
-#ifdef CONFIG_BT_STM32WBA
-	scm_init();
-#endif
-
 	LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_PWR);
 
 #ifdef CONFIG_DEBUG
@@ -204,6 +261,13 @@ void stm32_power_init(void)
 #else
 	LL_DBGMCU_DisableDBGStandbyMode();
 #endif
+
+	/* Enable SRAM full retention */
+	LL_PWR_SetSRAM1SBRetention(LL_PWR_SRAM1_SB_FULL_RETENTION);
+	LL_PWR_SetSRAM2SBRetention(LL_PWR_SRAM2_SB_FULL_RETENTION);
+
+	/* Enable Radio RAM full retention */
+	LL_PWR_SetRadioSBRetention(LL_PWR_RADIO_SB_FULL_RETENTION);
 
 	/* Enabling  Ultra Low power mode */
 	LL_PWR_EnableUltraLowPowerMode();
