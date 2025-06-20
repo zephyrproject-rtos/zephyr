c109ebaae548155cf123007118cabf54e5a97edc
James Roy <rruuaanng@outlook.com>
2025-06-20 17:28:26 +0800

    toolchain: gcc: Add the __deprecated_version macro
    Add the `__deprecated_version` macro to print version
    information when deprecation warnings are triggered.
    
    Signed-off-by: James Roy <rruuaanng@outlook.com>

--- include/zephyr/toolchain/gcc.h
+++ include/zephyr/toolchain/gcc.h
@@ -267,6 +267,10 @@ do {                                                                    \
  */
 #endif
 
+#ifndef __deprecated_version
+#define __deprecated_version(version)	__attribute__((deprecated(#version)))
+#endif
+
 #ifndef __attribute_const__
 #define __attribute_const__ __attribute__((__const__))
 #endif
