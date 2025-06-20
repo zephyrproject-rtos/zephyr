fb714fe1044d197609681af7f97482cccc218d7c
Dave Joseph <d-joseph@ti.com>
2025-06-20 11:58:22 +0530

    soc: ti: Turns on power domains in soc layer
    Turns on all the power domains for devices with a power-domain device
    tree property in the soc layer. Supported for the R5f0_0 core on the
    AM243x EVM.
    
    Signed-off-by: Dave Joseph <d-joseph@ti.com>

--- soc/ti/k3/am6x/r5/soc.c
+++ soc/ti/k3/am6x/r5/soc.c
@@ -6,9 +6,12 @@
 
 #include <stdint.h>
 #include <zephyr/fatal.h>
-
+#include <zephyr/logging/log.h>
 #include "soc.h"
+#include <zephyr/device.h>
 #include <common/ctrl_partitions.h>
+#include <zephyr/pm/device_runtime.h>
+LOG_MODULE_REGISTER(soc, LOG_LEVEL_DBG);
 
 unsigned int z_soc_irq_get_active(void)
 {
@@ -53,3 +56,31 @@ void soc_early_init_hook(void)
 {
 	k3_unlock_all_ctrl_partitions();
 }
+
+#if defined(CONFIG_SOC_POWER_DOMAIN_INIT)
+int soc_power_domain_init(void)
+{
+	LOG_INF("Starting power domain initialization\n");
+
+	int error_count = 0;
+
+#define CHECK_NODE_POWER_DOMAIN(child)                                                             \
+	if (DT_NODE_HAS_PROP(child, power_domains)) {                                              \
+		const struct device *dev_##child =                                                 \
+			DEVICE_DT_GET_OR_NULL(DT_PHANDLE(child, power_domains));                   \
+		if (dev_##child) {                                                                 \
+			LOG_INF("Turning on power domain: %s\n", dev_##child->name);               \
+			int err = pm_device_runtime_get(dev_##child);                              \
+			if (err < 0) {                                                             \
+				LOG_INF("Failed to get power domain: %s\n", dev_##child->name);    \
+				error_count++;                                                     \
+			}                                                                          \
+		}                                                                                  \
+	}
+
+	DT_FOREACH_CHILD(DT_ROOT, CHECK_NODE_POWER_DOMAIN);
+
+	return error_count;
+}
+SYS_INIT(soc_power_domain_init, APPLICATION, 99);
+#endif /* CONFIG_SOC_POWER_DOMAIN_INIT */
