6e4cb5dbb161783b2380752f46c1e7e4fbf215cf
Jilay Pandya <jilay.pandya@outlook.com>
2025-06-21 14:25:59 +0200

    drivers: stepper: introduce stepper_drv api
    introducing stepper_drv api which is to be implemented by
    the stepper drivers
    
    Add fault handling in drv84xx using a fault cb mechanism
    
    With the introduction of the stepper_drv api, the goal is
    to achieve better separation of concerns where motion
    controllers are responsible for generating step pulses
    whereaas stepper drivers do are responsible for stepping,
    enabling, setting microstep resolution
    
    Signed-off-by: Jilay Pandya <jilay.pandya@outlook.com>

--- tests/drivers/stepper/drv84xx/emul/src/main.c
+++ tests/drivers/stepper/drv84xx/emul/src/main.c
@@ -33,20 +33,19 @@ static void *drv84xx_emul_setup(void)
 static void drv84xx_emul_before(void *f)
 {
 	struct drv84xx_emul_fixture *fixture = f;
-	(void)stepper_set_reference_position(fixture->dev, 0);
-	(void)stepper_set_micro_step_res(fixture->dev, 1);
+	(void)stepper_drv_set_micro_stepper_res(fixture->dev, 1);
 }
 
 static void drv84xx_emul_after(void *f)
 {
 	struct drv84xx_emul_fixture *fixture = f;
-	(void)stepper_disable(fixture->dev);
+	(void)stepper_drv_disable(fixture->dev);
 }
 
 ZTEST_F(drv84xx_emul, test_enable_on_gpio_pins)
 {
 	int value = 0;
-	(void)stepper_enable(fixture->dev);
+	(void)stepper_drv_enable(fixture->dev);
 	/* As sleep and enable pins are optional, check if they exist*/
 	if (en_pin.port != NULL) {
 		value = gpio_emul_output_get(en_pin.port, en_pin.pin);
@@ -64,8 +63,8 @@ ZTEST_F(drv84xx_emul, test_enable_off_gpio_pins)
 	/* Enable first to ensure that disable works correctly and the check is not against values
 	 * from initialisation or from previous tests
 	 */
-	(void)stepper_enable(fixture->dev);
-	(void)stepper_disable(fixture->dev);
+	(void)stepper_drv_enable(fixture->dev);
+	(void)stepper_drv_disable(fixture->dev);
 	/* As sleep and enable pins are optional, check if they exist*/
 	if (en_pin.port != NULL) {
 		value = gpio_emul_output_get(en_pin.port, en_pin.pin);
