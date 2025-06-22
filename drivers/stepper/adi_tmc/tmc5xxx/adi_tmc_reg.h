4b2b46f463f26eb469c91e9e4cc0a4d58debbfc1
Dipak Shetty <shetty.dipak@gmx.com>
2025-06-22 18:14:43 +0200

    drivers: stepper: adi_tmc: tmc5xxx: create core handler
    The implementation moves the common bus transaction code from individual driver
    files into reusable core modules. The new adi_tmc5xxx_core files provide a unified
    API for basic controller operations like register read/write, motor control, and
    diagnostics.
    
    This refactoring also moves the register definitions to the tmc5xxx subdirectory
    and removes the no longer needed adi_tmc5xxx_common.h file.
    
    Signed-off-by: Dipak Shetty <shetty.dipak@gmx.com>

--- drivers/stepper/adi_tmc/tmc5xxx/adi_tmc_reg.h
+++ drivers/stepper/adi_tmc/tmc5xxx/adi_tmc_reg.h
@@ -1,5 +1,5 @@
 /**
- * @file drivers/stepper/adi_tmc/adi_tmc_reg.h
+ * @file drivers/stepper/adi/tmc_reg.h
  *
  * @brief TMC Registers
  *
@@ -21,8 +21,8 @@ extern "C" {
 /** Common Registers for TMC50XX and TMC51XX */
 #if defined(CONFIG_STEPPER_ADI_TMC50XX) || defined(CONFIG_STEPPER_ADI_TMC51XX)
 
-#define TMC5XXX_WRITE_BIT        0x80U
-#define TMC5XXX_ADDRESS_MASK     0x7FU
+#define TMC5XXX_WRITE_BIT    0x80U
+#define TMC5XXX_ADDRESS_MASK 0x7FU
 
 #define TMC5XXX_CLOCK_FREQ_SHIFT 24
 
@@ -34,8 +34,8 @@ extern "C" {
 #define TMC5XXX_RAMPMODE_NEGATIVE_VELOCITY_MODE 2
 #define TMC5XXX_RAMPMODE_HOLD_MODE              3
 
-#define TMC5XXX_SG_MIN_VALUE -64
-#define TMC5XXX_SG_MAX_VALUE 63
+#define TMC5XXX_SG_MIN_VALUE           -64
+#define TMC5XXX_SG_MAX_VALUE           63
 #define TMC5XXX_SW_MODE_SG_STOP_ENABLE BIT(10)
 
 #define TMC5XXX_COOLCONF_SG2_THRESHOLD_VALUE_SHIFT 16
@@ -70,7 +70,7 @@ extern "C" {
 #define TMC5XXX_POS_REACHED_AND_EVENT (TMC5XXX_POS_REACHED | TMC5XXX_POS_REACHED_EVENT)
 
 #define TMC5XXX_RAMPSTAT_STOP_SG_EVENT_MASK BIT(6)
-#define TMC5XXX_STOP_SG_EVENT                                                                      \
+#define TMC5XXX_STOP_SG_EVENT									   \
 	(TMC5XXX_RAMPSTAT_STOP_SG_EVENT_MASK >> TMC5XXX_RAMPSTAT_INT_SHIFT)
 
 #define TMC5XXX_RAMPSTAT_STOP_RIGHT_EVENT_MASK BIT(5)
@@ -89,24 +89,25 @@ extern "C" {
 #define TMC50XX_MOTOR_ADDR(m)     (0x20 << (m))
 #define TMC50XX_MOTOR_ADDR_DRV(m) ((m) << 4)
 
-#define TMC50XX_RAMPMODE(motor)   (0x00 | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_XACTUAL(motor)    (0x01 | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_VACTUAL(motor)    (0x02 | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_VSTART(motor)     (0x03 | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_A1(motor)         (0x04 | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_V1(motor)         (0x05 | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_AMAX(motor)       (0x06 | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_VMAX(motor)       (0x07 | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_DMAX(motor)       (0x08 | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_D1(motor)         (0x0A | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_VSTOP(motor)      (0x0B | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_TZEROWAIT(motor)  (0x0C | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_XTARGET(motor)    (0x0D | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_SWMODE(motor)     (0x14 | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_RAMPSTAT(motor)   (0x15 | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_CHOPCONF(motor)   (0x6C | TMC50XX_MOTOR_ADDR_DRV(motor))
-#define TMC50XX_COOLCONF(motor)   (0x6D | TMC50XX_MOTOR_ADDR_DRV(motor))
-#define TMC50XX_DRVSTATUS(motor)  (0x6F | TMC50XX_MOTOR_ADDR_DRV(motor))
+#define TMC5XXX_RAMPMODE(motor)  ((uint8_t)(0x00 | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_XACTUAL(motor)   ((uint8_t)(0x01 | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_VACTUAL(motor)   ((uint8_t)(0x02 | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_VSTART(motor)    ((uint8_t)(0x03 | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_A1(motor)        ((uint8_t)(0x04 | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_V1(motor)        ((uint8_t)(0x05 | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_AMAX(motor)      ((uint8_t)(0x06 | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_VMAX(motor)      ((uint8_t)(0x07 | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_DMAX(motor)      ((uint8_t)(0x08 | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_D1(motor)        ((uint8_t)(0x0A | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_VSTOP(motor)     ((uint8_t)(0x0B | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_TZEROWAIT(motor) ((uint8_t)(0x0C | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_XTARGET(motor)   ((uint8_t)(0x0D | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_SWMODE(motor)    ((uint8_t)(0x14 | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_RAMPSTAT(motor)  ((uint8_t)(0x15 | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC5XXX_CHOPCONF(motor)  ((uint8_t)(0x6C | TMC50XX_MOTOR_ADDR_DRV(motor)))
+#define TMC5XXX_COOLCONF(motor)  ((uint8_t)(0x6D | TMC50XX_MOTOR_ADDR_DRV(motor)))
+#define TMC5XXX_DRVSTATUS(motor) ((uint8_t)(0x6F | TMC50XX_MOTOR_ADDR_DRV(motor)))
+#define TMC5XXX_IHOLD_IRUN(motor) ((uint8_t)(0x10 | TMC50XX_MOTOR_ADDR(motor)))
 
 #endif
 
@@ -129,9 +130,8 @@ extern "C" {
 #define TMC50XX_PWMCONF(motor)    (0x10 | TMC50XX_MOTOR_ADDR_PWM(motor))
 #define TMC50XX_PWM_STATUS(motor) (0x11 | TMC50XX_MOTOR_ADDR_PWM(motor))
 
-#define TMC50XX_IHOLD_IRUN(motor) (0x10 | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_VCOOLTHRS(motor)  (0x11 | TMC50XX_MOTOR_ADDR(motor))
-#define TMC50XX_VHIGH(motor)      (0x12 | TMC50XX_MOTOR_ADDR(motor))
+#define TMC50XX_VCOOLTHRS(motor)  ((uint8_t)(0x11 | TMC50XX_MOTOR_ADDR(motor)))
+#define TMC50XX_VHIGH(motor)      ((uint8_t)(0x12 | TMC50XX_MOTOR_ADDR(motor)))
 #define TMC50XX_XLATCH(motor)     (0x16 | TMC50XX_MOTOR_ADDR(motor))
 
 #define TMC50XX_MSLUT0(motor)     (0x60 | TMC50XX_MOTOR_ADDR_DRV(motor))
