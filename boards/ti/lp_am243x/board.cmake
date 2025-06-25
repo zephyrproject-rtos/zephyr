eb68a988af2094473948e76e466087f4e627f43d
Spencer J. Coward <spencerjcoward@gmail.com>
2025-06-24 22:19:15 -0400

    boards: ti: lp_am243x: dtsi and board.cmake formatting cleanup
    Address comments from PR 91986, relating to the formatting of the
    board.cmake file and the dtsi files for the lp_am243x board.
    
    Signed-off-by: Spencer J. Coward <spencerjcoward@gmail.com>

--- boards/ti/lp_am243x/board.cmake
+++ boards/ti/lp_am243x/board.cmake
@@ -3,9 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 if(CONFIG_SOC_AM2434_M4)
-board_runner_args(openocd "--no-init" "--no-halt" "--no-targets" "--gdb-client-port=3338")
+  board_runner_args(openocd "--no-init" "--no-halt" "--no-targets" "--gdb-client-port=3338")
 elseif(CONFIG_SOC_AM2434_R5F0_0)
-board_runner_args(openocd "--no-init" "--no-halt" "--no-targets" "--gdb-client-port=3334")
+  board_runner_args(openocd "--no-init" "--no-halt" "--no-targets" "--gdb-client-port=3334")
 endif()
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
