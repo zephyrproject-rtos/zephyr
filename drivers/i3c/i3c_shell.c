0b3f415069e6ab4b6fe43ffd0b1d4c72d5a01b24
The Nguyen <the.nguyen.yf@renesas.com>
2025-06-26 12:47:03 +0700

    drivers: i3c: shell: enable i3c shell support for renesas,ra-i3c
    This commit introduces support for i3c shell on Renesas RA devices
    
    Signed-off-by: The Nguyen <the.nguyen.yf@renesas.com>

--- drivers/i3c/i3c_shell.c
+++ drivers/i3c/i3c_shell.c
@@ -86,6 +86,7 @@ DT_FOREACH_STATUS_OKAY(ite_it51xxx_i3cm, I3C_CTRL_FN)
 DT_FOREACH_STATUS_OKAY(ite_it51xxx_i3cs, I3C_CTRL_FN)
 DT_FOREACH_STATUS_OKAY(nuvoton_npcx_i3c, I3C_CTRL_FN)
 DT_FOREACH_STATUS_OKAY(nxp_mcux_i3c, I3C_CTRL_FN)
+DT_FOREACH_STATUS_OKAY(renesas_ra_i3c, I3C_CTRL_FN)
 DT_FOREACH_STATUS_OKAY(snps_designware_i3c, I3C_CTRL_FN)
 DT_FOREACH_STATUS_OKAY(st_stm32_i3c, I3C_CTRL_FN)
 /* zephyr-keep-sorted-stop */
@@ -105,6 +106,7 @@ const struct i3c_ctrl i3c_list[] = {
 	DT_FOREACH_STATUS_OKAY(ite_it51xxx_i3cs, I3C_CTRL_LIST_ENTRY)
 	DT_FOREACH_STATUS_OKAY(nuvoton_npcx_i3c, I3C_CTRL_LIST_ENTRY)
 	DT_FOREACH_STATUS_OKAY(nxp_mcux_i3c, I3C_CTRL_LIST_ENTRY)
+	DT_FOREACH_STATUS_OKAY(renesas_ra_i3c, I3C_CTRL_LIST_ENTRY)
 	DT_FOREACH_STATUS_OKAY(snps_designware_i3c, I3C_CTRL_LIST_ENTRY)
 	DT_FOREACH_STATUS_OKAY(st_stm32_i3c, I3C_CTRL_LIST_ENTRY)
 	/* zephyr-keep-sorted-stop */
