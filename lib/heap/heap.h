6e7238423e2165b1374645c50dcd43e165029278
Krzysztof Sychla <ksychla@antmicro.com>
2025-06-27 14:49:41 +0200

    kernel: heap: Add allocation metadata to allocated_bytes
    The allocated_bytes were missing the allocation metadata. Because of that
    the sum of free_bytes and allocated_bytes doesn't remain constant after
    each allocation. This convention doesn't match glibc's behavior. This
    commit changes the chunksz_to_bytes function to include the metadata in the
    calculation. The analysis of the mallinfo2 function from glibc has been
    done in #92392 Pull Request.
    
    Signed-off-by: Krzysztof Sychla <ksychla@antmicro.com>

--- lib/heap/heap.h
+++ lib/heap/heap.h
@@ -255,7 +255,7 @@ static inline chunksz_t min_chunk_size(struct z_heap *h)
 
 static inline size_t chunksz_to_bytes(struct z_heap *h, chunksz_t chunksz_in)
 {
-	return chunksz_in * CHUNK_UNIT - chunk_header_bytes(h);
+	return chunksz_in * CHUNK_UNIT;
 }
 
 static inline int bucket_idx(struct z_heap *h, chunksz_t sz)
