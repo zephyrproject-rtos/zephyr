f5251ba2d19e95b7cae6a74ed82cbb4da8a7b491
Robert Lubos <robert.lubos@nordicsemi.no>
2025-06-27 15:13:15 +0200

    net: lwm2m: senml_cbor: Verify data len before calling memcpy
    Check if there actually is data to copy before calling memcpy() to
    prevent potentially calling memcpy() with NULL value pointer.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- subsys/net/lib/lwm2m/lwm2m_rw_senml_cbor.c
+++ subsys/net/lib/lwm2m/lwm2m_rw_senml_cbor.c
@@ -644,8 +644,9 @@ static int get_string(struct lwm2m_input_context *in, uint8_t *buf, size_t bufle
 	}
 
 	len = MIN(buflen-1, fd->current->record_union.union_vs.len);
-
-	memcpy(buf, fd->current->record_union.union_vs.value, len);
+	if (len > 0) {
+		memcpy(buf, fd->current->record_union.union_vs.value, len);
+	}
 	buf[len] = '\0';
 
 	fd->current = NULL;
