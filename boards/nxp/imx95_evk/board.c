65ca88a535acb7fbe841957c08ed824bfdd73f15
Jiafei Pan <Jiafei.Pan@nxp.com>
2025-06-29 18:34:41 +0800

    drivers: ethernet: imx_netc: add netc block driver
    Add NETC block driver, it could do some block memory region MMIO mapping
    and also so dome block initialization, moved some netc related
    configuration form board_init() to block driver so that it could be reused
    between different platforms, although some configuration is different for
    different platform, but put all NETC related code in the same driver to
    make it easier to be maintained.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- boards/nxp/imx95_evk/board.c
+++ boards/nxp/imx95_evk/board.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 NXP
+ * Copyright 2024-2025 NXP
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -10,21 +10,6 @@
 
 static int board_init(void)
 {
-#if defined(CONFIG_ETH_NXP_IMX_NETC) && (DT_CHILD_NUM_STATUS_OKAY(DT_NODELABEL(netc)) != 0)
-	/* Port 0 to 2 protocol configure: RGMII, RGMII, XGMII */
-	BLK_CTRL_NETCMIX->CFG_LINK_MII_PROT = 0x00000522;
-	BLK_CTRL_NETCMIX->CFG_LINK_PCS_PROT_2 = 0x00000040;
-
-	/* Unlock the IERB. It will warm reset whole NETC. */
-	NETC_PRIV->NETCRR &= ~NETC_PRIV_NETCRR_LOCK_MASK;
-	while ((NETC_PRIV->NETCRR & NETC_PRIV_NETCRR_LOCK_MASK) != 0U) {
-	}
-
-	/* Lock the IERB. */
-	NETC_PRIV->NETCRR |= NETC_PRIV_NETCRR_LOCK_MASK;
-	while ((NETC_PRIV->NETCSR & NETC_PRIV_NETCSR_STATE_MASK) != 0U) {
-	}
-#endif
 	return 0;
 }
 
