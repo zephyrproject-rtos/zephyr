7895a773c7faa860f71c744f10c20b9de2de0446
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2025-07-01 12:21:31 +0200

    tests: bsim: bluetooth: ll: multiple_id: Enable RealEncryption
    Enable RealEncrytion.
    
    Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

--- tests/bsim/bluetooth/ll/multiple_id/tests_scripts/multiple.sh
+++ tests/bsim/bluetooth/ll/multiple_id/tests_scripts/multiple.sh
@@ -11,10 +11,10 @@ EXECUTE_TIMEOUT=1600
 
 cd ${BSIM_OUT_PATH}/bin
 
-Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_ll_multiple_id_prj_conf \
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_ll_multiple_id_prj_conf -RealEncryption=1 \
   -v=${verbosity_level} -s=${simulation_id} -d=0 -testid=central
 
-Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_ll_multiple_id_prj_conf\
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_ll_multiple_id_prj_conf -RealEncryption=1 \
   -v=${verbosity_level} -s=${simulation_id} -d=1 -testid=peripheral -rs=6
 
 Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
