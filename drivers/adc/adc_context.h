1faf6bf6e16c0e5c09c2e9776bdc4da8c6ceebd2
Jakub Zymelka <jakub.zymelka@nordicsemi.no>
2025-07-01 10:53:26 +0200

    drivers: adc: nrfx_saadc: Add support for SAADC internal sampling timer
    The SAMPLERATE register can be used as a local timer instead
    of triggering individual SAMPLE tasks. When SAMPLERATE.MODE is set
    to Timers, it is sufficient to trigger SAMPLE task only once in order
    to start the SAADC and triggering the STOP task will stop sampling.
    The SAMPLERATE.CC field controls the sample rate.
    
    The SAMPLERATE timer should not be combined with SCAN mode and
    only one channel should be enabled when using the internal timer.
    
    Signed-off-by: Jakub Zymelka <jakub.zymelka@nordicsemi.no>

--- drivers/adc/adc_context.h
+++ drivers/adc/adc_context.h
@@ -115,6 +115,7 @@ static inline void adc_context_request_next_sampling(struct adc_context *ctx)
 }
 
 #ifdef ADC_CONTEXT_USES_KERNEL_TIMER
+#ifndef ADC_DRIVER_USES_INTERNAL_TIMER
 static inline void adc_context_enable_timer(struct adc_context *ctx)
 {
 	k_timer_start(&ctx->timer, K_NO_WAIT, K_USEC(ctx->options.interval_us));
@@ -124,6 +125,7 @@ static inline void adc_context_disable_timer(struct adc_context *ctx)
 {
 	k_timer_stop(&ctx->timer);
 }
+#endif /* ADC_DRIVER_USES_INTERNAL_TIMER */
 
 static void adc_context_on_timer_expired(struct k_timer *timer_id)
 {
