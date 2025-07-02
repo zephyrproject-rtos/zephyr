06cbb1659b9adc93370749df0bf81a3e5d16aed5
Guillaume Gautier <guillaume.gautier-ext@st.com>
2025-07-02 10:34:17 +0200

    drivers: spi: context: check that config exists before reading it
    Function spi_context_release reads the content of ctx->config without
    checking first if it is set. In many drivers (including STM32), when a bad
    configuration is made for the first transaction, ctx->config is not set,
    and spi_context_release is called, resulting in hard fault.
    
    This commit adds a check that ctx->config exists before reading it to
    avoid this problem.
    
    Signed-off-by: Guillaume Gautier <guillaume.gautier-ext@st.com>

--- drivers/spi/spi_context.h
+++ drivers/spi/spi_context.h
@@ -144,7 +144,7 @@ static inline void spi_context_release(struct spi_context *ctx, int status)
 {
 #ifdef CONFIG_MULTITHREADING
 #ifdef CONFIG_SPI_SLAVE
-	if (status >= 0 && (ctx->config->operation & SPI_LOCK_ON)) {
+	if (status >= 0 && ((ctx->config == NULL) || (ctx->config->operation & SPI_LOCK_ON))) {
 		return;
 	}
 #endif /* CONFIG_SPI_SLAVE */
@@ -155,7 +155,7 @@ static inline void spi_context_release(struct spi_context *ctx, int status)
 		k_sem_give(&ctx->lock);
 	}
 #else
-	if (!(ctx->config->operation & SPI_LOCK_ON)) {
+	if ((ctx->config == NULL) || !(ctx->config->operation & SPI_LOCK_ON)) {
 		ctx->owner = NULL;
 		k_sem_give(&ctx->lock);
 	}
