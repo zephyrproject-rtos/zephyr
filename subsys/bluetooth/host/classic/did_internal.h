f2f41f166d08c6a3c3114b12289f2a60a03d54b2
Kai Cheng <chengkai@xiaomi.com>
2025-07-02 19:29:59 +0800

    Bluetooth: DID: add did profile
    add did profile codes
    
    Signed-off-by: Kai Cheng <chengkai@xiaomi.com>

---
+++ subsys/bluetooth/host/classic/did_internal.h
@@ -0,0 +1,12 @@
+/** @file
+ *  @brief Device Identification Protocol handling.
+ */
+
+/*
+ * Copyright 2025 Xiaomi Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/** Device Identification Init. **/
+int bt_did_init(void);
