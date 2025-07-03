3ec0096a6d7a3fd97ed8bbb1e51fbb8a2f598fdf
Tony Han <tony.han@microchip.com>
2025-07-03 13:39:04 +0800

    soc: microchip: sam: optimize array size for sama7g5 registered clocks
    Replace the array size for sama7g5 registered clocks with macros and
    put the macros to soc.h with descriptions.
    
    Signed-off-by: Tony Han <tony.han@microchip.com>

--- soc/microchip/sam/sama7g5/soc.h
+++ soc/microchip/sam/sama7g5/soc.h
@@ -13,4 +13,13 @@
 
 #include "sam.h"
 
+/* number of clocks registered */
+#define SOC_NUM_CLOCK_PLL_FRAC     7
+#define SOC_NUM_CLOCK_PLL_DIV      (SOC_NUM_CLOCK_PLL_FRAC + 1) /* AUDIO PLL: DIVPMC, DIVIO */
+#define SOC_NUM_CLOCK_MASTER       5	/* MCK 0 ~ 4 */
+#define SOC_NUM_CLOCK_PROGRAMMABLE 8	/* MCK 0 ~ 7 */
+#define SOC_NUM_CLOCK_SYSTEM       8	/* PCK 0 ~ 7 */
+#define SOC_NUM_CLOCK_PERIPHERAL   72
+#define SOC_NUM_CLOCK_GENERATED    46
+
 #endif /* __SAMA7G5_SOC__H_ */
