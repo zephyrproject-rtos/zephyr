45c07fa7047bd7d71d02bebb45620cfbbc02a8a0
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2025-07-04 14:16:13 +0200

    Bluetooth: Controller: Remove duplicate define CONN_DATA_BUFFERS
    Remove duplicate define CONN_DATA_BUFFERS and use
    CONFIG_BT_BUF_ACL_TX_COUNT instead.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_conn.c
+++ subsys/bluetooth/controller/ll_sw/ull_conn.c
@@ -130,23 +130,18 @@ static uint8_t force_md_cnt_calc(struct lll_conn *lll_conn, uint32_t tx_rate);
 				(LL_LENGTH_OCTETS_TX_MAX + \
 				BT_CTLR_USER_TX_BUFFER_OVERHEAD))
 
-#define CONN_DATA_BUFFERS CONFIG_BT_BUF_ACL_TX_COUNT
-
-static MFIFO_DEFINE(conn_tx, sizeof(struct lll_tx), CONN_DATA_BUFFERS);
+static MFIFO_DEFINE(conn_tx, sizeof(struct lll_tx), CONFIG_BT_BUF_ACL_TX_COUNT);
 static MFIFO_DEFINE(conn_ack, sizeof(struct lll_tx),
-		    (CONN_DATA_BUFFERS +
-		     LLCP_TX_CTRL_BUF_COUNT));
+		    (CONFIG_BT_BUF_ACL_TX_COUNT + LLCP_TX_CTRL_BUF_COUNT));
 
 static struct {
 	void *free;
-	uint8_t pool[CONN_TX_BUF_SIZE * CONN_DATA_BUFFERS];
+	uint8_t pool[CONN_TX_BUF_SIZE * CONFIG_BT_BUF_ACL_TX_COUNT];
 } mem_conn_tx;
 
 static struct {
 	void *free;
-	uint8_t pool[sizeof(memq_link_t) *
-		     (CONN_DATA_BUFFERS +
-		      LLCP_TX_CTRL_BUF_COUNT)];
+	uint8_t pool[sizeof(memq_link_t) * (CONFIG_BT_BUF_ACL_TX_COUNT + LLCP_TX_CTRL_BUF_COUNT)];
 } mem_link_tx;
 
 #if defined(CONFIG_BT_CTLR_DATA_LENGTH)
@@ -1702,14 +1697,11 @@ static int init_reset(void)
 	}
 
 	/* Initialize tx pool. */
-	mem_init(mem_conn_tx.pool, CONN_TX_BUF_SIZE, CONN_DATA_BUFFERS,
-		 &mem_conn_tx.free);
+	mem_init(mem_conn_tx.pool, CONN_TX_BUF_SIZE, CONFIG_BT_BUF_ACL_TX_COUNT, &mem_conn_tx.free);
 
 	/* Initialize tx link pool. */
 	mem_init(mem_link_tx.pool, sizeof(memq_link_t),
-		 (CONN_DATA_BUFFERS +
-		  LLCP_TX_CTRL_BUF_COUNT),
-		 &mem_link_tx.free);
+		 (CONFIG_BT_BUF_ACL_TX_COUNT + LLCP_TX_CTRL_BUF_COUNT), &mem_link_tx.free);
 
 	/* Initialize control procedure system. */
 	ull_cp_init();
