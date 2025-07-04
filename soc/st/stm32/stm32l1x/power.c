99451848044509b65c2ceabb942884a2aed7d559
Oleh Kravchenko <oleg@kaa.org.ua>
2025-07-04 10:45:37 +0300

    soc: stm32l1x: Add support for sleep/stop/standby modes
    Add stm32l1_disco and nucleo_l152re overlays for testing
    sleep/stop/standby modes:
    - samples/boards/st/power_mgmt/blinky;
    - samples/boards/st/power_mgmt/wkup_pins;
    
    I've measured consumption for each low-power mode:
    - low-power sleep ~1.72mA;
    - stop mode ~324uA;
    - standby mode ~2.2 uA;
    
    It's possible to use RTC as idle timer to exit from stop mode.
    
    Signed-off-by: Oleh Kravchenko <oleg@kaa.org.ua>

---
+++ soc/st/stm32/stm32l1x/power.c
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2025 Oleh Kravchenko <oleh@kaa.org.ua>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <clock_control/clock_stm32_ll_common.h>
+#include <stm32l1xx_ll_cortex.h>
+#include <stm32l1xx_ll_pwr.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/pm/pm.h>
+
+LOG_MODULE_DECLARE(soc, CONFIG_SOC_LOG_LEVEL);
+
+__weak void pm_state_set(enum pm_state state, uint8_t substate_id)
+{
+	switch (state) {
+	case PM_STATE_RUNTIME_IDLE:
+		LL_LPM_DisableEventOnPend();
+		LL_PWR_ClearFlag_WU();
+
+		LL_PWR_SetRegulModeLP(LL_PWR_REGU_LPMODES_LOW_POWER);
+		LL_PWR_SetPowerMode(LL_PWR_MODE_STOP);
+		LL_LPM_EnableSleep();
+
+		k_cpu_idle();
+		break;
+
+	case PM_STATE_SUSPEND_TO_IDLE:
+		LL_LPM_DisableEventOnPend();
+		LL_PWR_ClearFlag_WU();
+
+		LL_PWR_SetRegulModeLP(LL_PWR_REGU_LPMODES_LOW_POWER);
+		LL_PWR_SetPowerMode(LL_PWR_MODE_STOP);
+		LL_LPM_EnableDeepSleep();
+
+		k_cpu_idle();
+		break;
+
+	default:
+		LOG_DBG("Unsupported power state %u", state);
+		break;
+	}
+}
+
+__weak void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
+{
+	ARG_UNUSED(substate_id);
+
+	switch (state) {
+	case PM_STATE_RUNTIME_IDLE:
+		LL_PWR_SetRegulModeLP(LL_PWR_REGU_LPMODES_MAIN);
+		break;
+
+	case PM_STATE_SUSPEND_TO_IDLE:
+		LL_LPM_DisableSleepOnExit();
+		LL_LPM_EnableSleep();
+		LL_PWR_SetRegulModeLP(LL_PWR_REGU_LPMODES_MAIN);
+
+		/* Restore the clock setup. */
+		stm32_clock_control_init(NULL);
+		break;
+
+	default:
+		LOG_DBG("Unsupported power substate-id %u", state);
+		break;
+	}
+
+	/*
+	 * System is now in active mode. Reenable interrupts which were
+	 * disabled when OS started idling code.
+	 */
+	irq_unlock(0);
+}
