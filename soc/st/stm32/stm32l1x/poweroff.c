da95f0f50b0547d74ccf6178daeb76954fb8c76c
Oleh Kravchenko <oleg@kaa.org.ua>
2025-07-04 10:45:37 +0300

    soc: stm32l1x: Add support for sleep/stop/standby modes
    Add stm32l1_disco and nucleo_l152re overlays for testing
    sleep/stop/standby modes:
    - samples/boards/st/power_mgmt/blinky;
    - samples/boards/st/power_mgmt/wkup_pins;
    
    I've measured consumption for each low-power mode:
    - low-power sleep ~1.72mA;
    - stop mode ~324uA;
    - standby mode ~2.2 uA;
    
    It's possible to use RTC as idle timer to exit from stop mode.
    
    Signed-off-by: Oleh Kravchenko <oleg@kaa.org.ua>

---
+++ soc/st/stm32/stm32l1x/poweroff.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2025 Oleh Kravchenko <oleh@kaa.org.ua>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stm32l1xx_ll_cortex.h>
+#include <stm32l1xx_ll_pwr.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/poweroff.h>
+
+void z_sys_poweroff(void)
+{
+	LL_PWR_ClearFlag_SB();
+	LL_PWR_ClearFlag_WU();
+
+	LL_LPM_DisableEventOnPend();
+	LL_PWR_SetPowerMode(LL_PWR_MODE_STANDBY);
+	LL_LPM_EnableDeepSleep();
+
+	k_cpu_idle();
+
+	CODE_UNREACHABLE;
+}
