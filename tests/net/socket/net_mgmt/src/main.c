f41e1776ff18a5f8c587624a915388884cbae15c
Gaetan Perrot <gaetan.perrot@spacecubics.com>
2025-07-05 22:40:52 +0900

    tests: net: socket: net_mgmt: mark unused function argument
    Use ARG_UNUSED() to mark unused function argument.
    
    Signed-off-by: Gaetan Perrot <gaetan.perrot@spacecubics.com>

--- tests/net/socket/net_mgmt/src/main.c
+++ tests/net/socket/net_mgmt/src/main.c
@@ -87,6 +87,7 @@ static int eth_fake_send(const struct device *dev,
 
 static int eth_fake_get_total_bandwidth(struct eth_fake_context *ctx)
 {
+	ARG_UNUSED(ctx);
 	return 100 * 1000 * 1000 / 8;
 }
 
@@ -221,6 +222,7 @@ static int eth_fake_get_config(const struct device *dev,
 
 static enum ethernet_hw_caps eth_fake_get_capabilities(const struct device *dev)
 {
+	ARG_UNUSED(dev);
 	return ETHERNET_LINK_10BASE | ETHERNET_LINK_100BASE | ETHERNET_QAV |
 	       ETHERNET_PROMISC_MODE | ETHERNET_PRIORITY_QUEUES;
 }
