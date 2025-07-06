068f231cd17b4de0647f2755f202178ea5127602
Dmitrii Sharshakov <d3dx12.xx@gmail.com>
2025-07-06 19:30:32 +0200

    drivers: hwinfo: rpi_pico: use bootrom method on RP2350
    RP2350 provides a function to get OTP-backed chip ID. Prefer using this
    on supported platforms for these reasons:
    - a secure internal identifier, same as read by picotool
    - works on flashless boards
    - does not conflict with code running in XIP mode (e.g. when the other
     core tries to access device ID)
    - when used with USB HWID serial number will match one BootROM has
    
    Signed-off-by: Dmitrii Sharshakov <d3dx12.xx@gmail.com>
    Co-authored-by: Alexander Wachter <alexander@wachter.cloud>

--- drivers/hwinfo/hwinfo_rpi_pico.c
+++ drivers/hwinfo/hwinfo_rpi_pico.c
@@ -11,6 +11,8 @@
 #if defined(CONFIG_SOC_SERIES_RP2040)
 #include <hardware/structs/vreg_and_chip_reset.h>
 #else
+#include <zephyr/sys/byteorder.h>
+#include <pico/bootrom.h>
 #include <hardware/structs/powman.h>
 #endif
 
@@ -29,6 +31,20 @@
 ssize_t z_impl_hwinfo_get_device_id(uint8_t *buffer, size_t length)
 {
 	uint8_t id[FLASH_RUID_DATA_BYTES];
+
+#if CONFIG_HWINFO_RPI_PICO_CHIP_ID
+	rom_get_sys_info_fn get_sys_info = (rom_get_sys_info_fn)
+					rom_func_lookup_inline(ROM_FUNC_GET_SYS_INFO);
+	/* Words: CHIP_INFO, PACKAGE_SEL, DEVICE_ID, WAFER_ID */
+	uint32_t words[4];
+
+	int n = get_sys_info(words, ARRAY_SIZE(words), SYS_INFO_CHIP_INFO);
+	/* CHIP_INFO returns 4 words */
+	__ASSERT(n == ARRAY_SIZE(words), "Failed to get chip info");
+
+	/* Use DEVICE_ID + WAFER_ID, like BootROM uses for its USB ID */
+	sys_put_be(id, &words[2], 2 * sizeof(uint32_t));
+#else
 	uint32_t key;
 
 	/*
@@ -40,6 +56,7 @@ ssize_t z_impl_hwinfo_get_device_id(uint8_t *buffer, size_t length)
 	key = irq_lock();
 	flash_get_unique_id(id);
 	irq_unlock(key);
+#endif /* defined(CONFIG_SOC_SERIES_RP2350) */
 
 	if (length > sizeof(id)) {
 		length = sizeof(id);
