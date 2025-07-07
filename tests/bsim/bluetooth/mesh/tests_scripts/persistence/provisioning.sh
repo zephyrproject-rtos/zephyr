1863abd7c4ce45207eed2087605a9cf3296b7a8e
Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>
2025-07-07 14:23:08 +0200

    tests: bluetooth: mesh: extend provisioning test with gotten stuck key
    Commit extends existing persistent provisioning bsim mesh test that
    checks that mesh removes gotten stuck persisted key if key is reused.
    Correct key is imported normally after that.
    
    Signed-off-by: Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>

--- tests/bsim/bluetooth/mesh/tests_scripts/persistence/provisioning.sh
+++ tests/bsim/bluetooth/mesh/tests_scripts/persistence/provisioning.sh
@@ -4,14 +4,35 @@
 
 source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
 
-# Note:
+# Test checks that mesh provisioning data is saved and loaded correctly.
+# It also checks if some key exists in the PSA ITS, it is destroyed and
+# the correct key is used instead.
+#
 # Tests must be added in pairs and in sequence.
 # First test: saves data; second test: verifies it.
+#
+# Test procedure:
+# 1. Fake key is created in the PSA ITS (only if the fake key is used).
+# 2. Device Under Test (DUT) initializes the Mesh stack,
+#    and starts provisioning and configuration itself.
+# 3. Test scenario emulates hardware  reset by running the second test
+#    with stored data from the first test.
+# 4. DUT checks that the fake key was substituted by the real key (only if the fake key is used).
+# 5. DUT starts mesh with loading stored data and checks that if they were restored correctly.
 
-# SKIP=(persistence_provisioning_data_save)
 overlay=overlay_pst_conf
-RunTestFlash mesh_pst_prov_data_check persistence_provisioning_data_save -flash_erase
+RunTestFlash mesh_pst_prov_data_check persistence_provisioning_data_save -flash_erase \
+	-- -argstest persist-fake-key=0
 
-# SKIP=(persistence_provisioning_data_load)
 overlay=overlay_pst_conf
-RunTestFlash mesh_pst_prov_data_check persistence_provisioning_data_load -flash_rm
+RunTestFlash mesh_pst_prov_data_check persistence_provisioning_data_load -flash_rm \
+	-- -argstest persist-fake-key=0
+
+# the same scenario but with fake key
+overlay=overlay_pst_conf
+RunTestFlash mesh_pst_prov_data_check_fake_key persistence_provisioning_data_save -flash_erase \
+	-- -argstest persist-fake-key=1
+
+overlay=overlay_pst_conf
+RunTestFlash mesh_pst_prov_data_check_fake_key persistence_provisioning_data_load -flash_rm \
+	-- -argstest persist-fake-key=1
