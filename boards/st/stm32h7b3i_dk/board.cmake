48e258fc62087bf25ba5249d5e2bad094d4192b7
Joel Guittet <joelguittet@gmail.com>
2025-07-07 22:19:53 +0200

    boards: st: cleaning usage of --extload parameter
    --extload parameter is used with specific conditions.
    Cleaning and make uniform usage of this parameter for all STM32 boards.
    
    Signed-off-by: Joel Guittet <joelguittet@gmail.com>

--- boards/st/stm32h7b3i_dk/board.cmake
+++ boards/st/stm32h7b3i_dk/board.cmake
@@ -2,11 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # keep first
-if(CONFIG_STM32_MEMMAP)
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
-board_runner_args(stm32cubeprogrammer "--extload=MX25LM51245G_STM32H7B3I-DISCO.stldr")
-else()
-board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw" )
+if(CONFIG_XIP AND (CONFIG_STM32_MEMMAP OR CONFIG_BOOTLOADER_MCUBOOT))
+  board_runner_args(stm32cubeprogrammer "--extload=MX25LM51245G_STM32H7B3I-DISCO.stldr")
 endif()
 
 board_runner_args(jlink "--device=STM32H7B3LI" "--speed=4000")
--- boards/st/stm32u5g9j_dk2/board.cmake
+++ boards/st/stm32u5g9j_dk2/board.cmake
@@ -3,8 +3,8 @@
 
 # keep first
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
-if(CONFIG_STM32_MEMMAP)
-board_runner_args(stm32cubeprogrammer "--extload=MX66LM1G45G_STM32U5G9J-DK2.stldr")
+if(CONFIG_XIP AND (CONFIG_STM32_MEMMAP OR CONFIG_BOOTLOADER_MCUBOOT))
+  board_runner_args(stm32cubeprogrammer "--extload=MX66LM1G45G_STM32U5G9J-DK2.stldr")
 endif()
 
 board_runner_args(openocd "--tcl-port=6666")
