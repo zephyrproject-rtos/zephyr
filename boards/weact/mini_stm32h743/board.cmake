8304fb1a8d2be4486fee74c64227d11ceefbaabb
heronet <sirat4757@gmail.com>
2025-07-08 18:26:22 +0600

    boards: weact: mini_stm32h743: add pyocd support
    Add pyocd runner support to mini_stm32h743 board configuration.
    
    Signed-off-by: Siratul Islam <sirat4757@gmail.com>
    Signed-off-by: heronet <sirat4757@gmail.com>

--- boards/weact/mini_stm32h743/board.cmake
+++ boards/weact/mini_stm32h743/board.cmake
@@ -2,6 +2,8 @@
 
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
 board_runner_args(jlink "--device=STM32H743VI" "--speed=4000")
+board_runner_args(pyocd "--target" "stm32h743vitx")
 
 include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
