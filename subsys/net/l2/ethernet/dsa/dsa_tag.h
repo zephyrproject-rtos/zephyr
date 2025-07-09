5ec058861d93185d936cfef9eea084704b08cacd
Yangbo Lu <yangbo.lu@nxp.com>
2025-07-09 16:57:59 +0800

    net: dsa: add NXP NETC tag protocol driver
    Added NXP NETC tag protocol driver.
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- subsys/net/l2/ethernet/dsa/dsa_tag.h
+++ subsys/net/l2/ethernet/dsa/dsa_tag.h
@@ -6,6 +6,8 @@
 #ifndef ZEPHYR_SUBSYS_DSA_TAG_PRIV_H_
 #define ZEPHYR_SUBSYS_DSA_TAG_PRIV_H_
 
+#include "dsa_tag_netc.h"
+
 struct net_if *dsa_tag_recv(struct net_if *iface, struct net_pkt *pkt);
 struct net_pkt *dsa_tag_xmit(struct net_if *iface, struct net_pkt *pkt);
 
