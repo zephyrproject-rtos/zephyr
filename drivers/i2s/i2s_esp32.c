9db2aae9ae8e55348aa188ba82efa43c716ba051
Marcio Ribeiro <marcio.ribeiro@espressif.com>
2025-07-10 11:55:23 -0300

    drivers: i2s: esp32: remove I2S_DIR_BOTH handling
    Removes I2S_DIR_BOTH handling inside of i2s_esp32_configure() and
    i2s_esp32_trigger()
    
    Signed-off-by: Marcio Ribeiro <marcio.ribeiro@espressif.com>

--- drivers/i2s/i2s_esp32.c
+++ drivers/i2s/i2s_esp32.c
@@ -1107,38 +1107,9 @@ static int i2s_esp32_configure(const struct device *dev, enum i2s_dir dir,
 		err = -EINVAL;
 #endif /* I2S_ESP32_IS_DIR_EN(tx) */
 		break;
-	case I2S_DIR_BOTH:
-#if I2S_ESP32_IS_DIR_EN(tx)
-		stream = &dev_cfg->tx;
-		if (stream) {
-			err = i2s_esp32_configure_dir(dev, I2S_DIR_TX, stream, i2s_cfg);
-		} else {
-			LOG_ERR("I2S_DIR_TX not enabled");
-			err = -EINVAL;
-		}
-#else
-		LOG_ERR("I2S_DIR_TX not enabled");
-		err = -EINVAL;
-#endif /* I2S_ESP32_IS_DIR_EN(tx) */
-		if (err < 0) {
-			break;
-		}
-#if I2S_ESP32_IS_DIR_EN(rx)
-		stream = &dev_cfg->rx;
-		if (stream) {
-			err = i2s_esp32_configure_dir(dev, I2S_DIR_RX, stream, i2s_cfg);
-		} else {
-			LOG_ERR("I2S_DIR_RX not enabled");
-			err = -EINVAL;
-		}
-#else
-		LOG_ERR("I2S_DIR_RX not enabled");
-		err = -EINVAL;
-#endif /* I2S_ESP32_IS_DIR_EN(rx) */
-		break;
 	default:
 		LOG_ERR("Invalid direction: %d", (int)dir);
-		return -EINVAL;
+		err = -EINVAL;
 	}
 
 	return err;
@@ -1345,35 +1316,6 @@ static int i2s_esp32_trigger(const struct device *dev, enum i2s_dir dir, enum i2
 		LOG_ERR("I2S_DIR_TX not enabled");
 		err = -EINVAL;
 #endif /* I2S_ESP32_IS_DIR_EN(tx) */
-		break;
-	case I2S_DIR_BOTH:
-#if I2S_ESP32_IS_DIR_EN(tx)
-		stream = &dev_cfg->tx;
-		if (stream) {
-			err = i2s_esp32_trigger_stream(dev, stream, I2S_DIR_TX, cmd);
-		} else {
-			LOG_ERR("I2S_DIR_TX not enabled");
-			err = -EINVAL;
-		}
-#else
-		LOG_ERR("I2S_DIR_TX not enabled");
-		err = -EINVAL;
-#endif /* I2S_ESP32_IS_DIR_EN(tx) */
-		if (err < 0) {
-			break;
-		}
-#if I2S_ESP32_IS_DIR_EN(rx)
-		stream = &dev_cfg->rx;
-		if (stream) {
-			err = i2s_esp32_trigger_stream(dev, stream, I2S_DIR_RX, cmd);
-		} else {
-			LOG_ERR("I2S_DIR_RX not enabled");
-			err = -EINVAL;
-		}
-#else
-		LOG_ERR("I2S_DIR_RX not enabled");
-		err = -EINVAL;
-#endif /* I2S_ESP32_IS_DIR_EN(rx) */
 		break;
 	default:
 		LOG_ERR("Invalid direction: %d", (int)dir);
