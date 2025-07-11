00420dcf6bbbd37e0e43e26c5dae2dbe20289a2e
Gang Li <gang.li_1@nxp.com>
2025-07-11 12:05:36 +0200

    [nrf fromtree] modules: hostap: set the cipher suites for WPA2 & WPA3 SAE mode
    When the external AP is in WPA3 SAE mode, the group cipher uses
    TKIP and the pairwise cipher uses TKIP and CCMP. Should not
    connect to the AP.
    For WPA2 & WPA3 SAE & WPA2/WPA3 mixed mode, set group cipher to
    CCMP, pairwise cipher to CCMP.
    
    Signed-off-by: Gang Li <gang.li_1@nxp.com>
    (cherry picked from commit c8fa5e5899575fe5c0a0a6c3013fdc4527d2cdb4)

--- modules/hostap/src/supp_api.c
+++ modules/hostap/src/supp_api.c
@@ -752,6 +752,14 @@ static int wpas_add_and_config_network(struct wpa_supplicant *wpa_s,
 					goto out;
 				}
 			}
+
+			if (!wpa_cli_cmd_v("set_network %d group CCMP", resp.network_id)) {
+				goto out;
+			}
+
+			if (!wpa_cli_cmd_v("set_network %d pairwise CCMP", resp.network_id)) {
+				goto out;
+			}
 		} else if (params->security == WIFI_SECURITY_TYPE_PSK_SHA256) {
 			if (!wpa_cli_cmd_v("set_network %d psk \"%s\"",
 					   resp.network_id, psk_null_terminated)) {
@@ -760,6 +768,14 @@ static int wpas_add_and_config_network(struct wpa_supplicant *wpa_s,
 
 			if (!wpa_cli_cmd_v("set_network %d key_mgmt WPA-PSK-SHA256",
 					   resp.network_id)) {
+				goto out;
+			}
+
+			if (!wpa_cli_cmd_v("set_network %d group CCMP", resp.network_id)) {
+				goto out;
+			}
+
+			if (!wpa_cli_cmd_v("set_network %d pairwise CCMP", resp.network_id)) {
 				goto out;
 			}
 		} else if (params->security == WIFI_SECURITY_TYPE_PSK ||
@@ -776,6 +792,15 @@ static int wpas_add_and_config_network(struct wpa_supplicant *wpa_s,
 
 			if (params->security == WIFI_SECURITY_TYPE_WPA_PSK) {
 				if (!wpa_cli_cmd_v("set_network %d proto WPA",
+						   resp.network_id)) {
+					goto out;
+				}
+			} else {
+				if (!wpa_cli_cmd_v("set_network %d group CCMP", resp.network_id)) {
+					goto out;
+				}
+
+				if (!wpa_cli_cmd_v("set_network %d pairwise CCMP",
 						   resp.network_id)) {
 					goto out;
 				}
@@ -804,6 +829,14 @@ static int wpas_add_and_config_network(struct wpa_supplicant *wpa_s,
 
 			if (!wpa_cli_cmd_v("set_network %d key_mgmt WPA-PSK SAE",
 					   resp.network_id)) {
+				goto out;
+			}
+
+			if (!wpa_cli_cmd_v("set_network %d group CCMP", resp.network_id)) {
+				goto out;
+			}
+
+			if (!wpa_cli_cmd_v("set_network %d pairwise CCMP", resp.network_id)) {
 				goto out;
 			}
 #ifdef CONFIG_WIFI_NM_WPA_SUPPLICANT_CRYPTO_ENTERPRISE
