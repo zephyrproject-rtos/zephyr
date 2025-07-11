786e5cf563d915414e4b584f999e03eb6e6e6e37
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2025-07-11 14:30:40 +0200

    drivers: flash_mspi_nor: Get info from dts SFDP arrays
    Get parameters for used flash commands and requirements for enabling
    Quad and Octal modes from dts uint8-arrays containing data read from
    SFDP tables for particular flash chips.
    Also introduce `pre_init` quirk that allows alteration of the above
    parameters or complementation of them in a specific way for particular
    flash chip families.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- drivers/flash/flash_mspi_nor.h
+++ drivers/flash/flash_mspi_nor.h
@@ -20,9 +20,52 @@ extern "C" {
 #define WITH_RESET_GPIO 1
 #endif
 
+#define CMD_EXTENSION_NONE    0
+#define CMD_EXTENSION_SAME    1
+#define CMD_EXTENSION_INVERSE 2
+
+#define OCTAL_ENABLE_REQ_NONE 0
+#define OCTAL_ENABLE_REQ_S2B3 1
+
+#define ENTER_4BYTE_ADDR_NONE  0
+#define ENTER_4BYTE_ADDR_B7    1
+#define ENTER_4BYTE_ADDR_06_B7 2
+
+struct flash_mspi_nor_cmd_info {
+	uint8_t read_cmd;
+	uint8_t read_mode_bit_cycles : 3;
+	uint8_t read_dummy_cycles    : 5;
+	uint8_t pp_cmd;
+	bool    uses_4byte_addr : 1;
+	/* BFP, 18th DWORD, bits 30-29 */
+	uint8_t cmd_extension   : 2;
+	/* xSPI Profile 1.0 (ID FF05), 1st DWORD: */
+	/* - Read SFDP command address bytes: 4 (true) or 3 */
+	bool    sfdp_addr_4     : 1;
+	/* - Read SDFP command dummy cycles: 20 (true) or 8 */
+	bool    sfdp_dummy_20   : 1;
+	/* - Read Status Register command address bytes: 4 (true) or 0 */
+	bool    rdsr_addr_4     : 1;
+	/* - Read Status Register command dummy cycles: 0, 4, or 8 */
+	uint8_t rdsr_dummy      : 4;
+	/* - Read JEDEC ID command parameters; not sure where to get their
+	 *   values from, but since for many flash chips they are the same
+	 *   as for RDSR, those are taken as defaults, see DEFAULT_CMD_INFO()
+	 */
+	bool    rdid_addr_4     : 1;
+	uint8_t rdid_dummy      : 4;
+};
+
+struct flash_mspi_nor_switch_info {
+	uint8_t quad_enable_req  : 3;
+	uint8_t octal_enable_req : 3;
+	uint8_t enter_4byte_addr : 2;
+};
+
 struct flash_mspi_nor_config {
 	const struct device *bus;
 	uint32_t flash_size;
+	uint16_t page_size;
 	struct mspi_dev_id mspi_id;
 	struct mspi_dev_cfg mspi_nor_cfg;
 	struct mspi_dev_cfg mspi_nor_init_cfg;
@@ -42,7 +85,9 @@ struct flash_mspi_nor_config {
 	uint8_t jedec_id[SPI_NOR_MAX_ID_LEN];
 	const struct flash_mspi_nor_cmds *jedec_cmds;
 	struct flash_mspi_nor_quirks *quirks;
-	uint8_t dw15_qer;
+	const struct jesd216_erase_type *default_erase_types;
+	struct flash_mspi_nor_cmd_info default_cmd_info;
+	struct flash_mspi_nor_switch_info default_switch_info;
 };
 
 struct flash_mspi_nor_data {
@@ -50,6 +95,9 @@ struct flash_mspi_nor_data {
 	struct mspi_xfer_packet packet;
 	struct mspi_xfer xfer;
 	struct mspi_dev_cfg *curr_cfg;
+	struct jesd216_erase_type erase_types[JESD216_NUM_ERASE_TYPES];
+	struct flash_mspi_nor_cmd_info cmd_info;
+	struct flash_mspi_nor_switch_info switch_info;
 };
 
 struct flash_mspi_nor_cmd {
