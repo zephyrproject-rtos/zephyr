68898629c7af3ac8347637c06f1989245769907c
Luis Ubieda <luisf@croxel.com>
2025-07-11 19:59:48 -0400

    bmm350: Add read-decode support
    One-shot reads are now supported through the v2 model API.
    
    Signed-off-by: Luis Ubieda <luisf@croxel.com>

--- drivers/sensor/bosch/bmm350/bmm350_decoder.h
+++ drivers/sensor/bosch/bmm350/bmm350_decoder.h
@@ -14,4 +14,11 @@ void bmm350_decoder_compensate_raw_data(const struct bmm350_raw_mag_data *raw_da
 					const struct mag_compensate *comp,
 					struct bmm350_mag_temp_data *out);
 
+int bmm350_encode(const struct device *dev,
+		  const struct sensor_read_config *read_config,
+		  bool is_trigger,
+		  uint8_t *buf);
+
+int bmm350_get_decoder(const struct device *dev, const struct sensor_decoder_api **decoder);
+
 #endif /* ZEPHYR_DRIVERS_SENSOR_BMM350_BMM350_DECODER_H_ */
