b3e9a72f41833d800e9f4a163c19c007deec32bd
Luis Ubieda <luisf@croxel.com>
2025-07-15 17:50:35 -0400

    bmp581: stream: Add FIFO Watermark support
    Configurable through dts property: fifo-watermark.
    
    Signed-off-by: Luis Ubieda <luisf@croxel.com>

--- drivers/sensor/bosch/bmp581/bmp581_decoder.h
+++ drivers/sensor/bosch/bmp581/bmp581_decoder.h
@@ -12,15 +12,49 @@
 #include <zephyr/drivers/sensor.h>
 #include "bmp581.h"
 
-struct bmp581_encoded_data {
-	struct {
+struct bmp581_encoded_header {
 		uint8_t channels;
 		uint8_t events;
 		uint64_t timestamp;
 		uint8_t press_en;
-	} header;
-	uint8_t payload[6]; /* 3 bytes temp + 3 bytes press */
+		uint8_t fifo_count;
 };
+
+struct bmp581_frame {
+	uint8_t payload[6];
+};
+
+struct bmp581_encoded_data {
+	struct bmp581_encoded_header header;
+	union {
+		uint8_t payload[6]; /* 3 bytes temp + 3 bytes press */
+		struct bmp581_frame frame[0]; /* Used for FIFO frames */
+	};
+};
+
+static inline enum bmp581_event bmp581_encode_events_bitmask(
+					const struct sensor_stream_trigger *const triggers,
+					size_t count)
+{
+	enum bmp581_event result = 0;
+
+	for (size_t i = 0 ; i < count ; i++) {
+		switch (triggers[i].trigger) {
+		case SENSOR_TRIG_DATA_READY:
+			result |= ((triggers[i].opt == SENSOR_STREAM_DATA_INCLUDE) ?
+				   BMP581_EVENT_DRDY : 0);
+			break;
+		case SENSOR_TRIG_FIFO_WATERMARK:
+			result |= ((triggers[i].opt == SENSOR_STREAM_DATA_INCLUDE) ?
+				   BMP581_EVENT_FIFO_WM : 0);
+			break;
+		default:
+			break;
+		}
+	}
+
+	return result;
+}
 
 int bmp581_encode(const struct device *dev,
 		   const struct sensor_read_config *read_config,
