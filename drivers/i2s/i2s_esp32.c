cb2478b5a17e0588967c1c63e4bbd19bdd0c333c
Marcio Ribeiro <marcio.ribeiro@espressif.com>
2025-07-15 01:56:05 -0300

    drivers: i2s: esp32: fix last block loss
    Avoids erroneous memory block freeing when stopping transfers
    
    Signed-off-by: Marcio Ribeiro <marcio.ribeiro@espressif.com>

--- drivers/i2s/i2s_esp32.c
+++ drivers/i2s/i2s_esp32.c
@@ -50,7 +50,6 @@ struct i2s_esp32_stream_data {
 	struct i2s_config i2s_cfg;
 	void *mem_block;
 	size_t mem_block_len;
-	bool last_block;
 	bool stop_without_draining;
 	struct k_msgq queue;
 	struct intr_handle_data_t *irq_handle;
@@ -170,6 +169,18 @@ static void i2s_esp32_rx_callback(void *arg, int status)
 	const struct i2s_esp32_stream *stream = &dev_cfg->rx;
 	int err;
 
+	if (!stream->data->dma_pending) {
+		return;
+	}
+
+	stream->data->dma_pending = false;
+
+	if (stream->data->mem_block == NULL) {
+		LOG_ERR("RX mem_block NULL");
+		stream->data->state = I2S_STATE_ERROR;
+		goto rx_disable;
+	}
+
 #if SOC_GDMA_SUPPORTED
 	if (status < 0) {
 #else
@@ -178,16 +189,6 @@ static void i2s_esp32_rx_callback(void *arg, int status)
 		stream->data->state = I2S_STATE_ERROR;
 		LOG_ERR("RX status bad: %d", status);
 		goto rx_disable;
-	}
-
-	if (stream->data->mem_block == NULL) {
-		if (stream->data->state != I2S_STATE_READY) {
-			stream->data->state = I2S_STATE_ERROR;
-			LOG_ERR("RX mem_block NULL");
-			goto rx_disable;
-		} else {
-			return;
-		}
 	}
 
 #if SOC_GDMA_SUPPORTED
@@ -334,11 +335,8 @@ static void i2s_esp32_rx_stop_transfer(const struct device *dev)
 	i2s_hal_clear_intr_status(hal, I2S_INTR_MAX);
 #endif /* SOC_GDMA_SUPPORTED */
 
-	if (stream->data->mem_block != NULL) {
-		k_mem_slab_free(stream->data->i2s_cfg.mem_slab, stream->data->mem_block);
-		stream->data->mem_block = NULL;
-		stream->data->mem_block_len = 0;
-	}
+	stream->data->mem_block = NULL;
+	stream->data->mem_block_len = 0;
 }
 
 #endif /* I2S_ESP32_IS_DIR_EN(rx) */
@@ -356,7 +354,6 @@ static void i2s_esp32_tx_callback(void *arg, int status)
 	const struct i2s_esp32_cfg *const dev_cfg = dev->config;
 	const struct i2s_esp32_stream *stream = &dev_cfg->tx;
 	struct queue_item item;
-	void *mem_block_tmp;
 	int err;
 
 	if (!stream->data->dma_pending) {
@@ -364,6 +361,14 @@ static void i2s_esp32_tx_callback(void *arg, int status)
 	}
 
 	stream->data->dma_pending = false;
+
+	if (stream->data->mem_block == NULL) {
+		LOG_ERR("TX mem_block NULL");
+		stream->data->state = I2S_STATE_ERROR;
+		goto tx_disable;
+	}
+
+	k_mem_slab_free(stream->data->i2s_cfg.mem_slab, stream->data->mem_block);
 
 #if SOC_GDMA_SUPPORTED
 	if (status < 0) {
@@ -375,30 +380,12 @@ static void i2s_esp32_tx_callback(void *arg, int status)
 		goto tx_disable;
 	}
 
-	if (stream->data->mem_block == NULL) {
-		if (stream->data->state != I2S_STATE_READY) {
-			stream->data->state = I2S_STATE_ERROR;
-			LOG_ERR("TX mem_block NULL");
-			goto tx_disable;
-		} else {
-			return;
-		}
-	}
-
 	if (stream->data->state == I2S_STATE_STOPPING) {
-		if (k_msgq_num_used_get(&stream->data->queue) == 0) {
-			stream->data->state = I2S_STATE_READY;
-			goto tx_disable;
-		} else if (stream->data->stop_without_draining == true) {
-			stream->conf->queue_drop(stream);
+		if (k_msgq_num_used_get(&stream->data->queue) == 0 ||
+		    stream->data->stop_without_draining == true) {
 			stream->data->state = I2S_STATE_READY;
 			goto tx_disable;
 		}
-	}
-
-	if (stream->data->last_block) {
-		stream->data->state = I2S_STATE_READY;
-		goto tx_disable;
 	}
 
 	err = k_msgq_get(&stream->data->queue, &item, K_NO_WAIT);
@@ -407,8 +394,6 @@ static void i2s_esp32_tx_callback(void *arg, int status)
 		LOG_ERR("TX queue empty: %d", err);
 		goto tx_disable;
 	}
-
-	mem_block_tmp = stream->data->mem_block;
 
 	stream->data->mem_block = item.buffer;
 	stream->data->mem_block_len = item.size;
@@ -419,8 +404,6 @@ static void i2s_esp32_tx_callback(void *arg, int status)
 		LOG_ERR("Failed to restart TX transfer: %d", err);
 		goto tx_disable;
 	}
-
-	k_mem_slab_free(stream->data->i2s_cfg.mem_slab, mem_block_tmp);
 
 	return;
 
@@ -497,11 +480,8 @@ static void i2s_esp32_tx_stop_transfer(const struct device *dev)
 	i2s_hal_clear_intr_status(hal, I2S_INTR_MAX);
 #endif /* SOC_GDMA_SUPPORTED */
 
-	if (stream->data->mem_block != NULL) {
-		k_mem_slab_free(stream->data->i2s_cfg.mem_slab, stream->data->mem_block);
-		stream->data->mem_block = NULL;
-		stream->data->mem_block_len = 0;
-	}
+	stream->data->mem_block = NULL;
+	stream->data->mem_block_len = 0;
 }
 
 #endif /* I2S_ESP32_IS_DIR_EN(tx) */
@@ -658,7 +638,6 @@ static int i2s_esp32_start_dma(const struct device *dev, enum i2s_dir dir)
 		LOG_ERR("Failed to start DMA channel: %"PRIu32, stream->conf->dma_channel);
 		goto unlock;
 	}
-	stream->data->dma_pending = true;
 #else
 #if I2S_ESP32_IS_DIR_EN(rx)
 	if (dir == I2S_DIR_RX) {
@@ -676,6 +655,8 @@ static int i2s_esp32_start_dma(const struct device *dev, enum i2s_dir dir)
 	}
 #endif /* I2S_ESP32_IS_DIR_EN(tx) */
 #endif /* SOC_GDMA_SUPPORTED */
+
+	stream->data->dma_pending = true;
 
 unlock:
 	irq_unlock(key);
@@ -1197,7 +1178,6 @@ static int i2s_esp32_trigger_stream(const struct device *dev, const struct i2s_e
 			irq_unlock(key);
 			return -EIO;
 		}
-		stream->data->last_block = false;
 		stream->data->state = I2S_STATE_RUNNING;
 		irq_unlock(key);
 		break;
@@ -1215,7 +1195,6 @@ static int i2s_esp32_trigger_stream(const struct device *dev, const struct i2s_e
 			stream->data->state = I2S_STATE_STOPPING;
 		} else {
 			stream->conf->stop_transfer(dev);
-			stream->data->last_block = true;
 			stream->data->state = I2S_STATE_READY;
 		}
 
@@ -1250,7 +1229,6 @@ static int i2s_esp32_trigger_stream(const struct device *dev, const struct i2s_e
 				stream->data->state = I2S_STATE_STOPPING;
 			} else {
 				stream->conf->stop_transfer(dev);
-				stream->data->last_block = true;
 				stream->data->state = I2S_STATE_READY;
 			}
 		}
@@ -1439,7 +1417,6 @@ static DEVICE_API(i2s, i2s_esp32_driver_api) = {
 		.i2s_cfg = {0},                                                                    \
 		.mem_block = NULL,                                                                 \
 		.mem_block_len = 0,                                                                \
-		.last_block = false,                                                               \
 		.stop_without_draining = false,                                                    \
 		.queue = {},                                                                       \
 		.irq_handle = NULL,                                                                \
