95a9b1d0c99169f7e1c5b6788dcdabe5764fb36b
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2025-07-17 10:54:59 +0800

    soc: nxp: mcx/mcxa: Enable poweroff for mcxa series
    This commit introduced the following changes:
    1. Selected kconfig option 'HAS_POWEROFF' for NXP
       MCXA series.
    2. Added power.c for NXP MCXA series, in this file,
       we currently implemented 'z_sys_poweroff()' function.
    3. For the MCXA series, after waking up from the deep
       power down mode, the reset handler will be executed,
       and we need to release the I/O pads and certain peripheral
       devices to normal operating mode in 'soc_reset_hook'.
    
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

--- soc/nxp/mcx/mcxa/soc.c
+++ soc/nxp/mcx/mcxa/soc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 NXP
+ * Copyright 2024-2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -16,10 +16,23 @@
 #include <zephyr/device.h>
 #include <zephyr/init.h>
 #include <soc.h>
+#if defined(CONFIG_PM) || defined(CONFIG_POWEROFF)
+#include <fsl_spc.h>
+#include <fsl_cmc.h>
+#endif
 
 #ifdef CONFIG_SOC_RESET_HOOK
 void soc_reset_hook(void)
 {
+#if defined(CONFIG_PM) || defined(CONFIG_POWEROFF)
+	/* Release the I/O pads and certain peripherals to normal run mode state,
+	 * for in Power Down mode they will be in a latched state.
+	 */
+	if ((CMC_GetSystemResetStatus(CMC) & kCMC_WakeUpReset) != 0UL) {
+		SPC_ClearPeriphIOIsolationFlag(SPC0);
+	}
+#endif
+
 	SystemInit();
 }
 #endif
