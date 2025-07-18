11b5edfcb9c79fc83899d1c5ac9a43d76b99a20e
Jordan Yates <jordan@embeint.com>
2025-07-18 21:42:42 +1000

    fuel_gauge: composite: update next sample time
    Set the time for the next sample from the point the sampling finishes,
    not from when `composite_get_prop` is called.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- drivers/fuel_gauge/composite/fuel_gauge_composite.c
+++ drivers/fuel_gauge/composite/fuel_gauge_composite.c
@@ -61,7 +61,8 @@ static int composite_get_prop(const struct device *dev, fuel_gauge_prop_t prop,
 {
 	const struct composite_config *config = dev->config;
 	struct composite_data *data = dev->data;
-	k_ticks_t now = k_uptime_ticks();
+	const k_ticks_t validity_ticks =
+		k_ms_to_ticks_near64(CONFIG_FUEL_GAUGE_COMPOSITE_DATA_VALIDITY_MS);
 	struct sensor_value sensor_val;
 	enum sensor_channel sensor_chan;
 	int64_t voltage;
@@ -76,7 +77,7 @@ static int composite_get_prop(const struct device *dev, fuel_gauge_prop_t prop,
 	BUILD_ASSERT(offsetof(union fuel_gauge_prop_val, current_ua) ==
 		     offsetof(union fuel_gauge_prop_val, avg_current_ua));
 
-	if (now >= data->next_reading) {
+	if (k_uptime_ticks() >= data->next_reading) {
 		/* Trigger a sample on the input devices */
 		rc = composite_fetch(config->source_primary);
 		if ((rc == 0) && config->source_secondary) {
@@ -86,8 +87,7 @@ static int composite_get_prop(const struct device *dev, fuel_gauge_prop_t prop,
 			return rc;
 		}
 		/* Update timestamp for next reading */
-		data->next_reading =
-			now + k_ms_to_ticks_near64(CONFIG_FUEL_GAUGE_COMPOSITE_DATA_VALIDITY_MS);
+		data->next_reading = k_uptime_ticks() + validity_ticks;
 	}
 
 	switch (prop) {
