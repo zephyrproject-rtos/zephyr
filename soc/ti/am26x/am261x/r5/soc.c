2fd5bdfae62d708024a752becd07b27d039f9c31
Mihira Madhava Bollapragada <madhava@ti.com>
2025-07-21 16:11:57 +0530

    am261x: cache: enable cache
    the arch_cache_enable is currently empty. hence the cache initialization
    is now being called from the soc-init-hook
    
    Signed-off-by: Mihira Madhava Bollapragada <madhava@ti.com>

--- soc/ti/am26x/am261x/r5/soc.c
+++ soc/ti/am26x/am261x/r5/soc.c
@@ -6,6 +6,7 @@
 
 #include <stdint.h>
 #include <zephyr/fatal.h>
+#include <zephyr/cache.h>
 
 #include "soc.h"
 
@@ -46,7 +47,22 @@ int z_soc_irq_is_enabled(unsigned int irq)
 	return z_vim_irq_is_enabled(irq);
 }
 
+void cache_init(void)
+{
+#ifdef CONFIG_ICACHE
+	arch_icache_disable();
+	arch_icache_invd_all();
+	arch_icache_enable();
+#endif /* CONFIG_ICACHE */
+#ifdef CONFIG_DCACHE
+	arch_dcache_disable();
+	arch_dcache_invd_all();
+	arch_dcache_enable();
+#endif /* CONFIG_DCACHE */
+}
+
 void soc_early_init_hook(void)
 {
+	cache_init();
 	am26x_unlock_all_ctrl_partitions();
 }
