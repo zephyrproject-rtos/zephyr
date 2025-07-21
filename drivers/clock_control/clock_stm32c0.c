231e9b1702736e30ad84baa5e0cfa1828c30de0d
Tomáš Juřena <jurenatomas@gmail.com>
2025-07-21 15:35:33 +0200

    drivers: clock: stm32c0: Add an option to enable CRS for HSI48
    Allows enabling the Clock Recovery System (CRS) for HSI48 to achieve
    the expected accuracy for USB transfers. Uses USB SOF packet by default.
    
    Signed-off-by: Tomáš Juřena <jurenatomas@gmail.com>

--- drivers/clock_control/clock_stm32c0.c
+++ drivers/clock_control/clock_stm32c0.c
@@ -8,6 +8,7 @@
 
 #include <soc.h>
 #include <stm32_ll_bus.h>
+#include <stm32_ll_crs.h>
 #include <stm32_ll_rcc.h>
 #include <stm32_ll_utils.h>
 #include <zephyr/drivers/clock_control.h>
@@ -22,4 +23,11 @@ void config_enable_default_clocks(void)
 {
 	/* Enable the power interface clock */
 	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+
+#if IS_ENABLED(STM32_HSI48_CRS_USB_SOF)
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_CRS);
+	LL_CRS_SetSyncSignalSource(LL_CRS_SYNC_SOURCE_USB);
+	LL_CRS_EnableAutoTrimming();
+	LL_CRS_EnableFreqErrorCounter();
+#endif
 }
