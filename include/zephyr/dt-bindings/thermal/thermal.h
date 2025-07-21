2b1ece652df07b506bf30a876f8f53e28e5b5359
Niu Zhihong <zhihong@nzhnb.com>
2025-07-21 15:22:25 +0800

    dt-bindings: add thermal define
    Added thermal header file.
    
    Signed-off-by: Niu Zhihong <zhihong@nzhnb.com>

---
+++ include/zephyr/dt-bindings/thermal/thermal.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright The Zephyr Project Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_THERMAL_THERMAL_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_THERMAL_THERMAL_H_
+
+/* On cooling devices upper and lower limits */
+#define THERMAL_NO_LIMIT (~0)
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_THERMAL_THERMAL_H_ */
