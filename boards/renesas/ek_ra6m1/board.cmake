a60e2585b8b36d7af73c86c57db1389e314caef0
The Nguyen <the.nguyen.yf@renesas.com>
2025-07-23 04:20:01 +0000

    boards: renesas: support pyocd runner for Renesas RA boards
    Add pyocd runner support for Renesas RA boards
    
    Signed-off-by: The Nguyen <the.nguyen.yf@renesas.com>

--- boards/renesas/ek_ra6m1/board.cmake
+++ boards/renesas/ek_ra6m1/board.cmake
@@ -2,5 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--device=R7FA6M1AD")
+board_runner_args(pyocd "--target=R7FA6M1AD")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
