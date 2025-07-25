c09703cd5c6d4cd10a7f632e210f8b5df6a0756c
Zacck <coderv63@gmail.com>
2025-07-25 16:46:15 +0200

    drivers: spi: formatting lost commit

--- drivers/spi/spi_nxp_sc18is606.c
+++ drivers/spi/spi_nxp_sc18is606.c
@@ -13,66 +13,47 @@
 
 #define DT_DRV_COMPAT nxp_sc18is606
 
-
 LOG_MODULE_REGISTER(nxp_sc18is606, CONFIG_SPI_LOG_LEVEL);
 
+#define SC18IS606_CONFIG_SPI 0xF0
+#define CLEAR_INTERRUPT      0xF1
+#define IDLE_MODE            0xF2
+#define GPIO_CONFIGURATION   0xF7
+#define GPIO_ENABLE          0xF6
+#define GPIO_WRITE           0xF4
+#define GPIO_READ            0xF5
+#define READ_VERSION         0xFE
 
-#define SC18IS606_CONFIG_SPI		0xF0
-#define CLEAR_INTERRUPT		0xF1
-#define IDLE_MODE		0xF2
-#define GPIO_CONFIGURATION	0xF7
-#define GPIO_ENABLE		0xF6
-#define GPIO_WRITE		0xF4
-#define GPIO_READ		0xF5
-#define READ_VERSION		0xFE
-
-
-#if DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) == 0
-#warning "Nxp SC18IS606 driver enabled without any devices"
-#endif
-
-
-struct nxp_sc18is606_data
-{
+struct nxp_sc18is606_data {
 	const struct device *i2c_dev;
 	uint8_t i2c_addr;
 	uint32_t spi_clock_freq;
 	uint8_t spi_mode;
 };
 
-
-struct nxp_sc18is606_config
-{
+struct nxp_sc18is606_config {
 	struct i2c_dt_spec i2c_controller;
 };
 
-
 static int sc18is606_write_reg(const struct i2c_dt_spec *i2c, uint8_t reg, uint8_t value)
 {
-	uint8_t  buffer[2] = {reg, value};
+	uint8_t buffer[2] = {reg, value};
 	return i2c_write_dt(i2c, buffer, sizeof(buffer));
 }
 
-
-static int sc18is606_spi_transceive(const struct device *dev,
-		const struct spi_config *spi_cfg,
-		const struct spi_buf_set *tx_buffer_set,
-		const struct spi_buf_set *rx_buffer_set)
+static int sc18is606_spi_transceive(const struct device *dev, const struct spi_config *spi_cfg,
+				    const struct spi_buf_set *tx_buffer_set,
+				    const struct spi_buf_set *rx_buffer_set)
 {
-	//const struct nxp_sc18is606_config *cfg = dev->config;
 	const struct nxp_sc18is606_data *data = dev->data;
 	const struct device *i2c_dev = data->i2c_dev;
 	int ret;
 
-
-	//our buffers are real?
-	if(!tx_buffer_set || !rx_buffer_set) {
+	if (!tx_buffer_set || !rx_buffer_set) {
 		LOG_ERR("SC18IS606 Invalid Buffers");
 		return -EINVAL;
-
 	}
 
-	//TX
 	const struct spi_buf *tx_buf = &tx_buffer_set->buffers[0];
 	const size_t len = tx_buf->len;
 
@@ -81,17 +62,15 @@ static int sc18is606_spi_transceive(const struct device *dev,
 	memcpy(&sc18_buf[1], tx_buf->buf, len);
 
 	ret = i2c_write(i2c_dev, sc18_buf, sizeof(sc18_buf), data->i2c_addr);
-		if(ret) {
-			LOG_ERR("SPI write failed: %d", ret);
-			return ret;
-		}
+	if (ret) {
+		LOG_ERR("SPI write failed: %d", ret);
+		return ret;
+	}
 
-
-	//RX
-	if(rx_buffer_set && rx_buffer_set->buffers && rx_buffer_set->count > 0){
+	if (rx_buffer_set && rx_buffer_set->buffers && rx_buffer_set->count > 0) {
 		const struct spi_buf *buf = &rx_buffer_set->buffers[0];
 		ret = i2c_read(i2c_dev, buf->buf, buf->len, data->i2c_addr);
-		if(ret) {
+		if (ret) {
 			LOG_ERR("SPI read failed: %d", ret);
 			return ret;
 		}
@@ -100,23 +79,28 @@ static int sc18is606_spi_transceive(const struct device *dev,
 	return 0;
 }
 
+int sc18is606_spi_release(const struct device *dev, const struct spi_config *config)
+{
+	struct spi_bitbang_data *data = dev->data;
+	struct spi_context *ctx = &data->ctx;
 
-// expose driver
+	spi_context_unlock_unconditionally(ctx);
+	return 0;
+}
+
 static const struct spi_driver_api sc18is606_api = {
 	.transceive = sc18is606_spi_transceive,
+	.release = sc18is606_spi_release,
 };
 
-
-// Init  the driver
 static int sc18is606_init(const struct device *dev)
 {
 	const struct nxp_sc18is606_config *cfg = dev->config;
 	struct nxp_sc18is606_data *data = dev->data;
 	int ret;
 
-	// Get Parent
 	data->i2c_dev = cfg->i2c_controller.bus;
-	if(!data->i2c_dev) {
+	if (!data->i2c_dev) {
 		LOG_ERR("I2C controller %s not found", data->i2c_dev->name);
 		return -ENODEV;
 	}
@@ -124,7 +108,7 @@ static int sc18is606_init(const struct device *dev)
 	LOG_INF("Using I2C controller: %s", data->i2c_dev->name);
 
 	ret = sc18is606_write_reg(&cfg->i2c_controller, SC18IS606_CONFIG_SPI, data->spi_mode);
-	if(ret) {
+	if (ret) {
 		LOG_ERR("Failed to CONFIGURE the SC18IS606: %d", ret);
 		return ret;
 	}
@@ -133,21 +117,17 @@ static int sc18is606_init(const struct device *dev)
 	return 0;
 }
 
-//define our driver instance
-#define NXP_SC18IS606_DEFINE(inst) \
-	static struct nxp_sc18is606_data sc18is606_data_##inst = { \
-		.i2c_addr = DT_INST_REG_ADDR(inst), \
-		.spi_clock_freq = DT_INST_PROP(inst, spi_clock_frequency), \
-		.spi_mode = DT_INST_PROP(inst, spi_mode), \
-	}; \
-	static const struct nxp_sc18is606_config sc18is606_config_##inst = { \
-		.i2c_controller = I2C_DT_SPEC_INST_GET(inst), \
-	}; \
-	DEVICE_DT_INST_DEFINE(inst, sc18is606_init, NULL, \
-			&sc18is606_data_##inst, &sc18is606_config_##inst, \
-			POST_KERNEL, CONFIG_SPI_INIT_PRIORITY, \
-			&sc18is606_api);
-//when status is ok make an instance
+#define NXP_SC18IS606_DEFINE(inst)                                                                 \
+	static struct nxp_sc18is606_data sc18is606_data_##inst = {                                 \
+		.i2c_addr = DT_INST_REG_ADDR(inst),                                                \
+		.spi_clock_freq = DT_INST_PROP(inst, spi_clock_frequency),                         \
+		.spi_mode = DT_INST_PROP(inst, spi_mode),                                          \
+	};                                                                                         \
+	static const struct nxp_sc18is606_config sc18is606_config_##inst = {                       \
+		.i2c_controller = I2C_DT_SPEC_INST_GET(inst),                                      \
+	};                                                                                         \
+	DEVICE_DT_INST_DEFINE(inst, sc18is606_init, NULL, &sc18is606_data_##inst,                  \
+			      &sc18is606_config_##inst, POST_KERNEL, CONFIG_SPI_INIT_PRIORITY,     \
+			      &sc18is606_api);
+
 DT_INST_FOREACH_STATUS_OKAY(NXP_SC18IS606_DEFINE)
-
-
