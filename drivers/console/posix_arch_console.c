619d12ce2971952044940dc08cf8041c82fda4ed
Henrik Brix Andersen <henrik@brixandersen.dk>
2025-07-28 19:29:33 +0000

    drivers: console: posix_arch_console: remove build warning
    Remove build warning when building the posix_arch_console driver with
    CONFIG_PRINTK=n and CONFIG_STDOUT_CONSOLE=n.
    
    Fixes: #93790
    
    Signed-off-by: Henrik Brix Andersen <henrik@brixandersen.dk>
    (cherry picked from commit 3eedebe03139343104dd5b015159717a67215f42)

--- drivers/console/posix_arch_console.c
+++ drivers/console/posix_arch_console.c
@@ -12,6 +12,7 @@
 static char stdout_buff[_STDOUT_BUF_SIZE];
 static int n_pend; /* Number of pending characters in buffer */
 
+#if defined(CONFIG_PRINTK) || defined(CONFIG_STDOUT_CONSOLE)
 static int print_char(int c)
 {
 	int printnow = 0;
@@ -34,6 +35,7 @@ static int print_char(int c)
 	}
 	return c;
 }
+#endif /* defined(CONFIG_PRINTK) || defined(CONFIG_STDOUT_CONSOLE) */
 
 /**
  * Ensure that whatever was written thru printk is displayed now
