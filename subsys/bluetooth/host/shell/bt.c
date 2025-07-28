f2f430f8b82a38c32f437ef11e2cb9e5e3c3e59e
Yago Fontoura do Rosario <yafo@demant.com>
2025-07-28 15:20:39 +0200

    Bluetooth: Host: Conn callbacks registration with BT_CONN_CB_DEFINE
    This commit registers the connection callbacks using BT_CONN_CB_DEFINE,
    which ensures they are set up at link time rather than runtime.
    
    Signed-off-by: Yago Fontoura do Rosario <yafo@demant.com>

--- subsys/bluetooth/host/shell/bt.c
+++ subsys/bluetooth/host/shell/bt.c
@@ -1191,7 +1191,7 @@ static void le_cs_config_removed(struct bt_conn *conn, uint8_t config_id)
 }
 #endif
 
-static struct bt_conn_cb conn_callbacks = {
+BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.connected = connected,
 	.disconnected = disconnected,
 	.le_param_req = le_param_req,
@@ -1374,10 +1374,6 @@ static void bt_ready(int err)
 
 #if defined(CONFIG_BT_CONN)
 	default_conn = NULL;
-
-	/* Unregister to avoid register repeatedly */
-	bt_conn_cb_unregister(&conn_callbacks);
-	bt_conn_cb_register(&conn_callbacks);
 #endif /* CONFIG_BT_CONN */
 
 #if defined(CONFIG_BT_PER_ADV_SYNC)
