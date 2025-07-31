2769cb9e10c87e1cab074d9d7f5a380ff7550cad
Bas van Loon <bas@arch-embedded.com>
2025-07-31 15:43:22 +0200

    net: openthread: Use proper macro to calculate timeout.
    Use K_USEC() to convert the timeout value in microseconds to a value in
    ticks dropping the manual conversion from microseconds to milliseconds.
    
    Signed-off-by: Bas van Loon <bas@arch-embedded.com>

--- modules/openthread/platform/hdlc_interface.cpp
+++ modules/openthread/platform/hdlc_interface.cpp
@@ -152,14 +152,13 @@ exit:
 otError HdlcInterface::WaitForFrame(uint64_t aTimeoutUs)
 {
 	otError error = OT_ERROR_RESPONSE_TIMEOUT;
-	uint32_t timeout_ms = (uint32_t)(aTimeoutUs / 1000U);
 	uint32_t event_bits;
 
 	do {
 		/* Wait for k_spinel_hdlc_frame_ready_event indicating a frame has been received */
 		event_bits = k_event_wait(&spinel_hdlc_event,
 					  HdlcInterface::k_spinel_hdlc_frame_ready_event, false,
-					  K_MSEC(timeout_ms));
+					  K_USEC(aTimeoutUs));
 		k_event_clear(&spinel_hdlc_event, HdlcInterface::k_spinel_hdlc_frame_ready_event);
 		if ((event_bits & HdlcInterface::k_spinel_hdlc_frame_ready_event) != 0) {
 			/* Event is set, it means a frame has been received and can be decoded
