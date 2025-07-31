848b416af2e423ca8d2675cb27189c43c43cf8e5
Fabio Baltieri <fabiobaltieri@google.com>
2025-07-31 15:55:22 +0100

    twister: add a --flash-command flag
    Currently twister requires west to flash a board, either directly using
    west-flash or indirectly through the cmake flash target.
    
    Add an option to specify a custom flash script, this is passed a
    build-dir and board-id flags so it can be used to implement custom
    flashing scripts in a system that does not use west.
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

--- scripts/pylib/twister/twisterlib/handlers.py
+++ scripts/pylib/twister/twisterlib/handlers.py
@@ -9,6 +9,7 @@
 
 import argparse
 import contextlib
+import csv
 import logging
 import math
 import os
@@ -544,7 +545,19 @@ class DeviceHandler(Handler):
                 proc.communicate()
                 logger.error(f"{script} timed out")
 
+    def _create_flash_command(self, hardware):
+        command = next(csv.reader([self.options.flash_command]))
+        command.extend(['--build-dir', self.build_dir])
+
+        board_id = hardware.probe_id or hardware.id
+        if board_id:
+            command.extend(['--board-id', board_id])
+        return command
+
     def _create_command(self, runner, hardware):
+        if self.options.flash_command:
+            return self._create_flash_command(hardware)
+
         command = ["west", "flash", "--skip-rebuild", "-d", self.build_dir]
         command_extra_args = []
 
