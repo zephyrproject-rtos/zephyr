dd894035c5b4c4a5e94999e29af278b0c1aeaaea
Fabio Baltieri <fabiobaltieri@google.com>
2025-07-31 15:55:22 +0100

    twister: add a --flash-command flag
    Currently twister requires west to flash a board, either directly using
    west-flash or indirectly through the cmake flash target.
    
    Add an option to specify a custom flash script, this is passed a
    build-dir and board-id flags so it can be used to implement custom
    flashing scripts in a system that does not use west.
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

--- scripts/pylib/twister/twisterlib/environment.py
+++ scripts/pylib/twister/twisterlib/environment.py
@@ -836,6 +836,15 @@ structure in the main Zephyr tree: boards/<vendor>/<board_name>/""")
         will translate to "west flash --runner pyocd"
         """
     )
+    parser.add_argument(
+        "--flash-command",
+        help="""Instead of 'west flash', uses a custom flash command to flash
+            when running with --device-testing. Supports comma-separated
+            argument list, the script is also passed a --build-dir flag with
+            the build directory as an argument, and a --board-id flag with the
+            board or probe id if available.
+        """
+    )
 
     parser.add_argument(
         "-X", "--fixture", action="append", default=[],
