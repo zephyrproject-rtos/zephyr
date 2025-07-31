3b0296db2708350e62d0c3a8d67dca0c55e0a9f2
Declan Snyder <declan.snyder@nxp.com>
2025-07-31 14:47:41 -0500

    modules: hal_nxp: Remove CONFIG_HAS_MCUX_FLEXCOMM
    The presence of the flexcomm should be driven by DT, not this legacy
    kconfig.
    
    Signed-off-by: Declan Snyder <declan.snyder@nxp.com>

--- drivers/i2c/i2c_mcux_lpi2c.c
+++ drivers/i2c/i2c_mcux_lpi2c.c
@@ -472,6 +472,12 @@ static int mcux_lpi2c_target_unregister(const struct device *dev,
 }
 #endif /* CONFIG_I2C_TARGET */
 
+#if DT_HAS_COMPAT_STATUS_OKAY(nxp_lp_flexcomm)
+#define LPI2C_IRQHANDLE_ARG LPI2C_GetInstance(base)
+#else
+#define LPI2C_IRQHANDLE_ARG base
+#endif
+
 static void mcux_lpi2c_isr(const struct device *dev)
 {
 	struct mcux_lpi2c_data *data = dev->data;
@@ -482,11 +488,8 @@ static void mcux_lpi2c_isr(const struct device *dev)
 		mcux_lpi2c_slave_irq_handler(dev);
 	}
 #endif /* CONFIG_I2C_TARGET */
-#if CONFIG_HAS_MCUX_FLEXCOMM
-	LPI2C_MasterTransferHandleIRQ(LPI2C_GetInstance(base), &data->handle);
-#else
-	LPI2C_MasterTransferHandleIRQ(base, &data->handle);
-#endif
+
+	LPI2C_MasterTransferHandleIRQ(LPI2C_IRQHANDLE_ARG, &data->handle);
 }
 
 static int mcux_lpi2c_init(const struct device *dev)
