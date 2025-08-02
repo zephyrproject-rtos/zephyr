7f7413452b8e92c585603bfc0fa6ea6883ccfa4d
Sean Kyer <Sean.Kyer@analog.com>
2025-08-01 19:44:50 -0700

    tests: cpu_freq: Add SoC and on_demand tests
    Add tests for CPU frequency subsystem covering
    SoC integration of pstate as well as the on_demand
    policy.
    
    Signed-off-by: Sean Kyer <Sean.Kyer@analog.com>

---
+++ tests/subsys/cpu_freq/policies/on_demand/src/main.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2025 Analog Devices, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/ztest.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/cpu_freq/policy.h>
+#include <zephyr/cpu_freq/cpu_freq.h>
+
+LOG_MODULE_REGISTER(cpu_freq_on_demand_test, LOG_LEVEL_INF);
+
+/*
+ * Test APIs of on_demand CPU frequency policy.
+ */
+ZTEST(cpu_freq_on_demand, test_pstates)
+{
+	int ret;
+	const struct pstate *test_pstate;
+
+	/* Test invalid arg */
+	zassert_equal(cpu_freq_policy_select_pstate(NULL), -EINVAL,
+		      "Expected -EINVAL for NULL pstate_out");
+
+	/* Get pstate at near 100% load */
+	k_busy_wait(10000);
+
+	ret = cpu_freq_policy_select_pstate(&test_pstate);
+	zassert_equal(ret, 0, "Expected success from cpu_freq_policy_select_pstate");
+
+	int prev_threshold = test_pstate->load_threshold;
+
+	/* Get a different pstate policy */
+	ret = cpu_freq_policy_select_pstate(&test_pstate);
+	zassert_equal(ret, 0, "Expected success from cpu_freq_policy_select_pstate");
+
+	zassert_not_equal(test_pstate->load_threshold, prev_threshold,
+			  "Expected different p-state after sleep");
+}
+
+ZTEST_SUITE(cpu_freq_on_demand, NULL, NULL, NULL, NULL, NULL);
