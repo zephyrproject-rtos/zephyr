6d4006143cd443b7d9dce689c4f6a7d07d50145f
Camille BAUD <mail@massdriver.space>
2025-08-04 07:38:27 +0200

    drivers: syscon: Add condition for BL70x
    BL70x = BL60x in this case
    
    Signed-off-by: Camille BAUD <mail@massdriver.space>

--- drivers/syscon/syscon_bflb_efuse.c
+++ drivers/syscon/syscon_bflb_efuse.c
@@ -73,7 +73,7 @@ static void efuse_bflb_efuse_read(const struct device *dev)
 	tmp =	EF_CTRL_EFUSE_CTRL_PROTECT
 		| (EF_CTRL_OP_MODE_AUTO << EF_CTRL_EF_IF_0_MANUAL_EN_POS)
 		| (EF_CTRL_PARA_DFT << EF_CTRL_EF_IF_0_CYC_MODIFY_POS)
-#if defined(CONFIG_SOC_SERIES_BL60X)
+#if defined(CONFIG_SOC_SERIES_BL60X) || defined(CONFIG_SOC_SERIES_BL70X)
 		| (EF_CTRL_SAHB_CLK << EF_CTRL_EF_CLK_SAHB_DATA_SEL_POS)
 #endif
 		| (1 << EF_CTRL_EF_IF_AUTO_RD_EN_POS)
@@ -95,7 +95,7 @@ static void efuse_bflb_efuse_read(const struct device *dev)
 	tmp =	EF_CTRL_EFUSE_CTRL_PROTECT
 		| (EF_CTRL_OP_MODE_AUTO << EF_CTRL_EF_IF_0_MANUAL_EN_POS)
 		| (EF_CTRL_PARA_DFT << EF_CTRL_EF_IF_0_CYC_MODIFY_POS)
-#if defined(CONFIG_SOC_SERIES_BL60X)
+#if defined(CONFIG_SOC_SERIES_BL60X) || defined(CONFIG_SOC_SERIES_BL70X)
 		| (EF_CTRL_EF_CLK << EF_CTRL_EF_CLK_SAHB_DATA_SEL_POS)
 #endif
 		| (1 << EF_CTRL_EF_IF_AUTO_RD_EN_POS)
@@ -109,7 +109,7 @@ static void efuse_bflb_efuse_read(const struct device *dev)
 	tmp =	EF_CTRL_EFUSE_CTRL_PROTECT
 		| (EF_CTRL_OP_MODE_AUTO << EF_CTRL_EF_IF_0_MANUAL_EN_POS)
 		| (EF_CTRL_PARA_DFT << EF_CTRL_EF_IF_0_CYC_MODIFY_POS)
-#if defined(CONFIG_SOC_SERIES_BL60X)
+#if defined(CONFIG_SOC_SERIES_BL60X) || defined(CONFIG_SOC_SERIES_BL70X)
 		| (EF_CTRL_EF_CLK << EF_CTRL_EF_CLK_SAHB_DATA_SEL_POS)
 #endif
 		| (1 << EF_CTRL_EF_IF_AUTO_RD_EN_POS)
@@ -131,7 +131,7 @@ static void efuse_bflb_efuse_read(const struct device *dev)
 	tmp =	EF_CTRL_EFUSE_CTRL_PROTECT
 		| (EF_CTRL_OP_MODE_AUTO << EF_CTRL_EF_IF_0_MANUAL_EN_POS)
 		| (EF_CTRL_PARA_DFT << EF_CTRL_EF_IF_0_CYC_MODIFY_POS)
-#if defined(CONFIG_SOC_SERIES_BL60X)
+#if defined(CONFIG_SOC_SERIES_BL60X) || defined(CONFIG_SOC_SERIES_BL70X)
 		| (EF_CTRL_SAHB_CLK << EF_CTRL_EF_CLK_SAHB_DATA_SEL_POS)
 #endif
 		| (1 << EF_CTRL_EF_IF_AUTO_RD_EN_POS)
