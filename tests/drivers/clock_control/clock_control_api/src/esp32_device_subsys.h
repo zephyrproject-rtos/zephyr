d23617d47a500e8d7802254d5d100439bbb83e61
Raffael Rostagno <raffael.rostagno@espressif.com>
2025-08-05 14:00:11 -0300

    tests: clock: esp32h2: Manage testcases
    Render test settings more generic regarding clock options, in order
    to better support new devices. Add ESP32-H2 testcase to rtc_clk suite.
    Remove testcase from clock_control testsuite, as RNG clock control
    isn't yet supported.
    
    Signed-off-by: Raffael Rostagno <raffael.rostagno@espressif.com>

--- tests/drivers/clock_control/clock_control_api/src/esp32_device_subsys.h
+++ tests/drivers/clock_control/clock_control_api/src/esp32_device_subsys.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Espressif Systems (Shanghai) Co., Ltd.
+ * Copyright (c) 2024-2025 Espressif Systems (Shanghai) Co., Ltd.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -21,7 +21,9 @@ static const struct device_subsys_data subsys_data[] = {
 #else
 	{.subsys = (void *) ESP32_TIMG0_MODULE},
 #endif
+#if !defined(CONFIG_SOC_SERIES_ESP32H2)
 	{.subsys = (void *) ESP32_RNG_MODULE},
+#endif
 };
 
 static const struct device_data devices[] = {
