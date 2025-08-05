289653b678c234a48194fef723bac63c6f59fdb0
Andy Ross <andyross@google.com>
2025-08-05 12:12:04 -0700

    tests/benchmarks/swap: Add interrupt cases too
    This microbenchmark has been super-useful.  Add similar cases looking
    at targetting interupt latency:
    
    + "IRQ" tests the cycles taken from the instant of an interrupt
      trigger (as implemented, it's an arch_irq_lock() being released with
      a timer interrupt pending) through a noop ISR (a k_timer callback),
      and back to the interrupting thread.
    
    + "IRQ_P" is a similar preemption test: it measures the time taken
       from the same trigger, into a higher-priority thread that
       timestamps the instant of its wakeup (in this case as a return from
       k_sem_take()).
    
    Signed-off-by: Andy Ross <andyross@google.com>

--- tests/benchmarks/swap/src/main.c
+++ tests/benchmarks/swap/src/main.c
@@ -40,6 +40,14 @@
 #error "Don't enable memory management hardware in a microbenchmark!"
 #endif
 
+#if defined(CONFIG_FPU)
+#error "Don't enable FPU/DSP in a microbenchmark!"
+#endif
+
+#if defined(CONFIG_HW_STACK_PROTECTION)
+#error "Don't enable hardware stack protection in a microbenchmark!"
+#endif
+
 #define HI_PRIO   0
 #define LO_PRIO   1
 #define MAIN_PRIO 2
@@ -60,6 +68,30 @@ K_SEM_DEFINE(done_sem, 0, 999);
 K_THREAD_DEFINE(thread0, 1024, thread_fn, &t0_0, &t0_1, NULL, HI_PRIO, 0, 0);
 K_THREAD_DEFINE(thread1, 1024, thread_fn, &t1_0, &t1_1, NULL, HI_PRIO, 0, 0);
 
+void report(const char *name, size_t n)
+{
+	uint64_t total = 0;
+
+	for (int i = 0; i < n; i++) {
+		total += samples[i];
+	}
+
+	float var = 0, avg = total / (float)n;
+
+	for (int i = 0; i < n; i++) {
+		int d = samples[i] - avg;
+
+		var += d * d;
+	}
+
+	float stdev = sqrt(var / n);
+	int iavg = (int)(avg + 0.5f);
+	int stdev_i = (int)stdev;
+	int stdev_f = (int)(10 * (stdev - stdev_i));
+
+	printk("%s samples=%d average %d stdev %d.%d\n", name, n, iavg, stdev_i, stdev_f);
+}
+
 void thread_fn(void *t0_arg, void *t1_arg, void *c)
 {
 	uint32_t t0, t1, *t0_out = t0_arg, *t1_out = t1_arg;
@@ -79,9 +111,8 @@ void thread_fn(void *t0_arg, void *t1_arg, void *c)
 	}
 }
 
-int main(void)
+void swap_bench(void)
 {
-	uint64_t total = 0, avg8, var8 = 0;
 	size_t n = ARRAY_SIZE(samples);
 
 #ifdef CONFIG_SMP
@@ -122,25 +153,127 @@ int main(void)
 		uint32_t dt = time_delta(t0_0, t1_1);
 
 		samples[i] = dt;
-		total += dt;
 	}
 
 	k_thread_abort(thread0);
 	k_thread_abort(thread1);
 
-	/* Do the variance math left-shifted by 8 bits */
-	avg8 = (int)(total * 256 / n);
-	for (int i = 0; i < n; i++) {
-		int d = (int)(samples[i] * 256) - avg8;
+	report("SWAP", n);
+}
 
-		var8 += d * d;
+struct k_timer tm;
+
+K_SEM_DEFINE(hi_sem, 0, 9999);
+uint32_t t_preempt;
+
+void hi_thread_fn(void *a, void *b, void *c)
+{
+	while (true) {
+		k_sem_take(&hi_sem, K_FOREVER);
+		t_preempt = k_cycle_get_32();
+	}
+}
+
+K_THREAD_DEFINE(hi_thread, 1024, hi_thread_fn, NULL, NULL, NULL, -1, 0, 0);
+
+void timer0_fn(struct k_timer *t)
+{
+	/* empty */
+}
+
+/* The hardware devices I have see excursions in interrupt latency I
+ * don't understand.  It looks like more than one is queued up?  Check
+ * that nothing looks weird and retry if it does.  Doing this gives
+ * MUCH lower variance (like 2-3 cycle stdev and not 100+)
+ */
+bool retry(int i)
+{
+	if (i == 0) {
+		return false;
 	}
 
-	double stdev = sqrt((var8 / 256.0) / n);
-	int stdev_i = (int)stdev;
-	int stdev_f = (int)(10 * (stdev - stdev_i));
+	uint32_t dt = samples[i], dt0 = samples[i - 1];
 
-	printk("Swap average %d stdev %d.%d\n", (int)((avg8 + 128) / 256), stdev_i, stdev_f);
+	/* Check for >25% delta */
+	if (dt > dt0 && (dt - dt0) > (dt0 / 4)) {
+		return true;
+	}
+	return false;
+}
+
+void irq_bench(void)
+{
+	size_t n = MIN(ARRAY_SIZE(samples), 4 * CONFIG_SYS_CLOCK_TICKS_PER_SEC);
+	uint32_t t0, t1;
+	unsigned int key;
+
+	k_timer_init(&tm, timer0_fn, NULL);
+
+	for (int i = 0; i < n; i++) {
+		/* Lock interrupts before starting the timer, then
+		 * wait for two (!) ticks to roll over to be sure the
+		 * interrupt is queued.
+		 */
+		key = arch_irq_lock();
+		k_timer_start(&tm, K_TICKS(0), K_FOREVER);
+
+		k_busy_wait(k_ticks_to_us_ceil32(3));
+
+		/* Releasing the lock will fire the interrupt synchronously. */
+		t0 = k_cycle_get_32();
+		arch_irq_unlock(key);
+		t1 = k_cycle_get_32();
+
+		k_timer_stop(&tm);
+
+		samples[i] = t1 - t0;
+
+		if (retry(i)) {
+			i--;
+		}
+	}
+	report("IRQ", n);
+}
+
+/* Very similar test, but switches to hi_thread and checks time on
+ * interrupt exit there, to measure preemption overhead
+ */
+void irq_p_bench(void)
+{
+	size_t n = MIN(ARRAY_SIZE(samples), 4 * CONFIG_SYS_CLOCK_TICKS_PER_SEC);
+	uint32_t t0;
+	unsigned int key;
+
+	k_timer_init(&tm, timer0_fn, NULL);
+
+	for (int i = 0; i < n; i++) {
+		key = arch_irq_lock();
+		k_timer_start(&tm, K_TICKS(0), K_FOREVER);
+
+		k_busy_wait(k_ticks_to_us_ceil32(3));
+
+		k_sem_give(&hi_sem);
+		t0 = k_cycle_get_32();
+		arch_irq_unlock(key);
+
+		k_timer_stop(&tm);
+
+		samples[i] = t_preempt - t0;
+
+		if (retry(i)) {
+			i--;
+		}
+	}
+	report("IRQ_P", n);
+}
+
+int main(void)
+{
+	irq_bench();
+	irq_p_bench();
+
+	/* This disables the SysTick interrupt and must be last! */
+	swap_bench();
 
 	return 0;
 }
