a21522b7a80f3c3bf822d081f01533fb6f77e244
Michał Stasiak <michal.stasiak@nordicsemi.no>
2025-08-06 10:37:50 +0200

    drivers: spi: nrfx: release constlat mode on suspend
    Release instead of requesting constant latency mode
    on SPI suspend.
    
    Signed-off-by: Michał Stasiak <michal.stasiak@nordicsemi.no>

--- drivers/spi/spi_nrfx_spis.c
+++ drivers/spi/spi_nrfx_spis.c
@@ -437,7 +437,7 @@ static void spi_nrfx_suspend(const struct device *dev)
 #if SPIS_CROSS_DOMAIN_PINS_HANDLE
 		int err;
 
-		err = nrf_sys_event_request_global_constlat();
+		err = nrf_sys_event_release_global_constlat();
 		(void)err;
 		__ASSERT_NO_MSG(err >= 0);
 #else
