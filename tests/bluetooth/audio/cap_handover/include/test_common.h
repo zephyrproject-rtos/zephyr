69cabc4f5791b73a34156785728c431d62825e34
Emil Gydesen <emil.gydesen@nordicsemi.no>
2025-08-06 08:53:26 +0200

    tests: Bluetooth: CAP: Handover b->u unittests
    Add unit tests for the CAP handover broadcast to unicast
    functionality.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bluetooth/audio/cap_handover/include/test_common.h
+++ tests/bluetooth/audio/cap_handover/include/test_common.h
@@ -6,16 +6,24 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdint.h>
+
+#include <zephyr/bluetooth/addr.h>
 #include <zephyr/bluetooth/audio/bap.h>
 #include <zephyr/bluetooth/audio/bap_lc3_preset.h>
 #include <zephyr/bluetooth/audio/cap.h>
 #include <zephyr/bluetooth/conn.h>
 
+#define TEST_COMMON_ADV_TYPE     BT_ADDR_LE_PUBLIC
+#define TEST_COMMON_ADV_SID      0x01U
+#define TEST_COMMON_BROADCAST_ID 0x123456U
+#define TEST_COMMON_SRC_ID       0x00U
+
 void test_mocks_init(void);
 void test_mocks_cleanup(void);
 void mock_bt_csip_cleanup(void);
 
-void test_conn_init(struct bt_conn *conn);
+void test_conn_init(struct bt_conn *conn, uint8_t index);
 
 void test_unicast_set_state(struct bt_cap_stream *cap_stream, struct bt_conn *conn,
 			    struct bt_bap_ep *ep, struct bt_bap_lc3_preset *preset,
