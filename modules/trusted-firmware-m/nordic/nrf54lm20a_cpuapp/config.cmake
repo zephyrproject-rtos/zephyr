de402cb465e96a879816f613bbf686c747088026
Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>
2025-08-07 12:50:50 +0200

    boards: nordic: Add initial support for nRF54LM20A/ns
    Add board files for nRF54LM20A/ns.
    Update existing nRF54LM20A board files to support this.
    
    Signed-off-by: Dag Erik Gjørvad <dag.erik.gjorvad@nordicsemi.no>

---
+++ modules/trusted-firmware-m/nordic/nrf54lm20a_cpuapp/config.cmake
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2025, Nordic Semiconductor ASA.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set(NRF_SOC_VARIANT nrf54lm20a CACHE STRING "nRF SoC Variant")
+
+include(${PLATFORM_PATH}/common/nrf54lm20a/config.cmake)
