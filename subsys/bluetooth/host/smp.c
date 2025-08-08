b42525fc168bc374ee7fe7e6b5618d38fde2eefe
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2025-08-08 14:08:38 +0200

    bluetooth: host: smp: Update bt_keys addr when receiving identity addr
    In addition to aligning the destination address of all connection
    objects during the identity resolution phase, we also need to update the
    corresponding address field in the `struct bt_keys` structure for each
    of these connection objects.
    
    If we don't do this, `bt_keys_get_type` for that connection won't find
    the `bt_keys` structure in the pool and will allocate a new one. This
    will result in orphaned `bt_keys` structures in the pool.
    
    This completes the change done in https://github.com/zephyrproject-rtos/zephyr/pull/61030/commits/637dabc3c9b018f47dee656d2ca0fe8f2fa21984
    
    Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

--- subsys/bluetooth/host/smp.c
+++ subsys/bluetooth/host/smp.c
@@ -4130,6 +4130,7 @@ static uint8_t smp_id_add_replace(struct bt_smp *smp, struct bt_keys *new_bond)
 struct addr_match {
 	const bt_addr_le_t *rpa;
 	const bt_addr_le_t *id_addr;
+	struct bt_keys *keys;
 };
 
 static void convert_to_id_on_match(struct bt_conn *conn, void *data)
@@ -4138,6 +4139,10 @@ static void convert_to_id_on_match(struct bt_conn *conn, void *data)
 
 	if (bt_addr_le_eq(&conn->le.dst, addr_match->rpa)) {
 		bt_addr_le_copy(&conn->le.dst, addr_match->id_addr);
+
+		if (conn->le.keys && conn->le.keys != addr_match->keys) {
+			bt_addr_le_copy(&conn->le.keys->addr, addr_match->id_addr);
+		}
 	}
 }
 
@@ -4205,6 +4210,7 @@ static uint8_t smp_ident_addr_info(struct bt_smp *smp, struct net_buf *buf)
 				struct addr_match addr_match = {
 					.rpa = &conn->le.dst,
 					.id_addr = &req->addr,
+					.keys = keys,
 				};
 
 				bt_conn_foreach(BT_CONN_TYPE_LE,
