8efec4b7155d9d1c0cfe9a0828c3509102707552
Josuah Demangeon <josuah.demangeon@nordicsemi.no>
2025-08-08 21:00:09 +0000

    usb: host: wrap the mutex lock with a helper
    Hide the mutex lock details inside a wrapper call, like done for the
    device stack API. This is not used for the per-device lock of the host
    stack which use a different mutex lock.
    
    Signed-off-by: Josuah Demangeon <josuah.demangeon@nordicsemi.no>

--- subsys/usb/host/usbh_host.h
+++ subsys/usb/host/usbh_host.h
@@ -34,4 +34,24 @@ static inline bool usbd_is_initialized(const struct usbh_context *const uhs_ctx)
 	return uhs_ctx->status.initialized;
 }
 
+/**
+ * @brief Lock the USB host stack context
+ *
+ * @param[in] uhs_ctx Pointer to a host context
+ */
+static inline void usbh_host_lock(struct usbh_context *const uhs_ctx)
+{
+	k_mutex_lock(&uhs_ctx->mutex, K_FOREVER);
+}
+
+/**
+ * @brief Unlock the USB host stack context
+ *
+ * @param[in] uhs_ctx Pointer to a host context
+ */
+static inline void usbh_host_unlock(struct usbh_context *const uhs_ctx)
+{
+	k_mutex_unlock(&uhs_ctx->mutex);
+}
+
 #endif /* ZEPHYR_INCLUDE_USBH_HOST_H */
