7b26f3fd2feb51dc0e10d361b6e8df3d9d0320cb
Lauren Murphy <lauren.murphy@intel.com>
2025-08-11 13:45:38 -0700

    llext: reorder LLEXT_TYPE_ELF_OBJECT with linker script
    When using ET_REL / object files, apply a linker script to
    reorder sections. Enables compilation on MIPS and native_sim
    so developers can confirm the reordering works. Linking
    is still not enabled for these platforms, so tests do not pass.
    
    Signed-off-by: Lauren Murphy <lauren.murphy@intel.com>

--- cmake/modules/extensions.cmake
+++ cmake/modules/extensions.cmake
@@ -5961,34 +5961,57 @@ function(add_llext_target target_name)
   # to ensure that the ELF processed for binary generation contains SLIDs.
   # If executed too early, it is possible that some tools executed to modify
   # the ELF file (e.g., strip) undo the work performed here.
+  set(slid_inject_cmd)
+
   if (CONFIG_LLEXT_EXPORT_BUILTINS_BY_SLID)
     set(slid_inject_cmd
-      ${PYTHON_EXECUTABLE}
+      COMMAND ${PYTHON_EXECUTABLE}
       ${ZEPHYR_BASE}/scripts/build/llext_inject_slids.py
       --elf-file ${llext_pkg_output}
     )
-  else()
-    set(slid_inject_cmd ${CMAKE_COMMAND} -E true)
   endif()
 
   # When using the arcmwdt toolchain, the compiler may emit hundreds of
   # .arcextmap.* sections that bloat the shstrtab. stripac removes
   # these sections, but it does not remove their names from the shstrtab.
   # Use GNU strip to remove these sections beforehand.
+  set(gnu_strip_for_mwdt_cmd)
+
   if (${ZEPHYR_TOOLCHAIN_VARIANT} STREQUAL "arcmwdt")
     set(gnu_strip_for_mwdt_cmd
-      ${CMAKE_GNU_STRIP}
+      COMMAND ${CMAKE_GNU_STRIP}
       --remove-section=.arcextmap* --strip-unneeded
       ${llext_pkg_input}
     )
-  else()
-    set(gnu_strip_for_mwdt_cmd ${CMAKE_COMMAND} -E true)
   endif()
 
-  # Remove sections that are unused by the llext loader
+  get_property(TOPT GLOBAL PROPERTY TOPT)
+  get_property(COMPILER_TOPT TARGET compiler PROPERTY linker_script)
+  set_ifndef(  TOPT "${COMPILER_TOPT}")
+  set_ifndef(  TOPT -Wl,-T)
+
+  # The LLEXT loader cannot load ELF files where regions overlap, so
+  # reorder the sections in these cases.
+  set(backup_for_reorder_sects_cmd)
+  set(reorder_sects_cmd)
+
+  if (CONFIG_LLEXT_TYPE_ELF_OBJECT)
+    set(backup_for_reorder_sects_cmd
+      COMMAND ${CMAKE_COMMAND} -E copy ${llext_pkg_output} ${llext_pkg_output}.bak
+    )
+    set(reorder_sects_cmd
+      COMMAND ${CMAKE_C_COMPILER}
+      -r ${LLEXT_APPEND_FLAGS}
+      ${TOPT} ${ZEPHYR_BASE}/subsys/llext/llext_reorder_sections.ld
+      ${llext_pkg_output}.bak -o ${llext_pkg_output}
+    )
+  endif()
+
+  # Remove sections that are unused by the llext loader,
+  # reorder sections, and inject SLIDs
   add_custom_command(
     OUTPUT ${llext_pkg_output}
-    COMMAND ${gnu_strip_for_mwdt_cmd}
+    ${gnu_strip_for_mwdt_cmd}
     COMMAND $<TARGET_PROPERTY:bintools,elfconvert_command>
             $<TARGET_PROPERTY:bintools,elfconvert_flag>
             $<TARGET_PROPERTY:bintools,elfconvert_flag_strip_unneeded>
@@ -5997,7 +6020,9 @@ function(add_llext_target target_name)
             $<TARGET_PROPERTY:bintools,elfconvert_flag_infile>${llext_pkg_input}
             $<TARGET_PROPERTY:bintools,elfconvert_flag_outfile>${llext_pkg_output}
             $<TARGET_PROPERTY:bintools,elfconvert_flag_final>
-    COMMAND ${slid_inject_cmd}
+    ${backup_for_reorder_sects_cmd}
+    ${reorder_sects_cmd}
+    ${slid_inject_cmd}
     DEPENDS ${llext_pkg_input}
     COMMAND_EXPAND_LISTS
   )
