f2194857f0235f000e2e450942d77df49cb49fc8
Declan Snyder <declan.snyder@nxp.com>
2025-08-13 17:31:05 -0500

    soc: nxp: imxrt7xx: Split soc code files
    Instead of one soc.c with all the code, split into files for each area
    of concern.
    
    Signed-off-by: Declan Snyder <declan.snyder@nxp.com>

--- soc/nxp/imxrt/imxrt7xx/cm33/display_if.c
+++ soc/nxp/imxrt/imxrt7xx/cm33/display_if.c
@@ -4,41 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/**
- * @file
- * @brief System/hardware module for NXP RT7XX platform
- *
- * This module provides routines to initialize and support board-level
- * hardware for the RT7XX platforms.
- */
-
-#include <zephyr/init.h>
 #include <zephyr/devicetree.h>
-#include <zephyr/linker/sections.h>
-#include <zephyr/cache.h>
-#include <soc.h>
+#include <zephyr/autoconf.h>
 #include "fsl_power.h"
 #include "fsl_clock.h"
 
-void soc_early_init_hook(void)
-{
-	/* Enable data cache */
-	sys_cache_data_enable();
-
-	__ISB();
-	__DSB();
-}
-
-#ifdef CONFIG_SOC_RESET_HOOK
-
-void soc_reset_hook(void)
-{
-	SystemInit();
-}
-
-#endif /* CONFIG_SOC_RESET_HOOK */
-
-#ifdef CONFIG_MIPI_DSI
 /* Weak so board can override this function */
 void __weak imxrt_pre_init_display_interface(void)
 {
@@ -105,5 +75,3 @@ void __weak imxrt_deinit_display_interface(void)
 	/* Remove clock from DPHY */
 	CLOCK_AttachClk(kNONE_to_MIPI_DSI_HOST_PHY);
 }
-
-#endif
