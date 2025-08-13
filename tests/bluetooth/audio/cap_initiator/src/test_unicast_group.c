ba1ff92f172b94f3b711eee8b6a5375f0fe74c14
Emil Gydesen <emil.gydesen@nordicsemi.no>
2025-08-13 17:34:03 +0200

    tests: Bluetooth: CAP: Test return early for unicast_group_foreach_stream
    Add test that ensures that bt_cap_unicast_group_foreach_stream terminates
    correctly if the callback returns true.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bluetooth/audio/cap_initiator/src/test_unicast_group.c
+++ tests/bluetooth/audio/cap_initiator/src/test_unicast_group.c
@@ -357,6 +357,31 @@ static ZTEST_F(cap_initiator_test_unicast_group, test_initiator_unicast_group_fo
 	zassert_equal(cnt, expect_cnt, "Unexpected cnt (%zu != %zu)", cnt, expect_cnt);
 }
 
+static bool unicast_group_foreach_stream_return_early_cb(struct bt_cap_stream *stream,
+							 void *user_data)
+{
+	size_t *cnt = user_data;
+
+	(*cnt)++;
+
+	return true;
+}
+
+static ZTEST_F(cap_initiator_test_unicast_group,
+	       test_initiator_unicast_group_foreach_stream_return_early)
+{
+	size_t cnt = 0U;
+	int err;
+
+	err = bt_cap_unicast_group_create(fixture->group_param, &fixture->unicast_group);
+	zassert_equal(err, 0, "Unexpected return value %d", err);
+
+	err = bt_cap_unicast_group_foreach_stream(
+		fixture->unicast_group, unicast_group_foreach_stream_return_early_cb, &cnt);
+	zassert_equal(err, -ECANCELED, "Unexpected return value: %d", err);
+	zassert_equal(cnt, 1U, "Got %zu, expected %u", cnt, 1U);
+}
+
 static ZTEST_F(cap_initiator_test_unicast_group,
 	       test_initiator_unicast_group_foreach_stream_inval_null_group)
 {
