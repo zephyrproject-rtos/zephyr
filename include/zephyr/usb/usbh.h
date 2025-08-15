e4c5c97c1cbb4b1b68c0af27cb1e9dcf1cc3216a
Josuah Demangeon <josuah.demangeon@nordicsemi.no>
2025-08-15 19:04:59 +0000

    usb: host: introduce usbh_class with init/remove functions
    Add functions to probe/remove all classes as part of a new usbh_class.c
    and a matching usbh_class.h. These functions are called from the function
    usbh_init_device_intl() in usbh_core.c to initialize every class upon
    connection of a device. Every class driver provide filters to match the
    interfaces of the device.
    
    Co-authored-by: Aiden Hu <weiwei.hu@nxp.com>
    Signed-off-by: Josuah Demangeon <josuah.demangeon@nordicsemi.no>

--- include/zephyr/usb/usbh.h
+++ include/zephyr/usb/usbh.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2022 Nordic Semiconductor ASA
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ * Copyright 2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -17,6 +18,7 @@
 #include <stdint.h>
 #include <zephyr/device.h>
 #include <zephyr/net_buf.h>
+#include <zephyr/sys/util.h>
 #include <zephyr/sys/dlist.h>
 #include <zephyr/sys/bitarray.h>
 #include <zephyr/drivers/usb/uhc.h>
@@ -72,19 +74,25 @@ struct usbh_context {
 		.addr_ba = &ba_##device_name,				\
 	}
 
-/**
- * @brief USB Class Code triple
- */
-struct usbh_code_triple {
-	/** Device Class Code */
-	uint8_t dclass;
-	/** Class Subclass Code */
-	uint8_t sub;
-	/** Class Protocol Code */
-	uint8_t proto;
-};
-
 struct usbh_class_data;
+
+/**
+ * @brief Information about a device, which is relevant for matching a particular class.
+ */
+struct usbh_class_filter {
+	/** Vendor ID */
+	uint16_t vid;
+	/** Product ID */
+	uint16_t pid;
+	/** Class Code */
+	uint8_t class;
+	/** Subclass Code */
+	uint8_t sub;
+	/** Protocol Code */
+	uint8_t proto;
+	/** Flags that tell which field to match */
+	uint8_t flags;
+};
 
 /**
  * @brief USB host class instance API
@@ -127,10 +135,59 @@ struct usbh_class_data {
 };
 
 /**
+ * @cond INTERNAL_HIDDEN
+ *
+ * Internal state of an USB class. Not corresponding to an USB protocol state,
+ * but instead software life cycle.
  */
-#define USBH_DEFINE_CLASS(name) \
-	static STRUCT_SECTION_ITERABLE(usbh_class_data, name)
+enum usbh_class_state {
+	/** The class is available to be associated to an USB device function. */
+	USBH_CLASS_STATE_IDLE,
+	/** The class got bound to an USB function of a particular device on the bus. */
+	USBH_CLASS_STATE_BOUND,
+	/** The class failed to initialize and cannot be used. */
+	USBH_CLASS_STATE_ERROR,
+};
+/* @endcond */
 
+/**
+ * @cond INTERNAL_HIDDEN
+ *
+ * Variables used by the USB host stack but not exposed to the class
+ * through the class API.
+ */
+struct usbh_class_node {
+	/** Class information exposed to host class implementations (drivers). */
+	struct usbh_class_data *const c_data;
+	/** Filter rules to match this USB host class instance against a device class **/
+	struct usbh_class_filter *filters;
+	/** State of the USB class instance */
+	enum usbh_class_state state;
+};
+/* @endcond */
+
+/**
+ * @brief Define USB host support class data
+ *
+ * Macro defines class (function) data, as well as corresponding node
+ * structures used internally by the stack.
+ *
+ * @param[in] class_name Class name
+ * @param[in] class_api  Pointer to struct usbh_class_api
+ * @param[in] class_priv Class private data
+ * @param[in] filt Array of @ref usbh_class_filter to match this class or NULL to match everything.
+ *                 When non-NULL, the it has to be terminated by an entry with @c flags set to 0.
+ */
+#define USBH_DEFINE_CLASS(class_name, class_api, class_priv, filt)		\
+	static struct usbh_class_data UTIL_CAT(class_data_, class_name) = {	\
+		.name = STRINGIFY(class_name),					\
+		.api = class_api,						\
+		.priv = class_priv,						\
+	};									\
+	static STRUCT_SECTION_ITERABLE(usbh_class_node, class_name) = {		\
+		.c_data = &UTIL_CAT(class_data_, class_name),			\
+		.filters = filt,						\
+	};
 
 /**
  * @brief Initialize the USB host support;
