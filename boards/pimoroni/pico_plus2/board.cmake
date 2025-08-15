1e165cb3a966a94b1d8c0871fc1c4a0a07974a6d
Dmitrii Sharshakov <d3dx12.xx@gmail.com>
2025-08-15 13:56:34 +0200

    boards: arm: add probe-rs runner for RP2350 boards
    Add the chip parameter for probe-rs. Verified on Feather RP2350.
    
    Signed-off-by: Dmitrii Sharshakov <d3dx12.xx@gmail.com>

--- boards/pimoroni/pico_plus2/board.cmake
+++ boards/pimoroni/pico_plus2/board.cmake
@@ -3,6 +3,8 @@
 board_runner_args(openocd --cmd-pre-init "source [find interface/cmsis-dap.cfg]")
 board_runner_args(openocd --cmd-pre-init "source [find target/rp2350.cfg]")
 
+board_runner_args(probe-rs "--chip=RP235x")
+
 # The adapter speed is expected to be set by interface configuration.
 # The Raspberry Pi's OpenOCD fork doesn't, so match their documentation at
 # https://www.raspberrypi.com/documentation/microcontrollers/debug-probe.html#debugging-with-swd
@@ -12,3 +14,4 @@ board_runner_args(uf2 "--board-id=RP2350")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/probe-rs.board.cmake)
