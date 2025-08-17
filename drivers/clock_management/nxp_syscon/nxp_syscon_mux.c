2cf0c9efb172a94c5f2f77cbf909e2c45756bfee
Daniel DeGrasse <ddegrasse@tenstorrent.com>
2025-08-17 18:08:32 -0500

    drivers: clock_management: rework NXP LPC clock management drivers
    Rework NXP LPC clock management drivers to use the new API definitions.
    
    Signed-off-by: Daniel DeGrasse <ddegrasse@tenstorrent.com>

--- drivers/clock_management/nxp_syscon/nxp_syscon_mux.c
+++ drivers/clock_management/nxp_syscon/nxp_syscon_mux.c
@@ -1,10 +1,12 @@
 /*
  * Copyright 2024 NXP
+ * Copyright 2025 Tenstorrent AI ULC
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/drivers/clock_management/clock_driver.h>
+#include <zephyr/drivers/clock_management/clock_helpers.h>
 #include <stdlib.h>
 
 #include "nxp_syscon_internal.h"
@@ -12,15 +14,14 @@
 #define DT_DRV_COMPAT nxp_syscon_clock_mux
 
 struct syscon_clock_mux_config {
+	MUX_CLK_SUBSYS_DATA_DEFINE
 	uint8_t mask_width;
 	uint8_t mask_offset;
-	uint8_t src_count;
 	uint8_t safe_mux;
 	volatile uint32_t *reg;
-	const struct clk *parents[];
 };
 
-static int syscon_clock_mux_get_rate(const struct clk *clk_hw)
+static int syscon_clock_mux_get_parent(const struct clk *clk_hw)
 {
 	const struct syscon_clock_mux_config *config = clk_hw->hw_data;
 	uint32_t mux_mask = GENMASK((config->mask_width +
@@ -28,190 +29,103 @@ static int syscon_clock_mux_get_rate(const struct clk *clk_hw)
 				   config->mask_offset);
 	uint8_t sel = ((*config->reg) & mux_mask) >> config->mask_offset;
 
-	if (sel >= config->src_count) {
-		return -EIO;
+	if (sel >= config->parent_cnt) {
+		return -ENOTCONN;
 	}
 
-	return clock_get_rate(config->parents[sel]);
+	return sel;
 }
 
 static int syscon_clock_mux_configure(const struct clk *clk_hw, const void *mux)
 {
 	const struct syscon_clock_mux_config *config = clk_hw->hw_data;
-	int ret;
 
 	uint32_t mux_mask = GENMASK((config->mask_width +
 				   config->mask_offset - 1),
 				   config->mask_offset);
 	uint32_t mux_val = FIELD_PREP(mux_mask, ((uint32_t)mux));
-	uint32_t cur_rate = clock_get_rate(clk_hw);
-	uint32_t new_rate;
 
-	if (((uint32_t)mux) > config->src_count) {
+	if (((uint32_t)mux) > config->parent_cnt) {
 		return -EINVAL;
-	}
-
-	new_rate = clock_get_rate(config->parents[(uint32_t)mux]);
-
-	ret = clock_children_check_rate(clk_hw, new_rate);
-	if (ret < 0) {
-		return ret;
-	}
-
-	ret = clock_children_notify_pre_change(clk_hw, cur_rate, new_rate);
-	if (ret < 0) {
-		return ret;
 	}
 
 	(*config->reg) = ((*config->reg) & ~mux_mask) | mux_val;
 
-	ret = clock_children_notify_post_change(clk_hw, cur_rate, new_rate);
-	if (ret < 0) {
-		return ret;
-	}
 	return 0;
 }
 
 #if defined(CONFIG_CLOCK_MANAGEMENT_RUNTIME)
-static int syscon_clock_mux_notify(const struct clk *clk_hw, const struct clk *parent,
-				   const struct clock_management_event *event)
+static int syscon_clock_mux_configure_recalc(const struct clk *clk_hw,
+					     const void *mux)
+{
+	const struct syscon_clock_mux_config *config = clk_hw->hw_data;
+
+	if (((uint32_t)mux) > config->parent_cnt) {
+		return -EINVAL;
+	}
+
+	return (int)(uintptr_t)mux;
+}
+
+static int syscon_clock_mux_validate_parent(const struct clk *clk_hw,
+					    clock_freq_t parent_freq, uint8_t new_idx)
 {
 	const struct syscon_clock_mux_config *config = clk_hw->hw_data;
 	int ret;
-	uint8_t mux_mask = GENMASK((config->mask_width +
-				   config->mask_offset - 1),
-				   config->mask_offset);
-	uint8_t sel = ((*config->reg) & mux_mask) >> config->mask_offset;
-	struct clock_management_event notify_event;
 
-	notify_event.type = event->type;
-
-	if (sel >= config->src_count) {
-		notify_event.old_rate = 0;
-		notify_event.new_rate = 0;
-		/* Notify children mux rate is 0 */
-		clock_notify_children(clk_hw, &notify_event);
-		/* Selector has not been initialized */
-		return -ENOTCONN;
+	if (new_idx >= config->parent_cnt) {
+		return -EINVAL;
 	}
 
 	/*
-	 * Read mux reg, and if index matches parent index we should notify
-	 * children
+	 * Some syscon multiplexers are "safe", meaning they will not switch
+	 * sources unless both the current and new source are valid.
+	 * To prevent such a switch, don't permit moving to a new
+	 * source if the frequency is 0.
+	 *
+	 * Note that some parent drivers (such as PLLs) will gate during
+	 * reconfiguration, and receive an error from this function. To work
+	 * around this we use a specific return code, so the driver knows the
+	 * syscon mux is the driver preventing reconfiguration. The parent
+	 * driver can then ignore the error if it knowns it will restore
+	 * a valid frequency before returning.
+	 *
+	 * This allows parent drivers to distinguish between the case where the
+	 * mux is preventing gating, and the case where another consumer cannot
+	 * accept the parent gating.
 	 */
-	if (config->parents[sel] == parent) {
-		notify_event.old_rate = event->old_rate;
-		notify_event.new_rate = event->new_rate;
-		ret = clock_notify_children(clk_hw, &notify_event);
+	if (config->safe_mux && (parent_freq == 0)) {
+		/* Check with child clocks to make sure they will accept gating */
+		ret = clock_children_check_rate(clk_hw, 0);
 		if (ret < 0) {
+			/* Some child can't accept gating */
 			return ret;
 		}
-		if ((event->new_rate == 0) && config->safe_mux) {
-			/* These muxes are "fail-safe",
-			 * which means they refuse to switch clock outputs
-			 * if the one they are using is gated.
-			 */
-			ret = NXP_SYSCON_MUX_ERR_SAFEGATE;
-		}
-		return ret;
+		/* Only the mux can't gate- indicate this */
+		return NXP_SYSCON_MUX_ERR_SAFEGATE;
 	}
 
-	/* Parent is not in use */
-	return -ENOTCONN;
+	return 0;
 }
 #endif
 
 #if defined(CONFIG_CLOCK_MANAGEMENT_SET_RATE)
-static int syscon_clock_mux_round_rate(const struct clk *clk_hw,
-				       uint32_t rate_req)
+static int syscon_clock_mux_set_parent(const struct clk *clk_hw,
+				      uint8_t new_idx)
 {
-	const struct syscon_clock_mux_config *config = clk_hw->hw_data;
-	int cand_rate;
-	int best_delta = INT32_MAX;
-	int best_rate = 0;
-	int target_rate = (int)rate_req;
-	uint8_t idx = 0;
-
-	/*
-	 * Select a parent source based on the one able to
-	 * provide the rate closest to what was requested by the
-	 * caller
-	 */
-	while ((idx < config->src_count) && (best_delta > 0)) {
-		cand_rate = clock_round_rate(config->parents[idx], rate_req);
-		if ((abs(cand_rate - target_rate) < best_delta) &&
-		    (clock_children_check_rate(clk_hw, cand_rate) >= 0)) {
-			best_rate = cand_rate;
-			best_delta = abs(cand_rate - target_rate);
-		}
-		idx++;
-	}
-
-	return best_rate;
-}
-
-static int syscon_clock_mux_set_rate(const struct clk *clk_hw,
-				     uint32_t rate_req)
-{
-	const struct syscon_clock_mux_config *config = clk_hw->hw_data;
-	int cand_rate, best_rate, ret;
-	int best_delta = INT32_MAX;
-	uint32_t mux_val, cur_rate;
-	int target_rate = (int)rate_req;
-	uint32_t mux_mask = GENMASK((config->mask_width +
-				   config->mask_offset - 1),
-				   config->mask_offset);
-	uint8_t idx = 0;
-	uint8_t best_idx = 0;
-
-	/*
-	 * Select a parent source based on the one able to
-	 * provide the rate closest to what was requested by the
-	 * caller
-	 */
-	while ((idx < config->src_count) && (best_delta > 0)) {
-		cand_rate = clock_round_rate(config->parents[idx], rate_req);
-		if ((abs(cand_rate - target_rate) < best_delta) &&
-		    (clock_children_check_rate(clk_hw, cand_rate) >= 0)) {
-			best_idx = idx;
-			best_delta = abs(cand_rate - target_rate);
-		}
-		idx++;
-	}
-
-	/* Now set the clock rate for the best parent */
-	best_rate = clock_set_rate(config->parents[best_idx], rate_req);
-	if (best_rate < 0) {
-		return best_rate;
-	}
-	cur_rate = clock_get_rate(clk_hw);
-	ret = clock_children_notify_pre_change(clk_hw, cur_rate, best_rate);
-	if (ret < 0) {
-		return ret;
-	}
-	mux_val = FIELD_PREP(mux_mask, best_idx);
-	if ((*config->reg & mux_mask) != mux_val) {
-		(*config->reg) = ((*config->reg) & ~mux_mask) | mux_val;
-	}
-
-	ret = clock_children_notify_post_change(clk_hw, cur_rate, best_rate);
-	if (ret < 0) {
-		return ret;
-	}
-
-	return best_rate;
+	return syscon_clock_mux_configure(clk_hw, (const void *)(uintptr_t)new_idx);
 }
 #endif
 
-const struct clock_management_driver_api nxp_syscon_mux_api = {
-	.get_rate = syscon_clock_mux_get_rate,
-	.configure = syscon_clock_mux_configure,
+const struct clock_management_mux_api nxp_syscon_mux_api = {
+	.get_parent = syscon_clock_mux_get_parent,
+	.shared.configure = syscon_clock_mux_configure,
 #if defined(CONFIG_CLOCK_MANAGEMENT_RUNTIME)
-	.notify = syscon_clock_mux_notify,
+	.mux_configure_recalc = syscon_clock_mux_configure_recalc,
+	.mux_validate_parent = syscon_clock_mux_validate_parent,
 #endif
 #if defined(CONFIG_CLOCK_MANAGEMENT_SET_RATE)
-	.round_rate = syscon_clock_mux_round_rate,
-	.set_rate = syscon_clock_mux_set_rate,
+	.set_parent = syscon_clock_mux_set_parent,
 #endif
 };
 
@@ -219,20 +133,21 @@ const struct clock_management_driver_api nxp_syscon_mux_api = {
 	CLOCK_DT_GET(DT_PHANDLE_BY_IDX(node_id, prop, idx)),
 
 #define NXP_SYSCON_CLOCK_DEFINE(inst)                                          \
+	const struct clk *const nxp_syscon_mux_##inst##_parents[] = {          \
+		DT_INST_FOREACH_PROP_ELEM(inst, input_sources,                 \
+						GET_MUX_INPUT)                 \
+	};                                                                     \
 	const struct syscon_clock_mux_config nxp_syscon_mux_##inst = {         \
+		MUX_CLK_SUBSYS_DATA_INIT(nxp_syscon_mux_##inst##_parents,      \
+				 DT_INST_PROP_LEN(inst, input_sources))        \
 		.reg = (volatile uint32_t *)DT_INST_REG_ADDR(inst),            \
 		.mask_width = (uint8_t)DT_INST_REG_SIZE(inst),                 \
 		.mask_offset = (uint8_t)DT_INST_PROP(inst, offset),            \
-		.src_count = DT_INST_PROP_LEN(inst, input_sources),            \
 		.safe_mux = DT_INST_PROP(inst, safe_mux),                      \
-		.parents = {                                                   \
-			DT_INST_FOREACH_PROP_ELEM(inst, input_sources,         \
-						GET_MUX_INPUT)                 \
-		},                                                             \
 	};                                                                     \
 	                                                                       \
-	CLOCK_DT_INST_DEFINE(inst,                                             \
-			     &nxp_syscon_mux_##inst,                           \
-			     &nxp_syscon_mux_api);
+	MUX_CLOCK_DT_INST_DEFINE(inst,                                         \
+				 &nxp_syscon_mux_##inst,                       \
+				 &nxp_syscon_mux_api);
 
 DT_INST_FOREACH_STATUS_OKAY(NXP_SYSCON_CLOCK_DEFINE)
--- drivers/clock_management/nxp_syscon/nxp_syscon_source.c
+++ drivers/clock_management/nxp_syscon/nxp_syscon_source.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2024 NXP
+ * Copyright 2025 Tenstorrent AI ULC
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -16,7 +17,7 @@ struct syscon_clock_source_config {
 	volatile uint32_t *reg;
 };
 
-static int syscon_clock_source_get_rate(const struct clk *clk_hw)
+static clock_freq_t syscon_clock_source_get_rate(const struct clk *clk_hw)
 {
 	const struct syscon_clock_source_config *config = clk_hw->hw_data;
 
@@ -27,95 +28,45 @@ static int syscon_clock_source_get_rate(const struct clk *clk_hw)
 static int syscon_clock_source_configure(const struct clk *clk_hw, const void *data)
 {
 	const struct syscon_clock_source_config *config = clk_hw->hw_data;
-	int ret;
 	bool ungate = (bool)data;
-	int current_rate = clock_get_rate(clk_hw);
+	volatile uint32_t *power_reg;
+	uint32_t pdown_val;
 
 	if (ungate) {
-		/* Check if children will accept this rate */
-		ret = clock_children_check_rate(clk_hw, config->rate);
-		if (ret < 0) {
-			return ret;
-		}
-		ret = clock_children_notify_pre_change(clk_hw, current_rate,
-						       config->rate);
-		if (ret < 0) {
-			return ret;
-		}
-		(*config->reg) |= BIT(config->enable_offset);
-		PMC->PDRUNCFGCLR0 = config->pdown_mask;
-		return clock_children_notify_post_change(clk_hw, current_rate,
-							 config->rate);
+		power_reg = &PMC->PDRUNCFGCLR0;
+		pdown_val = (*config->reg) | BIT(config->enable_offset);
+	} else {
+		power_reg = &PMC->PDRUNCFGSET0;
+		pdown_val = (*config->reg) & ~BIT(config->enable_offset);
 	}
-	/* Check if children will accept this rate */
-	ret = clock_children_check_rate(clk_hw, 0);
-	if (ret < 0) {
-		return ret;
-	}
-	/* Pre rate change notification */
-	ret = clock_children_notify_pre_change(clk_hw, current_rate, 0);
-	if (ret < 0) {
-		return ret;
-	}
-	(*config->reg) &= ~BIT(config->enable_offset);
-	PMC->PDRUNCFGSET0 = config->pdown_mask;
-	return clock_children_notify_post_change(clk_hw, current_rate, 0);
+	(*config->reg) = pdown_val;
+	*power_reg = config->pdown_mask;
+	return 0;
 }
 
 #if defined(CONFIG_CLOCK_MANAGEMENT_RUNTIME)
-static int syscon_clock_source_notify(const struct clk *clk_hw, const struct clk *parent,
-			       const struct clock_management_event *event)
+static clock_freq_t syscon_clock_source_configure_recalc(const struct clk *clk_hw,
+					       const void *data)
 {
 	const struct syscon_clock_source_config *config = clk_hw->hw_data;
-	int ret;
-	int clock_rate = clock_get_rate(clk_hw);
-	const struct clock_management_event notify_event = {
-		/*
-		 * Use QUERY type, no need to forward this notification to
-		 * consumers
-		 */
-		.type = CLOCK_MANAGEMENT_QUERY_RATE_CHANGE,
-		.old_rate = clock_rate,
-		.new_rate = clock_rate,
-	};
+	bool ungate = (bool)data;
 
-	ARG_UNUSED(event);
-	ret = clock_notify_children(clk_hw, &notify_event);
-	if (ret == CLK_NO_CHILDREN) {
-		/* Gate this clock source */
-		(*config->reg) &= ~BIT(config->enable_offset);
-		PMC->PDRUNCFGSET0 = config->pdown_mask;
-	}
-
-	return 0;
+	return ungate ? config->rate : 0;
 }
 #endif
 
 
 #if defined(CONFIG_CLOCK_MANAGEMENT_SET_RATE)
-static int syscon_clock_source_round_rate(const struct clk *clk_hw,
-					  uint32_t rate_req)
+static clock_freq_t syscon_clock_source_round_rate(const struct clk *clk_hw,
+					  clock_freq_t rate_req)
 {
 	const struct syscon_clock_source_config *config = clk_hw->hw_data;
-	int ret;
 
-	if (rate_req != 0) {
-		ret = clock_children_check_rate(clk_hw, config->rate);
-		if (ret >= 0) {
-			return config->rate;
-		}
-	} else {
-		ret = clock_children_check_rate(clk_hw, 0);
-		if (ret >= 0) {
-			return 0;
-		}
-	}
-	/* Rate was not accepted */
-	return -ENOTSUP;
+	return (rate_req != 0) ? config->rate : 0;
 }
 
-static int syscon_clock_source_set_rate(const struct clk *clk_hw,
-					uint32_t rate_req)
+static clock_freq_t syscon_clock_source_set_rate(const struct clk *clk_hw,
+					clock_freq_t rate_req)
 {
 	const struct syscon_clock_source_config *config = clk_hw->hw_data;
 
@@ -125,20 +76,19 @@ static int syscon_clock_source_set_rate(const struct clk *clk_hw,
 	} else {
 		syscon_clock_source_configure(clk_hw, (void *)1);
 	}
-
 	return (rate_req != 0) ? config->rate : 0;
 }
 #endif
 
-const struct clock_management_driver_api nxp_syscon_source_api = {
+const struct clock_management_root_api nxp_syscon_source_api = {
 	.get_rate = syscon_clock_source_get_rate,
-	.configure = syscon_clock_source_configure,
+	.shared.configure = syscon_clock_source_configure,
 #if defined(CONFIG_CLOCK_MANAGEMENT_RUNTIME)
-	.notify = syscon_clock_source_notify,
+	.root_configure_recalc = syscon_clock_source_configure_recalc,
 #endif
 #if defined(CONFIG_CLOCK_MANAGEMENT_SET_RATE)
-	.round_rate = syscon_clock_source_round_rate,
-	.set_rate = syscon_clock_source_set_rate,
+	.root_round_rate = syscon_clock_source_round_rate,
+	.root_set_rate = syscon_clock_source_set_rate,
 #endif
 };
 
