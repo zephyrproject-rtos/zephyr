813a0bba054d0c12c3bf9ce1456fa7ac03391ab0
Zacck Osiemo <coderv63@gmail.com>
2025-08-17 08:13:24 +0200

    tests: spi: include correct overlay for sc18is606
    Apply correct device tree overlay is applied for SC18IS606
    
    Signed-off-by: Zacck Osiemo <coderv63@gmail.com>

--- drivers/spi/spi_sc18is606.h
+++ drivers/spi/spi_sc18is606.h
@@ -7,22 +7,20 @@
 #ifndef ZEPHYR_DRIVERS_I2C_SPI_SC18IS606_H_
 #define ZEPHYR_DRIVERS_I2C_SPI_SC18IS606_H_
 
-
-
 #include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* @brief Claim the the SC18IS606 bridge
+/* @brief Claim the SC18IS606 bridge
  *
  * @warning After calling this routine, the device cannot be used by any other thread
  * until the calling bridge releases it with the counterpart function of this.
  *
  * @param  dev SC18IS606  device
  * @retval 0 Device is claimed
- * @retval -EBUSY The device cannnot be claimed
+ * @retval -EBUSY The device cannot be claimed
  */
 int nxp_sc18is606_claim(const struct device *dev);
 
@@ -33,29 +31,27 @@ int nxp_sc18is606_claim(const struct device *dev);
  * @param dev SC18IS606 bridge
  *
  * @retval 0  Device is released
- * @retval -EPERM The current thread has not claimed this so cannot release it
  * @retval -EINVAL The device has no locks on it.
  */
-int nxp_sc18is606_release(const struct  device *dev);
+int nxp_sc18is606_release(const struct device *dev);
 
 /* @brief Transfer data using I2C to or from the bridge
  *
- * This routine implements the synchronization between the spi controller and gpio cntroller
+ * This routine implements the synchronization between the SPI controller and GPIO cntroller
  *
  * @param dev SC18IS606 bridge
  * @param tx_data Data to be sent out
  * @param tx_len Tx Data length
- * @param rx_data Container to recieve data
- * @param rx_let  size of expected receipt
- * @param function_id function id to be used, use NULL if none is needed ( for register writes)
+ * @param rx_data Container to receive data
+ * @param rx_len  size of expected receipt
+ * @param id_buf Function id data if used
  *
- * @retval 0 Tranfer success
- * @retvak -EAGAIN device did not complete and needs another turn
+ * @retval 0 Transfer success
+ * @retval -EAGAIN device lock timed out
+ * @retval -EBUSY device already locked
  */
-int nxp_sc18is606_transfer(const struct device *dev,
-		       const uint8_t *tx_data, uint8_t tx_len,
-		       uint8_t *rx_data, uint8_t rx_len);
-
+int nxp_sc18is606_transfer(const struct device *dev, const uint8_t *tx_data, uint8_t tx_len,
+			   uint8_t *rx_data, uint8_t rx_len, uint8_t *id_buf);
 
 #ifdef __cplusplus
 }
