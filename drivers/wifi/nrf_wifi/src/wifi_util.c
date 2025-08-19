8c5a5ae1990eab14c53bbdee45af7c7ca0d6c49e
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2025-08-20 01:45:18 +0530

    drivers: nrf_wifi: Fix NRF71 build
    NRF71 doesn't have direct memory access, so, disabled the rpu stats
    memory variant.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- drivers/wifi/nrf_wifi/src/wifi_util.c
+++ drivers/wifi/nrf_wifi/src/wifi_util.c
@@ -14,8 +14,10 @@
 #include "fmac_main.h"
 #include "wifi_util.h"
 
+#ifndef CONFIG_NRF71_ON_IPC
 #include "rpu_lmac_phy_stats.h"
 #include "rpu_umac_stats.h"
+#endif
 
 extern struct nrf_wifi_drv_priv_zep rpu_drv_priv_zep;
 struct nrf_wifi_ctx_zep *ctx = &rpu_drv_priv_zep.rpu_ctx_zep;
@@ -973,6 +975,7 @@ unlock:
 }
 #endif /* CONFIG_NRF_WIFI_RPU_RECOVERY */
 
+#ifndef CONFIG_NRF71_ON_IPC
 static int nrf_wifi_dump_stats(const struct shell *sh,
 				   struct nrf_wifi_hal_dev_ctx *hal_dev_ctx,
 				   const char *name,
@@ -1009,6 +1012,7 @@ static int nrf_wifi_dump_stats(const struct shell *sh,
 
 	return ret;
 }
+
 
 static int nrf_wifi_util_dump_rpu_stats_mem(const struct shell *sh,
 					size_t argc,
@@ -1095,6 +1099,7 @@ unlock:
 	k_mutex_unlock(&ctx->rpu_lock);
 	return ret;
 }
+#endif /* !CONFIG_NRF71_ON_IPC */
 
 SHELL_STATIC_SUBCMD_SET_CREATE(
 	nrf70_util,
@@ -1199,6 +1204,7 @@ SHELL_STATIC_SUBCMD_SET_CREATE(
 		      1,
 		      0),
 #endif /* CONFIG_NRF_WIFI_RPU_RECOVERY */
+#ifndef CONFIG_NRF71_ON_IPC
 	SHELL_CMD_ARG(rpu_stats_mem,
 		      NULL,
 		      "Display RPU stats by reading from memory "
@@ -1206,6 +1212,7 @@ SHELL_STATIC_SUBCMD_SET_CREATE(
 		      nrf_wifi_util_dump_rpu_stats_mem,
 		      1,
 		      1),
+#endif /* !CONFIG_NRF71_ON_IPC */
 	SHELL_SUBCMD_SET_END);
 
 
