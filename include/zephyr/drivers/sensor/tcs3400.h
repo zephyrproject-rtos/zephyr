dd4021e8cd28f5d820ee34855c06bca3a2d4d05f
Benjamin Cabé <benjamin@zephyrproject.org>
2025-08-19 12:31:18 +0200

    include: drivers: sensor: enhance Doxygen documentation for TCS3400
    Complete the Doxygen documentation for this sensor driver,
    put in sensor_interface_ext group.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- include/zephyr/drivers/sensor/tcs3400.h
+++ include/zephyr/drivers/sensor/tcs3400.h
@@ -4,14 +4,38 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/**
+ * @file
+ * @brief Header file for extended sensor API of TCS3400 sensor
+ * @ingroup tcs3400_interface
+ */
+
 #ifndef ZEPHYR_INCLUDE_DRIVERS_SENSOR_TCS3400_H_
 #define ZEPHYR_INCLUDE_DRIVERS_SENSOR_TCS3400_H_
 
+/**
+ * @brief AMS TCS3400 RGBC sensor
+ * @defgroup tcs3400_interface TCS3400
+ * @ingroup sensor_interface_ext
+ * @{
+ */
+
 #include <zephyr/drivers/sensor.h>
 
+/**
+ * @brief Custom sensor attributes for TCS3400
+ */
 enum sensor_attribute_tcs3400 {
-	/** RGBC Integration Cycles */
+	/**
+	 * Number of RGBC Integration Cycles
+	 *
+	 * - sensor_value.val1 should be between 1 and 256.
+	 */
 	SENSOR_ATTR_TCS3400_INTEGRATION_CYCLES = SENSOR_ATTR_PRIV_START,
 };
+
+/**
+ * @}
+ */
 
 #endif /* ZEPHYR_INCLUDE_DRIVERS_SENSOR_TCS3400_H_ */
