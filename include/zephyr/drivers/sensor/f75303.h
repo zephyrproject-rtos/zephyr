a02eac7440bdc158ffa98f67a7b14297ab329557
Benjamin Cabé <benjamin@zephyrproject.org>
2025-08-19 12:23:46 +0200

    include: drivers: sensor: enhance Doxygen documentation for F75303
    Complete the Doxygen documentation for this sensor driver,
    put in sensor_interface_ext group.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- include/zephyr/drivers/sensor/f75303.h
+++ include/zephyr/drivers/sensor/f75303.h
@@ -3,15 +3,36 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/**
+ * @file
+ * @brief Header file for extended sensor API of F75303 sensor
+ * @ingroup f75303_interface
+ */
+
 #ifndef ZEPHYR_INCLUDE_DRIVERS_SENSOR_F75303_H_
 #define ZEPHYR_INCLUDE_DRIVERS_SENSOR_F75303_H_
 
+/**
+ * @brief Fintek F75303 temperature sensor
+ * @defgroup f75303_interface F75303
+ * @ingroup sensor_interface_ext
+ * @{
+ */
+
 #include <zephyr/drivers/sensor.h>
 
-/* F75303 specific channels */
+/**
+ * @brief Custom sensor channels for F75303
+ */
 enum sensor_channel_f75303 {
+	/** Temperature of remote sensor 1 */
 	SENSOR_CHAN_F75303_REMOTE1 = SENSOR_CHAN_PRIV_START,
+	/** Temperature of remote sensor 2 */
 	SENSOR_CHAN_F75303_REMOTE2,
 };
+
+/**
+ * @}
+ */
 
 #endif
