8fcb347c96309c6f065d90610f3b93c1246aedc4
Benjamin Cabé <benjamin@zephyrproject.org>
2025-08-19 12:25:29 +0200

    include: drivers: sensor: enhance Doxygen documentation for PAJ7620
    Complete the Doxygen documentation for this sensor driver,
    put in sensor_interface_ext group.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- include/zephyr/drivers/sensor/paj7620.h
+++ include/zephyr/drivers/sensor/paj7620.h
@@ -6,14 +6,19 @@
 
 /**
  * @file
- * @brief Extended Public API for PAJ7620 sensor
- *
- * Some capabilities of the sensor cannot be expressed
- * within the sensor driver abstraction
+ * @brief Header file for extended sensor API of PAJ7620 sensor
+ * @ingroup paj7620_interface
  */
 
 #ifndef ZEPHYR_INCLUDE_DRIVERS_SENSOR_PAJ7620_H_
 #define ZEPHYR_INCLUDE_DRIVERS_SENSOR_PAJ7620_H_
+
+/**
+ * @brief Pixart PAJ7620 gesture sensor
+ * @defgroup paj7620_interface PAJ7620
+ * @ingroup sensor_interface_ext
+ * @{
+ */
 
 #ifdef __cplusplus
 extern "C"
@@ -22,18 +27,28 @@ extern "C"
 
 #include <zephyr/drivers/sensor.h>
 
-#define PAJ7620_FLAG_GES_UP               BIT(0)
-#define PAJ7620_FLAG_GES_DOWN             BIT(1)
-#define PAJ7620_FLAG_GES_LEFT             BIT(2)
-#define PAJ7620_FLAG_GES_RIGHT            BIT(3)
-#define PAJ7620_FLAG_GES_FORWARD          BIT(4)
-#define PAJ7620_FLAG_GES_BACKWARD         BIT(5)
-#define PAJ7620_FLAG_GES_CLOCKWISE        BIT(6)
-#define PAJ7620_FLAG_GES_COUNTERCLOCKWISE BIT(7)
-#define PAJ7620_FLAG_GES_WAVE             BIT(8)
+/**
+ * @name Gesture flags
+ * @{
+ */
+#define PAJ7620_FLAG_GES_UP               BIT(0) /**< "Up" gesture */
+#define PAJ7620_FLAG_GES_DOWN             BIT(1) /**< "Down" gesture */
+#define PAJ7620_FLAG_GES_LEFT             BIT(2) /**< "Left" gesture */
+#define PAJ7620_FLAG_GES_RIGHT            BIT(3) /**< "Right" gesture */
+#define PAJ7620_FLAG_GES_FORWARD          BIT(4) /**< "Forward" gesture */
+#define PAJ7620_FLAG_GES_BACKWARD         BIT(5) /**< "Backward" gesture */
+#define PAJ7620_FLAG_GES_CLOCKWISE        BIT(6) /**< "Clockwise" gesture */
+#define PAJ7620_FLAG_GES_COUNTERCLOCKWISE BIT(7) /**< "Counterclockwise" gesture */
+#define PAJ7620_FLAG_GES_WAVE             BIT(8) /**< "Wave" gesture */
+/** @} */
 
+/**
+ * @brief Custom sensor channels for PAJ7620
+ */
 enum sensor_channel_paj7620 {
 	/**
+	 * Gesture data (bit mask).
+	 *
 	 * This channel will contain gesture data as a bitmask where each
 	 * set bit represents a detected gesture. The possible gestures
 	 * that can be detected along with their corresponding bit are given
@@ -45,5 +60,9 @@ enum sensor_channel_paj7620 {
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
+
+/**
+ * @}
+ */
 
 #endif /* ZEPHYR_INCLUDE_DRIVERS_SENSOR_PAJ7620_H_ */
