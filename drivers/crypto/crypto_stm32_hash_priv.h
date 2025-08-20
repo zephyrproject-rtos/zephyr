8ed752484564002304f10bb346a0a8a9a8b5a7e8
Francois Ramu <francois.ramu@st.com>
2025-08-20 17:12:25 +0200

    drivers: crypto: hash driver supports the stm32h7rs series
    Adapt the stm32 crypto hash driver for the stm32h7rs HAL modules
    Create hal_func_hash_SHAxxx_start to abstract the algorithm
    The crypto algo is set by the HAL_HASH_SetConfig function
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- drivers/crypto/crypto_stm32_hash_priv.h
+++ drivers/crypto/crypto_stm32_hash_priv.h
@@ -7,10 +7,19 @@
 #ifndef ZEPHYR_DRIVERS_CRYPTO_CRYPTO_STM32_HASH_PRIV_H_
 #define ZEPHYR_DRIVERS_CRYPTO_CRYPTO_STM32_HASH_PRIV_H_
 
-#define hash_config_t HASH_InitTypeDef
-
 /* Max digest length: SHA256 = 32 bytes */
 #define STM32_HASH_MAX_DIGEST_SIZE (32)
+
+#if defined(CONFIG_SOC_SERIES_STM32H7RSX)
+
+#define hash_config_t	HASH_ConfigTypeDef
+#define HASH_DATATYPE_8B	HASH_BYTE_SWAP
+
+#else /* CONFIG_SOC_SERIES_STM32H7RSX */
+
+#define hash_config_t	HASH_InitTypeDef
+
+#endif /* CONFIG_SOC_SERIES_STM32H7RSX */
 
 struct crypto_stm32_hash_config {
 	const struct reset_dt_spec reset;
@@ -36,5 +45,41 @@ struct crypto_stm32_hash_session {
 
 #define CRYPTO_STM32_HASH_SESSN(ctx)                                                               \
 	((struct crypto_stm32_hash_session *const)(ctx)->drv_sessn_state)
+
+
+static inline HAL_StatusTypeDef hal_func_hash_SHA224_start(HASH_HandleTypeDef *hhash,
+					void *p_in_buffer,
+					uint32_t in_size_byte,
+					void *p_out_buffer)
+{
+#if defined(CONFIG_SOC_SERIES_STM32H7RSX)
+	return HAL_HASH_Start(hhash,
+			(const uint8_t *const)p_in_buffer,
+#else
+	return HAL_HASHEx_SHA224_Start(hhash,
+			p_in_buffer,
+#endif /* CONFIG_SOC_SERIES_STM32H7RSX */
+			in_size_byte,
+			p_out_buffer,
+			HAL_MAX_DELAY);
+}
+
+static inline HAL_StatusTypeDef hal_func_hash_SHA256_start(HASH_HandleTypeDef *hhash,
+					void *p_in_buffer,
+					uint32_t in_size_byte,
+					void *p_out_buffer)
+{
+#if defined(CONFIG_SOC_SERIES_STM32H7RSX)
+	return HAL_HASH_Start(hhash,
+			(const uint8_t *const)p_in_buffer,
+#else
+	return HAL_HASHEx_SHA256_Start(hhash,
+			p_in_buffer,
+#endif /* CONFIG_SOC_SERIES_STM32H7RSX */
+			in_size_byte,
+			p_out_buffer,
+			HAL_MAX_DELAY);
+}
+
 
 #endif /* ZEPHYR_DRIVERS_CRYPTO_CRYPTO_STM32_HASH_PRIV_H_ */
