0f7b1033d5c8709413c7734b5fcaed1e6fc63ad4
Benjamin Cabé <benjamin@zephyrproject.org>
2025-08-21 22:25:45 +0200

    include: sensing: doc: sensor types doxygen fixes
    add missing docs, group types using `@name`
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- include/zephyr/sensing/sensing_sensor_types.h
+++ include/zephyr/sensing/sensing_sensor_types.h
@@ -20,35 +20,50 @@
  */
 
 /**
- * sensor category light
+ * @name Light sensors
+ * @{
  */
+
+/** Sensor type for ambient light sensors. */
 #define SENSING_SENSOR_TYPE_LIGHT_AMBIENTLIGHT			0x41
 
+/** @} */
+
 /**
- * sensor category motion
+ * @name Motion sensors
+ * @{
  */
-/* Sensor type for 3D accelerometers. */
+
+/** Sensor type for 3D accelerometers. */
 #define SENSING_SENSOR_TYPE_MOTION_ACCELEROMETER_3D		0x73
-/* Sensor type for 3D gyrometers. */
+/** Sensor type for 3D gyrometers. */
 #define SENSING_SENSOR_TYPE_MOTION_GYROMETER_3D			0x76
-/* Sensor type for motion detectors. */
+/** Sensor type for motion detectors. */
 #define SENSING_SENSOR_TYPE_MOTION_MOTION_DETECTOR		0x77
+/** Sensor type for uncalibrated 3D accelerometers. */
+#define SENSING_SENSOR_TYPE_MOTION_UNCALIB_ACCELEROMETER_3D     0x240
+/** Sensor type for hinge angle sensors. */
+#define SENSING_SENSOR_TYPE_MOTION_HINGE_ANGLE                  0x20B
 
+/** @} */
 
 /**
- * sensor category other
+ * @name Other sensors
+ * @{
  */
-#define SENSING_SENSOR_TYPE_OTHER_CUSTOM			0xE1
 
-/* Sensor type for uncalibrated 3D accelerometers. */
-#define SENSING_SENSOR_TYPE_MOTION_UNCALIB_ACCELEROMETER_3D	0x240
-/* Sensor type for hinge angle sensors. */
-#define SENSING_SENSOR_TYPE_MOTION_HINGE_ANGLE			0x20B
+/** Sensor type for custom sensors. */
+#define SENSING_SENSOR_TYPE_OTHER_CUSTOM 0xE1
+
+/** @} */
 
 /**
  * @brief Sensor type for all sensors.
  *
  * This macro defines the sensor type for all sensors.
+ *
+ * @note This value is not a valid sensor type and is used as a sentinel value to indicate all
+ *       sensor types.
  */
 #define SENSING_SENSOR_TYPE_ALL					0xFFFF
 
