b7dbfce156fa8c38fb5a60276d99cc6076986ce8
BUDKE Gerson Fernando <gerson.budke@leica-geosystems.com>
2025-08-21 18:45:22 +0200

    boards: st: nucleo_u5a5zj_q: Import TF-M code
    Signed-off-by: BUDKE Gerson Fernando <gerson.budke@leica-geosystems.com>

---
+++ boards/st/nucleo_u5a5zj_q/tfm/tests/psa_arch_tests_config.cmake
@@ -0,0 +1,7 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2023 STMicroelectronics. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+#-------------------------------------------------------------------------------
+set(PSA_API_TEST_TARGET nucleo_u5a5zj_q)
