4200e177aea54c7b0feeaafe5746bdbb13018f53
Ruijia Wang <ruijia.wang@nxp.com>
2025-08-24 21:08:41 +0800

    drivers: xspi: add NXP xspi driver
    Add mcux xspi driver suppport and update the sample code to demonstrate
    related functions.
    
    Signed-off-by: Ruijia Wang <ruijia.wang@nxp.com>

--- samples/drivers/memc/src/main.c
+++ samples/drivers/memc/src/main.c
@@ -32,6 +32,11 @@
 #define MEMC_DEV  DT_ALIAS(sram_ext)
 #define MEMC_BASE DT_REG_ADDR(MEMC_DEV)
 #define MEMC_SIZE DT_REG_SIZE(MEMC_DEV)
+#elif DT_HAS_COMPAT_STATUS_OKAY(nxp_xspi_psram)
+#define MEMC_DEV DT_ALIAS(psram0)
+#define MSPI_BUS DT_BUS(MEMC_DEV)
+#define MEMC_BASE DT_PROP(MSPI_BUS, amba_address)
+#define MEMC_SIZE (DT_PROP(MEMC_DEV, size) / 8)
 #else
 #error At least one driver should be selected!
 #endif
