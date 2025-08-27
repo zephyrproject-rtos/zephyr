3b43a2d116343081cac1771027c2b60227df436f
Ayush Singh <ayush@beagleboard.org>
2025-08-27 17:38:12 +0530

    boards: beagle: pocketbeagle_2: Add rev A1 support
    - PocketBeagle 2 rev A1 replaces the AM6232 with AM6254.
    - Only rev A1 is available for sale now.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- boards/beagle/pocketbeagle_2/board.cmake
+++ boards/beagle/pocketbeagle_2/board.cmake
@@ -8,3 +8,8 @@ if(CONFIG_SOC_AM6232_M4)
   board_runner_args(openocd "--no-init" "--no-halt" "--no-targets" "--gdb-client-port=3339")
   include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 endif()
+
+if(CONFIG_SOC_AM6254_M4)
+  board_runner_args(openocd "--no-init" "--no-halt" "--no-targets" "--gdb-client-port=3339")
+  include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+endif()
