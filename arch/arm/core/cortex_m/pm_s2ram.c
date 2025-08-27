00d65a2f7aae6b376fd3cd255890bc74dfd6b79f
Mathieu CHOPLAIN <mathieu.choplain@st.com>
2025-08-27 11:47:49 +0200

    arch: arm: cortex_m: pm_s2ram: save/restore additional CPU state
    Save/restore additional CPU state when entering/existing S2RAM states:
    the SCB, MPU and FPU (if enabled) registers are now saved and restored
    appropriately at architecture level.
    
    Implement the feature in C to reduce the amount of assembly required.
    
    Signed-off-by: Mathieu CHOPLAIN <mathieu.choplain@st.com>

--- arch/arm/core/cortex_m/pm_s2ram.c
+++ arch/arm/core/cortex_m/pm_s2ram.c
@@ -7,18 +7,15 @@
 
 #include <zephyr/arch/cpu.h>
 #include <zephyr/linker/sections.h>
+#include <zephyr/sys/util_macro.h>
 #include <zephyr/toolchain.h>
 
 #include <zephyr/arch/common/pm_s2ram.h>
+#include <zephyr/arch/arm/cortex_m/scb.h>
+#include <zephyr/arch/arm/cortex_m/fpu.h>
 
 BUILD_ASSERT(CONFIG_MP_MAX_NUM_CPUS == 1,
 	"Suspend-to-RAM not yet supported on multi-core SoCs");
-
-#define MAGIC (0xDABBAD00)
-
-/**
- * CPU context for S2RAM
- */
 
 #if DT_NODE_EXISTS(DT_NODELABEL(pm_s2ram)) &&\
 	DT_NODE_HAS_COMPAT(DT_NODELABEL(pm_s2ram), zephyr_memory_region)
@@ -31,13 +28,39 @@ BUILD_ASSERT(CONFIG_MP_MAX_NUM_CPUS == 1,
 #define __s2ram_ctx __noinit
 #endif
 
+/* CPU state preserved across S2RAM */
 __s2ram_ctx _cpu_context_t _cpu_context;
+__s2ram_ctx struct scb_context _scb_context;
+__s2ram_ctx struct z_mpu_context_retained _mpu_context;
+IF_ENABLED(CONFIG_FPU, (__s2ram_ctx struct fpu_ctx_full _fpu_context;))
+/* TODO: assert section (if selected) is large enough */
+
+/**
+ * These functions are helpers invoked from `pm_s2ram.S`
+ * to save/restore CPU state other than general-purpose
+ * and special registers (which are handled in assembly)
+ */
+void z_arm_pm_s2ram_save_additional_state(void)
+{
+	z_arm_save_scb_context(&_scb_context);
+	z_arm_save_mpu_context(&_mpu_context);
+	IF_ENABLED(CONFIG_FPU, (z_arm_save_fp_context(&_fpu_context);))
+}
+
+void z_arm_pm_s2ram_restore_additional_state(void)
+{
+	z_arm_restore_scb_context(&_scb_context);
+	z_arm_restore_mpu_context(&_mpu_context);
+	IF_ENABLED(CONFIG_FPU, z_arm_restore_fp_context(&_fpu_context));
+}
 
 #ifndef CONFIG_PM_S2RAM_CUSTOM_MARKING
 /**
  * S2RAM Marker
  */
 static __noinit uint32_t marker;
+
+#define MAGIC (0xDABBAD00)
 
 void pm_s2ram_mark_set(void)
 {
