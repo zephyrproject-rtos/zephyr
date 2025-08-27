7f045f00e2206cc867785540dbd32ecc4855dd1a
Wilkins White <ww@novadynamics.com>
2025-08-27 16:11:20 -0700

    drivers: sensor: ti: ina2xx: Add common config
    This commit adds a common config struct, init function,
    and get/set attr functions to reduce repeated code in the
    INA2XX family.
    
    Signed-off-by: Wilkins White <ww@novadynamics.com>

--- drivers/sensor/ti/ina2xx/ina2xx_common.c
+++ drivers/sensor/ti/ina2xx/ina2xx_common.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2021 Grinn
+ * Copyright 2025 Nova Dynamics LLC
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -7,6 +8,14 @@
 #include "ina2xx_common.h"
 
 #include <zephyr/sys/byteorder.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(INA2XX, CONFIG_SENSOR_LOG_LEVEL);
+
+/**
+ * @brief Value of the manufacturer ID register (ASCII 'TI')
+ */
+#define INA2XX_MANUFACTURER_ID 0x5449
 
 int ina2xx_reg_read_24(const struct i2c_dt_spec *bus, uint8_t reg, uint32_t *val)
 {
@@ -46,4 +55,103 @@ int ina2xx_reg_write(const struct i2c_dt_spec *bus, uint8_t reg, uint16_t val)
 	sys_put_be16(val, &tx_buf[1]);
 
 	return i2c_write_dt(bus, tx_buf, sizeof(tx_buf));
+}
+
+int ina2xx_attr_set(const struct device *dev, enum sensor_channel chan,
+				enum sensor_attribute attr, const struct sensor_value *val)
+{
+	const struct ina2xx_config *config = dev->config;
+	uint16_t data = val->val1;
+
+	if (attr == SENSOR_ATTR_CONFIGURATION && config->config_reg >= 0) {
+		return ina2xx_reg_write(&config->bus, config->config_reg, data);
+	}
+
+	if (attr == SENSOR_ATTR_CALIBRATION && config->cal_reg >= 0) {
+		return ina2xx_reg_write(&config->bus, config->cal_reg, data);
+	}
+
+	return -ENOTSUP;
+}
+
+int ina2xx_attr_get(const struct device *dev, enum sensor_channel chan,
+				enum sensor_attribute attr, struct sensor_value *val)
+{
+	const struct ina2xx_config *config = dev->config;
+	uint16_t data;
+	int ret;
+
+	if (attr == SENSOR_ATTR_CONFIGURATION && config->config_reg >= 0) {
+		ret = ina2xx_reg_read_16(&config->bus, config->config_reg, &data);
+		if (ret < 0) {
+			return ret;
+		}
+	} else {
+		return -ENOTSUP;
+	}
+
+	if (attr == SENSOR_ATTR_CALIBRATION && config->cal_reg >= 0) {
+		ret = ina2xx_reg_read_16(&config->bus, config->cal_reg, &data);
+		if (ret < 0) {
+			return ret;
+		}
+	} else {
+		return -ENOTSUP;
+	}
+
+	val->val1 = data;
+	val->val2 = 0;
+
+	return 0;
+}
+
+int ina2xx_init(const struct device *dev)
+{
+	const struct ina2xx_config *config = dev->config;
+	uint16_t id;
+	int ret;
+
+	if (!device_is_ready(config->bus.bus)) {
+		LOG_ERR("I2C bus %s is not ready", config->bus.bus->name);
+		return -ENODEV;
+	}
+
+	if (config->id_reg >= 0) {
+		ret = ina2xx_reg_read_16(&config->bus, config->id_reg, &id);
+		if (ret < 0) {
+			LOG_ERR("failed to read manufacturer register");
+			return ret;
+		}
+
+		if (id != INA2XX_MANUFACTURER_ID) {
+			LOG_ERR("manufacturer ID doesn't match");
+			return -ENODEV;
+		}
+	}
+
+	if (config->config_reg >= 0) {
+		ret = ina2xx_reg_write(&config->bus, config->config_reg, config->config);
+		if (ret < 0) {
+			LOG_ERR("failed to write configuration register");
+			return ret;
+		}
+	}
+
+	if (config->adc_config_reg >= 0) {
+		ret = ina2xx_reg_write(&config->bus, config->adc_config_reg, config->adc_config);
+		if (ret < 0) {
+			LOG_ERR("failed to write ADC configuration register");
+			return ret;
+		}
+	}
+
+	if (config->cal_reg >= 0) {
+		ret = ina2xx_reg_write(&config->bus, config->cal_reg, config->cal);
+		if (ret < 0) {
+			LOG_ERR("failed to write calibration register");
+			return ret;
+		}
+	}
+
+	return 0;
 }
--- drivers/sensor/ti/ina2xx/ina2xx_common.h
+++ drivers/sensor/ti/ina2xx/ina2xx_common.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2021  Grinn
+ * Copyright 2021 Grinn
+ * Copyright 2025 Nova Dynamics LLC
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -13,12 +14,39 @@
 #include <zephyr/sys/util_macro.h>
 
 /**
- * @brief Macro used to check if the current's LSB is 1mA
+ * @brief Common configuration for INA2xx devices.
+ *
+ * @param bus I2C bus configuration.
+ * @param alert_gpio Alert GPIO pin.
+ * @param current_lsb LSB for calculating current in microamps/bit.
+ * @param adc_config Initial ADC configuration register value.
+ * @param cal Initial calibration register value.
+ * @param id_reg Manufacturer ID register address (-1 if not supported).
+ * @param adc_config_reg ADC configuration register address (-1 if not supported).
+ * @param cal_reg Calibration register address (-1 if not supported).
  */
-#define INA2XX_CURRENT_LSB_1MA         1
+struct ina2xx_config {
+	struct i2c_dt_spec bus;
+	uint32_t current_lsb;
+	uint16_t config;
+	uint16_t adc_config;
+	uint16_t cal;
+	int id_reg;
+	int config_reg;
+	int adc_config_reg;
+	int cal_reg;
+};
 
 int ina2xx_reg_read_24(const struct i2c_dt_spec *bus, uint8_t reg, uint32_t *val);
 int ina2xx_reg_read_16(const struct i2c_dt_spec *bus, uint8_t reg, uint16_t *val);
 int ina2xx_reg_write(const struct i2c_dt_spec *bus, uint8_t reg, uint16_t val);
+
+int ina2xx_attr_set(const struct device *dev, enum sensor_channel chan,
+				enum sensor_attribute attr, const struct sensor_value *val);
+
+int ina2xx_attr_get(const struct device *dev, enum sensor_channel chan,
+				enum sensor_attribute attr, struct sensor_value *val);
+
+int ina2xx_init(const struct device *dev);
 
 #endif /* ZEPHYR_DRIVERS_SENSOR_INA2XX_COMMON_H_ */
