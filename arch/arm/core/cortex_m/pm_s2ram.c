c4cb3b09fb059c1bf1261ea68602d65099106c8d
Mathieu Choplain <mathieu.choplain-ext@st.com>
2025-08-27 11:47:49 +0200

    arch: arm: cortex_m: pm_s2ram: save/restore SCB/MPU/FPU state
    When entering/exiting S2RAM states, save/restore SCB, MPU and FPU registers
    at architecture level. Also disable and power down the FPU for the duration
    of S2RAM sleep. (The exact operations performs depend on which options are
    enabled by Kconfig and the target CPU model)
    
    Implement this feature in C to reduce the amount of assembly required.
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain-ext@st.com>

--- arch/arm/core/cortex_m/pm_s2ram.c
+++ arch/arm/core/cortex_m/pm_s2ram.c
@@ -5,17 +5,28 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <cmsis_core.h>
+
 #include <zephyr/arch/cpu.h>
 #include <zephyr/linker/sections.h>
+#include <zephyr/sys/util.h>
 #include <zephyr/toolchain.h>
 
 #include <zephyr/arch/common/pm_s2ram.h>
+#include <zephyr/arch/arm/cortex_m/scb.h>
+#include <zephyr/arch/arm/cortex_m/fpu.h>
 
-#define MAGIC (0xDABBAD00)
-
-/**
- * CPU context for S2RAM
+#if defined(CONFIG_ARMV8_M_MAINLINE) && !defined(ICB_CPPWR_SU11_Pos)
+/*
+ * The CMSIS header for some cores (e.g., Cortex-M33) lacks the definitions
+ * for CPPWR even though the registers exists as long as the Main Extension
+ * is implemented. Provide definitions ourselves if not found.
  */
+#define ICB_CPPWR_SU11_Pos	22U
+#define ICB_CPPWR_SU11_Msk	(1UL << ICB_CPPWR_SU11_Pos)
+#define ICB_CPPWR_SU10_Pos	20U
+#define ICB_CPPWR_SU10_Msk	(1UL << ICB_CPPWR_SU10_Pos)
+#endif /* defined(CONFIG_ARMV8_M_MAINLINE) && !defined(ICB_CPPWR_SU11_Pos) */
 
 #if DT_NODE_EXISTS(DT_NODELABEL(pm_s2ram)) &&\
 	DT_NODE_HAS_COMPAT(DT_NODELABEL(pm_s2ram), zephyr_memory_region)
@@ -28,13 +39,69 @@
 #define __s2ram_ctx __noinit
 #endif
 
+/* CPU state preserved across S2RAM */
 __s2ram_ctx _cpu_context_t _cpu_context;
+__s2ram_ctx struct scb_context _scb_context;
+IF_ENABLED(CONFIG_MPU, (__s2ram_ctx struct z_mpu_context_retained _mpu_context;))
+IF_ENABLED(CONFIG_FPU, (__s2ram_ctx struct fpu_ctx_full _fpu_context;))
+
+/**
+ * These functions are helpers invoked from `pm_s2ram.S`
+ * to save/restore CPU state other than general-purpose
+ * and special registers (which are handled in assembly)
+ */
+void z_arm_pm_s2ram_save_scb_mpu(void)
+{
+	z_arm_save_scb_context(&_scb_context);
+	IF_ENABLED(CONFIG_MPU, (z_arm_save_mpu_context(&_mpu_context);))
+}
+
+void z_arm_pm_s2ram_restore_scb_mpu(void)
+{
+	z_arm_restore_scb_context(&_scb_context);
+	IF_ENABLED(CONFIG_MPU, (z_arm_restore_mpu_context(&_mpu_context);))
+}
+
+void z_arm_pm_s2ram_save_and_disable_fpu(void)
+{
+#if defined(CONFIG_FPU)
+	/* Save context BEFORE disabling the FPU */
+	z_arm_save_fp_context(&_fpu_context);
+
+	SCB->CPACR &= ~(CPACR_CP10_Msk | CPACR_CP11_Msk);
+#if defined(CONFIG_ARMV8_M_MAINLINE)
+	SCnSCB->CPPWR |= (SCnSCB_CPPWR_SU10_Msk | SCnSCB_CPPWR_SU11_Msk);
+#endif /* CONFIG_ARMV8_M_MAINLINE */
+
+	__DSB();
+	__ISB();
+#endif /* CONFIG_FPU */
+}
+
+void z_arm_pm_s2ram_enable_and_restore_fpu(void)
+{
+#if defined(CONFIG_FPU)
+	uint32_t old_control;
+
+#if defined(CONFIG_ARMV8_M_MAINLINE)
+	SCnSCB->CPPWR &= ~(SCnSCB_CPPWR_SU10_Msk | SCnSCB_CPPWR_SU11_Msk);
+#endif /* CONFIG_ARMV8_M_MAINLINE */
+	SCB->CPACR |= ~(CPACR_CP10_Msk | CPACR_CP11_Msk);
+
+	__DSB();
+	__ISB();
+
+	z_arm_restore_fp_context(&_fpu_context);
+#endif /* CONFIG_FPU */
+}
 
 #ifndef CONFIG_HAS_PM_S2RAM_CUSTOM_MARKING
 /**
  * S2RAM Marker
  */
 static __noinit uint32_t marker;
+
+#define MAGIC (0xDABBAD00)
 
 void pm_s2ram_mark_set(void)
 {
