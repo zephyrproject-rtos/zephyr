30696664179984fd95f2ca62634ee87f77525b1a
Jakub Zymelka <jakub.zymelka@nordicsemi.no>
2025-08-28 12:02:22 +0200

    dts: bindings: comparator: nordic: Change inputs type to int
    Unify external analog inputs type to be consistent
    in COMP, LPCOMP and SAADC nordic drivers.
    
    Signed-off-by: Jakub Zymelka <jakub.zymelka@nordicsemi.no>

---
+++ include/zephyr/dt-bindings/comparator/nrf-comp.h
@@ -0,0 +1,21 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_ADC_NRF_COMP_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_ADC_NRF_COMP_H_
+
+#define NRF_COMP_AIN0 0
+#define NRF_COMP_AIN1 1
+#define NRF_COMP_AIN2 2
+#define NRF_COMP_AIN3 3
+#define NRF_COMP_AIN4 4
+#define NRF_COMP_AIN5 5
+#define NRF_COMP_AIN6 6
+#define NRF_COMP_AIN7 7
+#define NRF_COMP_AIN_VDD_DIV2 8
+#define NRF_COMP_AIN_VDDH_DIV5 9
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_ADC_NRF_COMP_H_ */
