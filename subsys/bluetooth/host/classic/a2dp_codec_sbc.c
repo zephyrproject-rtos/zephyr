b7f33bfaea98fa9b7d193f86df6702041a2c89d3
Mark Wang <yichang.wang@nxp.com>
2025-08-28 10:52:06 +0800

    bluetooth: a2dp: fix spelling error of STEREO
    change A2DP_SBC_CH_MODE_STREO as A2DP_SBC_CH_MODE_STEREO
    
    Signed-off-by: Mark Wang <yichang.wang@nxp.com>

--- subsys/bluetooth/host/classic/a2dp_codec_sbc.c
+++ subsys/bluetooth/host/classic/a2dp_codec_sbc.c
@@ -24,7 +24,7 @@ uint8_t bt_a2dp_sbc_get_channel_num(struct bt_a2dp_codec_sbc_params *sbc_codec)
 		return 1U;
 	} else if (sbc_codec->config[0] & A2DP_SBC_CH_MODE_DUAL) {
 		return 2U;
-	} else if (sbc_codec->config[0] & A2DP_SBC_CH_MODE_STREO) {
+	} else if (sbc_codec->config[0] & A2DP_SBC_CH_MODE_STEREO) {
 		return 2U;
 	} else if (sbc_codec->config[0] & A2DP_SBC_CH_MODE_JOINT) {
 		return 2U;
@@ -41,7 +41,7 @@ enum sbc_ch_mode bt_a2dp_sbc_get_channel_mode(struct bt_a2dp_codec_sbc_params *s
 		return SBC_CH_MODE_MONO;
 	} else if (sbc_codec->config[0] & A2DP_SBC_CH_MODE_DUAL) {
 		return SBC_CH_MODE_DUAL_CHANNEL;
-	} else if (sbc_codec->config[0] & A2DP_SBC_CH_MODE_STREO) {
+	} else if (sbc_codec->config[0] & A2DP_SBC_CH_MODE_STEREO) {
 		return SBC_CH_MODE_STEREO;
 	} else if (sbc_codec->config[0] & A2DP_SBC_CH_MODE_JOINT) {
 		return SBC_CH_MODE_JOINT_STEREO;
