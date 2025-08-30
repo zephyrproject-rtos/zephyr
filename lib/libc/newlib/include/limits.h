3975c819afb236ed3f0601a080672ea74515b90b
Chris Friedt <cfriedt@tenstorrent.com>
2025-08-30 07:46:18 -0400

    libc: newlib: limits.h: include posix limits from zephyr
    Newlib does not seem to define a number of mandatory POSIX limits
    (e.g. minimum values).
    
    Include Zephyr's POSIX definitions in an override of Newlib's limits.h
    (when the appropriate application conformance macro is provided).
    
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

---
+++ lib/libc/newlib/include/limits.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright The Zephyr Project Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_LIB_LIBC_NEWLIB_INCLUDE_LIMITS_H_
+#define ZEPHYR_LIB_LIBC_NEWLIB_INCLUDE_LIMITS_H_
+
+#include_next <limits.h>
+#include <zephyr/posix/posix_limits.h>
+
+#endif /* ZEPHYR_LIB_LIBC_NEWLIB_INCLUDE_LIMITS_H_ */
