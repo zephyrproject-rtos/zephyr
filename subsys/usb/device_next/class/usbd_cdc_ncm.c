d88a7c5802cf5cfc388d6166425a831cdb55a5d6
Johann Fischer <johann.fischer@nordicsemi.no>
2025-09-01 16:04:02 +0200

    usb: device_next: avoid false error logging in CDC ECM/NCM
    The interface descriptor and its associated string descriptor are shared
    between different speed configurations. Do not try to add a string
    descriptor if it has already been added and the index is not zero.
    
    Similar to commit ec851ba7c7e2
    ("usb: device_next: avoid false error logging in CDC ACM").
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- subsys/usb/device_next/class/usbd_cdc_ncm.c
+++ subsys/usb/device_next/class/usbd_cdc_ncm.c
@@ -989,10 +989,12 @@ static int usbd_cdc_ncm_init(struct usbd_class_data *const c_data)
 
 	LOG_DBG("CDC NCM class initialized");
 
-	if (usbd_add_descriptor(uds_ctx, data->mac_desc_data)) {
-		LOG_ERR("Failed to add iMACAddress string descriptor");
-	} else {
-		desc->if0_ecm.iMACAddress = usbd_str_desc_get_idx(data->mac_desc_data);
+	if (desc->if0_ecm.iMACAddress == 0) {
+		if (usbd_add_descriptor(uds_ctx, data->mac_desc_data)) {
+			LOG_ERR("Failed to add iMACAddress string descriptor");
+		} else {
+			desc->if0_ecm.iMACAddress = usbd_str_desc_get_idx(data->mac_desc_data);
+		}
 	}
 
 	return 0;
@@ -1243,7 +1245,7 @@ static struct usbd_cdc_ncm_desc cdc_ncm_desc_##n = {				\
 		.bFunctionLength = sizeof(struct cdc_ecm_descriptor),		\
 		.bDescriptorType = USB_DESC_CS_INTERFACE,			\
 		.bDescriptorSubtype = ETHERNET_FUNC_DESC,			\
-		.iMACAddress = 4,						\
+		.iMACAddress = 0,						\
 		.bmEthernetStatistics = sys_cpu_to_le32(0),			\
 		.wMaxSegmentSize = sys_cpu_to_le16(NET_ETH_MAX_FRAME_SIZE),	\
 		.wNumberMCFilters = sys_cpu_to_le16(0),				\
