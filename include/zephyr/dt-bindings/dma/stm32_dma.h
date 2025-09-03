8c108e96d1fb24cf38fc6a3db85e39dde74abd61
Youssef Zini <youssef.zini@savoirfairelinux.com>
2025-09-03 15:19:46 +0200

    include/zephyr: dma stm32: add macro parentheses
    Add parentheses to macros in drivers and dt-bindings stm32 dma files to
    avoid potential issues with operator precedence when these macros are
    used in expressions.
    
    Signed-off-by: Youssef Zini <youssef.zini@savoirfairelinux.com>

--- include/zephyr/dt-bindings/dma/stm32_dma.h
+++ include/zephyr/dt-bindings/dma/stm32_dma.h
@@ -11,46 +11,46 @@
  * @{
  */
 /** DMA  cyclic mode config on bit 5*/
-#define STM32_DMA_CH_CFG_MODE(val)		((val & 0x1) << 5)
+#define STM32_DMA_CH_CFG_MODE(val)		(((val) & 0x1) << 5)
 #define STM32_DMA_MODE_NORMAL			STM32_DMA_CH_CFG_MODE(0)
 #define STM32_DMA_MODE_CYCLIC			STM32_DMA_CH_CFG_MODE(1)
 
 /** DMA  transfer direction config on bits 6-7 */
-#define STM32_DMA_CH_CFG_DIRECTION(val)		((val & 0x3) << 6)
+#define STM32_DMA_CH_CFG_DIRECTION(val)		(((val) & 0x3) << 6)
 #define STM32_DMA_MEMORY_TO_MEMORY		STM32_DMA_CH_CFG_DIRECTION(0)
 #define STM32_DMA_MEMORY_TO_PERIPH		STM32_DMA_CH_CFG_DIRECTION(1)
 #define STM32_DMA_PERIPH_TO_MEMORY		STM32_DMA_CH_CFG_DIRECTION(2)
 #define STM32_DMA_PERIPH_TO_PERIPH		STM32_DMA_CH_CFG_DIRECTION(3)
 
 /** DMA  Peripheral increment Address config on bit 9 */
-#define STM32_DMA_CH_CFG_PERIPH_ADDR_INC(val)	((val & 0x1) << 9)
+#define STM32_DMA_CH_CFG_PERIPH_ADDR_INC(val)	(((val) & 0x1) << 9)
 #define STM32_DMA_PERIPH_NO_INC			STM32_DMA_CH_CFG_PERIPH_ADDR_INC(0)
 #define STM32_DMA_PERIPH_INC			STM32_DMA_CH_CFG_PERIPH_ADDR_INC(1)
 
 /** DMA  Memory increment Address config on bit 10 */
-#define STM32_DMA_CH_CFG_MEM_ADDR_INC(val)	((val & 0x1) << 10)
+#define STM32_DMA_CH_CFG_MEM_ADDR_INC(val)	(((val) & 0x1) << 10)
 #define STM32_DMA_MEM_NO_INC			STM32_DMA_CH_CFG_MEM_ADDR_INC(0)
 #define STM32_DMA_MEM_INC			STM32_DMA_CH_CFG_MEM_ADDR_INC(1)
 
 /** DMA  Peripheral data size config on bits 11, 12 */
-#define STM32_DMA_CH_CFG_PERIPH_WIDTH(val)	((val & 0x3) << 11)
+#define STM32_DMA_CH_CFG_PERIPH_WIDTH(val)	(((val) & 0x3) << 11)
 #define STM32_DMA_PERIPH_8BITS			STM32_DMA_CH_CFG_PERIPH_WIDTH(0)
 #define STM32_DMA_PERIPH_16BITS			STM32_DMA_CH_CFG_PERIPH_WIDTH(1)
 #define STM32_DMA_PERIPH_32BITS			STM32_DMA_CH_CFG_PERIPH_WIDTH(2)
 
 /** DMA  Memory data size config on bits 13, 14 */
-#define STM32_DMA_CH_CFG_MEM_WIDTH(val)		((val & 0x3) << 13)
+#define STM32_DMA_CH_CFG_MEM_WIDTH(val)		(((val) & 0x3) << 13)
 #define STM32_DMA_MEM_8BITS			STM32_DMA_CH_CFG_MEM_WIDTH(0)
 #define STM32_DMA_MEM_16BITS			STM32_DMA_CH_CFG_MEM_WIDTH(1)
 #define STM32_DMA_MEM_32BITS			STM32_DMA_CH_CFG_MEM_WIDTH(2)
 
 /** DMA  Peripheral increment offset config on bit 15 */
-#define STM32_DMA_CH_CFG_PERIPH_INC_FIXED(val)	((val & 0x1) << 15)
+#define STM32_DMA_CH_CFG_PERIPH_INC_FIXED(val)	(((val) & 0x1) << 15)
 #define STM32_DMA_OFFSET_LINKED_BUS		STM32_DMA_CH_CFG_PERIPH_INC_FIXED(0)
 #define STM32_DMA_OFFSET_FIXED_4		STM32_DMA_CH_CFG_PERIPH_INC_FIXED(1)
 
 /** DMA  Priority config  on bits 16, 17*/
-#define STM32_DMA_CH_CFG_PRIORITY(val)		((val & 0x3) << 16)
+#define STM32_DMA_CH_CFG_PRIORITY(val)		(((val) & 0x3) << 16)
 #define STM32_DMA_PRIORITY_LOW			STM32_DMA_CH_CFG_PRIORITY(0)
 #define STM32_DMA_PRIORITY_MEDIUM		STM32_DMA_CH_CFG_PRIORITY(1)
 #define STM32_DMA_PRIORITY_HIGH			STM32_DMA_CH_CFG_PRIORITY(2)
