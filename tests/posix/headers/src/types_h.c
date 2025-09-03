6917aba0838b984bebc7cf69487954363a1ebb6d
Fabio Baltieri <fabiobaltieri@google.com>
2025-09-03 11:06:16 +0000

    tests: posix: add a test for types.h and _GNU_SOURCE
    This combination (with newlib) seems to catch some build failures due to
    include depdenency, adding a test file so it gets caught in CI.
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

---
+++ tests/posix/headers/src/types_h.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright The Zephyr Project Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* _GNU_SOURCE causes extra headers to be included and can cause dependency
+ * loops
+ */
+#define _GNU_SOURCE
+
+#include <sys/types.h>
