46f7761fb3d8c79cfd775d47f5147423a25949be
McAtee Maxwell <maxwell.mcatee@infineon.com>
2025-09-03 15:39:34 -0700

    drivers: clock_control: refactor ifx clock_control bindings
    - refactor infineon,fixed-clock binding
    - refactor infineon,fixed-factor binding
    - refactor infineon,peri-div binding
    
    Signed-off-by: McAtee Maxwell <maxwell.mcatee@infineon.com>

--- drivers/clock_control/clock_control_ifx_fixed_clock.c
+++ drivers/clock_control/clock_control_ifx_fixed_clock.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2025 Cypress Semiconductor Corporation (an Infineon company) or
- * an affiliate of Cypress Semiconductor Corporation
+ * Copyright (c) 2025 Infineon Technologies AG,
+ * or an affiliate of Infineon Technologies AG.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -14,29 +14,30 @@
 #include <stdlib.h>
 
 #include <zephyr/drivers/clock_control/clock_control_ifx_cat1.h>
-#include <zephyr/dt-bindings/clock/ifx_clock_source_def.h>
+#include <zephyr/dt-bindings/clock/ifx_clock_source_common.h>
+#include <zephyr/dt-bindings/clock/ifx_clock_source_boards.h>
 #include <cy_sysclk.h>
 
 #define DT_DRV_COMPAT infineon_fixed_clock
 
 struct fixed_rate_clock_config {
 	uint32_t rate;
-	uint32_t id; /* ifx_cat1_clock_block */
+	uint32_t system_clock; /* ifx_cat1_clock_block */
 };
 
 static int fixed_rate_clk_init(const struct device *dev)
 {
 	const struct fixed_rate_clock_config *const config = dev->config;
 
-	switch (config->id) {
+	switch (config->system_clock) {
 
-	case IFX_CAT1_CLOCK_BLOCK_IMO:
+	case IFX_IMO:
 		break;
 
-	case IFX_CAT1_CLOCK_BLOCK_FLL:
+	case IFX_FLL:
 		break;
 
-	case IFX_CAT1_CLOCK_BLOCK_IHO:
+	case IFX_IHO:
 		Cy_SysClk_IhoEnable();
 		break;
 
@@ -47,13 +48,12 @@ static int fixed_rate_clk_init(const struct device *dev)
 	return 0;
 }
 
-#define FIXED_CLK_INIT(idx)                                                                        \
-	static const struct fixed_rate_clock_config fixed_rate_clock_config_##idx = {              \
-		.rate = DT_INST_PROP(idx, clock_frequency),                                        \
-		.id = DT_INST_PROP(idx, clock_block),                                              \
+#define FIXED_CLK_INIT(n)                                                                          \
+	static const struct fixed_rate_clock_config fixed_rate_clock_config_##n = {                \
+		.rate = DT_INST_PROP(n, clock_frequency),                                          \
+		.system_clock = DT_INST_PROP(n, system_clock),                                     \
 	};                                                                                         \
-	DEVICE_DT_INST_DEFINE(idx, fixed_rate_clk_init, NULL, NULL,                                \
-			      &fixed_rate_clock_config_##idx, PRE_KERNEL_1,                        \
-			      CONFIG_CLOCK_CONTROL_INIT_PRIORITY, NULL);
+	DEVICE_DT_INST_DEFINE(n, fixed_rate_clk_init, NULL, NULL, &fixed_rate_clock_config_##n,    \
+			      PRE_KERNEL_1, CONFIG_CLOCK_CONTROL_INIT_PRIORITY, NULL);
 
 DT_INST_FOREACH_STATUS_OKAY(FIXED_CLK_INIT)
