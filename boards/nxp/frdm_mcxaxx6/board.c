711af9863c3cdd802e7b9b560346c19841182e0b
Peter Wang <chaoyi.wang@nxp.com>
2025-09-05 11:52:27 +0800

    boards: frdm_mcxaxx6: add frdm_mcxa366 board
    1. the three boards share the same board schematic
       - frdm_mcxa266, frdm_mcxa346, frdm_mcxa366
    2. board dts,kconfig and cmake file could share
    3. add MCXA366 soc and frdm_mcxa366 board
    
    Signed-off-by: Peter Wang <chaoyi.wang@nxp.com>

--- boards/nxp/frdm_mcxaxx6/board.c
+++ boards/nxp/frdm_mcxaxx6/board.c
@@ -17,49 +17,49 @@ extern uint32_t SystemCoreClock;
 
 void board_early_init_hook(void)
 {
-	uint32_t coreFreq;
-	spc_active_mode_core_ldo_option_t ldoOption;
-	spc_sram_voltage_config_t sramOption;
+	uint32_t core_freq;
+	spc_active_mode_core_ldo_option_t ldo_option;
+	spc_sram_voltage_config_t sram_option;
 
 	/* Get the CPU Core frequency */
-	coreFreq = CLOCK_GetCoreSysClkFreq();
+	core_freq = CLOCK_GetCoreSysClkFreq();
 
 	/* The flow of increasing voltage and frequency */
-	if (coreFreq <= BOARD_BOOTCLOCKFROHF180M_CORE_CLOCK) {
+	if (core_freq <= BOARD_BOOTCLOCKFROHF180M_CORE_CLOCK) {
 		/* Set the LDO_CORE VDD regulator level */
-		ldoOption.CoreLDOVoltage = kSPC_CoreLDO_OverDriveVoltage;
-		ldoOption.CoreLDODriveStrength = kSPC_CoreLDO_NormalDriveStrength;
-		(void)SPC_SetActiveModeCoreLDORegulatorConfig(SPC0, &ldoOption);
+		ldo_option.CoreLDOVoltage = kSPC_CoreLDO_OverDriveVoltage;
+		ldo_option.CoreLDODriveStrength = kSPC_CoreLDO_NormalDriveStrength;
+		(void)SPC_SetActiveModeCoreLDORegulatorConfig(SPC0, &ldo_option);
 		/* Configure Flash to support different voltage level and frequency */
 		FMU0->FCTRL =
 			(FMU0->FCTRL & ~((uint32_t)FMU_FCTRL_RWSC_MASK)) | (FMU_FCTRL_RWSC(0x4U));
 		/* Specifies the operating voltage for the SRAM's read/write timing margin */
-		sramOption.operateVoltage = kSPC_sramOperateAt1P2V;
-		sramOption.requestVoltageUpdate = true;
-		(void)SPC_SetSRAMOperateVoltage(SPC0, &sramOption);
+		sram_option.operateVoltage = kSPC_sramOperateAt1P2V;
+		sram_option.requestVoltageUpdate = true;
+		(void)SPC_SetSRAMOperateVoltage(SPC0, &sram_option);
 	}
 
 	/*!< Set up system dividers */
 	CLOCK_SetClockDiv(kCLOCK_DivAHBCLK, 1U); /* !< Set SYSCON.AHBCLKDIV divider to value 1 */
 	CLOCK_SetClockDiv(kCLOCK_DivFRO_HF, 1U); /* !< Set SYSCON.FROHFDIV divider to value 1 */
 	CLOCK_SetupFROHFClocking(BOARD_BOOTCLOCKFROHF180M_CORE_CLOCK); /*!< Enable FRO HF */
-	CLOCK_SetupFRO12MClocking();             /*!< Setup FRO12M clock */
+	CLOCK_SetupFRO12MClocking();                                   /*!< Setup FRO12M clock */
 
 	CLOCK_AttachClk(kFRO_HF_to_MAIN_CLK); /* !< Switch MAIN_CLK to kFRO_HF */
 
 	/* The flow of decreasing voltage and frequency */
-	if (coreFreq > BOARD_BOOTCLOCKFROHF180M_CORE_CLOCK) {
+	if (core_freq > BOARD_BOOTCLOCKFROHF180M_CORE_CLOCK) {
 		/* Configure Flash to support different voltage level and frequency */
 		FMU0->FCTRL =
 			(FMU0->FCTRL & ~((uint32_t)FMU_FCTRL_RWSC_MASK)) | (FMU_FCTRL_RWSC(0x4U));
 		/* Specifies the operating voltage for the SRAM's read/write timing margin */
-		sramOption.operateVoltage = kSPC_sramOperateAt1P2V;
-		sramOption.requestVoltageUpdate = true;
-		(void)SPC_SetSRAMOperateVoltage(SPC0, &sramOption);
+		sram_option.operateVoltage = kSPC_sramOperateAt1P2V;
+		sram_option.requestVoltageUpdate = true;
+		(void)SPC_SetSRAMOperateVoltage(SPC0, &sram_option);
 		/* Set the LDO_CORE VDD regulator level */
-		ldoOption.CoreLDOVoltage = kSPC_CoreLDO_OverDriveVoltage;
-		ldoOption.CoreLDODriveStrength = kSPC_CoreLDO_NormalDriveStrength;
-		(void)SPC_SetActiveModeCoreLDORegulatorConfig(SPC0, &ldoOption);
+		ldo_option.CoreLDOVoltage = kSPC_CoreLDO_OverDriveVoltage;
+		ldo_option.CoreLDODriveStrength = kSPC_CoreLDO_NormalDriveStrength;
+		(void)SPC_SetActiveModeCoreLDORegulatorConfig(SPC0, &ldo_option);
 	}
 
 	/*!< Set up clock selectors - Attach clocks to the peripheries */
@@ -158,7 +158,6 @@ void board_early_init_hook(void)
 	CLOCK_SetClockDiv(kCLOCK_DivWWDT0, 1u);
 #endif
 
-
 #if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(ctimer0))
 	CLOCK_SetClockDiv(kCLOCK_DivCTIMER0, 1u);
 	CLOCK_AttachClk(kFRO_LF_DIV_to_CTIMER0);
@@ -192,6 +191,11 @@ void board_early_init_hook(void)
 	CLOCK_SetClockDiv(kCLOCK_DivADC, 1u);
 	CLOCK_AttachClk(kFRO_LF_DIV_to_ADC);
 
+#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(i3c0))
+	CLOCK_SetClockDiv(kCLOCK_DivI3C0_FCLK, 15U);
+	CLOCK_AttachClk(kFRO_HF_DIV_to_I3C0FCLK);
+#endif
+
 #if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(lpadc0))
 	CLOCK_EnableClock(kCLOCK_GateADC0);
 #endif
@@ -216,6 +220,26 @@ void board_early_init_hook(void)
 	CLOCK_AttachClk(kFRO_LF_DIV_to_CMP2);
 	CLOCK_SetClockDiv(kCLOCK_DivCMP2_FUNC, 1U);
 	SPC_EnableActiveModeAnalogModules(SPC0, (kSPC_controlCmp2 | kSPC_controlCmp2Dac));
+#endif
+
+#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(opamp0))
+	RESET_ReleasePeripheralReset(kOPAMP0_RST_SHIFT_RSTn);
+	SPC_EnableActiveModeAnalogModules(SPC0, kSPC_controlOpamp0);
+#endif
+
+#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(opamp1))
+	RESET_ReleasePeripheralReset(kOPAMP1_RST_SHIFT_RSTn);
+	SPC_EnableActiveModeAnalogModules(SPC0, kSPC_controlOpamp1);
+#endif
+
+#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(opamp2))
+	RESET_ReleasePeripheralReset(kOPAMP2_RST_SHIFT_RSTn);
+	SPC_EnableActiveModeAnalogModules(SPC0, kSPC_controlOpamp2);
+#endif
+
+#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(opamp3))
+	RESET_ReleasePeripheralReset(kOPAMP3_RST_SHIFT_RSTn);
+	SPC_EnableActiveModeAnalogModules(SPC0, kSPC_controlOpamp3);
 #endif
 
 #if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(lpi2c0))
@@ -273,26 +297,6 @@ void board_early_init_hook(void)
 	CLOCK_SetClockDiv(kCLOCK_DivLPTMR0, 1u);
 #endif /* DT_PROP(DT_NODELABEL(lptmr0), clk_source) */
 
-#endif
-
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(opamp0))
-	RESET_ReleasePeripheralReset(kOPAMP0_RST_SHIFT_RSTn);
-	SPC_EnableActiveModeAnalogModules(SPC0, kSPC_controlOpamp0);
-#endif
-
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(opamp1))
-	RESET_ReleasePeripheralReset(kOPAMP1_RST_SHIFT_RSTn);
-	SPC_EnableActiveModeAnalogModules(SPC0, kSPC_controlOpamp1);
-#endif
-
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(opamp2))
-	RESET_ReleasePeripheralReset(kOPAMP2_RST_SHIFT_RSTn);
-	SPC_EnableActiveModeAnalogModules(SPC0, kSPC_controlOpamp2);
-#endif
-
-#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(opamp3))
-	RESET_ReleasePeripheralReset(kOPAMP3_RST_SHIFT_RSTn);
-	SPC_EnableActiveModeAnalogModules(SPC0, kSPC_controlOpamp3);
 #endif
 
 	/* Set SystemCoreClock variable. */
