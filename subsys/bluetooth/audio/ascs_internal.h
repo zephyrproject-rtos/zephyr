1a00b207da9d58472c6f0e9e330658fae67a5820
Emil Gydesen <emil.gydesen@nordicsemi.no>
2025-09-06 20:06:45 +0200

    Bluetooth: BAP: Add missing check for unicast server ep
    We check for all other roles when checking whether
    an endpoint can send or receive, except for the unicast server.
    It is technically implied, but if the ep is not a valid pointer,
    then we may access invalid memory.
    
    Add check for bt_bap_ep_is_unicast_server (which uses the new
    bt_ascs_has_ep function).
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/audio/ascs_internal.h
+++ subsys/bluetooth/audio/ascs_internal.h
@@ -364,5 +364,6 @@ void bt_ascs_foreach_ep(struct bt_conn *conn, bt_bap_ep_func_t func, void *user_
 
 int bt_ascs_register(uint8_t snk_cnt, uint8_t src_cnt);
 int bt_ascs_unregister(void);
+bool bt_ascs_has_ep(const struct bt_bap_ep *ep);
 
 #endif /* BT_ASCS_INTERNAL_H */
