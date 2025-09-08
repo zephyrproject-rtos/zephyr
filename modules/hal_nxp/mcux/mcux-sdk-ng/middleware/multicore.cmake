1fbab7a15c1439aebbabe96869029a16618ed165
Emilio Benavente <emilio.benavente@nxp.com>
2025-09-08 10:28:38 -0500

    boards: nxp: removing cpu0 from mcxw72
    Removing cpu0 reference from mcxw72 target
    as only cpu0 should be targeted on this device.
    
    Signed-off-by: Emilio Benavente <emilio.benavente@nxp.com>
    Signed-off-by: Andrej Butok <andrey.butok@nxp.com>

--- modules/hal_nxp/mcux/mcux-sdk-ng/middleware/multicore.cmake
+++ modules/hal_nxp/mcux/mcux-sdk-ng/middleware/multicore.cmake
@@ -1,12 +1,12 @@
 if(CONFIG_NXP_MULTICORE)
     set(CONFIG_MCUX_COMPONENT_middleware.multicore.mcmgr ON)
     set_variable_ifdef(CONFIG_SOC_MCXW716C      CONFIG_MCUX_COMPONENT_middleware.multicore.mcmgr.mcxw716)
-    set_variable_ifdef(CONFIG_SOC_MCXW727C_CPU0 CONFIG_MCUX_COMPONENT_middleware.multicore.mcmgr.mcxw727)
+    set_variable_ifdef(CONFIG_SOC_MCXW727C      CONFIG_MCUX_COMPONENT_middleware.multicore.mcmgr.mcxw727)
 
     set(CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite ON)
     set(CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite.zephyr ON)
     set_variable_ifdef(CONFIG_SOC_MCXW716C      CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite.mcxw71x)
-    set_variable_ifdef(CONFIG_SOC_MCXW727C_CPU0 CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite.mcxw72x)
+    set_variable_ifdef(CONFIG_SOC_MCXW727C      CONFIG_MCUX_COMPONENT_middleware.multicore.rpmsg-lite.mcxw72x)
 
     add_subdirectory(${MCUX_SDK_NG_DIR}/middleware/mcuxsdk-middleware-multicore
         ${CMAKE_CURRENT_BINARY_DIR}/mcuxsdk-middleware-multicore
