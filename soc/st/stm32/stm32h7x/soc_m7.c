adde493ce749a20086eb45e2c9f8b0498a2a02da
Matteo Vigni <mvigni@enphaseenergy.com>
2025-09-10 13:50:23 -0700

    soc: st: stm32: stm32h7x: Add initialization of TCM
    Add ITCM and DTCM initialization code in soc_reset_hook() on M7 core.
    
    Signed-off-by: Matteo Vigni <mvigni@enphaseenergy.com>

--- soc/st/stm32/stm32h7x/soc_m7.c
+++ soc/st/stm32/stm32h7x/soc_m7.c
@@ -22,6 +22,23 @@
 
 #include <cmsis_core.h>
 
+#if DT_NODE_HAS_STATUS_OKAY(DT_CHOSEN(zephyr_itcm)) &&                                             \
+	!DT_SAME_NODE(DT_CHOSEN(zephyr_itcm), DT_CHOSEN(zephyr_flash))
+#define ITCM_BASE DT_REG_ADDR(DT_CHOSEN(zephyr_itcm))
+#define ITCM_END  (DT_REG_ADDR(DT_CHOSEN(zephyr_itcm)) + DT_REG_SIZE(DT_CHOSEN(zephyr_itcm)))
+
+BUILD_ASSERT((ITCM_BASE & __alignof__(uint64_t)) == 0, "ITCM start address must be 64-bit aligned");
+BUILD_ASSERT((ITCM_END & __alignof__(uint64_t)) == 0, "ITCM size must be 64-bit aligned");
+#endif
+#if DT_NODE_HAS_STATUS_OKAY(DT_CHOSEN(zephyr_dtcm)) &&                                             \
+	!DT_SAME_NODE(DT_CHOSEN(zephyr_dtcm), DT_CHOSEN(zephyr_sram))
+#define DTCM_BASE DT_REG_ADDR(DT_CHOSEN(zephyr_dtcm))
+#define DTCM_END  (DT_REG_ADDR(DT_CHOSEN(zephyr_dtcm)) + DT_REG_SIZE(DT_CHOSEN(zephyr_dtcm)))
+
+BUILD_ASSERT((DTCM_BASE & __alignof__(uint32_t)) == 0, "DTCM start address must be 32-bit aligned");
+BUILD_ASSERT((DTCM_END & __alignof__(uint32_t)) == 0, "DTCM size must be 32-bit aligned");
+#endif
+
 #if defined(CONFIG_STM32H7_DUAL_CORE)
 static int stm32h7_m4_wakeup(void)
 {
@@ -48,6 +65,36 @@ static int stm32h7_m4_wakeup(void)
 	return 0;
 }
 #endif /* CONFIG_STM32H7_DUAL_CORE */
+
+void soc_reset_hook(void)
+{
+	/* ITCM/DTCM are equipped with ECC. On reset, the TCM content
+	 * is undefined and reads, unaligned or sub-sized accesses
+	 * will trigger an ECC error. Apply the TCM initialization
+	 * procedure described in the ST AN5342 "How to use error
+	 * correction code (ECC) management for internal memories
+	 * protection on STM32 MCUs" Rev. 7, section 3.1.1: write
+	 * an arbitrary value (0 in this case) to the entire TCM
+	 * using ECC data word-sized writes (as shown in Table 4,
+	 * this is 64-bit for ITCM and 32-bit for DTCM).
+	 */
+#ifdef ITCM_BASE
+	volatile uint64_t *itcm_start = (void *)ITCM_BASE;
+	volatile uint64_t *itcm_end = (void *)ITCM_END;
+
+	for (volatile uint64_t *p = itcm_start; p < itcm_end; p++) {
+		*p = 0;
+	}
+#endif
+#ifdef DTCM_BASE
+	volatile uint32_t *dtcm_start = (void *)DTCM_BASE;
+	volatile uint32_t *dtcm_end = (void *)DTCM_END;
+
+	for (volatile uint32_t *p = dtcm_start; p < dtcm_end; p++) {
+		*p = 0;
+	}
+#endif
+}
 
 /**
  * @brief Perform basic hardware initialization at boot.
