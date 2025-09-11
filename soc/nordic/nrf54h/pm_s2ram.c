6acecac030b742cd0d94c71e2cca4a434b92489d
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2025-09-11 14:15:55 +0200

    soc: nordic: nrf54h: Add MVDMA restore to S2RAM
    MVDMA is present in the application domain so it's configuration
    need to be restored when waking up from suspend to RAM.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- soc/nordic/nrf54h/pm_s2ram.c
+++ soc/nordic/nrf54h/pm_s2ram.c
@@ -8,6 +8,7 @@
 #include <zephyr/linker/sections.h>
 #include <zephyr/sys/util.h>
 #include <hal/nrf_resetinfo.h>
+#include <mvdma.h>
 #include "pm_s2ram.h"
 #include "power.h"
 
@@ -257,6 +258,9 @@ int soc_s2ram_suspend(pm_s2ram_system_off_fn_t system_off)
 #endif
 	nvic_restore(&backup_data.nvic_context);
 	scb_restore(&backup_data.scb_context);
+	if (IS_ENABLED(CONFIG_MVDMA)) {
+		mvdma_resume();
+	}
 
 	return ret;
 }
