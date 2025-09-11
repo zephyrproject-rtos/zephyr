5187a634c4b8a53dcf3f3b77ca93999099e3852b
Michal Frankiewicz <michal.frankiewicz@nordicsemi.no>
2025-09-11 09:53:47 +0200

    drivers: clock_control: Separated nrf xo shim from nrf clock shim.
    Separated clock_control_nrf_xo shim from clock_control_nrf shim.
    
    Signed-off-by: Michal Frankiewicz <michal.frankiewicz@nordicsemi.no>

--- tests/drivers/clock_control/clock_control_api/src/nrf_device_subsys.h
+++ tests/drivers/clock_control/clock_control_api/src/nrf_device_subsys.h
@@ -7,15 +7,9 @@
 #include "device_subsys.h"
 #include <zephyr/drivers/clock_control/nrf_clock_control.h>
 
-#if NRF_CLOCK_HAS_XO || !defined(CONFIG_SOC_NRF52832)
-static const struct device_subsys_data subsys_data[] = {
-#if NRF_CLOCK_HAS_XO
-	{
-		.subsys = CLOCK_CONTROL_NRF_SUBSYS_HF,
-		.startup_us = CONFIG_TEST_NRF_HF_STARTUP_TIME_US
-	},
-#endif // NRF_CLOCK_HAS_XO
+
 #ifndef CONFIG_SOC_NRF52832
+static const struct device_subsys_data subsys_data[] = {
 	/* On nrf52832 LF clock cannot be stopped because it leads
 	 * to RTC COUNTER register reset and that is unexpected by
 	 * system clock which is disrupted and may hang in the test.
@@ -25,14 +19,20 @@ static const struct device_subsys_data subsys_data[] = {
 		.startup_us = (CLOCK_CONTROL_NRF_K32SRC ==
 			NRF_CLOCK_LFCLK_RC) ? 1000 : 500000
 	}
-#endif /* !CONFIG_SOC_NRF52832 */
 };
-#endif // NRF_CLOCK_HAS_XO || !defined(CONFIG_SOC_NRF52832)
+#endif /* !CONFIG_SOC_NRF52832 */
 
 static const struct device_subsys_data subsys_data_hfclk[] = {
 	{
 		.subsys = CLOCK_CONTROL_NRF_SUBSYS_HF,
 		.startup_us = CONFIG_TEST_NRF_HF_STARTUP_TIME_US
+	}
+};
+
+static const struct device_subsys_data subsys_data_xo[] = {
+	{
+		.subsys = CLOCK_CONTROL_NRF_SUBSYS_HF,
+		.startup_us =  CONFIG_TEST_NRF_HF_STARTUP_TIME_US
 	}
 };
 
@@ -44,11 +44,18 @@ static const struct device_data devices[] = {
 		.subsys_cnt = ARRAY_SIZE(subsys_data_hfclk)
     },
 #endif // NRF_CLOCK_HAS_HFCLK
-#if NRF_CLOCK_HAS_XO || !defined(CONFIG_SOC_NRF52832)
+#if NRF_CLOCK_HAS_XO
+    {
+		.dev = DEVICE_DT_GET_ONE(nordic_nrf_clock_xo),
+		.subsys_data =  subsys_data_xo,
+		.subsys_cnt = ARRAY_SIZE(subsys_data_xo)
+    },
+#endif // NRF_CLOCK_HAS_XO
+#if !defined(CONFIG_SOC_NRF52832)
 	{
 		.dev = DEVICE_DT_GET_ONE(nordic_nrf_clock),
 		.subsys_data =  subsys_data,
 		.subsys_cnt = ARRAY_SIZE(subsys_data)
 	}
-#endif // NRF_CLOCK_HAS_XO || !defined(CONFIG_SOC_NRF52832)
+#endif // !defined(CONFIG_SOC_NRF52832)
 };
