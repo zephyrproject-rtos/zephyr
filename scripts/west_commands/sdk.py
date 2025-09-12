c7ff69b865b50ef63162c63a84d04e9c0ff02cb8
Miguel Gazquez <miguel.gazquez@bootlin.com>
2025-09-12 10:38:11 +0200

    scripts: west_commands: fix typo in west sdk install help message
    The help message for the option '-d'/'--instal-dir' of 'west sdk
    install' incorrectly use '-b' in its help message.
    Fix the typo by using '-d'.
    
    Signed-off-by: Miguel Gazquez <miguel.gazquez@bootlin.com>

--- scripts/west_commands/sdk.py
+++ scripts/west_commands/sdk.py
@@ -138,7 +138,7 @@ class Sdk(WestCommand):
             help="SDK install destination directory. "
             "The SDK will be installed on the specified path. "
             "The directory contained in the archive will be renamed and installed for the specified directory. "
-            "For example, if you specify -b /foo/bar/baz, The archive's zephyr-sdk-<version> directory will be renamed baz and placed under /foo/bar. "
+            "For example, if you specify -d /foo/bar/baz, The archive's zephyr-sdk-<version> directory will be renamed baz and placed under /foo/bar. "
             "If this option is specified, the --install-base option is ignored. "
         )
         install_args_parser.add_argument(
