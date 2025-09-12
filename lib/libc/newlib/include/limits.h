3fd186e71fd5ec2b0fd6a7770ad0c87de349561d
Al Semjonovs <asemjonovs@google.com>
2025-09-12 16:04:55 -0600

    newlib: allow limits to be defined externally
    Downstream toolchains may already define these macros, add guard to
    prevent macro redefined errors.
    
    Signed-off-by: Al Semjonovs <asemjonovs@google.com>

--- lib/libc/newlib/include/limits.h
+++ lib/libc/newlib/include/limits.h
@@ -8,6 +8,8 @@
 #define ZEPHYR_LIB_LIBC_NEWLIB_INCLUDE_LIMITS_H_
 
 #include_next <limits.h>
+#ifdef CONFIG_NEWLIB_LIBC_USE_POSIX_LIMITS_H
 #include <zephyr/posix/posix_limits.h>
+#endif
 
 #endif /* ZEPHYR_LIB_LIBC_NEWLIB_INCLUDE_LIMITS_H_ */
