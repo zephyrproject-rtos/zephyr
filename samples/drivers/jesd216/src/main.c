edb5010957a95a4a1b29df48fecd7e475ce91220
Gerard Marull-Paretas <gerard@teslabs.com>
2025-09-13 21:18:03 +0200

    samples: drivers: jesd216: add support for SF32LB
    Allow obtaining SFDP parameters using SF32LB driver.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- samples/drivers/jesd216/src/main.c
+++ samples/drivers/jesd216/src/main.c
@@ -34,6 +34,8 @@
 #define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(renesas_ra_ospi_b_nor)
 #elif DT_HAS_COMPAT_STATUS_OKAY(renesas_ra_qspi_nor)
 #define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(renesas_ra_qspi_nor)
+#elif DT_HAS_COMPAT_STATUS_OKAY(sifli_sf32lb_mpi_qspi_nor)
+#define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(sifli_sf32lb_mpi_qspi_nor)
 #else
 #error Unsupported flash driver
 #define FLASH_NODE DT_INVALID_NODE
