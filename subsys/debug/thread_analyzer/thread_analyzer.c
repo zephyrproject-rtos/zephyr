b9fda44c7ab0ec930e97073c744c8dfe1dbe391e
Dan Kalowsky <dkalowsky@amperecomputing.com>
2025-09-12 17:07:49 -0700

    debug: thread_analyzer: add thread analyzer option to reset frame
    Use THREAD_ANALYZER_LONG_FRAME_PER_INTERVAL config to reset
    longest frame value to zero after printing so that longest frame
    now represents longest frame for the most recent interval.  The
    longest overall frame is often at startup and this modification
    allows one to observe subsequent long frames.
    
    Signed-off-by: Jay Lawlor <jlawlor@amperecomputing.com>
    Signed-off-by: Dan Kalowsky <dkalowsky@amperecomputing.com>

--- subsys/debug/thread_analyzer/thread_analyzer.c
+++ subsys/debug/thread_analyzer/thread_analyzer.c
@@ -170,6 +170,11 @@ static void thread_analyze_cb(const struct k_thread *cthread, void *user_data)
 	ARG_UNUSED(ret);
 
 	cb(&info);
+
+#if IS_ENABLED(CONFIG_THREAD_ANALYZER_LONG_FRAME_PER_INTERVAL)
+	k_thread_runtime_stats_longest_frame_reset(thread);
+#endif
+
 }
 
 K_KERNEL_STACK_ARRAY_DECLARE(z_interrupt_stacks, CONFIG_MP_MAX_NUM_CPUS,
