d7c7919d04b2b2bc5507941eb39ab3809c97b50a
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2025-09-15 10:17:49 +0200

    tests: kernel: cache: Add test for __cacheline_aligned/padded
    Add test which checks that variable tagged with __cacheline_aligned and
    __cacheline_padded are correctly aligned.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- tests/kernel/cache/src/test_cache_api.c
+++ tests/kernel/cache/src/test_cache_api.c
@@ -79,6 +79,51 @@ ZTEST_USER(cache_api, test_data_cache_api_user)
 	zassert_true((ret == 0) || (ret == -ENOTSUP));
 }
 
+ZTEST(cache_api, test_cacheline_aligned_padded)
+{
+#if defined(CONFIG_DCACHE_LINE_SIZE)
+	static uint8_t var1 __cacheline_padded;
+	static uint8_t var2[5] __cacheline_padded;
+	static uint8_t var3 __cacheline_padded;
+	static uint8_t var4 __cacheline_aligned;
+	static uint8_t var5[5] __cacheline_aligned;
+	static uint8_t var6 __cacheline_aligned;
+	extern uintptr_t __data_cache_start;
+	extern uintptr_t __data_cache_end;
+
+	zassert_true(IS_ALIGNED(&var1, CONFIG_DCACHE_LINE_SIZE));
+	zassert_true(IS_ALIGNED(var2, CONFIG_DCACHE_LINE_SIZE));
+	zassert_true(IS_ALIGNED(&var3, CONFIG_DCACHE_LINE_SIZE));
+	zassert_true(IS_ALIGNED(&var4, CONFIG_DCACHE_LINE_SIZE));
+	zassert_true(IS_ALIGNED(var5, CONFIG_DCACHE_LINE_SIZE));
+	zassert_true(IS_ALIGNED(&var6, CONFIG_DCACHE_LINE_SIZE));
+
+	/* Validate that variables are placed in the dedicated section. */
+	zassert_between_inclusive((uintptr_t)&var1,
+				  (uintptr_t)&__data_cache_start,
+				  (uintptr_t)&__data_cache_end);
+	zassert_between_inclusive((uintptr_t)var2,
+				  (uintptr_t)&__data_cache_start,
+				  (uintptr_t)&__data_cache_end);
+	zassert_between_inclusive((uintptr_t)&var3,
+				  (uintptr_t)&__data_cache_start,
+				  (uintptr_t)&__data_cache_end);
+
+	var1 = 1;
+	var2[0] = 2;
+	var3 = 3;
+	var4 = 4;
+	var5[0] = 5;
+	var6 = 6;
+	zassert_equal(var1, 1);
+	zassert_equal(var2[0], 2);
+	zassert_equal(var3, 3);
+	zassert_equal(var4, 4);
+	zassert_equal(var5[0], 5);
+	zassert_equal(var6, 6);
+#endif
+}
+
 static void *cache_api_setup(void)
 {
 	sys_cache_data_enable();
