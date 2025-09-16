9d7890197c6f0319038cf4689b2c961eab5264ec
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2025-09-16 11:42:55 +0800

    drivers: sim: Enable vref clock through sim driver
    Enable vref clock through sim driver
    
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

--- include/zephyr/dt-bindings/clock/kinetis_sim.h
+++ include/zephyr/dt-bindings/clock/kinetis_sim.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2017, 2025 NXP
+ * Copyright (c) 2017, 2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -31,5 +32,6 @@
 #define KINETIS_SIM_ENET_1588_CLK	4322
 
 #define KINETIS_SIM_CMP_CLK		4323
+#define KINETIS_SIM_VREF_CLK		4324
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_KINETIS_SIM_H_ */
