Johnsonyu0926/zrtos

cmake: extensions: add add_clock_management_header() function
Add add_clock_management_header() function to cmake extensions. This can be
used by drivers to register their header files as part of the clock
management subsystem. Clock driver headers should be used to define the
`Z_CLOCK_MANAGEMENT_XXX` macros needed for each clock to read its
configuration data.

Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

cmake/modules/extensions.cmake
@@ -39,6 +39,8 @@ include(CheckCXXCompilerFlag)
 # 7.2 add_llext_* build control functions
 # 7.3 llext helper functions
 # 8. Script mode handling
+# 9. Clock Management extensions
+# 9.1 Clock Management headers
 
 ########################################################
 # 1. Zephyr-aware extensions
@@ -5884,3 +5886,50 @@ if(CMAKE_SCRIPT_MODE_FILE)
     #                         'Remember to create a YAML context'
   endfunction()
 endif()
+
+########################################################
+# 9. Clock Management extensions
+########################################################
+#
+# These functions enable clock drivers to register their headers for inclusion
+# into the clock management framework. This is required to properly support
+# out of tree clock drivers, as the clock driver header needs to be included
+# within the in-tree clock management code.
+
+# 9.1 Clock Management headers
+#
+# This function permits drivers to register headers for use with the clock
+# management framework, which provide macros to extract configuration data
+# from their clock nodes in devicetree
+
+# Usage:
+#   add_clock_management_header(<filename>)
+#
+# Adds the header file given by <filename> to the list of files included
+# within the clock management framework for clock drivers
+#
+function(add_clock_management_header filename)
+  # Get the real path of the file
+  file(REAL_PATH "${filename}" abs_path)
+  get_property(clock_management_includes TARGET clock_management_header_target PROPERTY
+    COMPILE_DEFINITIONS)
+  list(APPEND clock_management_includes "${abs_path}")
+  set_property(TARGET clock_management_header_target PROPERTY COMPILE_DEFINITIONS
+    "${clock_management_includes}")
+endfunction()
+
+# Usage:
+#   add_clock_management_header_ifdef(<setting> <filename>)
+#
+# Will add header to clock management framework if <setting> is enabled.
+#
+# <setting>: Setting to check for True value before invoking
+#            add_clock_management_header()
+#
+# See add_clock_management_header() description for other supported arguments.
+#
+macro(add_clock_management_header_ifdef feature_toggle)
+  if(${${feature_toggle}})
+    add_clock_management_header(${ARGN})
+  endif()
+endmacro()

zephyrproject-rtos/zephyr

cmake/modules/extensions.cmake
@@ -39,6 +39,8 @@ include(CheckCXXCompilerFlag)
 # 7.2 add_llext_* build control functions
 # 7.3 llext helper functions
 # 8. Script mode handling
+# 9. Clock Management extensions
+# 9.1 Clock Management headers
 
 ########################################################
 # 1. Zephyr-aware extensions
@@ -6165,3 +6167,50 @@ if(CMAKE_SCRIPT_MODE_FILE)
     # This silence the error: 'Unknown CMake command "yaml_context"'
   endfunction()
 endif()
+
+########################################################
+# 9. Clock Management extensions
+########################################################
+#
+# These functions enable clock drivers to register their headers for inclusion
+# into the clock management framework. This is required to properly support
+# out of tree clock drivers, as the clock driver header needs to be included
+# within the in-tree clock management code.
+
+# 9.1 Clock Management headers
+#
+# This function permits drivers to register headers for use with the clock
+# management framework, which provide macros to extract configuration data
+# from their clock nodes in devicetree
+
+# Usage:
+#   add_clock_management_header(<filename>)
+#
+# Adds the header file given by <filename> to the list of files included
+# within the clock management framework for clock drivers
+#
+function(add_clock_management_header filename)
+  # Get the real path of the file
+  file(REAL_PATH "${filename}" abs_path)
+  get_property(clock_management_includes TARGET clock_management_header_target PROPERTY
+	  INTERFACE_SOURCES)
+  list(APPEND clock_management_includes "${abs_path}")
+  set_property(TARGET clock_management_header_target PROPERTY INTERFACE_SOURCES
+    "${clock_management_includes}")
+endfunction()
+
+# Usage:
+#   add_clock_management_header_ifdef(<setting> <filename>)
+#
+# Will add header to clock management framework if <setting> is enabled.
+#
+# <setting>: Setting to check for True value before invoking
+#            add_clock_management_header()
+#
+# See add_clock_management_header() description for other supported arguments.
+#
+macro(add_clock_management_header_ifdef feature_toggle)
+  if(${${feature_toggle}})
+    add_clock_management_header(${ARGN})
+  endif()
+endmacro()