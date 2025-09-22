eaf626d98c75f25bfb0aa95764242e01faf95f0c
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2025-09-22 18:51:42 +0300

    Revert "[nrf noup] scripts: west: build: Use sysbuild by default if in NCS dir"
    This reverts commit 5bbb2c8dd32e564667eb7b6bc3bd27f9f58e1bfb.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- scripts/west_commands/build.py
+++ scripts/west_commands/build.py
@@ -624,12 +624,7 @@ class Build(Forceable):
         if user_args:
             cmake_opts.extend(shlex.split(user_args))
 
-        config_sysbuild = config_getboolean('sysbuild', None)
-
-        if config_sysbuild is None:
-            # If no option is set, then enable sysbuild globally
-            config_sysbuild = True
-
+        config_sysbuild = config_getboolean('sysbuild', False)
         if self.args.sysbuild or (config_sysbuild and not self.args.no_sysbuild):
             cmake_opts.extend([f'-S{SYSBUILD_PROJ_DIR}',
                                f'-DAPP_DIR:PATH={self.source_dir}'])
