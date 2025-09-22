92d9f35ab350c2902505a738f8053fbbe3f3fe76
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2025-09-22 18:51:33 +0300

    Revert "[nrf fromlist] tests: kernel: interrupt: generic ISR offset definition for nrf54h20 VPRs"
    This reverts commit 04dbe2e567ecae84c575a335a22a8aa8895bfbf5.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- tests/kernel/gen_isr_table/src/main.c
+++ tests/kernel/gen_isr_table/src/main.c
@@ -26,7 +26,7 @@ extern uintptr_t _irq_vector_table[];
 #define ISR3_OFFSET	17
 #define ISR5_OFFSET	18
 #define TRIG_CHECK_SIZE	19
-#elif defined(CONFIG_SOC_SERIES_NRF54HX) && defined(CONFIG_RISCV_CORE_NORDIC_VPR)
+#elif defined(CONFIG_SOC_NRF54H20_CPUPPR) || defined(CONFIG_SOC_NRF54H20_CPUFLPR)
 #define ISR1_OFFSET	14
 #define ISR3_OFFSET	15
 #define ISR5_OFFSET	16
