f7c295d17e5499677dce69e9b23e3f3ff654e288
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2025-09-22 18:51:28 +0300

    Revert "[nrf fromtree] drivers: flash_mspi_nor: Refactor handling of commands"
    This reverts commit 034396afc6d0dc498bd5e8f075f84ae258e58651.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- drivers/flash/flash_mspi_nor.h
+++ drivers/flash/flash_mspi_nor.h
@@ -69,6 +69,7 @@ struct flash_mspi_nor_config {
 	struct mspi_dev_id mspi_id;
 	struct mspi_dev_cfg mspi_nor_cfg;
 	struct mspi_dev_cfg mspi_nor_init_cfg;
+	enum mspi_dev_cfg_mask mspi_nor_cfg_mask;
 #if defined(CONFIG_MSPI_XIP)
 	struct mspi_xip_cfg xip_cfg;
 #endif
@@ -82,26 +83,212 @@ struct flash_mspi_nor_config {
 	struct flash_pages_layout layout;
 #endif
 	uint8_t jedec_id[SPI_NOR_MAX_ID_LEN];
+	const struct flash_mspi_nor_cmds *jedec_cmds;
 	struct flash_mspi_nor_quirks *quirks;
 	const struct jesd216_erase_type *default_erase_types;
 	struct flash_mspi_nor_cmd_info default_cmd_info;
 	struct flash_mspi_nor_switch_info default_switch_info;
-	bool jedec_id_specified  : 1;
-	bool rx_dummy_specified  : 1;
-	bool multiperipheral_bus : 1;
-	bool multi_io_cmd        : 1;
-	bool single_io_addr      : 1;
 };
 
 struct flash_mspi_nor_data {
 	struct k_sem acquired;
 	struct mspi_xfer_packet packet;
 	struct mspi_xfer xfer;
+	struct mspi_dev_cfg *curr_cfg;
 	struct jesd216_erase_type erase_types[JESD216_NUM_ERASE_TYPES];
 	struct flash_mspi_nor_cmd_info cmd_info;
 	struct flash_mspi_nor_switch_info switch_info;
-	bool in_target_io_mode;
 };
+
+struct flash_mspi_nor_cmd {
+	enum mspi_xfer_direction dir;
+	uint32_t cmd;
+	uint16_t tx_dummy;
+	uint16_t rx_dummy;
+	uint8_t cmd_length;
+	uint8_t addr_length;
+	bool force_single;
+};
+
+struct flash_mspi_nor_cmds {
+	struct flash_mspi_nor_cmd id;
+	struct flash_mspi_nor_cmd write_en;
+	struct flash_mspi_nor_cmd read;
+	struct flash_mspi_nor_cmd status;
+	struct flash_mspi_nor_cmd config;
+	struct flash_mspi_nor_cmd page_program;
+	struct flash_mspi_nor_cmd sector_erase;
+	struct flash_mspi_nor_cmd chip_erase;
+	struct flash_mspi_nor_cmd sfdp;
+};
+
+const struct flash_mspi_nor_cmds commands_single = {
+	.id = {
+		.dir = MSPI_RX,
+		.cmd = JESD216_CMD_READ_ID,
+		.cmd_length = 1,
+	},
+	.write_en = {
+		.dir = MSPI_TX,
+		.cmd = SPI_NOR_CMD_WREN,
+		.cmd_length = 1,
+	},
+	.read = {
+		.dir = MSPI_RX,
+		.cmd = SPI_NOR_CMD_READ_FAST,
+		.cmd_length = 1,
+		.addr_length = 3,
+		.rx_dummy = 8,
+	},
+	.status = {
+		.dir = MSPI_RX,
+		.cmd = SPI_NOR_CMD_RDSR,
+		.cmd_length = 1,
+	},
+	.config = {
+		.dir = MSPI_RX,
+		.cmd = SPI_NOR_CMD_RDCR,
+		.cmd_length = 1,
+	},
+	.page_program = {
+		.dir  = MSPI_TX,
+		.cmd = SPI_NOR_CMD_PP,
+		.cmd_length = 1,
+		.addr_length = 3,
+	},
+	.sector_erase = {
+		.dir = MSPI_TX,
+		.cmd = SPI_NOR_CMD_SE,
+		.cmd_length = 1,
+		.addr_length = 3,
+	},
+	.chip_erase = {
+		.dir = MSPI_TX,
+		.cmd = SPI_NOR_CMD_CE,
+		.cmd_length = 1,
+	},
+	.sfdp = {
+		.dir = MSPI_RX,
+		.cmd = JESD216_CMD_READ_SFDP,
+		.cmd_length = 1,
+		.addr_length = 3,
+		.rx_dummy = 8,
+	},
+};
+
+const struct flash_mspi_nor_cmds commands_quad_1_4_4 = {
+	.id = {
+		.dir = MSPI_RX,
+		.cmd = JESD216_CMD_READ_ID,
+		.cmd_length = 1,
+		.force_single = true,
+	},
+	.write_en = {
+		.dir = MSPI_TX,
+		.cmd = SPI_NOR_CMD_WREN,
+		.cmd_length = 1,
+	},
+	.read = {
+		.dir = MSPI_RX,
+		.cmd = SPI_NOR_CMD_4READ,
+		.cmd_length = 1,
+		.addr_length = 3,
+		.rx_dummy = 6,
+	},
+	.status = {
+		.dir = MSPI_RX,
+		.cmd = SPI_NOR_CMD_RDSR,
+		.cmd_length = 1,
+		.force_single = true,
+	},
+	.config = {
+		.dir = MSPI_RX,
+		.cmd = SPI_NOR_CMD_RDCR,
+		.cmd_length = 1,
+		.force_single = true,
+	},
+	.page_program = {
+		.dir  = MSPI_TX,
+		.cmd = SPI_NOR_CMD_PP_1_4_4,
+		.cmd_length = 1,
+		.addr_length = 3,
+	},
+	.sector_erase = {
+		.dir = MSPI_TX,
+		.cmd = SPI_NOR_CMD_SE,
+		.cmd_length = 1,
+		.addr_length = 3,
+		.force_single = true,
+	},
+	.chip_erase = {
+		.dir = MSPI_TX,
+		.cmd = SPI_NOR_CMD_CE,
+		.cmd_length = 1,
+	},
+	.sfdp = {
+		.dir = MSPI_RX,
+		.cmd = JESD216_CMD_READ_SFDP,
+		.cmd_length = 1,
+		.addr_length = 3,
+		.rx_dummy = 8,
+		.force_single = true,
+	},
+};
+
+const struct flash_mspi_nor_cmds commands_octal = {
+	.id = {
+		.dir = MSPI_RX,
+		.cmd = JESD216_OCMD_READ_ID,
+		.cmd_length = 2,
+		.addr_length = 4,
+		.rx_dummy = 4
+	},
+	.write_en = {
+		.dir = MSPI_TX,
+		.cmd = SPI_NOR_OCMD_WREN,
+		.cmd_length = 2,
+	},
+	.read = {
+		.dir = MSPI_RX,
+		.cmd = SPI_NOR_OCMD_RD,
+		.cmd_length = 2,
+		.addr_length = 4,
+		.rx_dummy = 20,
+	},
+	.status = {
+		.dir = MSPI_RX,
+		.cmd = SPI_NOR_OCMD_RDSR,
+		.cmd_length = 2,
+		.addr_length = 4,
+		.rx_dummy = 4,
+	},
+	.page_program = {
+		.dir = MSPI_TX,
+		.cmd = SPI_NOR_OCMD_PAGE_PRG,
+		.cmd_length = 2,
+		.addr_length = 4,
+	},
+	.sector_erase = {
+		.dir = MSPI_TX,
+		.cmd = SPI_NOR_OCMD_SE,
+		.cmd_length = 2,
+		.addr_length = 4,
+	},
+	.chip_erase = {
+		.dir = MSPI_TX,
+		.cmd = SPI_NOR_OCMD_CE,
+		.cmd_length = 2,
+	},
+	.sfdp = {
+		.dir = MSPI_RX,
+		.cmd = JESD216_OCMD_READ_SFDP,
+		.cmd_length = 2,
+		.addr_length = 4,
+		.rx_dummy = 20,
+	},
+};
+
+void flash_mspi_command_set(const struct device *dev, const struct flash_mspi_nor_cmd *cmd);
 
 #ifdef __cplusplus
 }
