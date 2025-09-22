a53b02e8e52acb6bdccd4ccc4470a179edbfaac8
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2025-09-22 18:51:31 +0300

    Revert "[nrf fromtree] samples: usb: move the legacy code out of the USB MSC sample"
    This reverts commit 928f44c8941d281cb934f7db038e6592cca73411.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- samples/subsys/usb/mass/src/main.c
+++ samples/subsys/usb/mass/src/main.c
@@ -9,12 +9,13 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/usb/usb_device.h>
 #include <zephyr/usb/usbd.h>
 #include <zephyr/usb/class/usbd_msc.h>
 #include <zephyr/fs/fs.h>
 #include <stdio.h>
 
-LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(main);
 
 #if CONFIG_DISK_DRIVER_FLASH
 #include <zephyr/storage/flash_map.h>
@@ -40,6 +41,7 @@ FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(storage);
 
 static struct fs_mount_t fs_mnt;
 
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
 static struct usbd_context *sample_usbd;
 
 #if CONFIG_DISK_DRIVER_RAM
@@ -53,6 +55,28 @@ USBD_DEFINE_MSC_LUN(nand, "NAND", "Zephyr", "FlashDisk", "0.00");
 #if CONFIG_DISK_DRIVER_SDMMC
 USBD_DEFINE_MSC_LUN(sd, "SD", "Zephyr", "SD", "0.00");
 #endif
+
+static int enable_usb_device_next(void)
+{
+	int err;
+
+	sample_usbd = sample_usbd_init_device(NULL);
+	if (sample_usbd == NULL) {
+		LOG_ERR("Failed to initialize USB device");
+		return -ENODEV;
+	}
+
+	err = usbd_enable(sample_usbd);
+	if (err) {
+		LOG_ERR("Failed to enable device support");
+		return err;
+	}
+
+	LOG_DBG("USB device support enabled");
+
+	return 0;
+}
+#endif /* defined(CONFIG_USB_DEVICE_STACK_NEXT) */
 
 static int setup_flash(struct fs_mount_t *mnt)
 {
@@ -191,24 +215,16 @@ int main(void)
 
 	setup_disk();
 
-	sample_usbd = sample_usbd_init_device(NULL);
-	if (sample_usbd == NULL) {
-		LOG_ERR("Failed to initialize USB device");
-		return -ENODEV;
-	}
-
-	ret = usbd_enable(sample_usbd);
-	if (ret) {
-		LOG_ERR("Failed to enable device support");
-		return ret;
-	}
-
+#if defined(CONFIG_USB_DEVICE_STACK_NEXT)
+	ret = enable_usb_device_next();
+#else
+	ret = usb_enable(NULL);
+#endif
 	if (ret != 0) {
 		LOG_ERR("Failed to enable USB");
 		return 0;
 	}
 
-	LOG_INF("The device is put in USB mass storage mode");
-
+	LOG_INF("The device is put in USB mass storage mode.\n");
 	return 0;
 }
