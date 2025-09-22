5a472d787cd781dc558518f65bd8009ab29cccd6
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2025-09-22 18:51:58 +0300

    Revert "[nrf fromtree] modules: hostap: hapd_events: Fix possible null deference"
    This reverts commit bd413c5868af9b981d8ca6c6d81e562d86dc2b8b.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- modules/hostap/src/hapd_events.c
+++ modules/hostap/src/hapd_events.c
@@ -69,14 +69,12 @@ int hostapd_send_wifi_mgmt_ap_sta_event(struct hostapd_iface *ap_ctx,
 					void *data)
 {
 	struct sta_info *sta = data;
-	char *ifname;
+	char *ifname = ap_ctx->bss[0]->conf->iface;
 	struct wifi_ap_sta_info sta_info = { 0 };
 
 	if (!ap_ctx || !sta) {
 		return -EINVAL;
 	}
-
-	ifname = ap_ctx->bss[0]->conf->iface;
 
 	memcpy(sta_info.mac, sta->addr, sizeof(sta_info.mac));
 
