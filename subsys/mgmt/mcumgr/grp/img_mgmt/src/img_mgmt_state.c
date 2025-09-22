05a9227510f5ef175119925b5535c3ae8a729806
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2025-09-22 18:51:44 +0300

    Revert "[nrf fromlist] mgmt: mcumgr: grp: img_mgmt: Show firmware loader slot info"
    This reverts commit b924e1adc5fa06b1e53e0f1088795a1a4529a271.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- subsys/mgmt/mcumgr/grp/img_mgmt/src/img_mgmt_state.c
+++ subsys/mgmt/mcumgr/grp/img_mgmt/src/img_mgmt_state.c
@@ -58,11 +58,6 @@ LOG_MODULE_DECLARE(mcumgr_img_grp, CONFIG_MCUMGR_GRP_IMG_LOG_LEVEL);
 #define DIRECT_XIP_BOOT_FOREVER		3
 #endif
 
-#if defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_FIRMWARE_UPDATER) && \
-	CONFIG_MCUMGR_GRP_IMG_UPDATABLE_IMAGE_NUMBER > 1
-#warning "MCUmgr img mgmt only supports 1 image"
-#endif
-
 /**
  * Collects information about the specified image slot.
  */
@@ -201,6 +196,7 @@ int img_mgmt_get_next_boot_slot(int image, enum img_mgmt_next_boot_type *type)
 #else
 
 #if defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_DIRECT_XIP_WITH_REVERT)
+
 static int read_directxip_state(int slot)
 {
 	struct boot_swap_state bss;
@@ -233,8 +229,6 @@ static int read_directxip_state(int slot)
 }
 #endif /* defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_DIRECT_XIP_WITH_REVERT) */
 
-#if defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_DIRECT_XIP) || \
-	defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_DIRECT_XIP_WITH_REVERT)
 int img_mgmt_get_next_boot_slot(int image, enum img_mgmt_next_boot_type *type)
 {
 	struct image_version aver;
@@ -307,13 +301,11 @@ out:
 
 	return return_slot;
 }
-#endif /* defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_DIRECT_XIP) || \
-	* defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_DIRECT_XIP_WITH_REVERT)
-	*/
 #endif /* !defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_DIRECT_XIP) && \
 	* !defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_DIRECT_XIP_WITH_REVERT) && \
 	* !defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_FIRMWARE_UPDATER)
 	*/
+
 
 /**
  * Indicates whether any image slot is pending (i.e., whether a test swap will
@@ -502,7 +494,6 @@ failed:
 /**
  * Command handler: image state read
  */
-#if !defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_FIRMWARE_UPDATER)
 int
 img_mgmt_state_read(struct smp_streamer *ctxt)
 {
@@ -560,43 +551,6 @@ img_mgmt_state_read(struct smp_streamer *ctxt)
 
 	return ok ? MGMT_ERR_EOK : MGMT_ERR_EMSGSIZE;
 }
-#else
-int
-img_mgmt_state_read(struct smp_streamer *ctxt)
-{
-	zcbor_state_t *zse = ctxt->writer->zs;
-	uint32_t i;
-	bool ok;
-
-	ok = zcbor_tstr_put_lit(zse, "images") &&
-	     zcbor_list_start_encode(zse, 2);
-
-	img_mgmt_take_lock();
-
-	for (i = 0; ok && i < CONFIG_MCUMGR_GRP_IMG_UPDATABLE_IMAGE_NUMBER; i++) {
-		/* _a is active slot, _o is opposite slot */
-		int slot_a = img_mgmt_active_slot(i);
-		int slot_o = img_mgmt_get_opposite_slot(slot_a);
-		int flags_a = REPORT_SLOT_ACTIVE;
-		int flags_o = REPORT_SLOT_CONFIRMED;
-
-		ok = img_mgmt_state_encode_slot(ctxt, slot_o, flags_o) &&
-		     img_mgmt_state_encode_slot(ctxt, slot_a, flags_a);
-	}
-
-	/* Ending list encoding for two slots per image */
-	ok = ok && zcbor_list_end_encode(zse, 2);
-	/* splitStatus is always 0 so in frugal list it is not present at all */
-	if (!IS_ENABLED(CONFIG_MCUMGR_GRP_IMG_FRUGAL_LIST) && ok) {
-		ok = zcbor_tstr_put_lit(zse, "splitStatus") &&
-		     zcbor_int32_put(zse, 0);
-	}
-
-	img_mgmt_release_lock();
-
-	return ok ? MGMT_ERR_EOK : MGMT_ERR_EMSGSIZE;
-}
-#endif /* !defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_FIRMWARE_UPDATER) */
 
 static int img_mgmt_set_next_boot_slot_common(int slot, int active_slot, bool confirm)
 {
