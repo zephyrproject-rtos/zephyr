868b7e7bb3820c1058d54097307dda2ccc5d594f
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2025-09-23 21:49:41 +0530

    drivers: nrf_wifi: Use new nRF71 interface files
    nRF70 and nRF71 now use different interface files, fix the build.
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- drivers/wifi/nrf_wifi/src/fmac_main.c
+++ drivers/wifi/nrf_wifi/src/fmac_main.c
@@ -69,9 +69,10 @@ BUILD_ASSERT(CONFIG_NRF70_MAX_TX_AGGREGATION <= 15,
 	"Max TX aggregation is 15");
 BUILD_ASSERT(CONFIG_NRF70_RX_NUM_BUFS >= 1,
 	"At least one RX buffer is required");
+#ifndef CONFIG_NRF71_ON_IPC
 BUILD_ASSERT(RPU_PKTRAM_SIZE - TOTAL_RX_SIZE >= TOTAL_TX_SIZE,
 	"Packet RAM overflow: not enough memory for TX");
-
+#endif /* CONFIG_NRF71_ON_IPC */
 BUILD_ASSERT(CONFIG_NRF70_TX_MAX_DATA_SIZE >= MAX_TX_FRAME_SIZE,
 	"TX buffer size must be at least as big as the MTU and headroom");
 
@@ -503,12 +504,9 @@ void reg_change_callbk_fn(void *vif_ctx,
 }
 #endif /* !CONFIG_NRF70_RADIO_TEST */
 
-#ifdef CONFIG_NRF71_ON_IPC
-#define MAX_TX_PWR(label) DT_PROP(DT_NODELABEL(wifi), label) * 4
-#else
+#ifndef CONFIG_NRF71_ON_IPC
 /* DTS uses 1dBm as the unit for TX power, while the RPU uses 0.25dBm */
 #define MAX_TX_PWR(label) DT_PROP(DT_NODELABEL(nrf70), label) * 4
-#endif /* CONFIG_NRF71_ON_IPC */
 
 void configure_tx_pwr_settings(struct nrf_wifi_tx_pwr_ctrl_params *tx_pwr_ctrl_params,
 				struct nrf_wifi_tx_pwr_ceil_params *tx_pwr_ceil_params)
@@ -587,6 +585,7 @@ void configure_board_dep_params(struct nrf_wifi_board_params *board_params)
 	board_params->pcb_loss_5g_band3 = CONFIG_NRF70_PCB_LOSS_5G_BAND3;
 #endif /* CONFIG_NRF70_2_4G_ONLY */
 }
+#endif /* CONFIG_NRF71_ON_IPC */
 
 enum nrf_wifi_status nrf_wifi_fmac_dev_add_zep(struct nrf_wifi_drv_priv_zep *drv_priv_zep)
 {
@@ -606,7 +605,6 @@ enum nrf_wifi_status nrf_wifi_fmac_dev_add_zep(struct nrf_wifi_drv_priv_zep *drv
 	struct nrf_wifi_tx_pwr_ctrl_params tx_pwr_ctrl_params;
 	struct nrf_wifi_tx_pwr_ceil_params tx_pwr_ceil_params;
 	struct nrf_wifi_board_params board_params;
-
 	unsigned int fw_ver = 0;
 
 #if defined(CONFIG_NRF70_SR_COEX_SLEEP_CTRL_GPIO_CTRL) && \
@@ -654,10 +652,12 @@ enum nrf_wifi_status nrf_wifi_fmac_dev_add_zep(struct nrf_wifi_drv_priv_zep *drv
 		NRF_WIFI_UMAC_VER_MIN(fw_ver),
 		NRF_WIFI_UMAC_VER_EXTRA(fw_ver));
 
+#ifndef CONFIG_NRF71_ON_IPC
 	configure_tx_pwr_settings(&tx_pwr_ctrl_params,
 				  &tx_pwr_ceil_params);
 
 	configure_board_dep_params(&board_params);
+#endif /* CONFIG_NRF71_ON_IPC */
 
 #if defined(CONFIG_NRF70_SR_COEX_SLEEP_CTRL_GPIO_CTRL) && \
 	defined(CONFIG_NRF70_SYSTEM_MODE)
@@ -853,9 +853,14 @@ static int nrf_wifi_drv_main_zep(const struct device *dev)
 	struct nrf_wifi_sys_fmac_priv *sys_fpriv = NULL;
 
 	sys_fpriv = wifi_fmac_priv(rpu_drv_priv_zep.fmac_priv);
+#ifdef CONFIG_NRF71_ON_IPC
+	/* TODO: Revisit this */
+	sys_fpriv->max_ampdu_len_per_token = 8192;
+#else
 	sys_fpriv->max_ampdu_len_per_token =
 		(RPU_PKTRAM_SIZE - (CONFIG_NRF70_RX_NUM_BUFS * CONFIG_NRF70_RX_MAX_DATA_SIZE)) /
 		CONFIG_NRF70_MAX_TX_TOKENS;
+#endif /* CONFIG_NRF71_ON_IPC */
 	/* Align to 4-byte */
 	sys_fpriv->max_ampdu_len_per_token &= ~0x3;
 
