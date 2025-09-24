37717b229f51d08121d4bee58931fd03ea57a50c
Fabio Baltieri <fabiobaltieri@google.com>
2025-09-24 09:30:41 +0000

    sys: util: rename Z_MIN Z_MAX Z_CLAMP to min max and clamp
    Rename these three macros to an unprefixed lower-case variant. This is
    normally not done for Zephyr macros (see container_of) but in this case
    it seems like a good idea to adopt the lowercase names to:
    
    1. have the same convention as the equivalent Linux macros, helping devs
       working cross project recognizing (mis)use patterns.
    2. make it somewhat intuitive that the lowercase ones are meant to be
       used in functions while the uppercase ones are to be used for static
       evaluation.
    
    Add few c++ guards to avoid colliding with std::min and std::max.
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

--- include/zephyr/sys/util.h
+++ include/zephyr/sys/util.h
@@ -376,8 +376,8 @@ extern "C" {
 /**
  * @brief Obtain the maximum of two values.
  *
- * @note Arguments are evaluated twice. Use Z_MAX for a GCC-only, single
- * evaluation version
+ * @note Arguments are evaluated twice. Use @ref max for a single evaluation
+ * version.
  *
  * @param a First value.
  * @param b Second value.
@@ -387,28 +387,30 @@ extern "C" {
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 #endif
 
+#ifndef __cplusplus
 /** @brief Return larger value of two provided expressions.
  *
  * Macro ensures that expressions are evaluated only once.
  *
  * @note Macro has limited usage compared to the standard macro as it cannot be
  *	 used:
- *	 - to generate constant integer, e.g. __aligned(Z_MAX(4,5))
- *	 - static variable, e.g. array like static uint8_t array[Z_MAX(...)];
+ *	 - to generate constant integer, e.g. __aligned(max(4,5))
+ *	 - static variable, e.g. array like static uint8_t array[max(...)];
  */
-#define Z_MAX(a, b) ({ \
+#define max(a, b) ({ \
 		/* random suffix to avoid naming conflict */ \
 		__typeof__(a) _value_a_ = (a); \
 		__typeof__(b) _value_b_ = (b); \
 		(_value_a_ > _value_b_) ? _value_a_ : _value_b_; \
 	})
+#endif
 
 #ifndef MIN
 /**
  * @brief Obtain the minimum of two values.
  *
- * @note Arguments are evaluated twice. Use Z_MIN for a GCC-only, single
- * evaluation version
+ * @note Arguments are evaluated twice. Use @ref min for a single evaluation
+ * version.
  *
  * @param a First value.
  * @param b Second value.
@@ -418,17 +420,19 @@ extern "C" {
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #endif
 
+#ifndef __cplusplus
 /** @brief Return smaller value of two provided expressions.
  *
- * Macro ensures that expressions are evaluated only once. See @ref Z_MAX for
+ * Macro ensures that expressions are evaluated only once. See @ref max for
  * macro limitations.
  */
-#define Z_MIN(a, b) ({ \
+#define min(a, b) ({ \
 		/* random suffix to avoid naming conflict */ \
 		__typeof__(a) _value_a_ = (a); \
 		__typeof__(b) _value_b_ = (b); \
 		(_value_a_ < _value_b_) ? _value_a_ : _value_b_; \
 	})
+#endif
 
 #ifndef MAX_FROM_LIST
 /**
@@ -555,8 +559,8 @@ extern "C" {
 /**
  * @brief Clamp a value to a given range.
  *
- * @note Arguments are evaluated multiple times. Use Z_CLAMP for a GCC-only,
- * single evaluation version.
+ * @note Arguments are evaluated multiple times. Use @ref clamp for a single
+ * evaluation version.
  *
  * @param val Value to be clamped.
  * @param low Lowest allowed value (inclusive).
@@ -567,12 +571,13 @@ extern "C" {
 #define CLAMP(val, low, high) (((val) <= (low)) ? (low) : MIN(val, high))
 #endif
 
+#ifndef __cplusplus
 /** @brief Return a value clamped to a given range.
  *
- * Macro ensures that expressions are evaluated only once. See @ref Z_MAX for
+ * Macro ensures that expressions are evaluated only once. See @ref max for
  * macro limitations.
  */
-#define Z_CLAMP(val, low, high) ({                                             \
+#define clamp(val, low, high) ({                                               \
 		/* random suffix to avoid naming conflict */                   \
 		__typeof__(val) _value_val_ = (val);                           \
 		__typeof__(low) _value_low_ = (low);                           \
@@ -581,6 +586,7 @@ extern "C" {
 		(_value_val_ > _value_high_) ? _value_high_ :                  \
 					       _value_val_;                    \
 	})
+#endif
 
 /**
  * @brief Checks if a value is within range.
