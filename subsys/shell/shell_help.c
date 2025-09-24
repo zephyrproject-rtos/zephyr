2bee6b5d4651b80c9c6f1c415c55c41637f936ee
Fabio Baltieri <fabiobaltieri@google.com>
2025-09-24 09:30:41 +0000

    sys: util: rename Z_MIN Z_MAX Z_CLAMP to min max and clamp
    Rename these three macros to an unprefixed lower-case variant. This is
    normally not done for Zephyr macros (see container_of) but in this case
    it seems like a good idea to adopt the lowercase names to:
    
    1. have the same convention as the equivalent Linux macros, helping devs
       working cross project recognizing (mis)use patterns.
    2. make it somewhat intuitive that the lowercase ones are meant to be
       used in functions while the uppercase ones are to be used for static
       evaluation.
    
    Add few c++ guards to avoid colliding with std::min and std::max.
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

--- subsys/shell/shell_help.c
+++ subsys/shell/shell_help.c
@@ -187,7 +187,7 @@ void z_shell_help_subcmd_print(const struct shell *sh,
 
 	/* Searching for the longest subcommand to print. */
 	while ((entry = z_shell_cmd_get(parent, idx++, &dloc)) != NULL) {
-		longest = Z_MAX(longest, z_shell_strlen(entry->syntax));
+		longest = max(longest, z_shell_strlen(entry->syntax));
 	}
 
 	/* No help to print */
