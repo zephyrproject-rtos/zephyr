8a1371c5d664313ec78aec20cfe531d90f21aac7
Logan Saint-Germain <l.saintgermain@catie.fr>
2025-09-25 10:28:56 +0200

    drivers: sensor: max30101: Enable support for multiple instance
    The max30101 sensor driver doesn't support multiple instance.
    Update Kconfig and maxim,max30101.yaml for instance based
    configuration. Propagate changes over existing files.
    
    Signed-off-by: Logan Saint-Germain <l.saintgermain@catie.fr>

--- drivers/sensor/maxim/max30101/max30101.h
+++ drivers/sensor/maxim/max30101/max30101.h
@@ -22,6 +22,7 @@
 #define MAX30101_REG_LED1_PA		0x0c
 #define MAX30101_REG_LED2_PA		0x0d
 #define MAX30101_REG_LED3_PA		0x0e
+#define MAX30101_REG_LED4_PA            0x0f
 #define MAX30101_REG_PILOT_PA		0x10
 #define MAX30101_REG_MULTI_LED		0x11
 #define MAX30101_REG_TINT		0x1f
@@ -34,6 +35,7 @@
 #define MAX30101_INT_PPG_MASK		(1 << 6)
 
 #define MAX30101_FIFO_CFG_SMP_AVE_SHIFT		5
+#define MAX30101_FIFO_CFG_ROLLOVER_EN_SHIFT     4
 #define MAX30101_FIFO_CFG_FIFO_FULL_SHIFT	0
 #define MAX30101_FIFO_CFG_ROLLOVER_EN_MASK	(1 << 4)
 
@@ -57,32 +59,40 @@
 #define MAX30101_FIFO_DATA_MASK		((1 << MAX30101_FIFO_DATA_BITS) - 1)
 
 enum max30101_mode {
-	MAX30101_MODE_HEART_RATE	= 2,
-	MAX30101_MODE_SPO2		= 3,
-	MAX30101_MODE_MULTI_LED		= 7,
+	MAX30101_MODE_HEART_RATE = 2,
+	MAX30101_MODE_SPO2 = 3,
+	MAX30101_MODE_MULTI_LED = 7,
 };
+
+static const uint8_t max30101_mode_convert[3] = {7, 2, 3};
 
 enum max30101_slot {
-	MAX30101_SLOT_DISABLED		= 0,
-	MAX30101_SLOT_RED_LED1_PA,
-	MAX30101_SLOT_IR_LED2_PA,
-	MAX30101_SLOT_GREEN_LED3_PA,
-	MAX30101_SLOT_RED_PILOT_PA,
-	MAX30101_SLOT_IR_PILOT_PA,
-	MAX30101_SLOT_GREEN_PILOT_PA,
+	MAX30101_SLOT_DISABLED = 0,
+	MAX30101_SLOT_RED_LED = 1,
+	MAX30101_SLOT_IR_LED = 2,
+	MAX30101_SLOT_GREEN_LED = 3,
 };
+
+#define MAX30101_HR_SLOTS                                                                          \
+	{MAX30101_SLOT_RED_LED, MAX30101_SLOT_DISABLED, MAX30101_SLOT_DISABLED,                    \
+	 MAX30101_SLOT_DISABLED}
+
+#define MAX30101_SPO2_SLOTS                                                                        \
+	{MAX30101_SLOT_RED_LED, MAX30101_SLOT_IR_LED, MAX30101_SLOT_DISABLED,                      \
+	 MAX30101_SLOT_DISABLED}
+
+#define MAX30101_MULTI_LED(n)                                                                      \
+	{                                                                                          \
+		DT_INST_PROP_BY_IDX(n, led_slot, 0),                                               \
+		DT_INST_PROP_BY_IDX(n, led_slot, 1),                                               \
+		DT_INST_PROP_BY_IDX(n, led_slot, 2),                                               \
+		DT_INST_PROP_BY_IDX(n, led_slot, 3),                                               \
+	}
 
 enum max30101_led_channel {
-	MAX30101_LED_CHANNEL_RED	= 0,
-	MAX30101_LED_CHANNEL_IR,
-	MAX30101_LED_CHANNEL_GREEN,
-};
-
-enum max30101_pw {
-	MAX30101_PW_15BITS		= 0,
-	MAX30101_PW_16BITS,
-	MAX30101_PW_17BITS,
-	MAX30101_PW_18BITS,
+	MAX30101_LED_CHANNEL_RED = 0,
+	MAX30101_LED_CHANNEL_IR = 1,
+	MAX30101_LED_CHANNEL_GREEN = 2,
 };
 
 struct max30101_config {
@@ -90,8 +100,8 @@ struct max30101_config {
 	uint8_t fifo;
 	uint8_t spo2;
 	uint8_t led_pa[MAX30101_MAX_NUM_CHANNELS];
-	enum max30101_mode mode;
-	enum max30101_slot slot[4];
+	uint8_t mode;
+	uint8_t slot[4];
 };
 
 struct max30101_data {
