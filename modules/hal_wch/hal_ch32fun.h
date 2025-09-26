99b0c25d011b6f3c94e85ffb8b4431442b6c3f8f
James Bennion-Pedley <james@bojit.org>
2025-09-26 10:20:04 +0100

    soc: wch: Add CH32V307 Support
    Fixes PLL Issues with PR#95814.
    Based on the work of Thomas Boje <info@andocs.biz>
    
    Signed-off-by: James Bennion-Pedley <james@bojit.org>

--- modules/hal_wch/hal_ch32fun.h
+++ modules/hal_wch/hal_ch32fun.h
@@ -39,6 +39,9 @@
 #define CH32V30x 1
 #if defined(CONFIG_SOC_CH32V303)
 #define CH32V30x_D8 1
+#elif defined(CONFIG_SOC_CH32V307)
+#define CH32V30x_D8C 1
+#endif
 #include <ch32fun.h>
 #endif /* defined(CONFIG_SOC_SERIES_QINGKE_V4F) */
 
