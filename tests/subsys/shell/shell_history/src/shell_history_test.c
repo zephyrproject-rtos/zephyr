6528605a87a37c5d63727c6ff2de0dc2804fdb37
Måns Ansgariusson <Mansgariusson@gmail.com>
2025-09-26 16:26:45 +0200

    shell: Remove shell history initialization function
    The shell history initialization function doesn't work in if the shell
    instance hasn't been created through the shell macro. This removes the
    function to avoid confusion.
    
    Signed-off-by: Måns Ansgariusson <Mansgariusson@gmail.com>

--- tests/subsys/shell/shell_history/src/shell_history_test.c
+++ tests/subsys/shell/shell_history/src/shell_history_test.c
@@ -24,6 +24,10 @@ static void init_test_buf(uint8_t *buf, size_t len, uint8_t offset)
 	}
 }
 
+static void reset_history(void *fixture)
+{
+	z_shell_history_purge(&history);
+}
 /**
  * Function tests getting line from history and compares it against expected
  * result.
@@ -54,7 +58,6 @@ static void test_get(bool ok, bool up, uint8_t *exp_buf, uint16_t exp_len)
 /* Test put line to history and get it.
  *
  * Test steps:
- * - initialize history.
  * - put line to the history.
  * - read line and verify that it is the one that was put.
  */
@@ -64,15 +67,11 @@ ZTEST(shell_test, test_history_add_get)
 
 	init_test_buf(exp_buf, sizeof(exp_buf), 0);
 
-	z_shell_history_init(&history);
-
 	test_get(false, true, NULL, 0);
 
 	z_shell_history_put(&history, exp_buf, 20);
 
 	test_get(true, true, exp_buf, 20);
-
-	z_shell_history_purge(&history);
 }
 
 /* Test verifies that after purging there is no line in the history. */
@@ -82,19 +81,16 @@ ZTEST(shell_test, test_history_purge)
 
 	init_test_buf(exp_buf, sizeof(exp_buf), 0);
 
-	z_shell_history_init(&history);
 	z_shell_history_put(&history, exp_buf, 20);
 	z_shell_history_put(&history, exp_buf, 20);
 
 	z_shell_history_purge(&history);
-
 	test_get(false, true, NULL, 0);
 }
 
 /* Test browsing history.
  *
  * Test steps:
- * - initialize history.
  * - put  lines 1,2,3 to history.
  * - get in up direction a line and verify that it's the last one added (3).
  * - get next line in up direction and verify that it's line 2.
@@ -116,7 +112,6 @@ ZTEST(shell_test, test_history_get_up_and_down)
 	init_test_buf(exp2_buf, sizeof(exp2_buf), 10);
 	init_test_buf(exp3_buf, sizeof(exp3_buf), 20);
 
-	z_shell_history_init(&history);
 	z_shell_history_put(&history, exp1_buf, 20);
 	z_shell_history_put(&history, exp2_buf, 15);
 	z_shell_history_put(&history, exp3_buf, 20);
@@ -129,8 +124,6 @@ ZTEST(shell_test, test_history_get_up_and_down)
 	test_get(true, false, exp2_buf, 15); /* down - 2 */
 	test_get(true, false, exp3_buf, 20); /* down - 3 */
 	test_get(false, false, NULL, 0); /* down - nothing */
-
-	z_shell_history_purge(&history);
 }
 
 /* Function for getting maximal buffer size that can be stored in the history */
@@ -141,7 +134,6 @@ static int get_max_buffer_len(void)
 	int len = sizeof(buf);
 	uint16_t out_len;
 
-	z_shell_history_init(&history);
 	do {
 		z_shell_history_put(&history, buf, len);
 		out_len = sizeof(out_buf);
@@ -157,7 +149,6 @@ static int get_max_buffer_len(void)
 /* Test verifies that line that cannot fit into history buffer is not stored.
  *
  * Test steps:
- * - initialize history.
  * - put buffer that is bigger than history overall capacity.
  * - verify that history is empty.
  * - put short line followed by line that is close to max.
@@ -169,7 +160,6 @@ ZTEST(shell_test, test_too_long_line_not_stored)
 	int max_len = get_max_buffer_len();
 
 	init_test_buf(exp1_buf, sizeof(exp1_buf), 0);
-	z_shell_history_init(&history);
 
 	z_shell_history_put(&history, exp1_buf, max_len + 1);
 
@@ -182,15 +172,12 @@ ZTEST(shell_test, test_too_long_line_not_stored)
 	/* Test that long entry evicts older entry. */
 	test_get(true, true, exp1_buf, max_len - 10);
 	test_get(false, true, NULL, 0); /* only one entry */
-
-	z_shell_history_purge(&history);
 }
 
 /* Test verifies that same line as the previous one is not stored in the
  * history.
  *
  * Test steps:
- * - initialize history.
  * - put same line twice.
  * - verify that only one line is in the history.
  */
@@ -199,7 +186,6 @@ ZTEST(shell_test, test_no_duplicates_in_a_row)
 	uint8_t exp1_buf[HIST_BUF_SIZE];
 
 	init_test_buf(exp1_buf, sizeof(exp1_buf), 0);
-	z_shell_history_init(&history);
 
 	z_shell_history_put(&history, exp1_buf, 20);
 	z_shell_history_put(&history, exp1_buf, 20);
@@ -207,14 +193,11 @@ ZTEST(shell_test, test_no_duplicates_in_a_row)
 	test_get(true, true, exp1_buf, 20);
 	/* only one line stored. */
 	test_get(false, true, NULL, 0);
-
-	z_shell_history_purge(&history);
 }
 
 /* Test storing long lines in the history.
  *
  * * Test steps:
- * - initialize history.
  * - Put max length line 1 in history.
  * - Verify that it is present.
  * - Put max length line 2 in history.
@@ -233,8 +216,6 @@ ZTEST(shell_test, test_storing_long_buffers)
 	init_test_buf(exp2_buf, sizeof(exp2_buf), 10);
 	init_test_buf(exp3_buf, sizeof(exp3_buf), 20);
 
-	z_shell_history_init(&history);
-
 	z_shell_history_put(&history, exp1_buf, max_len);
 	test_get(true, true, exp1_buf, max_len);
 	test_get(false, true, NULL, 0); /* only one entry */
@@ -246,8 +227,6 @@ ZTEST(shell_test, test_storing_long_buffers)
 	z_shell_history_put(&history, exp3_buf, max_len);
 	test_get(true, true, exp3_buf, max_len);
 	test_get(false, true, NULL, 0); /* only one entry */
-
-	z_shell_history_purge(&history);
 }
 
 ZTEST(shell_test, test_circle_through_history)
@@ -255,8 +234,6 @@ ZTEST(shell_test, test_circle_through_history)
 	uint8_t exp1_buf[HIST_BUF_SIZE];
 	uint8_t exp2_buf[HIST_BUF_SIZE];
 	uint8_t exp3_buf[HIST_BUF_SIZE];
-
-	z_shell_history_init(&history);
 
 	init_test_buf(exp1_buf, sizeof(exp1_buf), 0);
 	init_test_buf(exp2_buf, sizeof(exp2_buf), 10);
@@ -273,8 +250,6 @@ ZTEST(shell_test, test_circle_through_history)
 	test_get(true, true, exp3_buf, 20); /* up - 3*/
 	test_get(true, true, exp2_buf, 15); /* up - 2*/
 	test_get(true, true, exp1_buf, 20); /* up - 1*/
-
-	z_shell_history_purge(&history);
 }
 
-ZTEST_SUITE(shell_test, NULL, NULL, NULL, NULL, NULL);
+ZTEST_SUITE(shell_test, NULL, NULL, NULL, reset_history, NULL);
