be54d831ee5f5aab7acb2d70ffefc8c3e63d0c61
Pieter De Gendt <pieter.degendt@basalte.be>
2025-09-26 16:44:42 +0200

    drivers: ethernet: sam_gmac: Use ethernet MAC configuration
    Rework the Atmel SAM GMAC driver to read a MAC address from the provided
    NVMEM cell instead of using I2C commands directly.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- drivers/ethernet/eth_sam_gmac.c
+++ drivers/ethernet/eth_sam_gmac.c
@@ -33,6 +33,7 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
+#include <zephyr/nvmem.h>
 #include <zephyr/sys/__assert.h>
 #include <zephyr/sys/barrier.h>
 #include <zephyr/sys/util.h>
@@ -1729,38 +1730,6 @@ static int eth_initialize(const struct device *dev)
 	return retval;
 }
 
-#if DT_INST_NODE_HAS_PROP(0, mac_eeprom)
-static void get_mac_addr_from_i2c_eeprom(uint8_t mac_addr[6])
-{
-	uint32_t iaddr = CONFIG_ETH_SAM_GMAC_MAC_I2C_INT_ADDRESS;
-	int ret;
-	const struct i2c_dt_spec i2c = I2C_DT_SPEC_GET(DT_INST_PHANDLE(0, mac_eeprom));
-
-	if (!device_is_ready(i2c.bus)) {
-		LOG_ERR("Bus device is not ready");
-		return;
-	}
-
-	ret = i2c_write_read_dt(&i2c,
-			   &iaddr, CONFIG_ETH_SAM_GMAC_MAC_I2C_INT_ADDRESS_SIZE,
-			   mac_addr, 6);
-
-	if (ret != 0) {
-		LOG_ERR("I2C: failed to read MAC addr");
-		return;
-	}
-}
-#endif
-
-static void generate_mac(uint8_t mac_addr[6])
-{
-#if DT_INST_NODE_HAS_PROP(0, mac_eeprom)
-	get_mac_addr_from_i2c_eeprom(mac_addr);
-#elif DT_INST_PROP(0, zephyr_random_mac_address)
-	gen_random_mac(mac_addr, ATMEL_OUI_B0, ATMEL_OUI_B1, ATMEL_OUI_B2);
-#endif
-}
-
 static void phy_link_state_changed(const struct device *pdev,
 				   struct phy_link_state *state,
 				   void *user_data)
@@ -1836,7 +1805,11 @@ static void eth0_iface_init(struct net_if *iface)
 		return;
 	}
 
-	generate_mac(dev_data->mac_addr);
+	result = net_eth_mac_load(cfg->mcfg, dev_data->mac_addr);
+	if (result < 0) {
+		LOG_ERR("Failed to load MAC (%d)", result);
+		return;
+	}
 
 	LOG_INF("MAC: %02x:%02x:%02x:%02x:%02x:%02x",
 		dev_data->mac_addr[0], dev_data->mac_addr[1],
@@ -2153,6 +2126,7 @@ static void eth0_irq_config(void)
 }
 
 PINCTRL_DT_INST_DEFINE(0);
+NET_ETH_MAC_DT_INST_DEFINE(0);
 
 static const struct eth_sam_dev_cfg eth0_config = {
 	.regs = (Gmac *)DT_REG_ADDR(DT_INST_PARENT(0)),
@@ -2161,7 +2135,8 @@ static const struct eth_sam_dev_cfg eth0_config = {
 	.clock_cfg = SAM_DT_CLOCK_PMC_CFG(0, DT_INST_PARENT(0)),
 #endif
 	.config_func = eth0_irq_config,
-	.phy_dev = DEVICE_DT_GET(DT_INST_PHANDLE(0, phy_handle))
+	.phy_dev = DEVICE_DT_GET(DT_INST_PHANDLE(0, phy_handle)),
+	.mcfg = NET_ETH_MAC_DT_INST_DEV_CONFIG_GET(0),
 };
 
 static struct eth_sam_dev_data eth0_data = {
