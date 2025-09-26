943d7f4e621e75f6160f3de0545c34d288582a51
Kai Cheng <chengkai@xiaomi.com>
2025-09-26 19:50:00 +0800

    Bluetooth: Classic: add write supervision timeout feature
    Add bt_conn_br_set_supervision_timeout() API to configure the
    link supervision timeout for a BR/EDR connection via the HCI
    Write Link Supervision Timeout command.
    
    Signed-off-by: Kai Cheng <chengkai@xiaomi.com>

--- include/zephyr/bluetooth/conn.h
+++ include/zephyr/bluetooth/conn.h
@@ -3316,6 +3316,21 @@ int bt_conn_br_exit_sniff_mode(struct bt_conn *conn);
  */
 int bt_conn_br_get_supervision_timeout(struct bt_conn *conn, uint16_t *timeout);
 
+/** @brief Set BR/EDR supervision timeout.
+ *
+ *  Each physical link has a timer that is used for link supervision.
+ *  This timer is used to detect physical link loss caused by devices
+ *  moving out of range, or being blocked by interference, a device's
+ *  power-down, or other similar failure cases.
+ *
+ *  @param conn  Connection object.
+ *  @param timeout Link supervision timeout, Range: 0x0001 to 0xFFFF
+ *                 Time Range: 0.625 ms to 40.9 s.
+ *
+ *  @return  Zero for success, non-zero otherwise.
+ */
+int bt_conn_br_set_supervision_timeout(struct bt_conn *conn, uint16_t timeout);
+
 #ifdef __cplusplus
 }
 #endif
