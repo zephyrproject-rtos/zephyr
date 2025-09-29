a404a65832acf37fd11d497965db6e109af5c649
Allen Zhang <chunfeng.zhang@nxp.com>
2025-09-29 11:04:57 +0800

    tests: drivers: watchdog: Support wdt_basic_reset_none on mcxw23 boards
    Support wdt_basic_reset_none on frdm_mcxw23 and mcxw23_evk boards
    
    Signed-off-by: Allen Zhang <chunfeng.zhang@nxp.com>

--- drivers/clock_control/clock_control_mcux_syscon.c
+++ drivers/clock_control/clock_control_mcux_syscon.c
@@ -26,7 +26,7 @@ static int mcux_lpc_syscon_clock_control_on(const struct device *dev,
 #if defined(CONFIG_COUNTER_NXP_MRT)
 	if ((uint32_t)sub_system == MCUX_MRT_CLK) {
 #if defined(CONFIG_SOC_FAMILY_LPC) || defined(CONFIG_SOC_SERIES_RW6XX) ||                          \
-	defined(CONFIG_SOC_FAMILY_MCXN)
+	defined(CONFIG_SOC_FAMILY_MCXN) || defined(CONFIG_SOC_SERIES_MCXW2XX)
 		CLOCK_EnableClock(kCLOCK_Mrt);
 #elif defined(CONFIG_SOC_FAMILY_NXP_IMXRT)
 		CLOCK_EnableClock(kCLOCK_Mrt0);
