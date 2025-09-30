c64fc89184235568aa6f040bd6a6e243524b6405
Raffael Rostagno <raffael.rostagno@espressif.com>
2025-09-30 10:48:11 -0300

    tests: drivers: counter: esp32: Add RTC counter support
    Add RTC counter support (LP TIMER and RTC) for ESP32 chips.
    
    Signed-off-by: Raffael Rostagno <raffael.rostagno@espressif.com>

--- tests/drivers/counter/counter_basic_api/src/test_counter.c
+++ tests/drivers/counter/counter_basic_api/src/test_counter.c
@@ -105,6 +105,9 @@ static const struct device *const devices[] = {
 #ifdef CONFIG_COUNTER_TMR_ESP32
 	DEVS_FOR_DT_COMPAT(espressif_esp32_counter)
 #endif
+#ifdef CONFIG_COUNTER_RTC_ESP32
+	DEVS_FOR_DT_COMPAT(espressif_esp32_rtc_timer)
+#endif
 #ifdef CONFIG_COUNTER_NXP_S32_SYS_TIMER
 	DEVS_FOR_DT_COMPAT(nxp_s32_sys_timer)
 #endif
