zephyrproject-rtos/zephyr

commit 5c5e17f0f37e6a380b5c9fc7d8a7749722be3a64

kernel: dynamic: Optimize stack pool usage
Add the flags parameter to the z_thread_stack_alloc_pool function.
Determine the maximum possible stack size based on the size of the reserved
memory for stack and the thread type (flags).

The stack size that can be used by a thread depend on its type
(kerner/user). For the same stack size, the macros K_KERNEL_STACK_DECLARE
and K_THREAD_STACK_DEFINE may reserve different amount of memory.

Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

kernel/dynamic.c
@@ -31,15 +31,16 @@ static K_THREAD_STACK_ARRAY_DEFINE(dynamic_stack, CONFIG_DYNAMIC_THREAD_POOL_SIZ
 				   CONFIG_DYNAMIC_THREAD_STACK_SIZE);
 SYS_BITARRAY_DEFINE_STATIC(dynamic_ba, BA_SIZE);
 
-static k_thread_stack_t *z_thread_stack_alloc_pool(size_t size)
+static k_thread_stack_t *z_thread_stack_alloc_pool(size_t size, int flags)
 {
 	int rv;
 	size_t offset;
 	k_thread_stack_t *stack;
+	const size_t max_size = ((flags & K_USER) != 0) ? K_THREAD_STACK_SIZEOF(dynamic_stack[0]) :
+							  K_KERNEL_STACK_SIZEOF(dynamic_stack[0]);
 
-	if (size > CONFIG_DYNAMIC_THREAD_STACK_SIZE) {
-		LOG_DBG("stack size %zu is > pool stack size %d", size,
-			CONFIG_DYNAMIC_THREAD_STACK_SIZE);
+	if (size > max_size) {
+		LOG_DBG("stack size %zu is > pool stack size %zu", size, max_size);
 		return NULL;
 	}
 
@@ -79,11 +80,11 @@ k_thread_stack_t *z_impl_k_thread_stack_alloc(size_t size, int flags)
 	if (IS_ENABLED(CONFIG_DYNAMIC_THREAD_PREFER_ALLOC)) {
 		stack = z_thread_stack_alloc_dyn(size, flags);
 		if (stack == NULL && CONFIG_DYNAMIC_THREAD_POOL_SIZE > 0) {
-			stack = z_thread_stack_alloc_pool(size);
+			stack = z_thread_stack_alloc_pool(size, flags);
 		}
 	} else if (IS_ENABLED(CONFIG_DYNAMIC_THREAD_PREFER_POOL)) {
 		if (CONFIG_DYNAMIC_THREAD_POOL_SIZE > 0) {
-			stack = z_thread_stack_alloc_pool(size);
+			stack = z_thread_stack_alloc_pool(size, flags);
 		}
 
 		if ((stack == NULL) && IS_ENABLED(CONFIG_DYNAMIC_THREAD_ALLOC)) {

commit 5751f8a4c2a0533074fe3ccab888f1aeb687172a

kernel: dynamic: update storage size for pool of dynamic thread stacks
Commit 5c5e17f introduced a subtle regression when userspace was
configured on architectures requiring guard pages.

Prior to 5c5e17f, the assumption was that guard pages would be included in
`CONFIG_DYNAMIC_THREAD_STACK_SIZE`, and that was something that the caller
of `k_thread_stack_alloc()` would need to be aware of, although it was not
documented at all, unfortunately.

It seems that 5c5e17f intended to remove the need for that assumption, but
the necessary conditions for doing so had not been met.

Update pool storage size to account for guard pages, which ensures that
users can access every byte of `CONFIG_DYNAMIC_THREAD_STACK_SIZE` rather
than needing to be aware that guard pages would be included in the
requested size.

The compromise is a more intuitive API at the cost of more storage space
for the pool of thread stacks when userspace is enabled.

Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

kernel/dynamic.c
@@ -28,7 +28,7 @@ struct dyn_cb_data {
 };
 
 static K_THREAD_STACK_ARRAY_DEFINE(dynamic_stack, CONFIG_DYNAMIC_THREAD_POOL_SIZE,
-				   CONFIG_DYNAMIC_THREAD_STACK_SIZE);
+				   K_THREAD_STACK_LEN(CONFIG_DYNAMIC_THREAD_STACK_SIZE));
 SYS_BITARRAY_DEFINE_STATIC(dynamic_ba, BA_SIZE);
 
 static k_thread_stack_t *z_thread_stack_alloc_pool(size_t size, int flags)
@@ -36,11 +36,10 @@ static k_thread_stack_t *z_thread_stack_alloc_pool(size_t size, int flags)
 	int rv;
 	size_t offset;
 	k_thread_stack_t *stack;
-	const size_t max_size = ((flags & K_USER) != 0) ? K_THREAD_STACK_SIZEOF(dynamic_stack[0]) :
-							  K_KERNEL_STACK_SIZEOF(dynamic_stack[0]);
 
-	if (size > max_size) {
-		LOG_DBG("stack size %zu is > pool stack size %zu", size, max_size);
+	if (size > CONFIG_DYNAMIC_THREAD_STACK_SIZE) {
+		LOG_DBG("stack size %zu is > pool stack size %zu", size,
+			(size_t)CONFIG_DYNAMIC_THREAD_STACK_SIZE);
 		return NULL;
 	}
 

tests/posix/common/testcase.yaml
@@ -70,9 +70,6 @@ tests:
       - CONFIG_THREAD_STACK_INFO=n
   portability.posix.common.userspace:
     filter: CONFIG_ARCH_HAS_USERSPACE
-    # Excluded while #96602 is in review
-    platform_exclude:
-      - mps2/an385
     tags:
       - userspace
     extra_configs: