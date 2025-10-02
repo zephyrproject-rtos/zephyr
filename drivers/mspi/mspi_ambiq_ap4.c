5af4217040a8c627bf5b20eff7dfbda81757e62c
Richard Wheatley <richard.wheatley@ambiq.com>
2025-10-02 09:46:15 -0500

    boards: ambiq: apollo4p_evb: update for accel
    update connector to remove not needed info
    update pinctrl to remove duplicate
    add overlay for accel_polling
    
    Signed-off-by: Richard Wheatley <richard.wheatley@ambiq.com>

--- drivers/mspi/mspi_ambiq_ap4.c
+++ drivers/mspi/mspi_ambiq_ap4.c
@@ -1439,7 +1439,7 @@ static struct mspi_driver_api mspi_ambiq_driver_api = {
 		irq_enable(DT_INST_IRQN(n));                                                       \
 	}                                                                                          \
 	static uint32_t mspi_ambiq_cmdq##n[DT_INST_PROP_OR(n, cmdq_buffer_size, 1024) / 4]         \
-		__attribute__((section(DT_INST_PROP_OR(n, cmdq_buffer_location, ".mspi_buff"))));  \
+		__attribute__((section(DT_INST_PROP_OR(n, cmdq_buffer_location, ".nocache"))));    \
 	static struct gpio_dt_spec ce_gpios##n[] = MSPI_CE_GPIOS_DT_SPEC_INST_GET(n);              \
 	static struct mspi_ambiq_data mspi_ambiq_data##n = {                                       \
 		.mspiHandle = NULL,                                                                \
