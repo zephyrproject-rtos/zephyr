a172dfbe1235a80e1da7309b1c929ff0459e2c07
Alessandro Manganaro <alessandro.manganaro@st.com>
2025-10-03 16:50:38 +0200

    drivers: bluetooth: hci: STM32WBA cube fw 1.7.0 updates
    Changes required to update STM32WBA bluetooth HCI driver
    according STM32WBA Cube FW 1.7.0 version.
    
    Signed-off-by: Alessandro Manganaro <alessandro.manganaro@st.com>

--- drivers/bluetooth/hci/hci_stm32wba.c
+++ drivers/bluetooth/hci/hci_stm32wba.c
@@ -48,7 +48,9 @@ static K_SEM_DEFINE(hci_sem, 1, 1);
 			 + CFG_BLE_MBLOCK_COUNT_MARGIN)
 
 #define BLE_DYN_ALLOC_SIZE \
-	(BLE_TOTAL_BUFFER_SIZE(CFG_BLE_NUM_LINK, MBLOCK_COUNT))
+	(BLE_TOTAL_BUFFER_SIZE(CFG_BLE_NUM_LINK, \
+			       MBLOCK_COUNT, \
+			       (CFG_BLE_EATT_BEARER_PER_LINK * CFG_BLE_NUM_LINK)))
 
 /* GATT buffer size (in bytes)*/
 #define BLE_GATT_BUF_SIZE \
@@ -376,7 +378,7 @@ uint8_t BLECB_Indication(const uint8_t *data, uint16_t length,
 
 	k_sem_take(&hci_sem, K_FOREVER);
 
-	err = receive_data(dev, data, (size_t)length - 1,
+	err = receive_data(dev, data, (size_t)length,
 			   ext_data, (size_t)ext_length);
 
 	k_sem_give(&hci_sem);
