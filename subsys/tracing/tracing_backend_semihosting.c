d849ab12639e9faf754f38fc2bd7315c31fd2fad
Anas Nashif <anas.nashif@intel.com>
2025-10-05 07:04:46 -0400

    tracing: fix various style issues
    run clang-format and fix various checkpatch issues.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- subsys/tracing/tracing_backend_semihosting.c
+++ subsys/tracing/tracing_backend_semihosting.c
@@ -12,9 +12,8 @@
 
 static int tracing_fd = -1;
 
-static void tracing_backend_semihost_output(
-		const struct tracing_backend *backend,
-		uint8_t *data, uint32_t length)
+static void tracing_backend_semihost_output(const struct tracing_backend *backend, uint8_t *data,
+					    uint32_t length)
 {
 	semihost_write(tracing_fd, data, length);
 }
@@ -22,6 +21,7 @@ static void tracing_backend_semihost_output(
 static void tracing_backend_semihost_init(void)
 {
 	const char *tracing_file = "./tracing.bin";
+
 	tracing_fd = semihost_open(tracing_file, SEMIHOST_OPEN_AB_PLUS);
 	__ASSERT(tracing_fd >= 0, "semihost_open() returned %d", tracing_fd);
 	if (tracing_fd < 0) {
@@ -30,8 +30,6 @@ static void tracing_backend_semihost_init(void)
 }
 
 const struct tracing_backend_api tracing_backend_semihost_api = {
-	.init = tracing_backend_semihost_init,
-	.output  = tracing_backend_semihost_output
-};
+	.init = tracing_backend_semihost_init, .output = tracing_backend_semihost_output};
 
 TRACING_BACKEND_DEFINE(tracing_backend_semihost, tracing_backend_semihost_api);
