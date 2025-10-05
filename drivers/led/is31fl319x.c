6484d41cbc44783c195ec60f32e4565427b3fb74
Kurt Eckhardt <kurte@rockisland.com>
2025-10-05 08:24:36 -0700

    drivers: led: is31fl319x add is31fl3197 support
    Add the registers and the like to the .c file plus add a Kconf file
    plus bindings.
    
    Updated sample: to also check for is31fl3197 boards
    
    Signed-off-by: Kurt Eckhardt <kurte@rockisland.com>

--- drivers/led/is31fl319x.c
+++ drivers/led/is31fl319x.c
@@ -21,9 +21,15 @@
 #include <zephyr/dt-bindings/led/led.h>
 
 LOG_MODULE_REGISTER(is31fl319x, CONFIG_LED_LOG_LEVEL);
+
+/* define features that are specific subset of supported devices */
 #define REG_NOT_DEFINED 0xff
 
+#define FEATURE_ID_IS_ADDR	0x01	/* The id is the bus address */
+#define FEATURE_SET_CURRENT	0x02	/* the device supports setting current limits */
+
 struct is31f1319x_model {
+	const uint8_t features;
 	const uint8_t prod_id_reg;
 	const uint8_t shutdown_reg;
 	const uint8_t conf_reg;
@@ -38,7 +44,17 @@ struct is31f1319x_model {
 	const uint8_t led_channels[];
 };
 
+struct is31fl319x_config {
+	struct i2c_dt_spec bus;
+	uint8_t channel_count;
+	uint8_t num_leds;
+	const struct led_info *led_infos;
+	const uint8_t *current_limits;
+	const struct is31f1319x_model *model;
+};
 
+#ifdef CONFIG_DT_HAS_ISSI_IS31FL3194_ENABLED
+/* IS31FL3194 model registers and values */
 #define IS31FL3194_PROD_ID_REG		0x00
 #define IS31FL3194_CONF_REG		0x01
 #define IS31FL3194_CURRENT_REG		0x03
@@ -54,6 +70,8 @@ struct is31f1319x_model {
 #define IS31FL3194_CHANNEL_COUNT 3
 
 static const struct is31f1319x_model is31f13194_model = {
+	.features = FEATURE_SET_CURRENT,
+
 	/* register indexes */
 	.prod_id_reg = IS31FL3194_PROD_ID_REG,
 	.shutdown_reg = REG_NOT_DEFINED,
@@ -69,15 +87,46 @@ static const struct is31f1319x_model is31f13194_model = {
 
 	/* channel output registers */
 	.led_channels = {IS31FL3194_OUT1_REG, IS31FL3194_OUT2_REG, IS31FL3194_OUT3_REG}};
+#endif
 
-struct is31fl319x_config {
-	struct i2c_dt_spec bus;
-	uint8_t channel_count;
-	uint8_t num_leds;
-	const struct led_info *led_infos;
-	const uint8_t *current_limits;
-	const struct is31f1319x_model *regs;
+#ifdef CONFIG_DT_HAS_ISSI_IS31FL3197_ENABLED
+/* IS31FL3197 model registers and values */
+#define IS31FL3197_PROD_ID_REG		0x00
+#define IS31FL3197_SHUTDOWN_REG		0x01
+#define IS31FL3197_OPER_CONFIG_REG	0x02
+#define IS31FL3197_OUT1_REG		0x10
+#define IS31FL3197_OUT2_REG		0x11
+#define IS31FL3197_OUT3_REG		0x12
+#define IS31FL3197_OUT4_REG		0x13
+#define IS31FL3197_UPDATE_REG		0x2b
+
+#define IS31FL3197_SHUTDOWN_REG_VAL	0xf1 /* enable all channels */
+#define IS31FL3197_OPER_CONFIG_REG_VAL	0xff /* set all to current level */
+#define IS31FL3197_UPDATE_VAL		0xc5
+
+#define IS31FL3197_CHANNEL_COUNT 4
+
+static const struct is31f1319x_model is31f13197_model = {
+	.features = FEATURE_ID_IS_ADDR,
+
+	/* register indexes */
+	.prod_id_reg = IS31FL3197_PROD_ID_REG,
+	.shutdown_reg = IS31FL3197_SHUTDOWN_REG,
+	.conf_reg = IS31FL3197_OPER_CONFIG_REG,
+	.current_reg = REG_NOT_DEFINED,
+	.update_reg = IS31FL3197_UPDATE_REG,
+
+	/* values for those registers */
+	.prod_id_val = 0xff,
+	.shutdown_reg_val = IS31FL3197_SHUTDOWN_REG_VAL,
+	.conf_enable = IS31FL3197_OPER_CONFIG_REG_VAL,
+	.update_val = IS31FL3197_UPDATE_VAL,
+
+	/* channel output registers */
+	.led_channels = {IS31FL3197_OUT1_REG, IS31FL3197_OUT2_REG, IS31FL3197_OUT3_REG,
+			 IS31FL3197_OUT4_REG}
 };
+#endif
 
 static const struct led_info *is31fl319x_led_to_info(const struct is31fl319x_config *config,
 						     uint32_t led)
@@ -123,16 +172,15 @@ static int is31fl319x_write_channels(const struct device *dev, uint32_t start_ch
 				     uint32_t num_channels, const uint8_t *buf)
 {
 	const struct is31fl319x_config *config = dev->config;
-	const struct is31f1319x_model *regs = config->regs;
-	int ret;
-	int i;
+	const struct is31f1319x_model *model = config->model;
+	int ret = 0;
 
 	if ((start_channel + num_channels) > config->channel_count) {
 		return -ENOTSUP;
 	}
 
-	for (i = 0; i < num_channels; i++) {
-		ret = i2c_reg_write_byte_dt(&config->bus, regs->led_channels[i + start_channel],
+	for (int i = 0; i < num_channels; i++) {
+		ret = i2c_reg_write_byte_dt(&config->bus, model->led_channels[i + start_channel],
 					    buf[i]);
 		if (ret != 0) {
 			break;
@@ -141,8 +189,8 @@ static int is31fl319x_write_channels(const struct device *dev, uint32_t start_ch
 
 	if (ret == 0) {
 		ret = i2c_reg_write_byte_dt(&config->bus,
-					    regs->update_reg,
-					    regs->update_val);
+					    model->update_reg,
+					    model->update_val);
 	}
 
 	if (ret != 0) {
@@ -163,11 +211,15 @@ static int is31fl319x_set_color(const struct device *dev, uint32_t led, uint8_t 
 		return -ENODEV;
 	}
 
-	channel_start = is31fl319x_map_led_to_start_channel(config, led);
-
-	if (info->num_colors > config->channel_count) {
-		return -ENOTSUP;
+	if (num_colors != info->num_colors) {
+		LOG_ERR("%s: invalid number of colors: got=%d, expected=%d",
+			dev->name,
+			num_colors,
+			info->num_colors);
+		return -EINVAL;
 	}
+
+	channel_start = is31fl319x_map_led_to_start_channel(config, led);
 
 	return is31fl319x_write_channels(dev, channel_start, num_colors, color);
 }
@@ -176,7 +228,7 @@ static int is31fl319x_set_brightness(const struct device *dev, uint32_t led, uin
 {
 	const struct is31fl319x_config *config = dev->config;
 	const struct led_info *info = is31fl319x_led_to_info(config, led);
-	const struct is31f1319x_model *regs = config->regs;
+	const struct is31f1319x_model *model = config->model;
 	uint8_t channel_start;
 
 	int ret = 0;
@@ -194,11 +246,11 @@ static int is31fl319x_set_brightness(const struct device *dev, uint32_t led, uin
 
 	channel_start = is31fl319x_map_led_to_start_channel(config, led);
 
-	ret = i2c_reg_write_byte_dt(&config->bus, regs->led_channels[channel_start], value);
+	ret = i2c_reg_write_byte_dt(&config->bus, model->led_channels[channel_start], value);
 	if (ret == 0) {
 		ret = i2c_reg_write_byte_dt(&config->bus,
-					    regs->update_reg,
-					    regs->update_val);
+					    model->update_reg,
+					    model->update_val);
 	}
 
 	if (ret != 0) {
@@ -213,10 +265,9 @@ static int is31fl319x_check_config(const struct device *dev)
 	const struct is31fl319x_config *config = dev->config;
 	const struct led_info *info;
 	uint8_t rgb_count = 0;
-	uint8_t i;
 
 	/* verify that number of leds defined is not > number of channels */
-	for (i = 0; i < config->num_leds; i++) {
+	for (uint8_t i = 0; i < config->num_leds; i++) {
 		info = &config->led_infos[i];
 		rgb_count += info->num_colors;
 	}
@@ -232,14 +283,12 @@ static int is31fl319x_init(const struct device *dev)
 {
 	const struct is31fl319x_config *config = dev->config;
 	const struct led_info *info = NULL;
-	const struct is31f1319x_model *regs = config->regs;
+	const struct is31f1319x_model *model = config->model;
 	int ret;
 	uint8_t prod_id;
 	uint8_t band;
 	uint8_t channel;
 	uint8_t current_reg = 0;
-	int i;
-	int j;
 
 	ret = is31fl319x_check_config(dev);
 	if (ret != 0) {
@@ -251,40 +300,59 @@ static int is31fl319x_init(const struct device *dev)
 		return -ENODEV;
 	}
 
-	ret = i2c_reg_read_byte_dt(&config->bus, regs->prod_id_reg, &prod_id);
+	ret = i2c_reg_read_byte_dt(&config->bus, model->prod_id_reg, &prod_id);
 	if (ret != 0) {
 		LOG_ERR("%s: failed to read product ID", dev->name);
 		return ret;
 	}
 
-	if (prod_id != regs->prod_id_val) {
-		LOG_ERR("%s: invalid product ID 0x%02x (expected 0x%02x)", dev->name, prod_id,
-			regs->prod_id_val);
-		return -ENODEV;
+
+	if (model->features & FEATURE_ID_IS_ADDR) {
+		/* The product ID (8 bit) should be the I2C address(7 bit) */
+		if (prod_id != (config->bus.addr << 1)) {
+			LOG_ERR("%s: invalid product ID 0x%02x (expected 0x%02x)", dev->name,
+				prod_id, config->bus.addr << 1);
+			return -ENODEV;
+		}
+	} else {
+		if (prod_id != model->prod_id_val) {
+			LOG_ERR("%s: invalid product ID 0x%02x (expected 0x%02x)", dev->name,
+				prod_id, model->prod_id_val);
+			return -ENODEV;
+		}
 	}
 
 	/* calc current limit register value */
-	if (regs->current_reg != REG_NOT_DEFINED) {
+	if (model->features & FEATURE_SET_CURRENT) {
 		channel = 0;
-		for (i = 0; i < config->num_leds; i++) {
+		for (int i = 0; i < config->num_leds; i++) {
 			info = &config->led_infos[i];
 			band = (config->current_limits[i] / 10) - 1;
 
-			for (j = 0; j < info->num_colors; j++) {
+			for (int j = 0; j < info->num_colors; j++) {
 				current_reg |= band << (2 * channel);
 				channel++;
 			}
 		}
 
-		ret = i2c_reg_write_byte_dt(&config->bus, regs->current_reg, current_reg);
+		ret = i2c_reg_write_byte_dt(&config->bus, model->current_reg, current_reg);
 		if (ret != 0) {
 			LOG_ERR("%s: failed to set current limit", dev->name);
 			return ret;
 		}
 	}
+	if (model->shutdown_reg != REG_NOT_DEFINED) {
+		ret = i2c_reg_write_byte_dt(&config->bus, model->shutdown_reg,
+					    model->shutdown_reg_val);
+		if (ret != 0) {
+			LOG_ERR("%s: failed to set current limit", dev->name);
+			return ret;
+		}
+	}
+
 	/* enable device */
-	return i2c_reg_write_byte_dt(&config->bus, regs->conf_reg,
-				     regs->conf_enable);
+	return i2c_reg_write_byte_dt(&config->bus, model->conf_reg,
+				     model->conf_enable);
 }
 
 static DEVICE_API(led, is31fl319x_led_api) = {
@@ -308,7 +376,7 @@ static DEVICE_API(led, is31fl319x_led_api) = {
 #define LED_CURRENT(led_node_id)						\
 	DT_PROP(led_node_id, current_limit),
 
-#define IS31FL319X_DEVICE(n, id, nchannels, pregs)				\
+#define IS31FL319X_DEVICE(n, id, nchannels, pmodel)				\
 										\
 	DT_INST_FOREACH_CHILD(n, COLOR_MAPPING)					\
 										\
@@ -326,7 +394,7 @@ static DEVICE_API(led, is31fl319x_led_api) = {
 		.num_leds = ARRAY_SIZE(is31fl319##id##_leds_##n),		\
 		.led_infos = is31fl319##id##_leds_##n,				\
 		.current_limits = is31fl319##id##_currents_##n,			\
-		.regs = pregs,							\
+		.model = pmodel,						\
 	};									\
 	DEVICE_DT_INST_DEFINE(n, &is31fl319x_init, NULL, NULL,			\
 			      &is31fl319##id##_config_##n, POST_KERNEL,		\
@@ -335,3 +403,8 @@ static DEVICE_API(led, is31fl319x_led_api) = {
 #define DT_DRV_COMPAT issi_is31fl3194
 DT_INST_FOREACH_STATUS_OKAY_VARGS(IS31FL319X_DEVICE, 4, IS31FL3194_CHANNEL_COUNT,
 				  &is31f13194_model)
+#undef DT_DRV_COMPAT
+
+#define DT_DRV_COMPAT issi_is31fl3197
+DT_INST_FOREACH_STATUS_OKAY_VARGS(IS31FL319X_DEVICE, 7, IS31FL3197_CHANNEL_COUNT,
+				  &is31f13197_model)
--- samples/drivers/led/is31fl319x/src/main.c
+++ samples/drivers/led/is31fl319x/src/main.c
@@ -10,8 +10,10 @@
 #include <zephyr/dt-bindings/led/led.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define SLEEP_TIME_MS 1000
+#define BRIGHTNESS_SLEEP_TIME_MS 5
 
+#define MAX_CHANNELS 4
 /* Structure describing a color by its component values and name */
 struct color_data {
 	uint8_t r, g, b;
@@ -30,109 +32,155 @@ static const struct color_data color_sequence[] = {
 	{ 0xF4, 0x79, 0x20, "Orange" },
 };
 
+static const char * const led_colors[] = {
+	"WHITE", "RED", "GREEN", "BLUE", "AMBER", "VIOLET", "YELLOW", "IR"
+};
+
 /*
- * A build error on this line means your board is unsupported.
+ * A build error on these lines means your board is unsupported.
  */
-const struct device *led_dev = DEVICE_DT_GET_ANY(issi_is31fl3194);
+const struct device *led4_dev = DEVICE_DT_GET_ANY(issi_is31fl3194);
+const struct device *led7_dev = DEVICE_DT_GET_ANY(issi_is31fl3197);
 
 uint8_t rgb_mapping[3] = {0, 0, 0}; /* R G B */
 
 /*
- * Forward references
- */
-extern bool generate_rgb_color_mapping(const struct device *dev,  uint32_t led);
-extern bool output_rgb_color(const struct device *dev,  uint32_t led, const uint8_t *colors);
-extern void fade_one_channel(const struct device *dev,  uint8_t rgb_color_index, const char *name);
-
-/*
- * Main
- */
-int main(void)
-{
-	int i = 0;
-
-	if (!device_is_ready(led_dev)) {
-		return 0;
-	}
-
-	if (!generate_rgb_color_mapping(led_dev, 0)) {
-		printk("Failed to generate RGB Color mapping\n");
-		return 0;
-	}
-
-	/* Lets try to verify the color indexes */
-	fade_one_channel(led_dev, 0, "Red");
-	fade_one_channel(led_dev, 1, "Green");
-	fade_one_channel(led_dev, 2, "Blue");
-
-	while (1) {
-		/* Map the colors */
-		if (!output_rgb_color(led_dev, 0, &(color_sequence[i].r))) {
-			printk("Call to led_set_color failed\n");
-			return 0;
-		}
-
-		printk("LED color: %s\n", color_sequence[i].name);
-		k_msleep(SLEEP_TIME_MS);
-		i = (i + 1) % ARRAY_SIZE(color_sequence);
-	}
-
-	return 0;
-}
-
-/*
  * Generate RGB color mapping
  */
-bool generate_rgb_color_mapping(const struct device *dev,  uint32_t led)
+int generate_rgb_color_mapping(const struct device *dev)
 {
 	const struct led_info *info;
-	int i;
+	int rgb_led;
+	uint8_t colors_found;
 
 	/* Find the Red, Green, and blue color indexes */
-	if (led_get_info(dev, led, &info) < 0) {
-		printk("Failed to retrieve LED info\n");
-		return false;
-	}
-	/* Generate color mapping table for this device */
-	for (i = 0; i < info->num_colors; i++) {
-		switch (info->color_mapping[i]) {
-		case LED_COLOR_ID_RED:
-			rgb_mapping[0] = i;
-			break;
-		case LED_COLOR_ID_GREEN:
-			rgb_mapping[1] = i;
-			break;
-		case LED_COLOR_ID_BLUE:
-			rgb_mapping[2] = i;
-			break;
-		default:
-			printk("Unknown color: %x found in index %d\n", info->color_mapping[i], i);
-			/* Ignore */
+	for (rgb_led = 0;; rgb_led++) {
+		colors_found = 0;
+		printk("Check led:%u for RGB colors\n", rgb_led);
+		if (led_get_info(dev, rgb_led, &info) < 0) {
+			printk("End of LED list\n");
+			return -1;
+		}
+		/* Generate color mapping table for this device */
+		for (uint8_t i = 0; i < info->num_colors; i++) {
+			switch (info->color_mapping[i]) {
+			case LED_COLOR_ID_RED:
+				rgb_mapping[0] = i;
+				colors_found |= 0x1;
+				break;
+			case LED_COLOR_ID_GREEN:
+				rgb_mapping[1] = i;
+				colors_found |= 0x2;
+				break;
+			case LED_COLOR_ID_BLUE:
+				rgb_mapping[2] = i;
+				colors_found |= 0x4;
+				break;
+			default:
+				printk("Unknown color: %x found in index %d\n",
+				       info->color_mapping[i], i);
+				/* Ignore */
+				break;
+			}
+		}
+
+		if (colors_found == 0x07) {
 			break;
 		}
 	}
 
-	printk("Color indexes: R:%u G:%u B:%u\n", rgb_mapping[0],
+	printk("Color indexes: LED:%u R:%u G:%u B:%u\n", rgb_led, rgb_mapping[0],
 		rgb_mapping[1], rgb_mapping[2]);
-	return true;
+	return rgb_led;
 }
 
 /*
  * Set one color (R or G or B) to all on and then fade it down to off
  */
-void fade_one_channel(const struct device *dev,  uint8_t rgb_color_index, const char *name)
+void fade_one_color(const struct device *dev, uint8_t led, uint8_t led_count,
+		    uint8_t color_index, const char *color_name)
 {
-	uint8_t colors[3] = {0, 0, 0};
+	uint8_t colors[MAX_CHANNELS] = {0, 0, 0, 0};
 	int color = 0xff;
 
+	printk("%s\n", color_name);
+
 	/* Make sure all of the leds are off */
-	if (led_set_color(dev, 0, 3, colors) < 0) {
+	if (led_set_color(dev, led, led_count, colors) < 0) {
 		return;
 	}
 
-	printk("Test fading %s\n", name);
 	for (;;) {
-		led_set_channel(dev, rgb_mapping[rgb_color_index], color);
+		colors[color_index] = color;
+		if (led_set_color(dev, led, led_count, colors) < 0) {
+			return;
+		}
+		if (color == 0) {
+			break;
+		}
+		k_msleep(SLEEP_TIME_MS);
+		color >>= 2;
+	}
+}
+
+
+/*
+ * Test using led_set_brightness on LEDs with just one color
+ */
+void brightness_test_one_color(const struct device *dev, uint8_t led)
+{
+	uint8_t loop_count;
+	int brightness;
+
+	/* Go through the loop the led number count to make it easy to distinguish */
+	printk("Test set brightness for LED: %u\n", led);
+	for (loop_count = 0; loop_count <= led; loop_count++) {
+		for (brightness = 0; brightness <= 100; brightness++) {
+			if (led_set_brightness(dev, led, brightness) < 0) {
+				printk("led_set_brightness(%u, %d) failed\n", led, brightness);
+				return;
+			}
+			k_msleep(BRIGHTNESS_SLEEP_TIME_MS);
+		}
+		for (brightness = 100; brightness >= 0; brightness--) {
+			if (led_set_brightness(dev, led, brightness) < 0) {
+				printk("led_set_brightness(%u, %d) failed\n", led, brightness);
+				return;
+			}
+			k_msleep(BRIGHTNESS_SLEEP_TIME_MS);
+		}
+	}
+
+}
+
+/*
+ * Set all channels at the same time
+ */
+void fade_all_channels(const struct device *dev, uint8_t channel_count)
+{
+	uint8_t colors[MAX_CHANNELS] = {0, 0, 0, 0};
+	int color = 0xff;
+	int i;
+
+	/* Make sure all of the leds are off */
+	if (channel_count > MAX_CHANNELS) {
+		printk("Warning: channel count %u > Max channels %u\n", channel_count,
+		       MAX_CHANNELS);
+		channel_count = MAX_CHANNELS;
+
+	}
+	if (led_write_channels(dev, 0, channel_count, colors) < 0) {
+		return;
+	}
+
+	for (;;) {
+		for (i = 0; i < channel_count; i++) {
+			colors[i] = color;
+		}
+
+		if (led_write_channels(dev, 0, channel_count, colors) < 0) {
+			return;
+		}
 		if (color == 0) {
 			break;
 		}
@@ -144,7 +192,7 @@ void fade_one_channel(const struct device *dev,  uint8_t rgb_color_index, const 
 /*
  * Map the RGB color to the mapping order and output to the LED.
  */
-bool output_rgb_color(const struct device *dev,  uint32_t led, const uint8_t *colors)
+bool output_rgb_color(const struct device *dev, uint32_t led, const uint8_t *colors)
 {
 	uint8_t mapped_colors[3];
 
@@ -153,4 +201,109 @@ bool output_rgb_color(const struct device *dev,  uint32_t led, const uint8_t *co
 	}
 
 	return led_set_color(dev, led, 3, mapped_colors) >= 0;
+}
+
+/*
+ * Output LED RGB color table
+ */
+void display_rgb_color_table(const struct device *dev, uint32_t led)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(color_sequence); i++) {
+		printk("LED color: %s\n", color_sequence[i].name);
+		if (!output_rgb_color(dev, led, &(color_sequence[i].r))) {
+			printk("Call to led_set_color failed\n");
+			return;
+		}
+		k_msleep(SLEEP_TIME_MS);
+
+	}
+}
+
+int main(void)
+{
+	int err;
+	uint32_t led;
+	uint8_t color_index;
+	uint8_t device_channel_count = 0;
+	uint8_t color_id;
+	int rgb_led;
+	const struct led_info *info;
+	const struct device *led_dev = led4_dev;
+
+	if (led_dev == NULL) {
+		led_dev = led7_dev;
+	}
+
+	if (!led_dev) {
+		printk("No device with compatible issi,is31fl3194 or issi,is31fl3197 found\n");
+		return 0;
+	}
+
+	if (!device_is_ready(led_dev)) {
+		printk("LED device %s not ready\n", led_dev->name);
+		return 0;
+	}
+
+	/* Enumerate all of the LEDS and print information */
+	printk("\nLED List\n");
+	for (led = 0;; led++) {
+		err = led_get_info(led_dev, led, &info);
+		if (err < 0) {
+			printk("End of LED List\n");
+			break;
+		}
+		printk("%u: label:%s index:%u num colors:%u\n", led, info->label,
+			info->index, info->num_colors);
+		device_channel_count += info->num_colors;
+		for (color_index = 0; color_index < info->num_colors; color_index++) {
+			printk("\t%u: ", info->color_mapping[color_index]);
+			color_id = info->color_mapping[color_index];
+			fade_one_color(led_dev, led, info->num_colors, color_index,
+				       (color_id < LED_COLOR_ID_MAX) ?
+					led_colors[color_id] : "** unknown **");
+		}
+	}
+
+	/* Lets fade all of the channels at the same time */
+	printk("Fade all %u channels\n", device_channel_count);
+	fade_all_channels(led_dev, device_channel_count);
+
+	/* Check for RGB LED */
+	rgb_led = generate_rgb_color_mapping(led_dev);
+	if (rgb_led < 0) {
+		printk("No RGB LED found\n");
+	} else {
+		/* Lets try to verify the color indexes */
+		printk("RGB Colors found, now Fading: ");
+		led_get_info(led_dev, 0, &info);
+		fade_one_color(led_dev, rgb_led, info->num_colors, rgb_mapping[0], "RED");
+		fade_one_color(led_dev, rgb_led, info->num_colors, rgb_mapping[1], "GREEN");
+		fade_one_color(led_dev, rgb_led, info->num_colors, rgb_mapping[2], "BLUE");
+	}
+
+	while (1) {
+		/* Map the colors */
+		for (led = 0;; led++) {
+			err = led_get_info(led_dev, led, &info);
+			if (err < 0) {
+				printk("End of LED List\n");
+				break;
+			}
+			/* If this LED is our rgb_led, output all of the colors */
+			if (led == rgb_led) {
+				display_rgb_color_table(led_dev, led);
+			} else if (info->num_colors == 1) {
+				/* Only one color, lets try setting the brightness of it */
+				brightness_test_one_color(led_dev, led);
+			} else {
+				printk("No tests for led count: %u\n", info->num_colors);
+			}
+		}
+
+		k_msleep(SLEEP_TIME_MS);
+	}
+
+	return 0;
 }
