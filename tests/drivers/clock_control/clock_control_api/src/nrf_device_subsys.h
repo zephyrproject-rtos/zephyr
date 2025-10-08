ad18412dc4edfe1976a6b1c816862908d5dbacc3
Michal Frankiewicz <michal.frankiewicz@nordicsemi.no>
2025-10-08 09:01:46 +0200

    drivers: clock_control: Separated nrf hfclk shim from nrf clock shim.
    Separated clock_control_nrf_hfclk shim from clock_control_nrf shim.
    
    Signed-off-by: Michal Frankiewicz <michal.frankiewicz@nordicsemi.no>

--- tests/drivers/clock_control/clock_control_api/src/nrf_device_subsys.h
+++ tests/drivers/clock_control/clock_control_api/src/nrf_device_subsys.h
@@ -7,11 +7,14 @@
 #include "device_subsys.h"
 #include <zephyr/drivers/clock_control/nrf_clock_control.h>
 
+#if NRF_CLOCK_HAS_XO || !defined(CONFIG_SOC_NRF52832)
 static const struct device_subsys_data subsys_data[] = {
+#if NRF_CLOCK_HAS_XO
 	{
 		.subsys = CLOCK_CONTROL_NRF_SUBSYS_HF,
 		.startup_us = CONFIG_TEST_NRF_HF_STARTUP_TIME_US
 	},
+#endif // NRF_CLOCK_HAS_XO
 #ifndef CONFIG_SOC_NRF52832
 	/* On nrf52832 LF clock cannot be stopped because it leads
 	 * to RTC COUNTER register reset and that is unexpected by
@@ -24,11 +27,28 @@ static const struct device_subsys_data subsys_data[] = {
 	}
 #endif /* !CONFIG_SOC_NRF52832 */
 };
+#endif // NRF_CLOCK_HAS_XO || !defined(CONFIG_SOC_NRF52832)
+
+static const struct device_subsys_data subsys_data_hfclk[] = {
+	{
+		.subsys = CLOCK_CONTROL_NRF_SUBSYS_HF,
+		.startup_us = CONFIG_TEST_NRF_HF_STARTUP_TIME_US
+	}
+};
 
 static const struct device_data devices[] = {
+#if NRF_CLOCK_HAS_HFCLK    
+    {
+		.dev = DEVICE_DT_GET_ONE(nordic_nrf_clock_hfclk),
+		.subsys_data =  subsys_data_hfclk,
+		.subsys_cnt = ARRAY_SIZE(subsys_data_hfclk)
+    },
+#endif // NRF_CLOCK_HAS_HFCLK
+#if NRF_CLOCK_HAS_XO || !defined(CONFIG_SOC_NRF52832)
 	{
 		.dev = DEVICE_DT_GET_ONE(nordic_nrf_clock),
 		.subsys_data =  subsys_data,
 		.subsys_cnt = ARRAY_SIZE(subsys_data)
 	}
+#endif // NRF_CLOCK_HAS_XO || !defined(CONFIG_SOC_NRF52832)
 };
