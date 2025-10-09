169cf86969d7302ed297f6384827d682fede0ba6
Benjamin Cabé <benjamin@zephyrproject.org>
2025-10-09 22:02:26 +0200

    include: doc: fix bad @endcond markers
    Anything appearing after the @endcond tag is, well, not part of the
    conditional block anymore. This fixes "INTERNAL_HIDDEN" text appearing
    at odd polaces in the generated documentation.
    
    Fixes zephyrproject-rtos/zephyr#94862
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- include/zephyr/modbus/modbus.h
+++ include/zephyr/modbus/modbus.h
@@ -447,7 +447,7 @@ struct modbus_custom_fc {
 	uint8_t fc;
 	uint8_t excep_code;
 };
-/** @endcond INTERNAL_HIDDEN */
+/** INTERNAL_HIDDEN @endcond */
 
 /**
  * @brief Helper macro for initializing custom function code structs
