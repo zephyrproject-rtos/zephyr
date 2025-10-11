3a1b53424a458200f9f75c46fd9197891cae0f5b
Jilay Pandya <jilay.pandya@outlook.com>
2025-10-11 11:00:43 +0200

    tests: stepper_api: test motion control apis
    stepper_api does not comprise of apis for configuring stepper
    drivers. Hence the stepper drivers and corresponding api tests
    have now been removed from stepper_api test suite.
    
    gpio_step_dir and h_bridge_stepper controllers are added to the
    stepper_api test-suite
    
    Signed-off-by: Jilay Pandya <jilay.pandya@outlook.com>

--- tests/drivers/stepper/drv84xx/src/main.c
+++ tests/drivers/stepper/drv84xx/src/main.c
@@ -37,7 +37,7 @@ ZTEST_F(drv84xx_emul, test_enable_gpio_pins)
 	int value = 0;
 	int err;
 
-	err = stepper_enable(fixture->dev);
+	err = stepper_drv_enable(fixture->dev);
 	if (err == -ENOTSUP) {
 		ztest_test_skip();
 	}
@@ -52,7 +52,7 @@ ZTEST_F(drv84xx_emul, test_enable_gpio_pins)
 	}
 
 	/* As enable is supported, disable must also be supported */
-	zassert_ok(stepper_disable(fixture->dev));
+	zassert_ok(stepper_drv_disable(fixture->dev));
 
 	if (en_pin.port != NULL) {
 		value = gpio_emul_output_get(en_pin.port, en_pin.pin);
@@ -66,10 +66,10 @@ ZTEST_F(drv84xx_emul, test_enable_gpio_pins)
 
 ZTEST_F(drv84xx_emul, test_micro_step_res_set)
 {
-	enum stepper_micro_step_resolution res;
+	enum stepper_drv_micro_step_resolution res;
 	int value = 0;
 
-	zassert_ok(stepper_set_micro_step_res(fixture->dev, 4));
+	zassert_ok(stepper_drv_set_micro_step_res(fixture->dev, 4));
 
 	if (m0_pin.port == NULL || m1_pin.port == NULL) {
 		ztest_test_skip();
@@ -81,7 +81,7 @@ ZTEST_F(drv84xx_emul, test_micro_step_res_set)
 	value = gpio_emul_output_get(m1_pin.port, m1_pin.pin);
 	zassert_equal(value, 1, "M1 pin should be 1");
 
-	zassert_ok(stepper_get_micro_step_res(fixture->dev, &res));
+	zassert_ok(stepper_drv_get_micro_step_res(fixture->dev, &res));
 	zassert_equal(res, 4, "Micro step resolution not set correctly, should be %d but is %d", 4,
 		      res);
 }
