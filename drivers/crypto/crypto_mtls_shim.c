299680f32cdbfce1c096f8a957c5d053857780f4
Valerio Setti <vsetti@baylibre.com>
2025-10-13 15:53:40 +0200

    drivers: crypto: mbedtls_shim: replace legacy hash API with PSA API
    Replace usage of legacy crypto hash functions from Mbed TLS with PSA API.
    
    Signed-off-by: Valerio Setti <vsetti@baylibre.com>

--- drivers/crypto/crypto_mtls_shim.c
+++ drivers/crypto/crypto_mtls_shim.c
@@ -26,8 +26,7 @@
 #endif
 #include <mbedtls/aes.h>
 
-#include <mbedtls/sha256.h>
-#include <mbedtls/sha512.h>
+#include <psa/crypto.h>
 
 #define MTLS_SUPPORT (CAP_RAW_KEY | CAP_SEPARATE_IO_BUFS | CAP_SYNC_OPS | \
 		      CAP_NO_IV_PREFIX)
@@ -43,13 +42,12 @@ struct mtls_shim_session {
 		mbedtls_gcm_context mtls_gcm;
 #endif
 		mbedtls_aes_context mtls_aes;
-		mbedtls_sha256_context mtls_sha256;
-		mbedtls_sha512_context mtls_sha512;
+		psa_hash_operation_t hash_op;
 	};
 	bool in_use;
 	union {
 		enum cipher_mode mode;
-		enum hash_algo algo;
+		psa_algorithm_t psa_alg;
 	};
 };
 
@@ -462,25 +460,18 @@ static int mtls_session_free(const struct device *dev, struct cipher_ctx *ctx)
 	return 0;
 }
 
-static int mtls_sha256_compute(struct hash_ctx *ctx, struct hash_pkt *pkt,
-			       bool finish)
+static int mtls_hash_compute(struct hash_ctx *ctx, struct hash_pkt *pkt, bool finish)
 {
-	int ret;
-	mbedtls_sha256_context *sha256_ctx = MTLS_GET_CTX(ctx, sha256);
-
+	struct mtls_shim_session *session = ctx->drv_sessn_state;
+	size_t hash_out_len;
+	psa_status_t status;
 
 	if (!ctx->started) {
-		ret = mbedtls_sha256_starts(sha256_ctx,
-					    MTLS_GET_ALGO(ctx) == CRYPTO_HASH_ALGO_SHA224);
-		if (ret != 0) {
-			LOG_ERR("Could not compute the hash");
-			return -EINVAL;
-		}
 		ctx->started = true;
 	}
 
-	ret = mbedtls_sha256_update(sha256_ctx, pkt->in_buf, pkt->in_len);
-	if (ret != 0) {
+	status = psa_hash_update(&session->hash_op, pkt->in_buf, pkt->in_len);
+	if (status != PSA_SUCCESS) {
 		LOG_ERR("Could not update the hash");
 		ctx->started = false;
 		return -EINVAL;
@@ -488,43 +479,13 @@ static int mtls_sha256_compute(struct hash_ctx *ctx, struct hash_pkt *pkt,
 
 	if (finish) {
 		ctx->started = false;
-		ret = mbedtls_sha256_finish(sha256_ctx, pkt->out_buf);
-		if (ret != 0) {
-			LOG_ERR("Could not compute the hash");
-			return -EINVAL;
-		}
-	}
-
-	return 0;
-}
-
-static int mtls_sha512_compute(struct hash_ctx *ctx, struct hash_pkt *pkt,
-			       bool finish)
-{
-	int ret;
-	mbedtls_sha512_context *sha512_ctx = MTLS_GET_CTX(ctx, sha512);
-
-	if (!ctx->started) {
-		ret = mbedtls_sha512_starts(sha512_ctx,
-					    MTLS_GET_ALGO(ctx) == CRYPTO_HASH_ALGO_SHA384);
-		if (ret != 0) {
-			LOG_ERR("Could not compute the hash");
-			return -EINVAL;
-		}
-		ctx->started = true;
-	}
-
-	ret = mbedtls_sha512_update(sha512_ctx, pkt->in_buf, pkt->in_len);
-	if (ret != 0) {
-		LOG_ERR("Could not update the hash");
-		ctx->started = false;
-		return -EINVAL;
-	}
-
-	if (finish) {
-		ctx->started = false;
-		ret = mbedtls_sha512_finish(sha512_ctx, pkt->out_buf);
-		if (ret != 0) {
+		/* "strct hash_pkt" has no information about the size of the
+		 * output buffer, so we assume that it's at least large enough
+		 * to contain the hashing result, but this is not safe.
+		 */
+		status = psa_hash_finish(&session->hash_op, pkt->out_buf,
+					 PSA_HASH_LENGTH(session->psa_alg), &hash_out_len);
+		if (status != PSA_SUCCESS) {
 			LOG_ERR("Could not compute the hash");
 			return -EINVAL;
 		}
@@ -537,18 +498,12 @@ static int mtls_hash_session_setup(const struct device *dev,
 				   struct hash_ctx *ctx,
 				   enum hash_algo algo)
 {
+	struct mtls_shim_session *session;
 	int ctx_idx;
+	psa_status_t status;
 
 	if (ctx->flags & ~(MTLS_SUPPORT)) {
 		LOG_ERR("Unsupported flag");
-		return -EINVAL;
-	}
-
-	if ((algo != CRYPTO_HASH_ALGO_SHA224) &&
-	    (algo != CRYPTO_HASH_ALGO_SHA256) &&
-	    (algo != CRYPTO_HASH_ALGO_SHA384) &&
-	    (algo != CRYPTO_HASH_ALGO_SHA512)) {
-		LOG_ERR("Unsupported algo: %d", algo);
 		return -EINVAL;
 	}
 
@@ -558,22 +513,44 @@ static int mtls_hash_session_setup(const struct device *dev,
 		return -ENOSPC;
 	}
 
-	mtls_sessions[ctx_idx].algo = algo;
-	ctx->drv_sessn_state = &mtls_sessions[ctx_idx];
-	ctx->started = false;
-
-	if ((algo == CRYPTO_HASH_ALGO_SHA224) ||
-	    (algo == CRYPTO_HASH_ALGO_SHA256)) {
-		mbedtls_sha256_context *sha256_ctx =
-			&mtls_sessions[ctx_idx].mtls_sha256;
-		mbedtls_sha256_init(sha256_ctx);
-		ctx->hash_hndlr = mtls_sha256_compute;
-	} else {
-		mbedtls_sha512_context *sha512_ctx =
-			&mtls_sessions[ctx_idx].mtls_sha512;
-		mbedtls_sha512_init(sha512_ctx);
-		ctx->hash_hndlr = mtls_sha512_compute;
+	session = &mtls_sessions[ctx_idx];
+	session->hash_op = psa_hash_operation_init();
+	switch (algo) {
+#if IS_ENABLED(CONFIG_PSA_WANT_ALG_SHA_224)
+	case CRYPTO_HASH_ALGO_SHA224:
+		session->psa_alg = PSA_ALG_SHA_224;
+		break;
+#endif
+#if IS_ENABLED(CONFIG_PSA_WANT_ALG_SHA_256)
+	case CRYPTO_HASH_ALGO_SHA256:
+		session->psa_alg = PSA_ALG_SHA_256;
+		break;
+#endif
+#if IS_ENABLED(CONFIG_PSA_WANT_ALG_SHA_384)
+	case CRYPTO_HASH_ALGO_SHA384:
+		session->psa_alg = PSA_ALG_SHA_384;
+		break;
+#endif
+#if IS_ENABLED(CONFIG_PSA_WANT_ALG_SHA_512)
+	case CRYPTO_HASH_ALGO_SHA512:
+		session->psa_alg = PSA_ALG_SHA_512;
+		break;
+#endif
+	default:
+		LOG_ERR("Unsupported algo: %d", algo);
+		return -EINVAL;
 	}
+
+	status = psa_hash_setup(&session->hash_op, session->psa_alg);
+	if (status != PSA_SUCCESS) {
+		LOG_ERR("PSA hash operation setup failed");
+		return -EIO;
+	}
+	session->in_use = true;
+
+	ctx->hash_hndlr = mtls_hash_compute;
+	ctx->drv_sessn_state = session;
+	ctx->started = false;
 
 	return 0;
 }
@@ -583,10 +560,9 @@ static int mtls_hash_session_free(const struct device *dev, struct hash_ctx *ctx
 	struct mtls_shim_session *mtls_session =
 		(struct mtls_shim_session *)ctx->drv_sessn_state;
 
-	if (mtls_session->algo == CRYPTO_HASH_ALGO_SHA256) {
-		mbedtls_sha256_free(&mtls_session->mtls_sha256);
-	} else {
-		mbedtls_sha512_free(&mtls_session->mtls_sha512);
+	if (psa_hash_abort(&mtls_session->hash_op) != PSA_SUCCESS) {
+		LOG_ERR("PSA hash abort failed");
+		return -EIO;
 	}
 	mtls_session->in_use = false;
 
