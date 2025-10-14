a93a15a8b85e8e30b492102b5174107d4418d699
Ludvig Jordet <ludvig.jordet@nordicsemi.no>
2025-10-14 09:46:14 +0200

    Bluetooth: Mesh: Minor cleanup of prov link close on success
    Changes the link close upon success to use the `prov_link_close` helper
    function instead of doing it manually, as minor cleanup.
    
    Signed-off-by: Ludvig Jordet <ludvig.jordet@nordicsemi.no>

--- subsys/bluetooth/mesh/provisioner.c
+++ subsys/bluetooth/mesh/provisioner.c
@@ -577,10 +577,9 @@ static void prov_complete(const uint8_t *data)
 		bt_hex(&provisionee.new_dev_key, 16), node->net_idx,
 		node->num_elem, node->addr);
 
-	bt_mesh_prov_link.expect = PROV_NO_PDU;
 	atomic_set_bit(bt_mesh_prov_link.flags, COMPLETE);
 
-	bt_mesh_prov_link.bearer->link_close(PROV_BEARER_LINK_STATUS_SUCCESS);
+	prov_link_close(PROV_BEARER_LINK_STATUS_SUCCESS);
 }
 
 static void prov_node_add(void)
