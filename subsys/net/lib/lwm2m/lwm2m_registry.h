78f94f55668c5ff4f1ad9b5a3753911d050e5f2d
Julien Vermillard <julien@clunkymachines.com>
2025-10-14 15:02:51 +0200

    net: lwm2m: add cache filtering
    Introduce `lwm2m_set_cache_filter()` so applications can drop cached
    samples before they reach the LwM2M SEND path.
    
    Fixes #91590
    
    Signed-off-by: Julien Vermillard <julien@clunkymachines.com>

--- subsys/net/lib/lwm2m/lwm2m_registry.h
+++ subsys/net/lib/lwm2m/lwm2m_registry.h
@@ -209,6 +209,8 @@ struct lwm2m_time_series_resource {
 	sys_snode_t node;
 	/* Resource Path url */
 	struct lwm2m_obj_path path;
+	/* Optional filter for cached samples */
+	lwm2m_cache_filter_cb_t filter_cb;
 	/* Ring buffer */
 	struct ring_buf rb;
 };
