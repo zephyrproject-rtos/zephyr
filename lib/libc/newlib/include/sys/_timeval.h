9f13274c00d9165a207d7906b56c501627639736
Chris Friedt <cfriedt@tenstorrent.com>
2025-10-14 15:07:10 -0400

    libc: indicate timeval is defined
    In order to avoid multiple definition errors, indicate that struct
    timeval is declared.
    
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

---
+++ lib/libc/newlib/include/sys/_timeval.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright The Zephyr Project Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_LIB_LIBC_NEWLIB_INCLUDE_SYS__TIMEVAL_H_
+#define ZEPHYR_LIB_LIBC_NEWLIB_INCLUDE_SYS__TIMEVAL_H_
+
+#include_next <sys/_timeval.h>
+
+#define _TIMEVAL_DECLARED
+
+#endif /* ZEPHYR_LIB_LIBC_NEWLIB_INCLUDE_SYS__TIMEVAL_H_ */
