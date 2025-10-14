3b6d6960612f5ec1114570c30002e4539191db30
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2025-10-14 17:12:09 +0200

    samples: Bluetooth: bap broadcast source nRF5340DK 48_2_1 USB Audio
    bap broadcast source nRF5340DK with BAP_BROADCAST_48_2_1
    USB Audio Device support.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- samples/bluetooth/bap_broadcast_source/src/main.c
+++ samples/bluetooth/bap_broadcast_source/src/main.c
@@ -32,6 +32,10 @@
 #include <zephyr/usb/class/usbd_uac2.h>
 #include <zephyr/usb/usbd.h>
 
+#if defined(CONFIG_SOC_NRF5340_CPUAPP)
+#include <nrfx_clock.h>
+#endif /* CONFIG_SOC_NRF5340_CPUAPP */
+
 BUILD_ASSERT(strlen(CONFIG_BROADCAST_CODE) <= BT_ISO_BROADCAST_CODE_SIZE, "Invalid broadcast code");
 BUILD_ASSERT(IN_RANGE(strlen(CONFIG_BROADCAST_NAME), BT_AUDIO_BROADCAST_NAME_LEN_MIN,
 		      BT_AUDIO_BROADCAST_NAME_LEN_MAX),
@@ -512,6 +516,15 @@ int main(void)
 	};
 	struct bt_le_ext_adv *adv;
 	int err;
+
+#if defined(CONFIG_SOC_NRF5340_CPUAPP)
+	/* Use this to turn on 128 MHz clock for the nRF5340 cpu_app */
+	err = nrfx_clock_divider_set(NRF_CLOCK_DOMAIN_HFCLK, NRF_CLOCK_HFCLK_DIV_1);
+	if (err != 0) {
+		printk("Failed to set 128 MHz: %d\n", err);
+		return 0;
+	}
+#endif /* CONFIG_SOC_NRF5340_CPUAPP */
 
 	err = bt_enable(NULL);
 	if (err) {
