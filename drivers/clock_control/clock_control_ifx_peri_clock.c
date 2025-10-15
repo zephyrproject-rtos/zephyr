8cffa896dfdeee6b56a859c31a9b9f616bd285a6
McAtee Maxwell <maxwell.mcatee@infineon.com>
2025-10-15 15:40:13 -0700

    drivers: pwm: add support for Infineon kit_pse84_eval
    - Update the driver to support the kit_pse84_eval board
    - Update to new peripheral clock allocation scheme
    
    Signed-off-by: McAtee Maxwell <maxwell.mcatee@infineon.com>

--- drivers/clock_control/clock_control_ifx_peri_clock.c
+++ drivers/clock_control/clock_control_ifx_peri_clock.c
@@ -40,7 +40,7 @@ struct ifx_cat1_peri_clock_data {
 
 #define _IFX_CAT1_PCLK_GROUP(clkdst) ((uint8_t)((uint32_t)(clkdst) >> 8))
 #define _IFX_CAT1_TCPWM0_PCLK_CLOCK0 PCLK_TCPWM0_CLOCK_COUNTER_EN0
-#define _IFX_CAT1_TCPWM1_PCLK_CLOCK0 PCLK_TCPWM1_CLOCK_COUNTER_EN0
+#define _IFX_CAT1_TCPWM1_PCLK_CLOCK0 PCLK_TCPWM0_CLOCK_COUNTER_EN256
 #define _IFX_CAT1_SCB0_PCLK_CLOCK    PCLK_SCB0_CLOCK_SCB_EN
 #define _IFX_CAT1_SCB1_PCLK_CLOCK    PCLK_SCB1_CLOCK_SCB_EN
 #define _IFX_CAT1_SCB5_PCLK_CLOCK    PCLK_SCB5_CLOCK_SCB_EN
@@ -70,12 +70,34 @@ en_clk_dst_t ifx_cat1_scb_get_clock_index(uint32_t block_num)
 	return clk;
 }
 
+en_clk_dst_t ifx_cat1_tcpwm_get_clock_index(uint32_t block_num, uint32_t channel)
+{
+	en_clk_dst_t clk;
+
+	if (block_num == 0) {
+		/* block_num 0 is 32-bit tcpwm instances */
+		clk = (en_clk_dst_t)((uint32_t)_IFX_CAT1_TCPWM0_PCLK_CLOCK0 + channel);
+	} else {
+		/* block_num 1 is 16-bit tcwpm instances */
+		clk = (en_clk_dst_t)((uint32_t)_IFX_CAT1_TCPWM1_PCLK_CLOCK0 + channel);
+	}
+
+	return clk;
+}
+
 static int ifx_cat1_peri_clock_init(const struct device *dev)
 {
 	struct ifx_cat1_peri_clock_data *const data = dev->data;
 
 	if (data->hw_resource.type == IFX_RSC_SCB) {
 		en_clk_dst_t clk_idx = ifx_cat1_scb_get_clock_index(data->hw_resource.block_num);
+
+		ifx_cat1_utils_peri_pclk_set_divider(clk_idx, &data->clock, data->divider - 1);
+		ifx_cat1_utils_peri_pclk_assign_divider(clk_idx, &data->clock);
+		ifx_cat1_utils_peri_pclk_enable_divider(clk_idx, &data->clock);
+	} else if (data->hw_resource.type == IFX_RSC_TCPWM) {
+		en_clk_dst_t clk_idx = ifx_cat1_tcpwm_get_clock_index(
+			data->hw_resource.block_num, data->hw_resource.channel_num);
 
 		ifx_cat1_utils_peri_pclk_set_divider(clk_idx, &data->clock, data->divider - 1);
 		ifx_cat1_utils_peri_pclk_assign_divider(clk_idx, &data->clock);
@@ -106,10 +128,11 @@ static int ifx_cat1_peri_clock_init(const struct device *dev)
 
 #define INFINEON_CAT1_PERI_CLOCK_INIT(n)                                                           \
 	static struct ifx_cat1_peri_clock_data ifx_cat1_peri_clock##n##_data = {                   \
-		PERI_CLOCK_INIT(n).divider = DT_INST_PROP(n, clock_div),                           \
+		.divider = DT_INST_PROP(n, clock_div),                                             \
 		.hw_resource = {.type = DT_INST_PROP(n, resource_type),                            \
-				.block_num = DT_INST_PROP(n, resource_instance)},                  \
-	};                                                                                         \
+				.block_num = DT_INST_PROP(n, resource_instance),                   \
+				.channel_num = DT_INST_PROP_OR(n, resource_channel, 0)},           \
+		PERI_CLOCK_INIT(n)};                                                               \
                                                                                                    \
 	DEVICE_DT_INST_DEFINE(n, &ifx_cat1_peri_clock_init, NULL, &ifx_cat1_peri_clock##n##_data,  \
 			      NULL, PRE_KERNEL_1, CONFIG_CLOCK_CONTROL_INIT_PRIORITY, NULL);
