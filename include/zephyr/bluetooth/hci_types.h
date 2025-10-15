32e5858b3a51822b32387276923f214de686b8b4
Kai Cheng <chengkai@xiaomi.com>
2025-10-15 22:11:27 +0800

    Bluetooth: Classic: add inquiry scan parameters update
    Implement BR/EDR inquiry scan parameters update functionality with
    new API bt_br_inquiry_scan_update_param(). Adds support for setting
    inquiry scan interval, window and type via HCI commands. Includes
    default parameter set for standard inquiry scanning.
    
    Signed-off-by: Kai Cheng <chengkai@xiaomi.com>

--- include/zephyr/bluetooth/hci_types.h
+++ include/zephyr/bluetooth/hci_types.h
@@ -718,6 +718,12 @@ struct bt_hci_cp_write_page_scan_activity {
 	uint16_t window;
 } __packed;
 
+#define BT_HCI_OP_WRITE_INQUIRY_SCAN_ACTIVITY   BT_OP(BT_OGF_BASEBAND, 0x001e) /* 0x0c1e */
+struct bt_hci_cp_write_inquiry_scan_activity {
+	uint16_t interval;
+	uint16_t window;
+} __packed;
+
 #define BT_HCI_OP_READ_CLASS_OF_DEVICE          BT_OP(BT_OGF_BASEBAND, 0x0023) /* 0x0c23 */
 struct bt_hci_rp_read_class_of_device {
 	uint8_t  status;
@@ -857,6 +863,11 @@ struct bt_hci_iac_lap {
 struct bt_hci_cp_write_current_iac_lap {
 	uint8_t  num_current_iac;
 	struct bt_hci_iac_lap lap[0];
+} __packed;
+
+#define BT_HCI_OP_WRITE_INQUIRY_SCAN_TYPE       BT_OP(BT_OGF_BASEBAND, 0x0043) /* 0x0c43 */
+struct bt_hci_cp_write_inquiry_scan_type {
+	uint8_t type;
 } __packed;
 
 #define BT_HCI_OP_WRITE_INQUIRY_MODE            BT_OP(BT_OGF_BASEBAND, 0x0045) /* 0x0c45 */
