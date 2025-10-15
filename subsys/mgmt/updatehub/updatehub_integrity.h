b2dd3b4ba96d3d88a24d0634ab6ea1111a2d0fe4
Valerio Setti <vsetti@baylibre.com>
2025-10-15 13:07:18 +0200

    mgmt: updatehub: remove legacy Mbed TLS crypto support
    The long-term Zephyr's goal is rely only on PSA Crypto API for crypto
    support in Zephyr and at the same time Mbed TLS will remove this support
    from the next release.
    
    Therefore this commit removes usage of legacy crypto hash support from
    updatehub.
    
    Signed-off-by: Valerio Setti <vsetti@baylibre.com>

--- subsys/mgmt/updatehub/updatehub_integrity.h
+++ subsys/mgmt/updatehub/updatehub_integrity.h
@@ -7,11 +7,7 @@
 #ifndef __UPDATEHUB_INTEGRITY_H__
 #define __UPDATEHUB_INTEGRITY_H__
 
-#if defined(CONFIG_PSA_CRYPTO_CLIENT)
 #include <psa/crypto.h>
-#else
-#include <mbedtls/sha256.h>
-#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -20,11 +16,7 @@ extern "C" {
 #define SHA256_BIN_DIGEST_SIZE	(32)
 #define SHA256_HEX_DIGEST_SIZE	((SHA256_BIN_DIGEST_SIZE * 2) + 1)
 
-#if defined(CONFIG_PSA_CRYPTO_CLIENT)
 typedef psa_hash_operation_t updatehub_crypto_context_t;
-#else
-typedef mbedtls_sha256_context updatehub_crypto_context_t;
-#endif
 
 int updatehub_integrity_init(updatehub_crypto_context_t *ctx);
 int updatehub_integrity_update(updatehub_crypto_context_t *ctx,
