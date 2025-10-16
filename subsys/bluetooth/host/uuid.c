22cb183d288bfaeb6a891ad78563a8f5946f7bce
Kai Cheng <chengkai@xiaomi.com>
2025-10-16 22:30:31 +0800

    Blueototh: UUID: add string to uuid
    add string to uuid
    
    Signed-off-by: Kai Cheng <chengkai@xiaomi.com>

--- subsys/bluetooth/host/uuid.c
+++ subsys/bluetooth/host/uuid.c
@@ -9,6 +9,7 @@
 #include <errno.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 
 #include <zephyr/bluetooth/uuid.h>
@@ -128,5 +129,47 @@ void bt_uuid_to_str(const struct bt_uuid *uuid, char *str, size_t len)
 	default:
 		(void)memset(str, 0, len);
 		return;
+	}
+}
+
+bool bt_str_to_uuid(const char *str, struct bt_uuid *uuid)
+{
+	switch (strlen(str)) {
+	case BT_UUID_SIZE_16 * 2: {
+		uint16_t *p = &BT_UUID_16(uuid)->val;
+
+		if (sscanf(str, "%04hx", p) != 1) {
+			return false;
+		}
+
+		uuid->type = BT_UUID_TYPE_16;
+		return true;
+	}
+	case BT_UUID_SIZE_32 * 2: {
+		uint32_t *p = &BT_UUID_32(uuid)->val;
+
+		if (sscanf(str, "%08x", p) != 1) {
+			return false;
+		}
+
+		uuid->type = BT_UUID_TYPE_32;
+		return true;
+	}
+	case BT_UUID_STR_LEN - 1: {
+		uint8_t *p = BT_UUID_128(uuid)->val;
+
+		if (sscanf(str,
+			   "%02hhx%02hhx%02hhx%02hhx-%02hhx%02hhx-%02hhx%02hhx"
+			   "-%02hhx%02hhx-%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx",
+			   &p[0], &p[1], &p[2], &p[3], &p[4], &p[5], &p[6], &p[7], &p[8], &p[9],
+			   &p[10], &p[11], &p[12], &p[13], &p[14], &p[15]) != BT_UUID_SIZE_128) {
+			return false;
+		}
+
+		uuid->type = BT_UUID_TYPE_128;
+		return true;
+	}
+	default:
+		return false;
 	}
 }
