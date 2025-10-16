6e3f36e7c9715937b243bc1f01bed85d7fe76f4f
Arnout Vandecappelle <arnout@rnout.be>
2025-10-16 15:32:40 +0200

    drivers: adc: shell: add support for ad4130
    The adc shell commands have a separate command for every different ADC
    type. AD4130 was missing from that list.
    
    Signed-off-by: Arnout Vandecappelle <arnout@rnout.be>

--- drivers/adc/adc_shell.c
+++ drivers/adc/adc_shell.c
@@ -76,6 +76,7 @@ static struct adc_hdl {
 } adc_list[] = {
 	/* zephyr-keep-sorted-start */
 	DT_FOREACH_STATUS_OKAY(adi_ad4114_adc, ADC_HDL_LIST_ENTRY)
+	DT_FOREACH_STATUS_OKAY(adi_ad4130_adc, ADC_HDL_LIST_ENTRY)
 	DT_FOREACH_STATUS_OKAY(adi_ad559x_adc, ADC_HDL_LIST_ENTRY)
 	DT_FOREACH_STATUS_OKAY(atmel_sam0_adc, ADC_HDL_LIST_ENTRY)
 	DT_FOREACH_STATUS_OKAY(atmel_sam_adc, ADC_HDL_LIST_ENTRY)
