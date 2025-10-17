4efa0db944fcd74d2d636a0936275fa1457ce1b5
Łukasz Kędziora <lkedziora@antmicro.com>
2025-10-17 10:00:43 +0200

    drivers: clock_control: Add Synaptics clock control driver
    This adds the clock control driver for Synaptics SR100-series SoCs.
    
    Signed-off-by: Łukasz Kędziora <lkedziora@antmicro.com>
    Signed-off-by: Piotr Zierhoffer <pzierhoffer@antmicro.com>
    Signed-off-by: Andreas Weissel <andreas.weissel@synaptics.com>

---
+++ include/zephyr/drivers/clock_control/syna_sr100_clock_control.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2025 Synaptics, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_SYNA_CLOCK_CONTROL_H_
+#define ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_SYNA_CLOCK_CONTROL_H_
+
+#include <zephyr/drivers/clock_control.h>
+#include <zephyr/dt-bindings/clock/syna_sr100_clock.h>
+
+struct clock_control_syna_config {
+	mem_addr_t regs;
+};
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_SYNA_CLOCK_CONTROL_H_ */
