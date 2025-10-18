e544d696bcc7752d5c9b9fa76c64a6e81cc7e3dd
Johnny Chuang <johnny.chuang@emc.com.tw>
2025-10-18 17:15:39 +0800

    soc: add Elan em32f967 minimal SoC support
    Add initial support for the Elan em32f967 SoC based on ARM Cortex-M4.
    Minimal SoC definition for upstream:
    - soc.c contains only basic skeleton initialization for build
    - soc.h, Kconfig, and CMakeLists.txt provided for completeness
    - This PR does not implement drivers or UART functionality
    
    Signed-off-by: Johnny Chuang <johnny.chuang@emc.com.tw>

---
+++ soc/elan/em32f967/soc.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2025 Elan Microelectronics Corp.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "soc.h"
+#include <zephyr/kernel.h>
+#include <zephyr/init.h>
+
+/* Minimal SoC init for reviewer and build */
+static int soc_elan_em32f967_init(void)
+{
+	return 0;
+}
+
+SYS_INIT(soc_elan_em32f967_init, PRE_KERNEL_1, 0);
