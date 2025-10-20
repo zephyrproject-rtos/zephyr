4b7b55efc0386bf1021ae3a55a7043be2b5fe1e7
Immo Birnbaum <mail@birnbaum.immo>
2025-10-20 16:41:21 +0200

    boards: nxp: mimxrt1180_evk: adjust MPU header include
    Update include of header file arm_mpu_mem_cfg.h which has been moved
    to a Cortex-M/-R-agnostic include directory.
    
    Signed-off-by: Immo Birnbaum <mail@birnbaum.immo>

--- boards/nxp/mimxrt1180_evk/cm7/mpu_regions.c
+++ boards/nxp/mimxrt1180_evk/cm7/mpu_regions.c
@@ -5,7 +5,7 @@
  */
 
 #include <zephyr/devicetree.h>
-#include <zephyr/arch/arm/cortex_m/arm_mpu_mem_cfg.h>
+#include <zephyr/arch/arm/mpu/arm_mpu_mem_cfg.h>
 
 #define MEMORY_REGION_SIZE_KB(SIZE)    (SIZE / 1024)
 
