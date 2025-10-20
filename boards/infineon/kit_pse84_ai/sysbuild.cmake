ce76eb0cf937870d1063e2c88a46cdf2679441c6
John Batch <john.batch@infineon.com>
2025-10-20 15:21:43 -0700

    boards: infineon: Add kit_pse84_ai board support
    Adds basic support for the Infineon kit_pse84_ai board, including:
     * samples/hello_world
     * samples/basic/blinky
     * tests/driver/uart/uart_api
    
    Signed-off-by: John Batch <john.batch@infineon.com>

---
+++ boards/infineon/kit_pse84_ai/sysbuild.cmake
@@ -0,0 +1,14 @@
+# Copyright (c) 2025 Infineon Technologies AG,
+# or an affiliate of Infineon Technologies AG.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+if(SB_CONFIG_BOARD_KIT_PSE84_AI_PSE846GPS2DBZC4A_M55)
+  ExternalZephyrProject_Add(
+    APPLICATION enable_cm55
+    SOURCE_DIR ${ZEPHYR_BASE}/samples/basic/minimal
+    BOARD kit_pse84_ai/pse846gps2dbzc4a/m33
+  )
+
+  set_config_bool(enable_cm55 CONFIG_SOC_PSE84_M55_ENABLE 1)
+endif()
