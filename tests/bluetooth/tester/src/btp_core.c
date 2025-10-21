21f03017251327d3e4f17b77818c6efd207b3e9d
Cheng Chang <cheng.chang@nxp.com>
2025-10-21 17:42:53 +0800

    test: Bluetooth: Tester: Add support for A2DP
    This adds support for BTP commands and events required for running
    A2DP tests.
    
    The main changes are as follows:
    1. Add source code for a2dp tests in btp_a2dp.h and btp_a2dp.c.
    2. Add a2dp to the BTP system.
    
    Signed-off-by: Cheng Chang <cheng.chang@nxp.com>

--- tests/bluetooth/tester/src/btp_core.c
+++ tests/bluetooth/tester/src/btp_core.c
@@ -144,6 +144,9 @@ static uint8_t supported_services(const void *cmd, uint16_t cmd_len,
 #if defined(CONFIG_BT_CLASSIC)
 	tester_set_bit(rp->data, BTP_SERVICE_ID_SDP);
 #endif /* CONFIG_BT_CLASSIC */
+#if defined(CONFIG_BT_A2DP)
+	tester_set_bit(rp->data, BTP_SERVICE_ID_A2DP);
+#endif /* CONFIG_BT_A2DP */
 
 	*rsp_len = sizeof(*rp) + 4U;
 
@@ -300,6 +303,11 @@ static uint8_t register_service(const void *cmd, uint16_t cmd_len,
 		status = tester_init_sdp();
 		break;
 #endif /* CONFIG_BT_CLASSIC */
+#if defined(CONFIG_BT_A2DP)
+	case BTP_SERVICE_ID_A2DP:
+		status = tester_init_a2dp();
+		break;
+#endif /*CONFIG_BT_A2DP */
 	default:
 		LOG_WRN("unknown id: 0x%02x", cp->id);
 		status = BTP_STATUS_FAILED;
@@ -460,6 +468,11 @@ static uint8_t unregister_service(const void *cmd, uint16_t cmd_len,
 		status = tester_unregister_sdp();
 		break;
 #endif /* CONFIG_BT_CLASSIC */
+#if defined(CONFIG_BT_A2DP)
+	case BTP_SERVICE_ID_A2DP:
+		status = tester_unregister_a2dp();
+		break;
+#endif /* CONFIG_BT_A2DP */
 	default:
 		LOG_WRN("unknown id: 0x%x", cp->id);
 		status = BTP_STATUS_FAILED;
