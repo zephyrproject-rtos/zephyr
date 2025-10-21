4b4332ac1814ee675fdf25a32c313002d70918d4
Sylvio Alves <sylvio.alves@espressif.com>
2025-10-21 10:33:07 -0300

    drivers: crypto: return -ENOTSUP when needed
    Make sure all crypto driver returns proper error
    when feature is not supported.
    
    Signed-off-by: Sylvio Alves <sylvio.alves@espressif.com>

--- drivers/crypto/crypto_stm32.c
+++ drivers/crypto/crypto_stm32.c
@@ -335,12 +335,12 @@ static int crypto_stm32_session_setup(const struct device *dev,
 
 	if (ctx->flags & ~(CRYP_SUPPORT)) {
 		LOG_ERR("Unsupported flag");
-		return -EINVAL;
+		return -ENOTSUP;
 	}
 
 	if (algo != CRYPTO_CIPHER_ALGO_AES) {
 		LOG_ERR("Unsupported algo");
-		return -EINVAL;
+		return -ENOTSUP;
 	}
 
 	/* The CRYP peripheral supports the AES ECB, CBC, CTR, CCM and GCM
@@ -356,7 +356,7 @@ static int crypto_stm32_session_setup(const struct device *dev,
 	    (mode != CRYPTO_CIPHER_MODE_CBC) &&
 	    (mode != CRYPTO_CIPHER_MODE_CTR)) {
 		LOG_ERR("Unsupported mode");
-		return -EINVAL;
+		return -ENOTSUP;
 	}
 
 	/* The STM32F4 CRYP peripheral supports key sizes of 128, 192 and 256
@@ -368,7 +368,7 @@ static int crypto_stm32_session_setup(const struct device *dev,
 #endif
 	    (ctx->keylen != 32U)) {
 		LOG_ERR("%u key size is not supported", ctx->keylen);
-		return -EINVAL;
+		return -ENOTSUP;
 	}
 
 	ctx_idx = crypto_stm32_get_unused_session_index(dev);
