ed7db4b8a2ad7304d2b04cb71c3007fabccd4574
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2025-10-22 10:06:36 +0800

    drivers: mcux_syscon: enable MICFIL clock control
    Enable MICFIL clock control for NXP MCUX SoCs.
    
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

--- drivers/clock_control/clock_control_mcux_syscon.c
+++ drivers/clock_control/clock_control_mcux_syscon.c
@@ -185,6 +185,10 @@ static int mcux_lpc_syscon_clock_control_on(const struct device *dev,
 #endif
 #endif
 
+#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(micfil))
+	CLOCK_EnableClock(kCLOCK_Micfil);
+#endif
+
 	return 0;
 }
 
@@ -620,6 +624,12 @@ static int mcux_lpc_syscon_clock_control_get_subsys_rate(const struct device *de
 		*rate = CLOCK_GetLpspiClkFreq(1);
 		break;
 #endif /* defined(CONFIG_SPI_NXP_LPSPI) */
+
+#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(micfil))
+	case MCUX_MICFIL_CLK:
+		*rate = CLOCK_GetMicfilClkFreq();
+		break;
+#endif
 	}
 
 	return 0;
