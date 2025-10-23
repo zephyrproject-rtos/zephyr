219dc0d21359c483196e894881f246d6b86f6f56
Jakub Klimczak <jklimczak@internships.antmicro.com>
2025-10-23 17:31:00 +0200

    tests: c_lib: Rework stdio test to include new functions
    Test file operations through the new common libc functions in the
    stdio test suite.
    
    Signed-off-by: Jakub Klimczak <jklimczak@internships.antmicro.com>

--- tests/lib/c_lib/stdio/src/main.c
+++ tests/lib/c_lib/stdio/src/main.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2024 Linumiz
+ * Copyright (c) 2026 Antmicro <www.antmicro.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -13,10 +14,15 @@
 #include <ff.h>
 #include <zephyr/fs/fs.h>
 
-#define FATFS_MNTP	"/RAM:"
-#define TEST_FILE	FATFS_MNTP"/testfile.txt"
+#define FATFS_MNTP "/RAM:"
+#define TEST_FILE  FATFS_MNTP "/testfile.txt"
 
 static FATFS fat_fs;
+static FILE *file;
+
+static char testchar = 'a';
+static const char teststr[5] = "bcdef"; /* no null-termination */
+#define FINAL_CONTENT_LENGTH (sizeof(testchar) + sizeof(teststr))
 
 static struct fs_mount_t fatfs_mnt = {
 	.type = FS_FATFS,
@@ -24,25 +30,69 @@ static struct fs_mount_t fatfs_mnt = {
 	.fs_data = &fat_fs,
 };
 
-ZTEST_SUITE(libc_stdio, NULL, NULL, NULL, NULL, NULL);
+void create_file(void *fixture)
+{
+	ARG_UNUSED(fixture);
+	zassert_ok(fs_mount(&fatfs_mnt), "Error mounting file system\n");
+	file = fopen(TEST_FILE, "w+x");
+	zassert_not_null(file, "Error creating file: %d\n", errno);
+}
 
-/**
- * @brief Test for remove API
- *
- * @details Test deletes a file through remove API.
- */
+void close_file(void *fixture)
+{
+	ARG_UNUSED(fixture);
+	zassert_ok(fclose(file), "Error closing file\n");
+	zassert_ok(remove(TEST_FILE), "Error removing file: %d\n", errno);
+	zassert_ok(fs_unmount(&fatfs_mnt), "Error unmounting file system\n");
+}
+
+ZTEST_SUITE(libc_stdio, NULL, NULL, create_file, close_file, NULL);
+
+ZTEST(libc_stdio, test_fileobj_fputc)
+{
+	zassert_equal(testchar, fputc(testchar, file), "Error writing single character: %d\n",
+		      errno);
+}
+
+ZTEST(libc_stdio, test_fileobj_fwrite)
+{
+	zassert_equal(sizeof(teststr),
+		      fwrite(&teststr, sizeof(teststr[0]), ARRAY_SIZE(teststr), file),
+		      "Error writing to file: %d\n", errno);
+}
+
+ZTEST(libc_stdio, test_fileobj_ftell)
+{
+	zassert_equal(0, ftell(file));
+	zassert_equal(sizeof(teststr),
+		      fwrite(&teststr, sizeof(teststr[0]), ARRAY_SIZE(teststr), file),
+		      "Error writing to file: %d\n", errno);
+	zassert_equal(ARRAY_SIZE(teststr), ftell(file));
+}
+
+ZTEST(libc_stdio, test_fileobj_fseek)
+{
+	zassert_ok(fseek(file, 0, SEEK_SET), "Error seeking: %d\n", errno);
+}
+
+ZTEST(libc_stdio, test_fileobj_fread)
+{
+	zassert_equal(testchar, fputc(testchar, file), "Error writing single character: %d\n",
+		      errno);
+	zassert_equal(sizeof(teststr),
+		      fwrite(&teststr, sizeof(teststr[0]), ARRAY_SIZE(teststr), file),
+		      "Error writing to file: %d\n", errno);
+	zassert_ok(fseek(file, 0, SEEK_SET), "Error seeking: %d\n", errno);
+	char rdbuf[FINAL_CONTENT_LENGTH] = {0};
+
+	zassert_equal(sizeof(rdbuf), fread(&rdbuf, sizeof(rdbuf[0]), ARRAY_SIZE(rdbuf), file),
+		      "Error reading from file: %d\n", errno);
+	zassert_equal(rdbuf[0], testchar, "Read incorrect");
+	zassert_mem_equal(&rdbuf[1], &teststr, sizeof(teststr), "Read incorrect");
+}
+
 ZTEST(libc_stdio, test_remove)
 {
-	struct fs_file_t file;
-
-	fs_file_t_init(&file);
-
-	zassert_ok(fs_mount(&fatfs_mnt), "Error in mount file system\n");
-	zassert_equal(fs_open(&file, TEST_FILE, FS_O_CREATE), 0,
-		      "Error creating file\n");
-	zassert_ok(fs_close(&file), "Error closing file\n");
-	zassert_ok(remove(TEST_FILE), "Error removing file: %d\n", errno);
 	zassert_equal(remove(""), -1, "Error Invalid path removed\n");
 	zassert_equal(remove(NULL), -1, "Error Invalid path removed\n");
-	zassert_ok(fs_unmount(&fatfs_mnt), "Error while unmount file system\n");
 }
