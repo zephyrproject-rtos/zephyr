abf9525559a388d89a6880784455d6826aa35383
Abderrahmane JARMOUNI <git@jarmouni.me>
2025-10-23 21:28:09 +0200

    boards: stm32h750b-dk: add ST-Link GDB Server runner
    Add support for the ST-Link GDB Server runner to be able to debug
    STM32H750B-DK apps, including those running from external flash.
    Update the board's documentation accordingly.
    
    Signed-off-by: Abderrahmane JARMOUNI <git@jarmouni.me>

--- boards/st/stm32h750b_dk/board.cmake
+++ boards/st/stm32h750b_dk/board.cmake
@@ -7,10 +7,13 @@ if(CONFIG_STM32_MEMMAP OR CONFIG_BOOTLOADER_MCUBOOT)
   board_runner_args(stm32cubeprogrammer "--extload=MT25TL01G_STM32H750B-DISCO.stldr")
 endif()
 
+board_runner_args(stlink_gdbserver "--extload=MT25TL01G_STM32H750B-DISCO.stldr")
+
 board_runner_args(jlink "--device=STM32H735IG" "--speed=4000")
 board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
 
 # keep first
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stlink_gdbserver.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd-stm32.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
