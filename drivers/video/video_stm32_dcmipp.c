683a760afcb5d982dc1f785f91b73277be746d40
Alain Volmat <alain.volmat@foss.st.com>
2025-10-24 14:02:58 +0200

    video: dcmipp: put Pipe1 / pipe2 caps within preproc if statement
    Correct compilation error due to usage of DCMIPP_PIPE1 / DCMIPP_PIPE2
    on platform which do not have pixel pipes by putting those caps
    under #if defined(STM32_DCMIPP_HAS_PIXEL_PIPES)
    Put also DCMIPP_VIDEO_FORMAT_CAP macro inside since this is only used
    for AUX / MAIN pipes.
    
    Fixes: 126aaf6b727 ("video: dcmipp: expose dcmipp caps for all 3 pipes.")
    
    Signed-off-by: Alain Volmat <alain.volmat@foss.st.com>

--- drivers/video/video_stm32_dcmipp.c
+++ drivers/video/video_stm32_dcmipp.c
@@ -1340,18 +1340,6 @@ static int stm32_dcmipp_dequeue(const struct device *dev, struct video_buffer **
 #define DCMIPP_CEIL_DIV(val, div)								\
 		(((val) + (div) - 1) / (div))
 
-#define DCMIPP_VIDEO_FORMAT_CAP(format, pixmul)	{						\
-	.pixelformat = VIDEO_PIX_FMT_##format,							\
-	.width_min = DCMIPP_CEIL_DIV_ROUND_UP_MUL(CONFIG_VIDEO_STM32_DCMIPP_SENSOR_WIDTH,	\
-						  STM32_DCMIPP_MAX_PIPE_SCALE_FACTOR,		\
-						  pixmul),					\
-	.width_max = CONFIG_VIDEO_STM32_DCMIPP_SENSOR_WIDTH / (pixmul) * (pixmul),		\
-	.height_min = DCMIPP_CEIL_DIV(CONFIG_VIDEO_STM32_DCMIPP_SENSOR_HEIGHT,			\
-				      STM32_DCMIPP_MAX_PIPE_SCALE_FACTOR),			\
-	.height_max = CONFIG_VIDEO_STM32_DCMIPP_SENSOR_HEIGHT,					\
-	.width_step = pixmul, .height_step = 1,							\
-}
-
 static const struct video_format_cap stm32_dcmipp_dump_fmt[] = {
 	{
 		.pixelformat = VIDEO_FOURCC_FROM_STR(CONFIG_VIDEO_STM32_DCMIPP_SENSOR_PIXEL_FORMAT),
@@ -1363,6 +1351,19 @@ static const struct video_format_cap stm32_dcmipp_dump_fmt[] = {
 	},
 	{0},
 };
+
+#if defined(STM32_DCMIPP_HAS_PIXEL_PIPES)
+#define DCMIPP_VIDEO_FORMAT_CAP(format, pixmul)	{						\
+	.pixelformat = VIDEO_PIX_FMT_##format,							\
+	.width_min = DCMIPP_CEIL_DIV_ROUND_UP_MUL(CONFIG_VIDEO_STM32_DCMIPP_SENSOR_WIDTH,	\
+						  STM32_DCMIPP_MAX_PIPE_SCALE_FACTOR,		\
+						  pixmul),					\
+	.width_max = CONFIG_VIDEO_STM32_DCMIPP_SENSOR_WIDTH / (pixmul) * (pixmul),		\
+	.height_min = DCMIPP_CEIL_DIV(CONFIG_VIDEO_STM32_DCMIPP_SENSOR_HEIGHT,			\
+				      STM32_DCMIPP_MAX_PIPE_SCALE_FACTOR),			\
+	.height_max = CONFIG_VIDEO_STM32_DCMIPP_SENSOR_HEIGHT,					\
+	.width_step = pixmul, .height_step = 1,							\
+}
 
 static const struct video_format_cap stm32_dcmipp_main_fmts[] = {
 	DCMIPP_VIDEO_FORMAT_CAP(RGB565, 8),
@@ -1397,6 +1398,7 @@ static const struct video_format_cap stm32_dcmipp_aux_fmts[] = {
 	DCMIPP_VIDEO_FORMAT_CAP(BGRA32, 4),
 	{0},
 };
+#endif
 
 static int stm32_dcmipp_get_caps(const struct device *dev, struct video_caps *caps)
 {
@@ -1406,12 +1408,14 @@ static int stm32_dcmipp_get_caps(const struct device *dev, struct video_caps *ca
 	case DCMIPP_PIPE0:
 		caps->format_caps = stm32_dcmipp_dump_fmt;
 		break;
+#if defined(STM32_DCMIPP_HAS_PIXEL_PIPES)
 	case DCMIPP_PIPE1:
 		caps->format_caps = stm32_dcmipp_main_fmts;
 		break;
 	case DCMIPP_PIPE2:
 		caps->format_caps = stm32_dcmipp_aux_fmts;
 		break;
+#endif
 	default:
 		CODE_UNREACHABLE;
 	}
