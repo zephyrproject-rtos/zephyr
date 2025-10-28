462990f59ad6733ef8a8928c26044d1a1cc5bd6e
Phi Bang Nguyen <phibang.nguyen@nxp.com>
2025-10-28 13:05:20 +0100

    lib: Introduce libMP
    LibMP (Media Pipe library) is a lightweight gstreamer-like multimedia
    framework. LibMP reuses many concepts from GStreamer, such as elements,
    pads, caps negotiation, and buffer negotiation and adopts a pipeline-
    based architecture that decomposes multimedia processing into discrete,
    interconnected elements.
    
    It aims to simplify the development of multimedia applications by
    providing simple and stable APIs for users to rapidly create their
    specific applications, i.e., users simply select the built-in elements
    and plugins suited to their purpose to construct a pipeline, and it just
    works. This design promotes modularity, reusability, and efficient
    resource management (e.g., zero-copy data flow). Moreover, the APIs are
    generic enough so that application code can remain unchanged even as
    libMP evolves.
    
    LibMP also aims to facilitate the life of developers as it features a
    a highly modular, inheritance-based architecture inspired by GStreamer,
    ensuring modularity, scalability, and maintainability. For example, new
    custom elements can be easily added by extending existing elements,
    without requiring modifications to the core components.
    
    Signed-off-by: Phi Bang Nguyen <phibang.nguyen@nxp.com>
    Signed-off-by: Trung Hieu Le <trunghieu.le@nxp.com>

---
+++ lib/libmp/mp.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * mp.h: Main header for MediaPipe
+ *
+ * Applications should include this and only this.
+ *
+ */
+
+#ifndef __MP_H__
+#define __MP_H__
+
+#include <src/core/mp_bus.h>
+#if CONFIG_MP_CAPSFILTER
+#include <src/core/mp_capsfilter.h>
+#endif
+#include <src/core/mp_caps.h>
+#include <src/core/mp_element.h>
+#include <src/core/mp_element_factory.h>
+#include <src/core/mp_pipeline.h>
+#include <src/core/mp_pixel_format.h>
+
+#endif /* __MP_H__ */
