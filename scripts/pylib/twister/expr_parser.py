5be6aa13b98f6e9dde63196ca5e83189f480b180
Abderrahmane JARMOUNI <git@jarmouni.me>
2025-10-28 15:30:51 +0100

    scripts: pylib: twister: expr_parser: some enhancements
    Rename dt_node_prop_enabled filtering expression to
    dt_nodelabel_prop_enabled as it takes a node label as a 1st argument.
    
    Enhance dt_node_has_prop inline doc and param name.
    
    Signed-off-by: Abderrahmane JARMOUNI <git@jarmouni.me>

--- scripts/pylib/twister/expr_parser.py
+++ scripts/pylib/twister/expr_parser.py
@@ -273,7 +273,7 @@ def ast_expr(ast, env, edt):
         if node and node.status == "okay":
             return True
         return False
-    elif ast[0] == "dt_node_prop_enabled":
+    elif ast[0] == "dt_nodelabel_prop_enabled":
         label = ast[1][0]
         node = edt.label2node.get(label)
         prop = ast[1][1]
@@ -281,10 +281,11 @@ def ast_expr(ast, env, edt):
             return True
         return False
     elif ast[0] == "dt_node_has_prop":
-        # 1st arg 'label' must be a valid node alias or a node path
-        label = ast[1][0]
+        # 1st arg 'node_id' must be a valid node alias (defined in aliases node)
+        # or a node path
+        node_id = ast[1][0]
         try:
-            node = edt.get_node(label)
+            node = edt.get_node(node_id)
         except Exception:
             return False
         prop = ast[1][1]
