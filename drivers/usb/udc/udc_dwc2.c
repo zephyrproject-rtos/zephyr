0d515e777fb082ad144625c2fb9d9001157ba7ff
Johann Fischer <johann.fischer@nordicsemi.no>
2025-10-28 08:51:10 +0100

    drivers: udc_dwc2: avoid register read on disabled controller
    The VBUS bounces when the USB connector is plugged in. This can lead to
    events VBUS removed and Suspended occurring in that order. With
    hibernation support enabled, hibernation request, as result of the
    suspend interrupt, will be delegated to the driver thread. Once the
    driver thread is scheduled to process hibernation request, the
    controller may be already disabled and controller/phy clocks be off. On
    nRF54LM20 this leads to CPU crash and a hang.
    
    To avoid this happening, cancel the possible hibernation request after
    interrupts are disabled. Although the thread could be still waked up
    because of USBSUSP interrupt raised and event posted, this is considered
    to be harmless, as there would be a second check in the thread whether
    the event is still valid or is cleared in between.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- drivers/usb/udc/udc_dwc2.c
+++ drivers/usb/udc/udc_dwc2.c
@@ -2361,6 +2361,7 @@ static int udc_dwc2_disable(const struct device *dev)
 	}
 
 	config->irq_disable_func(dev);
+	cancel_hibernation_request(priv);
 
 	if (priv->hibernated) {
 		dwc2_exit_hibernation(dev, false, true);
