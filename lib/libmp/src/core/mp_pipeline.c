d21414b2dc411624989fca6199a4795c0aab1285
Phi Bang Nguyen <phibang.nguyen@nxp.com>
2025-10-28 13:05:20 +0100

    lib: Introduce libMP
    LibMP (Media Pipe library) is a lightweight gstreamer-like multimedia
    framework. LibMP reuses many concepts from GStreamer, such as elements,
    pads, caps negotiation, and buffer negotiation and adopts a pipeline-
    based architecture that decomposes multimedia processing into discrete,
    interconnected elements.
    
    It aims to simplify the development of multimedia applications by
    providing simple and stable APIs for users to rapidly create their
    specific applications, i.e., users simply select the built-in elements
    and plugins suited to their purpose to construct a pipeline, and it just
    works. This design promotes modularity, reusability, and efficient
    resource management (e.g., zero-copy data flow). Moreover, the APIs are
    generic enough so that application code can remain unchanged even as
    libMP evolves.
    
    LibMP also aims to facilitate the life of developers as it features a
    a highly modular, inheritance-based architecture inspired by GStreamer,
    ensuring modularity, scalability, and maintainability. For example, new
    custom elements can be easily added by extending existing elements,
    without requiring modifications to the core components.
    
    Signed-off-by: Phi Bang Nguyen <phibang.nguyen@nxp.com>
    Signed-off-by: Trung Hieu Le <trunghieu.le@nxp.com>

---
+++ lib/libmp/src/core/mp_pipeline.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+
+#include <zephyr/logging/log.h>
+
+#include "mp_bus.h"
+#include "mp_element.h"
+#include "mp_element_factory.h"
+#include "mp_pipeline.h"
+
+LOG_MODULE_REGISTER(mp_pipeline, CONFIG_LIBMP_LOG_LEVEL);
+
+static int mp_pipeline_set_property(struct mp_object *obj, uint32_t id, const void *val)
+{
+	return 0;
+}
+
+static int mp_pipeline_get_property(struct mp_object *obj, uint32_t id, void *val)
+{
+	return 0;
+}
+
+static enum mp_state_change_return mp_pipeline_change_state(struct mp_element *element,
+							    enum mp_state_change transition)
+{
+	return mp_bin_change_state_func(element, transition);
+}
+
+void mp_pipeline_init(struct mp_element *self)
+{
+	/* Init base class */
+	mp_bin_init(self);
+
+	/* Init bus */
+	mp_bus_init(&MP_PIPELINE(self)->bus);
+
+	self->object.set_property = mp_pipeline_set_property;
+	self->object.get_property = mp_pipeline_get_property;
+	self->change_state = mp_pipeline_change_state;
+
+	self->bus = &MP_PIPELINE(self)->bus;
+}
+
+struct mp_element *mp_pipeline_new(const char *name)
+{
+	return mp_element_factory_create("pipeline", name);
+}
