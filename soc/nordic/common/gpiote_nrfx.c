c7ac114b3992dc3b675074ff3aaf736ebbf868ee
Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>
2025-10-29 19:16:54 +0100

    soc: nordic: common: add nrfx gpiote instantiation component
    GPIOTE driver instances are no longer defined within nrfx.
    Add a component supplementing missing functionality,
    as GPIOTE driver instances are often shared across the system.
    
    Signed-off-by: Nikodem Kastelik <nikodem.kastelik@nordicsemi.no>

---
+++ soc/nordic/common/gpiote_nrfx.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <nrfx_gpiote.h>
+#include "gpiote_nrfx.h"
+
+#define GPIOTE_NRFX_INST_DEF(instname, reg) \
+	nrfx_gpiote_t instname = NRFX_GPIOTE_INSTANCE(reg);
+#define GPIOTE_NRFX_INST_DEFINE(node_id) \
+	GPIOTE_NRFX_INST_DEF(GPIOTE_NRFX_INST_BY_NODE(node_id), DT_REG_ADDR(node_id))
+
+DT_FOREACH_STATUS_OKAY(nordic_nrf_gpiote, GPIOTE_NRFX_INST_DEFINE)
