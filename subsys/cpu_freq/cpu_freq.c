dc5f15e6731fca9cd60b88c3985859c0ced27bbc
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2025-10-29 12:29:14 +0800

    cpu_freq: remove redundant XOR calculation in k_ipi_work_add
    In the current code, 'target_cpus ^= (1U << _current_cpu->id)'
    is first used to remove the current core. Then, k_ipi_work_add
    performs 'target_cpus ^ (1U << _current_cpu->id)' again when
    passing parameters. This will add the current core to the mask
    again, causing the current core to receive IPI and directly call
    cpu_freq_next_pstate() at the end, which may lead to duplicate
    execution.
    
    This commit changed 'target_cpus ^ (1U << _current_cpu->id)' to
    'target_cpus' in k_ipi_work_add to avoid the second XOR.
    
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

--- subsys/cpu_freq/cpu_freq.c
+++ subsys/cpu_freq/cpu_freq.c
@@ -70,11 +70,9 @@ static void cpu_freq_timer_handler(struct k_timer *timer)
 	 */
 
 	target_cpus = (num_cpus == 32U) ? 0xFFFFFFFF : (1U << num_cpus) - 1U;
-	target_cpus ^= (1U << _current_cpu->id),
+	target_cpus ^= (1U << _current_cpu->id);
 
-	ret = k_ipi_work_add(&cpu_freq_work,
-			     target_cpus ^ (1U << _current_cpu->id),
-			     cpu_freq_ipi_handler);
+	ret = k_ipi_work_add(&cpu_freq_work, target_cpus, cpu_freq_ipi_handler);
 
 	if (ret != 0) {
 		/*
