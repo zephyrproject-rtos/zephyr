3567b4ebd1b582d497c66db210d7fcc144afd859
Vinit Mehta <vinit.mehta@nxp.com>
2025-10-29 11:23:38 +0530

    [nxp fromlist] Bluetooth: Audio: Shell: Add SBC encoded audio
    Add code to use shell application to test A2DP
    profile with option to use SBC encoded audio. Add
    sine tone audio file which will be used to render
    to encoded audio A2DP sink device. There is an
    kconfig option to enable/disable encoded audio
    support
    
    Upstream PR Link:
    https://github.com/zephyrproject-rtos/zephyr/pull/98440/
    
    Signed-off-by: Vinit Mehta <vinit.mehta@nxp.com>

--- subsys/bluetooth/host/classic/shell/a2dp.c
+++ subsys/bluetooth/host/classic/shell/a2dp.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 NXP
+ * Copyright 2024-2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -19,6 +19,10 @@
 #include <zephyr/bluetooth/conn.h>
 #include <zephyr/bluetooth/l2cap.h>
 #include <zephyr/bluetooth/classic/a2dp_codec_sbc.h>
+#if defined(CONFIG_BT_A2DP_SOURCE_SBC_AUDIO_SHELL)
+#include <zephyr/bluetooth/sbc.h>
+#include "sine.h"
+#endif
 #include <zephyr/bluetooth/classic/a2dp.h>
 #include <zephyr/bluetooth/classic/sdp.h>
 
@@ -42,7 +46,22 @@ static struct bt_a2dp_ep *registered_sbc_endpoint;
 static struct bt_a2dp_stream sbc_stream;
 static struct bt_a2dp_stream_ops stream_ops;
 
+#if defined(CONFIG_BT_A2DP_SOURCE_SBC_AUDIO_SHELL)
+/* sbc audio stream control variables */
+static int64_t ref_time;
+static uint32_t a2dp_src_missed_count;
+static volatile bool a2dp_src_playback;
+static volatile int media_index;
+static uint32_t a2dp_src_sf;
+static uint8_t a2dp_src_nc;
+static uint32_t send_samples_count;
+static uint16_t send_count;
+/* 20ms max packet pcm data size. the max is 480 * 2 * 2 * 2 */
+static uint8_t a2dp_pcm_buffer[480 * 2 * 2 * 2];
+struct sbc_encoder encoder;
+#endif /* CONFIG_BT_A2DP_SOURCE_SBC_AUDIO_SHELL */
 #if defined(CONFIG_BT_A2DP_SOURCE)
+#if !defined(CONFIG_BT_A2DP_SOURCE_SBC_AUDIO_SHELL)
 static uint8_t media_data[] = {
 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10,
 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10,
@@ -56,10 +75,16 @@ static uint8_t media_data[] = {
 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10,
 };
 #endif
+#endif
 
+BT_A2DP_SBC_EP_CFG_DEFAULT(sbc_cfg_default, A2DP_SBC_SAMP_FREQ_44100);
 NET_BUF_POOL_DEFINE(a2dp_tx_pool, CONFIG_BT_MAX_CONN,
-		BT_L2CAP_BUF_SIZE(CONFIG_BT_L2CAP_TX_MTU),
+		BT_L2CAP_BUF_SIZE(CONFIG_BT_A2DP_SOURCE_DATA_BUF_SIZE),
 		CONFIG_BT_CONN_TX_USER_DATA_SIZE, NULL);
+#if defined(CONFIG_BT_A2DP_SOURCE_SBC_AUDIO_SHELL)
+static void a2dp_playback_timeout_handler(struct k_timer *timer);
+K_TIMER_DEFINE(a2dp_player_timer, a2dp_playback_timeout_handler, NULL);
+#endif
 
 static struct bt_sdp_attribute a2dp_sink_attrs[] = {
 	BT_SDP_NEW_SERVICE,
@@ -196,6 +221,159 @@ static struct bt_sdp_attribute a2dp_source_attrs[] = {
 	BT_SDP_SERVICE_NAME("A2DPSource"),
 	BT_SDP_SUPPORTED_FEATURES(0x0001U),
 };
+
+#if defined(CONFIG_BT_A2DP_SOURCE_SBC_AUDIO_SHELL)
+static uint8_t *a2dp_produce_media(uint32_t samples_num)
+{
+	uint8_t *media = NULL;
+	uint16_t  medialen;
+
+	/* Music Audio is Stereo */
+	medialen = (samples_num * a2dp_src_nc * 2);
+
+	/* For mono or dual configuration, skip alternative samples */
+	if (1 == a2dp_src_nc) {
+		uint16_t index;
+
+		media = (uint8_t *)&a2dp_pcm_buffer[0];
+
+		for (index = 0; index < samples_num; index++) {
+			media[(2 * index)] = *((uint8_t *)media_data + media_index);
+			media[(2 * index) + 1] = *((uint8_t *)media_data + media_index + 1);
+			/* Update the tone index */
+			media_index += 4u;
+			if (media_index >= sizeof(media_data)) {
+				media_index = 0U;
+			}
+		}
+	} else {
+		if ((media_index + (samples_num << 2)) > sizeof(media_data)) {
+			media = (uint8_t *)&a2dp_pcm_buffer[0];
+			memcpy(media, ((uint8_t *)media_data + media_index),
+				sizeof(media_data) - media_index);
+			memcpy(&media[sizeof(media_data) - media_index],
+				((uint8_t *)media_data),
+				((samples_num << 2) - (sizeof(media_data) - media_index)));
+			/* Update the tone index */
+			media_index = ((samples_num << 2) -
+				(sizeof(media_data) - media_index));
+		} else {
+			media = ((uint8_t *)media_data + media_index);
+			/* Update the tone index */
+			media_index += (samples_num << 2);
+			if (media_index >= sizeof(media_data)) {
+				media_index = 0U;
+			}
+		}
+	}
+
+	return media;
+}
+
+static void audio_work_handler(struct k_work *work)
+{
+	int64_t period_ms;
+	uint32_t a2dp_src_num_samples;
+	uint8_t *pcm_data;
+	uint8_t index;
+	uint32_t pcm_frame_size;
+	uint32_t pcm_frame_samples;
+	uint32_t encoded_frame_size;
+	struct net_buf *buf;
+	uint8_t frame_num = 0;
+	uint8_t *sbc_hdr;
+	uint32_t pdu_len;
+	uint32_t out_size;
+	int err;
+
+	/* If stopped then return */
+	if (!a2dp_src_playback) {
+		return;
+	}
+
+	buf = bt_a2dp_stream_create_pdu(&a2dp_tx_pool, K_NO_WAIT);
+	if (buf == NULL) {
+		/* fail */
+		return;
+	}
+
+	period_ms = k_uptime_delta(&ref_time);
+
+	pcm_frame_size = sbc_frame_bytes(&encoder);
+	pcm_frame_samples = sbc_frame_samples(&encoder);
+	encoded_frame_size = sbc_frame_encoded_bytes(&encoder);
+
+	sbc_hdr = net_buf_add(buf, 1u);
+	/* Get the number of samples */
+	a2dp_src_num_samples = (uint16_t)((period_ms * a2dp_src_sf) / 1000);
+	a2dp_src_missed_count += (uint32_t)((period_ms * a2dp_src_sf) % 1000);
+	a2dp_src_missed_count += ((a2dp_src_num_samples % pcm_frame_samples) * 1000);
+	a2dp_src_num_samples = (a2dp_src_num_samples / pcm_frame_samples) * pcm_frame_samples;
+	frame_num = a2dp_src_num_samples / pcm_frame_samples;
+
+	pdu_len = buf->len + frame_num * encoded_frame_size;
+
+	if (pdu_len > net_buf_tailroom(buf)) {
+		printk("need increase buf size\n");
+		return;
+	}
+
+	if (pdu_len > bt_a2dp_get_mtu(&sbc_stream)) {
+		printk("need decrease CONFIG_BT_A2DP_SOURCE_DATA_SEND_INTERVAL\n");
+		return;
+	}
+
+	/* Raw adjust for the drift */
+	while (a2dp_src_missed_count >= (1000 * pcm_frame_samples)) {
+		if (pdu_len + encoded_frame_size > bt_a2dp_get_mtu(&sbc_stream) ||
+		    pdu_len + encoded_frame_size > net_buf_tailroom(buf)) {
+			break;
+		}
+
+		pdu_len += encoded_frame_size;
+		a2dp_src_num_samples += pcm_frame_samples;
+		frame_num++;
+		a2dp_src_missed_count -= (1000 * pcm_frame_samples);
+	}
+
+	pcm_data = a2dp_produce_media(a2dp_src_num_samples);
+	if (pcm_data == NULL) {
+		printk("no media data\n");
+		return;
+	}
+
+	for (index = 0; index < frame_num; index++) {
+		out_size = sbc_encode(&encoder, (uint8_t *)&pcm_data[index * pcm_frame_size],
+				      net_buf_tail(buf));
+		if (encoded_frame_size != out_size) {
+			printk("sbc encode fail\n");
+			continue;
+		}
+
+		net_buf_add(buf, encoded_frame_size);
+	}
+
+	*sbc_hdr = (uint8_t)BT_A2DP_SBC_MEDIA_HDR_ENCODE(frame_num, 0, 0, 0);
+
+	if (default_a2dp != NULL) {
+		err = bt_a2dp_stream_send(&sbc_stream, buf, send_count, send_samples_count);
+		if (err < 0) {
+			printk("  Failed to send SBC audio data on streams(%d)\n", err);
+			net_buf_unref(buf);
+		}
+	}
+
+	send_count++;
+	send_samples_count += a2dp_src_num_samples;
+}
+
+static K_WORK_DEFINE(audio_work, audio_work_handler);
+
+static void a2dp_playback_timeout_handler(struct k_timer *timer)
+{
+	k_work_submit(&audio_work);
+}
+#endif
 
 static struct bt_sdp_record a2dp_source_rec = BT_SDP_RECORD(a2dp_source_attrs);
 
@@ -411,6 +589,30 @@ static void app_suspend_rsp(struct bt_a2dp_stream *stream, uint8_t rsp_err_code)
 static void stream_configured(struct bt_a2dp_stream *stream)
 {
 	bt_shell_print("stream configured");
+#if defined(CONFIG_BT_A2DP_SOURCE_SBC_AUDIO_SHELL)
+	struct sbc_encoder_init_param param;
+	struct bt_a2dp_codec_sbc_params *sbc_config = (struct bt_a2dp_codec_sbc_params *)
+						      &sbc_cfg_default.codec_config->codec_ie[0];
+
+	a2dp_src_sf = bt_a2dp_sbc_get_sampling_frequency(sbc_config);
+	a2dp_src_nc = bt_a2dp_sbc_get_channel_num(sbc_config);
+
+	param.bit_rate = CONFIG_BT_A2DP_SOURCE_SBC_BIT_RATE_DEFAULT;
+	param.samp_freq = a2dp_src_sf;
+	param.blk_len = bt_a2dp_sbc_get_block_length(sbc_config);
+	param.subband = bt_a2dp_sbc_get_subband_num(sbc_config);
+	param.alloc_mthd = bt_a2dp_sbc_get_allocation_method(sbc_config);
+	param.ch_mode = bt_a2dp_sbc_get_channel_mode(sbc_config);
+	param.ch_num = bt_a2dp_sbc_get_channel_num(sbc_config);
+	param.min_bitpool = sbc_config->min_bitpool;
+	param.max_bitpool = sbc_config->max_bitpool;
+
+	if (sbc_setup_encoder(&encoder, &param)) {
+		printk("sbc encoder initialization fail\n");
+	} else {
+		printk("sbc encoder initialization success\n");
+	}
+#endif
 }
 
 static void stream_established(struct bt_a2dp_stream *stream)
@@ -426,11 +628,23 @@ static void stream_released(struct bt_a2dp_stream *stream)
 static void stream_started(struct bt_a2dp_stream *stream)
 {
 	bt_shell_print("stream started");
+#if defined(CONFIG_BT_A2DP_SOURCE_SBC_AUDIO_SHELL)
+	uint32_t audio_time_interval = CONFIG_BT_A2DP_SOURCE_DATA_SEND_INTERVAL;
+
+	/* Start Audio Source */
+	a2dp_src_playback = true;
+
+	k_uptime_delta(&ref_time);
+	k_timer_start(&a2dp_player_timer, K_MSEC(audio_time_interval), K_MSEC(audio_time_interval));
+#endif
 }
 
 static void stream_suspended(struct bt_a2dp_stream *stream)
 {
 	bt_shell_print("stream suspended");
+#if defined(CONFIG_BT_A2DP_SOURCE_SBC_AUDIO_SHELL)
+	k_timer_stop(&a2dp_player_timer);
+#endif
 }
 
 #if defined(CONFIG_BT_A2DP_SINK)
@@ -645,7 +859,6 @@ static struct bt_a2dp_stream_ops stream_ops = {
 #endif
 };
 
-BT_A2DP_SBC_EP_CFG_DEFAULT(sbc_cfg_default, A2DP_SBC_SAMP_FREQ_44100);
 static int cmd_configure(const struct shell *sh, int32_t argc, char *argv[])
 {
 	int err;
@@ -672,7 +885,7 @@ static int cmd_configure(const struct shell *sh, int32_t argc, char *argv[])
 			registered_sbc_endpoint, found_peer_sbc_endpoint,
 			&sbc_cfg_default);
 		if (err) {
-			shell_error(sh, "fail to configure");
+			shell_error(sh, "fail to configure: %d", err);
 		}
 	} else {
 		shell_error(sh, "a2dp is not connected");
