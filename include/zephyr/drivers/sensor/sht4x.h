0d9c76012b6b49cfcd3cbf92ea5ee27cc98b1d36
Benjamin Cabé <benjamin@zephyrproject.org>
2025-10-30 22:45:54 +0100

    include: drivers: sensor: doxygen docs for sht4x
    Fixup doxygen documentation
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- include/zephyr/drivers/sensor/sht4x.h
+++ include/zephyr/drivers/sensor/sht4x.h
@@ -6,7 +6,8 @@
 
 /**
  * @file
- * @brief Extended public API for Sensirion's SHT4X T/RH sensors
+ * @brief Header file for extended sensor API of SHT4X sensor
+ * @ingroup sht4x_interface
  *
  * This exposes an API to the on-chip heater which is specific to
  * the application/environment and cannot be expressed within the
@@ -16,19 +17,35 @@
 #ifndef ZEPHYR_INCLUDE_DRIVERS_SENSOR_SHT4X_H_
 #define ZEPHYR_INCLUDE_DRIVERS_SENSOR_SHT4X_H_
 
+/**
+ * @defgroup sht4x_interface SHT4X
+ * @ingroup sensor_interface_ext
+ * @brief Sensirion SHT4X temperature and humidity sensor
+ * @{
+ */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #include <zephyr/drivers/sensor.h>
 
-/* Maximum temperature above which the heater should not be used */
+/** Maximum temperature above which the heater should not be used */
 #define SHT4X_HEATER_MAX_TEMP 65
 
+/**
+ * @brief Custom sensor attributes for SHT4X
+ */
 enum sensor_attribute_sht4x {
-	/* Heater Power: 0, 1, 2 -> high, med, low */
+	/**
+	 * Heater Power.
+	 * 0, 1, 2 -> high, med, low
+	 */
 	SENSOR_ATTR_SHT4X_HEATER_POWER = SENSOR_ATTR_PRIV_START,
-	/* Heater Duration: 0, 1 -> long, short */
+	/**
+	 * Heater Duration.
+	 * 0, 1 -> long, short
+	 */
 	SENSOR_ATTR_SHT4X_HEATER_DURATION
 };
 
@@ -46,5 +63,9 @@ int sht4x_fetch_with_heater(const struct device *dev);
 #ifdef __cplusplus
 }
 #endif
+
+/**
+ * @}
+ */
 
 #endif /* ZEPHYR_INCLUDE_DRIVERS_SENSOR_SHT4X_H_ */
