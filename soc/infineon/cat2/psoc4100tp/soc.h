747a8dfba759434bb1ae48402a59338f93224250
Manojkumar Konisetty <manoj@aerlync.com>
2025-10-30 17:41:33 +0530

    soc: infineon: add soc initialization support for PSoC4100TP
    Add SoC port for the Infineon PSoC4100TP series, including core
    initialization, respective Kconfigs and yaml files.
    
    dts: arm: Add device tree support for PSoC4100TP series
    
    Add device tree sources for the 44-pin TQFP, 48-pin QFN,
    48-pin TQFP, and 64-pin TQFP pin variants, system clock configuration
    
    Signed-off-by: Manojkumar Konisetty <manoj@aerlync.com>
    Signed-off-by: Sayooj K Karun <sayooj@aerlync.com>

---
+++ soc/infineon/cat2/psoc4100tp/soc.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025 Infineon Technologies AG,
+ * or an affiliate of Infineon Technologies AG.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_INFINEON_CAT2_PSOC4100TP_SOC_H_
+#define ZEPHYR_SOC_INFINEON_CAT2_PSOC4100TP_SOC_H_
+
+#ifndef _ASMLANGUAGE
+#include <cy_device_headers.h>
+#endif /* !_ASMLANGUAGE */
+
+#endif /* ZEPHYR_SOC_INFINEON_CAT2_PSOC4100TP_SOC_H_ */
