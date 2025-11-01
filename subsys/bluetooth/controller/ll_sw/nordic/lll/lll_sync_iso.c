eeff565c1227353568747736efd9330d09e917f1
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2025-11-01 14:06:55 +0100

    Bluetooth: Controller: Fix ISO Receiver Sync Lost assertion
    Fix ISO Receiver Sync Lost assertion due to successive
    calls to stop ticker instance under conditions where there
    are more than one ISO Sync LLL events enqueued in the LLL
    prepare pipeline. This is visible when supporting LLL resume
    for ISO Sync LLL events.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_sync_iso.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_sync_iso.c
@@ -186,6 +186,15 @@ static int prepare_cb_common(struct lll_prepare_param *p)
 
 	lll = p->param;
 
+	/* Check if stopped (on disconnection between prepare and preempt)
+	 */
+	if (unlikely(lll->is_lll_stop != 0U)) {
+		radio_isr_set(lll_isr_early_abort, lll);
+		radio_disable();
+
+		return 0;
+	}
+
 	/* Calculate the current event latency */
 	lll->lazy_prepare = p->lazy;
 	lll->latency_event = lll->latency_prepare + lll->lazy_prepare;
