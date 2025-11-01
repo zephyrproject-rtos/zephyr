8f9ac1d1ff6f0c86bf9d896562e1f0415eea7a74
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2025-11-01 10:33:56 +0100

    Bluetooth: Controller: Fix radio_tmr_start_us added +1 us
    Fix radio_tmr_start_us() calls for the added +1 us used
    when starting radio using the packet timer.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_sync_iso.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_sync_iso.c
@@ -1680,7 +1680,9 @@ isr_rx_next_subevent:
 
 		hcto -= jitter_us;
 
-		start_us = hcto;
+		/* +1 us radio_tmr_start_us compensation */
+		start_us = hcto - 1U;
+
 		hcto = radio_tmr_start_us(0U, start_us);
 		LL_ASSERT_ERR(hcto == (start_us + 1U));
 
@@ -1697,7 +1699,9 @@ isr_rx_next_subevent:
 		 */
 		hcto += radio_tmr_ready_restore();
 
-		start_us = hcto;
+		/* +1 us radio_tmr_start_us compensation */
+		start_us = hcto - 1U;
+
 		hcto = radio_tmr_start_us(0U, start_us);
 		LL_ASSERT_ERR(hcto == (start_us + 1U));
 
