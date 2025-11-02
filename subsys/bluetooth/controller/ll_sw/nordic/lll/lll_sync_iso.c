f2538c732b1d6c30ff9122922dac12628efbd3b0
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2025-11-02 06:34:26 +0100

    Bluetooth: Controller: ISO Sync Receiver with resume reservation
    Added implementation to have ISO Sync Receiver resume ticker
    to reserve time for next BIS when using sequential packing.
    This is required so that any overlapping role that can
    drift in its window be place after the next BIS subevent.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_sync_iso.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_sync_iso.c
@@ -7,9 +7,7 @@
 #include <stdint.h>
 #include <string.h>
 
-#include <soc.h>
 #include <zephyr/sys/byteorder.h>
-#include <zephyr/sys/util.h>
 #include <zephyr/bluetooth/hci_types.h>
 
 #include "hal/cpu.h"
@@ -20,6 +18,7 @@
 #include "util/util.h"
 #include "util/mem.h"
 #include "util/memq.h"
+#include "util/mayfly.h"
 
 #include "ticker/ticker.h"
 
@@ -38,8 +37,6 @@
 #include "lll_tim_internal.h"
 #include "lll_prof_internal.h"
 
-#include "ll_feat.h"
-
 #include "hal/debug.h"
 
 static int init_reset(void);
@@ -54,6 +51,11 @@ static void isr_rx_lll_done(void *param);
 static void isr_done(void *param);
 static uint16_t payload_index_get(const struct lll_sync_iso *lll);
 #if defined(CONFIG_BT_CTLR_SYNC_ISO_SEQUENTIAL)
+#if defined(CONFIG_BT_CTLR_SYNC_ISO_SLOT_WINDOW_JITTER)
+static void ticker_resume_cb(uint32_t ticks_at_expire, uint32_t ticks_drift, uint32_t remainder,
+			     uint16_t lazy, uint8_t force, void *param);
+static void ticker_op_start_cb(uint32_t status, void *param);
+#endif /* CONFIG_BT_CTLR_SYNC_ISO_SLOT_WINDOW_JITTER */
 static void next_chan_calc_seq(struct lll_sync_iso *lll, uint16_t event_counter,
 			       uint16_t data_chan_id);
 #endif /* CONFIG_BT_CTLR_SYNC_ISO_SEQUENTIAL */
@@ -330,10 +332,10 @@ static int prepare_cb_common(struct lll_prepare_param *p)
 	const bool is_sequential_packing = (lll->bis_spacing >= (lll->sub_interval * lll->nse));
 
 #if defined(CONFIG_BT_CTLR_SYNC_ISO_SLOT_WINDOW_JITTER)
+	uint8_t skipped_bis;
 	uint8_t skipped;
 
 	if (p->ticks_drift != 0U) {
-		uint8_t skipped_bis;
 		uint32_t drift_us;
 
 		/* FIXME: Add implementation to support interleaved packing BIG event drift and
@@ -404,6 +406,7 @@ static int prepare_cb_common(struct lll_prepare_param *p)
 		p->remainder = HAL_TICKER_REMAINDER(drift_us);
 #endif /* CONFIG_BT_TICKER_REMAINDER_SUPPORT */
 	} else {
+		skipped_bis = 0U;
 		skipped = 0U;
 	}
 #endif /* CONFIG_BT_CTLR_SYNC_ISO_SLOT_WINDOW_JITTER */
@@ -469,8 +472,10 @@ static int prepare_cb_common(struct lll_prepare_param *p)
 		}
 
 		/* Calculate the radio channel to use for subevent */
-		while (skipped != 0U) {
-			skipped--;
+		uint8_t skip = skipped;
+
+		while (skip != 0U) {
+			skip--;
 
 			data_chan_use = lll_chan_iso_subevent(data_chan_id,
 						lll->data_chan_map,
@@ -617,6 +622,28 @@ static int prepare_cb_common(struct lll_prepare_param *p)
 #if defined(CONFIG_BT_CTLR_SYNC_ISO_SEQUENTIAL)
 	} else if (is_sequential_packing) {
 		next_chan_calc_seq(lll, event_counter, data_chan_id);
+
+#if defined(CONFIG_BT_CTLR_SYNC_ISO_SLOT_WINDOW_JITTER)
+		if (skipped_bis < (lll->num_bis - 1U)) {
+			uint32_t ticks_slot;
+			uint32_t bis_offset;
+
+			ticks_slot = ull_sync_iso_lll_ticks_slot_get(lll);
+			bis_offset = lll->bis_spacing + HAL_TICKER_TICKS_TO_US(ticks_slot) -
+				     (lll->sub_interval * (skipped + 1U));
+
+			ret = ticker_start(TICKER_INSTANCE_ID_CTLR, TICKER_USER_ID_LLL,
+					   (TICKER_ID_SCAN_SYNC_ISO_RESUME_BASE +
+					    ull_sync_iso_lll_index_get(lll)),
+					   p->ticks_at_expire, HAL_TICKER_US_TO_TICKS(bis_offset),
+					   TICKER_NULL_PERIOD, TICKER_NULL_REMAINDER,
+					   TICKER_NULL_LAZY, ticks_slot,
+					   ticker_resume_cb, lll, ticker_op_start_cb,
+					   (void *)__LINE__);
+			LL_ASSERT_ERR((ret == TICKER_STATUS_SUCCESS) ||
+				      (ret == TICKER_STATUS_BUSY));
+		}
+#endif /* CONFIG_BT_CTLR_SYNC_ISO_SLOT_WINDOW_JITTER */
 #endif /* CONFIG_BT_CTLR_SYNC_ISO_SEQUENTIAL */
 
 #if defined(CONFIG_BT_CTLR_SYNC_ISO_INTERLEAVED)
@@ -1955,6 +1982,28 @@ static uint16_t payload_index_get(const struct lll_sync_iso *lll)
 }
 
 #if defined(CONFIG_BT_CTLR_SYNC_ISO_SEQUENTIAL)
+#if defined(CONFIG_BT_CTLR_SYNC_ISO_SLOT_WINDOW_JITTER)
+static void ticker_resume_cb(uint32_t ticks_at_expire, uint32_t ticks_drift, uint32_t remainder,
+			     uint16_t lazy, uint8_t force, void *param)
+{
+	/* TODO: Add implementation to place ULL time reservation for subsequent BIS spacing */
+}
+
+static void ticker_op_start_cb(uint32_t status, void *param)
+{
+	ARG_UNUSED(status);
+	ARG_UNUSED(param);
+
+	/* FIXME: Handle the skipped ULL scheduling of the subevent used to reserve time.
+	 *
+	 *  This assertion check will fail, i.e. ULL scheduled reception of AUX_ADV_IND PDU can
+	 *  cause the ULL time reservation for subevent to be skipped.
+	 *
+	 *  LL_ASSERT_ERR(status == TICKER_STATUS_SUCCESS);
+	 */
+}
+#endif /* CONFIG_BT_CTLR_SYNC_ISO_SLOT_WINDOW_JITTER */
+
 static void next_chan_calc_seq(struct lll_sync_iso *lll, uint16_t event_counter,
 			       uint16_t data_chan_id)
 {
