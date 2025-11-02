c8a1bf6f172362de7eb7bec7778e2696c2d51ad5
Harun Spago <harun.spago.code@gmail.com>
2025-11-02 21:50:48 +0100

    lib: libc: common: source: signal: added raise function
    Implemnted raise function by using using the k_oops kernel
    function to terminate the running thread, and logging to log the
    error to the debug interface
    
    Signed-off-by: Harun Spago <harun.spago.code@gmail.com>

--- lib/libc/common/source/signal/signal.c
+++ lib/libc/common/source/signal/signal.c
@@ -5,6 +5,10 @@
 
 #include <errno.h>
 #include <signal.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(libc_raise, LOG_LEVEL_DBG);
 
 void (*signal(int signum, void (*handler)(int)))(int)
 {
@@ -12,4 +16,41 @@ void (*signal(int signum, void (*handler)(int)))(int)
 	ARG_UNUSED(handler);
 	errno = EINVAL;
 	return SIG_ERR;
+}
+
+int raise(int sig)
+{
+	/** All ISO C99 signals checked */
+    switch (sig) {
+    case SIGABRT:
+        LOG_ERR("raise(): Caught SIGABRT - aborting");
+        k_oops(); /* Zephyr fatal error handler */
+        break;
+
+    case SIGFPE:
+        LOG_ERR("raise(): Caught SIGFPE - floating point exception");
+        k_oops();
+        break;
+
+    case SIGILL:
+        LOG_ERR("raise(): Caught SIGILL - illegal instruction");
+        k_oops();
+        break;
+
+    case SIGSEGV:
+        LOG_ERR("raise(): Caught SIGSEGV - segmentation fault");
+        k_oops();
+        break;
+
+    case SIGTERM:
+        LOG_WRN("raise(): Caught SIGTERM - termination request");
+        /** Optional: graceful shutdown point */
+        break;
+
+    default:
+        LOG_WRN("raise(): Unknown signal %d - ignoring", sig);
+        break;
+    }
+
+    return 0;
 }
