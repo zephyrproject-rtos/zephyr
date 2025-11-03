2443de2a7c2a3e97dbe7335076498cec08215a1c
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2025-11-03 12:38:58 +0000

    tests: mgmt: mcumgr: settings_mgmt: Add test for single setting save
    Expands the test to check thr functionality of the newly adding save
    subtree/save single setting
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- tests/subsys/mgmt/mcumgr/settings_mgmt/src/settings.c
+++ tests/subsys/mgmt/mcumgr/settings_mgmt/src/settings.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Nordic Semiconductor ASA
+ * Copyright (c) 2023-2025 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -16,6 +16,8 @@ static bool export_called;
 static bool commit_called;
 static uint8_t val_aa[4];
 static uint8_t val_bb;
+
+struct save_single_data single_data;
 
 static int val_handle_set(const char *name, size_t len, settings_read_cb read_cb, void *cb_arg);
 static int val_handle_commit(void);
@@ -75,6 +77,10 @@ static int val_handle_get(const char *name, char *val, int val_len_max)
 
 	get_called = true;
 
+	if (val_len_max < 0) {
+		return -EINVAL;
+	}
+
 	if (settings_name_steq(name, "aa", &next) && !next) {
 		val_len_max = MIN(val_len_max, sizeof(val_aa));
 		memcpy(val, val_aa, val_len_max);
@@ -102,4 +108,171 @@ void settings_state_get(bool *set, bool *get, bool *export, bool *commit)
 	*get = get_called;
 	*export = export_called;
 	*commit = commit_called;
+}
+
+static int first_set(const char *name, size_t len, settings_read_cb read_cb, void *cb_arg)
+{
+	const char *next;
+
+	if (settings_name_steq(name, "value1", &next) && !next) {
+		if (len != sizeof(single_data.first_val)) {
+			return -EINVAL;
+		}
+		(void)read_cb(cb_arg, &single_data.first_val, sizeof(single_data.first_val));
+		single_data.first_set_called = true;
+		return 0;
+	}
+	if (settings_name_steq(name, "value2", &next) && !next) {
+		if (len != sizeof(single_data.second_val)) {
+			return -EINVAL;
+		}
+		(void)read_cb(cb_arg, &single_data.second_val, sizeof(single_data.second_val));
+		single_data.second_set_called = true;
+		return 0;
+	}
+
+	return -ENOENT;
+}
+
+static int first_get(const char *name, char *val, int val_len_max)
+{
+	const char *next;
+
+	if (val_len_max < 0) {
+		return -EINVAL;
+	}
+
+	if (settings_name_steq(name, "value1", &next) && !next) {
+		val_len_max = MIN(val_len_max, sizeof(single_data.first_val));
+		memcpy(val, &single_data.first_val, val_len_max);
+		single_data.first_get_called = true;
+		return val_len_max;
+	} else if (settings_name_steq(name, "value2", &next) && !next) {
+		val_len_max = MIN(val_len_max, sizeof(single_data.second_val));
+		memcpy(val, &single_data.second_val, val_len_max);
+		single_data.second_get_called = true;
+		return val_len_max;
+	}
+
+	return -ENOENT;
+}
+
+static int first_commit(void)
+{
+	single_data.first_second_commit_called = true;
+	return 0;
+}
+
+static int first_export(int (*cb)(const char *name, const void *value, size_t val_len))
+{
+	(void)cb("first/value1", &single_data.first_val, sizeof(single_data.first_val));
+	(void)cb("first/value2", &single_data.second_val, sizeof(single_data.second_val));
+
+	single_data.first_second_export_called = true;
+	return 0;
+}
+
+SETTINGS_STATIC_HANDLER_DEFINE(first, "first", first_get, first_set, first_commit, first_export);
+
+static int third_set(const char *name, size_t len, settings_read_cb read_cb, void *cb_arg)
+{
+	const char *next;
+
+	if (settings_name_steq(name, "value3", &next) && !next) {
+		if (len != sizeof(single_data.third_val)) {
+			return -EINVAL;
+		}
+		(void)read_cb(cb_arg, &single_data.third_val, sizeof(single_data.third_val));
+		single_data.third_set_called = true;
+		return 0;
+	}
+
+	return -ENOENT;
+}
+
+static int third_get(const char *name, char *val, int val_len_max)
+{
+	const char *next;
+
+	if (val_len_max < 0) {
+		return -EINVAL;
+	}
+
+	if (settings_name_steq(name, "value3", &next) && !next) {
+		val_len_max = MIN(val_len_max, sizeof(single_data.third_val));
+		memcpy(val, &single_data.third_val, val_len_max);
+		single_data.third_get_called = true;
+		return val_len_max;
+	}
+
+	return -ENOENT;
+}
+
+static int third_commit(void)
+{
+	single_data.third_commit_called = true;
+	return 0;
+}
+
+static int third_export(int (*cb)(const char *name, const void *value, size_t val_len))
+{
+	(void)cb("first/other/value3", &single_data.third_val, sizeof(single_data.third_val));
+
+	single_data.third_export_called = true;
+	return 0;
+}
+
+SETTINGS_STATIC_HANDLER_DEFINE(third, "first/other", third_get, third_set, third_commit,
+			       third_export);
+
+static int forth_set(const char *name, size_t len, settings_read_cb read_cb, void *cb_arg)
+{
+	const char *next;
+
+	if (settings_name_steq(name, "value4", &next) && !next) {
+		if (len != sizeof(single_data.forth_val)) {
+			return -EINVAL;
+		}
+		(void)read_cb(cb_arg, &single_data.forth_val, sizeof(single_data.forth_val));
+		single_data.forth_set_called = true;
+		return 0;
+	}
+
+	return -ENOENT;
+}
+
+static int forth_commit(void)
+{
+	single_data.forth_commit_called = true;
+	return 0;
+}
+
+static int forth_export(int (*cb)(const char *name, const void *value, size_t val_len))
+{
+	(void)cb("first/expected_fail/value4", &single_data.forth_val,
+		 sizeof(single_data.forth_val));
+
+	single_data.forth_export_called = true;
+	return 0;
+}
+
+SETTINGS_STATIC_HANDLER_DEFINE(forth, "first/expected_fail", NULL, forth_set, forth_commit,
+			       forth_export);
+
+void single_modification_reset(void)
+{
+	single_data.first_second_export_called = false;
+	single_data.first_second_commit_called = false;
+	single_data.first_get_called = false;
+	single_data.first_set_called = false;
+	single_data.second_get_called = false;
+	single_data.second_set_called = false;
+	single_data.third_export_called = false;
+	single_data.third_commit_called = false;
+	single_data.third_get_called = false;
+	single_data.third_set_called = false;
+	single_data.forth_export_called = false;
+	single_data.forth_commit_called = false;
+	single_data.forth_get_called = false;
+	single_data.forth_set_called = false;
 }
