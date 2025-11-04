d32427d73505e16e9652f971ad88a32b09af6916
Anas Nashif <anas.nashif@intel.com>
2025-11-04 11:47:54 -0500

    drivers: promote APIs to unstable status
    Many APIs are stuck at experimental status although they already fullfil
    the requirements to be considered unstable, i.e. they have multiple
    implementations and have been in the tree for more than 2 releases.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- include/zephyr/drivers/sdhc.h
+++ include/zephyr/drivers/sdhc.h
@@ -21,7 +21,7 @@
  * @brief Interfaces for Secure Digital Host Controllers (SDHC).
  * @defgroup sdhc_interface SDHC
  * @since 3.1
- * @version 0.1.0
+ * @version 0.8.0
  * @ingroup io_interfaces
  * @{
  */
