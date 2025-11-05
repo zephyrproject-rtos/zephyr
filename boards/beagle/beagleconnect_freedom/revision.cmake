c6629df0778e835fbe7d8f99d593a66484cc5402
Ayush Singh <ayush@beagleboard.org>
2025-11-05 14:26:20 +0530

    boards: beagle: beagleconnect_freedom: Add rev C5 support
    There seem to be a decent number of C5 boards around. So add support for
    it while keeping C7 as the default version.
    
    The main difference between C7 and C5 is that C5 uses CC1352P instead
    of CC1352P7
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

---
+++ boards/beagle/beagleconnect_freedom/revision.cmake
@@ -0,0 +1,13 @@
+set(BOARD_REVISIONS "C5" "C7")
+
+if(NOT BOARD_REVISION IN_LIST BOARD_REVISIONS)
+  message(FATAL_ERROR "${BOARD_REVISION} is not a valid revision for BeagleConnect Freedom. Accepted revisions: ${BOARD_REVISIONS}")
+endif()
+
+if("${BOARD_QUALIFIERS}" MATCHES "^/cc1352p7$" AND "${BOARD_REVISION}" STREQUAL "C5")
+  message(FATAL_ERROR "C5 only supports CC1352P. Use C7 instead.")
+endif()
+
+if("${BOARD_QUALIFIERS}" MATCHES "^/cc1352p$" AND "${BOARD_REVISION}" STREQUAL "C7")
+  message(FATAL_ERROR "C7 only supports CC1352P7. Use C5 instead.")
+endif()
