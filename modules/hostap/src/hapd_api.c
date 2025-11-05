cae99704615cf2d3ad73d3d53b1e59a7d125393c
David Leach <david.leach@nxp.com>
2025-11-05 14:42:00 -0600

    Revert "[nxp fromlist] hostap: avoid configuring region domain when SAP is enabled"
    This reverts commit df29bc19805fb063b1bf71340233bcf429fe2bab.
    
    Signed-off-by: David Leach <david.leach@nxp.com>

--- modules/hostap/src/hapd_api.c
+++ modules/hostap/src/hapd_api.c
@@ -365,28 +365,18 @@ out:
 }
 #endif
 
-int hostapd_ap_reg_domain(const struct device *dev,
+bool hostapd_ap_reg_domain(const struct device *dev,
 	struct wifi_reg_domain *reg_domain)
 {
 	struct hostapd_iface *iface;
-	int ret = 0;
 
 	iface = get_hostapd_handle(dev);
 	if (iface == NULL) {
 		wpa_printf(MSG_ERROR, "Interface %s not found", dev->name);
-		ret = -ENODEV;
+		return false;
 	}
 
-	if (iface->state == HAPD_IFACE_ENABLED) {
-		wpa_printf(MSG_ERROR, "Interface %s is operational and in SAP mode", dev->name);
-		ret = -EACCES;
-	}
-
-	if (!hostapd_cli_cmd_v("set country_code %s", reg_domain->country_code)) {
-		ret = -ENOTSUP;
-	}
-
-	return ret;
+	return hostapd_cli_cmd_v("set country_code %s", reg_domain->country_code);
 }
 
 static int hapd_config_chan_center_seg0(struct hostapd_iface *iface,
