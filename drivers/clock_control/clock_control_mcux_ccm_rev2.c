1d22ca2df64f790252db32a0a8ca994ce8d28d16
David Leach <david.leach@nxp.com>
2025-11-05 14:42:01 -0600

    Revert "[nxp fromlist] drivers: clock_control_mcux_ccm_rev2: add imx91 enet support"
    This reverts commit 2710f8a25074bce23960786cbd2ebd17f87c6082.
    
    Signed-off-by: David Leach <david.leach@nxp.com>

--- drivers/clock_control/clock_control_mcux_ccm_rev2.c
+++ drivers/clock_control/clock_control_mcux_ccm_rev2.c
@@ -28,7 +28,7 @@ static int mcux_ccm_on(const struct device *dev,
 	switch (peripheral) {
 #ifdef CONFIG_ETH_NXP_ENET
 
-#if defined(CONFIG_SOC_MIMX9352) || defined(CONFIG_SOC_MIMX9131)
+#ifdef CONFIG_SOC_MIMX9352
 #define ENET1G_CLOCK	kCLOCK_Enet1
 #else
 #define ENET_CLOCK	kCLOCK_Enet
@@ -177,7 +177,7 @@ static int mcux_ccm_get_subsys_rate(const struct device *dev,
 #ifdef CONFIG_ETH_NXP_ENET
 	case IMX_CCM_ENET_CLK:
 	case IMX_CCM_ENET1G_CLK:
-#if defined(CONFIG_SOC_MIMX9352) || defined(CONFIG_SOC_MIMX9131)
+#ifdef CONFIG_SOC_MIMX9352
 		clock_root = kCLOCK_Root_WakeupAxi;
 #else
 		clock_root = kCLOCK_Root_Bus;
