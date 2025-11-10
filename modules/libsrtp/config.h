7b00a7d16924eb9cdfefc913572798de57eadc1c
Sayed Naser Moravej <seyednasermoravej@gmail.com>
2025-11-10 21:59:31 +0330

    modules: libSRTP glue added to the modules folder.
    gule files added so Zephyr RTOS can use libSRTP.
    
    Signed-off-by: Sayed Naser Moravej <seyednasermoravej@gmail.com>

---
+++ modules/libsrtp/config.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2025 Sayed Naser Moravej <seyednasermoravej@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdbool.h>
+
+#define PACKAGE_VERSION "1.0.0"
+
+#define PACKAGE_STRING "Zephyr 1.0.0"
+
+#define ENABLE_DEBUG_LOGGING 1
+
+#define CPU_CISC 1
+
+#define HAVE_ARPA_INET_H 1
+
+#define HAVE_INTTYPES_H 1
+
+#define HAVE_NETINET_IN_H 1
+
+#define HAVE_STDINT_H 1
+
+#define HAVE_STDLIB_H 1
+
+#define HAVE_SYS_SOCKET_H 1
+
+#define HAVE_SYS_TYPES_H 1
+
+#define HAVE_UNISTD_H 1
+
+#define HAVE_INET_ATON 1
+
+#define HAVE_USLEEP 1
+
+#define HAVE_UINT8_T 1
+
+#define HAVE_UINT16_T 1
+
+#define HAVE_UINT32_T 1
+
+#define HAVE_UINT64_T 1
+
+#define HAVE_INT32_T 1
+
+#define SIZEOF_UNSIGNED_LONG 4
+
+#define SIZEOF_UNSIGNED_LONG_LONG 8
+
+#define _SYS_SOCKET_H 1
+
+#include <zephyr/net/socket.h>
+
+#define MBEDTLS_AES_C
+
+#define MBEDTLS_CIPHER_C
+
+#define MBEDTLS_GCM_C
+
+#define MBEDTLS_CTR_DRBG_C
+
+#define MBEDTLS_MD_C
+
+#define MBEDTLS_HMAC_DRBG_C
+
+#define MBEDTLS 1
+
+#define HAVE_INLINE 1
