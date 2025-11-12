6e1fd5651332f074a286335d06f037b3bd404c17
Cristian Bulacu <cristian.bulacu@nxp.com>
2025-11-12 15:19:24 +0200

    openthread: platform: Add NAT64 support at platform level
    This commit adds platform code for NAT64 functionality
    
    Signed-off-by: Cristian Bulacu <cristian.bulacu@nxp.com>

--- modules/openthread/platform/platform-zephyr.h
+++ modules/openthread/platform/platform-zephyr.h
@@ -132,6 +132,9 @@ void border_agent_deinit(void);
 otError trel_plat_init(otInstance *instance, struct net_if *ail_iface_ptr);
 otError dhcpv6_pd_client_init(otInstance *ot_instance, uint32_t ail_iface_index);
 otError dns_upstream_resolver_init(otInstance *ot_instance);
+void openthread_border_router_set_nat64_translator_enabled(bool enable);
+otError infra_if_nat64_init(void);
+otError infra_if_send_raw_message(uint8_t *buf, uint16_t len);
 #endif /* CONFIG_OPENTHREAD_ZEPHYR_BORDER_ROUTER */
 
 #endif /* PLATFORM_ZEPHYR_H_ */
