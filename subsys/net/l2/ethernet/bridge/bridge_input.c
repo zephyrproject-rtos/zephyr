4f115db883804d8dce0371861f53a02e4fd6e9a0
Yangbo Lu <yangbo.lu@nxp.com>
2025-11-13 17:45:34 +0800

    net: bridge: add forward decision support
    Added forward decision support including for broadcast,
    multicast and local address packets.
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- subsys/net/l2/ethernet/bridge/bridge_input.c
+++ subsys/net/l2/ethernet/bridge/bridge_input.c
@@ -31,6 +31,28 @@ static int eth_bridge_forward(struct net_if *bridge, struct net_if *orig_iface, 
 	return 0;
 }
 
+static int eth_bridge_handle_locally(struct net_if *bridge, struct net_if *orig_iface,
+				     struct net_pkt *pkt)
+{
+	enum net_verdict verdict;
+
+	net_pkt_set_iface(pkt, bridge);
+	net_pkt_set_orig_iface(pkt, orig_iface);
+
+	if (net_if_l2(bridge)->recv != NULL) {
+		verdict = net_if_l2(bridge)->recv(bridge, pkt);
+		if (verdict == NET_DROP) {
+			return -EIO;
+		}
+	}
+
+	NET_DBG("Passing rx pkt %p to bridge %d rx path from %d",
+		pkt, net_if_get_by_iface(bridge),
+		net_if_get_by_iface(orig_iface));
+
+	return 0;
+}
+
 static inline bool is_link_local_addr(struct net_eth_addr *addr)
 {
 	if (addr->addr[0] == 0x01 &&
@@ -50,6 +72,8 @@ enum net_verdict eth_bridge_input_process(struct net_if *iface, struct net_pkt *
 	struct ethernet_context *ctx = net_if_l2_data(iface);
 	struct net_if *bridge = net_eth_get_bridge(ctx);
 	struct net_eth_addr *dst_addr = (struct net_eth_addr *)(net_pkt_lladdr_dst(pkt)->addr);
+	struct net_eth_addr *bridge_addr =
+		(struct net_eth_addr *)(net_if_get_link_addr(bridge)->addr);
 
 	/* Drop all link-local packets for now. */
 	if (is_link_local_addr(dst_addr)) {
@@ -57,9 +81,29 @@ enum net_verdict eth_bridge_input_process(struct net_if *iface, struct net_pkt *
 		return NET_DROP;
 	}
 
-	if (eth_bridge_forward(bridge, iface, pkt) != 0) {
-		return NET_DROP;
+	/* Handle broadcast and multicast */
+	if (net_eth_is_addr_broadcast(dst_addr) || net_eth_is_addr_multicast(dst_addr)) {
+		if (eth_bridge_forward(bridge, iface, pkt) != 0) {
+			return NET_DROP;
+		}
+
+		if (eth_bridge_handle_locally(bridge, iface, pkt) != 0) {
+			return NET_DROP;
+		}
+		return NET_OK;
 	}
 
-	return NET_CONTINUE;
+	/* Handle local address */
+	if (memcmp(bridge_addr, dst_addr, NET_ETH_ADDR_LEN) == 0) {
+		if (eth_bridge_handle_locally(bridge, iface, pkt) != 0) {
+			return NET_DROP;
+		}
+		return NET_OK;
+	}
+
+	/* Forward others */
+	(void)eth_bridge_forward(bridge, iface, pkt);
+
+	/* Drop forwarded pkt for original iface */
+	return NET_DROP;
 }
