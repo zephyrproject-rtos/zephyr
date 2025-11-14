bf7841e2bc57ace258eb985bba80f6d42fe30952
Ha Duong Quang <ha.duongquang@nxp.com>
2025-11-14 15:59:26 +0700

    boards: s32k5xxcvb: add support for adc
    Add devicetree of adc instances for s32k566.
    
    Signed-off-by: Ha Duong Quang <ha.duongquang@nxp.com>

--- drivers/adc/adc_nxp_s32_adc_sar.c
+++ drivers/adc/adc_nxp_s32_adc_sar.c
@@ -8,15 +8,26 @@
 #include <zephyr/drivers/adc.h>
 #include <zephyr/drivers/pinctrl.h>
 #include <zephyr/irq.h>
-#include <Adc_Sar_Ip_HwAccess.h>
 #include <Adc_Sar_Ip.h>
 #include <Adc_Sar_Ip_Irq.h>
 
 #define ADC_CONTEXT_USES_KERNEL_TIMER
 #include "adc_context.h"
 
+#if !defined(IP_ADC_0_BASE)
+#define IP_ADC_0_BASE IP_SARADC_0_BASE
+#define IP_ADC_1_BASE IP_SARADC_1_BASE
+#define IP_ADC_2_BASE IP_LPE_SARADC_BASE
+#endif
+
 #define DT_DRV_COMPAT nxp_s32_adc_sar
 LOG_MODULE_REGISTER(adc_nxp_s32_adc_sar, CONFIG_ADC_LOG_LEVEL);
+
+/* Define the hardware register size when calculating bit positions */
+#define ADC_SAR_IP_HW_REG_SIZE 32
+
+/* Definitions to compute bit positions from channel index */
+#define ADC_SAR_IP_CHAN_2_BIT(CHNIDX)  ((CHNIDX) % ADC_SAR_IP_HW_REG_SIZE)
 
 /* Convert channel of group ADC to channel of physical ADC instance */
 #define ADC_NXP_S32_GROUPCHAN_2_PHYCHAN(group, channel)	\
