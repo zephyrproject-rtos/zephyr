0d2d4c77e4daf46de8a637820fe72c4f0ca63bcc
Josuah Demangeon <me@josuah.net>
2025-11-15 16:39:00 +0000

    style: samples: apply coding style on CMakeLists.txt files
    Apply the CMake style guidelines to CMakeList.txt files in samples/.
    
    Signed-off-by: Josuah Demangeon <me@josuah.net>

--- samples/boards/nordic/coresight_stm/sysbuild.cmake
+++ samples/boards/nordic/coresight_stm/sysbuild.cmake
@@ -4,25 +4,25 @@
 set(REMOTE_APP remote)
 
 ExternalZephyrProject_Add(
-	APPLICATION ${REMOTE_APP}_rad
-	SOURCE_DIR  ${APP_DIR}/${REMOTE_APP}
-	BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpurad
+  APPLICATION ${REMOTE_APP}_rad
+  SOURCE_DIR  ${APP_DIR}/${REMOTE_APP}
+  BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpurad
 )
 
 # There are sample configurations which do not use PPR.
 if(SB_CONFIG_APP_CPUPPR_RUN)
-	ExternalZephyrProject_Add(
-		APPLICATION ${REMOTE_APP}_ppr
-		SOURCE_DIR  ${APP_DIR}/${REMOTE_APP}
-		BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpuppr
-	)
+  ExternalZephyrProject_Add(
+    APPLICATION ${REMOTE_APP}_ppr
+    SOURCE_DIR  ${APP_DIR}/${REMOTE_APP}
+    BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpuppr
+  )
 endif()
 
 # There are sample configurations which do not use FLPR.
 if(SB_CONFIG_APP_CPUFLPR_RUN)
-	ExternalZephyrProject_Add(
-		APPLICATION ${REMOTE_APP}_flpr
-		SOURCE_DIR  ${APP_DIR}/${REMOTE_APP}
-		BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpuflpr
-	)
+  ExternalZephyrProject_Add(
+    APPLICATION ${REMOTE_APP}_flpr
+    SOURCE_DIR  ${APP_DIR}/${REMOTE_APP}
+    BOARD ${SB_CONFIG_BOARD}/${SB_CONFIG_SOC}/cpuflpr
+  )
 endif()
