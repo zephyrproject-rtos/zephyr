9ece697de0b06cf87d4ef44a1e01de5d1eb505ce
Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>
2025-11-17 17:04:10 +0100

    drivers: i2c: i2c_nrfx_twi: use standard instantiation
    Switched nrfx_twi API to standard instantiation.
    
    Signed-off-by: Adam Kondraciuk <adam.kondraciuk@nordicsemi.no>

--- drivers/i2c/i2c_nrfx_twi_common.c
+++ drivers/i2c/i2c_nrfx_twi_common.c
@@ -16,7 +16,8 @@ LOG_MODULE_DECLARE(i2c_nrfx_twi);
 int i2c_nrfx_twi_init(const struct device *dev)
 {
 	const struct i2c_nrfx_twi_config *config = dev->config;
-	int result = nrfx_twi_init(&config->twi, &config->config,
+	struct i2c_nrfx_twi_common_data *data = dev->data;
+	int result = nrfx_twi_init(&data->twi, &config->config,
 				   config->event_handler, (void *)dev);
 	if (result != 0) {
 		LOG_ERR("Failed to initialize device: %s",
@@ -29,9 +30,8 @@ int i2c_nrfx_twi_init(const struct device *dev)
 
 int i2c_nrfx_twi_configure(const struct device *dev, uint32_t dev_config)
 {
-	const struct i2c_nrfx_twi_config *config = dev->config;
 	struct i2c_nrfx_twi_common_data *data = dev->data;
-	nrfx_twi_t const *inst = &config->twi;
+	nrfx_twi_t const *inst = &data->twi;
 
 	if (I2C_ADDR_10_BITS & dev_config) {
 		return -EINVAL;
@@ -39,10 +39,10 @@ int i2c_nrfx_twi_configure(const struct device *dev, uint32_t dev_config)
 
 	switch (I2C_SPEED_GET(dev_config)) {
 	case I2C_SPEED_STANDARD:
-		nrf_twi_frequency_set(inst->p_twi, NRF_TWI_FREQ_100K);
+		nrf_twi_frequency_set(inst->p_reg, NRF_TWI_FREQ_100K);
 		break;
 	case I2C_SPEED_FAST:
-		nrf_twi_frequency_set(inst->p_twi, NRF_TWI_FREQ_400K);
+		nrf_twi_frequency_set(inst->p_reg, NRF_TWI_FREQ_400K);
 		break;
 	default:
 		LOG_ERR("unsupported speed");
@@ -55,12 +55,12 @@ int i2c_nrfx_twi_configure(const struct device *dev, uint32_t dev_config)
 
 int i2c_nrfx_twi_recover_bus(const struct device *dev)
 {
-	const struct i2c_nrfx_twi_config *config = dev->config;
+	struct i2c_nrfx_twi_common_data *data = dev->data;
 	uint32_t scl_pin;
 	uint32_t sda_pin;
 
-	scl_pin = nrf_twi_scl_pin_get(config->twi.p_twi);
-	sda_pin = nrf_twi_sda_pin_get(config->twi.p_twi);
+	scl_pin = nrf_twi_scl_pin_get(data->twi.p_reg);
+	sda_pin = nrf_twi_sda_pin_get(data->twi.p_reg);
 
 	return nrfx_twi_bus_recover(scl_pin, sda_pin);
 }
@@ -69,7 +69,7 @@ int i2c_nrfx_twi_msg_transfer(const struct device *dev, uint8_t flags,
 			      uint8_t *buf, size_t buf_len,
 			      uint16_t i2c_addr, bool more_msgs)
 {
-	const struct i2c_nrfx_twi_config *config = dev->config;
+	struct i2c_nrfx_twi_common_data *data = dev->data;
 	int ret = 0;
 	uint32_t xfer_flags = 0;
 	nrfx_twi_xfer_desc_t cur_xfer = {
@@ -107,7 +107,7 @@ int i2c_nrfx_twi_msg_transfer(const struct device *dev, uint8_t flags,
 	}
 
 	if (!ret) {
-		ret = nrfx_twi_xfer(&config->twi, &cur_xfer, xfer_flags);
+		ret = nrfx_twi_xfer(&data->twi, &cur_xfer, xfer_flags);
 	}
 
 	return ret;
@@ -133,7 +133,7 @@ int twi_nrfx_pm_action(const struct device *dev, enum pm_device_action action)
 		break;
 
 	case PM_DEVICE_ACTION_SUSPEND:
-		nrfx_twi_uninit(&config->twi);
+		nrfx_twi_uninit(&data->twi);
 
 		ret = pinctrl_apply_state(config->pcfg, PINCTRL_STATE_SLEEP);
 		if (ret < 0) {
