59f135c149b56316bef80d1ddf4bbb4736dc11f3
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2025-11-19 16:28:09 +0200

    samples: net: common: Add Quic support
    Add Quic support to common sample file so that it can be used
    by different sample applications.
    
    The certificates in samples/net/common/quic_certs are copied
    from samples/net/sockets/http_server/src/certs directory.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- samples/net/common/net_sample_common.h
+++ samples/net/common/net_sample_common.h
@@ -4,7 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/net/net_ip.h>
+#include <zephyr/net/socket.h>
 #include <zephyr/net/net_if.h>
+#include <zephyr/net/tls_credentials.h>
+#include <zephyr/net/quic.h>
 
 #if defined(CONFIG_NET_CONNECTION_MANAGER)
 void wait_for_network(void);
@@ -36,3 +40,39 @@ static inline bool is_tunnel(struct net_if *iface)
 	return false;
 }
 #endif /* CONFIG_NET_L2_IPIP */
+
+#if defined(CONFIG_QUIC)
+int setup_quic(const struct net_sockaddr *remote_addr,
+	       const struct net_sockaddr *local_addr,
+	       enum quic_stream_direction direction,
+	       sec_tag_t sec_tag_list[],
+	       size_t sec_tag_list_size,
+	       const char *alpn_list[],
+	       size_t alpn_list_size);
+void close_quic(int quic_sock);
+#else
+static inline int setup_quic(const struct net_sockaddr *remote_addr,
+			     const struct net_sockaddr *local_addr,
+			     enum quic_stream_direction direction,
+			     sec_tag_t sec_tag_list[],
+			     size_t sec_tag_list_size,
+			     const char *alpn_list[],
+			     size_t alpn_list_size)
+{
+	ARG_UNUSED(remote_addr);
+	ARG_UNUSED(local_addr);
+	ARG_UNUSED(direction);
+	ARG_UNUSED(sec_tag_list);
+	ARG_UNUSED(sec_tag_list_size);
+	ARG_UNUSED(alpn_list);
+	ARG_UNUSED(alpn_list_size);
+
+	return -ENOTSUP;
+}
+
+static inline void close_quic(int quic_sock)
+{
+	ARG_UNUSED(quic_sock);
+}
+
+#endif /* CONFIG_QUIC */
