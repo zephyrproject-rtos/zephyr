64bf18007a9f3953c21eea8b2c8a79f52d26fe07
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2025-11-20 15:31:21 +0100

    tests: bsim: Bluetooth: audio: Enable tests that pass now
    Enable tests that pass now and respective github issues can
    be closed.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- tests/bsim/bluetooth/audio/test_scripts/audio_config_tests/cap_ac_3_48_1_1.sh
+++ tests/bsim/bluetooth/audio/test_scripts/audio_config_tests/cap_ac_3_48_1_1.sh
@@ -4,6 +4,4 @@
 
 source $(dirname "$0")/_ac_common.sh
 
-# No sent callback on peripheral and no RX on peripheral
-# https://github.com/zephyrproject-rtos/zephyr/issues/83585
-# ac_config=3 ac_tx_preset=48_1_1 ac_rx_preset=48_1_1 ac_acc_cnt=1 Execute_cap_unicast_ac $@
+ac_config=3 ac_tx_preset=48_1_1 ac_rx_preset=48_1_1 ac_acc_cnt=1 Execute_cap_unicast_ac $@
