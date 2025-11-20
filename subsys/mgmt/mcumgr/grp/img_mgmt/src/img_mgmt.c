764f0d183bc06d92acf6fb5ebabd17ef1a913add
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2025-11-20 15:20:23 +0100

    img_mgmt: Use absolute address in active partition
    Use absolute addresses while determining a running application
    partition.
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- subsys/mgmt/mcumgr/grp/img_mgmt/src/img_mgmt.c
+++ subsys/mgmt/mcumgr/grp/img_mgmt/src/img_mgmt.c
@@ -48,13 +48,9 @@
 	to be able to figure out application running slot.
 #endif
 
-#define FIXED_PARTITION_GET_FLASH_NODE(node_id)                                    \
-	COND_CODE_1(DT_NODE_HAS_COMPAT(DT_PARENT(node_id), fixed_subpartitions),   \
-		    (DT_PARENT(DT_GPARENT(node_id))), (DT_GPARENT(node_id)))
-
 #define FIXED_PARTITION_IS_RUNNING_APP_PARTITION(label)                                            \
-	DT_SAME_NODE(FIXED_PARTITION_GET_FLASH_NODE(DT_CHOSEN(zephyr_code_partition)),             \
-		     FIXED_PARTITION_GET_FLASH_NODE(DT_NODELABEL(label))) &&                       \
+	DT_SAME_NODE(FIXED_PARTITION_NODE_MTD(DT_CHOSEN(zephyr_code_partition)),                   \
+		FIXED_PARTITION_MTD(label)) &&                                                     \
 	(FIXED_PARTITION_OFFSET(label) <= CONFIG_FLASH_LOAD_OFFSET &&                              \
 	 FIXED_PARTITION_OFFSET(label) + FIXED_PARTITION_SIZE(label) > CONFIG_FLASH_LOAD_OFFSET)
 
