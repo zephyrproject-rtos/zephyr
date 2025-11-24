c89da03c968f7a2ec01968fef849a8ebaae3bd51
Tomáš Juřena <jurenatomas@gmail.com>
2025-11-24 17:44:28 +0100

    drivers: sensor: hx7xx: Add new driver
    Adds new driver for HX7xx based ADC chips. It supports HX710, HX711,
    HX712, HX717, HX720, and HX71708. These chips are designed to measure
    weight using bridge sensor, and this driver is providing an interface to
    weight data and basic control (tare, calibration).
    Communication with specified devices is almost identical and requires only
    small changes to support all of the listed devices. This driver is
    currently able to measure only the channel A. In the future will be nice
    to have measurement of the channel B which can provide additional
    information based on the type of ADC chip.
    
    Devices can be distinguished by the it's package to SOP-8 and SOP-16.
    SOP-8 devices do not have extra pin to control sampling frequency so it
    uses communication protocol to set it. It also have fixed gain. The
    SOP-16 devices have extra pins to control the sampling frequency so the
    communication protocol is used to control the gain.
    
    Signed-off-by: Georgios Gkinis <gkinis.georgios@gmail.com>
    Signed-off-by: Tomáš Juřena <jurenatomas@gmail.com>

---
+++ include/zephyr/drivers/sensor/hx7xx.h
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2020 George Gkinis
+ * Copyright (c) 2021 Jan Gnip
+ * SPDX-License-Identifier: Apache-2.0
+ */
+/**
+ * @file
+ * @brief Header file for HX7xx family of sensors
+ * @ingroup hx7xx_interface
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_SENSOR_HX711_H_
+#define ZEPHYR_INCLUDE_DRIVERS_SENSOR_HX711_H_
+
+/**
+ * @defgroup hx7xx_interface HX7xx
+ * @ingroup sensor_interface_ext
+ * @brief 24-Bit Analog-to-Digital Converter (ADC) for Weigh Scales
+ * @{
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
+
+/**
+ * Allowed set of gains used in HX7xx sensors.
+ */
+enum hx7xx_gain {
+	/** Gain factor 8 */
+	HX7xx_GAIN_8X = 8,
+
+	/** Gain factor 32 */
+	HX7xx_GAIN_32X = 32,
+
+	/** Gain factor 64 */
+	HX7xx_GAIN_64X = 64,
+
+	/** Gain factor 128 */
+	HX7xx_GAIN_128X = 128,
+};
+
+/**
+ * Allowed set of rates used in HX7xx sensors.
+ */
+enum hx7xx_rate {
+	/** 10 samples per second */
+	HX7xx_RATE_10HZ = 10,
+
+	/** 20 samples per second */
+	HX7xx_RATE_20HZ = 20,
+
+	/** 40 samples per second */
+	HX7xx_RATE_40HZ = 40,
+
+	/** 80 samples per second */
+	HX7xx_RATE_80HZ = 80,
+
+	/** 320 samples per second */
+	HX7xx_RATE_320HZ = 320,
+};
+
+/**
+ * @brief Zero the HX711.
+ *
+ * @param dev Pointer to the hx711 device structure
+ * @param readings Number of readings to get average offset.
+ *        5~10 readings should be enough, although more are allowed.
+ *
+ * @retval 0 If successful.
+ * @retval -EINVAL If bad arguments are passed.
+ *
+ */
+int avia_hx7xx_tare(const struct device *dev, uint8_t readings);
+
+/**
+ * @brief Callibrate the HX711.
+ *
+ * Given a target value of a known weight the slope gets calculated.
+ * This is actually unit agnostic.
+ * If the target weight is given in grams, lb, Kg or any other weight unit,
+ * the slope will be calculated accordingly.
+ * This function should be invoked before any readings are taken.
+ *
+ * @param dev Pointer to the hx711 device structure
+ * @param readings Number of readings to take for calibration.
+ *        5~10 readings should be enough, although more are allowed.
+ * @param calibration_weight Know weight in grams.
+ *
+ * @retval 0 If successful.
+ * @retval -EINVAL If bad arguments are passed.
+ */
+int avia_hx7xx_calibrate(const struct device *dev, uint8_t readings, double calibration_weight);
+
+#ifdef __cplusplus
+}
+#endif
+
+/**
+ * @}
+ */
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_SENSOR_HX711_H_ */
