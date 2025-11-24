592c22f25d26bb8e288fc5c89c9f2a3b1ca621ad
Deepika aerlync <deepika@aerlync.com>
2025-11-24 17:54:09 +0530

    modules: hal_infineon: add PSoC4 Kconfig support
    Add Kconfig configuration support for PSoC4 family in hal_infineon module.
    
    Signed-off-by: Manojkumar Konisetty <manoj@aerlync.com>
    Signed-off-by: Sayooj K Karun <sayooj@aerlync.com>
    Signed-off-by: Deepika aerlync <deepika@aerlync.com>

--- modules/hal_infineon/infineon_kconfig.h
+++ modules/hal_infineon/infineon_kconfig.h
@@ -44,4 +44,335 @@
 #endif /* CONFIG_CPU_CORTEXT_M33* */
 #endif /* CONFIG_SOC_PSE846GPS2DBZC4A* */
 
+#if defined(CONFIG_SOC_SERIES_PSOC4100TP)
+
+/*
+ * Map Zephyr Kconfig selections for PSOC4 SoCs to the macros expected by the
+ * ModusToolbox PDL headers. We define both the bare part macro and the variant
+ * with a trailing underscore to retain compatibility with existing build logic.
+ */
+
+#if defined(CONFIG_SOC_CY8C4146LQI_T403)
+#ifndef CY8C4146LQI_T403
+#define CY8C4146LQI_T403
+#endif
+#ifndef CY8C4146LQI_T403_
+#define CY8C4146LQI_T403_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4146AXI_T403)
+#ifndef CY8C4146AXI_T403
+#define CY8C4146AXI_T403
+#endif
+#ifndef CY8C4146AXI_T403_
+#define CY8C4146AXI_T403_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4146AZI_T403)
+#ifndef CY8C4146AZI_T403
+#define CY8C4146AZI_T403
+#endif
+#ifndef CY8C4146AZI_T403_
+#define CY8C4146AZI_T403_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4146AZI_T405)
+#ifndef CY8C4146AZI_T405
+#define CY8C4146AZI_T405
+#endif
+#ifndef CY8C4146AZI_T405_
+#define CY8C4146AZI_T405_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4146LQI_T413)
+#ifndef CY8C4146LQI_T413
+#define CY8C4146LQI_T413
+#endif
+#ifndef CY8C4146LQI_T413_
+#define CY8C4146LQI_T413_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4146AXI_T413)
+#ifndef CY8C4146AXI_T413
+#define CY8C4146AXI_T413
+#endif
+#ifndef CY8C4146AXI_T413_
+#define CY8C4146AXI_T413_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4146AZI_T413)
+#ifndef CY8C4146AZI_T413
+#define CY8C4146AZI_T413
+#endif
+#ifndef CY8C4146AZI_T413_
+#define CY8C4146AZI_T413_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4146AZI_T415)
+#ifndef CY8C4146AZI_T415
+#define CY8C4146AZI_T415
+#endif
+#ifndef CY8C4146AZI_T415_
+#define CY8C4146AZI_T415_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4146LQI_T453)
+#ifndef CY8C4146LQI_T453
+#define CY8C4146LQI_T453
+#endif
+#ifndef CY8C4146LQI_T453_
+#define CY8C4146LQI_T453_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4146AXI_T453)
+#ifndef CY8C4146AXI_T453
+#define CY8C4146AXI_T453
+#endif
+#ifndef CY8C4146AXI_T453_
+#define CY8C4146AXI_T453_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4146AZI_T453)
+#ifndef CY8C4146AZI_T453
+#define CY8C4146AZI_T453
+#endif
+#ifndef CY8C4146AZI_T453_
+#define CY8C4146AZI_T453_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4146AZI_T455)
+#ifndef CY8C4146AZI_T455
+#define CY8C4146AZI_T455
+#endif
+#ifndef CY8C4146AZI_T455_
+#define CY8C4146AZI_T455_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147LQI_T403)
+#ifndef CY8C4147LQI_T403
+#define CY8C4147LQI_T403
+#endif
+#ifndef CY8C4147LQI_T403_
+#define CY8C4147LQI_T403_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AXI_T403)
+#ifndef CY8C4147AXI_T403
+#define CY8C4147AXI_T403
+#endif
+#ifndef CY8C4147AXI_T403_
+#define CY8C4147AXI_T403_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AZI_T403)
+#ifndef CY8C4147AZI_T403
+#define CY8C4147AZI_T403
+#endif
+#ifndef CY8C4147AZI_T403_
+#define CY8C4147AZI_T403_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AZI_T405)
+#ifndef CY8C4147AZI_T405
+#define CY8C4147AZI_T405
+#endif
+#ifndef CY8C4147AZI_T405_
+#define CY8C4147AZI_T405_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147LQI_T413)
+#ifndef CY8C4147LQI_T413
+#define CY8C4147LQI_T413
+#endif
+#ifndef CY8C4147LQI_T413_
+#define CY8C4147LQI_T413_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AXI_T413)
+#ifndef CY8C4147AXI_T413
+#define CY8C4147AXI_T413
+#endif
+#ifndef CY8C4147AXI_T413_
+#define CY8C4147AXI_T413_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AZI_T413)
+#ifndef CY8C4147AZI_T413
+#define CY8C4147AZI_T413
+#endif
+#ifndef CY8C4147AZI_T413_
+#define CY8C4147AZI_T413_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AZI_T415)
+#ifndef CY8C4147AZI_T415
+#define CY8C4147AZI_T415
+#endif
+#ifndef CY8C4147AZI_T415_
+#define CY8C4147AZI_T415_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147LQI_T453)
+#ifndef CY8C4147LQI_T453
+#define CY8C4147LQI_T453
+#endif
+#ifndef CY8C4147LQI_T453_
+#define CY8C4147LQI_T453_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AXI_T453)
+#ifndef CY8C4147AXI_T453
+#define CY8C4147AXI_T453
+#endif
+#ifndef CY8C4147AXI_T453_
+#define CY8C4147AXI_T453_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AZI_T453)
+#ifndef CY8C4147AZI_T453
+#define CY8C4147AZI_T453
+#endif
+#ifndef CY8C4147AZI_T453_
+#define CY8C4147AZI_T453_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AZI_T455)
+#ifndef CY8C4147AZI_T455
+#define CY8C4147AZI_T455
+#endif
+#ifndef CY8C4147AZI_T455_
+#define CY8C4147AZI_T455_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147LQI_T463)
+#ifndef CY8C4147LQI_T463
+#define CY8C4147LQI_T463
+#endif
+#ifndef CY8C4147LQI_T463_
+#define CY8C4147LQI_T463_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AXI_T463)
+#ifndef CY8C4147AXI_T463
+#define CY8C4147AXI_T463
+#endif
+#ifndef CY8C4147AXI_T463_
+#define CY8C4147AXI_T463_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AZI_T463)
+#ifndef CY8C4147AZI_T463
+#define CY8C4147AZI_T463
+#endif
+#ifndef CY8C4147AZI_T463_
+#define CY8C4147AZI_T463_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AZI_T465)
+#ifndef CY8C4147AZI_T465
+#define CY8C4147AZI_T465
+#endif
+#ifndef CY8C4147AZI_T465_
+#define CY8C4147AZI_T465_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147LQI_T473)
+#ifndef CY8C4147LQI_T473
+#define CY8C4147LQI_T473
+#endif
+#ifndef CY8C4147LQI_T473_
+#define CY8C4147LQI_T473_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AXI_T473)
+#ifndef CY8C4147AXI_T473
+#define CY8C4147AXI_T473
+#endif
+#ifndef CY8C4147AXI_T473_
+#define CY8C4147AXI_T473_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AZI_T473)
+#ifndef CY8C4147AZI_T473
+#define CY8C4147AZI_T473
+#endif
+#ifndef CY8C4147AZI_T473_
+#define CY8C4147AZI_T473_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AZI_T475)
+#ifndef CY8C4147AZI_T475
+#define CY8C4147AZI_T475
+#endif
+#ifndef CY8C4147AZI_T475_
+#define CY8C4147AZI_T475_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147LQQ_T493)
+#ifndef CY8C4147LQQ_T493
+#define CY8C4147LQQ_T493
+#endif
+#ifndef CY8C4147LQQ_T493_
+#define CY8C4147LQQ_T493_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AXQ_T493)
+#ifndef CY8C4147AXQ_T493
+#define CY8C4147AXQ_T493
+#endif
+#ifndef CY8C4147AXQ_T493_
+#define CY8C4147AXQ_T493_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AZQ_T493)
+#ifndef CY8C4147AZQ_T493
+#define CY8C4147AZQ_T493
+#endif
+#ifndef CY8C4147AZQ_T493_
+#define CY8C4147AZQ_T493_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AZQ_T495)
+#ifndef CY8C4147AZQ_T495
+#define CY8C4147AZQ_T495
+#endif
+#ifndef CY8C4147AZQ_T495_
+#define CY8C4147AZQ_T495_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4146AZQ_T413)
+#ifndef CY8C4146AZQ_T413
+#define CY8C4146AZQ_T413
+#endif
+#ifndef CY8C4146AZQ_T413_
+#define CY8C4146AZQ_T413_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4146AZQ_T453)
+#ifndef CY8C4146AZQ_T453
+#define CY8C4146AZQ_T453
+#endif
+#ifndef CY8C4146AZQ_T453_
+#define CY8C4146AZQ_T453_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AZQ_T415)
+#ifndef CY8C4147AZQ_T415
+#define CY8C4147AZQ_T415
+#endif
+#ifndef CY8C4147AZQ_T415_
+#define CY8C4147AZQ_T415_
+#endif
+#endif
+#if defined(CONFIG_SOC_CY8C4147AZQ_T455)
+#ifndef CY8C4147AZQ_T455
+#define CY8C4147AZQ_T455
+#endif
+#ifndef CY8C4147AZQ_T455_
+#define CY8C4147AZQ_T455_
+#endif
+#endif
+
+#endif /* CONFIG_SOC_SERIES_PSOC4100TP */
+
 #endif /* INFINEON_KCONFIG_H__ */
