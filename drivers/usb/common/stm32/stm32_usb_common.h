452714a31567930ce482afada97429f7a3dcfd1f
Mathieu Choplain <mathieu.choplain-ext@st.com>
2025-11-24 14:28:32 +0100

    drivers: usb: udc: stm32: move macros to common code & refactor
    Refactor utility macros in the STM32 UDC driver, and move some of them to
    the STM32 common USB header as they can be useful in other places.
    
    While at it, add new helper macro USB_STM32_NODE_PHY_IS_EMBEDDED_FS.
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain-ext@st.com>

--- drivers/usb/common/stm32/stm32_usb_common.h
+++ drivers/usb/common/stm32/stm32_usb_common.h
@@ -7,6 +7,45 @@
 #ifndef ZEPHYR_DRIVERS_USB_COMMON_STM32_STM32_USB_COMMON_H_
 #define ZEPHYR_DRIVERS_USB_COMMON_STM32_STM32_USB_COMMON_H_
 
+#include <zephyr/devicetree.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/types.h>
+
+/* Compatible of all STM32 USB controllers */
+#define STM32_USB_COMPATIBLES								\
+	st_stm32_usb, st_stm32_otgfs, st_stm32_otghs
+
+/* Shorthand to obtain PHY node for an instance */
+#define USB_STM32_PHY(usb_node)			DT_PROP_BY_IDX(usb_node, phys, 0)
+
+/* Evaluates to 1 if `usb_node` is High-Speed capable, 0 otherwise. */
+#define USB_STM32_NODE_IS_HS_CAPABLE(usb_node)	DT_NODE_HAS_COMPAT(usb_node, st_stm32_otghs)
+
+/* Evaluates to 1 if PHY of `usb_node` is an ULPI PHY, 0 otherwise. */
+#define USB_STM32_NODE_PHY_IS_ULPI(usb_node)						\
+	UTIL_AND(USB_STM32_NODE_IS_HS_CAPABLE(usb_node),				\
+		DT_NODE_HAS_COMPAT(USB_STM32_PHY(usb_node), usb_ulpi_phy))
+
+/*
+ * Evaluates to 1 if PHY of `usb_node` is an embedded HS PHY, 0 otherwise.
+ *
+ * Implementation notes:
+ * All embedded HS PHYs have specific compatibles (with ST vendor).
+ */
+#define USB_STM32_NODE_PHY_IS_EMBEDDED_HS(usb_node)					\
+	UTIL_OR(DT_NODE_HAS_COMPAT(USB_STM32_PHY(usb_node), st_stm32_usbphyc),		\
+		DT_NODE_HAS_COMPAT(USB_STM32_PHY(usb_node), st_stm32u5_otghs_phy))
+
+/*
+ * Evaluates to 1 if PHY of `usb_node` is an embedded FS PHY, 0 otherwise.
+ *
+ * Implementation notes:
+ * USB PHYs are either ULPI, embedded HS or embedded FS.
+ */
+#define USB_STM32_NODE_PHY_IS_EMBEDDED_FS(usb_node)					\
+	UTIL_AND(UTIL_NOT(USB_STM32_NODE_PHY_IS_ULPI(usb_node)),			\
+		 UTIL_NOT(USB_STM32_NODE_PHY_IS_EMBEDDED_HS(usb_node)))
+
 /**
  * @brief Configures the Power Controller as necessary
  * for proper operation of the USB controllers
