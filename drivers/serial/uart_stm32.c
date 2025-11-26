4aa5e95135b56a9615131adc29eb76e458b41ca5
Mathieu Choplain <mathieu.choplain-ext@st.com>
2025-11-26 15:03:20 +0100

    drivers: uart: stm32: configure proper DMA burst length
    The burst length unit is bytes, not number of transfers. This had not been
    an issue since the DMA driver historically ignored the values, but has now
    become one since they are used and (most importantly for us) validated.
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain-ext@st.com>

--- drivers/serial/uart_stm32.c
+++ drivers/serial/uart_stm32.c
@@ -2506,8 +2506,11 @@ static int uart_stm32_pm_action(const struct device *dev, enum pm_device_action 
 					STM32_DMA_CHANNEL_CONFIG(index, dir)),\
 		.dest_data_size = STM32_DMA_CONFIG_##dest_dev##_DATA_SIZE(\
 				STM32_DMA_CHANNEL_CONFIG(index, dir)),	\
-		.source_burst_length = 1, /* SINGLE transfer */		\
-		.dest_burst_length = 1,					\
+		/* single transfers (burst length = data size) */	\
+		.source_burst_length = STM32_DMA_CONFIG_##src_dev##_DATA_SIZE(\
+				STM32_DMA_CHANNEL_CONFIG(index, dir)),	\
+		.dest_burst_length = STM32_DMA_CONFIG_##dest_dev##_DATA_SIZE(\
+				STM32_DMA_CHANNEL_CONFIG(index, dir)),	\
 		.block_count = 1,					\
 		.dma_callback = uart_stm32_dma_##dir##_cb,		\
 	},								\
