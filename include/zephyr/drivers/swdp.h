490fe49856789dfbf6b94b6a6aa1685f4e87e687
Johann Fischer <johann.fischer@nordicsemi.no>
2025-11-27 18:05:58 +0100

    include: drivers: add SWD driver user API
    Although the DAP subsystem is the only user in the tree, yet, we may have
    other subsystems, libraries, or simple samples that could use this API
    in the future.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- include/zephyr/drivers/swdp.h
+++ include/zephyr/drivers/swdp.h
@@ -194,10 +194,198 @@ struct swdp_api {
 	int (*swdp_port_off)(const struct device *dev);
 };
 
+/**
+ * @brief Write count bits to SWDIO from data LSB first
+ *
+ * Typically used for line reset sequence or switching JTAG<->SWD operation.
+ *
+ * @param dev SWDP device
+ * @param count Number of bits to write
+ * @param data Bits to write
+ * @return 0 on success, or error code
+ */
+__syscall int swdp_output_sequence(const struct device *dev,
+				   const uint32_t count,
+				   const uint8_t *const data);
+
+static inline int z_impl_swdp_output_sequence(const struct device *dev,
+					      const uint32_t count,
+					      const uint8_t *const data)
+{
+	const struct swdp_api *api = dev->api;
+
+	return api->swdp_output_sequence(dev, count, data);
+}
+
+/**
+ * @brief Read count bits from SWDIO into data LSB first
+ *
+ * @param dev SWDP device
+ * @param count Number of bits to read
+ * @param buf Buffer to store bits read
+ * @return 0 on success, or error code
+ */
+__syscall int swdp_input_sequence(const struct device *dev,
+				  const uint32_t count,
+				  uint8_t *const buf);
+
+static inline int z_impl_swdp_input_sequence(const struct device *dev,
+					     const uint32_t count,
+					     uint8_t *const buf)
+{
+	const struct swdp_api *api = dev->api;
+
+	return api->swdp_input_sequence(dev, count, buf);
+}
+
+/**
+ * @brief Perform SWDP transfer and store response
+ *
+ * Request must be in the form APnDP | RnW | A[2:3], without start, stop, or parity bits.
+ * The driver may ignore idle_cycles when the SWDCLK is always clocked.
+ *
+ * @param dev SWDP device
+ * @param request SWDP request bits
+ * @param data Data to be transferred with request
+ * @param idle_cycles Idle cycles between request and response
+ * @param response Buffer to store response (ACK/WAIT/FAULT)
+ * @return 0 on success, or error code
+ */
+__syscall int swdp_transfer(const struct device *dev,
+			    const uint8_t request,
+			    uint32_t *const data,
+			    const uint8_t idle_cycles,
+			    uint8_t *const response);
+
+static inline int z_impl_swdp_transfer(const struct device *dev,
+				       const uint8_t request,
+				       uint32_t *const data,
+				       const uint8_t idle_cycles,
+				       uint8_t *const response)
+{
+	const struct swdp_api *api = dev->api;
+
+	return api->swdp_transfer(dev, request, data, idle_cycles, response);
+}
+
+/**
+ * @brief Set SWCLK, SWDPIO, and nRESET pins state
+ * @note The bit positions are defined by the SWDP_*_PIN macros.
+ *
+ * @param dev SWDP device
+ * @param pins Bitmask of pins to set
+ * @param value Value to set pins to
+ * @return 0 on success, or error code
+ */
+__syscall int swdp_set_pins(const struct device *dev,
+			    const uint8_t pins,
+			    const uint8_t value);
+
+static inline int z_impl_swdp_set_pins(const struct device *dev,
+				       const uint8_t pins,
+				       const uint8_t value)
+{
+	const struct swdp_api *api = dev->api;
+
+	return api->swdp_set_pins(dev, pins, value);
+}
+
+/**
+ * @brief Get SWCLK, SWDPIO, and nRESET pins state
+ * @note The bit positions are defined by the SWDP_*_PIN macros.
+ *
+ * @param dev SWDP device
+ * @param state Place to store pins state
+ * @return 0 on success, or error code
+ */
+__syscall int swdp_get_pins(const struct device *dev,
+			    uint8_t *const state);
+
+static inline int z_impl_swdp_get_pins(const struct device *dev,
+				       uint8_t *const state)
+{
+	const struct swdp_api *api = dev->api;
+
+	return api->swdp_get_pins(dev, state);
+}
+
+/**
+ * @brief Set SWDP clock frequency
+ *
+ * @param dev SWDP device
+ * @param clock Clock frequency in Hz
+ * @return 0 on success, or error code
+ */
+__syscall int swdp_set_clock(const struct device *dev,
+			     const uint32_t clock);
+
+static inline int z_impl_swdp_set_clock(const struct device *dev,
+					 const uint32_t clock)
+{
+	const struct swdp_api *api = dev->api;
+
+	return api->swdp_set_clock(dev, clock);
+}
+
+/**
+ * @brief Enable interface, set pins to default state
+ *
+ * @note SWDPIO is set to output mode, SWCLK and nRESET are set to high level.
+ *
+ * @param dev SWDP device
+ * @return 0 on success, or error code
+ */
+__syscall int swdp_port_on(const struct device *dev);
+
+static inline int z_impl_swdp_port_on(const struct device *dev)
+{
+	const struct swdp_api *api = dev->api;
+
+	return api->swdp_port_on(dev);
+}
+
+/**
+ * @brief Disable interface, set pins to High-Z mode
+ *
+ * @param dev SWDP device
+ * @return 0 on success, or error code
+ */
+__syscall int swdp_port_off(const struct device *dev);
+
+static inline int z_impl_swdp_port_off(const struct device *dev)
+{
+	const struct swdp_api *api = dev->api;
+
+	return api->swdp_port_off(dev);
+}
+
+/**
+ * @brief Configure SWDP interface
+ *
+ * @param dev SWDP device
+ * @param turnaround Line turnaround cycles
+ * @param data_phase Always generate Data Phase (also on WAIT/FAULT)
+ * @return 0 on success, or error code
+ */
+__syscall int swdp_configure(const struct device *dev,
+			     const uint8_t turnaround,
+			     const bool data_phase);
+
+static inline int z_impl_swdp_configure(const struct device *dev,
+					const uint8_t turnaround,
+					const bool data_phase)
+{
+	const struct swdp_api *api = dev->api;
+
+	return api->swdp_configure(dev, turnaround, data_phase);
+}
+
 #ifdef __cplusplus
 }
 #endif
 
 /** @} */
+
+#include <zephyr/syscalls/swdp.h>
 
 #endif /* ZEPHYR_INCLUDE_SWDP_H_ */
