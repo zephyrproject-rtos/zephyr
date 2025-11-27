9f9ea79c7f88dca98e235141e068d6d1dd13df11
Martin Hoff <martin.hoff@silabs.com>
2025-11-27 11:05:47 +0100

    tests: drivers: dma: change power-state checks in dma loop test
    This patch allows a DMA device that is part of a power domain to start
    the test in a suspended power-management state. If another device makes
    the power domain active (for example, the console UART), the DMA device
    within the same power domain may enter a suspended state.
    The test will now verify that the device is suspended/off before the
    test begins, active during the test, and restored to its initial state
    at the end.
    
    Signed-off-by: Martin Hoff <martin.hoff@silabs.com>

--- tests/drivers/dma/loop_transfer/src/test_dma_loop.c
+++ tests/drivers/dma/loop_transfer/src/test_dma_loop.c
@@ -360,8 +360,12 @@ static bool check_dev_power_state(const struct device *dev, enum pm_device_state
 static int test_loop_repeated_start_stop(const struct device *dma)
 {
 	static int chan_id;
-	enum pm_device_state init_state = pm_device_on_power_domain(dma) ?
-					  PM_DEVICE_STATE_OFF : PM_DEVICE_STATE_SUSPENDED;
+
+	if (!check_dev_power_state(dma, PM_DEVICE_STATE_SUSPENDED) &&
+	    !check_dev_power_state(dma, PM_DEVICE_STATE_OFF)) {
+		TC_PRINT("ERROR: device %s is not in the correct init power state", dma->name);
+		return TC_FAIL;
+	}
 
 	test_case_id = 0;
 	TC_PRINT("DMA memory to memory transfer started\n");
@@ -394,10 +398,6 @@ static int test_loop_repeated_start_stop(const struct device *dma)
 	dma_cfg.dma_slot = CONFIG_DMA_MCUX_TEST_SLOT_START;
 #endif
 
-	if (!check_dev_power_state(dma, PM_DEVICE_STATE_OFF)) {
-		return TC_FAIL;
-	}
-
 	chan_id = dma_request_channel(dma, NULL);
 	if (chan_id < 0) {
 		TC_PRINT("this platform do not support the dma channel\n");
@@ -425,7 +425,9 @@ static int test_loop_repeated_start_stop(const struct device *dma)
 		return TC_FAIL;
 	}
 
-	if (!check_dev_power_state(dma, init_state)) {
+	if (!check_dev_power_state(dma, PM_DEVICE_STATE_SUSPENDED) &&
+	    !check_dev_power_state(dma, PM_DEVICE_STATE_OFF)) {
+		TC_PRINT("ERROR: device %s is not in the correct power state", dma->name);
 		return TC_FAIL;
 	}
 
@@ -465,7 +467,9 @@ static int test_loop_repeated_start_stop(const struct device *dma)
 		return TC_FAIL;
 	}
 
-	if (!check_dev_power_state(dma, init_state)) {
+	if (!check_dev_power_state(dma, PM_DEVICE_STATE_SUSPENDED) &&
+	    !check_dev_power_state(dma, PM_DEVICE_STATE_OFF)) {
+		TC_PRINT("ERROR: device %s is not in the correct power state", dma->name);
 		return TC_FAIL;
 	}
 
