f71d52c4ddf565bb0c4db20af085a2fd0c3247b6
Triveni Danda <triveni.danda@nordicsemi.no>
2025-11-28 14:33:47 +0530

    drivers: wifi: nrf_wifi: Fix invalid pointer access
    Fix pointer dereferencing by accessing the pointer only
    after initialization, preventing fault exceptions.
    
    Signed-off-by: Triveni Danda <triveni.danda@nordicsemi.no>

--- drivers/wifi/nrf_wifi/src/wifi_mgmt.c
+++ drivers/wifi/nrf_wifi/src/wifi_mgmt.c
@@ -875,18 +875,6 @@ int nrf_wifi_channel(const struct device *dev,
 		return ret;
 	}
 
-	for (i = 0; i < MAX_PEERS; i++) {
-		peer = &sys_dev_ctx->tx_config.peers[i];
-		if (peer->peer_id == -1) {
-			continue;
-		}
-		if (peer->authorized) {
-			LOG_ERR("%s: Cannot change channel when in station connected mode",
-				__func__);
-			return ret;
-		}
-	}
-
 	rpu_ctx_zep = vif_ctx_zep->rpu_ctx_zep;
 	if (!rpu_ctx_zep) {
 		LOG_ERR("%s: rpu_ctx_zep is NULL", __func__);
@@ -901,6 +889,18 @@ int nrf_wifi_channel(const struct device *dev,
 
 	fmac_dev_ctx = rpu_ctx_zep->rpu_ctx;
 	sys_dev_ctx = wifi_dev_priv(fmac_dev_ctx);
+
+	for (i = 0; i < MAX_PEERS; i++) {
+		peer = &sys_dev_ctx->tx_config.peers[i];
+		if (peer->peer_id == -1) {
+			continue;
+		}
+		if (peer->authorized) {
+			LOG_ERR("%s: Cannot change channel when in station connected mode",
+				__func__);
+			return ret;
+		}
+	}
 
 	if (channel->oper == WIFI_MGMT_SET) {
 		/**
