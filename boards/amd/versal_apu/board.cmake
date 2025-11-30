e30482736897bc3884536954e5d3f156c9d70229
Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
2025-11-30 14:14:28 +0530

    boards: amd: Add generic board support for Versal APU
    Add generic board support for the AMD Versal APU platform. This enables
    basic application development on Versal APU cores using standard board
    configuration.
    
    Includes board definition files, defconfig, and device tree integration
    for the Versal APU board.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>

---
+++ boards/amd/versal_apu/board.cmake
@@ -0,0 +1,20 @@
+# Copyright (c) 2025, Advanced Micro Devices, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+include(${ZEPHYR_BASE}/boards/common/xsdb.board.cmake)
+set(SUPPORTED_EMU_PLATFORMS qemu)
+set(QEMU_ARCH xilinx-aarch64)
+set(QEMU_CPU_TYPE_${ARCH} cortexa72)
+
+set(QEMU_FLAGS_${ARCH}
+    -machine arm-generic-fdt
+    -hw-dtb ${PROJECT_BINARY_DIR}/${BOARD}-qemu.dtb
+    -device loader,addr=0xFD1A0300,data=0x8000000e,data-len=4
+    -nographic
+    -m 2g
+)
+
+set(QEMU_KERNEL_OPTION
+    "-device;loader,cpu-num=0,file=\$<TARGET_FILE:\${logical_target_for_zephyr_elf}>"
+)
