a859f4d4f49d425bc34d43d1943114e537a27306
Alain Volmat <alain.volmat@foss.st.com>
2025-12-02 10:29:19 +0100

    soc: stm32n6x: addition of RIF configuration at soc init
    Perform all the IPs RIF configuration at init time in case of this
    isn't yet performed by an earlier entity during the boot sequence.
    This configuration depends on TRUSTED_EXECUTION_SECURE which is
    currently always enabled on STM32N6x but will be configurable in
    future.
    
    Signed-off-by: Alain Volmat <alain.volmat@foss.st.com>

--- soc/st/stm32/stm32n6x/soc.c
+++ soc/st/stm32/stm32n6x/soc.c
@@ -34,6 +34,46 @@ void soc_reset_hook(void)
 }
 #endif
 
+#define RIF_MASTER_CID1_SEC_PRIV(device)	\
+	do {										\
+		RIMC_MasterConfig_t rimc = {						\
+			.MasterCID = RIF_CID_1,						\
+			.SecPriv = RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV,		\
+		};									\
+		HAL_RIF_RIMC_ConfigMasterAttributes(RIF_MASTER_INDEX_##device, &rimc);	\
+	} while (0)
+
+#define RIF_SLAVE_SEC_PRIV(device)	\
+	HAL_RIF_RISC_SetSlaveSecureAttributes(RIF_RISC_PERIPH_INDEX_##device,		\
+					      RIF_ATTRIBUTE_SEC | RIF_ATTRIBUTE_PRIV)
+
+static void soc_rif_config(void)
+{
+	/* Enable the clock for the RIFSC (RIF Security Controller) */
+	__HAL_RCC_RIFSC_CLK_ENABLE();
+
+	/* DCMIPP */
+	RIF_MASTER_CID1_SEC_PRIV(DCMIPP);
+	RIF_SLAVE_SEC_PRIV(DCMIPP);
+	/* DMA2D */
+	RIF_MASTER_CID1_SEC_PRIV(DMA2D);
+	RIF_SLAVE_SEC_PRIV(DMA2D);
+	/* ETH */
+	RIF_MASTER_CID1_SEC_PRIV(ETH1);
+	RIF_SLAVE_SEC_PRIV(ETH1);
+	/* JPEG */
+	RIF_SLAVE_SEC_PRIV(JPEG);
+	/* LTDC Layer 1 */
+	RIF_MASTER_CID1_SEC_PRIV(LTDC1);
+	RIF_SLAVE_SEC_PRIV(LTDCL1);
+	/* NPU */
+	RIF_MASTER_CID1_SEC_PRIV(NPU);
+	RIF_SLAVE_SEC_PRIV(NPU);
+	/* VENC */
+	RIF_MASTER_CID1_SEC_PRIV(VENC);
+	RIF_SLAVE_SEC_PRIV(VENC);
+}
+
 /**
  * @brief Perform basic hardware initialization at boot.
  *
@@ -66,4 +106,9 @@ void soc_early_init_hook(void)
 	/* Set Vdd IO2 and IO3 to 1.8V */
 	LL_PWR_SetVddIO2VoltageRange(LL_PWR_VDDIO_VOLTAGE_RANGE_1V8);
 	LL_PWR_SetVddIO3VoltageRange(LL_PWR_VDDIO_VOLTAGE_RANGE_1V8);
+
+	/* RIF configuration */
+	if (IS_ENABLED(CONFIG_STM32N6_RIF_OPEN)) {
+		soc_rif_config();
+	}
 }
