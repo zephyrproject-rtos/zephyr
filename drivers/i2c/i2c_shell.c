685047058784782413280502d1f259f4161aa975
Fabio Baltieri <fabiobaltieri@google.com>
2025-12-02 14:01:33 +0000

    i2c: shell: make the buffer configurable
    Add a Kconfig option to tweak the i2c shell command buffer size,
    similarly to other shell commands.
    
    This is allocated on the stack so add a note about it.
    
    Signed-off-by: Fabio Baltieri <fabiobaltieri@google.com>

--- drivers/i2c/i2c_shell.c
+++ drivers/i2c/i2c_shell.c
@@ -21,7 +21,7 @@ LOG_MODULE_REGISTER(i2c_shell, CONFIG_LOG_DEFAULT_LEVEL);
 #define ARGV_REG	3
 
 /* Maximum bytes we can write or read at once */
-#define MAX_I2C_BYTES	16
+#define MAX_I2C_BYTES	CONFIG_I2C_SHELL_BUFFER_SIZE
 
 static int get_bytes_count_for_hex(char *arg)
 {
