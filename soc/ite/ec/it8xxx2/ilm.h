a214a05b8c12e5219cdc2221ed9a0a799e5ec056
Tim Lin <tim2.lin@ite.corp-partner.google.com>
2025-12-04 17:27:22 +0800

    soc/ite: it8xxx2: Add SOC_IT8XXX2_RAM_CODE_NOINLINE option
    Add the SOC_IT8XXX2_RAM_CODE_NOINLINE Kconfig option to prevent
    functions marked with __soc_ram_code from being inlined when LTO
    is enabled.
    
    This ensures RAM code functions remain in the RAM section instead of
    being merged into callers by the compiler.
    
    Signed-off-by: Tim Lin <tim2.lin@ite.corp-partner.google.com>

--- soc/ite/ec/it8xxx2/ilm.h
+++ soc/ite/ec/it8xxx2/ilm.h
@@ -7,8 +7,11 @@
 #include <stdbool.h>
 
 /* Places code in the section that gets mapped into ILM */
+#ifdef CONFIG_SOC_IT8XXX2_RAM_CODE_NOINLINE
+#define __soc_ram_code __attribute__((noinline)) __attribute__((section(".__ram_code")))
+#else
 #define __soc_ram_code __attribute__((section(".__ram_code")))
-
+#endif
 
 #ifndef _ASMLANGUAGE
 bool it8xxx2_is_ilm_configured(void);
