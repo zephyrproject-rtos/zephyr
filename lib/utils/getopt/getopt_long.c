34a3aa1ad23f8abdf76594311e8193b011895862
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2025-12-09 10:56:12 +0100

    sys: util: Rename SIGN(), gcd() and lcm() utilities with sys_ prefix
    These common functions were just added in
    646f25537331821f4c0df0ff4a2a7c3f7bf3b961
    1f31da3398582d577e314fa22deae381e3a76980
    but our guidelines tell us to not do this, to avoid
    conflicts with other libraries and applications code
    https://docs.zephyrproject.org/latest/contribute/style/
    naming.html#public-symbol-prefixes
    
    Let's rename this to avoid these to follow our guidelines and avoid
    this issues.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- lib/utils/getopt/getopt_long.c
+++ lib/utils/getopt/getopt_long.c
@@ -120,7 +120,7 @@ static void permute_args(int panonopt_start, int panonopt_end, int opt_end, char
 	 */
 	nnonopts = panonopt_end - panonopt_start;
 	nopts = opt_end - panonopt_end;
-	ncycle = gcd(nnonopts, nopts);
+	ncycle = sys_gcd(nnonopts, nopts);
 	cyclelen = (opt_end - panonopt_start) / ncycle;
 
 	for (int i = 0; i < ncycle; i++) {
