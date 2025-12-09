fae99b1c29ef3a56029ed77c48872fa2d7e9446a
Jiafei Pan <Jiafei.Pan@nxp.com>
2025-12-09 17:00:01 +0800

    boards: frdm_imx91: add spsdk runner support
    Added SPSDK runner support, it could be used to flash Zephyr image.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

---
+++ boards/nxp/frdm_imx91/board.cmake
@@ -0,0 +1,17 @@
+#
+# Copyright 2026 NXP
+#
+# SPDX-License-Identifier: Apache-2.0
+
+if(CONFIG_BOARD_NXP_SPSDK_IMAGE OR (DEFINED ENV{USE_NXP_SPSDK_IMAGE}
+  AND "$ENV{USE_NXP_SPSDK_IMAGE}" STREQUAL "y"))
+  board_set_flasher_ifnset(spsdk)
+
+  board_runner_args(spsdk "--family=mimx9131")
+  # reuse evk board's bootloader
+  board_runner_args(spsdk "--bootloader=${CMAKE_BINARY_DIR}/zephyr/imx91evk-boot-firmware-6.12.34-2.1.0/imx-boot-imx91evk-sd.bin-flash_singleboot")
+  board_runner_args(spsdk "--flashbin=${CMAKE_BINARY_DIR}/zephyr/flash.bin")
+  board_runner_args(spsdk "--containers=one")
+
+  include(${ZEPHYR_BASE}/boards/common/spsdk.board.cmake)
+endif()
