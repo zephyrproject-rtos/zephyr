aa455f295c5a7035ad19a888b30a80a095a50885
Pascal Linder <pascal.linder@zuehlke.com>
2025-12-09 15:54:25 +0100

    drivers: flash: simulator: Add extended operations
    NAND flash translation layers require the flash extended operations API
    for bad block management. Add it here without implementation to test an
    FTL on the flash simulator.
    
    Signed-off-by: Pascal Linder <pascal.linder@zuehlke.com>

--- drivers/flash/flash_simulator.c
+++ drivers/flash/flash_simulator.c
@@ -10,6 +10,7 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/linker/devicetree_regions.h>
 #include <zephyr/drivers/flash.h>
+#include <zephyr/drivers/flash/nand_flash_api_ex.h>
 #include <zephyr/init.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/util.h>
@@ -380,6 +381,18 @@ flash_sim_get_parameters(const struct device *dev)
 	return &flash_sim_parameters;
 }
 
+#ifdef CONFIG_FLASH_EX_OP_ENABLED
+int flash_sim_ex_op(const struct device *dev, uint16_t code, const uintptr_t in, void *out)
+{
+	ARG_UNUSED(dev);
+	ARG_UNUSED(code);
+	ARG_UNUSED(in);
+	ARG_UNUSED(out);
+
+	return -ENOTSUP;
+}
+#endif /* CONFIG_FLASH_EX_OP_ENABLED */
+
 static DEVICE_API(flash, flash_sim_api) = {
 	.read = flash_sim_read,
 	.write = flash_sim_write,
@@ -389,6 +402,9 @@ static DEVICE_API(flash, flash_sim_api) = {
 #ifdef CONFIG_FLASH_PAGE_LAYOUT
 	.page_layout = flash_sim_page_layout,
 #endif
+#ifdef CONFIG_FLASH_EX_OP_ENABLED
+	.ex_op = flash_sim_ex_op,
+#endif /* CONFIG_FLASH_EX_OP_ENABLED */
 };
 
 #ifdef CONFIG_ARCH_POSIX
