e2f4fc15aff6b55fd912596f748595a829ea94a7
Jérôme Pouiller <jerome.pouiller@silabs.com>
2025-12-10 20:36:17 +0100

    drivers: adc: siwx91x: Fix use of ADC_CONTEXT_USES_KERNEL_TIMER
    ADC_CONTEXT_USES_KERNEL_TIMER is required for ADC_CONTEXT_INIT_TIMER().
    However, it should be defined in the Zephry driver, not in the Wiseconnect
    integration.
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- drivers/adc/adc_silabs_siwx91x.c
+++ drivers/adc/adc_silabs_siwx91x.c
@@ -3,8 +3,8 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
 #define DT_DRV_COMPAT silabs_siwx91x_adc
+#define ADC_CONTEXT_USES_KERNEL_TIMER
 
 #include <zephyr/drivers/adc.h>
 #include <zephyr/logging/log.h>
@@ -12,6 +12,7 @@
 #include <zephyr/drivers/clock_control.h>
 #include <zephyr/irq.h>
 
+#include "adc_context.h"
 #include "rsi_adc.h"
 #include "rsi_bod.h"
 #include "rsi_ipmu.h"
@@ -19,8 +20,6 @@
 #include "aux_reference_volt_config.h"
 
 LOG_MODULE_REGISTER(adc_silabs_siwx91x, CONFIG_ADC_LOG_LEVEL);
-
-#include "adc_context.h"
 
 struct adc_siwx91x_chan_data {
 	uint8_t input_type;
