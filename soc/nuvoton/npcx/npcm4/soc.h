5f3e71bb4eaac1f2c9d73572dbc20451877e11d6
Joseph Liu <kwliu@nuvoton.com>
2025-12-10 18:07:32 +0800

    soc: nuvoton: npcm4: add register and devicetree support
    Flesh out the npcm4 SoC support after its relocation under the npcx
    family. Add the npcm4 devicetree peripheral, pin-control, MIWU, LVOL
    and alternate-function maps, the npcmn register and clock definitions
    (reg_def.h, clock_def.h, registers.c), the expanded soc.h with the
    register offsets, and the corresponding board defconfig and clock
    control wiring.
    
    Also correct all alternate-function pin assignments for npcm4.
    
    The npcmn MIWU register offsets follow the npcx7 layout documented in
    the NPCM400 datasheet (NPCM400F spec, section 4.3.3): WKEDG and
    WKAEDG are interleaved in pairs, the pending and clear registers are
    four bytes apart and WKMOD starts at offset 70h.
    
    Signed-off-by: Joseph Liu <kwliu@nuvoton.com>
    Signed-off-by: Tim Lee <CHLi30@nuvoton.com>

--- soc/nuvoton/npcx/npcm4/soc.h
+++ soc/nuvoton/npcx/npcm4/soc.h
@@ -7,12 +7,74 @@
 #ifndef _NUVOTON_NPCM_SOC_H_
 #define _NUVOTON_NPCM_SOC_H_
 
-/* CMSIS required definitions */
-#define __FPU_PRESENT CONFIG_CPU_HAS_FPU
-#define __MPU_PRESENT CONFIG_CPU_HAS_ARM_MPU
+#include <cmsis_core_m_defaults.h>
+
+/* NPCM4 SCFG multi-registers */
+#define NPCX_DEVALT_OFFSET(n)      (0x010 + n)
+#define NPCX_PUPD_EN_OFFSET(n)     (0x02b + n)
+#define NPCX_LV_GPIO_CTL_OFFSET(n) (0x150 + n)
+#define NPCX_DEVALT_LK_OFFSET(n)   (0x210 + n)
+
+/* NPCM4 MIWU multi-registers */
+#define NPCX_WKEDG_OFFSET(n)  (0x000 + (n * 2) + ((n < 5) ? 0 : 0x01e))
+#define NPCX_WKAEDG_OFFSET(n) (0x001 + (n * 2) + ((n < 5) ? 0 : 0x01e))
+#define NPCX_WKMOD_OFFSET(n)  (0x070 + (n))
+#define NPCX_WKPND_OFFSET(n)  (0x00a + (n * 4) + ((n < 5) ? 0 : 0x010))
+#define NPCX_WKPCL_OFFSET(n)  (0x00c + (n * 4) + ((n < 5) ? 0 : 0x010))
+#define NPCX_WKEN_OFFSET(n)   (0x01e + (n * 2) + ((n < 5) ? 0 : 0x012))
+#define NPCX_WKINEN_OFFSET(n) (0x01f + (n * 2) + ((n < 5) ? 0 : 0x012))
+
+/* NPCM4 PMC multi-registers */
+#define NPCX_PWDWN_CTL_OFFSET(n) (((n - 1) < 6) ? (0x008 + (n - 1)) : (0x01e + (n - 1)))
+
+/* NPCM4 ADC multi-registers */
+#define NPCX_CHNDAT_OFFSET(n) (0x040 + n * 2)
+#define NPCX_THRCTL_OFFSET(n) (0x080 + n * 2)
+#define NPCX_THEN_OFFSET      0x090
+#define THEN(base)            (*(volatile uint16_t *)(base + NPCX_THEN_OFFSET))
+
+/* NPCM4 ADC register fields */
+#define NPCX_THRCTL_L_H    15
+#define NPCX_THRCTL_CHNSEL FIELD(10, 5)
+#define NPCX_THRCTL_THRVAL FIELD(0, 10)
+
+/* NPCM4 FIU register fields */
+#define NPCX_FIU_EXT_CFG_SPI1_2DEV   6
+#define NPCX_FIU_EXT_CFG_LOW_DEV_NUM 7
+
+/* NPCM4 supported group mask of DEVALT_LK */
+#define NPCX_DEVALT_LK_GROUP_MASK                                                                  \
+	(BIT(0) | BIT(2) | BIT(3) | BIT(4) | BIT(5) | BIT(6) | BIT(11) | BIT(13) | BIT(15) |       \
+	 BIT(16) | BIT(17) | BIT(18) | BIT(19) | BIT(21)) /* DEVALT0_LK - DEVALTN_LK */
+
+/* NPCM4 Clock Configuration */
+#define MAX_OFMCLK 96000000
 
 /* Add include for DTS generated information */
 #include <zephyr/devicetree.h>
-#include <cmsis_core_m_defaults.h>
+
+#include "reg_def.h"
+#include "clock_def.h"
+#include <soc_dt.h>
+#include <soc_pins.h>
+#include <soc_power.h>
+
+/* NPCM4 Clock prescaler configurations */
+#define VAL_HFCGP ((FPRED_VAL << 4) | AHB6DIV_VAL)
+
+#if defined(FIU1DIV_VAL)
+#define VAL_HFCBCD ((FIU1DIV_VAL << 4) | (FIUDIV_VAL << 2))
+#else
+#define VAL_HFCBCD (FIUDIV_VAL << 2)
+#endif
+
+#define VAL_HFCBCD1 (APB1DIV_VAL | (APB2DIV_VAL << 4))
+
+#ifndef APB4DIV_VAL
+#define APB4DIV_VAL 0
+#endif
+#define VAL_HFCBCD2 (APB3DIV_VAL | (APB4DIV_VAL << 4))
+
+#define VAL_HFCBCD3 MCLKD_SL /* Used by I3C1/2/3 modules */
 
 #endif /* _NUVOTON_NPCM_SOC_H_ */
