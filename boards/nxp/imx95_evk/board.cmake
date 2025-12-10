1285fc6b9e66f00282c45fd3a54820fee5685b86
Jiafei Pan <Jiafei.Pan@nxp.com>
2025-12-10 10:47:23 +0800

    boards: imx95_evk: update boot firmwire for b0 silicon
    Updated boot firmware for b0 silicon according to SPSDK blob's
    update.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- boards/nxp/imx95_evk/board.cmake
+++ boards/nxp/imx95_evk/board.cmake
@@ -5,7 +5,7 @@ if(CONFIG_BOARD_NXP_SPSDK_IMAGE OR (DEFINED ENV{USE_NXP_SPSDK_IMAGE}
   board_set_flasher_ifnset(spsdk)
 
   board_runner_args(spsdk "--family=mimx9596")
-  board_runner_args(spsdk "--bootloader=${CMAKE_BINARY_DIR}/zephyr/imx95-19x19-lpddr5-evk-boot-firmware-0.1/imx-boot-imx95-19x19-lpddr5-evk-sd.bin-flash_all")
+  board_runner_args(spsdk "--bootloader=${CMAKE_BINARY_DIR}/zephyr/imx95-evk-boot-firmware-6.12.34-2.1.0/imx-boot-imx95-19x19-lpddr5-evk-sd.bin-flash_all")
   board_runner_args(spsdk "--flashbin=${CMAKE_BINARY_DIR}/zephyr/flash.bin")
   if(CONFIG_CPU_CORTEX_A55)
     board_runner_args(spsdk "--containers=two")
