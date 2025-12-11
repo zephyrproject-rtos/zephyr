37b8fd62e791c16ffaeed80fa1b0755c94f59e61
Cristian Bulacu <cristian.bulacu@nxp.com>
2025-12-11 17:08:32 +0200

    openthread: platform: Fix compile warnings related to NAT64 or IPV4
    In this commit, for Border Router application, added an extra check for
    `CONFIG_NET_IPV4` when checking for
    `CONFIG_OPENTHREAD_NAT64_TRANSLATOR`.
    Fixed some compile warnings in `mdns_socket.c` when IPV4 is not enabled,
    by moving some variables under `CONFIG_NET_IPV4` define
    
    Signed-off-by: Cristian Bulacu <cristian.bulacu@nxp.com>

--- modules/openthread/platform/infra_if.c
+++ modules/openthread/platform/infra_if.c
@@ -24,11 +24,11 @@
 #include <zephyr/net/icmp.h>
 #include <icmpv6.h>
 
-#if defined(CONFIG_OPENTHREAD_NAT64_TRANSLATOR)
+#if defined(CONFIG_OPENTHREAD_NAT64_TRANSLATOR) && defined(CONFIG_NET_IPV4)
 #include <zephyr/net/icmp.h>
 #include <zephyr/net/net_pkt_filter.h>
 #include <openthread/nat64.h>
-#endif /* CONFIG_OPENTHREAD_NAT64_TRANSLATOR */
+#endif /* CONFIG_OPENTHREAD_NAT64_TRANSLATOR && CONFIG_NET_IPV4 */
 
 static struct otInstance *ot_instance;
 static struct net_if *ail_iface_ptr;
@@ -40,7 +40,7 @@ static struct net_in6_addr mcast_addr;
 
 static void infra_if_handle_backbone_icmp6(struct otbr_msg_ctx *msg_ctx_ptr);
 static void handle_ra_from_ot(const uint8_t *buffer, uint16_t buffer_length);
-#if defined(CONFIG_OPENTHREAD_NAT64_TRANSLATOR)
+#if defined(CONFIG_OPENTHREAD_NAT64_TRANSLATOR) && defined(CONFIG_NET_IPV4)
 #define MAX_SERVICES CONFIG_OPENTHREAD_ZEPHYR_BORDER_ROUTER_NAT64_SERVICES
 
 static struct zsock_pollfd sockfd_raw[MAX_SERVICES];
@@ -157,9 +157,11 @@ otError infra_if_init(otInstance *instance, struct net_if *ail_iface)
 
 	VerifyOrExit((ret == 0 || ret == -EALREADY), error = OT_ERROR_FAILED);
 
+#if defined(CONFIG_OPENTHREAD_NAT64_TRANSLATOR) && defined(CONFIG_NET_IPV4)
 	for (uint8_t i = 0; i < MAX_SERVICES; i++) {
 		sockfd_raw[i].fd = -1;
 	}
+#endif /* CONFIG_OPENTHREAD_NAT64_TRANSLATOR && CONFIG_NET_IPV4 */
 exit:
 	return error;
 }
@@ -173,6 +175,17 @@ otError infra_if_deinit(void)
 
 	VerifyOrExit(net_ipv6_mld_leave(ail_iface_ptr, &mcast_addr) == 0,
 		     error = OT_ERROR_FAILED);
+
+#if defined(CONFIG_OPENTHREAD_NAT64_TRANSLATOR) && defined(CONFIG_NET_IPV4)
+	VerifyOrExit(raw_infra_if_sock != -1);
+	VerifyOrExit(zsock_close(raw_infra_if_sock) == 0);
+
+	sockfd_raw[0].fd = -1;
+	raw_infra_if_sock = -1;
+
+	net_socket_service_register(&handle_infra_if_raw_recv, sockfd_raw,
+				    ARRAY_SIZE(sockfd_raw), NULL);
+#endif /* CONFIG_OPENTHREAD_NAT64_TRANSLATOR && CONFIG_NET_IPV4 */
 
 exit:
 	ail_iface_ptr = NULL;
@@ -308,7 +321,7 @@ void infra_if_stop_icmp6_listener(void)
 	(void)net_icmp_cleanup_ctx(&na_ctx);
 }
 
-#if defined(CONFIG_OPENTHREAD_NAT64_TRANSLATOR)
+#if defined(CONFIG_OPENTHREAD_NAT64_TRANSLATOR) && defined(CONFIG_NET_IPV4)
 otError infra_if_nat64_init(void)
 {
 	otError error = OT_ERROR_NONE;
@@ -434,7 +447,6 @@ static void remove_checksums_for_eth_offloading(uint8_t *buf, uint16_t len)
 
 static bool infra_if_nat64_try_consume_packet(struct npf_test *test, struct net_pkt *pkt)
 {
-	ARG_UNUSED(test);
 
 	struct net_buf *buf = NULL;
 	otMessage *message = NULL;
@@ -470,5 +482,4 @@ exit:
 	openthread_mutex_unlock();
 	return false;
 }
-
-#endif /* CONFIG_OPENTHREAD_NAT64_TRANSLATOR */
+#endif /* CONFIG_OPENTHREAD_NAT64_TRANSLATOR && CONFIG_NET_IPV4 */
