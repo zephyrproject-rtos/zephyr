7f1c7a926ca08a94a3b100de38d40501fc371482
Mathieu Choplain <mathieu.choplain-ext@st.com>
2025-12-11 15:22:44 +0100

    drivers: usb: common: stm32: pwr: add power disable support
    Add code to disable the USB power supply.
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain-ext@st.com>

--- drivers/usb/common/stm32/stm32_usb_common.h
+++ drivers/usb/common/stm32/stm32_usb_common.h
@@ -15,4 +15,13 @@
  */
 int stm32_usb_pwr_enable(void);
 
+/**
+ * @brief Configures the Power Controller to disable
+ * USB-related regulators/etc if no controller is
+ * still active (refcounted).
+ *
+ * @returns 0 on success, negative error code otherwise.
+ */
+int stm32_usb_pwr_disable(void);
+
 #endif /* ZEPHYR_DRIVERS_USB_COMMON_STM32_STM32_USB_COMMON_H_ */
