5a159e1e78dbe0d6329d319d567b4cef79049b51
Stefan Schmidt <kontakt@stefanschmidt-embedded.de>
2025-12-11 18:21:50 +0100

    drivers: sensor: adi: adxl345: Unit test proposal
    This PR adds two unit tests with the required mocks to test the conversion
    function in the adxl345 for right justified data, in 2G and 4G mode.
    
    To run this test invoke twister from zephyr root:
    "twister -T zephyr/drivers/sensor/adi/adxl345/"
    
    Signed-off-by: Stefan Schmidt <kontakt@stefanschmidt-embedded.de>

---
+++ drivers/sensor/adi/adxl345/unit_test/zephyr/syscalls/kobject.h
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: Copyright The Zephyr Project Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+static inline void k_object_access_grant(const void *object, struct k_thread *thread)
+{
+}
