d2b3f4521b853f7dfd79bdfcbfea9aecdc1979b1
Raffael Rostagno <raffael.rostagno@espressif.com>
2025-12-12 14:12:51 -0300

    samples: boards: esp32: deep_sleep: Update test
    Update test with PM developments.
    
    Signed-off-by: Raffael Rostagno <raffael.rostagno@espressif.com>

--- samples/boards/espressif/deep_sleep/src/main.c
+++ samples/boards/espressif/deep_sleep/src/main.c
@@ -1,38 +1,40 @@
 /*
- * Copyright (c) 2022 Espressif Systems (Shanghai) Co., Ltd.
+ * Copyright (c) 2022-2026 Espressif Systems (Shanghai) Co., Ltd.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/poweroff.h>
 #include <zephyr/drivers/retained_mem.h>
+#include <zephyr/pm/device.h>
+#include <zephyr/sys/poweroff.h>
 #include <esp_sleep.h>
-#if SOC_RTCIO_INPUT_OUTPUT_SUPPORTED
-#include <driver/rtc_io.h>
-#endif
 
 #include <esp_attr.h>
 
 #define RETAINED_MEM_CONST 0x01234567
 
-#ifdef CONFIG_EXAMPLE_EXT1_WAKEUP
-#ifdef CONFIG_SOC_SERIES_ESP32H2
-#define EXT_WAKEUP_PIN_1	(10)
-#define EXT_WAKEUP_PIN_2	(11)
-#else
-#define EXT_WAKEUP_PIN_1	(2)
-#define EXT_WAKEUP_PIN_2	(4)
-#endif
+#define WAKE_GPIOS DT_ALIAS(wakeup_gpios)
+
+#if !DT_NODE_HAS_STATUS_OKAY(WAKE_GPIOS)
+#error "Unsupported: wakeup-gpios node/alias not defined"
 #endif
 
-#ifdef CONFIG_EXAMPLE_GPIO_WAKEUP
-#if !DT_NODE_HAS_STATUS_OKAY(DT_ALIAS(wakeup_button))
-#error "Unsupported: wakeup-button alias is not defined"
-#else
-static const struct gpio_dt_spec wakeup_button = GPIO_DT_SPEC_GET(DT_ALIAS(wakeup_button), gpios);
-#endif
-#endif
+#define BUILD_GPIO_SPEC(node_id, prop, idx) GPIO_DT_SPEC_GET_BY_IDX(node_id, prop, idx),
+
+static const struct gpio_dt_spec wake_gpios[] = {
+	DT_FOREACH_PROP_ELEM(WAKE_GPIOS, gpios, BUILD_GPIO_SPEC)};
+
+void dev_enable_wakeup(const struct device *dev)
+{
+	if (!device_is_ready(dev)) {
+		printk("%s: device not ready\n", dev->name);
+	}
+
+	if (!pm_device_wakeup_enable(dev, true)) {
+		printk("%s: could not enable wakeup source\n", dev->name);
+	}
+}
 
 static void print_counter(__maybe_unused esp_sleep_wakeup_cause_t sleep_wakeup_cause)
 {
@@ -49,12 +51,8 @@ static void print_counter(__maybe_unused esp_sleep_wakeup_cause_t sleep_wakeup_c
 	}
 
 	switch (sleep_wakeup_cause) {
-#ifdef CONFIG_EXAMPLE_EXT1_WAKEUP
 	case ESP_SLEEP_WAKEUP_EXT1:
-#endif /* CONFIG_EXAMPLE_EXT1_WAKEUP */
-#ifdef CONFIG_EXAMPLE_GPIO_WAKEUP
 	case ESP_SLEEP_WAKEUP_GPIO:
-#endif /* CONFIG_EXAMPLE_GPIO_WAKEUP */
 	case ESP_SLEEP_WAKEUP_TIMER:
 		offset = 0;
 		err = retained_mem_read(retained_mem_device, offset, (uint8_t *)&aux, sizeof(aux));
@@ -107,16 +105,25 @@ static void print_counter(__maybe_unused esp_sleep_wakeup_cause_t sleep_wakeup_c
 
 int main(void)
 {
+	for (int i = 0; i < ARRAY_SIZE(wake_gpios); i++) {
+		const struct device *dev_gpio = wake_gpios[i].port;
+
+		dev_enable_wakeup(dev_gpio);
+	}
+
 	esp_sleep_wakeup_cause_t sleep_wakeup_cause = esp_sleep_get_wakeup_cause();
 
 	print_counter(sleep_wakeup_cause);
 
 	switch (sleep_wakeup_cause) {
-#ifdef CONFIG_EXAMPLE_EXT1_WAKEUP
 	case ESP_SLEEP_WAKEUP_EXT1:
-	{
-		uint64_t wakeup_pin_mask = esp_sleep_get_ext1_wakeup_status();
-
+	case ESP_SLEEP_WAKEUP_GPIO: {
+		uint64_t wakeup_pin_mask = 0;
+#ifdef SOC_PM_SUPPORT_EXT1_WAKEUP
+		wakeup_pin_mask = esp_sleep_get_ext1_wakeup_status();
+#elif defined(SOC_GPIO_SUPPORT_DEEPSLEEP_WAKEUP)
+		wakeup_pin_mask = esp_sleep_get_gpio_wakeup_status();
+#endif
 		if (wakeup_pin_mask != 0) {
 			int pin = __builtin_ffsll(wakeup_pin_mask) - 1;
 
@@ -126,22 +133,6 @@ int main(void)
 		}
 		break;
 	}
-#endif /* CONFIG_EXAMPLE_EXT1_WAKEUP */
-#ifdef CONFIG_EXAMPLE_GPIO_WAKEUP
-	case ESP_SLEEP_WAKEUP_GPIO:
-	{
-		uint64_t wakeup_pin_mask = esp_sleep_get_gpio_wakeup_status();
-
-		if (wakeup_pin_mask != 0) {
-			int pin = __builtin_ffsll(wakeup_pin_mask) - 1;
-
-			printk("Wake up from GPIO %d\n", pin);
-		} else {
-			printk("Wake up from GPIO\n");
-		}
-		break;
-	}
-#endif /* CONFIG_EXAMPLE_GPIO_WAKEUP */
 	case ESP_SLEEP_WAKEUP_TIMER:
 		printk("Wake up from timer.\n");
 		break;
@@ -152,47 +143,26 @@ int main(void)
 
 	k_busy_wait(1000000);
 
+	for (int i = 0; i < ARRAY_SIZE(wake_gpios); i++) {
+		const struct gpio_dt_spec *g = &wake_gpios[i];
+
+		int ret = gpio_pin_configure_dt(g, g->dt_flags | GPIO_INPUT);
+
+		if (ret != 0) {
+			printk("Error %d: failed to configure %s pin %d\n", ret, g->port->name,
+			       g->pin);
+			return 0;
+		}
+	}
+
 	const int wakeup_time_sec = CONFIG_EXAMPLE_WAKEUP_TIME_SEC;
 
 	printk("Enabling timer wakeup, %ds\n", wakeup_time_sec);
+
+	/* Since sys_poweroff() is being called directly, timer wakeup
+	 * must be programmed by application.
+	 */
 	esp_sleep_enable_timer_wakeup(wakeup_time_sec * 1000000);
-
-#ifdef CONFIG_EXAMPLE_EXT1_WAKEUP
-	const int ext_wakeup_pin_1 = EXT_WAKEUP_PIN_1;
-	const uint64_t ext_wakeup_pin_1_mask = BIT64(ext_wakeup_pin_1);
-	const int ext_wakeup_pin_2 = EXT_WAKEUP_PIN_2;
-	const uint64_t ext_wakeup_pin_2_mask = BIT64(ext_wakeup_pin_2);
-
-	printk("Enabling EXT1 wakeup on pins GPIO%d, GPIO%d\n",
-			ext_wakeup_pin_1, ext_wakeup_pin_2);
-	esp_sleep_enable_ext1_wakeup(ext_wakeup_pin_1_mask | ext_wakeup_pin_2_mask,
-			ESP_EXT1_WAKEUP_ANY_HIGH);
-
-#if SOC_RTCIO_INPUT_OUTPUT_SUPPORTED
-	/* enable pull-down on ext1 pins to avoid random wake-ups */
-	rtc_gpio_pullup_dis(EXT_WAKEUP_PIN_1);
-	rtc_gpio_pulldown_en(EXT_WAKEUP_PIN_1);
-	rtc_gpio_pullup_dis(EXT_WAKEUP_PIN_2);
-	rtc_gpio_pulldown_en(EXT_WAKEUP_PIN_2);
-#endif
-#endif /* CONFIG_EXAMPLE_EXT1_WAKEUP */
-#ifdef CONFIG_EXAMPLE_GPIO_WAKEUP
-	if (!gpio_is_ready_dt(&wakeup_button)) {
-		printk("Error: wakeup button device %s is not ready\n", wakeup_button.port->name);
-		return 0;
-	}
-
-	int ret = gpio_pin_configure_dt(&wakeup_button, GPIO_INPUT);
-
-	if (ret != 0) {
-		printk("Error %d: failed to configure %s pin %d\n",
-				ret, wakeup_button.port->name, wakeup_button.pin);
-		return 0;
-	}
-
-	esp_deep_sleep_enable_gpio_wakeup(BIT(wakeup_button.pin), ESP_GPIO_WAKEUP_GPIO_HIGH);
-	printk("Enabling GPIO wakeup on pins GPIO%d\n", wakeup_button.pin);
-#endif /* CONFIG_EXAMPLE_GPIO_WAKEUP */
 
 	printk("Powering off\n");
 	sys_poweroff();
