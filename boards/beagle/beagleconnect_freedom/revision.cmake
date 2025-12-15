d8eb5ac1a867de1b3a44610bccc4e5f1858b7244
Ayush Singh <ayush@beagleboard.org>
2025-12-15 20:56:03 +0530

    boards: beagle: beagleconnect_freedom: Make C7 default revision
    - This allows using beagleconnect_freedom/cc1352p7 as target instead of
      beagleconnect_freedom@C7/cc1352p7.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- boards/beagle/beagleconnect_freedom/revision.cmake
+++ boards/beagle/beagleconnect_freedom/revision.cmake
@@ -1,5 +1,9 @@
 set(BOARD_REVISIONS "C5" "C7")
 
+if(NOT DEFINED BOARD_REVISION)
+  set(BOARD_REVISION ${LIST_BOARD_REVISION_DEFAULT})
+endif()
+
 if(NOT BOARD_REVISION IN_LIST BOARD_REVISIONS)
   message(FATAL_ERROR "${BOARD_REVISION} is not a valid revision for BeagleConnect Freedom. Accepted revisions: ${BOARD_REVISIONS}")
 endif()
