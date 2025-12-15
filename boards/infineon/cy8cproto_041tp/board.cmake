e6fd157a93db7297583d7b93f9a74c0e0977bc95
Bill Waters <bill.waters@infineon.com>
2025-12-15 14:38:32 -0800

    boards: infineon: cy8cproto_041tp: board.cmake
    The openocd --config paramter is not needed.
    And it causes an error when used on a ci runner.
    The path created by
    scripts/west_commands/runners/openocd.py is wrong.
    
    Signed-off-by: Bill Waters <bill.waters@infineon.com>

--- boards/infineon/cy8cproto_041tp/board.cmake
+++ boards/infineon/cy8cproto_041tp/board.cmake
@@ -3,8 +3,5 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# OpenOCD cfg
-board_runner_args(openocd "--config=${ZEPHYR_BASE}/boards/infineon/cy8cproto_041tp/support/openocd.cfg")
-
 # Include standard OpenOCD runner helpers
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
