b9f10bec523e828366cb9db3c2ec1664fafb1bda
Ederson de Souza <ederson.desouza@intel.com>
2025-12-15 13:42:17 -0800

    samples/subsys/pmci/mctp: More friendly I2C-GPIO samples messages
    This is done by:
      - No more sending periodic messages from endpoint to owner - keep
        parity with UART sample: owner sends periodic ping to endpoint, that
        replies to those;
      - Waiting one second between each message sent by the owner, so people
        can actually see what's going on;
      - Wrapping the content of the messages (strings "ping" and "pong")
        in quotes, so it's clear what they are;
      - Toning down the logging to INF - no need for DBG.
    
    Signed-off-by: Ederson de Souza <ederson.desouza@intel.com>

--- samples/subsys/pmci/mctp/i2c_gpio_bus_endpoint/src/main.c
+++ samples/subsys/pmci/mctp/i2c_gpio_bus_endpoint/src/main.c
@@ -17,20 +17,26 @@ LOG_MODULE_REGISTER(mctp_i2c_gpio_bus_endpoint);
 
 MCTP_I2C_GPIO_TARGET_DT_DEFINE(mctp_i2c_ctrl, DT_NODELABEL(mctp_i2c));
 
-static void rx_message(uint8_t eid, bool tag_owner, uint8_t msg_tag, void *data, void *msg,
-		       size_t len)
-{
-	LOG_INF("received message \"%s\" from endpoint %d, msg_tag %d, len %zu", (char *)msg, eid,
-		msg_tag, len);
-}
+K_SEM_DEFINE(mctp_rx, 0, 1);
+
+struct mctp *mctp_ctx;
 
 #define BUS_OWNER_ID 20
 
+static void rx_message(uint8_t eid, bool tag_owner, uint8_t msg_tag, void *data, void *msg,
+		       size_t len)
+{
+	LOG_INF("received message \"%s\" from endpoint %d, replying with \"pong\"", (char *)msg,
+		eid);
+
+	mctp_message_tx(mctp_ctx, BUS_OWNER_ID, false, 0, "pong", sizeof("pong"));
+
+	k_sem_give(&mctp_rx);
+}
+
+
 int main(void)
 {
-	int rc;
-	struct mctp *mctp_ctx;
-
 	LOG_INF("MCTP Host EID:%d on %s\n", mctp_i2c_ctrl.endpoint_id, CONFIG_BOARD_TARGET);
 
 	mctp_set_alloc_ops(malloc, free, realloc);
@@ -40,16 +46,10 @@ int main(void)
 	mctp_set_rx_all(mctp_ctx, rx_message, NULL);
 
 	/*
-	 * 1. MCTP poll loop, send "ping" to each endpoint and get "pong" back
-	 * 2. Then send a broadcast "hello" to each endpoint and get a "from endoint %d" back
+	 * MCTP poll loop, listening for messages.
 	 */
 	while (true) {
-		rc = mctp_message_tx(mctp_ctx, BUS_OWNER_ID, false,
-				0, "ping", sizeof("ping"));
-		if (rc != 0) {
-			LOG_WRN("Failed to send message \"ping\", errno %d\n", rc);
-		}
-		k_msleep(500);
+		k_sem_take(&mctp_rx, K_FOREVER);
 	}
 
 	return 0;
