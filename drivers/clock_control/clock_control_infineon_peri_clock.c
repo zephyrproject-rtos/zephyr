1d7e07d04b242a6ddcd30350679dd3d76950b9c0
Brett Peterson <brett.peterson@infineon.com>
2025-12-17 14:18:05 -0800

    drivers: clock_control: supporting TDM as peri clock destination
    Adding TDM as a possible resource type. Adding support for fractional
    dividers. Improving error reporting.
    
    Signed-off-by: Brett Peterson <brett.peterson@infineon.com>

--- drivers/clock_control/clock_control_infineon_peri_clock.c
+++ drivers/clock_control/clock_control_infineon_peri_clock.c
@@ -17,7 +17,6 @@
 
 #include <infineon_kconfig.h>
 #include <zephyr/drivers/clock_control/clock_control_ifx_cat1.h>
-#include <zephyr/dt-bindings/clock/ifx_clock_source_common.h>
 
 #include <cy_sysclk.h>
 #include <cy_systick.h>
@@ -26,36 +25,10 @@ struct ifx_peri_clock_data {
 	struct ifx_cat1_resource_inst hw_resource;
 	struct ifx_cat1_clock clock;
 	uint16_t divider;
+	uint8_t frac_divider;
+	uint8_t div_type;
 	CySCB_Type *reg_addr;
 };
-
-#if defined(CY_IP_MXPERI) || defined(CY_IP_M0S8PERI)
-
-#define IFX_PCLK_GROUP(clkdst) 0
-#if (CY_IP_MXTCPWM_INSTANCES > 1) || (CY_IP_M0S8TCPWM_INSTANCES > 1)
-#define IFX_TCPWM0_PCLK_CLOCK0 PCLK_TCPWM0_CLOCKS0
-#define IFX_TCPWM1_PCLK_CLOCK0 PCLK_TCPWM1_CLOCKS0
-#else
-#define IFX_TCPWM0_PCLK_CLOCK0 PCLK_TCPWM_CLOCKS0
-#endif
-#define IFX_SCB0_PCLK_CLOCK PCLK_SCB0_CLOCK
-
-#elif defined(CY_IP_MXSPERI)
-
-#define IFX_PCLK_GROUP(clkdst) ((uint8_t)((uint32_t)(clkdst) >> 8))
-#define IFX_TCPWM0_PCLK_CLOCK0 PCLK_TCPWM0_CLOCK_COUNTER_EN0
-#define IFX_TCPWM1_PCLK_CLOCK0 PCLK_TCPWM0_CLOCK_COUNTER_EN256
-#define IFX_SCB0_PCLK_CLOCK    PCLK_SCB0_CLOCK_SCB_EN
-#define IFX_SCB1_PCLK_CLOCK    PCLK_SCB1_CLOCK_SCB_EN
-#define IFX_SCB5_PCLK_CLOCK    PCLK_SCB5_CLOCK_SCB_EN
-#endif
-
-#if defined(CY_IP_MXSDHC)
-#define IFX_SDHC0_PCLK_CLOCK PCLK_SDHC0_CLK_HF
-#define IFX_SDHC1_PCLK_CLOCK PCLK_SDHC1_CLK_HF
-#endif
-
-#define CLK_FRAC_DIV_MODE 0x02
 
 static inline en_clk_dst_t peri_pclk_build_en_clk_dst(uint8_t output, uint8_t group,
 						      uint8_t instance)
@@ -65,7 +38,7 @@ static inline en_clk_dst_t peri_pclk_build_en_clk_dst(uint8_t output, uint8_t gr
 	clk_dst = output;
 #if defined(COMPONENT_CAT1B) || defined(COMPONENT_CAT1C) || defined(CONFIG_SOC_FAMILY_INFINEON_EDGE)
 	/* These devices pack instance, group, and output together in the en_clk_dst_t.  Group and
-	 * Instance are used by the enable_divider and set_divider functionns to determine which
+	 * Instance are used by the enable_divider and set_divider functions to determine which
 	 * clock is being referenced.
 	 */
 	clk_dst |= ((uint32_t)group << PERI_PCLK_GR_NUM_Pos);
@@ -78,7 +51,6 @@ static int ifx_cat1_peri_clock_init(const struct device *dev)
 {
 	struct ifx_peri_clock_data *const data = dev->data;
 	en_clk_dst_t clk_dst;
-	int err;
 
 	/* PDL calls to set the and enable peri clock divider use the en_clk_dst_t
 	 * enumeration. This enumeration contains the peripheral clock instance, peripheral
@@ -93,34 +65,22 @@ static int ifx_cat1_peri_clock_init(const struct device *dev)
 	 * use the clock must connect to the clock by calling:
 	 * ifx_cat1_utils_peri_pclk_assign_divider()
 	 */
-	if (data->hw_resource.type == IFX_RSC_SCB) {
-		if ((data->clock.block & CLK_FRAC_DIV_MODE) == 0) {
-			err = ifx_cat1_utils_peri_pclk_set_divider(clk_dst, &data->clock,
-								   data->divider - 1);
-		} else {
-			err = ifx_cat1_utils_peri_pclk_set_frac_divider(clk_dst, &(data->clock),
-									data->divider - 1, 0);
-		}
-
-		if (err != CY_SYSCLK_SUCCESS) {
-			return -EIO;
-		}
-
-	} else if (data->hw_resource.type == IFX_RSC_TCPWM ||
-		   data->hw_resource.type == IFX_RSC_SDHC) {
-		err = ifx_cat1_utils_peri_pclk_set_divider(clk_dst, &data->clock,
-							   data->divider - 1);
-		if (err != CY_SYSCLK_SUCCESS) {
+	if ((data->div_type == CY_SYSCLK_DIV_8_BIT) || (data->div_type == CY_SYSCLK_DIV_16_BIT)) {
+		if (CY_RSLT_SUCCESS != ifx_cat1_utils_peri_pclk_set_divider(clk_dst,
+						&data->clock, data->divider - 1)) {
 			return -EIO;
 		}
 	} else {
-		return -EINVAL;
+		if (CY_RSLT_SUCCESS != ifx_cat1_utils_peri_pclk_set_frac_divider(clk_dst,
+				&data->clock, data->divider - 1, data->frac_divider)) {
+			return -EIO;
+		}
 	}
 
-	err = ifx_cat1_utils_peri_pclk_enable_divider(clk_dst, &data->clock);
-	if (err != CY_SYSCLK_SUCCESS) {
+	if (CY_RSLT_SUCCESS != ifx_cat1_utils_peri_pclk_enable_divider(clk_dst, &data->clock)) {
 		return -EIO;
 	}
+
 	return 0;
 }
 
@@ -147,7 +107,9 @@ static int ifx_cat1_peri_clock_init(const struct device *dev)
 
 #define INFINEON_CAT1_PERI_CLOCK_INIT(n)                                                           \
 	static struct ifx_peri_clock_data ifx_cat1_peri_clock##n##_data = {                        \
+		.div_type = DT_INST_PROP(n, div_type),                                             \
 		.divider = DT_INST_PROP(n, clock_div),                                             \
+		.frac_divider = DT_INST_PROP_OR(n, div_frac_value, 0),                             \
 		.hw_resource = {.type = DT_INST_PROP(n, resource_type),                            \
 				.block_num = DT_INST_PROP(n, resource_instance),                   \
 				.channel_num = DT_INST_PROP_OR(n, resource_channel, 0)},           \
