5d73bc00cac520d8291ede4243523ed351b46f94
Grzegorz Chwierut <grzegorz.chwierut@nordicsemi.no>
2025-12-18 19:18:07 +0100

    twister: fix --list-tests output, remove duplication in names
    This fixes the issue where --list-tests and --test-tree commands
    showed duplicated testsuite IDs in the output,
    simplifies testcase name handling in harness and runner modules.
    Removed feature to extend behaviour of --no-detailed-test-id to shorten
    test case names - feature was introduced in #82302.
    This makes the twister output more predictable and the codebase easier
    to maintain.
    
    Signed-off-by: Grzegorz Chwierut <grzegorz.chwierut@nordicsemi.no>

--- scripts/pylib/twister/twisterlib/testsuite.py
+++ scripts/pylib/twister/twisterlib/testsuite.py
@@ -376,18 +376,13 @@ def _find_src_dir_path(test_dir_path):
 
 class TestCase(DisablePyTestCollectionMixin):
 
-    def __init__(self, name=None, testsuite=None):
+    def __init__(self, name):
         self.duration = 0
         self.name = name
         self._status = TwisterStatus.NONE
         self.reason = None
-        self.testsuite = testsuite
         self.output = ""
         self.freeform = False
-
-    @property
-    def detailed_name(self) -> str:
-        return TestSuite.get_case_name_(self.testsuite, self.name, detailed=True)
 
     @property
     def status(self) -> TwisterStatus:
@@ -447,7 +442,7 @@ class TestSuite(DisablePyTestCollectionMixin):
             os.path.realpath(suite_path), start=canonical_zephyr_base
         )
         self.yamlfile = suite_path
-        self.testcases = []
+        self.testcases: list[TestCase] = []
         self.integration_platforms = []
 
         self.ztest_suite_names = []
@@ -480,18 +475,8 @@ class TestSuite(DisablePyTestCollectionMixin):
                 'Harness config error: console harness defined without a configuration.'
             )
 
-    @staticmethod
-    def get_case_name_(test_suite, tc_name, detailed=True) -> str:
-        return f"{test_suite.id}.{tc_name}" \
-            if test_suite and detailed and not test_suite.detailed_test_id else f"{tc_name}"
-
-    @staticmethod
-    def compose_case_name_(test_suite, tc_name) -> str:
-        return f"{test_suite.id}.{tc_name}" \
-            if test_suite and test_suite.detailed_test_id else f"{tc_name}"
-
     def compose_case_name(self, tc_name) -> str:
-        return self.compose_case_name_(self, tc_name)
+        return f"{self.id}.{tc_name}" if self.id != tc_name else tc_name
 
     def add_subcases(self, data, parsed_subcases=None, suite_names=None):
         testcases = data.get("testcases", [])
@@ -509,7 +494,7 @@ class TestSuite(DisablePyTestCollectionMixin):
             self.ztest_suite_names = suite_names
 
     def add_testcase(self, name, freeform=False):
-        tc = TestCase(name=name, testsuite=self)
+        tc = TestCase(name=name)
         tc.freeform = freeform
         self.testcases.append(tc)
 
