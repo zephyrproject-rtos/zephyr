9501e8b4eda24a320ecd3218279b0b2d665e86e8
Ravi Dondaputi <ravi.dondaputi@nordicsemi.no>
2025-12-19 23:19:01 +0530

    drivers: wifi: nrf_wifi: Use filter_ssid for connect scan
    Modify the scan logic to use filter_ssids for connect scan
    and use ssids for other cases.
    
    Signed-off-by: Ravi Dondaputi <ravi.dondaputi@nordicsemi.no>

--- drivers/wifi/nrf_wifi/src/wpa_supp_if.c
+++ drivers/wifi/nrf_wifi/src/wpa_supp_if.c
@@ -546,7 +546,18 @@ int nrf_wifi_wpa_supp_scan2(void *if_priv, struct wpa_driver_scan_params *params
 		scan_info->scan_params.num_scan_channels = indx;
 	}
 
-	if (params->num_ssids) {
+
+	if (params->filter_ssids) {
+		scan_info->scan_params.num_scan_ssids = params->num_filter_ssids;
+		for (indx = 0; indx < params->num_filter_ssids; indx++) {
+			memcpy(scan_info->scan_params.scan_ssids[indx].nrf_wifi_ssid,
+			       params->filter_ssids[indx].ssid,
+			       params->filter_ssids[indx].ssid_len);
+
+			scan_info->scan_params.scan_ssids[indx].nrf_wifi_ssid_len =
+				params->filter_ssids[indx].ssid_len;
+		}
+	} else if (params->num_ssids) {
 		scan_info->scan_params.num_scan_ssids = params->num_ssids;
 
 		for (indx = 0; indx < params->num_ssids; indx++) {
