b145508a1fd05415b413c3de07b695226acd26fd
Alain Volmat <alain.volmat@foss.st.com>
2025-12-22 22:28:45 +0100

    display: rename current BGR_565 format into RGB_565X
    The format currently expected by devices and sample display
    application for BGR_565 is actually RGB_565 format with bytes
    swapped (not B / R swapped). That is:
    
    PIXEL_FORMAT_RGB_565:
             * @code{.unparsed}
             *   7......0 15.....8
             * | gggBbbbb RrrrrGgg | ...
             * @endcode
    
    current PIXEL_FORMAT_BGR_565:
             * @code{.unparsed}
             *   7......0 15.....8
             * | RrrrrGgg gggBbbbb | ...
             * @endcode
    
    This is explained in both st7796s display driver but also is
    what is generated by the sample display application. As a video
    format (ex: V4L2), such format is not mentioned as BGR (for
    which R and B are swapped) but RGB_565X.
    
    Within the whole Zephyr tree, rename the curremt BGR_565 format
    into RGB_565X in order to emphasis that this is a byte swapped
    format rather than a B/G component swapped format.
    
    This also correct the description of the format in display.h
    file, which wasn't correct based on what was being used by
    display driver or sample display app.
    
    Signed-off-by: Alain Volmat <alain.volmat@foss.st.com>

--- samples/drivers/display/src/main.c
+++ samples/drivers/display/src/main.c
@@ -119,8 +119,8 @@ static uint16_t get_rgb565_color(enum corner corner, uint8_t grey)
 	return color;
 }
 
-static void fill_buffer_bgr565(enum corner corner, uint8_t grey, uint8_t *buf,
-			       size_t buf_size)
+static void fill_buffer_rgb565x(enum corner corner, uint8_t grey, uint8_t *buf,
+				size_t buf_size)
 {
 	uint16_t color = get_rgb565_color(corner, grey);
 
@@ -295,9 +295,9 @@ int main(void)
 		fill_buffer_fnc = fill_buffer_rgb565;
 		buf_size *= 2;
 		break;
-	case PIXEL_FORMAT_BGR_565:
+	case PIXEL_FORMAT_RGB_565X:
 		bg_color = 0xFFu;
-		fill_buffer_fnc = fill_buffer_bgr565;
+		fill_buffer_fnc = fill_buffer_rgb565x;
 		buf_size *= 2;
 		break;
 	case PIXEL_FORMAT_L_8:
