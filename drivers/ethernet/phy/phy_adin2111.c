c9f8e8c856bedbaf6676c17612272b72eb4ba08f
Tim Pambor <tim.pambor@codewrights.de>
2025-12-23 10:55:18 +0100

    drivers: eth: phy: adin2111: Use C45 access by default
    Use clause 45 MDIO access by default and only fallback to the phy
    internal clause 45 bridge if the MDIO controller does not support
    clause 45 transactions.
    
    Signed-off-by: Tim Pambor <tim.pambor@codewrights.de>

--- drivers/ethernet/phy/phy_adin2111.c
+++ drivers/ethernet/phy/phy_adin2111.c
@@ -152,17 +152,19 @@ static int phy_adin2111_c45_read(const struct device *dev, uint16_t devad,
 	const struct phy_adin2111_config *cfg = dev->config;
 	int rval;
 
-	if (cfg->mii) {
+	rval = mdio_read_c45(cfg->mdio, cfg->phy_addr, devad, reg, val);
+
+	if (rval == -ENOSYS) {
 		/* Using C22 -> devad bridge */
 		rval = phy_adin2111_c45_setup_dev_reg(dev, devad, reg);
 		if (rval < 0) {
 			return rval;
 		}
 
-		return mdio_read(cfg->mdio, cfg->phy_addr, ADIN1100_MMD_ACCESS, val);
+		rval = mdio_read(cfg->mdio, cfg->phy_addr, ADIN1100_MMD_ACCESS, val);
 	}
 
-	return mdio_read_c45(cfg->mdio, cfg->phy_addr, devad, reg, val);
+	return rval;
 }
 
 static int phy_adin2111_c45_write(const struct device *dev, uint16_t devad,
@@ -171,17 +173,19 @@ static int phy_adin2111_c45_write(const struct device *dev, uint16_t devad,
 	const struct phy_adin2111_config *cfg = dev->config;
 	int rval;
 
-	if (cfg->mii) {
+	rval = mdio_write_c45(cfg->mdio, cfg->phy_addr, devad, reg, val);
+
+	if (rval == -ENOSYS) {
 		/* Using C22 -> devad bridge */
 		rval = phy_adin2111_c45_setup_dev_reg(dev, devad, reg);
 		if (rval < 0) {
 			return rval;
 		}
 
-		return mdio_write(cfg->mdio, cfg->phy_addr, ADIN1100_MMD_ACCESS, val);
+		rval = mdio_write(cfg->mdio, cfg->phy_addr, ADIN1100_MMD_ACCESS, val);
 	}
 
-	return mdio_write_c45(cfg->mdio, cfg->phy_addr, devad, reg, val);
+	return rval;
 }
 
 static int phy_adin2111_reg_read(const struct device *dev, uint16_t reg_addr,
