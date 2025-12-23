e53da36164703c3ed42f523524c16e7ae37a8436
Ederson de Souza <ederson.desouza@intel.com>
2025-12-23 11:22:28 -0800

    samples: tfm_integration: tfm_secure_partition: Support SFN backend
    When support for old "Library model" was removed, support for the new
    Secure Function (SFN) model - the FF-M 1.1 spec that succeded the "Library
    model" - wasn't implemented. This patch adds it.
    
    Signed-off-by: Ederson de Souza <ederson.desouza@intel.com>

--- samples/tfm_integration/tfm_secure_partition/src/dummy_partition.c
+++ samples/tfm_integration/tfm_secure_partition/src/dummy_partition.c
@@ -8,7 +8,6 @@
 
 #include "dummy_partition.h"
 
-#if defined(CONFIG_TFM_IPC)
 #include "psa/client.h"
 #include "psa_manifest/sid.h"
 
@@ -40,25 +39,3 @@ psa_status_t dp_secret_digest(uint32_t secret_index,
 
 	return status;
 }
-#else /* defined(CONFIG_TFM_IPC) */
-psa_status_t dp_secret_digest(uint32_t secret_index,
-			void *p_digest,
-			size_t digest_size)
-{
-	psa_status_t status;
-	psa_invec in_vec[] = {
-		{ .base = &secret_index, .len = sizeof(secret_index) },
-	};
-
-	psa_outvec out_vec[] = {
-		{ .base = p_digest, .len = digest_size }
-	};
-
-	status = tfm_ns_interface_dispatch(
-				(veneer_fn)tfm_dp_secret_digest_req_veneer,
-				(uint32_t)in_vec,  IOVEC_LEN(in_vec),
-				(uint32_t)out_vec, IOVEC_LEN(out_vec));
-
-	return status;
-}
-#endif /* defined(CONFIG_TFM_IPC) */
