d30b5c06299d49eca5f120e5aa16475d67c132d5
Muhammad Waleed Badar <walid.badar@gmail.com>
2025-12-26 17:17:06 +0500

    soc: allwinner: add sun8i h3 soc support
    Add initial support for the Allwinner H3 SoC, commonly
    found in development boards like the Orange Pi series.
    
    This commit introduces the intial SoC support files:
     - Basic Kconfig configuration and SoC definition
     - MMU region setup for memory management
     - SoC-specific headers and device tree source include
    
    Signed-off-by: Muhammad Waleed Badar <walid.badar@gmail.com>

---
+++ soc/allwinner/sun8i_h3/soc.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025 Muhammad Waleed Badar
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#ifndef _ASMLANGUAGE
+
+/*
+ * The following definitions are required for the inclusion of the CMSIS
+ * Common Peripheral Access Layer for aarch32 Cortex-A CPUs:
+ */
+
+#define __CORTEX_A 7U
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _SOC__H_ */
