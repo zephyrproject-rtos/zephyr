00569664794eecb278b75021288d0c324e6925fd
Ha Duong Quang <ha.duongquang@nxp.com>
2025-12-26 14:30:43 +0700

    soc: nxp: s32: k5: add adc base address mappings
    Add macro definitions to map generic ADC instance names to hardware
    specific SARADC base addresses, enabling driver portability.
    
    Signed-off-by: Ha Duong Quang <ha.duongquang@nxp.com>

--- soc/nxp/s32/s32k5/m7/soc.h
+++ soc/nxp/s32/s32k5/m7/soc.h
@@ -14,4 +14,8 @@
 #include <cmsis_rtos_v2_adapt.h>
 #endif
 
+#define IP_ADC_0_BASE	IP_SARADC_0_BASE
+#define IP_ADC_1_BASE	IP_SARADC_1_BASE
+#define IP_ADC_2_BASE	IP_LPE_SARADC_BASE
+
 #endif /* _NXP_S32_S32K5_M7_SOC_H_ */
