3d782993a2fc3d9fae0b9a0fe30ba0c5602c32b7
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2025-12-30 06:24:15 +0100

    Bluetooth: Controller: Use explicit margin for unreserved tickers
    Update ticker implementation to use explicit ticks slot
    margin for unreserved ticker collision resolution. This will
    allow differentiating an unreserved ticker's needed free
    space versus free space when the ticker is rescheduled.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ticker/ticker.c
+++ subsys/bluetooth/controller/ticker/ticker.c
@@ -858,7 +858,7 @@ static uint8_t ticker_resolve_collision(struct ticker_node *nodes,
 		uint32_t ticker_ticks_slot;
 
 		if (TICKER_HAS_SLOT_WINDOW(ticker) && (ticker->ticks_slot == 0U)) {
-			ticker_ticks_slot = HAL_TICKER_RESCHEDULE_MARGIN;
+			ticker_ticks_slot = HAL_TICKER_TICKS_SLOT_MARGIN;
 		} else {
 			ticker_ticks_slot = ticker->ticks_slot;
 		}
@@ -2215,7 +2215,7 @@ static inline void ticker_job_worker_bh(struct ticker_instance *instance,
 			uint32_t ticker_ticks_slot;
 
 			if (TICKER_HAS_SLOT_WINDOW(ticker) && (ticker->ticks_slot == 0U)) {
-				ticker_ticks_slot = HAL_TICKER_RESCHEDULE_MARGIN;
+				ticker_ticks_slot = HAL_TICKER_TICKS_SLOT_MARGIN;
 			} else {
 				ticker_ticks_slot = ticker->ticks_slot;
 			}
