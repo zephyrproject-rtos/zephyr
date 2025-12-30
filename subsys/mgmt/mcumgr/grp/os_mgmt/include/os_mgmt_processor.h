JaagupAverin/fox-zephyr

subsys/mgmt: Declare PROCESSOR_NAME for native_sim
In case of native build, the CONFIG_architecture
cannot currently be resolved with Kconfig, so we
must fall back to resolving it during compilation
using compiler definitions.

Signed-off-by: Jaagup Averin <jaagup.averin@gmail.com>

subsys/mgmt/mcumgr/grp/os_mgmt/include/os_mgmt_processor.h
@@ -184,6 +184,22 @@ extern "C" {
 #define PROCESSOR_NAME "sparc"
 #endif
 
+#if defined(CONFIG_BOARD_NATIVE_SIM)
+#if defined(PROCESSOR_NAME)
+#error "Processor name already selected; compile-time check should be removed."
+#endif
+
+#if defined(__x86_64__)
+#define PROCESSOR_NAME "x86_64"
+#elif defined(__i386__)
+#define PROCESSOR_NAME "x86"
+#elif defined(__aarch64__)
+#define PROCESSOR_NAME "aarch64"
+#elif defined(__arm__)
+#define PROCESSOR_NAME "arm"
+#endif
+#endif
+
 #ifndef PROCESSOR_NAME
 #warning "Processor type could not be determined"
 #define PROCESSOR_NAME "unknown"

zephyrproject-rtos/zephyr

subsys/mgmt/mcumgr/grp/os_mgmt/include/os_mgmt_processor.h
@@ -184,6 +184,22 @@ extern "C" {
 #define PROCESSOR_NAME "sparc"
 #endif
 
+#if defined(CONFIG_ARCH_POSIX)
+#if defined(PROCESSOR_NAME)
+#error "Processor name already selected; compile-time check should be removed."
+#endif
+
+#if defined(__x86_64__)
+#define PROCESSOR_NAME "x86_64"
+#elif defined(__i386__)
+#define PROCESSOR_NAME "x86"
+#elif defined(__aarch64__)
+#define PROCESSOR_NAME "aarch64"
+#elif defined(__arm__)
+#define PROCESSOR_NAME "arm"
+#endif
+#endif
+
 #ifndef PROCESSOR_NAME
 #warning "Processor type could not be determined"
 #define PROCESSOR_NAME "unknown"