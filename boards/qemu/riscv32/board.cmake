5e96ef49856c9f3247d88da8630e026d628b7ed6
Afonso Oliveira <afonsoo@synopsys.com>
2026-01-02 23:20:03 +0000

    boards: qemu: riscv32: add AIA board variants
    Add qemu_riscv32 board qualifiers for AIA single-core and SMP variants.
    
    Signed-off-by: Afonso Oliveira <afonsoo@synopsys.com>

--- boards/qemu/riscv32/board.cmake
+++ boards/qemu/riscv32/board.cmake
@@ -4,11 +4,28 @@ set(SUPPORTED_EMU_PLATFORMS qemu)
 set(QEMU_binary_suffix riscv32)
 set(QEMU_CPU_TYPE_${ARCH} riscv32)
 
-set(QEMU_FLAGS_${ARCH}
-  -nographic
-  -machine virt
-  -bios none
-  -m 256
-)
+# Check if this is an AIA SoC (check CONFIG_SOC_QEMU_VIRT_RISCV32_AIA at configure time)
+if(CONFIG_SOC_QEMU_VIRT_RISCV32_AIA)
+  # AIA SoC - enable APLIC+IMSIC
+  set(QEMU_FLAGS_${ARCH}
+    -nographic
+    -machine virt,aia=aplic-imsic
+    -bios none
+    -m 256M
+  )
+else()
+  # Default PLIC SoC
+  set(QEMU_FLAGS_${ARCH}
+    -nographic
+    -machine virt
+    -bios none
+    -m 256
+  )
+endif()
+
+# Add SMP flags for SMP variant
+if(CONFIG_SMP)
+  list(APPEND QEMU_FLAGS_${ARCH} -smp ${CONFIG_MP_MAX_NUM_CPUS})
+endif()
 
 board_set_debugger_ifnset(qemu)
