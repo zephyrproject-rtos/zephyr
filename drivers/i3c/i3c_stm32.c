87ce02b01188c137ab0d3c646320441fabf44eae
Tahsin Mutlugun <Tahsin.Mutlugun@analog.com>
2026-01-05 20:09:22 +0300

    drivers: i3c: Move i3c_bus_mode() to common layer
    i3c_bus_mode() is duplicated across several drivers. Move it into
    i3c_common.c to eliminate that.
    
    Signed-off-by: Tahsin Mutlugun <Tahsin.Mutlugun@analog.com>

--- drivers/i3c/i3c_stm32.c
+++ drivers/i3c/i3c_stm32.c
@@ -163,44 +163,6 @@ struct i3c_stm32_data {
 	bool hj_pm_lock;           /* Used as flag for setting pm */
 #endif
 };
-/**
- * Determine I3C bus mode from the i2c devices on the bus.
- *
- * Reads the LVR of all I2C devices and returns the I3C bus
- * Mode.
- *
- * @param dev_list Pointer to device list
- *
- * @return @see enum i3c_bus_mode.
- */
-static enum i3c_bus_mode i3c_bus_mode(const struct i3c_dev_list *dev_list)
-{
-	enum i3c_bus_mode mode = I3C_BUS_MODE_PURE;
-
-	for (int i = 0; i < dev_list->num_i2c; i++) {
-		switch (I3C_LVR_I2C_DEV_IDX(dev_list->i2c[i].lvr)) {
-		case I3C_LVR_I2C_DEV_IDX_0:
-			if (mode < I3C_BUS_MODE_MIXED_FAST) {
-				mode = I3C_BUS_MODE_MIXED_FAST;
-			}
-			break;
-		case I3C_LVR_I2C_DEV_IDX_1:
-			if (mode < I3C_BUS_MODE_MIXED_LIMITED) {
-				mode = I3C_BUS_MODE_MIXED_LIMITED;
-			}
-			break;
-		case I3C_LVR_I2C_DEV_IDX_2:
-			if (mode < I3C_BUS_MODE_MIXED_SLOW) {
-				mode = I3C_BUS_MODE_MIXED_SLOW;
-			}
-			break;
-		default:
-			mode = I3C_BUS_MODE_INVALID;
-			break;
-		}
-	}
-	return mode;
-}
 
 static int get_i3c_lvr_ic_mode(const struct i3c_dev_list *dev_list)
 {
