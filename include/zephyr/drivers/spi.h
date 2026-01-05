d1ad53cb171f45de45efc47ef84b0e2c73fe4618
Benjamin Cabé <benjamin@zephyrproject.org>
2026-01-05 14:56:16 +0100

    drivers: include: spi: use doxygen aliases to document driver ops
    Apply the recently introduced driver_ops Doxygen macros to the SPI driver
    API. This standardizes how driver operations are documented, making it
    easier for developers to distinguish between mandatory and optional
    operations directly in the public API documentation.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- include/zephyr/drivers/spi.h
+++ include/zephyr/drivers/spi.h
@@ -859,8 +859,14 @@ static inline void spi_transceive_stats(const struct device *dev, int error,
 	SPI_DEVICE_DT_DEFINE(DT_DRV_INST(inst), __VA_ARGS__)
 
 /**
+ * @def_driverbackendgroup{SPI,spi_interface}
+ * @{
+ */
+
+/**
  * @typedef spi_api_io
- * @brief Callback API for I/O
+ * @brief Callback API for I/O.
+ *
  * See spi_transceive() for argument descriptions
  */
 typedef int (*spi_api_io)(const struct device *dev,
@@ -878,8 +884,9 @@ typedef int (*spi_api_io)(const struct device *dev,
 typedef void (*spi_callback_t)(const struct device *dev, int result, void *data);
 
 /**
- * @typedef spi_api_io
- * @brief Callback API for asynchronous I/O
+ * @typedef spi_api_io_async
+ * @brief Callback API for asynchronous I/O.
+ *
  * See spi_transceive_signal() for argument descriptions
  */
 typedef int (*spi_api_io_async)(const struct device *dev,
@@ -889,7 +896,7 @@ typedef int (*spi_api_io_async)(const struct device *dev,
 				spi_callback_t cb,
 				void *userdata);
 
-#if defined(CONFIG_SPI_RTIO) || defined(DOXYGEN)
+#if defined(CONFIG_SPI_RTIO) || defined(__DOXYGEN__)
 
 /**
  * @typedef spi_api_iodev_submit
@@ -909,19 +916,35 @@ typedef int (*spi_api_release)(const struct device *dev,
 
 
 /**
- * @brief SPI driver API
- * This is the mandatory API any SPI driver needs to expose.
+ * @driver_ops{SPI}
  */
 __subsystem struct spi_driver_api {
+	/**
+	 * @driver_ops_mandatory @copybrief spi_transceive
+	 */
 	spi_api_io transceive;
-#ifdef CONFIG_SPI_ASYNC
+#if defined(CONFIG_SPI_ASYNC) || defined(__DOXYGEN__)
+	/**
+	 * @driver_ops_optional @copybrief spi_transceive_cb
+	 * @kconfig_dep{CONFIG_SPI_ASYNC}
+	 */
 	spi_api_io_async transceive_async;
 #endif /* CONFIG_SPI_ASYNC */
-#ifdef CONFIG_SPI_RTIO
+#if defined(CONFIG_SPI_RTIO) || defined(__DOXYGEN__)
+	/**
+	 * @driver_ops_optional @copybrief spi_iodev_submit
+	 * @kconfig_dep{CONFIG_SPI_RTIO}
+	 */
 	spi_api_iodev_submit iodev_submit;
 #endif /* CONFIG_SPI_RTIO */
+	/**
+	 * @driver_ops_mandatory @copybrief spi_release
+	 */
 	spi_api_release release;
 };
+/**
+ * @}
+ */
 
 /**
  * @brief Check if SPI CS is controlled using a GPIO.
@@ -1153,9 +1176,7 @@ static inline int spi_write_dt(const struct spi_dt_spec *spec,
  */
 
 /**
- * @brief Read/write the specified amount of data from the SPI driver.
- *
- * @note This function is asynchronous.
+ * @brief Read/write the specified amount of data from the SPI driver asynchronously.
  *
  * @note This function is available only if @kconfig{CONFIG_SPI_ASYNC}
  * is selected.
