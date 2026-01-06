15763e43858f09de3915c95d78ea81927a9350b8
Liam Ogletree <liam.ogletree@cirrus.com>
2026-01-06 10:30:52 -0600

    drivers: haptics: Add error callback mechanism to haptics API
    Haptics devices provide protection features to prevent damage during
    operation. Adds an error callback mechanism to the haptics API to
    enable haptics device drivers to raise these conditions to the
    application layer. Enumerates a subset of errors that are common
    to modern haptics devices across major vendors.
    
    Signed-off-by: Liam Ogletree <liam.ogletree@cirrus.com>

--- include/zephyr/drivers/haptics.h
+++ include/zephyr/drivers/haptics.h
@@ -32,6 +32,20 @@ extern "C" {
 #endif
 
 /**
+ * @brief Haptics error types
+ */
+enum haptics_error_type {
+	HAPTICS_ERROR_OVERCURRENT = BIT(0),     /**< Output overcurrent error */
+	HAPTICS_ERROR_OVERTEMPERATURE = BIT(1), /**< Device overtemperature error */
+	HAPTICS_ERROR_UNDERVOLTAGE = BIT(2),    /**< Power source undervoltage error */
+	HAPTICS_ERROR_OVERVOLTAGE = BIT(3),     /**< Power source overvoltage error */
+	HAPTICS_ERROR_DC = BIT(4),              /**< Output direct-current error */
+
+	/* Device-specific error codes can follow, refer to the device’s header file */
+	HAPTICS_ERROR_PRIV_START = BIT(5),
+};
+
+/**
  * @typedef haptics_stop_output_t
  * @brief Set the haptic device to stop output
  * @param dev Pointer to the device structure for haptic device instance
@@ -45,11 +59,31 @@ typedef int (*haptics_stop_output_t)(const struct device *dev);
 typedef int (*haptics_start_output_t)(const struct device *dev);
 
 /**
+ * @typedef haptics_error_callback_t
+ * @brief Callback function for error interrupt
+ *
+ * @param dev Pointer to the haptic device
+ * @param errors Device errors (bitmask of @ref haptics_error_type values)
+ */
+typedef void (*haptics_error_callback_t)(const struct device *dev, const uint32_t errors);
+
+/**
+ * @typedef haptics_register_error_callback_t
+ * @brief Register a callback function for haptics errors
+ *
+ * @param dev Pointer to the haptic device
+ * @param cb Callback function (of type @ref haptics_error_callback_t)
+ */
+typedef int (*haptics_register_error_callback_t)(const struct device *dev,
+						 haptics_error_callback_t cb);
+
+/**
  * @brief Haptic device API
  */
 __subsystem struct haptics_driver_api {
 	haptics_start_output_t start_output;
 	haptics_stop_output_t stop_output;
+	haptics_register_error_callback_t register_error_callback;
 };
 
 /**
@@ -84,6 +118,27 @@ static inline int z_impl_haptics_stop_output(const struct device *dev)
 	const struct haptics_driver_api *api = (const struct haptics_driver_api *)dev->api;
 
 	return api->stop_output(dev);
+}
+
+/**
+ * @brief Register a callback function for haptics errors
+ *
+ * @param dev Pointer to the haptic device
+ * @param cb Callback function (of type @ref haptics_error_callback_t)
+ *
+ * @retval 0 if successful
+ * @retval <0 if failed
+ */
+static inline int haptics_register_error_callback(const struct device *dev,
+						  haptics_error_callback_t cb)
+{
+	const struct haptics_driver_api *api = (const struct haptics_driver_api *)dev->api;
+
+	if (api->register_error_callback == NULL) {
+		return -ENOSYS;
+	}
+
+	return api->register_error_callback(dev, cb);
 }
 
 /**
