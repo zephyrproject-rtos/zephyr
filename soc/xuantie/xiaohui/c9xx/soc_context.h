a04c3ed9b1939adf95c4a15ddbdbae77980751e5
Dong Liu <ld02443552@alibaba-inc.com>
2026-01-06 19:15:22 +0800

    soc: xuantie: add RISC-V SoC family support for Xuantie
    Introduce the Xuantie RISC-V SoC: soc/xuantie/xiaohui/,
    providing infrastructure for CPU and board-specific logic.
    
    Signed-off-by: Dong Liu <ld02443552@alibaba-inc.com>

---
+++ soc/xuantie/xiaohui/c9xx/soc_context.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2017-2024 Alibaba Group Holding Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
+ * Extra definitions required for CONFIG_RISCV_SOC_CONTEXT_SAVE.
+ */
+
+#ifndef SOC_XUANTIE_RISCV_SOC_CONTEXT_H_
+#define SOC_XUANTIE_RISCV_SOC_CONTEXT_H_
+
+#ifdef CONFIG_RISCV_SOC_CONTEXT_SAVE
+
+#include <csr_context.h>
+
+#endif /* CONFIG_RISCV_SOC_CONTEXT_SAVE */
+
+#endif /* SOC_XUANTIE_RISCV_SOC_CONTEXT_H_ */
