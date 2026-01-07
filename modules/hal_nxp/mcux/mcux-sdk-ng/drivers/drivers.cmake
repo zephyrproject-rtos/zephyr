5987001b84737487c27a963b1a62e1088d95d442
Felix Wang <fei.wang_3@nxp.com>
2026-01-07 21:11:36 +0530

    modules: hal_nxp: mcux: mcux-sdk-ng: Add XBAR and XBAR_1 driver
    Add CONFIG_MCUX_XBAR and CONFIG_MCUX_XBAR_1 to the MCUX
    SDK driver components mapping to enable the corresponding MCUX
    SDK driver components when these Kconfig options are enabled.
    
    Signed-off-by: Felix Wang <fei.wang_3@nxp.com>

--- modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
+++ modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
@@ -122,6 +122,8 @@ set_variable_ifdef(CONFIG_MIPI_DSI_MCUX         CONFIG_MCUX_COMPONENT_driver.mip
 set_variable_ifdef(CONFIG_MIPI_DSI_MCUX_2L      CONFIG_MCUX_COMPONENT_driver.mipi_dsi)
 set_variable_ifdef(CONFIG_MIPI_DSI_NXP_DWC      CONFIG_MCUX_COMPONENT_driver.mipi_dsi_imx)
 set_variable_ifdef(CONFIG_MCUX_SDIF             CONFIG_MCUX_COMPONENT_driver.sdif)
+set_variable_ifdef(CONFIG_MCUX_XBAR             CONFIG_MCUX_COMPONENT_driver.xbar)
+set_variable_ifdef(CONFIG_MCUX_XBAR_1           CONFIG_MCUX_COMPONENT_driver.xbar_1)
 set_variable_ifdef(CONFIG_MCUX_XBARA            CONFIG_MCUX_COMPONENT_driver.xbara)
 set_variable_ifdef(CONFIG_MCUX_XBARB            CONFIG_MCUX_COMPONENT_driver.xbarb)
 set_variable_ifdef(CONFIG_QDEC_MCUX             CONFIG_MCUX_COMPONENT_driver.enc)
