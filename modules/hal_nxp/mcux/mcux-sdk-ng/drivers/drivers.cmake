8260974216f44a8550e9a8f89e3bc8e40486077d
Holt Sun <holt.sun@nxp.com>
2026-01-09 15:21:03 +0800

    soc: nxp: ke1xz: Add power management support
    Implement power management with IDLE, STOP, PSTOP1, and PSTOP2 modes.
    
    - Add power state definitions with timing parameters
    - Implement pm_state_set() with proper SLEEPDEEP handling
    - Add XIP-safe WFI execution from RAM
    - Enable SMC driver and power mode protection
    - Remove forced timer Kconfig defaults
    
    Signed-off-by: Holt Sun <holt.sun@nxp.com>

--- modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
+++ modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
@@ -69,6 +69,7 @@ set_variable_ifdef(CONFIG_ENTROPY_MCUX_TRNG     CONFIG_MCUX_COMPONENT_driver.trn
 set_variable_ifdef(CONFIG_ENTROPY_MCUX_CAAM     CONFIG_MCUX_COMPONENT_driver.caam)
 set_variable_ifdef(CONFIG_ETH_NXP_ENET          CONFIG_MCUX_COMPONENT_driver.enet)
 set_variable_ifdef(CONFIG_SOC_SERIES_KINETIS_K2X          CONFIG_MCUX_COMPONENT_driver.smc)
+set_variable_ifdef(CONFIG_SOC_SERIES_KE1XZ      CONFIG_MCUX_COMPONENT_driver.smc)
 set_variable_ifdef(CONFIG_I2C_MCUX              CONFIG_MCUX_COMPONENT_driver.i2c)
 set_variable_ifdef(CONFIG_I2C_NXP_II2C          CONFIG_MCUX_COMPONENT_driver.ii2c)
 set_variable_ifdef(CONFIG_I3C_MCUX              CONFIG_MCUX_COMPONENT_driver.i3c)
