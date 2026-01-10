4b192295c9856a2f3cbcfc90aee6c67d3ba64608
Camille BAUD <mail@massdriver.space>
2026-01-11 00:49:01 +0100

    drivers: clock_control: Improve overclock support for bl61x
    Add more description, and a new overclock setting
    
    Signed-off-by: Camille BAUD <mail@massdriver.space>

--- include/zephyr/dt-bindings/clock/bflb_bl61x_clock.h
+++ include/zephyr/dt-bindings/clock/bflb_bl61x_clock.h
@@ -23,11 +23,25 @@
 #define BL61X_WIFIPLL_320MHz	3
 
 /* Overclocked
- * Overclock PLL to 480MHz for div ID 1 and 360MHz for div ID 2
- * BCLK divider MUST be set so BCLK is 80MHz or slower
+ * BCLK divider MUST be set so BCLK is 80MHz or slower to operate safely,
+ * However BCLK of up to 130 MHz have been observed to be tolerated.
+ * Drivers are able to compose a clock viable for them from clock sources (typically BCLK)
+ * so no additional settings are required for the peripherals.
  * Breaks most complex peripherals (Wifi)
+ * and peripherals that can use the peripheral bus as master (DMA and DMA-using drivers).
+ * Flash input clock must be tuned to fit the new clocks.
+ */
+
+/* Overclock PLL to 480MHz for div ID 1 and 360MHz for div ID 2
+ * Safe (default) core voltages are used (1.10v).
  */
 #define BL61X_WIFIPLL_OC_360MHz	(2 | 0x10)
 #define BL61X_WIFIPLL_OC_480MHz	(3 | 0x10)
+
+/* Overclock PLL to 640MHz for div ID 1 and 480MHz for div ID 2.
+ * This is the absolute maximum the PLL can provide.
+ */
+#define BL61X_WIFIPLL_OCMAX_480MHz	(2 | 0x20)
+#define BL61X_WIFIPLL_OCMAX_640MHz	(3 | 0x20)
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_BFLB_BL61X_CLOCK_H_ */
