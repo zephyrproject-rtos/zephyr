zephyrproject-rtos/zephyr

commit e45ac11aeb9b874038a2925c2588b94100aa28cc

scripts: schemas: make `full_name` a required property for boards
When initially introduced, the property was kept optional in an attempt
to not risk disrupting downstream users by introducing a required property
they wouldn't necessarily care about. In practice, this is causing
boards contributed upstream to sometimes miss this propery which is
important for boards to show up nicely in the boards catalog, and it is
therefore being made required.

Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

scripts/schemas/board-schema.yaml
@@ -165,10 +165,10 @@ $defs:
         $ref: "#/$defs/extendVariantSchema"
     # Conditional logic for requirements
     allOf:
-      # 'name' and 'extend' are mutually exclusive: we're either defining a new board or extending
-      # an existing one.
+      # 'name'/'full_name' and 'extend' are mutually exclusive: we're either defining a new board or
+      # extending an existing one.
       - oneOf:
-          - required: [name]
+          - required: [name, full_name]
           - required: [extend]
       # A base board (identified by 'name') must define its hardware via the 'socs' property.
       - if:

commit c7c1fab98c7dfe24c1520d1c163112e38833c217

scripts: twister: Add full_name to board.yml
Fixes regression introduced by e45ac11aeb9b874038a2925c2588b94100aa28cc

Signed-off-by: Guðni Már Gilbert <gudni.m.g@gmail.com>

scripts/tests/twister/test_data/boards/1_level/2_level/board.yml
@@ -1,14 +1,17 @@
 boards:
 
   - name: demo_board_1
+    full_name: Demo Board 1
     vendor: zephyr
     socs:
       - name: unit_testing
   - name: demo_board_2
+    full_name: Demo Board 2
     vendor: zephyr
     socs:
       - name: unit_testing
   - name: demo_board_3
+    full_name: Demo Board 3
     vendor: zephyr
     socs:
       - name: unit_testing

scripts/tests/twister/test_platform.py
@@ -191,10 +191,12 @@ def test_generate_platforms(
         'm0/boards/zephyr/p1/board.yml': """\
 boards:
   - name: p1e1
+    full_name: p1e1
     vendor: zephyr
     socs:
       - name: s1
   - name: p1e2
+    full_name: p1e2
     vendor: zephyr
     socs:
       - name: s1
@@ -211,6 +213,7 @@ variants:
         'm0/boards/zephyr/p2/board.yml': """\
 boards:
   - name: p2
+    full_name: p2
     vendor: zephyr
     socs:
       - name: s1
@@ -226,6 +229,7 @@ testing:
         'm0/boards/arm/p3/board.yml': """\
 board:
   name: p3
+  full_name: p3
   vendor: arm
   revision:
     format: letter
@@ -296,6 +300,7 @@ identifier: p2/s1/v1
 boards:
   - extend: p3
   - name: p4
+    full_name: p4
     vendor: misc
     socs:
       - name: s1

scripts/tests/twister_blackbox/test_data/boards/others/dummy_board/board.yml
@@ -1,5 +1,6 @@
 board:
   name: dummy
+  full_name: dummy
   vendor: others
   socs:
   - name: unit_testing