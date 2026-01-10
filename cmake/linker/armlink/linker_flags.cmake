fdced6d1a58be0b02eca59d201905816fe2e5ccd
Sudan Landge <sudan.landge@arm.com>
2026-01-10 21:08:01 +0000

    cmake: linker: do not pass -Os to armlink
    `armlink` does not accept -O* flags so, drop linker optimization flags
    to avoid below build error:
    
    ```
      Fatal error: L3903U: Argument 's' not permitted for option 'O'.
    ```
    
    Signed-off-by: Sudan Landge <sudan.landge@arm.com>

--- cmake/linker/armlink/linker_flags.cmake
+++ cmake/linker/armlink/linker_flags.cmake
@@ -1,5 +1,13 @@
 # Copyright (c) 2025 Nordic Semiconductor
+# Copyright 2026 Arm Limited and/or its affiliates <open-source-office@arm.com>
 #
 # SPDX-License-Identifier: Apache-2.0
 
 set_property(TARGET linker PROPERTY undefined --undefined=)
+
+# `armlink` does not accept -O* flags so drop flags to avoid build error.
+set_property(TARGET linker PROPERTY no_optimization "")
+set_property(TARGET linker PROPERTY optimization_debug "")
+set_property(TARGET linker PROPERTY optimization_speed "")
+set_property(TARGET linker PROPERTY optimization_size "")
+set_property(TARGET linker PROPERTY optimization_size_aggressive "")
