ab1257be9f71f50a273b40754f2551c6060c2c33
Tien Nguyen <tien.nguyen.zg@renesas.com>
2026-01-12 07:46:21 +0000

    samples: driver: jesd216: Add Flash support for RZ/A2M
    Add Flash samples support for RZ/A2M-EVK
    
    Signed-off-by: Tien Nguyen <tien.nguyen.zg@renesas.com>

--- samples/drivers/jesd216/src/main.c
+++ samples/drivers/jesd216/src/main.c
@@ -38,6 +38,8 @@
 #define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(renesas_rz_qspi_xspi)
 #elif DT_HAS_COMPAT_STATUS_OKAY(renesas_rz_qspi_spibsc)
 #define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(renesas_rz_qspi_spibsc)
+#elif DT_HAS_COMPAT_STATUS_OKAY(renesas_rza2m_qspi_spibsc)
+#define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(renesas_rza2m_qspi_spibsc)
 #elif DT_HAS_COMPAT_STATUS_OKAY(sifli_sf32lb_mpi_qspi_nor)
 #define FLASH_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(sifli_sf32lb_mpi_qspi_nor)
 #else
