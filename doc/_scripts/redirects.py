32823f39fbf2663fa1bbb75e60e5743cd6d91579
Andrej Butok <andrey.butok@nxp.com>
2026-01-13 16:17:48 +0100

    boards: mikroe: move the hexiwear board
    - The vendor of the hexiwear board is mikroe (not nxp)
      https://www.mikroe.com/hexiwear.
    - Move the board files from 'nxp' to 'mikroe'.
    - Delete hexiwear from the nxp maintained board list.
    - Update paths in the documentation.
    
    Signed-off-by: Andrej Butok <andrey.butok@nxp.com>

--- doc/_scripts/redirects.py
+++ doc/_scripts/redirects.py
@@ -20,6 +20,7 @@ REDIRECTS = (
     ('boards/arm/fvp_baser_aemv8r/doc/aarch64', 'boards/arm/fvp_baser_aemv8r/doc/index'),
     ('boards/arm/fvp_baser_aemv8r/doc/debug-with-arm-ds', 'boards/arm/fvp_baser_aemv8r/doc/index'),
     ('boards/nordic/nrf54l20pdk/doc/index', 'boards/nordic/nrf54lm20dk/doc/index'),
+    ('boards/nxp/hexiwear/doc/index', 'boards/mikroe/hexiwear/doc/index'),
     ('boards/panasonic/panb511evb/doc/index', 'boards/panasonic/panb611evb/doc/index'),
     ('boards/phytec/mimx8mm_phyboard_polis/doc/index', 'boards/phytec/phyboard_polis/doc/index'),
     ('boards/phytec/mimx8mp_phyboard_pollux/doc/index', 'boards/phytec/phyboard_pollux/doc/index'),
