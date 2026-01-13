ce7a4f5953a20e7f2c5e70df89dc1008f89ff3ae
John Batch <john.batch@infineon.com>
2026-01-13 13:33:41 -0800

    drivers: infineon: tcpwm: counter and pwm clock updates
    Updates the Infineon Counter and PWM drivers based on the TCPWM device
    to connect with a clock.  This update allows multiple devices to connect
    to the same peripheral clock, and supports the TCPWM architecture of the
    PSC3 device family.
    
    Corrects initialization of the PWM on the PSC3 family to select the
    correct output type.
    
    Signed-off-by: John Batch <john.batch@infineon.com>

--- drivers/counter/counter_infineon_tcpwm.c
+++ drivers/counter/counter_infineon_tcpwm.c
@@ -33,6 +33,7 @@ struct ifx_tcpwm_counter_config {
 	cy_en_divider_types_t divider_type;
 	uint32_t divider_sel;
 	uint32_t divider_val;
+	en_clk_dst_t clk_dst;
 	void (*irq_enable_func)(const struct device *dev);
 };
 
@@ -161,6 +162,12 @@ static int ifx_tcpwm_counter_init(const struct device *dev)
 
 	Cy_TCPWM_Counter_DeInit(config->reg_base, config->index, &counter_config);
 
+	/* Connect this TCPWM to the peripheral clock */
+	rslt = ifx_cat1_utils_peri_pclk_assign_divider(config->clk_dst, &data->clock);
+	if (rslt != CY_RSLT_SUCCESS) {
+		return -EIO;
+	}
+
 	rslt = (cy_rslt_t)Cy_TCPWM_Counter_Init(config->reg_base, config->index, &counter_config);
 	if (rslt != CY_RSLT_SUCCESS) {
 		return -EIO;
@@ -210,15 +217,9 @@ static uint32_t ifx_tcpwm_counter_get_freq(const struct device *dev)
 {
 	struct ifx_tcpwm_counter_data *const data = dev->data;
 	const struct ifx_tcpwm_counter_config *config = dev->config;
-	en_clk_dst_t clk_connection;
 
-	/* Determine tcpwm block number based on its resolution */
-	uint32_t tcpwm_block = config->resolution_32_bits ? 0 : 1;
+	uint32_t frequency = ifx_cat1_utils_peri_pclk_get_frequency(config->clk_dst, &data->clock);
 
-	/* Calculate clock connection based on TCPWM index */
-	clk_connection = ifx_cat1_tcpwm_get_clock_index(tcpwm_block, config->index);
-
-	uint32_t frequency = ifx_cat1_utils_peri_pclk_get_frequency(clk_connection, &data->clock);
 	return frequency;
 }
 
@@ -535,6 +536,7 @@ static DEVICE_API(counter, counter_api) = {
 		.irq_num = DT_IRQN(DT_INST_PARENT(n)),                                             \
 		.resolution_32_bits =                                                              \
 			(DT_PROP(DT_INST_PARENT(n), resolution) == 32) ? true : false,             \
+		.clk_dst = DT_PROP(DT_INST_PARENT(n), clk_dst),                                    \
 		.irq_enable_func = ifx_counter_irq_enable_func_##n,                                \
 	};                                                                                         \
                                                                                                    \
