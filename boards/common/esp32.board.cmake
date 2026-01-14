cb196db575dca3f3d983f389b3517ab982bc7b00
Sylvio Alves <sylvio.alves@espressif.com>
2026-01-13 22:34:34 -0300

    boards: esp32: add openocd flash support
    Configure ESP32 boards to use OpenOCD for flashing with BIN files.
    The --flash-address argument is combined with its value to avoid
    YAML parsing issues where hex values like 0x0 are parsed as integers.
    
    Signed-off-by: Sylvio Alves <sylvio.alves@espressif.com>

--- boards/common/esp32.board.cmake
+++ boards/common/esp32.board.cmake
@@ -7,8 +7,14 @@ board_runner_args(openocd  --no-halt --no-targets --no-load --target-handle _TAR
 board_runner_args(openocd  --gdb-init "set remote hardware-watchpoint-limit 2")
 
 board_runner_args(openocd  --gdb-init "maintenance flush register-cache")
+board_runner_args(openocd  --gdb-init "mon esp appimage_offset ${CONFIG_FLASH_LOAD_OFFSET}")
 board_runner_args(openocd  --gdb-init "mon reset halt")
 board_runner_args(openocd  --gdb-init "thb main")
+
+board_runner_args(openocd  --use-bin)
+board_runner_args(openocd  "--flash-address=${CONFIG_FLASH_LOAD_OFFSET}")
+board_runner_args(openocd  --cmd-load "program_esp")
+board_runner_args(openocd  --cmd-verify "esp verify_bank_hash 0")
 
 set(ESP_IDF_PATH ${ZEPHYR_HAL_ESPRESSIF_MODULE_DIR})
 assert(ESP_IDF_PATH "ESP_IDF_PATH is not set")
