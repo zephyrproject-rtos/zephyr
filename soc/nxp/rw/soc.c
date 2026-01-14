4e49867e5c91fa9f25311ccbacb4cbcb593d6765
Mahesh Mahadevan <mahesh.mahadevan@nxp.com>
2026-01-14 09:09:59 -0600

    Revert "[nxp noup] soc: rdrw61x: updates added to support ns board target."
    This reverts commit eab6984078d96e8cbed83a87cbb06410e4ed5981.
    
    Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

--- soc/nxp/rw/soc.c
+++ soc/nxp/rw/soc.c
@@ -84,7 +84,6 @@ __imx_boot_ivt_section void (*const image_vector_table[])(void) = {
  */
 __weak __ramfunc void clock_init(void)
 {
-#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 	POWER_DisableGDetVSensors();
 
 	if ((PMU->CAU_SLP_CTRL & PMU_CAU_SLP_CTRL_SOC_SLP_RDY_MASK) == 0U) {
@@ -302,7 +301,6 @@ __weak __ramfunc void clock_init(void)
 	RESET_PeripheralReset(kENET_IPG_S_RST_SHIFT_RSTn);
 #endif
 
-#endif /* ! CONFIG_TRUSTED_EXECUTION_NONSECURE */
 }
 
 extern void nxp_rw6xx_power_init(void);
@@ -316,7 +314,6 @@ extern void nxp_rw6xx_power_init(void);
 
 void soc_early_init_hook(void)
 {
-#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 #if (DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(wwdt), nxp_lpc_wwdt, okay))
 	POWER_EnableResetSource(kPOWER_ResetSourceWdt);
 #endif
@@ -342,7 +339,6 @@ void soc_early_init_hook(void)
 #if defined(CONFIG_ADC_MCUX_GAU) || defined(CONFIG_DAC_MCUX_GAU)
 	POWER_PowerOnGau();
 #endif
-
 #if CONFIG_PM
 	nxp_rw6xx_power_init();
 
@@ -364,13 +360,10 @@ void soc_early_init_hook(void)
 #if defined(CONFIG_BT) || defined(CONFIG_IEEE802154)
 	nxp_nbu_init();
 #endif
-#endif /* ! CONFIG_TRUSTED_EXECUTION_NONSECURE */
 }
 
 void soc_reset_hook(void)
 {
-#if !defined(CONFIG_TRUSTED_EXECUTION_NONSECURE)
 	/* This is provided by the SDK */
 	SystemInit();
-#endif /* ! CONFIG_TRUSTED_EXECUTION_NONSECURE */
 }
