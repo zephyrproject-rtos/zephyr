33babd19f2ed581ecfa98a6b9e59a93c83520af8
Pieter De Gendt <pieter.degendt@basalte.be>
2026-01-14 10:43:41 +0100

    drivers: pwm: mcux: Print status value on error
    When PWM_SetupPwm returns an error, add the value of status to the error
    log message.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- drivers/pwm/pwm_mcux.c
+++ drivers/pwm/pwm_mcux.c
@@ -115,7 +115,7 @@ static int mcux_pwm_set_cycles_internal(const struct device *dev, uint32_t chann
 				      &data->channel[channel], 1U,
 				      config->mode, clock_freq >> config->prescale, clock_freq);
 		if (status != kStatus_Success) {
-			LOG_ERR("Could not set up pwm");
+			LOG_ERR("Could not set up pwm (%d)", status);
 			return -ENOTSUP;
 		}
 
