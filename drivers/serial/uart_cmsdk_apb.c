b1871baacda502bdb87030dbc13b988b4a8181d4
Marco Widmer <marco.widmer@bytesatwork.ch>
2026-01-14 11:53:00 +0100

    drivers: serial: cmsdk_apb: "prime" the rx interrupt
    Similar to the TX interrupt, the RX interrupt is also an edge interrupt
    that only fires when a new bytes is received. When enabling the RX
    interrupt, data may be already present in the RX FIFO. Call the
    interrupt handler manually to start processing.
    
    Signed-off-by: Marco Widmer <marco.widmer@bytesatwork.ch>

--- drivers/serial/uart_cmsdk_apb.c
+++ drivers/serial/uart_cmsdk_apb.c
@@ -340,8 +340,13 @@ static int uart_cmsdk_apb_irq_tx_ready(const struct device *dev)
 static void uart_cmsdk_apb_irq_rx_enable(const struct device *dev)
 {
 	const struct uart_cmsdk_apb_config *dev_cfg = dev->config;
+	unsigned int key;
 
 	dev_cfg->uart->ctrl |= UART_RX_IN_EN;
+	/* "Prime" the interrupt as described above */
+	key = irq_lock();
+	uart_cmsdk_apb_isr(dev);
+	irq_unlock(key);
 }
 
 /**
