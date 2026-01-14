1b84158ea4b92f9a60543a8eb61fd983a6c57b78
Benjamin Cabé <benjamin@zephyrproject.org>
2026-01-14 23:50:46 +0100

    cmake: doc: Add CMake build system reference documentation
    Document the CMake modules, variables and target properties exposed by
    the Zephyr build system, using the upstream CMake Sphinx domain so that
    entities can be cross-referenced from the rest of the documentation
    rather than looked up by reading the .cmake sources.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- cmake/modules/soc.cmake
+++ cmake/modules/soc.cmake
@@ -2,20 +2,27 @@
 #
 # Copyright (c) 2021, Nordic Semiconductor ASA
 
-# Configure SoC settings based on Kconfig settings.
-#
-# This CMake module will set the following variables in the build system based
-# on Kconfig settings for the selected SoC.
-#
-# Outcome:
-# The following variables will be defined when this CMake module completes:
-#
-# - SOC_FULL_DIR: Full directory of where the SoC files are located
-# - SOC_DIRECTORIES: List of directories where SoC files which include this SoC are located
-# - SOC_TOOLCHAIN_NAME: Optional toolchain name of the SoC
-#
-# Variables set by this module and not mentioned above are considered internal
-# use only and may be removed, renamed, or re-purposed without prior notice.
+#[=======================================================================[.rst:
+soc
+***
+
+Configure SoC settings based on Kconfig settings.
+
+This CMake module will set the following variables in the build system based on Kconfig settings
+for the selected SoC.
+
+Variables
+=========
+
+The following variables will be defined when this CMake module completes:
+
+* ``SOC_FULL_DIR``: Full directory of where the SoC files are located
+* ``SOC_DIRECTORIES``: List of directories where SoC files which include this SoC are located
+* ``SOC_TOOLCHAIN_NAME``: Optional toolchain name of the SoC
+
+Variables set by this module and not mentioned above are considered internal use only and may be
+removed, renamed, or re-purposed without prior notice.
+#]=======================================================================]
 
 include_guard(GLOBAL)
 
---
+++ doc/build/cmake-ref/variable/LLEXT_REMOVE_FLAGS.rst
@@ -0,0 +1,8 @@
+LLEXT_REMOVE_FLAGS
+##################
+
+Global list of regular expressions describing compiler flags to be removed from compiler
+invocations when compiling extensions.
+Target and compiler dependent, usually set in toolchain files.
+
+See :cmake:command:`add_llext_target`.
