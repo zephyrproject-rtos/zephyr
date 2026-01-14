e97b718a776f478bf9451e8a52a2dd645ea27774
Pieter De Gendt <pieter.degendt@basalte.be>
2026-01-14 16:51:07 +0100

    scripts: dts: gen_dts_cmake.py: Prevent trailing semicolon for lists
    The generated lists have a trailing semicolon, which for CMake would
    indicate a trailing empty item.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- scripts/dts/gen_dts_cmake.py
+++ scripts/dts/gen_dts_cmake.py
@@ -123,9 +123,7 @@ def main():
                 if "phandle" not in node.props[item].type:
                     if "array" in node.props[item].type:
                         # Convert array to CMake list
-                        cmake_value = ''
-                        for val in node.props[item].val:
-                            cmake_value = f'{cmake_value}{val};'
+                        cmake_value = ';'.join(str(val) for val in node.props[item].val)
                     else:
                         cmake_value = node.props[item].val
 
@@ -135,9 +133,8 @@ def main():
                     cmake_props.append(f'"{cmake_prop}" "{escape(cmake_value)}"')
         elif node.compats:
             # Manually output compatibles for nodes that have no properties
-            cmake_value = ''
-            for val in node.compats:
-                cmake_value = f'{cmake_value}{val};'
+            cmake_value = ';'.join(node.compats)
+
             cmake_prop = f'DT_PROP|{node.path}|compatible'
             cmake_props.append(f'"{cmake_prop}" "{escape(cmake_value)}"')
 
@@ -146,19 +143,13 @@ def main():
 
         if node.regs is not None:
             cmake_props.append(f'"DT_REG|{node.path}|NUM" "{len(node.regs)}"')
-            cmake_addr = ''
-            cmake_size = ''
 
-            for reg in node.regs:
-                if reg.addr is None:
-                    cmake_addr = f'{cmake_addr}NONE;'
-                else:
-                    cmake_addr = f'{cmake_addr}{hex(reg.addr)};'
-
-                if reg.size is None:
-                    cmake_size = f'{cmake_size}NONE;'
-                else:
-                    cmake_size = f'{cmake_size}{hex(reg.size)};'
+            cmake_addr = ';'.join(
+                'NONE' if reg.addr is None else hex(reg.addr) for reg in node.regs
+            )
+            cmake_size = ';'.join(
+                'NONE' if reg.size is None else hex(reg.size) for reg in node.regs
+            )
 
             cmake_props.append(f'"DT_REG|{node.path}|ADDR" "{cmake_addr}"')
             cmake_props.append(f'"DT_REG|{node.path}|SIZE" "{cmake_size}"')
@@ -168,12 +159,7 @@ def main():
             cmake_props.append(f'"DT_UNIT_ADDR|{node.path}" "{cmake_unit_addr_int}"')
 
     for comp in compatible2paths.keys():
-        cmake_path = ''
-        for path in compatible2paths[comp]:
-            cmake_path = f'{cmake_path}{path};'
-
-        # Remove the last ';'
-        cmake_path = cmake_path[:-1]
+        cmake_path = ';'.join(compatible2paths[comp])
 
         cmake_comp = f'DT_COMP|{comp}'
         cmake_props.append(f'"{cmake_comp}" "{cmake_path}"')
