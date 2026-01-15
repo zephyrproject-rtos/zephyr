26aa4765fd3ed274b3669a28b64ca2fd344dcd4b
Marek Matej <marek.matej@espressif.com>
2026-01-15 20:41:26 +0000

    samples: esp_tool: update sample code
    Signed-off-by: Marek Matej <marek.matej@espressif.com>

--- samples/drivers/misc/esp_tool/src/main.c
+++ samples/drivers/misc/esp_tool/src/main.c
@@ -9,6 +9,7 @@
 #include <zephyr/drivers/misc/esp_tool/esp_tool.h>
 #include <zephyr/device.h>
 
+/* local */
 #include "hexdump.h"
 
 static const struct device *esp = DEVICE_DT_GET(DT_INST(0, espressif_esp_tool));
@@ -19,6 +20,7 @@ int main(void)
 	int ret;
 	uint32_t flash_size, chip_id;
 	uint32_t offset;
+	char *name;
 
 	ret = device_is_ready(esp);
 	if (ret == 0) {
@@ -41,14 +43,22 @@ int main(void)
 	}
 	printk("Target chip id is %x\n", chip_id);
 
+	ret = esp_tool_get_target_name(esp, &name);
+	if (ret) {
+		printk("Target name detection failed!\n");
+		return -1;
+	}
+	printk("Target name %s\n", name);
+
 	ret = esp_tool_flash_detect_size(esp, &flash_size);
 	if (ret) {
 		printk("Could not detected flash size");
 		return -1;
 	}
-	printk("Detected flash size is %d MB (%d Bytes)\n", flash_size/1024/1024, flash_size);
+	printk("Detected flash size is %d MB (%d Bytes)\n",
+		flash_size/1024/1024, flash_size);
 
-	if (esp_tool_get_boot_offset(esp, chip_id, &offset)) {
+	if (esp_tool_get_boot_offset(esp, &offset)) {
 		printk("boot offset failed\n");
 		return -1;
 	}
@@ -63,7 +73,7 @@ int main(void)
 	hexdump("0x0", buf, sizeof(buf));
 
 	if (buf[0] == 0xe9) {
-		printk("Boot vector installed");
+		printk("Boot vector installed\n");
 	} else {
 		printk("Target chip not bootable!");
 	}
@@ -72,7 +82,7 @@ int main(void)
 
 	ret = esp_tool_reset_target(esp);
 	if (ret) {
-		printk("Target reset failed\n");
+		printk(" Failed\n");
 		return -1;
 	}
 	printk("OK\n\n");
