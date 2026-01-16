a50f778fae8207fb921bbf5bb3d3e5a05aca5d32
Flavio Ceolin <flavio@hubblenetwork.com>
2026-01-16 09:04:05 -0800

    pm: device_runtime: Fix documentation typo
    s/EWOUDBLOCK/EWOULDBLOCK in pm_device_runtime_get() documentation.
    
    Signed-off-by: Flavio Ceolin <flavio@hubblenetwork.com>

--- include/zephyr/pm/device_runtime.h
+++ include/zephyr/pm/device_runtime.h
@@ -91,7 +91,7 @@ int pm_device_runtime_disable(const struct device *dev);
  *
  * @retval 0 If it succeeds. In case device runtime PM is not enabled or not
  * available this function will be a no-op and will also return 0.
- * @retval -EWOUDBLOCK If call would block but it is not allowed (e.g. in ISR).
+ * @retval -EWOULDBLOCK If call would block but it is not allowed (e.g. in ISR).
  * @retval -errno Other negative errno, result of the PM action callback.
  */
 int pm_device_runtime_get(const struct device *dev);
