97dbd989e7a77d0e13a448ef0fa732b854381dc9
Deepika R <deepika@aerlync.com>
2026-01-20 19:36:10 +0530

    dt-bindings: adc: add Infineon PSoC4 SAR ADC bindings
    - Add a device tree binding header for the Infineon PSoC4 SAR ADC.
      Header provides descriptive macros for:
    - SARMUX hardware addresses (0-7) for dedicated port 2 pins.
    - GPIO pin mapping macros (Port 0 to Port 6) to simplify ADC channel
      configuration in devicetree overlays.
    
    Signed-off-by: Deepika R <deepika@aerlync.com>
    Signed-off-by: Sayooj K Karun <sayooj@aerlync.com>

---
+++ include/zephyr/dt-bindings/adc/infineon-psoc4-sar.h
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2025 Infineon Technologies AG
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_ADC_INFINEON_PSOC4_SAR_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_ADC_INFINEON_PSOC4_SAR_H_
+
+/*
+ * SARMUX Hardware Addresses
+ * Used for zephyr,input-positive and zephyr,input-negative when
+ * referencing dedicated SARMUX port 2
+ */
+
+/** SARMUX DEDICATED PORT2 PIN 0 */
+#define CY_SAR_ADDR_SARMUX_0 0x0
+/** SARMUX PORT2 PIN 1 */
+#define CY_SAR_ADDR_SARMUX_1 0x1
+/** SARMUX PORT2 PIN 2 */
+#define CY_SAR_ADDR_SARMUX_2 0x2
+/** SARMUX PORT2 PIN 3 */
+#define CY_SAR_ADDR_SARMUX_3 0x3
+/** SARMUX PORT2 PIN 4 */
+#define CY_SAR_ADDR_SARMUX_4 0x4
+/** SARMUX PORT2 PIN 5 */
+#define CY_SAR_ADDR_SARMUX_5 0x5
+/** SARMUX PORT2 PIN 6 */
+#define CY_SAR_ADDR_SARMUX_6 0x6
+/** SARMUX PORT2 PIN 7 */
+#define CY_SAR_ADDR_SARMUX_7 0x7
+
+/** GPIO Pin P0.0 */
+#define PSOC4_PIN_P0_0 0
+/** GPIO Pin P0.1 */
+#define PSOC4_PIN_P0_1 1
+/** GPIO Pin P0.2 */
+#define PSOC4_PIN_P0_2 2
+/** GPIO Pin P0.3 */
+#define PSOC4_PIN_P0_3 3
+/** GPIO Pin P0.4 */
+#define PSOC4_PIN_P0_4 4
+/** GPIO Pin P0.5 */
+#define PSOC4_PIN_P0_5 5
+/** GPIO Pin P0.6 */
+#define PSOC4_PIN_P0_6 6
+/** GPIO Pin P0.7 */
+#define PSOC4_PIN_P0_7 7
+
+/** GPIO Pin P1.0 */
+#define PSOC4_PIN_P1_0 8
+/** GPIO Pin P1.1 */
+#define PSOC4_PIN_P1_1 9
+/** GPIO Pin P1.2 */
+#define PSOC4_PIN_P1_2 10
+/** GPIO Pin P1.3 */
+#define PSOC4_PIN_P1_3 11
+/** GPIO Pin P1.4 */
+#define PSOC4_PIN_P1_4 12
+/** GPIO Pin P1.5 */
+#define PSOC4_PIN_P1_5 13
+/** GPIO Pin P1.6 */
+#define PSOC4_PIN_P1_6 14
+/** GPIO Pin P1.7 */
+#define PSOC4_PIN_P1_7 15
+
+/** GPIO Pin P2.0 */
+#define PSOC4_PIN_P2_0 16
+/** GPIO Pin P2.1 */
+#define PSOC4_PIN_P2_1 17
+/** GPIO Pin P2.2 */
+#define PSOC4_PIN_P2_2 18
+/** GPIO Pin P2.3 */
+#define PSOC4_PIN_P2_3 19
+/** GPIO Pin P2.4 */
+#define PSOC4_PIN_P2_4 20
+/** GPIO Pin P2.5 */
+#define PSOC4_PIN_P2_5 21
+/** GPIO Pin P2.6 */
+#define PSOC4_PIN_P2_6 22
+/** GPIO Pin P2.7 */
+#define PSOC4_PIN_P2_7 23
+
+/** GPIO Pin P3.0 */
+#define PSOC4_PIN_P3_0 24
+/** GPIO Pin P3.1 */
+#define PSOC4_PIN_P3_1 25
+/** GPIO Pin P3.2 */
+#define PSOC4_PIN_P3_2 26
+/** GPIO Pin P3.3 */
+#define PSOC4_PIN_P3_3 27
+/** GPIO Pin P3.4 */
+#define PSOC4_PIN_P3_4 28
+/** GPIO Pin P3.5 */
+#define PSOC4_PIN_P3_5 29
+/** GPIO Pin P3.6 */
+#define PSOC4_PIN_P3_6 30
+/** GPIO Pin P3.7 */
+#define PSOC4_PIN_P3_7 31
+
+/** GPIO Pin P4.0 */
+#define PSOC4_PIN_P4_0 32
+/** GPIO Pin P4.1 */
+#define PSOC4_PIN_P4_1 33
+/** GPIO Pin P4.2 */
+#define PSOC4_PIN_P4_2 34
+/** GPIO Pin P4.3 */
+#define PSOC4_PIN_P4_3 35
+/** GPIO Pin P4.4 */
+#define PSOC4_PIN_P4_4 36
+/** GPIO Pin P4.5 */
+#define PSOC4_PIN_P4_5 37
+/** GPIO Pin P4.6 */
+#define PSOC4_PIN_P4_6 38
+/** GPIO Pin P4.7 */
+#define PSOC4_PIN_P4_7 39
+
+/** GPIO Pin P5.0 */
+#define PSOC4_PIN_P5_0 40
+/** GPIO Pin P5.1 */
+#define PSOC4_PIN_P5_1 41
+/** GPIO Pin P5.2 */
+#define PSOC4_PIN_P5_2 42
+/** GPIO Pin P5.3 */
+#define PSOC4_PIN_P5_3 43
+/** GPIO Pin P5.4 */
+#define PSOC4_PIN_P5_4 44
+/** GPIO Pin P5.5 */
+#define PSOC4_PIN_P5_5 45
+/** GPIO Pin P5.6 */
+#define PSOC4_PIN_P5_6 46
+/** GPIO Pin P5.7 */
+#define PSOC4_PIN_P5_7 47
+
+/** GPIO Pin P6.0 */
+#define PSOC4_PIN_P6_0 48
+/** GPIO Pin P6.1 */
+#define PSOC4_PIN_P6_1 49
+/** GPIO Pin P6.2 */
+#define PSOC4_PIN_P6_2 50
+/** GPIO Pin P6.3 */
+#define PSOC4_PIN_P6_3 51
+/** GPIO Pin P6.4 */
+#define PSOC4_PIN_P6_4 52
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_ADC_INFINEON_PSOC4_SAR_H_ */
