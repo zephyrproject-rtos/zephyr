1e40e4426205172c304dc5dc0685285fd6924c08
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2026-01-20 08:33:43 +0100

    [nrf fromtree] soc: nordic: common: nrf_sys_event: Fix missing return
    nrf_sys_event_unregister was missing an immediate return when PPI
    was used by the handle. It was stepping into non-PPI case where
    an assert might have been triggered.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
    (cherry picked from commit b0ab79429a233f1def3012c8271d6b2b3d065afe)

--- soc/nordic/common/nrf_sys_event.c
+++ soc/nordic/common/nrf_sys_event.c
@@ -112,6 +112,12 @@ BUILD_ASSERT(IS_ENABLED(CONFIG_NRF_SYS_EVENT_IRQ_LATENCY_MANUAL) ||
 static uint32_t event_ref_cnt;
 static uint32_t chan_mask;
 
+/* Handle returned by the registering function can be a GRTC channel that was used which indicates
+ * that PPI RRAMC wake up is used. If manual mode is used (changing RRAMC power mode) than that
+ * handle value is used which exceeds any potential GRTC channel number.
+ */
+#define NRF_SYS_EVENT_MANUAL_HANDLE 32
+
 #define NVM_HW_WAKEUP_US 16
 #define NVM_MANUAL_SUPPORT IS_ENABLED(CONFIG_NRF_SYS_EVENT_IRQ_LATENCY_MANUAL)
 /* Due to software performance and risk of waking up too early (then RRAMC may go
@@ -154,7 +160,7 @@ union nrf_sys_evt_us {
 	uint64_t abs;
 };
 
-int event_register(union nrf_sys_evt_us us, bool force, bool abs)
+static int event_register(union nrf_sys_evt_us us, bool force, bool abs)
 {
 	int rv;
 
@@ -181,7 +187,7 @@ int event_register(union nrf_sys_evt_us us, bool force, bool abs)
 				irq_low_latency_on(true);
 			}
 			event_ref_cnt++;
-			rv = 32;
+			rv = NRF_SYS_EVENT_MANUAL_HANDLE;
 		}
 	}
 
@@ -203,11 +209,12 @@ int nrf_sys_event_unregister(int handle, bool cancel)
 	__ASSERT_NO_MSG(handle >= 0);
 	int rv = 0;
 
-	if (handle < 32) {
+	if (handle != NRF_SYS_EVENT_MANUAL_HANDLE) {
 		if (cancel) {
 			nrf_grtc_sys_counter_compare_event_disable(NRF_GRTC, handle);
 		}
 		atomic_or((atomic_t *)&chan_mask, BIT(handle));
+		return rv;
 	}
 
 	LOCKED() {
