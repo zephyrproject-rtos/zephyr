336169e2c8d605cfdef7bae0e5fafc229d3668b5
Julien Panis <jpanis@baylibre.com>
2026-01-21 11:01:37 +0100

    drivers: pinctrl: renesas: Add support for R-Car X5H pinctrl
    Add support for pinctrl to R-Car X5H SoC. Like for other Renesas SoCs,
    a node approach is implemented (no grouping approach).
    
    Pin configuration operations can optionally be performed through SCMI
    (System Control and Management Interface).
    
    Signed-off-by: Julien Panis <jpanis@baylibre.com>

---
+++ soc/renesas/rcar/rcar_gen5/r52/pinctrl_soc.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2026 BayLibre, SAS
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef ZEPHYR_SOC_ARM_RENESAS_RCAR_GEN5_PINCTRL_SOC_H_
+#define ZEPHYR_SOC_ARM_RENESAS_RCAR_GEN5_PINCTRL_SOC_H_
+
+#include <zephyr/drivers/pinctrl/pinctrl_rcar_x5h_common.h>
+
+#endif /* ZEPHYR_SOC_ARM_RENESAS_RCAR_GEN5_PINCTRL_SOC_H_ */
