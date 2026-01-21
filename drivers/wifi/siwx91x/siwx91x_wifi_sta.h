585fb9503451204a3f1c5ed5f575258eb5abc40e
Jérôme Pouiller <jerome.pouiller@silabs.com>
2026-01-21 17:21:13 +0100

    drivers: wifi: siwx91x: Use sl_wifi_callback_function_v2 API
    Let's get rid of deprecated sl_wifi_set_callback. The new
    sl_wifi_set_callback_v2 changes the callback prototypes, but they kept the
    semantic.
    
    We also take this opportunity to slightly clean up the prototupes of the
    callbacks.
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- drivers/wifi/siwx91x/siwx91x_wifi_sta.h
+++ drivers/wifi/siwx91x/siwx91x_wifi_sta.h
@@ -13,8 +13,9 @@ struct wifi_connect_req_params;
 int siwx91x_connect(const struct device *dev, struct wifi_connect_req_params *params);
 int siwx91x_disconnect(const struct device *dev);
 
-sl_status_t siwx91x_wifi_module_stats_event_handler(sl_wifi_event_t event, void *response,
-						    uint32_t result_length, void *arg);
-sl_status_t siwx91x_on_join(sl_wifi_event_t event, char *result, uint32_t result_size, void *arg);
+unsigned int siwx91x_wifi_module_stats_event_handler(sl_wifi_event_t event, unsigned int status,
+						    void *data, uint32_t data_length, void *arg);
+unsigned int siwx91x_on_join(sl_wifi_event_t event, unsigned int status,
+			     void *data, uint32_t data_length, void *arg);
 
 #endif
