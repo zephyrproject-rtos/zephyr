675b54c15ca01c3ff5fece3e40056fa49e3abf1d
Zhijie Zhong <zhongzhijie1@xiaomi.com>
2026-01-21 22:38:27 +0800

    Bluetooth: Core: add Mode Change event in event mask
    Since we have already supported sniff mode,
    the corresponding link mode change event should be received.
    
    Signed-off-by: Zhijie Zhong <zhongzhijie1@xiaomi.com>

--- include/zephyr/bluetooth/hci_types.h
+++ include/zephyr/bluetooth/hci_types.h
@@ -4234,6 +4234,7 @@ struct bt_hci_evt_le_conn_rate_change {
 #define BT_EVT_MASK_REMOTE_VERSION_INFO          BT_EVT_BIT(11)
 #define BT_EVT_MASK_HARDWARE_ERROR               BT_EVT_BIT(15)
 #define BT_EVT_MASK_ROLE_CHANGE                  BT_EVT_BIT(17)
+#define BT_EVT_MASK_MODE_CHANGE                  BT_EVT_BIT(19)
 #define BT_EVT_MASK_PIN_CODE_REQ                 BT_EVT_BIT(21)
 #define BT_EVT_MASK_LINK_KEY_REQ                 BT_EVT_BIT(22)
 #define BT_EVT_MASK_LINK_KEY_NOTIFY              BT_EVT_BIT(23)
