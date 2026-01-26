b1b3edd927f76e0a523c8c77d9a9c88fe2dd2a10
Erwan Gouriou <erwan.gouriou@st.com>
2026-01-26 09:44:10 +0100

    drivers: flash: stm32_xspi: Rework code relating to some properties
    Some DT properties where used directly in the code w/o being put in
    instatiable device structs (cfg, data).
    Move them in these structs and access it from there.
    
    Change definition of STM32_XSPI_RESET_GPIO to DT_ANY_INST
    
    While I'm at it, simplify use of DT macros, replace
    DT_NODE_HAS_PROP(DT_INST(0, st_stm32_ospi_nor), foo)
    by DT_INST_NODE_HAS_PROP(0, foo).
    
    Remove DT_XSPI_IO_PORT_PROP_OR, which is unused.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@st.com>

--- drivers/flash/flash_stm32_xspi.h
+++ drivers/flash/flash_stm32_xspi.h
@@ -76,6 +76,10 @@ struct flash_stm32_xspi_config {
 	int data_mode; /* SPI or QSPI or OSPI */
 	int data_rate; /* DTR or STR */
 	const struct pinctrl_dev_config *pcfg;
+	int reset_gpios_duration;
+	bool four_byte_opcodes;
+	bool requires_ulbpr;
+	uint32_t mem_map_based_address;
 #if STM32_XSPI_RESET_GPIO
 	const struct gpio_dt_spec reset;
 #endif /* STM32_XSPI_RESET_GPIO */
