2438d77403051e076300756c3eef161211b9c774
Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>
2026-01-28 17:09:12 +0200

    UPSTREAM: firmware: scmi: replace k_is_pre_kernel() with hardcoded value
    commit f096f2a82df5bea7e0acd8ea637d8ab96147b716 upstream.
    
    As of commit c92c8ab4611c
    ("firmware: scmi: core: merge send_message() logic"), the SCMI core is
    able to figure out that it needs to use polling-based messaging whenever
    in PRE_KERNEL stage. Thus, consumers are no longer required to query the
    kernel's state via a `k_is_pre_kernel()` call and pass its value to
    `scmi_send_message()`.
    
    Furthermore, said commit also changes the semantic of the "use_polling"
    parameter. Now, for interrupt-based messaging, the core may choose to
    ignore the caller's request and use polling instead.
    
    Thus, replace all usages of `k_is_pre_kernel()` with a hardcoded value.
    
    Signed-off-by: Laurentiu Mihalcea <laurentiu.mihalcea@nxp.com>

--- drivers/firmware/scmi/clk.c
+++ drivers/firmware/scmi/clk.c
@@ -39,7 +39,6 @@ int scmi_clock_rate_get(struct scmi_protocol *proto,
 	struct scmi_message msg, reply;
 	int ret;
 	struct scmi_clock_rate_set_reply reply_buffer;
-	bool use_polling;
 
 	/* input validation */
 	if (!proto || !rate) {
@@ -59,9 +58,7 @@ int scmi_clock_rate_get(struct scmi_protocol *proto,
 	reply.len = sizeof(reply_buffer);
 	reply.content = &reply_buffer;
 
-	use_polling = k_is_pre_kernel();
-
-	ret = scmi_send_message(proto, &msg, &reply, use_polling);
+	ret = scmi_send_message(proto, &msg, &reply, false);
 	if (ret < 0) {
 		return ret;
 	}
@@ -79,7 +76,6 @@ int scmi_clock_rate_set(struct scmi_protocol *proto, struct scmi_clock_rate_conf
 {
 	struct scmi_message msg, reply;
 	int status, ret;
-	bool use_polling;
 
 	/* input validation */
 	if (!proto || !cfg) {
@@ -103,9 +99,7 @@ int scmi_clock_rate_set(struct scmi_protocol *proto, struct scmi_clock_rate_conf
 	reply.len = sizeof(status);
 	reply.content = &status;
 
-	use_polling = k_is_pre_kernel();
-
-	ret = scmi_send_message(proto, &msg, &reply, use_polling);
+	ret = scmi_send_message(proto, &msg, &reply, false);
 	if (ret < 0) {
 		return ret;
 	}
@@ -118,7 +112,6 @@ int scmi_clock_parent_get(struct scmi_protocol *proto, uint32_t clk_id, uint32_t
 	struct scmi_message msg, reply;
 	int ret;
 	struct scmi_clock_parent_get_reply reply_buffer;
-	bool use_polling;
 
 	/* input validation */
 	if (!proto || !parent_id) {
@@ -138,9 +131,7 @@ int scmi_clock_parent_get(struct scmi_protocol *proto, uint32_t clk_id, uint32_t
 	reply.len = sizeof(reply_buffer);
 	reply.content = &reply_buffer;
 
-	use_polling = k_is_pre_kernel();
-
-	ret = scmi_send_message(proto, &msg, &reply, use_polling);
+	ret = scmi_send_message(proto, &msg, &reply, false);
 	if (ret < 0) {
 		return ret;
 	}
@@ -159,7 +150,6 @@ int scmi_clock_parent_set(struct scmi_protocol *proto, uint32_t clk_id, uint32_t
 	struct scmi_clock_parent_config cfg = {.clk_id = clk_id, .parent_id = parent_id};
 	struct scmi_message msg, reply;
 	int status, ret;
-	bool use_polling;
 
 	/* input validation */
 	if (!proto) {
@@ -179,9 +169,7 @@ int scmi_clock_parent_set(struct scmi_protocol *proto, uint32_t clk_id, uint32_t
 	reply.len = sizeof(status);
 	reply.content = &status;
 
-	use_polling = k_is_pre_kernel();
-
-	ret = scmi_send_message(proto, &msg, &reply, use_polling);
+	ret = scmi_send_message(proto, &msg, &reply, false);
 	if (ret < 0) {
 		return ret;
 	}
@@ -194,7 +182,6 @@ int scmi_clock_config_set(struct scmi_protocol *proto,
 {
 	struct scmi_message msg, reply;
 	int status, ret;
-	bool use_polling;
 
 	/* input validation */
 	if (!proto || !cfg) {
@@ -229,9 +216,7 @@ int scmi_clock_config_set(struct scmi_protocol *proto,
 	reply.len = sizeof(status);
 	reply.content = &status;
 
-	use_polling = k_is_pre_kernel();
-
-	ret = scmi_send_message(proto, &msg, &reply, use_polling);
+	ret = scmi_send_message(proto, &msg, &reply, false);
 	if (ret < 0) {
 		return ret;
 	}
@@ -244,7 +229,6 @@ int scmi_clock_protocol_attributes(struct scmi_protocol *proto, uint32_t *attrib
 	struct scmi_message msg, reply;
 	struct scmi_clock_attributes_reply reply_buffer;
 	int ret;
-	bool use_polling;
 
 	/* input validation */
 	if (!proto || !attributes) {
@@ -265,9 +249,7 @@ int scmi_clock_protocol_attributes(struct scmi_protocol *proto, uint32_t *attrib
 	reply.len = sizeof(reply_buffer);
 	reply.content = &reply_buffer;
 
-	use_polling = k_is_pre_kernel();
-
-	ret = scmi_send_message(proto, &msg, &reply, use_polling);
+	ret = scmi_send_message(proto, &msg, &reply, false);
 	if (ret < 0) {
 		return ret;
 	}
