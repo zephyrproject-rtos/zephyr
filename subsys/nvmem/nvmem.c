ab025c1f03b3f801ace7829663c8385900a39baa
Pieter De Gendt <pieter.degendt@basalte.be>
2026-01-28 15:45:24 +0100

    nvmem: Support MMIO memory access
    WIP
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- subsys/nvmem/nvmem.c
+++ subsys/nvmem/nvmem.c
@@ -12,6 +12,39 @@
 #include <zephyr/nvmem.h>
 #include <zephyr/sys/__assert.h>
 
+#ifdef CONFIG_NVMEM_MMIO
+
+#include <zephyr/sys/device_mmio.h>
+
+static int nvmem_mmio_read(const struct nvmem_cell *cell, void *buf, off_t off, size_t len)
+{
+	mm_reg_t addr;
+
+	device_map(&addr, cell->phys_addr + cell->offset, cell->size, K_MEM_CACHE_NONE);
+	memcpy(buf, (const void *)(addr + off), len);
+	device_unmap(addr, cell->size);
+
+	return 0;
+}
+
+static int nvmem_mmio_write(const struct nvmem_cell *cell, const void *buf, off_t off, size_t len)
+{
+	mm_reg_t addr;
+
+	device_map(&addr, cell->phys_addr + cell->offset, cell->size, K_MEM_CACHE_NONE);
+	memcpy((void *)(addr + off), buf, len);
+	device_unmap(addr, cell->size);
+
+	return 0;
+}
+
+#else
+
+#define nvmem_mmio_read(...) -ENOTSUP
+#define nvmem_mmio_write(...) -ENOTSUP
+
+#endif
+
 int nvmem_cell_read(const struct nvmem_cell *cell, void *buf, off_t off, size_t len)
 {
 	__ASSERT_NO_MSG(cell != NULL);
@@ -20,24 +53,30 @@ int nvmem_cell_read(const struct nvmem_cell *cell, void *buf, off_t off, size_t 
 		return -EINVAL;
 	}
 
-	if (!device_is_ready(cell->dev)) {
-		return -ENODEV;
+	if (cell->type == NVMEM_CELL_TYPE_MMIO) {
+		return nvmem_mmio_read(cell, buf, off, len);
 	}
 
-	if (IS_ENABLED(CONFIG_NVMEM_BBRAM) && DEVICE_API_IS(bbram, cell->dev)) {
-		return bbram_read(cell->dev, cell->offset + off, len, buf);
-	}
+	if (cell->type == NVMEM_CELL_TYPE_DEVICE) {
+		if (!device_is_ready(cell->dev)) {
+			return -ENODEV;
+		}
 
-	if (IS_ENABLED(CONFIG_NVMEM_EEPROM) && DEVICE_API_IS(eeprom, cell->dev)) {
-		return eeprom_read(cell->dev, cell->offset + off, buf, len);
-	}
+		if (IS_ENABLED(CONFIG_NVMEM_BBRAM) && DEVICE_API_IS(bbram, cell->dev)) {
+			return bbram_read(cell->dev, cell->offset + off, len, buf);
+		}
 
-	if (IS_ENABLED(CONFIG_NVMEM_FLASH) && DEVICE_API_IS(flash, cell->dev)) {
-		return flash_read(cell->dev, cell->offset + off, buf, len);
-	}
+		if (IS_ENABLED(CONFIG_NVMEM_EEPROM) && DEVICE_API_IS(eeprom, cell->dev)) {
+			return eeprom_read(cell->dev, cell->offset + off, buf, len);
+		}
 
-	if (IS_ENABLED(CONFIG_NVMEM_OTP) && DEVICE_API_IS(otp, cell->dev)) {
-		return otp_read(cell->dev, cell->offset + off, buf, len);
+		if (IS_ENABLED(CONFIG_NVMEM_FLASH) && DEVICE_API_IS(flash, cell->dev)) {
+			return flash_read(cell->dev, cell->offset + off, buf, len);
+		}
+
+		if (IS_ENABLED(CONFIG_NVMEM_OTP) && DEVICE_API_IS(otp, cell->dev)) {
+			return otp_read(cell->dev, cell->offset + off, buf, len);
+		}
 	}
 
 	return -ENXIO;
@@ -55,24 +94,30 @@ int nvmem_cell_write(const struct nvmem_cell *cell, const void *buf, off_t off, 
 		return -EROFS;
 	}
 
-	if (!device_is_ready(cell->dev)) {
-		return -ENODEV;
+	if (cell->type == NVMEM_CELL_TYPE_MMIO) {
+		return nvmem_mmio_write(cell, buf, off, len);
 	}
 
-	if (IS_ENABLED(CONFIG_NVMEM_BBRAM) && DEVICE_API_IS(bbram, cell->dev)) {
-		return bbram_write(cell->dev, cell->offset + off, len, buf);
-	}
+	if (cell->type == NVMEM_CELL_TYPE_DEVICE) {
+		if (!device_is_ready(cell->dev)) {
+			return -ENODEV;
+		}
 
-	if (IS_ENABLED(CONFIG_NVMEM_EEPROM) && DEVICE_API_IS(eeprom, cell->dev)) {
-		return eeprom_write(cell->dev, cell->offset + off, buf, len);
-	}
+		if (IS_ENABLED(CONFIG_NVMEM_BBRAM) && DEVICE_API_IS(bbram, cell->dev)) {
+			return bbram_write(cell->dev, cell->offset + off, len, buf);
+		}
 
-	if (IS_ENABLED(CONFIG_NVMEM_FLASH_WRITE) && DEVICE_API_IS(flash, cell->dev)) {
-		return flash_write(cell->dev, cell->offset + off, buf, len);
-	}
+		if (IS_ENABLED(CONFIG_NVMEM_EEPROM) && DEVICE_API_IS(eeprom, cell->dev)) {
+			return eeprom_write(cell->dev, cell->offset + off, buf, len);
+		}
 
-	if (IS_ENABLED(CONFIG_NVMEM_OTP_WRITE) && DEVICE_API_IS(otp, cell->dev)) {
-		return otp_program(cell->dev, cell->offset + off, buf, len);
+		if (IS_ENABLED(CONFIG_NVMEM_FLASH_WRITE) && DEVICE_API_IS(flash, cell->dev)) {
+			return flash_write(cell->dev, cell->offset + off, buf, len);
+		}
+
+		if (IS_ENABLED(CONFIG_NVMEM_OTP_WRITE) && DEVICE_API_IS(otp, cell->dev)) {
+			return otp_program(cell->dev, cell->offset + off, buf, len);
+		}
 	}
 
 	return -ENXIO;
