9840d675f81df71725bef00e82c2870a43652fb2
Qiang Zhang <qiang.zhang_6@nxp.com>
2026-01-29 13:45:26 +0800

    hal_nxp: mcux: add TSI driver component to build system
    Add the TSI v6 driver component (driver.tsi_v6) to the MCUX SDK build
    system when CONFIG_INPUT_MCUX_TSI is enabled.
    
    Signed-off-by: Qiang Zhang <qiang.zhang_6@nxp.com>

--- modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
+++ modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
@@ -140,6 +140,7 @@ set_variable_ifdef(CONFIG_S3MU_MCUX_S3MU        CONFIG_MCUX_COMPONENT_driver.s3m
 set_variable_ifdef(CONFIG_DAI_NXP_MICFIL        CONFIG_MCUX_COMPONENT_driver.pdm)
 set_variable_ifdef(CONFIG_PINCTRL_NXP_PORT      CONFIG_MCUX_COMPONENT_driver.port)
 set_variable_ifdef(CONFIG_INPUT_MCUX_KPP        CONFIG_MCUX_COMPONENT_driver.kpp)
+set_variable_ifdef(CONFIG_INPUT_MCUX_TSI        CONFIG_MCUX_COMPONENT_driver.tsi_v6)
 set_variable_ifdef(CONFIG_DMA_NXP_EDMA          CONFIG_MCUX_COMPONENT_driver.edma_soc_rev2)
 set_variable_ifdef(CONFIG_DISPLAY_MCUX_LCDIFV3  CONFIG_MCUX_COMPONENT_driver.lcdifv3)
 set_variable_ifdef(CONFIG_COUNTER_MCUX_SNVS_SRTC    CONFIG_MCUX_COMPONENT_driver.snvs_lp)
