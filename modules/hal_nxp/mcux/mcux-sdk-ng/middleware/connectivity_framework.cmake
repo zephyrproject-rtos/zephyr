77102664609a06f01e5d1ca2cf80eb9514d38433
Axel Le Bourhis <axel.lebourhis@nxp.com>
2026-01-30 16:47:27 +0100

    hal_nxp: integrate connectivity framework from mcuxsdk 26.03-pvw2
    The connectivity framework brings extra config flags in 26.03-pvw2
    to allow using minimal configuration for HCI support.
    
    Signed-off-by: Axel Le Bourhis <axel.lebourhis@nxp.com>

--- modules/hal_nxp/mcux/mcux-sdk-ng/middleware/connectivity_framework.cmake
+++ modules/hal_nxp/mcux/mcux-sdk-ng/middleware/connectivity_framework.cmake
@@ -8,13 +8,19 @@ if(CONFIG_SOC_SERIES_RW6XX)
         set(CONFIG_MCUX_COMPONENT_middleware.wireless.framework.platform.coex ON)
         set(CONFIG_MCUX_COMPONENT_middleware.wireless.framework.platform.rw61x ON)
 
-        zephyr_compile_definitions(gPlatformDisableVendorSpecificInit=1U)
+        zephyr_compile_definitions(
+            gPlatformDisableVendorSpecificInit=1U
+            gPlatformUseTimerManager_d=0
+        )
 
         if(CONFIG_NXP_MONOLITHIC_WIFI OR CONFIG_NXP_MONOLITHIC_NBU)
             zephyr_compile_definitions(
                 gPlatformMonolithicApp_d=1U
                 fw_cpu2_ble=fw_cpu2
                 fw_cpu2_combo=fw_cpu2
+                WIFI_LD_TARGET=LOAD_WIFI_FIRMWARE
+                BLE_LD_TARGET=LOAD_BLE_FIRMWARE
+                COMBO_LD_TARGET=LOAD_15D4_FIRMWARE
             )
 
             zephyr_compile_definitions_ifndef(CONFIG_NXP_MONOLITHIC_NBU
@@ -40,15 +46,28 @@ if(CONFIG_SOC_SERIES_MCXW7XX)
         set(CONFIG_MCUX_COMPONENT_middleware.wireless.framework.platform.ot ON)
         set_variable_ifdef(CONFIG_SOC_MCXW727C_CPU0 CONFIG_MCUX_COMPONENT_middleware.wireless.framework.platform.kw47_mcxw72)
         set_variable_ifdef(CONFIG_SOC_MCXW716C CONFIG_MCUX_COMPONENT_middleware.wireless.framework.platform.kw45_k32w1_mcxw71)
+        zephyr_compile_definitions(
+            gPlatformIcsUseWorkqueueRxProcessing_d=0
+            gPlatformUseTimerManager_d=0
+            gPlatformUseHwParameter_d=0
+            gPlatformNbuDebugGpioDAccessEnabled_d=0
+            gPlatformSetSfcConfigAtInit_d=0
+            gPlatformSetWakeUpDelayAtInit_d=0
+            gPlatformSetBleMaxTxPowerAtInit_d=0
+        )
     endif()
 endif()
 
 if(CONFIG_SOC_SERIES_MCXW2XX)
     if(CONFIG_BT)
+        set(CONFIG_MCUX_COMPONENT_driver.ostimer ON)
         set(CONFIG_MCUX_COMPONENT_middleware.wireless.framework ON)
         set(CONFIG_MCUX_COMPONENT_middleware.wireless.framework.platform ON)
         set(CONFIG_MCUX_COMPONENT_middleware.wireless.framework.platform.mcxw23 ON)
         set(CONFIG_MCUX_COMPONENT_middleware.wireless.framework.platform.ble ON)
+        zephyr_compile_definitions(
+            gPlatformUseTimerManager_d=0
+        )
     endif()
 endif()
 
