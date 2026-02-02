361e9663352c0e011f36b05064f07e84fe839641
Erwan Gouriou <erwan.gouriou@st.com>
2026-02-02 11:34:24 +0100

    drivers: flash: stm32_q/o/xspi: Add default to reset_gpios_duration
    Make use of a default 1ms value to reset_gpios_duration.
    Do not put default to dt prop reset-gpios-duration since it should remain
    optionnal.
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@st.com>

--- drivers/flash/flash_stm32_ospi.c
+++ drivers/flash/flash_stm32_ospi.c
@@ -913,7 +913,7 @@ static int stm32_ospi_mem_reset(const struct device *dev)
 
 	/* Generate RESETn pulse for the flash memory */
 	gpio_pin_configure_dt(&dev_cfg->reset, GPIO_OUTPUT_ACTIVE);
-	k_msleep(DT_INST_PROP(0, reset_gpios_duration));
+	k_msleep(DT_INST_PROP_OR(0, reset_gpios_duration, 1));
 	gpio_pin_set_dt(&dev_cfg->reset, 0);
 #else
 
