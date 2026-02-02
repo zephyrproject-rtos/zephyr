71efe471fc7085615c7d5b10d04b433c2246f255
Kate Wang <yumeng.wang@nxp.com>
2026-02-02 16:04:34 +0800

    samples: video: capture: Add support for ABGR32 pixel format
    Add handling for VIDEO_PIX_FMT_ABGR32 format in the display setup
    function.
    
    Signed-off-by: Kate Wang <yumeng.wang@nxp.com>

--- samples/drivers/video/capture/src/main.c
+++ samples/drivers/video/capture/src/main.c
@@ -69,6 +69,11 @@ static inline int app_setup_display(const struct device *const display_dev, cons
 			ret = display_set_pixel_format(display_dev, PIXEL_FORMAT_ARGB_8888);
 		}
 		break;
+	case VIDEO_PIX_FMT_ABGR32:
+		if (capabilities.current_pixel_format != PIXEL_FORMAT_ABGR_8888) {
+			ret = display_set_pixel_format(display_dev, PIXEL_FORMAT_ABGR_8888);
+		}
+		break;
 	default:
 		LOG_ERR("Display pixel format not supported by this sample");
 		return -ENOTSUP;
