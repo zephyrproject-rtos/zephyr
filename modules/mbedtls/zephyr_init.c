e2e667af892336986995f4da6a8660635ab32c1f
Valerio Setti <vsetti@baylibre.com>
2026-02-03 18:13:39 +0100

    modules: mbedtls: fix entropy polling
    ENTROPY_C is now an internal module in tf-psa-crypto so it cannot be
    included directly. So first thing all error codes are changed to
    standard Zephyr ones.
    
    Moreover MBEDTLS_ENTROPY_HARDWARE_ALT, MBEDTLS_NO_PLATFORM_ENTROPY and
    MBEDTLS_NO_DEFAULT_ENTROPY_SOURCES were removed. Now the platform must
    define MBEDTLS_PSA_DRIVER_GET_ENTROPY when not using
    MBEDTLS_PSA_CRYPTO_EXTERNAL_RNG.
    
    Due to the build symbol name change CONFIG_MBEDTLS_ENTROPY_POLL_ZEPHYR
    is modified to CONFIG_MBEDTLS_PSA_DRIVER_GET_ENTROPY to make a 1:1
    match between Kconfig and build symbol.
    
    Signed-off-by: Valerio Setti <vsetti@baylibre.com>

--- modules/mbedtls/zephyr_init.c
+++ modules/mbedtls/zephyr_init.c
@@ -12,8 +12,10 @@
  */
 
 #include <zephyr/init.h>
+#include <zephyr/kernel.h>
 #include <zephyr/app_memory/app_memdomain.h>
 #include <mbedtls/platform_time.h>
+#include <errno.h>
 
 #include <mbedtls/debug.h>
 
