f1eb856320e130993dc55fba6a1f229e034d70c0
Jacky Lee <jacky.lee@egistec.com>
2026-02-03 11:31:11 +0800

    soc: Enable `TIMER_READS_ITS_FREQUENCY_AT_RUNTIME` on et171
    Read the clock parameters calibrated at the factory to
    enable accurate clock calculations during runtime.
    
    Signed-off-by: Jacky Lee <jacky.lee@egistec.com>

--- soc/egis/et171/soc.c
+++ soc/egis/et171/soc.c
@@ -7,6 +7,13 @@
  #include <zephyr/init.h>
  #include <zephyr/kernel.h>
 
+/* ================================================================== ET171 HAL
+ * et171 core
+ */
+#include <et171_hal/et171.h>
+#include <et171_hal/et171_hal_smu.h>
+#include <et171_hal/et171_hal_otp.h>
+
 void soc_early_init_hook(void)
 {
 #if CONFIG_XIP && CONFIG_ICACHE
@@ -17,9 +24,21 @@ void soc_early_init_hook(void)
 	__asm__ volatile("fence.i" ::: "memory");
 #endif
 
+	/* Load Analog triming value from OTP */
+	HAL_OTP_LoadAnalogConfig();
+
+	/* Load Root clock triming value from OTP */
+	HAL_OTP_GetRootClock();
+
 	/* AHB ~200Mhz / 3 = 66MHz AHB , ~66Mhz / 2 / 2 = 18Mhz APB */
-	sys_write32(0x01 | 0x20 | 0x02, 0xF0100004U);
+	HAL_SMU_SetRootClock(ROOT_CLK_250M, ROOT_CLK_DIV_3, APB_CLK_DIV_2);
 
 	/* Workaround WFI wakeup issue. AE350_I2C->INTEN = 1;       */
 	sys_write32(1, 0xF0A00014U);
+
+#ifdef CONFIG_TIMER_READS_ITS_FREQUENCY_AT_RUNTIME
+	extern unsigned int z_clock_hw_cycles_per_sec;
+
+	z_clock_hw_cycles_per_sec = HAL_SMU_GetClock(SMU_CLK_APB);
+#endif
 }
