993e7e3286818011238965270436e7c67942dd69
Neil Chen <cheng.chen_1@nxp.com>
2026-02-03 11:33:10 +0800

    boards: nxp: frdm_mcxa344: Support lpcmp for NXP frdm_mcxa344 board
    Support lpcmp for NXP frdm_mcxa344 board.
    
    Signed-off-by: Neil Chen <cheng.chen_1@nxp.com>

--- boards/nxp/frdm_mcxa344/board.c
+++ boards/nxp/frdm_mcxa344/board.c
@@ -146,6 +146,12 @@ void board_early_init_hook(void)
 	CLOCK_EnableClock(kCLOCK_GateADC1);
 #endif
 
+#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(lpcmp0))
+	CLOCK_AttachClk(kFRO_LF_DIV_to_CMP0);
+	CLOCK_SetClockDiv(kCLOCK_DivCMP0_FUNC, 1U);
+	SPC_EnableActiveModeAnalogModules(SPC0, (kSPC_controlCmp0 | kSPC_controlCmp0Dac));
+#endif
+
 #if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(lpi2c0))
 	CLOCK_SetClockDiv(kCLOCK_DivLPI2C0, 1u);
 	CLOCK_AttachClk(kFRO_LF_DIV_to_LPI2C0);
