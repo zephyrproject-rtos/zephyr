349a69cb9cd71a9d3e6dc04481c7112f3714f466
Om Srivastava <srivastavaom97714@gmail.com>
2026-02-04 19:21:40 +0530

    tests: sensor: bh1750: add Doxygen documentation for emulator API
    Add file, struct, and member-level Doxygen comments to the BH1750
    emulator public header to satisfy documentation coverage requirements.
    
    Signed-off-by: Om Srivastava <srivastavaom97714@gmail.com>

--- include/zephyr/drivers/sensor/bh1750_emul.h
+++ include/zephyr/drivers/sensor/bh1750_emul.h
@@ -4,15 +4,34 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/**
+ * @file
+ * @brief BH1750 sensor emulator API
+ */
+
 #ifndef ZEPHYR_DRIVERS_SENSOR_BH1750_EMUL_H_
 #define ZEPHYR_DRIVERS_SENSOR_BH1750_EMUL_H_
 
 #include <stdint.h>
 #include <stdbool.h>
 
+/**
+ * @brief BH1750 emulator internal state
+ */
 struct bh1750_emul_data {
+	/**
+	 * Indicates whether the emulator is powered on.
+	 */
 	bool powered;
+
+	/**
+	 * Measurement mode configured in the emulator.
+	 */
 	uint8_t mode;
+
+	/**
+	 * Raw sensor value returned by the emulator.
+	 */
 	uint16_t raw;
 };
 
