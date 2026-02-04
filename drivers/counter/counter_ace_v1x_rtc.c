zephyrproject-rtos/zephyr

commit 7fc848153c0712121e74758448a474abfde753e5

drivers: counter: add kconfig for enabling 64b tick api
Add a kconfig to be used as a flag for indicating if it supports 64b
ticks and another flag which will enable the 64b api. The kconfig
indicating that it supports 64b is a dependency of the kconfig that
will enable the api.

Signed-off-by: Ryan McClelland <ryanmcclelland@meta.com>

drivers/counter/counter_ace_v1x_rtc.c
@@ -11,6 +11,16 @@
 #include <soc.h>
 #include <counter/counter_ace_v1x_rtc_regs.h>
 
+static int counter_ace_v1x_rtc_get_value_32(const struct device *dev,
+		uint32_t *value)
+{
+	ARG_UNUSED(dev);
+
+	*value = sys_read32(ACE_RTCWC_LO);
+
+	return 0;
+}
+
 static int counter_ace_v1x_rtc_get_value(const struct device *dev,
 		uint64_t *value)
 {
@@ -36,7 +46,10 @@ int counter_ace_v1x_rtc_init(const struct device *dev)
 }
 
 static DEVICE_API(counter, ace_v1x_rtc_counter_apis) = {
-	.get_value_64 = counter_ace_v1x_rtc_get_value
+	.get_value = counter_ace_v1x_rtc_get_value_32,
+#ifdef CONFIG_COUNTER_64BITS_TICKS
+	.get_value_64 = counter_ace_v1x_rtc_get_value,
+#endif /* CONFIG_COUNTER_64BITS_TICKS */
 };
 
 DEVICE_DT_DEFINE(DT_NODELABEL(ace_rtc_counter), counter_ace_v1x_rtc_init, NULL, NULL, NULL,

drivers/counter/Kconfig
@@ -29,12 +29,20 @@ config COUNTER_64BITS_FREQ
 	  This is typically set by drivers that have frequencies greater than
 	  4,294,967,295 Hz.
 
-config COUNTER_64BITS_TICKS
+config COUNTER_SUPPORTS_64BITS_TICKS
 	bool
 	help
-	  Select this option if the counter driver should use 64-bits for ticks.
+	  Select this option if the counter driver supports 64-bits for ticks.
 	  This is typically set by drivers that require extended
 	  tick ranges beyond the 32-bit limit.
+
+config COUNTER_64BITS_TICKS
+	bool "API to support ticks longer than 32-bits"
+	depends on COUNTER_SUPPORTS_64BITS_TICKS
+	help
+	  This enables APIs to handle ticks larger than 32-bits with a
+	  64-bit typedef. It is up to the driver to implement the necessary
+	  functions to supports.
 
 module = COUNTER
 module-str = counter

drivers/counter/Kconfig.ace
@@ -5,7 +5,7 @@
 config ACE_V1X_ART_COUNTER
 	bool "DSP ART Wall Clock for ACE V1X"
 	depends on DT_HAS_INTEL_ACE_ART_COUNTER_ENABLED
-	select COUNTER_64BITS_TICKS
+	select COUNTER_SUPPORTS_64BITS_TICKS
 	default y
 	help
 	  DSP ART Wall Clock used by ACE V1X.
@@ -13,7 +13,7 @@ config ACE_V1X_ART_COUNTER
 config ACE_V1X_RTC_COUNTER
 	bool "DSP RTC Wall Clock for ACE V1X"
 	depends on DT_HAS_INTEL_ACE_RTC_COUNTER_ENABLED
-	select COUNTER_64BITS_TICKS
+	select COUNTER_SUPPORTS_64BITS_TICKS
 	default y
 	help
 	  DSP RTC Wall Clock used by ACE V1X.

drivers/counter/Kconfig.cc23x0_rtc
@@ -6,6 +6,6 @@ config COUNTER_CC23X0_RTC
 	default y
 	depends on DT_HAS_TI_CC23X0_RTC_ENABLED
 	depends on CC23X0_SYSTIM_TIMER
-	select COUNTER_64BITS_TICKS
+	select COUNTER_SUPPORTS_64BITS_TICKS
 	help
 	  Enable counter driver based on RTC timer for cc23x0

drivers/counter/Kconfig.esp32_tmr
@@ -7,7 +7,7 @@ config COUNTER_TMR_ESP32
 	bool "ESP32 Counter Driver based on GP-Timers"
 	default y
 	depends on DT_HAS_ESPRESSIF_ESP32_COUNTER_ENABLED
-	select COUNTER_64BITS_TICKS
+	select COUNTER_SUPPORTS_64BITS_TICKS
 	help
 	  Enables the Counter driver API based on Espressif's General
 	  Purpose Timers for ESP32 series devices.

drivers/counter/Kconfig.stm32_rtc
@@ -23,6 +23,7 @@ config COUNTER_RTC_STM32_SAVE_VALUE_BETWEEN_RESETS
 config COUNTER_RTC_STM32_SUBSECONDS
 	bool "Use the subseconds as a basic tick."
 	depends on !SOC_SERIES_STM32F1X
+	select COUNTER_SUPPORTS_64BITS_TICKS
 	select COUNTER_64BITS_TICKS
 	help
 	  Use the subseconds as the basic time tick. It increases resolution

drivers/counter/counter_ace_v1x_art.c
@@ -59,6 +59,11 @@ static void counter_ace_v1x_art_hhtse_set(bool enable)
 	sys_write32(val, ACE_TSCTRL);
 }
 
+static uint32_t counter_ace_v1x_art_counter_get_32(void)
+{
+	return sys_read32(ACE_ARTCS_LO);
+}
+
 static uint64_t counter_ace_v1x_art_counter_get(void)
 {
 	uint32_t hi0, lo, hi1;
@@ -70,6 +75,36 @@ static uint64_t counter_ace_v1x_art_counter_get(void)
 	} while (hi0 != hi1);
 
 	return (((uint64_t)hi1) << 32) | lo;
+}
+
+int counter_ace_v1x_art_get_value_32(const struct device *dev, uint32_t *value)
+{
+	ARG_UNUSED(dev);
+
+	k_spinlock_key_t key = k_spin_lock(&lock);
+
+	counter_ace_v1x_art_ionte_set(1);
+	counter_ace_v1x_art_cdmas_set(1);
+
+	if (counter_ace_v1x_art_ntk_get()) {
+		counter_ace_v1x_art_ntk_set(1);
+		while (counter_ace_v1x_art_ntk_get()) {
+			k_busy_wait(10);
+		}
+	}
+
+	counter_ace_v1x_art_hhtse_set(1);
+
+	while (!counter_ace_v1x_art_ntk_get()) {
+		k_busy_wait(10);
+	}
+
+	*value = counter_ace_v1x_art_counter_get_32();
+
+	counter_ace_v1x_art_ntk_set(1);
+	k_spin_unlock(&lock, key);
+
+	return 0;
 }
 
 int counter_ace_v1x_art_get_value(const struct device *dev, uint64_t *value)
@@ -103,7 +138,10 @@ int counter_ace_v1x_art_get_value(const struct device *dev, uint64_t *value)
 }
 
 static DEVICE_API(counter, ace_v1x_art_counter_apis) = {
-	.get_value_64 = counter_ace_v1x_art_get_value
+	.get_value = counter_ace_v1x_art_get_value_32,
+#ifdef CONFIG_COUNTER_64BITS_TICKS
+	.get_value_64 = counter_ace_v1x_art_get_value,
+#endif /* CONFIG_COUNTER_64BITS_TICKS */
 };
 
 DEVICE_DT_DEFINE(DT_NODELABEL(ace_art_counter), NULL, NULL, NULL, NULL,

drivers/counter/counter_cc23x0_rtc.c
@@ -42,6 +42,7 @@ static int counter_cc23x0_get_value(const struct device *dev, uint32_t *ticks)
 	return 0;
 }
 
+#ifdef CONFIG_COUNTER_64BITS_TICKS
 static int counter_cc23x0_get_value_64(const struct device *dev, uint64_t *ticks)
 {
 	const struct counter_cc23x0_config *config = dev->config;
@@ -61,6 +62,7 @@ static int counter_cc23x0_get_value_64(const struct device *dev, uint64_t *ticks
 
 	return 0;
 }
+#endif /* CONFIG_COUNTER_64BITS_TICKS */
 
 static void counter_cc23x0_isr(const struct device *dev)
 {
@@ -230,7 +232,9 @@ static DEVICE_API(counter, rtc_cc23x0_api) = {
 	.start = counter_cc23x0_start,
 	.stop = counter_cc23x0_stop,
 	.get_value = counter_cc23x0_get_value,
+#ifdef CONFIG_COUNTER_64BITS_TICKS
 	.get_value_64 = counter_cc23x0_get_value_64,
+#endif /* CONFIG_COUNTER_64BITS_TICKS */
 	.set_alarm = counter_cc23x0_set_alarm,
 	.cancel_alarm = counter_cc23x0_cancel_alarm,
 	.get_top_value = counter_cc23x0_get_top_value,

commit 961856fc532965a83fd4f6f2b9eac00ec4175da3

drivers: counter: Fix unused function warning
This fixes an "unused function" warning, a regression introduced with
commit 7fc8481.

Signed-off-by: Serhiy Katsyuba <serhiy.katsyuba@intel.com>

drivers/counter/counter_ace_v1x_rtc.c
@@ -21,6 +21,7 @@ static int counter_ace_v1x_rtc_get_value_32(const struct device *dev,
 	return 0;
 }
 
+#ifdef CONFIG_COUNTER_64BITS_TICKS
 static int counter_ace_v1x_rtc_get_value(const struct device *dev,
 		uint64_t *value)
 {
@@ -37,6 +38,7 @@ static int counter_ace_v1x_rtc_get_value(const struct device *dev,
 
 	return 0;
 }
+#endif /* CONFIG_COUNTER_64BITS_TICKS */
 
 int counter_ace_v1x_rtc_init(const struct device *dev)
 {