zephyrproject-rtos/zephyr

commit 01d35752a4c963348bc90666bb899b57befb58d4

mm: add sys_mm_drv_map_* functions with region check
the commit adds sys_mm_drv_map_page_safe and sys_mm_drv_map_region_safe
functions, wrappers for sys_mm_drv_map_region and sys_mm_drv_map_region,
with additional check if a mapped region fits into given memory range

Using of those prevents collisions and/or hijacking of virtual memory

Signed-off-by: Marcin Szkudlinski <marcin.szkudlinski@intel.com>

drivers/mm/mm_drv_common.c
@@ -29,6 +29,37 @@
 
 struct k_spinlock sys_mm_drv_common_lock;
 
+int sys_mm_drv_map_region_safe(const struct sys_mm_drv_region *virtual_region,
+			       void *virt, uintptr_t phys, size_t size,
+			       uint32_t flags)
+{
+	uintptr_t virtual_region_start = POINTER_TO_UINT(virtual_region->addr);
+	uintptr_t virtual_region_end = virtual_region_start + virtual_region->size;
+
+	/* check if memory to be mapped is within given virtual region */
+	if ((POINTER_TO_UINT(virt) >= virtual_region_start) &&
+	    (POINTER_TO_UINT(virt) + size < virtual_region_end)) {
+		return sys_mm_drv_map_region(virt, phys, size, flags);
+	}
+
+	return -EINVAL;
+}
+
+int sys_mm_drv_map_page_safe(const struct sys_mm_drv_region *virtual_region,
+			     void *virt, uintptr_t phys, uint32_t flags)
+{
+	uintptr_t virtual_region_start = POINTER_TO_UINT(virtual_region->addr);
+	uintptr_t virtual_region_end = virtual_region_start + virtual_region->size;
+
+	/* check if memory to be mapped is within given virtual region */
+	if ((POINTER_TO_UINT(virt) >= virtual_region_start) &&
+	    (POINTER_TO_UINT(virt) + CONFIG_MM_DRV_PAGE_SIZE < virtual_region_end)) {
+		return sys_mm_drv_map_page(virt, phys, flags);
+	}
+
+	return -EINVAL;
+}
+
 bool sys_mm_drv_is_addr_array_aligned(uintptr_t *addr, size_t cnt)
 {
 	size_t idx;

include/zephyr/drivers/mm/system_mm.h
@@ -55,6 +55,18 @@ extern "C" {
 #define SYS_MM_MEM_CACHE_MASK		(BIT(3) - 1)
 
 /**
+ * @brief Represents an available memory region.
+ *
+ * A memory region that can be used by allocators. Driver defined
+ * attributes can be used to guide the proper usage of each region.
+ */
+struct sys_mm_drv_region {
+	void *addr; /**< @brief Address of the memory region */
+	size_t size; /**< @brief Size of the memory region */
+	uint32_t attr; /**< @brief Driver defined attributes of the memory region */
+};
+
+/**
  * @}
  */
 
@@ -110,6 +122,18 @@ extern "C" {
 int sys_mm_drv_map_page(void *virt, uintptr_t phys, uint32_t flags);
 
 /**
+ * @brief Map one physical page into the virtual address space with region check
+ *
+ * This maps one physical page into the virtual address space by calling
+ * sys_mm_drv_map_page. Refer to sys_mm_drv_map_page for references
+ *
+ * Before call it performs a safety check by verifying if the mapped virtual memory page
+ * fits into a given virtual region
+ */
+int sys_mm_drv_map_page_safe(const struct sys_mm_drv_region *virtual_region,
+			     void *virt, uintptr_t phys, uint32_t flags);
+
+/**
  * @brief Map a region of physical memory into the virtual address space
  *
  * This maps a region of physical memory into the virtual address space.
@@ -132,6 +156,19 @@ int sys_mm_drv_map_page(void *virt, uintptr_t phys, uint32_t flags);
  */
 int sys_mm_drv_map_region(void *virt, uintptr_t phys,
 			  size_t size, uint32_t flags);
+
+/**
+ * @brief Map a region of physical memory into the virtual address space with region check
+ *
+ * This maps a region of physical memory into the virtual address space by calling
+ * sys_mm_drv_map_region. Refer to sys_mm_drv_map_region for references
+ *
+ * Before call it performs a safety check by verifying if the mapped virtual memory pages
+ * fit into a given virtual region
+ */
+int sys_mm_drv_map_region_safe(const struct sys_mm_drv_region *virtual_region,
+			       void *virt, uintptr_t phys, size_t size,
+			       uint32_t flags);
 
 /**
  * @brief Map an array of physical memory into the virtual address space
@@ -401,18 +438,6 @@ int sys_mm_drv_update_region_flags(void *virt, size_t size, uint32_t flags);
  * @retval -EFAULT if virtual address is not mapped
  */
 int sys_mm_drv_page_phys_get(void *virt, uintptr_t *phys);
-
-/**
- * @brief Represents an available memory region.
- *
- * A memory region that can be used by allocators. Driver defined
- * attributes can be used to guide the proper usage of each region.
- */
-struct sys_mm_drv_region {
-	void *addr; /**< @brief Address of the memory region */
-	size_t size; /**< @brief Size of the memory region */
-	uint32_t attr; /**< @brief Driver defined attributes of the memory region */
-};
 
 /* TODO is it safe to assume no valid region has size == 0? */
 /**

commit 8adf95a41f0cebdcd1014af4cb8bd5f57aa9b59d

drivers: mm: Fix vm range check for page map
This fixes the range check condition. Without it, the last page cannot
be mapped.

This is a follow-up to 70961e2; the same fix is applied to the neighboring
function. Both functions with the problem were introduced by 01d3575.

Signed-off-by: Serhiy Katsyuba <serhiy.katsyuba@intel.com>

drivers/mm/mm_drv_common.c
@@ -53,7 +53,7 @@ int sys_mm_drv_map_page_safe(const struct sys_mm_drv_region *virtual_region,
 
 	/* check if memory to be mapped is within given virtual region */
 	if ((POINTER_TO_UINT(virt) >= virtual_region_start) &&
-	    (POINTER_TO_UINT(virt) + CONFIG_MM_DRV_PAGE_SIZE < virtual_region_end)) {
+	    (POINTER_TO_UINT(virt) + CONFIG_MM_DRV_PAGE_SIZE <= virtual_region_end)) {
 		return sys_mm_drv_map_page(virt, phys, flags);
 	}
 