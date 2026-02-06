288eb1170cf4bf9f103b87a7a6faf6a401c6ca56
Emil Gydesen <emil.gydesen@nordicsemi.no>
2026-02-06 21:10:47 +0100

    Bluetooth: MCP: Add functions to get OTS IDs
    There are some cases in the tree where btp_mcp.c needs to
    get the IDs of some OTS objects. Since the media_proxy is
    being removed, these will need to exist as
    bt_mcp_media_control_server APIs.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- include/zephyr/bluetooth/audio/mcp.h
+++ include/zephyr/bluetooth/audio/mcp.h
@@ -29,6 +29,8 @@
  * and the latter placed on more powerful devices like phones and PCs.
  */
 
+#include <stdint.h>
+
 #include <zephyr/bluetooth/audio/mcs.h>
 
 #ifdef __cplusplus
@@ -63,6 +65,73 @@ int bt_mcp_media_control_server_register(void);
  * @return 0 if success, errno on failure.
  */
 int bt_mcp_media_control_server_command(const struct bt_mcs_cmd *command);
+
+/**
+ * @brief Read Current Track Object ID
+ *
+ * Get an ID (48 bit) that can be used to retrieve the Current
+ * Track Object from an Object Transfer Service
+ *
+ * See the Media Control Service spec v1.0 sections 3.11 and
+ * 4.3 for a description of the Current Track Object.
+ *
+ * Requires Object Transfer Service
+ *
+ * @param[out] id Variable to store the resulting ID in
+ *
+ * @return 0 if success, errno on failure.
+ */
+int bt_mcp_media_control_server_get_current_track_id(uint64_t *id);
+
+/**
+ * @brief Read Next Track Object ID
+ *
+ * Get an ID (48 bit) that can be used to retrieve the Next
+ * Track Object from an Object Transfer Service
+ *
+ * Requires Object Transfer Service
+ *
+ * @param[out] id Variable to store the resulting ID in
+ *
+ * @return 0 if success, errno on failure.
+ */
+int bt_mcp_media_control_server_get_next_track_id(uint64_t *id);
+
+/**
+ * @brief Read Parent Group Object ID
+ *
+ * Get an ID (48 bit) that can be used to retrieve the Parent
+ * Track Object from an Object Transfer Service
+ *
+ * The parent group is the parent of the current group.
+ *
+ * See the Media Control Service spec v1.0 sections 3.14 and
+ * 4.4 for a description of the Current Track Object.
+ *
+ * Requires Object Transfer Service
+ *
+ * @param[out] id Variable to store the resulting ID in
+ *
+ * @return 0 if success, errno on failure.
+ */
+int bt_mcp_media_control_server_get_parent_group_id(uint64_t *id);
+
+/**
+ * @brief Read Current Group Object ID
+ *
+ * Get an ID (48 bit) that can be used to retrieve the Current
+ * Track Object from an Object Transfer Service
+ *
+ * See the Media Control Service spec v1.0 sections 3.14 and
+ * 4.4 for a description of the Current Group Object.
+ *
+ * Requires Object Transfer Service
+ *
+ * @param[out] id Variable to store the resulting ID in
+ *
+ * @return 0 if success, errno on failure.
+ */
+int bt_mcp_media_control_server_get_current_group_id(uint64_t *id);
 
 /** @} */ /* End of group bt_mcp_media_control_server */
 #ifdef __cplusplus
