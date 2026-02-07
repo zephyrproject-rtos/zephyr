bdc5ab30a1e913c0d8499175e42d6df677ddd981
Jordan Yates <jordan@embeint.com>
2026-02-07 10:36:39 +1000

    debug: thread_analyzer: default `THREAD_RUNTIME_STATS`
    `THREAD_RUNTIME_STATS` is not a requirement for the thread analyzer to
    operate, merely useful. Switch the control to `configdefault` to leave
    it enabled by default, while allowing both applications to disable the
    option with `CONFIG_THREAD_RUNTIME_STATS=n` and for alternate defaults
    to be specified elsewhere.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/debug/thread_analyzer/thread_analyzer.c
+++ subsys/debug/thread_analyzer/thread_analyzer.c
@@ -157,7 +157,6 @@ static void thread_analyze_cb(const struct k_thread *cthread, void *user_data)
 	size_t size = thread->stack_info.size;
 	struct ta_cb_user_data *ud = user_data;
 	thread_analyzer_cb cb = ud->cb;
-	unsigned int cpu = ud->cpu;
 	struct thread_analyzer_info info;
 	char hexname[PTR_STR_MAXLEN + 1];
 	const char *name;
@@ -211,7 +210,7 @@ static void thread_analyze_cb(const struct k_thread *cthread, void *user_data)
 	}
 
 	if (IS_ENABLED(CONFIG_THREAD_ANALYZER_AUTO_SEPARATE_CORES)) {
-		if (k_thread_runtime_stats_cpu_get(cpu, &rt_stats_all) != 0) {
+		if (k_thread_runtime_stats_cpu_get(ud->cpu, &rt_stats_all) != 0) {
 			ret++;
 		}
 	} else {
