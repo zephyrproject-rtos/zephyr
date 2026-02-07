75051411f6fa39da3b826c9936834bb30092f692
Jordan Yates <jordan@embeint.com>
2026-02-07 10:29:09 +1000

    debug: thread_analyzer: callbacks with user data
    Add a variant of `thread_analyzer_run` that supports a user data pointer
    in the callback.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- subsys/debug/thread_analyzer/thread_analyzer.c
+++ subsys/debug/thread_analyzer/thread_analyzer.c
@@ -16,6 +16,7 @@
 #include <zephyr/logging/log.h>
 #include <stdio.h>
 #include <zephyr/init.h>
+#include <zephyr/sys/__assert.h>
 
 LOG_MODULE_REGISTER(thread_analyzer, CONFIG_THREAD_ANALYZER_LOG_LEVEL);
 
@@ -122,8 +123,9 @@ static void thread_print_cb(struct thread_analyzer_info *info)
 }
 
 struct ta_cb_user_data {
-	thread_analyzer_cb cb;
+	thread_analyzer_cb_user_data cb;
 	unsigned int cpu;
+	void *user_data;
 };
 
 #ifdef CONFIG_THREAD_ANALYZER_STACK_SAFETY
@@ -160,7 +162,6 @@ static void thread_analyze_cb(const struct k_thread *cthread, void *user_data)
 #endif
 	size_t size = thread->stack_info.size;
 	struct ta_cb_user_data *ud = user_data;
-	thread_analyzer_cb cb = ud->cb;
 	struct thread_analyzer_info info;
 	char hexname[PTR_STR_MAXLEN + 1];
 	const char *name;
@@ -235,7 +236,7 @@ static void thread_analyze_cb(const struct k_thread *cthread, void *user_data)
 
 	ARG_UNUSED(ret);
 
-	cb(&info);
+	ud->cb(&info, ud->user_data);
 
 #ifdef CONFIG_THREAD_ANALYZER_LONG_FRAME_PER_INTERVAL
 	k_thread_runtime_stats_longest_frame_reset(thread);
@@ -273,7 +274,7 @@ static void isr_stack(struct ta_cb_user_data *ta_ctx)
 			.stack_used = size - unused,
 		};
 
-		ta_ctx->cb(&isr_info);
+		ta_ctx->cb(&isr_info, ta_ctx->user_data);
 	}
 }
 
@@ -313,14 +314,27 @@ static void thread_analyzer_internal(struct ta_cb_user_data *ta_ctx)
 	}
 }
 
-void thread_analyzer_run(thread_analyzer_cb cb, unsigned int cpu)
+void thread_analyzer_run_with_user_data(thread_analyzer_cb_user_data cb, unsigned int cpu, void *user_data)
 {
 	struct ta_cb_user_data ctx = {
 		.cb = cb,
 		.cpu = cpu,
+		.user_data = user_data,
 	};
 
 	thread_analyzer_internal(&ctx);
+}
+
+static void cb_dispatch(struct thread_analyzer_info *info, void *user_data)
+{
+	thread_analyzer_cb cb = user_data;
+
+	cb(info);
+}
+
+void thread_analyzer_run(thread_analyzer_cb cb, unsigned int cpu)
+{
+	thread_analyzer_run_with_user_data(cb_dispatch, cpu, cb);
 }
 
 void thread_analyzer_print(unsigned int cpu)
