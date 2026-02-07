4bbee880a08c34d0e0f97a96c883ab257ec52c97
Moritz Gericke <xmoexdev@gmail.com>
2026-02-07 23:20:31 +0100

    drivers: interrupt_controller: intc_gpio_stm32: Assert against invalid exti lines
    Unchecked indexing into intc_gpio_data.cb could lead to overflow in the
    following functions:
    
    - stm32_gpio_intc_set_irq_callback
    - stm32_gpio_intc_remove_irq_callback
    
    Fixes zephyrproject-rtos/zephyr#103573
    
    Signed-off-by: Moritz Gericke <xmoexdev@gmail.com>

--- drivers/interrupt_controller/intc_gpio_stm32.c
+++ drivers/interrupt_controller/intc_gpio_stm32.c
@@ -85,11 +85,19 @@ static inline uint32_t stm32_exti_linenum_to_src_cfg_line(gpio_pin_t linenum)
 }
 
 /**
- * @returns EXTI line number for LL_EXTI_LINE_n define
+ * @brief Translate EXTI line to line number
+ *
+ * @param line LL_EXTI_LINE_n, has to be a power of 2
+ *
+ * @returns EXTI line number (n) for LL_EXTI_LINE_n define
  */
 static inline gpio_pin_t ll_exti_line_to_linenum(stm32_gpio_irq_line_t line)
 {
-	return LOG2(line);
+	const gpio_pin_t line_num = LOG2CEIL(line);
+
+	__ASSERT_NO_MSG(IS_POWER_OF_TWO(line) && line_num < NUM_EXTI_LINES);
+
+	return line_num;
 }
 
 /**
@@ -190,8 +198,6 @@ void stm32_gpio_intc_enable_line(stm32_gpio_irq_line_t line)
 {
 	unsigned int irqnum;
 	uint32_t line_num = ll_exti_line_to_linenum(line);
-
-	__ASSERT_NO_MSG(line_num < NUM_EXTI_LINES);
 
 	/* Get matching exti irq provided line thanks to irq_table */
 	irqnum = exti_irq_table[line_num];
