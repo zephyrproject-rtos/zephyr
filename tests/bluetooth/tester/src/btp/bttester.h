941f978bf2490e339be9700c2fa749298a6b6413
Make Shi <make.shi@nxp.com>
2026-02-09 13:28:55 +0800

    tests: bluetooth: tester: add SPP(RFCOMM) service PTS support
    Add a Serial Port Profile (SPP) service to the Bluetooth tester over
    RFCOMM, enabling SDP-based discovery and simple server registration.
    
    Signed-off-by: Make Shi <make.shi@nxp.com>

--- tests/bluetooth/tester/src/btp/bttester.h
+++ tests/bluetooth/tester/src/btp/bttester.h
@@ -151,3 +151,6 @@ uint8_t tester_unregister_sdp(void);
 
 uint8_t tester_init_rfcomm(void);
 uint8_t tester_unregister_rfcomm(void);
+
+uint8_t tester_init_spp(void);
+uint8_t tester_unregister_spp(void);
--- tests/bluetooth/tester/src/btp_core.c
+++ tests/bluetooth/tester/src/btp_core.c
@@ -148,6 +148,10 @@ static uint8_t supported_services(const void *cmd, uint16_t cmd_len,
 #if defined(CONFIG_BT_CLASSIC)
 	tester_set_bit(rp->data, BTP_SERVICE_ID_SDP);
 #endif /* CONFIG_BT_CLASSIC */
+#if defined(CONFIG_BT_RFCOMM)
+	tester_set_bit(rp->data, BTP_SERVICE_ID_RFCOMM);
+	tester_set_bit(rp->data, BTP_SERVICE_ID_SPP);
+#endif /* CONFIG_BT_RFCOMM */
 
 	/* octet 4 */
 #if defined(CONFIG_BT_RFCOMM)
@@ -313,6 +317,9 @@ static uint8_t register_service(const void *cmd, uint16_t cmd_len,
 	case BTP_SERVICE_ID_RFCOMM:
 		status = tester_init_rfcomm();
 		break;
+	case BTP_SERVICE_ID_SPP:
+		status = tester_init_spp();
+		break;
 #endif /* CONFIG_BT_RFCOMM */
 	default:
 		LOG_WRN("unknown id: 0x%02x", cp->id);
@@ -477,6 +484,9 @@ static uint8_t unregister_service(const void *cmd, uint16_t cmd_len,
 #if defined(CONFIG_BT_RFCOMM)
 	case BTP_SERVICE_ID_RFCOMM:
 		status = tester_unregister_rfcomm();
+		break;
+	case BTP_SERVICE_ID_SPP:
+		status = tester_unregister_spp();
 		break;
 #endif /* CONFIG_BT_RFCOMM */
 	default:
