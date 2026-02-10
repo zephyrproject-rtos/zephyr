90ff9a6d63bc612e5ae3bde62578e5cdb0d22a20
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2026-02-10 16:57:31 +0800

    cmake: compiler: add warning_no_misleading_indentation flag
    Add a new compiler property warning_no_misleading_indentation to allow
    disabling misleading indentation warnings. This is implemented for GCC
    with the -Wno-misleading-indentation flag.
    
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

--- cmake/compiler/compiler_flags_template.cmake
+++ cmake/compiler/compiler_flags_template.cmake
@@ -140,6 +140,9 @@ set_property(TARGET asm PROPERTY imacros)
 # Compiler flag for disabling pointer arithmetic warnings
 set_compiler_property(PROPERTY warning_no_pointer_arithmetic)
 
+# Compiler flag for disabling misleading indentation warnings
+set_compiler_property(PROPERTY warning_no_misleading_indentation)
+
 # Compiler flags for disabling position independent code / executable
 set_compiler_property(PROPERTY no_position_independent)
 
