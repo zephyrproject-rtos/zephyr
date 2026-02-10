c8b79b43b63b8521dee9c1b737d298b6fc7d8f0d
Abderrahmane JARMOUNI <git@jarmouni.me>
2026-02-10 01:35:16 +0100

    cache: Add __dcacheline_aligned and __dcacheline_exclusive attributes
    Add __dcacheline_aligned which ensures data cache line alignment.
    
    Add __dcacheline_exclusive_... attributes which puts a static variable
    in a section that contains only data cache line aligned variables. This
    results in variables being aligned and having exclusive data cache line(s).
    
    Co-authored-by: Krzysztof Chruściński
    <krzysztof.chruscinski@nordicsemi.no>
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
    Signed-off-by: Abderrahmane JARMOUNI <git@jarmouni.me>

--- cmake/linker_script/common/common-noinit.cmake
+++ cmake/linker_script/common/common-noinit.cmake
@@ -1,3 +1,4 @@
+# SPDX-FileCopyrightText: Copyright The Zephyr Project Contributors
 # SPDX-License-Identifier: Apache-2.0
 # The contents of this file is based on include/zephyr/linker/common-noinit.ld
 # Please keep in sync
@@ -11,5 +12,17 @@ if(CONFIG_USERSPACE)
     SYMBOLS z_user_stacks_start z_user_stacks_end)
 
 endif()
+
+if(CONFIG_DCACHE_LINE_SIZE AND CONFIG_DCACHE_LINE_SIZE>0)
+  zephyr_linker_section(NAME ".${__DCACHELINE_EXCLUSIVE_NOINIT_SECTION_NAME}" GROUP NOINIT_REGION
+    SUBALIGN $(CONFIG_DCACHE_LINE_SIZE) NOINPUT
+  )
+  zephyr_linker_section_configure(SECTION ${__DCACHELINE_EXCLUSIVE_NOINIT_SECTION_NAME}
+    KEEP SORT NAME INPUT ".${__DCACHELINE_EXCLUSIVE_NOINIT_SECTION_NAME}.*"
+    ALIGN $(CONFIG_DCACHE_LINE_SIZE)
+    SYMBOLS __dcacheline_exclusive_noinit_start __dcacheline_exclusive_noinit_end
+  )
+endif()
+
 # TODO: #include <snippets-noinit.ld>
 include(${COMMON_ZEPHYR_LINKER_DIR}/kobject-priv-stacks.cmake)
