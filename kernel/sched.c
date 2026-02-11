364cb518597ca738de3082c98af8de5c4507d520
Yong Cong Sin <ycsin@meta.com>
2026-02-11 21:20:27 +0000

    kernel: check if interrupt is disabled in k_can_yield
    `k_yield()` can't be called when interrupt is disabled, update
    `k_can_yield()` to reflect that.
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>
    Signed-off-by: Yong Cong Sin <yongcong.sin@gmail.com>

--- kernel/sched.c
+++ kernel/sched.c
@@ -1127,7 +1127,11 @@ static inline void z_vrfy_k_reschedule(void)
 
 bool k_can_yield(void)
 {
-	return !(k_is_pre_kernel() || k_is_in_isr() ||
+	unsigned int k = arch_irq_lock();
+	bool irq_locked = !arch_irq_unlocked(k);
+
+	arch_irq_unlock(k);
+	return !(k_is_pre_kernel() || k_is_in_isr() || irq_locked ||
 		 z_is_idle_thread_object(_current));
 }
 
