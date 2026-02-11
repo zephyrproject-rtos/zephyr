b7db78f565d2f5d0cf2c5c678bfbbaa90fce1288
Alessandro Manganaro <alessandro.manganaro@st.com>
2026-02-11 14:29:15 +0100

    drivers: bluetooth: hci: stm32wba: align with radio power hook
    Update the stm32wbax ble hci driver to integrate the new SoC-level
    radio power management hook.
    
    Signed-off-by: Alessandro Manganaro <alessandro.manganaro@st.com>

--- drivers/bluetooth/hci/hci_stm32wba.c
+++ drivers/bluetooth/hci/hci_stm32wba.c
@@ -52,28 +52,6 @@ struct aci_set_ble_addr {
 } __packed;
 #endif /* CONFIG_BT_HCI_SETUP */
 
-#ifdef CONFIG_PM_DEVICE
-/* Proprietary command to enable notification of radio events */
-#define ACI_HAL_WRITE_SET_RADIO_ACTIVITY_MASK BT_OP(BT_OGF_VS, 0xFC18)
-#define RADIO_ACTIVITY_MASK_ALL               (0x7FFF)
-#define ACI_HAL_END_OF_RADIO_ACTIVITY_EVENT   (0x0004)
-
-struct aci_set_radio_activity_mask_params {
-	uint16_t Radio_Activity_Mask;
-} __packed;
-
-struct bt_hci_end_radio_activity_evt {
-	uint8_t evt_code;
-	uint8_t len;
-	uint16_t vs_code;
-	uint8_t last_state;
-	uint8_t next_state;
-	uint32_t next_state_sys_time;
-	uint8_t last_state_slot;
-	uint8_t next_state_slot;
-} __packed;
-#endif /* CONFIG_PM_DEVICE */
-
 /* ACI Reset command */
 #define ACI_RESET                             0xFF00
 
@@ -90,58 +68,6 @@ struct aci_reset {
 static uint8_t bt_hci_state = BT_HCI_STATE_DEINIT;
 
 extern uint8_t ll_state_busy;
-
-#ifdef CONFIG_PM_DEVICE
-static int bt_hci_stm32wba_set_radio_activity_mask(void)
-{
-	struct net_buf *buf;
-	struct aci_set_radio_activity_mask_params *params;
-	int err;
-
-	buf = bt_hci_cmd_alloc(K_FOREVER);
-	if (!buf) {
-		return -ENOBUFS;
-	}
-
-	params = net_buf_add(buf, sizeof(*params));
-	params->Radio_Activity_Mask = RADIO_ACTIVITY_MASK_ALL;
-
-	err = bt_hci_cmd_send_sync(ACI_HAL_WRITE_SET_RADIO_ACTIVITY_MASK, buf, NULL);
-
-	return err;
-}
-
-void register_radio_event(void)
-{
-	int64_t value_ticks;
-	static struct pm_policy_event radio_evt;
-	static bool first_event = true;
-	uint32_t cmd_status;
-	/* Flag indicating that no radio events have been scheduled */
-	uint32_t next_radio_event_us = 0;
-
-	/* Getting next radio event time if any */
-	cmd_status = ll_intf_le_get_remaining_time_for_next_event(&next_radio_event_us);
-	UNUSED(cmd_status);
-	__ASSERT(cmd_status, "Unable to retrieve next radio event");
-
-	if (next_radio_event_us == LL_DP_SLP_NO_WAKEUP) {
-		/* No next radio event scheduled */
-		if (!first_event) {
-			first_event = true;
-			pm_policy_event_unregister(&radio_evt);
-		}
-	} else {
-		value_ticks = k_us_to_ticks_floor64(next_radio_event_us) + k_uptime_ticks();
-		if (first_event) {
-			pm_policy_event_register(&radio_evt, value_ticks);
-			first_event = false;
-		} else {
-			pm_policy_event_update(&radio_evt, value_ticks);
-		}
-	}
-}
-#endif /* CONFIG_PM_DEVICE */
 
 static bool is_hci_event_discardable(const uint8_t *evt_data)
 {
@@ -319,17 +245,6 @@ static int receive_data(const struct device *dev, const uint8_t *data, size_t le
 
 	switch (pkt_indicator) {
 	case BT_HCI_H4_EVT:
-#ifdef CONFIG_PM_DEVICE
-		/* Filtering on next radio events */
-		const struct bt_hci_end_radio_activity_evt *evt_pckt =
-			(const struct bt_hci_end_radio_activity_evt *)(data);
-
-		if ((evt_pckt->evt_code == BT_HCI_EVT_VENDOR) &&
-		    (evt_pckt->vs_code == ACI_HAL_END_OF_RADIO_ACTIVITY_EVENT)) {
-			register_radio_event();
-			return err;
-		}
-#endif /* CONFIG_PM_DEVICE */
 		buf = treat_evt(data, len);
 		break;
 	case BT_HCI_H4_ACL:
@@ -601,13 +516,6 @@ static int bt_hci_stm32wba_setup(const struct device *dev,
 		return err;
 	}
 
-#ifdef CONFIG_PM_DEVICE
-	err = bt_hci_stm32wba_set_radio_activity_mask();
-	if (err) {
-		return err;
-	}
-#endif /* CONFIG_PM_DEVICE */
-
 	return err;
 }
 #endif /* CONFIG_BT_HCI_SETUP */
@@ -619,35 +527,29 @@ static int radio_pm_action(const struct device *dev, enum pm_device_action actio
 	case PM_DEVICE_ACTION_RESUME:
 		LL_AHB5_GRP1_EnableClock(LL_AHB5_GRP1_PERIPH_RADIO);
 #if defined(CONFIG_PM_S2RAM)
-		if (LL_PWR_IsActiveFlag_SB() == 1U) {
-			/* Put the radio in active state */
-			link_layer_register_isr(true);
+		if (ll_sys_dp_slp_get_state() == LL_SYS_DP_SLP_ENABLED) {
+			if (LL_PWR_IsActiveFlag_SB() == 1U) {
+				/* Restore NVIC configuration for radio */
+				link_layer_register_isr(true);
+				ll_sys_dp_slp_exit();
+			}
 		}
 #endif /* CONFIG_PM_S2RAM */
 		LINKLAYER_PLAT_NotifyWFIExit();
-		ll_sys_dp_slp_exit();
 		break;
 	case PM_DEVICE_ACTION_SUSPEND:
 #if defined(CONFIG_PM_S2RAM)
-		uint32_t radio_remaining_time = 0;
-		enum pm_state state = pm_state_next_get(_current_cpu->id)->state;
+		if (ll_sys_dp_slp_get_state() == LL_SYS_DP_SLP_DISABLED) {
+			uint64_t next_radio_evt;
+			enum pm_state state = pm_state_next_get(_current_cpu->id)->state;
 
-		if (state == PM_STATE_SUSPEND_TO_RAM) {
-			/* Checking next radio schedulet event */
-			uint32_t cmd_status =
-				ll_intf_le_get_remaining_time_for_next_event(&radio_remaining_time);
-			UNUSED(cmd_status);
-			__ASSERT(cmd_status, "Unable to retrieve next radio event");
-
-			if (radio_remaining_time == LL_DP_SLP_NO_WAKEUP) {
-				/* No radio event scheduled */
-				(void)ll_sys_dp_slp_enter(LL_DP_SLP_NO_WAKEUP);
-			} else if (radio_remaining_time > CFG_LPM_STDBY_WAKEUP_TIME) {
-				/* No event in a "near" future */
-				(void)ll_sys_dp_slp_enter(radio_remaining_time -
-							  CFG_LPM_STDBY_WAKEUP_TIME);
-			} else {
-				register_radio_event();
+			if (state == PM_STATE_SUSPEND_TO_RAM) {
+				next_radio_evt = os_timer_get_earliest_time();
+				if (next_radio_evt > CFG_LPM_STDBY_WAKEUP_TIME) {
+					/* No event in a "near" future */
+					next_radio_evt -= CFG_LPM_STDBY_WAKEUP_TIME;
+					ll_sys_dp_slp_enter(next_radio_evt);
+				}
 			}
 		}
 #endif /* CONFIG_PM_S2RAM */
