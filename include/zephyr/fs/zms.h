cf4f1b127ed08fdd04e2fb53acf5bbacccb5dbe9
Dominik Ermel <dominik.ermel@nordicsemi.no>
2026-02-13 11:59:15 +0100

    fs: Add redirections for nvs.h and zms.h
    Add header files that redirect to new paths for nvs.h and zms.h.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

---
+++ include/zephyr/fs/zms.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2026 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+#ifndef ZEPHYR_INCLUDE_FS_ZMS_H_
+#define ZEPHYR_INCLUDE_FS_ZMS_H_
+
+/**
+ * @file
+ * @brief Legacy interface header for ZMS API.
+ * @deprecated Please use @ref zephyr/kvss/zms.h instead.
+ * @ingroup zms
+ */
+#include <zephyr/kvss/zms.h>
+
+__WARN("This header file is deprecated, please use zephyr/kvss/zms.h instead.")
+
+#endif /* ZEPHYR_INCLUDE_FS_ZMS_H_ */
