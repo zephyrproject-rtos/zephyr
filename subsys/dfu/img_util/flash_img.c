ae2d5973e1ecbf4276aa398f828cf53a595682c3
Axel Le Bourhis <axel.lebourhis@nxp.com>
2026-02-13 18:30:05 +0100

    dfu: refactor flash_img to isolate MCUboot-specific code
    This commit refactors the flash_img module to separate MCUboot-specific
    functionality from generic flash image handling code. This allows the
    flash_img utilities to be potentially reused with other bootloaders.
    
    Changes:
    - Consolidate CMakeLists.txt by removing separate boot/ and img_util/
      subdirectory CMake files and merging build logic into
      subsys/dfu/CMakeLists.txt
    - Move IMG_BLOCK_BUF_SIZE, IMG_ERASE_PROGRESSIVELY, and
      IMG_ENABLE_IMAGE_CHECK Kconfig options outside the
      MCUBOOT_IMG_MANAGER conditional block to make them available for
      non-MCUboot configurations
    - Guard MCUboot-specific includes and code in flash_img.c with
      CONFIG_BOOTLOADER_MCUBOOT preprocessor conditionals:
      - mcuboot.h include and bootutil_public.h include
      - scramble_mcuboot_trailer() function
      - boot_get_image_start_offset() usage in flash_img_check()
    - Fix typos: "prgressive" -> "progressive", "progresively" ->
      "progressively"
    
    Signed-off-by: Axel Le Bourhis <axel.lebourhis@nxp.com>

--- subsys/dfu/img_util/flash_img.c
+++ subsys/dfu/img_util/flash_img.c
@@ -2,6 +2,7 @@
  * Copyright (c) 2017-2025 Nordic Semiconductor ASA
  * Copyright (c) 2017 Linaro Limited
  * Copyright (c) 2020 Gerson Fernando Budke <nandojve@gmail.com>
+ * Copyright (c) 2026 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -12,16 +13,18 @@
 #include <string.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/devicetree.h>
-#include <zephyr/dfu/mcuboot.h>
 #include <zephyr/dfu/flash_img.h>
 #include <zephyr/storage/flash_map.h>
 #include <zephyr/storage/stream_flash.h>
 
-LOG_MODULE_REGISTER(flash_img, CONFIG_IMG_MANAGER_LOG_LEVEL);
-
+#ifdef CONFIG_BOOTLOADER_MCUBOOT
+#include <zephyr/dfu/mcuboot.h>
 #ifdef CONFIG_IMG_ERASE_PROGRESSIVELY
 #include <bootutil/bootutil_public.h>
 #endif
+#endif /* CONFIG_BOOTLOADER_MCUBOOT */
+
+LOG_MODULE_REGISTER(flash_img, CONFIG_IMG_MANAGER_LOG_LEVEL);
 
 #if defined(CONFIG_FLASH_USES_MAPPED_PARTITION)
 #define PARTITION_IS_RUNNING_APP_PARTITION(label) \
@@ -68,6 +71,7 @@ BUILD_ASSERT((CONFIG_IMG_BLOCK_BUF_SIZE % FLASH_WRITE_BLOCK_SIZE == 0),
 #define FLASH_CHECK_ERASED_BUFFER_SIZE 16
 #define ERASED_VAL_32(x) (((x) << 24) | ((x) << 16) | ((x) << 8) | (x))
 
+#ifdef CONFIG_BOOTLOADER_MCUBOOT
 static int scramble_mcuboot_trailer(struct flash_img_context *ctx)
 {
 	int rc = 0;
@@ -80,7 +84,7 @@ static int scramble_mcuboot_trailer(struct flash_img_context *ctx)
 		const struct flash_parameters *fparams =
 			flash_get_parameters(flash_area_get_device(ctx->flash_area));
 #ifdef CONFIG_STREAM_FLASH_ERASE
-		/* for erasable devices prgressive-erase works only along with
+		/* for erasable devices progressive-erase works only along with
 		 * CONFIG_STREAM_FLASH_ERASE option.
 		 */
 		if (flash_params_get_erase_cap(fparams) & FLASH_ERASE_C_EXPLICIT) {
@@ -117,6 +121,7 @@ static int scramble_mcuboot_trailer(struct flash_img_context *ctx)
 
 	return rc;
 }
+#endif /* CONFIG_BOOTLOADER_MCUBOOT */
 
 
 int flash_img_buffered_write(struct flash_img_context *ctx, const uint8_t *data,
@@ -124,6 +129,7 @@ int flash_img_buffered_write(struct flash_img_context *ctx, const uint8_t *data,
 {
 	int rc;
 
+#ifdef CONFIG_BOOTLOADER_MCUBOOT
 	/* If there is a need to erase the trailer, that should happen before any
 	 * write is done to partition.
 	 */
@@ -131,10 +137,11 @@ int flash_img_buffered_write(struct flash_img_context *ctx, const uint8_t *data,
 	if (rc != 0) {
 		return rc;
 	}
+#endif
 
 
 	/* if CONFIG_IMG_ERASE_PROGRESSIVELY is enabled the enabled CONFIG_STREAM_FLASH_ERASE
-	 * ensures that stream_flash erases flash progresively.
+	 * ensures that stream_flash erases flash progressively.
 	 */
 	rc = stream_flash_buffered_write(&ctx->stream, data, len, flush);
 	if (!flush) {
@@ -304,7 +311,11 @@ int flash_img_check(struct flash_img_context *ctx,
 
 	fac.match = fic->match;
 	fac.clen = fic->clen;
+#ifdef CONFIG_BOOTLOADER_MCUBOOT
 	fac.off = boot_get_image_start_offset(area_id);
+#else
+	fac.off = 0;
+#endif
 	fac.rbuf = ctx->buf;
 	fac.rblen = sizeof(ctx->buf);
 
