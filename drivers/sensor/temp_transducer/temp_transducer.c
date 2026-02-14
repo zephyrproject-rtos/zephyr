c2ff12a93b6a2d24e01dca72229fd62aace191f6
Liam Beguin <liambeguin@gmail.com>
2026-02-14 14:38:28 -0500

    drivers: sensor: add temp_transducer driver
    A temperature transducer is a device that converts a thermal quantity
    into any other physical quantity. This patch adds support for
    temperature to voltage (like the LTC2997) and temperature to current
    (like the AD590) linear transducers.
    In both cases these are assumed to be connected to a voltage ADC.
    
    This driver is an adaptation of the temperature components of the
    iio-rescaler Linux kernel driver
    
    Signed-off-by: Liam Beguin <liambeguin@gmail.com>

---
+++ drivers/sensor/temp_transducer/temp_transducer.c
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2026 HawkEye 360
+ * Copyright (C) 2026 Liam Beguin <liambeguin@gmail.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT temperature_transducer
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/adc.h>
+#include <zephyr/drivers/adc/temp_transducer.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(temp_transducer, CONFIG_SENSOR_LOG_LEVEL);
+
+struct temp_transducer_config {
+	struct temp_transducer_dt_spec spec;
+};
+
+struct temp_transducer_data {
+	struct adc_sequence sequence;
+	int16_t raw;
+};
+
+static int temp_transducer_sample_fetch(const struct device *dev, enum sensor_channel chan)
+{
+	const struct temp_transducer_config *config = dev->config;
+	struct temp_transducer_data *data = dev->data;
+	int ret;
+
+	if (chan != SENSOR_CHAN_ALL && chan != SENSOR_CHAN_AMBIENT_TEMP) {
+		return -ENOTSUP;
+	}
+
+	ret = adc_read_dt(&config->spec.port, &data->sequence);
+	if (ret < 0) {
+		LOG_ERR("ADC read failed: %d", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+static int temp_transducer_channel_get(const struct device *dev, enum sensor_channel chan,
+					struct sensor_value *val)
+{
+	const struct temp_transducer_config *config = dev->config;
+	struct temp_transducer_data *data = dev->data;
+	int32_t millivolts = data->raw;
+	int32_t millicelsius;
+	int ret;
+
+	if (chan != SENSOR_CHAN_AMBIENT_TEMP) {
+		return -ENOTSUP;
+	}
+
+	ret = adc_raw_to_millivolts_dt(&config->spec.port, &millivolts);
+	if (ret < 0) {
+		LOG_ERR("ADC raw to millivolts conversion failed: %d", ret);
+		return ret;
+	}
+
+	millicelsius = temp_transducer_scale_dt(&config->spec, millivolts);
+
+	/* Convert to sensor_value (val1 = degrees, val2 = micro-degrees) */
+	val->val1 = millicelsius / 1000;
+	val->val2 = (millicelsius % 1000) * 1000;
+
+	return 0;
+}
+
+static int temp_transducer_init(const struct device *dev)
+{
+	const struct temp_transducer_config *config = dev->config;
+	struct temp_transducer_data *data = dev->data;
+	int ret;
+
+	if (!adc_is_ready_dt(&config->spec.port)) {
+		LOG_ERR("ADC device not ready");
+		return -ENODEV;
+	}
+
+	ret = adc_channel_setup_dt(&config->spec.port);
+	if (ret < 0) {
+		LOG_ERR("ADC channel setup failed: %d", ret);
+		return ret;
+	}
+
+	ret = adc_sequence_init_dt(&config->spec.port, &data->sequence);
+	if (ret < 0) {
+		LOG_ERR("ADC sequence init failed: %d", ret);
+		return ret;
+	}
+
+	data->sequence.buffer = &data->raw;
+	data->sequence.buffer_size = sizeof(data->raw);
+
+	return 0;
+}
+
+static DEVICE_API(sensor, temp_transducer_api) = {
+	.sample_fetch = temp_transducer_sample_fetch,
+	.channel_get = temp_transducer_channel_get,
+};
+
+#define TEMP_TRANSDUCER_INIT(inst)                                                                 \
+	static struct temp_transducer_data temp_transducer_data_##inst;                            \
+                                                                                                   \
+	static const struct temp_transducer_config temp_transducer_config_##inst = {               \
+		.spec = TEMP_TRANSDUCER_DT_SPEC_GET(DT_DRV_INST(inst)),                            \
+	};                                                                                         \
+                                                                                                   \
+	SENSOR_DEVICE_DT_INST_DEFINE(inst, temp_transducer_init, NULL,                             \
+				     &temp_transducer_data_##inst,                                 \
+				     &temp_transducer_config_##inst, POST_KERNEL,                  \
+				     CONFIG_SENSOR_INIT_PRIORITY, &temp_transducer_api);
+
+DT_INST_FOREACH_STATUS_OKAY(TEMP_TRANSDUCER_INIT)
