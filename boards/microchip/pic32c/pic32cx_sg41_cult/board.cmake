b53369092e62b5afa16fbae01a91b7e683281966
Muhammed Asif P <asifp3104@gmail.com>
2026-02-16 00:40:50 +0530

    boards: microchip: pic32cx_sg41_cult: Use MPLAB IPE runner
    - Use MPLAB IPE runner for flashing the board.
    - Makes MPLAB IPE as the default runner for flashing.
    
    Signed-off-by: Muhammed Asif P <asifp3104@gmail.com>

--- boards/microchip/pic32c/pic32cx_sg41_cult/board.cmake
+++ boards/microchip/pic32c/pic32cx_sg41_cult/board.cmake
@@ -1,6 +1,8 @@
 # Copyright (c) 2025 Microchip Technology Inc.
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(mplab_ipe "--tool" "PKOB4" "--part" "32CX1025SG41128" "--erase" "--verify")
 board_runner_args(jlink "--device=PIC32CX1025SG41128" "--speed=4000")
 
+include(${ZEPHYR_BASE}/boards/common/mplab_ipe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
