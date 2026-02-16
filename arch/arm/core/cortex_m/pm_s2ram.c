65ca84b1e6f1fd17121c02940c0d3eb194f29018
Mathieu Choplain <mathieu.choplain-ext@st.com>
2026-02-16 15:49:29 +0100

    arch: arm: cortex_m: pm_s2ram: save/restore NVIC state
    When the NVIC is used as interrupt controller, save and restore its
    non-volatile state when entering/exiting S2RAM states. This includes
    which interrupts are enabled, and the configured priorities, but not
    pending state since no interrupts can be pending upon S2RAM entry.
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain-ext@st.com>

--- arch/arm/core/cortex_m/pm_s2ram.c
+++ arch/arm/core/cortex_m/pm_s2ram.c
@@ -45,21 +45,67 @@ __s2ram_ctx struct scb_context _scb_context;
 IF_ENABLED(CONFIG_MPU, (__s2ram_ctx struct z_mpu_context_retained _mpu_context;))
 IF_ENABLED(CONFIG_FPU, (__s2ram_ctx struct fpu_ctx_full _fpu_context;))
 
+#if !defined(CONFIG_ARM_CUSTOM_INTERRUPT_CONTROLLER)
+/*
+ * When the NVIC is used as interrupt controller, its non-volatile
+ * state is saved and restored by the architecture layer.
+ *
+ * There should be no pending interrupt upon entry in S2RAM so this
+ * information is not preserved. (If one becomes pending during the
+ * S2RAM entry sequence, it will inhibit entry)
+ *
+ * Each bit in the 32-bit NVIC_ISERn register holds enable state for
+ * one interrupt, whereas each 8-bit NVIC_IPRn register holds priority
+ * for a single interrupt.
+ *
+ * Consume number of interrupts from CONFIG_NUM_IRQS to allow smaller
+ * S2RAM context when less IRQs are used than implemented; however,
+ * make sure this value is never more than what HW actually supports
+ * (according to the core-specific CMSIS header - no runtime check!)
+ * Note the trick: since there is one 8-bit `IPR` register per IRQ,
+ * `sizeof(NVIC->IPR)` is also the maximum number of IRQs!
+ */
+#define NUM_IRQS_FOR_NVIC MIN(CONFIG_NUM_IRQS, sizeof(NVIC->IPR))
+#define NVIC_ISER_IRQS_PER_REG (BITS_PER_BYTE * sizeof(NVIC->ISER[0]))
+
+__s2ram_ctx uint32_t _nvic_iser[DIV_ROUND_UP(NUM_IRQS_FOR_NVIC, NVIC_ISER_IRQS_PER_REG)];
+__s2ram_ctx uint8_t _nvic_ipr[NUM_IRQS_FOR_NVIC];
+#endif /* !CONFIG_ARM_CUSTOM_INTERRUPT_CONTROLLER */
+
 /**
  * These functions are helpers invoked from `pm_s2ram.S`
  * to save/restore CPU state other than general-purpose
  * and special registers (which are handled in assembly)
  */
-void z_arm_pm_s2ram_save_scb_mpu(void)
+void z_arm_pm_s2ram_save_scb_mpu_nvic(void)
 {
 	z_arm_save_scb_context(&_scb_context);
 	IF_ENABLED(CONFIG_MPU, (z_arm_save_mpu_context(&_mpu_context);))
+
+#if !defined(CONFIG_ARM_CUSTOM_INTERRUPT_CONTROLLER)
+	for (size_t i = 0; i < ARRAY_SIZE(_nvic_iser); i++) {
+		_nvic_iser[i] = NVIC->ISER[i];
+	}
+	for (size_t i = 0; i < ARRAY_SIZE(_nvic_ipr); i++) {
+		_nvic_ipr[i] = NVIC->IPR[i];
+	}
+#endif /* !CONFIG_ARM_CUSTOM_INTERRUPT_CONTROLLER */
 }
 
-void z_arm_pm_s2ram_restore_scb_mpu(void)
+void z_arm_pm_s2ram_restore_scb_mpu_nvic(void)
 {
 	z_arm_restore_scb_context(&_scb_context);
 	IF_ENABLED(CONFIG_MPU, (z_arm_restore_mpu_context(&_mpu_context);))
+
+
+#if !defined(CONFIG_ARM_CUSTOM_INTERRUPT_CONTROLLER)
+	for (size_t i = 0; i < ARRAY_SIZE(_nvic_iser); i++) {
+		NVIC->ISER[i] = _nvic_iser[i];
+	}
+	for (size_t i = 0; i < ARRAY_SIZE(_nvic_ipr); i++) {
+		NVIC->IPR[i] = _nvic_ipr[i];
+	}
+#endif /* !CONFIG_ARM_CUSTOM_INTERRUPT_CONTROLLER */
 }
 
 void z_arm_pm_s2ram_save_and_disable_fpu(void)
@@ -81,8 +127,6 @@ void z_arm_pm_s2ram_save_and_disable_fpu(void)
 void z_arm_pm_s2ram_enable_and_restore_fpu(void)
 {
 #if defined(CONFIG_FPU)
-	uint32_t old_control;
-
 #if defined(CONFIG_ARMV8_M_MAINLINE)
 	SCnSCB->CPPWR &= ~(SCnSCB_CPPWR_SU10_Msk | SCnSCB_CPPWR_SU11_Msk);
 #endif /* CONFIG_ARMV8_M_MAINLINE */
