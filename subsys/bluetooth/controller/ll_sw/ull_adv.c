ac9106845123749569a3a770ff6d03f6169428d3
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2026-02-19 11:10:05 +0100

    Bluetooth: Controller: Fix ADV_AUX_IND and SYNC_IND placement
    Fix the Extended Advertising and Periodic Advertising PDU
    placement in the timeline to not overlap due to incorrect
    ticks_slot value used in calculating the ticks_anchor value.
    
    Also fix Extended Advertising, Periodic and BIG event
    placement to consider the event scheduling jitter.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/ull_adv.c
+++ subsys/bluetooth/controller/ll_sw/ull_adv.c
@@ -1403,7 +1403,7 @@ uint8_t ll_adv_enable(uint8_t enable)
 			 */
 			ticks_anchor_aux =
 				ticks_anchor + ticks_slot +
-				HAL_TICKER_US_TO_TICKS(
+				HAL_TICKER_US_TO_TICKS_CEIL(
 					MAX(EVENT_MAFS_US,
 					    EVENT_OVERHEAD_START_US) -
 					EVENT_OVERHEAD_START_US +
@@ -1427,7 +1427,7 @@ uint8_t ll_adv_enable(uint8_t enable)
 						PDU_AC_PAYLOAD_SIZE_MAX,
 						PDU_AC_PAYLOAD_SIZE_MAX);
 				ticks_slot_aux =
-					HAL_TICKER_US_TO_TICKS(us_slot) +
+					HAL_TICKER_US_TO_TICKS_CEIL(us_slot) +
 					ticks_slot_overhead_aux;
 #else
 				ticks_slot_aux = aux->ull.ticks_slot +
@@ -1448,7 +1448,7 @@ uint8_t ll_adv_enable(uint8_t enable)
 				 */
 				uint32_t ticks_anchor_sync = ticks_anchor_aux +
 					ticks_slot_aux +
-					HAL_TICKER_US_TO_TICKS(
+					HAL_TICKER_US_TO_TICKS_CEIL(
 						MAX(EVENT_MAFS_US,
 						    EVENT_OVERHEAD_START_US) -
 						EVENT_OVERHEAD_START_US +
