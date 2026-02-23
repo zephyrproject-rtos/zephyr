fba78a3b734ee7ac28010efc5bfb7484f92bb238
Terry Geng <terry@terriex.com>
2026-02-23 10:44:18 -0500

    drivers: sensor: hx711_spi: Add data ready trigger to HX711 driver
    HX711 pulls DOUT low when data is ready. This commit enabled pin config
    to switch from SPI to GPIO and set up GPIIO interrupts for the falling
    edge.
    
    The implementation is similar to the `infineon,airoc-wifi` driver, by
    using pinctrl.
    
    Signed-off-by: Terry Geng <terry@terriex.com>

--- drivers/sensor/hx711_spi/hx711_spi.h
+++ drivers/sensor/hx711_spi/hx711_spi.h
@@ -13,6 +13,12 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/spi.h>
 
+#if defined(CONFIG_HX711_SPI_TRIGGER)
+#include <zephyr/drivers/pinctrl.h>
+
+#define PINCTRL_STATE_TRIGGER PINCTRL_STATE_PRIV_START
+#endif /* defined(CONFIG_HX711_SPI_TRIGGER) */
+
 #define HX711_SAMPLE_MAX 0x7FFFFF
 
 enum hx711_ch_gain {
@@ -23,15 +29,34 @@ enum hx711_ch_gain {
 
 struct hx711_config {
 	struct spi_dt_spec spi;
-	uint8_t gain;
 	int32_t avdd_uv;
+
+#if defined(CONFIG_HX711_SPI_TRIGGER)
+	const struct pinctrl_dev_config *pcfg;
+	struct gpio_dt_spec int_gpio;
+#endif
 };
 
 struct hx711_data {
 	int32_t sample_uv;
 
+	uint8_t gain;
 	int32_t conv_factor_uv_to_g;
 	int32_t offset;
+#if defined(CONFIG_HX711_SPI_TRIGGER)
+	bool trigger_armed;
+	struct gpio_callback gpio_cb;
+	const struct device *dev;
+
+	sensor_trigger_handler_t data_ready_handler;
+	const struct sensor_trigger *data_ready_trigger;
+	struct k_work work;
+#if defined(CONFIG_HX711_SPI_TRIGGER_OWN_THREAD)
+	struct k_work_q work_q;
+
+	K_KERNEL_STACK_MEMBER(thread_stack, CONFIG_HX711_SPI_THREAD_STACK_SIZE);
+#endif
+#endif /* CONFIG_HX711_SPI_TRIGGER */
 };
 
 #endif /* ZEPHYR_DRIVERS_SENSOR_HX711_SPI_HX711_SPI_H_ */
