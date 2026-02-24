c1ee78a65674fbbbd41d66187d0a304c7adb251a
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2026-02-24 20:10:10 +0100

    drivers: modem: hl78xx: Use native networking naming
    Use the Zephyr prefixed naming when refering to the networking
    functions, macros or types.
    This ensures this code builds when
    CONFIG_NET_NAMESPACE_COMPAT_MODE=n
    
    This is a plain rename without function impact, that otherwise
    is done thru macros given by net_compat.h
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- drivers/modem/hl78xx/hl78xx_sockets.c
+++ drivers/modem/hl78xx/hl78xx_sockets.c
@@ -1215,8 +1215,8 @@ int dns_work_cb(const struct device *dev, bool hard_reset)
 	if (strlen(socket_data->dns.v4_string) > 0) {
 		struct net_sockaddr_in *addr4 = (struct net_sockaddr_in *)&dns_addr;
 
-		if (net_addr_pton(AF_INET, socket_data->dns.v4_string, &addr4->sin_addr) == 0) {
-			addr4->sin_family = AF_INET;
+		if (net_addr_pton(NET_PF_INET, socket_data->dns.v4_string, &addr4->sin_addr) == 0) {
+			addr4->sin_family = NET_PF_INET;
 			addr4->sin_port = 0;
 			valid_address = true;
 		}
@@ -1226,8 +1226,9 @@ int dns_work_cb(const struct device *dev, bool hard_reset)
 	if (!valid_address && strlen(socket_data->dns.v6_string) > 0) {
 		struct net_sockaddr_in6 *addr6 = (struct net_sockaddr_in6 *)&dns_addr;
 
-		if (net_addr_pton(AF_INET6, socket_data->dns.v6_string, &addr6->sin6_addr) == 0) {
-			addr6->sin6_family = AF_INET6;
+		if (net_addr_pton(NET_PF_INET6, socket_data->dns.v6_string, &addr6->sin6_addr)
+		    == 0) {
+			addr6->sin6_family = NET_PF_INET6;
 			addr6->sin6_port = 0;
 			valid_address = true;
 		}
@@ -2115,7 +2116,7 @@ static int handle_tls_sockopts(void *obj, int optname, const void *optval, net_s
 		return 0;
 
 	case ZSOCK_TLS_PEER_VERIFY:
-		if (*(const uint32_t *)optval != TLS_PEER_VERIFY_REQUIRED) {
+		if (*(const uint32_t *)optval != ZSOCK_TLS_PEER_VERIFY_REQUIRED) {
 			LOG_WRN("Disabling peer verification is not supported");
 		}
 		return 0;
