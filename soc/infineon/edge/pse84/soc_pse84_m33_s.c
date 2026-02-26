f06be4df55ebb6420569d99ecc7c9cce612b6938
Laura Carlesso <laura.carlesso@infineon.com>
2026-02-25 21:10:27 -0800

    boards: infineon: kit_pse84_eval: Enable relocate code config on cm33
    Set CONFIG_CODE_DATA_RELOCATION for cm33 core so that
    code that needs to be executed from RAM rather than ext
    flash gets properly relocated. This also requires to
    enable the flash driver. Additionally, ensure that the
    SRAM memory allows code to be executed from it so set
    the MPU appropriately. This aligns the strategy with the cm55
    core for which all this is already in place.
    
    Signed-off-by: Laura Carlesso <laura.carlesso@infineon.com>

--- soc/infineon/edge/pse84/soc_pse84_m33_s.c
+++ soc/infineon/edge/pse84/soc_pse84_m33_s.c
@@ -20,7 +20,36 @@
 
 #include "pse84_boot.h"
 
-#define CY_IPC_MAX_ENDPOINTS (8UL)
+
+/*
+ * This function will allow execute from sram region.  This is needed only for
+ * this sample because by default all soc will disable the execute from SRAM.
+ * An application that requires that the code be executed from SRAM will have
+ * to configure the region appropriately in arm_mpu_regions.c.
+ */
+#ifdef CONFIG_ARM_MPU
+void disable_mpu_rasr_xn(void)
+{
+	uint32_t index;
+
+	/*
+	 * Kept the max index as 8(irrespective of soc) because the sram would
+	 * most likely be set at index 2.
+	 */
+	for (index = 0U; index < 8; index++) {
+		MPU->RNR = index;
+#if defined(CONFIG_ARMV8_M_BASELINE) || defined(CONFIG_ARMV8_M_MAINLINE)
+		if (MPU->RBAR & MPU_RBAR_XN_Msk) {
+			MPU->RBAR ^= MPU_RBAR_XN_Msk;
+		}
+#else
+		if (MPU->RASR & MPU_RASR_XN_Msk) {
+			MPU->RASR ^= MPU_RASR_XN_Msk;
+		}
+#endif /* CONFIG_ARMV8_M_BASELINE || CONFIG_ARMV8_M_MAINLINE */
+	}
+}
+#endif /* CONFIG_ARM_MPU */
 
 static void systeminit_enable_clocks(void)
 {
@@ -86,6 +115,10 @@ static void systeminit_enable_peri(void)
 
 void soc_early_init_hook(void)
 {
+#ifdef CONFIG_ARM_MPU
+	disable_mpu_rasr_xn();
+#endif /* CONFIG_ARM_MPU */
+
 	systeminit_enable_clocks();
 	systeminit_enable_peri();
 
