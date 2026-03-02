b98653311fa9d4fac6b4f1d4b54107872aac2f4a
Phuc Hoang <donp172748@gmail.com>
2026-03-02 13:28:47 +0700

    driver: crc: Fix crc macro in header file
    Fix crc macros in header file:
    include/zephyr/drivers/crc.h
    include/zephyr/sys/crc.h
    
    Signed-off-by: Phuc Hoang <donp172748@gmail.com>

--- include/zephyr/sys/crc.h
+++ include/zephyr/sys/crc.h
@@ -73,19 +73,19 @@ extern "C" {
 /** CRC8_REFLECT polynomial */
 #define CRC8_REFLECT_POLY 0xE0
 
-/** CRC8_ROHC polynomial */
+/** CRC16 polynomial */
 #define CRC16_POLY 0x8005
 
 /** CRC16_ANSI polynomial */
 #define CRC16_REFLECT_POLY 0xA001
 
-/** CRC16_CCITT polynomial */
+/** CRC16_CCITT/CRC16_ITU_T polynomial */
 #define CRC16_CCITT_POLY 0x1021
 
-/** CRC16_ITU_T polynomial */
+/**  CRC24_PGP polynomial */
 #define CRC24_PGP_POLY 0x01864CFBU
 
-/** CRC32_C polynomial */
+/** CRC32_IEEE polynomial */
 #define CRC32_IEEE_POLY 0x04C11DB7U
 
 /** CRC32C polynomial */
