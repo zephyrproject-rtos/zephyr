840dc3181900d3459cee458c972717909602a839
Emil Gydesen <emil.gydesen@nordicsemi.no>
2026-03-04 23:06:44 +0100

    tests: Bluetooth: Audio: Spring cleaning
    Add missing header files, remove unused header files,
    fix typos and missing static.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bluetooth/audio/codec/src/main.c
+++ tests/bluetooth/audio/codec/src/main.c
@@ -11,6 +11,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <zephyr/bluetooth/assigned_numbers.h>
 #include <zephyr/bluetooth/audio/audio.h>
 #include <zephyr/bluetooth/audio/bap_lc3_preset.h>
 #include <zephyr/bluetooth/audio/lc3.h>
--- tests/bsim/bluetooth/audio/src/common.h
+++ tests/bsim/bluetooth/audio/src/common.h
@@ -26,6 +26,7 @@
 #include <zephyr/bluetooth/gatt.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/atomic_types.h>
+#include <zephyr/sys/clock.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/sys/util_macro.h>
 #include <zephyr/sys_clock.h>
