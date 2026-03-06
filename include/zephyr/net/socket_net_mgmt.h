eaf184986251a51625d6631e9ad295365bc3136f
Pieter De Gendt <pieter.degendt@basalte.be>
2026-03-06 10:53:34 +0100

    net: ip: Move struct net_sockaddr_nm to net_ip.h
    Move the structure definition to common header for sockaddr definitions.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- include/zephyr/net/socket_net_mgmt.h
+++ include/zephyr/net/socket_net_mgmt.h
@@ -55,40 +55,6 @@ extern "C" {
 
 /** @} */ /* for @name */
 
-/**
- * struct net_sockaddr_nm - The net_sockaddr structure for NET_MGMT sockets
- *
- * Similar concepts are used as in Linux AF_NETLINK. The NETLINK name is not
- * used in order to avoid confusion between Zephyr and Linux as the
- * implementations are different.
- *
- * The socket domain (address family) is AF_NET_MGMT, and the type of socket
- * is either NET_SOCK_RAW or NET_SOCK_DGRAM, because this is a datagram-oriented
- * service.
- *
- * The protocol (protocol type) selects for which feature the socket is used.
- *
- * When used with bind(), the nm_pid field of the net_sockaddr_nm can be
- * filled with the calling thread' own id. The nm_pid serves here as the local
- * address of this net_mgmt socket. The application is responsible for picking
- * a unique integer value to fill in nm_pid.
- */
-struct net_sockaddr_nm {
-	/** AF_NET_MGMT address family. */
-	net_sa_family_t nm_family;
-
-	/** Network interface related to this address */
-	int nm_ifindex;
-
-	/** Thread id or similar that is used to separate the different
-	 * sockets. Application can decide how the pid is constructed.
-	 */
-	uintptr_t nm_pid;
-
-	/** net_mgmt mask */
-	uint64_t nm_mask;
-};
-
 
 /**
  * Each network management message is prefixed with this header.
