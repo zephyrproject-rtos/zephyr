2757098c15d4018ae119e99321a32a923af7640e
Pieter De Gendt <pieter.degendt@basalte.be>
2026-03-10 13:25:59 +0100

    net: lib: coap: observer: Use struct net_sockaddr_storage
    Replace the incomplete struct net_sockaddr with net_sockaddr_storage for
    CoAP observers.
    
    Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

--- subsys/net/lib/coap/coap.c
+++ subsys/net/lib/coap/coap.c
@@ -1676,7 +1676,7 @@ struct coap_observer *coap_observer_next_unused(
 	size_t i;
 
 	for (i = 0, o = observers; i < len; i++, o++) {
-		if (is_addr_unspecified(&o->addr)) {
+		if (is_addr_unspecified(net_sad(&o->addr))) {
 			return o;
 		}
 	}
@@ -1961,7 +1961,7 @@ void coap_observer_init(struct coap_observer *observer,
 {
 	observer->tkl = coap_header_get_token(request, observer->token);
 
-	memcpy(&observer->addr, addr, sizeof(*addr));
+	memcpy(&observer->addr, addr, net_family2size(addr->sa_family));
 }
 
 static inline void coap_observer_raise_event(struct coap_resource *resource,
@@ -2062,7 +2062,7 @@ struct coap_observer *coap_find_observer(
 
 		if (o->tkl == token_len &&
 		    memcmp(o->token, token, token_len) == 0 &&
-		    sockaddr_equal(&o->addr, addr)) {
+		    sockaddr_equal(net_sad(&o->addr), addr)) {
 			return o;
 		}
 	}
@@ -2079,7 +2079,7 @@ struct coap_observer *coap_find_observer_by_addr(
 	for (i = 0; i < len; i++) {
 		struct coap_observer *o = &observers[i];
 
-		if (sockaddr_equal(&o->addr, addr)) {
+		if (sockaddr_equal(net_sad(&o->addr), addr)) {
 			return o;
 		}
 	}
