bc86bf20cbd6e13435fc96bc74681716d977c1ec
Seppo Takalo <seppo.takalo@nordicsemi.no>
2026-03-11 15:51:11 +0200

    net: l2: ppp: Allow using zero asyncmap locally
    The L2 PPP driver was not working for any other
    Async-Control-Character-Map values than 0xffffffff.
    
    This was caused by two issues:
    * modem_ppp.c was expecting CONTROL field to be always escaped.
    * lcp.c was decoding async_map values wrongly. Either as 16bit,
      or raw 32bit, instead of big-endian 32 bit.
    
    Now it should be safe to default my_options.async_map to a zero.
    
    Signed-off-by: Seppo Takalo <seppo.takalo@nordicsemi.no>

--- include/zephyr/modem/ppp.h
+++ include/zephyr/modem/ppp.h
@@ -42,8 +42,8 @@ enum modem_ppp_receive_state {
 	/* Searching for start of frame and header */
 	MODEM_PPP_RECEIVE_STATE_HDR_SOF = 0,
 	MODEM_PPP_RECEIVE_STATE_HDR_FF,
-	MODEM_PPP_RECEIVE_STATE_HDR_7D,
-	MODEM_PPP_RECEIVE_STATE_HDR_23,
+	MODEM_PPP_RECEIVE_STATE_HDR_CTRL_UNESCAPE,
+	MODEM_PPP_RECEIVE_STATE_HDR_CTRL,
 	/* Writing bytes to network packet */
 	MODEM_PPP_RECEIVE_STATE_WRITING,
 	/* Unescaping next byte before writing to network packet */
