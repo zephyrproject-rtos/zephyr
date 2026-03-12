d9a202314ef697f9de61574b9a46ebd1bf6cf949
Hake Huang <hake.huang@nxp.com>
2026-03-12 18:38:01 +0800

    tests: pm: fix power_residency_time on rw612
    Use the native RW612 power-state node labels and fall back to stop0/1/2
    when a platform still defines those aliases. Also add SPDX headers to the
    new requirements files to satisfy compliance checks.
    
    Signed-off-by: Hake Huang <hake.huang@nxp.com>

--- tests/subsys/pm/power_residency_time/src/main.c
+++ tests/subsys/pm/power_residency_time/src/main.c
@@ -11,12 +11,22 @@
 #define DIFF_RESIDENCY_TIME_STATE_1 600
 #define DIFF_RESIDENCY_TIME_STATE_2 600
 
+#define PM_STATE0_NODE \
+	COND_CODE_1(DT_NODE_EXISTS(DT_NODELABEL(stop0)), (DT_NODELABEL(stop0)), \
+		     (DT_NODELABEL(idle)))
+#define PM_STATE1_NODE \
+	COND_CODE_1(DT_NODE_EXISTS(DT_NODELABEL(stop1)), (DT_NODELABEL(stop1)), \
+		     (DT_NODELABEL(suspend)))
+#define PM_STATE2_NODE \
+	COND_CODE_1(DT_NODE_EXISTS(DT_NODELABEL(stop2)), (DT_NODELABEL(stop2)), \
+		     (DT_NODELABEL(standby)))
+
 int main(void)
 {
 
-	int stop0_min_residency_time = DT_PROP(DT_NODELABEL(stop0), min_residency_us);
-	int stop1_min_residency_time = DT_PROP(DT_NODELABEL(stop1), min_residency_us);
-	int stop2_min_residency_time = DT_PROP(DT_NODELABEL(stop2), min_residency_us);
+	int stop0_min_residency_time = DT_PROP(PM_STATE0_NODE, min_residency_us);
+	int stop1_min_residency_time = DT_PROP(PM_STATE1_NODE, min_residency_us);
+	int stop2_min_residency_time = DT_PROP(PM_STATE2_NODE, min_residency_us);
 
 	while (1) {
 		printk("\nSleep time < min_residency_time of state 0\n");
