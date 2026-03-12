f3e2099438177ecb25ab6568a718411490d513fc
Michał Stasiak <michal.stasiak@nordicsemi.no>
2026-03-12 13:59:06 +0100

    modules: hal_nordic: add mdk_config file
    Adding mdk_config header which will work as
    a glue layer between nrfx-based MDK and Kconfig
    symbols.
    
    Signed-off-by: Michał Stasiak <michal.stasiak@nordicsemi.no>

---
+++ modules/hal_nordic/nrfx/mdk_config.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026, Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef MDK_CONFIG_H__
+#define MDK_CONFIG_H__
+
+/*
+ * These are mappings of Kconfig options enabling MDK-based anomaly workarounds
+   to the corresponding symbols used inside of nrfx-based MDK.
+ */
+
+#endif /* MDK_CONFIG_H__ */
