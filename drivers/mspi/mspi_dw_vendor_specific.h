9084f074820a2712fe5405eefc4de88b8d3b22be
Michał Stasiak <michal.stasiak@nordicsemi.no>
2026-03-13 11:11:19 +0100

    [nrf fromtree] drivers: mspi: allow using MSPI type for nrf-qspi-v2 binding
    MSPI type is just a rename of QSPI type. Code
    provided for nrf-qspi-v2 binding should work with both.
    
    Signed-off-by: Michał Stasiak <michal.stasiak@nordicsemi.no>
    (cherry picked from commit 1038bd14b03d3f84acfc35a3a9f59588d4972de3)

--- drivers/mspi/mspi_dw_vendor_specific.h
+++ drivers/mspi/mspi_dw_vendor_specific.h
@@ -100,6 +100,12 @@ static inline int vendor_specific_xip_disable(const struct device *dev,
 #elif DT_HAS_COMPAT_STATUS_OKAY(nordic_nrf_qspi_v2)
 #include <nrfx.h>
 
+#if defined(MSPI_PRESENT) && !defined(QSPI_PRESENT)
+#define NRF_QSPI_Type		  NRF_MSPI_Type
+#define QSPI_INTENSET_CORE_Pos	  MSPI_INTENSET_CORE_Pos
+#define QSPI_INTENSET_DMADONE_Pos MSPI_INTENSET_CORE_Pos
+#endif
+
 static inline void vendor_specific_init(const struct device *dev)
 {
 	const struct mspi_dw_config *config = dev->config;
