c75550fa7e5786ffa13db57f2a75f50397cf42bf
Kate Wang <yumeng.wang@nxp.com>
2026-03-16 20:31:59 +0800

    drivers: auxdisplay: Add NXP MCUX segmented LCD driver
    Add driver for NXP MCUX Segment LCD (SLCD) controller to support
    segment-style auxiliary displays.
    
    The driver provides a unified implementation that handles both
    the SLCD controller (nxp,slcd) and a generic SLCD panel.
    It supports the generic SLCD panel with common header and binding
    which includes the following panel features:
    - Segment types(7, 14, 16 segments)
    - Configurable front plane pins and back plane COMs
    - pin/COM for each segment
    - Optional pin/COM for each indicator
    - Optional colon/dot indicators
    
    Signed-off-by: Kate Wang <yumeng.wang@nxp.com>

--- modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
+++ modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
@@ -173,6 +173,7 @@ set_variable_ifdef(CONFIG_OPAMP_MCUX_OPAMP_FAST     CONFIG_MCUX_COMPONENT_driver
 set_variable_ifdef(CONFIG_CRC_DRIVER_NXP        CONFIG_MCUX_COMPONENT_driver.crc)
 set_variable_ifdef(CONFIG_CRC_DRIVER_NXP_LPC    CONFIG_MCUX_COMPONENT_driver.lpc_crc)
 set_variable_ifdef(CONFIG_PHY_NXP_T1S           CONFIG_MCUX_COMPONENT_driver.tenbaset_phy)
+set_variable_ifdef(CONFIG_AUXDISPLAY_NXP_SLCD CONFIG_MCUX_COMPONENT_driver.slcd)
 
 if(NOT CONFIG_SOC_MIMX9596)
   set_variable_ifdef(CONFIG_ETH_NXP_IMX_NETC          CONFIG_MCUX_COMPONENT_driver.netc_switch)
