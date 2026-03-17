a0c742edda304796d5342475851b1520dea3bb4f
Soumya Tripathy <s-tripathy@ti.com>
2026-03-17 17:44:39 +0530

    drivers: firmware: scmi: add SMC transport support
    Add support for SCMI transport layer using ARM SMC (Secure Monitor
    Call) as the doorbell mechanism with shared memory for data transfer.
    Communication is performed synchronously so polling is the only
    supported mode. The implementation follows the SCMI specification
    for SMC/HVC based transport.
    
    Signed-off-by: Soumya Tripathy <s-tripathy@ti.com>

--- include/zephyr/drivers/firmware/scmi/util.h
+++ include/zephyr/drivers/firmware/scmi/util.h
@@ -54,9 +54,19 @@
  */
 #define DT_SCMI_TRANSPORT_PROTO_HAS_CHAN(node_id, idx)\
 	DT_PROP_HAS_IDX(node_id, shmem, idx)
-#else /* CONFIG_ARM_SCMI_MAILBOX_TRANSPORT */
+#elif CONFIG_ARM_SCMI_SMC_TRANSPORT
+/** @brief Check if a protocol node has an associated channel
+ *
+ * For SMC transport, all protocols share the base channel.
+ * Return 0, to make all protocols fall back to base channel.
+ *
+ * @param node_id protocol node identifier
+ * @idx channel index
+ */
+#define DT_SCMI_TRANSPORT_PROTO_HAS_CHAN(node_id, idx) 0
+#else
 #error "Transport with static channels needs to define HAS_CHAN macro"
-#endif /* CONFIG_ARM_SCMI_MAILBOX_TRANSPORT */
+#endif
 
 #define SCMI_TRANSPORT_CHAN_NAME(proto, idx) CONCAT(scmi_channel_, proto, _, idx)
 
