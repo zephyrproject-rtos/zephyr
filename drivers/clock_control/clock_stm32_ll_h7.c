347c87463ddc910a7be87d6692bae5305ea3a186
Etienne Carriere <etienne.carriere@st.com>
2026-03-19 15:42:14 +0100

    drivers: clock_control: stm32: clarify inline comment on STM32_HSE_CSS
    Clarify inline comment describing HSE CSS (Clock Security System)
    enabling during clocks initialization.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- drivers/clock_control/clock_stm32_ll_h7.c
+++ drivers/clock_control/clock_stm32_ll_h7.c
@@ -784,8 +784,9 @@ static void set_up_fixed_clock_sources(void)
 		LL_RCC_HSE_Enable();
 		while (LL_RCC_HSE_IsReady() != 1) {
 		}
-		/* Check if we need to enable HSE clock security system or not */
+
 #ifdef STM32_HSE_CSS
+		/* Enable HSE clock security system */
 		z_arm_nmi_set_handler(HAL_RCC_NMI_IRQHandler);
 		LL_RCC_HSE_EnableCSS();
 #endif /* STM32_HSE_CSS */
