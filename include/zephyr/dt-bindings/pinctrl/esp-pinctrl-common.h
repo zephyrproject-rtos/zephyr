2e97d6bb3750c26fb0e826068d7a6801aa76cc6b
Raffael Rostagno <raffael.rostagno@espressif.com>
2026-03-19 10:48:00 -0300

    soc: pinctrl: esp32: Doxygen comments
    Add doxygen comments to pinctrl symbols.
    
    Signed-off-by: Raffael Rostagno <raffael.rostagno@espressif.com>

--- include/zephyr/dt-bindings/pinctrl/esp-pinctrl-common.h
+++ include/zephyr/dt-bindings/pinctrl/esp-pinctrl-common.h
@@ -9,13 +9,15 @@
 
 #include <zephyr/dt-bindings/dt-util.h>
 
+/** @brief GPIO pin number field */
 #define ESP32_PIN_NUM_SHIFT 0U
 #define ESP32_PIN_NUM_MASK  0x3FU
 
-/*
- * Definitions used to extract I/O
- * signal indexes used by the GPIO
- * matrix signal routing mechanism
+/**
+ * @brief Definitions used to extract I/O signal indexes.
+ *
+ * These fields encode the GPIO matrix signal routing mechanism,
+ * allowing internal peripheral signals to be connected to GPIO pins.
  */
 #define ESP32_PIN_SIGI_MASK  0x1FFU
 #define ESP32_PIN_SIGI_SHIFT 6U
@@ -23,15 +25,39 @@
 #define ESP32_PIN_SIGO_SHIFT 15U
 #define ESP_SIG_INVAL        ESP32_PIN_SIGI_MASK
 
+/**
+ * @brief Construct ESP32 pinmux value from pin number and GPIO matrix signals
+ *
+ * @param pin    GPIO pin number
+ * @param sig_i  Input signal index
+ * @param sig_o  Output signal index
+ */
 #define ESP32_PINMUX(pin, sig_i, sig_o)                                                            \
 	(((pin & ESP32_PIN_NUM_MASK) << ESP32_PIN_NUM_SHIFT) |                                     \
 	 ((sig_i & ESP32_PIN_SIGI_MASK) << ESP32_PIN_SIGI_SHIFT) |                                 \
 	 ((sig_o & ESP32_PIN_SIGO_MASK) << ESP32_PIN_SIGO_SHIFT))
 
-/*
- * Definitions used to extract pin
- * properties: bias, drive and
- * initial pin level
+/**
+ * @brief ESP32 pin configuration bit field.
+ *
+ * This field encodes pin configuration options used by the ESP32
+ * pinctrl driver.
+ *
+ * Fields:
+ *
+ * - bias         [ 0 : 1 ]
+ * - drive        [ 2 : 3 ]
+ * - output level [ 4 : 5 ]
+ * - input/output enable override [ 6 : 7 ]
+ * - sleep hold   [ 8 ]
+ *
+ * These values are combined into a single integer and later
+ * translated into GPIO configuration flags by the driver.
+ */
+
+/**
+ * @name ESP32 pin configuration bit field layout
+ * @{
  */
 #define ESP32_PIN_BIAS_SHIFT       0U
 #define ESP32_PIN_BIAS_MASK        0x3U
@@ -41,39 +67,58 @@
 #define ESP32_PIN_OUT_MASK         0x3U
 #define ESP32_PIN_EN_DIR_SHIFT     6U
 #define ESP32_PIN_EN_DIR_MASK      0x3U
-#define ESP32_PIN_SLEEP_HOLD_SHIFT 8U
-#define ESP32_PIN_SLEEP_HOLD_MASK  0x1U
+#define ESP32_PIN_SLEEP_HOLD_SHIFT 8U   /**< Sleep hold bit */
+#define ESP32_PIN_SLEEP_HOLD_MASK  0x1U /**< Sleep hold mask */
+/** @} */
 
-/* Bias definitions */
+/**
+ * @name ESP32 pin bias configuration values
+ * @{
+ */
 #define ESP32_NO_PULL   0x1
 #define ESP32_PULL_UP   0x2
 #define ESP32_PULL_DOWN 0x3
+/** @} */
 
-/* Pin drive definitions */
+/**
+ * @name ESP32 pin drive configuration values
+ * @{
+ */
 #define ESP32_PUSH_PULL  0x1
 #define ESP32_OPEN_DRAIN 0x2
+/** @} */
 
-/*
- * An output pin can be initialized
- * to either high or low
+/**
+ * @name ESP32 pin output level values
+ * @{
  */
 #define ESP32_PIN_OUT_HIGH 0x1
 #define ESP32_PIN_OUT_LOW  0x2
+/** @} */
 
-/*
- * Enable input or output on pin
- * regardless of its direction
+/**
+ * @name ESP32 input/output enable override values
+ * @{
  */
 #define ESP32_PIN_OUT_EN 0x1
 #define ESP32_PIN_IN_EN  0x2
+/** @} */
 
-/* Enable GPIO hold during sleep */
-#define ESP32_PIN_SLEEP_HOLD_EN 0x1
+/**
+ * @name ESP32 sleep hold configuration
+ * @{
+ */
+#define ESP32_PIN_SLEEP_HOLD_EN 0x1 /**< Sleep hold enable */
+/** @} */
 
-/*
- * These flags are used by the pinctrl
- * driver, based on the DTS properties
- * assigned to a specific pin state
+/**
+ * @brief Internal flags used by the ESP32 pinctrl driver.
+ *
+ * These flags are derived from the pin configuration bit field
+ * and control the GPIO driver behavior.
+ *
+ * @name ESP32 pinctrl internal flags
+ * @{
  */
 #define ESP32_NO_PULL_FLAG      BIT(0)
 #define ESP32_PULL_UP_FLAG      BIT(1)
@@ -86,6 +131,7 @@
 #define ESP32_PIN_OUT_LOW_FLAG  BIT(8)
 #define ESP32_PIN_OUT_EN_FLAG   BIT(9)
 #define ESP32_PIN_IN_EN_FLAG    BIT(10)
-#define ESP32_SLEEP_HOLD_FLAG   BIT(11)
+#define ESP32_SLEEP_HOLD_FLAG   BIT(11) /**< Sleep hold enable bit */
+/** @} */
 
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_PINCTRL_ESP_PINCTRL_COMMON_H_ */
