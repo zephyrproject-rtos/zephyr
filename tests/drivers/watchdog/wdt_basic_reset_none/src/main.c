8afd6d5d15e1ffb1de5661475a5046793a2427f3
Hake Huang <hake.huang@nxp.com>
2026-03-19 20:28:56 +0800

    tests: watchdog: make wdt_basic_reset_none reboot-safe
    Some watchdog implementations can invoke the timeout callback and
    still reset the board afterwards, while also not allowing the
    watchdog to be disabled once started. In that case,
    wdt_basic_reset_none can leave the board stuck in a reset loop.
    
    Make the test reboot-safe by storing persistent state in a noinit
    section, marking when the test is armed and when the callback has
    fired. After reboot, the test can detect whether the callback
    completed and finish cleanly instead of re-arming indefinitely.
    
    Also move the feed-count assertion out of interrupt context and
    perform a controlled warm reboot when wdt_disable() returns -EPERM
    after the callback, avoiding repeated watchdog resets.
    
    Wrap the reboot warning message to satisfy checkpatch line-length
    limits.
    
    Signed-off-by: Hake Huang <hake.huang@nxp.com>

--- tests/drivers/watchdog/wdt_basic_reset_none/src/main.c
+++ tests/drivers/watchdog/wdt_basic_reset_none/src/main.c
@@ -5,8 +5,15 @@
  */
 
 #include <zephyr/ztest.h>
+#include <zephyr/cache.h>
 #include <zephyr/drivers/watchdog.h>
+#include <zephyr/sys/reboot.h>
 
+#if DT_NODE_HAS_STATUS_OKAY(DT_CHOSEN(zephyr_dtcm))
+#define NOINIT_SECTION ".dtcm_noinit.test_wdt"
+#else
+#define NOINIT_SECTION ".noinit.test_wdt"
+#endif
 
 /*
  * To use this test, either the devicetree's /aliases must have a
@@ -35,14 +42,21 @@
 static struct wdt_timeout_cfg m_cfg_wdt0;
 static volatile int wdt_interrupted_flag;
 static volatile int wdt_feed_flag;
+static volatile uint32_t wdt_test_state __attribute__((section(NOINIT_SECTION)));
+
+#define WDT_TEST_IDLE            0x00000000U
+#define WDT_TEST_ARMED           0x57445401U
+#define WDT_TEST_CALLBACK_FIRED  0x57445402U
 
 
 static void wdt_callback(const struct device *dev, int channel_id)
 {
+	ARG_UNUSED(dev);
+	ARG_UNUSED(channel_id);
+
 	wdt_interrupted_flag += WDT_TEST_CB_TEST_VALUE;
-	zassert_equal(WDT_FEED_TRIES, wdt_feed_flag,
-			"%d: Invalid number of feeding (expected: %d)",
-			wdt_feed_flag, WDT_FEED_TRIES);
+	wdt_test_state = WDT_TEST_CALLBACK_FIRED;
+	sys_cache_data_flush_range((void *)&wdt_test_state, sizeof(wdt_test_state));
 }
 
 static int test_wdt_callback_reset_none(void)
@@ -52,6 +66,20 @@ static int test_wdt_callback_reset_none(void)
 
 	if (!device_is_ready(wdt)) {
 		TC_PRINT("WDT device is not ready\n");
+		return TC_FAIL;
+	}
+
+	if (wdt_test_state == WDT_TEST_CALLBACK_FIRED) {
+		TC_PRINT("Watchdog callback completed before reboot\n");
+		wdt_test_state = WDT_TEST_IDLE;
+		sys_cache_data_flush_range((void *)&wdt_test_state, sizeof(wdt_test_state));
+		return TC_PASS;
+	}
+
+	if (wdt_test_state == WDT_TEST_ARMED) {
+		TC_PRINT("Unexpected reset while watchdog test was armed\n");
+		wdt_test_state = WDT_TEST_IDLE;
+		sys_cache_data_flush_range((void *)&wdt_test_state, sizeof(wdt_test_state));
 		return TC_FAIL;
 	}
 
@@ -69,8 +97,13 @@ static int test_wdt_callback_reset_none(void)
 		return TC_FAIL;
 	}
 
+	wdt_test_state = WDT_TEST_ARMED;
+	sys_cache_data_flush_range((void *)&wdt_test_state, sizeof(wdt_test_state));
+
 	err = wdt_setup(wdt, WDT_SETUP_FLAGS);
 	if (err != 0) {
+		wdt_test_state = WDT_TEST_IDLE;
+		sys_cache_data_flush_range((void *)&wdt_test_state, sizeof(wdt_test_state));
 		TC_PRINT("Watchdog setup error\n");
 		return TC_FAIL;
 	}
@@ -97,14 +130,29 @@ static int test_wdt_callback_reset_none(void)
 	zassert_equal(wdt_interrupted_flag, WDT_TEST_CB_TEST_VALUE,
 			"wdt callback failed");
 
+	if (wdt_feed_flag != WDT_FEED_TRIES) {
+		wdt_test_state = WDT_TEST_IDLE;
+		sys_cache_data_flush_range((void *)&wdt_test_state, sizeof(wdt_test_state));
+		zassert_equal(wdt_feed_flag, WDT_FEED_TRIES,
+				"%d: Invalid number of feeding (expected: %d)",
+				wdt_feed_flag, WDT_FEED_TRIES);
+	}
+
 	err = wdt_disable(wdt);
 	if (err == -EPERM) {
-		TC_PRINT("Some of the options are not permitted, skip\n");
-		return TC_SKIP;
+		TC_PRINT("Watchdog cannot be disabled after callback, "
+			 "requesting reboot to avoid reset loop\n");
+		sys_reboot(SYS_REBOOT_WARM);
+		CODE_UNREACHABLE;
 	} else if (err != 0) {
+		wdt_test_state = WDT_TEST_IDLE;
+		sys_cache_data_flush_range((void *)&wdt_test_state, sizeof(wdt_test_state));
 		TC_PRINT("Disable watchdog error\n");
 		return TC_FAIL;
 	}
+
+	wdt_test_state = WDT_TEST_IDLE;
+	sys_cache_data_flush_range((void *)&wdt_test_state, sizeof(wdt_test_state));
 
 	return TC_PASS;
 }
