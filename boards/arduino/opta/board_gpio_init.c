9d5f3cbf2d4b9c5a296c62920f93e484cef365de
Pisit Sawangvonganan <pisit@ndrsolution.com>
2026-03-22 16:59:11 +0700

    boards: fix typo
    Use a code spell-checking tool to detect and fix spelling errors in
    the files under `boards/xxx`.
    
    Signed-off-by: Pisit Sawangvonganan <pisit@ndrsolution.com>

--- boards/arduino/opta/board_gpio_init.c
+++ boards/arduino/opta/board_gpio_init.c
@@ -28,7 +28,7 @@ static int board_gpio_init(void)
 	LL_GPIO_SetOutputPin(GPIOH, LL_GPIO_PIN_1);
 #endif
 
-	/* The ethernet adapter is enabled by settig the GPIOJ15 pin to 1.
+	/* The ethernet adapter is enabled by setting the GPIOJ15 pin to 1.
 	 * This is done only if the network has been explicitly configured
 	 */
 #ifdef CONFIG_NET_L2_ETHERNET
