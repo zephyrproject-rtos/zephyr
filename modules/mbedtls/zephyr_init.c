71090430907e361b2d4768d28c1bc0855f0a549f
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2026-03-23 12:21:16 +0200

    modules: mbedtls: stop including Mbed TLS configuration header file
    Standard Mbed TLS header files already take care of doing it properly.
    
    Signed-off-by: Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>

--- modules/mbedtls/zephyr_init.c
+++ modules/mbedtls/zephyr_init.c
@@ -19,14 +19,6 @@
 
 #include <mbedtls/debug.h>
 
-#if defined(CONFIG_MBEDTLS)
-#if !defined(CONFIG_MBEDTLS_CONFIG_FILE)
-#include "mbedtls/config.h"
-#else
-#include CONFIG_MBEDTLS_CONFIG_FILE
-#endif /* CONFIG_MBEDTLS_CONFIG_FILE */
-#endif
-
 #if defined(CONFIG_MBEDTLS_ENABLE_HEAP) && \
 	defined(MBEDTLS_MEMORY_BUFFER_ALLOC_C)
 #include <mbedtls/memory_buffer_alloc.h>
