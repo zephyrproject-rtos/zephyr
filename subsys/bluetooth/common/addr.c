919713204734f19c8b1de24d36f0e718fc9e9323
Johan Hedberg <johan.hedberg@silabs.com>
2026-03-24 16:24:12 +0100

    Bluetooth: Introduce public address to string inline APIs
    The Bluetooth stack has until now had private helpers to return an inline
    pointer to a string - something which can be useful e.g. as part of
    printk()/printf() call parameters. The reason these were never made public
    is that they are not safe when a format string contains more than one
    invocation of these.
    
    This commit introduces replacement APIs which allocate an on-stack buffer,
    thereby allowing arbitrary number of calls within the parameters of an
    output formatting call. The new APIs are defines, since the solution uses
    an encapsulating struct to force an on-stack copy, which then requires a
    translation to access the struct-internal string member variable.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@silabs.com>

--- subsys/bluetooth/common/addr.c
+++ subsys/bluetooth/common/addr.c
@@ -10,3 +10,21 @@ const bt_addr_t bt_addr_any = { { 0, 0, 0, 0, 0, 0 } };
 const bt_addr_t bt_addr_none = { { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff } };
 const bt_addr_le_t bt_addr_le_any = { 0, { { 0, 0, 0, 0, 0, 0 } } };
 const bt_addr_le_t bt_addr_le_none = { 0, { { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff } } };
+
+struct bt_addr_tmp_str bt_addr_tmp_str(const bt_addr_t *addr)
+{
+	struct bt_addr_tmp_str val;
+
+	(void)bt_addr_to_str(addr, val.str, sizeof(val.str));
+
+	return val;
+}
+
+struct bt_addr_le_tmp_str bt_addr_le_tmp_str(const bt_addr_le_t *addr)
+{
+	struct bt_addr_le_tmp_str val;
+
+	(void)bt_addr_le_to_str(addr, val.str, sizeof(val.str));
+
+	return val;
+}
