58d9adb7938a3632a5f4be9535ac7ae05ab0c2f1
Mathieu Choplain <mathieu.choplain-ext@st.com>
2026-03-24 11:20:36 +0100

    drivers: usb: udc: stm32: use SPDX-FileCopyrightText and add ST to list
    Update the copyright notice to use SPDX-FileCopyrightText. While at it, add
    STMicroelectronics to the list of copyright holders as we have made
    numerous contributions to this driver recently.
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain-ext@st.com>

--- drivers/usb/udc/udc_stm32.c
+++ drivers/usb/udc/udc_stm32.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2023 Linaro Limited
+ * SPDX-FileCopyrightText: Copyright (c) 2023 Linaro Limited
+ * SPDX-FileCopyrightText: Copyright (c) 2026 STMicroelectronics
  *
  * SPDX-License-Identifier: Apache-2.0
  */
