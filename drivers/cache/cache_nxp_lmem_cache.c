da4ee6ce23240a914f8efff4227677f6153445e7
Holt Sun <holt.sun@nxp.com>
2026-03-24 13:50:22 +0800

    drivers: cache: add NXP LMEM system cache ops
    Add LMEM data-cache operations to the NXP LMEM cache driver for
    SoCs that expose the system-bus cache and select CONFIG_DCACHE.
    
    Rely on SoC Kconfig capability selection instead of driver-local
    unsupported stubs so instruction-only LMEM parts only build
    instruction-cache support.
    
    This keeps one driver for both instruction-only LMEM parts and
    SoCs that also expose the LMEM system-bus cache.
    
    Signed-off-by: Holt Sun <holt.sun@nxp.com>

--- drivers/cache/cache_nxp_lmem_cache.c
+++ drivers/cache/cache_nxp_lmem_cache.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 NXP
+ * Copyright 2025-2026 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -8,6 +8,68 @@
 #include <zephyr/drivers/cache.h>
 #include <zephyr/logging/log.h>
 #include <fsl_cache.h>
+
+#if defined(CONFIG_DCACHE)
+
+static int cache_data_manage_range(void *addr, size_t size,
+				   void (*operation)(uint32_t, uint32_t))
+{
+	operation((uint32_t)addr, (uint32_t)size);
+
+	return 0;
+}
+
+void cache_data_enable(void)
+{
+	L1CACHE_EnableSystemCache();
+}
+
+void cache_data_disable(void)
+{
+	L1CACHE_DisableSystemCache();
+}
+
+int cache_data_flush_all(void)
+{
+	L1CACHE_CleanSystemCache();
+
+	return 0;
+}
+
+int cache_data_invd_all(void)
+{
+	L1CACHE_InvalidateSystemCache();
+
+	return 0;
+}
+
+int cache_data_flush_and_invd_all(void)
+{
+	L1CACHE_CleanInvalidateSystemCache();
+
+	return 0;
+}
+
+int cache_data_flush_range(void *addr, size_t size)
+{
+	return cache_data_manage_range(addr, size, L1CACHE_CleanSystemCacheByRange);
+}
+
+int cache_data_invd_range(void *addr, size_t size)
+{
+	return cache_data_manage_range(addr, size,
+				       L1CACHE_InvalidateSystemCacheByRange);
+}
+
+int cache_data_flush_and_invd_range(void *addr, size_t size)
+{
+	return cache_data_manage_range(addr, size,
+				       L1CACHE_CleanInvalidateSystemCacheByRange);
+}
+
+#endif
+
+#if defined(CONFIG_ICACHE)
 
 void cache_instr_enable(void)
 {
@@ -60,3 +122,4 @@ int cache_instr_flush_and_invd_range(void *addr, size_t size)
 
 	return 0;
 }
+#endif
