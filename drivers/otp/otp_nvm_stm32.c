e31b811625694ed3090d4b5b3916787365691030
Tobias Piep <piep@metratec.com>
2026-03-24 17:47:45 +0100

    drivers: otp: implement otp_program for stm32 MCUs
    adds support to program the OTP memory on stm32 MCUs
    
    Signed-off-by: Tobias Piep <piep@metratec.com>

--- drivers/otp/otp_nvm_stm32.c
+++ drivers/otp/otp_nvm_stm32.c
@@ -14,9 +14,13 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/otp.h>
 #include <zephyr/kernel.h>
+#include <zephyr/sys/barrier.h>
+#include <zephyr/sys/byteorder.h>
 #include <zephyr/sys/math_extras.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/toolchain.h>
+
+#include "../flash/flash_stm32.h"
 
 #define DT_DRV_COMPAT st_stm32_nvm_otp
 
@@ -117,8 +121,118 @@ static int otp_stm32_nvm_read(const struct device *dev, off_t offset, void *buf,
 	return 0;
 }
 
+#if CONFIG_OTP_PROGRAM
+#define STM32H7_FLASH_TIMEOUT (2 * DT_PROP(DT_INST(0, st_stm32_nv_flash), max_erase_time))
+
+static int _wait_ready(const struct device *dev)
+{
+	FLASH_TypeDef *regs = FLASH_STM32_REGS(dev);
+	k_timepoint_t tp = sys_timepoint_calc(K_MSEC(STM32H7_FLASH_TIMEOUT));
+
+	/* Wait for the data buffer to drain and the operation to finish. */
+	while (regs->FLASH_STM32_SR & (FLASH_SR_BSY | FLASH_SR_DBNE)) {
+		if (sys_timepoint_expired(tp)) {
+			return -ETIMEDOUT;
+		}
+	}
+
+	/* Check for (and clear) errors left by the previous/just-finished op. */
+	if (regs->FLASH_STM32_SR & FLASH_STM32_SR_ERRORS) {
+		regs->FLASH_STM32_CCR = regs->FLASH_STM32_SR & FLASH_STM32_SR_ERRORS;
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static void _flash_cr_unlock(const struct device *dev)
+{
+	FLASH_TypeDef *regs = FLASH_STM32_REGS(dev);
+
+	if (!(FLASH_STM32_REGS(dev)->NSCR & FLASH_CR_LOCK)) {
+		return;
+	}
+
+#ifdef CONFIG_SOC_SERIES_STM32H7X
+	regs->KEYR1 = FLASH_KEY1;
+	regs->KEYR1 = FLASH_KEY2;
+#ifdef DUAL_BANK
+	regs->KEYR2 = FLASH_KEY1;
+	regs->KEYR2 = FLASH_KEY2;
+#endif /* DUAL_BANK */
+#elif CONFIG_SOC_SERIES_STM32H5X
+	regs->NSKEYR = FLASH_KEY1;
+	regs->NSKEYR = FLASH_KEY2;
+#else  /* CONFIG_SOC_SERIES_STM32H7X */
+	regs->KEYR = FLASH_KEY1;
+	regs->KEYR = FLASH_KEY2;
+#endif /* CONFIG_SOC_SERIES_STM32H7X */
+	barrier_dsync_fence_full();
+}
+
+static int otp_stm32_nvm_program(const struct device *dev, off_t offset, const void *buf,
+				 size_t len)
+{
+	const struct otp_stm32_nvm_config *config = dev->config;
+	const size_t start = (size_t)offset;
+	size_t end;
+
+	/*16-bit alignment and halfwords only */
+	if (offset % 2 || len % 2) {
+		return -EINVAL;
+	}
+	if (size_add_overflow(start, len, &end) || end > config->size) {
+		return -EINVAL;
+	}
+	/* Wait until flash is ready */
+	int ret = _wait_ready(dev);
+
+	if (ret != 0) {
+		return ret;
+	}
+
+	_flash_cr_unlock(dev);
+	/* Enable programming mode */
+	FLASH_STM32_REGS(dev)->NSCR |= FLASH_CR_PG;
+	/* Check lock-bit state for the specified memory range */
+	if ((BIT_MASK(DIV_ROUND_UP(len, 64)) << start / 64) & FLASH_STM32_REGS(dev)->OTPBLR_CUR) {
+		ret = -EACCES;
+		goto error;
+	}
+
+	size_t cur = start;
+	const uint8_t *src = buf;
+
+	while (cur < end) {
+		uint16_t v = sys_le16_to_cpu(sys_get_le16(src));
+
+		sys_write16(v, (mem_addr_t)&config->base[cur]);
+		/* Wait for write operation to complete */
+		ret = _wait_ready(dev);
+		if (ret != 0) {
+			goto error;
+		}
+
+		cur += sizeof(uint16_t);
+		src += sizeof(uint16_t);
+	}
+
+error:
+	FLASH_STM32_REGS(dev)->NSCR &= ~FLASH_CR_PG;
+	FLASH_STM32_REGS(dev)->NSCR |= FLASH_CR_LOCK;
+	return ret;
+}
+#endif
+
+static struct flash_stm32_priv flash_data = {
+	.regs = (FLASH_TypeDef *)DT_REG_ADDR(DT_INST_PARENT(0)),
+};
+
 static DEVICE_API(otp, otp_stm32_nvm_api) = {
 	.read = otp_stm32_nvm_read,
+#if CONFIG_OTP_PROGRAM
+	.program = otp_stm32_nvm_program,
+#endif
 };
 
 #define OTP_STM32_NVM_INIT_INNER(inst, _cfg)							\
@@ -127,8 +241,8 @@ static DEVICE_API(otp, otp_stm32_nvm_api) = {
 		.size = DT_INST_REG_SIZE(inst),							\
 	};											\
 												\
-	DEVICE_DT_INST_DEFINE(inst, NULL, NULL, NULL, &_cfg,					\
-			      PRE_KERNEL_1, CONFIG_OTP_INIT_PRIORITY, &otp_stm32_nvm_api);
+	DEVICE_DT_INST_DEFINE(inst, NULL, NULL, &flash_data, &_cfg,				\
+			      POST_KERNEL, CONFIG_OTP_INIT_PRIORITY, &otp_stm32_nvm_api);
 
 #define OTP_STM32_NVM_INIT(inst)								\
 	OTP_STM32_NVM_INIT_INNER(inst, CONCAT(otp_stm32_nvm_cfg, inst))
