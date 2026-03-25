6d080db0b9caa422639968c29def168def49e7fd
Måns Ansgariusson <mansgariusson@gmail.com>
2026-03-25 14:31:07 +0100

    instrumentation: Update ring_buf usage to new zero-copy API
    This commit updates the usage of ring_buf to utilize the new
    put_ptr/commit/get_ptr/consume pattern instead of the traditional
    claim/finish approach. This change is part of a larger refactor aimed at
    streamlining the ring_buf API and improving its efficiency.
    
    Signed-off-by: Måns Ansgariusson <mansgariusson@gmail.com>

--- subsys/instrumentation/common/instr_common.c
+++ subsys/instrumentation/common/instr_common.c
@@ -296,7 +296,7 @@ void instr_dump_buffer_uart(void)
 	DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 
 	uint8_t *transferring_buf;
-	uint32_t transferring_length, instr_buffer_max_length;
+	uint32_t transferring_length;
 
 	/* Make sure instrumentation is disabled. */
 	instr_disable();
@@ -304,19 +304,15 @@ void instr_dump_buffer_uart(void)
 	/* Initiator mark */
 	printk("-*-#");
 
-	instr_buffer_max_length = instr_buffer_capacity_get();
-
-	while (!instr_buffer_is_empty()) {
+	while (!ring_buf_is_empty(instr_buffer_get_ring_buf())) {
 		transferring_length =
-			instr_buffer_get_claim(
-					&transferring_buf,
-					instr_buffer_max_length);
+			ring_buf_get_ptr(instr_buffer_get_ring_buf(), &transferring_buf);
 
 		for (uint32_t i = 0; i < transferring_length; i++) {
 			uart_poll_out(uart_dev, transferring_buf[i]);
 		}
 
-		instr_buffer_get_finish(transferring_length);
+		ring_buf_consume(instr_buffer_get_ring_buf(), transferring_length);
 	}
 
 	/* Terminator mark */
@@ -506,30 +502,12 @@ static void set_up_record(struct instr_record *record, enum instr_event_types ty
 
 static bool instr_record_data_put(struct instr_record *record)
 {
-	uint32_t total_size = 0U;
-
-	uint8_t *data = (uint8_t *) record, *buf;
-	uint32_t length = sizeof(struct instr_record), claimed_size;
-
 	/* If record won't fit, free enough space in the buffer */
-	if (instr_buffer_space_get() < sizeof(struct instr_record)) {
-		instr_buffer_get(NULL, sizeof(struct instr_record));
+	if (ring_buf_space_get(instr_buffer_get_ring_buf()) < sizeof(struct instr_record)) {
+		ring_buf_consume(instr_buffer_get_ring_buf(), sizeof(struct instr_record));
 	}
 
-	do {
-		claimed_size = instr_buffer_put_claim(&buf, length);
-		memcpy(buf, data, claimed_size);
-		total_size += claimed_size;
-		length -= claimed_size;
-		data += claimed_size;
-	} while (length && claimed_size);
-
-	if (length && claimed_size == 0) {
-		instr_buffer_put_finish(0);
-		return false;
-	}
-
-	instr_buffer_put_finish(total_size);
+	ring_buf_put(instr_buffer_get_ring_buf(), (uint8_t *)record, sizeof(struct instr_record));
 	return true;
 }
 
@@ -585,7 +563,8 @@ void instr_event_handler(enum instr_event_types type, void *callee, void *caller
 		struct instr_record record;
 
 		if (!IS_ENABLED(CONFIG_INSTRUMENTATION_MODE_CALLGRAPH_BUFFER_OVERWRITE) &&
-				instr_buffer_space_get() < sizeof(struct instr_record)) {
+		    ring_buf_space_get(instr_buffer_get_ring_buf()) <
+			    sizeof(struct instr_record)) {
 			_instr_tracing_disabled = true;
 			return;
 		}
