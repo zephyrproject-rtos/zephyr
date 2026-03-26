ebc029a44ebb39c3b60a5611a3a6292ed6e7cb5c
Kate Wang <yumeng.wang@nxp.com>
2026-03-26 17:23:11 +0800

    samples: video: capture: Add support for VIDEO_PIX_FMT_RGBA32 pixel format
    Add handling for VIDEO_PIX_FMT_RGBA32 format in the display setup
    function.
    
    Signed-off-by: Kate Wang <yumeng.wang@nxp.com>

--- samples/drivers/video/capture/src/main.c
+++ samples/drivers/video/capture/src/main.c
@@ -84,6 +84,11 @@ static inline int app_setup_display(const struct device *const display_dev, cons
 			}
 		}
 		break;
+	case VIDEO_PIX_FMT_RGBA32:
+		if (capabilities.current_pixel_format != PIXEL_FORMAT_ABGR_8888) {
+			ret = display_set_pixel_format(display_dev, PIXEL_FORMAT_ABGR_8888);
+		}
+		break;
 	default:
 		LOG_ERR("Display pixel format not supported by this sample");
 		return -ENOTSUP;
