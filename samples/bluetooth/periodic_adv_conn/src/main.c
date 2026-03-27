61aeae6fcb4195aad3ba74c034ff73f2bf8633de
Hieu Vo <hieu.vo@nordicsemi.no>
2026-03-27 14:12:19 +0100

    Samples: Bluetooth: Fix periodic_*_conn sample issue when Privacy is disabled.
    In periodic_adv_conn and periodic_sync_conn, use identity address when Privacy is disabled.
    
    Signed-off-by: Hieu Vo <hieu.vo@nordicsemi.no>

--- samples/bluetooth/periodic_adv_conn/src/main.c
+++ samples/bluetooth/periodic_adv_conn/src/main.c
@@ -183,7 +183,12 @@ int main(void)
 	}
 
 	/* Create a non-connectable advertising set */
+#if IS_ENABLED(CONFIG_BT_PRIVACY)
 	err = bt_le_ext_adv_create(BT_LE_EXT_ADV_NCONN, &adv_cb, &pawr_adv);
+#else
+	err = bt_le_ext_adv_create(BT_LE_EXT_ADV_NCONN_IDENTITY, &adv_cb, &pawr_adv);
+#endif
+
 	if (err) {
 		printk("Failed to create advertising set (err %d)\n", err);
 		return 0;
