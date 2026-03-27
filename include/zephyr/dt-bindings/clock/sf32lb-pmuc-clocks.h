c115d93bd9a7c4bdd9802943b90dccec417ea75c
Qingsong Gou <gouqs@hotmail.com>
2026-03-27 08:21:50 +0800

    dts: arm: sifli: sf32lb52x: Add sf32lb52x pmuc clock dts
    Adjust the pmuc clock source
    
    Signed-off-by: Qingsong Gou <gouqs@hotmail.com>

---
+++ include/zephyr/dt-bindings/clock/sf32lb-pmuc-clocks.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2026 SiFli Technologies(Nanjing) Co., Ltd
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DT_BINDINGS_CLOCK_SF32LB_PMUC_CLOCKS_H_
+#define ZEPHYR_DT_BINDINGS_CLOCK_SF32LB_PMUC_CLOCKS_H_
+
+/**
+ * @file
+ * @brief PMUC Clock IDs
+ * @{
+ */
+
+/** @brief Low-Rate Clock 10K */
+#define SF32LB_PMUC_CLOCK_LRC10 0
+/** @brief Low-Rate Clock 32K */
+#define SF32LB_PMUC_CLOCK_LRC32 1
+/** @brief Low-Rate Clock 32K from external crystal */
+#define SF32LB_PMUC_CLOCK_LXT32 2
+
+/** @} */
+
+#endif /* ZEPHYR_DT_BINDINGS_CLOCK_SF32LB_PMUC_CLOCKS_H_ */
