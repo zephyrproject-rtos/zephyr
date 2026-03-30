700409a19f5adb6fcf2703a3e0087a1381f333db
Laura Carlesso <laura.carlesso@infineon.com>
2026-03-30 14:45:28 -0700

    soc: infineon: edge: pse84: Clean up the init process on cm33/ns
    This commit introduces 2 changes:
    - cleans up the mpu initialziation, the old function is not
    required because the initialziation is done properly via
    mpu_regions.c for all cores
    - defaults to using SystemCoreClockSetup instead of
    SystemCoreClockUpdate because the former takes the
    input from DT whereas the latter would read the info
    from the HF clocks which in case of TF-M are secured
    and not directly accessible from the cm33/ns (if SRF is
    not set up)
    
    Signed-off-by: Laura Carlesso <laura.carlesso@infineon.com>

--- soc/infineon/edge/pse84/soc_pse84_m33_ns.c
+++ soc/infineon/edge/pse84/soc_pse84_m33_ns.c
@@ -10,44 +10,17 @@
  */
 
 #include <zephyr/device.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/init.h>
 #include <zephyr/kernel.h>
 
-/*
- * This function will allow execute from sram region.
- */
-#ifdef CONFIG_ARM_MPU
-void disable_mpu_rasr_xn(void)
-{
-	uint32_t index;
+#define PSE84_CPU_FREQ_HZ DT_PROP(DT_PATH(cpus, cpu_0), clock_frequency)
 
-	/*
-	 * Kept the max index as 8(irrespective of soc) because the sram
-	 * would most likely be set at index 2.
-	 */
-	for (index = 0U; index < 8; index++) {
-		MPU->RNR = index;
-#if defined(CONFIG_ARMV8_M_BASELINE) || defined(CONFIG_ARMV8_M_MAINLINE)
-		if (MPU->RBAR & MPU_RBAR_XN_Msk) {
-			MPU->RBAR ^= MPU_RBAR_XN_Msk;
-		}
-#else
-		if (MPU->RASR & MPU_RASR_XN_Msk) {
-			MPU->RASR ^= MPU_RASR_XN_Msk;
-		}
-#endif /* CONFIG_ARMV8_M_BASELINE || CONFIG_ARMV8_M_MAINLINE */
-	}
-}
-#endif /* CONFIG_ARM_MPU */
 
 void soc_early_init_hook(void)
 {
-#ifdef CONFIG_ARM_MPU
-	disable_mpu_rasr_xn();
-#endif /* CONFIG_ARM_MPU */
-
 	/* Initialize SystemCoreClock variable. */
-	SystemCoreClockUpdate();
+	SystemCoreClockSetup(PSE84_CPU_FREQ_HZ, PSE84_CPU_FREQ_HZ);
 }
 
 void soc_late_init_hook(void)
