f6d0638540b0c15062fbfeff1f4893f43b2d50e1
Nancy Sangani <9d.24.nancy.sangani@gmail.com>
2026-04-01 00:57:13 +0530

    runners: qemu: add run and debugserver support
    Signed-off-by: Nancy Sangani <9d.24.nancy.sangani@gmail.com>

--- scripts/west_commands/runners/qemu.py
+++ scripts/west_commands/runners/qemu.py
@@ -1,14 +1,21 @@
 # Copyright (c) 2017 Linaro Limited.
+# Copyright (c) 2025 Nordic Semiconductor ASA
 #
 # SPDX-License-Identifier: Apache-2.0
 
-'''Runner stub for QEMU.'''
+"""Runner for QEMU-based boards."""
 
 from runners.core import RunnerCaps, ZephyrBinaryRunner
 
+DEFAULT_QEMU_GDB_PORT = 1234
+
 
 class QemuBinaryRunner(ZephyrBinaryRunner):
-    '''Place-holder for QEMU runner customizations.'''
+    """Runner for QEMU-based boards."""
+
+    def __init__(self, cfg, gdb_port=DEFAULT_QEMU_GDB_PORT):
+        super().__init__(cfg)
+        self.gdb_port = gdb_port
 
     @classmethod
     def name(cls):
@@ -16,16 +23,24 @@ class QemuBinaryRunner(ZephyrBinaryRunner):
 
     @classmethod
     def capabilities(cls):
-        # This is a stub.
-        return RunnerCaps(commands=set())
+        return RunnerCaps(commands={'run', 'debugserver'})
 
     @classmethod
     def do_add_parser(cls, parser):
-        pass                    # Nothing to do.
+        parser.add_argument('--gdb-port', default=DEFAULT_QEMU_GDB_PORT,
+                            type=int,
+                            help=f'GDB port, default {DEFAULT_QEMU_GDB_PORT}')
 
     @classmethod
     def do_create(cls, cfg, args):
-        return QemuBinaryRunner(cfg)
+        return QemuBinaryRunner(cfg, gdb_port=args.gdb_port)
 
     def do_run(self, command, **kwargs):
-        pass
+        if command == 'run':
+            self.check_call(['cmake', '--build', self.cfg.build_dir,
+                             '--target', 'run'])
+        elif command == 'debugserver':
+            self.logger.info('Starting QEMU GDB server on port %d',
+                             self.gdb_port)
+            self.check_call(['cmake', '--build', self.cfg.build_dir,
+                             '--target', 'debugserver'])
