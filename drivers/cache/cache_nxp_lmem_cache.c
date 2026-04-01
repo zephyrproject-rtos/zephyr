983dfce05303a4591e2ec70225d7a8676e6ae9b0
Holt Sun <holt.sun@nxp.com>
2026-04-01 12:50:09 +0800

    drivers: cache: lmem: check cache enable state
    LMEM cache maintenance helpers can be called before the
    underlying code or system cache has been enabled.
    
    This becomes visible on RT11xx CM4 targets once the generic ARM MPU
    nocache-memory cleanup path uses the generic cache API for non-ARCH
    cache backends during early boot. In that case, LMEM maintenance
    requests must not blindly access the hardware while ENCACHE is clear.
    
    Check the corresponding LMEM enable bit before performing code or
    system cache maintenance. Return -EAGAIN when the selected cache is
    currently disabled, and keep disable requests as a no-op when the
    cache is already off.
    
    Signed-off-by: Holt Sun <holt.sun@nxp.com>

--- drivers/cache/cache_nxp_lmem_cache.c
+++ drivers/cache/cache_nxp_lmem_cache.c
@@ -10,10 +10,25 @@
 #include <fsl_cache.h>
 
 #if defined(CONFIG_DCACHE)
+static inline bool cache_data_is_enabled(void)
+{
+	return (LMEM->PSCCR & LMEM_PSCCR_ENCACHE_MASK) != 0U;
+}
+
+static inline int cache_data_require_enabled(void)
+{
+	return cache_data_is_enabled() ? 0 : -EAGAIN;
+}
 
 static int cache_data_manage_range(void *addr, size_t size,
 				   void (*operation)(uint32_t, uint32_t))
 {
+	int ret = cache_data_require_enabled();
+
+	if (ret != 0) {
+		return ret;
+	}
+
 	operation((uint32_t)addr, (uint32_t)size);
 
 	return 0;
@@ -26,11 +41,21 @@ void cache_data_enable(void)
 
 void cache_data_disable(void)
 {
+	if (!cache_data_is_enabled()) {
+		return;
+	}
+
 	L1CACHE_DisableSystemCache();
 }
 
 int cache_data_flush_all(void)
 {
+	int ret = cache_data_require_enabled();
+
+	if (ret != 0) {
+		return ret;
+	}
+
 	L1CACHE_CleanSystemCache();
 
 	return 0;
@@ -38,6 +63,12 @@ int cache_data_flush_all(void)
 
 int cache_data_invd_all(void)
 {
+	int ret = cache_data_require_enabled();
+
+	if (ret != 0) {
+		return ret;
+	}
+
 	L1CACHE_InvalidateSystemCache();
 
 	return 0;
@@ -45,6 +76,12 @@ int cache_data_invd_all(void)
 
 int cache_data_flush_and_invd_all(void)
 {
+	int ret = cache_data_require_enabled();
+
+	if (ret != 0) {
+		return ret;
+	}
+
 	L1CACHE_CleanInvalidateSystemCache();
 
 	return 0;
@@ -71,6 +108,30 @@ int cache_data_flush_and_invd_range(void *addr, size_t size)
 
 #if defined(CONFIG_ICACHE)
 
+static inline bool cache_instr_is_enabled(void)
+{
+	return (LMEM->PCCCR & LMEM_PCCCR_ENCACHE_MASK) != 0U;
+}
+
+static inline int cache_instr_require_enabled(void)
+{
+	return cache_instr_is_enabled() ? 0 : -EAGAIN;
+}
+
+static int cache_instr_manage_range(void *addr, size_t size,
+				    void (*operation)(uint32_t, uint32_t))
+{
+	int ret = cache_instr_require_enabled();
+
+	if (ret != 0) {
+		return ret;
+	}
+
+	operation((uint32_t)addr, (uint32_t)size);
+
+	return 0;
+}
+
 void cache_instr_enable(void)
 {
 	L1CACHE_EnableCodeCache();
@@ -78,11 +139,21 @@ void cache_instr_enable(void)
 
 void cache_instr_disable(void)
 {
+	if (!cache_instr_is_enabled()) {
+		return;
+	}
+
 	L1CACHE_DisableCodeCache();
 }
 
 int cache_instr_flush_all(void)
 {
+	int ret = cache_instr_require_enabled();
+
+	if (ret != 0) {
+		return ret;
+	}
+
 	L1CACHE_CleanCodeCache();
 
 	return 0;
@@ -90,6 +161,12 @@ int cache_instr_flush_all(void)
 
 int cache_instr_invd_all(void)
 {
+	int ret = cache_instr_require_enabled();
+
+	if (ret != 0) {
+		return ret;
+	}
+
 	L1CACHE_InvalidateCodeCache();
 
 	return 0;
@@ -97,6 +174,12 @@ int cache_instr_invd_all(void)
 
 int cache_instr_flush_and_invd_all(void)
 {
+	int ret = cache_instr_require_enabled();
+
+	if (ret != 0) {
+		return ret;
+	}
+
 	L1CACHE_CleanInvalidateCodeCache();
 
 	return 0;
@@ -104,22 +187,19 @@ int cache_instr_flush_and_invd_all(void)
 
 int cache_instr_flush_range(void *addr, size_t size)
 {
-	L1CACHE_CleanCodeCacheByRange((uint32_t)addr, (uint32_t)size);
-
-	return 0;
+	return cache_instr_manage_range(addr, size,
+				       L1CACHE_CleanCodeCacheByRange);
 }
 
 int cache_instr_invd_range(void *addr, size_t size)
 {
-	L1CACHE_InvalidateCodeCacheByRange((uint32_t)addr, (uint32_t)size);
-
-	return 0;
+	return cache_instr_manage_range(addr, size,
+				       L1CACHE_InvalidateCodeCacheByRange);
 }
 
 int cache_instr_flush_and_invd_range(void *addr, size_t size)
 {
-	L1CACHE_CleanInvalidateCodeCacheByRange((uint32_t)addr, (uint32_t)size);
-
-	return 0;
+	return cache_instr_manage_range(addr, size,
+				       L1CACHE_CleanInvalidateCodeCacheByRange);
 }
 #endif
