bb53b079a5d94ca1de8f8f470322ae75d8b174aa
Carlo Caione <ccaione@baylibre.com>
2026-04-02 22:33:54 +0200

    tests: syscon: add tests for update_bits and alignment rejection
    Add test coverage for the new syscon_update_bits() API and the
    misaligned access rejection.
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

--- tests/drivers/syscon/src/main.c
+++ tests/drivers/syscon/src/main.c
@@ -10,7 +10,7 @@
 
 #define RES_SECT LINKER_DT_NODE_REGION_NAME(DT_NODELABEL(res))
 
-uint8_t var_in_res0[DT_REG_SIZE(DT_NODELABEL(syscon))] __attribute((__section__(RES_SECT)));
+uint8_t var_in_res0[DT_REG_SIZE(DT_NODELABEL(res))] __attribute((__section__(RES_SECT)));
 
 ZTEST(syscon, test_size)
 {
@@ -39,7 +39,7 @@ ZTEST(syscon, test_read)
 	uint32_t val;
 
 	zassert_ok(syscon_get_base(dev, &base_addr));
-	for (size_t i = 0; i < ARRAY_SIZE(var_in_res0); ++i) {
+	for (size_t i = 0; i < DT_REG_SIZE(DT_NODELABEL(syscon)); ++i) {
 		((uint8_t *)base_addr)[i] = i;
 		zassert_ok(syscon_read_reg(dev, i, &val));
 		zassert_equal(i, val);
@@ -52,10 +52,66 @@ ZTEST(syscon, test_write)
 	uintptr_t base_addr;
 
 	zassert_ok(syscon_get_base(dev, &base_addr));
-	for (uint32_t i = 0; i < ARRAY_SIZE(var_in_res0); ++i) {
+	for (uint32_t i = 0; i < DT_REG_SIZE(DT_NODELABEL(syscon)); ++i) {
 		zassert_ok(syscon_write_reg(dev, i, i));
 		zassert_equal(((uint8_t *)base_addr)[i], i);
 	}
+}
+
+ZTEST(syscon, test_update_bits)
+{
+	const struct device *const dev = DEVICE_DT_GET(DT_NODELABEL(syscon));
+	uintptr_t base_addr;
+	uint32_t val;
+
+	zassert_ok(syscon_get_base(dev, &base_addr));
+
+	/* Write a known value, then update specific bits */
+	zassert_ok(syscon_write_reg(dev, 0, 0xA5));
+	zassert_ok(syscon_update_bits(dev, 0, 0x0F, 0x03));
+	zassert_ok(syscon_read_reg(dev, 0, &val));
+	zassert_equal(val, 0xA3, "expected 0xA3, got 0x%02x", val);
+
+	/* Update with mask that covers all bits */
+	zassert_ok(syscon_update_bits(dev, 0, 0xFF, 0x42));
+	zassert_ok(syscon_read_reg(dev, 0, &val));
+	zassert_equal(val, 0x42);
+
+	/* Update with zero mask changes nothing */
+	zassert_ok(syscon_update_bits(dev, 0, 0x00, 0xFF));
+	zassert_ok(syscon_read_reg(dev, 0, &val));
+	zassert_equal(val, 0x42);
+}
+
+ZTEST(syscon, test_update_bits_out_of_bounds)
+{
+	const struct device *const dev = DEVICE_DT_GET(DT_NODELABEL(syscon));
+
+	zassert_equal(syscon_update_bits(dev, DT_REG_SIZE(DT_NODELABEL(syscon)), 0xFF, 0x00),
+		      -EINVAL);
+}
+
+ZTEST(syscon, test_misaligned_access)
+{
+	const struct device *const dev32 = DEVICE_DT_GET(DT_NODELABEL(syscon32));
+	uint32_t val;
+
+	/* Aligned access should succeed */
+	zassert_ok(syscon_write_reg(dev32, 0, 0xDEADBEEF));
+	zassert_ok(syscon_read_reg(dev32, 0, &val));
+	zassert_equal(val, 0xDEADBEEF);
+
+	zassert_ok(syscon_write_reg(dev32, 4, 0xCAFEBABE));
+	zassert_ok(syscon_read_reg(dev32, 4, &val));
+	zassert_equal(val, 0xCAFEBABE);
+
+	/* Misaligned accesses should be rejected */
+	zassert_equal(syscon_read_reg(dev32, 1, &val), -EINVAL);
+	zassert_equal(syscon_read_reg(dev32, 2, &val), -EINVAL);
+	zassert_equal(syscon_read_reg(dev32, 3, &val), -EINVAL);
+	zassert_equal(syscon_write_reg(dev32, 1, 0), -EINVAL);
+	zassert_equal(syscon_write_reg(dev32, 5, 0), -EINVAL);
+	zassert_equal(syscon_update_bits(dev32, 3, 0xFF, 0), -EINVAL);
 }
 
 ZTEST_SUITE(syscon, NULL, NULL, NULL, NULL, NULL);
