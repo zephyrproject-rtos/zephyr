8f1eb6e5aebd99e87de3619fafcfcca40488e8ef
Yangbo Lu <yangbo.lu@nxp.com>
2026-04-02 10:43:07 +0800

    drivers: ethernet: dsa_nxp_imx_netc: use ptp_clock when needs timestamping
    Used ptp_clock only when NETC_PTP_TIMESTAMPING_SUPPORT is true.
    This is to keep same with driver timestamping operations code in
    other places, and also to keep same with dsa core driver which uses
    ptp_clock when NET_L2_PTP is true.
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- drivers/ethernet/dsa/dsa_nxp_imx_netc.c
+++ drivers/ethernet/dsa/dsa_nxp_imx_netc.c
@@ -558,7 +558,7 @@ static struct dsa_api dsa_netc_api = {
 		.phy_mode = DT_PROP_OR(port, phy_connection_type, ""),                      \
 		.tag_proto = DT_PROP_OR(port, dsa_tag_protocol, DSA_TAG_PROTO_NOTAG),       \
 		.ethernet_connection = DEVICE_DT_GET_OR_NULL(DT_PHANDLE(port, ethernet)),   \
-		IF_ENABLED(CONFIG_PTP_CLOCK_NXP_NETC,				            \
+		IF_ENABLED(NETC_PTP_TIMESTAMPING_SUPPORT,				    \
 			(.ptp_clock = DEVICE_DT_GET_OR_NULL(DT_PHANDLE(port, ptp_clock)),)) \
 		.prv_config = &dsa_netc_##n##_##port##_config,                              \
 	};                                                                                  \
