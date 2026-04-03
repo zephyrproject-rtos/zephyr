c1802dc51fcc316cf7ea59c2c67fb505bce0933c
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2026-04-03 15:17:12 +0800

    gpio: mcux: enable clocks from devicetree
    Use the optional clocks property to request the GPIO port clock
    during driver initialization.
    
    This lets RT7xx GPIO instances rely on the common clock control
    path instead of board-specific clock enabling.
    
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

--- drivers/gpio/gpio_mcux.c
+++ drivers/gpio/gpio_mcux.c
@@ -9,6 +9,7 @@
 
 #include <errno.h>
 #include <zephyr/device.h>
+#include <zephyr/drivers/clock_control.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/dt-bindings/gpio/nxp-kinetis-gpio.h>
 #include <zephyr/irq.h>
@@ -51,6 +52,8 @@ struct gpio_mcux_config {
 	PORT_Type *port_base;
 #endif /* defined(CONFIG_PINCTRL_NXP_IOCON) */
 	unsigned int flags;
+	const struct device *clock_dev;
+	clock_control_subsys_t clock_subsys;
 };
 
 struct gpio_mcux_data {
@@ -524,6 +527,11 @@ static DEVICE_API(gpio, gpio_mcux_driver_api) = {
 		GPIO_PERIPH_BASE_DEFINE(n)                                                         \
 		.flags = UTIL_AND(UTIL_OR(DT_INST_IRQ_HAS_IDX(n, 0), GPIO_HAS_SHARED_IRQ),         \
 				  GPIO_INT_ENABLE),                                                \
+		.clock_dev = COND_CODE_1(DT_INST_NODE_HAS_PROP(n, clocks),                         \
+			(DEVICE_DT_GET(DT_INST_CLOCKS_CTLR(n))), (NULL)),                          \
+		.clock_subsys = COND_CODE_1(DT_INST_NODE_HAS_PROP(n, clocks),                      \
+			((clock_control_subsys_t)DT_INST_CLOCKS_CELL(n, name)),                    \
+			((clock_control_subsys_t)0U)),                                             \
 	};                                                                                         \
                                                                                                    \
 	static struct gpio_mcux_data gpio_mcux_port##n##_data;                                     \
@@ -534,6 +542,20 @@ static DEVICE_API(gpio, gpio_mcux_driver_api) = {
                                                                                                    \
 	static int gpio_mcux_port##n##_init(const struct device *dev)                              \
 	{                                                                                          \
+		const struct gpio_mcux_config *config = dev->config;                               \
+		int ret;                                                                           \
+		                                                                                   \
+		if (config->clock_dev != NULL) {                                                   \
+			if (!device_is_ready(config->clock_dev)) {                                 \
+				return -ENODEV;                                                    \
+			}                                                                          \
+			                                                                           \
+			ret = clock_control_on(config->clock_dev, config->clock_subsys);           \
+			if (ret != 0) {                                                            \
+				return ret;                                                        \
+			}                                                                          \
+		}                                                                                  \
+		                                                                                   \
 		IF_ENABLED(DT_INST_IRQ_HAS_IDX(n, 0),			\
 			(GPIO_MCUX_IRQ_INIT(n);))                                         \
 		return 0;                                                                          \
