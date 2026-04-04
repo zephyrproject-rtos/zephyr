50cf2ba410a042c469e27efd19b6b0dd1a551009
Egill Sigurdur <egill@egill.xyz>
2026-04-04 13:43:07 +0000

    net: sockets: support FIONWRITE ioctl for TCP
    Add support for getting the number of bytes queued for TCP TX
    that have not yet been acknowledged. Follows FreeBSD's API.
    
    Signed-off-by: Egill Sigurdur <egill@egill.xyz>

--- include/zephyr/posix/sys/ioctl.h
+++ include/zephyr/posix/sys/ioctl.h
@@ -8,8 +8,12 @@
 
 #include <zephyr/sys/fdtable.h>
 
+/** Set non-blocking mode */
 #define FIONBIO ZFD_IOCTL_FIONBIO
+/** Get the number of bytes available to read */
 #define FIONREAD ZFD_IOCTL_FIONREAD
+/** Get the number of bytes queued for TCP TX which have not yet been acknowledged */
+#define FIONWRITE ZFD_IOCTL_FIONWRITE
 
 #ifdef __cplusplus
 extern "C" {
