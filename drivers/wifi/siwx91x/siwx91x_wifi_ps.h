956a82cec50dab5ab2dcc70d196daee7479cb246
Tejus Subramanya <ters@silabs.com>
2026-04-08 19:08:46 +0530

    drivers: wifi: siwx91x: raise NET_EVENT_WIFI_TWT from WiseConnect
    Register SL_WIFI_TWT_RESPONSE_EVENTS and map sl_wifi_event_t /
    sl_wifi_twt_response_t into struct wifi_twt_params, then call
    wifi_mgmt_raise_twt_event() so applications receive TWT setup/teardown
    results (resp_status, setup_cmd, negotiated parameters).
    
    Signed-off-by: Tejus Subramanya <ters@silabs.com>

--- drivers/wifi/siwx91x/siwx91x_wifi_ps.h
+++ drivers/wifi/siwx91x/siwx91x_wifi_ps.h
@@ -5,6 +5,8 @@
 #ifndef SIWX91X_WIFI_PS_H
 #define SIWX91X_WIFI_PS_H
 
+#include "sl_wifi_types.h"
+
 struct device;
 struct siwx91x_dev;
 struct wifi_twt_params;
@@ -16,5 +18,8 @@ int siwx91x_set_power_save(const struct device *dev, struct wifi_ps_params *para
 int siwx91x_get_power_save_config(const struct device *dev, struct wifi_ps_config *config);
 
 int siwx91x_apply_power_save(struct siwx91x_dev *sidev);
+
+sl_status_t siwx91x_on_twt(sl_wifi_event_t twt_event, sl_status_t status_code, void *data,
+			   uint32_t data_length, void *arg);
 
 #endif
