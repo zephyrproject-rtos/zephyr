09e2939d967be346fd542c54519d43ac6fbec87c
Carlo Caione <ccaione@baylibre.com>
2026-04-09 15:16:01 +0200

    scripts: ci: disable DeviceMmioCheck pending community discussion
    Disable the DeviceMmioCheck compliance check while documentation and
    community discussion are ongoing. The check code is kept in place and
    can be re-enabled by removing the self.skip() call.
    
    See https://github.com/zephyrproject-rtos/zephyr/issues/106966
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

--- scripts/ci/check_compliance.py
+++ scripts/ci/check_compliance.py
@@ -2673,6 +2673,11 @@ class DeviceMmioCheck(ComplianceTest):
         return added
 
     def run(self):
+        self.skip(
+            "Check disabled pending documentation and community discussion. "
+            "See https://github.com/zephyrproject-rtos/zephyr/issues/106966"
+        )
+
         for fname in get_files(filter='d'):
             if not fname.startswith('drivers/') or not fname.endswith(('.c', '.h')):
                 continue
