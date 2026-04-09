adbefad321aee7cf97ba216e4be78aa72f5ca946
Jérôme Pouiller <jerome.pouiller@silabs.com>
2026-04-09 14:43:03 +0200

    soc: silabs: siwx91x: nwp: Implement Zero-Copy design
    The previous implementation relied on the WiseConnect SDK bus layer and its
    memory manager to transfer frames between the M4 and the NWP. These
    components internally copied data into their own memory pools, adding
    latency and memory overhead.
    
    Replace them with a new native bus layer that operates directly on net_buf
    chains. Tx frames are built by the caller as a net_buf and handed to the
    bus layer without any intermediate copy. If the buffer is contiguous, it is
    directly sent to the NWP. Otherwise a bounce buffer is involved.
    
    Rx buffers are pre-allocated from a net_buf pool. In preparation for the
    next Wifi driver, if NETWORKING is enabled is set and
    NET_BUF_DATA_SIZE=1616, then the main network pool is used for the
    allocation. Then, the buffer fed directly to the DMA Rx descriptor, so
    received data lands in a net_buf that can be passed up the stack without
    copying.
    
    This is a major rewrite of the NWP driver. Since main buffer structure
    changed, most of the code has been rewritten. It was also the opportunity
    to clean up many aspect of the NWP interface (properly define the
    registers, simplify the code, use Zephyr Native structures, ...)
    
    At the end, this driver save >60kB of RAM. The performance gain depends of
    the configuration. Since the memory is less constraints, it is easier to
    allocate large buffers and improve the performances.
    
    The bus layer is split into dedicated source files:
      - siwx91x_nwp_bus.c contains the core of the communication mechanism with
        the NWP
      - siwx91x_nwp_fw.c is the register level communication with the NWP. It
        is essentially the boot-up sequence.
      - siwx91x_nwp_api.c provide high-level helpers for the NWP API.
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

---
+++ soc/silabs/silabs_siwx91x/nwp/siwx91x_nwp_api.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2025 Silicon Laboratories Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef SIWX91X_NWP_API_H_
+#define SIWX91X_NWP_API_H_
+#include <stdint.h>
+#include <zephyr/net/ethernet.h>
+#include <zephyr/net/wifi.h>
+#include "device/silabs/si91x/wireless/inc/sl_wifi_device.h"
+#include "protocol/wifi/inc/sl_wifi_constants.h"
+#include "protocol/wifi/inc/sl_wifi_types.h"
+#include "sli_wifi/inc/sli_wifi_types.h"
+
+struct device;
+
+/* sl_wifi_firmware_version_t is buggy because it lacks of __packed */
+struct siwx91x_nwp_version {
+	uint8_t chip_id;
+	uint8_t rom_id;
+	uint8_t major;
+	uint8_t minor;
+	uint8_t security_version;
+	uint8_t patch_num;
+	uint8_t customer_id;
+	uint16_t build_num;
+} __packed;
+
+/* NWP APIs */
+void siwx91x_nwp_soft_reset(const struct device *dev);
+void siwx91x_nwp_force_assert(const struct device *dev);
+void siwx91x_nwp_opermode(const struct device *dev,
+			  sl_wifi_system_boot_configuration_t *params);
+void siwx91x_nwp_feature(const struct device *dev, bool enable_pll);
+void siwx91x_nwp_dynamic_pool(const struct device *dev, int tx, int rx, int global);
+void siwx91x_nwp_get_firmware_version(const struct device *dev,
+				      struct siwx91x_nwp_version *version);
+
+#endif
---
+++ soc/silabs/silabs_siwx91x/nwp/siwx91x_nwp_fw_version.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2025 Silicon Laboratories Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "siwx91x_nwp_api.h"
+
+extern const struct siwx91x_nwp_version siwx91x_nwp_fw_expected_version;
