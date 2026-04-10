a90426556a7906d74333a15524edcaabb61da703
Jérôme Pouiller <jerome.pouiller@silabs.com>
2026-04-10 10:37:35 +0200

    drivers: wifi: siwx91x: Implement Access Point
    Implement the Access Point feature with the new NWP driver.
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- soc/silabs/silabs_siwx91x/nwp/siwx91x_nwp_api.h
+++ soc/silabs/silabs_siwx91x/nwp/siwx91x_nwp_api.h
@@ -47,14 +47,21 @@ int siwx91x_nwp_scan(const struct device *dev, uint16_t channel_list, const char
 int siwx91x_nwp_join(const struct device *dev, const char *ssid,
 		     const uint8_t *bssid, int security_type);
 int siwx91x_nwp_disconnect(const struct device *dev);
+void siwx91x_nwp_ap_config(const struct device *dev, sli_wifi_ap_config_request *params);
+int siwx91x_nwp_ap_start(const struct device *dev, const char *ssid, int security_type);
+void siwx91x_nwp_ap_stop(const struct device *dev);
+void siwx91x_nwp_sta_disconnect(const struct device *dev,
+				const uint8_t remote_addr[WIFI_MAC_ADDR_LEN]);
 void siwx91x_nwp_get_bss_info(const struct device *dev, sl_wifi_operational_statistics_t *info);
 void siwx91x_nwp_ps_disable(const struct device *dev);
 void siwx91x_nwp_ps_enable(const struct device *dev, sli_wifi_power_save_request_t *params);
 void siwx91x_nwp_twt_params(const struct device *dev, sl_wifi_twt_request_t *params);
+void siwx91x_nwp_set_region_ap(const struct device *dev);
 void siwx91x_nwp_set_region_sta(const struct device *dev, sl_wifi_region_code_t region_code);
 void siwx91x_nwp_set_psk(const struct device *dev, const char *psk);
 void siwx91x_nwp_set_sta_config(const struct device *dev);
 void siwx91x_nwp_set_band(const struct device *dev, sl_wifi_band_mode_t band);
+void siwx91x_nwp_set_ht_caps(const struct device *dev, bool enabled);
 void siwx91x_nwp_set_config(const struct device *dev, uint16_t type, uint16_t value);
 void siwx91x_nwp_get_mac_address(const struct device *dev, uint8_t mac[NET_ETH_ADDR_LEN]);
 
