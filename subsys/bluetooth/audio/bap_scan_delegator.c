81aefe2a485d295965b0a5a23fe520151910524f
Emil Gydesen <emil.gydesen@nordicsemi.no>
2026-04-10 11:10:33 +0200

    Bluetooth: BAP: Fix deligator typo
    It is called the BAP Scan Delegator, and not deligator.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/audio/bap_scan_delegator.c
+++ subsys/bluetooth/audio/bap_scan_delegator.c
@@ -92,7 +92,7 @@ struct bt_bap_scan_delegator_inst {
 
 enum scan_delegator_flag {
 	SCAN_DELEGATOR_FLAG_REGISTERED_CONN_CB,
-	SCAN_DELEGATOR_FLAG_REGISTERED_SCAN_DELIGATOR,
+	SCAN_DELEGATOR_FLAG_REGISTERED_SCAN_DELEGATOR,
 
 	SCAN_DELEGATOR_FLAG_NUM,
 };
@@ -1389,7 +1389,7 @@ int bt_bap_scan_delegator_register(struct bt_bap_scan_delegator_cb *cb)
 	int err;
 
 	if (atomic_test_and_set_bit(scan_delegator_flags,
-				    SCAN_DELEGATOR_FLAG_REGISTERED_SCAN_DELIGATOR)) {
+				    SCAN_DELEGATOR_FLAG_REGISTERED_SCAN_DELEGATOR)) {
 		LOG_DBG("Scan delegator already registered");
 		return -EALREADY;
 	}
@@ -1397,7 +1397,7 @@ int bt_bap_scan_delegator_register(struct bt_bap_scan_delegator_cb *cb)
 	err = bass_register();
 	if (err) {
 		atomic_clear_bit(scan_delegator_flags,
-				 SCAN_DELEGATOR_FLAG_REGISTERED_SCAN_DELIGATOR);
+				 SCAN_DELEGATOR_FLAG_REGISTERED_SCAN_DELEGATOR);
 		return err;
 	}
 
@@ -1432,15 +1432,14 @@ int bt_bap_scan_delegator_unregister(void)
 	int err;
 
 	if (!atomic_test_and_clear_bit(scan_delegator_flags,
-				       SCAN_DELEGATOR_FLAG_REGISTERED_SCAN_DELIGATOR)) {
+				       SCAN_DELEGATOR_FLAG_REGISTERED_SCAN_DELEGATOR)) {
 		LOG_DBG("Scan delegator not yet registered");
 		return -EALREADY;
 	}
 
 	err = bass_unregister();
 	if (err) {
-		atomic_set_bit(scan_delegator_flags,
-			       SCAN_DELEGATOR_FLAG_REGISTERED_SCAN_DELIGATOR);
+		atomic_set_bit(scan_delegator_flags, SCAN_DELEGATOR_FLAG_REGISTERED_SCAN_DELEGATOR);
 		return err;
 	}
 
