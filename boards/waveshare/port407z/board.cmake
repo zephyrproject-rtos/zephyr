82365d4e46800663db102378f9a240ea211d4367
Yongci <zhuyongci@hotmail.com>
2026-04-11 20:47:38 +0800

    Add WaveShare Port407Z board DTS

--- boards/waveshare/port407z/board.cmake
+++ boards/waveshare/port407z/board.cmake
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(pyocd "--target=STM32F407ZGTx" "--frequency=4000000")
+board_runner_args(pyocd "--target=stm32f407zg" "--frequency=4000000")
 
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
