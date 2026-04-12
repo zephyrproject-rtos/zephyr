c8b0fcb0e6aa141a09218432541a74ea87449f63
Anas Nashif <anas.nashif@intel.com>
2026-04-12 08:16:28 -0400

    twister: support unified test definition file
    Now use tests.yaml or tests.yml as the test definition file, sample.yaml
    and testcase.yaml are deprecated but will still work for a while while
    we transition.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- scripts/pylib/twister/twisterlib/testplan.py
+++ scripts/pylib/twister/twisterlib/testplan.py
@@ -156,8 +156,11 @@ class TestPlan:
         os.path.join(ZEPHYR_BASE,
                      "scripts", "schemas", "twister", "quarantine-schema.yaml"))
 
-    SAMPLE_FILENAME = 'sample.yaml'
-    TESTSUITE_FILENAME = 'testcase.yaml'
+    TEST_DEFINITION_FILENAME = [
+            'testcase.yaml',
+            'tests.yaml',
+            'sample.yaml'
+            ]
 
     def __init__(self, env: TwisterEnv):
 
@@ -541,11 +544,10 @@ class TestPlan:
             logger.debug(f"Reading testsuite configuration files under {root}...")
 
             for dirpath, _, filenames in os.walk(root, topdown=True):
-                if self.SAMPLE_FILENAME in filenames:
-                    filename = self.SAMPLE_FILENAME
-                elif self.TESTSUITE_FILENAME in filenames:
-                    filename = self.TESTSUITE_FILENAME
-                else:
+                filename = next(
+                    (f for f in self.TEST_DEFINITION_FILENAME if f in filenames), None
+                )
+                if filename is None:
                     continue
 
                 logger.debug("Found possible testsuite in " + dirpath)
