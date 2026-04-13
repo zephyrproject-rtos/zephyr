b813606149d3e4b36cad1be2fc6353fb3e9c940b
Fin Maaß <f.maass@vogl-electronic.com>
2026-04-13 17:46:58 +0200

    cmake: qemu: move qemu network interface selection
    move qemu network interface selection
    
    Signed-off-by: Fin Maaß <f.maass@vogl-electronic.com>

--- cmake/emu/qemu.cmake
+++ cmake/emu/qemu.cmake
@@ -425,6 +425,34 @@ if(CONFIG_UHC_OHCI_PCI)
   )
 endif()
 
+# If we are using a suitable ethernet driver inside qemu, then these options
+# must be set, otherwise a zephyr instance cannot receive any network packets.
+# The Qemu supported ethernet driver should define CONFIG_ETH_NIC_MODEL
+# string that tells what nic model Qemu should use.
+if(CONFIG_QEMU_TARGET)
+  if((CONFIG_NET_QEMU_ETHERNET OR CONFIG_NET_QEMU_USER) AND NOT CONFIG_ETH_NIC_MODEL)
+    message(FATAL_ERROR "
+      No Qemu ethernet driver configured!
+      Enable Qemu supported ethernet driver like e1000 at drivers/ethernet"
+    )
+  elseif(CONFIG_NET_QEMU_ETHERNET)
+    if(CONFIG_ETH_QEMU_EXTRA_ARGS)
+      set(NET_QEMU_ETH_EXTRA_ARGS ",${CONFIG_ETH_QEMU_EXTRA_ARGS}")
+    endif()
+    list(APPEND QEMU_EXTRA_FLAGS
+      -netdev tap,id=n1,script=no,downscript=no,ifname=${CONFIG_ETH_QEMU_IFACE_NAME}${NET_QEMU_ETH_EXTRA_ARGS} -device ${CONFIG_ETH_NIC_MODEL},netdev=n1
+    )
+  elseif(CONFIG_NET_QEMU_USER)
+    list(APPEND QEMU_EXTRA_FLAGS
+      -netdev user,id=n1,${CONFIG_NET_QEMU_USER_EXTRA_ARGS} -device ${CONFIG_ETH_NIC_MODEL},netdev=n1
+    )
+  else()
+    list(APPEND QEMU_EXTRA_FLAGS
+      -net none
+    )
+  endif()
+endif()
+
 if(NOT QEMU_PIPE)
   set(QEMU_PIPE_COMMENT "\nTo exit from QEMU enter: 'CTRL+a, x'\n")
 endif()
