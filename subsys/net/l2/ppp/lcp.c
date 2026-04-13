08830643f20ce4d0a9688d21e963d1cf3a76d588
Nikhil Namjoshi <nikhilnamjoshi@google.com>
2026-04-12 19:16:00 -0700

    net: Add net_pkt_write API return error handling
    There some more instances of net_pkt_write API
    whose return code is not being checked.
    
    Signed-off-by: Nikhil Namjoshi <nikhilnamjoshi@google.com>

--- subsys/net/l2/ppp/lcp.c
+++ subsys/net/l2/ppp/lcp.c
@@ -302,7 +302,13 @@ static void lcp_finished(struct ppp_fsm *fsm)
 
 static int lcp_add_mru(struct ppp_context *ctx, struct net_pkt *pkt)
 {
-	net_pkt_write_u8(pkt, MRU_OPTION_LEN);
+	int ret;
+
+	ret = net_pkt_write_u8(pkt, MRU_OPTION_LEN);
+	if (ret < 0) {
+		return ret;
+	}
+
 	return net_pkt_write_be16(pkt, ctx->lcp.my_options.mru);
 }
 
@@ -360,7 +366,13 @@ static int lcp_nak_mru(struct ppp_context *ctx, struct net_pkt *pkt,
 
 static int lcp_add_async_map(struct ppp_context *ctx, struct net_pkt *pkt)
 {
-	net_pkt_write_u8(pkt, ASYNC_MAP_OPTION_LEN);
+	int ret;
+
+	ret = net_pkt_write_u8(pkt, ASYNC_MAP_OPTION_LEN);
+	if (ret < 0) {
+		return ret;
+	}
+
 	return net_pkt_write_be32(pkt, ctx->lcp.my_options.async_map);
 }
 
