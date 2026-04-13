a743f6c854900120c483a3ffc8e87e3f084625c5
Travis Lam <travis.lam@nordicsemi.no>
2026-04-13 13:36:06 +0200

    driver: flash: nrf_mramc: add write verification
    Physical write block size of mram is 16 for nrf7120. While using
    direct write mode for write. Writing 32-bits word is handled by
    mramc hardware itself.
    
    In nrf7120 is capable to disable busfault when word written is
    corrupted. Data verification is added after write/erase to
    ensure a write is robust.
    
    Signed-off-by: Travis Lam <travis.lam@nordicsemi.no>

--- drivers/flash/soc_flash_nrf_mramc.c
+++ drivers/flash/soc_flash_nrf_mramc.c
@@ -17,25 +17,13 @@ LOG_MODULE_REGISTER(flash_nrf_mramc, CONFIG_FLASH_LOG_LEVEL);
 
 #define DT_DRV_COMPAT nordic_nrf_mramc
 
-#define _ADD_SIZE(node_id) + DT_REG_SIZE(node_id)
-#define _WBS(node_id)	   DT_PROP(node_id, write_block_size),
-#define _EBS(node_id)	   DT_PROP(node_id, erase_block_size),
+#define MRAM_NODE         DT_CHILD(DT_DRV_INST(0), mram_0)
+#define MRAM_SIZE         DT_REG_SIZE(MRAM_NODE)
+#define WRITE_BLOCK_SIZE  DT_PROP(MRAM_NODE, write_block_size)
+#define ERASE_BLOCK_SIZE  DT_PROP(MRAM_NODE, erase_block_size)
 
-#define MRAM_SIZE   (0 DT_INST_FOREACH_CHILD_STATUS_OKAY(0, _ADD_SIZE))
-#define ERASE_VALUE (uint8_t)NRFY_MRAMC_WORD_AFTER_ERASED
-
-/* Use DT_FOREACH_CHILD_STATUS_OKAY of mramc with _FIRST() helper
- * macro to get the write block size and erase block size from
- * the first child node.
- */
-#define WBS_LIST  DT_INST_FOREACH_CHILD_STATUS_OKAY(0, _WBS)
-#define EBS_LIST  DT_INST_FOREACH_CHILD_STATUS_OKAY(0, _EBS)
-
-#define _FIRST_HELPER(first, ...)  first
-#define _FIRST(...)  _FIRST_HELPER(__VA_ARGS__)
-
-#define WRITE_BLOCK_SIZE  _FIRST(WBS_LIST)
-#define ERASE_BLOCK_SIZE  _FIRST(EBS_LIST)
+#define ERASE_VALUE                NRFY_MRAMC_WORD_AFTER_ERASED
+#define MRAMC_MIN_BYTES_DATA_WIDTH NRFY_MRAMC_BYTES_IN_WORD
 
 BUILD_ASSERT((ERASE_BLOCK_SIZE % WRITE_BLOCK_SIZE) == 0,
 		 "erase-block-size expected to be a multiple of write-block-size");
@@ -59,12 +47,48 @@ static bool validate_action(uint32_t addr, size_t len, bool must_align)
 		return false;
 	}
 
-	if (must_align && !(nrfx_is_word_aligned((void const *)addr))) {
+	if (must_align && (!(nrfx_is_word_aligned((void const *)addr)) ||
+		 ((len % MRAMC_MIN_BYTES_DATA_WIDTH) != 0))) {
 		LOG_ERR("Address %x is not word aligned", addr);
 		return false;
 	}
 
 	return true;
+}
+
+/**
+ * @brief Verifies the data written to MRAM.
+ *
+ * @param[in] addr     Address of mram memory.
+ * @param[in] len      Number of bytes for the intended operation.
+ * @param[in] is_erase Whether is verifying erase operation.
+ *
+ * @return true if the address and length are valid, false otherwise.
+ */
+static int nrf_mramc_verify_data(off_t offset, const void *data, size_t size, bool is_erase)
+{
+	uint32_t addr = NRFX_MRAMC_MAP_TO_ADDR(offset);
+	uint32_t num_words = size / MRAMC_MIN_BYTES_DATA_WIDTH;
+
+	const uint32_t *mram = (const uint32_t *)addr;
+	const uint32_t *buf  = (const uint32_t *)data;
+
+	/* Compare and verify written data */
+	for (size_t i = 0; i < num_words; i++) {
+		uint32_t w = mram[i];
+
+		if (is_erase) {
+			if (w != ERASE_VALUE) {
+				return -EIO;
+			}
+		} else {
+			if (w != buf[i]) {
+				return -EIO;
+			}
+		}
+	}
+
+	return 0;
 }
 
 static int nrf_mramc_read(const struct device *dev, off_t offset, void *data, size_t len)
@@ -91,10 +115,11 @@ static int nrf_mramc_read(const struct device *dev, off_t offset, void *data, si
 }
 
 static int nrf_mramc_write(const struct device *dev, off_t offset,
-			 const void *data, size_t len)
+			 const void *data, size_t size)
 {
 	ARG_UNUSED(dev);
 	uint32_t addr = NRFX_MRAMC_MAP_TO_ADDR(offset);
+	uint8_t  retries = CONFIG_NRF_MRAMC_MAX_RETRIES;
 
 	if (data == NULL) {
 		LOG_ERR("Data pointer is NULL");
@@ -102,24 +127,36 @@ static int nrf_mramc_write(const struct device *dev, off_t offset,
 	}
 
 	/* Validate addr and length in the range */
-	if (!validate_action(addr, len, true)) {
+	if (!validate_action(addr, size, true)) {
 		return -EINVAL;
 	}
 
-	LOG_DBG("write: %x:%zu", addr, len);
+	LOG_DBG("write: %x:%zu", addr, size);
 
-	/* Words write function takes second argument as number of write blocks
-	 * and not number of bytes
-	 */
-	nrfx_mramc_words_write(addr, data, len / WRITE_BLOCK_SIZE);
+	while(retries--)
+	{
+		/* Words write function takes second argument as number of write blocks
+		 * and not number of bytes
+		 */
+		nrfx_mramc_words_write(addr, data, size / MRAMC_MIN_BYTES_DATA_WIDTH);
 
-	return 0;
+		if (!nrf_mramc_verify_data(offset, data, size, false))
+		{
+			return 0;
+		}
+		LOG_ERR("MRAM write verification failed at address 0x%x, retrying... (%u retries "
+			"left)",
+			addr, retries);
+	}
+
+	return -EIO;
 }
 
 static int nrf_mramc_erase(const struct device *dev, off_t offset, size_t size)
 {
 	ARG_UNUSED(dev);
 	uint32_t addr = NRFX_MRAMC_MAP_TO_ADDR(offset);
+	uint8_t  retries = CONFIG_NRF_MRAMC_MAX_RETRIES;
 
 	/* Validate addr and length in the range */
 	if (size == 0) {
@@ -133,14 +170,26 @@ static int nrf_mramc_erase(const struct device *dev, off_t offset, size_t size)
 
 	LOG_DBG("erase: %p:%zu", (void *)addr, size);
 
-	/* Erase function takes second argument as number of write blocks
-	 * and not number of bytes
-	 */
-	nrfx_mramc_area_erase(addr, size / WRITE_BLOCK_SIZE);
+	while(retries--)
+	{
+		/* Erase function takes second argument as number of 32-bit words
+		 * and not number of bytes
+		 */
+		nrfx_mramc_erase(addr, size / MRAMC_MIN_BYTES_DATA_WIDTH);
+
 #if defined(CONFIG_SOC_FLASH_NRF_MRAMC_FLUSH_CACHE)
-	sys_cache_instr_invd_all();
+	    sys_cache_instr_invd_all();
 #endif
-	return 0;
+		if (!nrf_mramc_verify_data(offset, NULL, size, true))
+		{
+			return 0;
+		}
+		LOG_ERR("MRAM Erase verification failed at address 0x%x, retrying... (%u retries "
+			"left)",
+			addr, retries);
+	}
+
+	return -EIO;
 }
 
 static int nrf_mramc_get_size(const struct device *dev, uint64_t *size)
@@ -156,7 +205,7 @@ static const struct flash_parameters *nrf_mramc_get_parameters(const struct devi
 
 	static const struct flash_parameters parameters = {
 		.write_block_size = WRITE_BLOCK_SIZE,
-		.erase_value = ERASE_VALUE,
+		.erase_value = (uint8_t) ERASE_VALUE,
 		.caps = {
 			.no_explicit_erase = true,
 		},
