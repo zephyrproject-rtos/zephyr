44c3ea72809c3b03ba1864ebbc29a5fe370be409
Camille BAUD <mail@massdriver.space>
2026-04-14 04:47:44 +0200

    soc: bflb: Imply instead of select, toggle flash offset
    Some elements are not strictly required but heavily recommended
    
    Signed-off-by: Camille BAUD <mail@massdriver.space>

--- soc/bflb/bl61x/soc.c
+++ soc/bflb/bl61x/soc.c
@@ -59,19 +59,13 @@ void system_sysmap_init(void)
 	sys_write32(SYSMAP_ATTR_STRONG_ORDER, (sysmap_base + SYSMAP_FLAGS_OFFSET));
 	sysmap_base += SYSMAP_ENTRY_OFFSET;
 
-	/* 5. flash(2x32M) 0xA0000000~0xA4000000: Weak-Order, Cacheable, Non-Bufferable */
-	sys_write32(BL616_FLASH_XIP_BUSREMAP_END >> SYSMAP_BASE_SHIFT,
+	/* 5. flashes (2x64M) 0xA0000000~0xA8000000: Weak-Order, Cacheable, Non-Bufferable */
+	sys_write32(BL616_FLASH2_XIP_BUSREMAP_END >> SYSMAP_BASE_SHIFT,
 		    (sysmap_base + SYSMAP_ADDR_OFFSET));
 	sys_write32(SYSMAP_ATTR_CACHE_ABLE, (sysmap_base + SYSMAP_FLAGS_OFFSET));
 	sysmap_base += SYSMAP_ENTRY_OFFSET;
 
-	/* 6. empty 0xA2000000~0xA8000000: Strong-Order, Non-Cacheable, Non-Bufferable */
-	sys_write32(BL616_PSRAM_BUSREMAP_BASE >> SYSMAP_BASE_SHIFT,
-		    (sysmap_base + SYSMAP_ADDR_OFFSET));
-	sys_write32(SYSMAP_ATTR_STRONG_ORDER, (sysmap_base + SYSMAP_FLAGS_OFFSET));
-	sysmap_base += SYSMAP_ENTRY_OFFSET;
-
-	/* 7. psram(128M (4M)) 0xA8000000~0xB0000000(0xA8400000):
+	/* 6. psram(128M (4M)) 0xA8000000~0xB0000000(0xA8400000):
 	 * Weak-Order, Cacheable, Bufferable
 	 */
 	sys_write32(BL616_PSRAM_BUSREMAP_END >> SYSMAP_BASE_SHIFT,
@@ -80,7 +74,7 @@ void system_sysmap_init(void)
 		    (sysmap_base + SYSMAP_FLAGS_OFFSET));
 	sysmap_base += SYSMAP_ENTRY_OFFSET;
 
-	/* 8. others: Strong-Order, Non-Cacheable, Non-Bufferable */
+	/* 7. others: Strong-Order, Non-Cacheable, Non-Bufferable */
 	sys_write32(0xFFFFF000U >> SYSMAP_BASE_SHIFT, (sysmap_base + SYSMAP_ADDR_OFFSET));
 	sys_write32(SYSMAP_ATTR_STRONG_ORDER, (sysmap_base + SYSMAP_FLAGS_OFFSET));
 }
