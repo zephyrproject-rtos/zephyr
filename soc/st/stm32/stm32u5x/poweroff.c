8dc3c6f3b09019e6ab8b390e04d3edc93ccafd63
Junqi.ZHANG <565666105@qq.com>
2026-04-15 15:50:12 +0200

    soc: stm32u5: configure RTC Alarm wakeup route in poweroff path
    On STM32U5, waking up from SHUTDOWN using RTC Alarm requires configuring
    the PWR wakeup signal route (WKUP7 <- signal3).
    
    This patch adds that routing setup in the STM32U5 poweroff path, and
    applies it when RTC AlarmA internal wakeup is enabled (ALRAE and ALRAIE set),
    so RTC AlarmA can wake the system from SHUTDOWN.
    
    Validation:
    - Built and functionally validated on STM32U595RIT6.
    - With RTC AlarmA interrupt/wakeup enabled, wakeup from SHUTDOWN works.
    
    Signed-off-by: Junqi Zhang <565666105@qq.com>

--- soc/st/stm32/stm32u5x/poweroff.c
+++ soc/st/stm32/stm32u5x/poweroff.c
@@ -11,7 +11,15 @@
 #include <zephyr/drivers/misc/stm32_wkup_pins/stm32_wkup_pins.h>
 
 #include <stm32_common.h>
+#include <stm32_ll_rtc.h>
 #include <stm32_ll_pwr.h>
+
+#if defined(CONFIG_RTC)
+static bool stm32u5_rtc_alarm_internal_wakeup_enabled(void)
+{
+	return ((RTC->CR & RTC_CR_ALRAE) != 0U) && ((RTC->CR & RTC_CR_ALRAIE) != 0U);
+}
+#endif
 
 void z_sys_poweroff(void)
 {
@@ -19,6 +27,14 @@ void z_sys_poweroff(void)
 	stm32_pwr_wkup_pin_cfg_pupd();
 #endif /* CONFIG_STM32_WKUP_PINS */
 
+#if defined(CONFIG_SOC_SERIES_STM32U5X) && defined(CONFIG_RTC)
+	if (stm32u5_rtc_alarm_internal_wakeup_enabled()) {
+		LL_PWR_EnableWakeUpPin(LL_PWR_WAKEUP_PIN7);
+		LL_PWR_SetWakeUpPinSignal3Selection(LL_PWR_WAKEUP_PIN7);
+	}
+#endif
+
+	LL_PWR_ClearFlag_SB();
 	LL_PWR_ClearFlag_WU();
 	LL_PWR_SetPowerMode(LL_PWR_SHUTDOWN_MODE);
 
