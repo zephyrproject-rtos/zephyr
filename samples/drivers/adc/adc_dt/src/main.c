zephyrproject-rtos/zephyr

commit 17bc03c31c87a1d84f82e28cca3af8c96ee85306

samples: drivers: adc: Increase sample buffer size
Modifies the adc_dt sample to increase the sample buffer size to support
ADCs with more than 16 bits of resolution, such as ad7124.

Signed-off-by: Maureen Helm <maureen.helm@analog.com>

samples/drivers/adc/adc_dt/src/main.c
@@ -33,7 +33,7 @@ int main(void)
 {
 	int err;
 	uint32_t count = 0;
-	uint16_t buf;
+	uint32_t buf;
 	struct adc_sequence sequence = {
 		.buffer = &buf,
 		/* buffer size in bytes, not number of samples */

commit f0b862e69d737c4637a8df47a1b9ef8de0421f9e

samples: drivers: adc_dt: fixing buffer initialization bug
Fixes a bug introduced in commit
17bc03c31c87a1d84f82e28cca3af8c96ee85306 in which drivers using 16-bit
buffers never initialize the upper 16-bits of the 32-bit variable.
Solution is to zero the buffer before use.

Signed-off-by: John Batch <john.batch@infineon.com>

samples/drivers/adc/adc_dt/src/main.c
@@ -34,7 +34,7 @@ int main(void)
 {
 	int err;
 	uint32_t count = 0;
-	uint32_t buf;
+	uint32_t buf = 0;
 	struct adc_sequence sequence = {
 		.buffer = &buf,
 		/* buffer size in bytes, not number of samples */
@@ -66,6 +66,12 @@ int main(void)
 		printk("ADC reading[%u]:\n", count++);
 		for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
 			int32_t val_mv;
+
+			/*
+			 * Clear buffer before reading.  This ensures the upper 16-bits will be zero
+			 * when the adc uses a 16-bit buffer size.
+			 */
+			buf = 0;
 
 			printk("- %s, channel %d: ",
 			       adc_channels[i].dev->name,