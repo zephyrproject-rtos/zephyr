5620fa99065c4a45680a09ba50f0d7eaf48fc0a8
Akansh Sinha <akansh.sinha.dev@gmail.com>
2026-04-19 11:59:36 +0530

    drivers: i2c: dw: add support for SDA hold time
    This PR introduces the `sda-hold-time-ns` DeviceTree property for
    DesignWare I2C controllers. The driver logic is updated to prioritize
    this nanosecond configuration, calculating the necessary hardware clock
    ticks at build time using the new `HOLD_TIME_TO_TICKS` macro. If the
    property is not defined, it safely falls back to the legacy
    `sda-hold-tx` tick configuration.
    
    Fixes #83437
    
    Signed-off-by: Akansh Sinha <akansh.sinha.dev@gmail.com>

--- drivers/i2c/i2c_dw.h
+++ drivers/i2c/i2c_dw.h
@@ -140,6 +140,11 @@ typedef int (*i2c_api_check_bus_t)(const struct device *dev);
 
 #define SDA_HOLD_INVALID UINT32_MAX
 
+/* convert sda hold time in nanoseconds to DW I2C clock ticks at build time */
+#define HOLD_TIME_TO_TICKS(i2c_sda_hold_time_ns)                                                \
+	   ((uint32_t)DIV_ROUND_UP((uint64_t)(CONFIG_I2C_DW_CLOCK_SPEED) * (i2c_sda_hold_time_ns), \
+							   1000000000ULL))
+
 struct i2c_dw_rom_config {
 	DEVICE_MMIO_ROM;
 	i2c_isr_cb_t config_func;
