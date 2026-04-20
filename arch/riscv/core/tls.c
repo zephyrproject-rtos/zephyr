72480c77f7373779f32247a15e0805d09b7d56cc
Mayur Salve <msalve@qti.qualcomm.com>
2026-04-20 14:54:21 -0700

    arch: riscv: address review nits on TLS canary patches
    Add copyright year, reflow comments, remote duplicate __tls_end,
    and remove commented-out code.
    
    Signed-off-by: Mayur Salve <msalve@qti.qualcomm.com>

--- arch/riscv/core/tls.c
+++ arch/riscv/core/tls.c
@@ -21,20 +21,19 @@ size_t FUNC_NO_STACK_PROTECTOR z_tls_data_size_asm(void)
 size_t arch_tls_stack_setup(struct k_thread *new_thread, char *stack_ptr)
 {
 	/*
-	 * TLS area for RISC-V is simple without any extra
-	 * data.
+	 * TLS area for RISC-V is simple without any extra data.
 	 */
 
 	/*
-	 * Since we are populating things backwards,
-	 * setup the TLS data/bss area first.
+	 * Since we are populating things backwards, setup the TLS data/bss
+	 * area first.
 	 */
 	stack_ptr -= z_tls_data_size();
 	z_tls_copy(stack_ptr);
 
 	/*
-	 * Set thread TLS pointer which is used in
-	 * context switch to point to TLS area.
+	 * Set thread TLS pointer which is used in context switch to point
+	 * to TLS area.
 	 */
 	new_thread->tls = POINTER_TO_UINT(stack_ptr);
 
@@ -44,22 +43,20 @@ size_t arch_tls_stack_setup(struct k_thread *new_thread, char *stack_ptr)
 void FUNC_NO_STACK_PROTECTOR arch_riscv_early_tls_stack_update(char *stack_ptr, size_t tls_size)
 {
 	/*
-	 * TLS area for RISC-V is simple without any extra
-	 * data.
+	 * TLS area for RISC-V is simple without any extra data.
 	 */
 
 	/*
-	 * Since we are populating things backwards,
-	 * setup the TLS data/bss area first.
-	 * tls_size was already computed by the caller (z_tls_data_size_asm)
-	 * to avoid a redundant call.
+	 * Since we are populating things backwards, setup the TLS data/bss
+	 * area first. tls_size was already computed by the caller
+	 * (z_tls_data_size_asm) to avoid a redundant call.
 	 */
 	stack_ptr -= tls_size;
 	z_tls_copy(stack_ptr);
 
 	/*
-	 * Set tp directly to point to the TLS area,
-	 * since there are no threads spawned early in boot.
+	 * Set tp directly to point to the TLS area, since there are no
+	 * threads spawned early in boot.
 	 */
 	uintptr_t tls_addr = POINTER_TO_UINT(stack_ptr);
 
