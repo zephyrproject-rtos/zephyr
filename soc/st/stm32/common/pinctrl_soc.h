8e311dd5e04391d4ac3ff1e486f06fa5aba3b0da
Mathieu Choplain <mathieu.choplain-ext@st.com>
2026-04-21 17:09:45 +0200

    drivers: pinctrl: stm32: use CMSIS-like defines for bit definitions
    Use `_Pos`/`_Msk` suffixed defines for pinctrl format bits definitions and
    declare `_Msk` already shifted. This is aligned with the CMSIS definitions
    format which shorter, easier to understand and enables use of
    macros _FLD2VAL/_VAL2FLD.
    
    Update all code using these defines accordingly.
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain-ext@st.com>

--- soc/st/stm32/common/pinctrl_soc.h
+++ soc/st/stm32/common/pinctrl_soc.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2020 Linaro Ltd.
  * Copyright (c) 2021 Nordic Semiconductor ASA
+ * Copyright (c) 2026 STMicroelectronics
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -78,47 +79,47 @@ typedef struct pinctrl_soc_pin {
  */
 
 /* Port Mode */
-#define STM32_MODE_INPUT		(0x0 << STM32_MODE_INOUT_SHIFT)
-#define STM32_MODE_OUTPUT		(0x1 << STM32_MODE_INOUT_SHIFT)
-#define STM32_MODE_INOUT_MASK		0x1
-#define STM32_MODE_INOUT_SHIFT		0
+#define STM32_MODE_INOUT_Pos		0
+#define STM32_MODE_INOUT_Msk		(0x1 << STM32_MODE_INOUT_Pos)
+#define STM32_MODE_INPUT		(0x0 << STM32_MODE_INOUT_Pos)
+#define STM32_MODE_OUTPUT		(0x1 << STM32_MODE_INOUT_Pos)
 
 /* Input Port configuration */
-#define STM32_CNF_IN_ANALOG		(0x0 << STM32_CNF_IN_SHIFT)
-#define STM32_CNF_IN_FLOAT		(0x1 << STM32_CNF_IN_SHIFT)
-#define STM32_CNF_IN_PUPD		(0x2 << STM32_CNF_IN_SHIFT)
-#define STM32_CNF_IN_MASK		0x3
-#define STM32_CNF_IN_SHIFT		1
+#define STM32_CNF_IN_Pos		1
+#define STM32_CNF_IN_Msk		(0x3 << STM32_CNF_IN_Pos)
+#define STM32_CNF_IN_ANALOG		(0x0 << STM32_CNF_IN_Pos)
+#define STM32_CNF_IN_FLOAT		(0x1 << STM32_CNF_IN_Pos)
+#define STM32_CNF_IN_PUPD		(0x2 << STM32_CNF_IN_Pos)
 
 /* Output Port configuration */
-#define STM32_MODE_OUTPUT_MAX_10	(0x0 << STM32_MODE_OSPEED_SHIFT)
-#define STM32_MODE_OUTPUT_MAX_2		(0x1 << STM32_MODE_OSPEED_SHIFT)
-#define STM32_MODE_OUTPUT_MAX_50	(0x2 << STM32_MODE_OSPEED_SHIFT)
-#define STM32_MODE_OSPEED_MASK		0x3
-#define STM32_MODE_OSPEED_SHIFT		3
+#define STM32_MODE_OSPEED_Pos		3
+#define STM32_MODE_OSPEED_Msk		(0x3 << STM32_MODE_OSPEED_Pos)
+#define STM32_MODE_OUTPUT_MAX_10	(0x0 << STM32_MODE_OSPEED_Pos)
+#define STM32_MODE_OUTPUT_MAX_2		(0x1 << STM32_MODE_OSPEED_Pos)
+#define STM32_MODE_OUTPUT_MAX_50	(0x2 << STM32_MODE_OSPEED_Pos)
 
-#define STM32_CNF_PUSH_PULL		(0x0 << STM32_CNF_OUT_0_SHIFT)
-#define STM32_CNF_OPEN_DRAIN		(0x1 << STM32_CNF_OUT_0_SHIFT)
-#define STM32_CNF_OUT_0_MASK		0x1
-#define STM32_CNF_OUT_0_SHIFT		5
+#define STM32_CNF_OUT_0_Pos		5
+#define STM32_CNF_OUT_0_Msk		(0x1 << STM32_CNF_OUT_0_Pos)
+#define STM32_CNF_PUSH_PULL		(0x0 << STM32_CNF_OUT_0_Pos)
+#define STM32_CNF_OPEN_DRAIN		(0x1 << STM32_CNF_OUT_0_Pos)
 
-#define STM32_CNF_GP_OUTPUT		(0x0 << STM32_CNF_OUT_1_SHIFT)
-#define STM32_CNF_ALT_FUNC		(0x1 << STM32_CNF_OUT_1_SHIFT)
-#define STM32_CNF_OUT_1_MASK		0x1
-#define STM32_CNF_OUT_1_SHIFT		6
+#define STM32_CNF_OUT_1_Pos		6
+#define STM32_CNF_OUT_1_Msk		(0x1 << STM32_CNF_OUT_1_Pos)
+#define STM32_CNF_GP_OUTPUT		(0x0 << STM32_CNF_OUT_1_Pos)
+#define STM32_CNF_ALT_FUNC		(0x1 << STM32_CNF_OUT_1_Pos)
 
 /* GPIO High impedance/Pull-up/Pull-down */
-#define STM32_PUPD_NO_PULL		(0x0 << STM32_PUPD_SHIFT)
-#define STM32_PUPD_PULL_UP		(0x1 << STM32_PUPD_SHIFT)
-#define STM32_PUPD_PULL_DOWN		(0x2 << STM32_PUPD_SHIFT)
-#define STM32_PUPD_MASK			0x3
-#define STM32_PUPD_SHIFT		7
+#define STM32_PUPD_Pos			7
+#define STM32_PUPD_Msk			(0x3 << STM32_PUPD_Pos)
+#define STM32_PUPD_NO_PULL		(0x0 << STM32_PUPD_Pos)
+#define STM32_PUPD_PULL_UP		(0x1 << STM32_PUPD_Pos)
+#define STM32_PUPD_PULL_DOWN		(0x2 << STM32_PUPD_Pos)
 
 /* GPIO plain output value */
-#define STM32_ODR_0			(0x0 << STM32_ODR_SHIFT)
-#define STM32_ODR_1			(0x1 << STM32_ODR_SHIFT)
-#define STM32_ODR_MASK			0x1
-#define STM32_ODR_SHIFT			9
+#define STM32_ODR_Pos			9
+#define STM32_ODR_Msk			(0x1 << STM32_ODR_Pos)
+#define STM32_ODR_0			(0x0 << STM32_ODR_Pos)
+#define STM32_ODR_1			(0x1 << STM32_ODR_Pos)
 
 /**
  * @brief Utility macro to initialize pincfg field in #pinctrl_pin_t (F1).
@@ -126,14 +127,14 @@ typedef struct pinctrl_soc_pin {
  * @param node_id Node identifier.
  */
 #define Z_PINCTRL_STM32_PINCFG_INIT(node_id)				       \
-	(((STM32_NO_PULL * DT_PROP(node_id, bias_disable)) << STM32_PUPD_SHIFT) | \
-	 ((STM32_PULL_UP * DT_PROP(node_id, bias_pull_up)) << STM32_PUPD_SHIFT) | \
-	 ((STM32_PULL_DOWN * DT_PROP(node_id, bias_pull_down)) << STM32_PUPD_SHIFT) | \
-	 ((STM32_PUSH_PULL * DT_PROP(node_id, drive_push_pull)) << STM32_CNF_OUT_0_SHIFT) | \
-	 ((STM32_OPEN_DRAIN * DT_PROP(node_id, drive_open_drain)) << STM32_CNF_OUT_0_SHIFT) | \
-	 ((STM32_OUTPUT_LOW * DT_PROP(node_id, output_low)) << STM32_ODR_SHIFT) | \
-	 ((STM32_OUTPUT_HIGH * DT_PROP(node_id, output_high)) << STM32_ODR_SHIFT) | \
-	 (DT_ENUM_IDX(node_id, slew_rate) << STM32_MODE_OSPEED_SHIFT))
+	(((STM32_NO_PULL * DT_PROP(node_id, bias_disable)) << STM32_PUPD_Pos) | \
+	 ((STM32_PULL_UP * DT_PROP(node_id, bias_pull_up)) << STM32_PUPD_Pos) | \
+	 ((STM32_PULL_DOWN * DT_PROP(node_id, bias_pull_down)) << STM32_PUPD_Pos) | \
+	 ((STM32_PUSH_PULL * DT_PROP(node_id, drive_push_pull)) << STM32_CNF_OUT_0_Pos) | \
+	 ((STM32_OPEN_DRAIN * DT_PROP(node_id, drive_open_drain)) << STM32_CNF_OUT_0_Pos) | \
+	 ((STM32_OUTPUT_LOW * DT_PROP(node_id, output_low)) << STM32_ODR_Pos) | \
+	 ((STM32_OUTPUT_HIGH * DT_PROP(node_id, output_high)) << STM32_ODR_Pos) | \
+	 (DT_ENUM_IDX(node_id, slew_rate) << STM32_MODE_OSPEED_Pos))
 #else
 
 /**
@@ -161,57 +162,57 @@ typedef struct pinctrl_soc_pin {
  */
 
 /* GPIO Mode */
-#define STM32_MODER_INPUT_MODE		(0x0 << STM32_MODER_SHIFT)
-#define STM32_MODER_OUTPUT_MODE		(0x1 << STM32_MODER_SHIFT)
-#define STM32_MODER_ALT_MODE		(0x2 << STM32_MODER_SHIFT)
-#define STM32_MODER_ANALOG_MODE		(0x3 << STM32_MODER_SHIFT)
-#define STM32_MODER_MASK		0x3
-#define STM32_MODER_SHIFT		4
+#define STM32_MODER_Pos			4
+#define STM32_MODER_Msk			(0x3 << STM32_MODER_Pos)
+#define STM32_MODER_INPUT_MODE		(0x0 << STM32_MODER_Pos)
+#define STM32_MODER_OUTPUT_MODE		(0x1 << STM32_MODER_Pos)
+#define STM32_MODER_ALT_MODE		(0x2 << STM32_MODER_Pos)
+#define STM32_MODER_ANALOG_MODE		(0x3 << STM32_MODER_Pos)
 
 /* GPIO Output type */
-#define STM32_OTYPER_PUSH_PULL		(0x0 << STM32_OTYPER_SHIFT)
-#define STM32_OTYPER_OPEN_DRAIN		(0x1 << STM32_OTYPER_SHIFT)
-#define STM32_OTYPER_MASK		0x1
-#define STM32_OTYPER_SHIFT		6
+#define STM32_OTYPER_Pos		6
+#define STM32_OTYPER_Msk		(0x1 << STM32_OTYPER_Pos)
+#define STM32_OTYPER_PUSH_PULL		(0x0 << STM32_OTYPER_Pos)
+#define STM32_OTYPER_OPEN_DRAIN		(0x1 << STM32_OTYPER_Pos)
 
 /* GPIO speed */
-#define STM32_OSPEEDR_LOW_SPEED		(0x0 << STM32_OSPEEDR_SHIFT)
-#define STM32_OSPEEDR_MEDIUM_SPEED	(0x1 << STM32_OSPEEDR_SHIFT)
-#define STM32_OSPEEDR_HIGH_SPEED	(0x2 << STM32_OSPEEDR_SHIFT)
-#define STM32_OSPEEDR_VERY_HIGH_SPEED	(0x3 << STM32_OSPEEDR_SHIFT)
-#define STM32_OSPEEDR_MASK		0x3
-#define STM32_OSPEEDR_SHIFT		7
+#define STM32_OSPEEDR_Pos		7
+#define STM32_OSPEEDR_Msk		(0x3 << STM32_OSPEEDR_Pos)
+#define STM32_OSPEEDR_LOW_SPEED		(0x0 << STM32_OSPEEDR_Pos)
+#define STM32_OSPEEDR_MEDIUM_SPEED	(0x1 << STM32_OSPEEDR_Pos)
+#define STM32_OSPEEDR_HIGH_SPEED	(0x2 << STM32_OSPEEDR_Pos)
+#define STM32_OSPEEDR_VERY_HIGH_SPEED	(0x3 << STM32_OSPEEDR_Pos)
 
 /* GPIO High impedance/Pull-up/pull-down */
-#define STM32_PUPDR_NO_PULL		(0x0 << STM32_PUPDR_SHIFT)
-#define STM32_PUPDR_PULL_UP		(0x1 << STM32_PUPDR_SHIFT)
-#define STM32_PUPDR_PULL_DOWN		(0x2 << STM32_PUPDR_SHIFT)
-#define STM32_PUPDR_MASK		0x3
-#define STM32_PUPDR_SHIFT		9
+#define STM32_PUPDR_Pos			9
+#define STM32_PUPDR_Msk			(0x3 << STM32_PUPDR_Pos)
+#define STM32_PUPDR_NO_PULL		(0x0 << STM32_PUPDR_Pos)
+#define STM32_PUPDR_PULL_UP		(0x1 << STM32_PUPDR_Pos)
+#define STM32_PUPDR_PULL_DOWN		(0x2 << STM32_PUPDR_Pos)
 
 /* GPIO plain output value */
-#define STM32_ODR_0			(0x0 << STM32_ODR_SHIFT)
-#define STM32_ODR_1			(0x1 << STM32_ODR_SHIFT)
-#define STM32_ODR_MASK			0x1
-#define STM32_ODR_SHIFT			11
+#define STM32_ODR_Pos			11
+#define STM32_ODR_Msk			(0x1 << STM32_ODR_Pos)
+#define STM32_ODR_0			(0x0 << STM32_ODR_Pos)
+#define STM32_ODR_1			(0x1 << STM32_ODR_Pos)
 
 /* I/O delay length (DELAYR) */
-#define STM32_IODELAY_LENGTH_MASK	0xFU
-#define STM32_IODELAY_LENGTH_SHIFT	12
+#define STM32_IODELAY_LENGTH_Pos	12
+#define STM32_IODELAY_LENGTH_Msk	(0xFU << STM32_IODELAY_LENGTH_Pos)
 
 /* I/O delay & retime configuration (ADVCFGR) */
-#define STM32_IORETIME_ADVCFGR_MASK	0xFU
-#define STM32_IORETIME_ADVCFGR_SHIFT	16
+#define STM32_IORETIME_ADVCFGR_Pos	16
+#define STM32_IORETIME_ADVCFGR_Msk	(0xFU << STM32_IORETIME_ADVCFGR_Pos)
 
-#define STM32_IODELAY_DIRECTION_SHIFT	STM32_IORETIME_ADVCFGR_SHIFT
-#define STM32_IORETIME_EDGE_SHIFT	17
-#define STM32_IORETIME_ENABLE_SHIFT	19
+#define STM32_IODELAY_DIRECTION_Pos	STM32_IORETIME_ADVCFGR_Pos
+#define STM32_IORETIME_EDGE_Pos		17
+#define STM32_IORETIME_ENABLE_Pos	19
 
 /* High-speed low-voltage mode (HSLVR) */
-#define STM32_HSLVR_DISABLE		(0x0 << STM32_HSLVR_SHIFT)
-#define STM32_HSLVR_ENABLED		(0x1 << STM32_HSLVR_SHIFT)
-#define STM32_HSLVR_MASK		0x1
-#define STM32_HSLVR_SHIFT		12
+#define STM32_HSLVR_Pos			12
+#define STM32_HSLVR_Msk			(0x1 << STM32_HSLVR_Pos)
+#define STM32_HSLVR_DISABLE		(0x0 << STM32_HSLVR_Pos)
+#define STM32_HSLVR_ENABLED		(0x1 << STM32_HSLVR_Pos)
 
 /**
  * @brief Definitions for the various fields related to I/O synchronization
@@ -233,10 +234,10 @@ typedef struct pinctrl_soc_pin {
 #if DT_HAS_COMPAT_STATUS_OKAY(st_stm32n6_pinctrl)
 /* Inner helper macro for Z_PINCTRL_STM32_IOSYNC_INIT */
 #define Z_PINCTRL_STM32_IOSYNC_INIT_INNER(delay_path, retime_edge, retime_enable, delay_ps)	\
-	((CONCAT(STM32_IOSYNC_DELAY_DIRECTION_, delay_path) << STM32_IODELAY_DIRECTION_SHIFT) | \
-	 ((retime_enable) << STM32_IORETIME_ENABLE_SHIFT) |					\
-	 (CONCAT(STM32_IOSYNC_RETIME_EDGE_, retime_edge) << STM32_IORETIME_EDGE_SHIFT) |	\
-	 (CONCAT(LL_GPIO_DELAY_, delay_ps) << STM32_IODELAY_LENGTH_SHIFT))
+	((CONCAT(STM32_IOSYNC_DELAY_DIRECTION_, delay_path) << STM32_IODELAY_DIRECTION_Pos) | \
+	 ((retime_enable) << STM32_IORETIME_ENABLE_Pos) |					\
+	 (CONCAT(STM32_IOSYNC_RETIME_EDGE_, retime_edge) << STM32_IORETIME_EDGE_Pos) |	\
+	 (CONCAT(LL_GPIO_DELAY_, delay_ps) << STM32_IODELAY_LENGTH_Pos))
 
 /**
  * @brief Utility macro to initialize fields of @ref{pinctrl_pin_t}.pincfg
@@ -273,16 +274,16 @@ typedef struct pinctrl_soc_pin {
  * @param node_id Node identifier.
  */
 #define Z_PINCTRL_STM32_PINCFG_INIT(node_id)				       \
-	(((STM32_NO_PULL * DT_PROP(node_id, bias_disable)) << STM32_PUPDR_SHIFT) | \
-	 ((STM32_PULL_UP * DT_PROP(node_id, bias_pull_up)) << STM32_PUPDR_SHIFT) | \
-	 ((STM32_PULL_DOWN * DT_PROP(node_id, bias_pull_down)) << STM32_PUPDR_SHIFT) | \
-	 ((STM32_PUSH_PULL * DT_PROP(node_id, drive_push_pull)) << STM32_OTYPER_SHIFT) | \
-	 ((STM32_OPEN_DRAIN * DT_PROP(node_id, drive_open_drain)) << STM32_OTYPER_SHIFT) | \
-	 ((STM32_OUTPUT_LOW * DT_PROP(node_id, output_low)) << STM32_ODR_SHIFT) | \
-	 ((STM32_OUTPUT_HIGH * DT_PROP(node_id, output_high)) << STM32_ODR_SHIFT) | \
-	 ((STM32_GPIO_OUTPUT * DT_PROP(node_id, output_low)) << STM32_MODER_SHIFT) | \
-	 ((STM32_GPIO_OUTPUT * DT_PROP(node_id, output_high)) << STM32_MODER_SHIFT) | \
-	 (DT_ENUM_IDX(node_id, slew_rate) << STM32_OSPEEDR_SHIFT) | \
+	(((STM32_NO_PULL * DT_PROP(node_id, bias_disable)) << STM32_PUPDR_Pos) | \
+	 ((STM32_PULL_UP * DT_PROP(node_id, bias_pull_up)) << STM32_PUPDR_Pos) | \
+	 ((STM32_PULL_DOWN * DT_PROP(node_id, bias_pull_down)) << STM32_PUPDR_Pos) | \
+	 ((STM32_PUSH_PULL * DT_PROP(node_id, drive_push_pull)) << STM32_OTYPER_Pos) | \
+	 ((STM32_OPEN_DRAIN * DT_PROP(node_id, drive_open_drain)) << STM32_OTYPER_Pos) | \
+	 ((STM32_OUTPUT_LOW * DT_PROP(node_id, output_low)) << STM32_ODR_Pos) | \
+	 ((STM32_OUTPUT_HIGH * DT_PROP(node_id, output_high)) << STM32_ODR_Pos) | \
+	 ((STM32_GPIO_OUTPUT * DT_PROP(node_id, output_low)) << STM32_MODER_Pos) | \
+	 ((STM32_GPIO_OUTPUT * DT_PROP(node_id, output_high)) << STM32_MODER_Pos) | \
+	 (DT_ENUM_IDX(node_id, slew_rate) << STM32_OSPEEDR_Pos) | \
 	 (Z_PINCTRL_STM32_IOSYNC_INIT(node_id)) | \
 	 (Z_PINCTRL_STM32_HSLV_INIT(node_id)))
 #endif /* CONFIG_SOC_SERIES_STM32F1X */
