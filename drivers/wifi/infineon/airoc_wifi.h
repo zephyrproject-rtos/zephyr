42974dddc2531ee95b1c180c8d271bf7f89f384f
Ramprasad Kannappan <Ramprasad.Kannappan@infineon.com>
2026-04-21 15:51:40 +0530

    drivers: wifi: infineon: wpa_supplicant support integration
    In this PR, wpa_supp interfacing to the infineon's wifi driver
    
    Signed-off-by: Ramprasad Kannappan <Ramprasad.Kannappan@infineon.com>

--- drivers/wifi/infineon/airoc_wifi.h
+++ drivers/wifi/infineon/airoc_wifi.h
@@ -19,6 +19,16 @@
 #endif
 
 #include <cy_utils.h>
+#ifdef CONFIG_WIFI_NM_WPA_SUPPLICANT
+#include <driver.h>
+#include <driver_zephyr.h>
+#include <whd_chip_constants.h>
+#include <whd_utils.h>
+#include <whd_wlioctl.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/wifi_nm.h>
+#endif
 
 #define DT_DRV_COMPAT infineon_airoc_wifi
 
@@ -30,10 +40,22 @@
 #endif
 
 #if defined(CONFIG_AIROC_WIFI_BUS_SPI)
-#define AIROC_WIFI_SPI_OPERATION (SPI_WORD_SET(DT_PROP_OR(DT_DRV_INST(0), spi_word_size, 8)) \
-			| (DT_PROP(DT_DRV_INST(0), spi_half_duplex) \
-				? SPI_HALF_DUPLEX : SPI_FULL_DUPLEX) \
-			| SPI_TRANSFER_MSB)
+#define AIROC_WIFI_SPI_OPERATION                                                                   \
+	(SPI_WORD_SET(DT_PROP_OR(DT_DRV_INST(0), spi_word_size, 8)) |                              \
+	 (DT_PROP(DT_DRV_INST(0), spi_half_duplex) ? SPI_HALF_DUPLEX : SPI_FULL_DUPLEX) |          \
+	 SPI_TRANSFER_MSB)
+#endif
+
+#ifdef CONFIG_WIFI_NM_WPA_SUPPLICANT
+/* Securty Settings (Associate) */
+typedef struct whd_wpa_assoc_security {
+	unsigned int wpa_version;
+	unsigned int auth_alg;
+	unsigned int pairwise_suite;
+	unsigned int group_suite;
+	unsigned int key_mgmt_suite;
+	uint32_t auth_assoc_res_status;
+} whd_wpa_assoc_security_t;
 #endif
 
 struct airoc_wifi_data {
@@ -61,6 +83,26 @@ struct airoc_wifi_data {
 	whd_driver_t whd_drv;
 	struct gpio_callback host_oob_pin_cb;
 	uint8_t frame_buf[NET_ETH_MAX_FRAME_SIZE];
+#if defined(CONFIG_WIFI_NM_WPA_SUPPLICANT)
+	wl_bss_info_t scan_bss_info;
+	void *supp_if_ctx;
+	void *hapd_if_ctx;
+	struct zep_wpa_supp_dev_callbk_fns airoc_wifi_wpa_supp_dev_callbk_fns;
+	struct wpa_scan_results supp_scan_res;
+	struct wpa_driver_set_key_params *key_params;
+	struct wpa_driver_capa capa;
+
+	whd_wpa_assoc_security_t assoc_sec;
+	whd_assoc_info_t assoc_info;
+	whd_auth_req_status_t sae_auth_req_info;
+	uint16_t chanspec;
+	uint16_t assoc_chanspec;
+	uint8_t *mgmt_tx_data;
+	size_t mgmt_tx_data_len;
+	uint8_t bssid[6];
+	int mode;
+	uint32_t key_mgmt_suite;
+#endif
 };
 
 union airoc_wifi_bus {
