6a3794b65baed3376203d0a1fafe896cb4ae178e
Qingling Wu <qingling.wu@nxp.com>
2026-04-21 19:49:55 +0800

    drivers: wifi: nxp: fix build when WMM_UAPSD is disabled
    Add #ifdef guards around WMM_UAPSD specific API calls in power save
    functions to fix compilation errors when CONFIG_NXP_WIFI_WMM_UAPSD
    is disabled.
    
    Signed-off-by: Qingling Wu <qingling.wu@nxp.com>

--- drivers/wifi/nxp/nxp_wifi_drv.c
+++ drivers/wifi/nxp/nxp_wifi_drv.c
@@ -1716,6 +1716,7 @@ static int nxp_wifi_power_save(const struct device *dev, struct wifi_ps_params *
 				wlan_configure_listen_interval(0);
 			}
 			break;
+#ifdef CONFIG_NXP_WIFI_WMM_UAPSD
 		case WIFI_PS_PARAM_MODE:
 			if (params->mode == WIFI_PS_MODE_WMM) {
 				ret = wlan_set_wmm_uapsd(1);
@@ -1730,6 +1731,7 @@ static int nxp_wifi_power_save(const struct device *dev, struct wifi_ps_params *
 					status = NXP_WIFI_RET_FAIL;
 				}
 			}
+#endif
 			break;
 		case WIFI_PS_PARAM_TIMEOUT:
 			wlan_configure_delay_to_ps((int)params->timeout_ms);
@@ -1779,11 +1781,15 @@ int nxp_wifi_get_power_save(const struct device *dev, struct wifi_ps_config *con
 				config->ps_params.wakeup_mode = WIFI_PS_WAKEUP_MODE_DTIM;
 			}
 
+#ifdef CONFIG_NXP_WIFI_WMM_UAPSD
 			if (wlan_is_wmm_uapsd_enabled()) {
 				config->ps_params.mode = WIFI_PS_MODE_WMM;
 			} else {
 				config->ps_params.mode = WIFI_PS_MODE_LEGACY;
 			}
+#else
+			config->ps_params.mode = WIFI_PS_MODE_LEGACY;
+#endif
 		} else {
 			status = NXP_WIFI_RET_FAIL;
 		}
