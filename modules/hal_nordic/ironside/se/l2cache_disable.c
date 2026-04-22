5da3997fa9366fe146d53939edc9cec06ea4a8af
Karsten Koenig <karsten.koenig@nordicsemi.no>
2026-04-22 16:42:26 +0200

    modules: hal_nordic: ironside: Option to disable l2cache
    The L2CACHE on the nrf54h20 can be disabled by adding a corresponding
    entry into the PERIPHCONF. Added a build option to make this
    configurable in the project configuration.
    
    Signed-off-by: Karsten Koenig <karsten.koenig@nordicsemi.no>

---
+++ modules/hal_nordic/ironside/se/l2cache_disable.c
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2026 Nordic Semiconductor ASA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ironside_zephyr/se/uicr_periphconf.h>
+
+UICR_PERIPHCONF_ENTRY(PERIPHCONF_L2CACHE_ENABLE(false));
