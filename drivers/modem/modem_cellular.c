36d5f4998bd30a189a2e12b26348e38905031ba9
Jordan Yates <jordan@embeint.com>
2026-04-22 11:46:55 +1000

    modem: cellular: move carrier state to `modem_ppp`
    Move the interface carrier state out of `modem_cellular` into
    `modem_ppp`, as the PPP module is the true "carrier" for the PPP
    networking interface.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- drivers/modem/modem_cellular.c
+++ drivers/modem/modem_cellular.c
@@ -1876,7 +1876,6 @@ static void modem_cellular_await_registered_event_handler(struct modem_cellular_
 		modem_cellular_start_timer(data, MODEM_CELLULAR_PERIODIC_SCRIPT_TIMEOUT);
 		break;
 	case MODEM_CELLULAR_EVENT_SUSPEND:
-		net_if_carrier_off(modem_ppp_get_iface(config->ppp));
 		modem_cellular_enter_state(data, MODEM_CELLULAR_STATE_INIT_POWER_OFF);
 		break;
 	case MODEM_CELLULAR_EVENT_RING:
@@ -1979,7 +1978,6 @@ static void modem_cellular_registered_event_handler(struct modem_cellular_data *
 		modem_cellular_start_timer(data, MODEM_CELLULAR_PERIODIC_SCRIPT_TIMEOUT);
 		break;
 	case MODEM_CELLULAR_EVENT_SUSPEND:
-		net_if_carrier_off(modem_ppp_get_iface(config->ppp));
 		modem_chat_release(&data->chat);
 		modem_ppp_release(config->ppp);
 		modem_cellular_enter_state(data, MODEM_CELLULAR_STATE_INIT_POWER_OFF);
@@ -2043,7 +2041,6 @@ static int modem_cellular_on_await_ppp_dead_state_leave(struct modem_cellular_da
 {
 	const struct modem_cellular_config *config = data->dev->config;
 
-	net_if_carrier_off(modem_ppp_get_iface(config->ppp));
 	modem_chat_release(&data->chat);
 	modem_ppp_release(config->ppp);
 
