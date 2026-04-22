05a96f27ee294891e5967c9a346b9efd7aac91e7
Etienne Carriere <etienne.carriere@st.com>
2026-04-22 17:11:01 +0200

    boards: st: nucleo_wba65ri: start address not needed on 'ns' variant
    Remove computation of the target start address for nucleo_wba65ri
    'ns' variant to have the target starting execution after flash
    is programmed since now stm32cubeprogrammer runner issues a system
    reset after flash (STM32_Programmer_CLI argument 'rst').
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- boards/st/nucleo_wba65ri/board.cmake
+++ boards/st/nucleo_wba65ri/board.cmake
@@ -5,26 +5,16 @@ if(CONFIG_BUILD_WITH_TFM)
   # Flash merged TF-M + Zephyr binary
   set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
 
-  math(EXPR ns_sec_flash_offset
-    "${CONFIG_STM32_INT_FLASH_SECURE_BASE_ADDRESS} - ${CONFIG_FLASH_BASE_ADDRESS}"
-  )
-
   # Flash is set all secure (<build>/tfm/api_ns/regression.sh) before flash programming
   # hence locate non-secure image from the secure flash base address in HEX file.
   dt_chosen(chosen_part_path PROPERTY "zephyr,code-partition")
   dt_reg_addr(chosen_part_addr PATH "${chosen_part_path}")
+  math(EXPR ns_sec_flash_offset
+    "${CONFIG_STM32_INT_FLASH_SECURE_BASE_ADDRESS} - ${CONFIG_FLASH_BASE_ADDRESS}"
+  )
   math(EXPR TFM_HEX_BASE_ADDRESS_NS "${chosen_part_addr} + ${ns_sec_flash_offset}")
 
-  # System entry point is TF-M vector, located 1kByte after tfm_fmw_partition in DTS
-  dt_nodelabel(tfm_partition_path NODELABEL slot0_secure_partition REQUIRED)
-  dt_reg_addr(tfm_partition_offset PATH ${tfm_partition_path} REQUIRED)
-  math(EXPR tfm_fwm_boot_address
-    "${tfm_partition_offset} + ${ns_sec_flash_offset} + ${CONFIG_ROM_START_OFFSET}"
-  )
-
-  board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw"
-    "--erase" "--start-address=${tfm_fwm_boot_address}"
-  )
+  board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw" "--erase")
 else()
   board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 endif()
