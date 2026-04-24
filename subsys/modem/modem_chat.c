5471b421d1d3a883fcdd8a8881a9f62fb9a22f0a
Seppo Takalo <seppo.takalo@nordicsemi.no>
2026-04-24 15:05:46 +0300

    modem: chat: Check NULL pointer for modem_chat_run_script_async()
    It is easier just check the NULL pointer in Chat module, so
    the modem_cellular.c does not need to check the existence of
    periodic scripts.
    
    Signed-off-by: Seppo Takalo <seppo.takalo@nordicsemi.no>

--- subsys/modem/modem_chat.c
+++ subsys/modem/modem_chat.c
@@ -859,7 +859,7 @@ int modem_chat_run_script_async(struct modem_chat *chat, const struct modem_chat
 	}
 
 	/* Validate script */
-	if (script->script_chats == NULL ||
+	if (script == NULL || script->script_chats == NULL ||
 	   (script->script_chats_size == 0
 	    && script->script_chats != modem_chat_empty_script_chats) ||
 	   (script->abort_matches_size == 0
