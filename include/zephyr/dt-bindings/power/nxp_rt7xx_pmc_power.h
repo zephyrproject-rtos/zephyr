50efc0742741faefb56cf62ff5e07975f6ea2a36
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2026-04-24 13:09:11 +0800

    drivers: misc: nxp_power_rail: add PMC controller driver
    1. Add the devicetree binding ``nxp,pmc-power-rail-controller``.
    2. Add the PMC (Power Management Controller) driver.
    3. Instantiate the controller on i.MX RT7xx.
    
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

---
+++ include/zephyr/dt-bindings/power/nxp_rt7xx_pmc_power.h
@@ -0,0 +1,27 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2026 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Named PMC power-rail identifiers for NXP RT7xx SoCs.
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_POWER_NXP_RT7XX_PMC_POWER_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_POWER_NXP_RT7XX_PMC_POWER_H_
+
+/*
+ * Named rail identifiers for use with
+ *   nxp,power-rails = <&pmc0 NXP_RT7XX_PMC_*>;
+ *
+ * The PMC identifier space is a flat coordinate over the PDRUNCFG0..5
+ * register array:
+ *     NXP_PMC_POWER_ID(idx, bit) = idx * 32 + bit
+ */
+
+/** @brief Build a flat PMC rail id from PDRUNCFGn register index and bit. */
+#define NXP_PMC_POWER_ID(pdruncfg_idx, bit) ((pdruncfg_idx) * 32 + (bit))
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_POWER_NXP_RT7XX_PMC_POWER_H_ */
