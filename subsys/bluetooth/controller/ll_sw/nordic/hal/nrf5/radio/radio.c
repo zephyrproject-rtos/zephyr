3ec218d5ff036837b1e8d6db9910a990a756a68d
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2026-04-27 15:23:55 +0200

    Bluetooth: Controller: Rename tx/rx_ready_delay_ns
    Rename hal_radio_tx/rx_ready_delay_ns_get to differentiate
    with ready delay inside tIFS.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/radio/radio.c
+++ subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/radio/radio.c
@@ -782,10 +782,8 @@ void sw_switch(uint8_t dir_curr, uint8_t dir_next, uint8_t phy_curr, uint8_t fla
 		 */
 		if (dir_curr == SW_SWITCH_TX) {
 			delay = HAL_RADIO_NS2US_ROUND(
-			    hal_radio_tx_ready_delay_ns_get(phy_next,
-							    flags_next) +
-			    hal_radio_tx_chain_delay_ns_get(phy_curr,
-							    flags_curr));
+			    hal_radio_tifs_tx_ready_delay_ns_get(phy_next, flags_next) +
+			    hal_radio_tx_chain_delay_ns_get(phy_curr, flags_curr));
 
 			hal_radio_b2b_txen_on_sw_switch(cc, ppi);
 		} else {
@@ -793,8 +791,7 @@ void sw_switch(uint8_t dir_curr, uint8_t dir_next, uint8_t phy_curr, uint8_t fla
 			 * Assumption being, S8 has higher delay.
 			 */
 			delay = HAL_RADIO_NS2US_ROUND(
-			    hal_radio_tx_ready_delay_ns_get(phy_next,
-							    flags_next) +
+			    hal_radio_tifs_tx_ready_delay_ns_get(phy_next, flags_next) +
 			    hal_radio_rx_chain_delay_ns_get(phy_curr, PHY_FLAGS_S8));
 
 			hal_radio_txen_on_sw_switch(cc, ppi);
@@ -835,8 +832,7 @@ void sw_switch(uint8_t dir_curr, uint8_t dir_next, uint8_t phy_curr, uint8_t fla
 
 			/* Calculate assuming reception on S2 coding scheme. */
 			delay_s2 = HAL_RADIO_NS2US_ROUND(
-				hal_radio_tx_ready_delay_ns_get(phy_next,
-								flags_next) +
+				hal_radio_tifs_tx_ready_delay_ns_get(phy_next, flags_next) +
 				hal_radio_rx_chain_delay_ns_get(phy_curr, PHY_FLAGS_S2));
 
 			new_cc_s2_value = SW_SWITCH_TIMER->CC[cc];
@@ -884,19 +880,15 @@ void sw_switch(uint8_t dir_curr, uint8_t dir_next, uint8_t phy_curr, uint8_t fla
 		/* Calculate delay with respect to current and next PHY. */
 		if (dir_curr) {
 			delay = HAL_RADIO_NS2US_CEIL(
-				hal_radio_rx_ready_delay_ns_get(phy_next,
-								flags_next) +
-				hal_radio_tx_chain_delay_ns_get(phy_curr,
-								flags_curr)) +
+				hal_radio_tifs_rx_ready_delay_ns_get(phy_next, flags_next) +
+				hal_radio_tx_chain_delay_ns_get(phy_curr, flags_curr)) +
 				(EVENT_CLOCK_JITTER_US << 1);
 
 			hal_radio_rxen_on_sw_switch(cc, ppi);
 		} else {
 			delay = HAL_RADIO_NS2US_CEIL(
-				hal_radio_rx_ready_delay_ns_get(phy_next,
-								flags_next) +
-				hal_radio_rx_chain_delay_ns_get(phy_curr,
-								flags_curr)) +
+				hal_radio_tifs_rx_ready_delay_ns_get(phy_next, flags_next) +
+				hal_radio_rx_chain_delay_ns_get(phy_curr, flags_curr)) +
 				(EVENT_CLOCK_JITTER_US << 1);
 
 			hal_radio_b2b_rxen_on_sw_switch(cc, ppi);
