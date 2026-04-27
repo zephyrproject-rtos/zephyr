Johnsonyu0926/zrtos

Bluetooth: CCP: Client: Add support for get bearer uci
Add support for getting the remote bearer UCI.

Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

include/zephyr/bluetooth/audio/ccp.h
@@ -192,6 +192,20 @@ struct bt_ccp_client_cb {
 				     const char *name);
 #endif /* CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME */
 
+#if defined(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)
+	/**
+	 * @brief Callback function for bt_ccp_client_read_bearer_provider_name().
+	 *
+	 * This callback is called once the read bearer provider name procedure is completed.
+	 *
+	 * @param client Call Control Client instance pointer.
+	 * @param err    Error value. 0 on success, GATT error on positive
+	 *               value or errno on negative value.
+	 * @param uci    The UCI of the bearer. NULL if @p err is not 0.
+	 */
+	void (*bearer_uci)(struct bt_ccp_client_bearer *bearer, int err, const char *uci);
+#endif /* CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME */
+
 	/** @internal Internally used field for list handling */
 	sys_snode_t _node;
 };
@@ -245,6 +259,23 @@ int bt_ccp_client_get_bearers(struct bt_ccp_client *client, struct bt_ccp_client
  * @retval -ENOTCONN The @ref bt_ccp_client identified by @p bearer is not connected
  */
 int bt_ccp_client_read_bearer_provider_name(struct bt_ccp_client_bearer *bearer);
+
+/**
+ * @brief Read the bearer Unicorm Caller Identifier (UCI) of a remote TBS bearer.
+ *
+ * @kconfig_dep{CONFIG_BT_TBS_CLIENT_BEARER_UCI}
+ *
+ * @param bearer The bearer to read the UCI from
+ *
+ * @retval 0 Success
+ * @retval -EINVAL @p bearer is NULL
+ * @retval -EFAULT @p bearer has not been discovered
+ * @retval -EEXIST A @ref bt_ccp_client could not be identified for @p bearer
+ * @retval -EBUSY The @ref bt_ccp_client identified by @p bearer is busy, or the TBS instance
+ *                of @p bearer is busy.
+ * @retval -ENOTCONN The @ref bt_ccp_client identified by @p bearer is not connected
+ */
+int bt_ccp_client_read_bearer_uci(struct bt_ccp_client_bearer *bearer);
 /** @} */ /* End of group bt_ccp_client */
 #ifdef __cplusplus
 }

samples/bluetooth/ccp_client/src/main.c
@@ -227,6 +227,21 @@ static void ccp_client_read_bearer_provider_name_cb(struct bt_ccp_client_bearer 
 }
 #endif /* CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME */
 
+#if defined(CONFIG_BT_TBS_CLIENT_BEARER_UCI)
+static void ccp_client_read_bearer_uci_cb(struct bt_ccp_client_bearer *bearer, int err,
+					  const char *name)
+{
+	if (err != 0) {
+		LOG_ERR("Failed to read bearer %p UCI: %d\n", (void *)bearer, err);
+		return;
+	}
+
+	LOG_INF("Bearer %p UCI: %s", (void *)bearer, name);
+
+	k_sem_give(&sem_ccp_action_completed);
+}
+#endif /* CONFIG_BT_TBS_CLIENT_BEARER_UCI */
+
 static int reset_ccp_client(void)
 {
 	int err;
@@ -297,24 +312,62 @@ static int read_bearer_name(struct bt_ccp_client_bearer *bearer)
 	return 0;
 }
 
-static int read_bearer_names(void)
+static int read_bearer_uci(struct bt_ccp_client_bearer *bearer)
+{
+	int err;
+
+	err = bt_ccp_client_read_bearer_uci(bearer);
+	if (err != 0) {
+		return err;
+	}
+
+	err = k_sem_take(&sem_ccp_action_completed, SEM_TIMEOUT);
+	if (err != 0) {
+		LOG_ERR("Failed to take sem_ccp_action_completed: %d", err);
+		return err;
+	}
+
+	return 0;
+}
+
+static int read_bearer_values(void)
 {
 	int err;
 
 #if defined(CONFIG_BT_TBS_CLIENT_GTBS)
-	err = read_bearer_name(client_bearers.gtbs_bearer);
-	if (err != 0) {
-		LOG_ERR("Failed to read name for GTBS bearer: %d", err);
-		return err;
+	if (IS_ENABLED(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)) {
+		err = read_bearer_name(client_bearers.gtbs_bearer);
+		if (err != 0) {
+			LOG_ERR("Failed to read name for GTBS bearer: %d", err);
+			return err;
+		}
+	}
+
+	if (IS_ENABLED(CONFIG_BT_TBS_CLIENT_BEARER_UCI)) {
+		err = read_bearer_uci(client_bearers.gtbs_bearer);
+		if (err != 0) {
+			LOG_ERR("Failed to read UCI for GTBS bearer: %d", err);
+			return err;
+		}
 	}
 #endif /* CONFIG_BT_TBS_CLIENT_GTBS */
 
 #if defined(CONFIG_BT_TBS_CLIENT_TBS)
 	for (size_t i = 0; i < client_bearers.tbs_count; i++) {
-		err = read_bearer_name(client_bearers.tbs_bearers[i]);
-		if (err != 0) {
-			LOG_ERR("Failed to read name for bearer[%zu]: %d", i, err);
-			return err;
+		if (IS_ENABLED(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)) {
+			err = read_bearer_name(client_bearers.tbs_bearers[i]);
+			if (err != 0) {
+				LOG_ERR("Failed to read name for bearer[%zu]: %d", i, err);
+				return err;
+			}
+		}
+
+		if (IS_ENABLED(CONFIG_BT_TBS_CLIENT_BEARER_UCI)) {
+			err = read_bearer_uci(client_bearers.tbs_bearers[i]);
+			if (err != 0) {
+				LOG_ERR("Failed to read UCI for bearer[%zu]: %d", i, err);
+				return err;
+			}
 		}
 	}
 #endif /* CONFIG_BT_TBS_CLIENT_TBS */
@@ -327,8 +380,11 @@ static int init_ccp_client(void)
 	static struct bt_ccp_client_cb ccp_client_cbs = {
 		.discover = ccp_client_discover_cb,
 #if defined(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)
-		.bearer_provider_name = ccp_client_read_bearer_provider_name_cb
+		.bearer_provider_name = ccp_client_read_bearer_provider_name_cb,
 #endif /* CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME */
+#if defined(CONFIG_BT_TBS_CLIENT_BEARER_UCI)
+		.bearer_uci = ccp_client_read_bearer_uci_cb,
+#endif /* CONFIG_BT_TBS_CLIENT_BEARER_UCI */
 	};
 	static struct bt_le_scan_cb scan_cbs = {
 		.recv = scan_recv_cb,
@@ -389,12 +445,7 @@ int main(void)
 			continue;
 		}
 
-		if (IS_ENABLED(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)) {
-			err = read_bearer_names();
-			if (err != 0) {
-				continue;
-			}
-		}
+		read_bearer_values();
 
 		/* Reset if disconnected */
 		err = k_sem_take(&sem_conn_state_change, K_FOREVER);

zephyrproject-rtos/zephyr

include/zephyr/bluetooth/audio/ccp.h
@@ -165,12 +165,12 @@ struct bt_ccp_call_control_client_bearer;
 
 /** Struct with information about bearers of a client */
 struct bt_ccp_call_control_client_bearers {
-#if defined(CONFIG_BT_TBS_CLIENT_GTBS)
+#if defined(CONFIG_BT_TBS_CLIENT_GTBS) || defined(__DOXYGEN__)
 	/** The GTBS bearer. */
 	struct bt_ccp_call_control_client_bearer *gtbs_bearer;
 #endif /* CONFIG_BT_TBS_CLIENT_GTBS */
 
-#if defined(CONFIG_BT_TBS_CLIENT_TBS)
+#if defined(CONFIG_BT_TBS_CLIENT_TBS) || defined(__DOXYGEN__)
 	/** Number of TBS bearers in @p tbs_bearers */
 	size_t tbs_count;
 
@@ -208,7 +208,7 @@ struct bt_ccp_call_control_client_cb {
 	 *
 	 * This callback is called once the read bearer provider name procedure is completed.
 	 *
-	 * @param bearer     Call Control Client bearer instance pointer.
+	 * @param bearer     Call Control Client bearer pointer.
 	 * @param err        Error value. 0 on success, GATT error on positive
 	 *                   value or errno on negative value.
 	 * @param name       The bearer provider name. NULL if @p err is not 0.
@@ -220,6 +220,24 @@ struct bt_ccp_call_control_client_cb {
 	void (*bearer_provider_name)(struct bt_ccp_call_control_client_bearer *bearer, int err,
 				     const char *name, void *user_data);
 #endif /* CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME */
+
+#if defined(CONFIG_BT_TBS_CLIENT_BEARER_UCI) || defined(__DOXYGEN__)
+	/**
+	 * @brief Callback function for bt_ccp_call_control_client_read_bearer_uci().
+	 *
+	 * This callback is called once the read bearer UCI procedure is completed.
+	 *
+	 * @param bearer Call Control Client bearer pointer.
+	 * @param err Error value. 0 on success, GATT error on positive
+	 *            value or errno on negative value.
+	 * @param uci The UCI of the bearer. NULL if @p err is not 0.
+	 * @param user_data  User data stored in the callback struct. Will always be NULL if
+	 *                   @kconfig{CONFIG_BT_CCP_CALL_CONTROL_CLIENT_CB_USER_DATA} is not
+	 *                   enabled.
+	 */
+	void (*bearer_uci)(struct bt_ccp_call_control_client_bearer *bearer, int err,
+			   const char *uci, void *user_data);
+#endif /* CONFIG_BT_TBS_CLIENT_BEARER_UCI */
 
 #if defined(CONFIG_BT_CCP_CALL_CONTROL_CLIENT_CB_USER_DATA) || defined(__DOXYGEN__)
 	/** User data that will be supplied to all callbacks */
@@ -304,6 +322,23 @@ int bt_ccp_call_control_client_get_bearers(struct bt_ccp_call_control_client *cl
  */
 int bt_ccp_call_control_client_read_bearer_provider_name(
 	struct bt_ccp_call_control_client_bearer *bearer);
+
+/**
+ * @brief Read the bearer Uniform Caller Identifier (UCI) of a remote TBS bearer.
+ *
+ * @kconfig_dep{CONFIG_BT_TBS_CLIENT_BEARER_UCI}
+ *
+ * @param bearer The bearer to read the UCI from
+ *
+ * @retval 0 Success
+ * @retval -EINVAL @p bearer is NULL
+ * @retval -EFAULT @p bearer has not been discovered
+ * @retval -EEXIST A @ref bt_ccp_call_control_client could not be identified for @p bearer
+ * @retval -EBUSY The @ref bt_ccp_call_control_client identified by @p bearer is busy, or the TBS
+ * instance of @p bearer is busy.
+ * @retval -ENOTCONN The @ref bt_ccp_call_control_client identified by @p bearer is not connected
+ */
+int bt_ccp_call_control_client_read_bearer_uci(struct bt_ccp_call_control_client_bearer *bearer);
 /** @} */ /* End of group bt_ccp_call_control_client */
 #ifdef __cplusplus
 }

samples/bluetooth/ccp_call_control_client/src/main.c
@@ -219,6 +219,22 @@ static void ccp_call_control_client_read_bearer_provider_name_cb(
 }
 #endif /* CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME */
 
+#if defined(CONFIG_BT_TBS_CLIENT_BEARER_UCI)
+static void
+ccp_call_control_client_read_bearer_uci_cb(struct bt_ccp_call_control_client_bearer *bearer,
+					   int err, const char *uci, void *user_data)
+{
+	if (err != 0) {
+		LOG_ERR("Failed to read bearer %p UCI: %d\n", (void *)bearer, err);
+		return;
+	}
+
+	LOG_INF("Bearer %p UCI: %s", (void *)bearer, uci);
+
+	k_sem_give(&sem_ccp_action_completed);
+}
+#endif /* CONFIG_BT_TBS_CLIENT_BEARER_UCI */
+
 static int reset_ccp_call_control_client(void)
 {
 	int err;
@@ -289,24 +305,62 @@ static int read_bearer_name(struct bt_ccp_call_control_client_bearer *bearer)
 	return 0;
 }
 
-static int read_bearer_names(void)
+static int read_bearer_uci(struct bt_ccp_call_control_client_bearer *bearer)
+{
+	int err;
+
+	err = bt_ccp_call_control_client_read_bearer_uci(bearer);
+	if (err != 0) {
+		return err;
+	}
+
+	err = k_sem_take(&sem_ccp_action_completed, SEM_TIMEOUT);
+	if (err != 0) {
+		LOG_ERR("Failed to take sem_ccp_action_completed: %d", err);
+		return err;
+	}
+
+	return 0;
+}
+
+static int read_bearer_values(void)
 {
 	int err;
 
 #if defined(CONFIG_BT_TBS_CLIENT_GTBS)
-	err = read_bearer_name(client_bearers.gtbs_bearer);
-	if (err != 0) {
-		LOG_ERR("Failed to read name for GTBS bearer: %d", err);
-		return err;
+	if (IS_ENABLED(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)) {
+		err = read_bearer_name(client_bearers.gtbs_bearer);
+		if (err != 0) {
+			LOG_ERR("Failed to read name for GTBS bearer: %d", err);
+			return err;
+		}
+	}
+
+	if (IS_ENABLED(CONFIG_BT_TBS_CLIENT_BEARER_UCI)) {
+		err = read_bearer_uci(client_bearers.gtbs_bearer);
+		if (err != 0) {
+			LOG_ERR("Failed to read UCI for GTBS bearer: %d", err);
+			return err;
+		}
 	}
 #endif /* CONFIG_BT_TBS_CLIENT_GTBS */
 
 #if defined(CONFIG_BT_TBS_CLIENT_TBS)
 	for (size_t i = 0; i < client_bearers.tbs_count; i++) {
-		err = read_bearer_name(client_bearers.tbs_bearers[i]);
-		if (err != 0) {
-			LOG_ERR("Failed to read name for bearer[%zu]: %d", i, err);
-			return err;
+		if (IS_ENABLED(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)) {
+			err = read_bearer_name(client_bearers.tbs_bearers[i]);
+			if (err != 0) {
+				LOG_ERR("Failed to read name for bearer[%zu]: %d", i, err);
+				return err;
+			}
+		}
+
+		if (IS_ENABLED(CONFIG_BT_TBS_CLIENT_BEARER_UCI)) {
+			err = read_bearer_uci(client_bearers.tbs_bearers[i]);
+			if (err != 0) {
+				LOG_ERR("Failed to read UCI for bearer[%zu]: %d", i, err);
+				return err;
+			}
 		}
 	}
 #endif /* CONFIG_BT_TBS_CLIENT_TBS */
@@ -319,8 +373,11 @@ static int init_ccp_call_control_client(void)
 	static struct bt_ccp_call_control_client_cb ccp_call_control_client_cbs = {
 		.discover = ccp_call_control_client_discover_cb,
 #if defined(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)
-		.bearer_provider_name = ccp_call_control_client_read_bearer_provider_name_cb
+		.bearer_provider_name = ccp_call_control_client_read_bearer_provider_name_cb,
 #endif /* CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME */
+#if defined(CONFIG_BT_TBS_CLIENT_BEARER_UCI)
+		.bearer_uci = ccp_call_control_client_read_bearer_uci_cb,
+#endif /* CONFIG_BT_TBS_CLIENT_BEARER_UCI */
 	};
 	static struct bt_le_scan_cb scan_cbs = {
 		.recv = scan_recv_cb,
@@ -381,11 +438,9 @@ int main(void)
 			continue;
 		}
 
-		if (IS_ENABLED(CONFIG_BT_TBS_CLIENT_BEARER_PROVIDER_NAME)) {
-			err = read_bearer_names();
-			if (err != 0) {
-				continue;
-			}
+		read_bearer_values();
+		if (err != 0) {
+			continue;
 		}
 
 		/* Reset if disconnected */