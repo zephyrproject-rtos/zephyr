265299654ad02a7bdcc0963c472a35a84d0da749
Joel Guittet <joelguittet@gmail.com>
2026-04-28 18:24:39 +0200

    scripts: twister: add raspberry pi debug probe
    Select the expected board id for Raspberry Pi Debug Probe hardware.
    
    Signed-off-by: Joel Guittet <joelguittet@gmail.com>

--- scripts/pylib/twister/twisterlib/handlers.py
+++ scripts/pylib/twister/twisterlib/handlers.py
@@ -596,6 +596,9 @@ class DeviceHandler(Handler):
                 ]:
                     command_extra_args.append("--cmd-pre-init")
                     command_extra_args.append(f"hla_serial {board_id}")
+                elif runner == 'openocd' and product == "Raspberry Pi Debug Probe (CMSIS-DAP)":
+                    command_extra_args.append("--cmd-pre-init")
+                    command_extra_args.append(f"cmsis_dap_serial {board_id}")
                 elif runner == "openocd" and (product in ("EDBG CMSIS-DAP", "LPC-LINK2 CMSIS-DAP")):
                     command_extra_args.append("--cmd-pre-init")
                     command_extra_args.append(f"adapter serial {board_id}")
