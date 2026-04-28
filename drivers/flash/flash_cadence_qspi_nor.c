b6ae8e3defbe3448743be1700892e709b3d45c86
Syed Muhammad Murtaza <syedx.muhammad.murtaza@altera.com>
2026-04-28 15:17:42 +0500

    drivers: flash: cad_qspi_nor: add clock control and page layout support
    Fall back to clock provider instead of relying on fixed configuration.
    Update write block size to use dt page-size property. Added support
    for page layout and exposing layout information.
    
    Signed-off-by: Syed Muhammad Murtaza <syedx.muhammad.murtaza@altera.com>

--- drivers/flash/flash_cadence_qspi_nor.c
+++ drivers/flash/flash_cadence_qspi_nor.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #define DT_DRV_COMPAT cdns_qspi_nor
+#define DEVICE_NODE   DT_INST(0, micron_mt25qu02g)
 
 #include "flash_cadence_qspi_nor_ll.h"
 
@@ -13,6 +14,7 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/flash.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/clock_control.h>
 
 LOG_MODULE_REGISTER(flash_cadence, CONFIG_FLASH_LOG_LEVEL);
 
@@ -20,15 +22,24 @@ struct flash_cad_priv {
 	DEVICE_MMIO_NAMED_RAM(qspi_reg);
 	DEVICE_MMIO_NAMED_RAM(qspi_data);
 	struct cad_qspi_params params;
+	/* Clock frequency of timer */
+	uint32_t freq;
+	/* Clock controller dev instance */
+	const struct device *clk_dev;
+	/* Identifier for timer to get clock freq from clk manager */
+	clock_control_subsys_t clkid;
 };
 
 struct flash_cad_config {
 	DEVICE_MMIO_NAMED_ROM(qspi_reg);
 	DEVICE_MMIO_NAMED_ROM(qspi_data);
+#if defined(CONFIG_FLASH_PAGE_LAYOUT)
+	struct flash_pages_layout pages_layout;
+#endif
 };
 
 static const struct flash_parameters flash_cad_parameters = {
-	.write_block_size = QSPI_BYTES_PER_DEV,
+	.write_block_size = DT_PROP(DEVICE_NODE, page_size),
 	.erase_value = 0xff,
 };
 
@@ -109,11 +120,24 @@ flash_cad_get_parameters(const struct device *dev)
 	return &flash_cad_parameters;
 }
 
+#if defined(CONFIG_FLASH_PAGE_LAYOUT)
+static void flash_cad_get_layout(const struct device *dev, const struct flash_pages_layout **layout,
+				 size_t *layout_size)
+{
+	const struct flash_cad_config *cfg = DEV_CFG(dev);
+	*layout = &cfg->pages_layout;
+	*layout_size = 1;
+}
+#endif /* CONFIG_FLASH_PAGE_LAYOUT */
+
 static DEVICE_API(flash, flash_cad_api) = {
 	.erase = flash_cad_erase,
 	.write = flash_cad_write,
 	.read = flash_cad_read,
 	.get_parameters = flash_cad_get_parameters,
+#if defined(CONFIG_FLASH_PAGE_LAYOUT)
+	.page_layout = flash_cad_get_layout,
+#endif
 };
 
 static int flash_cad_init(const struct device *dev)
@@ -125,8 +149,27 @@ static int flash_cad_init(const struct device *dev)
 	DEVICE_MMIO_NAMED_MAP(dev, qspi_reg, K_MEM_CACHE_NONE);
 	DEVICE_MMIO_NAMED_MAP(dev, qspi_data, K_MEM_CACHE_NONE);
 
+	/*
+	 * Get clock rate from clock_frequency property if valid,
+	 * otherwise, get clock rate from clock manager
+	 */
+	if (priv->freq == 0U) {
+		if (!device_is_ready(priv->clk_dev)) {
+			LOG_ERR("clock controller device not ready");
+			return -ENODEV;
+		}
+
+		rc = clock_control_get_rate(priv->clk_dev, priv->clkid, &priv->freq);
+		if (rc != 0) {
+			LOG_ERR("Unable to get clock rate: err:%d", rc);
+			return rc;
+		}
+	}
+
+
 	cad_params->reg_base = DEVICE_MMIO_NAMED_GET(dev, qspi_reg);
 	cad_params->data_base = DEVICE_MMIO_NAMED_GET(dev, qspi_data);
+	cad_params->clk_rate = priv->freq;
 
 	rc = cad_qspi_init(cad_params, QSPI_CONFIG_CPHA,
 			QSPI_CONFIG_CPOL, QSPI_CONFIG_CSDA,
