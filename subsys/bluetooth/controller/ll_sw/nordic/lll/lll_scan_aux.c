52fddca003938029f1bd3232a3d0c17a83dfc05d
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2026-04-30 21:20:58 +0200

    Bluetooth: Controller: Fix single timer elapsed time accumulation
    Fix missing packet/event timer accumulating the elapsed time
    under single timer use.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan_aux.c
+++ subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan_aux.c
@@ -1432,6 +1432,11 @@ static void isr_tx(struct lll_scan_aux *lll_aux, void (*isr)(void *arg), void *p
 		node_rx_prof = lll_prof_reserve();
 	}
 
+	/* Call to ensure packet/event timer accumulates the elapsed time
+	 * under single timer use.
+	 */
+	(void)radio_is_tx_done();
+
 	/* Clear radio tx status and events */
 	lll_isr_tx_status_reset();
 
