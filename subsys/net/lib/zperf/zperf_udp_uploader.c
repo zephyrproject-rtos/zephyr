d0e57d6e06b18fd42e326cf1151bd091208ca51f
Tim Pambor <tim.pambor@codewrights.de>
2026-05-02 10:39:45 +0200

    net: zperf: udp uploader: start packet IDs at one
    In iperf2, the first packet of a session has ID 1.
    Change the UDP uploader to start packet IDs at one,
    so that the behavior is the same as iperf2.
    
    Signed-off-by: Tim Pambor <tim.pambor@codewrights.de>

--- subsys/net/lib/zperf/zperf_udp_uploader.c
+++ subsys/net/lib/zperf/zperf_udp_uploader.c
@@ -79,7 +79,7 @@ static inline int zperf_upload_fin(int sock,
 		datagram = (struct zperf_udp_datagram *)sample_packet;
 
 		/* Fill the packet header */
-		datagram->id = net_htonl(-nb_packets);
+		datagram->id = net_htonl(-(nb_packets + 1));
 		datagram->tv_sec = net_htonl(secs);
 		datagram->tv_usec = net_htonl(usecs);
 
@@ -321,7 +321,7 @@ static int udp_upload(int sock, int port,
 		/* Fill the packet header */
 		datagram = (struct zperf_udp_datagram *)sample_packet;
 
-		datagram->id = net_htonl(nb_packets);
+		datagram->id = net_htonl(nb_packets + 1);
 		datagram->tv_sec = net_htonl(secs);
 		datagram->tv_usec = net_htonl(usecs);
 
