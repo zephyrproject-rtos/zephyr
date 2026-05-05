11354184d4a33bca1b8406fc223c3504758bb5b3
Flavio Ceolin <flavio@hubblenetwork.com>
2026-05-05 15:12:04 -0700

    net: http_server: Normalize URL path before lookup
    Add http_server_normalize_url() to resolve '.' and '..' segments in
    client->url_buffer once the URL is fully assembled to avoid a remote
    client to read files outside the configured web root.
    
    Signed-off-by: Flavio Ceolin <flavio@hubblenetwork.com>
    (cherry picked from commit f4a423c98554f209c5d2f22f041822422c9263b8)

--- subsys/net/lib/http/headers/server_internal.h
+++ subsys/net/lib/http/headers/server_internal.h
@@ -47,6 +47,7 @@ bool compression_value_is_valid(enum http_compression compression);
 /* Others */
 struct http_resource_detail *get_resource_detail(const struct http_service_desc *service,
 						 const char *path, int *len, bool is_ws);
+void http_server_remove_dot_segments(char *path);
 int http_server_sendall(struct http_client_ctx *client, const void *buf, size_t len);
 void http_server_get_content_type_from_extension(char *url, char *content_type,
 						 size_t content_type_size);
