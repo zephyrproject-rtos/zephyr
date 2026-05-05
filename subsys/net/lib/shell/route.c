8146a98c366df73e364f87d9cb139833e070b97d
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2026-05-05 18:06:31 +0300

    net: shell: route: Add IPv4 support to route cmd
    Make sure that user is able to add/remove/list IPv4 routes
    in the system.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- subsys/net/lib/shell/route.c
+++ subsys/net/lib/shell/route.c
@@ -11,15 +11,74 @@ LOG_MODULE_DECLARE(net_shell);
 #include "net_shell_private.h"
 
 #include "../ip/route_ipv6.h"
+#include "../ip/route_ipv4.h"
 
-#if defined(CONFIG_NET_IPV6_ROUTE) && defined(CONFIG_NET_NATIVE)
+#if (defined(CONFIG_NET_NATIVE_IPV6) && defined(CONFIG_NET_IPV6_ROUTE)) || \
+	(defined(CONFIG_NET_NATIVE_IPV4) && defined(CONFIG_NET_IPV4_ROUTE))
+#define NATIVE_ROUTE
+#else
+#if defined(NATIVE_ROUTE)
+#undef NATIVE_ROUTE
+#endif
+#endif
+
+#if defined(CONFIG_NET_NATIVE) && defined(NATIVE_ROUTE)
+
+struct user_data_lladdr {
+	const struct net_in_addr *nexthop;
+	uint8_t *lladdr;
+	size_t lladdr_len;
+	bool found;
+};
+
+#if defined(CONFIG_NET_ARP)
+#include "ethernet/arp.h"
+
+static void arp_cb(struct arp_entry *entry, void *user_data)
+{
+	struct user_data_lladdr *data = user_data;
+
+	if (!data->found && net_ipv4_addr_cmp(&entry->ip, data->nexthop)) {
+		memcpy(data->lladdr, entry->eth.addr,
+		       MIN(sizeof(entry->eth.addr), data->lladdr_len));
+		data->found = true;
+	}
+}
+#endif
+
+static int get_ipv4_lladdr(const struct net_in_addr *nexthop,
+			   uint8_t *lladdr, size_t lladdr_len)
+{
+#if defined(CONFIG_NET_ARP)
+	if (nexthop == NULL || lladdr == NULL || lladdr_len == 0U) {
+		return -ENOENT;
+	}
+
+	struct user_data_lladdr user_data = {
+		.nexthop = nexthop,
+		.lladdr = lladdr,
+		.lladdr_len = lladdr_len,
+		.found = false,
+	};
+
+	net_arp_foreach(arp_cb, &user_data);
+
+	if (!user_data.found) {
+		return -ENOENT;
+	}
+
+	return MIN(NET_ETH_ADDR_LEN, user_data.lladdr_len);
+#else
+	return -ENOTSUP;
+#endif /* CONFIG_NET_ARP */
+}
+
 static void route_cb(struct net_route_entry *entry, void *user_data)
 {
 	struct net_shell_user_data *data = user_data;
 	const struct shell *sh = data->sh;
 	struct net_if *iface = data->user_data;
 	struct net_nbr *nbr;
-	struct net_in6_addr *nexthop;
 	uint32_t now = k_uptime_get_32();
 	char remaining_str[sizeof("01234567890 sec")];
 	uint32_t remaining;
@@ -28,28 +87,63 @@ static void route_cb(struct net_route_entry *entry, void *user_data)
 		return;
 	}
 
-	PR("IPv6 prefix : %s/%d\n", net_sprint_ipv6_addr(&entry->addr.in6_addr),
-	   entry->prefix_len);
+	if (IS_ENABLED(CONFIG_NET_IPV6_ROUTE) && entry->addr.family == NET_AF_INET6) {
+		struct net_in6_addr *nexthop;
 
-	nexthop = net_route_ipv6_get_nexthop(entry);
-	nbr = net_route_ipv6_get_nbr(entry);
+		PR("IPv6 prefix : %s/%d\n", net_sprint_ipv6_addr(&entry->addr.in6_addr),
+		   entry->prefix_len);
 
-	if (entry->is_infinite) {
-		snprintk(remaining_str, sizeof(remaining_str) - 1, "infinite");
+		nexthop = net_route_ipv6_get_nexthop(entry);
+		nbr = net_route_ipv6_get_nbr(entry);
+
+		if (entry->is_infinite) {
+			snprintk(remaining_str, sizeof(remaining_str) - 1, "infinite");
+		} else {
+			remaining = net_timeout_remaining(&entry->lifetime, now);
+			snprintk(remaining_str, sizeof(remaining_str) - 1, "%u sec", remaining);
+		}
+
+		PR("\tnexthop  : %s\t",
+		   nexthop ? net_sprint_ipv6_addr(nexthop) : "<unknown>");
+
+		if (nbr != NULL && nbr->idx != NET_NBR_LLADDR_UNKNOWN) {
+			struct net_linkaddr *lladdr = net_nbr_get_lladdr(nbr->idx);
+
+			PR("ll addr : %s\t", net_sprint_ll_addr(lladdr->addr, lladdr->len));
+		} else {
+			PR("ll addr : <unknown>\t");
+		}
+
+	} else if (IS_ENABLED(CONFIG_NET_IPV4_ROUTE) && entry->addr.family == NET_AF_INET) {
+		struct net_in_addr *nexthop;
+		uint8_t lladdr[NET_LINK_ADDR_MAX_LENGTH];
+		int ret;
+
+		PR("IPv4 prefix : %s/%d\n", net_sprint_ipv4_addr(&entry->addr.in_addr),
+		   entry->prefix_len);
+
+		nexthop = net_route_ipv4_get_nexthop(entry);
+		ret = nexthop != NULL ? get_ipv4_lladdr(nexthop, lladdr, sizeof(lladdr)) :
+		      -ENOENT;
+
+		if (entry->is_infinite) {
+			snprintk(remaining_str, sizeof(remaining_str) - 1, "infinite");
+		} else {
+			remaining = net_timeout_remaining(&entry->lifetime, now);
+			snprintk(remaining_str, sizeof(remaining_str) - 1, "%u sec", remaining);
+		}
+
+		PR("\tnexthop  : %s\t",
+		   nexthop ? net_sprint_ipv4_addr(nexthop) : "<on-link>");
+
+		if (ret > 0 && ret <= sizeof(lladdr)) {
+			PR("ll addr : %s\t", net_sprint_ll_addr(lladdr, ret));
+		} else {
+			PR("ll addr : <unknown>\t");
+		}
 	} else {
-		remaining = net_timeout_remaining(&entry->lifetime, now);
-		snprintk(remaining_str, sizeof(remaining_str) - 1, "%u sec", remaining);
-	}
-
-	PR("\tnexthop  : %s\t",
-	   nexthop ? net_sprint_ipv6_addr(nexthop) : "<unknown>");
-
-	if (nbr != NULL && nbr->idx != NET_NBR_LLADDR_UNKNOWN) {
-		struct net_linkaddr *lladdr = net_nbr_get_lladdr(nbr->idx);
-
-		PR("ll addr : %s\t", net_sprint_ll_addr(lladdr->addr, lladdr->len));
-	} else {
-		PR("ll addr : <unknown>\t");
+		PR("Unknown route family %d\n", entry->addr.family);
+		return;
 	}
 
 	PR("lifetime : %s\n", remaining_str);
@@ -61,16 +155,29 @@ static void iface_per_route_cb(struct net_if *iface, void *user_data)
 	const struct shell *sh = data->sh;
 	const char *extra;
 
-	PR("\nIPv6 routes for interface %d (%p) (%s)\n",
-	   net_if_get_by_iface(iface), iface,
-	   iface2str(iface, &extra));
-	PR("=========================================%s\n", extra);
+	if (IS_ENABLED(CONFIG_NET_IPV6_ROUTE)) {
+		PR("\nIPv6 routes for interface %d (%p) (%s)\n",
+		   net_if_get_by_iface(iface), iface,
+		   iface2str(iface, &extra));
+		PR("=========================================%s\n", extra);
 
-	data->user_data = iface;
+		data->user_data = iface;
 
-	net_route_ipv6_foreach(route_cb, data);
+		net_route_ipv6_foreach(route_cb, data);
+	}
+
+	if (IS_ENABLED(CONFIG_NET_IPV4_ROUTE)) {
+		PR("\nIPv4 routes for interface %d (%p) (%s)\n",
+		   net_if_get_by_iface(iface), iface,
+		   iface2str(iface, &extra));
+		PR("=========================================%s\n", extra);
+
+		data->user_data = iface;
+
+		net_route_ipv4_foreach(route_cb, data);
+	}
 }
-#endif /* CONFIG_NET_IPV6_ROUTE */
+#endif /* NATIVE_ROUTE */
 
 #if defined(CONFIG_NET_IPV6_ROUTE_MCAST) && defined(CONFIG_NET_NATIVE)
 static void route_mcast_cb(struct net_route_ipv6_entry_mcast *entry,
@@ -95,14 +202,15 @@ static void route_mcast_cb(struct net_route_ipv6_entry_mcast *entry,
 }
 #endif /* CONFIG_NET_IPV6_ROUTE_MCAST */
 
-static int cmd_net_ip6_route_add(const struct shell *sh, size_t argc, char *argv[])
+static int cmd_net_route_add(const struct shell *sh, size_t argc, char *argv[])
 {
-#if defined(CONFIG_NET_NATIVE_IPV6) && (CONFIG_NET_IPV6_ROUTE)
+#if defined(NATIVE_ROUTE)
 	struct net_if *iface = NULL;
 	int idx;
 	struct net_route_entry *route;
-	struct net_in6_addr gw = {0};
-	struct net_in6_addr prefix = {0};
+	struct net_sockaddr_storage addr;
+	const char *str;
+	uint8_t mask_len;
 
 	if (argc != 4) {
 		PR_ERROR("Correct usage: net route add <index> "
@@ -121,44 +229,73 @@ static int cmd_net_ip6_route_add(const struct shell *sh, size_t argc, char *argv
 		return -ENOEXEC;
 	}
 
-	if (net_addr_pton(NET_AF_INET6, argv[2], &prefix)) {
-		PR_ERROR("Invalid address: %s\n", argv[2]);
+	str = net_ipaddr_parse_mask(argv[2], strlen(argv[2]), net_sad(&addr), &mask_len);
+	if (str == NULL) {
+		PR_ERROR("Invalid destination: %s\n", argv[2]);
 		return -EINVAL;
 	}
 
-	if (net_addr_pton(NET_AF_INET6, argv[3], &gw)) {
-		PR_ERROR("Invalid gateway: %s\n", argv[3]);
+	if (IS_ENABLED(CONFIG_NET_IPV4_ROUTE) && addr.ss_family == NET_AF_INET) {
+		struct net_in_addr gw4 = {0};
+
+		if (net_addr_pton(NET_AF_INET, argv[3], &gw4)) {
+			PR_ERROR("Invalid gateway: %s\n", argv[3]);
+			return -EINVAL;
+		}
+
+		route = net_route_ipv4_add(iface, &net_sin(net_sad(&addr))->sin_addr,
+					   mask_len,
+					   &gw4, NET_ROUTE_INFINITE_LIFETIME,
+					   NET_ROUTE_PREFERENCE_MEDIUM);
+
+	} else if (IS_ENABLED(CONFIG_NET_IPV6_ROUTE) && addr.ss_family == NET_AF_INET6) {
+		struct net_in6_addr gw6 = {0};
+
+		if (net_addr_pton(NET_AF_INET6, argv[3], &gw6)) {
+			PR_ERROR("Invalid gateway: %s\n", argv[3]);
+			return -EINVAL;
+		}
+
+		route = net_route_ipv6_add(iface, &net_sin6(net_sad(&addr))->sin6_addr,
+					   mask_len,
+					   &gw6, NET_IPV6_ND_INFINITE_LIFETIME,
+					   NET_ROUTE_PREFERENCE_MEDIUM);
+	} else {
+		PR("Unknown route family %d\n", addr.ss_family);
 		return -EINVAL;
 	}
 
-	route = net_route_ipv6_add(iface, &prefix, NET_IPV6_DEFAULT_PREFIX_LEN,
-				   &gw, NET_IPV6_ND_INFINITE_LIFETIME,
-				   NET_ROUTE_PREFERENCE_MEDIUM);
 	if (route == NULL) {
 		PR_ERROR("Failed to add route\n");
 		return -ENOEXEC;
 	}
 
-#else /* CONFIG_NET_NATIVE_IPV6 && CONFIG_NET_IPV6_ROUTE */
+#else /* NATIVE_ROUTE */
+
 	PR_INFO("Set %s and %s to enable native %s support."
-			" And enable CONFIG_NET_IPV6_ROUTE.\n",
-			"CONFIG_NET_NATIVE", "CONFIG_NET_IPV6", "IPv6");
-#endif /* CONFIG_NET_NATIVE_IPV6 && CONFIG_NET_IPV6_ROUTE */
+		" And enable CONFIG_NET_IPV6_ROUTE or CONFIG_NET_IPV4_ROUTE.\n",
+		"CONFIG_NET_NATIVE", "CONFIG_NET_IPV6 or CONFIG_NET_IPV4", "IPv6 or IPv4");
+
+#endif /* NATIVE_ROUTE */
+
 	return 0;
 }
 
-static int cmd_net_ip6_route_del(const struct shell *sh, size_t argc, char *argv[])
+static int cmd_net_route_del(const struct shell *sh, size_t argc, char *argv[])
 {
-#if defined(CONFIG_NET_NATIVE_IPV6) && (CONFIG_NET_IPV6_ROUTE)
+#if defined(NATIVE_ROUTE)
 	struct net_if *iface = NULL;
 	int idx;
 	struct net_route_entry *route;
-	struct net_in6_addr prefix = { 0 };
+	struct net_sockaddr_storage addr;
+	const char *str;
+	uint8_t mask_len;
 
 	if (argc != 3) {
 		PR_ERROR("Correct usage: net route del <index> <destination>\n");
 		return -EINVAL;
 	}
+
 	idx = get_iface_idx(sh, argv[1]);
 	if (idx < 0) {
 		return -ENOEXEC;
@@ -170,20 +307,38 @@ static int cmd_net_ip6_route_del(const struct shell *sh, size_t argc, char *argv
 		return -ENOEXEC;
 	}
 
-	if (net_addr_pton(NET_AF_INET6, argv[2], &prefix)) {
-		PR_ERROR("Invalid address: %s\n", argv[2]);
+	str = net_ipaddr_parse_mask(argv[2], strlen(argv[2]), net_sad(&addr), &mask_len);
+	if (str == NULL) {
+		PR_ERROR("Invalid destination: %s\n", argv[2]);
 		return -EINVAL;
 	}
 
-	route = net_route_ipv6_lookup(iface, &prefix);
-	if (route) {
-		net_route_ipv6_del(route);
+	if (IS_ENABLED(CONFIG_NET_IPV4_ROUTE) && addr.ss_family == NET_AF_INET) {
+		route = net_route_ipv4_lookup(iface, &net_sin(net_sad(&addr))->sin_addr);
+		if (route != NULL) {
+			net_route_ipv4_del(route);
+		}
+
+	} else if (IS_ENABLED(CONFIG_NET_IPV6_ROUTE) && addr.ss_family == NET_AF_INET6) {
+		route = net_route_ipv6_lookup(iface, &net_sin6(net_sad(&addr))->sin6_addr);
+		if (route != NULL) {
+			net_route_ipv6_del(route);
+		}
 	}
-#else /* CONFIG_NET_NATIVE_IPV6 && CONFIG_NET_IPV6_ROUTE */
+
+	if (route == NULL) {
+		PR_ERROR("No such route \"%s\" found\n", argv[2]);
+		return -ENOEXEC;
+	}
+
+#else /* NATIVE_ROUTE */
+
 	PR_INFO("Set %s and %s to enable native %s support."
-			" And enable CONFIG_NET_IPV6_ROUTE\n",
-			"CONFIG_NET_NATIVE", "CONFIG_NET_IPV6", "IPv6");
-#endif /* CONFIG_NET_NATIVE_IPV6 && CONFIG_NET_IPV6_ROUTE */
+		" And enable CONFIG_NET_IPV6_ROUTE or CONFIG_NET_IPV4_ROUTE.\n",
+		"CONFIG_NET_NATIVE", "CONFIG_NET_IPV6 or CONFIG_NET_IPV4", "IPv6 or IPv4");
+
+#endif /* NATIVE_ROUTE */
+
 	return 0;
 }
 
@@ -192,25 +347,27 @@ static int cmd_net_route(const struct shell *sh, size_t argc, char *argv[])
 	ARG_UNUSED(argc);
 	ARG_UNUSED(argv);
 
-#if defined(CONFIG_NET_NATIVE)
-#if defined(CONFIG_NET_IPV6_ROUTE) || defined(CONFIG_NET_IPV6_ROUTE_MCAST)
+#if defined(NATIVE_ROUTE) || defined(CONFIG_NET_IPV6_ROUTE_MCAST)
 	struct net_shell_user_data user_data;
-#endif
 
-#if defined(CONFIG_NET_IPV6_ROUTE) || defined(CONFIG_NET_IPV6_ROUTE_MCAST)
 	user_data.sh = sh;
 #endif
 
-#if defined(CONFIG_NET_IPV6_ROUTE)
+#if defined(NATIVE_ROUTE)
 	net_if_foreach(iface_per_route_cb, &user_data);
-#else
-	PR_INFO("Set %s to enable %s support.\n", "CONFIG_NET_IPV6_ROUTE",
-		"network route");
 #endif
 
 #if defined(CONFIG_NET_IPV6_ROUTE_MCAST)
 	net_route_ipv6_mcast_foreach(route_mcast_cb, NULL, &user_data);
 #endif
+
+#if !defined(CONFIG_NET_IPV6_ROUTE)
+	PR_INFO("Set %s to enable %s support.\n", "CONFIG_NET_IPV6_ROUTE",
+		"network route");
+#endif
+#if !defined(CONFIG_NET_IPV4_ROUTE)
+	PR_INFO("Set %s to enable %s support.\n", "CONFIG_NET_IPV4_ROUTE",
+		"network route");
 #endif
 	return 0;
 }
@@ -219,11 +376,11 @@ SHELL_STATIC_SUBCMD_SET_CREATE(net_cmd_route,
 	SHELL_CMD(add, NULL,
 		  SHELL_HELP("Adds the route to the destination",
 			     "<index> <destination> <gateway>"),
-		  cmd_net_ip6_route_add),
+		  cmd_net_route_add),
 	SHELL_CMD(del, NULL,
 		  SHELL_HELP("Deletes the route to the destination",
 			     "<index> <destination>"),
-		  cmd_net_ip6_route_del),
+		  cmd_net_route_del),
 	SHELL_SUBCMD_SET_END
 );
 
