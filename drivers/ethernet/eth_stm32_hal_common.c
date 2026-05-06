cf9ef7b13f1981b43e048961b5647399b9ffe117
Erwan SZYMANSKI <erwan.szymanski@st.com>
2026-05-06 10:28:02 +0200

    drivers: ethernet: eth_stm32_hal_common: multi instances support
    Adapt the driver to support Ethernet controller multi instances.
    This adaptation is aligned with Zephyr method used for other
    drivers.
    Now, for each MAC node in DT targetting this driver, a probe
    will be processed while a single one was done before.
    
    Signed-off-by: Erwan SZYMANSKI <erwan.szymanski@st.com>

--- drivers/ethernet/eth_stm32_hal_common.c
+++ drivers/ethernet/eth_stm32_hal_common.c
@@ -41,9 +41,6 @@ LOG_MODULE_REGISTER(eth_stm32_hal, CONFIG_ETHERNET_LOG_LEVEL);
 #define ETH_STM32_HAL_MTU NET_ETH_MTU
 #define ETH_STM32_HAL_FRAME_SIZE_MAX (ETH_STM32_HAL_MTU + 18)
 
-static struct eth_stm32_dma_buf eth0_dma_buf __eth_stm32_buf;
-static struct eth_stm32_dma_desc eth0_dma_desc __eth_stm32_desc;
-
 static void rx_thread(void *arg1, void *unused1, void *unused2)
 {
 	const struct device *dev = (const struct device *)arg1;
@@ -337,61 +334,97 @@ static const struct ethernet_api eth_api = {
 #endif /* CONFIG_NET_STATISTICS_ETHERNET */
 };
 
-static void eth0_irq_config(void)
-{
-	IRQ_CONNECT(DT_INST_IRQN(0), DT_INST_IRQ(0, priority), eth_isr,
-		    DEVICE_DT_INST_GET(0), 0);
-	irq_enable(DT_INST_IRQN(0));
-}
+#define ETH_STM32_HAS_PTP_CLOCK(n) \
+	DT_CLOCKS_HAS_NAME(DT_DRV_INST(n), mac_clk_ptp)
 
-PINCTRL_DT_INST_DEFINE(0);
+#define ETH_STM32_HAL_COMMON_DMA_BUF_DEFN(n) \
+	static struct eth_stm32_dma_buf eth##n##_dma_buf __eth_stm32_buf
 
-static const struct stm32_pclken eth0_pclken[] = STM32_DT_INST_CLOCKS(0);
+#define ETH_STM32_HAL_COMMON_DMA_DESC_DEFN(n) \
+	static struct eth_stm32_dma_desc eth##n##_dma_desc __eth_stm32_desc
 
-#define ETH_STM32_HAS_PTP_CLOCK	DT_INST_CLOCKS_HAS_NAME(0, mac_clk_ptp)
+#define ETH_STM32_HAL_COMMON_IRQ_CONFIG_DEFN(n)                          \
+	static void eth##n##_irq_config(void)                            \
+	{                                                                 \
+		IRQ_CONNECT(DT_INST_IRQN(n), DT_INST_IRQ(n, priority),    \
+			    eth_isr, DEVICE_DT_INST_GET(n), 0);          \
+		irq_enable(DT_INST_IRQN(n));                              \
+	}
 
-static const struct eth_stm32_hal_dev_cfg eth0_config = {
-	.config_func = eth0_irq_config,
-	.pclken = eth0_pclken,
-	.pclken_cnt = DT_INST_NUM_CLOCKS(0),
-#ifdef CONFIG_PTP_CLOCK_STM32_HAL
-	/* If no dedicated PTP clock is defined, fall back to the MAC bus clock. */
-	.rate_pclken_idx = DT_PHA_ELEM_IDX_BY_NAME(DT_DRV_INST(0), clocks,
-						   COND_CODE_1(ETH_STM32_HAS_PTP_CLOCK,
-							       (mac_clk_ptp), (stm_eth))),
-#endif
-	.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(0),
-	.mac_cfg = NET_ETH_MAC_DT_INST_CONFIG_INIT(0),
-	.phy_dev = DEVICE_DT_GET(DT_INST_PHANDLE(0, phy_handle)),
-	.dma_buf = &eth0_dma_buf,
-	.dma_desc = &eth0_dma_desc,
-};
+#define ETH_STM32_HAL_COMMON_PINCTRL_DEFN(n) \
+	PINCTRL_DT_INST_DEFINE(n)
 
-BUILD_ASSERT(DT_INST_ENUM_HAS_VALUE(0, phy_connection_type, mii)
-	|| DT_INST_ENUM_HAS_VALUE(0, phy_connection_type, rmii)
-	IF_ENABLED(DT_HAS_COMPAT_STATUS_OKAY(st_stm32n6_ethernet),
-	(|| DT_INST_ENUM_HAS_VALUE(0, phy_connection_type, rgmii)
-	 || DT_INST_ENUM_HAS_VALUE(0, phy_connection_type, gmii))),
-			"Unsupported PHY connection type");
+#define ETH_STM32_HAL_COMMON_PCLK_DEFN(n)                                 \
+	static const struct stm32_pclken eth##n##_pclken[] =               \
+		STM32_DT_CLOCKS(DT_DRV_INST(n))
 
-static struct eth_stm32_hal_dev_data eth0_data = {
-	.heth = {
-		.Instance = (ETH_TypeDef *)DT_INST_REG_ADDR(0),
-		.Init = {
-#if defined(CONFIG_ETH_STM32_HAL_API_V1)
-			.RxMode = ETH_RXINTERRUPT_MODE,
-			.ChecksumMode = IS_ENABLED(CONFIG_ETH_STM32_HW_CHECKSUM) ?
-					ETH_CHECKSUM_BY_HARDWARE : ETH_CHECKSUM_BY_SOFTWARE,
-#endif /* CONFIG_ETH_STM32_HAL_API_V1 */
-			.MediaInterface = STM32_ETH_PHY_MODE(0),
+#define ETH_STM32_HAL_COMMON_CFG_DEFN(n)                                  \
+	static const struct eth_stm32_hal_dev_cfg eth##n##_config = {     \
+		.config_func = eth##n##_irq_config,                       \
+		.pclken = eth##n##_pclken,                                \
+		.pclken_cnt = DT_NUM_CLOCKS(DT_DRV_INST(n)),           \
+		.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(n),                \
+		.mac_cfg = NET_ETH_MAC_DT_INST_CONFIG_INIT(n),            \
+		.phy_dev = DEVICE_DT_GET(DT_INST_PHANDLE(n, phy_handle)), \
+		.dma_buf = &eth##n##_dma_buf,                             \
+		.dma_desc = &eth##n##_dma_desc,                           \
+		IF_ENABLED(CONFIG_PTP_CLOCK_STM32_HAL,                    \
+			(.rate_pclken_idx = DT_PHA_ELEM_IDX_BY_NAME(      \
+				DT_DRV_INST(n), clocks,                  \
+				COND_CODE_1(ETH_STM32_HAS_PTP_CLOCK(n),     \
+					    (mac_clk_ptp), (stm_eth))),))     \
+	}
+
+#define ETH_STM32_HAL_COMMON_BUILD_ASSERT(n)                              \
+	BUILD_ASSERT(                                                     \
+		DT_INST_ENUM_HAS_VALUE(n, phy_connection_type, mii) ||    \
+		DT_INST_ENUM_HAS_VALUE(n, phy_connection_type, rmii)      \
+		IF_ENABLED(DT_HAS_COMPAT_STATUS_OKAY(st_stm32n6_ethernet), \
+			(|| DT_INST_ENUM_HAS_VALUE(n, phy_connection_type, rgmii) \
+			 || DT_INST_ENUM_HAS_VALUE(n, phy_connection_type, gmii))), \
+		"Unsupported PHY connection type")
+
 #if DT_HAS_COMPAT_STATUS_OKAY(st_stm32mp13_ethernet)
-			.ClockSelection = DT_INST_PROP(0, st_ext_phyclk) ? HAL_ETH_REF_CLK_RCC
-							     : HAL_ETH_REF_CLK_RX_CLK_PIN,
+#define ETH_STM32_HAL_COMMON_CLOCK_SELECTION(inst)                         \
+	.ClockSelection =                                                  \
+		(DT_INST_PROP(inst, st_ext_phyclk) ?                       \
+			HAL_ETH_REF_CLK_RCC :                              \
+			HAL_ETH_REF_CLK_RX_CLK_PIN),
+#else
+#define ETH_STM32_HAL_COMMON_CLOCK_SELECTION(inst)
 #endif
-		},
-	},
-};
 
-ETH_NET_DEVICE_DT_INST_DEFINE(0, eth_initialize,
-		    NULL, &eth0_data, &eth0_config,
-		    CONFIG_ETH_INIT_PRIORITY, &eth_api, ETH_STM32_HAL_MTU);
+#define ETH_STM32_HAL_COMMON_DATA_DEFN(n)                                 \
+	static struct eth_stm32_hal_dev_data eth##n##_data = {            \
+		.heth = {                                                  \
+			.Instance = (ETH_TypeDef *)DT_INST_REG_ADDR(n),\
+			.Init = {                                          \
+				IF_ENABLED(CONFIG_ETH_STM32_HAL_API_V1,     \
+					(.RxMode = ETH_RXINTERRUPT_MODE,    \
+					 .ChecksumMode =                  \
+						IS_ENABLED(CONFIG_ETH_STM32_HW_CHECKSUM) ? \
+						ETH_CHECKSUM_BY_HARDWARE : \
+						ETH_CHECKSUM_BY_SOFTWARE,)) \
+				.MediaInterface = STM32_ETH_PHY_MODE(n), \
+				ETH_STM32_HAL_COMMON_CLOCK_SELECTION(n) \
+			},                                                 \
+		},                                                         \
+	}
+
+#define ETH_STM32_HAL_COMMON_DT_INST_DEFN(n)                              \
+	ETH_NET_DEVICE_DT_INST_DEFINE(n, eth_initialize, NULL,            \
+		&eth##n##_data, &eth##n##_config,                         \
+		CONFIG_ETH_INIT_PRIORITY, &eth_api, ETH_STM32_HAL_MTU)
+
+#define ETH_STM32_HAL_COMMON_DEVICE(n)                                    \
+	ETH_STM32_HAL_COMMON_DMA_BUF_DEFN(n);                              \
+	ETH_STM32_HAL_COMMON_DMA_DESC_DEFN(n);                             \
+	ETH_STM32_HAL_COMMON_IRQ_CONFIG_DEFN(n);                           \
+	ETH_STM32_HAL_COMMON_PINCTRL_DEFN(n);                              \
+	ETH_STM32_HAL_COMMON_PCLK_DEFN(n);                                 \
+	ETH_STM32_HAL_COMMON_CFG_DEFN(n);                                  \
+	ETH_STM32_HAL_COMMON_BUILD_ASSERT(n);                              \
+	ETH_STM32_HAL_COMMON_DATA_DEFN(n);                                 \
+	ETH_STM32_HAL_COMMON_DT_INST_DEFN(n);
+
+DT_INST_FOREACH_STATUS_OKAY(ETH_STM32_HAL_COMMON_DEVICE)
