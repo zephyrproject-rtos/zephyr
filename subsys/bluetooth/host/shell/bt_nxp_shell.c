eb83031b3877f73d2163b99913c3b09af389a436
Vinit Mehta <vinit.mehta@nxp.com>
2026-05-07 21:01:27 +0530

    [nxp noup] bt: Update the shell API for IR
    -Update the shell api prototype to align with
     upstream driver changes
    
    Signed-off-by: Vinit Mehta <vinit.mehta@nxp.com>

--- subsys/bluetooth/host/shell/bt_nxp_shell.c
+++ subsys/bluetooth/host/shell/bt_nxp_shell.c
@@ -5,11 +5,15 @@
  */
 
 #include <zephyr/shell/shell.h>
+#include <zephyr/drivers/bluetooth.h>
 #include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/settings/settings.h>
 
-/* TODO: Move to proper header file */
+#define DT_DRV_COMPAT nxp_bt_hci_uart
+
 #if defined(CONFIG_HCI_NXP_CONFIG_IR)
-extern void bt_nxp_trigger_ir(void);
+extern int bt_nxp_trigger_ir(const struct device *dev);
 #endif
 
 static int cmd_default_handler(const struct shell *sh, size_t argc, char **argv)
@@ -26,9 +30,28 @@ static int cmd_default_handler(const struct shell *sh, size_t argc, char **argv)
 #if defined(CONFIG_HCI_NXP_CONFIG_IR)
 static int cmd_trigger_ir(const struct shell *sh, size_t argc, char *argv[])
 {
-	bt_nxp_trigger_ir();
-	shell_print(sh, "IR configuration triggered");
-	return 0;
+	int err = 0;
+	static const struct device *uart_dev = DEVICE_DT_GET(DT_INST_GPARENT(0));
+
+	if (!device_is_ready(uart_dev)) {
+		err = -ENODEV;
+	} else {
+		shell_print(sh, "Triggering IR");
+		err = bt_nxp_trigger_ir(uart_dev);
+		if (err) {
+			shell_error(sh, "IR Trigger failed (err %d)", err);
+		} else {
+			shell_print(sh, "IR recovery done, Load BT settings");
+			err = settings_load();
+			if (err) {
+				shell_error(sh, "settings load failed after IR (err %d)", err);
+			} else {
+				shell_print(sh, "BT Init success after IR operation");
+			}
+		}
+	}
+
+	return err;
 }
 #endif
 
