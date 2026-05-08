6f35cafa73744699315ab0506bf8fe77695719af
Joel Holdsworth <jholdsworth@nvidia.com>
2026-05-08 09:44:30 -0700

    arch: openrisc: add LLEXT ELF relocation support
    Implement arch_elf_relocate() for the OpenRISC architecture to enable
    the LLEXT (Linkable Loadable Extensions) subsystem.
    
    Supported relocation types:
    - R_OR1K_32, R_OR1K_32_PCREL (data)
    - R_OR1K_INSN_REL_26 (26-bit PC-relative branch/jump)
    - R_OR1K_HI_16_IN_INSN, R_OR1K_LO_16_IN_INSN (address pair)
    - R_OR1K_AHI16 (adjusted high, sign-compensated)
    - R_OR1K_SLO16 (split-immediate store encoding)
    - R_OR1K_PCREL_PG21, R_OR1K_LO13, R_OR1K_SLO13 (page-relative)
    - R_OR1K_TLS_LE_HI16, R_OR1K_TLS_LE_LO16, R_OR1K_TLS_LE_AHI16,
      R_OR1K_TLS_LE_SLO16, R_OR1K_TLS_TPOFF (TLS local-exec)
    
    Signed-off-by: Joel Holdsworth <jholdsworth@nvidia.com>

--- cmake/compiler/gcc/target_openrisc.cmake
+++ cmake/compiler/gcc/target_openrisc.cmake
@@ -5,3 +5,18 @@
 # script legitimately needs RWX permissions.  Suppress the ld >= 2.39
 # warning that would otherwise become an error with --fatal-warnings.
 list(APPEND TOOLCHAIN_LD_FLAGS -Wl,--no-warn-rwx-segments)
+
+# Flags not supported by llext linker
+# (regexps are supported and match whole word)
+set(LLEXT_REMOVE_FLAGS
+  -fno-pic
+  -fno-pie
+  -ffunction-sections
+  -fdata-sections
+  -g.*
+  -Os
+  )
+
+# Flags to be added to llext code compilation
+set(LLEXT_APPEND_FLAGS
+  )
