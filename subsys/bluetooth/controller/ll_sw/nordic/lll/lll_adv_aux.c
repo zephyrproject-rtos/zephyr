zephyrproject-rtos/zephyr

commit f64d123a3d7495632dfb64733c2e22a5c7990517

Bluetooth: Controller: Fix missing ext adv terminate event
Fix missing Extended Advertising terminate event and
advertising scheduling not being stopped.

Under race conditions, auxiliary event is aborted without
the generation  of done extra event which is suppose to
stop the scheduling when max events count is used.

The fix now generates the done extra event.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_aux.c
@@ -49,6 +49,9 @@
 
 static int init_reset(void);
 static int prepare_cb(struct lll_prepare_param *p);
+#if !defined(CONFIG_BT_TICKER_EXT_EXPIRE_INFO)
+static void isr_early_abort(void *param);
+#endif /* !CONFIG_BT_TICKER_EXT_EXPIRE_INFO */
 static void isr_done(void *param);
 #if defined(CONFIG_BT_CTLR_ADV_AUX_PDU_BACK2BACK)
 static void isr_tx_chain(void *param);
@@ -188,7 +191,7 @@ static int prepare_cb(struct lll_prepare_param *p)
 
 	/* Abort if no aux_ptr filled */
 	if (unlikely(!pri_hdr->aux_ptr || !PDU_ADV_AUX_PTR_OFFSET_GET(aux_ptr))) {
-		radio_isr_set(lll_isr_early_abort, lll);
+		radio_isr_set(isr_early_abort, lll);
 		radio_disable();
 
 		return 0;
@@ -325,7 +328,7 @@ static int prepare_cb(struct lll_prepare_param *p)
 	if (overhead) {
 		LL_ASSERT_OVERHEAD(overhead);
 
-		radio_isr_set(lll_isr_abort, lll);
+		radio_isr_set(isr_done, lll);
 		radio_disable();
 
 		return -ECANCELED;
@@ -346,6 +349,33 @@ static int prepare_cb(struct lll_prepare_param *p)
 
 	return 0;
 }
+
+#if !defined(CONFIG_BT_TICKER_EXT_EXPIRE_INFO)
+static void isr_race(void *param)
+{
+	radio_status_reset();
+}
+
+static void isr_early_abort(void *param)
+{
+	struct event_done_extra *extra;
+	int err;
+
+	/* Generate auxiliary radio event done */
+	extra = ull_done_extra_type_set(EVENT_DONE_EXTRA_TYPE_ADV_AUX);
+	LL_ASSERT(extra);
+
+	radio_isr_set(isr_race, param);
+	if (!radio_is_idle()) {
+		radio_disable();
+	}
+
+	err = lll_hfclock_off();
+	LL_ASSERT(err >= 0);
+
+	lll_done(NULL);
+}
+#endif /* !CONFIG_BT_TICKER_EXT_EXPIRE_INFO */
 
 static void isr_done(void *param)
 {

commit ca907beda79418de391898cb558245717e72af11

Bluetooth: Controller: Fix spurious radio disabled ISR
Fix spurious radio disabled ISR execution due to missing
clear of radio disabled event.

Regression in commit f64d123a3d74 ("Bluetooth: Controller:
Fix missing ext adv terminate event").

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_aux.c
@@ -399,29 +399,15 @@ static int aux_ptr_get(struct pdu_adv *pdu, struct pdu_adv_aux_ptr **aux_ptr)
 	*/
 
 #if !defined(CONFIG_BT_TICKER_EXT_EXPIRE_INFO)
-static void isr_race(void *param)
-{
-	radio_status_reset();
-}
-
 static void isr_early_abort(void *param)
 {
 	struct event_done_extra *extra;
-	int err;
 
 	/* Generate auxiliary radio event done */
 	extra = ull_done_extra_type_set(EVENT_DONE_EXTRA_TYPE_ADV_AUX);
 	LL_ASSERT_ERR(extra);
 
-	radio_isr_set(isr_race, param);
-	if (!radio_is_idle()) {
-		radio_disable();
-	}
-
-	err = lll_hfclock_off();
-	LL_ASSERT_ERR(err >= 0);
-
-	lll_done(NULL);
+	lll_isr_early_abort(param);
 }
 #endif /* !CONFIG_BT_TICKER_EXT_EXPIRE_INFO */
 