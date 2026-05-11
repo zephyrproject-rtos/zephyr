b35b74b8e047951e49d0178443b32af477b61c92
Johan Hedberg <johan.hedberg@silabs.com>
2026-05-11 21:33:48 +0300

    Bluetooth: Host: Get rid of public bt_hci_recv functions
    Make these static (no need to export them) and also name them in a way that
    doesn't class with the HCI driver API prefix.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@silabs.com>

--- subsys/bluetooth/host/hci_core.c
+++ subsys/bluetooth/host/hci_core.c
@@ -4545,7 +4545,7 @@ static int bt_recv_unsafe(struct net_buf *buf)
 	}
 }
 
-int bt_hci_recv(const struct device *dev, struct net_buf *buf)
+static int bt_recv(const struct device *dev, struct net_buf *buf)
 {
 	ARG_UNUSED(dev);
 	int err;
@@ -4740,7 +4740,7 @@ int bt_enable(bt_ready_cb_t cb)
 	k_thread_name_set(&bt_workq.thread, "BT RX WQ");
 #endif
 
-	err = bt_hci_open(bt_dev.hci, bt_hci_recv);
+	err = bt_hci_open(bt_dev.hci, bt_recv);
 	if (err) {
 		LOG_ERR("HCI driver open failed (%d)", err);
 		return err;
