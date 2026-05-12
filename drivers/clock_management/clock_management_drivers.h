8d490be866605547f68d2221de2fee9f323b8572
Eric Hay <Eric.Hay@analog.com>
2026-05-12 16:26:56 -0700

    drivers: clock_management: Promote the NXP clock gate driver
    Promote the NXP syscon clock gate driver to be a generic driver.
    
    A clock gate is an entity that can inhibit or pass a clock signal from a
    source. This generic driver expects a clock gate to be controlled by a
    single bit (1 to enable, 0 to disable) in the parent register space.
    
    Signed-off-by: Eric Hay <Eric.Hay@analog.com>

--- drivers/clock_management/clock_management_drivers.h
+++ drivers/clock_management/clock_management_drivers.h
@@ -21,6 +21,10 @@ extern "C" {
 #define Z_CLOCK_MANAGEMENT_DATA_GET_clock_source(node_id, prop, idx)      \
 	DT_PHA_BY_IDX(node_id, prop, idx, gate)
 
+#define Z_CLOCK_MANAGEMENT_DATA_DEFINE_clock_gate(node_id, prop, idx)
+#define Z_CLOCK_MANAGEMENT_DATA_GET_clock_gate(node_id, prop, idx)      \
+	DT_PHA_BY_IDX(node_id, prop, idx, gate)
+
 /** @endcond */
 
 #ifdef __cplusplus
