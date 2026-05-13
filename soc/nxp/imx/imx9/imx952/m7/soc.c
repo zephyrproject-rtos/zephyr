2fce64f8e60bca15892f9c5f1c1b4b81b38f6f8f
Qiang Zhao <qiang.zhao@nxp.com>
2026-05-13 11:43:53 +0530

    soc: nxp: imx9: add i.MX952 SoC support
    Add support for NXP i.MX952 SoC Cortex-M7 core:
    - Add SoC configuration and Kconfig files
    - Add M7 core specific initialization code
    - Add pinctrl definitions for i.MX952
    - Add SCMI CPU management support
    - Update imx9 CMakeLists to include imx952 subdirectory
    - Register imx952 series in soc.yml
    
    Signed-off-by: Qiang Zhao <qiang.zhao@nxp.com>

---
+++ soc/nxp/imx/imx9/imx952/m7/soc.c
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2026 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/cache.h>
+#include <soc.h>
+
+void soc_early_init_hook(void)
+{
+#ifdef CONFIG_CACHE_MANAGEMENT
+	sys_cache_data_enable();
+	sys_cache_instr_enable();
+#endif
+}
