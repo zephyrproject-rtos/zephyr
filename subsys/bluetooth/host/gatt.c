58ad85236a8c16f19b85a96efbc8b000ef3bc18d
Johan Hedberg <johan.hedberg@silabs.com>
2026-05-13 16:12:22 +0300

    Bluetooth: Host: GATT: Fix missing check for minimum read response size
    There's a for loop in parse_read_std_char_desc() that'll go on forever if
    rsp->len is 0. Add a minimum length check to the beginning of it to avoid
    this issue.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@silabs.com>
    (cherry picked from commit 494283d469a95b294badaf45c639de433bf5e35a)

--- subsys/bluetooth/host/gatt.c
+++ subsys/bluetooth/host/gatt.c
@@ -4224,6 +4224,7 @@ static uint16_t parse_read_std_char_desc(struct bt_conn *conn, const void *pdu,
 					 uint16_t length)
 {
 	const struct bt_att_read_type_rsp *rsp;
+	const struct bt_att_data *data;
 	uint16_t handle = 0U;
 	uint16_t uuid_val;
 
@@ -4240,6 +4241,11 @@ static uint16_t parse_read_std_char_desc(struct bt_conn *conn, const void *pdu,
 
 	rsp = pdu;
 
+	if (rsp->len < sizeof(*data)) {
+		LOG_WRN("Invalid data len %u", rsp->len);
+		goto done;
+	}
+
 	/* Parse characteristics found */
 	for (length--, pdu = rsp->data; length >= rsp->len;
 	     length -= rsp->len, pdu = (const uint8_t *)pdu + rsp->len) {
@@ -4249,7 +4255,6 @@ static uint16_t parse_read_std_char_desc(struct bt_conn *conn, const void *pdu,
 			struct bt_gatt_cep cep;
 			struct bt_gatt_scc scc;
 		} value;
-		const struct bt_att_data *data;
 		struct bt_gatt_attr attr;
 
 		if (length < sizeof(*data)) {
