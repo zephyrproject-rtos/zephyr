d7fbc979c446156ad4fd0ab599c81f5f8079cb63
Surya Prakash T <suryat@aerlync.com>
2026-05-15 12:05:28 +0530

    fs: fix FUSE open permissions and validate flags
    ffa_create_top() was opening files with FS_O_CREATE | FS_O_WRITE
    only, which caused host applications like 'cat' to fail with
    Permission denied when trying to read the file through FUSE. This
    is because FAT filesystem explicitly checks the READ flag before
    allowing read access.
    
    Fix by replacing FS_O_WRITE with FS_O_RDWR so the file is opened
    with both read and write access.
    
    Signed-off-by: Surya Prakash T <suryat@aerlync.com>
    (cherry picked from commit 8f959ad0e6b8ead3861cb23e65d627d4224b7f7a)

--- subsys/fs/fuse_fs_access.c
+++ subsys/fs/fuse_fs_access.c
@@ -271,7 +271,7 @@ static int fuse_fs_access_create(const char *path, mode_t mode,
 
 	fi->fh = handle;
 
-	err = fs_open(&files[handle], path, FS_O_CREATE | FS_O_WRITE);
+	err = fs_open(&files[handle], path, FS_O_CREATE | FS_O_RDWR);
 	if (err != 0) {
 		release_file_handle(handle);
 		fi->fh = INVALID_FILE_HANDLE;
