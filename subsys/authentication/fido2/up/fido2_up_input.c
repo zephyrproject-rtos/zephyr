11c8d3172f162b8645dee4fce86214b626e9ffa2
Siratul Islam <email@sirat.me>
2026-05-17 19:35:36 +0600

    fido2: implement transport progess indicator
    - Add CTAPHID Keepalive
    
    Signed-off-by: Siratul Islam <email@sirat.me>

--- subsys/authentication/fido2/up/fido2_up_input.c
+++ subsys/authentication/fido2/up/fido2_up_input.c
@@ -7,11 +7,13 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/input/input.h>
+#include <zephyr/sys/atomic.h>
 #include <zephyr/authentication/fido2/fido2_up.h>
 
 LOG_MODULE_DECLARE(fido2, CONFIG_FIDO2_LOG_LEVEL);
 
 static K_SEM_DEFINE(up_sem, 0, 1);
+static atomic_t up_cancelled;
 
 static void up_input_cb(struct input_event *evt, void *user_data)
 {
@@ -28,6 +30,7 @@ int fido2_up_wait(void)
 {
 	int ret;
 
+	atomic_clear(&up_cancelled);
 	k_sem_reset(&up_sem);
 
 	ret = k_sem_take(&up_sem, K_MSEC(CONFIG_FIDO2_UP_TIMEOUT_MS));
@@ -35,5 +38,15 @@ int fido2_up_wait(void)
 		return -ETIMEDOUT;
 	}
 
+	if (atomic_get(&up_cancelled)) {
+		return -ECANCELED;
+	}
+
 	return 0;
+}
+
+void fido2_up_cancel(void)
+{
+	atomic_set(&up_cancelled, 1);
+	k_sem_give(&up_sem);
 }
