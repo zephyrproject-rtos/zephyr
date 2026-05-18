59fe7c840d3c064cb2b220b90867853dd721c0b8
Aksel Skauge Mellbye <aksel.mellbye@silabs.com>
2026-05-18 16:23:47 +0200

    boards: silabs: Add sixg301_rb4407a radio board
    Add radio board for Six301 SoC family. The board uses the
    SiMG301 SoC with 4MB flash and 512 kB RAM.
    
    Signed-off-by: Aksel Skauge Mellbye <aksel.mellbye@silabs.com>

---
+++ boards/silabs/radio_boards/six301/board.cmake
@@ -0,0 +1,5 @@
+# Copyright The Zephyr Project Contributors
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(silabs_commander "--device=${CONFIG_SOC}")
+include(${ZEPHYR_BASE}/boards/common/silabs_commander.board.cmake)
