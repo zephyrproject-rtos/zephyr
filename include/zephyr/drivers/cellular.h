415e8273d14b37581aa835eb91faf0abd22c1387
Jordan Yates <jordan@embeint.com>
2026-05-18 11:11:50 +1000

    modem: cellular: network status event
    Add an event for broadcasting the current network status (Cell tower
    info) to application users. There is no common 3GPP AT command that
    provides this information, but vendor-specific commands appear to exist
    for most modems. e.g. `AT%XMONITOR` for Nordic and `AT#RFSTS` for Telit.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- include/zephyr/drivers/cellular.h
+++ include/zephyr/drivers/cellular.h
@@ -141,6 +141,8 @@ enum cellular_event {
 	CELLULAR_EVENT_REGISTRATION_STATUS_CHANGED = BIT(1),
 	/** Result of a communications link check to the modem */
 	CELLULAR_EVENT_MODEM_COMMS_CHECK_RESULT = BIT(2),
+	/** Cellular network status changed */
+	CELLULAR_EVENT_NETWORK_STATUS_CHANGED = BIT(3),
 };
 
 /* Opaque bit-mask large enough for all current & future events */
@@ -159,6 +161,30 @@ struct cellular_evt_registration_status {
 /** Payload for @ref CELLULAR_EVENT_MODEM_COMMS_CHECK_RESULT */
 struct cellular_evt_modem_comms_check_result {
 	bool success; /**< Communications to modem checked successfully */
+};
+
+/** E-UTRAN Global Cell Identifier (GCI) is a 28bit value */
+#define CELLULAR_EUTRAN_CELL_ID_MAX     0xFFFFFFFU
+/** Unknown E-UTRAN Global Cell Identifier (GCI) */
+#define CELLULAR_EUTRAN_CELL_ID_INVALID UINT32_MAX
+
+/** Payload for @ref CELLULAR_EVENT_NETWORK_STATUS_CHANGED */
+struct cellular_evt_network_status {
+	enum cellular_registration_status status; /**< Registration status */
+	enum cellular_access_technology access_tech; /**< Access technology */
+	union {
+		struct {
+			uint16_t mcc; /**< Mobile Country Code */
+			uint16_t mnc; /**< Mobile Network Code */
+			uint32_t tac; /**< Tracking Area Code */
+			uint32_t earfcn; /**< E-UTRAN assigned radio channel */
+			uint32_t gci; /**< E-UTRAN cell ID (0 - CELLULAR_EUTRAN_CELL_ID_MAX) */
+			uint16_t phys_cell_id; /**< Physical cell ID */
+			uint16_t band; /**< Band number (from 3GPP TS 36.101) */
+			int16_t rsrp; /**< Received signal power (dBm) */
+			int8_t rsrq; /**< Received signal quality (dB) */
+		} lte; /**< LTE Cell information */
+	} cell; /**< Generic Cell information */
 };
 
 /**
