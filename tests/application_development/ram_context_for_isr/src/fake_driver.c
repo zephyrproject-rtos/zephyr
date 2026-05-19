bb2226d8e9436f279fa1be10f978ea7cff8d364b
Etienne Carriere <etienne.carriere@st.com>
2026-05-19 15:21:13 +0200

    tests: application_development: ram_context_for_isr: Find a free irq
    Add a config symbol (CONFIG_TEST_FIND_FREE_IRQ) for the test to find
    during runtime initialization a free IRQ to run the test.
    The config is default enabled and existing configuration overlay files
    that set a specific test IRQ now disable this config switch.
    
    For this purpose, enable CONFIG_DYNAMIC_INTERRUPTS in the test
    configuration and remove struct fake_driver_config and the
    fake_driver_configure() helper function that are no more nedeed.
    
    Move TEST_IRQ_NUM macro deinition to fake_driver.c since it's only
    used in this file and add a trace message to print to IRQ number used.
    
    The NVIC scanning loop is copied from arm_custom_interrupt tests.
    Since implemented only for NVIC support, CONFIG_TEST_IRQ_NUM can only
    be enabled on Cortex-M based platforms.
    
    This change makes the test more flexible when last defined IRQ line
    is already used and CONFIG_TEST_IRQ_NUM cannot be easily set since it
    would require a per-SoC configuration, not really suitable for vendors
    that maintain a large number of SoCs.
    
    This change relies on Arm NVIC controller which suites well current
    implementation of ram_context_for_isr test that is only supported
    for Cortex-M SoCs.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- tests/application_development/ram_context_for_isr/src/fake_driver.c
+++ tests/application_development/ram_context_for_isr/src/fake_driver.c
@@ -4,8 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <cmsis_core.h>
 #include <zephyr/device.h>
 #include <zephyr/irq.h>
+#include <zephyr/ztest.h>
 #include "fake_driver.h"
 
 #define DT_DRV_COMPAT fakedriver
@@ -25,10 +27,77 @@ static void fake_driver_isr(const void *arg)
 	}
 }
 
-static int fake_driver_configure(const struct device *dev, int config)
+#ifdef CONFIG_TEST_FIND_FREE_IRQ
+/* Scan NVIC for a free IRQ */
+
+#if CONFIG_2ND_LVL_ISR_TBL_OFFSET > 0
+#define TEST_1ST_LEVEL_INTERRUPTS_MAX (CONFIG_2ND_LVL_ISR_TBL_OFFSET - 1)
+#else
+#define TEST_1ST_LEVEL_INTERRUPTS_MAX (CONFIG_NUM_IRQS - 1)
+#endif
+
+static unsigned int get_test_irq(void)
 {
-	return 0;
+	int i;
+
+	for (i = TEST_1ST_LEVEL_INTERRUPTS_MAX; i >= 0; i--) {
+		if (NVIC_GetEnableIRQ(i) == 0) {
+			/*
+			 * Interrupts configured statically with IRQ_CONNECT(.)
+			 * are automatically enabled. NVIC_GetEnableIRQ()
+			 * returning false, here, implies that the IRQ line is
+			 * either not implemented or it is not enabled, thus,
+			 * currently not in use by Zephyr.
+			 */
+
+			/* Set the NVIC line to pending. */
+			NVIC_SetPendingIRQ(i);
+
+			if (NVIC_GetPendingIRQ(i)) {
+				/* If the NVIC line is pending, it is
+				 * guaranteed that it is implemented; clear the
+				 * line.
+				 */
+				NVIC_ClearPendingIRQ(i);
+
+				if (!NVIC_GetPendingIRQ(i)) {
+					/*
+					 * If the NVIC line can be successfully
+					 * un-pended, it is guaranteed that it
+					 * can be used for software interrupt
+					 * triggering.
+					 */
+					break;
+				}
+			}
+		}
+	}
+
+	zassert_true(i >= 0, "No available IRQ line to use in the test\n");
+
+	return (unsigned int)i;
 }
+#else /* CONFIG_TEST_FIND_FREE_IRQ */
+
+#if CONFIG_TEST_IRQ_NUM == 0
+/* For all the other platforms, use the last available IRQ line for testing. */
+#ifdef CONFIG_MULTI_LEVEL_INTERRUPTS
+#define TEST_IRQ_NUM (CONFIG_2ND_LVL_ISR_TBL_OFFSET - 1)
+#else
+#define TEST_IRQ_NUM  (CONFIG_NUM_IRQS - 1)
+#endif
+#else
+#define TEST_IRQ_NUM CONFIG_TEST_IRQ_NUM
+#endif
+
+BUILD_ASSERT(TEST_IRQ_NUM >= 0,
+	     "CONFIG_TEST_IRQ_NUM defines the test IRQ when CONFIG_TEST_FIND_FREE_IRQ is disabled");
+
+static unsigned int get_test_irq(void)
+{
+	return TEST_IRQ_NUM;
+}
+#endif /* CONFIG_TEST_FIND_FREE_IRQ */
 
 static int fake_driver_register_irq_callback(const struct device *dev,
 					     fake_driver_irq_callback_t cb, void *user_data)
@@ -42,38 +111,27 @@ static int fake_driver_register_irq_callback(const struct device *dev,
 }
 
 DEVICE_API(fake, fake_driver_func) = {
-	.configure = fake_driver_configure,
 	.register_irq_callback = fake_driver_register_irq_callback,
 };
 
 static int fake_driver_init(const struct device *dev)
 {
-	const struct fake_driver_config *config = dev->config;
 	struct fake_driver_data *data = dev->data;
 
+	data->irq_num = get_test_irq();
 	data->irq_callback = NULL;
 	data->user_data = NULL;
 
-	config->irq_config_func();
+	(void)irq_connect_dynamic(data->irq_num, CONFIG_TEST_IRQ_PRIO, fake_driver_isr, dev, 0);
+	irq_enable(data->irq_num);
 
 	return 0;
 }
 
 #define FAKE_INIT(inst)                                                                            \
 	static struct fake_driver_data fake_driver_data_##inst;                                    \
-	static void fake_driver_irq_config_func_##inst(void)                                       \
-	{                                                                                          \
-		IRQ_CONNECT(TEST_IRQ_NUM, CONFIG_TEST_IRQ_PRIO, fake_driver_isr,                   \
-			    DEVICE_DT_INST_GET(inst), 0);                                          \
-		irq_enable(TEST_IRQ_NUM);                                                          \
-	}                                                                                          \
-	static struct fake_driver_config fake_driver_config_##inst = {                             \
-		.irq_config_func = fake_driver_irq_config_func_##inst,                             \
-		.irq_num = TEST_IRQ_NUM,                                                           \
-		.irq_priority = CONFIG_TEST_IRQ_PRIO,                                              \
-	};                                                                                         \
-	DEVICE_DT_INST_DEFINE(inst, &fake_driver_init, NULL, &fake_driver_data_##inst,             \
-			      &fake_driver_config_##inst, PRE_KERNEL_1,                            \
-			      CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &fake_driver_func);
+	                                                                                           \
+	DEVICE_DT_INST_DEFINE(inst, &fake_driver_init, NULL, &fake_driver_data_##inst, NULL,       \
+			      PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &fake_driver_func);
 
 DT_INST_FOREACH_STATUS_OKAY(FAKE_INIT)
