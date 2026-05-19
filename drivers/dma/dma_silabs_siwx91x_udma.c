0a822a5cab1de3cb6db7dda92089287e60c5ac51
Jérôme Pouiller <jerome.pouiller@silabs.com>
2026-05-19 16:57:25 +0200

    drivers: dma: siwx91x: udma: Fix symbols prefix
    We try to prefix all the symbols in a file with the same prefix (so the
    developer easily understand if a symbols is provided by Zephyr, the HAL or
    is internal to the driver).
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- drivers/dma/dma_silabs_siwx91x_udma.c
+++ drivers/dma/dma_silabs_siwx91x_udma.c
@@ -21,16 +21,16 @@
 #include "rsi_udma.h"
 #include "sl_status.h"
 
-#define DMA_MAX_TRANSFER_COUNT           1024
-#define DMA_CH_PRIORITY_HIGH             1
-#define DMA_CH_PRIORITY_LOW              0
+#define SIWX91X_UDMA_MAX_XFER_COUNT      1024
+#define SIWX91X_UDMA_MAX_PRIORITY           1
+#define SIWX91X_UDMA_MIN_PRIORITY           0
 
 /* This value is missing in rsi_udma.h */
 #define UDMA_MODE_PER_ALT_SCATTER_GATHER 0x07
 
-LOG_MODULE_REGISTER(si91x_dma, CONFIG_DMA_LOG_LEVEL);
+LOG_MODULE_REGISTER(siwx91x_udma, CONFIG_DMA_LOG_LEVEL);
 
-struct dma_siwx91x_channel_info {
+struct siwx91x_udma_channel_info {
 	dma_callback_t dma_callback;        /* User callback */
 	void *cb_data;                      /* User callback data */
 	RSI_UDMA_DESC_T *sg_desc_addr_info; /* Scatter-Gather table start address */
@@ -38,7 +38,7 @@ struct dma_siwx91x_channel_info {
 	bool channel_active;                /* Channel active flag */
 };
 
-struct dma_siwx91x_config {
+struct siwx91x_udma_config {
 	UDMA0_Type *reg;                 /* UDMA register base address */
 	uint8_t irq_number;              /* IRQ number */
 	RSI_UDMA_DESC_T *sram_desc_addr; /* SRAM Address for UDMA Descriptor Storage */
@@ -47,14 +47,12 @@ struct dma_siwx91x_config {
 	void (*irq_configure)(void);     /* IRQ configure function */
 };
 
-struct dma_siwx91x_data {
+struct siwx91x_udma_data {
 	struct dma_context dma_ctx;
 	UDMA_Channel_Info *chan_info;
-	struct dma_siwx91x_channel_info *zephyr_channel_info;
+	struct siwx91x_udma_channel_info *zephyr_channel_info;
 	struct sys_mem_blocks *dma_desc_pool; /* Pointer to the memory pool for DMA descriptor */
-	RSI_UDMA_DATACONTEXT_T udma_handle;  /* Buffer to store UDMA handle
-					      * related information
-					      */
+	RSI_UDMA_DATACONTEXT_T udma_handle;   /* Buffer to store UDMA handle related information */
 };
 
 static bool siwx91x_udma_is_valid_dir(uint32_t val)
@@ -114,10 +112,13 @@ static int siwx91x_udma_fill_block_descs(const struct dma_config *cfg,
 	int cnt = 0;
 
 	do {
-		uint32_t chunk = MIN(remaining, DMA_MAX_TRANSFER_COUNT * cfg->source_burst_length);
+		uint32_t chunk = SIWX91X_UDMA_MAX_XFER_COUNT * cfg->source_burst_length;
 		RSI_UDMA_CHA_CONFIG_DATA_T *desc_cfg =
 			(RSI_UDMA_CHA_CONFIG_DATA_T *)&descs->vsUDMAChaConfigData1;
 
+		if (chunk > remaining) {
+			chunk = remaining;
+		}
 		desc_cfg->srcSize = find_lsb_set(cfg->source_burst_length) - 1;
 		desc_cfg->dstSize = find_lsb_set(cfg->dest_burst_length) - 1;
 		desc_cfg->totalNumOfDMATrans = (chunk / cfg->source_burst_length) - 1;
@@ -206,7 +207,7 @@ static int siwx91x_udma_fill_sg_descs(const struct dma_config *config_zephyr,
 static uint32_t siwx91x_udma_count_hw_desc(const struct dma_config *config)
 {
 	const struct dma_block_config *block = config->head_block;
-	uint32_t max_chunk = DMA_MAX_TRANSFER_COUNT * config->source_burst_length;
+	uint32_t max_chunk = SIWX91X_UDMA_MAX_XFER_COUNT * config->source_burst_length;
 	uint32_t total = 0;
 
 	for (int i = 0; i < config->block_count; i++) {
@@ -225,8 +226,8 @@ static uint32_t siwx91x_udma_count_hw_desc(const struct dma_config *config)
 static int siwx91x_udma_config_sg(const struct device *dev, RSI_UDMA_HANDLE_T udma_handle,
 				  uint32_t channel, const struct dma_config *config)
 {
-	const struct dma_siwx91x_config *cfg = dev->config;
-	struct dma_siwx91x_data *data = dev->data;
+	const struct siwx91x_udma_config *cfg = dev->config;
+	struct siwx91x_udma_data *data = dev->data;
 	uint32_t desc_count = siwx91x_udma_count_hw_desc(config);
 	RSI_UDMA_DESC_T *desc_base = NULL;
 	uint8_t transfer_type;
@@ -284,8 +285,8 @@ static int siwx91x_udma_config_single(const struct device *dev, RSI_UDMA_HANDLE_
 				      uint32_t channel, const struct dma_config *config)
 {
 	uint32_t dma_transfer_num = config->head_block->block_size / config->source_burst_length;
-	const struct dma_siwx91x_config *cfg = dev->config;
-	struct dma_siwx91x_data *data = dev->data;
+	const struct siwx91x_udma_config *cfg = dev->config;
+	struct siwx91x_udma_data *data = dev->data;
 	UDMA_RESOURCES udma_resources = {
 		.reg = cfg->reg,
 		.udma_irq_num = cfg->irq_number,
@@ -310,9 +311,9 @@ static int siwx91x_udma_config_single(const struct device *dev, RSI_UDMA_HANDLE_
 	}
 
 	/* Obtain the number of transfers */
-	if (dma_transfer_num >= DMA_MAX_TRANSFER_COUNT) {
+	if (dma_transfer_num >= SIWX91X_UDMA_MAX_XFER_COUNT) {
 		/* Maximum number of transfers is 1024 */
-		channel_control.totalNumOfDMATrans = DMA_MAX_TRANSFER_COUNT - 1;
+		channel_control.totalNumOfDMATrans = SIWX91X_UDMA_MAX_XFER_COUNT - 1;
 	} else {
 		channel_control.totalNumOfDMATrans = dma_transfer_num;
 	}
@@ -358,11 +359,10 @@ static int siwx91x_udma_config_single(const struct device *dev, RSI_UDMA_HANDLE_
 	return 0;
 }
 
-/* Function to configure UDMA channel for transfer */
-static int siwx91x_dma_configure(const struct device *dev, uint32_t channel,
-				 struct dma_config *config)
+static int siwx91x_udma_config(const struct device *dev, uint32_t channel,
+			       struct dma_config *config)
 {
-	struct dma_siwx91x_data *data = dev->data;
+	struct siwx91x_udma_data *data = dev->data;
 	void *udma_handle = &data->udma_handle;
 	int status;
 
@@ -376,8 +376,8 @@ static int siwx91x_dma_configure(const struct device *dev, uint32_t channel,
 		return -EIO;
 	}
 
-	if (config->channel_priority != DMA_CH_PRIORITY_LOW &&
-	    config->channel_priority != DMA_CH_PRIORITY_HIGH) {
+	if (config->channel_priority < SIWX91X_UDMA_MIN_PRIORITY ||
+	    config->channel_priority > SIWX91X_UDMA_MAX_PRIORITY) {
 		return -EINVAL;
 	}
 
@@ -416,12 +416,11 @@ static int siwx91x_dma_configure(const struct device *dev, uint32_t channel,
 	return 0;
 }
 
-/* Function to reload UDMA channel for new transfer */
-static int siwx91x_dma_reload(const struct device *dev, uint32_t channel, uint32_t src,
-			      uint32_t dst, size_t size)
+static int siwx91x_udma_reload(const struct device *dev, uint32_t channel, uint32_t src,
+			       uint32_t dst, size_t size)
 {
-	const struct dma_siwx91x_config *cfg = dev->config;
-	struct dma_siwx91x_data *data = dev->data;
+	const struct siwx91x_udma_config *cfg = dev->config;
+	struct siwx91x_udma_data *data = dev->data;
 	void *udma_handle = &data->udma_handle;
 	void *desc_src_addr;
 	void *desc_dst_addr;
@@ -445,8 +444,8 @@ static int siwx91x_dma_reload(const struct device *dev, uint32_t channel, uint32
 	data->chan_info[channel].Size = size / xfer_size;
 
 	/* Update new transfer size to dev->data structure */
-	if (data->chan_info[channel].Size >= DMA_MAX_TRANSFER_COUNT) {
-		data->chan_info[channel].Cnt = DMA_MAX_TRANSFER_COUNT;
+	if (data->chan_info[channel].Size >= SIWX91X_UDMA_MAX_XFER_COUNT) {
+		data->chan_info[channel].Cnt = SIWX91X_UDMA_MAX_XFER_COUNT - 1;
 	} else {
 		data->chan_info[channel].Cnt = size / xfer_size;
 	}
@@ -473,11 +472,10 @@ static int siwx91x_dma_reload(const struct device *dev, uint32_t channel, uint32
 	return 0;
 }
 
-/* Function to start a DMA transfer */
-static int siwx91x_dma_start(const struct device *dev, uint32_t channel)
+static int siwx91x_udma_start(const struct device *dev, uint32_t channel)
 {
-	const struct dma_siwx91x_config *cfg = dev->config;
-	struct dma_siwx91x_data *data = dev->data;
+	const struct siwx91x_udma_config *cfg = dev->config;
+	struct siwx91x_udma_data *data = dev->data;
 	void *udma_handle = &data->udma_handle;
 
 	/* Expecting a fixed channel number between 0-31 for dma0 and 0-11 for ulpdma */
@@ -507,10 +505,9 @@ static int siwx91x_dma_start(const struct device *dev, uint32_t channel)
 	return 0;
 }
 
-/* Function to stop a DMA transfer */
-static int siwx91x_dma_stop(const struct device *dev, uint32_t channel)
+static int siwx91x_udma_stop(const struct device *dev, uint32_t channel)
 {
-	struct dma_siwx91x_data *data = dev->data;
+	struct siwx91x_udma_data *data = dev->data;
 	void *udma_handle = &data->udma_handle;
 
 	/* Expecting a fixed channel number between 0-31 for dma0 and 0-11 for ulpdma */
@@ -530,12 +527,11 @@ static int siwx91x_dma_stop(const struct device *dev, uint32_t channel)
 	return 0;
 }
 
-/* Function to fetch DMA channel status */
-static int siwx91x_dma_get_status(const struct device *dev, uint32_t channel,
-				  struct dma_status *stat)
+static int siwx91x_udma_get_status(const struct device *dev, uint32_t channel,
+				   struct dma_status *stat)
 {
-	const struct dma_siwx91x_config *cfg = dev->config;
-	struct dma_siwx91x_data *data = dev->data;
+	const struct siwx91x_udma_config *cfg = dev->config;
+	struct siwx91x_udma_data *data = dev->data;
 	RSI_UDMA_DESC_T *udma_table = cfg->sram_desc_addr;
 
 	/* Expecting a fixed channel number between 0-31 for dma0 and 0-11 for ulpdma */
@@ -562,7 +558,7 @@ static int siwx91x_dma_get_status(const struct device *dev, uint32_t channel,
 	return 0;
 }
 
-bool siwx91x_dma_chan_filter(const struct device *dev, int channel, void *filter_param)
+bool siwx91x_udma_chan_filter(const struct device *dev, int channel, void *filter_param)
 {
 	ARG_UNUSED(dev);
 
@@ -577,10 +573,10 @@ bool siwx91x_dma_chan_filter(const struct device *dev, int channel, void *filter
 	}
 }
 
-static int dma_siwx91x_pm_action(const struct device *dev, enum pm_device_action action)
+static int siwx91x_udma_pm_action(const struct device *dev, enum pm_device_action action)
 {
-	const struct dma_siwx91x_config *cfg = dev->config;
-	struct dma_siwx91x_data *data = dev->data;
+	const struct siwx91x_udma_config *cfg = dev->config;
+	struct siwx91x_udma_data *data = dev->data;
 	void *udma_handle = NULL;
 	UDMA_RESOURCES udma_resources = {
 		.reg = cfg->reg, /* UDMA register base address */
@@ -620,20 +616,20 @@ static int dma_siwx91x_pm_action(const struct device *dev, enum pm_device_action
 }
 
 /* Function to initialize DMA peripheral */
-static int siwx91x_dma_init(const struct device *dev)
+static int siwx91x_udma_init(const struct device *dev)
 {
-	const struct dma_siwx91x_config *cfg = dev->config;
+	const struct siwx91x_udma_config *cfg = dev->config;
 
 	/* Connect the DMA interrupt */
 	cfg->irq_configure();
 
-	return pm_device_driver_init(dev, dma_siwx91x_pm_action);
+	return pm_device_driver_init(dev, siwx91x_udma_pm_action);
 }
 
-static void siwx91x_dma_isr(const struct device *dev)
+static void siwx91x_udma_isr(const struct device *dev)
 {
-	const struct dma_siwx91x_config *cfg = dev->config;
-	struct dma_siwx91x_data *data = dev->data;
+	const struct siwx91x_udma_config *cfg = dev->config;
+	struct siwx91x_udma_data *data = dev->data;
 	UDMA_RESOURCES udma_resources = {
 		.reg = cfg->reg,
 		.udma_irq_num = cfg->irq_number,
@@ -697,54 +693,53 @@ out:
 }
 
 /* Store the Si91x DMA APIs */
-static DEVICE_API(dma, siwx91x_dma_api) = {
-	.config = siwx91x_dma_configure,
-	.reload = siwx91x_dma_reload,
-	.start = siwx91x_dma_start,
-	.stop = siwx91x_dma_stop,
-	.get_status = siwx91x_dma_get_status,
-	.chan_filter = siwx91x_dma_chan_filter,
+static DEVICE_API(dma, siwx91x_udma_api) = {
+	.config = siwx91x_udma_config,
+	.reload = siwx91x_udma_reload,
+	.start = siwx91x_udma_start,
+	.stop = siwx91x_udma_stop,
+	.get_status = siwx91x_udma_get_status,
+	.chan_filter = siwx91x_udma_chan_filter,
 };
 
-#define SIWX91X_DMA_INIT(inst)                                                                     \
-	static ATOMIC_DEFINE(dma_channels_atomic_##inst, DT_INST_PROP(inst, dma_channels));        \
-	static UDMA_Channel_Info dma_channel_info_##inst[DT_INST_PROP(inst, dma_channels)];        \
-	SYS_MEM_BLOCKS_DEFINE_STATIC_TYPE(desc_pool_##inst, RSI_UDMA_DESC_T,                       \
+#define SIWX91X_UDMA_INIT(inst)                                                                    \
+	static ATOMIC_DEFINE(siwx91x_udma_chans_atomic##inst, DT_INST_PROP(inst, dma_channels));   \
+	static UDMA_Channel_Info siwx91x_udma_hal_chans##inst[DT_INST_PROP(inst, dma_channels)];   \
+	SYS_MEM_BLOCKS_DEFINE_STATIC_TYPE(siwx91x_udma_desc_pool##inst, RSI_UDMA_DESC_T,           \
 					  CONFIG_DMA_SILABS_SIWX91X_UDMA_DESCR_COUNT);             \
 	COND_CODE_1(DT_INST_NODE_HAS_PROP(inst, silabs_sram_region),                               \
 		    (),                                                                            \
 		    (static __aligned(1024) RSI_UDMA_DESC_T                                        \
-			     siwx91x_dma_chan_desc##inst[DT_INST_PROP(inst, dma_channels) * 2];))  \
-	static struct dma_siwx91x_channel_info                                                     \
-		zephyr_channel_info_##inst[DT_INST_PROP(inst, dma_channels)];                      \
-	static struct dma_siwx91x_data dma_data_##inst = {                                         \
+			     siwx91x_udma_chan_desc##inst[DT_INST_PROP(inst, dma_channels) * 2];)) \
+	static struct siwx91x_udma_channel_info                                                    \
+		siwx91x_udma_drv_chans##inst[DT_INST_PROP(inst, dma_channels)];                    \
+	static struct siwx91x_udma_data siwx91x_udma_data##inst = {                                \
 		.dma_ctx.magic = DMA_MAGIC,                                                        \
 		.dma_ctx.dma_channels = DT_INST_PROP(inst, dma_channels),                          \
-		.dma_ctx.atomic = dma_channels_atomic_##inst,                                      \
-		.chan_info = dma_channel_info_##inst,                                              \
-		.zephyr_channel_info = zephyr_channel_info_##inst,                                 \
-		.dma_desc_pool = &desc_pool_##inst,                                                \
+		.dma_ctx.atomic = siwx91x_udma_chans_atomic##inst,                                 \
+		.chan_info = siwx91x_udma_hal_chans##inst,                                         \
+		.zephyr_channel_info = siwx91x_udma_drv_chans##inst,                               \
+		.dma_desc_pool = &siwx91x_udma_desc_pool##inst,                                    \
 	};                                                                                         \
-	static void siwx91x_dma_irq_configure_##inst(void)                                         \
+	static void siwx91x_udma_irq_configure##inst(void)                                         \
 	{                                                                                          \
-		IRQ_CONNECT(DT_INST_IRQ(inst, irq), DT_INST_IRQ(inst, priority), siwx91x_dma_isr,  \
-			    DEVICE_DT_INST_GET(inst), 0);                                          \
+		IRQ_CONNECT(DT_INST_IRQ(inst, irq), DT_INST_IRQ(inst, priority),                   \
+			    siwx91x_udma_isr, DEVICE_DT_INST_GET(inst), 0);                        \
 		irq_enable(DT_INST_IRQ(inst, irq));                                                \
 	}                                                                                          \
-	static const struct dma_siwx91x_config dma_cfg_##inst = {                                  \
+	static const struct siwx91x_udma_config siwx91x_udma_cfg##inst = {                         \
 		.clock_dev = DEVICE_DT_GET(DT_INST_CLOCKS_CTLR(inst)),                             \
 		.clock_subsys = (clock_control_subsys_t)DT_INST_PHA(inst, clocks, clkid),          \
 		.reg = (UDMA0_Type *)DT_INST_REG_ADDR(inst),                                       \
 		.irq_number = DT_INST_PROP_BY_IDX(inst, interrupts, 0),                            \
 		.sram_desc_addr = COND_CODE_1(DT_INST_NODE_HAS_PROP(inst, silabs_sram_region),     \
 					      ((RSI_UDMA_DESC_T *)DT_REG_ADDR(DT_INST_PHANDLE(inst, silabs_sram_region))), \
-					      (siwx91x_dma_chan_desc##inst)),                      \
-		.irq_configure = siwx91x_dma_irq_configure_##inst,                                 \
+					      (siwx91x_udma_chan_desc##inst)),                     \
+		.irq_configure = siwx91x_udma_irq_configure##inst,                                 \
 	};                                                                                         \
-	PM_DEVICE_DT_INST_DEFINE(inst, dma_siwx91x_pm_action);                                     \
-	DEVICE_DT_INST_DEFINE(inst, siwx91x_dma_init,  PM_DEVICE_DT_INST_GET(inst),                \
-			      &dma_data_##inst, &dma_cfg_##inst, POST_KERNEL,                      \
-			      CONFIG_DMA_INIT_PRIORITY,                                           \
-			      &siwx91x_dma_api);
+	PM_DEVICE_DT_INST_DEFINE(inst, siwx91x_udma_pm_action);                                    \
+	DEVICE_DT_INST_DEFINE(inst, siwx91x_udma_init,  PM_DEVICE_DT_INST_GET(inst),               \
+			      &siwx91x_udma_data##inst, &siwx91x_udma_cfg##inst, POST_KERNEL,      \
+			      CONFIG_DMA_INIT_PRIORITY, &siwx91x_udma_api);
 
-DT_INST_FOREACH_STATUS_OKAY(SIWX91X_DMA_INIT)
+DT_INST_FOREACH_STATUS_OKAY(SIWX91X_UDMA_INIT)
