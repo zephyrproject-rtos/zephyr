264cf1d6fd6f39b99078e3a7957ddb1f57f4c9ce
Mahesh Mahadevan <mahesh.mahadevan@nxp.com>
2026-05-20 12:13:25 -0500

    Revert "[nxp fromtree] net: dns: Add support for DNS private RR types"
    This reverts commit a2a7e4085f9510a6f93f14747e633bd29db91a8f.
    
    Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

--- subsys/net/lib/dns/dns_pack.h
+++ subsys/net/lib/dns/dns_pack.h
@@ -87,16 +87,14 @@ struct dns_msg_t {
 
 enum dns_rr_type {
 	DNS_RR_TYPE_INVALID = 0,
-	DNS_RR_TYPE_A	= 1,			/* IPv4  */
-	DNS_RR_TYPE_CNAME = 5,			/* CNAME */
-	DNS_RR_TYPE_PTR = 12,			/* PTR   */
-	DNS_RR_TYPE_TXT = 16,			/* TXT   */
-	DNS_RR_TYPE_AAAA = 28,			/* IPv6  */
-	DNS_RR_TYPE_SRV = 33,			/* SRV   */
-	DNS_RR_TYPE_HTTPS = 65,			/* HTTPS */
-	DNS_RR_TYPE_ANY = 0xff,			/* ANY (all records)   */
-	DNS_RR_TYPE_PRIVATE_START = 65280,	/* Private use start */
-	DNS_RR_TYPE_PRIVATE_END = 65534,	/* Private use end */
+	DNS_RR_TYPE_A	= 1,		/* IPv4  */
+	DNS_RR_TYPE_CNAME = 5,		/* CNAME */
+	DNS_RR_TYPE_PTR = 12,		/* PTR   */
+	DNS_RR_TYPE_TXT = 16,		/* TXT   */
+	DNS_RR_TYPE_AAAA = 28,		/* IPv6  */
+	DNS_RR_TYPE_SRV = 33,		/* SRV   */
+	DNS_RR_TYPE_HTTPS = 65,		/* HTTPS */
+	DNS_RR_TYPE_ANY = 0xff,		/* ANY (all records)   */
 };
 
 enum dns_response_type {
@@ -106,8 +104,7 @@ enum dns_response_type {
 	DNS_RESPONSE_TXT,
 	DNS_RESPONSE_SRV,
 	DNS_RESPONSE_CNAME_WITH_IP,
-	DNS_RESPONSE_CNAME_NO_IP,
-	DNS_RESPONSE_PRIVATE,
+	DNS_RESPONSE_CNAME_NO_IP
 };
 
 enum dns_class {
