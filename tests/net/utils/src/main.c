efe51020c60cd90b225041b793e97f911115e162
Fin Maaß <f.maass@vogl-electronic.com>
2026-05-21 11:10:50 +0200

    net: tests: utils: use correct length sub-specifier
    use correct length sub-specifier when
    printing a a pointer difference.
    
    Signed-off-by: Fin Maaß <f.maass@vogl-electronic.com>

--- tests/net/utils/src/main.c
+++ tests/net/utils/src/main.c
@@ -914,7 +914,7 @@ static const char *check_ipaddr(const char *addresses)
 
 		zassert_mem_equal(addr_str, expecting,
 				  *addresses == '\0' ? strlen(orig) : addresses - orig - 1,
-				  "Address mismatch, expecing %s, got %s (len %d)\n",
+				  "Address mismatch, expecing %s, got %s (len %td)\n",
 				  expecting, addr_str, addresses - orig - 1);
 	} while (addresses != NULL && *addresses != '\0');
 
