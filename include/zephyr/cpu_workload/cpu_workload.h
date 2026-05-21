718262df73e437f8b93fbf0dbcc7c365bcb4d375
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2026-05-21 13:20:51 +0800

    cpu_workload: introduce subsystem skeleton
    Add the experimental CPU workload subsystem skeleton.
    
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

---
+++ include/zephyr/cpu_workload/cpu_workload.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2026 NXP
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_CPU_WORKLOAD_H_
+#define ZEPHYR_INCLUDE_CPU_WORKLOAD_H_
+
+#include <stdint.h>
+
+#include <zephyr/sys/util.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief CPU Workload
+ * @defgroup subsys_cpu_workload CPU Workload
+ * @since 4.5
+ * @version 0.1.0
+ * @ingroup os_services
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_CPU_WORKLOAD_H_ */
