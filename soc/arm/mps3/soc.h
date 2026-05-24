3405d5bd5bc85141fe7cb8bf66efd1b1192d1e93
Andrei-Edward Popa <andrei.popa105@yahoo.com>
2026-05-24 18:40:29 +0300

    soc: arm: mps3: add SoC support for the AN536 on MPS3
    Add SoC support for the Arm MPS3 AN536 Cortex-R52x2 FPGA image.
    
    The AN536 platform uses a Cortex-R52 CPU, GICv3 and the Arm
    architectural timer. Add the SoC Kconfig entries, SoC metadata,
    Cortex-R linker script selection and MPU region configuration needed
    by the platform.
    
    Provide the CMSIS Core_R device definitions required by the Cortex-R
    CMSIS headers, including the AN536 IRQn_Type interrupt map and GIC base
    address definitions.
    
    Signed-off-by: Andrei-Edward Popa <andrei.popa105@yahoo.com>

--- soc/arm/mps3/soc.h
+++ soc/arm/mps3/soc.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2017-2021 Linaro Limited
+ * Copyright (c) 2026 Andrei-Edward Popa
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -7,6 +8,16 @@
 #ifndef _SOC_H_
 #define _SOC_H_
 
+#if defined(CONFIG_CPU_CORTEX_M)
 #include <cmsis_core_m_defaults.h>
+#endif
+
+#if defined(CONFIG_SOC_AN536)
+
+/* Do not let CMSIS to handle GIC and Timer */
+#define __GIC_PRESENT 0
+#define __TIM_PRESENT 0
+
+#endif
 
 #endif /* _SOC_H_ */
