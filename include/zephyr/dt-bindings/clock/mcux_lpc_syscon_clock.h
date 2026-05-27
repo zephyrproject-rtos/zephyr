8c5812e8c0b5e2b890344630fed7d70125dfaa46
Ruijia Wang <ruijia.wang@nxp.com>
2026-05-27 21:26:53 +0800

    dt-bindings: clock: add MCUX_ESPI0_CLK
    Add eSPI peripheral clock identifier.
    
    Signed-off-by: Ruijia Wang <ruijia.wang@nxp.com>

--- include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h
+++ include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h
@@ -225,4 +225,7 @@
 /** SLCD peripheral clock identifier. */
 #define MCUX_SLCD_CLK MCUX_LPC_CLK_ID(0x34, 0x00)
 
+/** eSPI peripheral clock identifier. */
+#define MCUX_ESPI0_CLK MCUX_LPC_CLK_ID(0x35, 0x00)
+
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_MCUX_LPC_SYSCON_H_ */
