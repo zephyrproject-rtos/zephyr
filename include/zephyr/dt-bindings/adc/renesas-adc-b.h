10ed9b427dd3ea44481f1d72e4ac639bffe4c1b4
Quy Tran <quy.tran.pz@renesas.com>
2026-05-28 10:49:42 +0000

    drivers: adc: Support ADC driver for Renesas RA with adc_b
    Add support adc driver on Renesas RA (ra8x2) with r_adc_b
    
    Signed-off-by: Quy Tran <quy.tran.pz@renesas.com>

---
+++ include/zephyr/dt-bindings/adc/renesas-adc-b.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2026 Renesas Electronics Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Renesas RA ADC_B physical analog input channel selectors.
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_ADC_RENESAS_ADC_B_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_ADC_RENESAS_ADC_B_H_
+
+/**
+ * @defgroup renesas_adc_b_channels Renesas RA ADC_B channel selectors
+ * @ingroup adc_interface
+ *
+ * Physical analog input channel identifiers for the Renesas RA ADC_B
+ * controller. Use these with the @c zephyr,input-positive property of a
+ * channel node to select which physical input the virtual channel samples.
+ * @{
+ */
+
+#define RENESAS_ADC_B_CH0  0  /**< Physical analog input channel 0. */
+#define RENESAS_ADC_B_CH1  1  /**< Physical analog input channel 1. */
+#define RENESAS_ADC_B_CH2  2  /**< Physical analog input channel 2. */
+#define RENESAS_ADC_B_CH3  3  /**< Physical analog input channel 3. */
+#define RENESAS_ADC_B_CH4  4  /**< Physical analog input channel 4. */
+#define RENESAS_ADC_B_CH5  5  /**< Physical analog input channel 5. */
+#define RENESAS_ADC_B_CH6  6  /**< Physical analog input channel 6. */
+#define RENESAS_ADC_B_CH7  7  /**< Physical analog input channel 7. */
+#define RENESAS_ADC_B_CH8  8  /**< Physical analog input channel 8. */
+#define RENESAS_ADC_B_CH9  9  /**< Physical analog input channel 9. */
+#define RENESAS_ADC_B_CH10 10 /**< Physical analog input channel 10. */
+#define RENESAS_ADC_B_CH11 11 /**< Physical analog input channel 11. */
+#define RENESAS_ADC_B_CH12 12 /**< Physical analog input channel 12. */
+#define RENESAS_ADC_B_CH13 13 /**< Physical analog input channel 13. */
+#define RENESAS_ADC_B_CH14 14 /**< Physical analog input channel 14. */
+#define RENESAS_ADC_B_CH15 15 /**< Physical analog input channel 15. */
+#define RENESAS_ADC_B_CH16 16 /**< Physical analog input channel 16. */
+#define RENESAS_ADC_B_CH17 17 /**< Physical analog input channel 17. */
+#define RENESAS_ADC_B_CH18 18 /**< Physical analog input channel 18. */
+#define RENESAS_ADC_B_CH19 19 /**< Physical analog input channel 19. */
+#define RENESAS_ADC_B_CH20 20 /**< Physical analog input channel 20. */
+#define RENESAS_ADC_B_CH21 21 /**< Physical analog input channel 21. */
+#define RENESAS_ADC_B_CH22 22 /**< Physical analog input channel 22. */
+
+/** @} */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_ADC_RENESAS_ADC_B_H_ */
