96e470a5969ac472dfdd398b48b611e1b2f5be1e
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2026-05-29 06:07:51 +0200

    tests: bsim: Bluetooth: Controller: Trivial re-order in compile script
    Trivial re-order of compilations in the compile script to
    keep similar feature tests grouped together.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

--- tests/bsim/bluetooth/ll/compile.sh
+++ tests/bsim/bluetooth/ll/compile.sh
@@ -10,11 +10,8 @@ set -ue
 
 source ${ZEPHYR_BASE}/tests/bsim/compile.source
 
-${ZEPHYR_BASE}/tests/bsim/bluetooth/ll/cis/compile.sh
-
 app=tests/bsim/bluetooth/ll/advx compile
-app=tests/bsim/bluetooth/ll/advx \
-  conf_overlay=overlay-ticker_expire_info.conf compile
+app=tests/bsim/bluetooth/ll/advx conf_overlay=overlay-ticker_expire_info.conf compile
 app=tests/bsim/bluetooth/ll/advx conf_overlay=overlay-scan_aux_use_chains.conf compile
 
 app=tests/bsim/bluetooth/ll/conn conf_file=prj_split.conf compile
@@ -25,6 +22,14 @@ app=tests/bsim/bluetooth/ll/conn conf_file=prj_split_low_lat.conf compile
 app=tests/bsim/bluetooth/ll/conn conf_file=prj_split_single_timer.conf compile
 app=tests/bsim/bluetooth/ll/conn conf_file=prj_split_meta_irq.conf compile
 
+app=tests/bsim/bluetooth/ll/multiple_id compile
+app=tests/bsim/bluetooth/ll/throughput compile
+app=tests/bsim/bluetooth/ll/throughput conf_overlay=overlay-no_phy_update.conf compile
+
+app=tests/bsim/bluetooth/ll/edtt/hci_test_app conf_file=prj_dut_llcp.conf compile
+app=tests/bsim/bluetooth/ll/edtt/hci_test_app conf_file=prj_tst_llcp.conf compile
+app=tests/bsim/bluetooth/ll/edtt/gatt_test_app conf_file=prj_llcp.conf compile
+
 app=tests/bsim/bluetooth/ll/bis conf_overlay=overlay-sequential.conf compile
 app=tests/bsim/bluetooth/ll/bis conf_overlay=overlay-interleaved.conf  compile
 app=tests/bsim/bluetooth/ll/bis conf_overlay=overlay-ll_interface.conf compile
@@ -33,15 +38,6 @@ app=tests/bsim/bluetooth/ll/bis conf_overlay=overlay-scan_aux_use_chains.conf co
 app=tests/bsim/bluetooth/ll/bis conf_file=prj_vs_dp.conf compile
 app=tests/bsim/bluetooth/ll/bis conf_file=prj_past.conf compile
 
-app=tests/bsim/bluetooth/ll/edtt/hci_test_app \
-  conf_file=prj_dut_llcp.conf compile
-app=tests/bsim/bluetooth/ll/edtt/hci_test_app \
-  conf_file=prj_tst_llcp.conf compile
-app=tests/bsim/bluetooth/ll/edtt/gatt_test_app \
-  conf_file=prj_llcp.conf compile
-
-app=tests/bsim/bluetooth/ll/multiple_id compile
-app=tests/bsim/bluetooth/ll/throughput compile
-app=tests/bsim/bluetooth/ll/throughput conf_overlay=overlay-no_phy_update.conf compile
+${ZEPHYR_BASE}/tests/bsim/bluetooth/ll/cis/compile.sh
 
 wait_for_background_jobs
