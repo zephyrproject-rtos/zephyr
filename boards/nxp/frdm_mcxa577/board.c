0890b521e6094a153dd5ad4752ada597aef1c429
Peter Wang <chaoyi.wang@nxp.com>
2026-05-29 11:38:34 +0800

    boards: frdm_mcxa577: add lpcmp support
    Enable LPCMP0 on frdm_mcxa577 using CMP0_IN2 (P1_4). Adds clock
    setup in board_early_init_hook and registers the board with the
    drivers/comparator/gpio_loopback test case.
    
    tested drivers/comparator/gpio_loopback
    
    Signed-off-by: Peter Wang <chaoyi.wang@nxp.com>

--- boards/nxp/frdm_mcxa577/board.c
+++ boards/nxp/frdm_mcxa577/board.c
@@ -236,6 +236,13 @@ void board_early_init_hook(void)
 #endif
 #endif
 
+#if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(lpcmp0))
+	CLOCK_AttachClk(kFRO_LF_DIV_to_CMP0);
+	CLOCK_SetClockDiv(kCLOCK_DivCMP0_FUNC, 1U);
+	CLOCK_EnableClock(kCLOCK_GateCMP0);
+	SPC_EnableActiveModeAnalogModules(SPC0, (kSPC_controlCmp0 | kSPC_controlCmp0Dac));
+#endif
+
 #if DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(ctimer0))
 	CLOCK_SetClockDiv(kCLOCK_DivCTIMER0, 1u);
 	CLOCK_AttachClk(kPll1ClkDiv_to_CTIMER0);
