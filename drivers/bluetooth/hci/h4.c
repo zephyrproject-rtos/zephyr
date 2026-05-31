zephyrproject-rtos/zephyr

commit 9f44b011de912a78a3410176833cdd1177f896ee

bluetooth: hci: h4: optional reset pin
Add an optional reset pin for H4 HCI driver.

Signed-off-by: Jordan Yates <jordan@embeint.com>

drivers/bluetooth/hci/h4.c
@@ -13,6 +13,7 @@
 #include <zephyr/arch/cpu.h>
 
 #include <zephyr/init.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/uart.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/sys/byteorder.h>
@@ -70,6 +71,10 @@ struct h4_config {
 	k_thread_stack_t *rx_thread_stack;
 	size_t rx_thread_stack_size;
 	struct k_thread *rx_thread;
+#if DT_ANY_INST_HAS_PROP_STATUS_OKAY(reset_gpios)
+	struct gpio_dt_spec reset;
+	uint16_t reset_ms;
+#endif /* DT_ANY_INST_HAS_PROP_STATUS_OKAY(reset_gpios) */
 };
 
 static inline void h4_get_type(const struct device *dev)
@@ -523,6 +528,14 @@ static int h4_open(const struct device *dev, bt_hci_recv_t recv)
 
 	uart_irq_callback_user_data_set(cfg->uart, bt_uart_isr, (void *)dev);
 
+#if DT_ANY_INST_HAS_PROP_STATUS_OKAY(reset_gpios)
+	if (cfg->reset.port) {
+		(void)gpio_pin_configure_dt(&cfg->reset, GPIO_OUTPUT_ACTIVE);
+		k_sleep(K_MSEC(cfg->reset_ms));
+		gpio_pin_set_dt(&cfg->reset, 0);
+	}
+#endif
+
 	tid = k_thread_create(cfg->rx_thread, cfg->rx_thread_stack,
 			      cfg->rx_thread_stack_size,
 			      rx_thread, (void *)dev, NULL, NULL,
@@ -601,6 +614,10 @@ static DEVICE_API(bt_hci, h4_driver_api) = {
 		.rx_thread_stack = rx_thread_stack_##inst, \
 		.rx_thread_stack_size = K_KERNEL_STACK_SIZEOF(rx_thread_stack_##inst), \
 		.rx_thread = &rx_thread_##inst, \
+		COND_CODE_1(DT_ANY_INST_HAS_PROP_STATUS_OKAY(reset_gpios), \
+			(.reset = GPIO_DT_SPEC_INST_GET_OR(inst, reset_gpios, {0}), \
+			.reset_ms = DT_INST_PROP_OR(0, reset_assert_duration_ms, 0), \
+		), ()) \
 	}; \
 	static struct h4_data h4_data_##inst = { \
 		.rx = { \

dts/bindings/bluetooth/zephyr,bt-hci-uart.yaml
@@ -11,3 +11,10 @@ properties:
     default: "H:4"
   bt-hci-bus:
     default: "uart"
+  reset-gpios:
+    type: phandle-array
+  reset-assert-duration-ms:
+    type: int
+    description:
+      Minimum duration to hold the reset-gpios pin low for.
+      If not specified no delay beyond the code path execution time is guaranteed.

commit 25b6b27a3b82d1839da4f84c4737ba3cd80679e6

drivers: bluetooth: h4: Take reset-assert-duration-ms from proper node
When there are multiple zephyr,bt-hci-uart nodes, we currently use the
reset-assert-duration-ms property from the first one for all of them due
to a typo in the initializer macro. Fix it.

Fixes: 9f44b011de91 ("bluetooth: hci: h4: optional reset pin")
Signed-off-by: Thomas Hebb <tommyhebb@gmail.com>

drivers/bluetooth/hci/h4.c
@@ -621,7 +621,7 @@ static DEVICE_API(bt_hci, h4_driver_api) = {
 		.rx_thread = &rx_thread_##inst, \
 		COND_CODE_1(DT_ANY_INST_HAS_PROP_STATUS_OKAY(reset_gpios), \
 			(.reset = GPIO_DT_SPEC_INST_GET_OR(inst, reset_gpios, {0}), \
-			.reset_ms = DT_INST_PROP_OR(0, reset_assert_duration_ms, 0), \
+			.reset_ms = DT_INST_PROP_OR(inst, reset_assert_duration_ms, 0), \
 		), ()) \
 	}; \
 	static struct h4_data h4_data_##inst = { \