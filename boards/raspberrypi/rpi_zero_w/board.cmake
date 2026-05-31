14d8a0f6b73fe3e5830b36b874cf21101ce632ce
Jonathan E. Peace <jep@alphabetiq.com>
2026-05-31 11:20:29 -0700

    boards: raspberrypi: rpi_zero_w: switch console to AUX mini-UART
    Move the board console from the PL011 (uart0) to the AUX mini-UART
    (uart1) on GPIO 14/15. The PL011 is shared with the on-board
    BCM43438 Bluetooth modem on the Pi Zero W; using it for console
    requires the `dtoverlay=disable-bt` dance and steals the BT block
    for the lifetime of the boot.
    
    The AUX mini-UART has a smaller FIFO and a 200 MHz core-clock
    divider quirk, but for an interactive shell at 115200 it is more
    than adequate and leaves the BT modem unmolested. This matches the
    default-console choice on the upstream `rpi_zero_2w` board.
    
    Signed-off-by: Jonathan E. Peace <jep@alphabetiq.com>

--- boards/raspberrypi/rpi_zero_w/board.cmake
+++ boards/raspberrypi/rpi_zero_w/board.cmake
@@ -6,10 +6,14 @@ set(QEMU_ARCH arm)
 
 set(QEMU_CPU_TYPE_${ARCH} arm1176)
 
+# raspi0 wires PL011 to serial0 and the AUX mini-UART to serial1. The
+# Zephyr console is the mini-UART, so claim serial0 (PL011) with null here;
+# qemu.board.cmake appends "-serial chardev:con" which then lands on serial1
+# (the mini-UART) and is muxed to stdio.
 set(QEMU_FLAGS_${ARCH}
   -cpu ${QEMU_CPU_TYPE_${ARCH}}
   -machine raspi0
-  -nographic
+  -serial null
   -icount shift=5,align=off,sleep=off
   )
 
