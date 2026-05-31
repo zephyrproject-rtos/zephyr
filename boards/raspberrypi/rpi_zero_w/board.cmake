0debca1574b9a3490d040016a815c850ab531c00
Jonathan E. Peace <jep@alphabetiq.com>
2026-05-31 11:18:58 -0700

    boards: raspberrypi: add Raspberry Pi Zero W
    Add board files for the original Raspberry Pi Zero W (BCM2835 /
    ARM1176JZF-S, single-core ARMv6KZ). The board is QEMU-runnable
    (`-M raspi0 -cpu arm1176`) and boots on real silicon from PINN- or
    NOOBS-imaged microSD via the `support/install-to-sdcard.sh` helper.
    
    Console defaults to the AUX mini-UART (uart1) on GPIO 14/15 @
    115200 8N1. The board defconfig also tunes USB-CDC so the upstream
    `--snippet cdc-acm-console` works out-of-the-box (DWC2 DMA off,
    USBD/UDC stacks bumped to 4 KB for AArch32, VC mailbox firmware
    driver enabled for the OTG power-on sequence). With the snippet
    applied, a single micro-USB cable serves console + power.
    
    Twister picks the board up as a QEMU simulation target;
    `tests/kernel` runs at 97/99 configs (1258/1259 cases) with the
    tickless timer + `-icount` runner config.
    
    Signed-off-by: Jonathan E. Peace <jep@alphabetiq.com>

---
+++ boards/raspberrypi/rpi_zero_w/board.cmake
@@ -0,0 +1,17 @@
+# Copyright (c) 2026 Jonathan Elliot Peace <jep@alphabetiq.com>
+# SPDX-License-Identifier: Apache-2.0
+
+set(SUPPORTED_EMU_PLATFORMS qemu)
+set(QEMU_ARCH arm)
+
+set(QEMU_CPU_TYPE_${ARCH} arm1176)
+
+set(QEMU_FLAGS_${ARCH}
+  -cpu ${QEMU_CPU_TYPE_${ARCH}}
+  -machine raspi0
+  -nographic
+  )
+
+board_set_debugger_ifnset(qemu)
+
+include(${ZEPHYR_BASE}/boards/common/qemu.board.cmake)
