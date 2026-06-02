94dca129c0b6c3dc121411159581c13260a099b5
Matthias Plöger <matthias.ploeger@phoenixcontact.com>
2026-06-02 09:38:29 +0200

    drivers: pwm: stm32: Avoid slave mode reg reconfiguration by capture
    - Only modify salve mode registers when not in 4-channel capture mode
    - Add compile time check for not compatible configuration.
      Capture support is only allowed if:
        - No slave mode is selected
        - or capture runs in four_channel_capture_support mode
    
    Signed-off-by: Matthias Plöger <matthias.ploeger@phoenixcontact.com>

--- drivers/pwm/pwm_stm32.c
+++ drivers/pwm/pwm_stm32.c
@@ -344,7 +344,7 @@ static int pwm_stm32_set_cycles(const struct device *dev, uint32_t channel,
 
 	if (!LL_TIM_CC_IsEnabledChannel(timer, current_ll_channel)) {
 #ifdef CONFIG_PWM_CAPTURE
-		if (IS_TIM_SLAVE_INSTANCE(timer)) {
+		if (IS_TIM_SLAVE_INSTANCE(timer) && !cfg->four_channel_capture_support) {
 			LL_TIM_SetSlaveMode(timer,
 					LL_TIM_SLAVEMODE_DISABLED);
 			LL_TIM_SetTriggerInput(timer, LL_TIM_TS_ITR0);
@@ -458,7 +458,9 @@ static int pwm_stm32_configure_capture(const struct device *dev,
 	cpt->prescaler_bits = prescaler_bits;
 
 	/* Prevents faulty behavior while making changes */
-	LL_TIM_SetSlaveMode(timer, LL_TIM_SLAVEMODE_DISABLED);
+	if (!cfg->four_channel_capture_support) {
+		LL_TIM_SetSlaveMode(timer, LL_TIM_SLAVEMODE_DISABLED);
+	}
 
 	init_capture_channels(dev, channel, flags);
 
@@ -895,6 +897,17 @@ static int pwm_stm32_init(const struct device *dev)
 		.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(index),			\
 		CAPTURE_INIT(index)						\
 	};									\
+										\
+	IF_ENABLED(CONFIG_PWM_CAPTURE, (					\
+		BUILD_ASSERT(							\
+			DT_INST_PROP(index, four_channel_capture_support) == 1	\
+			|| CONCAT(LL_TIM_SLAVEMODE_,				\
+				DT_STRING_UPPER_TOKEN(PWM(index), st_slavemode))\
+			== LL_TIM_SLAVEMODE_DISABLED,				\
+			"Slave mode is only compatible with capture mode in "	\
+			"`four-channel-capture-support` mode!");		\
+		)								\
+	)									\
 										\
 	DEVICE_DT_INST_DEFINE(index, &pwm_stm32_init, NULL,			\
 			    &pwm_stm32_data_##index,				\
