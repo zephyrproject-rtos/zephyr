ed759b9d6ff5e69fac54aca311c16c1d287af231
Benjamin Cabé <benjamin@zephyrproject.org>
2026-06-03 18:51:53 +0200

    include: drivers: sensor: enhance Doxygen documentation for MTCH9010
    Complete the Doxygen documentation for this sensor driver,
    put in sensor_interface_ext group.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- include/zephyr/drivers/sensor/mtch9010.h
+++ include/zephyr/drivers/sensor/mtch9010.h
@@ -4,8 +4,21 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/**
+ * @file
+ * @brief Extended public API for the Microchip MTCH9010 capacitive sensor.
+ * @ingroup mtch9010_interface
+ */
+
 #ifndef ZEPHYR_INCLUDE_DRIVERS_SENSOR_MTCH9010_H_
 #define ZEPHYR_INCLUDE_DRIVERS_SENSOR_MTCH9010_H_
+
+/**
+ * @brief Microchip MTCH9010 capacitive proximity/level sensor
+ * @defgroup mtch9010_interface MTCH9010
+ * @ingroup sensor_interface_ext
+ * @{
+ */
 
 #include <stdint.h>
 
@@ -15,24 +28,31 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
 
+/** @cond INTERNAL_HIDDEN */
 #define MTCH9010_MAX_RESULT 65535
 #define MTCH9010_MIN_RESULT 0
+/** @endcond */
 
+/** @brief Custom sensor channels for the MTCH9010. */
 enum sensor_channel_mtch9010 {
-	/* Polls the state of the OUT line */
+	/** Current state of the OUT line. */
 	SENSOR_CHAN_MTCH9010_OUT_STATE = SENSOR_CHAN_PRIV_START,
-	/* Calculates if the OUT line would be asserted based on previous result */
+	/** Whether the OUT line would be asserted based on the previous result. */
 	SENSOR_CHAN_MTCH9010_SW_OUT_STATE,
-	/* Returns the reference value set for the sensor */
+	/** Reference value set for the sensor. */
 	SENSOR_CHAN_MTCH9010_REFERENCE_VALUE,
-	/* Returns the threshold value set for the sensor */
+	/** Threshold value set for the sensor. */
 	SENSOR_CHAN_MTCH9010_THRESHOLD_VALUE,
-	/* Returns the last measured result */
+	/** Last measured result. */
 	SENSOR_CHAN_MTCH9010_MEAS_RESULT,
-	/* Returns the last measured result */
+	/** Delta between the last measurement and the reference. */
 	SENSOR_CHAN_MTCH9010_MEAS_DELTA,
-	/* Returns true if the heartbeat is an error state */
+	/** True if the heartbeat is in an error state. */
 	SENSOR_CHAN_MTCH9010_HEARTBEAT_ERROR_STATE
 };
+
+/**
+ * @}
+ */
 
 #endif
