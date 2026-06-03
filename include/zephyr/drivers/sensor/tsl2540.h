4966da162a3d3e4a8d2af9a0d87ce7e027d736fa
Benjamin Cabé <benjamin@zephyrproject.org>
2026-06-03 18:18:35 +0200

    include: drivers: sensor: enhance Doxygen documentation for TSL2540 and TSL2591
    Complete the Doxygen documentation for these sensor drivers,
    put in sensor_interface_ext group.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- include/zephyr/drivers/sensor/tsl2540.h
+++ include/zephyr/drivers/sensor/tsl2540.h
@@ -7,6 +7,7 @@
 /**
  * @file
  * @brief Extended public API for AMS's TSL2540 ambient light sensor
+ * @ingroup tsl2540_interface
  *
  * This exposes attributes for the TSL2540 which can be used for
  * setting the on-chip gain and integration time parameters.
@@ -15,36 +16,49 @@
 #ifndef ZEPHYR_INCLUDE_DRIVERS_SENSOR_TSL2540_H_
 #define ZEPHYR_INCLUDE_DRIVERS_SENSOR_TSL2540_H_
 
+/**
+ * @brief AMS TSL2540 ambient light sensor
+ * @defgroup tsl2540_interface TSL2540
+ * @ingroup sensor_interface_ext
+ * @{
+ */
+
 #include <zephyr/drivers/sensor.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/** @brief Extended sensor attributes for the TSL2540 ambient light sensor. */
 enum sensor_attribute_tsl2540 {
-	/* Sensor Integration Time (in ms) */
+	/** Sensor integration time, in ms. */
 	SENSOR_ATTR_INTEGRATION_TIME = SENSOR_ATTR_PRIV_START + 1,
-	/* Sensor ALS interrupt persistence filters */
+	/** Sensor ALS interrupt persistence filter. */
 	SENSOR_ATTR_INT_APERS,
-	/* Shutdown the sensor */
+	/** Shutdown the sensor. */
 	SENSOR_ATTR_TSL2540_SHUTDOWN_MODE,
-	/* Turn on continuous conversion */
+	/** Turn on continuous conversion. */
 	SENSOR_ATTR_TSL2540_CONTINUOUS_MODE,
-	/* Turn on continuous conversion without wait */
+	/** Turn on continuous conversion without wait. */
 	SENSOR_ATTR_TSL2540_CONTINUOUS_NO_WAIT_MODE,
 };
 
+/** @brief ADC gain settings for the TSL2540 ambient light sensor. */
 enum sensor_gain_tsl2540 {
-	TSL2540_SENSOR_GAIN_1_2,
-	TSL2540_SENSOR_GAIN_1,
-	TSL2540_SENSOR_GAIN_4,
-	TSL2540_SENSOR_GAIN_16,
-	TSL2540_SENSOR_GAIN_64,
-	TSL2540_SENSOR_GAIN_128,
+	TSL2540_SENSOR_GAIN_1_2, /**< 1/2x gain. */
+	TSL2540_SENSOR_GAIN_1,   /**< 1x gain. */
+	TSL2540_SENSOR_GAIN_4,   /**< 4x gain. */
+	TSL2540_SENSOR_GAIN_16,  /**< 16x gain. */
+	TSL2540_SENSOR_GAIN_64,  /**< 64x gain. */
+	TSL2540_SENSOR_GAIN_128, /**< 128x gain. */
 };
 
 #ifdef __cplusplus
 }
 #endif
+
+/**
+ * @}
+ */
 
 #endif /* ZEPHYR_INCLUDE_DRIVERS_SENSOR_TSL2540_H_ */
--- include/zephyr/drivers/sensor/tsl2591.h
+++ include/zephyr/drivers/sensor/tsl2591.h
@@ -7,6 +7,7 @@
 /**
  * @file
  * @brief Extended public API for AMS's TSL2591 ambient light sensor
+ * @ingroup tsl2591_interface
  *
  * This exposes attributes for the TSL2591 which can be used for
  * setting the on-chip gain, integration time, and persist filter parameters.
@@ -15,14 +16,22 @@
 #ifndef ZEPHYR_INCLUDE_DRIVERS_SENSOR_TSL2591_H_
 #define ZEPHYR_INCLUDE_DRIVERS_SENSOR_TSL2591_H_
 
+/**
+ * @brief AMS TSL2591 ambient light sensor
+ * @defgroup tsl2591_interface TSL2591
+ * @ingroup sensor_interface_ext
+ * @{
+ */
+
 #include <zephyr/drivers/sensor.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/** @brief Extended sensor attributes for the TSL2591 ambient light sensor. */
 enum sensor_attribute_tsl2591 {
-	/* Sensor ADC Gain Mode
+	/** Sensor ADC Gain Mode.
 	 * Rather than set this value directly, can only be set to operate in one of four modes:
 	 *
 	 * TSL2591_SENSOR_GAIN_LOW
@@ -34,14 +43,14 @@ enum sensor_attribute_tsl2591 {
 	 */
 	SENSOR_ATTR_GAIN_MODE = SENSOR_ATTR_PRIV_START + 1,
 
-	/* Sensor ADC Integration Time (in ms)
+	/** Sensor ADC Integration Time (in ms)
 	 * Can only be set to one of six values:
 	 *
 	 * 100, 200, 300, 400, 500, or 600
 	 */
 	SENSOR_ATTR_INTEGRATION_TIME,
 
-	/* Sensor ALS Interrupt Persist Filter
+	/** Sensor ALS Interrupt Persist Filter.
 	 * Represents the number of consecutive sensor readings outside of a set threshold
 	 * before triggering an interrupt. Can only be set to one of sixteen values:
 	 *
@@ -54,15 +63,20 @@ enum sensor_attribute_tsl2591 {
 	SENSOR_ATTR_INT_PERSIST
 };
 
+/** @brief ADC gain modes for the TSL2591 ambient light sensor. */
 enum sensor_gain_tsl2591 {
-	TSL2591_SENSOR_GAIN_LOW,
-	TSL2591_SENSOR_GAIN_MED,
-	TSL2591_SENSOR_GAIN_HIGH,
-	TSL2591_SENSOR_GAIN_MAX
+	TSL2591_SENSOR_GAIN_LOW,  /**< Low gain mode. */
+	TSL2591_SENSOR_GAIN_MED,  /**< Medium gain mode. */
+	TSL2591_SENSOR_GAIN_HIGH, /**< High gain mode. */
+	TSL2591_SENSOR_GAIN_MAX   /**< Maximum gain mode. */
 };
 
 #ifdef __cplusplus
 }
 #endif
+
+/**
+ * @}
+ */
 
 #endif /* ZEPHYR_INCLUDE_DRIVERS_SENSOR_TSL2591_H_ */
