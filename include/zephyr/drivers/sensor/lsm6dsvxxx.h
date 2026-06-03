9c14f921a27dc76a7e4fafc6b161ecfc4ce8bba7
Benjamin Cabé <benjamin@zephyrproject.org>
2026-06-03 18:24:55 +0200

    include: drivers: sensor: enhance Doxygen documentation for LSM6DSVXXX
    Document self-test result enum and cross-reference it from the
    extended sensor attribute.
    
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- include/zephyr/drivers/sensor/lsm6dsvxxx.h
+++ include/zephyr/drivers/sensor/lsm6dsvxxx.h
@@ -27,14 +27,17 @@
  */
 enum sensor_attribute_lsm6dsvxxx {
 	/**
-	 * Gets the self-test mode result in sensor_value.val1 field.
+	 * Gets the self-test mode result.
+	 * The result is stored in the sensor_value.val1 field.
+	 * See @ref lsm6dsvxxx_self_test_result for possible values.
 	 */
 	SENSOR_ATTR_GET_SELF_TEST_RESULT = SENSOR_ATTR_PRIV_START,
 };
 
+/** @brief Self-test result for the LSM6DSVXXX. */
 enum lsm6dsvxxx_self_test_result {
-	LSM6DSVXXX_ST_OK = 0,
-	LSM6DSVXXX_ST_FAIL = 1,
+	LSM6DSVXXX_ST_OK = 0,   /**< Self-test passed. */
+	LSM6DSVXXX_ST_FAIL = 1, /**< Self-test failed. */
 };
 
 /**
