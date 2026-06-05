226a5cee4c60fc4000f192205d8681afd3e54e95
Frederic Meslin <frederic.meslin@filics.eu>
2026-06-05 13:13:46 +0200

    drivers: i2s: stm32: fix uninitialized variable in DMA TX Callback
    Forces the mem_block_size variable to zero to prevent it non being
    initialized at the dma_tx_callback begin.
    Removes potential for undefined behaviors later on.
    
    Signed-off-by: Fred (marzac) Meslin <frederic.meslin@filics.eu>

--- drivers/i2s/i2s_ll_stm32.c
+++ drivers/i2s/i2s_ll_stm32.c
@@ -606,7 +606,7 @@ static void dma_tx_callback(const struct device *dma_dev, void *arg,
 	const struct i2s_stm32_cfg *cfg = dev->config;
 	struct i2s_stm32_data *const dev_data = dev->data;
 	struct stream *stream = &dev_data->tx;
-	size_t mem_block_size;
+	size_t mem_block_size = 0;
 	int ret;
 
 	if (status < 0) {
