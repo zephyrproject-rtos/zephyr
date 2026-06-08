d0834666d1e946ded33e396de4e3e3bc2955ab36
Emil Gydesen <emil.gydesen@nordicsemi.no>
2026-06-08 12:44:08 +0200

    Bluetooth: ASCS: Use is_valid_num_ases for is_valid_stop_len
    Instead of just checking the lower limit, use
    is_valid_num_ases to also test the upper limit.
    This approach is used by all other control point operations.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/audio/ascs.c
+++ subsys/bluetooth/audio/ascs.c
@@ -2824,8 +2824,6 @@ static bool is_valid_stop_len(struct bt_conn *conn, struct net_buf_simple *buf)
 	const struct bt_ascs_stop_op *op;
 	struct net_buf_simple_state state;
 
-	ARG_UNUSED(conn);
-
 	net_buf_simple_save(buf, &state);
 
 	if (buf->len < sizeof(*op)) {
@@ -2834,8 +2832,7 @@ static bool is_valid_stop_len(struct bt_conn *conn, struct net_buf_simple *buf)
 	}
 
 	op = net_buf_simple_pull_mem(buf, sizeof(*op));
-	if (op->num_ases < 1U) {
-		LOG_WRN("Number_of_ASEs parameter value is less than 1");
+	if (!is_valid_num_ases(conn, op->num_ases)) {
 		return false;
 	}
 
