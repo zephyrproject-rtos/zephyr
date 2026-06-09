c0a98292939b1cabc6379691d7742bc0b24d9a77
Travis Lam <travis.lam@nordicsemi.no>
2026-06-09 15:38:58 +0200

    soc: nordic: nrf7120:  Override Non-secure for MPC03 for RAM
    Override whole RAM to be NS attribute for Wi-Fi Application
    to easier allocation RAM usage.
    
    Signed-off-by: Travis Lam <travis.lam@nordicsemi.no>

--- soc/nordic/nrf71/soc.c
+++ soc/nordic/nrf71/soc.c
@@ -94,8 +94,8 @@ struct mpc_region_override {
 	}
 
 static const struct mpc_region_override mpc00_region_overrides[] = {
-	/* Make RAM_00/01/02 (AMBIX00 + AMBIX03) accessible to all domains */
-	MPC_REGION_OVERRIDE_INIT(0x20000000, 0x200E0000, 0, 0),
+	/* Make RAM_00/01/02/03 (AMBIX00 + AMBIX03) accessible to all domains */
+	MPC_REGION_OVERRIDE_INIT(0x20000000, 0x200FE000, 0, 0),
 	/* Make MRAM accessible to all domains */
 	MPC_REGION_OVERRIDE_INIT(0x00000000, 0x01000000, 0, 0),
 #if CONFIG_SOC_NRF71_WIFI_DAP
@@ -105,8 +105,8 @@ static const struct mpc_region_override mpc00_region_overrides[] = {
 };
 
 static const struct mpc_region_override mpc03_region_overrides[] = {
-	/* Make RAM_02 (AMBIX03) accessible to the Wi-Fi domain for IPC */
-	MPC_REGION_OVERRIDE_INIT(0x200C0000, 0x200E0000, 0, 0),
+	/* Make RAM_02/03 (AMBIX03)  accessible to all domains */
+	MPC_REGION_OVERRIDE_INIT(0x200C0000, 0x200FE000, 0, 0),
 };
 
 static void set_mpc_region_override(NRF_MPC_Type *mpc,
