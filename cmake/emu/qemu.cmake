b405ddba02cb1c25b24c2b8e04eb95be06560532
Carlo Caione <ccaione@baylibre.com>
2026-06-10 11:35:17 +0200

    cmake: qemu: keep host cursor visible with virtio input
    An absolute pointing device makes QEMU hide the host cursor over the
    guest display, but the guest does not draw one.
    
    Signed-off-by: Carlo Caione <ccaione@baylibre.com>

--- cmake/emu/qemu.cmake
+++ cmake/emu/qemu.cmake
@@ -108,6 +108,11 @@ if(CONFIG_DISPLAY)
   else()
     set(QEMU_DISPLAY_BACKEND sdl)
   endif()
+  if(CONFIG_INPUT_VIRTIO)
+    # An absolute pointing device makes QEMU hide the host cursor over the
+    # guest display, but the guest does not draw one
+    string(APPEND QEMU_DISPLAY_BACKEND ",show-cursor=on")
+  endif()
   if(CONFIG_QEMU_RAMFB_DISPLAY)
     list(APPEND QEMU_FLAGS -device ramfb -vga none -display ${QEMU_DISPLAY_BACKEND})
   else()
