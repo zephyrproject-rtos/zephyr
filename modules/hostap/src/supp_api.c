76d68a36a4aee6d062e0a6e8bd3c92966c6a5651
Fengming Ye <frank.ye@nxp.com>
2026-06-11 11:25:40 +0900

    net: wifi: add roaming retry count
    Add roaming retry count to fall back roaming method to next one.
    Roaming 11K/11V may fail because AP does not carry right candidate.
    In this case we can jump to next method when we receive signal change
    event again.
    
    Signed-off-by: Fengming Ye <frank.ye@nxp.com>

--- modules/hostap/src/supp_api.c
+++ modules/hostap/src/supp_api.c
@@ -1732,7 +1732,7 @@ int supplicant_11k_neighbor_request(const struct device *dev, struct net_if *ifa
 	if (wpa_s->reassociate || (wpa_s->wpa_state >= WPA_AUTHENTICATING &&
 	    wpa_s->wpa_state < WPA_COMPLETED)) {
 		wpa_printf(MSG_INFO, "Reassociation is in progress, skip");
-		return 0;
+		return -EALREADY;
 	}
 
 	if (params) {
@@ -2030,7 +2030,7 @@ int supplicant_legacy_roam(const struct device *dev __unused, struct net_if *ifa
 	if (wpa_s->reassociate || (wpa_s->wpa_state >= WPA_AUTHENTICATING &&
 	    wpa_s->wpa_state < WPA_COMPLETED)) {
 		wpa_printf(MSG_INFO, "Reassociation is in progress, skip");
-		ret = 0;
+		ret = -EALREADY;
 		goto out;
 	}
 
@@ -2141,7 +2141,7 @@ int supplicant_btm_query(const struct device *dev __unused, struct net_if *iface
 	if (wpa_s->reassociate || (wpa_s->wpa_state >= WPA_AUTHENTICATING &&
 	    wpa_s->wpa_state < WPA_COMPLETED)) {
 		wpa_printf(MSG_INFO, "Reassociation is in progress, skip");
-		ret = 0;
+		ret = -EALREADY;
 		goto out;
 	}
 
