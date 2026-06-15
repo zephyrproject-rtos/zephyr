e90463789e54f8f170008089edddb40df9910597
Francois Ramu <francois.ramu@st.com>
2026-06-15 11:03:55 +0200

    drivers: mspi: stm32 mspi xspi driver for XiP
    Fix the XiP mode for the mspi_stm32_xspi driver
    
    Authored: Sara Touqan <zephyr@exalt.ps>
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- drivers/mspi/mspi_stm32.h
+++ drivers/mspi/mspi_stm32.h
@@ -133,6 +133,7 @@ struct mspi_stm32_data {
 	const struct mspi_dev_id *dev_id;
 	struct k_mutex lock;
 	struct k_sem sync;
+	struct k_sem sem;
 	struct mspi_dev_cfg dev_cfg;
 	struct mspi_memmap_cfg memmap_cfg;
 	struct stm32_stream dma_tx;
