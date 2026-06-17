8ca2244c13e80e12b7ef66c00139125148ee8c6c
Brian Cain <brian.cain@oss.qualcomm.com>
2026-06-17 16:59:47 -0700

    debug: thread_info: add hexagon stack pointer offset
    Add THREAD_INFO_OFFSET_T_STACK_PTR for Hexagon using switch_handle,
    which holds the saved stack pointer for non-current threads. Required
    by GDB scripts and debugger plugins that walk the thread list.
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- subsys/debug/thread_info.c
+++ subsys/debug/thread_info.c
@@ -90,6 +90,9 @@ const size_t _kernel_thread_info_offsets[] = {
 #elif defined(CONFIG_ARCH_POSIX)
 	[THREAD_INFO_OFFSET_T_STACK_PTR] = offsetof(struct k_thread,
 						callee_saved.thread_status),
+#elif defined(CONFIG_HEXAGON)
+	[THREAD_INFO_OFFSET_T_STACK_PTR] = offsetof(struct k_thread,
+						switch_handle),
 #elif defined(CONFIG_XTENSA)
 /* Xtensa does not store stack pointers inside thread objects.
  * The registers are saved in thread stack where there is
