84ca1d830e8579c0257f3ea05902cec8dc4837ca
JaeHwan Jin <jaehwan.jin@rakwireless.com>
2026-06-21 19:56:46 +0900

    dt-bindings: adc: add WisBlock ADC channel map binding
    Map WisBlock Core Slot ADC channels to the underlying SoC ADC
    controller and channel indices. WisBlock Core Module boards use this
    binding to expose analog inputs through the connector abstraction.
    
    Signed-off-by: JaeHwan Jin <jaehwan.jin@rakwireless.com>

---
+++ include/zephyr/dt-bindings/adc/wisblock-adc.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2026 RAKwireless Technology Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief WisBlock ADC channel constants
+ * @ingroup wisblock-adc
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_ADC_WISBLOCK_ADC_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_ADC_WISBLOCK_ADC_H_
+
+/**
+ * @defgroup wisblock-adc WisBlock ADC
+ * @brief Constants for ADC channels exposed on WisBlock Core modules
+ * @{
+ */
+
+#define WISBLOCK_ADC_AIN0 0 /**< Analog input 0 (AIN0) */
+#define WISBLOCK_ADC_AIN1 1 /**< Analog input 1 (AIN1) */
+#define WISBLOCK_ADC_IO1  2 /**< Optional analog-capable IO1 */
+#define WISBLOCK_ADC_IO2  3 /**< Optional analog-capable IO2 */
+#define WISBLOCK_ADC_IO3  4 /**< Optional analog-capable IO3 */
+#define WISBLOCK_ADC_IO4  5 /**< Optional analog-capable IO4 */
+#define WISBLOCK_ADC_IO5  6 /**< Optional analog-capable IO5 */
+#define WISBLOCK_ADC_IO6  7 /**< Optional analog-capable IO6 */
+#define WISBLOCK_ADC_IO7  8 /**< Optional analog-capable IO7 */
+
+/** @} */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_ADC_WISBLOCK_ADC_H_ */
