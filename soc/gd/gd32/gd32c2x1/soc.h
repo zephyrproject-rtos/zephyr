7c2fc884b8ad3cedc4f22e2ba8e63ade430e036f
Jianping Wu <jianping.wu@gigadevice.com>
2026-06-22 23:31:51 +0800

    soc: add soc support for gd32c2x1
    Add soc support for gd32c2x1.
    
    Signed-off-by: Jianping Wu <jianping.wu@gigadevice.com>

---
+++ soc/gd/gd32/gd32c2x1/soc.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026 GigaDevice Semiconductor Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_GD_GD32_GD32C2X1_SOC_H_
+#define ZEPHYR_SOC_GD_GD32_GD32C2X1_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <gd32c2x1.h>
+
+#endif /* _ASMLANGUAGE */
+
+#endif /* ZEPHYR_SOC_GD_GD32_GD32C2X1_SOC_H_ */
