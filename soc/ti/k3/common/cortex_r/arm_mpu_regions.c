df437ff0f14a49b7ace351c4b2bbda6e7e2e948e
Amneesh Singh <amneesh@ti.com>
2026-06-22 15:19:15 +0530

    soc: ti: k3: cortex_r: add dedicated region for .rodata and .text
    Fix this by using the, now available, __rom_region_mpu_size_bits symbol to
    configure only what is required as U_RO, instead of the roundabout way of
    marking the entire SRAM with U_RO for userspace threads.
    
    Signed-off-by: Amneesh Singh <amneesh@ti.com>

--- soc/ti/k3/common/cortex_r/arm_mpu_regions.c
+++ soc/ti/k3/common/cortex_r/arm_mpu_regions.c
@@ -10,15 +10,11 @@
 #define NODE_ATCM DT_NODELABEL(atcm)
 #define NODE_BTCM DT_NODELABEL(btcm)
 
-/* userspace threads require RO access to .text and .rodata */
-#ifdef CONFIG_USERSPACE
-#define PERM_Msk P_RW_U_RO_Msk
-#else
-#define PERM_Msk P_RW_U_NA_Msk
-#endif
+extern const uint32_t __rom_region_start;
+extern const uint32_t __rom_region_mpu_size_bits;
 
 static const struct arm_mpu_region mpu_regions[] = {
-	MPU_REGION_ENTRY("Device", 0x0, REGION_2G, {MPU_RASR_S_Msk | NOT_EXEC | PERM_Msk}),
+	MPU_REGION_ENTRY("Device", 0x0, REGION_2G, {MPU_RASR_S_Msk | NOT_EXEC | P_RW_U_NA_Msk}),
 
 #if DT_NODE_EXISTS(NODE_ATCM)
 	/* ATCM - Mark as executable since it contains the vector table */
@@ -39,8 +35,13 @@ static const struct arm_mpu_region mpu_regions[] = {
 	/* Current SRAM region */
 	MPU_REGION_ENTRY("SRAM", DT_CHOSEN_SRAM_ADDR,
 			 REGION_CUSTOMED_MEMORY_SIZE(DT_CHOSEN_SRAM_SIZE >> 10),
-			 {NORMAL_OUTER_INNER_WRITE_BACK_WRITE_READ_ALLOCATE_NON_SHAREABLE |
-			  PERM_Msk}),
+			 {P_RW_U_NA_Msk | NOT_EXEC |
+			  NORMAL_OUTER_INNER_WRITE_BACK_WRITE_READ_ALLOCATE_NON_SHAREABLE}),
+
+	/* .rodata and .text should be read-only and executable */
+	MPU_REGION_ENTRY(
+		"ROM region", (uint32_t)&__rom_region_start, (uint32_t)&__rom_region_mpu_size_bits,
+		{P_RO_U_RO_Msk | NORMAL_OUTER_INNER_WRITE_BACK_WRITE_READ_ALLOCATE_NON_SHAREABLE}),
 };
 
 const struct arm_mpu_config mpu_config = {
