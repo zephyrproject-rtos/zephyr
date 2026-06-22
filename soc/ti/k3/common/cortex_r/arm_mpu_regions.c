cd2e31a61e506c8468b4bc3c638aa24fde90828e
Amneesh Singh <amneesh@ti.com>
2026-06-22 15:18:35 +0530

    soc: ti: k3: cortex_r: move MPU configuration from DT to C file
    We now configure ATCM and BTCM via a static configuration, hence we do not
    need to set these in the device tree anymore.
    
    Also move the header inclusion to the file that actually requires it.
    
    Signed-off-by: Amneesh Singh <amneesh@ti.com>

--- soc/ti/k3/common/cortex_r/arm_mpu_regions.c
+++ soc/ti/k3/common/cortex_r/arm_mpu_regions.c
@@ -4,10 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/arch/arm/mpu/arm_mpu_mem_cfg.h>
-#include <zephyr/devicetree.h>
 #include <zephyr/kernel.h>
-#include <zephyr/arch/arm/mpu/arm_mpu.h>
+#include <zephyr/arch/arm/mpu/arm_mpu_mem_cfg.h>
+
+#define NODE_ATCM DT_NODELABEL(atcm)
+#define NODE_BTCM DT_NODELABEL(btcm)
 
 /* userspace threads require RO access to .text and .rodata */
 #ifdef CONFIG_USERSPACE
@@ -17,13 +18,29 @@
 #endif
 
 static const struct arm_mpu_region mpu_regions[] = {
-#if defined CONFIG_SOC_AM2434_R5F0_0
 	MPU_REGION_ENTRY("Device", 0x0, REGION_2G, {MPU_RASR_S_Msk | NOT_EXEC | PERM_Msk}),
+
+#if DT_NODE_EXISTS(NODE_ATCM)
+	/* ATCM - Mark as executable since it contains the vector table */
+	MPU_REGION_ENTRY(
+		"ATCM", DT_REG_ADDR(NODE_ATCM),
+		REGION_CUSTOMED_MEMORY_SIZE(DT_REG_SIZE(NODE_ATCM) >> 10),
+		{P_RO_U_NA_Msk | NORMAL_OUTER_INNER_WRITE_BACK_WRITE_READ_ALLOCATE_NON_SHAREABLE}),
 #endif
 
-	MPU_REGION_ENTRY(
-		"SRAM", DT_CHOSEN_SRAM_ADDR, REGION_SRAM_SIZE,
-		{NORMAL_OUTER_INNER_WRITE_BACK_WRITE_READ_ALLOCATE_NON_SHAREABLE | PERM_Msk}),
+#if DT_NODE_EXISTS(NODE_BTCM)
+	/* BTCM */
+	MPU_REGION_ENTRY("BTCM", DT_REG_ADDR(NODE_BTCM),
+			 REGION_CUSTOMED_MEMORY_SIZE(DT_REG_SIZE(NODE_BTCM) >> 10),
+			 {P_RW_U_NA_Msk | NOT_EXEC |
+			  NORMAL_OUTER_INNER_WRITE_BACK_WRITE_READ_ALLOCATE_NON_SHAREABLE}),
+#endif
+
+	/* Current SRAM region */
+	MPU_REGION_ENTRY("SRAM", DT_CHOSEN_SRAM_ADDR,
+			 REGION_CUSTOMED_MEMORY_SIZE(DT_CHOSEN_SRAM_SIZE >> 10),
+			 {NORMAL_OUTER_INNER_WRITE_BACK_WRITE_READ_ALLOCATE_NON_SHAREABLE |
+			  PERM_Msk}),
 };
 
 const struct arm_mpu_config mpu_config = {
