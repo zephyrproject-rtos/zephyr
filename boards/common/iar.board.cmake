92cb40babfeee41cdd67185c5f9711805fcb1d9f
Gustav Holmberg <gustav.holmberg@qt.io>
2026-06-23 11:13:38 +0200

    runners: iar: create complete launch.json file + other minor fixes
    Generate complete version of debug launch file and use better defaults
    (Enable zephyr plugin, use correct vector table symbol and use reset
    strategy with bootloader when available. Also add launch file path to
    CMakeCache to automatically load the launch file in Embedded Workbench.
    
    Signed-off-by: Gustav Holmberg <gustav.holmberg@qt.io>

--- boards/common/iar.board.cmake
+++ boards/common/iar.board.cmake
@@ -3,3 +3,6 @@
 board_set_debugger_ifnset(iar)
 board_set_flasher_ifnset(iar)
 board_finalize_runner_args(iar)
+
+set(IAR_DEBUG_FILE "${APPLICATION_BINARY_DIR}/launch.json" CACHE FILEPATH
+  "Path to launch file for IAR Embedded Workbench. File is created by the iar flash/debug runner")
