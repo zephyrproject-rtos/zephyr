381c0f3b35b2132117ddc73af3ebb9451e557eb5
Emil Gydesen <emil.gydesen@nordicsemi.no>
2026-06-24 10:17:56 +0200

    tests: Bluetooth: BAP: BA/SD: Add testing of PAST
    Add proper testing of using PAST instead of scanning when it
    is available. This uses both PAST with a PA sync and with
    a local advertising set.
    
    Due to changing in timing, this triggered a bug in the broadcast
    assistant where it does not handle notifications while doing
    long reads. The tests were thus changed so that the scan delegator
    waits for the receive state to be removed properly and that the
    long metadata was cleared when terminating the PA and BIS syncs.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- tests/bsim/bluetooth/audio/test_scripts/bap_broadcast_audio_assistant_colocated_past.sh
+++ tests/bsim/bluetooth/audio/test_scripts/bap_broadcast_audio_assistant_colocated_past.sh
@@ -6,17 +6,17 @@
 
 source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
 
-SIMULATION_ID="${BOARD_TS}_bap_broadcast_audio_assistant_colocated"
+SIMULATION_ID="${BOARD_TS}_bap_broadcast_audio_assistant_colocated_past"
 VERBOSITY_LEVEL=2
 EXECUTE_TIMEOUT=240
 
 cd ${BSIM_OUT_PATH}/bin
 
-printf "\n\n======== Running BAP Broadcast Audio Assistant with Colocated Source =========\n\n"
+printf "\n\n===== Running BAP Broadcast Audio Assistant with Colocated Source with PAST ======\n\n"
 
 Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_audio_prj_conf \
   -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=0 \
-  -testid=broadcast_sink_with_assistant -RealEncryption=1 -rs=24 -D=2 -argstest past 0
+  -testid=broadcast_sink_with_assistant -RealEncryption=1 -rs=24 -D=2 -argstest past 1
 
 Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_audio_prj_conf \
   -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=1 \
