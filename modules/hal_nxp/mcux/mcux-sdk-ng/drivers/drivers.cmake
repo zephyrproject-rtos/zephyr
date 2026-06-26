vignesh-aerlync/zephyr_fork

modules: hal_nxp: mcux: enable iap driver for LPC84x
Enable the iap driver to LPC84x series when CONFIG_SOC_FLASH_LPC84X_IAP
is set.

Signed-off-by: Dilip Raman <dilipr@aerlync.com>
Signed-off-by: Sayooj K Karun <sayooj@aerlync.com>

modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
@@ -291,6 +291,8 @@ if(CONFIG_SOC_SERIES_LPC51U68 OR CONFIG_SOC_SERIES_LPC54XXX)
   set_variable_ifdef(CONFIG_SOC_FLASH_MCUX CONFIG_MCUX_COMPONENT_driver.iap)
 endif()
 
+set_variable_ifdef (CONFIG_SOC_FLASH_LPC84X_IAP CONFIG_MCUX_COMPONENT_driver.iap)
+
 if(CONFIG_SOC_SERIES_LPC51U68 OR CONFIG_SOC_SERIES_LPC54XXX)
   set_variable_ifdef(CONFIG_ENTROPY_MCUX_RNG CONFIG_MCUX_COMPONENT_driver.rng)
 endif()

zephyrproject-rtos/zephyr

modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
@@ -329,6 +329,7 @@ if(CONFIG_SOC_SERIES_LPC84X)
   endif()
   set_variable_ifdef(CONFIG_UART_LPC84X CONFIG_MCUX_COMPONENT_driver.lpc_miniusart)
   zephyr_library_compile_definitions_ifdef(CONFIG_UART_LPC84X FSL_SDK_DISABLE_DRIVER_CLOCK_CONTROL=1)
+  set_variable_ifdef(CONFIG_FLASH_SOC_LPC84X_IAP CONFIG_MCUX_COMPONENT_driver.iap)
 endif()
 
 if(CONFIG_SOC_LPC55S36)