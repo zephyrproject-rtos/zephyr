9841c82a0e9fcf83d27f3401d0654d10bc9892ae
Etienne Carriere <etienne.carriere@st.com>
2026-06-26 19:04:47 +0200

    settings: use diamond include for non-local header files
    Use #include <> instead of #include "" to include a header file which path
    is not relative to the directory path of the file emitting the #include
    directive.
    
    This change was made running scripts/check_quoted_includes.py script
    proposed in https://github.com/zephyrproject-rtos/zephyr/pull/112135
    with the Linux shell command below and manually selecting the applicable
    changes:
    $ find subsys/settings/ -type f -exec \
        ./scripts/check_quoted_includes.py -w {} \;
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- subsys/settings/src/settings_fcb.c
+++ subsys/settings/src/settings_fcb.c
@@ -11,7 +11,7 @@
 #include <string.h>
 
 #include <zephyr/settings/settings.h>
-#include "settings/settings_fcb.h"
+#include <settings/settings_fcb.h>
 #include "settings_priv.h"
 
 #include <zephyr/logging/log.h>
