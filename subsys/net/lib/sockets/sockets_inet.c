5dfb211f78a98bd26028e39240a56db020e20810
Andrea Gilardoni <a.gilardoni@arduino.cc>
2026-06-29 16:08:16 +0200

    tcp: recv_stream: implementing peek logic
    Instead of requeuing packets when peeking, get the next element in the
    queue
    
    Signed-off-by: Andrea Gilardoni <a.gilardoni@arduino.cc>

--- subsys/net/lib/sockets/sockets_inet.c
+++ subsys/net/lib/sockets/sockets_inet.c
@@ -1375,26 +1375,15 @@ static size_t zsock_recv_stream_immediate(struct net_context *ctx, uint8_t **buf
 	size_t len;
 	size_t pkt_len;
 	size_t recv_len = 0;
-	struct net_pkt *pkt;
+	struct net_pkt *pkt, *next = NULL;
 	struct net_pkt_cursor backup;
-	struct net_pkt *origin = NULL;
 	const bool do_recv = !(buf == NULL || max_len == NULL);
 	size_t _max_len = (max_len == NULL) ? SIZE_MAX : *max_len;
 	const bool peek = (flags & ZSOCK_MSG_PEEK) == ZSOCK_MSG_PEEK;
-	const struct net_pkt *head = k_fifo_peek_head(&ctx->recv_q);
 
-	while (_max_len > 0) {
-		/* only peek until we know we can dequeue and / or requeue buffer */
-		pkt = k_fifo_peek_head(&ctx->recv_q);
-		if (pkt == NULL || pkt == origin) {
-			break;
-		}
+	pkt = k_fifo_peek_head(&ctx->recv_q);
 
-		if (origin == NULL) {
-			/* mark first pkt to avoid cycles when observing */
-			origin = pkt;
-		}
-
+	while (_max_len > 0 && pkt != NULL) {
 		pkt_len = net_pkt_remaining_data(pkt);
 		len = MIN(_max_len, pkt_len);
 		recv_len += len;
@@ -1414,6 +1403,8 @@ static size_t zsock_recv_stream_immediate(struct net_context *ctx, uint8_t **buf
 			}
 		}
 
+		next = k_fifo_peek_next(&ctx->recv_q, pkt);
+
 		if (do_recv && !peek) {
 			if (len == pkt_len) {
 				/* dequeue empty packets when not observing */
@@ -1429,23 +1420,14 @@ static size_t zsock_recv_stream_immediate(struct net_context *ctx, uint8_t **buf
 
 				net_pkt_unref(pkt);
 			}
-		} else if ((!do_recv || peek) && _max_len > 0) {
-			/* requeue packets when observing, do not requeue if it is the last packet
-			 * as it will be requeued below. This allows to skip requeuing when
-			 * observing only the first packet
-			 */
-			k_fifo_put(&ctx->recv_q, k_fifo_get(&ctx->recv_q, K_NO_WAIT));
 		}
+
+		pkt = next;
 	}
 
 	if (do_recv) {
 		/* convey remaining buffer size back to caller */
 		*max_len = _max_len;
-	}
-
-	/* when observing, the queue should return to the initial state */
-	while ((!do_recv || peek) && k_fifo_peek_head(&ctx->recv_q) != head) {
-		k_fifo_put(&ctx->recv_q, k_fifo_get(&ctx->recv_q, K_NO_WAIT));
 	}
 
 	return recv_len;
