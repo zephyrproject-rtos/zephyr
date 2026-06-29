0ecf0b29f1f64a54ec62daae9e522e9fe363cf1d
Etienne Carriere <etienne.carriere@st.com>
2026-06-29 08:24:15 +0200

    include: zephyr: arch: arm: Rename header file guard macros
    Update header files in include/zephyr/ to use a ZEPHYR_INCLUDE_ prefixed
    header guard macro with the macro name matching the header file path
    in include/zephyr/ file tree.
    
    These changes were made using zephyr_header_guards.py script proposed
    in https://github.com/zephyrproject-rtos/zephyr/pull/111768 with a
    Linux shell command like the on below:
    $ scripts/zephyr_header_guards.py --apply include/zephyr/arch/arm
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- include/zephyr/arch/arm/mmu/arm_mem.h
+++ include/zephyr/arch/arm/mmu/arm_mem.h
@@ -3,8 +3,8 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#ifndef ZEPHYR_INCLUDE_ARCH_ARM_ARM_MEM_H_
-#define ZEPHYR_INCLUDE_ARCH_ARM_ARM_MEM_H_
+#ifndef ZEPHYR_INCLUDE_ARCH_ARM_MMU_ARM_MEM_H_
+#define ZEPHYR_INCLUDE_ARCH_ARM_MMU_ARM_MEM_H_
 
 /*
  * Define ARM specific memory flags used by k_mem_map_phys_bare()
@@ -14,4 +14,4 @@
 /** ARM Specific flags: normal memory with Non-cacheable */
 #define K_MEM_ARM_NORMAL_NC	5
 
-#endif /* ZEPHYR_INCLUDE_ARCH_ARM_ARM_MEM_H_ */
+#endif /* ZEPHYR_INCLUDE_ARCH_ARM_MMU_ARM_MEM_H_ */
--- include/zephyr/arch/arm/mpu/arm_mpu_v7m.h
+++ include/zephyr/arch/arm/mpu/arm_mpu_v7m.h
@@ -5,6 +5,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#ifndef ZEPHYR_INCLUDE_ARCH_ARM_MPU_ARM_MPU_V7M_H_
+#define ZEPHYR_INCLUDE_ARCH_ARM_MPU_ARM_MPU_V7M_H_
+
 #ifndef _ASMLANGUAGE
 
 #include <cmsis_core.h>
@@ -294,3 +297,5 @@ typedef struct {
 #define _ARCH_MEM_PARTITION_ALIGN_CHECK(start, size)                                               \
 	_ARCH_MEM_PARTITION_ALIGN_CHECK_SIZE(size);                                                \
 	_ARCH_MEM_PARTITION_ALIGN_CHECK_START(start, size)
+
+#endif /* ZEPHYR_INCLUDE_ARCH_ARM_MPU_ARM_MPU_V7M_H_ */
