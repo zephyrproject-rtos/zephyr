79817c51fe28cfa947713901f4a1d32fc6145688
Samuel Slesar <samuel.slesar@nxp.com>
2026-07-02 10:00:02 +0200

    dts: arm: nxp: mcxl: add WUU wakeup controller
    Describe the MCXL WUU as an NXP wakeup controller.
    
    Use the nxp,wuc-wuu compatible with wakeup controller cells so wakeup
    sources can be consumed through the Zephyr WUC subsystem. Gate the MCUX
    driver.wuu HAL component (which provides fsl_wuu.h) on
    CONFIG_DT_HAS_NXP_WUC_WUU_ENABLED so the single compatible pulls in the
    HAL sources without needing the legacy nxp,wuu compatible.
    
    Signed-off-by: Samuel Slesar <samuel.slesar@nxp.com>

--- modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
+++ modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
@@ -128,6 +128,7 @@ set_variable_ifdef(CONFIG_UART_MCUX_IUART       CONFIG_MCUX_COMPONENT_driver.iua
 set_variable_ifdef(CONFIG_ADC_MCUX_12B1MSPS_SAR CONFIG_MCUX_COMPONENT_driver.adc_12b1msps_sar)
 set_variable_ifdef(CONFIG_HWINFO_NXP_SRC       CONFIG_MCUX_COMPONENT_driver.src)
 set_variable_ifdef(CONFIG_DT_HAS_NXP_WUU_ENABLED CONFIG_MCUX_COMPONENT_driver.wuu)
+set_variable_ifdef(CONFIG_DT_HAS_NXP_WUC_WUU_ENABLED CONFIG_MCUX_COMPONENT_driver.wuu)
 set_variable_ifdef(CONFIG_HWINFO_NXP_SIM       CONFIG_MCUX_COMPONENT_driver.sim)
 set_variable_ifdef(CONFIG_HWINFO_NXP_RCM       CONFIG_MCUX_COMPONENT_driver.rcm)
 set_variable_ifdef(CONFIG_IPM_MCUX              CONFIG_MCUX_COMPONENT_driver.mailbox)
