fd4ac056bd34a12419325a55286b28a01a620019
Khai Do <dovankhai97@gmail.com>
2026-07-03 23:15:46 +0700

    clock_control: sunxi: Add base H618 CCU driver
    This adds the base driver for the Allwinner H616/H618 Clock Control Unit.
    
    Signed-off-by: Khai Do <dovankhai97@gmail.com>

---
+++ include/zephyr/dt-bindings/clock/sun50i-h618-ccu.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2026 Khai Do
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_SUN50I_H618_CCU_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_SUN50I_H618_CCU_H_
+
+/**
+ * @file
+ * @brief Sunxi H618 Clock Control Unit definitions
+ */
+
+/** @brief PLL Peripheral 0 clock */
+#define CLK_PLL_PERIPH0		4
+/** @brief APB2 bus clock */
+#define CLK_APB2		27
+
+/** @brief UART0 bus clock */
+#define CLK_BUS_UART0		66
+/** @brief UART1 bus clock */
+#define CLK_BUS_UART1		67
+/** @brief UART2 bus clock */
+#define CLK_BUS_UART2		68
+/** @brief UART3 bus clock */
+#define CLK_BUS_UART3		69
+/** @brief UART4 bus clock */
+#define CLK_BUS_UART4		70
+/** @brief UART5 bus clock */
+#define CLK_BUS_UART5		71
+
+#endif
