5fce1faf7d9183acc4fac99287ac3728d736ae8c
Khai Do <dovankhai97@gmail.com>
2026-07-03 23:15:46 +0700

    clock_control: sunxi: Add base H618 CCU driver
    This adds the base driver for the Allwinner H616/H618 Clock Control Unit.
    
    Signed-off-by: Khai Do <dovankhai97@gmail.com>

---
+++ include/zephyr/dt-bindings/reset/sun50i-h618-ccu.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2026 Khai Do
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_RESET_SUN50I_H618_CCU_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_RESET_SUN50I_H618_CCU_H_
+
+/**
+ * @file
+ * @brief Sunxi H618 CCU Reset definitions
+ */
+
+/** @brief UART0 reset */
+#define RST_BUS_UART0		17
+/** @brief UART1 reset */
+#define RST_BUS_UART1		18
+/** @brief UART2 reset */
+#define RST_BUS_UART2		19
+/** @brief UART3 reset */
+#define RST_BUS_UART3		20
+/** @brief UART4 reset */
+#define RST_BUS_UART4		21
+/** @brief UART5 reset */
+#define RST_BUS_UART5		22
+
+#endif
