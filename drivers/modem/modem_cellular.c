ea68151e2f0b706ee1f41e3cfa7dc459e857a405
Jordan Yates <jordan@embeint.com>
2026-07-06 15:07:31 +1000

    modem: cellular: don't run `NULL` script
    Don't attempt to run a `NULL` periodic script, and add assertions into
    `modem_chat` to raise the issue if this does occur.
    
    Signed-off-by: Jordan Yates <jordan@embeint.com>

--- drivers/modem/modem_cellular.c
+++ drivers/modem/modem_cellular.c
@@ -1519,6 +1519,9 @@ static void modem_cellular_await_registered_event_handler(struct modem_cellular_
 		break;
 
 	case MODEM_CELLULAR_EVENT_TIMEOUT:
+		if (config->vendor->scripts.periodic == NULL) {
+			break;
+		}
 		if (atomic_get(&data->periodic_paused)) {
 			data->periodic_timeout_skipped = true;
 			break;
@@ -1605,6 +1608,9 @@ static void modem_cellular_registered_event_handler(struct modem_cellular_data *
 		break;
 
 	case MODEM_CELLULAR_EVENT_TIMEOUT:
+		if (config->vendor->scripts.periodic == NULL) {
+			break;
+		}
 		if (atomic_get(&data->periodic_paused)) {
 			data->periodic_timeout_skipped = true;
 			break;
