068450ebfbccd4f5d6df6d06d5aad1f92aa6645e
Tarang Patel <tarang3399.patel@gmail.com>
2026-07-07 13:42:21 +0000

    boards: ubx_evk_iris_w1: add multi-flash revision support
    Add fidelix_8mb/16mb and macronix_8mb/16mb board revisions.
    
    Signed-off-by: Tarang Patel <tarang3399.patel@gmail.com>

--- boards/u-blox/ubx_evk_iris_w1/revision.cmake
+++ boards/u-blox/ubx_evk_iris_w1/revision.cmake
@@ -1,16 +1,16 @@
 # Copyright (c) 2025 u-blox AG
 # SPDX-License-Identifier: Apache-2.0
 
-set(BOARD_REVISIONS "macronix" "fidelix")
+set(BOARD_REVISIONS "fidelix" "fidelix_16mb" "macronix" "macronix_16mb")
 
 if(NOT BOARD_REVISION)
   set(BOARD_REVISION "fidelix")
 endif()
 
 # Validate revision
-if(NOT BOARD_REVISION STREQUAL "macronix" AND NOT BOARD_REVISION STREQUAL "fidelix")
+if(NOT BOARD_REVISION IN_LIST BOARD_REVISIONS)
   message(FATAL_ERROR
     "Invalid BOARD_REVISION: ${BOARD_REVISION}\n"
-    "Must be one of: macronix, fidelix"
+    "Must be one of: ${BOARD_REVISIONS}"
   )
 endif()
