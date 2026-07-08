e276b96e8f10403b33bb1baa206d4f8326c202ef
Mathieu Choplain <mathieu.choplain-ext@st.com>
2026-07-08 18:52:38 +0200

    include: drivers: misc: stm32_wkup_pins: make the API private
    This API was never intended to be "public" in the sense now documented
    in the Glossary, as said in 559c72d7a63446975d582162f8d460fa29438518:
      "Introduce public functions for GPIO pins configuration [...]  in
       stm32 drivers & routines such gpio_stm32 & poweroff."
    
    Remove the header from the public API directory and move the definitions
    to a private header in SoC-specific includes. Update all previous users
    of the header (which are as expected only those documented in the commit
    message quoted above: gpio_stm32 and SoC poweroff.c).
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain-ext@st.com>

--- soc/st/stm32/stm32f4x/poweroff.c
+++ soc/st/stm32/stm32f4x/poweroff.c
@@ -9,9 +9,9 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/poweroff.h>
 #include <zephyr/toolchain.h>
-#include <zephyr/drivers/misc/stm32_wkup_pins/stm32_wkup_pins.h>
 
 #include <stm32_common.h>
+#include <stm32_gpio_shared.h>
 #include <stm32_ll_pwr.h>
 
 void z_sys_poweroff(void)
