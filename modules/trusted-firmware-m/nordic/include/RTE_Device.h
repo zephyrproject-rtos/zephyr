zephyrproject-rtos/zephyr

commit 192ec301a3876cf73fabf8cfd891e5ca8ce9b132

manifest: Update tf-m to enable secure output on RTE_USART21
nRF7120DK will use uart21 for console output from S domain and so
RTE_USART21 must be included for config.

Signed-off-by: Robert Robinson <robert.robinson@nordicsemi.no>

modules/trusted-firmware-m/nordic/include/RTE_Device.h
@@ -44,6 +44,18 @@
 	) \
 }
 
+#elif DT_PINCTRL_HAS_NAME(DT_NODELABEL(uart21), default)
+
+#define RTE_USART21 1
+
+#define RTE_USART21_PINS \
+{ \
+	DT_FOREACH_CHILD_VARGS( \
+		DT_PINCTRL_BY_NAME(DT_NODELABEL(uart21), default, 0), \
+		DT_FOREACH_PROP_ELEM, psels, UART_PIN_INIT \
+	) \
+}
+
 #elif DT_PINCTRL_HAS_NAME(DT_NODELABEL(uart30), default)
 
 #define RTE_USART30 1

modules/trusted-firmware-m/nordic/include/tfm_peripherals_config.h
@@ -13,8 +13,10 @@ extern "C" {
 
 #ifdef SECURE_UART1
 
-#if defined(NRF54L_SERIES) || defined(NRF71_SERIES)
+#if defined(NRF54L_SERIES)
 #define TFM_PERIPHERAL_UARTE30_SECURE 1
+#elif defined(NRF71_SERIES)
+#define TFM_PERIPHERAL_UARTE21_SECURE 1
 #else
 #define TFM_PERIPHERAL_UARTE1_SECURE 1
 #endif

west.yml
@@ -399,7 +399,7 @@ manifest:
       groups:
         - tee
     - name: trusted-firmware-m
-      revision: 47b9f07b9ec2c879acc38709e9e7ba3202143a38
+      revision: f6ee0e2b3c049b0f054dbbcb76e827dc61a4f7d6
       path: modules/tee/tf-m/trusted-firmware-m
       groups:
         - tee

commit 761d62c4745014742c048a8362fd93721cfa7d3e

modules: tfm: nordic: gate RTE_USART21 on secure UART instance
Gate the uart21 branch on NRF_SECURE_UART_INSTANCE == 21 so it only applies
on platforms whose secure UART actually is instance 21 (nrf7120). Boards
using uart30 as the secure console fall through to the uart30 branch again.

Fixes issue introduced with commit 192ec301a38.

Signed-off-by: Anas Nashif <anas.nashif@intel.com>

modules/trusted-firmware-m/nordic/include/RTE_Device.h
@@ -44,7 +44,7 @@
 	) \
 }
 
-#elif DT_PINCTRL_HAS_NAME(DT_NODELABEL(uart21), default)
+#elif DT_PINCTRL_HAS_NAME(DT_NODELABEL(uart21), default) && (NRF_SECURE_UART_INSTANCE == 21)
 
 #define RTE_USART21 1
 