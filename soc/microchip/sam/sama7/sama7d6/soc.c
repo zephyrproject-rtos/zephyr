zephyrproject-rtos/zephyr

commit e79081f4443c4d646b196fc456eef736134037e6

soc: microchip: sama7d6: update MMU and config generic clocks for MCAN
When the MCAN is activated in the DT, configure it's register region
with strong ordered, read and write access.
As CANx accesses the lower or upper 64K SRAM is selected by bits in
register 'SFR_CAN_SRAM_SEL', also configure SRAM and SFR region with
strong ordered, read and write access.
Configure the generic clocks for CANx when it is enabled in the dt.

Signed-off-by: Tony Han <tony.han@microchip.com>

soc/microchip/sam/sama7/sama7d6/soc.c
@@ -15,6 +15,11 @@
 					       MT_STRONGLY_ORDERED | MPERM_R | MPERM_W),),	\
 			())
 
+#define MMU_REGION_MCAN_DEFN(idx, n)								\
+		IF_ENABLED(DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(mcan##n)),			\
+			   (MMU_REGION_FLAT_ENTRY("mcan"#n, MCAN##n##_BASE_ADDRESS, 0x4000,	\
+						  MT_STRONGLY_ORDERED | MPERM_R | MPERM_W),))
+
 static const struct arm_mmu_region mmu_regions[] = {
 	MMU_REGION_FLAT_ENTRY("vectors", CONFIG_KERNEL_VM_BASE, 0x1000,
 			      MT_STRONGLY_ORDERED | MPERM_R | MPERM_X),
@@ -23,10 +28,20 @@ static const struct arm_mmu_region mmu_regions[] = {
 		   (MMU_REGION_FLAT_ENTRY("aes", AES_BASE_ADDRESS, 0x100,
 					  MT_STRONGLY_ORDERED | MPERM_R | MPERM_W),))
 
+	IF_ENABLED(DT_HAS_COMPAT_STATUS_OKAY(atmel_sam_can),
+		   (MMU_REGION_FLAT_ENTRY("sram", IRAM_ADDR, IRAM_SIZE,
+					  MT_STRONGLY_ORDERED | MPERM_R | MPERM_W),))
+
+	IF_ENABLED(DT_HAS_COMPAT_STATUS_OKAY(atmel_sam_can),
+		   (MMU_REGION_FLAT_ENTRY("sfr", SFR_BASE_ADDRESS, 0x2050,
+					  MT_STRONGLY_ORDERED | MPERM_R | MPERM_W),))
+
 	FOR_EACH_IDX(MMU_REGION_FLEXCOM_DEFN, (), 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 	MMU_REGION_FLAT_ENTRY("gic", GIC_DISTRIBUTOR_BASE, 0x1100,
 			      MT_STRONGLY_ORDERED | MPERM_R | MPERM_W),
+
+	FOR_EACH_IDX(MMU_REGION_MCAN_DEFN, (), 0, 1, 2, 3, 4)
 
 	MMU_REGION_FLAT_ENTRY("pioa", PIO_BASE_ADDRESS, 0x4000,
 			      MT_STRONGLY_ORDERED | MPERM_R | MPERM_W),
@@ -55,10 +70,21 @@ void relocate_vector_table(void)
 	write_vbar(CONFIG_KERNEL_VM_BASE);
 }
 
+#define MCAN_CLK_INIT_DEFN(idx, n)								\
+		IF_ENABLED(DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(mcan##n)),			\
+			   (PMC_REGS->PMC_PCR = PMC_PCR_PID(ID_MCAN##n);			\
+			    PMC_REGS->PMC_PCR = PMC_REGS->PMC_PCR | PMC_PCR_CMD_Msk |		\
+						PMC_PCR_GCLKEN_Msk | PMC_PCR_EN_Msk |		\
+						PMC_PCR_GCLKDIV(4) | PMC_PCR_GCLKCSS_MCK1 |	\
+						PMC_PCR_PID(ID_MCAN##n);))
+
 void soc_early_init_hook(void)
 {
 	/* Enable Generic clock for PIT64B0 for system tick */
 	PMC_REGS->PMC_PCR = PMC_PCR_CMD(1) | PMC_PCR_GCLKEN(1) | PMC_PCR_EN(1) |
 			    PMC_PCR_GCLKDIV(20 - 1) | PMC_PCR_GCLKCSS_MCK1 |
 			    PMC_PCR_PID(ID_PIT64B0);
+
+	/* Enable generic clock for MCANx, frequency MCK1 / (4 + 1) = 40MHz */
+	FOR_EACH_IDX(MCAN_CLK_INIT_DEFN, (), 0, 1, 2, 3, 4)
 }

commit 4b339a8a4b4fc87d756fc2949cb510ff826c89c7

soc: microchip: sama7d6: fix the config of peripheral clock for MCAN
Correct the value written to the PMC_PCR register when configuring the
MCAN peripheral clock. Previously, incorrect GCLKDIV or GCLKCSS values
might be written if the MCAN clocks had already been configured before
Zephyr started.

Fixes: e79081f4443c ("soc: microchip: sama7d6: update MMU and config
generic clocks for MCAN")

Signed-off-by: Tony Han <tony.han@microchip.com>

soc/microchip/sam/sama7/sama7d6/soc.c
@@ -73,7 +73,7 @@ void relocate_vector_table(void)
 #define MCAN_CLK_INIT_DEFN(idx, n)								\
 		IF_ENABLED(DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(mcan##n)),			\
 			   (PMC_REGS->PMC_PCR = PMC_PCR_PID(ID_MCAN##n);			\
-			    PMC_REGS->PMC_PCR = PMC_REGS->PMC_PCR | PMC_PCR_CMD_Msk |		\
+			    PMC_REGS->PMC_PCR = PMC_PCR_CMD_Msk |				\
 						PMC_PCR_GCLKEN_Msk | PMC_PCR_EN_Msk |		\
 						PMC_PCR_GCLKDIV(4) | PMC_PCR_GCLKCSS_MCK1 |	\
 						PMC_PCR_PID(ID_MCAN##n);))