f583454d0f6a5ae4cbf7fc920b3f834a54db903b
Michał Stasiak <michal.stasiak@nordicsemi.no>
2026-07-09 10:34:20 +0200

    modules: trusted-firmware-m: add support for nRF54LV10A
    Added support for nRF54LV10A SoC in TFM module.
    
    Signed-off-by: Michał Stasiak <michal.stasiak@nordicsemi.no>

---
+++ modules/trusted-firmware-m/nordic/nrf54lv10a_cpuapp/config.cmake
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2026, Nordic Semiconductor ASA.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set(NRF_SOC_VARIANT nrf54lv10a CACHE STRING "nRF SoC Variant")
+
+include(${PLATFORM_PATH}/common/nrf54lv10a/config.cmake)
