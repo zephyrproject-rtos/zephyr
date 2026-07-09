b7595dcdca1f690102ab363a8179bdd103b311b1
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2026-07-09 15:27:36 +0100

    xtensa: adsp: record triple fault breadcrumbs in HP-SRAM window0
    When a double exception escalates to a triple fault (escaping the CPU),
    record the EPC1, double-exception EXCCAUSE, and DEPC into HP-SRAM
    window0 via its uncached alias. This is a lightweight tracing aid that
    can be enabled in production builds to diagnose fatal CPU locks and
    double faults.
    
    Add CONFIG_XTENSA_ADSP_TRIPLE_FAULT_BREADCRUMB (default y, depends on
    SOC_FAMILY_INTEL_ADSP) to govern this feature independently of general
    fatal exception breadcrumbs.
    
    Query the window0 base address and uncached region via Device Tree macros
    (HP_SRAM_WIN0_BASE and CONFIG_INTEL_ADSP_UNCACHED_REGION) to prevent using
    hardcoded magic pointers.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- soc/intel/intel_adsp/common/include/adsp_fatal_breadcrumb.h
+++ soc/intel/intel_adsp/common/include/adsp_fatal_breadcrumb.h
@@ -45,4 +45,40 @@
 #endif /* CONFIG_XTENSA_ADSP_FATAL_BREADCRUMB */
 
 #endif /* !_ASMLANGUAGE */
+
+#ifdef _ASMLANGUAGE
+
+#if defined(CONFIG_XTENSA_ADSP_TRIPLE_FAULT_BREADCRUMB)
+#if defined(CONFIG_SOC_SERIES_INTEL_ADSP_ACE)
+#define _ADSP_BC_WIN0_BASE_UC 0x40024000
+#else
+#define _ADSP_BC_WIN0_BASE_UC 0x5e024000
+#endif
+#endif /* CONFIG_XTENSA_ADSP_TRIPLE_FAULT_BREADCRUMB */
+
+/*
+ * adsp_triple_fault_breadcrumb - record triple fault state into HP-SRAM window0.
+ *
+ * Writes EPC1, EXCCAUSE|0xdb1e0000 and DEPC to the HP-SRAM window0 uncached
+ * alias so the host SOF driver can diagnose a triple fault from dmesg.
+ * Clobbers a2/a3.  Called from _TripleFault with no stack available.
+ */
+.macro adsp_triple_fault_breadcrumb
+#if defined(CONFIG_XTENSA_ADSP_TRIPLE_FAULT_BREADCRUMB)
+	rsr.epc1 a2
+	movi a3, _ADSP_BC_WIN0_BASE_UC
+	s32i a2, a3, 0
+	rsr.exccause a2
+	movi a3, 0xdb1e0000
+	or a2, a2, a3
+	movi a3, _ADSP_BC_WIN0_BASE_UC
+	s32i a2, a3, 4
+	rsr.depc a2
+	movi a3, _ADSP_BC_WIN0_BASE_UC
+	s32i a2, a3, 8
+	memw
+#endif /* CONFIG_XTENSA_ADSP_TRIPLE_FAULT_BREADCRUMB */
+.endm
+
+#endif /* _ASMLANGUAGE */
 #endif /* ZEPHYR_SOC_INTEL_ADSP_FATAL_BREADCRUMB_H_ */
