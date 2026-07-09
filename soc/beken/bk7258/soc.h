b58297721df3e1fd39060e28b1b659123291f6fb
Jian Li <15588296118@163.com>
2026-07-09 18:40:28 +0800

    soc: beken: add BK7258 SoC support
    Add initial SoC support for the Beken BK7258 IoT SoC.
    
    The BK7258 is based on ARM STAR-MC1 (ARMv8-M Mainline, compatible
    with Cortex-M33 instruction set). It features 640 KB SRAM, 4 MB
    internal flash, Wi-Fi 6, and BLE 5.4.
    
    This commit adds the SoC skeleton, Kconfig definitions, and
    devicetree include for the BK7258 SoC.
    
    Signed-off-by: Jian Li <15588296118@163.com>

---
+++ soc/beken/bk7258/soc.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026 Embracecactus
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_BEKEN_BK7258_SOC_H_
+#define ZEPHYR_SOC_BEKEN_BK7258_SOC_H_
+
+#ifndef _ASMLANGUAGE
+#include <cmsis_core_m_defaults.h>
+#include <zephyr/sys/util.h>
+#endif /* !_ASMLANGUAGE */
+
+#endif /* ZEPHYR_SOC_BEKEN_BK7258_SOC_H_ */
