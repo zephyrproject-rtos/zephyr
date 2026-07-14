3003576743ee6a001fb121d4a11d052a835d9a7f
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2026-07-14 15:30:31 +0300

    net: shell: pmtu: Add DPLPMTUD support
    Print also DPLPMTUD cache information for "net pmtu" command
    like this:
    
    $ net pmtu
    PMTU destination cache content:
                              Destination Address   MTU  Age (sec)
    [ 1]                                192.0.2.2  1500  14
    [ 2]                              2001:db8::2  1500  5
    [ 3]                 fe80::200:5eff:fe00:53ff  1500  0
    DPLPMTUD destination cache content:
                              Destination Address   PLPMTU  Age (sec)
    [ 1]                                192.0.2.2     1200  14
    [ 2]                              2001:db8::2     1200  5
    [ 3]                 fe80::200:5eff:fe00:53ff     1200  0
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- subsys/net/lib/shell/pmtu.c
+++ subsys/net/lib/shell/pmtu.c
@@ -10,21 +10,18 @@ LOG_MODULE_DECLARE(net_shell);
 #include "net_shell_private.h"
 
 #include "pmtu.h"
+#include "dplpmtud_internal.h"
 
 #if !defined(CONFIG_NET_PMTU)
 static void print_pmtu_error(const struct shell *sh)
 {
 	PR_INFO("Set %s to enable %s support.\n",
 		"CONFIG_NET_IPV6_PMTU or CONFIG_NET_IPV4_PMTU", "PMTU");
+	PR_INFO("Set %s to enable %s support.\n",
+		"CONFIG_NET_IPV6_PMTU_DPLPMTUD or CONFIG_NET_IPV4_PMTU_DPLPMTUD",
+		"DPLPMTUD");
 }
 #endif
-
-#if defined(CONFIG_NET_PMTU)
-static void pmtu_cb(struct net_pmtu_entry *entry, void *user_data)
-{
-	struct net_shell_user_data *data = user_data;
-	const struct shell *sh = data->sh;
-	int *count = data->user_data;
 
 #if defined(CONFIG_NET_IPV4) && defined(CONFIG_NET_IPV6)
 /* Use the value of NET_IPV6_ADDR_LEN */
@@ -36,6 +33,13 @@ static void pmtu_cb(struct net_pmtu_entry *entry, void *user_data)
 #else
 #define ADDR_STR_LEN NET_INET_ADDRSTRLEN
 #endif
+
+#if defined(CONFIG_NET_PMTU)
+static void pmtu_cb(struct net_pmtu_entry *entry, void *user_data)
+{
+	struct net_shell_user_data *data = user_data;
+	const struct shell *sh = data->sh;
+	int *count = data->user_data;
 
 	if (!entry->in_use) {
 		return;
@@ -55,6 +59,31 @@ static void pmtu_cb(struct net_pmtu_entry *entry, void *user_data)
 }
 #endif /* CONFIG_NET_PMTU */
 
+#if defined(CONFIG_NET_PMTU_DPLPMTUD)
+static void dplpmtud_cb(struct net_dplpmtud_entry *entry, void *user_data)
+{
+	struct net_shell_user_data *data = user_data;
+	const struct shell *sh = data->sh;
+	int *count = data->user_data;
+
+	if (!entry->in_use) {
+		return;
+	}
+
+	if (*count == 0) {
+		PR("     %" STRINGIFY(ADDR_STR_LEN) "s   PLPMTU  Age (sec)\n",
+		   "Destination Address");
+	}
+
+	PR("[%2d] %" STRINGIFY(ADDR_STR_LEN) "s %8d  %d\n", *count + 1,
+	   net_sprint_addr(entry->dst.family, (void *)&entry->dst.in_addr),
+	   entry->validated_plpmtu,
+	   (k_uptime_get_32() - entry->last_update) / 1000U);
+
+	(*count)++;
+}
+#endif /* CONFIG_NET_PMTU_DPLPMTUD */
+
 static int cmd_net_pmtu(const struct shell *sh, size_t argc, char *argv[])
 {
 #if defined(CONFIG_NET_PMTU)
@@ -71,11 +100,20 @@ static int cmd_net_pmtu(const struct shell *sh, size_t argc, char *argv[])
 		user_data.sh = sh;
 		user_data.user_data = &count;
 
+		PR("%s destination cache content:\n", "PMTU");
 		(void)net_pmtu_foreach(pmtu_cb, &user_data);
-
 		if (count == 0) {
-			PR("PMTU destination cache is empty.\n");
+			PR("%s destination cache is empty.\n", "PMTU");
 		}
+
+#if defined(CONFIG_NET_PMTU_DPLPMTUD)
+		PR("%s destination cache content:\n", "DPLPMTUD");
+		count = 0;
+		(void)net_dplpmtud_foreach(dplpmtud_cb, &user_data);
+		if (count == 0) {
+			PR("%s destination cache is empty.\n", "DPLPMTUD");
+		}
+#endif
 	}
 #else
 	print_pmtu_error(sh);
@@ -90,8 +128,13 @@ static int cmd_net_pmtu_flush(const struct shell *sh, size_t argc, char *argv[])
 	ARG_UNUSED(argv);
 
 #if defined(CONFIG_NET_PMTU)
-	PR("Flushing PMTU destination cache.\n");
+	PR("Flushing %s destination cache.\n", "PMTU");
 	net_pmtu_init();
+
+#if defined(CONFIG_NET_PMTU_DPLPMTUD)
+	PR("Flushing %s destination cache.\n", "DPLPMTUD");
+	net_dplpmtud_init();
+#endif
 #else
 	print_pmtu_error(sh);
 #endif
@@ -99,13 +142,21 @@ static int cmd_net_pmtu_flush(const struct shell *sh, size_t argc, char *argv[])
 	return 0;
 }
 
+#if defined(CONFIG_NET_PMTU) && defined(CONFIG_NET_PMTU_DPLPMTUD)
+#define PMTU_STR "PMTU/DPLPMTUD"
+#elif defined(CONFIG_NET_PMTU)
+#define PMTU_STR "PMTU"
+#else
+#define PMTU_STR "PMTU"
+#endif /* CONFIG_NET_PMTU && CONFIG_NET_PMTU_DPLPMTUD */
+
 SHELL_STATIC_SUBCMD_SET_CREATE(net_cmd_pmtu,
 	SHELL_CMD(flush, NULL,
-		  SHELL_HELP("Remove all entries from PMTU destination cache", ""),
+		  SHELL_HELP("Remove all entries from " PMTU_STR " destination cache", ""),
 		  cmd_net_pmtu_flush),
 	SHELL_SUBCMD_SET_END
 );
 
 SHELL_SUBCMD_ADD((net), pmtu, &net_cmd_pmtu,
-		 "Show PMTU information.",
+		 "Show " PMTU_STR " information.",
 		 cmd_net_pmtu, 1, 0);
