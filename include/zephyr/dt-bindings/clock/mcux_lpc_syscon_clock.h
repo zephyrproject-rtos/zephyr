97fc2da2531cbfe2f9d30195ab9ed64faa267fa7
Qiang Zhang <qiang.zhang_6@nxp.com>
2026-07-15 17:18:38 +0800

    drivers: clock_control: mcux: add EVTG clock support
    Add MCUX_EVTG_CLK and gate the EVTG bus clock in the mcux syscon clock
    controller, guarded by CONFIG_NXP_EVTG.
    
    Signed-off-by: Qiang Zhang <qiang.zhang_6@nxp.com>

--- include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h
+++ include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h
@@ -243,6 +243,9 @@
 /** PowerQuad DSP coprocessor clock identifier. */
 #define MCUX_POWERQUAD_CLK MCUX_LPC_CLK_ID(0x39, 0x00)
 
+/** EVTG (Event Generator) peripheral clock identifier. */
+#define MCUX_EVTG_CLK MCUX_LPC_CLK_ID(0x3A, 0x00)
+
 /** FRO high-frequency clock (FRO_HF) rate identifier. */
 #define MCUX_FRO_HF_CLK MCUX_LPC_CLK_ID(0x40, 0x00)
 
