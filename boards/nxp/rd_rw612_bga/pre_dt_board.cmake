bb0e2e0cbdbd0c73a4efc7a5cd41b482c9211ea9
Gaofeng Zhang <gaofeng.zhang@nxp.com>
2026-07-17 11:13:40 +0800

    drivers: sd_dev: Add RW6xx SDIO device driver to support RW6xx
    - drivers/sd_dev/rw6xx: Zephyr driver wrapping fsl_adapter_sdu with
      PM support and DT instantiation
    - dts/bindings/sdev/nxp,rw6xx-sdio-device.yaml: DT binding
    - samples/subsys/sd_dev/shell: throughput test sample for rd_rw612_bga
    
    Signed-off-by: Gaofeng Zhang <gaofeng.zhang@nxp.com>

--- boards/nxp/rd_rw612_bga/pre_dt_board.cmake
+++ boards/nxp/rd_rw612_bga/pre_dt_board.cmake
@@ -2,4 +2,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Suppress "spi_bus_bridge" as flexcomm node can be used as a SPI device.
+list(APPEND EXTRA_DTC_FLAGS "-Wno-unique_unit_address_if_enabled")
 list(APPEND EXTRA_DTC_FLAGS "-Wno-spi_bus_bridge")
