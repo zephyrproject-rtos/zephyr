87387e75e06b6dd1130d3e3588757c38e67298a3
Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>
2026-07-20 15:24:25 +0300

    secure_storage: improve zephyr/psa/*_ids.h headers
    - Add top-level comments to files that did not have one, and reference
    zephyr/psa/key_ids.h for a lengthier description.
    
    - Align all *_RANGE_SIZE macros to use /**< to start the comment as
    this is what makes the Doxygen check happy.
    
    Signed-off-by: Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>

--- include/zephyr/psa/its_ids.h
+++ include/zephyr/psa/its_ids.h
@@ -6,8 +6,16 @@
 #ifndef ZEPHYR_INCLUDE_PSA_ITS_IDS_H_
 #define ZEPHYR_INCLUDE_PSA_ITS_IDS_H_
 
+/**
+ * @file zephyr/psa/its_ids.h
+ *
+ * @brief This file defines the UID ranges of the existing users of the PSA ITS API.
+ *
+ * @see @ref zephyr/psa/key_ids.h
+ */
+
 /** UID range to be used by the TF-M PSA ITS Settings backend. */
 #define ZEPHYR_PSA_SETTINGS_TFM_ITS_UID_RANGE_BEGIN 0x28000000
-#define ZEPHYR_PSA_SETTINGS_TFM_ITS_UID_RANGE_SIZE  0x10000 /* 64 Ki */
+#define ZEPHYR_PSA_SETTINGS_TFM_ITS_UID_RANGE_SIZE  0x10000 /**< 64 Ki */
 
 #endif /* ZEPHYR_INCLUDE_PSA_ITS_IDS_H_ */
