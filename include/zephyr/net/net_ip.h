ec6cef056f70e97d99e8a2a83a974de3f18bf2ef
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2026-07-22 16:21:45 +0200

    net: ip: Ensure trivial conversion helpers are inlined
    `inline` is just a hint for the compiler. But without optimizations
    enabled it is ignored.
    Nonetheless, there is no scenario where we would not want these functions
    actually inlined, so let's use ALWAYS_INLINE instead so we ensure they are
    inlined.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- include/zephyr/net/net_ip.h
+++ include/zephyr/net/net_ip.h
@@ -1931,7 +1931,7 @@ static inline bool net_ipv6_addr_based_on_ll(const struct net_in6_addr *addr,
  *
  * @return Pointer to socket address (struct sockaddr)
  */
-static inline struct net_sockaddr *net_sad(const struct net_sockaddr_storage *addr)
+static ALWAYS_INLINE struct net_sockaddr *net_sad(const struct net_sockaddr_storage *addr)
 {
 	return (struct net_sockaddr *)addr;
 }
@@ -1944,7 +1944,7 @@ static inline struct net_sockaddr *net_sad(const struct net_sockaddr_storage *ad
  *
  * @return Pointer to socket storage address (struct net_sockaddr_storage)
  */
-static inline struct net_sockaddr_storage *net_sas(const struct net_sockaddr *addr)
+static ALWAYS_INLINE struct net_sockaddr_storage *net_sas(const struct net_sockaddr *addr)
 {
 	return (struct net_sockaddr_storage *)addr;
 }
@@ -1957,7 +1957,7 @@ static inline struct net_sockaddr_storage *net_sas(const struct net_sockaddr *ad
  *
  * @return Pointer to IPv6 socket address
  */
-static inline struct net_sockaddr_in6 *net_sin6(const struct net_sockaddr *addr)
+static ALWAYS_INLINE struct net_sockaddr_in6 *net_sin6(const struct net_sockaddr *addr)
 {
 	return (struct net_sockaddr_in6 *)addr;
 }
@@ -1970,7 +1970,7 @@ static inline struct net_sockaddr_in6 *net_sin6(const struct net_sockaddr *addr)
  *
  * @return Pointer to IPv4 socket address
  */
-static inline struct net_sockaddr_in *net_sin(const struct net_sockaddr *addr)
+static ALWAYS_INLINE struct net_sockaddr_in *net_sin(const struct net_sockaddr *addr)
 {
 	return (struct net_sockaddr_in *)addr;
 }
@@ -1983,7 +1983,7 @@ static inline struct net_sockaddr_in *net_sin(const struct net_sockaddr *addr)
  *
  * @return Pointer to IPv6 socket address
  */
-static inline
+static ALWAYS_INLINE
 struct net_sockaddr_in6_ptr *net_sin6_ptr(const struct net_sockaddr_ptr *addr)
 {
 	return (struct net_sockaddr_in6_ptr *)addr;
@@ -1997,7 +1997,7 @@ struct net_sockaddr_in6_ptr *net_sin6_ptr(const struct net_sockaddr_ptr *addr)
  *
  * @return Pointer to IPv4 socket address
  */
-static inline
+static ALWAYS_INLINE
 struct net_sockaddr_in_ptr *net_sin_ptr(const struct net_sockaddr_ptr *addr)
 {
 	return (struct net_sockaddr_in_ptr *)addr;
@@ -2011,7 +2011,7 @@ struct net_sockaddr_in_ptr *net_sin_ptr(const struct net_sockaddr_ptr *addr)
  *
  * @return Pointer to linklayer socket address
  */
-static inline
+static ALWAYS_INLINE
 struct net_sockaddr_ll_ptr *net_sll_ptr(const struct net_sockaddr_ptr *addr)
 {
 	return (struct net_sockaddr_ll_ptr *)addr;
@@ -2025,7 +2025,7 @@ struct net_sockaddr_ll_ptr *net_sll_ptr(const struct net_sockaddr_ptr *addr)
  *
  * @return Pointer to CAN socket address
  */
-static inline
+static ALWAYS_INLINE
 struct net_sockaddr_can_ptr *net_can_ptr(const struct net_sockaddr_ptr *addr)
 {
 	return (struct net_sockaddr_can_ptr *)addr;
