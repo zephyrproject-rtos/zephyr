c2dd8b250a28dfa2cf403529c7fff70655f10823
Li Long <li.long@nxp.com>
2026-07-22 16:08:17 +0900

    drivers: wifi: nxp: add imx91 ext1-pwren and wakeup GPIO support
    Add ext1-pwren-gpios binding to nxp,wifi.yaml for M.2 module VPCIe_3V3
    power control. On imx91 (AArch64), GPC is not available so read
    WL_WAKE_HOST GPIO directly via wakeup_gpios DT property instead.
    Use CONFIG_PM_MCUX_GPC to guard fsl_gpc.h include, which is more
    precise than excluding by SoC series.
    
    Signed-off-by: Li Long <li.long@nxp.com>

--- drivers/wifi/nxp/src/nxp_wifi_drv.c
+++ drivers/wifi/nxp/src/nxp_wifi_drv.c
@@ -20,7 +20,7 @@
 #include <zephyr/net/wifi_mgmt.h>
 #ifdef CONFIG_PM_DEVICE
 #include <zephyr/pm/device.h>
-#ifndef CONFIG_NXP_RW610
+#ifdef CONFIG_PM_MCUX_GPC
 #include <fsl_gpc.h>
 #endif
 #endif
@@ -417,6 +417,41 @@ static int nxp_wifi_cpu_reset(uint8_t enable)
 
 	struct gpio_dt_spec sdio_reset = GPIO_DT_SPEC_GET(DT_DRV_INST(0), sd_gpios);
 	struct gpio_dt_spec pwr_gpios = GPIO_DT_SPEC_GET(DT_DRV_INST(0), pwr_gpios);
+
+#if DT_NODE_HAS_PROP(DT_DRV_INST(0), ext1_pwren_gpios)
+	struct gpio_dt_spec ext1_pwren = GPIO_DT_SPEC_GET(DT_DRV_INST(0), ext1_pwren_gpios);
+
+	if (!gpio_is_ready_dt(&ext1_pwren)) {
+		LOG_ERR("Error: failed to configure ext1_pwren %s pin %d",
+			ext1_pwren.port->name, ext1_pwren.pin);
+		return -EIO;
+	}
+
+	err = gpio_pin_configure_dt(&ext1_pwren, GPIO_OUTPUT);
+	if (err) {
+		LOG_ERR("Error %d: failed to configure ext1_pwren %s pin %d", err,
+			ext1_pwren.port->name, ext1_pwren.pin);
+		return err;
+	}
+
+	if (enable) {
+		/* Enable VPCIe_3V3 power to M.2 module before PD_N */
+		err = gpio_pin_set_dt(&ext1_pwren, 1);
+		if (err) {
+			LOG_ERR("Error %d: failed to set ext1_pwren %s pin %d", err,
+				ext1_pwren.port->name, ext1_pwren.pin);
+			return err;
+		}
+		k_sleep(K_MSEC(100));
+	} else {
+		err = gpio_pin_set_dt(&ext1_pwren, 0);
+		if (err) {
+			LOG_ERR("Error %d: failed to clear ext1_pwren %s pin %d", err,
+				ext1_pwren.port->name, ext1_pwren.pin);
+			return err;
+		}
+	}
+#endif
 
 	if (!gpio_is_ready_dt(&sdio_reset)) {
 		LOG_ERR("Error: failed to configure sdio_reset %s pin %d", sdio_reset.port->name,
@@ -2373,6 +2408,15 @@ static bool nxp_wifi_wlan_wakeup(void)
 	return GPC_GetIRQStatusFlag(GPC, GPIO1_Combined_0_15_IRQn);
 #elif CONFIG_NXP_IW416
 	return GPC_GetIRQStatusFlag(GPC, GPIO1_Combined_16_31_IRQn);
+#elif defined(CONFIG_SOC_SERIES_IMX9)
+	/* imx91 has no GPC, read WL_WAKE_HOST GPIO directly instead */
+#if DT_NODE_HAS_PROP(DT_DRV_INST(0), wakeup_gpios)
+	struct gpio_dt_spec wakeup = GPIO_DT_SPEC_GET(DT_DRV_INST(0), wakeup_gpios);
+
+	return gpio_pin_get_dt(&wakeup) == 1;
+#else
+	return false;
+#endif
 #else
 	return false;
 #endif
