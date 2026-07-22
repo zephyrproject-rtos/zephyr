e70c36c849cd8eaad39f3654ba8ffbd07f9c61cc
Muhammed Asif <asifp3104@gmail.com>
2026-07-22 12:45:51 +0530

    boards: microchip: pic32cm_jh01_cpro: Adds support for mplab_ipe runner
    - Adds support for running with mplab_ipe runner
    
    Signed-off-by: Muhammed Asif <asifp3104@gmail.com>

--- boards/microchip/pic32c/pic32cm_jh01_cpro/board.cmake
+++ boards/microchip/pic32c/pic32cm_jh01_cpro/board.cmake
@@ -1,8 +1,10 @@
-# Copyright (c) 2025 Microchip Technology Inc.
+# Copyright (c) 2025-2026 Microchip Technology Inc.
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(pyocd "--target=pic32cm5164jh01100" "--frequency=4000")
 board_runner_args(jlink "--device=PIC32CM5164JH" "--speed=4000")
+board_runner_args(mplab_ipe "--tool" "EDBG" "--part" "32CM5164JH01100" "--erase" "--verify")
 
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/mplab_ipe.board.cmake)
