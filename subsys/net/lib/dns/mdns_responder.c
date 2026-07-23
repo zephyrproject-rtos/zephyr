dc1477ebf24b31111831666dc19c5ae34d2ffe11
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2026-07-23 17:20:59 +0300

    net: mdns_responder: Check iface null before use
    Make sure iface pointer is not null before checking network
    interface flags.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- subsys/net/lib/dns/mdns_responder.c
+++ subsys/net/lib/dns/mdns_responder.c
@@ -1549,7 +1549,7 @@ static int pre_init_listener(void)
 
 	ARRAY_FOR_EACH(v6_ctx, i) {
 		iface = net_if_get_by_index(i + 1);
-		if ((!net_if_flag_is_set(iface, NET_IF_IPV6)) || iface == NULL) {
+		if (iface == NULL || !net_if_flag_is_set(iface, NET_IF_IPV6)) {
 			continue;
 		}
 
@@ -1581,7 +1581,7 @@ static int pre_init_listener(void)
 
 	ARRAY_FOR_EACH(v4_ctx, i) {
 		iface = net_if_get_by_index(i + 1);
-		if ((!net_if_flag_is_set(iface, NET_IF_IPV4)) || iface == NULL) {
+		if (iface == NULL || !net_if_flag_is_set(iface, NET_IF_IPV4)) {
 			continue;
 		}
 
@@ -1669,7 +1669,7 @@ static int init_listener(void)
 		}
 
 		iface = net_if_get_by_index(i + 1);
-		if ((!net_if_flag_is_set(iface, NET_IF_IPV6)) || iface == NULL) {
+		if (iface == NULL || !net_if_flag_is_set(iface, NET_IF_IPV6)) {
 			zsock_close(v6);
 			continue;
 		}
@@ -1779,7 +1779,7 @@ static int init_listener(void)
 		}
 
 		iface = net_if_get_by_index(i + 1);
-		if ((!net_if_flag_is_set(iface, NET_IF_IPV4)) || iface == NULL) {
+		if (iface == NULL || !net_if_flag_is_set(iface, NET_IF_IPV4)) {
 			zsock_close(v4);
 			continue;
 		}
