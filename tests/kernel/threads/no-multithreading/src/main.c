7734232d7d14f5686b58d2b6378beb90f9277b98
Josh DeWitt <josh.dewitt@garmin.com>
2026-07-24 11:32:55 -0500

    arch: openrisc: Support TLS when multithreading is disabled
    Update the thread pointer register to space carved out of the main stack
    so it is not corrupted by ISRs.
    Increase the main stack size on openrisc when ZTEST is enabled to avoid
    a stack overflow when running kernel.no-mt.cpu_exception.
    
    Signed-off-by: Josh DeWitt <josh.dewitt@garmin.com>

--- tests/kernel/threads/no-multithreading/src/main.c
+++ tests/kernel/threads/no-multithreading/src/main.c
@@ -200,7 +200,7 @@ ZTEST(no_multithreading, test_no_multithreading_cpu_idle)
 /* TODO: Remove CONFIG checks once TLS is supported on all architectures
  * See https://github.com/zephyrproject-rtos/zephyr/issues/114503
  */
-#if defined(CONFIG_ARM) || defined(CONFIG_RISCV)
+#if defined(CONFIG_ARM) || defined(CONFIG_RISCV) || defined(CONFIG_OPENRISC)
 /**
  * @brief Verify that thread-local storage is initialized without
  *        multithreading.
