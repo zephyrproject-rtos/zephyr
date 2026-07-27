9df3db1f644ced954b119b3bb81466f1e6db9afa
Pete Johanson <pete.johanson@analog.com>
2026-07-27 10:45:13 -0600

    drivers: adc: Consistently return -ENOMEM for buffer size validation
    Fix up ADC drivers to consistently return -ENOMEM when validation of a
    sequence buffer size fails.
    
    Signed-off-by: Pete Johanson <pete.johanson@analog.com>
    (cherry picked from commit 3d0f9fb0d84d099a9269cfb2ef1f6b21340e972b)
    Signed-off-by: David Brown <david.brown@linaro.org>

--- drivers/adc/adc_npcx.c
+++ drivers/adc/adc_npcx.c
@@ -265,7 +265,7 @@ static void adc_npcx_isr(const struct device *dev)
 
 /*
  * Validate the buffer size with adc channels mask. If it is lower than what
- * we need return -ENOSPC.
+ * we need return -ENOMEM.
  */
 static int adc_npcx_validate_buffer_size(const struct device *dev,
 					const struct adc_sequence *sequence)
@@ -287,7 +287,7 @@ static int adc_npcx_validate_buffer_size(const struct device *dev,
 	}
 
 	if (sequence->buffer_size < needed) {
-		return -ENOSPC;
+		return -ENOMEM;
 	}
 
 	return 0;
