9b82224e094d133bc5382bb581f02ec795b65c46
Bayrem Gharsellaoui <bayrem.gharsellaoui@gmail.com>
2026-07-27 19:44:29 +0100

    drivers: sensor: linux_temp: split host file access
    To avoid assuming that ZVFS_O_RDONLY matches O_RDONLY, compile the
    Linux file access code directly with the host libc.
    
    This also removes the need for the nsi_host file operation wrappers and
    makes the host-side code easier to read.
    
    Signed-off-by: Bayrem Gharsellaoui <bayrem.gharsellaoui@gmail.com>

--- drivers/sensor/linux_temp/linux_temp.c
+++ drivers/sensor/linux_temp/linux_temp.c
@@ -6,15 +6,11 @@
 
 #define DT_DRV_COMPAT zephyr_native_linux_temp
 
-#include <nsi_errno.h>
-#include <nsi_host_trampolines.h>
-#include <stdlib.h>
-#include <string.h>
+#include <errno.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
-#include <zephyr/sys/fdtable.h>
-#include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(linux_temp, CONFIG_SENSOR_LOG_LEVEL);
+
+#include "linux_temp_bottom.h"
 
 struct linux_temp_data {
 	int32_t temperature_mc;
@@ -24,51 +20,20 @@ struct linux_temp_config {
 	const char *path;
 };
 
-static int linux_temp_read(const char *path, int32_t *temperature_mc)
-{
-	char buf[32];
-	char *end;
-	long value;
-	int fd;
-	int ret;
-
-	fd = nsi_host_open(path, ZVFS_O_RDONLY);
-	if (fd < 0) {
-		LOG_ERR("Failed to open %s: %s", path, strerror(nsi_host_get_errno()));
-		return -EIO;
-	}
-
-	ret = nsi_host_read(fd, buf, sizeof(buf) - 1);
-	nsi_host_close(fd);
-
-	if (ret <= 0) {
-		LOG_ERR("Failed to read %s", path);
-		return -EIO;
-	}
-
-	buf[ret] = '\0';
-
-	value = strtol(buf, &end, 10);
-	if (end == buf) {
-		LOG_ERR("Invalid temperature value: %s", buf);
-		return -EINVAL;
-	}
-
-	*temperature_mc = value;
-
-	return 0;
-}
-
 static int linux_temp_sample_fetch(const struct device *dev, enum sensor_channel chan)
 {
 	const struct linux_temp_config *config = dev->config;
 	struct linux_temp_data *data = dev->data;
 
-	if (chan != SENSOR_CHAN_ALL && chan != SENSOR_CHAN_DIE_TEMP) {
+	if ((chan != SENSOR_CHAN_ALL) && (chan != SENSOR_CHAN_DIE_TEMP)) {
 		return -ENOTSUP;
 	}
 
-	return linux_temp_read(config->path, &data->temperature_mc);
+	if (linux_temp_read(config->path, &data->temperature_mc) < 0) {
+		return -EIO;
+	}
+
+	return 0;
 }
 
 static int linux_temp_channel_get(const struct device *dev, enum sensor_channel chan,
@@ -76,7 +41,7 @@ static int linux_temp_channel_get(const struct device *dev, enum sensor_channel 
 {
 	struct linux_temp_data *data = dev->data;
 
-	if (chan != SENSOR_CHAN_DIE_TEMP) {
+	if ((chan != SENSOR_CHAN_ALL) && (chan != SENSOR_CHAN_DIE_TEMP)) {
 		return -ENOTSUP;
 	}
 
