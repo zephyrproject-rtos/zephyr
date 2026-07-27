0a71969bb0ecf55d32e52b0db107654fddec500a
Daniel Leung <daniel.leung@intel.com>
2026-07-27 10:01:09 -0700

    testsuite: ztest: cpuhold threads need to join
    Since we are reusing the cpuhold thread objects, we must join
    those threads to make sure the spawned threads have actually
    stopped. The situation is further compliacted due to those
    cpuhold threads are running with interrupt disabled inside
    cpu_hold(). This means the IPI sent by k_thread_abort() is not
    being processed to abort the thread. And there is no thread
    swapping where the scheduler checks if a thread is going away.
    Since k_thread_abort() is rather meaningless here and we must
    do k_thread_join(), simply replace abort with join.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>

--- subsys/testsuite/ztest/src/ztest.c
+++ subsys/testsuite/ztest/src/ztest.c
@@ -309,7 +309,10 @@ void z_impl_z_test_1cpu_stop(void)
 
 	for (int i = 0; i <= MAX_NUM_CPUHOLD; i++) {
 		if (cpuhold_pool_items[i].used) {
-			k_thread_abort(&cpuhold_pool_items[i].thread);
+			/* Must join the cpuhold threads before z_test_1cpu_start()
+			 * reuses the thread objects.
+			 */
+			k_thread_join(&cpuhold_pool_items[i].thread, K_FOREVER);
 			cpuhold_pool_items[i].used = false;
 		}
 	}
