6b12bc962b1780bc7179c5b02726c1ac7de5faa3
Amneesh Singh <amneesh@ti.com>
2026-07-28 19:28:40 +0530

    soc: ti: mspm: add SoC specific register headers
    Add native register layout headers for three MSPM hardware
    blocks, replacing dependence on TI driverlib for low-level
    SoC clock/power init:
    
    - soc_sysctl.h: system control region register struct and bit
      definitions for clock configuration.
    
    - soc_factoryregion.h: read-only factory flash region struct with
      calibration data (PLL startup params, temp sensor).
    
    - soc_cpuss.h: CPU subsystem register struct with prefetch/cache
      control for the flash region. Only found in MSPM0 SoCs.
    
    - soc_fri.h: flash read interface region for configuring flash access.
      Only found in MSPM33 SoCs.
    
    - soc_memcfg.h: memory configuration region for configuring RAM/ROM
      waitstates. Only found in MSPM33 SoCs.
    
    Add DT nodes for all as well.
    
    Signed-off-by: Amneesh Singh <amneesh@ti.com>

---
+++ soc/ti/mspm/common/soc_factoryregion.h
@@ -0,0 +1,39 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026 Texas Instruments Incorporated
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_TI_MSPM_COMMON_SOC_FACTORYREGION_H_
+#define ZEPHYR_SOC_TI_MSPM_COMMON_SOC_FACTORYREGION_H_
+
+/**
+ * @brief MSPM Factory Region Register Offsets
+ *
+ * Read-only flash region containing factory-programmed calibration data.
+ * Must disable CPUSS instruction cache before accessing (see soc_cpuss.h).
+ */
+#if defined(CONFIG_SOC_SERIES_MSPM33C)
+#define FACTORY_PLLSTARTUP0_4_8_OFFSET   0x20 /**< PLL param0 4-8 MHz */
+#define FACTORY_PLLSTARTUP1_4_8_OFFSET   0x24 /**< PLL param1 4-8 MHz */
+#define FACTORY_PLLSTARTUP0_8_16_OFFSET  0x28 /**< PLL param0 8-16 MHz */
+#define FACTORY_PLLSTARTUP1_8_16_OFFSET  0x2C /**< PLL param1 8-16 MHz */
+#define FACTORY_PLLSTARTUP0_16_32_OFFSET 0x30 /**< PLL param0 16-32 MHz */
+#define FACTORY_PLLSTARTUP1_16_32_OFFSET 0x34 /**< PLL param1 16-32 MHz */
+#define FACTORY_PLLSTARTUP0_32_48_OFFSET 0x38 /**< PLL param0 32-48 MHz */
+#define FACTORY_PLLSTARTUP1_32_48_OFFSET 0x3C /**< PLL param1 32-48 MHz */
+
+#define FACTORY_SYSPLLPARAM2_OFFSET  0x54 /**< PLL param2 */
+#define FACTORY_SYSPLLLDOCTL_OFFSET  0x58 /**< PLL LDO ctl */
+#define FACTORY_SYSPLLLDOPROG_OFFSET 0x5C /**< PLL LDO VOUT PROG */
+#else
+#define FACTORY_PLLSTARTUP0_4_8_OFFSET   0x1C /**< PLL param0 4-8 MHz */
+#define FACTORY_PLLSTARTUP1_4_8_OFFSET   0x20 /**< PLL param1 4-8 MHz */
+#define FACTORY_PLLSTARTUP0_8_16_OFFSET  0x24 /**< PLL param0 8-16 MHz */
+#define FACTORY_PLLSTARTUP1_8_16_OFFSET  0x28 /**< PLL param1 8-16 MHz */
+#define FACTORY_PLLSTARTUP0_16_32_OFFSET 0x2C /**< PLL param0 16-32 MHz */
+#define FACTORY_PLLSTARTUP1_16_32_OFFSET 0x30 /**< PLL param1 16-32 MHz */
+#define FACTORY_PLLSTARTUP0_32_48_OFFSET 0x34 /**< PLL param0 32-48 MHz */
+#define FACTORY_PLLSTARTUP1_32_48_OFFSET 0x38 /**< PLL param1 32-48 MHz */
+#endif
+
+#endif /* ZEPHYR_SOC_TI_MSPM_COMMON_SOC_FACTORYREGION_H_ */
