7817f31b4f433c64697118d91172ae69f7d49b22
Arjun Salunkhe <arjun.salunkhe@proton.me>
2026-07-28 22:38:29 +0530

    arch: riscv: Add Zicfilp SMP enablement
    Add mseccfg regsiter configuration for secondary cpus to
    enable landing pad enforcement for them.
    
    Signed-off-by: Arjun Salunkhe <arjun.salunkhe@proton.me>

--- arch/riscv/core/smp.c
+++ arch/riscv/core/smp.c
@@ -93,6 +93,9 @@ void arch_secondary_cpu_init(int hartid)
 #ifdef CONFIG_RISCV_USER_COUNTER_ACCESS
 	z_riscv_counteren_init();
 #endif
+#ifdef CONFIG_RISCV_LANDING_PADS
+	csr_set(mseccfg, MSECCFG_MLPE);
+#endif
 #ifdef CONFIG_CUSTOM_STACK_GUARD
 	z_riscv_custom_stack_guard_init();
 #endif /* CONFIG_CUSTOM_STACK_GUARD */
