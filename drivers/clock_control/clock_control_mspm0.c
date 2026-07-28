8a0549dd025a8260111f13d63c7babc0bc6646d2
Amneesh Singh <amneesh@ti.com>
2026-07-28 19:51:37 +0530

    drivers: clock_control: mspm0: switch SYSPLL config to native SYSCTL
    - Replace SYSPLL HAL configuration with native register I/O via configure()
      API
    - Configure dividers once during initialization
    - Copy factory-programmed startup parameters from the factoryregion based
      on the reference frequency at runtime instead of the currently hardcoded
      32-48MHz range.
    - Add SYSPLL on()/off() API support
    - Add SYSPLL get_status() API support
    - Implement get_rate() for all 3 SYSPLL output clocks, which calculate the
      output frequency based on the configured dividers and the input source.
    - Introduce raw polling loops for checking clock status since using systick
      timer to wait for n-cycles may cause the core to reset when clocks are
      being configured/switched.
    
    Signed-off-by: Amneesh Singh <amneesh@ti.com>

--- drivers/clock_control/clock_control_mspm0.c
+++ drivers/clock_control/clock_control_mspm0.c
@@ -5,13 +5,29 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/kernel.h>
 #include <zephyr/drivers/clock_control.h>
 #include <zephyr/drivers/clock_control/mspm0_clock_control.h>
+#include <zephyr/logging/log.h>
 
+#include <soc_cpuss.h>
+#include <soc_factoryregion.h>
 #include <soc_sysctl.h>
+
+LOG_MODULE_REGISTER(clock_control_mspm0, CONFIG_CLOCK_CONTROL_LOG_LEVEL);
 
 #include <ti/driverlib/driverlib.h>
 #include <string.h>
+
+#if defined(CONFIG_SOC_SERIES_MSPM33C)
+#define MSPM0_REQUIRES_SYSPLLPARAM2
+#endif
+
+#if DT_NODE_EXISTS(MSPM_CPUSS_NODE)
+#define MSPM0_HAS_CPUSS
+#endif
+
+#define MSPM0_CLK_WAIT_TIMEOUT_US 1000
 
 #define MSPM0_ULPCLK_DIV COND_CODE_1(					\
 		DT_NODE_HAS_PROP(DT_NODELABEL(ulpclk), clk_div),	\
@@ -46,9 +62,10 @@
 #define MSPM0_MFPCLK_ENABLED 1
 #endif
 
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(syspll), okay)
-#define MSPM0_SYSPLL_ENABLED 1
-#endif
+#define DT_HFCLK  DT_NODELABEL(hfclk)
+#define DT_SYSPLL DT_NODELABEL(syspll)
+
+#define DT_SYSPLL_OKAY DT_NODE_HAS_STATUS_OKAY(DT_SYSPLL)
 
 #if DT_NODE_HAS_STATUS(DT_NODELABEL(hfxt), okay)
 #define MSPM0_HFCLK_ENABLED 1
@@ -62,7 +79,40 @@
 #define DT_LFCLK_CLOCKS_CTRL	DT_CLOCKS_CTLR(DT_NODELABEL(lfclk))
 #define DT_HFCLK_CLOCKS_CTRL	DT_CLOCKS_CTLR(DT_NODELABEL(hfclk))
 #define DT_MFPCLK_CLOCKS_CTRL	DT_CLOCKS_CTLR(DT_NODELABEL(mfpclk))
-#define DT_SYSPLL_CLOCKS_CTRL	DT_CLOCKS_CTLR(DT_NODELABEL(syspll))
+#define DT_SYSPLL_CLOCKS_CTRL	DT_CLOCKS_CTLR(DT_SYSPLL)
+
+/* System PLL */
+#if DT_SYSPLL_OKAY
+
+#define MSPM0_SYSPLL_PDIV      DT_PROP(DT_SYSPLL, p_div)
+#define MSPM0_SYSPLL_QDIV      DT_PROP(DT_SYSPLL, q_div)
+#define MSPM0_SYSPLL_HAS_CLK2X DT_NODE_HAS_PROP(DT_SYSPLL, clk2x_div)
+#define MSPM0_SYSPLL_CLK2X_DIV DT_PROP_OR(DT_SYSPLL, clk2x_div, 1)
+#define MSPM0_SYSPLL_HAS_CLK1  DT_NODE_HAS_PROP(DT_SYSPLL, clk1_div)
+#define MSPM0_SYSPLL_CLK1_DIV  DT_PROP_OR(DT_SYSPLL, clk1_div, 1)
+#define MSPM0_SYSPLL_HAS_CLK0  DT_NODE_HAS_PROP(DT_SYSPLL, clk0_div)
+#define MSPM0_SYSPLL_CLK0_DIV  DT_PROP_OR(DT_SYSPLL, clk0_div, 1)
+
+#if MSPM0_SYSPLL_HAS_CLK2X && MSPM0_SYSPLL_HAS_CLK0
+#error "Only CLK2X or CLK0 can be enabled at a time on the SYSPLL"
+#endif
+
+#if MSPM0_SYSPLL_QDIV <= 1
+#error "Divide-by-one is not a valid QDIV option"
+#endif
+
+#if !DT_SAME_NODE(DT_CLOCKS_CTLR(DT_SYSPLL), DT_HFCLK) &&                                          \
+	!DT_SAME_NODE(DT_CLOCKS_CTLR(DT_SYSPLL), DT_SYSOSC)
+#error "Invalid SYSPLL source"
+#endif
+
+BUILD_ASSERT(DT_NODE_HAS_STATUS_OKAY(DT_CLOCKS_CTLR(DT_SYSPLL)), "SYSPLL source not enabled");
+
+#endif /* DT_SYSPLL_OKAY */
+
+#if DT_SYSPLL_OKAY
+static int clock_mspm0_syspll_output_rate(const struct device *dev, uint32_t clk, uint32_t *rate);
+#endif /* DT_SYSPLL_OKAY */
 
 struct mspm0_clk_cfg {
 	uint32_t clk_div;
@@ -97,37 +147,41 @@ static struct mspm0_clk_cfg mspm0_hfclk_cfg = {
 };
 #endif
 
-#if MSPM0_SYSPLL_ENABLED
-/* basic checks of the devicetree to follow */
-#if (DT_NODE_HAS_PROP(DT_NODELABEL(syspll), clk2x_div) && \
-	DT_NODE_HAS_PROP(DT_NODELABEL(syspll), clk0_div))
-#error "Only CLK2X or CLK0 can be enabled at a time on the SYSPLL"
-#endif
 
-static DL_SYSCTL_SYSPLLConfig clock_mspm0_cfg_syspll = {
-	.inputFreq = DL_SYSCTL_SYSPLL_INPUT_FREQ_32_48_MHZ,
-	.sysPLLMCLK = DL_SYSCTL_SYSPLL_MCLK_CLK2X,
-	.sysPLLRef = DL_SYSCTL_SYSPLL_REF_SYSOSC,
-	.rDivClk2x = (DT_PROP_OR(DT_NODELABEL(syspll), clk2x_div, 1) - 1),
-	.rDivClk1 = (DT_PROP_OR(DT_NODELABEL(syspll), clk1_div, 1) - 1),
-	.rDivClk0 = (DT_PROP_OR(DT_NODELABEL(syspll), clk0_div, 1) - 1),
-	.qDiv = (DT_PROP(DT_NODELABEL(syspll), q_div) - 1),
-	.pDiv = CONCAT(DL_SYSCTL_SYSPLL_PDIV_,
-		       DT_PROP(DT_NODELABEL(syspll), p_div)),
-	.enableCLK2x = COND_CODE_1(
-		DT_NODE_HAS_PROP(DT_NODELABEL(syspll), clk2x_div),
-		(DL_SYSCTL_SYSPLL_CLK2X_ENABLE),
-		(DL_SYSCTL_SYSPLL_CLK2X_DISABLE)),
-	.enableCLK1 = COND_CODE_1(
-		DT_NODE_HAS_PROP(DT_NODELABEL(syspll), clk1_div),
-		(DL_SYSCTL_SYSPLL_CLK1_ENABLE),
-		(DL_SYSCTL_SYSPLL_CLK1_DISABLE)),
-	.enableCLK0 = COND_CODE_1(
-		DT_NODE_HAS_PROP(DT_NODELABEL(syspll), clk0_div),
-		(DL_SYSCTL_SYSPLL_CLK0_ENABLE),
-		(DL_SYSCTL_SYSPLL_CLK0_DISABLE)),
-};
-#endif
+/* Only 32/4 MHz supported; 16/24 MHz needs board trim we can't source. */
+static int clock_mspm0_set_rate(const struct device *dev, clock_control_subsys_t sys,
+				clock_control_subsys_rate_t rate)
+{
+	struct mspm0_sys_clock *sys_clock = (struct mspm0_sys_clock *)sys;
+	volatile struct mspm_sysctl_regs *regs = MSPM_SYSCTL_REGS;
+	volatile struct mspm_sysctl_soclock_regs *soclock = &regs->soclock;
+	uint32_t freq = *(uint32_t *)rate;
+
+	if (sys_clock->clk != MSPM0_CLOCK_SYSOSC) {
+		return -ENOTSUP;
+	}
+
+	if (soclock->clkstatus & (SYSCTL_CLKSTATUS_CURMCLKSEL | SYSCTL_CLKSTATUS_HSCLKMUX)) {
+		return -EBUSY;
+	}
+
+	switch (freq) {
+	case MHZ(32):
+		soclock->sysosccfg = (soclock->sysosccfg & ~SYSCTL_SYSOSCCFG_FREQ) |
+				     FIELD_PREP(SYSCTL_SYSOSCCFG_FREQ, SYSCTL_SYSOSCCFG_FREQ_BASE);
+		break;
+
+	case MHZ(4):
+		soclock->sysosccfg = (soclock->sysosccfg & ~SYSCTL_SYSOSCCFG_FREQ) |
+				     FIELD_PREP(SYSCTL_SYSOSCCFG_FREQ, SYSCTL_SYSOSCCFG_FREQ_4M);
+		break;
+
+	default:
+		return -ENOTSUP;
+	}
+
+	return 0;
+}
 
 static int clock_mspm0_on(const struct device *dev, clock_control_subsys_t sys)
 {
@@ -139,6 +193,26 @@ static int clock_mspm0_on(const struct device *dev, clock_control_subsys_t sys)
 	case MSPM0_CLOCK_SYSOSC:
 		soclock->sysosccfg &= ~SYSCTL_SYSOSCCFG_DISABLE;
 		return 0;
+
+#if DT_SYSPLL_OKAY
+	case MSPM0_CLOCK_SYSPLL: {
+		struct mspm0_sys_clock sysosc_subsys = {.clk = MSPM0_CLOCK_SYSOSC};
+		uint32_t rate = MHZ(32);
+		int ret = clock_mspm0_set_rate(dev, &sysosc_subsys, &rate);
+
+		if (ret < 0) {
+			return ret;
+		}
+
+		soclock->hsclken |= SYSCTL_HSCLKEN_SYSPLLEN;
+		if (!WAIT_FOR((soclock->clkstatus & SYSCTL_CLKSTATUS_SYSPLLGOOD) != 0,
+			      MSPM0_CLK_WAIT_TIMEOUT_US, NULL)) {
+			LOG_ERR("timed out waiting for SYSPLL to start");
+			return -ETIMEDOUT;
+		}
+		return 0;
+	}
+#endif /* DT_SYSPLL_OKAY */
 
 	default:
 		return -ENOTSUP;
@@ -155,6 +229,18 @@ static int clock_mspm0_off(const struct device *dev, clock_control_subsys_t sys)
 	case MSPM0_CLOCK_SYSOSC:
 		soclock->sysosccfg |= SYSCTL_SYSOSCCFG_DISABLE;
 		return 0;
+
+#if DT_SYSPLL_OKAY
+	case MSPM0_CLOCK_SYSPLL: {
+		soclock->hsclken &= ~SYSCTL_HSCLKEN_SYSPLLEN;
+		if (!WAIT_FOR((soclock->clkstatus & SYSCTL_CLKSTATUS_SYSPLLOFF) != 0,
+			      MSPM0_CLK_WAIT_TIMEOUT_US, NULL)) {
+			LOG_ERR("timed out waiting for SYSPLL to stop");
+			return -ETIMEDOUT;
+		}
+		return 0;
+	}
+#endif /* DT_SYSPLL_OKAY */
 
 	default:
 		return -ENOTSUP;
@@ -175,44 +261,20 @@ static enum clock_control_status clock_mspm0_get_status(const struct device *dev
 		}
 		return CLOCK_CONTROL_STATUS_ON;
 
+#if DT_SYSPLL_OKAY
+	case MSPM0_CLOCK_SYSPLL:
+		if (soclock->clkstatus & SYSCTL_CLKSTATUS_SYSPLLOFF) {
+			return CLOCK_CONTROL_STATUS_OFF;
+		}
+		if (soclock->clkstatus & SYSCTL_CLKSTATUS_SYSPLLGOOD) {
+			return CLOCK_CONTROL_STATUS_ON;
+		}
+		return CLOCK_CONTROL_STATUS_STARTING;
+#endif /* DT_SYSPLL_OKAY */
+
 	default:
 		return CLOCK_CONTROL_STATUS_UNKNOWN;
 	}
-}
-
-/* Only 32/4 MHz supported; 16/24 MHz needs board trim we can't source. */
-static int clock_mspm0_set_rate(const struct device *dev, clock_control_subsys_t sys,
-				clock_control_subsys_rate_t rate)
-{
-	struct mspm0_sys_clock *sys_clock = (struct mspm0_sys_clock *)sys;
-	volatile struct mspm_sysctl_regs *regs = MSPM_SYSCTL_REGS;
-	volatile struct mspm_sysctl_soclock_regs *soclock = &regs->soclock;
-	uint32_t freq = *(uint32_t *)rate;
-
-	if (sys_clock->clk != MSPM0_CLOCK_SYSOSC) {
-		return -ENOTSUP;
-	}
-
-	if (soclock->mclkcfg & (SYSCTL_MCLKCFG_USELFCLK | SYSCTL_MCLKCFG_USEHSCLK)) {
-		return -EBUSY;
-	}
-
-	switch (freq) {
-	case MHZ(32):
-		soclock->sysosccfg = (soclock->sysosccfg & ~SYSCTL_SYSOSCCFG_FREQ) |
-				     FIELD_PREP(SYSCTL_SYSOSCCFG_FREQ, SYSCTL_SYSOSCCFG_FREQ_BASE);
-		break;
-
-	case MHZ(4):
-		soclock->sysosccfg = (soclock->sysosccfg & ~SYSCTL_SYSOSCCFG_FREQ) |
-				     FIELD_PREP(SYSCTL_SYSOSCCFG_FREQ, SYSCTL_SYSOSCCFG_FREQ_4M);
-		break;
-
-	default:
-		return -ENOTSUP;
-	}
-
-	return 0;
 }
 
 static uint32_t clock_mspm0_sysosc_rate(void)
@@ -280,6 +342,19 @@ static int clock_mspm0_get_rate(const struct device *dev,
 		break;
 #endif
 
+#if DT_SYSPLL_OKAY
+	case MSPM0_CLOCK_SYSPLL_CLK0:
+	case MSPM0_CLOCK_SYSPLL_CLK1:
+	case MSPM0_CLOCK_SYSPLL_CLK2X: {
+		int ret = clock_mspm0_syspll_output_rate(dev, sys_clock->clk, rate);
+
+		if (ret < 0) {
+			return ret;
+		}
+		break;
+	}
+#endif /* DT_SYSPLL_OKAY */
+
 	case MSPM0_CLOCK_MFCLK:
 	default:
 		return -ENOTSUP;
@@ -288,20 +363,296 @@ static int clock_mspm0_get_rate(const struct device *dev,
 	return 0;
 }
 
-static int clock_mspm0_configure(const struct device *dev, clock_control_subsys_t sys, void *data)
+#if DT_SYSPLL_OKAY
+static int clock_mspm0_syspll_ref_rate(const struct device *dev, uint32_t *rate)
 {
-	struct mspm0_sys_clock *sys_clock = (struct mspm0_sys_clock *)sys;
+	volatile struct mspm_sysctl_regs *regs = MSPM_SYSCTL_REGS;
+	volatile struct mspm_sysctl_soclock_regs *soclock = &regs->soclock;
 
-	switch (sys_clock->clk) {
+	if (soclock->syspllcfg0 & SYSCTL_SYSPLLCFG0_SYSPLLREF) {
+#if MSPM0_HFCLK_ENABLED
+		struct mspm0_sys_clock hfclk_subsys = {.clk = MSPM0_CLOCK_HFCLK};
+
+		return clock_mspm0_get_rate(dev, (clock_control_subsys_t)&hfclk_subsys, rate);
+#else
+		return -ENOTSUP;
+#endif /* MSPM0_HFCLK_ENABLED */
+	}
+
+	*rate = clock_mspm0_sysosc_rate();
+	return 0;
+}
+
+/* fVCO = ref_rate / PDIV * QDIV, from the live syspllcfg1 dividers. */
+static int clock_mspm0_syspll_fvco(const struct device *dev, uint32_t *fvco)
+{
+	volatile struct mspm_sysctl_regs *regs = MSPM_SYSCTL_REGS;
+	volatile struct mspm_sysctl_soclock_regs *soclock = &regs->soclock;
+	uint32_t pdiv = 1U << FIELD_GET(SYSCTL_SYSPLLCFG1_PDIV, soclock->syspllcfg1);
+	uint32_t qdiv = FIELD_GET(SYSCTL_SYSPLLCFG1_QDIV, soclock->syspllcfg1) + 1;
+	uint32_t ref_rate;
+	int ret = clock_mspm0_syspll_ref_rate(dev, &ref_rate);
+
+	if (ret < 0) {
+		return ret;
+	}
+
+	*fvco = (ref_rate / pdiv) * qdiv;
+	return 0;
+}
+
+/* SYSPLL output rate (CLK0/CLK1 = fVCO/RDIVCLKx, CLK2X = 2*fVCO/RDIVCLK2X); -ENODEV if disabled. */
+static int clock_mspm0_syspll_output_rate(const struct device *dev, uint32_t clk, uint32_t *rate)
+{
+	volatile struct mspm_sysctl_regs *regs = MSPM_SYSCTL_REGS;
+	volatile struct mspm_sysctl_soclock_regs *soclock = &regs->soclock;
+	uint32_t fvco;
+	uint32_t rdiv;
+	int ret;
+
+	switch (clk) {
+	case MSPM0_CLOCK_SYSPLL_CLK0:
+		if (!(soclock->syspllcfg0 & SYSCTL_SYSPLLCFG0_ENABLECLK0)) {
+			return -ENODEV;
+		}
+		rdiv = (FIELD_GET(SYSCTL_SYSPLLCFG0_RDIVCLK0, soclock->syspllcfg0) + 1) * 2;
+		break;
+
+	case MSPM0_CLOCK_SYSPLL_CLK1:
+		if (!(soclock->syspllcfg0 & SYSCTL_SYSPLLCFG0_ENABLECLK1)) {
+			return -ENODEV;
+		}
+		rdiv = (FIELD_GET(SYSCTL_SYSPLLCFG0_RDIVCLK1, soclock->syspllcfg0) + 1) * 2;
+		break;
+
+	case MSPM0_CLOCK_SYSPLL_CLK2X:
+		if (!(soclock->syspllcfg0 & SYSCTL_SYSPLLCFG0_ENABLECLK2X)) {
+			return -ENODEV;
+		}
+		rdiv = FIELD_GET(SYSCTL_SYSPLLCFG0_RDIVCLK2X, soclock->syspllcfg0) + 1;
+		break;
+
+	default:
+		return -EINVAL;
+	}
+
+	ret = clock_mspm0_syspll_fvco(dev, &fvco);
+	if (ret < 0) {
+		return ret;
+	}
+
+	*rate = (clk == MSPM0_CLOCK_SYSPLL_CLK2X) ? ((2 * fvco) / rdiv) : (fvco / rdiv);
+	return 0;
+}
+
+/* Bin floopin_hz into its factory-trimmed range and (re)load syspllparam0/1 accordingly. */
+static int clock_mspm0_syspll_load_trim(uint32_t floopin_hz)
+{
+	volatile struct mspm_sysctl_regs *regs = MSPM_SYSCTL_REGS;
+	volatile struct mspm_sysctl_soclock_regs *soclock = &regs->soclock;
+	volatile struct mspm_factoryregion_regs *factoryregion = MSPM_FACTORY_REGS;
+	uint32_t param0;
+	uint32_t param1;
+#if defined(MSPM0_HAS_CPUSS)
+	volatile struct mspm_cpuss_regs *cpuss = MSPM_CPUSS_REGS;
+	uint32_t old_cpuss_ctl = cpuss->ctl;
+
+	cpuss->ctl &= ~CPUSS_CTL_ICACHE;
+#endif /* MSPM0_HAS_CPUSS */
+
+	if (floopin_hz >= MHZ(4) && floopin_hz < MHZ(8)) {
+		param0 = factoryregion->pllstartup0_4_8;
+		param1 = factoryregion->pllstartup1_4_8;
+	} else if (floopin_hz >= MHZ(8) && floopin_hz < MHZ(16)) {
+		param0 = factoryregion->pllstartup0_8_16;
+		param1 = factoryregion->pllstartup1_8_16;
+	} else if (floopin_hz >= MHZ(16) && floopin_hz < MHZ(32)) {
+		param0 = factoryregion->pllstartup0_16_32;
+		param1 = factoryregion->pllstartup1_16_32;
+	} else if (floopin_hz >= MHZ(32) && floopin_hz <= MHZ(48)) {
+		param0 = factoryregion->pllstartup0_32_48;
+		param1 = factoryregion->pllstartup1_32_48;
+	} else {
+		return -EINVAL;
+	}
+
+	soclock->syspllparam0 = param0;
+	soclock->syspllparam1 = param1;
+
+#if defined(MSPM0_REQUIRES_SYSPLLPARAM2)
+	soclock->syspllparam2 = factoryregion->syspllparam2;
+	soclock->syspllldoctl = factoryregion->syspllldoctl;
+	soclock->syspllldoprog = factoryregion->syspllldoprog;
+#endif /* MSPM0_REQUIRES_SYSPLLPARAM2 */
+
+#if defined(MSPM0_HAS_CPUSS)
+	/* restore CPUSS flash cache control */
+	cpuss->ctl = old_cpuss_ctl;
+#endif /* MSPM0_HAS_CPUSS */
+
+	return 0;
+}
+
+/* Switch SYSPLL reference at runtime, reloading the trim bin for the new fLOOPIN. */
+static int clock_mspm0_configure_syspll(const struct device *dev,
+					volatile struct mspm_sysctl_soclock_regs *soclock,
+					enum mspm0_clock_source source)
+{
+	uint32_t pdiv = 1U << FIELD_GET(SYSCTL_SYSPLLCFG1_PDIV, soclock->syspllcfg1);
+	uint32_t ref_rate;
+
+	switch (source) {
+#if MSPM0_HFCLK_ENABLED
+	case MSPM0_CLOCK_SRC_HFCLK: {
+		struct mspm0_sys_clock hfclk_subsys = {.clk = MSPM0_CLOCK_HFCLK};
+		int ret = clock_mspm0_get_rate(dev, (clock_control_subsys_t)&hfclk_subsys,
+					       &ref_rate);
+
+		if (ret < 0) {
+			return ret;
+		}
+
+		soclock->syspllcfg0 |= SYSCTL_SYSPLLCFG0_SYSPLLREF;
+		break;
+	}
+#endif /* MSPM0_HFCLK_ENABLED */
+
+	case MSPM0_CLOCK_SRC_SYSOSC:
+		ref_rate = clock_mspm0_sysosc_rate();
+		soclock->syspllcfg0 &= ~SYSCTL_SYSPLLCFG0_SYSPLLREF;
+		break;
+
 	default:
 		return -ENOTSUP;
 	}
+
+	return clock_mspm0_syspll_load_trim(ref_rate / pdiv);
+}
+
+static int clock_mspm0_init_syspll(const struct device *dev)
+{
+	volatile struct mspm_sysctl_regs *regs = MSPM_SYSCTL_REGS;
+	volatile struct mspm_sysctl_soclock_regs *soclock = &regs->soclock;
+	struct mspm0_sys_clock syspll_subsys = {.clk = MSPM0_CLOCK_SYSPLL};
+	int ret;
+
+	/* disable SYSPLL before programming it */
+	ret = clock_mspm0_off(dev, (clock_control_subsys_t)&syspll_subsys);
+	if (ret < 0) {
+		return ret;
+	}
+
+	/* set predivider */
+	soclock->syspllcfg1 =
+		(soclock->syspllcfg1 & ~SYSCTL_SYSPLLCFG1_PDIV) |
+		FIELD_PREP(SYSCTL_SYSPLLCFG1_PDIV, SYSCTL_SYSPLLCFG1_PDIV_VAL(MSPM0_SYSPLL_PDIV));
+
+	/* set reference and factory trim via the shared runtime path */
+#if DT_SAME_NODE(DT_SYSPLL_CLOCKS_CTRL, DT_HFCLK)
+	LOG_DBG("SYSPLL booting from HFCLK");
+	ret = clock_mspm0_configure_syspll(dev, soclock, MSPM0_CLOCK_SRC_HFCLK);
+#else
+	LOG_DBG("SYSPLL booting from SYSOSC");
+	ret = clock_mspm0_configure_syspll(dev, soclock, MSPM0_CLOCK_SRC_SYSOSC);
+#endif /* DT_SAME_NODE(DT_SYSPLL_CLOCKS_CTRL, DT_HFCLK) */
+	if (ret < 0) {
+		return ret;
+	}
+
+	/* set QDIV */
+	soclock->syspllcfg1 =
+		(soclock->syspllcfg1 & ~SYSCTL_SYSPLLCFG1_QDIV) |
+		FIELD_PREP(SYSCTL_SYSPLLCFG1_QDIV, SYSCTL_SYSPLLCFG1_QDIV_VAL(MSPM0_SYSPLL_QDIV));
+
+	/* set syspllclk2x divider */
+	if (MSPM0_SYSPLL_HAS_CLK2X) {
+		soclock->syspllcfg0 =
+			(soclock->syspllcfg0 & ~SYSCTL_SYSPLLCFG0_RDIVCLK2X) |
+			SYSCTL_SYSPLLCFG0_ENABLECLK2X |
+			FIELD_PREP(SYSCTL_SYSPLLCFG0_RDIVCLK2X,
+				   SYSCTL_SYSPLLCFG0_RDIVCLK2X_VAL(MSPM0_SYSPLL_CLK2X_DIV));
+	} else {
+		soclock->syspllcfg0 &= ~SYSCTL_SYSPLLCFG0_ENABLECLK2X;
+	}
+
+	/* set syspllclk1 divider */
+	if (MSPM0_SYSPLL_HAS_CLK1) {
+		soclock->syspllcfg0 = (soclock->syspllcfg0 & ~SYSCTL_SYSPLLCFG0_RDIVCLK1) |
+				      SYSCTL_SYSPLLCFG0_ENABLECLK1 |
+				      FIELD_PREP(SYSCTL_SYSPLLCFG0_RDIVCLK1,
+						 SYSCTL_SYSPLLCFG0_RDIVCLK1_VAL(
+							 MSPM0_SYSPLL_CLK1_DIV));
+	} else {
+		soclock->syspllcfg0 &= ~SYSCTL_SYSPLLCFG0_ENABLECLK1;
+	}
+
+	/* set syspllclk0 divider */
+	if (MSPM0_SYSPLL_HAS_CLK0) {
+		soclock->syspllcfg0 = (soclock->syspllcfg0 & ~SYSCTL_SYSPLLCFG0_RDIVCLK0) |
+				      SYSCTL_SYSPLLCFG0_ENABLECLK0 |
+				      FIELD_PREP(SYSCTL_SYSPLLCFG0_RDIVCLK0,
+						 SYSCTL_SYSPLLCFG0_RDIVCLK0_VAL(
+							 MSPM0_SYSPLL_CLK0_DIV));
+	} else {
+		soclock->syspllcfg0 &= ~SYSCTL_SYSPLLCFG0_ENABLECLK0;
+	}
+
+#if DT_SAME_NODE(DT_MCLK_CLOCKS_CTRL, DT_SYSPLL)
+	if (MSPM0_SYSPLL_HAS_CLK2X) {
+		soclock->syspllcfg0 |= SYSCTL_SYSPLLCFG0_MCLK2XVCO;
+	} else {
+		soclock->syspllcfg0 &= ~SYSCTL_SYSPLLCFG0_MCLK2XVCO;
+	}
+#endif
+
+	/* enable SYSPLL */
+	return clock_mspm0_on(dev, (clock_control_subsys_t)&syspll_subsys);
+}
+#endif /* DT_SYSPLL_OKAY */
+
+static int clock_mspm0_configure(const struct device *dev, clock_control_subsys_t sys, void *data)
+{
+	volatile struct mspm_sysctl_regs *regs = MSPM_SYSCTL_REGS;
+	volatile struct mspm_sysctl_soclock_regs *soclock = &regs->soclock;
+	struct mspm0_sys_clock *sys_clock = (struct mspm0_sys_clock *)sys;
+	enum mspm0_clock_source *source = (enum mspm0_clock_source *)data;
+	bool disabled = false;
+	int ret;
+
+	if (clock_mspm0_get_status(dev, sys) == CLOCK_CONTROL_STATUS_ON) {
+		disabled = clock_mspm0_off(dev, sys) == 0;
+	}
+
+	switch (sys_clock->clk) {
+#if DT_SYSPLL_OKAY
+	case MSPM0_CLOCK_SYSPLL:
+		ret = clock_mspm0_configure_syspll(dev, soclock, *source);
+		break;
+#endif /* DT_SYSPLL_OKAY */
+
+	default:
+		ret = -ENOTSUP;
+	}
+
+	/* restore the clock to whatever state it was in before this call */
+	if (disabled) {
+		if (ret < 0) {
+			clock_mspm0_on(dev, sys);
+		} else {
+			return clock_mspm0_on(dev, sys);
+		}
+	}
+
+	return ret;
 }
 
 static int clock_mspm0_init(const struct device *dev)
 {
 	volatile struct mspm_sysctl_regs *regs = MSPM_SYSCTL_REGS;
 	volatile struct mspm_sysctl_soclock_regs *soclock = &regs->soclock;
+	int ret = 0;
+
+	LOG_DBG("initializing MSPM0 clock tree");
 
 	/* set SYSOSCFG frequency */
 	soclock->sysosccfg = (soclock->sysosccfg & ~SYSCTL_SYSOSCCFG_FREQ) |
@@ -315,18 +666,13 @@ static int clock_mspm0_init(const struct device *dev)
 	DL_SYSCTL_setULPCLKDivider(mspm0_ulpclk_cfg.clk_div);
 #endif
 
-#if MSPM0_SYSPLL_ENABLED
-#if DT_SAME_NODE(DT_MCLK_CLOCKS_CTRL, DT_NODELABEL(syspll))
-	if (clock_mspm0_cfg_syspll.enableCLK0 == DL_SYSCTL_SYSPLL_CLK0_ENABLE) {
-		clock_mspm0_cfg_syspll.sysPLLMCLK = DL_SYSCTL_SYSPLL_MCLK_CLK0;
+#if DT_SYSPLL_OKAY
+	ret = clock_mspm0_init_syspll(dev);
+	if (ret < 0) {
+		LOG_ERR("failed to init SYSPLL: %d", ret);
+		return ret;
 	}
-#endif
-#if DT_SAME_NODE(DT_SYSPLL_CLOCKS_CTRL, DT_NODELABEL(hfclk))
-	clock_mspm0_cfg_syspll.sysPLLRef = DL_SYSCTL_SYSPLL_REF_HFCLK;
-#endif
-	DL_SYSCTL_configSYSPLL(
-			(DL_SYSCTL_SYSPLLConfig *)&clock_mspm0_cfg_syspll);
-#endif
+#endif /* DT_SYSPLL_OKAY */
 
 #if MSPM0_HFCLK_ENABLED
 #if DT_SAME_NODE(DT_HFCLK_CLOCKS_CTRL, DT_NODELABEL(hfxt))
