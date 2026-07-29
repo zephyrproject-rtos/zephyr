c81b2d0406778938a33dd7e152aa7ba51fef6e1d
Mahesh Mahadevan <mahesh.mahadevan@nxp.com>
2026-07-29 16:01:01 -0500

    Revert "[nxp fromlist] drivers: wifi: nxp: use wlan API for country code"
    This reverts commit 311221d9b99bcdae6e6cf8e8785c2ad4d4672ddd.
    
    Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

--- drivers/wifi/nxp/src/nxp_wifi_drv.c
+++ drivers/wifi/nxp/src/nxp_wifi_drv.c
@@ -1941,7 +1941,7 @@ static int nxp_wifi_reg_domain(const struct device *dev, struct net_if *iface __
 			index += nxp_wifi_cfp_no;
 		}
 		reg_domain->num_channels = index;
-		wlan_get_country_code(reg_domain->country_code);
+		wifi_get_country_code(reg_domain->country_code);
 	} else {
 		if (is_uap_started()) {
 			LOG_ERR("region code can not be set after uAP start!");
