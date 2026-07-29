7e0b4adea3c8a84b0cc6c0d302393c3e91f28b3b
Mathieu Choplain <mathieu.choplain-ext@st.com>
2026-07-29 17:52:38 +0200

    arch: arm: core: mpu: drop arm_core_mpu_mem_partition_config_update()
    Commit 2222fa1426a0aa83503e906282591ae7df23c211 removed all callers of
    the function arm_core_mpu_mem_partition_config_update()... but not the
    function itself, which became dead code lingering in tree since then...
    
    Get rid of this function - it has served no purpose for long enough.
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain-ext@st.com>

--- arch/arm/core/mpu/arm_core_mpu_dev.h
+++ arch/arm/core/mpu/arm_core_mpu_dev.h
@@ -203,27 +203,6 @@ void arm_core_mpu_configure_dynamic_mpu_regions(
 	const struct z_arm_mpu_partition *dynamic_regions,
 	uint8_t regions_num);
 
-#if defined(CONFIG_USERSPACE)
-/**
- * @brief update configuration of an active memory partition
- *
- * Internal API function to re-configure the access permissions of an
- * active memory partition, i.e. a partition that has earlier been
- * configured in the (current) thread context.
- *
- * @param partition Pointer to a structure holding the partition information
- *                  (must be valid).
- * @param new_attr  New access permissions attribute for the partition.
- *
- * The function shall assert if the operation cannot be not performed
- * successfully (e.g. the given partition can not be found).
- */
-void arm_core_mpu_mem_partition_config_update(
-	struct z_arm_mpu_partition *partition,
-	k_mem_partition_attr_t *new_attr);
-
-#endif /* CONFIG_USERSPACE */
-
 /**
  * @brief configure the base address and size for an MPU region
  *
