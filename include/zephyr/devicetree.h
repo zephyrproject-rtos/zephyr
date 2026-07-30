c2a142c65c1af0dadf47794d691cc544224e885b
Mika Braunschweig <mika.braunschweig@siemens.com>
2026-07-30 15:55:53 +0200

    include: zephyr: devicetree: romstart: add header to get romstart region
    Add a devicetree header file to get the memory region that is used inside
    the chosen zephyr,romstart property and check, if it exists.
    
    Signed-off-by: Mika Braunschweig <mika.braunschweig@siemens.com>

--- include/zephyr/devicetree.h
+++ include/zephyr/devicetree.h
@@ -6429,5 +6429,6 @@
 #include <zephyr/devicetree/partitions.h>
 #include <zephyr/devicetree/sram.h>
 #include <zephyr/devicetree/cpu.h>
+#include <zephyr/devicetree/romstart.h>
 
 #endif /* ZEPHYR_INCLUDE_DEVICETREE_H_ */
