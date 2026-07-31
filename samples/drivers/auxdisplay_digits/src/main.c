2332946d3d89bc58b92497b4027f8c81e9b70683
Basile GRUNER <basile.gruner@smile.fr>
2026-07-31 14:45:00 +0200

    samples: auxdisplay: generalize sample for more devices
    The sample try to define `AUXDISPLAY_DIGIT_COUNT` using node labeled
    `slcd_panel` instead of `auxdisplay_0`, resulting in a build error when
    using other auxdisplay devices.
    
    Correct this behavior by using the correct node label to define
    `AUXDISPLAY_DIGIT_COUNT` when `slcd_panel` available.
    
    Signed-off-by: Basile GRUNER <basile.gruner@smile.fr>

--- samples/drivers/auxdisplay_digits/src/main.c
+++ samples/drivers/auxdisplay_digits/src/main.c
@@ -18,9 +18,12 @@ LOG_MODULE_REGISTER(auxdisplay_sample, LOG_LEVEL_DBG);
 #define AUXDISPLAY_NODE        DT_NODELABEL(auxdisplay_0)
 #if DT_NODE_HAS_COMPAT(AUXDISPLAY_NODE, gpio_7_segment)
 #define AUXDISPLAY_DIGIT_COUNT DT_PROP_LEN(AUXDISPLAY_NODE, digit_gpios)
-#else
+#elif DT_NODE_EXISTS(DT_NODELABEL(slcd_panel))
 #define AUXDISPLAY_DIGIT_COUNT (DT_PROP(DT_NODELABEL(slcd_panel), columns) * \
 	DT_PROP(DT_NODELABEL(slcd_panel), rows))
+#else
+#define AUXDISPLAY_DIGIT_COUNT (DT_PROP(AUXDISPLAY_NODE, columns) * \
+	DT_PROP(AUXDISPLAY_NODE, rows))
 #endif
 
 static const struct device *const dev = DEVICE_DT_GET(AUXDISPLAY_NODE);
