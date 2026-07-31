3d489fe28f3c609922816e53ea8652b02ba973c0
Syed Maaz <Syed.Maaz@microchip.com>
2026-07-31 16:26:27 +0530

    soc: microchip: pic32c: pic32cm_jh: add SUPC driver support
    Add a SUPC (Supply Controller) source file with a driver API to
    control the internal voltage reference (VREF) output used by
    ADC/DAC input channels: enable/disable the reference output and
    select/read the reference voltage level.
    
    Wire up SUPC initialization in the SoC reset hook. soc_supc_init()
    reads the standby-regulator-sel, vref-on-demand, and
    vref-standby-mode properties from the supc devicetree node and
    configures the voltage regulator and voltage reference behavior
    accordingly during early boot.
    
    Convert the common SoC CMakeLists.txt to use zephyr_library() so
    the new supc_mchp.c source can be added via
    zephyr_library_sources().
    
    Signed-off-by: Syed Maaz <Syed.Maaz@microchip.com>

--- soc/microchip/pic32c/pic32cm_jh/common/soc.c
+++ soc/microchip/pic32c/pic32cm_jh/common/soc.c
@@ -10,10 +10,47 @@
  */
 
 #include <zephyr/devicetree.h>
+#include <soc.h>
 
 #define SRAM0_NODE DT_CHOSEN(zephyr_sram)
 #define SRAM0_BASE DT_REG_ADDR(SRAM0_NODE)
 #define SRAM0_SIZE DT_REG_SIZE(SRAM0_NODE)
+
+/**
+ * @brief Initialize SUPC peripheral from devicetree settings.
+ *
+ * Enables MCLK for SUPC and configures voltage regulator and
+ * voltage reference (on-demand and standby mode) based on
+ * standby-regulator-sel, vref-on-demand, and vref-standby-mode
+ * devicetree properties of the supc node.
+ *
+ * @retval 0 On success.
+ */
+__attribute__((always_inline)) static inline int soc_supc_init(void)
+{
+	supc_registers_t *supc_regs = (supc_registers_t *)DT_REG_ADDR(DT_NODELABEL(supc));
+	int standby_regulator_sel = DT_PROP(DT_NODELABEL(supc), standby_regulator_sel);
+	int vref_on_demand = DT_PROP(DT_NODELABEL(supc), vref_on_demand);
+	int vref_standby_mode = DT_PROP(DT_NODELABEL(supc), vref_standby_mode);
+
+	uint32_t vref_val = supc_regs->SUPC_VREF;
+
+	/* Configure VREG. Mask the values loaded from NVM during reset.*/
+	supc_regs->SUPC_VREG = (standby_regulator_sel != 0) ?
+			(supc_regs->SUPC_VREG | SUPC_VREG_RUNSTDBY_Msk) : supc_regs->SUPC_VREG;
+
+	/* Configure SUPC VREF On demand */
+	vref_val = (vref_on_demand != 0) ?
+			(vref_val | SUPC_VREF_ONDEMAND_Msk) : vref_val;
+
+	/* Configure SUPC VREF Standby mode*/
+	vref_val = (vref_standby_mode != 0) ?
+			(vref_val | SUPC_VREF_RUNSTDBY_Msk) : vref_val;
+
+	supc_regs->SUPC_VREF = vref_val;
+
+	return 0;
+}
 
 /**
  * @brief Early Reset hook to run SoC-specific initialization.
@@ -41,4 +78,14 @@ __attribute__((naked)) void soc_early_reset_hook(void)
 		"	blo  loop\n"
 		"	bx   lr\n"
 		:: [start] "i" (SRAM0_BASE), [end] "i" (SRAM0_BASE + SRAM0_SIZE));
+}
+
+/**
+ * @brief Reset hook to run SoC-specific initialization.
+ *
+ * This is invoked early at reset.
+ */
+void soc_reset_hook(void)
+{
+	soc_supc_init();
 }
