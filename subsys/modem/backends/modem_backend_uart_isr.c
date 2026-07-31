4d6ad1b19f4f96563078e526ba2ec6304a60375e
Liam Ogletree <liam.ogletree@cirrus.com>
2026-07-31 14:38:47 -0500

    modem: backends: Unify runtime PM log messages
    Propagates #113545 to modems subsystem.
    
    Signed-off-by: Liam Ogletree <liam.ogletree@cirrus.com>

--- subsys/modem/backends/modem_backend_uart_isr.c
+++ subsys/modem/backends/modem_backend_uart_isr.c
@@ -116,7 +116,7 @@ static int modem_backend_uart_isr_open(void *data)
 
 	ret = pm_device_runtime_get(backend->uart);
 	if (ret < 0) {
-		LOG_ERR("Failed to power on UART: %d", ret);
+		LOG_ERR_PM_DEVICE_RUNTIME_GET(backend->uart, ret);
 		return ret;
 	}
 	if (backend->dtr_gpio) {
@@ -269,7 +269,7 @@ static int modem_backend_uart_isr_close(void *data)
 	}
 	ret = pm_device_runtime_put_async(backend->uart, K_NO_WAIT);
 	if (ret < 0) {
-		LOG_ERR("Failed to power off UART: %d", ret);
+		LOG_ERR_PM_DEVICE_RUNTIME_PUT(backend->uart, ret);
 		return ret;
 	}
 	modem_pipe_notify_closed(&backend->pipe);
