ddcdffeff00d27a943c22737bb32b517632329f0
Andrej Butok <andrey.butok@nxp.com>
2026-07-31 13:08:03 +0200

    hwinfo: nxp: unify driver naming
    - Unify the naming of all NXP hwinfo drivers to use a consistent
      HWINFO_NXP_* Kconfig prefix and hwinfo_nxp_*.c source file naming,
      replacing the previous inconsistent mix of symbols.
    - Consolidate the per-driver Kconfig fragments into a single
      Kconfig.nxp file.
    - No functional change.
    
    Signed-off-by: Andrej Butok <andrey.butok@nxp.com>

--- drivers/hwinfo/hwinfo_nxp_rstctl.c
+++ drivers/hwinfo/hwinfo_nxp_rstctl.c
@@ -9,16 +9,16 @@
 #include <zephyr/logging/log.h>
 #include <fsl_reset.h>
 
-LOG_MODULE_REGISTER(hwinfo_rstctl, CONFIG_HWINFO_LOG_LEVEL);
+LOG_MODULE_REGISTER(hwinfo_nxp_rstctl, CONFIG_HWINFO_LOG_LEVEL);
 
 /* The reset source bits are defined in the rstctl_reset_source_t enum. */
-#define MCUX_RESET_POR_FLAG      RSTCTL3_SYSRSTSTAT_VDD_POR_MASK
-#define MCUX_RESET_PIN_FLAG      RSTCTL3_SYSRSTSTAT_RESETN_RESET_MASK
-#define MCUX_RESET_DEBUG_FLAG    RSTCTL3_SYSRSTSTAT_ISP_AP_RESET_MASK
-#define MCUX_RESET_SOFTWARE_FLAG RSTCTL3_SYSRSTSTAT_ITRC_SW_RESET_MASK
-#define MCUX_RESET_CPU_FLAG                                                         \
+#define NXP_RESET_POR_FLAG      RSTCTL3_SYSRSTSTAT_VDD_POR_MASK
+#define NXP_RESET_PIN_FLAG      RSTCTL3_SYSRSTSTAT_RESETN_RESET_MASK
+#define NXP_RESET_DEBUG_FLAG    RSTCTL3_SYSRSTSTAT_ISP_AP_RESET_MASK
+#define NXP_RESET_SOFTWARE_FLAG RSTCTL3_SYSRSTSTAT_ITRC_SW_RESET_MASK
+#define NXP_RESET_CPU_FLAG                                                         \
 	(RSTCTL3_SYSRSTSTAT_CPU0_RESET_MASK | RSTCTL3_SYSRSTSTAT_CPU1_RESET_MASK)
-#define MCUX_RESET_WATCHDOG_FLAG                                                    \
+#define NXP_RESET_WATCHDOG_FLAG                                                    \
 	(RSTCTL3_SYSRSTSTAT_WWDT0_RESET_MASK | RSTCTL3_SYSRSTSTAT_WWDT1_RESET_MASK |    \
 	 RSTCTL3_SYSRSTSTAT_WWDT2_RESET_MASK | RSTCTL3_SYSRSTSTAT_WWDT3_RESET_MASK |    \
 	 RSTCTL3_SYSRSTSTAT_CDOG0_RESET_MASK | RSTCTL3_SYSRSTSTAT_CDOG1_RESET_MASK |    \
@@ -28,37 +28,37 @@ LOG_MODULE_REGISTER(hwinfo_rstctl, CONFIG_HWINFO_LOG_LEVEL);
 /**
  * @brief Translate from RSTCTL reset source mask to Zephyr hwinfo sources mask.
  *
- * Translate bitmask from MCUX RSTCTL reset source bitmask to Zephyr
+ * Translate bitmask from NXP RSTCTL reset source bitmask to Zephyr
  * hwinfo reset source bitmask.
  *
- * @param NXP MCUX RSTCTL reset source mask.
+ * @param NXP RSTCTL reset source mask.
  * @retval Zephyr hwinfo reset source mask.
  */
-static uint32_t hwinfo_mcux_rstctl_xlate_reset_sources(uint32_t sources)
+static uint32_t hwinfo_nxp_rstctl_xlate_reset_sources(uint32_t sources)
 {
 	uint32_t mask = 0;
 
-	if (sources & MCUX_RESET_POR_FLAG) {
+	if (sources & NXP_RESET_POR_FLAG) {
 		mask |= RESET_POR;
 	}
 
-	if (sources & MCUX_RESET_PIN_FLAG) {
+	if (sources & NXP_RESET_PIN_FLAG) {
 		mask |= RESET_PIN;
 	}
 
-	if (sources & MCUX_RESET_DEBUG_FLAG) {
+	if (sources & NXP_RESET_DEBUG_FLAG) {
 		mask |= RESET_DEBUG;
 	}
 
-	if (sources & MCUX_RESET_SOFTWARE_FLAG) {
+	if (sources & NXP_RESET_SOFTWARE_FLAG) {
 		mask |= RESET_SOFTWARE;
 	}
 
-	if (sources & MCUX_RESET_CPU_FLAG) {
+	if (sources & NXP_RESET_CPU_FLAG) {
 		mask |= RESET_SOFTWARE;
 	}
 
-	if (sources & MCUX_RESET_WATCHDOG_FLAG) {
+	if (sources & NXP_RESET_WATCHDOG_FLAG) {
 		mask |= RESET_WATCHDOG;
 	}
 
@@ -69,7 +69,7 @@ int z_impl_hwinfo_get_reset_cause(uint32_t *cause)
 {
 	const uint32_t sources = RESET_GetPreviousResetSources();
 
-	*cause = hwinfo_mcux_rstctl_xlate_reset_sources(sources);
+	*cause = hwinfo_nxp_rstctl_xlate_reset_sources(sources);
 
 	return 0;
 }
