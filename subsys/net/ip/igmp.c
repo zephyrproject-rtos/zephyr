58930578b533a61ef5dcda51e1c63019b7456285
Fin Maaß <f.maass@vogl-electronic.com>
2026-08-04 19:59:43 +0200

    net: if: mcast: remove lookup from rejoin
    remove lookup from rejoin, as no longer needed
    as we only use it then using addresses that are
    already on the iface.
    
    Signed-off-by: Fin Maaß <f.maass@vogl-electronic.com>

--- subsys/net/ip/igmp.c
+++ subsys/net/ip/igmp.c
@@ -605,38 +605,32 @@ drop:
 }
 #endif
 
-int net_ipv4_igmp_rejoin(struct net_if *iface, const struct net_in_addr *addr)
+int net_ipv4_igmp_rejoin(struct net_if *iface, struct net_if_mcast_addr *addr)
 {
-	struct net_if_mcast_addr *maddr;
-	int ret = 0;
-
-	maddr = net_if_ipv4_maddr_lookup(addr, &iface);
-	if (maddr == NULL) {
-		return -ENOENT;
-	}
+	int ret;
 
 	if (net_if_is_offloaded(iface)) {
 		goto out;
 	}
 
 #if defined(CONFIG_NET_IPV4_IGMPV3)
-	ret = igmpv3_send_generic(iface, maddr);
+	ret = igmpv3_send_generic(iface, addr);
 #else
-	ret = igmp_send_generic(iface, addr, true);
+	ret = igmp_send_generic(iface, &addr->address.in_addr, true);
 #endif
 	if (ret < 0) {
 		return ret;
 	}
 
 out:
-	net_if_ipv4_maddr_join(iface, maddr);
+	net_if_ipv4_maddr_join(iface, addr);
 
-	net_if_mcast_monitor(iface, &maddr->address, true);
+	net_if_mcast_monitor(iface, &addr->address, true);
 
-	net_mgmt_event_notify_with_info(NET_EVENT_IPV4_MCAST_JOIN, iface, &maddr->address.in_addr,
+	net_mgmt_event_notify_with_info(NET_EVENT_IPV4_MCAST_JOIN, iface, &addr->address.in_addr,
 					sizeof(struct net_in_addr));
 
-	return ret;
+	return 0;
 }
 
 int net_ipv4_igmp_join(struct net_if *iface, const struct net_in_addr *addr,
