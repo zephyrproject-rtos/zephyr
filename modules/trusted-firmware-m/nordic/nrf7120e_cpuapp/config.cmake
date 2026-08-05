ab151a09294589d0f9b5cc2efbb083fac5dfcc92
Robert Robinson <robert.robinson@nordicsemi.no>
2026-08-05 17:09:30 +0100

    modules: tf-m: Add support for nRF7120e ns
    Add support for nRF7120e non-secure application core in TF-M.
    
    Signed-off-by: Robert Robinson <robert.robinson@nordicsemi.no>

---
+++ modules/trusted-firmware-m/nordic/nrf7120e_cpuapp/config.cmake
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2025, Nordic Semiconductor ASA.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set(NRF_SOC_VARIANT nrf7120e CACHE STRING "nRF SoC Variant")
+
+include(${PLATFORM_PATH}/common/nrf7120e/config.cmake)
