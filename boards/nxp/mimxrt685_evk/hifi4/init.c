35a92518f692d88ed7cc5e03d8ff7d8509b88ec1
Vit Stanicek <vit.stanicek@nxp.com>
2026-08-05 09:55:46 +0200

    boards: mimxrt685_evk: Update I2S Flexcomm sharing for hifi4
    Modify boards/nxp/mimxrt685_evk/hifi4/init.c to set up signal between
    Flexcomm peripheral instances 1 and 3, such that Flexcomm 1 receives SCK
    and WS and shares it with Flexcomm 3.
    
    This simplifies configuration when duplex audio is not involved (only
    one of those Flexcomms is needed) - they all just have to be I2S
    targets.
    
    Signed-off-by: Vit Stanicek <vit.stanicek@nxp.com>

--- boards/nxp/mimxrt685_evk/hifi4/init.c
+++ boards/nxp/mimxrt685_evk/hifi4/init.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2024 NXP
+ * Copyright 2020-2024, 2026 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -15,30 +15,46 @@ void board_early_init_hook(void)
 	(DT_NODE_HAS_COMPAT_STATUS(DT_NODELABEL(flexcomm3), nxp_lpc_i2s, okay)) && \
 	CONFIG_I2S
 
-	/* Set shared signal set 0 SCK, WS from Transmit I2S - Flexcomm3 */
+#if IS_ENABLED(CONFIG_I2S_TEST_SEPARATE_DEVICES)
+	/*
+	 * Internal loopback
+	 *
+	 * Flexcomm 3 (I2S Tx) provides SCK, WS and DATA (controller) into set #0.
+	 */
 	SYSCTL1->SHAREDCTRLSET[0] = SYSCTL1_SHAREDCTRLSET_SHAREDSCKSEL(3) |
-				SYSCTL1_SHAREDCTRLSET_SHAREDWSSEL(3);
+		SYSCTL1_SHAREDCTRLSET_SHAREDWSSEL(3) |
+		SYSCTL1_SHAREDCTRLSET_SHAREDDATASEL(3) |
+		SYSCTL1_SHAREDCTRLSET_FC3DATAOUTEN(1);
 
-#ifdef CONFIG_I2S_TEST_SEPARATE_DEVICES
-	/* Select Data in from Transmit I2S - Flexcomm 3 */
-	SYSCTL1->SHAREDCTRLSET[0] |= SYSCTL1_SHAREDCTRLSET_SHAREDDATASEL(3);
-	/* Enable Transmit I2S - Flexcomm 3 for Shared Data Out */
-	SYSCTL1->SHAREDCTRLSET[0] |= SYSCTL1_SHAREDCTRLSET_FC3DATAOUTEN(1);
-#endif
-
-	/* Set Receive I2S - Flexcomm 1 SCK, WS from shared signal set 0 */
+	/* Flexcomm 1 (I2S Rx) consumes the set. */
 	SYSCTL1->FCCTRLSEL[1] = SYSCTL1_FCCTRLSEL_SCKINSEL(1) |
-				SYSCTL1_FCCTRLSEL_WSINSEL(1);
+				SYSCTL1_FCCTRLSEL_WSINSEL(1) |
+				SYSCTL1_FCCTRLSEL_DATAINSEL(1);
 
-	/* Set Transmit I2S - Flexcomm 3 SCK, WS from shared signal set 0 */
+	/* Flexcomm 3 (I2S Tx) consumes the provided signals internally. */
 	SYSCTL1->FCCTRLSEL[3] = SYSCTL1_FCCTRLSEL_SCKINSEL(1) |
-				SYSCTL1_FCCTRLSEL_WSINSEL(1);
+				SYSCTL1_FCCTRLSEL_WSINSEL(1) |
+				SYSCTL1_FCCTRLSEL_DATAOUTSEL(1);
+#else
+	/*
+	 * Duplex audio configuration
+	 *
+	 * Flexcomm 1 (I2S Rx) provides SCK and WS into set #0.
+	 * These are sourced from outside pads, being routed to the WM8904.
+	 *
+	 * When configuring any of the Flexcomms for I2S traffic, configure them
+	 * as I2S targets.
+	 */
+	SYSCTL1->SHAREDCTRLSET[0] = SYSCTL1_SHAREDCTRLSET_SHAREDSCKSEL(1) |
+		SYSCTL1_SHAREDCTRLSET_SHAREDWSSEL(1);
 
-#ifdef CONFIG_I2S_TEST_SEPARATE_DEVICES
-	/* Select Receive I2S - Flexcomm 1 Data in from shared signal set 0 */
-	SYSCTL1->FCCTRLSEL[1] |= SYSCTL1_FCCTRLSEL_DATAINSEL(1);
-	/* Select Transmit I2S - Flexcomm 3 Data out to shared signal set 0 */
-	SYSCTL1->FCCTRLSEL[3] |= SYSCTL1_FCCTRLSEL_DATAOUTSEL(1);
+	/* Flexcomm 1 (I2S Rx) consumes provided signals internally. */
+	SYSCTL1->FCCTRLSEL[1] = SYSCTL1_FCCTRLSEL_SCKINSEL(1) |
+		SYSCTL1_FCCTRLSEL_WSINSEL(1);
+
+	/* Flexcomm 3 (I2S Tx) consumes the set. */
+	SYSCTL1->FCCTRLSEL[3] = SYSCTL1_FCCTRLSEL_SCKINSEL(1) |
+		SYSCTL1_FCCTRLSEL_WSINSEL(1);
 #endif
 
 #endif
