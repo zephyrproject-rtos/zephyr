zephyrproject-rtos/zephyr

commit 0e41b07309a893f1f5eafeede9bbeee237ccf8a4

drivers : flash: update way to get flash size
The LL_GetFlashSize function has been removed for
this new HAL H7RS release. Retrieves the value now from
the devicetree using the DT_REG_SIZE macro.

Signed-off-by: Fabrice DJIATSA <fabrice.djiatsa-ext@st.com>

drivers/flash/flash_stm32h7x.c
@@ -45,7 +45,7 @@ LOG_MODULE_REGISTER(LOG_DOMAIN);
 #define REAL_FLASH_SIZE_KB (KB(STM32H7_M4_FLASH_SIZE * 2))
 #endif
 #else
-#define REAL_FLASH_SIZE_KB KB(LL_GetFlashSize())
+#define REAL_FLASH_SIZE_KB DT_REG_SIZE(DT_INST(0, st_stm32_nv_flash))
 #endif
 #define SECTOR_PER_BANK ((REAL_FLASH_SIZE_KB / FLASH_SECTOR_SIZE) / 2)
 #if defined(DUAL_BANK)

commit 94429f24120557e394bf7c5bc8eb922e64ca308c

drivers: flash: stm32: Restore full access to bank 1 on dual core H7s
The changeset in which the removal of LL_GetFLashSize was accounted for
failed to consider that the size cell of the flash nodes on the dual
core MCUs - i.e. the STM32H747, STM32H757, STM32H745 and STM32H755 -
contains the size of a single bank rather than that of the entire flash.
As a result, the driver mistakes the flash mapped at
0x08008000-0x80fffff for a gap between two discontinuous memory banks
rather than a part of bank 1, thereby rendering sectors 4 through 7 of
the latter inaccessible.

Since both banks on the affected MCUs are the same size, simply
multiply said size by 2 to attain the correct total flash size.

Fixes: 0e41b07309a893 (drivers : flash: update way to get flash size)

Signed-off-by: Vilhelm Engström <vilhelm.engstrom@tuta.io>

drivers/flash/flash_stm32h7x.c
@@ -34,6 +34,7 @@ LOG_MODULE_REGISTER(LOG_DOMAIN);
 #define STM32H7_FLASH_OPT_TIMEOUT_MS 800
 
 #if DT_NODE_HAS_PROP(DT_INST(0, st_stm32_nv_flash), bank2_flash_size)
+
 #define STM32H7_M4_FLASH_SIZE DT_PROP_OR(DT_INST(0, st_stm32_nv_flash), bank2_flash_size, 0)
 #ifdef CONFIG_CPU_CORTEX_M4
 #if STM32H7_M4_FLASH_SIZE == 0
@@ -44,9 +45,17 @@ LOG_MODULE_REGISTER(LOG_DOMAIN);
 #else
 #define REAL_FLASH_SIZE_KB (DT_REG_SIZE(DT_INST(0, st_stm32_nv_flash)) * 2)
 #endif
+
+#else /* DT_NODE_HAS_PROP(DT_INST(0, st_stm32_nv_flash), bank2_flash_size) */
+
+#if defined(CONFIG_STM32H7_DUAL_CORE)
+/* Dual core STM32H7x products feature two banks of the same size */
+#define REAL_FLASH_SIZE_KB (DT_REG_SIZE(DT_INST(0, st_stm32_nv_flash)) * 2)
 #else
 #define REAL_FLASH_SIZE_KB DT_REG_SIZE(DT_INST(0, st_stm32_nv_flash))
-#endif
+#endif /* CONFIG_STM32H7_DUAL_CORE */
+
+#endif /* DT_NODE_HAS_PROP(DT_INST(0, st_stm32_nv_flash), bank2_flash_size) */
 #define SECTOR_PER_BANK ((REAL_FLASH_SIZE_KB / FLASH_SECTOR_SIZE) / 2)
 #if defined(DUAL_BANK)
 #define STM32H7_SERIES_MAX_FLASH_KB KB(2048)