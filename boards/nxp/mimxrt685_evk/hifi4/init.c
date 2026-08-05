f57448c067ab320d14712c4447780abff58cbf47
Vit Stanicek <vit.stanicek@nxp.com>
2026-08-05 09:55:46 +0200

    boards: mimxrt685_evk: Update I2S Flexcomm sharing for hifi4
    Modify boards/nxp/mimxrt685_evk/hifi4/init.c to set up signal between
    Flexcomm peripheral instances 1 and 3, such that Flexcomm 1 receives SCK
    and WS and shares it with Flexcomm 3.
    
    This simplifies configuration when duplex audio is not involved (only
    one of those Flexcomms is needed) - they all just have to be I2S
    targets.
    
    Signed-off-by: Vit Stanicek <vit.stanicek@nxp.com>

--- boards/nxp/mimxrt685_evk/hifi4/init.c
+++ boards/nxp/mimxrt685_evk/hifi4/init.c
@@ -24,6 +24,10 @@ void board_early_init_hook(void)
 	SYSCTL1->SHAREDCTRLSET[0] |= SYSCTL1_SHAREDCTRLSET_SHAREDDATASEL(3);
 	/* Enable Transmit I2S - Flexcomm 3 for Shared Data Out */
 	SYSCTL1->SHAREDCTRLSET[0] |= SYSCTL1_SHAREDCTRLSET_FC3DATAOUTEN(1);
+#else
+	/* Provide SCK and WS for shared set #0 from Flexcomm 1 */
+	SYSCTL1->SHAREDCTRLSET[0] = SYSCTL1_SHAREDCTRLSET_SHAREDSCKSEL(1) |
+				SYSCTL1_SHAREDCTRLSET_SHAREDWSSEL(1);
 #endif
 
 	/* Set Receive I2S - Flexcomm 1 SCK, WS from shared signal set 0 */
