13cfc03db17da9c2a2f2295c2084eeb693fdc4da
Guilherme Costa <guilhermecosta@stratioautomotive.com>
2026-08-06 14:36:15 +0000

    drivers: modem: Add support for quectel EG21-G
    Create a new quectel,21-g.yaml device tree binding and modify the
    current EG25-G modem cellular code to also support EG21-G, removing
    specific references to EG25-G from the implementation and
    making sure the proper definitions exist for both EG21 and EG25.
    
    Also make mdm-reset-gpios optional for EG25-G.
    
    Signed-off-by: Guilherme Costa <guilhermecosta@stratioautomotive.com>

--- drivers/modem/vendor_modem_cellular/cellular_quectel_eg2x_g.c
+++ drivers/modem/vendor_modem_cellular/cellular_quectel_eg2x_g.c
@@ -9,99 +9,96 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define DT_DRV_COMPAT quectel_eg25_g
-
-static void quectel_eg25_g_on_qeng(struct modem_chat *chat, char **argv, uint16_t argc,
+static void quectel_eg2x_g_on_qeng(struct modem_chat *chat, char **argv, uint16_t argc,
 				   void *user_data);
 
 MODEM_CELLULAR_COMMON_CHAT_MATCHES();
 
-MODEM_CHAT_MATCHES_DEFINE(quectel_eg25_g_unsol, MODEM_CELLULAR_COMMON_UNSOL_MATCHES,
-			  MODEM_CHAT_MATCH("+QENG: ", ",", quectel_eg25_g_on_qeng));
+MODEM_CHAT_MATCHES_DEFINE(quectel_eg2x_g_unsol, MODEM_CELLULAR_COMMON_UNSOL_MATCHES,
+			  MODEM_CHAT_MATCH("+QENG: ", ",", quectel_eg2x_g_on_qeng));
 
 /*
  * AT+CMUX <port_speed> is specified in 3GPP TS 27.007 defining values 1..6 (up to 230400);
- * the EG25-G accepts vendor values for higher rates. The modem moves its multiplexer UART
+ * the EG2X-G accepts vendor values for higher rates. The modem moves its multiplexer UART
  * to the named rate on CMUX entry, so it must track the host rate selected by
  * CONFIG_MODEM_CELLULAR_NEW_BAUDRATE.
  */
 #if CONFIG_MODEM_CELLULAR_NEW_BAUDRATE == 921600
-#define QUECTEL_EG25_G_CMUX_PORT_SPEED "8"
+#define QUECTEL_EG2X_G_CMUX_PORT_SPEED "8"
 #elif CONFIG_MODEM_CELLULAR_NEW_BAUDRATE == 460800
-#define QUECTEL_EG25_G_CMUX_PORT_SPEED "7"
+#define QUECTEL_EG2X_G_CMUX_PORT_SPEED "7"
 #elif CONFIG_MODEM_CELLULAR_NEW_BAUDRATE == 230400
-#define QUECTEL_EG25_G_CMUX_PORT_SPEED "6"
+#define QUECTEL_EG2X_G_CMUX_PORT_SPEED "6"
 #elif CONFIG_MODEM_CELLULAR_NEW_BAUDRATE == 115200
-#define QUECTEL_EG25_G_CMUX_PORT_SPEED "5"
+#define QUECTEL_EG2X_G_CMUX_PORT_SPEED "5"
 #elif CONFIG_MODEM_CELLULAR_NEW_BAUDRATE == 57600
-#define QUECTEL_EG25_G_CMUX_PORT_SPEED "4"
+#define QUECTEL_EG2X_G_CMUX_PORT_SPEED "4"
 #elif CONFIG_MODEM_CELLULAR_NEW_BAUDRATE == 38400
-#define QUECTEL_EG25_G_CMUX_PORT_SPEED "3"
+#define QUECTEL_EG2X_G_CMUX_PORT_SPEED "3"
 #elif CONFIG_MODEM_CELLULAR_NEW_BAUDRATE == 19200
-#define QUECTEL_EG25_G_CMUX_PORT_SPEED "2"
+#define QUECTEL_EG2X_G_CMUX_PORT_SPEED "2"
 #elif CONFIG_MODEM_CELLULAR_NEW_BAUDRATE == 9600
-#define QUECTEL_EG25_G_CMUX_PORT_SPEED "1"
+#define QUECTEL_EG2X_G_CMUX_PORT_SPEED "1"
 #else
-#error "CONFIG_MODEM_CELLULAR_NEW_BAUDRATE unsupported for EG25-G CMUX port speed"
+#error "CONFIG_MODEM_CELLULAR_NEW_BAUDRATE unsupported for EG2X-G CMUX port speed"
 #endif
 
+#define BUS_HAS_FLOW_CONTROL(modem)                                                                \
+	DT_PROP_OR(DT_BUS(DT_COMPAT_GET_ANY_STATUS_OKAY(modem)), hw_flow_control, 0)
+
 MODEM_CHAT_SCRIPT_CMDS_DEFINE(
-	quectel_eg25_g_init_chat_script_cmds, MODEM_CHAT_SCRIPT_CMD_RESP("ATE0", ok_match),
+	quectel_eg2x_g_init_chat_script_cmds, MODEM_CHAT_SCRIPT_CMD_RESP("ATE0", ok_match),
 	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CFUN=4", ok_match),
 	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CMEE=1", ok_match),
-	IF_ENABLED(DT_PROP(DT_BUS(DT_COMPAT_GET_ANY_STATUS_OKAY(quectel_eg25_g)), hw_flow_control),
-		   (MODEM_CHAT_SCRIPT_CMD_RESP("AT+IFC=2,2", ok_match),))
-			MODEM_CHAT_SCRIPT_CMD_RESP("AT+CREG=1", ok_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGREG=1", ok_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("AT+CEREG=1", ok_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("AT+CREG?", ok_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("AT+CEREG?", ok_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGREG?", ok_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGSN", imei_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("", ok_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGMM", cgmm_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("", ok_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGMI", cgmi_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("", ok_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGMR", cgmr_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("", ok_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("AT+CIMI", cimi_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP("", ok_match),
-		MODEM_CHAT_SCRIPT_CMD_RESP_NONE("AT+CMUX=0,0," QUECTEL_EG25_G_CMUX_PORT_SPEED
-						",127,10,3,30,10,2",
-						100));
+#if BUS_HAS_FLOW_CONTROL(quectel_eg21_g) || BUS_HAS_FLOW_CONTROL(quectel_eg25_g)
+	MODEM_CHAT_SCRIPT_CMD_RESP("AT+IFC=2,2", ok_match),
+#endif
+	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CREG=1", ok_match),
+	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGREG=1", ok_match),
+	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CEREG=1", ok_match),
+	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CREG?", ok_match),
+	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CEREG?", ok_match),
+	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGREG?", ok_match),
+	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGSN", imei_match), MODEM_CHAT_SCRIPT_CMD_RESP("", ok_match),
+	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGMM", cgmm_match), MODEM_CHAT_SCRIPT_CMD_RESP("", ok_match),
+	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGMI", cgmi_match), MODEM_CHAT_SCRIPT_CMD_RESP("", ok_match),
+	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGMR", cgmr_match), MODEM_CHAT_SCRIPT_CMD_RESP("", ok_match),
+	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CIMI", cimi_match), MODEM_CHAT_SCRIPT_CMD_RESP("", ok_match),
+	MODEM_CHAT_SCRIPT_CMD_RESP_NONE("AT+CMUX=0,0," QUECTEL_EG2X_G_CMUX_PORT_SPEED
+					",127,10,3,30,10,2",
+					100));
 
-MODEM_CHAT_SCRIPT_DEFINE(quectel_eg25_g_init_chat_script, quectel_eg25_g_init_chat_script_cmds,
+MODEM_CHAT_SCRIPT_DEFINE(quectel_eg2x_g_init_chat_script, quectel_eg2x_g_init_chat_script_cmds,
 			 abort_matches, modem_cellular_chat_callback_handler, 10);
 
 #if CONFIG_MODEM_CELLULAR_NEW_BAUDRATE != 115200
 MODEM_CHAT_SCRIPT_CMDS_DEFINE(
-	quectel_eg25_g_set_baudrate_chat_script_cmds, MODEM_CHAT_SCRIPT_CMD_RESP("ATE0", ok_match),
+	quectel_eg2x_g_set_baudrate_chat_script_cmds, MODEM_CHAT_SCRIPT_CMD_RESP("ATE0", ok_match),
 	MODEM_CHAT_SCRIPT_CMD_RESP("AT+IPR=" STRINGIFY(CONFIG_MODEM_CELLULAR_NEW_BAUDRATE),
 						       ok_match));
 
-MODEM_CHAT_SCRIPT_DEFINE(quectel_eg25_g_set_baudrate_chat_script,
-			 quectel_eg25_g_set_baudrate_chat_script_cmds, abort_matches,
+MODEM_CHAT_SCRIPT_DEFINE(quectel_eg2x_g_set_baudrate_chat_script,
+			 quectel_eg2x_g_set_baudrate_chat_script_cmds, abort_matches,
 			 modem_cellular_chat_callback_handler, 10);
 #endif
 
-MODEM_CHAT_SCRIPT_CMDS_DEFINE(quectel_eg25_g_dial_chat_script_cmds,
+MODEM_CHAT_SCRIPT_CMDS_DEFINE(quectel_eg2x_g_dial_chat_script_cmds,
 			      MODEM_CHAT_SCRIPT_CMD_RESP_MULT("AT+CGACT=0,1", allow_match),
 			      MODEM_CHAT_SCRIPT_CMD_RESP("AT+CFUN=1", ok_match),
 			      MODEM_CHAT_SCRIPT_CMD_RESP("ATD*99***1#", connect_match));
 
-MODEM_CHAT_SCRIPT_DEFINE(quectel_eg25_g_dial_chat_script, quectel_eg25_g_dial_chat_script_cmds,
+MODEM_CHAT_SCRIPT_DEFINE(quectel_eg2x_g_dial_chat_script, quectel_eg2x_g_dial_chat_script_cmds,
 			 dial_abort_matches, modem_cellular_chat_callback_handler, 10);
 
-MODEM_CHAT_SCRIPT_CMDS_DEFINE(quectel_eg25_g_periodic_chat_script_cmds,
+MODEM_CHAT_SCRIPT_CMDS_DEFINE(quectel_eg2x_g_periodic_chat_script_cmds,
 			      MODEM_CHAT_SCRIPT_CMD_RESP("AT+CREG?", ok_match),
 			      MODEM_CHAT_SCRIPT_CMD_RESP("AT+CEREG?", ok_match),
 			      MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGREG?", ok_match),
 			      MODEM_CHAT_SCRIPT_CMD_RESP("AT+CSQ", csq_match),
 			      MODEM_CHAT_SCRIPT_CMD_RESP("AT+QENG=\"servingcell\"", ok_match));
 
-MODEM_CHAT_SCRIPT_DEFINE(quectel_eg25_g_periodic_chat_script,
-			 quectel_eg25_g_periodic_chat_script_cmds, abort_matches,
+MODEM_CHAT_SCRIPT_DEFINE(quectel_eg2x_g_periodic_chat_script,
+			 quectel_eg2x_g_periodic_chat_script_cmds, abort_matches,
 			 modem_cellular_chat_callback_handler, 4);
 
 /*
@@ -129,7 +126,7 @@ enum {
 	QENG_LTE_MIN_ARGC = 16,
 };
 
-static void quectel_eg25_g_on_qeng(struct modem_chat *chat, char **argv, uint16_t argc,
+static void quectel_eg2x_g_on_qeng(struct modem_chat *chat, char **argv, uint16_t argc,
 				   void *user_data)
 {
 	struct modem_cellular_data *data = (struct modem_cellular_data *)user_data;
@@ -160,19 +157,19 @@ static void quectel_eg25_g_on_qeng(struct modem_chat *chat, char **argv, uint16_
 	modem_cellular_emit_network_status(data, &evt);
 }
 
-static const struct modem_cellular_vendor_config quectel_eg25_g_vendor = {
+static const struct modem_cellular_vendor_config quectel_eg2x_g_vendor = {
 	/* clang-format off */
 	.scripts = {
 #if CONFIG_MODEM_CELLULAR_NEW_BAUDRATE != 115200
-		.set_baudrate = &quectel_eg25_g_set_baudrate_chat_script,
+		.set_baudrate = &quectel_eg2x_g_set_baudrate_chat_script,
 #endif
-		.init = &quectel_eg25_g_init_chat_script,
-		.dial = &quectel_eg25_g_dial_chat_script,
-		.periodic = &quectel_eg25_g_periodic_chat_script,
+		.init = &quectel_eg2x_g_init_chat_script,
+		.dial = &quectel_eg2x_g_dial_chat_script,
+		.periodic = &quectel_eg2x_g_periodic_chat_script,
 	},
 	.unsol_matches = {
-		.matches = quectel_eg25_g_unsol,
-		.size = ARRAY_SIZE(quectel_eg25_g_unsol),
+		.matches = quectel_eg2x_g_unsol,
+		.size = ARRAY_SIZE(quectel_eg2x_g_unsol),
 	},
 	/* clang-format on */
 	.chat_delimiter = "\r",
@@ -183,13 +180,19 @@ static const struct modem_cellular_vendor_config quectel_eg25_g_vendor = {
 	.shutdown_time_ms = 5000,
 };
 
-#define MODEM_CELLULAR_DEVICE_QUECTEL_EG25_G(inst)                                                 \
+#define MODEM_CELLULAR_DEVICE_QUECTEL_EG2X_G(inst)                                                 \
 	MODEM_DT_INST_PPP_DEFINE(inst, MODEM_CELLULAR_INST_NAME(ppp, inst), NULL, 1500, 64);       \
                                                                                                    \
 	static struct modem_cellular_data MODEM_CELLULAR_INST_NAME(data, inst);                    \
                                                                                                    \
 	MODEM_CELLULAR_DEFINE_AND_INIT_USER_PIPES(inst, (user_pipe_0, 3), (user_pipe_1, 4))        \
                                                                                                    \
-	MODEM_CELLULAR_DEFINE_INSTANCE(inst, &quectel_eg25_g_vendor)
+	MODEM_CELLULAR_DEFINE_INSTANCE(inst, &quectel_eg2x_g_vendor)
 
-DT_INST_FOREACH_STATUS_OKAY(MODEM_CELLULAR_DEVICE_QUECTEL_EG25_G)
+#define DT_DRV_COMPAT quectel_eg21_g
+DT_INST_FOREACH_STATUS_OKAY(MODEM_CELLULAR_DEVICE_QUECTEL_EG2X_G)
+#undef DT_DRV_COMPAT
+
+#define DT_DRV_COMPAT quectel_eg25_g
+DT_INST_FOREACH_STATUS_OKAY(MODEM_CELLULAR_DEVICE_QUECTEL_EG2X_G)
+#undef DT_DRV_COMPAT
