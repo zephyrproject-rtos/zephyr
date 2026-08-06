bb485e28fe774bc0a3479176f5af6f1a9a1a57ff
Michal Frankiewicz <michal.frankiewicz@nordicsemi.no>
2026-08-06 11:02:03 +0200

    dts: Added split clocks to dts.
    Added bindings and dts nodes for split clocks.
    
    Signed-off-by: Michal Frankiewicz <michal.frankiewicz@nordicsemi.no>

--- boards/nordic/thingy52/pre_dt_board.cmake
+++ boards/nordic/thingy52/pre_dt_board.cmake
@@ -2,6 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Suppress "unique_unit_address_if_enabled" to handle the following overlaps:
-# - power@40000000 & clock@40000000 & bprot@40000000
+# - power@40000000 & clock@40000000 & hfclk@40000000 & lfclk@40000000 & bprot@40000000
 # - acl@4001e000 & flash-controller@4001e000
 list(APPEND EXTRA_DTC_FLAGS "-Wno-unique_unit_address_if_enabled")
