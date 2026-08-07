75fd032dcf4749c5041795c1f91884475078d121
Måns Ansgariusson <mansgariusson@gmail.com>
2026-08-07 13:15:18 +0200

    sys: assert: remove the experimental CONFIG_ASSERT_CUSTOM_HEADER
    CONFIG_ASSERT_CUSTOM_HEADER let an application inject a
    "zephyr_custom_assert.h" at the end of <zephyr/sys/__assert.h> to extend
    the assert macros at the preprocessor level. It has always been
    experimental and explicitly warned that the internal assert API may change,
    so it is removed without a deprecation period, per the experimental API
    policy.
    
    Applications that need to customize assertion handling should override the
    weak assert hooks instead.
    
    Remove the Kconfig option, the __assert.h include hook and the
    assert_custom_header test.
    
    Signed-off-by: Måns Ansgariusson <mansgariusson@gmail.com>

--- include/zephyr/sys/__assert.h
+++ include/zephyr/sys/__assert.h
@@ -129,9 +129,4 @@ void assert_post_action(const char *file, unsigned int line);
 
 #endif
 
-#ifdef CONFIG_ASSERT_CUSTOM_HEADER
-/* This include must always be at the end of __assert.h */
-#include <zephyr_custom_assert.h>
-#endif
-
 #endif /* ZEPHYR_INCLUDE_SYS___ASSERT_H_ */
