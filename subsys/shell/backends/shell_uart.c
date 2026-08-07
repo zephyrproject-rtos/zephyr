aa35e91a9d19be50798d904fdbccc79a2dcd9f1d
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2026-08-07 13:45:05 +0200

    shell: backends: uart: Add support for uninit for asynchronous API
    Add option to stop shell with UART backend. Added flag which is used
    to not re-enable RX after it is disabled.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- subsys/shell/backends/shell_uart.c
+++ subsys/shell/backends/shell_uart.c
@@ -79,21 +79,20 @@ static void async_callback(const struct device *dev, struct uart_event *evt, voi
 		uart_async_rx_on_buf_rel(&sh_uart->async_rx, evt->data.rx_buf.buf);
 		break;
 	case  UART_RX_DISABLED:
-	{
-		uint8_t *buf = uart_async_rx_buf_req(&sh_uart->async_rx);
-		size_t len;
-		int err;
-
 		sh_uart->rx_enabled = false;
-		if (buf) {
-			len = uart_async_rx_get_buf_len(&sh_uart->async_rx);
-			err = rx_enable(dev, sh_uart, buf, len);
-			(void)err;
-			__ASSERT_NO_MSG(err == 0);
-		}
+		if (sh_uart->shell_enabled) {
+			uint8_t *buf = uart_async_rx_buf_req(&sh_uart->async_rx);
+			size_t len;
+			int err;
 
+			if (buf) {
+				len = uart_async_rx_get_buf_len(&sh_uart->async_rx);
+				err = rx_enable(dev, sh_uart, buf, len);
+				(void)err;
+				__ASSERT_NO_MSG(err == 0);
+			}
+		}
 		break;
-	}
 	default:
 		break;
 	};
@@ -272,18 +271,19 @@ static void async_init(struct shell_uart_async *sh_uart)
 	k_sem_init(&sh_uart->tx_sem, 0, 1);
 
 	err = uart_async_rx_init(async_rx, &sh_uart->async_rx_config);
-	(void)err;
 	__ASSERT_NO_MSG(err == 0);
 
 	uint8_t *buf = uart_async_rx_buf_req(async_rx);
 
 	err = uart_callback_set(dev, async_callback, (void *)sh_uart);
-	(void)err;
 	__ASSERT_NO_MSG(err == 0);
 
 	err = rx_enable(dev, sh_uart, buf, uart_async_rx_get_buf_len(async_rx));
-	(void)err;
 	__ASSERT_NO_MSG(err == 0);
+
+	if (err == 0) {
+		sh_uart->shell_enabled = true;
+	}
 }
 
 static void polling_rx_timeout_handler(struct k_timer *timer)
@@ -353,9 +353,20 @@ static void irq_uninit(struct shell_uart_int_driven *sh_uart)
 
 static void async_uninit(struct shell_uart_async *sh_uart)
 {
+	__ASSERT_NO_MSG(!k_is_in_isr());
 	const struct device *dev = sh_uart->common.dev;
+	int ret;
 
-	(void)uart_rx_disable(dev);
+	sh_uart->shell_enabled = false;
+
+	(void)uart_tx_abort(dev);
+	ret = uart_rx_disable(dev);
+	if (ret == 0) {
+		/* Wait for RX to be disabled */
+		while (sh_uart->rx_enabled) {
+			k_sleep(K_MSEC(1));
+		}
+	}
 }
 
 static void polling_uninit(struct shell_uart_polling *sh_uart)
@@ -612,9 +623,7 @@ static int enable_shell_uart(void)
 		smp_shell_init();
 	}
 
-	shell_init(&shell_uart, dev, cfg_flags, log_backend, level);
-
-	return 0;
+	return shell_init(&shell_uart, dev, cfg_flags, log_backend, level);
 }
 
 SYS_INIT(enable_shell_uart, POST_KERNEL,
