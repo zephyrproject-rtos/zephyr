d5d2b9d2ddba03863f698a52d213628ba6ec928c
Emil Gydesen <emil.gydesen@nordicsemi.no>
2026-08-10 16:39:36 +0200

    Bluetooth: CAP: Shell: Refactor cap_unicast_ac_param
    Refactor cap_unicast_ac_param to be more expressive.
    This simplifies some setup while moving some of the choices
    to the parameters instead.
    Additionally this allows for AC_7_1 which was not supported by
    the existing definition.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/audio/shell/audio.h
+++ subsys/bluetooth/audio/shell/audio.h
@@ -252,13 +252,22 @@ extern struct shell_stream unicast_streams[CONFIG_BT_MAX_CONN * MAX(UNICAST_SERV
 
 #if defined(CONFIG_BT_BAP_UNICAST_CLIENT)
 
+struct cap_unicast_ac_cis_param {
+	bool has_snk;
+	bool has_src;
+	enum bt_audio_location snk_loc;
+	enum bt_audio_location src_loc;
+};
+
+struct cap_unicast_ac_conn_param {
+	size_t cis_cnt;
+	struct cap_unicast_ac_cis_param cis_param[BAP_UNICAST_AC_MAX_PAIR];
+};
+
 struct cap_unicast_ac_param {
 	char *name;
 	size_t conn_cnt;
-	size_t snk_cnt[BAP_UNICAST_AC_MAX_CONN];
-	size_t src_cnt[BAP_UNICAST_AC_MAX_CONN];
-	size_t snk_chan_cnt;
-	size_t src_chan_cnt;
+	struct cap_unicast_ac_conn_param conn_param[BAP_UNICAST_AC_MAX_CONN];
 };
 
 extern struct unicast_group default_unicast_group;
