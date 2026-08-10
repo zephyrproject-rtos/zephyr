2247cc61ab77ce1abf9e0b5ea8f6a9738adc820b
Etienne Carriere <etienne.carriere@st.com>
2026-08-10 15:51:49 +0200

    drivers: i2c: stm32: Fix stalled semaphore on bus recovery
    Correct the STM32 I2C bus recovery sequence in the non-RTIO driver
    where bus semaphore (acting as a mutex) was taken twice before this
    change. Fix that by taking the semaphore outside of the
    i2c_stm32_runtime_configure() functions.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- drivers/i2c/i2c_stm32_v2.c
+++ drivers/i2c/i2c_stm32_v2.c
@@ -508,7 +508,10 @@ int i2c_stm32_target_register(const struct device *dev,
 
 	bitrate_cfg = i2c_map_dt_bitrate(cfg->bitrate);
 
+	k_sem_take(&data->bus_mutex, K_FOREVER);
 	ret = i2c_stm32_runtime_configure(dev, bitrate_cfg);
+	k_sem_give(&data->bus_mutex);
+
 	if (ret < 0) {
 		LOG_ERR("i2c: failure initializing");
 		return ret;
