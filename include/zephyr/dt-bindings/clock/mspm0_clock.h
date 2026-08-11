6e1844b60d255a4f89dbe65c8409f3b5325bdbd6
Amneesh Singh <amneesh@ti.com>
2026-08-11 14:57:51 +0530

    drivers: clock_control: mspm0: switch SYSPLL config to native SYSCTL
    - Replace SYSPLL HAL configuration with native register I/O via configure()
      API
    - Configure dividers once during initialization
    - Copy factory-programmed startup parameters from the factoryregion based
      on the reference frequency at runtime instead of the currently hardcoded
      32-48MHz range.
    - Add SYSPLL on()/off() API support
    - Add SYSPLL get_status() API support
    - Implement get_rate() for all 3 SYSPLL output clocks, which calculate the
      output frequency based on the configured dividers and the input source.
    - Introduce raw polling loops for checking clock status since using systick
      timer to wait for n-cycles may cause the core to reset when clocks are
      being configured/switched.
    
    Signed-off-by: Amneesh Singh <amneesh@ti.com>

--- include/zephyr/dt-bindings/clock/mspm0_clock.h
+++ include/zephyr/dt-bindings/clock/mspm0_clock.h
@@ -11,19 +11,27 @@
 #define MSPM0_CLOCK(clk, bit) ((clk << 8) | bit)
 
 /* Peripheral clock source selection register mask */
-#define MSPM0_CLOCK_PERIPH_REG_MASK(X)	(X & 0xFF)
+#define MSPM0_CLOCK_PERIPH_REG_MASK(X) (X & 0xFF)
 
 /* Clock references */
-#define MSPM0_CLOCK_SYSOSC		MSPM0_CLOCK(0x0, 0x0)
-#define MSPM0_CLOCK_LFCLK		MSPM0_CLOCK(0x1, 0x2)
-#define MSPM0_CLOCK_MFCLK		MSPM0_CLOCK(0x2, 0x4)
-#define MSPM0_CLOCK_BUSCLK		MSPM0_CLOCK(0x3, 0x8)
-#define MSPM0_CLOCK_ULPCLK		MSPM0_CLOCK(0x4, 0x8)
-#define MSPM0_CLOCK_MCLK		MSPM0_CLOCK(0x5, 0x8)
-#define MSPM0_CLOCK_MFPCLK		MSPM0_CLOCK(0x6, 0x0)
-#define MSPM0_CLOCK_CANCLK		MSPM0_CLOCK(0x7, 0x0)
-#define MSPM0_CLOCK_CLK_OUT		MSPM0_CLOCK(0x8, 0x0)
+#define MSPM0_CLOCK_SYSOSC       MSPM0_CLOCK(0x0, 0x0)
+#define MSPM0_CLOCK_LFCLK        MSPM0_CLOCK(0x1, 0x2)
+#define MSPM0_CLOCK_MFCLK        MSPM0_CLOCK(0x2, 0x4)
+#define MSPM0_CLOCK_BUSCLK       MSPM0_CLOCK(0x3, 0x8)
+#define MSPM0_CLOCK_ULPCLK       MSPM0_CLOCK(0x4, 0x8)
+#define MSPM0_CLOCK_MCLK         MSPM0_CLOCK(0x5, 0x8)
+#define MSPM0_CLOCK_MFPCLK       MSPM0_CLOCK(0x6, 0x0)
+#define MSPM0_CLOCK_CANCLK       MSPM0_CLOCK(0x7, 0x0)
+#define MSPM0_CLOCK_CLK_OUT      MSPM0_CLOCK(0x8, 0x0)
 /** @brief High frequency clock reference */
-#define MSPM0_CLOCK_HFCLK		MSPM0_CLOCK(0x9, 0x2)
+#define MSPM0_CLOCK_HFCLK        MSPM0_CLOCK(0x9, 0x2)
+/** @brief System PLL block (reference mux, on/off, status) */
+#define MSPM0_CLOCK_SYSPLL       MSPM0_CLOCK(0xB, 0x0)
+/** @brief System PLL CLK0 output */
+#define MSPM0_CLOCK_SYSPLL_CLK0  MSPM0_CLOCK(0xC, 0x0)
+/** @brief System PLL CLK1 output */
+#define MSPM0_CLOCK_SYSPLL_CLK1  MSPM0_CLOCK(0xD, 0x0)
+/** @brief System PLL CLK2X output */
+#define MSPM0_CLOCK_SYSPLL_CLK2X MSPM0_CLOCK(0xE, 0x0)
 
 #endif
