e6ec105a4f5795e523c7764718d7afe09372192a
Etienne Carriere <etienne.carriere@st.com>
2026-08-11 18:48:02 +0200

    drivers: entropy: psa_crypto: Move local functions definition
    Move entropy_psa_crypto_rng_get_entropy() definition above
    entropy_psa_crypto_rng_init() definition in entropy_psa_crypto driver
    to ease integration of .get_entropy_irs handler.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- drivers/entropy/entropy_psa_crypto.c
+++ drivers/entropy/entropy_psa_crypto.c
@@ -9,21 +9,6 @@
 #include <zephyr/drivers/entropy.h>
 #include <psa/crypto.h>
 
-/* API implementation: PSA Crypto initialization */
-static int entropy_psa_crypto_rng_init(const struct device *dev)
-{
-	psa_status_t status = PSA_ERROR_CORRUPTION_DETECTED;
-
-	ARG_UNUSED(dev);
-
-	status = psa_crypto_init();
-	if (status != PSA_SUCCESS) {
-		return -EIO;
-	}
-
-	return 0;
-}
-
 /* API implementation: get_entropy */
 static int entropy_psa_crypto_rng_get_entropy(const struct device *dev,
 					      uint8_t *buffer, uint16_t length)
@@ -33,6 +18,21 @@ static int entropy_psa_crypto_rng_get_entropy(const struct device *dev,
 	ARG_UNUSED(dev);
 
 	status = psa_generate_random(buffer, length);
+	if (status != PSA_SUCCESS) {
+		return -EIO;
+	}
+
+	return 0;
+}
+
+/* API implementation: PSA Crypto initialization */
+static int entropy_psa_crypto_rng_init(const struct device *dev)
+{
+	psa_status_t status = PSA_ERROR_CORRUPTION_DETECTED;
+
+	ARG_UNUSED(dev);
+
+	status = psa_crypto_init();
 	if (status != PSA_SUCCESS) {
 		return -EIO;
 	}
