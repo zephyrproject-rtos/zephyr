5110ed0782fd2e8a3af8db7037c29596c2bc4886
Etienne Carriere <etienne.carriere@st.com>
2026-08-11 18:49:08 +0200

    drivers: entropy: psa_crypto: Get entropy from an interrupt context
    Implement .get_entropy_isr handler in entropy_psa_crypto driver using
    a random bytes pool filled from a workqueue whose bytes can be consumed
    from a interrupt handler.
    
    Configuration set the pool size, the threshold to trigger its refilling
    and the size of the buffer chunk use to refill it.
    
    This added support is useful for example when the entropy PSA Crypto
    service is implemented in TF-M which has re-entrance constraints
    preventing reliable calls from a Zephyr interrupt context, notably
    for example, considering a wireless stack execution constraints.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- drivers/entropy/entropy_psa_crypto.c
+++ drivers/entropy/entropy_psa_crypto.c
@@ -7,7 +7,33 @@
 #define DT_DRV_COMPAT zephyr_psa_crypto_rng
 
 #include <zephyr/drivers/entropy.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/ring_buffer.h>
+#include <zephyr/sys/minmax.h>
 #include <psa/crypto.h>
+
+#ifdef CONFIG_ENTROPY_PSA_CRYPTO_RNG_ISR
+#define ISR_BUFFER_SIZE		CONFIG_ENTROPY_PSA_CRYPTO_RNG_ISR_BUFFER_SIZE
+#define ISR_REFILL_SIZE		CONFIG_ENTROPY_PSA_CRYPTO_RNG_ISR_REFILL_SIZE
+#define ISR_REFILL_THRESHOLD	CONFIG_ENTROPY_PSA_CRYPTO_RNG_ISR_REFILL_THRESHOLD
+#else
+#define ISR_BUFFER_SIZE		1 /* Dummy value to keep compiler happy */
+#define ISR_REFILL_SIZE		0 /* Dummy value to keep compiler happy */
+#define ISR_REFILL_THRESHOLD	0 /* Dummy value to keep compiler happy */
+#endif /* CONFIG_ENTROPY_PSA_CRYPTO_RNG_ISR */
+
+LOG_MODULE_REGISTER(entropy_psa_crypto, CONFIG_ENTROPY_LOG_LEVEL);
+
+struct entropy_psa_crypto_context {
+	struct ring_buf isr_rbuf;
+	struct k_spinlock isr_lock;
+	struct k_work isr_refill_work;
+};
+
+/* Context used only when CONFIG_ENTROPY_PSA_CRYPTO_RNG_ISR is enabled */
+static struct entropy_psa_crypto_context entropy_psa_crypto_ctx;
+static uint8_t __noinit entropy_psa_crypto_isr_pool[ISR_BUFFER_SIZE];
 
 /* API implementation: get_entropy */
 static int entropy_psa_crypto_rng_get_entropy(const struct device *dev,
@@ -19,6 +45,132 @@ static int entropy_psa_crypto_rng_get_entropy(const struct device *dev,
 
 	status = psa_generate_random(buffer, length);
 	if (status != PSA_SUCCESS) {
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static int __maybe_unused entropy_psa_crypto_rng_get_entropy_isr(const struct device *dev,
+								 uint8_t *buffer, uint16_t length,
+								 uint32_t flags)
+{
+	struct entropy_psa_crypto_context *ctx = &entropy_psa_crypto_ctx;
+	k_spinlock_key_t key;
+	uint32_t rand_size;
+	bool refill;
+	int ret;
+
+	if (unlikely((flags & ENTROPY_BUSYWAIT) != 0U)) {
+		LOG_WRN_ONCE("ENTROPY_BUSYWAIT is ignored");
+	}
+
+	key = k_spin_lock(&ctx->isr_lock);
+
+	rand_size = ring_buf_get(&ctx->isr_rbuf, buffer,
+				 min(ring_buf_size_get(&ctx->isr_rbuf), length));
+
+	refill = (ring_buf_size_get(&ctx->isr_rbuf) <= ISR_REFILL_THRESHOLD);
+
+	k_spin_unlock(&ctx->isr_lock, key);
+
+	if (refill) {
+		ret = k_work_submit(&ctx->isr_refill_work);
+		if ((ret < 0) && (ret != -ENODEV)) {
+			LOG_ERR("Failed to launch RNG pool refill: %d", ret);
+		}
+	}
+
+	if ((rand_size < length) && (flags & ENTROPY_BUSYWAIT)) {
+		LOG_WRN_RATELIMIT("ISR random bytes underflow: %u/%u", rand_size, length);
+	}
+
+	return (int)rand_size;
+}
+
+static void entropy_psa_crypto_isr_refill_work_fn(struct k_work *work)
+{
+	struct entropy_psa_crypto_context *ctx = &entropy_psa_crypto_ctx;
+	__maybe_unused size_t total = 0;
+	k_spinlock_key_t key;
+	bool done = false;
+	uint32_t size;
+	uint8_t *ptr;
+
+	/* Get random byte per small chunks to lower latency to TF-M services */
+	do {
+		key = k_spin_lock(&ctx->isr_lock);
+
+		if (ISR_REFILL_SIZE == 0) {
+			size = UINT32_MAX;
+		} else {
+			size = ISR_REFILL_SIZE;
+		}
+
+		size = ring_buf_put_claim(&ctx->isr_rbuf, &ptr, size);
+
+		k_spin_unlock(&ctx->isr_lock, key);
+
+		if (psa_generate_random(ptr, size) != PSA_SUCCESS) {
+			(void)ring_buf_put_finish(&ctx->isr_rbuf, 0);
+			LOG_ERR("psa_generate_random() failed");
+			break;
+		}
+
+		key = k_spin_lock(&ctx->isr_lock);
+
+		if (unlikely(ring_buf_put_finish(&ctx->isr_rbuf, size) < 0)) {
+			LOG_ERR("Failed to finish ring buffer update");
+			done = true;
+		} else {
+			total += size;
+			done = (ring_buf_space_get(&ctx->isr_rbuf) == 0);
+		}
+
+		k_spin_unlock(&ctx->isr_lock, key);
+	} while (!done);
+
+	LOG_DBG("Refilled %zu bytes", total);
+}
+
+static int __maybe_unused entropy_psa_crypto_init_refill_isr_pool(void)
+{
+	int ret;
+
+	/* Refill the pool in case it was used prio system work was started */
+	ret = k_work_submit(&entropy_psa_crypto_ctx.isr_refill_work);
+	if (ret < 0) {
+		LOG_ERR("Failed to launch RNG pool refill: %d", ret);
+	}
+
+	return ret;
+}
+
+#ifdef CONFIG_ENTROPY_PSA_CRYPTO_RNG_ISR
+SYS_INIT(entropy_psa_crypto_init_refill_isr_pool, POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+#endif /* CONFIG_ENTROPY_PSA_CRYPTO_RNG_ISR */
+
+static int entropy_psa_crypto_init_isr_pool(void)
+{
+	struct entropy_psa_crypto_context *ctx = &entropy_psa_crypto_ctx;
+	k_spinlock_key_t key;
+	uint32_t filled_size;
+
+	ring_buf_init(&ctx->isr_rbuf, sizeof(entropy_psa_crypto_isr_pool),
+		      entropy_psa_crypto_isr_pool);
+
+	/* Start with a fully filled pool of random bytes */
+	entropy_psa_crypto_isr_refill_work_fn(&ctx->isr_refill_work);
+
+	key = k_spin_lock(&ctx->isr_lock);
+	filled_size = ring_buf_size_get(&ctx->isr_rbuf);
+	k_spin_unlock(&ctx->isr_lock, key);
+
+	k_work_init(&ctx->isr_refill_work, entropy_psa_crypto_isr_refill_work_fn);
+
+	if (filled_size < ISR_BUFFER_SIZE) {
+		LOG_ERR("Failed to fully fill the ISR pool: %u/%u bytes",
+			filled_size, ISR_BUFFER_SIZE);
 		return -EIO;
 	}
 
@@ -37,12 +189,19 @@ static int entropy_psa_crypto_rng_init(const struct device *dev)
 		return -EIO;
 	}
 
+	if (IS_ENABLED(CONFIG_ENTROPY_PSA_CRYPTO_RNG_ISR)) {
+		return entropy_psa_crypto_init_isr_pool();
+	}
+
 	return 0;
 }
 
 /* Entropy driver APIs structure */
 static DEVICE_API(entropy, entropy_psa_crypto_rng_api) = {
 	.get_entropy = entropy_psa_crypto_rng_get_entropy,
+#ifdef CONFIG_ENTROPY_PSA_CRYPTO_RNG_ISR
+	.get_entropy_isr = entropy_psa_crypto_rng_get_entropy_isr,
+#endif /* CONFIG_ENTROPY_PSA_CRYPTO_RNG_ISR */
 };
 
 /* Entropy driver registration */
