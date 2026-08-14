da9ee04f4d048d866c4ace2dec16fef232f210a6
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2026-08-14 14:51:09 +0200

    tests: ipc: ipc_service_api: Align configuration
    Align test configuration after adding the default IPC service backend
    setup to nrf54l15 and flpr,ppr on nrf54h20. nrf54l15dk is now using
    nordic-flpr snippet for the test instead of custom IPC configuration in
    the overlay.
    
    Added option to detect if there is only a single IPC device. It is used
    if detected and test specific alias is not mandatory then.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- tests/subsys/ipc/ipc_service_api/src/main.c
+++ tests/subsys/ipc/ipc_service_api/src/main.c
@@ -24,7 +24,19 @@ LOG_MODULE_REGISTER(ipc_service_api, LOG_LEVEL_INF);
 static struct test_context test_ctx[TEST_EP_COUNT];
 static const char *ep_name[TEST_EP_COUNT] = {"ep0", "ep1"};
 
-static const struct device *ipc_instance = DEVICE_DT_GET(DT_ALIAS(dut_ipc));
+#if DT_HAS_ALIAS(dut_ipc)
+#define IPC_NODE DT_ALIAS(dut_ipc)
+#elif DT_NUM_INST_STATUS_OKAY(zephyr_ipc_icbmsg) == 1
+#define IPC_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_ipc_icbmsg)
+#elif DT_NUM_INST_STATUS_OKAY(zephyr_ipc_icmsg) == 1
+#define IPC_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_ipc_icmsg)
+#elif DT_NUM_INST_STATUS_OKAY(zephyr_ipc_openamp_static_vrings) == 1
+#define IPC_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_ipc_openamp_static_vrings)
+#else
+#error "No IPC node found"
+#endif
+
+static const struct device *ipc_instance = DEVICE_DT_GET(IPC_NODE);
 
 static void (*recv_override)(const void *data, size_t len, struct test_context *ctx);
 
