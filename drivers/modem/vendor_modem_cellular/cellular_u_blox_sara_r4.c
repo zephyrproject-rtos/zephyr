3e7653080823b5df5472d0f3aa9585c2fcfbdf87
Konstantinos Papadopoulos <kostas.papadopulos@gmail.com>
2026-08-16 18:29:23 +0300

    modem: cellular: vendor: sara-r4 enable GNSS operation
    Enable GNSS operation earlier during initialization to get faster fix.
    Removed user pipe 4 as there is no such DLCI channel in R4 variant.
    
    Signed-off-by: Konstantinos Papadopoulos <kostas.papadopulos@gmail.com>

--- drivers/modem/vendor_modem_cellular/cellular_u_blox_sara_r4.c
+++ drivers/modem/vendor_modem_cellular/cellular_u_blox_sara_r4.c
@@ -35,6 +35,9 @@ MODEM_CHAT_SCRIPT_CMDS_DEFINE(
 	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CREG?", ok_match),
 	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CEREG?", ok_match),
 	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGREG?", ok_match),
+	IF_ENABLED(CONFIG_GNSS, (
+	MODEM_CHAT_SCRIPT_CMD_RESP("AT+UGPRF=2", ok_match),
+	MODEM_CHAT_SCRIPT_CMD_RESP("AT+UGPS=1,0,15", ok_match),))
 	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGSN", imei_match),
 	MODEM_CHAT_SCRIPT_CMD_RESP("", ok_match),
 	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CGMM", cgmm_match),
@@ -97,7 +100,7 @@ static const struct modem_cellular_vendor_config u_blox_sara_r4_vendor = {
 		.ppp = &MODEM_CELLULAR_INST_NAME(ppp, inst),                                       \
 	};                                                                                         \
                                                                                                    \
-	MODEM_CELLULAR_DEFINE_AND_INIT_USER_PIPES(inst, (gnss_pipe, 3), (user_pipe_0, 4))          \
+	MODEM_CELLULAR_DEFINE_AND_INIT_USER_PIPES(inst, (gnss_pipe, 3))                            \
                                                                                                    \
 	MODEM_CELLULAR_DEFINE_INSTANCE(inst, &u_blox_sara_r4_vendor)
 
