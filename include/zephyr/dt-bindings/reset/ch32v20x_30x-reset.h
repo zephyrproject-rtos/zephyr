a36a77e9b8d9c0436727acc0d86112e9695db65f
Fiona Behrens <me@kloenk.dev>
2026-08-18 15:56:51 +0000

    drivers: reset: add ch32 reset controller
    Add reset driver for the reset part of the ch32 rcc.
    
    Use this new reset driven in `wch,usart` to reset the usart peripheral
    before use.
    
    Signed-off-by: Fiona Behrens <me@kloenk.dev>

---
+++ include/zephyr/dt-bindings/reset/ch32v20x_30x-reset.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2026 Fiona Behrens
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief CH32 reset controller devicetree helper macros for CH32V20X-V30X.
+ * @ingroup reset_controller_ch32
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_RESET_CH32V20X_30X_RESET_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_RESET_CH32V20X_30X_RESET_H_
+
+#include "ch32-common.h"
+
+/** @cond INTERNAL_HIDDEN */
+
+/* RCC bus reset register offset */
+#define CH32_RESET_BUS_APB2 0x0C
+#define CH32_RESET_BUS_APB1 0x10
+#define CH32_RESET_BUS_AHB  0x28
+
+/** @endcond */
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_RESET_CH32V20X_30X_RESET_H_ */
