48b8bb17030c9cca426bde6029001d73d4c63891
Daniel DeGrasse <daniel.degrasse@analog.com>
2026-08-18 18:10:19 +0000

    tests: drivers: clock_management_minimal: update test for new rework
    Update clock_management_minimal test for new clock management rework
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@analog.com>

--- tests/drivers/clock_management/clock_management_minimal/src/test_clock_management_minimal.c
+++ tests/drivers/clock_management/clock_management_minimal/src/test_clock_management_minimal.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2024 Tenstorrent AI ULC
+ * Copyright (c) 2026 Analog Devices, Inc
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -9,23 +10,21 @@
 
 LOG_MODULE_REGISTER(test);
 
-/* Define clock management outputs for both states */
-CLOCK_MANAGEMENT_DT_DEFINE_OUTPUT_BY_NAME(DT_NODELABEL(emul_dev1), slow);
-CLOCK_MANAGEMENT_DT_DEFINE_OUTPUT_BY_NAME(DT_NODELABEL(emul_dev1), fast);
+/* Define clock management data */
+CLOCK_MANAGEMENT_DT_DEFINE(DT_NODELABEL(emul_dev1));
+static const struct clock_management_data *data =
+	CLOCK_MANAGEMENT_DT_GET(DT_NODELABEL(emul_dev1));
 
 /* Get references to each clock management output and state */
-static const struct clock_output *dev1_slow =
+clock_output_t dev1_slow =
 	CLOCK_MANAGEMENT_DT_GET_OUTPUT_BY_NAME(DT_NODELABEL(emul_dev1), slow);
-static const struct clock_output *dev1_fast =
+clock_output_t dev1_fast =
 	CLOCK_MANAGEMENT_DT_GET_OUTPUT_BY_NAME(DT_NODELABEL(emul_dev1), fast);
-static clock_management_state_t dev1_slow_default =
-	CLOCK_MANAGEMENT_DT_GET_STATE(DT_NODELABEL(emul_dev1), slow, default);
-static clock_management_state_t dev1_fast_default =
-	CLOCK_MANAGEMENT_DT_GET_STATE(DT_NODELABEL(emul_dev1), fast, default);
-static clock_management_state_t dev1_slow_sleep =
-	CLOCK_MANAGEMENT_DT_GET_STATE(DT_NODELABEL(emul_dev1), slow, sleep);
-static clock_management_state_t dev1_fast_sleep =
-	CLOCK_MANAGEMENT_DT_GET_STATE(DT_NODELABEL(emul_dev1), fast, sleep);
+clock_request_t default_request =
+	CLOCK_MANAGEMENT_DT_GET_REQUEST(DT_NODELABEL(emul_dev1), default);
+clock_request_t sleep_request =
+	CLOCK_MANAGEMENT_DT_GET_REQUEST(DT_NODELABEL(emul_dev1), sleep);
+
 
 /* Runs before every test, resets clocks to default state */
 void reset_clock_states(void *unused)
@@ -34,10 +33,12 @@ void reset_clock_states(void *unused)
 	int ret;
 
 	/* Reset clock tree to default state */
-	ret = clock_management_apply_state(dev1_slow, dev1_slow_default);
+	ret = clock_management_request_state(data, default_request);
+	zassert_equal(ret, 0, "Failed to apply default clock management request");
+	ret = clock_management_get_rate(data, dev1_slow);
 	zassert_equal(ret, DT_PROP(DT_NODELABEL(emul_dev1), slow_default_freq),
 		      "Failed to apply default clock management state for slow clock");
-	ret = clock_management_apply_state(dev1_fast, dev1_fast_default);
+	ret = clock_management_get_rate(data, dev1_fast);
 	zassert_equal(ret, DT_PROP(DT_NODELABEL(emul_dev1), fast_default_freq),
 		      "Failed to apply default clock management state for fast clock");
 }
@@ -51,20 +52,16 @@ ZTEST(clock_management_minimal, test_default_states)
 	/* Apply default clock states for both clock outputs, make sure
 	 * that rates match what is expected
 	 */
-	TC_PRINT("Applying default clock states\n");
+	TC_PRINT("Requesting default clock state\n");
 
-	ret = clock_management_apply_state(dev1_slow, dev1_slow_default);
-	zassert_equal(ret, slow_default,
-		      "Failed to apply default clock management state for slow clock");
-	ret = clock_management_get_rate(dev1_slow);
+	ret = clock_management_request_state(data, default_request);
+	zassert_equal(ret, 0,
+		      "Failed to apply default clock management request");
+	ret = clock_management_get_rate(data, dev1_slow);
 	TC_PRINT("Slow clock default clock rate: %d\n", ret);
 	zassert_equal(ret, slow_default,
 		      "Slow clock has invalid clock default clock rate");
-
-	ret = clock_management_apply_state(dev1_fast, dev1_fast_default);
-	zassert_equal(ret, fast_default,
-		      "Failed to apply default clock management state for fast clock");
-	ret = clock_management_get_rate(dev1_fast);
+	ret = clock_management_get_rate(data, dev1_fast);
 	TC_PRINT("Fast clock default clock rate: %d\n", ret);
 	zassert_equal(ret, fast_default,
 		      "Fast clock has invalid clock default clock rate");
@@ -79,51 +76,19 @@ ZTEST(clock_management_minimal, test_sleep_states)
 	/* Apply sleep clock states for both clock outputs, make sure
 	 * that rates match what is expected
 	 */
-	TC_PRINT("Applying sleep clock states\n");
+	TC_PRINT("Requesting sleep clock state\n");
 
-	ret = clock_management_apply_state(dev1_slow, dev1_slow_sleep);
-	zassert_equal(ret, slow_sleep,
-		      "Failed to apply sleep clock management state for slow clock");
-	ret = clock_management_get_rate(dev1_slow);
+	ret = clock_management_request_state(data, sleep_request);
+	zassert_equal(ret, 0,
+		      "Failed to apply sleep clock management request");
+	ret = clock_management_get_rate(data, dev1_slow);
 	TC_PRINT("Slow clock sleep clock rate: %d\n", ret);
 	zassert_equal(ret, slow_sleep,
 		      "Slow clock has invalid clock sleep clock rate");
-
-	ret = clock_management_apply_state(dev1_fast, dev1_fast_sleep);
-	zassert_equal(ret, fast_sleep,
-		      "Failed to apply sleep clock management state for fast clock");
-	ret = clock_management_get_rate(dev1_fast);
+	ret = clock_management_get_rate(data, dev1_fast);
 	TC_PRINT("Fast clock sleep clock rate: %d\n", ret);
 	zassert_equal(ret, fast_sleep,
 		      "Fast clock has invalid clock sleep clock rate");
-}
-
-ZTEST(clock_management_minimal, test_rate_req)
-{
-	const struct clock_management_rate_req dev1_slow_req = {
-		.min_freq = DT_PROP(DT_NODELABEL(emul_dev1), slow_request_freq),
-		.max_freq = DT_PROP(DT_NODELABEL(emul_dev1), slow_request_freq),
-		.max_rank = CLOCK_MANAGEMENT_ANY_RANK,
-	};
-	const struct clock_management_rate_req dev1_fast_req = {
-		.min_freq = DT_PROP(DT_NODELABEL(emul_dev1), fast_request_freq),
-		.max_freq = DT_PROP(DT_NODELABEL(emul_dev1), fast_request_freq),
-		.max_rank = CLOCK_MANAGEMENT_ANY_RANK,
-	};
-
-	int dev1_slow_freq = DT_PROP(DT_NODELABEL(emul_dev1), slow_request_freq);
-	int dev1_fast_freq = DT_PROP(DT_NODELABEL(emul_dev1), fast_request_freq);
-	int ret;
-
-	/* Apply constraints for slow clock */
-	ret = clock_management_req_rate(dev1_slow, &dev1_slow_req);
-	zassert_equal(ret, dev1_slow_freq,
-		      "Slow clock got incorrect frequency for request");
-	TC_PRINT("Slow clock configured to rate %d\n", dev1_slow_freq);
-	ret = clock_management_req_rate(dev1_fast, &dev1_fast_req);
-	zassert_equal(ret, dev1_fast_freq,
-		      "Fast clock got incorrect frequency for request");
-	TC_PRINT("Fast clock configured to rate %d\n", dev1_fast_freq);
 }
 
 ZTEST_SUITE(clock_management_minimal, NULL, NULL, reset_clock_states, NULL, NULL);
