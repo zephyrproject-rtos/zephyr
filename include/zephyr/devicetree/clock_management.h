1e43de204c7677c33bdaa279df14061bf4943009
Daniel DeGrasse <daniel.degrasse@analog.com>
2026-08-18 17:38:17 +0000

    include: clock_management: rework clock management header for requests
    Rework the clock management header for clock requests. This represents a
    significant rework of the clock management subsystem, as consumers now
    request a group of allowed states (which may be children of any clock in
    the system) rather than a specific state from a specific output.
    
    This rework also changes the following:
    - states no longer define frequencies, as this approach was fragile.
      states did not need to configure all clocks in tree, so a state could
      claim to offer a frequency but not configure enough clock settings to
      actually realize it
    - frequency requests require that CONFIG_CLOCK_MANAGEMENT_SET_RATE is
      enabled
    - clocks can now be locked explicitly using "clock_management_lock", and
      there are no longer frequency min/max limits saved per clock. Implicit
      locks are also possible by setting `clock-request-n-locking` for a
      given clock request
    - each consumer must now define a clock_management_data singleton, which
      tracks consumer specific data.
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@analog.com>

--- include/zephyr/devicetree/clock_management.h
+++ include/zephyr/devicetree/clock_management.h
@@ -24,6 +24,57 @@ extern "C" {
  * @{
  */
 
+/** @cond INTERNAL_HIDDEN */
+
+
+/**
+ * @brief Defines a clock request from devicetree
+ *
+ * Defines a clock request from devicetree. Intended to be used with
+ * @ref DT_CLOCK_REQUEST_FOREACH_BY_NAME to generate clock request structures
+ * @param node_id Node identifier with the clock-request-n property
+ * @param _allowed_states Allowed states for this clock request
+ */
+#define DT_GET_CLOCK_REQUEST(node_id, _allowed_states) \
+	{ \
+		.clk = CLOCK_DT_GET(node_id), \
+		.allowed_states = _allowed_states, \
+	},
+
+/** @endcond */
+
+/**
+ * @brief Calls @p fn for every clock request in a clock-request-n array
+ *
+ * Calls @p fn for every clock request in a clock-request-n array, with the
+ * arguments being the clock node identifier and the allowed states for that
+ * clock request. The clock-request-n array is defined from the clock-request-n
+ * property, but phandles that affect the same clock output are grouped together
+ * to form one "allowed_state" bitmask for that clock. The @p fn macro
+ * should take two arguments:
+ * 1. The clock node identifier
+ * 2. The allowed states for that clock request
+ * @param node_id Node identifier with the clock-request-n property
+ * @param request_name Name of the clock request to get for this clock output
+ * @param fn Macro to call for each clock request
+ */
+#define DT_CLOCK_REQUEST_FOREACH_BY_NAME(node_id, request_name, fn) \
+	CONCAT(node_id, _CLOCK_REQUEST_, \
+		DT_CLOCK_REQUEST_NAME_IDX(node_id, request_name), _FOREACH)(fn)
+
+/**
+ * @brief Get the length of a clock-request-n array by the name
+ *
+ * Gets the length of a given clock-request-n array by the name, set by
+ * clock-request-names property. Note that since the clock-request-n
+ * phandles are grouped by clock output they affect, the length may not
+ * match the number of phandles in the clock-request-n property.
+ * @param node_id Node identifier with the clock-request-n property
+ * @param name Name of the clock request to get the length of
+ */
+#define DT_CLOCK_REQUEST_LEN_BY_NAME(node_id, name) \
+	CONCAT(node_id, _CLOCK_REQUEST_, DT_CLOCK_REQUEST_NAME_IDX(node_id, name), _LEN)
+
 /**
  * @brief Get index of clock output name
  * @param node_id Node ID with clock-output-names property
@@ -33,18 +84,18 @@ extern "C" {
 	DT_CAT4(node_id, _CLOCK_OUTPUT_NAME_, name, _IDX)
 
 /**
- * @brief Get index of clock state
- * @param node_id Node ID with clock-state-names property
- * @param name Name in the clock-state-names states to get the index of
+ * @brief Get index of clock request
+ * @param node_id Node ID with clock-request-names property
+ * @param name Name in the clock-request-names property to get the index of
  */
-#define DT_CLOCK_STATE_NAME_IDX(node_id, name) \
-	DT_CAT4(node_id, _CLOCK_STATE_NAME_, name, _IDX)
+#define DT_CLOCK_REQUEST_NAME_IDX(node_id, name) \
+	DT_CAT4(node_id, _CLOCK_REQUEST_NAME_, name, _IDX)
 
 /**
  * @brief Get a list of dependency ordinals of clocks that depend on a node
  *
  * This differs from `DT_SUPPORTS_DEP_ORDS` in that clock nodes that
- * reference the clock via the clock-state-n property will not be present
+ * reference the clock via the clock-request-n property will not be present
  * in this list.
  *
  * There is a comma after each ordinal in the expansion, **including**
@@ -60,6 +111,27 @@ extern "C" {
  *         by a comma (<tt>,</tt>), or an empty expansion
  */
 #define DT_SUPPORTS_CLK_ORDS(node_id) DT_CAT(node_id, _SUPPORTS_CLK_ORDS)
+
+/**
+ * @brief Call @p fn for every clock state of a node that is in use
+ *
+ * Call @p fn for every clock state of a clock-output node that is referenced
+ * by a clock-request-n property. This macro will call @p fn in the order
+ * that clock states are ranked, with the lowest ranked state first.
+ */
+#define DT_CLOCK_STATE_SORTED_FOREACH(node_id, fn) \
+	CONCAT(node_id, _CLOCK_STATE_SORTED_FOREACH)(fn)
+
+/**
+ * @brief Call @p fn for every clock state of an instance that is in use
+ *
+ * Call @p fn for every clock state of a clock-output instance that is referenced
+ * by a clock-request-n property. This macro will call @p fn in the order
+ * that clock states are ranked, with the lowest ranked state first.
+ * It is equivalent to DT_CLOCK_STATE_SORTED_FOREACH(DT_DRV_INST(inst), fn).
+ */
+#define DT_INST_CLOCK_STATE_SORTED_FOREACH(inst, fn) \
+	DT_CLOCK_STATE_SORTED_FOREACH(DT_DRV_INST(inst), fn)
 
 #ifdef __cplusplus
 }
