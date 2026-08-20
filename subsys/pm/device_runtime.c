cc29333610501a1d7352bde9f83ada3ab41eb07d
Holt Sun <holt.sun@nxp.com>
2026-08-20 15:59:25 +0800

    pm: device_runtime: add test operation hooks
    Add dormant hooks around runtime get and put operations so tests can
    enforce deterministic API ordering. Keep the hooks behind a test-only
    configuration that is not enabled by this commit.
    
    This preliminary change isolates hook plumbing from external power-loss
    behavior and builds without a hook implementation.
    
    Signed-off-by: Holt Sun <holt.sun@nxp.com>

--- subsys/pm/device_runtime.c
+++ subsys/pm/device_runtime.c
@@ -9,6 +9,7 @@
 
 #include <zephyr/pm/device.h>
 #include <zephyr/pm/device_runtime.h>
+#include <zephyr/pm/device_runtime_internal.h>
 #include <zephyr/sys/__assert.h>
 
 #include <zephyr/logging/log.h>
@@ -39,6 +40,14 @@ static struct k_work_q pm_device_runtime_wq;
 #define EVENT_STATE_SUSPENDED	BIT(PM_DEVICE_STATE_SUSPENDED)
 
 #define EVENT_MASK		(EVENT_STATE_ACTIVE | EVENT_STATE_SUSPENDED)
+
+#ifdef CONFIG_PM_DEVICE_RUNTIME_TESTING
+#define RUNTIME_TEST_HOOK(dev, hook) z_pm_device_runtime_test_hook(dev, hook)
+#else
+#define RUNTIME_TEST_HOOK(dev, hook)                                                               \
+	do {                                                                                       \
+	} while (false)
+#endif /* CONFIG_PM_DEVICE_RUNTIME_TESTING */
 
 /* Increment the usage counter of a device under the global lock. */
 static void runtime_usecount_inc(struct pm_device *pm)
@@ -291,6 +300,8 @@ int pm_device_runtime_get(const struct device *dev)
 		goto end;
 	}
 
+	RUNTIME_TEST_HOOK(dev, Z_PM_DEVICE_RUNTIME_HOOK_BEFORE_GET);
+
 	if (atomic_test_bit(&dev->pm_base->flags, PM_DEVICE_FLAG_ISR_SAFE)) {
 		struct pm_device_isr *pm_sync = dev->pm_isr;
 		k_spinlock_key_t k = k_spin_lock(&pm_sync->lock);
@@ -401,6 +412,7 @@ unlock:
 	}
 
 end:
+	RUNTIME_TEST_HOOK(dev, Z_PM_DEVICE_RUNTIME_HOOK_AFTER_GET);
 	SYS_PORT_TRACING_FUNC_EXIT(pm, device_runtime_get, dev, ret);
 
 	return ret;
@@ -453,6 +465,8 @@ int pm_device_runtime_put(const struct device *dev)
 
 	SYS_PORT_TRACING_FUNC_ENTER(pm, device_runtime_put, dev);
 
+	RUNTIME_TEST_HOOK(dev, Z_PM_DEVICE_RUNTIME_HOOK_BEFORE_PUT);
+
 	/*
 	 * Early return if device runtime is not enabled.
 	 */
@@ -468,6 +482,7 @@ int pm_device_runtime_put(const struct device *dev)
 	} else {
 		ret = runtime_suspend(dev, false, K_NO_WAIT);
 	}
+	RUNTIME_TEST_HOOK(dev, Z_PM_DEVICE_RUNTIME_HOOK_AFTER_PUT);
 	SYS_PORT_TRACING_FUNC_EXIT(pm, device_runtime_put, dev, ret);
 
 	return ret;
@@ -484,6 +499,8 @@ int pm_device_runtime_put_async(const struct device *dev, k_timeout_t delay)
 
 	SYS_PORT_TRACING_FUNC_ENTER(pm, device_runtime_put_async, dev, delay);
 
+	RUNTIME_TEST_HOOK(dev, Z_PM_DEVICE_RUNTIME_HOOK_BEFORE_PUT);
+
 	/*
 	 * Early return if device runtime is not enabled.
 	 */
@@ -499,6 +516,7 @@ int pm_device_runtime_put_async(const struct device *dev, k_timeout_t delay)
 	} else {
 		ret = runtime_suspend(dev, true, delay);
 	}
+	RUNTIME_TEST_HOOK(dev, Z_PM_DEVICE_RUNTIME_HOOK_AFTER_PUT);
 	SYS_PORT_TRACING_FUNC_EXIT(pm, device_runtime_put_async, dev, delay, ret);
 
 	return ret;
