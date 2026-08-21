acdb0caa086ffb406ce35d1f8c844cac2c00a0e6
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2026-08-21 12:02:49 +0200

    native_simulator: Get latest from upstream
    Align with native_simulator's upstream main
    0d3eafc13a7bf37ef146da540986f99356ac48bd
    
    Which includes:
    0d3eafc native: hw_timer: split real time pacing from tick logic
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- scripts/native_simulator/native/src/include/nsi_timer_model.h
+++ scripts/native_simulator/native/src/include/nsi_timer_model.h
@@ -16,7 +16,10 @@ extern "C" {
 #endif
 
 void hwtimer_set_real_time_mode(bool new_rt);
-void hwtimer_timer_reached(void);
+void hwtimer_set_rt_period(uint64_t period);
+void hwtimer_set_rt_ratio(double ratio);
+void hwtimer_adjust_rt_ratio(double ratio_correction);
+
 void hwtimer_wake_in_time(uint64_t time);
 void hwtimer_set_silent_ticks(int64_t sys_ticks);
 void hwtimer_enable(uint64_t period);
@@ -24,10 +27,7 @@ int64_t hwtimer_get_pending_silent_ticks(void);
 
 void hwtimer_reset_rtc(void);
 void hwtimer_set_rtc_offset(int64_t offset);
-void hwtimer_set_rt_ratio(double ratio);
-
 void hwtimer_adjust_rtc_offset(int64_t offset_delta);
-void hwtimer_adjust_rt_ratio(double ratio_correction);
 int64_t hwtimer_get_simu_rtc_time(void);
 void hwtimer_get_pseudohost_rtc_time(uint32_t *nsec, uint64_t *sec);
 
