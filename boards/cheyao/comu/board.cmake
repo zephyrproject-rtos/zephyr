2db803ed748333bbe8fea800a496e61c8cf8c640
Michael Hope <michaelh@juju.nz>
2026-08-21 14:14:56 +0200

    boards: comu: add the CH32V203 based Comu board

---
+++ boards/cheyao/comu/board.cmake
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: Copyright Michael Hope <michaelh@juju.nz>
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(minichlink "--dev-id=0x1209b003")
+include(${ZEPHYR_BASE}/boards/common/minichlink.board.cmake)
