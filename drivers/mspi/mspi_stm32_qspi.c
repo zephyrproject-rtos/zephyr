ea88bb8a40e9e6e98e9bd641e65574cf1f1478a8
Filip Stojanovic <filipembedded@gmail.com>
2026-08-22 11:02:18 +0200

    drivers: mspi: stm32: skip address phase - qspi
    Same as on XSPI and OSPI, address phase of transaction
    on QSPI was only skipped on WREN command. This commit applies
    the same fix as on XSPI and OSPI - removes address phase on
    every transfer that has addr_lenght = 0.
    
    Signed-off-by: Filip Stojanovic <filipembedded@gmail.com>

--- drivers/mspi/mspi_stm32_qspi.c
+++ drivers/mspi/mspi_stm32_qspi.c
@@ -508,7 +508,8 @@ static int mspi_stm32_qspi_access(const struct device *dev, const struct mspi_xf
 		cmd.DataMode = QSPI_DATA_NONE;
 	}
 
-	if (cmd.Instruction == MSPI_NOR_CMD_WREN) {
+	if (dev_data->ctx.xfer.addr_length == 0) {
+		/* Commands without an address phase, e.g. RDID or WREN */
 		cmd.AddressMode = QSPI_ADDRESS_NONE;
 	}
 
