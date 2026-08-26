4e5cb5e77bb599f7c84b2edfa71fbe4b9f7f3c52
Amneesh Singh <amneesh@ti.com>
2026-08-26 20:13:07 +0530

    drivers: dma: mspm0: support internal-trigger channel cascading
    Implement cascading using source_chaining_en, dest_chaining_in and
    linked_channel. Don't use SW trigger if these are enabled.
    
    Signed-off-by: Amneesh Singh <amneesh@ti.com>

--- drivers/dma/dma_ti_mspm0.c
+++ drivers/dma/dma_ti_mspm0.c
@@ -401,6 +401,14 @@ static int dma_ti_mspm0_configure(const struct device *dev, uint32_t channel,
 			dma_ti_mspm0_get_dmasz(b_cfg->block_size, config->source_data_size,
 					       config->dest_data_size, config->channel_direction);
 		cfg->regs->dmatctl[channel] = FIELD_PREP(DMA_MSPM0_TCTL_DMATSEL, trigger);
+
+		if (config->source_chaining_en || config->dest_chaining_en) {
+			cfg->regs->dmatctl[config->linked_channel] =
+				DMA_MSPM0_TCTL_DMATINT |
+				FIELD_PREP(DMA_MSPM0_TCTL_DMATSEL, channel);
+			cfg->regs->dmachan[config->linked_channel].dmactl |= DMA_MSPM0_CTL_DMAEN;
+		}
+
 		cfg->regs->dmachan[channel].dmactl = ctl;
 		cfg->regs->dmachan[channel].dmasa = b_cfg->source_address;
 		cfg->regs->dmachan[channel].dmada = b_cfg->dest_address;
