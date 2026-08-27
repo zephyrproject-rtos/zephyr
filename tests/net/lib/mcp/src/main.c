fff10629d1affaef705eec47928a95f4e3fa0459
Anas Nashif <anas.nashif@intel.com>
2026-08-27 14:16:52 -0400

    tests: net: mcp: check the captured token contents
    zassert_not_equal(token, 0) compares the address of a char array with
    0, which is always true and checks nothing. Check the first character
    instead.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- tests/net/lib/mcp/src/main.c
+++ tests/net/lib/mcp/src/main.c
@@ -1300,7 +1300,7 @@ ZTEST(mcp_server_tests, test_10_invalid_execution_tokens)
 
 	mcp_safe_strcpy(used_token, sizeof(used_token), last_execution_token);
 
-	zassert_not_equal(used_token, 0, "Should have captured the execution token");
+	zassert_not_equal(used_token[0], '\0', "Should have captured the execution token");
 
 	printk("=== Test 3a: Attempting to reuse token %s ===\n", used_token);
 	ret = mcp_server_submit_tool_message(server, &tool_msg, used_token);
@@ -1409,7 +1409,7 @@ ZTEST(mcp_server_tests, test_11_health_monitor)
 				"{\"test\":\"idle\"}");
 	memset(execution_token_idle, 0, sizeof(execution_token_idle));
 	mcp_safe_strcpy(execution_token_idle, sizeof(execution_token_idle), last_execution_token);
-	zassert_not_equal(execution_token_idle, 0,
+	zassert_not_equal(execution_token_idle[0], '\0',
 			  "Execution token should be captured for idle test");
 
 	k_msleep(CONFIG_MCP_TOOL_IDLE_TIMEOUT_MS / 2);
@@ -1432,7 +1432,7 @@ ZTEST(mcp_server_tests, test_11_health_monitor)
 	memset(execution_token_cancel, 0, sizeof(execution_token_cancel));
 	mcp_safe_strcpy(execution_token_cancel, sizeof(execution_token_cancel),
 			last_execution_token);
-	zassert_not_equal(execution_token_cancel, 0,
+	zassert_not_equal(execution_token_cancel[0], '\0',
 			  "Execution token should be captured for idle test");
 
 	k_msleep(CONFIG_MCP_TOOL_IDLE_TIMEOUT_MS / 2);
