a7d49d0d6a8b953f576f7fbf34e12135373ade2d
Tien Nguyen <tien.nguyen.zg@renesas.com>
2026-08-27 07:40:02 +0000

    driver: flash: renesas: update memory_size property
    update memory_size property follow by RZ FSP 4.2.0
    
    Signed-off-by: Tien Nguyen <tien.nguyen.zg@renesas.com>

--- drivers/flash/flash_renesas_rz_qspi.c
+++ drivers/flash/flash_renesas_rz_qspi.c
@@ -433,8 +433,6 @@ static DEVICE_API(flash, flash_renesas_rz_qspi_driver_api) = {
 #define DT_DRV_COMPAT renesas_rz_qspi_xspi
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
-#define XSPI_QSPI_MEMORY_SIZE(size_bytes)                                                          \
-	((xspi_qspi_memory_size_t)(((size_bytes) / (1 * 1024 * 1024)) - 1))
 
 #define RZ_QSPI_XSPI_SOC_NV_FLASH_COMPAT(node_id)                                                  \
 	COND_CODE_1(DT_NODE_HAS_COMPAT(node_id, soc_nv_flash), (node_id), ())
@@ -458,8 +456,7 @@ static DEVICE_API(flash, flash_renesas_rz_qspi_driver_api) = {
 	static const xspi_qspi_extended_cfg_t g_qspi##n##_extended_cfg = {                         \
 		.unit = DT_PROP(DT_INST_PARENT(n), unit),                                          \
 		.chip_select = XSPI_QSPI_CHIP_SELECT_##n,                                          \
-		.memory_size =                                                                     \
-			XSPI_QSPI_MEMORY_SIZE(DT_REG_SIZE(RZ_QSPI_XSPI_SOC_NV_FLASH_NODE(n))),     \
+		.memory_size = (uint32_t)(DT_REG_SIZE(RZ_QSPI_XSPI_SOC_NV_FLASH_NODE(n))),         \
 		.p_timing_settings = &g_qspi##n##_timing_settings,                                 \
 		.prefetch_en =                                                                     \
 			(xspi_qspi_prefetch_function_t)XSPI_QSPI_CFG_UNIT_##n##_PREFETCH_FUNCTION, \
