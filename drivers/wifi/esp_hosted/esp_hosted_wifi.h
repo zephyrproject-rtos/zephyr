8ffe494d0790888f1078a263aaa17db7f212d13d
Ibrahim Abdalkader <i.abdalkader@gmail.com>
2026-08-31 20:13:33 +0200

    drivers: wifi: esp_hosted: Walk the init event TLVs
    The init event is a list of {tag, length, value} triplets, and which tags
    the slave sends varies between firmware versions, so reading them at fixed
    offsets logs the wrong values.
    
    Look them up by tag, and check the event length against the frame length
    before walking.
    
    Signed-off-by: Ibrahim Abdalkader <i.abdalkader@gmail.com>

--- drivers/wifi/esp_hosted/esp_hosted_wifi.h
+++ drivers/wifi/esp_hosted/esp_hosted_wifi.h
@@ -61,6 +61,18 @@ typedef enum {
 	ESP_PRIV_EVENT_INIT,
 } esp_hosted_priv_event_t;
 
+/* Tags of the TLVs carried by ESP_PRIV_EVENT_INIT. Which ones the slave emits,
+ * and in what order, depends on the firmware version and the transport.
+ */
+typedef enum {
+	ESP_PRIV_CAPABILITY = 0x00,
+	ESP_PRIV_SPI_CLK_MHZ = 0x01,
+	ESP_PRIV_FIRMWARE_CHIP_ID = 0x02,
+	ESP_PRIV_TEST_RAW_TP = 0x03,
+	ESP_PRIV_FW_DATA = 0x04,
+	ESP_PRIV_TAG_MAX,
+} esp_hosted_priv_tag_t;
+
 /* TLV payload. */
 typedef struct __packed {
 	uint8_t ep_type;
