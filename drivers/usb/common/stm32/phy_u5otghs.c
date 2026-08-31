ba33986b8d1903524758f7ef052d6294bd964e66
Mathieu Choplain <mathieu.choplain-ext@st.com>
2026-08-31 15:25:09 +0200

    drivers: usb: common: stm32: phy_u5otghs: determine CLKSEL automatically
    Query the frequency of the OTG_HS PHY and compute the appropriate CLKSEL
    value at runtime if the `clock-reference` property is not specified.
    This removes the user burden of providing the same value twice in DTS.
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain-ext@st.com>

--- drivers/usb/common/stm32/phy_u5otghs.c
+++ drivers/usb/common/stm32/phy_u5otghs.c
@@ -30,8 +30,21 @@
 /* Even though we won't use this macro, define it for grep-ability */
 #define DT_DRV_COMPAT st_stm32u5_otghs_phy
 
+/*
+ * Detect appropriate CLKSEL value automatically based on input clock frequency.
+ * This value must differ from all SYSCFG_OTG_HS_PHY_CLK_SELECT_<n> values!
+ */
+#define CLKSEL_AUTODETECT	0u
+
+/*
+ * Does any PHY node need CLKSEL autodetection?
+ * True if any node lacks the `clock-reference` property.
+ */
+#define ANY_U5OTGHS_PHY_NEEDS_AUTODETECT \
+	!DT_ALL_COMPAT_HAS_PROP_STATUS_OKAY(DT_DRV_COMPAT, clock_reference)
+
 struct stm32_u5otghs_phy_config {
-	uint32_t reference;
+	uint32_t phycr_clksel;
 	uint32_t num_clocks;
 	struct stm32_pclken clocks[];
 };
@@ -41,13 +54,54 @@ static const struct device *rcc = DEVICE_DT_GET(STM32_CLOCK_CONTROL_NODE);
 static int stm32_u5otghs_phy_enable(const struct stm32_usb_phy *phy)
 {
 	const struct stm32_u5otghs_phy_config *cfg = phy->pcfg;
+	uint32_t clksel;
 	int res;
 
 	/* Enable SYSCFG where PHY configuration registers reside */
 	__HAL_RCC_SYSCFG_CLK_ENABLE();
 
 	/* Configure PHY input frequency selection  */
-	HAL_SYSCFG_SetOTGPHYReferenceClockSelection(cfg->reference);
+	if (ANY_U5OTGHS_PHY_NEEDS_AUTODETECT && cfg->phycr_clksel == CLKSEL_AUTODETECT) {
+		clock_control_subsys_t ck_src;
+		uint32_t phy_ck_freq;
+
+		if (cfg->num_clocks > 1) {
+			ck_src = (clock_control_subsys_t)&cfg->clocks[1];
+		} else {
+			ck_src = (clock_control_subsys_t)&cfg->clocks[0];
+		}
+
+		res = clock_control_get_rate(rcc, ck_src, &phy_ck_freq);
+		if (res != 0) {
+			return res;
+		}
+
+		switch (phy_ck_freq) {
+		case MHZ(16):
+			clksel = SYSCFG_OTG_HS_PHY_CLK_SELECT_1;
+			break;
+		case KHZ(19200):
+			clksel = SYSCFG_OTG_HS_PHY_CLK_SELECT_2;
+			break;
+		case MHZ(20):
+			clksel = SYSCFG_OTG_HS_PHY_CLK_SELECT_3;
+			break;
+		case MHZ(24):
+			clksel = SYSCFG_OTG_HS_PHY_CLK_SELECT_4;
+			break;
+		case MHZ(26):
+			clksel = SYSCFG_OTG_HS_PHY_CLK_SELECT_5;
+			break;
+		case MHZ(32):
+			clksel = SYSCFG_OTG_HS_PHY_CLK_SELECT_6;
+			break;
+		default:
+			return -EINVAL;
+		}
+	} else {
+		clksel = cfg->phycr_clksel;
+	}
+	HAL_SYSCFG_SetOTGPHYReferenceClockSelection(clksel);
 
 	/* Deassert PHY reset */
 	HAL_SYSCFG_EnableOTGPHY(SYSCFG_OTG_HS_PHY_ENABLE);
@@ -76,11 +130,13 @@ static int stm32_u5otghs_phy_disable(const struct stm32_usb_phy *phy)
  * but DT_ENUM_IDX() is zero-based, hence the UTIL_INC().
  */
 #define PHY_CLK_REF(n)	\
-	CONCAT(SYSCFG_OTG_HS_PHY_CLK_SELECT_, UTIL_INC(DT_ENUM_IDX(n, clock_reference)))
+	COND_CODE_0(DT_NODE_HAS_PROP(n, clock_reference),					\
+		(CLKSEL_AUTODETECT),								\
+		(CONCAT(SYSCFG_OTG_HS_PHY_CLK_SELECT_, UTIL_INC(DT_ENUM_IDX(n, clock_reference)))))
 
 #define DEFINE_U5OTGHS_PHY(usb_node, phy_node)							\
 	static const struct stm32_u5otghs_phy_config CONCAT(phy, DT_DEP_ORD(phy_node), _cfg) = {\
-		.reference = PHY_CLK_REF(phy_node),						\
+		.phycr_clksel = PHY_CLK_REF(phy_node),						\
 		.num_clocks = DT_NUM_CLOCKS(phy_node),						\
 		.clocks = STM32_DT_CLOCKS(phy_node)						\
 	};											\
