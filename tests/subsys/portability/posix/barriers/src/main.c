49f67bdf8a1c5a3e18412af4daac5bb69a5dc3f3
harshit kudhial <harshitkudhial@gmail.com>
2026-08-31 17:00:41 +0530

    tests: posix: barrier: test multi-cycle barrier reuse
    Add unit test test_pthread_barrier_wait to verify that multiple threads
    can synchronize back-to-back across multiple barrier cycles without
    deadlocking, and verify that exactly one thread per cycle receives
    PTHREAD_BARRIER_SERIAL_THREAD.
    
    Signed-off-by: harshit kudhial <harshitkudhial@gmail.com>

--- tests/subsys/portability/posix/barriers/src/main.c
+++ tests/subsys/portability/posix/barriers/src/main.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2023, Harshil Bhatt
+ * Copyright (c) 2026, Harshit Kudhial
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -10,6 +11,36 @@
 
 #include <zephyr/sys/util.h>
 #include <zephyr/ztest.h>
+
+#define BARRIER_THREAD_COUNT 3
+BUILD_ASSERT(BARRIER_THREAD_COUNT <= CONFIG_DYNAMIC_THREAD_POOL_SIZE,
+	     "CONFIG_DYNAMIC_THREAD_POOL_SIZE must be >= BARRIER_THREAD_COUNT");
+
+#define BARRIER_CYCLE_COUNT 5
+
+static pthread_barrier_t test_barrier;
+static pthread_mutex_t serial_mutex = PTHREAD_MUTEX_INITIALIZER;
+static int serial_count;
+
+static void *barrier_cycle_thread(void *arg)
+{
+	ARG_UNUSED(arg);
+
+	for (int i = 0; i < BARRIER_CYCLE_COUNT; i++) {
+		int ret = pthread_barrier_wait(&test_barrier);
+
+		zassert_true(ret == 0 || ret == PTHREAD_BARRIER_SERIAL_THREAD,
+			     "pthread_barrier_wait returned unexpected value %d", ret);
+
+		if (ret == PTHREAD_BARRIER_SERIAL_THREAD) {
+			zassert_ok(pthread_mutex_lock(&serial_mutex));
+			serial_count++;
+			zassert_ok(pthread_mutex_unlock(&serial_mutex));
+		}
+	}
+
+	return NULL;
+}
 
 ZTEST(posix_barriers, test_barrier)
 {
@@ -38,6 +69,51 @@ ZTEST(posix_barriers, test_barrier)
 
 	ret = pthread_barrierattr_destroy(&attr);
 	zassert_equal(ret, 0, "pthread_barrierattr_destroy failed");
+}
+
+ZTEST(posix_barriers, test_pthread_barrier_wait)
+{
+	pthread_t threads[BARRIER_THREAD_COUNT];
+	int ret;
+
+	serial_count = 0;
+
+	ret = pthread_barrier_init(&test_barrier, NULL, BARRIER_THREAD_COUNT + 1);
+	zassert_equal(ret, 0, "pthread_barrier_init failed");
+
+	for (int i = 0; i < BARRIER_THREAD_COUNT; i++) {
+		ret = pthread_create(&threads[i], NULL, barrier_cycle_thread, NULL);
+		zassert_equal(ret, 0, "pthread_create failed for thread %d", i);
+	}
+
+	/* Main thread participates as the (N+1)th member of the barrier */
+	for (int i = 0; i < BARRIER_CYCLE_COUNT; i++) {
+		ret = pthread_barrier_wait(&test_barrier);
+		zassert_true(ret == 0 || ret == PTHREAD_BARRIER_SERIAL_THREAD,
+			     "main pthread_barrier_wait returned unexpected value %d", ret);
+
+		if (ret == PTHREAD_BARRIER_SERIAL_THREAD) {
+			zassert_ok(pthread_mutex_lock(&serial_mutex));
+			serial_count++;
+			zassert_ok(pthread_mutex_unlock(&serial_mutex));
+		}
+	}
+
+	for (int i = 0; i < BARRIER_THREAD_COUNT; i++) {
+		ret = pthread_join(threads[i], NULL);
+		zassert_equal(ret, 0, "pthread_join failed for thread %d", i);
+	}
+
+	/*
+	 * Exactly one thread must receive PTHREAD_BARRIER_SERIAL_THREAD per
+	 * cycle, across all BARRIER_CYCLE_COUNT cycles.
+	 */
+	zassert_equal(serial_count, BARRIER_CYCLE_COUNT,
+		      "Expected %d serial thread callbacks, got %d",
+		      BARRIER_CYCLE_COUNT, serial_count);
+
+	ret = pthread_barrier_destroy(&test_barrier);
+	zassert_equal(ret, 0, "pthread_barrier_destroy failed");
 }
 
 ZTEST_SUITE(posix_barriers, NULL, NULL, NULL, NULL, NULL);
