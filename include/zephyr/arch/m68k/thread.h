378d0ef2a34284addc2b2597e48762a47e65cd20
Dimitri Varpusvuori <dimitri.varpusvuori@gmail.com>
2026-09-01 22:22:01 +0300

    arch: m68k: document architecture interfaces
    Add Doxygen comments for the m68k architecture headers and interrupt
    controller bindings so new API coverage checks pass.
    
    Signed-off-by: Dimitri Varpusvuori <dimitri.varpusvuori@gmail.com>

--- include/zephyr/arch/m68k/thread.h
+++ include/zephyr/arch/m68k/thread.h
@@ -4,6 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/**
+ * @file
+ * @brief M68K thread context definitions
+ */
+
 #ifndef ZEPHYR_INCLUDE_ARCH_M68K_THREAD_H_
 #define ZEPHYR_INCLUDE_ARCH_M68K_THREAD_H_
 
