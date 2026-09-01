a56874e289c1fe1d00a4a1a75da97ad9dcbe5655
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2026-09-01 16:28:49 +0200

    samples: logging: multidomain: Update sample configuration
    Update sample to use single IPC device (the default setup) but multiple
    endpoints on that device. Remove configuration for specific IPC service
    backend from the sample configuration as sample do not care which backend
    is being used.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- samples/subsys/logging/multidomain/remote/src/ipc_service.c
+++ samples/subsys/logging/multidomain/remote/src/ipc_service.c
@@ -12,78 +12,76 @@ LOG_MODULE_DECLARE(app);
 #define STACKSIZE	2048
 #define PRIORITY	K_PRIO_PREEMPT(2)
 
-K_THREAD_STACK_DEFINE(ipc1_stack, STACKSIZE);
-static volatile uint8_t ipc1_received_data;
-static K_SEM_DEFINE(ipc1_bound_sem, 0, 1);
-static K_SEM_DEFINE(ipc1_data_sem, 0, 1);
+K_THREAD_STACK_DEFINE(sample_stack, STACKSIZE);
+static volatile uint8_t sample_received_data;
+static K_SEM_DEFINE(bound_sem, 0, 1);
+static K_SEM_DEFINE(data_sem, 0, 1);
 
 /*
  * ==> THREAD 1 (IPC instance 1) <==
  */
 
-static void ipc1_ept_bound(void *priv)
+static void ept_bound(void *priv)
 {
-	k_sem_give(&ipc1_bound_sem);
+	k_sem_give(&bound_sem);
 }
 
-static void ipc1_ept_recv(const void *data, size_t len, void *priv)
+static void ept_recv(const void *data, size_t len, void *priv)
 {
-	ipc1_received_data = *((uint8_t *) data);
+	sample_received_data = *((uint8_t *) data);
 
-	k_sem_give(&ipc1_data_sem);
+	k_sem_give(&data_sem);
 }
 
-static struct ipc_ept_cfg ipc1_ept_cfg = {
-	.name = "ipc1",
+static struct ipc_ept_cfg ept_cfg = {
+	.name = "sample",
 	.cb = {
-		.bound    = ipc1_ept_bound,
-		.received = ipc1_ept_recv,
+		.bound    = ept_bound,
+		.received = ept_recv,
 	},
 };
 
-static void ipc1_entry(void *dummy0, void *dummy1, void *dummy2)
+static void sample_entry(void *dummy0, void *dummy1, void *dummy2)
 {
 	ARG_UNUSED(dummy0);
 	ARG_UNUSED(dummy1);
 	ARG_UNUSED(dummy2);
 
-	const struct device *ipc1_instance;
+	const struct device *ipc_instance;
 	unsigned char message = 0;
-	struct ipc_ept ipc1_ept;
+	struct ipc_ept ept;
 	int ret;
 
 	printk("IPC-service REMOTE [INST 1] demo started\n");
 
-	ipc1_instance = DEVICE_DT_GET(DT_NODELABEL(ipc1));
+	ipc_instance = DEVICE_DT_GET(DT_NODELABEL(ipc0));
 
 	/*k_sleep(K_FOREVER);*/
-	ret = ipc_service_open_instance(ipc1_instance);
+	ret = ipc_service_open_instance(ipc_instance);
 	if (ret < 0 && ret != -EALREADY) {
 		printk("ipc_service_open_instance() failure\n");
 		return;
 	}
 
-	LOG_INF("ipc open %d", ret);
-
-	ret = ipc_service_register_endpoint(ipc1_instance, &ipc1_ept, &ipc1_ept_cfg);
+	ret = ipc_service_register_endpoint(ipc_instance, &ept, &ept_cfg);
 	if (ret < 0) {
 		printf("ipc_service_register_endpoint() failure\n");
 		return;
 	}
 
 	LOG_INF("wait for bound");
-	k_sem_take(&ipc1_bound_sem, K_FOREVER);
+	k_sem_take(&bound_sem, K_FOREVER);
 	LOG_INF("bounded");
 
 	while (message < 99) {
-		k_sem_take(&ipc1_data_sem, K_FOREVER);
-		message = ipc1_received_data;
+		k_sem_take(&data_sem, K_FOREVER);
+		message = sample_received_data;
 
 		LOG_INF("REMOTE [1]: %d", message);
 
 		message++;
 
-		ret = ipc_service_send(&ipc1_ept, &message, sizeof(message));
+		ret = ipc_service_send(&ept, &message, sizeof(message));
 		if (ret < 0) {
 			printk("send_message(%d) failed with ret %d\n", message, ret);
 			break;
@@ -92,4 +90,4 @@ static void ipc1_entry(void *dummy0, void *dummy1, void *dummy2)
 
 	printk("IPC-service REMOTE [INST 1] demo ended.\n");
 }
-K_THREAD_DEFINE(ipc1_thread_id, STACKSIZE, ipc1_entry, NULL, NULL, NULL, PRIORITY, 0, 0);
+K_THREAD_DEFINE(sample_thread_id, STACKSIZE, sample_entry, NULL, NULL, NULL, PRIORITY, 0, 0);
