zephyrproject-rtos/zephyr

commit 0443254b69ec18d9d9e148be1a5823bca65c05b1

soc: microchip: sam: add new SoC sam5d2
Product URL: https://www.microchip.com/en-us/products/microprocessors/
32-bit-mpus/sama5/sama5d2-series

Signed-off-by: Tony Han <tony.han@microchip.com>

soc/microchip/sam/sama5/sama5d2/soc.c
@@ -1,0 +1,43 @@
+/*
+ * Copyright (C) 2026 Microchip Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#include <zephyr/init.h>
+#include <zephyr/arch/arm/mmu/arm_mmu.h>
+#include <zephyr/kernel.h>
+
+#define MMU_REGION_STRONG(cond, name, base, size) IF_ENABLED(cond,			\
+		(MMU_REGION_FLAT_ENTRY(name, base, size,				\
+				       MT_STRONGLY_ORDERED | MPERM_R | MPERM_W),))
+
+static const struct arm_mmu_region mmu_regions[] = {
+	MMU_REGION_FLAT_ENTRY("vectors", CONFIG_KERNEL_VM_BASE, 0x1000,
+			      MT_STRONGLY_ORDERED | MPERM_R | MPERM_X),
+
+	MMU_REGION_STRONG(DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(aic)), "sfr",
+			  SFR_BASE_ADDRESS, 0xa0)
+
+	MMU_REGION_STRONG(DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(aic)), "aic",
+			  AIC_BASE_ADDRESS, 0x100)
+
+	MMU_REGION_STRONG(DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(PMC)), "pmc",
+			  PMC_BASE_ADDRESS, 0x200)
+};
+
+const struct arm_mmu_config mmu_config = {
+	.num_regions = ARRAY_SIZE(mmu_regions),
+	.mmu_regions = mmu_regions,
+};
+
+void relocate_vector_table(void)
+{
+	write_vbar(CONFIG_KERNEL_VM_BASE);
+}
+
+void soc_early_init_hook(void)
+{
+	/* Apply SoC related preinit configuration */
+}

soc/microchip/sam/sama5/CMakeLists.txt
@@ -1,0 +1,10 @@
+# Copyright (C) 2025 Microchip Technology Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_subdirectory(${SOC_SERIES})
+
+add_subdirectory(../common ${CMAKE_BINARY_DIR}/common)
+
+set(SOC_LINKER_SCRIPT ${ZEPHYR_BASE}/include/zephyr/arch/arm/cortex_a_r/scripts/linker.ld CACHE INTERNAL "")

soc/microchip/sam/sama5/Kconfig
@@ -1,0 +1,12 @@
+# Copyright (C) 2026 Microchip Technology Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+config SOC_FAMILY_MICROCHIP_SAMA5
+	select MICROCHIP_SAM
+	select ARM
+	select CPU_CORTEX_A5
+	select MMU
+	select CACHE_MANAGEMENT
+	select ARM_CUSTOM_INTERRUPT_CONTROLLER

soc/microchip/sam/sama5/Kconfig.defconfig
@@ -1,0 +1,16 @@
+# Copyright (C) 2026 Microchip Technology Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+if SOC_FAMILY_MICROCHIP_SAMA5
+
+configdefault SOC_EARLY_INIT_HOOK
+	default y
+
+configdefault CLOCK_CONTROL
+	default y
+
+endif # SOC_FAMILY_MICROCHIP_SAMA5
+
+rsource "*/Kconfig.defconfig"

soc/microchip/sam/sama5/Kconfig.soc
@@ -1,0 +1,14 @@
+# Copyright (C) 2026 Microchip Technology Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+config SOC_FAMILY_MICROCHIP_SAMA5
+	bool
+	help
+	  Enable support for Microchip SAMA5 Cortex-A Microprocessors.
+
+config SOC_FAMILY
+	default "microchip_sama5" if SOC_FAMILY_MICROCHIP_SAMA5
+
+rsource "*/Kconfig.soc"

soc/microchip/sam/sama5/sama5d2/CMakeLists.txt
@@ -1,0 +1,7 @@
+# Copyright (C) 2026 Microchip Technology Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+zephyr_sources(soc.c)
+zephyr_include_directories(.)

soc/microchip/sam/sama5/sama5d2/Kconfig.defconfig
@@ -1,0 +1,17 @@
+# Copyright (C) 2026 Microchip Technology Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+if SOC_SERIES_SAMA5D2
+
+configdefault NUM_IRQS
+	default 110
+
+configdefault SYS_CLOCK_HW_CYCLES_PER_SEC
+	default $(dt_node_int_prop_int,/soc/timer@f8048030,clock-frequency)
+
+configdefault SYS_CLOCK_TICKS_PER_SEC
+	default 1000
+
+endif # SOC_SERIES_SAMA5D2

soc/microchip/sam/sama5/sama5d2/Kconfig.soc
@@ -1,0 +1,122 @@
+# Copyright (C) 2026 Microchip Technology Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+config SOC_SERIES_SAMA5D2
+	bool
+	select SOC_FAMILY_MICROCHIP_SAMA5
+	help
+	  Enable support for Microchip SAMA5D2 Cortex-A Microprocessors.
+
+config SOC_SERIES_SAMA5D27SOM
+	bool
+	select SOC_SERIES_SAMA5D2
+	help
+	  Enable support for Microchip SAMA5D2 System-On-Module (SOM).
+
+config SOC_SERIES_SAMA5D29
+	bool
+	select SOC_SERIES_SAMA5D2
+	help
+	  Enable support for Microchip SAMA5D29 Cortex-A Microprocessors.
+
+config SOC_SERIES
+	default "sama5d2" if SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D21
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D22
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D225CD1M
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D23
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D24
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D26
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D27
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D27CD1G
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D27CD5M
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D27CLD1G
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D27CLD2G
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D28
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D28CD1G
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D28CLD1G
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D28CLD2G
+	bool
+	select SOC_SERIES_SAMA5D2
+
+config SOC_SAMA5D27SOM1
+	bool
+	select SOC_SERIES_SAMA5D27SOM
+
+config SOC_SAMA5D27WLSOM1
+	bool
+	select SOC_SERIES_SAMA5D27SOM
+
+config SOC_SAMA5D29
+	bool
+	select SOC_SERIES_SAMA5D29
+
+config SOC_SAMA5D29TA100
+	bool
+	select SOC_SERIES_SAMA5D29
+
+config SOC
+	default"sama5d21" if SOC_SAMA5D21
+	default"sama5d22" if SOC_SAMA5D22
+	default"sama5d225cd1m" if SOC_SAMA5D225CD1M
+	default"sama5d23" if SOC_SAMA5D23
+	default"sama5d24" if SOC_SAMA5D24
+	default"sama5d26" if SOC_SAMA5D26
+	default"sama5d27" if SOC_SAMA5D27
+	default"sama5d27cd1g" if SOC_SAMA5D27CD1G
+	default"sama5d27cd5m" if SOC_SAMA5D27CD5M
+	default"sama5d27cld1g" if SOC_SAMA5D27CLD1G
+	default"sama5d27cld2g" if SOC_SAMA5D27CLD2G
+	default"sama5d28" if SOC_SAMA5D28
+	default"sama5d28cd1g" if SOC_SAMA5D28CD1G
+	default"sama5d28cld1g" if SOC_SAMA5D28CLD1G
+	default"sama5d28cld2g" if SOC_SAMA5D28CLD2G
+	default"sama5d27som1" if SOC_SAMA5D27SOM1
+	default"sama5d27wlsom1" if SOC_SAMA5D27WLSOM1
+	default"sama5d29" if SOC_SAMA5D29
+	default"sama5d29ta100" if SOC_SAMA5D29TA100

commit 3914c4e3ce935f94471e0d980b88d138284b7002

soc: microchip: sama5d2: fix PMC DT node label in MMU region
The MMU region for the PMC used the uppercase label PMC instead of
the correct lowercase label pmc, causing DT_NODE_HAS_STATUS_OKAY()
to silently evaluate to false and the PMC region to be omitted from
the MMU table.

There is no runtime impact because the PMC is not accessed after the
MMU is configured.

Fixes: 0443254b69ec ("soc: microchip: sam: add new SoC sam5d2")

Signed-off-by: Tony Han <tony.han@microchip.com>

soc/microchip/sam/sama5/sama5d2/soc.c
@@ -23,7 +23,7 @@ static const struct arm_mmu_region mmu_regions[] = {
 	MMU_REGION_STRONG(DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(aic)), "aic",
 			  AIC_BASE_ADDRESS, 0x100)
 
-	MMU_REGION_STRONG(DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(PMC)), "pmc",
+	MMU_REGION_STRONG(DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(pmc)), "pmc",
 			  PMC_BASE_ADDRESS, 0x200)
 };
 