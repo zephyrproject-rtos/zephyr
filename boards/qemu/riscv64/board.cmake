c559c17386ad7920f88973c6ae6133c2c7d1cb20
Omar Naffaa <onaffaa@qti.qualcomm.com>
2026-09-01 09:41:00 -0700

    boards: Add device loader support for QEMU RV64
    Update RISCV-64 board.make allow "-device,loader" to be used instead
    of "-kernel" flag if configured.
    
    Signed-off-by: Omar Naffaa <onaffaa@qti.qualcomm.com>

--- boards/qemu/riscv64/board.cmake
+++ boards/qemu/riscv64/board.cmake
@@ -38,4 +38,14 @@ set(QEMU_BOARD_FLAGS
   ${QEMU_VIRTIO_INPUT_FLAGS}
   )
 
+if(CONFIG_QEMU_DEVICE_LOADER)
+  set(QEMU_KERNEL_OPTION "")
+  math(EXPR max_cpu_index "${CONFIG_MP_MAX_NUM_CPUS} - 1")
+  foreach(cpu_num RANGE 0 ${max_cpu_index})
+    list(APPEND QEMU_KERNEL_OPTION
+      "-device;loader,file=\$<TARGET_FILE:\${logical_target_for_zephyr_elf}>,cpu-num=${cpu_num}"
+    )
+  endforeach()
+endif()
+
 include(${ZEPHYR_BASE}/boards/common/qemu.board.cmake)
