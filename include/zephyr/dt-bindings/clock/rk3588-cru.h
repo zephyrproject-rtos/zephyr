93bcb96333571442f7e174f17071e4dfbed96ba8
Yanzhao Cui <cuiyanzhao@kylinos.cn>
2026-09-01 15:32:55 +0800

    include: dt-bindings: add RK3588 CRU clock and reset IDs for GMAC
    Add clock and reset identifiers required to gate and reset RK3588 GMAC
    blocks from platform glue and devicetree nodes.
    
    Signed-off-by: Yanzhao Cui <cuiyanzhao@kylinos.cn>

---
+++ include/zephyr/dt-bindings/clock/rk3588-cru.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2026 KylinSoft
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Subset of Linux include/dt-bindings/clock/rk3588-cru.h for GMAC.
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_RK3588_CRU_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_RK3588_CRU_H_
+
+#define MCLK_GMAC0_OUT     248
+#define CLK_GMAC0_PTP_REF  308
+#define CLK_GMAC1_PTP_REF  309
+#define CLK_GMAC_125M      310
+#define CLK_GMAC_50M       311
+#define PCLK_GMAC0         344
+#define PCLK_GMAC1         345
+#define ACLK_GMAC0         349
+#define ACLK_GMAC1         350
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_RK3588_CRU_H_ */
