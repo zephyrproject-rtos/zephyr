977fdaa93c1e356b44b66f107cff5c5536965e8d
Francois Ramu <francois.ramu@st.com>
2026-09-02 14:48:56 +0200

    drivers: flash: stm32 xspi warn to move to MSPI instead
    Add a warning to recommend to move the stm32h5 devices
    to the mspi stm32 controller with the mspi stm32 xspi driver.
    The flash stm32 xspi driver will be deprecated v4.5.0
    
    Signed-off-by: Francois Ramu <francois.ramu@st.com>

--- drivers/flash/flash_stm32_xspi.c
+++ drivers/flash/flash_stm32_xspi.c
@@ -13,6 +13,10 @@
  */
 #define DT_DRV_COMPAT st_stm32_xspi_nor
 
+#if defined(CONFIG_SOC_SERIES_STM32H5X)
+#warning "This driver will be deprecated in favor of its MSPI alternative following v4.5.0"
+#endif /* CONFIG_SOC_SERIES_STM32H5X */
+
 #include <errno.h>
 #include <zephyr/kernel.h>
 #include <soc.h>
