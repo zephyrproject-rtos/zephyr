28a5f5f98389fb3809cb4cebea465c178dff816b
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2026-09-04 08:26:27 +0100

    kconfig: Error out on deprecated usage when requested
    Adds a new Kconfig to both Zephyr and sysbuild,
    ``DEPRECATED_KCONFIGS_AS_ERRORS`` which, when enabled, will cause
    a build to fail if a deprecated Kconfig is enabled. This is being
    introduced to ensure API lifecycle policy is being followed when
    features are deprecated
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- scripts/kconfig/kconfig.py
+++ scripts/kconfig/kconfig.py
@@ -277,6 +277,12 @@ def check_deprecated(kconf):
             selector_name = split_expr(selector, AND)[0].name
             warn(f'Deprecated symbol {selector_name} is enabled.')
 
+        if (
+            kconf.syms.get('DEPRECATED_KCONFIGS_AS_ERRORS', kconf.y).tri_value == 2
+            and len(selectors) > 0
+        ):
+            err("Aborting due to Kconfig deprecation warnings")
+
 
 def check_experimental(kconf):
     experimental = kconf.syms.get('EXPERIMENTAL')
