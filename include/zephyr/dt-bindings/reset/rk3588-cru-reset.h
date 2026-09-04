4a4ef5ec290b3e1728010dfdff3c07461456c2a5
Yanzhao Cui <cuiyanzhao@kylinos.cn>
2026-09-04 16:35:24 +0800

    include: dt-bindings: rockchip: add RK3588 CRU clock and reset IDs
    Add the clock and reset identifiers exposed by the RK3588 Clock and Reset
    Unit that are needed to describe the GMAC controllers in devicetree. The
    IDs follow the Linux rk3588-cru numbering so devicetree nodes can be kept
    close to their Linux counterparts.
    
    Signed-off-by: Yanzhao Cui <cuiyanzhao@kylinos.cn>

---
+++ include/zephyr/dt-bindings/reset/rk3588-cru-reset.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026 KylinSoft Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Subset of Linux include/dt-bindings/reset/rk3588-cru.h.
+ */
+
+#ifndef ZEPHYR_INCLUDE_DT_BINDINGS_RESET_RK3588_CRU_RESET_H_
+#define ZEPHYR_INCLUDE_DT_BINDINGS_RESET_RK3588_CRU_RESET_H_
+
+#define SRST_A_GMAC0 291
+#define SRST_A_GMAC1 292
+
+#endif /* ZEPHYR_INCLUDE_DT_BINDINGS_RESET_RK3588_CRU_RESET_H_ */
