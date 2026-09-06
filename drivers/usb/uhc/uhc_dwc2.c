zephyrproject-rtos/zephyr

commit 89a75416b7b33b108c9973846ee1ab73b9844fb0

drivers: usb: uhc_dwc2: add initial support
Note: Control and Bulk transfers only

Add initial usb host driver for Synopsys DWC2 with vendor quirks.

Signed-off-by: Roman Leonov <jam_roma@yahoo.com>

drivers/usb/uhc/uhc_dwc2.h
@@ -1,0 +1,88 @@
+/*
+ * Copyright (c) 2026 Roman Leonov <jam_roma@yahoo.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_USB_UHC_DWC2_H
+#define ZEPHYR_DRIVERS_USB_UHC_DWC2_H
+
+#include <stdint.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/usb/uhc.h>
+#include <usb_dwc2_hw.h>
+
+/* Vendor quirks per driver instance */
+struct uhc_dwc2_vendor_quirks {
+	/* Called on uhc_dwc2_preinit() after the thread is initialized */
+	int (*post_preinit)(const struct device *dev);
+	/* Called at the beginning of uhc_dwc2_init(), before controller is initialized  */
+	int (*pre_init)(const struct device *dev);
+	/* Called on uhc_dwc2_enable() before enable the port power */
+	int (*pre_enable)(const struct device *dev);
+	/* Called on uhc_dwc2_enable() after enable the port power */
+	int (*post_enable)(const struct device *dev);
+	/* Called at the end of uhc_dwc2_disable() */
+	int (*disable)(const struct device *dev);
+	/* Called at the end of uhc_dwc2_shutdown() */
+	int (*shutdown)(const struct device *dev);
+	/* Called at the end of IRQ handling */
+	int (*irq_clear)(const struct device *dev);
+	/* Called while waiting for bits that require PHY to be clocked */
+	int (*is_phy_clk_off)(const struct device *dev);
+};
+
+/* Driver configuration per instance */
+struct uhc_dwc2_config {
+	/* Pointer to base address of DWC_OTG registers */
+	struct usb_dwc2_reg *const base;
+	/* Vendor specific quirks */
+	const struct uhc_dwc2_vendor_quirks *const quirks;
+	void *quirk_data;
+	const void *quirk_config;
+	/* Pointer to the stack used by the driver thread */
+	k_thread_stack_t *stack;
+	/* Size of the stack used by the driver thread */
+	size_t stack_size;
+
+	void (*irq_enable_func)(const struct device *dev);
+	void (*irq_disable_func)(const struct device *dev);
+};
+
+#define UHC_DWC2_QUIRK_CONFIG(dev)						\
+	(((const struct uhc_dwc2_config *)dev->config)->quirk_config)
+
+#define UHC_DWC2_QUIRK_DATA(dev)						\
+	(((const struct uhc_dwc2_config *)dev->config)->quirk_data)
+
+#if DT_HAS_COMPAT_STATUS_OKAY(espressif_esp32_usb_otg)
+#include "uhc_dwc2_esp32_usb_otg.h"
+#endif
+
+#define UHC_DWC2_VENDOR_QUIRK_GET(n)						\
+	COND_CODE_1(DT_NODE_VENDOR_HAS_IDX(DT_DRV_INST(n), 1),			\
+			(&uhc_dwc2_vendor_quirks_##n),				\
+			(NULL))
+
+#define DWC2_QUIRK_FUNC_DEFINE(fname)						\
+static inline int uhc_dwc2_quirk_##fname(const struct device *const dev)	\
+{										\
+	const struct uhc_dwc2_config *const config = dev->config;		\
+	const struct uhc_dwc2_vendor_quirks *const quirks = config->quirks;	\
+										\
+	if (quirks != NULL && quirks->fname != NULL) {				\
+		return config->quirks->fname(dev);				\
+	}									\
+	return 0;								\
+}
+
+DWC2_QUIRK_FUNC_DEFINE(post_preinit)
+DWC2_QUIRK_FUNC_DEFINE(pre_init)
+DWC2_QUIRK_FUNC_DEFINE(pre_enable)
+DWC2_QUIRK_FUNC_DEFINE(post_enable)
+DWC2_QUIRK_FUNC_DEFINE(disable)
+DWC2_QUIRK_FUNC_DEFINE(shutdown)
+DWC2_QUIRK_FUNC_DEFINE(irq_clear)
+DWC2_QUIRK_FUNC_DEFINE(is_phy_clk_off)
+
+#endif /* ZEPHYR_DRIVERS_USB_UHC_DWC2_H */

drivers/usb/uhc/CMakeLists.txt
@@ -3,8 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
 zephyr_library()
+zephyr_library_include_directories(${ZEPHYR_BASE}/drivers/usb/common/)
 
 zephyr_library_sources(uhc_common.c)
+zephyr_library_sources_ifdef(CONFIG_UHC_DWC2 uhc_dwc2.c)
 zephyr_library_sources_ifdef(CONFIG_UHC_MAX3421E uhc_max3421e.c)
 zephyr_library_sources_ifdef(CONFIG_UHC_VIRTUAL uhc_virtual.c)
 zephyr_library_sources_ifdef(CONFIG_UHC_NXP_EHCI uhc_mcux_common.c uhc_mcux_ehci.c)

drivers/usb/uhc/Kconfig
@@ -43,6 +43,7 @@ module = UHC_DRIVER
 module-str = uhc drv
 source "subsys/logging/Kconfig.template.log_config"
 
+source "drivers/usb/uhc/Kconfig.dwc2"
 source "drivers/usb/uhc/Kconfig.max3421e"
 source "drivers/usb/uhc/Kconfig.virtual"
 source "drivers/usb/uhc/Kconfig.mcux"

drivers/usb/uhc/Kconfig.dwc2
@@ -1,0 +1,80 @@
+# Copyright (c) 2026 Roman Leonov <jam_roma@yahoo.com>
+# SPDX-License-Identifier: Apache-2.0
+
+config UHC_DWC2
+	bool "DWC2 USB host controller driver"
+	default y
+	depends on DT_HAS_SNPS_DWC2_ENABLED
+	select EVENTS
+	help
+	  DWC2 USB host controller driver.
+
+if UHC_DWC2
+
+config UHC_DWC2_STACK_SIZE
+	int "UHC DWC2 driver thread stack size"
+	default 512
+	help
+	  DWC2 driver thread stack size.
+
+config UHC_DWC2_THREAD_PRIORITY
+	int "UHC DWC2 driver thread priority"
+	default 8
+	help
+	  DWC2 driver thread priority.
+
+config UHC_DWC2_DEBOUNCE_DELAY_MS
+	int "Debounce delay in ms"
+	range 100 250
+	default 250
+	help
+	  On connection of a USB device, the USB 2.0 specification requires a
+	  "debounce interval with a minimum duration of 100ms" to allow
+	  the connection to stabilize (see USB 2.0 chapter 7.1.7.3 for more details).
+	  During the debounce interval, no new connection/disconnection events are registered.
+
+	  The default value is set to 250 ms to be safe.
+
+config UHC_DWC2_RESET_HOLD_MS
+	int "Reset hold in ms"
+	range 10 50
+	default 50
+	help
+	  Root-port reset hold time in milliseconds.
+
+	  USB 2.0 requires ordinary reset signaling to be asserted for at least
+	  10 ms, but reset issued from a root port must last at least 50 ms..
+
+	  The default value is set to 50 ms.
+
+config UHC_DWC2_RESET_RECOVERY_MS
+	int "Reset recovery delay in ms"
+	range 10 30
+	default 30
+	help
+	  Post-reset recovery delay in milliseconds.
+
+	  USB 2.0 requires system software to allow at least 10 ms for reset
+	  recovery after the port stops driving reset, before the attached
+	  device is expected to respond to data transfers.
+
+	  The attached device may ignore data transfers during this recovery
+	  interval.
+
+	  The default value is set to 30 ms.
+
+config UHC_DWC2_SET_ADDR_DELAY_MS
+	int "Delay after set device address"
+	range 2 30
+	default 10
+	help
+	  "After successful completion of the Status stage, the device is allowed
+	  a SetAddress() recovery interval of 2 ms. At the end of this interval,
+	  the device must be able to accept Setup packets addressed to the new address.
+	  Also, at the end of the recovery interval, the device must not respond
+	  to tokens sent to the old address (unless, of course, the old and new
+	  address is the same)." See USB 2.0 chapter 9.2.6.3 for more details.
+
+	  The default value is set to 10 ms to be safe.
+
+endif # UHC_DWC2

drivers/usb/uhc/uhc_dwc2_esp32_usb_otg.h
@@ -1,0 +1,243 @@
+/*
+ * Copyright (c) 2026 Roman Leonov <jam_roma@yahoo.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_USB_UHC_DWC2_ESP32_USB_OTG_H
+#define ZEPHYR_DRIVERS_USB_UHC_DWC2_ESP32_USB_OTG_H
+
+#include <zephyr/drivers/interrupt_controller/intc_esp32.h>
+#include <zephyr/drivers/clock_control.h>
+#include <zephyr/logging/log.h>
+
+#include <esp_err.h>
+#include <esp_private/usb_phy.h>
+#include <hal/usb_wrap_hal.h>
+#include <soc/usb_periph.h>
+
+#include <esp_rom_gpio.h>
+#include <hal/gpio_ll.h>
+#include <soc/usb_pins.h>
+#include <soc/gpio_sig_map.h>
+
+struct esp32_usb_otg_config {
+	const struct device *clock_dev;
+	const clock_control_subsys_t clock_subsys;
+	int irq_source;
+	int irq_priority;
+	int irq_flags;
+	usb_phy_target_t phy_target;
+};
+
+struct esp32_usb_otg_data {
+	struct intr_handle_data_t *int_handle;
+	usb_wrap_hal_context_t wrap_hal;
+};
+
+static void uhc_dwc2_isr_handler(const struct device *dev);
+
+static inline int esp32_usb_otg_init(const struct device *dev)
+{
+	const struct esp32_usb_otg_config *const cfg = UHC_DWC2_QUIRK_CONFIG(dev);
+	struct esp32_usb_otg_data *const data = UHC_DWC2_QUIRK_DATA(dev);
+
+	int ret;
+
+	if (!device_is_ready(cfg->clock_dev)) {
+		return -ENODEV;
+	}
+
+	ret = clock_control_on(cfg->clock_dev, cfg->clock_subsys);
+
+	if (ret != 0) {
+		return ret;
+	}
+
+	/* pinout config to work in USB_OTG_MODE_HOST */
+	esp_rom_gpio_connect_in_signal(GPIO_MATRIX_CONST_ZERO_INPUT,
+					USB_OTG_IDDIG_IN_IDX,
+					false);
+	esp_rom_gpio_connect_in_signal(GPIO_MATRIX_CONST_ZERO_INPUT,
+					USB_SRP_BVALID_IN_IDX,
+					false);
+	esp_rom_gpio_connect_in_signal(GPIO_MATRIX_CONST_ONE_INPUT,
+					USB_OTG_VBUSVALID_IN_IDX,
+					false);
+	esp_rom_gpio_connect_in_signal(GPIO_MATRIX_CONST_ONE_INPUT,
+					USB_OTG_AVALID_IN_IDX,
+					false);
+
+	/* allocate interrupt but keep it disabled to avoid
+	 * spurious suspend/resume event at enumeration phase
+	 */
+	ret = esp_intr_alloc(cfg->irq_source,
+			ESP_INTR_FLAG_INTRDISABLED |
+			ESP_PRIO_TO_FLAGS(cfg->irq_priority) |
+					ESP_INT_FLAGS_CHECK(cfg->irq_flags),
+			(intr_handler_t)uhc_dwc2_isr_handler,
+			(void *)dev,
+			&data->int_handle);
+
+	return ret;
+}
+
+static int esp32_usb_otg_enable_phy(const struct device *const dev)
+{
+	const struct esp32_usb_otg_config *const cfg = UHC_DWC2_QUIRK_CONFIG(dev);
+	struct esp32_usb_otg_data *const data = UHC_DWC2_QUIRK_DATA(dev);
+
+	usb_wrap_hal_init(&data->wrap_hal);
+	usb_wrap_ll_phy_enable_pad(data->wrap_hal.dev, true);
+
+#if USB_WRAP_LL_EXT_PHY_SUPPORTED
+	if (cfg->phy_target == USB_PHY_TARGET_INT) {
+		usb_wrap_hal_phy_set_external(&data->wrap_hal, false);
+	} else {
+		/* If External PHY target is supported, set external to true */
+		LOG_WRN("esp32 phy: External phy support isn't implemented");
+		return -EINVAL;
+	}
+#endif
+
+	if (cfg->phy_target == USB_PHY_TARGET_INT) {
+		/* Set drive capabilities for DM and DP */
+		gpio_ll_set_drive_capability(GPIO_LL_GET_HW(0), USBPHY_DM_NUM, GPIO_DRIVE_CAP_3);
+		gpio_ll_set_drive_capability(GPIO_LL_GET_HW(0), USBPHY_DP_NUM, GPIO_DRIVE_CAP_3);
+		/* Configure pull resistors for Host DM and DP */
+		usb_wrap_pull_override_vals_t vals = {
+			.dp_pu = false,
+			.dm_pu = false,
+			.dp_pd = true,
+			.dm_pd = true,
+		};
+		usb_wrap_hal_phy_enable_pull_override(&data->wrap_hal, &vals);
+	}
+
+	LOG_DBG("PHY enabled");
+	return 0;
+}
+
+static int esp32_usb_otg_disable_phy(const struct device *const dev)
+{
+	struct esp32_usb_otg_data *const data = UHC_DWC2_QUIRK_DATA(dev);
+
+	usb_wrap_hal_disable();
+	usb_wrap_ll_phy_enable_pad(data->wrap_hal.dev, false);
+
+	LOG_DBG("PHY disabled");
+	return 0;
+}
+
+static int esp32_usb_otg_pre_init(const struct device *const dev)
+{
+	int ret;
+
+	/* Power up PHY */
+	ret = esp32_usb_otg_enable_phy(dev);
+
+	if (ret) {
+		LOG_ERR("Unable to power on esp32 usb otg PHY");
+		return ret;
+	}
+
+	/* Enable clock, configure pins and init interrupt */
+	return esp32_usb_otg_init(dev);
+}
+
+static int esp32_usb_otg_shutdown(const struct device *const dev)
+{
+	const struct esp32_usb_otg_config *const cfg = UHC_DWC2_QUIRK_CONFIG(dev);
+	struct esp32_usb_otg_data *const data = UHC_DWC2_QUIRK_DATA(dev);
+	int ret;
+
+	/* Disable & free interrupt handler */
+	if (data->int_handle != NULL) {
+		/* Stor interrupts first */
+		ret = esp_intr_disable(data->int_handle);
+		if (ret != 0) {
+			LOG_ERR("Unable to disable interrupt: %d", ret);
+			return ret;
+		}
+
+		ret = esp_intr_free(data->int_handle);
+		if (ret != 0) {
+			LOG_ERR("Unable to free interrupt: %d", ret);
+			return ret;
+		}
+
+		data->int_handle = NULL;
+	}
+
+	/* Disable PHY */
+	esp32_usb_otg_disable_phy(dev);
+
+	/* Put USB OTG input signals into a safe inactive state */
+	esp_rom_gpio_connect_in_signal(GPIO_MATRIX_CONST_ONE_INPUT,
+				       USB_OTG_IDDIG_IN_IDX,
+				       false);
+	esp_rom_gpio_connect_in_signal(GPIO_MATRIX_CONST_ZERO_INPUT,
+				       USB_SRP_BVALID_IN_IDX,
+				       false);
+	esp_rom_gpio_connect_in_signal(GPIO_MATRIX_CONST_ZERO_INPUT,
+				       USB_OTG_VBUSVALID_IN_IDX,
+				       false);
+	esp_rom_gpio_connect_in_signal(GPIO_MATRIX_CONST_ZERO_INPUT,
+				       USB_OTG_AVALID_IN_IDX,
+				       false);
+
+	ret = clock_control_off(cfg->clock_dev, cfg->clock_subsys);
+	if (ret != 0) {
+		LOG_ERR("Unable to off the clock: %d", ret);
+	}
+
+	return ret;
+}
+
+static void esp32_usb_otg_irq_enable_func(const struct device *const dev)
+{
+	struct esp32_usb_otg_data *const data = UHC_DWC2_QUIRK_DATA(dev);
+
+	esp_intr_enable(data->int_handle);
+}
+
+static void esp32_usb_otg_irq_disable_func(const struct device *const dev)
+{
+	struct esp32_usb_otg_data *const data = UHC_DWC2_QUIRK_DATA(dev);
+
+	esp_intr_disable(data->int_handle);
+}
+
+#define QUIRK_ESP32_USB_OTG_DEFINE(n)						\
+										\
+	static const struct esp32_usb_otg_config uhc_dwc2_quirk_config_##n = {	\
+		.clock_dev = DEVICE_DT_GET(DT_INST_CLOCKS_CTLR(n)),		\
+		.clock_subsys = (clock_control_subsys_t)			\
+			DT_INST_CLOCKS_CELL(n, offset),				\
+		.irq_source = DT_INST_IRQ_BY_IDX(n, 0, irq),			\
+		.irq_priority = DT_INST_IRQ_BY_IDX(n, 0, priority),		\
+		.irq_flags = DT_INST_IRQ_BY_IDX(n, 0, flags),			\
+		.phy_target = USB_PHY_TARGET_INT,				\
+	};									\
+										\
+	static struct esp32_usb_otg_data uhc_dwc2_quirk_data_##n;		\
+										\
+	static const struct uhc_dwc2_vendor_quirks uhc_dwc2_vendor_quirks_##n = {\
+		.pre_init = esp32_usb_otg_pre_init,				\
+		.shutdown = esp32_usb_otg_shutdown,				\
+	};
+
+#define UHC_DWC2_IRQ_DT_INST_DEFINE(n)						\
+	static void uhc_dwc2_irq_enable_func_##n(const struct device *dev)	\
+	{									\
+		esp32_usb_otg_irq_enable_func(dev);				\
+	}									\
+										\
+	static void uhc_dwc2_irq_disable_func_##n(const struct device *dev)	\
+	{									\
+		esp32_usb_otg_irq_disable_func(dev);				\
+	}
+
+DT_INST_FOREACH_STATUS_OKAY(QUIRK_ESP32_USB_OTG_DEFINE)
+
+#endif /* ZEPHYR_DRIVERS_USB_UHC_DWC2_ESP32_USB_OTG_H */

commit 8f3c741ec46940ced90943624c8c0a3f68e78335

drivers: usb: uhc_dwc2: guard optional quirk data
Generic snps,dwc2 instances do not have the vendor quirk data/config
objects emitted by the vendor-specific quirk headers. The driver already
allowed the quirk table pointer to be NULL, but still initialized
quirk_data and quirk_config unconditionally. This causes generic-only
instances to fail to build because the corresponding vendor symbols do
not exist.

Use the same vendor-quirk condition for the table, data, and config
pointers so generic instances get NULL quirk pointers.

Fixes: 89a75416b7b ("drivers: usb: uhc_dwc2: add initial support")

Signed-off-by: Duc Viet Le <levietduc0712@gmail.com>

drivers/usb/uhc/uhc_dwc2.c
@@ -2248,8 +2248,8 @@ static DEVICE_API(uhc, uhc_dwc2_api) = {
 	static const struct uhc_dwc2_config uhc_dwc2_config_##n = {		\
 		UHC_DWC2_DT_INST_REG_ADDR(n),					\
 		.quirks = UHC_DWC2_VENDOR_QUIRK_GET(n),				\
-		.quirk_data = &uhc_dwc2_quirk_data_##n,				\
-		.quirk_config = &uhc_dwc2_quirk_config_##n,			\
+		.quirk_data = UHC_DWC2_VENDOR_QUIRK_DATA_GET(n),		\
+		.quirk_config = UHC_DWC2_VENDOR_QUIRK_CONFIG_GET(n),		\
 		.stack = uhc_dwc2_stack_##n,					\
 		.stack_size = K_THREAD_STACK_SIZEOF(uhc_dwc2_stack_##n),	\
 		.irq_enable_func = uhc_dwc2_irq_enable_func_##n,		\

drivers/usb/uhc/uhc_dwc2.h
@@ -70,9 +70,22 @@ struct uhc_dwc2_config {
 #include "uhc_dwc2_nrf_usbhs_nrf54l.h"
 #endif
 
+#define UHC_DWC2_HAS_VENDOR_QUIRK(n)						\
+	DT_NODE_VENDOR_HAS_IDX(DT_DRV_INST(n), 1)
+
 #define UHC_DWC2_VENDOR_QUIRK_GET(n)						\
-	COND_CODE_1(DT_NODE_VENDOR_HAS_IDX(DT_DRV_INST(n), 1),			\
+	COND_CODE_1(UHC_DWC2_HAS_VENDOR_QUIRK(n),				\
 			(&uhc_dwc2_vendor_quirks_##n),				\
+			(NULL))
+
+#define UHC_DWC2_VENDOR_QUIRK_DATA_GET(n)					\
+	COND_CODE_1(UHC_DWC2_HAS_VENDOR_QUIRK(n),				\
+			(&uhc_dwc2_quirk_data_##n),				\
+			(NULL))
+
+#define UHC_DWC2_VENDOR_QUIRK_CONFIG_GET(n)					\
+	COND_CODE_1(UHC_DWC2_HAS_VENDOR_QUIRK(n),				\
+			(&uhc_dwc2_quirk_config_##n),				\
 			(NULL))
 
 #define DWC2_QUIRK_FUNC_DEFINE(fname)						\