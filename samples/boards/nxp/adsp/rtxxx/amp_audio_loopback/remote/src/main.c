753c03274e91d9e64b248a2269ab1d20952078fd
Vit Stanicek <vit.stanicek@nxp.com>
2026-09-08 13:08:35 +0200

    samples: amp_audio_loopback: Revert to 16 kHz
    Revert the sample rate to 16 kHz. This was done accidentally and may
    cause for the audio buffers not to fit on some platforms.
    
    Signed-off-by: Vit Stanicek <vit.stanicek@nxp.com>

--- samples/boards/nxp/adsp/rtxxx/amp_audio_loopback/remote/src/main.c
+++ samples/boards/nxp/adsp/rtxxx/amp_audio_loopback/remote/src/main.c
@@ -14,7 +14,7 @@
 #define I2S_RX_NODE  DT_ALIAS(i2s_rx)
 #define I2S_TX_NODE  DT_ALIAS(i2s_tx)
 
-#define SAMPLE_FREQUENCY    48000
+#define SAMPLE_FREQUENCY    16000
 #define SAMPLE_BIT_WIDTH    16
 #define BYTES_PER_SAMPLE    sizeof(int16_t)
 #define NUMBER_OF_CHANNELS  2
