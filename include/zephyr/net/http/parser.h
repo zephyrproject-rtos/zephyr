4f3529791eee5767286a268db398a2f296bab971
Yishai Jaffe <yishai1999@gmail.com>
2026-09-09 11:25:02 -0400

    net: http: support configuring the max header size
    Added HTTP_MAX_HEADER_SIZE to Kconfig so that it's configurable.
    
    Signed-off-by: Yishai Jaffe <yishai1999@gmail.com>

--- include/zephyr/net/http/parser.h
+++ include/zephyr/net/http/parser.h
@@ -54,17 +54,6 @@ typedef unsigned __int64 uint64_t;
 extern "C" {
 #endif
 
-/* Maximum header size allowed. If the macro is not defined
- * before including this header then the default is used. To
- * change the maximum header size, define the macro in the build
- * environment (e.g. -DHTTP_MAX_HEADER_SIZE=<value>). To remove
- * the effective limit on the size of the header, define the macro
- * to a very large number (e.g. -DHTTP_MAX_HEADER_SIZE=0x7fffffff)
- */
-#ifndef HTTP_MAX_HEADER_SIZE
-# define HTTP_MAX_HEADER_SIZE (80 * 1024)
-#endif
-
 struct http_parser;
 struct http_parser_settings;
 
