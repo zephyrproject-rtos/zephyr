fccbc8fbdce9e4ff55ec7a181bb9b892faf908bf
Paulo Santos <pauloxrms@gmail.com>
2026-09-10 08:38:53 -0300

    modem: cellular: vendor: request Kconfig CMUX MTU for the rest
    Four vendor scripts still asked the module for a hardcoded N1 of 127
    while the CMUX transmit cap follows CONFIG_MODEM_CMUX_MTU, so raising
    that symbol left the host willing to emit frames the module was told
    never to expect. Convert them as the other thirteen already are.
    
    Signed-off-by: Paulo Santos <pauloxrms@gmail.com>

--- drivers/modem/vendor_modem_cellular/cellular_telit_mex10g1.c
+++ drivers/modem/vendor_modem_cellular/cellular_telit_mex10g1.c
@@ -29,7 +29,9 @@ MODEM_CHAT_SCRIPT_CMDS_DEFINE(
 	MODEM_CHAT_SCRIPT_CMD_RESP_MULT("AT+CGMI", cgmi_match),
 	MODEM_CHAT_SCRIPT_CMD_RESP_MULT("AT+CGMR", cgmr_match),
 	MODEM_CHAT_SCRIPT_CMD_RESP("AT+CFUN=1", ok_match),
-	MODEM_CHAT_SCRIPT_CMD_RESP_NONE("AT+CMUX=0,0,5,127,10,3,30,10,2", 300));
+	MODEM_CHAT_SCRIPT_CMD_RESP_NONE("AT+CMUX=0,0,5," STRINGIFY(CONFIG_MODEM_CMUX_MTU)
+					",10,3,30,10,2",
+					300));
 
 MODEM_CHAT_SCRIPT_DEFINE(telit_mex10g1_init_chat_script, telit_mex10g1_init_chat_script_cmds,
 			 abort_matches, modem_cellular_chat_callback_handler, 10);
