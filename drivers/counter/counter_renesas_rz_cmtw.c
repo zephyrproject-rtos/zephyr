adc92e66156cf7c6ae18ae1e00153118cb9fe560
Duc Bui <duc.bui.vt@bp.renesas.com>
2026-09-10 13:41:21 +0700

    drivers: counter: renesas: Fix const qualifier warning
    Add an explicit (void *) cast when assigning the const device pointer
    to the FSP context field to avoid the build warning.
    
    Signed-off-by: Duc Bui <duc.bui.vt@bp.renesas.com>
    Signed-off-by: Tien Nguyen <tien.nguyen.zg@renesas.com>

--- drivers/counter/counter_renesas_rz_cmtw.c
+++ drivers/counter/counter_renesas_rz_cmtw.c
@@ -618,7 +618,7 @@ void counter_rz_cmtw_cmwi_isr(const struct device *dev)
 		.source_div = DT_ENUM_IDX(RZ_CMTW(inst), prescaler),                               \
 		.channel = DT_PROP(RZ_CMTW(inst), channel),                                        \
 		.p_callback = counter_rz_cmtw_irq_handler,                                         \
-		.p_context = DEVICE_DT_GET(DT_DRV_INST(inst)),                                     \
+		.p_context = (void *)DEVICE_DT_GET(DT_DRV_INST(inst)),                             \
 		.p_extend = &g_timer##inst##_extend,                                               \
 		.cycle_end_ipl = DT_IRQ_BY_NAME(RZ_CMTW(inst), cmwi, priority),                    \
 		.cycle_end_irq = DT_IRQ_BY_NAME(RZ_CMTW(inst), cmwi, irq),                         \
