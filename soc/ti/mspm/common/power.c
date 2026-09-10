d9c844dbd4271d3efc724c66a5b722458f25ee19
Amneesh Singh <amneesh@ti.com>
2026-09-10 19:18:25 +0530

    soc: ti: mspm33c: fix WFI in pm_state_set
    Cortex-M33 has BASEPRI which can prevent wakeup from happening on
    interrupts below a certain priority. This is set to MAX when doing PM state
    entry. So cache the value, set it to zero, do __WFI then restore the value.
    
    Signed-off-by: Amneesh Singh <amneesh@ti.com>

--- soc/ti/mspm/common/power.c
+++ soc/ti/mspm/common/power.c
@@ -125,6 +125,17 @@ static void set_mode_standby(uint8_t state)
 	}
 }
 
+static void enter_wfi(void)
+{
+#ifdef CONFIG_PM_STATE_SET_IRQ_UNLOCKED
+	__WFI();
+#else
+	unsigned int key = arch_pm_state_set_prepare();
+	__WFI();
+	arch_pm_state_set_finish(key);
+#endif /* CONFIG_PM_STATE_SET_IRQ_UNLOCKED */
+}
+
 void pm_state_set(enum pm_state state, uint8_t substate_id)
 {
 	switch (state) {
@@ -142,7 +153,7 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 		return;
 	}
 
-	__WFI();
+	enter_wfi();
 }
 
 void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
