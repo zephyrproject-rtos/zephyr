994c68ec4ce6f48a5974fc750bab0688b81c85c7
Johann Fischer <johann.fischer@nordicsemi.no>
2026-09-10 13:48:53 +0200

    usb: host: fix possible memory leak in usbh_device_set_configuration()
    The user or application should be able to set a specific USB device
    configuration. This is, therefore, a required function that a host stack
    must provide and it needs to be safe to call again on an already
    configured device.
    usbh_device_set_configuration() is not yet ready to be a public
    interface. It leaks memory buffers when called in the configured state.
    Note that this is not reachable in the current state of the host stack.
    
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

--- subsys/usb/host/usbh_device.c
+++ subsys/usb/host/usbh_device.c
@@ -357,6 +357,12 @@ static void reset_configuration(struct usb_device *const udev)
 	udev->state = USB_STATE_ADDRESSED;
 }
 
+/*
+ * usbh_device_set_configuration() does not attempt to restore the previous
+ * configuration. If something fails, the device state will be set to addressed
+ * and all resources will be released. We could increase resilience by reading
+ * all configuration descriptors at once, but that would increase memory usage.
+ */
 int usbh_device_set_configuration(struct usb_device *const udev, const uint8_t num)
 {
 	struct usb_cfg_descriptor cfg_desc;
@@ -374,8 +380,12 @@ int usbh_device_set_configuration(struct usb_device *const udev, const uint8_t n
 		goto error;
 	}
 
+	/* Free any allocated resources unconditionally */
+	k_heap_free(&usb_device_heap, udev->cfg_desc);
+	udev->cfg_desc = NULL;
+	reset_configuration(udev);
+
 	if (num == 0) {
-		reset_configuration(udev);
 		err = usbh_req_set_cfg(udev, num);
 		if (err) {
 			LOG_ERR("Set Configuration %u request failed", num);
@@ -426,10 +436,6 @@ int usbh_device_set_configuration(struct usb_device *const udev, const uint8_t n
 	}
 
 	memset(udev->cfg_desc, 0, cfg_desc.wTotalLength + sizeof(struct usb_desc_header));
-	if (udev->state == USB_STATE_CONFIGURED) {
-		reset_configuration(udev);
-	}
-
 	err = usbh_req_desc_cfg(udev, idx, cfg_desc.wTotalLength, udev->cfg_desc);
 	if (err) {
 		LOG_ERR("Failed to read configuration descriptor of %u bytes: %d",
