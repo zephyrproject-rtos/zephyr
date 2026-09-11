2671f00ae948f960e1c408edf0ac7340c54d73d8
Fabian Blatz <fabianblatz@gmail.com>
2026-09-11 11:27:59 +0200

    manifest: Update LVGL to 9.6.0
    Update the west yaml to point to the new LVGL version.
    Fix the Kconfig symbol deprecation around color formatting/depth
    in all boards/shields.
    
    Signed-off-by: Fabian Blatz <fabianblatz@gmail.com>

--- modules/lvgl/include/lv_conf.h
+++ modules/lvgl/include/lv_conf.h
@@ -8,6 +8,7 @@
 #ifndef ZEPHYR_MODULES_LVGL_LV_CONF_H_
 #define ZEPHYR_MODULES_LVGL_LV_CONF_H_
 
+#include <zephyr/sys/__assert.h>
 #include <zephyr/toolchain.h>
 #include <string.h>
 #include <stdint.h>
@@ -30,12 +31,10 @@
 #define lv_free_core      lvgl_free
 #endif
 
-#define LV_ASSERT_HANDLER         __ASSERT_NO_MSG(false);
-#define LV_ASSERT_HANDLER_INCLUDE "zephyr/sys/__assert.h"
-
-/* Provide definition to align LVGL buffers */
+#define LV_ASSERT_HANDLER      __ASSERT_NO_MSG(false);
 #define LV_ATTRIBUTE_MEM_ALIGN __aligned(CONFIG_LV_ATTRIBUTE_MEM_ALIGN_SIZE)
 
+#define LV_COLOR_16_SWAP_DISABLE_WARNING 1
 #ifdef CONFIG_LV_COLOR_16_SWAP
 #define LV_COLOR_16_SWAP 1
 #endif /* CONFIG_LV_COLOR_16_SWAP */
