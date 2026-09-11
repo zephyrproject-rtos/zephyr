cb3a4d795c30cfb7ad4dd4016881574543d638d2
Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>
2026-09-11 10:47:58 +0200

    tests bsim bluetooth host gatt notify: Don't use redundant mini script
    Don't use the _run_tests.sh script:
    a) it is actually used incorrectly: parameters passed to tests scripts
       should be passed to the phy, instead of dropped. This will for example
       cause to silently not run the phy in check mode when requested.
    b) This script is trivially small (8 lines), while the callers are
       4 lines just to set its variables and call it.
       Let's just do it directly and save this unnecessary layer, and
       the obfuscation it introduces.
    c) It is normal for users to need to tweak parameters to devices,
       run them in valgrind, or the like. That is all hidden when using this
       intermediate _run_tests.sh layer.
    
    Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

--- tests/bsim/bluetooth/host/gatt/notify/test_scripts/gatt_notify_mixed_enhanced.sh
+++ tests/bsim/bluetooth/host/gatt/notify/test_scripts/gatt_notify_mixed_enhanced.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 # Copyright 2022 Nordic Semiconductor ASA
 # SPDX-License-Identifier: Apache-2.0
+set -eu
 
-simulation_id="gatt_notify_mixed_enhanced" \
-    server_id="gatt_server_mixed" \
-    client_id="gatt_client_enhanced" \
-    $(dirname "${BASH_SOURCE[0]}")/_run_test.sh
+source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
+
+simulation_id="${BOARD_TS}_gatt_notify_mixed_enhanced"
+test_exe="bs_${BOARD_TS}_$(guess_test_long_name)_prj_conf"
+verbosity_level=2
+
+cd ${BSIM_OUT_PATH}/bin
+
+Execute ./${test_exe} \
+    -v=${verbosity_level} -s=${simulation_id} -d=0 -testid=gatt_client_enhanced -RealEncryption=1
+
+Execute ./${test_exe} \
+    -v=${verbosity_level} -s=${simulation_id} -d=1 -testid=gatt_server_mixed -RealEncryption=1
+
+Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
+    -D=2 -sim_length=60e6 $@
+
+wait_for_background_jobs
