11cf016daa2cbaae0ec29160acca91363eb37e39
Tim Pambor <tim.pambor@codewrights.de>
2026-09-13 00:38:18 +0200

    drivers: ethernet: dwc_mac: esp32: configure the DMA bus mode
    The bus mode register was left at its reset value, so the AHB master
    issued undefined length bursts and did not align them. Utilize fixed
    length bursts with address aligned beats instead.
    
    Signed-off-by: Tim Pambor <tim.pambor@codewrights.de>

--- drivers/ethernet/dwc_mac/eth_esp32_dwc_ether_1000.c
+++ drivers/ethernet/dwc_mac/eth_esp32_dwc_ether_1000.c
@@ -31,7 +31,7 @@ BUILD_ASSERT(DT_INST_ENUM_HAS_VALUE(0, phy_connection_type, rmii) ||
 		     DT_INST_ENUM_HAS_VALUE(0, phy_connection_type, mii),
 	     "Unsupported PHY interface type. Only RMII and MII are supported.");
 
-/* The DMA bus master interface is 32-bit on this IP */
+/* The DMA bus master interface is a 32-bit AHB interface on this IP */
 #define DATA_BUS_WIDTH 32
 
 DWMAC_ASSERT_BUFFER_ALIGNMENT(DATA_BUS_WIDTH);
@@ -119,6 +119,8 @@ int dwmac_platform_init(const struct device *dev)
 	const struct net_eth_mac_config mac_cfg = NET_ETH_MAC_DT_INST_CONFIG_INIT(0);
 	struct dwmac_priv *p = dev->data;
 	int ret;
+
+	DWMAC_REG_WRITE(DWMAC_DMABMR, DWMAC_DMABMR_AAL | DWMAC_DMABMR_FB);
 
 	p->tx_descs = dwmac_tx_descs;
 	p->rx_descs = dwmac_rx_descs;
