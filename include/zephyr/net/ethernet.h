8e2a8225327483877c5f74900d95c218faafc02e
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2026-09-12 15:46:00 +0200

    tests: net: virtual: Verify that non-virtual interfaces drop VLAN frames
    Add a regression test verifying that virtual_recv() drops packets
    received on non-virtual interfaces when none of the associated VLAN
    counterparts have handled the net_pkt.
    
    The test enables a VLAN interface associated with the eth_test device,
    constructs an R-APS PDU and passes it to net_process_rx_packet() while
    the VLAN interface is down. That the packet is dropped is verified by
    enabling CONFIG_NET_STATISTICS, associating a struct net_stats_eth with
    the eth_test device's API and comparing the values of the errors.rx count
    before and after the call to net_process_rx_packet().
    
    [davidb: adapt to v4.4: get_stats() takes only the device pointer.]
    
    Signed-off-by: Vilhelm Engström <vilhelm.engstrom@tuta.io>
    (cherry picked from commit 5daae48b83e9dd5dc10f66bda82f096fef3aa646)
    Signed-off-by: David Brown <david.brown@linaro.org>

--- include/zephyr/net/ethernet.h
+++ include/zephyr/net/ethernet.h
@@ -56,6 +56,7 @@ struct net_eth_addr {
 
 #define NET_ETH_HDR(pkt) ((struct net_eth_hdr *)net_pkt_data(pkt))
 
+/* clang-format off */
 /* zephyr-keep-sorted-start */
 #define NET_ETH_PTYPE_ALL		0x0003 /* from linux/if_ether.h */
 #define NET_ETH_PTYPE_ARP		0x0806
@@ -68,10 +69,12 @@ struct net_eth_addr {
 #define NET_ETH_PTYPE_IP		0x0800
 #define NET_ETH_PTYPE_IPV6		0x86dd
 #define NET_ETH_PTYPE_LLDP		0x88cc
+#define NET_ETH_PTYPE_OAM		0x8902
 #define NET_ETH_PTYPE_PTP		0x88f7
 #define NET_ETH_PTYPE_TSN		0x22f0 /* TSN (IEEE 1722) packet */
 #define NET_ETH_PTYPE_VLAN		0x8100
 /* zephyr-keep-sorted-stop */
+/* clang-format on */
 
 /* zephyr-keep-sorted-start re(^#define) */
 #if !defined(ETH_P_8021Q)
