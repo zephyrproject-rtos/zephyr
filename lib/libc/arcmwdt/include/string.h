9a516fe4aa05fe0e70719a41301ca5be0927b109
Mohamed Moawad <melhosar@gfoundries.com>
2026-09-13 09:16:22 -0700

    libc: arcmwdt: expose Annex K function declarations
    MWDT advertises Annex K support through __STDC_LIB_EXT1__, but requires
    __STDC_WANT_LIB_EXT1__ to expose functions such as memset_s().
    
    Define the request macro in the string header wrapper so these declarations
    are available.
    
    Signed-off-by: Mohamed Moawad <melhosar@gfoundries.com>

--- lib/libc/arcmwdt/include/string.h
+++ lib/libc/arcmwdt/include/string.h
@@ -7,6 +7,11 @@
 #ifndef LIB_LIBC_ARCMWDT_INCLUDE_STRING_H_
 #define LIB_LIBC_ARCMWDT_INCLUDE_STRING_H_
 
+/* Enable declarations for the Annex K functions advertised by MWDT. */
+#if defined(__STDC_LIB_EXT1__) && !defined(__STDC_WANT_LIB_EXT1__)
+#define __STDC_WANT_LIB_EXT1__ 1
+#endif
+
 #include_next <string.h>
 
 #ifdef __cplusplus
