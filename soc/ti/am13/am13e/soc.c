0c9d8f48380297b5b24405b1e0328d84ade2103f
Aditya Chopra <a-chopra1@ti.com>
2026-09-15 14:05:36 +0530

    soc: ti: am13e: power up XBAR peripheral domain
    PRE_KERNEL_1 hook sets SYSCTL PWREN_XBAR bit; crossbar
    registers are inert until this runs.
    
    Signed-off-by: Aditya Chopra <a-chopra1@ti.com>

---
+++ soc/ti/am13/am13e/soc.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2026 Texas Instruments Incorporated
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/device.h>
+#include <zephyr/init.h>
+#include <zephyr/sys/util.h>
+
+/* SYSCTL_BASE (0x400A_F000) + PWREN_MCPERIPH offset (0x1424) */
+#define SYSCTL_PWREN_MCPERIPH (*(volatile uint32_t *)0x400B0424)
+
+#define PWREN_KEY      (0x26 << 24)
+#define PWREN_XBAR_BIT BIT(14)
+
+static int am13e_power_init(void)
+{
+	SYSCTL_PWREN_MCPERIPH = PWREN_KEY | SYSCTL_PWREN_MCPERIPH | PWREN_XBAR_BIT;
+
+	return 0;
+}
+SYS_INIT(am13e_power_init, PRE_KERNEL_1, 0);
