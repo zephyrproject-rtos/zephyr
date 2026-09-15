f5a2fb09af76905301caee616606e2ea8b116c2c
Amneesh Singh <amneesh@ti.com>
2026-09-15 19:08:50 +0530

    soc: ti: mspm: introduce msp_delay_peripheral_startup
    Several MSP peripherals require a small delay after enabling their
    power. This is configured by CONFIG_MSPM0_PERIPH_STARTUP_DELAY. Since this
    number is too small, k_busy_wait seems overkill, so just do that amount of
    NOPs instead and inline them.
    
    Signed-off-by: Amneesh Singh <amneesh@ti.com>

--- soc/ti/mspm/common/soc.c
+++ soc/ti/mspm/common/soc.c
@@ -10,6 +10,15 @@
 
 #include <soc.h>
 
+/* The BL/BX instructions themselves will be worth 1-3 overhead cycles depending on optimisation */
+void __noinline msp_delay_peripheral_startup(void)
+{
+#define NOP(i, ...)    "nop;"
+#define NOP_N_TIMES(n) LISTIFY(n, NOP, ())
+
+	__asm__ volatile(NOP_N_TIMES(CONFIG_MSPM0_PERIPH_STARTUP_DELAY));
+}
+
 void soc_early_init_hook(void)
 {
 	/* Low Power Mode is configured to be SLEEP0 */
