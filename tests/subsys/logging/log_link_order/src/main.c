e290e3434fc9cabc44e5dbce012f607598ce4f90
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2026-09-15 12:15:34 +0200

    tests: logging: log_link_order: Align test to multi-domain log overhaul
    Align test after changes in handling of remote message.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>

--- tests/subsys/logging/log_link_order/src/main.c
+++ tests/subsys/logging/log_link_order/src/main.c
@@ -18,15 +18,49 @@
 #include <zephyr/logging/log_link.h>
 #include <zephyr/logging/log.h>
 #include <stdbool.h>
+#include "mock_log_link.h"
 
 LOG_MODULE_REGISTER(test);
 
-static log_timestamp_t timestamp;
+static log_timestamp_t exp_timestamp[8];
+static uint8_t exp_domain[8];
+static uint32_t exp_msg_count;
+static uint32_t msg_count;
+static int timestamp_offset;
 
-static void process(const struct log_backend *const backend,
-			union log_msg_generic *msg)
+static void backend_reset(void)
 {
+	exp_msg_count = 0;
+	msg_count = 0;
+}
 
+static void backend_validate(int exp_count)
+{
+	zassert_equal(msg_count, exp_count, "Expected message count mismatch got:%d (expected:%d)",
+		msg_count, exp_count);
+}
+
+static void backend_add_message(log_timestamp_t t, uint8_t domain)
+{
+	exp_timestamp[exp_msg_count] = t;
+	exp_domain[exp_msg_count] = domain;
+	exp_msg_count++;
+}
+
+static void process(const struct log_backend *const backend, union log_msg_generic *msg)
+{
+	uint32_t delta = k_us_to_cyc_floor32(100);
+
+	zassert_equal(msg->log.hdr.desc.domain, exp_domain[msg_count],
+		"Expected domain mismatch got:%d (expected:%d)",
+		msg->log.hdr.desc.domain, exp_domain[msg_count]);
+	zassert_within(msg->log.hdr.timestamp, exp_timestamp[msg_count], delta,
+		"Expected timestamp mismatch got:%d (expected:%d) (now:%d)",
+		msg->log.hdr.timestamp, exp_timestamp[msg_count], k_cycle_get_32());
+	msg_count++;
+	zassert_true(msg_count <= exp_msg_count,
+		"Expected message count mismatch got:%d (expected:%d)",
+		msg_count, exp_msg_count);
 }
 
 static void panic(const struct log_backend *const backend)
@@ -41,158 +75,158 @@ const struct log_backend_api log_backend_test_api = {
 
 LOG_BACKEND_DEFINE(backend1, log_backend_test_api, false);
 
+static log_timestamp_t set_timestamp_offset(int offset)
+{
+	timestamp_offset = offset;
+	return k_cycle_get_32() + offset;
+}
+
 static log_timestamp_t timestamp_get(void)
 {
-	return timestamp++;
+	log_timestamp_t timestamp = k_cycle_get_32() + timestamp_offset;
+
+	timestamp_offset = 0;
+
+	return timestamp;
 }
 
 static void log_setup(void)
 {
 	uint8_t offset = 0;
-	int err;
 
 	log_init();
-
-	timestamp = 0;
-	err = log_set_timestamp_func(timestamp_get, 0);
-	zassert_equal(err, 0);
 
 	(void)z_log_links_activate(0xFFFFFFFF, &offset);
 
 	log_backend_enable(&backend1, NULL, LOG_LEVEL_DBG);
 }
 
-extern struct log_link_api mock_log_link_api;
-
-LOG_LINK_DEF(mock_link1, mock_log_link_api, 0, NULL);
-LOG_LINK_DEF(mock_link2, mock_log_link_api, 512, NULL);
-LOG_LINK_DEF(mock_link3, mock_log_link_api, 1024, NULL);
-
-static void check_msg(int exp_timestamp, int line)
+static void test_log_process(uint32_t exp_msg_count, bool drain, uint32_t timeout)
 {
-	union log_msg_generic *msg;
-	k_timeout_t t;
+	uint32_t now = k_cycle_get_32();
 
-	msg = z_log_msg_claim(&t);
+	while (1) {
+		(void)LOG_PROCESS();
 
-	if (exp_timestamp < 0) {
-		zassert_equal(msg, NULL, "%d: Unexpected msg", line);
+		uint32_t diff = k_cycle_get_32() - now;
+
+		if (k_cyc_to_us_floor32(diff) > timeout) {
+			break;
+		}
+
+		k_msleep(1);
+	}
+
+	if (drain) {
+		backend_reset();
 		return;
 	}
 
-	zassert_true(msg != NULL, "%d: Unexpected msg", line);
-	zassert_equal(msg->log.hdr.timestamp, exp_timestamp, "%d: got:%d, exp:%d",
-			line, (int)msg->log.hdr.timestamp, exp_timestamp);
-
-	z_log_msg_free(msg);
+	backend_validate(exp_msg_count);
 }
 
-#define CHECK_MSG(t) check_msg(t, __LINE__)
+MOCK_LOG_LINK_DEFINE(mock_link1);
+MOCK_LOG_LINK_DEFINE(mock_link2);
+MOCK_LOG_LINK_DEFINE(mock_link3);
 
-ZTEST(log_link_order, test_log_only_local)
+static void remote_message_prepare(const struct log_link *link)
 {
-	struct log_msg log2;
-	struct log_msg *log1;
+	struct log_msg log1;
 
-	log1 = z_log_msg_alloc(sizeof(struct log_msg) / sizeof(int));
-	log1->hdr.desc.type = Z_LOG_MSG_LOG;
-	log1->hdr.desc.package_len = 0;
-	log1->hdr.desc.data_len = 0;
+	log1.hdr.desc.type = Z_LOG_MSG_LOG;
+	log1.hdr.desc.domain = 0;
+	log1.hdr.desc.package_len = 0;
+	log1.hdr.desc.data_len = 0;
+	log1.hdr.timestamp = timestamp_get();
 
-	/* Commit local message. */
-	z_log_msg_commit(log1);
-
-	log2.hdr.desc.type = Z_LOG_MSG_LOG;
-	log2.hdr.desc.package_len = 0;
-	log2.hdr.desc.data_len = 0;
-
-	log2.hdr.timestamp = timestamp_get();
-
-	z_log_msg_enqueue(&mock_link1, &log2, sizeof(log2));
-
-	CHECK_MSG(0);
-	CHECK_MSG(1);
-	CHECK_MSG(-1);
+	mock_log_link_add_message(link, &log1);
 }
 
-ZTEST(log_link_order, test_log_local_unordered)
+ZTEST(log_link_order, test_log_local_remote_ordered)
 {
-	struct log_msg *log1;
-	struct log_msg log2;
+	log_timestamp_t t0, t1;
 
-	/* Get timestamp for second message before first. Simulate that it is
-	 * taken by remote.
-	 */
-	log_timestamp_t t = timestamp_get();
+	/* Create local message with current timestamp. */
+	t0 = set_timestamp_offset(0);
+	LOG_INF("log1");
 
-	log1 = z_log_msg_alloc(sizeof(struct log_msg) / sizeof(int));
+	/* Create link message with timestamp now + 1.*/
+	t1 = set_timestamp_offset(1);
+	remote_message_prepare(&mock_link1);
 
-	log1->hdr.desc.package_len = 0;
-	log1->hdr.desc.data_len = 0;
+	/* Expect ordered processing: first local then remote. */
+	backend_add_message(t0, 0);
+	backend_add_message(t1, 1);
 
-	/* Commit local message. */
-	z_log_msg_commit(log1);
+	test_log_process(2, false, 100000);
+}
+
+ZTEST(log_link_order, test_log_unordered)
+{
+	log_timestamp_t t0, t1;
+
+	int processing_latency = k_us_to_cyc_floor32(CONFIG_LOG_PROCESSING_LATENCY_US + 300);
+
+	/* Create local message with current timestamp. */
+	t0 = set_timestamp_offset(0);
+	LOG_INF("log1");
+
+	/* Message is not processed yet. System is waiting for potential remote messages. */
+	test_log_process(0, false, 0);
 
 	/* Simulate receiving of remote message. It is enqueued later but with
 	 * earlier timestamp.
 	 */
-	log2.hdr.desc.type = Z_LOG_MSG_LOG;
-	log2.hdr.timestamp = t;
-	log2.hdr.desc.package_len = 0;
-	log2.hdr.desc.data_len = 0;
+	t1 = set_timestamp_offset(-processing_latency);
+	remote_message_prepare(&mock_link1);
 
-	z_log_msg_enqueue(&mock_link1, &log2, sizeof(log2));
-
-	CHECK_MSG(1);
-	CHECK_MSG(0);
-	CHECK_MSG(-1);
+	backend_add_message(t1, 1);
+	backend_add_message(t0, 0);
+	test_log_process(2, false, 100000);
 }
 
-ZTEST(log_link_order, test_log_one_remote_ordering)
+ZTEST(log_link_order, test_log_multiple_links)
 {
-	struct log_msg *log1;
-	struct log_msg log2;
+	log_timestamp_t t0, t1, t2, t3, t4;
 
-	/* Get timestamp for second message before first. Simulate that it is
-	 * taken by remote.
+	/* Create local message with current timestamp. */
+	t0 = set_timestamp_offset(0);
+	LOG_INF("log1");
+
+	/* Simulate receiving of remote messages. It is enqueued later but with
+	 * earlier timestamp.
 	 */
-	log_timestamp_t t = timestamp_get();
+	t1 = set_timestamp_offset(-k_us_to_cyc_floor32(700));
+	remote_message_prepare(&mock_link1);
 
-	log1 = z_log_msg_alloc(sizeof(struct log_msg) / sizeof(int));
+	t2 = set_timestamp_offset(-k_us_to_cyc_floor32(680));
+	remote_message_prepare(&mock_link1);
 
-	log1->hdr.desc.package_len = 0;
-	log1->hdr.desc.data_len = 0;
+	t3 = set_timestamp_offset(-k_us_to_cyc_floor32(800));
+	remote_message_prepare(&mock_link2);
 
-	/* Commit local message. */
-	z_log_msg_commit(log1);
+	t4 = set_timestamp_offset(-k_us_to_cyc_floor32(200));
+	remote_message_prepare(&mock_link3);
 
-	/* Simulate receiving of remote message. It is enqueued later but with
-	 * earlier timestamp. However, it is enqueued to link with dedicated buffer
-	 * thus when processing, earliest from the buffers is taken
-	 */
+	/* Messages are processed in the order of their timestamps and not order of arrival. */
+	backend_add_message(t3, 2);
+	backend_add_message(t1, 1);
+	backend_add_message(t2, 1);
+	backend_add_message(t4, 3);
+	backend_add_message(t0, 0);
 
-	log2.hdr.timestamp = t;
-	log2.hdr.desc.type = Z_LOG_MSG_LOG;
-	log2.hdr.desc.package_len = 0;
-	log2.hdr.desc.data_len = 0;
-
-	/* link2 has dedicated buffer. */
-	z_log_msg_enqueue(&mock_link2, &log2, sizeof(log2));
-
-	log2.hdr.timestamp = timestamp_get();
-	/* link2 has dedicated buffer. Log another message with the latest timestamp. */
-	z_log_msg_enqueue(&mock_link2, &log2, sizeof(log2));
-
-	CHECK_MSG(0);
-	CHECK_MSG(1);
-	CHECK_MSG(2);
-	CHECK_MSG(-1);
+	test_log_process(5, false, 100000);
 }
 
 static void before(void *data)
 {
 	ARG_UNUSED(data);
 
+	log_set_timestamp_func(timestamp_get, sys_clock_hw_cycles_per_sec());
+	test_log_process(0, true, 100);
+	mock_log_link_reset(&mock_link1);
+	mock_log_link_reset(&mock_link2);
+	mock_log_link_reset(&mock_link3);
 	log_setup();
 }
 
--- tests/subsys/logging/log_link_order/src/mock_log_link.c
+++ tests/subsys/logging/log_link_order/src/mock_log_link.c
@@ -4,12 +4,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include <string.h>
-#include <zephyr/logging/log_link.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/ztest.h>
+#include "mock_log_link.h"
 
 static int initiate(const struct log_link *link, struct log_link_config *configa)
 {
-	link->ctrl_blk->domain_cnt = 1;
-	link->ctrl_blk->source_cnt[0] = 1;
+	link->ctrl_blk->source_cnt = 1;
 
 	return 0;
 }
@@ -19,16 +20,7 @@ static int activate(const struct log_link *link)
 	return 0;
 }
 
-static int get_domain_name(const struct log_link *link, uint32_t domain_id,
-			char *buf, size_t *length)
-{
-	if (length) {
-		*length = 0;
-	}
-	return 0;
-}
-
-static int get_source_name(const struct log_link *link, uint32_t domain_id,
+static int get_source_name(const struct log_link *link,
 			uint16_t source_id, char *buf, size_t *length)
 {
 	if (length) {
@@ -38,8 +30,8 @@ static int get_source_name(const struct log_link *link, uint32_t domain_id,
 	return 0;
 }
 
-static int get_levels(const struct log_link *link, uint32_t domain_id,
-			uint16_t source_id, uint8_t *level, uint8_t *runtime_level)
+static int get_levels(const struct log_link *link,
+		      uint16_t source_id, uint8_t *level, uint8_t *runtime_level)
 {
 	if (level) {
 		*level = LOG_LEVEL_INF;
@@ -51,17 +43,59 @@ static int get_levels(const struct log_link *link, uint32_t domain_id,
 	return 0;
 }
 
-static int set_runtime_level(const struct log_link *link, uint32_t domain_id,
-				uint16_t source_id, uint8_t level)
+static int set_runtime_level(const struct log_link *link, uint16_t source_id, uint8_t level)
 {
 	return 0;
+}
+
+void mock_log_link_add_message(const struct log_link *link, struct log_msg *msg)
+{
+	size_t msg_len = log_msg_get_total_wlen(msg->hdr.desc) * sizeof(uint32_t);
+	struct mock_log_link_ctx *ctx = (struct mock_log_link_ctx *)link->ctx;
+	void *dst = &ctx->buf[ctx->buf_index];
+
+	memcpy(dst, msg, msg_len);
+	zassert_true(ctx->buf_index + msg_len <= sizeof(ctx->buf), "Buffer overflow");
+	zassert_true(ctx->msg_count < ARRAY_SIZE(ctx->msgs), "Message buffer overflow");
+	ctx->buf_index += msg_len;
+	ctx->msgs[ctx->msg_count++] = (struct log_msg *)dst;
+
+	z_log_msg_remote_notify(1);
+}
+
+void mock_log_link_reset(const struct log_link *link)
+{
+	struct mock_log_link_ctx *ctx = (struct mock_log_link_ctx *)link->ctx;
+
+	ctx->buf_index = 0;
+	ctx->msg_count = 0;
+	ctx->msg_read_index = 0;
+}
+
+static union log_msg_generic *get_msg(const struct log_link *link)
+{
+	struct mock_log_link_ctx *ctx = (struct mock_log_link_ctx *)link->ctx;
+
+	if (ctx->msg_read_index < ctx->msg_count) {
+		return (union log_msg_generic *)ctx->msgs[ctx->msg_read_index];
+	}
+
+	return NULL;
+}
+
+static void put_msg(const struct log_link *link, union log_msg_generic *msg)
+{
+	struct mock_log_link_ctx *ctx = (struct mock_log_link_ctx *)link->ctx;
+
+	ctx->msg_read_index++;
 }
 
 struct log_link_api mock_log_link_api = {
 	.initiate = initiate,
 	.activate = activate,
-	.get_domain_name = get_domain_name,
 	.get_source_name = get_source_name,
 	.get_levels = get_levels,
 	.set_runtime_level = set_runtime_level,
+	.get_msg = get_msg,
+	.put_msg = put_msg,
 };
