88382a5d4d3815abcdad07c1221f0fd909963270
Aman Lachhiramka <a-lachhiramka@ti.com>
2026-09-15 17:59:18 +0530

    soc: ti: am13: add pinctrl_soc.h wrapper for AM13E
    AM13E shares the IOMUX/PINCM interface with MSPM0. Add a thin
    pinctrl_soc.h wrapper that includes the MSPM0 common header.
    
    Signed-off-by: Aman Lachhiramka <a-lachhiramka@ti.com>

---
+++ soc/ti/am13/am13e/pinctrl_soc.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2026 Texas Instruments Incorporated
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_SOC_TI_AM13E_PINCTRL_SOC_H_
+#define ZEPHYR_SOC_TI_AM13E_PINCTRL_SOC_H_
+
+/* AM13E reuses the MSPM0 IOMUX/PINCM pinctrl interface */
+#include "../../mspm/common/pinctrl_soc.h"
+
+#endif /* ZEPHYR_SOC_TI_AM13E_PINCTRL_SOC_H_ */
