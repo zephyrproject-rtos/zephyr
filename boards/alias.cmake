d9ffac1b7b845b3fcab7886b370307f5e716b47a
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2026-09-15 22:00:13 +0200

    cmake: extend board alias to support board qualifiers
    This commit extends the board alias functionality so that it's possible
    to map `<alias_board>/<alias_qualifier>` to `<board>/<qualifier>` in
    addition to board name only aliasing.
    
    Secondly, support for `<zephyr_base>/board/alias.cmake` file is added.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

---
+++ boards/alias.cmake
@@ -0,0 +1,7 @@
+# Board alias file for in-tree aliases.
+#
+# This file allows boards to have an alias that can be used to allow a secondary name.
+# For example `plank/foo` (alias) -> `plank/bar` (real board target)
+# Defined using the form: `set(<alias_board>/<alias_qualifier>_BOARD_ALIAS <board>/<qualifier>)`
+# Example:
+# set(plank/foo_BOARD_ALIAS plank/bar)
