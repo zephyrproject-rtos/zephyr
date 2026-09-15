02e87f22642638a4b505f4ecbf17c2abb7645573
Duc Viet Le <levietduc0712@gmail.com>
2026-09-15 11:46:02 +0700

    drivers: i3c: mcux: fix write length truncation
    Use size_t for the write transfer length instead of uint8_t to
    prevent transfer lengths greater than 255 bytes from being silently
    truncated.
    
    Signed-off-by: Duc Viet Le <levietduc0712@gmail.com>

--- drivers/i3c/i3c_mcux.c
+++ drivers/i3c/i3c_mcux.c
@@ -930,7 +930,7 @@ static int mcux_i3c_do_one_xfer_read(I3C_Type *base, struct mcux_i3c_data *data,
  * @return Number of bytes written, or negative if error.
  */
 static int mcux_i3c_do_one_xfer_write(I3C_Type *base, struct mcux_i3c_data *data,
-				      uint8_t *buf, uint8_t buf_sz, bool no_ending)
+				      uint8_t *buf, size_t buf_sz, bool no_ending)
 {
 	int offset = 0;
 	int remaining = buf_sz;
