67c81a2605b2b4087f6ef5233742903fae056b3f
Mathieu Choplain <mathieu.choplain-ext@st.com>
2026-09-16 17:48:32 +0200

    cmake: modules: boards: integrate all information in FATAL_ERROR message
    When the target board cannot be found, we log a first message then call
    list_boards.py to make it print some additional stuff, before printing
    another message *and only then* calling message(FATAL_ERROR), with a
    message saying to "scroll up"...
    
    Instead, call list_boards.py first and capture what it prints, then
    build a single string and give it directly to message(FATAL_ERROR) such
    that all information is regrouped in a single place. This also makes the
    error more readable on terminals with color support because the error
    message is displayed with emphasis ("in red") compared to subprocess
    output.
    
    This approach also avoids a very unpleasant effect of the previous one:
    since the list_boards.py script is somewhat slow, the first print would
    be visible, then nothing would happen for a few seconds before the rest
    of the message finally appeared and the build halted. Since we build the
    message all at once now, this unpleasant delay is avoided: the error is
    printed then CMake halts immediately.
    
    A second pitfall it avoids is that the "did you mean" hint message can
    be displayed only if list_boards.py actually finds a board for which
    a fuzzy match exists, which avoids a small awkwardness in the error
    message.
    
    Signed-off-by: Mathieu Choplain <mathieu.choplain-ext@st.com>

--- cmake/modules/boards.cmake
+++ cmake/modules/boards.cmake
@@ -241,11 +241,22 @@ elseif(BOARD_DIR)
           "Please run a pristine build."
   )
 else()
-  message("No board named '${BOARD}' found. Did you mean:\n")
-  execute_process(${list_boards_commands} --fuzzy-match ${BOARD})
-  message("\nRun 'west boards' for the full list.")
   unset(CACHED_BOARD CACHE)
-  message(FATAL_ERROR "Invalid BOARD; see above.")
+  execute_process(
+    ${list_boards_commands} --fuzzy-match ${BOARD}
+    OUTPUT_VARIABLE fuzzymatch_boards
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  set(board_hint "")
+  if(NOT "${fuzzymatch_boards}" STREQUAL "")
+    set(board_hint " Did you mean:\n${fuzzymatch_boards}")
+  endif()
+
+  message(FATAL_ERROR
+    "No board named '${BOARD}' found."
+    "${board_hint}\n"
+    "Run 'west boards' for the full list."
+  )
 endif()
 
 cmake_path(IS_PREFIX ZEPHYR_BASE "${BOARD_DIR}" NORMALIZE in_zephyr_tree)
