c48a8c1d9aefe21adbea3708cb94e42d3c7c293f
Mateo Ivankovic <mateo.ivankovic@inovex.de>
2026-09-16 13:08:08 +0200

    drivers: clock_control: fix get_subsys_rate for PTP clock
    get_subsys_rate() did not differentiate between enet and enet1g.
    If both instances were used, only the clock rate of one instance
    would be returned for both.
    The fix uses the instance to differentiate between enet and enet1g.
    
    Signed-off-by: Mateo Ivankovic <mateo.ivankovic@inovex.de>

--- drivers/clock_control/clock_control_mcux_ccm_rev2.c
+++ drivers/clock_control/clock_control_mcux_ccm_rev2.c
@@ -286,6 +286,16 @@ static int mcux_ccm_get_subsys_rate(const struct device *dev,
 	case IMX_CCM_ENET1G_CLK:
 #if defined(CONFIG_SOC_MIMX9352) || defined(CONFIG_SOC_MIMX9131)
 		clock_root = kCLOCK_Root_WakeupAxi;
+#elif defined(CONFIG_SOC_SERIES_IMXRT11XX)
+		if (instance == 1) {
+			//enet
+			clock_root = kCLOCK_Root_Enet_Timer1;
+		} else if (instance == 3) {
+			//enet1g
+			clock_root = kCLOCK_Root_Enet_Timer2;
+		} else {
+			clock_root = kCLOCK_Root_Bus;
+		}
 #else
 		clock_root = kCLOCK_Root_Bus;
 #endif
