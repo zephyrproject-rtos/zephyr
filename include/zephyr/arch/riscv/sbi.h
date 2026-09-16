7b5adfa44afc9022073b29bdda56fdc70f19845b
Ayush Singh <ayush@beagleboard.org>
2026-09-16 13:55:27 +0530

    include: arch: riscv: sbi.h: Add ecall abstraction
    Add abstraction for calling SBI functions as described in Chapter 3 of
    RISCV SBI specification v3 [0].
    
    [0]: https://docs.riscv.org/reference/sbi/v3.0/binary-encoding.html
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- include/zephyr/arch/riscv/sbi.h
+++ include/zephyr/arch/riscv/sbi.h
@@ -48,4 +48,40 @@
 /** @brief SBI return code: requested extension/function is not available */
 #define SBI_ERR_NOT_SUPPORTED		-1
 
+#ifndef _ASMLANGUAGE
+struct sbiret {
+	long error;
+	union {
+		long value;
+		unsigned long uvalue;
+	};
+};
+
+static inline struct sbiret sbi_ecall(unsigned long arg0, unsigned long arg1, unsigned long arg2,
+				      unsigned long arg3, unsigned long arg4, unsigned long arg5,
+				      unsigned long fid, unsigned long ext)
+{
+	struct sbiret ret;
+
+	register unsigned long a0 __asm__("a0") = (arg0);
+	register unsigned long a1 __asm__("a1") = (arg1);
+	register unsigned long a2 __asm__("a2") = (arg2);
+	register unsigned long a3 __asm__("a3") = (arg3);
+	register unsigned long a4 __asm__("a4") = (arg4);
+	register unsigned long a5 __asm__("a5") = (arg5);
+	register unsigned long a6 __asm__("a6") = (fid);
+	register unsigned long a7 __asm__("a7") = (ext);
+
+	__asm__ volatile("ecall"
+			 : "+r"(a0), "+r"(a1)
+			 : "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(a6), "r"(a7)
+			 : "memory");
+
+	ret.error = a0;
+	ret.value = a1;
+
+	return ret;
+}
+#endif /* _ASMLANGUAGE */
+
 #endif /* ZEPHYR_INCLUDE_ARCH_RISCV_SBI_H_ */
