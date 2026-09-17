76ec252f6e0b94a950b0e887953701be179761fc
“Daiyaan Ahmed” <dahmed@qti.qualcomm.com>
2026-09-17 15:50:19 +0530

    modules: hostap: fix size_t format specifiers in log message
    Use %zu when logging values of type size_t to avoid format
    type mismatches and compiler warnings.
    
    Signed-off-by: “Daiyaan Ahmed” <dahmed@qti.qualcomm.com>

--- modules/hostap/src/supp_main.c
+++ modules/hostap/src/supp_main.c
@@ -730,8 +730,8 @@ static void event_socket_handler(int sock, void *eloop_ctx, void *user_data)
 		}
 
 		if (msg->len != sizeof(event_msg)) {
-			LOG_ERR("Received incomplete message: got: %d, expected:%d",
-				msg->len, sizeof(event_msg));
+			LOG_ERR("Received incomplete message: got: %zu, expected:%zu", msg->len,
+				sizeof(event_msg));
 			goto out;
 		}
 
