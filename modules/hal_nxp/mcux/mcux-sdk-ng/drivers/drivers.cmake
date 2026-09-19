1773441bc230de3dafb5e96038c9007be0937684
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2026-09-19 12:12:15 +0800

    modules: hal_nxp: drop the dead XBARA and XBARB glue
    Nothing includes fsl_xbara.h or fsl_xbarb.h and nothing calls the
    XBARA_ or XBARB_ API. The crossbar has one Zephyr driver,
    drivers/mux/mux_nxp_xbar.c, and it programs the registers through the
    device header.
    
    The gating symbols MCUX_XBARA and MCUX_XBARB are never selected or set
    by any board, SoC or driver either, so remove those two Kconfig options
    as well; they were placeholders carrying a "find a better place for this
    option" TODO.
    
    The matching driver directories are removed from hal_nxp in the same
    series.
    
    No functional change intended; verified with hello_world on
    mimxrt1170_evk/mimxrt1176/cm7 (with and without CONFIG_PM=y),
    mimxrt1064_evk and frdm_mcxn947/mcxn947/cpu0.
    
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

--- modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
+++ modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
@@ -142,8 +142,6 @@ set_variable_ifdef(CONFIG_MIPI_DSI_MCUX         CONFIG_MCUX_COMPONENT_driver.mip
 set_variable_ifdef(CONFIG_MIPI_DSI_MCUX_2L      CONFIG_MCUX_COMPONENT_driver.mipi_dsi)
 set_variable_ifdef(CONFIG_MIPI_DSI_NXP_DWC      CONFIG_MCUX_COMPONENT_driver.mipi_dsi_imx)
 set_variable_ifdef(CONFIG_MCUX_SDIF             CONFIG_MCUX_COMPONENT_driver.sdif)
-set_variable_ifdef(CONFIG_MCUX_XBARA            CONFIG_MCUX_COMPONENT_driver.xbara)
-set_variable_ifdef(CONFIG_MCUX_XBARB            CONFIG_MCUX_COMPONENT_driver.xbarb)
 set_variable_ifdef(CONFIG_QDC_MCUX              CONFIG_MCUX_COMPONENT_driver.qdc)
 set_variable_ifdef(CONFIG_EQDC_MCUX             CONFIG_MCUX_COMPONENT_driver.eqdc)
 set_variable_ifdef(CONFIG_QDEC_MCUX             CONFIG_MCUX_COMPONENT_driver.enc)
