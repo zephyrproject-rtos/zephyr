2e36a7d3c5a9ab2ced4e77f77df575363b920c08
Henrik Brix Andersen <hebad@vestas.com>
2026-09-19 11:36:02 +0000

    drivers: net: canbus: init canbus network driver for each CAN controller
    Initialize a canbus network driver for each CAN controller enabled in the
    devicetree.
    
    Signed-off-by: Henrik Brix Andersen <hebad@vestas.com>

--- drivers/net/canbus.c
+++ drivers/net/canbus.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Vestas Wind Systems A/S
+ * Copyright (c) 2022-2026 Vestas Wind Systems A/S
  * Copyright (c) 2022 Alexander Wachter
  * Copyright (c) 2019 Intel Corporation.
  *
@@ -152,12 +152,17 @@ static struct canbus_api net_canbus_api = {
 	.setsockopt = net_canbus_setsockopt,
 };
 
-static struct net_canbus_context net_canbus_ctx;
+#define NET_CANBUS_INIT(node_id)                                                                   \
+	static struct net_canbus_context net_canbus_ctx_##node_id;                                 \
+	static const struct net_canbus_config net_canbus_cfg_##node_id = {                         \
+		.can_dev = DEVICE_DT_GET(node_id),                                                 \
+	};                                                                                         \
+                                                                                                   \
+	NET_DEVICE_INIT(net_canbus_##node_id, DEVICE_DT_NAME(node_id),                             \
+			net_canbus_init, NULL,                                                     \
+			&net_canbus_ctx_##node_id,                                                 \
+			&net_canbus_cfg_##node_id,                                                 \
+			CONFIG_NET_CANBUS_INIT_PRIORITY, &net_canbus_api,                          \
+			CANBUS_RAW_L2, NET_L2_GET_CTX_TYPE(CANBUS_RAW_L2), CAN_MTU);               \
 
-static const struct net_canbus_config net_canbus_cfg = {
-	.can_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_canbus))
-};
-
-NET_DEVICE_INIT(net_canbus, "NET_CANBUS", net_canbus_init, NULL, &net_canbus_ctx, &net_canbus_cfg,
-		CONFIG_NET_CANBUS_INIT_PRIORITY, &net_canbus_api, CANBUS_RAW_L2,
-		NET_L2_GET_CTX_TYPE(CANBUS_RAW_L2), CAN_MTU);
+DT_FOREACH_CLASS_STATUS_OKAY(can, NET_CANBUS_INIT)
