448dc59a2db1bd5201bf78908cb87f74756b5981
meowmeowahr <meowmeowahr@gmail.com>
2026-09-20 00:49:56 -0500

    boards: add stm32h743_can_hs, vendors: add Kevinbot Robotics
    adds the STM32H743 CAN + USB-HS Dev Board from Kevinbot Robotics
    https://kevinbot.net/dev/stm32h743-can-hs/
    
    Additionally adds the `kevinbot` vendor for Kevinbot Robotics
    
    Signed-off-by: meowmeowahr <meowmeowahr@gmail.com>

---
+++ boards/kevinbot/stm32h743_can_hs/board.cmake
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
+board_runner_args(jlink "--device=STM32H743II" "--speed=4000")
+board_runner_args(pyocd "--target=stm32h743xx")
+
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
