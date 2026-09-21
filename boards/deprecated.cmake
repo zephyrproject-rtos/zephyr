5dddba5901e92cd38239dc70e81f19ca179ef93a
Anas Nashif <anas.nashif@intel.com>
2026-09-21 08:53:21 -0400

    boards: deprecated: fix reversed it515xx_evb rename entry
    commit 7b81e406d64a ("board: ite: it515xx_evb: rename it515xx_evb to
    it51xxx_evb") added the deprecation entry with the old and new names
    swapped: it maps the current board it51xxx_evb to the removed
    it515xx_evb. Map the old name it515xx_evb to it51xxx_evb/it51526aw.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- boards/deprecated.cmake
+++ boards/deprecated.cmake
@@ -31,8 +31,8 @@ set(esp32s3_devkitm/esp32s3/appcpu_DEPRECATED
 set(ubx_evk_iris_w1_fidelex/rw612_DEPRECATED
     ubx_evk_iris_w1@fidelix/rw612
 )
-set(it51xxx_evb_DEPRECATED
-    it515xx_evb/it51526aw
+set(it515xx_evb_DEPRECATED
+    it51xxx_evb/it51526aw
 )
 set(weact_stm32h5_core_DEPRECATED
     weact_stm32h562_core
