3417c94eec38aab26df0e32ce3d08f1dee94bca3
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2026-09-22 07:38:43 +0100

    scripts: ci: check_compliance: Add FileNameCheck
    Adds a check which ensures Kconfig fragment files - which are not
    and have never been overlay files - do not begin with overlay in
    the file name
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- scripts/ci/check_compliance.py
+++ scripts/ci/check_compliance.py
@@ -477,6 +477,30 @@ class ClangFormatCheck(ComplianceTest):
                     self._process_patch_error(file, patch)
 
 
+class FileNameCheck(ComplianceTest):
+    """
+    Check if Kconfig files have wrongly be named overlays
+    """
+
+    name = "FileName"
+    doc = zephyr_doc_detail_builder("/develop/application/index.html#application-configuration")
+
+    def run(self):
+        for file in get_files(filter="d"):
+            file_path = Path(file)
+            if file_path.suffix != '.conf':
+                continue
+
+            if file_path.name.lower().startswith('overlay'):
+                self.fmtd_failure(
+                    "error",
+                    "FileNameDisallowed",
+                    file_path,
+                    0,
+                    desc='Kconfig fragment files are not overlay files',
+                )
+
+
 class StyleCheckMixin:
     """
     Shared plumbing for compliance checks that delegate to a stand-alone style
