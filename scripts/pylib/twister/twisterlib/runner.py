dfe05d69d260bebe65f37f6b52521c95c7a915ff
Hake Huang <hake.huang@nxp.com>
2026-09-22 14:16:18 +0800

    twister: match platform extra args against aliases
    Match platform extra_args selectors against the aliases associated with
    a platform. This lets a default board target select its default revision.
    
    Signed-off-by: Hake Huang <hake.huang@nxp.com>

--- scripts/pylib/twister/twisterlib/runner.py
+++ scripts/pylib/twister/twisterlib/runner.py
@@ -1545,7 +1545,7 @@ class ProjectBuilder(FilterBuilder):
                 if self.instance.platform.arch == cond_args[1]:
                     args.append(cond_args[2])
             elif cond_args[0] == "platform" and len(cond_args) == 3:
-                if cond_args[1] in self.instance.platform.name:
+                if cond_args[1] in self.instance.platform.aliases:
                     args.append(cond_args[2])
             elif cond_args[0] == "simulation" and len(cond_args) == 3:
                 if self.instance.platform.simulation == cond_args[1]:
