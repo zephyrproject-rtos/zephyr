84befea709b1ae78806b23c467920e281068fef7
Lucien Zhao <lucien.zhao@nxp.com>
2026-09-22 14:20:50 +0800

    modules: hal_nxp: wire up the RT266x SDK components
    Select the RT266x device and driver components in the mcux-sdk-ng module
    build so the SoC series pulls in its HAL sources.
    
    Signed-off-by: Lucien Zhao <lucien.zhao@nxp.com>

--- modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
+++ modules/hal_nxp/mcux/mcux-sdk-ng/drivers/drivers.cmake
@@ -72,6 +72,7 @@ set_variable_ifdef(CONFIG_DAC_MCUX_DAC12        CONFIG_MCUX_COMPONENT_driver.dac
 set_variable_ifdef(CONFIG_DAC_MCUX_DAC32        CONFIG_MCUX_COMPONENT_driver.dac32)
 set_variable_ifdef(CONFIG_DMA_MCUX_EDMA         CONFIG_MCUX_COMPONENT_driver.edma)
 set_variable_ifdef(CONFIG_DMA_MCUX_EDMA_V3      CONFIG_MCUX_COMPONENT_driver.dma3)
+set_variable_ifdef(CONFIG_DMA_MCUX_EDMA_V4      CONFIG_MCUX_COMPONENT_driver.edma4)
 set_variable_ifdef(CONFIG_DMA_MCUX_EDMA_V4      CONFIG_MCUX_COMPONENT_driver.edma_unified)
 set_variable_ifdef(CONFIG_DMA_MCUX_EDMA_DMAMUX  CONFIG_MCUX_COMPONENT_driver.dmamux)
 set_variable_ifdef(CONFIG_DMA_NXP_EDMA          CONFIG_MCUX_COMPONENT_driver.edma_rev2)
@@ -115,6 +116,7 @@ set_variable_ifdef(CONFIG_DISPLAY_SOCIONEXT_DPU CONFIG_MCUX_COMPONENT_driver.dpu
 set_variable_ifdef(CONFIG_MCUX_PXP              CONFIG_MCUX_COMPONENT_driver.pxp)
 set_variable_ifdef(CONFIG_LV_USE_GPU_NXP_PXP    CONFIG_MCUX_COMPONENT_driver.pxp)
 set_variable_ifdef(CONFIG_GPIO_MCUX_RGPIO       CONFIG_MCUX_COMPONENT_driver.rgpio)
+set_variable_ifdef(CONFIG_GPIO_MCUX_GPIO        CONFIG_MCUX_COMPONENT_driver.gpio)
 set_variable_ifdef(CONFIG_I2S_MCUX_SAI          CONFIG_MCUX_COMPONENT_driver.sai)
 set_variable_ifdef(CONFIG_DAI_NXP_SAI           CONFIG_MCUX_COMPONENT_driver.sai)
 set_variable_ifdef(CONFIG_MEMC_MCUX_FLEXSPI     CONFIG_MCUX_COMPONENT_driver.flexspi)
@@ -464,6 +466,16 @@ endif()
 if(CONFIG_MSPI_NXP_QSPI)
   set_variable_ifdef(CONFIG_SOC_SERIES_MCXE24X CONFIG_MCUX_COMPONENT_driver.qspi_mcxe247)
   set_variable_ifdef(CONFIG_SOC_SERIES_MCXE31X CONFIG_MCUX_COMPONENT_driver.qspi_mcxe31b)
+endif()
+
+# RT266x power and clock components require explicit selection; they are
+# device-level and not auto-selected.  This file runs before device/device.cmake.
+if(CONFIG_SOC_SERIES_IMXRT266X)
+  set(CONFIG_MCUX_COMPONENT_driver.powercon ON)
+  set(CONFIG_MCUX_COMPONENT_driver.pdcon ON)
+  set(CONFIG_MCUX_COMPONENT_driver.rt2k_pmu ON)
+  set(CONFIG_MCUX_COMPONENT_driver.modcon ON)
+  set(CONFIG_MCUX_COMPONENT_driver.memcon ON)
 endif()
 
 # Load all drivers
