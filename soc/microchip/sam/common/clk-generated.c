zephyrproject-rtos/zephyr

commit 21da37b40098786954370c423c6cd120c3880884

soc: microchip: sam: add code for sama7g5 clocks
Add code for sama7g5 Generic Clock, Main Clock, Main System Bus Clock,
Peripheral Clock, Programmable Clock and PLL Clock.

Signed-off-by: Tony Han <tony.han@microchip.com>

soc/microchip/sam/common/clk-generated.c
@@ -1,0 +1,191 @@
+/*
+ * Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <pmc.h>
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
+#include <zephyr/spinlock.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(clk_generated, CONFIG_CLOCK_CONTROL_LOG_LEVEL);
+
+struct clk_generated {
+	struct device clk;
+	struct device *parents[8];
+	pmc_registers_t *pmc;
+	struct clk_range range;
+	struct k_spinlock *lock;
+	uint32_t mux_table[8];
+	uint32_t id;
+	uint32_t gckdiv;
+	const struct clk_pcr_layout *layout;
+	uint8_t num_parents;
+	uint8_t parent_id;
+	int chg_pid;
+};
+
+#define to_clk_generated(ptr) CONTAINER_OF(ptr, struct clk_generated, clk)
+
+static struct clk_generated clocks_gck[ID_PERIPH_MAX];
+static uint32_t clocks_gck_idx;
+
+static int clk_generated_on(const struct device *dev, clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_generated *gck = to_clk_generated(dev);
+	uint32_t mask = gck->layout->cmd | PMC_PCR_GCLKEN_Msk;
+	uint32_t val = gck->layout->cmd | PMC_PCR_GCLKEN_Msk;
+	k_spinlock_key_t key;
+
+	LOG_DBG("gckdiv = %d, parent id = %d\n", gck->gckdiv, gck->parent_id);
+
+	mask |= PMC_PCR_GCLKDIV_Msk | gck->layout->gckcss_mask;
+	val |= FIELD_PREP(PMC_PCR_GCLKDIV_Msk, gck->gckdiv);
+	val |= FIELD_PREP(gck->layout->gckcss_mask, gck->parent_id);
+
+	key = k_spin_lock(gck->lock);
+	regmap_write((void *)gck->pmc, gck->layout->offset, (gck->id & gck->layout->pid_mask));
+	regmap_update_bits((void *)gck->pmc, gck->layout->offset, mask, val);
+	k_spin_unlock(gck->lock, key);
+
+	return 0;
+}
+
+static int clk_generated_off(const struct device *dev, clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_generated *gck = to_clk_generated(dev);
+	k_spinlock_key_t key = k_spin_lock(gck->lock);
+
+	regmap_write((void *)gck->pmc, gck->layout->offset, (gck->id & gck->layout->pid_mask));
+	regmap_update_bits((void *)gck->pmc, gck->layout->offset,
+			   gck->layout->cmd | PMC_PCR_GCLKEN_Msk,
+			   gck->layout->cmd);
+
+	k_spin_unlock(gck->lock, key);
+
+	return 0;
+}
+
+static enum clock_control_status clk_generated_get_status(const struct device *dev,
+							  clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_generated *gck = to_clk_generated(dev);
+	k_spinlock_key_t key = k_spin_lock(gck->lock);
+	uint32_t status;
+
+	regmap_write((void *)gck->pmc, gck->layout->offset, (gck->id & gck->layout->pid_mask));
+	regmap_read((void *)gck->pmc, gck->layout->offset, &status);
+
+	k_spin_unlock(gck->lock, key);
+
+	if (!!(status & PMC_PCR_GCLKEN_Msk)) {
+		return CLOCK_CONTROL_STATUS_ON;
+	} else {
+		return CLOCK_CONTROL_STATUS_OFF;
+	}
+}
+
+static int clk_generated_get_rate(const struct device *dev,
+				  clock_control_subsys_t sys, uint32_t *rate)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_generated *gck = to_clk_generated(dev);
+	const struct device *parent = NULL;
+	uint32_t status, css, i = 0;
+	k_spinlock_key_t key;
+	int ret;
+
+	if (clk_generated_get_status(dev, sys) == CLOCK_CONTROL_STATUS_OFF) {
+		*rate = 0;
+		return 0;
+	}
+
+	key = k_spin_lock(gck->lock);
+	regmap_write((void *)gck->pmc, gck->layout->offset, (gck->id & gck->layout->pid_mask));
+	regmap_read((void *)gck->pmc, gck->layout->offset, &status);
+	k_spin_unlock(gck->lock, key);
+
+	while (!(gck->layout->gckcss_mask & (1 << i))) {
+		i++;
+	}
+	css = (status & gck->layout->gckcss_mask) >> i;
+
+	for (i = 0; i < gck->num_parents; i++) {
+		if (css == gck->mux_table[i]) {
+			parent = gck->parents[i];
+			break;
+		}
+	}
+	if (!parent) {
+		LOG_ERR("find parent clock failed.");
+		return -ENXIO;
+	}
+
+	ret = clock_control_get_rate(parent, NULL, rate);
+	if (ret) {
+		LOG_ERR("get parent clock rate failed.");
+		return ret;
+	}
+	*rate /= (gck->gckdiv + 1);
+
+	return 0;
+}
+
+static DEVICE_API(clock_control, generated_api) = {
+	.on = clk_generated_on,
+	.off = clk_generated_off,
+	.get_rate = clk_generated_get_rate,
+	.get_status = clk_generated_get_status,
+};
+
+int clk_register_generated(pmc_registers_t *const pmc, struct k_spinlock *lock,
+			   const struct clk_pcr_layout *layout,
+			   const char *name,
+			   const struct device **parents, uint32_t *mux_table,
+			   uint8_t num_parents, uint8_t id,
+			   const struct clk_range *range, int chg_pid, struct device **clk)
+{
+	struct clk_generated *gck;
+
+	if (!parents) {
+		return -EINVAL;
+	}
+
+	gck = &clocks_gck[clocks_gck_idx++];
+	if (clocks_gck_idx > ARRAY_SIZE(clocks_gck)) {
+		LOG_ERR("Array for generated clock not enough");
+		return -ENOMEM;
+	}
+	if (num_parents > ARRAY_SIZE(gck->parents)) {
+		LOG_ERR("Array for parent clock not enough");
+		return -ENOMEM;
+	}
+	if (num_parents > ARRAY_SIZE(gck->mux_table)) {
+		LOG_ERR("Array for mux table not enough");
+		return -ENOMEM;
+	}
+	memcpy(gck->parents, parents, sizeof(struct device *) * num_parents);
+
+	*clk = &gck->clk;
+	(*clk)->name = name;
+	(*clk)->api = &generated_api;
+	gck->num_parents = num_parents;
+	gck->id = id;
+	gck->pmc = pmc;
+	gck->lock = lock;
+	gck->range = *range;
+	gck->chg_pid = chg_pid;
+	gck->layout = layout;
+	memcpy(gck->mux_table, mux_table, num_parents * sizeof(mux_table));
+
+	return 0;
+}

soc/microchip/sam/common/CMakeLists.txt
@@ -1,0 +1,17 @@
+# Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+zephyr_include_directories(.)
+
+zephyr_sources(clk-generated.c)
+zephyr_sources(clk-main.c)
+zephyr_sources(clk-master.c)
+zephyr_sources(clk-peripheral.c)
+zephyr_sources(clk-programmable.c)
+zephyr_sources(clk-sam9x60-pll.c)
+zephyr_sources(clk-system.c)
+zephyr_sources(clk-utmi.c)
+zephyr_sources(pmc.c)
+zephyr_sources_ifdef(CONFIG_SOC_SERIES_SAMA7G5 sama7g5.c)

soc/microchip/sam/common/clk-main.c
@@ -1,0 +1,356 @@
+/*
+ * Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <pmc.h>
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(clk_main, CONFIG_CLOCK_CONTROL_LOG_LEVEL);
+
+#define SLOW_CLOCK_FREQ		32768
+#define MAINF_DIV		16
+
+#define clk_main_parent_select(s)	(((s) & \
+					(CKGR_MOR_MOSCXTEN_Msk | \
+					AT91_PMC_OSCBYPASS)) ? 1 : 0)
+
+struct clk_main_osc {
+	struct device clk;
+
+	pmc_registers_t *pmc;
+	uint32_t frequency;
+};
+
+#define to_clk_main_osc(ptr) CONTAINER_OF(ptr, struct clk_main_osc, clk)
+
+struct clk_main_rc_osc {
+	struct device clk;
+
+	pmc_registers_t *pmc;
+	uint32_t frequency;
+};
+
+#define to_clk_main_rc_osc(ptr) CONTAINER_OF(ptr, struct clk_main_rc_osc, clk)
+
+struct clk_main {
+	struct device clk;
+
+	pmc_registers_t *pmc;
+	struct device *parents[2];
+	uint8_t num_parents;
+	uint8_t parent;
+};
+
+#define to_clk_main(ptr) CONTAINER_OF(ptr, struct clk_main, clk)
+
+static struct clk_main_osc main_osc;
+static struct clk_main_rc_osc main_rc_osc;
+static struct clk_main clocks_main;
+
+static int main_osc_on(const struct device *dev, clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_main_osc *osc = to_clk_main_osc(dev);
+	pmc_registers_t *pmc = osc->pmc;
+	uint32_t tmp, status;
+
+	tmp = pmc->CKGR_MOR & ~CKGR_MOR_KEY_Msk;
+
+	if (tmp & AT91_PMC_OSCBYPASS) {
+		return 0;
+	}
+
+	if (!(tmp & CKGR_MOR_MOSCXTEN_Msk)) {
+		tmp |= CKGR_MOR_MOSCXTEN_Msk | CKGR_MOR_KEY_PASSWD;
+		pmc->CKGR_MOR = tmp;
+	}
+
+	do {
+		status = pmc->PMC_SR;
+	} while (!(status & CKGR_MOR_MOSCXTEN_Msk));
+
+	return 0;
+}
+
+static int main_osc_off(const struct device *dev, clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_main_osc *osc = to_clk_main_osc(dev);
+	pmc_registers_t *pmc = osc->pmc;
+	uint32_t tmp;
+
+	tmp = pmc->CKGR_MOR;
+	if (!(tmp & AT91_PMC_OSCBYPASS) && (tmp & CKGR_MOR_MOSCXTEN_Msk)) {
+		tmp &= ~(CKGR_MOR_KEY_PASSWD | CKGR_MOR_MOSCXTEN_Msk);
+		pmc->CKGR_MOR = tmp | CKGR_MOR_KEY_PASSWD;
+	}
+
+	return 0;
+}
+
+static int main_osc_get_rate(const struct device *dev, clock_control_subsys_t sys, uint32_t *rate)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_main_osc *osc = to_clk_main_osc(dev);
+
+	*rate = osc->frequency;
+
+	return 0;
+}
+
+static enum clock_control_status main_osc_get_status(const struct device *dev,
+						     clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_main_osc *osc = to_clk_main_osc(dev);
+	pmc_registers_t *pmc = osc->pmc;
+	uint32_t tmp, status;
+
+	tmp = pmc->CKGR_MOR;
+	if (tmp & AT91_PMC_OSCBYPASS) {
+		return CLOCK_CONTROL_STATUS_ON;
+	}
+
+	status = pmc->PMC_SR;
+	if ((status & PMC_SR_MOSCXTS_Msk) && clk_main_parent_select(tmp)) {
+		return CLOCK_CONTROL_STATUS_ON;
+	} else {
+		return CLOCK_CONTROL_STATUS_OFF;
+	}
+}
+
+static DEVICE_API(clock_control, main_osc_api) = {
+	.on = main_osc_on,
+	.off = main_osc_off,
+	.get_rate = main_osc_get_rate,
+	.get_status = main_osc_get_status,
+};
+
+int clk_register_main_osc(pmc_registers_t *const pmc, const char *name, uint32_t bypass,
+			  uint32_t frequency, struct device **clk)
+{
+	struct clk_main_osc *osc;
+
+	if (!name) {
+		return -EINVAL;
+	}
+
+	osc = &main_osc;
+
+	*clk = &osc->clk;
+	(*clk)->name = name;
+	(*clk)->api = &main_osc_api;
+	osc->pmc = pmc;
+	osc->frequency = frequency;
+
+	if (bypass) {
+		reg_update_bits(pmc->CKGR_MOR, CKGR_MOR_KEY_Msk | AT91_PMC_OSCBYPASS,
+				AT91_PMC_OSCBYPASS | CKGR_MOR_KEY_PASSWD);
+	}
+
+	return 0;
+}
+
+static int main_rc_osc_on(const struct device *dev, clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_main_rc_osc *osc = to_clk_main_rc_osc(dev);
+	pmc_registers_t *pmc = osc->pmc;
+	uint32_t mor, status;
+
+	mor = pmc->CKGR_MOR;
+
+	if (!(mor & CKGR_MOR_MOSCRCEN_Msk)) {
+		reg_update_bits(pmc->CKGR_MOR,
+				CKGR_MOR_KEY_Msk | CKGR_MOR_MOSCRCEN_Msk,
+				CKGR_MOR_MOSCRCEN_Msk | CKGR_MOR_KEY_PASSWD);
+	}
+
+	do {
+		status = pmc->PMC_SR;
+	} while (!(status & PMC_SR_MOSCRCS_Msk));
+
+	return 0;
+}
+
+static int main_rc_osc_off(const struct device *dev, clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_main_rc_osc *osc = to_clk_main_rc_osc(dev);
+	pmc_registers_t *pmc = osc->pmc;
+
+	if (pmc->CKGR_MOR & CKGR_MOR_MOSCRCEN_Msk) {
+		reg_update_bits(pmc->CKGR_MOR,
+				CKGR_MOR_KEY_Msk | CKGR_MOR_MOSCRCEN_Msk, CKGR_MOR_KEY_PASSWD);
+	}
+
+	return 0;
+}
+
+static int main_rc_osc_get_rate(const struct device *dev,
+				clock_control_subsys_t sys, uint32_t *rate)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_main_rc_osc *osc = to_clk_main_rc_osc(dev);
+
+	*rate = osc->frequency;
+
+	return 0;
+}
+
+static enum clock_control_status main_rc_osc_get_status(const struct device *dev,
+							clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_main_rc_osc *osc = to_clk_main_rc_osc(dev);
+	pmc_registers_t *pmc = osc->pmc;
+
+	if ((pmc->CKGR_MOR & CKGR_MOR_MOSCRCEN_Msk) && (pmc->PMC_SR & PMC_SR_MOSCRCS_Msk)) {
+		return CLOCK_CONTROL_STATUS_ON;
+	} else {
+		return CLOCK_CONTROL_STATUS_OFF;
+	}
+}
+
+static DEVICE_API(clock_control, main_rc_osc_api) = {
+	.on = main_rc_osc_on,
+	.off = main_rc_osc_off,
+	.get_rate = main_rc_osc_get_rate,
+	.get_status = main_rc_osc_get_status,
+};
+
+int clk_register_main_rc_osc(pmc_registers_t *const pmc, const char *name,
+			     uint32_t frequency, struct device **clk)
+{
+	struct clk_main_rc_osc *osc;
+
+	if (!name || !frequency) {
+		return -EINVAL;
+	}
+
+	osc = &main_rc_osc;
+
+	*clk = &osc->clk;
+	(*clk)->api = &main_rc_osc_api;
+	(*clk)->name = name;
+	osc->pmc = pmc;
+	osc->frequency = frequency;
+
+	return 0;
+}
+
+static int clk_main_probe_frequency(pmc_registers_t *pmc)
+{
+	uint32_t timeout = 0;
+
+	do {
+		if (pmc->CKGR_MCFR & CKGR_MCFR_MAINFRDY_Msk) {
+			return 0;
+		}
+		k_busy_wait(10);
+	} while (timeout++ < 10);
+	LOG_WRN("Wait Main Clock Frequency Measure Ready timeout.");
+
+	return -ETIMEDOUT;
+}
+
+static inline bool clk_main_ready(pmc_registers_t *pmc)
+{
+	return !!(pmc->PMC_SR & PMC_SR_MOSCSELS_Msk);
+}
+
+static int clk_main_on(const struct device *dev, clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_main *clkmain = to_clk_main(dev);
+	pmc_registers_t *pmc = clkmain->pmc;
+
+	while (!clk_main_ready(pmc)) {
+		k_busy_wait(1);
+	}
+
+	return clk_main_probe_frequency(pmc);
+}
+
+static int clk_main_get_rate(const struct device *dev, clock_control_subsys_t sys, uint32_t *rate)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_main *clkmain = to_clk_main(dev);
+	pmc_registers_t *pmc = clkmain->pmc;
+	uint32_t mcfr;
+
+	if (clkmain->parent == 1) {
+		return clock_control_get_rate(clkmain->parents[1], NULL, rate);
+	}
+
+	mcfr = pmc->CKGR_MCFR;
+	if (!(mcfr & CKGR_MCFR_MAINFRDY_Msk)) {
+		*rate = 0;
+	} else {
+		*rate = ((mcfr & CKGR_MCFR_MAINF_Msk) * SLOW_CLOCK_FREQ) / MAINF_DIV;
+	}
+
+	return 0;
+}
+
+static enum clock_control_status clk_main_get_status(const struct device *dev,
+						     clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_main *clkmain = to_clk_main(dev);
+
+	if (clk_main_ready(clkmain->pmc)) {
+		return CLOCK_CONTROL_STATUS_ON;
+	} else {
+		return CLOCK_CONTROL_STATUS_OFF;
+	}
+}
+
+static DEVICE_API(clock_control, main_api) = {
+	.on = clk_main_on,
+	.get_rate = clk_main_get_rate,
+	.get_status = clk_main_get_status,
+};
+
+int clk_register_main(pmc_registers_t *const pmc, const char *name,
+		      const struct device **parents, int num_parents, struct device **clk)
+{
+	struct clk_main *clkmain;
+	unsigned int status;
+
+	if (!name || !parents || !num_parents) {
+		return -EINVAL;
+	}
+
+	clkmain = &clocks_main;
+	if (num_parents > ARRAY_SIZE(clkmain->parents)) {
+		LOG_ERR("Array for parent clock not enough");
+		return -ENOMEM;
+	}
+	memcpy(clkmain->parents, parents, sizeof(struct device *) * num_parents);
+
+	*clk = &clkmain->clk;
+	(*clk)->name = name;
+	(*clk)->api = &main_api;
+	clkmain->num_parents = num_parents;
+	clkmain->pmc = pmc;
+	status = pmc->CKGR_MOR;
+	clkmain->parent = clk_main_parent_select(status);
+
+	return 0;
+}

soc/microchip/sam/common/clk-master.c
@@ -1,0 +1,320 @@
+/*
+ * Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <pmc.h>
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
+#include <zephyr/spinlock.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(clk_mck, CONFIG_CLOCK_CONTROL_LOG_LEVEL);
+
+#define MASTER_PRES_MASK	0x7
+#define MASTER_PRES_MAX		MASTER_PRES_MASK
+#define MASTER_DIV_SHIFT	8
+#define MASTER_DIV_MASK		0x7
+
+#define PMC_MCR_CSS_SHIFT	16
+
+#define MASTER_MAX_ID		4
+
+#define to_clk_master(ptr) CONTAINER_OF(ptr, struct clk_master, clk)
+
+struct clk_master {
+	pmc_registers_t *pmc;
+	struct device clk;
+	struct device *parents[6];
+	struct k_spinlock *lock;
+	const struct clk_master_layout *layout;
+	const struct clk_master_characteristics *characteristics;
+	uint32_t mux_table[6];
+	uint32_t mckr;
+	uint8_t id;
+	uint8_t num_parents;
+	uint8_t parent;
+	uint8_t div;
+};
+
+static struct clk_master clocks_master[5];
+static uint32_t clocks_master_idx;
+
+static inline bool clk_master_ready(struct clk_master *master)
+{
+	uint32_t bit = master->id ? PMC_SR_MCKXRDY_Msk : PMC_SR_MCKRDY_Msk;
+	uint32_t status;
+
+	status = master->pmc->PMC_SR;
+
+	return !!(status & bit);
+}
+
+static int clk_master_div_on(const struct device *dev, clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_master *master = to_clk_master(dev);
+	k_spinlock_key_t key;
+
+	key = k_spin_lock(master->lock);
+
+	while (!clk_master_ready(master)) {
+		k_busy_wait(1);
+	}
+
+	k_spin_unlock(master->lock, key);
+
+	return 0;
+}
+
+static enum clock_control_status clk_master_div_get_status(const struct device *dev,
+							   clock_control_subsys_t sys)
+
+{
+	ARG_UNUSED(sys);
+
+	struct clk_master *master = to_clk_master(dev);
+	k_spinlock_key_t key;
+	bool status;
+
+	key = k_spin_lock(master->lock);
+	status = clk_master_ready(master);
+	k_spin_unlock(master->lock, key);
+
+	return status ? CLOCK_CONTROL_STATUS_ON : CLOCK_CONTROL_STATUS_OFF;
+}
+
+static int clk_master_div_get_rate(const struct device *dev,
+				   clock_control_subsys_t sys, uint32_t *rate)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_master *master = to_clk_master(dev);
+	uint32_t retval = 0;
+
+	retval = clock_control_get_rate(master->parents[master->parent], NULL, rate);
+	if (!retval) {
+		*rate /= master->div;
+	}
+
+	LOG_DBG("%s, rate %d", dev->name, *rate);
+
+	return retval;
+}
+
+static DEVICE_API(clock_control, master_div_api) = {
+	.on = clk_master_div_on,
+	.get_rate = clk_master_div_get_rate,
+	.get_status = clk_master_div_get_status,
+};
+
+int clk_register_master_div(pmc_registers_t *const pmc, const char *name,
+			    const struct device *parent, const struct clk_master_layout *layout,
+			    const struct clk_master_characteristics *characteristics,
+			    struct k_spinlock *lock, uint32_t safe_div, struct device **clk)
+{
+	struct clk_master *master;
+	k_spinlock_key_t key;
+	uint32_t mckr;
+
+	if (!name || !lock) {
+		return -EINVAL;
+	}
+
+	master = &clocks_master[clocks_master_idx++];
+	if (clocks_master_idx > ARRAY_SIZE(clocks_master)) {
+		LOG_ERR("Array for master clock not enough");
+		return -ENOMEM;
+	}
+
+	*clk = &master->clk;
+	(*clk)->name = name;
+	(*clk)->api = &master_div_api;
+	memcpy(master->parents, &parent, sizeof(struct device *) * 1);
+	master->num_parents = 1;
+	master->parent = 0;
+	master->layout = layout;
+	master->characteristics = characteristics;
+	master->pmc = pmc;
+	master->lock = lock;
+
+	key = k_spin_lock(master->lock);
+	regmap_read((void *)master->pmc, master->layout->offset, &mckr);
+	k_spin_unlock(master->lock, key);
+
+	mckr &= layout->mask;
+	mckr = (mckr >> MASTER_DIV_SHIFT) & MASTER_DIV_MASK;
+	master->div = characteristics->divisors[mckr];
+
+	return 0;
+}
+
+static int clk_mck_get_rate(const struct device *dev, clock_control_subsys_t sys, uint32_t *rate)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_master *master = to_clk_master(dev);
+	int retval = 0;
+
+	const struct device *parent;
+	uint8_t i;
+
+	for (i = 0; ; i++) {
+		if (i >= master->num_parents) {
+			LOG_ERR("clk %s: source %d not found", dev->name, i);
+			return ESRCH;
+		}
+		if (master->mux_table[i] == master->parent) {
+			parent = master->parents[i];
+			break;
+		}
+	}
+
+	retval = clock_control_get_rate(parent, NULL, rate);
+	if (retval) {
+		LOG_ERR("get parent clock rate failed.");
+	} else {
+		if (master->div == MASTER_PRES_MAX) {
+			*rate /= 3;
+		} else {
+			*rate /= 1 << master->div;
+		}
+	}
+	LOG_DBG("clk %s: rate %d", dev->name, *rate);
+
+	return retval;
+}
+
+static int clk_mck_on(const struct device *dev, clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_master *master = to_clk_master(dev);
+	k_spinlock_key_t key;
+	uint32_t val, cparent;
+	uint32_t enable = PMC_MCR_EN_Msk;
+	uint32_t parent = master->parent << PMC_MCR_CSS_SHIFT;
+	uint32_t div = master->div << MASTER_DIV_SHIFT;
+
+	key = k_spin_lock(master->lock);
+
+	master->pmc->PMC_MCR = PMC_MCR_ID(master->id);
+	val = master->pmc->PMC_MCR;
+	reg_update_bits(master->pmc->PMC_MCR,
+			enable | PMC_MCR_CSS_Msk | PMC_MCR_DIV_Msk |
+			PMC_MCR_CMD_Msk | PMC_MCR_ID_Msk,
+			enable | parent | div | PMC_MCR_CMD_Msk |
+			PMC_MCR_ID(master->id));
+
+	cparent = (val & PMC_MCR_CSS_Msk) >> PMC_MCR_CSS_SHIFT;
+
+	/* Wait here only if parent is being changed. */
+	while ((cparent != master->parent) && !clk_master_ready(master)) {
+		k_busy_wait(1);
+	}
+
+	k_spin_unlock(master->lock, key);
+
+	return 0;
+}
+
+static int clk_mck_off(const struct device *dev, clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_master *master = to_clk_master(dev);
+	k_spinlock_key_t key;
+
+	key = k_spin_lock(master->lock);
+	master->pmc->PMC_MCR = master->id;
+	reg_update_bits(master->pmc->PMC_MCR,
+			PMC_MCR_EN_Msk | PMC_MCR_CMD_Msk |
+			PMC_MCR_ID_Msk,
+			PMC_MCR_CMD_Msk |
+			PMC_MCR_ID(master->id));
+	k_spin_unlock(master->lock, key);
+
+	return 0;
+}
+
+static enum clock_control_status clk_mck_get_status(const struct device *dev,
+						    clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_master *master = to_clk_master(dev);
+	k_spinlock_key_t key;
+	uint32_t val;
+
+	key = k_spin_lock(master->lock);
+
+	master->pmc->PMC_MCR = master->id;
+	val = master->pmc->PMC_MCR;
+
+	k_spin_unlock(master->lock, key);
+
+	if (!!(val & PMC_MCR_EN_Msk)) {
+		return CLOCK_CONTROL_STATUS_ON;
+	} else {
+		return CLOCK_CONTROL_STATUS_OFF;
+	}
+}
+
+static DEVICE_API(clock_control, sama7g5_master_api) = {
+	.on = clk_mck_on,
+	.off = clk_mck_off,
+	.get_rate = clk_mck_get_rate,
+	.get_status = clk_mck_get_status,
+};
+
+int clk_register_master(pmc_registers_t *const pmc,
+			const char *name, int num_parents,
+			const struct device **parents,
+			uint32_t *mux_table,
+			struct k_spinlock *lock, uint8_t id,
+			struct device **clk)
+{
+	struct clk_master *master;
+	k_spinlock_key_t key;
+	unsigned int val;
+
+	if (!num_parents || !parents || !mux_table || !lock || id > MASTER_MAX_ID) {
+		return -EINVAL;
+	}
+
+	master = &clocks_master[clocks_master_idx++];
+	if (clocks_master_idx > ARRAY_SIZE(clocks_master)) {
+		LOG_ERR("Array for master clock not enough");
+		return -ENOMEM;
+	}
+	if (num_parents > ARRAY_SIZE(master->parents)) {
+		LOG_ERR("Array for parent clock not enough");
+		return -ENOMEM;
+	}
+	if (num_parents > ARRAY_SIZE(master->mux_table)) {
+		LOG_ERR("Array for mux table not enough");
+		return -ENOMEM;
+	}
+	memcpy(master->parents, parents, sizeof(struct device *) * num_parents);
+
+	*clk = &master->clk;
+	(*clk)->name = name;
+	(*clk)->api = &sama7g5_master_api;
+
+	master->num_parents = num_parents;
+	master->pmc = pmc;
+	master->id = id;
+	master->lock = lock;
+	memcpy(master->mux_table, mux_table, num_parents * sizeof(mux_table));
+
+	key = k_spin_lock(master->lock);
+	pmc->PMC_MCR = master->id;
+	val = pmc->PMC_MCR;
+	master->parent = (val & PMC_MCR_CSS_Msk) >> PMC_MCR_CSS_SHIFT;
+	master->div = (val & PMC_MCR_DIV_Msk) >> MASTER_DIV_SHIFT;
+	k_spin_unlock(master->lock, key);
+
+	return 0;
+}

soc/microchip/sam/common/clk-peripheral.c
@@ -1,0 +1,159 @@
+/*
+ * Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#include <pmc.h>
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
+#include <zephyr/spinlock.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(clk_peripheral, CONFIG_CLOCK_CONTROL_LOG_LEVEL);
+
+#define PERIPHERAL_ID_MIN	2
+
+struct clk_peripheral {
+	pmc_registers_t *pmc;
+	struct device clk;
+	struct device *parent;
+	struct clk_range range;
+	struct k_spinlock *lock;
+	uint32_t id;
+	const struct clk_pcr_layout *layout;
+};
+
+static struct clk_peripheral clocks_periph[72];
+static uint32_t clocks_periph_idx;
+
+#define to_clk_peripheral(ptr) CONTAINER_OF(ptr, struct clk_peripheral, clk)
+
+static void clk_peripheral_set(struct clk_peripheral *periph,
+			       uint32_t enable)
+{
+	k_spinlock_key_t key = k_spin_lock(periph->lock);
+
+	LOG_DBG("id %d, enable %d", periph->id, enable);
+	enable = enable ? PMC_PCR_EN_Msk : 0;
+	regmap_write((void *)periph->pmc, periph->layout->offset,
+		     periph->id & periph->layout->pid_mask);
+	regmap_update_bits((void *)periph->pmc, periph->layout->offset,
+			   periph->layout->cmd | enable,
+			   periph->layout->cmd | enable);
+
+	k_spin_unlock(periph->lock, key);
+}
+
+static int clk_peripheral_on(const struct device *clk,
+			     clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	clk_peripheral_set(to_clk_peripheral(clk), 1);
+
+	return 0;
+}
+
+static int clk_peripheral_off(const struct device *clk,
+			      clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	clk_peripheral_set(to_clk_peripheral(clk), 0);
+
+	return 0;
+}
+
+static enum clock_control_status clk_peripheral_get_status(const struct device *dev,
+							   clock_control_subsys_t sys)
+{
+	ARG_UNUSED(sys);
+
+	struct clk_peripheral *periph = to_clk_peripheral(dev);
+	k_spinlock_key_t key;
+	uint32_t status;
+
+	if (periph->id < PERIPHERAL_ID_MIN) {
+		return CLOCK_CONTROL_STATUS_UNKNOWN;
+	}
+
+	key = k_spin_lock(periph->lock);
+	regmap_write((void *)periph->pmc, periph->layout->offset,
+		     (periph->id & periph->layout->pid_mask));
+	regmap_read((void *)periph->pmc, periph->layout->offset, &status);
+	k_spin_unlock(periph->lock, key);
+
+	if (!!(status & PMC_PCR_EN_Msk)) {
+		return CLOCK_CONTROL_STATUS_ON;
+	} else {
+		return CLOCK_CONTROL_STATUS_OFF;
+	}
+}
+
+static int clk_peripheral_get_rate(const struct device *clk,
+				   clock_control_subsys_t sys,
+				   uint32_t *rate)
+{
+	ARG_UNUSED(sys);
+
+	uint32_t retval = 0;
+	uint32_t status;
+
+	struct clk_peripheral *periph = to_clk_peripheral(clk);
+
+	regmap_write((void *)periph->pmc, periph->layout->offset,
+		     (periph->id & periph->layout->pid_mask));
+	regmap_read((void *)periph->pmc, periph->layout->offset, &status);
+
+	if (status & PMC_PCR_EN_Msk) {
+		retval = clock_control_get_rate(periph->parent, NULL, rate);
+	} else {
+		*rate = 0;
+	}
+	LOG_DBG("id %d, rate %d", periph->id, *rate);
+
+	return retval;
+}
+
+static DEVICE_API(clock_control, clk_peripheral_api) = {
+	.on = clk_peripheral_on,
+	.off = clk_peripheral_off,
+	.get_rate = clk_peripheral_get_rate,
+	.get_status = clk_peripheral_get_status,
+};
+
+int clk_register_peripheral(pmc_registers_t *const pmc,
+			    struct k_spinlock *lock,
+			    const struct clk_pcr_layout *layout,
+			    const char *name,
+			    struct device *parent,
+			    uint32_t id,
+			    const struct clk_range *range,
+			    struct device **clk)
+{
+	struct clk_peripheral *periph;
+
+	if (!name) {
+		return -EINVAL;
+	}
+
+	periph = &clocks_periph[clocks_periph_idx++];
+	if (clocks_periph_idx > ARRAY_SIZE(clocks_periph)) {
+		LOG_ERR("Array for peripheral clock not enough");
+		return -ENOMEM;
+	}
+
+	*clk = &periph->clk;
+	periph->clk.name = name;
+	periph->clk.api = &clk_peripheral_api;
+	periph->parent = parent;
+	periph->id = id;
+	periph->pmc = pmc;
+	periph->lock = lock;
+	periph->layout = layout;
+	periph->range = *range;
+
+	return 0;
+}

commit 1eb83e5d9b641956363edf8c9deca087e9242f62

soc: microchip: sam: common: fix gck enable with omitting div and css
clk_generated_on() updated div and css fields with cached gck_div and
parent_id values, which may change the gck unexpectedly. Remove the
lines that configure divisor and clock source selector, restoring the
enable path to write only the CMD and GCLKEN bits. Also removes the
LOG_DBG call that logged the unused values.

Fixes: 21da37b40098 ("soc: microchip: sam: add code for sama7g5 clocks")

Signed-off-by: Tony Han <tony.han@microchip.com>

soc/microchip/sam/common/clk-generated.c
@@ -41,12 +41,6 @@ static int clk_generated_on(const struct device *dev, clock_control_subsys_t sys
 	uint32_t val = gck->layout->cmd | PMC_PCR_GCLKEN_Msk;
 	k_spinlock_key_t key;
 
-	LOG_DBG("gckdiv = %d, parent id = %d\n", gck->gckdiv, gck->parent_id);
-
-	mask |= PMC_PCR_GCLKDIV_Msk | gck->layout->gckcss_mask;
-	val |= FIELD_PREP(PMC_PCR_GCLKDIV_Msk, gck->gckdiv);
-	val |= FIELD_PREP(gck->layout->gckcss_mask, gck->parent_id);
-
 	key = k_spin_lock(gck->lock);
 	regmap_write((void *)gck->pmc, gck->layout->offset, (gck->id & gck->layout->pid_mask));
 	regmap_update_bits((void *)gck->pmc, gck->layout->offset, mask, val);