7f27bee0f13cd4d73904bbc34af70e533c609278
Chris Friedt <chris@fr4.co>
2026-09-23 20:19:16 -0400

    posix: add <netpacket/packet.h>
    Provide the packet socket address header so that applications and
    modules can include <netpacket/packet.h> as they do on other systems.
    The Zephyr header pulls in the native packet socket definitions from
    <zephyr/net/net_ip.h>, and the newlib and picolibc shims forward to it
    since neither C library ships this header.
    
    Signed-off-by: Chris Friedt <chris@fr4.co>

---
+++ lib/libc/newlib/include/netpacket/packet.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright The Zephyr Project Contributors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_LIB_LIBC_NEWLIB_INCLUDE_NETPACKET_PACKET_H_
+#define ZEPHYR_LIB_LIBC_NEWLIB_INCLUDE_NETPACKET_PACKET_H_
+
+#include <zephyr/posix/netpacket/packet.h>
+
+#endif /* ZEPHYR_LIB_LIBC_NEWLIB_INCLUDE_NETPACKET_PACKET_H_ */
